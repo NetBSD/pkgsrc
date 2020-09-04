9500eb7a9a663eff5a06be876e27072b097d6cff
taca <taca@pkgsrc.org>
2020-09-04 15:01:04 +0000

    lang/php74: udpate to 7.4.10
    Update php74 to 7.4.10.
    
    
    03 Sep 2020, PHP 7.4.10
    
    - Core:
      . Fixed bug #79884 (PHP_CONFIG_FILE_PATH is meaningless). (cmb)
      . Fixed bug #77932 (File extensions are case-sensitive). (cmb)
      . Fixed bug #79806 (realpath() erroneously resolves link to link). (cmb)
      . Fixed bug #79895 (PHP_CHECK_GCC_ARG does not allow flags with equal sign).
        (Santiago M. Mola)
      . Fixed bug #79919 (Stack use-after-scope in define()). (cmb)
      . Fixed bug #79934 (CRLF-only line in heredoc causes parsing error).
        (Pieter van den Ham)
      . Fixed bug #79947 (Memory leak on invalid offset type in compound
        assignment). (Nikita)
    
    - COM:
      . Fixed bug #48585 (com_load_typelib holds reference, fails on second call).
        (cmb)
    
    - Exif:
      . Fixed bug #75785 (Many errors from exif_read_data).
        (Níckolas Daniel da Silva)
    
    - Gettext:
      . Fixed bug #70574 (Tests fail due to relying on Linux fallback behavior for
        gettext()). (Florian Engelhardt)
    
    - LDAP:
      . Fixed memory leaks. (ptomulik)
    
    - OPcache:
      . Fixed bug #73060 (php failed with error after temp folder cleaned up).
        (cmb)
      . Fixed bug #79917 (File cache segfault with a static variable in inherited
        method). (Nikita)
    
    - PDO:
      . Fixed bug #64705 (errorInfo property of PDOException is null when
        PDO::__construct() fails). (Ahmed Abdou)
    
    - Session:
      . Fixed bug #79724 (Return type does not match in ext/session/mod_mm.c).
        (Nikita)
    
    - Standard:
      . Fixed bug #79930 (array_merge_recursive() crashes when called with array
        with single reference). (Nikita)
      . Fixed bug #79944 (getmxrr always returns true on Alpine linux). (Nikita)
      . Fixed bug #79951 (Memory leak in str_replace of empty string). (Nikita)
    
    - XML:
      . Fixed bug #79922 (Crash after multiple calls to xml_parser_free()). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.307 2020/09/04 14:59:16 taca Exp $
+# $NetBSD: phpversion.mk,v 1.308 2020/09/04 15:01:04 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.33
 PHP73_VERSION=	7.3.22
-PHP74_VERSION=	7.4.9
+PHP74_VERSION=	7.4.10
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
