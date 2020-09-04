9ac977132cfd7bd147e3260b11d160106a101aa3
js <js@pkgsrc.org>
2020-09-04 19:36:30 +0000

    mk/platform/QNX.mk: Add /usr/qnx650 to _OPSYS_LIB_DIRS

--- mk/platform/QNX.mk
+++ mk/platform/QNX.mk
@@ -49,7 +49,7 @@ IMAKE_MISCMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}7
 IMAKE_MANINSTALL?=	maninstall catinstall
 
 _OPSYS_SYSTEM_RPATH?=	/lib:/usr/lib	# STANDARD_LIBRARY_PATH
-_OPSYS_LIB_DIRS?=	/lib /usr/lib
+_OPSYS_LIB_DIRS?=	/lib /usr/lib /usr/qnx650
 _OPSYS_INCLUDE_DIRS?=	/usr/include
 
 .if exists(/usr/include/netinet6)
