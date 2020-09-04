6d5823f0ea44dfe0892bf739ef36cee5f7f8dc64
prlw1 <prlw1@pkgsrc.org>
2020-09-04 10:50:08 +0000

    Update harfbuzz to 2.7.2
    Add introspection option defaulting to ON as new pango needs this.
    
    Overview of changes leading to 2.7.2
    Saturday, August 29, 2020
    ====================================
    - Fix a regression in the previous release that caused a crash with Kaithi.
    - More OOM fixes.

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.8 2020/08/16 10:40:16 leot Exp $
+# $NetBSD: options.mk,v 1.9 2020/09/04 10:50:08 prlw1 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
-PKG_SUPPORTED_OPTIONS+=	doc icu
-PKG_SUGGESTED_OPTIONS=	doc icu
+PKG_SUPPORTED_OPTIONS+=	doc icu introspection
+PKG_SUGGESTED_OPTIONS=	doc icu introspection
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		doc icu
+PLIST_VARS+=		doc icu introspection
 
 .if !empty(PKG_OPTIONS:Mdoc)
 MESON_ARGS+=	-Ddocs=enabled
@@ -22,4 +22,13 @@ MESON_ARGS+=	-Dicu=enabled
 PLIST.icu=		yes
 .else
 MESON_ARGS+=	-Dicu=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mintrospection)
+BUILDLINK_DEPMETHOD.gobject-introspection+=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+MESON_ARGS+=	-Dintrospection=enabled
+PLIST.introspection=	yes
+.else
+MESON_ARGS+=	-Dintrospection=disabled
 .endif
