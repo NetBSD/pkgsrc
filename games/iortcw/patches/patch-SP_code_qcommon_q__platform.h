72b42f7c714bdac955a7c25cb17a0cd5a0857ceb
nia <nia@pkgsrc.org>
2020-09-09 11:22:27 +0000

    iortcw: Remove hardcoded list of CPU architectures

--- games/iortcw/patches/patch-SP_code_qcommon_q__platform.h
+++ games/iortcw/patches/patch-SP_code_qcommon_q__platform.h
@@ -1,15 +1,42 @@
-$NetBSD: patch-SP_code_qcommon_q__platform.h,v 1.1 2019/11/17 17:56:48 nia Exp $
+$NetBSD: patch-SP_code_qcommon_q__platform.h,v 1.2 2020/09/09 11:22:27 nia Exp $
 
-Always call it x86_64 regardless of operating system.
+Remove hardcoded list of architectures.
 
 --- SP/code/qcommon/q_platform.h.orig	2019-03-16 18:09:48.000000000 +0000
 +++ SP/code/qcommon/q_platform.h
-@@ -226,7 +226,7 @@ Foundation, Inc., 51 Franklin St, Fifth 
- #elif defined __amd64__
+@@ -201,7 +201,7 @@ Foundation, Inc., 51 Franklin St, Fifth 
+ 
+ //=================================================================== BSD ===
+ 
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+ 
+ #include <sys/types.h>
+ #include <machine/endian.h>
+@@ -216,19 +216,20 @@ Foundation, Inc., 51 Franklin St, Fifth 
+ #define OS_STRING "openbsd"
+ #elif defined(__NetBSD__)
+ #define OS_STRING "netbsd"
++#elif defined(__DragonFly__)
++#define OS_STRING "dragonfly"
+ #endif
+ 
+ #define ID_INLINE inline
+ #define PATH_SEP '/'
+ 
+-#ifdef __i386__
+-#define ARCH_STRING "i386"
+-#elif defined __amd64__
++#if !defined(ARCH_STRING)
++# error ARCH_STRING should be defined by the Makefile
++#endif
++
++#if defined __x86_64__
  #undef idx64
  #define idx64 1
 -#define ARCH_STRING "amd64"
-+#define ARCH_STRING "x86_64"
- #elif defined __axp__
- #define ARCH_STRING "alpha"
+-#elif defined __axp__
+-#define ARCH_STRING "alpha"
  #endif
+ 
+ #if BYTE_ORDER == BIG_ENDIAN
