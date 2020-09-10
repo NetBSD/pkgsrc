f06fcb0d7afaffe9b9e48b7549338a771e34a59d
nia <nia@pkgsrc.org>
2020-09-10 10:10:17 +0000

    libretro-fbneo: Unbreak gcc7 build.
    Tried to fix this properly then got confused by C++ nonsense.

--- emulators/libretro-fbneo/patches/patch-src_burner_libretro_retro__common.h
+++ emulators/libretro-fbneo/patches/patch-src_burner_libretro_retro__common.h
@@ -1,15 +1,18 @@
-$NetBSD: patch-src_burner_libretro_retro__common.h,v 1.1 2020/08/11 18:47:14 joerg Exp $
+$NetBSD: patch-src_burner_libretro_retro__common.h,v 1.2 2020/09/10 10:10:18 nia Exp $
 
 Don't cast lvalues.
 
---- src/burner/libretro/retro_common.h.orig	2020-08-10 22:37:10.415674566 +0000
+--- src/burner/libretro/retro_common.h.orig	2020-05-30 15:55:11.000000000 +0000
 +++ src/burner/libretro/retro_common.h
-@@ -7,7 +7,7 @@
+@@ -7,7 +7,11 @@
  #include <vector>
  #include "burner.h"
  
--#define SSTR( x ) static_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
++#ifndef __clang__
+ #define SSTR( x ) static_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
++#else
 +#define SSTR( x ) ((( std::ostringstream() << std::dec << x ) ).str())
++#endif
  
  #define RETRO_GAME_TYPE_CV		1
  #define RETRO_GAME_TYPE_GG		2
