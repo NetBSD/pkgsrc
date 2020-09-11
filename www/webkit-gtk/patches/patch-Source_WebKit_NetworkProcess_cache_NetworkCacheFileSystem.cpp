b1252359d7ea1f4b674bdc02d275d6516cc84310
leot <leot@pkgsrc.org>
2020-09-11 14:47:34 +0000

    webkit-gtk: Update to 2.30.0
    pkgsrc changes:
     - Unconditionally disable systemd support via CMAKE_ARGS (by default systemd is
       enabled unconditionally)
    
    Changes:
    WebKitGTK 2.30.0
    ================
      - Fix NTLM authentication.
      - Don't try to use SHM on wayland with older wpebackend-fdo.
      - Fix the build due to seccomp.h header not found.
    
    WebKitGTK 2.29.92
    =================
      - Fix user agent header after a redirect when a new quirk is required.
      - Stop using firefox user agent quirk for google docs.
      - Fix rendering frames timeline panel in web inspector.
      - Fix per-thread cpu usage in web inspector.
      - Fix several crashes and rendering issues.
      - Translation updates: Polish
    
    WebKitGTK 2.29.91
    =================
      - Fix a web process crash introduced in 2.29.90.
    
    WebKitGTK 2.29.90
    =================
      - Fix font variation settings when font smoothing setting is also present.
      - Fix HTML drag and drop operations.
      - Fix argument order for clone syscall seccomp filter on s390x.
      - Fix a crash when selecting text.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.29.4
    ================
      - Add support for backdrop filters.
      - Add support for text-underline-offset and text-decoration-thickness.
      - Add OpenCDM and AV1 support to media backend.
      - Add new API to get ITP data summary.
      - Use mobile user-agent on tablets.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.29.3
    ================
      - Add webkit_authentication_request_get_security_origin.
      - Change the cookies accept policy to always when no-third-party is set and ITP is enabled.
      - Fix web process hangs on large GitHub pages.
      - Bubblewrap sandbox should not attempt to bind empty paths.
      - Add support for sndio to bubblewrap sandbox.
      - Also handle dark themes when the name ends with -Dark.
      - Fix a race condition causing a crash in media player.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.29.2
    ================
     - Add Intelligent Tracking Prevention (ITP) support.
     - Add support for video formats in img elements.
     - Add API to handle video autoplay policy that now defaults to disallow autoplay videos with audio.
     - Add API to mute a web view.
     - Add API to allow applications to handle the HTTP authentication credential storage.
     - Add a WebKitSetting to set the media content types requiring hardware support.
     - Fix a crash during drag an drop due to a bug introduced in 2.29.1.
     - Do not start page load during animation in back/forward gesture.
     - Fix several crashes and rendering issues.
     - Translation updates: Ukrainian.
    
    WebKitGTK 2.29.1
    ================
     - Stop using GTK theming to render form controls.
     - Add API to disable GTK theming for scrollbars too.
     - Fix several race conditions and threading issues in the media player.
     - Add USER_AGENT_BRANDING build option.
     - Add paste as plain text option to the context menu for rich editable content.
     - Fix several crashes and rendering issues.

--- www/webkit-gtk/patches/patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp
+++ www/webkit-gtk/patches/patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp
@@ -1,53 +1,19 @@
-$NetBSD: patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp,v 1.3 2019/03/13 20:49:41 leot Exp $
+$NetBSD: patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp,v 1.4 2020/09/11 14:47:35 leot Exp $
 
 SunOS does not support DT_{DIR,REG}
 
---- Source/WebKit/NetworkProcess/cache/NetworkCacheFileSystem.cpp.orig	2019-02-12 11:21:18.000000000 +0000
+--- Source/WebKit/NetworkProcess/cache/NetworkCacheFileSystem.cpp.orig	2020-08-12 09:17:56.000000000 +0000
 +++ Source/WebKit/NetworkProcess/cache/NetworkCacheFileSystem.cpp
-@@ -52,6 +52,12 @@ namespace WebKit {
- namespace NetworkCache {
- 
- #if !OS(WINDOWS)
+@@ -36,6 +36,12 @@
+ #include <dirent.h>
+ #include <sys/stat.h>
+ #include <sys/time.h>
 +#ifndef DT_DIR
 +#define DT_DIR	S_IFDIR
 +#endif
 +#ifndef DT_REG
 +#define DT_REG	S_IFREG
 +#endif
- static DirectoryEntryType directoryEntryType(uint8_t dtype)
- {
-     switch (dtype) {
-@@ -69,13 +75,21 @@ static DirectoryEntryType directoryEntry
- 
- void traverseDirectory(const String& path, const Function<void (const String&, DirectoryEntryType)>& function)
- {
-+#if OS(SOLARIS)
-+    struct stat s;
-+#endif
- #if !OS(WINDOWS)
-     DIR* dir = opendir(FileSystem::fileSystemRepresentation(path).data());
-     if (!dir)
-         return;
-     dirent* dp;
-     while ((dp = readdir(dir))) {
-+#if OS(SOLARIS)
-+        stat(dp->d_name, &s);
-+        if (s.st_mode != S_IFDIR && s.st_mode != S_IFREG)
-+#else
-         if (dp->d_type != DT_DIR && dp->d_type != DT_REG)
-+#endif
-             continue;
-         const char* name = dp->d_name;
-         if (!strcmp(name, ".") || !strcmp(name, ".."))
-@@ -83,7 +97,11 @@ void traverseDirectory(const String& pat
-         auto nameString = String::fromUTF8(name);
-         if (nameString.isNull())
-             continue;
-+#if OS(SOLARIS)
-+        function(nameString, directoryEntryType(s.st_mode));
-+#else
-         function(nameString, directoryEntryType(dp->d_type));
-+#endif
-     }
-     closedir(dir);
  #else
+ #include <windows.h>
+ #endif
