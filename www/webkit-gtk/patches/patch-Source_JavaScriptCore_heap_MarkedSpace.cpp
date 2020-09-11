ecfa8e77d81f50f6d95baec471407d515b99dc56
leot <leot@pkgsrc.org>
2020-09-11 14:47:34 +0000

    webkit-gtk: Update to 2.30.0
    pkgsrc changes:
     - Unconditionally disable systemd support via CMAKE_ARGS (by default systemd is
       enabled unconditionally)
    
    Changes:
    WebKitGTK 2.30.0
    ================
      - Fix NTLM authentication.
      - Don't try to use SHM on wayland with older wpebackend-fdo.
      - Fix the build due to seccomp.h header not found.
    
    WebKitGTK 2.29.92
    =================
      - Fix user agent header after a redirect when a new quirk is required.
      - Stop using firefox user agent quirk for google docs.
      - Fix rendering frames timeline panel in web inspector.
      - Fix per-thread cpu usage in web inspector.
      - Fix several crashes and rendering issues.
      - Translation updates: Polish
    
    WebKitGTK 2.29.91
    =================
      - Fix a web process crash introduced in 2.29.90.
    
    WebKitGTK 2.29.90
    =================
      - Fix font variation settings when font smoothing setting is also present.
      - Fix HTML drag and drop operations.
      - Fix argument order for clone syscall seccomp filter on s390x.
      - Fix a crash when selecting text.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.29.4
    ================
      - Add support for backdrop filters.
      - Add support for text-underline-offset and text-decoration-thickness.
      - Add OpenCDM and AV1 support to media backend.
      - Add new API to get ITP data summary.
      - Use mobile user-agent on tablets.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.29.3
    ================
      - Add webkit_authentication_request_get_security_origin.
      - Change the cookies accept policy to always when no-third-party is set and ITP is enabled.
      - Fix web process hangs on large GitHub pages.
      - Bubblewrap sandbox should not attempt to bind empty paths.
      - Add support for sndio to bubblewrap sandbox.
      - Also handle dark themes when the name ends with -Dark.
      - Fix a race condition causing a crash in media player.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.29.2
    ================
     - Add Intelligent Tracking Prevention (ITP) support.
     - Add support for video formats in img elements.
     - Add API to handle video autoplay policy that now defaults to disallow autoplay videos with audio.
     - Add API to mute a web view.
     - Add API to allow applications to handle the HTTP authentication credential storage.
     - Add a WebKitSetting to set the media content types requiring hardware support.
     - Fix a crash during drag an drop due to a bug introduced in 2.29.1.
     - Do not start page load during animation in back/forward gesture.
     - Fix several crashes and rendering issues.
     - Translation updates: Ukrainian.
    
    WebKitGTK 2.29.1
    ================
     - Stop using GTK theming to render form controls.
     - Add API to disable GTK theming for scrollbars too.
     - Fix several race conditions and threading issues in the media player.
     - Add USER_AGENT_BRANDING build option.
     - Add paste as plain text option to the context menu for rich editable content.
     - Fix several crashes and rendering issues.

--- www/webkit-gtk/patches/patch-Source_JavaScriptCore_heap_MarkedSpace.cpp
+++ www/webkit-gtk/patches/patch-Source_JavaScriptCore_heap_MarkedSpace.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-Source_JavaScriptCore_heap_MarkedSpace.cpp,v 1.2 2020/03/10 18:14:04 leot Exp $
+$NetBSD: patch-Source_JavaScriptCore_heap_MarkedSpace.cpp,v 1.3 2020/09/11 14:47:35 leot Exp $
 
 Avoid ambiguous function call.
 
---- Source/JavaScriptCore/heap/MarkedSpace.cpp.orig	2020-02-04 10:23:59.000000000 +0000
+--- Source/JavaScriptCore/heap/MarkedSpace.cpp.orig	2020-08-12 09:17:52.000000000 +0000
 +++ Source/JavaScriptCore/heap/MarkedSpace.cpp
-@@ -30,6 +30,8 @@
+@@ -28,6 +28,8 @@
  #include "MarkedSpaceInlines.h"
  #include <wtf/ListDump.h>
  
@@ -12,4 +12,4 @@ Avoid ambiguous function call.
 +
  namespace JSC {
  
- std::array<size_t, MarkedSpace::numSizeClasses> MarkedSpace::s_sizeClassForSizeStep;
+ std::array<unsigned, MarkedSpace::numSizeClasses> MarkedSpace::s_sizeClassForSizeStep;
