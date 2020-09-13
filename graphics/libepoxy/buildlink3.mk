c45b7c861bc2a529256963e757de687f700f1ab5
leot <leot@pkgsrc.org>
2020-09-13 16:01:04 +0000

    libepoxy: OPSYS definition needs bsd.fast.prefs.mk
    (Probably it was exposed by accident to all libepoxy bl3 users.)

--- graphics/libepoxy/buildlink3.mk
+++ graphics/libepoxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 21:16:12 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/09/13 16:01:04 leot Exp $
 
 BUILDLINK_TREE+=	libepoxy
 
@@ -7,6 +7,8 @@ LIBEPOXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libepoxy+=	libepoxy>=1.2
 BUILDLINK_PKGSRCDIR.libepoxy?=		../../graphics/libepoxy
+
+.include "../../mk/bsd.fast.prefs.mk"
 
 .if ${OPSYS} != "Darwin"
 .include "../../graphics/MesaLib/buildlink3.mk"
