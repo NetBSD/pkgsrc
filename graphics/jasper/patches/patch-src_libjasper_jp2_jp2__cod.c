d3db9502ec8d8b7073435f01ea6cedb3ae045360
adam <adam@pkgsrc.org>
2020-09-14 17:47:13 +0000

    jasper: updated to 2.0.20
    2.0.20
    
    * fixed several ISO/IEC 15444-4 conformance bugs
    
    * fixed new variant of CVE-2016-9398
    
    * disabled the MIF codec by default for security reasons (but it is still
      included in the library);
      in a future release, the MIF codec may also be excluded from the
      library by default
    
    * added documentation for the I/O streams library API

--- graphics/jasper/patches/patch-src_libjasper_jp2_jp2__cod.c
+++ graphics/jasper/patches/patch-src_libjasper_jp2_jp2__cod.c
@@ -1,24 +1,24 @@
-$NetBSD: patch-src_libjasper_jp2_jp2__cod.c,v 1.3 2016/12/16 09:44:44 he Exp $
+$NetBSD: patch-src_libjasper_jp2_jp2__cod.c,v 1.4 2020/09/14 17:47:14 adam Exp $
 
 Only output debug info if debuglevel >= 1.
 
---- src/libjasper/jp2/jp2_cod.c.orig	2016-11-16 15:03:41.000000000 +0000
+--- src/libjasper/jp2/jp2_cod.c.orig	2020-09-05 14:52:22.000000000 +0000
 +++ src/libjasper/jp2/jp2_cod.c
-@@ -808,11 +808,15 @@ static void jp2_cmap_dumpdata(jp2_box_t 
- 	jp2_cmap_t *cmap = &box->data.cmap;
+@@ -828,11 +828,15 @@ static void jp2_cmap_dumpdata(const jp2_
+ {
+ 	const jp2_cmap_t *cmap = &box->data.cmap;
  	unsigned int i;
- 	jp2_cmapent_t *ent;
 -	fprintf(out, "numchans = %d\n", (int) cmap->numchans);
 +	if (jas_getdbglevel() >= 1) {
 +		fprintf(out, "numchans = %d\n", (int) cmap->numchans);
 +	}
  	for (i = 0; i < cmap->numchans; ++i) {
- 		ent = &cmap->ents[i];
+ 		const jp2_cmapent_t *ent = &cmap->ents[i];
 -		fprintf(out, "cmptno=%d; map=%d; pcol=%d\n",
 -		  (int) ent->cmptno, (int) ent->map, (int) ent->pcol);
 +		if (jas_getdbglevel() >= 1) {
 +			fprintf(out, "cmptno=%d; map=%d; pcol=%d\n",
-+				(int) ent->cmptno, (int) ent->map, (int) ent->pcol);
++			  (int) ent->cmptno, (int) ent->map, (int) ent->pcol);
 +		}
  	}
  }
