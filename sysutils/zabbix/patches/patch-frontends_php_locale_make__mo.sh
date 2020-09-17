ff7caa09cc0b5d3e3e5d58f0ae4cd741e49bdf06
otis <otis@pkgsrc.org>
2020-09-17 13:52:15 +0000

    sysutils/zabbix: Update to 4.0.24
    Changelog since 4.0.21:
    
    * New features:
      - Improved Zabbix server performance when evaluating trigger, discovery, autoregistration and internal actions
      - Added host info to vmware event message log
      - Added vmware datacenters discovery
    
    * Bugfixes:
      - Fixed security vulnerability cve-2020-15803 (stored xss attack on url widget)
      - Exclude disabled items from preprocessing configuration sync
      - Fixed possible deadlocks when deleting escalations
      - Fixed null value for a field of compositedata in jmx discovery
      - Changed log, log.count monitoring to ignore file modification time
      - Fixed strict dfsg compliance issue in using minified jquery sources
      - Fixed recovery expression for "link down" trigger and updated for following zabbix template guidelines
      - Fixed escalation cancellation message not being sent when action is disabled
      - Fixed context of {$temp_crit} macro in temperature trigger prototype
      - Fixed high memory consumption when using dependent items
      - Fixed memory leak
      - Fixed remove of selected items in multiselect
      - Fixed confusing error message being displayed if dbversion table cannot be found
      - Shadow global auto_increment mysql variables only for proxy
      - Fixed 'cmdline' parameter truncation on solaris 11.3 and later
      - Fixed memory leak when action message does not have to be sent
      - Fixed graph configuration with large amount of items
      - Fixed server exit when trying to execute ipmi script without ipmi pollers
      - Fixed undefined index in trends api
      - Fixed button disable state in template item list and discovery list
      - Fixed classic graph widget default header
      - Fixed php errors in "system information" window for mysql
      - Changed vm.memory.size keys on solaris to get memory usage statistics from kstat
      - Fixed cpu counter wraparound handling on solaris
      - Fixed misleading error message in case of not available datastorage for vmware.hv.datastore.read and vmware.hv.datastore.write metrics
      - Fixed host prototype import
      - Removed mtime check when matching files on disk with the cached list, this should reduce chances of log files being scanned again after system time changes
      - Fixed sql errors with oracle backend due to incorrect use of the using statement
      - Improved error message generation performance of user macro and item key parser
      - Fixed use on uninitialised variable
      - Added odbc empty query check
      - Fixed fatal error in items.php and host_discovery.php
      - Improved performance of the trigger and trigger prototype inheritance
      - Implement timeouts for wmi queries
      - Improved performance of "problems" widget with mysql backend by removing distinct modifier from sql statements with tables joined by primary keys
      - Fixed excess audit log record being added on host status update
      - Fixed php notice and multiple undefined index errors in hostinterface api
      - Added red label for disabled media types in user profile
      - Fixed runtime error when creating a host via api
      - Removed problem calculation for dependent triggers when master trigger is in problem state
      - Fixed usage of unsupported parameter in api call
      - Fixed inaccessible user be displayed as accessible in slide show properties screen
      - Fixed building of zabbix daemons with link time optimization

--- sysutils/zabbix/patches/patch-frontends_php_locale_make__mo.sh
+++ sysutils/zabbix/patches/patch-frontends_php_locale_make__mo.sh
@@ -1,14 +1,15 @@
-$NetBSD: patch-frontends_php_locale_make__mo.sh,v 1.2 2019/02/07 12:36:25 he Exp $
+$NetBSD: patch-frontends_php_locale_make__mo.sh,v 1.3 2020/09/17 13:52:15 otis Exp $
 
 Make script runnable with non-bash bourne shell.
 
---- frontends/php/locale/make_mo.sh.orig	2017-05-05 14:50:27.000000000 +0000
+--- frontends/php/locale/make_mo.sh.orig	2020-09-13 19:14:35.190972278 +0000
 +++ frontends/php/locale/make_mo.sh
-@@ -1,5 +1,5 @@
+@@ -1,5 +1,6 @@
  #!/bin/bash
  
 -while read pofile; do
-+find $(dirname $0) -type f ! -wholename '*/.svn*' -name '*.po' | while read pofile; do
++find $(dirname $0) -type f ! -wholename '*/.svn*' -name '*.po' | \
++   while read pofile; do
          msgfmt --use-fuzzy -c -o ${pofile%po}mo $pofile || exit $?
--done < <(find $(dirname $0) -type f ! -wholename '*/.svn*' -name '*.po')
+-done < <(find $(dirname $0) -type f -name '*.po')
 +done
