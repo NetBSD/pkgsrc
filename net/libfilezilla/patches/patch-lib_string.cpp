3ac7156d7be5501d7e2a2a08248ded09e9dcbe76
jperkin <jperkin@pkgsrc.org>
2020-09-17 10:05:44 +0000

    libfilezilla: Update to 0.24.1.
    While here fix the build on SunOS, based on patches in joyent/pkgsrc#266
    from mrferda.
    
    0.24.1 (2020-08-27)
    
    - fz::to_integral can now handle strongly typed enum return types
    
    0.24.0 (2020-08-21)
    
    + Added fz::equal_consttime
    - fz::sprintf now works corrcetly if arguments are passed as (w)string_view
    
    0.23.0 (2020-07-07)
    
    + Added reader/writer locks
    - fz::mkdir can now return the the longest created path in case of partial failures

---
+++ net/libfilezilla/patches/patch-lib_string.cpp
@@ -0,0 +1,24 @@
+$NetBSD: patch-lib_string.cpp,v 1.6 2020/09/17 10:05:44 jperkin Exp $
+
+Explicitly use std::wcsnrtombs.
+
+--- lib/string.cpp.orig	2020-07-07 12:06:31.000000000 +0000
++++ lib/string.cpp
+@@ -354,7 +354,7 @@ std::string to_string(std::wstring_view
+ 
+ 			std::mbstate_t ps{};
+ 			wchar_t const* in_p = in.data() + start;
+-			size_t len = wcsnrtombs(nullptr, &in_p, inlen, 0, &ps);
++			size_t len = std::wcsnrtombs(nullptr, &in_p, inlen, 0, &ps);
+ 			if (len != static_cast<size_t>(-1)) {
+ 				size_t old = ret.size();
+ 				if (start) {
+@@ -364,7 +364,7 @@ std::string to_string(std::wstring_view
+ 				char* out_p = &ret[old];
+ 
+ 				in_p = in.data() + start; // Some implementations of wcsrtombs change src even on null dst
+-				wcsnrtombs(out_p, &in_p, inlen, len, &ps);
++				std::wcsnrtombs(out_p, &in_p, inlen, len, &ps);
+ 			}
+ 			else {
+ 				ret.clear();
