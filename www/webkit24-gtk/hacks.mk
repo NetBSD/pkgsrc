67922fd3a2f9cc744a048fe3ac8f4fdcc5abd5a3
bsiegert <bsiegert@pkgsrc.org>
2020-09-18 18:22:25 +0000

    Pullup ticket #6322 - requested by he www/webkit24-gtk: PowerPC build fix
    Revisions pulled up:
    - www/webkit24-gtk/hacks.mk                                     1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	he
       Date:		Sat Sep 12 16:39:06 UTC 2020
    
       Modified Files:
       	pkgsrc/www/webkit24-gtk: hacks.mk
    
       Log Message:
       Pull in devel/libatomic on powerpc due to lack of native 8-byte atomics.
       Fixes build on NetBSD/macppc 9.0, should have no effect elsewhere, so no
       revision bump.

--- www/webkit24-gtk/hacks.mk
+++ www/webkit24-gtk/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2016/03/22 20:10:15 leot Exp $
+# $NetBSD: hacks.mk,v 1.1.34.1 2020/09/18 18:22:25 bsiegert Exp $
 
 .if !defined(WEBKIT24_GTK_HACKS_MK)
 WEBKIT24_GTK_HACKS_MK=	defined
@@ -11,6 +11,15 @@ WEBKIT24_GTK_HACKS_MK=	defined
 .if ${MACHINE_ARCH} == "i386"
 PKG_HACKS+=	i386-atomicops
 CXXFLAGS+=	-march=i586
+.endif
+
+# [Fri Sep 11 11:23:52 CEST 2020 : he]
+# On NetBSD/powerpc, we don't have native 8-byte atomics,
+# but webkit24-gtk insists on using them, so here we need libatomic.
+# (This is a trial balloon -- let's see if autoconf will pick it up...)
+.if ${MACHINE_ARCH} == "powerpc"
+PKG_HACKS+=	powerpc-libatomic
+.include "../../devel/libatomic/buildlink3.mk"
 .endif
 
 .endif	# WEBKIT24_GTK_HACKS_MK
