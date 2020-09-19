76b3498206aa71e116cc4e58c55785a77b5aa6e4
schmonz <schmonz@pkgsrc.org>
2020-09-19 03:14:16 +0000

    Buildlink glu and MesaLib with 'x11' option to fix build, at least on macOS, and restore PLIST decorations to fix packaging on macOS.

--- multimedia/gpac/options.mk
+++ multimedia/gpac/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2017/07/22 13:07:04 adam Exp $
+# $NetBSD: options.mk,v 1.2 2020/09/19 03:14:16 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gpac
 PKG_SUPPORTED_OPTIONS=	x11
@@ -9,6 +9,8 @@ PKG_SUGGESTED_OPTIONS=	x11
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--X11-path=${X11BASE}
 PLIST.x11=		yes
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXv/buildlink3.mk"
 .endif
