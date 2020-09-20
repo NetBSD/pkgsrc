e361df02fc9a8bd121530ebf2e190f0c42ef9b32
nia <nia@pkgsrc.org>
2020-09-20 13:30:45 +0000

    fluxbox: Enable xft support by default, rename option for consistency
    Bump PKGREVISION

--- wm/fluxbox/options.mk
+++ wm/fluxbox/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.7 2020/03/21 15:58:49 rillig Exp $
+# $NetBSD: options.mk,v 1.8 2020/09/20 13:30:45 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fluxbox
-PKG_SUPPORTED_OPTIONS=	imlib2 xft xrender nls
-PKG_SUGGESTED_OPTIONS=	xrender
+PKG_SUPPORTED_OPTIONS=	imlib2 xft2 xrender nls
+PKG_SUGGESTED_OPTIONS=	xft2 xrender
+
+PKG_OPTIONS_LEGACY_OPTS+=	xft:xft2
 
 .include "../../mk/bsd.options.mk"
 
@@ -19,7 +21,7 @@ CONFIGURE_ARGS+=	--enable-imlib2
 CONFIGURE_ARGS+=	--disable-imlib2
 .endif
 
-.if !empty(PKG_OPTIONS:Mxft)
+.if !empty(PKG_OPTIONS:Mxft2)
 CONFIGURE_ARGS+=	--enable-xft
 .include "../../x11/libXft/buildlink3.mk"
 .else
