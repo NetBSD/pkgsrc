e01db85145d451718ddfebfdf4c5473614189d6b
wiz <wiz@pkgsrc.org>
2020-09-20 15:44:33 +0000

    emacs27: update patch status

--- editors/emacs27/patches/patch-lib-src_emacsclient.c
+++ editors/emacs27/patches/patch-lib-src_emacsclient.c
@@ -1,8 +1,11 @@
-$NetBSD: patch-lib-src_emacsclient.c,v 1.1 2020/08/23 20:12:34 wiz Exp $
+$NetBSD: patch-lib-src_emacsclient.c,v 1.2 2020/09/20 15:44:33 wiz Exp $
 
 Workaround for:
 On NetBSD, fstat on a socket returns zero instead of the user id.
 Reported upstream as http://debbugs.gnu.org/cgi/bugreport.cgi?bug=43002
+
+This is only needed for NetBSD 9 and earlier, fixed in current in
+https://mail-index.netbsd.org/source-changes/2020/08/26/msg121023.html
 
 --- lib-src/emacsclient.c.orig	2020-07-29 21:40:41.000000000 +0000
 +++ lib-src/emacsclient.c
