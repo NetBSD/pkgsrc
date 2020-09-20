ae8c416533dceb4c3ae10b306976890d2a54cab1
joerg <joerg@pkgsrc.org>
2020-09-20 03:17:40 +0000

    Fix Jasper fallout

--- x11/kdelibs4/patches/patch-kimgio_jp2.cpp
+++ x11/kdelibs4/patches/patch-kimgio_jp2.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-kimgio_jp2.cpp,v 1.1 2020/08/09 00:01:09 joerg Exp $
+$NetBSD: patch-kimgio_jp2.cpp,v 1.2 2020/09/20 03:17:40 joerg Exp $
 
---- kimgio/jp2.cpp.orig	2020-08-08 22:34:50.646978346 +0000
+--- kimgio/jp2.cpp.orig	2017-11-05 01:51:22.000000000 +0000
 +++ kimgio/jp2.cpp
 @@ -103,13 +103,13 @@ static void jas_stream_initbuf(jas_strea
          stream->bufmode_ |= bufmode & JAS_STREAM_BUFMODEMASK;
@@ -14,7 +14,7 @@ $NetBSD: patch-kimgio_jp2.cpp,v 1.1 2020/08/09 00:01:09 joerg Exp $
  }
  
 -static int qiodevice_write(jas_stream_obj_t *obj, char *buf, int cnt)
-+static int qiodevice_write(jas_stream_obj_t *obj, char *buf, unsigned int cnt)
++static int qiodevice_write(jas_stream_obj_t *obj, const char *buf, unsigned int cnt)
  {
          QIODevice *io = (QIODevice*) obj;
          return io->write(buf, cnt);
