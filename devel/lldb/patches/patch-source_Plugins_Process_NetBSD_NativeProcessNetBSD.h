d5ea4459b7e6f3f6d9ffc6d0f675f02549b6b8c8
adam <adam@pkgsrc.org>
2020-09-23 06:58:46 +0000

    lldb: better patch and small pylint fixes

--- devel/lldb/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h
+++ devel/lldb/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h,v 1.2 2020/09/23 06:01:01 mef Exp $
+$NetBSD: patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h,v 1.3 2020/09/23 06:58:46 adam Exp $
 
 Declare StopProcess.
 
---- work/lldb-10.0.1.src/source/Plugins/Process/NetBSD/NativeProcessNetBSD.h.orig	2020-07-07 16:21:37.000000000 +0000
+--- source/Plugins/Process/NetBSD/NativeProcessNetBSD.h.orig	2020-07-07 16:21:37.000000000 +0000
 +++ source/Plugins/Process/NetBSD/NativeProcessNetBSD.h
 @@ -86,6 +86,7 @@ public:
    // Interface used by NativeRegisterContext-derived classes.
