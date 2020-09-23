8d3ec6cf89fb931a9a6efba3c55c809853edf73d
mef <mef@pkgsrc.org>
2020-09-23 06:01:00 +0000

    (devel/lldb) correction to patch path and regen distinfo

--- devel/lldb/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h
+++ devel/lldb/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h,v 1.1 2020/09/19 17:25:02 he Exp $
+$NetBSD: patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h,v 1.2 2020/09/23 06:01:01 mef Exp $
 
 Declare StopProcess.
 
 --- work/lldb-10.0.1.src/source/Plugins/Process/NetBSD/NativeProcessNetBSD.h.orig	2020-07-07 16:21:37.000000000 +0000
-+++ work/lldb-10.0.1.src/source/Plugins/Process/NetBSD/NativeProcessNetBSD.h
++++ source/Plugins/Process/NetBSD/NativeProcessNetBSD.h
 @@ -86,6 +86,7 @@ public:
    // Interface used by NativeRegisterContext-derived classes.
    static Status PtraceWrapper(int req, lldb::pid_t pid, void *addr = nullptr,
