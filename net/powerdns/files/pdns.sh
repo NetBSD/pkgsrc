70ea9935e587a898e3a96192b4abb13a2bbdc4a2
otis <otis@pkgsrc.org>
2020-09-24 19:09:53 +0000

    net/powerdns: Update to 4.3.1
    Security fix:
    * CVE-2020-17482:
      https://doc.powerdns.com/authoritative/security-advisories/powerdns-advisory-2020-05.html
    
      An issue has been found in PowerDNS Authoritative Server before 4.3.1
      where an authorized user with the ability to insert crafted records
      into a zone might be able to leak the content of uninitialized memory.
      Such a user could be a customer inserting data via a control panel,
      or somebody with access to the REST API. Crafted records cannot be
      inserted via AXFR.
    
    Changelog:
    * New Features
      - Add ubuntu focal target
    
    * Improvements
      - EL8 pkgs: Build mysql backend against mariadb-connector-c-devel
      - gpgsql: Reintroduce prepared statements
      - gsqlite3backend: add missing indexes
      - Use real remote for supermaster createSlaveDomain()
      - Optimize IXFR-to-AXFR fallback path
      - Install bind SQL schema files as part of bindbackend
      - Do not send out of zone lookups to the backends
    
    * Bug Fixes:
      - Raise an exception on invalid hex content in unknown records.
      - Handle the extra single-row result set of MySQL stored procedures
    
    * pkgsrc-specific:
      - The default pid file patch in rc.d script has been fixed

--- net/powerdns/files/pdns.sh
+++ net/powerdns/files/pdns.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pdns.sh,v 1.1.1.1 2006/08/13 09:34:21 ghen Exp $
+# $NetBSD: pdns.sh,v 1.2 2020/09/24 19:09:53 otis Exp $
 #
 
 # PROVIDE: pdns
@@ -12,7 +12,7 @@ name="pdns"
 rcvar=$name
 command="@PREFIX@/sbin/pdns_server"
 command_args="--daemon=yes"
-pidfile=/var/run/${name}.pid
+pidfile=/var/run/${name}/${name}.pid
 required_files="@PKG_SYSCONFDIR@/pdns.conf"
 
 load_rc_config $name
