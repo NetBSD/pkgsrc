f9102301ef1af30b9f16e053d02d14ea2fd64906
khorben <khorben@pkgsrc.org>
2020-09-26 02:08:10 +0000

    compiler-rt: fix building on netbsd-9/amd64

--- lang/compiler-rt/patches/patch-lib_sanitizer__common_sanitizer__linux__libcdep.cpp
+++ lang/compiler-rt/patches/patch-lib_sanitizer__common_sanitizer__linux__libcdep.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_sanitizer__common_sanitizer__linux__libcdep.cpp,v 1.1 2020/04/18 08:03:53 adam Exp $
+$NetBSD: patch-lib_sanitizer__common_sanitizer__linux__libcdep.cpp,v 1.2 2020/09/26 02:08:10 khorben Exp $
 
 Fix building on NetBSD.
 
---- lib/sanitizer_common/sanitizer_linux_libcdep.cpp.orig	2018-10-07 10:50:07.000000000 +0000
+--- lib/sanitizer_common/sanitizer_linux_libcdep.cpp.orig	2020-03-23 15:01:02.000000000 +0000
 +++ lib/sanitizer_common/sanitizer_linux_libcdep.cpp
 @@ -520,13 +520,13 @@ void GetThreadStackAndTls(bool main, upt
  #endif
@@ -11,7 +11,8 @@ Fix building on NetBSD.
 -#if !SANITIZER_FREEBSD && !SANITIZER_OPENBSD
 +#if !SANITIZER_FREEBSD && !SANITIZER_NETBSD && !SANITIZER_OPENBSD
  typedef ElfW(Phdr) Elf_Phdr;
- #elif SANITIZER_WORDSIZE == 32 && __FreeBSD_version <= 902001 // v9.2
+-#elif SANITIZER_WORDSIZE == 32 && __FreeBSD_version <= 902001 // v9.2
++#elif SANITIZER_WORDSIZE == 32 && SANITIZER_FREEBSD && __FreeBSD_version <= 902001 // v9.2
  #define Elf_Phdr XElf32_Phdr
  #define dl_phdr_info xdl_phdr_info
  #define dl_iterate_phdr(c, b) xdl_iterate_phdr((c), (b))
