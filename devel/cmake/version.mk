94ce19945257776b5d76d00dd55163f8152c8c90
adam <adam@pkgsrc.org>
2020-09-28 07:37:43 +0000

    cmake: updated to 3.18.3
    CMake 3.18.3
    * AutoGen: Fix moc and uic dependencies when building Qt itself
    * foreach: Fix crash parsing integer out of range
    * gitlab-ci: Prefer MR rules for any pipeline associated with a MR
    * Help: Update 3.18.2 release notes to mention 3.16.9 and 3.17.5
    * gitlab-ci: update macOS jobs to use Xcode 11.7
    * Check*CompilerFlag: Do not set result as a normal variable too
    * PCH: Mark CMake PCH source files as -x -header
    * PCH: Fix 30s wait for VS2008 when used via -Tv90
    * AutoMoc: Restore support for re-running after project file changes
    * AutoMoc: Re-run moc if a dependency is missing
    * file(GENERATE): Create output file structures for all directories first
    * Tests: Pass additional Qt information to Ninja and NMC tests
    * Ninja Multi-Config: Fix clean:all target
    * Ninja Multi-Config: Fix cleaning of utility targets with commands
    * Ninja Multi-Config: Fix dependencies of utility targets
    * Ninja Multi-Config: Fix dependencies of custom commands
    * FindJNI: Add arm64 support
    * UseSWIG: Update option -interface usage
    * FindPython: enhance ABI checks against include directory
    * FindPython: CMP0012 must be set to NEW
    * ARMClang: Fix link line generation after addition of armlink support
    * FindMatlab: add R2020b => 9.9
    * MSVC: Record support for c_static_assert
    * CUDA: Support setting CUDA14/17 when using MSVC
    * Implement cm::static_reference_cast by declval

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2020/08/21 07:26:51 adam Exp $
+# $NetBSD: version.mk,v 1.3 2020/09/28 07:37:43 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.18.2
+CMAKE_VERSION=	3.18.3
 CMAKE_API=	${CMAKE_VERSION:R}
