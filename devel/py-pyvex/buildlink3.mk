2e5436c7ffd69598299ded2d8ebdda12294a0ace
khorben <khorben@pkgsrc.org>
2020-09-28 22:56:38 +0000

    py-angr: update to version 9.0.4378
    The project did not release any list of changes. This spans 7 years worth
    of commits; tough to summarize for an outsider.

--- devel/py-pyvex/buildlink3.mk
+++ devel/py-pyvex/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/03/15 02:49:25 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/09/28 22:56:38 khorben Exp $
 
 BUILDLINK_TREE+=	py-pyvex
 
 .if !defined(PY_PYVEX_BUILDLINK3_MK)
 PY_PYVEX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-pyvex+=	${PYPKGPREFIX}-pyvex>=7.8.2.21
+.include "../../devel/py-angr/version.mk"
+BUILDLINK_API_DEPENDS.py-pyvex+=	${PYPKGPREFIX}-pyvex>=${ANGR_VERSION}
 BUILDLINK_PKGSRCDIR.py-pyvex?=		../../devel/py-pyvex
 .endif  # PY_PYVEX_BUILDLINK3_MK
 
