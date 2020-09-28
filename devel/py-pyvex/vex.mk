96f5397da937e1d4460a84220512b87bbcac5938
khorben <khorben@pkgsrc.org>
2020-09-28 22:56:38 +0000

    py-angr: update to version 9.0.4378
    The project did not release any list of changes. This spans 7 years worth
    of commits; tough to summarize for an outsider.

--- devel/py-pyvex/vex.mk
+++ devel/py-pyvex/vex.mk
@@ -1,9 +1,9 @@
-# $NetBSD: vex.mk,v 1.2 2020/01/09 14:02:32 wiz Exp $
+# $NetBSD: vex.mk,v 1.3 2020/09/28 22:56:38 khorben Exp $
 
 DISTNAME=	vex-0.0.0
 CATEGORIES=	devel
 MASTER_SITES=	${MASTER_SITE_GITHUB:=angr/}
-GITHUB_TAG=	0e76ee25dc2793d23ec5f9695d38a2ca76780bbc
+GITHUB_TAG=	4648030b364608388b33e1cfd90409e981406982
 
 MAINTAINER=	khorben@defora.org
 HOMEPAGE=	https://github.com/angr/vex/
