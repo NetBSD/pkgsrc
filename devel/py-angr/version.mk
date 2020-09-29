33f110c4b2cf78953a02886097c55910ae218e61
khorben <khorben@pkgsrc.org>
2020-09-29 00:51:20 +0000

    py-angr: update the list of files using version.mk

--- devel/py-angr/version.mk
+++ devel/py-angr/version.mk
@@ -1,9 +1,11 @@
-#	$NetBSD: version.mk,v 1.2 2020/09/28 22:56:38 khorben Exp $
+#	$NetBSD: version.mk,v 1.3 2020/09/29 00:51:20 khorben Exp $
 
 # used by devel/py-ailment/Makefile
 # used by devel/py-archinfo/Makefile
 # used by devel/py-cle/Makefile
+# used by devel/py-manticore/Makefile
 # used by devel/py-pyvex/Makefile
+# used by devel/py-pyvex/buildlink3.mk
 # used by math/py-claripy/Makefile
 
 ANGR_VERSION=	9.0.4378
