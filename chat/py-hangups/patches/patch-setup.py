2831c4974deda3c15fb0f21d7c6281c58ba50225
leot <leot@pkgsrc.org>
2020-09-29 21:42:59 +0000

    py-hangups: Update to 0.4.12
    Changes:
    0.4.12
    ------
     * update parameters for consistency with web client
     * upgrade pinned dependencies

--- chat/py-hangups/patches/patch-setup.py
+++ chat/py-hangups/patches/patch-setup.py
@@ -1,23 +1,23 @@
-$NetBSD: patch-setup.py,v 1.6 2020/09/07 19:34:29 leot Exp $
+$NetBSD: patch-setup.py,v 1.7 2020/09/29 21:42:59 leot Exp $
 
 Avoid too strict version requirements.
 
---- setup.py.orig	2020-01-16 05:13:05.000000000 +0000
+--- setup.py.orig	2020-09-28 01:43:12.000000000 +0000
 +++ setup.py
 @@ -25,16 +25,16 @@ with open('README.rst') as f:
  # hangups from breaking when new versions of dependencies are released,
  # especially for end-users (non-developers) who use pip to install hangups.
  install_requires = [
--    'ConfigArgParse==0.11.0',
+-    'ConfigArgParse>=0.11.0,<2',
 -    'aiohttp>=3.3,<4',
 -    'async-timeout>=2,<4',
 -    'appdirs>=1.4,<1.5',
--    'readlike==0.1.2',
+-    'readlike>=0.1.2,<0.2',
 -    'requests>=2.6.0,<3',  # uses semantic versioning (after 2.6)
 -    'ReParser==1.4.3',
--    'protobuf>=3.1.0,<3.12',
--    'urwid==1.3.1',
--    'MechanicalSoup==0.6.0',
+-    'protobuf>=3.1.0,<3.14',
+-    'urwid>=1.3.1,<2.2',
+-    'MechanicalSoup>=0.6.0,<0.13',
 +    'ConfigArgParse>=0.11.0',
 +    'aiohttp>=3.3',
 +    'async-timeout>=2',
