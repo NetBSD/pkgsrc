aac7c090c4d7cf6c97b8bf16a7035101ac75f422
prlw1 <prlw1@pkgsrc.org>
2020-09-29 10:43:33 +0000

    Update glib2 to 2.66.0
    Highlights:
    * Fixes to the new `statx()` calls - note that since GLib 2.65.2 uses `statx()`
      (if available) instead of `stat()`/`fstat()`/`lstat()`/`fstatat()`, syscall
      sandboxing for third party applications might need to be updated
    * Fix deadlock in `g_subprocess_communicate_async()` (work by Alexander Larsson) (#2182)
    * Add `%f`/microsecond placeholder support to `g_date_time_format()` (work by Johan Bjäreholt) (!1605)
    * Add `GUri` API for parsing, building and representing URIs according to
      [RFC 3986](https://tools.ietf.org/html/rfc3986) (work by Marc-Andre Lureau) (#110)
    * D-Bus credentials support on macOS (#507)
    * Year 2038 fixes involving new API in `GBookmarkFile` (#1931)
    
    For full details, see:
    https://gitlab.gnome.org/GNOME/glib/-/blob/2.66.0/NEWS

--- devel/glib2/patches/patch-gio_gcredentialsprivate.h
+++ devel/glib2/patches/patch-gio_gcredentialsprivate.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-gio_gcredentialsprivate.h,v 1.3 2018/10/19 16:44:15 leot Exp $
+$NetBSD: patch-gio_gcredentialsprivate.h,v 1.4 2020/09/29 10:43:33 prlw1 Exp $
 
 NetBSD has G_CREDENTIALS_SOCKET_GET_CREDENTIALS_SUPPORTED.
 
---- gio/gcredentialsprivate.h.orig	2017-07-13 23:03:38.000000000 +0000
+--- gio/gcredentialsprivate.h.orig	2020-08-18 10:52:24.847177300 +0000
 +++ gio/gcredentialsprivate.h
-@@ -47,7 +47,7 @@
+@@ -134,7 +134,7 @@
  #define G_CREDENTIALS_USE_NETBSD_UNPCBID 1
  #define G_CREDENTIALS_NATIVE_TYPE G_CREDENTIALS_TYPE_NETBSD_UNPCBID
  #define G_CREDENTIALS_NATIVE_SIZE (sizeof (struct unpcbid))
 -/* #undef G_CREDENTIALS_UNIX_CREDENTIALS_MESSAGE_SUPPORTED */
 +#define G_CREDENTIALS_SOCKET_GET_CREDENTIALS_SUPPORTED 1
  #define G_CREDENTIALS_SPOOFING_SUPPORTED 1
+ #define G_CREDENTIALS_HAS_PID 1
  
- #elif defined(__OpenBSD__)
