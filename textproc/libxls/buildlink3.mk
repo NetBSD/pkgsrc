3a86fd6e75d30fa889d32e095b6e541d70e7e82b
sjmulder <sjmulder@pkgsrc.org>
2020-10-01 19:34:09 +0000

    textproc/libxls: Update to 1.6.1
    Changes in 1.6.0:
    
     - This release enables decoding of non-Unicode character sets in older
       (BIFF5) XLS files. In addition, it improves string-conversion
       performance in newer files.
    
       See: https://github.com/libxls/libxls/pull/77
    
    Changes in 1.6.1:
    
     - This release corrects the libtool version information found in libxls
       1.6.0, and fixes undefined behavior for applications that were
       originally linked against the 1.5.x dynamic library. The C source
       code is unchanged from the last release.
    
       See: https://github.com/libxls/libxls/issues/79

--- textproc/libxls/buildlink3.mk
+++ textproc/libxls/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/07/14 12:48:58 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/10/01 19:34:09 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxls
 
 .if !defined(LIBXLS_BUILDLINK3_MK)
 LIBXLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.5.3
+BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.6.1
 BUILDLINK_PKGSRCDIR.libxls?=	../../textproc/libxls
 
 .include "../../converters/libiconv/buildlink3.mk"
