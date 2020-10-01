ee658c7bdb17578f6ed5af75d3d0de10d4b1f4ce
sjmulder <sjmulder@pkgsrc.org>
2020-10-01 21:10:41 +0000

    textproc/libxlsxwriter: Update to 1.0.0
    Changes:
     - Added support for worksheet conditional formatting.
     - Added performance optimization for search for control characters in
       strings in worksheet_write_string().
     - Made lxw_datetime_to_excel_datetime() a function public.
     - There are now over 1000 test cases, including 650 tests that compare
       the output from libxlsxwriter, byte for byte, against test files
       created in Excel. This is also the 100th release of libxlsxwriter.
    
    Package changes:
     - Disable hardcoded -O3

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/08/23 22:27:52 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/10/01 21:10:41 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.9
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.0.0
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
