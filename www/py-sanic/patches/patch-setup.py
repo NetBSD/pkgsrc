be86bf1b7f6eb55e0eddba8aea67aef8f53946a8
adam <adam@pkgsrc.org>
2020-10-01 06:53:12 +0000

    py-sanic: updated to 20.9.0
    Version 20.9.0
    
    Features
    * Pass subprotocols in websockets (both sanic server and ASGI)
    * Automatically set test_mode flag on app instance
    * Add new unified method for updating app values
    * Adds WEBSOCKET_PING_TIMEOUT and WEBSOCKET_PING_INTERVAL configuration values
    * httpx version dependency updated, it is slated for removal as a dependency in v20.12
    * Added auto, text, and json fallback error handlers (in v21.3, the default will change form html to auto)
    
    Bugfixes
    * Resolves exception from unread bytes in stream
    
    Deprecations and Removals
    * config.from_envar, config.from_pyfile, and config.from_object are deprecated and set to be removed in v21.3
    
    Developer infrastructure
    * Update isort calls to be compatible with new API
    * Remove version section from setup.cfg
    * Adding --strict-markers for pytest
    
    Improved Documentation
    * Add explicit ASGI compliance to the README

--- www/py-sanic/patches/patch-setup.py
+++ www/py-sanic/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.6 2020/06/29 14:29:20 adam Exp $
+$NetBSD: patch-setup.py,v 1.7 2020/10/01 06:53:12 adam Exp $
 
 Allow newer versions.
 
---- setup.py.orig	2020-06-28 10:14:53.000000000 +0000
+--- setup.py.orig	2020-09-30 14:40:18.000000000 +0000
 +++ setup.py
 @@ -81,7 +81,7 @@ requirements = [
      "aiofiles>=0.3.0",
      "websockets>=8.1,<9.0",
      "multidict>=4.0,<5.0",
--    "httpx==0.11.1",
-+    "httpx>=0.11.1",
+-    "httpx==0.15.4",
++    "httpx>=0.15.4",
  ]
  
  tests_require = [
