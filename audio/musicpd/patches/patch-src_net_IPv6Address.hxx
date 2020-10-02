eee2d9aa313252febde4e60a3082bbfa697670a1
wiz <wiz@pkgsrc.org>
2020-10-02 09:47:09 +0000

    musicpd: update to 0.22.
    ver 0.22 (2020/09/23)
    * protocol
      - "findadd"/"searchadd"/"searchaddpl" support the "sort" and
        "window" parameters
      - add command "readpicture" to download embedded pictures
      - command "moveoutput" moves an output between partitions
      - command "delpartition" deletes a partition
      - show partition name in "status" response
    * tags
      - new tags "Grouping" (for ID3 "TIT1"), "Work" and "Conductor"
    * input
      - curl: support "charset" parameter in URI fragment
      - ffmpeg: allow partial reads
      - io_uring: new plugin for local files on Linux (using liburing)
      - smbclient: close unused SMB/CIFS connections
    * database
      - upnp: drop support for libupnp versions older than 1.8
    * playlist
      - cue: integrate contents in database
    * decoder
      - ffmpeg: support RTSP
      - mad: remove option "gapless", always do gapless
      - sidplay: add option "default_genre"
      - sidplay: map SID name field to "Album" tag
      - sidplay: add support for new song length format with libsidplayfp 2.0
      - vorbis, opus: improve seeking accuracy
    * playlist
      - flac: support reading CUE sheets from remote FLAC files
    * filter
      - ffmpeg: new plugin based on FFmpeg's libavfilter library
      - hdcd: new plugin based on FFmpeg's "af_hdcd" for HDCD playback
      - volume: convert S16 to S24 to preserve quality and reduce dithering noise
      - dsd: add integer-only DSD to PCM converter
    * output
      - jack: add option "auto_destination_ports"
      - jack: report error details
      - pulse: add option "media_role"
      - solaris: support S8 and S32
    * lower the real-time priority from 50 to 40
    * switch to C++17
      - GCC 8 or clang 5 (or newer) recommended
    
    ver 0.21.26 (2020/09/21)
    * database
      - inotify: obey ".mpdignore" files
    * output
      - osx: fix crash bug
      - sles: support floating point samples
    * archive
      - bzip2: fix crash on corrupt bzip2 file
      - bzip2: flush output at end of input file
      - iso9660: fix unaligned reads
      - iso9660: support seeking
      - zzip: fix crash on corrupt ZIP file
    * decoder
      - ffmpeg: remove "rtsp://" from the list of supported protocols
      - ffmpeg: add "hls+http://" to the list of supported protocols
      - opus: support the gain value from the Opus header
      - sndfile: fix lost samples at end of file
    * fix "single" mode bug after resuming playback
    * the default log_level is "default", not "info"

--- audio/musicpd/patches/patch-src_net_IPv6Address.hxx
+++ audio/musicpd/patches/patch-src_net_IPv6Address.hxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_net_IPv6Address.hxx,v 1.1 2018/12/11 10:37:20 wiz Exp $
+$NetBSD: patch-src_net_IPv6Address.hxx,v 1.2 2020/10/02 09:47:09 wiz Exp $
 
 Fixes
 In file included from /usr/include/netinet/in.h:372:0,
@@ -13,7 +13,7 @@ https://mail-index.netbsd.org/source-changes/2018/12/10/msg101328.html
 
 https://github.com/MusicPlayerDaemon/MPD/issues/438
 
---- src/net/IPv6Address.hxx.orig	2018-11-16 12:27:58.000000000 +0000
+--- src/net/IPv6Address.hxx.orig	2020-09-23 13:26:51.000000000 +0000
 +++ src/net/IPv6Address.hxx
 @@ -41,6 +41,7 @@
  #include <ws2tcpip.h>
@@ -22,4 +22,4 @@ https://github.com/MusicPlayerDaemon/MPD/issues/438
 +#include <arpa/inet.h>
  #endif
  
- /**
+ class IPv4Address;
