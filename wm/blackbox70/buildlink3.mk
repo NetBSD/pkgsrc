e98aa66b99f29018dc2572d64e2dd9568aa068ca
gutteridge <gutteridge@pkgsrc.org>
2020-10-02 16:40:32 +0000

    blackbox70: add conditional libXft inclusion in buildlink3.mk
    Now that libXft is an option, it has to be reflected in buildlink3.mk,
    or build failures can occur in wm/bbpager04.

--- wm/blackbox70/buildlink3.mk
+++ wm/blackbox70/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:34 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/10/02 16:40:32 gutteridge Exp $
 
 BUILDLINK_TREE+=	blackbox70
 
@@ -9,7 +9,13 @@ BUILDLINK_API_DEPENDS.blackbox70+=	blackbox70>=0.70.0
 BUILDLINK_ABI_DEPENDS.blackbox70+=	blackbox70>=0.70.0
 BUILDLINK_PKGSRCDIR.blackbox70?=	../../wm/blackbox70
 
+pkgbase:= blackbox70
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../x11/libX11/buildlink3.mk"
+.if ${PKG_BUILD_OPTIONS.blackbox70:Mxft2}
+.include "../../x11/libXft/buildlink3.mk"
+.endif
 .endif # BLACKBOX70_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-blackbox70
