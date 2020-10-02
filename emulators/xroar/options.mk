8b83456e28648344daaf23f13fdf7d318208d9b6
ryoon <ryoon@pkgsrc.org>
2020-10-02 10:25:03 +0000

    emulators/xroar: import xroar-0.36.2
    XRoar - a Dragon/Tandy Coco emulator
    
    XRoar is a Dragon emulator that runs on a wide variety of platforms.
    Due to hardware similarities, XRoar also emulates the Tandy Colour
    Computer (CoCo) models 1 & 2.  Some features are:
    
       * Emulates Dragon 32, Dragon 64, Dragon 200-E, Tandy CoCo 1 & 2, and
         compatibles.
       * Emulates DragonDOS, Delta and RSDOS disk systems.
       * Emulates the Orchestra 90-CC stereo sound cartridge.
       * Supports both raw and translated keyboard modes.
       * Reads and writes virtual cassettes (compact '.cas' files and audio
         files).
       * Reads and writes VDK, JVC and DMK format virtual floppy diskettes.
       * Saves and loads machine snapshots.
       * Provides a GDB target for remote debugging.
       * Games Master Cartridge support, including SN76489 sound chip.
       * MOOH RAM expansion + SPI support.
       * Glenside IDE support.

---
+++ emulators/xroar/options.mk
@@ -0,0 +1,43 @@
+# $NetBSD: options.mk,v 1.1 2020/10/02 10:25:03 ryoon Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.xroar
+
+PKG_SUPPORTED_OPTIONS+=		alsa oss pulseaudio
+PKG_OPTIONS_REQUIRED_GROUPS=	audio
+PKG_OPTIONS_GROUP.audio=	alsa oss pulseaudio
+
+.if ${OPSYS} == "Linux"
+PKG_SUGGESTED_OPTIONS+=	alsa
+.else
+PKG_SUGGESTED_OPTIONS+=	oss
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Malsa)
+CONFIGURE_ARGS+=	--with-alsa
+.include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-alsa
+.endif
+
+.if !empty(PKG_OPTIONS:Moss)
+.include "../../mk/oss.buildlink3.mk"
+LDFLAGS=		${LIBOSSAUDIO}
+CONFIGURE_ARGS+=	--with-oss
+
+SUBST_CLASSES+=		oss
+SUBST_STAGE.oss=	pre-configure
+SUBST_MESSAGE.oss=	Set OSS device node
+SUBST_FILES.oss+=	src/oss/ao_oss.c
+SUBST_SED.oss+=		-e 's,/dev/dsp,${DEVOSSAUDIO},g'
+.else
+CONFIGURE_ARGS+=	--without-oss
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pulse
+.else
+CONFIGURE_ARGS+=	--without-pulse
+.endif
