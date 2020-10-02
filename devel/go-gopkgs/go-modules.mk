ecccb98971af560a0619ddb6254ef4c4e9e509be
bsiegert <bsiegert@pkgsrc.org>
2020-10-02 19:44:47 +0000

    Update gopkgs to 2.1.2.
    No changelog from upstream.
    
    This is now a Go module.

---
+++ devel/go-gopkgs/go-modules.mk
@@ -0,0 +1,4 @@
+GO_MODULE_FILES+=	github.com/karrick/godirwalk/@v/v1.12.0.mod
+GO_MODULE_FILES+=	github.com/karrick/godirwalk/@v/v1.12.0.zip
+GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.8.1.mod
+GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.8.1.zip
