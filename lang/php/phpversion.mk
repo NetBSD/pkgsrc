c71eef9cad55373b49a6bc38a241e05ed659bd6b
taca <taca@pkgsrc.org>
2020-10-04 03:14:53 +0000

    lang/php72: update to 7.2.34
    Update php72 package to 7.2.34.
    
    
    01 Oct 2020, PHP 7.2.34
    
    - Core:
      . Fixed bug #79699 (PHP parses encoded cookie names so malicious `__Host-`
        cookies can be sent). (CVE-2020-7070) (Stas)
    
    - OpenSSL:
      . Fixed bug #79601 (Wrong ciphertext/tag in AES-CCM encryption for a 12
        bytes IV). (CVE-2020-7069) (Jakub Zelenka)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.310 2020/10/04 03:12:46 taca Exp $
+# $NetBSD: phpversion.mk,v 1.311 2020/10/04 03:14:53 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP72_VERSION=	7.2.33
+PHP72_VERSION=	7.2.34
 PHP73_VERSION=	7.3.23
 PHP74_VERSION=	7.4.11
 
