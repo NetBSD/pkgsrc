7d92ba1f8458af350c9ae3a7af4daa75a6041b5c
schmonz <schmonz@pkgsrc.org>
2020-10-05 12:17:20 +0000

    If any of our several rcvars aren't YES, that's fine: don't exit nonzero. Bump version.
    While here, document in qmail-qread-client that its rc.conf vars are
    shared with the qmailqread service.

--- mail/qmail-run/files/qmail-qread-client.sh
+++ mail/qmail-run/files/qmail-qread-client.sh
@@ -1,6 +1,6 @@
 #!@SH@
 #
-# $NetBSD: qmail-qread-client.sh,v 1.5 2017/04/11 14:04:37 schmonz Exp $
+# $NetBSD: qmail-qread-client.sh,v 1.6 2020/10/05 12:17:20 schmonz Exp $
 #
 # @PKGNAME@ client program to allow local non-root users to see
 # the queue. Requires that the corresponding server program be running.
@@ -9,10 +9,15 @@
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
-	load_rc_config qmailqread
 fi
 
+# User-settable rc.conf variables and their default values
+# (shared with the qmailqread service):
 : ${qmailqread_tcphost:="127.0.0.1"}
 : ${qmailqread_tcpport:="20025"}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config qmailqread
+fi
 
 exec @PREFIX@/bin/tcpclient -RHl0 -- ${qmailqread_tcphost} ${qmailqread_tcpport} @SH@ -c 'exec @CAT@ <&6'
