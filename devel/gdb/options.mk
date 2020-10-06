83821740e10be8bada43f8d7b86d5adb48d21ed3
mcf <mcf@pkgsrc.org>
2020-10-06 21:13:38 +0000

    gdb: make python files in PLIST conditional on python option
    These files are only installed when python support is enabled. This
    issue was already fixed for devel/gdb7 by micha, so apply the same
    fix here.

--- devel/gdb/options.mk
+++ devel/gdb/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.7 2019/09/02 13:19:36 adam Exp $
+# $NetBSD: options.mk,v 1.8 2020/10/06 21:13:38 mcf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gdb
 PKG_SUPPORTED_OPTIONS=	python
 PKG_SUGGESTED_OPTIONS=	python
+PLIST_VARS+=		python
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +11,5 @@ PKG_SUGGESTED_OPTIONS=	python
 CONFIGURE_ARGS+=	--with-python=${PYTHONBIN}
 PYTHON_VERSIONS_ACCEPTED=	 27
 .include "../../lang/python/application.mk"
+PLIST.python=	yes
 .endif
