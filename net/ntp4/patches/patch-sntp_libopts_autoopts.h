9c9962a6f5408126cb4ef4ef9ebcd7ce57f4c872
sjmulder <sjmulder@pkgsrc.org>
2020-10-07 09:09:39 +0000

    net/ntp4: Fix Linux build

--- net/ntp4/patches/patch-sntp_libopts_autoopts.h
+++ net/ntp4/patches/patch-sntp_libopts_autoopts.h
@@ -1,22 +1,29 @@
-$NetBSD: patch-sntp_libopts_autoopts.h,v 1.1 2020/06/21 15:10:47 taca Exp $
+$NetBSD: patch-sntp_libopts_autoopts.h,v 1.2 2020/10/07 09:09:39 sjmulder Exp $
 
 * Changes from NetBSD base.
+* Compat for noreturn on old compilers
 
 --- sntp/libopts/autoopts.h.orig	2015-04-26 18:43:14.000000000 +0000
 +++ sntp/libopts/autoopts.h
-@@ -32,7 +32,11 @@
+@@ -32,7 +32,16 @@
  
  #ifndef AUTOGEN_AUTOOPTS_H
  #define AUTOGEN_AUTOOPTS_H
-+#if 0
- #include <stdnoreturn.h>
+-#include <stdnoreturn.h>
++
++#if __STDC_VERSION__-0 >= 201112L
++#  include <stdnoreturn.h>
++#elif defined(__GNUC__)
++#  define noreturn __attribute__((noreturn))
++#elif defined(__NetBSD__)
++#  define noreturn __dead
 +#else
-+#define noreturn __dead
++#  define noreturn
 +#endif
  
  #define AO_NAME_LIMIT           127
  #define AO_NAME_SIZE            ((size_t)(AO_NAME_LIMIT + 1))
-@@ -452,7 +456,7 @@ typedef enum { AOFLAG_TABLE } ao_flags_t
+@@ -452,7 +461,7 @@ typedef enum { AOFLAG_TABLE } ao_flags_t
  #undef  _aof_
  
  static char const   zNil[] = "";
