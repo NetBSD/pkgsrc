b87ee747d03da080dd109d112033491c234fc13a
schmonz <schmonz@pkgsrc.org>
2020-10-07 19:46:53 +0000

    Update to 0.16.0. From the changelog:
    API changes:
    
    * refactoring into a somewhat MVC model: split large feeds.py into
      model.py and controller.py
    * rename FeedCacheStorage to FeedItemCacheStorage
    * factor out getter/setters in the base sqlite class
    * remove conn member in sqlite to force use of context manager
    * move session and fetching to the feed manager
    * rename feeds to feed_manager in main
    * allow absolute path in folder setting (Closes: #14)
    
    New features:
    
    * caching: latest feed contents get cached to avoid re-downloading
      unchanged feeds. this includes parsing HTTP headers and so on, and
      relies on the good behavior of the `cachecontrol` Python module
    * Python 3.6-3.8 support
    
    Bugfixes:
    * recover from feedparser exceptions (Closes: #964597)
    
    [ Roland Hieber ]
    * README: don't let the example config create a folder named '~/Mailbox/'
    
    [ Ian Zimmerman ]
    * add --quiet option to silence warnings since --loglevel was broken

--- mail/feed2exec/patches/patch-setup.py
+++ mail/feed2exec/patches/patch-setup.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-setup.py,v 1.1 2020/01/29 16:48:48 schmonz Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/10/07 19:46:53 schmonz Exp $
 
 Avoid setuptools_scm since tarball has no .git.
 
---- setup.py.orig	2019-02-26 22:03:32.000000000 +0000
+--- setup.py.orig	2020-10-06 18:35:43.000000000 +0000
 +++ setup.py
-@@ -113,10 +113,6 @@ if __name__ == '__main__':
+@@ -126,10 +126,6 @@ if __name__ == '__main__':
            long_description=sphinx2rst('README.rst'),
            license=mod.__license_short__,
            url=mod.__website__,
@@ -15,12 +15,11 @@ Avoid setuptools_scm since tarball has no .git.
            packages=packages,
            package_data=package_data,
            entry_points={
-@@ -126,7 +122,7 @@ if __name__ == '__main__':
-                   % (mod.__prog__, mod.__prog__),
+@@ -140,7 +136,6 @@ if __name__ == '__main__':
                ]
            },
--          setup_requires=['setuptools_scm',
-+          setup_requires=[
-                           'pytest-runner',
-                           'pytest-cov',
-                           'sphinx',
+           setup_requires=[
+-              'setuptools_scm',
+               'sphinx',
+           ],
+           install_requires=requires,
