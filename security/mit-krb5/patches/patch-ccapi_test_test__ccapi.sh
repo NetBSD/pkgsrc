75fd8e7ed36007e2a413ef8af81dfcc1118af8fb
gdt <gdt@pkgsrc.org>
2020-10-08 00:23:14 +0000

    mit-krb5: Add patch comment and note that it has been filed upstream

--- security/mit-krb5/patches/patch-ccapi_test_test__ccapi.sh
+++ security/mit-krb5/patches/patch-ccapi_test_test__ccapi.sh
@@ -1,4 +1,9 @@
-$NetBSD: patch-ccapi_test_test__ccapi.sh,v 1.1 2020/10/05 23:24:45 gdt Exp $
+$NetBSD: patch-ccapi_test_test__ccapi.sh,v 1.2 2020/10/08 00:23:14 gdt Exp $
+
+Remediate bashism.
+Remediate lack of final newline (by accident, but it should be there).
+
+Reported upstream via krb5-bugs@mit.edu on20201007.
 
 --- ccapi/test/test_ccapi.sh.orig	2020-02-12 17:21:58.000000000 +0000
 +++ ccapi/test/test_ccapi.sh
