e88e414ac09dcacc80ab70e7a1636390627d289e
adam <adam@pkgsrc.org>
2020-10-08 10:57:46 +0000

    cmake: updated to 3.18.4
    CMake 3.18.4
    * FindJNI: Add aarch64 support
    * VS: Populate `std:c{11,17}` flag table entries for v142
    * OBJCXX: Fix regression for compiling cpp files as objcxx
    * VS: Fix regression in C# source links
    * FindPython: Fix erroneous regex in ABI check
    * Clang: Look for llvm-lib when using MSVC-like front-end
    * FindCUDA/select_compute_arch: Add CUDA 11.1 and SM86 support

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.3 2020/09/28 07:37:43 adam Exp $
+# $NetBSD: version.mk,v 1.4 2020/10/08 10:57:46 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.18.3
+CMAKE_VERSION=	3.18.4
 CMAKE_API=	${CMAKE_VERSION:R}
