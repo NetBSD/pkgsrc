5d364eb542f18886d84bc69b6ebf6d4e3d4f4e7e
leot <leot@pkgsrc.org>
2020-10-08 22:50:16 +0000

    mupdf: Update to 1.18.0
    Changes:
    1.18.0
    ------
    mutool draw: bbox output format.
    mutool draw: structured text extraction in JSON format.
    mutool run: StructuredText.walk()
    mutool run: PDFPage.process() and PDFAnnotation.process()
    
    mupdf-gl: Added IBM Common User Access shortcuts for copy & paste.
    mupdf-gl: Improved redaction UI.
    
    java: Example desktop Java viewer.
    
    wasm: WebAssembly viewer that runs in a browser.
    
    api: Dehyphenation option in structured text extraction, to improve
         searching for words that are hyphenated across lines.
    api: pdf_new_date, pdf_to_date, pdf_dict_put_date and pdf_dict_get_date.
    api: Added choice of image redaction algorithms: none, full, partial.
    api: Optional use of Tesseract to use OCR to extract text.
    
    svg: Many small parsing fixes.
    svg: Parse inline CSS style attributes.
    svg: Text elements (simple text layout only).
    
    html: Added HTML5 parser.
    
    pdf: High security redaction -- save redacted PDF as flattened bitmap,
         guaranteed to not leak any sensitive redacted information.
    
    Experimental C++ bindings.
    Experimental Python bindings.

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.39 2020/05/22 10:55:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2020/10/08 22:50:16 leot Exp $
 
 BUILDLINK_TREE+=	mupdf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mupdf
 MUPDF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.14.0
-BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.17.0nb1
+BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.18.0
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
 pkgbase := mupdf
@@ -27,6 +27,7 @@ pkgbase := mupdf
 .include "../../graphics/jbig2dec/buildlink3.mk"
 .include "../../graphics/openjpeg/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
+.include "../../www/gumbo-parser/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif	# MUPDF_BUILDLINK3_MK
