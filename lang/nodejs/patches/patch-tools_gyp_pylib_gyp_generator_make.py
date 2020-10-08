b32ba12397a179ae02b29ae4c4d9cc49262405d6
adam <adam@pkgsrc.org>
2020-10-08 10:58:35 +0000

    nodejs: updated to 14.13.1
    Version 14.13.1 (Current)
    
    Notable Changes
    fs:
    remove experimental from rmdir recursive

--- lang/nodejs/patches/patch-tools_gyp_pylib_gyp_generator_make.py
+++ lang/nodejs/patches/patch-tools_gyp_pylib_gyp_generator_make.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_generator_make.py,v 1.3 2013/12/12 11:52:37 jperkin Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_generator_make.py,v 1.4 2020/10/08 10:58:35 adam Exp $
 
+Use the system libtool on Darwin.
 Add support for NetBSD and DragonFly.
-Ensure we use the system libtool on OSX.
 
---- tools/gyp/pylib/gyp/generator/make.py.orig	2013-12-12 05:20:06.000000000 +0000
+--- tools/gyp/pylib/gyp/generator/make.py.orig	2020-10-07 17:47:43.000000000 +0000
 +++ tools/gyp/pylib/gyp/generator/make.py
-@@ -174,7 +174,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
+@@ -180,7 +180,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
  
  LINK_COMMANDS_MAC = """\
  quiet_cmd_alink = LIBTOOL-STATIC $@
@@ -14,12 +14,12 @@ Ensure we use the system libtool on OSX.
  
  quiet_cmd_link = LINK($(TOOLSET)) $@
  cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o "$@" $(LD_INPUTS) $(LIBS)
-@@ -2012,7 +2012,7 @@ def GenerateOutput(target_list, target_d
-         'flock': './gyp-flock-tool flock',
-         'flock_index': 2,
-     })
--  elif flavor == 'freebsd':
-+  elif flavor == 'freebsd' or flavor == 'dragonflybsd' or flavor == 'netbsd':
-     # Note: OpenBSD has sysutils/flock. lockf seems to be FreeBSD specific.
-     header_params.update({
-         'flock': 'lockf',
+@@ -2331,7 +2331,7 @@ def GenerateOutput(target_list, target_d
+                 "flock_index": 2
+             }
+         )
+-    elif flavor == "freebsd":
++    elif flavor == "freebsd" or flavor == 'dragonflybsd' or flavor == 'netbsd':
+         # Note: OpenBSD has sysutils/flock. lockf seems to be FreeBSD specific.
+         header_params.update({"flock": "lockf"})
+     elif flavor == "openbsd":
--- lang/nodejs/patches/patch-tools_gyp_pylib_gyp_xcode__emulation.py
+++ lang/nodejs/patches/patch-tools_gyp_pylib_gyp_xcode__emulation.py
@@ -1,15 +1,24 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_xcode__emulation.py,v 1.1 2017/05/30 14:26:38 adam Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_xcode__emulation.py,v 1.2 2020/10/08 10:58:35 adam Exp $
 
 Do not generate debugging symbols (uses lots of disk space).
 
---- tools/gyp/pylib/gyp/xcode_emulation.py.orig	2017-05-30 14:15:04.000000000 +0000
+--- tools/gyp/pylib/gyp/xcode_emulation.py.orig	2020-10-07 17:47:43.000000000 +0000
 +++ tools/gyp/pylib/gyp/xcode_emulation.py
-@@ -577,7 +577,7 @@ class XcodeSettings(object):
+@@ -610,7 +610,7 @@ class XcodeSettings(object):
  
-     self._Appendf(cflags, 'GCC_OPTIMIZATION_LEVEL', '-O%s', default='s')
+         self._Appendf(cflags, "GCC_OPTIMIZATION_LEVEL", "-O%s", default="s")
  
--    if self._Test('GCC_GENERATE_DEBUGGING_SYMBOLS', 'YES', default='YES'):
-+    if self._Test('GCC_GENERATE_DEBUGGING_SYMBOLS', 'YES', default='NO'):
-       dbg_format = self._Settings().get('DEBUG_INFORMATION_FORMAT', 'dwarf')
-       if dbg_format == 'dwarf':
-         cflags.append('-gdwarf-2')
+-        if self._Test("GCC_GENERATE_DEBUGGING_SYMBOLS", "YES", default="YES"):
++        if self._Test("GCC_GENERATE_DEBUGGING_SYMBOLS", "YES", default="NO"):
+             dbg_format = self._Settings().get("DEBUG_INFORMATION_FORMAT", "dwarf")
+             if dbg_format == "dwarf":
+                 cflags.append("-gdwarf-2")
+@@ -1097,7 +1097,7 @@ class XcodeSettings(object):
+         # For static libraries, no dSYMs are created.
+         result = []
+         if (
+-            self._Test("GCC_GENERATE_DEBUGGING_SYMBOLS", "YES", default="YES")
++            self._Test("GCC_GENERATE_DEBUGGING_SYMBOLS", "YES", default="NO")
+             and self._Test(
+                 "DEBUG_INFORMATION_FORMAT", "dwarf-with-dsym", default="dwarf"
+             )
