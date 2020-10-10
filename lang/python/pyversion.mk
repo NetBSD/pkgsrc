8500484dd75dee29fe1fb10b0e4b8e43dcc6a6f5
adam <adam@pkgsrc.org>
2020-10-10 20:19:47 +0000

    python39: added version 3.9.0
    3.9.0 Release highlights
    
    New syntax features:
    
    PEP 584, union operators added to dict;
    PEP 585, type hinting generics in standard collections;
    PEP 614, relaxed grammar restrictions on decorators.
    
    New built-in features:
    
    PEP 616, string methods to remove prefixes and suffixes.
    
    New features in the standard library:
    
    PEP 593, flexible function and variable annotations;
    os.pidfd_open() added that allows process management without races and signals.
    
    Interpreter improvements:
    
    PEP 573, fast access to module state from methods of C extension types;
    PEP 617, CPython now uses a new parser based on PEG;
    a number of Python builtins (range, tuple, set, frozenset, list, dict) are now sped up using PEP 590 vectorcall;
    garbage collection does not block on resurrected objects;
    a number of Python modules (_abc, audioop, _bz2, _codecs, _contextvars, _crypt, _functools, _json, _locale, math, operator, resource, time, _weakref) now use multiphase initialization as defined by PEP 489;
    a number of standard library modules (audioop, ast, grp, _hashlib, pwd, _posixsubprocess, random, select, struct, termios, zlib) are now using the stable ABI defined by PEP 384.
    
    New library modules:
    
    PEP 615, the IANA Time Zone Database is now present in the standard library in the zoneinfo module;
    an implementation of a topological sort of a graph is now provided in the new graphlib module.
    
    Release process changes:
    
    PEP 602, CPython adopts an annual release cycle.

--- lang/python/pyversion.mk
+++ lang/python/pyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: pyversion.mk,v 1.131 2020/03/15 14:28:36 rillig Exp $
+# $NetBSD: pyversion.mk,v 1.132 2020/10/10 20:19:47 adam Exp $
 
 # This file determines which Python version is used as a dependency for
 # a package.
@@ -8,7 +8,7 @@
 # PYTHON_VERSION_DEFAULT
 #	The preferred Python version to use.
 #
-#	Possible values: 27 36 37 38
+#	Possible values: 27 36 37 38 39
 #	Default: 37
 #
 # === Infrastructure variables ===
@@ -27,13 +27,13 @@
 #	order of the entries matters, since earlier entries are
 #	preferred over later ones.
 #
-#	Possible values: 38 37 36 27
-#	Default: 38 37 36 27
+#	Possible values: 39 38 37 36 27
+#	Default: 39 38 37 36 27
 #
 # PYTHON_VERSIONS_INCOMPATIBLE
 #	The Python versions that are NOT acceptable for the package.
 #
-#	Possible values: 27 36 37 38
+#	Possible values: 27 36 37 38 39
 #	Default: (empty)
 #
 # PYTHON_FOR_BUILD_ONLY
@@ -85,7 +85,7 @@ BUILD_DEFS+=		PYTHON_VERSION_DEFAULT
 BUILD_DEFS_EFFECTS+=	PYPACKAGE
 
 PYTHON_VERSION_DEFAULT?=		37
-PYTHON_VERSIONS_ACCEPTED?=		38 37 36 27
+PYTHON_VERSIONS_ACCEPTED?=		39 38 37 36 27
 PYTHON_VERSIONS_INCOMPATIBLE?=		# empty by default
 
 # transform the list into individual variables
