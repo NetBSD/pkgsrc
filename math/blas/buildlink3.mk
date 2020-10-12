87edcb24b1717c733c136e2fac89a164e3c60850
bacon <bacon@pkgsrc.org>
2020-10-12 21:51:57 +0000

    math/blas, math/lapack: Install interchangeable BLAS system
    Install the new interchangeable BLAS system created by Thomas Orgis,
    currently supporting Netlib BLAS/LAPACK, OpenBLAS, cblas, lapacke, and
    Apple's Accelerate.framework.  This system allows the user to select any
    BLAS implementation without modifying packages or using package options, by
    setting PKGSRC_BLAS_TYPES in mk.conf. See mk/blas.buildlink3.mk for details.
    
    This commit should not alter behavior of existing packages as the system
    defaults to Netlib BLAS/LAPACK, which until now has been the only supported
    implementation.
    
    Details:
    
    Add new mk/blas.buildlink3.mk for inclusion in dependent packages
    Install compatible Netlib math/blas and math/lapack packages
    Update math/blas and math/lapack MAINTAINER approved by adam@
    OpenBLAS, cblas, and lapacke will follow in separate commits
    Update direct dependents to use mk/blas.buildlink3.mk
    Perform recursive revbump

--- math/blas/buildlink3.mk
+++ math/blas/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/02/28 21:48:53 plunky Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/10/12 21:51:57 bacon Exp $
 
 BUILDLINK_TREE+=	blas
 
 .if !defined(BLAS_BUILDLINK3_MK)
 BLAS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.blas+=	blas>=1.0nb2
-BUILDLINK_ABI_DEPENDS.blas+=	blas>=1.0nb3
+BUILDLINK_API_DEPENDS.blas+=	blas>=3.9.0
+BUILDLINK_ABI_DEPENDS.blas+=	blas>=3.9.0nb1
 BUILDLINK_PKGSRCDIR.blas?=	../../math/blas
-
-BLAS_LIBS=			-lblas
 .endif # BLAS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-blas
