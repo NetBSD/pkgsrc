bc6f1f6cd613e8bb271b43a9b258c855a0803267
wiz <wiz@pkgsrc.org>
2020-10-13 17:32:16 +0000

    py-numpy/bl3.mk: fix ABI depends for python 2.7 case

--- math/py-numpy/buildlink3.mk
+++ math/py-numpy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/10/12 21:51:58 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/10/13 17:32:16 wiz Exp $
 
 BUILDLINK_TREE+=	py-numpy
 
@@ -9,9 +9,10 @@ PY_NUMPY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.py-numpy+=	${PYPKGPREFIX}-numpy>=1.0
 .if ${_PYTHON_VERSION} == 27
-BUILDLINK_ABI_DEPENDS.py-numpy?=		${PYPKGPREFIX}-numpy>=1.19.2nb1
+BUILDLINK_ABI_DEPENDS.py-numpy?=	${PYPKGPREFIX}-numpy>=1.16.6nb1
 BUILDLINK_PKGSRCDIR.py-numpy?=		../../math/py-numpy16
 .else
+BUILDLINK_ABI_DEPENDS.py-numpy?=	${PYPKGPREFIX}-numpy>=1.19.2nb1
 BUILDLINK_PKGSRCDIR.py-numpy?=		../../math/py-numpy
 .endif
 
