74f339728da7a68e63ae4f381cd0c6af6a5cce57
wiz <wiz@pkgsrc.org>
2020-10-14 16:39:50 +0000

    lua53: update to 5.3.6.
    This is a bug-fix release.
    
    Full diffs here:
    https://www.lua.org/work/diffs-lua-5.3.5-lua-5.3.6.html

--- lang/lua53/version.mk
+++ lang/lua53/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.6 2018/07/17 21:52:25 alnsn Exp $
+# $NetBSD: version.mk,v 1.7 2020/10/14 16:39:50 wiz Exp $
 
-LUA_VERSION=	5.3.5
+LUA_VERSION=	5.3.6
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
