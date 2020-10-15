91476562f60c3a8f2b5aad10601562b056772a2e
fox <fox@pkgsrc.org>
2020-10-15 16:10:23 +0000

    print/foliate: Updates to 2.5.0
    Changes since 2.4.2:
    
    Changes:
    
      * Improved support for touch screen (#591)
      * OPDS credentials can now be remembered (#569)
      * Various fixes for GNOME 3.38 (#548, #603, #608)
      * Fixed Wiktionary internal links (#577)
      * Added Basque (#579), Korean (#604), and Norwegian (#574, #575) translations

--- print/foliate/patches/patch-src_epubView.js
+++ print/foliate/patches/patch-src_epubView.js
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_epubView.js,v 1.3 2020/07/03 12:18:12 fox Exp $
+$NetBSD: patch-src_epubView.js,v 1.4 2020/10/15 16:10:24 fox Exp $
 
 Adds variable to help substitute the correct python version suffix.
 
---- src/epubView.js.orig	2020-07-02 07:05:56.000000000 +0000
+--- src/epubView.js.orig	2020-10-15 00:52:44.000000000 +0000
 +++ src/epubView.js
-@@ -25,7 +25,7 @@ const {
-     mimetypes, mimetypeIs, execCommand, recursivelyDeleteDir
+@@ -26,7 +26,7 @@ const {
+     debounce
  } = imports.utils
  
 -const python = GLib.find_program_in_path('python') || GLib.find_program_in_path('python3')
