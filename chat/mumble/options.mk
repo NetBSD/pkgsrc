dddcef494622a5fc2e23707c29e253a13a4cbd30
nia <nia@pkgsrc.org>
2020-10-16 07:42:48 +0000

    mumble: Update to 1.3.3
    pkgsrc changes:
    
    - Fixed device iteration on NetBSD
    - Fixed the portaudio option
    - Added a jack option
    
    Client
    
    Fixed: Chatbox invisble (zero height)
    Fixed: Handling of invalid packet sizes
    Fixed: Race-condition leading to loss of shortcuts
    Fixed: Link in About dialog is now clickable again
    Fixed: Sizing issues in ACL-Editor
    Improved: PulseAudio now always samples at 48 kHz
    
    Server
    
    Fixed: Crash due to problems when using PostgreSQL
    Fixed: Handling of invalid package sizes

--- chat/mumble/options.mk
+++ chat/mumble/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.5 2020/04/28 05:47:10 riastradh Exp $
+# $NetBSD: options.mk,v 1.6 2020/10/16 07:42:48 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mumble
-PKG_SUPPORTED_OPTIONS=		alsa dbus pulseaudio portaudio
+PKG_SUPPORTED_OPTIONS=		alsa dbus jack pulseaudio portaudio
 PKG_SUGGESTED_OPTIONS=		dbus portaudio
 PKG_SUGGESTED_OPTIONS.Linux=	alsa
 
@@ -16,18 +16,26 @@ CONFIG_OPTIONS+=	CONFIG+=no-alsa
 
 .if !empty(PKG_OPTIONS:Mdbus)
 CONFIG_OPTIONS+=	CONFIG+=dbus
-#.include "../../x11/qt5-qtdbus/buildlink3.mk"
 .else
 CONFIG_OPTIONS+=	CONFIG+=no-dbus
 .endif
 
+.if !empty(PKG_OPTIONS:Mjack)
+CONFIG_OPTIONS+=	CONFIG+=jackaudio
+.include "../../audio/jack/buildlink3.mk"
+.else
+CONFIG_OPTIONS+=	CONFIG+=no-jackaudio
+.endif
+
 .if !empty(PKG_OPTIONS:Mpulseaudio)
+CONFIG_OPTIONS+=	CONFIG+=pulseaudio
 .include "../../audio/pulseaudio/buildlink3.mk"
 .else
 CONFIG_OPTIONS+=	CONFIG+=no-pulseaudio
 .endif
 
 .if !empty(PKG_OPTIONS:Mportaudio)
+CONFIG_OPTIONS+=	CONFIG+=portaudio
 .include "../../audio/portaudio/buildlink3.mk"
 .else
 CONFIG_OPTIONS+=	CONFIG+=no-portaudio
