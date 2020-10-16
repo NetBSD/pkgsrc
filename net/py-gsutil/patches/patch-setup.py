fab3ad646d943a5758fb7e446979a5a3fe66ae85
bsiegert <bsiegert@pkgsrc.org>
2020-10-16 07:41:10 +0000

    Update py-gsutil to 4.53.
    Release 4.53 (release date: 2020-08-19)
    ======================================
    Bug Fixes
    ------------------
    - Remove socksipy dependency to fix proxy authentication
    - Retry on errors relating to region specification for S3
    
    Other Changes
    ------------------
    - Prompt Daisy chain users to try STS
    - Add new IAM types, and disable URL parsing for  IAM b/c it has subcommands.
    - Many text updates and clarifications.

--- net/py-gsutil/patches/patch-setup.py
+++ net/py-gsutil/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.4 2020/07/07 16:49:19 bsiegert Exp $
+$NetBSD: patch-setup.py,v 1.5 2020/10/16 07:41:10 bsiegert Exp $
 
 Relax requirements.
 
---- setup.py.orig	2020-05-19 21:00:16.000000000 +0000
+--- setup.py.orig	2020-08-19 21:17:24.000000000 +0000
 +++ setup.py
-@@ -44,14 +44,14 @@ requires = [
-     'httplib2>=0.11.3',
+@@ -44,7 +44,7 @@ requires = [
+     'httplib2>=0.18',
      'google-reauth>=0.1.0',
      # TODO: Sync submodule with tag referenced here once #339 is fixed in mock.
 -    'mock==2.0.0',
@@ -13,11 +13,3 @@ Relax requirements.
      'monotonic>=1.4',
      'pyOpenSSL>=0.13',
      'retry_decorator>=1.0.0',
-     'six>=1.12.0',
-     # Not using 1.02 because of:
-     #   https://code.google.com/p/socksipy-branch/issues/detail?id=3
--    'SocksiPy-branch==1.01',
-+    'SocksiPy-branch>=1.01',
- ]
- 
- CURDIR = os.path.abspath(os.path.dirname(__file__))
