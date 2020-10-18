3c812e94f688feed6ed70adb82a4f7d093ff6bfe
adam <adam@pkgsrc.org>
2020-10-18 09:24:02 +0000

    postgresql13: added version 13.0
    PostgreSQL 13 contains many new features and enhancements, including:
    * Space savings and performance gains from de-duplication of B-tree index entries
    * Improved performance for queries that use aggregates or partitioned tables
    * Better query planning when using extended statistics
    * Parallelized vacuuming of indexes
    * Incremental sorting

---
+++ databases/postgresql13-client/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/10/18 09:24:02 adam Exp $
+
+BUILDLINK_TREE+=	postgresql13-client
+
+.if !defined(POSTGRESQL13_CLIENT_BUILDLINK3_MK)
+POSTGRESQL13_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql13-client+=	postgresql13-client>=13.0
+BUILDLINK_ABI_DEPENDS.postgresql13-client+=	postgresql13-client>=13.0
+BUILDLINK_PKGSRCDIR.postgresql13-client?=	../../databases/postgresql13-client
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql13-client=	-lpq ${BUILDLINK_LDADD.gettext}
+BUILDLINK_FILES.postgresql13-client+=	bin/pg_config
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif # POSTGRESQL13_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql13-client
