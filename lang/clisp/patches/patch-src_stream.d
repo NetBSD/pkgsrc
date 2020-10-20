2361f3aaa80d2368c86fe17b4a3f91a291621e83
jperkin <jperkin@pkgsrc.org>
2020-10-20 12:46:17 +0000

    clisp: Remove iconv warning avoidance.
    This breaks the build on Darwin when using pkgsrc libiconv.

--- lang/clisp/patches/patch-src_stream.d
+++ lang/clisp/patches/patch-src_stream.d
@@ -1,13 +1,24 @@
-$NetBSD: patch-src_stream.d,v 1.1 2016/01/26 09:13:26 dbj Exp $
+$NetBSD: patch-src_stream.d,v 1.2 2020/10/20 12:46:17 jperkin Exp $
+
+Remove incomplete iconv handling, it doesn't work everywhere, and warnings
+are far preferable to errors.
 
 --- src/stream.d.orig	2010-07-06 14:21:51.000000000 +0000
 +++ src/stream.d
-@@ -3994,7 +3994,7 @@ global object iconv_range (object encodi
+@@ -3993,16 +3993,6 @@ global object iconv_range (object encodi
+  error_unencodable(encoding); */
  nonreturning_function(extern, error_unencodable, (object encoding, chart ch));
  
- /* Avoid annoying warning caused by a wrongly standardized iconv() prototype. */
+-/* Avoid annoying warning caused by a wrongly standardized iconv() prototype. */
 -#ifdef GNU_LIBICONV
-+#if defined(GNU_LIBICONV) && !defined(__APPLE__)
-   #undef iconv
-   #define iconv(cd,inbuf,inbytesleft,outbuf,outbytesleft) \
-     libiconv(cd,(ICONV_CONST char **)(inbuf),inbytesleft,outbuf,outbytesleft)
+-  #undef iconv
+-  #define iconv(cd,inbuf,inbytesleft,outbuf,outbytesleft) \
+-    libiconv(cd,(ICONV_CONST char **)(inbuf),inbytesleft,outbuf,outbytesleft)
+-#else
+-  #define iconv(cd,inbuf,inbytesleft,outbuf,outbytesleft) \
+-    (iconv)(cd,(ICONV_CONST char **)(inbuf),inbytesleft,outbuf,outbytesleft)
+-#endif
+-
+ /* open the iconv conversion and signal errors when necessary
+  skip error when CHARSET is NULLOBJ
+  begin_system_call() must be called before this!!!
