ea4ae9626c98586fed88662c67d5b77d4f5d0ce6
tnn <tnn@pkgsrc.org>
2020-10-20 22:16:16 +0000

    backuppc: update to 4.4.0
    This release contains several new features and some bug fixes.
    New features include:
    - any full/filled backup can be marked for keeping, which prevents any
      expiry or deletion
    - any backup can be annotated with a comment (eg, "prior to upgrade of xyz")
    - added metrics CGI that replaces RSS and adds Prometheus support
    - tar XferMethod now supports xattrs and acls
    - rsync XferMethod now correctly supports xattrs on directories and symlinks
    - nightly pool scanning now verifies the md5 digests of a configurable
      fraction of pool files
    - code runs through perltidy so format is now uniform

--- sysutils/backuppc/patches/patch-configure.pl
+++ sysutils/backuppc/patches/patch-configure.pl
@@ -1,15 +1,15 @@
-$NetBSD: patch-configure.pl,v 1.2 2019/11/08 17:11:12 tnn Exp $
+$NetBSD: patch-configure.pl,v 1.3 2020/10/20 22:16:16 tnn Exp $
 
 Prevent previous installation from altering build results.
 
---- configure.pl.orig	2018-05-07 17:14:29.000000000 +0000
+--- configure.pl.orig	2020-06-20 22:55:58.000000000 +0000
 +++ configure.pl
-@@ -266,7 +266,7 @@ EOF
+@@ -250,7 +250,7 @@ EOF
  #
- my $ConfigDir = $opts{"config-dir"} || "/etc/BackupPC";
- my $ConfigPath = "";
+ my $ConfigDir    = $opts{"config-dir"} || "/etc/BackupPC";
+ my $ConfigPath   = "";
 -my $ConfigFileOK = 1;
 +my $ConfigFileOK = 0;
  while ( 1 ) {
-     if ( $ConfigFileOK && -f "$DestDir$ConfigDir/config.pl"
-             && (!defined($opts{fhs}) || $opts{fhs}) && !defined($opts{"config-path"}) ) {
+     if (   $ConfigFileOK
+         && -f "$DestDir$ConfigDir/config.pl"
