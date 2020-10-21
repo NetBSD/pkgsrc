f836a2a959191aeae699a7eddeaa98488cd2a666
spz <spz@pkgsrc.org>
2020-10-21 19:23:29 +0000

    Pullup ticket #6334 - requested by taca lang/php74: security update
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.310
    - lang/php74/distinfo                                           1.13
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Oct  4 03:12:46 UTC 2020
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php74: distinfo
    
       Log Message:
       lang/php74: update to 7.4.11
    
       Update php74 to 7.4.11.
    
       01 Oct 2020, PHP 7.4.11
    
       - Core:
         . Fixed bug #79699 (PHP parses encoded cookie names so malicious `__Host-`
           cookies can be sent). (CVE-2020-7070) (Stas)
         . Fixed bug #79979 (passing value to by-ref param via CUFA crashes). (cmb,
           Nikita)
         . Fixed bug #80037 (Typed property must not be accessed before initialization
           when __get() declared). (Nikita)
         . Fixed bug #80048 (Bug #69100 has not been fixed for Windows). (cmb)
         . Fixed bug #80049 (Memleak when coercing integers to string via variadic
           argument). (Nikita)
    
       - Calendar:
         . Fixed bug #80007 (Potential type confusion in unixtojd() parameter parsing).
           (Andy Postnikov)
    
       - COM:
         . Fixed bug #64130 (COM obj parameters passed by reference are not updated).
           (cmb)
    
       - OPcache:
         . Fixed bug #80002 (calc free space for new interned string is wrong).
           (t-matsuno)
         . Fixed bug #80046 (FREE for SWITCH_STRING optimized away). (Nikita)
         . Fixed bug #79825 (opcache.file_cache causes SIGSEGV when custom opcode
           handlers changed). (SammyK)
    
       - OpenSSL:
         . Fixed bug #79601 (Wrong ciphertext/tag in AES-CCM encryption for a 12
           bytes IV). (CVE-2020-7069) (Jakub Zelenka)
    
       - PDO:
         . Fixed bug #80027 (Terrible performance using $query->fetch on queries with
           many bind parameters (Matteo)
    
       - Standard:
         . Fixed bug #79986 (str_ireplace bug with diacritics characters). (cmb)
         . Fixed bug #80077 (getmxrr test bug). (Rainer Jung)
         . Fixed bug #72941 (Modifying bucket->data by-ref has no effect any longer).
           (cmb)
         . Fixed bug #80067 (Omitting the port in bindto setting errors). (cmb)
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.309 -r1.310 pkgsrc/lang/php/phpversion.mk
       cvs rdiff -u -r1.12 -r1.13 pkgsrc/lang/php74/distinfo

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.308.2.1 2020/10/21 19:13:41 spz Exp $
+# $NetBSD: phpversion.mk,v 1.308.2.2 2020/10/21 19:23:29 spz Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.33
 PHP73_VERSION=	7.3.23
-PHP74_VERSION=	7.4.10
+PHP74_VERSION=	7.4.11
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
