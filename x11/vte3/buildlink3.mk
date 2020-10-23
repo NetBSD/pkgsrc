dde5b1db083341daac6aeb722e7d239bc0bbb544
bsiegert <bsiegert@pkgsrc.org>
2020-10-23 17:34:42 +0000

    Pullup ticket #6351 - requested by nia x11/vte3: NetBSD 8 build fix
    Revisions pulled up:
    - x11/vte3/buildlink3.mk                                        1.18
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Sun Oct 18 09:47:41 UTC 2020
    
       Modified Files:
       	pkgsrc/x11/vte3: buildlink3.mk
    
       Log Message:
       vte3: Force gcc7 for dependent packages

--- x11/vte3/buildlink3.mk
+++ x11/vte3/buildlink3.mk
@@ -1,9 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2020/08/17 20:18:01 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.17.2.1 2020/10/23 17:34:42 bsiegert Exp $
 
 BUILDLINK_TREE+=	vte3
 
 .if !defined(VTE3_BUILDLINK3_MK)
 VTE3_BUILDLINK3_MK:=
+
+# for c++17
+GCC_REQD+=		7
 
 BUILDLINK_API_DEPENDS.vte3+=	vte3>=0.52.2
 BUILDLINK_ABI_DEPENDS.vte3+=	vte3>=0.60.3nb2
