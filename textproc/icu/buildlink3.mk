cf24273a4000b4df3eddbb630b4a1640b3fc7579
nia <nia@pkgsrc.org>
2020-10-25 04:36:53 +0000

    icu: Build dependencies with an equivalent gcc version
    Should avoid linker errors such as
    libicuuc.so: undefined reference to `__cxa_throw_bad_array_new_length@CXXABI_1.3.8'

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2020/06/02 08:34:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2020/10/25 04:36:53 nia Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -8,6 +8,8 @@ ICU_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
 BUILDLINK_ABI_DEPENDS.icu+=	icu>=67.1
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
+
+GCC_REQD+=		4.9
 .endif # ICU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-icu
