6d217d6321b1d73a82059f1f24d7f3e2dd79cca4
nia <nia@pkgsrc.org>
2020-10-25 12:43:45 +0000

    libvpx: Set compiler requirements properly.
    Needs at least gcc 4.8, does not need c++11 in USE_LANGUAGES because it
    sets -std=c++11 itself.

--- multimedia/libvpx/buildlink3.mk
+++ multimedia/libvpx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/01/16 23:45:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/10/25 12:43:45 nia Exp $
 
 BUILDLINK_TREE+=	libvpx
 
@@ -8,6 +8,8 @@ LIBVPX_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.6.0
 BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.6.1
 BUILDLINK_PKGSRCDIR.libvpx?=	../../multimedia/libvpx
+
+GCC_REQD+=	4.8
 
 .include "../../mk/pthread.buildlink3.mk"
 .endif # LIBVPX_BUILDLINK3_MK
