1ff0bbb0671219a5d529d519032b5b7f417019d4
nia <nia@pkgsrc.org>
2020-10-25 12:51:24 +0000

    protobuf: Needs a c++11 capable compiler

--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/05/21 16:30:33 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/10/25 12:51:24 nia Exp $
 
 BUILDLINK_TREE+=	protobuf
 
@@ -8,6 +8,8 @@ PROTOBUF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
 BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=3.12.0
 BUILDLINK_PKGSRCDIR.protobuf?=		../../devel/protobuf
+
+GCC_REQD+=	4.8
 
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# PROTOBUF_BUILDLINK3_MK
