9ca1a8de0972105d077aded7dbed586f04a5a588
nia <nia@pkgsrc.org>
2020-10-25 12:59:26 +0000

    googletest: Require a c++11 compiler with GCC_REQD.
    Do not set USE_LANGUAGES, this passes -std=c++11 by itself.

--- devel/googletest/buildlink3.mk
+++ devel/googletest/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/04/07 21:36:58 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/10/25 12:59:26 nia Exp $
 
 BUILDLINK_TREE+=	googletest
 
@@ -8,6 +8,8 @@ GOOGLETEST_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.googletest+=	googletest>=1.4.0
 BUILDLINK_DEPMETHOD.googletest?=	build
 BUILDLINK_PKGSRCDIR.googletest?=	../../devel/googletest
+
+GCC_REQD+=	4.8
 .endif	# GOOGLETEST_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-googletest
