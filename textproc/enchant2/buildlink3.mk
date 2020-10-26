f68fd4b254c28baa97ca9ec12e6fd1193baef7bc
nia <nia@pkgsrc.org>
2020-10-26 17:19:29 +0000

    enchant2: require c++11 support with GCC_REQD instead of USE_LANGUAGES.

--- textproc/enchant2/buildlink3.mk
+++ textproc/enchant2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/03/08 16:48:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/10/26 17:19:29 nia Exp $
 
 BUILDLINK_TREE+=	enchant2
 
@@ -8,6 +8,8 @@ ENCHANT2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.enchant2+=	enchant2>=2
 BUILDLINK_ABI_DEPENDS.enchant2+=	enchant2>=2.2.8nb1
 BUILDLINK_PKGSRCDIR.enchant2?=		../../textproc/enchant2
+
+GCC_REQD+=	4.8
 
 # Lots of older software looks for enchant.pc instead of enchant-2.pc.
 ${BUILDLINK_DIR}/lib/pkgconfig/enchant.pc:
