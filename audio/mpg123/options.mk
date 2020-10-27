e45630fe6cc950a306e14b020dc104d2232ee3df
nia <nia@pkgsrc.org>
2020-10-27 08:58:13 +0000

    mpg123: Actually, the 'x86_dither' option is missing from ./configure
    So we can probably just remove this option.

--- audio/mpg123/options.mk
+++ audio/mpg123/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2020/10/27 08:53:57 nia Exp $
+# $NetBSD: options.mk,v 1.8 2020/10/27 08:58:13 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mpg123
 PKG_OPTIONS_OPTIONAL_GROUPS=	fpu
@@ -7,12 +7,6 @@ PKG_SUGGESTED_OPTIONS+=		mpg123-fifo
 PKG_OPTIONS_GROUP.fpu=		mpg123-with-fpu
 
 .include "../../mk/bsd.fast.prefs.mk"
-
-# x86-dither causes text relocations and doesn't work with the Sun linker
-# Don't enable it by default.
-.if ${MACHINE_ARCH} == "i386"
-PKG_OPTIONS_GROUP.fpu+=		mpg123-x86-dither
-.endif
 
 .if ${MACHINE_ARCH} == "aarch64"
 PKG_OPTIONS_GROUP.fpu+=		mpg123-neon64 mpg123-aarch64
@@ -31,10 +25,6 @@ PKG_SUGGESTED_OPTIONS+=		mpg123-with-fpu
 
 .if !empty(PKG_OPTIONS:Mmpg123-with-fpu)
 CONFIGURE_ARGS+=	--with-cpu=generic_fpu
-.elif !empty(PKG_OPTIONS:Mmpg123-altivec)
-CONFIGURE_ARGS+=	--with-cpu=altivec
-.elif !empty(PKG_OPTIONS:Mmpg123-x86-dither)
-CONFIGURE_ARGS+=	--with-cpu=x86_dither
 .elif !empty(PKG_OPTIONS:Mmpg123-altivec)
 CONFIGURE_ARGS+=	--with-cpu=altivec
 .elif !empty(PKG_OPTIONS:Mmpg123-neon64)
