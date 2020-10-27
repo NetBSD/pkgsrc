c92afb1843c9e67d8106220e385b127bc66a5abf
adam <adam@pkgsrc.org>
2020-10-27 23:07:42 +0000

    py-sanic: updated to 20.9.1
    Version 20.9.1
    
    Bugfixes
    Fix static route registration on blueprints
    Removes duplicate headers in ASGI streaming body

--- www/py-sanic/patches/patch-setup.py
+++ www/py-sanic/patches/patch-setup.py
@@ -1,14 +1,16 @@
-$NetBSD: patch-setup.py,v 1.7 2020/10/01 06:53:12 adam Exp $
+$NetBSD: patch-setup.py,v 1.8 2020/10/27 23:07:42 adam Exp $
 
 Allow newer versions.
 
---- setup.py.orig	2020-09-30 14:40:18.000000000 +0000
+--- setup.py.orig	2020-10-25 13:09:05.000000000 +0000
 +++ setup.py
-@@ -81,7 +81,7 @@ requirements = [
+@@ -80,8 +80,8 @@ requirements = [
+     ujson,
      "aiofiles>=0.3.0",
      "websockets>=8.1,<9.0",
-     "multidict>=4.0,<5.0",
+-    "multidict==5.0.0",
 -    "httpx==0.15.4",
++    "multidict>=5.0.0",
 +    "httpx>=0.15.4",
  ]
  
