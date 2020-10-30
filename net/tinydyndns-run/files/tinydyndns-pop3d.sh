d4a1d53ae138484345d7d6d2ecf435afb01d31f9
schmonz <schmonz@pkgsrc.org>
2020-10-30 10:23:42 +0000

    Instead of a fiddly shell script, use nopop3d as the fake POP3 server. Bump version.

--- net/tinydyndns-run/files/tinydyndns-pop3d.sh
+++ net/tinydyndns-run/files/tinydyndns-pop3d.sh
@@ -1,22 +1,7 @@
 #!@SH@
 #
-# $NetBSD: tinydyndns-pop3d.sh,v 1.2 2020/10/19 07:15:55 schmonz Exp $
+# $NetBSD: tinydyndns-pop3d.sh,v 1.3 2020/10/30 10:23:42 schmonz Exp $
 #
 # @PKGNAME@ fake POP3 server.
 
-main() {
-	echo '+OK '
-	read input
-	if [ "$input" = 'LIST' ]; then
-		echo '+OK'
-		echo '.'
-	elif [ "$input" = 'STAT' ]; then
-		echo '+OK 0 0'
-	fi
-	read input
-	echo '+OK'
-	return 0
-}
-
-main "$@"
-exit $?
+exec @PREFIX@/bin/nopop3d "$@"
