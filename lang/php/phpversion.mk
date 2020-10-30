d22f0e391ae956a25e82cf5b7a882dfb63e9dd2a
taca <taca@pkgsrc.org>
2020-10-30 07:12:37 +0000

    lang/php73: update to 7.3.24
    PHP                                                                        NEWS
    |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
    29 Oct 2020, PHP 7.3.24
    
    - Core:
      . Fixed bug #79423 (copy command is limited to size of file it can copy).
        (cmb)
    
    - Calendar:
      . Fixed bug #80185 (jdtounix() fails after 2037). (cmb)
    
    - IMAP:
      . Fixed bug #80213 (imap_mail_compose() segfaults on certain $bodies). (cmb)
      . Fixed bug #80215 (imap_mail_compose() may modify by-val parameters). (cmb)
      . Fixed bug #80220 (imap_mail_compose() may leak memory). (cmb)
      . Fixed bug #80223 (imap_mail_compose() leaks envelope on malformed bodies).
        (cmb)
      . Fixed bug #80216 (imap_mail_compose() does not validate types/encodings).
        (cmb)
      . Fixed bug #80226 (imap_sort() leaks sortpgm memory). (cmb)
    
    - MySQLnd:
      . Fixed bug #80115 (mysqlnd.debug doesn't recognize absolute paths with
        slashes). (cmb)
      . Fixed bug #80107 (mysqli_query() fails for ~16 MB long query when
        compression is enabled). (Nikita)
    
    - ODBC:
      . Fixed bug #78470 (odbc_specialcolumns() no longer accepts $nullable). (cmb)
      . Fixed bug #80147 (BINARY strings may not be properly zero-terminated).
        (cmb)
      . Fixed bug #80150 (Failure to fetch error message). (cmb)
      . Fixed bug #80152 (odbc_execute() moves internal pointer of $params). (cmb)
      . Fixed bug #46050 (odbc_next_result corrupts prepared resource). (cmb)
    
    - OPcache:
      . Fixed bug #80083 (Optimizer pass 6 removes variables used for ibm_db2 data
        binding). (Nikita)
    
    - PDO_ODBC:
      . Fixed bug #67465 (NULL Pointer dereference in odbc_handle_preparer). (cmb)
    
    - Standard:
      . Fixed bug #80114 (parse_url does not accept URLs with port 0). (cmb, twosee)
      . Fixed bug #76943 (Inconsistent stream_wrapper_restore() errors). (cmb)
      . Fixed bug #76735 (Incorrect message in fopen on invalid mode). (cmb)
    
    - Tidy:
      . Fixed bug #77040 (tidyNode::isHtml() is completely broken). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.311 2020/10/04 03:14:53 taca Exp $
+# $NetBSD: phpversion.mk,v 1.312 2020/10/30 07:12:37 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.34
-PHP73_VERSION=	7.3.23
+PHP73_VERSION=	7.3.24
 PHP74_VERSION=	7.4.11
 
 # Define initial release of major version.
