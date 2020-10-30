8e79e5d4bed39c9d93220b2910b5abbfa8050bc0
taca <taca@pkgsrc.org>
2020-10-30 07:14:16 +0000

    lang/php74: update to 7.4.12
    PHP                                                                        NEWS
    |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
    29 Oct 2020, PHP 7.4.12
    
    - Core:
      . Fixed bug #80061 (Copying large files may have suboptimal performance).
        (cmb)
      . Fixed bug #79423 (copy command is limited to size of file it can copy).
        (cmb)
      . Fixed bug #80126 (Covariant return types failing compilation). (Nikita)
      . Fixed bug #80186 (Segfault when iterating over FFI object). (Nikita)
    
    - Calendar:
      . Fixed bug #80185 (jdtounix() fails after 2037). (cmb)
    
    - IMAP:
      . Fixed bug #80213 (imap_mail_compose() segfaults on certain $bodies). (cmb)
      . Fixed bug #80215 (imap_mail_compose() may modify by-val parameters). (cmb)
      . Fixed bug #80220 (imap_mail_compose() may leak memory). (cmb)
      . Fixed bug #80223 (imap_mail_compose() leaks envelope on malformed bodies).
        (cmb)
      . Fixed bug #80216 (imap_mail_compose() does not validate types/encodings).
        (cmb)
      . Fixed bug #80226 (imap_sort() leaks sortpgm memory). (cmb)
    
    - MySQLnd:
      . Fixed bug #80115 (mysqlnd.debug doesn't recognize absolute paths with
        slashes). (cmb)
      . Fixed bug #80107 (mysqli_query() fails for ~16 MB long query when
        compression is enabled). (Nikita)
    
    - ODBC:
      . Fixed bug #78470 (odbc_specialcolumns() no longer accepts $nullable). (cmb)
      . Fixed bug #80147 (BINARY strings may not be properly zero-terminated).
        (cmb)
      . Fixed bug #80150 (Failure to fetch error message). (cmb)
      . Fixed bug #80152 (odbc_execute() moves internal pointer of $params). (cmb)
      . Fixed bug #46050 (odbc_next_result corrupts prepared resource). (cmb)
    
    - OPcache:
      . Fixed bug #80083 (Optimizer pass 6 removes variables used for ibm_db2 data
        binding). (Nikita)
      . Fixed bug #80194 (Assertion failure during block assembly of unreachable
        free with leading nop). (Nikita)
    
    - PCRE:
      . Updated to PCRE 10.35. (cmb)
      . Fixed bug #80118 (Erroneous whitespace match with JIT only). (cmb)
    
    - PDO_ODBC:
      . Fixed bug #67465 (NULL Pointer dereference in odbc_handle_preparer). (cmb)
    
    - Standard:
      . Fixed bug #80114 (parse_url does not accept URLs with port 0). (cmb, twosee)
      . Fixed bug #76943 (Inconsistent stream_wrapper_restore() errors). (cmb)
      . Fixed bug #76735 (Incorrect message in fopen on invalid mode). (cmb)
    
    - Tidy:
      . Fixed bug #77040 (tidyNode::isHtml() is completely broken). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.312 2020/10/30 07:12:37 taca Exp $
+# $NetBSD: phpversion.mk,v 1.313 2020/10/30 07:14:16 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.34
 PHP73_VERSION=	7.3.24
-PHP74_VERSION=	7.4.11
+PHP74_VERSION=	7.4.12
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
