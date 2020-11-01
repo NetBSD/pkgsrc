c34d0581e6ac14039afc2d6881d64cbee25ec075
spz <spz@pkgsrc.org>
2020-11-01 14:21:43 +0000

    building the gld package against pgsql broke, this commit fixes it. pkgrev bumped for the changes to files/gld.sh

--- mail/gld/options.mk
+++ mail/gld/options.mk
@@ -1,14 +1,19 @@
-# $NetBSD: options.mk,v 1.7 2007/01/02 11:47:40 ghen Exp $
+# $NetBSD: options.mk,v 1.8 2020/11/01 14:21:43 spz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gld
 PKG_OPTIONS_REQUIRED_GROUPS=	database
 PKG_OPTIONS_GROUP.database=	mysql pgsql
 PKG_SUGGESTED_OPTIONS=		mysql
 
+BUILD_DEFS+=			GLDDB
+FILES_SUBST+= 			GLDDB=${GLDDB}
+
+
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mmysql)
 .  include "../../mk/mysql.buildlink3.mk"
+GLDDB=mysqld
 CONFIGURE_ARGS+=	--with-mysql=${BUILDLINK_PREFIX.mysql-client}
 LIBS+=			-L${BUILDLINK_PREFIX.mysql-client}/lib \
 	${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/lib -lmysqlclient
@@ -17,12 +22,8 @@ MESSAGE_SRC+=		MESSAGE.mysql
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .  include "../../mk/pgsql.buildlink3.mk"
+GLDDB=pgsql
 CONFIGURE_ARGS+=	--with-pgsql=${BUILDLINK_PREFIX.postgresql-libs}
 LIBS+=			-L${BUILDLINK_PREFIX.mysql-client}/lib \
 	${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/lib -lpq
-# Replace rc.d dependency by pgsql
-SUBST_CLASSES+=		pgsql
-SUBST_STAGE.pgsql=	pre-install
-SUBST_FILES.pgsql=	../gld
-SUBST_SED.pgsql=	-e "s,mysqld,pgsql,"
 .endif
