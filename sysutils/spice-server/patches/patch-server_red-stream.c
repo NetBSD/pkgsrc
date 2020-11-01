83beb1957a7748d57a54f9fefabb32ad67604f21
maya <maya@pkgsrc.org>
2020-11-01 19:36:37 +0000

    spice-server: update to 0.14.3
    Major Changes in 0.14.3:
    ========================
    
    Main changes are WebSocket and support for Windows.
    
    * Add support for WebSocket, this will allow to use spice-html5 without proxy
    * Support Windows, now Qemu Windows can be build enabling Spice
    * Fix some alignment problem
    * Converted some documentation to Asciidoc format to make easier to update,
      updated some
    * Minor compatibility fix for PPC64EL and ARMHF
    * Minor fixes for big endian machines like MIPS
    * Avoid some crashes with some buggy guest drivers, simply ignore the invalid
      request
    * Fix for old OpenSSL versions
    * Minor fix for Windows clients and brushes, fixed an issue with Photoshop
      under Windows 7
    * Add ability to query video-codecs
    * Small use-after-free fix
    * Fix for debugging recording/replaying using QUIC images
    * Fix a regression where spice reported no monitors to the client
    * Fix DoS in spicevmc if WebDAV used
    * Updated and improved test migration script
    * Some minor fixes to smartcard support
    * Avoid possible disconnection using proxies using a in-flow keepalive
      mechanism

--- sysutils/spice-server/patches/patch-server_red-stream.c
+++ sysutils/spice-server/patches/patch-server_red-stream.c
@@ -1,28 +1,15 @@
-$NetBSD: patch-server_red-stream.c,v 1.1 2020/02/06 21:57:42 kamil Exp $
+$NetBSD: patch-server_red-stream.c,v 1.2 2020/11/01 19:36:37 maya Exp $
 
-Add NetBSD support.
+No TCP_CORK on netbsd. Disable (like win32)
 
---- server/red-stream.c.orig	2019-04-30 08:51:11.000000000 +0000
+--- server/red-stream.c.orig	2020-02-27 11:26:12.000000000 +0000
 +++ server/red-stream.c
-@@ -27,6 +27,9 @@
- #include <sys/socket.h>
- #include <netinet/tcp.h>
- #endif
-+#ifdef __NetBSD__
-+#include <netinet/in.h>
-+#endif
+@@ -105,7 +105,7 @@ struct RedStreamPrivate {
+     SpiceCoreInterfaceInternal *core;
+ };
  
- #include <glib.h>
- 
-@@ -109,7 +112,11 @@ struct RedStreamPrivate {
- static int socket_set_cork(int socket, int enabled)
- {
-     SPICE_VERIFY(sizeof(enabled) == sizeof(int));
-+#if defined(__NetBSD__)
-+    return 1; /* Not supported */
-+#else
-     return setsockopt(socket, IPPROTO_TCP, TCP_CORK, &enabled, sizeof(enabled));
-+#endif
- }
- 
- static ssize_t stream_write_cb(RedStream *s, const void *buf, size_t size)
+-#ifndef _WIN32
++#if !defined(_WIN32) && !defined(__NetBSD__)
+ /**
+  * Set TCP_CORK on socket
+  */
