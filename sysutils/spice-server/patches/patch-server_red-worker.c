a1c1acc80a88c428110e48359c40208c0d9f5c9b
maya <maya@pkgsrc.org>
2020-11-01 19:36:37 +0000

    spice-server: update to 0.14.3
    Major Changes in 0.14.3:
    ========================
    
    Main changes are WebSocket and support for Windows.
    
    * Add support for WebSocket, this will allow to use spice-html5 without proxy
    * Support Windows, now Qemu Windows can be build enabling Spice
    * Fix some alignment problem
    * Converted some documentation to Asciidoc format to make easier to update,
      updated some
    * Minor compatibility fix for PPC64EL and ARMHF
    * Minor fixes for big endian machines like MIPS
    * Avoid some crashes with some buggy guest drivers, simply ignore the invalid
      request
    * Fix for old OpenSSL versions
    * Minor fix for Windows clients and brushes, fixed an issue with Photoshop
      under Windows 7
    * Add ability to query video-codecs
    * Small use-after-free fix
    * Fix for debugging recording/replaying using QUIC images
    * Fix a regression where spice reported no monitors to the client
    * Fix DoS in spicevmc if WebDAV used
    * Updated and improved test migration script
    * Some minor fixes to smartcard support
    * Avoid possible disconnection using proxies using a in-flow keepalive
      mechanism

--- sysutils/spice-server/patches/patch-server_red-worker.c
+++ sysutils/spice-server/patches/patch-server_red-worker.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-server_red-worker.c,v 1.1 2020/02/06 21:57:42 kamil Exp $
+$NetBSD: patch-server_red-worker.c,v 1.2 2020/11/01 19:36:37 maya Exp $
 
 Add NetBSD support.
 
 --- server/red-worker.c.orig	2019-04-30 08:51:11.000000000 +0000
 +++ server/red-worker.c
-@@ -1161,7 +1161,11 @@ bool red_worker_run(RedWorker *worker)
-         spice_error("create thread failed %d", r);
-     }
+@@ -1159,7 +1159,11 @@ bool red_worker_run(RedWorker *worker)
+ #ifndef _WIN32
      pthread_sigmask(SIG_SETMASK, &curr_sig_mask, NULL);
+ #endif
 +#if defined(__NetBSD__)
 +    pthread_setname_np(worker->thread, "%s", (void *)"SPICE Worker");
 +#else
