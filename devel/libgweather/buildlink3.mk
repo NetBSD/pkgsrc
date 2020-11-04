4271405795326a622810394b18dabf33bc0dde1e
nia <nia@pkgsrc.org>
2020-11-04 16:08:51 +0000

    libgweather: update to 3.36.1
    updated to GNOME 3 version.

--- devel/libgweather/buildlink3.mk
+++ devel/libgweather/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.62 2020/08/17 20:17:23 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.63 2020/11/04 16:08:51 nia Exp $
 
 BUILDLINK_TREE+=	libgweather
 
@@ -9,8 +9,9 @@ BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=2.22.0
 BUILDLINK_ABI_DEPENDS.libgweather+=	libgweather>=2.30.3nb54
 BUILDLINK_PKGSRCDIR.libgweather?=	../../devel/libgweather
 
+.include "../../geography/geocode-glib/buildlink3.mk"
 .include "../../net/libsoup/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif # LIBGWEATHER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgweather
