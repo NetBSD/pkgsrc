98c243b944b265c3de6911124de69a294b4032e5
triaxx <triaxx@pkgsrc.org>
2020-11-04 08:36:52 +0000

    gnustep-objc: fix build on FreeBSD
    pkgsrc changes:
    ---------------
      * Add conditional installation of libobjcxx.so in GNUmakefile for FreeBSD
        and adjust PLIST accordingly.
      * Fix some typos that have been kindly suggested by pkglint.

--- devel/gnustep-objc/buildlink3.mk
+++ devel/gnustep-objc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2020/04/16 17:32:09 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2020/11/04 08:36:52 triaxx Exp $
 
 BUILDLINK_TREE+=	gnustep-objc
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.gnustep-objc+=	gnustep-objc>=1.3.0
 BUILDLINK_ABI_DEPENDS.gnustep-objc+=	gnustep-objc>=1.3.0nb1
 BUILDLINK_PKGSRCDIR.gnustep-objc?=	../../devel/gnustep-objc
 
-BUILDLINK_CFLAGS.gnustep-objc+=	-I${BUILDLINK_PREFIX.gnustep-objc}/include/objc
+BUILDLINK_CFLAGS.gnustep-objc+=		-I${BUILDLINK_PREFIX.gnustep-objc}/include/objc
 BUILDLINK_LDFLAGS.gnustep-objc+=	-lobjc -lpthread
 
 .include "../../devel/gnustep-make/buildlink3.mk"
