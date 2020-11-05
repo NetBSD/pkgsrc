6598e88a36510b35cc3a5d00ea0567f86aa78472
wiz <wiz@pkgsrc.org>
2020-11-05 17:14:45 +0000

    gjs: fix path to itself in bl3.mk

--- lang/gjs/buildlink3.mk
+++ lang/gjs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/11/05 17:12:00 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/11/05 17:14:45 wiz Exp $
 
 BUILDLINK_TREE+=	gjs
 
@@ -7,7 +7,7 @@ GJS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gjs+=	gjs>=1.53.4
 BUILDLINK_ABI_DEPENDS.gjs?=	gjs>=1.53.4nb3
-BUILDLINK_PKGSRCDIR.gjs?=	../../wip/gjs
+BUILDLINK_PKGSRCDIR.gjs?=	../../lang/gjs
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/gobject-introspection/buildlink3.mk"
