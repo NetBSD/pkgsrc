6bf9818ac12ff0a5e3a5ac321cf99d2624fcd2f6
bacon <bacon@pkgsrc.org>
2020-11-05 17:06:10 +0000

    openblas_openmp, openblas_pthread: Remove wip references

--- math/openblas_openmp/buildlink3.mk
+++ math/openblas_openmp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/11/05 16:53:16 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/11/05 17:06:10 bacon Exp $
 
 BUILDLINK_TREE+=	openblas_openmp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openblas_openmp
 OPENBLAS_OPENMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openblas_openmp+=	openblas_openmp>=0.3.5
-BUILDLINK_PKGSRCDIR.openblas_openmp?=	../../wip/openblas_openmp
+BUILDLINK_PKGSRCDIR.openblas_openmp?=	../../math/openblas_openmp
 .endif	# OPENBLAS_OPENMP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openblas_openmp
