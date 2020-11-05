0c9408c794029d0e7125156fe70af0977b41ac78
nia <nia@pkgsrc.org>
2020-11-05 09:16:00 +0000

    sites: add some working GNOME mirrors

--- mk/fetch/sites.mk
+++ mk/fetch/sites.mk
@@ -1,4 +1,4 @@
-# $NetBSD: sites.mk,v 1.167 2020/11/05 09:07:24 nia Exp $
+# $NetBSD: sites.mk,v 1.168 2020/11/05 09:16:00 nia Exp $
 #
 # This Makefile fragment defines read-only MASTER_SITE_* variables
 # representing some well-known master distribution sites for software.
@@ -73,7 +73,10 @@ MASTER_SITE_SUNSITE+=   \
 	ftp://ftp.lip6.fr/pub/linux/sunsite/
 
 MASTER_SITE_GNOME+=     \
-	https://download.gnome.org/
+	https://download.gnome.org/ \
+	https://www.mirrorservice.org/sites/ftp.gnome.org/pub/GNOME/ \
+	https://mirror.umd.edu/gnome/ \
+	https://mirrors.dotsrc.org/gnome/
 
 MASTER_SITE_KDE+=	\
 	http://download.kde.org/stable/ \
