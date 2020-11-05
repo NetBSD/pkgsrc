83a23d3a0b7e0b2cb1113f7e0a919329feafde86
bacon <bacon@pkgsrc.org>
2020-11-05 16:31:45 +0000

    math/openblas: import openblas-0.3.7
    OpenBLAS is an optimized BLAS library based on GotoBLAS2 1.13 BSD version.
    OpenBLAS is an open source project supported by
    Lab of Parallel Software and Computational Science, ISCAS.
    
    This package builds the serial library.
    
    Needs patching and testing on platforms besides Linux

---
+++ math/openblas/patches/patch-cpuid_ia64.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-cpuid_ia64.c,v 1.1 2020/11/05 16:31:45 bacon Exp $
+
+FreeBSD does not have sysinfo in base
+
+--- cpuid_ia64.c.orig	2015-10-27 20:44:50 UTC
++++ cpuid_ia64.c
+@@ -38,7 +38,9 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
++#ifdef linux
+ #include <sys/sysinfo.h>
++#endif
+ #include "cpuid.h"
+ 
+ #ifdef __ECC
---
+++ math/openblas/version.mk
@@ -0,0 +1,3 @@
+# $NetBSD: version.mk,v 1.1 2020/11/05 16:31:45 bacon Exp $
+
+OPENBLAS_VERSION=	0.3.7
