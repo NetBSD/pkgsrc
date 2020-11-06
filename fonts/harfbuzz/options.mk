185faaf701253e57691f5710d7275e95e6c85949
nia <nia@pkgsrc.org>
2020-11-06 15:06:49 +0000

    Split out ICU integration library from the harfbuzz package.
    Only a few pieces of software are using the ICU integration library,
    these aren't the big harfbuzz users (i.e. gtk). this is intended to lessen
    the impact of icu's frequent ABI breaks.

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.9 2020/09/04 10:50:08 prlw1 Exp $
+# $NetBSD: options.mk,v 1.10 2020/11/06 15:06:49 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
-PKG_SUPPORTED_OPTIONS+=	doc icu introspection
-PKG_SUGGESTED_OPTIONS=	doc icu introspection
+PKG_SUPPORTED_OPTIONS+=	doc introspection
+PKG_SUGGESTED_OPTIONS=	doc introspection
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		doc icu introspection
+PLIST_VARS+=		doc introspection
 
 .if !empty(PKG_OPTIONS:Mdoc)
 MESON_ARGS+=	-Ddocs=enabled
@@ -14,14 +14,6 @@ MESON_ARGS+=	-Ddocs=enabled
 PLIST.doc=	yes
 .else
 MESON_ARGS+=	-Ddocs=disabled
-.endif
-
-.if !empty(PKG_OPTIONS:Micu)
-.include "../../textproc/icu/buildlink3.mk"
-MESON_ARGS+=	-Dicu=enabled
-PLIST.icu=		yes
-.else
-MESON_ARGS+=	-Dicu=disabled
 .endif
 
 .if !empty(PKG_OPTIONS:Mintrospection)
