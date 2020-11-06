8034b2be769b6589f8274318b3d31efe1e41953a
gutteridge <gutteridge@pkgsrc.org>
2020-11-06 04:05:57 +0000

    icu: reflect shlib major bump
    Ride PKGREVISION bump already made earlier.

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2020/10/25 04:36:53 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2020/11/06 04:05:57 gutteridge Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	icu
 ICU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
-BUILDLINK_ABI_DEPENDS.icu+=	icu>=67.1
+BUILDLINK_ABI_DEPENDS.icu+=	icu>=68.1
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
 
 GCC_REQD+=		4.9
