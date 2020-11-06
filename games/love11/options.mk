8758cf71c94c8fecb2c0e4e06f8186b5153cae39
nia <nia@pkgsrc.org>
2020-11-06 12:15:53 +0000

    Make luajit PaX MPROTECT safe.

--- games/love11/options.mk
+++ games/love11/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2020/06/29 13:04:14 nia Exp $
+# $NetBSD: options.mk,v 1.2 2020/11/06 12:15:54 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.love
 PKG_SUPPORTED_OPTIONS=	luajit
@@ -11,8 +11,6 @@ PKG_SUGGESTED_OPTIONS=	luajit
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mluajit)
-# JIT needs WX mappings
-NOT_PAX_MPROTECT_SAFE+=		love11/bin/love
 .  include "../../lang/LuaJIT2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=		--with-lua=lua
