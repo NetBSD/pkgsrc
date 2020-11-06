17de3ba31deeba7ee4620f755c310a788d26a310
ryoon <ryoon@pkgsrc.org>
2020-11-06 17:00:06 +0000

    harfbuzz-icu: Pick up fonts/harfbuzz-icu directory

--- fonts/harfbuzz-icu/buildlink3.mk
+++ fonts/harfbuzz-icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/11/06 15:06:49 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/11/06 17:00:06 ryoon Exp $
 
 BUILDLINK_TREE+=	harfbuzz-icu
 
@@ -7,6 +7,7 @@ HARFBUZZ_ICU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.harfbuzz-icu+=	harfbuzz-icu>=2.1.1
 BUILDLINK_ABI_DEPENDS.harfbuzz-icu+=	harfbuzz-icu>=2.6.4nb2
+BUILDLINK_PKGSRCDIR.harfbuzz-icu?=	../../fonts/harfbuzz-icu
 
 .include "../../fonts/harfbuzz/buildlink3.mk"
 .endif	# HARFBUZZ_ICU_BUILDLINK3_MK
