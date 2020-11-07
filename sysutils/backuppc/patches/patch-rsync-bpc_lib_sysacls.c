f96e3b05abc886c72f6bf81fe674ddd33d3dd894
tnn <tnn@pkgsrc.org>
2020-11-07 14:53:30 +0000

    backuppc: downgrade the rsync-bpc component to the previous version. Bump.
    rsync-bpc-3.1.3.0 seems to use a lot more memory than 3.1.2.2 for some
    reason and it is making my backup host run out of swap.

--- sysutils/backuppc/patches/patch-rsync-bpc_lib_sysacls.c
+++ sysutils/backuppc/patches/patch-rsync-bpc_lib_sysacls.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-rsync-bpc_lib_sysacls.c,v 1.2 2020/10/20 22:16:16 tnn Exp $
+$NetBSD: patch-rsync-bpc_lib_sysacls.c,v 1.3 2020/11/07 14:53:31 tnn Exp $
 
 No ACL support on NetBSD and probably other platforms
 
---- ../rsync-bpc-3.1.3.0/lib/sysacls.c.orig	2018-12-01 22:25:23.000000000 +0000
-+++ ../rsync-bpc-3.1.3.0/lib/sysacls.c
+--- ../rsync-bpc-3.1.2.2/lib/sysacls.c.orig	2018-12-01 22:25:23.000000000 +0000
++++ ../rsync-bpc-3.1.2.2/lib/sysacls.c
 @@ -24,6 +24,8 @@
  #include "rsync.h"
  #include "sysacls.h"
