6f372f3a06634fb1862fe20d94ef9a45efced85a
otis <otis@pkgsrc.org>
2020-11-08 23:55:43 +0000

    php74: Document patches for ext/intl

--- lang/php74/patches/patch-ext_intl_dateformat_dateformat__attr.c
+++ lang/php74/patches/patch-ext_intl_dateformat_dateformat__attr.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-ext_intl_dateformat_dateformat__attr.c,v 1.1 2020/11/08 23:51:18 otis Exp $
+$NetBSD: patch-ext_intl_dateformat_dateformat__attr.c,v 1.2 2020/11/08 23:55:44 otis Exp $
+
+Use stdbool true/false macros.
 
 --- ext/intl/dateformat/dateformat_attr.c.orig	2020-10-27 15:01:59.000000000 +0000
 +++ ext/intl/dateformat/dateformat_attr.c
