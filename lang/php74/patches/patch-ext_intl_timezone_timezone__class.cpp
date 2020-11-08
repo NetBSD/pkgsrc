a492d46a295cf90f853ce543c3ec151eef97e029
otis <otis@pkgsrc.org>
2020-11-08 23:51:18 +0000

    php74: Fix build with ICU 68

---
+++ lang/php74/patches/patch-ext_intl_timezone_timezone__class.cpp
@@ -0,0 +1,13 @@
+$NetBSD: patch-ext_intl_timezone_timezone__class.cpp,v 1.1 2020/11/08 23:51:18 otis Exp $
+
+--- ext/intl/timezone/timezone_class.cpp.orig	2020-10-27 15:01:59.000000000 +0000
++++ ext/intl/timezone/timezone_class.cpp
+@@ -325,7 +325,7 @@ static HashTable *TimeZone_get_debug_inf
+ 
+ 	int32_t rawOffset, dstOffset;
+ 	UDate now = Calendar::getNow();
+-	tz->getOffset(now, FALSE, rawOffset, dstOffset, uec);
++	tz->getOffset(now, false, rawOffset, dstOffset, uec);
+ 	if (U_FAILURE(uec)) {
+ 		return debug_info;
+ 	}
