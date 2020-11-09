1983323ecb046ff8e9ce370e9b747bb9c087422d
gutteridge <gutteridge@pkgsrc.org>
2020-11-09 23:48:31 +0000

    mate-terminal: remove unused PLIST_VAR
    The skey option hasn't made a difference in PLIST content in some time.

--- x11/mate-terminal/options.mk
+++ x11/mate-terminal/options.mk
@@ -1,16 +1,13 @@
-# $NetBSD: options.mk,v 1.1 2016/05/21 19:16:49 youri Exp $
+# $NetBSD: options.mk,v 1.2 2020/11/09 23:48:31 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-terminal
 PKG_SUPPORTED_OPTIONS=	skey
 PKG_SUGGESTED_OPTIONS=	skey
 
-PLIST_VARS+=	skey
-
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mskey)
 CONFIGURE_ARGS+=	--enable-skey
-PLIST.skey=		yes
 .else
 CONFIGURE_ARGS+=	--disable-skey
 .endif
