70beb7f2a6dc4b157fc898b28bc8e1c5310e1667
nia <nia@pkgsrc.org>
2020-11-10 11:31:25 +0000

    mozjs78: Fix reference to wip

--- lang/mozjs78/buildlink3.mk
+++ lang/mozjs78/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/11/10 11:27:10 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/11/10 11:31:25 nia Exp $
 
 BUILDLINK_TREE+=	mozjs78
 
@@ -7,7 +7,7 @@ MOZJS78_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mozjs78+=	mozjs78>=78.3.0
 BUILDLINK_ABI_DEPENDS.mozjs78?=	mozjs78>=78.3.0
-BUILDLINK_PKGSRCDIR.mozjs78?=	../../wip/mozjs78
+BUILDLINK_PKGSRCDIR.mozjs78?=	../../lang/mozjs78
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
