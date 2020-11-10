d22e8fd311f9a9df68d25988696102208796bb37
nia <nia@pkgsrc.org>
2020-11-10 17:45:05 +0000

    Reimport libhandy1 at the correct location.
    This is a new version of devel/libhandy that is incompatible with
    the previous version. It belongs at devel/libhandy1.

---
+++ devel/libhandy1/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/11/10 17:45:05 nia Exp $
+
+BUILDLINK_TREE+=	libhandy1
+
+.if !defined(LIBHANDY1_BUILDLINK3_MK)
+LIBHANDY1_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libhandy1+=	libhandy1>=1.0
+BUILDLINK_ABI_DEPENDS.libhandy1?=	libhandy1>=1.0
+BUILDLINK_PKGSRCDIR.libhandy1?=		../../devel/libhandy1
+
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# LIBHANDY1_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libhandy1
