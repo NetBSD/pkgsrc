dc6a168bb05868c6825a93265cef5b42f71c436f
markd <markd@pkgsrc.org>
2020-11-11 20:05:10 +0000

    php73: Fix build with ICU 68
    Untested - but since the same patches work for 72 and 74, assume they work
    for 73.

---
+++ lang/php73/patches/patch-ext_intl_timezone_timezone__methods.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-ext_intl_timezone_timezone__methods.cpp,v 1.1 2020/11/11 20:05:10 markd Exp $
+
+Use stdbool true/false macros.
+
+--- ext/intl/timezone/timezone_methods.cpp.orig	2020-10-27 15:01:59.000000000 +0000
++++ ext/intl/timezone/timezone_methods.cpp
+@@ -95,7 +95,7 @@ U_CFUNC PHP_FUNCTION(intltz_from_date_ti
+ 		RETURN_NULL();
+ 	}
+ 
+-	tz = timezone_convert_datetimezone(tzobj->type, tzobj, FALSE, NULL,
++	tz = timezone_convert_datetimezone(tzobj->type, tzobj, false, NULL,
+ 		"intltz_from_date_time_zone");
+ 	if (tz == NULL) {
+ 		RETURN_NULL();
