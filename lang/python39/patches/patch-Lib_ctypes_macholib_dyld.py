e3441b452ec56679a079ad9223519227c12bfa7e
sjmulder <sjmulder@pkgsrc.org>
2020-11-12 10:58:20 +0000

    lang/python39: Fix for macOS 11 and Apple Silicon
    (Apple Silicon being their aarch64 platform.)
    
    Patches consist of:
     - Upstream work: https://github.com/python/cpython/pull/22855
     - Fix for setup.py to find libbz2.tbd and libz.tbd now that with the
       shared library cache there's nothing in /usr/lib.
       See: https://bugs.python.org/issue41116
     - Addition of __arch64__ case to fix _decimal module. A very similar fix
       has since been committed upstream.

---
+++ lang/python39/patches/patch-Lib_ctypes_macholib_dyld.py
@@ -0,0 +1,35 @@
+$NetBSD: patch-Lib_ctypes_macholib_dyld.py,v 1.1 2020/11/12 10:58:21 sjmulder Exp $
+
+Support for macOS 11 and Apple Silicon (ARM). Backported from:
+https://github.com/python/cpython/pull/22855
+
+--- Lib/ctypes/macholib/dyld.py.orig	2020-10-05 15:07:58.000000000 +0000
++++ Lib/ctypes/macholib/dyld.py
+@@ -6,6 +6,11 @@ import os
+ from ctypes.macholib.framework import framework_info
+ from ctypes.macholib.dylib import dylib_info
+ from itertools import *
++try:
++    from _ctypes import _dyld_shared_cache_contains_path
++except ImportError:
++    def _dyld_shared_cache_contains_path(*args):
++        raise NotImplementedError
+ 
+ __all__ = [
+     'dyld_find', 'framework_find',
+@@ -122,8 +127,15 @@ def dyld_find(name, executable_path=None
+                 dyld_executable_path_search(name, executable_path),
+                 dyld_default_search(name, env),
+             ), env):
++
+         if os.path.isfile(path):
+             return path
++        try:
++            if _dyld_shared_cache_contains_path(path):
++                return path
++        except NotImplementedError:
++            pass
++
+     raise ValueError("dylib %s could not be found" % (name,))
+ 
+ def framework_find(fn, executable_path=None, env=None):
