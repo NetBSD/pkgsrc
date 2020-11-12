b4f8ae1b93276301a5be6ff4aee56d63c0b3da2a
markd <markd@pkgsrc.org>
2020-11-12 10:59:05 +0000

    php72: commit the actual normalize.c patch

--- lang/php72/patches/patch-ext_intl_normalizer_normalizer__normalize.c
+++ lang/php72/patches/patch-ext_intl_normalizer_normalizer__normalize.c
@@ -1,19 +1,10 @@
-$NetBSD: patch-ext_intl_normalizer_normalizer__normalize.c,v 1.1 2020/11/11 20:01:34 markd Exp $
+$NetBSD: patch-ext_intl_normalizer_normalizer__normalize.c,v 1.2 2020/11/12 10:59:05 markd Exp $
 
 Use stdbool true/false macros.
 
---- ext/intl/normalizer/normalizer_normalize.c.orig	2020-10-27 15:01:59.000000000 +0000
+--- ext/intl/normalizer/normalizer_normalize.c.orig	2020-09-30 05:15:54.000000000 +0000
 +++ ext/intl/normalizer/normalizer_normalize.c
-@@ -90,7 +90,7 @@ static UBool intl_is_normalized(zend_lon
- 	const UNormalizer2 *norm = intl_get_normalizer(form, err);
- 
- 	if(U_FAILURE(*err)) {
--		return FALSE;
-+		return false;
- 	}
- 
- 	return unorm2_isNormalized(norm, uinput, uinput_len, err);
-@@ -261,7 +261,7 @@ PHP_FUNCTION( normalizer_is_normalized )
+@@ -178,7 +178,7 @@ PHP_FUNCTION( normalizer_is_normalized )
  	int		uinput_len = 0;
  	UErrorCode	status = U_ZERO_ERROR;
  
