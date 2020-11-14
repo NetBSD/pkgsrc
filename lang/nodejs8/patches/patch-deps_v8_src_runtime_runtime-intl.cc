fb085e3b6619dc15388b5aeba9c0a5369ed596b1
jperkin <jperkin@pkgsrc.org>
2020-11-14 09:54:22 +0000

    nodejs*: Fix builds with icu-68.1.

---
+++ lang/nodejs8/patches/patch-deps_v8_src_runtime_runtime-intl.cc
@@ -0,0 +1,45 @@
+$NetBSD: patch-deps_v8_src_runtime_runtime-intl.cc,v 1.1 2020/11/14 09:54:23 jperkin Exp $
+
+icu 68.1 compat.
+
+--- deps/v8/src/runtime/runtime-intl.cc.orig	2019-12-17 06:54:05.000000000 +0000
++++ deps/v8/src/runtime/runtime-intl.cc
+@@ -89,7 +89,7 @@ RUNTIME_FUNCTION(Runtime_CanonicalizeLan
+   char result[ULOC_FULLNAME_CAPACITY];
+ 
+   // Force strict BCP47 rules.
+-  uloc_toLanguageTag(icu_result, result, ULOC_FULLNAME_CAPACITY, TRUE, &error);
++  uloc_toLanguageTag(icu_result, result, ULOC_FULLNAME_CAPACITY, true, &error);
+ 
+   if (U_FAILURE(error) || error == U_STRING_NOT_TERMINATED_WARNING) {
+     return *factory->NewStringFromAsciiChecked(kInvalidTag);
+@@ -136,7 +136,7 @@ RUNTIME_FUNCTION(Runtime_AvailableLocale
+ 
+     error = U_ZERO_ERROR;
+     // No need to force strict BCP47 rules.
+-    uloc_toLanguageTag(icu_name, result, ULOC_FULLNAME_CAPACITY, FALSE, &error);
++    uloc_toLanguageTag(icu_name, result, ULOC_FULLNAME_CAPACITY, false, &error);
+     if (U_FAILURE(error) || error == U_STRING_NOT_TERMINATED_WARNING) {
+       // This shouldn't happen, but lets not break the user.
+       continue;
+@@ -168,7 +168,7 @@ RUNTIME_FUNCTION(Runtime_GetDefaultICULo
+   char result[ULOC_FULLNAME_CAPACITY];
+   UErrorCode status = U_ZERO_ERROR;
+   uloc_toLanguageTag(default_locale.getName(), result, ULOC_FULLNAME_CAPACITY,
+-                     FALSE, &status);
++                     false, &status);
+   if (U_SUCCESS(status)) {
+     return *factory->NewStringFromAsciiChecked(result);
+   }
+@@ -550,9 +550,9 @@ RUNTIME_FUNCTION(Runtime_InternalCompare
+     std::unique_ptr<uc16[]> sap1;
+     std::unique_ptr<uc16[]> sap2;
+     icu::UnicodeString string_val1(
+-        FALSE, GetUCharBufferFromFlat(flat1, &sap1, length1), length1);
++        false, GetUCharBufferFromFlat(flat1, &sap1, length1), length1);
+     icu::UnicodeString string_val2(
+-        FALSE, GetUCharBufferFromFlat(flat2, &sap2, length2), length2);
++        false, GetUCharBufferFromFlat(flat2, &sap2, length2), length2);
+     result = collator->compare(string_val1, string_val2, status);
+   }
+   if (U_FAILURE(status)) return isolate->ThrowIllegalOperation();
