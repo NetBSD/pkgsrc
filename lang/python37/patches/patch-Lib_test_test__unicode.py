7fd51fd8b020d5963b91e9ca60b3cf94b4227644
sjmulder <sjmulder@pkgsrc.org>
2020-11-17 19:33:26 +0000

    lang/python37: Fix for macOS 11 and Apple Silicon
    (Apple Silicon being their aarch64 platform.)
    
    This is backport of the same in lang/python39 and lang/python38. Some
    parts weren't applicable in 3.7. The setup.py script needed some work
    on the ffi code. Otherwise, minor changes.
    
    Patches consist of:
     - Upstream work: https://github.com/python/cpython/pull/22855
     - Fix for setup.py to find libbz2.tbd and libz.tbd now that with the
       shared library cache there's nothing in /usr/lib.
       See: https://bugs.python.org/issue41116
     - Addition of __arch64__ case to fix _decimal module. A very similar fix
       has since been committed upstream.

---
+++ lang/python37/patches/patch-Lib_test_test__unicode.py
@@ -0,0 +1,21 @@
+$NetBSD: patch-Lib_test_test__unicode.py,v 1.1 2020/11/17 19:33:26 sjmulder Exp $
+
+Support for macOS 11 and Apple Silicon (ARM). Mostly backported from:
+https://github.com/python/cpython/pull/22855
+
+--- Lib/test/test_unicode.py.orig	2020-08-15 05:20:16.000000000 +0000
++++ Lib/test/test_unicode.py
+@@ -2458,11 +2458,13 @@ class CAPITest(unittest.TestCase):
+     def test_from_format(self):
+         support.import_module('ctypes')
+         from ctypes import (
++            c_char_p,
+             pythonapi, py_object, sizeof,
+             c_int, c_long, c_longlong, c_ssize_t,
+             c_uint, c_ulong, c_ulonglong, c_size_t, c_void_p)
+         name = "PyUnicode_FromFormat"
+         _PyUnicode_FromFormat = getattr(pythonapi, name)
++        _PyUnicode_FromFormat.argtypes = (c_char_p,)
+         _PyUnicode_FromFormat.restype = py_object
+ 
+         def PyUnicode_FromFormat(format, *args):
