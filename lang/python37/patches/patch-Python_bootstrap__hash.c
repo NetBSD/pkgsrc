2456f926444a07a3eddbb82d3209ddadafa30aac
sjmulder <sjmulder@pkgsrc.org>
2020-11-17 19:33:26 +0000

    lang/python37: Fix for macOS 11 and Apple Silicon
    (Apple Silicon being their aarch64 platform.)
    
    This is backport of the same in lang/python39 and lang/python38. Some
    parts weren't applicable in 3.7. The setup.py script needed some work
    on the ffi code. Otherwise, minor changes.
    
    Patches consist of:
     - Upstream work: https://github.com/python/cpython/pull/22855
     - Fix for setup.py to find libbz2.tbd and libz.tbd now that with the
       shared library cache there's nothing in /usr/lib.
       See: https://bugs.python.org/issue41116
     - Addition of __arch64__ case to fix _decimal module. A very similar fix
       has since been committed upstream.

---
+++ lang/python37/patches/patch-Python_bootstrap__hash.c
@@ -0,0 +1,73 @@
+$NetBSD: patch-Python_bootstrap__hash.c,v 1.1 2020/11/17 19:33:26 sjmulder Exp $
+
+Support for macOS 11 and Apple Silicon (ARM). Mostly backported from:
+https://github.com/python/cpython/pull/22855
+
+--- Python/bootstrap_hash.c.orig	2020-08-15 05:20:16.000000000 +0000
++++ Python/bootstrap_hash.c
+@@ -24,6 +24,16 @@
+ #  include <sanitizer/msan_interface.h>
+ #endif
+ 
++#if defined(__APPLE__) && defined(__has_builtin)
++#  if __has_builtin(__builtin_available)
++#    define HAVE_GETENTRYPY_GETRANDOM_RUNTIME __builtin_available(macOS 10.12, iOS 10.10, tvOS 10.0, watchOS 3.0, *)
++#  endif
++#endif
++#ifndef HAVE_GETENTRYPY_GETRANDOM_RUNTIME
++#  define HAVE_GETENTRYPY_GETRANDOM_RUNTIME 1
++#endif
++
++
+ #ifdef Py_DEBUG
+ int _Py_HashSecret_Initialized = 0;
+ #else
+@@ -209,6 +219,16 @@ py_getrandom(void *buffer, Py_ssize_t si
+      error.
+ 
+    getentropy() is retried if it failed with EINTR: interrupted by a signal. */
++
++#if defined(__APPLE__) && defined(__has_attribute) && __has_attribute(availability)
++static int
++py_getentropy(char *buffer, Py_ssize_t size, int raise)
++        __attribute__((availability(macos,introduced=10.12)))
++        __attribute__((availability(ios,introduced=10.0)))
++        __attribute__((availability(tvos,introduced=10.0)))
++        __attribute__((availability(watchos,introduced=3.0)));
++#endif
++
+ static int
+ py_getentropy(char *buffer, Py_ssize_t size, int raise)
+ {
+@@ -499,19 +519,21 @@ pyurandom(void *buffer, Py_ssize_t size,
+ #else
+ 
+ #if defined(PY_GETRANDOM) || defined(PY_GETENTROPY)
++    if (HAVE_GETENTRYPY_GETRANDOM_RUNTIME) {
+ #ifdef PY_GETRANDOM
+-    res = py_getrandom(buffer, size, blocking, raise);
++        res = py_getrandom(buffer, size, blocking, raise);
+ #else
+-    res = py_getentropy(buffer, size, raise);
++        res = py_getentropy(buffer, size, raise);
+ #endif
+-    if (res < 0) {
+-        return -1;
+-    }
+-    if (res == 1) {
+-        return 0;
+-    }
+-    /* getrandom() or getentropy() function is not available: failed with
+-       ENOSYS or EPERM. Fall back on reading from /dev/urandom. */
++        if (res < 0) {
++            return -1;
++        }
++        if (res == 1) {
++            return 0;
++        }
++        /* getrandom() or getentropy() function is not available: failed with
++           ENOSYS or EPERM. Fall back on reading from /dev/urandom. */
++    } /* end of availability block */
+ #endif
+ 
+     return dev_urandom(buffer, size, raise);
