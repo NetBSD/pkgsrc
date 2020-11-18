d539fde5013419dd018736cb89858286d6dd5ec6
schmonz <schmonz@pkgsrc.org>
2020-11-18 15:12:39 +0000

    Quell pkglint: sprinkle .PHONY and terminate list of commands with semicolon.

--- net/ucspi-ssl/options.mk
+++ net/ucspi-ssl/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2020/09/22 09:59:05 schmonz Exp $
+# $NetBSD: options.mk,v 1.11 2020/11/18 15:12:39 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ucspi-ssl
 PKG_SUPPORTED_OPTIONS+=	inet6 perl
@@ -12,11 +12,12 @@ DEPENDS+=		{ucspi-tcp6>=1.10.7nb1,ucspi-tcp-[0-9]*}:../../net/ucspi-tcp
 .endif
 
 PLIST_VARS+=		perl
+.PHONY: do-install-sslperl
 .if !empty(PKG_OPTIONS:Mperl)
 .include "../../lang/perl5/buildlink3.mk"
 USE_TOOLS+=		perl:run
 DJB_CONFIG_CMDS+=	${ECHO} ${PERL5} > conf-perl; \
-			${ECHO} it-sslperl >> src/it=d
+			${ECHO} it-sslperl >> src/it=d;
 PLIST.perl=		yes
 do-install-sslperl:
 	${INSTALL_PROGRAM} ${WRKSRC}/command/sslperl ${DESTDIR}${PREFIX}/bin
