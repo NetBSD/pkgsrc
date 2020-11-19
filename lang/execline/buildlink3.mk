3d1ffe62aa2980d6df4facc888ab6196f406b089
schmonz <schmonz@pkgsrc.org>
2020-11-19 19:23:39 +0000

    Initial import of execline, packaged in wip by cfkoch@edgebsd.org.
    execline is a (non-interactive) scripting language, like sh, but its syntax
    is quite different from a traditional shell syntax. The execlineb program
    is meant to be used as an interpreter for a text file; the other commands
    are essentially useful inside an execlineb script.

---
+++ lang/execline/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/11/19 19:23:39 schmonz Exp $
+
+BUILDLINK_TREE+=	execline
+
+.if !defined(EXECLINE_BUILDLINK3_MK)
+EXECLINE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.2.0.0
+BUILDLINK_PKGSRCDIR.execline?=		../../lang/execline
+BUILDLINK_DEPMETHOD.execline?=		build
+.endif # EXECLINE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-execline
