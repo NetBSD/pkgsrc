537f4cde159dea92796533970d07b7877a42c3de
adam <adam@pkgsrc.org>
2020-11-19 10:37:24 +0000

    py-lz4: updated to 3.1.1
    3.1.1:
    Correct how pkgconfig is handled in setup.py to account for multiple build flags
    Improve how CHAR_BIT is handled

--- archivers/py-lz4/patches/patch-setup.py
+++ archivers/py-lz4/patches/patch-setup.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-setup.py,v 1.2 2018/12/18 12:02:34 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2020/11/19 10:37:24 adam Exp $
 
 Prefer external compiler optimizations.
 
---- setup.py.orig	2018-12-18 02:40:14.000000000 +0000
+--- setup.py.orig	2020-11-19 10:31:44.000000000 +0000
 +++ setup.py
-@@ -100,7 +100,6 @@ elif compiler in ('unix', 'mingw32'):
-             extra_compile_args.append(pkgconfig_cflags('liblz4'))
+@@ -102,7 +102,6 @@ elif compiler in ('unix', 'mingw32'):
+         extension_kwargs = pkgconfig_parse('liblz4')
      else:
-         extra_compile_args = [
+         extension_kwargs['extra_compile_args'] = [
 -            '-O3',
              '-Wall',
              '-Wundef'
