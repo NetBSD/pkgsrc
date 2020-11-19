9eabedc09a15c646220b652d94f226432b5468bb
schmonz <schmonz@pkgsrc.org>
2020-11-19 09:08:06 +0000

    None of the command names appear anywhere else in a pkgsrc-wide PLIST grep. Install directly to ${PREFIX}. Bump PKGREVISION.

--- mail/qmailanalog/patches/patch-hier.c
+++ mail/qmailanalog/patches/patch-hier.c
@@ -1,17 +1,24 @@
-$NetBSD: patch-hier.c,v 1.2 2017/09/01 03:50:12 schmonz Exp $
+$NetBSD: patch-hier.c,v 1.3 2020/11/19 09:08:06 schmonz Exp $
 
+Follow hier(7).
 Don't install catpages.
 
---- hier.c.orig	2012-03-13 17:48:44.000000000 +0000
+--- hier.c.orig	1998-08-30 21:39:27.000000000 +0000
 +++ hier.c
-@@ -8,21 +8,15 @@ void hier()
-   d(auto_home,"doc",-1,-1,02755);
+@@ -5,24 +5,18 @@ void hier()
+   h(auto_home,-1,-1,02755);
+ 
+   d(auto_home,"bin",-1,-1,02755);
+-  d(auto_home,"doc",-1,-1,02755);
++  d(auto_home,"share/doc/qmailanalog",-1,-1,02755);
    d(auto_home,"man",-1,-1,02755);
    d(auto_home,"man/man1",-1,-1,02755);
 -  d(auto_home,"man/cat1",-1,-1,02755);
  
-   c(auto_home,"doc","MATCHUP",-1,-1,0644);
-   c(auto_home,"doc","ACCOUNTING",-1,-1,0644);
+-  c(auto_home,"doc","MATCHUP",-1,-1,0644);
+-  c(auto_home,"doc","ACCOUNTING",-1,-1,0644);
++  c(auto_home,"share/doc/qmailanalog","MATCHUP",-1,-1,0644);
++  c(auto_home,"share/doc/qmailanalog","ACCOUNTING",-1,-1,0644);
  
    c(auto_home,"man/man1","matchup.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","matchup.0",-1,-1,0644);
