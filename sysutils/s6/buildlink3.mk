6fb801252bd2d0d6fc3ad3ddd9ba19b094a8ccfc
schmonz <schmonz@pkgsrc.org>
2020-11-19 20:00:05 +0000

    Initial import of s6, packaged in wip by cfkoch@edgebsd.org.
    s6 is a small suite of programs for UNIX, designed to allow process
    supervision (a.k.a service supervision), in the line of daemontools and
    runit, as well as various operations on processes and daemons. It is meant
    to be a toolbox for low-level process and service administration, providing
    different sets of independent tools that can be used within or without the
    framework, and that can be assembled together to achieve powerful
    functionality with a very small amount of code.

---
+++ sysutils/s6/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/11/19 20:00:05 schmonz Exp $
+
+BUILDLINK_TREE+=	s6
+
+.if !defined(S6_BUILDLINK3_MK)
+S6_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.s6+=	s6>=2.9.2.0
+BUILDLINK_PKGSRCDIR.s6?=	../../sysutils/s6
+BUILDLINK_INCDIRS.s6+=		include/s6
+BUILDLINK_LIBDIRS.s6+=		lib/s6
+BUILDLINK_DEPMETHOD.s6?=	build
+.endif	# S6_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-s6
