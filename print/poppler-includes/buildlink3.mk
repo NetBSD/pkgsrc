a79c6d7cf815a2311a4ec1811d19809544ddfff8
wiz <wiz@pkgsrc.org>
2020-11-21 09:34:53 +0000

    *: recursive bump for poppler 20.11.0

--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.47 2020/04/03 10:30:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.48 2020/11/21 09:34:53 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-includes
 
@@ -8,7 +8,7 @@ POPPLER_INCLUDES_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
 BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.22.2
-BUILDLINK_ABI_DEPENDS.poppler-includes+=	poppler-includes>=0.87.0
+BUILDLINK_ABI_DEPENDS.poppler-includes+=	poppler-includes>=20.11.0
 BUILDLINK_PKGSRCDIR.poppler-includes?=		../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
