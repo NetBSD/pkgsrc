06ef3c1d9205e49a7c82a0113ef250b4e724e9c3
adam <adam@pkgsrc.org>
2020-11-21 22:33:59 +0000

    py-pygit2: updated to 1.4.0
    1.4.0
    - Upgrade to libgit2 1.1, new ``GIT_BLAME_IGNORE_WHITESPACE`` constant
    - Add wheels for Python 3.9
    - Drop support for PyPy3 7.2
    - New optional ``flags`` argument in ``Repository.__init__(...)``,
      new ``GIT_REPOSITORY_OPEN_*`` constants
    - Documentation

--- devel/py-pygit2/patches/patch-src_types.h
+++ devel/py-pygit2/patches/patch-src_types.h
@@ -1,17 +1,16 @@
-$NetBSD: patch-src_types.h,v 1.1 2020/10/22 16:24:10 jperkin Exp $
+$NetBSD: patch-src_types.h,v 1.2 2020/11/21 22:33:59 adam Exp $
 
-Builds and tests fine against libgit2 1.1.x.
+Builds and tests fine against newer libgit2.
 
---- src/types.h.orig	2020-07-24 11:39:38.000000000 +0000
+--- src/types.h.orig	2020-10-14 06:40:15.000000000 +0000
 +++ src/types.h
-@@ -32,10 +32,6 @@
+@@ -32,9 +32,6 @@
  #include <Python.h>
  #include <git2.h>
  
--#if !(LIBGIT2_VER_MAJOR == 1 && LIBGIT2_VER_MINOR == 0)
--#error You need a compatible libgit2 version (1.0.x)
+-#if !(LIBGIT2_VER_MAJOR == 1 && LIBGIT2_VER_MINOR == 1)
+-#error You need a compatible libgit2 version (1.1.x)
 -#endif
--
+ 
  /*
   * Python objects
-  *
