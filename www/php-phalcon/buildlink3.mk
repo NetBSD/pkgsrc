2166fa54ec4c0b7e4f378bd96e035e62918e94c6
otis <otis@pkgsrc.org>
2020-11-21 13:57:21 +0000

    php-phalcon4: Add php-phalcon4 4.1.0
    Phalcon is a full stack PHP framework offering low resource consumption and
    high performance.
    
    See https://pecl.php.net/package/phalcon

---
+++ www/php-phalcon/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/11/21 13:57:21 otis Exp $
+
+BUILDLINK_TREE+=	php-phalcon4
+
+.if !defined(PHP_PHALCON4_BUILDLINK3_MK)
+PHP_PHALCON4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.php-phalcon4+=	php-phalcon4>=1.0.0
+BUILDLINK_PKGSRCDIR.php-phalcon4?=	../../www/php-phalcon4
+.endif	# PHP_PHALCON4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-php-phalcon4
