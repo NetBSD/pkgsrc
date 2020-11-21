41d478c847face2ac9047a8f292d0b95ce883c8d
adam <adam@pkgsrc.org>
2020-11-21 11:27:23 +0000

    meson: updated to 0.56.0
    0.56.0:
    * meson test can now filter tests by subproject
    * Native (build machine) compilers not always required by project()
    * New extra_files key in target introspection
    * Preliminary AIX support
    * Wraps from subprojects are automatically promoted
    * meson.build_root() and meson.source_root() are deprecated
    * dep.as_link_whole()
    * Add support for all Windows subsystem types
    * Added NVidia HPC SDK compilers
    * Project and built-in options can be set in native or cross files
    * unstable-keyval is now stable keyval
    * CMake subproject cross compilation support
    * Machine file keys are stored case sensitive
    * Consistency between declare_dependency() and pkgconfig.generate() variables
    * Qt5 compile_translations now supports qresource preprocessing
    * Controlling subproject dependencies with dependency(allow_fallback: ...)
    * Custom standard library
    * Improvements for the builtin curses dependency
    * HDF5 dependency improvements
    * External projects
    * Per subproject warning_level option
    * meson subprojects command
    * Added CompCert C compiler
    * Dependencies listed in test and benchmark introspection
    * include_type support for the CMake subproject object dependency method
    * Deprecate Dependency.get_pkgconfig_variable and Dependency.get_configtool_variable

--- devel/meson/patches/patch-mesonbuild_scripts_depfixer.py
+++ devel/meson/patches/patch-mesonbuild_scripts_depfixer.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-mesonbuild_scripts_depfixer.py,v 1.2 2020/07/27 16:08:13 adam Exp $
+$NetBSD: patch-mesonbuild_scripts_depfixer.py,v 1.3 2020/11/21 11:27:23 adam Exp $
 
 Do not touch rpath for ELF.
 
---- mesonbuild/scripts/depfixer.py.orig	2020-06-29 17:00:15.000000000 +0000
+--- mesonbuild/scripts/depfixer.py.orig	2020-09-17 22:00:44.000000000 +0000
 +++ mesonbuild/scripts/depfixer.py
-@@ -457,8 +457,6 @@ def fix_rpath(fname, rpath_dirs_to_remov
-         if fname.endswith('.jar'):
-             fix_jar(fname)
+@@ -465,8 +465,6 @@ def fix_rpath(fname: str, rpath_dirs_to_
              return
+         if isinstance(new_rpath, str):
+             new_rpath = new_rpath.encode('utf8')
 -        fix_elf(fname, rpath_dirs_to_remove, new_rpath, verbose)
 -        return
      except SystemExit as e:
