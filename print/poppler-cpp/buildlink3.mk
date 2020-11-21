c72d3cbb17d19060300e34f3e86a137e1a4fb9dc
wiz <wiz@pkgsrc.org>
2020-11-21 09:34:53 +0000

    *: recursive bump for poppler 20.11.0

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.52 2020/08/17 20:17:40 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.53 2020/11/21 09:34:53 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.87.0nb1
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=20.11.0
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.80 2020/08/17 20:17:40 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.81 2020/11/21 09:34:53 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-glib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-glib
 POPPLER_GLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-glib+=	poppler-glib>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.87.0nb1
+BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=20.11.0
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 
 .include "../../devel/glib2/buildlink3.mk"
