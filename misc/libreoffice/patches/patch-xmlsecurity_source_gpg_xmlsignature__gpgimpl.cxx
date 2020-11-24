a9d0569fec0d0205c5455b8a170cf92408b07259
ryoon <ryoon@pkgsrc.org>
2020-11-24 14:33:22 +0000

    libreoffice: Fix comments about security/gpgme

--- misc/libreoffice/patches/patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx
+++ misc/libreoffice/patches/patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx
@@ -1,6 +1,6 @@
-$NetBSD: patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx,v 1.3 2018/08/18 00:11:32 ryoon Exp $
+$NetBSD: patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx,v 1.4 2020/11/24 14:33:22 ryoon Exp $
 
-* Do not confuse with key.h from gpgmepp and nss
+* Do not confuse with key.h from security/gpgme and devel/nss
 
 --- xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx.orig	2018-08-02 19:54:54.000000000 +0000
 +++ xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx
