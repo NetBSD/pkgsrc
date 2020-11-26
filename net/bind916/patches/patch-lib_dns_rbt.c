c2fdc45d4a01c601404abf28c31a98a3ec7c7b7e
taca <taca@pkgsrc.org>
2020-11-26 13:21:51 +0000

    net/bind916: update to 9.16.9
    	--- 9.16.9 released ---
    
    5533.	[func]		Add the "stale-refresh-time" option, a time window that
    			starts after a failed lookup, during which a stale RRset
    			is served directly from cache before a new attempt to
    			refresh it is made. [GL #2066]
    
    5530.	[bug]		dnstap did not capture responses to forwarded UPDATE
    			requests. [GL #2252]
    
    5527.	[bug]		A NULL pointer dereference occurred when creating an NTA
    			recheck query failed. [GL #2244]
    
    5525.	[bug]		Change 5503 inadvertently broke cross-compilation by
    			replacing a call to AC_LINK_IFELSE() with a call to
    			AC_RUN_IFELSE() in configure.ac.  This has been fixed,
    			making cross-compilation possible again. [GL #2237]
    
    5523.	[bug]		The initial lookup in a zone transitioning to/from a
    			signed state could fail if the DNSKEY RRset was not
    			found. [GL #2236]
    
    5522.	[bug]		Fixed a race/NULL dereference in TCPDNS send. [GL #2227]
    
    5520.	[bug]		Fixed a number of shutdown races, reference counting
    			errors, and spurious log messages that could occur
    			in the network manager. [GL #2221]
    
    5518.	[bug]		Stub zones now work correctly with primary servers using
    			"minimal-responses yes". [GL #1736]
    
    5517.	[bug]		Do not treat UV_EOF as a TCP4RecvErr or a TCP6RecvErr.
    			[GL #2208]
    
    	--- 9.16.8 released ---
    
    5516.	[func]		The default EDNS buffer size has been changed from 4096
    			to 1232 bytes. [GL #2183]
    
    5515.	[func]		Add 'rndc dnssec -rollover' command to trigger a manual
    			rollover for a specific key. [GL #1749]
    
    5514.	[bug]		Fix KASP expected key size for Ed25519 and Ed448.
    			[GL #2171]
    
    5513.	[doc]		The ARM section describing the "rrset-order" statement
    			was rewritten to make it unambiguous and up-to-date with
    			the source code. [GL #2139]
    
    5512.	[bug]		"rrset-order" rules using "order none" were causing
    			named to crash despite named-checkconf treating them as
    			valid. [GL #2139]
    
    5511.	[bug]		'dig -u +yaml' failed to display timestamps to the
    			microsecond. [GL #2190]
    
    5510.	[bug]		Implement the attach/detach semantics for dns_message_t
    			to fix a data race in accessing an already-destroyed
    			fctx->rmessage. [GL #2124]
    
    5509.	[bug]		filter-aaaa: named crashed upon shutdown if it was in
    			the process of recursing for A RRsets. [GL #1040]
    
    5508.	[func]		Added new parameter "-expired" for "rndc dumpdb" that
    			also prints expired RRsets (awaiting cleanup) to the
    			dump file. [GL #1870]
    
    5507.	[bug]		Named could compute incorrect SIG(0) responses.
    			[GL #2109]
    
    5506.	[bug]		Properly handle failed sysconf() calls, so we don't
    			report invalid memory size. [GL #2166]
    
    5505.	[bug]		Updating contents of a mixed-case RPZ could cause some
    			rules to be ignored. [GL #2169]
    
    5503.	[bug]		Cleaned up reference counting of network manager
    			handles, now using isc_nmhandle_attach() and _detach()
    			instead of _ref() and _unref(). [GL #2122]

--- net/bind916/patches/patch-lib_dns_rbt.c
+++ net/bind916/patches/patch-lib_dns_rbt.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-lib_dns_rbt.c,v 1.1 2020/08/09 15:20:22 taca Exp $
+$NetBSD: patch-lib_dns_rbt.c,v 1.2 2020/11/26 13:21:51 taca Exp $
 
 * Take from NetBSD base.
 * Disable inline on powerpc.
 
---- lib/dns/rbt.c.orig	2020-05-06 09:59:35.000000000 +0000
+--- lib/dns/rbt.c.orig	2020-11-16 14:44:37.000000000 +0000
 +++ lib/dns/rbt.c
-@@ -301,12 +301,13 @@ Name(dns_rbtnode_t *node) {
+@@ -319,12 +319,13 @@ Name(dns_rbtnode_t *node) {
  }
  
  static void
@@ -20,7 +20,7 @@ $NetBSD: patch-lib_dns_rbt.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  
  	fprintf(stderr, "%s: ", desc);
  	do {
-@@ -324,6 +325,10 @@ hexdump(const char *desc, unsigned char 
+@@ -342,6 +343,10 @@ hexdump(const char *desc, unsigned char 
  }
  #endif /* DEBUG */
  
@@ -31,32 +31,7 @@ $NetBSD: patch-lib_dns_rbt.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  /*
   * Upper node is the parent of the root of the passed node's
   * subtree. The passed node must not be NULL.
-@@ -545,6 +550,9 @@ serialize_node(FILE *file, dns_rbtnode_t
- 	off_t file_position;
- 	unsigned char *node_data = NULL;
- 	size_t datasize;
-+#ifdef DEBUG
-+	dns_name_t nodename;
-+#endif
- 
- 	INSIST(node != NULL);
- 
-@@ -597,10 +605,12 @@ serialize_node(FILE *file, dns_rbtnode_t
- 	CHECK(isc_stdio_write(node_data, 1, datasize, file, NULL));
- 
- #ifdef DEBUG
-+	dns_name_init(&nodename, NULL);
-+	NODENAME(node, &nodename);
- 	fprintf(stderr, "serialize ");
--	dns_name_print(name, stderr);
-+	dns_name_print(&nodename, stderr);
- 	fprintf(stderr, "\n");
--	hexdump("node header", (unsigned char *)&temp_node,
-+	hexdump("node header", &temp_node,
- 		sizeof(dns_rbtnode_t));
- 	hexdump("node data", node_data, datasize);
- #endif /* ifdef DEBUG */
-@@ -721,7 +731,7 @@ dns_rbt_serialize_tree(FILE *file, dns_r
+@@ -748,7 +753,7 @@ dns_rbt_serialize_tree(FILE *file, dns_r
  
  	isc_crc64_final(&crc);
  #ifdef DEBUG
@@ -65,7 +40,7 @@ $NetBSD: patch-lib_dns_rbt.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  #endif /* ifdef DEBUG */
  
  	/* Serialize header */
-@@ -742,7 +752,7 @@ cleanup:
+@@ -769,7 +774,7 @@ cleanup:
  			result = ISC_R_INVALIDFILE; \
  			goto cleanup;               \
  		}                                   \
@@ -74,7 +49,7 @@ $NetBSD: patch-lib_dns_rbt.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  
  static isc_result_t
  treefix(dns_rbt_t *rbt, void *base, size_t filesize, dns_rbtnode_t *n,
-@@ -852,7 +862,7 @@ treefix(dns_rbt_t *rbt, void *base, size
+@@ -879,7 +884,7 @@ treefix(dns_rbt_t *rbt, void *base, size
  	fprintf(stderr, "deserialize ");
  	dns_name_print(&nodename, stderr);
  	fprintf(stderr, "\n");
@@ -83,7 +58,7 @@ $NetBSD: patch-lib_dns_rbt.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  	hexdump("node data", node_data, datasize);
  #endif /* ifdef DEBUG */
  	isc_crc64_update(crc, (const uint8_t *)&header, sizeof(dns_rbtnode_t));
-@@ -928,7 +938,7 @@ dns_rbt_deserialize_tree(void *base_addr
+@@ -955,7 +960,7 @@ dns_rbt_deserialize_tree(void *base_addr
  
  	isc_crc64_final(&crc);
  #ifdef DEBUG
