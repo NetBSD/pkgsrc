dbe1908af3d5f2b3948d4d20f0bf64c986293a36
pin <pin@pkgsrc.org>
2020-11-26 21:43:43 +0000

    textproc/diffr: import package
    Reviewing changes involves reading diffs. Sometimes, a line-oriented
    presentation of changes is not precise enough, especially when changes
    involve long lines or very similar consecutive lines.
    
    This program processes such diffs, and output them (in the console)
    with additional diff information on top of the unified diff format,
    using text attributes.
    
    It works hunk by hunk, recomputing the diff on a word-by-word basis.

---
+++ textproc/diffr/cargo-depends.mk
@@ -0,0 +1,20 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2020/11/26 21:43:43 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	ansi_term-0.11.0
+CARGO_CRATE_DEPENDS+=	atty-0.2.11
+CARGO_CRATE_DEPENDS+=	bitflags-1.1.0
+CARGO_CRATE_DEPENDS+=	clap-2.33.0
+CARGO_CRATE_DEPENDS+=	libc-0.2.58
+CARGO_CRATE_DEPENDS+=	numtoa-0.1.0
+CARGO_CRATE_DEPENDS+=	redox_syscall-0.1.54
+CARGO_CRATE_DEPENDS+=	redox_termios-0.1.1
+CARGO_CRATE_DEPENDS+=	strsim-0.8.0
+CARGO_CRATE_DEPENDS+=	termcolor-1.1.0
+CARGO_CRATE_DEPENDS+=	termion-1.5.3
+CARGO_CRATE_DEPENDS+=	textwrap-0.11.0
+CARGO_CRATE_DEPENDS+=	unicode-width-0.1.5
+CARGO_CRATE_DEPENDS+=	vec_map-0.8.1
+CARGO_CRATE_DEPENDS+=	winapi-0.3.7
+CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
+CARGO_CRATE_DEPENDS+=	winapi-util-0.1.3
+CARGO_CRATE_DEPENDS+=	winapi-x86_64-pc-windows-gnu-0.4.0
