d336d479ff8b44893692a96be802662e22897b75
taca <taca@pkgsrc.org>
2020-11-26 14:44:01 +0000

    lang/php73: update to 7.3.25
    26 Nov 2020, PHP 7.3.25
    
    - Core:
      . Fixed bug #80280 (ADD_EXTENSION_DEP() fails for ext/standard and ext/date).
        (cmb)
      . Fixed bug #80258 (Windows Deduplication Enabled, randon permission errors).
        (cmb)
    
    - COM:
      . Fixed bug #62474 (com_event_sink crashes on certain arguments). (cmb)
    
    - DOM:
      . Fixed bug #80268 (loadHTML() truncates at NUL bytes). (cmb)
    
    - IMAP:
      . Fixed bug #64076 (imap_sort() does not return FALSE on failure). (cmb)
      . Fixed bug #76618 (segfault on imap_reopen). (girgias)
      . Fixed bug #80239 (imap_rfc822_write_address() leaks memory). (cmb)
      . Fixed minor regression caused by fixing bug #80220. (cmb)
      . Fixed bug #80242 (imap_mail_compose() segfaults for multipart with rfc822).
        (cmb)
    
    - Intl:
      . Fixed bug #80310 (ext-intl with icu4c 68.1: use of undeclared identifier
        'TRUE'). (Alexander M. Turek)
    
    - ODBC:
      . Fixed bug #44618 (Fetching may rely on uninitialized data). (cmb)
    
    - SNMP:
      . Fixed bug #70461 (disable md5 code when it is not supported in net-snmp).
        (Alexander Bergmann, cmb)
    
    - Standard:
      . Fixed bug #80266 (parse_url silently drops port number 0). (cmb, Nikita)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.313 2020/10/30 07:14:16 taca Exp $
+# $NetBSD: phpversion.mk,v 1.314 2020/11/26 14:44:01 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.34
-PHP73_VERSION=	7.3.24
+PHP73_VERSION=	7.3.25
 PHP74_VERSION=	7.4.12
 
 # Define initial release of major version.
