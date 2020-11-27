3bb00a223562eb35e7db5f793e7c3e1c944fc81f
bsiegert <bsiegert@pkgsrc.org>
2020-11-27 09:11:39 +0000

    Update gh (Github CLI) to 1.3.0.
    New Stuff
    
      * If an interactive invocation of either pr create or issue create fails, gh
        will now save a user's input. Both create commands now support a --recover
        option to pick up where you left off.
      * Add support for GH_TOKEN and GH_ENTERPRISE_TOKEN
    
    Bug Fixes
    
      * Correctly select branch when viewing repository
      * Downgrade survey to v2.1.1
    
    Cleanup
    
      * Refactor pr/issue creation code for readability and maintainability
      * Fix spelling errors
      * Fix USAGE help info for some commands

--- net/gh/go-modules.mk
+++ net/gh/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.4 2020/11/20 17:15:59 leot Exp $
+# $NetBSD: go-modules.mk,v 1.5 2020/11/27 09:11:39 bsiegert Exp $
 
 GO_MODULE_FILES+=	cloud.google.com/go/@v/v0.26.0.mod
 GO_MODULE_FILES+=	cloud.google.com/go/@v/v0.34.0.mod
@@ -20,8 +20,8 @@ GO_MODULE_FILES+=	cloud.google.com/go/storage/@v/v1.0.0.mod
 GO_MODULE_FILES+=	cloud.google.com/go/storage/@v/v1.0.0.zip
 GO_MODULE_FILES+=	dmitri.shuralyov.com/gpu/mtl/@v/v0.0.0-20190408044501-666a987793e9.mod
 GO_MODULE_FILES+=	dmitri.shuralyov.com/gpu/mtl/@v/v0.0.0-20190408044501-666a987793e9.zip
-GO_MODULE_FILES+=	github.com/!alec!aivazis/survey/v2/@v/v2.2.2.mod
-GO_MODULE_FILES+=	github.com/!alec!aivazis/survey/v2/@v/v2.2.2.zip
+GO_MODULE_FILES+=	github.com/!alec!aivazis/survey/v2/@v/v2.1.1.mod
+GO_MODULE_FILES+=	github.com/!alec!aivazis/survey/v2/@v/v2.1.1.zip
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.mod
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.zip
 GO_MODULE_FILES+=	github.com/!burnt!sushi/xgb/@v/v0.0.0-20160522181843-27f122750802.mod
