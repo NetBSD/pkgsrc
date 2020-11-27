9bff5ade07f71908da89948e1bbc15ed35239d01
schmonz <schmonz@pkgsrc.org>
2020-11-27 13:48:25 +0000

    Not sure this is right (can't test httppc easily), but it's a lot less wrong than before. Bump PKGREVISION.

--- net/ucspi-tools/patches/patch-fdprintf.c
+++ net/ucspi-tools/patches/patch-fdprintf.c
@@ -1,21 +1,25 @@
-$NetBSD: patch-fdprintf.c,v 1.1 2020/11/27 11:34:17 schmonz Exp $
+$NetBSD: patch-fdprintf.c,v 1.2 2020/11/27 13:48:25 schmonz Exp $
 
 Not all systems have dprintf().
 
---- fdprintf.c.orig	2020-11-27 10:59:35.151164978 +0000
+--- fdprintf.c.orig	2020-11-27 13:14:12.000000000 +0000
 +++ fdprintf.c
-@@ -0,0 +1,14 @@
+@@ -0,0 +1,18 @@
 +#include <stdarg.h>
 +#include <stdio.h>
++#include <sys/errno.h>
++
++static FILE *f;
 +
 +int fdprintf(int fd, const char *fmt, ...) {
 +	va_list ap;
-+	FILE *f = fdopen(fd, "w");
 +	int rc;
++
++	f = fdopen(fd, "w");
++	if (f == NULL) return -1;
 +
 +	va_start(ap, fmt);
 +	rc = vfprintf(f, fmt, ap);
-+	fclose(f);
 +	va_end(ap);
 +	return rc;
 +}
