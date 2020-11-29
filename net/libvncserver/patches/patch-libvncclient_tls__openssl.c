14d9bd8ad413798e0735c0760011830f88ccfe54
nia <nia@pkgsrc.org>
2020-11-29 20:33:53 +0000

    libvncserver: Fix missing decl of fd_set when compiled with openssl

--- net/libvncserver/patches/patch-libvncclient_tls__openssl.c
+++ net/libvncserver/patches/patch-libvncclient_tls__openssl.c
@@ -1,10 +1,12 @@
-$NetBSD: patch-libvncclient_tls__openssl.c,v 1.1 2019/02/25 09:48:26 jperkin Exp $
+$NetBSD: patch-libvncclient_tls__openssl.c,v 1.2 2020/11/29 20:33:53 nia Exp $
 
 Bump _XOPEN_SOURCE to the same as other files to avoid C99 issues.
 
+Fix missing declaration of fd_set.
+
 --- libvncclient/tls_openssl.c.orig	2019-01-06 19:09:30.000000000 +0000
 +++ libvncclient/tls_openssl.c
-@@ -19,7 +19,7 @@
+@@ -19,10 +19,11 @@
   */
  
  #ifndef _MSC_VER
@@ -13,3 +15,7 @@ Bump _XOPEN_SOURCE to the same as other files to avoid C99 issues.
  #endif
  
  #include <rfb/rfbclient.h>
++#include <sys/select.h>
+ #include <errno.h>
+ 
+ #include <openssl/err.h>
