347c5bef86bff657767929bc0eb10d63c274dbaf
nia <nia@pkgsrc.org>
2020-12-03 13:00:48 +0000

    speech-dispatcher: Update to 0.10.2
    Version 0.10.2
    * generic: Add support for sound icons
    * Add French, Norwegian Bokmål, Portuguese, Brazilian Portuguese, and Russian translations.
    * Add voxin module variant.
    * Factorize espeak module with espeak-ng module.
    * Make ibmtts and voxin use the common queue helpers.
    * Fix python bindings against python 3.9.
    * Reference the golang bindings.
    * Add spd_get_client_id.
    * Make key, char and sound_icon commands return message id.
    * Expose modulebindir in speech-dispatcher.pc.
    * Remove generic modules for synths that have a non-generic module.
    
    Version 0.10
    * Add punctuation level "most".
    * Rework thread synchronization.
    * Move espeak/espeak-ng audio queuing to shared file, rework it.
    * Make baratinoo use this audio queueing, enable marks, and support
    responsiveness parameter.
    * Add support for Baratinoo engine 8.6
    * Show mark progression in spd-say.
    * Add --character option to spd-say.
    * Put country/region in language instead of variant.
    * pico, generic: fix setting voice by language.
    * generic: Add fallback voices for generic languages.
    * symbols: Add support for SSML.
    * symbols: Add SymbolPreprocFile directive to load several symbol files.
    * symbols: Replace DefaultSymbolsPreproc directive with SymbolsPreproc directive
    to have more fine-grained control over server preprocessing.
    * symbols: Import emojis and unicode font variants support from NVDA and Unicode
    CLDR and UnicodeData.
    * symbols: Import some symbols from Orca.
    * symbols: Add rules to drop gender-neutral forms.
    * symbols: Add support for group references.
    * Disable Mary-TTS module by default.
    * Replace AudioPulseServer option with AudioPulseDevice.
    * Fix default pulse latency to 10ms to avoid pulseaudio distorted output.
    * Made client isolation stronger for Snap/Flatpak support.

--- audio/speech-dispatcher/patches/patch-src_modules_espeak.c
+++ audio/speech-dispatcher/patches/patch-src_modules_espeak.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_modules_espeak.c,v 1.2 2019/11/28 16:51:49 nia Exp $
+$NetBSD: patch-src_modules_espeak.c,v 1.3 2020/12/03 13:00:48 nia Exp $
 
 Fix path to espeak header file
 
---- src/modules/espeak.c.orig	2019-01-01 17:32:54.000000000 +0000
+--- src/modules/espeak.c.orig	2020-09-28 13:31:15.000000000 +0000
 +++ src/modules/espeak.c
-@@ -35,7 +35,7 @@
- #include <semaphore.h>
- 
- /* espeak header file */
+@@ -39,7 +39,7 @@
+ #ifdef ESPEAK_NG_INCLUDE
+ #include <espeak-ng/espeak_ng.h>
+ #else
 -#include <espeak/speak_lib.h>
 +#include <speak_lib.h>
+ #endif
  
  #ifndef ESPEAK_API_REVISION
- #define ESPEAK_API_REVISION 1
