30b391dabbf59e9dc4f97c99c5dfcb2ae85cd6eb
nia <nia@pkgsrc.org>
2020-12-05 12:03:25 +0000

    mozjs78: Fix configure where the readline library isn't builtin.
    This commit also moves the location of WRKSRC so that no patches are
    applied to files in parent directories of WRKSRC.

--- lang/mozjs78/patches/patch-js_src_jit_ProcessExecutableMemory.cpp
+++ lang/mozjs78/patches/patch-js_src_jit_ProcessExecutableMemory.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-jit_ProcessExecutableMemory.cpp,v 1.1 2020/11/10 11:27:10 nia Exp $
+$NetBSD: patch-js_src_jit_ProcessExecutableMemory.cpp,v 1.1 2020/12/05 12:03:26 nia Exp $
 
 PaX MPROTECT safety for NetBSD.
 
---- jit/ProcessExecutableMemory.cpp.orig	2020-09-25 09:22:55.000000000 +0000
-+++ jit/ProcessExecutableMemory.cpp
+--- js/src/jit/ProcessExecutableMemory.cpp.orig	2020-11-04 10:51:58.000000000 +0000
++++ js/src/jit/ProcessExecutableMemory.cpp
 @@ -362,9 +362,16 @@ static void* ReserveProcessExecutableMem
    // Note that randomAddr is just a hint: if the address is not available
    // mmap will pick a different address.
    void* randomAddr = ComputeRandomAllocationAddress();
-+#ifdef MPROTECT
++#ifdef PROT_MPROTECT
 +  void* p = MozTaggedAnonymousMmap(randomAddr, bytes,
 +                                   PROT_MPROTECT(PROT_EXEC | PROT_WRITE | PROT_READ), 
 +                                   MAP_NORESERVE | MAP_PRIVATE | MAP_ANON, -1,
