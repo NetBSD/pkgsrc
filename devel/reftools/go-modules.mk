62e692125ad5294750999e3d2e2dd122fd0b7d98
bsiegert <bsiegert@pkgsrc.org>
2020-12-06 19:50:14 +0000

    New package, reftools.
    reftools is a collection of refactoring tools for Go.
    
    fixplurals: remove redundant parameter and result types from function signatures
    fillstruct: fills a struct literal with default values
    fillswitch: fills a (type) switch statement with case statements

---
+++ devel/reftools/go-modules.mk
@@ -0,0 +1,14 @@
+# $NetBSD: go-modules.mk,v 1.1 2020/12/06 19:50:14 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/kisielk/gotool/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/kisielk/gotool/@v/v1.0.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190311183353-d8887717615a.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190311183353-d8887717615a.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20190408220357-e5b8258f4918.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20190408220357-e5b8258f4918.zip
