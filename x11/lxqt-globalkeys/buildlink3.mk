01da6adf258bfa16e69df55d49f279d246135da1
pin <pin@pkgsrc.org>
2020-12-07 12:25:25 +0000

    x11/lxqt-globalkeys: update to 0.16.0
    -Added shortcut to configure display.
    -Silenced unused function parameters warnings.
    -Do not grab keys of disabled client actions.
    -Do not requite daemon restart for enabling shortcuts.

--- x11/lxqt-globalkeys/buildlink3.mk
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/12/04 04:56:18 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/12/07 12:25:25 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.15.0
-BUILDLINK_ABI_DEPENDS.lxqt-globalkeys?=	lxqt-globalkeys>=0.15.0nb4
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.16.0
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
