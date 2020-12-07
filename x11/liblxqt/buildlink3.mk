f0600381ce58a35c7daad70a7cae20fd100b871d
pin <pin@pkgsrc.org>
2020-12-07 11:32:53 +0000

    x11/liblxqt: update to 0.16.0 -Use QFlags default constructor -Added an action for showing LXQt leave dialog (to be used when the power button is pressed). -Don't use deprecated QProcess methods.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/12/04 04:56:18 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/12/07 11:32:53 pin Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.15.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=0.15.1nb4
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.16.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
