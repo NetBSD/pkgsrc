4da16a70452a36900e1fe4e5d350c8c8c8e7c5f0
pin <pin@pkgsrc.org>
2020-12-07 11:29:28 +0000

    devel/lxqt-build-tools: update to 0.8.0
    -Removed version checks for some LXQt dependencies.
    -Added a Fontconfig CMake find module.
    -Suppressed warning about find_package_handle_standard_args package name mismatch.

--- x11/libqtxdg/buildlink3.mk
+++ x11/libqtxdg/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/11/05 09:07:17 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2020/12/07 11:29:28 pin Exp $
 
 BUILDLINK_TREE+=	libqtxdg
 
 .if !defined(LIBQTXDG_BUILDLINK3_MK)
 LIBQTXDG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.5.0
-BUILDLINK_ABI_DEPENDS.libqtxdg?=	libqtxdg>=3.5.0nb3
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.6.0
 BUILDLINK_PKGSRCDIR.libqtxdg?=		../../x11/libqtxdg
 
 .include "../../devel/glib2/buildlink3.mk"
