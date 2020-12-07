74d26997840c391eef5ad4a805f9d93cb0ba89e6
pin <pin@pkgsrc.org>
2020-12-07 11:48:54 +0000

    x11/qtermwidget: update to 0.16.1
    0.16.1
    -Bumped version to 0.16.1, for a point release of qterminal.
    
    0.16.0
    -Use qAsConst.
    -Don't search application dir for keyboard and scheme data.
    -Use Q_EMIT to call signals, instead of the emit keyword.
    -Dropped the deprecated class QLinkedList.
    -Don't use 0/nullptr to initialize QFlags.
    -Fixed ColorEntry copy operator.
    -Disabled the use of copy constructors and assignment operators.
    -Check for successful ioctl() calls by testing that the call did not return -1.
    -Fix find_package() developer warning in cmake.
    -Use const references wherever possible.
    -Handle keyboard commands properly.

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/06/01 10:53:09 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/12/07 11:48:54 pin Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.15.0
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.16.1
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
 #.include "../../x11/qt5-qtbase/buildlink3.mk"
