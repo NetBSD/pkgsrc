d1a2b4bbaf745118de92013e5e01bed24981aa3f
adam <adam@pkgsrc.org>
2020-12-08 15:03:58 +0000

    py-pybtex: updated to 0.23.0
    Version 0.23.0
    --------------
    
    - Reimplemented :py:class:`~pybtex.utils.OrderedCaseInsensitiveDict` using
      :py:class:`python:collections.OrderedDict` (so it has a __delitem__).
    
    - ``unsrt.py`` now supports type when formatting phdthesis.
    
    - Added :py:meth:`~pybtex.database.BibliographyData.from_string` to
      :py:class:`pybtex.database.BibliographyData`.
    
    - Added :py:meth:`~pybtex.database.Entry.from_string` and
      :py:meth:`~pybtex.database.Entry.to_string` to
      :py:class:`pybtex.database.Entry`.
    
    - Added indentation to ``__repr__`` in
      :py:class:`pybtex.database.BibliographyData` and
      :py:class:`pybtex.database.Entry`.
    
    - Preserve order in
      :py:meth:`pybtex.utils.OrderedCaseInsensitiveDict.__repr__`.
    
    - Fixed entries with duplicate keys being removed during sorting.
    
    - Fixed handling of duplicate person fields
    
    - Use :py:mod:`~xml.etree.ElementTree` instead of the deprecated
      ``cElementTree``.
    
    - Import base classes from :py:mod:`python:collections.abc` instead of
      :py:mod:`python:collections`.
    
    - Use ``__iter__`` instead of deprecated ``Element.getchildren()``.

--- textproc/py-pybtex/patches/patch-setup.py
+++ textproc/py-pybtex/patches/patch-setup.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-setup.py,v 1.1 2018/12/13 21:01:01 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/12/08 15:03:58 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2018-12-13 20:55:25.000000000 +0000
+--- setup.py.orig	2020-12-08 14:55:32.000000000 +0000
 +++ setup.py
-@@ -82,7 +82,7 @@ setup(
-     ],
+@@ -92,7 +92,7 @@ setup(
      install_requires=install_requires,
+     extras_require=extras_require,
      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
 -    packages=find_packages(exclude=['docs']),
 +    packages=find_packages(exclude=['docs','tests*']),
