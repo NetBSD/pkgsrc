4502fe0bb2e914d20e23bcbf28ed47b7ee24c898
jaapb <jaapb@pkgsrc.org>
2020-12-09 10:48:33 +0000

    Updated devel/ocaml-dune to version 2.7.1.
    Changes from the previous version are too many to mention (see the
    CHANGELOG.md file in the distribution for full details). From a practical
    point of view the package bootstrap procedure has changed now so that
    dune-configurator had to be split off as a separate package; there are
    also compatibility changes so the package works with newer versions of
    OCaml.

--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/17 10:29:34 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/12/09 10:48:33 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
 .if !defined(OCAML_DUNE_BUILDLINK3_MK)
 OCAML_DUNE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=2.1.3nb1
+BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=2.2.0
+BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=2.7.1
 BUILDLINK_PKGSRCDIR.ocaml-dune?=	../../devel/ocaml-dune
 .endif	# OCAML_DUNE_BUILDLINK3_MK
 
