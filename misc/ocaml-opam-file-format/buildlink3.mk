a3cec5ce90ebf9463d45be5e1e57e93533193a22
jaapb <jaapb@pkgsrc.org>
2020-12-09 10:36:09 +0000

    Updated misc/ocaml-opam-file-format to 2.1.0
    Changes include:
    * Don't add a newline at the start of strings with newlines
    * Report starting position of strings correctly
    * Add dune files

--- misc/ocaml-opam-file-format/buildlink3.mk
+++ misc/ocaml-opam-file-format/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/11/27 14:03:45 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/12/09 10:36:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam-file-format
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam-file-format
 OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.1.0
 BUILDLINK_PKGSRCDIR.ocaml-opam-file-format?=	../../misc/ocaml-opam-file-format
 .endif	# OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK
 
