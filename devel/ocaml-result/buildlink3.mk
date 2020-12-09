0c74423c9a036b7c25f7c8c889be569018eda5ad
jaapb <jaapb@pkgsrc.org>
2020-12-09 11:07:26 +0000

    Updated devel/ocaml-result to 1.5.
    The sole change is adding an alias for the result type on OCaml versions
    4.08 and higher.

--- devel/ocaml-result/buildlink3.mk
+++ devel/ocaml-result/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/01/24 09:16:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/12/09 11:07:26 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-result
 
 .if !defined(OCAML_RESULT_BUILDLINK3_MK)
 OCAML_RESULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-result+=	ocaml-result>=1.2
-BUILDLINK_ABI_DEPENDS.ocaml-result+=	ocaml-result>=1.4
+BUILDLINK_API_DEPENDS.ocaml-result+=	ocaml-result>=1.5
+BUILDLINK_ABI_DEPENDS.ocaml-result+=	ocaml-result>=1.5
 BUILDLINK_PKGSRCDIR.ocaml-result?=	../../devel/ocaml-result
 .endif	# OCAML_RESULT_BUILDLINK3_MK
 
