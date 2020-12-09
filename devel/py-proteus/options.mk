e9c96fe22283f659de842a3de0f29f9d882889ed
gutteridge <gutteridge@pkgsrc.org>
2020-12-09 01:46:07 +0000

    Update a couple of comments that reference Python packages
    Those comments were written before converters/unoconv and textproc/
    py-Levenshtein were imported. Point to what's in pkgsrc, rather than to
    upstream URLs (in the case of py-Levenshtein, an older, unmaintained
    version). (I'm not going to change anything to do with Tryton right
    now, given the recent related build breakage.)

--- devel/py-proteus/options.mk
+++ devel/py-proteus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2019/11/03 10:18:29 rillig Exp $
+# $NetBSD: options.mk,v 1.11 2020/12/09 01:46:07 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-proteus
 PKG_SUPPORTED_OPTIONS+=	cdecimal simplejson
@@ -8,8 +8,7 @@ PKG_SUGGESTED_OPTIONS+=	simplejson
 
 .include "../../mk/bsd.options.mk"
 
-# XXX Supports unoconv http://dag.wieers.com/home-made/unoconv/) and
-# XXX python-Levenshtein (http://github.com/miohtama/python-Levenshtein) options
+# XXX Also supports converters/unoconv and textproc/py-Levenshtein options.
 
 .if !empty(PKG_OPTIONS:Mcdecimal)
 PYTHON_VERSIONS_ACCEPTED=	27 # py-cdecimal is obsolete for that version and thus not available
