3b6344ce5f6dd51bf4292f8ddf6631c0e680540a
jaapb <jaapb@pkgsrc.org>
2020-12-10 09:15:57 +0000

    Updated devel/ocaml-ppx_deriving to 4.5.
    This adds support for OCaml 4.11.

--- devel/ocaml-ppx_deriving/buildlink3.mk
+++ devel/ocaml-ppx_deriving/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/04/08 08:58:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/12/10 09:15:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_deriving
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_deriving
 OCAML_PPX_DERIVING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.4.1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.5
 BUILDLINK_PKGSRCDIR.ocaml-ppx_deriving?=	../../devel/ocaml-ppx_deriving
 .endif	# OCAML_PPX_DERIVING_BUILDLINK3_MK
 
