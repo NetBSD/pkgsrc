c9943b693dbb89fe2d4bc4a8c5846d06eb52a3f0
jaapb <jaapb@pkgsrc.org>
2020-12-11 08:02:22 +0000

    Updated devel/ocaml-ppx_bench to 0.14.1.
    Amongst other things, this adds support for newer versions of
    ocaml-ppxlib.

--- devel/ocaml-ppx_bench/buildlink3.mk
+++ devel/ocaml-ppx_bench/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:26:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/12/11 08:02:22 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_bench
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_bench
 OCAML_PPX_BENCH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_bench+=	ocaml-ppx_bench>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_bench+=	ocaml-ppx_bench>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_bench+=	ocaml-ppx_bench>=0.14.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_bench?=	../../devel/ocaml-ppx_bench
 
 .endif	# OCAML_PPX_BENCH_BUILDLINK3_MK
