b2080ce363313b9fc58f20e9a3e589ebb89c7e80
jaapb <jaapb@pkgsrc.org>
2020-12-11 10:58:20 +0000

    Updated www/ocsigen-i18n to 3.7.0.
    Changelog is sketchy, but this adds a parameter that is needed by newer
    versions of ocsigen-start.

--- www/ocsigen-i18n/buildlink3.mk
+++ www/ocsigen-i18n/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/04/21 13:02:02 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/12/11 10:58:20 jaapb Exp $
 
 BUILDLINK_TREE+=	ocsigen-i18n
 
 .if !defined(OCSIGEN_I18N_BUILDLINK3_MK)
 OCSIGEN_I18N_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.1.0
-BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.5.0
+BUILDLINK_API_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.7.0
+BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.7.0
 BUILDLINK_PKGSRCDIR.ocsigen-i18n?=	../../www/ocsigen-i18n
 .endif	# OCSIGEN_I18N_BUILDLINK3_MK
 
