eac505b722a01a35564a58919707bb485f4a099e
jaapb <jaapb@pkgsrc.org>
2020-12-11 08:08:41 +0000

    Updated devel/ocaml-ppx_inline_test to 0.14.1
    Not easy to find out what exactly has been changed, but one thing is that
    newer versions of ocaml-ppxlib are now supported.

--- devel/ocaml-ppx_inline_test/buildlink3.mk
+++ devel/ocaml-ppx_inline_test/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:24:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/12/11 08:08:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_inline_test
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_inline_test
 OCAML_PPX_INLINE_TEST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.14.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_inline_test?=	../../devel/ocaml-ppx_inline_test
 
 .endif	# OCAML_PPX_INLINE_TEST_BUILDLINK3_MK
