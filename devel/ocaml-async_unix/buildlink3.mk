58ba26d263d0766822d6a4f7556f5616a9a4a3b5
jaapb <jaapb@pkgsrc.org>
2020-12-11 09:42:41 +0000

    Updated devel/ocaml-async_unix to 0.13.1.
    This adds compatibility with OCaml 4.11.

--- devel/ocaml-async_unix/buildlink3.mk
+++ devel/ocaml-async_unix/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/04/08 11:08:59 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/12/11 09:42:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-async_unix
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-async_unix
 OCAML_ASYNC_UNIX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-async_unix+=	ocaml-async_unix>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-async_unix+=	ocaml-async_unix>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-async_unix+=	ocaml-async_unix>=0.13.1
 BUILDLINK_PKGSRCDIR.ocaml-async_unix?=		../../devel/ocaml-async_unix
 
 .endif	# OCAML_ASYNC_UNIX_BUILDLINK3_MK
