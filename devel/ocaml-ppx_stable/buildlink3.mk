d875b85ad63669ac53d8df795262e9ff50cb9254
jaapb <jaapb@pkgsrc.org>
2020-12-11 09:10:57 +0000

    Updated devel/ocaml-ppx_stable to 0.14.1
    Changelog is incomplete, but this version adds support for newer
    versions of ocaml-ppxlib.

--- devel/ocaml-ppx_stable/buildlink3.mk
+++ devel/ocaml-ppx_stable/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:24 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/12/11 09:10:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_stable
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_stable
 OCAML_PPX_STABLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_stable+=	ocaml-ppx_stable>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_stable+=	ocaml-ppx_stable>=0.14.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_stable?=		../../devel/ocaml-ppx_stable
 
 .endif	# OCAML_PPX_STABLE_BUILDLINK3_MK
