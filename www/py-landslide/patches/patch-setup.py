835075eb65c8d5673c99056d88f4a9bae28dbfdf
adam <adam@pkgsrc.org>
2020-12-15 09:07:41 +0000

    py-landslide: updated to 1.1.8
    v1.1.8
    - Explicitly support Python v3.8
    - Test Python v3.7 and v3.8 with CI
    - Fix sample slideshow link in `setup.py`
    
    v1.1.7
    - Change "PrinceXML" references to "Prince"
    - Upgrade `Jinja2` from v2.10 to v2.10.1
    - Support `markdown` v3.0+
    - Fix Prince integration for PDF output
    - Use HTTPS URLs in generated HTML code

--- www/py-landslide/patches/patch-setup.py
+++ www/py-landslide/patches/patch-setup.py
@@ -1,19 +1,19 @@
-$NetBSD: patch-setup.py,v 1.1 2019/07/02 04:19:59 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/12/15 09:07:41 adam Exp $
 
 Relax dependencies.
 
---- setup.py.orig	2019-07-02 04:14:10.000000000 +0000
+--- setup.py.orig	2020-08-20 22:45:13.000000000 +0000
 +++ setup.py
 @@ -23,11 +23,11 @@ setup(
          'textile'
      ],
      install_requires=[
--        'Jinja2==2.10',
+-        'Jinja2==2.10.1',
 -        'Markdown==2.6.11',
 -        'Pygments==2.2.0',
 -        'docutils==0.14',
 -        'six==1.11.0'
-+        'Jinja2>=2.10',
++        'Jinja2>=2.10.1',
 +        'Markdown>=2.6.11',
 +        'Pygments>=2.2.0',
 +        'docutils>=0.14',
