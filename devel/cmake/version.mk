eca59bf07526ad6f0adf65de3728d49d04dee91e
adam <adam@pkgsrc.org>
2020-12-17 12:14:35 +0000

    cmake: updated to 3.19.2
    CMake 3.19.2
    * Compiler/TI: Fix C standard flags in C-only project
    * execute_process: Improve COMMAND_ERROR_IS_FATAL error capture scenarios
    * FindHDF5: improve error messages when a location variable isn't known
    * FindHDF5: support the hdf5hl_fortran-based variable names
    * FindPython2: avoid doubling the extension in CPython2 SOABI
    * Tests: Add CMake_TEST_Java option to control Java tests
    * Tests: Teach RunCMake to ignore Xcode install_name_tool signature warnings
    * gitlab-ci: update macOS jobs to use Xcode 12.2
    * Tests: Fix RunCMake.Check* conditions for enabling Fortran
    * Check*: Tolerate variables set with names of languages
    * Ninja: Clean metadata after regen during build on Windows with 1.10.2+
    * ci: update to use ninja 1.10.2
    * Tests: Look for Visual Studio only on Windows hosts
    * CMakeDetermineCompilerId: Filter IAR-AVR format INFO strings earlier
    * CMakeDetermineCompilerId: Add whitespace to clarify logic
    * CMakeDetermineCompilerId: Tolerate stray text around INFO strings
    * Revert "Intel: Add Intel Clang compiler identification"
    * Revert "Intel: Add Intel DPC++ compiler identification"
    * Tests: Add cache entry to control XCTest deployment target
    * Tests: Remove outdated exclusion of tests on OS X 10.3
    * ci: Hard-code XCTest deployment target to 10.15
    * Utilities/Release: Add script to build Qt 5.9.9 macOS universal binaries
    * ci: update to pre-built Qt 5.9.9 universal binaries for macOS packages
    * ci: do not use sccache for macOS packaging
    * ci: update macOS package to produce universal binaries
    * macOS: Offer control over host architecture on Apple Silicon hosts
    * Tests: Cover macOS host architecture selection on Apple Silicon hosts
    * cmake: Clear INSTALL file properties between runs
    * macOS: Add /opt/homebrew to CMAKE_SYSTEM_PREFIX_PATH on Apple Silicon
    * Ninja: Remove cleandead on regeneration
    * Utilities/Release: Update macOS Qt 5.9 build for platform versioning
    * cmake-gui: Restore completion during path editing
    * Help: xref variable that cmake_minimum_required() sets
    * Help: Mention how OPTIMIZE_DEPENDENCIES is initialized
    * Help: Re-sort indexes in the manuals
    * Help: Clarify scope details of deferred call ids for cmake_language()
    * PCH: Fix compiler errors on iOS multi-arch using Ninja Multi-Config
    * export: Do not fail generation for separate namelink only case
    * Cray: Fix Cray compiler detection on new platforms
    * FPHSA: ensure it can be used outside 'find_package'
    * Clang on Windows: 'LINKER:' prefix must be honored
    * CMakePackageConfigHelpers: Relax restrictions on version range
    * CUDA: Fix user-set architectures during detection with Visual Studio
    * macOS: Add architecture flags only for native ASM dialect
    * Modules: Rename Internal/CMake{CheckCompiler => TryCompilerOrLinker}Flag
    * ISPC: Ninja properly compute ISPC_HEADER_DIRECTORY location
    * ISPC: Handle OBJECT sources in different directories
    * Document CMP0112 covers $
    * ISPC: Treat system includes as '-I' includes
    * FindGLUT: Handle Mojave and Big Sur 'tbd' framework files
    * ISPC: Generated Headers suffix configurable with a better default
    * Help: Fix presets typo (longDescription to displayName)
    * QNX: Do not disable compiler extensions for CMake itself
    * llvm-rc: Force C language for the clang gnu frontend
    * llvm-rc: Add CMAKE_VFS_OVERLAY to the preprocessing flags

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.5 2020/11/25 10:33:28 adam Exp $
+# $NetBSD: version.mk,v 1.6 2020/12/17 12:14:35 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.19.1
+CMAKE_VERSION=	3.19.2
 CMAKE_API=	${CMAKE_VERSION:R}
