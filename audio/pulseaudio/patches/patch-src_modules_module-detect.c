e70c778b140ab4771432d74fb6f9466dcc763234
ryoon <ryoon@pkgsrc.org>
2020-12-17 09:42:59 +0000

    pulseaudio: Support more /dev/audio* in OSS module
    Bump PKGREVISION for NetBSD binary change.

--- audio/pulseaudio/patches/patch-src_modules_module-detect.c
+++ audio/pulseaudio/patches/patch-src_modules_module-detect.c
@@ -1,30 +1,38 @@
-$NetBSD: patch-src_modules_module-detect.c,v 1.5 2018/07/06 14:10:15 ryoon Exp $
+$NetBSD: patch-src_modules_module-detect.c,v 1.6 2020/12/17 09:42:59 ryoon Exp $
 
 Hack to set proper sound device on NetBSD.
 
---- src/modules/module-detect.c.orig	2018-05-11 11:43:30.000000000 +0000
+--- src/modules/module-detect.c.orig	2019-07-05 00:06:47.000000000 +0000
 +++ src/modules/module-detect.c
-@@ -121,7 +121,19 @@ static int detect_alsa(pa_core *c, int j
+@@ -121,7 +121,27 @@ static int detect_alsa(pa_core *c, int j
  static int detect_oss(pa_core *c, int just_one) {
      FILE *f;
      int n = 0, b = 0;
 +    pa_module *m = NULL;
- 
++
 +#if defined(__NetBSD__)
 +/*
-+ * Assume primary sound device is used (/dev/audio).
-+ * And only primary sound device will be used (n=1).
++ * Use audio and audio0 to audio3 that are enabled by default
++ * under NetBSD.
 + */
 +    char args[64];
+ 
 +    pa_snprintf(args, sizeof(args), "mmap=0 device=/dev/audio");
 +    if (pa_module_load(&m, c, "module-oss", args) < 0)
-+	return 0;
-+    n = 1;
++      return 0;
++
++    for (int i = 0; i < 4; i++) {
++      pa_snprintf(args, sizeof(args), "mmap=0 device=/dev/audio%d", i);
++      if (pa_module_load(&m, c, "module-oss", args) < 0)
++        return i + 1;
++    }
++
++    n = 5;
 +#else /* __linux__ or __FreeBSD__ */
      if (!(f = pa_fopen_cloexec("/dev/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/asound/oss/sndstat", "r"))) {
-@@ -135,7 +147,6 @@ static int detect_oss(pa_core *c, int ju
+@@ -135,7 +155,6 @@ static int detect_oss(pa_core *c, int ju
      while (!feof(f)) {
          char line[256], args[64];
          unsigned device;
@@ -32,7 +40,7 @@ Hack to set proper sound device on NetBSD.
  
          if (!fgets(line, sizeof(line), f))
              break;
-@@ -174,6 +185,7 @@ static int detect_oss(pa_core *c, int ju
+@@ -174,6 +193,7 @@ static int detect_oss(pa_core *c, int ju
      }
  
      fclose(f);
