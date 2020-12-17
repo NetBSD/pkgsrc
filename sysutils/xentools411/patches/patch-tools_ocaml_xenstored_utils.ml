3aecf53deb5e8c10fd737542f51f5231528b148c
bouyer <bouyer@pkgsrc.org>
2020-12-17 16:48:12 +0000

    Add upstream patches for a bunch of Xen security avisories, related to xenstore permissions. Bump PKGREVISION

--- sysutils/xentools411/patches/patch-tools_ocaml_xenstored_utils.ml
+++ sysutils/xentools411/patches/patch-tools_ocaml_xenstored_utils.ml
@@ -1,13 +1,13 @@
-$NetBSD: patch-tools_ocaml_xenstored_utils.ml,v 1.1 2018/07/24 13:40:11 bouyer Exp $
+$NetBSD: patch-tools_ocaml_xenstored_utils.ml,v 1.2 2020/12/17 16:48:12 bouyer Exp $
 
---- ./tools/ocaml/xenstored/utils.ml.orig	2018-07-09 15:47:19.000000000 +0200
-+++ ./tools/ocaml/xenstored/utils.ml	2018-07-16 13:50:03.000000000 +0200
+--- tools/ocaml/xenstored/utils.ml.orig	2020-12-17 15:47:15.866790468 +0100
++++ tools/ocaml/xenstored/utils.ml	2020-12-17 15:53:47.618682147 +0100
 @@ -86,7 +86,7 @@
  	let buf = Bytes.make 20 '\000' in
  	let sz = Unix.read fd buf 0 20 in
  	Unix.close fd;
 -	int_of_string (Bytes.sub_string buf 0 sz)
-+	int_of_string (String.trim (String.sub buf 0 sz))
++	int_of_string (String.trim (Bytes.sub_string buf 0 sz))
  
- let path_complete path connection_path =
- 	if String.get path 0 <> '/' then
+ (* @path may be guest data and needs its length validating.  @connection_path
+  * is generated locally in xenstored and always of the form "/local/domain/$N/" *)
