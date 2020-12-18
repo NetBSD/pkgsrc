e42e0972a234201bd1b91430f2dc8d0ed3d05acd
nia <nia@pkgsrc.org>
2020-12-18 19:58:31 +0000

    lua54: Update to 5.4.2.
    this is a bugfix release, see http://www.lua.org/work/

--- lang/lua54/version.mk
+++ lang/lua54/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2020/10/14 16:42:59 wiz Exp $
+# $NetBSD: version.mk,v 1.3 2020/12/18 19:58:31 nia Exp $
 
-LUA_VERSION=	5.4.1
+LUA_VERSION=	5.4.2
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
