b55d86be399d447867890c6ec051b6c01c1dbcb0
nia <nia@pkgsrc.org>
2020-12-18 23:33:05 +0000

    xine-lib: Update to 1.2.11
    xine-lib (1.2.11) 2020-12-06
      * Add gopher input plugin.
      * Add user settings for IPv6.
      * Split a52 spdif output to separate decoder.
      * Code cleanup.
      * More engine optimization.
      * Small optimizations (opengl2, dvb subtitles, png, mpeg-ts).
      * Better A/V sync.
      * Fix a few crashes.
      * Fix bluray playback.
      * Fix VAAPI picture jitter and green flashes.
      * Fix Opus and EAC3 audio playback.
      * Fix text subtitles.
      * Fix mpeg-ts (hdmv mode, detection of cut TV recordings).
      * Fix mpeg layer 1-4 audio timing.
      * Fix some VDR issues.
      * Fix HLS VOD mode.
      * Many small fixes.
      * Build fixes.
      * BSD fixes.
      * Update german translation.
      * Update internal libdca to upstream 0.0.7.

--- multimedia/xine-lib/patches/patch-include_xine_xineutils.h
+++ multimedia/xine-lib/patches/patch-include_xine_xineutils.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-include_xine_xineutils.h,v 1.1 2020/04/12 11:09:05 nia Exp $
+$NetBSD: patch-include_xine_xineutils.h,v 1.2 2020/12/18 23:33:06 nia Exp $
 
 Don't reimplement memcpy. Actually not compatible with clang:
 
 ../../include/xine/xineutils.h:211:63: error: fields must have a constant size:
 'variable length array in structure' extension will never be supported
 
---- include/xine/xineutils.h.orig	2019-12-13 20:47:50.000000000 +0000
+--- include/xine/xineutils.h.orig	2020-11-19 10:30:37.000000000 +0000
 +++ include/xine/xineutils.h
-@@ -200,26 +200,6 @@ extern void *(* xine_fast_memcpy)(void *
+@@ -200,35 +200,6 @@ extern void *(* xine_fast_memcpy)(void *
  /* len (usually) < 500, but not a build time constant. */
  #define xine_small_memcpy(xsm_to,xsm_from,xsm_len) memcpy (xsm_to, xsm_from, xsm_len)
  
@@ -17,12 +17,21 @@ Don't reimplement memcpy. Actually not compatible with clang:
 -static inline void *xine_small_memcpy (void *to, const void *from, size_t len) {
 -  void *t2 = to;
 -  size_t l2 = len;
+-#    if !defined(__clang__)
 -  __asm__ __volatile__ (
 -    "cld\n\trep movsb"
 -    : "=S" (from), "=D" (t2), "=c" (l2), "=m" (*(struct {char foo[len];} *)to)
 -    : "0"  (from), "1"  (t2), "2"  (l2)
 -    : "cc"
 -  );
+-#    else /* clang dislikes virtual variable size struct */
+-  __asm__ __volatile__ (
+-      "cld\n\trep movsb"
+-      : "=S" (from), "=D" (t2), "=c" (l2)
+-      : "0"  (from), "1"  (t2), "2"  (l2)
+-      : "cc", "memory"
+-  );
+-#    endif
 -  (void)from;
 -  (void)t2;
 -  (void)l2;
