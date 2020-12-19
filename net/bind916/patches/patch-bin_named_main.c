488a151389efa3f9d4fb8e23e72c67219f78ec76
taca <taca@pkgsrc.org>
2020-12-19 16:41:36 +0000

    net/bind916: update to 9.16.10
    Update bind916 to 9.16.10 (BIND 9.16.10).
    
    pkgsrc changes:
    
    * Make blocklist/blacklist support really compiled in.
    * Fix build problem with pkcs11 PKG_OPTIONS and allow to use it.
    
    
    	--- 9.16.10 released ---
    
    5544.	[func]		Restore the default value of "nocookie-udp-size" to 4096
    			bytes. [GL #2250]
    
    5541.	[func]		Adjust the "max-recursion-queries" default from 75 to
    			100. [GL #2305]
    
    5540.	[port]		Fix building with native PKCS#11 support for AEP Keyper.
    			[GL #2315]
    
    5539.	[bug]		Tighten handling of missing DNS COOKIE responses over
    			UDP by falling back to TCP. [GL #2275]
    
    5538.	[func]		Add NSEC3 support to KASP. A new option for
    			"dnssec-policy", "nsec3param", can be used to set the
    			desired NSEC3 parameters. NSEC3 salt collisions are
    			automatically prevented during resalting. Salt
    			generation is now logged with zone context. [GL #1620]
    
    5534.	[bug]		The CNAME synthesized from a DNAME was incorrectly
    			followed when the QTYPE was CNAME or ANY. [GL #2280]

--- net/bind916/patches/patch-bin_named_main.c
+++ net/bind916/patches/patch-bin_named_main.c
@@ -1,25 +1,25 @@
-$NetBSD: patch-bin_named_main.c,v 1.1 2020/08/09 15:20:22 taca Exp $
+$NetBSD: patch-bin_named_main.c,v 1.2 2020/12/19 16:41:36 taca Exp $
 
-* Take from NetBSD base including support for blacklistd.
+* Based on NetBSD, add support for blocklist(blacklist).
 
---- bin/named/main.c.orig	2020-06-10 21:01:43.000000000 +0000
+--- bin/named/main.c.orig	2020-12-07 08:16:53.000000000 +0000
 +++ bin/named/main.c
-@@ -94,6 +94,10 @@
+@@ -95,6 +95,10 @@
  #ifdef HAVE_ZLIB
  #include <zlib.h>
  #endif /* ifdef HAVE_ZLIB */
-+#ifdef HAVE_BLACKLIST
++#if defined(HAVE_BLACKLIST_H) || defined(HAVE_BLOCKLIST_H)
 +#include <ns/pfilter.h>
-+#endif /* ifdef HAVE_BLACKLIST */
++#endif
 +
  /*
   * Include header files for database drivers here.
   */
-@@ -1529,6 +1533,10 @@ main(int argc, char *argv[]) {
+@@ -1535,6 +1539,10 @@ main(int argc, char *argv[]) {
  
  	parse_command_line(argc, argv);
  
-+#ifdef HAVE_BLACKLIST
++#if defined(HAVE_BLACKLIST_H) || defined(HAVE_BLOCKLIST_H)
 +	pfilter_enable();
 +#endif
 +
