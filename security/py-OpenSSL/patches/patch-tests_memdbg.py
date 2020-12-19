6c15a6a2d8bf2a189965ae6b296534c610f4f93f
leot <leot@pkgsrc.org>
2020-12-19 11:52:26 +0000

    py-OpenSSL: Update to 20.0.1
    Changes:
    20.0.1 (2020-12-15)
    -------------------
    Backward-incompatible changes:
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    Deprecations:
    ^^^^^^^^^^^^^
    
    Changes:
    ^^^^^^^^
    - Fixed compatibility with OpenSSL 1.1.0.
    
    20.0.0 (2020-11-27)
    -------------------
    Backward-incompatible changes:
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    - The minimum ``cryptography`` version is now 3.2.
    - Remove deprecated ``OpenSSL.tsafe`` module.
    - Removed deprecated ``OpenSSL.SSL.Context.set_npn_advertise_callback``, ``OpenSSL.SSL.Context.set_npn_select_callback``, and ``OpenSSL.SSL.Connection.get_next_proto_negotiated``.
    - Drop support for Python 3.4
    - Drop support for OpenSSL 1.0.1 and 1.0.2
    
    Deprecations:
    ^^^^^^^^^^^^^
    - Deprecated ``OpenSSL.crypto.loads_pkcs7`` and ``OpenSSL.crypto.loads_pkcs12``.
    
    Changes:
    ^^^^^^^^
    - Added a new optional ``chain`` parameter to ``OpenSSL.crypto.X509StoreContext()``
      where additional untrusted certificates can be specified to help chain building.
      `#948 <https://github.com/pyca/pyopenssl/pull/948>`_
    - Added ``OpenSSL.crypto.X509Store.load_locations`` to set trusted
      certificate file bundles and/or directories for verification.
      `#943 <https://github.com/pyca/pyopenssl/pull/943>`_
    - Added ``Context.set_keylog_callback`` to log key material.
      `#910 <https://github.com/pyca/pyopenssl/pull/910>`_
    - Added ``OpenSSL.SSL.Connection.get_verified_chain`` to retrieve the
      verified certificate chain of the peer.
      `#894 <https://github.com/pyca/pyopenssl/pull/894>`_.
    - Make verification callback optional in ``Context.set_verify``.
      If omitted, OpenSSL's default verification is used.
      `#933 <https://github.com/pyca/pyopenssl/pull/933>`_
    - Fixed a bug that could truncate or cause a zero-length key error due to a
      null byte in private key passphrase in ``OpenSSL.crypto.load_privatekey``
      and ``OpenSSL.crypto.dump_privatekey``.
      `#947 <https://github.com/pyca/pyopenssl/pull/947>`_

--- security/py-OpenSSL/patches/patch-tests_memdbg.py
+++ security/py-OpenSSL/patches/patch-tests_memdbg.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-tests_memdbg.py,v 1.2 2017/05/09 16:49:07 adam Exp $
+$NetBSD: patch-tests_memdbg.py,v 1.3 2020/12/19 11:52:26 leot Exp $
 
 Fix running test on NetBSD.
 https://github.com/pyca/pyopenssl/issues/595
 
---- tests/memdbg.py.orig	2016-02-15 10:25:58.000000000 +0000
+--- tests/memdbg.py.orig	2020-12-15 15:30:54.000000000 +0000
 +++ tests/memdbg.py
-@@ -18,16 +18,16 @@ _ffi.cdef(
- 
-     int  CRYPTO_set_mem_functions(void *(*m)(size_t),void *(*r)(void *,size_t), void (*f)(void *));
+@@ -19,9 +19,9 @@ _ffi.cdef(
+     int  CRYPTO_set_mem_functions(
+         void *(*m)(size_t),void *(*r)(void *,size_t), void (*f)(void *));
  
 -    int backtrace(void **buffer, int size);
 -    char **backtrace_symbols(void *const *buffer, int size);
@@ -15,14 +15,15 @@ https://github.com/pyca/pyopenssl/issues/595
 +    size_t backtrace(void **buffer, size_t size);
 +    char **backtrace_symbols(void *const *buffer, size_t size);
 +    void backtrace_symbols_fd(void *const *buffer, size_t size, int fd);
-     """)  # noqa
- _api = _ffi.verify(
      """
-     #include <openssl/crypto.h>
+ )  # noqa
+ _api = _ffi.verify(
+@@ -30,7 +30,7 @@ _api = _ffi.verify(
      #include <stdlib.h>
      #include <execinfo.h>
--    """, libraries=["crypto"])
-+    """, libraries=["crypto", "execinfo"] if sys.platform.startswith('netbsd') else ["crypto"])
+     """,
+-    libraries=["crypto"],
++    libraries=["crypto", "execinfo"] if sys.platform.startswith('netbsd') else ["crypto"],
+ )
  C = _ffi.dlopen(None)
  
- verbose = False
