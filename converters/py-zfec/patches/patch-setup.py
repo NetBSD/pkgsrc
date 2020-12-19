e80ca7d3f1f0da9d36f60aab818b3d7c226acf82
gdt <gdt@pkgsrc.org>
2020-12-19 15:28:05 +0000

    py-zfec: Update to 1.5.4
    Upstream changes relative to 1.5.3 are negligible.

--- converters/py-zfec/patches/patch-setup.py
+++ converters/py-zfec/patches/patch-setup.py
@@ -1,13 +1,16 @@
-$NetBSD: patch-setup.py,v 1.1 2020/08/27 12:28:06 joerg Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/12/19 15:28:05 gdt Exp $
 
---- setup.py.orig	2020-08-26 11:55:49.857831827 +0000
+\todo Document this patch.
+\todo File this patch upstream.
+
+--- setup.py.orig	2020-09-18 02:16:19.000000000 +0000
 +++ setup.py
 @@ -57,7 +57,7 @@ setup(
      name="zfec",
      version=versioneer.get_version(),
      description="An efficient, portable erasure coding tool",
--    long_description=open('README.rst', 'rU').read(),
+-    long_description=open('README.rst', 'r').read(),
 +    long_description=open('README.rst', 'rU', **({'encoding': 'UTF-8'} if sys.version_info.major>=3 else {})).read(),
      url="https://github.com/tahoe-lafs/zfec",
      install_requires=[
-         "pyutil >= 3.0.0",
+         "argparse >= 0.8",
