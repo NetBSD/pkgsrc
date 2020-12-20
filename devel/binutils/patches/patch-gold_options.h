fe19de9b0d2699c30ef3d51ad05dc801a26d6703
fcambus <fcambus@pkgsrc.org>
2020-12-20 19:07:05 +0000

    binutils: remove now unneeded OpenBSD specific checks in gold.
    Support for DT_INIT_ARRAY, DT_FINI_ARRAY and DT_PREINIT_ARRAY was added
    in 2016 [1], and support for DT_RUNPATH in 2017 [2].
    
    [1] https://marc.info/?l=openbsd-cvs&m=147193481229072&w=2
    [2] https://marc.info/?l=openbsd-cvs&m=148504804104054&w=2

--- devel/binutils/patches/patch-gold_options.h
+++ devel/binutils/patches/patch-gold_options.h
@@ -1,8 +1,9 @@
-$NetBSD: patch-gold_options.h,v 1.4 2020/12/06 18:07:53 fcambus Exp $
+$NetBSD: patch-gold_options.h,v 1.5 2020/12/20 19:07:05 fcambus Exp $
 
- The NetBSD and OpenBSD real-time linkers do not understand DT_RUNPATH
- or DT_INIT_ARRAY.  The current gold defaults are not suitable for them.
- The last couple of DragonFly and FreeBSD releases do support these tags.
+ The NetBSD real-time linker do not understand DT_RUNPATH or
+ DT_INIT_ARRAY.  The current gold defaults are not suitable for it.
+ The last couple of DragonFly, FreeBSD, and OpenBSD releases do
+ support these tags.
 
 --- gold/options.h.orig	2020-07-24 09:12:20.000000000 +0000
 +++ gold/options.h
@@ -10,7 +11,7 @@ $NetBSD: patch-gold_options.h,v 1.4 2020/12/06 18:07:53 fcambus Exp $
  	      N_("Output cross reference table"),
  	      N_("Do not output cross reference table"));
  
-+#if defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__NetBSD__)
 +  DEFINE_bool(ctors_in_init_array, options::TWO_DASHES, '\0', false,
 +#else
    DEFINE_bool(ctors_in_init_array, options::TWO_DASHES, '\0', true,
@@ -22,7 +23,7 @@ $NetBSD: patch-gold_options.h,v 1.4 2020/12/06 18:07:53 fcambus Exp $
  	      N_("Do not create exception frame header"));
  
    // Alphabetized under 'e' because the option is spelled --enable-new-dtags.
-+#if defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__NetBSD__)
 +  DEFINE_enable(new_dtags, options::EXACTLY_TWO_DASHES, '\0', false,
 +#else
    DEFINE_enable(new_dtags, options::EXACTLY_TWO_DASHES, '\0', true,
