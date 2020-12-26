d9ce991e78631028aa0d6628614c217fb026b25e
nia <nia@pkgsrc.org>
2020-12-26 23:39:30 +0000

    librsvg: Restore default of librsvg-c on ARM
    Avoid rust version in pbulk when C version is selected
    
    Follows comments by gdt by email

--- graphics/librsvg/available.mk
+++ graphics/librsvg/available.mk
@@ -1,4 +1,4 @@
-# $NetBSD: available.mk,v 1.3 2020/12/26 10:02:39 nia Exp $
+# $NetBSD: available.mk,v 1.4 2020/12/26 23:39:30 nia Exp $
 #
 # This mk fragment define LIBRSVG_TYPE variable used in other librsvg mk
 # fragments.
@@ -19,7 +19,8 @@ LIBRSVG_AVAILABLE_MK=	defined
 
 .include "../../lang/rust/platform.mk"
 # https://mail-index.netbsd.org/tech-pkg/2020/06/16/msg023380.html
-.if !empty(PLATFORM_SUPPORTS_RUST:M[Yy][Ee][Ss])
+.if !empty(PLATFORM_SUPPORTS_RUST:M[Yy][Ee][Ss]) && \
+    empty(MACHINE_PLATFORM:MNetBSD-*-earm*)
 LIBRSVG_TYPE?=	rust
 .else
 LIBRSVG_TYPE?=	c
