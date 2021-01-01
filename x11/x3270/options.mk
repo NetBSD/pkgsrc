3d048963af6c935b3af367d92402d28ea4806e16
rhialto <rhialto@pkgsrc.org>
2021-01-01 13:05:22 +0000

    x11/x3270: update to 4.0ga13.
    Includes the curses version of the terminal emulator now.
    
    Changelogs are quite fragmented over various alpha/beta versions; here
    are some main points.
    
    Major changes
    Task engine
    
    The scripting infrastructure has been rewritten. It is now called the task engine. There are no longer any artificial dependencies between actions initiated by different sources (keyboard, scripts, HTTP requests, etc.). Accidental blocking behavior has been (almost) eliminated. Unrelated operations are now properly concurrent, within the limitations of there still being just one emulated terminal and host session to control. For example, c3270 and wc3270 now continue to respond to host messages while at the prompt or while a printer dialog is showing.
    
    Tasks also carry context consistently now. For example, the Wait(Output) now works with HTTP requests.
    
    The state of the task engine can be displayed with the Tasks query.
    b3270
    
    A new emulator has been created, b3270. It is a common back-end process that handles the 3270 protocol and host interactions, communicating with different user-interface implementations using a simple XML-based protocol. None of the existing emulators have been converted to use b3270, but a new Windows emulator (wx3270) uses it.
    x3270 prompt
    
    The c3270 prompt has been generalized and is available to all of the 3270 emulators as the x3270 prompt. The Prompt() action (or a menu option) creates a new instance of the prompt in a window. The prompt now uses color to distinguish error messages.
    tcl3270 re-implementation
    
    tcl3270 has been re-implemented as a simple wrapper around s3270. This eliminates many of the arbitrary limitations and blocking behaviors of the previous implementation.
    Show() / Query() merge and expansion
    
    The common Query() action and the c3270/wc3270-specific Show() action have been merged and expanded. Show() is an alias for Query(), and many queries have been added or changed:
    
    (omitted)
    
    Set()/Toggle() changes
    
    A new Set() action has been added, which is similar to the Toggle() action. The difference between Set() and Toggle() happens when no value is specified for a setting. Without a value specified, Set() will display the current value; Toggle() will (if the setting is Boolean) invert the setting's value. Both actions list the current values of all settings if no parameters are given.
    
    Many additional resources can now be changed at run-time via Set() or Toggle().
    
    These previously-deprecated resources were removed:
    
        cursorPos (assumed true now)
        dsTrace
        eventTrace
        dftBufferSize
    
    x3270 APL mode
    
    APL keyboard operation on x3270 has changed. There is no longer a fixed APL keymap. Instead, the key combination Shift-Escape toggles a new APL mode, which changes how how paste operations are handled (preferring the APL definitions for Unicode characters that have both APL and non-APL mappings), as well as setting a temporary keyboard map and a temporary compose map.
    
    The separate APL host code page has been removed; APL text can be displayed using any host code page.
    
    
    after this, lots of bug fixes and minor tweaks, as far as I can tell.

---
+++ x11/x3270/options.mk
@@ -0,0 +1,36 @@
+# $NetBSD: options.mk,v 1.1 2021/01/01 13:05:22 rhialto Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.x3270
+PKG_SUPPORTED_OPTIONS=	curses x11 tcl
+PKG_SUGGESTED_OPTIONS+=	curses x11
+PLIST_VARS=		curses x11 tcl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcurses)
+.include "../../mk/curses.buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-c3270
+PLIST.curses=		yes
+.else
+CONFIGURE_ARGS+=	--disable-c3270
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libXaw/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/xbitmaps/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-x3270
+CONFIGURE_ARGS+=	--enable-x3270if
+PLIST.x11=		yes
+.else
+CONFIGURE_ARGS+=	--disable-x3270
+CONFIGURE_ARGS+=	--disable-x3270if
+.endif
+
+.if !empty(PKG_OPTIONS:Mtcl)
+.include "../../lang/tcl/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-tcl3270
+PLIST.tcl=		yes
+.else
+CONFIGURE_ARGS+=	--disable-tcl3270
+.endif
