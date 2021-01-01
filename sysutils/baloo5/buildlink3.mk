bfac3f1b42b7e6b2686693e088814d3518169890
ryoon <ryoon@pkgsrc.org>
2021-01-01 09:52:09 +0000

    *: Recursive revbump from audio/pulseaudio-14.0

--- sysutils/baloo5/buildlink3.mk
+++ sysutils/baloo5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2020/12/04 04:56:13 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2021/01/01 09:52:12 ryoon Exp $
 
 BUILDLINK_TREE+=	baloo5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	baloo5
 BALOO5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.baloo5+=	baloo5>=5.21.0
-BUILDLINK_ABI_DEPENDS.baloo5?=	baloo5>=5.70.0nb4
+BUILDLINK_ABI_DEPENDS.baloo5?=	baloo5>=5.70.0nb5
 BUILDLINK_PKGSRCDIR.baloo5?=	../../sysutils/baloo5
 
 .include "../../databases/lmdb/buildlink3.mk"
