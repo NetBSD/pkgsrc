35ba739356d93efd9a24551e68d81ab773aeba72
adam <adam@pkgsrc.org>
2021-01-03 22:41:44 +0000

    py-sanic: updated to 20.12.0
    Version 20.12.0
    
    Features
    
    * Static route more verbose if file not found
    * Fix static routes registration on a blueprint
    * Add Python 3.9 support
    * Sanic CLI upgrade
    * Update aiofile version requirements
    * Update multidict version requirements
    * Add py.typed file
    * Speed optimization in request handler
    * Add app registry and Sanic class level app retrieval
    
    Bugfixes
    
    * Fix Chunked Transport-Encoding in ASGI streaming
    response
    
    Deprecations and Removals
    
    * Cleanup and remove deprecated code
    
    Developer infrastructure
    
    * Fix load module test
    * Transition Travis from .org to .com
    * Update tox requirements
    
    Improved Documentation
    
    * Documentation improvements
    * Remove duplicate contents in testing.rst
    * Fix typo in routing.rst

--- www/py-sanic/patches/patch-setup.py
+++ www/py-sanic/patches/patch-setup.py
@@ -1,17 +1,26 @@
-$NetBSD: patch-setup.py,v 1.8 2020/10/27 23:07:42 adam Exp $
+$NetBSD: patch-setup.py,v 1.9 2021/01/03 22:41:44 adam Exp $
 
 Allow newer versions.
 
---- setup.py.orig	2020-10-25 13:09:05.000000000 +0000
+--- setup.py.orig	2020-12-28 21:58:53.000000000 +0000
 +++ setup.py
-@@ -80,8 +80,8 @@ requirements = [
-     ujson,
-     "aiofiles>=0.3.0",
+@@ -89,15 +89,15 @@ requirements = [
+     "aiofiles>=0.6.0",
      "websockets>=8.1,<9.0",
--    "multidict==5.0.0",
+     "multidict>=5.0,<6.0",
 -    "httpx==0.15.4",
-+    "multidict>=5.0.0",
 +    "httpx>=0.15.4",
  ]
  
  tests_require = [
+-    "pytest==5.2.1",
++    "pytest>=5.2.1",
+     "multidict>=5.0,<6.0",
+-    "gunicorn==20.0.4",
++    "gunicorn>=20.0.4",
+     "pytest-cov",
+-    "httpcore==0.11.*",
++    "httpcore>=0.11.*",
+     "beautifulsoup4",
+     uvloop,
+     ujson,
