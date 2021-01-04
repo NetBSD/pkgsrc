82cb38583e637807d6bdf9213526598a18900853
adam <adam@pkgsrc.org>
2021-01-04 08:06:51 +0000

    prometheus: provide React UI

---
+++ databases/prometheus/files/react/precache-manifest.600e18226bdf38566b4336cf73cd409f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff7ab42bb5329b9dd2375d2c996e75a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "350954693135d610d66d",
+    "url": "./static/css/2.3cee880a.chunk.css"
+  },
+  {
+    "revision": "6276c18d91772fc9798e",
+    "url": "./static/css/main.1087ae1e.chunk.css"
+  },
+  {
+    "revision": "350954693135d610d66d",
+    "url": "./static/js/2.57113e20.chunk.js"
+  },
+  {
+    "revision": "8a14a1fd602a4d3db7d556b3c3a28860",
+    "url": "./static/js/2.57113e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6276c18d91772fc9798e",
+    "url": "./static/js/main.ca4ac742.chunk.js"
+  },
+  {
+    "revision": "7caf0cf1c289c5c9d7be",
+    "url": "./static/js/runtime-main.7e00e584.js"
+  }
+]);
