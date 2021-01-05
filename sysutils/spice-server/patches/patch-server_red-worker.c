9c798c1fcc42d402b7589096599464c9eeececfe
jperkin <jperkin@pkgsrc.org>
2021-01-05 15:14:38 +0000

    spice-server: Fix build on SunOS.

--- sysutils/spice-server/patches/patch-server_red-worker.c
+++ sysutils/spice-server/patches/patch-server_red-worker.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-server_red-worker.c,v 1.2 2020/11/01 19:36:37 maya Exp $
+$NetBSD: patch-server_red-worker.c,v 1.3 2021/01/05 15:14:39 jperkin Exp $
 
 Add NetBSD support.
 
---- server/red-worker.c.orig	2019-04-30 08:51:11.000000000 +0000
+--- server/red-worker.c.orig	2020-02-27 11:26:12.000000000 +0000
 +++ server/red-worker.c
 @@ -1159,7 +1159,11 @@ bool red_worker_run(RedWorker *worker)
  #ifndef _WIN32
@@ -10,7 +10,7 @@ Add NetBSD support.
  #endif
 +#if defined(__NetBSD__)
 +    pthread_setname_np(worker->thread, "%s", (void *)"SPICE Worker");
-+#else
++#elif !defined(__sun)
      pthread_setname_np(worker->thread, "SPICE Worker");
 +#endif
  
