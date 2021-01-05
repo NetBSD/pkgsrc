d4427fe907979f46d0e50c7c6e1a8c18027e2f1a
nia <nia@pkgsrc.org>
2021-01-05 11:01:49 +0000

    dosbox-x: Don't assume anything that 80-bit long double.
    Add missing SDL_net dependency.
    
    bump PKGREVISION.

--- emulators/dosbox-x/patches/patch-include_dosbox.h
+++ emulators/dosbox-x/patches/patch-include_dosbox.h
@@ -1,17 +1,23 @@
-$NetBSD: patch-include_dosbox.h,v 1.1 2020/11/19 17:17:15 nia Exp $
+$NetBSD: patch-include_dosbox.h,v 1.2 2021/01/05 11:01:49 nia Exp $
 
-Only attempt to use long double on x86.
+GCC having 80-bit long double is far from a safe assumption.
 
---- include/dosbox.h.orig	2020-11-01 15:36:10.000000000 +0000
+--- include/dosbox.h.orig	2020-12-01 02:49:53.000000000 +0000
 +++ include/dosbox.h
-@@ -72,9 +72,7 @@
- // TODO: The autoconf script should test the size of long double
- #if defined(_MSC_VER)
- // Microsoft C++ sizeof(long double) == sizeof(double)
+@@ -69,16 +69,6 @@
+ # define pref_struct_stat	struct stat
+ #endif
+ 
+-// TODO: The autoconf script should test the size of long double
+-#if defined(_MSC_VER)
+-// Microsoft C++ sizeof(long double) == sizeof(double)
 -#elif defined(__arm__)
 -// ARMv7 (Raspberry Pi) does not have long double, sizeof(long double) == sizeof(double)
 -#else
-+#elif C_TARGETCPU == X86_64 || C_TARGETCPU == X86
- // GCC, other compilers, have sizeof(long double) == 10 80-bit IEEE
- # define HAS_LONG_DOUBLE		1
- #endif
+-// GCC, other compilers, have sizeof(long double) == 10 80-bit IEEE
+-# define HAS_LONG_DOUBLE		1
+-#endif
+-
+ GCC_ATTRIBUTE(noreturn) void		E_Exit(const char * format,...) GCC_ATTRIBUTE( __format__(__printf__, 1, 2));
+ 
+ typedef Bits cpu_cycles_count_t;
