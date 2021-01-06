22aa4f77212db43c178d41d1ad60c392760e5413
triaxx <triaxx@pkgsrc.org>
2021-01-06 11:21:40 +0000

    firefox: Fix nss dependency

--- www/firefox/mozilla-common.mk
+++ www/firefox/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.192 2020/12/31 20:04:13 nia Exp $
+# $NetBSD: mozilla-common.mk,v 1.193 2021/01/06 11:21:40 triaxx Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -161,7 +161,7 @@ BUILDLINK_API_DEPENDS.libevent+=	libevent>=1.1
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.26
 .include "../../devel/nspr/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.59
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.59.1
 .include "../../devel/nss/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 #.include "../../mk/jpeg.buildlink3.mk"
