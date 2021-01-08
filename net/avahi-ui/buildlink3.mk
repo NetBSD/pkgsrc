fafb7b5d727c3f9b2a1416c7939cc93e670b9ed9
nia <nia@pkgsrc.org>
2021-01-08 14:08:35 +0000

    avahi: Split off package into *-ui variant for the GTK UIs.
    thou shalt not add a confusing amount of PKG_OPTIONS to a library.

---
+++ net/avahi-ui/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/01/08 14:08:35 nia Exp $
+
+BUILDLINK_TREE+=	avahi-ui
+
+.if !defined(AVAHI_UI_BUILDLINK3_MK)
+AVAHI_UI_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.avahi-ui+=	avahi-ui>=0.8
+BUILDLINK_PKGSRCDIR.avahi-ui?=	../../net/avahi-ui
+
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../net/avahi/buildlink3.mk"
+.endif	# AVAHI_UI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-avahi-ui
