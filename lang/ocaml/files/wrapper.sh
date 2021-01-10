29ba58e98003b5b35a0e6c5fef99268f36d5cba8
jperkin <jperkin@pkgsrc.org>
2021-01-10 11:13:33 +0000

    ocaml: Try to fix wrapper script.
    Passing LDFLAGS verbatim no longer works, prefix each of them with -ccopt,
    this seems to work across more ocaml binaries than -ldopt.
    
    Tested across a number of packages that previously failed.

--- lang/ocaml/files/wrapper.sh
+++ lang/ocaml/files/wrapper.sh
@@ -1,25 +1,22 @@
 #!@SH@
+#
+# $NetBSD: wrapper.sh,v 1.4 2021/01/10 11:13:33 jperkin Exp $
 
-# $NetBSD: wrapper.sh,v 1.3 2016/02/29 13:51:28 jperkin Exp $
-
-BINDIR='@OCAML_PREFIX@'
 CFLAGS='@CFLAGS@'
 LDFLAGS='@LDFLAGS@'
 WRAPPEE='@WRAPPEE@'
 
-if echo "$@" | grep ' -c ' >/dev/null || [ "${WRAPPEE}" = "ocamlmklib" ]; then
-	flags="${CFLAGS}"
-else
-	flags="${CFLAGS} ${LDFLAGS}"
-fi
-
-for f in ${flags}
-do
-	MLFLAGS="${MLFLAGS} -ccopt ${f}"
+for flag in ${CFLAGS}; do
+	cflags="${cflags} -ccopt ${flag}"
+done
+for flag in ${LDFLAGS}; do
+	ldflags="${ldflags} -ccopt ${flag}"
 done
 
-if [ "${WRAPPEE}" = "ocamlmklib" ]; then
-	MLFLAGS="${MLFLAGS} ${LDFLAGS}"
+if echo "$@" | grep ' -c ' >/dev/null; then
+	MLFLAGS="${cflags}"
+else
+	MLFLAGS="${cflags} ${ldflags}"
 fi
 
 exec "@OCAML_PREFIX@/bin/${WRAPPEE}" ${MLFLAGS} "$@"
