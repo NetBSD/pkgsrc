fb8ffbc7fef700a190deb8c546378c48de5b20bd
schmonz <schmonz@pkgsrc.org>
2021-01-11 18:31:10 +0000

    Update to 2.4.0.0. From the changelog:
    - Can be built against OpenSSL + libretls.
    - execline is now optional.
    - s6-tlsc and s6-tlsd rewrite. They're now wrappers around new
      binaries: s6-tlsc-io and s6-tlsd-io, which establish and run a
      TLS tunnel over already existing fds.
    - New functionality: s6-ucspitlsc and s6-ucspitlsd, for an
      implementation of delayed encryption.
    
    pkgsrc changes:
    
    - Switch from BearSSL to LibreTLS (atop OpenSSL).
    - Turn 'execline' option off by default.

--- net/s6-networking/options.mk
+++ net/s6-networking/options.mk
@@ -1,14 +1,39 @@
-# $NetBSD: options.mk,v 1.1 2020/11/22 11:33:22 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2021/01/11 18:31:10 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.s6-networking
-PKG_SUPPORTED_OPTIONS+=	tls
+PKG_SUPPORTED_OPTIONS+=	execline tls
 PKG_SUGGESTED_OPTIONS+=	tls
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Mexecline)
+.  include "../../lang/execline/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-execline
+.endif
+
 PLIST_VARS=		tls
 .if !empty(PKG_OPTIONS:Mtls)
 PLIST.tls=		yes
-.  include "../../security/bearssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-ssl=bearssl
+.  include "../../security/libretls/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-ssl=libressl
+
+PKG_USERS_VARS+=	UCSPI_SSL_USER
+PKG_GROUPS_VARS+=	UCSPI_SSL_GROUP
+PKG_GROUPS+=		${UCSPI_SSL_GROUP}
+PKG_USERS+=		${UCSPI_SSL_USER}:${UCSPI_SSL_GROUP}
+
+SUBST_CLASSES+=		paths
+SUBST_STAGE.paths=	pre-configure
+SUBST_FILES.paths=	s6-pkgsrc-cadir
+SUBST_VARS.paths=	SH SETENV SSLDIR
+
+post-extract:
+	for f in s6-pkgsrc-cadir; do \
+		${CP} ${FILESDIR}/$$f.sh ${WRKSRC}/$$f; \
+	done
+post-install:
+	for f in s6-pkgsrc-cadir; do \
+		${INSTALL_SCRIPT} ${WRKSRC}/$$f ${DESTDIR}${PREFIX}/bin/$$f; \
+	done
 .endif
