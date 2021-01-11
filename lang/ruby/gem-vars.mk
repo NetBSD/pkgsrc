3366dd777ca038099a46729940a22e9f3b441acb
taca <taca@pkgsrc.org>
2021-01-11 11:21:31 +0000

    lang/ruby: add GEM_PLUGINSDIR support
    * Add GEM_PLUGINSDIR support required by rubygems 3.2 and later.
    * GEM_EXTSDIR is required unconditonally.

--- lang/ruby/gem-vars.mk
+++ lang/ruby/gem-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gem-vars.mk,v 1.11 2021/01/11 06:51:27 taca Exp $
+# $NetBSD: gem-vars.mk,v 1.12 2021/01/11 11:21:31 taca Exp $
 #
 # This Makefile fragment defines various make(1) variables for Ruby gems
 # support.
@@ -21,10 +21,15 @@ RUBYGEM=	${LOCALBASE}/bin/${RUBYGEM_NAME}
 
 GEM_EXTSDIR_NEEDS_SUBDIR?=	yes
 
+GEM_PLUGINSDIR=	${GEM_HOME}/plugins
+
 .if exists(${RUBYGEM})
 RUBY_GEM_ARCH!=	${RUBYGEM} environment platform | ${SED} -e 's|.*:||'
+RUBY_GEM_VER!= ${RUBYGEM} --version
 
 GEM_EXTSBASE=	${GEM_HOME}/extensions
+PLIST_SUBST+=	GEM_EXTSDIR=${GEM_EXTSDIR}
+PLIST_SUBST+=	GEM_PLUGINSDIR=${GEM_PLUGINSDIR}
 
 .  if !empty(GEM_EXTSDIR_NEEDS_SUBDIR:M[nN][oO])
 GEM_EXTSDIR=	${GEM_EXTSBASE}/${RUBY_GEM_ARCH}/${RUBY_VER_DIR}
@@ -32,11 +37,15 @@ GEM_EXTSDIR=	${GEM_EXTSBASE}/${RUBY_GEM_ARCH}/${RUBY_VER_DIR}
 GEM_EXTSDIR=	${GEM_EXTSBASE}/${RUBY_GEM_ARCH}/${RUBY_VER_DIR}/${GEM_NAME}
 .  endif
 
-.  if !empty(GEM_EXTSDIR)
-PLIST_SUBST+=		GEM_EXTSDIR=${GEM_EXTSDIR}
-.  else
-PLIST_SUBST+=		GEM_EXTSDIR="@comment "
-.  endif
+_RUBY_GEM_MAJOR=	${RUBY_GEM_VER:C/([0-9]+)\..*/\1/}
+_RUBY_GEM_MINOR=	${RUBY_GEM_VER:C/[0-9]+\.([0-9]+)\..*/\1/}
+
+PLIST_VARS+=	ruby_plugins
+
+.if ${_RUBY_GEM_MAJOR} >= 3 && ${_RUBY_GEM_MINOR} >= 2
+PLIST.ruby_plugins=	YES
+.endif
+
 .endif
 
 .endif
