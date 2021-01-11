7ae66cbc3303a52fa26904f4053cc2364e81161c
prlw1 <prlw1@pkgsrc.org>
2021-01-11 09:43:21 +0000

    Update pango to 1.48.0
    Overview of changes in 1.48.0
    =============================
    - docs: Clarify use of PangoAnalysis in shaping
    - fontconfig: Make pango_font_family_get_face more reliable
    - introspection: Fix use of PangoFcSubstituteFunc
    - introspection: Skip fontconfig types
    - build: Use yielding meson feature for introspection
    
    Overview of changes in 1.47.0
    =============================
    - Add pango_language_get_preferred
    - Add pango_fc_font_get_languages
    - Sort faces of a family by slant and weight
    
    Overview of changes in 1.46.2
    =============================
    - Fix pango_win32_font_map_load_font with falback families
    - Fix an assertion in pango_language_get_scripts
    - Fix a crash in get_items_log_attrs
    - Fix attribute iterators with overlapping attributes
    - Fix rendering of Emoji keycap sequences
    - ci: Run the testsuite under asan and fix all reported issues
    - build: Make libthai, cairo, xft, fontconfig, freetype
      dependencies meson features

--- devel/pango/options.mk
+++ devel/pango/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.17 2019/08/16 10:43:24 wiz Exp $
+# $NetBSD: options.mk,v 1.18 2021/01/11 09:43:21 prlw1 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pango
 PKG_SUPPORTED_OPTIONS=	libthai quartz x11
@@ -6,7 +6,7 @@ PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		coretext quartz x11 thai
+PLIST_VARS+=		coretext quartz x11
 
 ###
 ### X11 support
@@ -14,20 +14,25 @@ PLIST_VARS+=		coretext quartz x11 thai
 .if !empty(PKG_OPTIONS:Mx11)
 # fix for pangox.pc
 PLIST.x11=		yes
+MESON_ARGS+=	-Dxft=enabled
 BUILDLINK_API_DEPENDS.Xft2+=	Xft2>=2.1.7nb3
 .include "../../x11/libXft/buildlink3.mk"
 .include "../../x11/libXrender/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libXt?=	build # only for configure
 .include "../../x11/libXt/buildlink3.mk"
+.else
+MESON_ARGS+=	-Dxft=disabled
 .endif
 
 ###
 ### Thai language support
 ###
 .if !empty(PKG_OPTIONS:Mlibthai)
-PLIST.thai=		yes
+MESON_ARGS+=	-Dlibthai=enabled
 .include "../../devel/libthai/buildlink3.mk"
+.else
+MESON_ARGS+=	-Dlibthai=disabled
 .endif
 
 .include "../../mk/bsd.prefs.mk"
