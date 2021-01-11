2604ac3e3b7bea5d1c703c737fa97c2ed5fcb930
schmonz <schmonz@pkgsrc.org>
2021-01-11 18:21:30 +0000

    Bump default BUILDLINK_API_DEPENDS.skalibs to the new version.

--- devel/skalibs/buildlink3.mk
+++ devel/skalibs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/10/03 00:31:39 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/01/11 18:21:30 schmonz Exp $
 
 BUILDLINK_TREE+=	skalibs
 
 .if !defined(SKALIBS_BUILDLINK3_MK)
 SKALIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.skalibs+=		skalibs>=2.6.0.0
+BUILDLINK_API_DEPENDS.skalibs+=		skalibs>=2.10.0.0
 BUILDLINK_PKGSRCDIR.skalibs?=		../../devel/skalibs
 
 .endif # SKALIBS_BUILDLINK3_MK
