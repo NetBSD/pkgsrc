1e8abb9c86582d530307610fbfa381c293eac20d
ryoon <ryoon@pkgsrc.org>
2021-01-12 12:10:27 +0000

    gtar: Update to 1.33
    Changelog:
    version 1.33 - Sergey Poznyakoff, 2021-01-07
    
    * POSIX extended format headers do not include PID by default
    
    The intent is to make binary-equivalent PAX archives easy to create.  If
    POSIXLY_CORRECT is set, the POSIX standard default is used, which embeds
    the pid.
    
    * --delay-directory-restore works for archives with reversed member ordering
    
    * Fix extraction of a symbolic link hardlinked to another symbolic link
    
    * Wildcards in exclude-vcs-ignore mode don't match slash
    
    * Fix the --no-overwrite-dir option
    
    Given this option, previous versions of tar failed to preserve
    permissions of empty directories and to create files under directories
    owned by the current user that did not have the S_IWUSR bit set.
    
    * Fix handling of chained renames in incremental backups
    
    * Link counting works for file names supplied with -T
    
    * Accept only position-sensitive (file-selection) options in file list files.
    
    Using such options as -f, -z, etc. is senseless in a file list file and
    bypasses option consistency checks in decode_options.  Therefore,
    only options related to file selection (a.k.a position-sensitive options)
    are allowed in file list files.

--- archivers/gtar-base/patches/patch-gnu_readlinkat.c
+++ archivers/gtar-base/patches/patch-gnu_readlinkat.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-gnu_readlinkat.c,v 1.2 2016/05/31 15:04:51 mef Exp $
+$NetBSD: patch-gnu_readlinkat.c,v 1.3 2021/01/12 12:10:27 ryoon Exp $
 
 * workaround for NetBSD-5.x, readlink is defined as a macro.
 
---- gnu/readlinkat.c.orig	2015-08-24 19:08:25.000000000 +0900
-+++ gnu/readlinkat.c	2016-05-31 23:53:11.000000000 +0900
-@@ -69,6 +69,7 @@ rpl_readlinkat (int fd, char const *file
- # define AT_FUNC_POST_FILE_PARAM_DECLS , char *buf, size_t len
- # define AT_FUNC_POST_FILE_ARGS        , buf, len
+--- gnu/readlinkat.c.orig	2021-01-07 14:29:45.000000000 +0000
++++ gnu/readlinkat.c
+@@ -103,6 +103,7 @@ rpl_readlinkat (int fd, char const *file
+ # define AT_FUNC_POST_FILE_PARAM_DECLS , char *buf, size_t bufsize
+ # define AT_FUNC_POST_FILE_ARGS        , buf, bufsize
  # define AT_FUNC_RESULT ssize_t
 +# undef readlink
  # include "at-func.c"
