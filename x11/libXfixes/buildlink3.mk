07182f21697dd04ba3f2f306e3bda6b48aa654c3
wiz <wiz@pkgsrc.org>
2021-01-14 17:42:58 +0000

    libXfixes: remove reference to Xfixes

--- x11/libXfixes/buildlink3.mk
+++ x11/libXfixes/buildlink3.mk
@@ -1,16 +1,8 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/03/07 11:57:36 wiz Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${X11_TYPE} != "modular" && \
-	!exists(${X11BASE}/lib/pkgconfig/xfixes.pc) && \
-	!exists(${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xfixes.pc)
-.include "../../x11/Xfixes/buildlink3.mk"
-.else
+# $NetBSD: buildlink3.mk,v 1.7 2021/01/14 17:42:58 wiz Exp $
 
 BUILDLINK_TREE+=	libXfixes
 
-.  if !defined(LIBXFIXES_BUILDLINK3_MK)
+.if !defined(LIBXFIXES_BUILDLINK3_MK)
 LIBXFIXES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libXfixes+=	libXfixes>=4.0.1
@@ -18,8 +10,6 @@ BUILDLINK_PKGSRCDIR.libXfixes?=		../../x11/libXfixes
 
 .include "../../x11/xorgproto/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
-.  endif # LIBXFIXES_BUILDLINK3_MK
+.endif # LIBXFIXES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libXfixes
-
-.endif
