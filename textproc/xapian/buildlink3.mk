038e806acd6ff1b0eaaf985a436c989f98f67596
schmonz <schmonz@pkgsrc.org>
2021-01-14 18:17:09 +0000

    Update to 1.4.18. From the changelog:
    API:
    
    * QueryParser::FLAG_ACCUMULATE: New flag.  Previously the unstem and stoplist
      data was always reset by a call to QueryParser::parse_query(), which makes
      sense if you use the same QueryParser object to parse a series of independent
      queries.  If you're using the same QueryParser object to parse several fields
      on the same query form, you may want to have the unstem and stoplist data
      combined for all of them, in which case you can use this flag to prevent this
      data from being reset.
    
    * QueryParser::unstem_begin(): Eliminate unnecessary copying of the data.
    
    * Fix typo in Swedish stopword list, syncing change made to Snowball by Daniel
      Gómez Villanueva.
    
    * Remove some French stop words with other meanings, syncing change made to
      Snowball by PhilippeOuellet.
    
    testsuite:
    
    * Run testcase testlock4 using backend chert, not just using glass
    
    * Skip testcase testlock4 on platforms that don't allow us to implement
      Database::locked() (which notably include GNU Hurd and Microsoft Windows).
    
    documentation:
    
    * List DB_NO_TERMLIST in the WritableDatabase constructor API documentation
      where we already list the other DB_* constants.
    
    portability:
    
    * Eliminate single use of std::mem_fun() which was deprecated in C++11 and
      removed in C++17.  Reported by Mateusz Pusz in #806.
    
    * Add missing includes for std::numeric_limits<>.  Reported by stac47 in #805.
    
    * Work around mingw.org header issue.  MSVC seems to implicitly include
      <winerror.h> but mingw.org's headers don't, leading to ERROR_PIPE_CONNECTED
      not being defined.  Fixes https://github.com/xapian/xapian/pull/318, reported
      by Alex Sandro.
    
    * Suppress MSVC warnings about possible loss of data.  The values involved are
      the number of set bits in a value of integer type, so these warnings are
      bogus.
    
    * Include <sys/types.h> for size_t and off_t, which is the appropriate header,
      and needed with Android's bionic libc.  Patch from Matthieu Gautier.
    
    * Use a temporary file for the Doxygen configuration to work around Doxygen
      1.8.19 bug which truncates a config file read from stdin to 4096 bytes
      (https://github.com/doxygen/doxygen/issues/7975).

--- textproc/xapian/buildlink3.mk
+++ textproc/xapian/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2016/11/07 13:46:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/01/14 18:17:09 schmonz Exp $
 
 BUILDLINK_TREE+=	xapian
 
 .if !defined(XAPIAN_BUILDLINK3_MK)
 XAPIAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xapian+=	xapian>=1.0.7
-BUILDLINK_ABI_DEPENDS.xapian+=	xapian>=1.4.1
+BUILDLINK_API_DEPENDS.xapian+=	xapian>=1.4.18
+BUILDLINK_ABI_DEPENDS.xapian+=	xapian>=1.4.18
 BUILDLINK_PKGSRCDIR.xapian?=	../../textproc/xapian
 
 .include "../../devel/libuuid/buildlink3.mk"
