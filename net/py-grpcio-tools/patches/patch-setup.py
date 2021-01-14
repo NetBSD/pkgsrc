061f9041c450250184dec423443189bfc082edd2
adam <adam@pkgsrc.org>
2021-01-14 10:17:46 +0000

    grpc: updated to 1.34.1
    Release v1.34.1
    
    This is release 1.34.1 (gauntlet) of gRPC Core.
    
    For gRPC documentation, see grpc.io. For previous releases, see Releases.
    
    This release is a patch specifically for Python and C#, which adds three backported PRs:
    
    Backport "Fix visibility of libprotobuf symbols in protoc_compiler.so on Mac" to 1.34.x
    Backport "Lazily import grpc_tools when using runtime stub/message generation" to 1.34.x
    Backport "do not use <PublicSign>true</PublicSign> on non-windows" to 1.34.x

--- net/py-grpcio-tools/patches/patch-setup.py
+++ net/py-grpcio-tools/patches/patch-setup.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-setup.py,v 1.3 2019/10/24 18:34:17 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2021/01/14 10:17:46 adam Exp $
 
 Use pthread on FreeBSD.
 Use external protobuf.
 
---- setup.py.orig	2019-10-22 19:40:19.000000000 +0000
+--- setup.py.orig	2021-01-13 06:42:53.000000000 +0000
 +++ setup.py
-@@ -108,7 +108,7 @@ if EXTRA_ENV_COMPILE_ARGS is None:
-         EXTRA_ENV_COMPILE_ARGS += ' -fno-wrapv -frtti'
- if EXTRA_ENV_LINK_ARGS is None:
-     EXTRA_ENV_LINK_ARGS = ''
+@@ -146,7 +146,7 @@ if EXTRA_ENV_LINK_ARGS is None:
+     if "darwin" in sys.platform:
+         EXTRA_ENV_LINK_ARGS += ' -Wl,-exported_symbol,_{}'.format(
+             _EXT_INIT_SYMBOL)
 -    if "linux" in sys.platform or "darwin" in sys.platform:
 +    if "linux" in sys.platform or "darwin" in sys.platform or "freebsd" in sys.platform:
          EXTRA_ENV_LINK_ARGS += ' -lpthread'
          if check_linker_need_libatomic():
              EXTRA_ENV_LINK_ARGS += ' -latomic'
-@@ -123,11 +123,9 @@ if EXTRA_ENV_LINK_ARGS is None:
+@@ -159,11 +159,9 @@ if EXTRA_ENV_LINK_ARGS is None:
  EXTRA_COMPILE_ARGS = shlex.split(EXTRA_ENV_COMPILE_ARGS)
  EXTRA_LINK_ARGS = shlex.split(EXTRA_ENV_LINK_ARGS)
  
@@ -26,7 +26,7 @@ Use external protobuf.
  PROTO_INCLUDE = os.path.normpath(protoc_lib_deps.PROTO_INCLUDE)
  
  GRPC_PYTHON_TOOLS_PACKAGE = 'grpc_tools'
-@@ -138,7 +136,7 @@ if "win32" in sys.platform:
+@@ -174,7 +172,7 @@ if "win32" in sys.platform:
      DEFINE_MACROS += (('WIN32_LEAN_AND_MEAN', 1),)
      if '64bit' in platform.architecture()[0]:
          DEFINE_MACROS += (('MS_WIN64', 1),)
@@ -35,7 +35,7 @@ Use external protobuf.
      DEFINE_MACROS += (('HAVE_PTHREAD', 1),)
  
  # By default, Python3 distutils enforces compatibility of
-@@ -185,7 +183,7 @@ def extension_modules():
+@@ -221,7 +219,7 @@ def extension_modules():
      plugin_sources += [
          os.path.join('grpc_tools', 'main.cc'),
          os.path.join('grpc_root', 'src', 'compiler', 'python_generator.cc')
@@ -44,7 +44,7 @@ Use external protobuf.
  
      plugin_ext = extension.Extension(
          name='grpc_tools._protoc_compiler',
-@@ -194,7 +192,6 @@ def extension_modules():
+@@ -230,7 +228,6 @@ def extension_modules():
              '.',
              'grpc_root',
              os.path.join('grpc_root', 'include'),
