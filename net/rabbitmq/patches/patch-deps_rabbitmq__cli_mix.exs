8b34d03a589015ac47860867df361924527816c4
adam <adam@pkgsrc.org>
2021-01-15 12:13:11 +0000

    rabbitmq: updated to 3.8.9
    RabbitMQ 3.8.9 is a maintenance release.
    It focuses on bug fixes and usability improvements.
    
    
    RabbitMQ 3.8.8 is a maintenance release.
    It focuses on bug fixes and usability improvements.
    
    This releases introduces a new node operation mode, called the maintenance mode.
    This feature is useful when performing rolling upgrades and is covered
    in more detail below.
    
    
    RabbitMQ 3.8.7 is a maintenance release that patches
    a security vulnerability.
    
    
    RabbitMQ 3.8.6 is a maintenance release.
    It focuses on bug fixes and usability improvements.
    
    
    RabbitMQ 3.8.5 is a maintenance release.
    It focuses on bug fixes and usability improvements.
    
    
    RabbitMQ 3.8.4 is a maintenance release. It focuses on bug fixes and usability
    improvements. etcd peer discovery plugin includes potentially breaking changes which are covered in detail below.

--- net/rabbitmq/patches/patch-deps_rabbitmq__cli_mix.exs
+++ net/rabbitmq/patches/patch-deps_rabbitmq__cli_mix.exs
@@ -1,4 +1,4 @@
-$NetBSD: patch-deps_rabbitmq__cli_mix.exs,v 1.3 2020/02/13 18:31:07 adam Exp $
+$NetBSD: patch-deps_rabbitmq__cli_mix.exs,v 1.4 2021/01/15 12:13:12 adam Exp $
 
 Allow newer elixir.
 
@@ -8,8 +8,8 @@ Allow newer elixir.
      [
        app: :rabbitmqctl,
        version: "3.8.0-dev",
--      elixir: ">= 1.7.0 and < 1.10.0",
-+      elixir: ">= 1.7.0",
+-      elixir: ">= 1.8.0 and < 1.11.0",
++      elixir: ">= 1.8.0",
        build_embedded: Mix.env == :prod,
        start_permanent: Mix.env == :prod,
        escript: [main_module: RabbitMQCtl,
