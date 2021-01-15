29148b9c3196523da78b646e1b78938a4cd795d5
nia <nia@pkgsrc.org>
2021-01-15 13:40:13 +0000

    erlang: Update to 23.2.2
    I am only committing this to unbreak the package post-autoconf-update.
    
    Regardless, changelog for 23.2:
    https://erlang.org/download/OTP-23.2.README

--- lang/erlang/patches/patch-erts_emulator_sys_unix_sys__uds.c
+++ lang/erlang/patches/patch-erts_emulator_sys_unix_sys__uds.c
@@ -1,18 +1,19 @@
-$NetBSD: patch-erts_emulator_sys_unix_sys__uds.c,v 1.1 2018/07/03 11:01:21 fhajny Exp $
+$NetBSD: patch-erts_emulator_sys_unix_sys__uds.c,v 1.2 2021/01/15 13:40:14 nia Exp $
 
 Fix SunOS/C99.
 
---- erts/emulator/sys/unix/sys_uds.c.orig	2018-06-19 07:07:55.000000000 +0000
+--- erts/emulator/sys/unix/sys_uds.c.orig	2021-01-15 07:20:03.000000000 +0000
 +++ erts/emulator/sys/unix/sys_uds.c
-@@ -23,7 +23,11 @@
+@@ -23,8 +23,12 @@
  #endif
  
  #if defined(__sun__) && !defined(_XOPEN_SOURCE)
 +#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
- #define _XOPEN_SOURCE 500
++#define _XOPEN_SOURCE 500
 +#else
-+#define _XOPEN_SOURCE 600
-+#endif
+ #define _XOPEN_SOURCE 600
  #endif
++#endif
  
  #include <limits.h>
+ 
