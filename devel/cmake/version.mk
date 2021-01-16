ecbf48910e1460b493b4c6f7f758780ccdfba8f1
adam <adam@pkgsrc.org>
2021-01-16 11:16:25 +0000

    cmake: updated to 3.19.3
    CMake 3.19.3
    * cmSystemTools: Revert use of MOVEFILE_WRITE_THROUGH by RenameFile on Windows
    * Tests: Fix ConfigSources test with empty CMAKE_BUILD_TYPE
    * gitlab-ci: consolidate Linux release package job spec for x86_64
    * Help: Add 3.19.3 release note for Linux aarch64 binary
    * gitlab-ci: update macOS jobs to use Xcode 12.3
    * cmGlobalXCodeGenerator: Adopt pbxproj object id generation
    * cmGlobalXCodeGenerator: Add infrastructure for deterministic object ids
    * Xcode: Use deterministic object ids for script build phases
    * gitlab-ci: update upload jobs to go to cmake.org
    * cmake-gui: Fix macOS styling for binaries distributed with Qt 5.10+
    * Utilities/Release: Fix macOS Qt 5.9 build script umask
    * Utilities/Release: Add script to build Qt 5.15.2 macOS universal binaries
    * ci: build separate macOS packages for macOS 10.13+ and macOS 10.10+
    * Help: Fix Sphinx not recognising list in XCODE_LINK_BUILD_PHASE_MODE
    * FetchContent: Relative SOURCE_DIR override cannot be a hard error
    * FindMPI: remove single quotes from include path
    * Utilities/Release: Add docker specs for Linux aarch64 binaries
    * gitlab-ci: add jobs to make Linux release packages for aarch64 on 3.19 branch
    * FindHDF5: Fix finding both debug and release libs
    * cmake-gui: Restore linking of Qt resources
    * cmMakefile: Don't expand bracket arguments in --trace-expand

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.6 2020/12/17 12:14:35 adam Exp $
+# $NetBSD: version.mk,v 1.7 2021/01/16 11:16:25 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.19.2
+CMAKE_VERSION=	3.19.3
 CMAKE_API=	${CMAKE_VERSION:R}
