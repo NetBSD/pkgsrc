7a13b6accd881da6989e737129e6a3eb84b831e5
nia <nia@pkgsrc.org>
2021-01-16 14:44:50 +0000

    osg: Update to 3.6.5
    OpenSceneGraph 3.6 release
    Updates include:
    
      * OpenThreads::Affinity introduced to enable setting of processor affinity on viewer and database threads
      * osgText rewritten to improve visual quality, add signed distance field support and full GLES2/3 and GL3/4 support
      * Added VertexArrayObject support, enable full OpenGL Core Profile support under OSX.
      * Added OpenCASCADE plugin
      * Added STEP (.stp) plugin
      * Improvements to FBX and COLLADA loaders
      * Improvements to gles plugin to provide better Sketchfab support
      * Added osgemscripten example
      * Improvements to osgAnimation
      * NodeVisitor ValueMap for storing values that can be stored and accessed across frames, such as update, event and cull traversals
      * ShapeDrawable rewritten as an osg::Geometry to improve performance and flexibility
      * Added osg::MultiDrawArrays support
      * Added osgdeferred example that illustrates how to implement deferred rendering
      * Added MultiDrawIndirect support
      * Moved glDispatchCompute control out of osg::Program into a dedicated osg::DispatchCompute class to improve control of compute shaders
      * KdTree support added for PolytopeIntersector, and ability to work with points, lines and polygons
      * osgQt has been moved out to it's own dedicated osgQt github repository
      * CMake build support for iOS bitcode builds
      * CoverityScan testing introduced, fixes bring defect density to 0.0 per 1,0000 lines of code!
      * Support for Codedoc automated documentation
      * Support for Travis automated build system

--- graphics/osg/options.mk
+++ graphics/osg/options.mk
@@ -1,61 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2018/08/04 01:05:10 nia Exp $
+# $NetBSD: options.mk,v 1.2 2021/01/16 14:44:50 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.osg
-PKG_SUPPORTED_OPTIONS+=	curl ffmpeg freetype gdal jasper libxml2 lua
-PKG_SUPPORTED_OPTIONS+=	openexr pdf sdl2 svg xrandr
-PKG_SUGGESTED_OPTIONS+=	ffmpeg freetype xrandr
-
-PLIST_VARS+=		curl ffmpeg freetype gdal jasper lua openexr pdf svg
+PKG_SUPPORTED_OPTIONS+=	gdal pdf svg
+PLIST_VARS+=		gdal pdf svg
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mcurl)
-.include "../../www/curl/buildlink3.mk"
-PLIST.curl=	yes
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_CURL=TRUE
-.endif
-
-.if !empty(PKG_OPTIONS:Mffmpeg)
-.include "../../multimedia/ffmpeg4/buildlink3.mk"
-PLIST.ffmpeg=	yes
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_FFmpeg=TRUE
-.endif
-
-.if !empty(PKG_OPTIONS:Mfreetype)
-.include "../../graphics/freetype2/buildlink3.mk"
-PLIST.freetype=	yes
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_Freetype=TRUE
-.endif
 
 .if !empty(PKG_OPTIONS:Mgdal)
 .include "../../geography/gdal-lib/buildlink3.mk"
 PLIST.gdal=	yes
 .else
 CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_GDAL=TRUE
-.endif
-
-.if !empty(PKG_OPTIONS:Mjasper)
-.include "../../graphics/jasper/buildlink3.mk"
-PLIST.jasper=	yes
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_Jasper=TRUE
-.endif
-
-.if !empty(PKG_OPTIONS:Mlua)
-.include "../../lang/lua52/buildlink3.mk"
-PLIST.lua=	yes
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_Lua52=TRUE
-.endif
-
-.if !empty(PKG_OPTIONS:Mopenexr)
-.include "../../graphics/openexr/buildlink3.mk"
-PLIST.openexr=	yes
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_OpenEXR=TRUE
 .endif
 
 .if !empty(PKG_OPTIONS:Mpdf)
@@ -65,28 +20,9 @@ PLIST.pdf=	yes
 CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_Poppler-glib=TRUE
 .endif
 
-.if !empty(PKG_OPTIONS:Msdl2)
-.include "../../devel/SDL2/buildlink3.mk"
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_SDL2=TRUE
-.endif
-
 .if !empty(PKG_OPTIONS:Msvg)
 .include "../../graphics/librsvg/buildlink3.mk"
 PLIST.svg=	yes
 .else
 CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_RSVG=TRUE
-.endif
-
-.if !empty(PKG_OPTIONS:Mxrandr)
-.include "../../x11/libXrandr/buildlink3.mk"
-CMAKE_ARGS+=	-DOSGVIEWER_USE_XRANDR:BOOL=ON
-.else
-CMAKE_ARGS+=	-DOSGVIEWER_USE_XRANDR:BOOL=OFF
-.endif
-
-.if !empty(PKG_OPTIONS:Mlibxml2)
-.include "../../textproc/libxml2/buildlink3.mk"
-.else
-CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_LibXml2=TRUE
 .endif
