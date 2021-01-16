65b8b5552aaa6b6a210b080ba32004efd5ccf9aa
adam <adam@pkgsrc.org>
2021-01-16 09:41:57 +0000

    py-django-filer: updated to 2.0.2
    2.0.2
    Fix 1214: serve() missing 1 required positional argument: filer_file.
    Fix 1211: On upload MIME-type is not set correctly.
    
    2.0.1
    Fixed NotNullViolation: null value in column "mime_type" in migration filer.0012_file_mime_type.py.
    
    2.0.0
    Added support for Django 3.1
    Dropped support for Python 2.7 and Python 3.4
    Dropped support for Django < 2.2
    Changed the preferred way to do model registration via model inheritance and mptt.AlreadyRegistered, which is deprecated since django-mptt 0.4
    Use dashed name for django-polymorphic dependency in setup.py
    In models.File, add field mime_type to store the Content-Type as set by the browser during file upload
    For extended Django Filer models, adopt the classmethod matches_file_type to its new signature, this is a breaking change
    Add attribute download to the download link in order to offer the file under its original name

---
+++ www/py-django-filer/patches/patch-setup.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-setup.py,v 1.1 2021/01/16 09:41:57 adam Exp $
+
+Do not install tests.
+
+--- setup.py.orig	2021-01-16 09:34:19.000000000 +0000
++++ setup.py
+@@ -50,7 +50,7 @@ setup(
+     description='A file management application for django that makes handling '
+                 'of files and images a breeze.',
+     long_description=open('README.rst').read(),
+-    packages=find_packages(),
++    packages=find_packages(exclude=['tests*']),
+     include_package_data=True,
+     zip_safe=False,
+     install_requires=REQUIREMENTS,
