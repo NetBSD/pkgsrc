2f5fb0881998fd3255fdfbd8192a90dee09c00b2
nia <nia@pkgsrc.org>
2021-01-18 09:22:19 +0000

    libusb1: require gcc>=4.7 for -std=c11

--- devel/libusb1/buildlink3.mk
+++ devel/libusb1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/05/25 06:47:40 plunky Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/01/18 09:22:19 nia Exp $
 
 BUILDLINK_TREE+=	libusb1
 
@@ -7,6 +7,9 @@ LIBUSB1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libusb1+=	libusb1>=1.0.9
 BUILDLINK_PKGSRCDIR.libusb1?=	../../devel/libusb1
+
+GCC_REQD+=	4.7 # -std=c11
+
 .endif	# LIBUSB1_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libusb1
