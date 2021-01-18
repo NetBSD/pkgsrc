2aa6871375b9d10976bfc7e5abb7bebf7a30c071
nia <nia@pkgsrc.org>
2021-01-18 09:34:30 +0000

    vamp-plugin-sdk: build dependent packages with same toolchain

--- audio/vamp-plugin-sdk/buildlink3.mk
+++ audio/vamp-plugin-sdk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/08/18 17:57:26 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/01/18 09:34:30 nia Exp $
 
 BUILDLINK_TREE+=	vamp-plugin-sdk
 
@@ -8,6 +8,9 @@ VAMP_PLUGIN_SDK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.vamp-plugin-sdk+=	vamp-plugin-sdk>=2.6
 BUILDLINK_ABI_DEPENDS.vamp-plugin-sdk?=	vamp-plugin-sdk>=2.9.0nb1
 BUILDLINK_PKGSRCDIR.vamp-plugin-sdk?=	../../audio/vamp-plugin-sdk
+
+# C++11
+GCC_REQD+=	4.7
 
 .include "../../audio/libsndfile/buildlink3.mk"
 .endif	# VAMP_PLUGIN_SDK_BUILDLINK3_MK
