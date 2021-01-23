990ae77d0645aec832f872bf90ab1eeab0316e03
nia <nia@pkgsrc.org>
2021-01-23 11:43:40 +0000

    profanity: Add explicit dependency on mozilla-rootcerts
    Point profanity at the certificates directory installed by the package.
    
    Should help with PR pkg/55949

---
+++ chat/profanity/patches/patch-src_config_preferences.c
@@ -0,0 +1,29 @@
+$NetBSD: patch-src_config_preferences.c,v 1.1 2021/01/23 11:43:40 nia Exp $
+
+Set paths for security/mozilla-rootcerts.
+
+--- src/config/preferences.c.orig	2020-07-01 16:49:19.000000000 +0000
++++ src/config/preferences.c
+@@ -527,20 +527,8 @@ prefs_get_tls_certpath(void)
+     }
+ 
+     if (setting == NULL) {
+-        if (g_file_test("/etc/ssl/certs",  G_FILE_TEST_IS_DIR)) {
+-            return strdup("/etc/ssl/certs");
+-        }
+-        if (g_file_test("/etc/pki/tls/certs",  G_FILE_TEST_IS_DIR)) {
+-            return strdup("/etc/pki/tls/certs");
+-        }
+-        if (g_file_test("/etc/ssl",  G_FILE_TEST_IS_DIR)) {
+-            return strdup("/etc/ssl");
+-        }
+-        if (g_file_test("/etc/pki/tls",  G_FILE_TEST_IS_DIR)) {
+-            return strdup("/etc/pki/tls");
+-        }
+-        if (g_file_test("/system/etc/security/cacerts",  G_FILE_TEST_IS_DIR)) {
+-            return strdup("/system/etc/security/cacerts");
++        if (g_file_test("@PREFIX@/share/mozilla-rootcerts",  G_FILE_TEST_IS_DIR)) {
++            return strdup("@PREFIX@/share/mozilla-rootcerts");
+         }
+ 
+         return NULL;
