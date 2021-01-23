cdad88157cb68afcc6c520bcfdd6030ec31c8f26
nia <nia@pkgsrc.org>
2021-01-23 12:11:12 +0000

    libao: simplify dependency patterns.
    there's no harm having multiple plugins installed concurrently,
    see https://xiph.org/ao/doc/config.html for advice on selecting one

--- audio/libao/buildlink3.mk
+++ audio/libao/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2019/11/24 00:35:56 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2021/01/23 12:11:12 nia Exp $
 
 BUILDLINK_TREE+=	libao
 
@@ -12,19 +12,13 @@ BUILDLINK_PKGSRCDIR.libao?=	../../audio/libao
 .if !defined(_LIBAO_BUILDING_PLUGIN)
 .  include "../../mk/bsd.fast.prefs.mk"
 .  if ${OPSYS} == "NetBSD" || ${OPSYS} == "SunOS"
-_LIBAO_DEFAULT_PLUGIN=		sun
+DEPENDS+=	libao-sun-[0-9]*:../../audio/libao-sun
 .  elif ${OPSYS} == "Linux"
-_LIBAO_DEFAULT_PLUGIN=		alsa
+DEPENDS+=	libao-alsa-[0-9]*:../../audio/libao-alsa
 .  elif ${OPSYS} == "Darwin"
-_LIBAO_DEFAULT_PLUGIN=		macosx
+DEPENDS+=	libao-macosx-[0-9]*:../../audio/libao-macosx
 .  elif ${OPSYS} != "Interix"
-_LIBAO_DEFAULT_PLUGIN=		oss
-.  else
-_LIBAO_DEFAULT_PLUGIN=
-.  endif
-
-.  if !empty(_LIBAO_DEFAULT_PLUGIN)
-DEPENDS+=	libao-[a-z]*-[0-9]*:../../audio/libao-${_LIBAO_DEFAULT_PLUGIN}
+DEPENDS+=	libao-oss-[0-9]*:../../audio/libao-oss
 .  endif
 .endif
 
