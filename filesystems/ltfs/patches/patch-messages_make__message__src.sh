f1952ba008bb5430e318b397fc97405c35d0de38
mef <mef@pkgsrc.org>
2021-01-23 13:17:00 +0000

    (filesystems/ltfs) Add commet for patch from cvs log

--- filesystems/ltfs/patches/patch-messages_make__message__src.sh
+++ filesystems/ltfs/patches/patch-messages_make__message__src.sh
@@ -1,4 +1,8 @@
-$NetBSD: patch-messages_make__message__src.sh,v 1.1 2020/05/14 20:08:30 joerg Exp $
+$NetBSD: patch-messages_make__message__src.sh,v 1.2 2021/01/23 13:17:00 mef Exp $
+
+date: 2020-05-15 05:08:30 +0900;  author: joerg;  state: Exp;  commitid: gg4PmA6YBrH0nf8C;
+Replace internal .libs copy of the static archive after modification.
+Still not MAKE_JOBS_SAFE, but at least it finally builds consistently.
 
 --- messages/make_message_src.sh.orig	2020-05-13 20:13:12.288742533 +0000
 +++ messages/make_message_src.sh
