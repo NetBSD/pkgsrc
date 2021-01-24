735e206a65f2e80bd0ac6924c2cdca73934e61ee
kamil <kamil@pkgsrc.org>
2021-01-24 10:28:30 +0000

    vte3: Suggest by default icu
    Requested by terminator (py-terminator).

--- x11/vte3/options.mk
+++ x11/vte3/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2020/11/07 10:05:11 nia Exp $
+# $NetBSD: options.mk,v 1.2 2021/01/24 10:28:30 kamil Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vte3
 PKG_SUPPORTED_OPTIONS=	icu
+PKG_SUGGESTED_OPTIONS=	icu
 
 .include "../../mk/bsd.options.mk"
 
