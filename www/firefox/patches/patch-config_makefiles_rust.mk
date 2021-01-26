4d4e29ec3d65eaa5d2421fa250299328449a7b2d
ryoon <ryoon@pkgsrc.org>
2021-01-26 15:02:55 +0000

    firefox: Update to 85.0
    Changelog:
    New
    
      * Firefox now protects you from supercookies, a type of tracker that can stay
        hidden in your browser and track you online, even after you clear cookies.
        By isolating supercookies, Firefox prevents them from tracking your web
        browsing from one site to the next.
    
      * It??s easier than ever to save and access your bookmarks. Firefox now
        remembers your preferred location for saved bookmarks, displays the
        bookmarks toolbar by default on new tabs, and gives you easy access to all
        of your bookmarks via a toolbar folder.
    
      * The password manager now allows you to remove all of your saved logins with
        one click, as opposed to having to delete each login individually.
    
    Fixed
    
      * Various security fixes.
    
    
    Changed
    
      * Firefox no longer supports Adobe Flash. There is no setting available to
        re-enable Flash support.
    
    
    Enterprise
    
      * Various bug fixes and new policies have been implemented in the latest
        version of Firefox. You can see more details in the Firefox for Enterprise
        85 Release Notes.
    
    
    Developer
    
      * Developer Information
      * CSS: We have added support for the :focus-visible pseudo class.
    
      * It's possible to prettify JS expressions in Console source code Editor
        (available in multiline mode) using a new toolbar button.
        Console Editor Pretty Print Expression Screenshot
    
    Security fixes:
    #CVE-2021-23953: Cross-origin information leakage via redirected PDF requests
    #CVE-2021-23954: Type confusion when using logical assignment operators in
     JavaScript switch statements
    #CVE-2021-23955: Clickjacking across tabs through misusing requestPointerLock
    #CVE-2021-23956: File picker dialog could have been used to disclose a complete
     directory
    #CVE-2021-23957: Iframe sandbox could have been bypassed on Android via the
     intent URL scheme
    #CVE-2021-23958: Screen sharing permission leaked across tabs
    #CVE-2021-23959: Cross-Site Scripting in error pages on Firefox for Android
    #CVE-2021-23960: Use-after-poison for incorrectly redeclared JavaScript
     variables during GC
    #CVE-2021-23961: More internal network hosts could have been probed by a
    malicious webpage
    #CVE-2021-23962: Use-after-poison in <code>nsTreeBodyFrame::RowCountChanged</
     code>
    #CVE-2021-23963: Permission prompt inaccessible after asking for additional
     permissions
    #CVE-2021-23964: Memory safety bugs fixed in Firefox 85 and Firefox ESR 78.7
    #CVE-2021-23965: Memory safety bugs fixed in Firefox 85

--- www/firefox/patches/patch-config_makefiles_rust.mk
+++ www/firefox/patches/patch-config_makefiles_rust.mk
@@ -1,10 +1,9 @@
-$NetBSD: patch-config_makefiles_rust.mk,v 1.6 2020/12/17 09:53:15 ryoon Exp $
+$NetBSD: patch-config_makefiles_rust.mk,v 1.7 2021/01/26 15:02:55 ryoon Exp $
 
 NetBSD doesn't get along with parallel rust builds (it causes issues
 with ld.so) which are the default. Force -j1.
 
-+++ config/makefiles/rust.mk
---- config/makefiles/rust.mk.orig	2020-12-03 23:12:38.000000000 +0000
+--- config/makefiles/rust.mk.orig	2021-01-14 21:11:33.000000000 +0000
 +++ config/makefiles/rust.mk
 @@ -52,6 +52,9 @@ endif
  ifeq (1,$(MOZ_PARALLEL_BUILD))
@@ -14,5 +13,5 @@ with ld.so) which are the default. Force -j1.
 +cargo_build_flags += -j1
 +endif
  
- # This should also be paired with -Zbuild-std, but that doesn't work yet.
- ifdef MOZ_TSAN
+ # We also need to rebuild the rust stdlib so that it's instrumented. Because
+ # build-std is still pretty experimental, we need to explicitly request
