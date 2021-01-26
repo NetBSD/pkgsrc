5669a82d4914fb0d22cc6cfe2d7d07f77c1f9997
gutteridge <gutteridge@pkgsrc.org>
2021-01-26 00:31:10 +0000

    fna3d: adjust buildlink3.mk to fix bulk builds

--- graphics/fna3d/buildlink3.mk
+++ graphics/fna3d/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/01/24 19:03:47 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/01/26 00:31:10 gutteridge Exp $
 
-BUILDLINK_TREE+=	FNA3D
+BUILDLINK_TREE+=	fna3d
 
 .if !defined(FNA3D_BUILDLINK3_MK)
 FNA3D_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.FNA3D+=	FNA3D>=21.01
-BUILDLINK_PKGSRCDIR.FNA3D?=	../../graphics/fna3d
+BUILDLINK_API_DEPENDS.fna3d+=	fna3d>=21.01
+BUILDLINK_PKGSRCDIR.fna3d?=	../../graphics/fna3d
 
 .include "../../devel/SDL2/buildlink3.mk"
 .include "../../graphics/mojoshader/buildlink3.mk"
 .endif	# FNA3D_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-FNA3D
+BUILDLINK_TREE+=	-fna3d
