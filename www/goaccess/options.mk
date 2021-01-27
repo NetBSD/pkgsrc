f1a81003149aa0b53a235ebabd7b8adbb0255ccc
schmonz <schmonz@pkgsrc.org>
2021-01-27 08:17:31 +0000

    Update to 1.4. From the changelog:
    - Added a caching storage mechanism to improve parsing raw data and data
      rendering.
    - Added a mechanism to avoid counting duplicate data when restoring persisted
      data from disk.
    - Added additional option to the HTML report to set a maximum number of items
      per page to 3.
    - Added a list of podcast-related user agents under '%sysconfdir%'.
    - Added 'Android 10' to the list of Android codenames.
    - Added a 'widescreen' layout to the HTML report (e.g., 4K TV/KPI Dashboard).
    - Added 'Beaker', 'Brave', and 'Firefox Focus' to the list of browsers
    - Added command line option --user-name=username to avoid running GoAccess as
      root when outputting a real-time report.
    - Added 'DuckDuckGo' and 'MSNBot' browsers to the browsers.list.
    - Added 'facebookexternalhit' to the default crawler list.
    - Added German translation (DE).
    - Added Kubernetes Nginx Ingress Log Format to the default config file.
    - Added 'macOS Catalina' to the list of OSX codenames.
    - Added minor CSS updates to HTML report.
    - Added missing header '<sys/socket.h>' to fix FreeBSD build
    - Added new 'Edg' token to the list of browsers.
    - Added '--no-ip-validation' command line to disable client IP validation
    - Added '--persist' and '--restore' options to persist to disk and restore a
      dump from disk.
    - Added Portuguese translation (pt-BR)
    - Added Swedish translation (SV)
    - Added the ability to parse server cache status and a new panel to display
      those metrics.
    - Changed accumulated time to work by default on '--persist' and '--restore'.
    - Changed back how the hits and visitors percentage is calculated to be more
      intuitive.
    - Changed Geo Location panel display default to show only if database file is
      provided ('LIBMAXMINDDB').
    - Changed initial processing time from secs to HH:MM:SS in HTML output.
    - Changed '--max-items' for the static HTML report to allow no limit on
      output entries.
    - Changed required 'gettext' version to 0.19
    - Changed to ignore 'SIGPIPE' with 'SIG_IGN'
    - Changed version to 10.15 for 'macOS Catalina'.
    - Ensure proper escaping on default AWSELB log format.
    - Ensure valid requests counter is not affected on duplicate entries when
      restoring data.
    - Fixed issue preventing Ctrl-C (SIGINT) for the curses interface to stop the
      program.
    - Fixed issue where HTML report wouldn't update the tables when changing per
      page option.
    - Fixed issue where it wouldn't find either the user's or global config file.
    - Fixed issue where changing the number of items per page in the HTML report
      would not automatically refresh the tables.
    - Fixed issue where last updated label was not updated in real-time.
    - Fixed issue where overall date range wasn't showing the right start/end parse
      dates.
    - Fixed issue where tailing a file could potentially re-parse part of the log.
    - Fixed memory leak when fetching country/continent while using 'LIBMAXMINDDB'.
    - Fixed several '-Wcast-qual' warnings.
    - Fixed unwanted added characters to the HTML output.
    - Fixed websocket issue returning a 400 due to request header size.
    - Increased 'MAX_LINE_CONF' so a JSON string can be properly parsed from the
      config file.
    - Removed deprecated option '--geoip-city-data' from config file.
    - Removed unnecessary dependency from snapcraft.yaml.
    - Removed some old browsers from the default curated list.
    - Replaced TokyoCabinet storage for a non-dependency in-memory persistent
      storage.
    
    pkgsrc changes:
    
    - Remove 'tokyocabinet' option, no longer needed
    - Switch 'geoip' option to libmaxminddb

--- www/goaccess/options.mk
+++ www/goaccess/options.mk
@@ -1,25 +1,20 @@
-# $NetBSD: options.mk,v 1.6 2020/03/24 11:50:10 roy Exp $
+# $NetBSD: options.mk,v 1.7 2021/01/27 08:17:31 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.goaccess
-PKG_SUPPORTED_OPTIONS=	geoip ssl tokyocabinet wide-curses
-PKG_SUGGESTED_OPTIONS=	ssl tokyocabinet wide-curses
+PKG_SUPPORTED_OPTIONS=	geoip ssl wide-curses
+PKG_SUGGESTED_OPTIONS=	ssl wide-curses
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgeoip)
-.  include "../../net/GeoIP/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-geoip=legacy
+.  include "../../geography/libmaxminddb/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-geoip=mmdb
 .endif # geoip
 
 .if !empty(PKG_OPTIONS:Mssl)
 .  include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-openssl
 .endif # ssl
-
-.if !empty(PKG_OPTIONS:Mtokyocabinet)
-CONFIGURE_ARGS+=	--enable-tcb=btree
-.  include "../../databases/tokyocabinet/buildlink3.mk"
-.endif #tokyocabinet
 
 .if !empty(PKG_OPTIONS:Mwide-curses)
 CONFIGURE_ARGS+=	--enable-utf8
