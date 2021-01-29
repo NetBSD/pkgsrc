93e356ad22aca1dd5fdefeb3d70542ab192ae65d
scole <scole@pkgsrc.org>
2021-01-29 18:33:09 +0000

    libdatrie 0.2.13 Released
    @thep thep released this Jan 29, 2021
    
        Fix wrong key listing in byte trie
        (Issue #9, Thanks @legale for the report.)
        Fix cross-compiling issue caused by AC_FUNC_MALLOC
        (Issue #11, Thanks @vmchale for the report.)
        Fix isspace() arg problem on NetBSD.
        (Personal mail, Thanks Sean for the report;
        PR #8, Thanks @obache for an individual pull request.)
        Fix some documentations.
        Really use TRIE_CHAR_TERM in TrieChar string termination.
        Changing TRIE_CHAR_TERM definition now won't break the code.
        Fix Windows build issue by avoiding <unistd.h> include.
        (Partially addressing PR #15, Thanks @fanc999 for first raising this.)
        [New APIs] Add serialization of the trie into memory buffer.
        (PR #12, Thanks @KOLANICH for the contribution.)

--- devel/libdatrie/buildlink3.mk
+++ devel/libdatrie/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/07 13:04:05 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/01/29 18:33:09 scole Exp $
 
 BUILDLINK_TREE+=	libdatrie
 
 .if !defined(LIBDATRIE_BUILDLINK3_MK)
 LIBDATRIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdatrie+=	libdatrie>=0.2.0
-BUILDLINK_ABI_DEPENDS.libdatrie+=	libdatrie>=0.2.7.1
+BUILDLINK_API_DEPENDS.libdatrie+=	libdatrie>=0.2.13
 BUILDLINK_PKGSRCDIR.libdatrie?=		../../devel/libdatrie
 .endif # LIBDATRIE_BUILDLINK3_MK
 
