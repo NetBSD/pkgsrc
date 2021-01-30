00dde3bed658edb9f8f1f26774d6588dbf93cbca
jperkin <jperkin@pkgsrc.org>
2021-01-30 08:23:18 +0000

    libnet: Remove default DEPMETHOD of build.

--- devel/libnet/buildlink3.mk
+++ devel/libnet/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2021/01/26 11:20:17 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2021/01/30 08:23:18 jperkin Exp $
 
 BUILDLINK_TREE+=	libnet
 
@@ -7,7 +7,6 @@ LIBNET_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libnet+=		libnet>=1.2
 BUILDLINK_PKGSRCDIR.libnet?=		../../devel/libnet
-BUILDLINK_DEPMETHOD.libnet?=		build
 .endif
 
 BUILDLINK_TREE+=	-libnet
