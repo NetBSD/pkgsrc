b21f553a24621cf09518777e0be5b65d3439f708
adam <adam@pkgsrc.org>
2021-01-31 11:40:59 +0000

    cmake: updated to 3.19.4
    CMake 3.19.4
    * FindHDF5: avoid writing to the output when testing h5cc
    * FindHDF5: silence STATUS messages when found with QUIET
    * GetPrerequisites: Include weak macOS dependencies
    * file(GET_RUNTIME_DEPENDENCIES): Fix weak macOS libraries not detected
    * ci: speed up cmake and ninja downloads on Windows
    * ci: download WiX on Windows
    * Intel: Replace deprecated Fortran flag -nofor_main with -nofor-main
    * Help: Add Ninja Multi-Config to list in GENERATOR_IS_MULTI_CONFIG
    * IRSL: Install msvcp${v}${d}_atomic_wait.dll if available with CRT
    * Restore support for cross-compiling CMake itself
    * VS: Generalize Win10 max SDK version to all VS generators

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.7 2021/01/16 11:16:25 adam Exp $
+# $NetBSD: version.mk,v 1.8 2021/01/31 11:40:59 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.19.3
+CMAKE_VERSION=	3.19.4
 CMAKE_API=	${CMAKE_VERSION:R}
