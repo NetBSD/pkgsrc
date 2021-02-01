599a1bbdc9dc7b8db3559dcb03eea98c4313c071
fcambus <fcambus@pkgsrc.org>
2021-02-01 09:33:12 +0000

    htmldoc: update to 1.9.11.
    ChangeLog:
    
    # Changes in HTMLDOC v1.9.11
    
    - Added high-resolution desktop icons for Linux.
    - Updated the internal HTTP library to fix truncation of redirection URLs
      (Issue #396)
    - Fixed a regression in the handling of character entities for UTF-8 input
      (Issue #401)
    - The `--numbered` option did not work when the table-of-contents was disabled
      (Issue #405)
    
    
    # Changes in HTMLDOC v1.9.10
    
    - Updated local zlib to v1.2.11.
    - Updated local libpng to v1.6.37.
    - Fixed packaging issues on macOS and Windows (Issue #377, Issue #386)
    - Now ignore sRGB profile errors in PNG files (Issue #390)
    - The GUI would crash when saving (Issue #391)
    - Page comments are now allowed in `pre` text (Issue #394)
    
    
    # Changes in HTMLDOC v1.9.9
    
    - Fixed a redirection issue - some sites (incorrectly) provide an incomplete
      Location: URL in the HTTP response.
    - Fixed https: support on newer versions of Windows (Issue #378)
    - Fixed a problem with remote URLs containing spaces (Issue #379)
    - Fixed a UTF-8 processing bug for Markdown files (Issue #383)
    - Added support for `<FONT FACE="monospace">` (Issue #385)

--- www/htmldoc/patches/patch-htmldoc_http-addrlist.c
+++ www/htmldoc/patches/patch-htmldoc_http-addrlist.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-htmldoc_http-addrlist.c,v 1.1 2019/10/18 11:02:13 micha Exp $
+$NetBSD: patch-htmldoc_http-addrlist.c,v 1.2 2021/02/01 09:33:12 fcambus Exp $
 
 Portability fix, s6_addr32 is not a standard.
 
---- htmldoc/http-addrlist.c.orig	2019-10-08 18:56:34.000000000 +0000
+--- htmldoc/http-addrlist.c.orig	2021-02-01 09:20:13.146886279 +0000
 +++ htmldoc/http-addrlist.c
-@@ -804,7 +804,7 @@ httpAddrGetList(const char *hostname,	/*
- #  ifdef WIN32
+@@ -866,7 +866,7 @@ httpAddrGetList(const char *hostname,	/*
+ #  ifdef _WIN32
  	temp->addr.ipv6.sin6_addr.u.Byte[15]   = 1;
  #  else
 -	temp->addr.ipv6.sin6_addr.s6_addr32[3] = htonl(1);
 +	temp->addr.ipv6.sin6_addr.s6_addr[15] = 1;
- #  endif /* WIN32 */
+ #  endif /* _WIN32 */
  
          if (!first)
