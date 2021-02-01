42f690eb565ea4c6f0899760b2933dc833e4fa6d
mef <mef@pkgsrc.org>
2021-02-01 11:19:16 +0000

    (www/emacs-w3m) Updated from 1.4.632.20190920.1116 to 1.4.632.20210106.2144
    Followings are from ChangeLog but some 450 lines are omitted, sorry
    
    2020-01-05  Katsumi Yamaoka  <yamaoka@jpl.org>
    
    	* w3m.el (w3m-download): Add option to w3m so to work for downloading
    	any kinds of contents ([emacs-w3m:13731]).  Thanks to Kinoshita-san.
    
    2021-01-03  Boruch Baum  <boruch_baum@gmx.com>
    
    	* w3m.el (w3m-view-previous-page): When currently in an "about://" url,
    	return to the calling url (PR#89 for upstream).
    
    	* w3m-hist.el (w3m-history-push): Don't put "about://" pages in the
    	history (PR#89 for upstream).
    
    2020-12-28  Boruch Baum  <boruch_baum@gmx.com>
    
    	* w3m.el (w3m-select-buffer-mode):
    	* w3m-session.el (w3m-session-select-mode): Set variable
    	buffer-quit-function (PR#88).
    
    2020-12-22  Boruch Baum  <boruch_baum@gmx.com>
    	(w3m-perldoc-pretty): New function.
    	(w3m-perldoc): Use them.
    
     .... ( omimt some 450 lines) ...
    
    2019-10-02  Katsumi Yamaoka  <yamaoka@jpl.org>
    
    	Abolish w3m-max-anchor-sequence (issue #71)
    
    	* w3m.el (w3m-max-anchor-sequence): Abolish.
    
    	* w3m-form.el (w3m-form-parse-and-fontify):
    	* w3m.el (w3m-clear-local-variables, w3m-copy-local-variables)
    	(w3m-fontify-anchors, w3m-next-anchor, w3m-previous-anchor):
    	Don't handle/use w3m-max-anchor-sequence.
    
    2019-10-01  Katsumi Yamaoka  <yamaoka@jpl.org>
    
    	w3m-next-anchor and w3m-previous-anchor return t if success (issue #71)
    
    	* w3m.el (w3m-next-anchor, w3m-previous-anchor): Return t if success.

--- www/emacs-w3m-snapshot/patches/patch-w3mhack.el
+++ www/emacs-w3m-snapshot/patches/patch-w3mhack.el
@@ -1,4 +1,4 @@
-$NetBSD: patch-w3mhack.el,v 1.3 2019/10/31 22:41:44 mef Exp $
+$NetBSD: patch-w3mhack.el,v 1.4 2021/02/01 11:19:16 mef Exp $
 
 CF-SX2# cd /tmp/www/emacs-w3m-snapshot/work/emacs-w3m-emacs-w3m-c9cdb7e/
 
@@ -10,7 +10,7 @@ mime-w3m.el:38:1:Error: Cannot open load file: No such file or directory, w3m
 
 --- emacs-w3m-emacs-w3m-c9cdb7e/w3mhack.el.orig	2019-09-20 20:16:36.000000000 +0900
 +++ w3mhack.el	2019-10-30 21:16:09.444149861 +0900
-@@ -256,6 +256,10@@ There seems to be no shell command which
+@@ -256,6 +256,10 @@ There seems to be no shell command which
  	(push (file-name-directory x) paths))
      (if (setq x (locate-library "gnus"))
  	(push (file-name-directory x) paths))
