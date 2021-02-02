d512e81bf5101a17bb3277260e879aef3d0403f8
taca <taca@pkgsrc.org>
2021-02-02 15:03:36 +0000

    devel/php-xdebug: update to 3.0.2
    3.0.2 (2021-01-04)
    
    Fixed bugs:
    
    - Fixed issue #1907: Empty exception message when setting the $message
      property to a stringable object
    - Fixed issue #1910: Code coverage misses constructor property promotion
      code
    - Fixed issue #1914: Compillation failure on OpenBSD
    - Fixed issue #1915: Debugger should only start with XDEBUG_SESSION and
      not XDEBUG_PROFILE
    - Fixed issue #1918: Warn if PHP's Garbage Collection is disabled in
      gc_stats mode
    - Fixed issue #1919: Crash when enabling filter without the right mode
      active
    - Fixed issue #1921: Xdebug does not start step debugging if
      start_with_request=trigger
    - Fixed issue #1922: Code coverage misses array assignment lines
    - Fixed issue #1924: Deprecated INI settings displayed in phpinfo()
    - Fixed issue #1925: xdebug.start_with_request and start_upon_error
      display inconsistent values
    - Fixed issue #1926: Add Xdebug mode's source to xdebug_info() output
    - Fixed issue #1927: Crash when calling xdebug_stop_trace without a trace
      in progress
    - Fixed issue #1928: xdebug_stop_gcstats() can also return false

--- devel/php-xdebug/patches/patch-src_debugger_com.c
+++ devel/php-xdebug/patches/patch-src_debugger_com.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_debugger_com.c,v 1.2 2021/01/03 07:28:56 taca Exp $
+$NetBSD: patch-src_debugger_com.c,v 1.3 2021/02/02 15:03:36 taca Exp $
 
 Use standard value for second argument of setsockopt(2).
 
---- src/debugger/com.c.orig	2020-12-04 15:57:15.000000000 +0000
+--- src/debugger/com.c.orig	2021-01-04 17:17:01.000000000 +0000
 +++ src/debugger/com.c
-@@ -109,7 +109,7 @@ void set_keepalive_options(int fd)
- 	}
+@@ -110,7 +110,7 @@ void set_keepalive_options(int fd)
  
+ # if defined(TCP_KEEPIDLE)
  	optval = 600;
 -	ret = setsockopt(fd, SOL_TCP, TCP_KEEPIDLE, &optval, optlen);
 +	ret = setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &optval, optlen);
  	if (ret) {
  		xdebug_log_ex(XLOG_CHAN_DEBUG, XLOG_WARN, "KEEPALIVE", "Could not set TCP_KEEPIDLE to %d: %s.", optval, strerror(errno));
  		return;
-@@ -117,7 +117,7 @@ void set_keepalive_options(int fd)
+@@ -119,7 +119,7 @@ void set_keepalive_options(int fd)
  
  # if defined(TCP_KEEPCNT)
  	optval = 20;
@@ -22,7 +22,7 @@ Use standard value for second argument of setsockopt(2).
  	if (ret) {
  		xdebug_log_ex(XLOG_CHAN_DEBUG, XLOG_WARN, "KEEPALIVE", "Could not set TCP_KEEPCNT to %d: %s.", optval, strerror(errno));
  		return;
-@@ -126,7 +126,7 @@ void set_keepalive_options(int fd)
+@@ -128,7 +128,7 @@ void set_keepalive_options(int fd)
  
  # if defined(TCP_KEEPINTVL)
  	optval = 60;
