e83a9e581948dbe00b221647721aae4724e0c21e
jaapb <jaapb@pkgsrc.org>
2021-02-03 09:35:31 +0000

    Fixed PLIST for devel/ocaml-optint with current dune. Revbump.
    In short: the current version of dune doesn't build/install everything
    anymore, just what it needs. The PLIST has been updated to reflect that.

--- devel/ocaml-optint/buildlink3.mk
+++ devel/ocaml-optint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 17:47:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/02/03 09:35:31 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-optint
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-optint
 OCAML_OPTINT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-optint+=	ocaml-optint>=0.0.2
+BUILDLINK_ABI_DEPENDS.ocaml-optint+=	ocaml-optint>=0.0.4nb1
 BUILDLINK_PKGSRCDIR.ocaml-optint?=	../../devel/ocaml-optint
 .endif	# OCAML_OPTINT_BUILDLINK3_MK
 
