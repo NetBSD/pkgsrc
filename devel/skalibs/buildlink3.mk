103724bdd02fe3bdf240637a363132f464b7d09d
schmonz <schmonz@pkgsrc.org>
2021-02-04 14:08:23 +0000

    Most packages that link with skalibs fail configure if the current platform tuple doesn't match the one skalibs was built with. In pkgsrc, this almost certainly means the OS has been updated, and almost certainly doesn't need to break anyone's update builds. Explicitly pass the contents of ${PREFIX}/lib/skalibs/sysdeps/target as the --target of those configure scripts, then make sure we don't cross-compile.
    skalibs-using packages not needing this workaround can define
    SKALIBS_TOLERATE_TARGET_SKEW=no.

--- devel/skalibs/buildlink3.mk
+++ devel/skalibs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/01/11 18:21:30 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/02/04 14:08:23 schmonz Exp $
 
 BUILDLINK_TREE+=	skalibs
 
@@ -7,6 +7,17 @@ SKALIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.skalibs+=		skalibs>=2.10.0.0
 BUILDLINK_PKGSRCDIR.skalibs?=		../../devel/skalibs
+
+SKALIBS_TOLERATE_TARGET_SKEW?=		yes
+.if !empty(SKALIBS_TOLERATE_TARGET_SKEW:M[yY][eE][sS])
+SKALIBS_TARGET_cmd=			${CAT} ${PREFIX}/lib/skalibs/sysdeps/target
+CONFIGURE_ARGS+=			--target=${SKALIBS_TARGET_cmd:sh}
+
+SUBST_CLASSES+=				skalibs-target
+SUBST_STAGE.skalibs-target=		post-configure
+SUBST_FILES.skalibs-target=		config.mak
+SUBST_SED.skalibs-target=		-e 's|\(CROSS_COMPILE := \).*|\1|'
+.endif # SKALIBS_TOLERATE_TARGET_SKEW
 
 .endif # SKALIBS_BUILDLINK3_MK
 
