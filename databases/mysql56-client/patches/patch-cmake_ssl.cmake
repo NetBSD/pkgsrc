19552b92cdb5daaac6cb80e624073c67091a65b7
adam <adam@pkgsrc.org>
2021-02-05 09:28:07 +0000

    mysql56: updated to 5.6.51
    Changes in MySQL 5.6.51
    
    Security Notes
    
    The linked OpenSSL library for MySQL Server has been updated to version 1.1.1i. Issues fixed in the new OpenSSL version are described at https://www.openssl.org/news/cl111.txt and https://www.openssl.org/news/vulnerabilities.html.
    
    Bugs Fixed
    
    InnoDB: The full-text search synchronization thread attempted to read a previously-freed word from the index cache.
    
    The server did not handle all cases of the WHERE_CONDITION optimization correctly.
    
    Privileges for some INFORMATION_SCHEMA tables were checked incorrectly.
    
    In certain cases, the server did not handle multiply-nested subqueries correctly.
    
    A buffer overflow in the client library was fixed.

--- databases/mysql56-client/patches/patch-cmake_ssl.cmake
+++ databases/mysql56-client/patches/patch-cmake_ssl.cmake
@@ -1,21 +1,29 @@
-$NetBSD: patch-cmake_ssl.cmake,v 1.3 2019/10/14 19:44:47 adam Exp $
+$NetBSD: patch-cmake_ssl.cmake,v 1.4 2021/02/05 09:28:07 adam Exp $
 
 Allow OpenSSH versions below 1.0.
 Do not prefer static libraries (or linking will fail).
 
---- cmake/ssl.cmake.orig	2019-09-27 07:04:56.000000000 +0000
+--- cmake/ssl.cmake.orig	2021-01-05 10:19:59.000000000 +0000
 +++ cmake/ssl.cmake
-@@ -152,9 +152,7 @@ MACRO (MYSQL_CHECK_SSL)
-     ENDIF()
- 
+@@ -154,7 +154,6 @@ MACRO (MYSQL_CHECK_SSL)
      # On mac this list is <.dylib;.so;.a>
--    # We prefer static libraries, so we revert it here.
+     # We prefer static libraries, so we revert it here.
      IF (WITH_SSL_PATH)
 -      LIST(REVERSE CMAKE_FIND_LIBRARY_SUFFIXES)
        MESSAGE(STATUS "suffixes <${CMAKE_FIND_LIBRARY_SUFFIXES}>")
      ENDIF()
  
-@@ -194,8 +192,7 @@ MACRO (MYSQL_CHECK_SSL)
+@@ -164,9 +163,6 @@ MACRO (MYSQL_CHECK_SSL)
+     FIND_LIBRARY(CRYPTO_LIBRARY
+                  NAMES crypto libcrypto libeay32
+                  HINTS ${OPENSSL_ROOT_DIR}/lib)
+-    IF (WITH_SSL_PATH)
+-      LIST(REVERSE CMAKE_FIND_LIBRARY_SUFFIXES)
+-    ENDIF()
+ 
+     IF(OPENSSL_INCLUDE_DIR)
+       # Verify version number. Version information looks like:
+@@ -194,8 +190,7 @@ MACRO (MYSQL_CHECK_SSL)
      ENDIF()
      IF(OPENSSL_INCLUDE_DIR AND
         OPENSSL_LIBRARY   AND
