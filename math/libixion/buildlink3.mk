764d15d2bd34d58e9b993bc4e0f2ba4470ad4718
ryoon <ryoon@pkgsrc.org>
2021-02-06 06:23:22 +0000

    libixion, py-libixion: Update to 0.16.1
    Changelog:
    0.16.1
        fixed a build issue on 32-bit linux platforms, caused by slicing
        of integer string ID values.
    
        worked around floating point rounding errors which prevented
        two theoretically-equal numeric values from being evaluated as
        equal in test code.
    
    
    0.16.0
        documentation
    	added quickstart sections for using model_context and
    	document classes.
    
    	added code example for cell_access class.
    
        C++ API
    	added new function to allow printing of single formula
    	tokens.
    
    	added method for setting cached results on formula cells
    	in model_context.
    
    	changed the model_context design to ensure that all sheets
    	are of the same size.
    
    	added an accessor method to formula_model_access interface
    	(and implicitly in model_context) that directly returns a
    	string value from cell.
    
    	added cell_access class for querying of cell states without
    	knowing its type ahead of time.
    
    	added document class which provides a layer on top of
    	model_context, to abstract away the handling of formula
    	calculations.
    
    	deprecated model_context::erase_cell() in favor of
    	empty_cell().
    
        formula parser / interpreter
    	added support for 3D references - references that contain
    	multiple sheets.
    
    	added support for the exponent (^) and concatenation (&)
    	operators.
    
    	fixed incorrect handling of range references containing
    	whole columns such as A:A.
    
    	added support for unordered range references - range
    	references whose start row or column is greater than their
    	end position counterparts, such as A3:A1.
    
    	fixed a bug that prevented nested formula functions from
    	working properly.
    
    	implemented Calc A1 style reference resolver.
    
        misc
    	formula results now directly store the string values when
    	the results are of string type. They previously stored
    	string ID values after interning the original strings.
    
    	removed build-time dependency on spdlog.

--- math/libixion/buildlink3.mk
+++ math/libixion/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/02/05 13:46:13 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2021/02/06 06:23:22 ryoon Exp $
 
 BUILDLINK_TREE+=	libixion
 
 .if !defined(LIBIXION_BUILDLINK3_MK)
 LIBIXION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.15.0
+BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.16.1
 BUILDLINK_PKGSRCDIR.libixion?=		../../math/libixion
 
 .include "../../devel/mdds/buildlink3.mk"
