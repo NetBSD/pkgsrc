909aa4248242ce69e406e97e0cb180918d265b69
dbj <dbj@pkgsrc.org>
2021-02-06 15:25:17 +0000

    x11/kwindowsystem: use qt5-qtmacextras on Darwin

--- x11/kwindowsystem/buildlink3.mk
+++ x11/kwindowsystem/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2020/11/05 09:07:16 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2021/02/06 15:25:17 dbj Exp $
 
 BUILDLINK_TREE+=	kwindowsystem
 
@@ -9,8 +9,15 @@ BUILDLINK_API_DEPENDS.kwindowsystem+=	kwindowsystem>=5.18.0
 BUILDLINK_ABI_DEPENDS.kwindowsystem?=	kwindowsystem>=5.70.0nb3
 BUILDLINK_PKGSRCDIR.kwindowsystem?=	../../x11/kwindowsystem
 
+
+.include "../../mk/bsd.fast.prefs.mk"
+
 .include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.if ${OPSYS} == "Darwin"
+.  include "../../x11/qt5-qtmacextras/buildlink3.mk"
+.else
+.  include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.endif
 .endif	# KWINDOWSYSTEM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kwindowsystem
