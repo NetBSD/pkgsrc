8e476db14fdb85b39216e86a865b166022ca7ca0
maya <maya@pkgsrc.org>
2021-02-06 14:27:41 +0000

    love09: fix build with the mesa in netbsd-current Add missing PLIST entry from my previous commit, oops.

---
+++ games/love09/patches/patch-src_modules_graphics_opengl_GLee.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_modules_graphics_opengl_GLee.h,v 1.1 2021/02/06 14:27:41 maya Exp $
+
+Newer mesa seems to use different include guards, add those as well.
+
+--- src/modules/graphics/opengl/GLee.h.orig	2015-01-15 09:39:26.000000000 +0000
++++ src/modules/graphics/opengl/GLee.h
+@@ -61,7 +61,9 @@
+ 	#include <OpenGL/gl.h>
+ #else // GLX
+ 	#define __glext_h_  /* prevent glext.h from being included  */
++	#define __gl_glext_h_  /* prevent glext.h from being included - newer version */
+ 	#define __glxext_h_ /* prevent glxext.h from being included */
++	#define __glx_glxext_h_ /* prevent glxext.h from being included - newer version */
+ 	#define GLX_GLXEXT_PROTOTYPES
+ 	#include <GL/gl.h>
+ 	#include <GL/glx.h>
