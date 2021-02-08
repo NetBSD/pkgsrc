b1de7669df18f5dd870e6b9cffedd0e140c30ad3
ryoon <ryoon@pkgsrc.org>
2021-02-08 12:26:20 +0000

    seamonkey: Update to 2.53.6
    CHangelog:
    What's New in SeaMonkey 2.53.6
    
    SeaMonkey 2.53.6 contains (among other changes) the following major changes
    relative to SeaMonkey 2.53.5.1:
    
      * Improve usability of multiple mailboxes/folders selectionbug 1600103.
      * Add Greek localisation (el).
      * Remove more RDF from mailnews code.
      * Switch to mozilla as topsrcdir and component for building is comm/suite
        now.
      * Rust support is now up to 1.48 and official build is now using 1.47.0
      * Various security and general platform fixes.
    
    SeaMonkey 2.53.6 contains (among other changes) the following major changes
    relative to SeaMonkey 2.49.5:
    
      * The Bookmarks Manager has switched its name to Library, and now also
        includes the History list. When invoking History, the Library will be shown
        with the History list selected. The extensive modifications were needed
        because of Mozilla Gecko platform API changes.
      * Download Manager has been migrated to a new API. Although it looks pretty
        much the same as before, the search option is missing and some other minor
        details work differently. The previous downloads history is removed during
        the upgrade.
      * The layout panel was added to the CSS Grid tools.
      * TLS 1.3 is the default SSL version now.
      * The only NPAPI plugin which will work with SeaMonkey 2.53.6 is Flash.
        Support for other NPAPI plugins like Java and Silverlight has been removed.
        For displaying pdf files in the browser you can use pdf.js-seamonkey from
        Isaac Schemm.
      * SeaMonkey now uses a new api for formatting regional data like time and
        date. Default is to use the application locale of the current SeaMonkey
        build. If you use a language pack or a different OS formatting this is
        usually not desired. You can change the formatting from the application
        locale to the regional settings locale (OS) in the preferences dialog under
        "Appearance".
    
    SeaMonkey 2.53.6 uses the same backend as Firefox and contains the relevant
    Firefox 60.8 security fixes.
    
    SeaMonkey 2.53.6 shares most parts of the mail and news code with Thunderbird.
    Please read the Thunderbird 60.0 release notes for specific changes and
    security fixes in this release.
    
    Additional important security fixes up to Current Firefox 78.6 ESR and a few
    enhancements have been backported. We will continue to enhance SeaMonkey
    security in subsequent 2.53.x beta and release versions as fast as we are able
    to.
    
    SeaMonkey-specific changes
    
      * SeaMonkey now uses gtk3 on Linux. If you experience a problem because of
        this please file a bug and link it to bug 1367257. Please try another OS
        theme first. Some of them are buggy and cause problems with SeaMonkey,
        Thunderbird and Firefox.

--- www/seamonkey/patches/patch-mozilla_media_libcubeb_update.sh
+++ www/seamonkey/patches/patch-mozilla_media_libcubeb_update.sh
@@ -1,9 +1,9 @@
-$NetBSD: patch-mozilla_media_libcubeb_update.sh,v 1.4 2020/05/07 19:36:53 nia Exp $
+$NetBSD: patch-mozilla_media_libcubeb_update.sh,v 1.5 2021/02/08 12:26:20 ryoon Exp $
 
 - Sun Audio support.
 
---- mozilla/media/libcubeb/update.sh.orig	2020-04-01 11:01:50.000000000 +0000
-+++ mozilla/media/libcubeb/update.sh
+--- media/libcubeb/update.sh.orig	2020-04-01 11:01:50.000000000 +0000
++++ media/libcubeb/update.sh
 @@ -32,6 +32,7 @@ cp $1/src/cubeb_ringbuffer.h src
  cp $1/src/cubeb_sndio.c src
  cp $1/src/cubeb_strings.c src
