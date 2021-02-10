25280e862ab8a93bb2dba1295f2861821a0fa246
nia <nia@pkgsrc.org>
2021-02-10 12:12:39 +0000

    xbomb: Update to 2.2b
    2014-08-15  Andrew M. Bishop <amb>
    
            Version 2.2b Released
    
    2014-08-15 [r76]  Andrew M. Bishop <amb>
    
    	* FILES, README, xbomb.c, xwindow.c: Updated for version 2.2b.
    
    2014-08-15 [r74]  Andrew M. Bishop <amb>
    
    	* xwindow.c: Fix off-by-one error in creation of graphic contexts
    	  leading to writing beyond end of array [fixes Debian bug
    	  #757902]..
    
    2013-03-16 [r73]  Andrew M. Bishop <amb>
    
    	* README, xbomb.c: Change web-page and e-mail addresses from
    	  gedanken.demon.co.uk to gedanken.org.uk.
    
    2013-03-16 [r73]  Andrew M. Bishop <amb>
    
    	* xbomb.c, README: Change web-page and e-mail addresses from
    	  gedanken.demon.co.uk to gedanken.org.uk.
    
    2011-10-03 [r72]  Andrew M. Bishop <amb>
    
    	* FILES (added), ANNOUNCE (removed), LSM (removed): Don't include
    	  the ANNOUNCE or LSM files in future releases. Do include the
    	  FILES file in subversion.
    
    2010-12-27 [r70]  Andrew M. Bishop <amb>
    
    	* ChangeLog (removed): Remove the ChangeLog from version control.
    
    2010-12-24 [r67]  Andrew M. Bishop <amb>
    
    	* ANNOUNCE (added), COPYING (added), ChangeLog (added), LSM
    	  (added): Added the uncontrolled files from the xbomb 2.2a
    	  release.
    
    2010-12-24  Andrew M. Bishop <amb@gedanken.demon.co.uk>
    
    	Changed version control environment from RCS to CVS to SVN.
    
    2009-11-29  Andrew M. Bishop  <amb@gedanken.demon.co.uk>
    
            Version 2.2a Released
    
    2009-11-29  Andrew M. Bishop  <amb@gedanken.demon.co.uk>
    
    	* xbomb.ad:
    	Add the app-defaults for the different numbers (missed in version 2.2 release).
    
    	* xbomb.c, xwindow.c:
    	Change the colour of the number 8 from white (invisible) to light green.
    	Change the version number to 2.2a.

--- games/xbomb/patches/patch-xwindow.c
+++ games/xbomb/patches/patch-xwindow.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-xwindow.c,v 1.1 2019/10/06 14:41:07 jakllsch Exp $
+$NetBSD: patch-xwindow.c,v 1.2 2021/02/10 12:12:39 nia Exp $
 
 Avoid segfault in InitialiseX()
 
---- xwindow.c.orig	2008-01-05 19:24:16.000000000 +0000
+--- xwindow.c.orig	2014-08-15 16:25:59.000000000 +0000
 +++ xwindow.c
-@@ -245,7 +245,8 @@ void InitialiseX(int *argc,char **argv)
+@@ -243,7 +243,8 @@ void InitialiseX(int *argc,char **argv)
   values.font=resources.fontstruct->fid;
-  for(i=0;i<NUM_GC;i++)
+  for(i=0;i<NUM_GC-1;i++)
     {
 -    values.foreground=resources.colours[i];
 +    if(i<(sizeof(resources.colours)/sizeof(resources.colours[0])))
-+       values.foreground=resources.colours[i];
++      values.foreground=resources.colours[i];
      if(i==GC_UNSEEN)
        {
         GC tempgc;
