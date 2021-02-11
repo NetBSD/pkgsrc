5a24f849d1dec18ccd79a45323e8747b3d434c47
markd <markd@pkgsrc.org>
2021-02-11 20:57:44 +0000

    baloo-widgets: update to 20.04.1
    qt5/kf5 version.

--- sysutils/baloo-widgets/buildlink3.mk
+++ sysutils/baloo-widgets/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/11/15 20:48:28 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/02/11 20:57:44 markd Exp $
 
 BUILDLINK_TREE+=	baloo-widgets
 
 .if !defined(BALOO_WIDGETS_BUILDLINK3_MK)
 BALOO_WIDGETS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.baloo-widgets+=	baloo-widgets>=4.14.3
+BUILDLINK_API_DEPENDS.baloo-widgets+=	baloo-widgets>=16.04.0
 BUILDLINK_PKGSRCDIR.baloo-widgets?=	../../sysutils/baloo-widgets
 
+.include "../../sysutils/baloo5/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# BALOO_WIDGETS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-baloo-widgets
