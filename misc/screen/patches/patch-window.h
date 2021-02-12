fc9e1386c4620903aabd80559ffd1ef7bb5e5e7c
cjep <cjep@pkgsrc.org>
2021-02-12 22:53:14 +0000

    better approach. thanks wiz@

--- misc/screen/patches/patch-window.h
+++ misc/screen/patches/patch-window.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-window.h,v 1.2 2021/02/12 22:29:37 cjep Exp $
+$NetBSD: patch-window.h,v 1.3 2021/02/12 22:53:14 cjep Exp $
 
 OpenBSD grumbles without the socket definitions
 
 --- window.h.orig	Wed Feb  5 20:09:38 2020
-+++ window.h	Thu Feb 11 21:01:43 2021
++++ window.h	Thu Feb 11 21:27:43 2021
 @@ -33,6 +33,10 @@
  #ifndef SCREEN_WINDOW_H
  #define SCREEN_WINDOW_H
  
-+#ifdef _OpenBSD_
++#ifdef __OpenBSD__
 +#include <sys/socket.h>
 +#endif
 +
