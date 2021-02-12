2e87779f2e7de8e4b25700e29b456b8d06bd32b2
cjep <cjep@pkgsrc.org>
2021-02-12 22:28:03 +0000

    OpenBSD grumbles without the socket definitions. Fixes build.

---
+++ misc/screen/patches/patch-window.h
@@ -0,0 +1,17 @@
+# $NetBSD: patch-window.h,v 1.1 2021/02/12 22:28:03 cjep Exp $
+
+# OpenBSD grumbles without the socket definitions
+
+--- window.h.orig	Wed Feb  5 20:09:38 2020
++++ window.h	Thu Feb 11 21:01:43 2021
+@@ -33,6 +33,10 @@
+ #ifndef SCREEN_WINDOW_H
+ #define SCREEN_WINDOW_H
+ 
++#ifdef _OpenBSD_
++#include <sys/socket.h>
++#endif
++
+ /* keep this in sync with the initialisations in window.c */
+ struct NewWindow
+ {
