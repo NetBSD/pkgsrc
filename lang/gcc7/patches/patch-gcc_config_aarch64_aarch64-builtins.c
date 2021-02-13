c97f2cbb9402491624cbf37217cd8b037c5a12c0
maya <maya@pkgsrc.org>
2021-02-13 15:56:16 +0000

    lang/gcc{6,7,8,9,0}: call SUBTARGET_INIT_BUILTINS on sparc* too
    While here point out that the aarch64 equivalent patch was sent upstream.
    Bump PKGREVISION. fix gcc*-libs PKGREVISION accordingly.
    
    Fixes PR pkg/55992: math/blas fails on NetBSD/sparc64
    Fixes report by Connor McLaughlan on pkgsrc-users

--- lang/gcc7/patches/patch-gcc_config_aarch64_aarch64-builtins.c
+++ lang/gcc7/patches/patch-gcc_config_aarch64_aarch64-builtins.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-gcc_config_aarch64_aarch64-builtins.c,v 1.1 2018/11/10 04:24:00 mrg Exp $
+$NetBSD: patch-gcc_config_aarch64_aarch64-builtins.c,v 1.2 2021/02/13 15:56:16 maya Exp $
 
 initialise subtarget builtins so cabs*() are renamed.
+https://gcc.gnu.org/pipermail/gcc-patches/2021-February/565289.html
 
 --- gcc/config/aarch64/aarch64-builtins.c.orig	2017-01-20 21:03:41.000000000 +0000
 +++ gcc/config/aarch64/aarch64-builtins.c	2018-11-10 00:44:41.905576216 +0000
