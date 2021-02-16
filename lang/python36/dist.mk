b87b1e28c376fb3a4698d7ca99356c3906bc17ca
adam <adam@pkgsrc.org>
2021-02-16 19:39:53 +0000

    python36 py36-html-docs: updated to 3.6.13
    Python 3.6.13 final
    
    Security
    bpo-42967: Fix web cache poisoning vulnerability by defaulting the query args separator to &, and allowing the user to choose a custom separator.
    bpo-42938: Avoid static buffers when computing the repr of ctypes.c_double and ctypes.c_longdouble values.
    bpo-42103: Prevented potential DoS attack via CPU and RAM exhaustion when processing malformed Apple Property List files in binary format.
    bpo-42051: The plistlib module no longer accepts entity declarations in XML plist files to avoid XML vulnerabilities. This should not affect users as entity declarations are not used in regular plist files.
    bpo-40791: Add volatile to the accumulator variable in hmac.compare_digest, making constant-time-defeating optimizations less likely.
    
    Core and Builtins
    bpo-35560: Fix an assertion error in format() in debug build for floating point formatting with “n” format, zero padding and small width. Release build is not impacted. Patch by Karthikeyan Singaravelan.
    
    Library
    bpo-42103: InvalidFileException and RecursionError are now the only errors caused by loading malformed binary Plist file (previously ValueError and TypeError could be raised in some specific cases).
    
    Tests
    bpo-42794: Update test_nntplib to use offical group name of news.aioe.org for testing. Patch by Dong-hee Na.
    bpo-41944: Tests for CJK codecs no longer call eval() on content received via HTTP.

--- lang/python36/dist.mk
+++ lang/python36/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.13 2020/08/19 07:07:46 adam Exp $
+# $NetBSD: dist.mk,v 1.14 2021/02/16 19:39:53 adam Exp $
 
-PY_DISTVERSION=	3.6.12
+PY_DISTVERSION=	3.6.13
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python36/distinfo
