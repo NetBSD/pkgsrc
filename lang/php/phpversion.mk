1f4c5533059e7eb1e3d123ed9b5da70fdba5ef80
bsiegert <bsiegert@pkgsrc.org>
2021-02-16 08:41:32 +0000

    Pullup ticket #6427 - requested by taca lang/php73: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.320
    - lang/php73/distinfo                                           1.32
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Feb  5 14:46:58 UTC 2021
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php73: distinfo
    
       Log Message:
       lang/php73: update to 7.3.27
    
       04 Feb 2021, PHP 7.3.27
    
       - SOAP:
         . Fixed bug #80672 (Null Dereference in SoapClient). (CVE-2021-21702) (cmb, Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.315.2.3 2021/02/16 08:36:26 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.315.2.4 2021/02/16 08:41:32 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.34
-PHP73_VERSION=	7.3.26
+PHP73_VERSION=	7.3.27
 PHP74_VERSION=	7.4.15
 
 # Define initial release of major version.
