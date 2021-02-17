dfc726eca0e9cc29b5581fd3e9e3e1c92f9d3d12
wiz <wiz@pkgsrc.org>
2021-02-17 21:36:03 +0000

    bind: update to 9.16.12.
    XXX: why does this have so many patches?
    
    	--- 9.16.12 released ---
    
    5578.	[protocol]	Make "check-names" accept A records below "_spf",
    			"_spf_rate", and "_spf_verify" labels in order to cater
    			for the "exists" SPF mechanism specified in RFC 7208
    			section 5.7 and appendix D.1. [GL #2377]
    
    5577.	[bug]		Fix the "three is a crowd" key rollover bug in KASP by
    			correctly implementing Equation (2) of the "Flexible and
    			Robust Key Rollover" paper. [GL #2375]
    
    5575.	[bug]		When migrating to KASP, BIND 9 considered keys with the
    			"Inactive" and/or "Delete" timing metadata to be
    			possible active keys. This has been fixed. [GL #2406]
    
    5572.	[bug]		Address potential double free in generatexml().
    			[GL #2420]
    
    5571.	[bug]		named failed to start when its configuration included a
    			zone with a non-builtin "allow-update" ACL attached.
    			[GL #2413]
    
    5570.	[bug]		Improve performance of the DNSSEC verification code by
    			reducing the number of repeated calls to
    			dns_dnssec_keyfromrdata(). [GL #2073]
    
    5569.	[bug]		Emit useful error message when "rndc retransfer" is
    			applied to a zone of inappropriate type. [GL #2342]
    
    5568.	[bug]		Fixed a crash in "dnssec-keyfromlabel" when using ECDSA
    			keys. [GL #2178]
    
    5567.	[bug]		Dig now reports unknown dash options while pre-parsing
    			the options. This prevents "-multi" instead of "+multi"
    			from reporting memory usage before ending option parsing
    			with "Invalid option: -lti". [GL #2403]
    
    5566.	[func]		Add "stale-answer-client-timeout" option, which is the
    			amount of time a recursive resolver waits before
    			attempting to answer the query using stale data from
    			cache. [GL #2247]
    
    5565.	[func]		The SONAMEs for BIND 9 libraries now include the current
    			BIND 9 version number, in an effort to tightly couple
    			internal libraries with a specific release. [GL #2387]
    
    5562.	[security]	Fix off-by-one bug in ISC SPNEGO implementation.
    			(CVE-2020-8625) [GL #2354]
    
    5561.	[bug]		KASP incorrectly set signature validity to the value of
    			the DNSKEY signature validity. This is now fixed.
    			[GL #2383]
    
    5560.	[func]		The default value of "max-stale-ttl" has been changed
    			from 12 hours to 1 day and the default value of
    			"stale-answer-ttl" has been changed from 1 second to 30
    			seconds, following RFC 8767 recommendations. [GL #2248]
    
    5456.	[func]		Added "primaries" as a synonym for "masters" in
    			named.conf, and "primary-only" as a synonym for
    			"master-only" in the parameters to "notify", to bring
    			terminology up-to-date with RFC 8499. [GL #1948]
    
    5362.	[func]		Limit the size of IXFR responses so that AXFR will
    			be used instead if it would be smaller. This is
    			controlled by the "max-ixfr-ratio" option, which
    			is a percentage representing the ratio of IXFR size
    			to the size of the entire zone. This value cannot
    			exceed 100%, which is the default. [GL #1515]

--- net/bind916/patches/patch-lib_ns_client.c
+++ net/bind916/patches/patch-lib_ns_client.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_ns_client.c,v 1.1 2020/08/09 15:20:22 taca Exp $
+$NetBSD: patch-lib_ns_client.c,v 1.2 2021/02/17 21:36:03 wiz Exp $
 
 * Take from NetBSD base.
 
---- lib/ns/client.c.orig	2020-05-06 09:59:35.000000000 +0000
+--- lib/ns/client.c.orig	2021-02-04 11:35:16.000000000 +0000
 +++ lib/ns/client.c
 @@ -87,8 +87,8 @@
  	isc_log_write(ns_lctx, NS_LOGCATEGORY_CLIENT, NS_LOGMODULE_CLIENT, \
@@ -24,7 +24,7 @@ $NetBSD: patch-lib_ns_client.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  LIBNS_EXTERNAL_DATA atomic_uint_fast32_t ns_client_requests;
  #else  /* if defined(_WIN32) && !defined(_WIN64) */
  LIBNS_EXTERNAL_DATA atomic_uint_fast64_t ns_client_requests;
-@@ -2334,6 +2334,8 @@ ns__client_setup(ns_client_t *client, ns
+@@ -2309,6 +2309,8 @@ ns__client_setup(ns_client_t *client, ns
  		 * Retain these values from the existing client, but
  		 * zero every thing else.
  		 */
@@ -33,11 +33,11 @@ $NetBSD: patch-lib_ns_client.c,v 1.1 2020/08/09 15:20:22 taca Exp $
  		*client = (ns_client_t){ .magic = 0,
  					 .mctx = oldmctx,
  					 .manager = oldmgr,
-@@ -2342,6 +2344,7 @@ ns__client_setup(ns_client_t *client, ns
+@@ -2317,6 +2319,7 @@ ns__client_setup(ns_client_t *client, ns
  					 .sendbuf = sendbuf,
  					 .message = message,
  					 .query = query };
 +#endif
  	}
  
- 	client->state = NS_CLIENTSTATE_INACTIVE;
+ 	client->query.attributes &= ~NS_QUERYATTR_ANSWERED;
