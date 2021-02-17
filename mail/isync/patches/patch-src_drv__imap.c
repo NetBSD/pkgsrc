0b170dc397b2647400d008073e485363bd7f550d
ryoon <ryoon@pkgsrc.org>
2021-02-17 15:10:11 +0000

    isync: Update to 1.4.0
    * Import upstream patch to fix runtime errors.
    
    Changelog:
    This is a feature release that comes with significant new functionality:
    
    - The IMAP '$Forwarded' / Maildir 'P' (passed) flag is supported now.
    
    - Support for configuring a TLS cipher string was added.
    
    - IMAP mailbox subscriptions are supported now.
    
    - The IMAP user query can be scripted now.
    
    - Added built-in support for the macOS Keychain.
    
    - Messages excluded by MaxSize will now result in placeholders.
    
    Compatibility concerns:
    
    - The 'isync' compatibility wrapper was removed.
    
    - A C11 compiler is required for building now.
    
    - The validity of the config file is checked more stricly now, including:
      - Appearance of options in unexpected places
      - The capitalization of INBOX
    
    - The new TLSv1.3 flag must be added to SSLVersions if the option is
      used, unless disabling that version is desired (which is unlikely).
    
    - Removed support for the obsolete/insecure SSL v3.
    
    - The use of Master/Slave terminology has been deprecated.
    
    Bugfixes:
    
    - All bugfixes up to 1.3.4 are included.
    - IMAP protocol errors are handled more robustly now.
    - Fixed support for SASL's built-in EXTERNAL mechanism.
    - Improved reliability of synchronization when resuming interrupted runs.
    - Fixed MaxSize being ignored under certain circumstances when only one of
      New and ReNew was requested.
    - Fixed a network inefficiency occurring with server-side mailboxes that
      receive new messages only via mbsync.

--- mail/isync/patches/patch-src_drv__imap.c
+++ mail/isync/patches/patch-src_drv__imap.c
@@ -1,15 +1,103 @@
-$NetBSD: patch-src_drv__imap.c,v 1.1 2020/06/12 13:46:22 ryoon Exp $
+$NetBSD: patch-src_drv__imap.c,v 1.2 2021/02/17 15:10:11 ryoon Exp $
 
-* Google's Gmail access token is longer than 80 bytes.
+* Fix some runtime errors.
+  From upstream Git: 9e3041de9367113f3b2d7d7543d2eae4c78e2c67 to 95a83c8220861185906df5b38eed589984847bfb
 
---- src/drv_imap.c.orig	2019-03-10 10:27:55.000000000 +0000
+--- src/drv_imap.c
 +++ src/drv_imap.c
-@@ -1867,7 +1867,7 @@ ensure_password( imap_server_conf_t *srv
- 	if (cmd) {
- 		FILE *fp;
- 		int ret;
--		char buffer[80];
-+		char buffer[1024];
+@@ -924,6 +924,7 @@ parse_imap_list( imap_store_t *ctx, char **sp, parse_list_state_t *sts )
+ 	}
+   bail:
+ 	free_list( sts->head );
++	sts->level = 0;
+ 	return LIST_BAD;
+ }
  
- 		if (*cmd == '+') {
- 			flushn();
+@@ -1181,7 +1182,8 @@ parse_fetch_rsp( imap_store_t *ctx, list_t *list, char *s ATTR_UNUSED )
+ 		for (cmdp = ctx->in_progress; cmdp; cmdp = cmdp->next)
+ 			if (cmdp->param.uid == uid)
+ 				goto gotuid;
+-		goto badrsp;
++		error( "IMAP error: unexpected FETCH response with BODY (UID %u)\n", uid );
++		return LIST_BAD;
+ 	  gotuid:
+ 		msgdata = ((imap_cmd_fetch_msg_t *)cmdp)->msg_data;
+ 		msgdata->data = body->val;
+@@ -1208,9 +1210,8 @@ parse_fetch_rsp( imap_store_t *ctx, list_t *list, char *s ATTR_UNUSED )
+ 			memcpy( cur->tuid, tuid, TUIDL );
+ 		status &= ~(M_FLAGS | M_RECENT | M_SIZE | M_HEADER);
+ 	} else {
+-	  badrsp:
+-		error( "IMAP error: unexpected FETCH response (UID %u)\n", uid );
+-		return LIST_BAD;
++		// These may come in as a result of STORE FLAGS despite .SILENT.
++		status &= ~(M_FLAGS | M_RECENT);
+ 	}
+ 
+ 	if (status) {
+@@ -1252,48 +1253,53 @@ parse_response_code( imap_store_t *ctx, imap_cmd_t *cmd, char *s )
+ 	if (!s || *s != '[')
+ 		return RESP_OK;		/* no response code */
+ 	s++;
+-	if (!(p = strchr( s, ']' ))) {
+-	  bad_resp:
++	if (!(arg = next_arg( &s ))) {
+ 		error( "IMAP error: malformed response code\n" );
+ 		return RESP_CANCEL;
+ 	}
+-	*p++ = 0;
+-	if (!(arg = next_arg( &s )))
+-		goto bad_resp;
+ 	if (!strcmp( "UIDVALIDITY", arg )) {
+ 		if (!(arg = next_arg( &s )) ||
+-		    (ctx->uidvalidity = strtoul( arg, &earg, 10 ), *earg))
++		    (ctx->uidvalidity = strtoul( arg, &earg, 10 ), *earg != ']'))
+ 		{
+ 			error( "IMAP error: malformed UIDVALIDITY status\n" );
+ 			return RESP_CANCEL;
+ 		}
+ 	} else if (!strcmp( "UIDNEXT", arg )) {
+ 		if (!(arg = next_arg( &s )) ||
+-		    (ctx->uidnext = strtoul( arg, &earg, 10 ), *earg))
++		    (ctx->uidnext = strtoul( arg, &earg, 10 ), *earg != ']'))
+ 		{
+ 			error( "IMAP error: malformed UIDNEXT status\n" );
+ 			return RESP_CANCEL;
+ 		}
+ 	} else if (!strcmp( "CAPABILITY", arg )) {
++		if (!(p = strchr( s, ']' ))) {
++			error( "IMAP error: malformed CAPABILITY status\n" );
++			return RESP_CANCEL;
++		}
++		*p = 0;
+ 		parse_capability( ctx, s );
+-	} else if (!strcmp( "ALERT", arg )) {
++	} else if (!strcmp( "ALERT]", arg )) {
+ 		/* RFC2060 says that these messages MUST be displayed
+ 		 * to the user
+ 		 */
+-		for (; isspace( (uchar)*p ); p++);
+-		error( "*** IMAP ALERT *** %s\n", p );
++		if (!s) {
++			error( "IMAP error: malformed ALERT status\n" );
++			return RESP_CANCEL;
++		}
++		for (; isspace( (uchar)*s ); s++);
++		error( "*** IMAP ALERT *** %s\n", s );
+ 	} else if (cmd && !strcmp( "APPENDUID", arg )) {
+ 		if (!(arg = next_arg( &s )) ||
+ 		    (ctx->uidvalidity = strtoul( arg, &earg, 10 ), *earg) ||
+ 		    !(arg = next_arg( &s )) ||
+-		    (((imap_cmd_out_uid_t *)cmd)->out_uid = strtoul( arg, &earg, 10 ), *earg))
++		    (((imap_cmd_out_uid_t *)cmd)->out_uid = strtoul( arg, &earg, 10 ), *earg != ']'))
+ 		{
+ 			error( "IMAP error: malformed APPENDUID status\n" );
+ 			return RESP_CANCEL;
+ 		}
+ 	} else if (!strcmp( "PERMANENTFLAGS", arg )) {
+ 		parse_list_init( &ctx->parse_list_sts );
+-		if (parse_imap_list( NULL, &s, &ctx->parse_list_sts ) != LIST_OK) {
++		if (parse_imap_list( NULL, &s, &ctx->parse_list_sts ) != LIST_OK || *s != ']') {
+ 			error( "IMAP error: malformed PERMANENTFLAGS status\n" );
+ 			return RESP_CANCEL;
+ 		}
