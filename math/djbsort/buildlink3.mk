d7f2e077804544519e31b74a106428d89748f237
schmonz <schmonz@pkgsrc.org>
2021-02-18 10:08:31 +0000

    Update to 20190516. From the changelog:
    Benchmarking:
    - Speed tests now call cpucycles() before setting resource limits. This
      is important on platforms where cpucycles() needs to read files.
    
    Verification:
    - Support for SignExt and several more peephole optimizations, working
      towards support for simpler symbolic-execution backend. Various
      updates to work with angr8 and python3.

--- math/djbsort/buildlink3.mk
+++ math/djbsort/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/07/31 05:38:56 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/02/18 10:08:31 schmonz Exp $
 
 BUILDLINK_TREE+=	djbsort
 
 .if !defined(DJBSORT_BUILDLINK3_MK)
 DJBSORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.djbsort+=	djbsort>=20180717
+BUILDLINK_API_DEPENDS.djbsort+=	djbsort>=20190516
 BUILDLINK_PKGSRCDIR.djbsort?=	../../math/djbsort
 BUILDLINK_DEPMETHOD.djbsort?=	build
 BUILDLINK_FILES_CMD.djbsort=	${CAT} ${BUILDLINK_PREFIX.djbsort}/share/djbsort/pseudo-PLIST
