c0ce0bbe994edc5cb4e95c7cb6e7a43b2551916d
ryoon <ryoon@pkgsrc.org>
2021-02-20 22:55:19 +0000

    emulators/qemu51: import qemu-5.1.0nb13
    QEMU is a FAST! processor emulator using dynamic translation to achieve
    good emulation speed, QEMU has two operating modes:
    
        * Full system emulation. In this mode, QEMU emulates a full system
          (for example a PC), including a processor and various peripherals.
          It can be used to launch different Operating Systems without rebooting
          the PC or to debug system code.
        * User mode emulation (Linux host only). In this mode, QEMU can launch
          Linux processes compiled for one CPU on another CPU. It can be used
          to launch the Wine Windows API emulator or to ease cross-compilation
          and cross-debugging.
    
    This package contains Qemu 5.1.0 with NetBSD NVMM support.

---
+++ emulators/qemu51/patches/patch-hw_usb_dev-mtp.c
@@ -0,0 +1,27 @@
+$NetBSD: patch-hw_usb_dev-mtp.c,v 1.1 2021/02/20 22:55:19 ryoon Exp $
+
+Support NAME_MAX and compat for O_DIRECTORY.
+
+--- hw/usb/dev-mtp.c.orig	2019-04-23 18:14:46.000000000 +0000
++++ hw/usb/dev-mtp.c
+@@ -26,6 +26,10 @@
+ #include "desc.h"
+ #include "qemu/units.h"
+ 
++#ifndef NAME_MAX
++#define NAME_MAX 255
++#endif
++
+ /* ----------------------------------------------------------------------- */
+ 
+ enum mtp_container_type {
+@@ -614,6 +618,9 @@ static void usb_mtp_object_readdir(MTPSt
+     }
+     o->have_children = true;
+ 
++#ifndef O_DIRECTORY
++#define O_DIRECTORY	0
++#endif
+     fd = open(o->path, O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW);
+     if (fd < 0) {
+         return;
---
+++ emulators/qemu51/patches/patch-include_sysemu_hw__accel.h
@@ -0,0 +1,55 @@
+$NetBSD: patch-include_sysemu_hw__accel.h,v 1.1 2021/02/20 22:55:19 ryoon Exp $
+
+Add NVMM support.
+
+--- include/sysemu/hw_accel.h.orig	2020-08-11 19:17:15.000000000 +0000
++++ include/sysemu/hw_accel.h
+@@ -16,6 +16,7 @@
+ #include "sysemu/kvm.h"
+ #include "sysemu/hvf.h"
+ #include "sysemu/whpx.h"
++#include "sysemu/nvmm.h"
+ 
+ static inline void cpu_synchronize_state(CPUState *cpu)
+ {
+@@ -31,6 +32,9 @@ static inline void cpu_synchronize_state
+     if (whpx_enabled()) {
+         whpx_cpu_synchronize_state(cpu);
+     }
++    if (nvmm_enabled()) {
++        nvmm_cpu_synchronize_state(cpu);
++    }
+ }
+ 
+ static inline void cpu_synchronize_post_reset(CPUState *cpu)
+@@ -47,6 +51,10 @@ static inline void cpu_synchronize_post_
+     if (whpx_enabled()) {
+         whpx_cpu_synchronize_post_reset(cpu);
+     }
++    if (nvmm_enabled()) {
++        nvmm_cpu_synchronize_post_reset(cpu);
++    }
++
+ }
+ 
+ static inline void cpu_synchronize_post_init(CPUState *cpu)
+@@ -63,6 +71,9 @@ static inline void cpu_synchronize_post_
+     if (whpx_enabled()) {
+         whpx_cpu_synchronize_post_init(cpu);
+     }
++    if (nvmm_enabled()) {
++        nvmm_cpu_synchronize_post_init(cpu);
++    }
+ }
+ 
+ static inline void cpu_synchronize_pre_loadvm(CPUState *cpu)
+@@ -79,6 +90,9 @@ static inline void cpu_synchronize_pre_l
+     if (whpx_enabled()) {
+         whpx_cpu_synchronize_pre_loadvm(cpu);
+     }
++    if (nvmm_enabled()) {
++        nvmm_cpu_synchronize_pre_loadvm(cpu);
++    }
+ }
+ 
+ #endif /* QEMU_HW_ACCEL_H */
