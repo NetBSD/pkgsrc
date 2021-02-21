c62fb30b7d480fda24b50aa0b6b8586ec3e4edb3
adam <adam@pkgsrc.org>
2021-02-21 12:45:22 +0000

    meson: updated to 0.57.1
    Release 0.57.0
    * Project version can be specified with a file
    * Support for reading files at configuration time with the fs module
    * meson install --dry-run
    * Experimental support for C++ modules in Visual Studio
    * Qt6 module
    * Unstable Rust module
    * Meson test() now accepts protocol : 'rust'
    * MSVC/Clang-Cl Argument Changes/Cleanup
    * Buildtype remains even if dependent options are changed
    * Passing internal dependencies to the compiler object
    * unstable_external_project improvements
    * gnome.post_install()
    * "Edit and continue" (/ZI) is no longer used by default for Visual Studio
    * Minimum required Python version updated to 3.6
    * Packaging a subproject
    * custom_target() and run_target() now accepts an env keyword argument
    * summary() accepts external programs or dependencies
    * CMake find_package version support
    * meson test only rebuilds test dependencies
    * The add_*_script methods now accept a File as the first argument
    * Unity build with Vala disabled
    * New logging format for meson test
    * Specify DESTDIR on command line
    * Skip install scripts if DESTDIR is set
    * Add support for prelinked static libraries
    * Rust now has an std option
    * Ctrl-C behavior in meson test
    * Support added for LLVM's thinLTO
    * test() timeout and timeout_multiplier value <= 0
    * Knob to control LTO thread
    * summary() now uses left alignment for both keys and values
    * // is now allowed as a function id for meson rewrite.
    * Get keys of configuration data object

--- devel/meson/patches/patch-mesonbuild_compilers_mixins_gnu.py
+++ devel/meson/patches/patch-mesonbuild_compilers_mixins_gnu.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-mesonbuild_compilers_mixins_gnu.py,v 1.1 2020/08/07 13:04:08 jperkin Exp $
+$NetBSD: patch-mesonbuild_compilers_mixins_gnu.py,v 1.2 2021/02/21 12:45:23 adam Exp $
 
 Do not default to -z ignore on SunOS, it breaks for example -fstack-protector.
 
---- mesonbuild/compilers/mixins/gnu.py.orig	2020-06-29 17:00:07.000000000 +0000
+--- mesonbuild/compilers/mixins/gnu.py.orig	2021-02-08 21:39:00.000000000 +0000
 +++ mesonbuild/compilers/mixins/gnu.py
-@@ -144,7 +144,7 @@ class GnuLikeCompiler(metaclass=abc.ABCM
-                              'b_ndebug', 'b_staticpic', 'b_pie']
+@@ -153,7 +153,7 @@ class GnuLikeCompiler(Compiler, metaclas
+                                    'b_ndebug', 'b_staticpic', 'b_pie']}
          if not (self.info.is_windows() or self.info.is_cygwin() or self.info.is_openbsd()):
-             self.base_options.append('b_lundef')
+             self.base_options.add(OptionKey('b_lundef'))
 -        if not self.info.is_windows() or self.info.is_cygwin():
 +        if not (self.info.is_windows() or self.info.is_sunos()) or self.info.is_cygwin():
-             self.base_options.append('b_asneeded')
+             self.base_options.add(OptionKey('b_asneeded'))
          if not self.info.is_hurd():
-             self.base_options.append('b_sanitize')
+             self.base_options.add(OptionKey('b_sanitize'))
