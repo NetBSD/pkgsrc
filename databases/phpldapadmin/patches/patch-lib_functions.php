1ba7330de3e09473715ed12941ead639ed06513c
khorben <khorben@pkgsrc.org>
2021-02-21 22:22:22 +0000

    phpldapadmin: import fixes for PHP > 5.5, 7.2, 7.3
    With help from the patches available at
    https://packages.debian.org/source/sid/phpldapadmin.

--- databases/phpldapadmin/patches/patch-lib_functions.php
+++ databases/phpldapadmin/patches/patch-lib_functions.php
@@ -1,11 +1,65 @@
-$NetBSD: patch-lib_functions.php,v 1.3 2015/11/29 11:25:53 taca Exp $
+$NetBSD: patch-lib_functions.php,v 1.4 2021/02/21 22:22:22 khorben Exp $
 
 Fix for PHP 5.5 and later: 
 	https://bugzilla.redhat.com/show_bug.cgi?id=974928
 
 --- lib/functions.php.orig	2012-10-01 06:54:14.000000000 +0000
 +++ lib/functions.php
-@@ -2127,7 +2127,7 @@ function password_types() {
+@@ -51,7 +51,7 @@ if (file_exists(LIBDIR.'functions.custom
+ /**
+  * Loads class definition
+  */
+-function __autoload($className) {
++function pla_autoload($className) {
+ 	if (file_exists(HOOKSDIR."classes/$className.php"))
+ 		require_once(HOOKSDIR."classes/$className.php");
+ 	elseif (file_exists(LIBDIR."$className.php"))
+@@ -66,6 +66,12 @@ function __autoload($className) {
+ 			'type'=>'error'));
+ }
+ 
++if (version_compare(phpversion(), '7.0', '>=')) {
++	spl_autoload_register('pla_autoload');
++} else {
++	eval('function __autoload($className) {pla_autoload($className);}');
++}
++
+ /**
+  * Strips all slashes from the specified array in place (pass by ref).
+  * @param Array The array to strip slashes from, typically one of
+@@ -994,6 +1000,22 @@ function get_custom_file($index,$filenam
+ }
+ 
+ /**
++ * Replacement for create_function() which is deprecated as of PHP 7.2
++ *
++ * @param string The function arguments
++ * @param string The function code
++ */
++function pla_create_function($args, $code) {
++	if (version_compare(phpversion(), '7.0', '>=')) {
++		# anonymous functions were introduced in PHP 5.3.0
++		return eval("return function(".$args."){".$code."};");
++	} else {
++		# create_function is deprecated in PHP 7.2
++		return create_function($args, $code);
++	}
++}
++
++/**
+  * Sort a multi dimensional array.
+  *
+  * @param array Multi demension array passed by reference
+@@ -1080,7 +1102,7 @@ function masort(&$data,$sortby,$rev=0) {
+ 
+ 		$code .= 'return $c;';
+ 
+-		$CACHE[$sortby] = create_function('$a, $b',$code);
++		$CACHE[$sortby] = pla_create_function('$a, $b',$code);
+ 	}
+ 
+ 	uasort($data,$CACHE[$sortby]);
+@@ -2127,7 +2149,7 @@ function password_types() {
   *        crypt, ext_des, md5crypt, blowfish, md5, sha, smd5, ssha, sha512, or clear.
   * @return string The hashed password.
   */
@@ -14,7 +68,7 @@ Fix for PHP 5.5 and later:
  	if (DEBUG_ENABLED && (($fargs=func_get_args())||$fargs='NOARGS'))
  		debug_log('Entered (%%)',1,0,__FILE__,__LINE__,__METHOD__,$fargs);
  
-@@ -2318,7 +2318,7 @@ function password_check($cryptedpassword
+@@ -2318,7 +2340,7 @@ function password_check($cryptedpassword
  
  		# SHA crypted passwords
  		case 'sha':
@@ -23,7 +77,7 @@ Fix for PHP 5.5 and later:
  				return true;
  			else
  				return false;
-@@ -2327,7 +2327,7 @@ function password_check($cryptedpassword
+@@ -2327,7 +2349,7 @@ function password_check($cryptedpassword
  
  		# MD5 crypted passwords
  		case 'md5':
@@ -32,7 +86,16 @@ Fix for PHP 5.5 and later:
  				return true;
  			else
  				return false;
-@@ -2565,12 +2565,14 @@ function dn_unescape($dn) {
+@@ -2392,7 +2414,7 @@ function password_check($cryptedpassword
+ 
+ 		# SHA512 crypted passwords
+ 		case 'sha512':
+-			if (strcasecmp(password_hash($plainpassword,'sha512'),'{SHA512}'.$cryptedpassword) == 0)
++			if (strcasecmp(pla_password_hash($plainpassword,'sha512'),'{SHA512}'.$cryptedpassword) == 0)
+ 				return true;
+ 			else
+ 				return false;
+@@ -2565,12 +2587,14 @@ function dn_unescape($dn) {
  		$a = array();
  
  		foreach ($dn as $key => $rdn)
