23b4961558bcb0e905bc4fb9ef44c914670ef0d2
adam <adam@pkgsrc.org>
2021-02-21 12:45:22 +0000

    meson: updated to 0.57.1
    Release 0.57.0
    * Project version can be specified with a file
    * Support for reading files at configuration time with the fs module
    * meson install --dry-run
    * Experimental support for C++ modules in Visual Studio
    * Qt6 module
    * Unstable Rust module
    * Meson test() now accepts protocol : 'rust'
    * MSVC/Clang-Cl Argument Changes/Cleanup
    * Buildtype remains even if dependent options are changed
    * Passing internal dependencies to the compiler object
    * unstable_external_project improvements
    * gnome.post_install()
    * "Edit and continue" (/ZI) is no longer used by default for Visual Studio
    * Minimum required Python version updated to 3.6
    * Packaging a subproject
    * custom_target() and run_target() now accepts an env keyword argument
    * summary() accepts external programs or dependencies
    * CMake find_package version support
    * meson test only rebuilds test dependencies
    * The add_*_script methods now accept a File as the first argument
    * Unity build with Vala disabled
    * New logging format for meson test
    * Specify DESTDIR on command line
    * Skip install scripts if DESTDIR is set
    * Add support for prelinked static libraries
    * Rust now has an std option
    * Ctrl-C behavior in meson test
    * Support added for LLVM's thinLTO
    * test() timeout and timeout_multiplier value <= 0
    * Knob to control LTO thread
    * summary() now uses left alignment for both keys and values
    * // is now allowed as a function id for meson rewrite.
    * Get keys of configuration data object

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,19 +1,19 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.1 2020/04/23 07:00:09 triaxx Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.2 2021/02/21 12:45:23 adam Exp $
 
-* Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
-  a condition to match FreeBSD path norm for pkg-config files.
+Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
+a condition to match FreeBSD path norm for pkg-config files.
 
---- mesonbuild/modules/pkgconfig.py.orig	2020-03-23 17:22:09.000000000 +0000
+--- mesonbuild/modules/pkgconfig.py.orig	2021-02-01 20:35:16.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -496,10 +496,7 @@ class PkgConfigModule(ExtensionModule):
+@@ -530,10 +530,7 @@ class PkgConfigModule(ExtensionModule):
          pcfile = filebase + '.pc'
          pkgroot = kwargs.get('install_dir', default_install_dir)
          if pkgroot is None:
 -            if mesonlib.is_freebsd():
--                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('prefix'), 'libdata', 'pkgconfig')
+-                pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('prefix')), 'libdata', 'pkgconfig')
 -            else:
--                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
-+            pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
+-                pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
++            pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
          if not isinstance(pkgroot, str):
              raise mesonlib.MesonException('Install_dir must be a string.')
          self.generate_pkgconfig_file(state, deps, subdirs, name, description, url,
