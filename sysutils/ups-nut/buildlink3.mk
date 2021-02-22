1de19921be9ba01ccbfbaf6be5e47009ccdef6fa
wiz <wiz@pkgsrc.org>
2021-02-22 17:07:50 +0000

    ups-nut: fix option handling in buildlink3.mk
    From Edgar Fuß.

--- sysutils/ups-nut/buildlink3.mk
+++ sysutils/ups-nut/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/07/13 18:50:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/02/22 17:07:50 wiz Exp $
 
 BUILDLINK_TREE+=	ups-nut
 
@@ -7,6 +7,15 @@ UPS_NUT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ups-nut+=	ups-nut>=2.7.4
 BUILDLINK_PKGSRCDIR.ups-nut?=	../../sysutils/ups-nut
+
+pkgbase:= ups-nut
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.ups-nut:Mopenssl}
+.  include "../../security/openssl/buildlink3.mk"
+.elif ${PKG_BUILD_OPTIONS.ups-nut:Mnss}
+.  include "../../devel/nss/buildlink3.mk"
+.endif
 
 .include "../../mk/pthread.buildlink3.mk"
 .endif	# UPS_NUT_BUILDLINK3_MK
