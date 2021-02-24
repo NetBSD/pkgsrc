9528675f72d88c81577b0953ca67c862b2b92d00
hauke <hauke@pkgsrc.org>
2021-02-24 18:54:03 +0000

    Fix buildlink path.

--- x11/nx-libs/buildlink3.mk
+++ x11/nx-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/02/24 18:38:42 hauke Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/02/24 18:54:03 hauke Exp $
 
 BUILDLINK_TREE+=	nx-libs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nx-libs
 NX_LIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nx-libs+=		nx-libs>=3.5.99
-BUILDLINK_PKGSRCDIR.nx-libs?=		../../hauke/nx-libs
+BUILDLINK_PKGSRCDIR.nx-libs?=		../../x11/nx-libs
 .endif	# NX_LIBS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-nx-libs
