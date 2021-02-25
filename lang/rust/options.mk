1818407c1d4831dead6ee5861c6f4c9357fee1dd
wiz <wiz@pkgsrc.org>
2021-02-25 08:54:05 +0000

    rust: fix typo in comment

--- lang/rust/options.mk
+++ lang/rust/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2021/02/25 08:47:16 nia Exp $
+# $NetBSD: options.mk,v 1.11 2021/02/25 08:54:05 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rust
 PKG_SUPPORTED_OPTIONS+=	rust-cargo-static
@@ -14,7 +14,7 @@ PKG_SUGGESTED_OPTIONS+=		rust-llvm
 .  endif
 .endif
 
-# As of 2021-02-25 cargo is only a static binary in x88_64 and
+# As of 2021-02-25 cargo is only a static binary in x86_64 and
 # powerpc bootstraps
 .if ${OPSYS} == "NetBSD" && \
     ${MACHINE_ARCH} != "i386" && \
