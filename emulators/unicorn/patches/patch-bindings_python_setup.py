411bd00fd6f48883ff749b98fbdd970d16c0373e
leot <leot@pkgsrc.org>
2021-02-25 19:47:50 +0000

    {,py-}unicorn: Update to 1.0.2
    Changes:
    1.0.2
    -----
    - Fix Java binding compilation
    - Enable building for ARM little-endian only (ignore big-endian)
    - Add uc_context_free() API
    - Fix context saving/retoring API (core & Python binding)
    - Add cmake option to build Unicorn as a static library
    - Fix error handling of mmap()
    - uc_emu_start() can be reentrant
    - Fix naming conflicts when built with systemd
    - Fix setjmp/longjmp on native Windows
    - Fix enabled hooks even after deleting them
    - X86:
        - Fix 64bit fstenv
        - Fix IP value of 16bit mode
    - ARM:
        - Fix APSR handling
    - Python: Remove UC_ERR_TIMEOUT
    - No longer require Python to build
    - Fix recursive UC_HOOK_MEM callbacks for cross pages access
    - Remove UC_ERR_TIMEOUT, so timeout on uc_emu_start() is not considered error
    - Added UC_QUERY_TIMEOUT to query exit reason
    - Fix UAF when deleting hook while in hook callback
    - Ensure that hooks are unaffected by a request to stop emulation.
    - Fix block hooks being called twice after an early exit from execution.
    - Fix binding install on python2 (MacOS)
    - X86:
        - Support read/write STn registers
        - Support read/write X64 base regs
    - ARM64:
        - Support some new registers

--- emulators/unicorn/patches/patch-bindings_python_setup.py
+++ emulators/unicorn/patches/patch-bindings_python_setup.py
@@ -1,20 +1,18 @@
-$NetBSD: patch-bindings_python_setup.py,v 1.1 2018/03/24 17:09:34 joerg Exp $
+$NetBSD: patch-bindings_python_setup.py,v 1.2 2021/02/25 19:47:51 leot Exp $
 
---- bindings/python/setup.py.orig	2018-03-24 14:17:22.780317467 +0000
+--- bindings/python/setup.py.orig	2020-10-21 11:34:41.000000000 +0000
 +++ bindings/python/setup.py
-@@ -195,7 +195,6 @@ class custom_sdist(sdist):
- class custom_build(build):
+@@ -208,13 +208,11 @@ class custom_build(build):
+             log.info("Skipping building C extensions since LIBUNICORN_PATH is set")
+         else:
+             log.info("Building C extensions")
+-            build_libraries()
+         return build.run(self)
+ 
+ class custom_develop(develop):
      def run(self):
          log.info("Building C extensions")
 -        build_libraries()
-         return build.run(self)
+         return develop.run(self)
  
  class custom_bdist_egg(bdist_egg):
-@@ -235,7 +234,6 @@ try:
-     class custom_develop(develop):
-         def run(self):
-             log.info("Building C extensions")
--            build_libraries()
-             return develop.run(self)
- 
-     cmdclass['develop'] = custom_develop
--- emulators/unicorn/patches/patch-bindings_python_unicorn_unicorn.py
+++ emulators/unicorn/patches/patch-bindings_python_unicorn_unicorn.py
@@ -1,18 +1,20 @@
-$NetBSD: patch-bindings_python_unicorn_unicorn.py,v 1.1 2018/03/24 17:09:34 joerg Exp $
+$NetBSD: patch-bindings_python_unicorn_unicorn.py,v 1.2 2021/02/25 19:47:51 leot Exp $
 
---- bindings/python/unicorn/unicorn.py.orig	2018-03-24 14:18:11.440281233 +0000
+--- bindings/python/unicorn/unicorn.py.orig	2020-10-21 11:34:41.000000000 +0000
 +++ bindings/python/unicorn/unicorn.py
-@@ -71,18 +71,9 @@ def _load_lib(path):
+@@ -71,20 +71,9 @@ def _load_lib(path):
  _uc = None
  
  # Loading attempts, in order
+-# - user-provided environment variable
 -# - pkg_resources can get us the path to the local libraries
 -# - we can get the path to the local libraries by parsing our filename
 -# - global load
 -# - python's lib directory
 -# - last-gasp attempt at some hardcoded paths on darwin and linux
 -
--_path_list = [pkg_resources.resource_filename(__name__, 'lib'),
+-_path_list = [os.getenv('LIBUNICORN_PATH', None),
+-              pkg_resources.resource_filename(__name__, 'lib'),
 -              os.path.join(os.path.split(__file__)[0], 'lib'),
 -              '',
 -              distutils.sysconfig.get_python_lib(),
