3a4f160c04695a654d8913a95df8744e5835d6ab
nia <nia@pkgsrc.org>
2021-02-25 10:22:14 +0000

    simgear, flightgear: Update to 2020.3.6
    Release notes:
    
    https://wiki.flightgear.org/Changelog_2020.3

--- games/flightgear/hacks.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: hacks.mk,v 1.2 2019/11/23 19:41:02 gdt Exp $
-
-.if ${OPSYS} == "NetBSD" 
-.  include "../../mk/compiler.mk"
-.  if !(${PKGSRC_COMPILER:Mgcc}) && \
-      (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
-CONFIGURE_ARGS+=	--disable-precomp-headers
-.  endif
-.endif
