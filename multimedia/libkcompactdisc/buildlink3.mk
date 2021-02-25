c7128df5d57113bd0da2f07f2193849183fd4c86
markd <markd@pkgsrc.org>
2021-02-25 03:23:49 +0000

    libkcompactdisc: update to 20.04.1
    qt5/kf5 version

--- multimedia/libkcompactdisc/buildlink3.mk
+++ multimedia/libkcompactdisc/buildlink3.mk
@@ -1,13 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/03 12:23:51 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/02/25 03:23:49 markd Exp $
 
 BUILDLINK_TREE+=	libkcompactdisc
 
 .if !defined(LIBKCOMPACTDISC_BUILDLINK3_MK)
 LIBKCOMPACTDISC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkcompactdisc+=	libkcompactdisc>=4.10.2
+BUILDLINK_API_DEPENDS.libkcompactdisc+=	libkcompactdisc>=20.04.1
 BUILDLINK_PKGSRCDIR.libkcompactdisc?=	../../multimedia/libkcompactdisc
 
+.include "../../devel/kcoreaddons/buildlink3.mk"
+.include "../../devel/ki18n/buildlink3.mk"
+.include "../../multimedia/phonon-qt5/buildlink3.mk"
+.include "../../sysutils/solid/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKCOMPACTDISC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkcompactdisc
