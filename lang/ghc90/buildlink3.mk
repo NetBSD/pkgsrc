a2c516a8098d8786ed5fe424892119e7aeeecdd9
nia <nia@pkgsrc.org>
2021-02-25 10:35:48 +0000

    ghc90: Fix buildlink3 file

--- lang/ghc90/buildlink3.mk
+++ lang/ghc90/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/02/09 13:18:36 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/02/25 10:35:48 nia Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=8.8.1
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=8.8.1nb2
-BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc88
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.0
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.0
+BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc90
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/libffi/buildlink3.mk"
