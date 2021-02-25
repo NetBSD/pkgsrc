a8030a215766df497aabf03df35d5f64ef7b1443
nia <nia@pkgsrc.org>
2021-02-25 10:22:14 +0000

    simgear, flightgear: Update to 2020.3.6
    Release notes:
    
    https://wiki.flightgear.org/Changelog_2020.3

--- games/simgear/patches/patch-simgear_canvas_ShivaVG_src_shDefs.h
+++ games/simgear/patches/patch-simgear_canvas_ShivaVG_src_shDefs.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-simgear_canvas_ShivaVG_src_shDefs.h,v 1.1 2018/08/04 04:04:28 nia Exp $
+$NetBSD: patch-simgear_canvas_ShivaVG_src_shDefs.h,v 1.2 2021/02/25 10:22:14 nia Exp $
 
 Support NetBSD.
 
---- simgear/canvas/ShivaVG/src/shDefs.h.orig	2018-06-03 18:56:36.000000000 +0000
+--- simgear/canvas/ShivaVG/src/shDefs.h.orig	2021-01-23 18:01:02.000000000 +0000
 +++ simgear/canvas/ShivaVG/src/shDefs.h
 @@ -161,7 +161,7 @@ SHfloat getMaxFloat();
  
  /* OpenGL headers */
  
--#if defined(VG_API_LINUX) || defined(VG_API_FREEBSD)
-+#if defined(VG_API_LINUX) || defined(VG_API_FREEBSD) || defined(VG_API_NETBSD)
+-#if defined(VG_API_LINUX) || defined(VG_API_FREEBSD) || defined(VG_API_OPENBSD)
++#if defined(VG_API_LINUX) || defined(VG_API_FREEBSD) || defined(VG_API_NETBSD) || defined(VG_API_OPENBSD)
      #include <GL/gl.h>
      #include <GL/glx.h>
  #elif defined(VG_API_MACOSX)
--- games/simgear/patches/patch-simgear_misc_strutils.cxx
+++ games/simgear/patches/patch-simgear_misc_strutils.cxx
@@ -1,16 +1,15 @@
-$NetBSD: patch-simgear_misc_strutils.cxx,v 1.1 2018/08/04 04:04:28 nia Exp $
+$NetBSD: patch-simgear_misc_strutils.cxx,v 1.2 2021/02/25 10:22:14 nia Exp $
 
 Support NetBSD.
 
---- simgear/misc/strutils.cxx.orig	2018-06-03 18:56:36.000000000 +0000
+--- simgear/misc/strutils.cxx.orig	2021-01-23 18:01:02.000000000 +0000
 +++ simgear/misc/strutils.cxx
-@@ -1017,7 +1017,8 @@ std::string error_string(int errnum)
+@@ -1170,7 +1170,7 @@ std::string error_string(int errnum)
    retcode = strerror_s(buf, sizeof(buf), errnum);
  #elif defined(_GNU_SOURCE)
    return std::string(strerror_r(errnum, buf, sizeof(buf)));
--#elif (_POSIX_C_SOURCE >= 200112L) || defined(SG_MAC) || defined(__FreeBSD__)
-+#elif (_POSIX_C_SOURCE >= 200112L) || defined(SG_MAC) || \
-+    defined(__FreeBSD__) || defined(__NetBSD__)
+-#elif (_POSIX_C_SOURCE >= 200112L) || defined(SG_MAC) || defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif (_POSIX_C_SOURCE >= 200112L) || defined(SG_MAC) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
    int retcode;
    // POSIX.1-2001 and POSIX.1-2008
    retcode = strerror_r(errnum, buf, sizeof(buf));
