8b40bea7a66a823cebfbe75b600847a6f9a1263b
nia <nia@pkgsrc.org>
2021-02-25 10:36:52 +0000

    ghc*: nail down dependency patterns

--- lang/ghc7/buildlink3.mk
+++ lang/ghc7/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/03/08 16:42:27 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2021/02/25 10:36:52 nia Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.10.3
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=7.10.3nb3
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.10.3<8
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=7.10.3nb3<8
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc7
 
 .include "../../converters/libiconv/buildlink3.mk"
--- lang/ghc84/buildlink3.mk
+++ lang/ghc84/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/03/08 16:42:27 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/02/25 10:36:52 nia Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=8.4.4
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=8.4.4nb1
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=8.4.4<8.5
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=8.4.4nb1<8.5
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc84
 
 .include "../../converters/libiconv/buildlink3.mk"
