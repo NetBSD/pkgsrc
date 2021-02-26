419ff5f9918436388f19802a0d33031bb509e601
adam <adam@pkgsrc.org>
2021-02-26 06:20:42 +0000

    cmake: updated to 3.19.6
    CMake 3.19.6
    * Intel: Make explicit Fortran preprocessing under Ninja more robust
    * Tests: Update for upstream ninja change to write status on stderr
    * CMakePresets.json: Remove undocumented support for comments
    * FindPython: fix erroneous variable handling

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.9 2021/02/15 20:35:44 adam Exp $
+# $NetBSD: version.mk,v 1.10 2021/02/26 06:20:42 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.19.5
+CMAKE_VERSION=	3.19.6
 CMAKE_API=	${CMAKE_VERSION:R}
