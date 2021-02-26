7af2386d9a06e466176dab08520f0dca3cd52c12
nia <nia@pkgsrc.org>
2021-02-26 12:48:29 +0000

    ImageMagick: allow building without documentation
    the HTML docs make up a significant % of the installed pkg size

--- graphics/ImageMagick6/options.mk
+++ graphics/ImageMagick6/options.mk
@@ -1,11 +1,19 @@
-# $NetBSD: options.mk,v 1.3 2019/11/02 22:37:53 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2021/02/26 12:48:29 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ImageMagick
-PKG_SUPPORTED_OPTIONS=		x11 jp2 djvu openexr wmf
-PKG_SUGGESTED_OPTIONS=		x11 jp2
+PKG_SUPPORTED_OPTIONS=		doc x11 jp2 djvu openexr wmf
+PKG_SUGGESTED_OPTIONS=		doc x11 jp2
 PKG_OPTIONS_LEGACY_OPTS+=	jasper:jp2
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_SRC+=		PLIST
+
+.if !empty(PKG_OPTIONS:Mdoc)
+PLIST_SRC+=		PLIST.doc
+.else
+CONFIGURE_ARGS+=	--disable-docs
+.endif
 
 .if !empty(PKG_OPTIONS:Mx11)
 .include "../../x11/libX11/buildlink3.mk"
