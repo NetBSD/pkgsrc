7f964590d577ac6f563c7cd0a75830d972be3aa0
wiz <wiz@pkgsrc.org>
2021-03-04 14:05:00 +0000

    fna: mono6 -> mono

--- games/fna/buildlink3.mk
+++ games/fna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/01/26 00:34:10 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/03/04 14:05:00 wiz Exp $
 
 BUILDLINK_TREE+=	fna
 
@@ -10,7 +10,7 @@ BUILDLINK_PKGSRCDIR.fna?=	../../games/fna
 
 .include "../../graphics/mojoshader/buildlink3.mk"
 .include "../../graphics/fna3d/buildlink3.mk"
-.include "../../lang/mono6/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
 .endif	# FNA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fna
