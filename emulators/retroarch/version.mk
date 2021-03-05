3da85b3c1a45b5236447435b949f90c0a8b45ecd
nia <nia@pkgsrc.org>
2021-03-05 11:18:39 +0000

    retroarch: Update to 1.9.0
    # 1.9.0
    - AUTOCONFIG: Ensure correct directory is used when saving autoconfig profiles
    - BLUETOOTH: Add a Bluetooth driver (Lakka-only for now)
    - CHEATS: Fix for wrong number of remaining cheat search matches on some machines
    - CHEEVOS: Option to play sound on achievement unlock.
    - CHEEVOS: Upgrade to rcheevos 9.1
    - CHEEVOS: Restore display of unlocked achievements across hardcore modes
    - CHEEVOS: Hash buffered data when available
    - CHEEVOS: Fix 'Auto Save State freezes RetroArch while Cheevos is enabled'
    - CORE OPTIONS: Pressing OK (or clicking/tapping) on a 'boolean toggle' core option no longer opens a drop-down list. The value now toggles directly, just like boolean options everywhere else in the menu
    - CORE OPTIONS: Toggling an option that changes the number of core options being displayed (i.e. things like `Show Advanced Audio/Video Settings) no longer resets the navigation pointer to the start of the list
    - CORE OPTIONS: Before, RetroArch would identify core option values as being 'boolean' if they had labels matching the specific strings enabled or disabled. Most core devs would abide by this, but not always... As a result, we sometimes would end up with misidentified values, with all kinds of Enabled, Off, True, etc. strings littering the menu, in place of proper toggle switches. All boolean-type value labels are now detected, and replaced with standard ON/OFF strings.
    - CLI: A new command line option --load-menu-on-error has been added
    - CRT: On the fly CRT porch adjuments - these changes allow a user to adjust how the porch algorithm generates the 15khz/31khz output. Giving the ability to change over/under scan.
    - CONFIG FILE: Optimise parsing of configuration files
    - DRIVERS: Implemented protection to avoid setting critical drivers to nothing thus preventing the user from locking him/herself out of the program
    - EMSCRIPTEN: Fix input code to ignore unknown keys
    - FFMPEG CORE: Prevent seeking past the end of files (hang fix)
    - FILE I/O: VFS and NBIO interfaces will now use 64-bit fseek/ftell where possible, should allow for reading/writing to files bigger than 2GB
    - INPUT MAPPING/REMAPPING: Add input remap drop-down lists
    - IOS: Fixed iOS 6 version
    - IOS: Hide the home indicator as it obscures the content too frequently
    - IOS/METAL: Metal video driver now works on RetroArch iOS
    - IOS/METAL: Support getting video metrics to support proper touchscreen interactions
    - LOCALIZATION: Updates for several languages (synchronized from Crowdin)
    - MEMORY/LINUX/ANDROID: Fix reporting of free memory
    - MENU: Enlarged INT/UINT selection limit from 999 to 9999
    - MENU: Fix cursor forced to first entry after displaying lists
    - MENU: Make Notification Font option visible when Graphics Widgets are enabled
    - MENU/RGUI: Add optional 'toggle switch' icons
    - MENU/WIDGETS: Add optional widget-based 'load content' launch feedback animation
    - MENU/WIDGETS: Make notification font size option visible when graphics widgets are enabled
    - ODROID GO ADVANCE: Video driver - fix race condition with RGUI callback
    - PLAYLISTS: Change playlists to use dynamic arrays. Instead of a fixed initial 12MB memory allocation (99999 * 128 byte (on 64bit arch)), use a dynamically growing array
    - PLAYLISTS: Playlist base content directory paths - portable playlists
    - PLAYLISTS/SEARCH: Enhanced playlist search functionality
    - PLAYLISTS/DATABASE: Add 'Explore' view
    - PLAYLISTS/DATABASE/EXPLORE: Show system icons in explore view
    - PS2: Improve FPS Limiter
    - RUNAHEAD: Prevent runahead from being disabled *permanently* when an error occurs
    - SHADERS/SLANG: Increased Slang max Parameters, Textures & Passes
    - VIDEO FILTERS/BLARGG: Make Blargg_snes filter customizable
    - X11: Add lightgun support

--- emulators/retroarch/version.mk
+++ emulators/retroarch/version.mk
@@ -1,8 +1,8 @@
-# $NetBSD: version.mk,v 1.7 2020/05/30 14:26:52 nia Exp $
+# $NetBSD: version.mk,v 1.8 2021/03/05 11:18:39 nia Exp $
 #
 # used by emulators/libretro-core-info/Makefile
 # used by emulators/retroarch/Makefile
 
 .if !defined(RETROARCH_VERSION)
-RETROARCH_VERSION=	1.8.8
+RETROARCH_VERSION=	1.9.0
 .endif
