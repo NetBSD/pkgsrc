6efdfff1f0bf59793adfea01f43fd6786323a769
jaapb <jaapb@pkgsrc.org>
2021-03-05 09:19:58 +0000

    Updated lang/polyml to 5.8.1.
    Changes since last version: an option to create a version that supports
    32-bit values for the X86/64 platform with a heap size of up to 16Gb.
    
    Otherwise, minor bugfixes and changes.
    
    The package has also been updated to remove some superfluous patches that
    have been moved upstream and fix a build problem reported in PR pkg/55569

--- lang/polyml/buildlink3.mk
+++ lang/polyml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/06/12 19:32:44 alnsn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/03/05 09:19:58 jaapb Exp $
 
 BUILDLINK_TREE+=	polyml
 
 .if !defined(POLYML_BUILDLINK3_MK)
 POLYML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.polyml+=	polyml>=5.7.1
-BUILDLINK_ABI_DEPENDS.polyml+=	polyml>=5.7.1
+BUILDLINK_API_DEPENDS.polyml+=	polyml>=5.8.1
+BUILDLINK_ABI_DEPENDS.polyml+=	polyml>=5.8.1
 BUILDLINK_PKGSRCDIR.polyml?=	../../lang/polyml
 .endif	# POLYML_BUILDLINK3_MK
 
