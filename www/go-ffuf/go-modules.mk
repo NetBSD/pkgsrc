41b68e447ceaff968cad5366d5442438aef7a8b5
leot <leot@pkgsrc.org>
2021-03-05 21:36:17 +0000

    go-ffuf: Update to 1.2.1
    pkgsrc changes:
     - Switch to use go-module.mk (per upstream)
    
    Changes:
    1.2.1
    -----
     - Changed
       - Add `-input-shell` flag
    
    1.2.0
    -----
     - New
       - Added 405 Method Not Allowed to list of status codes matched by default.
       - New CLI flag `-rate` to set maximum rate of requests per second.
         The adjustment is dynamic.
       - New CLI flag `-config` to define a configuration file with preconfigured
         settings for the job.
       - Ffuf now reads a default configuration file `$HOME/.ffufrc` upon startup.
         Options set in this file are overwritten by the ones provided on CLI.
       - Change banner logging to stderr instead of stdout.
       - New CLI flag `-or` to avoid creating result files if we didn't get any.
       - New CLI flag `-input-shell` to set the shell to be used by `input-cmd`
    
     - Changed
       - Pre-flight errors are now displayed also after the usage text to prevent
         the need to scroll through backlog.
       - Cancelling via SIGINT (Ctrl-C) is now more responsive
       - Fixed issue where a thread would hang due to TCP errors
       - Fixed the issue where the option -ac was overwriting existing filters.
         Now auto-calibration will add them where needed.
       - The `-w` flag now accepts comma delimited values in the form of
         `file1:W1,file2:W2`.
       - Links in the HTML report are now clickable
       - Fixed panic during wordlist flag parsing in Windows systems.

---
+++ www/go-ffuf/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/03/05 21:36:17 leot Exp $
+
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.zip
+GO_MODULE_FILES+=	github.com/pelletier/go-toml/@v/v1.8.1.mod
+GO_MODULE_FILES+=	github.com/pelletier/go-toml/@v/v1.8.1.zip
