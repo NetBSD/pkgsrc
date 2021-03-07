36286d81ef206f9998304274924594dd9c3a66b8
he <he@pkgsrc.org>
2021-03-07 19:49:12 +0000

    Update softhsm2 to version 2.6.1.
    Pkgsrc changes:
     * Change dependency from botan to botan-devel to get version 2.x.
    
    Upstream changes:
    
    SoftHSM 2.6.1 - 2020-04-29
    
    * Issue #542: Support Ed448/X448 for OpenSSL
    * Issue #538: Improved warning and compilation issues for GCC10
    * Issue #527: Fixed compilation issues for MacOS 10.15.4/Xcode 11.4
    
    SoftHSM 2.6.0 - 2020-03-17
    
    * Issue #493: Upgrade to Botan 2.
    * Issue #530: Update appveyor build.
    * Issue #438: Detect crypto algorithms by default.
      (Patch from Alon Bar-Lev)
    * Issue #455: Provide a new configuration option to allow enabling and
      disabling various mechanisms (slots.mechanisms in the softhsm2.conf).
      (Thanks to Jakub Jelen)
    * Issue #479: Increase SQLite busy timeout from 15 seconds to 3 minutes.
      (Patch from Jan Luebbe)
    * Issue #513: Add configuration option to reset state on fork closing all
      sessions rather than keeping all sessions open in duplicate process.
      (Thanks to Anderson Toshiyuki Sasaki)
    * Issue #500: C_WaitForSlotEvent implementation.
      (Patch from massey101)
    * Issue #445: Add wrap support with CKM_AES_CBC.
    
    Bugfixes:
    * Issue #418: Set fields to NULL to avoid double free.
      (Patch from Brian J Murray)
    * Issue #423: ENGINE_load_rdrand is not supported with older openssl.
      (Patch from Alon Bar-Lev)
    * Issue #429: Updated prerequisite to build from repository.
      (Patch from Dharmesh Khandelwal)
    * Issue #434: Fix build issues with CMake.
      (Patch from Peter Wu)
    * Issue #435: Fix botan build without EDDSA.
      (Patch from Peter Wu)
    * Issue #442: Release resources from OSSLEVPSymmetricAlgorithm.
      (Patch from Petr Menšík)
    * Issue #449/#502: Do not copy zero sized buffer avoid null pointer reference.
      (Patch from space88man)
    * Issue #464: Race condition with multiple threads closing last session and
      opening a newer sessions.
      (Patch from Takarth)
    * Issue #452: Fixes to automake build fir undefined macros.
    * Issue #462: User PIN count wrongly calculated.
      (Patch from Ondrej Hlavaty)
    * Issue #516: Fix memory leak in OSSLCryptoFactory.
      (Patch from Anderson Sasaki)
    * Issue #494: Allow null pointers as arguments when count is zero.
      (Patch from Yunjong Jeong)
    * Issue #518: Sporadic problem in closing sessions because of lookup of
      object without prior locking.
    * Issue #506: Check key type for C_EncryptInit and C_DecryptInit.
      (Patch from Yunjong Jeong)
    * Issue #526: Adjust EDDSA code to return valid EC_PARAMS.
      (Patch from Jakub Jelen)
    * Issue #452: Autogen failure on undefined macro AC_MSG_ERROR.
    * Issue #527: Fixed some build errors for GCC 10.
    * Issue #470: Null pointer arguments validation for C_EncryptFinal, etc.

--- security/softhsm2/buildlink3.mk
+++ security/softhsm2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/11/05 09:07:07 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/03/07 19:49:12 he Exp $
 
 BUILDLINK_TREE+=	softhsm2
 
@@ -7,10 +7,10 @@ SOFTHSM2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.softhsm2+=	softhsm2>=2.0.0
 BUILDLINK_ABI_DEPENDS.softhsm2+=	softhsm2>=2.5.0nb4
-BUILDLINK_PKGSRCDIR.softhsm2?=	../../security/softhsm2
+BUILDLINK_PKGSRCDIR.softhsm2?=		../../security/softhsm2
 
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../security/botan/buildlink3.mk"
+.include "../../security/botan-devel/buildlink3.mk"
 .endif	# SOFTHSM2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-softhsm2
