adbe47501d5016541961d5c8ec55da6717f96aea
taca <taca@pkgsrc.org>
2021-03-07 08:08:38 +0000

    lang/php56: properly define PKG_BUILD_OPTIONS
    Include "../../mk/pkg-build-options.mk" in order to define
    PKG_BUILD_OPTIONS properly.

--- lang/php56/buildlink3.mk
+++ lang/php56/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/11/05 09:06:58 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/03/07 08:08:38 taca Exp $
 
 BUILDLINK_TREE+=	php
 
@@ -8,6 +8,10 @@ PHP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.php+=	php>=5.6.0<5.7
 BUILDLINK_ABI_DEPENDS.php+=	php>=5.6.40nb4
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php56
+
+# XXX php?? use PKG_OPTIONS.${PHP_PKG_PREFIX}, but PKGBASE is php
+pkgbase := php
+.include "../../mk/pkg-build-options.mk"
 
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif # PHP_BUILDLINK3_MK
