39925cf7e093ae733eef423b5f0938e217ae0677
jaapb <jaapb@pkgsrc.org>
2021-03-08 20:41:00 +0000

    Updated misc/ocaml-topkg to version 1.0.3.
    This adds support for OCaml 4.12. Otherwise changes are minor.

--- misc/ocaml-topkg/buildlink3.mk
+++ misc/ocaml-topkg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 16:43:05 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/03/08 20:41:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-topkg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-topkg
 OCAML_TOPKG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.3
 BUILDLINK_PKGSRCDIR.ocaml-topkg?=	../../misc/ocaml-topkg
 
 .endif	# OCAML_TOPKG_BUILDLINK3_MK
