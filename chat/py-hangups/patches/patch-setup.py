50b63596005e130df3b89c08205de9276ec3ade1
leot <leot@pkgsrc.org>
2021-03-08 21:28:27 +0000

    py-hangups: Update to 0.4.13
    Changes:
    0.4.13
    ------
     - add --keep-emoticons
     - drop support for Python 3.5
     - add support for Python 3.9
     - show message when image is not found
     - return ConversationEvent from Conversation methods
     - add new Conversation methods
     - add group link sharing support to Conversation
     - add canonical_email to user.User

--- chat/py-hangups/patches/patch-setup.py
+++ chat/py-hangups/patches/patch-setup.py
@@ -1,25 +1,25 @@
-$NetBSD: patch-setup.py,v 1.7 2020/09/29 21:42:59 leot Exp $
+$NetBSD: patch-setup.py,v 1.8 2021/03/08 21:28:27 leot Exp $
 
 Avoid too strict version requirements.
 
---- setup.py.orig	2020-09-28 01:43:12.000000000 +0000
+--- setup.py.orig	2021-02-28 19:50:45.000000000 +0000
 +++ setup.py
-@@ -25,16 +25,16 @@ with open('README.rst') as f:
+@@ -22,16 +22,16 @@ with open('README.rst') as f:
  # hangups from breaking when new versions of dependencies are released,
  # especially for end-users (non-developers) who use pip to install hangups.
  install_requires = [
 -    'ConfigArgParse>=0.11.0,<2',
--    'aiohttp>=3.3,<4',
+-    'aiohttp>=3.7,<4',
 -    'async-timeout>=2,<4',
 -    'appdirs>=1.4,<1.5',
 -    'readlike>=0.1.2,<0.2',
 -    'requests>=2.6.0,<3',  # uses semantic versioning (after 2.6)
 -    'ReParser==1.4.3',
--    'protobuf>=3.1.0,<3.14',
+-    'protobuf>=3.1.0,<3.16',
 -    'urwid>=1.3.1,<2.2',
 -    'MechanicalSoup>=0.6.0,<0.13',
 +    'ConfigArgParse>=0.11.0',
-+    'aiohttp>=3.3',
++    'aiohttp>=3.7',
 +    'async-timeout>=2',
 +    'appdirs>=1.4',
 +    'readlike>=0.1.2',
