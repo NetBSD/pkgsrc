ccdeb6b0b7d033bfa924ac001c20069477c66391
jaapb <jaapb@pkgsrc.org>
2021-03-08 08:12:45 +0000

    Recursive revbump associated with update of lang/ocaml

--- devel/ocaml-lwt_ssl/buildlink3.mk
+++ devel/ocaml-lwt_ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/04/03 14:36:44 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2021/03/08 08:12:54 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_ssl
 OCAML_LWT_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_ssl+=	ocaml-lwt_ssl>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.3
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.3nb1
 BUILDLINK_PKGSRCDIR.ocaml-lwt_ssl?=	../../devel/ocaml-lwt_ssl
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
