4925b195e4e0b6490aa12bfce5d64a9b95944bb1
jaapb <jaapb@pkgsrc.org>
2021-03-08 08:12:45 +0000

    Recursive revbump associated with update of lang/ocaml

--- security/ocaml-ssl/buildlink3.mk
+++ security/ocaml-ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2020/01/18 21:48:23 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2021/03/08 08:13:06 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ssl
 OCAML_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.8
-BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.9nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.9nb2
 BUILDLINK_PKGSRCDIR.ocaml-ssl?=		../../security/ocaml-ssl
 
 .include "../../lang/ocaml/buildlink3.mk"
