2fb4bb91cb53d8f73ffc338f4bd10e98dc433417
nia <nia@pkgsrc.org>
2021-03-08 15:25:47 +0000

    pkg_install-20210308
    - update pkg_add's examples in its man page for 2021 compliance.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.185 2020/12/18 17:10:54 maya Exp $	*/
+/*	$NetBSD: version.h,v 1.186 2021/03/08 15:25:47 nia Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20201218
+#define PKGTOOLS_VERSION 20210308
 
 #endif /* _INST_LIB_VERSION_H_ */
