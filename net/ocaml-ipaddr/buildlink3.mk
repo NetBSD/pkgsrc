f821d959c13a355fb13777adcc8d787b854d1219
jaapb <jaapb@pkgsrc.org>
2021-03-08 08:12:45 +0000

    Recursive revbump associated with update of lang/ocaml

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2020/04/08 12:31:01 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2021/03/08 08:13:05 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ipaddr
 OCAML_IPADDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=4.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=4.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=4.0.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
 BUILDLINK_FILES.ocaml-ipaddr+=		lib/ocaml/site-lib/*
 
--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2019/05/14 16:01:17 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2021/03/08 08:13:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-graphics
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-graphics?=	ocaml-graphics>=2.0.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
