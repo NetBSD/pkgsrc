e36976525c472a07cd8b43abdc9ee98eaae426c7
martin <martin@pkgsrc.org>
2021-03-09 16:49:12 +0000

    Provide enough rope to allow individual u-boot pkgs to request a different version. This allows for incremental updates and tests.

--- sysutils/u-boot/u-boot-version.mk
+++ sysutils/u-boot/u-boot-version.mk
@@ -1,3 +1,12 @@
-# $NetBSD: u-boot-version.mk,v 1.3 2018/12/14 00:52:26 jmcneill Exp $
+# $NetBSD: u-boot-version.mk,v 1.4 2021/03/09 16:49:12 martin Exp $
 
-UBOOT_VERSION?=	2018.11
+UBOOT_DEFAULT_VERSION=	2018.11
+
+UBOOT_VERSION?=	${UBOOT_DEFAULT_VERSION}
+
+.if ${UBOOT_VERSION} != ${UBOOT_DEFAULT_VERSION}
+DISTINFO_FILE=	${.CURDIR}/../../sysutils/u-boot/distinfo-${UBOOT_VERSION}
+PATCHDIR=	${.CURDIR}/../../sysutils/u-boot/patches-${UBOOT_VERSION}
+.else
+PATCHDIR?=	${.CURDIR}/../../sysutils/u-boot/patches
+.endif
