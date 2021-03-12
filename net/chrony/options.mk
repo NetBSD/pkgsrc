fdea10e73cd2dc5db1f690f46c8f27845f844868
nia <nia@pkgsrc.org>
2021-03-12 09:57:18 +0000

    chrony: Update to 4.0
    New in version 4.0
    ==================
    
    Enhancements
    ------------
    * Add support for Network Time Security (NTS) authentication
    * Add support for AES-CMAC keys (AES128, AES256) with Nettle
    * Add authselectmode directive to control selection of unauthenticated sources
    * Add binddevice, bindacqdevice, bindcmddevice directives
    * Add confdir directive to better support fragmented configuration
    * Add sourcedir directive and "reload sources" command to support dynamic
      NTP sources specified in files
    * Add clockprecision directive
    * Add dscp directive to set Differentiated Services Code Point (DSCP)
    * Add -L option to limit log messages by severity
    * Add -p option to print whole configuration with included files
    * Add -U option to allow start under non-root user
    * Allow maxsamples to be set to 1 for faster update with -q/-Q option
    * Avoid replacing NTP sources with sources that have unreachable address
    * Improve pools to repeat name resolution to get "maxsources" sources
    * Improve source selection with trusted sources
    * Improve NTP loop test to prevent synchronisation to itself
    * Repeat iburst when NTP source is switched from offline state to online
    * Update clock synchronisation status and leap status more frequently
    * Update seccomp filter
    * Add "add pool" command
    * Add "reset sources" command to drop all measurements
    * Add authdata command to print details about NTP authentication
    * Add selectdata command to print details about source selection
    * Add -N option and sourcename command to print original names of sources
    * Add -a option to some commands to print also unresolved sources
    * Add -k, -p, -r options to clients command to select, limit, reset data
    
    Bug fixes
    ---------
    * Don't set interface for NTP responses to allow asymmetric routing
    * Handle RTCs that don't support interrupts
    * Respond to command requests with correct address on multihomed hosts
    
    Removed features
    ----------------
    * Drop support for RIPEMD keys (RMD128, RMD160, RMD256, RMD320)
    * Drop support for long (non-standard) MACs in NTPv4 packets (chrony 2.x
      clients using non-MD5/SHA1 keys need to use option "version 3")
    * Drop support for line editing with GNU Readline

--- net/chrony/options.mk
+++ net/chrony/options.mk
@@ -1,14 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2019/03/10 19:16:46 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2021/03/12 09:57:18 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.chrony
-PKG_SUPPORTED_OPTIONS=	nettle
-PKG_SUGGESTED_OPTIONS=	nettle
+PKG_OPTIONS_VAR=		PKG_OPTIONS.chrony
+PKG_SUPPORTED_OPTIONS=		gnutls
+PKG_SUGGESTED_OPTIONS=		gnutls
+PKG_OPTIONS_LEGACY_OPTS+=	nettle:gnutls
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mnettle)
+.if !empty(PKG_OPTIONS:Mgnutls)
 USE_TOOLS+=	pkg-config
+.include "../../security/gnutls/buildlink3.mk"
 .include "../../security/nettle/buildlink3.mk"
 .else
+CONFIGURE_ARGS+=	--without-gnutls
 CONFIGURE_ARGS+=	--without-nettle
 .endif
