45aa74e807c6502ee88ff4d2f4458017777aa578
leot <leot@pkgsrc.org>
2021-03-14 14:10:18 +0000

    amass: Update to 3.11.9
    Changes:
    3.11.9
    ------
     - Updated the snapcraft build process
     - Updated installation documentation to resolve #517
     - Removed the 386 platform support
     - Enumeration storage has better recovery for errors
     - Adding support for the Apple Silicon and fixed #590
     - Added some Close and Stop calls to address #508

--- security/amass/go-modules.mk
+++ security/amass/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.2 2021/03/04 09:59:11 leot Exp $
+# $NetBSD: go-modules.mk,v 1.3 2021/03/14 14:10:18 leot Exp $
 
 GO_MODULE_FILES+=	cloud.google.com/go/@v/v0.26.0.mod
 GO_MODULE_FILES+=	cloud.google.com/go/@v/v0.34.0.mod
