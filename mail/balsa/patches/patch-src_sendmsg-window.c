bbc3e867296bbcff4640a20c7dd79d841b2a0add
nia <nia@pkgsrc.org>
2021-03-14 07:43:47 +0000

    balsa: Update to 2.6.2
    * Balsa-2.6.2 release. Release date 2020-12-21
    
    - Webkit widgets are now sandboxed.
    - Improved desktop notifications.
    - iTIP calendar reply are improved.
    - Handling of multiple crypto keys for the same recipient.
    - build cleanups and improvements, e.g. application logging.
    - code cleanups and simplifications as enabled by moving to more modern
      libraries.
    - bug fixes: draft with message/external-body attachments;
      do not crash on importing half-broken keys
    - improved translations.

--- mail/balsa/patches/patch-src_sendmsg-window.c
+++ mail/balsa/patches/patch-src_sendmsg-window.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_sendmsg-window.c,v 1.1 2019/09/14 18:57:10 nia Exp $
+$NetBSD: patch-src_sendmsg-window.c,v 1.2 2021/03/14 07:43:48 nia Exp $
 
 Support enchant2.
 
---- src/sendmsg-window.c.orig	2019-05-25 15:01:14.000000000 +0000
+--- src/sendmsg-window.c.orig	2020-12-18 20:27:46.000000000 +0000
 +++ src/sendmsg-window.c
-@@ -68,7 +68,7 @@
- #include "geometry-manager.h"
- 
- #if !HAVE_GSPELL && !HAVE_GTKSPELL_3_0_3
+@@ -72,7 +72,7 @@
+ #elif HAVE_GSPELL
+ #include <gspell/gspell.h>
+ #else                           /* HAVE_GTKSPELL */
 -#include <enchant/enchant.h>
 +#include <enchant.h>
- #endif                          /* HAVE_GTKSPELL_3_0_3 */
- #if HAVE_GTKSPELL
- #include "gtkspell/gtkspell.h"
+ #include "spell-check.h"
+ #endif                          /* HAVE_GTKSPELL */
+ #if HAVE_GTKSOURCEVIEW
