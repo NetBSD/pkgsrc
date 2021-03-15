76bd64c0ae0a1952daede99dbb3d0ad87706e678
nia <nia@pkgsrc.org>
2021-03-15 11:04:55 +0000

    lightspark: Update to 0.8.4.1
    Version 0.8.4.1:
    
    	* Hotfix for crash on Windows, no changes for Linux
    
    Version 0.8.4:
    
    	* improved handling of mixed AVM1/AVM2 files
    	* implement streaming of videos embedded in the swf file
    	* implement class inheritance for AVM1
    	* added settings dialog, currently only used to enable/disable local storage
    	* support for local storage added
    	* use SDL threading methods, remove glibmm dependency

--- multimedia/lightspark/patches/patch-src_platforms_engineutils.cpp
+++ multimedia/lightspark/patches/patch-src_platforms_engineutils.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_platforms_engineutils.cpp,v 1.2 2020/12/03 22:23:33 nia Exp $
+$NetBSD: patch-src_platforms_engineutils.cpp,v 1.3 2021/03/15 11:04:56 nia Exp $
 
 These are undefined, take advantage of SDL defines instead.
 
---- src/platforms/engineutils.cpp.orig	2020-07-05 10:31:09.000000000 +0000
+--- src/platforms/engineutils.cpp.orig	2021-02-10 15:43:30.000000000 +0000
 +++ src/platforms/engineutils.cpp
-@@ -33,7 +33,7 @@
- #include "scripting/flash/events/flashevents.h"
+@@ -37,7 +37,7 @@
+ #include <sys/stat.h>
  
  //The interpretation of texture data change with the endianness
 -#if __BYTE_ORDER == __BIG_ENDIAN
