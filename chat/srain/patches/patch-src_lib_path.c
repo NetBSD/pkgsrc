56fabfc5777e8a87054f1901715c35700396d0dd
nia <nia@pkgsrc.org>
2021-03-17 15:32:24 +0000

    srain: update to 1.2.0
    2021-02-28 Version 1.2.0
    ========================
    
    - Features:
    
      - Switch build system from Make to Meson
      - Add FreeBSD implementations for ``srn_get_executable_{path,dir}``,
        thanks to :people:`wahjava`
      - Add ``/quote`` command for sending special IRC commands,
        thanks to :people:`hno`
      - Add support for hiding server buffer
    
    
    - Bug fixes:
    
       - Fix an use-after-free BUG
       - Fix implicit declaration error on some systems,
         thanks to :people:`lgbaldoni`

--- chat/srain/patches/patch-src_lib_path.c
+++ chat/srain/patches/patch-src_lib_path.c
@@ -1,20 +1,44 @@
-$NetBSD: patch-src_lib_path.c,v 1.1 2020/11/24 14:17:53 nia Exp $
+$NetBSD: patch-src_lib_path.c,v 1.2 2021/03/17 15:32:24 nia Exp $
 
-Use the Linux code for finding the executable filename using
-/proc on other OSes too.
+Use the Linux procfs code path for finding the executable
+name as a portable fallback.
 
---- src/lib/path.c.orig	2020-10-04 01:11:49.000000000 +0000
+The code for other platforms is wrong.
+
+--- src/lib/path.c.orig	2021-02-28 09:19:09.000000000 +0000
 +++ src/lib/path.c
-@@ -48,7 +48,7 @@
+@@ -50,21 +50,20 @@
  #include <mach-o/dyld.h>
  #include <unistd.h>
  
 -#elif defined __linux__
++#elif defined __FreeBSD__
++#include <sys/types.h>
++#include <sys/sysctl.h>
++
 +#else
  #include <limits.h>
  #include <libgen.h>
  #include <unistd.h>
-@@ -364,20 +364,6 @@ char *srn_get_executable_dir() {
+ 
+-#define PROC_SELF_EXE "/proc/self/exe"
+-
+-#elif defined __sun
+-
++#if defined __sun
+ #define PROC_SELF_EXE "/proc/self/path/a.out"
+-
+-#elif defined __FreeBSD__
+-#include <sys/types.h>
+-#include <sys/sysctl.h>
+-
++#else
++#define PROC_SELF_EXE "/proc/self/exe"
++#endif
+ #endif
+ 
+ #define DEFAULT_FILE_MODE   (S_IRUSR | S_IWUSR)
+@@ -371,20 +370,6 @@ char *srn_get_executable_dir() {
      return executableDir;
  }
  
@@ -35,7 +59,16 @@ Use the Linux code for finding the executable filename using
  #elif defined __APPLE__
  char *srn_get_executable_path() {
      char rawPathName[PATH_MAX];
-@@ -396,4 +382,18 @@ char *srn_get_executable_dir() {
+@@ -404,7 +389,7 @@ char *srn_get_executable_dir() {
+     return executableDir;
+ }
+ 
+-#elif defined __FreeBSD__
++
+ char *srn_get_executable_path() {
+     int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
+     char rawPath[PATH_MAX];
+@@ -424,4 +409,18 @@ char *srn_get_executable_dir() {
      g_free(executablePath);
      return executableDir;
  }
