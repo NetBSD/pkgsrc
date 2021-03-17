7135cc4c85606fa8aff2b805f903cfb7f6721fdb
markd <markd@pkgsrc.org>
2021-03-17 22:41:06 +0000

    purpose: not in wip

--- devel/purpose/buildlink3.mk
+++ devel/purpose/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/03/17 22:33:37 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/03/17 22:41:06 markd Exp $
 
 BUILDLINK_TREE+=	purpose
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	purpose
 PURPOSE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.purpose+=	purpose>=5.70.0
-BUILDLINK_PKGSRCDIR.purpose?=	../../wip/purpose
+BUILDLINK_PKGSRCDIR.purpose?=	../../devel/purpose
 
 .include "../../devel/kio/buildlink3.mk"
 .include "../../x11/kirigami2/buildlink3.mk"
