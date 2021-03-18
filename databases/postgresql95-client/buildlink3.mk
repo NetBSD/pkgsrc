61a21a1491aea14b1fc02c3eb9d8ada52a1a474a
adam <adam@pkgsrc.org>
2021-03-18 15:04:53 +0000

    postgresql: fix buildling contrib/uuid-ossp; fixes for LLVM enabled; should fix #55803

--- databases/postgresql95-client/buildlink3.mk
+++ databases/postgresql95-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/12/04 04:55:45 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/03/18 15:04:56 adam Exp $
 
 BUILDLINK_TREE+=	postgresql95-client
 
@@ -12,6 +12,11 @@ BUILDLINK_PKGSRCDIR.postgresql95-client?=	../../databases/postgresql95-client
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql95-client=	-lpq ${BUILDLINK_LDADD.gettext}
 BUILDLINK_FILES.postgresql95-client+=	bin/pg_config
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "SunOS"
+.include "../../devel/ossp-uuid/buildlink3.mk"
+.endif
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
