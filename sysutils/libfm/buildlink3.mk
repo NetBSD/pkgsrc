4d3852864fc7086817faa005f81029a0f285dcc6
gutteridge <gutteridge@pkgsrc.org>
2021-03-18 00:12:23 +0000

    Adjust libfm bl3 to include libexif, and rev bump dependencies
    Packages that depend on libfm and use libtool need to link against
    libexif, so reflect that in libfm's buildlink3.mk. (This will be more
    relevant when lxhotkey is added. lxpanel does not directly depend on
    libexif, so that's corrected here, too.)

--- sysutils/libfm/buildlink3.mk
+++ sysutils/libfm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/08/17 20:17:44 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/03/18 00:12:23 gutteridge Exp $
 
 BUILDLINK_TREE+=	libfm
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	libfm
 LIBFM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libfm+=	libfm>=1.2.3
-BUILDLINK_ABI_DEPENDS.libfm?=	libfm>=1.3.1nb5
+BUILDLINK_ABI_DEPENDS.libfm?=	libfm>=1.3.2nb1
 BUILDLINK_PKGSRCDIR.libfm?=	../../sysutils/libfm
 
 .include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
 .include "../../sysutils/menu-cache/buildlink3.mk"
 .endif	# LIBFM_BUILDLINK3_MK
 
