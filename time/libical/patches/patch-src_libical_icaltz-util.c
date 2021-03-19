eea76978b588c35155c40e17df9c0225c441a540
nia <nia@pkgsrc.org>
2021-03-19 10:45:31 +0000

    libical: Update to 3.0.9
    Version 3.0.9 is a patch release.
    This release is binary and source compatible with version 3.0.0.
    Note that the libical-glib library is considered unstable; therefore not binary compatible with previous releases
    
    Release Notes:
    
    - Add support for empty parameters, e.g. CN=""
    - Accept VTIMEZONE with more than one X- property
    - Several fixes for recurrences containing BYWEEKNO
    - icalrecurrencetype_from_string() will reject any RRULE that contains a rule-part that occurs more than once
    - Improve thread safety
    - Fix compiled-in path for the built-in timezone data
    - Fix reading TZif files with empty v1 data (use v2+ whenever possible)
    - Add backwards compatibility for previous TZIDs
    - Built-in timezones updated to tzdata2020d
    - Fix build with newer libicu
    - Fix cross-compile support in libical-glib

--- time/libical/patches/patch-src_libical_icaltz-util.c
+++ time/libical/patches/patch-src_libical_icaltz-util.c
@@ -1,17 +1,29 @@
-$NetBSD: patch-src_libical_icaltz-util.c,v 1.4 2020/05/19 15:33:10 nia Exp $
+$NetBSD: patch-src_libical_icaltz-util.c,v 1.5 2021/03/19 10:45:31 nia Exp $
 
 Define bswap_32 on NetBSD
 
---- src/libical/icaltz-util.c.orig	2020-03-07 14:42:42.000000000 +0000
+--- src/libical/icaltz-util.c.orig	2021-01-16 14:19:41.000000000 +0000
 +++ src/libical/icaltz-util.c
-@@ -49,6 +49,10 @@
+@@ -42,6 +42,7 @@
+ #include <sys/endian.h>
+ #if defined(bswap32)
+ #define bswap_32 bswap32
++#define bswap_64 bswap64
+ #else
+ #define bswap_32 swap32
+ #endif
+@@ -49,8 +50,14 @@
  #endif
  #endif
  
 +#if defined(__NetBSD__) && !defined(bswap_32)
 +#define bswap_32 bswap32
++#define bswap_64 bswap64
 +#endif
 +
  #if defined(__OpenBSD__) && !defined(bswap_32)
  #define bswap_32 swap32
++#define bswap_64 swap64
  #endif
+ 
+ #if defined(_MSC_VER)
