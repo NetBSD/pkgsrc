b0568d989803fdc879029afb3e8f76e594f646cc
sborrill <sborrill@pkgsrc.org>
2021-03-19 08:50:57 +0000

    nmap: Make liblinear an option, on by default.
    liblinear is used for 'machine learning IPv6 OS detection'. It depends on
    a number of packages, some of which may not build on some OS versions.

--- net/nmap/options.mk
+++ net/nmap/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.21 2020/03/13 08:55:37 tnn Exp $
+# $NetBSD: options.mk,v 1.22 2021/03/19 08:50:57 sborrill Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nmap
 
-PKG_SUPPORTED_OPTIONS=	lua
+PKG_SUPPORTED_OPTIONS=	lua liblinear
+
+PKG_SUGGESTED_OPTIONS=	liblinear
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,4 +17,12 @@ CONFIGURE_ARGS+=	--with-liblua=${BUILDLINK_PREFIX.lua}
 PLIST.lua=		yes
 .else
 CONFIGURE_ARGS+=	--without-liblua
+.endif
+
+.if !empty(PKG_OPTIONS:Mliblinear)
+# liblinear is used for IPv6 OS detection machine learning techniques
+.include "../../math/liblinear/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-liblinear=${BUILDLINK_PREFIX.liblinear}
+.else
+CONFIGURE_ARGS+=	--without-liblinear
 .endif
