3c43c79e80d1eff88c7ac4682bd52115bbaf2272
markd <markd@pkgsrc.org>
2021-03-21 19:32:45 +0000

    libkolabxml: cleanup installed cmake files

--- textproc/libkolabxml/buildlink3.mk
+++ textproc/libkolabxml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/03/19 22:17:02 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/03/21 19:32:45 markd Exp $
 
 BUILDLINK_TREE+=	libkolabxml
 
@@ -8,6 +8,9 @@ LIBKOLABXML_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libkolabxml+=	libkolabxml>=1.1.6
 BUILDLINK_PKGSRCDIR.libkolabxml?=	../../textproc/libkolabxml
 
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../textproc/xerces-c/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
 .endif	# LIBKOLABXML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkolabxml
