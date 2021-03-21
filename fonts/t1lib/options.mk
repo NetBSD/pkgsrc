cf014ffa524793a99bf9edd63f5e47a5275e797c
rillig <rillig@pkgsrc.org>
2021-03-21 00:01:46 +0000

    fonts/t1lib: allow to be built using neXtaw
    Suggested by Simon Adda-Reyss via private mail.
    
    No change to the default configuration.

--- fonts/t1lib/options.mk
+++ fonts/t1lib/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2019/11/02 22:24:43 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2021/03/21 00:01:46 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.t1lib
 PKG_SUPPORTED_OPTIONS+=	x11
@@ -8,7 +8,7 @@ PKG_SUGGESTED_OPTIONS+=	x11
 
 .if !empty(PKG_OPTIONS:Mx11)
 .include "../../x11/libX11/buildlink3.mk"
-.include "../../x11/libXaw/buildlink3.mk"
+.include "../../mk/xaw.buildlink3.mk"
 .include "../../x11/libXpm/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
 .endif
