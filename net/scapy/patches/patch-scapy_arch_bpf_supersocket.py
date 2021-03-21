b84acbf842686ff860e44e461bf370c841155225
gutteridge <gutteridge@pkgsrc.org>
2021-03-21 19:50:54 +0000

    scapy: update to 2.4.4
    Main Changes
    Core
    
        784 commits to master since this v2.4.3
        fix how timestamps are measured on layer 3 (broken since 2.4.1)
        drop DNET support (deprecated since 2.4.0)
        Scapy will now use libpcap instead of tcpdump to compile the filters (tcpdump is still used to filter pcaps)
        major changes to the online doc: add an automatically generated API reference with visual representation of each layer, move to Sphinx 3.0.0, new layer-specific doc...
        *BSD fixes
        more doc to many functions
    
    Layers
    
        New layers (homeplug sg, smb2, NSH, RPL, ERSPAN, BFD, ROCE...)
        TLS: TLS 1.3 support & many fixes
        802.11 improvements / WPA3 detection
        Netflow 9 fixes
        Major refactor & changes of the Automotive layers
        HTTP changes
        DHCPv6 fixes according to revision of the RFC
        Bluetooth improvements (+BLE)
        minor fixes to Radius, PPP, GTP, IPsec, CDP, VTP, HTTP/2, ...

--- net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
+++ net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.1 2019/11/04 05:48:26 gutteridge Exp $
+$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.2 2021/03/21 19:50:54 gutteridge Exp $
 
 Add DragonFly support.
+Fix NetBSD 32-bit architecture alignment.
 
---- scapy/arch/bpf/supersocket.py.orig	2019-08-04 16:12:30.000000000 +0000
+--- scapy/arch/bpf/supersocket.py.orig	2020-09-02 11:26:21.000000000 +0000
 +++ scapy/arch/bpf/supersocket.py
-@@ -16,14 +16,14 @@ from scapy.arch.bpf.consts import BIOCGB
+@@ -18,18 +18,16 @@ from scapy.arch.bpf.consts import BIOCGB
      BIOCIMMEDIATE, BIOCPROMISC, BIOCSBLEN, BIOCSETIF, BIOCSHDRCMPLT, \
      BPF_BUFFER_LENGTH, BIOCSDLT, DLT_IEEE802_11_RADIO
  from scapy.config import conf
@@ -16,17 +17,21 @@ Add DragonFly support.
  from scapy.compat import raw
  
  
--if FREEBSD or NETBSD:
+-if FREEBSD:
 +if FREEBSD or NETBSD or DRAGONFLY:
-     BPF_ALIGNMENT = 8  # sizeof(long)
+     # On 32bit architectures long might be 32bit.
+     BPF_ALIGNMENT = sizeof(c_long)
+-elif NETBSD:
+-    BPF_ALIGNMENT = 8  # sizeof(long)
  else:
      BPF_ALIGNMENT = 4  # sizeof(int32_t)
-@@ -260,7 +260,7 @@ class L2bpfListenSocket(_L2bpfSocket):
-             return
  
-         # Extract useful information from the BPF header
--        if FREEBSD or NETBSD:
-+        if FREEBSD or NETBSD or DRAGONFLY:
-             # struct bpf_xhdr or struct bpf_hdr32
+@@ -281,7 +279,7 @@ class L2bpfListenSocket(_L2bpfSocket):
+             else:
+                 # struct bpf_hdr (64bit time_t) or struct bpf_xhdr
+                 bh_tstamp_offset = 16
+-        elif NETBSD:
++        elif NETBSD or DRAGONFLY:
+             # struct bpf_hdr or struct bpf_hdr32
              bh_tstamp_offset = 16
          else:
--- net/scapy/patches/patch-scapy_consts.py
+++ net/scapy/patches/patch-scapy_consts.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-scapy_consts.py,v 1.1 2019/11/04 05:48:26 gutteridge Exp $
+$NetBSD: patch-scapy_consts.py,v 1.2 2021/03/21 19:50:54 gutteridge Exp $
 
 Add DragonFly support.
 
---- scapy/consts.py.orig	2019-10-22 16:33:00.860254151 +0000
+--- scapy/consts.py.orig	2020-09-02 11:26:21.000000000 +0000
 +++ scapy/consts.py
-@@ -15,7 +15,8 @@ DARWIN = platform.startswith("darwin")
+@@ -14,7 +14,8 @@ DARWIN = platform.startswith("darwin")
  SOLARIS = platform.startswith("sunos")
  WINDOWS = platform.startswith("win32")
  WINDOWS_XP = platform_lib.release() == "XP"
@@ -13,4 +13,4 @@ Add DragonFly support.
 +BSD = DARWIN or FREEBSD or OPENBSD or NETBSD or DRAGONFLY
  # See https://docs.python.org/3/library/platform.html#cross-platform
  IS_64BITS = maxsize > 2**32
- 
+ # LOOPBACK_NAME moved to conf.loopback_name
