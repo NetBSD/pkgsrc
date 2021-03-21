43c40098a009b7da0dc091e0daf7fa4599405659
taca <taca@pkgsrc.org>
2021-03-21 04:16:17 +0000

    net/bind916: update to 9.16.13
    9.16.13 (2022-03-17)
    
    New Features
    
    * A new purge-keys option has been added to dnssec-policy. It sets the
      period of time that key files are retained after becoming obsolete
      due to a key rollover; the default is 90 days. This feature can be
      disabled by setting purge-keys to 0. [GL #2408]
    
    Feature Changes
    
    * When serve-stale is enabled and stale data is available, named now
      returns stale answers upon encountering any unexpected error in the
      query resolution process. This may happen, for example, if the
      fetches-per-server or fetches-per-zone limits are reached. In this
      case, named attempts to answer DNS requests with stale data, but
      does not start the stale-refresh-time window. [GL #2434]
    
    Bug Fixes
    
    * Zone journal (.jnl) files created by versions of named prior to
      9.16.12 were no longer compatible; this could cause problems when
      upgrading if journal files were not synchronized first. This has
      been corrected: older journal files can now be read when starting
      up. When an old-style journal file is detected, it is updated to the
      new format immediately after loading.
    
      Note that journals created by the current version of named are not
      usable by versions prior to 9.16.12. Before downgrading to a prior
      release, users are advised to ensure that all dynamic zones have
      been synchronized using rndc sync -clean.
    
      A journal file's format can be changed manually by running
      named-journalprint -d (downgrade) or named-journalprint -u
      (upgrade). Note that this must not be done while named is
      running. [GL #2505]
    
    * named crashed when it was allowed to serve stale answers and
      stale-answer-client-timeout was triggered without any (stale) data
      available in the cache to answer the query. [GL #2503]
    
    * If an outgoing packet exceeded max-udp-size, named dropped it
      instead of sending back a proper response. To prevent this problem,
      the IP_DONTFRAG option is no longer set on UDP sockets, which has
      been happening since BIND 9.16.11. [GL #2466]
    
    * NSEC3 records were not immediately created when signing a dynamic
      zone using dnssec-policy with nsec3param. This has been fixed. [GL
      #2498]
    
    * A memory leak occurred when named was reconfigured after adding an
      inline-signed zone with auto-dnssec maintain enabled. This has been
      fixed. [GL #2041]
    
    * An invalid direction field (not one of N, S, E, W) in a LOC record
      resulted in an INSIST failure when a zone file containing such a
      record was loaded. [GL #2499]

--- net/bind916/patches/patch-lib_isc_include_isc_netmgr.h
+++ net/bind916/patches/patch-lib_isc_include_isc_netmgr.h
@@ -1,13 +1,16 @@
-$NetBSD: patch-lib_isc_include_isc_netmgr.h,v 1.2 2021/01/29 16:17:15 otis Exp $
+$NetBSD: patch-lib_isc_include_isc_netmgr.h,v 1.3 2021/03/21 04:16:17 taca Exp $
 
 Declare missing prototype.
 
---- lib/isc/include/isc/netmgr.h.orig	2021-01-11 13:23:31.000000000 +0000
+--- lib/isc/include/isc/netmgr.h.orig	2021-03-11 13:20:59.000000000 +0000
 +++ lib/isc/include/isc/netmgr.h
-@@ -510,3 +510,6 @@ isc_nm_tlsdnsconnect(isc_nm_t *mgr, isc_
- isc_result_t
- isc_nm_tls_create_server_ctx(const char *keyfile, const char *certfile,
- 			     isc_ssl_ctx_t **ctxp);
-+
+@@ -146,6 +146,9 @@ isc_nmhandle_getdata(isc_nmhandle_t *han
+ void *
+ isc_nmhandle_getextra(isc_nmhandle_t *handle);
+ 
 +int
 +isc_nmhandle_getfd(isc_nmhandle_t *handle);
++
+ bool
+ isc_nmhandle_is_stream(isc_nmhandle_t *handle);
+ 
