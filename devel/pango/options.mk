4f52ef46e9a62af069e254f3533e1ecce642e58f
adam <adam@pkgsrc.org>
2021-03-22 07:03:57 +0000

    pango: remove unused options; pkglint fixes

--- devel/pango/options.mk
+++ devel/pango/options.mk
@@ -1,19 +1,19 @@
-# $NetBSD: options.mk,v 1.19 2021/03/22 06:57:53 adam Exp $
+# $NetBSD: options.mk,v 1.20 2021/03/22 07:03:57 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pango
-PKG_SUPPORTED_OPTIONS=	libthai quartz x11
+PKG_SUPPORTED_OPTIONS=	libthai x11
 PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		coretext quartz x11
+PLIST_VARS+=	x11
 
 ###
 ### X11 support
 ###
 .if !empty(PKG_OPTIONS:Mx11)
 # fix for pangox.pc
-PLIST.x11=		yes
+PLIST.x11=	yes
 MESON_ARGS+=	-Dxft=enabled
 BUILDLINK_API_DEPENDS.Xft2+=	Xft2>=2.1.7nb3
 .include "../../x11/libXft/buildlink3.mk"
@@ -36,18 +36,3 @@ MESON_ARGS+=	-Dlibthai=disabled
 .endif
 
 .include "../../mk/bsd.prefs.mk"
-
-###
-### Quartz "support"
-###
-.if ${OPSYS} == "Darwin"
-### always looks for system CoreText
-.  if empty(MACHINE_PLATFORM:MDarwin-[1-8].*-*)
-### CoreText is only available in 10.5 or newer
-PLIST.coretext=		yes
-.  endif
-.  if !empty(PKG_OPTIONS:Mquartz)
-### installs its coretext header file if cairo was built with "quartz" option
-#PLIST.quartz=		yes
-.  endif
-.endif
