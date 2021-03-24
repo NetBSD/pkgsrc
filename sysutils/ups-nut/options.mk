2e37b9954f9af105a0481ced33a53e02f66d4f03
wiz <wiz@pkgsrc.org>
2021-03-24 11:01:55 +0000

    ups-nut: fix man page references
    From Edgar Fuß in private email.
    
    Fix some pkglint while here (openssl option disabled because only openssl 1.0
    supported).
    
    Bump PKGREVISION.

--- sysutils/ups-nut/options.mk
+++ sysutils/ups-nut/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2021/02/11 10:05:13 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2021/03/24 11:01:55 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ups-nut
 PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
@@ -7,12 +7,12 @@ PKG_SUGGESTED_OPTIONS=		nss
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mopenssl)
-CONFIGURE_ARGS+=	--with-ssl=openssl --with-openssl --without-nss
-.  include "../../security/openssl/buildlink3.mk"
-.elif !empty(PKG_OPTIONS:Mnss)
+.if !empty(PKG_OPTIONS:Mnss)
 CONFIGURE_ARGS+=	--with-ssl=nss --with-nss --without-openssl
 .  include "../../devel/nss/buildlink3.mk"
+#.elif !empty(PKG_OPTIONS:Mopenssl)
+#CONFIGURE_ARGS+=	--with-ssl=openssl --with-openssl --without-nss
+#.  include "../../security/openssl/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-ssl
 .endif
