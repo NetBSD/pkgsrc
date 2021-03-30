42b5c63817fd1f744b3d66545c91ffa8a011b8e6
cirnatdan <cirnatdan@pkgsrc.org>
2021-03-30 14:45:36 +0000

    compiz: Remove broken PKG_OPTION "gnome"
    "gnome" option depends on wm/metacity, which is gone.

--- wm/compiz/options.mk
+++ wm/compiz/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2018/08/03 21:17:47 minskim Exp $
+# $NetBSD: options.mk,v 1.6 2021/03/30 14:45:36 cirnatdan Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.compiz
-PKG_SUPPORTED_OPTIONS=	fuse gnome inotify kde4
-PLIST_VARS+=		gnome inotify kde
+PKG_SUPPORTED_OPTIONS=	fuse inotify kde4
+PLIST_VARS+=		inotify kde
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -18,21 +18,6 @@ PKG_SUGGESTED_OPTIONS+=	inotify
 CONFIGURE_ARGS+=	--enable-fuse
 .else
 CONFIGURE_ARGS+=	--disable-fuse
-.endif
-
-.if !empty(PKG_OPTIONS:Mgnome)
-CONFIGURE_ARGS+=	--enable-librsvg
-PLIST.gnome=		yes
-BUILDLINK_DEPMETHOD.gnome-desktop=	build
-.include "../../x11/gnome-desktop/buildlink3.mk"
-BUILDLINK_DEPMETHOD.gnome-control-center=	build
-.include "../../x11/gnome-control-center/buildlink3.mk"
-BUILDLINK_DEPMETHOD.metacity=	build
-.include "../../wm/metacity/buildlink3.mk"
-BUILDLINK_DEPMETHOD.librsvg=	build
-.include "../../graphics/librsvg/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-gnome --disable-gnome-keybindings
 .endif
 
 .if !empty(PKG_OPTIONS:Minotify)
