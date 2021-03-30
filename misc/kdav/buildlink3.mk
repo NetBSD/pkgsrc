327a2950be222b4440ee27d6e18e8f61f155094a
markd <markd@pkgsrc.org>
2021-03-30 10:58:21 +0000

    kf5: KDE Frameworks update to 5.80.0
    10 months of patches/updates.

--- misc/kdav/buildlink3.mk
+++ misc/kdav/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2021/02/07 06:30:08 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/03/30 10:58:26 markd Exp $
 
 BUILDLINK_TREE+=	kdav
 
 .if !defined(KDAV_BUILDLINK3_MK)
 KDAV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kdav+=	kdav>=17.12.1
-BUILDLINK_ABI_DEPENDS.kdav?=	kdav>=20.04.1nb6
+BUILDLINK_API_DEPENDS.kdav+=	kdav>=5.80.0
+BUILDLINK_ABI_DEPENDS.kdav?=	kdav>=5.80.0
 BUILDLINK_PKGSRCDIR.kdav?=	../../misc/kdav
 
 .include "../../devel/kio/buildlink3.mk"
---
+++ www/syndication/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/03/30 10:58:29 markd Exp $
+
+BUILDLINK_TREE+=	syndication
+
+.if !defined(SYNDICATION_BUILDLINK3_MK)
+SYNDICATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.syndication+=	syndication>=5.70.0
+BUILDLINK_PKGSRCDIR.syndication?=	../../www/syndication
+
+.include "../../textproc/kcodecs/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# SYNDICATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-syndication
