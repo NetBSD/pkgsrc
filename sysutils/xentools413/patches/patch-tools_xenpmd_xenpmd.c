f9efc41cd1a852ac4a5dbd048627eb743f8b0287
bouyer <bouyer@pkgsrc.org>
2021-03-30 06:59:03 +0000

    Update xentools413 and xentools413 to 4.13.3. Changes since 4.13.2: inlcude security fixes for all XSA known to date (up to XSA-369). Other minor bug fixes.

--- sysutils/xentools413/patches/patch-tools_xenpmd_xenpmd.c
+++ sysutils/xentools413/patches/patch-tools_xenpmd_xenpmd.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-tools_xenpmd_xenpmd.c,v 1.1 2020/05/26 11:12:11 bouyer Exp $
+$NetBSD: patch-tools_xenpmd_xenpmd.c,v 1.2 2021/03/30 06:59:03 bouyer Exp $
 
 Elide format-truncation errors with GCC >= 7.
 
---- tools/xenpmd/xenpmd.c.orig	2018-07-09 13:47:19.000000000 +0000
-+++ tools/xenpmd/xenpmd.c
-@@ -100,7 +100,7 @@ FILE *get_next_battery_file(DIR *battery
+--- tools/xenpmd/xenpmd.c.orig	2021-03-22 17:57:42.000000000 +0100
++++ tools/xenpmd/xenpmd.c	2021-03-29 22:09:44.630232427 +0200
+@@ -101,7 +101,7 @@
  {
      FILE *file = 0;
      struct dirent *dir_entries;
 -    char file_name[284];
 +    char file_name[FILENAME_MAX];
+     int ret;
      
      do 
-     {
