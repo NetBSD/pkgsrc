d7b8514d495ec88e83ae639e92a431db66b75b29
markd <markd@pkgsrc.org>
2021-03-30 10:58:21 +0000

    kf5: KDE Frameworks update to 5.80.0
    10 months of patches/updates.

--- devel/kpty/patches/patch-src_kpty.cpp
+++ devel/kpty/patches/patch-src_kpty.cpp
@@ -1,26 +1,26 @@
-$NetBSD: patch-src_kpty.cpp,v 1.1 2016/05/26 08:29:06 markd Exp $
+$NetBSD: patch-src_kpty.cpp,v 1.2 2021/03/30 10:58:23 markd Exp $
 
 NetBSD loginx() complains if ut_type not set before calling
 
---- src/kpty.cpp.orig	2016-04-03 21:01:19.000000000 +0000
+--- src/kpty.cpp.orig	2021-03-06 16:29:41.000000000 +0000
 +++ src/kpty.cpp
-@@ -538,6 +538,9 @@ void KPty::login(const char *user, const
+@@ -496,6 +496,9 @@ void KPty::login(const char *user, const
      l_struct.ut_time = time(0);
- # endif
+ #endif
  
-+# if HAVE_STRUCT_UTMP_UT_TYPE
++#if HAVE_STRUCT_UTMP_UT_TYPE
 +    l_struct.ut_type = USER_PROCESS;
-+# endif
- # if HAVE_LOGIN
- #  if HAVE_LOGINX
++#endif
+ #if HAVE_LOGIN
+ #if HAVE_LOGINX
      ::loginx(&l_struct);
-@@ -545,9 +548,6 @@ void KPty::login(const char *user, const
+@@ -503,9 +506,6 @@ void KPty::login(const char *user, const
      ::login(&l_struct);
- #  endif
- # else
--#  if HAVE_STRUCT_UTMP_UT_TYPE
+ #endif
+ #else
+-#if HAVE_STRUCT_UTMP_UT_TYPE
 -    l_struct.ut_type = USER_PROCESS;
--#  endif
- #  if HAVE_STRUCT_UTMP_UT_PID
+-#endif
+ #if HAVE_STRUCT_UTMP_UT_PID
      l_struct.ut_pid = getpid();
- #   if HAVE_STRUCT_UTMP_UT_SESSION
+ #if HAVE_STRUCT_UTMP_UT_SESSION
--- devel/kpty/patches/patch-src_kptydevice.cpp
+++ devel/kpty/patches/patch-src_kptydevice.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_kptydevice.cpp,v 1.1 2016/05/26 08:29:06 markd Exp $
+$NetBSD: patch-src_kptydevice.cpp,v 1.2 2021/03/30 10:58:23 markd Exp $
 
 NetBSD like FreeBSD
 
---- src/kptydevice.cpp.orig	2016-04-03 21:01:19.000000000 +0000
+--- src/kptydevice.cpp.orig	2021-03-06 16:29:41.000000000 +0000
 +++ src/kptydevice.cpp
-@@ -43,7 +43,7 @@
- # include <sys/time.h>
+@@ -29,7 +29,7 @@
+ #include <sys/time.h>
  #endif
  
 -#if defined(Q_OS_FREEBSD) || defined(Q_OS_MAC)
 +#if defined(Q_OS_FREEBSD) || defined(Q_OS_MAC) || defined(Q_OS_NETBSD)
  // "the other end's output queue size" - kinda braindead, huh?
- # define PTY_BYTES_AVAILABLE TIOCOUTQ
+ #define PTY_BYTES_AVAILABLE TIOCOUTQ
  #elif defined(TIOCINQ)
