fcbcde15f52c81e0ec245f4eb69d4972781e96d6
nia <nia@pkgsrc.org>
2021-04-02 16:30:09 +0000

    gcc6-aux: copy iconv workaround from gcc5-aux
    with this, i can build this package.

--- lang/gcc6-aux/buildlink3.mk
+++ lang/gcc6-aux/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/04/08 09:12:46 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/02 16:30:09 nia Exp $
 
 BUILDLINK_TREE+=	gcc6-aux
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.gcc6-aux?=	gcc6-aux>=20160822nb1
 BUILDLINK_PKGSRCDIR.gcc6-aux?=		../../lang/gcc6-aux
 BUILDLINK_DEPMETHOD.gcc6-aux?=		build
 
-.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
 .include "../../math/mpfr/buildlink3.mk"
 .include "../../math/mpcomplex/buildlink3.mk"
