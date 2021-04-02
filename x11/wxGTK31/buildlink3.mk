3bcb24ae1db018e925e3f2dcdfd1c04f3c9b222c
wiz <wiz@pkgsrc.org>
2021-04-02 10:06:27 +0000

    x11/wxGTK31: import wxGTK31-3.1.4
    wxWidgets is a set of libraries that allows C++ applications to compile and
    run on several different types of computer, with minimal source code changes.
    There is one library per supported GUI (such as Windows, GTK+, Motif, and Mac).
    As well as providing a common API (Application Programming Interface) for GUI
    functionality, it provides functionality for accessing some commonly-used
    operating system facilities, from copying and deleting files to socket and
    thread support. wxWidgets is a 'framework' in the sense that it provides a lot
    of built-in functionality, which the application can use or replace as required,
    thus saving a great deal of coding effort. Basic data structures such as
    strings, arrays, linked lists and hash tables are also supported.
    
    Such a toolkit is not unique - there are others to choose from - but wxWidgets
    is free, well-established, well-documented, and very broad in its coverage of
    GUI functionality. It has some extras that make it stand out from the crowd,
    such as the many convenience dialogs, built-in HTML display and printing,
    virtual filesystems, easy-to-use OLE automation controller class, Open GL
    support, and many other features that make it easier to write modern and
    user-friendly applications.
    
    This package contains the 3.1 branch of the library.

---
+++ x11/wxGTK31/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/04/02 10:06:27 wiz Exp $
+
+BUILDLINK_TREE+=	wxGTK31
+
+.if !defined(WXGTK31_BUILDLINK3_MK)
+WXGTK31_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.wxGTK31+=	wxGTK31>=3.1
+BUILDLINK_ABI_DEPENDS.wxGTK31+=	wxGTK31>=3.1
+BUILDLINK_PKGSRCDIR.wxGTK31?=	../../x11/wxGTK31
+
+BUILDLINK_INCDIRS.wxGTK31+=	include/wx-3.1
+PREPEND_PATH+=			${PREFIX}/libexec/wx-3.1
+CONFIGURE_ARGS+=		--with-wx-version=3.1
+CONFIGURE_ARGS+=		--with-wx-config=${PREFIX}/libexec/wx-3.1/wx-config
+
+pkgbase := wxGTK31
+.include "../../mk/pkg-build-options.mk"
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.if ${OPSYS} != "Darwin"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif
+.endif # WXGTK31_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-wxGTK31
