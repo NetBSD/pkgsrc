2c41cd4b81211e775bb6f9e3c9310f41cd0bf463
wiz <wiz@pkgsrc.org>
2021-04-05 10:54:15 +0000

    fluidsynth: update to 2.2.0.
    This release breaks ABI compatibility! Refer to the API docs for details.
    
    A C++98 compliant compiler is now required to build fluidsynth (mailing list thread)
    
    New features
    
        Support loading SoundFonts >2GiB on Windows (#629)
        Major overhaul of the sequencer and its event queue (#604)
            Overlapping notes can be handled (#637)
            Performance improvement, since the event queue no longer blocks the rendering thread
            Time scale is not limited to 1000 anymore and can therefore be used for tempo changes
        The following audio drivers have gained multi-channel support
            DSound (#667, thanks to @jjceresa)
            WaveOut (#667, thanks to @jjceresa)
        The WinMIDI driver supports multiple devices (#677, thanks to @jjceresa)
        Handle GS DT1 SysEx messages for setting whether a channel is used for rhythm part (#708, thanks to @chirs241097)
        Support use of UTF-8 filenames under Windows (#718, thanks to @getraid-gg)
        Improved support for overriding tempo of the MIDI player (#711, #713, thanks to @jjceresa)
        Handle settings-related commands in user command file before initializing other objects (#739)
        SoundFont loading has been parallelized (#746, #812, requires openMP)
        The Oboe driver has gained a lower latency and other updates (#740, #741, #747)
        WASAPI driver has been added (#754, thanks to @chirs241097)
    
    General
    
        Fix race condition in fluid_player_callback (#783, thanks to @arcln)
        Improvements to LADSPA subsystem (#795, thanks to @mawe42)

--- audio/fluidsynth/patches/patch-include_fluidsynth_log.h
+++ audio/fluidsynth/patches/patch-include_fluidsynth_log.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-include_fluidsynth_log.h,v 1.1 2019/12/19 23:59:56 joerg Exp $
+$NetBSD: patch-include_fluidsynth_log.h,v 1.2 2021/04/05 10:54:16 wiz Exp $
 
 Use the protected form to avoid conflicts with printf macros.
 
---- include/fluidsynth/log.h.orig	2019-12-19 23:29:25.082934836 +0000
+--- include/fluidsynth/log.h.orig	2021-03-27 17:35:00.000000000 +0000
 +++ include/fluidsynth/log.h
-@@ -79,7 +79,7 @@ FLUIDSYNTH_API void fluid_default_log_fu
+@@ -85,7 +85,7 @@ FLUIDSYNTH_API void fluid_default_log_fu
  
  FLUIDSYNTH_API int fluid_log(int level, const char *fmt, ...)
  #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)
@@ -12,4 +12,4 @@ Use the protected form to avoid conflicts with printf macros.
 +__attribute__ ((__format__ (__printf__, 2, 3)))
  #endif
  ;
- 
+ /* @} */
