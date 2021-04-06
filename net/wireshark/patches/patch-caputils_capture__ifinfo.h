3f40b386f34a814c7298e70ee9d690261a30ab62
tnn <tnn@pkgsrc.org>
2021-04-06 10:13:32 +0000

    wireshark: replace incomplete fix for Glib2 fallout with upstream patch

--- net/wireshark/patches/patch-caputils_capture__ifinfo.h
+++ net/wireshark/patches/patch-caputils_capture__ifinfo.h
@@ -1,8 +1,11 @@
-$NetBSD: patch-caputils_capture__ifinfo.h,v 1.1 2021/03/23 10:48:42 adam Exp $
+$NetBSD: patch-caputils_capture__ifinfo.h,v 1.2 2021/04/06 10:13:32 tnn Exp $
 
-Fix building with Glib 2.68.
+From c8246c99737c7a844f45eb0e777382cc68397d17 Mon Sep 17 00:00:00 2001
+From: Michal Ruprich <michalruprich@gmail.com>
+Date: Wed, 10 Feb 2021 15:32:18 +0100
+Subject: [PATCH] Moving glib.h out of extern C
 
---- caputils/capture_ifinfo.h.orig	2021-03-23 09:25:20.000000000 +0000
+--- caputils/capture_ifinfo.h.orig	2021-03-10 18:21:20.000000000 +0000
 +++ caputils/capture_ifinfo.h
 @@ -11,12 +11,12 @@
  #ifndef __CAPTURE_IFINFO_H__
