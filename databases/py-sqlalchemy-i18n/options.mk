e80fa4499ccc780267b2f4ec210321f4331779b7
adam <adam@pkgsrc.org>
2021-04-06 14:23:24 +0000

    use versioned py-pygemnts

--- databases/py-sqlalchemy-i18n/options.mk
+++ databases/py-sqlalchemy-i18n/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2020/05/18 10:45:44 adam Exp $
+# $NetBSD: options.mk,v 1.3 2021/04/06 14:23:25 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-sqlalchemy-i18n
 PKG_SUPPORTED_OPTIONS=	tests
@@ -7,11 +7,10 @@ PKG_SUGGESTED_OPTIONS+=	# blank
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mtests)
-PYTHON_VERSIONED_DEPENDENCIES=	test
+PYTHON_VERSIONED_DEPENDENCIES=	pygments test
 .include "../../lang/python/versioned_dependencies.mk"
-DEPENDS+=	${PYPKGPREFIX}-pygments>=1.2:../../textproc/py-pygments
-DEPENDS+=	${PYPKGPREFIX}-jinja2>=2.3:../../textproc/py-jinja2
 DEPENDS+=	${PYPKGPREFIX}-docutils>=0.10:../../textproc/py-docutils
 DEPENDS+=	${PYPKGPREFIX}-flexmock>=0.9.7:../../devel/py-flexmock
+DEPENDS+=	${PYPKGPREFIX}-jinja2>=2.3:../../textproc/py-jinja2
 DEPENDS+=	${PYPKGPREFIX}-psycopg2>=2.4.6:../../databases/py-psycopg2
 .endif
