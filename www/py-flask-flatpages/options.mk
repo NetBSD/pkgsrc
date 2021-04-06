086d1c315323fb9a70bd9ad6a2eb1b7570c4df75
adam <adam@pkgsrc.org>
2021-04-06 15:22:38 +0000

    use versioned py-pygemnts

--- www/py-flask-flatpages/options.mk
+++ www/py-flask-flatpages/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2020/05/12 10:49:35 kleink Exp $
+# $NetBSD: options.mk,v 1.2 2021/04/06 15:22:38 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-flask-flatpages
 PKG_SUPPORTED_OPTIONS=	pygments
@@ -7,5 +7,5 @@ PKG_SUGGESTED_OPTIONS+=	pygments
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mpygments)
-DEPENDS+=		${PYPKGPREFIX}-pygments>=1.6:../../textproc/py-pygments
+PYTHON_VERSIONED_DEPENDENCIES=	pygments
 .endif
