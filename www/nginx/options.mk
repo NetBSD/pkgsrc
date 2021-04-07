5ec8cb87e29c2d71bd1e522504fb509f20b0240f
tpaul <tpaul@pkgsrc.org>
2021-04-07 13:09:13 +0000

    nginx: Add support for the geoip2 module

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.67 2020/12/16 20:32:01 otis Exp $
+# $NetBSD: options.mk,v 1.68 2021/04/07 13:09:13 tpaul Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nginx
 PKG_SUPPORTED_OPTIONS=	array-var auth-request cache-purge dav debug
 PKG_SUPPORTED_OPTIONS+=	echo encrypted-session flv form-input
-PKG_SUPPORTED_OPTIONS+=	geoip gtools gzip headers-more http2
+PKG_SUPPORTED_OPTIONS+=	geoip geoip2 gtools gzip headers-more http2
 PKG_SUPPORTED_OPTIONS+=	image-filter luajit mail-proxy memcache
 PKG_SUPPORTED_OPTIONS+=	naxsi njs pcre perl push realip rtmp
 PKG_SUPPORTED_OPTIONS+=	secure-link set-misc slice ssl status
@@ -154,6 +154,16 @@ SETMISC_DISTFILE=		${SETMISC_DISTNAME}.tar.gz
 SITES.${SETMISC_DISTFILE}=	-${MASTER_SITE_GITHUB:=openresty/set-misc-nginx-module/archive/}v${SETMISC_VERSION}.tar.gz
 DISTFILES+=			${SETMISC_DISTFILE}
 CONFIGURE_ARGS+=		--add-module=../${SETMISC_DISTNAME}
+.endif
+
+.if !empty(PKG_OPTIONS:Mgeoip2) || make(makesum) || make(mdi) || make(distclean)
+GEOIP2_VERSION=			3.3
+GEOIP2_DISTNAME=		ngx_http_geoip2_module-${GEOIP2_VERSION}
+GEOIP2_DISTFILE=		${GEOIP2_DISTNAME}.tar.gz
+SITES.${GEOIP2_DISTFILE}=	-${MASTER_SITE_GITHUB:=leev/ngx_http_geoip2_module/archive/}${GEOIP2_VERSION}.tar.gz
+DISTFILES+=			${GEOIP2_DISTFILE}
+CONFIGURE_ARGS+=		--add-module=../${GEOIP2_DISTNAME}
+.include "../../geography/libmaxminddb/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Marray-var) || make(makesum) || make(mdi) || make(distclean)
