dc8c7fb142ecaa33bb4cb6c8cdfb7ba1a56bad6b
fox <fox@pkgsrc.org>
2021-04-07 13:11:14 +0000

    chat/coyim: Fixes the broken build.
    Remove the missing accel_flags for now from gotk3 vendor library.

---
+++ chat/coyim/patches/patch-vendor_github.com_gotk3_gotk3_gtk_accel.go
@@ -0,0 +1,24 @@
+$NetBSD: patch-vendor_github.com_gotk3_gotk3_gtk_accel.go,v 1.1 2021/04/07 13:11:14 fox Exp $
+
+Remove the missing accel_flags field to prevent build failure.
+
+XXX: Investigate this further.
+
+--- vendor/github.com/gotk3/gotk3/gtk/accel.go.orig	2021-04-07 12:06:33.563048146 +0000
++++ vendor/github.com/gotk3/gotk3/gtk/accel.go
+@@ -244,7 +244,6 @@ func (v *AccelKey) native() *C.struct__G
+ 	var val C.struct__GtkAccelKey
+ 	val.accel_key = C.guint(v.key)
+ 	val.accel_mods = C.GdkModifierType(v.mods)
+-	val.accel_flags = v.flags
+ 	return &val
+ }
+ 
+@@ -253,7 +252,6 @@ func wrapAccelKey(obj *C.struct__GtkAcce
+ 
+ 	v.key = uint(obj.accel_key)
+ 	v.mods = gdk.ModifierType(obj.accel_mods)
+-	v.flags = uint16(obj.accel_flags)
+ 
+ 	return &v
+ }
