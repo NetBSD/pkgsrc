7aca7b1e4793e631bbbdd97c93e427373c3564eb
markd <markd@pkgsrc.org>
2021-04-07 12:28:14 +0000

    kde applications release: update to 20.12.3
    10 months worth of updates.

--- meta-pkgs/kde/applications.mk
+++ meta-pkgs/kde/applications.mk
@@ -1,6 +1,6 @@
-# $NetBSD: applications.mk,v 1.6 2020/06/09 06:51:46 markd Exp $
+# $NetBSD: applications.mk,v 1.7 2021/04/07 12:28:14 markd Exp $
 
-KAPPSVER?=	20.04.1
+KAPPSVER?=	20.12.3
 CATEGORIES+=	kde
 MASTER_SITES?=	${MASTER_SITE_KDE:=release-service/${KAPPSVER}/src/}
 EXTRACT_SUFX?=	.tar.xz
