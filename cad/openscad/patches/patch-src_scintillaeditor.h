79822ae18bb99a310d670a495f980371a40bace3
bouyer <bouyer@pkgsrc.org>
2021-04-08 10:26:08 +0000

    Update to 2021.01, ok reinoud@ Main changes since 2019.05: The big language change is the introduction of function literals but there are a number of other useful changes too, like the new exponent operator (^) and much better and more general support for tail recursion optimization.
    The GUI and especially the Editor has seen a huge amount of new features.
    Most visible is probably the auto-complete which can propose built-in
    functions and modules.
    
    Another much anticipated feature is the "jump to code" from the 3d preview.
    While it's not possible to go into the deepest levels of the design in all
    cases, it's a great tool to navigate designs.
    
    But it's worth highlighting also some of the not so obvious smaller features
    which might improve the editing workflow.
    
        » Bookmarks - Ctrl+F2 to toogle bookmarks, F2 / Shift+F2 for jumping to
          next/previous bookmark.
        » Changing numbers via mouse - Similar to ALT+Cursor-Up/Down, numbers can
          be modified by placing the cursor at the number and using the mouse
          scroll wheel while pressing the ALT key.
        » Code snippets - Insert often used code snippets using ALT+Insert or via
          editor context menu, custom templates can be added as simple files.
    
    for a complete list of changes see
    http://www.openscad.org/news.html#20210131

--- cad/openscad/patches/patch-src_scintillaeditor.h
+++ cad/openscad/patches/patch-src_scintillaeditor.h
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_scintillaeditor.h,v 1.2 2019/06/22 12:57:50 mef Exp $
+$NetBSD: patch-src_scintillaeditor.h,v 1.3 2021/04/08 10:26:08 bouyer Exp $
 
 the Qt4 moc has some difficulty parsing in the boost headers. Apply
 the workaround from https://bugreports.qt.io/browse/QTBUG-22829
 
---- src/scintillaeditor.h.orig	2018-03-13 12:12:43.000000000 +0000
-+++ src/scintillaeditor.h
-@@ -12,8 +12,10 @@
- #include "parsersettings.h"
+--- src/scintillaeditor.h.orig	2021-01-31 18:41:09.000000000 +0100
++++ src/scintillaeditor.h	2021-04-04 21:01:50.732077378 +0200
+@@ -1,8 +1,10 @@
+ #pragma once
  
- #include "memory.h"
+ #include <functional>
 +#ifndef Q_MOC_RUN
  #include <boost/property_tree/ptree.hpp>
  #include <boost/property_tree/json_parser.hpp>
 +#endif
  
- class EditorColorScheme
- {
+ #include <QMap>
+ #include <QObject>
