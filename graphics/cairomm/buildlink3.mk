5323aaf9900a078ef1a399ca8952e58a96d74064
adam <adam@pkgsrc.org>
2021-04-08 21:08:40 +0000

    cairomm: updated to 1.14.2
    1.14.2 (stable):
    
    The tarball for 1.14.2 has been created with 'meson dist'.
    If you build with Autotools from the tarball, please read the relevant
    part of the README file.
    
    Build:
    * Meson build: Fix versioning on macOS
    * Require cairo >= 1.12.0
      Not a new requirement, but now it's specified in configure.ac and
      meson.build, so a failure to meet it is noticed earlier when building
    
    
    1.14.0 (stable):
    
    The tarball for 1.14.0 has been created with 'meson dist'.
    If you build with Autotools from the tarball, please read the relevant
    part of the README file.
    
    Cairo:
    * Add Context::get_source_for_surface()
    
    Build:
    * Fix the build with MinGW headers
    * enums.h: Guard the FT-specific include and the enum with an ifdef
    * Add NMake Makefiles
      Remove Visual Studio 2013 projects
    * Add support for building with Meson
    * Use __declspec(dllexport) when building cairomm on Visual Studio
      Stop using gendef.exe by default
    * docs/reference/: Update for Doxygen >= 1.8.16
    
    Tests:
    * Update the source code
    * Don't include deprecated Boost header files
      Fix tests in Autotools builds
    
    Documentation:
    * README: Update with instructions for building cairomm

--- graphics/cairomm/buildlink3.mk
+++ graphics/cairomm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2020/08/17 20:17:27 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2021/04/08 21:08:40 adam Exp $
 
 BUILDLINK_TREE+=	cairomm
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.cairomm+=	cairomm>=1.2.2
 BUILDLINK_ABI_DEPENDS.cairomm+=	cairomm>=1.12.2nb5
 BUILDLINK_PKGSRCDIR.cairomm?=	../../graphics/cairomm
 
+.include "../../devel/libsigc++/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
 .endif # CAIROMM_BUILDLINK3_MK
 
