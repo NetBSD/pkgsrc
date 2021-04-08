ee43c3b42320eab5c0dbb7dad605da7b52065f4b
adam <adam@pkgsrc.org>
2021-04-08 21:21:38 +0000

    gtkmm3: updated to 3.24.4
    3.24.4 (stable):
    
    The tarball for 3.24.4 has been created with 'meson dist'.
    If you build with Autotools from the tarball, please read the relevant
    part of the README file.
    
    Build on Windows:
    * Gtk::TreePath: Export comparison operators
    
    
    3.24.3 (stable):
    
    The tarball for 3.24.3 has been created with 'meson dist'.
    If you build with Autotools from the tarball, please read the relevant
    part of the README file.
    
    Gtk:
    * Actionable::get_action_target_value(): Fix ref count
    
    Documentation:
    * Improve the documentation of Gtk::Application::add_window()
      and Gtk::Window::set_application()
    * Gtk::Button::set_image(): Fix the documentation
    * Gtk::TreeView::append_column(): Add code example
    * README: Add instructions for building gtkmm
    * Explain key values
    
    Demos:
    * Don't use C++14 auto deduction
    * Update the GLArea demo (similar to updates in gtk+)
    * GLArea demo: Fix a crash
    
    Tests:
    * Builder test: Test derived props declared in C++
    
    Build:
    * Improve NMake support
    * Fix Autotools build with X11 backend disabled (--enable-x11-backend=no)
    * Use __declspec(dllexport) when building with Visual Studio
    * Add support for building gtkmm with Meson
    * docs/reference/: Update for Doxygen >= 1.8.16

--- x11/gtkmm3/buildlink3.mk
+++ x11/gtkmm3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2020/11/05 09:07:14 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2021/04/08 21:21:38 adam Exp $
 
 BUILDLINK_TREE+=	gtkmm3
 
@@ -9,15 +9,13 @@ BUILDLINK_API_DEPENDS.gtkmm3+=	gtkmm3>=3.4.2
 BUILDLINK_ABI_DEPENDS.gtkmm3?=	gtkmm3>=3.24.2nb4
 BUILDLINK_PKGSRCDIR.gtkmm3?=	../../x11/gtkmm3
 
-.include "../../textproc/libxslt/buildlink3.mk"
 .include "../../devel/atkmm/buildlink3.mk"
 .include "../../graphics/cairomm/buildlink3.mk"
-.include "../../x11/gtk3/buildlink3.mk"
 .include "../../devel/pangomm/buildlink3.mk"
 .include "../../devel/glibmm/buildlink3.mk"
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libsigc++/buildlink3.mk"
+.include "../../textproc/libxslt/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif	# GTKMM3_BUILDLINK3_MK
 
