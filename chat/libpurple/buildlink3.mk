df7d0c1627a96a84e0cfacce9aafaea9c23e6fa3
wiz <wiz@pkgsrc.org>
2021-04-09 06:54:56 +0000

    *: bump PKGREVISION for nss linking fix

--- chat/libpurple/buildlink3.mk
+++ chat/libpurple/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2020/11/05 09:06:43 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2021/04/09 06:54:56 wiz Exp $
 
 BUILDLINK_TREE+=	libpurple
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libpurple
 LIBPURPLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpurple+=	libpurple>=2.11.0
-BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.14.1nb3
+BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.14.1nb5
 BUILDLINK_PKGSRCDIR.libpurple?=		../../chat/libpurple
 
 pkgbase := libpurple
