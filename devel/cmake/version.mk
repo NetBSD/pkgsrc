d08be64615811569d727b20b6c0ceb3b2eaf8628
adam <adam@pkgsrc.org>
2021-04-10 08:35:12 +0000

    cmake: updated to 3.20.1
    CMake 3.20.1
    * Help: Add Q_NAMESPACE_EXPORT to CMAKE_AUTOMOC_MACRO_NAMES default values
    * FindHDF5: search for the new Fortran HL library name
    * gitlab-ci: Update Windows builds to MSVC 19.28-16.9 toolset
    * FindIntl: Fix detection of intl built in to C library
    * GNUInstallDirs: Clarify that CMAKE_INSTALL_<dir> may be absolute
    * Tests: Teach RunCMake to ignore incidental 'Recompacting log' ninja output
    * Ninja Multi-Config: Fix crash on custom command config with no output
    * Tests: Add RunCMake helper to run a plain script
    * Help: CMAKE_APPLE_SILICON_PROCESSOR cannot be set in a toolchain file
    * gitlab-ci: Tell CDash when a test-ext job is done
    * Help: Document in add_library how to import libraries with SONAME
    * libarchive: Use uint8_t instead of u_char
    * Utilities/Release: Add script to generate a table of files
    * Utilities/Release: Add deprecation fields to File Table v1
    * UseSWIG: Transform swig depfile to match Ninja generator paths
    * ci: add jq and DevIL to Debian and Fedora base images
    * ci: Enable jq-based tests on Linux builds
    * BinUtils: Avoid clobbering a variable named without a private prefix
    * Makefiles: Fix dependency extraction with CUDA < 10.2 and host compiler
    * FindBLAS: Fix detection of OpenMP as dependency of BLA_STATIC
    * Restore support for backslashes in initial language-wide flags
    * CPack: Validate and document NSIS branding text trim positions
    * Help: Custom OUTPUT and BYPRODUCTS genexes cannot refer to targets
    * Tests: Remove redundant files for configure_file() tests
    * Tests: Check host platform instead of target for running stat
    * Cleanup: Fix misspelt name of local C++ variable
    * Help: Clarify permission-related command options
    * Apple: Set CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG on non-macOS too
    * Android: Fix search for binutils
    * Cray: Detect Fortran compiler version patch level if available
    * Ninja Multi-Config: Include configs in long CC scripts
    * autogen: Don't include SKIP_AUTOMOC files in depfile
    * Tests: Add test for Ninja automoc dependency cyle
    * Makefiles dependencies: normalize windows paths
    * CUDA Device link: Ensure all link options are correctly formatted
    * Genex: $<HOST_LINK:> and $<DEVICE_LINK:> must be usable in try_compile
    * FindMPI: avoid host link options to be propagated to device link step
    * UseSWIG: Run using policy settings from includer
    * cmCommandLineArgument: correctly compute next parse index
    * CMakeDetermineCompilerABI: Revert "Parse library arch from versioned paths"
    * CMakeDetermineCompilerABI: Extract lib arch from implicit object file paths
    * FindIntl: Improve documentation formatting
    * Help: Clarify IMPORTED_LOCATION documentation

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.12 2021/03/31 07:43:39 adam Exp $
+# $NetBSD: version.mk,v 1.13 2021/04/10 08:35:12 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.20.0
+CMAKE_VERSION=	3.20.1
 CMAKE_API=	${CMAKE_VERSION:R}
