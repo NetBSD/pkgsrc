b8674b0c9e6f11ee7b7ee29ce06d096be17496cc
taca <taca@pkgsrc.org>
2021-04-11 13:28:01 +0000

    www/ruby-rails61: update to 6.1.3.1
    Real changes are in devel/devel/ruby-activestorage61 only.
    
    ## Rails 6.1.3.1 (March 26, 2021) ##
    
    *  Marcel is upgraded to version 1.0.0 to avoid a dependency on GPL-licensed
       mime types data.
    
       *George Claghorn*

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.97 2021/04/11 13:24:56 taca Exp $
+# $NetBSD: rails.mk,v 1.98 2021/04/11 13:28:01 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -49,7 +49,7 @@ _RUBY_RAILS_MK=	# defined
 #
 RUBY_RAILS52_VERSION?=	5.2.5
 RUBY_RAILS60_VERSION?=	6.0.3.6
-RUBY_RAILS61_VERSION?=	6.1.3
+RUBY_RAILS61_VERSION?=	6.1.3.1
 
 RUBY_RAILS_ACCEPTED?=	# defined
 RUBY_RAILS_DEFAULT?=	52
