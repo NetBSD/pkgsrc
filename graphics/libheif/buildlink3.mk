146b8ad5420242b9cdc436502a8bc76f0786db31
adam <adam@pkgsrc.org>
2021-04-12 08:29:59 +0000

    Revbump for multimedia/x265 update

--- graphics/libheif/buildlink3.mk
+++ graphics/libheif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/08/31 17:24:12 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/12 08:29:59 adam Exp $
 
 BUILDLINK_TREE+=	libheif
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libheif
 LIBHEIF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libheif+=	libheif>=1.8.0
+BUILDLINK_ABI_DEPENDS.libheif?=	libheif>=1.11.0nb1
 BUILDLINK_PKGSRCDIR.libheif?=	../../graphics/libheif
 
 .include "../../graphics/png/buildlink3.mk"
