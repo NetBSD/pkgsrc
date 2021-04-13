32e9618483b68c1b3fcdc1663ac8630af4fcff74
wiz <wiz@pkgsrc.org>
2021-04-13 11:36:59 +0000

    gupnp: add vala build dependency now that it does not inherit it from gssdp any longer

--- net/gupnp/buildlink3.mk
+++ net/gupnp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2020/11/05 09:07:03 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2021/04/13 11:36:59 wiz Exp $
 
 BUILDLINK_TREE+=	gupnp
 
@@ -12,7 +12,7 @@ BUILDLINK_PKGSRCDIR.gupnp?=	../../net/gupnp
 .include "../../mk/bsd.fast.prefs.mk"
 .if (!empty(OPSYS:M*BSD) || ${OPSYS} == "DragonFly" || ${OPSYS} == "Darwin") && (!defined(USE_INTERNAL_UUID) || empty(USE_INTERNAL_UUID:M[Yy][Ee][Ss]))
 pre-configure:
-	if ! test -e ${BUILDLINK_DIR}/lib/pkgconfig/uuid.pc; then cp ${BUILDLINK_PKGSRCDIR.gupnp}/files/uuid.pc ${BUILDLINK_DIR}/lib/pkgconfig/uuid.pc; fi
+	if ! ${TEST} -e ${BUILDLINK_DIR}/lib/pkgconfig/uuid.pc; then cp ${BUILDLINK_PKGSRCDIR.gupnp}/files/uuid.pc ${BUILDLINK_DIR}/lib/pkgconfig/uuid.pc; fi
 .else
 .include "../../devel/libuuid/buildlink3.mk"
 .endif
