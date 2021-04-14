75403230d543a2d93ed587d40f30ff5117e71e6a
wiz <wiz@pkgsrc.org>
2021-04-14 07:28:16 +0000

    *: bump for vala 0.52

--- x11/libdesktop-agnostic/buildlink3.mk
+++ x11/libdesktop-agnostic/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.40 2020/11/05 09:07:17 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.41 2021/04/14 07:28:16 wiz Exp $
 
 BUILDLINK_TREE+=	libdesktop-agnostic
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdesktop-agnostic
 LIBDESKTOP_AGNOSTIC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92
-BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92nb41
+BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92nb43
 BUILDLINK_PKGSRCDIR.libdesktop-agnostic?=	../../x11/libdesktop-agnostic
 
 .include "../../lang/vala/buildlink3.mk"
