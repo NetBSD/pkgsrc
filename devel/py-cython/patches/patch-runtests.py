d0ee07f50e36f3edec2cefee58726807c18c8269
adam <adam@pkgsrc.org>
2021-04-14 19:25:15 +0000

    py-cython: updated to 0.29.23
    0.29.23 (2021-04-14)
    ====================
    Bugs fixed
    ----------
    * Some problems with Python 3.10 were resolved.
    * An incorrect "optimisation" was removed that allowed changes to a keyword
      dict to leak into keyword arguments passed into a function.
    * Multiplied str constants could end up as bytes constants with language_level=2.
    * ``PY_SSIZE_T_CLEAN`` does not get defined any more if it is already defined.

--- devel/py-cython/patches/patch-runtests.py
+++ devel/py-cython/patches/patch-runtests.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-runtests.py,v 1.2 2018/10/15 08:21:03 adam Exp $
+$NetBSD: patch-runtests.py,v 1.3 2021/04/14 19:25:15 adam Exp $
 
 Use correct make for pkgsrc.
 
---- runtests.py.orig	2018-10-14 15:27:58.000000000 +0000
+--- runtests.py.orig	2021-04-14 15:24:45.000000000 +0000
 +++ runtests.py
-@@ -1770,12 +1770,12 @@ class EmbedTest(unittest.TestCase):
+@@ -1773,12 +1773,12 @@ class EmbedTest(unittest.TestCase):
          self.old_dir = os.getcwd()
          os.chdir(self.working_dir)
          os.system(
@@ -19,12 +19,12 @@ Use correct make for pkgsrc.
          except:
              pass
          os.chdir(self.old_dir)
-@@ -1795,7 +1795,7 @@ class EmbedTest(unittest.TestCase):
-             cython = os.path.join(CY3_DIR, cython)
-         cython = os.path.abspath(os.path.join('..', '..', cython))
-         self.assertEqual(0, os.system(
--            "make PYTHON='%s' CYTHON='%s' LIBDIR1='%s' test > make.output" % (sys.executable, cython, libdir)))
-+            "gmake PYTHON='%s' CYTHON='%s' LIBDIR1='%s' test > make.output" % (sys.executable, cython, libdir)))
+@@ -1800,7 +1800,7 @@ class EmbedTest(unittest.TestCase):
+ 
          try:
-             os.remove('make.output')
-         except OSError:
+             subprocess.check_call([
+-                "make",
++                "gmake",
+                 "PYTHON='%s'" % sys.executable,
+                 "CYTHON='%s'" % cython,
+                 "LIBDIR1='%s'" % libdir,
