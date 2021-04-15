25740ba5c6d7684cb92fc1af5739ced51f88d5dc
wiz <wiz@pkgsrc.org>
2021-04-15 08:42:22 +0000

    grive2: update to 0.5.1.
    grive2 (0.5.1+git20160731) unstable; urgency=medium
    
      * Newer dev version
    
    grive2 (0.5+git20160114) unstable; urgency=medium
    
      * Newer release, with support for faster sync and rename detection

---
+++ net/grive2/patches/patch-libgrive_src_util_OS.hh
@@ -0,0 +1,17 @@
+$NetBSD: patch-libgrive_src_util_OS.hh,v 1.1 2021/04/15 08:42:23 wiz Exp $
+
+off64_t is not universally available.
+
+--- libgrive/src/util/OS.hh.orig	2019-11-12 22:05:31.000000000 +0000
++++ libgrive/src/util/OS.hh
+@@ -31,6 +31,10 @@ class Path ;
+ 
+ enum FileType { FT_FILE = 1, FT_DIR = 2, FT_UNKNOWN = 3 } ;
+ 
++#ifndef off64_t
++#define off64_t off_t
++#endif
++
+ namespace os
+ {
+ 	struct Error : virtual Exception {} ;
