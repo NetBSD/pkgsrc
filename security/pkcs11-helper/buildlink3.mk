15a46c9d16b77eb11cfe1f64957b5c503e930c59
ryoon <ryoon@pkgsrc.org>
2021-04-15 11:23:03 +0000

    *: Recursive revbump from devel/nss

--- security/pkcs11-helper/buildlink3.mk
+++ security/pkcs11-helper/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2021/04/10 08:39:23 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2021/04/15 11:23:04 ryoon Exp $
 
 BUILDLINK_TREE+=	pkcs11-helper
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pkcs11-helper
 PKCS11_HELPER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.06
-BUILDLINK_ABI_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.10nb14
+BUILDLINK_ABI_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.27.0nb1
 BUILDLINK_PKGSRCDIR.pkcs11-helper?=	../../security/pkcs11-helper
 
 pkgbase := pkcs11-helper
