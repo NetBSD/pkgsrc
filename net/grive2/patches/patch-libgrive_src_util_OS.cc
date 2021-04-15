4f6a8ab8b054f696e2c7d8fa744221cc91d1b51a
wiz <wiz@pkgsrc.org>
2021-04-15 08:42:22 +0000

    grive2: update to 0.5.1.
    grive2 (0.5.1+git20160731) unstable; urgency=medium
    
      * Newer dev version
    
    grive2 (0.5+git20160114) unstable; urgency=medium
    
      * Newer release, with support for faster sync and rename detection

--- net/grive2/patches/patch-libgrive_src_util_OS.cc
+++ net/grive2/patches/patch-libgrive_src_util_OS.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-libgrive_src_util_OS.cc,v 1.1.1.1 2015/06/16 16:15:37 abs Exp $
+$NetBSD: patch-libgrive_src_util_OS.cc,v 1.2 2021/04/15 08:42:23 wiz Exp $
 
 Add __NetBSD__ to the 64bit Apple ctimespec stat case
 
---- libgrive/src/util/OS.cc.orig	2015-05-31 19:35:15.000000000 +0000
+--- libgrive/src/util/OS.cc.orig	2019-11-12 22:05:31.000000000 +0000
 +++ libgrive/src/util/OS.cc
-@@ -57,7 +57,7 @@ DateTime FileCTime( const std::string& f
- 		) ;
- 	}
+@@ -59,7 +59,7 @@ void Stat( const std::string& filename, 
  	
+ 	if ( t )
+ 	{
 -#if defined __APPLE__ && defined __DARWIN_64_BIT_INO_T
 +#if defined __NetBSD__ || ( defined __APPLE__ && defined __DARWIN_64_BIT_INO_T )
- 	return DateTime( s.st_ctimespec.tv_sec, s.st_ctimespec.tv_nsec ) ;
+ 		*t = DateTime( s.st_ctimespec.tv_sec, s.st_ctimespec.tv_nsec ) ;
  #else
- 	return DateTime( s.st_ctim.tv_sec, s.st_ctim.tv_nsec);
+ 		*t = DateTime( s.st_ctim.tv_sec, s.st_ctim.tv_nsec);
