c91029babbd3dc4ea1262e8a540b9c29d5a12424
adam <adam@pkgsrc.org>
2021-04-15 05:16:35 +0000

    py-acme py-certbot*: updated to 1.14.0
    Certbot 1.14.0
    
    Changed
    
    certbot-auto no longer checks for updates on any operating system.
    The module acme.magic_typing is deprecated and will be removed in a future release.
    Please use the built-in module typing instead.
    The DigitalOcean plugin now creates TXT records for the DNS-01 challenge with a lower 30s TTL.
    
    Fixed
    
    Don't output an empty line for a hidden certificate when certbot certificates is being used
    in combination with --cert-name or -d.

--- security/py-certbot-nginx/patches/patch-certbot__nginx___internal_constants.py
+++ security/py-certbot-nginx/patches/patch-certbot__nginx___internal_constants.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-certbot__nginx___internal_constants.py,v 1.3 2020/08/26 11:10:15 adam Exp $
+$NetBSD: patch-certbot__nginx___internal_constants.py,v 1.4 2021/04/15 05:16:37 adam Exp $
 
 Look for nginx.conf in PKG_SYSCONFDIR
 
---- certbot_nginx/_internal/constants.py.orig	2020-08-04 18:20:15.000000000 +0000
+--- certbot_nginx/_internal/constants.py.orig	2021-04-06 17:17:00.000000000 +0000
 +++ certbot_nginx/_internal/constants.py
-@@ -4,19 +4,8 @@ import platform
- from acme.magic_typing import Any
- from acme.magic_typing import Dict
+@@ -3,19 +3,8 @@ import platform
+ from typing import Any
+ from typing import Dict
  
 -FREEBSD_DARWIN_SERVER_ROOT = "/usr/local/etc/nginx"
 -LINUX_SERVER_ROOT = "/etc/nginx"
@@ -19,9 +19,9 @@ Look for nginx.conf in PKG_SYSCONFDIR
 -else:
 -    server_root_tmp = LINUX_SERVER_ROOT
 -
- CLI_DEFAULTS = dict(
+ CLI_DEFAULTS: Dict[str, Any] = dict(
 -    server_root=server_root_tmp,
 +    server_root="@PKG_SYSCONFDIR@/nginx",
      ctl="nginx",
      sleep_seconds=1
- ) # type: Dict[str, Any]
+ )
