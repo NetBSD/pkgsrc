06466d3324ec12b69e526d0018f466da8166a2ca
ryoon <ryoon@pkgsrc.org>
2021-04-16 14:55:42 +0000

    libclucene: Fix build with GCC 10
    * Match all GCC versions, ignore GCC 2 or earlier cases.
      Fix build with GCC 10 or later.

--- textproc/libclucene/patches/patch-src_shared_cmake_MacroCheckGccVisibility.cmake
+++ textproc/libclucene/patches/patch-src_shared_cmake_MacroCheckGccVisibility.cmake
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_shared_cmake_MacroCheckGccVisibility.cmake,v 1.1 2018/02/08 07:28:58 mrg Exp $
+$NetBSD: patch-src_shared_cmake_MacroCheckGccVisibility.cmake,v 1.2 2021/04/16 14:55:42 ryoon Exp $
 
-also look for GCC 6, 7, 8 and 9.
+* Match all GCC versions, ignore GCC 2 or earlier cases.
+  Fix build with GCC 10 or later.
 
 --- src/shared/cmake/MacroCheckGccVisibility.cmake.orig	2011-03-17 00:21:07.000000000 +0000
 +++ src/shared/cmake/MacroCheckGccVisibility.cmake	2018-02-08 06:10:02.136958010 +0000
@@ -9,7 +10,7 @@ also look for GCC 6, 7, 8 and 9.
     exec_program(${CMAKE_C_COMPILER} ARGS --version OUTPUT_VARIABLE _gcc_version_info)
  
 -   string (REGEX MATCH "[345]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
-+   string (REGEX MATCH "[3456789]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
++   string (REGEX MATCH "[0-9]+\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
     if (NOT _gcc_version)
     
        # clang reports: clang version 1.1 (trunk 95754)
