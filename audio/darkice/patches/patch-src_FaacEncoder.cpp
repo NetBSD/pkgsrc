fb14e4d520e34383efec671d2eb6a354c1a41cda
nia <nia@pkgsrc.org>
2021-04-17 09:07:46 +0000

    darkice: Update to 1.4
    pkgsrc changes:
    
    - add many more package options.
    - adjust the man page for device names on NetBSD.
    
    01-03-2020 Darkice 1.4 released
        o Shoutcast and Icecast 2 connection issues fixed.
    
    04-08-2016 Darkice 1.3 released
        o Small bugs fixed by Nicolas Boulenguez <nicolas@debian.org>.
        o Bugs related to streaming to remote servers fixed. Patch by Kalle Kulonen
    	<kulonenk@gmail.com> and Mark Turner <jmarkturner@gmail.com>.

--- audio/darkice/patches/patch-src_FaacEncoder.cpp
+++ audio/darkice/patches/patch-src_FaacEncoder.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_FaacEncoder.cpp,v 1.1 2014/02/22 14:51:03 wiedi Exp $
+$NetBSD: patch-src_FaacEncoder.cpp,v 1.2 2021/04/17 09:07:46 nia Exp $
 
 string.h is needed for memmove()
+
 --- src/FaacEncoder.cpp.orig	2013-07-15 05:50:42.000000000 +0000
 +++ src/FaacEncoder.cpp
 @@ -37,7 +37,7 @@
