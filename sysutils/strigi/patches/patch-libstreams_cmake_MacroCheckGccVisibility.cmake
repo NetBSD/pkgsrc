43bdd581e0a15e9101214d056c0e68f587062b15
mrg <mrg@pkgsrc.org>
2021-04-18 20:34:04 +0000

    copy libclucene patch for MacroCheckGccVisibility.cmake to make this work for future GCC too.

--- sysutils/strigi/patches/patch-libstreams_cmake_MacroCheckGccVisibility.cmake
+++ sysutils/strigi/patches/patch-libstreams_cmake_MacroCheckGccVisibility.cmake
@@ -1,6 +1,6 @@
-$NetBSD: patch-libstreams_cmake_MacroCheckGccVisibility.cmake,v 1.1 2018/02/08 07:28:58 mrg Exp $
+$NetBSD: patch-libstreams_cmake_MacroCheckGccVisibility.cmake,v 1.2 2021/04/18 20:34:04 mrg Exp $
 
-support netbsd 6, 7, 8, and 9.
+* Match all GCC versions.  Fix build with GCC 10 or later.
 
 --- libstreams/cmake/MacroCheckGccVisibility.cmake.orig	2013-02-05 13:34:57.000000000 -0800
 +++ libstreams/cmake/MacroCheckGccVisibility.cmake	2018-02-07 22:38:19.000000000 -0800
@@ -9,7 +9,7 @@ support netbsd 6, 7, 8, and 9.
     exec_program(${CMAKE_C_COMPILER} ARGS ${CMAKE_C_COMPILER_ARG1} --version OUTPUT_VARIABLE _gcc_version_info)
  
 -   string (REGEX MATCH "[345]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
-+   string (REGEX MATCH "[3456789]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
++   string (REGEX MATCH "[0-9]+\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
     # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the patch level, handle this here:
     if (NOT _gcc_version)
        string (REGEX REPLACE ".*\\(GCC\\).* ([34]\\.[0-9]) .*" "\\1.0" _gcc_version "${_gcc_version_info}")
