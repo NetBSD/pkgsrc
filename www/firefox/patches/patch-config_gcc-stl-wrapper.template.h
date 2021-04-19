ebae22235fa7a9643e8643c52f7b3210ca24e88c
ryoon <ryoon@pkgsrc.org>
2021-04-19 13:50:07 +0000

    firefox: Update to 88.0
    Changelog:
    New
    
      * PDF forms now support JavaScript embedded in PDF files. Some PDF forms use
        JavaScript for validation and other interactive features.
    
      * Print updates: Margin units are now localized.
    
      * Smooth pinch-zooming using a touchpad is now supported on Linux
    
      * To protect against cross-site privacy leaks, Firefox now isolates
        window.name data to the website that created it. Learn more
    
    Fixed
    
      * Screen readers no longer incorrectly read content that websites have
        visually hidden, as in the case of articles in the Google Help panel.
    
      * Various security fixes.
    
    Changed
    
      * Firefox will not prompt for access to your microphone or camera if you've
        already granted access to the same device on the same site in the same tab
        within the past 50 seconds. This new grace period reduces the number of
        times you're prompted to grant device access.
    
      * The "Take a Screenshot" feature was removed from the Page Actions menu in
        the url bar. To take a screenshot, right-click to open the context menu.
        You can also add a screenshots shortcut directly to your toolbar via the
        Customize menu. Open the Firefox menu and select Customize...
    
      * FTP support has been disabled, and its full removal is planned for an
        upcoming release. Addressing this security risk reduces the likelihood of
        an attack while also removing support for a non-encrypted protocol.
    
    Security fixes:
    #CVE-2021-23994: Out of bound write due to lazy initialization
    #CVE-2021-23995: Use-after-free in Responsive Design Mode
    #CVE-2021-23996: Content rendered outside of webpage viewport
    #CVE-2021-23997: Use-after-free when freeing fonts from cache
    #CVE-2021-23998: Secure Lock icon could have been spoofed
    #CVE-2021-23999: Blob URLs may have been granted additional privileges
    #CVE-2021-24000: requestPointerLock() could be applied to a tab different from
     the visible tab
    #CVE-2021-24001: Testing code could have enabled session history manipulations
     by a compromised content process
    #CVE-2021-24002: Arbitrary FTP command execution on FTP servers using an
     encoded URL
    #CVE-2021-29945: Incorrect size computation in WebAssembly JIT could lead to
     null-reads
    #CVE-2021-29944: HTML injection vulnerability in Firefox for Android's Reader
     View
    #CVE-2021-29946: Port blocking could be bypassed
    #CVE-2021-29947: Memory safety bugs fixed in Firefox 88

--- www/firefox/patches/patch-config_gcc-stl-wrapper.template.h
+++ www/firefox/patches/patch-config_gcc-stl-wrapper.template.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.2 2020/04/09 14:01:26 ryoon Exp $
+$NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.3 2021/04/19 13:50:07 ryoon Exp $
 
---- config/gcc-stl-wrapper.template.h.orig	2020-04-03 19:34:34.000000000 +0000
+--- config/gcc-stl-wrapper.template.h.orig	2021-04-08 21:20:01.000000000 +0000
 +++ config/gcc-stl-wrapper.template.h
-@@ -28,8 +28,8 @@
- #endif
- 
- // Don't include mozalloc for cstdlib. See bug 1245076.
+@@ -32,8 +32,8 @@
+ // bug 1694575 (iosfwd).
+ // Please be careful when adding more exceptions, especially regarding
+ // the header not directly or indirectly including <new>.
 -#ifndef moz_dont_include_mozalloc_for_cstdlib
 -#  define moz_dont_include_mozalloc_for_cstdlib
 +#ifndef moz_dont_include_mozalloc_for_${HEADER}
@@ -13,7 +13,7 @@ $NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.2 2020/04/09 14:01:26 ryoon
  #endif
  
  #ifndef moz_dont_include_mozalloc_for_type_traits
-@@ -43,7 +43,7 @@
+@@ -51,7 +51,7 @@
  // Include mozalloc after the STL header and all other headers it includes
  // have been preprocessed.
  #if !defined(MOZ_INCLUDE_MOZALLOC_H) && \
@@ -22,7 +22,7 @@ $NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.2 2020/04/09 14:01:26 ryoon
  #  define MOZ_INCLUDE_MOZALLOC_H
  #  define MOZ_INCLUDE_MOZALLOC_H_FROM_${HEADER}
  #endif
-@@ -72,4 +72,6 @@
+@@ -80,4 +80,6 @@
  #  include "mozilla/throw_gcc.h"
  #endif
  
