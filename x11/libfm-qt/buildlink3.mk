11fcca8748d9eba00abdbf0ee91eb23dd1cfee63
pin <pin@pkgsrc.org>
2021-04-20 18:01:18 +0000

    x11/libfm-qt: update to 0.17.0
    libfm-qt-0-17.1 / 2021-04-16
    ============================
       - Fixed compilation against Qt5.12.
    
    libfm-qt-0.17.0 / 2021-04-15
    ============================
      - Fixed compilation with Qt >=5.15.3.
      - Add Alacritty to terminals list.
      - Fixed template renaming before creating.
      - Fixed the localization of LXQt file dialog.
      - Used Qt5.15's Qt::SplitBehavior.
      - Enabled the Group entry of Properties dialog when file belongs to user.
      - Fixed a rare crash on emptying Trash.
      - Used Qt5 signal/slot syntax.
      - Added the missing eject button in a special case.
      - Fixed the xarchiver command to create archives.
      - Launch multiple files associated with the same app together.
      - Added an open-with menu-item to open mixed selections with default apps.
      - Don't try to auto-select hidden files when they aren't shown.
      - Added support for creation time.
      - File dialog: Allow opening of desktop files that are shortcuts.
      - Fixed the size limit for thumbnail creation.
      - Workaround for a bug in QCollator when sorting files.
      - Added a Delete action to trash file context menu.
      - Removed redundant context menu items inside Trash.
      - Silenced the compilation warning about QMatrix.
      - Don't show Unix start time for unsupported times.
      - Set a separate file size limit for external thumbnails.
      - Fixed the emblem positions in compact and detailed views.
      - Guarantee a separator before all custom actions (in context menus).
      - Fixed the sorting by deletion time.
      - Fixed blurred thumbnails with scale factors > 1.
      - Added a missing nullity check in mountoperation.
      - Updated the mimetypes lists of some archivers.
      - Fixed a memory leak in LXQt file dialog.

--- x11/libfm-qt/buildlink3.mk
+++ x11/libfm-qt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/12/07 11:34:52 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/04/20 18:01:18 pin Exp $
 
 BUILDLINK_TREE+=	libfm-qt
 
 .if !defined(LIBFM_QT_BUILDLINK3_MK)
 LIBFM_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=0.16.0
+BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=0.17.1
 BUILDLINK_PKGSRCDIR.libfm-qt?=		../../x11/libfm-qt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
