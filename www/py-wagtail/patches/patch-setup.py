9a3c526e6abd325a1650a03500ae783ec91942c3
joerg <joerg@pkgsrc.org>
2021-04-20 14:44:04 +0000

    Update wagtail to 2.12.4:
     * Fix: CVE-2021-29434 - fix improper validation of URLs ('Cross-site Scripting') in rich text fields (Kevin Breen, Matt Westcott)
     * Fix: Reverse migration errors in images and documents (Mike Brown)
     * Fix: Avoid wagtailembeds migration failure on MySQL 8.0.13+ (Matt Westcott)
     * Fix: Un-pin django-treebeard following upstream fix for migration issue (Matt Westcott)
     * Fix: Prevent crash when copying an alias page (Karl Hobley)
     * Fix: Prevent errors on page editing after changing LANGUAGE_CODE (Matt Westcott)
     * Fix: Correctly handle model inheritance and `ClusterableModel` on `copy_for_translation` (Karl Hobley)

--- www/py-wagtail/patches/patch-setup.py
+++ www/py-wagtail/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.3 2021/04/06 19:15:02 joerg Exp $
+$NetBSD: patch-setup.py,v 1.4 2021/04/20 14:44:04 joerg Exp $
 
 Unrestrict dependencies
 
---- setup.py.orig	2021-02-18 09:45:56.000000000 +0000
+--- setup.py.orig	2021-04-19 09:28:55.000000000 +0000
 +++ setup.py
 @@ -26,9 +26,9 @@ install_requires = [
-     "django-treebeard>=4.2.0,<4.5",
+     "django-treebeard>=4.2.0,<5.0,!=4.5",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<3.0",
 -    "draftjs_exporter>=2.1.5,<3.0",
