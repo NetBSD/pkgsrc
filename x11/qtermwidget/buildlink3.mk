f680313cd70572df7e7c4cdb7665c22cc3f2024d
gutteridge <gutteridge@pkgsrc.org>
2021-04-20 23:38:23 +0000

    qtermwidget: fix BUILDLINK_API_DEPENDS

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/04/20 18:11:44 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/04/20 23:38:23 gutteridge Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.17.1
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.17.0
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
 #.include "../../x11/qt5-qtbase/buildlink3.mk"
