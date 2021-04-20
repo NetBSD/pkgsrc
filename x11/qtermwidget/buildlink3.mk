13fed0f3b7dcc83637290052d7f87668b950082f
pin <pin@pkgsrc.org>
2021-04-20 18:11:44 +0000

    x11/qtermwidget: update to 0.17.0
    qtermwidget-0.17.0 / 2021-04-15
    ===============================
      - Drop support for KDE3 color scheme formats.
      - Some code cleanup and modernization.
      - Added a method to disable bracketed paste mode.
      - Added an example for PyQt5.
      - Improve "Undecodable sequence" warnings.
      - Properly implement the "Action after paste" feature.
      - Fix execution after paste when Ctrl is hold.
      - Workaround an issue with glibc 2.33 on old Docker engines.
      - Added modes for background image. How background images are drawn is
      changed and manual reconfiguration is needed.
      See docs/configuration.md for more details.

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/12/07 11:48:54 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/04/20 18:11:44 pin Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.16.1
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.17.1
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
 #.include "../../x11/qt5-qtbase/buildlink3.mk"
