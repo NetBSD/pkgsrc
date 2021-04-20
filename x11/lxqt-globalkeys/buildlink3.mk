0e09133a8e305a90fa37cbf09bb983f394dedc13
pin <pin@pkgsrc.org>
2021-04-20 18:43:41 +0000

    x11/lxqt-globalkeys: update to 0.17.0
    lxqt-globalkeys-0.17.0 / 2021-04-15
    ===================================
      - Completely moved to Qt5 signal/slot syntax.

--- x11/lxqt-globalkeys/buildlink3.mk
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/12/07 12:25:25 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/04/20 18:43:41 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.16.0
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.17.0
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
