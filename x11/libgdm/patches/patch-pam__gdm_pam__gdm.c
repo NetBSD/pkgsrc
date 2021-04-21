7e802d0187c6ef3577b774839c664e20fd40b21c
cirnatdan <cirnatdan@pkgsrc.org>
2021-04-21 12:23:21 +0000

    libgdm: Fix building with OpenPAM on Linux

--- x11/libgdm/patches/patch-pam__gdm_pam__gdm.c
+++ x11/libgdm/patches/patch-pam__gdm_pam__gdm.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-pam__gdm_pam__gdm.c,v 1.1 2021/04/16 13:57:52 cirnatdan Exp $
+$NetBSD: patch-pam__gdm_pam__gdm.c,v 1.2 2021/04/21 12:23:21 cirnatdan Exp $
 
 Include only supported pam headers
 
---- pam_gdm/pam_gdm.c.orig	2020-12-15 18:16:43.000000000 +0000
+--- pam_gdm/pam_gdm.c.orig	2021-03-30 13:25:30.000000000 +0000
 +++ pam_gdm/pam_gdm.c
 @@ -21,11 +21,13 @@
  
  #include <unistd.h>
  
-+#ifdef __linux__
++#include <security/pam_modules.h>
++#ifndef OPENPAM
  #include <security/_pam_macros.h>
  #include <security/pam_ext.h>
  #include <security/pam_misc.h>
 -#include <security/pam_modules.h>
  #include <security/pam_modutil.h>
 +#endif
-+#include <security/pam_modules.h>
  
  #ifdef HAVE_KEYUTILS
  #include <keyutils.h>
