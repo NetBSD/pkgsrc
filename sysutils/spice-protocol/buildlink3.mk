da241e2d05fd08194ac6933f8999d9e62527e532
pho <pho@pkgsrc.org>
2021-04-21 16:51:40 +0000

    Fix build on platforms where the default compiler is GCC < 6

--- sysutils/spice-protocol/buildlink3.mk
+++ sysutils/spice-protocol/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/06/03 11:21:03 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/21 16:51:40 pho Exp $
 
 BUILDLINK_TREE+=	spice-protocol
 
@@ -9,6 +9,11 @@ BUILDLINK_DEPMETHOD.spice-protocol?=	build
 
 BUILDLINK_API_DEPENDS.spice-protocol+=	spice-protocol>=0.12.5
 BUILDLINK_PKGSRCDIR.spice-protocol?=	../../sysutils/spice-protocol
+
+# spice/enums.h uses __attribute__(__deprecated__) on enums, which is
+# only available on GCC 6 or later.
+GCC_REQD+=	6
+
 .endif	# SPICE_PROTOCOL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-spice-protocol
