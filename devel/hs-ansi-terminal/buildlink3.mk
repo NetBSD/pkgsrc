5ee0db6c1f26edd0681e99157dcbffa8bb5dc0f0
pho <pho@pkgsrc.org>
2021-04-23 10:49:00 +0000

    Update to ansi-terminal-0.11
    Version 0.11
    * Remove deprecated getCursorPosition0. (Use getCursorPosition
      instead.)
    * On Unix-like operating systems, the temporary turning off of echoing
      is moved from getReportedCursorPosition to hGetCursorPositon.
    * On Unix-like operating systems, fix a bug in getCursorPosition and
      hGetCursorPosition, where the console input stream was was not
      always clear before the cursor position was emitted into it.
    
    Version 0.10.3
    * Add getCursorPosition as a synonym of getCursorPosition0 and
      deprecate the latter.

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 13:08:35 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/23 10:49:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.2
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.2.1
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
 
 .include "../../devel/hs-colour/buildlink3.mk"
