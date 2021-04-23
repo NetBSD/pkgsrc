3c3cbe83846b8edd266f29257ae688211da0ab82
pho <pho@pkgsrc.org>
2021-04-23 07:45:58 +0000

    Update to comonad-5.0.8
    5.0.8 [2020.12.30]
    * Explicitly mark modules as Safe or Trustworthy.
    * The build-type has been changed from Custom to Simple. To achieve
      this, the doctests test suite has been removed in favor of using
      cabal-docspec to run the doctests.
    
    5.0.7 [2020.12.15]
    * Move FunctorWithIndex (TracedT m w) instance from lens. This
      instance depends on the indexed-traversable package. This can be
      disabled using the flag of the same name.

--- math/hs-comonad/buildlink3.mk
+++ math/hs-comonad/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 04:36:38 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 07:45:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-comonad
 
 .if !defined(HS_COMONAD_BUILDLINK3_MK)
 HS_COMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=5.0.6
-BUILDLINK_ABI_DEPENDS.hs-comonad+=	hs-comonad>=5.0.6
+BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=5.0.8
+BUILDLINK_ABI_DEPENDS.hs-comonad+=	hs-comonad>=5.0.8
 BUILDLINK_PKGSRCDIR.hs-comonad?=	../../math/hs-comonad
 
+.include "../../math/hs-distributive/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
-.include "../../math/hs-distributive/buildlink3.mk"
 .endif	# HS_COMONAD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-comonad
