3a7cb3394ed149dc8e7699124d0e6e5cf2723e6e
pho <pho@pkgsrc.org>
2021-04-23 08:24:36 +0000

    Import strict-0.4.0.1

---
+++ devel/hs-strict/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/04/23 08:24:36 pho Exp $
+
+BUILDLINK_TREE+=	hs-strict
+
+.if !defined(HS_STRICT_BUILDLINK3_MK)
+HS_STRICT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-strict+=	hs-strict>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-strict+=	hs-strict>=0.4.0.1
+BUILDLINK_PKGSRCDIR.hs-strict?=		../../devel/hs-strict
+
+.include "../../devel/hs-assoc/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-these/buildlink3.mk"
+.endif	# HS_STRICT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-strict
