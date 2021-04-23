5b0c80f267ae5111bed744faa20e0d2c7c0e8434
pho <pho@pkgsrc.org>
2021-04-23 12:27:42 +0000

    Update to mmorph-1.1.5
    1.1.5
    * Fix build failures on GHC 8.4 and 8.6
    * Add support for safe Haskell
      - Specifically, this marks the Control.Monad.Trans.Compose module as
        Trustworthy
      - The change in 1.1.4 to use GeneralizedNewtypeDeriving meant that
        the Control.Monad.Trans.Compose module was no longer inferred as
        safe
    * Restore Traversable instance removed by mistake in 1.1.4
    
    1.1.4 (Blacklisted)
    * Unintentional removal of Traversable instance for ComposeT
      - This missing instance is restored in 1.1.5
      - This is the reason why the 1.1.4 release is blacklisted
    
    * Fix MonadFail-related code to work for GHCJS
    * The MonadRWS instance for ComposeT has a more flexible constraint
      - The constraint is now MonadReader r (f (g m)), MonadWriter w (f (g
        m)), MonadState s (f (g m)) instead of MonadRWS r w s (f g m)
      - This loosening of the constraint is backwards-compatible

--- devel/hs-mmorph/buildlink3.mk
+++ devel/hs-mmorph/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/11 08:20:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/04/23 12:27:42 pho Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.3
-BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.3
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.5
+BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.5
 BUILDLINK_PKGSRCDIR.hs-mmorph?=		../../devel/hs-mmorph
 
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
