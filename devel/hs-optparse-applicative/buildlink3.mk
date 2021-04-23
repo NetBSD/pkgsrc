4daacc65a469c4e0a52fe9d4c2e18db78fd3346e
pho <pho@pkgsrc.org>
2021-04-23 16:41:34 +0000

    Update to optparse-applicative-0.16.1.0
    Version 0.16.1.0 (21 Nov 2020)
    * Guard process dependency behind an on by default flag. This allows
      one to shrink the dependency tree significantly by turning off the
      ability to use bash completion actions.
    * Remove bytestring dependency from the test suite.
    
    Version 0.16.0.0 (14 Aug 2020)
    * Add Options.Applicative.NonEmpty.some1 function, which parses
      options the same as some1 from base, but doesn't cause duplicates in
      the usage texts.
    * Further improve help text generation in the presence of optional
      values when nesting is involved, and many and some when displayed
      with a suffix.
    * Add "global" options to the usage texts for subcommands. When using
      subcommands, a "global options" section can now appear below the
      options and commands sections.
    
      Global options are off by default, to enable them, use the
      helpShowGlobals modifier.
    
      The noGlobal builder will suppress a single option being displayed
      in the global options list.
    * Allow the --help option to take the name of a command. Usage without
      any arguments is the same, but now, when an argument is given, if it
      is the name of a currently reachable command, the help text for that
      command will be show.
    * Updated dependency bounds.
    * Add builder for the all positional parser policy.
    * Remove deprecated functions
      - nullOption
      - execParserMaybe
      - customExecParserMaybe

--- devel/hs-optparse-applicative/buildlink3.mk
+++ devel/hs-optparse-applicative/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 15:40:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 16:41:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-optparse-applicative
 
 .if !defined(HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK)
 HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.15.1
-BUILDLINK_ABI_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.15.1.0
+BUILDLINK_API_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.16.1
+BUILDLINK_ABI_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.16.1.0
 BUILDLINK_PKGSRCDIR.hs-optparse-applicative?=	../../devel/hs-optparse-applicative
 
 .include "../../textproc/hs-ansi-wl-pprint/buildlink3.mk"
