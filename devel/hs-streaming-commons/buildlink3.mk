93f2815ab408b34544bd6654a9fb4aebeb408426
pho <pho@pkgsrc.org>
2021-04-23 10:06:52 +0000

    Update to streaming-commons-0.2.2.1
    0.2.2.1
    * Fix test suite compilation issue stackage#5528
    
    0.2.2.0
    * Remove AI_ADDRCONFIG #58

--- devel/hs-streaming-commons/buildlink3.mk
+++ devel/hs-streaming-commons/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/16 13:06:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 10:06:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-streaming-commons
 
 .if !defined(HS_STREAMING_COMMONS_BUILDLINK3_MK)
 HS_STREAMING_COMMONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.1.2
+BUILDLINK_API_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2.1
 BUILDLINK_PKGSRCDIR.hs-streaming-commons?=	../../devel/hs-streaming-commons
 
 .include "../../devel/hs-async/buildlink3.mk"
