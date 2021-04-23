4fda4407ab0b3a842d33866121cd40d6ecdfb5e7
pho <pho@pkgsrc.org>
2021-04-23 13:15:46 +0000

    Update to semigroupoids-5.3.5
    * The build-type has been changed from Custom to Simple. To achieve
      this, the doctests test suite has been removed in favor of using
      cabal-docspec to run the doctests.
    * Explicitly mark modules as Safe.

--- math/hs-semigroupoids/buildlink3.mk
+++ math/hs-semigroupoids/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 05:15:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 13:15:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.4
-BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.4
-BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../math/hs-semigroupoids
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.5
+BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.5
+BUILDLINK_PKGSRCDIR.hs-semigroupoids?=		../../math/hs-semigroupoids
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
 .include "../../math/hs-bifunctors/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../math/hs-contravariant/buildlink3.mk"
 .include "../../math/hs-distributive/buildlink3.mk"
 .include "../../math/hs-comonad/buildlink3.mk"
