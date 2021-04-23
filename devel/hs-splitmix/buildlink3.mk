52ded985825af487047e29b8db10b8a4c8017600
pho <pho@pkgsrc.org>
2021-04-23 06:50:03 +0000

    Update to splitmix-0.1.0.3
    0.1.0.3
    * Fix oops bugs in 0.1.0.2
      - It's lowercase windows.h. I blame Microsoft docs for using capital
        case Windows.h in the
        docs. https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocessid
      - accidental shiftL vs shiftR mixup for 32-bit generator
        initialization. Doesn't affect Linux.
    
    0.1.0.2
    * Drop time dependency in favour of handcoded initialization
      - On Unix platforms we use /dev/urandom if it exists, otherwise use
        gettimeofday, clock and getpid.
      - On Windows we use GetCurrentProcessID, GetCurrentThreadId(),
        GetTickCount, GetSystemTime and QueryPerformanceCounter.
      - On GHCJS use Math.random()
      - Using time is a fallback option (e.g. for Hugs).
    
    0.1.0.1
    * Add INLINEABLE pragmas to bitmaskWithRejection* functions
    * Support GHC-9.0
    
    0.1
    * Drop random dependency
      unconditionally. https://github.com/phadej/splitmix/issues/34
    
    0.0.5
    * Add nextInteger
    * Use smaller range in bitmaskWithRejection32 and 64, when upper bound
      is 2^n - 1. This changes generated values when they were on the
      boundary.
    
    0.0.4
    * Add bitmaskWithRejection32' and bitmaskWithRejection64' which
      generate numbers in closed range [0, n]. Unticked variants generate
      in closed-open range [0, n).

--- devel/hs-splitmix/buildlink3.mk
+++ devel/hs-splitmix/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/01 03:41:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 06:50:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-splitmix
 
 .if !defined(HS_SPLITMIX_BUILDLINK3_MK)
 HS_SPLITMIX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-splitmix+=	hs-splitmix>=0.0.3
-BUILDLINK_ABI_DEPENDS.hs-splitmix+=	hs-splitmix>=0.0.3
+BUILDLINK_API_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0.3
 BUILDLINK_PKGSRCDIR.hs-splitmix?=	../../devel/hs-splitmix
-
-.include "../../devel/hs-random/buildlink3.mk"
 .endif	# HS_SPLITMIX_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-splitmix
