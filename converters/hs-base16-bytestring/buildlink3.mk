9d9ebbaa5b7bee4d32b08ad3e1d10bc89b29b02c
pho <pho@pkgsrc.org>
2021-04-23 08:59:49 +0000

    Update to base16-bytestring-1.0.1.0
    1.0.1.0
    * Backwards-compatible support for bytestring ^>= 0.11 (#15)
    
    1.0.0.0
    * Merged omnibus PR doing a variety of things in (#10):
      - Improves performance by 3-4x for encode, 4-5x for decode.
      - The decode signature returning the tuple and actually returns an
        error message with offset. The signature will now be ByteString ->
        Either String ByteString.
      - Actually tests using the test vectors defined in the RFC, and uses
        property tests to ensure invariants hold.
      - Adds lenient decoders to the API
      - Adds -XTrustworthy annotations to the relevant exposed modules
      - Rewrites the haddocks to be more up to date and fancy-styled.
      - Adds benchmarks to the .cabal file so they can be run at toplevel,
        and make them better.
      - Bumps the Cabal version to 1.12
    
    Because of the breadth of this change, we are calling this a new epoch
    for the base16-bytestring library. Hence, the version 1.0.0.0.
    
    0.1.1.7
    - Fix some bugs in lazy decoding (#8).

--- converters/hs-base16-bytestring/buildlink3.mk
+++ converters/hs-base16-bytestring/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 07:13:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 08:59:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-base16-bytestring
 
 .if !defined(HS_BASE16_BYTESTRING_BUILDLINK3_MK)
 HS_BASE16_BYTESTRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=0.1.1.6
+BUILDLINK_API_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=1.0.1.0
 BUILDLINK_PKGSRCDIR.hs-base16-bytestring?=	../../converters/hs-base16-bytestring
 .endif	# HS_BASE16_BYTESTRING_BUILDLINK3_MK
 
