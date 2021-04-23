af21c6b692f482981f01b1105b657fb8b17289b5
pho <pho@pkgsrc.org>
2021-04-23 07:16:05 +0000

    Update to contravariant-1.5.3
    * Explicitly mark modules as Safe.

--- math/hs-contravariant/buildlink3.mk
+++ math/hs-contravariant/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/01 02:29:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 07:16:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-contravariant
 
 .if !defined(HS_CONTRAVARIANT_BUILDLINK3_MK)
 HS_CONTRAVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.2
-BUILDLINK_ABI_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.2
-BUILDLINK_PKGSRCDIR.hs-contravariant?=	../../math/hs-contravariant
+BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.3
+BUILDLINK_ABI_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.3
+BUILDLINK_PKGSRCDIR.hs-contravariant?=		../../math/hs-contravariant
 
+.include "../../devel/hs-StateVar/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../math/hs-semigroups/buildlink3.mk"
-.include "../../devel/hs-StateVar/buildlink3.mk"
 .endif	# HS_CONTRAVARIANT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-contravariant
