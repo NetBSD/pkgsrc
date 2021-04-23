8003da40a5171690de83025a80b6468a4f4d0f33
pho <pho@pkgsrc.org>
2021-04-23 16:09:09 +0000

    Update to parser-combinators-1.3.0
    * Changed the Control.Applicative.Permutations module to only require
      Applicative and not Monad. This module is the least restrictive and
      works with parsers which are not Monads.
    
    * Added the Control.Monad.Permutations module. This module may be
      substantially more efficient for some parsers which are Monads.
    
    * Corrected how permutation parsers intercalate effects and
      components; parsing an effect requires that a component immediately
      follows or else a parse error will result.

--- devel/hs-parser-combinators/buildlink3.mk
+++ devel/hs-parser-combinators/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 14:41:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 16:09:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-parser-combinators
 
 .if !defined(HS_PARSER_COMBINATORS_BUILDLINK3_MK)
 HS_PARSER_COMBINATORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parser-combinators+=	hs-parser-combinators>=1.2.1
-BUILDLINK_ABI_DEPENDS.hs-parser-combinators+=	hs-parser-combinators>=1.2.1
+BUILDLINK_API_DEPENDS.hs-parser-combinators+=	hs-parser-combinators>=1.3.0
+BUILDLINK_ABI_DEPENDS.hs-parser-combinators+=	hs-parser-combinators>=1.3.0
 BUILDLINK_PKGSRCDIR.hs-parser-combinators?=	../../devel/hs-parser-combinators
 .endif	# HS_PARSER_COMBINATORS_BUILDLINK3_MK
 
