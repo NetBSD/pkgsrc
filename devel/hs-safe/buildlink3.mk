cfd0cb3bc2fdb10106c05e38326b5dddd82f097c
pho <pho@pkgsrc.org>
2021-04-23 09:07:28 +0000

    Update to safe-0.3.19
    0.3.19, released 2020-05-24
    * #30, undeprecate maximumDef and friends, fold*1Def

--- devel/hs-safe/buildlink3.mk
+++ devel/hs-safe/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/02 07:19:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/04/23 09:07:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-safe
 
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.18
-BUILDLINK_ABI_DEPENDS.hs-safe+=	hs-safe>=0.3.18
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.19
+BUILDLINK_ABI_DEPENDS.hs-safe+=	hs-safe>=0.3.19
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../devel/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
