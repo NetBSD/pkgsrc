1d9366ea0f9547d6cb93956b19244b4fb1da90b3
pho <pho@pkgsrc.org>
2021-04-23 12:57:15 +0000

    Update to vault-0.3.1.5
    0.3.1.5
    * Compatibility with GHC-9.0
      - Bump dependencies to allow base >= 4.5 && < 4.16
    * Depend on semigroups only with GHC < 8.0
    
    0.3.1.4
    * Compatibility with GHC-8.10
      - Bump dependencies to allow base >= 4.5 && < 4.15

--- devel/hs-vault/buildlink3.mk
+++ devel/hs-vault/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/01/02 09:31:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/04/23 12:57:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-vault
 
@@ -6,11 +6,10 @@ BUILDLINK_TREE+=	hs-vault
 HS_VAULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.1.3
+BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.1.5
 BUILDLINK_PKGSRCDIR.hs-vault?=		../../devel/hs-vault
 
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_VAULT_BUILDLINK3_MK
 
