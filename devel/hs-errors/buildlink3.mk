2b67c7665e898d84b726d44c7c84d84776ab2a60
pho <pho@pkgsrc.org>
2021-04-23 09:10:06 +0000

    Update PLIST; drop dependency on hs-exceptions (which will be removed later)

--- devel/hs-errors/buildlink3.mk
+++ devel/hs-errors/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 07:19:38 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 09:10:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-errors
 
@@ -9,9 +9,8 @@ BUILDLINK_API_DEPENDS.hs-errors+=	hs-errors>=2.3.0
 BUILDLINK_ABI_DEPENDS.hs-errors+=	hs-errors>=2.3.0
 BUILDLINK_PKGSRCDIR.hs-errors?=		../../devel/hs-errors
 
-.include "../../devel/hs-exceptions/buildlink3.mk"
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-safe/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_ERRORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-errors
