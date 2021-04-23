dd94fc0748225d0bd5fb2fd90c088f2b9c8186ab
pho <pho@pkgsrc.org>
2021-04-23 08:44:05 +0000

    Update to aeson-1.5.6.0
    1.5.6.0
    * Make Show Value instance print object keys in lexicographic order.
    
    1.5.5.1
    * Fix a bug in FromJSON QuarterOfYear instance.
    
    1.5.5.0
    * Add instances for Month, Quarter and QuarterOfYear (from time-1.11),
      thanks to Oleg Grenrus.
    * The aeson repository has been moved to the haskell github
      organization!
    
    1.5.4.1
    * Use Text.Encoding.decodeLatin1 to speed up ASCII string decoding,
      thanks to Dmitry Ivanov.
    * Support bytestring 0.11.* and th-abstraction 0.4.*, thanks to Oleg
      Grenrus.
    
    1.5.4.0
    * Add instances for ToJSONKey and FromJSONKey to Const, thanks to Dan
      Fithian.
    * Add support for template-haskell 2.17, thanks to Galen Huntington.
    * Documentation typo fix, thanks to Jean-Baptiste Mazon.
    
    1.5.3.0
    * Add instances for types in strict and data-fix packages, thanks to
      Oleg Grenrus.
    * CPP cleanup, thanks to Oleg Grenrus.
    * Instances for dlist's Data.DList.DNonEmpty.DNonEmpty, thanks to Oleg
      Grenrus.
    
    1.5.2.0
    * Add Ord Value instance, thanks to Oleg Grenrus.
    * Export rejectUnknownFields from Data.Aeson
    
    1.5.1.0
    * Add instances for these, thanks to Oleg Grenrus.
    
    1.5.0.0
    * Fix bug in rejectUnknownFields not respecting fieldLabelModifier,
      thanks to Markus Schirp.
    * GFromJSON members are no longer exported from Data.Aeson(.Types), if
      you are using gParseJSON consider switching to gParseJSON', thanks
      to Oleg Grenrus.
    * Aeson no longer accepts unescaped control characters, thanks to Oleg
      Grenrus.
    * Remove CoerceText since GHC >=7.8 has Coercible, thanks to Oleg
      Grenrus.
    * Rename the GToJSON class to GToJSON' and expose it, thanks to Oleg
      Grenrus.
    
    1.4.7.1
    * GHC 8.10 compatibility, thanks to Ryan Scott.
    
    1.4.7.0
    
    Long overdue release (once again), so there's quite a bit of stuff
    included even though it's a "minor" release. Big thanks to all the
    contributors, the project would not exist without you!
    
    Special thanks to Oleg Grenrus and Xia Li-Yao for reviewing tons of
    stuff.
    
    New stuff:
    * Add rejectUnknownFields to Options which rejects unknown fields on
      deserialization. Useful to find errors during development, but
      enabling this should be considered a breaking change as previously
      accepted inputs may now be rejected. Thanks to rmanne.
    * FromJSON instance of Ratio a now parses numbers in addtion to
      standard {numerator=..., denumerator=...} encoding. Thanks to
      Aleksey Khudyakov.
    * Add more information to parse errors, including a sample of the
      surrounding text. Hopefully this will lead to less "Failed to read:
      satisfy" confusion! Thanks to Sasha Bogicevic. We expect some
      downstream test suites to break because of this, apologies in
      advance. Hopefully you will like the improvement anyway :-)
    * Add parseFail to Data.Aeson.Types. parseFail = fail but doesn't
      require users to know about MonadFail. Thanks to Colin Woodbury.
    * Make Template Haskell type family detection smarter when deriving
      ToJSON1 instances, thanks to Ryan Scott.
    * Optimize string parsing for the common case of strings without
      escapes, thanks to Yuras.
    
    Misc:
    * Clean up compiler warnings and switch from base-compat to
      base-compat-batteries. Thanks to Colin Woodbury & Oleg Grenrus.
    * Clarification & fixes to documentation regarding treatment of Maybe
      fields, thanks to Roman Cheplyaka.
    * Add documentation for internal development workflows. Thanks to Guru
      Devanla.
    * Drop support for GHC < 7.8. We've chosen to support older GHCs as
      long as it doesn't prevent us from adding new features, but now it
      does! Thanks to Oleg Grenrus for the patch.
    * Allow generic-deriving 1.13 in test suite.
    * Some DRY fixes thanks to Mark Fajkus.

--- converters/hs-aeson/buildlink3.mk
+++ converters/hs-aeson/buildlink3.mk
@@ -1,22 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.20 2020/01/11 09:59:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2021/04/23 08:44:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-aeson
 
 .if !defined(HS_AESON_BUILDLINK3_MK)
 HS_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=1.4.6
-BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=1.4.6.0
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=1.5.6
+BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=1.5.6.0
 BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../devel/hs-base-compat-batteries/buildlink3.mk"
+.include "../../devel/hs-data-fix/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-strict/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
+.include "../../devel/hs-these/buildlink3.mk"
 .include "../../time/hs-time-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-uuid-types/buildlink3.mk"
