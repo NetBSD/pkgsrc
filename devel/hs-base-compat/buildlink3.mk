c4d6e9d6d6af2bc190d3d94ac73a05277c6a4c2d
pho <pho@pkgsrc.org>
2021-04-23 05:06:44 +0000

    Update to base-compat-0.11.2
    Changes in 0.11.2 [2020.09.30]
    * Sync with base-4.15/GHC 9.0
    * Backport singleton to Data.List and Data.List.NonEmpty
    * Backport hGetContents', getContents', and readFile' added to System.IO
    
    Changes in 0.11.1 [2020.01.27]
    * Sync with base-4.14/GHC 8.10
    * Backport isResourceVanishedError, resourceVanishedErrorType, and
      isResourceVanishedErrorType to System.IO.Error.Compat.

--- devel/hs-base-compat/buildlink3.mk
+++ devel/hs-base-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/12/31 17:40:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 05:06:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat
 
 .if !defined(HS_BASE_COMPAT_BUILDLINK3_MK)
 HS_BASE_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.11.0
-BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.11.0
+BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.11.2
+BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.11.2
 BUILDLINK_PKGSRCDIR.hs-base-compat?=	../../devel/hs-base-compat
 .endif	# HS_BASE_COMPAT_BUILDLINK3_MK
 
