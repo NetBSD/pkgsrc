2547568ab95d45631c16e725cecf7f313ca27c32
pho <pho@pkgsrc.org>
2021-04-23 12:36:02 +0000

    Update to resourcet-1.2.4.2
    1.2.4.2
    * Mask exceptions in Acquire allocation action
    
    1.2.4.1
    * Document risk of using forkIO within a ResourceT #441
    
    1.2.4
    * Add allocate_ #437
    
    1.2.3
    * Support unliftio-core 0.2.0.0

--- devel/hs-resourcet/buildlink3.mk
+++ devel/hs-resourcet/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 08:26:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/23 12:36:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.2
-BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.2
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4.2
+BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4.2
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 
-.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-unliftio-core/buildlink3.mk"
 .endif	# HS_RESOURCET_BUILDLINK3_MK
