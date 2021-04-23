5947589144f69ef1c5ff990e32bebe1536bae1c2
pho <pho@pkgsrc.org>
2021-04-23 05:03:57 +0000

    Update to attoparsec-0.14.1
    0.14.1
    * Added Data.Attoparsec.ByteString.getChunk.
    
    0.14.0
    * Added Data.Attoparsec.ByteString.takeWhileIncluding.
    * Make Data.Attoparsec.{Text,ByteString}.Lazy.parseOnly accept lazy
      input.

--- textproc/hs-attoparsec/buildlink3.mk
+++ textproc/hs-attoparsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2020/01/11 09:34:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2021/04/23 05:03:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec
 
 .if !defined(HS_ATTOPARSEC_BUILDLINK3_MK)
 HS_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.13.2
-BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.13.2.3
+BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.14.1
+BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.14.1
 BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../textproc/hs-attoparsec
 
 .include "../../math/hs-scientific/buildlink3.mk"
