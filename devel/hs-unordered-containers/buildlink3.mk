78389ad893c715902ff112ecc057969bece8ab82
pho <pho@pkgsrc.org>
2021-04-23 06:28:04 +0000

    Update to unordered-containers-0.2.13.0
    0.2.13.0
    * Add HashMap.compose. Thanks Alexandre Esteves.
    
    0.2.12.0
    * Add HashMap.isSubmapOf[By] and HashSet.isSubsetOf. Thanks Sven
      Keidel. (#282)
    * Expose internal modules. (#283)
    * Documentation improvements in Data.HashSet, including a
      beginner-friendly introduction. Thanks Matt Renaud. (#267)
    * HashMap.alterF: Skip key deletion for absent keys. (#288)
    * Remove custom unsafeShift{L,R} definitions. (#281)
    * Various other documentation improvements.
    
    0.2.11.0
    * Add HashMap.findWithDefault (soft-deprecates
      HashMap.lookupDefault). Thanks, Matt Renaud.
    * Add HashMap.fromListWithKey. Thanks, Josef Svenningsson.
    * Add more folding functions and use them in Foldable
      instances. Thanks, David Feuer.
    * Add HashMap.!?, a flipped version of lookup. Thanks, Matt Renaud.
    * Add a Bifoldable instance for HashMap. Thanks, Joseph Sible.
    * Add a HasCallStack constraint to (!). Thanks, Roman Cheplyaka.
    
    Bug fixes
    * Fix a space leak affecting updates on keys with hash
      collisions. Thanks, Neil Mitchell. (#254)
    * Get rid of some silly thunks that could be left lying
      around. (#232). Thanks, David Feuer.
    
    Other changes
    * Speed up the Hashable instances for HashMap and HashSet. Thanks,
      Edward Amsden.
    * Remove a dependency cycle hack from the benchmark suite. Thanks,
      Andrew Martin.
    * Improve documentation. Thanks, Tristan McLeay, Li-yao Xia, Gareth
      Smith, Simon Jakobi, Sergey Vinokurov, and likely others.

--- devel/hs-unordered-containers/buildlink3.mk
+++ devel/hs-unordered-containers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2019/12/31 16:26:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/04/23 06:28:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.10
-BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.10.0
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.13
+BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.13.0
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../devel/hs-unordered-containers
 
 .include "../../devel/hs-hashable/buildlink3.mk"
