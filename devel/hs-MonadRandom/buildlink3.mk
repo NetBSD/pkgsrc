1e40435ff0d3fb506f491b4df308b09dee5d7e39
pho <pho@pkgsrc.org>
2021-04-23 10:41:51 +0000

    Update to MonadRandom-0.5.3
    0.5.3 (8 April 2021)
    * StatefulGen instances for RandT
    * Addition of RandGen
    * Additioon of withRandGen and withRandGen_
    
    0.5.2 (24 June 2020)
    * Support for random-1.2.
    * Drop support for GHC 7.4, test with GHC 8.10.
    
    0.5.1.2 (18 Jan 2020)
    * Better error message when total probability sum is negative in
      fromListMay, weightedMay, weighted

--- devel/hs-MonadRandom/buildlink3.mk
+++ devel/hs-MonadRandom/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 08:05:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/23 10:41:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-MonadRandom
 
 .if !defined(HS_MONADRANDOM_BUILDLINK3_MK)
 HS_MONADRANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.1
-BUILDLINK_ABI_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.1.1
+BUILDLINK_API_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.3
+BUILDLINK_ABI_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.3
 BUILDLINK_PKGSRCDIR.hs-MonadRandom?=	../../devel/hs-MonadRandom
 
 .include "../../devel/hs-primitive/buildlink3.mk"
