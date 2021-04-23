6b717a543d69ac970782f7bc427ef4626ff092be
pho <pho@pkgsrc.org>
2021-04-23 10:18:36 +0000

    Update to asn1-types-0.3.4
    * Add support for GHC 8.8.1.
    * Fix UTF8 encoding more multi byte encoding (continuation byte)

--- devel/hs-asn1-types/buildlink3.mk
+++ devel/hs-asn1-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/16 23:53:58 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 10:18:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-asn1-types
 
 .if !defined(HS_ASN1_TYPES_BUILDLINK3_MK)
 HS_ASN1_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-asn1-types+=	hs-asn1-types>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-asn1-types+=	hs-asn1-types>=0.3.3
+BUILDLINK_API_DEPENDS.hs-asn1-types+=	hs-asn1-types>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-asn1-types+=	hs-asn1-types>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-asn1-types?=	../../devel/hs-asn1-types
 
 .include "../../time/hs-hourglass/buildlink3.mk"
