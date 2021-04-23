2bef9c052927b18481508b5e4329cece18efa7c9
pho <pho@pkgsrc.org>
2021-04-23 09:49:45 +0000

    Update PLIST; drop dependency on hs-exceptions (which will be removed later)

--- sysutils/hs-temporary/buildlink3.mk
+++ sysutils/hs-temporary/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/01 05:24:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/04/23 09:49:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-temporary
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.hs-temporary+=	hs-temporary>=1.3
 BUILDLINK_ABI_DEPENDS.hs-temporary+=	hs-temporary>=1.3
 BUILDLINK_PKGSRCDIR.hs-temporary?=	../../sysutils/hs-temporary
 
-.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
 .endif	# HS_TEMPORARY_BUILDLINK3_MK
 
