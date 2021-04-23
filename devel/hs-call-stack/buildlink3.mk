219f31aa02f2ba0d2271a4425e17fc765941ca08
pho <pho@pkgsrc.org>
2021-04-23 17:57:45 +0000

    Update to call-stack-0.3.0
    No change logs are provided by the upstream.

--- devel/hs-call-stack/buildlink3.mk
+++ devel/hs-call-stack/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:35:08 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 17:57:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-call-stack
 
 .if !defined(HS_CALL_STACK_BUILDLINK3_MK)
 HS_CALL_STACK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-call-stack+=	hs-call-stack>=0.2.0
-BUILDLINK_ABI_DEPENDS.hs-call-stack+=	hs-call-stack>=0.2.0
+BUILDLINK_API_DEPENDS.hs-call-stack+=	hs-call-stack>=0.3.0
+BUILDLINK_ABI_DEPENDS.hs-call-stack+=	hs-call-stack>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-call-stack?=	../../devel/hs-call-stack
 .endif	# HS_CALL_STACK_BUILDLINK3_MK
 
