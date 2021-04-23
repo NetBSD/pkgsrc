ff40f2c18e702879eb70913fcd93aa8e5a8a41f9
pho <pho@pkgsrc.org>
2021-04-23 08:00:24 +0000

    Update to bifunctors-5.5.10
    5.5.10 [2021.01.21]
    * Fix a bug in which deriveBifoldable could generate code that
      triggers -Wunused-matches warnings.
    
    5.5.9 [2020.12.30]
    * Explicitly mark modules as Safe or Trustworthy.
    
    5.5.8 [2020.10.01]
    * Fix a bug in which deriveBifunctor would fail on sufficiently
      complex uses of rank-n types in constructor fields.
    * Fix a bug in which deriveBiunctor and related functions would
      needlessly reject data types whose two last type parameters appear
      as oversaturated arguments to a type family.
    
    5.5.7 [2020.01.29]
    * Add Data.Bifunctor.Biap.

--- math/hs-bifunctors/buildlink3.mk
+++ math/hs-bifunctors/buildlink3.mk
@@ -1,20 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 04:45:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 08:00:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-bifunctors
 
 .if !defined(HS_BIFUNCTORS_BUILDLINK3_MK)
 HS_BIFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.6
-BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.6
+BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.10
+BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.10
 BUILDLINK_PKGSRCDIR.hs-bifunctors?=	../../math/hs-bifunctors
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
 .include "../../math/hs-comonad/buildlink3.mk"
-.include "../../devel/hs-th-abstraction/buildlink3.mk"
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-th-abstraction/buildlink3.mk"
 .endif	# HS_BIFUNCTORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-bifunctors
