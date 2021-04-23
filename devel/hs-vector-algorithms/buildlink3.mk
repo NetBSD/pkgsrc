ba3feb71fd57038d094d6dd8af5d75a466e59c49
pho <pho@pkgsrc.org>
2021-04-23 13:02:09 +0000

    Update to vector-algorithms-0.8.0.4
    * Fix out of range access in Intro.partialSort.
    * Update QuickCheck dependency bounds.

--- devel/hs-vector-algorithms/buildlink3.mk
+++ devel/hs-vector-algorithms/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/02 10:28:24 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/04/23 13:02:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-algorithms
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-vector-algorithms
 HS_VECTOR_ALGORITHMS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0
-BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0.3
+BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0.4
 BUILDLINK_PKGSRCDIR.hs-vector-algorithms?=	../../devel/hs-vector-algorithms
 
 .include "../../devel/hs-primitive/buildlink3.mk"
