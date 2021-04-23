879fd502fe52035f6cb0307433e2d2e0423c5efc
pho <pho@pkgsrc.org>
2021-04-23 10:46:52 +0000

    Update to QuickCheck-2.14.2
    QuickCheck 2.14.2 (released 2020-11-14)
    * Add Arbitrary instances for Tree (thanks to Oleg Grenrus)
    * GHC 9.0 compatibility (thanks to Vilem-Benjamin Liepelt)
    
    QuickCheck 2.14.1 (released 2020-07-05)
    * Compatibility with random >= 1.2.
    
    QuickCheck 2.14 (released 2020-03-28)
    * QuickCheck is now much faster at generating test data!
    
      As a result, many properties can now be tested a lot faster; the
      examples distributed with QuickCheck run about twice as fast as
      before, for example. Of course, your mileage may vary.
    
      The reason for this is that there are now specialised versions of
      the 'choose' combinator for various types. These are: chooseInt,
      chooseInteger, chooseBoundedIntegral, and chooseEnum.  These
      combinators are identical to 'choose' but much faster.  All
      QuickCheck combinators, with the exception of 'choose' itself, use
      the new combinators behind the scenes.
    
      You should see a speedup without doing anything, but to get a
      further speedup, consider replacing any uses of 'choose' in your own
      generators with the new combinators.
    
      We are hoping that future releases of the 'random' library may speed
      up 'choose', in which case these combinators may no longer be
      needed.
    
      Thanks to Oleg Grenrus for suggesting to bypass 'choose' for random
      number generation, and providing the appropriate primitive in his
      'splitmix' random number library.
    
    * Smaller changes and bugfixes:
      - RecursivelyShrink and GSubterms are exported from
        Test.QuickCheck.Test (thanks to Tom Mortiboy).
      - Don't generate invalid unicode characters (thanks to Boris
        Stepanov).
      - When a call to 'within' fails, include the duration of the timeout
        in the failure report (thanks to William Rusnack).
      - In Gen, avoid splitting the seed in the implementation of >>, *>
        and <* (thanks to David Feuer).
      - Fix a couple of bugs with shrinking of floating-point numbers.
      - Export functionMapWith, functionEitherWith and functionPairWith
        from Test.QuickCheck.Function (thanks to Oleg Grenrus).
      - Remove redundant RealFloat constraint from Arbitrary/CoArbitrary
        instances for Complex (thanks to Bodigrim).

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/01 03:44:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/23 10:46:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.13.2
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.13.2
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.2
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.2
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
