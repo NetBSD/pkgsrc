9d371866d3ddea99dab23d25c9164d829ad1a96b
pho <pho@pkgsrc.org>
2021-04-23 07:13:57 +0000

    Update to StateVar-1.2.1
    * Explicitly mark Data.StateVar as Safe (or Trustworthy for GHC before
      7.10).

--- devel/hs-StateVar/buildlink3.mk
+++ devel/hs-StateVar/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 10:41:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/23 07:13:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-StateVar
 
 .if !defined(HS_STATEVAR_BUILDLINK3_MK)
 HS_STATEVAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2
-BUILDLINK_ABI_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2
+BUILDLINK_API_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2.1
 BUILDLINK_PKGSRCDIR.hs-StateVar?=	../../devel/hs-StateVar
 .endif	# HS_STATEVAR_BUILDLINK3_MK
 
