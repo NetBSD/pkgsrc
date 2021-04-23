28cb9d632986a2941ed7e7dddf6113befa96e385
pho <pho@pkgsrc.org>
2021-04-23 07:33:09 +0000

    Update to distributive-0.6.2.1
    0.6.2.1 [2020.12.30]
    * The build-type has been changed from Custom to Simple. To achieve
      this, the doctests test suite has been removed in favor of using
      cabal-docspec to run the doctests.
    
    0.6.2 [2020.04.10]
    * Make the Distributive instance for Tagged poly-kinded.

--- math/hs-distributive/buildlink3.mk
+++ math/hs-distributive/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 05:21:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/04/23 07:33:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-distributive
 
 .if !defined(HS_DISTRIBUTIVE_BUILDLINK3_MK)
 HS_DISTRIBUTIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-distributive+=	hs-distributive>=0.6.1
+BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.6.2
+BUILDLINK_ABI_DEPENDS.hs-distributive+=	hs-distributive>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-distributive?=	../../math/hs-distributive
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
-.include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
 .endif	# HS_DISTRIBUTIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-distributive
