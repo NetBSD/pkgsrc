4cbb0e059a71a10a7e29bcded88071983bb2b91f
pho <pho@pkgsrc.org>
2021-04-23 09:12:12 +0000

    Update to text-conversions-0.3.1
    * Added support for base16-bytestring-1.0.

--- converters/hs-text-conversions/buildlink3.mk
+++ converters/hs-text-conversions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 11:29:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 09:12:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-conversions
 
 .if !defined(HS_TEXT_CONVERSIONS_BUILDLINK3_MK)
 HS_TEXT_CONVERSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-conversions+=	hs-text-conversions>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-text-conversions+=	hs-text-conversions>=0.3.0
+BUILDLINK_API_DEPENDS.hs-text-conversions+=	hs-text-conversions>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-text-conversions+=	hs-text-conversions>=0.3.1
 BUILDLINK_PKGSRCDIR.hs-text-conversions?=	../../converters/hs-text-conversions
 
 .include "../../converters/hs-base16-bytestring/buildlink3.mk"
