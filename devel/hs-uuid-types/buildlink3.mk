d2ca5261a9e1a10de4a94ef0f84586eedc4fe9f8
pho <pho@pkgsrc.org>
2021-04-23 06:54:13 +0000

    Update to uuid-types-1.0.4
    * Declare Data.UUID.Types module "Trustworthy" with respect to
      SafeHaskell.
    * Use more compact heap object representation which saves 16 bytes on
      64bit platforms.
    * Add toWords64/fromWords64 functions
    * Drop support for GHC < 7.
    * Add support for random-1.2, i.e. idiomatic Random and added Uniform
      instances.

--- devel/hs-uuid-types/buildlink3.mk
+++ devel/hs-uuid-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/11 09:41:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/04/23 06:54:13 pho Exp $
 
 BUILDLINK_TREE+=	hs-uuid-types
 
 .if !defined(HS_UUID_TYPES_BUILDLINK3_MK)
 HS_UUID_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.3
+BUILDLINK_API_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.4
 BUILDLINK_PKGSRCDIR.hs-uuid-types?=	../../devel/hs-uuid-types
 
 .include "../../devel/hs-hashable/buildlink3.mk"
