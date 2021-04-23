cb99f3751dc134c4dd5663a2f5d258b79d9321dd
pho <pho@pkgsrc.org>
2021-04-23 04:58:10 +0000

    Update to primitive-0.7.1.0
    Changes in version 0.7.1.0
    * Introduce convenience class MonadPrim and MonadPrimBase.
    * Add PrimMonad and PrimBase instances for Lazy.ST (GHC >=
      8.2). thanks to Avi Dessauer (@Avi-D-coder) for this first
      contribution
    * Add freezeByteArray and freezePrimArray.
    * Add compareByteArrays.
    * Add shrinkMutableByteArray.
    * Add Eq instances for MutableByteArray and MutablePrimArray. by
      Andrew Martin
    * Add functions for manipulating pinned Prim Arrays by Andrew Martin
    * Add copyPtrToMutableByteArray.
    * Add NFData instances for ByteArray, MutableByteArray, PrimArray and
      MutablePrimArray. by Callan McGill
    * Add shrinkSmallMutableArray.
    * Add clonePrimArray and cloneMutablePrimArray.
    * Add cloneMutableByteArray and cloneByteArray.
    * Add Prim instances for WordPtr and IntPtr.
    * Add NFData instances for Array and SmallArray. by Callan McGill
    * Add copyByteArrayToPtr and copyMutableByteArrayToPtr.
    * Export arrayFromList and arrayFromListN.
    
    Changes in version 0.7.0.1
    * Allow building with GHC 8.12. Thanks Ryan GL Scott for this and
      every compat patch over time.

--- devel/hs-primitive/buildlink3.mk
+++ devel/hs-primitive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/11 08:00:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/04/23 04:58:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.7.0.0
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.7.1.0
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../devel/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
