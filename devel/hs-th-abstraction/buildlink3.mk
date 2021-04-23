f89385a3412580f98805439eee2fe5b048a2c4f9
pho <pho@pkgsrc.org>
2021-04-23 06:13:07 +0000

    Update to th-abstraction-0.4.2.0
    0.4.2.0 -- 2020-12-30
    * Explicitly mark modules as Safe (or Trustworthy for GHC versions
      prior to 8.4).
    
    0.4.1.0 -- 2020-12-09
    * Fix a bug in which normalizeDec would give incorrect kind
      annotations to type variables in quoted Decs. normalizeDec now
      leaves the kinds of type variable binders alone.
    
    0.4.0.0 -- 2020-09-29
    * Adapt to the TyVarBndr data type gaining a new flag type parameter
      (in template-haskell-2.17.0.0) to represent its specificity:
    * Introduce a new Language.Haskell.TH.Datatype.TyVarBndr module that
      defines TyVarBndr_, a backwards-compatible type synonym for
      TyVarBndr, as well as backporting TyVarBndrSpec, TyVarBndrUnit, and
      Specificity. This module also defines other useful functions for
      constructing and manipulating TyVarBndrs.
    * The types in Language.Haskell.TH.Datatype now use TyVarBndr_,
      TyVarBndrUnit, and TyVarBndrSpec where appropriate. Technically,
      this is not a breaking change, since all three are simple type
      synonyms around TyVarBndr, but it is likely that you will need to
      update your th-abstraction-using code anyway if it involves a
      TyVarBndr-consuming function.
    
    0.3.2.0 -- 2020-02-06
    * Support substituting into and extracting free variables from
      ForallVisTs on template-haskell-2.16.0.0 (GHC 8.10) or later.
    * Fix a bug in which freeVariables could report duplicate kind
      variables when they occur in the kinds of the type variable binders
      in a ForallT.
    * Fix a bug in which resolveInfixT would not resolve UInfixTs
      occurring in the kinds of type variable binders in a ForallT.
    * Fix a bug in which the TypeSubstitution ConstructorInfo instance
      would not detect free kind variables in the constructorVars.

--- devel/hs-th-abstraction/buildlink3.mk
+++ devel/hs-th-abstraction/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/12/31 15:34:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 06:13:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-abstraction
 
 .if !defined(HS_TH_ABSTRACTION_BUILDLINK3_MK)
 HS_TH_ABSTRACTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.3.1.0
-BUILDLINK_PKGSRCDIR.hs-th-abstraction?=	../../devel/hs-th-abstraction
+BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.2.0
+BUILDLINK_PKGSRCDIR.hs-th-abstraction?=		../../devel/hs-th-abstraction
 .endif	# HS_TH_ABSTRACTION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-th-abstraction
