4cc53c1deb940a773faa3305095422a627c55971
pho <pho@pkgsrc.org>
2021-04-23 12:47:27 +0000

    Update to syb-0.7.2.1
    * 0.7.2: Add compatibility with GHC 9, switch to tasty for tests, fix
      tests on GHCJS

--- devel/hs-syb/buildlink3.mk
+++ devel/hs-syb/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 03:13:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/04/23 12:47:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-syb
 
 .if !defined(HS_SYB_BUILDLINK3_MK)
 HS_SYB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-syb+=	hs-syb>=0.7.1
+BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.7.2.1
+BUILDLINK_ABI_DEPENDS.hs-syb+=	hs-syb>=0.7.2.1
 BUILDLINK_PKGSRCDIR.hs-syb?=	../../devel/hs-syb
 .endif	# HS_SYB_BUILDLINK3_MK
 
