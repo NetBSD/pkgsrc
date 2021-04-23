b711023f37d7ae5e34e3e59e223c3d3147d75635
pho <pho@pkgsrc.org>
2021-04-23 12:16:15 +0000

    Update to haskell-src-exts-1.23.1
    1.22.0 --> 1.23.0
    =================
    * Add support for BlockArguments (#444)
      - Technically breaking change: KnownExtension gained a new constructor BlockArguments
    * Add support for Arrow Brackets (#445)
      - Tecnically breaking change: Exp gained new constructor ArrOp

--- devel/hs-haskell-src-exts/buildlink3.mk
+++ devel/hs-haskell-src-exts/buildlink3.mk
@@ -1,16 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/02 04:52:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/04/23 12:16:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-haskell-src-exts
 
 .if !defined(HS_HASKELL_SRC_EXTS_BUILDLINK3_MK)
 HS_HASKELL_SRC_EXTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.22.0
-BUILDLINK_ABI_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.22.0
+BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.23.1
+BUILDLINK_ABI_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.23.1
 BUILDLINK_PKGSRCDIR.hs-haskell-src-exts?=	../../devel/hs-haskell-src-exts
-
-.include "../../devel/hs-fail/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_HASKELL_SRC_EXTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-haskell-src-exts
