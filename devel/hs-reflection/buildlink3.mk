acaeffc9c2ae529ff488341fccd7069fe30f3389
pho <pho@pkgsrc.org>
2021-04-23 12:30:17 +0000

    Update to reflection-2.1.6
    Fix a bug in which give (and possibly reify, reifyNat, and
    reifySymbol) could be unsoundly inlined by GHC 8.10 or older to
    produce incorrect runtime results.

--- devel/hs-reflection/buildlink3.mk
+++ devel/hs-reflection/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/02 07:16:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/23 12:30:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-reflection
 
 .if !defined(HS_REFLECTION_BUILDLINK3_MK)
 HS_REFLECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.5
-BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.5
+BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.6
+BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.6
 BUILDLINK_PKGSRCDIR.hs-reflection?=	../../devel/hs-reflection
-
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_REFLECTION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-reflection
