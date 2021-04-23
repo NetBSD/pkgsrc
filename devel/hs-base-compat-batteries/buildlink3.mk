00f244285a14dc9d837ed7251d41053898b9e6e0
pho <pho@pkgsrc.org>
2021-04-23 08:33:28 +0000

    Update to base-compat-batteries-0.11.2
    Changes in 0.11.2 [2020.09.30]
    * This coincides with the base-compat-0.11.2 release. Refer to the
      base-compat changelog for more details.
    
    Changes in 0.11.1 [2020.01.27]
    * This coincides with the base-compat-0.11.1 release. Refer to the
      base-compat changelog for more details.

--- devel/hs-base-compat-batteries/buildlink3.mk
+++ devel/hs-base-compat-batteries/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:33:17 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 08:33:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat-batteries
 
 .if !defined(HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK)
 HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.11.0
-BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.11.0
+BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.11.2
+BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.11.2
 BUILDLINK_PKGSRCDIR.hs-base-compat-batteries?=		../../devel/hs-base-compat-batteries
+
+.include "../../devel/hs-base-compat/buildlink3.mk"
 .endif	# HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-base-compat-batteries
