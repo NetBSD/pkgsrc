64cd86f70cafab68125278a5c1eabf1fbef52454
pho <pho@pkgsrc.org>
2021-04-23 12:34:06 +0000

    Update to unliftio-core-0.2.0.1
    0.2.0.1
    * Remove faulty default implementation of withRunInIO #56
    
    0.2.0.0
    * Move askUnliftIO out of class #55

--- devel/hs-unliftio-core/buildlink3.mk
+++ devel/hs-unliftio-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/08 18:57:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/23 12:34:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-unliftio-core
 
 .if !defined(HS_UNLIFTIO_CORE_BUILDLINK3_MK)
 HS_UNLIFTIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.1.2.0
+BUILDLINK_API_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.2.0.1
+BUILDLINK_ABI_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.2.0.1
 BUILDLINK_PKGSRCDIR.hs-unliftio-core?=		../../devel/hs-unliftio-core
 .endif	# HS_UNLIFTIO_CORE_BUILDLINK3_MK
 
