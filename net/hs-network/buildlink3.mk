732988f320e9e4b7676b9a66a60e2aeb01dc3f8e
pho <pho@pkgsrc.org>
2021-04-23 10:03:01 +0000

    Update to network-3.1.2.1
    Version 3.1.2.1
    * Increasing base lower bound to 4.9. #473
    * Suppressing errors from removeFile in UNIX bind. #478
    * Restoring UNIX stub functions on Windows. #489
    
    Version 3.1.2.0
    * Added -f devel for test cases that are known to fail. #471
    * Improved precedence-compliant Read/Show instances. Verified via
      QuickCheck. #465 #466
    * Removed the racing graceful close implementation to avoid issues
      with CLOSE_WAIT. #460
    * Gracefully handle binding of UNIX domain sockets. #460
    * Replace Socket type and family with extensible CInt pattern and
      synonyms. #459
    * Fixed race conditions in tests. #458
    * Removed many legacy uses of undefined. #456
    * Defined extensible CustomSockOpt via ViewPatterns. #455
    * Defined openSocket in terms of
      AddrInfo. 5b0987197fe2ed7beddd7b2096522d624e71151e
    * Improved FreeBSD portability for Control Messages and tests #452
    * Support sendMsg and recvMsg #433 #445 #451
      - Added sendMsg and recvMsg APIs
      - Redefined SocketOption as pattern synonym
    * Implement total Show functions for SockAddr #441
    * Improve portability changing u_int32_t to uint32_t. #442
    * Removed obsolete CPP
      statements. d1f4ee60ce6a4a85abb79532f64d4a4e71e2b1ce
    * Loads of improved test
      coverage. cbd67cc50a37770432eb978ac8b8eb6da3664817
      fcc2d86d53a6bec793f6a979a9e8fdf7fe3f4c22
      6db96969b3e8974abbfd50a7f073baa57376fd5e

--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/01/01 03:10:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/04/23 10:03:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-network
 
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.1.1
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.1.1.1
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.1.2
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.1.2.1
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 .endif	# HS_NETWORK_BUILDLINK3_MK
 
