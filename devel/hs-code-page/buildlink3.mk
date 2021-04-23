11de4a98007271b110e2920d690a486225ee17d9
pho <pho@pkgsrc.org>
2021-04-23 15:44:40 +0000

    Update to code-page-0.2.1
    0.2.1 [2021.02.06]
    * Allow building with base-4.15 (GHC 9.0) or later on Windows.

--- devel/hs-code-page/buildlink3.mk
+++ devel/hs-code-page/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 12:37:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 15:44:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-code-page
 
 .if !defined(HS_CODE_PAGE_BUILDLINK3_MK)
 HS_CODE_PAGE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-code-page+=	hs-code-page>=0.2
+BUILDLINK_API_DEPENDS.hs-code-page+=	hs-code-page>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-code-page+=	hs-code-page>=0.2.1
 BUILDLINK_PKGSRCDIR.hs-code-page?=	../../devel/hs-code-page
 .endif	# HS_CODE_PAGE_BUILDLINK3_MK
 
