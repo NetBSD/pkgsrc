2da982859fab2309afc1f1eacd1cc4e6c42ac1a0
pho <pho@pkgsrc.org>
2021-04-23 06:06:12 +0000

    Update to dlist-1.0
    v1.0
    Released on 2020-07-18, Nelson Mandela International Day.
    
    Added
    * intercalate for DList (#43, Jacob Leach)
    * Traversable instance for DList (#45, Veronika Romashkina)
    * Data.DList.Internal for the DList implementation, Data.DList.Unsafe
      for exporting the DList constructor UnsafeDList and record label
      unsafeApplyDList (#55, #59)
    * Data.DList.DNonEmpty (#60)
    * GitHub Action for uploading a release (#74)
    * dlist-bench, a benchmark package (#71)
    
    Changed
    * stimes for DList defined with stimesMonoid (#46, Janek Spaderna)
    * Type of tail: DList a -> DList a to DList a -> [a] (#69)
    * GitHub Action for continuous integration testing to replace Travis-CI (#47, #50)
    * GHC warning and error improvements (#72, #73)
    * Improved documentation (#55, #70, #76, #77)
    
    Removed
    * list :: b -> (a -> DList a -> b) -> DList a -> b (#69)
    
    
    v0.8.0.8
    Released on 2020-04-02, World Autism Awareness Day.
    
    Added
    * toList in the Foldable instance for DList (#36, Ryan Scott)
    
    Changed
    * QuickCheck upper bound: 2.14 to 2.15 (a7ea60d)
    
    Fixed
    * Documented time complexity of head for DList (#35, Simon Jakobi)

--- devel/hs-dlist/buildlink3.mk
+++ devel/hs-dlist/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2019/12/31 07:33:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2021/04/23 06:06:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-dlist
 
 .if !defined(HS_DLIST_BUILDLINK3_MK)
 HS_DLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.8.0
-BUILDLINK_ABI_DEPENDS.hs-dlist+=	hs-dlist>=0.8.0.7
+BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=1.0
+BUILDLINK_ABI_DEPENDS.hs-dlist+=	hs-dlist>=1.0
 BUILDLINK_PKGSRCDIR.hs-dlist?=		../../devel/hs-dlist
 .endif	# HS_DLIST_BUILDLINK3_MK
 
