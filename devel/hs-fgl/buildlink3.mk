aca328940181d1ff586406a96fad0a363a00ec72
pho <pho@pkgsrc.org>
2021-04-23 17:54:46 +0000

    Update to fgl-5.7.0.3
    * Bump QuickCheck dependency

--- devel/hs-fgl/buildlink3.mk
+++ devel/hs-fgl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:36:48 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 17:54:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-fgl
 
 .if !defined(HS_FGL_BUILDLINK3_MK)
 HS_FGL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.7.0.2
-BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.7.0.2
+BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.7.0
+BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.7.0.3
 BUILDLINK_PKGSRCDIR.hs-fgl?=	../../devel/hs-fgl
 .endif	# HS_FGL_BUILDLINK3_MK
 
