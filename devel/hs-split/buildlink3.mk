f1270cd163d46f41b03ec18dbd028a897948a6b0
pho <pho@pkgsrc.org>
2021-04-23 12:44:47 +0000

    Update to split-0.2.3.4
    * Allow base-4.14 for GHC-8.10

--- devel/hs-split/buildlink3.mk
+++ devel/hs-split/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 09:11:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/04/23 12:44:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.3
-BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.3.3
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.3.4
+BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.3.4
 BUILDLINK_PKGSRCDIR.hs-split?=		../../devel/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
