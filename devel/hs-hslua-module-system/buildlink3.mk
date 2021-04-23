785df21fbfdfb3c5470abd8c9679d10d3f5e55af
pho <pho@pkgsrc.org>
2021-04-23 09:51:49 +0000

    Update to hslua-module-system-0.2.2.1
    0.2.2.1 -- 2020-10-16
    * Relaxed upper bound for hslua, allow hslua-1.3.*.
    
    0.2.2 -- 2020-08-15
    * Relaxed upper bound for hslua, allow hslua-1.2.*.
    * Improved documentation of internal types.
    * Use tasty-lua for unit tests.
    * Update CI to test with all GHC versions.

--- devel/hs-hslua-module-system/buildlink3.mk
+++ devel/hs-hslua-module-system/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 13:53:58 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 09:51:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-system
 
 .if !defined(HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK)
 HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=0.2.1
+BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=0.2.2.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-system?=	../../devel/hs-hslua-module-system
 
-.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../lang/hs-hslua/buildlink3.mk"
 .include "../../sysutils/hs-temporary/buildlink3.mk"
 .endif	# HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK
