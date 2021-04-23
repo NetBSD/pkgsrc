06e509fbb373841349304dc2af1b402904768d05
pho <pho@pkgsrc.org>
2021-04-23 10:31:55 +0000

    Update to pretty-show-1.10
    * Add support for quasi-quotes, as the preferred way of dealing with
      non-properly showable things.

--- devel/hs-pretty-show/buildlink3.mk
+++ devel/hs-pretty-show/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 17:24:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 10:31:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-pretty-show
 
 .if !defined(HS_PRETTY_SHOW_BUILDLINK3_MK)
 HS_PRETTY_SHOW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pretty-show+=	hs-pretty-show>=1.9.5
-BUILDLINK_ABI_DEPENDS.hs-pretty-show+=	hs-pretty-show>=1.9.5
+BUILDLINK_API_DEPENDS.hs-pretty-show+=	hs-pretty-show>=1.10
+BUILDLINK_ABI_DEPENDS.hs-pretty-show+=	hs-pretty-show>=1.10
 BUILDLINK_PKGSRCDIR.hs-pretty-show?=	../../devel/hs-pretty-show
 
 .include "../../devel/hs-haskell-lexer/buildlink3.mk"
