7c083b94d91f68394b616427bf9a007b22d16d36
pho <pho@pkgsrc.org>
2021-04-23 13:11:36 +0000

    Update to profunctors-5.6.2
    5.6.2 [2021.02.17]
    * Add Semigroup and Monoid instances for Forget
    
    5.6.1 [2020.12.31]
    * Add Functor instances for PastroSum, CopastroSum, Environment,
      FreeMapping, Pastro, Copastro, FreeTraversing, and Coyoneda.
    * Explicitly mark modules as Safe.
    
    5.6 [2020.10.01]
    * Enable the PolyKinds extension. The following datatypes now have
      polymorphic kinds: (:->), Cayley, Procompose, Rift,
      ProfunctorFunctor, Ran, Codensity, Prep, Coprep, Star, Costar,
      WrappedArrow, Forget.
    * Allow building with GHC 9.0.
    
    5.5.2 [2020.02.13]
    * Add Cochoice, Costrong, Closed, Traversing, and Mapping instances
      for Cayley.
    * Add Mapping and Traversing instances for Tannen.

--- math/hs-profunctors/buildlink3.mk
+++ math/hs-profunctors/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 05:03:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 13:11:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-profunctors
 
 .if !defined(HS_PROFUNCTORS_BUILDLINK3_MK)
 HS_PROFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=5.5.1
-BUILDLINK_ABI_DEPENDS.hs-profunctors+=	hs-profunctors>=5.5.1
+BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=5.6.2
+BUILDLINK_ABI_DEPENDS.hs-profunctors+=	hs-profunctors>=5.6.2
 BUILDLINK_PKGSRCDIR.hs-profunctors?=	../../math/hs-profunctors
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
@@ -15,7 +15,6 @@ BUILDLINK_PKGSRCDIR.hs-profunctors?=	../../math/hs-profunctors
 .include "../../math/hs-contravariant/buildlink3.mk"
 .include "../../math/hs-distributive/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_PROFUNCTORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-profunctors
