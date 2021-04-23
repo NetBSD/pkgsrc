1e4d1767bfa6be5003276d76f2f5337167d27e70
pho <pho@pkgsrc.org>
2021-04-23 12:54:09 +0000

    Update to utf8-string-1.0.2
    1.0.2
    * Add fromChar
    * Add IsString UTF8 instance
    * Fixup documentation and tests

--- devel/hs-utf8-string/buildlink3.mk
+++ devel/hs-utf8-string/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/01/04 12:22:20 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/04/23 12:54:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-utf8-string
 
 .if !defined(HS_UTF8_STRING_BUILDLINK3_MK)
 HS_UTF8_STRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-utf8-string+=	hs-utf8-string>=1.0.1.1
+BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-utf8-string+=	hs-utf8-string>=1.0.2
 BUILDLINK_PKGSRCDIR.hs-utf8-string?=	../../devel/hs-utf8-string
 .endif	# HS_UTF8_STRING_BUILDLINK3_MK
 
