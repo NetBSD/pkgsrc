1a3efddbf4a75dd6e49b3e91fb1fbb6aa7599575
pho <pho@pkgsrc.org>
2021-04-23 15:29:38 +0000

    Update PLIST

--- devel/hs-libffi/buildlink3.mk
+++ devel/hs-libffi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/03/08 16:42:25 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/23 15:29:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-libffi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-libffi
 HS_LIBFFI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-libffi+=	hs-libffi>=0.1
-BUILDLINK_ABI_DEPENDS.hs-libffi?=		hs-libffi>=0.1nb1
+BUILDLINK_ABI_DEPENDS.hs-libffi?=	hs-libffi>=0.1nb1
 BUILDLINK_PKGSRCDIR.hs-libffi?=		../../devel/hs-libffi
 
 .include "../../devel/libffi/buildlink3.mk"
