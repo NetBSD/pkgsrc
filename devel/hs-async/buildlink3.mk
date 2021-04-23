53c0bf50ba6fa89de69c7b3cc8dd558e57d15aba
pho <pho@pkgsrc.org>
2021-04-23 09:55:49 +0000

    Update to async-2.2.3
    * Documentation fixes

--- devel/hs-async/buildlink3.mk
+++ devel/hs-async/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 10:43:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/23 09:55:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-async
 
 .if !defined(HS_ASYNC_BUILDLINK3_MK)
 HS_ASYNC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-async+=	hs-async>=2.2.2
-BUILDLINK_ABI_DEPENDS.hs-async+=	hs-async>=2.2.2
-BUILDLINK_PKGSRCDIR.hs-async?=	../../devel/hs-async
+BUILDLINK_API_DEPENDS.hs-async+=	hs-async>=2.2.3
+BUILDLINK_ABI_DEPENDS.hs-async+=	hs-async>=2.2.3
+BUILDLINK_PKGSRCDIR.hs-async?=		../../devel/hs-async
 
 .include "../../devel/hs-hashable/buildlink3.mk"
 .endif	# HS_ASYNC_BUILDLINK3_MK
