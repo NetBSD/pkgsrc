5ae0983f38e8afdd0b97eeeea6863fa494acbb1c
pho <pho@pkgsrc.org>
2021-04-23 10:53:02 +0000

    Update to blaze-builder-0.4.2.1
    * 0.4.2.1
      - Bump cabal file to Cabal >= 1.10
    
    * 0.4.2.0
      - Make semigroup instances unconditional
      - Support bytestring-0.11
      - Support semigroups-0.19

--- devel/hs-blaze-builder/buildlink3.mk
+++ devel/hs-blaze-builder/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/01/11 10:48:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2021/04/23 10:53:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-builder
 
 .if !defined(HS_BLAZE_BUILDER_BUILDLINK3_MK)
 HS_BLAZE_BUILDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.1.0
+BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.2.1
 BUILDLINK_PKGSRCDIR.hs-blaze-builder?=		../../devel/hs-blaze-builder
 .endif	# HS_BLAZE_BUILDER_BUILDLINK3_MK
 
