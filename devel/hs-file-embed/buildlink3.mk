15b8396a9b28ce0f16f7e4fb2dcc4d080e8c0332
pho <pho@pkgsrc.org>
2021-04-23 18:00:28 +0000

    Update to file-embed-0.0.13.0
    0.0.13.0
    * Ensure that directory listings are returned in sorted order for
      reproducibility yesodweb/yesod#1684
    
    0.0.12.0
    * Use the Bytes literal on newer GHCs to reduce memory usage during
      compilation #36

--- devel/hs-file-embed/buildlink3.mk
+++ devel/hs-file-embed/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:39:47 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 18:00:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-file-embed
 
 .if !defined(HS_FILE_EMBED_BUILDLINK3_MK)
 HS_FILE_EMBED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.11.2
-BUILDLINK_ABI_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.11.2
+BUILDLINK_API_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.13
+BUILDLINK_ABI_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.13.0
 BUILDLINK_PKGSRCDIR.hs-file-embed?=	../../devel/hs-file-embed
 .endif	# HS_FILE_EMBED_BUILDLINK3_MK
 
