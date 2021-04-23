73cd4c156857025db8dc41b87469ad91fb610450
pho <pho@pkgsrc.org>
2021-04-23 04:50:04 +0000

    Update to hashable-1.3.1.0
    * Add Hashable1 instances to semigroups types.
    * Use ghc-bignum with GHC-9.0
    * Use FNV-1 constants.
    * Make hashable-examples a test-suite

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/01/11 09:31:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2021/04/23 04:50:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.3.0
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.3.0.0
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.3.1
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.3.1.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
