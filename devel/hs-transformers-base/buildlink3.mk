1e46f9dcf6590d2ceb7f4f5fa2b64a3f664c2b82
pho <pho@pkgsrc.org>
2021-04-23 12:23:00 +0000

    Update PLIST

--- devel/hs-transformers-base/buildlink3.mk
+++ devel/hs-transformers-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 10:38:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2021/04/23 12:23:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers-base
 
@@ -9,8 +9,8 @@ BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.5
 BUILDLINK_ABI_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.5.2
 BUILDLINK_PKGSRCDIR.hs-transformers-base?=	../../devel/hs-transformers-base
 
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-base-orphans/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_TRANSFORMERS_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-transformers-base
