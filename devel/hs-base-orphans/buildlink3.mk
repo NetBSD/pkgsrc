3a4c89b55c59d2896116b905f49aa1435e67d673
pho <pho@pkgsrc.org>
2021-04-23 07:54:05 +0000

    Update to base-orphans-0.8.4
    Changes in 0.8.4 [2020.12.09]
    * Backport the Ord instances for TyCon and TypeRep to be available on
      GHC 7.0.
    
    Changes in 0.8.3 [2020.09.30]
    * Backport new instances from GHC 9.0/base-4.15
      - MonadFix and MonadZip instances for Complex
      - Ix instances for tuples of size 6 through 15
    
    Changes in 0.8.2 [2020.01.27]
    * Backport new instances from GHC 8.10/base-4.14
      - Bits, Bounded, Enum, FiniteBits, Floating, Fractional, Integral,
        Ix, Real, RealFrac, RealFloat and Storable instances for
        Data.Ord.Down
      - Functor, Applicative, Monad, Alternative, and MonadPlus instances
        for Kleisli
      - Functor, Applicative, and Monad instances for (,,) a b and (,,,) a
        b c
      - Data instances for WrappedArrow and WrappedMonad
      - Data and IsList instances for ZipList
      - TestEquality instance for Compose
    * Backport the Typeable (() :: Constraint) instance to be available on
      GHC 7.8.

--- devel/hs-base-orphans/buildlink3.mk
+++ devel/hs-base-orphans/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/12/31 17:57:42 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 07:54:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-orphans
 
 .if !defined(HS_BASE_ORPHANS_BUILDLINK3_MK)
 HS_BASE_ORPHANS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.1
-BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.1
+BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.4
+BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.4
 BUILDLINK_PKGSRCDIR.hs-base-orphans?=	../../devel/hs-base-orphans
 .endif	# HS_BASE_ORPHANS_BUILDLINK3_MK
 
