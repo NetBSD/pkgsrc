0d0623c8d01aa82c1ee6625bd6b77e159b07943e
pho <pho@pkgsrc.org>
2021-04-23 08:54:21 +0000

    Update to base64-bytestring-1.2.0.1
    1.2.0.1
    * Package update: support for bytestring >=0.11
    
    1.2.0.0
    * Security fix: reject non-canonical base64 encoded values - (#38)
      fixing issue #24.
    * Security fix: reject bytestrings with improper padding that can be
      "completed" by the unpadded-Base64url workflow, and homogenize error
      messages (#33)
    * Test coverage expanded to 98% of the library. All critical paths
      covered.
    
    1.1.0.0
    * joinWith has been removed (#32)
    * Bugfix: decode formerly allowed for padding chars to be interspersed
      in a valid base64-encoded string. This is now not the case, and it
      is fully spec-compliant as of #31
    * The default behavior for Base64url decode is now to support
      arbitrary padding. If you need strict padded or unpadded decode
      semantics, use decodePadded or decodeUnpadded.
    * Added strict unpadded and padded decode functions for Base64url
      (#30)
    * Added unpadded encode for Base64url (#26).
    
    1.0.0.3
    * Made performance more robust (#27).
    * Improved documentation (#23).
    * Improved the performance of decodeLenient a bit (#21).

--- converters/hs-base64-bytestring/buildlink3.mk
+++ converters/hs-base64-bytestring/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 23:26:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/04/23 08:54:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-base64-bytestring
 
 .if !defined(HS_BASE64_BYTESTRING_BUILDLINK3_MK)
 HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0
-BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0.2
+BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.2.0.1
 BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../converters/hs-base64-bytestring
 .endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
 
