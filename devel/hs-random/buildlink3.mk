eb0cb0dc07c1fd76952ccca0e6ae2fc6911a0039
pho <pho@pkgsrc.org>
2021-04-23 06:51:27 +0000

    Update to random-1.2.0
    * Breaking change which mostly maintains backwards compatibility, see
      "Breaking Changes" below.
    * Support for monadic generators e.g. mwc-random.
    * Monadic adapters for pure generators (providing a uniform monadic
      interface to pure and monadic generators).
    * Faster in all cases except one by more than x18 (N.B. x18 not 18%)
      and some cases (depending on the type) faster by more than x1000
    * Passes a large number of random number test suites:
      - dieharder
      - TestU01 (SmallCrush, Crush, BigCrush)
      - PractRand
      - gjrand
      - See https://github.com/tweag/random-quality for details on how to
        do this yourself.
    * Better quality split as judged by these tests:
      https://www.cambridge.org/core/journals/journal-of-functional-programming/article/evaluation-of-splittable-pseudorandom-generators/3EBAA9F14939C5BB5560E32D1A132637
      Again see https://github.com/tweag/random-quality for details on how
      to do this yourself.
    * Unbiased generation of ranges.
    * Updated tests and benchmarks.
    * Continuous integration.
    
    Breaking Changes
    
    Version 1.2.0 introduces these breaking changes:
    * requires base >= 4.8 (GHC-7.10)
    * StdGen is no longer an instance of Read
    * randomIO and randomRIO were extracted from the Random class into
      separate functions
    
    In addition, there may be import clashes with new functions,
    e.g. uniform and uniformR.
    
    Deprecations
    
    Version 1.2.0 introduces genWord64, genWord32 and similar methods to
    the RandomGen class. The significantly slower method next and its
    companion genRange are now deprecated.

--- devel/hs-random/buildlink3.mk
+++ devel/hs-random/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/07 13:04:04 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/04/23 06:51:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-random
 
 .if !defined(HS_RANDOM_BUILDLINK3_MK)
 HS_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.1
-BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.1
+BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.2.0
 BUILDLINK_PKGSRCDIR.hs-random?=		../../devel/hs-random
 .endif	# HS_RANDOM_BUILDLINK3_MK
 
