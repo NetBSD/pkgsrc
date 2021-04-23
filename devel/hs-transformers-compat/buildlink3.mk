3d9a6492a7c4637a7bdb63b784890b8ed9767e00
pho <pho@pkgsrc.org>
2021-04-23 07:09:50 +0000

    Update to transformers-compat-0.6.6
    * Add FunctorClassesDefault, an adapter newtype suitable for
      DerivingVia, to Data.Functor.Classes.Generic.
    * Fix a bug in which readsPrec1Default/liftReadsPrecDefault would
      parse empty data types too strictly.

--- devel/hs-transformers-compat/buildlink3.mk
+++ devel/hs-transformers-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/12/31 18:54:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/23 07:09:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers-compat
 
 .if !defined(HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK)
 HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.6.5
-BUILDLINK_ABI_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.6.5
+BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.6.6
+BUILDLINK_ABI_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.6.6
 BUILDLINK_PKGSRCDIR.hs-transformers-compat?=	../../devel/hs-transformers-compat
 
 .include "../../devel/hs-fail/buildlink3.mk"
