1b4b6f8fa23fd67b5efcfbb059dca91ce8576d3a
pho <pho@pkgsrc.org>
2021-04-23 15:24:09 +0000

    Update to conduit-1.3.4.1
    1.3.4.1
    * Library and tests compile and run with GHC 9.0.1 #455
    
    1.3.4
    * Add foldWhile #453 #456.
    
    1.3.3
    * Add uncons, unconsM, unconsEither, unconsEitherM.
    
    1.3.2.1
    * Fix isChunksForExactlyE #445 #446
    
    1.3.2
    * Add mapInputM #435

--- devel/hs-conduit/buildlink3.mk
+++ devel/hs-conduit/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/11 09:04:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/04/23 15:24:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-conduit
 
 .if !defined(HS_CONDUIT_BUILDLINK3_MK)
 HS_CONDUIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-conduit+=	hs-conduit>=1.3.1
-BUILDLINK_ABI_DEPENDS.hs-conduit+=	hs-conduit>=1.3.1.2
+BUILDLINK_API_DEPENDS.hs-conduit+=	hs-conduit>=1.3.4
+BUILDLINK_ABI_DEPENDS.hs-conduit+=	hs-conduit>=1.3.4.1
 BUILDLINK_PKGSRCDIR.hs-conduit?=	../../devel/hs-conduit
 
-.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-mono-traversable/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-resourcet/buildlink3.mk"
