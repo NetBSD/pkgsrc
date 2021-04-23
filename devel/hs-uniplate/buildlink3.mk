62fd6db798096cbd7193f52c46d282609bd5849c
pho <pho@pkgsrc.org>
2021-04-23 12:51:29 +0000

    Update to uniplate-1.6.13
    1.6.13, released 2020-11-07
    * Remove support from pre-GHC 8.0
    * #31, GHC 9.0 compatibility
    * Change descendM to be applicative, not monadic

--- devel/hs-uniplate/buildlink3.mk
+++ devel/hs-uniplate/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/04/23 12:51:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-uniplate
 
 .if !defined(HS_UNIPLATE_BUILDLINK3_MK)
 HS_UNIPLATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.12
-BUILDLINK_ABI_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.12nb9
+BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.13
+BUILDLINK_ABI_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.13
 BUILDLINK_PKGSRCDIR.hs-uniplate?=	../../devel/hs-uniplate
 
 .include "../../devel/hs-hashable/buildlink3.mk"
