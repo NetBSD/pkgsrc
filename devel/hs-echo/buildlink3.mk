253feb54e4de9df699a999c73d2d42bd77a62130
pho <pho@pkgsrc.org>
2021-04-23 09:14:44 +0000

    Update to echo-0.1.4
    0.1.4 [2021.02.17]
    * Use Trustworthy, not Safe, on GHC 7.2 to work around an old GHC bug.

--- devel/hs-echo/buildlink3.mk
+++ devel/hs-echo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/18 22:57:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/23 09:14:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-echo
 
 .if !defined(HS_ECHO_BUILDLINK3_MK)
 HS_ECHO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-echo+=	hs-echo>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-echo+=	hs-echo>=0.1.3
+BUILDLINK_API_DEPENDS.hs-echo+=	hs-echo>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-echo+=	hs-echo>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-echo?=	../../devel/hs-echo
 .endif	# HS_ECHO_BUILDLINK3_MK
 
