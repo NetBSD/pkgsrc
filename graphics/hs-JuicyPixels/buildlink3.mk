5e0ad45df37fa0817b180ea138cee4ecde0724bd
pho <pho@pkgsrc.org>
2021-04-24 12:12:01 +0000

    Update to JuicyPixels-3.3.5
    v3.3.5 January 2020
    -------------------
    
    Maintenance release to push various pull requests onto hackage
    
     * Exporting Pixel32 (will)
     * Palettization of transparent frames in Gif (flutterlice)
     * Documentation fixes (lehins)

--- graphics/hs-JuicyPixels/buildlink3.mk
+++ graphics/hs-JuicyPixels/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/16 15:51:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 12:12:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-JuicyPixels
 
 .if !defined(HS_JUICYPIXELS_BUILDLINK3_MK)
 HS_JUICYPIXELS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.4
-BUILDLINK_ABI_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.4
+BUILDLINK_API_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.5
+BUILDLINK_ABI_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.5
 BUILDLINK_PKGSRCDIR.hs-JuicyPixels?=	../../graphics/hs-JuicyPixels
 
 .include "../../devel/hs-primitive/buildlink3.mk"
