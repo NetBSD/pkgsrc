9ea277465105dff0bb1254ef682dcca199fe4858
pho <pho@pkgsrc.org>
2021-04-24 05:45:29 +0000

    Update to Decimal-0.5.2
    No change logs are provided by the upstream.

--- finance/hs-Decimal/buildlink3.mk
+++ finance/hs-Decimal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:44:12 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:45:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-Decimal
 
 .if !defined(HS_DECIMAL_BUILDLINK3_MK)
 HS_DECIMAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Decimal+=	hs-Decimal>=0.5.1
-BUILDLINK_ABI_DEPENDS.hs-Decimal+=	hs-Decimal>=0.5.1
+BUILDLINK_API_DEPENDS.hs-Decimal+=	hs-Decimal>=0.5.2
+BUILDLINK_ABI_DEPENDS.hs-Decimal+=	hs-Decimal>=0.5.2
 BUILDLINK_PKGSRCDIR.hs-Decimal?=	../../finance/hs-Decimal
 .endif	# HS_DECIMAL_BUILDLINK3_MK
 
