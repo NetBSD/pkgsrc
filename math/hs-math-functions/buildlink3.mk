0d622942dc7dbd6185a4f7da0b5d137ab9170d22
pho <pho@pkgsrc.org>
2021-04-24 10:15:50 +0000

    Update to math-functions-0.3.4.2
    Changes in 0.3.4.2
    * Fixed crash in invIncompleteBeta (#68) for some inputs initial
      approximation was computed incorrectly.
    
    Changes in 0.3.4.1
    * Precision of incompleteGamma improved.
    
    Changes in 0.3.4.0
    * Dependency on vector-th-unbox is dropped. All instances are written
      by hand now.

--- math/hs-math-functions/buildlink3.mk
+++ math/hs-math-functions/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/02 11:15:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 10:15:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-math-functions
 
 .if !defined(HS_MATH_FUNCTIONS_BUILDLINK3_MK)
 HS_MATH_FUNCTIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.3.0
-BUILDLINK_PKGSRCDIR.hs-math-functions?=	../../math/hs-math-functions
+BUILDLINK_API_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.4.2
+BUILDLINK_PKGSRCDIR.hs-math-functions?=		../../math/hs-math-functions
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
-.include "../../devel/hs-vector-th-unbox/buildlink3.mk"
 .endif	# HS_MATH_FUNCTIONS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-math-functions
