f453c42882ca79e812794ef9834485793540d081
pho <pho@pkgsrc.org>
2021-04-24 17:31:23 +0000

    Update to unicode-transforms-0.3.7.1
    0.3.7.1
    * Fix x32 build
    
    0.3.7
    * Significant performance improvements
    * Update to Unicode version 13.0.0

--- textproc/hs-unicode-transforms/buildlink3.mk
+++ textproc/hs-unicode-transforms/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 18:49:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 17:31:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-transforms
 
 .if !defined(HS_UNICODE_TRANSFORMS_BUILDLINK3_MK)
 HS_UNICODE_TRANSFORMS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.3.6
-BUILDLINK_ABI_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.3.6
+BUILDLINK_API_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.3.7
+BUILDLINK_ABI_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.3.7.1
 BUILDLINK_PKGSRCDIR.hs-unicode-transforms?=	../../textproc/hs-unicode-transforms
-
-.include "../../devel/hs-bitarray/buildlink3.mk"
 .endif	# HS_UNICODE_TRANSFORMS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-unicode-transforms
