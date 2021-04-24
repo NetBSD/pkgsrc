2fc61efd9f78698e886d4b0ba45f116a1655bb62
pho <pho@pkgsrc.org>
2021-04-24 05:28:22 +0000

    Update to tasty-hunit-0.10.0.3
    The only point of this release is to introduce compatibility with GHCs
    back to 7.0 (see https://github.com/feuerbach/tasty/pull/287).
    
    Note, however, that these changes are not merged to the master branch,
    and the future releases will only support the GHC/base versions from
    the last 5 years, as per our usual policy. To test with even older
    GHCs, you'll have to use this particular version of tasty-hunit (or
    have the constraint solver pick it for you when testing with older
    GHCs).
    
    The source of this release is in the support-old-ghcs branch of the
    tasty repository.

--- devel/hs-tasty-hunit/buildlink3.mk
+++ devel/hs-tasty-hunit/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:51:43 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:28:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty-hunit
 
 .if !defined(HS_TASTY_HUNIT_BUILDLINK3_MK)
 HS_TASTY_HUNIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.0.2
-BUILDLINK_ABI_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.0.2
+BUILDLINK_API_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.0
+BUILDLINK_ABI_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.0.3
 BUILDLINK_PKGSRCDIR.hs-tasty-hunit?=	../../devel/hs-tasty-hunit
 .endif	# HS_TASTY_HUNIT_BUILDLINK3_MK
 
