e781f6522f1bdc0385d86944c88a27dbc985c9cc
pho <pho@pkgsrc.org>
2021-04-24 17:18:03 +0000

    Update to texmath-0.12.2
    texmath (0.12.2)
      * MathML input: support mmultiscripts element (#158, #100).
      * Make MathML tag/attr recognition case-insensitive (#158).
      * Pandoc writer: better handling of styling such as `\mathrm` (#145).
        Previously identifiers were always italic, no matter what
        styling was applied.
      * Ignore `\tag` in TeX input (#162).
      * TeX writer: avoid unneeded `\left` and `\right` for delimited.
        We don't need `\left` and `\right` when the contents are
        "standard height."
      * TeX reader: parse implicit EDelimited sections (#172).
        We now parse `(x)` as EDelimited, even though `\right` and `\left`
        are not used.
    
    texmath (0.12.1.1)
      * Fix compilation with GHC-9.0.1 (#169, Simon Jakobi).
        Background:
        https://gitlab.haskell.org/ghc/ghc/-/wikis/migration/9.0#simplified-subsumption
      * Add eqn to online demo.
      * Improve error messages for unknown control sequences, and restructure
        tex parser to be more efficient (#167).
    
    texmath (0.12.1)
      * OMML writer: explicitly mark symbols as non-italic (#109).
        Otherwise, for some reason, they appear as italic by default.
      * Improve error messages in reading tex arrays.
      * Improve support for `\bmod`, `\mod`, etc. (#165).
        Allow them to take complex arguments like `\left( 1 \right)`.
      * Improve support for `\genfrac` (#164).
      * Ignore `\textstyle`, `\scriptstyle`, `\scriptscriptstyle`,
        as we currently ignore `\displaystyle`.
      * Parse siunitx commands in reading tex (#157).
      * Improve handling of `\not` in reading tex (#161).
        Previously we only handled `\not` in front of certain symbols.
      * Support `\pod` and `\pmod` and clean up spacing and font for
        `\mod` and `\bmod` (#160).
    
    texmath (0.12.0.3)
      * Allow pandoc-types 1.22.
    
    texmath (0.12.0.2)
      * Allow pandoc-types 1.21.
      * Pandoc output: omit empty Emph for sub/superscript without base (#155).
      * tex writer: Use `\overline{\overline{B}}` instead of unicode
        double line accent (#153).
    
    texmath (0.12.0.1)
      * OMML writer: Fix overline and accent rendering (#152).
      * OMML reader: Fix dropped arrows (#153). Add tests.

--- textproc/hs-texmath/buildlink3.mk
+++ textproc/hs-texmath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2020/01/11 10:21:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2021/04/24 17:18:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
@@ -6,13 +6,12 @@ BUILDLINK_TREE+=	hs-texmath
 HS_TEXMATH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.12
-BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.12
+BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.12.2
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../textproc/hs-texmath
 
 .include "../../textproc/hs-pandoc-types/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .include "../../textproc/hs-xml/buildlink3.mk"
-.include "../../converters/hs-aeson/buildlink3.mk"
 .endif	# HS_TEXMATH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-texmath
