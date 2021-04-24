5845b260a69112b3dc3292988c7eb17b134ad196
pho <pho@pkgsrc.org>
2021-04-24 16:32:23 +0000

    Update to skylighting-0.10.5.1
    The change log is too long to paste here. See
    http://hackage.haskell.org/package/skylighting-core-0.10.5.1/changelog

--- textproc/hs-skylighting/buildlink3.mk
+++ textproc/hs-skylighting/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 17:32:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 16:32:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-skylighting
 
 .if !defined(HS_SKYLIGHTING_BUILDLINK3_MK)
 HS_SKYLIGHTING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.8.3
-BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.8.3
+BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.10.5
+BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.10.5.1
 BUILDLINK_PKGSRCDIR.hs-skylighting?=	../../textproc/hs-skylighting
 
 .include "../../textproc/hs-skylighting-core/buildlink3.mk"
