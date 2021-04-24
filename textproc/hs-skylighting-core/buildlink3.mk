36f3b709d23e89df884bd77d18bd797cf84c8f19
pho <pho@pkgsrc.org>
2021-04-24 16:28:50 +0000

    Update to skylighting-core-0.10.5.1
    The change log is too long to paste here. See
    http://hackage.haskell.org/package/skylighting-core-0.10.5.1/changelog

--- textproc/hs-skylighting-core/buildlink3.mk
+++ textproc/hs-skylighting-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 17:09:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 16:28:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-skylighting-core
 
 .if !defined(HS_SKYLIGHTING_CORE_BUILDLINK3_MK)
 HS_SKYLIGHTING_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.8.3
-BUILDLINK_ABI_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.8.3
+BUILDLINK_API_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.10.5
+BUILDLINK_ABI_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.10.5.1
 BUILDLINK_PKGSRCDIR.hs-skylighting-core?=	../../textproc/hs-skylighting-core
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -16,10 +16,9 @@ BUILDLINK_PKGSRCDIR.hs-skylighting-core?=	../../textproc/hs-skylighting-core
 .include "../../textproc/hs-blaze-html/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../devel/hs-colour/buildlink3.mk"
-.include "../../textproc/hs-hxt//buildlink3.mk"
-.include "../../devel/hs-safe//buildlink3.mk"
-.include "../../devel/hs-utf8-string//buildlink3.mk"
-.include "../../textproc/hs-regex-pcre//buildlink3.mk"
+.include "../../devel/hs-safe/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../textproc/hs-xml-conduit/buildlink3.mk"
 .endif	# HS_SKYLIGHTING_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-skylighting-core
