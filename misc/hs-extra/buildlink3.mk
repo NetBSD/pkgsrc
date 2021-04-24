23eb10a4c2db361f81d590ee1df2d411e874eb54
pho <pho@pkgsrc.org>
2021-04-24 05:50:38 +0000

    Update to extra-1.7.9
    1.7.9, released 2020-12-19
        #72, add pureIf
        #73, make takeEnd, dropEnd, splitAtEnd: return immediately if i <= 0
        #71, add compareLength and comparingLength
    1.7.8, released 2020-09-12
        #68, make sure Data.Foldable.Extra is exposed
    1.7.7, released 2020-08-25
        #67, defer to System.IO readFile', hGetContents' in GHC 9.0
    1.7.6, released 2020-08-21
        #66, add lots of functions to Data.Foldable.Extra (anyM etc)
    1.7.5, released 2020-08-12
        #65, add Data.Foldable.Extra
        #65, add sum', product', sumOn' and productOn'
    1.7.4, released 2020-07-15
        #59, add whileJustM and untilJustM
        #61, optimise nubOrd (10% or so)
        Add first3, second3, third3
    1.7.3, released 2020-05-30
        #58, add disjointOrd and disjointOrdBy
    1.7.2, released 2020-05-25
        #56, add zipWithLongest
        #57, make duration in MonadIO
        Simplify and optimise Barrier
        Mark modules that are empty as DEPRECATED
        Remove support for GHC 7.10
    1.7.1, released 2020-03-10
        Add NOINLINE to errorIO to work around a GHC 8.4 bug
    1.7, released 2020-03-05
    *   #40, delete deprecated function for
    *   zipFrom now truncates lists, rather than error, just like zip
    1.6.21, released 2020-03-02
        #54, deprecate nubOn since its O(n^2). Use nubOrdOn
        #53, add some nub functions to NonEmpty
    1.6.20, released 2020-02-16
        Add firstM, secondM
    1.6.19, released 2020-02-11
        #50, add headDef, lastDef, and dropEnd1

--- misc/hs-extra/buildlink3.mk
+++ misc/hs-extra/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 06:04:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:50:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-extra
 
 .if !defined(HS_EXTRA_BUILDLINK3_MK)
 HS_EXTRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.6.18
-BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.6.18
-BUILDLINK_PKGSRCDIR.hs-extra?=	../../misc/hs-extra
+BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.7.9
+BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.7.9
+BUILDLINK_PKGSRCDIR.hs-extra?=		../../misc/hs-extra
 
 .include "../../time/hs-clock/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_EXTRA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-extra
