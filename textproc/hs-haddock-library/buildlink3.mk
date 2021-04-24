b37733babc941c8fcb4cfcd74e0a5c84b8639955
pho <pho@pkgsrc.org>
2021-04-24 15:31:23 +0000

    Update to haddock-library-1.10.0
    Changes in version 1.10.0
    * Add support for labeled module references (#1319, #1315)
    
    Changes in version 1.9.0
    * Fix build-time regression for base < 4.7 (#1119)
    * Update parsing to strip whitespace from table cells (#1074)

--- textproc/hs-haddock-library/buildlink3.mk
+++ textproc/hs-haddock-library/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 12:25:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:31:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-haddock-library
 
 .if !defined(HS_HADDOCK_LIBRARY_BUILDLINK3_MK)
 HS_HADDOCK_LIBRARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.8.0
-BUILDLINK_ABI_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.8.0
+BUILDLINK_API_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.10.0
+BUILDLINK_ABI_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.10.0
 BUILDLINK_PKGSRCDIR.hs-haddock-library?=	../../textproc/hs-haddock-library
 .endif	# HS_HADDOCK_LIBRARY_BUILDLINK3_MK
 
