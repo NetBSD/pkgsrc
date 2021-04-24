112a39d0676f69df0d99acb971a77a7ef30f52d6
pho <pho@pkgsrc.org>
2021-04-24 06:40:11 +0000

    Import prettyprinter-ansi-terminal-1.1.2

---
+++ textproc/hs-prettyprinter-ansi-terminal/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/04/24 06:40:11 pho Exp $
+
+BUILDLINK_TREE+=	hs-prettyprinter-ansi-terminal
+
+.if !defined(HS_PRETTYPRINTER_ANSI_TERMINAL_BUILDLINK3_MK)
+HS_PRETTYPRINTER_ANSI_TERMINAL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-prettyprinter-ansi-terminal+=	hs-prettyprinter-ansi-terminal>=1.1.2
+BUILDLINK_ABI_DEPENDS.hs-prettyprinter-ansi-terminal+=	hs-prettyprinter-ansi-terminal>=1.1.2
+BUILDLINK_PKGSRCDIR.hs-prettyprinter-ansi-terminal?=	../../textproc/hs-prettyprinter-ansi-terminal
+
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter/buildlink3.mk"
+.endif	# HS_PRETTYPRINTER_ANSI_TERMINAL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-prettyprinter-ansi-terminal
