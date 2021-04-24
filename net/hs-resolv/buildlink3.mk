77de4f994bed0abb09ed32f15ebae816ec70dd5e
pho <pho@pkgsrc.org>
2021-04-24 12:25:09 +0000

    Update to resolv-0.1.2.0
    * Add new high-level API functions queryPTR, arpaIPv4, and arpaIPv6
      for performing reverse address lookups.

--- net/hs-resolv/buildlink3.mk
+++ net/hs-resolv/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 02:37:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 12:25:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-resolv
 
 .if !defined(HS_RESOLV_BUILDLINK3_MK)
 HS_RESOLV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resolv+=	hs-resolv>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-resolv+=	hs-resolv>=0.1.1.3
+BUILDLINK_API_DEPENDS.hs-resolv+=	hs-resolv>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-resolv+=	hs-resolv>=0.1.2.0
 BUILDLINK_PKGSRCDIR.hs-resolv?=		../../net/hs-resolv
 
 .include "../../converters/hs-base16-bytestring/buildlink3.mk"
