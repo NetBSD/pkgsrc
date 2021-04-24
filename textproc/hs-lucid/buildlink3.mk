18fc46b79d8804ca979dca1e7ebef89b9606dbdc
pho <pho@pkgsrc.org>
2021-04-24 10:13:05 +0000

    Update to lucid-2.9.12.1
    * Allow different orderings of attributes in test-suite

--- textproc/hs-lucid/buildlink3.mk
+++ textproc/hs-lucid/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:47:25 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 10:13:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-lucid
 
@@ -6,8 +6,13 @@ BUILDLINK_TREE+=	hs-lucid
 HS_LUCID_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-lucid+=	hs-lucid>=2.9.12
-BUILDLINK_ABI_DEPENDS.hs-lucid+=	hs-lucid>=2.9.12
+BUILDLINK_ABI_DEPENDS.hs-lucid+=	hs-lucid>=2.9.12.1
 BUILDLINK_PKGSRCDIR.hs-lucid?=		../../textproc/hs-lucid
+
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-mmorph/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_LUCID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-lucid
