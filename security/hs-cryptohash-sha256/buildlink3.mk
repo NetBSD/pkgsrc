2e556cc384bbf7b2028ef74dda6481b1f9a0fa04
pho <pho@pkgsrc.org>
2021-04-24 13:25:14 +0000

    Update to cryptohash-sha-256-0.11.102.0
    * Add Eq instance for Ctx
    * Add start and startlazy producing Ctx
    * Introduce new use-cbits cabal flag in order to add support for
      FFI-less cryptohash-sha256-pure pass-thru

--- security/hs-cryptohash-sha256/buildlink3.mk
+++ security/hs-cryptohash-sha256/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/18 15:25:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 13:25:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-cryptohash-sha256
 
 .if !defined(HS_CRYPTOHASH_SHA256_BUILDLINK3_MK)
 HS_CRYPTOHASH_SHA256_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cryptohash-sha256+=	hs-cryptohash-sha256>=0.11.101
-BUILDLINK_ABI_DEPENDS.hs-cryptohash-sha256+=	hs-cryptohash-sha256>=0.11.101.0
+BUILDLINK_API_DEPENDS.hs-cryptohash-sha256+=	hs-cryptohash-sha256>=0.11.102
+BUILDLINK_ABI_DEPENDS.hs-cryptohash-sha256+=	hs-cryptohash-sha256>=0.11.102.0
 BUILDLINK_PKGSRCDIR.hs-cryptohash-sha256?=	../../security/hs-cryptohash-sha256
 .endif	# HS_CRYPTOHASH_SHA256_BUILDLINK3_MK
 
