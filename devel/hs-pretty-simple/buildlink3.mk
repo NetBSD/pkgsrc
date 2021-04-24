a1cd345c03e33ca6bcceb053ae12dba23c1514fc
pho <pho@pkgsrc.org>
2021-04-24 06:45:59 +0000

    Import pretty-simple-4.0.0.0

---
+++ devel/hs-pretty-simple/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/04/24 06:45:59 pho Exp $
+
+BUILDLINK_TREE+=	hs-pretty-simple
+
+.if !defined(HS_PRETTY_SIMPLE_BUILDLINK3_MK)
+HS_PRETTY_SIMPLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.0.0
+BUILDLINK_ABI_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.0.0.0
+BUILDLINK_PKGSRCDIR.hs-pretty-simple?=		../../devel/hs-pretty-simple
+
+.include "../../textproc/hs-prettyprinter/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter-ansi-terminal/buildlink3.mk"
+.endif	# HS_PRETTY_SIMPLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pretty-simple
