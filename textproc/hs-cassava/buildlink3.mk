3eea4cb9729ad978af9a1b8a3c12885ec3f12626
pho <pho@pkgsrc.org>
2021-04-24 06:05:49 +0000

    Fix build with GHC 9

--- textproc/hs-cassava/buildlink3.mk
+++ textproc/hs-cassava/buildlink3.mk
@@ -1,13 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:49:19 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 06:10:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-cassava
 
 .if !defined(HS_CASSAVA_BUILDLINK3_MK)
 HS_CASSAVA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cassava+=	hs-cassava>=0.5.2.0
+BUILDLINK_API_DEPENDS.hs-cassava+=	hs-cassava>=0.5.2
 BUILDLINK_ABI_DEPENDS.hs-cassava+=	hs-cassava>=0.5.2.0
 BUILDLINK_PKGSRCDIR.hs-cassava?=	../../textproc/hs-cassava
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-Only/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-text-short/buildlink3.mk"
 .endif	# HS_CASSAVA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-cassava
