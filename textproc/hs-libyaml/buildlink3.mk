3e3893e4c3e339451d2fe60a002f90ea071f50ba
pho <pho@pkgsrc.org>
2021-04-24 14:25:50 +0000

    Update to libyaml-0.1.2
    0.1.2
    * Upgrade libyaml to 0.2.2

--- textproc/hs-libyaml/buildlink3.mk
+++ textproc/hs-libyaml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/11 11:04:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 14:25:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-libyaml
 
 .if !defined(HS_LIBYAML_BUILDLINK3_MK)
 HS_LIBYAML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-libyaml+=	hs-libyaml>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-libyaml+=	hs-libyaml>=0.1.1.1
+BUILDLINK_API_DEPENDS.hs-libyaml+=	hs-libyaml>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-libyaml+=	hs-libyaml>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-libyaml?=	../../textproc/hs-libyaml
 
 .include "../../devel/hs-conduit/buildlink3.mk"
