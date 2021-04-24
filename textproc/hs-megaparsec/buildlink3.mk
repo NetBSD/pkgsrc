9fcb29e9e561ac6d5af53c65b7c49434fbd95ddc
pho <pho@pkgsrc.org>
2021-04-24 06:15:36 +0000

    Update to megaparsec-9.0.1
    Megaparsec 9.0.1
    * Added Safe Haskell support.
    
    Megaparsec 9.0.0
    * Split the Stream type class. The methods showTokens and tokensLength
      have been put into a separate type class VisualStream, while
      reachOffset and reachOffsetNoLine are now in TraversableStream. This
      should make defining Stream instances for custom streams easier.
    * Defined Stream instances for lists and Seqs.
    * Added the functions hspace and hspace1 to the Text.Megaparsec.Char
      and Text.Megaparsec.Byte modules.

--- textproc/hs-megaparsec/buildlink3.mk
+++ textproc/hs-megaparsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 15:01:24 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 06:15:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-megaparsec
 
 .if !defined(HS_MEGAPARSEC_BUILDLINK3_MK)
 HS_MEGAPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-megaparsec+=	hs-megaparsec>=8.0.0
-BUILDLINK_ABI_DEPENDS.hs-megaparsec+=	hs-megaparsec>=8.0.0
+BUILDLINK_API_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.0.1
+BUILDLINK_ABI_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.0.1
 BUILDLINK_PKGSRCDIR.hs-megaparsec?=	../../textproc/hs-megaparsec
 
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
