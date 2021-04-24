b770fab4ced1b957bce668128d07235fca3e2be9
pho <pho@pkgsrc.org>
2021-04-24 18:01:15 +0000

    Update PLIST

--- www/hs-http-client-tls/buildlink3.mk
+++ www/hs-http-client-tls/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 15:17:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 18:01:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-http-client-tls
 
 .if !defined(HS_HTTP_CLIENT_TLS_BUILDLINK3_MK)
 HS_HTTP_CLIENT_TLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-client-tls+=	hs-http-client-tls>=0.3.5.3
+BUILDLINK_API_DEPENDS.hs-http-client-tls+=	hs-http-client-tls>=0.3.5
 BUILDLINK_ABI_DEPENDS.hs-http-client-tls+=	hs-http-client-tls>=0.3.5.3
 BUILDLINK_PKGSRCDIR.hs-http-client-tls?=	../../www/hs-http-client-tls
 
@@ -13,7 +13,6 @@ BUILDLINK_PKGSRCDIR.hs-http-client-tls?=	../../www/hs-http-client-tls
 .include "../../net/hs-connection/buildlink3.mk"
 .include "../../security/hs-cryptonite/buildlink3.mk"
 .include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../www/hs-http-client/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../devel/hs-memory/buildlink3.mk"
