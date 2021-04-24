f7149686a7487b5f5e22773dc1a3f3c280fcff4c
pho <pho@pkgsrc.org>
2021-04-24 10:30:18 +0000

    Update to shakespeare-2.0.25
    2.0.25
    * Support for GHC 9.0 #254

--- textproc/hs-shakespeare/buildlink3.mk
+++ textproc/hs-shakespeare/buildlink3.mk
@@ -1,13 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:48:01 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 10:30:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-shakespeare
 
 .if !defined(HS_SHAKESPEARE_BUILDLINK3_MK)
 HS_SHAKESPEARE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.24
-BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.24
+BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.25
+BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.25
 BUILDLINK_PKGSRCDIR.hs-shakespeare?=	../../textproc/hs-shakespeare
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-blaze-html/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-th-lift/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_SHAKESPEARE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-shakespeare
