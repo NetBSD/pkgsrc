fa783ab96694f8d48aa5058d260431ffa687098d
pho <pho@pkgsrc.org>
2021-04-24 17:53:42 +0000

    Import iproute-1.7.11

---
+++ net/hs-iproute/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/04/24 17:53:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-iproute
+
+.if !defined(HS_IPROUTE_BUILDLINK3_MK)
+HS_IPROUTE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-iproute+=	hs-iproute>=1.7.11
+BUILDLINK_ABI_DEPENDS.hs-iproute+=	hs-iproute>=1.7.11
+BUILDLINK_PKGSRCDIR.hs-iproute?=	../../net/hs-iproute
+
+.include "../../textproc/hs-appar/buildlink3.mk"
+.include "../../sysutils/hs-byteorder/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.endif	# HS_IPROUTE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-iproute
