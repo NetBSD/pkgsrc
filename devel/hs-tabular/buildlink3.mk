275b78d7cb86e3f5cb843bd671c146e15ec35936
pho <pho@pkgsrc.org>
2021-04-24 05:01:28 +0000

    Update to tabular-0.2.2.8
    No change logs are provided by the upstream.

--- devel/hs-tabular/buildlink3.mk
+++ devel/hs-tabular/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:46:47 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:01:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-tabular
 
 .if !defined(HS_TABULAR_BUILDLINK3_MK)
 HS_TABULAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tabular+=	hs-tabular>=0.2.2.7
-BUILDLINK_ABI_DEPENDS.hs-tabular+=	hs-tabular>=0.2.2.7
+BUILDLINK_API_DEPENDS.hs-tabular+=	hs-tabular>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-tabular+=	hs-tabular>=0.2.2.8
 BUILDLINK_PKGSRCDIR.hs-tabular?=	../../devel/hs-tabular
 .endif	# HS_TABULAR_BUILDLINK3_MK
 
