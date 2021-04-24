21cee7b368882c5def2669143c04b67a1f3ab406
pho <pho@pkgsrc.org>
2021-04-24 12:17:06 +0000

    Update to mwc-random-0.15.0.1
    Changes in 0.15.0.1
    * Bug in generation of Int/Word in both uniform and uniformR is
      fixed. (#75)
    
    Changes in 0.15.0.0
    * withSystemRandomST and createSystemSeed are added.
    * withSystemRandom is deprecated.
    * random>=1.2 is dependency of mwc-random.
    * Instances for type classes StatefulGen & FrozenGen defined in
      random-1.2 are added for Gen.
    * Functions in System.Random.MWC.Distributions and
      System.Random.MWC.CondensedTable now work with arbitrary StatefulGen
    * System.Random.MWC.uniformVector now works with arbitrary StatefulGen
      as well and uses in-place initialization instead of generateM. It
      should be faster for anything but IO and ST (those shoud remain
      same).

--- math/hs-mwc-random/buildlink3.mk
+++ math/hs-mwc-random/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/01/02 11:34:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2021/04/24 12:17:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-mwc-random
 
 .if !defined(HS_MWC_RANDOM_BUILDLINK3_MK)
 HS_MWC_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.14.0
-BUILDLINK_ABI_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.14.0.0
+BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.15.0
+BUILDLINK_ABI_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.15.0.1
 BUILDLINK_PKGSRCDIR.hs-mwc-random?=	../../math/hs-mwc-random
 
 .include "../../math/hs-math-functions/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_MWC_RANDOM_BUILDLINK3_MK
 
