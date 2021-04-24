9999e47450130da58d5933a4022104130f16d1d7
pho <pho@pkgsrc.org>
2021-04-24 12:55:43 +0000

    Update to cryptonite-0.28
    0.28
    * Add hash constant time capability
    * Prevent possible overflow during hashing by hashing in 4GB chunks
    
    0.27
    * Optimise AES GCM and CCM
    * Optimise P256R1 implementation
    * Various AES-NI building improvements
    * Add better ECDSA support
    * Add XSalsa derive
    * Implement square roots for ECC binary curve
    * Various tests and benchmarks

--- security/hs-cryptonite/buildlink3.mk
+++ security/hs-cryptonite/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 00:17:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 12:55:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-cryptonite
 
 .if !defined(HS_CRYPTONITE_BUILDLINK3_MK)
 HS_CRYPTONITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.26
-BUILDLINK_ABI_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.26
+BUILDLINK_API_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.28
+BUILDLINK_ABI_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.28
 BUILDLINK_PKGSRCDIR.hs-cryptonite?=	../../security/hs-cryptonite
 
 .include "../../devel/hs-basement/buildlink3.mk"
