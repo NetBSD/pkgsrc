e798766faa7ccba0bc508a78b7f1266a0df8d157
pho <pho@pkgsrc.org>
2021-04-24 18:04:02 +0000

    Update to HTTP-4000.3.16
    Version 4000.3.16: release 2021-03-20
     * Support GHC-9.0 (Oleg Genrus)
     * Various dependency bumps (multiple people)
     * Try all addresses returned by getAddrInfo (Fraser Tweedale)

--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2020/01/11 10:25:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2021/04/24 18:04:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.14
-BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.14
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.16
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.16
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
 .include "../../net/hs-network/buildlink3.mk"
