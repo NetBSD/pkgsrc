8a5d3dc00f7fa1ce6957abf6310aa9bd7677a4f0
pho <pho@pkgsrc.org>
2021-04-24 05:17:54 +0000

    Update to clock-0.8.2
    No change logs are provided by the upstream.

--- time/hs-clock/buildlink3.mk
+++ time/hs-clock/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 05:31:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:17:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-clock
 
 .if !defined(HS_CLOCK_BUILDLINK3_MK)
 HS_CLOCK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-clock+=	hs-clock>=0.8
-BUILDLINK_ABI_DEPENDS.hs-clock+=	hs-clock>=0.8
-BUILDLINK_PKGSRCDIR.hs-clock?=	../../time/hs-clock
+BUILDLINK_API_DEPENDS.hs-clock+=	hs-clock>=0.8.2
+BUILDLINK_ABI_DEPENDS.hs-clock+=	hs-clock>=0.8.2
+BUILDLINK_PKGSRCDIR.hs-clock?=		../../time/hs-clock
 .endif	# HS_CLOCK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-clock
