8c842d18d0174b3331f32895a9b8a3efcc510752
pho <pho@pkgsrc.org>
2021-04-24 15:21:03 +0000

    Update to doclayout-0.3.0.2
    0.3.0.2
    * NOINLINE literal instead of fromString (#2, sjakobi). This produces
      a further reduction in allocations and pandoc compile time.
    
    0.3.0.1
    * NOINLINE fromString (#1). @sjakobi reports that this change reduced
      total allocations for building pandoc-2.12 with GHC 8.10.4 by 8.5%
      and reduced peak allocations are reduced from 3854MB to 3389MB.

--- textproc/hs-doclayout/buildlink3.mk
+++ textproc/hs-doclayout/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 06:45:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:21:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-doclayout
 
 .if !defined(HS_DOCLAYOUT_BUILDLINK3_MK)
 HS_DOCLAYOUT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3
-BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3
+BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3.0
+BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3.0.2
 BUILDLINK_PKGSRCDIR.hs-doclayout?=	../../textproc/hs-doclayout
 
 .include "../../devel/hs-safe/buildlink3.mk"
