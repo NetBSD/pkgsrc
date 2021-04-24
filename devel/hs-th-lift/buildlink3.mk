a72dd19eb849ddf1eb7f41da1e95155d4c197e0b
pho <pho@pkgsrc.org>
2021-04-24 10:23:50 +0000

    Import th-lift-0.8.2

---
+++ devel/hs-th-lift/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/04/24 10:23:50 pho Exp $
+
+BUILDLINK_TREE+=	hs-th-lift
+
+.if !defined(HS_TH_LIFT_BUILDLINK3_MK)
+HS_TH_LIFT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.2
+BUILDLINK_ABI_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.2
+BUILDLINK_PKGSRCDIR.hs-th-lift?=	../../devel/hs-th-lift
+
+.include "../../devel/hs-th-abstraction/buildlink3.mk"
+.endif	# HS_TH_LIFT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-th-lift
