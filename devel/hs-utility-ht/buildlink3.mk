3f3cdc3915e45d50fec3f5dd8b89030d912b0068
pho <pho@pkgsrc.org>
2021-04-24 05:33:25 +0000

    Update to utility-ht-0.0.16
    No change logs are provided by the upstream.

--- devel/hs-utility-ht/buildlink3.mk
+++ devel/hs-utility-ht/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:42:57 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:33:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-utility-ht
 
 .if !defined(HS_UTILITY_HT_BUILDLINK3_MK)
 HS_UTILITY_HT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.15
-BUILDLINK_ABI_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.15
+BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.16
+BUILDLINK_ABI_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.16
 BUILDLINK_PKGSRCDIR.hs-utility-ht?=	../../devel/hs-utility-ht
 .endif	# HS_UTILITY_HT_BUILDLINK3_MK
 
