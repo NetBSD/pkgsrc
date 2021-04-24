04e2c7ce8c687acd035f0bb8fd80ffd77d444d7f
pho <pho@pkgsrc.org>
2021-04-24 13:16:34 +0000

    Update to network-uri-2.6.4.1
    network-uri-2.6.4.1 (2021-02-07)
    * Fix: Restore "Safe" designation which was accidentally removed.
    
    network-uri-2.6.4.0 (2021-02-07)
    * Add compatibility with GHC 9.0.1.
    
    network-uri-2.6.3.0 (2020-02-18)
    * Add official support for SafeHaskell NOTE: This is the first version
      whose SafeHaskell properties have become an intentional part of the
      API contract; previous versions were merely accidentally
      safe-inferred (or not depending on various factors; in other words,
      this was a fragile property).
    * Derive Lift instances using DeriveLift extension, when available.
    
    network-uri-2.6.2.0 (2020-01-30)
    * Mark the modules as Safe for SafeHaskell.
    
    network-uri-2.6.2.0 (2020-01-30)
    * Merge network-uri-static (Network.URI.Static) into this package,
      which offers a way to parse URI strings at compile time.
    * Add Lenses for the URI types
    * Add Generic instances for the URI type
    * Add Lift instances for the URI type
    * Optimize isReserved and related character-class functions.
    * Start to add some benchmarks for performance analysis
    * Fix a bug: Correctly parse IPv6 addresses in URIs.
    * Add rectify which normalizes a URI if it is missing certain
      separator characters required by the module. Some users found adding
      those characters inconvenient when building a URI from parts.
    * Add nullURIAuth and uriAuthToString, paralleling nullURI and
      uriToString.

--- net/hs-network-uri/buildlink3.mk
+++ net/hs-network-uri/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 10:08:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/24 13:16:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-network-uri
 
 .if !defined(HS_NETWORK_URI_BUILDLINK3_MK)
 HS_NETWORK_URI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network-uri+=	hs-network-uri>=2.6.1
-BUILDLINK_ABI_DEPENDS.hs-network-uri+=	hs-network-uri>=2.6.1.0
+BUILDLINK_API_DEPENDS.hs-network-uri+=	hs-network-uri>=2.6.4
+BUILDLINK_ABI_DEPENDS.hs-network-uri+=	hs-network-uri>=2.6.4.1
 BUILDLINK_PKGSRCDIR.hs-network-uri?=	../../net/hs-network-uri
+
+.include "../../devel/hs-th-compat/buildlink3.mk"
 .endif	# HS_NETWORK_URI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-network-uri
