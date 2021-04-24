1578340110fc5c17a940600cdcccd58024dc59b3
pho <pho@pkgsrc.org>
2021-04-24 05:25:25 +0000

    Update to tasty-1.4.1
    Version 1.4.1
    * Deduplicate command line options when there is more than one
      TestReporter. Expose the function that does that,
      uniqueOptionDescriptions.
    
    Version 1.4.0.3
    * Fix CPP warning/error macro expansion producing 'defined' has
      undefined behavior
    
    Version 1.4.0.2
    * Automatically disable the dependency on the clock package when
      compiled by ghcjs.
    
    Version 1.4.0.1
    * The only point of this release is to introduce compatibility with
      GHCs back to 7.0 (see https://github.com/feuerbach/tasty/pull/287).
    
      Note, however, that these changes are not merged to the master
      branch, and the future releases will only support the GHC/base
      versions from the last 5 years, as per our usual policy. To test
      with even older GHCs, you'll have to use this particular version of
      tasty (or have the constraint solver pick it for you when testing
      with older GHCs).
    
      The source of this release is in the support-old-ghcs branch of the
      tasty repository.
    
    Version 1.4
    * Change the TreeFold data type to give all functions access to
      OptionSet
    * Fix a bug where a looping failure message escaped the time out set
      for the test
    * Fix a bug where pattern changes inside the TestTree weren't
      respected
    
    Version 1.3.1
    * Add an ability for a test provider to print colorful/formatted
      output
    
    Version 1.3
    * IsOption has a new method showDefaultValue for customizing how
      defaultValues are rendered in the --help output.
    * Drop support for GHCs older than 5 years
    * Do not install handlers for the signals that dump core
    * Export the AnsiTricks type/option
    * In addition to a Parser, optionParser and suiteOptionParser now
      return a [String] representing warning messages:
      - A warning is emitted if an IsOption instance defines multiple
        options in the implementation of optionCLParser.
      - An warning is emitted if an IsOption instance's optionCLParser
        implementation assigns a default value (e.g., with
        Options.Applicative.value), as this interferes with tasty's
        ability to read environment variable arguments.

--- devel/hs-tasty/buildlink3.mk
+++ devel/hs-tasty/buildlink3.mk
@@ -1,13 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:51:09 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:25:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty
 
 .if !defined(HS_TASTY_BUILDLINK3_MK)
 HS_TASTY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.2.3
-BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.2.3
+BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.4.1
+BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.4.1
 BUILDLINK_PKGSRCDIR.hs-tasty?=		../../devel/hs-tasty
+
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-unbounded-delays/buildlink3.mk"
+.include "../../time/hs-clock/buildlink3.mk"
+.include "../../textproc/hs-wcwidth/buildlink3.mk"
 .endif	# HS_TASTY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tasty
