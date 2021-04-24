def506e5f2660449d7949a4ec7a23f98a4be5cf5
pho <pho@pkgsrc.org>
2021-04-24 14:40:31 +0000

    Update to cmark-gfm-0.2.2
    cmark-gfm 0.2.2 (16 Sep 2020)
    * Pull in upstream changes.

--- textproc/hs-cmark-gfm/buildlink3.mk
+++ textproc/hs-cmark-gfm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 06:36:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 14:40:31 pho Exp $
 
 BUILDLINK_TREE+=	hs-cmark-gfm
 
 .if !defined(HS_CMARK_GFM_BUILDLINK3_MK)
 HS_CMARK_GFM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.1
+BUILDLINK_API_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-cmark-gfm?=	../../textproc/hs-cmark-gfm
 
 .include "../../textproc/cmark-gfm/buildlink3.mk"
