7cd6983ba97c16146370fe348cf44f1602b617ce
pho <pho@pkgsrc.org>
2021-04-24 06:21:41 +0000

    Update to Glob-0.10.1
    0.10.1, 2020-07-19:
    	Updated code and dependencies to support dlist-1.0.
    
    	Cabal-Version minimum bumped to 1.10 as Hackage now rejects older minima.

--- textproc/hs-Glob/buildlink3.mk
+++ textproc/hs-Glob/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 12:15:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 06:21:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-Glob
 
 .if !defined(HS_GLOB_BUILDLINK3_MK)
 HS_GLOB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Glob+=	hs-Glob>=0.10.0
-BUILDLINK_ABI_DEPENDS.hs-Glob+=	hs-Glob>=0.10.0
+BUILDLINK_API_DEPENDS.hs-Glob+=	hs-Glob>=0.10.1
+BUILDLINK_ABI_DEPENDS.hs-Glob+=	hs-Glob>=0.10.1
 BUILDLINK_PKGSRCDIR.hs-Glob?=	../../textproc/hs-Glob
 
 .include "../../devel/hs-dlist/buildlink3.mk"
