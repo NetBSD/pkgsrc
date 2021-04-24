32ab2c94a61659cf29773802ad1450cd31ad99d1
pho <pho@pkgsrc.org>
2021-04-24 15:28:11 +0000

    Update to doctemplates-0.9
    0.9
    * Add BoolVal constructor to Val. This gives a smoother interface with
      JSON and YAML. [API change]
    * Remove overlapping instances by generalizing ToContext String String
      and FromContext String String to TemplateTarget [a] => ToContext [a]
      [a] and TemplateTarget [a] => FromContext [a] [a]. Remove the
      instance ToContext String (Doc String). Remove redundant
      constraints. (#9, favonia) [API change]
    
    0.8.3
    * Properly handle nested loops (#15). Previously "it" was always used
      for the variable in a loop, and in a nested loop there was no way to
      distinguish the value of the inner iteration from the value of the
      outer one. Now we assign the iterated value to both "it" and to the
      original variable name (e.g. "foo.bar"). This probably has a small
      negative performance impact. Note that this change also affects the
      output of the template parser: original variable names are now
      retained instead of being replaced by "it".
    * Remove duplicate IsString constraint (#14, Mario Lang).
    * Update haddocks from README (#10).
    * Minor code clean-ups (#7, favonia).
    * Add hsyaml >= 0.2 constraint (#6).
    
    0.8.2
    * Add filters: first, rest, last, allbutlast.
    * New constructors for Filter: FirstItem, LastItem, Rest, AllButLast
      [API change].

--- textproc/hs-doctemplates/buildlink3.mk
+++ textproc/hs-doctemplates/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 11:35:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:28:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-doctemplates
 
 .if !defined(HS_DOCTEMPLATES_BUILDLINK3_MK)
 HS_DOCTEMPLATES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.8.1
-BUILDLINK_ABI_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.8.1
+BUILDLINK_API_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.9
+BUILDLINK_ABI_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.9
 BUILDLINK_PKGSRCDIR.hs-doctemplates?=	../../textproc/hs-doctemplates
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -16,6 +16,7 @@ BUILDLINK_PKGSRCDIR.hs-doctemplates?=	../../textproc/hs-doctemplates
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../converters/hs-text-conversions/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_DOCTEMPLATES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-doctemplates
