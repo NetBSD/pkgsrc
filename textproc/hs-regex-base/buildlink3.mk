55d537e7332b7ad9c1f2adc55b2dcdfc61cac10a
pho <pho@pkgsrc.org>
2021-04-24 06:49:43 +0000

    Update to regex-base-0.94.0.1
    * Workaround for {-# LANGUAGE Haskell2010 #-} parser regression
      introduced in GHC 9.0
    * Optimization flag -O2 has been removed

--- textproc/hs-regex-base/buildlink3.mk
+++ textproc/hs-regex-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 09:10:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/04/24 06:49:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-base
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-regex-base
 HS_REGEX_BASE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0
-BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0.0
+BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0.1
 BUILDLINK_PKGSRCDIR.hs-regex-base?=	../../textproc/hs-regex-base
 .endif	# HS_REGEX_BASE_BUILDLINK3_MK
 
