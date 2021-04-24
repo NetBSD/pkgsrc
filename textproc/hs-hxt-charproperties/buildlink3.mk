e074f90fc30d2385496198df737ea3e757212b43
pho <pho@pkgsrc.org>
2021-04-24 15:42:57 +0000

    Update to hxt-charproperties-9.5.0.0
    No change logs are provided by the upstream.

--- textproc/hs-hxt-charproperties/buildlink3.mk
+++ textproc/hs-hxt-charproperties/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 15:46:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:42:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-hxt-charproperties
 
 .if !defined(HS_HXT_CHARPROPERTIES_BUILDLINK3_MK)
 HS_HXT_CHARPROPERTIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.4.0
-BUILDLINK_ABI_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.4.0.0
+BUILDLINK_API_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.5.0
+BUILDLINK_ABI_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.5.0.0
 BUILDLINK_PKGSRCDIR.hs-hxt-charproperties?=	../../textproc/hs-hxt-charproperties
 .endif	# HS_HXT_CHARPROPERTIES_BUILDLINK3_MK
 
