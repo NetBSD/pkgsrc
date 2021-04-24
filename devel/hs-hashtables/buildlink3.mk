42d7cd77f3d8c33a952130d6121f872fbdc1872e
pho <pho@pkgsrc.org>
2021-04-24 04:49:49 +0000

    Update to hashtables-1.2.4.1
    1.2.4.1
    * Update some test suite dep upper bounds.
    
    1.2.4.0
    * Fix a correctness bug with cuckoo hash tables and the new mutate
      function introduced in 1.2.3.0.
    * Bring test suite into main .cabal file

--- devel/hs-hashtables/buildlink3.mk
+++ devel/hs-hashtables/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:40:26 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 04:49:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashtables
 
 .if !defined(HS_HASHTABLES_BUILDLINK3_MK)
 HS_HASHTABLES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashtables+=	hs-hashtables>=1.2.3.4
-BUILDLINK_ABI_DEPENDS.hs-hashtables+=	hs-hashtables>=1.2.3.4
+BUILDLINK_API_DEPENDS.hs-hashtables+=	hs-hashtables>=1.2.4
+BUILDLINK_ABI_DEPENDS.hs-hashtables+=	hs-hashtables>=1.2.4.1
 BUILDLINK_PKGSRCDIR.hs-hashtables?=	../../devel/hs-hashtables
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_HASHTABLES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hashtables
