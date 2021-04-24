fb0890d3a628769bff8172913cc20410679d3db2
pho <pho@pkgsrc.org>
2021-04-24 15:44:55 +0000

    Update to hxt-regex-xmlschema-9.2.0.7
    No change logs are provided by the upstream.

--- textproc/hs-hxt-regex-xmlschema/buildlink3.mk
+++ textproc/hs-hxt-regex-xmlschema/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 15:52:41 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:44:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-hxt-regex-xmlschema
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-hxt-regex-xmlschema
 HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.2.0
-BUILDLINK_ABI_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.2.0.3
+BUILDLINK_ABI_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.2.0.7
 BUILDLINK_PKGSRCDIR.hs-hxt-regex-xmlschema?=	../../textproc/hs-hxt-regex-xmlschema
 
 .include "../../textproc/hs-hxt-charproperties/buildlink3.mk"
