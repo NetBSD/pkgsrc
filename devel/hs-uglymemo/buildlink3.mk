559d33f87cddcd8aee2ea2bd457f43e6ed4138da
pho <pho@pkgsrc.org>
2021-04-24 05:29:49 +0000

    Update PLIST

--- devel/hs-uglymemo/buildlink3.mk
+++ devel/hs-uglymemo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:41:41 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 05:31:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-uglymemo
 
 .if !defined(HS_UGLYMEMO_BUILDLINK3_MK)
 HS_UGLYMEMO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uglymemo+=	hs-uglymemo>=0.1.0.1
+BUILDLINK_API_DEPENDS.hs-uglymemo+=	hs-uglymemo>=0.1.0
 BUILDLINK_ABI_DEPENDS.hs-uglymemo+=	hs-uglymemo>=0.1.0.1
 BUILDLINK_PKGSRCDIR.hs-uglymemo?=	../../devel/hs-uglymemo
 .endif	# HS_UGLYMEMO_BUILDLINK3_MK
