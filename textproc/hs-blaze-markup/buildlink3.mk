05918bc166dd199e42c34702686c85810d63c0f3
pho <pho@pkgsrc.org>
2021-04-24 05:59:54 +0000

    Update to blaze-markup-0.8.2.8
    - 0.8.2.8 (2021-03-04)
        * Bump `base` upper bound to 4.16
        * Bump `tasty` upper bound to 1.5
        * Bump `bytestring` upper bound to 0.12
    
    - 0.8.2.7 (2020-06-30)
        * Bump Cabal version lower bound to 1.10
    
    - 0.8.2.6 (2020-06-30)
        * Bump `tasty` upper bound to 1.4
    
    - 0.8.2.5 (2020-04-20)
        * Bump `base` upper bound to 4.15
    
    - 0.8.2.4 (2020-03-29)
        * Bump `QuickCheck` upper bound to 2.15

--- textproc/hs-blaze-markup/buildlink3.mk
+++ textproc/hs-blaze-markup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/01/11 11:15:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/04/24 05:59:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-markup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-blaze-markup
 HS_BLAZE_MARKUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.8.2
-BUILDLINK_ABI_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.8.2.3
+BUILDLINK_ABI_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.8.2.8
 BUILDLINK_PKGSRCDIR.hs-blaze-markup?=	../../textproc/hs-blaze-markup
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
