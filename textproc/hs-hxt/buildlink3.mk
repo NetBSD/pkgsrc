1796ddea2e75ba4d1f1f2803ad471d6eae7144b1
pho <pho@pkgsrc.org>
2021-04-24 15:50:15 +0000

    Update to hxt-9.3.1.22
    Changes from 9.3.1.21: ghc-9.0 compatibility
    
    Changes from 9.3.1.20: ghc 8.10 and 9.0 compatibility, tuple picker up
    to 24-tuples, Either instance for xpickle
    
    Changes from 9.3.1.19: ghc-8.8.2 compatibility

--- textproc/hs-hxt/buildlink3.mk
+++ textproc/hs-hxt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 16:21:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:50:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-hxt
 
@@ -6,13 +6,12 @@ BUILDLINK_TREE+=	hs-hxt
 HS_HXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=9.3.1
-BUILDLINK_ABI_DEPENDS.hs-hxt+=	hs-hxt>=9.3.1.18
+BUILDLINK_ABI_DEPENDS.hs-hxt+=	hs-hxt>=9.3.1.22
 BUILDLINK_PKGSRCDIR.hs-hxt?=	../../textproc/hs-hxt
 
 .include "../../textproc/hs-hxt-charproperties/buildlink3.mk"
 .include "../../textproc/hs-hxt-regex-xmlschema/buildlink3.mk"
 .include "../../textproc/hs-hxt-unicode/buildlink3.mk"
-.include "../../net/hs-network/buildlink3.mk"
 .include "../../net/hs-network-uri/buildlink3.mk"
 .endif	# HS_HXT_BUILDLINK3_MK
 
