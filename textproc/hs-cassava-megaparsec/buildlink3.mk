61f484db129e47482a79ae791b157ca102a7fc24
pho <pho@pkgsrc.org>
2021-04-24 06:18:24 +0000

    Update to cassava-megaparsec-2.0.2
    * Add suppport for megaparsec-9.0.0

--- textproc/hs-cassava-megaparsec/buildlink3.mk
+++ textproc/hs-cassava-megaparsec/buildlink3.mk
@@ -1,13 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/30 16:49:55 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 06:18:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-cassava-megaparsec
 
 .if !defined(HS_CASSAVA_MEGAPARSEC_BUILDLINK3_MK)
 HS_CASSAVA_MEGAPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.0.1
-BUILDLINK_ABI_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.0.1
+BUILDLINK_API_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.0.2
+BUILDLINK_ABI_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.0.2
 BUILDLINK_PKGSRCDIR.hs-cassava-megaparsec?=	../../textproc/hs-cassava-megaparsec
+
+.include "../../textproc/hs-cassava/buildlink3.mk"
+.include "../../textproc/hs-megaparsec/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_CASSAVA_MEGAPARSEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-cassava-megaparsec
