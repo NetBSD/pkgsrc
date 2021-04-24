65fda1d7f64d9eaf0283bd5e56554bf041fe2af1
pho <pho@pkgsrc.org>
2021-04-24 15:33:39 +0000

    Update to hslua-module-text-0.3.0.1
    0.3.0.1 -- 2020-10-16
    * Relaxed upper bound for hslua, allow hslua-1.3.*.
    
    0.3.0 -- 2020-08-15
    * Use self-documenting module. This allows to include documentation
      with the module definition, and to auto-generate documentation from
      that. Requires hslua-1.2.0 or newer.
    
    * Run CI tests with all GHC 8 versions, test stack builds.

--- textproc/hs-hslua-module-text/buildlink3.mk
+++ textproc/hs-hslua-module-text/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 15:51:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/24 15:33:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-text
 
 .if !defined(HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK)
 HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.2.1
+BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.3.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.3.0.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-text?=	../../textproc/hs-hslua-module-text
 
 .include "../../lang/hs-hslua/buildlink3.mk"
