c2c165346d4df777bbe0c61179536def94a67e92
pho <pho@pkgsrc.org>
2021-04-25 11:39:03 +0000

    Update to idris-1.3.3
    * Updated to work with GHC 8.8 and cabal 3.0
    * Bugfixes and documentation updates

--- lang/idris/patches/patch-src_Idris_Parser_Stack.hs
+++ lang/idris/patches/patch-src_Idris_Parser_Stack.hs
@@ -1,30 +1,15 @@
-$NetBSD: patch-src_Idris_Parser_Stack.hs,v 1.1 2020/01/20 23:32:38 pho Exp $
+$NetBSD: patch-src_Idris_Parser_Stack.hs,v 1.2 2021/04/25 11:39:03 pho Exp $
 
-Fix build with recent megaparsec.
+Fix build with megaparsec-9.0.*.
 
---- src/Idris/Parser/Stack.hs.orig	2020-01-20 13:28:28.122727647 +0000
+--- src/Idris/Parser/Stack.hs.orig	2021-04-25 10:29:33.784393017 +0000
 +++ src/Idris/Parser/Stack.hs
-@@ -76,11 +76,12 @@ parseErrorOffset = P.errorOffset . parse
- instance Message ParseError where
-   messageExtent err = sourcePositionFC pos
-     where
--      (pos, _) = P.reachOffsetNoLine (parseErrorOffset err) (parseErrorPosState err)
-+      pos = P.pstateSourcePos $
-+            P.reachOffsetNoLine (parseErrorOffset err) (parseErrorPosState err)
+@@ -84,7 +84,7 @@ instance Message ParseError where
+       (pos, _) = P.reachOffsetNoLine (parseErrorOffset err) (parseErrorPosState err)
+ #endif
    messageText = PP.text . init . P.parseErrorTextPretty . parseError
-   messageSource err = Just sline
+-  messageSource err = Just sline
++  messageSource err = sline
      where
--      (_, sline, _) = P.reachOffset (parseErrorOffset err) (parseErrorPosState err)
-+      (sline, _) = P.reachOffset (parseErrorOffset err) (parseErrorPosState err)
- 
- -- | A fully formatted parse error, with caret and bar, etc.
- prettyError :: ParseError -> String
-@@ -88,7 +89,7 @@ prettyError =  P.errorBundlePretty . unP
- 
- {- * Mark and restore -}
- 
--type Mark = P.State String
-+type Mark = P.State String Void
- 
- -- | Retrieve the parser state so we can restart from this point later.
- mark :: Parsing m => m Mark
+ #if MIN_VERSION_megaparsec(8,0,0)
+       (sline, _) = P.reachOffset (parseErrorOffset err) (parseErrorPosState err)
