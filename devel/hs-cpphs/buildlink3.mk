789d73c9fedc8e96b9a495823a7b55f280c5ee2c
pho <pho@pkgsrc.org>
2021-04-25 15:28:44 +0000

    Update to cpphs-1.20.9.1
    No change logs are provided by the upstream.

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/01/02 03:27:12 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/04/25 15:28:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.20.8
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.20.8
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.20.9
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.20.9.1
 BUILDLINK_PKGSRCDIR.hs-cpphs?=		../../devel/hs-cpphs
 
-.include "../../devel/hs-old-locale/buildlink3.mk"
-.include "../../time/hs-old-time/buildlink3.mk"
 .include "../../textproc/hs-polyparse/buildlink3.mk"
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
