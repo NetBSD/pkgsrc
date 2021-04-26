c816b7ce20442f5e2b48b54c13667a31980e077c
schmonz <schmonz@pkgsrc.org>
2021-04-26 15:05:09 +0000

    Update to 2.3.5.1. From the changelog:
    - Libraries and binaries don't have the .note.GNU-stack section stripped
      anymore. Previously, "make strip" would strip that section, which
      would sometimes (depending on the toolchain) cause binaries to be
      incorrectly tagged as needing an executable stack. This is not a
      security issue in itself, but an executable stack makes it easier for
      an attacker to turn bugs into exploits, so it should be avoided
      whenever possible. Thanks to Xavier Stonestreet for reporting and
      finding the cause of the problem.
    
    - Link tests are now performed with a regular file as their
      output, instead of /dev/null, which makes them more portable to
      old/buggy linkers.
    
    Bump default BUILDLINK_API_DEPENDS to match.

--- net/s6-dns/buildlink3.mk
+++ net/s6-dns/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/11/19 20:05:54 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/04/26 15:05:09 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-dns
 
 .if !defined(S6_DNS_BUILDLINK3_MK)
 S6_DNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.3.0
+BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.5.1
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../net/s6-dns
 BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
 BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
