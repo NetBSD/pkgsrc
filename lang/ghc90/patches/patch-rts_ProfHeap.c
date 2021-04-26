cf0ff7c5f19bcc82fdccefc1f4094c347baed581
pho <pho@pkgsrc.org>
2021-04-26 07:41:21 +0000

    Add a bootkit for FreeBSD-*-i386

--- lang/ghc90/patches/patch-rts_ProfHeap.c
+++ lang/ghc90/patches/patch-rts_ProfHeap.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-rts_ProfHeap.c,v 1.2 2021/04/22 14:20:24 pho Exp $
+$NetBSD: patch-rts_ProfHeap.c,v 1.3 2021/04/26 07:41:21 pho Exp $
 
 NetBSD does not have uselocale(3).
 
 --- rts/ProfHeap.c.orig	2021-01-25 16:30:28.000000000 +0000
 +++ rts/ProfHeap.c
-@@ -69,7 +69,7 @@ static locale_t prof_locale = 0, saved_l
+@@ -69,7 +69,7 @@ static locale_t prof_locale = 0, saved_locale = 0;
  STATIC_INLINE void
  init_prof_locale( void )
  {
@@ -32,16 +32,12 @@ NetBSD does not have uselocale(3).
  #else
      saved_locale = uselocale(prof_locale);
  #endif
-@@ -106,9 +109,11 @@ set_prof_locale( void )
- STATIC_INLINE void
- restore_locale( void )
+@@ -108,6 +111,8 @@ restore_locale( void )
  {
--#if defined(mingw32_HOST_OS)
-+#if !defined(mingw32_HOST_OS) && !defined(netbsd_HOST_OS)
+ #if defined(mingw32_HOST_OS)
      _configthreadlocale(prof_locale_per_thread);
-     setlocale(LC_NUMERIC, saved_locale);
-+#elif defined(netbsd_HOST_OS)
 +    setlocale(LC_NUMERIC, saved_locale);
++#elif defined(netbsd_HOST_OS)
+     setlocale(LC_NUMERIC, saved_locale);
  #else
      uselocale(saved_locale);
- #endif
