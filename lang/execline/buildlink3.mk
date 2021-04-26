14b0563a6c33c33c64cd525474bd0668bed4038a
schmonz <schmonz@pkgsrc.org>
2021-04-26 15:02:52 +0000

    Update to 2.8.0.1. From the changelog:
    - Libraries and binaries don't have the .note.GNU-stack section stripped
      anymore. Previously, "make strip" would strip that section, which
      would sometimes (depending on the toolchain) cause binaries to be
      incorrectly tagged as needing an executable stack. This is not a
      security issue in itself, but an executable stack makes it easier for
      an attacker to turn bugs into exploits, so it should be avoided
      whenever possible. Thanks to Xavier Stonestreet for reporting and
      finding the cause of the problem.
    
    - Link tests are now performed with a regular file as their
      output, instead of /dev/null, which makes them more portable to
      old/buggy linkers.
    
    Bump default BUILDLINK_API_DEPENDS to match.

--- lang/execline/buildlink3.mk
+++ lang/execline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/01/11 18:23:54 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/04/26 15:02:52 schmonz Exp $
 
 BUILDLINK_TREE+=	execline
 
 .if !defined(EXECLINE_BUILDLINK3_MK)
 EXECLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.execline+=	execline>=2.7.0.0
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.8.0.1
 BUILDLINK_PKGSRCDIR.execline?=		../../lang/execline
 BUILDLINK_DEPMETHOD.execline?=		build
 .endif # EXECLINE_BUILDLINK3_MK
