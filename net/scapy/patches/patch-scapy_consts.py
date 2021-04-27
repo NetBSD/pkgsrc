fb7dd5ca57b26e7634da262431d3bb7da53c3b1c
gutteridge <gutteridge@pkgsrc.org>
2021-04-27 21:53:11 +0000

    scapy: update to 2.4.5
    Changelog
    Core
    
        354 commits to master since v2.4.4, from 73 contributors
        Python 3.9 support
        New interfaces system. conf.iface is now an object (retro-compatible as a string) which contains additional information about the interface and allows for an automatic selection of the socket type. conf.ifaces now lists all available interfaces.
        Fix *BSD support. Improve filters handling on Linux and libpcap
        Automaton: support for STOP event - allows to cleanly end an Automata. Implemented in all Scapy's automatons
        [Deprecated] Naming different fields with the same name will now raise a deprecation warning. This behavior has never properly worked and should never be necessary
        Enhance Net and Net6
        Improvements to scapy's logging, colored output and to UTscapy
        Fix edge-cases with ConditionalField and MultipleTypeField to make them more resilient
        [Doc] Enhancements: improved MultipleTypeField handling, add view source.
        [Internal] Move the test suite to GitHub Actions
        [Internal] Unit test housekeeping
        [Internal] Begin type hinting
    
    Layers
    
        6LoWPAN refactor
        TLS improvements (TLS 1.3 server downgrade, TLS 1.2 EXT MS...)
        HTTP improvements (http_request, support for zstd...)
        Refactor TunTap support
        Cleanup MACsec
        Many small bug fixes or improvements to layers (SMB2, BFD, DNS, Zigbee, EAP, HomeplugGP, DHCPv6, 802.11...)
    
    Automotive
    
        Renaming of ENET to HSFZ
        Added XCP layer
        Added DoIP layer
        [Internal] Cleanup of interface preparation code in unit tests
        Renaming of ECU to Ecu
        Refactoring of EcuState class to be more versatile
        [Internal] Started with typing
        [Internal] Multiple minor cleanups
        Minor updates to the documentation

--- net/scapy/patches/patch-scapy_consts.py
+++ net/scapy/patches/patch-scapy_consts.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-scapy_consts.py,v 1.2 2021/03/21 19:50:54 gutteridge Exp $
+$NetBSD: patch-scapy_consts.py,v 1.3 2021/04/27 21:53:11 gutteridge Exp $
 
 Add DragonFly support.
 
---- scapy/consts.py.orig	2020-09-02 11:26:21.000000000 +0000
+--- scapy/consts.py.orig	2021-04-18 18:36:15.000000000 +0000
 +++ scapy/consts.py
-@@ -14,7 +14,8 @@ DARWIN = platform.startswith("darwin")
+@@ -18,7 +18,8 @@ DARWIN = platform.startswith("darwin")
  SOLARIS = platform.startswith("sunos")
  WINDOWS = platform.startswith("win32")
  WINDOWS_XP = platform_lib.release() == "XP"
@@ -13,4 +13,4 @@ Add DragonFly support.
 +BSD = DARWIN or FREEBSD or OPENBSD or NETBSD or DRAGONFLY
  # See https://docs.python.org/3/library/platform.html#cross-platform
  IS_64BITS = maxsize > 2**32
- # LOOPBACK_NAME moved to conf.loopback_name
+ BIG_ENDIAN = byteorder == 'big'
