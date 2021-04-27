1b35bdf5534c1f960bd7dae3c52c74393897e156
gutteridge <gutteridge@pkgsrc.org>
2021-04-27 21:53:11 +0000

    scapy: update to 2.4.5
    Changelog
    Core
    
        354 commits to master since v2.4.4, from 73 contributors
        Python 3.9 support
        New interfaces system. conf.iface is now an object (retro-compatible as a string) which contains additional information about the interface and allows for an automatic selection of the socket type. conf.ifaces now lists all available interfaces.
        Fix *BSD support. Improve filters handling on Linux and libpcap
        Automaton: support for STOP event - allows to cleanly end an Automata. Implemented in all Scapy's automatons
        [Deprecated] Naming different fields with the same name will now raise a deprecation warning. This behavior has never properly worked and should never be necessary
        Enhance Net and Net6
        Improvements to scapy's logging, colored output and to UTscapy
        Fix edge-cases with ConditionalField and MultipleTypeField to make them more resilient
        [Doc] Enhancements: improved MultipleTypeField handling, add view source.
        [Internal] Move the test suite to GitHub Actions
        [Internal] Unit test housekeeping
        [Internal] Begin type hinting
    
    Layers
    
        6LoWPAN refactor
        TLS improvements (TLS 1.3 server downgrade, TLS 1.2 EXT MS...)
        HTTP improvements (http_request, support for zstd...)
        Refactor TunTap support
        Cleanup MACsec
        Many small bug fixes or improvements to layers (SMB2, BFD, DNS, Zigbee, EAP, HomeplugGP, DHCPv6, 802.11...)
    
    Automotive
    
        Renaming of ENET to HSFZ
        Added XCP layer
        Added DoIP layer
        [Internal] Cleanup of interface preparation code in unit tests
        Renaming of ECU to Ecu
        Refactoring of EcuState class to be more versatile
        [Internal] Started with typing
        [Internal] Multiple minor cleanups
        Minor updates to the documentation

--- net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
+++ net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.3 2021/03/23 02:03:11 gutteridge Exp $
+$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.4 2021/04/27 21:53:11 gutteridge Exp $
 
 Add DragonFly support.
 Fix NetBSD 32-bit architecture alignment.
 
---- scapy/arch/bpf/supersocket.py.orig	2020-09-02 11:26:21.000000000 +0000
+--- scapy/arch/bpf/supersocket.py.orig	2021-04-18 18:36:15.000000000 +0000
 +++ scapy/arch/bpf/supersocket.py
-@@ -18,18 +18,16 @@ from scapy.arch.bpf.consts import BIOCGB
+@@ -18,7 +18,7 @@ from scapy.arch.bpf.consts import BIOCGB
      BIOCIMMEDIATE, BIOCPROMISC, BIOCSBLEN, BIOCSETIF, BIOCSHDRCMPLT, \
      BPF_BUFFER_LENGTH, BIOCSDLT, DLT_IEEE802_11_RADIO
  from scapy.config import conf
@@ -13,20 +13,21 @@ Fix NetBSD 32-bit architecture alignment.
 +from scapy.consts import FREEBSD, NETBSD, DARWIN, DRAGONFLY
  from scapy.data import ETH_P_ALL
  from scapy.error import Scapy_Exception, warning
- from scapy.supersocket import SuperSocket
- from scapy.compat import raw
+ from scapy.interfaces import network_name
+@@ -27,10 +27,10 @@ from scapy.compat import raw
+ from scapy.layers.l2 import Loopback
  
  
 -if FREEBSD:
-+if FREEBSD or NETBSD or DRAGONFLY:
++if FREEBSD or NETBSD:
      # On 32bit architectures long might be 32bit.
      BPF_ALIGNMENT = sizeof(c_long)
 -elif NETBSD:
--    BPF_ALIGNMENT = 8  # sizeof(long)
++elif DRAGONFLY:
+     BPF_ALIGNMENT = 8  # sizeof(long)
  else:
      BPF_ALIGNMENT = 4  # sizeof(int32_t)
- 
-@@ -283,6 +281,9 @@ class L2bpfListenSocket(_L2bpfSocket):
+@@ -295,6 +295,9 @@ class L2bpfListenSocket(_L2bpfSocket):
                  bh_tstamp_offset = 16
          elif NETBSD:
              # struct bpf_hdr or struct bpf_hdr32
