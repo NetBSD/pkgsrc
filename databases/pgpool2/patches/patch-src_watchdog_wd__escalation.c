1f59e9ec2ef3a43c2604ba7009ee44c743db7282
wiz <wiz@pkgsrc.org>
2021-04-28 09:18:55 +0000

    pgpool2: update to 4.2.2.
    Based on a diff provided by Mustafa Dogan.
    
    4.2.1 and 4.2.2 are bugfix releases.
    
    4.2 contains new features and enhancements, including:
    
        Some items in the configuration file pgpool.conf are vastly
        enhanced for easier configuration and administration.
    
        Implement logging_collector for easier log management.
    
        Implement log_disconnections to collect disconnection logs.
    
        Implement pg_enc and pg_md5 to allow to register multiple
        passwords at once.
    
        Allow to show statistics of health check by using SHOW
        POOL_HEALTH_CHECK_STATS command, and also allow to show statistics
        of issued SQL by using SHOW POOL_BACKEND_STATS command.
    
        New PCP command pcp_reload_config is added.
    
        Now it is possible to omit write_function_list and
        read_only_function_list by looking at system catalog information.
    
        Add new clustering mode snapshot_isolation_mode which guarantees
        not only data modifications to multiple PostgreSQL but read
        consistency.
    
        Support LDAP authentication between clients and Pgpool-II.
    
        Add ssl_crl_file and ssl_passphrase_command to SSL configuration.
    
        Import PostgreSQL 13's SQL parser.

--- databases/pgpool2/patches/patch-src_watchdog_wd__escalation.c
+++ databases/pgpool2/patches/patch-src_watchdog_wd__escalation.c
@@ -1,16 +1,17 @@
-$NetBSD: patch-src_watchdog_wd__escalation.c,v 1.1 2016/03/11 21:30:55 fhajny Exp $
+$NetBSD: patch-src_watchdog_wd__escalation.c,v 1.2 2021/04/28 09:18:55 wiz Exp $
 
 Add sys/wait.h unconditionally, same as other source files.
 
---- src/watchdog/wd_escalation.c.orig	2016-02-10 08:53:31.000000000 +0000
-+++ src/watchdog/wd_escalation.c
-@@ -27,9 +27,7 @@
+--- src/watchdog/wd_escalation.c.orig	2021-04-24 00:49:41.737572235 +0300
++++ src/watchdog/wd_escalation.c	2021-04-24 00:49:56.140555487 +0300
+@@ -26,10 +26,7 @@
+ #include <stdlib.h>
  #include <unistd.h>
  #include <errno.h>
- 
+-
 -#ifdef __FreeBSD__
  #include <sys/wait.h>
 -#endif
  
- #include "pool.h"
+ #include "utils/pool_signal.h"
  #include "utils/elog.h"
