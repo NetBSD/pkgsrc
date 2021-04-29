9c40fd5607ca7a198a185a3abb0d8ee50b12d2f4
thor <thor@pkgsrc.org>
2021-04-29 23:30:12 +0000

    math/fftw and dependents: merge math/fftwf backin and add MPI and OpenMP
    This yields one common package to provide libfftw3 (double) and libfftw3f
    (single) by default and optionally the Fortran bindings as well as
    OpenMP and MPI variants, and those with long double or quad precision.
    
    This changes all packages depending on fftwf in the same commit to minimize
    the time of inconsistency. Soon, math/fftwf will disappear.

--- math/fftw/buildlink3.mk
+++ math/fftw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:56 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2021/04/29 23:30:15 thor Exp $
 
 BUILDLINK_TREE+=	fftw
 
@@ -8,6 +8,13 @@ FFTW_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.fftw+=	fftw>=3.0
 BUILDLINK_ABI_DEPENDS.fftw+=	fftw>=3.1nb1
 BUILDLINK_PKGSRCDIR.fftw?=	../../math/fftw
+
+pkgbase := fftw
+.include "../../mk/pkg-build-options.mk"
+.if ${PKG_BUILD_OPTIONS.fftw:Mmpi}
+.include "../../mk/mpi.buildlink3.mk"
+.endif
+
 .endif # FFTW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fftw
