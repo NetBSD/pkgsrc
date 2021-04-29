c8eba2dc7f48342cc6fbb53cebdc01a6df9c30c2
taca <taca@pkgsrc.org>
2021-04-29 15:21:16 +0000

    net/samba4: update to 4.14.4
    pkgsrc changes: remove extra spaces in some patch files.
    
    
                       ==============================
                       Release Notes for Samba 4.14.4
                               April 29, 2021
                       ==============================
    
    
    This is a security release in order to address the following defect:
    
    o CVE-2021-20254: Negative idmap cache entries can cause incorrect group entries
      in the Samba file server process token.
    
    
    =======
    Details
    =======
    
    o  CVE-2021-20254:
       The Samba smbd file server must map Windows group identities (SIDs) into unix
       group ids (gids). The code that performs this had a flaw that could allow it
       to read data beyond the end of the array in the case where a negative cache
       entry had been added to the mapping cache. This could cause the calling code
       to return those values into the process token that stores the group
       membership for a user.
    
       Most commonly this flaw caused the calling code to crash, but an alert user
       (Peter Eriksson, IT Department, Linköping University) found this flaw by
       noticing an unprivileged user was able to delete a file within a network
       share that they should have been disallowed access to.
    
       Analysis of the code paths has not allowed us to discover a way for a
       remote user to be able to trigger this flaw reproducibly or on demand,
       but this CVE has been issued out of an abundance of caution.
    
    
    Changes since 4.14.3
    --------------------
    
    o  Volker Lendecke <vl@samba.org>
       * BUG 14571: CVE-2021-20254: Fix buffer overrun in sids_to_unixids().

--- net/samba4/patches/patch-lib_pthreadpool_pthreadpool.c
+++ net/samba4/patches/patch-lib_pthreadpool_pthreadpool.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_pthreadpool_pthreadpool.c,v 1.1 2019/09/05 12:39:56 hauke Exp $
+$NetBSD: patch-lib_pthreadpool_pthreadpool.c,v 1.2 2021/04/29 15:21:16 taca Exp $
 
 On SunOS (OpenSolaris), pthread_mutex_init() expects a zeroed-out
 mutex data structure
-    
+
 --- lib/pthreadpool/pthreadpool.c.orig	2019-01-15 10:07:00.000000000 +0000
 +++ lib/pthreadpool/pthreadpool.c
 @@ -153,6 +153,7 @@ int pthreadpool_init(unsigned max_thread
