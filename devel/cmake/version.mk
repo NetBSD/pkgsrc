ece05e340bb0c93ecdbf7cedd14da4137a6dc753
adam <adam@pkgsrc.org>
2021-04-30 06:45:13 +0000

    cmake: updated to 3.20.2
    CMake 3.20.2
    * ci: pay attention to the machine load when running tests
    * ci: limit builds by machine load
    * Help: Add 3.20 release note for error on unknown arguments
    * gitlab-ci: equally delay all jobs on integration branches
    * Autogen: Restore mocs_compilation in OBJECT libraries
    * Help: Fix typos in cmake-compile-features(7)
    * Help: Do not recommend WCDH in cmake-compile-features(7)
    * Intel: Update Classic compiler version detection for 2021
    * IntelLLVM: Add special case for ifx 2021.1 version extraction
    * Help: Behavior of file(TO_NATIVE_PATH) depends on the host platform
    * Help: Document special cases for if(IS_ABSOLUTE)
    * Add missing 'not' in error messages
    * KWIML 2021-04-21 (49d91529)
    * autogen: fix race in depfile parsing
    * Ninja Multi-Config: Correctly generate POST_BUILD custom targets
    * BinUtils: Restore toolchain prefix detection from compiler name 'c++'
    * OpenBSD: Fix system feature definitions

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.13 2021/04/10 08:35:12 adam Exp $
+# $NetBSD: version.mk,v 1.14 2021/04/30 06:45:14 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.20.1
+CMAKE_VERSION=	3.20.2
 CMAKE_API=	${CMAKE_VERSION:R}
