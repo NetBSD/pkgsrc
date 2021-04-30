72e3030593fc6bc48e1cfd75f3fedf609a96f060
adam <adam@pkgsrc.org>
2021-04-30 10:32:17 +0000

    abseil: updated to 20210324.1
    Abseil LTS 20210324.1
    
    What's New:
    
    The cleanup library has been released. This library contains the control-flow-construct-like type absl::Cleanup which is used for executing a callback on scope exit.
    The numeric library now includes bits.h, a polyfill header containing implementations of C++20's bitwise math functions.
    Abseil now installs pkg-config files to make it easier to use Abseil with some other build systems.
    Abseil now respects the default CMake installation paths. Standard CMake variables like CMAKE_INSTALL_PREFIX can be used to change the installation path.
    
    Breaking Changes:
    
    The empty absl::container target has been removed from the CMake build. This target had no effect and references to this target in user code can safely be removed.

--- devel/abseil/patches/patch-absl_base_config.h
+++ devel/abseil/patches/patch-absl_base_config.h
@@ -1,18 +1,18 @@
-$NetBSD: patch-absl_base_config.h,v 1.1 2020/03/15 22:25:56 adam Exp $
+$NetBSD: patch-absl_base_config.h,v 1.2 2021/04/30 10:32:17 adam Exp $
 
 Add support for NetBSD.
 
---- absl/base/config.h.orig	2020-03-15 11:32:41.206104135 +0000
+--- absl/base/config.h.orig	2021-04-20 16:55:14.000000000 +0000
 +++ absl/base/config.h
-@@ -365,6 +365,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
+@@ -370,6 +370,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
  #ifdef ABSL_HAVE_MMAP
  #error ABSL_HAVE_MMAP cannot be directly set
  #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) ||   \
 +    defined(__NetBSD__) || \
      defined(__ros__) || defined(__native_client__) || defined(__asmjs__) || \
      defined(__wasm__) || defined(__Fuchsia__) || defined(__sun) || \
-     defined(__ASYLO__)
-@@ -378,6 +379,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
+     defined(__ASYLO__) || defined(__myriad2__)
+@@ -383,6 +384,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
  #ifdef ABSL_HAVE_PTHREAD_GETSCHEDPARAM
  #error ABSL_HAVE_PTHREAD_GETSCHEDPARAM cannot be directly set
  #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || \
