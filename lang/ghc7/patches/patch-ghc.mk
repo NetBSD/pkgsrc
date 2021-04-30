792d8065306aafd2e1f96d02db73c8e11930bb95
pho <pho@pkgsrc.org>
2021-04-30 18:52:05 +0000

    Backport the $ORIGIN removal patch from ghc88

--- lang/ghc7/patches/patch-ghc.mk
+++ lang/ghc7/patches/patch-ghc.mk
@@ -1,10 +1,34 @@
-$NetBSD: patch-ghc.mk,v 1.2 2019/12/29 16:59:09 pho Exp $
+$NetBSD: patch-ghc.mk,v 1.3 2021/04/30 18:52:05 pho Exp $
 
-Fix building bootkits: we don't generate documentation for them.
+* Use the wrapper scripts for ghc as we can't use the ones from the DESTDIR
+  as the libraries are not in the right place yet.
 
---- ghc.mk.orig	2019-12-27 02:36:02.858039422 +0000
+* Fix building bootkits: we don't generate documentation for them.
+
+--- ghc.mk.orig	2015-10-19 15:59:58.000000000 +0000
 +++ ghc.mk
-@@ -989,7 +989,6 @@ $(eval $(call bindist,.,\
+@@ -897,6 +897,12 @@ endif
+ 
+ INSTALLED_PACKAGE_CONF=$(DESTDIR)$(topdir)/package.conf.d
+ 
++ifneq "$(BINDIST)" "YES"
++# when installing ghc-stage2 we can't run target's
++# 'ghc-pkg' and 'ghc-stage2' but those are needed for registration.
++INSTALLED_GHC_REAL=$(TOP)/inplace/bin/ghc-stage1
++INSTALLED_GHC_PKG_REAL=$(TOP)/utils/ghc-pkg/dist/build/tmp/ghc-pkg
++else
+ # Install packages in the right order, so that ghc-pkg doesn't complain.
+ # Also, install ghc-pkg first.
+ ifeq "$(Windows_Host)" "NO"
+@@ -906,6 +912,7 @@ else
+ INSTALLED_GHC_REAL=$(DESTDIR)$(bindir)/ghc.exe
+ INSTALLED_GHC_PKG_REAL=$(DESTDIR)$(bindir)/ghc-pkg.exe
+ endif
++endif
+ 
+ # Set the INSTALL_DISTDIR_p for each package; compiler is special
+ $(foreach p,$(filter-out compiler,$(INSTALL_PACKAGES)),\
+@@ -989,7 +996,6 @@ $(eval $(call bindist,.,\
      $(INSTALL_LIBRARY_DOCS) \
      $(addsuffix /*,$(INSTALL_HTML_DOC_DIRS)) \
      docs/index.html \
