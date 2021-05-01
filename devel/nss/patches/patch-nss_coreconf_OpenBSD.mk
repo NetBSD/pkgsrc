39a5f07d03e7bba28cf8b059a7dbd1271e3be32e
wiz <wiz@pkgsrc.org>
2021-05-01 21:52:02 +0000

    nss: hide symbols on NetBSD like on other platforms
    Remove local workarounds again
    
    Bump PKGREVISION.

--- devel/nss/patches/patch-nss_coreconf_OpenBSD.mk
+++ devel/nss/patches/patch-nss_coreconf_OpenBSD.mk
@@ -1,8 +1,8 @@
-$NetBSD: patch-nss_coreconf_OpenBSD.mk,v 1.3 2019/05/05 22:47:28 ryoon Exp $
+$NetBSD: patch-nss_coreconf_OpenBSD.mk,v 1.4 2021/05/01 21:52:02 wiz Exp $
 
 * Use non-versioned libraries for OpenBSD too
 
---- nss/coreconf/OpenBSD.mk.orig	2016-05-17 07:58:45.000000000 +0000
+--- nss/coreconf/OpenBSD.mk.orig	2021-04-15 16:17:44.000000000 +0000
 +++ nss/coreconf/OpenBSD.mk
 @@ -24,7 +24,7 @@ OS_LIBS			+= -pthread
  DSO_LDOPTS		+= -pthread
