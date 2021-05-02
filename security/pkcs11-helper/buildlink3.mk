fc2e6919cc770f635ba747c0bf8008be56f90a0d
nia <nia@pkgsrc.org>
2021-05-02 08:16:40 +0000

    Recursive revbump for security/mbedtls

--- security/pkcs11-helper/buildlink3.mk
+++ security/pkcs11-helper/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2021/04/21 13:24:16 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2021/05/02 08:16:41 nia Exp $
 
 BUILDLINK_TREE+=	pkcs11-helper
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pkcs11-helper
 PKCS11_HELPER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.06
-BUILDLINK_ABI_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.27.0nb2
+BUILDLINK_ABI_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.27.0nb3
 BUILDLINK_PKGSRCDIR.pkcs11-helper?=	../../security/pkcs11-helper
 
 pkgbase := pkcs11-helper
