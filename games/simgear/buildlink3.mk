938d43dcf08d80c5f64368d5a2d22a7ea63a75a5
gdt <gdt@pkgsrc.org>
2021-05-03 16:28:46 +0000

    revbump for gdal-lib udpate

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.58 2021/04/21 13:24:11 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.59 2021/05/03 16:28:46 gdt Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2nb7
-BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.6nb5
+BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.6nb6
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
