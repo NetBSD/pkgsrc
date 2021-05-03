681d145ea9bcf4346c991499c23a1bdc1b5abbbf
thor <thor@pkgsrc.org>
2021-05-03 17:29:58 +0000

    graphics/pcl: duplicate deps as is customary, drop external metslib
    The shared depends.mk is not the pkgsrc way. The metslib dependency
    is not properly supported by upstream, so we switch to the internal
    copy for now.
    
    Also, the Makefile got a bit of further refinement.

--- graphics/pcl/buildlink3.mk
+++ graphics/pcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/05/03 09:12:58 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/05/03 17:29:58 thor Exp $
 
 BUILDLINK_TREE+=	pcl
 
@@ -9,7 +9,19 @@ BUILDLINK_API_DEPENDS.pcl+=	pcl>=1.10.0
 BUILDLINK_ABI_DEPENDS.pcl+=	pcl>=1.10.0
 BUILDLINK_PKGSRCDIR.pcl?=	../../graphics/pcl
 
-.include "../../graphics/pcl/depends.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/libusb1/buildlink3.mk"
+.include "../../math/flann-lib/buildlink3.mk"
+# waiting for VTK update, not usable yet
+#.include "../../graphics/vtk/buildlink3.mk"
+#.include "../../graphics/glew/buildlink3.mk"
+.include "../../math/eigen3/buildlink3.mk"
+.include "../../math/qhull/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../net/libpcap/buildlink3.mk"
+# Not used correctly right now, see
+# https://github.com/PointCloudLibrary/pcl/issues/4744
+#.include "../../devel/metslib/buildlink3.mk"
 
 .endif # PCL_BUILDLINK3_MK
 
