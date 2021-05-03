6139bcfecbfa8853f13d3f4ee325ce1ae43a9b1d
jdolecek <jdolecek@pkgsrc.org>
2021-05-03 18:58:53 +0000

    bump buildlink rev dep for freerdp2, particularly since many CVEs were fixed since 2.0.0rc4

--- net/freerdp2/buildlink3.mk
+++ net/freerdp2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/09/15 12:55:14 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/05/03 18:58:53 jdolecek Exp $
 
 BUILDLINK_TREE+=	freerdp2
 
 .if !defined(FREERDP2_BUILDLINK3_MK)
 FREERDP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.freerdp2+=	freerdp2>=2.0.0rc4
+BUILDLINK_API_DEPENDS.freerdp2+=	freerdp2>=2.3.2
 BUILDLINK_PKGSRCDIR.freerdp2?=		../../net/freerdp2
 .endif	# FREERDP2_BUILDLINK3_MK
 
