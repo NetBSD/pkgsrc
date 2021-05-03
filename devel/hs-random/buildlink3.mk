787acd0fcb2b0d5fafd6aa9184d6ab3c1c593cc8
pho <pho@pkgsrc.org>
2021-05-03 14:38:23 +0000

    Add missing dependency on splitmix

--- devel/hs-random/buildlink3.mk
+++ devel/hs-random/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/04/23 06:51:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/05/03 14:38:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-random
 
@@ -8,6 +8,8 @@ HS_RANDOM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.2.0
 BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.2.0
 BUILDLINK_PKGSRCDIR.hs-random?=		../../devel/hs-random
+
+.include "../../devel/hs-splitmix/buildlink3.mk"
 .endif	# HS_RANDOM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-random
