a2b40fb90729757327de5919f33a6fdb8e6c0de1
pho <pho@pkgsrc.org>
2021-05-04 14:37:33 +0000

    Add missing dependency on hs-csv and hs-html

--- devel/hs-tabular/buildlink3.mk
+++ devel/hs-tabular/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/05/03 19:01:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/05/04 14:37:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-tabular
 
@@ -8,6 +8,9 @@ HS_TABULAR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-tabular+=	hs-tabular>=0.2.2
 BUILDLINK_ABI_DEPENDS.hs-tabular+=	hs-tabular>=0.2.2.8nb1
 BUILDLINK_PKGSRCDIR.hs-tabular?=	../../devel/hs-tabular
+
+.include "../../textproc/hs-csv/buildlink3.mk"
+.include "../../textproc/hs-html/buildlink3.mk"
 .endif	# HS_TABULAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tabular
