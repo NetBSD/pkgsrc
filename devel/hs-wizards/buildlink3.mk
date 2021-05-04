e95a5c5487e37dfa1a8abeebe53250a215cf898b
pho <pho@pkgsrc.org>
2021-05-04 14:57:58 +0000

    Add missing dependency on hs-control-monad-free

--- devel/hs-wizards/buildlink3.mk
+++ devel/hs-wizards/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/05/03 19:01:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/05/04 14:57:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-wizards
 
@@ -8,6 +8,8 @@ HS_WIZARDS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-wizards+=	hs-wizards>=1.0.3
 BUILDLINK_ABI_DEPENDS.hs-wizards+=	hs-wizards>=1.0.3nb1
 BUILDLINK_PKGSRCDIR.hs-wizards?=	../../devel/hs-wizards
+
+.include "../../devel/hs-control-monad-free/buildlink3.mk"
 .endif	# HS_WIZARDS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-wizards
