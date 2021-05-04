95d02480c53f3f676464361033b67f8a38a593bf
gdt <gdt@pkgsrc.org>
2021-05-04 16:27:38 +0000

    geography/qgis: Mark as not for python 2.7
    It's unlikely that this built ok with 2.7, and really unlikely anyone
    was actually doing that.  However, py-gdal is not python 3.x only, so be explicit.
    
    (No revbump because default has been some form of python 3.x.)

--- geography/qgis/options.mk
+++ geography/qgis/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.20 2021/05/03 15:32:03 gdt Exp $
+# $NetBSD: options.mk,v 1.21 2021/05/04 16:27:38 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qgis
 PKG_SUPPORTED_OPTIONS=	python
@@ -12,25 +12,32 @@ PKG_SUGGESTED_OPTIONS+=	python
 .include "../../mk/bsd.options.mk"
 
 .if empty(PKG_OPTIONS:Mpython)
+
 CMAKE_ARGS+=		-DWITH_BINDINGS:BOOL=FALSE
+
 PYTHON_FOR_BUILD_ONLY=	YES
 .include "../../lang/python/application.mk"
+
 .else
+
 CMAKE_ARGS+=		-DWITH_BINDINGS:BOOL=TRUE
 CMAKE_ARGS+=		-DSIP_BINARY_PATH:PATH=${BUILDLINK_PREFIX.py-sip}/bin
 CMAKE_ARGS+=		-DWITH_INTERNAL_MARKUPSAFE=FALSE
 PLIST_SRC+=             ${PKGDIR}/PLIST.python
+
 .include "../../lang/python/application.mk"
+PYTHON_VERSIONS_INCOMPATIBLE=	27
+
 .include "../../math/py-numpy/buildlink3.mk"
 .include "../../x11/py-qt5/buildlink3.mk"
 .include "../../x11/py-qt5-qscintilla/buildlink3.mk"
 .include "../../x11/py-sip/buildlink3.mk"
+
 DEPENDS+=	${PYPKGPREFIX}-requests-[0-9]*:../../devel/py-requests
 DEPENDS+=	${PYPKGPREFIX}-psycopg2-[0-9]*:../../databases/py-psycopg2
 DEPENDS+=	${PYPKGPREFIX}-gdal-[0-9]*:../../geography/py-gdal
 DEPENDS+=	${PYPKGPREFIX}-jinja2-[0-9]*:../../textproc/py-jinja2
 DEPENDS+=	${PYPKGPREFIX}-markupsafe-[0-9]*:../../textproc/py-markupsafe
-
 # py-owslib # metasearch
 # py-yaml # quickosm
 
