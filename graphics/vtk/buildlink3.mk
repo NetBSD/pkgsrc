714d9d0dcb528f55bea6d61cd81c316fee6be3e3
prlw1 <prlw1@pkgsrc.org>
2021-05-04 14:48:34 +0000

    Update vtk to 9.0.1
    18 years of updates. In memory of Matthias Drochner.

--- graphics/vtk/buildlink3.mk
+++ graphics/vtk/buildlink3.mk
@@ -1,18 +1,30 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/04/27 14:51:49 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/05/04 14:48:34 prlw1 Exp $
 
 BUILDLINK_TREE+=	vtk
 
 .if !defined(VTK_BUILDLINK3_MK)
 VTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vtk+=	vtk>=4.2
-BUILDLINK_ABI_DEPENDS.vtk+=	vtk>=4.2
+BUILDLINK_API_DEPENDS.vtk+=	vtk>=9.0.1
 BUILDLINK_PKGSRCDIR.vtk?=	../../graphics/vtk
 
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/glu/buildlink3.mk"
-.include "../../x11/libXt/buildlink3.mk"
-
-.endif # VTK_BUILDLINK3_MK
+.include "../../archivers/lz4/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/hdf5/buildlink3.mk"
+.include "../../devel/netcdf-cxx/buildlink3.mk"
+.include "../../geography/proj/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/gl2ps/buildlink3.mk"
+.include "../../graphics/glew/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../math/double-conversion/buildlink3.mk"
+.include "../../multimedia/libtheora/buildlink3.mk"
+.include "../../print/libharu/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
+.include "../../textproc/jsoncpp/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../textproc/pugixml/buildlink3.mk"
+.endif	# VTK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-vtk
