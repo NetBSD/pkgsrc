7db85ed081f29279e9e25678c3684309176aecf4
pho <pho@pkgsrc.org>
2021-05-05 10:23:03 +0000

    Import hlint-3.3.1
    HLint is a tool for suggesting possible improvements to Haskell
    code. These suggestions include ideas such as using alternative
    functions, simplifying code and spotting redundancies.

---
+++ devel/hlint/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 10:23:03 pho Exp $
+
+BUILDLINK_TREE+=	hlint
+
+.if !defined(HLINT_BUILDLINK3_MK)
+HLINT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=3.3.1
+BUILDLINK_ABI_DEPENDS.hlint+=	hlint>=3.3.1
+BUILDLINK_PKGSRCDIR.hlint?=	../../devel/hlint
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-cmdargs/buildlink3.mk"
+.include "../../devel/hs-cpphs/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-file-embed/buildlink3.mk"
+.include "../../sysutils/hs-filepattern/buildlink3.mk"
+.include "../../devel/hs-ghc-lib-parser-ex/buildlink3.mk"
+.include "../../devel/hs-refact/buildlink3.mk"
+.include "../../devel/hs-uniplate/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-hscolour/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
+.endif	# HLINT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hlint
