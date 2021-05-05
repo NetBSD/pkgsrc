42be36fa0773397d8ad73922d218b3957f64d228
pho <pho@pkgsrc.org>
2021-05-05 11:46:54 +0000

    Import process-extras-0.7.4
    Extends the "process" package. Read process input and output as
    ByteStrings or Text, or write your own ProcessOutput instance. Lazy
    process input and output. ProcessMaker class for more flexibility in
    the process creation API.

---
+++ sysutils/hs-process-extras/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 11:46:54 pho Exp $
+
+BUILDLINK_TREE+=	hs-process-extras
+
+.if !defined(HS_PROCESS_EXTRAS_BUILDLINK3_MK)
+HS_PROCESS_EXTRAS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-process-extras+=	hs-process-extras>=0.7.4
+BUILDLINK_ABI_DEPENDS.hs-process-extras+=	hs-process-extras>=0.7.4
+BUILDLINK_PKGSRCDIR.hs-process-extras?=		../../sysutils/hs-process-extras
+
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-generic-deriving/buildlink3.mk"
+.include "../../devel/hs-ListLike/buildlink3.mk"
+.endif	# HS_PROCESS_EXTRAS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-process-extras
