ff343e47502f49658e2b2d2c8953329906721ae9
pho <pho@pkgsrc.org>
2021-05-05 11:18:51 +0000

    Import http-conduit-2.3.8
    Provides for making efficient HTTP/HTTPS requests, providing either a
    simple or streaming interface.

---
+++ www/hs-http-conduit/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 11:18:51 pho Exp $
+
+BUILDLINK_TREE+=	hs-http-conduit
+
+.if !defined(HS_HTTP_CONDUIT_BUILDLINK3_MK)
+HS_HTTP_CONDUIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-http-conduit+=	hs-http-conduit>=2.3.8
+BUILDLINK_ABI_DEPENDS.hs-http-conduit+=	hs-http-conduit>=2.3.8
+BUILDLINK_PKGSRCDIR.hs-http-conduit?=	../../www/hs-http-conduit
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-conduit-extra/buildlink3.mk"
+.include "../../www/hs-http-client/buildlink3.mk"
+.include "../../www/hs-http-client-tls/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
+.endif	# HS_HTTP_CONDUIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-http-conduit
