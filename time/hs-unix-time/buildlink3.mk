588eac03510612cf20b00c96e5d2509071b18e54
pho <pho@pkgsrc.org>
2021-05-05 12:22:59 +0000

    Import unix-time-0.4.7
    Fast parser/formatter/utilities for Unix time

---
+++ time/hs-unix-time/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 12:22:59 pho Exp $
+
+BUILDLINK_TREE+=	hs-unix-time
+
+.if !defined(HS_UNIX_TIME_BUILDLINK3_MK)
+HS_UNIX_TIME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.7
+BUILDLINK_ABI_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.7
+BUILDLINK_PKGSRCDIR.hs-unix-time?=	../../time/hs-unix-time
+
+.include "../../time/hs-old-time/buildlink3.mk"
+.endif	# HS_UNIX_TIME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-unix-time
