203e440e72ab5f0d5bf928888ff5044c93ebd24c
pho <pho@pkgsrc.org>
2021-05-05 12:13:42 +0000

    Import auto-update-0.1.6
    A common problem is the desire to have an action run at a scheduled
    interval, but only if it is needed. For example, instead of having
    every web request result in a new getCurrentTime call, we’d like to
    have a single worker thread run every second, updating an
    IORef. However, if the request frequency is less than once per second,
    this is a pessimization, and worse, kills idle GC.
    
    This library allows you to define actions which will either be
    performed by a dedicated thread or, in times of low volume, will be
    executed by the calling thread.

---
+++ devel/hs-auto-update/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 12:13:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-auto-update
+
+.if !defined(HS_AUTO_UPDATE_BUILDLINK3_MK)
+HS_AUTO_UPDATE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-auto-update+=	hs-auto-update>=0.1.6
+BUILDLINK_ABI_DEPENDS.hs-auto-update+=	hs-auto-update>=0.1.6
+BUILDLINK_PKGSRCDIR.hs-auto-update?=	../../devel/hs-auto-update
+.endif	# HS_AUTO_UPDATE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-auto-update
