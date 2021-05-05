906c1a4f30a57e54edc72bb9b4bb53f08c76d7e4
pho <pho@pkgsrc.org>
2021-05-05 16:19:08 +0000

    Forgot to bump buildlink3.mk

--- devel/hs-foundation/buildlink3.mk
+++ devel/hs-foundation/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 11:05:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/05/05 16:19:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-foundation
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-foundation
 HS_FOUNDATION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26
-BUILDLINK_ABI_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26
+BUILDLINK_ABI_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26nb1
 BUILDLINK_PKGSRCDIR.hs-foundation?=	../../devel/hs-foundation
 
 .include "../../devel/hs-basement/buildlink3.mk"
