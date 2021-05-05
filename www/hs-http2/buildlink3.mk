d25a2f763ef5f45c9934aa5536f6c46b3e777522
pho <pho@pkgsrc.org>
2021-05-05 15:18:44 +0000

    Import http2-3.0.1
    HTTP/2 library including frames, priority queues, HPACK, client and
    server.

---
+++ www/hs-http2/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 15:18:44 pho Exp $
+
+BUILDLINK_TREE+=	hs-http2
+
+.if !defined(HS_HTTP2_BUILDLINK3_MK)
+HS_HTTP2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=3.0.1
+BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=3.0.1
+BUILDLINK_PKGSRCDIR.hs-http2?=		../../www/hs-http2
+
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-byte-order/buildlink3.mk"
+.include "../../devel/hs-psqueues/buildlink3.mk"
+.include "../../time/hs-time-manager/buildlink3.mk"
+.include "../../time/hs-unix-time/buildlink3.mk"
+.endif	# HS_HTTP2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-http2
