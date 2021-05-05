4b45a60eed93fdc70e06c4f9487d6f4887803231
pho <pho@pkgsrc.org>
2021-05-05 16:03:34 +0000

    Import warp-tls-3.3.0
    warp-tls: HTTP over TLS support for Warp via the TLS package
    
    SSLv1 and SSLv2 are obsoleted by IETF. We should use TLS 1.2 (or TLS
    1.1 or TLS 1.0 if necessary). HTTP/2 can be negotiated by ALPN.

---
+++ www/hs-warp-tls/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 16:03:34 pho Exp $
+
+BUILDLINK_TREE+=	hs-warp-tls
+
+.if !defined(HS_WARP_TLS_BUILDLINK3_MK)
+HS_WARP_TLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.0
+BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.0
+BUILDLINK_PKGSRCDIR.hs-warp-tls?=	../../www/hs-warp-tls
+
+.include "../../security/hs-cryptonite/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../devel/hs-streaming-commons/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.include "../../security/hs-tls-session-manager/buildlink3.mk"
+.include "../../www/hs-wai/buildlink3.mk"
+.include "../../www/hs-warp/buildlink3.mk"
+.endif	# HS_WARP_TLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-warp-tls
