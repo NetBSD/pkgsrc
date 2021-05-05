c04b06805c2aff9a247125a7ac7e282da699054c
pho <pho@pkgsrc.org>
2021-05-05 15:54:00 +0000

    Import warp-3.3.15
    warp: A fast, light-weight web server for WAI applications.
    
    HTTP/1.0, HTTP/1.1 and HTTP/2 are supported. For HTTP/2, Warp supports
    direct and ALPN (in TLS) but not upgrade.

---
+++ www/hs-warp/buildlink3.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 15:54:00 pho Exp $
+
+BUILDLINK_TREE+=	hs-warp
+
+.if !defined(HS_WARP_BUILDLINK3_MK)
+HS_WARP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-warp+=	hs-warp>=3.3.15
+BUILDLINK_ABI_DEPENDS.hs-warp+=	hs-warp>=3.3.15
+BUILDLINK_PKGSRCDIR.hs-warp?=	../../www/hs-warp
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-auto-update/buildlink3.mk"
+.include "../../www/hs-bsb-http-chunked/buildlink3.mk"
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../www/hs-http-date/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../www/hs-http2/buildlink3.mk"
+.include "../../net/hs-iproute/buildlink3.mk"
+.include "../../net/hs-simple-sendfile/buildlink3.mk"
+.include "../../devel/hs-streaming-commons/buildlink3.mk"
+.include "../../time/hs-time-manager/buildlink3.mk"
+.include "../../devel/hs-unix-compat/buildlink3.mk"
+.include "../../devel/hs-vault/buildlink3.mk"
+.include "../../www/hs-wai/buildlink3.mk"
+.include "../../devel/hs-word8/buildlink3.mk"
+.include "../../security/hs-x509/buildlink3.mk"
+.endif	# HS_WARP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-warp
