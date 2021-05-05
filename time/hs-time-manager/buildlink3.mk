2d6559d30b3f6b136060c49b7071dca7a0b75c3a
pho <pho@pkgsrc.org>
2021-05-05 15:14:41 +0000

    Import time-manager-0.0.0
    Scalable timer functions provided by a timer manager.

---
+++ time/hs-time-manager/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 15:14:41 pho Exp $
+
+BUILDLINK_TREE+=	hs-time-manager
+
+.if !defined(HS_TIME_MANAGER_BUILDLINK3_MK)
+HS_TIME_MANAGER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.0
+BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.0
+BUILDLINK_PKGSRCDIR.hs-time-manager?=	../../time/hs-time-manager
+
+.include "../../devel/hs-auto-update/buildlink3.mk"
+.endif	# HS_TIME_MANAGER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-time-manager
