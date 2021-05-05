4dd15f59cd426de51084d1aff372bb021ee889d6
pho <pho@pkgsrc.org>
2021-05-05 11:27:42 +0000

    Import js-jquery-3.3.1
    This package bundles the minified jQuery code into a Haskell package,
    so it can be depended upon by Cabal packages. The first three
    components of the version number match the upstream jQuery
    version. The package is designed to meet the redistribution
    requirements of downstream users (e.g. Debian).

---
+++ www/hs-js-jquery/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 11:27:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-js-jquery
+
+.if !defined(HS_JS_JQUERY_BUILDLINK3_MK)
+HS_JS_JQUERY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-js-jquery+=	hs-js-jquery>=3.3.1
+BUILDLINK_PKGSRCDIR.hs-js-jquery?=	../../www/hs-js-jquery
+.endif	# HS_JS_JQUERY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-js-jquery
