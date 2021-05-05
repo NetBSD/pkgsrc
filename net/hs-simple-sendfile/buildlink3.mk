2b584b65d2386c1ee7e1bc9fa59694b26ddf1f68
pho <pho@pkgsrc.org>
2021-05-05 15:43:54 +0000

    Import simple-sendfile-0.2.30
    Cross platform library for the sendfile system call. This library
    tries to call minimum system calls which are the bottleneck of web
    servers.

---
+++ net/hs-simple-sendfile/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 15:43:54 pho Exp $
+
+BUILDLINK_TREE+=	hs-simple-sendfile
+
+.if !defined(HS_SIMPLE_SENDFILE_BUILDLINK3_MK)
+HS_SIMPLE_SENDFILE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-simple-sendfile+=	hs-simple-sendfile>=0.2.30
+BUILDLINK_ABI_DEPENDS.hs-simple-sendfile+=	hs-simple-sendfile>=0.2.30
+BUILDLINK_PKGSRCDIR.hs-simple-sendfile?=	../../net/hs-simple-sendfile
+
+.include "../../mk/bsd.fast.prefs.mk" # for ${OPSYS}
+
+.include "../../net/hs-network/buildlink3.mk"
+.if ${OPSYS} != "FreeBSD" && ${OPSYS} != "Darwin" && ${OPSYS} != "Linux"
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-conduit-extra/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.endif
+.endif	# HS_SIMPLE_SENDFILE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-simple-sendfile
