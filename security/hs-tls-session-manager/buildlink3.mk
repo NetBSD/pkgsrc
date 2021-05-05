4312c8a9a201650f7563de9cad717509442a88df
pho <pho@pkgsrc.org>
2021-05-05 16:00:30 +0000

    Import tls-session-manager-0.0.4
    TLS session manager with limitation, automatic pruning, energy saving
    and replay resistance

---
+++ security/hs-tls-session-manager/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 16:00:30 pho Exp $
+
+BUILDLINK_TREE+=	hs-tls-session-manager
+
+.if !defined(HS_TLS_SESSION_MANAGER_BUILDLINK3_MK)
+HS_TLS_SESSION_MANAGER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tls-session-manager+=	hs-tls-session-manager>=0.0.4
+BUILDLINK_ABI_DEPENDS.hs-tls-session-manager+=	hs-tls-session-manager>=0.0.4
+BUILDLINK_PKGSRCDIR.hs-tls-session-manager?=	../../security/hs-tls-session-manager
+
+.include "../../devel/hs-auto-update/buildlink3.mk"
+.include "../../devel/hs-basement/buildlink3.mk"
+.include "../../time/hs-clock/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../devel/hs-psqueues/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.endif	# HS_TLS_SESSION_MANAGER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tls-session-manager
