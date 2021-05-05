961c8b62d9d9feab0123b2d9978b09e492e05b87
pho <pho@pkgsrc.org>
2021-05-05 12:17:04 +0000

    Import easy-file-0.2.2
    Cross-platform File handling for Unix/Mac/Windows

---
+++ sysutils/hs-easy-file/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 12:17:04 pho Exp $
+
+BUILDLINK_TREE+=	hs-easy-file
+
+.if !defined(HS_EASY_FILE_BUILDLINK3_MK)
+HS_EASY_FILE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-easy-file+=	hs-easy-file>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-easy-file+=	hs-easy-file>=0.2.2
+BUILDLINK_PKGSRCDIR.hs-easy-file?=	../../sysutils/hs-easy-file
+.endif	# HS_EASY_FILE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-easy-file
