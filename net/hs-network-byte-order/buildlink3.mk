d963d4102c3533e0c7b02439c201df1f8f39c982
pho <pho@pkgsrc.org>
2021-05-05 14:58:58 +0000

    Import network-byte-order-0.1.6
    Peek and poke functions for network byte order.

---
+++ net/hs-network-byte-order/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 14:58:58 pho Exp $
+
+BUILDLINK_TREE+=	hs-network-byte-order
+
+.if !defined(HS_NETWORK_BYTE_ORDER_BUILDLINK3_MK)
+HS_NETWORK_BYTE_ORDER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-network-byte-order+=	hs-network-byte-order>=0.1.6
+BUILDLINK_ABI_DEPENDS.hs-network-byte-order+=	hs-network-byte-order>=0.1.6
+BUILDLINK_PKGSRCDIR.hs-network-byte-order?=	../../net/hs-network-byte-order
+.endif	# HS_NETWORK_BYTE_ORDER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-network-byte-order
