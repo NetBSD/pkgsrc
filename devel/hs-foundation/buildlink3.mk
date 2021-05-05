7966dd95711554e5766d55927a816c2e94170946
pho <pho@pkgsrc.org>
2021-05-05 11:05:56 +0000

    Import foundation-0.0.26
    A custom prelude with no dependencies apart from base.

---
+++ devel/hs-foundation/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 11:05:56 pho Exp $
+
+BUILDLINK_TREE+=	hs-foundation
+
+.if !defined(HS_FOUNDATION_BUILDLINK3_MK)
+HS_FOUNDATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26
+BUILDLINK_ABI_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26
+BUILDLINK_PKGSRCDIR.hs-foundation?=	../../devel/hs-foundation
+
+.include "../../devel/hs-basement/buildlink3.mk"
+.endif	# HS_FOUNDATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-foundation
