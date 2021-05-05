436e99875205f4d9ebed49d19688e9bfe489f157
pho <pho@pkgsrc.org>
2021-05-05 09:29:16 +0000

    Import network-info-0.2.0.10
    This library provides simple read-only access to the local computer's
    networking configuration. It is currently capable of getting a list of
    all the network interfaces and their respective IPv4, IPv6 and MAC
    addresses.

---
+++ net/hs-network-info/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 09:29:16 pho Exp $
+
+BUILDLINK_TREE+=	hs-network-info
+
+.if !defined(HS_NETWORK_INFO_BUILDLINK3_MK)
+HS_NETWORK_INFO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-network-info+=	hs-network-info>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-network-info+=	hs-network-info>=0.2.0.10
+BUILDLINK_PKGSRCDIR.hs-network-info?=	../../net/hs-network-info
+.endif	# HS_NETWORK_INFO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-network-info
