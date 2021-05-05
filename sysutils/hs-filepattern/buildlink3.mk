871477d6d663ed56edc9a8a2f84d3e00c1de319a
pho <pho@pkgsrc.org>
2021-05-05 09:48:29 +0000

    Import filepattern-0.1.2
    A library for matching files using patterns such as "src/**/*.png" for
    all .png files recursively under the src directory.

---
+++ sysutils/hs-filepattern/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/05 09:48:29 pho Exp $
+
+BUILDLINK_TREE+=	hs-filepattern
+
+.if !defined(HS_FILEPATTERN_BUILDLINK3_MK)
+HS_FILEPATTERN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-filepattern+=	hs-filepattern>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-filepattern+=	hs-filepattern>=0.1.2
+BUILDLINK_PKGSRCDIR.hs-filepattern?=	../../sysutils/hs-filepattern
+
+.include "../../misc/hs-extra/buildlink3.mk"
+.endif	# HS_FILEPATTERN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-filepattern
