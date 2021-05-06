6059974f13aa2c5f8bc7136098cd39543214d14f
tnn <tnn@pkgsrc.org>
2021-05-06 15:19:39 +0000

    jimtcl: make sure the unversioned solib symlink exists. Bump.
    pkgsrc wrappers get mighty confused without this

--- lang/jimtcl/buildlink3.mk
+++ lang/jimtcl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/05/06 10:38:34 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/05/06 15:19:39 tnn Exp $
 
 BUILDLINK_TREE+=	jimtcl
 
 .if !defined(JIMTCL_BUILDLINK3_MK)
 JIMTCL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jimtcl+=	jimtcl>=0.80
+BUILDLINK_API_DEPENDS.jimtcl+=	jimtcl>=0.80nb1
 BUILDLINK_PKGSRCDIR.jimtcl?=	../../lang/jimtcl
 .endif	# JIMTCL_BUILDLINK3_MK
 
