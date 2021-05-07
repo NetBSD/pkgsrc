fa579dcccf58104f2dbab6fedfd68170a52ac32d
thor <thor@pkgsrc.org>
2021-05-07 12:31:22 +0000

    math/fftw: fix pkglint issues

--- math/fftw/options.mk
+++ math/fftw/options.mk
@@ -1,14 +1,14 @@
-# $NetBSD: options.mk,v 1.4 2021/04/29 23:30:15 thor Exp $
+# $NetBSD: options.mk,v 1.5 2021/05/07 12:31:22 thor Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fftw
 # fftw (double) and fftwf (single) are always built, you can add
 # fftwl (long) and fftwq (quad).
 PKG_SUPPORTED_OPTIONS=	fftw-fortran openmp mpi fftw-long fftw-quad
 # Enable fortran support by default on platforms supported by lang/g95.
-.if (!empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Mx86_64) || \
-	!empty(MACHINE_ARCH:Mia64) || !empty(MACHINE_ARCH:Mpowerpc*) || \
-	!empty(MACHINE_ARCH:Mhppa) || !empty(MACHINE_ARCH:Msparc*) || \
-	!empty(MACHINE_ARCH:Malpha) || !empty(MACHINE_ARCH:Mmips*))
+.if (${MACHINE_ARCH} == i386 || ${MACHINE_ARCH} == x86_64 || \
+	${MACHINE_ARCH} == ia64 || !empty(MACHINE_ARCH:Mpowerpc*) || \
+	${MACHINE_ARCH} == hppa || !empty(MACHINE_ARCH:Msparc*) || \
+	${MACHINE_ARCH} == alpha || !empty(MACHINE_ARCH:Mmips*))
 # ...but disable it until lang/g95 issue is resolved.
 #PKG_SUGGESTED_OPTIONS=	fftw-fortran
 .endif
@@ -21,9 +21,9 @@ USE_LANGUAGES+=		fortran77
 CONFIGURE_ARGS+=	--disable-fortran
 .endif
 
-PLIST_VARS+=	omp
+PLIST_VARS+=		omp
 .if !empty(PKG_OPTIONS:Mopenmp)
-PLIST.omp=	yes
+PLIST.omp=		yes
 CONFIGURE_ARGS+=	--enable-openmp
 .endif
 
@@ -38,10 +38,10 @@ PLIST_VARS+=	long quad
 
 .if !empty(PKG_OPTIONS:Mfftw-long)
 FFTW_PRECISION+=	long-double
-PLIST.long=	yes
+PLIST.long=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mfftw-quad)
 FFTW_PRECISION+=	quad-precision
-PLIST.quad=	yes
+PLIST.quad=		yes
 .endif
