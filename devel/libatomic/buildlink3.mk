cc70856b07037f48418f7860036a37fed8d10268
rin <rin@pkgsrc.org>
2021-05-07 11:34:48 +0000

    Set BUILDLINK_{LDADD,LDFLAGS}, which is required at least for multimedia/ffmpeg4 and net/bind916 (reported by jklos).

--- devel/libatomic/buildlink3.mk
+++ devel/libatomic/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/06/02 21:27:05 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/05/07 11:34:48 rin Exp $
 
 BUILDLINK_TREE+=	libatomic
 
@@ -8,6 +8,9 @@ LIBATOMIC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libatomic+=	libatomic>=10.0
 BUILDLINK_ABI_DEPENDS.libatomic+=	libatomic>=10.0
 BUILDLINK_PKGSRCDIR.libatomic?=		../../devel/libatomic
+
+BUILDLINK_LDADD.libatomic+=	-latomic
+BUILDLINK_LDFLAGS.libatomic?=	${BUILDLINK_LDADD.libatomic}
 
 .include "../../mk/pthread.buildlink3.mk"
 .endif
