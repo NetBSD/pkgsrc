7835f5bbe43377cd71e787052003d357875f8456
bsiegert <bsiegert@pkgsrc.org>
2021-05-09 14:46:27 +0000

    hub: make the build use modules
    Bump revision.

---
+++ net/hub/go-modules.mk
@@ -0,0 +1,37 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/05/09 14:46:27 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.0.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.0.zip
+GO_MODULE_FILES+=	github.com/atotto/clipboard/@v/v0.0.0-20171229224153-bc5958e1c833.mod
+GO_MODULE_FILES+=	github.com/atotto/clipboard/@v/v0.0.0-20171229224153-bc5958e1c833.zip
+GO_MODULE_FILES+=	github.com/bmizerany/assert/@v/v0.0.0-20160611221934-b7ed37b82869.mod
+GO_MODULE_FILES+=	github.com/bmizerany/assert/@v/v0.0.0-20160611221934-b7ed37b82869.zip
+GO_MODULE_FILES+=	github.com/kballard/go-shellquote/@v/v0.0.0-20170619183022-cd60e84ee657.mod
+GO_MODULE_FILES+=	github.com/kballard/go-shellquote/@v/v0.0.0-20170619183022-cd60e84ee657.zip
+GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.0.0-20160823170715-cfb55aafdaf3.mod
+GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.0.0-20160823170715-cfb55aafdaf3.zip
+GO_MODULE_FILES+=	github.com/kr/text/@v/v0.0.0-20160504234017-7cafcd837844.mod
+GO_MODULE_FILES+=	github.com/kr/text/@v/v0.0.0-20160504234017-7cafcd837844.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.0.9.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.0.9.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.3.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.3.zip
+GO_MODULE_FILES+=	github.com/mitchellh/go-homedir/@v/v0.0.0-20161203194507-b8bc1bf76747.mod
+GO_MODULE_FILES+=	github.com/mitchellh/go-homedir/@v/v0.0.0-20161203194507-b8bc1bf76747.zip
+GO_MODULE_FILES+=	github.com/russross/blackfriday/@v/v0.0.0-20180526075726-670777b536d3.mod
+GO_MODULE_FILES+=	github.com/russross/blackfriday/@v/v0.0.0-20180526075726-670777b536d3.zip
+GO_MODULE_FILES+=	github.com/shurcoo!l/sanitized_anchor_name/@v/v0.0.0-20170918181015-86672fcb3f95.mod
+GO_MODULE_FILES+=	github.com/shurcoo!l/sanitized_anchor_name/@v/v0.0.0-20170918181015-86672fcb3f95.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20191002035440-2ec189313ef0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20191002035440-2ec189313ef0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190531175056-4c3a928424d2.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190531175056-4c3a928424d2.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.zip
+GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.0.0-20190319135612-7b8349ac747c.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.0.0-20190319135612-7b8349ac747c.zip
