21081114521404acb2a20ab2671d34a53a94087e
ryoon <ryoon@pkgsrc.org>
2021-05-12 14:19:03 +0000

    2fa: Update to 1.2.0
    Changelog:
    * go modulized
    * Depend on github.com/atotto/clipboard

---
+++ security/2fa/go-modules.mk
@@ -0,0 +1,4 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/05/12 14:19:03 ryoon Exp $
+
+GO_MODULE_FILES+=	github.com/atotto/clipboard/@v/v0.1.2.mod
+GO_MODULE_FILES+=	github.com/atotto/clipboard/@v/v0.1.2.zip
