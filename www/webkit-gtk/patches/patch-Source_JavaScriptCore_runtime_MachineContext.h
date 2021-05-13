de0951ab77533d960715fd16443d68f9074b5c49
leot <leot@pkgsrc.org>
2021-05-13 08:20:58 +0000

    webkit-gtk: Update to 2.32.1
    pkgsrc changes:
     - Remove patches applied upstream
    
    Changes:
    2.32.1
    ------
     - Support building against the Musl C library.
     - Support building against ICU version 69 or newer.
     - Improve handling of Media Capture devices.
     - Improve WebAudio playback.
     - Improve video orientation handling.
     - Improve seeking support for MSE playback.
     - Improve flush support in EME decryptors.
     - Fix HTTP status codes for requests done through a custom URI handler.
     - Fix the Bubblewrap sandbox in certain 32-bit systems.
     - Fix inconsistencies between the WebKitWebView.is-muted property state
       and values returned by webkit_web_view_is_playing_audio().
     - Fix the build with ENABLE_VIDEO=OFF.
     - Fix wrong timestamps for long-lived cookies.
     - Fix UI process crash when failing to load favicons.
     - Fix several crashes and rendering issues.
     - Translation updates: Swedish.

--- www/webkit-gtk/patches/patch-Source_JavaScriptCore_runtime_MachineContext.h
+++ www/webkit-gtk/patches/patch-Source_JavaScriptCore_runtime_MachineContext.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-Source_JavaScriptCore_runtime_MachineContext.h,v 1.1 2018/10/24 18:31:07 leot Exp $
+$NetBSD: patch-Source_JavaScriptCore_runtime_MachineContext.h,v 1.2 2021/05/13 08:20:58 leot Exp $
 
 Add support for NetBSD.
 
---- Source/JavaScriptCore/runtime/MachineContext.h.orig	2018-08-21 16:06:10.000000000 +0000
+--- Source/JavaScriptCore/runtime/MachineContext.h.orig	2021-05-05 05:33:24.000000000 +0000
 +++ Source/JavaScriptCore/runtime/MachineContext.h
-@@ -188,6 +188,22 @@ static inline void*& stackPointerImpl(mc
+@@ -196,6 +196,22 @@ static inline void*& stackPointerImpl(mc
  #error Unknown Architecture
  #endif
  
@@ -24,10 +24,10 @@ Add support for NetBSD.
 +#error Unknown Architecture
 +#endif
 +
- #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
+ #elif OS(FUCHSIA) || OS(LINUX)
  
  #if CPU(X86)
-@@ -335,6 +351,22 @@ static inline void*& framePointerImpl(mc
+@@ -347,6 +363,22 @@ static inline void*& framePointerImpl(mc
  #error Unknown Architecture
  #endif
  
@@ -47,13 +47,14 @@ Add support for NetBSD.
 +#error Unknown Architecture
 +#endif
 +
- #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
+ #elif OS(FUCHSIA) || OS(LINUX)
  
  // The following sequence depends on glibc's sys/ucontext.h.
-@@ -482,6 +514,22 @@ static inline void*& instructionPointerI
+@@ -497,6 +529,22 @@ static inline void*& instructionPointerI
+ #else
  #error Unknown Architecture
  #endif
- 
++ 
 +#elif OS(NETBSD)
 +
 +#if CPU(X86)
@@ -69,14 +70,14 @@ Add support for NetBSD.
 +#else
 +#error Unknown Architecture
 +#endif
-+
- #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
- // The following sequence depends on glibc's sys/ucontext.h.
-@@ -639,6 +687,22 @@ inline void*& argumentPointer<1>(mcontex
+ #elif OS(FUCHSIA) || OS(LINUX)
+ 
+@@ -655,6 +703,22 @@ inline void*& argumentPointer<1>(mcontex
+ #else
  #error Unknown Architecture
  #endif
- 
++ 
 +#elif OS(NETBSD)
 +
 +#if CPU(X86)
@@ -92,14 +93,14 @@ Add support for NetBSD.
 +#else
 +#error Unknown Architecture
 +#endif
-+
- #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
- // The following sequence depends on glibc's sys/ucontext.h.
-@@ -756,6 +820,22 @@ inline void*& llintInstructionPointer(mc
+ #elif OS(FUCHSIA) || OS(LINUX)
+ 
+@@ -772,6 +836,22 @@ inline void*& llintInstructionPointer(mc
+ #else
  #error Unknown Architecture
  #endif
- 
++ 
 +#elif OS(NETBSD)
 +
 +#if CPU(X86)
@@ -115,7 +116,6 @@ Add support for NetBSD.
 +#else
 +#error Unknown Architecture
 +#endif
-+
- #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
- // The following sequence depends on glibc's sys/ucontext.h.
+ #elif OS(FUCHSIA) || OS(LINUX)
+ 
