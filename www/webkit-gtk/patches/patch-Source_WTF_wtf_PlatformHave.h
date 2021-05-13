27e8b109b4fa08de7c2aa5eab070c27cdcd24f0d
leot <leot@pkgsrc.org>
2021-05-13 08:20:58 +0000

    webkit-gtk: Update to 2.32.1
    pkgsrc changes:
     - Remove patches applied upstream
    
    Changes:
    2.32.1
    ------
     - Support building against the Musl C library.
     - Support building against ICU version 69 or newer.
     - Improve handling of Media Capture devices.
     - Improve WebAudio playback.
     - Improve video orientation handling.
     - Improve seeking support for MSE playback.
     - Improve flush support in EME decryptors.
     - Fix HTTP status codes for requests done through a custom URI handler.
     - Fix the Bubblewrap sandbox in certain 32-bit systems.
     - Fix inconsistencies between the WebKitWebView.is-muted property state
       and values returned by webkit_web_view_is_playing_audio().
     - Fix the build with ENABLE_VIDEO=OFF.
     - Fix wrong timestamps for long-lived cookies.
     - Fix UI process crash when failing to load favicons.
     - Fix several crashes and rendering issues.
     - Translation updates: Swedish.

--- www/webkit-gtk/patches/patch-Source_WTF_wtf_PlatformHave.h
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_PlatformHave.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-Source_WTF_wtf_PlatformHave.h,v 1.1 2020/03/10 18:14:04 leot Exp $
+$NetBSD: patch-Source_WTF_wtf_PlatformHave.h,v 1.2 2021/05/13 08:20:58 leot Exp $
 
 Add support for NetBSD.
 
---- Source/WTF/wtf/PlatformHave.h.orig	2020-02-26 10:34:01.000000000 +0000
+--- Source/WTF/wtf/PlatformHave.h.orig	2021-05-05 05:33:24.000000000 +0000
 +++ Source/WTF/wtf/PlatformHave.h
-@@ -222,7 +222,7 @@
+@@ -206,7 +206,7 @@
  #define HAVE_HOSTED_CORE_ANIMATION 1
  #endif
  
--#if OS(DARWIN) || OS(FUCHSIA) || ((OS(FREEBSD) || defined(__GLIBC__) || defined(__BIONIC__)) && (CPU(X86) || CPU(X86_64) || CPU(ARM) || CPU(ARM64) || CPU(MIPS)))
-+#if OS(DARWIN) || OS(FUCHSIA) || ((OS(FREEBSD) || OS(NETBSD) || defined(__GLIBC__) || defined(__BIONIC__)) && (CPU(X86) || CPU(X86_64) || CPU(ARM) || CPU(ARM64) || CPU(MIPS)))
+-#if OS(DARWIN) || OS(FUCHSIA) || ((OS(FREEBSD) || OS(LINUX)) && (CPU(X86) || CPU(X86_64) || CPU(ARM) || CPU(ARM64) || CPU(MIPS)))
++#if OS(DARWIN) || OS(FUCHSIA) || ((OS(FREEBSD) || OS(NETBSD) || OS(LINUX)) && (CPU(X86) || CPU(X86_64) || CPU(ARM) || CPU(ARM64) || CPU(MIPS)))
  #define HAVE_MACHINE_CONTEXT 1
  #endif
  
