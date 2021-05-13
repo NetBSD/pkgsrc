f026446f1d250491f1dff356070209a4d9272fa9
schmonz <schmonz@pkgsrc.org>
2021-05-13 14:06:09 +0000

    Update to 4.1. From the changelog:
    - improved handling of Custom theme attributes (#182)
    - fixed wrong color code in edit-kwrite.theme
    - added rng file mapping (#129)
    - improved Lisp highlighting
    - GUI: fixed highlighting options tab title (thanks to Craig)
    - renamed `std` style name to `def`
    - version and README updates
    - removed `extras/web_plugins`
    - added user-select default property to HTML line number style
    - revised color themes
    - added two more keyword styles for default themes
    - added Custom theme attributes for Plain TeX, LaTeX, SVG and Pango
    - enabled syntax message output with `--ls-syntax-error`
    - GUI: enabled syntax error checkbox
    - added Custom theme attribute
    - enabled inline stylesheets with `--ls-hover`
    - added Error and Hover theme properties
    - enabled syntax error highlighting with `ls-semantic`
    - improved LSP message handling
    - added delay LSP parameter
    - added LSP semantic token styles to base16 themes
    - renamed `str` style name to `sng`
    - CLI: enabled `--ls-semantic` option
    - GUI: enabled semantic checkbox and a server capability test
    - added support for the language server protocol
    - added new configuration file lsp.conf
    - CLI: added `ls-profile`, `--ls-workspace`,`--ls-hover`
    - CLI: deprecated `--start-nested`, `--reformat=user`,
      `--reformat-option`, `--base16`, `--delim-cr`, `--plug-in-read`
    - GUI: added LSP configuration tab
    - added `--syntax-supported` option

--- textproc/libhighlight/patches/patch-src_gui-qt_mainwindow.cpp
+++ textproc/libhighlight/patches/patch-src_gui-qt_mainwindow.cpp
@@ -1,15 +1,14 @@
-$NetBSD: patch-src_gui-qt_mainwindow.cpp,v 1.3 2019/05/21 21:56:57 schmonz Exp $
+$NetBSD: patch-src_gui-qt_mainwindow.cpp,v 1.4 2021/05/13 14:06:10 schmonz Exp $
 
 Use X11 on Darwin, like everywhere else.
 
---- src/gui-qt/mainwindow.cpp.orig	2019-05-17 19:04:13.000000000 +0000
+--- src/gui-qt/mainwindow.cpp.orig	2021-05-10 17:47:34.000000000 +0000
 +++ src/gui-qt/mainwindow.cpp
-@@ -36,6 +36,8 @@ along with Highlight.  If not, see <http
+@@ -36,6 +36,7 @@ along with Highlight.  If not, see <http
  #include "io_report.h"
  #include "syntax_chooser.h"
  
 +#undef Q_OS_OSX
-+
+ 
  #ifdef Q_OS_WIN
  #include <windows.h>
- #endif
