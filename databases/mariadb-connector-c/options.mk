c4c7d31573460a217e140cd5acfed2168080b01d
nia <nia@pkgsrc.org>
2021-05-13 11:46:13 +0000

    mariadb-connector-c: update to 3.1.13
    release notes:
    https://mariadb.com/kb/en/mariadb-connector-c-31-release-notes/

--- databases/mariadb-connector-c/options.mk
+++ databases/mariadb-connector-c/options.mk
@@ -1,23 +1,40 @@
-# $NetBSD: options.mk,v 1.1 2018/04/13 08:04:14 fhajny Exp $
+# $NetBSD: options.mk,v 1.2 2021/05/13 11:46:13 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.mariadb-connector-c
-PKG_SUPPORTED_OPTIONS=	curl kerberos
-PKG_SUGGESTED_OPTIONS=	# empty
+PKG_OPTIONS_VAR=		PKG_OPTIONS.mariadb-connector-c
+PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
+PKG_OPTIONS_GROUP.ssl=		gnutls openssl
+PKG_SUPPORTED_OPTIONS=		curl kerberos
+PKG_SUGGESTED_OPTIONS=		openssl
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		curl gss
+PLIST_VARS+=	curl gss openssl ssl
+
+.if !empty(PKG_OPTIONS:Mgnutls)
+PLIST.ssl=	yes
+CMAKE_ARGS+=	-DWITH_SSL=GNUTLS
+.  include "../../security/gnutls/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mopenssl)
+PLIST.openssl=	yes
+PLIST.ssl=	yes
+CMAKE_ARGS+=	-DWITH_SSL=OPENSSL
+.  include "../../security/openssl/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_SSL=OFF
+.endif
 
 .if !empty(PKG_OPTIONS:Mcurl)
-PLIST.curl=		yes
+PLIST.curl=	yes
 .  include "../../www/curl/buildlink3.mk"
 .else
-CMAKE_ARGS+=		-DWITH_CURL=OFF
+CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_CURL=ON
+CMAKE_ARGS+=	-DREMOTEIO_PLUGIN_TYPE=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mkerberos)
-PLIST.gss=		yes
+PLIST.gss=	yes
 .  include "../../mk/krb5.buildlink3.mk"
 .else
-CMAKE_ARGS+=		-DWITH_GSS=OFF
+CMAKE_ARGS+=	-DCMAKE_DISABLE_FIND_PACKAGE_GSSAPI=ON
+CMAKE_ARGS+=	-DAUTH_GSSAPI_PLUGIN_TYPE=OFF
 .endif
