120b027d7e830a218c3a3c52f1b2c097ea663abb
wiz <wiz@pkgsrc.org>
2021-05-14 19:36:47 +0000

    protobuf: fix build on NetBSD

---
+++ devel/protobuf/patches/patch-src_google_protobuf_stubs_port.h
@@ -0,0 +1,29 @@
+$NetBSD: patch-src_google_protobuf_stubs_port.h,v 1.1 2021/05/14 19:36:47 wiz Exp $
+
+Fix parentheses.
+
+--- src/google/protobuf/stubs/port.h.orig	2021-05-12 23:23:56.000000000 +0000
++++ src/google/protobuf/stubs/port.h
+@@ -263,14 +263,14 @@ static inline uint32 bswap_32(uint32 x) 
+ 
+ #ifndef bswap_64
+ static inline uint64 bswap_64(uint64 x) {
+-  return (((x & uint64_t{0xFFu)) << 56) |
+-          ((x & uint64_t{0xFF00u)) << 40) |
+-          ((x & uint64_t{0xFF0000u)) << 24) |
+-          ((x & uint64_t{0xFF000000u)) << 8) |
+-          ((x & uint64_t{0xFF00000000u)) >> 8) |
+-          ((x & uint64_t{0xFF0000000000u)) >> 24) |
+-          ((x & uint64_t{0xFF000000000000u)) >> 40) |
+-          ((x & uint64_t{0xFF00000000000000u)) >> 56));
++  return (((x & uint64_t{0xFFu}) << 56) |
++          ((x & uint64_t{0xFF00u}) << 40) |
++          ((x & uint64_t{0xFF0000u}) << 24) |
++          ((x & uint64_t{0xFF000000u}) << 8) |
++          ((x & uint64_t{0xFF00000000u}) >> 8) |
++          ((x & uint64_t{0xFF0000000000u}) >> 24) |
++          ((x & uint64_t{0xFF000000000000u}) >> 40) |
++          ((x & uint64_t{0xFF00000000000000u}) >> 56));
+ }
+ #define bswap_64(x) bswap_64(x)
+ #endif
