2864f70b2b1a7b64815937666231473d4ee436e8
bsiegert <bsiegert@pkgsrc.org>
2021-05-15 17:39:05 +0000

    Update go-mmark to 2.2.10, now a Go module
    Lots of changes, too many to list.

---
+++ textproc/go-mmark/go-modules.mk
@@ -0,0 +1,10 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/05/15 17:39:05 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.zip
+GO_MODULE_FILES+=	github.com/gomarkdown/markdown/@v/v0.0.0-20200817091206-6efd7696db82.mod
+GO_MODULE_FILES+=	github.com/gomarkdown/markdown/@v/v0.0.0-20200817091206-6efd7696db82.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.2.0.zip
+GO_MODULE_FILES+=	golang.org/dl/@v/v0.0.0-20190829154251-82a15e2f2ead.mod
+GO_MODULE_FILES+=	golang.org/dl/@v/v0.0.0-20190829154251-82a15e2f2ead.zip
