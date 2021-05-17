db9b2f3e39df87926b19fe521890f7e7838eef1d
thor <thor@pkgsrc.org>
2021-05-17 11:12:53 +0000

    geography/pdal-lib: new package for PDAL
    PDAL is Point Data Abstraction Library. It is a C/C++ open source library
    and applications for translating and processing point cloud data. It is
    not limited to LiDAR data, although the focus and impetus for many of
    the tools in the library have their origins in LiDAR.

---
+++ geography/pdal-lib/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/17 11:12:53 thor Exp $
+
+BUILDLINK_TREE+=	pdal-lib
+
+.if !defined(PDAL_LIB_BUILDLINK3_MK)
+PDAL_LIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.pdal-lib+=	pdal-lib>=2.1.0
+BUILDLINK_ABI_DEPENDS.pdal-lib+=	pdal-lib>=2.1.0
+BUILDLINK_PKGSRCDIR.pdal-lib?=		../../geography/pdal-lib
+
+.include "../../geography/gdal-lib/buildlink3.mk"
+.include "../../devel/hdf5-c++/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../geography/libgeotiff/buildlink3.mk"
+.include "../../geography/laszip/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../archivers/zstd/buildlink3.mk"
+.include "../../graphics/pcl/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+
+.endif # PDAL_LIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-pdal-lib
