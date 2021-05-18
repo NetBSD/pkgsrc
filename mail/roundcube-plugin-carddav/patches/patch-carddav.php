ba2a75f804c89783f972b4c6e04445ee2125967e
triaxx <triaxx@pkgsrc.org>
2021-05-18 07:13:26 +0000

    rouncube-plugin-carddarv: Update to 4.1.1
    pkgsrc changes:
    ---------------
      * Change HOMEPAGE since the development lead is now on Github.
      * Add a composer.lock file to avoid PLIST breaking each time a dependency
        is updated.
    
    upstream changes:
    -----------------
    Version 4.1.1 (to 4.1.0)
      o Fix: A fatal error would be raised when a password could not be decrypted,
        only on photo download. This would not be notable to the user (except for
        the photo not being displayed), but show up in the logs.
      o Fix #339: Allow adding public/shared addressbooks by giving full URL.
        Discovery is still used if the given URL does not point to an addressbook
        directly, or points to an addressbook inside the user's addressbook home.
    
    Version 4.1.0 (to 4.0.4)
      o Fix: Prefer labels from X-ABLabel extension if available over standard
        labels
      o Fix #317: Support specification of department with empty organization
      o Support several levels of departments separated by semicolon that end up as
        structured value in the VCard
      o Fix #318: Some attributes (e.g. gender) could not be deleted when updating
        a contact
      o Fix #53: Only create displayname when not present in VCard / not provided
        by roundcube
      o Fix #325: Roundcube setting for contact sorting field was not used
      o Fix #279: More specific error message when syntactically wrong URL is
        entered for new addressbook
      o Fix #328: Contact search with MySQL might not have returned all results
      o Fix #332: When adding a new contact via "add to addressbook" from mail
        view, the email address was missing in the new card
      o New: Download externally referenced photos on demand, drastically speeding
        up sync with when photos are stored separately from the VCard (e.g.
        iCloud).  For details see #247.
      o New: Support for instant messaging data fields and maiden name (resolves
        #46). Interoperability with other CardDAV clients suffers some caveats, but
        I tried my best to achieve maximum possible interoperability. See IMPP.md
        for the gory details.
      o Removed a workaround that appears to be needed in the part to provide
        address data to the calendar plugin. It seems this is no longer the case
        for current versions of calendar.

--- mail/roundcube-plugin-carddav/patches/patch-carddav.php
+++ mail/roundcube-plugin-carddav/patches/patch-carddav.php
@@ -1,12 +1,12 @@
-$NetBSD: patch-carddav.php,v 1.1 2021/03/01 12:42:26 triaxx Exp $
+$NetBSD: patch-carddav.php,v 1.2 2021/05/18 07:13:26 triaxx Exp $
 
 * Make pkgsrc-compliant the path of configuration file.
 
---- carddav.php.orig	2020-11-29 16:41:48.000000000 +0000
+--- carddav.php.orig	2021-03-20 16:59:43.000000000 +0000
 +++ carddav.php
-@@ -1043,7 +1043,7 @@ class carddav extends rcube_plugin
-         }
- 
+@@ -1158,7 +1158,7 @@ class carddav extends rcube_plugin
+         $logger = Config::inst()->logger();
+         $httpLogger = Config::inst()->httpLogger();
          $prefs = [];
 -        $configfile = dirname(__FILE__) . "/config.inc.php";
 +        $configfile = "@PKG_SYSCONFDIR@/config.inc.php";
