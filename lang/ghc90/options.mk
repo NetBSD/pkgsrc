796751357bc4fd780803eb50aee8113ebfcb8841
pho <pho@pkgsrc.org>
2021-05-21 01:45:00 +0000

    Built a bootkit for NetBSD/aarch64
    Also the package is now more explicit about LLVM. It used to invoke whatever llc(1) and opt(1) found in the $PATH when -fllvm is passed to the compiler, though it's not the default backend. Now LLVM is an explicit, optional dependency on platforms where NCG backend is available, and a mandatory dependency on others.
    
    Note that the LLVM backend tends to produce faster code but it runs significantly slower than NCG. AArch64 support of NCG is being worked on, but it hasn't been merged to the 9.0 branch yet (or even the master branch).

---
+++ lang/ghc90/options.mk
@@ -0,0 +1,48 @@
+# $NetBSD: options.mk,v 1.1 2021/05/21 01:45:00 pho Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ghc
+
+# GHC has a native implementation of codegen for some platforms. On
+# those platforms LLVM is optional. It's a requirement anywhere else.
+# See compiler/GHC/Driver/Backend.hs
+
+GHC_NCG_SUPPORTED=	i386 x86_64 powerpc powerpc64 sparc
+.if !empty(GHC_NCG_SUPPORTED:M${MACHINE_ARCH})
+PKG_SUPPORTED_OPTIONS+=	llvm
+PKG_SUGGESTED_OPTIONS+=	llvm
+GHC_LLVM_REQUIRED=	no
+.else
+GHC_LLVM_REQUIRED=	yes
+.endif
+
+.if !empty(PKG_SUPPORTED_OPTIONS)
+.  include "../../mk/bsd.options.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mllvm) || ${GHC_LLVM_REQUIRED} == "yes"
+.  include "../../lang/llvm/buildlink3.mk"
+.  include "../../lang/llvm/version.mk"
+CONFIGURE_ENV+=	LLC=${PREFIX:Q}/bin/llc
+CONFIGURE_ENV+=	OPT=${PREFIX:Q}/bin/opt
+
+# Maybe GHC doesn't like this but it's the only option available to us.
+SUBST_CLASSES+=		llvm
+SUBST_STAGE.llvm=	post-extract
+SUBST_MESSAGE.llvm=	Accept whichever version of LLVM installed via pkgsrc
+SUBST_FILES.llvm=	configure.ac
+SUBST_SED.llvm=		-e 's/LlvmVersion=[0-9]*/LlvmVersion=${LLVM_VERSION:C/^([0-9]*)\..*/\1/}/'
+
+# Clang is also required on Darwin.
+# See compiler/GHC/SysTools/Tasks.hs (runClang).
+.  if ${OPSYS} == "Darwin"
+.    include "../../lang/clang/buildlink3.mk"
+CONFIGURE_ENV+=	CLANG=${PREFIX:Q}/bin/clang
+.  else
+CONFIGURE_ENV+=	CLANG=${FALSE}
+.  endif
+
+.else
+CONFIGURE_ENV+=	LLC=${FALSE:Q}
+CONFIGURE_ENV+=	OPT=${FALSE:Q}
+CONFIGURE_ENV+=	CLANG=${FALSE:Q}
+.endif
