bebf6b07081a7ab0cfae7ebaa46adb888d9ea8c6
bouyer <bouyer@pkgsrc.org>
2021-05-22 11:36:01 +0000

    Properly detect and use NetBSD's socketcan implementation. Bump PKGREVISION

--- lang/python39/patches/patch-Modules_socketmodule.h
+++ lang/python39/patches/patch-Modules_socketmodule.h
@@ -1,17 +1,15 @@
-$NetBSD: patch-Modules_socketmodule.h,v 1.1 2020/10/10 20:20:12 adam Exp $
+$NetBSD: patch-Modules_socketmodule.h,v 1.2 2021/05/22 11:36:01 bouyer Exp $
 
 Support NetBSD's socketcan implementation
 
---- Modules/socketmodule.h.orig	2020-10-05 15:07:58.000000000 +0000
-+++ Modules/socketmodule.h
-@@ -148,6 +148,10 @@ typedef int socklen_t;
- #include <linux/can/j1939.h>
- #endif
+--- Modules/socketmodule.h.orig	2021-05-03 16:54:42.000000000 +0200
++++ Modules/socketmodule.h	2021-05-22 11:52:19.072276647 +0200
+@@ -131,6 +131,8 @@
  
-+#ifdef HAVE_NETCAN_CAN_H
-+#include <netcan/can.h>
-+#endif
-+
- #ifdef HAVE_SYS_SYS_DOMAIN_H
- #include <sys/sys_domain.h>
- #endif
+ #ifdef HAVE_LINUX_CAN_H
+ # include <linux/can.h>
++#elif defined(HAVE_NETCAN_CAN_H)
++# include <netcan/can.h>
+ #else
+ # undef AF_CAN
+ # undef PF_CAN
