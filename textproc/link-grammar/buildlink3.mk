43eb7a08c6f29315dd4582aa6e45b65af485ed2b
nia <nia@pkgsrc.org>
2021-05-22 14:15:27 +0000

    link-grammar: dependencies need argp to link
    should help fix the build of abiword-plugins.

--- textproc/link-grammar/buildlink3.mk
+++ textproc/link-grammar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/08/26 12:23:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/05/22 14:15:27 nia Exp $
 
 BUILDLINK_TREE+=	link-grammar
 
@@ -8,6 +8,8 @@ LINK_GRAMMAR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.link-grammar+=	link-grammar>=4.3.4
 BUILDLINK_ABI_DEPENDS.link-grammar+=	link-grammar>=5.2.5
 BUILDLINK_PKGSRCDIR.link-grammar?=	../../textproc/link-grammar
+
+.include "../../devel/argp/buildlink3.mk"
 .endif # LINK_GRAMMAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-link-grammar
