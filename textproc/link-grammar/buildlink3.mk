27bb9837b367346e003ec8e6fffe459261dd9351
nia <nia@pkgsrc.org>
2021-05-22 15:58:49 +0000

    link-grammar: increase ABI_DEPENDS, requested by wiz

--- textproc/link-grammar/buildlink3.mk
+++ textproc/link-grammar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/05/22 14:15:27 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/05/22 15:58:49 nia Exp $
 
 BUILDLINK_TREE+=	link-grammar
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	link-grammar
 LINK_GRAMMAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.link-grammar+=	link-grammar>=4.3.4
-BUILDLINK_ABI_DEPENDS.link-grammar+=	link-grammar>=5.2.5
+BUILDLINK_ABI_DEPENDS.link-grammar+=	link-grammar>=5.9.1
 BUILDLINK_PKGSRCDIR.link-grammar?=	../../textproc/link-grammar
 
 .include "../../devel/argp/buildlink3.mk"
