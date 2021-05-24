423722857c042de266ff5cc1d9ac28cdae6f969b
adam <adam@pkgsrc.org>
2021-05-24 08:23:18 +0000

    Adjust PGSQL_VERSIONS_ACCEPTED

--- databases/postgresql-postgis2/buildlink3.mk
+++ databases/postgresql-postgis2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2021/04/21 11:40:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2021/05/24 08:23:18 adam Exp $
 
 BUILDLINK_TREE+=	postgresql-postgis
 
@@ -8,8 +8,6 @@ POSTGRESQL_POSTGIS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=2.0
 BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql13-postgis>=3.1.1nb4
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis2
-
-PGSQL_VERSIONS_ACCEPTED=	10 96 95
 
 .include "../../geography/geos/buildlink3.mk"
 .include "../../geography/proj/buildlink3.mk"
