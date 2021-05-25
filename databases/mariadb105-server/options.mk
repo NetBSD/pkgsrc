8bf66506300230b58edb827f394d6b0a19883c87
nia <nia@pkgsrc.org>
2021-05-25 14:16:14 +0000

    mariadb105-server: add more PKG_OPTIONS.
    The defaults are basically based on what FreeBSD/OpenBSD are doing.
    
    MariaDB includes several third-party storage engines (e.g. RocksDB)
    which are developed out-of-tree and don't generally have the same
    (e.g. portability) guarantees. Keep these disabled by default for now.
    
    bump PKGREVISION.

--- databases/mariadb105-server/options.mk
+++ databases/mariadb105-server/options.mk
@@ -1,17 +1,73 @@
-# $NetBSD: options.mk,v 1.1 2021/05/23 15:32:48 nia Exp $
+# $NetBSD: options.mk,v 1.2 2021/05/25 14:16:14 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql-server
 
-PKG_SUPPORTED_OPTIONS+=	embedded-server ssl
+PKG_SUPPORTED_OPTIONS+=	columnstore embedded-server lzo lz4 oqgraph rocksdb
+PKG_SUPPORTED_OPTIONS+=	snappy ssl zstd
 PKG_SUGGESTED_OPTIONS+=	embedded-server ssl
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Mcolumnstore)
+.include "../../devel/boost-headers/buildlink3.mk"
+CMAKE_ARGS+=	-DPLUGIN_COLUMNSTORE=YES
+.else
+CMAKE_ARGS+=	-DDPLUGIN_COLUMNSTORE=NO
+.endif
+
+PLIST_VARS+=	embedded
 .if !empty(PKG_OPTIONS:Membedded-server)
 CMAKE_ARGS+=	-DWITH_EMBEDDED_SERVER=ON
-PLIST_SRC+=	PLIST.embedded
+PLIST.embedded=	yes
 .else
 CMAKE_ARGS+=	-DWITH_EMBEDDED_SERVER=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mlzo)
+.include "../../archivers/lzo/buildlink3.mk"
+CMAKE_ARGS+=	-DWITH_INNODB_LZO=ON
+.else
+CMAKE_ARGS+=	-DWITH_INNODB_LZO=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mlz4)
+.include "../../archivers/lz4/buildlink3.mk"
+CMAKE_ARGS+=	-DGRN_WITH_LZ4=ON
+CMAKE_ARGS+=	-DWITH_INNODB_LZ4=ON
+CMAKE_ARGS+=	-DWITH_ROCKSDB_LZ4=ON
+.else
+CMAKE_ARGS+=	-DGRN_WITH_LZ4=OFF
+CMAKE_ARGS+=	-DWITH_INNODB_LZ4=OFF
+CMAKE_ARGS+=	-DWITH_ROCKSDB_LZ4=OFF
+.endif
+
+PLIST_VARS+=	oqgraph
+.if !empty(PKG_OPTIONS:Moqgraph)
+CMAKE_ARGS+=	-DPLUGIN_OQGRAPH=YES
+PLIST.oqgraph=	yes
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/libjudy/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DPLUGIN_OQGRAPH=NO
+.endif
+
+PLIST_VARS+=	rocksdb
+.if !empty(PKG_OPTIONS:Mrocksdb)
+.include "../../lang/python/application.mk"
+PLIST.rocksdb=	yes
+CMAKE_ARGS+=	-DWITHOUT_ROCKSDB_STORAGE_ENGINE=OFF
+CMAKE_ARGS+=	-DPYTHON_SHEBANG=${PYTHONBIN}
+.else
+CMAKE_ARGS+=	-DWITHOUT_ROCKSDB_STORAGE_ENGINE=ON
+.endif
+
+.if !empty(PKG_OPTIONS:Msnappy)
+.include "../../devel/snappy/buildlink3.mk"
+CMAKE_ARGS+=	-DWITH_INNODB_SNAPPY=ON
+CMAKE_ARGS+=	-DWITH_ROCKSDB_snappy=ON
+.else
+CMAKE_ARGS+=	-DWITH_INNODB_SNAPPY=OFF
+CMAKE_ARGS+=	-DWITH_ROCKSDB_snappy=OFF
 .endif
 
 # Enable OpenSSL support
@@ -21,4 +77,11 @@ CMAKE_ARGS+=	-DWITH_SSL=system
 LDFLAGS+=	-lcrypto -lssl
 .else
 CMAKE_ARGS+=	-DWITH_SSL=no
+.endif
+
+.if !empty(PKG_OPTIONS:Mzstd)
+.include "../../archivers/zstd/buildlink3.mk"
+CMAKE_ARGS+=	-DWITH_ROCKSDB_zstd=ON
+.else
+CMAKE_ARGS+=	-DWITH_ROCKSDB_zstd=OFF
 .endif
