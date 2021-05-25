780de97e52e62e521755b7697c498e6aa5161dba
adam <adam@pkgsrc.org>
2021-05-25 05:45:53 +0000

    grpc py-grpcio py-grpcio-testing py-grpcio-tools: updated to 1.38.0
    Release v1.38.0
    
    Core
    
    Invalidate ExecCtx now before computing timeouts in all repeating timer events using a WorkSerializer or combiner.
    Fix use-after-unref bug in fault_injection_filter.
    Fix 25897 to avoid crashes when certificates are not yet updated.
    gRPC EventEngine Interface.
    Allow the AWS_DEFAULT_REGION environment variable.
    
    C++
    
    s/OnServingStatusChange/OnServingStatusUpdate.
    cmake: only install channelz if gRPC_USE_PROTO_LITE is off.
    
    C#
    
    Grpc.Tools: Use x64 protoc on macOS arm64.
    Provide runtime/framework info in gRPC C# user agent string.
    
    Python
    
    Add grpcio-admin Python package.
    Add CSDS API to Python.
    Expose code and details from context on the server side.
    Explicitly import importlib.abc; required on Python 3.10.
    Fix potential deadlock on the GIL in AuthMetdataPlugin.
    Introduce new Python package "xds_protos".
    also build python3.6 aarch64 manylinux2014 wheel.
    Remove async mark for set_trailing_metadata interface.
    
    Ruby
    
    ruby: use x86_64 binary on M1 macs.
    Add ruby 3.0 support for mac binary packages.

---
+++ net/grpc/patches/patch-include_grpc_event__engine_port.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-include_grpc_event__engine_port.h,v 1.1 2021/05/25 05:45:53 adam Exp $
+
+Support NetBSD.
+https://github.com/grpc/grpc/issues/26357
+
+--- include/grpc/event_engine/port.h.orig	2021-05-25 05:21:42.595878017 +0000
++++ include/grpc/event_engine/port.h
+@@ -22,7 +22,7 @@
+ #elif defined(GPR_ANDROID) || defined(GPR_LINUX) || defined(GPR_APPLE) ||   \
+     defined(GPR_FREEBSD) || defined(GPR_OPENBSD) || defined(GPR_SOLARIS) || \
+     defined(GPR_AIX) || defined(GPR_NACL) || defined(GPR_FUCHSIA) ||        \
+-    defined(GRPC_POSIX_SOCKET)
++    defined(GRPC_POSIX_SOCKET) || defined(GPR_NETBSD)
+ #define GRPC_EVENT_ENGINE_POSIX
+ #include <netdb.h>
+ #include <netinet/in.h>
