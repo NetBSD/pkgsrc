249597e77133693040da60b9c0fd67202e42bf6e
nia <nia@pkgsrc.org>
2021-05-26 11:24:56 +0000

    mariadb104-server: sync with mariadb105-server

--- databases/mariadb104-server/buildlink3.mk
+++ databases/mariadb104-server/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/05/10 17:31:27 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/05/26 11:24:56 nia Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
 .if !defined(MYSQL_SERVER_BUILDLINK3_MK)
 MYSQL_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-server+=	mariadb-server>=10.4.0
-BUILDLINK_ABI_DEPENDS.mysql-server+=	mariadb-server>=10.4.0
+BUILDLINK_API_DEPENDS.mysql-server+=	mariadb-server>=10.4.0<10.5.0
+BUILDLINK_ABI_DEPENDS.mysql-server+=	mariadb-server>=10.4.0<10.5.0
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mariadb104-server
 BUILDLINK_LIBDIRS.mysql-server+=	lib
 
