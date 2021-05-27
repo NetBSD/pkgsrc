473b25f67b3280c60bf530655df1bcbf89d06985
manu <manu@pkgsrc.org>
2021-05-27 16:51:59 +0000

    Update mail/opendmarc to 1.4.1.1
    Changes since 1.4.0 from the RELEASE_NOTES file
            NOTE: In response to CVE-2019-20790, opendmarc has changed
                    how it evaluates headers added by previous
                    SPF milters.  Users are encouraged to read the
                    CVE-2019-20790 file in the "SECURITY" folder
                    for more details. (#49, #158).  Originally reported by
                    Jianjun Chen, feedback by Simon Wilson and
                    David Bürgin <dbuergin@gluet.ch>.
            NOTE: OpenDMARC's internal SPF handling will be removed
                    in a future version.  Users are encouraged to
                    build linked against libspf2.  Many pre-built
                    packages provided by OS packagers already do this.
                    (See https://www.libspf2.org)
            Addition of defines for MUSL C Library. (#129/#133).  Patches by
                    Marco Rebhan.
            Updated opendmarc.conf manpage and opendmarc.conf.sample to point to
                    https://publicsuffix.org/list/.
            Added a CONTRIBUTING document.
            Fix two #ifdefs in arc functions for strlcpy. (#138).  Reported by
                    Leo Bicknell.
            Fixes to MySQL Schema (#98/#99).  Patch by Bond Keevil.
            LIBSPF2 calls would not compile on OpenBSD due to OpenBSD not
                    having the ns_type definition in arpa/resolv.h.
                    Added detection to configure script.  (#134)
            Reworked hcreate_r calls to use hcreate, to compile natively on
                    OpenBSD and MacOS. (Part of #94)  Reported by Rupert
                    Gallagher.
            Add compatibility with AutoConf 2.70. (#95)
            Documentation updates about SourceForge being deprecated.  (#101)
            Only accept results from Received-SPF fields that indicate clearly
                    which identifier was being evaluated, since DMARC specifically
                    only wants results based on MAIL FROM.
            Many build-time fixes (#100, #91, #90, #86, #85, #84, #83, #82, #81)
                    Patches provided by Rupert Gallagher (ruga@protonmail.com)
            Added config option HoldQuarantinedMessages (default false), which
                    controls if messages with p=quarantine will be passed on to
                    the mail stream (if False) or placed in the MTA's "hold"
                    queue (if True).  Issue #105.  Patch by Marcos Moraes, on
                    the OpenDMARC mailing list.
            Remove "--with-wall" from "configure".  Suggested by Leo Bicknell.
            LIBOPENDMARC: Fix bug #50: Ignore all RRTYPEs other than TXT.
                    Problem reported by Jan Bouwhuis.
            LIBOPENDMARC: Fix bug #89: Repair absurd RRTYPE test in SPF code.
            LIBOPENDMARC: Fix bug #104: Fix bogus header field parsing code.
            LIBOPENDMARC: Fix bug #161: Don't pass the client IP address through
                    htonl() since it's already in network byte order.  This
                    was causing SPF errors when the internal SPF
                    implementation was in use.
            LIBOPENDMARC: Fix numerous problems with the internal SPF
                    implementation.

--- mail/opendmarc/patches/patch-opendmarc_opendmarc-arcares.c
+++ mail/opendmarc/patches/patch-opendmarc_opendmarc-arcares.c
@@ -1,10 +1,11 @@
-$NetBSD: patch-opendmarc_opendmarc-arcares.c,v 1.1 2021/02/17 01:49:12 manu Exp $
+$NetBSD: patch-opendmarc_opendmarc-arcares.c,v 1.2 2021/05/27 16:52:00 manu Exp $
 
 Avoid handling a NULL pointer when parsing a malformed header
 
---- opendmarc/opendmarc-arcares.c.orig	2021-02-16 16:33:34.454279528 +0000
-+++ opendmarc/opendmarc-arcares.c	2021-02-16 16:35:14.240570993 +0000
-@@ -324,8 +324,10 @@
+--- opendmarc/opendmarc-arcares.c.orig	2021-04-30 18:34:43.000000000 +0200
++++ opendmarc/opendmarc-arcares.c	2021-05-27 10:30:03.036068852 +0200
+@@ -265,8 +265,10 @@
+ 		token_ptr = token + leading_space_len;
  		if (*token_ptr == '\0')
  			return 0;
  		tag_label = strsep(&token_ptr, "=");
@@ -14,4 +15,3 @@ Avoid handling a NULL pointer when parsing a malformed header
  		tag_code = opendmarc_arcares_convert(aar_arc_tags, tag_label);
  
  		switch (tag_code)
- 		{
