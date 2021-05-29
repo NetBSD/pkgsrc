d2d5ceb4ce6682a485a066f027e8dbd862fab474
adam <adam@pkgsrc.org>
2021-05-29 06:14:40 +0000

    cmake: updated to 3.20.3
    CMake 3.20.3
    * Help: Use relative path for IDE Integration guide link to preset schema
    * BinUtils: Use more-private temporary variable names
    * ObjectiveC: Respect OSX_ARCHITECTURES for OBJC
    * FindBoost: Add support for Boost 1.76
    * Ninja: Restore support for Fortran in a symlinked build tree
    * Utilities/Sphinx: Update man page config for Sphinx 4
    * ExternalProject: Ensure git fetch if updating to hash we don't have yet
    * ExternalProject: Only add git config setting with git 1.7.7 or later
    * Ninja Multi-Config: Split long command lines by config
    * CMP0082: Check EXCLUDE_FROM_ALL property at generate time
    * GNU: C++17 default version
    * GNU: Final C++20 flags
    * GNU: C++23 support
    * CUDA: improve regex for CUDA Toolkit root from nvcc verbose output
    * cmCommandLineArgument: Correctly record parsing failures
    * cmake: `--build` and `--install` error out when encountering bad flags
    * cmCommandLineArgument: Provide more information syntax error messages
    * NVHPC: Support explicit language flags
    * NVHPC: Support Ninja dependency scanning

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.14 2021/04/30 06:45:14 adam Exp $
+# $NetBSD: version.mk,v 1.15 2021/05/29 06:14:40 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.20.2
+CMAKE_VERSION=	3.20.3
 CMAKE_API=	${CMAKE_VERSION:R}
