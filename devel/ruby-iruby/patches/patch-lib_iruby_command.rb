179b6677d7f6d5a0e94b507f3ab7274750c523fa
taca <taca@pkgsrc.org>
2021-05-29 15:04:31 +0000

    devel/ruby-iruby: update to 0.7.0
    0.7.0 (2021-05-28)
    
    Enhancements
    * The default backend is changed to IRB (@mrkn)
    * Add IRuby::Kernel#switch_backend! method (@mrkn)
    
    Bug Fixes
    * Fix the handling of image/svg+xml
      https://github.com/SciRuby/iruby/pull/300,
      https://github.com/SciRuby/iruby/pull/301 (@kojix2)
    
    0.6.1 (2021-05-26)
    
    Bug Fixes
    * Follow the messages and hooks orders during execute_request processing
      (@mrkn)
    
    0.6.0 (2021-05-25)
    
    Bug Fixes
    * Fix the handling of application/javascript
      https://github.com/SciRuby/iruby/issues/292,
      https://github.com/SciRuby/iruby/pull/294 (@kylekyle, @mrkn)
    
    Enhancements
    * Add the initialized event in IRuby::Kernel class
      https://github.com/SciRuby/iruby/pull/168,
      https://github.com/SciRuby/iruby/pull/296 (@Yuki-Inoue, @mrkn)
    * Add the following four events https://github.com/SciRuby/iruby/pull/295
      (@mrkn):
      - pre-execute -- occurs before every code execution
      - pre-run-cell -- occurs before every non-silent code execution
      - post-execute -- occurs after every code execution
      - post-run-cell -- occurs after every non-silent code execution
    * Replace Bond with IRB in PlainBackend
      https://github.com/SciRuby/iruby/pull/276,
      https://github.com/SciRuby/iruby/pull/297 (@cfis, @mrkn)

--- devel/ruby-iruby/patches/patch-lib_iruby_command.rb
+++ devel/ruby-iruby/patches/patch-lib_iruby_command.rb
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_iruby_command.rb,v 1.1 2020/09/20 08:57:32 ryoon Exp $
+$NetBSD: patch-lib_iruby_command.rb,v 1.2 2021/05/29 15:04:32 taca Exp $
 
 * For versioned ipython command from pkgsrc.
 
---- lib/iruby/command.rb.orig	2020-09-18 09:20:04.795404140 +0000
+--- lib/iruby/command.rb.orig	2021-05-29 02:19:05.405882756 +0000
 +++ lib/iruby/command.rb
-@@ -115,7 +115,7 @@ Try `ipython help` for more information.
+@@ -111,7 +111,7 @@ Try `ipython help` for more information.
  
      def check_version
        required = '3.0.0'
@@ -13,12 +13,12 @@ $NetBSD: patch-lib_iruby_command.rb,v 1.1 2020/09/20 08:57:32 ryoon Exp $
        if version < required
          STDERR.puts "Your IPython version #{version} is too old, at least #{required} is required"
          exit 1
-@@ -131,7 +131,7 @@ Try `ipython help` for more information.
+@@ -127,7 +127,7 @@ Try `ipython help` for more information.
        check_registered_kernel
        check_bundler {|e| STDERR.puts "Could not load bundler: #{e.message}" }
  
--      Kernel.exec('ipython', *@args)
-+      Kernel.exec('ipython-@PYVERSSUFFIX@', *@args)
+-      Process.exec('ipython', *@args)
++      Process.exec('ipython-@PYVERSSUFFIX@', *@args)
      end
  
      def check_registered_kernel
