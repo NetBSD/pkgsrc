da399e473282cb74ca15ce6d9eb8b92d7aa021ed
pin <pin@pkgsrc.org>
2021-05-29 17:21:42 +0000

    textproc/molybdenum: import package
    Recursive _search and replace_ CLI application.
    
    Powerful search can be found without problems, eg, grep, ack, ag, ripgrep
    or broot.
    
    Tools for replacing recursively in a folder are more difficult to find,
    although some exist: fart-it. Typically, people use a combination of searching,
    xargs and a replacement tool like sed or rpl.
    
    I use code searching a lot to investigate a large source code base before
    attempting a replace. Even with 100k files, search is fast and fairly easy.
    Recursively replacing text is much more dangerous, especially if it requires
    the combination of several less frequently used tools; it's difficult to
    remember a search-xargs-replace combination if not used on a daily basis.
    On top of this, the search tool used to filter the set of files and perform a
    dry-run, is not per-se using the same search query as the replace tool.
    After all, these are different tools. It would be better if a single tool could
    be used for every-day searching and replacing.
    This is exactly what The Molybdenum Replacer intends to achieve.

---
+++ textproc/molybdenum/cargo-depends.mk
@@ -0,0 +1,14 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2021/05/29 17:21:42 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	aho-corasick-0.7.18
+CARGO_CRATE_DEPENDS+=	atty-0.2.14
+CARGO_CRATE_DEPENDS+=	colored-2.0.0
+CARGO_CRATE_DEPENDS+=	hermit-abi-0.1.18
+CARGO_CRATE_DEPENDS+=	lazy_static-1.4.0
+CARGO_CRATE_DEPENDS+=	libc-0.2.94
+CARGO_CRATE_DEPENDS+=	memchr-2.4.0
+CARGO_CRATE_DEPENDS+=	regex-1.5.4
+CARGO_CRATE_DEPENDS+=	regex-syntax-0.6.25
+CARGO_CRATE_DEPENDS+=	winapi-0.3.9
+CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
+CARGO_CRATE_DEPENDS+=	winapi-x86_64-pc-windows-gnu-0.4.0
