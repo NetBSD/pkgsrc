ce07488acdb9cd56e3f57b0c0726363ee2ed0b56
wiz <wiz@pkgsrc.org>
2021-06-01 00:32:07 +0000

    x11/gtkmm4: import gtkmm4-4.2.0
    gtkmm is the official C++ interface for the popular GUI library GTK+. Highlights
    include typesafe callbacks, and a comprehensive set of widgets that are easily
    extensible via inheritance. You can create user interfaces either in code or
    with the Glade User Interface designer, using Gtk::Builder. There's extensive
    documentation, including API reference and a tutorial.
    
    This is major version 4 of the library using interfaces for GTK4.

---
+++ x11/gtkmm4/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/06/01 00:32:07 wiz Exp $
+
+BUILDLINK_TREE+=	gtkmm4
+
+.if !defined(GTKMM4_BUILDLINK3_MK)
+GTKMM4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtkmm4+=	gtkmm4>=4.2.0
+BUILDLINK_PKGSRCDIR.gtkmm4?=	../../x11/gtkmm4
+
+.include "../../x11/gtk4/buildlink3.mk"
+.include "../../devel/glibmm2.68/buildlink3.mk"
+.include "../../devel/pangomm2.48/buildlink3.mk"
+.include "../../graphics/cairomm1.16/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
+.endif	# GTKMM4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtkmm4
