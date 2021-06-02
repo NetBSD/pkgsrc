66e01fff800f31c0d3098873adb590fda5f40a1c
khorben <khorben@pkgsrc.org>
2021-06-02 23:27:10 +0000

    py-angr: update to version 9.0.7833
    This also affects py-{ailment,archinfo,cle,pyvex,claripy}, which get updated to
    the same version at the same time.
    
    I could not find any specific release notes or changelog.
    The whole list of changes can be found at:
    https://github.com/angr/angr/compare/v9.0.4378...v9.0.7833.

--- devel/py-angr/patches/patch-setup.py
+++ devel/py-angr/patches/patch-setup.py
@@ -1,17 +1,17 @@
-$NetBSD: patch-setup.py,v 1.1 2020/08/19 16:25:08 riastradh Exp $
+$NetBSD: patch-setup.py,v 1.2 2021/06/02 23:27:11 khorben Exp $
 
 Allow unicorn 1.0.1, which is what we have in pkgsrc.
 
 Judging by the commit history, unicorn>=1.0.2 is needed only for
 building on Windows.
 
---- setup.py.orig	2020-07-28 02:00:38.000000000 +0000
+--- setup.py.orig	2021-05-18 20:52:07.000000000 +0000
 +++ setup.py
-@@ -129,7 +129,7 @@ if 'bdist_wheel' in sys.argv and '--plat
+@@ -130,7 +130,7 @@ if 'bdist_wheel' in sys.argv and '--plat
          # https://www.python.org/dev/peps/pep-0425/
          sys.argv.append(name.replace('.', '_').replace('-', '_'))
  
--_UNICORN = "unicorn>=1.0.2rc4"
+-_UNICORN = "unicorn==1.0.2rc4"
 +_UNICORN = "unicorn>=1.0.1"
  
  setup(
