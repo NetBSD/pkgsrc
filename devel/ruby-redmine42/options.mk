67a6843f18f414b5335ce032c7ca2fe83349bc52
taca <taca@pkgsrc.org>
2021-06-02 16:23:48 +0000

    devel/ruby-redmine42: add package version 4.2.1
    Note: ruby-redmine42 supports not only ruby26 but also ruby27.
    
    
    Redmine is a flexible project management web application using the
    Ruby on Rails framework, it is cross-platform and cross-database.
    
    Redmine 4.2 introduces these new features in these categories:
    
    Authentication / User accounts
    * two-factor authentication.
    * email domains restriction for user accounts.
    
    Notifications
    * Notify users about high issues (only).
    
    Issues
    * Bulk addition of related issues.
    * Query links for subtasks on issue page.
    
    Watchers
    * Groups can be added as watchers for issues.
    * Forum threads can now be watched.
    * Watchers that are not going to receive a notification because they watch a
      non visible object (for ex: issue) are now marked in the UI as invalid.
    
    Text formatting
    * New toolbar button to insert a table.
    * Wiki table column sorting.
    * Languages in Highlighted code button in toolbar are now customizable by
      each user.
    
    Keyboard shortcuts
    
    * Switch between Edit/Preview tabs using Command/Ctrl + Shift + P.
    * Bold, italic and underline text using Command/Ctrl + b, Command/Ctrl + i
      and Command/Ctrl + u.
    * Submit a form using Ctrl+Enter / Command+Return.
    
    Activity improvements
    * UI options to filter activities by date or by user.
    * Slight design improvements.
    
    Another nice features
    * Download all attachments at once.
    * Auto complete wiki page links.
    * Auto-select fields mapping in Importing.
    * Fields with validation errors are now highlighted.

---
+++ devel/ruby-redmine42/options.mk
@@ -0,0 +1,59 @@
+# $NetBSD: options.mk,v 1.1 2021/06/02 16:23:48 taca Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.redmine42
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql sqlite3
+PKG_SUPPORTED_OPTIONS+=		ruby-unicorn
+PKG_SUGGESTED_OPTIONS=		mysql ruby-unicorn
+
+PLIST_VARS+=			unicorn
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Use mysql, pgsql, or sqlite3 backend
+###
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${RUBY_PKGPREFIX}-mysql2>=0.5.0<0.6:../../databases/ruby-mysql2
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${RUBY_PKGPREFIX}-pg>=1.2.2<1.3:../../databases/ruby-pg
+.elif !empty(PKG_OPTIONS:Msqlite3)
+DEPENDS+=	${RUBY_PKGPREFIX}-sqlite3>=1.4.0<1.5:../../databases/ruby-sqlite3
+.endif
+
+###
+### Use Unicorn web server
+###
+.if !empty(PKG_OPTIONS:Mruby-unicorn) || make (distinfo) || make (mdi)
+DEPENDS+=	${RUBY_PKGPREFIX}-unicorn-[0-9]*:../../www/ruby-unicorn
+
+PLIST.unicorn=	yes
+
+SUBST_CLASSES+=			prefix
+SUBST_STAGE.prefix=		pre-install
+SUBST_MESSAGE.prefix=		Setting PREFIX, RUBY_SUFFIX and RM_DIR.
+SUBST_FILES.prefix=		${WRKDIR}/unicorn.rb
+SUBST_VARS.prefix+=		PREFIX RUBY_SUFFIX RM_DIR
+
+RCD_SCRIPTS+=	redmine42_unicorn${RUBY_SUFFIX}
+RCD_SCRIPT_SRC.redmine42_unicorn${RUBY_SUFFIX}=	${FILESDIR}/redmine42_unicorn.sh
+
+CONF_FILES+=	${EGDIR}/unicorn.rb.example \
+		${PREFIX}/${RM_DIR}/config/unicorn.rb
+
+PLIST_SRC=	${PLIST_SRC_DFLT} ${RM_PLIST_SRC}
+
+post-extract:
+	${CP} ${FILESDIR}/unicorn.rb ${WRKDIR}/unicorn.rb
+
+.PHONY: unicorn-post-install
+unicorn-post-install:
+	${CP} ${WRKDIR}/unicorn.rb \
+		${DESTDIR}${EGDIR}/unicorn.rb.example
+	${CP} ${FILESDIR}/Gemfile.local \
+		${DESTDIR}${PREFIX}/${RM_DIR}
+.endif
+.PHONY: unicorn-post-install
+unicorn-post-install:
+# nothing
