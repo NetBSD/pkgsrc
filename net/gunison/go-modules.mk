75240586faa09a1d07b6db0a7aba11846cf596fd
wiz <wiz@pkgsrc.org>
2021-06-02 19:59:23 +0000

    net/gunison: import gunison-0.1.0
    Gunison is a new GUI (GTK 3) frontend for the Unison file synchronizer.
    Unison already has a built-in GTK 2 frontend, but Gunison is nicer
    and more convenient. Gunison works by wrapping Unison's console
    frontend in an expect-like fashion.

---
+++ net/gunison/go-modules.mk
@@ -0,0 +1,20 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/06/02 19:59:23 wiz Exp $
+
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.zip
+GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.5.3-0.20210514043925-3f44af595c5e.mod
+GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.5.3-0.20210514043925-3f44af595c5e.zip
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/skratchdot/open-golang/@v/v0.0.0-20200116055534-eef842397966.mod
+GO_MODULE_FILES+=	github.com/skratchdot/open-golang/@v/v0.0.0-20200116055534-eef842397966.zip
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.7.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.7.0.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.zip
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.zip
+GO_MODULE_FILES+=	pgregory.net/rapid/@v/v0.4.5.mod
+GO_MODULE_FILES+=	pgregory.net/rapid/@v/v0.4.5.zip
