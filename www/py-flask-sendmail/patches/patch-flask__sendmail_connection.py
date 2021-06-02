8968c2283659b5a1577e3e3eb7f95cf5a6a96b88
khorben <khorben@pkgsrc.org>
2021-06-02 21:06:03 +0000

    py-flask-sendmail: improve and import a couple fixes
    The updated patches:
    - fix the suppressing of messages when testing or disabled in configuration;
    - import what should be a better fix when piping messages to sendmail(1).
    
    Bumps PKGREVISION.

--- www/py-flask-sendmail/patches/patch-flask__sendmail_connection.py
+++ www/py-flask-sendmail/patches/patch-flask__sendmail_connection.py
@@ -1,15 +1,19 @@
-$NetBSD: patch-flask__sendmail_connection.py,v 1.1 2021/05/27 22:25:05 khorben Exp $
+$NetBSD: patch-flask__sendmail_connection.py,v 1.2 2021/06/02 21:06:03 khorben Exp $
 
-Fix type error when sending messages
+Do not send messages when testing or disabled in configuration
+
+Submitted upstream in PR #12 (at
+https://github.com/ajford/flask-sendmail/pull/12).
 
 --- flask_sendmail/connection.py.orig	2012-04-23 02:11:53.000000000 +0000
 +++ flask_sendmail/connection.py
-@@ -20,7 +20,7 @@ class Connection(object):
+@@ -18,6 +18,9 @@ class Connection(object):
+         pass
+ 
      def send(self, message):
++        if self.suppress:
++            return 0
++
          sm = Popen([self.mail.mailer, self.mail.mailer_flags], stdin=PIPE,
                     stdout=PIPE, stderr=STDOUT)
--        sm.stdin.write(message.dump())
-+        sm.stdin.write(message.dump().encode())
-         sm.communicate()
- 
-         return sm.returncode
+         sm.stdin.write(message.dump())
