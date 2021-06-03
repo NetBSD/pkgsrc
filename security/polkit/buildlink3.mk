8d6382967c96260b0395465934a38548750cc14c
wiz <wiz@pkgsrc.org>
2021-06-03 14:43:07 +0000

    polkit: update to 0.119.
    Fix some pkglint, including renaming a patch to match the file it applies
    to.
    
    This is polkit 0.119.
    
    Highlights:
     meson build system added
     CVE-2021-3560 mitigation
    
    Build requirements
    
     glib, gobject, gio    >= 2.32
     mozjs-78
     gobject-introspection >= 0.6.2 (optional)
     pam (optional)
     ConsoleKit OR systemd
    
    Changes since polkit 0.118:
    
     Inigo Martinez:
        meson build system added alongside autotools (many thanks!!)
    
     Matthias Classen:
        properties in text listener
    
     René Genz:
        typos fixups
    
     Balázs Úr:
        Update Hungarian translation
    
     Hendrik Werner:
        meson post-install script mod to avoid calling external processes
    
     Kevin Backhouse, Jan Rybar:
        CVE-2021-3560 mitigation
    
    Many thanks to all contributors!
    
    Jan Rybar et al.,
    June 3, 2021

--- security/polkit/buildlink3.mk
+++ security/polkit/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/01/19 22:36:19 nia Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.6 2021/06/03 14:43:07 wiz Exp $
 
 BUILDLINK_TREE+=	polkit
 
 .if !defined(POLKIT_BUILDLINK3_MK)
+POLKIT_BUILDLINK3_MK:=
+
 # configure: error: *** A compiler with support for C++17 language features is required.
 GCC_REQD+=	7
-
-POLKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.polkit+=	polkit>=0.103
 BUILDLINK_ABI_DEPENDS.polkit?=	polkit>=0.118nb1
