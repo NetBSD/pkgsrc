680c6b6b68e0b87adc1ae17264d42236ba16b03a
nia <nia@pkgsrc.org>
2021-06-06 13:38:43 +0000

    bozohttpd: define LICENSE, add lua and ssl options, honor CFLAGS

--- www/bozohttpd/options.mk
+++ www/bozohttpd/options.mk
@@ -1,17 +1,28 @@
-# $NetBSD: options.mk,v 1.3 2014/01/02 09:02:22 mrg Exp $
+# $NetBSD: options.mk,v 1.4 2021/06/06 13:38:43 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bozohttpd
-PKG_SUPPORTED_OPTIONS=	bozohttpd-do-htpasswd
+PKG_SUPPORTED_OPTIONS=	lua ssl
+PKG_SUGGESTED_OPTIONS=	lua ssl
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### Support Apache's .htpasswd files.
+### Support for Lua
 ###
-.if !empty(PKG_OPTIONS:Mbozohttpd-do-htpasswd)
-CPPFLAGS+=	-DDO_HTPASSWD
+.if !empty(PKG_OPTIONS:Mlua)
+.  include "../../lang/lua/buildlink3.mk"
+MAKE_FLAGS+=	EXTRALIBS="-llua"
+.else
+BOZO_CFLAGS+=	-DNO_LUA_SUPPORT
 .endif
 
 ###
-### Support for lua -- missing
+### Support for SSL
 ###
+.if !empty(PKG_OPTIONS:Mssl)
+CPPFLAGS+=	-DDO_HTPASSWD
+.  include "../../security/openssl/buildlink3.mk"
+.else
+BOZO_CFLAGS+=	-DNO_SSL_SUPPORT
+MAKE_FLAGS+=	CRYPTOLIBS=""
+.endif
