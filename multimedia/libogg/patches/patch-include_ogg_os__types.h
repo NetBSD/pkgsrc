0c1d1ef5ae742d31d3022415d997b9f64791d4c5
adam <adam@pkgsrc.org>
2021-06-07 09:12:32 +0000

    libogg: updated to 1.3.5
    Version 1.3.5 (2020 June 3)
    * Fix unsigned typedef problem on macOS.
    * Fix overflow check in ogg_sync_buffer.
    * Clean up cmake and autotools build files.
    * Remove Symbian and Apple XCode build files.
    * Fix documentation cross-reference links.

--- multimedia/libogg/patches/patch-include_ogg_os__types.h
+++ multimedia/libogg/patches/patch-include_ogg_os__types.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-include_ogg_os__types.h,v 1.1 2019/09/08 19:11:30 dbj Exp $
+$NetBSD: patch-include_ogg_os__types.h,v 1.2 2021/06/07 09:12:32 adam Exp $
 
 use stdint.h on macos
 This was broken by a recent change.  See comments in:
 https://gitlab.xiph.org/xiph/ogg/issues/2294
 
---- include/ogg/os_types.h.orig	2019-08-13 16:31:53.000000000 +0000
+--- include/ogg/os_types.h.orig	2021-06-07 08:37:18.000000000 +0000
 +++ include/ogg/os_types.h
 @@ -70,7 +70,7 @@
  
@@ -13,5 +13,5 @@ https://gitlab.xiph.org/xiph/ogg/issues/2294
 -#  include <sys/types.h>
 +#  include <stdint.h>
     typedef int16_t ogg_int16_t;
-    typedef uint16_t ogg_uint16_t;
+    typedef u_int16_t ogg_uint16_t;
     typedef int32_t ogg_int32_t;
