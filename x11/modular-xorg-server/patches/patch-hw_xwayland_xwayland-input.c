a2b583d466b35f86cb67959976964d59a8304375
khorben <khorben@pkgsrc.org>
2021-06-08 22:51:11 +0000

    modular-xorg-xephyr: fix build on Solaris 10
    From pkg/56169; thanks for reporting!

--- x11/modular-xorg-server/patches/patch-hw_xwayland_xwayland-input.c
+++ x11/modular-xorg-server/patches/patch-hw_xwayland_xwayland-input.c
@@ -1,13 +1,22 @@
-$NetBSD: patch-hw_xwayland_xwayland-input.c,v 1.2 2019/10/29 22:46:39 nia Exp $
+$NetBSD: patch-hw_xwayland_xwayland-input.c,v 1.3 2021/06/08 22:51:11 khorben Exp $
 
+Fix build on Solaris 10/amd64 (from pkg/56169)
 Support non-evdev platforms.
 
---- hw/xwayland/xwayland-input.c.orig	2019-02-26 19:28:50.000000000 +0000
+--- hw/xwayland/xwayland-input.c.orig	2021-04-13 14:11:40.000000000 +0000
 +++ hw/xwayland/xwayland-input.c
-@@ -26,7 +26,14 @@
+@@ -26,7 +26,22 @@
  
  #include "xwayland.h"
  
++#if defined(HAVE_NBCOMPAT_H)
++#include <nbcompat/config.h>
++#include <nbcompat/cdefs.h>
++#include <nbcompat/string.h>
++#else
++#include <string.h>
++#endif
++
 +#if defined(__linux__) || defined(__FreeBSD__)
  #include <linux/input.h>
 +#else
