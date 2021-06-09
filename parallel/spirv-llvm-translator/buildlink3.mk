09d5011559bb6cc0f82bd9463eead9202ee6393f
nia <nia@pkgsrc.org>
2021-06-09 14:56:51 +0000

    spirv-llvm-translator: add gcc requirement

--- parallel/spirv-llvm-translator/buildlink3.mk
+++ parallel/spirv-llvm-translator/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/06/09 14:55:10 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/06/09 14:56:51 nia Exp $
 
 BUILDLINK_TREE+=	spirv-llvm-translator
 
 .if !defined(SPIRV_LLVM_TRANSLATOR_BUILDLINK3_MK)
 SPIRV_LLVM_TRANSLATOR_BUILDLINK3_MK:=
+
+GCC_REQD+=	5 # C++14
 
 BUILDLINK_API_DEPENDS.spirv-llvm-translator+=	spirv-llvm-translator>=10.0.0
 BUILDLINK_PKGSRCDIR.spirv-llvm-translator?=	../../parallel/spirv-llvm-translator
