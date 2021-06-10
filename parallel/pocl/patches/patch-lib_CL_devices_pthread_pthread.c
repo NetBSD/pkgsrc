486d344d33bd7b31af971927a36333e4f20dc537
nia <nia@pkgsrc.org>
2021-06-10 09:57:39 +0000

    add parallel/pocl
    OpenCL (Open Computing Language) is an open, royalty-free standard for
    cross-platform, parallel programming of diverse accelerators found in
    supercomputers, cloud servers, personal computers, mobile devices and embedded
    platforms.
    
    PoCL is a portable open source (MIT-licensed) implementation of the OpenCL
    standard (1.2 with some 2.0 features supported). In addition to being an easily
    portable multi-device (truely heterogeneous) open-source OpenCL implementation,
    a major goal of this project is improving interoperability of diversity of
    OpenCL-capable devices by integrating them to a single centrally orchestrated
    platform.

---
+++ parallel/pocl/patches/patch-lib_CL_devices_pthread_pthread.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-lib_CL_devices_pthread_pthread.c,v 1.1 2021/06/10 09:57:40 nia Exp $
+
+Detect dlfcn.h (portable) instead of libdl (not portable).
+
+--- lib/CL/devices/pthread/pthread.c.orig	2021-05-19 08:12:19.000000000 +0000
++++ lib/CL/devices/pthread/pthread.c
+@@ -51,8 +51,8 @@
+ #include "pocl_util.h"
+ #include "pocl_mem_management.h"
+ 
+-#ifndef HAVE_LIBDL
+-#error Pthread driver requires DL library
++#ifndef HAVE_DLFCN_H
++#error Pthread driver requires dlopen
+ #endif
+ 
+ #ifdef OCS_AVAILABLE
