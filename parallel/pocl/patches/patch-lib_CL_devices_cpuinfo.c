bb1b7a9ae0222527135919797d7705a1a6afb599
nia <nia@pkgsrc.org>
2021-06-10 11:18:06 +0000

    pocl: Fix a null pointer dererence if sysfs is not present *sigh*

---
+++ parallel/pocl/patches/patch-lib_CL_devices_cpuinfo.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-lib_CL_devices_cpuinfo.c,v 1.1 2021/06/10 11:18:06 nia Exp $
+
+Do not segfault if /sys/bus/pci/devices/0000:00:00.0/vendor does not exist.
+
+--- lib/CL/devices/cpuinfo.c.orig	2021-05-19 08:12:19.000000000 +0000
++++ lib/CL/devices/cpuinfo.c
+@@ -416,9 +416,12 @@ pocl_cpuinfo_get_cpu_name_and_vendor(cl_
+   if (!device->vendor_id)
+     {
+       f = fopen (pci_bus_root_vendor_file, "r");
+-      num_read = fscanf (f, "%x", &device->vendor_id);
+-      fclose (f);
+-      /* no error checking, if it failed we just won't have the info */
++      if (f)
++        {
++          /* no error checking, if it failed we just won't have the info */
++          num_read = fscanf (f, "%x", &device->vendor_id);
++          fclose (f);
++        }
+     }
+ }
+ 
