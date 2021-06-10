14897e6523ea0ef7ff23e1028a6af100f1097fad
nia <nia@pkgsrc.org>
2021-06-10 12:26:26 +0000

    pocl: add links to upstream pull requests for patches
    cpuinfo: check for fopen() failure when opening sysfs nodes to avoid segfault
    https://github.com/pocl/pocl/pull/948
    
    pocl_timing: fix detection of Unix monotonic clocks
    https://github.com/pocl/pocl/pull/949
    
    Fix detection of dlopen()/libdl
    https://github.com/pocl/pocl/pull/950

--- parallel/pocl/patches/patch-lib_CL_pocl__timing.c
+++ parallel/pocl/patches/patch-lib_CL_pocl__timing.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-lib_CL_pocl__timing.c,v 1.1 2021/06/10 09:57:40 nia Exp $
+$NetBSD: patch-lib_CL_pocl__timing.c,v 1.2 2021/06/10 12:26:26 nia Exp $
 
 NetBSD does not have CLOCK_UPTIME_FAST.
+https://github.com/pocl/pocl/pull/949
 
 --- lib/CL/pocl_timing.c.orig	2021-05-19 08:12:19.000000000 +0000
 +++ lib/CL/pocl_timing.c
