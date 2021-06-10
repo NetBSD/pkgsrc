0e8c08a391f87299ca8e370263b3917dd0de7452
nia <nia@pkgsrc.org>
2021-06-10 12:48:49 +0000

    Make wm/blackbox the latest version, remove weird versioned packages
    concept ok'd by gutteridge

--- wm/bbpager/patches/patch-src_main.cxx
+++ wm/bbpager/patches/patch-src_main.cxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_main.cxx,v 1.1 2011/11/25 22:22:05 joerg Exp $
+$NetBSD: patch-src_main.cxx,v 1.1 2021/06/10 12:48:50 nia Exp $
 
 --- src/main.cxx.orig	2011-11-25 17:02:24.000000000 +0000
 +++ src/main.cxx
