0a817f06bb5b3d95ffca4c89367af188347545db
gutteridge <gutteridge@pkgsrc.org>
2021-06-11 20:14:37 +0000

    blackbox: adjust options.mk to current package name

--- wm/blackbox/options.mk
+++ wm/blackbox/options.mk
@@ -1,8 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2021/06/10 19:05:47 gutteridge Exp $
+# $NetBSD: options.mk,v 1.2 2021/06/11 20:14:37 gutteridge Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.blackbox70
+PKG_OPTIONS_VAR=	PKG_OPTIONS.blackbox
 PKG_SUPPORTED_OPTIONS=	nls xft2
 PKG_SUGGESTED_OPTIONS=	nls xft2
+
+.include "../../mk/bsd.prefs.mk"
+
+# This package was previously named blackbox70.
+.if defined(PKG_OPTIONS.blackbox70)
+PKG_LEGACY_OPTIONS+=	${PKG_OPTIONS.blackbox70}
+PKG_OPTIONS_DEPRECATED_WARNINGS+=	\
+	"Deprecated variable PKG_OPTIONS.blackbox70 used, use "${PKG_OPTIONS_VAR:Q}" instead."
+.endif
 
 .include "../../mk/bsd.options.mk"
 
