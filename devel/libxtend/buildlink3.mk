fccc1e4b5748aea47f13b2949ea76e96b1e3093d
bacon <bacon@pkgsrc.org>
2021-06-11 00:40:59 +0000

    devel/libxtend: import libxtend-0.1.2.4
    Libxtend is a library of miscellaneous functions, the likes of
    which might be found in libc or libm.  It provides some convenient
    functionality lacking in standard library functions as well some
    more esoteric features.

---
+++ devel/libxtend/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/06/11 00:40:59 bacon Exp $
+
+BUILDLINK_TREE+=		libxtend
+
+.if !defined(LIBXTEND_BUILDLINK3_MK)
+LIBXTEND_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.2.4
+BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
+.endif	# LIBXTEND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libxtend
