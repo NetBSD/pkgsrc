4f7e971a4746719c59ec2f4b941fb741f3b41921
adam <adam@pkgsrc.org>
2021-06-11 05:24:08 +0000

    py-xmlschema: updated to 1.6.4
    v1.6.4
    * Add testing config for Python 3.10 (Tox and CI)
    * Fix internal _PurePath class with Python 3.10
    * Remove redundant xmlns="" declaration when encoding with lxml
    
    v1.6.3
    * Refactor normalize_url() using pathlib.PurePath
    * Support UNC paths
    * Fix API docs

--- textproc/py-xmlschema/patches/patch-setup.py
+++ textproc/py-xmlschema/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2020/09/14 08:57:38 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2021/06/11 05:24:08 adam Exp $
 
 Fix for Python 3.6.
 
---- setup.py.orig	2020-09-13 19:12:09.000000000 +0000
+--- setup.py.orig	2021-06-09 17:22:21.000000000 +0000
 +++ setup.py
-@@ -10,7 +10,7 @@
- #
- from setuptools import setup, find_packages
+@@ -12,7 +12,7 @@ from setuptools import setup, find_packa
+ from pathlib import Path
  
--with open("README.rst") as readme:
-+with open("README.rst", encoding='utf-8') as readme:
+ 
+-with Path(__file__).parent.joinpath('README.rst').open() as readme:
++with Path(__file__).parent.joinpath('README.rst').open(encoding='utf-8') as readme:
      long_description = readme.read()
  
  
