6bfad400353fc95aa97059d1e0b79b4f67bf4cf3
bacon <bacon@pkgsrc.org>
2021-06-11 17:04:55 +0000

    biology/biolibc: Update to 0.1.3
    Import sam_buff_t class and VCF functions from ad2vcf
    Add BED and GFF support
    Isolate headers under include/biolibc
    Numerous small enhancements and fixes
    
    Upstream change log: https://github.com/auerlab/biolibc/releases

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/03/24 15:20:08 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/06/11 17:04:55 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.1
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.3
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
-BUILDLINK_DEPMETHOD.biolibc?=	build
 .endif	# BIOLIBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-biolibc
