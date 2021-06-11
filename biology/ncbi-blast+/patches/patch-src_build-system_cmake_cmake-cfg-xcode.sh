1192ec602695050e0ee08bcdafbd658f35bb9822
bacon <bacon@pkgsrc.org>
2021-06-11 13:47:39 +0000

    biology/ncbi-blast+: Update to 2.11.0
    Release notes: https://www.ncbi.nlm.nih.gov/books/NBK131777/

---
+++ biology/ncbi-blast+/patches/patch-src_build-system_cmake_cmake-cfg-xcode.sh
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_build-system_cmake_cmake-cfg-xcode.sh,v 1.1 2021/06/11 13:47:40 bacon Exp $
+
+# Portability
+
+--- src/build-system/cmake/cmake-cfg-xcode.sh.orig	2021-06-10 11:54:05.800996703 +0000
++++ src/build-system/cmake/cmake-cfg-xcode.sh
+@@ -218,7 +218,7 @@ CMAKE_ARGS="$CMAKE_ARGS -DBUILD_SHARED_L
+ 
+ if [ -z "$BUILD_ROOT" ]; then
+   BUILD_ROOT=CMake-${CC_NAME}${CC_VERSION}
+-  if [ "$BUILD_SHARED_LIBS" == "ON" ]; then
++  if [ "$BUILD_SHARED_LIBS" = "ON" ]; then
+     BUILD_ROOT="$BUILD_ROOT"-DLL
+   fi
+ fi
