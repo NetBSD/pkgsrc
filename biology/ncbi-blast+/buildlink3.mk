ecf9746b887c6aa472699f282aca775b79e80316
bacon <bacon@pkgsrc.org>
2021-06-11 13:47:39 +0000

    biology/ncbi-blast+: Update to 2.11.0
    Release notes: https://www.ncbi.nlm.nih.gov/books/NBK131777/

--- biology/ncbi-blast+/buildlink3.mk
+++ biology/ncbi-blast+/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/04/29 21:00:04 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/06/11 13:47:40 bacon Exp $
 
 BUILDLINK_TREE+=	ncbi-blast+
 
 .if !defined(NCBI_BLAST+_BUILDLINK3_MK)
 NCBI_BLAST+_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.7.1
+BUILDLINK_API_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.11.0
 BUILDLINK_PKGSRCDIR.ncbi-blast+?=	../../biology/ncbi-blast+
 
 .endif	# NCBI_BLAST+_BUILDLINK3_MK
