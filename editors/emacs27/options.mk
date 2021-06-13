c6e94a7eca44c2d9e8dc334cec0de1448721a5d9
wiz <wiz@pkgsrc.org>
2021-06-13 20:28:12 +0000

    emacs27: switch to harfbuzz backend for fonts
    This is the dependency recommended by upstream.
    
    While here, clean up options:
    - remove undocumented duplicate name for the same option (lucid)
    - remove xaw3d from default options since it has no effect with gtk,
      which is on by default
    - remove gconf option, since gsettings (via glib) is preferred,
      and gconfmm dependency was wrong anyway
    
    Bump PKGREVISION.

--- editors/emacs27/options.mk
+++ editors/emacs27/options.mk
@@ -1,28 +1,27 @@
-# $NetBSD: options.mk,v 1.3 2020/10/21 10:51:58 kleink Exp $
+# $NetBSD: options.mk,v 1.4 2021/06/13 20:28:12 wiz Exp $
 
 ### Set options
 PKG_OPTIONS_VAR=			PKG_OPTIONS.emacs
-PKG_SUPPORTED_OPTIONS=			dbus gconf gnutls imagemagick jansson svg xaw3d xft2 xml
+PKG_SUPPORTED_OPTIONS=			dbus gnutls imagemagick jansson svg xaw3d xml
 # xaw3d is only valid with tookit = xaw
 
 PKG_OPTIONS_OPTIONAL_GROUPS+=		window-system
 PKG_OPTIONS_GROUP.window-system=	x11 nextstep
 
-# tempted to have 'nox11' :-)
-
 PKG_OPTIONS_OPTIONAL_GROUPS+=		toolkit
 PKG_SUGGESTED_OPTIONS.Darwin=		nextstep
-#  --with-x-toolkit=KIT    use an X toolkit (KIT one of: yes or  gtk2,
-#                          gtk3, lucid or athena, motif, no)
+#  --with-x-toolkit=KIT    use an X toolkit (KIT one of: yes or gtk2,
+#                          gtk3, xaw or lucid or athena, motif, no)
 # gtk in next line implies gtk2, xaw = athena = lucid
-PKG_OPTIONS_GROUP.toolkit=		gtk gtk2 gtk3 motif xaw lucid
+PKG_OPTIONS_GROUP.toolkit=		gtk gtk2 gtk3 motif xaw
 # gtk2 and gtk has the same effect
-# gtk3 is default in the logic below (even not included in SUGGESTED_=
-# gconf, gtk* and xft2 will be ignored for nextstep even shown as selected.
+# gtk3 is default in the logic below (even not included in SUGGESTED_=)
+# gtk* will be ignored for nextstep even shown as selected.
 
-# svg is omitted because it is rarely needed and either very
-# heavyweight or unmaintained.
-PKG_SUGGESTED_OPTIONS=	dbus gconf gnutls gtk3 jansson xaw3d xft2 xml x11
+# imagemagick is disabled because of stability/security
+# svg is omitted because it is rarely needed and heavyweight due to the rust dependency
+# xaw3d is omitted because it is only valid with xaw
+PKG_SUGGESTED_OPTIONS=	dbus gnutls gtk3 jansson xml x11
 
 .include "../../mk/bsd.options.mk"
 
@@ -67,7 +66,7 @@ CONFIGURE_ARGS+=	--without-gnutls
 .endif
 
 ###
-### Check non nextstep (implies x11) options  ---------------------
+### Check non nextstep (implies x11) options
 ###
 ### x11 is selected (as SUGGESTED above)
 ###
@@ -90,15 +89,6 @@ CONFIGURE_ARGS+=	--without-rsvg
 .  endif
 
 ###
-### Support gconf
-###
-.  if !empty(PKG_OPTIONS:Mgconf)
-.include "../../devel/gconfmm/buildlink3.mk"
-.  else
-CONFIGURE_ARGS+=	--without-gconf
-.  endif
-
-###
 ### Support ImageMagick (not recognized for now, sorry)
 ###
 .  if !empty(PKG_OPTIONS:Mimagemagick)
@@ -116,23 +106,13 @@ CONFIGURE_ARGS+=	--without-imagemagick
 .  else
 CONFIGURE_ARGS+=	--without-xaw3d
 .  endif
-### Enable font backend
-###
-.  if !empty(PKG_OPTIONS:Mxft2)
-.include "../../fonts/fontconfig/buildlink3.mk"
-.include "../../graphics/libotf/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../x11/libXft/buildlink3.mk"
-.include "../../devel/m17n-lib/buildlink3.mk"
-.  else
-CONFIGURE_ARGS+=	--without-xft --without-libotf --without-m17n-flt
-.  endif
 
 ###
 ### Toolkit selection
 ###
-.  if (empty(PKG_OPTIONS:Mxaw) && empty(PKG_OPTIONS:Mlucid) && \
-       empty(PKG_OPTIONS:Mgtk) && empty(PKG_OPTIONS:Mgtk2) && \
+.  if (empty(PKG_OPTIONS:Mxaw) && \
+       empty(PKG_OPTIONS:Mgtk) && \
+       empty(PKG_OPTIONS:Mgtk2) && \
        empty(PKG_OPTIONS:Mmotif))
 # defaults to gtk3
 USE_TOOLS+=		pkg-config
@@ -145,16 +125,13 @@ CONFIGURE_ARGS+=	--with-x-toolkit=gtk2
 .  elif !empty(PKG_OPTIONS:Mxaw)
 .include "../../mk/xaw.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-x-toolkit=athena
-.  elif !empty(PKG_OPTIONS:Mlucid)
-.include "../../mk/xaw.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-x-toolkit=athena
 .  elif !empty(PKG_OPTIONS:Mmotif)
 .include "../../mk/motif.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-x-toolkit=motif
 .  endif
 
 ###
-### End of Check non nextstep (implies x11) options  ---------------------
+### End of Check non nextstep (implies x11) options
 
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
@@ -164,6 +141,7 @@ CONFIGURE_ARGS+=	--with-x-toolkit=motif
 .include "../../x11/libXaw/buildlink3.mk"
 .include "../../x11/libXpm/buildlink3.mk"
 .include "../../x11/libXrender/buildlink3.mk"
+
 
 ###
 ### Support using NextStep (Cocoa or GNUstep) windowing system
@@ -198,7 +176,6 @@ post-install:
 		pax -rw -pp -pm Emacs.app ${DESTDIR}${PREFIX}/${APPLICATIONS_DIR}
 
 .else  # no window system
-#.if empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--without-x
 CONFIGURE_ARGS+=	--without-xpm
 CONFIGURE_ARGS+=	--without-jpeg
@@ -211,30 +188,3 @@ CONFIGURE_ARGS+=	--without-png
 # mode: outline-minor
 # outline-regexp: "\\(.[ \t]*\\(if\\|endif\\|else\\|elif\\|include.*options\\|PKG_SUGGES\\)\\)\\|### .\\|# Local"
 # End:
-
-### How To Test (or the possible combinations) -- watch the result of 'make configure'
-# Set PKG_OPTIONS.emacs=	result
-# ----------		----------------------------------------------------------
-# (none)		.. x11 gtk    svg gconf       xft2 dbus gnutls imagemagick
-
-#  xaw			.. x11 lucid  svg gconf xaw3d xft2 dbus gnutls imagemagick
-#  lucid		.. x11 lucid  svg gconf xaw3d xft2 dbus gnutls imagemagick
-#  motif		.. x11 motif  svg gconf       xft2 dbus gnutls imagemagick
-
-# -x11 nextstep		.. nextstep
-# -xft2			.. x11 gtk    svg gconf		   dbus gnutls imagemagick
-# -gnutls		.. x11 gtk    svg gconf       xft2 dbus	       imagemagick
-# -gnutls -imagemagick -dbus
-#                       .. x11 gtk    svg gconf       xft2
-# -x11 -svg -gconf -xaw3d -xft2
-#                       .. nox11	                   dbus gnutls             xml2
-# -x11 			.. nox11			   dbus gnutls imagemagick
-#			.. nox11 but several x11 libraries built and not used
-
-### Window system options and result matrix
-#        x11 | YES  none     NO
-#   nextstep |
-#   -------- +----- -------- -------
-#        YES | --   NextStep NextStep
-#       none | X11  X11      (no Window)
-#         NO | X11  X11      (no Window)
