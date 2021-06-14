bb95da093d1ea4c75f2b1873e5389e397279ad86
wiz <wiz@pkgsrc.org>
2021-06-14 22:39:01 +0000

    octave: fix URL in comment

--- math/octave/patches/patch-liboctave_wrappers_iconv-wrappers.c
+++ math/octave/patches/patch-liboctave_wrappers_iconv-wrappers.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-liboctave_wrappers_iconv-wrappers.c,v 1.1 2021/06/14 22:16:33 nros Exp $
+$NetBSD: patch-liboctave_wrappers_iconv-wrappers.c,v 1.2 2021/06/14 22:39:01 wiz Exp $
 
  * fix iconv_t void* confict pkg/56240
  from upstream:
- https://hg.savvannah.gnu.org/hgweb/octave/rev/69087119ee84
+ https://hg.savannah.gnu.org/hgweb/octave/rev/69087119ee84
 
 --- liboctave/wrappers/iconv-wrappers.c.orig	2021-02-19 17:36:34.000000000 +0000
 +++ liboctave/wrappers/iconv-wrappers.c
