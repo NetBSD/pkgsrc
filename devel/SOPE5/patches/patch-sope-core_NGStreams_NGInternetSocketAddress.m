63b150a2ddc28bbd3239fec0f98a36b70106a133
gdt <gdt@pkgsrc.org>
2021-06-14 16:51:39 +0000

    devel/SOPE5: Add version 5.1.0
    From pkg/55459 submitted by Dima Veselov, and followup via email, with
    some modifications by me.
    
    SOPE is an extensive set of frameworks which form a complete Web
    application server environment. Besides the Apple WebObjects
    compatible appserver extended with Zope concepts, it contains a large
    set of reusable classes: XML processing (SAX, DOM, XML-RPC),
    MIME/IMAP4 processing, LDAP connectivity, RDBMS connectivity, and
    iCalendar parsing.

---
+++ devel/SOPE5/patches/patch-sope-core_NGStreams_NGInternetSocketAddress.m
@@ -0,0 +1,17 @@
+$NetBSD: patch-sope-core_NGStreams_NGInternetSocketAddress.m,v 1.1 2021/06/14 16:51:39 gdt Exp $
+
+Add support for DragonFly.
+
+--- sope-core/NGStreams/NGInternetSocketAddress.m.orig	2012-11-15 18:51:03.000000000 +0100
++++ sope-core/NGStreams/NGInternetSocketAddress.m	2012-11-22 18:39:01.000000000 +0100
+@@ -47,9 +47,9 @@
+ #include "NGInternetSocketAddress.h"
+ #include "NGInternetSocketDomain.h"
+ #include "common.h"
+
+-#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__GLIBC__)
++#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__GLIBC__) && !defined(__DragonFly__)
+ #define USE_GETHOSTBYNAME_R 1
+ #endif
+
+ @implementation NGInternetSocketAddress
