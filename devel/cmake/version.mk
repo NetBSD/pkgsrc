69cbb6bc8c33e2e10e5ffd67a4735d9526a6f4c9
adam <adam@pkgsrc.org>
2021-06-15 10:27:33 +0000

    cmake: updated to 3.20.4
    CMake 3.20.4
    * ci: use consistent sccache builds
    * VS: Add special case for '-T version=14.29.16.10' under VS 16.10
    * VS: Add flag table entries for '/external:W*' flags in VS 16.10
    * gitlab-ci: Update Windows builds to MSVC 19.29-16.10 toolset
    * Makefiles: Fix CMAKE_EXPORT_COMPILE_COMMANDS crash with custom compile rule
    * presets: Fix buildPreset "jobs" field test case
    * IRSL: Add Intel oneAPI redist location on Windows
    * fileapi: Fix codemodel-v2 link command fragment relative paths
    * FindBoost: Add check for json component header in Boost 1.75+
    * Help: cmake_path: fix erroneous example for IS_PREFIX
    * MSVC: C++20 final flag, C++23 support
    * Clang/MSVC: C++20 final flag, C++23 support
    * presets: Fix buildPreset "jobs"
    * presets: Fix buildPreset "targets" not allowing a single string

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.15 2021/05/29 06:14:40 adam Exp $
+# $NetBSD: version.mk,v 1.16 2021/06/15 10:27:33 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.20.3
+CMAKE_VERSION=	3.20.4
 CMAKE_API=	${CMAKE_VERSION:R}
