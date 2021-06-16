7d7f4240c5d76639caa574b85256d99feffdd9b7
markd <markd@pkgsrc.org>
2021-06-16 10:33:51 +0000

    kconfig: buildlink libexec/kf5/* files. needed for latest cmake

--- devel/kconfig/buildlink3.mk
+++ devel/kconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2021/04/21 11:40:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2021/06/16 10:33:51 markd Exp $
 
 BUILDLINK_TREE+=	kconfig
 
@@ -8,6 +8,8 @@ KCONFIG_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kconfig+=	kconfig>=5.18.0
 BUILDLINK_ABI_DEPENDS.kconfig?=	kconfig>=5.80.0nb1
 BUILDLINK_PKGSRCDIR.kconfig?=	../../devel/kconfig
+
+BUILDLINK_FILES.kconfig+=	libexec/kf5/*
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KCONFIG_BUILDLINK3_MK
