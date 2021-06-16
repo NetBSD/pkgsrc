9108fef58d78da58ddfcb7724084034683c493fe
markd <markd@pkgsrc.org>
2021-06-16 12:48:12 +0000

    kdoctools: buildlink share/kf5/kdoctools/customization/*.xsl files.
    needed for latest cmake.

--- devel/kdoctools/buildlink3.mk
+++ devel/kdoctools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2021/04/21 11:40:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2021/06/16 12:48:12 markd Exp $
 
 BUILDLINK_TREE+=	kdoctools
 
@@ -8,6 +8,8 @@ KDOCTOOLS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kdoctools+=	kdoctools>=5.21.0
 BUILDLINK_ABI_DEPENDS.kdoctools?=	kdoctools>=5.80.0nb1
 BUILDLINK_PKGSRCDIR.kdoctools?=		../../devel/kdoctools
+
+BUILDLINK_FILES.kdoctools+=		share/kf5/kdoctools/customization/*.xsl
 
 .include "../../textproc/libxslt/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
