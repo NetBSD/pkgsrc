f3f7f725c1654f57b53ca780fd390be62cc99afc
markd <markd@pkgsrc.org>
2021-06-16 10:23:02 +0000

    kcoreaddons: buildlink bin/* files.  Needed for latest cmake.

--- devel/kcoreaddons/buildlink3.mk
+++ devel/kcoreaddons/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2021/04/21 11:40:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2021/06/16 10:23:02 markd Exp $
 
 BUILDLINK_TREE+=	kcoreaddons
 
@@ -8,6 +8,8 @@ KCOREADDONS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kcoreaddons+=	kcoreaddons>=5.18.0
 BUILDLINK_ABI_DEPENDS.kcoreaddons?=	kcoreaddons>=5.80.0nb1
 BUILDLINK_PKGSRCDIR.kcoreaddons?=	../../devel/kcoreaddons
+
+BUILDLINK_FILES.kcoreaddons+=		bin/*
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KCOREADDONS_BUILDLINK3_MK
