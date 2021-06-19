21fe9383aab42cf85a22dc464e7365af3c2ba7e1
markd <markd@pkgsrc.org>
2021-06-19 00:44:34 +0000

    kjs: more files needed in buildlink. Gets khtml building again.

--- www/kjs/buildlink3.mk
+++ www/kjs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2021/04/21 11:40:43 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2021/06/19 00:44:34 markd Exp $
 
 BUILDLINK_TREE+=	kjs
 
@@ -8,6 +8,8 @@ KJS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kjs+=	kjs>=5.21.0
 BUILDLINK_ABI_DEPENDS.kjs?=	kjs>=5.80.0nb1
 BUILDLINK_PKGSRCDIR.kjs?=	../../www/kjs
+
+BUILDLINK_FILES.kjs+=		share/kf5/kjs/create_hash_table
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KJS_BUILDLINK3_MK
