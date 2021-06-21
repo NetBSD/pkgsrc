03c89b41b10f4603697348ef690cad3ee65e4e5f
nia <nia@pkgsrc.org>
2021-06-21 10:23:48 +0000

    samba4: Warn before starting if procfs is not mounted on NetBSD...

--- net/samba4/files/samba.sh
+++ net/samba4/files/samba.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: samba.sh,v 1.1 2015/05/12 12:19:52 ryoon Exp $
+# $NetBSD: samba.sh,v 1.2 2021/06/21 10:23:48 nia Exp $
 #
 # PROVIDE: samba
 # REQUIRE: DAEMON
@@ -13,6 +13,15 @@ command="@PREFIX@/sbin/${name}"
 pidfile="@SMB_PID@/${name}.pid"
 required_files="@SMB_CONFIG@/smb.conf"
 extra_commands="reload"
+start_precmd="samba_precmd"
+
+samba_precmd()
+{
+	if ! [ -f /proc/cpuinfo ]; then
+		echo "WARNING: Samba requires a Linux-compatible procfs!"
+		echo "WARNING: Please mount /proc before starting Samba."
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
