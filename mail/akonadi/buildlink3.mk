c047f522074364281d376582d93215cbf4890252
markd <markd@pkgsrc.org>
2021-06-21 11:23:01 +0000

    akonadi: more buildlinked files. Should get kdepimlibs4 building again.

--- mail/akonadi/buildlink3.mk
+++ mail/akonadi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2020/08/17 20:17:34 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2021/06/21 11:23:01 markd Exp $
 
 BUILDLINK_TREE+=	akonadi
 
@@ -8,6 +8,8 @@ AKONADI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.akonadi+=	akonadi>=0.82.0
 BUILDLINK_ABI_DEPENDS.akonadi+=	akonadi>=1.13.0nb34
 BUILDLINK_PKGSRCDIR.akonadi?=	../../mail/akonadi
+
+BUILDLINK_FILES.akonadi+=	share/config/akonadi/*.conf
 
 .include "../../x11/qt4-libs/buildlink3.mk"
 .include "../../x11/qt4-qdbus/buildlink3.mk"
