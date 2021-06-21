068098b20aafee20160d996235744879ec08c7bf
nia <nia@pkgsrc.org>
2021-06-21 10:09:10 +0000

    clang: add BUILDLINK_FILES to work around cmake changes

--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2020/11/05 09:06:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2021/06/21 10:09:10 nia Exp $
 
 BUILDLINK_TREE+=	clang
 
@@ -8,6 +8,27 @@ CLANG_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.clang+=	clang>=9.0.1
 BUILDLINK_ABI_DEPENDS.clang?=	clang>=10.0.1nb2
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
+
+BUILDLINK_FILES.clang+=		bin/c-index-test
+BUILDLINK_FILES.clang+=		bin/clang
+BUILDLINK_FILES.clang+=		bin/clang++
+BUILDLINK_FILES.clang+=		bin/clang-10
+BUILDLINK_FILES.clang+=		bin/clang-check
+BUILDLINK_FILES.clang+=		bin/clang-cl
+BUILDLINK_FILES.clang+=		bin/clang-cpp
+BUILDLINK_FILES.clang+=		bin/clang-extdef-mapping
+BUILDLINK_FILES.clang+=		bin/clang-format
+BUILDLINK_FILES.clang+=		bin/clang-import-test
+BUILDLINK_FILES.clang+=		bin/clang-offload-bundler
+BUILDLINK_FILES.clang+=		bin/clang-offload-wrapper
+BUILDLINK_FILES.clang+=		bin/clang-refactor
+BUILDLINK_FILES.clang+=		bin/clang-rename
+BUILDLINK_FILES.clang+=		bin/clang-scan-deps
+BUILDLINK_FILES.clang+=		bin/diagtool
+BUILDLINK_FILES.clang+=		bin/git-clang-format
+BUILDLINK_FILES.clang+=		bin/hmaptool
+BUILDLINK_FILES.clang+=		bin/scan-build
+BUILDLINK_FILES.clang+=		bin/scan-view
 
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
