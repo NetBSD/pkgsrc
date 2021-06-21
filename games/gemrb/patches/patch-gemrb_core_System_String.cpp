f8f05a0ed1223f9e2d43c57b42e4b31ff49aa7f5
nia <nia@pkgsrc.org>
2021-06-21 14:02:47 +0000

    gemrb: fix broken patch

--- games/gemrb/patches/patch-gemrb_core_System_String.cpp
+++ games/gemrb/patches/patch-gemrb_core_System_String.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-gemrb_core_System_String.cpp,v 1.2 2021/06/06 16:59:51 nia Exp $
+$NetBSD: patch-gemrb_core_System_String.cpp,v 1.3 2021/06/21 14:02:47 nia Exp $
 
 Deal with old NetBSD/SunOS style iconv.
 
 --- gemrb/core/System/String.cpp.orig	2021-06-02 16:16:47.000000000 +0000
 +++ gemrb/core/System/String.cpp
-@@ -31,6 +31,16 @@
+@@ -31,6 +31,15 @@
  #include <iconv.h>
  #endif
  
@@ -16,12 +16,11 @@ Deal with old NetBSD/SunOS style iconv.
 +#define NETBSD_POSIX_ICONV 0
 +#endif /* __NetBSD_Prereq__(9,99,17) */
 +#endif /* defined(__NetBSD__) */
-+#endif
 +
  namespace GemRB {
  
  static String* StringFromEncodedData(const ieByte* string, const EncodingStruct& encoded)
-@@ -123,7 +133,11 @@ char* ConvertCharEncoding(const char* st
+@@ -123,7 +132,11 @@ char* ConvertCharEncoding(const char* st
  	size_t out_len_left = out_len;
  	char* buf = (char*) malloc(out_len);
  	char* buf_out = buf;
