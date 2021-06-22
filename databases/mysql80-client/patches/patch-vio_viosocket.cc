b3bf6e260a13484950ed6d66d98e338c493af368
nia <nia@pkgsrc.org>
2021-06-22 10:12:36 +0000

    mysql80: Fix build on NetBSD 9.x
    - Disable kqueue when EVFILT_USER is detected as missing by CMake.
    - Use Linux procfs when F_GETPATH is missing.
    - Define ppoll to pollts.

--- databases/mysql80-client/patches/patch-vio_viosocket.cc
+++ databases/mysql80-client/patches/patch-vio_viosocket.cc
@@ -1,17 +1,18 @@
-$NetBSD: patch-vio_viosocket.cc,v 1.1 2021/05/13 15:25:20 jdolecek Exp $
+$NetBSD: patch-vio_viosocket.cc,v 1.2 2021/06/22 10:12:37 nia Exp $
 
-* don't use kqueue on older NetBSD which misses EVFILT_USER
-
-$NetBSD: patch-vio_viosocket.cc,v 1.1 2021/05/13 15:25:20 jdolecek Exp $
+* define ppoll on older NetBSD that only has pollts
 
 --- vio/viosocket.cc.orig	2021-03-22 08:44:50.000000000 +0000
 +++ vio/viosocket.cc
-@@ -70,6 +70,10 @@
+@@ -70,6 +70,13 @@
  
  #include "mysql/psi/mysql_socket.h"
  
-+#if defined(HAVE_QUEUE) && !defined(EVFILT_USER)
-+#undef HAVE_KQUEUE
++#ifdef __NetBSD__
++#include <sys/param.h>
++#if !__NetBSD_Prereq__(9,99,71)
++#define ppoll pollts
++#endif
 +#endif
 +
  int vio_errno(Vio *vio MY_ATTRIBUTE((unused))) {
