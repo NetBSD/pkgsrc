85d10bb6754fd634d7a61a2206124c056ccb2d72
nia <nia@pkgsrc.org>
2021-06-22 09:36:41 +0000

    samba4: downgrade to 4.13.9, as discussed on netbsd-users@

--- net/samba4/patches/patch-lib_tevent_tevent.c
+++ net/samba4/patches/patch-lib_tevent_tevent.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_tevent_tevent.c,v 1.2 2021/04/29 15:21:16 taca Exp $
+$NetBSD: patch-lib_tevent_tevent.c,v 1.3 2021/06/22 09:36:41 nia Exp $
 
 On SunOS (OpenSolaris), pthread_mutex_init() expects a zeroed-out
 mutex data structure
 
---- lib/tevent/tevent.c.orig	2019-01-15 10:07:00.000000000 +0000
+--- lib/tevent/tevent.c.orig	2020-07-09 09:33:56.000000000 +0000
 +++ lib/tevent/tevent.c
 @@ -423,6 +423,7 @@ static int tevent_common_context_constru
  		return ret;
