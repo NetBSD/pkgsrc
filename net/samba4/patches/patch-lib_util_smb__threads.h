19d0d9d3dd21a4d5cf20e1b10718079c7940a6bb
nia <nia@pkgsrc.org>
2021-06-22 09:36:41 +0000

    samba4: downgrade to 4.13.9, as discussed on netbsd-users@

--- net/samba4/patches/patch-lib_util_smb__threads.h
+++ net/samba4/patches/patch-lib_util_smb__threads.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_util_smb__threads.h,v 1.2 2021/04/29 15:21:16 taca Exp $
+$NetBSD: patch-lib_util_smb__threads.h,v 1.3 2021/06/22 09:36:41 nia Exp $
 
 On SunOS (OpenSolaris), pthread_mutex_init() expects a zeroed-out
 mutex data structure
 
---- lib/util/smb_threads.h.orig	2019-01-15 10:07:00.000000000 +0000
+--- lib/util/smb_threads.h.orig	2020-07-09 09:33:56.000000000 +0000
 +++ lib/util/smb_threads.h
 @@ -66,6 +66,7 @@ static int smb_create_mutex_pthread(cons
  	if (!pmut) { \
