6d9ffe533c65c46adaf134ad20dd053fc147326e
wiz <wiz@pkgsrc.org>
2021-06-23 21:00:24 +0000

    age: update to 1.0.0rc3.
    Never two without three
    
    Maybe™️ actually™️ the last v1.0.0 release candidate!
    
    Encrypted identity files are now supported. Regular passphrase-encrypted
    age files can be used with -i, the passphrase will be requested
    interactively, and the decrypted file will be read line-by-line as
    a standard identity file.
    
    Passphrases can now be requested interactively from the terminal
    on Windows even if standard input is in use.
    
    Errors are now tidier and all start with age: error: .

--- security/age/go-modules.mk
+++ security/age/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.1.1.1 2021/06/09 17:03:49 abs Exp $
+# $NetBSD: go-modules.mk,v 1.2 2021/06/23 21:00:24 wiz Exp $
 
 GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-beta.3.mod
 GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-beta.3.zip
@@ -6,9 +6,11 @@ GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
 GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20201221181555-eec23a3978ad.mod
 GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20201221181555-eec23a3978ad.zip
 GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.zip
 GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201117132131-f5c789dd3221.mod
 GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201117132131-f5c789dd3221.zip
 GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
