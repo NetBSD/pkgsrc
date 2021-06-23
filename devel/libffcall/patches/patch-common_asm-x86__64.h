b37ad2e6bc190cc3008811256938623b11bbfbed
wiz <wiz@pkgsrc.org>
2021-06-23 19:05:20 +0000

    libffcall: update to 2.4.
    New in 2.4:
    
    * Added support for the following platforms:
      (Previously, a build on these platforms failed.)
      - arm64: macOS 11.
      - arm: FreeBSD 12.
      - sparc: Solaris 11 32-bit.
    
    New in 2.3:
    
    * Added support for the following platforms:
      (Previously, a build on these platforms failed.)
      - x86_64: FreeBSD 13, OpenBSD 6.5 and newer.
      - powerpc64: AIX 7 with xlclang compiler.
    
    * Fixed a couple of multithread-safety bugs in the trampoline and callback
      packages.

--- devel/libffcall/patches/patch-common_asm-x86__64.h
+++ devel/libffcall/patches/patch-common_asm-x86__64.h
@@ -1,12 +1,17 @@
-$NetBSD: patch-common_asm-x86__64.h,v 1.1 2020/04/18 22:00:40 joerg Exp $
+$NetBSD: patch-common_asm-x86__64.h,v 1.2 2021/06/23 19:05:20 wiz Exp $
 
---- common/asm-x86_64.h.orig	2020-03-11 16:04:30.072321269 +0000
+https://savannah.gnu.org/bugs/index.php?60815
+
+--- common/asm-x86_64.h.orig	2021-04-15 00:58:54.000000000 +0000
 +++ common/asm-x86_64.h
-@@ -280,6 +280,6 @@
+@@ -279,8 +279,8 @@
+ // Solaris/ELF
  #define EH_FRAME_SECTION .eh_frame,"aL",link=.text,@unwind
  #else
+-#if defined __FreeBSD__
+-// FreeBSD/ELF
++#if defined __FreeBSD__ || defined __NetBSD__
++// FreeBSD or NetBSD/ELF
+ #define EH_FRAME_SECTION .eh_frame,"a",@progbits
+ #else
  // Linux/ELF
--#define EH_FRAME_SECTION .eh_frame,"aw",@progbits
-+#define EH_FRAME_SECTION .eh_frame,"a",@progbits
- #endif
- #endif
