dc1fcaf630d5a7db38a515f23aedf31886858722
adam <adam@pkgsrc.org>
2021-06-23 19:35:44 +0000

    hiredis: updated to 1.0.0
    Release of v1.0.0
    
    Hiredis v1.0.0 marks the first stable release of Hiredis and introduces
    RESP3 support, SSL connections, allocator injection, better Windows support,
    and more.
    
    IMPORTANT:  There are breaking changes in this release meaning your code
    will need to be recompiled and may need small changes.  The exact
    details of the breaking changes can be found in README.md.
    
    CHANGELOG.md has a detailed list of changes between v0.14.1 and v1.0.0.

--- databases/hiredis/buildlink3.mk
+++ databases/hiredis/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/04/02 17:25:44 wiedi Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/06/23 19:35:44 adam Exp $
 
 BUILDLINK_TREE+=	hiredis
 
 .if !defined(HIREDIS_BUILDLINK3_MK)
 HIREDIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hiredis+=	hiredis>=0.11.0
+BUILDLINK_API_DEPENDS.hiredis+=	hiredis>=1.0.0
 BUILDLINK_PKGSRCDIR.hiredis?=	../../databases/hiredis
 .endif	# HIREDIS_BUILDLINK3_MK
 
