6ed79f4c60233ec9744d3e724a7e5da4e725defd
adam <adam@pkgsrc.org>
2021-06-23 19:34:15 +0000

    ntopng: updated to 4.2
    4.2 Stable
    
    Breakthroughs
    
    Flexible Alert Handling
    Added recipients and endpoints to send alerts to different recipients on different channels, including email, Discord, Slack and Elasticsearch
    Initial SCADA protocol support
    Many internal components of ntopng have been rewritten in order to improve the overall ntopng performance, reduce system load, and capable of processing more data while reducing memory usage with respect to 4.0.
    Cybersecurity extensions have been greatly enhanced by leveraging on the latest nDPI enhancements that enabled the creation of several user scripts able to supervise many security aspects of modern systems.
    Behavioral traffic analysis and lateral traffic movement detection for finding cybersecurity threats in traffic noise.
    Initial Scada support with native IEC 60870-5-104 support. We acknowledge switch.ch for having supported this development.
    Consolidation of Suricata and external alerts integration to further open ntopng to the integration of commercial security devices.
    SNMP support has been enhanced in terms of speed, SNMPv3 protocol support, and variety of supported devices.
    New REST API that enabled the integration of ntopng with third party applications such as CheckMK.
    
    New features
    
    Traffic Behavioral Analysis
    Periodic Traffic
    Lateral Movements
    TLS with self-signed certificates, issuerDN, subjectDN
    Support for Industrial IOT and Scada with modbus, DNP3 and IEC60870
    Support for attack mitigation via SNMP
    Active monitoring
    Support for ICMP v4/v6, HTTP, HTTPS and Speedtest
    Ability to generate alerts upon unreachable or slow hosts or services
    Detection of unexpected servers
    DHCP, NTP, SMTP, DNS
    Services map
    nIndex direct to maximixe flows dump performance
    MacOS package
    
    Improvements
    
    Implements per-category indicator of compromise score
    Flexible configuration import/export/reset
    Ability to import/export/reset all the ntopng configurations or parts of it
    Increased nIndex dump throughput by a factor 10
    Increased user scripts execution throughput
    Massive cleanup/simplifications of plugins to ease community contributions
    Improved cardinality estimation (e.g., number of contacted hosts, number of contacted ports) using Hyper-Log-Log
    Added DSCP information
    Reworked handling of dissected virtual hosts to improve speed and reduce memory
    
    nEdge
    
    Support for hardware bypass
    
    Fixes
    
    Fixed race conditions in view interfaces
    Fixed crash when restoring serialized hosts in memory
    Fixed conditions causing high CPU load
    Fixes CSRF vulnerabilities when POSTing JSON
    Fixes heap-use-after-free on HTTP dissected last_url

--- net/ntopng/patches/patch-src_Utils.cpp
+++ net/ntopng/patches/patch-src_Utils.cpp
@@ -1,26 +1,25 @@
-$NetBSD: patch-src_Utils.cpp,v 1.4 2020/05/25 20:26:51 adam Exp $
+$NetBSD: patch-src_Utils.cpp,v 1.5 2021/06/23 19:34:15 adam Exp $
 
 Fix building on NetBSD.
 d_type is not POSIX, provide workaround for SunOS.
 
---- src/Utils.cpp.orig	2020-03-27 16:51:24.000000000 +0000
+--- src/Utils.cpp.orig	2020-11-02 21:27:19.000000000 +0000
 +++ src/Utils.cpp
-@@ -25,10 +25,13 @@ extern "C" {
+@@ -25,9 +25,12 @@ extern "C" {
  #include "third-party/fast-sha1/sha1-fast.c"
  }
  
 -#if defined(__OpenBSD__) || defined(__APPLE__)
-+#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__OpenBSD__) || defined(__APPLE__) || defined(__NetBSD__)
  #include <net/if_dl.h>
- #include <ifaddrs.h>
  #endif
 +#ifdef __sun
 +struct stat s;
 +#endif
  
- static map<string, int> initTcpStatesStr2State() {
-   map<string, int>states_map;
-@@ -2167,7 +2170,12 @@ static bool scan_dir(const char * dir_na
+ #ifndef WIN32
+ #include <ifaddrs.h>
+@@ -2259,7 +2262,12 @@ static bool scan_dir(const char * dir_na
      if(!entry) break;
      d_name = entry->d_name;
  
@@ -33,7 +32,7 @@ d_type is not POSIX, provide workaround for SunOS.
        snprintf(path, sizeof(path), "%s/%s", dir_name, entry->d_name);
        if(!stat(path, &buf)) {
          struct dirent *temp = (struct dirent *)malloc(sizeof(struct dirent));
-@@ -2177,7 +2185,11 @@ static bool scan_dir(const char * dir_na
+@@ -2269,7 +2277,11 @@ static bool scan_dir(const char * dir_na
  	  *total += buf.st_size;
        }
  
