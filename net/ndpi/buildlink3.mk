98db9439d33f11833ad2817d4e835ea69e766282
adam <adam@pkgsrc.org>
2021-06-23 19:31:49 +0000

    ndpi: updated to 3.4
    3.4 Stable
    
    New Features
    
    Completely reworked and extended QUIC dissector
    Added flow risk concept to move nDPI towards result interpretation
    Added ndpi_dpi2json() API call
    Added DGA risk for names that look like a DGA
    Added HyperLogLog cardinality estimator API calls
    Added ndpi_bin_XXX API calls to handle bin handling
    Fully fuzzy tested code that has greatly improved reliability and robustness
    
    New Supported Protocols and Services
    
    QUIC
    SMBv1
    WebSocket
    TLS: added ESNI support
    SOAP
    DNScrypt
    
    Improvements
    
    Python CFFI bindings
    Various TLS extensions and fixes including extendede metadata support
    Added various pcap files for testing corner cases in protocols
    Various improvements in JSON/Binary data serialization
    CiscoVPN
    H323
    MDNS
    MySQL 8
    IEC 60870-5-104
    DoH/DoT dissection improvements
    Office365 renamed to Microsoft365
    Major protocol dissection improvement in particular with unknwon traffic
    Improvement in Telegram v6 protocol support
    HTTP improvements to detect file download/upload and binary files
    BitTorrent and WhatsApp dissection improvement
    Spotify
    Added detection of malformed packets
    Fuzzy testing support has been greatly improved
    SSH code cleanup
    
    Fixes
    
    Fixed various memory leaks and race conditions in protocol decoding
    NATS, CAPWAP dissector
    Removed HyperScan support that greatly simplified the code
    ARM platform fixes on memory alignment
    Wireshark extcap support
    DPDK support
    OpenWRT, OpenBSD support
    MINGW compiler support
    
    MISC
    
    Created demo app for nDPI newcomers
    Removed obsolete pplive and pando protocols

--- net/ndpi/buildlink3.mk
+++ net/ndpi/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/05/19 12:09:08 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/06/23 19:31:49 adam Exp $
 
 BUILDLINK_TREE+=	ndpi
 
 .if !defined(NDPI_BUILDLINK3_MK)
 NDPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ndpi+=	ndpi>=2.8
-BUILDLINK_ABI_DEPENDS.ndpi?=	ndpi>=2.8nb1
+BUILDLINK_API_DEPENDS.ndpi+=	ndpi>=3.4
+BUILDLINK_ABI_DEPENDS.ndpi+=	ndpi>=3.4
 BUILDLINK_PKGSRCDIR.ndpi?=	../../net/ndpi
 
 .include "../../textproc/json-c/buildlink3.mk"
