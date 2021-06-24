4b142d8fea9b335f48297c5961fa0e7a22ac5d1d
adam <adam@pkgsrc.org>
2021-06-24 09:30:55 +0000

    cmake: updated to 3.20.5
    CMake 3.20.5
    * gitlab-ci: update macOS jobs to use Xcode 12.5
    * cmGlobalVisualStudio10Generator: Move static functions to anonymous namespace
    * cmGlobalVisualStudio10Generator: Adopt GetVSInstanceVersion method
    * VS: Do not apply '/external:W*' flag table mapping on VS < 16.10
    * Tests: Fix xcode version detection for Xcode 13 on ARM
    * Help: Add 3.20 release note section for 3.20.{3,4,5}
    * ExternalProject: Use ${ref}^0 to avoid MSYS/Cygwin shell substitutions
    * CMake: Allow override of unexpected non-flow-control commands

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.16 2021/06/15 10:27:33 adam Exp $
+# $NetBSD: version.mk,v 1.17 2021/06/24 09:30:55 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.20.4
+CMAKE_VERSION=	3.20.5
 CMAKE_API=	${CMAKE_VERSION:R}
