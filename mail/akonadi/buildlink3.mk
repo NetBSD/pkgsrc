175bb27d8976f5d5a05d282a78afbac38572cd2b
markd <markd@pkgsrc.org>
2021-06-24 20:17:31 +0000

    akonadi: more buildlink files.

--- mail/akonadi/buildlink3.mk
+++ mail/akonadi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2021/06/21 11:23:01 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2021/06/24 20:17:31 markd Exp $
 
 BUILDLINK_TREE+=	akonadi
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.akonadi+=	akonadi>=1.13.0nb34
 BUILDLINK_PKGSRCDIR.akonadi?=	../../mail/akonadi
 
 BUILDLINK_FILES.akonadi+=	share/config/akonadi/*.conf
+BUILDLINK_FILES.akonadi+=	share/dbus-1/interfaces/org.freedesktop.Akonadi.*.xml
 
 .include "../../x11/qt4-libs/buildlink3.mk"
 .include "../../x11/qt4-qdbus/buildlink3.mk"
