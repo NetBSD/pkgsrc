253625a68bfa26215107f079e0aedb834a53a8d0
triaxx <triaxx@pkgsrc.org>
2021-06-24 02:32:27 +0000

    php-dotclear: Add www/php-dotclear version 2.18.1
    Dotclear's proposed aim is to develop a software that fully respects web
    standards based on open source solutions, with multilingual interface and
    publishing capabilities. It is written in PHP.
    Notable features include handling many blogs, using Wiki or XHTML syntax for
    entries, adding pages that are independent from the flow of entries and support
    for several database types: (MySQL, PostgreSQL, SQLite).
    
    This pkgsrc packages provides:
      * Patches to move the configuration file config.php to PKG_SYSCONFDIR,
      * Patches to move files writable by the web server (public files, cache
        files and additional plugins) to directories relative to VARBASE.

---
+++ www/php-dotclear/patches/patch-admin_install_wizard.php
@@ -0,0 +1,34 @@
+$NetBSD: patch-admin_install_wizard.php,v 1.1 2021/06/24 02:32:28 triaxx Exp $
+
+Honor PKG_SYSCONFDIR.
+Set pkgsrc compliant directories for SQLite database.
+
+--- admin/install/wizard.php.orig	2021-02-13 10:31:06.000000000 +0000
++++ admin/install/wizard.php
+@@ -12,7 +12,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
+ } elseif (isset($_SERVER['REDIRECT_DC_RC_PATH'])) {
+     define('DC_RC_PATH', $_SERVER['REDIRECT_DC_RC_PATH']);
+ } else {
+-    define('DC_RC_PATH', dirname(__FILE__) . '/../../inc/config.php');
++    define('DC_RC_PATH', '@PKG_SYSCONFDIR@/config.php');
+ }
+ 
+ #  ClearBricks and DotClear classes auto-loader
+@@ -63,7 +63,7 @@ if (!empty($_POST)) {
+     {
+         if ($DBDRIVER == 'sqlite') {
+             if (strpos($DBNAME, '/') === false) {
+-                $sqlite_db_directory = dirname(DC_RC_PATH) . '/../db/';
++                $sqlite_db_directory = '@DOTCLEAR_DBDIR@';
+                 files::makeDir($sqlite_db_directory, true);
+ 
+                 # Can we write sqlite_db_directory ?
+@@ -99,7 +99,7 @@ if (!empty($_POST)) {
+         }
+ 
+         # Does config.php.in exist?
+-        $config_in = dirname(__FILE__) . '/../../inc/config.php.in';
++        $config_in = '@PKG_SYSCONFDIR@/config.php.in';
+         if (!is_file($config_in)) {
+             throw new Exception(sprintf(__('File %s does not exist.'), $config_in));
+         }
