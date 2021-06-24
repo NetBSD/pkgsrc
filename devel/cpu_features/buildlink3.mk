9d4dd888d00104b4a16f444a7167ff9f243132e1
tnn <tnn@pkgsrc.org>
2021-06-24 10:50:21 +0000

    cpu_features: paper over CMake brain damage
    fixes math/volk build

--- devel/cpu_features/buildlink3.mk
+++ devel/cpu_features/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/12/14 15:07:49 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/06/24 10:50:21 tnn Exp $
 
 BUILDLINK_TREE+=	cpu_features
 
 .if !defined(CPU_FEATURES_BUILDLINK3_MK)
 CPU_FEATURES_BUILDLINK3_MK:=
+
+BUILDLINK_FILES.cpu_features+=		bin/list_cpu_features
 
 BUILDLINK_API_DEPENDS.cpu_features+=	cpu_features>=0.6.0
 BUILDLINK_PKGSRCDIR.cpu_features?=	../../devel/cpu_features
