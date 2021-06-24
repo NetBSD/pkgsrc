0f738a2bae0f1d2fa0732ec65d0ee4827bffa20f
tnn <tnn@pkgsrc.org>
2021-06-24 18:43:08 +0000

    modular-xorg-server: revert PR pkg/56169 patch. Different solution needed.
    ok khorben

--- x11/modular-xorg-server/patches/patch-hw_kdrive_ephyr_hostx.c
+++ x11/modular-xorg-server/patches/patch-hw_kdrive_ephyr_hostx.c
@@ -1,24 +1,10 @@
-$NetBSD: patch-hw_kdrive_ephyr_hostx.c,v 1.2 2021/06/08 22:51:11 khorben Exp $
+$NetBSD: patch-hw_kdrive_ephyr_hostx.c,v 1.3 2021/06/24 18:43:08 tnn Exp $
 
 Fix Xephyr visual with -parent option
 
---- hw/kdrive/ephyr/hostx.c.orig	2021-04-13 14:11:40.000000000 +0000
+--- hw/kdrive/ephyr/hostx.c.orig	2020-03-29 20:21:15.000000000 +0000
 +++ hw/kdrive/ephyr/hostx.c
-@@ -36,7 +36,13 @@
- #include <string.h>             /* for memset */
- #include <errno.h>
- #include <time.h>
-+#if defined(HAVE_NBCOMPAT_H)
-+#include <nbcompat/config.h>
-+#include <nbcompat/cdefs.h>
-+#include <nbcompat/err.h>
-+#else
- #include <err.h>
-+#endif
- 
- #include <sys/ipc.h>
- #include <sys/shm.h>
-@@ -622,7 +628,7 @@ hostx_init(void)
+@@ -622,7 +622,7 @@ hostx_init(void)
                                scrpriv->win_height,
                                0,
                                XCB_WINDOW_CLASS_COPY_FROM_PARENT,
