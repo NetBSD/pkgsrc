d767671831a4d45a3e89c1e410da5683be5b0a0b
tnn <tnn@pkgsrc.org>
2021-06-25 09:49:23 +0000

    synergy: update to 1.14.0.4
    - bug fixes
    - build system fixes
    - code quality improvements

--- net/synergy/patches/patch-src_lib_server_Config.cpp
+++ net/synergy/patches/patch-src_lib_server_Config.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_lib_server_Config.cpp,v 1.1 2020/10/22 13:47:34 tnn Exp $
+$NetBSD: patch-src_lib_server_Config.cpp,v 1.2 2021/06/25 09:49:24 tnn Exp $
 
 Fix for some EOF problem. Not sure it's still required as the
 previous patch has been adapted to upstream changes.
 
---- src/lib/server/Config.cpp.orig	2014-12-02 15:03:19.000000000 +0000
+--- src/lib/server/Config.cpp.orig	2021-06-08 13:44:14.000000000 +0000
 +++ src/lib/server/Config.cpp
-@@ -1931,7 +1931,7 @@ ConfigReadContext::getLineNumber() const
+@@ -1974,7 +1974,7 @@ ConfigReadContext::getLineNumber() const
  bool
  ConfigReadContext::operator!() const
  {
