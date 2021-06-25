ab8ce95e3df8e9d04904c6c5538821b3e7a80e5c
tnn <tnn@pkgsrc.org>
2021-06-25 13:34:41 +0000

    databases/timescaledb-tune: import timescaledb-tune-0.11.0git
    timescaledb-tune is a program for tuning a TimescaleDB database to perform its
    best based on the host's resources such as memory and number of CPUs. It parses
    the existing postgresql.conf file to ensure that the TimescaleDB extension is
    appropriately installed and provides recommendations for memory, parallelism,
    WAL, and other settings.

---
+++ databases/timescaledb-tune/go-modules.mk
@@ -0,0 +1,14 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/06/25 13:34:41 tnn Exp $
+
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.9.0.mod
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.9.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.11.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.11.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.8.mod
+GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20190104145345-974d429e7ae4.mod
+GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20190104145345-974d429e7ae4.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190222072716-a9d3bda3a223.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.zip
