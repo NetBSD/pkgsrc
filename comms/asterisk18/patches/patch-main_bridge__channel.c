2e4630f7660ad3fc893d416158c26ebc6583efdc
jnemeth <jnemeth@pkgsrc.org>
2021-06-27 21:39:27 +0000

    comms/asterisk18: update to Asterisk 18.5.0.
    pkgsrc change: Fix segfault under aarch64 from ryoon for comms/asterisk16.
    
    -----
    
    The Asterisk Development Team would like to announce the release
    of Asterisk 18.5.0.
    
    The release of Asterisk 18.5.0 resolves several issues reported by the
    community and would have not been possible without your participation.
    
    Thank you!
    
    The following issues are resolved in this release:
    
    New Features made in this release:
    -----------------------------------
     * ASTERISK-29446 - app_confbridge: New ConfKick application
          (Reported by N A)
     * ASTERISK-29440 - app_confbridge: Allow ConfBridge answer to
          be suppressed
          (Reported by N A)
     * ASTERISK-29431 - Minimum and maximum dialplan functions
          (Reported by N A)
     * ASTERISK-29439 - func_volume: Volume function can't be read
          (Reported by N A)
    
    Bugs fixed in this release:
    -----------------------------------
     * ASTERISK-29475 - SayNumber triggers WARNING if caller hangs
          up during application execution
          (Reported by N A)
     * ASTERISK-29404 - Consolidate res_pjsip_messaging fixes for
          domain name
          (Reported by George Joseph)
     * ASTERISK-29441 - Core reload making TCP endpoints go offline
          (Reported by Luke Escude)
     * ASTERISK-28237 - "FRACK!, Failed assertion bad magic number"
          happens when unsubscribe an application from an event source
          (Reported by Lucas Tardioli Silveira)
     * ASTERISK-28393 - Multidomain support issue
          (Reported by Andrea Sannucci)
     * ASTERISK-29433 - res_rtp_asterisk: Server reflexive
          candidates use incorrect raddr for RTCP
          (Reported by Chris)
     * ASTERISK-29397 - pjsip: Asterisk isn't tolerant of RFC8760 UASs
          (Reported by George Joseph)
     * ASTERISK-24601 - [patch]Missing RFC4235 tags and attributes
          in PJSIP NOTIFY event: dialog  XML body
          (Reported by Marco Paland)
     * ASTERISK-29370 - chan_sip does not recognize
          application/hook-flash
          (Reported by N A)
     * ASTERISK-29377 - cpool_release_pool "double free or
          corruption (out)"
          (Reported by Robert Sutton)
     * ASTERISK-29372 - file.c switch does not account for flash
          events
          (Reported by N A)
     * ASTERISK-29358 - chan_pjsip: Trace message for progress is
          output even if frame is not queued
          (Reported by Michael Maier)
     * ASTERISK-29407 - chan_local: Filtering audio formats should
          not occur on removed streams
          (Reported by Joshua C. Colp)
     * ASTERISK-29030 - res_rtp_asterisk: Additional RTP-frame (with
          wrong SSRC) gets inserted when switching from progress to
          established
          (Reported by Matthias Hensler)
    
    Improvements made in this release:
    -----------------------------------
     * ASTERISK-29450 - Allow setting channel variables using
          Originate application
          (Reported by N A)
     * ASTERISK-29459 - Missing configuration from PJSIP to SIP
          conversion script
          (Reported by N A)
     * ASTERISK-29460 - Recognize application/hook-flash in PJSIP
          (Reported by N A)
     * ASTERISK-29434 - Asterisk reveals pjproject version in STUN packets
          (Reported by Jeremy Lain??)
     * ASTERISK-29349 - Silent voicemail option is not completely silent
          (Reported by N A)
     * ASTERISK-29380 - Add Flash AMI event to handle flash events
          (Reported by N A)
    
    For a full list of changes in this release, please see the ChangeLog:
    https://downloads.asterisk.org/pub/telephony/asterisk/ChangeLog-18.5.0
    
    Thank you for your continued support of Asterisk!
    -------------- next part --------------
    An HTML attachment was scrubbed...
    URL: <http://lists.digium.com/pipermail/asterisk-announce/attachments/20210624/fe9defa9/attachment.html>
    
        Previous message (by thread): [asterisk-announce] Asterisk 16.19.0 Now Available
        Messages sorted by: [ date ] [ thread ] [ subject ] [ author ]
    
    More information about the asterisk-announce mailing list

---
+++ comms/asterisk18/patches/patch-main_bridge__channel.c
@@ -0,0 +1,38 @@
+$NetBSD: patch-main_bridge__channel.c,v 1.1 2021/06/27 21:39:27 jnemeth Exp $
+
+* Fix segfault under NetBSD/aarch64 9.99.80.
+
+--- main/bridge_channel.c.orig	2021-01-21 16:28:04.000000000 +0000
++++ main/bridge_channel.c
+@@ -58,6 +58,13 @@
+ #include "asterisk/stream.h"
+ #include "asterisk/message.h"
+ 
++/* XXX, pthread_equal() is misused to compare non-valid thread pointers */
++static int
++pt_pthread_equal(pthread_t t1, pthread_t t2)
++{
++	return t1 == t2;
++}
++
+ /*!
+  * \brief Used to queue an action frame onto a bridge channel and write an action frame into a bridge.
+  * \since 12.0.0
+@@ -258,7 +265,7 @@ int ast_bridge_channel_notify_talking(st
+  */
+ static void bridge_channel_poke(struct ast_bridge_channel *bridge_channel)
+ {
+-	if (!pthread_equal(pthread_self(), bridge_channel->thread)) {
++	if (!pt_pthread_equal(pthread_self(), bridge_channel->thread)) {
+ 		/* Wake up the bridge channel thread. */
+ 		ast_queue_frame(bridge_channel->chan, &ast_null_frame);
+ 	}
+@@ -971,7 +978,7 @@ static int bridge_channel_queue_action_d
+ 	};
+ 
+ 	/* Make sure we don't end up trying to wait on ourself to deliver the frame */
+-	ast_assert(!pthread_equal(pthread_self(), bridge_channel->thread));
++	ast_assert(!pt_pthread_equal(pthread_self(), bridge_channel->thread));
+ 
+ 	sync_payload = ast_alloca(sync_payload_len);
+ 	sync_payload->id = ast_atomic_fetchadd_int(&sync_ids, +1);
--- comms/asterisk18/patches/patch-res_res__xmpp.c
+++ comms/asterisk18/patches/patch-res_res__xmpp.c
@@ -1,8 +1,31 @@
-$NetBSD: patch-res_res__xmpp.c,v 1.2 2021/06/13 07:57:53 jnemeth Exp $
+$NetBSD: patch-res_res__xmpp.c,v 1.3 2021/06/27 21:39:27 jnemeth Exp $
 
---- res/res_xmpp.c.orig	2018-06-19 21:29:29.310245367 +0000
+--- res/res_xmpp.c.orig	2021-06-24 12:50:57.000000000 +0000
 +++ res/res_xmpp.c
-@@ -3746,7 +3746,7 @@ static int xmpp_client_receive(struct as
+@@ -62,6 +62,13 @@
+ #include "asterisk/config_options.h"
+ #include "asterisk/json.h"
+ 
++/* XXX, pthread_equal() is misused to compare non-valid thread pointers */
++static int
++pt_pthread_equal(pthread_t t1, pthread_t t2)
++{
++	return t1 == t2;
++}
++
+ /*** DOCUMENTATION
+ 	<application name="JabberSend" language="en_US" module="res_xmpp">
+ 		<synopsis>
+@@ -3527,7 +3534,7 @@ static int xmpp_action_hook(void *data, 
+ 
+ int ast_xmpp_client_disconnect(struct ast_xmpp_client *client)
+ {
+-	if ((client->thread != AST_PTHREADT_NULL) && !pthread_equal(pthread_self(), client->thread)) {
++	if ((client->thread != AST_PTHREADT_NULL) && !pt_pthread_equal(pthread_self(), client->thread)) {
+ 		xmpp_client_change_state(client, XMPP_STATE_DISCONNECTING);
+ 		pthread_cancel(client->thread);
+ 		pthread_join(client->thread, NULL);
+@@ -3669,7 +3676,7 @@ static int xmpp_client_receive(struct as
  			/* if we stumble on the ending tag character,
  			   we skip any whitespace that follows it*/
  			if (c == '>') {
