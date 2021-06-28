0c3de681599205a71e8f3b6c7d64897be89b222a
tnn <tnn@pkgsrc.org>
2021-06-28 23:12:22 +0000

    openjdk11: options: accept jdk-hotspot-vm on arm

--- lang/openjdk11/options.mk
+++ lang/openjdk11/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2021/06/27 07:53:43 tnn Exp $
+# $NetBSD: options.mk,v 1.7 2021/06/28 23:12:22 tnn Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.openjdk11
 PKG_OPTIONS_OPTIONAL_GROUPS=	variant
@@ -6,7 +6,7 @@ PKG_OPTIONS_GROUP.variant=	jdk-zero-vm
 PKG_SUPPORTED_OPTIONS=		debug dtrace jre-jce x11 static-libstdcpp
 PKG_SUGGESTED_OPTIONS=		jre-jce x11
 
-.if ${MACHINE_ARCH} == "aarch64" || ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
+.if ${MACHINE_ARCH} == "aarch64" || !empty(MACHINE_ARCH:M*arm*) || ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
 PKG_OPTIONS_GROUP.variant+=	jdk-hotspot-vm
 PKG_SUGGESTED_OPTIONS+=		jdk-hotspot-vm
 .else
