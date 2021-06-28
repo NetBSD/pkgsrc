c90bd0bff3539a4714c8404beb382ccaced4e961
mrg <mrg@pkgsrc.org>
2021-06-28 09:47:56 +0000

    buildlink a few more paths, fixes kdepimlibs4 build.
    ideas from nia.

--- mail/akonadi/buildlink3.mk
+++ mail/akonadi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2021/06/24 20:17:31 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2021/06/28 09:47:56 mrg Exp $
 
 BUILDLINK_TREE+=	akonadi
 
@@ -9,8 +9,11 @@ BUILDLINK_API_DEPENDS.akonadi+=	akonadi>=0.82.0
 BUILDLINK_ABI_DEPENDS.akonadi+=	akonadi>=1.13.0nb34
 BUILDLINK_PKGSRCDIR.akonadi?=	../../mail/akonadi
 
-BUILDLINK_FILES.akonadi+=	share/config/akonadi/*.conf
-BUILDLINK_FILES.akonadi+=	share/dbus-1/interfaces/org.freedesktop.Akonadi.*.xml
+BUILDLINK_FILES.akonadi+=	lib/cmake/Akonadi/* \
+				share/config/akonadi/*.conf \
+				share/dbus-1/interfaces/org.freedesktop.Akonadi* \
+				share/dbus-1/services/org.freedesktop.Akonadi* \
+				share/mime/packages/akonadi*
 
 .include "../../x11/qt4-libs/buildlink3.mk"
 .include "../../x11/qt4-qdbus/buildlink3.mk"
