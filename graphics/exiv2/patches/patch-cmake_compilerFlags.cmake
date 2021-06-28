e85712924946cb1e5509b0bfcc84d10d033820b1
wiz <wiz@pkgsrc.org>
2021-06-28 07:50:44 +0000

    exiv2: update to 0.27.4.
    Exiv2 v0.27.4 Features
    
        bmff support (.CR3, .AVIF, .HEIC, .HIF, .JXL/bmff) files.
        Rewrite 0.27 bash test scripts in python.
        Support for Exif 2.32 and DNG 1.6.
        Crowdin Localisation Support
        Completion of Image Metadata and Exiv2 Architecture https://clanmills.com/exiv2/book/
        Improved documentation.
        Various minor bugs and fixes.
        RC3 issued to deal with 12 security issues. After 18 months without a CVE, we were attacked between RC2 and GM.
        Security policy defined and published on GitHub.

--- graphics/exiv2/patches/patch-cmake_compilerFlags.cmake
+++ graphics/exiv2/patches/patch-cmake_compilerFlags.cmake
@@ -1,15 +1,16 @@
-$NetBSD: patch-cmake_compilerFlags.cmake,v 1.3 2021/04/25 07:51:26 mrg Exp $
+$NetBSD: patch-cmake_compilerFlags.cmake,v 1.4 2021/06/28 07:50:44 wiz Exp $
 
 Gcc on some platforms does not support -fcf-protection.
 
---- cmake/compilerFlags.cmake.orig	2020-06-30 13:33:22.000000000 +0000
+--- cmake/compilerFlags.cmake.orig	2021-06-15 14:08:36.000000000 +0000
 +++ cmake/compilerFlags.cmake
-@@ -25,7 +25,7 @@ if ( MINGW OR UNIX OR MSYS ) # MINGW, Li
- 
-         # This fails under Fedora, MinGW GCC 8.3.0 and CYGWIN/MSYS 9.3.0
-         if (NOT (MINGW OR CMAKE_HOST_SOLARIS OR CYGWIN OR MSYS) )
--            if (COMPILER_IS_GCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8.0)
-+            if (COMPILER_IS_GCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8.0 AND NOT (${CMAKE_SYSTEM_PROCESSOR} MATCHES "sparc.*|aarch64.*"))
-                 add_compile_options(-fstack-clash-protection -fcf-protection)
-             endif()
- 
+@@ -26,7 +26,9 @@ if ( MINGW OR UNIX OR MSYS ) # MINGW, Li
+         # This fails under Fedora - MinGW - Gcc 8.3
+         if (NOT (MINGW OR CYGWIN OR CMAKE_HOST_SOLARIS))
+             check_cxx_compiler_flag(-fstack-clash-protection HAS_FSTACK_CLASH_PROTECTION)
++            if (NOT (${CMAKE_SYSTEM_PROCESSOR} MATCHES "sparc.*|aarch64.*"))
+             check_cxx_compiler_flag(-fcf-protection HAS_FCF_PROTECTION)
++            endif()
+             check_cxx_compiler_flag(-fstack-protector-strong HAS_FSTACK_PROTECTOR_STRONG)
+             if(HAS_FSTACK_CLASH_PROTECTION)
+                 add_compile_options(-fstack-clash-protection)
