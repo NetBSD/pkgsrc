98636afd7c5edc626c5ff650a7ba858e8bcb6aaf
wiz <wiz@pkgsrc.org>
2021-06-28 07:03:44 +0000

    tor: update rust option
    This still does not build for me.
    Add comment on tor implementation project in rust.

--- net/tor/options.mk
+++ net/tor/options.mk
@@ -1,6 +1,5 @@
-# $NetBSD: options.mk,v 1.19 2021/06/27 21:18:38 wiz Exp $
+# $NetBSD: options.mk,v 1.20 2021/06/28 07:03:44 wiz Exp $
 
-# XXX Add rust to supported options.
 PKG_OPTIONS_VAR=		PKG_OPTIONS.tor
 PKG_SUPPORTED_OPTIONS=		doc
 PKG_OPTIONS_REQUIRED_GROUPS=	ssl
@@ -34,7 +33,22 @@ CONFIGURE_ARGS+=	--enable-nss
 .include "../../devel/nss/buildlink3.mk"
 .endif
 
+# see
+# https://gitlab.torproject.org/tpo/core/arti
+# instead
 #.if !empty(PKG_OPTIONS:Mrust)
 #CONFIGURE_ARGS+=	--enable-rust
+#CONFIGURE_ENV+=		TOR_RUST_DEPENDENCIES=${WRKDIR}/vendor
+##CONFIGURE_ARGS+=	--enable-cargo-online-mode
+## needed to override cargo.mk
+#do-build:
+#	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${MAKE} ${MAKE_ARGS} all
+#CARGO_CRATE_DEPENDS+=   digest-0.7.2
+#CARGO_CRATE_DEPENDS+=   generic-array-0.9.0
+#CARGO_CRATE_DEPENDS+=   libc-0.2.39
+#CARGO_CRATE_DEPENDS+=   rand-0.5.0-pre.2
+#CARGO_CRATE_DEPENDS+=   rand_core-0.2.0-pre.0
+#CARGO_CRATE_DEPENDS+=   typenum-1.9.0
 #.include "../../lang/rust/buildlink3.mk"
+#.include "../../lang/rust/cargo.mk"
 #.endif
