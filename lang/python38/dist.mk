68003968a480bd8521ed78bf669c5ed525d7df31
adam <adam@pkgsrc.org>
2021-06-29 12:41:06 +0000

    python38: updated to 3.8.11
    Python 3.8.11 final
    
    Security
    
    bpo-44022: mod:http.client now avoids infinitely reading potential HTTP headers after a 100 Continue status response from the server.
    bpo-43882: The presence of newline or tab characters in parts of a URL could allow some forms of attacks.
    
    Following the controlling specification for URLs defined by WHATWG urllib.parse() now removes ASCII newlines and tabs from URLs, preventing such attacks.
    bpo-42800: Audit hooks are now fired for frame.f_code, traceback.tb_frame, and generator code/frame attribute access.
    
    Core and Builtins
    
    bpo-44070: No longer eagerly makes import filenames absolute, except for extension modules, which was introduced in 3.8.10.
    
    Library
    
    bpo-44061: Fix regression in previous release when calling pkgutil.iter_modules() with a list of pathlib.Path objects

--- lang/python38/dist.mk
+++ lang/python38/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.11 2021/05/04 05:16:07 adam Exp $
+# $NetBSD: dist.mk,v 1.12 2021/06/29 12:41:06 adam Exp $
 
-PY_DISTVERSION=	3.8.10
+PY_DISTVERSION=	3.8.11
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python38/distinfo
