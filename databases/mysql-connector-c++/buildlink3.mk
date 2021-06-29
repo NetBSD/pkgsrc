98413e16f871413e6b4f4c9972b6b9f9d781cb31
wiz <wiz@pkgsrc.org>
2021-06-29 06:43:30 +0000

    mysql-connector-c++: update to 8.0.25.
    MySQL Connectors and other MySQL client tools and applications now
    synchronize the first digit of their version number with the
    (highest) MySQL server version they support. For example, MySQL
    Connector/C++ 8.0.12 would be designed to support all features of
    MySQL server version 8 (or lower). This change makes it easy and
    intuitive to decide which client version to use for which server
    version.

--- databases/mysql-connector-c++/buildlink3.mk
+++ databases/mysql-connector-c++/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/03/19 01:06:25 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/06/29 06:43:30 wiz Exp $
 
 BUILDLINK_TREE+=	mysql-connector-c++
 
 .if !defined(MYSQL_CONNECTOR_C++_BUILDLINK3_MK)
 MYSQL_CONNECTOR_C++_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-connector-c+++=	mysql-connector-c++>=1.1.5
+BUILDLINK_API_DEPENDS.mysql-connector-c+++=	mysql-connector-c++>=8
 BUILDLINK_PKGSRCDIR.mysql-connector-c++?=	../../databases/mysql-connector-c++
 
 .include "../../mk/mysql.buildlink3.mk"
