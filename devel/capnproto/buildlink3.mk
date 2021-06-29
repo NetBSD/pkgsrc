0f2b2952da76878d356873844b1a23763b420409
nia <nia@pkgsrc.org>
2021-06-29 08:25:35 +0000

    capnproto: paper over cmake breakage

--- devel/capnproto/buildlink3.mk
+++ devel/capnproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/02/20 21:58:35 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/06/29 08:25:35 nia Exp $
 
 BUILDLINK_TREE+=	capnproto
 
@@ -7,6 +7,11 @@ CAPNPROTO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.capnproto+=	capnproto>=0.8.0
 BUILDLINK_PKGSRCDIR.capnproto?=		../../devel/capnproto
+
+BUILDLINK_FILES.capnproto+=	bin/capnp
+BUILDLINK_FILES.capnproto+=	bin/capnpc
+BUILDLINK_FILES.capnproto+=	bin/capnpc-c++
+BUILDLINK_FILES.capnproto+=	bin/capnpc-capnp
 
 .endif	# CAPNPROTO_BUILDLINK3_MK
 
