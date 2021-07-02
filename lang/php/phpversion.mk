6ab660e68d04cf71cb51ef54c9395da922faaf99
taca <taca@pkgsrc.org>
2021-07-02 17:32:06 +0000

    lang/php73: update to 7.3.29
    01 Jul 2021, PHP 7.3.29
    
    - Core:
      . Fixed bug #81122: SSRF bypass in FILTER_VALIDATE_URL. (CVE-2021-21705) (cmb)
    
    - PDO_Firebird:
      . Fixed bug #76448: Stack buffer overflow in firebird_info_cb.
        (CVE-2021-21704) (cmb)
      . Fixed bug #76449: SIGSEGV in firebird_handle_doer. (CVE-2021-21704) (cmb)
      . Fixed bug #76450: SIGSEGV in firebird_stmt_execute. (CVE-2021-21704) (cmb)
      . Fixed bug #76452: Crash while parsing blob data in firebird_fetch_blob.
        (CVE-2021-21704) (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.332 2021/07/02 17:30:35 taca Exp $
+# $NetBSD: phpversion.mk,v 1.333 2021/07/02 17:32:06 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP73_VERSION=	7.3.28
+PHP73_VERSION=	7.3.29
 PHP74_VERSION=	7.4.21
 PHP80_VERSION=	8.0.8
 
