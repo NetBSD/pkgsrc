c6944b1f1470d9195cd91ae31d5c683c5b4449dc
wiz <wiz@pkgsrc.org>
2021-07-02 06:59:15 +0000

    inkscape: update to 1.1.
    Much help provided by Nathan Lee in fixing a startup issue.
    
    Inkscape 1.1 is the latest major Inkscape release that brings users
    many fresh new features and new functionality.
    
    The most notable changes include:
    
        A Welcome dialog, where the look of Inkscape can be selected,
        and some choices for the new document's size or file to open
        are available
    
        A Command palette that opens when the ? key is pressed and that
        allows to search and use many functions without having to use
        a keyboard shortcut or going through the menus
    
        It is now possible to copy, cut and paste parts of paths with
        the Node tool
    
        The dialog docking system has been rewritten, which resolves
        many issues with Inkscape's docked dialogs and allows you to
        dock dialogs on either side of the screen
    
        New Outline Overlay mode that displays object outlines while
        also showing their real colors
    
        Preferences options are now easier to find by using the new
        search field
    
        It is no longer necessary to remember to click on 'Export' in
        the PNG Export dialog, as the exporting will already happen
        after the click on 'Save' in the file selection dialog.
    
        Export as JPG, TIFF, optimized PNG and WebP directly from
        Inkscape
    
        When pasting a copied object, Inkscape now pastes it directly
        on top of the currently selected object by default
    
        An extension for updating extensions and installing additional
        extensions, called the Extension Manager (currently in beta
        stage)

--- graphics/inkscape/patches/patch-src_extension_implementation_script.cpp
+++ graphics/inkscape/patches/patch-src_extension_implementation_script.cpp
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_extension_implementation_script.cpp,v 1.1 2020/05/17 00:38:47 tnn Exp $
+$NetBSD: patch-src_extension_implementation_script.cpp,v 1.2 2021/07/02 06:59:15 wiz Exp $
 
 Replace python3 interpreter.
 
---- src/extension/implementation/script.cpp.orig	2020-05-01 13:18:52.000000000 +0000
+--- src/extension/implementation/script.cpp.orig	2021-05-17 19:26:40.000000000 +0000
 +++ src/extension/implementation/script.cpp
-@@ -77,10 +77,10 @@ const std::map<std::string, Script::inte
-         { "python",  {"python-interpreter",  {"pythonw"           }}},
+@@ -82,10 +82,10 @@ const std::map<std::string, Script::inte
+     { "python",  {"python-interpreter",  {"pythonw"           }}},
  #elif defined __APPLE__
-         { "perl",    {"perl-interpreter",    {"perl"              }}},
--        { "python",  {"python-interpreter",  {"python3"           }}},
-+        { "python",  {"python-interpreter",  {"@PYTHONBIN@", "python3"           }}},
+     { "perl",    {"perl-interpreter",    {"perl"              }}},
+-    { "python",  {"python-interpreter",  {"python3"           }}},
++    { "python",  {"python-interpreter",  {"@PYTHONBIN@"           }}},
  #else
-         { "perl",    {"perl-interpreter",    {"perl"              }}},
--        { "python",  {"python-interpreter",  {"python3", "python" }}},
-+        { "python",  {"python-interpreter",  {"@PYTHONBIN@", "python3", "python" }}},
+     { "perl",    {"perl-interpreter",    {"perl"              }}},
+-    { "python",  {"python-interpreter",  {"python3", "python" }}},
++    { "python",  {"python-interpreter",  {"@PYTHONBIN@", "python" }}},
  #endif
-         { "python2", {"python2-interpreter", {"python2", "python" }}},
-         { "ruby",    {"ruby-interpreter",    {"ruby"    }}},
+     { "python2", {"python2-interpreter", {"python2", "python" }}},
+     { "ruby",    {"ruby-interpreter",    {"ruby"    }}},
