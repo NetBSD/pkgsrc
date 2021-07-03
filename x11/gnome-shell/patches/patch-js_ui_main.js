2ea8dde7b18bb3f831062ba4cd9bed1c24fb382d
cirnatdan <cirnatdan@pkgsrc.org>
2021-07-03 19:12:10 +0000

    gnome-shell: Update x11/gnome-shell to 40.2
    40.2
    ====
    * Unbreak Xwayland apps when not using systemd in user session [Olivier; !1850]
    * Fix distributed man pages [Florian; !1859]
    * Avoid unnecessary icon shadow changes [Daniel; !1869]
    * Immediately start app grid drags for non-touch devices [Georges; !1872]
    * Handle screencast failures more gracefully [Florian; !1878]
    * Fixed crashes [Gustavo; !1848]
    * Misc. bug fixes [Georges, Florian, Daniel; !1867, !1854, !1860, !1876]
    
    Contributors:
      Olivier Fourdan, Florian Müllner, Georges Basile Stavracas Neto,
      Gustavo Noronha Silva, Daniel van Vugt
    
    Translators:
      Quentin PAGÈS [oc], Cheng-Chia Tseng [zh_TW], Hugo Carvalho [pt]

--- x11/gnome-shell/patches/patch-js_ui_main.js
+++ x11/gnome-shell/patches/patch-js_ui_main.js
@@ -1,38 +1,49 @@
-$NetBSD: patch-js_ui_main.js,v 1.1 2021/06/03 15:11:05 cirnatdan Exp $
+$NetBSD: patch-js_ui_main.js,v 1.2 2021/07/03 19:12:10 cirnatdan Exp $
 
-Remove usages of unsupported functions:
-- Gio.Credentials.get_unix_user
-- file.touch_async
+Remove initial-setup and lock screen warning due to unsupported functions
 
---- js/ui/main.js.orig	2021-03-20 12:17:01.417523000 +0000
+--- js/ui/main.js.orig	2021-06-10 11:33:01.609929600 +0000
 +++ js/ui/main.js
-@@ -289,19 +289,11 @@ function _initializeUI() {
-             });
+@@ -290,12 +290,6 @@ function _initializeUI() {
          }
  
--        let credentials = new Gio.Credentials();
+         let credentials = new Gio.Credentials();
 -        if (credentials.get_unix_user() === 0) {
 -            notify(_('Logged in as a privileged user'),
 -                   _('Running a session as a privileged user should be avoided for security reasons. If possible, you should log in as a normal user.'));
--        } else if (sessionMode.currentMode !== 'gdm' &&
-+        if (sessionMode.currentMode !== 'gdm' &&
-                    sessionMode.currentMode !== 'initial-setup') {
-             _handleShowWelcomeScreen();
-         }
+-        } else if (sessionMode.showWelcomeDialog) {
+-            _handleShowWelcomeScreen();
+-        }
  
--        if (sessionMode.currentMode !== 'gdm' &&
--            sessionMode.currentMode !== 'initial-setup')
--            _handleLockScreenWarning();
--
-         LoginManager.registerSessionWithGDM();
+         if (sessionMode.currentMode !== 'gdm' &&
+             sessionMode.currentMode !== 'initial-setup')
+@@ -321,29 +315,6 @@ function _handleShowWelcomeScreen() {
+ }
  
-         let perfModuleName = GLib.getenv("SHELL_PERF_MODULE");
-@@ -324,7 +316,7 @@ function _handleShowWelcomeScreen() {
  async function _handleLockScreenWarning() {
-     const path = '%s/lock-warning-shown'.format(global.userdatadir);
-     const file = Gio.File.new_for_path(path);
+-    const path = '%s/lock-warning-shown'.format(global.userdatadir);
+-    const file = Gio.File.new_for_path(path);
 -
-+return;
-     const hasLockScreen = screenShield !== null;
-     if (hasLockScreen) {
-         try {
+-    const hasLockScreen = screenShield !== null;
+-    if (hasLockScreen) {
+-        try {
+-            await file.delete_async(0, null);
+-        } catch (e) {
+-            if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.NOT_FOUND))
+-                logError(e);
+-        }
+-    } else {
+-        try {
+-            if (!await file.touch_async())
+-                return;
+-        } catch (e) {
+-            logError(e);
+-        }
+-
+-        notify(
+-            _('Screen Lock disabled'),
+-            _('Screen Locking requires the GNOME display manager.'));
+-    }
+ }
+ 
+ function _getStylesheet(name) {
