243c0029277ceb2ad2691a20bb0dfdd776de83f4
taca <taca@pkgsrc.org>
2021-07-04 06:24:45 +0000

    www/ruby-rails52: update to 5.2.6
    Ruby on Rails 5.2.6 (2021-05-05)
    
    There are changes in www/ruby-actionpack52 only, including security fix.
    
    Action Pack
    
    * Accept base64_urlsafe CSRF tokens to make forward compatible.
    
      Base64 strict-encoded CSRF tokens are not inherently websafe, which
      makes them difficult to deal with.  For example, the common practice
      of sending the CSRF token to a browser in a client-readable cookie
      does not work properly out of the box: the value has to be
      url-encoded and decoded to survive transport.
    
      In this version, we generate Base64 urlsafe-encoded CSRF tokens,
      which are inherently safe to transport.  Validation accepts both
      urlsafe tokens, and strict-encoded tokens for backwards
      compatibility.
    
      How the tokes are encoded is controllr by the
      action_controller.urlsafe_csrf_tokens config.
    
      In Rails 5.2.5, the CSRF token format was accidentally changed to
      urlsafe-encoded.
    
      Atention: If you already upgraded your application to 5.2.5, set the
      config urlsafe_csrf_tokens to true, otherwise your form submission
      will start to fail during the deploy of this new version.
    
    	Rails.application.config.action_controller.urlsafe_csrf_tokens = true
    
      If you are upgrading from 5.2.4.x, you don't need to change this
      configuration.
    
      Scott Blum, Étienne Barrié

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.100 2021/05/08 14:08:55 taca Exp $
+# $NetBSD: rails.mk,v 1.101 2021/07/04 06:24:45 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -47,7 +47,7 @@ _RUBY_RAILS_MK=	# defined
 #
 # current Ruby on Rails versions.
 #
-RUBY_RAILS52_VERSION?=	5.2.5
+RUBY_RAILS52_VERSION?=	5.2.6
 RUBY_RAILS60_VERSION?=	6.0.3.7
 RUBY_RAILS61_VERSION?=	6.1.3.2
 
