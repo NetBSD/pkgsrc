b37470910ab54b0f2b7bb8a82a8b53e3dee0cf2c
pin <pin@pkgsrc.org>
2021-07-05 08:43:27 +0000

    x11/rterm: update to 0.0.4
    -fix for arm64.
    
    This change hopefully fixes building on aarch64

--- x11/rterm/cargo-depends.mk
+++ x11/rterm/cargo-depends.mk
@@ -1,21 +1,21 @@
-# $NetBSD: cargo-depends.mk,v 1.2 2021/04/17 17:47:48 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.3 2021/07/05 08:43:27 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	arrayvec-0.5.2
 CARGO_CRATE_DEPENDS+=	bitflags-1.2.1
-CARGO_CRATE_DEPENDS+=	cc-1.0.67
+CARGO_CRATE_DEPENDS+=	cc-1.0.68
 CARGO_CRATE_DEPENDS+=	cfg-if-0.1.10
 CARGO_CRATE_DEPENDS+=	cmake-0.1.45
 CARGO_CRATE_DEPENDS+=	expat-sys-2.1.6
 CARGO_CRATE_DEPENDS+=	freetype-sys-0.13.1
-CARGO_CRATE_DEPENDS+=	libc-0.2.93
+CARGO_CRATE_DEPENDS+=	libc-0.2.97
 CARGO_CRATE_DEPENDS+=	nix-0.18.0
 CARGO_CRATE_DEPENDS+=	pkg-config-0.3.19
-CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.26
+CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.27
 CARGO_CRATE_DEPENDS+=	quote-1.0.9
 CARGO_CRATE_DEPENDS+=	servo-fontconfig-0.5.1
 CARGO_CRATE_DEPENDS+=	servo-fontconfig-sys-5.1.0
 CARGO_CRATE_DEPENDS+=	unicode-width-0.1.8
-CARGO_CRATE_DEPENDS+=	unicode-xid-0.2.1
+CARGO_CRATE_DEPENDS+=	unicode-xid-0.2.2
 CARGO_CRATE_DEPENDS+=	utf8parse-0.2.0
 CARGO_CRATE_DEPENDS+=	vte-0.10.1
 CARGO_CRATE_DEPENDS+=	vte_generate_state_changes-0.1.1
