eafb85e5d58a5e42c300e6bbb32450eecde89da0
markd <markd@pkgsrc.org>
2021-07-08 21:13:06 +0000

    openexr: update to 3.0.5
    ## Version 3.0.5 (July 1, 2021)
    
    Patch release that fixes problems with library symlinks and
    pkg-config, as well as miscellaneous bugs/security issues.
    
    ## Version 3.0.4 (June 3, 2021)
    
    Patch release that corrects a problem with the release version number
    of v3.0.2/v3.0.3:
    
    ## Version 3.0.3 (May 18, 2021)
    
    Patch release that fixes a regression in v3.0.2 the prevented headers
    from being installed properly.
    
    # Version 3.0.2 (May 17, 2021)
    
    Patch release with miscellaneous bug/build fixes, including:
    
    * Fix TimeCode.frame max value
    * Don't impose C++14 on downstream projects
    * Restore fix to macOS universal 2 build lost from #854
    * Imath auto-build version defaults to v3.0.2
    
    ## Version 3.0.1 (April 1, 2021)
    
    Major release with major build restructing, security improvements, and
    new features:
    
    * Restructuring:
      - The IlmBase/PyIlmBase submodules have been separated into the
        Imath project, now included by OpenEXR via a CMake submodule
        dependency, fetched automatically via CMake's FetchContent if
        necessary.
      - The library is now called ``libOpenEXR`` (instead of
        ``libIlmImf``).  No header files have been renamed, they retain
        the ``Imf`` prefix.
      - Symbol linkage visibility is limited to specific public symbols.
    
    * Build improvements:
      - No more simultaneous static/shared build option.
      - Community-provided support for bazel.
    
    * New Features:
      - ID Manifest Attributes, as described in ["A Scheme for Storing
        Object ID Manifests in OpenEXR
        Images"](https://doi.org/10.1145/3233085.3233086), Peter Hillman,
        DigiPro 18: Proceedings of the 8th Annual Digital Production
        Symposium, August 2018.
      - New program: exrcheck validates the contents of an EXR file.
    
    * Changes:
      - EXR files with no channels are no longer allowed.
      - Hard limit on the size of deep tile sizes; tiles must be less than
        2^30 pixels.
      - Tiled DWAB files used STATIC_HUFFMAN compression.
      - ``Int64`` and ``SInt64`` types are deprecated in favor of
        ``uint64_t`` and ``int64_t``.
      - Header files have been pruned of extraneous ``#include``'s
        ("Include What You Use"), which may generate compiler errors in
        application source code from undefined symbols or
        partially-defined types. These can be resolved by identifying and
        including the appropriate header.

--- graphics/openexr/patches/patch-src_lib_OpenEXR_ImfSystemSpecific.h
+++ graphics/openexr/patches/patch-src_lib_OpenEXR_ImfSystemSpecific.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-OpenEXR_IlmImf_ImfSystemSpecific.h,v 1.1 2019/09/21 21:34:44 nia Exp $
+$NetBSD: patch-src_lib_OpenEXR_ImfSystemSpecific.h,v 1.1 2021/07/08 21:13:06 markd Exp $
 
 Work around lack of posix_memalign() on (some?) Solaris, from PR 47867.
 
---- OpenEXR/IlmImf/ImfSystemSpecific.h.orig	2019-09-18 01:02:06.000000000 +0000
-+++ OpenEXR/IlmImf/ImfSystemSpecific.h
+--- src/lib/OpenEXR/ImfSystemSpecific.h.orig	2019-09-18 01:02:06.000000000 +0000
++++ src/lib/OpenEXR/ImfSystemSpecific.h
 @@ -59,6 +59,8 @@ EXRAllocAligned (size_t size, size_t ali
      return _aligned_malloc (size, alignment);
  #elif defined(__INTEL_COMPILER) || defined(__ICL) || defined(__ICC) || defined(__ECC)
