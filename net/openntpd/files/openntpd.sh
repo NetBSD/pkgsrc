b0adbef5d745dba76d44464bfe0946b3b01f5726
dholland <dholland@pkgsrc.org>
2021-07-10 18:32:15 +0000

    net/openntpd: make the rc script set the pidfile.
    Seems the daemon writes no pid file unless explicitly told where.
    Fixes PR 55244. PKGREVISION -> 1.

--- net/openntpd/files/openntpd.sh
+++ net/openntpd/files/openntpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: openntpd.sh,v 1.1.1.1 2004/07/19 19:17:25 xtraeme Exp $
+# $NetBSD: openntpd.sh,v 1.2 2021/07/10 18:32:15 dholland Exp $
 #
 
 # PROVIDE: openntpd
@@ -12,7 +12,14 @@
 name="ntpd"
 rcvar="openntpd"
 command="@PREFIX@/sbin/${name}"
+pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/ntpd.conf"
+start_precmd="$openntpd_precmd"
+
+openntpd_precmd()
+{
+	rc_flags="-p ${pidfile} $rc_flags"
+}
 
 load_rc_config $name
 run_rc_command "$1"
