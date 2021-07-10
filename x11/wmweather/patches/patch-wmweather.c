25c12b1d273cd52b518ca21a90411d38e661b26a
dholland <dholland@pkgsrc.org>
2021-07-10 17:13:44 +0000

    x11/wmweather: apply patch from Romain Dolbeau in PR 55284.
    Fixes time_t abuse leading to crashes on 32-bit platforms.

--- x11/wmweather/patches/patch-wmweather.c
+++ x11/wmweather/patches/patch-wmweather.c
@@ -1,10 +1,11 @@
-$NetBSD: patch-wmweather.c,v 1.1 2016/06/23 18:24:07 dholland Exp $
+$NetBSD: patch-wmweather.c,v 1.2 2021/07/10 17:13:44 dholland Exp $
 
 Use ctype.h functions correctly.
+Use the proper type with time().
 
---- wmweather.c~	2009-07-03 16:00:38.000000000 +0000
+--- wmweather.c.orig	2019-02-13 19:54:49.000000000 +0000
 +++ wmweather.c
-@@ -270,7 +270,7 @@ static void do_conf(const char *rcfile)
+@@ -269,7 +269,7 @@ static void do_conf(const char *rcfile)
  				exit(1);
  			}
  			for (i = 0; i < 4; i++)
@@ -13,7 +14,7 @@ Use ctype.h functions correctly.
  			if (station != NULL)
  				free(station);
  			station = strdup(optarg);
-@@ -407,7 +407,7 @@ static void do_opts(int argc, char *argv
+@@ -406,7 +406,7 @@ static void do_opts(int argc, char *argv
  				exit(1);
  			}
  			for (i = 0; i < 4; i++)
@@ -22,3 +23,30 @@ Use ctype.h functions correctly.
  			if (station != NULL)
  				free(station);
  			station = optarg;
+@@ -690,11 +690,12 @@ static void update(int force_read)
+ 	char   buffer[MAX_STRING], *i;
+ 	int    line, n, q, sgn;
+ 	long   l = 0;
++	time_t tl;
+ 
+-	time(&l);
+-	tm = gmtime(&l);
++	time(&tl);
++	tm = gmtime(&tl);
+ 	utc_diff = tm->tm_hour;
+-	tm = localtime(&l);
++	tm = localtime(&tl);
+ 	utc_diff = (tm->tm_hour - utc_diff + 24) % 24 * 3600;
+ 
+ 	if (stat(report, &rst) < 0 && errno != ENOENT) {
+@@ -1129,8 +1130,8 @@ static void update(int force_read)
+ 
+ 	} else {
+ 
+-		time(&l);
+-		tm = localtime(&l);
++		time(&tl);
++		tm = localtime(&tl);
+ 		if (gusting) {
+ 			showwinddir = tm->tm_sec % 30 < 10;
+ 			showgusting = tm->tm_sec % 30 >= 10 && tm->tm_sec % 30 < 20;
