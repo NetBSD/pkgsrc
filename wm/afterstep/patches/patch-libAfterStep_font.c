d8fb418bf672fc41864028b5f9f80c0aba379870
bsiegert <bsiegert@pkgsrc.org>
2021-07-11 12:13:17 +0000

    Update afterstep to 2.2.12 (released in 2013, lol).
    version 2.2.12 release
    
    * Session Management. AfterStep will utilize gnome-session for session
      management if available. Several things needs to be manually adjusted for
      full functionality :
    	1) go over list of autostart application files in /etc/xdg/autostart and for
    	   those that you want started and have line OnlyShowIn=  - add AfterStep to
    	   the list of environments. One example is gnome-keyring* stuff -
    	   by default it is only enabled in GNOME session.
    	2) if session takes a long time to close after Logout dialog was shown -
    	   most likely culprit is the pulseaudio. You may need to get rid of it and
    	   switch to more standard and stable ALSA.
    	3) It is recommended to uninstall zeitgeist daemon since its useless under
    	   AfterStep anyway but wastes resources with its snooping activity.
    	4) gnome-session will save any app that support session mamgement at the end
    	   of the session to be restarted the next time. Unless you run UBUNTU in
    	   which case this functionality my be disabled in stock gnome-session. If
    	   you want it back, then it is recommended that you build your own
    	   gnome-session from sources. On the other hand if you do not want this
    	   functionality, again, get gnome-session source and comment out contents of
    	   maybe_load_saved_session_apps() function in gnome-session/gsm-session-fill.c.
    	5) If you find that your GTK apps look butt-ugly - its probably because gconfd
    	   is not running. It is normally started by gnome-session, but bastardised
    	   version of it that comes with Ubuntu does not do it for some reason.
    * More Useless packages : ubuntu-desktop deja-dup

--- wm/afterstep/patches/patch-libAfterStep_font.c
+++ wm/afterstep/patches/patch-libAfterStep_font.c
@@ -1,22 +1,21 @@
-$NetBSD: patch-libAfterStep_font.c,v 1.1 2016/05/01 19:26:35 dholland Exp $
+$NetBSD: patch-libAfterStep_font.c,v 1.2 2021/07/11 12:13:17 bsiegert Exp $
 
 Use ctype.h correctly.
 
---- libAfterStep/font.c~	2007-07-10 17:48:06.000000000 +0000
+--- libAfterStep/font.c.orig	2013-05-01 13:34:11.000000000 +0000
 +++ libAfterStep/font.c
-@@ -50,12 +50,12 @@ load_font (const char *name_in, MyFont *
- 		register char *ptr = clean_name ;
- 		while( ptr[i] ) ++i ;
- 		while( --i >= 0 )
--			if( !isdigit(ptr[i]) )
-+			if( !isdigit((unsigned char)ptr[i]) )
+@@ -49,11 +49,11 @@ Bool load_font (const char *name_in, MyF
+ 		while (ptr[i])
+ 			++i;
+ 		while (--i >= 0)
+-			if (!isdigit (ptr[i]))
++			if (!isdigit ((unsigned char)ptr[i]))
  				break;
--		if( (isspace( ptr[i] ) || ptr[i] == '-') && ptr[i+1] )
-+		if( (isspace( (unsigned char)ptr[i] ) || ptr[i] == '-') && ptr[i+1] )
- 		{
- 			font_size = atoi( &(ptr[i+1]) );
--			while( i > 0 && isspace(ptr[i-1]) )	--i ;
-+			while( i > 0 && isspace((unsigned char)ptr[i-1]) )	--i ;
- 			clean_name = mystrndup( name, i );
+-		if ((isspace (ptr[i]) || ptr[i] == '-') && ptr[i + 1]) {
++		if ((isspace ((unsigned char)ptr[i]) || ptr[i] == '-') && ptr[i + 1]) {
+ 			font_size = atoi (&(ptr[i + 1]));
+-			while (i > 0 && isspace (ptr[i - 1]))
++			while (i > 0 && isspace ((unsigned char)ptr[i - 1]))
+ 				--i;
+ 			clean_name = mystrndup (name, i);
  		}
- 	}
