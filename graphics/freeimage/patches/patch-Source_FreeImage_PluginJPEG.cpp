505426bb1d0eb84996341b7ec6cc651b94dc5ba8
markd <markd@pkgsrc.org>
2021-07-11 04:08:06 +0000

    freeimage: get building again.
    In PluginJPEG.cpp avoid a name clash with jpeglib function.
    Restore accidentally deleted part of patch-Source_FreeImage_PluginTIFF.cpp.
    On Linux PluginTIFF.cpp extracts with carriage return/linefeeds whereas
    NetBSD only has linefeeds so strip carriage returns so patch will apply on
    both.

--- graphics/freeimage/patches/patch-Source_FreeImage_PluginJPEG.cpp
+++ graphics/freeimage/patches/patch-Source_FreeImage_PluginJPEG.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-Source_FreeImage_PluginJPEG.cpp,v 1.1 2020/05/14 16:42:14 nia Exp $
+$NetBSD: patch-Source_FreeImage_PluginJPEG.cpp,v 1.2 2021/07/11 04:08:06 markd Exp $
 
 Unbundle image libraries.
+Dont clash name with actual jpeglib function
 
 --- Source/FreeImage/PluginJPEG.cpp.orig	2018-07-28 18:22:24.000000000 +0000
 +++ Source/FreeImage/PluginJPEG.cpp
@@ -17,3 +18,21 @@ Unbundle image libraries.
  }
  
  #include "FreeImage.h"
+@@ -503,7 +503,7 @@ marker_is_icc(jpeg_saved_marker_ptr mark
+   return FALSE.  You might want to issue an error message instead.
+ */
+ static BOOL 
+-jpeg_read_icc_profile(j_decompress_ptr cinfo, JOCTET **icc_data_ptr, unsigned *icc_data_len) {
++jpeg_read_icc_profileX(j_decompress_ptr cinfo, JOCTET **icc_data_ptr, unsigned *icc_data_len) {
+ 	jpeg_saved_marker_ptr marker;
+ 	int num_markers = 0;
+ 	int seq_no;
+@@ -745,7 +745,7 @@ read_markers(j_decompress_ptr cinfo, FIB
+ 	BYTE *icc_profile = NULL;
+ 	unsigned icc_length = 0;
+ 
+-	if( jpeg_read_icc_profile(cinfo, &icc_profile, &icc_length) ) {
++	if( jpeg_read_icc_profileX(cinfo, &icc_profile, &icc_length) ) {
+ 		// copy ICC profile data
+ 		FreeImage_CreateICCProfile(dib, icc_profile, icc_length);
+ 		// clean up
