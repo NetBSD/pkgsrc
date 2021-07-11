a87466477980ea51358a821caaebd93f539dae8c
bsiegert <bsiegert@pkgsrc.org>
2021-07-11 12:13:17 +0000

    Update afterstep to 2.2.12 (released in 2013, lol).
    version 2.2.12 release
    
    * Session Management. AfterStep will utilize gnome-session for session
      management if available. Several things needs to be manually adjusted for
      full functionality :
    	1) go over list of autostart application files in /etc/xdg/autostart and for
    	   those that you want started and have line OnlyShowIn=  - add AfterStep to
    	   the list of environments. One example is gnome-keyring* stuff -
    	   by default it is only enabled in GNOME session.
    	2) if session takes a long time to close after Logout dialog was shown -
    	   most likely culprit is the pulseaudio. You may need to get rid of it and
    	   switch to more standard and stable ALSA.
    	3) It is recommended to uninstall zeitgeist daemon since its useless under
    	   AfterStep anyway but wastes resources with its snooping activity.
    	4) gnome-session will save any app that support session mamgement at the end
    	   of the session to be restarted the next time. Unless you run UBUNTU in
    	   which case this functionality my be disabled in stock gnome-session. If
    	   you want it back, then it is recommended that you build your own
    	   gnome-session from sources. On the other hand if you do not want this
    	   functionality, again, get gnome-session source and comment out contents of
    	   maybe_load_saved_session_apps() function in gnome-session/gsm-session-fill.c.
    	5) If you find that your GTK apps look butt-ugly - its probably because gconfd
    	   is not running. It is normally started by gnome-session, but bastardised
    	   version of it that comes with Ubuntu does not do it for some reason.
    * More Useless packages : ubuntu-desktop deja-dup

--- wm/afterstep/patches/patch-libAfterStep_functions.c
+++ wm/afterstep/patches/patch-libAfterStep_functions.c
@@ -1,44 +1,51 @@
-$NetBSD: patch-libAfterStep_functions.c,v 1.2 2016/05/01 19:26:35 dholland Exp $
+$NetBSD: patch-libAfterStep_functions.c,v 1.3 2021/07/11 12:13:17 bsiegert Exp $
 
 - use ctype.h correctly
 - patch up gcc inline mess
 
---- libAfterStep/functions.c.orig	2009-12-11 22:38:48.000000000 +0000
+--- libAfterStep/functions.c.orig	2013-05-01 13:34:11.000000000 +0000
 +++ libAfterStep/functions.c
-@@ -52,19 +52,19 @@ txt2func (const char *text, FunctionData
+@@ -63,7 +63,7 @@ int txt2func_code (const char *text)
  {
- 	TermDef      *fterm;
+ 	TermDef *fterm;
  
--	for (; isspace (*text); text++);
-+	for (; isspace ((unsigned char)*text); text++);
+-	for (; isspace (*text); text++) ;
++	for (; isspace ((unsigned char)*text); text++) ;
+ 	fterm = txt2fterm (text, True);
+ 	return (fterm != NULL) ? fterm->id : F_FUNCTIONS_NUM;
+ }
+@@ -72,17 +72,17 @@ int txt2func (const char *text, Function
+ {
+ 	TermDef *fterm;
+ 
+-	for (; isspace (*text); text++) ;
++	for (; isspace ((unsigned char)*text); text++) ;
  	fterm = txt2fterm (text, quiet);
- 	if (fterm != NULL)
- 	{
+ 	if (fterm != NULL) {
  		init_func_data (fdata);
  		fdata->func = fterm->id;
--		for (; !isspace (*text) && *text; text++);
--		for (; isspace (*text); text++);
-+		for (; !isspace ((unsigned char)*text) && *text; text++);
-+		for (; isspace ((unsigned char)*text); text++);
- 		if (*text)
- 		{
- 			const char   *ptr = text + strlen ((char*)text);
+-		for (; !isspace (*text) && *text; text++) ;
+-		for (; isspace (*text); text++) ;
++		for (; !isspace ((unsigned char)*text) && *text; text++) ;
++		for (; isspace ((unsigned char)*text); text++) ;
+ 		if (*text) {
+ 			const char *ptr = text + strlen ((char *)text);
  
--			for (; isspace (*(ptr - 1)); ptr--);
-+			for (; isspace ((unsigned char)*(ptr - 1)); ptr--);
+-			for (; isspace (*(ptr - 1)); ptr--) ;
++			for (; isspace ((unsigned char)*(ptr - 1)); ptr--) ;
  			fdata->text = mystrndup (text, ptr - text);
  		}
  	}
-@@ -80,7 +80,7 @@ parse_func (const char *text, FunctionDa
- 	int           sign = 0;
+@@ -97,7 +97,7 @@ int parse_func (const char *text, Functi
+ 	int sign = 0;
  
  	init_func_data (data);
--	for (ptr = (char *)text; isspace (*ptr); ptr++);
-+	for (ptr = (char *)text; isspace ((unsigned char)*ptr); ptr++);
- 	if (*ptr == '\0')
- 	{
+-	for (ptr = (char *)text; isspace (*ptr); ptr++) ;
++	for (ptr = (char *)text; isspace ((unsigned char)*ptr); ptr++) ;
+ 	if (*ptr == '\0') {
  		if (!quiet)
-@@ -94,7 +94,7 @@ parse_func (const char *text, FunctionDa
+ 			show_error ("empty function specification encountered.%s");
+@@ -110,7 +110,7 @@ int parse_func (const char *text, Functi
  	if (IsInternFunc (fterm->id))
  		return 0;
  
@@ -47,44 +54,40 @@ $NetBSD: patch-libAfterStep_functions.c,v 1.2 2016/05/01 19:26:35 dholland Exp $
  		ptr++;
  	data->func = fterm->id;
  	if (fterm->flags & TF_SYNTAX_TERMINATOR)
-@@ -109,14 +109,14 @@ parse_func (const char *text, FunctionDa
- 	{										   /* we still want to strip trailing whitespaces */
- 		char         *tail = ptr + strlen (ptr) - 1;
+@@ -124,13 +124,13 @@ int parse_func (const char *text, Functi
+ 	else {												/* we still want to strip trailing whitespaces */
+ 		char *tail = ptr + strlen (ptr) - 1;
  
--		for (; isspace (*tail) && tail > ptr; tail--);
-+		for (; isspace ((unsigned char)*tail) && tail > ptr; tail--);
+-		for (; isspace (*tail) && tail > ptr; tail--) ;
++		for (; isspace ((unsigned char)*tail) && tail > ptr; tail--) ;
  		*(tail + 1) = '\0';
  	}
  	/* this function is very often called so we try to use as little
  	   calls to other function as possible */
- 	for (; *ptr; ptr++)
- 	{
--		if (!isspace (*ptr))
-+		if (!isspace ((unsigned char)*ptr))
- 		{
- 			int           is_text = 0;
+ 	for (; *ptr; ptr++) {
+-		if (!isspace (*ptr)) {
++		if (!isspace ((unsigned char)*ptr)) {
+ 			int is_text = 0;
  
-@@ -138,15 +138,15 @@ parse_func (const char *text, FunctionDa
+ 			if (*ptr == '"') {
+@@ -149,13 +149,13 @@ int parse_func (const char *text, Functi
  				else if (data->text == NULL)
  					data->text = text;
  				ptr = tail + 1;
--			} else if (isdigit (*ptr))
-+			} else if (isdigit ((unsigned char)*ptr))
- 			{
- 				int           count;
- 				char          unit = '\0';
- 				int           val = 0;
+-			} else if (isdigit (*ptr)) {
++			} else if (isdigit ((unsigned char)*ptr)) {
+ 				int count;
+ 				char unit = '\0';
+ 				int val = 0;
  
--				for (count = 1; isdigit (*(ptr + count)); count++);
+-				for (count = 1; isdigit (*(ptr + count)); count++) ;
 -				if (*(ptr + count) != '\0' && !isspace (*(ptr + count)))
--					is_text = (!isspace (*(ptr + count + 1)) && *(ptr + count + 1) != '\0') ? 1 : 0;
-+				for (count = 1; isdigit ((unsigned char)*(ptr + count)); count++);
++				for (count = 1; isdigit ((unsigned char)*(ptr + count)); count++) ;
 +				if (*(ptr + count) != '\0' && !isspace ((unsigned char)*(ptr + count)))
-+					is_text = (!isspace ((unsigned char)*(ptr + count + 1)) && *(ptr + count + 1) != '\0') ? 1 : 0;
+ 					is_text = (!isspace (*(ptr + count + 1))
+ 										 && *(ptr + count + 1) != '\0') ? 1 : 0;
  				if (is_text == 0)
- 					ptr = parse_func_args (ptr, &unit, &val) - 1;
- 				if (curr_arg < MAX_FUNC_ARGS)
-@@ -187,7 +187,7 @@ parse_func (const char *text, FunctionDa
+@@ -190,7 +190,7 @@ int parse_func (const char *text, Functi
  					}
  					ptr = parse_token (ptr, &(data->text)) - 1;
  				} else
@@ -93,12 +96,12 @@ $NetBSD: patch-libAfterStep_functions.c,v 1.2 2016/05/01 19:26:35 dholland Exp $
  						ptr++;
  			}
  			sign = 0;
-@@ -314,7 +314,7 @@ dup_func_data (FunctionData * dst, Funct
-     }
+@@ -307,7 +307,7 @@ void dup_func_data (FunctionData * dst, 
+ 	}
  }
  
--inline FunctionData *
-+FunctionData *
- create_named_function( int func, char *name)
+-inline FunctionData *create_named_function (int func, char *name)
++FunctionData *create_named_function (int func, char *name)
  {
-     FunctionData *fdata = safecalloc( 1, sizeof(FunctionData) );
+ 	FunctionData *fdata = safecalloc (1, sizeof (FunctionData));
+ 
--- wm/afterstep/patches/patch-libAfterStep_mylook.c
+++ wm/afterstep/patches/patch-libAfterStep_mylook.c
@@ -1,24 +1,24 @@
-$NetBSD: patch-libAfterStep_mylook.c,v 1.2 2016/05/01 19:30:58 dholland Exp $
+$NetBSD: patch-libAfterStep_mylook.c,v 1.3 2021/07/11 12:13:17 bsiegert Exp $
 
 Patch up gcc inline mess.
 
---- libAfterStep/mylook.c.orig	2012-05-04 14:35:51.000000000 +0000
+--- libAfterStep/mylook.c.orig	2013-05-01 13:34:11.000000000 +0000
 +++ libAfterStep/mylook.c
-@@ -813,7 +813,7 @@ mylook_get_style(MyLook *look, const cha
-     return NULL;
+@@ -843,7 +843,7 @@ MyStyle *mylook_get_style (MyLook * look
+ 	return NULL;
  }
  
--inline MyDesktopConfig *
-+MyDesktopConfig *
- mylook_get_desk_config(MyLook *look, long desk)
+-inline MyDesktopConfig *mylook_get_desk_config (MyLook * look, long desk)
++MyDesktopConfig *mylook_get_desk_config (MyLook * look, long desk)
  {
-     if( look )
-@@ -827,7 +827,7 @@ mylook_get_desk_config(MyLook *look, lon
+ 	if (look) {
+ 		ASHashData hdata = { 0 };
+@@ -856,7 +856,7 @@ inline MyDesktopConfig *mylook_get_desk_
  }
  
  
--inline MyBackground *
-+MyBackground *
- mylook_get_desk_back(MyLook *look, long desk)
+-inline MyBackground *mylook_get_desk_back (MyLook * look, long desk)
++MyBackground *mylook_get_desk_back (MyLook * look, long desk)
  {
- 	MyBackground *myback = NULL ;
+ 	MyBackground *myback = NULL;
+ 
