d44697503d9a1f49a9cb818823591f81bdc26720
adam <adam@pkgsrc.org>
2021-07-12 18:46:07 +0000

    compiler-rt libunwind libcxx libcxxabi: updated to 12.0.1
    What’s New in Libc++ 12.0.0?
    
    New Features
    
    Random device support has been made optional. It’s enabled by default and can be disabled by building libc++ with -DLIBCXX_ENABLE_RANDOM_DEVICE=OFF. Disabling random device support can be useful when building the library for platforms that don’t have a source of randomness, such as some embedded platforms. When this is not supported, most of <random> will still be available, but std::random_device will not.
    Localization support has been made optional. It’s enabled by default and can be disabled by building libc++ with -DLIBCXX_ENABLE_LOCALIZATION=OFF. Disabling localization can be useful when porting to platforms that don’t support the C locale API (e.g. embedded). When localization is not supported, several parts of the library will be disabled: <iostream>, <regex>, <locale> will be completely unusable, and other parts may be only partly available.
    If libc++ is compiled with a C++20 capable compiler it will be compiled in C++20 mode. Else libc++ will be compiled in C++17 mode.
    Several unqualified lookups in libc++ have been changed to qualified lookups. This makes libc++ more ADL-proof.
    The libc++ implementation status pages have been overhauled. Like other parts documentation they now use restructured text instead of html. Starting with libc++12 the status pages are part of libc++’s documentation.
    More C++20 features have been implemented. libc++ C++20 Status has the full overview of libc++’s C++20 implementation status.
    Work has started to implement new C++2b features. libc++ C++2b Status has the full overview of libc++’s C++2b implementation status.
    
    API Changes
    
    By default, libc++ will _not_ include the definition for new and delete, since those are provided in libc++abi. Vendors wishing to provide new and delete in libc++ can build the library with -DLIBCXX_ENABLE_NEW_DELETE_DEFINITIONS=ON to get back the old behavior. This was done to avoid providing new and delete in both libc++ and libc++abi, which is technically an ODR violation. Also note that we couldn’t decide to put the operators in libc++ only, because they are needed from libc++abi (which would create a circular dependency).
    During the C++20 standardization process some new low-level bit functions have been renamed. Libc++ has renamed these functions to match the C++20 Standard. - ispow2 has been renamed to has_single_bit - ceil2 has been renamed to bit_ceil - floor2 has been renamed to bit_floor - log2p1 has been renamed to bit_width
    In C++20 mode, std::filesystem::path::u8string() and generic_u8string() now return std::u8string according to P0428, while they return std::string in C++17. This can cause source incompatibility, which is discussed and acknowledged in P1423, but that paper doesn’t suggest any remediation for this incompatibility.

--- lang/compiler-rt/patches/patch-cmake_config-ix.cmake
+++ lang/compiler-rt/patches/patch-cmake_config-ix.cmake
@@ -1,19 +1,19 @@
-$NetBSD: patch-cmake_config-ix.cmake,v 1.3 2019/06/02 08:35:56 adam Exp $
+$NetBSD: patch-cmake_config-ix.cmake,v 1.4 2021/07/12 18:46:07 adam Exp $
 
 Disable components that aren't ready for SunOS yet.
 
---- cmake/config-ix.cmake.orig	2019-01-25 22:55:41.000000000 +0000
+--- cmake/config-ix.cmake.orig	2021-04-06 16:38:18.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -529,7 +529,7 @@ set(COMPILER_RT_SANITIZERS_TO_BUILD all 
+@@ -646,7 +646,7 @@ set(COMPILER_RT_SANITIZERS_TO_BUILD all 
  list_replace(COMPILER_RT_SANITIZERS_TO_BUILD all "${ALL_SANITIZERS}")
  
  if (SANITIZER_COMMON_SUPPORTED_ARCH AND NOT LLVM_USE_SANITIZER AND
--    (OS_NAME MATCHES "Android|Darwin|Linux|FreeBSD|NetBSD|OpenBSD|Fuchsia|SunOS" OR
-+    (OS_NAME MATCHES "Android|Darwin|Linux|FreeBSD|NetBSD|OpenBSD|Fuchsia" OR
+-    (OS_NAME MATCHES "Android|Darwin|Linux|FreeBSD|NetBSD|Fuchsia|SunOS" OR
++    (OS_NAME MATCHES "Android|Darwin|Linux|FreeBSD|NetBSD|Fuchsia" OR
      (OS_NAME MATCHES "Windows" AND NOT CYGWIN AND
          (NOT MINGW OR CMAKE_CXX_COMPILER_ID MATCHES "Clang"))))
    set(COMPILER_RT_HAS_SANITIZER_COMMON TRUE)
-@@ -550,7 +550,7 @@ else()
+@@ -666,7 +666,7 @@ else()
    set(COMPILER_RT_HAS_ASAN FALSE)
  endif()
  
@@ -22,21 +22,21 @@ Disable components that aren't ready for SunOS yet.
    set(COMPILER_RT_ASAN_HAS_STATIC_RUNTIME TRUE)
  else()
    set(COMPILER_RT_ASAN_HAS_STATIC_RUNTIME FALSE)
-@@ -587,7 +587,7 @@ else()
+@@ -716,7 +716,7 @@ else()
  endif()
  
  if (PROFILE_SUPPORTED_ARCH AND NOT LLVM_USE_SANITIZER AND
--    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android|Fuchsia|SunOS|NetBSD")
-+    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android|Fuchsia|NetBSD")
+-    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android|Fuchsia|SunOS|NetBSD|AIX")
++    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android|Fuchsia|NetBSD|AIX")
    set(COMPILER_RT_HAS_PROFILE TRUE)
  else()
    set(COMPILER_RT_HAS_PROFILE FALSE)
-@@ -601,7 +601,7 @@ else()
+@@ -730,7 +730,7 @@ else()
  endif()
  
  if (COMPILER_RT_HAS_SANITIZER_COMMON AND UBSAN_SUPPORTED_ARCH AND
--    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD|OpenBSD|Windows|Android|Fuchsia|SunOS")
-+    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD|OpenBSD|Windows|Android|Fuchsia")
+-    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD|Windows|Android|Fuchsia|SunOS")
++    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD|Windows|Android|Fuchsia")
    set(COMPILER_RT_HAS_UBSAN TRUE)
  else()
    set(COMPILER_RT_HAS_UBSAN FALSE)
--- lang/libunwind/buildlink3.mk
+++ lang/libunwind/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2019/12/29 15:31:23 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2021/07/12 18:46:07 adam Exp $
 
 BUILDLINK_TREE+=	libunwind
 
 .if !defined(LIBUNWIND_BUILDLINK3_MK)
 LIBUNWIND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=9.0.1
+BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=12.0.0
 BUILDLINK_PKGSRCDIR.libunwind?=		../../lang/libunwind
 
 .include "../../lang/llvm/buildlink3.mk"
