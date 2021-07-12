484b8301a57e206aa982f199998678aea13a2c35
adam <adam@pkgsrc.org>
2021-07-12 18:47:15 +0000

    lld lldb: updated to 12.0.1
    12.0.1
    Non-comprehensive list of changes in this release
    ELF Improvements
    --dependency-file has been added. (Similar to cc -M -MF.) (D82437)
    --error-handling-script has been added to allow for user-defined handlers upon missing libraries. (D87758)
    --exclude-libs can now localize defined version symbols and bitcode referenced libcall symbols. (D94280)
    --gdb-index now works with DWARF v5 and --icf={safe,all}. (D85579) (D89751)
    --gdb-index --emit-relocs can now be used together. (D94354)
    --icf={safe,all} conservatively no longer fold text sections with LSDA. Previously ICF on -fexceptions code could be unsafe. (D84610)
    --icf={safe,all} can now fold two sections with relocations referencing aliased symbols. (D88830)
    --lto-pseudo-probe-for-profiling has been added. (D95056)
    --no-lto-whole-program-visibility has been added. (D92060)
    --oformat-binary has been fixed to respect LMA. (D85086)
    --reproduce includes --lto-sample-profile, --just-symbols, --call-graph-ordering-file, --retain-symbols-file files.
    -r --gc-sections is now supported. (D84131)
    A -u specified symbol will no longer change the binding to STB_WEAK. (D88945)
    --wrap support has been improved. + If foo is not referenced, there is no longer an undefined symbol __wrap_foo. + If __real_foo is not referenced, there is no longer an undefined symbol foo.
    SHF_LINK_ORDER sections can now have zero sh_link values.
    SHF_LINK_ORDER and non-SHF_LINK_ORDER sections can now be mixed within an input section description. (D84001)
    LOG2CEIL is now supported in linker scripts. (D84054)
    DEFINED has been fixed to check whether the symbol is defined. (D83758)
    An input section description may now have multiple SORT_*. The matched sections are ordered by radix sort with the keys being (SORT*, --sort-section, input order). (D91127)
    Users can now provide a GNU style linker script to convert .ctors into .init_array. (D91187)
    An empty output section can now be discarded even if it is assigned to a program header. (D92301)
    Non-SHF_ALLOC sections now have larger file offsets than SHF_ALLOC sections. (D85867)
    Some symbol versioning improvements. + Defined foo@@v1 now resolve undefined foo@v1 (D92259) + Undefined foo@v1 now gets an error (D92260)
    The AArch64 port now has support for STO_AARCH64_VARIANT_PCS and DT_AARCH64_VARIANT_PCS. (D93045)
    The AArch64 port now has support for R_AARCH64_LD64_GOTPAGE_LO15.
    The PowerPC64 port now detects missing R_PPC64_TLSGD/R_PPC64_TLSLD and disables TLS relaxation. This allows linking with object files produced by very old IBM XL compilers. (D92959)
    Many PowerPC PC-relative relocations are now supported.
    R_PPC_ADDR24 and R_PPC64_ADDR16_HIGH are now supported.
    powerpcle is now supported. Tested with FreeBSD loader and freestanding. (D93917)
    RISC-V: the first SHT_RISCV_ATTRIBUTES section is now retained. (D86309)
    LTO pipeline now defaults to the new PM if the CMake variable ENABLE_EXPERIMENTAL_NEW_PASS_MANAGER is on. (D92885)

--- devel/lld/patches/patch-ELF_Driver.cpp
+++ devel/lld/patches/patch-ELF_Driver.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-ELF_Driver.cpp,v 1.8 2020/04/18 08:00:50 adam Exp $
+$NetBSD: patch-ELF_Driver.cpp,v 1.9 2021/07/12 18:47:15 adam Exp $
 
 Add dummy option for an AArch64 erratum.
 
---- ELF/Driver.cpp.orig	2020-03-23 15:01:02.000000000 +0000
+--- ELF/Driver.cpp.orig	2020-10-07 10:10:48.000000000 +0000
 +++ ELF/Driver.cpp
-@@ -132,7 +132,7 @@ static std::tuple<ELFKind, uint16_t, uin
+@@ -135,7 +135,7 @@ static std::tuple<ELFKind, uint16_t, uin
    std::pair<ELFKind, uint16_t> ret =
        StringSwitch<std::pair<ELFKind, uint16_t>>(s)
            .Cases("aarch64elf", "aarch64linux", "aarch64_elf64_le_vec",
@@ -13,7 +13,7 @@ Add dummy option for an AArch64 erratum.
            .Cases("armelf", "armelf_linux_eabi", {ELF32LEKind, EM_ARM})
            .Case("elf32_x86_64", {ELF32LEKind, EM_X86_64})
            .Cases("elf32btsmip", "elf32btsmipn32", {ELF32BEKind, EM_MIPS})
-@@ -296,6 +296,9 @@ static void checkOptions() {
+@@ -300,6 +300,9 @@ static void checkOptions() {
    if (config->emachine == EM_MIPS && config->gnuHash)
      error("the .gnu.hash section is not compatible with the MIPS target");
  
@@ -23,11 +23,12 @@ Add dummy option for an AArch64 erratum.
    if (config->fixCortexA53Errata843419 && config->emachine != EM_AARCH64)
      error("--fix-cortex-a53-843419 is only supported on AArch64 targets");
  
-@@ -879,6 +882,7 @@ static void readConfigs(opt::InputArgLis
+@@ -937,6 +940,8 @@ static void readConfigs(opt::InputArgLis
        args.hasFlag(OPT_export_dynamic, OPT_no_export_dynamic, false);
    config->filterList = args::getStrings(args, OPT_filter);
    config->fini = args.getLastArgValue(OPT_fini, "_fini");
-+  config->fixCortexA53Errata835769 = args.hasArg(OPT_fix_cortex_a53_843419);
-   config->fixCortexA53Errata843419 = args.hasArg(OPT_fix_cortex_a53_843419);
-   config->fixCortexA8 = args.hasArg(OPT_fix_cortex_a8);
-   config->forceBTI = hasZOption(args, "force-bti");
++  config->fixCortexA53Errata835769 = args.hasArg(OPT_fix_cortex_a53_843419) &&
++                                     !args.hasArg(OPT_relocatable);
+   config->fixCortexA53Errata843419 = args.hasArg(OPT_fix_cortex_a53_843419) &&
+                                      !args.hasArg(OPT_relocatable);
+   config->fixCortexA8 =
