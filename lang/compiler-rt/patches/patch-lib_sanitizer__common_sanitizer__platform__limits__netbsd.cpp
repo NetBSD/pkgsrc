54fcec90d3fb9ac6183c7e8de352116ea2ba43c8
adam <adam@pkgsrc.org>
2021-07-12 18:46:07 +0000

    compiler-rt libunwind libcxx libcxxabi: updated to 12.0.1
    What’s New in Libc++ 12.0.0?
    
    New Features
    
    Random device support has been made optional. It’s enabled by default and can be disabled by building libc++ with -DLIBCXX_ENABLE_RANDOM_DEVICE=OFF. Disabling random device support can be useful when building the library for platforms that don’t have a source of randomness, such as some embedded platforms. When this is not supported, most of <random> will still be available, but std::random_device will not.
    Localization support has been made optional. It’s enabled by default and can be disabled by building libc++ with -DLIBCXX_ENABLE_LOCALIZATION=OFF. Disabling localization can be useful when porting to platforms that don’t support the C locale API (e.g. embedded). When localization is not supported, several parts of the library will be disabled: <iostream>, <regex>, <locale> will be completely unusable, and other parts may be only partly available.
    If libc++ is compiled with a C++20 capable compiler it will be compiled in C++20 mode. Else libc++ will be compiled in C++17 mode.
    Several unqualified lookups in libc++ have been changed to qualified lookups. This makes libc++ more ADL-proof.
    The libc++ implementation status pages have been overhauled. Like other parts documentation they now use restructured text instead of html. Starting with libc++12 the status pages are part of libc++’s documentation.
    More C++20 features have been implemented. libc++ C++20 Status has the full overview of libc++’s C++20 implementation status.
    Work has started to implement new C++2b features. libc++ C++2b Status has the full overview of libc++’s C++2b implementation status.
    
    API Changes
    
    By default, libc++ will _not_ include the definition for new and delete, since those are provided in libc++abi. Vendors wishing to provide new and delete in libc++ can build the library with -DLIBCXX_ENABLE_NEW_DELETE_DEFINITIONS=ON to get back the old behavior. This was done to avoid providing new and delete in both libc++ and libc++abi, which is technically an ODR violation. Also note that we couldn’t decide to put the operators in libc++ only, because they are needed from libc++abi (which would create a circular dependency).
    During the C++20 standardization process some new low-level bit functions have been renamed. Libc++ has renamed these functions to match the C++20 Standard. - ispow2 has been renamed to has_single_bit - ceil2 has been renamed to bit_ceil - floor2 has been renamed to bit_floor - log2p1 has been renamed to bit_width
    In C++20 mode, std::filesystem::path::u8string() and generic_u8string() now return std::u8string according to P0428, while they return std::string in C++17. This can cause source incompatibility, which is discussed and acknowledged in P1423, but that paper doesn’t suggest any remediation for this incompatibility.

--- lang/compiler-rt/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp
+++ lang/compiler-rt/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp
@@ -1,30 +1,10 @@
-$NetBSD: patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp,v 1.2 2020/07/26 19:20:13 adam Exp $
+$NetBSD: patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp,v 1.3 2021/07/12 18:46:07 adam Exp $
 
 urio and smb have been removed from NetBSD.
 
---- lib/sanitizer_common/sanitizer_platform_limits_netbsd.cpp.orig	2020-07-07 16:21:37.000000000 +0000
+--- lib/sanitizer_common/sanitizer_platform_limits_netbsd.cpp.orig	2020-10-07 10:10:48.000000000 +0000
 +++ lib/sanitizer_common/sanitizer_platform_limits_netbsd.cpp
-@@ -166,7 +166,9 @@
- #include <netinet/ip_proxy.h>
- #include <netinet6/in6_var.h>
- #include <netinet6/nd6.h>
-+#if __NetBSD_Version__ < 999000000
- #include <netsmb/smb_dev.h>
-+#endif
- #include <dev/biovar.h>
- #include <dev/bluetooth/btdev.h>
- #include <dev/bluetooth/btsco.h>
-@@ -190,7 +192,9 @@
- #include <dev/sun/vuid_event.h>
- #include <dev/tc/sticio.h>
- #include <dev/usb/ukyopon.h>
-+#if __NetBSD_Version__ < 999000000
- #include <dev/usb/urio.h>
-+#endif
- #include <dev/usb/usb.h>
- #include <dev/usb/utoppy.h>
- #include <dev/vme/xio.h>
-@@ -672,12 +676,14 @@ unsigned struct_session_op_sz = sizeof(s
+@@ -803,12 +803,14 @@ unsigned struct_session_op_sz = sizeof(s
  unsigned struct_sgttyb_sz = sizeof(sgttyb);
  unsigned struct_sioc_sg_req_sz = sizeof(sioc_sg_req);
  unsigned struct_sioc_vif_req_sz = sizeof(sioc_vif_req);
@@ -39,7 +19,7 @@ urio and smb have been removed from NetBSD.
  unsigned struct_spppauthcfg_sz = sizeof(spppauthcfg);
  unsigned struct_spppauthfailuresettings_sz = sizeof(spppauthfailuresettings);
  unsigned struct_spppauthfailurestats_sz = sizeof(spppauthfailurestats);
-@@ -702,7 +708,9 @@ unsigned struct_twe_drivecommand_sz = si
+@@ -833,7 +835,9 @@ unsigned struct_twe_drivecommand_sz = si
  unsigned struct_twe_paramcommand_sz = sizeof(twe_paramcommand);
  unsigned struct_twe_usercommand_sz = sizeof(twe_usercommand);
  unsigned struct_ukyopon_identify_sz = sizeof(ukyopon_identify);
@@ -49,7 +29,7 @@ urio and smb have been removed from NetBSD.
  unsigned struct_usb_alt_interface_sz = sizeof(usb_alt_interface);
  unsigned struct_usb_bulk_ra_wb_opt_sz = sizeof(usb_bulk_ra_wb_opt);
  unsigned struct_usb_config_desc_sz = sizeof(usb_config_desc);
-@@ -1291,8 +1299,10 @@ unsigned IOCTL_STICIO_RESET = STICIO_RES
+@@ -1422,8 +1426,10 @@ unsigned IOCTL_STICIO_RESET = STICIO_RES
  unsigned IOCTL_STICIO_STARTQ = STICIO_STARTQ;
  unsigned IOCTL_STICIO_STOPQ = STICIO_STOPQ;
  unsigned IOCTL_UKYOPON_IDENTIFY = UKYOPON_IDENTIFY;
@@ -60,7 +40,7 @@ urio and smb have been removed from NetBSD.
  unsigned IOCTL_USB_REQUEST = USB_REQUEST;
  unsigned IOCTL_USB_SETDEBUG = USB_SETDEBUG;
  unsigned IOCTL_USB_DISCOVER = USB_DISCOVER;
-@@ -1677,6 +1687,7 @@ unsigned IOCTL_SIOCCONNECTXDEL = SIOCCON
+@@ -1808,6 +1814,7 @@ unsigned IOCTL_SIOCCONNECTXDEL = SIOCCON
  unsigned IOCTL_SIOCSIFINFO_FLAGS = SIOCSIFINFO_FLAGS;
  unsigned IOCTL_SIOCAADDRCTL_POLICY = SIOCAADDRCTL_POLICY;
  unsigned IOCTL_SIOCDADDRCTL_POLICY = SIOCDADDRCTL_POLICY;
@@ -68,7 +48,7 @@ urio and smb have been removed from NetBSD.
  unsigned IOCTL_SMBIOC_OPENSESSION = SMBIOC_OPENSESSION;
  unsigned IOCTL_SMBIOC_OPENSHARE = SMBIOC_OPENSHARE;
  unsigned IOCTL_SMBIOC_REQUEST = SMBIOC_REQUEST;
-@@ -1684,6 +1695,7 @@ unsigned IOCTL_SMBIOC_SETFLAGS = SMBIOC_
+@@ -1815,6 +1822,7 @@ unsigned IOCTL_SMBIOC_SETFLAGS = SMBIOC_
  unsigned IOCTL_SMBIOC_LOOKUP = SMBIOC_LOOKUP;
  unsigned IOCTL_SMBIOC_READ = SMBIOC_READ;
  unsigned IOCTL_SMBIOC_WRITE = SMBIOC_WRITE;
--- lang/libcxxabi/buildlink3.mk
+++ lang/libcxxabi/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/12/29 15:31:22 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/07/12 18:46:07 adam Exp $
 
 BUILDLINK_TREE+=	libcxxabi
 
 .if !defined(LIBCXXABI_BUILDLINK3_MK)
 LIBCXXABI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcxxabi+=	libcxxabi>=9.0.1
+BUILDLINK_API_DEPENDS.libcxxabi+=	libcxxabi>=12.0.0
 BUILDLINK_PKGSRCDIR.libcxxabi?=		../../lang/libcxxabi
 
 .include "../../lang/llvm/buildlink3.mk"
+.include "../../lang/libunwind/buildlink3.mk"
 .endif	# LIBCXXABI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libcxxabi
