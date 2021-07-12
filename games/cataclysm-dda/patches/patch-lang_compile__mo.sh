4fee853bc7892ecfad9995cfe922780ddefdc562
nia <nia@pkgsrc.org>
2021-07-12 20:54:02 +0000

    cataclysm-dda: update to 0.F
    ## Highlights
    Nested Containers rationalize inventory management and enable dropping and retrieving go-bags during fights.
    Achievements track your deeds and misdeeds across games.
    Proficiencies better represent deeper knowledge required for various endeavors, mostly crafting.
    Bleeding added to both the player and monsters as the first step toward a more comprehensive wound and wound treatment system.
    Weariness tracking added to represent longer-term physical exhaustion.
    Elevated bridges over navigable rivers added, allowing better navigability while using boats.
    Large-scale audit of weapon and armor values for better representativeness and consistency.
    Improved armor handling by separating ballistic damage into its own damage type.
    Pervasive performance enhancements throughout the game.
    Tileset vehicle support for more cohesive vehicle rendering.
    Aftershock changes direction to a total conversion mod with a new far-future setting on a frozen world.
    Dinomod added 238 dinosaurs, pterosaurs, mosasaurs, and dino-related NPCs with missions and dino locations.
    Added many dino features, including zombie, fungal, evolved, bionic, baby, and mutant dino variants.
    Dinomod added many dino interactions, including farming, riding, butchering, cooking, and special attacks.
    Magiclysm added a huge content update including many new traits called Attunements that switch up gameplay at the endgame.
    
    ## Statistics
    3974 files changed, 5440251 insertions(+), 3904330 deletions(-)
    10,463 commits
    ~454 contributors

--- games/cataclysm-dda/patches/patch-lang_compile__mo.sh
+++ games/cataclysm-dda/patches/patch-lang_compile__mo.sh
@@ -1,15 +1,15 @@
-$NetBSD: patch-lang_compile__mo.sh,v 1.1 2020/04/30 14:57:30 nia Exp $
+$NetBSD: patch-lang_compile__mo.sh,v 1.2 2021/07/12 20:54:02 nia Exp $
 
 Shell portability.
 
---- lang/compile_mo.sh.orig	2020-04-01 05:39:38.000000000 +0000
+--- lang/compile_mo.sh.orig	2021-07-03 05:00:06.000000000 +0000
 +++ lang/compile_mo.sh
-@@ -24,7 +24,7 @@ then
+@@ -26,7 +26,7 @@ then
      for n in $@
      do
          f="lang/po/${n}.po"
--        if [ $n == "en" ]; then
-+        if [ $n = "en" ]; then
+-        if ( [ "$n" == "en" ]) && ((! [ "${os##CYGWIN*}" ]) || (! [ "${os##MINGW*}" ])); then
++        if ( [ "$n" = "en" ]) && ((! [ "${os##CYGWIN*}" ]) || (! [ "${os##MINGW*}" ])); then
              # English is special: we do not actually need translation for English,
              # but due to a libintl bug (https://savannah.gnu.org/bugs/index.php?58006),
              # gettext would be extremely slow on MinGW targets if we do not compile
