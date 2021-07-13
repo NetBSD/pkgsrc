c1ed3f6d815ded60286267befa34599665aacc4d
nia <nia@pkgsrc.org>
2021-07-13 12:35:09 +0000

    claws-mail: update to 4.0.0.
    This is the first release using gtk3.

--- mail/claws-mail/patches/patch-src_plugins_vcalendar_vcal__folder.c
+++ mail/claws-mail/patches/patch-src_plugins_vcalendar_vcal__folder.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-ca,v 1.5 2019/11/26 19:13:25 nia Exp $
+$NetBSD: patch-src_plugins_vcalendar_vcal__folder.c,v 1.1 2021/07/13 12:35:09 nia Exp $
 
---- src/plugins/vcalendar/vcal_folder.c.orig	2019-01-27 15:57:37.000000000 +0000
+--- src/plugins/vcalendar/vcal_folder.c.orig	2021-07-10 08:47:14.000000000 +0000
 +++ src/plugins/vcalendar/vcal_folder.c
 @@ -778,7 +778,7 @@ GSList *vcal_get_events_list(FolderItem 
          			struct icaltimetype dtstart;
