101dda9f9de10c6c6275bfd8265360eb9865f3f4
bsiegert <bsiegert@pkgsrc.org>
2021-07-13 11:45:02 +0000

    gif2png: unbreak build.
    This was accidentally committed in a half-finished state. Add the missing
    files to make this build.

---
+++ graphics/gif2png/go-modules.mk
@@ -0,0 +1,15 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/07/13 11:45:02 bsiegert Exp $
+
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210513164829-c07d793c2f9a.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210513164829-c07d793c2f9a.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20201119102817-f84b799fce68.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210514084401-e8d321eab015.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210514084401-e8d321eab015.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.zip
