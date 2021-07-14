8f3ef2d37eaf07bbefd3cae834f33d80635c525d
jperkin <jperkin@pkgsrc.org>
2021-07-14 14:32:04 +0000

    go: Remove previous change, fixed in go-bin instead.

--- lang/go/bootstrap.mk
+++ lang/go/bootstrap.mk
@@ -1,16 +1,9 @@
-# $NetBSD: bootstrap.mk,v 1.6 2021/07/14 10:18:28 jperkin Exp $
+# $NetBSD: bootstrap.mk,v 1.7 2021/07/14 14:32:04 jperkin Exp $
 
 .if !defined(GOROOT_BOOTSTRAP) || !exists(${GOROOT_BOOTSTRAP}/bin/go)
 .  if ${MACHINE_ARCH} == "aarch64"
 BUILD_DEPENDS+=		go-bin-[0-9]*:../../lang/go-bin
 GOROOT_BOOTSTRAP=	${PREFIX}/go-bin
-.    if ${OPSYS} == "Darwin"
-pre-build: remove-codesign
-remove-codesign:
-	@for f in ${PREFIX}/go-bin/bin/* ${PREFIX}/go-bin/pkg/tool/darwin_arm64/*; do \
-		/usr/bin/codesign --remove-signature $$f; \
-	done
-.    endif
 .  else
 BUILD_DEPENDS+=		go14-1.4*:../../lang/go14
 GOROOT_BOOTSTRAP=	${PREFIX}/go14
