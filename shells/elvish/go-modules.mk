6dd0cfa0044986cfe7f7c9bc4c8164f84edafe08
pin <pin@pkgsrc.org>
2021-07-14 20:51:27 +0000

    shells/elvish: import package
    Packaged in wip by mjturner
    
    Elvish is a friendly interactive shell and an expressive programming language.
    Despite its pre-1.0 status, it is already suitable for most daily interactive
    use.

---
+++ shells/elvish/go-modules.mk
@@ -0,0 +1,16 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/07/14 20:51:27 pin Exp $
+
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.zip
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.11.mod
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.11.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.12.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.12.zip
+GO_MODULE_FILES+=	github.com/xiaq/persistent/@v/v0.0.0-20200820214153-3175cfb92e14.mod
+GO_MODULE_FILES+=	github.com/xiaq/persistent/@v/v0.0.0-20200820214153-3175cfb92e14.zip
+GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.5.mod
+GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.5.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200116001909-b77594299b42.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200202164722-d101bd2416d5.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200824131525-c12d262b63d8.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200824131525-c12d262b63d8.zip
