7c7b971898057068086c9c6f614aa78f04b82667
wiz <wiz@pkgsrc.org>
2021-07-14 09:08:48 +0000

    creduce: fix build with llvm-12
    From https://github.com/csmith-project/creduce/tree/llvm-12.0-dev

--- devel/creduce/patches/patch-clang__delta_TransformationManager.cpp
+++ devel/creduce/patches/patch-clang__delta_TransformationManager.cpp
@@ -1,46 +1,45 @@
-$NetBSD: patch-clang__delta_TransformationManager.cpp,v 1.6 2020/04/20 16:33:51 adam Exp $
+$NetBSD: patch-clang__delta_TransformationManager.cpp,v 1.7 2021/07/14 09:08:49 wiz Exp $
 
-Port to LLVM 10.0
-https://github.com/csmith-project/creduce/pull/204/
+Build fix for llvm 12.0.1 from https://github.com/csmith-project/creduce/tree/llvm-12.0-dev
 
 --- clang_delta/TransformationManager.cpp.orig	2019-05-13 21:17:30.000000000 +0000
 +++ clang_delta/TransformationManager.cpp
-@@ -16,6 +16,7 @@
+@@ -16,9 +16,12 @@
  
  #include <sstream>
  
 +#include "clang/Basic/Builtins.h"
  #include "clang/Basic/Diagnostic.h"
++#include "clang/Basic/FileManager.h"
  #include "clang/Basic/TargetInfo.h"
  #include "clang/Lex/Preprocessor.h"
-@@ -101,6 +102,7 @@ bool TransformationManager::initializeCo
++#include "clang/Lex/PreprocessorOptions.h"
+ #include "clang/Frontend/CompilerInstance.h"
+ #include "clang/Parse/ParseAST.h"
+ 
+@@ -101,16 +104,16 @@ bool TransformationManager::initializeCo
    CompilerInvocation &Invocation = ClangInstance->getInvocation();
    InputKind IK = FrontendOptions::getInputKindForExtension(
          StringRef(SrcFileName).rsplit('.').second);
-+#if LLVM_VERSION_MAJOR < 10
-   if (IK.getLanguage() == InputKind::C) {
-     Invocation.setLangDefaults(ClangInstance->getLangOpts(), InputKind::C, T, PPOpts);
-   }
-@@ -111,6 +113,18 @@ bool TransformationManager::initializeCo
-     Invocation.setLangDefaults(ClangInstance->getLangOpts(), InputKind::CXX, T, PPOpts);
-   }
-   else if(IK.getLanguage() == InputKind::OpenCL) {
-+#else
+-  if (IK.getLanguage() == InputKind::C) {
+-    Invocation.setLangDefaults(ClangInstance->getLangOpts(), InputKind::C, T, PPOpts);
 +  if (IK.getLanguage() == Language::C) {
-+    Invocation.setLangDefaults(ClangInstance->getLangOpts(), InputKind(Language::C), T, PPOpts);
-+  }
++    Invocation.setLangDefaults(ClangInstance->getLangOpts(), Language::C, T, PPOpts.Includes);
+   }
+-  else if (IK.getLanguage() == InputKind::CXX) {
 +  else if (IK.getLanguage() == Language::CXX) {
-+    // ISSUE: it might cause some problems when building AST
-+    // for a function which has a non-declared callee, e.g.,
-+    // It results an empty AST for the caller.
-+    Invocation.setLangDefaults(ClangInstance->getLangOpts(), InputKind(Language::CXX), T, PPOpts);
-+  }
+     // ISSUE: it might cause some problems when building AST
+     // for a function which has a non-declared callee, e.g.,
+     // It results an empty AST for the caller.
+-    Invocation.setLangDefaults(ClangInstance->getLangOpts(), InputKind::CXX, T, PPOpts);
++    Invocation.setLangDefaults(ClangInstance->getLangOpts(), Language::CXX, T, PPOpts.Includes);
+   }
+-  else if(IK.getLanguage() == InputKind::OpenCL) {
 +  else if(IK.getLanguage() == Language::OpenCL) {
-+#endif
      //Commandline parameters
      std::vector<const char*> Args;
      Args.push_back("-x");
-@@ -122,7 +136,7 @@ bool TransformationManager::initializeCo
+@@ -122,7 +125,7 @@ bool TransformationManager::initializeCo
      ClangInstance->createFileManager();
  
      if(CLCPath != NULL && ClangInstance->hasFileManager() &&
@@ -49,24 +48,16 @@ https://github.com/csmith-project/creduce/pull/204/
          Args.push_back("-I");
          Args.push_back(CLCPath);
      }
-@@ -132,10 +146,19 @@ bool TransformationManager::initializeCo
+@@ -132,10 +135,10 @@ bool TransformationManager::initializeCo
      Args.push_back("-fno-builtin");
  
      CompilerInvocation::CreateFromArgs(Invocation,
-+#if LLVM_VERSION_MAJOR >= 10
-+                                       Args,
-+#else
-                                        &Args[0], &Args[0] + Args.size(),
-+#endif
+-                                       &Args[0], &Args[0] + Args.size(),
++		                       ArrayRef<const char*>(&Args[0], &Args[0] + Args.size()),
                                         ClangInstance->getDiagnostics());
      Invocation.setLangDefaults(ClangInstance->getLangOpts(),
 -                               InputKind::OpenCL, T, PPOpts);
-+#if LLVM_VERSION_MAJOR >= 10
-+                               InputKind(Language::OpenCL),
-+#else
-+                               InputKind::OpenCL,
-+#endif
-+			       T, PPOpts);
++                               Language::OpenCL, T, PPOpts.Includes);
    }
    else {
      ErrorMsg = "Unsupported file type!";
