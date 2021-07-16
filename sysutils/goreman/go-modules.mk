e8e6e41fc637e047c93f7458eb61c0da8d8287cb
nia <nia@pkgsrc.org>
2021-07-16 06:46:18 +0000

    goreman: update to 0.3.7. Converted to go-module.

---
+++ sysutils/goreman/go-modules.mk
@@ -0,0 +1,17 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/07/16 06:46:18 nia Exp $
+
+GO_MODULE_FILES+=	github.com/joho/godotenv/@v/v1.3.0.mod
+GO_MODULE_FILES+=	github.com/joho/godotenv/@v/v1.3.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.10.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.10.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.8.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190222072716-a9d3bda3a223.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191008105621-543471e840be.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200124204421-9fbb57f87de9.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200124204421-9fbb57f87de9.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.zip
+GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.2.7.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.2.7.zip
