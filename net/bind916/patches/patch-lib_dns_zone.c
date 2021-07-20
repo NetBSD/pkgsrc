938dc0f1d67e9a53635de40f7457020615403c58
rin <rin@pkgsrc.org>
2021-07-20 06:39:45 +0000

    net/bind916 Use atomic 32-bit integers where appropriate, revision++
    Mostly taken from NetBSD base:
    
    - For counters, make sure to use 32-bit integers for !_LP64 platforms.
      In the previous revisions, this is partially done, i.e., incomplete.
    
    - For flags fit within 32-bit width, use 32-bit integers for everyone.
      In the previous, this is incomplete, and restricted for __NetBSD__.
      Fix and generalize to everyone.
    
    - Make comments in patches more helpful.
    
    Fix build for ILP32 platforms as reported in PR pkg/56315.
    
    Thanks jklos@ for testing.

--- net/bind916/patches/patch-lib_dns_zone.c
+++ net/bind916/patches/patch-lib_dns_zone.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-lib_dns_zone.c,v 1.2 2021/03/21 04:16:17 taca Exp $
+$NetBSD: patch-lib_dns_zone.c,v 1.3 2021/07/20 06:39:45 rin Exp $
 
-* Take from NetBSD base.
+No need to use atomic 64-bit integers for flags fit within 32-bit width.
 
---- lib/dns/zone.c.orig	2021-03-11 13:20:59.000000000 +0000
-+++ lib/dns/zone.c
-@@ -224,8 +224,13 @@ struct dns_zone {
+--- lib/dns/zone.c.orig	2021-06-18 19:08:07.000000000 +0900
++++ lib/dns/zone.c	2021-07-19 08:46:21.613595923 +0900
+@@ -241,8 +241,13 @@ struct dns_zone {
  	int32_t journalsize;
  	dns_rdataclass_t rdclass;
  	dns_zonetype_t type;
-+#ifdef __NetBSD__
++#if 1
 +	atomic_uint_fast32_t flags;
 +	atomic_uint_fast32_t options;
 +#else
@@ -18,11 +18,11 @@ $NetBSD: patch-lib_dns_zone.c,v 1.2 2021/03/21 04:16:17 taca Exp $
  	unsigned int db_argc;
  	char **db_argv;
  	isc_time_t expiretime;
-@@ -370,7 +375,11 @@ struct dns_zone {
+@@ -387,7 +392,11 @@ struct dns_zone {
  	/*%
  	 * Autosigning/key-maintenance options
  	 */
-+#ifdef __NetBSD__
++#if 1
 +	atomic_uint_fast32_t keyopts;
 +#else
  	atomic_uint_fast64_t keyopts;
@@ -30,3 +30,13 @@ $NetBSD: patch-lib_dns_zone.c,v 1.2 2021/03/21 04:16:17 taca Exp $
  
  	/*%
  	 * True if added by "rndc addzone"
+@@ -524,7 +533,9 @@ typedef enum {
+ 	 * support 64-bit enum values. Currently they fail on
+ 	 * Windows.
+ 	 */
++#if 1
+ 	DNS_ZONEFLG___MAX = UINT64_MAX, /* trick to make the ENUM 64-bit wide */
++#endif
+ } dns_zoneflg_t;
+ 
+ #define DNS_ZONE_OPTION(z, o)	 ((atomic_load_relaxed(&(z)->options) & (o)) != 0)
