9d000fffde80cc7131cb71b6fc6a9703b0c1b35e
yhardy <yhardy@pkgsrc.org>
2021-07-21 12:29:44 +0000

    games/endless-sky: update to endless-sky-0.9.14
    Pkgsrc changes:
    
    * Update the makefile to handle subdirectories in the cource code.
    * Update the ImageBuffer patch to load images correctly.
    
    
    Announcement:
    
    0.9.14 Polish release
    
    This is a "stable" release, focusing on bug fixes and polishing
    the recent v0.9.13 release. New changes include the following
    
    * Numerous typo fixes
    * Fixes to how several missions are offered to the player
    * Better suggestions of possibly incomplete conditions in some
      missions' "visit" dialogues
    * Differentiation between two Remnant engines' reversing capabilities
    * Certain Navy shipyards now stock the Dropship
    * Ships that have no specified "drag" attribute default to 100,
      rather than causing crashes
    * Weaponry that alters the ship's status conditions (e.g. ionization)
      now respects the required minimum value of 0
    * The "interrupt fast-forward" preference defaults to off
    
    Some minor improvements were added to help support plugin authors
    and content creators:
    
    * Quarg planets now have regional attributes to enable better
      targeting via missions
    * Missions that fail to be instantiated print more informative
      messages in the error file
    * Large numbers of missing image frames no longer pollute the error
      file
    
    
    0.9.13: Unstable release
    
    This is an unstable release, containing big changes that may
    introduce new bugs.
    
    Breaking changes:
    
    * Starting conditions no longer automatically override the game's
      default start. Plugins that use a "start" node will need to make
      changes to restore the previous behavior.
    
    Major new changes include the following:
    
    * 70+ new missions in human and alien space
    * Outfits can now be stored on planets, without needing to install
      them on a parked ship
    * An overhaul of almost all human outfit images
    * Players can now choose their starting scenario
    * Spaceport news messages no longer include portraits by default
    * Better handling for missing content definitions (e.g. from plugin
      removal)
    
    Several features have been added to the game engine, for use by
    plugins and other content creators. These include:
    
    * Dynamic stopover selection will consider planets without spaceports
    * A number of new outfit and weapon attributes, offering shield/hull
      multipliers, weapon aiming & firing overrides, percentage-based
      damage, and additional damage mitigation
    * Arbitrary angles for hardpoints, engine flares, and fighter bays
    * NPCs' objectives can now include provocation or capture
    * Mission NPCs can be conditionally spawned or removed without
      needing to change the player's active missions
    * Missions can react differently when the player aborts a mission,
      and when the player fails a mission
    * Missions can give ships to the player
    * Systems may generate environmental hazards, control the range at
      which ships enter, and be hidden on the map

--- games/endless-sky/patches/patch-source_ImageBuffer.cpp
+++ games/endless-sky/patches/patch-source_ImageBuffer.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-source_ImageBuffer.cpp,v 1.1 2020/07/26 05:21:20 dholland Exp $
+$NetBSD: patch-source_ImageBuffer.cpp,v 1.2 2021/07/21 12:29:44 yhardy Exp $
 
 Build with both jpeg and libjpeg-turbo.
 
---- source/ImageBuffer.cpp~	2020-05-01 19:02:38.000000000 +0000
+--- source/ImageBuffer.cpp.orig	2020-05-01 19:02:38.000000000 +0000
 +++ source/ImageBuffer.cpp
 @@ -271,7 +271,11 @@ namespace {
  		
@@ -16,3 +16,24 @@ Build with both jpeg and libjpeg-turbo.
  		
  		jpeg_start_decompress(&cinfo);
  		int width = cinfo.image_width;
+@@ -297,6 +301,20 @@ namespace {
+ 		jpeg_finish_decompress(&cinfo);
+ 		jpeg_destroy_decompress(&cinfo);
+ 		
++#ifndef JCS_EXTENSIONS
++		height = buffer.Height();
++		for(int y = 0; y < height; ++y)
++		{
++			for(int x = width-1; x >= 0; --x)
++			{
++				rows[y][4*x+3] = 255;
++				rows[y][4*x+2] = rows[y][3*x];
++				rows[y][4*x+1] = rows[y][3*x+1];
++				rows[y][4*x]   = rows[y][3*x+2];
++			}
++		}
++#endif
++
+ 		return true;
+ 	}
+ 	
