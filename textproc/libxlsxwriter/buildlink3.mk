97a2154e51f4851399d49c7a2f6da90051b7b028
sjmulder <sjmulder@pkgsrc.org>
2021-07-26 01:02:12 +0000

    libxlsxwriter: Update to 1.1.1
    Packaging changes:
     - Now uses pkgsrc or system libraries instead of bundled (minizip, md5).
    
    Upstream changes:
    
    1.1.1
    - Added optional third party library to handle sprintf handling of
      doubles. This is to avoid issues with number formatting in some locales. The
      optional library is the Milo Yip DTOA implementation.
    - Added the LXW_EXPLICIT_FALSE variable to allow the default bold property in
      chart title fonts to be turned off.
    
    1.1.0
    - Fix for Cocoapod issue where local md5 files conflicted with BoringSSL headers.
    
    1.0.9
    - Added support for Excel 365 dynamic arrays. See
      worksheet_write_dynamic_array_formula() worksheet_write_dynamic_formula()
      and ww_formulas_dynamic_arrays.
    
    1.0.8
    - Fix for dynamic library soname on Linux.
    
    1.0.7
    - Added support for writing Unix date/times via the
      worksheet_write_unixtime() function.
    - Added support for dynamic library soname version to help packagers and build
      systems differentiate ABI versions.
    
    1.0.6
    - Added support for using OpenSSL MD5 functions instead of built in third
      party library. See gsg_md5.
    
    1.0.5
    - Added support for worksheet background images via
      worksheet_set_background().
    
    1.0.4
    - Added support for GIF image files (and in Excel 365, animated GIF files).
    
    1.0.3
    - Added some fixes to make the library compile more cleanly as an R library.
    
    1.0.2
    - Added option to set row heights and column widths in pixels via the
      worksheet_set_row_pixels() and worksheet_set_column_pixels() functions.
    
    1.0.1 March 30 2021
    - Added support for pkg-config to Make/Cmake installs. See the
      gsg_using section of the Getting Started guide.
    - Added ability to add accessibility options "description" and "decorative" to
      images via worksheet_insert_image_opt() and lxw_image_options.
    - Added the workbook_read_only_recommended() function to set the Excel
      "Read-only Recommended" option that is available when saving a file.
    - Fixed issue where pattern formats without colors were given a default black
      fill color.
    - Added option to set a chart crossing to 'min' via
      chart_axis_set_crossing_min() as well as the existing 'max' option. The
      'min' option isn't available in the Excel interface but can be enabled via
      VBA.
    - Added some additional information on using constant_memory mode with memory
      mounted /tmp directories. See ww_mem_temp.
    - Added build option to compile libxlsxwriter as a "universal binary" for both
      Apple silicon and Intel-based Macs, i.e., arm64 and x86_64. See
      gsg_universal.
    - Fixed issue where the limit for header/footer strings was
      255 bytes instead of 255 characters and as a result UTF8
      strings were being truncated.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,14 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/10/01 21:10:41 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2021/07/26 01:02:12 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.0.0
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.1
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
+.include "../../archivers/minizip/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+
 .endif	# LIBXLSXWRITER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libxlsxwriter
