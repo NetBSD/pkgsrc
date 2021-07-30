fe93697a0d6974adcb06e7d424ef58ecd9eef516
ryoon <ryoon@pkgsrc.org>
2021-07-30 12:26:37 +0000

    *: Recursive revbump from audio/pulseaudio-15.0

--- devel/libkomparediff2/buildlink3.mk
+++ devel/libkomparediff2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2021/04/21 11:40:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2021/07/30 12:26:46 ryoon Exp $
 
 BUILDLINK_TREE+=	libkomparediff2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkomparediff2
 LIBKOMPAREDIFF2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkomparediff2+=	libkomparediff2>=19.08.3
-BUILDLINK_ABI_DEPENDS.libkomparediff2?=	libkomparediff2>=20.12.3nb1
+BUILDLINK_ABI_DEPENDS.libkomparediff2?=	libkomparediff2>=20.12.3nb2
 BUILDLINK_PKGSRCDIR.libkomparediff2?=	../../devel/libkomparediff2
 
 .include "../../devel/kio/buildlink3.mk"
--- devel/purpose/buildlink3.mk
+++ devel/purpose/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/04/21 11:40:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/07/30 12:26:46 ryoon Exp $
 
 BUILDLINK_TREE+=	purpose
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	purpose
 PURPOSE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.purpose+=	purpose>=5.70.0
-BUILDLINK_ABI_DEPENDS.purpose?=	purpose>=5.80.0nb1
+BUILDLINK_ABI_DEPENDS.purpose?=	purpose>=5.80.0nb2
 BUILDLINK_PKGSRCDIR.purpose?=	../../devel/purpose
 
 .include "../../devel/kio/buildlink3.mk"
