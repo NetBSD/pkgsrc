9924f01e968cacebbf60526a39f1c9e2bea7332a
ryoon <ryoon@pkgsrc.org>
2021-07-30 12:21:06 +0000

    pulseaudio: Update to 15.0
    * Convert to use meson to build.
    
    Changelog:
     1. Notes for end users
         1. Support for LDAC and AptX bluetooth codecs, plus "SBC XQ" (SBC with
            higher-quality parameters)
         2. Support for HFP bluetooth profiles
         3. Support for Bluetooth A2DP AVRCP Absolute Volume
         4. ALSA path configuration files can now be placed in user home directory
         5. module-virtual-surround-sink rewritten
         6. More options for module-jackdbus-detect
         7. Improved hardware support
             1. SteelSeries Arctis 9
             2. HP Thunderbolt Dock 120W G2
             3. Behringer U-Phoria UMC22
             4. OnePlus Type-C Bullets
             5. Sennheiser GSX 1000/1200 PRO
         8. New udev variable: PULSE_MODARGS
         9. max_latency_msec argument added to module-null-source
        10. module-filter-apply can take filter parameters from device properties
        11. module-match can now be loaded multiple times
        12. Improvements to FreeBSD support
        13. Windows support added to Meson
        14. Additional commands for pactl
        15. Card profiles can be set to sticky
     2. Notes for application developers
         1. New API for sending messages from clients to PulseAudio objects
         2. New mechanism for applications to disable shared memory on their
            connection to PulseAudio
     3. Notes for packagers
         1. Autotools build system have been dropped
         2. The startup script can now read additional configuration from the /etc/
            pulse/default.pa.d/ directory
         3. Option to build client library and utilities only
         4. Avoid loading X11 modules on Wayland (GNOME-only for now)
         5. OSS support is now configurable in Meson
         6. Valgrind support is now configurable in Meson

--- audio/pulseaudio/patches/patch-src_modules_module-detect.c
+++ audio/pulseaudio/patches/patch-src_modules_module-detect.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_modules_module-detect.c,v 1.7 2021/02/20 22:10:39 ryoon Exp $
+$NetBSD: patch-src_modules_module-detect.c,v 1.8 2021/07/30 12:21:06 ryoon Exp $
 
 Hack to set proper sound device on NetBSD.
 
---- src/modules/module-detect.c.orig	2019-07-05 00:06:47.000000000 +0000
+--- src/modules/module-detect.c.orig	2021-07-27 20:02:27.767868500 +0000
 +++ src/modules/module-detect.c
-@@ -121,7 +121,27 @@ static int detect_alsa(pa_core *c, int j
+@@ -121,7 +121,28 @@ static int detect_alsa(pa_core *c, int j
  static int detect_oss(pa_core *c, int just_one) {
      FILE *f;
      int n = 0, b = 0;
@@ -20,19 +20,20 @@ Hack to set proper sound device on NetBSD.
 +    pa_snprintf(args, sizeof(args), "mmap=0 device=@DEVOSSAUDIO@");
 +    if (pa_module_load(&m, c, "module-oss", args) < 0)
 +      return 0;
++    n = 1;
 +
 +    for (int i = 0; i < 4; i++) {
 +      pa_snprintf(args, sizeof(args), "mmap=0 device=@DEVOSSAUDIO@%d", i);
 +      if (pa_module_load(&m, c, "module-oss", args) < 0)
-+        return i + 1;
++        return n;
++      n++;
 +    }
 +
-+    n = 5;
 +#else /* __linux__ or __FreeBSD__ */
      if (!(f = pa_fopen_cloexec("/dev/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/asound/oss/sndstat", "r"))) {
-@@ -135,7 +155,6 @@ static int detect_oss(pa_core *c, int ju
+@@ -135,7 +156,6 @@ static int detect_oss(pa_core *c, int ju
      while (!feof(f)) {
          char line[256], args[64];
          unsigned device;
@@ -40,7 +41,7 @@ Hack to set proper sound device on NetBSD.
  
          if (!fgets(line, sizeof(line), f))
              break;
-@@ -174,6 +193,7 @@ static int detect_oss(pa_core *c, int ju
+@@ -203,6 +223,7 @@ static int detect_oss(pa_core *c, int ju
      }
  
      fclose(f);
