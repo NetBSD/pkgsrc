43bb74008206d6d42521f714533e428974b9557a
schmonz <schmonz@pkgsrc.org>
2021-08-04 13:32:07 +0000

    Update to 18. From the changelog:
    - Due to problems binding with IPv4-mapped IPv6 addresses for a DNS
      lookup, splitted up randombind into randombind4 and randombind6.
      socket operations on IPv4 use IPv4 address only. (tx. Kouichi).

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2021/04/03 19:33:28 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2021/08/04 13:32:07 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
 .if !defined(FEHQLIBS_BUILDLINK3_MK)
 FEHQLIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fehqlibs+=	fehqlibs>=17
+BUILDLINK_API_DEPENDS.fehqlibs+=	fehqlibs>=18
 BUILDLINK_PKGSRCDIR.fehqlibs?=		../../net/fehqlibs
 BUILDLINK_INCDIRS.fehqlibs+=		include/qlibs
 BUILDLINK_LIBDIRS.fehqlibs+=		lib/qlibs
