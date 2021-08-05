fbc0596857a4cc2b9bdfbc84a2dedb4e982d032e
adam <adam@pkgsrc.org>
2021-08-05 13:22:30 +0000

    py-uwsgi: updated to 2.0.19.1
    uWSGI 2.0.19.1
    
    Changes
    
    Reverted CGI chunked encoding support
    Fixed bug with WSGI responses returning None
    
    
    uWSGI 2.0.19
    
    Changes
    
    Update travis to xenial
    Fix segfault in logsocket plugin
    Backport Coverity fixes from master
    Fix Python 3.7 warnings
    Fix uwsgi.workers() leak in Python plugin
    Backport redislog plugin 32-bit build fixes
    Fix stack overflow in core/rpc
    Fix build with spaces in the path
    Add missing initialization for zend_file_handle in php plugin
    Build Python 3.7 and 3.8 plugins in CI
    Add Trove classifiers for Python 3.7 and 3.8
    Graceful shutdown for vassals
    Improve yaml parsing with libyaml
    Add smart-daemon2 option to notify daemon of master reloading
    Do not chroot multiple times when root
    Support io.BytesIO with wsgi.file_wrapper
    Add websocket continuation frames support
    Fix compilation with gevent 1.5.0
    Fix PSGI plugin build with gcc 10
    Get rid of paste.script dependency in pypy/python plugins
    Improve performance for santitizing file descriptors with cgi plugin
    Fix offload-threads with honour-range
    Fix logging packet size length overflow
    Fix possible deadlock in install
    Fix parsing of http port for ipv6
    Fix impossibility of determining the end of the chunked stream with psgi plugin
    Fix parsing of http-socket port for ipv6
    Add chunked request decoding to the CGI plugin
    Add add max-worker-lifetime-delta to reload workers with a delta

--- www/py-uwsgi/patches/patch-uwsgi.h
+++ www/py-uwsgi/patches/patch-uwsgi.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-uwsgi.h,v 1.7 2019/05/05 17:55:34 wiedi Exp $
+$NetBSD: patch-uwsgi.h,v 1.8 2021/08/05 13:22:30 adam Exp $
 
 Avoid clash with sys/user.h inclusion in logging.c on DragonFly BSD.
 Avoid using "sun" which breaks on SunOS
 
---- uwsgi.h.orig	2019-02-09 14:48:07.000000000 +0000
+--- uwsgi.h.orig	2020-06-17 09:03:34.000000000 +0000
 +++ uwsgi.h
 @@ -322,6 +322,10 @@ extern int pivot_root(const char *new_ro
  #elif defined(__CYGWIN__)
@@ -16,12 +16,12 @@ Avoid using "sun" which breaks on SunOS
  #include <sys/event.h>
  #endif
  
-@@ -1629,7 +1633,7 @@ struct wsgi_request {
+@@ -1631,7 +1635,7 @@ struct wsgi_request {
  	union address {
  		struct sockaddr_in sin;
  		struct sockaddr_in6 sin6;
 -		struct sockaddr_un sun;
 +		struct sockaddr_un so_sun;
  	} client_addr;
- };
  
+ 	uint8_t websocket_is_fin;
