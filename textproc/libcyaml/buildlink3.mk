f13b577055b1250697e50bf7b47896104d60df22
nia <nia@pkgsrc.org>
2021-08-06 14:10:21 +0000

    libcyaml: dependencies need libyaml to link

--- textproc/libcyaml/buildlink3.mk
+++ textproc/libcyaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/08/06 12:29:23 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/08/06 14:10:21 nia Exp $
 
 BUILDLINK_TREE+=	libcyaml
 
@@ -8,6 +8,8 @@ LIBCYAML_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libcyaml+=	libcyaml>=1.1.0
 BUILDLINK_ABI_DEPENDS.libcyaml+=	libcyaml>=1.1.0
 BUILDLINK_PKGSRCDIR.libcyaml?=		../../wip/libcyaml
+
+.include "../../textproc/libyaml/buildlink3.mk"
 .endif # LIBCYAML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libcyaml
