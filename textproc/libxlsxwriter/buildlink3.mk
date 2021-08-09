13e0fcdaeea253c968835751f86af9678b29e845
sjmulder <sjmulder@pkgsrc.org>
2021-08-09 07:05:23 +0000

    textproc/libxlsxwriter: Update to 1.1.2
    - Added support for autofilter rules.
    - Added Description/Alt Text and Decorative accessibility options for
      charts. These options were already available for images
    - Fix incorrect use of LDFLAGS when building object files.
    - Fix for not using LDFLAGS when linking tests.
    - Fix time type, Excel always needs 64-bit values so time_t is not
      always sufficient.
    - Fix minor const issue.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2021/07/26 01:02:12 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/08/09 07:05:23 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.1
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.2
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../archivers/minizip/buildlink3.mk"
