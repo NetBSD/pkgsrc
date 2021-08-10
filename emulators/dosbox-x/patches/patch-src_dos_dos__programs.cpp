37b02570ad7ad33c35d562313f56baaa76c08f9d
nia <nia@pkgsrc.org>
2021-08-10 11:03:50 +0000

    dosbox-x: update to 0.83.16
    Release notes:
    https://dosbox-x.com/release-0.83.16.html

--- emulators/dosbox-x/patches/patch-src_dos_dos__programs.cpp
+++ emulators/dosbox-x/patches/patch-src_dos_dos__programs.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_dos_dos__programs.cpp,v 1.1 2020/07/07 12:05:49 nia Exp $
+$NetBSD: patch-src_dos_dos__programs.cpp,v 1.2 2021/08/10 11:03:50 nia Exp $
 
 dos_programs.cpp: In member function 'bool AUTOTYPE::ReadDoubleArg(const string&, const char*, const double&, const double&, const double&, double&)':
 dos_programs.cpp:5556:12: error: expected unqualified-id before '(' token
    if (std::isfinite(user_value)) {
 
---- src/dos/dos_programs.cpp.orig	2020-06-30 23:09:17.000000000 +0000
+--- src/dos/dos_programs.cpp.orig	2021-08-01 07:45:55.000000000 +0000
 +++ src/dos/dos_programs.cpp
-@@ -5550,11 +5550,7 @@ bool AUTOTYPE::ReadDoubleArg(const std::
+@@ -6446,11 +6446,7 @@ bool AUTOTYPE::ReadDoubleArg(const std::
  	if (cmd->FindString(flag, str_value, true)) {
  		// Can the user's value be parsed?
  		const double user_value = to_finite<double>(str_value);
--#if defined(MACOSX)
+-#if defined(MACOSX) || defined(EMSCRIPTEN) || ((defined(ANDROID) || defined(__ANDROID__)) && defined(__clang__))
  		if (isfinite(user_value)) { /* *sigh* Really, clang, really? */
 -#else
 -		if (std::isfinite(user_value)) {
