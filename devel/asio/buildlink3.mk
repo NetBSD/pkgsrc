51af047aa477ca1d623f567d7a72b543c339b0c4
prlw1 <prlw1@pkgsrc.org>
2021-08-10 19:24:22 +0000

    update asio to 1.19.2
    Five years of updates including
    patch-include_asio_detail_impl_kqueue__reactor.ipp is upstreamed.
    
    abiword-plugins, the sole user in pkgsrc, compile tested.

--- devel/asio/buildlink3.mk
+++ devel/asio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/18 21:48:02 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/08/10 19:24:22 prlw1 Exp $
 
 BUILDLINK_TREE+=	asio
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.asio+=	asio>=1.10.8nb2
 BUILDLINK_PKGSRCDIR.asio?=	../../devel/asio
 BUILDLINK_DEPMETHOD.asio?=	build
 
-.include "../../devel/boost-headers/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif	# ASIO_BUILDLINK3_MK
 
