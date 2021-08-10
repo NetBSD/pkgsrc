cfb0c1d95712168079475388186fdee6895d175b
nia <nia@pkgsrc.org>
2021-08-10 12:03:37 +0000

    gajim: update to 1.3.2
    Release notes:
    https://gajim.org/post/2021-04-24-gajim-1.3.2-released/

--- chat/gajim/patches/patch-setup.py
+++ chat/gajim/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2018/10/05 12:56:36 nia Exp $
+$NetBSD: patch-setup.py,v 1.4 2021/08/10 12:03:37 nia Exp $
 
 Fix man page install location.
 
---- setup.py.orig	2018-10-05 12:43:13.496034076 +0000
+--- setup.py.orig	2021-04-24 11:27:40.000000000 +0000
 +++ setup.py
-@@ -113,7 +113,7 @@ def build_man(build_cmd):
+@@ -109,7 +109,7 @@ def build_man(build_cmd):
  def install_man(install_cmd):
      data_files = install_cmd.distribution.data_files
-     man_dir = os.path.join(build_dir, 'man')
+     man_dir = build_dir / 'man'
 -    target = 'share/man/man1'
 +    target = os.path.join(os.environ['PKGMANDIR'], 'man1')
  
      for man in MAN_FILES:
-         man_file_gz = os.path.join(man_dir, man + '.gz')
+         man_file_gz = str(man_dir / (man + '.gz'))
