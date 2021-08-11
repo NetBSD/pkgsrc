6945efef10be7a9a39cdd2551a3c025aa490f16f
kardel <kardel@pkgsrc.org>
2021-08-11 10:09:54 +0000

    net/frr: upgrade to frr 8.0

--- net/frr/patches/patch-zebra_kernel__socket.c
+++ net/frr/patches/patch-zebra_kernel__socket.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-zebra_kernel__socket.c,v 1.1 2020/12/10 16:43:24 kardel Exp $
+$NetBSD: patch-zebra_kernel__socket.c,v 1.2 2021/08/11 10:09:54 kardel Exp $
 
 	Pass IFP interface index if known (normal case) to make
 	interface specific routes work such as in
@@ -6,9 +6,13 @@ $NetBSD: patch-zebra_kernel__socket.c,v 1.1 2020/12/10 16:43:24 kardel Exp $
 		ipv6 route ::/0 <ipv6 address> carp0 100
 		ivv6 route ::/0 <other ipv6 address> 200
 
---- zebra/kernel_socket.c.orig	2020-06-30 11:08:57.000000000 +0000
+	frr-8 now enables SO_RERROR but exits cowardly when the
+	routing socket returns ENOBUFS. ensure at least a bit more
+	socket buffer space by increasing SO_RCVBUF.
+
+--- zebra/kernel_socket.c.orig	2021-07-21 13:06:19.000000000 +0000
 +++ zebra/kernel_socket.c
-@@ -1277,6 +1277,19 @@ int rtm_write(int message, union sockuni
+@@ -1243,6 +1243,19 @@ int rtm_write(int message, union sockuni
  #ifdef __OpenBSD__
  	SOCKADDRSET(mpls, RTA_SRC);
  #endif
@@ -28,3 +32,13 @@ $NetBSD: patch-zebra_kernel__socket.c,v 1.1 2020/12/10 16:43:24 kardel Exp $
  
  	msg.rtm.rtm_msglen = pnt - (caddr_t)&msg;
  
+@@ -1425,6 +1438,9 @@ static void routing_socket(struct zebra_
+ 		return;
+ 	}
+ 
++	setsockopt_so_recvbuf(routing_sock, 1048576);
++	setsockopt_so_recvbuf(dplane_routing_sock, 1048576);
++
+ #ifdef SO_RERROR
+ 	/* Allow reporting of route(4) buffer overflow errors */
+ 	int n = 1;
