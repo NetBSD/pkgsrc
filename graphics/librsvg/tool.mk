f2a3dfe4d5a09453f76ab1e9bd2298980a6509a1
gutteridge <gutteridge@pkgsrc.org>
2021-08-11 00:25:35 +0000

    librsvg: fix grammar and style in several comments

--- graphics/librsvg/tool.mk
+++ graphics/librsvg/tool.mk
@@ -1,7 +1,7 @@
-# $NetBSD: tool.mk,v 1.1 2019/06/08 19:51:38 leot Exp $
+# $NetBSD: tool.mk,v 1.2 2021/08/11 00:25:35 gutteridge Exp $
 #
 # This mk fragment can be included by all packages that need librsvg as a tool
-# dependency (e.g. for rsvg-convert).
+# dependency (e.g., for rsvg-convert).
 #
 
 .if !defined(LIBRSVG_TOOL_MK)
