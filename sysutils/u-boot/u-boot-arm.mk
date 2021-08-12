151331a422c6f303c5f310e6815fca3c4b88d416
wiz <wiz@pkgsrc.org>
2021-08-12 07:25:54 +0000

    u-boot*: fix some pkglint

--- sysutils/u-boot/u-boot-arm.mk
+++ sysutils/u-boot/u-boot-arm.mk
@@ -1,4 +1,4 @@
-# $NetBSD: u-boot-arm.mk,v 1.2 2018/06/06 21:58:28 wiz Exp $
+# $NetBSD: u-boot-arm.mk,v 1.3 2021/08/12 07:25:54 wiz Exp $
 
 MAINTAINER=	port-arm@NetBSD.org
 BUILD_DEPENDS+=	cross-arm-none-eabi-gcc-[0-9]*:../../cross/arm-none-eabi-gcc
@@ -6,4 +6,3 @@ BUILD_DEPENDS+=	cross-arm-none-eabi-gcc-[0-9]*:../../cross/arm-none-eabi-gcc
 UBOOT_ENV+=	CROSS_COMPILE=${PREFIX}/cross-arm-none-eabi/bin/arm-none-eabi-
 
 .include "u-boot.mk"
-.include "../../mk/bsd.pkg.mk"
