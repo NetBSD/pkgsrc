1018989a9b4603c1cca0f1ac74b4691a276dea3a
wiz <wiz@pkgsrc.org>
2021-08-12 07:30:21 +0000

    u-boot*: fix more pkglint

--- sysutils/u-boot/u-boot-mipsel.mk
+++ sysutils/u-boot/u-boot-mipsel.mk
@@ -1,4 +1,4 @@
-# $NetBSD: u-boot-mipsel.mk,v 1.1 2020/06/20 14:38:13 thorpej Exp $
+# $NetBSD: u-boot-mipsel.mk,v 1.2 2021/08/12 07:30:21 wiz Exp $
 
 MAINTAINER=	port-mips@NetBSD.org
 TOOL_DEPENDS+=	cross-mipsel-none-elf-gcc-[0-9]*:../../cross/mipsel-none-elf-gcc
@@ -6,4 +6,3 @@ TOOL_DEPENDS+=	cross-mipsel-none-elf-gcc-[0-9]*:../../cross/mipsel-none-elf-gcc
 UBOOT_ENV+=	CROSS_COMPILE=${PREFIX}/cross-mipsel-none-elf/bin/mipsel-none-elf-
 
 .include "u-boot.mk"
-.include "../../mk/bsd.pkg.mk"
