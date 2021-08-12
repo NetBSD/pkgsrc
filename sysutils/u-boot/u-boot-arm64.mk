a0602b56efcf24fa753ffacc4963ba427a07f480
wiz <wiz@pkgsrc.org>
2021-08-12 07:30:21 +0000

    u-boot*: fix more pkglint

--- sysutils/u-boot/u-boot-arm64.mk
+++ sysutils/u-boot/u-boot-arm64.mk
@@ -1,4 +1,4 @@
-# $NetBSD: u-boot-arm64.mk,v 1.3 2018/06/06 21:58:28 wiz Exp $
+# $NetBSD: u-boot-arm64.mk,v 1.4 2021/08/12 07:30:21 wiz Exp $
 
 MAINTAINER=	port-arm@NetBSD.org
 BUILD_DEPENDS+=	cross-aarch64-none-elf-gcc-[0-9]*:../../cross/aarch64-none-elf-gcc
@@ -15,5 +15,3 @@ PKG_SUPPORTED_OPTIONS=	el1
 .if !empty(PKG_OPTIONS:Mel1)
 UBOOT_OPTIONS+=	CONFIG_ARMV8_SWITCH_TO_EL1=y
 .endif
-
-.include "../../mk/bsd.pkg.mk"
