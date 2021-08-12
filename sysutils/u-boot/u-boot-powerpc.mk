10692f6fe2615c3582b001d5625c9aafc2afa6e4
wiz <wiz@pkgsrc.org>
2021-08-12 07:30:21 +0000

    u-boot*: fix more pkglint

--- sysutils/u-boot/u-boot-powerpc.mk
+++ sysutils/u-boot/u-boot-powerpc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: u-boot-powerpc.mk,v 1.1 2020/03/30 16:30:54 riastradh Exp $
+# $NetBSD: u-boot-powerpc.mk,v 1.2 2021/08/12 07:30:21 wiz Exp $
 
 MAINTAINER=	port-powerpc@NetBSD.org
 TOOL_DEPENDS+=	cross-powerpc-none-elf-gcc-[0-9]*:../../cross/powerpc-none-elf-gcc
@@ -6,4 +6,3 @@ TOOL_DEPENDS+=	cross-powerpc-none-elf-gcc-[0-9]*:../../cross/powerpc-none-elf-gc
 UBOOT_ENV+=	CROSS_COMPILE=${PREFIX}/cross-powerpc-none-elf/bin/powerpc-none-elf-
 
 .include "u-boot.mk"
-.include "../../mk/bsd.pkg.mk"
