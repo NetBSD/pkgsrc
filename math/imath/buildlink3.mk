e7694f180bb0d7cd34e3d6e45aaa22c3b45bcba2
wiz <wiz@pkgsrc.org>
2021-08-15 14:14:43 +0000

    imath: update to 3.1.2.
    ## Version 3.1.2 (July 31, 2021)
    
    Patch release that fixes a Windows header issue.
    
    * Improve handling of ``#include <*intrin.h>``
    
    ## Version 3.1.1 (July 20, 2021)
    
    Patch release that fixes a build failure on ARM64 macOS
    
    ## Version 3.1.0 (July 13, 2021)
    
    Minor release with new features:
    
    * Optimized half-to-float and float-to-half conversion, using F16C SSE
      instruction set if available. Non-SSE conversion eliminates the
      float-to-half exponent lookup table, and half-to-float conversion
      provides a compile-time-optional bit shifting that is slower but
      eliminates the need for the lookup table, for applications where
      memory is limited.
    
      Half-to-float and float-to-half conversion is also available as
      C-language functions ``imath_half_to_float()`` and
      ``imath_float_to_half()``.
    
      All new conversions produced identical results, and new options are
      off by default to ensure backwards compatibility. See
      https://imath.readthedocs.io for more info.
    
    * NOEXCEPT specifier can be eliminated at compile-time via the
      ``IMATH_USE_NOEXCEPT`` CMake option.
    
    * Python bindings:
      * FixedArray objects support a "read only" state.
      * FixedArray objects support python buffer protocol.
    
    * Optimized 4x4 matrix multiplication.

--- math/imath/buildlink3.mk
+++ math/imath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/07/08 21:09:04 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/08/15 14:14:43 wiz Exp $
 
 BUILDLINK_TREE+=	imath
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	imath
 IMATH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.imath+=	imath>=3.0.4
+BUILDLINK_ABI_DEPENDS.imath+=	imath>=3.1
 BUILDLINK_PKGSRCDIR.imath?=	../../math/imath
 .endif	# IMATH_BUILDLINK3_MK
 
