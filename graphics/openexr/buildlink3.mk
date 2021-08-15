52a40349b1b773ca899478af609d7202b819d396
wiz <wiz@pkgsrc.org>
2021-08-15 14:15:03 +0000

    openexr: update to 3.1.1.
    ## Version 3.1.1 (August 2, 2021)
    
    Patch release that fixes build failures on various systems, introduces
    CMake ``CMAKE_CROSSCOMPILING_EMULATOR`` support, and fixes a few other
    minor issues.
    
    ## Version 3.1.0 (July 22, 2021)
    
    The 3.1 release of OpenEXR introduces a new library, OpenEXRCore,
    which is the result of a significant re-thinking of how OpenEXR
    manages file I/O and provides access to image data. It begins to
    address long-standing scalability issues with multithreaded image
    reading and writing.
    
    The OpenEXRCore library provides thread-safe, non-blocking access to
    files, which was not possible with the current API, where the
    framebuffer management is separate from read requests. It is written
    entirely in C and provides a new C-language API alongside the existing
    C++ API. This new low-level API allows applications to do custom
    unpacking of EXR data, such as on the GPU, while still benefiting from
    efficient I/O, file validation, and other semantics. It provides
    efficient direct access to EXR files in texturing applications. This C
    library also introduces an easier path to implementing OpenEXR
    bindings in other languages, such as Rust.
    
    The 3.1 release represents a technology preview for upcoming
    releases. The initial release is incremental; the existing API and
    underlying behavior has not changed. The new API is available now for
    performance validation testing, and then in future OpenEXR releases,
    the C++ API will migrate to use the new core in stages.  It is not the
    intention to entirely deprecate the C++ API, nor must all applications
    re-implement EXR I/O in terms of the C library. The C API does not,
    and will not, provide the rich set of utility classes that exist in
    the C++ layer. The 3.1 release of the OpenEXRCore library simply
    offers new functionality for specialty applications seeking the
    highest possible performance. In the future, the ABI will evolve, but
    the API will remain consistent, or only have additions.

--- graphics/openexr/buildlink3.mk
+++ graphics/openexr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2021/07/08 21:13:06 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2021/08/15 14:15:03 wiz Exp $
 
 BUILDLINK_TREE+=	openexr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openexr
 OPENEXR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openexr+=	openexr>=3.0.5
-BUILDLINK_ABI_DEPENDS.openexr+=	openexr>=3.0.5
+BUILDLINK_ABI_DEPENDS.openexr+=	openexr>=3.1
 BUILDLINK_PKGSRCDIR.openexr?=	../../graphics/openexr
 
 PTHREAD_OPTS+=	require
