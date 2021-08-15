e7cf6ce6bef80f6943f04bc666b50a1b3d07363e
wiz <wiz@pkgsrc.org>
2021-08-15 14:23:57 +0000

    *: recursive bump for openexr 3.1

--- graphics/opencv-contrib-face/buildlink3.mk
+++ graphics/opencv-contrib-face/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/05/21 20:53:37 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/08/15 14:23:58 wiz Exp $
 
 BUILDLINK_TREE+=	opencv-contrib-face
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	opencv-contrib-face
 OPENCV_CONTRIB_FACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencv-contrib-face+=	opencv-contrib-face>=3.0.0
-BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.9nb1
+BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.9nb2
 BUILDLINK_PKGSRCDIR.opencv-contrib-face?=	../../graphics/opencv-contrib-face
 
 .include "../../graphics/opencv/buildlink3.mk"
