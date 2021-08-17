67be13bcfb677e38e5c19c79594e9ed045bc0ba9
wiz <wiz@pkgsrc.org>
2021-08-17 08:02:50 +0000

    qr-code-generator: bump API depends to 1.7, not fully backwards compatible

--- graphics/qr-code-generator/buildlink3.mk
+++ graphics/qr-code-generator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/02/05 13:17:21 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/08/17 08:02:50 wiz Exp $
 
 BUILDLINK_TREE+=	qr-code-generator
 
@@ -7,7 +7,7 @@ QR_CODE_GENERATOR_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.qr-code-generator?=	build
 
-BUILDLINK_API_DEPENDS.qr-code-generator+=	qr-code-generator>=1.4.0
+BUILDLINK_API_DEPENDS.qr-code-generator+=	qr-code-generator>=1.7.0
 BUILDLINK_PKGSRCDIR.qr-code-generator?=		../../graphics/qr-code-generator
 .endif	# QR_CODE_GENERATOR_BUILDLINK3_MK
 
