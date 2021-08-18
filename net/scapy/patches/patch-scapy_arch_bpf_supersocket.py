bac6f8f6147b6a74d570d44659ec71f9d666d7d6
gutteridge <gutteridge@pkgsrc.org>
2021-08-18 01:44:21 +0000

    scapy: add a patch to fix lo and utun use on Darwin and FreeBSD
    Tested on NetBSD to see if it made a useful difference for an example
    in PR pkg/55446; it does not, but it was more revealing than before.
    Adding in case it's of use for pkgsrc users on those other OSes. (I've
    also added the corresponding test case, in case someone else is curious
    about how this is applied.)

--- net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
+++ net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
@@ -1,7 +1,11 @@
-$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.4 2021/04/27 21:53:11 gutteridge Exp $
+$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.5 2021/08/18 01:44:21 gutteridge Exp $
 
 Add DragonFly support.
+
 Fix NetBSD 32-bit architecture alignment.
+
+Fix send() on loopback and utun devices for Darwin and FreeBSD.
+https://github.com/secdev/scapy/pull/3257
 
 --- scapy/arch/bpf/supersocket.py.orig	2021-04-18 18:36:15.000000000 +0000
 +++ scapy/arch/bpf/supersocket.py
@@ -37,3 +41,43 @@ Fix NetBSD 32-bit architecture alignment.
              bh_tstamp_offset = 16
          else:
              # struct bpf_hdr
+@@ -390,14 +393,31 @@ class L3bpfSocket(L2bpfSocket):
+             self.assigned_interface = iff
+ 
+         # Build the frame
+-        if self.guessed_cls == Loopback:
+-            # bpf(4) man page (from macOS, but also for BSD):
+-            # "A packet can be sent out on the network by writing to a bpf
+-            # file descriptor. [...] Currently only writes to Ethernets and
+-            # SLIP links are supported"
+-            #
+-            # Headers are only mentioned for reads, not writes. tuntaposx's tun
+-            # device reports as a "loopback" device, but it does IP.
++        #
++        # LINKTYPE_NULL / DLT_NULL (Loopback) is a special case. From the
++        # bpf(4) man page (from macOS/Darwin, but also for BSD):
++        #
++        # "A packet can be sent out on the network by writing to a bpf file
++        # descriptor. [...] Currently only writes to Ethernets and SLIP links
++        # are supported."
++        #
++        # Headers are only mentioned for reads, not writes, and it has the
++        # name "NULL" and id=0.
++        #
++        # The _correct_ behaviour appears to be that one should add a BSD
++        # Loopback header to every sent packet. This is needed by FreeBSD's
++        # if_lo, and Darwin's if_lo & if_utun.
++        #
++        # tuntaposx appears to have interpreted "NULL" as "no headers".
++        # Thankfully its interfaces have a different name (tunX) to Darwin's
++        # if_utun interfaces (utunX).
++        #
++        # There might be other drivers which make the same mistake as
++        # tuntaposx, but these are typically provided with VPN software, and
++        # Apple are breaking these kexts in a future version of macOS... so
++        # the problem will eventually go away. They already don't work on Macs
++        # with Apple Silicon (M1).
++        if DARWIN and iff.startswith('tun') and self.guessed_cls == Loopback:
+             frame = raw(pkt)
+         else:
+             frame = raw(self.guessed_cls() / pkt)
