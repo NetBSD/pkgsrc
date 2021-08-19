1db6a1f28f5256fc043b4b3d3994f07038ee8a20
wiz <wiz@pkgsrc.org>
2021-08-19 07:42:23 +0000

    u-boot*: allow packages overriding the patches/distinfo
    Even after setting a particular version to use.

--- sysutils/u-boot/u-boot-version.mk
+++ sysutils/u-boot/u-boot-version.mk
@@ -1,12 +1,12 @@
-# $NetBSD: u-boot-version.mk,v 1.4 2021/03/09 16:49:12 martin Exp $
+# $NetBSD: u-boot-version.mk,v 1.5 2021/08/19 07:42:23 wiz Exp $
 
 UBOOT_DEFAULT_VERSION=	2018.11
 
 UBOOT_VERSION?=	${UBOOT_DEFAULT_VERSION}
 
 .if ${UBOOT_VERSION} != ${UBOOT_DEFAULT_VERSION}
-DISTINFO_FILE=	${.CURDIR}/../../sysutils/u-boot/distinfo-${UBOOT_VERSION}
-PATCHDIR=	${.CURDIR}/../../sysutils/u-boot/patches-${UBOOT_VERSION}
+DISTINFO_FILE?=	${.CURDIR}/../../sysutils/u-boot/distinfo-${UBOOT_VERSION}
+PATCHDIR?=	${.CURDIR}/../../sysutils/u-boot/patches-${UBOOT_VERSION}
 .else
 PATCHDIR?=	${.CURDIR}/../../sysutils/u-boot/patches
 .endif
