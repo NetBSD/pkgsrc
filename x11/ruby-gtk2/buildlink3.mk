eb357915603e3cac6ae0cc94d4093a2c014b8af0
tsutsui <tsutsui@pkgsrc.org>
2021-08-22 17:49:19 +0000

    ruby-gtk2: fix comment per pkglint. (missed in 3.4.3 updates)

--- x11/ruby-gtk2/buildlink3.mk
+++ x11/ruby-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/08/17 20:18:01 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/08/22 17:49:19 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gtk2
 
@@ -11,6 +11,6 @@ BUILDLINK_PKGSRCDIR.ruby-gtk2?=		../../x11/ruby-gtk2
 
 .include "../../devel/ruby-pango/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.endif # RUBY_GNOME2_GTK_BUILDLINK3_MK
+.endif # RUBY_GTK2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ruby-gtk2
