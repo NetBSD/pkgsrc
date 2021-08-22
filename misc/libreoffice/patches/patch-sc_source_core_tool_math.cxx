1a0c8480043bbd1810241acdb6f78d0fd45cadb2
ryoon <ryoon@pkgsrc.org>
2021-08-22 02:41:50 +0000

    libreoffice: Update to 7.2.0.4
    Changelog:
    GENERAL
         Popup list to search for menu commands
         Scrollable style picker in the NotebookBar
         Fontwork panel in the Sidebar
         New list view for the templates dialog
         Built-in "Xray"-like UNO object inspector
    WRITER
         Background fills can cover whole pages, beyond margins
         Page styles can now have a gutter margin
         Mail merge shows a warning about nonexistent data sources
         RDF metadata in Style Inspector
         Custom color metadata field shadings
    CALC
         Calc now can filter by color in AutoFilter
         HTML tables listed in the External Data dialogue now show captions
         "Fat cross" cursor available in the options
         Type can be selected in "Moving Average" trend lines
    IMPRESS & DRAW
    
     New templates: Candy, Freshes, Grey Elegant, Growing Liberty, Yellow Idea
    
         Multiple columns now available in text boxes
         Direct access to the scaling factor via the status bar

--- misc/libreoffice/patches/patch-sc_source_core_tool_math.cxx
+++ misc/libreoffice/patches/patch-sc_source_core_tool_math.cxx
@@ -1,8 +1,8 @@
-$NetBSD: patch-sc_source_core_tool_math.cxx,v 1.3 2021/02/06 06:47:24 ryoon Exp $
+$NetBSD: patch-sc_source_core_tool_math.cxx,v 1.4 2021/08/22 02:41:50 ryoon Exp $
 
 NetBSD has no math_errhandling support yet.
 
---- sc/source/core/tool/math.cxx.orig	2021-01-27 19:33:49.000000000 +0000
+--- sc/source/core/tool/math.cxx.orig	2021-08-16 19:56:28.000000000 +0000
 +++ sc/source/core/tool/math.cxx
 @@ -20,7 +20,9 @@ static double err_pow(const double& fVal
  {
@@ -14,14 +14,17 @@ NetBSD has no math_errhandling support yet.
      std::feclearexcept(FE_ALL_EXCEPT);
      errno = 0;
      return pow(fVal1, fVal2);
-@@ -52,10 +54,14 @@ double power(const double& fVal1, const 
+@@ -52,6 +54,7 @@ double power(const double& fVal1, const 
          fPow = err_pow(fVal1, fVal2);
      }
      // The pow() call must had been the most recent call to check errno or exception.
 +#if !defined(__NetBSD__)
      if ((((math_errhandling & MATH_ERRNO) != 0) && (errno == EDOM || errno == ERANGE))
-         || (((math_errhandling & MATH_ERREXCEPT) != 0)
+ // emscripten is currently broken by https://github.com/emscripten-core/emscripten/pull/11087
+ // While the removal is correct for C99, it's not for C++11 (see http://www.cplusplus.com/reference/cfenv/FE_INEXACT/)
+@@ -61,6 +64,9 @@ double power(const double& fVal1, const 
              && std::fetestexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW))
+ #endif
          || !std::isfinite(fPow))
 +#else /* NetBSD */
 +    if (!std::isfinite(fPow))
