0bc61cc321cd727336de32b6220b2961d85b14c7
ryoon <ryoon@pkgsrc.org>
2021-08-22 02:41:50 +0000

    libreoffice: Update to 7.2.0.4
    Changelog:
    GENERAL
         Popup list to search for menu commands
         Scrollable style picker in the NotebookBar
         Fontwork panel in the Sidebar
         New list view for the templates dialog
         Built-in "Xray"-like UNO object inspector
    WRITER
         Background fills can cover whole pages, beyond margins
         Page styles can now have a gutter margin
         Mail merge shows a warning about nonexistent data sources
         RDF metadata in Style Inspector
         Custom color metadata field shadings
    CALC
         Calc now can filter by color in AutoFilter
         HTML tables listed in the External Data dialogue now show captions
         "Fat cross" cursor available in the options
         Type can be selected in "Moving Average" trend lines
    IMPRESS & DRAW
    
     New templates: Candy, Freshes, Grey Elegant, Growing Liberty, Yellow Idea
    
         Multiple columns now available in text boxes
         Direct access to the scaling factor via the status bar

--- misc/libreoffice/patches/patch-external_pdfium_UnpackedTarball__pdfium.mk
+++ misc/libreoffice/patches/patch-external_pdfium_UnpackedTarball__pdfium.mk
@@ -1,13 +1,14 @@
-$NetBSD: patch-external_pdfium_UnpackedTarball__pdfium.mk,v 1.5 2021/02/06 06:47:24 ryoon Exp $
+$NetBSD: patch-external_pdfium_UnpackedTarball__pdfium.mk,v 1.6 2021/08/22 02:41:50 ryoon Exp $
 
 * Support pkgsrc and NetBSD
 
---- external/pdfium/UnpackedTarball_pdfium.mk.orig	2021-01-27 19:33:49.000000000 +0000
+--- external/pdfium/UnpackedTarball_pdfium.mk.orig	2021-08-16 19:56:28.000000000 +0000
 +++ external/pdfium/UnpackedTarball_pdfium.mk
-@@ -18,6 +18,7 @@ pdfium_patches += AnnotationInkAndVertic
- pdfium_patches += AnnotationBorderProperties.patch.1
- pdfium_patches += AnnotationLineStartAndEnd.patch.1
- pdfium_patches += SignatureGetDocMDPPermission.patch.1
+@@ -18,6 +18,8 @@ pdfium_patches += c++20-comparison.patch
+ pdfium_patches += cg-instead-of-carbon.patch.1
+ # Android NDK 19 - that is known to work well - does not have 2 defines
+ pdfium_patches += AndroidNDK19.patch.1
++# NetBSD support
 +pdfium_patches += pkgsrc.patch.1
  
  # Work around <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=94141> "c++20 rewritten operator==
--- misc/libreoffice/patches/patch-sw_source_core_bastyp_calc.cxx
+++ misc/libreoffice/patches/patch-sw_source_core_bastyp_calc.cxx
@@ -1,15 +1,15 @@
-$NetBSD: patch-sw_source_core_bastyp_calc.cxx,v 1.1 2021/02/06 06:47:24 ryoon Exp $
+$NetBSD: patch-sw_source_core_bastyp_calc.cxx,v 1.2 2021/08/22 02:41:50 ryoon Exp $
 
 For NetBSD at least.
 
---- sw/source/core/bastyp/calc.cxx.orig	2021-01-27 19:33:49.000000000 +0000
+--- sw/source/core/bastyp/calc.cxx.orig	2021-08-16 19:56:28.000000000 +0000
 +++ sw/source/core/bastyp/calc.cxx
-@@ -1080,7 +1080,7 @@ SwSbxValue SwCalc::PrimFunc(bool &rChkPo
-             break;
+@@ -1070,7 +1070,7 @@ SwSbxValue SwCalc::PrimFunc(bool &rChkPo
+             return StdFunc(&acos, true);
          case CALC_ABS:
              SAL_INFO("sw.calc", "abs");
 -            return StdFunc(&abs, false);
 +            return StdFunc(&fabs, false);
-             break;
          case CALC_SIGN:
          {
+             SAL_INFO("sw.calc", "sign");
