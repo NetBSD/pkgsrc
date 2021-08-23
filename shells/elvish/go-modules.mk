b342bbf036e3110ee5110dc78563d8423d5b2f0b
bsiegert <bsiegert@pkgsrc.org>
2021-08-23 13:15:06 +0000

    elvish: update to 0.16.1.
    - go 1.17 fixes

--- shells/elvish/go-modules.mk
+++ shells/elvish/go-modules.mk
@@ -1,13 +1,9 @@
-# $NetBSD: go-modules.mk,v 1.1 2021/07/14 20:51:27 pin Exp $
+# $NetBSD: go-modules.mk,v 1.2 2021/08/23 13:15:06 bsiegert Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.zip
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.11.mod
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.11.zip
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.12.mod
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.12.zip
-GO_MODULE_FILES+=	github.com/xiaq/persistent/@v/v0.0.0-20200820214153-3175cfb92e14.mod
-GO_MODULE_FILES+=	github.com/xiaq/persistent/@v/v0.0.0-20200820214153-3175cfb92e14.zip
 GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.5.mod
 GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.5.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200116001909-b77594299b42.mod
