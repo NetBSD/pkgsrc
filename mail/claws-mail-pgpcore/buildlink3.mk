4d1d5cd47dd2e6ea1aa10eac6f0daed1a2a88f44
nia <nia@pkgsrc.org>
2021-08-24 13:38:18 +0000

    claws-mail-pgpcore: Remove gtk2 reference

--- mail/claws-mail-pgpcore/buildlink3.mk
+++ mail/claws-mail-pgpcore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.39 2020/08/17 20:17:34 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2021/08/24 13:38:18 nia Exp $
 
 BUILDLINK_TREE+=	claws-mail-pgpcore
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.7.0
 BUILDLINK_ABI_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.17.5nb3
 BUILDLINK_PKGSRCDIR.claws-mail-pgpcore?=	../../mail/claws-mail-pgpcore
 
-.include "../../x11/gtk2/buildlink3.mk"
 .include "../../security/gpgme/buildlink3.mk"
 .endif # CLAWS_MAIL_PGPCORE_BUILDLINK3_MK
 
