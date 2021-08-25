fd0cbe9c4dc68a16cc49af172d1ebebf0878805d
adam <adam@pkgsrc.org>
2021-08-25 09:01:07 +0000

    py-curl: updated to 7.44.1
    PycURL 7.44.1 - 2021-08-15
    --------------------------
    This release repairs incorrect Python thread initialization logic which
    caused operations to hang.
    
    PycURL 7.44.0 - 2021-08-08
    --------------------------
    This release reinstates best effort Python 2 support, adds Python 3.9 and
    Python 3.10 alpha support and implements support for several libcurl options.

--- www/py-curl/patches/patch-setup.py
+++ www/py-curl/patches/patch-setup.py
@@ -1,25 +1,23 @@
-$NetBSD: patch-setup.py,v 1.2 2018/08/20 07:31:51 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2021/08/25 09:01:07 adam Exp $
 
 Stick to distutils.
 Add multi Python variant support.
 
---- setup.py.orig	2018-06-02 04:59:02.000000000 +0000
+--- setup.py.orig	2021-08-15 19:30:48.000000000 +0000
 +++ setup.py
-@@ -10,12 +10,7 @@ VERSION = "7.43.0.2"
+@@ -10,10 +10,7 @@ VERSION = "7.44.1"
  
  import glob, os, re, sys, subprocess
  import distutils
 -try:
--    import wheel
--    if wheel:
--        from setuptools import setup
+-    from setuptools import setup
 -except ImportError:
 -    from distutils.core import setup
 +from distutils.core import setup
  from distutils.extension import Extension
  from distutils.util import split_quoted
  from distutils.version import LooseVersion
-@@ -605,9 +600,9 @@ def get_data_files():
+@@ -687,9 +684,9 @@ def get_data_files():
      # a list of tuples with (path to install to, a list of local files)
      data_files = []
      if sys.platform == "win32":
