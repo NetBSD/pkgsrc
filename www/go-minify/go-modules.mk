5dbe833f00164bc3b1efa045c363c3b64aaa23a4
nia <nia@pkgsrc.org>
2021-08-25 07:02:29 +0000

    go-minify: Update to 2.9.21. Convert to go-module build.
    Now builds only the command line tool.
    
    Update includes various bug fixes.

---
+++ www/go-minify/go-modules.mk
@@ -0,0 +1,19 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/08/25 07:02:29 nia Exp $
+
+GO_MODULE_FILES+=	github.com/cheekybits/is/@v/v0.0.0-20150225183255-68e9c0620927.mod
+GO_MODULE_FILES+=	github.com/cheekybits/is/@v/v0.0.0-20150225183255-68e9c0620927.zip
+GO_MODULE_FILES+=	github.com/dustin/go-humanize/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/dustin/go-humanize/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/fsnotify/fsnotify/@v/v1.4.9.mod
+GO_MODULE_FILES+=	github.com/fsnotify/fsnotify/@v/v1.4.9.zip
+GO_MODULE_FILES+=	github.com/matryer/try/@v/v0.0.0-20161228173917-9ac251b645a2.mod
+GO_MODULE_FILES+=	github.com/matryer/try/@v/v0.0.0-20161228173917-9ac251b645a2.zip
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.mod
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.zip
+GO_MODULE_FILES+=	github.com/tdewolff/parse/v2/@v/v2.5.19.mod
+GO_MODULE_FILES+=	github.com/tdewolff/parse/v2/@v/v2.5.19.zip
+GO_MODULE_FILES+=	github.com/tdewolff/test/@v/v1.0.6.mod
+GO_MODULE_FILES+=	github.com/tdewolff/test/@v/v1.0.6.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191005200804-aed5e4c7ecf9.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210510120138-977fb7262007.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210510120138-977fb7262007.zip
