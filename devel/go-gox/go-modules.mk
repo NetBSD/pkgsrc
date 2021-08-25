5e3e62bd3acbaca9f60f83b97684db95368a64ea
nia <nia@pkgsrc.org>
2021-08-25 06:48:58 +0000

    go-gox: convert to go-module build, clean up makefile

---
+++ devel/go-gox/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/08/25 06:48:58 nia Exp $
+
+GO_MODULE_FILES+=	github.com/hashicorp/go-version/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/hashicorp/go-version/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/mitchellh/iochan/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/mitchellh/iochan/@v/v1.0.0.zip
