b6bb0637f6383e979b37d5e1cb4e6ceb05c12251
adam <adam@pkgsrc.org>
2021-08-26 06:32:59 +0000

    cmake: updated to 3.21.2
    CMake 3.21.2
    * AUTOUIC: Add test for cyclic dependency between UI headers and timestamp
    * CPackRPM: avoid a spurious `;` in the `%pre` and other sections with scripts
    * FindMPI: do not detect `-framework` as a compile flag
    * GNUInstallDirs: avoid unwanted variable dereference
    * VS: Fix assertion failure on INCLUDE_DIRECTORIES in INTERFACE libraries
    * VS: Fix /reference and /headerUnit flag table entries for v142 and v143
    * CheckLanguage: Avoid CMP0126 warning
    * AUTOUIC: Revert "Fix generating of dependency rules for UI header files"
    * VS: Add special case for '-T version=14.29.16.11' under VS 16.11
    * VS: Update Visual Studio 17 2022 generator for Preview 3.1
    * Help: Add missing versionadded annotation for CPACK_DMG_FILESYSTEM
    * Help: Fix unlinked mention of CMAKE_TOOLCHAIN_FILE
    * BinUtils: Avoid searching CMAKE_PREFIX_PATH
    * macOS: Restore support for Mac OS X 10.4 (Tiger)
    * Platform/Haiku: Remove the include-once behavior
    * CMakePresets: Check presets with their own file version
    * CTest: Reset multi-options to persistent multi-options
    * add_custom_command(DEPFILE) independent from CMAKE_DEPENDS_USE_COMPILER
    * Help: get_filename_component: fix version info for cmake_path
    * FindPkgConfig: Restore legacy behavior when CMP0126 is set to NEW
    * Help: cmake_path: add missing parameter
    * NVHPC: Support 21.07 change to '-MD' where it behaves like gcc
    * NVHPC-CXX: Add support for C++20
    * NVHPC-C: Add support for C17
    * FindBinUtils: Fall back to ld for Clang without lld
    * Android: Fix NDK toolchain dir on arm64 mac
    * CUDA/Clang: Fix separable compilation in non-root directories with Makefiles
    * CUDA/Clang: Simplify --register-link-binaries logic
    * Ninja: Revert accidental LINK_WHAT_YOU_USE impl for static libraries

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.18 2021/07/29 11:37:40 adam Exp $
+# $NetBSD: version.mk,v 1.19 2021/08/26 06:32:59 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.21.1
+CMAKE_VERSION=	3.21.2
 CMAKE_API=	${CMAKE_VERSION:R}
