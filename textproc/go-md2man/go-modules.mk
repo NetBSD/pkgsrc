d3041cce8737a666271ffd40f383734cad0c225f
nia <nia@pkgsrc.org>
2021-08-27 07:45:37 +0000

    go-md2man: add missing file

---
+++ textproc/go-md2man/go-modules.mk
@@ -0,0 +1,4 @@
+# $NetBSD: go-modules.mk,v 1.1 2021/08/27 07:45:37 nia Exp $
+
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.mod
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.zip
