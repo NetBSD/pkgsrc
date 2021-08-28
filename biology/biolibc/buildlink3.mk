0b7391d13282a8a8ecb29ae93e3181d7c21b5cf7
bacon <bacon@pkgsrc.org>
2021-08-28 18:34:37 +0000

    biology/biolibc: Update to 0.2.0
    Major API overhaul
    New classes for FASTA and FASTQ
    Generate accessor and mutator functions for all classes
    Generate man pages for all functions and macros
    Export delimiter-separated-value class to libxtend

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/06/15 13:47:46 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/08/28 18:34:37 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.3
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.0
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 .endif	# BIOLIBC_BUILDLINK3_MK
 
