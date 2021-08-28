48780cf5adee59efbc8d3d785b5773eaba50c43f
bacon <bacon@pkgsrc.org>
2021-08-28 18:34:09 +0000

    devel/libxtend: Update to 0.1.3.1
    Major API overhaul
    Generate accessor and mutator functions
    Generate man pages for all functions
    Import delimiter-separated-value class from biolibc

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/06/15 13:43:55 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/08/28 18:34:09 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.2.4
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.3
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
