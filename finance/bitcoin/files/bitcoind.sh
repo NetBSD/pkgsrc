01ff0563f8c3c62f1711c8745549169d340a1b7b
khorben <khorben@pkgsrc.org>
2021-08-29 22:38:58 +0000

    bitcoin: actually ship the RC script for bitcoind
    While there, take advantage of the introduction of the SYSCONFBASE variable.
    Tested on NetBSD/amd64.
    
    Bumps PKGREVISION.

--- finance/bitcoin/files/bitcoind.sh
+++ finance/bitcoin/files/bitcoind.sh
@@ -1,14 +1,15 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: bitcoind.sh,v 1.1 2018/07/08 11:13:46 khorben Exp $
+# $NetBSD: bitcoind.sh,v 1.2 2021/08/29 22:38:58 khorben Exp $
 #
-
+# Startup script for bitcoind
+#
 # PROVIDE: bitcoind
-# REQUIRE: DAEMON LOGIN wscons
+# REQUIRE: DAEMON
 # KEYWORD: shutdown
 
-if [ -f /etc/rc.subr ]; then
-	. /etc/rc.subr
+if [ -f @SYSCONFBASE@/rc.subr ]; then
+	. @SYSCONFBASE@/rc.subr
 fi
 
 name="bitcoind"
@@ -18,7 +19,7 @@ pidfile="@VARBASE@/run/bitcoind.pid"
 required_directory="@VARBASE@/bitcoin"
 command_args="-daemon -conf=@PREFIX@/etc/bitcoin.conf -pid=${pidfile} -datadir=${required_directory}"
 
-if [ -f /etc/rc.subr ]; then
+if [ -f @SYSCONFBASE@/rc.subr ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
