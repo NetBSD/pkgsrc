dff5da10f82cf36406e3c22ff0aaddf3dd418a4d
khorben <khorben@pkgsrc.org>
2021-08-30 22:29:18 +0000

    beanstalkd: let the RC script work unprivileged
    This takes advantage of the introduction of the SYSCONFBASE variable.
    Tested on NetBSD/amd64.
    
    While there, improve the general behaviour, and avoid `pkill -u` in particular
    when stopping the service.
    
    Bumps PKGREVISION.

--- net/beanstalkd/files/beanstalkd.sh
+++ net/beanstalkd/files/beanstalkd.sh
@@ -1,42 +1,45 @@
 #!/bin/sh
 #
-# $NetBSD: beanstalkd.sh,v 1.1 2021/03/18 22:54:52 khorben Exp $
+# $NetBSD: beanstalkd.sh,v 1.2 2021/08/30 22:29:18 khorben Exp $
 #
 # beanstalkd - simple, fast work queue daemon
 #
 # PROVIDE: beanstalkd
 # REQUIRE: DAEMON NETWORKING SERVERS
+# KEYWORD: shutdown
 #
 # You will need to set some variables in @PKG_SYSCONFDIR@/rc.conf to start beanstalkd:
 #
 # beanstalkd=YES
 
-. /etc/rc.subr
+if [ -f @SYSCONFBASE@/rc.subr ]; then
+	. @SYSCONFBASE@/rc.subr
+	load_rc_config_var beanstalkd_user beanstalkd_user
+	load_rc_config_var beanstalkd_host beanstalkd_host
+	load_rc_config_var beanstalkd_port beanstalkd_port
+fi
 
 name="beanstalkd"
 rcvar=${name}
-beanstalkd_user="@BEANSTALK_USER@"
-beanstalkd_host="127.0.0.1"
-beanstalkd_port="11300"
+beanstalkd_user="${beanstalkd_user:-@BEANSTALK_USER@}"
+beanstalkd_host="${beanstalkd_host:-127.0.0.1}"
+beanstalkd_port="${beanstalkd_port:-11300}"
+pidfile="@VARBASE@/run/${name}.pid"
 start_cmd="beanstalkd_start"
-restart_cmd="beanstalkd_stop ; beanstalkd_start"
-stop_cmd="beanstalkd_stop"
 command="@PREFIX@/bin/${name}"
 command_args="-u ${beanstalkd_user} -l ${beanstalkd_host} -p ${beanstalkd_port}"
 
 beanstalkd_start()
 {
-	${command} ${command_args} &
+	echo "Starting ${name}."
+	${command} ${beanstalkd_flags} ${command_args} &
+	echo $! > ${pidfile}
 }
 
-beanstalkd_stop()
-{
-        pkill -u @BEANSTALK_USER@ 2>/dev/null
-}
-
-if [ -f /etc/rc.subr ]; then
+if [ -f @SYSCONFBASE@/rc.subr ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
-	eval ${start_cmd}
+	echo -n "${name}"
+	${start_cmd}
 fi
