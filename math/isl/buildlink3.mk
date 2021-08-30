ef5272e9d3c643c20ed0a65e885f7cd3cf831ee0
micha <micha@pkgsrc.org>
2021-08-30 11:22:53 +0000

    math/isl: Add missing GMP dependency to bl3 file
    At least one of the installed header files includes "gmp.h".

--- math/isl/buildlink3.mk
+++ math/isl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/02/17 22:10:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/08/30 11:22:53 micha Exp $
 
 BUILDLINK_TREE+=	isl
 
@@ -7,6 +7,8 @@ ISL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.isl+=	isl>=0.11.1
 BUILDLINK_PKGSRCDIR.isl?=	../../math/isl
+
+.include "../../devel/gmp/buildlink3.mk"
 
 .endif # ISL_BUILDLINK3_MK
 
