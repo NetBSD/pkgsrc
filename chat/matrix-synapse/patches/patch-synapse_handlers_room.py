714a8c9ec6ded7cfe0b45250620f4c012b388c93
js <js@pkgsrc.org>
2021-08-31 19:57:59 +0000

    Update chat/matrix-synapse to 1.41.1
    This contains 2 security fixes.
    
    Since this skips many versions (this was at 1.38.1 before), I'm not including
    the full change log here as it is too long. Please look at the change log at
    https://github.com/matrix-org/synapse/blob/v1.41.1/CHANGES.md instead.

--- chat/matrix-synapse/patches/patch-synapse_handlers_room.py
+++ chat/matrix-synapse/patches/patch-synapse_handlers_room.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-synapse_handlers_room.py,v 1.1 2021/06/06 09:40:34 js Exp $
+$NetBSD: patch-synapse_handlers_room.py,v 1.2 2021/08/31 19:57:59 js Exp $
 
 This patch allows changing the default power level for the creator of a new
 room. It's useful to set this higher than 100 so that the creator of the room
@@ -6,12 +6,12 @@ can give admin to others while still retaining the ability to revoke admin from
 someone. In the past, this turned out to be the only thing that could recover a
 room in which a bridge with admin permissions went wild and kicked everybody.
 
---- synapse/handlers/room.py.orig	2021-06-03 12:24:19.000000000 +0000
+--- synapse/handlers/room.py.orig	2021-08-31 12:43:28.000000000 +0000
 +++ synapse/handlers/room.py
-@@ -936,7 +936,7 @@ class RoomCreationHandler(BaseHandler):
+@@ -933,7 +933,7 @@ class RoomCreationHandler(BaseHandler):
              )
          else:
-             power_level_content = {
+             power_level_content: JsonDict = {
 -                "users": {creator_id: 100},
 +                "users": {creator_id: @SYNAPSE_DEFAULT_CREATOR_PL@},
                  "users_default": 0,
