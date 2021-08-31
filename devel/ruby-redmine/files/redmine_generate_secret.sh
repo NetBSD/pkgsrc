733f10657b11c775f357c8ec4ee799e96cd660c8
taca <taca@pkgsrc.org>
2021-08-31 15:01:13 +0000

    devel/ruby-redmine: remove Redmine 4.0
    Redmine 4.0.9 was last release of Redmine 4.0 seriese.  No more fixes
    including security problem and there are security release for 4.1/4.2
    but not for 4.0.
    
    Discussing with MAINTAINER, we decide to remove ruby-redmine package.
    Please migrate to ruby-redmine41 or ruby-redmine42.

--- devel/ruby-redmine/files/redmine_generate_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd @PREFIX@/@RM_DIR@/app
-
-GEM_HOME=@PREFIX@/@RM_DIR@/gems \
-  GEM_PATH=@PREFIX@/@RM_DIR@/gems \
-    @RUBY@ bin/rake secret RAILS_ENV=production
--- devel/ruby-redmine/files/unicorn.rb
+++
@@ -1,8 +0,0 @@
-working_directory "@PREFIX@/@RM_DIR@/app"
-pid "@PREFIX@/@RM_DIR@/unicorn.pid"
-stderr_path "@PREFIX@/@RM_DIR@/unicorn.log"
-stdout_path "@PREFIX@/@RM_DIR@/unicorn.log"
-
-worker_processes 3
-listen "@PREFIX@/@RM_DIR@/unicorn.redmine.sock"
-timeout 60
