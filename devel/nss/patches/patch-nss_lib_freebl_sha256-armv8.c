8dea9f94c02f3bea7c7b51f909ea060856ff6b69
wiz <wiz@pkgsrc.org>
2021-08-31 11:12:30 +0000

    nss: update to 3.69.1.
    Bugs fixed:
       - Bug 1722613 (Backout) - Disable DTLS 1.0 and 1.1 by default
       - Bug 1720226 (Backout) - integrity checks in key4.db not happening on private components with AES_CBC

--- devel/nss/patches/patch-nss_lib_freebl_sha256-armv8.c
+++ devel/nss/patches/patch-nss_lib_freebl_sha256-armv8.c
@@ -1,3 +1,10 @@
+$NetBSD: patch-nss_lib_freebl_sha256-armv8.c,v 1.2 2021/08/31 11:12:30 wiz Exp $
+
+date: 2021-08-27 23:33:18 +0200;  author: mrg;  state: Exp;  commitid: r84ANY9lYgSAJE6D;
+fix build on arm64eb: sha512.c and sha256-armv8.c both provided the
+same symbols.  (sha256-x86.c has the same problem, but that file
+already requires little endian so is not a big deal.)
+
 --- nss/lib/freebl/sha256-armv8.c.orig	2021-08-05 01:03:36.000000000 -0700
 +++ nss/lib/freebl/sha256-armv8.c	2021-08-27 14:21:56.160037512 -0700
 @@ -2,7 +2,7 @@
