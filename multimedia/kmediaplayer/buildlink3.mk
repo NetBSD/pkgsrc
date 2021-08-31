7cb4e2e90e859990f0771fd5feffb8d7f6450c51
markd <markd@pkgsrc.org>
2021-08-31 11:49:15 +0000

    kmediaplayer: add version 5.80.0
    KMediaPlayer builds on the KParts framework to provide a common interface for
    KParts that can play media files.
    
    This framework is a porting aid. It is not recommended for new projects, and
    existing projects that use it are advised to port away from it, and use plain
    KParts instead.

---
+++ multimedia/kmediaplayer/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/08/31 11:49:15 markd Exp $
+
+BUILDLINK_TREE+=	kmediaplayer
+
+.if !defined(KMEDIAPLAYER_BUILDLINK3_MK)
+KMEDIAPLAYER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kmediaplayer+=	kmediaplayer>=5.80.0
+BUILDLINK_PKGSRCDIR.kmediaplayer?=	../../multimedia/kmediaplayer
+
+.include "../../devel/kparts/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KMEDIAPLAYER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kmediaplayer
