3d0844a12e96047a38e47e09ad987a4776197f25
mrg <mrg@pkgsrc.org>
2021-09-01 09:40:46 +0000

    re-do this patch using a GCC defined macro.
    this is still wrong, but it's less wrong than before and once again
    both arm64 and arm64eb (and amd64) build.
    
    
    this is really strange.  the code in sha512.c uses:
    
    #if !defined(USE_HW_SHA2) || !defined(IS_LITTLE_ENDIAN)
    
    which originally this patch attempted to match, but IS_LITTLE_ENDIAN
    is never defined inside nss, even though it's used a few dozen times.
    there is a MP_IS_LITTLE_ENDIAN defined that is setup, but almost
    never used.

--- devel/nss/patches/patch-nss_lib_freebl_sha256-armv8.c
+++ devel/nss/patches/patch-nss_lib_freebl_sha256-armv8.c
@@ -1,18 +1,28 @@
-$NetBSD: patch-nss_lib_freebl_sha256-armv8.c,v 1.2 2021/08/31 11:12:30 wiz Exp $
+$NetBSD: patch-nss_lib_freebl_sha256-armv8.c,v 1.3 2021/09/01 09:40:46 mrg Exp $
 
-date: 2021-08-27 23:33:18 +0200;  author: mrg;  state: Exp;  commitid: r84ANY9lYgSAJE6D;
+
 fix build on arm64eb: sha512.c and sha256-armv8.c both provided the
 same symbols.  (sha256-x86.c has the same problem, but that file
 already requires little endian so is not a big deal.)
 
+XXX  this is really strange.  the code in sha512.c uses:
+
+#if !defined(USE_HW_SHA2) || !defined(IS_LITTLE_ENDIAN)
+
+which originally this patch attempted to match, but IS_LITTLE_ENDIAN
+is never defined inside nss, even though it's used a few dozen times.
+there is a MP_IS_LITTLE_ENDIAN defined that is setup, but almost
+never used.
+
+
 --- nss/lib/freebl/sha256-armv8.c.orig	2021-08-05 01:03:36.000000000 -0700
-+++ nss/lib/freebl/sha256-armv8.c	2021-08-27 14:21:56.160037512 -0700
++++ nss/lib/freebl/sha256-armv8.c	2021-09-01 01:59:50.464034605 -0700
 @@ -2,7 +2,7 @@
   * License, v. 2.0. If a copy of the MPL was not distributed with this
   * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
  
 -#ifdef USE_HW_SHA2
-+#if defined(USE_HW_SHA2) && defined(IS_LITTLE_ENDIAN)
++#if defined(USE_HW_SHA2) && !defined(__ARM_BIG_ENDIAN)
  
  #ifndef __ARM_FEATURE_CRYPTO
  #error "Compiler option is invalid"
@@ -21,4 +31,4 @@ already requires little endian so is not a big deal.)
  }
  
 -#endif /* USE_HW_SHA2 */
-+#endif /* USE_HW_SHA2 && IS_LITTLE_ENDIAN */
++#endif /* USE_HW_SHA2 && !__ARM_BIG_ENDIAN */
