7cb71d58d4db4613f6626560e79fc7fbe5a2428b
micha <micha@pkgsrc.org>
2021-09-01 16:43:59 +0000

    news/tin: Build fix for the option combination curses(pdcurses) + icu

--- news/tin/patches/patch-include_tcurses.h
+++ news/tin/patches/patch-include_tcurses.h
@@ -1,10 +1,21 @@
-$NetBSD: patch-include_tcurses.h,v 1.1 2021/08/27 17:45:26 micha Exp $
+$NetBSD: patch-include_tcurses.h,v 1.2 2021/09/01 16:43:59 micha Exp $
 
-Backported upstream patch for ncurses (without w).
+Backported upstream patch for ncurses (without w) and pdcurses.
 
 --- include/tcurses.h.orig	2021-07-13 05:56:52.000000000 +0000
 +++ include/tcurses.h
-@@ -75,7 +75,7 @@
+@@ -44,7 +44,10 @@
+ 
+ #	if defined(USE_CURSES) || defined(NEED_CURSES_H)
+ #		ifdef HAVE_XCURSES
++#			undef bool
++#			define bool XCursesBool
+ #			include <xcurses.h>
++#			undef bool
+ #			define getattrs(w) (w)->_attrs
+ #		else
+ #			if defined(HAVE_NCURSESW_NCURSES_H)
+@@ -75,7 +78,7 @@
  #					endif /* HAVE_NCURSES_NCURSES_H */
  #				endif /* HAVE_NCURSES_H */
  #			endif /* HAVE_NCURSESW_NCURSES_H */
