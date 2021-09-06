ea926370930e06718274b0cda055ca112a636d03
adam <adam@pkgsrc.org>
2021-09-06 07:25:13 +0000

    python37: updated to 3.7.12
    Python 3.7.12 final
    
    Security
    
    bpo-44394: Update the vendored copy of libexpat to 2.4.1 (from 2.2.8) to get the fix for the CVE-2013-0340 “Billion Laughs” vulnerability. This copy is most used on Windows and macOS.
    bpo-43124: Made the internal putcmd function in smtplib sanitize input for presence of \r and \n characters to avoid (unlikely) command injection.
    
    Library
    
    bpo-45001: Made email date parsing more robust against malformed input, namely a whitespace-only Date: header. Patch by Wouter Bolsterlee.

--- lang/python37/dist.mk
+++ lang/python37/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.12 2021/06/29 12:39:10 adam Exp $
+# $NetBSD: dist.mk,v 1.13 2021/09/06 07:25:13 adam Exp $
 
-PY_DISTVERSION=	3.7.11
+PY_DISTVERSION=	3.7.12
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python37/distinfo
