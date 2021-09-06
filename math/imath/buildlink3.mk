f11c926d52a765aca67b9f1c5ae14865540aa99d
nia <nia@pkgsrc.org>
2021-09-06 07:02:57 +0000

    math: require at least gcc6 to link dependencies

--- math/imath/buildlink3.mk
+++ math/imath/buildlink3.mk
@@ -1,9 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/08/15 14:14:43 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/09/06 07:02:57 nia Exp $
 
 BUILDLINK_TREE+=	imath
 
 .if !defined(IMATH_BUILDLINK3_MK)
 IMATH_BUILDLINK3_MK:=
+
+# C++14
+GCC_REQD+=	6
 
 BUILDLINK_API_DEPENDS.imath+=	imath>=3.0.4
 BUILDLINK_ABI_DEPENDS.imath+=	imath>=3.1
