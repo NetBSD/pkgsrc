d697ad7cc86fac314fd43ccb85d79646367f95e4
pin <pin@pkgsrc.org>
2021-09-08 10:42:54 +0000

    audio/spotify-player: import package
    spotify-player is a custom Spotify player built and tweaked based on my personal
    preferences. It is fast, easy to use, and configurable.
    
    It's designed to be a player, not a fully-fledged Spotify clone, so it does not
    aim to support all possible Spotify features.
    The main goal is to provide a quick and intuitive way to modify the current
    playback by either using player commands or navigating between different
    contexts.
    Similar to other TUI applications, is keyboard driven. User will use a set of
    predefined commands with shortcuts to interact with the player.
    spotify-player has a simple UI with three main components:
    -a playback window displaying the current playback
    -a context window displaying a context (playlist, album, artist)
    -popup windows for using some commands (switch theme, browser playlists, etc)
    or displaying additional information
    
    spotify-player is built on top of tui, rspotify, and librespot libraries.
    It's inspired by spotify-tui and ncspot.

---
+++ audio/spotify-player/options.mk
@@ -0,0 +1,56 @@
+# $NetBSD: options.mk,v 1.1 2021/09/08 10:42:54 pin Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.spotify-player
+PKG_SUPPORTED_OPTIONS=	alsa gstreamer jack portaudio pulseaudio rodio sdl
+PKG_SUGGESTED_OPTIONS=	portaudio
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Malsa)
+CARGO_FEATURES+=	alsa-backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.alsa-lib}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.alsa-lib}/lib
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mgstreamer)
+CARGO_FEATURES+=	gstreamer-backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.gstreamer1}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.gstreamer1}/lib
+.include "../../multimedia/gstreamer1/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mjack)
+CARGO_FEATURES+=	jackaudio-backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.jack}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.jack}/lib
+.include "../../audio/jack/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mportaudio)
+CARGO_FEATURES+=	portaudio-backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.portaudio}/lib
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.portaudio}/${BUILDLINK_LIBDIRS.portaudio}
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.portaudio}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.portaudio}/${BUILDLINK_LIBDIRS.portaudio}
+.include "../../audio/portaudio/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+CARGO_FEATURES+=	pulseaudio-backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.pulseaudio}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.pulseaudio}/lib
+.include "../../audio/pulseaudio/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mrodio)
+CARGO_FEATURES+=	rodio-backend
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl)
+CARGO_FEATURES+=	sdl-backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.SDL2}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.SDL2}/lib
+.include "../../devel/SDL2/buildlink3.mk"
+.endif
+
