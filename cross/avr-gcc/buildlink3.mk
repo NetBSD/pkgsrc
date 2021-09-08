4c78431ef737c8848661127fa79049ab5ee068d0
micha <micha@pkgsrc.org>
2021-09-08 12:07:06 +0000

    cross/avr-gcc: Update to 10.3.0
    Changelog:
    https://gcc.gnu.org/gcc-10/changes.html

--- cross/avr-gcc/buildlink3.mk
+++ cross/avr-gcc/buildlink3.mk
@@ -1,16 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 04:26:46 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/09/08 12:07:06 micha Exp $
 
 BUILDLINK_TREE+=	avr-gcc
 
 .if !defined(AVR_GCC_BUILDLINK3_MK)
 AVR_GCC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.avr-gcc+=		avr-gcc>=4.8.3
-BUILDLINK_PKGSRCDIR.avr-gcc?=		../../cross/avr-gcc
-# Folling line was harmful, .buildlink/lib/ -> buildlinklib/
-# It did not have first ending slash, probably was OK.
-#BUILDLINK_FNAME_TRANSFORM.avr-gcc+=	-e "s,${AVR_TARGET}/lib/,lib/,g"
-
-.endif # AVR_GCC_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.avr-gcc+=	avr-gcc>=10.3.0
+BUILDLINK_PKGSRCDIR.avr-gcc?=	../../cross/avr-gcc
+.endif	# AVR_GCC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-avr-gcc
