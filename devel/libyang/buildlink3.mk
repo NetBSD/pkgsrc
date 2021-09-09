7d9fa526460144fe1acd7687d016290839e28300
kardel <kardel@pkgsrc.org>
2021-09-09 07:39:15 +0000

    devel/libyang: update to v1.0.240

--- devel/libyang/buildlink3.mk
+++ devel/libyang/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/12/10 16:25:19 kardel Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/09/09 07:39:15 kardel Exp $
 
 BUILDLINK_TREE+=	libyang
 
 .if !defined(LIBYANG_BUILDLINK3_MK)
 LIBYANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libyang+=	libyang>=1.0.184
+BUILDLINK_API_DEPENDS.libyang+=	libyang>=1.0.240
 BUILDLINK_PKGSRCDIR.libyang?=	../../devel/libyang
 TOOL_DEPENDS+=			cmocka>=1.1.5:../../devel/cmocka
 
