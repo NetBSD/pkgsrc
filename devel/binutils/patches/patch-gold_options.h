c8342fde1b9cf4f39bd5c53d41bc0a2efd4fd3e0
fcambus <fcambus@pkgsrc.org>
2021-09-11 15:54:39 +0000

    binutils: update to 2.37.
    This release contains numerous bug fixes, and also the following
    new features:
    
      * The GNU Binutils sources now requires a C99 compiler and library to
        build.
    
      * Support for the arm-symbianelf format has been removed.
    
      * Support for Realm Management Extension (RME) for AArch64 has been
        added.
    
      * A new linker option '-z report-relative-reloc' for x86 ELF targets
        has been added to report dynamic relative relocations.
    
      * A new linker option '-z start-stop-gc' has been added to disable
        special treatment of __start_*/__stop_* references when
        --gc-sections.
    
      * A new linker options '-Bno-symbolic' has been added which will
        cancel the '-Bsymbolic' and '-Bsymbolic-functions' options.
    
      * The readelf tool has a new command line option which can be used to
        specify how the numeric values of symbols are reported.
        --sym-base=0|8|10|16 tells readelf to display the values in base 8,
        base 10 or base 16.  A sym base of 0 represents the default action
        of displaying values under 10000 in base 10 and values above that in
        base 16.
    
      * A new format has been added to the nm program.  Specifying
        '--format=just-symbols' (or just using -j) will tell the program to
        only display symbol names and nothing else.
    
      * A new command line option '--keep-section-symbols' has been added to
        objcopy and strip.  This stops the removal of unused section symbols
        when the file is copied.  Removing these symbols saves space, but
        sometimes they are needed by other tools.
    
      * The '--weaken', '--weaken-symbol' and '--weaken-symbols' options
        supported by objcopy now make undefined symbols weak on targets that
        support weak symbols.
    
      * Readelf and objdump can now display and use the contents of .debug_sup
        sections.
    
      * Readelf and objdump will now follow links to separate debug info
        files by default.  This behaviour can be stopped via the use of the
        new '-wN' or '--debug-dump=no-follow-links' options for readelf and
        the '-WN' or '--dwarf=no-follow-links' options for objdump.  Also
        the old behaviour can be restored by the use of the
        '--enable-follow-debug-links=no' configure time option.
    
        The semantics of the =follow-links option have also been slightly
        changed.  When enabled, the option allows for the loading of symbol
        tables and string tables from the separate files which can be used
        to enhance the information displayed when dumping other sections,
        but it does not automatically imply that information from the
        separate files should be displayed.
    
        If other debug section display options are also enabled (eg
        '--debug-dump=info') then the contents of matching sections in both
        the main file and the separate debuginfo file *will* be displayed.
        This is because in most cases the debug section will only be present
        in one of the files.
    
        If however non-debug section display options are enabled (eg
        '--sections') then the contents of matching parts of the separate
        debuginfo file will *not* be displayed.  This is because in most
        cases the user probably only wanted to load the symbol information
        from the separate debuginfo file.  In order to change this behaviour
        a new command line option --process-links can be used.  This will
        allow di0pslay options to applied to both the main file and any
        separate debuginfo files.
    
      * Nm has a new command line option: '--quiet'.  This suppresses "no
        symbols" diagnostic.

--- devel/binutils/patches/patch-gold_options.h
+++ devel/binutils/patches/patch-gold_options.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-gold_options.h,v 1.5 2020/12/20 19:07:05 fcambus Exp $
+$NetBSD: patch-gold_options.h,v 1.6 2021/09/11 15:54:39 fcambus Exp $
 
  The NetBSD real-time linker do not understand DT_RUNPATH or
  DT_INIT_ARRAY.  The current gold defaults are not suitable for it.
  The last couple of DragonFly, FreeBSD, and OpenBSD releases do
  support these tags.
 
---- gold/options.h.orig	2020-07-24 09:12:20.000000000 +0000
+--- gold/options.h.orig	2021-07-08 11:37:20.000000000 +0000
 +++ gold/options.h
-@@ -772,7 +772,11 @@ class General_options
+@@ -781,7 +781,11 @@ class General_options
  	      N_("Output cross reference table"),
  	      N_("Do not output cross reference table"));
  
@@ -19,7 +19,7 @@ $NetBSD: patch-gold_options.h,v 1.5 2020/12/20 19:07:05 fcambus Exp $
  	      N_("Use DT_INIT_ARRAY for all constructors"),
  	      N_("Handle constructors as directed by compiler"));
  
-@@ -834,7 +838,11 @@ class General_options
+@@ -843,7 +847,11 @@ class General_options
  	      N_("Do not create exception frame header"));
  
    // Alphabetized under 'e' because the option is spelled --enable-new-dtags.
