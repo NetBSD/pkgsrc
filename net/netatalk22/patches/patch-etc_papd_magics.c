e94ed182b7f1b4420fba81e497dd107b0cff2722
nat <nat@pkgsrc.org>
2021-09-12 15:52:37 +0000

    Fixes for earlier versions of appletalk and phase 1 interfaces.
    Add option to disable afp session timeouts.
    Fix setting of phase1 addresses on NetBSD.
    Send replies to client when printing to prompt more data to be sent.
    
    ok markd@.

---
+++ net/netatalk22/patches/patch-etc_papd_magics.c
@@ -0,0 +1,43 @@
+$NetBSD: patch-etc_papd_magics.c,v 1.1 2021/09/12 15:52:37 nat Exp $
+
+Send a reply when more data required by papd server.
+As required by appletalk 58.
+
+Also prevent an erronous return value from ps().
+
+AFAICT this should have no consequence on later appletalk versions.
+
+--- etc/papd/magics.c.orig	2014-03-16 11:17:48.000000000 +0000
++++ etc/papd/magics.c
+@@ -74,6 +74,7 @@ int ps( struct papfile *infile, struct p
+ 		return( 0 );
+ 
+ 	    case -1 :
++		spoolreply( outfile, "Processing..." );
+ 		return( 0 );
+ 	    }
+ 
+@@ -98,6 +99,7 @@ int ps( struct papfile *infile, struct p
+ 	    CONSUME( infile, linelength + crlflength );
+ 	}
+     }
++    return 0;
+ }
+ 
+ int cm_psquery( struct papfile *in, struct papfile *out, struct sockaddr_at *sat _U_)
+@@ -115,6 +117,7 @@ int cm_psquery( struct papfile *in, stru
+ 	    return( CH_DONE );
+ 
+ 	case -1 :
++	    spoolreply( out, "Processing..." );
+ 	    return( CH_MORE );
+ 
+         case -2 :
+@@ -149,6 +152,7 @@ int cm_psadobe( struct papfile *in, stru
+ 	    return( CH_DONE );
+ 
+ 	case -1 :
++	    spoolreply( out, "Processing..." );
+ 	    return( CH_MORE );
+ 
+         case -2 :
