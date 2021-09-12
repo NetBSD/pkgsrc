fc0a7e6179c6cb7b9fb3d88b10b34fb1dcf38528
wiz <wiz@pkgsrc.org>
2021-09-12 09:08:32 +0000

    age: update to 1.0.0.
    v1.0.0 is the first stable release of the Go CLI and API, twenty
    months after the first beta.

--- security/age/go-modules.mk
+++ security/age/go-modules.mk
@@ -1,16 +1,14 @@
-# $NetBSD: go-modules.mk,v 1.2 2021/06/23 21:00:24 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.3 2021/09/12 09:08:32 wiz Exp $
 
-GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-beta.3.mod
-GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-beta.3.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20201221181555-eec23a3978ad.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20201221181555-eec23a3978ad.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.zip
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201117132131-f5c789dd3221.mod
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201117132131-f5c789dd3221.zip
-GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
-GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-rc.1.mod
+GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-rc.1.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210817164053-32db794688a5.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210817164053-32db794688a5.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210903071746-97244b99971b.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210903071746-97244b99971b.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20210615171337-6886f2dfbf5b.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20210615171337-6886f2dfbf5b.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.zip
