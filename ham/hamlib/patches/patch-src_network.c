64f22d6f6cdf2d0c74a810fed7223df912b7f603
gdt <gdt@pkgsrc.org>
2021-09-15 22:52:45 +0000

    ham/hamlib: Update to 4.3.1
    packaging:
    
      - Adjust upstream program testlibusb to have rigctl_ prefix to avoid
        collisions.
    
      - Note that sunos patch should be sent upstream.
    
    Version 4.3.1
            * 2021-09-14
            * Bug fix release
    
    Version 4.3
            * 2021-09-01
            * libusb-1.0.23 or greater is now required or use --without-libusb
            * Generating documentation now requires GNU source-highlighter.
            * Added IC-575
            * Less VFO swapping of newer Icom rigs -- zero swapping in WSJTX and JTDX
            * Dual rotator control in rotctl -R option
            * Started work on simulators -- very crude right now but usable to debug some things
            * Overhaul of rig split -- reverse split (VFOA=RX VFOB=TX) should work for rigs capable of it
              Starting VFO does not matter -- rig will end up on RX VFO
              S VFOA 1 VFOB
              S VFOB 1 VFOA
              S Main 1 Sub
              S Sub 1 Main
    
    Version 4.2
            * 2021-05-17
            * New rig_get_mode_bandwidths -- returns token set for bandwidths for given mode
                  Rig command: \get_mode_bandwidths CW
                  Mode=CW
                  Normal=500Hz
                  Narrow=50Hz
                  Wide=2400Hz
            * New rig_get_info  -- returns token set for all vfos where order does not matter
              This is a string return to allow for easy future expansion without changing the API
              New tokens may be introduced and can be skipped if not used by clients
                  Rig command: \get_rig_info
                  VFO=Main Freq=145000000 Mode=None Width=0 RX=1 TX=1
                  VFO=VFOB Freq=145000000 Mode=FM Width=15000 RX=0 TX=0
                  Split=0 SatMode=0
                  Rig=Dummy
                  App=Hamlib
                  Version=20210429
                  CRC=0xf49f4708
            * New rig_get_vfo_info
                  Rig command: \get_vfo_info VFOA
                  Freq: 145000000
                  Mode: None
                  Width: 0
                  Split: 0
                  SatMode: 0
    
            * FILPATHLEN has changed to HAMLIB_FILPATHLEN
    
            * USRP lib and gnuradio are deprecated and will be removed in 5.0
            * Added Radan rotator
            * Added Malachite SDR
            * Major rework for PRM80
            * Add twiddle_timeout and twiddle_rit --set-conf options
                  rigctld --set-conf=twiddle_timeout=5,twiddle_rit=1
                  This will set the twiddle timeout to 5 seconds and turn on twiddle_rit
                  For twiddle timeout VFOB will not be polled for 5 seconds after VFO twiddling
    	      is detected
            * rigctld --twiddle is deprecated and will be removed in 5.0 along with
              get_twiddle and set_twiddle
            * Rework Doxygen manual including default layout for Doxygen 1.9.1.  So far
              the amplifier, rotator, and utilities API sections have been updated.  The
              rig (radio) section remains to be updated.

--- ham/hamlib/patches/patch-src_network.c
+++ ham/hamlib/patches/patch-src_network.c
@@ -1,11 +1,14 @@
-$NetBSD: patch-src_network.c,v 1.1 2018/06/12 12:40:02 wiedi Exp $
+$NetBSD: patch-src_network.c,v 1.2 2021/09/15 22:52:46 gdt Exp $
 
 Add include for FIONREAD on SunOS
---- src/network.c.orig	2018-03-31 13:12:24.000000000 +0000
+
+\todo File this patch upstream and include bugtracker URL.
+
+--- src/network.c.orig	2021-09-13 23:33:26.000000000 +0000
 +++ src/network.c
-@@ -45,6 +45,10 @@
- #include <sys/types.h>
+@@ -46,6 +46,10 @@
  #include <signal.h>
+ #include <pthread.h>
  
 +#if defined(__sun)
 +#include <sys/filio.h>
