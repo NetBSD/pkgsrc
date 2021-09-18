5e8ed7c27a1251a46684e23d3d2747806cadd4cc
mef <mef@pkgsrc.org>
2021-09-18 05:06:06 +0000

    (math/nlopt)  Updated 2.4.2 to 2.6.2
    # NLopt Release Notes
    
    ## NLopt 2.6.2
    
    15 April 2020
    
    * Fixed forced stop exception with dimension elimination ([#317])
    
    * Fixed `get_initial_step` wrapping ([#319])
    
    * Various build fixes ([#314], [#308], [#303], [#278])
    
    ## NLopt 2.6.1
    
    13 April 2019
    
    * Fix `nlopt_version` result for 2.6.x and update soname.
    
    ## NLopt 2.6
    
    12 April 2019
    
    * New `nlopt_set_upper_bound` and `nlopt_set_lower_bound` functions in the low-level C API to set one bound at a time ([#257]).
    
    * There is no longer a separate `libnlopt_cxx` library: C++ algorithms (STOGO and AGS) are compiled and included by default ([#198]).
    
    * Various build fixes ([#197], [#216], [#245], [#250], [#230], [#261], etc.), other fixes ([#242], [#258]).
    
    ## NLopt 2.5
    
    26 July 2018
    
    * New AGS global solver ([#194]), thanks to Vladislav Sovrasov.
    
    * New `nlopt_get_numevals` function providing a built-in evaluation counter ([#160]).
    
    * New `nlopt_get_errmsg` function for more descriptive error messages.
    
    * Build system is converted to `cmake` ([#49]), thanks to Julien Schueller
    
    * Plugins updated for recent Octave and Guile versions.
    
    * Various other build fixes and minor bug fixes.

--- math/nlopt/buildlink3.mk
+++ math/nlopt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:23 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/09/18 05:06:06 mef Exp $
 
 BUILDLINK_TREE+=	nlopt
 
 .if !defined(NLOPT_BUILDLINK3_MK)
 NLOPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.4.2
-BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.4.2
+BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.6.2
+BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.6.2
 BUILDLINK_PKGSRCDIR.nlopt?=	../../math/nlopt
 .endif # NLOPT_BUILDLINK3_MK
 
