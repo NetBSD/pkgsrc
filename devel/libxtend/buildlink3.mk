36394e8ecf1d7fd25ebd3d0bb8d9243a602c3b72
bacon <bacon@pkgsrc.org>
2021-09-18 00:33:35 +0000

    devel/libxtend: Update to 0.1.3.5
    dsv_read_field*(): Treat multiple space characters as one delimiter
    Regenerate man pages with improved auto-c2man
    Improved formatting and added missing return value sections

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/08/28 18:34:09 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2021/09/18 00:33:35 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.3
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.3.5
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
