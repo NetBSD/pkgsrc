bbd8ecf06bb781077bde4fea1b12b8c3005ae3c3
gdt <gdt@pkgsrc.org>
2021-09-18 15:03:04 +0000

    ham/trustedQSL: reduce patches
    This commit does not intend to make any functional changes.
    
      - drop removal of LIB_SUFFIX via SUBST, as that seems overtaken by
        upstream
    
      - set db5 include prefix correctly and drop patches to work around
        the old way

--- ham/trustedQSL/patches/patch-openssl__cert.cpp
+++ ham/trustedQSL/patches/patch-openssl__cert.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-openssl__cert.cpp,v 1.1 2015/10/11 02:59:07 mef Exp $
+$NetBSD: patch-openssl__cert.cpp,v 1.2 2021/09/18 15:03:04 gdt Exp $
 
-error: 'PATH_MAX' was not declared in this scope
+\todo Figure out why this is needed, as PATH_MAX is specified by POSIX
+to be defined by <limits.h>.
 
 --- src/openssl_cert.cpp.orig	2015-09-20 09:15:24.000000000 +0900
 +++ src/openssl_cert.cpp	2015-10-11 01:10:26.000000000 +0900
