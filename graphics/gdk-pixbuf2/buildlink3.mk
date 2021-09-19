b8d21f59c467740031fa841f4d37fc4dd8965a12
ryoon <ryoon@pkgsrc.org>
2021-09-19 15:09:09 +0000

    gdk-pixbuf2: Dependencies require graphics/tiff too
    This change should be applied to gdk-pixbuf2-2.42.6.
    
    * Buildlink graphics/tiff to fix graphics/librsvg at least.

--- graphics/gdk-pixbuf2/buildlink3.mk
+++ graphics/gdk-pixbuf2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/03/08 16:47:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2021/09/19 15:09:09 ryoon Exp $
 
 BUILDLINK_TREE+=	gdk-pixbuf2
 
@@ -12,6 +12,7 @@ BUILDLINK_PKGSRCDIR.gdk-pixbuf2?=	../../graphics/gdk-pixbuf2
 .include "../../databases/shared-mime-info/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
 .endif	# GDK_PIXBUF2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gdk-pixbuf2
