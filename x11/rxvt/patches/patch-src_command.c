4e8cb76a4d1fd87bd48830ea36f76e7aaacce3ea
nia <nia@pkgsrc.org>
2021-09-19 08:41:13 +0000

    rxvt: apply fix for CVE-2021-33477. bump PKGREVISION.

--- x11/rxvt/patches/patch-src_command.c
+++ x11/rxvt/patches/patch-src_command.c
@@ -1,8 +1,24 @@
-$NetBSD: patch-src_command.c,v 1.1 2017/08/05 07:00:20 nros Exp $
+$NetBSD: patch-src_command.c,v 1.2 2021/09/19 08:41:13 nia Exp $
+
+* fix CVE-2021-33477
 * fix CVE-2017-7483
---- src/command.c.orig	2017-08-05 06:35:48.000000000 +0000
+
+--- src/command.c.orig	2003-03-26 06:01:23.000000000 +0000
 +++ src/command.c
-@@ -2096,7 +2096,7 @@ rxvt_process_csi_seq(rxvt_t *r)
+@@ -1954,10 +1954,12 @@ rxvt_process_escape_seq(rxvt_t *r)
+ 	rxvt_scr_add_lines(r, (const unsigned char *)"\n\r", 1, 2);
+ 	break;
+ 
++#if 0 /* disabled because embedded newlines can make exploits easier */
+     /* kidnapped escape sequence: Should be 8.3.48 */
+     case C1_ESA:		/* ESC G */
+ 	rxvt_process_graphics(r);
+ 	break;
++#endif
+ 
+     /* 8.3.63: CHARACTER TABULATION SET */
+     case C1_HTS:		/* ESC H */
+@@ -2096,7 +2098,7 @@ rxvt_process_csi_seq(rxvt_t *r)
      i = ch - CSI_ICH;
      ndef = get_byte_array_bit(csi_defaults, i);
      for (p = 0; p < nargs; p++)
