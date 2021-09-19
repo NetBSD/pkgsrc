cf80b8d72e53ed9681670582119e72c76e0f8893
hauke <hauke@pkgsrc.org>
2021-09-19 14:53:15 +0000

    Providing functions missing from xemacs 21.4 (stable) with a conditional defun will be less intrusive, and shorten the patch.

--- editors/xemacs-packages/patches/patch-lisp_w3_url.el
+++ editors/xemacs-packages/patches/patch-lisp_w3_url.el
@@ -1,36 +1,24 @@
-$NetBSD: patch-lisp_w3_url.el,v 1.1 2019/07/31 14:04:40 hauke Exp $
+$NetBSD: patch-lisp_w3_url.el,v 1.2 2021/09/19 14:53:15 hauke Exp $
 
 Provide buffer-local-value for the sake of XEmacs 21.4
 
 --- lisp/w3/url.el.orig	2015-12-14 21:38:21.000000000 +0000
 +++ lisp/w3/url.el
-@@ -57,6 +57,16 @@
+@@ -57,6 +57,17 @@
    :type 'directory
    :group 'url)
  
-+(defun url-buffer-local-value (sym buffer)
-+  "Get the buffer-local value of variable SYM in BUFFER. If there is no
-+buffer-local value in BUFFER then the global value of SYM is used."
-+  (if (fboundp 'buffer-local-value)
++(if (not (fboundp 'buffer-local-value))
++    (defun url-buffer-local-value (sym buffer)
++      "Get the buffer-local value of variable SYM in BUFFER. If there
++is no buffer-local value in BUFFER then the global value of SYM is
++used."
 +      (buffer-local-value sym buffer)
-+    (or (cdr (assoc sym (buffer-local-variables buffer)))
-+        (save-excursion
-+          (set-buffer buffer)
-+          (symbol-value sym)))))
++      (or (cdr (assoc sym (buffer-local-variables buffer)))
++	  (save-excursion
++	    (set-buffer buffer)
++	    (symbol-value sym)))))
 +
  (defun url-do-setup ()
    "Setup the URL package.
  This is to avoid conflict with user settings if URL is dumped with
-@@ -256,10 +266,10 @@ no further processing).  URL is either a
- 	  (url-debug 'retrieval
- 		     "Spinning in url-retrieve-synchronously: %S (%S)"
- 		     retrieval-done asynch-buffer)
--          (if (buffer-local-value 'url-redirect-buffer asynch-buffer)
-+          (if (url-buffer-local-value 'url-redirect-buffer asynch-buffer)
-               (setq proc (get-buffer-process
-                           (setq asynch-buffer
--                                (buffer-local-value 'url-redirect-buffer
-+                                (url-buffer-local-value 'url-redirect-buffer
-                                                     asynch-buffer))))
-             (if (and proc (memq (process-status proc)
-                                 '(closed exit signal failed))
