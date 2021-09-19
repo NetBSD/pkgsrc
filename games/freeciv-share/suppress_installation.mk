a17ece95ae130ebb60fc41f1e4308aa8a1dcf8ab
nia <nia@pkgsrc.org>
2021-09-19 07:31:27 +0000

    freeciv: Update to 2.6.5. This is a bugfix-only release.
    Changelog:
    https://freeciv.fandom.com/wiki/NEWS-2.6.5

--- games/freeciv-share/suppress_installation.mk
+++ games/freeciv-share/suppress_installation.mk
@@ -1,4 +1,4 @@
-# $NetBSD: suppress_installation.mk,v 1.7 2020/05/21 15:58:40 rillig Exp $
+# $NetBSD: suppress_installation.mk,v 1.8 2021/09/19 07:31:27 nia Exp $
 
 # Makefile snippet to be included by freeciv-client and freeciv-server to
 # prevent files installed by freeciv-share from being installed again.
@@ -26,11 +26,11 @@ SUBST_STAGE.installmanfix3=	post-configure
 SUBST_FILES.installmanfix3=	Makefile
 SUBST_SED.installmanfix3+=	-e 's|translations doc ||g'
 SUBST_MESSAGE.installmanfix3=	Suppressing installation of man pages and localization files already in freeciv-share.
-SUBST_NOOP_OK.installmanfix3=	yes 
+SUBST_NOOP_OK.installmanfix3=	yes
 
 SUBST_CLASSES+=			installmanfix4
 SUBST_STAGE.installmanfix4=	post-configure
 SUBST_FILES.installmanfix4=	Makefile
 SUBST_SED.installmanfix4+=	-e 's|install-data-am: install-docDATA|install-data-am: \# install-docDATA|'
 SUBST_MESSAGE.installmanfix4=	Suppressing installation of doc files already in freeciv-share
-SUBST_NOOP_OK.installmanfix4=	yes 
+SUBST_NOOP_OK.installmanfix4=	yes
