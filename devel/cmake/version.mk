f6a03b7aa4108900110e865aba7338509e4be81f
adam <adam@pkgsrc.org>
2021-09-27 19:02:38 +0000

    cmake: updated to 3.21.3
    CMake 3.21.3
    * FindBoost: Add support for Boost 1.77
    * MSVC: Fix MSVC_TOOLSET_VERSION for VS 2022 v143 toolset
    * IRSL: Add discovery of VS 2022 v143 toolset redistributables
    * gitlab-ci: Update Windows builds to MSVC 19.29-16.11 toolset
    * VS: Fix managed C++ project generation for VS 2022
    * VS: Update Visual Studio 17 2022 generator for Preview 4
    * HIP: Detect ROCm path earlier
    * HIP: Remove ROMClang compiler id and use Clang directly
    * HIP: Simplify detection of HIP runtime CMake package
    * Android: Restore searching PATH for executables
    * IAR: Restore support for projects not enabling policy CMP0057
    * FindPython: Ensure homebrew on Mac M1 is used
    * Ninja: fix ARMClang paths for Windows
    * HIP: Use 'rocm_agent_enumerator' to determine CMAKE_HIP_ARCHITECTURES
    * KWSys: SystemTools: Ensure Windows Vista APIs are available before using them

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.19 2021/08/26 06:32:59 adam Exp $
+# $NetBSD: version.mk,v 1.20 2021/09/27 19:02:38 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.21.2
+CMAKE_VERSION=	3.21.3
 CMAKE_API=	${CMAKE_VERSION:R}
