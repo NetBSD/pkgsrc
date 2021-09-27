659b36959a7b971920e583ce55433d2685dbab73
adam <adam@pkgsrc.org>
2021-09-27 18:50:06 +0000

    meson: updated to 0.59.1
    0.59.0:
    Unescaped variables in pkgconfig files
    The custom_target() function now accepts a feed argument
    Separate functions for qt preprocess
    Cython as as first class language
    Support for the Wine Resource Compiler
    New vs2012 and vs2013 backend options
    Developer environment
    Fs Module now accepts files objects
    Compiler argument checking for get_supported_arguments
    New custom dependency for libintl
    Parallelized meson subprojects commands
    Using Vala no longer requires C in the project languages
    The import() function gains required and disabler arguments
    Objective C/C++ standard versions
    Qt.preprocess source arguments deprecated
    New build target methods
    Automatically set up Visual Studio environment
    gnome.compile_schemas() sets GSETTINGS_SCHEMA_DIR into devenv
    update_desktop_database added to gnome.post_install()

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.2 2021/02/21 12:45:23 adam Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.3 2021/09/27 18:50:06 adam Exp $
 
 Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
 a condition to match FreeBSD path norm for pkg-config files.
 
---- mesonbuild/modules/pkgconfig.py.orig	2021-02-01 20:35:16.000000000 +0000
+--- mesonbuild/modules/pkgconfig.py.orig	2021-08-18 11:22:33.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -530,10 +530,7 @@ class PkgConfigModule(ExtensionModule):
+@@ -549,10 +549,7 @@ class PkgConfigModule(ExtensionModule):
          pcfile = filebase + '.pc'
          pkgroot = kwargs.get('install_dir', default_install_dir)
          if pkgroot is None:
@@ -16,4 +16,4 @@ a condition to match FreeBSD path norm for pkg-config files.
 +            pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
          if not isinstance(pkgroot, str):
              raise mesonlib.MesonException('Install_dir must be a string.')
-         self.generate_pkgconfig_file(state, deps, subdirs, name, description, url,
+         self._generate_pkgconfig_file(state, deps, subdirs, name, description, url,
