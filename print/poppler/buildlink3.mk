22f79abf04a5199c4a198000f8fda627175c14b3
wiz <wiz@pkgsrc.org>
2021-09-29 10:10:30 +0000

    *: recursive bump for poppler 21.09.0

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.87 2021/06/10 11:06:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.88 2021/09/29 10:10:30 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=21.06.1
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=21.09.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 GCC_REQD+=	5
