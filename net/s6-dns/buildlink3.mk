4848a5224133d70f1caf4e66a335f4d8c7d913e2
schmonz <schmonz@pkgsrc.org>
2021-09-29 09:55:43 +0000

    Update to 2.3.5.2. From the changelog:
    - Adaptation to skalibs-2.11.0.0.
    - Bugfixes.

--- net/s6-dns/buildlink3.mk
+++ net/s6-dns/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/04/26 15:05:09 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/09/29 09:55:43 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-dns
 
 .if !defined(S6_DNS_BUILDLINK3_MK)
 S6_DNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.5.1
+BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.5.2
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../net/s6-dns
 BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
 BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
