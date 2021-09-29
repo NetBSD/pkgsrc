d122cb0395dd124f704942686af61d56798fc2ee
wiz <wiz@pkgsrc.org>
2021-09-29 10:10:30 +0000

    *: recursive bump for poppler 21.09.0

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.62 2021/08/15 14:23:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.63 2021/09/29 10:10:30 wiz Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2nb7
-BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.6nb9
+BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.11nb1
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
--- print/poppler-qt5/buildlink3.mk
+++ print/poppler-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2021/06/10 11:06:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2021/09/29 10:10:30 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt5
 POPPLER_QT5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt5+=	poppler-qt5>=0.26.0
-BUILDLINK_ABI_DEPENDS.poppler-qt5+=	poppler-qt5>=21.06.1
+BUILDLINK_ABI_DEPENDS.poppler-qt5+=	poppler-qt5>=21.09.0
 BUILDLINK_PKGSRCDIR.poppler-qt5?=	../../print/poppler-qt5
 
 .include "../../print/poppler/buildlink3.mk"
