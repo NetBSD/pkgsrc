e77432ae900900ed18263a4167c308b426664955
mef <mef@pkgsrc.org>
2021-09-30 14:18:14 +0000

    (lang/nqp) Update version in buildlink3.mk

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2021/09/04 11:31:19 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2021/09/30 14:18:14 mef Exp $
 
 BUILDLINK_TREE+=	nqp
 
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2021.08
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2021.08
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2021.09
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2021.09
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
 .include "../../devel/MoarVM/buildlink3.mk"
