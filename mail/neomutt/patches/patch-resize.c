9818d0a837f119fc637b13ffec7a5eea1e3cc32e
wiz <wiz@pkgsrc.org>
2021-09-30 09:34:01 +0000

    neomutt: add upstream pull request URL

--- mail/neomutt/patches/patch-resize.c
+++ mail/neomutt/patches/patch-resize.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-resize.c,v 1.3 2020/01/16 13:52:39 tm Exp $
+$NetBSD: patch-resize.c,v 1.4 2021/09/30 09:34:01 wiz Exp $
 
 Required to have the correct struct information for winsize.
+https://github.com/neomutt/neomutt/pull/3048
 
 --- resize.c.orig	2019-11-29 15:02:20.000000000 +0000
 +++ resize.c
