49a48ce2842b4b3e61fe0c0ef76eff17f3f2539b
nros <nros@pkgsrc.org>
2021-10-03 17:34:11 +0000

    Fix dynamic linking with rhash
    Make the rhash package install a .so file
    not just .so.0 so that -lrhash use
    dynamic linking not static linking.
    Remove depmethod build since the package now
    works with dynamic linking.
    While here add pkgconfig override to pkgconfig
    file.
    Bump PKGREVISION due to PLIST change.

--- misc/rhash/buildlink3.mk
+++ misc/rhash/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/18 21:48:18 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/10/03 17:34:11 nros Exp $
 
 BUILDLINK_TREE+=	rhash
 
@@ -8,8 +8,6 @@ RHASH_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.rhash+=	rhash>=1.3.1
 BUILDLINK_ABI_DEPENDS.rhash?=	rhash>=1.3.9nb1
 BUILDLINK_PKGSRCDIR.rhash?=	../../misc/rhash
-
-BUILDLINK_DEPMETHOD.rhash?=	build
 
 .include "../../security/openssl/buildlink3.mk"
 .endif	# RHASH_BUILDLINK3_MK
