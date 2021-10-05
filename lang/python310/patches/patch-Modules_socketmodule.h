2a4f7c8e0a95572963229241238a503cf2ecc24a
adam <adam@pkgsrc.org>
2021-10-05 19:07:13 +0000

    python310 py310-html-docs: added version 3.10.0
    Python 3.10
    
    Summary – Release highlights
    
    New syntax features:
    
    PEP 634, Structural Pattern Matching: Specification
    PEP 635, Structural Pattern Matching: Motivation and Rationale
    PEP 636, Structural Pattern Matching: Tutorial
    bpo-12782, Parenthesized context managers are now officially allowed.
    
    New features in the standard library:
    
    PEP 618, Add Optional Length-Checking To zip.
    
    Interpreter improvements:
    
    PEP 626, Precise line numbers for debugging and other tools.
    
    New typing features:
    
    PEP 604, Allow writing union types as X | Y
    PEP 613, Explicit Type Aliases
    PEP 612, Parameter Specification Variables
    
    Important deprecations, removals or restrictions:
    
    PEP 644, Require OpenSSL 1.1.1 or newer
    PEP 632, Deprecate distutils module.
    PEP 623, Deprecate and prepare for the removal of the wstr member in PyUnicodeObject.
    PEP 624, Remove Py_UNICODE encoder APIs
    PEP 597, Add optional EncodingWarning

---
+++ lang/python310/patches/patch-Modules_socketmodule.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-Modules_socketmodule.h,v 1.1 2021/10/05 19:07:13 adam Exp $
+
+Support NetBSD's socketcan implementation
+
+--- Modules/socketmodule.h.orig	2021-05-03 16:54:42.000000000 +0200
++++ Modules/socketmodule.h	2021-05-22 11:52:19.072276647 +0200
+@@ -131,6 +131,8 @@
+ 
+ #ifdef HAVE_LINUX_CAN_H
+ # include <linux/can.h>
++#elif defined(HAVE_NETCAN_CAN_H)
++# include <netcan/can.h>
+ #else
+ # undef AF_CAN
+ # undef PF_CAN
