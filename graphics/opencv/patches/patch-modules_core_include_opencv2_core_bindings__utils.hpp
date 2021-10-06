55318d0585ae9eebe98f2d9eb2281dbf03044c07
adam <adam@pkgsrc.org>
2021-10-06 21:10:24 +0000

    opencv opencv-contrib-face: updated to 3.4.15
    OpenCV 3.4.15 has been released. Bug fixes, optimizations and other enhancements are propagated into OpenCV 4.5.3.
    
    Long-lived OpenCV 3.x release series is here since 2015. We are going to reduce support of 3.x branch in the future to move forward to OpenCV 5.0.

--- graphics/opencv/patches/patch-modules_core_include_opencv2_core_bindings__utils.hpp
+++ graphics/opencv/patches/patch-modules_core_include_opencv2_core_bindings__utils.hpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-modules_core_include_opencv2_core_bindings__utils.hpp,v 1.1 2020/04/18 22:01:33 joerg Exp $
+$NetBSD: patch-modules_core_include_opencv2_core_bindings__utils.hpp,v 1.2 2021/10/06 21:10:24 adam Exp $
 
---- modules/core/include/opencv2/core/bindings_utils.hpp.orig	2020-04-18 02:33:26.310026773 +0000
+--- modules/core/include/opencv2/core/bindings_utils.hpp.orig	2021-07-04 21:10:13.000000000 +0000
 +++ modules/core/include/opencv2/core/bindings_utils.hpp
 @@ -5,6 +5,7 @@
  #ifndef OPENCV_CORE_BINDINGS_UTILS_HPP
@@ -9,4 +9,4 @@ $NetBSD: patch-modules_core_include_opencv2_core_bindings__utils.hpp,v 1.1 2020/
 +#include <sstream>
  #include <opencv2/core/async.hpp>
  #include <opencv2/core/detail/async_promise.hpp>
- 
+ #include <opencv2/core/utils/logger.hpp>
