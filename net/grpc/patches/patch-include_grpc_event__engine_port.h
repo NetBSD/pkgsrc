69b1ac4aa9604011cfce0f0e736f2f4aace09bb7
adam <adam@pkgsrc.org>
2021-10-06 10:58:30 +0000

    grpc py-grpcio py-grpcio-testing py-grpcio-tools: updated to 1.41.0
    Release v1.41.0
    
    This release contains refinements, improvements, and bug fixes, with highlights listed below.
    
    Core
    
    De-experimentalize XdsCredentials and XdsServerCredentials API.
    xDS: Remove environmental variable guard for security.
    xDS Security: Use new way to fetch certificate provider plugin instance config.
    xDS server serving status: Use a struct to allow more fields to be added in the future.
    Annotate impl/codegen with IWYU pragmas.
    Update submodule envoy-api to origin/main.
    Upgrade third_party/protobuf to v3.17.3.
    update submodule boringssl-with-bazel with origin/master-with-bazel.
    Delete libuv-iomgr implementation and GRPC_UV build option.
    Allow access to Google API regional endpoints via Google Default Credentials.
    Remove GPR_*_TLS macros except PTHREAD.
    Limit initial window size increases and per-stream window delta.
    
    C++
    
    Bump version to v1.41.0-pre1.
    De-experimentalize XdsServerBuilder.
    C++ opencensus filter: Fix point of creating context for overall call.
    Flag grpc++_test library testonly.
    Add note on officially supported platforms.
    Open census call attempt span name and attribute changes
    Open census call attempt span name and attribute changes.
    
    C#
    
    Backport 27382 to v1.41.x.
    [csharp] Fix error loading library grpc_csharp_ext.*.dll on windows with non-ASCII encoding.
    Annotate copied Content native lib items with package id to enable customization.
    
    Objective-C
    
    Objective-C: Fix issue with creating a Unix file socket.
    
    Python
    
    Use manylinux_2_17 instead of manylinux_2_24 tag for manylinux2014 aarch64 wheels.
    Add Python 3.10 drop 3.5.
    [Aio] Remove custom IO manager support.

--- net/grpc/patches/patch-include_grpc_event__engine_port.h
+++ net/grpc/patches/patch-include_grpc_event__engine_port.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-include_grpc_event__engine_port.h,v 1.3 2021/07/27 15:47:18 adam Exp $
+$NetBSD: patch-include_grpc_event__engine_port.h,v 1.4 2021/10/06 10:58:30 adam Exp $
 
 Support NetBSD.
 
---- include/grpc/event_engine/port.h.orig	2021-07-27 15:11:38.000000000 +0000
+--- include/grpc/event_engine/port.h.orig	2021-09-25 02:33:41.000000000 +0000
 +++ include/grpc/event_engine/port.h
-@@ -22,7 +22,7 @@
- #elif defined(GPR_ANDROID) || defined(GPR_LINUX) || defined(GPR_APPLE) ||   \
+@@ -20,7 +20,7 @@
+ #if defined(GPR_ANDROID) || defined(GPR_LINUX) || defined(GPR_APPLE) ||     \
      defined(GPR_FREEBSD) || defined(GPR_OPENBSD) || defined(GPR_SOLARIS) || \
      defined(GPR_AIX) || defined(GPR_NACL) || defined(GPR_FUCHSIA) ||        \
 -    defined(GRPC_POSIX_SOCKET)
