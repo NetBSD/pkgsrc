684b46166a359c6e50d271ada39e8919d7b32b94
bsiegert <bsiegert@pkgsrc.org>
2021-10-08 14:47:44 +0000

    Update go117 to 1.17.2.
    This minor release includes a security fix according to the new security policy.
    
    When invoking functions from WASM modules, built using GOARCH=wasm GOOS=js,
    passing very large arguments can cause portions of the module to be overwritten
    with data from the arguments.
    
    If using wasm_exec.js to execute WASM modules, users will need to replace their
    copy (as described in https://golang.org/wiki/WebAssembly#getting-started)
    after rebuilding any modules.
    
    This is issue 48797 and CVE-2021-38297. Thanks to Ben Lubar for reporting this
    issue.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.130 2021/10/08 14:21:44 bsiegert Exp $
+# $NetBSD: version.mk,v 1.131 2021/10/08 14:47:44 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,7 +6,7 @@
 #
 .include "go-vars.mk"
 
-GO117_VERSION=	1.17.1
+GO117_VERSION=	1.17.2
 GO116_VERSION=	1.16.9
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
