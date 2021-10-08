3aa44854bceeca6012e616ca0abd52b7804d2ee6
adam <adam@pkgsrc.org>
2021-10-08 10:23:43 +0000

    postgresql14: added version 14.0
    PostgreSQL 14 contains many new features and enhancements, including:
    
    Stored procedures can now return data via OUT parameters.
    
    The SQL-standard SEARCH and CYCLE options for common table expressions have been implemented.
    
    Subscripting can now be applied to any data type for which it is a useful notation, not only arrays. In this release, the jsonb and hstore types have gained subscripting operators.
    
    Range types have been extended by adding multiranges, allowing representation of noncontiguous data ranges.
    
    Numerous performance improvements have been made for parallel queries, heavily-concurrent workloads, partitioned tables, logical replication, and vacuuming.
    
    B-tree index updates are managed more efficiently, reducing index bloat.
    
    VACUUM automatically becomes more aggressive, and skips inessential cleanup, if the database starts to approach a transaction ID wraparound condition.
    
    Extended statistics can now be collected on expressions, allowing better planning results for complex queries.
    
    libpq now has the ability to pipeline multiple queries, which can boost throughput over high-latency connections.

---
+++ databases/postgresql14/options.mk
@@ -0,0 +1,71 @@
+# $NetBSD: options.mk,v 1.1 2021/10/08 10:23:43 adam Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.postgresql14
+PKG_SUPPORTED_OPTIONS=	bonjour dtrace icu llvm gssapi ldap nls pam
+PKG_SUGGESTED_OPTIONS=	gssapi nls
+
+PLIST_VARS+=		gssapi llvm nls
+
+.include "../../mk/bsd.options.mk"
+
+# Bonjour support
+.if !empty(PKG_OPTIONS:Mbonjour)
+CONFIGURE_ARGS+=	--with-bonjour
+.  if ${OPSYS} != "Darwin"
+LIBS+=			-ldns_sd
+.  endif
+.  include "../../net/mDNSResponder/buildlink3.mk"
+.endif
+
+# Dtrace support
+.if !empty(PKG_OPTIONS:Mdtrace)
+CONFIGURE_ARGS+=	--enable-dtrace
+.endif
+
+# ICU collatium support
+.if !empty(PKG_OPTIONS:Micu)
+USE_TOOLS+=		pkg-config
+CONFIGURE_ARGS+=	--with-icu
+.  include "../../textproc/icu/buildlink3.mk"
+.endif
+
+# GSSAPI (Kerberos5) authentication for the PostgreSQL backend
+.if !empty(PKG_OPTIONS:Mgssapi)
+.  include "../../mk/krb5.buildlink3.mk"
+PLIST.gssapi=		yes
+CONFIGURE_ARGS+=       --with-gssapi
+.else
+CONFIGURE_ARGS+=       --without-gssapi
+.endif
+
+# LDAP authentication for the PostgreSQL backend
+.if !empty(PKG_OPTIONS:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ldap
+.endif
+
+# LLVM based JIT support
+.if !empty(PKG_OPTIONS:Mllvm)
+.  include "../../lang/llvm/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-llvm
+CONFIGURE_ENV+=		CLANG=${CC}	# XXX: make it be better
+PLIST.llvm=		yes
+.endif
+
+# NLS support
+.if !empty(PKG_OPTIONS:Mnls)
+USE_PKGLOCALEDIR=	yes
+CONFIGURE_ARGS+=	--enable-nls
+PLIST.nls=		yes
+BROKEN_GETTEXT_DETECTION=	yes
+.  include "../../devel/gettext-lib/buildlink3.mk"
+LIBS.SunOS+=		-lintl
+.else
+CONFIGURE_ARGS+=	--disable-nls
+.endif
+
+# PAM authentication for the PostgreSQL backend
+.if !empty(PKG_OPTIONS:Mpam)
+.  include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pam
+.endif
