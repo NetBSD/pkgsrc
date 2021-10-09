7239763d58f426567350554b586d2c4545b8af71
nia <nia@pkgsrc.org>
2021-10-09 14:47:24 +0000

    libaom: update to 3.1.3
    This includes various bug fixes and improvements to the encoder.

--- multimedia/libaom/patches/patch-build_cmake_version.cmake
+++ multimedia/libaom/patches/patch-build_cmake_version.cmake
@@ -1,13 +1,36 @@
-$NetBSD: patch-build_cmake_version.cmake,v 1.1 2018/11/07 13:08:29 ryoon Exp $
+$NetBSD: patch-build_cmake_version.cmake,v 1.2 2021/10/09 14:47:24 nia Exp $
 
---- build/cmake/version.cmake.orig	2018-10-01 03:20:05.000000000 +0000
+Write the correct version number in the aom.pc file.
+
+--- build/cmake/version.cmake.orig	2021-10-09 07:54:04.000000000 +0000
 +++ build/cmake/version.cmake
-@@ -36,7 +36,7 @@ if(EXISTS "${GIT_EXECUTABLE}")
- endif()
+@@ -23,28 +23,8 @@ include("${AOM_ROOT}/build/cmake/util.cm
  
- if("${aom_version}" STREQUAL "")
+ # Generate the version string for this run.
+ unset(aom_version)
+-if(EXISTS "${GIT_EXECUTABLE}")
+-  execute_process(COMMAND ${GIT_EXECUTABLE} --git-dir=${AOM_ROOT}/.git describe
+-                  OUTPUT_VARIABLE aom_version
+-                  ERROR_QUIET
+-                  RESULT_VARIABLE version_check_result)
+ 
+-  if(${version_check_result} EQUAL 0)
+-    string(STRIP "${aom_version}" aom_version)
+-
+-    # Remove the leading 'v' from the version string.
+-    string(FIND "${aom_version}" "v" v_pos)
+-    if(${v_pos} EQUAL 0)
+-      string(SUBSTRING "${aom_version}" 1 -1 aom_version)
+-    endif()
+-  else()
+-    set(aom_version "")
+-  endif()
+-endif()
+-
+-if("${aom_version}" STREQUAL "")
 -  set(aom_version "${AOM_ROOT}/CHANGELOG")
-+  set(aom_version "@PKGVERSION_NOREV@")
- endif()
+-endif()
++set(aom_version "@PKGVERSION_NOREV@")
  
  unset(last_aom_version)
+ set(version_file "${AOM_CONFIG_DIR}/config/aom_version.h")
