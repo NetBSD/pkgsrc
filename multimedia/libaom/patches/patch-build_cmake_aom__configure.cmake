6da9abef7bb02197d6e75b080814b42945b2495f
nia <nia@pkgsrc.org>
2021-10-09 14:47:24 +0000

    libaom: update to 3.1.3
    This includes various bug fixes and improvements to the encoder.

--- multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake
+++ multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake
@@ -1,39 +1,36 @@
-$NetBSD: patch-build_cmake_aom__configure.cmake,v 1.3 2018/11/11 18:03:17 leot Exp $
+$NetBSD: patch-build_cmake_aom__configure.cmake,v 1.4 2021/10/09 14:47:24 nia Exp $
 
 - Don't disable fortify just because it's a release build.
 - Set CPU correctly on SunOS.
 - Add support for NetBSD/*arm*
 
---- build/cmake/aom_configure.cmake.orig	2018-10-01 03:20:05.000000000 +0000
+--- build/cmake/aom_configure.cmake.orig	2021-10-09 07:54:04.000000000 +0000
 +++ build/cmake/aom_configure.cmake
-@@ -65,7 +65,11 @@ if(NOT AOM_TARGET_CPU)
+@@ -66,12 +66,16 @@ if(NOT AOM_TARGET_CPU)
+                     "      CMAKE_GENERATOR=${CMAKE_GENERATOR}\n")
      endif()
-   elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "i386" OR
-          "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86")
+   elseif(cpu_lowercase STREQUAL "i386" OR cpu_lowercase STREQUAL "x86")
 -    set(AOM_TARGET_CPU "x86")
+-  elseif(cpu_lowercase MATCHES "^arm" OR cpu_lowercase MATCHES "^mips")
 +    if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
 +      set(AOM_TARGET_CPU "x86")
 +    elseif(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
 +      set(AOM_TARGET_CPU "x86_64")
 +    endif()
-   elseif("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "^arm" OR
-          "${CMAKE_SYSTEM_PROCESSOR}" MATCHES "^mips")
-     set(AOM_TARGET_CPU "${CMAKE_SYSTEM_PROCESSOR}")
-@@ -151,7 +155,8 @@ elseif("${AOM_TARGET_CPU}" MATCHES "arm"
-   if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
-     set(AS_EXECUTABLE as)
-     set(AOM_AS_FLAGS -arch ${AOM_TARGET_CPU} -isysroot ${CMAKE_OSX_SYSROOT})
--  elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
-+  elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux" OR
-+         "${AOM_TARGET_SYSTEM}" STREQUAL "NetBSD")
-     if(NOT AS_EXECUTABLE)
-       set(AS_EXECUTABLE as)
-     endif()
-@@ -290,9 +295,6 @@ else()
++  elseif(cpu_lowercase MATCHES "^(arm|earm)" OR cpu_lowercase MATCHES "^mips")
+     set(AOM_TARGET_CPU "${cpu_lowercase}")
+   elseif(cpu_lowercase MATCHES "aarch64")
+     set(AOM_TARGET_CPU "arm64")
+-  elseif(cpu_lowercase MATCHES "^ppc")
++  elseif(cpu_lowercase MATCHES "^(ppc|powerpc)")
+     set(AOM_TARGET_CPU "ppc")
+   else()
+     message(WARNING "The architecture ${CMAKE_SYSTEM_PROCESSOR} is not "
+@@ -321,9 +325,6 @@ else()
      add_compiler_flag_if_supported("-Werror")
    endif()
  
--  if("${CMAKE_BUILD_TYPE}" MATCHES "Rel")
+-  if(build_type_lowercase MATCHES "rel")
 -    add_compiler_flag_if_supported("-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0")
 -  endif()
    add_compiler_flag_if_supported("-D_LARGEFILE_SOURCE")
