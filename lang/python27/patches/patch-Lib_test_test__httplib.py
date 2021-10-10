9bde62f94b3beb1c00a1276a487f3b261752a7a0
gutteridge <gutteridge@pkgsrc.org>
2021-10-10 03:00:59 +0000

    python27: fix various security issues
    Addresses CVE-2020-27619, CVE-2021-3177, CVE-2021-3733, CVE-2021-3737
    and CVE-2021-23336. Patches mostly sourced via Fedora.

--- lang/python27/patches/patch-Lib_test_test__httplib.py
+++ lang/python27/patches/patch-Lib_test_test__httplib.py
@@ -1,9 +1,13 @@
-$NetBSD: patch-Lib_test_test__httplib.py,v 1.3 2020/09/27 14:57:22 leot Exp $
+$NetBSD: patch-Lib_test_test__httplib.py,v 1.4 2021/10/10 03:00:59 gutteridge Exp $
 
 bpo-39603 (CVE-2020-26116): header injection via HTTP method
 
-taken from:
+Taken from:
 https://gitweb.gentoo.org/fork/cpython.git/commit/?h=gentoo-2.7-vanilla&id=138e2caeb4827ccfd1eaff2cf63afb79dfeeb3c4
+
+Fix CVE-2021-3737: http client infinite line reading (DoS) after a HTTP 100 Continue
+Via Fedora:
+https://src.fedoraproject.org/rpms/python2.7/blob/rawhide/f/00368-CVE-2021-3737.patch
 
 --- Lib/test/test_httplib.py.orig	2020-04-19 21:13:39.000000000 +0000
 +++ Lib/test/test_httplib.py
@@ -34,3 +38,18 @@ https://gitweb.gentoo.org/fork/cpython.git/commit/?h=gentoo-2.7-vanilla&id=138e2
  
  class BasicTest(TestCase):
      def test_status_lines(self):
+@@ -655,6 +675,14 @@ class BasicTest(TestCase):
+         resp = httplib.HTTPResponse(FakeSocket(body))
+         self.assertRaises(httplib.LineTooLong, resp.begin)
+ 
++    def test_overflowing_header_limit_after_100(self):
++        body = (
++            'HTTP/1.1 100 OK\r\n'
++            'r\n' * 32768
++        )
++        resp = httplib.HTTPResponse(FakeSocket(body))
++        self.assertRaises(httplib.HTTPException, resp.begin)
++
+     def test_overflowing_chunked_line(self):
+         body = (
+             'HTTP/1.1 200 OK\r\n'
