3a1f22a30bbfc304713115b9f170d1b196add0cf
nia <nia@pkgsrc.org>
2021-10-17 10:06:28 +0000

    pkgin: Prefer gzip on more 32-bit archs.

--- pkgtools/pkgin/options.mk
+++ pkgtools/pkgin/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2021/10/12 12:46:14 jperkin Exp $
+# $NetBSD: options.mk,v 1.4 2021/10/17 10:06:28 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pkgin
 PKG_SUPPORTED_OPTIONS=	pkgin-prefer-gzip
@@ -9,7 +9,7 @@ PKG_SUPPORTED_OPTIONS=	pkgin-prefer-gzip
 # Prefer pkg_summary.gz instead of .bz2 or .xz on hosts that are likely to be
 # slower or have limited memory where the higher compression can cause issues.
 #
-PROBABLY_SLOW_ARCHS=	alpha hppa m68k sparc vax
+PROBABLY_SLOW_ARCHS=	alpha hppa i386 m68k powerpc sh3eb sh3el sparc vax
 .if !empty(PROBABLY_SLOW_ARCHS:M${MACHINE_ARCH})
 PKG_SUGGESTED_OPTIONS=	pkgin-prefer-gzip
 .endif
