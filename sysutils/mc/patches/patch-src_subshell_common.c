74d583d45e16c828d3bea594692db0eefdc94d1e
nia <nia@pkgsrc.org>
2021-10-18 13:36:45 +0000

    mc: 4.8.27
    Version 4.8.27
    
    - Core
    
        * Minimal version of Autoconf is 2.64
        * Minimal version of Automake is 1.12
        * Minimal version of Gettext is 0.18.2
        * Minimal version of libssh2 is 1.2.8
        * Reimplement version detection
        * Significantly reduce rebuilt time after version change
        * Drop automatic migration of configuration from ~/.mc to XDG-based directories
        * zsh: support custom configuration file: ~/.local/share/mc/.zshrc
        * Widgets: implement WST_VISIBLE state to show/hide widgets
        * "Find File": add "Follow symlinks" option
    
    - VFS
    
        * extfs: support unrar-6
        * extfs: support official 7z binary
        * ftpfs: apply file list parser from lftp project
    
    - Editor
    
        * Word completion: get candidates from all open files
        * etags: get rid of hardcoded list length and window width
        * Update syntax files:
            - Python
        * Add syntax highlighting:
            - Verilog and SystemVerilog header files
            - JSON
            - openrc-run scripts
    
    - Misc
    
        * Code clean up
        * Filehighlight of c++ and h++ files as sources
        * Filehighlight of JSON files as documents
        * Support of alacritty terminal emulator (https://github.com/alacritty/alacritty)
        * Support of foot terminal emulator (https://codeberg.org/dnkl/foot)
        * Support of (alt+)shift+arrow keys in st terminal emulator (st.suckless.org)
        * Mouse support in screen: don't check $DISPLAY variable
        * mc.ext: support fb2 e-books
        * ext.d: use mediainfo to view info about various media files
        * Remove OS/distro-specific package-related stuff from source tree
    
    - Fixes
    
        * FTBFS against NCurses on OS X 10.9.5
        * Segfault on dialog before panels get visible
        * Crash if shadow is out of screen (build against NCurses)
        * Crash in search
        * Crash on startup with enabled subshell in FreeBSD (workaround)
        * Hang on start randomly with zsh as subshell
        * If command line is invisible it's partially displayed
        * Broken handling of zip archives
        * Broken handling of jar files as zip archives
        * Timestamps of symlinks, sockets, fifos, etc are not preserved after copy/move
        * %view action in the user menu doesn't work on no-exec filesystem
        * Hardlinks are not colored by file type or extension
        * mcedit: silent macro makes terminal disrupted
        * mcedit: disrupting of TAGS file path
        * vfs: unable to browse compressed tar archives
        * sftpfs vfs: CVE-2021-36370: server fingerprint isn't verified (discovered by AUT-milCERT during an audit of open source software)
        * ftpfs vfs: month of file is always January
        * Tests: log files are written by libcheck and automake simultaneously

--- sysutils/mc/patches/patch-src_subshell_common.c
+++ sysutils/mc/patches/patch-src_subshell_common.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_subshell_common.c,v 1.2 2019/01/18 22:28:38 bsiegert Exp $
+$NetBSD: patch-src_subshell_common.c,v 1.3 2021/10/18 13:36:45 nia Exp $
 
 Report error from read since tcsetattr can replace errno.
 Recognize /bin/sh as valid shell and don't complain about it.
 
---- src/subshell/common.c.orig	2018-12-28 19:35:25.000000000 +0000
+--- src/subshell/common.c.orig	2021-08-01 16:45:58.000000000 +0000
 +++ src/subshell/common.c
-@@ -317,6 +317,7 @@ init_subshell_child (const char *pty_nam
+@@ -345,6 +345,7 @@ init_subshell_child (const char *pty_nam
  
      case SHELL_ASH_BUSYBOX:
      case SHELL_DASH:
 +    case SHELL_SH:
          /* Do we have a custom init file ~/.local/share/mc/ashrc? */
-         init_file = mc_config_get_full_path ("ashrc");
+         init_file = mc_config_get_full_path (MC_ASHRC_FILE);
  
-@@ -510,7 +511,7 @@ static gboolean
+@@ -740,7 +741,7 @@ static gboolean
  feed_subshell (int how, gboolean fail_on_error)
  {
      fd_set read_set;            /* For 'select' */
@@ -22,7 +22,24 @@ Recognize /bin/sh as valid shell and don't complain about it.
      int i;                      /* Loop counter */
  
      struct timeval wtime;       /* Maximum time we wait for the subshell */
-@@ -565,19 +566,20 @@ feed_subshell (int how, gboolean fail_on
+@@ -774,6 +775,7 @@ feed_subshell (int how, gboolean fail_on
+ 
+         if (select (maxfdp + 1, &read_set, NULL, NULL, wptr) == -1)
+         {
++            serrno = errno;
+             /* Despite using SA_RESTART, we still have to check for this */
+             if (errno == EINTR)
+             {
+@@ -784,7 +786,7 @@ feed_subshell (int how, gboolean fail_on
+             }
+             tcsetattr (STDOUT_FILENO, TCSANOW, &shell_mode);
+             fprintf (stderr, "select (FD_SETSIZE, &read_set...): %s\r\n",
+-                     unix_error_string (errno));
++                     unix_error_string (serrno));
+             exit (EXIT_FAILURE);
+         }
+ 
+@@ -797,19 +799,20 @@ feed_subshell (int how, gboolean fail_on
              /* for (i=0; i<5; ++i)  * FIXME -- experimental */
          {
              bytes = read (mc_global.tty.subshell_pty, pty_buffer, sizeof (pty_buffer));
@@ -46,3 +63,27 @@ Recognize /bin/sh as valid shell and don't complain about it.
                  exit (EXIT_FAILURE);
  #endif
              }
+@@ -827,9 +830,10 @@ feed_subshell (int how, gboolean fail_on
+             bytes = read (subshell_pipe[READ], subshell_cwd, sizeof (subshell_cwd));
+             if (bytes <= 0)
+             {
++                serrno = errno;
+                 tcsetattr (STDOUT_FILENO, TCSANOW, &shell_mode);
+                 fprintf (stderr, "read (subshell_pipe[READ]...): %s\r\n",
+-                         unix_error_string (errno));
++                         unix_error_string (serrno));
+                 exit (EXIT_FAILURE);
+             }
+ 
+@@ -854,9 +858,10 @@ feed_subshell (int how, gboolean fail_on
+             bytes = read (STDIN_FILENO, pty_buffer, sizeof (pty_buffer));
+             if (bytes <= 0)
+             {
++                serrno = errno;
+                 tcsetattr (STDOUT_FILENO, TCSANOW, &shell_mode);
+                 fprintf (stderr,
+-                         "read (STDIN_FILENO, pty_buffer...): %s\r\n", unix_error_string (errno));
++                         "read (STDIN_FILENO, pty_buffer...): %s\r\n", unix_error_string (serrno));
+                 exit (EXIT_FAILURE);
+             }
+ 
