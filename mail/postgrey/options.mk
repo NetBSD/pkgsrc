cb864e47c5e357cff14fc6a04b02158c08649994
tm <tm@pkgsrc.org>
2021-10-20 16:15:52 +0000

    mail/postgrey: Update to 1.37
    * 2016-09-22: version 1.37
       - added initial test suite
       - testing using travis-ci (https://travis-ci.org/schweikert/postgrey)
       - removed IP pool-detection code for --lookup-by-net, because it matched
         also the naming of some big hosters like facebook (#32, Michal Petrucha,
         Andrew Ayer, Jon Sailor)
       - fix early logging of errors and warnings to syslog
       - simplified IP matching code
       - added support for IPv6 whitelists with netmask
       - add network-range based whitelist for Office 365 (Holger Stember)
       - updated whitelist

--- mail/postgrey/options.mk
+++ mail/postgrey/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2015/10/12 11:10:34 fhajny Exp $
+# $NetBSD: options.mk,v 1.3 2021/10/20 16:15:52 tm Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.postgrey
 PKG_SUPPORTED_OPTIONS=	postgrey-targrey
@@ -6,8 +6,8 @@ PKG_SUPPORTED_OPTIONS=	postgrey-targrey
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mpostgrey-targrey)
-# See http://k2net.hakuba.jp/targrey/index.en.html
-PATCHFILES+=		targrey-0.31-postgrey-1.34.patch
-PATCH_SITES=		http://k2net.hakuba.jp/pub/
+ See http://k2net.hakuba.jp/targrey/index.en.html
+PATCHFILES+=		targrey-0.31-postgrey-1.37.patch
+PATCH_SITES=		${MASTER_SITE_LOCAL}
 PATCH_DIST_STRIP=	-p0
 .endif
