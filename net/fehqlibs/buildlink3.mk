616105db652471011f50457655639fe4fb885793
schmonz <schmonz@pkgsrc.org>
2021-10-21 22:28:42 +0000

    Update to 19. From the changelog:
    - Provided socket_accept4 again to avoid IPv4-mapped IPv6 adress
      binding problems.

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2021/08/04 13:32:07 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/10/21 22:28:42 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
 .if !defined(FEHQLIBS_BUILDLINK3_MK)
 FEHQLIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fehqlibs+=	fehqlibs>=18
+BUILDLINK_API_DEPENDS.fehqlibs+=	fehqlibs>=19
 BUILDLINK_PKGSRCDIR.fehqlibs?=		../../net/fehqlibs
 BUILDLINK_INCDIRS.fehqlibs+=		include/qlibs
 BUILDLINK_LIBDIRS.fehqlibs+=		lib/qlibs
