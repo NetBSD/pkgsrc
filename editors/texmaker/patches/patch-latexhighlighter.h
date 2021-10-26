899b8d0b614b6c2dc85a20d2814655f96fdde834
nros <nros@pkgsrc.org>
2021-10-26 17:56:54 +0000

    Update editors/texmaker to version 5.1.2
    pkgsrc changes:
    qt5script is not used anymore, this means that
    only qt5declarative is needed as a dependency.
    USE_LIBTOOL line removed since qt5 doesn't
    use libtool.
    
    Changes from ChangeLog:
    
    version 5.1.1 -> 5.1.2 :
    - a bug related to replace action with a regular expression has been fixed
    - a bug related to mouse wheel events in the pdf viewer has been fixed
    - support for non integer dpi scale factor has been added
      (such as Windows using 125% or 150% scale on laptops)
    version 5.1.0 -> 5.1.1 :
    - a critical bug on the windows version compiled with msvc 2019 while
      inserting a symbol has been fixed
    - a trouble in the syntax highlighting with the verb latex command
      has been fixed
    version 5.0.4 -> 5.1.0 :
    - compatibility with Qt6 has been added. Texmaker can now be compiled
      with Qt5 (>=5.7) and Qt6 (>= 6.2). This ensures that Texmaker will
      be available for, at least, the next ten years.
    - the internal pdf engine (from chromium) has been updated
    - hunspell has been updated to 1.7.O
    - synctex has been updated to 1.21
    - default windows paths are now searched in the windows registry and path
    - a potential trouble with user commands beginning with a space has been
      fixed
    - the texmaker.appdata.xml file has been updated
    - GTK-based theming for systems without a "GTK+" theme has been added
    - a bug with the use of latexmk with the use of the "output directory"
      option has been fixed
    - the pdf viewer is replaced in the default position on the main screen
      if it has been closed on a screen which is no more connected
    - the scale in the pdf viewer is now automatically adapted after
      closing/restoring the list of pages panel
    - layout has been improved in the find/replace widgets
    - the obsolete use of QTScript has been replaced by QJSEngine
    - the ability to change the interface font has been expanded to windows
      and macos
    - the ability to check spelling/grammar the text extracted from a pdf
      page in the internal pdf viewer is back (via online languagetool)
    - a link to import more spell dictionaries has been added to
      the configuration dialog

--- editors/texmaker/patches/patch-latexhighlighter.h
+++ editors/texmaker/patches/patch-latexhighlighter.h
@@ -1,5 +1,7 @@
-$NetBSD: patch-latexhighlighter.h,v 1.1 2019/09/05 08:07:25 nros Exp $
+$NetBSD: patch-latexhighlighter.h,v 1.2 2021/10/26 17:56:54 nros Exp $
+
 * use pkgsrc (system) hunspell, from openbsd ports
+
 --- latexhighlighter.h.orig	2019-09-03 13:23:28.000000000 +0000
 +++ latexhighlighter.h
 @@ -20,7 +20,7 @@
