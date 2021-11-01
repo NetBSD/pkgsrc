99faf17aed3f81d1b24b3c38115e8c4b17cdab41
schmonz <schmonz@pkgsrc.org>
2021-11-01 20:39:18 +0000

    Bump BUILDLINK_API_DEPENDS to match current version. Found via p5-highlight 4.1 building against libhighlight 3.x but giving runtime errors on wiki.n.o. Spotted by mspo@.

--- textproc/libhighlight/buildlink3.mk
+++ textproc/libhighlight/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/23 05:16:43 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/11/01 20:39:18 schmonz Exp $
 
 BUILDLINK_TREE+=	libhighlight
 
 .if !defined(LIBHIGHLIGHT_BUILDLINK3_MK)
 LIBHIGHLIGHT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libhighlight+=	libhighlight>=3.44
+BUILDLINK_API_DEPENDS.libhighlight+=	libhighlight>=4.1
 BUILDLINK_PKGSRCDIR.libhighlight?=	../../textproc/libhighlight
 .endif	# LIBHIGHLIGHT_BUILDLINK3_MK
 
