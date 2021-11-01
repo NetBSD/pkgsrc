a5ef0f797ae383fb195d7178bd7dd8833c22bcb6
wiz <wiz@pkgsrc.org>
2021-11-01 21:47:01 +0000

    net/libnpupnp: import libnpupnp-4.1.5
    npupnp is an UPnP library derived from the venerable pupnp, based
    on its 1.6.x branch (around 1.6.25).
    
    Main modifications:
    
     - Support multiple network interfaces
     - Support multiple root devices (already in the late pupnp versions).
     - Use libcurl for HTTP client functions.

---
+++ net/libnpupnp/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/11/01 21:47:01 wiz Exp $
+
+BUILDLINK_TREE+=	libnpupnp
+
+.if !defined(LIBNPUPNP_BUILDLINK3_MK)
+LIBNPUPNP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libnpupnp+=	libnpupnp>=4.1.5
+BUILDLINK_PKGSRCDIR.libnpupnp?=		../../net/libnpupnp
+
+.include "../../textproc/expat/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.include "../../www/libmicrohttpd/buildlink3.mk"
+.endif	# LIBNPUPNP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libnpupnp
