bb8877360588f46d5dfe71d4518d2e2ecbaccd1b
ryoon <ryoon@pkgsrc.org>
2021-11-02 13:48:03 +0000

    sfsexp: Fix version number in buildlink3.mk
    Before importing, I had forgotten to fix harf-baked buildlink3.mk.

--- devel/sfsexp/buildlink3.mk
+++ devel/sfsexp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/11/01 14:15:42 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/11/02 13:48:03 ryoon Exp $
 
 BUILDLINK_TREE+=	sfsexp
 
 .if !defined(SFSEXP_BUILDLINK3_MK)
 SFSEXP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.sfsexp+=	sfsexp>=1.3.1pre20210719
+BUILDLINK_API_DEPENDS.sfsexp+=	sfsexp>=1.3.1pre20211025
 BUILDLINK_PKGSRCDIR.sfsexp?=	../../devel/sfsexp
 .endif	# SFSEXP_BUILDLINK3_MK
 
