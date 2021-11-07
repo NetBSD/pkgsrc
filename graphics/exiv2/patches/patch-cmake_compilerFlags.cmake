1fa86c56e2e333b75d52620f10c8953f0ce74dec
wiz <wiz@pkgsrc.org>
2021-11-07 12:09:12 +0000

    exiv2: update to 0.27.5.
    Exiv2 v0.27.5 Features
    
    1. BMFF bug fixes and CR3 Previews
    2. Security fixes
    3. libFuzzer target _(for improved security testing)_
    4. Minor bugs and fixes

--- graphics/exiv2/patches/patch-cmake_compilerFlags.cmake
+++ graphics/exiv2/patches/patch-cmake_compilerFlags.cmake
@@ -1,13 +1,13 @@
-$NetBSD: patch-cmake_compilerFlags.cmake,v 1.4 2021/06/28 07:50:44 wiz Exp $
+$NetBSD: patch-cmake_compilerFlags.cmake,v 1.5 2021/11/07 12:09:12 wiz Exp $
 
 Gcc on some platforms does not support -fcf-protection.
 
---- cmake/compilerFlags.cmake.orig	2021-06-15 14:08:36.000000000 +0000
+--- cmake/compilerFlags.cmake.orig	2021-10-21 17:01:10.000000000 +0000
 +++ cmake/compilerFlags.cmake
-@@ -26,7 +26,9 @@ if ( MINGW OR UNIX OR MSYS ) # MINGW, Li
-         # This fails under Fedora - MinGW - Gcc 8.3
-         if (NOT (MINGW OR CYGWIN OR CMAKE_HOST_SOLARIS))
-             check_cxx_compiler_flag(-fstack-clash-protection HAS_FSTACK_CLASH_PROTECTION)
+@@ -28,7 +28,9 @@ if ( MINGW OR UNIX OR MSYS ) # MINGW, Li
+             if (NOT APPLE) # Don't know why this isn't working correctly on Apple with M1 processor
+                 check_cxx_compiler_flag(-fstack-clash-protection HAS_FSTACK_CLASH_PROTECTION)
+             endif()
 +            if (NOT (${CMAKE_SYSTEM_PROCESSOR} MATCHES "sparc.*|aarch64.*"))
              check_cxx_compiler_flag(-fcf-protection HAS_FCF_PROTECTION)
 +            endif()
