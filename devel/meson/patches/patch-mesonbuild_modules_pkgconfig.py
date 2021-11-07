f92b6166cd49c72faec572baee946684cbabc6de
adam <adam@pkgsrc.org>
2021-11-07 13:23:05 +0000

    meson: updated to 0.60.1
    0.60.0:
    
    run_target can now be used as a dependency
    The Python Modules dependency method no longer accepts positional arguments
    Override python installation paths
    New subprojects packagefiles subcommand
    Deprecated project options
    More efficient static linking of uninstalled libraries
    gnome.yelp variadic argument deprecation
    static keyword argument to meson.override_dependency()
    dependency() sets default_library on fallback subproject
    install_emptydir function
    Cython can now transpile to C++ as an intermediate language
    New custom dependency for iconv
    Unknown options are now always fatal
    Install DESTDIR relative to build directory
    Java Module
    Link tests can use sources for a different compiler
    Relax restrictions of str.join()
    Improvements for the Rustc compiler
    The qt modules now accept generated outputs as inputs for qt.compile_*
    Waf support in external-project module
    Comparing two objects with different types is now an error
    Installation tags
    Compiler.unittest_args has been removed
    Dependencies with multiple names
    i18n module now returns gettext targets
    Added support for CLA sources when cross-compiling with the C2000 toolchain
    Support for clippy-driver as a rustc wrapper
    Force Visual Studio environment activation
    MSVC compiler now assumes UTF-8 source code by default
    Add support for find_library in Emscripten
    Optional custom_target() name

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,19 +1,22 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.3 2021/09/27 18:50:06 adam Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.4 2021/11/07 13:23:05 adam Exp $
 
 Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
 a condition to match FreeBSD path norm for pkg-config files.
 
---- mesonbuild/modules/pkgconfig.py.orig	2021-08-18 11:22:33.000000000 +0000
+--- mesonbuild/modules/pkgconfig.py.orig	2021-11-02 20:00:40.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -549,10 +549,7 @@ class PkgConfigModule(ExtensionModule):
+@@ -545,12 +545,8 @@ class PkgConfigModule(ExtensionModule):
          pcfile = filebase + '.pc'
-         pkgroot = kwargs.get('install_dir', default_install_dir)
+         pkgroot = pkgroot_name = kwargs.get('install_dir', default_install_dir)
          if pkgroot is None:
 -            if mesonlib.is_freebsd():
 -                pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('prefix')), 'libdata', 'pkgconfig')
+-                pkgroot_name = os.path.join('{prefix}', 'libdata', 'pkgconfig')
 -            else:
 -                pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
+-                pkgroot_name = os.path.join('{libdir}', 'pkgconfig')
 +            pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
++            pkgroot_name = os.path.join('{libdir}', 'pkgconfig')
          if not isinstance(pkgroot, str):
              raise mesonlib.MesonException('Install_dir must be a string.')
          self._generate_pkgconfig_file(state, deps, subdirs, name, description, url,
