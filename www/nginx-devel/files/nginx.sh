88b416b08b589a44fdd3a4b44bbdab2132b960b9
khorben <khorben@pkgsrc.org>
2021-11-08 01:07:37 +0000

    nginx-devel: let the RC script work unprivileged
    This takes advantage of the introduction of the SYSCONFBASE variable.
    Tested on NetBSD/amd64.
    
    While there, add support for the "configtest" command in the RC script.
    
    Bumps PKGREVISION.

--- www/nginx-devel/files/nginx.sh
+++ www/nginx-devel/files/nginx.sh
@@ -1,11 +1,15 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: nginx.sh,v 1.1 2013/02/22 17:06:54 imil Exp $
+# $NetBSD: nginx.sh,v 1.2 2021/11/08 01:07:37 khorben Exp $
 #
 # PROVIDE: nginx
 # REQUIRE: DAEMON
+#
+# You will need to set some variables in @SYSCONFBASE@/rc.conf to start nginx:
+#
+# nginx=YES
 
-. /etc/rc.subr
+$_rc_subr_loaded . @SYSCONFBASE@/rc.subr
 
 name="nginx"
 rcvar=${name}
@@ -13,7 +17,17 @@ command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/${name}.conf"
 pidfile="@VARBASE@/run/${name}.pid"
 start_precmd="ulimit -n 2048"
-extra_commands="reload"
+extra_commands="configtest reload"
+configtest_cmd="nginx_configtest"
+
+nginx_configtest()
+{
+	if [ ! -f ${required_files} ]; then
+		warn "${required_files} does not exist."
+		return 1;
+	fi
+	${command} -t -c ${required_files}
+}
 
 load_rc_config $name
 run_rc_command "$1"
