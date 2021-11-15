96d17e09f2e03677e75392f5adef0ac5a78a5d0c
wiz <wiz@pkgsrc.org>
2021-11-15 22:01:15 +0000

    *: update gstreamer1 and plugins and related packages to 1.18.5
    Based on the wip packages by nia and myself.

--- multimedia/gst-plugins1-ugly/buildlink3.mk
+++ multimedia/gst-plugins1-ugly/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/03/08 16:47:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/11/15 22:01:21 wiz Exp $
 
 BUILDLINK_TREE+=	gst-plugins1-ugly
 
 .if !defined(GST_PLUGINS1_UGLY_BUILDLINK3_MK)
 GST_PLUGINS1_UGLY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gst-plugins1-ugly+=	gst-plugins1-ugly>=1.0.3
-BUILDLINK_ABI_DEPENDS.gst-plugins1-ugly+=	gst-plugins1-ugly>=1.16.2nb1
+BUILDLINK_API_DEPENDS.gst-plugins1-ugly+=	gst-plugins1-ugly>=1.18.4
 BUILDLINK_PKGSRCDIR.gst-plugins1-ugly?=		../../multimedia/gst-plugins1-ugly
 
 .include "../../multimedia/gstreamer1/buildlink3.mk"
-.include "../../multimedia/gst-plugins1-base/buildlink3.mk"
 .endif # GST_PLUGINS1_UGLY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gst-plugins1-ugly
