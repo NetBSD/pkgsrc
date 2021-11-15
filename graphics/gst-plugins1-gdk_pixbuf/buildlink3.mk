a76603db9b1b403dd23daa4db25b895ca04c9141
wiz <wiz@pkgsrc.org>
2021-11-15 22:01:15 +0000

    *: update gstreamer1 and plugins and related packages to 1.18.5
    Based on the wip packages by nia and myself.

--- graphics/gst-plugins1-gdk_pixbuf/buildlink3.mk
+++ graphics/gst-plugins1-gdk_pixbuf/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/03/08 16:47:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/11/15 22:01:18 wiz Exp $
 
 BUILDLINK_TREE+=	gst-plugins1-gdk_pixbuf
 
 .if !defined(GST_PLUGINS1_GDK_PIXBUF_BUILDLINK3_MK)
 GST_PLUGINS1_GDK_PIXBUF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gst-plugins1-gdk_pixbuf+=	gst-plugins1-gdk_pixbuf>=1.16.0
-BUILDLINK_ABI_DEPENDS.gst-plugins1-gdk_pixbuf?=	gst-plugins1-gdk_pixbuf>=1.16.2nb1
+BUILDLINK_API_DEPENDS.gst-plugins1-gdk_pixbuf+=	gst-plugins1-gdk_pixbuf>=1.18.0
 BUILDLINK_PKGSRCDIR.gst-plugins1-gdk_pixbuf?=	../../graphics/gst-plugins1-gdk_pixbuf
 
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
--- multimedia/gstreamer1/buildlink3.mk
+++ multimedia/gstreamer1/buildlink3.mk
@@ -1,21 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/03/08 16:47:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/11/15 22:01:21 wiz Exp $
 
 BUILDLINK_TREE+=	gstreamer1
 
 .if !defined(GSTREAMER1_BUILDLINK3_MK)
 GSTREAMER1_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gstreamer1+=	gstreamer1>=1.0.0
-BUILDLINK_ABI_DEPENDS.gstreamer1+=	gstreamer1>=1.16.2nb1
+BUILDLINK_API_DEPENDS.gstreamer1+=	gstreamer1>=1.18.4
 BUILDLINK_PKGSRCDIR.gstreamer1?=	../../multimedia/gstreamer1
 
 pkgbase := gstreamer1
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.gstreamer1:Mgstcheck)
 .include "../../devel/check/buildlink3.mk"
-.endif
-
 .include "../../devel/glib2/buildlink3.mk"
 .endif # GSTREAMER1_BUILDLINK3_MK
 
