391f88bd2935f2934ce0c8b0b1fbbaddce888aba
wiz <wiz@pkgsrc.org>
2021-11-16 08:45:14 +0000

    gst-plugins1-gtk: update to 1.18.5

--- multimedia/gst-plugins1-gtk/buildlink3.mk
+++ multimedia/gst-plugins1-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2021/11/15 22:53:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2021/11/16 08:45:14 wiz Exp $
 
 BUILDLINK_TREE+=	gst-plugins1-gtk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gst-plugins1-gtk
 GST_PLUGINS1_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gst-plugins1-gtk+=	gst-plugins1-gtk>=1.16.0
-BUILDLINK_ABI_DEPENDS.gst-plugins1-gtk?=		gst-plugins-good>=1.18.5nb3
+BUILDLINK_ABI_DEPENDS.gst-plugins1-gtk?=	gst-plugins1-gtk>=1.18.5nb3
 BUILDLINK_PKGSRCDIR.gst-plugins1-gtk?=		../../multimedia/gst-plugins1-gtk
 
 .include "../../multimedia/gst-plugins1-base/buildlink3.mk"
