8bc0cc30f75558b8bfede51fa24d4655b18f8c79
adam <adam@pkgsrc.org>
2021-11-16 14:23:50 +0000

    python39 py39-html-docs: updated to 3.9.9
    Python 3.9.9 final
    
    Core and Builtins
    bpo-45738: Fix computation of error location for invalid continuation characters in the parser. Patch by Pablo Galindo.
    
    Library
    bpo-45235: Reverted an argparse bugfix that caused regression in the handling of default arguments for subparsers. This prevented leaf level arguments from taking precedence over root level arguments.
    bpo-45765: In importlib.metadata, fix distribution discovery for an empty path.
    bpo-45644: In-place JSON file formatting using python3 -m json.tool infile infile now works correctly, previously it left the file empty. Patch by Chris Wesseling.
    
    Documentation
    bpo-45772: socket.socket documentation is corrected to a class from a function.
    bpo-45392: Update the docstring of the type built-in to remove a redundant line and to mention keyword arguments for the constructor.
    
    Windows
    bpo-45732: Updates bundled Tcl/Tk to 8.6.12.
    bpo-45720: Internal reference to shlwapi.dll was dropped to help improve startup time. This DLL will no longer be loaded at the start of every Python process.

--- lang/python39/dist.mk
+++ lang/python39/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.9 2021/11/06 12:24:34 adam Exp $
+# $NetBSD: dist.mk,v 1.10 2021/11/16 14:23:50 adam Exp $
 
-PY_DISTVERSION=	3.9.8
+PY_DISTVERSION=	3.9.9
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python39/distinfo
