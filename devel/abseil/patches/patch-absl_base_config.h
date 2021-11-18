2e99aa46c60c0afb8cb59910a55659b38ec056fe
adam <adam@pkgsrc.org>
2021-11-18 13:57:34 +0000

    abseil: updated to 20211102.0
    Abseil LTS 20211102
    
    What's New:
    
    absl::Cord is now implemented as a b-tree. The new implementation offers improved performance in most workloads.
    absl::SimpleHexAtoi() has been added to strings library for parsing hexadecimal strings.
    
    Breaking Changes:
    
    Bazel builds now depend on the bazelbuild/platforms repository. See Abseil's WORKSPACE file for an example of how to add this dependency.

--- devel/abseil/patches/patch-absl_base_config.h
+++ devel/abseil/patches/patch-absl_base_config.h
@@ -1,22 +1,22 @@
-$NetBSD: patch-absl_base_config.h,v 1.2 2021/04/30 10:32:17 adam Exp $
+$NetBSD: patch-absl_base_config.h,v 1.3 2021/11/18 13:57:34 adam Exp $
 
 Add support for NetBSD.
 
---- absl/base/config.h.orig	2021-04-20 16:55:14.000000000 +0000
+--- absl/base/config.h.orig	2021-11-03 15:26:14.000000000 +0000
 +++ absl/base/config.h
-@@ -370,6 +370,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
+@@ -411,6 +411,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
  #ifdef ABSL_HAVE_MMAP
  #error ABSL_HAVE_MMAP cannot be directly set
- #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) ||   \
+ #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || \
 +    defined(__NetBSD__) || \
-     defined(__ros__) || defined(__native_client__) || defined(__asmjs__) || \
-     defined(__wasm__) || defined(__Fuchsia__) || defined(__sun) || \
-     defined(__ASYLO__) || defined(__myriad2__)
-@@ -383,6 +384,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
+     defined(_AIX) || defined(__ros__) || defined(__native_client__) ||    \
+     defined(__asmjs__) || defined(__wasm__) || defined(__Fuchsia__) ||    \
+     defined(__sun) || defined(__ASYLO__) || defined(__myriad2__) ||       \
+@@ -425,6 +426,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
  #ifdef ABSL_HAVE_PTHREAD_GETSCHEDPARAM
  #error ABSL_HAVE_PTHREAD_GETSCHEDPARAM cannot be directly set
  #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || \
 +    defined(__NetBSD__) || \
-     defined(__ros__)
+     defined(_AIX) || defined(__ros__)
  #define ABSL_HAVE_PTHREAD_GETSCHEDPARAM 1
  #endif
