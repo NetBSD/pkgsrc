4b2d44c67543264e286a84b5b4be4f9f5e002345
adam <adam@pkgsrc.org>
2021-11-18 13:57:34 +0000

    abseil: updated to 20211102.0
    Abseil LTS 20211102
    
    What's New:
    
    absl::Cord is now implemented as a b-tree. The new implementation offers improved performance in most workloads.
    absl::SimpleHexAtoi() has been added to strings library for parsing hexadecimal strings.
    
    Breaking Changes:
    
    Bazel builds now depend on the bazelbuild/platforms repository. See Abseil's WORKSPACE file for an example of how to add this dependency.

---
+++ devel/abseil/patches/patch-absl_debugging_internal_vdso__support.cc
@@ -0,0 +1,31 @@
+$NetBSD: patch-absl_debugging_internal_vdso__support.cc,v 1.1 2021/11/18 13:57:34 adam Exp $
+
+Support NetBSD.
+
+--- absl/debugging/internal/vdso_support.cc.orig	2021-11-18 10:00:05.006467401 +0000
++++ absl/debugging/internal/vdso_support.cc
+@@ -50,6 +50,10 @@
+ #define AT_SYSINFO_EHDR 33  // for crosstoolv10
+ #endif
+ 
++#if defined(__NetBSD__)
++using Elf32_auxv_t = Aux32Info;
++using Elf64_auxv_t = Aux64Info;
++#endif
+ #if defined(__FreeBSD__)
+ using Elf64_auxv_t = Elf64_Auxinfo;
+ using Elf32_auxv_t = Elf32_Auxinfo;
+@@ -104,8 +108,13 @@ const void *VDSOSupport::Init() {
+     ElfW(auxv_t) aux;
+     while (read(fd, &aux, sizeof(aux)) == sizeof(aux)) {
+       if (aux.a_type == AT_SYSINFO_EHDR) {
++#if defined(__NetBSD__)
++        vdso_base_.store(reinterpret_cast<void *>(aux.a_v),
++                         std::memory_order_relaxed);
++#else
+         vdso_base_.store(reinterpret_cast<void *>(aux.a_un.a_val),
+                          std::memory_order_relaxed);
++#endif
+         break;
+       }
+     }
