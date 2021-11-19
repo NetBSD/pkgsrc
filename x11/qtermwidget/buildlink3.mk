50f9631a352d4bc0691906889105ba4b4f13d9e9
pin <pin@pkgsrc.org>
2021-11-19 09:50:40 +0000

    x11/qtermwidget: update to 1.0.0
    -Bumped minimum required Qt version to 5.15.
    -Fixed crash under (Plasma) Wayland on opening tab and splitting.
    -Quote drag-and-drop file names.
    -Backported CNL and CPL support from Konsole.
    -Some code cleanup.

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/04/20 23:38:23 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/11/19 09:50:40 pin Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.17.0
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=1.0.0
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
 #.include "../../x11/qt5-qtbase/buildlink3.mk"
