0018d529ec6c1b8a39399b9624525267627df953
sjmulder <sjmulder@pkgsrc.org>
2021-11-19 22:13:32 +0000

    textproc/libxlsxwriter: Update to 1.1.4
    Changes:
     - Added support for Worksheet tables. Tables in Excel are a way of
       grouping a range of cells into a single entity that has common
       formatting or that can be referenced from formulas. Tables can have
       column headers, autofilters, total rows, column formulas and default
       formatting. See working_with_tables and tables.c.
     - Added support for adding a macro button to a worksheet. See macro.c.
    
    Package changes:
     - Fixed test target (missing py.test version suffix).

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2021/08/09 07:05:23 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2021/11/19 22:13:32 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.2
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.4
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../archivers/minizip/buildlink3.mk"
