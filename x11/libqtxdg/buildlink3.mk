da716a3b1cef732d930fc091c0e829c1fad9cd57
pin <pin@pkgsrc.org>
2021-11-19 09:30:11 +0000

    x11/libqtxdg: update to 3.8.0
    -Added support for default terminal.
    -Considered XDG_DATA_HOME when computing desktop entry id.
    -Made sure that hicolor is searched before dash fallbacks.
    -Considered Qt's fallback search paths when finding icons.
    -Handle relative paths in qtxdg-desktop-file-start.

--- x11/libqtxdg/buildlink3.mk
+++ x11/libqtxdg/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2021/04/21 11:40:48 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2021/11/19 09:30:11 pin Exp $
 
 BUILDLINK_TREE+=	libqtxdg
 
 .if !defined(LIBQTXDG_BUILDLINK3_MK)
 LIBQTXDG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.7.1
-BUILDLINK_ABI_DEPENDS.libqtxdg?=		libqtxdg>=3.7.1nb1
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.8.0
 BUILDLINK_PKGSRCDIR.libqtxdg?=		../../x11/libqtxdg
 
 .include "../../devel/glib2/buildlink3.mk"
