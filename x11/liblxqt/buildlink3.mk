050030516efbeb093ba334bd7091deb03d71ba6c
pin <pin@pkgsrc.org>
2021-11-19 09:34:56 +0000

    x11/liblxqt: update to 1.0.0
    -Bumped version to 1.0.0 and required Qt 5.15.0.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.12 2021/04/21 11:40:48 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2021/11/19 09:34:56 pin Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.17.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=0.17.0nb1
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.0.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
