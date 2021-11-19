31ef662e77b9edc102001d6718c674a273207a1b
taca <taca@pkgsrc.org>
2021-11-19 14:26:28 +0000

    lang/php74: udpate to 7.4.26
    This release contains security fix.
    
    18 Nov 2021, PHP 7.4.26
    
    - Core:
      . Fixed bug #81518 (Header injection via default_mimetype / default_charset).
        (cmb)
    
    - Date:
      . Fixed bug #81500 (Interval serialization regression since 7.3.14 / 7.4.2).
        (cmb)
    
    - MBString:
      . Fixed bug #76167 (mbstring may use pointer from some previous request).
        (cmb, cataphract)
    
    - MySQLi:
      . Fixed bug #81494 (Stopped unbuffered query does not throw error). (Nikita)
    
    - PCRE:
      . Fixed bug #81424 (PCRE2 10.35 JIT performance regression). (cmb)
    
    - Streams:
      . Fixed bug #54340 (Memory corruption with user_filter). (Nikita)
    
    - XML:
      . Fixed bug #79971 (special character is breaking the path in xml function).
        (CVE-2021-21707) (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.344 2021/10/30 07:45:42 taca Exp $
+# $NetBSD: phpversion.mk,v 1.345 2021/11/19 14:26:28 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP73_VERSION=	7.3.32
-PHP74_VERSION=	7.4.25
+PHP74_VERSION=	7.4.26
 PHP80_VERSION=	8.0.12
 
 # Define initial release of major version.
