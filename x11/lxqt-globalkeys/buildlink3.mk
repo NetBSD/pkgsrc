7cf52ccdde22812733b375c426cff631e872b530
pin <pin@pkgsrc.org>
2021-11-19 11:32:55 +0000

    x11/lxqt-globalkeys: update to 1.0.0
    -Added shortcut filtering and did some code cleanup.

--- x11/lxqt-globalkeys/buildlink3.mk
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2021/04/21 11:40:48 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2021/11/19 11:32:55 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.17.0
-BUILDLINK_ABI_DEPENDS.lxqt-globalkeys?=	lxqt-globalkeys>=0.17.0nb1
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=1.0.0
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
