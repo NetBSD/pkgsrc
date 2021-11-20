d78658d08f21ced7c48f150056dd3b54c066577d
hauke <hauke@pkgsrc.org>
2021-11-20 20:56:05 +0000

    Update gtkdatabox to v1.0.0
    From the changelog:
    
    2021:
    - Clean-up code for GTK3 and release it.
    - Added new function to set the background color of databox:
      gtk_databox_set_bg_color().
    - Added GtkDoc documentation option: ./configure --enable-gtk-doc.
    - Removed libglade support, as it's not maintained anymore and
      depends on GTK2.

--- x11/gtkdatabox/buildlink3.mk
+++ x11/gtkdatabox/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.14 2020/08/17 20:17:50 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2021/11/20 20:56:05 hauke Exp $
 
 BUILDLINK_TREE+=	gtkdatabox
 
 .if !defined(GTKDATABOX_BUILDLINK3_MK)
 GTKDATABOX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtkdatabox+=	gtkdatabox>=0.9.2.0
-BUILDLINK_ABI_DEPENDS.gtkdatabox?=	gtkdatabox>=0.9.2.0nb13
+BUILDLINK_API_DEPENDS.gtkdatabox+=	gtkdatabox>=1.0.0
+BUILDLINK_ABI_DEPENDS.gtkdatabox?=	gtkdatabox>=1.0.0
 BUILDLINK_PKGSRCDIR.gtkdatabox?=	../../x11/gtkdatabox
 
 .include "../../devel/pango/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif	# GTKDATABOX_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gtkdatabox
