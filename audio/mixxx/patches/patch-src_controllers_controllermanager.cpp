eb3d724613ed7f213b69f7a22178df179a723d48
nia <nia@pkgsrc.org>
2021-11-20 19:07:37 +0000

    mixxx: update to 2.3.1
    Changes in 2.3.x: https://mixxx.org/news/2021-06-28-mixxx-2-3-0-released/
    mCVS: ----------------------------------------------------------------------

--- audio/mixxx/patches/patch-src_controllers_controllermanager.cpp
+++ audio/mixxx/patches/patch-src_controllers_controllermanager.cpp
@@ -1,16 +1,25 @@
-$NetBSD: patch-src_controllers_controllermanager.cpp,v 1.1 2019/07/08 16:46:32 nia Exp $
+$NetBSD: patch-src_controllers_controllermanager.cpp,v 1.2 2021/11/20 19:07:37 nia Exp $
 
 Support building without PortMIDI.
 
---- src/controllers/controllermanager.cpp.orig	2019-04-22 07:50:04.000000000 +0000
+--- src/controllers/controllermanager.cpp.orig	2021-09-29 13:23:02.000000000 +0000
 +++ src/controllers/controllermanager.cpp
-@@ -127,7 +127,9 @@ void ControllerManager::slotInitialize()
+@@ -24,7 +24,7 @@
+ // http://developer.qt.nokia.com/wiki/Threads_Events_QObjects
+ 
+ // Poll every 1ms (where possible) for good controller response
+-#ifdef __LINUX__
++#if defined(__LINUX__) || defined(__NetBSD__)
+ // Many Linux distros ship with the system tick set to 250Hz so 1ms timer
+ // reportedly causes CPU hosage. See Bug #990992 rryan 6/2012
+ const mixxx::Duration ControllerManager::kPollInterval = mixxx::Duration::fromMillis(5);
+@@ -139,7 +139,9 @@ void ControllerManager::slotInitialize()
  
      // Instantiate all enumerators. Enumerators can take a long time to
      // construct since they interact with host MIDI APIs.
 +#ifdef __PORTMIDI__
-     m_enumerators.append(new PortMidiEnumerator());
+     m_enumerators.append(new PortMidiEnumerator(m_pConfig));
 +#endif
  #ifdef __HSS1394__
-     m_enumerators.append(new Hss1394Enumerator());
+     m_enumerators.append(new Hss1394Enumerator(m_pConfig));
  #endif
