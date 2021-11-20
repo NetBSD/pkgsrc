4eab519acb9ac012c15a5571dd21784dfae41cb3
sjmulder <sjmulder@pkgsrc.org>
2021-11-20 00:31:10 +0000

    devel/mustach: Update to 1.2.2
    (Also adds test target)
    
    Changes in 1.2.2:
    
    Fix:
     - Fix: SONAME of libmustach-json-c.so
    
    Changes in 1.2.1:
    
    New:
     - Add SONAME in libraries.
     - Flag Mustach_With_PartialDataFirst to switch the
       policy of resolving partials.
    Fix:
     - Identification of types in cJSON
    
    Changes in 1.2.0:
    
    New:
     - Add hook 'mustach_wrap_get_partial' for handling partials.
     - Add test of mustache specifications https://github.com/mustache/spec.
    Changes:
     - Mustach_With_SingleDot is always set.
     - Mustach_With_IncPartial is always set.
     - Mustach_With_AllExtensions is changed to use currently known extensions.
     - Output of tests changed.
     - Makefile improved.
     - Partials are first searched as file then in current selection.
     - Improved management of delimiters.
    Fixes:
     - Improved output accordingly to https://github.com/mustache/spec:
       - escaping of quote "
       - interpolating null with empty string
       - removal of empty lines with standalone tag
       - don't enter section if null
       - indentation of partials
     - comment improved for get of mustach_wrap_itf.

--- devel/mustach/buildlink3.mk
+++ devel/mustach/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/08/06 00:49:20 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2021/11/20 00:31:10 sjmulder Exp $
 
 BUILDLINK_TREE+=	mustach
 
 .if !defined(MUSTACH_BUILDLINK3_MK)
 MUSTACH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mustach+=	mustach>=1.1.0
+BUILDLINK_API_DEPENDS.mustach+=	mustach>=1.2.2
 BUILDLINK_PKGSRCDIR.mustach?=	../../devel/mustach
 
 .include "../../textproc/jansson/buildlink3.mk"
