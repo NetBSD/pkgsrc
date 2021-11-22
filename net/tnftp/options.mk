715d301916b1966d920775b26147975b387379d5
nia <nia@pkgsrc.org>
2021-11-22 16:09:33 +0000

    tnftp: Disable ipv6 option if the OS doesn't have it.
    Needed on unixware.

--- net/tnftp/options.mk
+++ net/tnftp/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.13 2020/06/02 12:17:20 jperkin Exp $
+# $NetBSD: options.mk,v 1.14 2021/11/22 16:09:33 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftp
 PKG_SUPPORTED_OPTIONS=	editline inet6 ssl socks
-PKG_SUGGESTED_OPTIONS=	inet6
 
 CHECK_BUILTIN.openssl:=	yes
 .include "../../security/openssl/builtin.mk"
@@ -10,6 +9,12 @@ CHECK_BUILTIN.openssl:=	no
 
 .if !empty(USE_BUILTIN.openssl:M[yY][eE][sS])
 PKG_SUGGESTED_OPTIONS+=	ssl
+.endif
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${IPV6_READY:tl} == "yes"
+PKG_SUGGESTED_OPTIONS+=	inet6
 .endif
 
 .include "../../mk/bsd.options.mk"
