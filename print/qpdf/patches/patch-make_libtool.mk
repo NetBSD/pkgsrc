895e1c8ce1dd835975f63ecc001d08115667d667
ryoon <ryoon@pkgsrc.org>
2021-11-25 15:06:48 +0000

    qpdf: Update to 10.4.0
    Changelog:
    10.4.0: November 16, 2021
          + Handling of Weak Cryptography Algorithms
    
              o From the qpdf CLI, the --allow-weak-crypto is now required to
                suppress a warning when explicitly creating PDF files using RC4
                encryption. While qpdf will always retain the ability to read and
                write such files, doing so will require explicit acknowledgment
                moving forward. For qpdf 10.4, this change only affects the
                command-line tool. Starting in qpdf 11, there will be small API
                changes to require explicit acknowledgment in those cases as well.
                For additional information, see Chapter 6, Weak Cryptography.
    
          + Bug Fixes
    
              o Fix potential bounds error when handling shell completion that
                could occur when given bogus input.
    
              o Properly handle overlay/underlay on completely empty pages (with no
                resource dictionary).
    
              o Fix crash that could occur under certain conditions when using
                --pages with files that had form fields.
    
          + Library Enhancements
    
              o Make QPDF::findPage functions public.
    
              o Add methods to Pl_Flate to be able to receive warnings on certain
                recoverable conditions.
    
              o Add an extra check to the library to detect when foreign objects
                are inserted directly (instead of using QPDF::copyForeignObject) at
                the time of insertion rather than when the file is written.
                Catching the error sooner makes it much easier to locate the
                incorrect code.
    
          + CLI Enhancements
    
              o Improve diagnostics around parsing --pages command-line options
    
          + Packaging Changes
    
              o The Windows binary distribution is now built with crypto provided
                by OpenSSL 3.0.

--- print/qpdf/patches/patch-make_libtool.mk
+++ print/qpdf/patches/patch-make_libtool.mk
@@ -1,8 +1,6 @@
-$NetBSD: patch-make_libtool.mk,v 1.4 2020/12/24 20:35:25 yhardy Exp $
+$NetBSD: patch-make_libtool.mk,v 1.5 2021/11/25 15:06:48 ryoon Exp $
 
 libtool does not need bash.
-
-make the libtool tags explicit (fixes compiling on NetBSD/i386).
 
 --- make/libtool.mk.orig	2020-10-27 15:57:48.000000000 +0000
 +++ make/libtool.mk
@@ -15,21 +13,3 @@ make the libtool tags explicit (fixes compiling on NetBSD/i386).
  
  OBJ=o
  LOBJ=lo
-@@ -60,7 +60,7 @@ endef
- #                          1   2
- # Usage: $(call libcompile,src,includes)
- define libcompile
--	$(LIBTOOL) --quiet --mode=compile \
-+	$(LIBTOOL) --quiet --mode=compile --tag=CXX \
- 		$(CXX) $(CXXFLAGS) \
- 		$(call libdepflags,$(basename $(call src_to_obj,$(1)))) \
- 		$(foreach I,$(2),-I$(I)) \
-@@ -72,7 +72,7 @@ endef
- #                          1   2
- # Usage: $(call libcompile,src,includes)
- define c_libcompile
--	$(LIBTOOL) --quiet --mode=compile \
-+	$(LIBTOOL) --quiet --mode=compile --tag=CC \
- 		$(CC) $(CFLAGS) \
- 		$(call libdepflags,$(basename $(call c_src_to_obj,$(1)))) \
- 		$(foreach I,$(2),-I$(I)) \
