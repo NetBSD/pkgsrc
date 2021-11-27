a22b442ecf0429641940b27848791149c6643188
pho <pho@pkgsrc.org>
2021-11-27 05:37:59 +0000

    Apply some suggestions by pkglint
    Also renamed the option "ssl" to "openssl" because asking users to
    choose between "ssl" and "gnutls" is very confusing. TLS support is
    now optional too, as gkrellm can be built fine without any TLS
    libraries.

--- sysutils/gkrellm/options.mk
+++ sysutils/gkrellm/options.mk
@@ -1,14 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2005/09/08 13:19:56 cube Exp $
+# $NetBSD: options.mk,v 1.2 2021/11/27 05:37:59 pho Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.${GKRELLM_PKGBASE}
-PKG_OPTIONS_REQUIRED_GROUPS=	tls
-PKG_OPTIONS_GROUP.tls=		gnutls ssl
+PKG_OPTIONS_VAR=		PKG_OPTIONS.gkrellm
+PKG_OPTIONS_OPTIONAL_GROUPS=	tls
+PKG_OPTIONS_GROUP.tls=		gnutls openssl
 
-PKG_SUGGESTED_OPTIONS=		ssl
+PKG_OPTIONS_LEGACY_OPTS+=	ssl:openssl
+PKG_SUGGESTED_OPTIONS=		openssl
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mssl)
+.if !empty(PKG_OPTIONS:Mopenssl)
 .include "../../security/openssl/buildlink3.mk"
 GKRELLM_SSL_TYPE=	openssl
 .endif
