84c580f21e4dd15284ed81f85270cc75edb62c37
pho <pho@pkgsrc.org>
2021-11-27 05:37:59 +0000

    Apply some suggestions by pkglint
    Also renamed the option "ssl" to "openssl" because asking users to
    choose between "ssl" and "gnutls" is very confusing. TLS support is
    now optional too, as gkrellm can be built fine without any TLS
    libraries.

--- sysutils/gkrellm/buildlink3.mk
+++ sysutils/gkrellm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.52 2020/08/17 20:17:43 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.53 2021/11/27 05:37:59 pho Exp $
 
 BUILDLINK_TREE+=	gkrellm
 
@@ -10,8 +10,20 @@ BUILDLINK_ABI_DEPENDS.gkrellm+=	gkrellm>=2.2.10nb43
 BUILDLINK_PKGSRCDIR.gkrellm?=	../../sysutils/gkrellm
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../security/openssl/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+
+pkgbase := gkrellm
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.gkrellm:Mopenssl}
+.include "../../security/openssl/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gkrellm:Mgnutls}
+.include "../../security/gnutls/buildlink3.mk"
+.endif
+
 .endif # GKRELLM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gkrellm
