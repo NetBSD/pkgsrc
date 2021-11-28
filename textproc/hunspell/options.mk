9d9286a1c24020ff7c8ccb961ac6d560ee735226
nia <nia@pkgsrc.org>
2021-11-28 19:24:19 +0000

    hunspell: Allow NetBSD curses to count as "wide curses"

--- textproc/hunspell/options.mk
+++ textproc/hunspell/options.mk
@@ -1,17 +1,9 @@
-# $NetBSD: options.mk,v 1.8 2021/05/03 09:46:59 wiz Exp $
+# $NetBSD: options.mk,v 1.9 2021/11/28 19:24:19 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.hunspell
+
+# handled by curses framework
 PKG_SUPPORTED_OPTIONS=		wide-curses
 PKG_SUGGESTED_OPTIONS=		wide-curses
 
 .include "../../mk/bsd.options.mk"
-
-###
-### Wide curses support; otherwise, default to using narrow curses.
-###
-.if !empty(PKG_OPTIONS:Mwide-curses)
-.include "../../devel/ncursesw/buildlink3.mk"
-CPPFLAGS.SunOS+=		-D_XPG6
-.else
-.include "../../mk/curses.buildlink3.mk"
-.endif
