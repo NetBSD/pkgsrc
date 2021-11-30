427bccaf47fcc05627ff651ee5c558fe7ee4d37e
gutteridge <gutteridge@pkgsrc.org>
2021-11-30 14:52:32 +0000

    xfce4-power-manager: update to 4.16.0
    Change log:
    
    4.16.0
    ======
    - bump glib (and gio, gthread, etc) minimum to 2.50.0
    - panel-plugin: Prefer ac-adapter-symbolic (Fixes #103)
    - Add new icons in size 24x24 and 32x32
    - Translation Updates:
      Albanian, Basque, Catalan, Danish, Estonian, French, Galician,
      Hebrew, Indonesian, Norwegian Bokmål, Portuguese, Portuguese
      (Brazil), Russian, Serbian, Slovenian, Swedish, Turkish
    
    1.7.1
    ======
    This is a development release.
    - Add new app icon and switch to rDNS icon name
    - Use more accurate status icons (Fixes #95)
    - Also use symbolic icons in battery notifications
    - Reorder screensavers so Xfce comes first (Fixes #101)
    - settings: Adjust title and show min/max buttons
    - settings: Allow critical battery level up to 70% (Fixes #67)
    - settings: Add keywords for discoverability (Bug #10694)
    - settings: Fix GtkSwitch signal handlers (Issue #99)
    - xfpm: avoid default double fork
    - Respect the notification daemon's timeouts (Fixes #57)
    - Fix Laptop lid settings (Bug #15300)
    - Fix spelling mistake (Bug #16698)
    - Only update blank time when appropriate
    - Bug #16717: Move from exo-csource to xdt-csource
    - Guard against NULL pointer dereference
    - More blank time debug code
    - Indentation fixes
    - Cleanup code
    - Move default icon names into one place
    - Silence compiler warnings
    - Add README.md
    - Add basic GitLab pipeline
    - Translation Updates:
      Albanian, Arabic, Armenian (Armenia), Basque, Belarusian, Bulgarian,
      Catalan, Chinese (China), Chinese (Hong Kong), Chinese (Taiwan),
      Croatian, Czech, Danish, Dutch, Eastern Armenian, English (Australia),
      English (Canada), English (United Kingdom), Estonian, Finnish, French,
      Galician, German, Greek, Hebrew, Hungarian, Icelandic, Indonesian,
      Interlingue, Italian, Japanese, Kannada, Kazakh, Korean, Lithuanian,
      Malay, Norwegian Bokmål, Norwegian Nynorsk, Occitan (post 1500),
      Polish, Portuguese, Portuguese (Brazil), Romanian, Russian, Serbian,
      Sinhala, Slovak, Slovenian, Spanish, Swedish, Telugu, Thai, Turkish,
      Ukrainian, Uyghur, Vietnamese
    
    1.7.0
    ======
    This is a development release.
    - Fix inhibiting xfce4-screensaver (Bug #16364)
    - Fix indentation (makes git blame harder, but code more readable)
    - settings: Unclutter system and display tabs
    - settings: Move % sign out of spinbutton (Bug #15994)
    - Switch to symbolic window-close icons
    - Dismiss critical notification when connecting to AC
    - Make brightness steps configurable (Bug #12062)
    - Allow compilation with panel 4.15
    - panel-plugin: Properly show 'About' menu item
    - panel-plugin: Add (optional) Presentation mode indicator (Bug #15773)
    - panel-plugin: Toggle presentation mode on middle click
    - panel-plugin: Add warning color to presentation mode indicator
    - panel-plugin: Use GtkSwitch for Presentation mode
    - panel-plugin: Replace deprecated call
    - systray: Ensure presentation mode can be toggled (Bug #16337)
    - Translation Updates:
      Albanian, Arabic, Belarusian, Catalan, Chinese (China), Chinese
      (Taiwan), Croatian, Czech, Danish, Dutch, Finnish, French, Galician,
      German, Greek, Hebrew, Italian, Japanese, Kazakh, Korean, Lithuanian,
      Malay, Norwegian Bokmål, Portuguese, Portuguese (Brazil), Russian,
      Serbian, Slovak, Slovenian, Spanish, Turkish

--- sysutils/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
+++ sysutils/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-src_xfpm-pm-helper.c,v 1.1 2018/05/18 17:07:29 youri Exp $
+$NetBSD: patch-src_xfpm-pm-helper.c,v 1.2 2021/11/30 14:52:32 gutteridge Exp $
 
 Add NetBSD support.
 
---- src/xfpm-pm-helper.c.orig	2015-07-14 08:02:18.000000000 +0000
+--- src/xfpm-pm-helper.c.orig	2021-11-26 18:17:09.701758792 +0000
 +++ src/xfpm-pm-helper.c
 @@ -77,6 +77,10 @@
- #define UP_BACKEND_SUSPEND_COMMAND	"/usr/sbin/zzz"
+ #define UP_BACKEND_SUSPEND_COMMAND  "/usr/sbin/zzz"
  #define UP_BACKEND_HIBERNATE_COMMAND "/usr/sbin/ZZZ"
  #endif
 +#ifdef BACKEND_TYPE_NETBSD
-+#define UP_BACKEND_SUSPEND_COMMAND "sysctl -w hw.acpi.sleep.state=1"
++#define UP_BACKEND_SUSPEND_COMMAND "sysctl -w hw.acpi.sleep.state=3"
 +#define UP_BACKEND_HIBERNATE_COMMAND "sysctl -w hw.acpi.sleep.state=4"
 +#endif
  
  
  static gboolean
-@@ -174,19 +178,25 @@ main (int argc, char **argv)
+@@ -178,22 +182,26 @@ main (int argc, char **argv)
    /* run the command */
    if(suspend)
    {
@@ -23,10 +23,10 @@ Add NetBSD support.
      if (run (UP_BACKEND_SUSPEND_COMMAND))
      {
        return EXIT_CODE_SUCCESS;
--    } else {
-+    } else
+     }
+     else
 +#endif
-+    {
+     {
        return EXIT_CODE_FAILED;
      }
    }
@@ -36,10 +36,9 @@ Add NetBSD support.
      if(run (UP_BACKEND_HIBERNATE_COMMAND))
      {
        return EXIT_CODE_SUCCESS;
--    } else {
-+    } else
+     }
+     else
 +#endif
-+    {
+     {
        return EXIT_CODE_FAILED;
      }
-   }
