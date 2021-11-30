ac541cba30533ee029c8212658f537f4db58b033
pho <pho@pkgsrc.org>
2021-11-30 05:25:57 +0000

    Update to fuse-python-1.0.4; now it supports Python 3
    * v1.0.3
    unbreak xmp.py for python2
    
    * v1.0.2
    fix race in xmp.py (Carl Drougge)
    fix surrogateescape handling in fsyncdir/readdir/write (Carl Drougge)
    
    * v1.0.1
    Improve path handling for Python >= 3.6 (thanks to seahawk1986 for the
    patch)
    
    * v1.0.0
    IOCTL definitions to fix compilation on FreeBSD (patch from Mikhail
    Zakharov zmey20000@yahoo.com)
    
    Add poll support (patch from David Lechner david@lechnology.com)
    
    Use bytes objects for read and write data: please note that while this
    has no effect on Python 2,it is a breaking change in Python 3; it's
    worth it however since it fixes a serious usability bug that
    arbitrarily limited file content to valid UTF-8 data (patch from David
    Lechner david@lechnology.com)
    
    * v0.3.1
    IOCTL support added by Cédric Carrée
    
    * 0.3.0
    Add Python 3 compatibility.

--- filesystems/py-fuse-bindings/patches/patch-fuseparts___fusemodule.c
+++ filesystems/py-fuse-bindings/patches/patch-fuseparts___fusemodule.c
@@ -1,17 +1,151 @@
-$NetBSD: patch-fuseparts___fusemodule.c,v 1.1 2011/12/16 00:50:47 gdt Exp $
+$NetBSD: patch-fuseparts___fusemodule.c,v 1.2 2021/11/30 05:25:58 pho Exp $
 
 /*
  * This patch is not known to be filed upstream.
- * 
+ *
  * It adapts for NetBSD's (re)fuse implementation, specifically
  *   the lack of an attribute (rdev)
- *   [something about loop that I don't understand]
- *   stubbing out fuse_invalidate calls (because refuse doesn't support them?)
+ *   the lack of fuse_loop_mt(), fuse_invalidate(), fuse_setup(), and fuse_teardown()
+ *   the lack of polling support
  */
 
---- fuseparts/_fusemodule.c.orig	2010-04-14 22:29:21.000000000 +0000
+--- fuseparts/_fusemodule.c.orig	2021-05-02 06:20:20.000000000 +0000
 +++ fuseparts/_fusemodule.c
-@@ -213,7 +213,7 @@ fi_to_py(struct fuse_file_info *fi)
+@@ -33,6 +33,133 @@
+    to fix compilation errors on FreeBSD
+    Mikhail Zakharov <zmey20000@thoo.com> 2018.10.22 */
+ 
++#if defined(__NetBSD__)
++
++/* NetBSD librefuse doesn't support fuse_loop_mt() yet. Use the
++ * single-threaded loop instead. This is harmless, only a missed
++ * opportunity for performance gain. */
++#  define fuse_loop_mt(fuse) fuse_loop(fuse)
++
++/* NetBSD librefuse doesn't support fuse_invalidate() yet. Make it
++ * no-op. This too is harmless because librefuse caches nothing
++ * atm. */
++#  define fuse_invalidate(fuse, path) 0
++
++/* fuse_setup() has been superseded by fuse_new(). */
++#include <fuse_lowlevel.h>
++#include <signal.h>
++
++static void exit_handler(int sig __attribute__((__unused__)))
++{
++    struct fuse_context *ctx = fuse_get_context();
++
++    if (ctx)
++        fuse_exit(ctx->fuse);
++}
++
++static int set_signal_handler(int sig, void (*handler)(int))
++{
++    struct sigaction sa;
++    struct sigaction old_sa;
++
++    memset(&sa, 0, sizeof(struct sigaction));
++    sa.sa_handler = handler;
++    sigemptyset(&sa.sa_mask);
++    sa.sa_flags = 0;
++
++    if (sigaction(sig, NULL, &old_sa) == -1)
++        return -1;
++
++    if (old_sa.sa_handler == SIG_DFL)
++        if (sigaction(sig, &sa, NULL) == -1)
++            return -1;
++
++    return 0;
++}
++
++static int fuse_set_signal_handlers()
++{
++	if (set_signal_handler(SIGHUP, exit_handler) == -1 ||
++		set_signal_handler(SIGINT, exit_handler) == -1 ||
++		set_signal_handler(SIGTERM, exit_handler) == -1 ||
++		set_signal_handler(SIGPIPE, SIG_IGN) == -1)
++		return -1;
++
++	return 0;
++}
++
++static void fuse_remove_signal_handlers()
++{
++	set_signal_handler(SIGHUP, SIG_DFL);
++	set_signal_handler(SIGINT, SIG_DFL);
++	set_signal_handler(SIGTERM, SIG_DFL);
++	set_signal_handler(SIGPIPE, SIG_DFL);
++}
++
++static struct fuse *fuse_setup(
++	int argc, char **argv,
++	const struct fuse_operations *ops, size_t size,
++	char **mountpoint, int *multithreaded, void *userdata)
++{
++	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
++	struct fuse_cmdline_opts opts;
++	struct fuse* fuse = NULL;
++
++	if (fuse_parse_cmdline(&args, &opts) == -1)
++		return NULL;
++
++	if (opts.show_version)
++		fuse_lowlevel_version();
++
++	if (opts.show_help) {
++		printf("Usage: %s [options] mountpoint\n\n", args.argv[0]);
++		fuse_cmdline_help();
++	}
++
++	if (opts.mountpoint == NULL) {
++		fprintf(stderr, "fuse: no mountpoint specified\n");
++		goto free_args;
++	}
++
++	if (opts.debug)
++		if (fuse_opt_add_arg(&args, "-odebug") == -1)
++			goto free_args;
++
++	fuse = fuse_new(&args, ops, size, userdata);
++	if (!fuse)
++		goto free_args;
++
++	if (!opts.foreground)
++		if (fuse_daemonize(fuse) == -1)
++			goto destroy;
++
++	if (fuse_set_signal_handlers() == -1)
++		goto destroy;
++
++	if (fuse_mount(fuse, opts.mountpoint) == -1)
++		goto destroy;
++
++	goto free_args;
++destroy:
++	fuse_destroy(fuse);
++    fuse = NULL;
++free_args:
++	free(opts.mountpoint);
++	fuse_opt_free_args(&args);
++	return fuse;
++}
++
++/* fuse_teardown() has been removed. */
++static void fuse_teardown(
++	struct fuse *fuse,
++	char *mountpoint __attribute__((__unused__)))
++{
++	fuse_remove_signal_handlers();
++	fuse_unmount(fuse);
++	fuse_destroy(fuse);
++}
++#endif /* defined(__NetBSD__) */
++
+ #define _IOC_NRBITS     8
+ #define _IOC_TYPEBITS   8
+ 
+@@ -309,7 +436,7 @@ fi_to_py(struct fuse_file_info *fi)
  	 * autotools so we just dare to throw these in as is.		\
  	 */								\
  									\
@@ -20,33 +154,24 @@ $NetBSD: patch-fuseparts___fusemodule.c,v 1.1 2011/12/16 00:50:47 gdt Exp $
  	fetchattr_soft_d(st, st_blksize, 4096);				\
  	fetchattr_soft_d(st, st_blocks, (st->st_size + 511)/512)
  
-@@ -903,6 +903,9 @@ bmap_func(const char *path, size_t block
- static int
- pyfuse_loop_mt(struct fuse *f)
+@@ -1598,6 +1725,7 @@ static const char FuseNotifyPoll__doc__[
+ static PyObject *
+ FuseNotifyPoll(PyObject *self, PyObject *arg)
  {
-+#ifdef __NetBSD__
-+	return fuse_loop(f);
-+#else /* __NetBSD__ */
- 	int err = -1;
- #ifdef WITH_THREAD
- 	PyThreadState *save;
-@@ -916,6 +919,7 @@ pyfuse_loop_mt(struct fuse *f)
- #endif
++#if FUSE_VERSION >= 28
+ 	struct fuse_pollhandle *ph;
+ 	int ret;
  
- 	return(err);
-+#endif /* __NetBSD__ */
+@@ -1611,6 +1739,12 @@ FuseNotifyPoll(PyObject *self, PyObject 
+ 	ret = fuse_notify_poll(ph);
+ 
+ 	return PyInt_FromLong(ret);
++
++#else /* FUSE_VERSION >= 28 */
++	PyErr_SetString(Py_FuseError, "Polling not supported");
++	return NULL;
++
++#endif /* FUSE_VERSION >= 28 */
  }
  
- static struct fuse *fuse=NULL;
-@@ -1137,7 +1141,11 @@ FuseInvalidate(PyObject *self, PyObject 
- 
- 	path = PyString_AsString(arg1);
- 
-+#ifdef __NetBSD__
-+	err = 0;
-+#else
- 	err = fuse_invalidate(fuse, path);
-+#endif
- 
- 	ret = PyInt_FromLong(err);
- 
+ static PyMethodDef Fuse_methods[] = {
