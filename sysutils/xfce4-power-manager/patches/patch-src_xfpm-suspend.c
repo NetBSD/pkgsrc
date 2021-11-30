3746ebf0442022b76ab6712d56c6456da5c1298e
gutteridge <gutteridge@pkgsrc.org>
2021-11-30 15:28:00 +0000

    xfce4-power-manager: fix merge botch in a patch, ride update

--- sysutils/xfce4-power-manager/patches/patch-src_xfpm-suspend.c
+++ sysutils/xfce4-power-manager/patches/patch-src_xfpm-suspend.c
@@ -1,16 +1,26 @@
-$NetBSD: patch-src_xfpm-suspend.c,v 1.2 2021/11/30 14:52:32 gutteridge Exp $
+$NetBSD: patch-src_xfpm-suspend.c,v 1.3 2021/11/30 15:28:00 gutteridge Exp $
 
 Add NetBSD support.
 
---- src/xfpm-suspend.c.orig	2021-11-26 18:17:09.717521832 +0000
+--- src/xfpm-suspend.c.orig	2020-11-04 23:23:14.000000000 +0000
 +++ src/xfpm-suspend.c
-@@ -137,6 +137,9 @@ out:
-   return ret;
- }
+@@ -152,6 +151,9 @@ xfpm_suspend_can_suspend (void)
+ #ifdef BACKEND_TYPE_OPENBSD
+   return TRUE;
  #endif
 +#ifdef BACKEND_TYPE_NETBSD
-+    return TRUE;
++  return TRUE;
 +#endif
  
+   return FALSE;
+ }
+@@ -169,6 +171,9 @@ xfpm_suspend_can_hibernate (void)
+ #ifdef BACKEND_TYPE_OPENBSD
+   return TRUE;
+ #endif
++#ifdef BACKEND_TYPE_NETBSD
++  return TRUE;
++#endif
  
- gboolean
+   return FALSE;
+ }
