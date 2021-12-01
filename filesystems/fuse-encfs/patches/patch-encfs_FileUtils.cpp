9653ac9496873f20ee2eee613b928fd40b063ee9
pho <pho@pkgsrc.org>
2021-12-01 04:45:51 +0000

    Update to encfs-1.9.5
    v1.9.5 / 2018-04-27
    ===================
    	* Correct a int/off_t buffer overflow in getSize()
    	* Correct a possible write crash
    	* Correct encfsctl cat and add reverse cat
    	* Add -c & -u cmdline options
    	* Add --noattrcache & --nodatacache cmdline options
    	* Ignore .encfs6.xml file in reverse mode
    	* Do not count usage on root path (to avoid resetting the --idle option)
    	* Use stderr instead of stdout for error messages
    	* Add the ability to disable data encryption (absolutely discouraged)
    	* Add Cygwin support

--- filesystems/fuse-encfs/patches/patch-encfs_FileUtils.cpp
+++ filesystems/fuse-encfs/patches/patch-encfs_FileUtils.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-encfs_FileUtils.cpp,v 1.2 2019/04/09 16:43:10 maya Exp $
+$NetBSD: patch-encfs_FileUtils.cpp,v 1.3 2021/12/01 04:45:52 pho Exp $
 
 NetBSD ReFUSE doesn't have the fuse_unmount from FUSE 2.2 - FUSE 2.9.
 Workaround by using the version belonging to FUSE >= 3.0.
@@ -6,14 +6,17 @@ Workaround by using the version belonging to FUSE >= 3.0.
 This might be a temporal aberration: NetBSD ReFUSE had the newer FUSE
 API before upstream FUSE did.
 
---- encfs/FileUtils.cpp.orig	2018-01-28 21:07:41.000000000 +0000
+--- encfs/FileUtils.cpp.orig	2018-04-27 08:52:22.000000000 +0000
 +++ encfs/FileUtils.cpp
-@@ -1690,7 +1690,7 @@ bool unmountFS(EncFS_Context *ctx) {
-     return false;
-   }
- // Time to unmount!
--#if FUSE_USE_VERSION < 30
-+#if (FUSE_USE_VERSION < 30) && !defined(__NetBSD__)
-   fuse_unmount(ctx->opts->mountPoint.c_str(), nullptr);
- #else
-   fuse_unmount(fuse_get_context()->fuse);
+@@ -1734,7 +1734,11 @@ RootPtr initFS(EncFS_Context *ctx, const
+ 
+ void unmountFS(const char *mountPoint) {
+   // fuse_unmount returns void, is assumed to succeed
++#if defined(__NetBSD__)
++  fuse_unmount(fuse_get_context()->fuse);
++#else
+   fuse_unmount(mountPoint, nullptr);
++#endif
+ #ifdef __APPLE__
+   // fuse_unmount does not work on Mac OS, see #428
+   // However it makes encfs to hang, so we must unmount
