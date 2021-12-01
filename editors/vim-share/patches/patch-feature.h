258c684251ccd9ec2f42366436c42fee52074bbb
wiz <wiz@pkgsrc.org>
2021-12-01 21:36:59 +0000

    vim*: do not define NO_BUILTIN_TCAPS unconditionally
    This makes vim disregard color schemes. See
    https://mail-index.netbsd.org/pkgsrc-bugs/2021/05/28/msg070066.html
    and reading the CVS logs I couldn't find the reason it was done.
    
    Fix from RVP.
    
    Bump PKGREVISION.

--- editors/vim-share/patches/patch-feature.h
+++ editors/vim-share/patches/patch-feature.h
@@ -1,16 +1,7 @@
-$NetBSD: patch-feature.h,v 1.4 2020/02/23 10:39:33 morr Exp $
+$NetBSD: patch-feature.h,v 1.5 2021/12/01 21:36:59 wiz Exp $
 
 --- src/feature.h.orig	2019-12-03 21:59:23.000000000 +0000
 +++ src/feature.h
-@@ -454,7 +454,7 @@
-  *			(used only with NO_BUILTIN_TCAPS not defined).
-  */
- #ifdef HAVE_TGETENT
--// #define NO_BUILTIN_TCAPS
-+#define NO_BUILTIN_TCAPS
- #endif
- 
- #if !defined(NO_BUILTIN_TCAPS)
 @@ -814,7 +814,7 @@
  /*
   * SYS_VIMRC_FILE	Name of the system-wide .vimrc file.
