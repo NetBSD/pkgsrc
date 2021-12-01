95904a1216a898a3f278da542bd5d7ebf879b3de
pho <pho@pkgsrc.org>
2021-12-01 14:35:15 +0000

    Update to mp3fs-1.1.1; tested on NetBSD 9.2 release
    ## Important changes in 1.1.1 (2021-03-08)
    
    The only change in this release is a fix for an issue with filenames containing
    square brackets.
    
    ## Important changes in 1.1 (2021-01-30)
    
    This contains various bug fixes, mostly.
    
    Fixes/improvements:
    
      - Recognize upper/mixed case in input filenames
      - Fix file size handling with and without cache
      - Various code simplifications/cleanup
    
    New features:
    
      - Log lines now include thread ID and can be customized.
    
    ## Important changes in 1.0 (2020-05-24)
    
    mp3fs 1.0 is finally here\!
    
    Fixes/code improvements:
    
      - Many, many bug fixes (buffer overflows, memory leaks, and others)
      - Memory handling improvements (using RAII with C++ nearly everywhere)
      - Adopting C++11 and modernizing code
      - Static tests for code (e.g. clang-format, clang-tidy, IWYU)
    
    New features:
    
      - Ogg Vorbis decoding support
      - MP3 VBR encoding support
      - Improved, much more customizable logging
    
    Other:
    
      - Docs licensed under GPL 3+ (now entire codebase is distributable as GPL 3)
      - All docs switched to Markdown (including manpage, using pandoc)
      - Docker image now available
    
    ## Important changes in 0.91 (2014-05-14)
    
    This contains mainly bug fixes.
    
    Changes in this release:
    
      - Fixed a segfault caused by an overflow reading the list of available
        decoders.
      - A number of problems with the previous distribution tar are now fixed.
      - The output of `mp3fs --version` has been made more complete.
    
    ## Important changes in 0.9 (2014-04-06)
    
    This is a major new release, and brings us very close to a 1.0 release\!
    
    Changes in this release:
    
      - All transcoding code has been completely rewritten. Encoding and decoding
        have been abstracted out into base classes defining interfaces that can be
        implemented by different codec classes, with just a FLAC decoder and MP3
        encoder at the moment.
      - The build system has been modified as well to support this usage.
      - A number of small bugs or code inefficiencies have been fixed.
    
    ## Important changes in 0.32 (2012-06-18)
    
    This release has a lot of bug fixes and some code cleanup.
    
    Changes in this release:
    
      - The file size calculation should always be correct.
      - A crash affecting programs like scp that might try to access past the end
        of the file has been fixed.
      - Too many other little fixes were made to list here. See the ChangeLog for
        full details.
    
    ## Important changes in 0.31 (2011-12-04)
    
    This is a minor update, with bug fixes and a new feature.
    
    Changes in this release:
    
      - The ReplayGain support added earlier now can be configured through the
        command line.
      - Filename translation (from .flac to .mp3) is now fixed on filesystems such
        as XFS that do not populate dirent.d\_type.
      - A couple other minor bugs fixes and changes were made.
    
    ## Important changes in 0.30 (2010-12-01)
    
    This is a major new release, and brings mp3fs much closer to an eventual 1.0
    release.
    
    Changes in this release:
    
      - Support for additional metadata tags has been added. (From Gregor Zurowski)
      - Documentation improvements: the help message is more useful, and a man page
        has been added.
      - Choosing bitrate is now done with a command-line or mount option, rather
        than the old comma syntax.
      - A new option to select LAME encoding quality is now available. (From Gregor
        Zurowski)
      - Debug output can be enabled at runtime.
      - Old external libraries included in distribution (StringIO, talloc) have
        been removed and replaced.
      - Numerous bug fixes have been made. (Some from Gregor Zurowski)

---
+++ filesystems/fuse-mp3fs/patches/patch-src_mp3fs.cc
@@ -0,0 +1,40 @@
+$NetBSD: patch-src_mp3fs.cc,v 1.1 2021/12/01 14:35:15 pho Exp $
+
+Hunk #0:
+    <fuse_common.h> is not meant to be directly #include'd by user
+    code. It's only an implementation detail, and librefuse doesn't
+    provide one.
+
+Hunk #1:
+    Work around an issue in librefuse <fuse_opt.h>. It has been
+    incompatible with C++11 compilers. Fixed in HEAD:
+    * http://mail-index.netbsd.org/source-changes/2021/12/01/msg134169.html
+    * http://mail-index.netbsd.org/source-changes/2021/12/01/msg134169.html
+
+--- src/mp3fs.cc.orig	2021-01-31 04:07:18.000000000 +0000
++++ src/mp3fs.cc
+@@ -26,7 +26,6 @@
+ #define FUSE_USE_VERSION 26
+ 
+ #include <fuse.h>
+-#include <fuse_common.h>
+ #include <fuse_opt.h>
+ #ifdef __APPLE__
+ #include <fuse_darwin.h>
+@@ -63,6 +62,8 @@ enum { KEY_HELP, KEY_VERSION, KEY_KEEP_O
+ #define FUSE_OPT_KEY(templ, key) \
+     { templ, (unsigned int)(-1), key }
+ 
++#pragma GCC diagnostic push
++#pragma GCC diagnostic ignored "-Wnarrowing"
+ struct fuse_opt mp3fs_opts[] = {
+     MP3FS_OPT("-b %d", bitrate, 0),
+     MP3FS_OPT("bitrate=%d", bitrate, 0),
+@@ -98,6 +99,7 @@ struct fuse_opt mp3fs_opts[] = {
+     FUSE_OPT_KEY("-d", KEY_KEEP_OPT),
+     FUSE_OPT_KEY("debug", KEY_KEEP_OPT),
+     FUSE_OPT_END};
++#pragma GCC diagnostic pop
+ 
+ void usage(const std::string& name) {
+     std::cout << "Usage: " << name << " [OPTION]... IN_DIR OUT_DIR"
