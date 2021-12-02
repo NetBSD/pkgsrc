4e0c733788edd7d831cd1dcad3b92a7125a0b9b1
nia <nia@pkgsrc.org>
2021-12-02 18:14:34 +0000

    xf86-video-*: Add builtin.mk files

---
+++ x11/xf86-video-nv/builtin.mk
@@ -0,0 +1,6 @@
+# $NetBSD: builtin.mk,v 1.1 2021/12/02 18:14:36 nia Exp $
+
+X11_BUILTIN_NAME=	xf86-video-nv
+X11_DRV_NAME=		nv_drv
+
+.include "../../meta-pkgs/modular-xorg-drivers/builtin-common.mk"
---
+++ x11/xf86-video-suncg14/builtin.mk
@@ -0,0 +1,6 @@
+# $NetBSD: builtin.mk,v 1.1 2021/12/02 18:14:37 nia Exp $
+
+X11_BUILTIN_NAME=	xf86-video-suncg14
+X11_DRV_NAME=		suncg14_drv
+
+.include "../../meta-pkgs/modular-xorg-drivers/builtin-common.mk"
