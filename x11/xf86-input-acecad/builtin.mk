7c9be28f4c6103a97f8062af1f7b31aa3834dff2
nia <nia@pkgsrc.org>
2021-12-03 12:47:53 +0000

    xf86-input-*: Add builtin.mk files

---
+++ x11/xf86-input-acecad/builtin.mk
@@ -0,0 +1,6 @@
+# $NetBSD: builtin.mk,v 1.1 2021/12/03 12:47:53 nia Exp $
+
+X11_BUILTIN_NAME=	xf86-input-acecad
+X11_DRV_NAME=		acecad_drv
+
+.include "../../meta-pkgs/modular-xorg-drivers/builtin-common.mk"
