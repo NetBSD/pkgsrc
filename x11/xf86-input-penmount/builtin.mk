1e65a6925594a3c9db7eb70e35e980da61ced89a
nia <nia@pkgsrc.org>
2021-12-03 12:47:53 +0000

    xf86-input-*: Add builtin.mk files

---
+++ x11/xf86-input-penmount/builtin.mk
@@ -0,0 +1,6 @@
+# $NetBSD: builtin.mk,v 1.1 2021/12/03 12:47:54 nia Exp $
+
+X11_BUILTIN_NAME=	xf86-input-penmount
+X11_DRV_NAME=		penmount_drv
+
+.include "../../meta-pkgs/modular-xorg-drivers/builtin-common.mk"
