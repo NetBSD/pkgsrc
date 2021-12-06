2a3ae9c2ad182d6d5dd3ca729855e04d2074230f
wiz <wiz@pkgsrc.org>
2021-12-06 16:03:06 +0000

    python39: make curses module more amenable to NetBSD curses

---
+++ lang/python39/patches/patch-Modules___cursesmodule.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-Modules___cursesmodule.c,v 1.1 2021/12/06 16:03:06 wiz Exp $
+
+NetBSD curses compatibility.
+
+--- Modules/_cursesmodule.c.orig	2021-11-15 17:43:00.000000000 +0000
++++ Modules/_cursesmodule.c
+@@ -1077,8 +1077,8 @@ PyCursesWindow_ChgAt(PyCursesWindowObjec
+         return NULL;
+     }
+ 
+-    color = (short)((attr >> 8) & 0xff);
+-    attr = attr - (color << 8);
++    color = (short) PAIR_NUMBER(attr);
++    attr = attr & A_ATTRIBUTES;
+ 
+     if (use_xy) {
+         rtn = mvwchgat(self->win,y,x,num,attr,color,NULL);
