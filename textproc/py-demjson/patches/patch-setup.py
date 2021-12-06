667768781794cde036a6fd11c9e1f48a2ef4f34c
wiz <wiz@pkgsrc.org>
2021-12-06 15:03:50 +0000

    py-demjson: fix build with latest setuptools.

--- textproc/py-demjson/patches/patch-setup.py
+++ textproc/py-demjson/patches/patch-setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-setup.py,v 1.1 2020/05/07 09:29:04 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2021/12/06 15:03:50 wiz Exp $
 
 Always use setuptools.
 
---- setup.py.orig	2020-05-07 09:23:44.000000000 +0000
+--- setup.py.orig	2015-12-22 20:02:25.000000000 +0000
 +++ setup.py
 @@ -9,23 +9,8 @@ try:
  except AttributeError:
@@ -30,3 +30,24 @@ Always use setuptools.
  
  if False:
      sys.stdout.write("Using Python:    %s\n" % sys.version.split(None,1)[0])
+@@ -33,20 +18,6 @@ if False:
+ 
+ py3extra = {}
+ 
+-if py_major >= 3:
+-    # Make sure 2to3 gets run
+-    if distmech == 'setuptools':
+-        py3extra['use_2to3'] = True
+-        #py3extra['convert_2to3_doctests'] = ['src/your/module/README.txt']
+-        #py3extra['use_2to3_fixers'] = ['your.fixers']
+-    elif distmech == 'distutils':
+-        import distutils, distutils.command, distutils.command.build_py, distutils.command.build_scripts
+-        cmdclass = {
+-            'build_py':      distutils.command.build_py.build_py_2to3,
+-            'build_scripts': distutils.command.build_scripts.build_scripts_2to3
+-            }
+-        py3extra['cmdclass'] = cmdclass
+-
+ setup( name=name,
+        version=version,
+        py_modules=[name],
