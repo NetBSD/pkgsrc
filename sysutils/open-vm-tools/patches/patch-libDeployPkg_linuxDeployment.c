6f6d75e159d97b623b818ff8875a0d0da59e43f3
adam <adam@pkgsrc.org>
2021-12-07 18:25:45 +0000

    open-vm-tools: updated to 11.3.5
    open-vm-tools 11.3.5
    
    For issues resolved in this release, see Resolved Issues section.
    
    Added a configurable logging capability to the network script.
    
    The network script has been updated to:
    
    use vmware-toolbox-cmd to query any network logging configuration from the tools.conf file.
    use vmtoolsd --cmd "log ..." to log a message to the vmx logfile when the logging handler is configured to "vmx" or when the logfile is full or is not writeable.
    The hgfsmounter (mount.vmhgfs) command has been removed from open-vm-tools.
    
    The hgfsmounter (mount.vmhgfs) command is no longer used in Linux open-vm-tools. It has been replaced by hgfs-fuse. Therefore, removing all references to the hgfsmounter in Linux builds.
    
    
    Open-vm-tools 11.3.0
    
    A small command line tool, vmwgfxctrl, has been added to open-vm-tools for Linux that can be used to control various aspects of the vmwgfx Linux kernel module. Currently it can both display and set the current topology of the vmwgfx kernel driver. It is useful when trying to configure custom resolutions on recent Linux distributions, including multi-monitor setups.
    A command line tool, vmware-alias-import, has been added to open-vm-tools that can be used to import vgauth config data and apply it to the running vgauth service.

--- sysutils/open-vm-tools/patches/patch-libDeployPkg_linuxDeployment.c
+++ sysutils/open-vm-tools/patches/patch-libDeployPkg_linuxDeployment.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-libDeployPkg_linuxDeployment.c,v 1.1 2016/10/09 03:41:56 ryoon Exp $
+$NetBSD: patch-libDeployPkg_linuxDeployment.c,v 1.2 2021/12/07 18:25:46 adam Exp $
 
---- libDeployPkg/linuxDeployment.c.orig	2016-02-16 20:06:47.000000000 +0000
+--- libDeployPkg/linuxDeployment.c.orig	2021-08-31 09:03:13.000000000 +0000
 +++ libDeployPkg/linuxDeployment.c
-@@ -1058,7 +1058,7 @@ ExtractZipPackage(const char* pkgName,
+@@ -1583,7 +1583,7 @@ ExtractZipPackage(const char* pkgName,
  {
     ProcessHandle h;
     char* args[32];
@@ -11,16 +11,16 @@ $NetBSD: patch-libDeployPkg_linuxDeployment.c,v 1.1 2016/10/09 03:41:56 ryoon Ex
  
     int pkgFd, zipFd;
     char zipName[1024];
-@@ -1106,9 +1106,9 @@ ExtractZipPackage(const char* pkgName,
-    sLog(log_info, "unzip output: %s\n", Process_GetStdout(h));
+@@ -1644,9 +1644,9 @@ ExtractZipPackage(const char* pkgName,
+    sLog(log_info, "unzip output: '%s'.", Process_GetStdout(h));
  
     // Assume zip failed if it wrote to stderr
 -   stderr = Process_GetStderr(h);
 -   if (strlen(stderr) > 0) {
--      sLog(log_error, "Package unzip failed: %s\n", stderr);
+-      sLog(log_error, "Package unzip failed: '%s'.", stderr);
 +   mystderr = Process_GetStderr(h);
 +   if (strlen(mystderr) > 0) {
-+      sLog(log_error, "Package unzip failed: %s\n", mystderr);
++      sLog(log_error, "Package unzip failed: '%s'.", mystderr);
        ret = FALSE;
     }
  
