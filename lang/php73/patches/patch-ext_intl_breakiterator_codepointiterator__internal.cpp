ff627f7a321354e4d5397b5181d2110e1dfc2f3b
tnn <tnn@pkgsrc.org>
2021-12-08 23:55:19 +0000

    php73-intl: fix icu>=70 fallout. Backport from php74-intl.

--- lang/php73/patches/patch-ext_intl_breakiterator_codepointiterator__internal.cpp
+++ lang/php73/patches/patch-ext_intl_breakiterator_codepointiterator__internal.cpp
@@ -1,8 +1,9 @@
-$NetBSD: patch-ext_intl_breakiterator_codepointiterator__internal.cpp,v 1.1 2020/11/11 20:05:10 markd Exp $
+$NetBSD: patch-ext_intl_breakiterator_codepointiterator__internal.cpp,v 1.2 2021/12/08 23:55:19 tnn Exp $
 
 Use stdbool true/false macros.
+php73-intl: fix icu>=70 fallout. Backport from php74-intl.
 
---- ext/intl/breakiterator/codepointiterator_internal.cpp.orig	2020-10-27 15:01:59.000000000 +0000
+--- ext/intl/breakiterator/codepointiterator_internal.cpp.orig	2021-11-16 11:18:31.000000000 +0000
 +++ ext/intl/breakiterator/codepointiterator_internal.cpp
 @@ -58,7 +58,7 @@ CodePointBreakIterator& CodePointBreakIt
  		return *this;
@@ -13,8 +14,15 @@ Use stdbool true/false macros.
  
  	//don't bother copying the character iterator, getText() is deprecated
  	clearCurrentCharIter();
-@@ -78,17 +78,17 @@ CodePointBreakIterator::~CodePointBreakI
+@@ -75,20 +75,24 @@ CodePointBreakIterator::~CodePointBreakI
+ 	clearCurrentCharIter();
+ }
+ 
++#if U_ICU_VERSION_MAJOR_NUM >= 70
++bool CodePointBreakIterator::operator==(const BreakIterator& that) const
++#else
  UBool CodePointBreakIterator::operator==(const BreakIterator& that) const
++#endif
  {
  	if (typeid(*this) != typeid(that)) {
 -		return FALSE;
@@ -34,7 +42,7 @@ Use stdbool true/false macros.
  }
  
  CodePointBreakIterator* CodePointBreakIterator::clone(void) const
-@@ -109,7 +109,7 @@ CharacterIterator& CodePointBreakIterato
+@@ -109,7 +113,7 @@ CharacterIterator& CodePointBreakIterato
  
  UText *CodePointBreakIterator::getUText(UText *fillIn, UErrorCode &status) const
  {
@@ -43,7 +51,7 @@ Use stdbool true/false macros.
  }
  
  void CodePointBreakIterator::setText(const UnicodeString &text)
-@@ -128,7 +128,7 @@ void CodePointBreakIterator::setText(UTe
+@@ -128,7 +132,7 @@ void CodePointBreakIterator::setText(UTe
  		return;
  	}
  
@@ -52,7 +60,7 @@ Use stdbool true/false macros.
  
  	clearCurrentCharIter();
  }
-@@ -280,7 +280,7 @@ CodePointBreakIterator &CodePointBreakIt
+@@ -280,7 +284,7 @@ CodePointBreakIterator &CodePointBreakIt
  	}
  
  	int64_t pos = utext_getNativeIndex(this->fText);
