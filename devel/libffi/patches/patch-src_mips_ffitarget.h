dc73b600b7f5b6df96dbfe1c49557e57c8a20db4
adam <adam@pkgsrc.org>
2021-12-08 08:20:17 +0000

    libffi: updated to 3.4.2
    3.4.2 Jun-28-21
        Add static trampoline support for Linux on x86_64 and ARM64.
        Add support for Alibaba's CSKY architecture.
        Add support for Kalray's KVX architecture.
        Add support for Intel Control-flow Enforcement Technology (CET).
        Add support for ARM Pointer Authentication (PA).
        Fix 32-bit PPC regression.
        Fix MIPS soft-float problem.
        Enable tmpdir override with the $LIBFFI_TMPDIR environment variable.
        Enable compatibility with MSVC runtime stack checking.
        Reject float and small integer argument in ffi_prep_cif_var().
          Callers must promote these types themselves.

--- devel/libffi/patches/patch-src_mips_ffitarget.h
+++ devel/libffi/patches/patch-src_mips_ffitarget.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_mips_ffitarget.h,v 1.1 2020/02/26 19:15:44 adam Exp $
+$NetBSD: patch-src_mips_ffitarget.h,v 1.2 2021/12/08 08:20:17 adam Exp $
 
 Make a minimal adjustment to support the various NetBSD mips ports.
 
---- src/mips/ffitarget.h.orig	2019-10-31 14:49:54.000000000 +0000
+--- src/mips/ffitarget.h.orig	2021-06-27 15:17:08.000000000 +0000
 +++ src/mips/ffitarget.h
 @@ -41,8 +41,10 @@
  #define _MIPS_SIM_ABI32		1
  #define _MIPS_SIM_NABI32	2
  #define _MIPS_SIM_ABI64		3
--#elif !defined(__OpenBSD__)
+-#elif !defined(__OpenBSD__) && !defined(__FreeBSD__)
 +#elif defined(__sgi)
  # include <sgidefs.h>
 +#else
