2f552c7c2a8f82046987d2964c5850965a6c43aa
tnn <tnn@pkgsrc.org>
2021-12-08 23:53:30 +0000

    icu: properly bump the bl3 for version 70 solib major
    I'll let someone else decide if the recursive revbump needs to be redone
    or if this can ride the one that took place 8 hours ago.

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2021/04/21 11:42:46 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2021/12/08 23:53:30 tnn Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	icu
 ICU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
-BUILDLINK_ABI_DEPENDS.icu+=	icu>=69.1
+BUILDLINK_ABI_DEPENDS.icu+=	icu>=70.1
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
 
 GCC_REQD+=		4.9
