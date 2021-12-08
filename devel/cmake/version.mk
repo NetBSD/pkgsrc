2f4303bfd7039928cb26fdcede2b360af2a43761
adam <adam@pkgsrc.org>
2021-12-08 07:59:09 +0000

    cmake: updated to 3.22.1
    CMake 3.22.1
    * gitlab-ci: fix comment typo
    * gitlab-ci: shorten job prefixes
    * ci: add gmock to Debian base images
    * try_compile: Do not use CMAKE_BUILD_TYPE or CMAKE_CONFIGURATION_TYPES env vars
    * Help: Document more use cases for CMAKE_POLICY_DEFAULT_CMPNNNN
    * Help: Suggest CMAKE_POLICY_DEFAULT_CMP0077 in CMP0077 docs
    * Help: Suggest CMAKE_POLICY_DEFAULT_CMP0126 in CMP0126 docs
    * gitlab-ci: update macOS jobs to use Xcode 13.1
    * CompilerId: Restore support for classic C by avoiding C++ style comments
    * cmSystemTools: Remove unnecessary mark-as-used casts to void
    * file(RPATH): Restore tolerance of unknown formats if new RPATH is empty
    * FindPkgConfig: Restore finding pkg-config before pkgconf
    * VS: Revert "Add missing label in C# project-build events"
    * ci: Enable more VS tests that use managed code
    * Utilities/Release: Add script to sign/notarize macOS application bundle
    * HIP: Enable CMAKE_EXPORT_COMPILE_COMMANDS for HIP
    * mingw: fix calling convention for strftime()
    * Help: ENVIRONMENT_MODIFICATION path_list_... separator is based on host
    * Help: Use definition list for allowed values of CMAKE_INSTALL_MODE
    * Help: Explicitly state that if(ENV{some_var}) is always false
    * Help: Be more explicit about the behavior of if(<string>)
    * Help: Clarify CMAKE_INSTALL_MODE documentation
    * UseSWIG: ensure directory for depfile exists
    * FindPython: clarify static libraries hint usage
    * CMakeParseLibraryArchitecture: Fix parsing /lib/<arch> implicit object path
    * GNUInstallDirs: Prefer system lib64 over conda lib when ambiguous

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.22 2021/11/22 08:59:17 adam Exp $
+# $NetBSD: version.mk,v 1.23 2021/12/08 07:59:09 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.22.0
+CMAKE_VERSION=	3.22.1
 CMAKE_API=	${CMAKE_VERSION:R}
