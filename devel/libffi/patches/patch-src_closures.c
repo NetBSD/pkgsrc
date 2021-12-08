a09ffbc85bc711927094ec2a0e187d76a1d3f298
adam <adam@pkgsrc.org>
2021-12-08 08:20:17 +0000

    libffi: updated to 3.4.2
    3.4.2 Jun-28-21
        Add static trampoline support for Linux on x86_64 and ARM64.
        Add support for Alibaba's CSKY architecture.
        Add support for Kalray's KVX architecture.
        Add support for Intel Control-flow Enforcement Technology (CET).
        Add support for ARM Pointer Authentication (PA).
        Fix 32-bit PPC regression.
        Fix MIPS soft-float problem.
        Enable tmpdir override with the $LIBFFI_TMPDIR environment variable.
        Enable compatibility with MSVC runtime stack checking.
        Reject float and small integer argument in ffi_prep_cif_var().
          Callers must promote these types themselves.

--- devel/libffi/patches/patch-src_closures.c
+++ devel/libffi/patches/patch-src_closures.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_closures.c,v 1.4 2020/02/28 18:14:28 tnn Exp $
+$NetBSD: patch-src_closures.c,v 1.5 2021/12/08 08:20:17 adam Exp $
 
 provide stub of ffi_data_to_code_pointer to get aarch64 to link
 
---- src/closures.c.orig	2019-11-20 11:15:41.000000000 +0000
+--- src/closures.c.orig	2021-06-27 18:03:12.000000000 +0000
 +++ src/closures.c
-@@ -37,6 +37,7 @@
+@@ -38,6 +38,7 @@
  
  #ifdef __NetBSD__
  #include <sys/param.h>
@@ -12,9 +12,9 @@ provide stub of ffi_data_to_code_pointer to get aarch64 to link
  #endif
  
  #if __NetBSD_Version__ - 0 >= 799007200
-@@ -109,6 +110,14 @@ ffi_closure_free (void *ptr)
-   munmap(dataseg, rounded_size);
-   munmap(codeseg, rounded_size);
+@@ -119,6 +120,14 @@ ffi_tramp_is_present (__attribute__((unu
+ {
+   return 0;
  }
 +
 +void *
