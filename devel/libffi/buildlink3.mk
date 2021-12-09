eaff359c6178166fa7dd533b8e00184db6cb2747
tnn <tnn@pkgsrc.org>
2021-12-09 00:39:34 +0000

    libffi: bl3 bump for solib major change

--- devel/libffi/buildlink3.mk
+++ devel/libffi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/03/04 16:35:58 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/12/09 00:39:34 tnn Exp $
 
 BUILDLINK_TREE+=	libffi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libffi
 LIBFFI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libffi+=	libffi>=1.20
-BUILDLINK_ABI_DEPENDS.libffi+=	libffi>=3.3nb1
+BUILDLINK_ABI_DEPENDS.libffi+=	libffi>=3.4
 BUILDLINK_PKGSRCDIR.libffi?=	../../devel/libffi
 .endif # LIBFFI_BUILDLINK3_MK
 
