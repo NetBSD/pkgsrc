ec646e9a56d4fd7443cd23d52c4610fd20758299
tnn <tnn@pkgsrc.org>
2021-12-11 14:57:49 +0000

    gnuradio*: update to 3.9.4.0
    Minor update bringing C++ code cleanups, bug fixes
    and some performance improvements.

--- ham/gnuradio-vocoder/buildlink3.mk
+++ ham/gnuradio-vocoder/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/08/30 21:12:55 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/12/11 14:57:51 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-vocoder
 
 .if !defined(GNURADIO_VOCODER_BUILDLINK3_MK)
 GNURADIO_VOCODER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.8.1.0
-BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.8.1.0
+BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.9.4.0
+BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.9.4.0
 BUILDLINK_PKGSRCDIR.gnuradio-vocoder?=		../../ham/gnuradio-vocoder
 .endif # GNURADIO_VOCODER_BUILDLINK3_MK
 
