628e1661eb6ef83a47e33f6188a12816ed0e13ca
tnn <tnn@pkgsrc.org>
2021-12-11 17:39:56 +0000

    uhd: update to 4.1.0.4
    With UHD 4.0, RFNoC has undergone a major overhaul for improved usability,
    performance, and compatibility with tools like GNU Radio.
    
    Improved Radio Performance
      Increase channel bandwidth to 250 MSPS and beyond.
      Flexible bus width to increase throughput.
      Static routing for lower latency and higher throughput
      Updates to the NOC shell layer for improved latency and throughput.
    FPGA Design Flow Enhancements
      Ability to scale to a larger number of user-defined blocks.
      Reduced FPGA resource utilization in many instances.
    More Flexibility
      Architectural flexibility enables users to make more design tradeoffs.
      Allows the user to balance flexibility, resource utilization, and performance.
    Improved User Experience
     Improved documentation with a full RFNoC Specification
     Power calibration utility API (requires external calibration reference)
     Python API enhancements with full Python 3 support.
     Stability improvements from new automated testing infrastructure.
     Use embedded series radios without programming the ARM processing with
       "Network Mode" over the RJ45

--- ham/uhd/patches/patch-lib_transport_nirio_lvbitx_process-lvbitx.py
+++ ham/uhd/patches/patch-lib_transport_nirio_lvbitx_process-lvbitx.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-lib_transport_nirio_lvbitx_process-lvbitx.py,v 1.1 2019/10/04 10:41:44 adam Exp $
+$NetBSD: patch-lib_transport_nirio_lvbitx_process-lvbitx.py,v 1.2 2021/12/11 17:39:56 tnn Exp $
 
 Do not modify output path.
 
---- lib/transport/nirio/lvbitx/process-lvbitx.py.orig	2019-10-03 18:11:53.000000000 +0000
+--- lib/transport/nirio/lvbitx/process-lvbitx.py.orig	2021-09-29 14:25:58.000000000 +0000
 +++ lib/transport/nirio/lvbitx/process-lvbitx.py
 @@ -30,7 +30,7 @@ if (len(args) < 1):
  
  lvbitx_filename = args[0]
- input_filename = os.path.relpath(lvbitx_filename)
--autogen_src_path = os.path.relpath(options.output_src_path) if (options.output_src_path is not None) else os.path.dirname(input_filename)
+ input_filename = os.path.realpath(lvbitx_filename)
+-autogen_src_path = os.path.realpath(options.output_src_path) if (options.output_src_path is not None) else os.path.dirname(input_filename)
 +autogen_src_path = options.output_src_path if options.output_src_path is not None else os.path.dirname(input_filename)
  class_name = os.path.splitext(os.path.basename(input_filename))[0]
  
