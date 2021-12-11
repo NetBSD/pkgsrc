19af6ef56d565b1733b07b2d8a0c5f51494fc1ce
tnn <tnn@pkgsrc.org>
2021-12-11 14:57:49 +0000

    gnuradio*: update to 3.9.4.0
    Minor update bringing C++ code cleanups, bug fixes
    and some performance improvements.

--- ham/gnuradio-trellis/buildlink3.mk
+++ ham/gnuradio-trellis/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/08/30 21:05:13 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2021/12/11 14:57:51 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-trellis
 
 .if !defined(GNURADIO_TRELLIS_BUILDLINK3_MK)
 GNURADIO_TRELLIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.8.1.0
-BUILDLINK_ABI_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.8.1.0
+BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.9.4.0
+BUILDLINK_ABI_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.9.4.0
 BUILDLINK_PKGSRCDIR.gnuradio-trellis?=		../../ham/gnuradio-trellis
 .endif # GNURADIO_TRELLIS_BUILDLINK3_MK
 
