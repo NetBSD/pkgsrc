483938d44d3d675e000ce60938ba3672dcb8f884
tnn <tnn@pkgsrc.org>
2021-12-11 14:03:21 +0000

    volk: update to 2.5.0
    Kernels
        volk_32f_stddev_and_mean_32f_x2: implemented Young and Cramer's algorithm
        volk_32fc_accumulator_s32fc: Add new kernel
        volk_16ic_x2_dot_prod_16ic_u_avx2: Fix Typo, was _axv2.
        Remove _mm256_zeroupper() calls
        Enforce consistent function prototypes
        32fc_index_max: Improve speed of AVX2 version
        conv_k7_r2: Disable broken AVX2 code
        improve volk_8i_s32f_convert_32f for ARM NEON
        Calculate cos in AVX512F
        Calculate sin using AVX512F
    Build
        Fix python version detection
        cmake: Check that 'distutils' is available
        c11: Remove pre-C11 preprocessor instructions

--- math/volk/buildlink3.mk
+++ math/volk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/12/14 19:04:40 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2021/12/11 14:03:21 tnn Exp $
 
 BUILDLINK_TREE+=	volk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	volk
 VOLK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.volk+=	volk>=2.3.0
-BUILDLINK_ABI_DEPENDS.volk+=	volk>=2.4.0
+BUILDLINK_ABI_DEPENDS.volk+=	volk>=2.5.0
 BUILDLINK_PKGSRCDIR.volk?=	../../math/volk
 
 .include "../../devel/orc/buildlink3.mk"
