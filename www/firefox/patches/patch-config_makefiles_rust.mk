c1e97ea12b4428ede612676ec8827e9fdcbcdeff
ryoon <ryoon@pkgsrc.org>
2021-12-11 14:10:01 +0000

    firefox: Update to 95.0
    Changelog:
    New
    
      * RLBox --- a new technology that hardens Firefox against potential security
        vulnerabilities in third-party libraries --- is now enabled on all
        platforms.
    
      * Good news! You can now download Firefox from the Microsoft Store on Windows
        10 and Windows 11 platforms.
    
      * We've reduced CPU usage on macOS in Firefox and WindowServer during event
        processing.
    
      * We've also reduced the power usage of software decoded video on macOS,
        especially in fullscreen. This includes streaming sites such as Netflix and
        Amazon Prime Video.
    
      * You can now move the Picture-in-Picture toggle button to the opposite side
        of the video. Simply look for the new context menu option Move
        Picture-in-Picture Toggle to Left (Right) Side.
    
      * To better protect Firefox users against side-channel attacks such as
        Spectre, Site Isolation is now enabled for all Firefox 95 users.
    
    Fixed
    
      * After starting Firefox, users of the JAWS screen reader and ZoomText
        magnifier will no longer need to switch applications in order to access
        Firefox.
    
      * You'll find the state of controls using the ARIA switch role is now
        correctly reported by Mac OS VoiceOver.
    
      * You'll see a faster content process startup on macOS.
    
      * We've also made memory allocator improvements.
    
      * And we've improved page load performance by speculatively compiling
        JavaScript ahead of time.
    
      * Various security fixes
    
    Changed
    
      * We've added a User Agent override for Slack.com, which allows Firefox
        users to use more Call features and have access to Huddles.
    
    Security fixes:
    Mozilla Foundation Security Advisory 2021-52
    
    #CVE-2021-43536: URL leakage when navigating while executing asynchronous
     function
    #CVE-2021-43537: Heap buffer overflow when using structured clone
    #CVE-2021-43538: Missing fullscreen and pointer lock notification when
     requesting both
    #CVE-2021-43539: GC rooting failure when calling wasm instance methods
    #MOZ-2021-0010: Use-after-free in fullscreen objects on MacOS
    #CVE-2021-43540: WebExtensions could have installed persistent ServiceWorkers
    #CVE-2021-43541: External protocol handler parameters were unescaped
    #CVE-2021-43542: XMLHttpRequest error codes could have leaked the existence of
     an external protocol handler
    #CVE-2021-43543: Bypass of CSP sandbox directive when embedding
    #CVE-2021-43544: Receiving a malicious URL as text through a SEND intent could
     have led to XSS
    #CVE-2021-43545: Denial of Service when using the Location API in a loop
    #CVE-2021-43546: Cursor spoofing could overlay user interface when native
     cursor is zoomed
    #MOZ-2021-0009: Memory safety bugs fixed in Firefox 95 and Firefox ESR 91.4

--- www/firefox/patches/patch-config_makefiles_rust.mk
+++ www/firefox/patches/patch-config_makefiles_rust.mk
@@ -1,9 +1,9 @@
-$NetBSD: patch-config_makefiles_rust.mk,v 1.8 2021/11/11 16:48:04 ryoon Exp $
+$NetBSD: patch-config_makefiles_rust.mk,v 1.9 2021/12/11 14:10:01 ryoon Exp $
 
 NetBSD doesn't get along with parallel rust builds (it causes issues
 with ld.so) which are the default. Force -j1.
 
---- config/makefiles/rust.mk.orig	2021-10-28 18:58:50.000000000 +0000
+--- config/makefiles/rust.mk.orig	2021-11-25 21:18:06.000000000 +0000
 +++ config/makefiles/rust.mk
 @@ -58,6 +58,9 @@ ifdef MOZ_TSAN
  cargo_build_flags += -Zbuild-std=std,panic_abort
@@ -13,5 +13,5 @@ with ld.so) which are the default. Force -j1.
 +cargo_build_flags += -j1
 +endif
  
- # These flags are passed via `cargo rustc` and only apply to the final rustc
- # invocation (i.e., only the top-level crate, not its dependencies).
+ rustflags_sancov =
+ ifdef LIBFUZZER
