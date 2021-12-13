79fe048061d4efbc6f40ac722e5a66c5b05d0bd7
micha <micha@pkgsrc.org>
2021-12-13 13:47:06 +0000

    devel/doxygen: Update to 1.9.2
    Changelog is very long:
    https://www.doxygen.nl/manual/changelog.html

--- devel/doxygen/patches/patch-src_dirdef.cpp
+++ devel/doxygen/patches/patch-src_dirdef.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_dirdef.cpp,v 1.1 2019/10/17 20:35:58 adam Exp $
+$NetBSD: patch-src_dirdef.cpp,v 1.2 2021/12/13 13:47:06 micha Exp $
 
 Doxygen encodes the source directory path into some of the output filenames.
 This is problematic in a pkgsrc context as it means WRKDIR pathnames can appear
 in generated filenames. Fix by reverting to the old way of naming the files.
 
---- src/dirdef.cpp.orig	2019-06-13 19:36:00.000000000 +0000
+--- src/dirdef.cpp.orig	2021-08-17 17:10:43.000000000 +0000
 +++ src/dirdef.cpp
-@@ -198,7 +198,7 @@ QCString DirDefImpl::getOutputFileBase()
+@@ -211,7 +211,7 @@ QCString DirDefImpl::getOutputFileBase()
  {
    //printf("DirDefImpl::getOutputFileBase() %s->dir_%s\n",
-   //    m_diskName.data(),encodeDirName(m_diskName).data());
+   //    qPrint(m_diskName),qPrint(encodeDirName(m_diskName)));
 -  return "dir_"+encodeDirName(m_diskName);
 +  return QCString().sprintf("dir_%06d",m_dirCount);
  }
