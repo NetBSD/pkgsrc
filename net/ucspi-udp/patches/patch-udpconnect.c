c8228d9f39ee3be123bb43fdc1efe8aea39c7c03
schmonz <schmonz@pkgsrc.org>
2021-12-13 21:59:09 +0000

    Provide definition of struct sockaddr_in to fix NetBSD-current build.

--- net/ucspi-udp/patches/patch-udpconnect.c
+++ net/ucspi-udp/patches/patch-udpconnect.c
@@ -1,11 +1,15 @@
-$NetBSD: patch-udpconnect.c,v 1.1 2021/11/15 10:08:17 schmonz Exp $
+$NetBSD: patch-udpconnect.c,v 1.2 2021/12/13 21:59:09 schmonz Exp $
 
-Provide definition of struct timeval.
+Provide definition of structs timeval and sockaddr_in.
 
 --- udpconnect.c.orig	2016-03-29 19:44:38.000000000 +0000
 +++ udpconnect.c
-@@ -1,3 +1,4 @@
+@@ -1,6 +1,8 @@
 +#include <sys/time.h>
  #include <sys/types.h>
  #include <sys/param.h>
  #include <netdb.h>
++#include <netinet/in.h>
+ #include "sig.h"
+ #include "exit.h"
+ #include "sgetopt.h"
