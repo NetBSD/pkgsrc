da2af24382816e4822603c28d7d675f8cd92ed88
bacon <bacon@pkgsrc.org>
2021-12-14 18:58:56 +0000

    biology/biolibc: Update to 0.2.1
    Add orf.c with start/stop codon locators
    Standardize BED and GFF APIs
    Implement VCF input filtering
    Eliminate mutator macros mirroring mutator functions
    Numerous minor bug fixes and enhancements
    
    Changes: https://github.com/auerlab/biolibc/releases/tag/0.2.1

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.6 2021/09/18 00:42:39 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2021/12/14 18:58:57 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.0
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.1
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
+
+.include "../../devel/libxtend/buildlink3.mk"
 .endif	# BIOLIBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-biolibc
