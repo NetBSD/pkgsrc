468bc478a0e3a5ed454b94543f99190b3622f330
bacon <bacon@pkgsrc.org>
2021-12-14 18:55:11 +0000

    devel/libxtend: Update to 0.1.4
    Several new functions
    Eliminate mutator macros mirroring mutator functions
    Numerous minor bug fixes and enhancements
    
    Changes: https://github.com/outpaddling/libxtend/releases/tag/0.1.4

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/09/18 00:33:35 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2021/12/14 18:55:11 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.3.5
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.4
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
