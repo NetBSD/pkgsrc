2be0efbb88c36cab014c834e69d04fff53ac7011
taca <taca@pkgsrc.org>
2021-12-19 05:03:19 +0000

    lang/php80: update to 8.0.14
    pkgsrc change: fix indentation in options.mk.
    
    16 Dec 2021, PHP 8.0.14
    
    - Core:
      . Fixed bug #81582 (Stringable not implicitly declared if __toString() came
        from a trait). (Nikita)
      . Fixed bug #81591 (Fatal Error not properly logged in particular cases).
        (Nikita)
      . Fixed bug #81626 (Error on use static:: in __сallStatic() wrapped to
        Closure::fromCallable()). (Nikita)
      . Fixed bug #81631 (::class with dynamic class name may yield wrong line
        number). (Nikita)
    
    - FPM:
      . Fixed bug #81513 (Future possibility for heap overflow in FPM zlog).
        (Jakub Zelenka)
    
    - GD:
      . Fixed bug #71316 (libpng warning from imagecreatefromstring). (cmb)
    
    - IMAP:
      . Fixed bug #81649 (imap_(un)delete accept sequences, not single numbers).
        (cmb)
    
    - OpenSSL:
      . Fixed bug #75725 (./configure: detecting RAND_egd). (Dilyan Palauzov)
    
    - PCRE:
      . Fixed bug #74604 (Out of bounds in php_pcre_replace_impl). (cmb, Dmitry)
    
    - SPL:
      . Fixed bug #81587 (MultipleIterator Segmentation fault w/ SimpleXMLElement
        attached). (Nikita)
    
    - Standard:
      . Fixed bug #81618 (dns_get_record fails on FreeBSD for missing type).
        (fsbruva)
      . Fixed bug #81659 (stream_get_contents() may unnecessarily overallocate).
        (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.350 2021/12/19 05:01:05 taca Exp $
+# $NetBSD: phpversion.mk,v 1.351 2021/12/19 05:03:19 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP73_VERSION=	7.3.33
 PHP74_VERSION=	7.4.27
-PHP80_VERSION=	8.0.13
+PHP80_VERSION=	8.0.14
 PHP81_VERSION=	8.1.0
 
 # Define initial release of major version.
