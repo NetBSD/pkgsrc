84a2f01172a3198fdabf5f6a7430d3d8f58bcd95
ryoon <ryoon@pkgsrc.org>
2021-12-19 14:12:20 +0000

    lang/wasi-libcxx: import wasi-libcxx-13.0.0
    libc++abi and libc++ for WebAssembly WASI environment.

---
+++ lang/wasi-libcxx/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2021/12/19 14:12:20 ryoon Exp $
+
+BUILDLINK_TREE+=	wasi-libcxx
+
+BUILDLINK_DEPMETHOD.wasi-libcxx?=	build
+
+.if !defined(WASI_LIBCXX_BUILDLINK3_MK)
+WASI_LIBCXX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=13.0.0
+BUILDLINK_PKGSRCDIR.wasi-libcxx?=	../../lang/wasi-libcxx
+
+.endif	# WASI_LIBCXX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-wasi-libcxx
