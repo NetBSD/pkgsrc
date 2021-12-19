66316e3d6d5406f6a61923b19272414da3b7c087
wiz <wiz@pkgsrc.org>
2021-12-19 13:59:32 +0000

    py-cxfreeze: fix build with python 3.10
    Clean up some pkglint while here.

--- lang/py-cxfreeze/patches/patch-setup.py
+++ lang/py-cxfreeze/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.1 2018/02/09 08:04:41 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2021/12/19 13:59:32 wiz Exp $
 
 Link with proper run-time library path,
 link with -lpthread on NetBSD.
 
---- setup.py.orig	2017-05-20 21:59:18.000000000 +0000
+--- setup.py.orig	2017-11-20 15:18:06.000000000 +0000
 +++ setup.py
 @@ -71,6 +71,7 @@ class build_ext(distutils.command.build_
          fileName = os.path.splitext(self.get_ext_filename(ext.name))[0]
@@ -27,7 +27,7 @@ link with -lpthread on NetBSD.
                  extra_postargs = extraArgs,
                  debug = self.debug)
  
-@@ -136,13 +139,17 @@ if sys.platform == "win32":
+@@ -136,7 +139,11 @@ if sys.platform == "win32":
  if sys.platform == "win32":
      libraries = ["imagehlp", "Shlwapi"]
  else:
@@ -40,10 +40,3 @@ link with -lpthread on NetBSD.
  utilModule = Extension("cx_Freeze.util", ["source/util.c"],
          libraries = libraries)
  
- # build base executables
- docFiles = "README.txt"
--scripts = ["cxfreeze", "cxfreeze-quickstart"]
-+scripts = ["cxfreeze" + sys.version[:3], "cxfreeze-quickstart" + sys.version[:3]]
- options = dict(bdist_rpm = dict(doc_files = docFiles),
-         install = dict(optimize = 1))
- depends = ["source/bases/Common.c"]
