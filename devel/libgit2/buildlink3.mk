9e2840c0a012df93170d9c36fa25fc0c504522ca
bsiegert <bsiegert@pkgsrc.org>
2021-12-21 14:01:26 +0000

    libgit2: include pcre in buildlink3 file
    The pkg-config file for libgit2 specifies pcre as a hard dependency, which
    means that "pkg-config --libs libgit2" will exit with an error. This means
    that every dependent package also depends on pcre, so add it to the list
    of dependencies exposed by buildlink3.
    
    Noticed in the pending wip/cargo-edit package, which swallows the error
    message, leaving me to debug in the bowels of the Cargo build system.

--- devel/libgit2/buildlink3.mk
+++ devel/libgit2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/04/06 08:33:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2021/12/21 14:01:26 bsiegert Exp $
 
 BUILDLINK_TREE+=	libgit2
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.28.0
 BUILDLINK_ABI_DEPENDS.libgit2+=	libgit2>=1.0
 BUILDLINK_PKGSRCDIR.libgit2?=	../../devel/libgit2
 
+.include "../../devel/pcre2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# LIBGIT2_BUILDLINK3_MK
 
