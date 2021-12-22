ba041e39290f192e38330dc65af58a271e64df49
wiz <wiz@pkgsrc.org>
2021-12-22 22:41:18 +0000

    py-tryton: fix dependencies so that 'make test' passes
    Ride update

--- devel/py-tryton/options.mk
+++ devel/py-tryton/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.14 2021/12/22 22:38:14 wiz Exp $
+# $NetBSD: options.mk,v 1.15 2021/12/22 22:41:18 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-tryton
 PKG_SUPPORTED_OPTIONS=	goocalendar
-PKG_SUGGESTED_OPTIONS+=	
+PKG_SUGGESTED_OPTIONS+=
 
 .include "../../mk/bsd.options.mk"
 
