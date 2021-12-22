e2e65deb7b4e0dd995daddda1b52003b98465f98
wiz <wiz@pkgsrc.org>
2021-12-22 22:38:14 +0000

    py-tryton: remove some options
    These three modules are not referenced from the 6.2 sources.

--- devel/py-tryton/options.mk
+++ devel/py-tryton/options.mk
@@ -1,23 +1,11 @@
-# $NetBSD: options.mk,v 1.13 2021/12/22 22:25:12 mef Exp $
+# $NetBSD: options.mk,v 1.14 2021/12/22 22:38:14 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-tryton
-PKG_SUPPORTED_OPTIONS=	cdecimal goocalendar pytz simplejson
-PKG_SUGGESTED_OPTIONS+=	cdecimal pytz simplejson
+PKG_SUPPORTED_OPTIONS=	goocalendar
+PKG_SUGGESTED_OPTIONS+=	
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mcdecimal)
-#DEPENDS+=		${PYPKGPREFIX}-cdecimal-[0-9]*:../../math/py-cdecimal
-.endif
-
 .if !empty(PKG_OPTIONS:Mgoocalendar)
 DEPENDS+=		${PYPKGPREFIX}-goocalendar-[0-9]*:../../time/py-goocalendar
-.endif
-
-.if !empty(PKG_OPTIONS:Mpytz)
-DEPENDS+=		${PYPKGPREFIX}-pytz-[0-9]*:../../time/py-pytz
-.endif
-
-.if !empty(PKG_OPTIONS:Msimplejson)
-DEPENDS+=		${PYPKGPREFIX}-simplejson-[0-9]*:../../converters/py-simplejson
 .endif
