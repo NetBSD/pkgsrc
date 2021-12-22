808e214763d2e1e5e5866069a59398b7ac85a4a2
wiz <wiz@pkgsrc.org>
2021-12-22 23:00:59 +0000

    py-trytond: update options.mk and actually include it

--- devel/py-trytond/options.mk
+++ devel/py-trytond/options.mk
@@ -1,21 +1,13 @@
-# $NetBSD: options.mk,v 1.17 2020/12/09 01:46:07 gutteridge Exp $
+# $NetBSD: options.mk,v 1.18 2021/12/22 23:00:59 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-trytond
-PKG_SUPPORTED_OPTIONS=	cdecimal mysql pydot psycopg pytz simplejson sphinx
-PKG_SUGGESTED_OPTIONS+=	psycopg pytz simplejson sphinx
+PKG_SUPPORTED_OPTIONS=	levenshtein pydot psycopg
+PKG_SUGGESTED_OPTIONS+=	levenshtein pydot psycopg
 
 .include "../../mk/bsd.options.mk"
 
-# XXX Also supports converters/unoconv and textproc/py-Levenshtein options.
-
-.if !empty(PKG_OPTIONS:Mcdecimal)
-PYTHON_VERSIONS_ACCEPTED=	27 # py-cdecimal is obsolete for that version and thus not available
-DEPENDS+=		${PYPKGPREFIX}-cdecimal-[0-9]*:../../math/py-cdecimal
-.endif
-
-.if !empty(PKG_OPTIONS:Mmysql)
-PYTHON_VERSIONS_ACCEPTED=	27 # py-mysqldb
-DEPENDS+=		${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
+.if !empty(PKG_OPTIONS:Mlevenshtein)
+DEPENDS+=		${PYPKGPREFIX}-Levenshtein-[0-9]*:../../textproc/py-Levenshtein
 .endif
 
 .if !empty(PKG_OPTIONS:Mpydot)
@@ -23,18 +15,5 @@ DEPENDS+=		${PYPKGPREFIX}-dot-[0-9]*:../../graphics/py-dot
 .endif
 
 .if !empty(PKG_OPTIONS:Mpsycopg)
-DEPENDS+=		${PYPKGPREFIX}-psycopg2>=2:../../databases/py-psycopg2
-.endif
-
-.if !empty(PKG_OPTIONS:Mpytz)
-DEPENDS+=		${PYPKGPREFIX}-pytz-[0-9]*:../../time/py-pytz
-.endif
-
-.if !empty(PKG_OPTIONS:Msimplejson)
-DEPENDS+=		${PYPKGPREFIX}-simplejson-[0-9]*:../../converters/py-simplejson
-.endif
-
-.if !empty(PKG_OPTIONS:Msphinx)
-PYTHON_VERSIONED_DEPENDENCIES=	sphinx
-.include "../../lang/python/versioned_dependencies.mk"
+DEPENDS+=		${PYPKGPREFIX}-psycopg2>=2.5:../../databases/py-psycopg2
 .endif
