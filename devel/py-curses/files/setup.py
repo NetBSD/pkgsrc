6abaa423d67b6875698f6f57f47e6e10a2e13845
nros <nros@pkgsrc.org>
2021-12-26 19:57:05 +0000

    Fix py-curses with python 3.10 . The fix has been tested with python versions 2.7 3.6 3.9 and 3.10 .

--- devel/py-curses/files/setup.py
+++ devel/py-curses/files/setup.py
@@ -1,19 +1,22 @@
-# $NetBSD: setup.py,v 1.1 2002/01/28 09:32:55 drochner Exp $
+# $NetBSD: setup.py,v 1.2 2021/12/26 19:57:05 nros Exp $
 
 import distutils
+import os
 from distutils.core import setup, Extension
+from distutils.sysconfig import get_python_inc
 
 ncursesprefix = '@NCURSESPREFIX@'
 ncincl = ncursesprefix + '/include'
 nclib = ncursesprefix + '/lib'
+py_inc_int = os.path.join(get_python_inc(), 'internal')
 
 setup(
 	ext_modules = [
 		Extension(
 			'_curses',
 			['Modules/_cursesmodule.c'],
-			define_macros=[('HAVE_NCURSES_H', None)],
-			include_dirs=[ncincl],
+			define_macros=[('HAVE_NCURSES_H', None), ('Py_BUILD_CORE_MODULE', None)],
+			include_dirs=[ncincl, py_inc_int],
 			library_dirs=[nclib],
 			runtime_library_dirs=[nclib],
 			libraries=['ncurses']
