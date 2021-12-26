e8bd70dbe7ef464c368eb9e045be0961b6f3194c
he <he@pkgsrc.org>
2021-12-26 21:52:11 +0000

    Allow overriding the pthread library selection via PKGSRC_LLVM_PTHREADLIB.  LLVM insists on 64-bit atomics, and on NetBSD/powerpc we don't have that natively, so have to rely on -latomic from gcc.  This library is linked with -lpthread, so the cmake logic to detect whether -lpthread is included in the C library says "yes", leading to later breakage when clang is being built.  On NetBSD, set PKGSRC_LLVM_PTHREADLIB to -lpthread.
    OK'ed by wiz@,
    bump PKGREVISION.

--- lang/llvm/patches/patch-cmake_config-ix.cmake
+++ lang/llvm/patches/patch-cmake_config-ix.cmake
@@ -1,10 +1,24 @@
-$NetBSD: patch-cmake_config-ix.cmake,v 1.5 2021/07/12 18:41:02 adam Exp $
+$NetBSD: patch-cmake_config-ix.cmake,v 1.6 2021/12/26 21:52:11 he Exp $
 
 Do not generate invalid llvm-config in pkgsrc.
+Allow override of pthread library selection via PKGSRC_LLVM_PTHREADLIB.
 
 --- cmake/config-ix.cmake.orig	2021-04-06 16:38:18.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -175,7 +175,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
+@@ -113,7 +113,11 @@ if(HAVE_LIBPTHREAD)
+   set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
+   set(THREADS_HAVE_PTHREAD_ARG Off)
+   find_package(Threads REQUIRED)
+-  set(LLVM_PTHREAD_LIB ${CMAKE_THREAD_LIBS_INIT})
++  if(PKGSRC_LLVM_PTHREADLIB)
++    set(LLVM_PTHREAD_LIB ${PKGSRC_LLVM_PTHREADLIB})
++  else()
++    set(LLVM_PTHREAD_LIB ${CMAKE_THREAD_LIBS_INIT})
++  endif()
+ endif()
+ 
+ if(LLVM_ENABLE_ZLIB)
+@@ -176,7 +180,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
        set(MAYBE_REQUIRED)
      endif()
      if(LLVM_ENABLE_TERMINFO)
