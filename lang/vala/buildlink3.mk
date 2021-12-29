d500ef4e1efd248f681ed548f19a18258462f5d4
adam <adam@pkgsrc.org>
2021-12-29 16:31:40 +0000

    vala: updated to 0.54.5
    Vala 0.54.5
    ===========
     * Various improvements and bug fixes:
      - codegen:
        + Correctly handle chain up of struct creation methods
        + Use a dedicated EmitContext for _variant_get*() functions
        + gtkmodule: Handle nested closure elements and bind them accordingly
      - vala:
        + NullLiteral is not a valid argument for string concatenation
        + Set is_yield_expression in async context when chaining up to async base ctor
        + Report statement parsing error if it is not meant to be an expression
        + girparser: Avoid possibily creating duplicated attributes
      - vapigen.m4: use $PKG_CONFIG_SYSROOT_DIR
    
     * Bindings:
      - glib-2.0: Always use the actual C type for CCode.array_length_type
      - gstreamer: Cherry-pick bindings fixes from 0.56
      - gstreamer-1.0: Unskip the ElementFactory.make/create_full() symbols

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2021/12/08 16:02:18 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2021/12/29 16:31:40 adam Exp $
 
 BUILDLINK_TREE+=	vala
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vala
 VALA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vala+=	vala>=0.54
-BUILDLINK_ABI_DEPENDS.vala?=	vala>=0.54.3
+BUILDLINK_ABI_DEPENDS.vala+=	vala>=0.54.3
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
 BUILDLINK_DEPMETHOD.vala?=	build
 
