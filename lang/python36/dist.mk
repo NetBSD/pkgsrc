b3b7bd2b56fbb1e6f6bc7e42ae83bdaf55158c5b
adam <adam@pkgsrc.org>
2021-12-30 11:52:47 +0000

    python36: removed

--- lang/python36/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.16 2021/09/06 07:24:22 adam Exp $
-
-PY_DISTVERSION=	3.6.15
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.xz
-DISTINFO_FILE=	${.CURDIR}/../../lang/python36/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python36/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
