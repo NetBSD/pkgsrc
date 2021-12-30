03d93665c794e351133557a7ec851d9b4e9b63d0
adam <adam@pkgsrc.org>
2021-12-30 13:36:26 +0000

    py-sparqlwrapper: patch for Python 3.6 is no longer needed

--- www/py-sparqlwrapper/patches/patch-setup.py
+++ www/py-sparqlwrapper/patches/patch-setup.py
@@ -1,18 +1,9 @@
-$NetBSD: patch-setup.py,v 1.2 2021/12/06 14:14:35 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2021/12/30 13:36:26 adam Exp $
 
-Fix for Python 3.6 and latest setuptools.
+Fix for latest setuptools.
 
 --- setup.py.orig	2019-04-18 10:02:26.000000000 +0000
 +++ setup.py
-@@ -23,7 +23,7 @@ _version_re = re.compile(r'__version__\s
- _authors_re = re.compile(r'__authors__\s*=\s*"(.*)"')
- _url_re = re.compile(r'__url__\s*=\s*"(.*)"')
- 
--for line in open('SPARQLWrapper/__init__.py'):
-+for line in open('SPARQLWrapper/__init__.py', encoding='utf-8'):
- 
-     version_match = _version_re.match(line)
-     if version_match:
 @@ -75,8 +75,6 @@ setup(
          'Topic :: Software Development :: Libraries :: Python Modules',
        ],
