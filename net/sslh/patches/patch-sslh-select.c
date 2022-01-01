05f35898b3d0e599bfd4e82e1d8a2be096e98bd5
rhialto <rhialto@pkgsrc.org>
2022-01-01 16:47:20 +0000

    net/sslh: update to 1.22c
    - sslh-select now supports UDP protocols. Probes specified in the
      protocols configuration entry are tried on incoming packets, TCP or
      UDP, and forwarded based on the input protocol (an incoming TCP
      connection will be forwarded as TCP, and same with UDP). This has been
      tested with DNS as shown in udp.cfg: incoming packets that contain my
      domain name are assumed to be a DNS request and forwarded accordingly.
      Note this could cause problems if combined with incoming TLS with SNI.
      UDP clients and servers need to agree on the IPv4/IPv6 they use: use
      the same protocol on all sides! Often, this means explicitely using
      `ip4-localhost'. UDP sender-receiver pairs (connections, so to speak)
      are kept for 60s, which can be changed with udp_timeout in the
      configuration.
    
    - Added probes for UDP protocols QUICK and Teamspeak.
    
    - Added probes for syslog protocol.
    
    - sslh-select refactored to change linear searches through connections
      to linear searches through fd_set.
    
    - Fixed a libconfig call to support libconfig 1.7.3.
    
    - Added symbol to support libconfig 1.4.9, still in use in CentOS7.
    
    - Warn about unknown settings in the configuration file.
    
    - Added per-protocol transparent option. sslh-fork drops the capability after
      creating the server-side transparent socket. Transparent now uses CAP_NET_RAW
      instead of CAP_NET_ADMIN.
    
    - Removed compile-time option to use POSIX regex. Now regex must be
      PCRE2 (Perl-Compatible). This was in fact the case since v1.21, as
      PCRE are used to parse the config file.

--- net/sslh/patches/patch-sslh-select.c
+++ net/sslh/patches/patch-sslh-select.c
@@ -1,25 +1,24 @@
-$NetBSD: patch-sslh-select.c,v 1.1 2017/08/15 13:13:36 jperkin Exp $
+$NetBSD: patch-sslh-select.c,v 1.2 2022/01/01 16:47:21 rhialto Exp $
 
 Avoid queue variable conflict.
 
---- sslh-select.c.orig	2016-03-29 19:19:05.000000000 +0000
+--- sslh-select.c.orig	2021-08-28 14:33:20.000000000 +0000
 +++ sslh-select.c
-@@ -131,7 +131,7 @@ int accept_new_connection(int listen_soc
- /* Connect queue 1 of connection to SSL; returns new file descriptor */
- int connect_queue(struct connection *cnx, fd_set *fds_r, fd_set *fds_w)
+@@ -129,7 +129,7 @@ static struct connection* accept_new_con
+ static int connect_queue(struct connection* cnx,
+                          struct select_info* fd_info)
  {
 -    struct queue *q = &cnx->q[1];
 +    struct sslhqueue *q = &cnx->q[1];
  
-     q->fd = connect_addr(cnx, cnx->q[0].fd);
+     q->fd = connect_addr(cnx, cnx->q[0].fd, NON_BLOCKING);
      if ((q->fd != -1) && fd_is_in_range(q->fd)) {
-@@ -156,7 +156,8 @@ int connect_queue(struct connection *cnx
- void shovel(struct connection *cnx, int active_fd, 
-             fd_set *fds_r, fd_set *fds_w)
+@@ -153,7 +153,7 @@ static int connect_queue(struct connecti
+  */
+ static void shovel(struct connection *cnx, int active_fd, struct select_info* fd_info)
  {
 -    struct queue *read_q, *write_q;
 +    struct sslhqueue *read_q, *write_q;
-+
  
      read_q = &cnx->q[active_fd];
      write_q = &cnx->q[1-active_fd];
