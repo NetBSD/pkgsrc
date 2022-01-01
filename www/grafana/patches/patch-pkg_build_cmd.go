2775d7c500bec502916cd007bbac3dd58793d4f0
triaxx <triaxx@pkgsrc.org>
2022-01-01 06:34:13 +0000

    grafana: Update to 8.3.3
    upstream changes:
    -----------------
    8.3.3 (2021-12-10)
    Features and enhancements
      o BarChart: Use new data error view component to show actions in panel edit. #42474, @torkelo
      o CloudMonitor: Iterate over pageToken for resources. #42546, @iwysiu
      o Macaron: Prevent WriteHeader invalid HTTP status code panic. #42973, @bergquist
    Bug fixes
      o AnnoListPanel: Fix interpolation of variables in tags. #42318, @francoisdtm
      o CloudWatch: Allow queries to have no dimensions specified. #42800, @sunker
      o CloudWatch: Fix broken queries for users migrating from 8.2.4/8.2.5 to 8.3.0. #42611, @sunker
      o CloudWatch: Make sure MatchExact flag gets the right value. #42621, @sunker
      o Dashboards: Fix so that empty folders can be deleted from the manage dashboards/folders page. #42527, @ashharrison90
      o InfluxDB: Improve handling of metadata query errors in InfluxQL. #42500, @gabor
      o Loki: Fix adding of ad hoc filters for queries with parser and line_format expressions. #42590, @ivanahuckova
      o Prometheus: Fix running of exemplar queries for non-histogram metrics. #42749, @ivanahuckova
      o Prometheus: Interpolate template variables in interval. #42637, @ivanahuckova
      o StateTimeline: Fix toolitp not showing when for frames with multiple fields. #42741, @dprokop
      o TraceView: Fix virtualized scrolling when trace view is opened in right pane in Explore. #42480, @autoric
      o Variables: Fix repeating panels for on time range changed variables. #42828, @hugohaggmark
      o Variables: Fix so queryparam option works for scoped variables. #42742, @hugohaggmark
    
    8.3.2 (2021-12-10)
      o Security: Fixes CVE-2021-43813 and CVE-2021-43815. For more information, see our [blog](https://grafana.com/blog/2021/12/10/grafana-8.3.2-and-7.5.12-released-with-moderate-severity-security-fix/

--- www/grafana/patches/patch-pkg_build_cmd.go
+++ www/grafana/patches/patch-pkg_build_cmd.go
@@ -1,11 +1,11 @@
-$NetBSD: patch-pkg_build_cmd.go,v 1.1 2021/12/08 01:51:47 tnn Exp $
+$NetBSD: patch-pkg_build_cmd.go,v 1.2 2022/01/01 06:34:13 triaxx Exp $
 
 Use the same go binary as the one used to build.
 from: patch-build.go,v 1.1 2021/03/23 04:16:08 triaxx Exp
 
 --- pkg/build/cmd.go.orig	2021-12-06 07:21:15.000000000 +0000
 +++ pkg/build/cmd.go
-@@ -164,7 +164,7 @@ func setup(goos string) {
+@@ -130,7 +130,7 @@ func setup(goos string) {
  		args = append(args, "-buildmode=exe")
  	}
  	args = append(args, "./pkg/cmd/grafana-server")
@@ -14,7 +14,7 @@ from: patch-build.go,v 1.1 2021/03/23 04:16:08 triaxx Exp
  }
  
  func doBuild(binaryName, pkg string, opts BuildOpts) error {
-@@ -211,7 +211,7 @@ func doBuild(binaryName, pkg string, opt
+@@ -177,7 +177,7 @@ func doBuild(binaryName, pkg string, opt
  	args = append(args, "-o", binary)
  	args = append(args, pkg)
  
@@ -23,7 +23,7 @@ from: patch-build.go,v 1.1 2021/03/23 04:16:08 triaxx Exp
  
  	if opts.isDev {
  		return nil
-@@ -220,7 +220,7 @@ func doBuild(binaryName, pkg string, opt
+@@ -186,7 +186,7 @@ func doBuild(binaryName, pkg string, opt
  	if err := setBuildEnv(opts); err != nil {
  		return err
  	}
