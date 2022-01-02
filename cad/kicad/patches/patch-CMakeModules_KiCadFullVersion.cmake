f193a6d148d0db47bf76cc5f39c66e749d4d7246
tnn <tnn@pkgsrc.org>
2022-01-02 19:10:03 +0000

    kicad: update to 6.0.0
    XXXTNN package phase is broken on macOS due to app bundling changes.
    Extensive CMake surgery required. Will fix this in a follow-up commit.
    
    New in 6.0:
    
    - Modern, consistent look and feel
      KiCad 6.0 features a refreshed user interface designed to reduce the
      barriers of entry for new users
    - Upgraded schematic editing
      KiCad's schematic editor has received its biggest overhaul ever for
      version 6.0
    - Improved PCB design experience
      KiCad's PCB editor has had a complete look and feel upgrade
    
    The following features are deprecated as of version 6.0:
    
      XSLT BOM generation scripts
      The SWIG Python API

--- cad/kicad/patches/patch-CMakeModules_KiCadFullVersion.cmake
+++ cad/kicad/patches/patch-CMakeModules_KiCadFullVersion.cmake
@@ -1,16 +1,16 @@
-$NetBSD: patch-CMakeModules_KiCadFullVersion.cmake,v 1.1 2021/02/21 18:56:36 bouyer Exp $
+$NetBSD: patch-CMakeModules_KiCadFullVersion.cmake,v 1.2 2022/01/02 19:10:03 tnn Exp $
 
 Don't use ${CMAKE_MODULE_PATH} directly as it will be changed
-and containt multiple directories
+and contains multiple directories
 
---- CMakeModules/KiCadFullVersion.cmake.orig	2021-02-20 11:23:32.177620736 +0100
-+++ CMakeModules/KiCadFullVersion.cmake	2021-02-20 11:23:47.641157793 +0100
+--- CMakeModules/KiCadFullVersion.cmake.orig	2021-12-22 13:32:19.000000000 +0000
++++ CMakeModules/KiCadFullVersion.cmake
 @@ -28,7 +28,7 @@
  # KICAD_VERSION_EXTRA is then appended to the base version string.
  
  # Use git to determine the version string if it's available.
 -include( ${CMAKE_MODULE_PATH}/CreateGitVersionHeader.cmake )
-+include( ${CMAKE_CURRENT_LIST_DIR}/CreateGitVersionHeader.cmake )
++include( ${SRC_PATH}/CMakeModules/CreateGitVersionHeader.cmake )
  create_git_version_header( ${SRC_PATH} )
  
  # $KICAD_VERSION is set in KiCadVersion.cmake or by git (if it is available).
--- cad/kicad/patches/patch-common_lib__tree__model.cpp
+++ cad/kicad/patches/patch-common_lib__tree__model.cpp
@@ -1,16 +1,15 @@
-$NetBSD: patch-common_lib__tree__model.cpp,v 1.1 2020/06/01 03:50:21 mef Exp $
+$NetBSD: patch-common_lib__tree__model.cpp,v 1.2 2022/01/02 19:10:03 tnn Exp $
 
 /tmp/cad/kicad/work/kicad-5.1.4/common/lib_tree_model.cpp:78:14: error: 'sort' is not a member of 'std'
          std::sort( sort_buf.begin(), sort_buf.end(),
-              ^~~~
 
---- common/lib_tree_model.cpp.orig	2020-06-01 08:09:39.469102942 +0900
-+++ common/lib_tree_model.cpp	2020-06-01 08:10:04.891802170 +0900
+--- common/lib_tree_model.cpp.orig	2021-12-22 13:32:19.000000000 +0000
++++ common/lib_tree_model.cpp
 @@ -25,6 +25,7 @@
+ #include <eda_pattern_match.h>
  #include <lib_tree_item.h>
- #include <make_unique.h>
  #include <utility>
 +#include <algorithm>
  #include <pgm_base.h>
- #include <kicad_string.h>
+ #include <string_utils.h>
  
