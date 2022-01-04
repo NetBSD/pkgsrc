f5fe1fb85ec6908a8ffbc1a5280533a4a916471b
schmonz <schmonz@pkgsrc.org>
2022-01-04 21:03:22 +0000

    In addition to "qmail-tcpclient" being identical to ucspi-tcp's tcpclient, the author suggests we rely on the curvecpclient and curvecpmessage binaries provided by NaCl. Bump PKGREVISION.

--- mail/qremote/patches/patch-qmail-remote.c
+++ mail/qremote/patches/patch-qmail-remote.c
@@ -1,10 +1,25 @@
-$NetBSD: patch-qmail-remote.c,v 1.1 2020/11/02 10:24:58 schmonz Exp $
+$NetBSD: patch-qmail-remote.c,v 1.2 2022/01/04 21:03:22 schmonz Exp $
 
-The bundled tcpclient isn't special; use an installed one.
+The bundled tcpclient, curvecpclient, and curvecpmessage aren't special.
+Use whatever's installed.
 
 --- qmail-remote.c.orig	2013-04-14 10:07:50.000000000 +0000
 +++ qmail-remote.c
-@@ -139,7 +139,7 @@ void child(stralloc *d1, stralloc *d2, s
+@@ -124,7 +124,7 @@ void child(stralloc *d1, stralloc *d2, s
+ 
+     i = 0;
+     if (ix->haskey) {
+-        run[i++] = "qmail-curvecpclient";
++        run[i++] = "curvecpclient";
+         if (flagkeydir) {
+             run[i++] = "-c";
+             run[i++] = keydir.s;
+@@ -135,11 +135,11 @@ void child(stralloc *d1, stralloc *d2, s
+         run[i++] = ipstr;
+         run[i++] = strport;
+         run[i++] = ix->ext;
+-        run[i++] = "qmail-curvecpmessage";
++        run[i++] = "curvecpmessage";
          run[i++] = "-c";
      }
      else {
