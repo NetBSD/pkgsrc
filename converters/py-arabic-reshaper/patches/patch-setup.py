b59a452bb90cf42a5a33fc3fcb757091c4e07418
wiz <wiz@pkgsrc.org>
2022-01-05 09:37:15 +0000

    py-arabic-reshaper: revert
    This uses setuptools for pkg_resources
    
    Bump PKGREVISION

--- converters/py-arabic-reshaper/patches/patch-setup.py
+++ converters/py-arabic-reshaper/patches/patch-setup.py
@@ -1,8 +1,6 @@
-$NetBSD: patch-setup.py,v 1.2 2022/01/05 09:12:55 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2022/01/05 09:37:15 wiz Exp $
 
 Fix building with Python 2.7.
-
-https://github.com/mpcabd/python-arabic-reshaper/issues/77
 
 --- setup.py.orig	2021-03-23 11:03:30.000000000 +0000
 +++ setup.py
@@ -17,13 +15,3 @@ https://github.com/mpcabd/python-arabic-reshaper/issues/77
  
  setup(
      name="arabic_reshaper",
-@@ -19,8 +19,7 @@ setup(
-     license="MIT",
-     packages=['arabic_reshaper'],
-     install_requires=['configparser; python_version <"3"',
--                      'future',
--                      'setuptools'],
-+                      'future',],
-     extras_require={
-         'with-fonttools': ['fonttools>=4.0; python_version >="3"',
-                            'fonttools>=3.0<4.0; python_version <"3"']
