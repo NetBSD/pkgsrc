adb29140ddc2eee0a4e34eb43d29d016882a733a
wiz <wiz@pkgsrc.org>
2022-01-05 09:12:55 +0000

    py-arabic-reshaper: fix requirements.txt
    Ride bump

--- converters/py-arabic-reshaper/patches/patch-setup.py
+++ converters/py-arabic-reshaper/patches/patch-setup.py
@@ -1,8 +1,10 @@
-$NetBSD: patch-setup.py,v 1.1 2021/04/28 09:52:18 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2022/01/05 09:12:55 wiz Exp $
 
 Fix building with Python 2.7.
 
---- setup.py.orig	2021-04-28 09:49:06.000000000 +0000
+https://github.com/mpcabd/python-arabic-reshaper/issues/77
+
+--- setup.py.orig	2021-03-23 11:03:30.000000000 +0000
 +++ setup.py
 @@ -5,8 +5,8 @@ from setuptools import setup
  
@@ -15,3 +17,13 @@ Fix building with Python 2.7.
  
  setup(
      name="arabic_reshaper",
+@@ -19,8 +19,7 @@ setup(
+     license="MIT",
+     packages=['arabic_reshaper'],
+     install_requires=['configparser; python_version <"3"',
+-                      'future',
+-                      'setuptools'],
++                      'future',],
+     extras_require={
+         'with-fonttools': ['fonttools>=4.0; python_version >="3"',
+                            'fonttools>=3.0<4.0; python_version <"3"']
