e9f5131cb5162be4331e71104b444bcd75e6fce5
nia <nia@pkgsrc.org>
2022-01-07 13:02:31 +0000

    xtar: Patch some more obvious 1995-era code problems.

--- x11/xtar/patches/patch-Xbr_XbrCB.c
+++ x11/xtar/patches/patch-Xbr_XbrCB.c
@@ -1,14 +1,15 @@
-$NetBSD: patch-Xbr_XbrCB.c,v 1.1 2022/01/07 12:43:14 nia Exp $
+$NetBSD: patch-Xbr_XbrCB.c,v 1.2 2022/01/07 13:02:31 nia Exp $
 
 - Fix implicit declaration warnings.
 
 --- Xbr/XbrCB.c.orig	1995-05-09 08:06:46.000000000 +0000
 +++ Xbr/XbrCB.c
-@@ -19,6 +19,7 @@
+@@ -19,6 +19,8 @@
   */
  
  #include <Xm/Xm.h>
 +#include <stdlib.h>
++#include <stdio.h>
  
  void XbrCBExitApplication(Widget, XtPointer, XtPointer);
  void XbrCBDestroyWidget(Widget, XtPointer, XtPointer);
--- x11/xtar/patches/patch-common_pipe.c
+++ x11/xtar/patches/patch-common_pipe.c
@@ -1,13 +1,14 @@
-$NetBSD: patch-common_pipe.c,v 1.1 2022/01/07 12:43:14 nia Exp $
+$NetBSD: patch-common_pipe.c,v 1.2 2022/01/07 13:02:31 nia Exp $
 
 - Fix implicit declaration warnings.
 
 --- common/pipe.c.orig	1995-05-09 08:06:46.000000000 +0000
 +++ common/pipe.c
-@@ -23,7 +23,9 @@
+@@ -23,7 +23,10 @@
  
  #include <errno.h>
  #include <stdio.h>
++#include <stdlib.h>
 +#include <string.h>
  #include <fcntl.h>
 +#include <unistd.h>
