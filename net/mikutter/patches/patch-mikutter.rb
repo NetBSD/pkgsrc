e45a6d88484174081910373dd5a75ca9ac3cc5ac
tsutsui <tsutsui@pkgsrc.org>
2022-01-08 15:17:54 +0000

    mikutter: update to 5.0.2.
    pkgsrc changes:
    * make ${PREFIX}/share/mikutter/vendor/plugin as a pkgsrc specific
      plugin directory using PLUGIN_PATH env
    
    Upstream changes:
    
    mikutter 5.0.2
     https://mikutter.hatenablog.com/entry/2022/01/08/194013
    
    * [mastodon_sse_streaming] crash on disconnect
    
    mikutter 5.0.1
     https://mikutter.hatenablog.com/entry/2022/01/01/002306
    
    * happy new year
    * [mastodon] change initial data sources properly
    * [mastodon_sse_streaming] execute startup and closing connections per
      a number of handlers
    * [mastodon_sse_streaming] support streaming receiving of mentions
    * [mastodon_sse_streaming] refactoring and fix SSE parse bugs
    * stop using overlay vertical scrollbars on settings window
     * thanks Izumi Tsutsui
    * [mastodon] add data source of mentions and poll it by REST API
    * mastodon: fetch latest replies on startup
     * thanks Yuto Tokunaga
    * revive lost Ttimeline#postbox_delegation_generator
     * thanks Shibafu Midorino
    
    mikutter 5.0.0
     https://mikutter.hatenablog.com/entry/mikutter5_0
    
    * Gtk3
    * Incompatiple changes
     * gtk3 plugin is provided and won't be compatible with gtk plugins
    * Diva::Model by diva 2.0
    * show remaining characters on mastodon postbox
    * support Ruby 2.7 and later
    
    mikutter 4.1.8
     https://mikutter.hatenablog.com/entry/2021/12/21/232815
    
    * crash by right click on settings of list
    * duplicated settings for background color of "share with comment"
     * thanks Izumi Tsutsui
    * skin icons are not rendered properly on settings

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,9 +1,9 @@
-$NetBSD: patch-mikutter.rb,v 1.9 2021/01/01 05:00:12 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.10 2022/01/08 15:17:54 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
 
---- mikutter.rb.orig	2020-02-01 07:51:58.000000000 +0000
+--- mikutter.rb.orig	2021-12-31 15:11:57.000000000 +0000
 +++ mikutter.rb
 @@ -1,7 +1,5 @@
 -#!/bin/sh
@@ -13,4 +13,4 @@ $NetBSD: patch-mikutter.rb,v 1.9 2021/01/01 05:00:12 tsutsui Exp $
 -#!ruby
  =begin rdoc
  = mikutter - simple, powerful and moeful Mastodon client
- Copyright (C) 2009-2021 Toshiaki Asai
+ Copyright (C) 2009-2022 Toshiaki Asai
