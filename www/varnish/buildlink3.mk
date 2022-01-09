547c2f0e91cf201e2277563de08195193d5a46d6
tnn <tnn@pkgsrc.org>
2022-01-09 02:55:08 +0000

    varnish: update to 7.0.1
    New in varnish 7:
    - switch from PCRE to PCRE2
    - RFC8941 Structured Fields
    - increased default values for memory settings
    - bit flags in ACLs

--- www/varnish/buildlink3.mk
+++ www/varnish/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/06/25 12:21:08 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/01/09 02:55:08 tnn Exp $
 
 BUILDLINK_TREE+=	varnish
 
 .if !defined(VARNISH_BUILDLINK3_MK)
 VARNISH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.varnish+=	varnish>=6.6.0
+BUILDLINK_API_DEPENDS.varnish+=	varnish>=7.0.1
 BUILDLINK_PKGSRCDIR.varnish?=	../../www/varnish
 
 .include "../../devel/libexecinfo/buildlink3.mk"
-.include "../../devel/pcre/buildlink3.mk"
+.include "../../devel/pcre2/buildlink3.mk"
 
 .endif	# VARNISH_BUILDLINK3_MK
 
