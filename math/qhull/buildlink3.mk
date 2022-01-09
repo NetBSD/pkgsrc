6d8d08bb33682234344043be3a4cace1d88116ef
gdt <gdt@pkgsrc.org>
2022-01-09 01:30:46 +0000

    math/qhull: Also buildlink3 the bin/ programs
    From Paul Ripke on pkgsrc-users; needed for wip/prusaslicer.

--- math/qhull/buildlink3.mk
+++ math/qhull/buildlink3.mk
@@ -1,9 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/06/15 04:07:20 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/01/09 01:30:46 gdt Exp $
 
 BUILDLINK_TREE+=	qhull
 
 .if !defined(QHULL_BUILDLINK3_MK)
 QHULL_BUILDLINK3_MK:=
+
+BUILDLINK_FILES.qhull+=		bin/qconvex
+BUILDLINK_FILES.qhull+=		bin/qdelaunay
+BUILDLINK_FILES.qhull+=		bin/qhalf
+BUILDLINK_FILES.qhull+=		bin/qhull
+BUILDLINK_FILES.qhull+=		bin/qvoronoi
+BUILDLINK_FILES.qhull+=		bin/rbox
 
 BUILDLINK_API_DEPENDS.qhull+=	qhull>=2003.1
 BUILDLINK_ABI_DEPENDS.qhull+=	qhull>=2020.2nb1
