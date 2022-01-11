eb2046d4e0e27730fffd632ab852ae067a16a428
schmonz <schmonz@pkgsrc.org>
2022-01-11 13:54:01 +0000

    Apply upstream c023d98dcf2ba1cc30f545ae54d0e037e80a8794: Darwin platform allows to build on releases before Yosemite/ios 8. Fixes build on Snow Leopard. Still builds on Monterey.

--- security/openssl/patches/patch-crypto_rand_rand__unix.c
+++ security/openssl/patches/patch-crypto_rand_rand__unix.c
@@ -1,14 +1,26 @@
-$NetBSD: patch-crypto_rand_rand__unix.c,v 1.3 2021/08/25 15:22:05 adam Exp $
+$NetBSD: patch-crypto_rand_rand__unix.c,v 1.4 2022/01/11 13:54:01 schmonz Exp $
 
-Fix building on older Darwin.
+Apply upstream c023d98dcf2ba1cc30f545ae54d0e037e80a8794:
+Darwin platform allows to build on releases before Yosemite/ios 8.
 
---- crypto/rand/rand_unix.c.orig	2021-08-25 15:07:06.000000000 +0000
+--- crypto/rand/rand_unix.c.orig	2021-08-24 13:38:47.000000000 +0000
 +++ crypto/rand/rand_unix.c
-@@ -35,6 +35,7 @@
+@@ -34,9 +34,6 @@
+ #if defined(__OpenBSD__)
  # include <sys/param.h>
  #endif
- #if defined(__APPLE__)
-+# include <CommonCrypto/CommonCryptoError.h>
- # include <CommonCrypto/CommonRandom.h>
- #endif
+-#if defined(__APPLE__)
+-# include <CommonCrypto/CommonRandom.h>
+-#endif
+ 
+ #if defined(OPENSSL_SYS_UNIX) || defined(__DJGPP__)
+ # include <sys/types.h>
+@@ -381,7 +378,7 @@ static ssize_t syscall_random(void *buf,
+         if (errno != ENOSYS)
+             return -1;
+     }
+-#  elif defined(__APPLE__)
++#  elif defined(OPENSSL_APPLE_CRYPTO_RANDOM)
+     if (CCRandomGenerateBytes(buf, buflen) == kCCSuccess)
+ 	    return (ssize_t)buflen;
  
