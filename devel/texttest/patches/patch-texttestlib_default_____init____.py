348bfa6bf815e3fab099da272084145e16e60f2c
schmonz <schmonz@pkgsrc.org>
2022-01-12 09:47:24 +0000

    Update to 4.0.9. From the changelog:
    Bugfixes:
    - Allow for data files (e.g. new GUI XML files) to exist in
      texttest_config_modules along with python code
    - Fixing trailing characters in floating point diff and involuntarily
      triggered split mode #98
    - Making floating point diff split actually useful #45

--- devel/texttest/patches/patch-texttestlib_default_____init____.py
+++ devel/texttest/patches/patch-texttestlib_default_____init____.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-texttestlib_default_____init____.py,v 1.1 2020/04/18 19:42:18 schmonz Exp $
+$NetBSD: patch-texttestlib_default_____init____.py,v 1.2 2022/01/12 09:47:24 schmonz Exp $
 
 Find pkgsrc-provided diff tools.
 
---- texttestlib/default/__init__.py.orig	2020-03-23 09:09:04.000000000 +0000
+--- texttestlib/default/__init__.py.orig	2021-11-26 07:53:10.000000000 +0000
 +++ texttestlib/default/__init__.py
-@@ -1422,7 +1422,7 @@ class Config:
+@@ -1425,7 +1425,7 @@ class Config:
      def defaultDiffProgram(self):
          if os.name == "posix":
              for diff in ('tkdiff', 'kdiff3', 'meld'):
