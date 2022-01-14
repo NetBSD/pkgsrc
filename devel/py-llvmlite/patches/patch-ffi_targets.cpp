495cf99ed89b91c03e70ec41cb5520d0b4179001
adam <adam@pkgsrc.org>
2022-01-14 19:49:10 +0000

    py-llvmlite: updated to 0.38.0
    v0.38.0 (January 13, 2022)
    
    This release makes llvmlite compatible with Python 3.10. It also adds an abiname option to the target machine creation interface that mimics the same in LLVM. Further, a large number of functions are added to the IR API to support common uses of constant expressions. Finally, a number of bugs were fixed!

--- devel/py-llvmlite/patches/patch-ffi_targets.cpp
+++ devel/py-llvmlite/patches/patch-ffi_targets.cpp
@@ -1,17 +1,17 @@
-$NetBSD: patch-ffi_targets.cpp,v 1.1 2022/01/12 21:13:50 wiz Exp $
+$NetBSD: patch-ffi_targets.cpp,v 1.2 2022/01/14 19:49:10 adam Exp $
 
 Stopgap fix for llvm-12+
 https://github.com/numba/llvmlite/pull/802/files
 
---- ffi/targets.cpp.orig	2021-08-20 18:17:31.365363100 +0000
+--- ffi/targets.cpp.orig	2022-01-14 14:39:38.000000000 +0000
 +++ ffi/targets.cpp
-@@ -232,7 +232,9 @@ LLVMPY_CreateTargetMachine(LLVMTargetRef
+@@ -233,7 +233,9 @@ LLVMPY_CreateTargetMachine(LLVMTargetRef
          rm = Reloc::DynamicNoPIC;
  
      TargetOptions opt;
 +#if LLVM_VERSION_MAJOR < 12
      opt.PrintMachineCode = PrintMC;
 +#endif
+     opt.MCOptions.ABIName = ABIName;
  
      bool jit = JIT;
- 
