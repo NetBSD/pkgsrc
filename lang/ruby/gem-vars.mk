9d91ade83bf4d6eb35588a33f5cc76221bae667e
taca <taca@pkgsrc.org>
2022-01-14 15:42:08 +0000

    lang/ruby: clean up some macro handling
    1. Move some variables from gem-vars.mk to rubyversion.mk:
    
    	GEM_EXTSBASE
    	GEM_PATH
    	GEM_PLUGINSDIR
    	RUBYGEM
    	RUBY_GEM_ARCH
    
    2. Drop unused GEM_EXTSDIR_NEEDS_SUBDIR (it is always true).
    
    3. Define bundled module's version as "MODULE_VER".
    
    4. Generate PLIST_SUBST and PRINT_PLIST_AWK from bundled module's name
       if corresponding MODULE_VER is defined.
    
    5. Do not run ${RUBYGEM} for RUYB_GEM_ARCH and RUBY_GEM_VER.
    
    NFCI.

--- lang/ruby/gem-vars.mk
+++ lang/ruby/gem-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gem-vars.mk,v 1.12 2021/01/11 11:21:31 taca Exp $
+# $NetBSD: gem-vars.mk,v 1.13 2022/01/14 15:42:08 taca Exp $
 #
 # This Makefile fragment defines various make(1) variables for Ruby gems
 # support.
@@ -8,44 +8,20 @@ _RUBYGEM_VARS_MK=	# defined
 
 RUBYGEM_VERBOSE?=	No
 
-# Specify GEM_PATH
-GEM_PATH?=	${PREFIX}/${GEM_HOME}
-
-# Base directory for Gems
-MAKE_ENV+=	GEM_PATH=${GEM_PATH}
-
 RUBYGEM_LANG?=	en_US.UTF-8
 RUBYGEM_ENV?=	LANG=${RUBYGEM_LANG} LC_CTYPE=${RUBYGEM_LANG}
 
-RUBYGEM=	${LOCALBASE}/bin/${RUBYGEM_NAME}
-
-GEM_EXTSDIR_NEEDS_SUBDIR?=	yes
-
-GEM_PLUGINSDIR=	${GEM_HOME}/plugins
-
-.if exists(${RUBYGEM})
-RUBY_GEM_ARCH!=	${RUBYGEM} environment platform | ${SED} -e 's|.*:||'
-RUBY_GEM_VER!= ${RUBYGEM} --version
-
-GEM_EXTSBASE=	${GEM_HOME}/extensions
-PLIST_SUBST+=	GEM_EXTSDIR=${GEM_EXTSDIR}
-PLIST_SUBST+=	GEM_PLUGINSDIR=${GEM_PLUGINSDIR}
-
-.  if !empty(GEM_EXTSDIR_NEEDS_SUBDIR:M[nN][oO])
-GEM_EXTSDIR=	${GEM_EXTSBASE}/${RUBY_GEM_ARCH}/${RUBY_VER_DIR}
-.  else
-GEM_EXTSDIR=	${GEM_EXTSBASE}/${RUBY_GEM_ARCH}/${RUBY_VER_DIR}/${GEM_NAME}
-.  endif
-
-_RUBY_GEM_MAJOR=	${RUBY_GEM_VER:C/([0-9]+)\..*/\1/}
-_RUBY_GEM_MINOR=	${RUBY_GEM_VER:C/[0-9]+\.([0-9]+)\..*/\1/}
+_RUBY_GEM_MAJOR=	${RUBY_RUBYGEMS_VER:C/([0-9]+)\..*/\1/}
+_RUBY_GEM_MINOR=	${RUBY_RUBYGEMS_VER:C/[0-9]+\.([0-9]+)\..*/\1/}
 
 PLIST_VARS+=	ruby_plugins
 
+GEM_EXTSDIR=	${GEM_EXTSBASE}/${GEM_NAME}
+
+PLIST_SUBST+=	GEM_EXTSDIR=${GEM_EXTSDIR}
+
 .if ${_RUBY_GEM_MAJOR} >= 3 && ${_RUBY_GEM_MINOR} >= 2
 PLIST.ruby_plugins=	YES
-.endif
-
 .endif
 
 .endif
