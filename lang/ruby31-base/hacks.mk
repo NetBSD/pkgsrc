fd7aa8991650c804fbdcc60f9314faf23e0db9dd
taca <taca@pkgsrc.org>
2022-01-16 13:57:10 +0000

    lang/ruby31-base: add package version 3.1.0
    Ruby is a dynamic, open source programming language with a focus on
    simplicity and productivity.  It has an elegant syntax that is natural to
    read and easy to write.
    
    Features of Ruby are shown below.
      + Simple Syntax
      + *Normal* Object-Oriented features (ex. class, method calls)
      + *Advanced* Object-Oriented features (ex. Mix-in, Singleton-method)
      + Operator Overloading
      + Exception Handling
      + Iterators and Closures
      + Garbage Collection
      + Dynamic Loading of Object files (on some architecture)
      + Highly Portable (works on many UNIX machines, and on Windows, Mac, etc.)
    
    Ruby 3.1 introduces a number of new features and performance improvements,
    here are some of them:
      * Performance: YJIT: New experimental in-process JIT compiler
      * Development supports
    	- debug gem: A new debugger
    	- error_highlight: Fine-grained error location in backtrace
    	- IRB Autocomplete and Document Display
    
    This package is Ruby 3.1 release minimum base package.

---
+++ lang/ruby31-base/hacks.mk
@@ -0,0 +1,44 @@
+# $NetBSD: hacks.mk,v 1.1 2022/01/16 13:57:10 taca Exp $
+
+.if !defined(RUBY31_BASE_HACKS_MK)
+RUBY31_BASE_HACKS_MK=	defined
+
+.include "../../mk/compiler.mk"
+
+### [ Sun Jun  5 10:05:39 CEST 2005 : seb ]
+### On NetBSD/sparc64, gcc optimisation, at least for version 3.3.3,
+### produces segmentation faulting miniruby binary.
+### Also note that `-O' level optimisation produces a miniruby
+### binary that loops while running the installation scripts.
+###
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-sparc64)
+.  if !empty(CC_VERSION:Mgcc-3.3.*)
+PKG_HACKS+=		optimisation
+BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
+.  endif
+### ruby193 binary built on NetBSD/sparc64 with gcc 4.5.1 and the default -O2
+### dumps core during generating RDocs.
+### Using -O1 works around.
+.  if !empty(CC_VERSION:Mgcc-4.5.*)
+PKG_HACKS+=		optimisation
+BUILDLINK_TRANSFORM+=	opt:-O2:-O1
+.  endif
+.endif
+
+# On NetBSD/sh3el 6.0, the default -Os causes an error on compiling node.c:
+#  {standard input}: Assembler messages: {standard input}:1458: \
+#  Error: pcrel too far
+# and -O1 works around.
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-sh3*) && !empty(CC_VERSION:Mgcc-4.5.*)
+PKG_HACKS+=		optimisation
+BUILDLINK_TRANSFORM+=	opt:-Os:-O1 rm:-freorder-blocks
+.endif
+
+# On NetBSD/aarch64, GCC before GCC 10 optimisation produces segmentation
+# faulting miniruby binary.
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-aarch64*) && !empty(CC_VERSION:Mgcc-[2-9]*)
+PKG_HACKS+=		optimisation
+BUILDLINK_TRANSFORM+=	rm:-fomit-frame-pointer
+.endif
+
+.endif	# RUBY31_BASE_HACKS_MK
