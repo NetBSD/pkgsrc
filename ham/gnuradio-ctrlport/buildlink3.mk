cc2652676a883ff1356f5b86a88785daff0b06b7
tnn <tnn@pkgsrc.org>
2022-01-16 18:06:25 +0000

    gnuradio: update to 3.10.0.0
    - gr-pdu: Protocol Data Units library
    - gr-iio: Linux industrial I/O library (but not packaged in pkgsrc)
    - Custom Buffers Support
    - switch from Log4CPP to spdlog

--- ham/gnuradio-ctrlport/buildlink3.mk
+++ ham/gnuradio-ctrlport/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/12/11 14:57:50 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/01/16 18:06:25 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-ctrlport
 
 .if !defined(GNURADIO_CTRLPORT_BUILDLINK3_MK)
 GNURADIO_CTRLPORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-ctrlport+=	gnuradio-ctrlport>=3.9.4.0
-BUILDLINK_ABI_DEPENDS.gnuradio-ctrlport+=	gnuradio-ctrlport>=3.9.4.0
+BUILDLINK_API_DEPENDS.gnuradio-ctrlport+=	gnuradio-ctrlport>=3.10.0.0
 BUILDLINK_PKGSRCDIR.gnuradio-ctrlport?=		../../ham/gnuradio-ctrlport
 .endif # GNURADIO_CTRLPORT_BUILDLINK3_MK
 
--- ham/gnuradio-vocoder/buildlink3.mk
+++ ham/gnuradio-vocoder/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/12/11 14:57:51 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/01/16 18:06:27 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-vocoder
 
 .if !defined(GNURADIO_VOCODER_BUILDLINK3_MK)
 GNURADIO_VOCODER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.9.4.0
-BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.9.4.0
+BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.10.0.0
 BUILDLINK_PKGSRCDIR.gnuradio-vocoder?=		../../ham/gnuradio-vocoder
 .endif # GNURADIO_VOCODER_BUILDLINK3_MK
 
