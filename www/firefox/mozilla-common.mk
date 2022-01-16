8f07b236a6d3021c75a786eec110450add5147dc
gutteridge <gutteridge@pkgsrc.org>
2022-01-16 20:07:49 +0000

    firefox: 96 requires nss>=3.73

--- www/firefox/mozilla-common.mk
+++ www/firefox/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.222 2022/01/16 13:54:51 tnn Exp $
+# $NetBSD: mozilla-common.mk,v 1.223 2022/01/16 20:07:49 gutteridge Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -198,7 +198,7 @@ BUILDLINK_API_DEPENDS.libevent+=	libevent>=1.1
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.32
 .include "../../devel/nspr/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.72
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.73
 .include "../../devel/nss/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 #.include "../../mk/jpeg.buildlink3.mk"
