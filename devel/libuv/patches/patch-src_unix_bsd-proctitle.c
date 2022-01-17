bf209806d9fd6fe28193aafc2faef496ffb4072f
tnn <tnn@pkgsrc.org>
2022-01-17 10:12:54 +0000

    libuv: switch to upstream-provided patch

--- devel/libuv/patches/patch-src_unix_bsd-proctitle.c
+++ devel/libuv/patches/patch-src_unix_bsd-proctitle.c
@@ -1,29 +1,14 @@
-$NetBSD: patch-src_unix_bsd-proctitle.c,v 1.1 2022/01/17 01:38:53 tnn Exp $
+$NetBSD: patch-src_unix_bsd-proctitle.c,v 1.2 2022/01/17 10:12:54 tnn Exp $
 
-Only destroy the process title mutex if it had been initialized.
+https://github.com/libuv/libuv/pull/3428/commits/4ea2e7629dcedac0eb84bdc85861fbc51386e20b
 
 --- src/unix/bsd-proctitle.c.orig	2022-01-04 14:18:00.000000000 +0000
 +++ src/unix/bsd-proctitle.c
-@@ -27,6 +27,7 @@
- 
- 
- static uv_mutex_t process_title_mutex;
-+static int process_title_mutex_inited = 0;
- static uv_once_t process_title_mutex_once = UV_ONCE_INIT;
- static char* process_title;
- 
-@@ -34,11 +35,13 @@ static char* process_title;
- static void init_process_title_mutex_once(void) {
-   if (uv_mutex_init(&process_title_mutex))
-     abort();
-+  process_title_mutex_inited = 1;
- }
+@@ -38,6 +38,7 @@ static void init_process_title_mutex_onc
  
  
  void uv__process_title_cleanup(void) {
--  uv_mutex_destroy(&process_title_mutex);
-+  if (process_title_mutex_inited)
-+    uv_mutex_destroy(&process_title_mutex);
++  uv_once(&process_title_mutex_once, init_process_title_mutex_once);
+   uv_mutex_destroy(&process_title_mutex);
  }
- 
  
