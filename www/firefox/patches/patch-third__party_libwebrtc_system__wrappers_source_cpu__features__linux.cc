5f3ead1ecdf3b2e1fcaa26e9f723a7afb0f5c566
tnn <tnn@pkgsrc.org>
2022-01-17 10:22:54 +0000

    firefox: fix build on NetBSD/evbarm

--- www/firefox/patches/patch-third__party_libwebrtc_system__wrappers_source_cpu__features__linux.cc
+++ www/firefox/patches/patch-third__party_libwebrtc_system__wrappers_source_cpu__features__linux.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-third__party_libwebrtc_system__wrappers_source_cpu__features__linux.cc,v 1.1 2022/01/15 15:57:38 ryoon Exp $
+$NetBSD: patch-third__party_libwebrtc_system__wrappers_source_cpu__features__linux.cc,v 1.2 2022/01/17 10:22:54 tnn Exp $
 
 * Fix build under NetBSD at least.
 
---- third_party/libwebrtc/system_wrappers/source/cpu_features_linux.cc.orig	2021-12-28 21:40:10.000000000 +0000
+--- third_party/libwebrtc/system_wrappers/source/cpu_features_linux.cc.orig	2022-01-13 19:26:15.000000000 +0000
 +++ third_party/libwebrtc/system_wrappers/source/cpu_features_linux.cc
 @@ -8,7 +8,9 @@
   *  be found in the AUTHORS file in the root of the source tree.
@@ -14,3 +14,12 @@ $NetBSD: patch-third__party_libwebrtc_system__wrappers_source_cpu__features__lin
  #include <stdlib.h>
  #include <string.h>
  
+@@ -30,7 +32,7 @@
+ #include "rtc_base/system/arch.h"
+ #include "system_wrappers/include/cpu_features_wrapper.h"
+ 
+-#if defined(WEBRTC_ARCH_ARM_FAMILY)
++#if defined(WEBRTC_ARCH_ARM_FAMILY) && defined(__linux__)
+ #include <asm/hwcap.h>
+ 
+ namespace webrtc {
