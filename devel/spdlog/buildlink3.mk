6a86eb90014704b0bd56b3383c0dbcfda6073304
tnn <tnn@pkgsrc.org>
2022-01-18 15:20:29 +0000

    spdlog: drop BUILDLINK_DEPMETHOD.spdlog=build
    no longer appropriate with dynamic library

--- devel/spdlog/buildlink3.mk
+++ devel/spdlog/buildlink3.mk
@@ -1,13 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/02/05 13:32:27 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/01/18 15:20:29 tnn Exp $
 
 BUILDLINK_TREE+=	spdlog
 
 .if !defined(SPDLOG_BUILDLINK3_MK)
 SPDLOG_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.spdlog?=	build
-
-BUILDLINK_API_DEPENDS.spdlog+=	spdlog>=1.5.0
+BUILDLINK_API_DEPENDS.spdlog+=	spdlog>=1.8.5nb1
 BUILDLINK_PKGSRCDIR.spdlog?=	../../devel/spdlog
 .endif	# SPDLOG_BUILDLINK3_MK
 
