79e6344bb176db6bd4b1561e6e1fa9d641fe29d7
pho <pho@pkgsrc.org>
2022-01-18 02:22:49 +0000

    Update to GHC 9.0.2
    * Now the package supports PKGSRC_MKPIE.
    * The full list of changes is too long to paste here. See
      https://downloads.haskell.org/~ghc/9.0.2/docs/html/users_guide/9.0.2-notes.html

--- lang/ghc90/patches/patch-rts_posix_OSThreads.c
+++ lang/ghc90/patches/patch-rts_posix_OSThreads.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-rts_posix_OSThreads.c,v 1.2 2021/05/05 09:18:52 pho Exp $
+$NetBSD: patch-rts_posix_OSThreads.c,v 1.3 2022/01/18 02:22:50 pho Exp $
 
 Implementation of getNumberOfProcessors() for NetBSD
 
 Sent to the upstream:
 https://gitlab.haskell.org/ghc/ghc/-/merge_requests/5679
 
---- rts/posix/OSThreads.c.orig	2021-04-22 12:36:33.476115260 +0000
+--- rts/posix/OSThreads.c.orig	2021-09-15 15:27:32.000000000 +0000
 +++ rts/posix/OSThreads.c
-@@ -14,6 +14,11 @@
-  * DragonflyBSD, because of some specific types, like u_char, u_int, etc. */
+@@ -25,6 +25,11 @@
+ #if defined(freebsd_HOST_OS) || defined(dragonfly_HOST_OS)
  #define __BSD_VISIBLE   1
  #endif
 +#if defined(netbsd_HOST_OS)
@@ -17,9 +17,9 @@ https://gitlab.haskell.org/ghc/ghc/-/merge_requests/5679
 +#define _NETBSD_SOURCE 1
 +#endif
  #if defined(darwin_HOST_OS)
- /* Inclusion of system headers usually requires _DARWIN_C_SOURCE on Mac OS X
-  * because of some specific types like u_char, u_int, etc. */
-@@ -42,7 +47,7 @@
+ #define _DARWIN_C_SOURCE 1
+ #endif
+@@ -51,7 +56,7 @@
  #include <string.h>
  #endif
  
@@ -28,7 +28,7 @@ https://gitlab.haskell.org/ghc/ghc/-/merge_requests/5679
  #include <sys/types.h>
  #include <sys/sysctl.h>
  #endif
-@@ -279,6 +284,13 @@ getNumberOfProcessors (void)
+@@ -292,6 +297,13 @@ getNumberOfProcessors (void)
              if(sysctlbyname("hw.ncpu",&nproc,&size,NULL,0) != 0)
                  nproc = 1;
          }
