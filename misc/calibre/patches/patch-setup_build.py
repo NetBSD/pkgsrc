6377fd18fd33bc04556a878d3a17e0578a38b85d
rhialto <rhialto@pkgsrc.org>
2022-01-20 19:03:07 +0000

    misc/calibre: update major version, to 5.34.0.
    Major change: now uses Python 3.
    Other changes: too many to list; see the file Changelog.txt in the distfile.
    
    New option: rar, to allow calibre to look inside rar archives.
    
    Actually connecting an ebook-reader device is still untested, but might
    work better with the libusb and libmtp extensions that are enabled since
    version 4.23.0.

--- misc/calibre/patches/patch-setup_build.py
+++ misc/calibre/patches/patch-setup_build.py
@@ -1,29 +1,35 @@
-$NetBSD: patch-setup_build.py,v 1.4 2022/01/09 15:43:51 rhialto Exp $
+$NetBSD: patch-setup_build.py,v 1.5 2022/01/20 19:03:07 rhialto Exp $
 
 - Fix linking against native X.
 - Include netbsd in various conditional building criteria.
+- Patch in the directory /usr/pkg/share/sip3.9/PyQt5 for
+  QtWidgets/QtWidgetsmod.sip etc.
+- Define the %Platform value WS_X11, so that Q_PID gets defined in
+  PyQt5/QtCore/qprocess.sip.
+- Autodetection of the sip API version of PyQt5.so fails because sip 4
+  does not write the QtCore.toml file with that information.
 
---- setup/build.py.orig	2020-02-21 03:27:12.000000000 +0000
+--- setup/build.py.orig	2021-12-17 00:40:19.000000000 +0000
 +++ setup/build.py
-@@ -10,7 +10,7 @@ __docformat__ = 'restructuredtext en'
- import textwrap, os, shlex, subprocess, glob, shutil, re, sys, json
+@@ -8,7 +8,7 @@ __docformat__ = 'restructuredtext en'
+ import textwrap, os, shlex, subprocess, glob, shutil, sys, json, errno, sysconfig
  from collections import namedtuple
  
--from setup import Command, islinux, isbsd, isfreebsd, isosx, ishaiku, SRC, iswindows, __version__, ispy3
-+from setup import Command, islinux, isbsd, isfreebsd, isnetbsd, isosx, ishaiku, SRC, iswindows, __version__, ispy3
- isunix = islinux or isosx or isbsd or ishaiku
+-from setup import Command, islinux, isbsd, isfreebsd, ismacos, ishaiku, SRC, iswindows
++from setup import Command, islinux, isbsd, isfreebsd, ismacos, ishaiku, SRC, iswindows, isnetbsd
+ isunix = islinux or ismacos or isbsd or ishaiku
  
  py_lib = os.path.join(sys.prefix, 'libs', 'python%d%d.lib' % sys.version_info[:2])
-@@ -103,7 +103,7 @@ def is_ext_allowed(ext):
+@@ -116,7 +116,7 @@ def is_ext_allowed(ext):
      only = ext.get('only', '')
      if only:
          only = set(only.split())
--        q = set(filter(lambda x: globals()["is" + x], ["bsd", "freebsd", "haiku", "linux", "osx", "windows"]))
-+        q = set(filter(lambda x: globals()["is" + x], ["bsd", "freebsd", "haiku", "linux", "osx", "windows", "netbsd"]))
+-        q = set(filter(lambda x: globals()["is" + x], ["bsd", "freebsd", "haiku", "linux", "macos", "windows"]))
++        q = set(filter(lambda x: globals()["is" + x], ["bsd", "freebsd", "haiku", "linux", "macos", "windows", "netbsd"]))
          return len(q.intersection(only)) > 0
      return True
  
-@@ -124,6 +124,8 @@ def parse_extension(ext):
+@@ -137,6 +137,8 @@ def parse_extension(ext):
              ans = ext.pop('bsd_' + k, ans)
          elif isfreebsd:
              ans = ext.pop('freebsd_' + k, ans)
@@ -32,7 +38,7 @@ $NetBSD: patch-setup_build.py,v 1.4 2022/01/09 15:43:51 rhialto Exp $
          elif ishaiku:
              ans = ext.pop('haiku_' + k, ans)
          else:
-@@ -412,6 +414,7 @@ class Build(Command):
+@@ -496,6 +498,7 @@ class Build(Command):
              INCLUDEPATH += {freetype}
              DESTDIR = {destdir}
              CONFIG -= create_cmake  # Prevent qmake from generating a cmake build file which it puts in the calibre src directory
@@ -40,11 +46,31 @@ $NetBSD: patch-setup_build.py,v 1.4 2022/01/09 15:43:51 rhialto Exp $
              ''').format(
                  headers=' '.join(headers), sources=' '.join(sources), others=' '.join(others), destdir=self.d(
                      target), freetype=' '.join(ft_inc_dirs))
-@@ -472,6 +475,7 @@ class Build(Command):
-         SOURCES = {sources}
-         INCLUDEPATH += {sipinc} {pyinc}
-         VERSION = {ver}
-+        QMAKE_LFLAGS += $(COMPILER_RPATH_FLAG)$(X11BASE)/lib
-         win32 {{
-             LIBS += {py_lib}
-             TARGET_EXT = .dll
+@@ -521,8 +524,11 @@ class Build(Command):
+         abi_version = ''
+         if pyqt_sip_abi_version():
+             abi_version = f'abi-version = "{pyqt_sip_abi_version()}"'
++        else:
++            abi_version = f'abi-version = "12.7"'   # 12.7 sip-4.19.25
+         sipf = ext.sip_files[0]
+         needs_exceptions = 'true' if ext.needs_exceptions else 'false'
++        sip_include_dirs = [os.getenv('SIP_DIR')]
+         with open(os.path.join(src_dir, 'pyproject.toml'), 'w') as f:
+             f.write(f'''
+ [build-system]
+@@ -538,6 +544,7 @@ project-factory = "pyqtbuild:PyQtProject
+ 
+ [tool.sip.project]
+ sip-files-dir = "."
++sip-include-dirs = {sip_include_dirs}
+ {abi_version}
+ 
+ [tool.sip.bindings.pictureflow]
+@@ -547,6 +554,7 @@ exceptions = {needs_exceptions}
+ include-dirs = {ext.inc_dirs}
+ qmake-QT = ["widgets"]
+ sip-file = "{os.path.basename(sipf)}"
++tags = ["WS_X11"]
+ ''')
+         shutil.copy2(sipf, src_dir)
+ 
