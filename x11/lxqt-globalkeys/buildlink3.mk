1773dff5d568b919adae5299431f3c504f85d73e
pin <pin@pkgsrc.org>
2022-01-21 07:26:31 +0000

    x11/lxqt-globalkeys: update to 1.0.1
    -The asynchronous behavior of Xlib is kept intact. It guarantees that the
     global shortcut daemon starts correctly with the session after a change in the
     behavior of libx11 1.7.3.1.

--- x11/lxqt-globalkeys/buildlink3.mk
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2021/12/08 16:02:56 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/01/21 07:26:31 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=1.0.0
-BUILDLINK_ABI_DEPENDS.lxqt-globalkeys?=	lxqt-globalkeys>=1.0.0
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=1.0.1
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
