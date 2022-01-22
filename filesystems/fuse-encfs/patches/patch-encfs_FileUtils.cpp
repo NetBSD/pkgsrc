f7648bd4a8097071b54ede86f27833c3a03f64db
pho <pho@pkgsrc.org>
2022-01-22 18:32:47 +0000

    Conditionalise workarounds for old NetBSD librefuse

--- filesystems/fuse-encfs/patches/patch-encfs_FileUtils.cpp
+++ filesystems/fuse-encfs/patches/patch-encfs_FileUtils.cpp
@@ -1,10 +1,9 @@
-$NetBSD: patch-encfs_FileUtils.cpp,v 1.3 2021/12/01 04:45:52 pho Exp $
+$NetBSD: patch-encfs_FileUtils.cpp,v 1.4 2022/01/22 18:32:47 pho Exp $
 
-NetBSD ReFUSE doesn't have the fuse_unmount from FUSE 2.2 - FUSE 2.9.
+NetBSD ReFUSE didn't have the fuse_unmount from FUSE 2.2 - FUSE 2.9.
 Workaround by using the version belonging to FUSE >= 3.0.
 
-This might be a temporal aberration: NetBSD ReFUSE had the newer FUSE
-API before upstream FUSE did.
+Already fixed in HEAD.
 
 --- encfs/FileUtils.cpp.orig	2018-04-27 08:52:22.000000000 +0000
 +++ encfs/FileUtils.cpp
@@ -12,7 +11,7 @@ API before upstream FUSE did.
  
  void unmountFS(const char *mountPoint) {
    // fuse_unmount returns void, is assumed to succeed
-+#if defined(__NetBSD__)
++#if defined(__NetBSD__) && FUSE_H_ < 20211204
 +  fuse_unmount(fuse_get_context()->fuse);
 +#else
    fuse_unmount(mountPoint, nullptr);
