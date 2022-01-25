610ddee3311f9dabe13540287b131b637dc6e598
wiz <wiz@pkgsrc.org>
2022-01-25 10:30:50 +0000

    py-uwsgi: update to 2.0.20.
    Changes
    
        Switch default python for build to python3 (Riccardo Magliocchetti)
        Add support for PHP 8 (Riccardo Magliocchetti)
        Drop support for PHP < 7 as it is EOL since end of 2018 (Riccardo Magliocchetti)
        Fix segfaults when using –wsgi-env-behavior=holy (Antonio Cuni)
        Replace uwsgi.h system includes in core and proto dirs for Bazel (Serge Bazanski)
        gevent: fix compilation with clang11 (László Károlyi)
        Fix Python 3.9 deprecations warnings (Riccardo Magliocchetti)
        Add trove classifier for Python 3.9 (Adrian)
        Fix message in Log SIGINT/SIGTERM triggered kill_them_all (Delena Malan)
        Support 7 in weekedays as an alias for sunday to match crontab behaviour (Riccardo Magliocchetti)
        Document http-timeout default of 60 seconds (Etienne H)
        Add option to override python sys.executable using py-executable config
        Allow specifying an iteration to uwsgi::add_rb_timer (Luciano Rocha)
        Allow to compile with Python versions with minor version with 2+ digits (Cyrille Pontvieux)
        Take into account new naming for LIBPL since python 3.6 (ilrico)
        Added missing HTTP status codes messages (James Brown)
        Official support for Python 3.10

--- www/py-uwsgi/patches/patch-uwsgi.h
+++ www/py-uwsgi/patches/patch-uwsgi.h
@@ -1,21 +1,9 @@
-$NetBSD: patch-uwsgi.h,v 1.8 2021/08/05 13:22:30 adam Exp $
+$NetBSD: patch-uwsgi.h,v 1.9 2022/01/25 10:30:51 wiz Exp $
 
-Avoid clash with sys/user.h inclusion in logging.c on DragonFly BSD.
 Avoid using "sun" which breaks on SunOS
 
 --- uwsgi.h.orig	2020-06-17 09:03:34.000000000 +0000
 +++ uwsgi.h
-@@ -322,6 +322,10 @@ extern int pivot_root(const char *new_ro
- #elif defined(__CYGWIN__)
- #elif defined(__HURD__)
- #else
-+#if defined(__DragonFly__)
-+/* avoid clash with sys/user.h inclusion in logging.c */
-+#define _KERNEL_STRUCTURES
-+#endif /* __DragonFly__ */
- #include <sys/event.h>
- #endif
- 
 @@ -1631,7 +1635,7 @@ struct wsgi_request {
  	union address {
  		struct sockaddr_in sin;
