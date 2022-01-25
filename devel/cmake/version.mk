2b9ab9c71920f576e5058412927035c5033b17f0
adam <adam@pkgsrc.org>
2022-01-25 21:18:08 +0000

    cmake cmake-gui: updated to 3.22.2
    CMake 3.22.2
    * VS: Remove the '/guard:cf' flag from v143 link flag table
    * FortranCInterface: Fix compatibility with GCC gfortran 12 LTO
    * FindBoost: Add support for Boost 1.78
    * ci: Explicitly disable Java tests on Windows
    * FindGLUT: Provide legacy GLUT_INCLUDE_DIR result in pkg-config code path
    * FindBoost: Do not warn about now-supported version 1.78
    * ci: Explicitly disable Java tests on VS builds
    * file: Restore error capture in undocumented READ_ELF mode
    * MSVC: Use -external:I flag without space to support Clang tools
    * Help: Add missing 3.22.1 section to the release notes
    * gitlab-ci: update macOS jobs to use Xcode 13.2
    * ASM: Fix identification of armasm 6.17+
    * FindBoost: Add support for Python 3.10
    * XL: Detect default extensions mode for legacy compiler
    * CMP0128: Prefix test names with mode
    * CMP0128: Avoid test code duplication
    * CMP0128: Add flag in OLD mode even when standard matches the default
    * Android: Fix linking android_support for pre-21 system STL
    * FindPython: Add support for Python 3.11
    * VS: Fix detecting icx.exe with Intel Compiler toolsets newer than 2021

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.23 2021/12/08 07:59:09 adam Exp $
+# $NetBSD: version.mk,v 1.24 2022/01/25 21:18:08 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.22.1
+CMAKE_VERSION=	3.22.2
 CMAKE_API=	${CMAKE_VERSION:R}
