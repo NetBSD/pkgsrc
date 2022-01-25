3ad61f57203111b87f72b8242f8a8a74d45c74dc
adam <adam@pkgsrc.org>
2022-01-25 21:18:08 +0000

    cmake cmake-gui: updated to 3.22.2
    CMake 3.22.2
    * VS: Remove the '/guard:cf' flag from v143 link flag table
    * FortranCInterface: Fix compatibility with GCC gfortran 12 LTO
    * FindBoost: Add support for Boost 1.78
    * ci: Explicitly disable Java tests on Windows
    * FindGLUT: Provide legacy GLUT_INCLUDE_DIR result in pkg-config code path
    * FindBoost: Do not warn about now-supported version 1.78
    * ci: Explicitly disable Java tests on VS builds
    * file: Restore error capture in undocumented READ_ELF mode
    * MSVC: Use -external:I flag without space to support Clang tools
    * Help: Add missing 3.22.1 section to the release notes
    * gitlab-ci: update macOS jobs to use Xcode 13.2
    * ASM: Fix identification of armasm 6.17+
    * FindBoost: Add support for Python 3.10
    * XL: Detect default extensions mode for legacy compiler
    * CMP0128: Prefix test names with mode
    * CMP0128: Avoid test code duplication
    * CMP0128: Add flag in OLD mode even when standard matches the default
    * Android: Fix linking android_support for pre-21 system STL
    * FindPython: Add support for Python 3.11
    * VS: Fix detecting icx.exe with Intel Compiler toolsets newer than 2021

--- devel/cmake/patches/patch-Modules_FindPython_Support.cmake
+++ devel/cmake/patches/patch-Modules_FindPython_Support.cmake
@@ -1,8 +1,8 @@
-$NetBSD: patch-Modules_FindPython_Support.cmake,v 1.2 2020/11/25 10:33:28 adam Exp $
+$NetBSD: patch-Modules_FindPython_Support.cmake,v 1.3 2022/01/25 21:18:08 adam Exp $
 
 Insist on select Python version for pkgsrc.
 
---- Modules/FindPython/Support.cmake.orig	2020-11-24 13:50:26.000000000 +0000
+--- Modules/FindPython/Support.cmake.orig	2022-01-25 20:22:39.000000000 +0000
 +++ Modules/FindPython/Support.cmake
 @@ -21,7 +21,9 @@ endif()
  if (NOT DEFINED _${_PYTHON_PREFIX}_REQUIRED_VERSION_MAJOR)
@@ -12,6 +12,6 @@ Insist on select Python version for pkgsrc.
 +if (DEFINED PYVERSSUFFIX)
 +  set(_${_PYTHON_PREFIX}_VERSIONS ${PYVERSSUFFIX})
 +elseif (_${_PYTHON_PREFIX}_REQUIRED_VERSION_MAJOR EQUAL "3")
-   set(_${_PYTHON_PREFIX}_VERSIONS 3.10 3.9 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0)
+   set(_${_PYTHON_PREFIX}_VERSIONS 3.11 3.10 3.9 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0)
  elseif (_${_PYTHON_PREFIX}_REQUIRED_VERSION_MAJOR EQUAL "2")
    set(_${_PYTHON_PREFIX}_VERSIONS 2.7 2.6 2.5 2.4 2.3 2.2 2.1 2.0)
