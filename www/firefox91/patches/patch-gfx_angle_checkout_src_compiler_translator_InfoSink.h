69861af66f6f17e8894b01b184dbfaea42d0fe08
ryoon <ryoon@pkgsrc.org>
2022-01-26 13:38:06 +0000

    firefox91: Update to 91.5.0
    Changelog:
    Security fixes:
    #CVE-2022-22746: Calling into reportValidity could have lead to fullscreen
    #CVE-2022-22743: Browser window spoof using fullscreen mode
    #CVE-2022-22742: Out-of-bounds memory access when inserting text in edit mode
    #CVE-2022-22741: Browser window spoof using fullscreen mode
    #CVE-2022-22740: Use-after-free of ChannelEventQueue::mOwner
    #CVE-2022-22738: Heap-buffer-overflow in blendGaussianBlur
    #CVE-2022-22737: Race condition when playing audio files
    #CVE-2021-4140: Iframe sandbox bypass with XSLT
    #CVE-2022-22748: Spoofed origin on external protocol launch dialog
    #CVE-2022-22745: Leaking cross-origin URLs through securitypolicyviolation
     event
    #CVE-2022-22744: The 'Copy as curl' feature in DevTools did not fully escape
     website-controlled data, potentially leading to command injection
    #CVE-2022-22747: Crash when handling empty pkcs7 sequence
    #CVE-2022-22739: Missing throttling on external protocol launch dialog
    #CVE-2022-22751: Memory safety bugs fixed in Firefox 96 and Firefox ESR 91.5

--- www/firefox91/patches/patch-gfx_angle_checkout_src_compiler_translator_InfoSink.h
+++ www/firefox91/patches/patch-gfx_angle_checkout_src_compiler_translator_InfoSink.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-gfx_angle_checkout_src_compiler_translator_InfoSink.h,v 1.1 2021/09/08 22:19:50 nia Exp $
+$NetBSD: patch-gfx_angle_checkout_src_compiler_translator_InfoSink.h,v 1.2 2022/01/26 13:38:07 ryoon Exp $
 
 * isinf/isnan in make.h is defined as macro. Use non-macro version
   to fix build.
 
---- gfx/angle/checkout/src/compiler/translator/InfoSink.h.orig	2018-10-18 20:06:05.000000000 +0000
+--- gfx/angle/checkout/src/compiler/translator/InfoSink.h.orig	2022-01-06 00:52:35.000000000 +0000
 +++ gfx/angle/checkout/src/compiler/translator/InfoSink.h
 @@ -7,7 +7,7 @@
  #ifndef COMPILER_TRANSLATOR_INFOSINK_H_
@@ -12,5 +12,5 @@ $NetBSD: patch-gfx_angle_checkout_src_compiler_translator_InfoSink.h,v 1.1 2021/
 -#include <math.h>
 +#include <cmath>
  #include <stdlib.h>
+ #include "GLSLANG/ShaderLang.h"
  #include "compiler/translator/Common.h"
- #include "compiler/translator/Severity.h"
