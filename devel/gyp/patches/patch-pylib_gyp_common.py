e2d9e33d843792de6cc2dd284c98b0e4a3f31877
wiz <wiz@pkgsrc.org>
2022-01-27 18:42:15 +0000

    gyp: update to last snapshot
    This project is now "idle"...
    
    Add patch for compatibility with python 3.10.

--- devel/gyp/patches/patch-pylib_gyp_common.py
+++ devel/gyp/patches/patch-pylib_gyp_common.py
@@ -1,10 +1,20 @@
-$NetBSD: patch-pylib_gyp_common.py,v 1.4 2017/01/11 14:47:51 wiz Exp $
+$NetBSD: patch-pylib_gyp_common.py,v 1.5 2022/01/27 18:42:15 wiz Exp $
 
+* Python 3.10 support
 * Add NetBSD support
 
---- pylib/gyp/common.py.orig	2017-01-11 14:31:33.000000000 +0000
+--- pylib/gyp/common.py.orig	2022-01-27 18:36:11.000000000 +0000
 +++ pylib/gyp/common.py
-@@ -441,6 +441,7 @@ def CopyTool(flavor, out_path, generator
+@@ -4,7 +4,7 @@
+ 
+ from __future__ import with_statement
+ 
+-import collections
++import collections.abc as collections
+ import errno
+ import filecmp
+ import os.path
+@@ -445,6 +445,7 @@ def CopyTool(flavor, out_path, generator
    prefix = {
        'aix': 'flock',
        'solaris': 'flock',
