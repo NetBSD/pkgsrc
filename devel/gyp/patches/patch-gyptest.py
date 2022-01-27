02c7010f8ca626b10c59539561fa85b46103fdf0
wiz <wiz@pkgsrc.org>
2022-01-27 18:42:15 +0000

    gyp: update to last snapshot
    This project is now "idle"...
    
    Add patch for compatibility with python 3.10.

--- devel/gyp/patches/patch-gyptest.py
+++ devel/gyp/patches/patch-gyptest.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-gyptest.py,v 1.4 2014/08/21 14:49:43 he Exp $
+$NetBSD: patch-gyptest.py,v 1.5 2022/01/27 18:42:15 wiz Exp $
 
 * Add NetBSD 5, 6 and 7 target
 
---- gyptest.py.orig	2014-07-14 14:19:50.000000000 +0000
+--- gyptest.py.orig	2022-01-27 18:36:11.000000000 +0000
 +++ gyptest.py
-@@ -219,6 +219,10 @@ def main(argv=None):
+@@ -98,6 +98,10 @@ def main(argv=None):
        'aix5':     ['make'],
        'freebsd7': ['make'],
        'freebsd8': ['make'],
