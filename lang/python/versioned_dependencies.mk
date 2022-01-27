656e2be144b5ef2040e4f0c9bec363c6e5ee5dc1
wiz <wiz@pkgsrc.org>
2022-01-27 10:50:51 +0000

    python: support testtools as versioned dependency

--- lang/python/versioned_dependencies.mk
+++ lang/python/versioned_dependencies.mk
@@ -1,4 +1,4 @@
-# $NetBSD: versioned_dependencies.mk,v 1.59 2022/01/25 09:04:29 wiz Exp $
+# $NetBSD: versioned_dependencies.mk,v 1.60 2022/01/27 10:50:51 wiz Exp $
 #
 # This file determines which separate distribution of a Python
 # package is used as dependency, depending on the Python version
@@ -9,7 +9,7 @@
 # PYTHON_VERSIONED_DEPENDENCIES
 #       The Python package which should be added as a dependency.
 #
-#       Possible values: Pmw Pillow X cairo html2text hypothesis importlib-metadata ipython jsonlib markdown more-itertools pip packaging pygments pyparsing python-digest rsa setuptools setuptools_scm sphinx test zipp
+#       Possible values: Pmw Pillow X cairo html2text hypothesis importlib-metadata ipython jsonlib markdown more-itertools pip packaging pygments pyparsing python-digest rsa setuptools setuptools_scm sphinx test testtools zipp
 #       Default: (nothing)
 #
 
@@ -41,6 +41,7 @@ _SUPPORTED_PACKAGES+=	setuptools devel/py-setuptools44 devel/py-setuptools
 _SUPPORTED_PACKAGES+=	setuptools_scm devel/py-setuptools_scm5 devel/py-setuptools_scm
 _SUPPORTED_PACKAGES+=	sphinx textproc/py-sphinx1 textproc/py-sphinx
 _SUPPORTED_PACKAGES+=	test devel/py-test4 devel/py-test
+_SUPPORTED_PACKAGES+=	testtools devel/py-testtools24 devel/py-testtools
 _SUPPORTED_PACKAGES+=	zipp archivers/py-zipp1 archivers/py-zipp
 
 .for pattern in ${PYTHON_VERSIONED_DEPENDENCIES}
