4c20ae087ab4cc9baa5206af49d9a3d559fca0e0
schmonz <schmonz@pkgsrc.org>
2022-01-28 20:38:18 +0000

    Go has dropped support for Darwin-*-i386.

--- pkgtools/pkglint/select.mk
+++ pkgtools/pkglint/select.mk
@@ -1,4 +1,4 @@
-# $NetBSD: select.mk,v 1.7 2020/05/02 10:43:44 tnn Exp $
+# $NetBSD: select.mk,v 1.8 2022/01/28 20:38:18 schmonz Exp $
 #
 # Selects the proper version of pkglint, depending on whether the
 # platform supports the Go programming language.
@@ -10,6 +10,7 @@
 # While it's wrong in the above, go14 does not build on NetBSD 5.
 .if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm:Naarch64} || \
     ${MACHINE_PLATFORM:MSunOS-*-i386} || \
+    ${MACHINE_PLATFORM:MDarwin-*-i386} || \
     ${MACHINE_PLATFORM:MNetBSD-[1-5].*-*} || \
     ${MACHINE_PLATFORM} == "NetBSD-9.0-aarch64"
 DEPENDS+=	pkglint4>=4.82<5:../../pkgtools/pkglint4
