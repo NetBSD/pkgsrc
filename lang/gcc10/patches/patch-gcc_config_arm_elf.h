3f8bd4648f89af791be5e4841ca852f473903a0e
mrg <mrg@pkgsrc.org>
2022-02-02 22:07:28 +0000

    pull over the rest of netbsd arm32 configuration from -current.
    fixes PR#56679.  (tested in armv7hf-el chroot on armv8, and also tested
    doesn't break amd64 version.)

---
+++ lang/gcc10/patches/patch-gcc_config_arm_elf.h
@@ -0,0 +1,14 @@
+$NetBSD: patch-gcc_config_arm_elf.h,v 1.1 2022/02/02 22:07:28 mrg Exp $
+
+--- gcc/config/arm/elf.h.orig	2021-04-08 04:56:28.065740630 -0700
++++ gcc/config/arm/elf.h	2022-01-31 21:14:04.684223580 -0800
+@@ -147,6 +147,8 @@
+ #undef L_floatdidf
+ #undef L_floatdisf
+ #undef L_floatundidf
++/* XXXMRG: don't take this out, we need it! */
++# ifndef __NetBSD__
+ #undef L_floatundisf
++# endif
+ #endif
+-
