9e9ae534308dbceb4c3085593f0bca9e45e509d1
wiz <wiz@pkgsrc.org>
2022-02-04 12:12:27 +0000

    wasi-compiler-rt: pkglint whitespace fix

--- lang/wasi-compiler-rt/buildlink3.mk
+++ lang/wasi-compiler-rt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/12/19 14:05:53 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/02/04 12:12:27 wiz Exp $
 
 BUILDLINK_TREE+=	wasi-compiler-rt
 
@@ -8,7 +8,7 @@ BUILDLINK_DEPMETHOD.wasi-compiler-rt?=	build
 WASI_COMPILER_RT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.wasi-compiler-rt+=	wasi-compiler-rt>=13.0.0
-BUILDLINK_PKGSRCDIR.wasi-compiler-rt?=	../../lang/wasi-compiler-rt
+BUILDLINK_PKGSRCDIR.wasi-compiler-rt?=		../../lang/wasi-compiler-rt
 
 .endif	# WASI_COMPILER_RT_BUILDLINK3_MK
 
