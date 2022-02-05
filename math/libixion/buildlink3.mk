bec61e649eb65ffd217f40b01e1b991096fdec99
ryoon <ryoon@pkgsrc.org>
2022-02-05 04:28:11 +0000

    {py-,}libixion: Update to 0.17.0
    Changelog:
    0.17.0:
    C++ API
        set the baseline C++ version to 17.
        mdds has been internalized so that the public header no longer contains references to mdds. With this change, the users can use different API versions of mdds between the ixion build and run-time use.
        cleaned up public API to make use of std::string_view and std::variant where appropriate.
    formula interpreter
        implemented built-in LEFT() function.
    misc
        it is no longer required to set the size of void* at build time to ensure the binaries to be fully functional.
        fixed a bug where named expressions with names containing invalid characters were still allowed in.

--- math/libixion/buildlink3.mk
+++ math/libixion/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2021/02/06 06:23:22 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/02/05 04:28:11 ryoon Exp $
 
 BUILDLINK_TREE+=	libixion
 
 .if !defined(LIBIXION_BUILDLINK3_MK)
 LIBIXION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.16.1
+BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.17.0
 BUILDLINK_PKGSRCDIR.libixion?=		../../math/libixion
 
 .include "../../devel/mdds/buildlink3.mk"
