3880b6f2a111cf4caf66d3a01a23c9521934c347
ryoon <ryoon@pkgsrc.org>
2022-02-05 04:14:31 +0000

    orcus: Update to 0.17.2
    Changelog:
    orcus 0.17.2
    
    * ods
    
      * fixed a bug where the state of style:cell-protect="none" was not
        explicitly pushed, thereby having had the same effect as not having this
        attribute.  After the fix, style:cell-protect="none" will explicitly push
        the hidden state to false, locked state to false, and the formula-hidden
        state to false.
    
    orcus 0.17.1
    
    * general
    
      * addressed a number of coverity issues.
    
      * removed a variety of compiler warnings.
    
    * ods
    
      * re-generated sax parser tokens from ODF v1.3.
    
      * revised the style import code to only push style attributes that are
        actually specified in the XML.
    
    * xls-xml
    
      * revised the XML structure validation strategy to ignore any mis-placed
        elements and their sub structures rather than aborting the import.
    
    orcus 0.17.0
    
    * general
    
      * set the baseline C++ version to 17.
    
      * cleaned up the public API to replace pstring with std::string_view, union
        with std::variant, and boost::optional with std::optional.  With this
        change, the public API no longer has dependency on boost.
    
    * spreadsheet document
    
      * switched to using ixion::model_iterator for horizontal iteration of cells
        instead of using mdds::mtv::collection.
    
      * fixed a bug where exporting a spreadsheet document containing adjacent
        merged cells regions to html incorrectly exported the merged cell areas.
    
    * xlsx
    
      * cached cell values are now correctly loaded from the file.
    
    * sax parser
    
      * utf-8 names are now allowed as element and attribute names.
    
    * css parser
    
      * unquoted utf-8 property values are now allowed.
    
    * orcus-json
    
      * fixed segmentation fault when using --mode structure with the Windows
        build.
    
      * added yaml output option.
    
    * xml-map
    
      * fixed a bug where mapping of an XML document with namespace aliases
        sometimes corrupts the alias values.
    
    * python
    
      * added orcus.FormulaTokenOp enum type which describes type formula token
        operator types in a more finer grained manner.
    
    * documentation
    
      * added notes to how to use orcus-xml and orcus-json to map XML and JSON
        documents to spreadsheet documents.

--- converters/orcus/buildlink3.mk
+++ converters/orcus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2021/02/06 06:29:04 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2022/02/05 04:14:31 ryoon Exp $
 
 BUILDLINK_TREE+=	orcus
 
 .if !defined(ORCUS_BUILDLINK3_MK)
 ORCUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.16.1
+BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.17.0
 BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
 
 .endif	# ORCUS_BUILDLINK3_MK
