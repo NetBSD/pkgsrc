74f17bbcdd4c9701db05f4b5325451762e80261f
wiz <wiz@pkgsrc.org>
2022-02-07 10:48:26 +0000

    libfilezilla: update to 0.36.0.
    0.36.0 (2022-02-02)
    
    + Added parameter to tls_layer::set_alpn to control which peer has priority over which ALPN is chosen.
    + Added fz::hmac_sha1
    + Mutex debugging framework to detect locking order inversions that may lead to deadlocks
    + Add additional io_redirect mode to fz::process_spawn where the parent-side handles for stdin/out/err are always closed
    - fz::json: Handle object members with empty string as name
    - Minor performance improvements to removing timers and to spawning processes
    
    0.35.0 (2021-12-08)
    
    + *nix: Added fz::forkblock which can be used to safely set FD_CLOEXEC on descriptors even if the system lacks SOCK_CLOCKEXEC, MSG_CMSG_CLOEXEC, pipe2 or accept4
    + macOS: Impersonation support
    + Added fz::tls_layer::set_unexpected_eof_cb, in some situations it may be desirable that unexpected closure is not reported as a hard errror
    - Added various convenience overloads for fz::buffer
    - Performance improvement for fz::json::to_string
    
    0.34.2 (2021-10-26)
    
    + fz::file::open now returns fz::result
    - fz::tls_layer: Additional fixes for trust path extration
    - MSW: fz::local_filesys::get_next_file now handles directory contents returned by the kernel in oversized buffers with embedded nulls
    
    0.34.1 (2021-10-19)
    
    - *nix: Handle supplementary groups when impersonating
    - *nix: fz::recv_fd now sets the MSG_CMSG_CLOEXEC flag
    - *nix: fz::local_filesys::get_link_target now handles link targets larger than 1024 bytes
    - MSW: Restrict DLL search path for system DLLs to the system32 directory
    - fz::tls_layer: Fixed how trust path extration interacts with CRLs
    
    0.34.0 (2021-10-11)
    
    - MSW: Load shell32.dll and ole32.dll on-demand at runtime, it is not available in all environments
    - Made fz::local_filesys movable
    
    0.33.0 (2021-09-29)
    
    + MSW: Add local_filesys::begin_find_files overload accepting a directory HANDLE
    + If peer certificate chain is trusted by the system trust store, tls_session_info::get_certificates now returns the actual path to the trust anchor, use tls_session_info::get_peer_certificates to get the peer certificates as received by the server. GnuTLS 3.7.0 or later is required.
    - JSON: Correctly handle \u-encoded UTF-16 surrogate pairs
    
    0.32.0 (2021-09-14)
    
    + MSW: Impersonation support
    - Allow more direct control over session ticket/PSK generation under TLS 1.3, requires GnuTLS 3.6.14 or later
    - Ensure an error is returned if accepting a socket fails
    - Fixed appending to fz::buffer
    
    0.32.0-beta1 (2021-08-30)
    
    + *nix: Impersonation support
    + *nix: Sending of file descriptors over Unix Domain Sockets
    * nix: Add local_filesys::begin_find_files overload accepting a directory file descriptor
    + Allow creating fz::file from a file descriptors/handle, add function to detach the descriptor/handle
    + Added fz::datetime::operator>=
    + Added fz::duration::absolute()
    - fz::sprintf no longer crashes on x and X conversion specifiers if a negative signed integer is passed as argument
    - Replace std::random_device as the C++ standard allows it to not be random
    - JSON: Fixed crash if assigning values from nested values
    - JWS: Fixed memory leak and padding of signature components

---
+++ net/libfilezilla/patches/patch-lib_impersonation.cpp
@@ -0,0 +1,33 @@
+$NetBSD: patch-lib_impersonation.cpp,v 1.1 2022/02/07 10:48:26 wiz Exp $
+
+Hide Linux-specific stuff in ifdef __Linux__
+
+--- lib/impersonation.cpp.orig	2021-12-08 15:10:58.000000000 +0000
++++ lib/impersonation.cpp
+@@ -7,7 +7,7 @@
+ #include <optional>
+ #include <tuple>
+ 
+-#if FZ_UNIX
++#if FZ_UNIX && defined(__Linux__)
+ #include <crypt.h>
+ #include <shadow.h>
+ #endif
+@@ -58,7 +58,7 @@ passwd_holder get_passwd(fz::native_stri
+ 	return ret;
+ }
+ 
+-#if FZ_UNIX
++#if FZ_UNIX && defined(__Linux__)
+ struct shadow_holder {
+ 	shadow_holder() = default;
+ 	shadow_holder(shadow_holder const&) = delete;
+@@ -150,7 +150,7 @@ std::vector<gid_t> get_supplementary(std
+ 
+ bool check_auth(fz::native_string const& username, fz::native_string const& password)
+ {
+-#if FZ_UNIX
++#if FZ_UNIX && defined(__Linux__)
+ 	auto shadow = get_shadow(username);
+ 	if (shadow.shadow_) {
+ 		struct crypt_data data{};
