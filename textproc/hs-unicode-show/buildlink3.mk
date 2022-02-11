7d1f6c5fb69bfca8fd8b6782db7d26e370db1718
pho <pho@pkgsrc.org>
2022-02-11 01:18:01 +0000

    Update to unicode-show-0.1.1.0
    No changelogs are provided by the upstream.

--- textproc/hs-unicode-show/buildlink3.mk
+++ textproc/hs-unicode-show/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 01:18:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-show
 
 .if !defined(HS_UNICODE_SHOW_BUILDLINK3_MK)
 HS_UNICODE_SHOW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.0.4nb2
+BUILDLINK_API_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.1.0
 BUILDLINK_PKGSRCDIR.hs-unicode-show?=	../../textproc/hs-unicode-show
 .endif	# HS_UNICODE_SHOW_BUILDLINK3_MK
 
