2149990483d2ecb9954d3fea653f3739b6b53a68
pho <pho@pkgsrc.org>
2022-02-11 12:53:20 +0000

    Update to base16-bytestring-1.0.2.0
    1.0.2.0
        Support sized primitive types in GHC 9.2 (#16 - thanks Bodigrim!)

--- converters/hs-base16-bytestring/buildlink3.mk
+++ converters/hs-base16-bytestring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 12:53:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-base16-bytestring
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-base16-bytestring
 HS_BASE16_BYTESTRING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=1.0.1.0nb2
+BUILDLINK_ABI_DEPENDS.hs-base16-bytestring+=	hs-base16-bytestring>=1.0.2.0
 BUILDLINK_PKGSRCDIR.hs-base16-bytestring?=	../../converters/hs-base16-bytestring
 .endif	# HS_BASE16_BYTESTRING_BUILDLINK3_MK
 
