e7d368d48084b619d3d075d454dee24b17a22621
pho <pho@pkgsrc.org>
2022-02-11 09:22:07 +0000

    Update to unix-compat-0.5.4
    No changelogs are provided by the upstream.

--- devel/hs-unix-compat/buildlink3.mk
+++ devel/hs-unix-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 09:22:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-compat
 
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.3
-BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.3nb2
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.4
+BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.4
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../devel/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
