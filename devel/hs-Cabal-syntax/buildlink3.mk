b8b7ce19e9f3a731e00dbc9f8364a04d227bea4c
pho <pho@pkgsrc.org>
2022-02-11 13:13:24 +0000

    devel/hs-Cabal-syntax: import hs-Cabal-syntax-3.6.0.0
    This library provides tools for reading and manipulating the .cabal file
    format.

---
+++ devel/hs-Cabal-syntax/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 13:13:24 pho Exp $
+
+BUILDLINK_TREE+=	hs-Cabal-syntax
+
+.if !defined(HS_CABAL_SYNTAX_BUILDLINK3_MK)
+HS_CABAL_SYNTAX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-Cabal-syntax+=	hs-Cabal-syntax>=3.6.0
+BUILDLINK_ABI_DEPENDS.hs-Cabal-syntax+=	hs-Cabal-syntax>=3.6.0.0
+BUILDLINK_PKGSRCDIR.hs-Cabal-syntax?=	../../devel/hs-Cabal-syntax
+.endif	# HS_CABAL_SYNTAX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-Cabal-syntax
