ae2954e5700570aab8323c07f28a98afd691337a
pho <pho@pkgsrc.org>
2022-02-11 12:06:12 +0000

    lang/hs-hslua-classes: import hs-hslua-classes-2.1.0
    Type classes for convenient marshalling and calling of Lua functions.

---
+++ lang/hs-hslua-classes/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 12:06:12 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-classes
+
+.if !defined(HS_HSLUA_CLASSES_BUILDLINK3_MK)
+HS_HSLUA_CLASSES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-hslua-classes?=		../../lang/hs-hslua-classes
+
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.endif	# HS_HSLUA_CLASSES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-classes
