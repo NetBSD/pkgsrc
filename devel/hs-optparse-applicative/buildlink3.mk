7241d69a09072ba352838813461f45d20feea234
pho <pho@pkgsrc.org>
2022-02-11 19:58:53 +0000

    Update to optparse-applicative-
    Version 0.17.0.0 (1 Feb 2022)
    * Make tabulation width configurable in usage texts.
    * Separate program name and description in ParserHelp type.
    * Add helperWith function, which can be easily used to localize the help
      flag.
    * Improve usage texts when command names are long.
    * Improve Documentation.

--- devel/hs-optparse-applicative/buildlink3.mk
+++ devel/hs-optparse-applicative/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 19:58:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-optparse-applicative
 
 .if !defined(HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK)
 HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.16.1
-BUILDLINK_ABI_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.16.1.0nb2
+BUILDLINK_API_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.17.0
+BUILDLINK_ABI_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.17.0.0
 BUILDLINK_PKGSRCDIR.hs-optparse-applicative?=	../../devel/hs-optparse-applicative
 
 .include "../../textproc/hs-ansi-wl-pprint/buildlink3.mk"
