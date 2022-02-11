714ac07e8704c6741cd2827e638f67aacc006f04
pho <pho@pkgsrc.org>
2022-02-11 13:22:37 +0000

    Update to prettyprinter-ansi-terminal-1.1.3
    1.1.3
        Deprecate the Data.Text.Prettyprint.* modules
            Users should migrate to the new Prettyprinter module hierarchy.
            The old modules will be removed no sooner than September 2022.

--- textproc/hs-prettyprinter-ansi-terminal/buildlink3.mk
+++ textproc/hs-prettyprinter-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 13:22:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-prettyprinter-ansi-terminal
 
 .if !defined(HS_PRETTYPRINTER_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_PRETTYPRINTER_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-prettyprinter-ansi-terminal+=	hs-prettyprinter-ansi-terminal>=1.1.2
-BUILDLINK_ABI_DEPENDS.hs-prettyprinter-ansi-terminal+=	hs-prettyprinter-ansi-terminal>=1.1.2nb2
+BUILDLINK_API_DEPENDS.hs-prettyprinter-ansi-terminal+=	hs-prettyprinter-ansi-terminal>=1.1.3
+BUILDLINK_ABI_DEPENDS.hs-prettyprinter-ansi-terminal+=	hs-prettyprinter-ansi-terminal>=1.1.3
 BUILDLINK_PKGSRCDIR.hs-prettyprinter-ansi-terminal?=	../../textproc/hs-prettyprinter-ansi-terminal
 
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
