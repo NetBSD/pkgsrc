862798dd87fcccbb8a3412cf962f5f4838eea840
pho <pho@pkgsrc.org>
2022-02-11 12:07:14 +0000

    lang/hs-hslua-objectorientation: import hs-hslua-objectorientation-2.1.0
    Expose Haskell objects to Lua with an object oriented interface.

---
+++ lang/hs-hslua-objectorientation/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 12:07:14 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-objectorientation
+
+.if !defined(HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK)
+HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-hslua-objectorientation?=	../../lang/hs-hslua-objectorientation
+
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.endif	# HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-objectorientation
