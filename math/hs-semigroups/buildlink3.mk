f10aaa76ade506e62b4896fe95411d8511d44c15
pho <pho@pkgsrc.org>
2022-02-11 19:49:22 +0000

    Update to semigroups-0.20
    0.20 [2021.11.15]
    * Support hashable-1.4. The Hashable1 instances added in 0.19.2 are removed
      for all types except NonEmpty, in accordance with the corresponding
      changes from hashable-1.4.
    
    0.19.2 [2021.08.30]
    * Backport Hashable1 instances for NonEmpty, Min, Max, First, Last,
      WrappedMonoid, and Option.

--- math/hs-semigroups/buildlink3.mk
+++ math/hs-semigroups/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2022/01/18 02:48:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2022/02/11 19:49:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.19.1
-BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.19.1nb3
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.20
+BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.20
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../math/hs-semigroups
 
 .include "../../devel/hs-hashable/buildlink3.mk"
