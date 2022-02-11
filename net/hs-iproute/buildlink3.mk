44b641df2b8cbce2b4f7e7861b24066d2e74418f
pho <pho@pkgsrc.org>
2022-02-11 13:11:33 +0000

    Update to iproute-1.7.12
    No changelogs are provided by the upstream.

--- net/hs-iproute/buildlink3.mk
+++ net/hs-iproute/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 13:11:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-iproute
 
 .if !defined(HS_IPROUTE_BUILDLINK3_MK)
 HS_IPROUTE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-iproute+=	hs-iproute>=1.7.11
-BUILDLINK_ABI_DEPENDS.hs-iproute+=	hs-iproute>=1.7.11nb2
+BUILDLINK_API_DEPENDS.hs-iproute+=	hs-iproute>=1.7.12
+BUILDLINK_ABI_DEPENDS.hs-iproute+=	hs-iproute>=1.7.12
 BUILDLINK_PKGSRCDIR.hs-iproute?=	../../net/hs-iproute
 
 .include "../../textproc/hs-appar/buildlink3.mk"
