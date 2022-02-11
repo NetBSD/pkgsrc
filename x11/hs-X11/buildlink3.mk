d13c5262684d5bfadfdea4f50cb868321642c0c6
pho <pho@pkgsrc.org>
2022-02-11 15:53:34 +0000

    Update to X11-1.10.2
    1.10.2 (2021-10-24)
    * Restored compatibility with GHC 7.10 (and possibly even older) (#80)
    
    1.10.1 (2021-08-15)
    * Fixed possible high CPU usage of some blocking calls with the threaded
      RTS (#78)
    
    1.10 (2021-05-31)
    * Added setClientMessageEvent' (#71)
    * Fixed type of xrrUpdateConfiguration (#72)
    * Fixed bottom when deserializing XRRNotifyEvent in getEvent (#72)
    * Added xrrGetMonitors and XRRMonitorInfo (#42)
    * Added setClassHint (#76)
    * Added a few missing event mask fields to WindowAttributes (#77)

--- x11/hs-X11/buildlink3.mk
+++ x11/hs-X11/buildlink3.mk
@@ -1,20 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/01/18 02:48:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/02/11 15:53:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-X11
 
 .if !defined(HS_X11_BUILDLINK3_MK)
 HS_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-X11+=	hs-X11>=1.9.2
-BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.9.2nb2
+BUILDLINK_API_DEPENDS.hs-X11+=	hs-X11>=1.10.2
+BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.10.2
 BUILDLINK_PKGSRCDIR.hs-X11?=	../../x11/hs-X11
-
-# Work around a Cabal bug. It ignores "ld-options" in X11.buildinfo
-# while linking shared libraries:
-# https://github.com/haskell/cabal/issues/4925
-HASKELL_ENABLE_SHARED_LIBRARY=	no
-# Note to devs: when you remove this please also remove it from
-# ../../x11/hs-X11/Makefile
 
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
