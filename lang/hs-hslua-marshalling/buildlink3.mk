bbd79a32baee795f9ca90f9ed7a20a4738de2225
pho <pho@pkgsrc.org>
2022-02-11 12:05:02 +0000

    lang/hs-hslua-marshalling: import hs-hslua-marshalling-2.1.0
    Provides functions to marshal values from Haskell to Lua, and vice versa.
    
    This package is part of HsLua, a Haskell framework built around the
    embeddable scripting language Lua.

---
+++ lang/hs-hslua-marshalling/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 12:05:02 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-marshalling
+
+.if !defined(HS_HSLUA_MARSHALLING_BUILDLINK3_MK)
+HS_HSLUA_MARSHALLING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-hslua-marshalling?=	../../lang/hs-hslua-marshalling
+
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.endif	# HS_HSLUA_MARSHALLING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-marshalling
