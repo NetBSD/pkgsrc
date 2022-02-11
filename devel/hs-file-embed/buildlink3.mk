fd5f314e118fde23789489d48bbde8b7100b5360
pho <pho@pkgsrc.org>
2022-02-11 12:15:07 +0000

    Update to file-embed-0.0.15.0
    0.0.15.0
        Add makeRelativeToLocationPredicate
    
    0.0.14.0
        Add embedFileIfExists

--- devel/hs-file-embed/buildlink3.mk
+++ devel/hs-file-embed/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 12:15:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-file-embed
 
 .if !defined(HS_FILE_EMBED_BUILDLINK3_MK)
 HS_FILE_EMBED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.13
-BUILDLINK_ABI_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.13.0nb2
+BUILDLINK_API_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.15
+BUILDLINK_ABI_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.15.0
 BUILDLINK_PKGSRCDIR.hs-file-embed?=	../../devel/hs-file-embed
 .endif	# HS_FILE_EMBED_BUILDLINK3_MK
 
