cc3357dfff1adfe20ce4f3efa68fbf420571ec78
pho <pho@pkgsrc.org>
2022-02-11 13:30:32 +0000

    Update to doclayout-0.3.1.1
    0.3.1.1
    * Fix the end of the block of zero width characters which contains the
      zero-width joiners and directional markings (Stephen Morgan, #5). This
      fixes a regression introduced in 0.3.1, affecting code points 0x2010 to
      0x2030.
    
    0.3.1
    * Improved handling of emojis. Emojis are double-wide, but previously this
      library did not treat them as such. We now have comprehensive support of
      emojis, including variation modifiers and zero-width joiners, verified by
      a test suite. Performance has been confirmed to be no worse for text
      without emojis. (Stephen Morgan, #1). API changes: export
      realLengthNoShortcut, isEmojiModifier, isEmojiVariation, isEmojiJoiner.

--- textproc/hs-doclayout/buildlink3.mk
+++ textproc/hs-doclayout/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:30:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-doclayout
 
 .if !defined(HS_DOCLAYOUT_BUILDLINK3_MK)
 HS_DOCLAYOUT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3.0.2nb2
+BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.3.1.1
 BUILDLINK_PKGSRCDIR.hs-doclayout?=	../../textproc/hs-doclayout
 
+.include "../../converters/hs-emojis/buildlink3.mk"
 .include "../../devel/hs-safe/buildlink3.mk"
 .endif	# HS_DOCLAYOUT_BUILDLINK3_MK
 
