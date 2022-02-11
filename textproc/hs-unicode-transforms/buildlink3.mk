ad2955511c50ca2ee6bf264e8970ad22603fe201
pho <pho@pkgsrc.org>
2022-02-11 09:51:15 +0000

    Import unicode-transform-0.4.0
    0.4.0 (November 2021)
        Bump unicode-data to 0.2
        Allow text 2.0
        Update to Unicode version 14.0.0
        Drop support for GHC 7.10
    
    0.3.8
        Allow ghc-prim 0.7
        Extract unicode-data into its own package
        Depend on the latest stable text

--- textproc/hs-unicode-transforms/buildlink3.mk
+++ textproc/hs-unicode-transforms/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 09:51:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-transforms
 
 .if !defined(HS_UNICODE_TRANSFORMS_BUILDLINK3_MK)
 HS_UNICODE_TRANSFORMS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.3.7
-BUILDLINK_ABI_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.3.7.1nb2
+BUILDLINK_API_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-unicode-transforms?=	../../textproc/hs-unicode-transforms
+
+.include "../../textproc/hs-unicode-data/buildlink3.mk"
 .endif	# HS_UNICODE_TRANSFORMS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-unicode-transforms
