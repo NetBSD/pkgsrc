a78d356c240086775a7889675448613939b21c43
pho <pho@pkgsrc.org>
2022-02-11 12:08:17 +0000

    lang/hs-hslua-packaging: import hs-hslua-packaging-2.1.0
    Utilities to package up Haskell functions and values into a Lua module.
    
    This package is part of HsLua, a Haskell framework built around the
    embeddable scripting language Lua.

---
+++ lang/hs-hslua-packaging/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 12:08:17 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-packaging
+
+.if !defined(HS_HSLUA_PACKAGING_BUILDLINK3_MK)
+HS_HSLUA_PACKAGING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-hslua-packaging?=	../../lang/hs-hslua-packaging
+
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-objectorientation/buildlink3.mk"
+.endif	# HS_HSLUA_PACKAGING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-packaging
