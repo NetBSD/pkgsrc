df7b71e32989262507d2a1604209a60c214de950
pho <pho@pkgsrc.org>
2022-02-11 12:18:39 +0000

    Update to emojis-0.1.2
    0.1.2
    * Update emoji data (Unicode 14.0) and aliases from gemoji.
    * Export baseEmojis and zwjEmojis, generated directly from Unicode emoji
      data files, so that the library contains a complete list of emojis,
      including emojis for which gemoji provides no alias (Stephen Morgan).
    
    0.1.1
    * Update emoji data.

--- converters/hs-emojis/buildlink3.mk
+++ converters/hs-emojis/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 12:18:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-emojis
 
 .if !defined(HS_EMOJIS_BUILDLINK3_MK)
 HS_EMOJIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1
-BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1nb2
+BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-emojis?=		../../converters/hs-emojis
 .endif	# HS_EMOJIS_BUILDLINK3_MK
 
