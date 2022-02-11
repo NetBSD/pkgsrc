102d8273663980c9336d6db2d77734fbc412c7d1
pho <pho@pkgsrc.org>
2022-02-11 09:46:16 +0000

    Update to ansi-terminal-0.11.1
    Version 0.11.1
    * On Windows, fix compatability with the Windows I/O Manager (WinIO)
      introduced in GHC 9.0.1, by incorporating changes made in package
      Win32-2.13.2.0 in that regard.
    * Improvements to Haddock documentation.

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/01/18 02:48:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/02/11 09:46:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11nb2
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.1
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
 
 .include "../../devel/hs-colour/buildlink3.mk"
