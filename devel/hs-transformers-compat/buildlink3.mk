f028cfc40f75ac9996ae47d1429663cde768f499
pho <pho@pkgsrc.org>
2022-02-11 13:27:44 +0000

    Update to transformers-compat-0.7.1
    0.7.1 [2021.10.30]
    * Backport new instances from GHC 9.2/base-4.16
      * Eq1, Read1, and Show1 instances for Complex
      * Eq{1,2}, Ord{1,2}, Show{1,2}, and Read{1,2} instances for (,,) and
        (,,,)
    
    0.7 [2021.07.25]
    * Backport changes from transformers-0.6.*:
      * Remove the long-deprecated selectToCont function from
        Control.Monad.Trans.Select.
      * Backport various Generic, Generic1, and Data instances.
      * Backport handleE, tryE, and finallyE to Control.Monad.Trans.Except.
      * Backport explicit implementations of (<$), liftA2, (*>), and (<*) for
        Control.Applicative.Backwards.
      * Backport a lazier implementation of (<*>) for Control.Applicative.Lift.

--- devel/hs-transformers-compat/buildlink3.mk
+++ devel/hs-transformers-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/01/18 02:48:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/02/11 13:27:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers-compat
 
 .if !defined(HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK)
 HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.6.6
-BUILDLINK_ABI_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.6.6nb2
+BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.7.1
 BUILDLINK_PKGSRCDIR.hs-transformers-compat?=	../../devel/hs-transformers-compat
 
 .include "../../devel/hs-fail/buildlink3.mk"
