de1ca39f85556b639872b946bb148ee6455a874e
pho <pho@pkgsrc.org>
2022-02-11 11:29:12 +0000

    Import jira-wiki-markup-1.4.0
    1.4.0 - Released 2021-05-25.
        Allow quoted image parameters.
        Added support for "smart links".
        API Change: Add new constructors SmartCard and SmartLink to
        Text.Jira.Markup.LinkType.
    
    1.3.5 - Released 2021-05-24.
        Allow spaces and most unicode characters in attachment links.
        No longer require a newline character after {noformat}.
        Only allow URI path segment characters in bare links.
        The file: schema is no longer allowed in bare links; these rarely make
        sense.

--- textproc/hs-jira-wiki-markup/buildlink3.mk
+++ textproc/hs-jira-wiki-markup/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 11:29:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-jira-wiki-markup
 
 .if !defined(HS_JIRA_WIKI_MARKUP_BUILDLINK3_MK)
 HS_JIRA_WIKI_MARKUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.3.4
-BUILDLINK_ABI_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.3.4nb2
+BUILDLINK_API_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.4.0
+BUILDLINK_ABI_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.4.0
 BUILDLINK_PKGSRCDIR.hs-jira-wiki-markup?=	../../textproc/hs-jira-wiki-markup
 .endif	# HS_JIRA_WIKI_MARKUP_BUILDLINK3_MK
 
