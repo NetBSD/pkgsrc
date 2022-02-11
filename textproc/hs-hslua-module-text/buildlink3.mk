8a4c8a0e59446c996524867c25bdf58d5fd1da01
pho <pho@pkgsrc.org>
2022-02-11 13:29:21 +0000

    Update to hslua-module-text-1.0.1
    hslua-module-text-1.0.1 - Released 29-01-2022.
    * Relaxed upper bound of hslua-core, hslua-marshalling, and
      hslua-packaging, allowing their respective version 2.1.
    
    hslua-module-text-1.0.0 - Released 2021-10-22.
    * Use hslua 2.0.

--- textproc/hs-hslua-module-text/buildlink3.mk
+++ textproc/hs-hslua-module-text/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:29:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-text
 
 .if !defined(HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK)
 HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.3.0.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-text?=	../../textproc/hs-hslua-module-text
 
-.include "../../lang/hs-hslua/buildlink3.mk"
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-packaging/buildlink3.mk"
 .endif	# HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hslua-module-text
