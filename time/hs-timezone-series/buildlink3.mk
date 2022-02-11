f2607f1a81a08adff745dfd6408f4dbfe8120eb8
pho <pho@pkgsrc.org>
2022-02-11 01:16:29 +0000

    Update to timezone-series-0.1.13
    No changelogs are provided by the upstream.

--- time/hs-timezone-series/buildlink3.mk
+++ time/hs-timezone-series/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/01/18 02:48:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/02/11 01:16:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-timezone-series
 
 .if !defined(HS_TIMEZONE_SERIES_BUILDLINK3_MK)
 HS_TIMEZONE_SERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.9
-BUILDLINK_ABI_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.9nb2
+BUILDLINK_API_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.13
+BUILDLINK_ABI_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.13
 BUILDLINK_PKGSRCDIR.hs-timezone-series?=	../../time/hs-timezone-series
 .endif	# HS_TIMEZONE_SERIES_BUILDLINK3_MK
 
