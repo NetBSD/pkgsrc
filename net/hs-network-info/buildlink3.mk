afd80d35ddf0195c0319e875cc395fc0a0fb4af1
pho <pho@pkgsrc.org>
2022-02-11 09:07:31 +0000

    Update to network-info-0.2.1
    0.2.1 (2022-01-31)
    * Support NetBSD (#21)
    * Increase upper bound on interfaces to 128 (#24)
    * Fix segmentation fault for more than 64 interfaces (#23)

--- net/hs-network-info/buildlink3.mk
+++ net/hs-network-info/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/01/18 02:48:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/02/11 09:07:31 pho Exp $
 
 BUILDLINK_TREE+=	hs-network-info
 
 .if !defined(HS_NETWORK_INFO_BUILDLINK3_MK)
 HS_NETWORK_INFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network-info+=	hs-network-info>=0.2.0
-BUILDLINK_ABI_DEPENDS.hs-network-info+=	hs-network-info>=0.2.0.10nb1
+BUILDLINK_API_DEPENDS.hs-network-info+=	hs-network-info>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-network-info+=	hs-network-info>=0.2.1
 BUILDLINK_PKGSRCDIR.hs-network-info?=	../../net/hs-network-info
 .endif	# HS_NETWORK_INFO_BUILDLINK3_MK
 
