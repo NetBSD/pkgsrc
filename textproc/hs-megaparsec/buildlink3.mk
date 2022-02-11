a5aed88459734b0e416ffeac036d292b9aece8c3
pho <pho@pkgsrc.org>
2022-02-11 17:51:59 +0000

    Update to megaparsec-
    Megaparsec 9.2.0
    * Added parsers for binary representations (little/big endian) of numbers
      in Text.Megaparsec.Byte.Binary.
    
    Megaparsec 9.1.0
    * Added dbg' in Text.Megaparsec.Debug for debugging parsers that have
      unshowable return values.
    * Documentation improvements.

--- textproc/hs-megaparsec/buildlink3.mk
+++ textproc/hs-megaparsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 17:51:59 pho Exp $
 
 BUILDLINK_TREE+=	hs-megaparsec
 
 .if !defined(HS_MEGAPARSEC_BUILDLINK3_MK)
 HS_MEGAPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.0.1
-BUILDLINK_ABI_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.0.1nb2
+BUILDLINK_API_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.2.0
+BUILDLINK_ABI_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.2.0
 BUILDLINK_PKGSRCDIR.hs-megaparsec?=	../../textproc/hs-megaparsec
 
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
