d2845d2d497054818ce6a26990cfab7456a0df13
pho <pho@pkgsrc.org>
2022-02-11 09:23:39 +0000

    Update to fingertree-0.1.5.0
    0.1.5.0 Ross Paterson Jan 2022
    * Added foldlWithPos, foldrWithPos, foldlWithContext, foldrWithContext
      (James Cranch)
    * Fixed bug in traverseWithContext

--- devel/hs-fingertree/buildlink3.mk
+++ devel/hs-fingertree/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:23:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-fingertree
 
 .if !defined(HS_FINGERTREE_BUILDLINK3_MK)
 HS_FINGERTREE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fingertree+=	hs-fingertree>=0.0.1
-BUILDLINK_ABI_DEPENDS.hs-fingertree+=	hs-fingertree>=0.1.4.2nb2
+BUILDLINK_API_DEPENDS.hs-fingertree+=	hs-fingertree>=0.1.5
+BUILDLINK_ABI_DEPENDS.hs-fingertree+=	hs-fingertree>=0.1.5.0
 BUILDLINK_PKGSRCDIR.hs-fingertree?=	../../devel/hs-fingertree
 .endif	# HS_FINGERTREE_BUILDLINK3_MK
 
