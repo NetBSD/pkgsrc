7bd55dad362187401ea27f77370cdd7217ee76f4
pho <pho@pkgsrc.org>
2022-02-11 13:37:21 +0000

    Update to scientific-0.3.7.0
    0.3.7.0
    * Make division (/) on Scientifics slightly more efficient.
    * Fix the Show instance to surround negative numbers with parentheses when
      necessary.
    * Add (Template Haskell) Lift Scientific instance
    * Mark modules as Safe or Trustworthy (Safe Haskell).

--- math/hs-scientific/buildlink3.mk
+++ math/hs-scientific/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2022/01/18 02:48:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2022/02/11 13:37:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-scientific
 
 .if !defined(HS_SCIENTIFIC_BUILDLINK3_MK)
 HS_SCIENTIFIC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-scientific+=	hs-scientific>=0.3.6
-BUILDLINK_ABI_DEPENDS.hs-scientific+=	hs-scientific>=0.3.6.2nb3
+BUILDLINK_API_DEPENDS.hs-scientific+=	hs-scientific>=0.3.7
+BUILDLINK_ABI_DEPENDS.hs-scientific+=	hs-scientific>=0.3.7.0
 BUILDLINK_PKGSRCDIR.hs-scientific?=	../../math/hs-scientific
 
 .include "../../devel/hs-hashable/buildlink3.mk"
