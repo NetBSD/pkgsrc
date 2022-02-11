81abeef98b09b6ad52318f7781f6855469de8c85
pho <pho@pkgsrc.org>
2022-02-11 13:25:31 +0000

    Update to resourcet-1.2.4.3
    1.2.4.3
        Fix a space leak when using forever with ResourceT. #470

--- devel/hs-resourcet/buildlink3.mk
+++ devel/hs-resourcet/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/01/18 02:48:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/02/11 13:25:31 pho Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4.2
-BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4.2nb2
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4
+BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4.3
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 
 .include "../../devel/hs-primitive/buildlink3.mk"
