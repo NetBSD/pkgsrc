b7a189c16307bf88afd2087ce1396decddc9abd9
pho <pho@pkgsrc.org>
2022-02-11 09:21:05 +0000

    Update to indexed-traversable-0.1.2
    0.1.2 [2021-10-30]
    
        Changed (<$>) + (<*>) to liftA2 to potentially avoid extra fmap.
        Add (#..), coercive composition after a 2-parameter function.
        Switched references to lens 'Of'- functions to base functions.
    
    Thanks to wygulmage for contributions.

--- devel/hs-indexed-traversable/buildlink3.mk
+++ devel/hs-indexed-traversable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:21:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-indexed-traversable
 
 .if !defined(HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK)
 HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.1nb2
+BUILDLINK_API_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-indexed-traversable?=	../../devel/hs-indexed-traversable
 .endif	# HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK
 
