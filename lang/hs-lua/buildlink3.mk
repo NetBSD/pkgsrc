369126afe46a1d97679f5b5b533dd1644d821c1a
pho <pho@pkgsrc.org>
2022-02-11 11:59:11 +0000

    Import lua-2.1.0
    This package provides low-level bindings and types to bridge Haskell and
    Lua.

---
+++ lang/hs-lua/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 11:59:11 pho Exp $
+
+BUILDLINK_TREE+=	hs-lua
+
+.if !defined(HS_LUA_BUILDLINK3_MK)
+HS_LUA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-lua?=	../../lang/hs-lua
+
+.include "../../lang/lua53/buildlink3.mk"
+.endif	# HS_LUA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lua
