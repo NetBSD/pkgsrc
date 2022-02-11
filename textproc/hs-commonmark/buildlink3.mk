c1a248fc5294c8248934295863e2c52c88840a82
pho <pho@pkgsrc.org>
2022-02-11 11:31:47 +0000

    Import commonmark-0.2.1.1
    0.2.1.1
    * Fix bug in prettyShow for SourceRange (#80). The bug led to an infinite
      loop in certain cases.
    
    0.2.1
    * Use official 0.30 spec.txt.
    * Update HTML block parser for recent spec changes.
    * Fix test case from commonmark/cmark#383. We need to index the list of
      stack bottoms not just by the length mod 3 of the closer but by whether
      it can be an opener, since this goes into the calculation of whether the
      delimiters can match.
    
    0.2
    * Commonmark.Inlines: export LinkInfo(..) [API change].
    * Commonmark.Inlines: export pLink [API chage].
    * Comonmark.ReferenceMap: Add linkPos field to LinkInfo [API change].
    * Commonmark.Tokens: normalize unicode to NFC before tokenizing
      (#57). Normalization might affect detection of flankingness, recognition
      of reference links, etc.
    * Commonmark.Html: add data-prefix to non-HTML5 attributes, as pandoc does.
    * Remove unnecessary build-depends.
    * Use lightweight tasty-bench instead of criterion for benchmarks.

--- textproc/hs-commonmark/buildlink3.mk
+++ textproc/hs-commonmark/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 11:31:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark
 
 .if !defined(HS_COMMONMARK_BUILDLINK3_MK)
 HS_COMMONMARK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.1.1.4nb2
+BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.1.1
 BUILDLINK_PKGSRCDIR.hs-commonmark?=	../../textproc/hs-commonmark
+
+.include "../../textproc/hs-unicode-transforms/buildlink3.mk"
 .endif	# HS_COMMONMARK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-commonmark
