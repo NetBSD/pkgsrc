fb6e0a92fcf4249ba5542a0a1337ea7499622191
pho <pho@pkgsrc.org>
2022-02-11 12:13:47 +0000

    Update to random-1.2.1
    1.2.1
    * Fix support for ghc-9.2 #99
    * Fix performance regression for ghc-9.0 #101
    * Add uniformEnumM and uniformEnumRM
    * Add initStdGen #103
    * Add globalStdGen #117
    * Add runStateGenST_
    * Ensure that default implementation of ShortByteString generation uses
      unpinned memory. #116
    * Fix #54 with #68 - if exactly one value in the range of floating point is
      infinite, then uniformRM/randomR returns that value.
    * Add default implementation of uniformM that uses Generic #70
    * Random instance for CBool #77
    * Addition of TGen and TGenM #95
    * Addition of tuple instances for Random up to 7-tuple #72

--- devel/hs-random/buildlink3.mk
+++ devel/hs-random/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/01/18 02:48:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/02/11 12:13:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-random
 
 .if !defined(HS_RANDOM_BUILDLINK3_MK)
 HS_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.2.0nb2
+BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.2.1
 BUILDLINK_PKGSRCDIR.hs-random?=		../../devel/hs-random
 
 .include "../../devel/hs-splitmix/buildlink3.mk"
