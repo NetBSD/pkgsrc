88ffaab5b5b8a217b214ec02c097b509a4304b54
pho <pho@pkgsrc.org>
2022-02-11 13:00:37 +0000

    Update to hashable-1.4.0.2
    Version 1.4.0.2
    * Restore older GHC support
    * Support GHC-9.0.2
    
    Version 1.4.0.1
    * text-2.0 compatibility
    
    Version 1.4.0.0
    * Eq is now a superclass of Hashable. Also Eq1 is a superclass of Hashable1
      and Eq2 is a superclass of Hashable2 when exists.
    * Remove Hashable1 Fixed instance
    * Remove Hashable1 Semi.Min/Max/... instances as they don't have Eq1
      instance.
    
    Version 1.3.5.0
    * Add Solo instance (base-4.15+, GHC-9+)
    
    Version 1.3.4.1
    * Fix compilation on 32 bit platforms
    * Fix Tree instance
    
    Version 1.3.4.0
    * Text and ByteString hashes include length. This fixes a variant of
      https://github.com/haskell-unordered-containers/hashable/issues/74 for
      texts and
      bytestrings. https://github.com/haskell-unordered-containers/hashable/pull/223
    * Use correct prime in combine. This should improve the hash quality of
      compound structures on 64bit
      systems. https://github.com/haskell-unordered-containers/hashable/pull/224
    * Add instance for types in containers package
      https://github.com/haskell-unordered-containers/hashable/pull/226
    * Change Int, Int64 and Word64 hashWithSalt
      slightly. https://github.com/haskell-unordered-containers/hashable/pull/227
    
    Version 1.3.3.0
    * Text hashing uses 64-bit FNV prime
    * Don't truncate Text hashvalues on 64bit Windows:
      https://github.com/haskell-unordered-containers/hashable/pull/211
    
    Version 1.3.2.0
    * Add Hashable (Fixed a) for base <4.7 versions.
    * Add documentation:
      * hashable is not a stable hash
      * hashWithSalt may return negative values
      * there is time-compat with Hashable instances for time types.
    * Add random-initial-seed flag causing the initial seed to be randomized on
      each start of an executable using hashable.

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2022/01/18 02:48:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2022/02/11 13:00:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.3.1
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.3.1.0nb3
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.0
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.0.2
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
