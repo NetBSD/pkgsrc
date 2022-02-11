914a83bb37935adaff80209d5bddd97f32c505c1
pho <pho@pkgsrc.org>
2022-02-11 12:19:53 +0000

    Update to base64-bytestring-1.2.1.0
    1.2.1.0
        Bugfix for GHC 9.0.1 memory corruption bug (#46)
            Thanks to Fraser Tweedale and Andrew Lelechenko for logging and
            helping with this fix.

--- converters/hs-base64-bytestring/buildlink3.mk
+++ converters/hs-base64-bytestring/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/01/18 02:48:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/02/11 12:19:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-base64-bytestring
 
 .if !defined(HS_BASE64_BYTESTRING_BUILDLINK3_MK)
 HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.2.0.1nb2
+BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.2.1.0
 BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../converters/hs-base64-bytestring
 .endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
 
