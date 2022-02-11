40f9764b3e0d30458bd233dd9de8931033675da3
pho <pho@pkgsrc.org>
2022-02-11 13:02:00 +0000

    Update to base-compat-batteries-0.12.1
    Changes in 0.12.1 [2021.10.30]
    * Backport Solo to the Data.Tuple.Compat module. If using ghc-prim-0.7.0 or
      later, Solo is taken from ghc-prim. If using an older version of
      ghc-prim, Solo is taken from the OneTuple compatibility library.
    * This coincides with the base-compat-0.12.1 release. Refer to the
      base-compat changelog for more details.
    
    Changes in 0.12.0 [2021.08.29]
    * Data.Semigroup.Compat{.Repl.Batteries} no longer re-exports the Option
      data type or the option function, as both have been removed in base-4.16.
    * This coincides with the base-compat-0.12.0 release. Refer to the
      base-compat changelog for more details.

--- devel/hs-base-compat-batteries/buildlink3.mk
+++ devel/hs-base-compat-batteries/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:02:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat-batteries
 
 .if !defined(HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK)
 HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.11.2
-BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.11.2nb2
+BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.12.1
+BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.12.1
 BUILDLINK_PKGSRCDIR.hs-base-compat-batteries?=		../../devel/hs-base-compat-batteries
 
 .include "../../devel/hs-base-compat/buildlink3.mk"
