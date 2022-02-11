2c5eb1910e7f8a56a85fce14bfa4a1fea514339c
pho <pho@pkgsrc.org>
2022-02-11 09:37:45 +0000

    Update to StateVar-1.2.2
    1.2.2
        Relaxed upper version bound for transformers.

--- devel/hs-StateVar/buildlink3.mk
+++ devel/hs-StateVar/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/01/18 02:48:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/02/11 09:37:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-StateVar
 
 .if !defined(HS_STATEVAR_BUILDLINK3_MK)
 HS_STATEVAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2.1
-BUILDLINK_ABI_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2.1nb2
+BUILDLINK_API_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2.2
+BUILDLINK_ABI_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2.2
 BUILDLINK_PKGSRCDIR.hs-StateVar?=	../../devel/hs-StateVar
 .endif	# HS_STATEVAR_BUILDLINK3_MK
 
