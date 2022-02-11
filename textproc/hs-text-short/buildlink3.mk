1e97ab1081ca70672ba8b9482259daed29ab2051
pho <pho@pkgsrc.org>
2022-02-11 13:08:44 +0000

    Update to text-short-0.1.5
    0.1.5
    * text-2.0 support
    
    0.1.4
    * Fix fromString for single character
      strings. https://github.com/haskell-hvr/text-short/issues/20
    * Add Template Haskell Lift ShortText instance.

--- textproc/hs-text-short/buildlink3.mk
+++ textproc/hs-text-short/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:08:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-short
 
 .if !defined(HS_TEXT_SHORT_BUILDLINK3_MK)
 HS_TEXT_SHORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-short+=	hs-text-short>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-text-short+=	hs-text-short>=0.1.3nb2
+BUILDLINK_API_DEPENDS.hs-text-short+=	hs-text-short>=0.1.5
+BUILDLINK_ABI_DEPENDS.hs-text-short+=	hs-text-short>=0.1.5
 BUILDLINK_PKGSRCDIR.hs-text-short?=	../../textproc/hs-text-short
 
 .include "../../devel/hs-hashable/buildlink3.mk"
