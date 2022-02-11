d1ebb5ab15180699193b87c7d61cf4fe76d303f1
pho <pho@pkgsrc.org>
2022-02-11 09:14:48 +0000

    Update to Diff-0.4.1
    No changelogs are provided by the upstream.

--- textproc/hs-Diff/buildlink3.mk
+++ textproc/hs-Diff/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:14:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-Diff
 
 .if !defined(HS_DIFF_BUILDLINK3_MK)
 HS_DIFF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.4.0
-BUILDLINK_ABI_DEPENDS.hs-Diff+=	hs-Diff>=0.4.0nb2
+BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.4.1
+BUILDLINK_ABI_DEPENDS.hs-Diff+=	hs-Diff>=0.4.1
 BUILDLINK_PKGSRCDIR.hs-Diff?=	../../textproc/hs-Diff
 .endif	# HS_DIFF_BUILDLINK3_MK
 
