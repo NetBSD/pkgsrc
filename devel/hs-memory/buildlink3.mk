c7187483e37182d72ae0c05204a6b616324fa6b7
pho <pho@pkgsrc.org>
2022-02-11 09:35:09 +0000

    Update to memory-0.16.0
    No changelogs are provided by the upstream.

--- devel/hs-memory/buildlink3.mk
+++ devel/hs-memory/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:35:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-memory
 
 .if !defined(HS_MEMORY_BUILDLINK3_MK)
 HS_MEMORY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-memory+=	hs-memory>=0.15.0
-BUILDLINK_ABI_DEPENDS.hs-memory+=	hs-memory>=0.15.0nb2
+BUILDLINK_API_DEPENDS.hs-memory+=	hs-memory>=0.16.0
+BUILDLINK_ABI_DEPENDS.hs-memory+=	hs-memory>=0.16.0
 BUILDLINK_PKGSRCDIR.hs-memory?=		../../devel/hs-memory
 
 .include "../../devel/hs-basement/buildlink3.mk"
