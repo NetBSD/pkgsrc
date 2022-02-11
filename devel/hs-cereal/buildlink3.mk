03d4a4781dc9330c016cbec1367181d6916280a0
pho <pho@pkgsrc.org>
2022-02-11 09:25:14 +0000

    Update to cereal-0.5.8.2
    No changelogs are provided by the upstream.

--- devel/hs-cereal/buildlink3.mk
+++ devel/hs-cereal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:25:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-cereal
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-cereal
 HS_CEREAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-cereal+=	hs-cereal>=0.5.8
-BUILDLINK_ABI_DEPENDS.hs-cereal+=	hs-cereal>=0.5.8.1nb2
+BUILDLINK_ABI_DEPENDS.hs-cereal+=	hs-cereal>=0.5.8.2
 BUILDLINK_PKGSRCDIR.hs-cereal?=		../../devel/hs-cereal
 .endif	# HS_CEREAL_BUILDLINK3_MK
 
