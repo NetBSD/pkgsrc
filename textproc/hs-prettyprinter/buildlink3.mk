3353d0a5023aab4ae7f38f1535567b5c00752449
pho <pho@pkgsrc.org>
2022-02-11 09:19:43 +0000

    Update to prettyprinter-1.7.1
    * Deprecate the Data.Text.Prettyprint.* modules
      * Users should migrate to the new Prettyprinter module hierarchy.
      * The old modules will be removed no sooner than September 2022.
    * Make text an optional dependency:
      * When built with -f-text, any text-based APIs will operate on Strings
        instead.
    * Documentation improvements:
      * #194
      * 1f0bffe

--- textproc/hs-prettyprinter/buildlink3.mk
+++ textproc/hs-prettyprinter/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:19:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-prettyprinter
 
 .if !defined(HS_PRETTYPRINTER_BUILDLINK3_MK)
 HS_PRETTYPRINTER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-prettyprinter+=	hs-prettyprinter>=1.7.0
-BUILDLINK_ABI_DEPENDS.hs-prettyprinter+=	hs-prettyprinter>=1.7.0nb2
+BUILDLINK_API_DEPENDS.hs-prettyprinter+=	hs-prettyprinter>=1.7.1
+BUILDLINK_ABI_DEPENDS.hs-prettyprinter+=	hs-prettyprinter>=1.7.1
 BUILDLINK_PKGSRCDIR.hs-prettyprinter?=		../../textproc/hs-prettyprinter
 .endif	# HS_PRETTYPRINTER_BUILDLINK3_MK
 
