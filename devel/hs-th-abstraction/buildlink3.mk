35172cc667cf3c10df71d2d96236d5bb10a38d5d
pho <pho@pkgsrc.org>
2022-02-11 12:56:30 +0000

    Update to th-abstraction-0.4.3.0
    0.4.3.0 -- 2021.08.30
    * Make applySubstitution avoid capturing type variable binders when
      substituting into foralls.
    * Fix a bug in which resolveTypeSynonyms would incorrectly expand type
      synonyms that are not applied to enough arguments.
    * Allow the test suite to build with GHC 9.2.

--- devel/hs-th-abstraction/buildlink3.mk
+++ devel/hs-th-abstraction/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 12:56:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-abstraction
 
 .if !defined(HS_TH_ABSTRACTION_BUILDLINK3_MK)
 HS_TH_ABSTRACTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.2.0nb2
+BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.3
+BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.3.0
 BUILDLINK_PKGSRCDIR.hs-th-abstraction?=		../../devel/hs-th-abstraction
 .endif	# HS_TH_ABSTRACTION_BUILDLINK3_MK
 
