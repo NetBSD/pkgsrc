2d8a8392671c713d17550eadbfb8372fda6d36d8
pho <pho@pkgsrc.org>
2022-02-11 13:15:07 +0000

    Update to hackage-security-0.6.2.0
    0.6.2.0
    * Safely prepare for when cabal factors out Cabal-syntax
    
    0.6.1.0
    * Support basic auth in package-indices (#252)
    * Fix tests due to new aeson handling of unescaped control sequences (#256)
    * Bump a lot of bounds on packages we depend on

--- security/hs-hackage-security/buildlink3.mk
+++ security/hs-hackage-security/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:15:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-hackage-security
 
 .if !defined(HS_HACKAGE_SECURITY_BUILDLINK3_MK)
 HS_HACKAGE_SECURITY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hackage-security+=	hs-hackage-security>=0.6.0
-BUILDLINK_ABI_DEPENDS.hs-hackage-security+=	hs-hackage-security>=0.6.0.1nb2
+BUILDLINK_API_DEPENDS.hs-hackage-security+=	hs-hackage-security>=0.6.2
+BUILDLINK_ABI_DEPENDS.hs-hackage-security+=	hs-hackage-security>=0.6.2.0
 BUILDLINK_PKGSRCDIR.hs-hackage-security?=	../../security/hs-hackage-security
 
 .include "../../converters/hs-base16-bytestring/buildlink3.mk"
@@ -16,6 +16,7 @@ BUILDLINK_PKGSRCDIR.hs-hackage-security?=	../../security/hs-hackage-security
 .include "../../archivers/hs-tar/buildlink3.mk"
 .include "../../archivers/hs-zlib/buildlink3.mk"
 .include "../../devel/hs-lukko/buildlink3.mk"
+.include "../../devel/hs-Cabal-syntax/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
 .include "../../net/hs-network-uri/buildlink3.mk"
 .endif	# HS_HACKAGE_SECURITY_BUILDLINK3_MK
