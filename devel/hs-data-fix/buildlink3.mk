82f14ec79c99d563b731d10314e45909470392a5
pho <pho@pkgsrc.org>
2022-02-11 13:45:52 +0000

    Update to data-fix-0.3.2
    0.3.2
        Add (un)wrapFix/Mu/Nu
        Support transformers-0.6

--- devel/hs-data-fix/buildlink3.mk
+++ devel/hs-data-fix/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 13:45:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-fix
 
 .if !defined(HS_DATA_FIX_BUILDLINK3_MK)
 HS_DATA_FIX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.1nb2
+BUILDLINK_API_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.2
 BUILDLINK_PKGSRCDIR.hs-data-fix?=	../../devel/hs-data-fix
 
 .include "../../devel/hs-hashable/buildlink3.mk"
