0214c5282813a88fd8761603a840e53a98d6f745
pho <pho@pkgsrc.org>
2022-02-11 13:35:51 +0000

    Update to hslua-module-system-1.0.1
    hslua-module-system-1.0.1 - Released 29-01-2022.
    * Relaxed upper bound of hslua-core, hslua-marshalling, and
      hslua-packaging, allowing their respective version 2.1.
    
    hslua-module-system-1.0.0 - Released 2021-10-21.
    * Use hslua 2.0.

--- devel/hs-hslua-module-system/buildlink3.mk
+++ devel/hs-hslua-module-system/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:35:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-system
 
 .if !defined(HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK)
 HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=0.2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-system?=	../../devel/hs-hslua-module-system
 
-.include "../../lang/hs-hslua/buildlink3.mk"
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-packaging/buildlink3.mk"
 .include "../../sysutils/hs-temporary/buildlink3.mk"
 .endif	# HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK
 
