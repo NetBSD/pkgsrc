d83b717250b66fe43211d28b0c39a09c5bef2cef
pho <pho@pkgsrc.org>
2022-02-11 09:09:05 +0000

    Update to regex-base-0.94.0.2
    0.94.0.2 - 2021-11-16 Andreas Abel
    * Allow text-2.0
    * Remove unused dependency mtl
    * Warning free up to GHC 9.2
    * Tested with GHC 7.0 - 9.2

--- textproc/hs-regex-base/buildlink3.mk
+++ textproc/hs-regex-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/01/18 02:48:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/02/11 09:09:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-base
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-regex-base
 HS_REGEX_BASE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0
-BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0.1nb2
+BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0.2
 BUILDLINK_PKGSRCDIR.hs-regex-base?=	../../textproc/hs-regex-base
 .endif	# HS_REGEX_BASE_BUILDLINK3_MK
 
