5b42b517b79a599bfe5e1bd4bc7d0ea86f43e30e
pho <pho@pkgsrc.org>
2022-02-11 19:55:33 +0000

    Update to storable-record-0.0.6
    No changelogs are provided by the upstream.

--- devel/hs-storable-record/buildlink3.mk
+++ devel/hs-storable-record/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/01/18 02:48:12 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/02/11 19:55:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-storable-record
 
 .if !defined(HS_STORABLE_RECORD_BUILDLINK3_MK)
 HS_STORABLE_RECORD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-storable-record+=	hs-storable-record>=0.0.5
-BUILDLINK_ABI_DEPENDS.hs-storable-record+=	hs-storable-record>=0.0.5nb1
+BUILDLINK_API_DEPENDS.hs-storable-record+=	hs-storable-record>=0.0.6
+BUILDLINK_ABI_DEPENDS.hs-storable-record+=	hs-storable-record>=0.0.6
 BUILDLINK_PKGSRCDIR.hs-storable-record?=	../../devel/hs-storable-record
 
+.include "../../devel/hs-QuickCheck/buildlink3.mk"
 .include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-utility-ht/buildlink3.mk"
 .endif	# HS_STORABLE_RECORD_BUILDLINK3_MK
