eee8fab058640d5548a2e4c025257af5933386d2
pho <pho@pkgsrc.org>
2022-02-11 13:26:15 +0000

    Update to async-2.2.4
    No changelogs are provided by the upstream.

--- devel/hs-async/buildlink3.mk
+++ devel/hs-async/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/01/18 02:48:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/02/11 13:26:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-async
 
 .if !defined(HS_ASYNC_BUILDLINK3_MK)
 HS_ASYNC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-async+=	hs-async>=2.2.3
-BUILDLINK_ABI_DEPENDS.hs-async+=	hs-async>=2.2.3nb2
+BUILDLINK_API_DEPENDS.hs-async+=	hs-async>=2.2.4
+BUILDLINK_ABI_DEPENDS.hs-async+=	hs-async>=2.2.4
 BUILDLINK_PKGSRCDIR.hs-async?=		../../devel/hs-async
 
 .include "../../devel/hs-hashable/buildlink3.mk"
