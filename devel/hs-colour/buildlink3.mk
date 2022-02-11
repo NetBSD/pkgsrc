9324f46458a138a21fdc2ba8f309f62be073ea2d
pho <pho@pkgsrc.org>
2022-02-11 09:36:53 +0000

    Update to colour-2.3.6
    New in version 2.3.6:
    - Minimum base of 4.13.
    - Locked down non-colour imports.
    - Made semigroup instances canonical.
    - Updated dependencies (for testing).

--- devel/hs-colour/buildlink3.mk
+++ devel/hs-colour/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 09:36:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-colour
 
 .if !defined(HS_COLOUR_BUILDLINK3_MK)
 HS_COLOUR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-colour+=	hs-colour>=2.3.5
-BUILDLINK_ABI_DEPENDS.hs-colour+=	hs-colour>=2.3.5nb2
+BUILDLINK_API_DEPENDS.hs-colour+=	hs-colour>=2.3.6
+BUILDLINK_ABI_DEPENDS.hs-colour+=	hs-colour>=2.3.6
 BUILDLINK_PKGSRCDIR.hs-colour?=		../../devel/hs-colour
 .endif	# HS_COLOUR_BUILDLINK3_MK
 
