0d7462484a17f78e1ae13f5f89911e21b93a8f13
pho <pho@pkgsrc.org>
2022-02-11 12:11:55 +0000

    Update to hslua-2.1.0
    hslua-2.1.0 - Released 29-01-2022.
    * Update to hslua-objectorientation 2.1.0. This entails changes to deftype'
      and deftypeGeneric, switching the order of item pusher and list-extractor
      function in the tuple passed as the last argument.
    * Update to hslua-core 2.1.0, hslua-marshalling 2.1.0, and hslua-classes
      2.1.0.
    
    hslua-2.0.1 - Released 2021-11-04.
    * Updated lower bounds of hslua packages:
      hslua >= 2.0.0.2,
      hslua-marshalling >= 2.0.1, and
      hslua-objectorientation >= 2.0.1.
    * This fixes a number of smaller issues; see the respective package
      changelogs for details.
    
    hslua-2.0.0 - Released 2021-10-21.
    * Move module hierarchy from Foreign.Lua to HsLua.
    * Error handling has been reworked completely. The type of exceptions used
      and handled by HsLua is now exposed to the type system. The type Lua
      makes use of a default error type. Custom error handling can be
      implemented by using the LuaE type with an exception type that is an
      instance of class LuaError.
    * Renamed stack index helpers to nth, nthTop, nthBottom, top. The following
      have been removed: stackTop, nthFromTop, nthFromBottom.
    * Extracted raw Lua bindings into new package lua. This means that all
      cabal flags have been moved to package lua as well. Version lua-1.0.0
      contained the Foreign.Lua.Raw hierarchy as present in hslua-1.3.0. See
      that package’s changelog for info on the additional modifications since
      then.
    * The module Foreign.Lua.Raw.Error was merged into the HsLua.Core.Error
      module.
    * The functions getglobal and gettable now return the Lua Type of the
      pushed value.
    * Extracted new packages:
      * hslua-core: the package contains all modules from the Core sub-hierarchy.
      * hslua-classes: typclasses Peekable and Pushable for pushing and
        pulling, as well as function calling.
      * tasty-hslua: makes testing helpers available for reuse.
    * Moved run functions from Util to Core.Run.
    * Moved module Utf8 from the base level into Core.
    * Refactored code to expose Haskell functions to Lua:
    * Removed functions newCFunction, freeCFunction. Use pushHaskellFunction
      instead, it takes care of garbage collection.
    * Renamed typeclass ToHaskellFunction to Exposable, function callFunc to
      invoke. All these have been moved to hslua-classes.
    * The type PreCFunction is now defined in package lua; HaskellFunction is
      defined in hslua-core.
    * Changed pushHaskellFunction to only accept HaskellFunction arguments,
      move it to hslua-core.
    * Removed helper functions addfunction and addfield from Module. Use
      documented functions and fields instead.
    * Added support for a “since” tag on documented functions; allows to mark
      the library version when a function was introduced in its present form.

--- lang/hs-hslua/buildlink3.mk
+++ lang/hs-hslua/buildlink3.mk
@@ -1,15 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 12:11:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua
 
 .if !defined(HS_HSLUA_BUILDLINK3_MK)
 HS_HSLUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua+=	hs-hslua>=1.3.0
-BUILDLINK_ABI_DEPENDS.hs-hslua+=	hs-hslua>=1.3.0.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua+=	hs-hslua>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua+=	hs-hslua>=2.1.0
 BUILDLINK_PKGSRCDIR.hs-hslua?=		../../lang/hs-hslua
 
-.include "../../lang/lua53/buildlink3.mk"
+.include "../../lang/hs-hslua-classes/buildlink3.mk"
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-objectorientation/buildlink3.mk"
+.include "../../lang/hs-hslua-packaging/buildlink3.mk"
 .endif	# HS_HSLUA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hslua
