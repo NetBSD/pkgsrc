d6725fca87bc2f7d35d3d8ac6dbc6605510f2fcd
pho <pho@pkgsrc.org>
2022-02-11 09:31:38 +0000

    Update to base-orphans-0.8.6
    Changes in 0.8.6 [2021.10.29]
    * Backport Eq, Ord, Bounded, Enum, and Ix instances for Solo, introduced in
      GHC 9.2/base-4.16
    * Remove the backported Eq1 and Ord1 instances for Fixed that were
      introduced in base-orphans-0.8.5. While these briefly appeared in a
      release candidate version of GHC 9.2.1, they were ultimately removed from
      the final 9.2.1 release. This release of base-orphans synchronizes with
      that change.
    
    Changes in 0.8.5 [2021.08.29]
    * Backport new instances from GHC 9.2/base-4.16
      * Eq1, Read1, and Show1 instances for Complex
      * Eq1 and Ord1 instances for Fixed
      * Eq{1,2}, Ord{1,2}, Show{1,2}, and Read{1,2} instances for (,,) and (,,,)
      * Semigroup and Monoid instances for Data.Functor.Product and
        Data.Functor.Compose

--- devel/hs-base-orphans/buildlink3.mk
+++ devel/hs-base-orphans/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 09:31:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-orphans
 
 .if !defined(HS_BASE_ORPHANS_BUILDLINK3_MK)
 HS_BASE_ORPHANS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.4
-BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.4nb2
+BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.6
+BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.6
 BUILDLINK_PKGSRCDIR.hs-base-orphans?=	../../devel/hs-base-orphans
 .endif	# HS_BASE_ORPHANS_BUILDLINK3_MK
 
