3baa4289b715acee66199177c8f20dd55423e6b8
pho <pho@pkgsrc.org>
2022-02-11 13:32:14 +0000

    Update to commonmark-extensions-
    0.2.3
    * Allow bare word attribute in fenced_divs (#84). This follows a similar
      change in pandoc (jgm/pandoc#7242).
    
    0.2.2.1
    * Fix commonmark-extensions to build with GHC 9.2 (#81, Joseph
      C. Sible). Currently --allow-newer is needed.
    
    0.2.2
    * Add footnote to gfmExtensions. Note that this also requires additional
      type constraints on gfmExtensions [API change].
    
    0.2.1.2
    * Fix bug with absolute paths in rebase_relative_paths extension on
      Windows.
    
    0.2.1.1
    * Fix bug in wikilinks extensions.
    
    0.2.1
    * Add rebase_relative_paths extension. New exported module
      Commonmark.Extensions.RebaseRelativePaths [API change].
    * Add wikilinks_title_before_pipe and wikilinks_title_after_pipe extensions
      (#69). New exported module Commonmark.Extensions.Wikilinks [API change].

--- textproc/hs-commonmark-extensions/buildlink3.mk
+++ textproc/hs-commonmark-extensions/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 13:32:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark-extensions
 
 .if !defined(HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK)
 HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.0
-BUILDLINK_ABI_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.0.4nb2
+BUILDLINK_API_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.3
 BUILDLINK_PKGSRCDIR.hs-commonmark-extensions?=		../../textproc/hs-commonmark-extensions
 
 .include "../../textproc/hs-commonmark/buildlink3.mk"
 .include "../../converters/hs-emojis/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
 .endif	# HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-commonmark-extensions
