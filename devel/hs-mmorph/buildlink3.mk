0d3467f7745527e5a70b4bb19defa340bab67613
pho <pho@pkgsrc.org>
2022-02-11 20:14:14 +0000

    Update to mmorph-1.2.0
    1.2.0
        BREAKING CHANGE: Remove instances for ErrorT and ListT
            These types are deprecated

--- devel/hs-mmorph/buildlink3.mk
+++ devel/hs-mmorph/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/01/18 02:48:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/02/11 20:14:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.5
-BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.5nb2
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.2.0
 BUILDLINK_PKGSRCDIR.hs-mmorph?=		../../devel/hs-mmorph
 
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
