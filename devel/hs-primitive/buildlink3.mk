05c80c7d8f27f953738889da64f27ab6d47dfd1c
pho <pho@pkgsrc.org>
2022-02-11 12:58:29 +0000

    Update to primitive-0.7.3.0
    Changes in version 0.7.3.0
    * Correct implementations of *> for Array and SmallArray.
    * Drop support for GHC < 7.10
    * Export runByteArray and runPrimArray.
    * Export createArray and createSmallArray.
    * Export emptyByteArray, emptyPrimArray, emptyArray and emptySmallArray.
    
    Changes in version 0.7.2.0
    * Add thawByteArray and thawPrimArray.
    * Changed the Show instance of ByteArray, so that all 8-bit words are
      rendered as two digits. For example, display 0x0D instead of 0xD.

--- devel/hs-primitive/buildlink3.mk
+++ devel/hs-primitive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/01/18 02:48:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/02/11 12:58:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.7.1.0nb2
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.7.3
+BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.7.3.0
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../devel/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
