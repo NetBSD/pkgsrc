6db12add4e18038db29a487eb94a399de775a3b0
pho <pho@pkgsrc.org>
2022-02-11 13:42:30 +0000

    Update to uuid-types-1.0.5
    1.0.5 (2021-05-03)
        Add (Template Haskell) Lift UUID instance

--- devel/hs-uuid-types/buildlink3.mk
+++ devel/hs-uuid-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/01/18 02:48:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/02/11 13:42:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-uuid-types
 
 .if !defined(HS_UUID_TYPES_BUILDLINK3_MK)
 HS_UUID_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.4
-BUILDLINK_ABI_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.4nb2
+BUILDLINK_API_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.5
+BUILDLINK_ABI_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.5
 BUILDLINK_PKGSRCDIR.hs-uuid-types?=	../../devel/hs-uuid-types
 
 .include "../../devel/hs-hashable/buildlink3.mk"
