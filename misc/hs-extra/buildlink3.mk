2905ee357b793e52765ceebd79d0cfcf648b4002
pho <pho@pkgsrc.org>
2022-02-11 09:42:17 +0000

    Update to extra-1.7.10
    1.7.10, released 2021-08-29
        #81, add assertIO function
        #85, add !? to do !! list indexing returning a Maybe
        #80, add strict Var functions modifyVar', modifyVar_', writeVar'

--- misc/hs-extra/buildlink3.mk
+++ misc/hs-extra/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 09:42:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-extra
 
 .if !defined(HS_EXTRA_BUILDLINK3_MK)
 HS_EXTRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.7.9
-BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.7.9nb2
+BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.7.10
+BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.7.10
 BUILDLINK_PKGSRCDIR.hs-extra?=		../../misc/hs-extra
 
 .include "../../time/hs-clock/buildlink3.mk"
