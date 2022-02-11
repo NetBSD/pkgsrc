52daa0cb5b8d8f2ad729c8aefd6550e4d7c0a363
pho <pho@pkgsrc.org>
2022-02-11 13:43:56 +0000

    Update to unordered-containers-0.2.16.0
    0.2.16.0
    * Increase maximum branching factor from 16 to 32
    * Tweak union.goDifferentHash
    * Fix debug mode bounds check in cloneM
    * Remove some old internal compatibility code
    
    0.2.15.0
    * Add security advisory regarding hash collision attacks
    * Add support for hashable 1.4
    * Drop support for GHC < 8
    
    0.2.14.0
    * Add HashMap.mapKeys. Thanks, Marco Perone!
    * Add instances for NFData1 and NFData2. Thanks, Isaac Elliott and Oleg
      Grenrus!
    * Fix @since-annotation for compose. Thanks, @Mathnerd314!

--- devel/hs-unordered-containers/buildlink3.mk
+++ devel/hs-unordered-containers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2022/01/18 02:48:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2022/02/11 13:43:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.13
-BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.13.0nb2
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.16
+BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.16.0
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../devel/hs-unordered-containers
 
 .include "../../devel/hs-hashable/buildlink3.mk"
