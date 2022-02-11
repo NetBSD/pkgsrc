781eb63ae264543a10fc53f7ad4d1a72203d5c05
pho <pho@pkgsrc.org>
2022-02-11 18:02:34 +0000

    Update to vector-th-unbox-0.2.2
    0.2.2
        Fixed the build failure on GHC 9.2
        Dropped the support for GHC older than 8.0

--- devel/hs-vector-th-unbox/buildlink3.mk
+++ devel/hs-vector-th-unbox/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 18:02:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-th-unbox
 
 .if !defined(HS_VECTOR_TH_UNBOX_BUILDLINK3_MK)
 HS_VECTOR_TH_UNBOX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector-th-unbox+=	hs-vector-th-unbox>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-vector-th-unbox+=	hs-vector-th-unbox>=0.2.1.9nb2
+BUILDLINK_API_DEPENDS.hs-vector-th-unbox+=	hs-vector-th-unbox>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-vector-th-unbox+=	hs-vector-th-unbox>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-vector-th-unbox?=	../../devel/hs-vector-th-unbox
 
 .include "../../devel/hs-vector/buildlink3.mk"
