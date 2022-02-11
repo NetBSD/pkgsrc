4af5c48c3163f34923e28ec63d1ed04654ba4399
pho <pho@pkgsrc.org>
2022-02-11 12:03:30 +0000

    lang/hs-hslua-core: import hs-hslua-core-2.1.0
    Wrappers and helpers to bridge Haskell and Lua.
    
    It builds upon the lua package, which allows to bundle a Lua interpreter
    with a Haskell program.

---
+++ lang/hs-hslua-core/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/11 12:03:30 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-core
+
+.if !defined(HS_HSLUA_CORE_BUILDLINK3_MK)
+HS_HSLUA_CORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-hslua-core?=	../../lang/hs-hslua-core
+
+.include "../../lang/hs-lua/buildlink3.mk"
+.endif	# HS_HSLUA_CORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-core
