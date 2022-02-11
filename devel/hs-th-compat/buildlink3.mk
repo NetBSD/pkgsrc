47c66c6b9c3f56137db697e468909ad7e5a7a569
pho <pho@pkgsrc.org>
2022-02-11 09:17:06 +0000

    Update to th-compat-0.1.3
    0.1.3 [2021.08.29]
    
    * Implement qGetDoc and qPutDoc in the Quasi instance for QuoteToQuasi.
    * Add expToSplice.

--- devel/hs-th-compat/buildlink3.mk
+++ devel/hs-th-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/11 09:17:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-compat
 
 .if !defined(HS_TH_COMPAT_BUILDLINK3_MK)
 HS_TH_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.2nb2
+BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-th-compat?=	../../devel/hs-th-compat
 .endif	# HS_TH_COMPAT_BUILDLINK3_MK
 
