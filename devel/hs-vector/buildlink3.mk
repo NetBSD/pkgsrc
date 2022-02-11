28cb4b0c9f0664a0c9d54bcc56d62020104fbe35
pho <pho@pkgsrc.org>
2022-02-11 13:40:50 +0000

    Update to vector-0.12.3.1
    Changes in version 0.12.3.1
    * Bugfix for ghcjs and Double memset for Storable vector: #410
    * Avoid haddock bug: #383
    * Improve haddock and doctests
    * Disable problematic tests with -boundschecks #407
    
    Changes in version 0.12.3.0
    * Fix performance regression due to introduction of keepAlive# primop in
      ghc-9.0: #372
    * Add monadic functions for mutable vectors: #338
      * Added folds for monadic functions: mapM_, imapM_, forM_, iforM_, foldl,
        foldl', foldM, foldM', ifoldl, ifoldl', ifoldM, ifoldM'
      * Added modifyM and unsafeModifyM for mutable vectors
      * Added generate and generateM for mutable vectors
    
    Changes in version 0.12.2.0
    * Add MINIMAL pragma to Vector & MVector type classes: #11
    * Export unstreamM fromfrom Data.Vector.Generic: #70
    * New functions: unfoldrExactN and unfoldrExactNM: #140
    * Added iforM and iforM_: #262
    * Added MonadFix instance for boxed vectors: #178
    * Added uncons and unsnoc: #212
    * Added foldMap and foldMap': #263
    * Added isSameVector for storable vectors
    * Added toArray, fromArray, toMutableArray and fromMutableArray
    * Added iscanl, iscanl', iscanr, iscanr' to Primitive, Storable and Unboxed
    * Added izipWithM, izipWithM_, imapM and imapM_ to Primitive and Storable
    * Added ifoldM, ifoldM', ifoldM_ and ifoldM'_ to Primitive and Storable
    * Added eqBy and cmpBy
    * Added findIndexR to Generic: #172
    * Added catMaybes: #329
    * Added mapMaybeM and imapMaybeM: #183
    
    Changes in version 0.12.1.2
    * Fix for lost function Data.Vector.Generic.mkType: #287
    
    Changes in version 0.12.1.1 (deprecated)
    * add semigrioups dep to test suite so CI actually runs again on GHC < 8
    
    Changes in version 0.12.1.0 (deprecated)
    * Fix integer overflows in specializations of Bundle/Stream enumFromTo on
      Integral types
    * Fix possibility of OutOfMemory with take and very large arguments.
    * Fix slice function causing segfault and not checking the bounds properly.
    * updated specialization rule for EnumFromTo on Float and Double to make
      sure it always matches the version in GHC Base (which changed as of 8.6)
      Thanks to Aleksey Khudyakov @Shimuuar for this fix.
    * fast rejection short circuiting in eqBy operations
    * the O2 test suite now has reasonable memory usage on every GHC version,
      special thanks to Alexey Kuleshevich (@lehins).
    * The Mutable type family is now injective on GHC 8.0 or later.
    * Using empty Storable vectors no longer results in division-by-zero
      errors.
    * The Data instances for Vector types now have well defined implementations
      for toConstr, gunfold, and dataTypeOf.
    * New function: partitionWith.
    * Add Unbox instances for Identity, Const, Down, Dual, Sum, Product, Min,
      Max, First, Last, WrappedMonoid, Arg, Any, All, Alt, and Compose.
    * Add NFData1 instances for applicable Vector types.

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2022/01/18 02:48:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2022/02/11 13:40:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.12.0
-BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.12.0.3nb2
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.12.3
+BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.12.3.1
 BUILDLINK_PKGSRCDIR.hs-vector?=		../../devel/hs-vector
 
 .include "../../devel/hs-primitive/buildlink3.mk"
