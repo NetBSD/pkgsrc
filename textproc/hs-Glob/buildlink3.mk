a1516b2262e1a6c8943aebfc6b8b20f0229a2f22
pho <pho@pkgsrc.org>
2022-02-11 13:34:01 +0000

    Update to Glob-0.10.2
    0.10.2, 2021-11-10:
    * Relaxed transformers-compat version bound.
    * Supported Win32 2.12.0.0 or higher.

--- textproc/hs-Glob/buildlink3.mk
+++ textproc/hs-Glob/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 13:34:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-Glob
 
 .if !defined(HS_GLOB_BUILDLINK3_MK)
 HS_GLOB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Glob+=	hs-Glob>=0.10.1
-BUILDLINK_ABI_DEPENDS.hs-Glob+=	hs-Glob>=0.10.1nb2
+BUILDLINK_API_DEPENDS.hs-Glob+=	hs-Glob>=0.10.2
+BUILDLINK_ABI_DEPENDS.hs-Glob+=	hs-Glob>=0.10.2
 BUILDLINK_PKGSRCDIR.hs-Glob?=	../../textproc/hs-Glob
 
 .include "../../devel/hs-dlist/buildlink3.mk"
