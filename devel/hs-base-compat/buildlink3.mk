8c12f441179c69eb98c6825caf6718ea53613f4e
pho <pho@pkgsrc.org>
2022-02-11 12:55:14 +0000

    Update to base-compat-0.12.1
    Changes in 0.12.1 [2021.10.30]
    * Backport Solo to Data.Tuple.Compat when building with ghc-prim-0.7.0 or
      later
    
    Changes in 0.12.0 [2021.08.29]
    * Sync with base-4.16/GHC 9.2
    * Data.Semigroup.Compat{.Repl} no longer re-exports the Option data type or
      the option function, as both have been removed in base-4.16.
    * Backport readBin and showBin to Numeric.Compat
    * Backport readBinP to Text.Read.Lex.Compat

--- devel/hs-base-compat/buildlink3.mk
+++ devel/hs-base-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/11 12:55:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat
 
 .if !defined(HS_BASE_COMPAT_BUILDLINK3_MK)
 HS_BASE_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.11.2
-BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.11.2nb2
+BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.1
+BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.1
 BUILDLINK_PKGSRCDIR.hs-base-compat?=	../../devel/hs-base-compat
 .endif	# HS_BASE_COMPAT_BUILDLINK3_MK
 
