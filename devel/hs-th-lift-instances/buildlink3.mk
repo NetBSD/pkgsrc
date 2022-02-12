d3aac7d30ab1bd40ad0f757f12a1b3d607052c56
pho <pho@pkgsrc.org>
2022-02-12 08:02:30 +0000

    devel/hs-th-lift-instances: import hs-th-lift-instances-0.1.19
    Most data types in the haskell platform do not have Lift instances. This
    package provides orphan instances for containers, text, bytestring and
    vector. It also provides compat instances for older versions of
    template-haskell.

---
+++ devel/hs-th-lift-instances/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 08:02:30 pho Exp $
+
+BUILDLINK_TREE+=	hs-th-lift-instances
+
+.if !defined(HS_TH_LIFT_INSTANCES_BUILDLINK3_MK)
+HS_TH_LIFT_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-th-lift-instances+=	hs-th-lift-instances>=0.1.19
+BUILDLINK_ABI_DEPENDS.hs-th-lift-instances+=	hs-th-lift-instances>=0.1.19
+BUILDLINK_PKGSRCDIR.hs-th-lift-instances?=	../../devel/hs-th-lift-instances
+
+.include "../../devel/hs-th-lift/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_TH_LIFT_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-th-lift-instances
