75a1eb6f6f57ddcb2976a068ce58b63bd360c1bf
pho <pho@pkgsrc.org>
2022-02-12 06:59:05 +0000

    Update to x509-system-1.6.7
    No changelogs are provided by the upstream.

--- security/hs-x509-system/buildlink3.mk
+++ security/hs-x509-system/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/12 06:59:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-x509-system
 
 .if !defined(HS_X509_SYSTEM_BUILDLINK3_MK)
 HS_X509_SYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-x509-system+=	hs-x509-system>=1.6.6
-BUILDLINK_ABI_DEPENDS.hs-x509-system+=	hs-x509-system>=1.6.6nb2
+BUILDLINK_API_DEPENDS.hs-x509-system+=	hs-x509-system>=1.6.7
+BUILDLINK_ABI_DEPENDS.hs-x509-system+=	hs-x509-system>=1.6.7
 BUILDLINK_PKGSRCDIR.hs-x509-system?=	../../security/hs-x509-system
 
 .include "../../security/hs-pem/buildlink3.mk"
