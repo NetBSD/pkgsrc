8f478f2aff5a32d1670613e3cd18d85c18df2973
pho <pho@pkgsrc.org>
2022-02-12 05:12:54 +0000

    Update to x509-store-1.6.9
    No changelogs are provided by the upstream.

--- security/hs-x509-store/buildlink3.mk
+++ security/hs-x509-store/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/12 05:12:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-x509-store
 
 .if !defined(HS_X509_STORE_BUILDLINK3_MK)
 HS_X509_STORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-x509-store+=	hs-x509-store>=1.6.7
-BUILDLINK_ABI_DEPENDS.hs-x509-store+=	hs-x509-store>=1.6.7nb2
+BUILDLINK_API_DEPENDS.hs-x509-store+=	hs-x509-store>=1.6.9
+BUILDLINK_ABI_DEPENDS.hs-x509-store+=	hs-x509-store>=1.6.9
 BUILDLINK_PKGSRCDIR.hs-x509-store?=	../../security/hs-x509-store
 
 .include "../../devel/hs-asn1-encoding/buildlink3.mk"
