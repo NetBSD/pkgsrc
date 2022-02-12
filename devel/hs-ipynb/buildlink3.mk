f3388559f26a6ef897ded31db775e461cb0e16fa
pho <pho@pkgsrc.org>
2022-02-12 07:39:06 +0000

    Update to ipynb-
    0.2 -- 2022-01-11
    * Make MimeAttachments a newtype, and export it [API change]. Derive
      ToJSON, ToEncoding using generics for deterministic field order.
    * Add Ord instance for MimeBundle and MimeData. [API change]
    * Derive ToJSON for MimeBundle, with a deterministic order of keys.
    * Add ToJSON instance for MimeData [API change].
    * Make JSONMeta a newtype and export it [API change]. Derive ToJSON with a
      deterministic order of keys.
    * Add cellId field to Cell (#2). [API change] Needed for Nb4.5+.
    
    0.1.0.2 -- 2020-10-23
    * Changes for aeson 2 compatibility.
    * Simplified round trip test to avoid use of aeson-diff, which doesn't work
      with aeson 2.

--- devel/hs-ipynb/buildlink3.mk
+++ devel/hs-ipynb/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/12 07:39:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-ipynb
 
 .if !defined(HS_IPYNB_BUILDLINK3_MK)
 HS_IPYNB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ipynb+=	hs-ipynb>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-ipynb+=	hs-ipynb>=0.1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-ipynb+=	hs-ipynb>=0.2
+BUILDLINK_ABI_DEPENDS.hs-ipynb+=	hs-ipynb>=0.2
 BUILDLINK_PKGSRCDIR.hs-ipynb?=		../../devel/hs-ipynb
 
 .include "../../converters/hs-aeson/buildlink3.mk"
