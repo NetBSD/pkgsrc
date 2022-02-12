8f59c3a6af68f8bd14abcfad626e8ea01ab6410a
pho <pho@pkgsrc.org>
2022-02-12 08:14:11 +0000

    devel/hs-pandoc-lua-marshal: import hs-pandoc-lua-marshal-0.1.4
    This package provides functions to marshal and unmarshal pandoc document
    types to and from Lua.
    
    The values of most types are pushed to pandoc as "userdata" objects that
    wrap a stable pointer to the Haskell value; these objects come with methods
    to access and modify their properties.
    
    Sequences are pushed as normal Lua tables, but are augmented with
    convenience functions.

---
+++ devel/hs-pandoc-lua-marshal/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 08:14:11 pho Exp $
+
+BUILDLINK_TREE+=	hs-pandoc-lua-marshal
+
+.if !defined(HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK)
+HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.1.4
+BUILDLINK_PKGSRCDIR.hs-pandoc-lua-marshal?=	../../devel/hs-pandoc-lua-marshal
+
+.include "../../lang/hs-hslua/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-lua/buildlink3.mk"
+.include "../../textproc/hs-pandoc-types/buildlink3.mk"
+.include "../../devel/hs-safe/buildlink3.mk"
+.endif	# HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pandoc-lua-marshal
