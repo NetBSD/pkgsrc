5f1e27e9e98f6b7a9a7e699083f5ab42805f9a65
pho <pho@pkgsrc.org>
2022-02-12 08:03:51 +0000

    textproc/hs-unicode-collation: import hs-unicode-collation-0.1.3.1
    This library provides a pure Haskell implementation of the Unicode
    Collation Algorithm described at http://www.unicode.org/reports/tr10/. It
    is not as fully-featured or as performant as text-icu, but it avoids a
    dependency on a large C library. Locale-specific tailorings are also
    provided.

---
+++ textproc/hs-unicode-collation/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 08:03:51 pho Exp $
+
+BUILDLINK_TREE+=	hs-unicode-collation
+
+.if !defined(HS_UNICODE_COLLATION_BUILDLINK3_MK)
+HS_UNICODE_COLLATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-unicode-collation+=	hs-unicode-collation>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-unicode-collation+=	hs-unicode-collation>=0.1.3.1
+BUILDLINK_PKGSRCDIR.hs-unicode-collation?=	../../textproc/hs-unicode-collation
+
+.include "../../devel/hs-th-lift-instances/buildlink3.mk"
+.endif	# HS_UNICODE_COLLATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-unicode-collation
