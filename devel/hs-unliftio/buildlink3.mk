1ecd8c843d0e8e45f47c630466c20eafee161f09
pho <pho@pkgsrc.org>
2022-02-12 07:02:21 +0000

    devel/hs-unliftio: import hs-unliftio-0.2.21.0
    Provides the core MonadUnliftIO typeclass, a number of common instances,
    and a collection of common functions working with it.

---
+++ devel/hs-unliftio/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 07:02:21 pho Exp $
+
+BUILDLINK_TREE+=	hs-unliftio
+
+.if !defined(HS_UNLIFTIO_BUILDLINK3_MK)
+HS_UNLIFTIO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-unliftio+=	hs-unliftio>=0.2.21
+BUILDLINK_ABI_DEPENDS.hs-unliftio+=	hs-unliftio>=0.2.21.0
+BUILDLINK_PKGSRCDIR.hs-unliftio?=	../../devel/hs-unliftio
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.endif	# HS_UNLIFTIO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-unliftio
