d39b13123c8c648cb3d7cea04a5df766faba0325
pho <pho@pkgsrc.org>
2022-02-12 08:11:28 +0000

    devel/hs-hslua-module-version: import hs-hslua-module-version-1.0.1
    Wrapper for the Data.Version.Version Haskell type.

---
+++ devel/hs-hslua-module-version/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 08:11:28 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-module-version
+
+.if !defined(HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK)
+HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.1
+BUILDLINK_PKGSRCDIR.hs-hslua-module-version?=	../../devel/hs-hslua-module-version
+
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-packaging/buildlink3.mk"
+.endif	# HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-module-version
