cbaa1d71d8715266d5b868a5d3d02d8263bad34a
pho <pho@pkgsrc.org>
2022-02-12 04:41:58 +0000

    Update to transformers-base-0.4.6
    No changelogs are provided by the upstream.

--- devel/hs-transformers-base/buildlink3.mk
+++ devel/hs-transformers-base/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/01/18 02:48:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2022/02/12 04:41:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers-base
 
 .if !defined(HS_TRANSFORMERS_BASE_BUILDLINK3_MK)
 HS_TRANSFORMERS_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.5
-BUILDLINK_ABI_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.5.2nb2
+BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.6
+BUILDLINK_ABI_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.6
 BUILDLINK_PKGSRCDIR.hs-transformers-base?=	../../devel/hs-transformers-base
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
