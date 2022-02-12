c3b3397c4b120b0eda95402952d7a7d2c6748476
pho <pho@pkgsrc.org>
2022-02-12 05:07:04 +0000

    Update to X11-xft-0.3.4
    0.3.4 (2021-12-11)
    * Dropped support for GHC 7.10.
    * Added xftDrawStringFallback, which works like xftDrawString but supports
      font fallback.
    * Added xftTextAccumExtents, which works like xftTextExtents but possibly
      uses different fonts for different parts of the string and returns the
      accumulative extents.
    * Added the functions xftfont_max_ascent, xftfont_max_descent, and
      xftfont_max_height to gain information about a non-empty list of
      XftFonts.
    
    0.3.3 (2021-12-01)
    * Fixed flipped green/blue values in XRenderColor.

--- x11/hs-X11-xft/buildlink3.mk
+++ x11/hs-X11-xft/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/01/18 02:48:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2022/02/12 05:07:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-X11-xft
 
 .if !defined(HS_X11_XFT_BUILDLINK3_MK)
 HS_X11_XFT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-X11-xft+=	hs-X11-xft>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-X11-xft+=	hs-X11-xft>=0.3.1nb7
+BUILDLINK_API_DEPENDS.hs-X11-xft+=	hs-X11-xft>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-X11-xft+=	hs-X11-xft>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-X11-xft?=	../../x11/hs-X11-xft
 
 .include "../../devel/hs-utf8-string/buildlink3.mk"
