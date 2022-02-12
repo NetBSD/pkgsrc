21585eb6082963ba0e49ee7bbd6e5086ceb11af7
pho <pho@pkgsrc.org>
2022-02-12 07:11:03 +0000

    Update to wai-logger-2.4.0
    No changelogs are provided by the upstream.

--- www/hs-wai-logger/buildlink3.mk
+++ www/hs-wai-logger/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/01/18 02:48:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/02/12 07:11:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-wai-logger
 
 .if !defined(HS_WAI_LOGGER_BUILDLINK3_MK)
 HS_WAI_LOGGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-wai-logger+=	hs-wai-logger>=2.3.6
-BUILDLINK_ABI_DEPENDS.hs-wai-logger+=	hs-wai-logger>=2.3.6nb1
+BUILDLINK_API_DEPENDS.hs-wai-logger+=	hs-wai-logger>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-wai-logger+=	hs-wai-logger>=2.4.0
 BUILDLINK_PKGSRCDIR.hs-wai-logger?=	../../www/hs-wai-logger
 
 .include "../../sysutils/hs-byteorder/buildlink3.mk"
