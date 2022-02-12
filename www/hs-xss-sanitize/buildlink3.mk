39ef482cac3866a0772d175bb743144d2ceaa71d
pho <pho@pkgsrc.org>
2022-02-12 05:12:03 +0000

    Update to xss-sanitize-0.3.7
    0.3.7
    * clear the contents of style and script tags instead of escaping them

--- www/hs-xss-sanitize/buildlink3.mk
+++ www/hs-xss-sanitize/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/12 05:12:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-xss-sanitize
 
 .if !defined(HS_XSS_SANITIZE_BUILDLINK3_MK)
 HS_XSS_SANITIZE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.6
-BUILDLINK_ABI_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.6nb2
+BUILDLINK_API_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.7
+BUILDLINK_ABI_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.7
 BUILDLINK_PKGSRCDIR.hs-xss-sanitize?=	../../www/hs-xss-sanitize
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
