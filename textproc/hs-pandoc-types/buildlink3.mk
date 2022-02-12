80993b9aa2e27b21602c8d8693211d85577d52c4
pho <pho@pkgsrc.org>
2022-02-12 07:32:58 +0000

    Update to pandoc-types-1.22.1
    [1.22.1]
    
      * Text.Pandoc.Builder: add simpleFigure, simpleFigureWith,
        and the SimpleFigure bidirectional pattern synonym
        (Aner Lucero) [API change].
    
      * Allow bytestring 0.11 (Alexander Batischev).
    
      * Update stack resolver to lts-18.10
    
      * Allow aeson 2+.  Tested with aeson 2.0.1.0.
    
      * Allow transformers 0.6.
    
      * Fix incorrect table ColWidth documentation (#85, Nils Carlson).
        The documentation stated that the ColWidth represented the width
        of the column as a fraction of the table width when in represents
        a percentage of the text width.

--- textproc/hs-pandoc-types/buildlink3.mk
+++ textproc/hs-pandoc-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.22 2022/01/18 02:48:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2022/02/12 07:32:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-types
 
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.22
-BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.22nb2
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.22.1
+BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.22.1
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../textproc/hs-pandoc-types
 
 .include "../../converters/hs-aeson/buildlink3.mk"
