9406ef41312a60bf6a7590d7e8f0c0c4e90d53f6
pho <pho@pkgsrc.org>
2022-02-12 05:28:34 +0000

    devel/hs-witherable: import hs-witherable-0.4.2
    A stronger variant of traverse which can remove elements and generalised
    mapMaybe, catMaybes, filter.

---
+++ devel/hs-witherable/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 05:28:34 pho Exp $
+
+BUILDLINK_TREE+=	hs-witherable
+
+.if !defined(HS_WITHERABLE_BUILDLINK3_MK)
+HS_WITHERABLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-witherable+=	hs-witherable>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-witherable+=	hs-witherable>=0.4.2
+BUILDLINK_PKGSRCDIR.hs-witherable?=	../../devel/hs-witherable
+
+.include "../../devel/hs-base-orphans/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable-instances/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_WITHERABLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-witherable
