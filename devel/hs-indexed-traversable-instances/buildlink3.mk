6d8724617f5cd077d8f40ca28cc0672bb4ed2851
pho <pho@pkgsrc.org>
2022-02-12 05:21:58 +0000

    devel/hs-indexed-traversable-instances: import hs-indexed-traversable-instances-0.1.1
    This package provides extra instances for type-classes in the
    indexed-traversable package.
    
    The intention is to keep this package minimal; it provides instances that
    formely existed in lens or optics-extra. We recommend putting other
    instances directly into their defining packages. The indexed-traversable
    package is light, having only GHC boot libraries as its dependencies.

---
+++ devel/hs-indexed-traversable-instances/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 05:21:58 pho Exp $
+
+BUILDLINK_TREE+=	hs-indexed-traversable-instances
+
+.if !defined(HS_INDEXED_TRAVERSABLE_INSTANCES_BUILDLINK3_MK)
+HS_INDEXED_TRAVERSABLE_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-indexed-traversable-instances+=	hs-indexed-traversable-instances>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-indexed-traversable-instances+=	hs-indexed-traversable-instances>=0.1.1
+BUILDLINK_PKGSRCDIR.hs-indexed-traversable-instances?=		../../devel/hs-indexed-traversable-instances
+
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../devel/hs-OneTuple/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_INDEXED_TRAVERSABLE_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-indexed-traversable-instances
