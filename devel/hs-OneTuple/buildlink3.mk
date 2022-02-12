b6fd29ad2c21b8677e462b3ac0363a410505f907
pho <pho@pkgsrc.org>
2022-02-12 05:20:39 +0000

    devel/hs-OneTuple: import hs-OneTuple-0.3.1
    This package is a compatibility package for a singleton data type
    "Solo". It is available in base-4.16 (GHC-9.2).

---
+++ devel/hs-OneTuple/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 05:20:39 pho Exp $
+
+BUILDLINK_TREE+=	hs-OneTuple
+
+.if !defined(HS_ONETUPLE_BUILDLINK3_MK)
+HS_ONETUPLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-OneTuple?=	../../devel/hs-OneTuple
+.endif	# HS_ONETUPLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-OneTuple
