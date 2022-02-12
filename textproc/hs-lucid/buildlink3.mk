208134ffeb2b8b42bcac129f60f9df766fc8bc92
pho <pho@pkgsrc.org>
2022-02-12 05:17:21 +0000

    Update to lucid-2.11.0
    2.11.0
    * Change internal attributes to Seq Attribute. This preserves
      ordering. Attributes are merged in a left-biased way, preserving the key
      order as first encountered.
    
    2.9.13
    * Change internal attributes representation from HashMap to Map. This
      introduces stable ordering, at a negligible performance cost for
      realistic element sizes. This may affect some test suites.
    * doctype no longer accepts attributes. You can use with with doctypeHtml
      now, if needed.

--- textproc/hs-lucid/buildlink3.mk
+++ textproc/hs-lucid/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/12 05:17:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-lucid
 
 .if !defined(HS_LUCID_BUILDLINK3_MK)
 HS_LUCID_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lucid+=	hs-lucid>=2.9.12
-BUILDLINK_ABI_DEPENDS.hs-lucid+=	hs-lucid>=2.9.12.1nb2
+BUILDLINK_API_DEPENDS.hs-lucid+=	hs-lucid>=2.11.0
+BUILDLINK_ABI_DEPENDS.hs-lucid+=	hs-lucid>=2.11.0
 BUILDLINK_PKGSRCDIR.hs-lucid?=		../../textproc/hs-lucid
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
