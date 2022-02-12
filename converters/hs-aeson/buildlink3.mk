c3432c17cde2bd1644343e8ab72eaae2087d8f43
pho <pho@pkgsrc.org>
2022-02-12 05:31:25 +0000

    Update to aeson-2.0.3.0
    2.0.3.0
    * text-2.0 support
    * bytestring-0.11.2.0 support
    * Rewrite pure text literal unescaper.
    * Add QuickCheck's Arbitrary, CoArbitrary and Function instances
    
    2.0.2.0
    * Add IsList (KeyMap v) instance.
    * Add toMapText and fromMapText to Data.Aeson.KeyMap.
    * Add ShortText instances
    * Add Solo instances
    
    2.0.1.0
    * Add FromJSON KeyMap instance.
    * Make ordered-keymap on by default.
    
    2.0.0.0
    * Remove forced -O2 and then unneeded fast flag. Also remove most of INLINE
      pragmas. In the effect, aeson compiles almost twice as fast.
    * To get fast compilation effect cabal-install users may specify
      optimization: False.
    * Make map type used by Object abstract so the underlying implementation
      can be modified, thanks to Callan McGill
    * Add ordered-keymap flag allowing to change the underlying implementation
      of object KeyMap.
    * Drop GHC-7 support
    * Remove Data.Aeson.Encode module
    * Double and Float infinities are encoded as "+inf" and "-inf". Change
      To/FromJSONKey instances to use "+inf" and "-inf" too.
    * FromJSON () and FromJSON (Proxy tag) accept any JSON value.

--- converters/hs-aeson/buildlink3.mk
+++ converters/hs-aeson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.23 2022/01/18 02:48:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2022/02/12 05:31:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-aeson
 
 .if !defined(HS_AESON_BUILDLINK3_MK)
 HS_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=1.5.6
-BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=1.5.6.0nb2
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=2.0.3
+BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=2.0.3.0
 BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
@@ -14,16 +14,22 @@ BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 .include "../../devel/hs-data-fix/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../devel/hs-OneTuple/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-QuickCheck/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-semialign/buildlink3.mk"
 .include "../../devel/hs-strict/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../textproc/hs-text-short/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
 .include "../../devel/hs-these/buildlink3.mk"
 .include "../../time/hs-time-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-uuid-types/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
+.include "../../devel/hs-witherable/buildlink3.mk"
 .endif	# HS_AESON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-aeson
