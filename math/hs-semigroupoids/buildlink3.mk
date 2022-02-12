3fdf081c59261d0302b50ab768a67c8c0309fb46
pho <pho@pkgsrc.org>
2022-02-12 05:26:25 +0000

    Update to semigroupoids-5.3.7
    5.3.7 [2022.01.09]
    * Relax the Bind constraints in the following instances to Functor:
    
        -instance (Bind f,    Monad f) => Alt  (MaybeT f)
        -instance (Bind f,    Monad f) => Plus (MaybeT f)
        +instance (Functor f, Monad f) => Alt  (MaybeT f)
        +instance (Functor f, Monad f) => Plus (MaybeT f)
    
        -instance (Bind f,    Monad f, Semigroup e)           => Alt  (ExceptT e f)
        -instance (Bind f,    Monad f, Semigroup e, Monoid e) => Plus (ExceptT e f)
        +instance (Functor f, Monad f, Semigroup e)           => Alt  (ExceptT e f)
        +instance (Functor f, Monad f, Semigroup e, Monoid e) => Plus (ExceptT e f)
    
         -- If building with transformers-0.5.* or older
        -instance (Bind f,    Monad f)          => Alt  (ErrorT e f)
        -instance (Bind f,    Monad f, Error e) => Plus (ErrorT e f
        +instance (Functor f, Monad f)          => Alt  (ErrorT e f)
        +instance (Functor f, Monad f, Error e) => Plus (ErrorT e f)
    
    5.3.6 [2021.10.07]
    * Allow building with GHC 9.2.
    * Allow building with transformers-0.6.*.
    * Add Alt instance for Identity.
    * Add Conclude, Decide and Divise type classes and instances.
    * Add (<.*>), (<*.>), and traverseMaybe functions, which make it easier to
      defined Traversable1 instances for data types that have fields with a
      combination of Traversable and Traversable1 instances.
    * Add Semigroupoids.Do module with overloads for use with QualifiedDo.
    * Add Apply, Alt, Plus, Bind and BindTrans instances for the CPS versions
      of WriterT and RWST.
    * Add psum function to Data.Functor.Plus.
    * Add Categorical data type.

--- math/hs-semigroupoids/buildlink3.mk
+++ math/hs-semigroupoids/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/12 05:26:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.5
-BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.5nb2
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.7
+BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.7
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=		../../math/hs-semigroupoids
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
