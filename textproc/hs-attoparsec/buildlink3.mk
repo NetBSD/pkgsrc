72896df699f0ff554f9871296f72287da6e04892
pho <pho@pkgsrc.org>
2022-02-12 05:04:53 +0000

    Update to attoparsec-0.14.4
    0.14.4
    * Fix a segmentation fault when built against text-2.0
    * Restructure project to allow more convenient usage of benchmark suite
    * Allow benchmarks to build with GHC 9.2
    
    0.14.3
    * Support for GHC 9.2.1
    
    0.14.2
    * Support for GHC 9.2.1

--- textproc/hs-attoparsec/buildlink3.mk
+++ textproc/hs-attoparsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2022/01/18 02:48:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2022/02/12 05:04:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec
 
 .if !defined(HS_ATTOPARSEC_BUILDLINK3_MK)
 HS_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.14.1
-BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.14.1nb2
+BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.14.4
+BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.14.4
 BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../textproc/hs-attoparsec
 
 .include "../../math/hs-scientific/buildlink3.mk"
