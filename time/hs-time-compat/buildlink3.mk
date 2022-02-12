3078bf3a69bef50cb4b9d7a62bee2267377acbcb
pho <pho@pkgsrc.org>
2022-02-12 05:03:42 +0000

    Update to time-compat-1.9.6.1
    1.9.6.1
    * Support time-1.12.
    
    1.9.6
    * Move Hashable instance here from hashable-time package. Note: ZonedTime
      instance is dropped, as ZonedTime doesn't have Eq instance.
    * Drop GHC-7.0 and GHC-7.2 support.

--- time/hs-time-compat/buildlink3.mk
+++ time/hs-time-compat/buildlink3.mk
@@ -1,17 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/12 05:03:42 pho Exp $
 
 BUILDLINK_TREE+=	hs-time-compat
 
 .if !defined(HS_TIME_COMPAT_BUILDLINK3_MK)
 HS_TIME_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-time-compat+=	hs-time-compat>=1.9.5
-BUILDLINK_ABI_DEPENDS.hs-time-compat?=	hs-time-compat>=1.9.5nb2
+BUILDLINK_API_DEPENDS.hs-time-compat+=	hs-time-compat>=1.9.6
+BUILDLINK_ABI_DEPENDS.hs-time-compat?=	hs-time-compat>=1.9.6.1
 BUILDLINK_PKGSRCDIR.hs-time-compat?=	../../time/hs-time-compat
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
-.include "../../devel/hs-fail/buildlink3.mk"
-.include "../../devel/hs-old-locale/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_TIME_COMPAT_BUILDLINK3_MK
 
