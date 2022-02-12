037008ebe6f7391a0f6ba1f995459aa40b14f3c6
pho <pho@pkgsrc.org>
2022-02-12 06:56:38 +0000

    Update to x509-validation-1.16.12
    No changelogs are provided by the upstream.

--- security/hs-x509-validation/buildlink3.mk
+++ security/hs-x509-validation/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/12 06:56:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-x509-validation
 
 .if !defined(HS_X509_VALIDATION_BUILDLINK3_MK)
 HS_X509_VALIDATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-x509-validation+=	hs-x509-validation>=1.6.11
-BUILDLINK_ABI_DEPENDS.hs-x509-validation+=	hs-x509-validation>=1.6.11nb2
+BUILDLINK_API_DEPENDS.hs-x509-validation+=	hs-x509-validation>=1.6.12
+BUILDLINK_ABI_DEPENDS.hs-x509-validation+=	hs-x509-validation>=1.6.12
 BUILDLINK_PKGSRCDIR.hs-x509-validation?=	../../security/hs-x509-validation
 
 .include "../../devel/hs-asn1-encoding/buildlink3.mk"
