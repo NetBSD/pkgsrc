19d7eb67d1ecdf8040ff9914bc5a8fc165b51f5d
pho <pho@pkgsrc.org>
2022-02-12 07:42:52 +0000

    Update to warp-tls-3.3.2
    3.3.2
    * Providing the Internal module. #841
    
    3.3.1
    * Move exception handling over to unliftio for better async exception
      support #845
    * Cleanly close connection when client closes connection prematurely #844

--- www/hs-warp-tls/buildlink3.mk
+++ www/hs-warp-tls/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/01/18 02:48:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/02/12 07:42:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-warp-tls
 
 .if !defined(HS_WARP_TLS_BUILDLINK3_MK)
 HS_WARP_TLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.0
-BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.0nb1
+BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.2
+BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.2
 BUILDLINK_PKGSRCDIR.hs-warp-tls?=	../../www/hs-warp-tls
 
 .include "../../security/hs-cryptonite/buildlink3.mk"
