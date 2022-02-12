3ad6be0395af2efdf6ca23851357845415936a03
pho <pho@pkgsrc.org>
2022-02-12 04:45:11 +0000

    Update to monad-control-1.0.3.1
    1.0.3.1.
    * Support transformers-0.6
    
    1.0.3
    * Add `controlT`
    * Support transformers-compat-0.7

--- devel/hs-monad-control/buildlink3.mk
+++ devel/hs-monad-control/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/01/18 02:48:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/02/12 04:45:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-monad-control
 
 .if !defined(HS_MONAD_CONTROL_BUILDLINK3_MK)
 HS_MONAD_CONTROL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.2.3nb2
+BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.3.1
 BUILDLINK_PKGSRCDIR.hs-monad-control?=		../../devel/hs-monad-control
 
 .include "../../devel/hs-transformers-base/buildlink3.mk"
