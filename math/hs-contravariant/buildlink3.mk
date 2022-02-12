eb3be19bbbf3311883dbcdb44a3f8a994b3b1453
pho <pho@pkgsrc.org>
2022-02-12 05:15:54 +0000

    Update to contravariant-1.5.5
    1.5.5 [2021.07.27]
        Fix the build on old GHCs using transformers-0.6.*.
    
    1.5.4 [2021.07.25]
        Allow building with transformers-0.6.*.

--- math/hs-contravariant/buildlink3.mk
+++ math/hs-contravariant/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/12 05:15:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-contravariant
 
 .if !defined(HS_CONTRAVARIANT_BUILDLINK3_MK)
 HS_CONTRAVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.3
-BUILDLINK_ABI_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.3nb2
+BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.5
+BUILDLINK_ABI_DEPENDS.hs-contravariant+=	hs-contravariant>=1.5.5
 BUILDLINK_PKGSRCDIR.hs-contravariant?=		../../math/hs-contravariant
 
 .include "../../devel/hs-StateVar/buildlink3.mk"
