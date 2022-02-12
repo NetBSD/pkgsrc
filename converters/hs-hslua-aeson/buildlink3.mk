6b87ab46a0156197441e82cd11c5b9897f317e13
pho <pho@pkgsrc.org>
2022-02-12 08:10:19 +0000

    converters/hs-hslua-aeson: import hs-hslua-aeson-2.1.0
    This package provides instances to push and receive any datatype encodable
    as JSON to and from the Lua stack.

---
+++ converters/hs-hslua-aeson/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 08:10:19 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-aeson
+
+.if !defined(HS_HSLUA_AESON_BUILDLINK3_MK)
+HS_HSLUA_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-aeson+=	hs-hslua-aeson>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-aeson+=	hs-hslua-aeson>=2.1.0
+BUILDLINK_PKGSRCDIR.hs-hslua-aeson?=	../../converters/hs-hslua-aeson
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_HSLUA_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-aeson
