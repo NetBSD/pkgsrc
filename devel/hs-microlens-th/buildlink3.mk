d9052fb6c3f0360d75dd930d9b5d9e38f13d2bf8
pho <pho@pkgsrc.org>
2022-02-12 07:45:42 +0000

    devel/hs-microlens-th: import hs-microlens-th-0.4.3.10
    This package lets you automatically generate lenses for data types; code
    was extracted from the lens package, and therefore generated lenses are
    fully compatible with ones generated by lens (and can be used both from
    lens and microlens).

---
+++ devel/hs-microlens-th/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 07:45:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-microlens-th
+
+.if !defined(HS_MICROLENS_TH_BUILDLINK3_MK)
+HS_MICROLENS_TH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3
+BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.10
+BUILDLINK_PKGSRCDIR.hs-microlens-th?=	../../devel/hs-microlens-th
+
+.include "../../devel/hs-microlens/buildlink3.mk"
+.include "../../devel/hs-th-abstraction/buildlink3.mk"
+.endif	# HS_MICROLENS_TH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-microlens-th
