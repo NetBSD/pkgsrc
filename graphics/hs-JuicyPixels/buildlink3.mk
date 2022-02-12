85d72fb73043b1e28aae26cb10052a4cfae47615
pho <pho@pkgsrc.org>
2022-02-12 05:01:27 +0000

    Update to JuicyPixels-3.3.6
    v3.3.6 October 2021
    -------------------
     * Bytestring bound bump
     * Fix bug #187.  (Some JPEGs are misidentified as SourceTiff.)
     * Fix EXIF handling of strings of four characters or fewer.
     * Fix endianness bug in short ExifString and ExifUndefined.

--- graphics/hs-JuicyPixels/buildlink3.mk
+++ graphics/hs-JuicyPixels/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/18 02:48:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/02/12 05:01:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-JuicyPixels
 
 .if !defined(HS_JUICYPIXELS_BUILDLINK3_MK)
 HS_JUICYPIXELS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.5
-BUILDLINK_ABI_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.5nb2
+BUILDLINK_API_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.6
+BUILDLINK_ABI_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.6
 BUILDLINK_PKGSRCDIR.hs-JuicyPixels?=	../../graphics/hs-JuicyPixels
 
 .include "../../devel/hs-primitive/buildlink3.mk"
