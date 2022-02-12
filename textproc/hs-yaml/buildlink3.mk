9cd9e56018c9e555d111a69b19eab26054af939f
pho <pho@pkgsrc.org>
2022-02-12 07:19:30 +0000

    Update to yaml-0.11.7.0
    0.11.7.0
        Support aeson 2 #202
    
    0.11.6.0
        yaml2json: add --help and --version options #197
        json2yaml: add --help and --version options #198
        Add the -o options to both yaml2json and json2yaml #200

--- textproc/hs-yaml/buildlink3.mk
+++ textproc/hs-yaml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/01/18 02:48:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/02/12 07:19:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-yaml
 
 .if !defined(HS_YAML_BUILDLINK3_MK)
 HS_YAML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-yaml+=	hs-yaml>=0.11.5
-BUILDLINK_ABI_DEPENDS.hs-yaml+=	hs-yaml>=0.11.5.0nb2
+BUILDLINK_API_DEPENDS.hs-yaml+=	hs-yaml>=0.11.7
+BUILDLINK_ABI_DEPENDS.hs-yaml+=	hs-yaml>=0.11.7.0
 BUILDLINK_PKGSRCDIR.hs-yaml?=	../../textproc/hs-yaml
 
 .include "../../converters/hs-aeson/buildlink3.mk"
