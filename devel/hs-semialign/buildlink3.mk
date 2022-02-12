5ce77a4c154d47b5a2e573158588580373dbf3c6
pho <pho@pkgsrc.org>
2022-02-12 05:27:27 +0000

    devel/hs-semialign: import hs-semialign-1.2.0.1
    The major use of These of this is provided by the align member of Semialign
    class, representing a generalized notion of "zipping with padding" that
    combines structures without truncating to the size of the smaller input.
    
    It turns out that zip operation fits well the Semialign class, forming
    lattice-like structure.

---
+++ devel/hs-semialign/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 05:27:27 pho Exp $
+
+BUILDLINK_TREE+=	hs-semialign
+
+.if !defined(HS_SEMIALIGN_BUILDLINK3_MK)
+HS_SEMIALIGN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-semialign+=	hs-semialign>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-semialign+=	hs-semialign>=1.2.0.1
+BUILDLINK_PKGSRCDIR.hs-semialign?=	../../devel/hs-semialign
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable-instances/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-these/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.endif	# HS_SEMIALIGN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-semialign
