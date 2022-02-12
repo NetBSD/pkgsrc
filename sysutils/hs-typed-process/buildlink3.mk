f27b87570ca5fc034e3aa2a3d77be65bb9edea94
pho <pho@pkgsrc.org>
2022-02-12 04:49:33 +0000

    Update to typed-process-0.2.8.0
    0.2.8.0
        Re-export ExitCode, ExitSuccess and ExitFailure.
    
    0.2.7.0
        Include empty argument in the show instance.
    
    0.2.6.3
        Doc improvements
    
    0.2.6.2
        Doc improvements

--- sysutils/hs-typed-process/buildlink3.mk
+++ sysutils/hs-typed-process/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/01/18 02:48:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/02/12 04:49:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-typed-process
 
 .if !defined(HS_TYPED_PROCESS_BUILDLINK3_MK)
 HS_TYPED_PROCESS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.6
-BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.6.0nb2
+BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.8
+BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.8.0
 BUILDLINK_PKGSRCDIR.hs-typed-process?=		../../sysutils/hs-typed-process
 
 .include "../../devel/hs-async/buildlink3.mk"
