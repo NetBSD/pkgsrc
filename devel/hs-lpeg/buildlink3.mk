0192118c871bdb7fbeff3ca9f6f0aee8fa179559
pho <pho@pkgsrc.org>
2022-02-12 08:13:05 +0000

    devel/hs-lpeg: import hs-lpeg-1.0.2
    This package contains the C sources of LPeg, as well as some tiny Haskell
    helper to load the package.

---
+++ devel/hs-lpeg/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 08:13:05 pho Exp $
+
+BUILDLINK_TREE+=	hs-lpeg
+
+.if !defined(HS_LPEG_BUILDLINK3_MK)
+HS_LPEG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lpeg+=	hs-lpeg>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-lpeg+=	hs-lpeg>=1.0.2
+BUILDLINK_PKGSRCDIR.hs-lpeg?=	../../devel/hs-lpeg
+
+.include "../../lang/hs-lua/buildlink3.mk"
+.endif	# HS_LPEG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lpeg
