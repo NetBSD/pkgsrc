0122e64cb5c7f5e57dbd335a04ea23fc4ac64f98
pho <pho@pkgsrc.org>
2022-02-12 07:52:15 +0000

    devel/hs-githash: import hs-githash-0.1.6.2
    Some handy Template Haskell splices for including the current git hash and
    branch in the code of your project. Useful for including in panic messages,
    --version output, or diagnostic info for more informative bug reports.

---
+++ devel/hs-githash/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/12 07:52:15 pho Exp $
+
+BUILDLINK_TREE+=	hs-githash
+
+.if !defined(HS_GITHASH_BUILDLINK3_MK)
+HS_GITHASH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-githash+=	hs-githash>=0.1.6
+BUILDLINK_ABI_DEPENDS.hs-githash+=	hs-githash>=0.1.6.2
+BUILDLINK_PKGSRCDIR.hs-githash?=	../../devel/hs-githash
+
+.include "../../devel/hs-th-compat/buildlink3.mk"
+.endif	# HS_GITHASH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-githash
