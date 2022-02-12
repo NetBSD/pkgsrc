45dd0cd28dc404867712a18f687b4bbf6a883d3e
pho <pho@pkgsrc.org>
2022-02-12 05:05:55 +0000

    Update to http2-3.0.3
    3.0.3
    * Return correct status messages in HTTP2 client
      (#31)[https://github.com/kazu-yamamoto/http2/pull/31]
    * Follow changes in Aeson 2
      (#32)[https://github.com/kazu-yamamoto/http2/pull/32]
    * Make sure connection preface is always sent first
      (#33)[https://github.com/kazu-yamamoto/http2/pull/33]
    * Avoid empty data (#34)[https://github.com/kazu-yamamoto/http2/pull/34]
    
    3.0.2
    * Skip inserting entries that do not fit in the encoding table
      (#28)[https://github.com/kazu-yamamoto/http2/pull/28]

--- www/hs-http2/buildlink3.mk
+++ www/hs-http2/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/01/18 02:48:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/02/12 05:05:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-http2
 
 .if !defined(HS_HTTP2_BUILDLINK3_MK)
 HS_HTTP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=3.0.1
-BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=3.0.1nb1
+BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=3.0.3
+BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=3.0.3
 BUILDLINK_PKGSRCDIR.hs-http2?=		../../www/hs-http2
 
+.include "../../devel/hs-async/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
