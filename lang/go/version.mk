61eb1a0e605856a48fa110ad232cc54f5f6e6e5b
bsiegert <bsiegert@pkgsrc.org>
2022-02-12 19:52:40 +0000

    Update go116 to 1.16.14 (security update).
    crypto/elliptic: fix IsOnCurve for big.Int values that are not valid coordinates
    
    Some big.Int values that are not valid field elements (negative or overflowing)
    might cause Curve.IsOnCurve to incorrectly return true. Operating on those values
    may cause a panic or an invalid curve operation. Note that Unmarshal will never
    return such values.
    
    Thanks to Guido Vranken for reporting this.
    
    This is CVE-2022-23806 and https://go.dev/issue/50974.
    
    math/big: prevent large memory consumption in Rat.SetString
    
    An attacker can cause unbounded memory growth in a program using (*Rat).SetString
    due to an unhandled overflow.
    
    Thanks to the OSS-Fuzz project for discovering this issue and to Emmanuel Odeke
    (@odeke_et) for reporting it.
    
    This is CVE-2022-23772 and Go issue https://go.dev/issue/50699.
    
    cmd/go: prevent branches from materializing into versions
    
    A branch whose name resembles a version tag (such as "v1.0.0" or "subdir/v2.0.0-dev")
    can be considered a valid version by the go command. Materializing versions from
    branches might be unexpected and bypass ACLs that limit the creation of tags but not
    branches.
    
    This is CVE-2022-23773 and Go issue https://go.dev/issue/35671.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.139 2022/01/09 19:54:46 bsiegert Exp $
+# $NetBSD: version.mk,v 1.140 2022/02/12 19:52:40 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO117_VERSION=	1.17.6
-GO116_VERSION=	1.16.13
+GO116_VERSION=	1.16.14
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
 GO14_VERSION=	1.4.3
