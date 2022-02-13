d3c44559e49ad9f8eb677597f14fbd359a19a977
nia <nia@pkgsrc.org>
2022-02-13 10:44:38 +0000

    modular-xorg/avoid-duplicate.mk: Use PKG_SKIP_REASON instead of RESTRICTED, because the modular Xorg components this is applied to will often fail to build against native Xorg components anyway.

--- meta-pkgs/modular-xorg/avoid-duplicate.mk
+++ meta-pkgs/modular-xorg/avoid-duplicate.mk
@@ -1,11 +1,9 @@
-# $NetBSD: avoid-duplicate.mk,v 1.1 2021/12/01 12:48:17 nia Exp $
+# $NetBSD: avoid-duplicate.mk,v 1.2 2022/02/13 10:44:38 nia Exp $
 
 BUILTIN_PKG=		${X11_BUILTIN_NAME}
 
 .include "${.INCLUDEDFROMDIR}/builtin.mk"
 
 .if ${X11_TYPE} == "native" && ${USE_BUILTIN.${X11_BUILTIN_NAME}} == "yes"
-RESTRICTED=		"Package set is using native X11 component"
-NO_BIN_ON_FTP=		${RESTRICTED}
-NO_BIN_ON_CDROM=	${RESTRICTED}
+PKG_SKIP_REASON+=	"Package set is using native X11 component"
 .endif
