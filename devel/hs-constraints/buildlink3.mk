5347812f508694ef10080d5dacc41fda4f044845
pho <pho@pkgsrc.org>
2022-02-14 14:38:56 +0000

    devel/hs-constraints: import hs-constraints-0.13.3
    GHC 7.4 gave us the ability to talk about ConstraintKinds. They stopped
    crashing the compiler in GHC 7.6.
    
    This package provides a vocabulary for working with them.

---
+++ devel/hs-constraints/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/14 14:38:56 pho Exp $
+
+BUILDLINK_TREE+=	hs-constraints
+
+.if !defined(HS_CONSTRAINTS_BUILDLINK3_MK)
+HS_CONSTRAINTS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-constraints+=	hs-constraints>=0.13.3
+BUILDLINK_ABI_DEPENDS.hs-constraints+=	hs-constraints>=0.13.3
+BUILDLINK_PKGSRCDIR.hs-constraints?=	../../devel/hs-constraints
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-type-equality/buildlink3.mk"
+.endif	# HS_CONSTRAINTS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-constraints
