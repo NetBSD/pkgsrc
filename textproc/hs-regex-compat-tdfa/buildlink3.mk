022db80de303cb39cfb15143ab5c1dbbeec12392
pho <pho@pkgsrc.org>
2022-02-14 14:42:57 +0000

    textproc/hs-regex-compat-tdfa: import hs-regex-compat-tdfa-0.95.1.4
    This package provide a fork of the regex-compat package which supports
    Unicode.

---
+++ textproc/hs-regex-compat-tdfa/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/14 14:42:57 pho Exp $
+
+BUILDLINK_TREE+=	hs-regex-compat-tdfa
+
+.if !defined(HS_REGEX_COMPAT_TDFA_BUILDLINK3_MK)
+HS_REGEX_COMPAT_TDFA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-regex-compat-tdfa+=	hs-regex-compat-tdfa>=0.95.1
+BUILDLINK_ABI_DEPENDS.hs-regex-compat-tdfa+=	hs-regex-compat-tdfa>=0.95.1.4
+BUILDLINK_PKGSRCDIR.hs-regex-compat-tdfa?=	../../textproc/hs-regex-compat-tdfa
+
+.include "../../textproc/hs-regex-base/buildlink3.mk"
+.include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.endif	# HS_REGEX_COMPAT_TDFA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-regex-compat-tdfa
