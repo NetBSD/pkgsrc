e23f9bacff2a983bf76e78c48ced8fbd14de50a0
pho <pho@pkgsrc.org>
2022-02-14 14:41:59 +0000

    textproc/hs-regex-applicative: import hs-regex-applicative-0.3.4
    regex-applicative is a Haskell library for parsing using regular
    expressions. Parsers can be built using Applicative interface.

---
+++ textproc/hs-regex-applicative/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/14 14:41:59 pho Exp $
+
+BUILDLINK_TREE+=	hs-regex-applicative
+
+.if !defined(HS_REGEX_APPLICATIVE_BUILDLINK3_MK)
+HS_REGEX_APPLICATIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-regex-applicative+=	hs-regex-applicative>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-regex-applicative+=	hs-regex-applicative>=0.3.4
+BUILDLINK_PKGSRCDIR.hs-regex-applicative?=	../../textproc/hs-regex-applicative
+
+.include "../../devel/hs-filtrable/buildlink3.mk"
+.endif	# HS_REGEX_APPLICATIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-regex-applicative
