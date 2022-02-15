e7d062fd41ea5533d4d6ea1d3770eb6a78387926
rillig <rillig@pkgsrc.org>
2022-02-15 21:21:24 +0000

    devel/intellij-ce-bin: update to 2021.3.2
    Changes since 2019.1.2:
    
    From https://www.jetbrains.com/idea/whatsnew/:
    IntelliJ IDEA 2021.3 provides support for remote development (Beta) and
    introduces a new way to troubleshoot IDE problems with the Repair IDE...
    action. It also brings debugger updates and the Constant conditions
    inspection for Kotlin, along with other valuable changes.
    
    From https://www.jetbrains.com/idea/whatsnew/2021-2/:
    IntelliJ IDEA 2021.2 introduces project-wide analysis for Java, new
    actions that can be triggered when you save changes, a new UI for
    managing Maven and Gradle dependencies, and other useful updates.
    
    From https://www.jetbrains.com/idea/whatsnew/2021-1/:
    IntelliJ IDEA 2021.1 introduces Java 16 and WSL 2 support, Space
    integration, Code With Me, a HTML preview window inside the IDE, the
    option to run code on SSH hosts and in Docker containers, and other
    useful updates.
    
    From https://www.jetbrains.com/idea/whatsnew/2020-3/:
    IntelliJ IDEA 2020.3 adds interactive hints and inline watches in the
    debugger, improves support for Java 15, and introduces support for Git
    staging, along with a variety of other features.
    
    From https://www.jetbrains.com/idea/whatsnew/2020-2/:
    IntelliJ IDEA 2020.2 lets you review and merge GitHub pull requests
    right from inside the IDE, quickly navigate between warnings and errors
    in a file with the Inspections widget, view the full list of issues in a
    current file with the Problems tool window, and get notified if your
    changes would break other files. You can use Jakarta EE, and get better
    support for Quarkus, Micronaut, and OpenAPI.
    
    From https://www.jetbrains.com/idea/whatsnew/2020-1/:
    IntelliJ IDEA 2020.1 adds support for Java 14 and new features for a
    number of frameworks, upgrades the debugger with dataflow analysis
    assistance, adds a new LightEdit mode, downloads and configures the JDK
    for you, and does so much more!
    
    From https://www.jetbrains.com/idea/whatsnew/2019-3/:
    IntelliJ IDEA 2019.3 delivers major performance and usability
    improvements, including faster startup, easier installation of theme and
    keymap plugins, enhanced VCS workflows, and adds support for
    microservices frameworks, MongoDB and more.
    
    From https://www.jetbrains.com/idea/whatsnew/2019-2/:
    
    Java 13, Refactoring methods with multiple exit points, Each directory
    can have its own code style, Syntax highlighting is available for over
    20 languages, IntelliJ IDEA works with large files smoothly, Maven
    dependency completion works out of the box, Step Into action offers you
    a choice of methods to step into, and several more.

--- devel/intellij-ce-bin/patches/patch-bin_idea.sh
+++ devel/intellij-ce-bin/patches/patch-bin_idea.sh
@@ -1,24 +1,24 @@
-$NetBSD: patch-bin_idea.sh,v 1.1 2019/03/03 16:34:27 youri Exp $
+$NetBSD: patch-bin_idea.sh,v 1.2 2022/02/15 21:21:24 rillig Exp $
 
 Add a few JVM options to make it work in a common case.
 
---- bin/idea.sh.orig	2019-02-26 12:34:54.000000000 +0000
+--- bin/idea.sh.orig	2022-01-28 04:01:00.000000000 +0000
 +++ bin/idea.sh
-@@ -21,6 +21,7 @@ message()
-   fi
- }
+@@ -49,6 +49,7 @@ CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/
+ # ---------------------------------------------------------------------
+ JRE=""
  
 +IDEA_JDK=@PKG_JAVA_HOME@
- UNAME=`which uname`
- GREP=`which egrep`
- GREP_OPTIONS=""
-@@ -202,6 +203,9 @@ fi
- # ---------------------------------------------------------------------
+ # shellcheck disable=SC2154
+ if [ -n "$IDEA_JDK" ] && [ -x "$IDEA_JDK/bin/java" ]; then
+   JRE="$IDEA_JDK"
+@@ -148,6 +149,9 @@ fi
  IFS="$(printf '\n\t')"
+ # shellcheck disable=SC2086
  "$JAVA_BIN" \
 +  -Xmx4096 -Xms4096 \
 +  -XX:-UseCompressedClassPointers \
 +  -XX:-UseCompressedOops \
    -classpath "$CLASSPATH" \
    ${VM_OPTIONS} \
-   "-XX:ErrorFile=$HOME/java_error_in_IDEA_%p.log" \
+   "-XX:ErrorFile=$HOME/java_error_in_idea_%p.log" \
