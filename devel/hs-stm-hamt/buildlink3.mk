c8bd3f638ce4803740668264ea9679073616f367
pho <pho@pkgsrc.org>
2022-02-16 09:54:36 +0000

    devel/hs-stm-hamt: import hs-stm-hamt-1.2.0.7
    A low-level data-structure, which can be used to implement higher-level
    interfaces like hash-map and hash-set. Such implementations are presented
    by the "stm-containers" library.

---
+++ devel/hs-stm-hamt/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:54:36 pho Exp $
+
+BUILDLINK_TREE+=	hs-stm-hamt
+
+.if !defined(HS_STM_HAMT_BUILDLINK3_MK)
+HS_STM_HAMT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-stm-hamt+=	hs-stm-hamt>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-stm-hamt+=	hs-stm-hamt>=1.2.0.7
+BUILDLINK_PKGSRCDIR.hs-stm-hamt?=	../../devel/hs-stm-hamt
+
+.include "../../devel/hs-deferred-folds/buildlink3.mk"
+.include "../../devel/hs-focus/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-list-t/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-primitive-extras/buildlink3.mk"
+.endif	# HS_STM_HAMT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-stm-hamt
