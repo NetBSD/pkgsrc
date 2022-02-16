d1b9e5a74d4928d27c6c040051d113e6ddc9c0d4
pho <pho@pkgsrc.org>
2022-02-16 10:14:35 +0000

    devel/hs-hls-explicit-imports-plugin: import hs-hls-explicit-imports-plugin-1.0.2.0
    Explicit imports plugin for Haskell Language Server

---
+++ devel/hs-hls-explicit-imports-plugin/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:14:35 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-explicit-imports-plugin
+
+.if !defined(HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK)
+HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.0.2.0
+BUILDLINK_PKGSRCDIR.hs-hls-explicit-imports-plugin?=	../../devel/hs-hls-explicit-imports-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-graph/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-explicit-imports-plugin
