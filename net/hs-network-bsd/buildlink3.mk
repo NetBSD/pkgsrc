476b3763873e9032e9ff522146a4a8bc05e141e5
pho <pho@pkgsrc.org>
2022-02-16 09:15:18 +0000

    net/hs-network-bsd: import hs-network-bsd-2.8.1.0
    This package provides Haskell bindings to the the POSIX network database
    (netdb.h) API.

---
+++ net/hs-network-bsd/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:15:18 pho Exp $
+
+BUILDLINK_TREE+=	hs-network-bsd
+
+.if !defined(HS_NETWORK_BSD_BUILDLINK3_MK)
+HS_NETWORK_BSD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-network-bsd+=	hs-network-bsd>=2.8.1
+BUILDLINK_ABI_DEPENDS.hs-network-bsd+=	hs-network-bsd>=2.8.1.0
+BUILDLINK_PKGSRCDIR.hs-network-bsd?=	../../net/hs-network-bsd
+
+.include "../../net/hs-network/buildlink3.mk"
+.endif	# HS_NETWORK_BSD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-network-bsd
