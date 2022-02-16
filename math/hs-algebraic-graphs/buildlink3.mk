57a90337ed33226748b57d8e7d069ec34e9e19f7
pho <pho@pkgsrc.org>
2022-02-16 09:35:17 +0000

    math/hs-algebraic-graphs: import hs-algebraic-graphs-0.6
    A library for algebraic construction and manipulation of graphs in Haskell.

---
+++ math/hs-algebraic-graphs/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:35:17 pho Exp $
+
+BUILDLINK_TREE+=	hs-algebraic-graphs
+
+.if !defined(HS_ALGEBRAIC_GRAPHS_BUILDLINK3_MK)
+HS_ALGEBRAIC_GRAPHS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-algebraic-graphs+=	hs-algebraic-graphs>=0.6
+BUILDLINK_ABI_DEPENDS.hs-algebraic-graphs+=	hs-algebraic-graphs>=0.6
+BUILDLINK_PKGSRCDIR.hs-algebraic-graphs?=	../../math/hs-algebraic-graphs
+.endif	# HS_ALGEBRAIC_GRAPHS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-algebraic-graphs
