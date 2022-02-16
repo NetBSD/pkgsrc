31448907bdcdcf59e54c0baeb175b98c89d7f062
pho <pho@pkgsrc.org>
2022-02-16 10:13:19 +0000

    devel/hs-optparse-simple: import hs-optparse-simple-0.1.1.4
    Simple interface to optparse-applicative.

---
+++ devel/hs-optparse-simple/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:13:19 pho Exp $
+
+BUILDLINK_TREE+=	hs-optparse-simple
+
+.if !defined(HS_OPTPARSE_SIMPLE_BUILDLINK3_MK)
+HS_OPTPARSE_SIMPLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-optparse-simple+=	hs-optparse-simple>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-optparse-simple+=	hs-optparse-simple>=0.1.1.4
+BUILDLINK_PKGSRCDIR.hs-optparse-simple?=	../../devel/hs-optparse-simple
+
+.include "../../devel/hs-githash/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../devel/hs-th-compat/buildlink3.mk"
+.endif	# HS_OPTPARSE_SIMPLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-optparse-simple
