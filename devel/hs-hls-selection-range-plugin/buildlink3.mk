5e9b8eeb5bced29862bd65279426a5e31c7073f1
pho <pho@pkgsrc.org>
2022-02-16 10:18:09 +0000

    devel/hs-hls-selection-range-plugin: import hs-hls-selection-range-plugin-1.0.0.0
    HLS Plugin to support smart selection range

---
+++ devel/hs-hls-selection-range-plugin/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:18:09 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-selection-range-plugin
+
+.if !defined(HS_HLS_SELECTION_RANGE_PLUGIN_BUILDLINK3_MK)
+HS_HLS_SELECTION_RANGE_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-selection-range-plugin+=	hs-hls-selection-range-plugin>=1.0.0
+BUILDLINK_ABI_DEPENDS.hs-hls-selection-range-plugin+=	hs-hls-selection-range-plugin>=1.0.0.0
+BUILDLINK_PKGSRCDIR.hs-hls-selection-range-plugin?=	../../devel/hs-hls-selection-range-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.endif	# HS_HLS_SELECTION_RANGE_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-selection-range-plugin
