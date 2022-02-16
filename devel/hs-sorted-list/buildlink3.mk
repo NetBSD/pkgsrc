abe897ff871f8b27f75b22aa50a0672ea4f5aa8a
pho <pho@pkgsrc.org>
2022-02-16 10:04:42 +0000

    devel/hs-sorted-list: import hs-sorted-list-0.2.1.0
    Type-enforced sorted lists and related functions.
    
    These are useful for:
    * Constraining the argument of a function to be a sorted list by stating in
      your type that the input list is a sorted list.
    * Avoiding sorting a list twice.
    * Creating a list that is sorted from the moment of its construction, so it
      doesn't have to be sorted later.
    * Performing list operations keeping the input list sorted.
    * Improving those list operations that can be benefited from the ordering
      of its elements.
    * Creating infinite lists that are sorted!
    * And more!

---
+++ devel/hs-sorted-list/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:04:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-sorted-list
+
+.if !defined(HS_SORTED_LIST_BUILDLINK3_MK)
+HS_SORTED_LIST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-sorted-list+=	hs-sorted-list>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-sorted-list+=	hs-sorted-list>=0.2.1.0
+BUILDLINK_PKGSRCDIR.hs-sorted-list?=	../../devel/hs-sorted-list
+.endif	# HS_SORTED_LIST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-sorted-list
