f36772187dd71a2796bddbc3a4937ce8092710c4
pho <pho@pkgsrc.org>
2022-02-16 09:45:28 +0000

    devel/hs-foldl: import hs-foldl-1.4.12
    This library provides strict left folds that stream in constant memory, and
    you can combine folds using Applicative style to derive new folds. Derived
    folds still traverse the container just once and are often as efficient as
    hand-written folds.

---
+++ devel/hs-foldl/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:45:28 pho Exp $
+
+BUILDLINK_TREE+=	hs-foldl
+
+.if !defined(HS_FOLDL_BUILDLINK3_MK)
+HS_FOLDL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.12
+BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.12
+BUILDLINK_PKGSRCDIR.hs-foldl?=		../../devel/hs-foldl
+
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-contravariant/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_FOLDL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-foldl
