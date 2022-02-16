cc001479ef98e5ecac3daec65e04a3fe9bcef4d5
pho <pho@pkgsrc.org>
2022-02-16 10:13:56 +0000

    devel/hs-hls-call-hierarchy-plugin: import hs-hls-call-hierarchy-plugin-1.0.2.0
    The call hierarchy plugin can review the code to determine where functions
    are called and how they relate to other functions.
    
    This plugin is useful when debugging and refactoring code because it allows
    you to see how different parts of the code are related. And it is more
    conducive for users to quickly understand their macro architecture in the
    face of strange code.

---
+++ devel/hs-hls-call-hierarchy-plugin/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:13:56 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-call-hierarchy-plugin
+
+.if !defined(HS_HLS_CALL_HIERARCHY_PLUGIN_BUILDLINK3_MK)
+HS_HLS_CALL_HIERARCHY_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-call-hierarchy-plugin+=	hs-hls-call-hierarchy-plugin>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-hls-call-hierarchy-plugin+=	hs-hls-call-hierarchy-plugin>=1.0.2.0
+BUILDLINK_PKGSRCDIR.hs-hls-call-hierarchy-plugin?=	../../devel/hs-hls-call-hierarchy-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hiedb/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../databases/hs-sqlite-simple/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_CALL_HIERARCHY_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-call-hierarchy-plugin
