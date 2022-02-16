1da78c3f5d4b76c6c98ac8dfba3170dc52511da2
pho <pho@pkgsrc.org>
2022-02-16 08:53:40 +0000

    math/hs-free: import hs-free-5.1.7
    Free monads are useful for many tree-like structures and domain specific
    languages.
    
    If f is a Functor then the free Monad on f is the type of trees whose nodes
    are labeled with the constructors of f. The word "free" is used in the
    sense of "unrestricted" rather than "zero-cost": Free f makes no
    constraining assumptions beyond those given by f and the definition of
    Monad. As used here it is a standard term from the mathematical theory of
    adjoint functors.
    
    Cofree comonads are dual to free monads. They provide convenient ways to
    talk about branching streams and rose-trees, and can be used to annotate
    syntax trees. The cofree comonad can be seen as a stream parameterized by a
    Functor that controls its branching factor.

---
+++ math/hs-free/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 08:53:40 pho Exp $
+
+BUILDLINK_TREE+=	hs-free
+
+.if !defined(HS_FREE_BUILDLINK3_MK)
+HS_FREE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-free+=	hs-free>=5.1.7
+BUILDLINK_ABI_DEPENDS.hs-free+=	hs-free>=5.1.7
+BUILDLINK_PKGSRCDIR.hs-free?=	../../math/hs-free
+
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-distributive/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../devel/hs-th-abstraction/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.endif	# HS_FREE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-free
