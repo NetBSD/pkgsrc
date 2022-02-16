282982cf9171ad6e1dc3553dfe34ce225542939e
pho <pho@pkgsrc.org>
2022-02-16 10:22:25 +0000

    devel/hs-hls-fourmolu-plugin: import hs-hls-fourmolu-plugin-1.0.2.0
    Integration with the Fourmolu code formatter

---
+++ devel/hs-hls-fourmolu-plugin/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:22:25 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-fourmolu-plugin
+
+.if !defined(HS_HLS_FOURMOLU_PLUGIN_BUILDLINK3_MK)
+HS_HLS_FOURMOLU_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-fourmolu-plugin+=	hs-hls-fourmolu-plugin>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-hls-fourmolu-plugin+=	hs-hls-fourmolu-plugin>=1.0.2.0
+BUILDLINK_PKGSRCDIR.hs-hls-fourmolu-plugin?=	../../devel/hs-hls-fourmolu-plugin
+
+.include "../../devel/fourmolu/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.endif	# HS_HLS_FOURMOLU_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-fourmolu-plugin
