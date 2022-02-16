f53a9abe8ea3bcb8646017312991e2ceaca32dc0
pho <pho@pkgsrc.org>
2022-02-16 09:13:57 +0000

    devel/hs-ghc-exactprint: import hs-ghc-exactprint-1.5.0
    Using the API Annotations available from GHC 9.2.1, this library provides a
    means to round trip any code that can be compiled by GHC, currently
    excluding lhs files.

---
+++ devel/hs-ghc-exactprint/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:13:57 pho Exp $
+
+BUILDLINK_TREE+=	hs-ghc-exactprint
+
+.if !defined(HS_GHC_EXACTPRINT_BUILDLINK3_MK)
+HS_GHC_EXACTPRINT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.5.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.5.0
+BUILDLINK_PKGSRCDIR.hs-ghc-exactprint?=		../../devel/hs-ghc-exactprint
+
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
+.include "../../math/hs-free/buildlink3.mk"
+.include "../../devel/hs-ghc-paths/buildlink3.mk"
+.include "../../devel/hs-ordered-containers/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.endif	# HS_GHC_EXACTPRINT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-ghc-exactprint
