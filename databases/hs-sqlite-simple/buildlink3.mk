f738b585843e978cf6895b5bfdc8d378b3e164d6
pho <pho@pkgsrc.org>
2022-02-16 09:43:12 +0000

    databases/hs-sqlite-simple: import hs-sqlite-simple-0.4.18.0
    This library is a mid-level Haskell binding to the SQLite database.
    
    Sqlite-simple provides a convenient API to sqlite that does some level of
    automatic data conversion between the database and Haskell types. The API
    has been modeled directly after postgresql-simple which in turn borrows
    from mysql-simple.

---
+++ databases/hs-sqlite-simple/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:43:12 pho Exp $
+
+BUILDLINK_TREE+=	hs-sqlite-simple
+
+.if !defined(HS_SQLITE_SIMPLE_BUILDLINK3_MK)
+HS_SQLITE_SIMPLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18
+BUILDLINK_ABI_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18.0
+BUILDLINK_PKGSRCDIR.hs-sqlite-simple?=		../../databases/hs-sqlite-simple
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../devel/hs-blaze-textual/buildlink3.mk"
+.include "../../databases/hs-direct-sqlite/buildlink3.mk"
+.include "../../devel/hs-Only/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.endif	# HS_SQLITE_SIMPLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-sqlite-simple
