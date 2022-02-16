dbe65dd7d1e36cbf2ec344e4ad872b61eecb315e
pho <pho@pkgsrc.org>
2022-02-16 10:01:34 +0000

    converters/hs-lens-aeson: import hs-lens-aeson-1.1.3
    The goal of lens-aeson is to provide traversals and prisms for the Aeson
    library's Value type, while obeying the Traversal/Prism laws.

---
+++ converters/hs-lens-aeson/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:01:34 pho Exp $
+
+BUILDLINK_TREE+=	hs-lens-aeson
+
+.if !defined(HS_LENS_AESON_BUILDLINK3_MK)
+HS_LENS_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lens-aeson+=	hs-lens-aeson>=1.1.3
+BUILDLINK_ABI_DEPENDS.hs-lens-aeson+=	hs-lens-aeson>=1.1.3
+BUILDLINK_PKGSRCDIR.hs-lens-aeson?=	../../converters/hs-lens-aeson
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_LENS_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lens-aeson
