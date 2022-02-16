7f8602aaf81965c82b5a9097b5fb0056dbfce099
pho <pho@pkgsrc.org>
2022-02-16 09:34:26 +0000

    devel/hs-hie-compat: import hs-hie-compat-0.2.1.1
    Backports for HIE files to GHC 8.6, along with a few other backports of HIE
    file related fixes for ghcide. THIS DOES NOT LET YOU READ HIE FILES WITH
    MISMATCHED VERSIONS OF GHC.

---
+++ devel/hs-hie-compat/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:34:26 pho Exp $
+
+BUILDLINK_TREE+=	hs-hie-compat
+
+.if !defined(HS_HIE_COMPAT_BUILDLINK3_MK)
+HS_HIE_COMPAT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hie-compat+=	hs-hie-compat>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-hie-compat+=	hs-hie-compat>=0.2.1.1
+BUILDLINK_PKGSRCDIR.hs-hie-compat?=	../../devel/hs-hie-compat
+.endif	# HS_HIE_COMPAT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hie-compat
