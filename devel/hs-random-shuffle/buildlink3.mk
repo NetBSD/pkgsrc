9813602d617145993f84e3a82ed5ebfe2ef752dd
pho <pho@pkgsrc.org>
2022-02-16 10:10:15 +0000

    devel/hs-random-shuffle: import hs-random-shuffle-0.0.4
    Random shuffle implementation, on immutable lists. Based on "perfect
    shuffle" implementation by Oleg Kiselyov, available on
    http://okmij.org/ftp/Haskell/perfect-shuffle.txt

---
+++ devel/hs-random-shuffle/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:10:15 pho Exp $
+
+BUILDLINK_TREE+=	hs-random-shuffle
+
+.if !defined(HS_RANDOM_SHUFFLE_BUILDLINK3_MK)
+HS_RANDOM_SHUFFLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-random-shuffle+=	hs-random-shuffle>=0.0.4
+BUILDLINK_ABI_DEPENDS.hs-random-shuffle+=	hs-random-shuffle>=0.0.4
+BUILDLINK_PKGSRCDIR.hs-random-shuffle?=		../../devel/hs-random-shuffle
+
+.include "../../devel/hs-MonadRandom/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.endif	# HS_RANDOM_SHUFFLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-random-shuffle
