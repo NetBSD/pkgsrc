ee2c72edd965b6b60d4e3a021980cd0ed6c27981
pho <pho@pkgsrc.org>
2022-02-16 10:17:37 +0000

    devel/hs-hls-qualify-imported-names-plugin: import hs-hls-qualify-imported-names-plugin-1.0.1.0
    You've imported a number of modules, and have written a lot of code with
    unqualified names. You want to import a new module but you know there will
    be a number name clashes so you want to switch your current usage of names
    to be qualified. It would be nice if you could change
    
      import Blah
    
    to
    
      import Blah as Bloo
    
    then magically qualify all the previous names imported from Blah with
    Bloo. After doing that you could then change
    
      import Blah as Bloo
    
    to
    
      import qualified Blah as Bloo
    
    and import your the new module using names from it without worry. Well, now
    you can...

---
+++ devel/hs-hls-qualify-imported-names-plugin/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:17:37 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-qualify-imported-names-plugin
+
+.if !defined(HS_HLS_QUALIFY_IMPORTED_NAMES_PLUGIN_BUILDLINK3_MK)
+HS_HLS_QUALIFY_IMPORTED_NAMES_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-qualify-imported-names-plugin+=	hs-hls-qualify-imported-names-plugin>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hls-qualify-imported-names-plugin+=	hs-hls-qualify-imported-names-plugin>=1.0.1.0
+BUILDLINK_PKGSRCDIR.hs-hls-qualify-imported-names-plugin?=	../../devel/hs-hls-qualify-imported-names-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-graph/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_QUALIFY_IMPORTED_NAMES_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-qualify-imported-names-plugin
