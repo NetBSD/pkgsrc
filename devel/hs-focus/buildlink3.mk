b258f52b96c7ae3fb8c4293ef8f7956c9d42fd88
pho <pho@pkgsrc.org>
2022-02-16 05:08:34 +0000

    devel/hs-focus: import hs-focus-1.0.3
    An API for construction of free-form strategies of access and manipulation
    of elements of arbitrary data structures. It allows to implement efficient
    composite patterns, e.g., a simultaneous update and lookup of an element,
    and even more complex things.
    
    Strategies are meant to be interpreted by the host data structure
    libraries. Thus they allow to implement all access and modification
    patterns of a data structure with just a single function, which interprets
    strategies.
    
    This library provides pure and monadic interfaces, so it supports both
    immutable and mutable data structures.

---
+++ devel/hs-focus/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 05:08:34 pho Exp $
+
+BUILDLINK_TREE+=	hs-focus
+
+.if !defined(HS_FOCUS_BUILDLINK3_MK)
+HS_FOCUS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-focus+=	hs-focus>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-focus+=	hs-focus>=1.0.3
+BUILDLINK_PKGSRCDIR.hs-focus?=		../../devel/hs-focus
+.endif	# HS_FOCUS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-focus
