259279b51e2ff2c7dbb20e0e40f9b40a3f9b5f5c
pho <pho@pkgsrc.org>
2022-02-16 10:08:27 +0000

    devel/hs-hls-plugin-api: import hs-hls-plugin-api-1.3.0.0
    Haskell Language Server API for plugin communication

---
+++ devel/hs-hls-plugin-api/buildlink3.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:08:27 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-plugin-api
+
+.if !defined(HS_HLS_PLUGIN_API_BUILDLINK3_MK)
+HS_HLS_PLUGIN_API_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-plugin-api+=	hs-hls-plugin-api>=1.3.0
+BUILDLINK_ABI_DEPENDS.hs-hls-plugin-api+=	hs-hls-plugin-api>=1.3.0.0
+BUILDLINK_PKGSRCDIR.hs-hls-plugin-api?=		../../devel/hs-hls-plugin-api
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-dependent-map/buildlink3.mk"
+.include "../../devel/hs-dependent-sum/buildlink3.mk"
+.include "../../textproc/hs-Diff/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-hls-graph/buildlink3.mk"
+.include "../../sysutils/hs-hslogger/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../converters/hs-lens-aeson/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../sysutils/hs-opentelemetry/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_PLUGIN_API_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-plugin-api
