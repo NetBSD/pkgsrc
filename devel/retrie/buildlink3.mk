0a13ad1af5e09e4c74b0b41b68cf406c88cdd7e9
pho <pho@pkgsrc.org>
2022-02-16 10:11:00 +0000

    devel/retrie: import retrie-1.2.0.1
    Retrie is a tool for codemodding Haskell. Key goals include:
    
    * Speed: Efficiently rewrite in large (>1 million line) codebases.
    * Safety: Avoids large classes of codemod-related errors.
    * Ease-of-use: Haskell syntax instead of regular expressions. No
      hand-rolled AST traversals.
    
    This package provides a command-line tool (retrie) and a library (Retrie)
    for making equational edits to Haskell code.

---
+++ devel/retrie/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:11:00 pho Exp $
+
+BUILDLINK_TREE+=	retrie
+
+.if !defined(RETRIE_BUILDLINK3_MK)
+RETRIE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.retrie+=	retrie>=1.2.0
+BUILDLINK_ABI_DEPENDS.retrie+=	retrie>=1.2.0.1
+BUILDLINK_PKGSRCDIR.retrie?=	../../devel/retrie
+
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-ghc-exactprint/buildlink3.mk"
+.include "../../devel/hs-list-t/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../devel/hs-random-shuffle/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-ghc-paths/buildlink3.mk"
+.include "../../devel/hs-haskell-src-exts/buildlink3.mk"
+.endif	# RETRIE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-retrie
