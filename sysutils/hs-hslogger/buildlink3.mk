c340b692f3d2d19354d733e2bbe0446652df2d56
pho <pho@pkgsrc.org>
2022-02-16 09:19:42 +0000

    sysutils/hs-hslogger: import hs-hslogger-1.3.1.0
    hslogger is a logging framework for Haskell, roughly similar to Python's
    logging module.
    
    hslogger lets each log message have a priority and source be associated
    with it. The programmer can then define global handlers that route or
    filter messages based on the priority and source. hslogger also has a
    Syslog handler built in.

---
+++ sysutils/hs-hslogger/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:19:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslogger
+
+.if !defined(HS_HSLOGGER_BUILDLINK3_MK)
+HS_HSLOGGER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslogger+=	hs-hslogger>=1.3.1
+BUILDLINK_ABI_DEPENDS.hs-hslogger+=	hs-hslogger>=1.3.1.0
+BUILDLINK_PKGSRCDIR.hs-hslogger?=	../../sysutils/hs-hslogger
+
+.include "../../devel/hs-old-locale/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-bsd/buildlink3.mk"
+.endif	# HS_HSLOGGER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslogger
