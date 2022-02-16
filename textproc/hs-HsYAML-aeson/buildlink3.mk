5ecd9d6db91e13b583f3e53660d51d79caeb08cb
pho <pho@pkgsrc.org>
2022-02-16 10:21:09 +0000

    textproc/hs-HsYAML-aeson: import hs-HsYAML-aeson-0.2.0.1
    The YAML 1.2 format provides a much richer data-model and feature-set than
    the JavaScript Object Notation (JSON) format. However, sometimes it's
    desirable to ignore the extra capabilities and treat YAML as if it was
    merely a more convenient markup format for humans to write JSON data. To
    this end this module provides a compatibility layer atop HsYAML which
    allows decoding YAML documents in the more limited JSON data-model while
    also providing convenience by reusing aeson's FromJSON instances for
    decoding the YAML data into native Haskell data types.

---
+++ textproc/hs-HsYAML-aeson/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:21:09 pho Exp $
+
+BUILDLINK_TREE+=	hs-HsYAML-aeson
+
+.if !defined(HS_HSYAML_AESON_BUILDLINK3_MK)
+HS_HSYAML_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HsYAML-aeson+=	hs-HsYAML-aeson>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-HsYAML-aeson+=	hs-HsYAML-aeson>=0.2.0.1
+BUILDLINK_PKGSRCDIR.hs-HsYAML-aeson?=	../../textproc/hs-HsYAML-aeson
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-HsYAML/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_HSYAML_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HsYAML-aeson
