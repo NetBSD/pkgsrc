5ecd423358e0cebf91885ad004cd5f8e225634fa
pho <pho@pkgsrc.org>
2022-02-16 05:00:36 +0000

    devel/hs-constraints-extras: import hs-constraints-extras-0.3.2.1
    Convenience functions and TH for working with constraints.

---
+++ devel/hs-constraints-extras/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 05:00:36 pho Exp $
+
+BUILDLINK_TREE+=	hs-constraints-extras
+
+.if !defined(HS_CONSTRAINTS_EXTRAS_BUILDLINK3_MK)
+HS_CONSTRAINTS_EXTRAS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-constraints-extras+=	hs-constraints-extras>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-constraints-extras+=	hs-constraints-extras>=0.3.2.1
+BUILDLINK_PKGSRCDIR.hs-constraints-extras?=	../../devel/hs-constraints-extras
+
+.include "../../devel/hs-constraints/buildlink3.mk"
+.endif	# HS_CONSTRAINTS_EXTRAS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-constraints-extras
