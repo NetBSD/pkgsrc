b7d6be1c6bc1bece9951365b5c63726edd0571b4
pho <pho@pkgsrc.org>
2022-02-16 10:06:28 +0000

    devel/hs-uuid: import hs-uuid-1.3.15
    This library is useful for creating, comparing, parsing and printing
    Universally Unique Identifiers.
    
    See http://en.wikipedia.org/wiki/UUID for the general idea.

---
+++ devel/hs-uuid/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:06:28 pho Exp $
+
+BUILDLINK_TREE+=	hs-uuid
+
+.if !defined(HS_UUID_BUILDLINK3_MK)
+HS_UUID_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-uuid+=	hs-uuid>=1.3.15
+BUILDLINK_ABI_DEPENDS.hs-uuid+=	hs-uuid>=1.3.15
+BUILDLINK_PKGSRCDIR.hs-uuid?=	../../devel/hs-uuid
+
+.include "../../security/hs-cryptohash-md5/buildlink3.mk"
+.include "../../security/hs-cryptohash-sha1/buildlink3.mk"
+.include "../../security/hs-entropy/buildlink3.mk"
+.include "../../net/hs-network-info/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../devel/hs-uuid-types/buildlink3.mk"
+.endif	# HS_UUID_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-uuid
