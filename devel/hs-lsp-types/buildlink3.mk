14705b5e954bef156e714532f5712de9bcfe8a13
pho <pho@pkgsrc.org>
2022-02-16 10:04:05 +0000

    devel/hs-lsp-types: import hs-lsp-types-1.4.0.1
    An implementation of the types to allow language implementors to support
    the Language Server Protocol for their specific language.

---
+++ devel/hs-lsp-types/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:04:05 pho Exp $
+
+BUILDLINK_TREE+=	hs-lsp-types
+
+.if !defined(HS_LSP_TYPES_BUILDLINK3_MK)
+HS_LSP_TYPES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lsp-types+=	hs-lsp-types>=1.4.0
+BUILDLINK_ABI_DEPENDS.hs-lsp-types+=	hs-lsp-types>=1.4.0.1
+BUILDLINK_PKGSRCDIR.hs-lsp-types?=	../../devel/hs-lsp-types
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../textproc/hs-Diff/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../sysutils/hs-hslogger/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../math/hs-mod/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../textproc/hs-rope-utf16-splay/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-some/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_LSP_TYPES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lsp-types
