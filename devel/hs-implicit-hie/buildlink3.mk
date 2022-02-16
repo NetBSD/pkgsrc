dbe5386f4a9a14fb6ad1f07aea7b0884b52c14d2
pho <pho@pkgsrc.org>
2022-02-16 10:09:04 +0000

    devel/hs-implicit-hie: import hs-implicit-hie-0.1.2.6
    Auto generate a stack or cabal multi component hie.yaml file.

---
+++ devel/hs-implicit-hie/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:09:04 pho Exp $
+
+BUILDLINK_TREE+=	hs-implicit-hie
+
+.if !defined(HS_IMPLICIT_HIE_BUILDLINK3_MK)
+HS_IMPLICIT_HIE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2.6
+BUILDLINK_PKGSRCDIR.hs-implicit-hie?=	../../devel/hs-implicit-hie
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../sysutils/hs-filepattern/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
+.endif	# HS_IMPLICIT_HIE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-implicit-hie
