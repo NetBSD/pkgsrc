39441f726a0fa8b7e38b24025850c398a9c3c08e
pho <pho@pkgsrc.org>
2022-02-16 09:57:50 +0000

    math/hs-adjunctions: import hs-adjunctions-4.4
    This package provides adjunctions for Haskell.

---
+++ math/hs-adjunctions/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:57:50 pho Exp $
+
+BUILDLINK_TREE+=	hs-adjunctions
+
+.if !defined(HS_ADJUNCTIONS_BUILDLINK3_MK)
+HS_ADJUNCTIONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-adjunctions+=	hs-adjunctions>=4.4
+BUILDLINK_ABI_DEPENDS.hs-adjunctions+=	hs-adjunctions>=4.4
+BUILDLINK_PKGSRCDIR.hs-adjunctions?=	../../math/hs-adjunctions
+
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-contravariant/buildlink3.mk"
+.include "../../math/hs-distributive/buildlink3.mk"
+.include "../../math/hs-free/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-void/buildlink3.mk"
+.endif	# HS_ADJUNCTIONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-adjunctions
