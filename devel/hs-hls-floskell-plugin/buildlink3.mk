8a098d6dc00583c61795abe8696bc077e8e3f401
pho <pho@pkgsrc.org>
2022-02-16 10:19:54 +0000

    devel/hs-hls-floskell-plugin: import hs-hls-floskell-plugin-1.0.1.0
    Integration with the Floskell code formatter

---
+++ devel/hs-hls-floskell-plugin/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:19:54 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-floskell-plugin
+
+.if !defined(HS_HLS_FLOSKELL_PLUGIN_BUILDLINK3_MK)
+HS_HLS_FLOSKELL_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-floskell-plugin+=	hs-hls-floskell-plugin>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hls-floskell-plugin+=	hs-hls-floskell-plugin>=1.0.1.0
+BUILDLINK_PKGSRCDIR.hs-hls-floskell-plugin?=	../../devel/hs-hls-floskell-plugin
+
+.include "../../devel/floskell/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lsp-types/buildlink3.mk"
+.endif	# HS_HLS_FLOSKELL_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-floskell-plugin
