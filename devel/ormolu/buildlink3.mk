3e949a38180161e6428f927789c8731037f95fc2
pho <pho@pkgsrc.org>
2022-02-16 10:23:02 +0000

    devel/ormolu: import ormolu-0.4.0.0
    Ormolu is a formatter for Haskell source code. The project was created with
    the following goals in mind:
    
    * Using GHC's own parser to avoid parsing problems caused by
      haskell-src-exts.
    * Let some whitespace be programmable. The layout of the input influences
      the layout choices in the output. This means that the choices between
      single-line/multi-line layouts in certain situations are made by the
      user, not by an algorithm. This makes the implementation simpler and
      leaves some control to the user while still guaranteeing that the
      formatted code is stylistically consistent.
    * Writing code in such a way so it's easy to modify and maintain.
    * Implementing one "true" formatting style which admits no configuration.
    * The formatting style aims to result in minimal diffs.
    * Choose a style compatible with modern dialects of Haskell. As new Haskell
      extensions enter broad use, we may change the style to accommodate them.
    * Idempotence: formatting already formatted code doesn't change it.
    * Be well-tested and robust so that the formatter can be used in large
      projects.

---
+++ devel/ormolu/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:23:02 pho Exp $
+
+BUILDLINK_TREE+=	ormolu
+
+.if !defined(ORMOLU_BUILDLINK3_MK)
+ORMOLU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.4.0
+BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.4.0.0
+BUILDLINK_PKGSRCDIR.ormolu?=	../../devel/ormolu
+
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../textproc/hs-Diff/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-gitrev/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.endif	# ORMOLU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ormolu
