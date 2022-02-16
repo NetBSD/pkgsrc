81962af2da1cc98ade1ee252a6070e96bcb86390
pho <pho@pkgsrc.org>
2022-02-16 05:06:29 +0000

    devel/hs-dependent-sum: import hs-dependent-sum-0.7.1.0
    A dependent sum is a generalization of a particular way of thinking about
    the Either type. Either a b can be thought of as a 2-tuple (tag, value),
    where the value of the tag determines the type of the value. In particular,
    either tag = Left and value :: a or tag = Right and value :: b.
    
    This package allows you to define your own dependent sum types by using
    your own "tag" types.

---
+++ devel/hs-dependent-sum/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 05:06:29 pho Exp $
+
+BUILDLINK_TREE+=	hs-dependent-sum
+
+.if !defined(HS_DEPENDENT_SUM_BUILDLINK3_MK)
+HS_DEPENDENT_SUM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-dependent-sum+=	hs-dependent-sum>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-dependent-sum+=	hs-dependent-sum>=0.7.1.0
+BUILDLINK_PKGSRCDIR.hs-dependent-sum?=		../../devel/hs-dependent-sum
+
+.include "../../devel/hs-constraints-extras/buildlink3.mk"
+.include "../../devel/hs-some/buildlink3.mk"
+.endif	# HS_DEPENDENT_SUM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-dependent-sum
