3a6667bbd2345e460d985bf040fd0ef15aa4d69c
pho <pho@pkgsrc.org>
2022-02-16 09:51:51 +0000

    devel/hs-deferred-folds: import hs-deferred-folds-0.9.18.1
    This library provides sbstractions over deferred folds.

---
+++ devel/hs-deferred-folds/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:51:51 pho Exp $
+
+BUILDLINK_TREE+=	hs-deferred-folds
+
+.if !defined(HS_DEFERRED_FOLDS_BUILDLINK3_MK)
+HS_DEFERRED_FOLDS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-deferred-folds+=	hs-deferred-folds>=0.9.18
+BUILDLINK_ABI_DEPENDS.hs-deferred-folds+=	hs-deferred-folds>=0.9.18.1
+BUILDLINK_PKGSRCDIR.hs-deferred-folds?=		../../devel/hs-deferred-folds
+
+.include "../../devel/hs-foldl/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_DEFERRED_FOLDS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-deferred-folds
