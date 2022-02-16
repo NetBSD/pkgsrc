357664353ac3a9b15e6af128316931f41ce3b58a
pho <pho@pkgsrc.org>
2022-02-16 09:59:14 +0000

    math/hs-kan-extensions: import hs-kan-extensions-5.2.3
    This package provides tools for working with various Kan extensions and Kan
    lifts in Haskell.
    
    Among the interesting bits included are:
    * Right and left Kan extensions (Ran and Lan)
    * Right and left Kan lifts (Rift and Lift)
    * Multiple forms of the Yoneda lemma (Yoneda)
    * The Codensity monad, which can be used to improve the asymptotic
      complexity of code over free monads (Codensity, Density)
    * A "comonad to monad-transformer transformer" that is a special case of a
      right Kan lift. (CoT, Co)

---
+++ math/hs-kan-extensions/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:59:14 pho Exp $
+
+BUILDLINK_TREE+=	hs-kan-extensions
+
+.if !defined(HS_KAN_EXTENSIONS_BUILDLINK3_MK)
+HS_KAN_EXTENSIONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-kan-extensions+=	hs-kan-extensions>=5.2.3
+BUILDLINK_ABI_DEPENDS.hs-kan-extensions+=	hs-kan-extensions>=5.2.3
+BUILDLINK_PKGSRCDIR.hs-kan-extensions?=		../../math/hs-kan-extensions
+
+.include "../../math/hs-adjunctions/buildlink3.mk"
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-contravariant/buildlink3.mk"
+.include "../../math/hs-distributive/buildlink3.mk"
+.include "../../math/hs-free/buildlink3.mk"
+.include "../../math/hs-invariant/buildlink3.mk"
+.endif	# HS_KAN_EXTENSIONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-kan-extensions
