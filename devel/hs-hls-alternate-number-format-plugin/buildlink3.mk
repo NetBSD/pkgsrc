0ff78306ebb92023f81e30ad7790006969324a7e
pho <pho@pkgsrc.org>
2022-02-16 10:17:01 +0000

    devel/hs-hls-alternate-number-format-plugin: import hs-hls-alternate-number-format-plugin-1.0.1.0
    The alternate number format plugin provides alternative formatting for
    Numeric Literals in source code. These can be any numeric literal such as
    123, 0x45 or any of the other numeric formats. The plugin is context aware
    and will provide suggestions based on currently active GHC extensions.

---
+++ devel/hs-hls-alternate-number-format-plugin/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:17:01 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-alternate-number-format-plugin
+
+.if !defined(HS_HLS_ALTERNATE_NUMBER_FORMAT_PLUGIN_BUILDLINK3_MK)
+HS_HLS_ALTERNATE_NUMBER_FORMAT_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-alternate-number-format-plugin+=	hs-hls-alternate-number-format-plugin>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hls-alternate-number-format-plugin+=	hs-hls-alternate-number-format-plugin>=1.0.1.0
+BUILDLINK_PKGSRCDIR.hs-hls-alternate-number-format-plugin?=	../../devel/hs-hls-alternate-number-format-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hie-compat/buildlink3.mk"
+.include "../../devel/hs-hls-graph/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_ALTERNATE_NUMBER_FORMAT_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-alternate-number-format-plugin
