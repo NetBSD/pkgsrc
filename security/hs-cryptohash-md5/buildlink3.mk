00f91ab62b945d89496c13d14febc943e2b75dae
pho <pho@pkgsrc.org>
2022-02-16 10:05:17 +0000

    security/hs-cryptohash-md5: import hs-cryptohash-md5-0.11.101.0
    A practical incremental and one-pass, pure API to the MD5 hash algorithm
    (including HMAC support) with performance close to the fastest
    implementations available in other languages.
    
    The implementation is made in C with a haskell FFI wrapper that hides the C
    implementation.

---
+++ security/hs-cryptohash-md5/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:05:17 pho Exp $
+
+BUILDLINK_TREE+=	hs-cryptohash-md5
+
+.if !defined(HS_CRYPTOHASH_MD5_BUILDLINK3_MK)
+HS_CRYPTOHASH_MD5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-cryptohash-md5+=	hs-cryptohash-md5>=0.11.101
+BUILDLINK_ABI_DEPENDS.hs-cryptohash-md5+=	hs-cryptohash-md5>=0.11.101.0
+BUILDLINK_PKGSRCDIR.hs-cryptohash-md5?=		../../security/hs-cryptohash-md5
+.endif	# HS_CRYPTOHASH_MD5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-cryptohash-md5
