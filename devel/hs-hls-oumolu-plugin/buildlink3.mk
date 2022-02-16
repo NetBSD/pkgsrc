df0c55b368eb87a55cc75ff1204777254b05326d
pho <pho@pkgsrc.org>
2022-02-16 10:23:36 +0000

    devel/hs-hls-oumolu-plugin: import hs-hls-ormolu-plugin-1.0.2.0
    Integration with the Ormolu code formatter

---
+++ devel/hs-hls-oumolu-plugin/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:23:36 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-ormolu-plugin
+
+.if !defined(HS_HLS_ORMOLU_PLUGIN_BUILDLINK3_MK)
+HS_HLS_ORMOLU_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-ormolu-plugin+=	hs-hls-ormolu-plugin>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-hls-ormolu-plugin+=	hs-hls-ormolu-plugin>=1.0.2.0
+BUILDLINK_PKGSRCDIR.hs-hls-ormolu-plugin?=	../../devel/hs-hls-oumolu-plugin
+
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/ormolu/buildlink3.mk"
+.endif	# HS_HLS_ORMOLU_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-ormolu-plugin
