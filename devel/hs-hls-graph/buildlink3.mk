89e3363b0a63afba993043eba4e1c4173e46b481
pho <pho@pkgsrc.org>
2022-02-16 09:56:44 +0000

    devel/hs-hls-graph: import hs-hls-graph-1.6.0.0
    ghcide was originally built on top of Shake, a Haskell build
    system. Nowadays Shake has been replaced by a special purpose
    implementation of a build graph called hls-graph, which drops all the
    persistency features in exchange for simplicity and performance.

---
+++ devel/hs-hls-graph/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:56:44 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-graph
+
+.if !defined(HS_HLS_GRAPH_BUILDLINK3_MK)
+HS_HLS_GRAPH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-graph+=	hs-hls-graph>=1.6.0.0
+BUILDLINK_ABI_DEPENDS.hs-hls-graph+=	hs-hls-graph>=1.6.0.0
+BUILDLINK_PKGSRCDIR.hs-hls-graph?=	../../devel/hs-hls-graph
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-focus/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../www/hs-js-dgtable/buildlink3.mk"
+.include "../../www/hs-js-flot/buildlink3.mk"
+.include "../../www/hs-js-jquery/buildlink3.mk"
+.include "../../devel/hs-list-t/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-stm-containers/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_GRAPH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-graph
