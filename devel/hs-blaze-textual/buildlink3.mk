224ae8e164a9384f8e9981298e229a4341f6fce4
pho <pho@pkgsrc.org>
2022-02-16 09:35:58 +0000

    devel/hs-blaze-textual: import hs-blaze-textual-0.2.2.1
    blaze-textual is a fast Haskell library for rendering common Haskell
    datatypes in text form using the blaze-builder library.

---
+++ devel/hs-blaze-textual/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:35:58 pho Exp $
+
+BUILDLINK_TREE+=	hs-blaze-textual
+
+.if !defined(HS_BLAZE_TEXTUAL_BUILDLINK3_MK)
+HS_BLAZE_TEXTUAL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.2.1
+BUILDLINK_PKGSRCDIR.hs-blaze-textual?=		../../devel/hs-blaze-textual
+
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../devel/hs-old-locale/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_BLAZE_TEXTUAL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-blaze-textual
