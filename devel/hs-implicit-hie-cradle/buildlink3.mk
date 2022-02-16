5c4a7283654bf6d7f4a16f377203fcd8a1f22bba
pho <pho@pkgsrc.org>
2022-02-16 10:09:42 +0000

    devel/hs-implicit-hie-cradle: import hs-implicit-hie-cradle-0.5.0.0
    Auto generate a stack or cabal multi component cradles.

---
+++ devel/hs-implicit-hie-cradle/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:09:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-implicit-hie-cradle
+
+.if !defined(HS_IMPLICIT_HIE_CRADLE_BUILDLINK3_MK)
+HS_IMPLICIT_HIE_CRADLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-implicit-hie-cradle+=	hs-implicit-hie-cradle>=0.5.0
+BUILDLINK_ABI_DEPENDS.hs-implicit-hie-cradle+=	hs-implicit-hie-cradle>=0.5.0.0
+BUILDLINK_PKGSRCDIR.hs-implicit-hie-cradle?=	../../devel/hs-implicit-hie-cradle
+
+.include "../../converters/hs-base16-bytestring/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-hie-bios/buildlink3.mk"
+.include "../../sysutils/hs-hslogger/buildlink3.mk"
+.include "../../devel/hs-implicit-hie/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-unix-compat/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
+.endif	# HS_IMPLICIT_HIE_CRADLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-implicit-hie-cradle
