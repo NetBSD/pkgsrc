1c2db0d9c932f07a378ff84f66730840d7017994
pho <pho@pkgsrc.org>
2022-02-16 10:24:28 +0000

    devel/haskell-language-server: import haskell-language-server-1.6.1.1
    The haskell-language-server (HLS) project is an implementation of a server
    (a "language server") for the Language Server Protocol (LSP). A language
    server talks to a client (typically an editor), which can ask the server to
    perform various operations, such as reporting errors or providing code
    completions. The advantage of this system is that clients and servers can
    interoperate more easily so long as they all speak the LSP protocol. In the
    case of HLS, that means that it can be used with many different editors,
    since editor support for the LSP protocol is now widespread.

---
+++ devel/haskell-language-server/buildlink3.mk
@@ -0,0 +1,51 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:24:28 pho Exp $
+
+BUILDLINK_TREE+=	haskell-language-server
+
+.if !defined(HASKELL_LANGUAGE_SERVER_BUILDLINK3_MK)
+HASKELL_LANGUAGE_SERVER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.haskell-language-server+=	haskell-language-server>=1.6.1
+BUILDLINK_ABI_DEPENDS.haskell-language-server+=	haskell-language-server>=1.6.1.1
+BUILDLINK_PKGSRCDIR.haskell-language-server?=	../../devel/haskell-language-server
+
+.include "../../converters/hs-aeson-pretty/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../converters/hs-base16-bytestring/buildlink3.mk"
+.include "../../security/hs-cryptohash-sha1/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-githash/buildlink3.mk"
+.include "../../devel/hs-hie-bios/buildlink3.mk"
+.include "../../devel/hs-hiedb/buildlink3.mk"
+.include "../../devel/hs-hls-graph/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../sysutils/hs-hslogger/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../devel/hs-optparse-simple/buildlink3.mk"
+.include "../../devel/hs-safe-exceptions/buildlink3.mk"
+.include "../../databases/hs-sqlite-simple/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-hls-call-hierarchy-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-explicit-imports-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-refine-imports-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-module-name-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-pragmas-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-alternate-number-format-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-qualify-imported-names-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-selection-range-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-floskell-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-fourmolu-plugin/buildlink3.mk"
+.include "../../devel/hs-hls-oumolu-plugin/buildlink3.mk"
+.include "../../devel/hs-ghc-paths/buildlink3.mk"
+.include "../../devel/hs-gitrev/buildlink3.mk"
+.endif	# HASKELL_LANGUAGE_SERVER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-haskell-language-server
