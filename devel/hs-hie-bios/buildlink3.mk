c4451109e0e529e638c068d24e06eb9a7b8449e9
pho <pho@pkgsrc.org>
2022-02-16 09:21:09 +0000

    devel/hs-hie-bios: import hs-hie-bios-0.8.1
    hie-bios is the way to specify how haskell-language-server and ghcide set
    up a GHC API session.
    
    Given a Haskell project that is managed by Stack, Cabal, or other package
    tools, haskell-language-server needs to know the full set of flags to pass
    to GHC in order to build the project. These flags might contain some
    compilation options like -O2, but a lot of the time they are package
    dependencies such as -package-id directory-1.3.6.0, which also need to be
    built beforehand. hie-bios satisfies both these needs.

---
+++ devel/hs-hie-bios/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:21:09 pho Exp $
+
+BUILDLINK_TREE+=	hs-hie-bios
+
+.if !defined(HS_HIE_BIOS_BUILDLINK3_MK)
+HS_HIE_BIOS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hie-bios+=	hs-hie-bios>=0.8.1
+BUILDLINK_ABI_DEPENDS.hs-hie-bios+=	hs-hie-bios>=0.8.1
+BUILDLINK_PKGSRCDIR.hs-hie-bios?=	../../devel/hs-hie-bios
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../converters/hs-base16-bytestring/buildlink3.mk"
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-conduit-extra/buildlink3.mk"
+.include "../../security/hs-cryptohash-sha1/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-file-embed/buildlink3.mk"
+.include "../../sysutils/hs-hslogger/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-unix-compat/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.endif	# HS_HIE_BIOS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hie-bios
