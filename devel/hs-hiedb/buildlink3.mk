816bf3b1fb9f257a582f65c506316b4cff5eefda
pho <pho@pkgsrc.org>
2022-02-16 09:44:11 +0000

    devel/hs-hiedb: import hs-hiedb-0.4.1.0
    Tool and library to index and query a collection of ".hie" files.

---
+++ devel/hs-hiedb/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:44:11 pho Exp $
+
+BUILDLINK_TREE+=	hs-hiedb
+
+.if !defined(HS_HIEDB_BUILDLINK3_MK)
+HS_HIEDB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hiedb+=	hs-hiedb>=0.4.1
+BUILDLINK_ABI_DEPENDS.hs-hiedb+=	hs-hiedb>=0.4.1.0
+BUILDLINK_PKGSRCDIR.hs-hiedb?=		../../devel/hs-hiedb
+
+.include "../../math/hs-algebraic-graphs/buildlink3.mk"
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-hie-compat/buildlink3.mk"
+.include "../../textproc/hs-lucid/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../databases/hs-sqlite-simple/buildlink3.mk"
+.include "../../devel/hs-terminal-size/buildlink3.mk"
+.include "../../devel/hs-ghc-paths/buildlink3.mk"
+.endif	# HS_HIEDB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hiedb
