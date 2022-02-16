6568d4e292fa059bad79e172249707bb4b80d793
pho <pho@pkgsrc.org>
2022-02-16 10:00:52 +0000

    devel/hs-lens: import hs-lens-5.1
    This package provides families of lenses, isomorphisms, folds, traversals,
    getters and setters.

---
+++ devel/hs-lens/buildlink3.mk
@@ -0,0 +1,37 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:00:52 pho Exp $
+
+BUILDLINK_TREE+=	hs-lens
+
+.if !defined(HS_LENS_BUILDLINK3_MK)
+HS_LENS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lens+=	hs-lens>=5.1
+BUILDLINK_ABI_DEPENDS.hs-lens+=	hs-lens>=5.1
+BUILDLINK_PKGSRCDIR.hs-lens?=	../../devel/hs-lens
+
+.include "../../devel/hs-assoc/buildlink3.mk"
+.include "../../devel/hs-base-orphans/buildlink3.mk"
+.include "../../math/hs-bifunctors/buildlink3.mk"
+.include "../../devel/hs-call-stack/buildlink3.mk"
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-contravariant/buildlink3.mk"
+.include "../../math/hs-distributive/buildlink3.mk"
+.include "../../math/hs-free/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable-instances/buildlink3.mk"
+.include "../../math/hs-kan-extensions/buildlink3.mk"
+.include "../../devel/hs-parallel/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.include "../../devel/hs-reflection/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../devel/hs-strict/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-th-abstraction/buildlink3.mk"
+.include "../../devel/hs-these/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_LENS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lens
