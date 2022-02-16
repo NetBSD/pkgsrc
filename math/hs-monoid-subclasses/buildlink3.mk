e47e318ed054dd5ffa562a72a89272d6ac5cb35e
pho <pho@pkgsrc.org>
2022-02-16 05:20:48 +0000

    math/hs-monoid-subclasses: import hs-monoid-subclasses-1.1.3
    A hierarchy of subclasses of Monoid together with their instances for all
    data structures from base, containers, and text packages.

---
+++ math/hs-monoid-subclasses/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 05:20:48 pho Exp $
+
+BUILDLINK_TREE+=	hs-monoid-subclasses
+
+.if !defined(HS_MONOID_SUBCLASSES_BUILDLINK3_MK)
+HS_MONOID_SUBCLASSES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.1.3
+BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.1.3
+BUILDLINK_PKGSRCDIR.hs-monoid-subclasses?=	../../math/hs-monoid-subclasses
+
+.include "../../math/hs-primes/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_MONOID_SUBCLASSES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-monoid-subclasses
