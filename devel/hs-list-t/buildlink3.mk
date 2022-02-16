35817b73768dee2a51ad7137b43122f8d50f8267
pho <pho@pkgsrc.org>
2022-02-16 09:46:47 +0000

    devel/hs-list-t: import hs-list-t-1.0.5.1
    A correct implementation of the list monad-transformer. Useful for basic
    streaming.

---
+++ devel/hs-list-t/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 09:46:47 pho Exp $
+
+BUILDLINK_TREE+=	hs-list-t
+
+.if !defined(HS_LIST_T_BUILDLINK3_MK)
+HS_LIST_T_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-list-t+=	hs-list-t>=1.0.5
+BUILDLINK_ABI_DEPENDS.hs-list-t+=	hs-list-t>=1.0.5.1
+BUILDLINK_PKGSRCDIR.hs-list-t?=		../../devel/hs-list-t
+
+.include "../../devel/hs-foldl/buildlink3.mk"
+.include "../../devel/hs-logict/buildlink3.mk"
+.include "../../devel/hs-mmorph/buildlink3.mk"
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.endif	# HS_LIST_T_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-list-t
