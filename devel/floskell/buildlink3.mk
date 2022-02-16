c2ec60752fe4b87d63311c45c2eedee713c1329f
pho <pho@pkgsrc.org>
2022-02-16 10:19:20 +0000

    devel/floskell: import floskell-0.10.6
    Floskell started as a fork of version 4 of Chris Done's hindent. The
    formatting styles present in hindent 4 have been preserved in spirit, but
    generally will not produce exactly the same output.

---
+++ devel/floskell/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:19:20 pho Exp $
+
+BUILDLINK_TREE+=	floskell
+
+.if !defined(FLOSKELL_BUILDLINK3_MK)
+FLOSKELL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.floskell+=	floskell>=0.10.6
+BUILDLINK_ABI_DEPENDS.floskell+=	floskell>=0.10.6
+BUILDLINK_PKGSRCDIR.floskell?=		../../devel/floskell
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-haskell-src-exts/buildlink3.mk"
+.include "../../devel/hs-monad-dijkstra/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../converters/hs-aeson-pretty/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.endif	# FLOSKELL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-floskell
