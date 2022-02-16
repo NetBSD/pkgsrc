38d500c2346f65b36c42d6201140452fc6802337
pin <pin@pkgsrc.org>
2022-02-16 14:12:57 +0000

    wm/wmutils-libwm: update to 1.3
    -wm_get_atom_name(): retrieve the name of an atom
    -Add support for multi-monitor through xrandr
    -Add ATTR_D to retrieve window color depth
    -Match monitors backware in wm_find_monitor()

--- wm/wmutils-libwm/buildlink3.mk
+++ wm/wmutils-libwm/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/02/13 11:15:02 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/02/16 14:12:57 pin Exp $
 
 BUILDLINK_TREE+=	wmutils-libwm
 
 .if !defined(WMUTILS_LIBWM_BUILDLINK3_MK)
 WMUTILS_LIBWM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wmutils-libwm+=	wmutils-libwm>=1.2
+BUILDLINK_API_DEPENDS.wmutils-libwm+=	wmutils-libwm>=1.3
 BUILDLINK_PKGSRCDIR.wmutils-libwm?=	../../wm/wmutils-libwm
 
 .include "../../x11/libxcb/buildlink3.mk"
