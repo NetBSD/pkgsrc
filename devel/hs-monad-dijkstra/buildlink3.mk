edb1bbb7c076c2795d5ffc38099223f0a0dd42d7
pho <pho@pkgsrc.org>
2022-02-16 10:18:45 +0000

    devel/hs-monad-dijkstra: import hs-monad-dijkstra-0.1.1.3
    A monad transformer for weighted graph searches using Dijkstra's or A*
    algorithm.

---
+++ devel/hs-monad-dijkstra/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/16 10:18:45 pho Exp $
+
+BUILDLINK_TREE+=	hs-monad-dijkstra
+
+.if !defined(HS_MONAD_DIJKSTRA_BUILDLINK3_MK)
+HS_MONAD_DIJKSTRA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-monad-dijkstra+=	hs-monad-dijkstra>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-monad-dijkstra+=	hs-monad-dijkstra>=0.1.1.3
+BUILDLINK_PKGSRCDIR.hs-monad-dijkstra?=		../../devel/hs-monad-dijkstra
+
+.include "../../math/hs-free/buildlink3.mk"
+.include "../../devel/hs-psqueues/buildlink3.mk"
+.endif	# HS_MONAD_DIJKSTRA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-monad-dijkstra
