d5b3cf6696276d3779cacfbbdfa5edee587236d5
wiz <wiz@pkgsrc.org>
2022-02-18 22:58:05 +0000

    xscreensaver: make proper executable setuid
    The authentication part was split off into a separate program in 6.x.
    
    Bump PKGREVISION.

--- x11/xscreensaver/options.mk
+++ x11/xscreensaver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2022/02/18 15:18:23 wiz Exp $
+# $NetBSD: options.mk,v 1.15 2022/02/18 22:58:05 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xscreensaver
 PKG_SUPPORTED_OPTIONS=	pam
@@ -18,5 +18,5 @@ DEPENDS+=	pam-pwauth_suid-[0-9]*:../../security/pam-pwauth_suid
 CONFIGURE_ARGS+=	--without-pam
 # configure should figure out
 #CONFIGURE_ARGS+=	--without-shadow
-SPECIAL_PERMS+=		bin/xscreensaver ${SETUID_ROOT_PERMS}
+SPECIAL_PERMS+=		libexec/xscreensaver/xscreensaver-auth ${SETUID_ROOT_PERMS}
 .endif
