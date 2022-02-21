a54354dedfdd0c2dd1076fa018fbbb85bb307e16
adam <adam@pkgsrc.org>
2022-02-21 08:19:41 +0000

    flac: updated to 1.3.4
    FLAC 1.3.4
    
    This release mostly fixes (security related) bugs. When building with MSVC, using CMake is preferred, see the README under "Building with CMake" for more information. Building with MSVC using solution files is deprecated and these files will be removed in the future.
    
    General:
    * Fix 12 decoder bugs found by oss-fuzz, including CVE-2020-0499 (erikd, Martijn van Beurden)
      - Fix encoder bug CVE-2021-0561 (NeelkamalSemwal)
      - Integrate oss-fuzzers (erikd, Guido Vranken)
      - Seeking fixes (NeelkamalSemwal, Robert Kausch)
      - Various fixes and improvements (Andrei Astafev, Rosen Penev, Håkan Kvist, oreo639, erikd, Tamás Zahola, Ulrik Mikaelsson, Tyler Dunn, tmkk)
    * FLAC format:
      - (none)
    * Ogg FLAC format:
      - (none)
    * flac:
      - Various fixes and improvements (Andrei Astafev, Martijn van Beurden)
    * metaflac:
      - (none)
    * build system:
      - CMake improvements (evpobr, Vitaliy Kirsanov, erikd, Ozkan Sezer, Tyler Dunn, tg-m DeadSix27, ericLemanissier, Chocobo1).
      - Fixes for MinGW and MSVC (Ozkan Sezer).
      - Fix for clang (Ozkan Sezer)
      - Fix for PowerPC (Peter Seiderer, Thomas BERNARD)
      - Fix for FreeBSD PowerPC (pkubaj).
    * testing/validation:
      - Add Windows target to CI, improve logging (Ralph Giles)
      - CI improvements (Ralph Giles, Ewout ter Hoeven)
    * documentation:
      - Doxygen fixes (Tyler Dunn)
      - Fix typos (Tim Gates, maxz)
    *  Interface changes:
      - libFLAC:
        (none)
      - libFLAC++:
        (none)

--- audio/flac/patches/patch-src_libFLAC_cpu.c
+++ audio/flac/patches/patch-src_libFLAC_cpu.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_libFLAC_cpu.c,v 1.1 2019/12/17 13:59:50 nros Exp $
+$NetBSD: patch-src_libFLAC_cpu.c,v 1.2 2022/02/21 08:19:41 adam Exp $
 
-* check for getauxval and sys/auxv.h since it is not available on all systems
+Check for getauxval and sys/auxv.h since it is not available on all systems
 
---- src/libFLAC/cpu.c.orig	2018-08-20 08:17:21.000000000 +0000
+--- src/libFLAC/cpu.c.orig	2021-07-09 20:24:42.000000000 +0000
 +++ src/libFLAC/cpu.c
 @@ -53,7 +53,7 @@
  #define dfprintf(file, format, ...)
@@ -10,10 +10,10 @@ $NetBSD: patch-src_libFLAC_cpu.c,v 1.1 2019/12/17 13:59:50 nros Exp $
  
 -#if defined FLAC__CPU_PPC
 +#if (defined(FLAC__CPU_PPC) && defined(HAVE_SYS_AUXV_H))
+ #if defined(__linux__) || (defined(__FreeBSD__) && (__FreeBSD__ >= 12))
  #include <sys/auxv.h>
  #endif
- 
-@@ -236,7 +236,7 @@ x86_cpu_info (FLAC__CPUInfo *info)
+@@ -238,7 +238,7 @@ x86_cpu_info (FLAC__CPUInfo *info)
  static void
  ppc_cpu_info (FLAC__CPUInfo *info)
  {
