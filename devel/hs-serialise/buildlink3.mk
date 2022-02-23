6caea98207407ac00b2e1afe0e9d1cd10434237a
pho <pho@pkgsrc.org>
2022-02-23 16:19:03 +0000

    devel/hs-serialise: import hs-serialise-0.2.4.0
    This package (formerly binary-serialise-cbor) provides pure, efficient
    serialization of Haskell values directly into ByteStrings for storage or
    transmission purposes. By providing a set of type class instances, you can
    also serialise any custom data type you have as well.
    
    The underlying binary format used is the 'Concise Binary Object
    Representation', or CBOR, specified in RFC 7049. As a result, serialised
    Haskell values have implicit structure outside of the Haskell program
    itself, meaning they can be inspected or analyzed without custom tools.

---
+++ devel/hs-serialise/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:19:03 pho Exp $
+
+BUILDLINK_TREE+=	hs-serialise
+
+.if !defined(HS_SERIALISE_BUILDLINK3_MK)
+HS_SERIALISE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-serialise+=	hs-serialise>=0.2.4
+BUILDLINK_ABI_DEPENDS.hs-serialise+=	hs-serialise>=0.2.4.0
+BUILDLINK_PKGSRCDIR.hs-serialise?=	../../devel/hs-serialise
+
+.include "../../devel/hs-cborg/buildlink3.mk"
+.include "../../devel/hs-half/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-strict/buildlink3.mk"
+.include "../../devel/hs-these/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_SERIALISE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-serialise
