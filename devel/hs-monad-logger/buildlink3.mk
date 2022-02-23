e4f615baf80c022c290a2c54b965ac3cbc0c0fbd
pho <pho@pkgsrc.org>
2022-02-23 16:16:33 +0000

    devel/hs-monad-logger: import hs-monad-logger-0.3.36
    A monad transformer approach for logging.
    
    This package provides Template Haskell functions for determining source
    code locations of messages.

---
+++ devel/hs-monad-logger/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:16:33 pho Exp $
+
+BUILDLINK_TREE+=	hs-monad-logger
+
+.if !defined(HS_MONAD_LOGGER_BUILDLINK3_MK)
+HS_MONAD_LOGGER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-monad-logger+=	hs-monad-logger>=0.3.36
+BUILDLINK_ABI_DEPENDS.hs-monad-logger+=	hs-monad-logger>=0.3.36
+BUILDLINK_PKGSRCDIR.hs-monad-logger?=	../../devel/hs-monad-logger
+
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-conduit-extra/buildlink3.mk"
+.include "../../sysutils/hs-fast-logger/buildlink3.mk"
+.include "../../devel/hs-lifted-base/buildlink3.mk"
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../devel/hs-monad-loops/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../devel/hs-stm-chans/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.endif	# HS_MONAD_LOGGER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-monad-logger
