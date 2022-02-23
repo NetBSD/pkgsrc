6e3dd3dcd1eb7098f453725d45ece262e945ae40
pho <pho@pkgsrc.org>
2022-02-23 16:21:40 +0000

    www/hs-wai-websockets: import hs-wai-websockets-3.0.1.2
    Use websockets with WAI applications, primarily those hosted via Warp.

---
+++ www/hs-wai-websockets/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:21:40 pho Exp $
+
+BUILDLINK_TREE+=	hs-wai-websockets
+
+.if !defined(HS_WAI_WEBSOCKETS_BUILDLINK3_MK)
+HS_WAI_WEBSOCKETS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-wai-websockets+=	hs-wai-websockets>=3.0.1
+BUILDLINK_ABI_DEPENDS.hs-wai-websockets+=	hs-wai-websockets>=3.0.1.2
+BUILDLINK_PKGSRCDIR.hs-wai-websockets?=		../../www/hs-wai-websockets
+
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../www/hs-wai/buildlink3.mk"
+.include "../../www/hs-websockets/buildlink3.mk"
+.endif	# HS_WAI_WEBSOCKETS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-wai-websockets
