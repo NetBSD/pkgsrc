59fc97c7049691ad5c832ad26aadbb92c4155bfc
pho <pho@pkgsrc.org>
2022-02-23 16:18:22 +0000

    devel/hs-protolude: import hs-protolude-0.3.0
    A sensible starting Prelude for building custom Preludes.
    
    Design points:
    
    * Banishes String.
    * Banishes partial functions.
    * Compiler warning on bottoms.
    * Polymorphic string IO functions.
    * Polymorphic show.
    * Automatic string conversions.
    * Types for common data structures in scope.
    * Types for all common string types (Text/ByteString) in scope.
    * Banishes impure exception throwing outside of IO.
    * StateT/ReaderT/ExceptT transformers in scope by default.
    * Foldable / Traversable functions in scope by default.
    * Unsafe functions are prefixed with "unsafe" in separate module.
    * Compiler agnostic, GHC internal modules are abstracted out into Base.
    * sum and product are strict by default.
    * Includes Semiring for GHC >= 7.6.
    * Includes Bifunctor for GHC >= 7.6.
    * Includes Semigroup for GHC >= 7.6.

---
+++ devel/hs-protolude/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:18:22 pho Exp $
+
+BUILDLINK_TREE+=	hs-protolude
+
+.if !defined(HS_PROTOLUDE_BUILDLINK3_MK)
+HS_PROTOLUDE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-protolude+=	hs-protolude>=0.3.0
+BUILDLINK_ABI_DEPENDS.hs-protolude+=	hs-protolude>=0.3.0
+BUILDLINK_PKGSRCDIR.hs-protolude?=	../../devel/hs-protolude
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-mtl-compat/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.endif	# HS_PROTOLUDE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-protolude
