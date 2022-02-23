31a91a4ad18755c321224b06e53a59dd69e31c8e
bsiegert <bsiegert@pkgsrc.org>
2022-02-23 09:36:11 +0000

    Pullup ticket #6585 - requested by nia databases/mariadb106-client, databases/mariadb106-server: security fix
    Revisions pulled up:
    - databases/mariadb106-client/Makefile                          1.6
    - databases/mariadb106-client/Makefile.common                   1.7
    - databases/mariadb106-client/PLIST                             1.3
    - databases/mariadb106-client/distinfo                          1.6
    - databases/mariadb106-client/patches/patch-storage_innobase_include_transactional__lock__guard.h 1.2
    - databases/mariadb106-server/Makefile                          1.12
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Sat Feb 19 09:13:21 UTC 2022
    
       Modified Files:
       	pkgsrc/databases/mariadb106-client: Makefile Makefile.common PLIST
       	    distinfo
       	pkgsrc/databases/mariadb106-client/patches:
       	    patch-storage_innobase_include_transactional__lock__guard.h
       	pkgsrc/databases/mariadb106-server: Makefile
    
       Log Message:
       mariadb106: update to 10.6.7
    
                                 MariaDB 10.6.7 Release Notes
    
         InnoDB
    
            * Set innodb_change_buffering=none by default (MDEV-27734)
    
         Security
    
            * Fixes for the following security vulnerabilities:
                 * CVE-2021-46665
                 * CVE-2021-46664
                 * CVE-2021-46661
                 * CVE-2021-46668
                 * CVE-2021-46663
    
                                 MariaDB 10.6.6 Release Notes
    
       Notable Items
    
         InnoDB
    
            * --skip-symbolic-links does not disallow .isl file creation
              (MDEV-26870)
            * Indexed CHAR columns are broken with NO_PAD collations (MDEV-25440)
            * insert-intention lock conflicts with waiting ORDINARY lock
              (MDEV-27025)
            * Crash recovery improvements (MDEV-26784, MDEV-27022, MDEV-27183,
              MDEV-27610)
            * mariabackup skips valid .ibd file (MDEV-26326)
            * Allow seamless upgrade despite ROW_FORMAT=COMPRESSED (MDEV-27736)
    
         Galera
    
            * Galera updated to 26.4.11
            * Galera SST scripts should use ssl_capath (not ssl_ca) for CA directory
              (MDEV-27181)
            * Alter Sequence do not replicate to another nodes with in Galera
              Cluster (MDEV-19353)
            * Galera crash - Assertion. Possible parallel writeset problem
              (MDEV-26803)
            * CREATE TABLE with FOREIGN KEY constraint fails to apply in parallel
              (MDEV-27276)
            * Galera cluster node consider old server_id value even after
              modification of server_id [wsrep_gtid_mode=ON] (MDEV-26223)
    
         Replication
    
            * Seconds behind master corrected from artificial spikes at relay-log
              rotation (MDEV-16091)
            * Statement rollback in binlog when transaction creates or drop
              temporary table is set right (MDEV-26833)
            * CREATE-or-REPLACE SEQUENCE is made to binlog with the DDL flag to
              stabilize its parallel execution on slave (MDEV-27365)
    
         Security
    
            * Fixes for the following security vulnerabilities:
                 * CVE-2022-24052
                 * CVE-2022-24051
                 * CVE-2022-24050
                 * CVE-2022-24048
                 * CVE-2021-46659

--- databases/mariadb106-client/patches/patch-storage_innobase_include_transactional__lock__guard.h
+++ databases/mariadb106-client/patches/patch-storage_innobase_include_transactional__lock__guard.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-storage_innobase_include_transactional__lock__guard.h,v 1.1 2021/11/27 23:40:28 nia Exp $
+$NetBSD: patch-storage_innobase_include_transactional__lock__guard.h,v 1.1.4.1 2022/02/23 09:36:11 bsiegert Exp $
 
 Strange build failures on NetBSD:
 error: 'rw_lock' is not an accessible base of 'page_hash_latch'
 
---- storage/innobase/include/transactional_lock_guard.h.orig	2021-11-05 20:03:31.000000000 +0000
+--- storage/innobase/include/transactional_lock_guard.h.orig	2022-02-10 20:17:09.000000000 +0000
 +++ storage/innobase/include/transactional_lock_guard.h
 @@ -21,12 +21,6 @@ this program; if not, write to the Free 
  #if defined __powerpc64__ && defined __clang__ && defined __linux__
  #elif defined __powerpc64__&&defined __GNUC__&&defined __linux__&&__GNUC__ > 4
- #elif defined _MSC_VER && (defined _M_IX86 || defined _M_X64)
+ #elif defined _MSC_VER && (defined _M_IX86 || defined _M_X64) && !defined(__clang__)
 -#elif defined __GNUC__ && (defined __i386__ || defined __x86_64__)
 -# if __GNUC__ >= 8
 -# elif defined __clang_major__ && __clang_major__ > 6
