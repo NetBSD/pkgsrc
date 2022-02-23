6f776829dc3879edb4713a8ed7e815f76dff14e8
pho <pho@pkgsrc.org>
2022-02-23 16:11:00 +0000

    devel/hs-half: import hs-half-0.3.1
    This package supplies half-precision floating point values w/ 1 bit of
    sign, 5 bits of exponent, 11 bits of mantissa trailing a leading 1 bit with
    proper underflow.
    
    These arise commonly in GPU applications.

---
+++ devel/hs-half/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:11:00 pho Exp $
+
+BUILDLINK_TREE+=	hs-half
+
+.if !defined(HS_HALF_BUILDLINK3_MK)
+HS_HALF_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-half+=	hs-half>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-half+=	hs-half>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-half?=	../../devel/hs-half
+.endif	# HS_HALF_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-half
