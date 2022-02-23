4771799450159b577299472a93c0862547923d9c
pho <pho@pkgsrc.org>
2022-02-23 16:17:07 +0000

    devel/hs-pattern-arrows: import hs-pattern-arrows-0.0.2
    A library for generating concise pretty printers based on precedence rules.

---
+++ devel/hs-pattern-arrows/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:17:07 pho Exp $
+
+BUILDLINK_TREE+=	hs-pattern-arrows
+
+.if !defined(HS_PATTERN_ARROWS_BUILDLINK3_MK)
+HS_PATTERN_ARROWS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pattern-arrows+=	hs-pattern-arrows>=0.0.2
+BUILDLINK_ABI_DEPENDS.hs-pattern-arrows+=	hs-pattern-arrows>=0.0.2
+BUILDLINK_PKGSRCDIR.hs-pattern-arrows?=		../../devel/hs-pattern-arrows
+.endif	# HS_PATTERN_ARROWS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pattern-arrows
