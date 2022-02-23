1f2d1b2d1fd8aeeda8f84a50eea5fe1191e7feb3
pho <pho@pkgsrc.org>
2022-02-23 16:55:50 +0000

    devel/hs-repline: import hs-repline-0.4.0.0
    Haskeline wrapper for GHCi-like REPL interfaces. Composable with normal mtl
    transformers.

---
+++ devel/hs-repline/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:55:50 pho Exp $
+
+BUILDLINK_TREE+=	hs-repline
+
+.if !defined(HS_REPLINE_BUILDLINK3_MK)
+HS_REPLINE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-repline+=	hs-repline>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-repline+=	hs-repline>=0.4.0.0
+BUILDLINK_PKGSRCDIR.hs-repline?=	../../devel/hs-repline
+.endif	# HS_REPLINE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-repline
