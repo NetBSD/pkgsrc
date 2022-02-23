c9e5492e8bd71226453d45f99c2181aed7ad4e9a
pho <pho@pkgsrc.org>
2022-02-23 16:19:45 +0000

    lang/hs-purescript-cst: import hs-purescript-cst-0.4.0.0
    The parser for the PureScript programming language.

---
+++ lang/hs-purescript-cst/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:19:45 pho Exp $
+
+BUILDLINK_TREE+=	hs-purescript-cst
+
+.if !defined(HS_PURESCRIPT_CST_BUILDLINK3_MK)
+HS_PURESCRIPT_CST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-purescript-cst+=	hs-purescript-cst>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-purescript-cst+=	hs-purescript-cst>=0.4.0.0
+BUILDLINK_PKGSRCDIR.hs-purescript-cst?=		../../lang/hs-purescript-cst
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-microlens/buildlink3.mk"
+.include "../../devel/hs-protolude/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-serialise/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_PURESCRIPT_CST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-purescript-cst
