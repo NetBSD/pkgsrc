bfcd3e3f27dedd8824fffe5d68397732615bca63
pho <pho@pkgsrc.org>
2022-02-23 16:21:08 +0000

    www/hs-websockets: import hs-websockets-0.12.7.3
    This library allows you to write WebSocket-capable servers.
    
    An example server:
    https://github.com/jaspervdj/websockets/blob/master/example/server.lhs
    
    An example client:
    https://github.com/jaspervdj/websockets/blob/master/example/client.hs
    
    See also:
    * The specification of the WebSocket protocol:
      http://www.whatwg.org/specs/web-socket-protocol/
    * The JavaScript API for dealing with WebSockets:
      http://www.w3.org/TR/websockets/

---
+++ www/hs-websockets/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:21:08 pho Exp $
+
+BUILDLINK_TREE+=	hs-websockets
+
+.if !defined(HS_WEBSOCKETS_BUILDLINK3_MK)
+HS_WEBSOCKETS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-websockets+=	hs-websockets>=0.12.7
+BUILDLINK_ABI_DEPENDS.hs-websockets+=	hs-websockets>=0.12.7.3
+BUILDLINK_PKGSRCDIR.hs-websockets?=	../../www/hs-websockets
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
+.include "../../devel/hs-bytestring-builder/buildlink3.mk"
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../time/hs-clock/buildlink3.mk"
+.include "../../security/hs-entropy/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../security/hs-SHA/buildlink3.mk"
+.include "../../devel/hs-streaming-commons/buildlink3.mk"
+.endif	# HS_WEBSOCKETS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-websockets
