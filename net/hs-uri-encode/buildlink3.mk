cff39938ebd8160535f95bc4645275c4821d7fa4
pho <pho@pkgsrc.org>
2022-02-23 16:56:57 +0000

    net/hs-uri-encode: import hs-uri-encode-1.5.0.7
    This package allows you to uri encode and uri decode Strings, Texts and
    ByteString values.

---
+++ net/hs-uri-encode/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:56:57 pho Exp $
+
+BUILDLINK_TREE+=	hs-uri-encode
+
+.if !defined(HS_URI_ENCODE_BUILDLINK3_MK)
+HS_URI_ENCODE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-uri-encode+=	hs-uri-encode>=1.5.0
+BUILDLINK_ABI_DEPENDS.hs-uri-encode+=	hs-uri-encode>=1.5.0.7
+BUILDLINK_PKGSRCDIR.hs-uri-encode?=	../../net/hs-uri-encode
+
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.endif	# HS_URI_ENCODE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-uri-encode
