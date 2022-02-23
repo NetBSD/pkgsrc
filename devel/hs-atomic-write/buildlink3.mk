0e66caab7d3d46219f7e5619620954db837443fe
pho <pho@pkgsrc.org>
2022-02-23 16:52:15 +0000

    devel/hs-atomic-write: import hs-atomic-write-0.2.0.7
    Atomically write to a file on POSIX-compliant systems while preserving
    permissions.
    
    On most Unix systems, mv is an atomic operation. This makes it simple to
    write to a file atomically just by using the mv operation. However, this
    will destroy the permissions on the original file. This library does the
    following to preserve permissions while atomically writing to a file:
    
    * If an original file exists, take those permissions and apply them to the
      temp file before mving the file into place.
    
    * If the original file does not exist, create a following with default
      permissions (based on the currently-active umask).
    
    This way, when the file is mv'ed into place, the permissions will be the
    ones held by the original file.

---
+++ devel/hs-atomic-write/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:52:15 pho Exp $
+
+BUILDLINK_TREE+=	hs-atomic-write
+
+.if !defined(HS_ATOMIC_WRITE_BUILDLINK3_MK)
+HS_ATOMIC_WRITE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-atomic-write+=	hs-atomic-write>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-atomic-write+=	hs-atomic-write>=0.2.0.7
+BUILDLINK_PKGSRCDIR.hs-atomic-write?=	../../devel/hs-atomic-write
+
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-unix-compat/buildlink3.mk"
+.endif	# HS_ATOMIC_WRITE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-atomic-write
