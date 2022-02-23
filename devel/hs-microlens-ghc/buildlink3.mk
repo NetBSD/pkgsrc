e8e67b91c46ae1d4bf4e464151d9297fbe55b205
pho <pho@pkgsrc.org>
2022-02-23 16:13:23 +0000

    devel/hs-microlens-ghc: import hs-microlens-ghc-0.4.13.1
    Use this package instead of microlens if you don't mind depending on all
    dependencies here -- Lens.Micro.GHC reexports everything from Lens.Micro
    and additionally provides orphan instances of microlens classes for
    packages coming with GHC (array, bytestring, containers, transformers).

---
+++ devel/hs-microlens-ghc/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:13:23 pho Exp $
+
+BUILDLINK_TREE+=	hs-microlens-ghc
+
+.if !defined(HS_MICROLENS_GHC_BUILDLINK3_MK)
+HS_MICROLENS_GHC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-microlens-ghc+=	hs-microlens-ghc>=0.4.13
+BUILDLINK_ABI_DEPENDS.hs-microlens-ghc+=	hs-microlens-ghc>=0.4.13.1
+BUILDLINK_PKGSRCDIR.hs-microlens-ghc?=		../../devel/hs-microlens-ghc
+
+.include "../../devel/hs-microlens/buildlink3.mk"
+.endif	# HS_MICROLENS_GHC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-microlens-ghc
