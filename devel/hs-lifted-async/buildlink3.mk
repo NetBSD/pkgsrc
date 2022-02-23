bf597194fa387f55981beec8ea8fb98ceb86b594
pho <pho@pkgsrc.org>
2022-02-23 16:12:47 +0000

    devel/hs-lifted-async: import hs-lifted-async-0.10.2.2
    This package provides IO operations from async package lifted to any
    instance of MonadBase or MonadBaseControl from monad-control package.

---
+++ devel/hs-lifted-async/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 16:12:47 pho Exp $
+
+BUILDLINK_TREE+=	hs-lifted-async
+
+.if !defined(HS_LIFTED_ASYNC_BUILDLINK3_MK)
+HS_LIFTED_ASYNC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lifted-async+=	hs-lifted-async>=0.10.2
+BUILDLINK_ABI_DEPENDS.hs-lifted-async+=	hs-lifted-async>=0.10.2.2
+BUILDLINK_PKGSRCDIR.hs-lifted-async?=	../../devel/hs-lifted-async
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-lifted-base/buildlink3.mk"
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../devel/hs-constraints/buildlink3.mk"
+.endif	# HS_LIFTED_ASYNC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lifted-async
