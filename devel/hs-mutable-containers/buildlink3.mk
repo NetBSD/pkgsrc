feee925f0f3fa73d23da9c6ed89d9a7278968b07
pho <pho@pkgsrc.org>
2022-02-24 01:15:33 +0000

    devel/hs-mutable-containers: import hs-mutable-containers-0.3.4
    One of Haskell's strengths is immutable data structures. These structures
    make it easier to reason about code, simplify concurrency and parallelism,
    and in some cases can improve performance by allowing sharing. However,
    there are still classes of problems where mutable data structures can both
    be more convenient, and provide a performance boost. This library is meant
    to provide such structures in a performant, well tested way. It also
    provides a simple abstraction over such data structures via typeclasses.

---
+++ devel/hs-mutable-containers/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:15:33 pho Exp $
+
+BUILDLINK_TREE+=	hs-mutable-containers
+
+.if !defined(HS_MUTABLE_CONTAINERS_BUILDLINK3_MK)
+HS_MUTABLE_CONTAINERS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-mutable-containers+=	hs-mutable-containers>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-mutable-containers+=	hs-mutable-containers>=0.3.4
+BUILDLINK_PKGSRCDIR.hs-mutable-containers?=	../../devel/hs-mutable-containers
+
+.include "../../devel/hs-mono-traversable/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_MUTABLE_CONTAINERS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-mutable-containers
