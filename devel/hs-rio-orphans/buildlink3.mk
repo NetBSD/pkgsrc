8b7c3cd6843ad76e9b529043b029a54db9677ee7
pho <pho@pkgsrc.org>
2022-02-24 01:01:05 +0000

    devel/hs-rio-orphans: import hs-rio-orphans-0.1.2.0
    Provides orphan instances for the RIO data type. Currently supports:
    
    * MonadBase from transformers-base
    * MonadBaseControl from monad-control
    * MonadCatch and MonadMask from exceptions
    * MonadLogger from monad-logger
    * MonadResource from resourcet

---
+++ devel/hs-rio-orphans/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:01:05 pho Exp $
+
+BUILDLINK_TREE+=	hs-rio-orphans
+
+.if !defined(HS_RIO_ORPHANS_BUILDLINK3_MK)
+HS_RIO_ORPHANS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-rio-orphans+=	hs-rio-orphans>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-rio-orphans+=	hs-rio-orphans>=0.1.2.0
+BUILDLINK_PKGSRCDIR.hs-rio-orphans?=	../../devel/hs-rio-orphans
+
+.include "../../sysutils/hs-fast-logger/buildlink3.mk"
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../devel/hs-monad-logger/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../devel/hs-rio/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.endif	# HS_RIO_ORPHANS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-rio-orphans
