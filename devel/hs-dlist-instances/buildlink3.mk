9072bb083d6edfaff16b5444491d18fffbd5337c
pho <pho@pkgsrc.org>
2022-02-24 01:10:18 +0000

    devel/hs-dlist-instances: import hs-dlist-instances-0.1.1.1
    See the dlist packages. This package is the canonical source for some
    orphan instances. Orphan instances are placed here to avoid dependencies
    elsewhere.

---
+++ devel/hs-dlist-instances/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:10:18 pho Exp $
+
+BUILDLINK_TREE+=	hs-dlist-instances
+
+.if !defined(HS_DLIST_INSTANCES_BUILDLINK3_MK)
+HS_DLIST_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-dlist-instances+=	hs-dlist-instances>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-dlist-instances+=	hs-dlist-instances>=0.1.1.1
+BUILDLINK_PKGSRCDIR.hs-dlist-instances?=	../../devel/hs-dlist-instances
+
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.endif	# HS_DLIST_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-dlist-instances
