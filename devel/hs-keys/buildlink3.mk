65b9d2d36a2583e0cf9ad6f123e56d1f5920ea16
pho <pho@pkgsrc.org>
2022-02-24 01:11:07 +0000

    devel/hs-keys: import hs-keys-3.12.3
    This package provides a bunch of ad hoc classes for accessing parts of a
    container.
    
    In practice this package is largely subsumed by the lens package, but it is
    maintained for now as it has much simpler dependencies.

---
+++ devel/hs-keys/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:11:07 pho Exp $
+
+BUILDLINK_TREE+=	hs-keys
+
+.if !defined(HS_KEYS_BUILDLINK3_MK)
+HS_KEYS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-keys+=	hs-keys>=3.12.3
+BUILDLINK_ABI_DEPENDS.hs-keys+=	hs-keys>=3.12.3
+BUILDLINK_PKGSRCDIR.hs-keys?=	../../devel/hs-keys
+
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-free/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_KEYS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-keys
