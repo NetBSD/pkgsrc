5e7d6ac17aa41c094674d18e58bcc5d08dc4dcfe
pho <pho@pkgsrc.org>
2022-02-24 01:00:24 +0000

    devel/hs-rio: import hs-rio-0.1.21.0
    The goal of the rio library is to make it easier to adopt Haskell for
    writing production software. It is intended as a cross between:
    
    * Collection of well designed, trusted libraries
    * Useful Prelude replacement
    * A set of best practices for writing production quality Haskell code

---
+++ devel/hs-rio/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:00:24 pho Exp $
+
+BUILDLINK_TREE+=	hs-rio
+
+.if !defined(HS_RIO_BUILDLINK3_MK)
+HS_RIO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-rio+=	hs-rio>=0.1.21
+BUILDLINK_ABI_DEPENDS.hs-rio+=	hs-rio>=0.1.21.0
+BUILDLINK_PKGSRCDIR.hs-rio?=	../../devel/hs-rio
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-microlens/buildlink3.mk"
+.include "../../devel/hs-microlens-mtl/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../sysutils/hs-typed-process/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_RIO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-rio
