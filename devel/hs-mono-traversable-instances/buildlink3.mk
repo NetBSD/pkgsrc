89dbcf7656d864e84178f433a3716d3b3588b83b
pho <pho@pkgsrc.org>
2022-02-24 01:13:05 +0000

    devel/hs-mono-traversable-instances: import hs-mono-traversable-instances-0.1.1.0
    Extra typeclass instances for mono-traversable.
    
    Set up this way to allow mono-traversable itself to have minimal
    dependencies.

---
+++ devel/hs-mono-traversable-instances/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:13:05 pho Exp $
+
+BUILDLINK_TREE+=	hs-mono-traversable-instances
+
+.if !defined(HS_MONO_TRAVERSABLE_INSTANCES_BUILDLINK3_MK)
+HS_MONO_TRAVERSABLE_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-mono-traversable-instances+=	hs-mono-traversable-instances>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-mono-traversable-instances+=	hs-mono-traversable-instances>=0.1.1.0
+BUILDLINK_PKGSRCDIR.hs-mono-traversable-instances?=	../../devel/hs-mono-traversable-instances
+
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-dlist-instances/buildlink3.mk"
+.include "../../devel/hs-mono-traversable/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-vector-instances/buildlink3.mk"
+.endif	# HS_MONO_TRAVERSABLE_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-mono-traversable-instances
