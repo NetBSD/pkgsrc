2b501394e17684d492e3d1d8b9d195f65f9184e5
pho <pho@pkgsrc.org>
2022-02-24 00:59:04 +0000

    devel/hs-generic-lens: import hs-generic-lens-2.2.1.0
    This library uses GHC.Generics to derive efficient optics (traversals,
    lenses and prisms) for algebraic data types in a type-directed way, with a
    focus on good type inference and error messages when possible.

---
+++ devel/hs-generic-lens/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 00:59:04 pho Exp $
+
+BUILDLINK_TREE+=	hs-generic-lens
+
+.if !defined(HS_GENERIC_LENS_BUILDLINK3_MK)
+HS_GENERIC_LENS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-generic-lens+=	hs-generic-lens>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-generic-lens+=	hs-generic-lens>=2.2.1.0
+BUILDLINK_PKGSRCDIR.hs-generic-lens?=	../../devel/hs-generic-lens
+
+.include "../../devel/hs-generic-lens-core/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.endif	# HS_GENERIC_LENS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-generic-lens
