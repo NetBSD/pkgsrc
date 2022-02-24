3e989409c4f7e12801fae836ba585a7549559997
pho <pho@pkgsrc.org>
2022-02-24 01:11:41 +0000

    devel/hs-pointed: import hs-pointed-5.0.3
    Haskell 98 pointed and copointed data types.

---
+++ devel/hs-pointed/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:11:41 pho Exp $
+
+BUILDLINK_TREE+=	hs-pointed
+
+.if !defined(HS_POINTED_BUILDLINK3_MK)
+HS_POINTED_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pointed+=	hs-pointed>=5.0.3
+BUILDLINK_ABI_DEPENDS.hs-pointed+=	hs-pointed>=5.0.3
+BUILDLINK_PKGSRCDIR.hs-pointed?=	../../devel/hs-pointed
+
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../math/hs-comonad/buildlink3.mk"
+.include "../../math/hs-kan-extensions/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_POINTED_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pointed
