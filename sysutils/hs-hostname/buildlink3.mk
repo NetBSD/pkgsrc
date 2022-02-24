fe62ba21cd79cf1880dd72614e9fcb1b38c256f3
pho <pho@pkgsrc.org>
2022-02-24 01:18:23 +0000

    sysutils/hs-hostname: import hs-hostname-1.0
    A very simple package providing a cross-platform means of determining the
    hostname.

---
+++ sysutils/hs-hostname/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:18:23 pho Exp $
+
+BUILDLINK_TREE+=	hs-hostname
+
+.if !defined(HS_HOSTNAME_BUILDLINK3_MK)
+HS_HOSTNAME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hostname+=	hs-hostname>=1.0
+BUILDLINK_ABI_DEPENDS.hs-hostname+=	hs-hostname>=1.0
+BUILDLINK_PKGSRCDIR.hs-hostname?=	../../sysutils/hs-hostname
+.endif	# HS_HOSTNAME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hostname
