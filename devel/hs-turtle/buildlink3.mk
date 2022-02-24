e149f13f8b336031f6cae040feecacd149771464
pho <pho@pkgsrc.org>
2022-02-24 01:22:47 +0000

    devel/hs-turtle: import hs-turtle-1.5.24
    turtle is a reimplementation of the Unix command line environment in
    Haskell so that you can use Haskell as both a shell and a scripting
    language.
    
    Features include:
    * Batteries included: Command an extended suite of predefined utilities
    * Interoperability: You can still run external shell commands
    * Portability: Works on Windows, OS X, and Linux
    * Exception safety: Safely acquire and release resources
    * Streaming: Transform or fold command output in constant space
    * Patterns: Use typed regular expressions that can parse structured values
    * Formatting: Type-safe printf-style text formatting
    * Modern: Supports text and system-filepath

---
+++ devel/hs-turtle/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:22:47 pho Exp $
+
+BUILDLINK_TREE+=	hs-turtle
+
+.if !defined(HS_TURTLE_BUILDLINK3_MK)
+HS_TURTLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-turtle+=	hs-turtle>=1.5.24
+BUILDLINK_ABI_DEPENDS.hs-turtle+=	hs-turtle>=1.5.24
+BUILDLINK_PKGSRCDIR.hs-turtle?=		../../devel/hs-turtle
+
+.include "../../textproc/hs-ansi-wl-pprint/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../time/hs-clock/buildlink3.mk"
+.include "../../devel/hs-foldl/buildlink3.mk"
+.include "../../sysutils/hs-hostname/buildlink3.mk"
+.include "../../devel/hs-managed/buildlink3.mk"
+.include "../../devel/hs-optional-args/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../devel/hs-streaming-commons/buildlink3.mk"
+.include "../../sysutils/hs-system-fileio/buildlink3.mk"
+.include "../../sysutils/hs-system-filepath/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-unix-compat/buildlink3.mk"
+.endif	# HS_TURTLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-turtle
