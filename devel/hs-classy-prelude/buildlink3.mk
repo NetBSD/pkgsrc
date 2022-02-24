4e368ec54534b1738402834656bb686cef2153c9
pho <pho@pkgsrc.org>
2022-02-24 01:17:02 +0000

    devel/hs-classy-prelude: import hs-classy-prelude-1.5.0.2
    A better Prelude. Haskell's Prelude needs to maintain backwards
    compatibility and has many aspects that no longer represents best
    practice. The goals of classy-prelude are:
    
    * remove all partial functions
    * modernize data structures
      * generally use Text instead of String
      * encourage the use of appropriate data structures such as Vectors or
        HashMaps instead of always using lists and associated lists
    * reduce import lists and the need for qualified imports
    
    classy-prelude should only be used by application developers. Library
    authors should consider using mono-traversable, which classy-prelude builds
    upon.

---
+++ devel/hs-classy-prelude/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:17:02 pho Exp $
+
+BUILDLINK_TREE+=	hs-classy-prelude
+
+.if !defined(HS_CLASSY_PRELUDE_BUILDLINK3_MK)
+HS_CLASSY_PRELUDE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-classy-prelude+=	hs-classy-prelude>=1.5.0
+BUILDLINK_ABI_DEPENDS.hs-classy-prelude+=	hs-classy-prelude>=1.5.0.2
+BUILDLINK_PKGSRCDIR.hs-classy-prelude?=		../../devel/hs-classy-prelude
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-basic-prelude/buildlink3.mk"
+.include "../../math/hs-bifunctors/buildlink3.mk"
+.include "../../devel/hs-chunked-data/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-mono-traversable/buildlink3.mk"
+.include "../../devel/hs-mono-traversable-instances/buildlink3.mk"
+.include "../../devel/hs-mutable-containers/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-say/buildlink3.mk"
+.include "../../devel/hs-stm-chans/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../devel/hs-vector-instances/buildlink3.mk"
+.endif	# HS_CLASSY_PRELUDE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-classy-prelude
