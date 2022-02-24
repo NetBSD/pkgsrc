a1e4cd2f16b6d484643cd19bad0a9a6e4ff0e4f4
pho <pho@pkgsrc.org>
2022-02-24 01:03:05 +0000

    devel/hs-chunked-data: import hs-chunked-data-0.3.1
    Typeclasses for dealing with various chunked data representations.

---
+++ devel/hs-chunked-data/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/02/24 01:03:05 pho Exp $
+
+BUILDLINK_TREE+=	hs-chunked-data
+
+.if !defined(HS_CHUNKED_DATA_BUILDLINK3_MK)
+HS_CHUNKED_DATA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-chunked-data+=	hs-chunked-data>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-chunked-data+=	hs-chunked-data>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-chunked-data?=	../../devel/hs-chunked-data
+
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_CHUNKED_DATA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-chunked-data
