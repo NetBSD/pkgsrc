348e2651ea9eae7a67db937ec1c07d62a9746272
pho <pho@pkgsrc.org>
2022-02-26 12:33:45 +0000

    devel/hs-fsnotify: Use hinotify on Linux and some BSDs
    On any other platforms it falls back to the polling method as the last
    resort, which is obviously isn't very practical.

--- devel/hs-fsnotify/buildlink3.mk
+++ devel/hs-fsnotify/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:57:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 12:33:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-fsnotify
 
@@ -6,8 +6,14 @@ BUILDLINK_TREE+=	hs-fsnotify
 HS_FSNOTIFY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.3.0.1nb4
+BUILDLINK_ABI_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.3.0.1nb5
 BUILDLINK_PKGSRCDIR.hs-fsnotify?=	../../devel/hs-fsnotify
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "Linux" || !empty(OPSYS:M*BSD)
+.  include "../../devel/hs-hinotify/buildlink3.mk"
+.  include "../../devel/hs-shelly/buildlink3.mk"
+.endif
 
 .include "../../devel/hs-async/buildlink3.mk"
 .include "../../devel/hs-unix-compat/buildlink3.mk"
