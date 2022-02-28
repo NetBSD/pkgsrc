976d569e38ee3059097af3d6fd5b98cb1297ae8c
jaapb <jaapb@pkgsrc.org>
2022-02-28 13:16:24 +0000

    Updated x11/ocaml-lablgtk3 to version 3.1.2.
    Upstream changes are mostly minor, though this update is needed for
    compatibility with OCaml 4.14.
    
    Package options have now been properly integrated.

--- x11/ocaml-lablgtk3/options.mk
+++ x11/ocaml-lablgtk3/options.mk
@@ -1,6 +1,6 @@
-# $NetBSD: options.mk,v 1.2 2020/01/24 14:50:42 jaapb Exp $
+# $NetBSD: options.mk,v 1.3 2022/02/28 13:16:24 jaapb Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.lablgtk3
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml-lablgtk3
 PKG_SUPPORTED_OPTIONS=	gtksourceview3 gtkspell
 PKG_SUGGESTED_OPTIONS=	gtksourceview3 gtkspell
 PLIST_VARS+=		gtkspell gtksourceview
@@ -9,22 +9,12 @@ PLIST_VARS+=		gtkspell gtksourceview
 
 .if !empty(PKG_OPTIONS:Mgtkspell)
 .include "../../textproc/gtkspell3/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-gtkspell
 PLIST.gtkspell=	yes
 OCAML_FINDLIB_DIRS+=	lablgtk3-gtkspell3
-OPAM_INSTALL_FILES+=	lablgtk3-gtkspell3
-DUNE_BUILD_PACKAGES+=	lablgtk3-gtkspell3
-.else
-CONFIGURE_ARGS+=	--without-gtkspell
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtksourceview3)
 .include "../../x11/gtksourceview3/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-gtksourceview3
 PLIST.gtksourceview=	yes
 OCAML_FINDLIB_DIRS+=	lablgtk3-sourceview3
-OPAM_INSTALL_FILES+=	lablgtk3-sourceview3
-DUNE_BUILD_PACKAGES+=	lablgtk3-sourceview3
-.else
-CONFIGURE_ARGS+=	--without-gtksourceview3
 .endif
