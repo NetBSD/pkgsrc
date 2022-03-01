2ccb07fbf2733d174c76cced352e204b3b607615
jaapb <jaapb@pkgsrc.org>
2022-03-01 09:50:00 +0000

    Updated math/ocaml-num to version 1.4.
    Upstream changes are mostly minor, but DESTDIRS are now natively
    supported in the Makefile, so we can remove that patch.

--- math/ocaml-num/buildlink3.mk
+++ math/ocaml-num/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/11/02 16:16:20 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/03/01 09:50:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-num
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-num
 OCAML_NUM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-num+=	ocaml-num>=1.1
-BUILDLINK_ABI_DEPENDS.ocaml-num+=	ocaml-num>=1.1nb2
+BUILDLINK_ABI_DEPENDS.ocaml-num+=	ocaml-num>=1.4
 BUILDLINK_PKGSRCDIR.ocaml-num?=		../../math/ocaml-num
 .endif	# OCAML_NUM_BUILDLINK3_MK
 
