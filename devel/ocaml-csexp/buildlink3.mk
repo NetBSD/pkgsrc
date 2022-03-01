e3a1dc2064026d4f394039390aeb2c7fde7be46e
jaapb <jaapb@pkgsrc.org>
2022-03-01 09:44:12 +0000

    Update package devel/ocaml-csexp to version 1.5.1.
    Upstream changes are minor.

--- devel/ocaml-csexp/buildlink3.mk
+++ devel/ocaml-csexp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/12/09 11:10:47 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/03/01 09:44:12 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-csexp
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-csexp
 OCAML_CSEXP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-csexp+=	ocaml-csexp>=1.3.2
+BUILDLINK_ABI_DEPENDS.ocaml-csexp+=	ocaml-csexp>=1.5.1
 BUILDLINK_PKGSRCDIR.ocaml-csexp?=	../../devel/ocaml-csexp
 
 .endif	# OCAML_CSEXP_BUILDLINK3_MK
