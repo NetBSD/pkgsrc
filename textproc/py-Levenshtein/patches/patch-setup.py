793265eef6b5d27446928b9c4b46bdf7651f6923
gutteridge <gutteridge@pkgsrc.org>
2022-03-02 22:10:48 +0000

    py-Levenshtein: adjust further to un-break Python 2.7 builds
    Python 2.7 (or older pkgsrc states where a less recent py-setuptools is
    present) builds would still have expected the redundant entry_points.txt
    to be found, so explicitly remove it from setup.py for consistency. Ride
    previous update.

--- textproc/py-Levenshtein/patches/patch-setup.py
+++ textproc/py-Levenshtein/patches/patch-setup.py
@@ -1,15 +1,22 @@
-$NetBSD: patch-setup.py,v 1.1 2022/01/05 10:01:08 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2022/03/02 22:10:48 gutteridge Exp $
 
-setuptools not needed
+setuptools not needed.
+
+Also remove the empty entry_points definition, as it causes different
+behaviour between old and most recent py-setuptools, resulting in
+unnecessary variation of PLIST entries.
+
 https://github.com/ztane/python-Levenshtein/issues/79
 
 --- setup.py.orig	2021-02-01 12:03:07.000000000 +0000
 +++ setup.py
-@@ -39,7 +39,6 @@ setup(name='python-Levenshtein',
+@@ -39,9 +39,6 @@ setup(name='python-Levenshtein',
        zip_safe=False,
        ext_modules = [extLevensthein],
        install_requires=[
 -          'setuptools',
            # -*- Extra requirements: -*-
        ],
-       entry_points="""
+-      entry_points="""
+-      """,
+       )
