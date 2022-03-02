44bf726350053b6ff679abd4e483b2760aefa919
pin <pin@pkgsrc.org>
2022-03-02 22:55:15 +0000

    audio/davis: import package
    Davis is a MDP client for music lovers.
    Davis displays any metadata you like! The performers, conductor, ensemble,
    work, movement, recording location, etc., can all be displayed so long as it's
    in your tags.
    
    It can fetch album art directly from MPD, using the albumart command of
    the MPD protocol. This means that davis can fetch album art even from remote
    MPD instances, and does not need to know the location of your music directory.
    With a custom subcommand, it is also possible to display the album art as sixel
    graphics in the terminal.
    
    Davis can be extended with custom subcommands, see upstream repository.

---
+++ audio/davis/cargo-depends.mk
@@ -0,0 +1,9 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2022/03/02 22:55:15 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	bufstream-0.1.4
+CARGO_CRATE_DEPENDS+=	cfg-if-1.0.0
+CARGO_CRATE_DEPENDS+=	configparser-3.0.0
+CARGO_CRATE_DEPENDS+=	lexopt-0.2.0
+CARGO_CRATE_DEPENDS+=	libc-0.2.97
+CARGO_CRATE_DEPENDS+=	log-0.4.14
+CARGO_CRATE_DEPENDS+=	mpdrs-0.1.0
