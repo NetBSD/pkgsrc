19ed252700c924885ea80d7133c232c22debec4f
nia <nia@pkgsrc.org>
2022-03-04 09:16:05 +0000

    profanity: update to 0.11.1
    Some pkgsrc patches were upstreamed.
    
    Changes:
    
    - Fix typo in shade color theme
    - Fix memory leaks
    - Fix buffer overread
    - Don't handle /me in the middle of a message with mentions
    - Explicitly check for curl version
    - Fix OMEMO devicelist access model by reconfiguring it
    - Cosmetic cleanup
    - Change attentionflag shortcut from alt-f to alt+v
    - Update man page and /help information
    - Enable whole word only notifications by default

--- chat/profanity/patches/patch-src_config_preferences.c
+++ chat/profanity/patches/patch-src_config_preferences.c
@@ -1,28 +1,28 @@
-$NetBSD: patch-src_config_preferences.c,v 1.2 2021/01/23 13:08:11 nia Exp $
+$NetBSD: patch-src_config_preferences.c,v 1.3 2022/03/04 09:16:05 nia Exp $
 
 Set paths for security/mozilla-rootcerts.
 
---- src/config/preferences.c.orig	2020-07-01 16:49:19.000000000 +0000
+--- src/config/preferences.c.orig	2021-09-08 10:03:47.000000000 +0000
 +++ src/config/preferences.c
-@@ -527,20 +527,8 @@ prefs_get_tls_certpath(void)
+@@ -636,20 +636,8 @@ prefs_get_tls_certpath(void)
      }
  
      if (setting == NULL) {
--        if (g_file_test("/etc/ssl/certs",  G_FILE_TEST_IS_DIR)) {
+-        if (g_file_test("/etc/ssl/certs", G_FILE_TEST_IS_DIR)) {
 -            return strdup("/etc/ssl/certs");
 -        }
--        if (g_file_test("/etc/pki/tls/certs",  G_FILE_TEST_IS_DIR)) {
+-        if (g_file_test("/etc/pki/tls/certs", G_FILE_TEST_IS_DIR)) {
 -            return strdup("/etc/pki/tls/certs");
 -        }
--        if (g_file_test("/etc/ssl",  G_FILE_TEST_IS_DIR)) {
+-        if (g_file_test("/etc/ssl", G_FILE_TEST_IS_DIR)) {
 -            return strdup("/etc/ssl");
 -        }
--        if (g_file_test("/etc/pki/tls",  G_FILE_TEST_IS_DIR)) {
+-        if (g_file_test("/etc/pki/tls", G_FILE_TEST_IS_DIR)) {
 -            return strdup("/etc/pki/tls");
 -        }
--        if (g_file_test("/system/etc/security/cacerts",  G_FILE_TEST_IS_DIR)) {
+-        if (g_file_test("/system/etc/security/cacerts", G_FILE_TEST_IS_DIR)) {
 -            return strdup("/system/etc/security/cacerts");
-+        if (g_file_test("@SSLCERTS@",  G_FILE_TEST_IS_DIR)) {
++        if (g_file_test("@SSLCERTS@", G_FILE_TEST_IS_DIR)) {
 +            return strdup("@SSLCERTS@");
          }
  
