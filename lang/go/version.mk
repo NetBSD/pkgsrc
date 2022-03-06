ae6820024a8f9ca9af89a12ed69c1171a272c2d7
bsiegert <bsiegert@pkgsrc.org>
2022-03-06 09:53:43 +0000

    Update go116 to 1.16.15.
    This minor release includes a security fix following the security policy:
    
    regexp: stack exhaustion compiling deeply nested expressions
    
    On 64-bit platforms, an extremely deeply nested expression can cause
    regexp.Compile to cause goroutine stack exhaustion, forcing the program to
    exit. Note this applies to very large expressions, on the order of 2MB.
    
    Thanks to Juho Nurminen of Mattermost for reporting this.
    
    This is CVE-2022-24921 and https://go.dev/issue/51112.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.141 2022/02/12 20:14:01 bsiegert Exp $
+# $NetBSD: version.mk,v 1.142 2022/03/06 09:53:43 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO117_VERSION=	1.17.7
-GO116_VERSION=	1.16.14
+GO116_VERSION=	1.16.15
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
 GO14_VERSION=	1.4.3
