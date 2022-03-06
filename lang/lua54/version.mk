aed3715b073a65a7c3ecf30a4e2d8e3469af5c89
nia <nia@pkgsrc.org>
2022-03-06 07:46:10 +0000

    lua54: update to 5.4.4
    Bugs fixed:
    
    - lua.c assumes that argv has at least one element.
    - Lua can generate wrong code when _ENV is <const>.

--- lang/lua54/version.mk
+++ lang/lua54/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.4 2021/04/09 14:18:12 nia Exp $
+# $NetBSD: version.mk,v 1.5 2022/03/06 07:46:10 nia Exp $
 
-LUA_VERSION=	5.4.3
+LUA_VERSION=	5.4.4
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
