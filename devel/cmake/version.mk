ae0812e9b341291554a2e4d98b02e248b146c41a
adam <adam@pkgsrc.org>
2022-03-07 19:59:44 +0000

    cmake: updated to 3.22.3
    CMake 3.22.3
    * while: Restore tolerance of condition error
    * FindMPI: Place static first in mpi test source
    * NMake: Document response file encoding heuristic in a comment
    * NMake: Use UTF-8 BOM in response files only with MSVC tooling
    * message: Restore explicit flushing of messages on stderr
    * gitlab-ci: enable assertions on fedora34-ninja jobs
    * CompilerId: Fix default extensions check for Clang targeting MSVC ABI
    * Intel: Add dependencies on system header files on Windows
    * Tests: Add missing guards on Qt4 and Qt5 tests
    * BinUtils: Avoid llvm-ar on Apple platforms
    * Tests: Simplify RunCMake.{if,while} unbalanced parenthesis cases
    * install(TARGETS): Restore per-export INCLUDES DESTINATION
    * Help: Add MSVC_TOOLSET_VERSION value for v143 toolset
    * IAR/CXX: Fix compatibility with CMP0057 OLD
    * FindGLUT: Use link directories for libs from pkg-config
    * FindMatlab: Fix version detection for MCR >= 9.10
    * IntelLLVM: Add dependencies on system header files on Windows
    * Help: Drop incorrect versionadded for try_compile result variable
    * FindGSL: Improve version extraction regex

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.24 2022/01/25 21:18:08 adam Exp $
+# $NetBSD: version.mk,v 1.25 2022/03/07 19:59:44 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.22.2
+CMAKE_VERSION=	3.22.3
 CMAKE_API=	${CMAKE_VERSION:R}
