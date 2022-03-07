f90e42e2c11e85007bd1d11542d8a5014590297a
wiz <wiz@pkgsrc.org>
2022-03-07 20:34:16 +0000

    lang/guile18: import guile18-1.8.8nb11
    Re-import of lang/guile to avoid having an ancient version as the default.
    
    GUILE, GNU's Ubiquitous Intelligent Language for Extension, is a library
    that implements the Scheme language plus various convenient facilities.
    It's designed so that you can link it into an application or utility to
    make it extensible. Our plan is to link this library into all GNU programs
    that call for extensibility.
    
    This is an extremely old version of guile.  It is installed into
    guile/1.8 within PREFIX.

---
+++ lang/guile18/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/03/07 20:34:16 wiz Exp $
+
+BUILDLINK_TREE+=	guile
+
+.if !defined(GUILE_BUILDLINK3_MK)
+GUILE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.guile+=	guile18>=1.8.1
+BUILDLINK_ABI_DEPENDS.guile+=	guile18>=1.8.8nb8
+BUILDLINK_PKGSRCDIR.guile?=	../../lang/guile18
+
+BUILDLINK_PASSTHRU_DIRS+=		${PREFIX}/guile/1.8
+BUILDLINK_FILES.guile+=			guile/1.8/bin/*
+BUILDLINK_FNAME_TRANSFORM.guile+=	-e s,guile/1.8/bin,bin,
+BUILDLINK_FNAME_TRANSFORM.guile+=	-e s,guile/1.8/lib/pkgconfig,lib/pkgconfig,
+
+GUILE_SUBDIR=				guile/1.8
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/readline/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.endif # GUILE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-guile
