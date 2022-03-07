8aa57a6885a3028c2a6842652e2e69fec59d3d85
wiz <wiz@pkgsrc.org>
2022-03-07 22:59:49 +0000

    *: bump for guile20 PREFIX change

--- devel/g-wrap/buildlink3.mk
+++ devel/g-wrap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2020/03/08 16:42:24 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2022/03/07 22:59:49 wiz Exp $
 
 BUILDLINK_TREE+=	g-wrap
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	g-wrap
 G_WRAP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.g-wrap+=	g-wrap>=1.9.5
-BUILDLINK_ABI_DEPENDS.g-wrap+=	g-wrap>=1.9.15nb1
+BUILDLINK_ABI_DEPENDS.g-wrap+=	g-wrap>=1.9.15nb3
 BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 
 .include "../../devel/libffi/buildlink3.mk"
