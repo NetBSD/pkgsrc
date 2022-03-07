89b6af14c91f51dc6cafe2eb11d2a53abb162ad7
adam <adam@pkgsrc.org>
2022-03-07 18:31:30 +0000

    libuv: updated to 1.44
    Version 1.44.0 (Stable)
    
    Changes since version 1.43.0:
    * darwin: remove EPROTOTYPE error workaround (Ben Noordhuis)
    * doc: fix v1.43.0 changelog entries (cjihrig)
    * win: replace CRITICAL_SECTION+Semaphore with SRWLock (David Machaj)
    * darwin: translate EPROTOTYPE to ECONNRESET (Ben Noordhuis)
    * android: use libc getifaddrs() (Ben Noordhuis)
    * unix: fix STATIC_ASSERT to check what it means to check (Jessica Clarke)
    * unix: ensure struct msghdr is zeroed in recvmmsg (Ondřej Surý)
    * test: test with maximum recvmmsg buffer (Ondřej Surý)
    * unix: don't allow too small thread stack size (Ben Noordhuis)
    * bsd: ensure mutex is initialized (Ben Noordhuis)
    * doc: add gengjiawen as maintainer (gengjiawen)
    * process: monitor for exit with kqueue on BSDs (Jeremy Rose)
    * test: fix flaky uv_fs_lutime test (Momtchil Momtchev)
    * build: fix cmake install locations (Jameson Nash)
    * thread,win: fix C90 style nit (ssrlive)
    * build: rename CFLAGS to AM_CFLAGS (Jameson Nash)
    * doc/guide: update content and sample code (woclass)
    * process,bsd: handle kevent NOTE_EXIT failure (Jameson Nash)
    * test: remove flaky test ipc_closed_handle (Ben Noordhuis)
    * darwin: bump minimum supported version to 10.15 (Ben Noordhuis)
    * win: return fractional seconds in uv_uptime() (Luca Adrian L)
    * build: export uv_a for cmake (WenTao Ou)
    * loop: add pending work to loop-alive check (Jameson Nash)
    * win: use GetTickCount64 for uptime again (Jameson Nash)
    * win: restrict system DLL load paths (jonilaitinen)
    * win,errors: remap ERROR_ACCESS_DENIED to UV_EACCES (Darshan Sen)
    * bench: add `uv_queue_work` ping-pong measurement (Momtchil Momtchev)
    * build: fix error C4146 on MSVC (UMU)
    * test: fix benchmark-ping-udp (Ryan Liptak)
    * win,fs: consider broken pipe error a normal EOF (Momtchil Momtchev)
    * document the values of enum uv_stdio_flags (Paul Evans)
    * win,loop: add missing uv_update_time (twosee)
    * win,fs: avoid closing an invalid handle (Jameson Nash)
    * fix oopsie from
    * doc: clarify android api level (Ben Noordhuis)
    * win: fix style nits [NFC] (Jameson Nash)
    * test: fix flaky udp_mmsg test (Santiago Gimeno)
    * test: fix ipc_send_recv_pipe flakiness (Ben Noordhuis)
    * doc: checkout -> check out (wyckster)
    * core: change uv_get_password uid/gid to unsigned (Jameson Nash)
    * hurd: unbreak build on GNU/Hurd (Vittore F. Scolari)
    * freebsd: use copy_file_range() in uv_fs_sendfile() (David Carlier)
    * test: use closefd in runner-unix.c (Guilherme Íscaro)
    * Reland "macos: use posix_spawn instead of fork" (Jameson Nash)
    * android: fix build error when no ifaddrs.h (ssrlive)
    * unix,win: add uv_available_parallelism() (Ben Noordhuis)
    * process: remove OpenBSD from kevent list (Jameson Nash)
    * zos: fix build breakage (Ben Noordhuis)
    * process: only use F_DUPFD_CLOEXEC if it is defined (Jameson Nash)
    * win,poll: add the MSAFD GUID for AF_UNIX (roflcopter4)
    * unix: simplify uv__cloexec_fcntl() (Ben Noordhuis)
    * doc: add secondary GPG ID for vtjnash (Jameson Nash)
    * unix: remove uv__cloexec_ioctl() (Jameson Nash)

--- devel/libuv/patches/patch-src_unix_udp.c
+++ devel/libuv/patches/patch-src_unix_udp.c
@@ -1,25 +1,25 @@
-$NetBSD: patch-src_unix_udp.c,v 1.5 2022/01/28 21:13:10 schmonz Exp $
+$NetBSD: patch-src_unix_udp.c,v 1.6 2022/03/07 18:31:30 adam Exp $
 
 Apply MacPorts patch-libuv-legacy.diff for pre-10.7 platforms.
 
---- src/unix/udp.c.orig	2022-01-04 14:18:00.000000000 +0000
+--- src/unix/udp.c.orig	2022-03-07 16:39:08.000000000 +0000
 +++ src/unix/udp.c
-@@ -927,7 +927,8 @@ static int uv__udp_set_membership6(uv_ud
-     !defined(__NetBSD__) &&                                         \
+@@ -929,7 +929,8 @@ static int uv__udp_set_membership6(uv_ud
      !defined(__ANDROID__) &&                                        \
      !defined(__DragonFly__) &&                                      \
--    !defined(__QNX__)
-+    !defined(__QNX__) &&                                            \
+     !defined(__QNX__) &&                                            \
+-    !defined(__GNU__)
++    !defined(__GNU__) &&                                            \
 +    (!defined(__APPLE__) || MAC_OS_X_VERSION_MAX_ALLOWED >= 1070)
  static int uv__udp_set_source_membership4(uv_udp_t* handle,
                                            const struct sockaddr_in* multicast_addr,
                                            const char* interface_addr,
-@@ -1119,7 +1120,8 @@ int uv_udp_set_source_membership(uv_udp_
-     !defined(__NetBSD__) &&                                         \
+@@ -1122,7 +1123,8 @@ int uv_udp_set_source_membership(uv_udp_
      !defined(__ANDROID__) &&                                        \
      !defined(__DragonFly__) &&                                      \
--    !defined(__QNX__)
-+    !defined(__QNX__) &&                                            \
+     !defined(__QNX__) &&                                            \
+-    !defined(__GNU__)
++    !defined(__GNU__) &&                                            \
 +    (!defined(__APPLE__) || MAC_OS_X_VERSION_MAX_ALLOWED >= 1070)
    int err;
    union uv__sockaddr mcast_addr;
