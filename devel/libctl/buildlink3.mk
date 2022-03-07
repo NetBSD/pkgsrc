468e16efc7cdccc95a03205e35c2aad9ab3f25f6
wiz <wiz@pkgsrc.org>
2022-03-07 22:59:49 +0000

    *: bump for guile20 PREFIX change

--- devel/libctl/buildlink3.mk
+++ devel/libctl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/03/08 16:47:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/03/07 22:59:49 wiz Exp $
 
 BUILDLINK_TREE+=	libctl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libctl
 LIBCTL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.2.2
-BUILDLINK_ABI_DEPENDS.libctl?=	libctl>=3.2.2nb2
+BUILDLINK_ABI_DEPENDS.libctl?=	libctl>=3.2.2nb4
 BUILDLINK_PKGSRCDIR.libctl?=	../../devel/libctl
 
 .include "../../lang/guile20/buildlink3.mk"
