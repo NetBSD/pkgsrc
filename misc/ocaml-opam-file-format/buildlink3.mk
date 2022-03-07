6d1a9fe58610cfcc179246b619268654434673a2
jaapb <jaapb@pkgsrc.org>
2022-03-07 10:27:08 +0000

    Updated misc/ocaml-opam-file-format to version 2.1.3.
    Changes are mostly minor, but this is needed to compile opaline 0.3.3.

--- misc/ocaml-opam-file-format/buildlink3.mk
+++ misc/ocaml-opam-file-format/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/12/09 10:36:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/03/07 10:27:08 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam-file-format
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam-file-format
 OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.1.0
+BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.1.3
 BUILDLINK_PKGSRCDIR.ocaml-opam-file-format?=	../../misc/ocaml-opam-file-format
 .endif	# OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK
 
