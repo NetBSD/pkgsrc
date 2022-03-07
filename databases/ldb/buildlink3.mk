6f2795fd7ab966faee10ccf37bb274cbd0424e47
thor <thor@pkgsrc.org>
2022-03-07 21:36:19 +0000

    ldb: version 2.4.2 for incoming samba4 update

--- databases/ldb/buildlink3.mk
+++ databases/ldb/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/11/10 16:58:06 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/03/07 21:36:19 thor Exp $
 
 BUILDLINK_TREE+=	ldb
 
 .if !defined(LDB_BUILDLINK3_MK)
 LDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ldb+=	ldb>=2.0.7
-BUILDLINK_PKGSRCDIR.ldb?=	../../databases/ldb
+BUILDLINK_API_DEPENDS.ldb+=	ldb>=2.4.0
+BUILDLINK_PKGSRCDIR.ldb?=	../../wip/ldb
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../databases/tdb/buildlink3.mk"
