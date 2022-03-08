12ed7140d4d20fb4bd27eb99386aba766fbd788e
wiz <wiz@pkgsrc.org>
2022-03-08 11:16:00 +0000

    dbus: update to 1.14.0.
    1.14.x is a new stable branch, superseding 1.12.x.
    
    Summary of major changes between 1.12.x and 1.14.0
    --------------------------------------------------
    
    Dependencies:
    
    • On Unix platforms, if getpwnam_r() and getgrnam_r() are implemented,
      they must be POSIX-conformant. The non-POSIX signature seen in ancient
      Solaris versions will no longer work.
    
    ...
    
    Deprecations:
    
    • Third-party software should install default dbus policies for the system
      bus into ${datadir}/dbus-1/system.d (this has been supported since dbus
      1.10, released in August 2015). Installing default dbus policies in
      ${sysconfdir}/dbus-1/system.d is now considered to be deprecated. Policy
      files in ${sysconfdir}/dbus-1/system.d continue to be read, but this
      directory should only be used by system administrators wishing to
      override the default policies.
    
      The ${datadir} applicable to dbus is usually /usr/share and the
      ${sysconfdir} is usually /etc.
    
    • A similar pattern applies to the session bus policies in session.d.
    
    • The dbus-send(1) man page now documents --bus and --peer instead of
      the old --address synonym for --peer, which has been deprecated since
      the introduction of --bus and --peer in 1.7.6
    
    • The dbus-daemon man page now has scarier warnings about
      <allow_anonymous/> and non-local TCP, which are insecure and should
      not be used, particularly for the standard system and session buses
    
    • DBusServer (and hence the dbus-daemon) no longer accepts usernames
      (login names) for the recommended EXTERNAL authentication mechanism,
      only numeric user IDs or the empty string. See 1.13.0 release notes
      for full details.
    
    New features:
    
    • On Linux 4.13 or later when built against a suitable glibc version,
      GetConnectionCredentials() now includes UnixGroupIDs, the effective
      group IDs of the initiator of the connection, taken from
      SO_PEERGROUPS.
    
    • On Linux 4.13 or later, <policy group="…"> now uses the SO_PEERGROUPS
      credentials-passing socket option to get the effective group IDs
      of the initiator of the connection. See 1.13.4 release notes for details.
    
    • Add a --sender option to dbus-send, which requests a name and holds it
      until the signal has been sent
    
    • dbus-daemon <allow> and <deny> rules can now specify a
      send_destination_prefix attribute, which is like a combination of
      send_destination and the arg0namespace keyword in match rules.
      See 1.13.12 release notes for more details
    
    • The dbus-daemon now filters the messages that it relays, removing
      header fields that it does not understand. Clients must not rely on
      this behaviour unless they have confirmed that they are connected to
      a suitable message bus implementation, for example by querying its
      Features property.
    
    • The dbus-daemon now emits a signal, ActivatableServicesChanged, when
      the list of activatable services may have changed. Support for this
      signal can be discovered by querying the Features property.
    
    • It is now possible to disable traditional (non-systemd) service
      activation at build-time (Autotools: --disable-traditional-activation,
      CMake: -DENABLE_TRADITIONAL_ACTIVATION=OFF). See 1.13.10 release notes
      for details.
    
    • The API reference manual can be built as a Qt compiled help file if
      qhelpgenerator(-qt5) is available. See 1.13.16 release notes for details.
    
    Miscellaneous behaviour changes:
    
    • When using the "user bus" (--enable-user-session), put the dbus-daemon
      in the session slice
    
    • Several environment variables set by systemd are no longer passed
      on to activated services
    
    • If the dbus-daemon is compiled for Linux with systemd support, it
      now informs systemd that it is ready for use via the sd_notify()
      mechanism

--- sysutils/dbus/patches/patch-dbus_dbus-sysdeps-util-unix.c
+++ sysutils/dbus/patches/patch-dbus_dbus-sysdeps-util-unix.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-dbus_dbus-sysdeps-util-unix.c,v 1.4 2017/11/23 14:43:23 wiz Exp $
+$NetBSD: patch-dbus_dbus-sysdeps-util-unix.c,v 1.5 2022/03/08 11:16:00 wiz Exp $
 
 add solaris specific console_user test
 
---- dbus/dbus-sysdeps-util-unix.c.orig	2017-10-30 12:26:18.000000000 +0000
+--- dbus/dbus-sysdeps-util-unix.c.orig	2022-02-23 14:39:11.000000000 +0000
 +++ dbus/dbus-sysdeps-util-unix.c
-@@ -51,6 +51,10 @@
- #include <dirent.h>
- #include <sys/un.h>
+@@ -56,6 +56,10 @@
+ #include <sys/prctl.h>
+ #endif
  
 +#if defined(__sun) && defined(__SVR4)
 +#include <pwd.h>
@@ -15,7 +15,7 @@ add solaris specific console_user test
  #ifdef HAVE_SYS_SYSLIMITS_H
  #include <sys/syslimits.h>
  #endif
-@@ -551,8 +555,31 @@ _dbus_user_at_console (const char *usern
+@@ -581,8 +585,31 @@ _dbus_user_at_console (const char *usern
  #ifdef DBUS_CONSOLE_AUTH_DIR
    DBusString u, f;
    dbus_bool_t result;
@@ -47,7 +47,7 @@ add solaris specific console_user test
    if (!_dbus_string_init (&f))
      {
        _DBUS_SET_OOM (error);
-@@ -577,6 +604,7 @@ _dbus_user_at_console (const char *usern
+@@ -607,6 +634,7 @@ _dbus_user_at_console (const char *usern
  
   out:
    _dbus_string_free (&f);
