6c6ecc3050eabd6b74bc1bdc7d10a78c0ed36a38
prlw1 <prlw1@pkgsrc.org>
2022-03-10 21:28:03 +0000

    Fix build with meson >= 0.61
    Update epiphany to 40.6
    
    Two year's worth of development, bug fixes and translation updates.
    
    Note:
    
    The Safe Browsing phishing protection feature is now disabled by
    default (#682) to comply with changes to Google's terms of service,
    which now prohibit inclusion of API keys in open source projects.
    
    For details, see:
    https://gitlab.gnome.org/GNOME/epiphany/-/blob/40.6/NEWS

--- www/epiphany/patches/patch-lib_ephy-sync-utils.c
+++ www/epiphany/patches/patch-lib_ephy-sync-utils.c
@@ -1,35 +1,24 @@
-$NetBSD: patch-lib_ephy-sync-utils.c,v 1.1 2020/06/10 19:45:51 nia Exp $
+$NetBSD: patch-lib_ephy-sync-utils.c,v 1.2 2022/03/10 21:28:03 prlw1 Exp $
 
 NetBSD does not have getrandom().
 
---- lib/ephy-sync-utils.c.orig	2020-05-29 15:32:59.180212500 +0000
+--- lib/ephy-sync-utils.c.orig	2021-12-21 00:24:23.810496800 +0000
 +++ lib/ephy-sync-utils.c
-@@ -30,7 +30,11 @@
- #include <libsoup/soup.h>
- #include <stdio.h>
+@@ -32,7 +32,7 @@
  #include <string.h>
-+#ifndef __NetBSD__
+ #if defined(__linux__)
  #include <sys/random.h>
-+#else
-+#include <stdlib.h>
-+#endif
+-#elif defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+ #include <unistd.h>
+ #endif
  
- static const char hex_digits[] = "0123456789abcdef";
- 
-@@ -179,12 +183,16 @@ ephy_sync_utils_generate_random_bytes (v
-   g_assert (num_bytes > 0);
-   g_assert (out);
- 
-+#ifndef __NetBSD__
+@@ -187,7 +187,7 @@ ephy_sync_utils_generate_random_bytes (v
+   if (getentropy (out, num_bytes) == -1) {
+     g_error ("Failed to get entropy: %s", g_strerror (errno));
+   }
+-#else
++#elif !defined(__NetBSD__)
    do {
      ret = getrandom (out, num_bytes, 0);
    } while (ret < (gssize)num_bytes && errno == EINTR);
- 
-   if (ret != (gssize)num_bytes)
-     g_error ("Failed to generate randomness: %s", g_strerror (errno));
-+#else
-+  arc4random_buf (out, num_bytes);
-+#endif
- }
- 
- char *
