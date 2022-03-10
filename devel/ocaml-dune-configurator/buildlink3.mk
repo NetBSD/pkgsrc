5539c9c569b2e89cdcc09b5c81b71ee40bcdf114
jaapb <jaapb@pkgsrc.org>
2022-03-10 08:49:18 +0000

    Updated PLIST for devel/ocaml-dune-configurator, revbump
    This updates the PLIST so that it is in accordance with the new version
    of devel/ocaml-dune.

--- devel/ocaml-dune-configurator/buildlink3.mk
+++ devel/ocaml-dune-configurator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/12/09 10:52:17 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/03/10 08:49:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune-configurator
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-dune-configurator
 OCAML_DUNE_CONFIGURATOR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=2.7.1
+BUILDLINK_ABI_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=2.9.3nb2
 BUILDLINK_PKGSRCDIR.ocaml-dune-configurator?=	../../devel/ocaml-dune-configurator
 
 .include "../../devel/ocaml-dune/buildlink3.mk"
