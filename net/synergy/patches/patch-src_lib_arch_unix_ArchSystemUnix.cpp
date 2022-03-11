38b80e65b82d9bb125da9ee37ab92b129d685684
tnn <tnn@pkgsrc.org>
2022-03-11 20:49:13 +0000

    synergy: update to 1.14.3.5
    - Fix issue with function keys on macOS
    - Configure scroll direction on a per-computer basis

--- net/synergy/patches/patch-src_lib_arch_unix_ArchSystemUnix.cpp
+++ net/synergy/patches/patch-src_lib_arch_unix_ArchSystemUnix.cpp
@@ -1,20 +1,20 @@
-$NetBSD: patch-src_lib_arch_unix_ArchSystemUnix.cpp,v 1.1 2022/01/07 23:02:48 tnn Exp $
+$NetBSD: patch-src_lib_arch_unix_ArchSystemUnix.cpp,v 1.2 2022/03/11 20:49:13 tnn Exp $
 
 make dbus optional
 
---- src/lib/arch/unix/ArchSystemUnix.cpp.orig	2021-11-22 13:50:34.000000000 +0000
+--- src/lib/arch/unix/ArchSystemUnix.cpp.orig	2022-02-21 11:08:47.000000000 +0000
 +++ src/lib/arch/unix/ArchSystemUnix.cpp
-@@ -23,7 +23,7 @@
- #include <array>
- #include <memory>
- #include <string>
+@@ -19,7 +19,7 @@
+ #include "arch/unix/ArchSystemUnix.h"
+ 
+ #include <sys/utsname.h>
 -#ifndef __APPLE__
 +#ifdef WITH_DBUS
  #include <QtDBus>
  #endif
  
-@@ -104,7 +104,7 @@ ArchSystemUnix::runCommand(const std::st
-     return result;
+@@ -82,7 +82,7 @@ ArchSystemUnix::getLibsUsed(void) const
+     return "not implemented.\nuse lsof on shell";
  }
  
 -#ifndef __APPLE__
