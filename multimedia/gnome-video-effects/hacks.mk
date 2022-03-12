1d00c760efdb37a2c6eeca6ec228391924a27b1a
nia <nia@pkgsrc.org>
2022-03-12 07:17:19 +0000

    multimedia: Use hacks.mk to avoid NetBSD msgfmt on NetBSD only.

---
+++ multimedia/gnome-video-effects/hacks.mk
@@ -0,0 +1,13 @@
+# $NetBSD: hacks.mk,v 1.1 2022/03/12 07:17:19 nia Exp $
+
+.if !defined(GNOME_VIDEO_EFFECTS_HACKS_MK)
+GNOME_VIDEO_EFFECTS_HACKS_MK:=
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "NetBSD"
+# Old GPLv2 version of msgfmt in NetBSD does not support --desktop/--xml.
+TOOLS_PLATFORM.msgfmt=	# none
+PKG_HACKS+=		avoid-gplv2-msgfmt
+.endif
+.endif # GNOME_VIDEO_EFFECTS_HACKS_MK
