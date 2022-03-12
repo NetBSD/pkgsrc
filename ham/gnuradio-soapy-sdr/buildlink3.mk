6d899a49087743cb7ca0432028e6291bd6129d5f
tnn <tnn@pkgsrc.org>
2022-03-12 23:04:44 +0000

    gnuradio*: update to 3.10.1.1
    Bugfix release for the 3.10.x.y release series.

--- ham/gnuradio-soapy-sdr/buildlink3.mk
+++ ham/gnuradio-soapy-sdr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/01/16 18:06:26 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/03/12 23:04:46 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-soapy-sdr
 
 .if !defined(GNURADIO_SOAPY_SDR_BUILDLINK3_MK)
 GNURADIO_SOAPY_SDR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-soapy-sdr+=	gnuradio-soapy-sdr>=3.10.0.0
+BUILDLINK_API_DEPENDS.gnuradio-soapy-sdr+=	gnuradio-soapy-sdr>=3.10.1.1
 BUILDLINK_PKGSRCDIR.gnuradio-soapy-sdr?=	../../ham/gnuradio-soapy-sdr
 
 .include	"../../ham/soapy-sdr/buildlink3.mk"
