4bc5d80827bf4263ad926f28ce7ae551c31b528c
nia <nia@pkgsrc.org>
2022-03-12 08:01:48 +0000

    *: Replace per-package msgfmt hacks with msgfmt-desktop.mk

--- devel/gettext-tools/msgfmt-desktop.mk
+++ devel/gettext-tools/msgfmt-desktop.mk
@@ -1,13 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1 2022/03/12 07:19:24 nia Exp $
-
-.if !defined(EVINCE_HACKS_MK)
-EVINCE_HACKS_MK:=
+# $NetBSD: msgfmt-desktop.mk,v 1.1 2022/03/12 08:01:48 nia Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
 .if ${OPSYS} == "NetBSD"
 # Old GPLv2 version of msgfmt in NetBSD does not support --desktop/--xml.
 TOOLS_PLATFORM.msgfmt=	# none
-PKG_HACKS+=		avoid-gplv2-msgfmt
 .endif
-.endif # EVINCE_HACKS_MK
