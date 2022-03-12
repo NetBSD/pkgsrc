ab0c29c4072382f79b4e2274dc2837568b189a4b
tnn <tnn@pkgsrc.org>
2022-03-12 23:04:44 +0000

    gnuradio*: update to 3.10.1.1
    Bugfix release for the 3.10.x.y release series.

--- ham/gnuradio-core/version.mk
+++ ham/gnuradio-core/version.mk
@@ -1,3 +1,3 @@
-# $NetBSD: version.mk,v 1.5 2022/01/16 18:06:25 tnn Exp $
+# $NetBSD: version.mk,v 1.6 2022/03/12 23:04:45 tnn Exp $
 
-VERSION=	3.10.0.0
+VERSION=	3.10.1.1
--- ham/gnuradio-zeromq/buildlink3.mk
+++ ham/gnuradio-zeromq/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/16 18:06:27 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/03/12 23:04:46 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-zeromq
 
 .if !defined(GNURADIO_ZEROMQ_BUILDLINK3_MK)
 GNURADIO_ZEROMQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.10.0.0
+BUILDLINK_API_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.10.1.1
 BUILDLINK_PKGSRCDIR.gnuradio-zeromq?=	../../ham/gnuradio-zeromq
 .endif # GNURADIO_ZEROMQ_BUILDLINK3_MK
 
