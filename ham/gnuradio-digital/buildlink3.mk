80e36de135f4320b57d009c197679eafef131b7b
tnn <tnn@pkgsrc.org>
2022-03-12 23:04:44 +0000

    gnuradio*: update to 3.10.1.1
    Bugfix release for the 3.10.x.y release series.

--- ham/gnuradio-digital/buildlink3.mk
+++ ham/gnuradio-digital/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/01/16 18:06:26 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/03/12 23:04:45 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-digital
 
 .if !defined(GNURADIO_DIGITAL_BUILDLINK3_MK)
 GNURADIO_DIGITAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.10.0.0
+BUILDLINK_API_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.10.1.1
 BUILDLINK_PKGSRCDIR.gnuradio-digital?=		../../ham/gnuradio-digital
 .endif # GNURADIO_DIGITAL_BUILDLINK3_MK
 
--- ham/gnuradio-utils/buildlink3.mk
+++ ham/gnuradio-utils/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/01/16 18:06:27 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/03/12 23:04:46 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-utils
 
 .if !defined(GNURADIO_UTILS_BUILDLINK3_MK)
 GNURADIO_UTILS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-utils+=	gnuradio-utils>=3.10.0.0
+BUILDLINK_API_DEPENDS.gnuradio-utils+=	gnuradio-utils>=3.10.1.1
 BUILDLINK_PKGSRCDIR.gnuradio-utils?=	../../ham/gnuradio-utils
 .endif # GNURADIO_UTILS_BUILDLINK3_MK
 
