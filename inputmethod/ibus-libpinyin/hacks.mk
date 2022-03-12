76e0daee0f5d2a61c51d17592834f1a37529ac80
nia <nia@pkgsrc.org>
2022-03-12 07:11:13 +0000

    inputmethod: Use hacks.mk to avoid NetBSD msgfmt on NetBSD only.

---
+++ inputmethod/ibus-libpinyin/hacks.mk
@@ -0,0 +1,13 @@
+# $NetBSD: hacks.mk,v 1.1 2022/03/12 07:11:14 nia Exp $
+
+.if !defined(IBUS_LIBPINYIN_HACKS_MK)
+IBUS_LIBPINYIN_HACKS_MK:=
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "NetBSD"
+# Old GPLv2 version of msgfmt in NetBSD does not support --desktop/--xml.
+TOOLS_PLATFORM.msgfmt=	# none
+PKG_HACKS+=		avoid-gplv2-msgfmt
+.endif
+.endif # IBUS_LIBPINYIN_HACKS_MK
