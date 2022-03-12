11b800dca200092747c75f8fcf80ed3313a3d7fc
tnn <tnn@pkgsrc.org>
2022-03-12 23:04:44 +0000

    gnuradio*: update to 3.10.1.1
    Bugfix release for the 3.10.x.y release series.

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.34 2022/01/16 18:06:25 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2022/03/12 23:04:45 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
 .if !defined(GNURADIO_CORE_BUILDLINK3_MK)
 GNURADIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.10.0.0
+BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.10.1.1
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
 
 .include "../../devel/gmp/buildlink3.mk"
--- ham/gnuradio-trellis/buildlink3.mk
+++ ham/gnuradio-trellis/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/01/16 18:06:27 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/03/12 23:04:46 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-trellis
 
 .if !defined(GNURADIO_TRELLIS_BUILDLINK3_MK)
 GNURADIO_TRELLIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.10.0.0
+BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.10.1.1
 BUILDLINK_PKGSRCDIR.gnuradio-trellis?=		../../ham/gnuradio-trellis
 .endif # GNURADIO_TRELLIS_BUILDLINK3_MK
 
