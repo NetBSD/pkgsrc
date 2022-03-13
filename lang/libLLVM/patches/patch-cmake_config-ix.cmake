7d8bdd2b0d6b5c2bf1ca9aea0e46be889b724e42
tnn <tnn@pkgsrc.org>
2022-03-13 15:22:32 +0000

    libLLVM: update to 13.0.1, for mesa update

--- lang/libLLVM/patches/patch-cmake_config-ix.cmake
+++ lang/libLLVM/patches/patch-cmake_config-ix.cmake
@@ -1,15 +1,15 @@
-$NetBSD: patch-cmake_config-ix.cmake,v 1.2 2020/03/06 22:44:18 tnn Exp $
+$NetBSD: patch-cmake_config-ix.cmake,v 1.3 2022/03/13 15:22:32 tnn Exp $
 
 Do not generate invalid llvm-config in pkgsrc.
 
---- cmake/config-ix.cmake.orig	2019-12-11 19:15:30.000000000 +0000
+--- cmake/config-ix.cmake.orig	2021-06-28 16:23:38.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -141,7 +141,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
+@@ -175,7 +175,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
+       set(MAYBE_REQUIRED)
      endif()
      if(LLVM_ENABLE_TERMINFO)
-       set(HAVE_TERMINFO 0)
--      foreach(library terminfo tinfo curses ncurses ncursesw)
-+      foreach(library terminfo curses ncurses ncursesw tinfo)
-         string(TOUPPER ${library} library_suffix)
-         check_library_exists(${library} setupterm "" HAVE_TERMINFO_${library_suffix})
-         if(HAVE_TERMINFO_${library_suffix})
+-      find_library(TERMINFO_LIB NAMES terminfo tinfo curses ncurses ncursesw ${MAYBE_REQUIRED})
++      find_library(TERMINFO_LIB NAMES terminfo curses ncurses ncursesw tinfo ${MAYBE_REQUIRED})
+     endif()
+     if(TERMINFO_LIB)
+       set(LLVM_ENABLE_TERMINFO 1)
