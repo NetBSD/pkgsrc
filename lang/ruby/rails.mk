bd6f3de5b1c38acd99c80f686ba47c22dfdcbd42
taca <taca@pkgsrc.org>
2022-03-13 15:04:26 +0000

    www/ruby-rails52: update to 5.2.6.3
    Ruby on Rails 5.2.6.3 is not latest version but it should be easy to pull-up
    to pkgsrc-2021Q4.
    
    Changes are in devel/ruby-activestorage52 only.
    
    
    Rails 5.2.6.3 (March 08, 2022)
    
    * Added image transformation validation via configurable allow-list.
    
      Variant now offers a configurable allow-list for
      transformation methods in addition to a configurable deny-list for arguments.
    
      [CVE-2022-21831]

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.114 2022/02/13 07:39:14 taca Exp $
+# $NetBSD: rails.mk,v 1.115 2022/03/13 15:04:26 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -47,7 +47,7 @@ _RUBY_RAILS_MK=	# defined
 #
 # current Ruby on Rails versions.
 #
-RUBY_RAILS52_VERSION?=	5.2.6.2
+RUBY_RAILS52_VERSION?=	5.2.6.3
 RUBY_RAILS60_VERSION?=	6.0.4.6
 RUBY_RAILS61_VERSION?=	6.1.4.6
 RUBY_RAILS70_VERSION?=	7.0.2.2
