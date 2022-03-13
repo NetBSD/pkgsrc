074288eda33cf501dd7f9f15af83ae740f1bc302
gutteridge <gutteridge@pkgsrc.org>
2022-03-13 05:46:55 +0000

    hplip: fix botched patches and qt5 option packaging
    When some patches were last updated, some incorrect changes were
    inadvertently added. This broke some of the functionality, since there
    were hard-coded paths added (e.g., "/usr/pkg/bin/python3.7").
    
    Also fix qt5 option packaging so this actually builds again. There are
    still issues that need investigation, but at least an executable can
    now run and be interacted with (e.g., attaching as a plugin in Xfce).

--- print/hplip/patches/patch-diagnose_queues.py
+++ print/hplip/patches/patch-diagnose_queues.py
@@ -1,15 +1,9 @@
-$NetBSD: patch-diagnose_queues.py,v 1.3 2020/01/04 04:24:12 khorben Exp $
+$NetBSD: patch-diagnose_queues.py,v 1.4 2022/03/13 05:46:55 gutteridge Exp $
 
 Correct the spelling on the module name.
 
 --- diagnose_queues.py.orig	2019-12-10 05:00:36.000000000 +0000
 +++ diagnose_queues.py
-@@ -1,4 +1,4 @@
--#!/usr/bin/env python
-+#!/usr/pkg/bin/python3.7
- # -*- coding: utf-8 -*-
- #
- # (c) Copyright 2011-2015 HP Development Company, L.P.
 @@ -22,7 +22,7 @@
  from __future__ import print_function
  __version__ = '1.1'
