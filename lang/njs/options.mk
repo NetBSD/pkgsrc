d2fcfb5c3d61f7012909e08bd8885452dcdeb296
osa <osa@pkgsrc.org>
2022-03-14 02:52:48 +0000

    lang/njs: add new port NGINX JavaScript (njs) command line utility
    NGINX JavaScript, also knows as njs, is a subset of the JavaScript
    language that allows extending nginx functionality.  njs is created
    in compliance with ECMAScript 5.1 (strict mode) with some ECMAScript 6
    and later extensions.  It's easy to use njs command line utility to
    develop and debug additional functionality.  Also, it's light-weight
    and very useful as a shebang in some cases as the nodejs substitutor.

---
+++ lang/njs/options.mk
@@ -0,0 +1,38 @@
+# $NetBSD: options.mk,v 1.1 2022/03/14 02:52:48 osa Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.njs
+PKG_SUPPORTED_OPTIONS=	pcre pcre2 ssl
+PKG_SUGGESTED_OPTIONS=	pcre ssl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpcre)
+.include "../../devel/pcre/buildlink3.mk"
+SUBST_CLASSES+=		fix-pcre
+SUBST_STAGE.fix-pcre=	pre-configure
+SUBST_FILES.fix-pcre=	auto/pcre
+SUBST_SED.fix-pcre=	-e 's,/usr/pkg,${BUILDLINK_PREFIX.pcre},g'
+SUBST_NOOP_OK.fix-pcre=	yes
+.else
+CONFIGURE_ARGS+=	--no-pcre2
+.endif
+
+.if !empty(PKG_OPTIONS:Mpcre2)
+.include "../../devel/pcre2/buildlink3.mk"
+SUBST_CLASSES+=		fix-pcre2
+SUBST_STAGE.fix-pcre2=	pre-configure
+SUBST_FILES.fix-pcre2=	auto/pcre
+SUBST_SED.fix-pcre2=	-e 's,/usr/pkg,${BUILDLINK_PREFIX.pcre2},g'
+SUBST_NOOP_OK.fix-pcre2=yes
+.endif
+
+.if empty(PKG_OPTIONS:Mpcre2) && empty(PKG_OPTIONS:Mpcre)
+CONFIGURE_ARGS+=	--no-pcre
+CONFIGURE_ARGS+=	--no-pcre2
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+.include "../../security/openssl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--no-openssl
+.endif
