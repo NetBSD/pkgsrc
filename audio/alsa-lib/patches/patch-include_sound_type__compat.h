791bb86c922ee3ae5fc47fd97f457a9266fdf455
wiz <wiz@pkgsrc.org>
2022-03-14 14:08:42 +0000

    alsa-lib: fix build on Solaris
    From Claes Nästén in PR 56749

--- audio/alsa-lib/patches/patch-include_sound_type__compat.h
+++ audio/alsa-lib/patches/patch-include_sound_type__compat.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-include_sound_type__compat.h,v 1.1 2021/05/12 14:12:13 ryoon Exp $
+$NetBSD: patch-include_sound_type__compat.h,v 1.2 2022/03/14 14:08:42 wiz Exp $
+
+NetBSD and Solaris support.
 
 --- include/sound/type_compat.h.orig	2020-06-29 10:51:08.000000000 +0000
 +++ include/sound/type_compat.h
@@ -10,10 +12,13 @@ $NetBSD: patch-include_sound_type__compat.h,v 1.1 2021/05/12 14:12:13 ryoon Exp 
  
  #ifndef DOC_HIDDEN
  #include <stdint.h>
-@@ -16,6 +16,20 @@ typedef int32_t __s32;
+@@ -15,7 +15,30 @@ typedef int16_t __s16;
+ typedef int32_t __s32;
  typedef int64_t __s64;
  
++#ifndef __sun
  #include <endian.h>
++#endif
 +#if defined(__NetBSD__)
 +#include <machine/bswap.h>
 +#if BYTE_ORDER == LITTLE_ENDIAN
@@ -28,18 +33,26 @@ $NetBSD: patch-include_sound_type__compat.h,v 1.1 2021/05/12 14:12:13 ryoon Exp 
 +#define __cpu_to_be16(x) (x)
 +#endif
 +#else
++#ifdef __sun
++#include <sys/byteorder.h>
++#define __cpu_to_le32(x) LE_32(x)
++#define __cpu_to_be32(x) BE_32(x)
++#define __cpu_to_le16(x) LE_16(x)
++#define __cpu_to_be16(x) BE_16(x)
++#else
  #include <byteswap.h>
  #if __BYTE_ORDER == __LITTLE_ENDIAN
  #define __cpu_to_le32(x) (x)
-@@ -28,6 +42,7 @@ typedef int64_t __s64;
+@@ -28,6 +51,8 @@ typedef int64_t __s64;
  #define __cpu_to_le16(x) bswap_16(x)
  #define __cpu_to_be16(x) (x)
  #endif
 +#endif
++#endif
  
  #define __le32_to_cpu __cpu_to_le32
  #define __be32_to_cpu __cpu_to_be32
-@@ -54,4 +69,4 @@ typedef int64_t __s64;
+@@ -54,4 +79,4 @@ typedef int64_t __s64;
  
  #endif /* DOC_HIDDEN */
  
