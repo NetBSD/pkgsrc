2004b8923f453bd6dd47e4ebf8dc93112ea80db6
bacon <bacon@pkgsrc.org>
2022-03-15 21:06:32 +0000

    biology/biolibc: Update to 0.2.2
    Numerous bug fixes and enhancements
    Serveral new functions
    3 new classes
    Cleaned up some API slop
    Changes: https://github.com/auerlab/biolibc/tags

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2021/12/14 18:58:57 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/03/15 21:06:32 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.1
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.2
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
