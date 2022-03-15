8a0cd87c693a25ce48624ef96693bd4682327a3d
bsiegert <bsiegert@pkgsrc.org>
2022-03-15 20:13:45 +0000

    go118: add Go 1.18 (final release).
    Due to how close the freeze is, this will not be the default for the next
    quarterly release.
    
    There are lots of news, including support for Generics. Read the release
    notes for details.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.143 2022/03/07 08:44:52 bsiegert Exp $
+# $NetBSD: version.mk,v 1.144 2022/03/15 20:13:45 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,6 +6,7 @@
 #
 .include "go-vars.mk"
 
+GO118_VERSION=	1.18
 GO117_VERSION=	1.17.8
 GO116_VERSION=	1.16.15
 GO110_VERSION=	1.10.8
