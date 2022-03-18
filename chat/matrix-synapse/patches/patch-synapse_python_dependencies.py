425fe117fc47b6630c9ea9218d7f6964830e6776
gdt <gdt@pkgsrc.org>
2022-03-18 12:23:50 +0000

    =========================================================================== $NetBSD: MESSAGE,v 1.2 2021/04/15 15:54:00 jperkin Exp $
    To generate a Synapse configuration, run:
    
    cd ${SYNAPSE_DATA} &&
    ${PYTHONBIN} -m synapse.app.homeserver \
    	--server-name my.domain.name \
    	--config-path ${PKG_SYSCONFDIR}/homeserver.yaml \
    	--config-directory ${PKG_SYSCONFDIR} \
    	--generate-config \
    	--report-stats=no
    
    If you want to use Synapse with SQLite3 on NetBSD, you need to add
    PREFER_PKGSRC+= sqlite3 to mk.conf and rebuild databases/py-sqlite3.
    ===========================================================================

--- chat/matrix-synapse/patches/patch-synapse_python_dependencies.py
+++ chat/matrix-synapse/patches/patch-synapse_python_dependencies.py
@@ -1,18 +1,18 @@
-$NetBSD: patch-synapse_python_dependencies.py,v 1.6 2022/02/25 16:18:36 js Exp $
+$NetBSD: patch-synapse_python_dependencies.py,v 1.7 2022/03/18 12:23:50 gdt Exp $
 
 Patch out cryptography check: It doesn't need a version this high and it was
 added by upstream as an ugly hack to force a patched OpenSSL, in case it is
 linked statically.
 
---- synapse/python_dependencies.py.orig	2022-02-22 11:32:11.000000000 +0000
+--- synapse/python_dependencies.py.orig	2022-03-08 14:01:19.000000000 +0000
 +++ synapse/python_dependencies.py
-@@ -84,9 +84,6 @@ REQUIREMENTS = [
+@@ -77,9 +77,6 @@ REQUIREMENTS = [
      "Jinja2>=2.9",
      "bleach>=1.4.3",
      "typing-extensions>=3.7.4",
 -    # We enforce that we have a `cryptography` version that bundles an `openssl`
 -    # with the latest security patches.
 -    "cryptography>=3.4.7",
-     "ijson>=3.1",
+     # ijson 3.1.4 fixes a bug with "." in property names
+     "ijson>=3.1.4",
      "matrix-common~=1.1.0",
- ]
