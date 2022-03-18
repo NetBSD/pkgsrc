dab706df67c75d42ae60f3ffc43247d985c4bc89
taca <taca@pkgsrc.org>
2022-03-18 14:41:23 +0000

    lang/php80: update to 8.0.17
    17 Mar 2022, PHP 8.0.17
    
    - Core:
      . Fixed Haiku ZTS build. (David Carlier)
    
    - GD:
      . Fixed libpng warning when loading interlaced images. (Brett)
    
    - FPM:
      . Fixed bug #76109 (Unsafe access to fpm scoreboard).
        (Till Backhaus, Jakub Zelenka)
    
    - Iconv:
      . Fixed bug GH-7953 (ob_clean() only does not set Content-Encoding). (cmb)
      . Fixed bug GH-7980 (Unexpected result for iconv_mime_decode). (cmb)
    
    - MySQLnd:
      . Fixed bug GH-8058 (NULL pointer dereference in mysqlnd package). (Kamil Tekiela)
    
    - OPcache:
      . Fixed bug GH-8074 (Wrong type inference of range() result). (cmb)
    
    - Reflection:
      . Fixed bug GH-8080 (ReflectionClass::getConstants() depends on def. order).
        (cmb)
    
    - Zlib:
      . Fixed bug GH-7953 (ob_clean() only does not set Content-Encoding). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.358 2022/02/20 13:10:37 taca Exp $
+# $NetBSD: phpversion.mk,v 1.359 2022/03/18 14:41:23 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.28
-PHP80_VERSION=	8.0.16
+PHP80_VERSION=	8.0.17
 PHP81_VERSION=	8.1.3
 
 # Define initial release of major version.
