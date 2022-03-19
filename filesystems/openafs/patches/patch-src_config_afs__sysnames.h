e7e37207ea6e5d011804fb80f2b177384dffeaf0
jakllsch <jakllsch@pkgsrc.org>
2022-03-19 16:19:02 +0000

    Update to OpenAFS 1.8.8.1
    Numerous changes since 1.6.21 many years ago not enumerated here.
    
    Update lightly tested on NetBSD/amd64 8.0 and 9.2

--- filesystems/openafs/patches/patch-src_config_afs__sysnames.h
+++ filesystems/openafs/patches/patch-src_config_afs__sysnames.h
@@ -1,15 +1,13 @@
-$NetBSD: patch-src_config_afs__sysnames.h,v 1.4 2017/10/08 02:41:39 sevan Exp $
+$NetBSD: patch-src_config_afs__sysnames.h,v 1.5 2022/03/19 16:19:02 jakllsch Exp $
 
-- Add SYS_NAME_IDs for NetBSD 7 to 9 on arm/i386/amd64
+- Add SYS_NAME_IDs for NetBSD 8 to 9 on arm/i386/amd64
 
---- src/config/afs_sysnames.h.orig	2017-07-04 09:38:22.000000000 +0000
+--- src/config/afs_sysnames.h.orig	2021-12-09 17:07:41.000000000 +0000
 +++ src/config/afs_sysnames.h
-@@ -275,6 +275,13 @@
- #define SYS_NAME_ID_macppc_nbsd50       2541
- #define SYS_NAME_ID_amd64_nbsd60        2542
+@@ -297,6 +297,11 @@
  #define SYS_NAME_ID_i386_nbsd60         2543
-+#define SYS_NAME_ID_amd64_nbsd70        2544
-+#define SYS_NAME_ID_i386_nbsd70         2545
+ #define SYS_NAME_ID_amd64_nbsd70        2544
+ #define SYS_NAME_ID_i386_nbsd70         2545
 +#define SYS_NAME_ID_amd64_nbsd80        2546
 +#define SYS_NAME_ID_arm32_nbsd80        2547
 +#define SYS_NAME_ID_i386_nbsd80         2548
