88e607af0e50f2fa0acb6de543e33822f971a076
jakllsch <jakllsch@pkgsrc.org>
2022-03-19 16:19:02 +0000

    Update to OpenAFS 1.8.8.1
    Numerous changes since 1.6.21 many years ago not enumerated here.
    
    Update lightly tested on NetBSD/amd64 8.0 and 9.2

--- filesystems/openafs/patches/patch-src_config_param.nbsd90.h
+++ filesystems/openafs/patches/patch-src_config_param.nbsd90.h
@@ -1,21 +1,19 @@
-$NetBSD: patch-src_config_param.nbsd90.h,v 1.1 2020/09/20 22:15:00 jakllsch Exp $
+$NetBSD: patch-src_config_param.nbsd90.h,v 1.2 2022/03/19 16:19:02 jakllsch Exp $
 
 - Add support for NetBSD 9
 
---- src/config/param.nbsd90.h.orig	2020-09-20 22:11:06.272187304 +0000
+--- src/config/param.nbsd90.h.orig	2022-03-18 17:06:12.819973595 +0000
 +++ src/config/param.nbsd90.h
-@@ -41,6 +41,10 @@
- #define AFS_NBSD30_ENV 1
- #define AFS_NBSD40_ENV 1
+@@ -26,6 +26,8 @@
  #define AFS_NBSD50_ENV 1
-+#define AFS_NBSD60_ENV 1
-+#define AFS_NBSD70_ENV 1
+ #define AFS_NBSD60_ENV 1
+ #define AFS_NBSD70_ENV 1
 +#define AFS_NBSD80_ENV 1
 +#define AFS_NBSD90_ENV 1
  #undef  AFS_NONFSTRANS
  #define AFS_NONFSTRANS 1
  
-@@ -52,6 +56,8 @@
+@@ -37,6 +39,8 @@
  #define AFS_HAVE_STATVFS    1	/* System supports statvfs */
  #endif
  
