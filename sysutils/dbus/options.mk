f50b18e21826d6bce8b30ee205910ed4eef9702b
wiz <wiz@pkgsrc.org>
2022-03-20 13:37:13 +0000

    dbus: candidate fix for PR 56763
    To fix interaction between dbus and xfce4.
    
    Debugging by martin@
    
    Bump PKGREVISION.

--- sysutils/dbus/options.mk
+++ sysutils/dbus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.13 2019/11/04 21:28:43 rillig Exp $
+# $NetBSD: options.mk,v 1.14 2022/03/20 13:37:13 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbus
 PKG_SUPPORTED_OPTIONS+=	debug x11
@@ -37,6 +37,7 @@ CONFIGURE_ARGS+=	--disable-kqueue
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--with-x
 .  include "../../x11/libX11/buildlink3.mk"
+CPPFLAGS+=		-DDBUS_BUILD_X11
 BUILDLINK_DEPMETHOD.libXt=	build
 .  include "../../x11/libXt/buildlink3.mk"
 .else
