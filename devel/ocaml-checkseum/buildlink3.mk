ca1f7c6daf1bb20659c5d4667d7e3fab611b191e
jaapb <jaapb@pkgsrc.org>
2022-03-21 07:52:04 +0000

    Fixed PLIST error in devel/ocaml-checkseum, revbump.
    The new version of ocaml-dune puts files in a different place, so the
    PLIST for this package had to be updated.

--- devel/ocaml-checkseum/buildlink3.mk
+++ devel/ocaml-checkseum/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/07/01 12:32:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/03/21 07:52:04 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-checkseum
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-checkseum
 OCAML_CHECKSEUM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-checkseum+=	ocaml-checkseum>=0.0.3
-BUILDLINK_ABI_DEPENDS.ocaml-checkseum+=	ocaml-checkseum>=0.1.0
+BUILDLINK_ABI_DEPENDS.ocaml-checkseum+=	ocaml-checkseum>=0.3.1nb1
 BUILDLINK_PKGSRCDIR.ocaml-checkseum?=	../../devel/ocaml-checkseum
 
 .endif	# OCAML_CHECKSEUM_BUILDLINK3_MK
