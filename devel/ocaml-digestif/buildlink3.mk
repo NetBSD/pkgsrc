20680695836a94d9c234c46a2869331a9e2b5564
jaapb <jaapb@pkgsrc.org>
2022-03-21 07:59:31 +0000

    Fixed PLIST error in devel/ocaml-digestif and revbump.
    The new version of dune installs some files in a different place,
    so the PLIST has been fixed accordingly.

--- devel/ocaml-digestif/buildlink3.mk
+++ devel/ocaml-digestif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/07/01 12:42:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/03/21 07:59:31 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-digestif
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-digestif
 OCAML_DIGESTIF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-digestif+=	ocaml-digestif>=0.7.1
-BUILDLINK_ABI_DEPENDS.ocaml-digestif+=	ocaml-digestif>=0.7.2
+BUILDLINK_ABI_DEPENDS.ocaml-digestif+=	ocaml-digestif>=1.1.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-digestif?=	../../devel/ocaml-digestif
 
 .endif	# OCAML_DIGESTIF_BUILDLINK3_MK
