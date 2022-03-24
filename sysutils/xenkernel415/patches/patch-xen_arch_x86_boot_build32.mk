17c49ff1e0d71631ec1551dfd4cca25612dbf5b2
bsiegert <bsiegert@pkgsrc.org>
2022-03-24 18:59:43 +0000

    Pullup ticket #6597 - requested by bouyer sysutils/xenkernel415: security fix sysutils/xentools415: security fix
    Revisions pulled up:
    - sysutils/xenkernel415/Makefile                                1.4
    - sysutils/xenkernel415/distinfo                                1.5
    - sysutils/xenkernel415/patches/patch-xen_arch_x86_boot_build32.mk 1.2
    - sysutils/xentools415/Makefile                                 1.10
    - sysutils/xentools415/distinfo                                 1.7
    
    ---
       Module Name:	pkgsrc
       Committed By:	bouyer
       Date:		Fri Mar  4 17:54:08 UTC 2022
    
       Modified Files:
       	pkgsrc/sysutils/xenkernel415: Makefile distinfo
       	pkgsrc/sysutils/xenkernel415/patches:
       	    patch-xen_arch_x86_boot_build32.mk
       	pkgsrc/sysutils/xentools415: Makefile distinfo
    
       Log Message:
       Update xenkernel415 and xentools415 to 4.15.2
       Changes from 4.15.1 are bugfixes, some performance improvements and
       some security hardening. It also includes all fixes for XSA up to 395.

--- sysutils/xenkernel415/patches/patch-xen_arch_x86_boot_build32.mk
+++ sysutils/xenkernel415/patches/patch-xen_arch_x86_boot_build32.mk
@@ -1,9 +1,9 @@
-$NetBSD: patch-xen_arch_x86_boot_build32.mk,v 1.1 2021/04/18 12:31:26 bouyer Exp $
+$NetBSD: patch-xen_arch_x86_boot_build32.mk,v 1.1.6.1 2022/03/24 18:59:43 bsiegert Exp $
 linux's toolchain doesn't generate a .eh_frame section but NetBSD does.
 remove it.
 
---- xen/arch/x86/boot/build32.mk.orig	2018-04-17 19:21:31.000000000 +0200
-+++ xen/arch/x86/boot/build32.mk	2018-04-23 13:29:47.000000000 +0200
+--- xen/arch/x86/boot/build32.mk.orig	2022-01-31 10:42:09.000000000 +0100
++++ xen/arch/x86/boot/build32.mk	2022-03-03 14:12:56.486320239 +0100
 @@ -25,7 +25,7 @@
  				exit $$(expr $$idx + 1);; \
  			esac; \
@@ -11,5 +11,5 @@ remove it.
 -	$(OBJCOPY) -O binary -R .got.plt $< $@
 +	$(OBJCOPY) -O binary -R .got.plt -R .eh_frame $< $@
  
- %.lnk: %.o
+ %.lnk: %.o build32.lds
  	$(LD) $(LDFLAGS_DIRECT) -N -T build32.lds -o $@ $<
