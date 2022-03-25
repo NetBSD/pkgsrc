634c07f22febcd2eb3ab2c2085fa2d0c603afbb2
adam <adam@pkgsrc.org>
2022-03-25 17:55:19 +0000

    python310 py310-html-docs: updated to 3.10.4
    Python 3.10.4 final
    
    Core and Builtins
    
    bpo-46968: Check for the existence of the “sys/auxv.h” header in faulthandler to avoid compilation problems in systems where this header doesn’t exist. Patch by Pablo Galindo
    
    Library
    
    bpo-23691: Protect the re.finditer() iterator from re-entering.
    
    bpo-42369: Fix thread safety of zipfile._SharedFile.tell() to avoid a “zipfile.BadZipFile: Bad CRC-32 for file” exception when reading a ZipFile from multiple threads.
    
    bpo-38256: Fix binascii.crc32() when it is compiled to use zlib’c crc32 to work properly on inputs 4+GiB in length instead of returning the wrong result. The workaround prior to this was to always feed the function data in increments smaller than 4GiB or to just call the zlib module function.
    
    bpo-39394: A warning about inline flags not at the start of the regular expression now contains the position of the flag.
    
    bpo-47061: Deprecate the various modules listed by PEP 594:
    
    aifc, asynchat, asyncore, audioop, cgi, cgitb, chunk, crypt, imghdr, msilib, nntplib, nis, ossaudiodev, pipes, smtpd, sndhdr, spwd, sunau, telnetlib, uu, xdrlib
    
    bpo-2604: Fix bug where doctests using globals would fail when run multiple times.
    
    bpo-45997: Fix asyncio.Semaphore re-aquiring FIFO order.
    
    bpo-47022: The asynchat, asyncore and smtpd modules have been deprecated since at least Python 3.6. Their documentation and deprecation warnings and have now been updated to note they will removed in Python 3.12 (PEP 594).
    
    bpo-46421: Fix a unittest issue where if the command was invoked as python -m unittest and the filename(s) began with a dot (.), a ValueError is returned.
    
    bpo-40296: Fix supporting generic aliases in pydoc.

--- lang/python310/dist.mk
+++ lang/python310/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.4 2022/03/19 18:59:40 adam Exp $
+# $NetBSD: dist.mk,v 1.5 2022/03/25 17:55:19 adam Exp $
 
-PY_DISTVERSION=	3.10.3
+PY_DISTVERSION=	3.10.4
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python310/distinfo
