79fcd6c6f195298dabce78062d8ddc7268f4c873
wiz <wiz@pkgsrc.org>
2022-03-25 10:37:30 +0000

    newsbeuter: fix NetBSD iconv change
    Should fix build on NetBSD<9.99.17

--- news/newsbeuter/patches/patch-src_utils.cpp
+++ news/newsbeuter/patches/patch-src_utils.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_utils.cpp,v 1.1 2021/12/20 07:19:42 nia Exp $
+$NetBSD: patch-src_utils.cpp,v 1.2 2022/03/25 10:37:30 wiz Exp $
 
 Adapt to NetBSD iconv prototype change.
 
 --- src/utils.cpp.orig	2015-02-19 10:56:59.000000000 +0000
 +++ src/utils.cpp
-@@ -37,6 +37,15 @@ GCRY_THREAD_OPTION_PTHREAD_IMPL;
+@@ -37,6 +37,13 @@ GCRY_THREAD_OPTION_PTHREAD_IMPL;
  #include <openssl/crypto.h>
  #endif
  
@@ -12,15 +12,13 @@ Adapt to NetBSD iconv prototype change.
 +#include <sys/param.h>
 +#if __NetBSD_Prereq__(9,99,17)
 +#define NETBSD_POSIX_ICONV 1
-+#else
-+#define NETBSD_POSIX_ICONV 0
 +#endif
 +#endif
 +
  namespace newsbeuter {
  
  std::vector<std::string> utils::tokenize_quoted(const std::string& str, std::string delimiters) {
-@@ -274,7 +283,7 @@ std::string utils::convert_text(const st
+@@ -274,7 +281,7 @@ std::string utils::convert_text(const st
  	 * of all the Unix-like systems around there, only Linux/glibc seems to
  	 * come with a SuSv3-conforming iconv implementation.
  	 */
