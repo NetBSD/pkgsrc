82138e1fc5433bbc510a4f78188ed15f9a1855ef
tnn <tnn@pkgsrc.org>
2022-03-26 22:50:18 +0000

    abseil: fix build on SunOS

--- devel/abseil/patches/patch-absl_debugging_internal_vdso__support.cc
+++ devel/abseil/patches/patch-absl_debugging_internal_vdso__support.cc
@@ -1,9 +1,10 @@
-$NetBSD: patch-absl_debugging_internal_vdso__support.cc,v 1.2 2022/03/06 09:34:06 wiz Exp $
+$NetBSD: patch-absl_debugging_internal_vdso__support.cc,v 1.3 2022/03/26 22:50:18 tnn Exp $
 
 Support NetBSD.
 https://github.com/abseil/abseil-cpp/pull/1121
+No support for SunOS yet, so disable for the time being.
 
---- absl/debugging/internal/vdso_support.cc.orig	2021-11-18 10:00:05.006467401 +0000
+--- absl/debugging/internal/vdso_support.cc.orig	2021-11-03 15:26:14.000000000 +0000
 +++ absl/debugging/internal/vdso_support.cc
 @@ -50,6 +50,10 @@
  #define AT_SYSINFO_EHDR 33  // for crosstoolv10
@@ -16,7 +17,17 @@ https://github.com/abseil/abseil-cpp/pull/1121
  #if defined(__FreeBSD__)
  using Elf64_auxv_t = Elf64_Auxinfo;
  using Elf32_auxv_t = Elf32_Auxinfo;
-@@ -104,8 +108,13 @@ const void *VDSOSupport::Init() {
+@@ -93,6 +97,9 @@ const void *VDSOSupport::Init() {
+     }
+   }
+ #endif  // ABSL_HAVE_GETAUXVAL
++#if defined(__sun)
++      return nullptr;
++#else // !__sun
+   if (vdso_base_.load(std::memory_order_relaxed) == kInvalidBase) {
+     int fd = open("/proc/self/auxv", O_RDONLY);
+     if (fd == -1) {
+@@ -104,8 +111,13 @@ const void *VDSOSupport::Init() {
      ElfW(auxv_t) aux;
      while (read(fd, &aux, sizeof(aux)) == sizeof(aux)) {
        if (aux.a_type == AT_SYSINFO_EHDR) {
@@ -30,3 +41,11 @@ https://github.com/abseil/abseil-cpp/pull/1121
          break;
        }
      }
+@@ -127,6 +139,7 @@ const void *VDSOSupport::Init() {
+   // from assigning to getcpu_fn_ more than once.
+   getcpu_fn_.store(fn, std::memory_order_relaxed);
+   return vdso_base_.load(std::memory_order_relaxed);
++#endif // __sun
+ }
+ 
+ const void *VDSOSupport::SetBase(const void *base) {
