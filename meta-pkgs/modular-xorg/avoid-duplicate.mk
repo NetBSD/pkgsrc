d0a656cfa5f0e1cd6e2cd04e1a0f93909433e54a
nia <nia@pkgsrc.org>
2022-03-26 19:48:21 +0000

    avoid-duplicate.mk: defang per tech-pkg thread

--- meta-pkgs/modular-xorg/avoid-duplicate.mk
+++ meta-pkgs/modular-xorg/avoid-duplicate.mk
@@ -1,9 +1,9 @@
-# $NetBSD: avoid-duplicate.mk,v 1.2 2022/02/13 10:44:38 nia Exp $
+# $NetBSD: avoid-duplicate.mk,v 1.3 2022/03/26 19:48:21 nia Exp $
 
 BUILTIN_PKG=		${X11_BUILTIN_NAME}
 
-.include "${.INCLUDEDFROMDIR}/builtin.mk"
+#.include "${.INCLUDEDFROMDIR}/builtin.mk"
 
-.if ${X11_TYPE} == "native" && ${USE_BUILTIN.${X11_BUILTIN_NAME}} == "yes"
-PKG_SKIP_REASON+=	"Package set is using native X11 component"
-.endif
+#.if ${X11_TYPE} == "native" && ${USE_BUILTIN.${X11_BUILTIN_NAME}} == "yes"
+#PKG_SKIP_REASON+=	"Package set is using native X11 component"
+#.endif
