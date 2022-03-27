6b6d7b24e976e95f0feb4656a7129e083d25aba4
tnn <tnn@pkgsrc.org>
2022-03-27 14:05:36 +0000

    postgresql-promscale_extension: fix build on SunOS

--- databases/postgresql-promscale_extension/patches/patch-pgx_pgx-pg-sys_src_submodules_mod.rs
+++ databases/postgresql-promscale_extension/patches/patch-pgx_pgx-pg-sys_src_submodules_mod.rs
@@ -1,13 +1,14 @@
-$NetBSD: patch-pgx_pgx-pg-sys_src_submodules_mod.rs,v 1.2 2021/12/10 11:44:19 tnn Exp $
+$NetBSD: patch-pgx_pgx-pg-sys_src_submodules_mod.rs,v 1.3 2022/03/27 14:05:36 tnn Exp $
 
-NetBSD support
+NetBSD, SunOS support
 
 --- ../vendor/pgx-pg-sys-0.2.0/src/submodules/mod.rs.orig	1973-11-29 21:33:09.000000000 +0000
 +++ ../vendor/pgx-pg-sys-0.2.0/src/submodules/mod.rs
-@@ -20,6 +20,15 @@ extern "C" {
+@@ -20,7 +20,16 @@ extern "C" {
      ) -> std::os::raw::c_int;
  }
  
+-#[cfg(any(target_os = "macos", target_os = "freebsd"))]
 +#[cfg(target_os = "netbsd")]
 +extern "C" {
 +    #[link_name = "__sigsetjmp14"]
@@ -17,6 +18,7 @@ NetBSD support
 +    ) -> std::os::raw::c_int;
 +}
 +
- #[cfg(any(target_os = "macos", target_os = "freebsd"))]
++#[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "solaris", target_os = "illumos"))]
  extern "C" {
      pub(crate) fn sigsetjmp(
+         env: *mut crate::sigjmp_buf,
