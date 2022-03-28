fed0a97eaa5e211a29482d06911bb66019986715
tnn <tnn@pkgsrc.org>
2022-03-28 10:56:15 +0000

    {f,h,l,n,p}*/*: revbump(1) for libsndfile

--- net/knewstuff/buildlink3.mk
+++ net/knewstuff/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2021/11/15 22:53:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2022/03/28 10:56:18 tnn Exp $
 
 BUILDLINK_TREE+=	knewstuff
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	knewstuff
 KNEWSTUFF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.knewstuff+=	knewstuff>=5.21.0
-BUILDLINK_ABI_DEPENDS.knewstuff?=	knewstuff>=5.80.0nb3
+BUILDLINK_ABI_DEPENDS.knewstuff?=	knewstuff>=5.80.0nb5
 BUILDLINK_PKGSRCDIR.knewstuff?=		../../net/knewstuff
 
 .include "../../devel/kio/buildlink3.mk"
