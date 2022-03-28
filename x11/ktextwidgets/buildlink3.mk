d19736e1065360c747bacf06f0f553c5b186ec0c
tnn <tnn@pkgsrc.org>
2022-03-28 10:59:47 +0000

    x*/*: revbump(1) for libsndfile

--- x11/ktextwidgets/buildlink3.mk
+++ x11/ktextwidgets/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2021/11/15 22:54:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2022/03/28 10:59:49 tnn Exp $
 
 BUILDLINK_TREE+=	ktextwidgets
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ktextwidgets
 KTEXTWIDGETS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ktextwidgets+=	ktextwidgets>=5.19.0
-BUILDLINK_ABI_DEPENDS.ktextwidgets?=	ktextwidgets>=5.80.0nb3
+BUILDLINK_ABI_DEPENDS.ktextwidgets?=	ktextwidgets>=5.80.0nb5
 BUILDLINK_PKGSRCDIR.ktextwidgets?=	../../x11/ktextwidgets
 
 .include "../../devel/kservice/buildlink3.mk"
