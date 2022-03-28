1e9d2271d7690ffaa5d86073a0e055ceb1e952d3
wiz <wiz@pkgsrc.org>
2022-03-28 21:57:36 +0000

    py-psutil: remove obsolete patch chunk
    Ride PKGREVISION bump.

--- sysutils/py-psutil/patches/patch-psutil___psbsd.py
+++ sysutils/py-psutil/patches/patch-psutil___psbsd.py
@@ -1,18 +1,10 @@
-$NetBSD: patch-psutil___psbsd.py,v 1.6 2022/03/28 21:50:43 wiz Exp $
+$NetBSD: patch-psutil___psbsd.py,v 1.7 2022/03/28 21:57:36 wiz Exp $
 
-Stop using SDEAD as it is no longer available.
+Fix reporting of cached memory on NetBSD.
+https://github.com/giampaolo/psutil/pull/2088
 
 --- psutil/_psbsd.py.orig	2021-12-15 09:34:00.000000000 +0000
 +++ psutil/_psbsd.py
-@@ -61,7 +61,7 @@ elif OPENBSD:
-         # equivalent. Also it appears there's no equivalent of
-         # psutil.STATUS_DEAD. SDEAD really means STATUS_ZOMBIE.
-         # cext.SZOMB: _common.STATUS_ZOMBIE,
--        cext.SDEAD: _common.STATUS_ZOMBIE,
-+        # cext.SDEAD: _common.STATUS_ZOMBIE,
-         cext.SZOMB: _common.STATUS_ZOMBIE,
-         # From http://www.eecs.harvard.edu/~margo/cs161/videos/proc.h.txt
-         # OpenBSD has SRUN and SONPROC: SRUN indicates that a process
 @@ -189,6 +189,8 @@ def virtual_memory():
                      buffers = int(line.split()[1]) * 1024
                  elif line.startswith(b'MemShared:'):
