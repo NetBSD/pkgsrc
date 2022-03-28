c6135e8bc6c50e20b5fc37f427ee4403cd197220
tnn <tnn@pkgsrc.org>
2022-03-28 10:59:47 +0000

    x*/*: revbump(1) for libsndfile

--- x11/kactivities5/buildlink3.mk
+++ x11/kactivities5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2021/11/15 22:54:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2022/03/28 10:59:48 tnn Exp $
 
 BUILDLINK_TREE+=	kactivities5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kactivities5
 KACTIVITIES5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kactivities5+=	kactivities5>=5.21.0
-BUILDLINK_ABI_DEPENDS.kactivities5?=	kactivities5>=5.80.0nb5
+BUILDLINK_ABI_DEPENDS.kactivities5?=	kactivities5>=5.80.0nb8
 BUILDLINK_PKGSRCDIR.kactivities5?=	../../x11/kactivities5
 
 .include "../../devel/kio/buildlink3.mk"
--- x11/plasma-framework/buildlink3.mk
+++ x11/plasma-framework/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2021/11/15 22:54:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2022/03/28 10:59:50 tnn Exp $
 
 BUILDLINK_TREE+=	plasma-framework
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	plasma-framework
 PLASMA_FRAMEWORK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.plasma-framework+=	plasma-framework>=5.21.0
-BUILDLINK_ABI_DEPENDS.plasma-framework?=	plasma-framework>=5.80.0nb3
+BUILDLINK_ABI_DEPENDS.plasma-framework?=	plasma-framework>=5.80.0nb5
 BUILDLINK_PKGSRCDIR.plasma-framework?=		../../x11/plasma-framework
 
 .include "../../devel/kdeclarative/buildlink3.mk"
