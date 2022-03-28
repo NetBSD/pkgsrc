536ce33d1f65db746d121fafbad0840e95eb3906
tnn <tnn@pkgsrc.org>
2022-03-28 10:45:17 +0000

    m*/*: revbump(1) for libsndfile

--- misc/libkeduvocdocument/buildlink3.mk
+++ misc/libkeduvocdocument/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2021/11/15 22:53:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2022/03/28 10:45:21 tnn Exp $
 
 BUILDLINK_TREE+=	libkeduvocdocument
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkeduvocdocument
 LIBKEDUVOCDOCUMENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkeduvocdocument+=	libkeduvocdocument>=19.08.3
-BUILDLINK_ABI_DEPENDS.libkeduvocdocument?=	libkeduvocdocument>=20.12.3nb3
+BUILDLINK_ABI_DEPENDS.libkeduvocdocument?=	libkeduvocdocument>=20.12.3nb5
 BUILDLINK_PKGSRCDIR.libkeduvocdocument?=	../../misc/libkeduvocdocument
 
 .include "../../devel/kio/buildlink3.mk"
--- multimedia/kmediaplayer/buildlink3.mk
+++ multimedia/kmediaplayer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/11/15 22:53:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/03/28 10:45:24 tnn Exp $
 
 BUILDLINK_TREE+=	kmediaplayer
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kmediaplayer
 KMEDIAPLAYER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kmediaplayer+=	kmediaplayer>=5.80.0
-BUILDLINK_ABI_DEPENDS.kmediaplayer?=	kmediaplayer>=5.80.0nb1
+BUILDLINK_ABI_DEPENDS.kmediaplayer?=	kmediaplayer>=5.80.0nb3
 BUILDLINK_PKGSRCDIR.kmediaplayer?=	../../multimedia/kmediaplayer
 
 .include "../../devel/kparts/buildlink3.mk"
