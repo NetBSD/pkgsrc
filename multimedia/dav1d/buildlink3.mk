30a96ced9440b5b049da14bc86f6c473b8ce1a07
wiz <wiz@pkgsrc.org>
2022-03-30 09:20:26 +0000

    *: recursive bump for dav1d shlib bump

--- multimedia/dav1d/buildlink3.mk
+++ multimedia/dav1d/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/02/19 18:01:39 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/03/30 09:20:26 wiz Exp $
 
 BUILDLINK_TREE+=	dav1d
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	dav1d
 DAV1D_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.dav1d+=	dav1d>=0.8.1
+BUILDLINK_ABI_DEPENDS.dav1d?=	dav1d>=1.0.0
 BUILDLINK_PKGSRCDIR.dav1d?=	../../multimedia/dav1d
 .endif	# DAV1D_BUILDLINK3_MK
 
