d2ab36a84b59b5805d3099b79555b65054dfa3c7
wiz <wiz@pkgsrc.org>
2022-04-01 08:06:26 +0000

    botan2: fix bl3.mk

--- security/botan2/buildlink3.mk
+++ security/botan2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/04/01 08:01:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/04/01 08:06:26 wiz Exp $
 
 BUILDLINK_TREE+=	botan
 
@@ -7,7 +7,7 @@ BOTAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.botan+=	botan>=2.11
 BUILDLINK_ABI_DEPENDS.botan?=	botan>=2.19.1
-BUILDLINK_PKGSRCDIR.botan?=	../../security/botan-devel
+BUILDLINK_PKGSRCDIR.botan?=	../../security/botan2
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
