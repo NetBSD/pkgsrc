5cd3813f0a81b437cb20d73cf64e46d9888e0b35
wiz <wiz@pkgsrc.org>
2022-04-01 08:01:11 +0000

    security/botan2: import botan-2.19.1
    Reimported from security/botan-devel.
    
    Botan is a crypto library written in C++. It provides a variety of
    cryptographic algorithms, including common ones such as AES, MD5, SHA,
    HMAC, RSA, Diffie-Hellman, DSA, and ECDSA, as well as many others that
    are more obscure or specialized. It also offers X.509v3 certificates
    and CRLs, and PKCS #10 certificate requests. A message processing
    system that uses a filter/pipeline metaphor allows for many common
    cryptographic tasks to be completed with just a few lines of code.
    Assembly optimizations for common CPUs, including x86, x86-64, and
    PowerPC, offers further speedups for critical tasks such as SHA-1
    hashing and multiple precision integer operations.
    
    This package contains major version 2 of the library.
    
    The version contains a much improved TLS infrastructure.  It also
    depends on C++11.

---
+++ security/botan2/patches/patch-src_lib_utils_os__utils.cpp
@@ -0,0 +1,29 @@
+$NetBSD: patch-src_lib_utils_os__utils.cpp,v 1.1 2022/04/01 08:01:12 wiz Exp $
+
+Add check for ability to do mlock() on SunOS.
+
+--- src/lib/utils/os_utils.cpp.orig	2019-07-15 18:14:39.932394200 +0000
++++ src/lib/utils/os_utils.cpp
+@@ -325,7 +325,21 @@ size_t OS::system_page_size()
+ 
+ size_t OS::get_memory_locking_limit()
+    {
+-#if defined(BOTAN_TARGET_OS_HAS_POSIX1) && defined(BOTAN_TARGET_OS_HAS_POSIX_MLOCK) && defined(RLIMIT_MEMLOCK)
++#if defined(__sun)
++   priv_set_t *priv_set = priv_allocset();
++   if (priv_set == nullptr)
++     return 0;
++   bool can_mlock = false;
++
++   if(getppriv(PRIV_EFFECTIVE, priv_set) == 0)
++     can_mlock = priv_ismember(priv_set, PRIV_PROC_LOCK_MEMORY);
++
++   priv_freeset(priv_set);
++
++   /* XXX how to obtain the real limit? */
++   size_t mlock_requested = BOTAN_MLOCK_ALLOCATOR_MAX_LOCKED_KB;
++   return can_mlock ? std::min<size_t>(512 * 1024, mlock_requested) : 0;
++#elif defined(BOTAN_TARGET_OS_HAS_POSIX1) && defined(BOTAN_TARGET_OS_HAS_POSIX_MLOCK) && defined(RLIMIT_MEMLOCK)
+    /*
+    * If RLIMIT_MEMLOCK is not defined, likely the OS does not support
+    * unprivileged mlock calls.
