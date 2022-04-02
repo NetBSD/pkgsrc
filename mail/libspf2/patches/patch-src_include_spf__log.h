9ca2f78856cf62a618b6119fd2d2e29e662dada3
spz <spz@pkgsrc.org>
2022-04-02 21:41:38 +0000

    libspf2-1.2.11 from https://github.com/shevek/libspf2/commit/c37b7c13c30e225183899364b9f2efdfa85552ef
    fixes CVE-2021-33912, CVE-2021-33913 and CVE-2021-33914

--- mail/libspf2/patches/patch-src_include_spf__log.h
+++ mail/libspf2/patches/patch-src_include_spf__log.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_include_spf__log.h,v 1.1 2012/12/22 02:25:57 joerg Exp $
+$NetBSD: patch-src_include_spf__log.h,v 1.2 2022/04/02 21:41:39 spz Exp $
 
 Dropping a trailing comma when __VA_ARGS__ would be empty is a GCC extension.
 
---- src/include/spf_log.h.orig	2012-12-21 21:29:20.000000000 +0000
+--- src/include/spf_log.h.orig	2021-06-09 05:43:12.000000000 +0000
 +++ src/include/spf_log.h
 @@ -60,10 +60,10 @@ void SPF_debugv( const char *file, int l
  
  #if defined( __STDC_VERSION__ ) && __STDC_VERSION__ >= 199901L
  
--#define SPF_errorf(format, ... ) SPF_errorx( __FILE__, __LINE__, format, __VA_ARGS__ )
--#define SPF_warningf(format, ... ) SPF_warningx( __FILE__, __LINE__, format, __VA_ARGS__ )
--#define SPF_infof(format, ... ) SPF_infox( __FILE__, __LINE__, format, __VA_ARGS__ )
--#define SPF_debugf(format, ... ) SPF_debugx( __FILE__, __LINE__, format, __VA_ARGS__ )
+-#define SPF_errorf(format, ... ) SPF_errorx( __FILE__, __LINE__, format, ##__VA_ARGS__ )
+-#define SPF_warningf(format, ... ) SPF_warningx( __FILE__, __LINE__, format, ##__VA_ARGS__ )
+-#define SPF_infof(format, ... ) SPF_infox( __FILE__, __LINE__, format, ##__VA_ARGS__ )
+-#define SPF_debugf(format, ... ) SPF_debugx( __FILE__, __LINE__, format, ##__VA_ARGS__ )
 +#define SPF_errorf(...) SPF_errorx( __FILE__, __LINE__, __VA_ARGS__ )
 +#define SPF_warningf(...) SPF_warningx( __FILE__, __LINE__, __VA_ARGS__ )
 +#define SPF_infof(...) SPF_infox( __FILE__, __LINE__, __VA_ARGS__ )
