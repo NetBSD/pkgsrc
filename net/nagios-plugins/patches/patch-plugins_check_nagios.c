2e4c8871b52e49baf4f246ed5b9bebc8360d9a6d
nia <nia@pkgsrc.org>
2022-04-02 10:35:35 +0000

    nagios-plugins: Fix building on NetBSD-current.

--- net/nagios-plugins/patches/patch-plugins_check_nagios.c
+++ net/nagios-plugins/patches/patch-plugins_check_nagios.c
@@ -1,11 +1,10 @@
-$NetBSD: patch-plugins_check_nagios.c,v 1.1 2018/08/03 14:03:09 manu Exp $
+$NetBSD: patch-plugins_check_nagios.c,v 1.2 2022/04/02 10:35:35 nia Exp $
 
 Crash fix: initialize variable, check bounds on string copy.
 
---- plugins/check_nagios.c.orig
+--- plugins/check_nagios.c.orig	2017-01-19 16:01:31.000000000 +0000
 +++ plugins/check_nagios.c
-@@ -71,9 +71,9 @@
- 	char procstat[8];
+@@ -72,7 +72,7 @@ main (int argc, char **argv)
  #ifdef PS_USES_PROCETIME
  	char procetime[MAX_INPUT_BUFFER];
  #endif /* PS_USES_PROCETIME */
@@ -14,9 +13,7 @@ Crash fix: initialize variable, check bounds on string copy.
  	char *procargs;
  	int pos, cols;
  	int expected_cols = PS_COLS - 1;
- 	const char *zombie = "Z";
-@@ -143,9 +143,10 @@
- 
+@@ -144,7 +144,8 @@ main (int argc, char **argv)
  			/* Some ps return full pathname for command. This removes path */
  			temp_string = strtok ((char *)procprog, "/");
  			while (temp_string) {
@@ -26,4 +23,3 @@ Crash fix: initialize variable, check bounds on string copy.
  				temp_string = strtok (NULL, "/");
  			}
  
- 			/* May get empty procargs */
