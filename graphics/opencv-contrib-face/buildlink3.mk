e39295c91d1dd54b26ed4b796df2f4147dace633
adam <adam@pkgsrc.org>
2022-04-03 18:50:16 +0000

    revbump for devel/protobuf

--- graphics/opencv-contrib-face/buildlink3.mk
+++ graphics/opencv-contrib-face/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/11/11 12:02:46 kim Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/04/03 18:50:17 adam Exp $
 
 BUILDLINK_TREE+=	opencv-contrib-face
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	opencv-contrib-face
 OPENCV_CONTRIB_FACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencv-contrib-face+=	opencv-contrib-face>=3.0.0
-BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.16nb2
+BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.16nb3
 BUILDLINK_PKGSRCDIR.opencv-contrib-face?=	../../graphics/opencv-contrib-face
 
 .include "../../graphics/opencv/buildlink3.mk"
