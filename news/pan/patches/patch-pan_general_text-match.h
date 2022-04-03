de89896a6eb79f29857c82e824acc058d26b618d
rhialto <rhialto@pkgsrc.org>
2022-04-03 19:33:11 +0000

    news/pan: update to 0.150.
    Switch back to fetching the distfile from gitlab.gnome.org, since
    a) the configure.ac file no longer uses the problematic "$Format:%d$"
       which can change if commits are added (only the stable
       GIT_HASH='$Format:%h$' is left). Hopefully this fixes the problem.
    b) the upstream of the pre-made tarball is getting behind
    Also there are some pkglint fixes.
    
    The main points of this release are:
    - Dropped support for GMime2. Only Gmime3 is supported
    - Gtk3 is now default build, Gtk2 is deprecated
    
    15fcc65 set maximum number of serveur connections to 20 (Dominique Dumont)
    431f56f Nuke $GIT_REMOTE if not obtained by git config (Petr Kovar)

--- news/pan/patches/patch-pan_general_text-match.h
+++ news/pan/patches/patch-pan_general_text-match.h
@@ -1,5 +1,7 @@
-$NetBSD: patch-pan_general_text-match.h,v 1.3 2021/09/19 18:02:37 rhialto Exp $
+$NetBSD: patch-pan_general_text-match.h,v 1.4 2022/04/03 19:33:11 rhialto Exp $
+
 Avoid clash with host defined ERR (on SunOS)
+
 --- pan/general/text-match.h.orig	2012-06-29 22:24:54.000000000 +0000
 +++ pan/general/text-match.h
 @@ -108,7 +108,7 @@ private:
