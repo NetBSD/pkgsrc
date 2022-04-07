198c65bf77afa8ffff32658041d612a760bc3348
riastradh <riastradh@pkgsrc.org>
2022-04-07 19:08:40 +0000

    devel/nss: Patch ctype(3) abuse.

--- devel/nss/patches/patch-nss_lib_util_utilpars.c
+++ devel/nss/patches/patch-nss_lib_util_utilpars.c
@@ -1,10 +1,21 @@
-$NetBSD: patch-nss_lib_util_utilpars.c,v 1.1 2018/02/24 11:35:48 ryoon Exp $
+$NetBSD: patch-nss_lib_util_utilpars.c,v 1.2 2022/04/07 19:08:40 riastradh Exp $
 
 Revert Bug 1377940 to fix misc/libreoffice
 
---- nss/lib/util/utilpars.c.orig	2018-01-18 14:19:59.000000000 +0000
+Fix ctype(3) abuse: https://bugzilla.mozilla.org/show_bug.cgi?id=1246768
+
+--- nss/lib/util/utilpars.c.orig	2022-03-03 10:18:53.000000000 +0000
 +++ nss/lib/util/utilpars.c
-@@ -1111,8 +1111,12 @@ _NSSUTIL_EvaluateConfigDir(const char *c
+@@ -274,7 +274,7 @@ NSSUTIL_ArgDecodeNumber(const char *num)
+     }
+ 
+     for (; *num; num++) {
+-        if (isdigit(*num)) {
++        if (isdigit((unsigned char)*num)) {
+             digit = *num - '0';
+         } else if ((*num >= 'a') && (*num <= 'f')) {
+             digit = *num - 'a' + 10;
+@@ -1197,8 +1197,12 @@ _NSSUTIL_EvaluateConfigDir(const char *c
      NSSDBType dbType;
      PRBool checkEnvDefaultDB = PR_FALSE;
      *appName = NULL;
