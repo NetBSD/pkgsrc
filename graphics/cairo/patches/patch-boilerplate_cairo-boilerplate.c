034a73ab882f046e7e6d0716a8f215c6817bf1e4
riastradh <riastradh@pkgsrc.org>
2022-04-07 10:57:57 +0000

    graphics/cairo: Cite upstream submission of ctype(3) patch.

--- graphics/cairo/patches/patch-boilerplate_cairo-boilerplate.c
+++ graphics/cairo/patches/patch-boilerplate_cairo-boilerplate.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-boilerplate_cairo-boilerplate.c,v 1.1 2022/04/05 10:06:41 riastradh Exp $
+$NetBSD: patch-boilerplate_cairo-boilerplate.c,v 1.2 2022/04/07 10:57:57 riastradh Exp $
 
 Fix ctype(3) abuse.
+https://gitlab.freedesktop.org/cairo/cairo/-/merge_requests/309
 
 --- boilerplate/cairo-boilerplate.c.orig	2018-08-17 01:10:53.000000000 +0000
 +++ boilerplate/cairo-boilerplate.c
