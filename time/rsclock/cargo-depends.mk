97858cc5801114ce791c63a5b2fff5f1a0a088e5
pin <pin@pkgsrc.org>
2022-04-08 10:21:52 +0000

    time/rsclock
    rsClock is a simple terminal clock.

---
+++ time/rsclock/cargo-depends.mk
@@ -0,0 +1,18 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2022/04/08 10:21:52 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	autocfg-0.1.6
+CARGO_CRATE_DEPENDS+=	bitflags-1.3.2
+CARGO_CRATE_DEPENDS+=	chrono-0.4.19
+CARGO_CRATE_DEPENDS+=	libc-0.2.121
+CARGO_CRATE_DEPENDS+=	num-integer-0.1.41
+CARGO_CRATE_DEPENDS+=	num-traits-0.2.8
+CARGO_CRATE_DEPENDS+=	numtoa-0.1.0
+CARGO_CRATE_DEPENDS+=	redox_syscall-0.1.56
+CARGO_CRATE_DEPENDS+=	redox_syscall-0.2.11
+CARGO_CRATE_DEPENDS+=	redox_termios-0.1.1
+CARGO_CRATE_DEPENDS+=	termion-1.5.6
+CARGO_CRATE_DEPENDS+=	time-0.1.44
+CARGO_CRATE_DEPENDS+=	wasi-0.10.0+wasi-snapshot-preview1
+CARGO_CRATE_DEPENDS+=	winapi-0.3.8
+CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
+CARGO_CRATE_DEPENDS+=	winapi-x86_64-pc-windows-gnu-0.4.0
