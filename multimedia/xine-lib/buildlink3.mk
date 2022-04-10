bfaa40a8a574e4f7d34532c21f5aace25ed85377
nia <nia@pkgsrc.org>
2022-04-10 16:47:32 +0000

    xine-lib: update to 1.2.12
    xine-lib (1.2.12) 2022-03-09
      * Add string tree library (generic info parser).
      * Add OpenSL ES audio output plugin.
      * Add mpeg-dash input plugin.
      * Add crypto input plugin.
      * Add VAAPI support to opengl2 video output (requires using EGL).
      * Better hls support.
      * Improved DVB/live stream timing.
      * Improved BT.2020 support.
      * Fix compatibility with ffmpeg 5.0.
      * Fix RSS parser.
      * Fix output parameter setting (audio amplification, video hue etc.).
      * Fix AC3 demuxer.
      * Diverse small fixes and optimizations.
      * Update german translation.
      * Code cleanup.
      * Build fixes.

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2021/10/09 15:35:10 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2022/04/10 16:47:32 nia Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
 BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.11nb6
 BUILDLINK_PKGSRCDIR.xine-lib?=		../../multimedia/xine-lib
 
-.include "../../multimedia/ffmpeg4/buildlink3.mk"
+.include "../../multimedia/ffmpeg5/buildlink3.mk"
 .endif # XINE_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xine-lib
