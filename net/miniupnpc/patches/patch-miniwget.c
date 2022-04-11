24af1de1cd0af5b231b0728f287c1fb9b36e1856
nikita <nikita@pkgsrc.org>
2022-04-11 10:04:38 +0000

    miniupnpc: Update to 2.2.3
    Changelog:
    
    2021/08/13:
      Change directory structure : include/ and src/ directories.

--- net/miniupnpc/patches/patch-miniwget.c
+++ net/miniupnpc/patches/patch-miniwget.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-miniwget.c,v 1.1 2020/11/23 20:15:19 nia Exp $
+$NetBSD: patch-miniwget.c,v 1.2 2022/04/11 10:04:38 nikita Exp $
 
 ctype arguments must be unsigned char.
 
---- miniwget.c.orig	2020-11-09 19:43:34.000000000 +0000
-+++ miniwget.c
+--- src/miniwget.c.orig	2020-11-09 19:43:34.000000000 +0000
++++ src/miniwget.c
 @@ -249,7 +249,7 @@ getHTTPResponse(SOCKET s, int * size, in
  						if(buf[i] == '\r') i++;
  						if(i<n && buf[i] == '\n') i++;
--- net/miniupnpc/patches/patch-upnpc.c
+++ net/miniupnpc/patches/patch-upnpc.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-upnpc.c,v 1.1 2020/11/23 20:15:19 nia Exp $
+$NetBSD: patch-upnpc.c,v 1.2 2022/04/11 10:04:38 nikita Exp $
 
 ctype arguments must be unsigned char.
 
---- upnpc.c.orig	2020-11-09 19:43:35.000000000 +0000
-+++ upnpc.c
+--- src/upnpc.c.orig	2020-11-09 19:43:35.000000000 +0000
++++ src/upnpc.c
 @@ -53,7 +53,7 @@ int is_int(char const* s)
  		return 0;
  	while(*s) {
