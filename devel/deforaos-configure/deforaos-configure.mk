ffbc5b88081a3b328a7fbe8ce81878aa1d0e3419
khorben <khorben@pkgsrc.org>
2022-04-12 23:32:03 +0000

    deforaos-cpp: re-generate the Makefiles before building
    This makes use of a new helper for DeforaOS configure, found in
    devel/deforaos-configure/deforaos-configure.mk.
    
    Bumps PKGREVISION for DeforaOS libSystem update.

---
+++ devel/deforaos-configure/deforaos-configure.mk
@@ -0,0 +1,17 @@
+# $NetBSD: deforaos-configure.mk,v 1.1 2022/04/12 23:32:03 khorben Exp $
+#
+# used by devel/deforaos-cpp/Makefile
+
+BUILD_DEPENDS+=	deforaos-configure-[0-9]*:../../devel/deforaos-configure
+
+DEFORAOS_CONFIGURE?=		${PREFIX}/bin/configure
+DEFORAOS_CONFIGURE_ARGS?=	-v
+DEFORAOS_CONFIGURE_DIRS?=	.
+_BUILD_DEFS+=			DEFORAOS_CONFIGURE DEFORAOS_CONFIGURE_ARGS DEFORAOS_CONFIGURE_DIRS
+_DO_CONFIGURE_TARGETS+=		do-configure-deforaos
+
+do-configure-deforaos:
+.for dir in ${DEFORAOS_CONFIGURE_DIRS}
+	@${ECHO_MSG} "Running DeforaOS configure in ${dir}"
+	@${RUN} cd ${WRKSRC} && ${DEFORAOS_CONFIGURE} ${DEFORAOS_CONFIGURE_ARGS} ${dir}
+.endfor
