85ad4f3efc98a514627a30f9c7a40f4cd0b4a1f5
khorben <khorben@pkgsrc.org>
2022-04-12 22:56:53 +0000

    deforaos-libsystem: reflect the API/ABI change

--- devel/deforaos-libsystem/buildlink3.mk
+++ devel/deforaos-libsystem/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/04/13 00:41:02 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/04/12 22:56:53 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libsystem
 
 .if !defined(DEFORAOS_LIBSYSTEM_BUILDLINK3_MK)
 DEFORAOS_LIBSYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.3.1
+BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.4.3
+BUILDLINK_ABI_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.4.3
 BUILDLINK_PKGSRCDIR.deforaos-libsystem?=	../../devel/deforaos-libsystem
 
 .endif	# DEFORAOS_LIBSYSTEM_BUILDLINK3_MK
