fad536374a9be327dd7d6247224e2d85e27ac07a
nros <nros@pkgsrc.org>
2022-04-14 13:20:42 +0000

    Update multimedia/assimp to version 5.2.3
    Update multimedia/assimp to version 5.2.3.
    
    The reason for updating is that this
    version has changes, mOrthographicWith in
    aiCamera, needed by some packages in wip
    (qt6).
    
    This update should be safe since only wip
    packages depend on it.
    
    pkgsrc changes:
    patch-assimp.pc.in came from upstream and
    is now removed.
    contrib/irrXML does not exist anymore so
    patch-contrib_irrXML_CMakeLists.txt has
    been removed.
    
    Changes in package:
    To many to mention.

--- multimedia/assimp/buildlink3.mk
+++ multimedia/assimp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/01/02 07:35:44 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/04/14 13:20:42 nros Exp $
 
 BUILDLINK_TREE+=	assimp
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	assimp
 ASSIMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.assimp+=	assimp>=5.0.1
+BUILDLINK_ABI_DEPENDS.assimp+=	assimp>=5.2.3
 BUILDLINK_PKGSRCDIR.assimp?=	../../multimedia/assimp
 
 .include "../../devel/zlib/buildlink3.mk"
