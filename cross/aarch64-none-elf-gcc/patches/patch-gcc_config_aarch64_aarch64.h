000d68588f49afe5612e6195e14826e7e9310c31
tnn <tnn@pkgsrc.org>
2022-04-15 16:56:06 +0000

    cross/*gcc: kludge in support for Darwin/aarch64 hosts in old GCC packages

--- cross/aarch64-none-elf-gcc/patches/patch-gcc_config_aarch64_aarch64.h
+++ cross/aarch64-none-elf-gcc/patches/patch-gcc_config_aarch64_aarch64.h
@@ -1,11 +1,6 @@
-$NetBSD: patch-gcc_config_aarch64_aarch64.h,v 1.2 2018/11/23 05:30:45 ryoon Exp $
+$NetBSD: patch-gcc_config_aarch64_aarch64.h,v 1.3 2022/04/15 16:56:07 tnn Exp $
 
-driver-aarch64.o is not included for NetBSD and it is responsible for
-the definition of host_detect_local_cpu.
-
-Without it, we get an undefined reference.
-
-Similar to GCC bugzilla #77800 for arm.
+Disable -march=native for cross compilers.
 
 --- gcc/config/aarch64/aarch64.h.orig	2018-02-21 14:05:45.000000000 +0000
 +++ gcc/config/aarch64/aarch64.h
@@ -15,7 +10,7 @@ Similar to GCC bugzilla #77800 for arm.
  
 -#if defined(__aarch64__)
 +/* NetBSD does not export necessary info via /proc/cpuinfo. */
-+#if defined(__aarch64__) && !defined(__NetBSD__)
++#if defined(__aarch64__) && !defined(CROSS_DIRECTORY_STRUCTURE)
  extern const char *host_detect_local_cpu (int argc, const char **argv);
  #define HAVE_LOCAL_CPU_DETECT
  # define EXTRA_SPEC_FUNCTIONS						\
---
+++ cross/pdp11-aout-gcc/patches/patch-gcc_config_host-darwin.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-gcc_config_host-darwin.c,v 1.1 2022/04/15 16:56:08 tnn Exp $
+
+Add host hooks to allow hosting on Darwin/aarch64.
+The patch is done differently from the upstream fix in order to be small.
+Delete this patch when updating to GCC 12 or later.
+
+--- gcc/config/host-darwin.c.orig	2018-01-03 10:03:58.000000000 +0000
++++ gcc/config/host-darwin.c
+@@ -75,3 +75,9 @@ darwin_gt_pch_use_address (void *addr, s
+ 
+   return ret;
+ }
++
++#if defined(__aarch64__)
++#include "hosthooks.h"
++#include "hosthooks-def.h"
++const struct host_hooks host_hooks = HOST_HOOKS_INITIALIZER;
++#endif
