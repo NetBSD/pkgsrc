9322356ec1edf96d5f0662a184584bf9b155f98e
taca <taca@pkgsrc.org>
2022-04-16 00:53:58 +0000

    lang/php80: update to 8.0.18
    14 Apr 2022, PHP 8.0.18
    
    - Core:
      . Fixed freeing of internal attribute arguments. (Bob)
      . Fixed bug GH-8070 (memory leak of internal function attribute hash).
        (Tim Düsterhus)
      . Fixed bug GH-8160 (ZTS support on Alpine is broken). (Michael Voříšek)
    
    - Filter:
      . Fixed signedness confusion in php_filter_validate_domain(). (cmb)
    
    - Intl:
      . Fixed bug GH-8142 (Compilation error on cygwin). (David Carlier)
    
    - MBString:
      . Fixed bug GH-8208 (mb_encode_mimeheader: $indent functionality broken).
        (cmb)
    
    - MySQLi:
      . Fixed bug GH-8068 (mysqli_fetch_object creates inaccessible properties).
        (cmb)
    
    - Pcntl:
      . Fixed bug GH-8142 (Compilation error on cygwin). (David Carlier)
    
    - PgSQL:
      . Fixed result_type related stack corruption on LLP64 architectures. (cmb)
      . Fixed bug GH-8253 (pg_insert() fails for references). (cmb)
    
    - Sockets:
      . Fixed Solaris builds. (David Carlier)
    
    - SPL:
      . Fixed bug GH-8121 (SplFileObject - seek and key with csv file inconsistent).
        (cmb)
    
    - Standard:
      . Fixed bug GH-8048 (Force macOS to use statfs). (risner)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.361 2022/04/16 00:52:28 taca Exp $
+# $NetBSD: phpversion.mk,v 1.362 2022/04/16 00:53:58 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.29
-PHP80_VERSION=	8.0.17
+PHP80_VERSION=	8.0.18
 PHP81_VERSION=	8.1.4
 
 # Define initial release of major version.
