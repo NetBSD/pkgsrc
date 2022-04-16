ad3c03aa2774066a63bad24d7f87ecfd40b3019c
taca <taca@pkgsrc.org>
2022-04-16 00:55:47 +0000

    lang/php81: update to 8.1.5
    14 Apr 2022, PHP 8.1.5
    
    - Core:
      . Fixed bug GH-8176 (Enum values in property initializers leak). (Bob)
      . Fixed freeing of internal attribute arguments. (Bob)
      . Fixed bug GH-8070 (memory leak of internal function attribute hash).
        (Tim Düsterhus)
      . Fixed bug GH-8160 (ZTS support on Alpine is broken). (Michael Voříšek)
    
    - Filter:
      . Fixed signedness confusion in php_filter_validate_domain(). (cmb)
    
    - Intl:
      . Fixed bug GH-8115 (Can't catch arg type deprecation when instantiating Intl
        classes). (ilutov)
      . Fixed bug GH-8142 (Compilation error on cygwin). (David Carlier)
      . Fixed bug GH-7734 (Fix IntlPartsIterator key off-by-one error and first
        key). (ilutov)
    
    - MBString:
      . Fixed bug GH-8208 (mb_encode_mimeheader: $indent functionality broken).
        (cmb)
    
    - MySQLi:
      . Fixed bug GH-8068 (mysqli_fetch_object creates inaccessible properties).
        (cmb)
    
    - Pcntl:
      . Fixed bug GH-8142 (Compilation error on cygwin). (David Carlier)
    
    - PgSQL:
      . Fixed result_type related stack corruption on LLP64 architectures. (cmb)
      . Fixed bug GH-8253 (pg_insert() fails for references). (cmb)
    
    - Sockets:
      . Fixed Solaris builds. (David Carlier)
    
    - SPL:
      . Fixed bug GH-8121 (SplFileObject - seek and key with csv file inconsistent).
        (cmb)
      . Fixed bug GH-8192 (Cannot override DirectoryIterator::current() without
        return typehint in 8.1). (Nikita)
    
    - Standard:
      . Fixed bug GH-8048 (Force macOS to use statfs). (risner)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.362 2022/04/16 00:53:58 taca Exp $
+# $NetBSD: phpversion.mk,v 1.363 2022/04/16 00:55:47 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.29
 PHP80_VERSION=	8.0.18
-PHP81_VERSION=	8.1.4
+PHP81_VERSION=	8.1.5
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
