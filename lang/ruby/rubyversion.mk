af5d2c9a995088e68f5bba7363eaf80048dce808
spz <spz@pkgsrc.org>
2022-04-16 09:41:21 +0000

    Pullup ticket #6618 - requested by taca lang/ruby31-base: security update
    Revisions pulled up:
    - lang/ruby/rubyversion.mk                                      1.249
    - lang/ruby31-base/distinfo                                     1.3
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Tue Apr 12 15:12:13 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/ruby: rubyversion.mk
       	pkgsrc/lang/ruby31-base: distinfo
    
       Log Message:
       lang/ruby31-base: update to 3.1.2
    
       Ruby 3.1.2 has been released.
    
       This release includes security fixes.  Please check the topics below for
       details.
    
       * CVE-2022-28738: Double free in Regexp compilation
       * CVE-2022-28739: Buffer overrun in String-to-Float conversion
    
       See the commit logs for further details.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.248 -r1.249 pkgsrc/lang/ruby/rubyversion.mk
       cvs rdiff -u -r1.2 -r1.3 pkgsrc/lang/ruby31-base/distinfo

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.244.2.4 2022/04/16 09:23:40 spz Exp $
+# $NetBSD: rubyversion.mk,v 1.244.2.5 2022/04/16 09:41:21 spz Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -216,7 +216,7 @@ RUBY_VERSION_REQD?=	${PKGNAME_REQD:C/ruby([0-9][0-9])-.*/\1/}
 RUBY26_VERSION=		2.6.10
 RUBY27_VERSION=		2.7.6
 RUBY30_VERSION=		3.0.4
-RUBY31_VERSION=		3.1.1
+RUBY31_VERSION=		3.1.2
 
 # current API compatible version; used for version of shared library
 RUBY26_API_VERSION=	2.6.0
