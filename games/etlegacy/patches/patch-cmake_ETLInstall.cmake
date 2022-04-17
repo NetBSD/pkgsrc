d972844e8f0cb44c34f2be34a05d6f14e6cdcded
nia <nia@pkgsrc.org>
2022-04-17 09:24:14 +0000

    etlegacy[-server]: update to 2.80.0
    2.80.0 (released 09/04/2022)
    
      Engine
    
        General
    
         * Fixed infinite loop in filesystem initalization when pak0.pk3 was
           missing
         * Changed default URI from ET:// (upper case) to et:// (lower case) and
           made case insensitive for compatibility / flexibility
    
      Client
    
        General
    
         * Removed cl_activateLean cvar (moved behaviour to mod
           (cg_activateLean))
    
        SDL2 Audio backend
    
         * Fixed double played sound in one frame increasing sound (i.e mg42)
    
      Mod
    
        General
    
         * Fixed players not set unready after swapping teams
         * Fixed wait map-script command not matching the correct time on higher
           sv_fps (comparing to sv_fps 20), which affected and broke certain
           gameplay elements
         * Updated game command list and related help in console
         * Fixed accuracy calculation for splashdamage weapons
         * Fixed dropping objective was allowed during pause
         * Added cg_activateLean, allowing all clients activate lean usage
         * Added g_shoveNoZ to allow disabling Z shove completely
         * Added binds for shifting enemy spawn timer and requesting artillery
         * Changed the way for displaying help command by swapping arguments
           order (Before: <command_name> ?) (After: ? <command_name>)
    
        Stability & Performance
    
         * Fixed hitsounds reduced network performance and sometimes get
           duplicated, especially with high pings
         * Added commands flood protection (g_floodProtection / g_floodLimit /
           g_floodWait cvars)
         * Changed unprotect flood for team cmd so class cmd doesn't get dropped
         * Fixed prediction error by changing cg_errorDecay flag with CVAR_CHEAT
    
        Audio
    
         * Fixed missing hitsound when player has exactly FORCE_LIMBO_HEALTH
           health (-113 HP)
    
        Graphics
    
         * Fixed players breath puffs effect missing completely
         * Fixed pause weapon animation in 1st person during pause
         * Fixed viewmodel FOV calculation wasn't lowering enough 1st person
           weapon draw on FOV > 90
         * Remove g_altSuicideAnim and default to the normal death anim on player
           suiciding
         * Fixed a crash with cg_drawgun 2 and cg_weapanims 0 combination
         * Added voice chat icons for :
              * Affirmative / Negative
              * Hold your fire
              * Say player class
    
        UI
    
         * Fixed disconnect icon rendering through walls
         * Added hud componement scale and color configurability from hud file
         * Fixed fireteam highlight overlay spacing on top edge
         * Fixed * characters in map description were not replaced by line feed
         * Added cg_popupTime and cg_numPopups cvar to control popups more
           thoroughly
         * Added sharetimer/sharetimer_buddy commands and cg_sharetimerText cvar
           share the spawntimer you have set for when you think the enemy spawns
         * Removed grid drawing on compass
         * Added cg_fireteamBgAlpha cvar to control fireteam background alpha
           value
         * Added cvar name tooltip in option menus
         * Added missile camera for practice
         * Enhanced squared compass
              * Fixed icon scaling to match regular compass
              * Fixed slightly incorrect out of bounds check for icons
              * Icons are now drawn when they're inside the map and respect map
                zoom
              * Added indicators for N, S, E, W
    
        Weapons
    
         * Increased landmine splash radius by 25 (225 -> 250) to match etpro
         * Fixed picking up rifle with full riflenade ammo was restoring 4
           riflenade instead of 5
         * Fixed throwable missiles (i.e: grenade, satchel, dynamite, landmine)
           infinite bouncing
         * Removed hardcoded sensitivity on mounted mg's and use common
           sensitivity
         * Added cg_scopedSensitivityScaler to scale sensitivity when scoped

--- games/etlegacy/patches/patch-cmake_ETLInstall.cmake
+++ games/etlegacy/patches/patch-cmake_ETLInstall.cmake
@@ -1,10 +1,10 @@
-$NetBSD: patch-cmake_ETLInstall.cmake,v 1.2 2021/12/09 16:10:59 nia Exp $
+$NetBSD: patch-cmake_ETLInstall.cmake,v 1.3 2022/04/17 09:24:14 nia Exp $
 
 Install man pages to PKGMANDIR.
 
---- cmake/ETLInstall.cmake.orig	2021-11-01 20:57:18.000000000 +0000
+--- cmake/ETLInstall.cmake.orig	2022-04-09 17:17:04.000000000 +0000
 +++ cmake/ETLInstall.cmake
-@@ -42,7 +42,7 @@ if(UNIX AND NOT APPLE)
+@@ -46,7 +46,7 @@ if(UNIX AND NOT APPLE)
  		DESTINATION "${INSTALL_DEFAULT_SHAREDIR}/metainfo"
  	)
  	install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/docs/linux/man/man6/"
@@ -12,4 +12,4 @@ Install man pages to PKGMANDIR.
 +		DESTINATION "${CMAKE_INSTALL_MANDIR}/man6"
  	)
  	install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/docs/INSTALL.txt"
- 		DESTINATION "${INSTALL_DEFAULT_SHAREDIR}/doc/etlegacy"
+ 		DESTINATION "${DOCDIR}"
