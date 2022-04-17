0c4ad058274d870b4ec475fffbf5b1e1a9be8129
gutteridge <gutteridge@pkgsrc.org>
2022-04-17 01:44:23 +0000

    xview*: explicitly require font-sun-misc for correct operation
    Without the expected fonts, fundamental graphical content like icons
    goes missing, and the X server may fail to start, at that.
    
    XXX I tested on NetBSD 9.2_STABLE amd64 and i386. On amd64, this
    crashes the X server for me, while it works fine on i386. The package
    was already marked not for Alpha, so that may be a clue that it's not
    64-bit clean in general, though some of the original commentary about
    Alpha sounded quite specific.

--- x11/xview-lib/buildlink3.mk
+++ x11/xview-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2018/01/07 13:04:44 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2022/04/17 01:44:23 gutteridge Exp $
 
 BUILDLINK_TREE+=	xview-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xview-lib
 XVIEW_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xview-lib+=	xview-lib>=3.2.1
-BUILDLINK_ABI_DEPENDS.xview-lib+=	xview-lib>=3.2.1nb8
+BUILDLINK_ABI_DEPENDS.xview-lib+=	xview-lib>=3.2.1nb11
 BUILDLINK_PKGSRCDIR.xview-lib?=		../../x11/xview-lib
 
 .include "../../x11/libX11/buildlink3.mk"
