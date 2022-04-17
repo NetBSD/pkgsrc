3386ff0ef14b044b2b86d223a18b93aa088d9bb4
wiz <wiz@pkgsrc.org>
2022-04-17 18:48:17 +0000

    ats2: PLIST_VARS needs to be defined unconditionally
    Fixes build with emacs option turned off (the default)

--- lang/ats2/options.mk
+++ lang/ats2/options.mk
@@ -1,13 +1,14 @@
-# $NetBSD: options.mk,v 1.1 2022/04/14 20:13:19 nikita Exp $
+# $NetBSD: options.mk,v 1.2 2022/04/17 18:48:17 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ats2
 
 PKG_SUPPORTED_OPTIONS+=		emacs
 PKG_SUGGESTED_OPTIONS=
 
+PLIST_VARS+=	emacs
+
 .include "../../mk/bsd.options.mk"
 
 .if ${PKG_OPTIONS:Memacs}
-PLIST_VARS+=	emacs
 PLIST.emacs=	yes
 .endif
