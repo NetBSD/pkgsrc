03ce59cdea840d6cf562b2dbf278daf4d932750e
adam <adam@pkgsrc.org>
2022-04-18 11:01:33 +0000

    cmake cmake-gui: updated to 3.23.1
    CMake 3.23.1
    * CMakeDependentOption: improve documentation
    * cmFileSet: store visibility with the fileset
    * cmTarget: make HEADER_SETS and INTERFACE_HEADER_SETS read-only
    * GNUInstallDirs: Revert "Apply Debian multiarch LIBDIR to more prefixes"
    * Help: Clarify when policy CMP0112 takes effect
    * gitlab-ci: clarify name of package upload job template
    * gitlab-ci: distinguish release and development pipeline schedules
    * gitlab-ci: add sanity check to upload jobs
    * gitlab-ci: simplify package pipeline job conditions
    * gitlab-ci: start release package pipelines manually
    * Utilities/Sphinx: Add undocumented option to build docs for cmake.org
    * Utilities/Sphinx: Add option to build outdated version banner for cmake.org
    * Utilities/Sphinx: Add OpenSearch link to html page headers on cmake.org
    * gitlab-ci: consolidate jobs for cmake.org/cmake/help/git-{master,stage} docs
    * gitlab-ci: update cmake.org documentation in release package pipeline
    * gitlab-ci: Add objects.inv to cmake.org html documentation
    * gitlab-ci: Build qthelp-format release documentation for cmake.org
    * Help: Add "Updates" section header in 3.22 release notes
    * Utilities/Sphinx: Update qthelp generation to qhelpgenerator
    * CheckCompilerFlag: Fix regression in locale environment preservation
    * Help: Document that target_sources defines [INTERFACE_]HEADER_SETS
    * CheckSourceCompiles: Avoid linker warning with -fembed-bitcode
    * Help: Correct/add missing details for CheckPIESupported
    * CheckPIESupported: Prevent separate output lines running together
    * PCH: Fix Xcode non-pch language exclusion
    * Help: Fix typo in install command documentation
    * FindPostgreSQL: add support for PostgreSQL 14
    * Tests: Ignore all classes in Xcode internal objc warnings
    * FILE_SET: Forbid adding header sets to Apple FRAMEWORK libraries
    * CUDA: Ninja generator generates valid compile database
    * cmake: --list=presets=[type] doesn't generate incorrect warnings
    * Help: Fix string(JSON ... LENGTH ...) signature
    * Help: Add that CMAKE_CACHEFILE_DIR might not be defined

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.25 2022/03/07 19:59:44 adam Exp $
+# $NetBSD: version.mk,v 1.26 2022/04/18 11:01:33 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.22.3
+CMAKE_VERSION=	3.23.1
 CMAKE_API=	${CMAKE_VERSION:R}
