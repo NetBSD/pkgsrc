8352cc81e452f0546d7e4aaea81202936e65a4fb
tnn <tnn@pkgsrc.org>
2022-04-18 20:08:55 +0000

    try to unbreak the tree

--- x11/py-qt5-qscintilla/buildlink3.mk
+++ x11/py-qt5-qscintilla/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/04/18 19:10:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/04/18 20:08:55 tnn Exp $
 
 BUILDLINK_TREE+=	py-qt5-qscintilla
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	py-qt5-qscintilla
 PY_QT5_QSCINTILLA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}-qt5-qscintilla>=2.11.2
-BUILDLINK_ABI_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}0-qt5-qscintilla>=2.11.5nb5
+BUILDLINK_ABI_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}-qt5-qscintilla>=2.11.5nb5
 BUILDLINK_PKGSRCDIR.py-qt5-qscintilla?=		../../x11/py-qt5-qscintilla
 
 .include "../../x11/py-sip/buildlink3.mk"
