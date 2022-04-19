c7c8c263858ec6224aecd92a9373ff28b80d8913
adam <adam@pkgsrc.org>
2022-04-19 11:34:27 +0000

    py-jupyter_core: updated to 4.10.0
    4.10.0
    * Include all files from ``jupyter_core``.
    * Add project URLs to ``setup.cfg``.
    * Set up pre-commit.
    * Add flake8 and mypy settings.
    * Clean up CI.

--- devel/py-jupyter_core/patches/patch-jupyter__core_paths.py
+++ devel/py-jupyter_core/patches/patch-jupyter__core_paths.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-jupyter__core_paths.py,v 1.1 2018/02/06 01:40:48 minskim Exp $
+$NetBSD: patch-jupyter__core_paths.py,v 1.2 2022/04/19 11:34:27 adam Exp $
 
 Enable Jupyter to discover kernels installed by pkgsrc packages.
 
---- jupyter_core/paths.py.orig	2017-10-11 14:52:06.000000000 +0000
+--- jupyter_core/paths.py.orig	2022-04-18 10:53:22.000000000 +0000
 +++ jupyter_core/paths.py
-@@ -118,6 +118,7 @@ if os.name == 'nt':
-         SYSTEM_JUPYTER_PATH = [os.path.join(sys.prefix, 'share', 'jupyter')]
+@@ -131,6 +131,7 @@ if os.name == "nt":
+         SYSTEM_JUPYTER_PATH = [os.path.join(sys.prefix, "share", "jupyter")]
  else:
      SYSTEM_JUPYTER_PATH = [
 +        "@PREFIX@/share/jupyter",
