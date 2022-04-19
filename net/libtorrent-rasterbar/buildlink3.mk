0d4cddfcee3a5ac49b7c6c0afd5524f1e16282a2
adam <adam@pkgsrc.org>
2022-04-19 16:34:41 +0000

    libtorrent-rasterbar: bump buildlink

--- net/libtorrent-rasterbar/buildlink3.mk
+++ net/libtorrent-rasterbar/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/01/10 01:46:42 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2022/04/19 16:34:41 adam Exp $
 
 BUILDLINK_TREE+=	libtorrent-rasterbar
 
 .if !defined(LIBTORRENT_RASTERBAR_BUILDLINK3_MK)
 LIBTORRENT_RASTERBAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=1.2.2
-BUILDLINK_ABI_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=1.2.15nb1
+BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=2.0.6
+BUILDLINK_ABI_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=2.0.6
 BUILDLINK_PKGSRCDIR.libtorrent-rasterbar?=	../../net/libtorrent-rasterbar
 
 .include "../../devel/boost-libs/buildlink3.mk"
