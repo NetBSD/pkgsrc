942268edc80c1e4452bb93a5f5b103d5cc246f6f
khorben <khorben@pkgsrc.org>
2022-04-20 10:10:59 +0000

    mangos: import 3.4.1
    Mangos is an implementation in pure Go of the SP (Scalability Protocols)
    messaging system. These are colloquially known as nanomsg.

---
+++ net/mangos/go-modules.mk
@@ -0,0 +1,25 @@
+# $NetBSD: go-modules.mk,v 1.1 2022/04/20 10:10:59 khorben Exp $
+
+GO_MODULE_FILES+=	github.com/!microsoft/go-winio/@v/v0.5.2.mod
+GO_MODULE_FILES+=	github.com/!microsoft/go-winio/@v/v0.5.2.zip
+GO_MODULE_FILES+=	github.com/gdamore/optopia/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/gdamore/optopia/@v/v0.2.0.zip
+GO_MODULE_FILES+=	github.com/gorilla/websocket/@v/v1.5.0.mod
+GO_MODULE_FILES+=	github.com/gorilla/websocket/@v/v1.5.0.zip
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.7.0.mod
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.7.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210124154548-22da62e12c0c.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.zip
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.2.2.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.2.2.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.3.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210124154548-22da62e12c0c.zip
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.3.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.3.0.zip
