d7cdc4f10e42ab72996bb5c58493c59100af5983
nia <nia@pkgsrc.org>
2022-04-21 06:56:18 +0000

    sdl12-compat: needs glu on !darwin

--- devel/sdl12-compat/buildlink3.mk
+++ devel/sdl12-compat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/04/01 10:43:05 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/04/21 06:56:18 nia Exp $
 
 BUILDLINK_TREE+=	sdl12-compat
 
@@ -8,6 +8,13 @@ SDL12_COMPAT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.sdl12-compat+=	sdl12-compat>=1.2.52
 BUILDLINK_PKGSRCDIR.sdl12-compat?=	../../devel/sdl12-compat
 BUILDLINK_INCDIRS.sdl12-compat+=	include/SDL
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} != "Darwin"
+.  include "../../graphics/MesaLib/buildlink3.mk"
+.  include "../../graphics/glu/buildlink3.mk"
+.endif
 
 .endif	# SDL12_COMPAT_BUILDLINK3_MK
 
