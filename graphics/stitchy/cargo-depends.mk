d5b9a53e4e3e10dae42150ba23401e01da8c000a
pin <pin@pkgsrc.org>
2022-04-22 07:38:56 +0000

    graphics/stitchy: update to 0.1.4
    - Updated to Rust 2021
    - Updated dependencies (structopt to 0.3.26, image to 0.24.0)
    - Print output file size in terminal output
    - Added ability to save defaults that will apply automatically were not
      overridden

--- graphics/stitchy/cargo-depends.mk
+++ graphics/stitchy/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.2 2022/03/06 08:46:01 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.3 2022/04/22 07:38:56 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
 CARGO_CRATE_DEPENDS+=	adler32-1.2.0
@@ -14,7 +14,9 @@ CARGO_CRATE_DEPENDS+=	crc32fast-1.2.0
 CARGO_CRATE_DEPENDS+=	deflate-1.0.0
 CARGO_CRATE_DEPENDS+=	gif-0.11.3
 CARGO_CRATE_DEPENDS+=	heck-0.3.1
+CARGO_CRATE_DEPENDS+=	home-0.5.3
 CARGO_CRATE_DEPENDS+=	image-0.24.0
+CARGO_CRATE_DEPENDS+=	itoa-1.0.1
 CARGO_CRATE_DEPENDS+=	jpeg-decoder-0.2.2
 CARGO_CRATE_DEPENDS+=	lazy_static-1.4.0
 CARGO_CRATE_DEPENDS+=	miniz_oxide-0.5.1
@@ -25,11 +27,15 @@ CARGO_CRATE_DEPENDS+=	num-traits-0.2.14
 CARGO_CRATE_DEPENDS+=	png-0.17.3
 CARGO_CRATE_DEPENDS+=	proc-macro-error-1.0.2
 CARGO_CRATE_DEPENDS+=	proc-macro-error-attr-1.0.2
-CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.6
+CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.36
 CARGO_CRATE_DEPENDS+=	quote-1.0.2
+CARGO_CRATE_DEPENDS+=	ryu-1.0.9
+CARGO_CRATE_DEPENDS+=	serde-1.0.136
+CARGO_CRATE_DEPENDS+=	serde_derive-1.0.136
+CARGO_CRATE_DEPENDS+=	serde_json-1.0.79
 CARGO_CRATE_DEPENDS+=	structopt-0.3.26
 CARGO_CRATE_DEPENDS+=	structopt-derive-0.4.18
-CARGO_CRATE_DEPENDS+=	syn-1.0.5
+CARGO_CRATE_DEPENDS+=	syn-1.0.91
 CARGO_CRATE_DEPENDS+=	syn-mid-0.5.0
 CARGO_CRATE_DEPENDS+=	textwrap-0.11.0
 CARGO_CRATE_DEPENDS+=	unicode-segmentation-1.3.0
@@ -37,3 +43,6 @@ CARGO_CRATE_DEPENDS+=	unicode-width-0.1.6
 CARGO_CRATE_DEPENDS+=	unicode-xid-0.2.0
 CARGO_CRATE_DEPENDS+=	version_check-0.9.1
 CARGO_CRATE_DEPENDS+=	weezl-0.1.5
+CARGO_CRATE_DEPENDS+=	winapi-0.3.9
+CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
+CARGO_CRATE_DEPENDS+=	winapi-x86_64-pc-windows-gnu-0.4.0
