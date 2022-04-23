8e0c68fbeb266e222ebf93f27f751d643af8445b
tnn <tnn@pkgsrc.org>
2022-04-23 23:56:23 +0000

    pdvipsk: use GNU patch unconditionally to apply distribution patch
    It is difficult to detect if TOOLS_PATCH is nbpatch or not.
    Depends on local pkgsrc bootstrap situation.

--- print/pdvipsk/hacks.mk
+++ print/pdvipsk/hacks.mk
@@ -1,18 +1,12 @@
-# $NetBSD: hacks.mk,v 1.3 2019/11/04 19:58:00 rillig Exp $
+# $NetBSD: hacks.mk,v 1.4 2022/04/23 23:56:23 tnn Exp $
 
 .if !defined(PDVIPSK_HACKS_MK)
 PDVIPSK_HACKS_MK=	# defined
 
 ### NetBSD's patch(1) fails in patching dvips.info.
 ###
-.if ${OPSYS} == "NetBSD"
 BUILD_DEPENDS+=	patch-[0-9]*:../../devel/patch
 
 PATCH=			${LOCALBASE}/bin/gpatch
 PKG_HACKS+=		patch-dvips_info
-.elif ${OPSYS} == "Darwin"
-TOOLS_PLATFORM.patch=	/usr/bin/patch
-PKG_HACKS+=		patch-dvips_info
-.endif
-
 .endif
