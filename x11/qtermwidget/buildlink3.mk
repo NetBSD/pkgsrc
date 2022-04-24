0d2b8c8ac4bc1badceb5c2978b8ea945809aaf6e
pin <pin@pkgsrc.org>
2022-04-24 15:17:39 +0000

    x11/qtermwidget: update to 1.1.0
    Main changes:
    -Handled the zero history size.
    -Added API documentation and followed the naming convention in some places of
     the code.
    -Return focus to the terminal on closing the search bar.
    -Code cleanup and enhancement.

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2021/11/19 09:50:40 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/04/24 15:17:39 pin Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=1.0.0
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=1.1.0
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
 #.include "../../x11/qt5-qtbase/buildlink3.mk"
