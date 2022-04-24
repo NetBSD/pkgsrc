90a88dbdc34b5ba59d65dbf384993a79e3010a3d
tnn <tnn@pkgsrc.org>
2022-04-24 01:35:38 +0000

    rinetd: update HOMEPAGE and MASTER_SITE. While here update to 0.73.
    Version 0.73 (2021/02/19)
      improve error reporting
      fixed another configuration parsing bug
    Version 0.72 (2021/02/16)
      fixed a configuration parsing bug making 0.71 almost unusable
    Version 0.71 (2021/02/14)
      IPv6 support
      fixed a display overflow in data usage statistics
      made the configuration parser more tolerant with service names that contain
        dashes (e.g. snmp-trap) or hostnames with underscores
      create the log file if it does not exist
      new Visual Studio project file
    Version 0.70 (2017/09/09)
      UDP support
      source address binding support
      use a real grammar for the configuration file and get rid of the hand-made
        parser
    Version 0.63 (2017/07/19)
      added a -f flag to run in the foreground
      quit cleanly when SIGINT is received
      increased data buffer size for better performance
      fixed random uninitialised data accesses
      fixed a memory leak in connection reallocation
      fixed a file descriptor leak in configuration reload
      fixed configuration file parsing (many bugs including a buffer overflow)
      fixed configuration error reporting (line numbers were wrong)
      log accepted connections in addition to denied ones
      log DNS errors
      code quality refactoring: got rid of a lot of old code, used C library
        functions instead of custom ones, enforced const correctness

---
+++ net/rinetd/patches/patch-src_rinetd.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_rinetd.c,v 1.1 2022/04/24 01:35:39 tnn Exp $
+
+include <netdb.h> for NI_MAXHOST
+
+--- src/rinetd.c.orig	2021-02-19 08:57:10.000000000 +0000
++++ src/rinetd.c
+@@ -37,6 +37,7 @@
+ #include <signal.h>
+ #include <stdlib.h>
+ #include <fcntl.h>
++#include <netdb.h>
+ #if _WIN32 || (!TIME_WITH_SYS_TIME && !HAVE_SYS_TIME_H)
+ #	include <time.h>
+ #endif
