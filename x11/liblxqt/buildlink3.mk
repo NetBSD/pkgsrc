e25d6011585ad90ecf360ab0d63444dcdda4fffa
pin <pin@pkgsrc.org>
2022-04-24 15:01:31 +0000

    x11/lblxqt: update to 1.1.0
    Main changes:
    -Added support for relative wallpaper paths.
    -Fixed an issue in loading of cached settings.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2022/04/18 19:10:16 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2022/04/24 15:01:31 pin Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.0.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=1.0.0nb2
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.1.0
+BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=1.1.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
