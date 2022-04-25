254cc2322fffcf24dc8e8ec9569ef319e32b7bc0
tnn <tnn@pkgsrc.org>
2022-04-25 17:58:29 +0000

    kirigami2: regen patch

--- x11/kirigami2/patches/patch-src_imagecolors.cpp
+++ x11/kirigami2/patches/patch-src_imagecolors.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_imagecolors.cpp,v 1.3 2022/04/25 13:36:38 tnn Exp $
+$NetBSD: patch-src_imagecolors.cpp,v 1.4 2022/04/25 17:58:29 tnn Exp $
 
 Explicitly use std::pow.
 
---- src/imagecolors.cpp.orig	2021-03-06 16:26:33.000000000 +0000
+--- src/imagecolors.cpp.orig	2022-04-02 10:23:09.000000000 +0000
 +++ src/imagecolors.cpp
-@@ -25,6 +25,8 @@
- 
+@@ -26,6 +26,8 @@
+ #include "loggingcategory.h"
  #include <cmath>
  
 +using std::pow;
 +
- #define return_fallback(value) if (m_imageData.m_samples.size() == 0) {\
-     return value;\
- }
+ #define return_fallback(value)                                                                                                                                 \
+     if (m_imageData.m_samples.size() == 0) {                                                                                                                   \
+         return value;                                                                                                                                          \
