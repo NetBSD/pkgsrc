05e53bb403b0c6afed5f26800c9676709f56273c
tnn <tnn@pkgsrc.org>
2022-04-25 15:33:58 +0000

    tcpexec: fix build w/o SO_REUSEPORT

--- net/tcpexec/patches/patch-tcpexec.c
+++ net/tcpexec/patches/patch-tcpexec.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-tcpexec.c,v 1.1 2021/11/15 11:36:58 schmonz Exp $
+$NetBSD: patch-tcpexec.c,v 1.2 2022/04/25 15:33:58 tnn Exp $
 
-Fix macOS build.
+Fix build without close-on-exec and/or SO_REUSEPORT.
 
 --- tcpexec.c.orig	2021-11-13 13:34:51.000000000 +0000
 +++ tcpexec.c
@@ -14,3 +14,14 @@ Fix macOS build.
      fd = socket(rp->ai_family, rp->ai_socktype | SOCK_CLOEXEC, rp->ai_protocol);
  
      if (fd == -1)
+@@ -160,8 +163,10 @@ static int tcpexec_listen(const char *ad
+     if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) == -1)
+       return -1;
+ 
++#ifdef SO_REUSEPORT
+     if (setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(enable)) == -1)
+       return -1;
++#endif
+ 
+     if (bind(fd, rp->ai_addr, rp->ai_addrlen) == -1)
+       return -1;
