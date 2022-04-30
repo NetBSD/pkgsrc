ddf296552ce0c6abac7e7ad8c510c6305a85b873
mef <mef@pkgsrc.org>
2022-04-30 14:33:39 +0000

    (lang/R-cpp11)  Updated 0.3.1 to 0.4.2
    # cpp11 0.4.2
    
    * Romain François is now the maintainer.
    
    # cpp11 0.4.1
    
    * Fix crash related to unwind protect optimization (#244)
    
    # cpp11 0.4.0
    
    ## New Features
    
    * New  opt-in  message  formatting  with the  {fmt}  C++  library  for
      `cpp11::messages()` `cpp11::stop()` and `cpp11::warning()`.  Set the
      `CPP11_USE_FMT`    macro   to    use    this    feature   in    your
      package. (@sbearrows, #169, #208)
    
    * New `as_double()` and `as_integer()` methods to coerce integers to
      doubles and doubles to integers to doubles (@sbearrows, #46)
    
    * `cpp11::matrix` iterators can now be used either row-wise or
      column-wise (the default) depending on the user's choice (@alyst,
      #229)
    
    ## Improvements and fixes
    
    * Read-only matrix accessors are now marked const (#234)
    
    * `writable::r_vector` default constructors now return a 0 length
      vector when converted to `SEXP` (#166)
    
    * Read-only `r_vector` constructors now disallow implicit construction
      with named arguments (#237)
    
    * Read-only `r_vector.attr()` methods now return const objects, so it
      is a compile time error to try to assign to them (#237)
    
    * Fixed `+` and `+=` operators of `r_vector::[const_]iterator` to
      conform the *iterators* concept: `+=` updates the iterator, and `+`
      returns the updated copy, while keeping the original unchanged
      (@alyst, #231)
    
    * Remove undefined behavior when constructing global `cpp11::sexp`s (#224)
    
    * Removed redundant `.Call calls` in cpp11.cpp file (@sbearrows, #170)
    
    * Error messages now output original file name rather than the
      temporary file name (@sbearrows, #194)
    
    * `cpp_register()` now includes `attribute_visible` in the init
      function, so packages compiled with `C_VISIBILITY` will find the
      init function.
    
    * Fixed bug when running `cpp_source()` on the same file more than
      once (@sbearrows, #202)
    
    * Allow cpp11 decorators of the form `cpp11::linking_to` (@sbearrows,
      #193)
    
    * Removed internal instances of `cpp11::stop()` and replaced with C++
      exceptions (@sbearrows, #203)
    
    * Names of named lists are now resized along with the list elements
      (@sbearrows, #206)

--- lang/R-cpp11/buildlink3.mk
+++ lang/R-cpp11/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/09/08 14:50:28 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/04/30 14:33:39 mef Exp $
 #
 
 BUILDLINK_TREE+=	r_cpp11
@@ -6,9 +6,11 @@ BUILDLINK_TREE+=	r_cpp11
 .if !defined(R_CPP11_BUILDLINK3_MK)
 R_CPP11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.r_cpp11+=	R-cpp11>=0.3.1
-BUILDLINK_ABI_DEPENDS.r_cpp11+=	R-cpp11>=0.3.1
+BUILDLINK_API_DEPENDS.r_cpp11+=	R-cpp11>=0.4.2
+BUILDLINK_ABI_DEPENDS.r_cpp11+=	R-cpp11>=0.4.2
 BUILDLINK_PKGSRCDIR.r_cpp11?=	../../lang/R-cpp11
+
+BUILDLINK_FILES.r_cpp11=	lib/R/library/cpp11/include/cpp11/*
 
 .endif # R_CPP11_BUILDLINK3_MK
 
