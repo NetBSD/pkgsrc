887967ad6f4a8cb2f7d756841e607f324eb42bc3
jaapb <jaapb@pkgsrc.org>
2022-05-02 14:54:16 +0000

    Updated devel/ocaml-stdlib-shims to version 0.3.0.
    Minor updates, mostly adding support for extra OCaml versions.

--- devel/ocaml-stdlib-shims/buildlink3.mk
+++ devel/ocaml-stdlib-shims/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/12/10 08:55:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/02 14:54:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-stdlib-shims
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-stdlib-shims
 OCAML_STDLIB_SHIMS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-stdlib-shims+=	ocaml-stdlib-shims>=0.2.0
-BUILDLINK_ABI_DEPENDS.ocaml-stdlib-shims+=	ocaml-stdlib-shims>=0.2.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-stdlib-shims+=	ocaml-stdlib-shims>=0.3.0
 BUILDLINK_PKGSRCDIR.ocaml-stdlib-shims?=	../../devel/ocaml-stdlib-shims
 .endif	# OCAML_STDLIB_SHIMS_BUILDLINK3_MK
 
