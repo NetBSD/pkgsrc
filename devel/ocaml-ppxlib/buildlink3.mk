6c4721eb52b09ebb8113c1b19564c20081475932
jaapb <jaapb@pkgsrc.org>
2022-05-02 15:04:06 +0000

    Updated devel/ocaml-ppxlib to version 0.26.0.
    Many minor improvements and bugfixes, including support for OCaml 4.14.0.

--- devel/ocaml-ppxlib/buildlink3.mk
+++ devel/ocaml-ppxlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/12/10 08:57:18 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/05/02 15:04:06 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppxlib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppxlib
 OCAML_PPXLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.15.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.26.0
 BUILDLINK_PKGSRCDIR.ocaml-ppxlib?=	../../devel/ocaml-ppxlib
 
 .endif	# OCAML_PPXLIB_BUILDLINK3_MK
