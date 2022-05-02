2603fbfe1b5c1e56205475b63dab5bf4276953cb
jaapb <jaapb@pkgsrc.org>
2022-05-02 13:53:44 +0000

    Updated devel/ocaml-seq to version 0.2.2.
    Minor changes, bugfixes and updates only.

--- devel/ocaml-seq/buildlink3.mk
+++ devel/ocaml-seq/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:26 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/02 13:53:44 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-seq
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-seq
 OCAML_SEQ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-seq+=	ocaml-seq>=0.1
+BUILDLINK_API_DEPENDS.ocaml-seq+=	ocaml-seq>=0.2.2
 BUILDLINK_PKGSRCDIR.ocaml-seq?=		../../devel/ocaml-seq
 
 .endif	# OCAML_SEQ_BUILDLINK3_MK
