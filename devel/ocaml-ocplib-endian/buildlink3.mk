8ed91125ffb6ef7c3d2f7a93ba6b68925080b032
jaapb <jaapb@pkgsrc.org>
2022-05-02 13:44:25 +0000

    Updated devel/ocaml-ocplib-endian to version 1.2.
    Support fixes and minor additions only.

--- devel/ocaml-ocplib-endian/buildlink3.mk
+++ devel/ocaml-ocplib-endian/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 11:35:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/02 13:44:25 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ocplib-endian
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ocplib-endian
 OCAML_OCPLIB_ENDIAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ocplib-endian+=	ocaml-ocplib-endian>=1.0
+BUILDLINK_ABI_DEPENDS.ocaml-ocplib-endian+=	ocaml-ocplib-endian>=1.2
 BUILDLINK_PKGSRCDIR.ocaml-ocplib-endian?=	../../devel/ocaml-ocplib-endian
 
 .endif	# OCAML_OCPLIB_ENDIAN_BUILDLINK3_MK
