66e3dbb8d40a6c593e64e8dcf27b3854bbdae807
jaapb <jaapb@pkgsrc.org>
2022-05-02 13:13:34 +0000

    Updated devel/ocaml-migrate-parsetree to version 2.3.0.
    This mostly adds support for OCaml 4.14 (and versions in between); it also
    removes some unneeded dependencies and adds some minor bugfixes.

--- devel/ocaml-migrate-parsetree/buildlink3.mk
+++ devel/ocaml-migrate-parsetree/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/12/09 11:25:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2022/05/02 13:13:34 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-migrate-parsetree
 
 .if !defined(OCAML_MIGRATE_PARSETREE_BUILDLINK3_MK)
 OCAML_MIGRATE_PARSETREE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-migrate-parsetree+=	ocaml-migrate-parsetree>=1.0.1
-BUILDLINK_ABI_DEPENDS.ocaml-migrate-parsetree+=	ocaml-migrate-parsetree>=1.8.0
+BUILDLINK_API_DEPENDS.ocaml-migrate-parsetree+=	ocaml-migrate-parsetree>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-migrate-parsetree+=	ocaml-migrate-parsetree>=2.3.0
 BUILDLINK_PKGSRCDIR.ocaml-migrate-parsetree?=	../../devel/ocaml-migrate-parsetree
 .endif	# OCAML_MIGRATE_PARSETREE_BUILDLINK3_MK
 
