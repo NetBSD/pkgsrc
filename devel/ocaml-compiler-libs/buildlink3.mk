64d7027ab44562d658802d04e7354316956af221
jaapb <jaapb@pkgsrc.org>
2022-05-02 14:27:07 +0000

    Updated devel/ocaml-compiler-libs to version 0.12.4.
    Minor updates and bugfixes only.

--- devel/ocaml-compiler-libs/buildlink3.mk
+++ devel/ocaml-compiler-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/14 22:14:42 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/05/02 14:27:07 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-compiler-libs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-compiler-libs
 OCAML_COMPILER_LIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.12.1
+BUILDLINK_ABI_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.12.4
 BUILDLINK_PKGSRCDIR.ocaml-compiler-libs?=	../../devel/ocaml-compiler-libs
 .endif	# OCAML_COMPILER_LIBS_BUILDLINK3_MK
 
