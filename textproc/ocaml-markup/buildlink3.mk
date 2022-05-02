2b53c2e3c8c97bcb185ee4f65c3019414c116235
jaapb <jaapb@pkgsrc.org>
2022-05-02 18:20:27 +0000

    Updated textproc/ocaml-markup to version 1.0.3.
    This adds support for the upcoming OCaml 5.0, adds two breaking changes:
    * include tag attributes in `Misnested_tag errors
    * delete type content_signal
    and fixes several minor bugs.

--- textproc/ocaml-markup/buildlink3.mk
+++ textproc/ocaml-markup/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/04/21 11:00:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/05/02 18:20:27 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-markup
 
 .if !defined(OCAML_MARKUP_BUILDLINK3_MK)
 OCAML_MARKUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-markup+=	ocaml-markup>=0.7.2
-BUILDLINK_ABI_DEPENDS.ocaml-markup+=	ocaml-markup>=0.8.2
+BUILDLINK_API_DEPENDS.ocaml-markup+=	ocaml-markup>=1.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-markup+=	ocaml-markup>=1.0.3
 BUILDLINK_PKGSRCDIR.ocaml-markup?=	../../textproc/ocaml-markup
 
 .endif	# OCAML_MARKUP_BUILDLINK3_MK
