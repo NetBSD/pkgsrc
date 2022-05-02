0dbfe0a6bfe4b892fb88e614c3f8ba85d946c4a2
jaapb <jaapb@pkgsrc.org>
2022-05-02 18:17:18 +0000

    Updated textproc/ocaml-uutf to version 1.0.3.
    This adds support for the upcoming OCaml 5.0.

--- textproc/ocaml-uutf/buildlink3.mk
+++ textproc/ocaml-uutf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:06:52 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/05/02 18:17:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-uutf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-uutf
 OCAML_UUTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-uutf+=	ocaml-uutf>=0.9.3nb1
-BUILDLINK_ABI_DEPENDS.ocaml-uutf+=	ocaml-uutf>=1.0.2
+BUILDLINK_ABI_DEPENDS.ocaml-uutf+=	ocaml-uutf>=1.0.3
 BUILDLINK_PKGSRCDIR.ocaml-uutf?=	../../textproc/ocaml-uutf
 .endif	# OCAML_UUTF_BUILDLINK3_MK
 
