9805c0ad2651de12f2f35f8cc7a99000308d37cd
jaapb <jaapb@pkgsrc.org>
2022-05-03 19:24:37 +0000

    Updated devel/ocaml-fmt to version 0.9.0.
    This adds many minor bugfixes and improvements, but no breaking changes.
    Some depdendencies are no longer needed.

--- devel/ocaml-fmt/buildlink3.mk
+++ devel/ocaml-fmt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/11/03 10:39:22 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/03 19:24:37 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-fmt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-fmt
 OCAML_FMT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-fmt+=	ocaml-fmt>=0.8.5
-BUILDLINK_ABI_DEPENDS.ocaml-fmt+=	ocaml-fmt>=0.8.5nb1
+BUILDLINK_ABI_DEPENDS.ocaml-fmt+=	ocaml-fmt>=0.9.0
 BUILDLINK_PKGSRCDIR.ocaml-fmt?=		../../devel/ocaml-fmt
 
 .endif	# OCAML_FMT_BUILDLINK3_MK
