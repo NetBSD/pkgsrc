58ec3b26e72f9d42f6b77dac9fd8a4f1a2e1a95e
nia <nia@pkgsrc.org>
2022-05-03 20:03:31 +0000

    swift: Update to 4.0.2. Fixes build with latest version of Qt/Boost.

--- chat/swift/patches/patch-Swift_QtUI_UserSearch_QtUserSearchWindow.h
+++ chat/swift/patches/patch-Swift_QtUI_UserSearch_QtUserSearchWindow.h
@@ -1,14 +1,21 @@
-$NetBSD: patch-Swift_QtUI_UserSearch_QtUserSearchWindow.h,v 1.1 2018/06/23 09:40:53 wiz Exp $
+$NetBSD: patch-Swift_QtUI_UserSearch_QtUserSearchWindow.h,v 1.2 2022/05/03 20:03:31 nia Exp $
 
-Fix build with Qt 5.11.
+Support newer Qt versions. via Arch Linux.
 
---- Swift/QtUI/UserSearch/QtUserSearchWindow.h.orig	2016-02-29 10:29:02.000000000 +0000
+--- Swift/QtUI/UserSearch/QtUserSearchWindow.h.orig	2018-04-06 10:06:46.000000000 +0000
 +++ Swift/QtUI/UserSearch/QtUserSearchWindow.h
-@@ -6,6 +6,7 @@
+@@ -1,5 +1,5 @@
+ /*
+- * Copyright (c) 2010-2016 Isode Limited.
++ * Copyright (c) 2010-2018 Isode Limited.
+  * All rights reserved.
+  * See the COPYING file for more information.
+  */
+@@ -8,6 +8,7 @@
  
- #pragma once
+ #include <set>
  
 +#include <QAbstractItemModel>
  #include <QWizard>
- #include <set>
  
+ #include <Swiften/Base/Override.h>
