468546fadede005fe9e087d575075810c0ba1039
jaapb <jaapb@pkgsrc.org>
2022-05-03 09:21:36 +0000

    Updated devel/pcre-ocaml to version 7.5.0
    This adds support for JIT compilation and updates dependencies.

--- devel/pcre-ocaml/buildlink3.mk
+++ devel/pcre-ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/01/17 10:31:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/05/03 09:21:36 jaapb Exp $
 
 BUILDLINK_TREE+=	pcre-ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pcre-ocaml
 PCRE_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pcre-ocaml+=	pcre-ocaml>=5.10.3nb1
-BUILDLINK_ABI_DEPENDS.pcre-ocaml+=	pcre-ocaml>=7.4.3
+BUILDLINK_ABI_DEPENDS.pcre-ocaml+=	pcre-ocaml>=7.5.0
 BUILDLINK_PKGSRCDIR.pcre-ocaml?=	../../devel/pcre-ocaml
 
 .include "../../devel/pcre/buildlink3.mk"
