568438cdc5cf5a829bf02cb2178a6e212564341c
pin <pin@pkgsrc.org>
2022-05-03 19:17:22 +0000

    textproc/choose: import package
    The AWK programming language is designed for text processing and is extremely
    capable in this endeavor. However, the awk command is not ideal for rapid shell
    use, with its requisite quoting of a line wrapped in curly braces, even for
    the simplest of programs.
    
    Likewise, cut is far from ideal for rapid shell use, because of its confusing
    syntax. Field separators and ranges are just plain difficult to get right on
    the first try.
    
    It is for these reasons that I present to you choose. It is not meant to be a
    drop-in or complete replacement for either of the aforementioned tools, but
    rather a simple and intuitive tool to reach for when the basics of awk or cut
    will do, but the overhead of getting them to behave should not be necessary.

---
+++ textproc/choose/cargo-depends.mk
@@ -0,0 +1,34 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2022/05/03 19:17:22 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	aho-corasick-0.7.13
+CARGO_CRATE_DEPENDS+=	ansi_term-0.11.0
+CARGO_CRATE_DEPENDS+=	atty-0.2.14
+CARGO_CRATE_DEPENDS+=	backslash-0.1.0
+CARGO_CRATE_DEPENDS+=	bitflags-1.2.1
+CARGO_CRATE_DEPENDS+=	clap-2.33.1
+CARGO_CRATE_DEPENDS+=	heck-0.3.1
+CARGO_CRATE_DEPENDS+=	hermit-abi-0.1.14
+CARGO_CRATE_DEPENDS+=	lazy_static-1.4.0
+CARGO_CRATE_DEPENDS+=	libc-0.2.71
+CARGO_CRATE_DEPENDS+=	memchr-2.3.3
+CARGO_CRATE_DEPENDS+=	proc-macro-error-1.0.3
+CARGO_CRATE_DEPENDS+=	proc-macro-error-attr-1.0.3
+CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.18
+CARGO_CRATE_DEPENDS+=	quote-1.0.7
+CARGO_CRATE_DEPENDS+=	regex-1.3.9
+CARGO_CRATE_DEPENDS+=	regex-syntax-0.6.18
+CARGO_CRATE_DEPENDS+=	strsim-0.8.0
+CARGO_CRATE_DEPENDS+=	structopt-0.3.15
+CARGO_CRATE_DEPENDS+=	structopt-derive-0.4.8
+CARGO_CRATE_DEPENDS+=	syn-1.0.33
+CARGO_CRATE_DEPENDS+=	syn-mid-0.5.0
+CARGO_CRATE_DEPENDS+=	textwrap-0.11.0
+CARGO_CRATE_DEPENDS+=	thread_local-1.0.1
+CARGO_CRATE_DEPENDS+=	unicode-segmentation-1.6.0
+CARGO_CRATE_DEPENDS+=	unicode-width-0.1.7
+CARGO_CRATE_DEPENDS+=	unicode-xid-0.2.1
+CARGO_CRATE_DEPENDS+=	vec_map-0.8.2
+CARGO_CRATE_DEPENDS+=	version_check-0.9.2
+CARGO_CRATE_DEPENDS+=	winapi-0.3.9
+CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
+CARGO_CRATE_DEPENDS+=	winapi-x86_64-pc-windows-gnu-0.4.0
