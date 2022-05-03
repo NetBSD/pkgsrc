df690469fe46da727f47a3969d930aa07cbe587b
nia <nia@pkgsrc.org>
2022-05-03 17:09:19 +0000

    [libretro-]snes9x[-gtk]: update to 1.61
    - Restructured tile.cpp tile renderer from heavy macros into C++
      templates, significantly lowering compile time. (yoffy)
    - MichaelBuckley provided a brand new MacOS port that is completely
      rewritten based on newer, supported APIs.
    - Fixed an out-of-bound memory access in sound DSP. (Sour, byuu)
    - Revert default SFX2 clock speed increase.
    - Added a render position hack. Fixes glitches in several games.
    - Allow an SRAM mapping up to 128KB.
    
    Gtk:
    - Added an option to show time. (taimoorgit)
    - Use a submodule for glslang, because it doesn't guarantee API stability.
    - Switched to GTK's C++ interface, gtkmm3. This effectively drops
      GTK+ 2.0 support.
    - Joysticks can now be hotplugged. (jraby)
    
    Unix:
    - Added audio output threading. (yoffy)
    - Added screensaver prevention on joystick use. (greg-kennedy)
    - Fix YUY2 conversion. (greg-kennedy)
    - Add alsa output support. (cjacker)
    - Add I420 conversion. (cjacker)

--- emulators/snes9x/patches/patch-unix_unix.cpp
+++ emulators/snes9x/patches/patch-unix_unix.cpp
@@ -1,33 +1,24 @@
-$NetBSD: patch-unix_unix.cpp,v 1.3 2019/04/25 13:06:58 nia Exp $
+$NetBSD: patch-unix_unix.cpp,v 1.4 2022/05/03 17:09:19 nia Exp $
 
 Avoid colliding with libc functions.
 
---- unix/unix.cpp.orig	2019-04-23 18:57:18.000000000 +0000
+--- unix/unix.cpp.orig	2022-03-03 16:41:25.000000000 +0000
 +++ unix/unix.cpp
-@@ -156,7 +156,7 @@ bool S9xDisplayPollButton (uint32, bool 
- bool S9xDisplayPollAxis (uint32, int16 *);
- bool S9xDisplayPollPointer (uint32, int16 *, int16 *);
- 
--static long log2 (long);
-+static long log2_l (long);
- static void SoundTrigger (void);
- static void InitTimer (void);
- static void NSRTControllerSetup (void);
-@@ -230,7 +230,7 @@ void _makepath (char *path, const char *
- 	}
- }
+@@ -164,7 +164,7 @@ static bool8 ReadJoysticks (void);
+ void S9xLatchJSEvent();
+ #endif
  
 -static long log2 (long num)
-+static long log2_l (long num)
++static long snes9x_log2 (long num)
  {
  	long	n = 0;
  
-@@ -1356,7 +1356,7 @@ bool8 S9xOpenSoundDevice (void)
- 		return (FALSE);
- 	}
+@@ -1467,7 +1467,7 @@ bool8 S9xOpenSoundDevice (void)
+ 		bool(unixSettings.ThreadSound)
+ 	);
  
 -	J = log2(unixSettings.SoundFragmentSize) | (4 << 16);
-+	J = log2_l(unixSettings.SoundFragmentSize) | (4 << 16);
++	J = snes9x_log2(unixSettings.SoundFragmentSize) | (4 << 16);
  	if (ioctl(so.sound_fd, SNDCTL_DSP_SETFRAGMENT, &J) == -1)
  		return (FALSE);
  
