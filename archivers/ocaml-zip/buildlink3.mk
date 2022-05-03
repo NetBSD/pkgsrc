30c40b3f3ac7ad772c6ef271b841107994c117dc
jaapb <jaapb@pkgsrc.org>
2022-05-03 09:27:09 +0000

    Updated archivers/ocaml-zip to version 1.11.
    This includes a lot of minor changes and bugfixes, including better
    support for newer OCaml versions.

--- archivers/ocaml-zip/buildlink3.mk
+++ archivers/ocaml-zip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/07/19 15:15:20 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/05/03 09:27:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-zip
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-zip
 OCAML_ZIP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-zip+=	ocaml-zip>=1.05nb6
-BUILDLINK_ABI_DEPENDS.ocaml-zip+=	ocaml-zip>=1.07nb4
+BUILDLINK_ABI_DEPENDS.ocaml-zip+=	ocaml-zip>=1.11
 BUILDLINK_PKGSRCDIR.ocaml-zip?=		../../archivers/ocaml-zip
 
 .include "../../devel/zlib/buildlink3.mk"
