a1ef6bd1408347aa9b6d7b04ba4ecc09ab0fa850
jaapb <jaapb@pkgsrc.org>
2022-05-03 19:45:12 +0000

    Updated archivers/ocaml-decompress to version 1.4.3.
    Changes are mostly incremental, but some do break API compatibility:
    * different behaviour of the De module
    * the Higher returns a result value instead of raising an exception

--- archivers/ocaml-decompress/buildlink3.mk
+++ archivers/ocaml-decompress/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/02 22:54:27 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/03 19:45:12 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-decompress
 
 .if !defined(OCAML_DECOMPRESS_BUILDLINK3_MK)
 OCAML_DECOMPRESS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-decompress+=	ocaml-decompress>=0.8.1
+BUILDLINK_API_DEPENDS.ocaml-decompress+=	ocaml-decompress>=1.4.1
+BUILDLINK_API_DEPENDS.ocaml-decompress+=	ocaml-decompress>=1.4.3
 BUILDLINK_PKGSRCDIR.ocaml-decompress?=		../../archivers/ocaml-decompress
 
 .endif	# OCAML_DECOMPRESS_BUILDLINK3_MK
