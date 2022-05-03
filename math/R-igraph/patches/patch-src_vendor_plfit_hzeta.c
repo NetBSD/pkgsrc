0aefc47aac3ed3671424ac40043887989a9afffc
mef <mef@pkgsrc.org>
2022-05-03 00:24:20 +0000

    (math/R-igraph) correct the macro define for expm1l, tks tnn@
    Ref:
    http://mail-index.netbsd.org/pkgsrc-users/2022/05/02/msg035568.html

--- math/R-igraph/patches/patch-src_vendor_plfit_hzeta.c
+++ math/R-igraph/patches/patch-src_vendor_plfit_hzeta.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_vendor_plfit_hzeta.c,v 1.1 2022/04/28 15:44:47 mef Exp $
+$NetBSD: patch-src_vendor_plfit_hzeta.c,v 1.2 2022/05/03 00:24:20 mef Exp $
 
 /* tweak the macro, not found in NetBSD as of 9.99.96  */
+/* corrected by tnn. thanks */
 
 --- src/vendor/plfit/hzeta.c.orig	2022-04-20 01:23:25.000000000 +0900
 +++ src/vendor/plfit/hzeta.c	2022-04-29 00:33:15.125029161 +0900
@@ -11,7 +12,7 @@ $NetBSD: patch-src_vendor_plfit_hzeta.c,v 1.1 2022/04/28 15:44:47 mef Exp $
 +/* tweak the macro, not found in NetBSD as of 9.99.96  */
 +#ifdef __NetBSD__
 +#define log1pl(x) logl(1.0f+(x))
-+#define expm1l(x) expl(x) - 1
++#define expm1l(x) (expl(x) - 1.0f)
 +#endif
 +
  /* imported from gsl_sf_result.h */
