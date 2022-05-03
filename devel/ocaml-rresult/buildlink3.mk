f4eb641a6abeacad1585b26ae83155757f0f4302
jaapb <jaapb@pkgsrc.org>
2022-05-03 19:30:18 +0000

    Updated devel/ocaml-rresult to version 0.7.0.
    This drops the dependency on ocaml-result

--- devel/ocaml-rresult/buildlink3.mk
+++ devel/ocaml-rresult/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/05/30 10:00:07 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/03 19:30:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-rresult
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-rresult
 OCAML_RRESULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-rresult+=	ocaml-rresult>=0.6.0
-BUILDLINK_ABI_DEPENDS.ocaml-rresult+=	ocaml-rresult>=0.6.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-rresult+=	ocaml-rresult>=0.7.0
 BUILDLINK_PKGSRCDIR.ocaml-rresult?=	../../devel/ocaml-rresult
 
 .endif	# OCAML_RRESULT_BUILDLINK3_MK
