91f092863b7d32ad50ee8bdb95203ad7bb951860
jaapb <jaapb@pkgsrc.org>
2022-05-04 17:24:19 +0000

    Updated devel/ocaml-variantslib to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-variantslib/buildlink3.mk
+++ devel/ocaml-variantslib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:26 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 17:24:19 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-variantslib
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-variantslib
 OCAML_VARIANTSLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-variantslib+=	ocaml-variantslib>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-variantslib+=	ocaml-variantslib>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-variantslib?=		../../devel/ocaml-variantslib
 
 .endif	# OCAML_VARIANTSLIB_BUILDLINK3_MK
