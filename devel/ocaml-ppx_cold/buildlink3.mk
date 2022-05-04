594d28a3c3a1dd96479b4257a58f1bfa9fb26558
jaapb <jaapb@pkgsrc.org>
2022-05-04 15:24:10 +0000

    Updated devel/ocaml-ppx_cold to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-ppx_cold/buildlink3.mk
+++ devel/ocaml-ppx_cold/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/16 13:18:13 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/04 15:24:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_cold
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_cold
 OCAML_PPX_COLD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_cold+=	ocaml-ppx_cold>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_cold+=	ocaml-ppx_cold>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_cold?=	../../devel/ocaml-ppx_cold
 
 .endif	# OCAML_PPX_COLD_BUILDLINK3_MK
