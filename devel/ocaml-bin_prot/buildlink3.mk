927390a714a32d7cd82a53bb5e1a3a014cf712f5
jaapb <jaapb@pkgsrc.org>
2022-05-04 17:52:52 +0000

    Updated devel/ocaml-bin_prot to version 0.14.1.
    This includes support for the upcoming OCaml 5.0.

--- devel/ocaml-bin_prot/buildlink3.mk
+++ devel/ocaml-bin_prot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/04/08 09:19:42 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 17:52:52 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-bin_prot
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-bin_prot
 OCAML_BIN_PROT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=0.14.1
 BUILDLINK_PKGSRCDIR.ocaml-bin_prot?=	../../devel/ocaml-bin_prot
 
 .endif	# OCAML_BIN_PROT_BUILDLINK3_MK
