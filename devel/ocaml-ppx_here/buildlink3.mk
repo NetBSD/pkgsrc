faf60299c1c22d0ec7a36e700be4f6ce284d28b1
jaapb <jaapb@pkgsrc.org>
2022-05-04 16:00:58 +0000

    Updated devel/ocaml-ppx_here to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-ppx_here/buildlink3.mk
+++ devel/ocaml-ppx_here/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 12:58:31 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 16:00:58 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_here
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_here
 OCAML_PPX_HERE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_here+=	ocaml-ppx_here>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_here+=	ocaml-ppx_here>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_here+=	ocaml-ppx_here>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_here?=	../../devel/ocaml-ppx_here
 
 .endif	# OCAML_PPX_HERE_BUILDLINK3_MK
