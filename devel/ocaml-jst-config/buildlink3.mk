98ee4000ea5983e98d4bc7f9f6c94cf0a3b823c1
jaapb <jaapb@pkgsrc.org>
2022-05-04 16:37:41 +0000

    Updated devel/ocaml-jst-config to version 0.15.1.
    There is no changelog, but changes seem minor.

--- devel/ocaml-jst-config/buildlink3.mk
+++ devel/ocaml-jst-config/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:23 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 16:37:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-jst-config
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-jst-config
 OCAML_JST_CONFIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-jst-config+=	ocaml-jst-config>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-jst-config+=	ocaml-jst-config>=0.15.1
 BUILDLINK_PKGSRCDIR.ocaml-jst-config?=		../../devel/ocaml-jst-config
 
 .endif	# OCAML_JST_CONFIG_BUILDLINK3_MK
