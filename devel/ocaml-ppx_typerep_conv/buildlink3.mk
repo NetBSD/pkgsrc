afa9b5c15ad24dc7dfb3d3429b776d570698397a
jaapb <jaapb@pkgsrc.org>
2022-05-04 18:45:39 +0000

    Updated devel/ocaml-ppx_typerep_conv to version 0.15.0.
    There is no changelog, but changes are minor.

--- devel/ocaml-ppx_typerep_conv/buildlink3.mk
+++ devel/ocaml-ppx_typerep_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/12/11 09:15:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/04 18:45:39 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_typerep_conv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_typerep_conv
 OCAML_PPX_TYPEREP_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_typerep_conv+=	ocaml-ppx_typerep_conv>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_typerep_conv+=	ocaml-ppx_typerep_conv>=0.14.1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_typerep_conv+=	ocaml-ppx_typerep_conv>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_typerep_conv?=	../../devel/ocaml-ppx_typerep_conv
 
 .endif	# OCAML_PPX_TYPEREP_CONV_BUILDLINK3_MK
