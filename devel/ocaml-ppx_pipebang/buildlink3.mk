5b70ea4b0413726fbe1f9413a7975f52f5451ea1
jaapb <jaapb@pkgsrc.org>
2022-05-04 18:27:14 +0000

    Updated devel/ocaml-ppx_pipebang to version 0.15.0.
    There is no changelog, but changes are minor.

--- devel/ocaml-ppx_pipebang/buildlink3.mk
+++ devel/ocaml-ppx_pipebang/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 10:25:53 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/04 18:27:14 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_pipebang
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_pipebang
 OCAML_PPX_PIPEBANG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_pipebang+=	ocaml-ppx_pipebang>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_pipebang+=	ocaml-ppx_pipebang>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_pipebang?=	../../devel/ocaml-ppx_pipebang
 
 .endif	# OCAML_PPX_PIPEBANG_BUILDLINK3_MK
