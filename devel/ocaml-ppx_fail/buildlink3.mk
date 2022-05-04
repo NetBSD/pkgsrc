0292c05598bd77ec058d8ec041616452c2b2bfc2
jaapb <jaapb@pkgsrc.org>
2022-05-04 18:16:40 +0000

    Updated devel/ocaml-ppx_fail to version 0.14.0.
    There is no changelog, but changes are minor.

--- devel/ocaml-ppx_fail/buildlink3.mk
+++ devel/ocaml-ppx_fail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 11:24:49 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/04 18:16:40 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_fail
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_fail
 OCAML_PPX_FAIL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_fail+=	ocaml-ppx_fail>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_fail+=	ocaml-ppx_fail>=0.14.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_fail?=	../../devel/ocaml-ppx_fail
 
 .endif	# OCAML_PPX_FAIL_BUILDLINK3_MK
