0225b7b2e85b4ad2221bf41bf6263ba6385f1b85
wiz <wiz@pkgsrc.org>
2022-05-04 05:46:46 +0000

    libfilezilla: update to 0.37.2.
    0.37.2 (2022-04-29)
    
    + Added a null_logger that doesn't do anything
    - MSW: fz::mkdir with restricted permissions now uses inheritable ACLs
    - MSW: Minor performance improvements reading from fz::process and explicit cancellation of pending I/O when killing child processes.
    
    0.37.1 (2022-04-08)
    
    + Added convenience functions ot fz::json
    - MSW: Fixed spawning fz::process with blocking communication
    
    0.37.0 (2022-04-01)
    
    + *nix: password-less impersonation can take an optional group
    + Added demo https client showcasing usage of fz::socket and fz::tls_layer
    + Added some compile-time checks for fz::sprintf arguments
    + MSW: Added fz::registry
    + Added a way to use non-blocking read/write with fz::process
    - Added fz::logger_interface::log_u which assumes string arguments are in UTF-8 as opposed to the character set used by the current locale
    - Disallow spawning additional threads while fz::thread_pool is in the process of being destroyed
    - *nix: poll() is now mandatory, the select() based socket event loop has been removed
    - Simplified fz::socket implementation through use of internal fz::poller

--- net/libfilezilla/patches/patch-lib_impersonation.cpp
+++ net/libfilezilla/patches/patch-lib_impersonation.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_impersonation.cpp,v 1.2 2022/02/07 10:53:52 wiz Exp $
+$NetBSD: patch-lib_impersonation.cpp,v 1.3 2022/05/04 05:46:46 wiz Exp $
 
 Hide Linux-specific stuff in ifdef __Linux__
 https://trac.filezilla-project.org/ticket/12658
 
---- lib/impersonation.cpp.orig	2021-12-08 15:10:58.000000000 +0000
+--- lib/impersonation.cpp.orig	2022-04-01 12:35:07.000000000 +0000
 +++ lib/impersonation.cpp
 @@ -7,7 +7,7 @@
  #include <optional>
@@ -14,8 +14,8 @@ https://trac.filezilla-project.org/ticket/12658
  #include <crypt.h>
  #include <shadow.h>
  #endif
-@@ -58,7 +58,7 @@ passwd_holder get_passwd(fz::native_stri
- 	return ret;
+@@ -79,7 +79,7 @@ std::optional<gid_t> get_group(fz::nativ
+ 	return {};
  }
  
 -#if FZ_UNIX
@@ -23,7 +23,7 @@ https://trac.filezilla-project.org/ticket/12658
  struct shadow_holder {
  	shadow_holder() = default;
  	shadow_holder(shadow_holder const&) = delete;
-@@ -150,7 +150,7 @@ std::vector<gid_t> get_supplementary(std
+@@ -171,7 +171,7 @@ std::vector<gid_t> get_supplementary(std
  
  bool check_auth(fz::native_string const& username, fz::native_string const& password)
  {
