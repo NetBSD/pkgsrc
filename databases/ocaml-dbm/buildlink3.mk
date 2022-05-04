b638dcf9169ebdf04f0db85985168f07dab815c7
jaapb <jaapb@pkgsrc.org>
2022-05-04 08:56:02 +0000

    Update databases/ocaml-dbm to version 1.3.
    This migrates the distribution to github and makes other minor changes.

--- databases/ocaml-dbm/buildlink3.mk
+++ databases/ocaml-dbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2021/03/08 08:12:46 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2022/05/04 08:56:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dbm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-dbm
 OCAML_DBM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-dbm+=	ocaml-dbm>=1.0
-BUILDLINK_ABI_DEPENDS.ocaml-dbm?=	ocaml-dbm>=1.0nb18
+BUILDLINK_ABI_DEPENDS.ocaml-dbm?=	ocaml-dbm>=1.0nb21
 BUILDLINK_PKGSRCDIR.ocaml-dbm?=		../../databases/ocaml-dbm
 
 .include "../../databases/gdbm/buildlink3.mk"
