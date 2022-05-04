8cef49d36f4bd1d6ecfb978175aebac854c9e53c
jaapb <jaapb@pkgsrc.org>
2022-05-04 16:14:30 +0000

    Updated devel/ocaml-ppx_sexp_message to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-ppx_sexp_message/buildlink3.mk
+++ devel/ocaml-ppx_sexp_message/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:03:19 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 16:14:30 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_sexp_message
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_sexp_message
 OCAML_PPX_SEXP_MESSAGE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_sexp_message+=	ocaml-ppx_sexp_message>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_sexp_message+=	ocaml-ppx_sexp_message>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_sexp_message+=	ocaml-ppx_sexp_message>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_sexp_message?=	../../devel/ocaml-ppx_sexp_message
 
 .endif	# OCAML_PPX_SEXP_MESSAGE_BUILDLINK3_MK
