b417404c5c9d340d725100d12b0571d9b4cd375c
jaapb <jaapb@pkgsrc.org>
2022-05-04 15:39:58 +0000

    Updated devel/ocaml-fieldslib to version 0.15.0.
    There does not seem to be a changelog, but changes are minor.

--- devel/ocaml-fieldslib/buildlink3.mk
+++ devel/ocaml-fieldslib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:29:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 15:39:58 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-fieldslib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-fieldslib
 OCAML_FIELDSLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-fieldslib+=	ocaml-fieldslib>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-fieldslib+=	ocaml-fieldslib>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-fieldslib+=	ocaml-fieldslib>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-fieldslib?=	../../devel/ocaml-fieldslib
 .endif	# OCAML_FIELDSLIB_BUILDLINK3_MK
 
