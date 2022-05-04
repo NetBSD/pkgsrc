a0943cf17cbb9927657a189889b815fd4cb1d266
jaapb <jaapb@pkgsrc.org>
2022-05-04 18:33:21 +0000

    Updated devel/ocaml-ppx_stable to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-ppx_stable/buildlink3.mk
+++ devel/ocaml-ppx_stable/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/12/11 09:10:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/04 18:33:21 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_stable
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_stable
 OCAML_PPX_STABLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_stable+=	ocaml-ppx_stable>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_stable+=	ocaml-ppx_stable>=0.14.1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_stable+=	ocaml-ppx_stable>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_stable?=		../../devel/ocaml-ppx_stable
 
 .endif	# OCAML_PPX_STABLE_BUILDLINK3_MK
