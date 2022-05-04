5c2b2616d00d2f3a6944c1004e0cdd638338e409
jaapb <jaapb@pkgsrc.org>
2022-05-04 16:32:13 +0000

    Updated devel/ocaml-jane-street-headers to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-jane-street-headers/buildlink3.mk
+++ devel/ocaml-jane-street-headers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/17 09:19:30 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/05/04 16:32:13 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-jane-street-headers
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-jane-street-headers
 OCAML_JANE_STREET_HEADERS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-jane-street-headers+=	ocaml-jane-street-headers>=0.13.0
-BUILDLINK_ABI_DEPENDS.ocaml-jane-street-headers+=	ocaml-jane-street-headers>=0.13.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-jane-street-headers+=	ocaml-jane-street-headers>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-jane-street-headers?=		../../devel/ocaml-jane-street-headers
 .endif	# OCAML_JANE_STREET_HEADERS_BUILDLINK3_MK
 
