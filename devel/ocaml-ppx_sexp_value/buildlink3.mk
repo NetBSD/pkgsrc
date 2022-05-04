2fb37ca079638013c011ec3c2297c9487e115a06
jaapb <jaapb@pkgsrc.org>
2022-05-04 16:20:15 +0000

    Updated devel/ocaml-ppx_sexp_value to verion 0.15.0.
    There is no changelog, but changes are minor.

--- devel/ocaml-ppx_sexp_value/buildlink3.mk
+++ devel/ocaml-ppx_sexp_value/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/25 09:34:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/04 16:20:15 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_sexp_value
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_sexp_value
 OCAML_PPX_SEXP_VALUE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_sexp_value+=	ocaml-ppx_sexp_value>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_sexp_value+=	ocaml-ppx_sexp_value>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_sexp_value?=	../../devel/ocaml-ppx_sexp_value
 
 .endif	# OCAML_PPX_SEXP_VALUE_BUILDLINK3_MK
