1705b5257d3396dd3240b6ad0d2c0a9b3e4d069a
jaapb <jaapb@pkgsrc.org>
2022-05-04 14:11:35 +0000

    Updated devel/ocaml-ppx_enumerate to version 0.15.0.
    There is no changelog, but changes seem to be minor.

--- devel/ocaml-ppx_enumerate/buildlink3.mk
+++ devel/ocaml-ppx_enumerate/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/16 09:19:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/05/04 14:11:35 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_enumerate
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_enumerate
 OCAML_PPX_ENUMERATE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_enumerate?=	../../devel/ocaml-ppx_enumerate
 .endif	# OCAML_PPX_ENUMERATE_BUILDLINK3_MK
 
