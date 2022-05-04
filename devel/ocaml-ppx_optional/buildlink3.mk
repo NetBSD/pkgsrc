ede4ded1e642d894c21e614f2c2d94568284c972
jaapb <jaapb@pkgsrc.org>
2022-05-04 18:21:38 +0000

    Updated devel/ocaml-ppx_optional to version 0.15.0.
    There is no changelog, but changes are minor.

--- devel/ocaml-ppx_optional/buildlink3.mk
+++ devel/ocaml-ppx_optional/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 14:03:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 18:21:38 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_optional
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_optional
 OCAML_PPX_OPTIONAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_optional+=	ocaml-ppx_optional>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_optional+=	ocaml-ppx_optional>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_optional+=	ocaml-ppx_optional>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_optional?=	../../devel/ocaml-ppx_optional
 
 .endif	# OCAML_PPX_OPTIONAL_BUILDLINK3_MK
