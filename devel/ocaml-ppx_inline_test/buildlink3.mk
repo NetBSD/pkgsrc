b20a097280b767720d6944620ad995a25b12a103
jaapb <jaapb@pkgsrc.org>
2022-05-04 16:55:50 +0000

    Updated devel/ocaml-ppx_inline_test to version 0.15.0.
    There is no changelog. but changes seem minor.

--- devel/ocaml-ppx_inline_test/buildlink3.mk
+++ devel/ocaml-ppx_inline_test/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/12/11 08:08:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/04 16:55:50 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_inline_test
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_inline_test
 OCAML_PPX_INLINE_TEST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.14.1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_inline_test?=	../../devel/ocaml-ppx_inline_test
 
 .endif	# OCAML_PPX_INLINE_TEST_BUILDLINK3_MK
