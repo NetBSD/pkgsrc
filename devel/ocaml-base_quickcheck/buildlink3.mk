c48f447c2c1b0144f28384aeaf2eedf2c980d209
jaapb <jaapb@pkgsrc.org>
2022-05-04 17:12:41 +0000

    Updated devel/ocaml-base_quickcheck to version 0.15.0.
    There is no changelog, but changes are minor.

--- devel/ocaml-base_quickcheck/buildlink3.mk
+++ devel/ocaml-base_quickcheck/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:31:13 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/04 17:12:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base_quickcheck
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-base_quickcheck
 OCAML_BASE_QUICKCHECK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-base_quickcheck+=	ocaml-base_quickcheck>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-base_quickcheck+=	ocaml-base_quickcheck>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-base_quickcheck?=	../../devel/ocaml-base_quickcheck
 
 .endif	# OCAML_BASE_QUICKCHECK_BUILDLINK3_MK
