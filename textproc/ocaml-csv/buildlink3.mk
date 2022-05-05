1b6aec07166511939d99c46f14a16f28682e6652
jaapb <jaapb@pkgsrc.org>
2022-05-05 12:00:47 +0000

    Updated textproc/ocaml-csv to version 2.4.
    This introduces compatilibity with later OCaml verions (post-4.08), and
    several other minor bugfixes and changes.

--- textproc/ocaml-csv/buildlink3.mk
+++ textproc/ocaml-csv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2019/11/04 21:43:39 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/05/05 12:00:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-csv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-csv
 OCAML_CSV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-csv+=	ocaml-csv>=2.0
-BUILDLINK_ABI_DEPENDS.ocaml-csv+=	ocaml-csv>=2.2nb1
+BUILDLINK_ABI_DEPENDS.ocaml-csv+=	ocaml-csv>=2.4
 BUILDLINK_PKGSRCDIR.ocaml-csv?=		../../textproc/ocaml-csv
 
 .endif	# OCAML_CSV_BUILDLINK3_MK
