e7e8b853d9a1bae4d7e47e74d782ce6a6f3b3414
nia <nia@pkgsrc.org>
2022-05-05 08:31:55 +0000

    xfce4-wm: Use OPSYS_VERSION to numerically compare NetBSD versions

--- wm/xfce4-wm/hacks.mk
+++ wm/xfce4-wm/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2020/10/16 06:21:13 nia Exp $
+# $NetBSD: hacks.mk,v 1.2 2022/05/05 08:31:55 nia Exp $
 
 .if !defined(XFCE4_WM_HACKS_MK)
 XFCE4_WM_HACKS_MK=	defined
@@ -7,7 +7,7 @@ XFCE4_WM_HACKS_MK=	defined
 # whether XRes is available, not its version.
 # It's using XRes 1.2 features, but the base version installed in NetBSD 8.x
 # is too old to support that.
-.if ${OPSYS} == "NetBSD" && !empty(OS_VERSION:M[5678].*) && \
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 090000 && \
     ${X11_TYPE} == "native"
 PKG_HACKS+=	old-netbsd-xres
 CONFIGURE_ENV+=	ac_cv_lib_XRes_XResQueryClients=no
