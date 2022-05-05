bb6794431dcf55aeff0255cd37dfab320578bf39
jaapb <jaapb@pkgsrc.org>
2022-05-05 11:40:16 +0000

    Updated devel/ocaml-cstruct to version 6.1.0.
    There are many changes, including API-breaking ones (see the changelog in
    the distribution for more details). The package has also been updated to
    allow for lwt and async options.

--- devel/ocaml-cstruct/buildlink3.mk
+++ devel/ocaml-cstruct/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 11:11:31 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/05 11:40:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cstruct
 
 .if !defined(OCAML_CSTRUCT_BUILDLINK3_MK)
 OCAML_CSTRUCT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-cstruct+=	ocaml-cstruct>=3.7.0
+BUILDLINK_API_DEPENDS.ocaml-cstruct+=	ocaml-cstruct>=6.1.0
+BUILDLINK_ABI_DEPENDS.ocaml-cstruct+=	ocaml-cstruct>=6.1.0
 BUILDLINK_PKGSRCDIR.ocaml-cstruct?=	../../devel/ocaml-cstruct
 .endif	# OCAML_CSTRUCT_BUILDLINK3_MK
 
