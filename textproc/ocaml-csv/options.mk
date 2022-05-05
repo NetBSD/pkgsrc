7fbbae49d9486b8b6d7edbb9cdc37ae718d11b8c
jaapb <jaapb@pkgsrc.org>
2022-05-05 12:00:47 +0000

    Updated textproc/ocaml-csv to version 2.4.
    This introduces compatilibity with later OCaml verions (post-4.08), and
    several other minor bugfixes and changes.

--- textproc/ocaml-csv/options.mk
+++ textproc/ocaml-csv/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2019/11/04 21:43:39 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2022/05/05 12:00:47 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml-csv
 PKG_SUPPORTED_OPTIONS=	lwt
@@ -15,7 +15,7 @@ PLIST_VARS+=		lwt
 .if !empty(PKG_OPTIONS:Mlwt)
 .include "../../devel/ocaml-lwt/buildlink3.mk"
 PLIST.lwt=	yes
-DUNE_BUILD_PACKAGES+=	csv-lwt
 OPAM_INSTALL_FILES+=	csv-lwt
+OCAML_FINDLIB_DIRS+=	csv-lwt
 .else
 .endif
