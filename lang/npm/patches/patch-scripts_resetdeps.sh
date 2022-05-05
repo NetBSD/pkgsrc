3a839c03a86599d397f09a3f3fa433d3616ad652
adam <adam@pkgsrc.org>
2022-05-05 10:52:39 +0000

    npm: updated to 8.8.0
    v8.8.0 (2022-04-27)
    
    Features
    
    * feat: add install-links config definition (@nlf)
    
    Bug Fixes
    
    * fix(exec): workspaces support (@ruyadorno)
    * fix(libnpmpublish): unpublish from custom registry (@ruyadorno)
    * fix: Use node in and fallback to PATH if not found (@elibus)
    * fix: completion for deprecate cmd (@wraithgar)
    * fix: consolidate registryConfig application logic (@wraithgar)
    * fix(install): do not install invalid package name (@ruyadorno)
    * fix: normalize win32 paths before globbing (@lukekarrys)
    * fix: remove lib/utils/read-package-name.js (@wraithgar)
    
    Documentation
    
    * docs: add some more docs for --install-links (@nlf)
    * docs: explain that _auth only goes to npm registry (@wraithgar)
    * docs: include org instructions in scoped publish (@bnb)
    
    
    v8.7.0 (2022-04-13)
    
    Features
    
    * feat(config): add more npm/node information to config ls (@lukekarrys)
    * feat(config): warn on deprecated configs (@lukekarrys)
    
    Bug Fixes
    
    * fix: update readme badges (@lukekarrys)
    * fix: replace deprecated String.prototype.substr() (@CommanderRoot)
    * fix: remove dedupe --save (@wraithgar)
    * fix: do not export npm_config_include_workspace_root (@wraithgar)
    * fix(run-script): don't cascade if-present config (@ruyadorno)
    * fix(arborist): when reloading an edge, also refresh overrides (@nlf)
    * fix: skip update notifier file if not requested (@lukekarrys)
    * fix: show more information during publish dry-run (@lukekarrys)
    * fix(arborist): dont skip adding advisories to audit based on name/range (@lukekarrys)
    * fix: mitigate doctor test race condition (@wraithgar)
    * fix(ls): make --omit filter npm ls (@lukekarrys)
    
    Documentation
    
    * docs(ci): add note that configuration must be consistent between install and ci (@nlf)
    * docs: fix npm-uninstall typo (@JSKitty)

--- lang/npm/patches/patch-scripts_resetdeps.sh
+++ lang/npm/patches/patch-scripts_resetdeps.sh
@@ -1,15 +1,16 @@
-$NetBSD: patch-scripts_resetdeps.sh,v 1.1 2022/04/11 18:50:31 adam Exp $
+$NetBSD: patch-scripts_resetdeps.sh,v 1.2 2022/05/05 10:52:39 adam Exp $
 
 Don't be destructive.
 
---- scripts/resetdeps.sh.orig	2022-04-11 16:50:23.000000000 +0000
+--- scripts/resetdeps.sh.orig	2022-04-26 22:02:20.000000000 +0000
 +++ scripts/resetdeps.sh
-@@ -1,10 +1,6 @@
+@@ -1,11 +1,6 @@
  #!/bin/bash
  set -e
  set -x
 -rm -rf node_modules
 -rm -rf docs/node_modules
+-rm -rf smoke-tests/node_modules
 -rm -rf "workspaces/*/node_modules"
 -git checkout node_modules
  node . i --ignore-scripts --no-audit --no-fund
