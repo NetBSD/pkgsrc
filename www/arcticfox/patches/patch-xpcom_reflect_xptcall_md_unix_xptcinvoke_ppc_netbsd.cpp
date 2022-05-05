27fc227c5c5f7efe0653490ac267d4b5e34d4b71
nia <nia@pkgsrc.org>
2022-05-05 14:07:38 +0000

    arcticfox: update to 39.1
    This release only includes bug fixes.

--- www/arcticfox/patches/patch-xpcom_reflect_xptcall_md_unix_xptcinvoke_ppc_netbsd.cpp
+++ www/arcticfox/patches/patch-xpcom_reflect_xptcall_md_unix_xptcinvoke_ppc_netbsd.cpp
@@ -1,11 +1,10 @@
-$NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcinvoke_ppc_netbsd.cpp,v 1.1 2022/05/01 00:36:11 manu Exp $
+$NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcinvoke_ppc_netbsd.cpp,v 1.2 2022/05/05 14:07:39 nia Exp $
 
 Update to fit current code
 
---- xpcom/reflect/xptcall/md/unix/xptcinvoke_ppc_netbsd.cpp.orig	2022-04-18 02:30:50.508558042 +0000
-+++ xpcom/reflect/xptcall/md/unix/xptcinvoke_ppc_netbsd.cpp	2022-04-17 06:36:22.117064855 +0000
-@@ -109,7 +109,7 @@
-     }
+--- xpcom/reflect/xptcall/md/unix/xptcinvoke_ppc_netbsd.cpp.orig	2022-02-03 04:20:33.000000000 +0000
++++ xpcom/reflect/xptcall/md/unix/xptcinvoke_ppc_netbsd.cpp
+@@ -110,6 +110,6 @@ invoke_copy_to_stack(uint32_t* d,
  }
  
  extern "C"
--- www/arcticfox/patches/patch-xpcom_reflect_xptcall_md_unix_xptcstubs_ppc_linux.cpp
+++ www/arcticfox/patches/patch-xpcom_reflect_xptcall_md_unix_xptcstubs_ppc_linux.cpp
@@ -1,11 +1,10 @@
-$NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcstubs_ppc_linux.cpp,v 1.1 2022/05/01 00:36:11 manu Exp $
+$NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcstubs_ppc_linux.cpp,v 1.2 2022/05/05 14:07:39 nia Exp $
 
 Update to fit current code
 
---- xpcom/reflect/xptcall/md/unix/xptcstubs_ppc_linux.cpp.orig	2022-04-18 02:30:50.539220748 +0000
-+++ xpcom/reflect/xptcall/md/unix/xptcstubs_ppc_linux.cpp	2022-04-18 01:42:03.669133784 +0000
-@@ -48,8 +48,13 @@
-     NS_ASSERTION(self,"no self");
+--- xpcom/reflect/xptcall/md/unix/xptcstubs_ppc_linux.cpp.orig	2022-02-03 04:20:33.000000000 +0000
++++ xpcom/reflect/xptcall/md/unix/xptcstubs_ppc_linux.cpp
+@@ -49,6 +49,11 @@ PrepareAndDispatch(nsXPTCStubBase* self,
  
      self->mEntry->GetMethodInfo(uint16_t(methodIndex), &info);
      NS_ASSERTION(info,"no method info");
@@ -17,4 +16,3 @@ Update to fit current code
      if (! info)
          return NS_ERROR_UNEXPECTED;
  
-     paramCount = info->GetParamCount();
