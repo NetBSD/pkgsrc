349b7c613ea3fa3226997136275dc02c9766afc6
jaapb <jaapb@pkgsrc.org>
2022-05-05 11:52:00 +0000

    Updated devel/ocaml-ppx_deriving to version 5.2.1.
    This changes dependencies to use ppxlib, and introduces some more
    minor changes.

--- devel/ocaml-ppx_deriving/buildlink3.mk
+++ devel/ocaml-ppx_deriving/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/12/10 09:15:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2022/05/05 11:52:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_deriving
 
 .if !defined(OCAML_PPX_DERIVING_BUILDLINK3_MK)
 OCAML_PPX_DERIVING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.5
+BUILDLINK_API_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=5.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=5.2.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_deriving?=	../../devel/ocaml-ppx_deriving
 .endif	# OCAML_PPX_DERIVING_BUILDLINK3_MK
 
