5b4f46e5b961661a1f7e93e3691a9f79e59d61bc
jaapb <jaapb@pkgsrc.org>
2022-05-05 07:13:22 +0000

    Updated devel/ocaml-ppx_jane to version 0.15.0.
    There is no changelog, but changes seem minor (except for adding a bunch
    of new dependencies).

--- devel/ocaml-ppx_jane/buildlink3.mk
+++ devel/ocaml-ppx_jane/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 14:10:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/05 07:13:22 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_jane
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_jane
 OCAML_PPX_JANE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_jane+=	ocaml-ppx_jane>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_jane+=	ocaml-ppx_jane>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_jane+=	ocaml-ppx_jane>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_jane?=	../../devel/ocaml-ppx_jane
 
 .endif	# OCAML_PPX_JANE_BUILDLINK3_MK
