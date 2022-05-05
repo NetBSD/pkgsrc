a1f6b3d876bd0c783041fc75cf712f635ca510df
jaapb <jaapb@pkgsrc.org>
2022-05-05 07:23:10 +0000

    Update devel/ocaml-spawn to version 0.15.1.
    Upstream changes are minor bugfixes and addition.

--- devel/ocaml-spawn/buildlink3.mk
+++ devel/ocaml-spawn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/04/03 10:56:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/05 07:23:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-spawn
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-spawn
 OCAML_SPAWN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-spawn+=	ocaml-spawn>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-spawn+=	ocaml-spawn>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-spawn+=	ocaml-spawn>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-spawn?=	../../devel/ocaml-spawn
 
 .endif	# OCAML_SPAWN_BUILDLINK3_MK
