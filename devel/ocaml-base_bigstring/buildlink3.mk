f8efadcff685ebaac2a237d6a94dc124ba244d00
jaapb <jaapb@pkgsrc.org>
2022-05-05 07:18:20 +0000

    Updated devel/ocaml-base_bigstring to version 0.15.0.
    There is no changelog, but changes seem minor.

--- devel/ocaml-base_bigstring/buildlink3.mk
+++ devel/ocaml-base_bigstring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/25 09:24:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/05 07:18:20 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base_bigstring
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-base_bigstring
 OCAML_BASE_BIGSTRING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-base_bigstring+=	ocaml-base_bigstring>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-base_bigstring+=	ocaml-base_bigstring>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-base_bigstring?=	../../devel/ocaml-base_bigstring
 
 .endif	# OCAML_BASE_BIGSTRING_BUILDLINK3_MK
