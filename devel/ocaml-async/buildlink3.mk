6ae25652250104066e5c7cb20508ac9160c18273
jaapb <jaapb@pkgsrc.org>
2022-05-05 11:33:13 +0000

    Updated devel/ocaml-async to version 0.15.0.
    There is no changelog, but upstream changes seem to be minor except for
    some dependency changes.

--- devel/ocaml-async/buildlink3.mk
+++ devel/ocaml-async/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/04/08 11:55:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/05 11:33:13 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-async
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-async
 OCAML_ASYNC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-async+=	ocaml-async>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-async+=	ocaml-async>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-async+=	ocaml-async>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-async?=	../../devel/ocaml-async
 
 .endif	# OCAML_ASYNC_BUILDLINK3_MK
