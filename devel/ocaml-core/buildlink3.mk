fc7cb382715c4fabdb95e3894366e1bb2903da03
jaapb <jaapb@pkgsrc.org>
2022-05-05 07:58:36 +0000

    Updated devel/ocaml-core to version 0.15.0.
    There is no changelog, but there seem to have been quite a few changes
    in the dependency structure between ocaml-core and ocaml-core_kernel, as
    well as adding support for OCaml 4.12.

--- devel/ocaml-core/buildlink3.mk
+++ devel/ocaml-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/17 09:33:45 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/05 07:58:36 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-core
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-core
 OCAML_CORE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-core+=	ocaml-core>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-core+=	ocaml-core>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-core+=	ocaml-core>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-core?=	../../devel/ocaml-core
 
 .endif	# OCAML_CORE_BUILDLINK3_MK
