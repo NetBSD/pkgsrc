a6f43dcbdc9e8ff82a427a602c4a31d614c3cae9
nia <nia@pkgsrc.org>
2022-05-07 07:35:06 +0000

    htop: Apply a reliability hotfix.  Thanks upstream for notice.

---
+++ sysutils/htop/patches/patch-netbsd_Platform.c
@@ -0,0 +1,31 @@
+$NetBSD: patch-netbsd_Platform.c,v 1.1 2022/05/07 07:35:06 nia Exp $
+
+Ensure buffer for environment is large enough
+
+https://github.com/htop-dev/htop/pull/997
+
+--- netbsd/Platform.c.orig	2021-11-30 01:03:21.000000000 +0000
++++ netbsd/Platform.c
+@@ -303,7 +303,13 @@ char* Platform_getProcessEnv(pid_t pid) 
+    for (char** p = ptr; *p; p++) {
+       size_t len = strlen(*p) + 1;
+ 
+-      if (size + len > capacity) {
++      while (size + len > capacity) {
++         if (capacity > (SIZE_MAX / 2)) {
++            free(env);
++            env = NULL;
++            goto end;
++         }
++
+          capacity *= 2;
+          env = xRealloc(env, capacity);
+       }
+@@ -319,6 +325,7 @@ char* Platform_getProcessEnv(pid_t pid) 
+       env[size + 1] = 0;
+    }
+ 
++end:
+    (void) kvm_close(kt);
+    return env;
+ }
