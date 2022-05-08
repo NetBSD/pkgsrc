6310014c637d8c22e5a75b70c485fb7412900b22
nia <nia@pkgsrc.org>
2022-05-08 10:23:33 +0000

    jack: use numeric comparison with OPSYS_VERSION

--- audio/jack/platform.mk
+++ audio/jack/platform.mk
@@ -1,4 +1,4 @@
-# $NetBSD: platform.mk,v 1.1 2021/11/13 10:48:03 nia Exp $
+# $NetBSD: platform.mk,v 1.2 2022/05/08 10:23:33 nia Exp $
 
 .if !defined(PLATFORM_SUPPORTS_JACK)
 .  include "../../mk/bsd.fast.prefs.mk"
@@ -6,7 +6,7 @@
 JACK_PLATFORMS+=	Darwin-*-*
 JACK_PLATFORMS+=	Linux-*-*
 
-.if ${OPSYS} == "NetBSD" && empty(OPSYS_VERSION:M[5-8].*)
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} > 090000
 PLATFORM_SUPPORTS_JACK=	yes
 .endif
 
