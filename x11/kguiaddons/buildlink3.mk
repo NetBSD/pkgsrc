71a5d487253abf09671874b3fc1effbb66fd6d81
jperkin <jperkin@pkgsrc.org>
2022-05-09 09:24:50 +0000

    kguiaddons: Remove hard requirement on wayland.

--- x11/kguiaddons/buildlink3.mk
+++ x11/kguiaddons/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2022/04/18 19:10:14 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2022/05/09 09:24:50 jperkin Exp $
 
 BUILDLINK_TREE+=	kguiaddons
 
@@ -9,7 +9,12 @@ BUILDLINK_API_DEPENDS.kguiaddons+=	kguiaddons>=5.18.0
 BUILDLINK_ABI_DEPENDS.kguiaddons?=	kguiaddons>=5.80.0nb3
 BUILDLINK_PKGSRCDIR.kguiaddons?=	../../x11/kguiaddons
 
-.include "../../x11/qt5-qtwayland/buildlink3.mk"
+.include "../../devel/wayland/platform.mk"
+
+.if ${PLATFORM_SUPPORTS_WAYLAND} == "yes"
+.  include "../../x11/qt5-qtwayland/buildlink3.mk"
+.endif
+
 .include "../../x11/qt5-qtx11extras/buildlink3.mk"
 .endif	# KGUIADDONS_BUILDLINK3_MK
 
