b972dab679d2e8fdbcc4e008b492e9694da4e007
jaapb <jaapb@pkgsrc.org>
2022-05-10 08:33:34 +0000

    Updated devel/ocaml-base to version 0.15.0.
    There is no changelog, but upstream changes seem minor.

--- devel/ocaml-base/buildlink3.mk
+++ devel/ocaml-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2020/12/09 11:13:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/05/10 08:33:34 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-base
 OCAML_BASE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-base+=	ocaml-base>=0.9.3
-BUILDLINK_ABI_DEPENDS.ocaml-base+=	ocaml-base>=0.13.2
+BUILDLINK_ABI_DEPENDS.ocaml-base+=	ocaml-base>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-base?=	../../devel/ocaml-base
 .endif	# OCAML_BASE_BUILDLINK3_MK
 
