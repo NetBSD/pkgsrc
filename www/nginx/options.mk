fbfbd32f71686d3bf6581a82fc0a2f14e801ae88
nia <nia@pkgsrc.org>
2022-05-10 17:40:58 +0000

    nginx*: Enable http2 by default. Requested by various.

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.77 2022/04/27 20:39:38 wiz Exp $
+# $NetBSD: options.mk,v 1.78 2022/05/10 17:40:58 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nginx
 PKG_SUPPORTED_OPTIONS=	array-var auth-request cache-purge dav debug
@@ -8,7 +8,8 @@ PKG_SUPPORTED_OPTIONS+=	image-filter luajit mail-proxy memcache
 PKG_SUPPORTED_OPTIONS+=	naxsi njs pcre perl push realip rtmp
 PKG_SUPPORTED_OPTIONS+=	secure-link set-misc slice ssl status
 PKG_SUPPORTED_OPTIONS+=	stream-ssl-preread sub upload uwsgi
-PKG_SUGGESTED_OPTIONS=	pcre ssl
+
+PKG_SUGGESTED_OPTIONS=	http2 pcre ssl
 
 PKG_OPTIONS_LEGACY_OPTS+=	v2:http2
 
