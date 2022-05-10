df0bc78f6deffeb62f190b069673f6a1f0a9af18
jaapb <jaapb@pkgsrc.org>
2022-05-10 08:51:06 +0000

    Updated devel/js_of_ocaml to version 4.0.0.
    This includes a lot of changes - see the internal CHANGES.md file for
    details. Also included is support for OCaml 4.14, as well as a revamp
    of the internal structure and dependencies.

--- devel/js_of_ocaml/options.mk
+++ devel/js_of_ocaml/options.mk
@@ -1,21 +1,33 @@
-# $NetBSD: options.mk,v 1.7 2019/01/29 12:43:53 jaapb Exp $
+# $NetBSD: options.mk,v 1.8 2022/05/10 08:51:06 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.js_of_ocaml
-PKG_SUPPORTED_OPTIONS=	ocaml-tyxml
-PKG_SUGGESTED_OPTIONS=	ocaml-tyxml
+PKG_SUPPORTED_OPTIONS=	lwt ocaml-tyxml
+PKG_SUGGESTED_OPTIONS=	lwt ocaml-tyxml
 
 .include "../../mk/bsd.prefs.mk"
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	tyxml
+PLIST_VARS+=	lwt tyxml
+
+###
+### lwt support
+###
+.if !empty(PKG_OPTIONS:Mlwt)
+.include "../../devel/ocaml-lwt/buildlink3.mk"
+PLIST.lwt=	yes
+DUNE_BUILD_PACKAGES+=	js_of_ocaml-lwt
+OPAM_INSTALL_FILES+=	js_of_ocaml-lwt
+.else
+.endif
 
 ###
 ### TyXML support
 ###
 .if !empty(PKG_OPTIONS:Mocaml-tyxml)
-.include "../../textproc/ocaml-tyxml/buildlink3.mk"
+.include "../../devel/ocaml-react/buildlink3.mk"
 .include "../../devel/ocaml-reactiveData/buildlink3.mk"
+.include "../../textproc/ocaml-tyxml/buildlink3.mk"
 PLIST.tyxml=	yes
 DUNE_BUILD_PACKAGES+=	js_of_ocaml-tyxml
 OPAM_INSTALL_FILES+=	js_of_ocaml-tyxml
