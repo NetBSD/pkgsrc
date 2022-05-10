9dedc69c93ec3540a2b1f2c4bb36614fa7cc75be
jaapb <jaapb@pkgsrc.org>
2022-05-10 08:14:02 +0000

    Updated devel/ocaml-reactiveData to version 0.3.
    This adds support for the upcoming OCaml 5.0 and switches to dune for
    building.

--- devel/ocaml-reactiveData/buildlink3.mk
+++ devel/ocaml-reactiveData/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:14:52 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/05/10 08:14:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-reactiveData
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-reactiveData
 OCAML_REACTIVEDATA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2.1
-BUILDLINK_ABI_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2.2
+BUILDLINK_ABI_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.3
 BUILDLINK_PKGSRCDIR.ocaml-reactiveData?=	../../devel/ocaml-reactiveData
 
 .include "../../devel/ocaml-react/buildlink3.mk"
