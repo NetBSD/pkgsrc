89a282a4e357196600708b7ac033024e3a1991ab
jaapb <jaapb@pkgsrc.org>
2022-05-11 14:16:37 +0000

    Update www/ocaml-cohttp to version 5.0.0.
    There are many changes, including API-breaking ones. Full details in the
    CHANGES.md file included in the distribution.

--- www/ocaml-cohttp/options.mk
+++ www/ocaml-cohttp/options.mk
@@ -1,31 +1,46 @@
-# $NetBSD: options.mk,v 1.2 2020/01/10 21:19:20 joerg Exp $
+# $NetBSD: options.mk,v 1.3 2022/05/11 14:16:37 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml-cohttp
-PKG_SUPPORTED_OPTIONS=	async lwt js_of_ocaml
-PKG_SUGGESTED_OPTIONS=	async lwt js_of_ocaml
+PKG_SUPPORTED_OPTIONS=	lwt js_of_ocaml # async
+PKG_SUGGESTED_OPTIONS=	lwt js_of_ocaml # async
 
 .include "../../mk/bsd.prefs.mk"
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	lwt jsoo
+PLIST_VARS+=	lwt jsoo # async
 
 ###
 ### async support
 ###
-.if !empty(PKG_OPTIONS:Masync)
-DUNE_BUILD_PACKAGES+=	cohttp-async
-.endif
+# doesn't work on NetBSD for the moment
+#.if !empty(PKG_OPTIONS:Masync)
+#.include "../../devel/ocaml-async/buildlink3.mk"
+#.include "../../devel/ocaml-async_kernel/buildlink3.mk"
+#.include "../../devel/ocaml-async_unix/buildlink3.mk"
+#.include "../../devel/ocaml-base/buildlink3.mk"
+#.include "../../devel/ocaml-core_unix/buildlink3.mk"
+#.include "../../net/ocaml-ipaddr/buildlink3.mk"
+#PLIST.async=	yes
+#DUNE_BUILD_PACKAGES+=	cohttp-async
+#OPAM_INSTALL_FILES+=	cohttp-async
+#OCAML_FINDLIB_DIRS+=	cohttp-async
+#.endif
 
 ###
 ### lwt support
 ###
 .if !empty(PKG_OPTIONS:Mlwt)
+.include "../../devel/ocaml-cmdliner/buildlink3.mk"
+.include "../../devel/ocaml-fmt/buildlink3.mk"
+.include "../../devel/ocaml-logs/buildlink3.mk"
 .include "../../devel/ocaml-lwt/buildlink3.mk"
 .include "../../misc/ocaml-magic-mime/buildlink3.mk"
+.include "../../net/ocaml-conduit/buildlink3.mk"
 PLIST.lwt=	yes
 DUNE_BUILD_PACKAGES+=	cohttp-lwt cohttp-lwt-unix
 OPAM_INSTALL_FILES+=	cohttp-lwt cohttp-lwt-unix
+OCAML_FINDLIB_DIRS+=	cohttp-lwt cohttp-lwt-unix
 .endif
 
 ###
@@ -36,4 +51,5 @@ OPAM_INSTALL_FILES+=	cohttp-lwt cohttp-lwt-unix
 PLIST.jsoo=	yes
 DUNE_BUILD_PACKAGES+=	cohttp-lwt-jsoo
 OPAM_INSTALL_FILES+=	cohttp-lwt-jsoo
+OCAML_FINDLIB_DIRS+=	cohttp-lwt-jsoo
 .endif
