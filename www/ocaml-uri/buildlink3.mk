e762a3a3d9badfa96fd648490f9ee1c119682c2f
jaapb <jaapb@pkgsrc.org>
2022-05-11 10:27:50 +0000

    Updated www/ocaml-uril to version 4.2.0.
    Upstream changes are minor bugfixes and additions.

--- www/ocaml-uri/buildlink3.mk
+++ www/ocaml-uri/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/04/21 11:21:47 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/11 10:27:50 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-uri
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-uri
 OCAML_URI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-uri+=	ocaml-uri>=3.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-uri+=	ocaml-uri>=3.1.0
+BUILDLINK_ABI_DEPENDS.ocaml-uri+=	ocaml-uri>=4.2.0
 BUILDLINK_PKGSRCDIR.ocaml-uri?=		../../www/ocaml-uri
 
 .endif	# OCAML_URI_BUILDLINK3_MK
