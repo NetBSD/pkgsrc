27156bca5dcbdf34edd8e117d168639765b397f1
jperkin <jperkin@pkgsrc.org>
2022-05-11 15:12:25 +0000

    py-carbon: Remove ldap option.
    py-ldap no longer supports python27 which makes this unbuildable.

--- databases/py-carbon/options.mk
+++ databases/py-carbon/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.7 2019/11/02 15:38:09 rillig Exp $
+# $NetBSD: options.mk,v 1.8 2022/05/11 15:12:25 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-carbon
-PKG_SUPPORTED_OPTIONS=	amqp ldap memcached
+PKG_SUPPORTED_OPTIONS=	amqp memcached
 PKG_SUGGESTED_OPTIONS=	#
 
 .include "../../mk/bsd.options.mk"
@@ -11,14 +11,6 @@ PKG_SUGGESTED_OPTIONS=	#
 ###
 .if !empty(PKG_OPTIONS:Mamqp)
 DEPENDS+=	${PYPKGPREFIX}-txamqp-[0-9]*:../../net/py-txamqp
-.endif
-
-###
-### LDAP support
-###
-.if !empty(PKG_OPTIONS:Mldap)
-DEPENDS+=	${PYPKGPREFIX}-ldap-[0-9]*:../../databases/py-ldap
-PYTHON_VERSIONS_ACCEPTED= 27 # py-ldap
 .endif
 
 ###
