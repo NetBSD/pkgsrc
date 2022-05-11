104b4b0947537145096d0d2d88bff52fdf60a75f
jaapb <jaapb@pkgsrc.org>
2022-05-11 08:58:03 +0000

    Update net/ocaml-ipaddr to version 5.3.0.
    Upstream changes are mostly minor, with one breaking change in how
    Ipaddr.of_string handles its arguments. More details in the
    CHANGES.md file in the distribution.

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.20 2021/03/08 08:13:05 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2022/05/11 08:58:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
 .if !defined(OCAML_IPADDR_BUILDLINK3_MK)
 OCAML_IPADDR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=4.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=4.0.0nb1
+BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=5.3.0
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=5.3.0
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
 BUILDLINK_FILES.ocaml-ipaddr+=		lib/ocaml/site-lib/*
 
