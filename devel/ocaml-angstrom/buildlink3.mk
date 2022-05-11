74bdc4dea03224d95e179293ea8bf88d140fabb3
jaapb <jaapb@pkgsrc.org>
2022-05-11 10:20:21 +0000

    Updated devel/ocaml-angstrom to version 0.15.0.
    Upstream changes include minor bugfixes and addition, plus two API-breaking
    changes:
    * count fails if its argument is negative, rather than raising an
      exception;
    * parse_string and parse_bigstring take a required consume argument.

--- devel/ocaml-angstrom/buildlink3.mk
+++ devel/ocaml-angstrom/buildlink3.mk
@@ -1,10 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 11:07:40 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/11 10:20:21 jaapb Exp $
+
 BUILDLINK_TREE+=	ocaml-angstrom
 
 .if !defined(OCAML_ANGSTROM_BUILDLINK3_MK)
 OCAML_ANGSTROM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-angstrom+=	ocaml-angstrom>=0.11.1
+BUILDLINK_API_DEPENDS.ocaml-angstrom+=	ocaml-angstrom>=0.14.0
+BUILDLINK_ABI_DEPENDS.ocaml-angstrom+=	ocaml-angstrom>=0.15.0
 BUILDLINK_PKGSRCDIR.ocaml-angstrom?=	../../devel/ocaml-angstrom
 .endif	# OCAML_ANGSTROM_BUILDLINK3_MK
 
