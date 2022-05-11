bbf49f833c14f8281f1ad670f574eb710d2e0a57
jaapb <jaapb@pkgsrc.org>
2022-05-11 11:56:46 +0000

    Updated misc/ocaml-magic-mime to version 1.2.0.
    Upstream changes are mostly minor.

--- misc/ocaml-magic-mime/buildlink3.mk
+++ misc/ocaml-magic-mime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/24 16:29:14 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/11 11:56:46 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-magic-mime
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-magic-mime
 OCAML_MAGIC_MIME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-magic-mime+=	ocaml-magic-mime>=1.1.1
-BUILDLINK_ABI_DEPENDS.ocaml-magic-mime+=	ocaml-magic-mime>=1.1.2
+BUILDLINK_ABI_DEPENDS.ocaml-magic-mime+=	ocaml-magic-mime>=1.2.0
 BUILDLINK_PKGSRCDIR.ocaml-magic-mime?=		../../misc/ocaml-magic-mime
 .endif	# OCAML_MAGIC_MIME_BUILDLINK3_MK
 
