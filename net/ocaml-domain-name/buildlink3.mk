135626d65007c8dd64d8ebb32dbde469fefb147f
jaapb <jaapb@pkgsrc.org>
2022-05-11 08:49:56 +0000

    Updated net/ocaml-domain-name to version 0.4.0.
    This removes dependencies on fmt and astring, and fixes minor errors.

--- net/ocaml-domain-name/buildlink3.mk
+++ net/ocaml-domain-name/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/04/08 12:26:20 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/11 08:49:56 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-domain-name
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-domain-name
 OCAML_DOMAIN_NAME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-domain-name+=	ocaml-domain-name>=0.3.0
+BUILDLINK_ABI_DEPENDS.ocaml-domain-name+=	ocaml-domain-name>=0.4.0
 BUILDLINK_PKGSRCDIR.ocaml-domain-name?=		../../net/ocaml-domain-name
 
 .endif	# OCAML_DOMAIN_NAME_BUILDLINK3_MK
