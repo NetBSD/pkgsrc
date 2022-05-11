e31fc04e82e745bcdf1c79c5ae3b80021cd7bfd0
adam <adam@pkgsrc.org>
2022-05-11 11:01:39 +0000

    py-pypiserver: updated to 1.5.0
    1.5.0 (2022-05-01)
    ------------------
    - CHORE: pull CHANGES.rst from v1.4.x
    - DOC: add call for maintainers to README
    - DOC: Fixes in README examples
    - DOC: start of empty contributor documentation directory
    - ENH: added JSON Topic for use with micropython-upip
    - ENH: Backwards-compatible argparse config (not yet in use)
    - ENH: Refactor storage operations into separate Backend classes
    - ENH: Restore ability to drop hashing in new config
    - ENH: Specify doctype for generated pages.
      Fixed violation of PEP 503 warning with pip>=22.
    - ENH: Use argparse config throughout app
    - FIX: Fix silly typo in the readme :)
    - FIX: fix small typing and lint errors in config.py
    - FIX: fix version incompatibility that breaks twine in docker test
    - FIX: fix(setuptools-in-tests) add `packages=[]` in test setup
    - FIX: Gunicorn/gevent docker, log fixes, cache busting
    - MAINT: Add aggregate "tests" job
    - MAINT: Add release to PyPi job
    - MAINT: add github action for release management
    - MAINT: Add python 3.9 testing
    - MAINT: Cleanup code to python 3.6
    - MAINT: contributor update in README
    - MAINT: disable unstable test for Python 3.9
    - MAINT: Docker improvements
    - MAINT: drop standalone, drop py 2.7 and 3.5
    - MAINT: Merge branch 'v1.4.x'
    - MAINT: Push to Docker Hub from CI
    - MAINT: Refactor test_server to increase speed
    - MAINT: Run black on codebase
    - MAINT: run gh actions on PRs from forks
    - MAINT: small code style fixes
    - MAINT: Switch to GH actions

--- net/py-pypiserver/patches/patch-setup.py
+++ net/py-pypiserver/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2020/01/16 08:20:02 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2022/05/11 11:01:39 adam Exp $
 
 Do not require setuptools-git for building.
 
---- setup.py.orig	2019-09-17 18:56:54.000000000 +0000
+--- setup.py.orig	2022-05-01 18:53:34.000000000 +0000
 +++ setup.py
-@@ -20,7 +20,7 @@ tests_require = [
- if sys.version_info == (2, 7):
-     tests_require.append("mock")
+@@ -14,7 +14,7 @@ tests_require = [
+     "webtest",
+ ]
  
--setup_requires = ["setuptools", "setuptools-git >= 0.3"]
-+setup_requires = ["setuptools"]
- if sys.version_info >= (3, 5):
-     setup_requires.append("wheel >= 0.25.0")  # earlier wheels fail in 3.5
- else:
+-setup_requires = ["setuptools", "setuptools-git >= 0.3", "wheel >= 0.25.0"]
++setup_requires = ["setuptools", "wheel >= 0.25.0"]
+ 
+ 
+ def read_file(rel_path: str):
