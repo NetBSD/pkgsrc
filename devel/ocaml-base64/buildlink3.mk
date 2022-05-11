b0602b0b7c8c02aeaee7d6333ff2a2735b46dd67
jaapb <jaapb@pkgsrc.org>
2022-05-11 13:26:05 +0000

    Updated devel/ocaml-base64 to version 3.5.0.
    There is one API-breaking change:
    * Base64.alphabet is updated and returns a simple string now
    
    Other changes are minor bugfixes and improvements.

--- devel/ocaml-base64/buildlink3.mk
+++ devel/ocaml-base64/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:34:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/05/11 13:26:05 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base64
 
 .if !defined(OCAML_BASE64_BUILDLINK3_MK)
 OCAML_BASE64_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=3.0.0
-BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=3.1.0
+BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=3.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-base64+=	ocaml-base64>=3.5.0
 BUILDLINK_PKGSRCDIR.ocaml-base64?=	../../devel/ocaml-base64
 .endif	# OCAML_BASE64_BUILDLINK3_MK
 
