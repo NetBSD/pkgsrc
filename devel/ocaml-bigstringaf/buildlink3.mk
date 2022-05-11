ea44e64348ebfb26f9c718e51761ed782dd74759
jaapb <jaapb@pkgsrc.org>
2022-05-11 09:52:09 +0000

    Updated devel/ocaml-bigstringaf to version 0.9.0.
    Upstream changes are minor.

--- devel/ocaml-bigstringaf/buildlink3.mk
+++ devel/ocaml-bigstringaf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/24 16:25:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/11 09:52:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-bigstringaf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-bigstringaf
 OCAML_BIGSTRINGAF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-bigstringaf+=	ocaml-bigstringaf>=0.4.0
-BUILDLINK_ABI_DEPENDS.ocaml-bigstringaf+=	ocaml-bigstringaf>=0.6.0
+BUILDLINK_ABI_DEPENDS.ocaml-bigstringaf+=	ocaml-bigstringaf>=0.9.0
 BUILDLINK_PKGSRCDIR.ocaml-bigstringaf?=		../../devel/ocaml-bigstringaf
 
 .endif	# OCAML_BIGSTRINGAF_BUILDLINK3_MK
