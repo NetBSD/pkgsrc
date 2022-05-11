b3a5510241d63f1d78bbcd9d8247e5194fefc03b
jaapb <jaapb@pkgsrc.org>
2022-05-11 08:26:28 +0000

    Updated devel/ocaml-astring to version 0.8.5.
    Upstream changes are minor; included is support for newer OCaml versions.

--- devel/ocaml-astring/buildlink3.mk
+++ devel/ocaml-astring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:04:58 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/11 08:26:28 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-astring
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-astring
 OCAML_ASTRING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-astring+=	ocaml-astring>=0.8.3
+BUILDLINK_ABI_DEPENDS.ocaml-astring+=	ocaml-astring>=0.8.5
 BUILDLINK_PKGSRCDIR.ocaml-astring?=	../../devel/ocaml-astring
 .endif	# OCAML_ASTRING_BUILDLINK3_MK
 
