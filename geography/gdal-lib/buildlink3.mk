d9acf912c1c57da7010efd0e30922b1371d31aed
gdt <gdt@pkgsrc.org>
2022-05-13 13:28:52 +0000

    geography/gdal*: Update to 3.5.0
    ## Summary
    
    * [RFC 84](https://gdal.org/development/rfc/rfc84_cmake.html):
      Addition of a CMake build system, which deprecates the existing
      autoconf/automake and nmake build systems, that will be removed
      in GDAL 3.6.0. Users are encouraged to adopt the new CMake build system.
      Documentation of the CMake build system is at
      https://gdal.org/build_hints.html.
    * Add GDT_Int64 and GDT_UInt64 data types and handle them in MEM, GTiff, netCDF and Zarr drivers
    * Add read/write OGR Parquet (Apache Parquet) and 'Arrow' (Apache Arrow IPC File/Feather or stream) drivers. Only in CMake builds
    * Add OGR HANA database driver. Only in autoconf & cmake builds
    * Removed drivers: RDA, JPEG2000 (Jasper-based), CharLS, MG4 LIDAR, FujiBAS, IDA, INGR, ARCGEN, ArcObjects, CouchDB, Cloudant, DB2, FME, Geomedia, MDB (Java Jackess based), GTM, Ingres, MongoDB (old one. MongoDBv3 is the one to use now), REC, Walk, GMT raster, DODS raster and vector
    * GDAL and OGR GRASS drivers moved to https://github.com/OSGeo/gdal-grass repository
    * Tiger: remove deprecated write side of the driver (#4216)
    * Remove deprecated SWIG Perl bindings
    * Code linting and security fixes
    * Bump of shared lib major version
    
    ## Removed files
    
    * Remove deprecated testepsg utility (#3993)

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.75 2022/04/18 19:09:51 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.76 2022/05/13 13:28:52 gdt Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdal-lib
 GDAL_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdal-lib+=	gdal-lib>=3.0.0
-BUILDLINK_ABI_DEPENDS.gdal-lib+=	gdal-lib>=3.4.2nb1
+BUILDLINK_ABI_DEPENDS.gdal-lib+=	gdal-lib>=3.5.0
 BUILDLINK_PKGSRCDIR.gdal-lib?=		../../geography/gdal-lib
 
 pkgbase := gdal-lib
