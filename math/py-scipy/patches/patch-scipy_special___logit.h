96756223a87d48e12b448d897ecbf697ffe95f6d
tnn <tnn@pkgsrc.org>
2022-05-13 09:49:31 +0000

    py-scipy: redo NetBSD fix so it doesn't have side effects on other opsys
    Previous workaround could fail to compile when double and long double are
    effectively the same type.

--- math/py-scipy/patches/patch-scipy_special___logit.h
+++ math/py-scipy/patches/patch-scipy_special___logit.h
@@ -1,50 +1,31 @@
-$NetBSD: patch-scipy_special___logit.h,v 1.1 2022/05/03 15:14:54 tnn Exp $
+$NetBSD: patch-scipy_special___logit.h,v 1.2 2022/05/13 09:49:31 tnn Exp $
 
 py-scipy: work around undefined PLT symbol "log1pl" on NetBSD
 
---- scipy/special/_logit.h.orig	2022-01-29 22:59:08.475390400 +0000
+--- scipy/special/_logit.h.orig	2022-01-29 22:59:08.000000000 +0000
 +++ scipy/special/_logit.h
-@@ -31,15 +31,41 @@ inline T _expit(T x) {
+@@ -31,6 +31,17 @@ inline T _expit(T x) {
  // obtain accurate results (compared to the naive implementation
  // log(expit(x))).
  //
--template <typename T>
--inline T _log_expit(T x) {
-+inline npy_float _log_expit(npy_float x) {
++#if defined(__NetBSD__)
++template <typename T>
++inline T _log_expit(T x) {
++    if (x < 0.0) {
++        return x - std::log(std::exp(x) + 1.0);
++    }
++    else {
++        return -std::log(std::exp(-x) + 1.0);
++    }
++};
++#else
+ template <typename T>
+ inline T _log_expit(T x) {
      if (x < 0.0) {
-         return x - std::log1p(std::exp(x));
-     }
-     else {
+@@ -40,6 +51,7 @@ inline T _log_expit(T x) {
          return -std::log1p(std::exp(-x));
      }
--};
-+}
-+inline npy_double _log_expit(npy_double x) {
-+    if (x < 0.0) {
-+        return x - std::log1p(std::exp(x));
-+    }
-+    else {
-+        return -std::log1p(std::exp(-x));
-+    }
-+}
-+#if defined(__NetBSD__)
-+inline npy_longdouble _log_expit(npy_longdouble x) {
-+    if (x < 0.0) {
-+        return x - std::log(1.0l + std::exp(x));
-+    }
-+    else {
-+        return -std::log(1.0l + std::exp(-x));
-+    }
-+}
-+#else
-+inline npy_longdouble _log_expit(npy_longdouble x) {
-+    if (x < 0.0) {
-+        return x - std::log1p(std::exp(x));
-+    }
-+    else {
-+        return -std::log1p(std::exp(-x));
-+    }
-+}
+ };
 +#endif
  
  
