dbfba285703d01597beaefe6013fa79f73e7c68e
tnn <tnn@pkgsrc.org>
2022-05-13 10:39:35 +0000

    cpu_features: update to 0.7.0
    API Change
    [x86] Embed brand_string and mark FillX86BrandString as deprecated
    New code layout - breaking change in cpu_features_macros.h
    
    New features / Enhancements
    [cmake] Use of CMAKE_POSITION_INDEPENDENT_CODE instead of BUILD_PIC
    Add support for ZHAOXIN CPU
    Fix a getauxval comment and expand the Krait idiv workaround
    Update AArch64 features to Linux 5.10
    Detect Zen 3 (K19) cpus
    Detect AVX512 on Darwin use GetDarwinSysCtlByName("hw.optional.avx512f")
    Detect Intel's Multi-Precision Add-Carry Instruction Extensions
    CPU features for AMD
    Update uarch detection for Intel processors
    Add cache info for new AMD CPUs (0x8000001D)
    [NFC] Add bazel support to cpu_features
    Add support for FreeBSD on x86
    [cmake] cmake: use CTest default

---
+++ devel/cpu_features/patches/patch-src_impl__aarch64__linux__or__android.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_impl__aarch64__linux__or__android.c,v 1.1 2022/05/13 10:39:35 tnn Exp $
+
+Use /proc/cpuinfo on NetBSD/evbarm-aarch64 for now.
+
+--- src/impl_aarch64_linux_or_android.c.orig	2022-03-08 10:31:24.000000000 +0000
++++ src/impl_aarch64_linux_or_android.c
+@@ -15,7 +15,7 @@
+ #include "cpu_features_macros.h"
+ 
+ #ifdef CPU_FEATURES_ARCH_AARCH64
+-#if defined(CPU_FEATURES_OS_LINUX) || defined(CPU_FEATURES_OS_ANDROID)
++#if defined(CPU_FEATURES_OS_LINUX) || defined(CPU_FEATURES_OS_ANDROID) || defined(CPU_FEATURES_OS_NETBSD)
+ 
+ #include "cpuinfo_aarch64.h"
+ 
