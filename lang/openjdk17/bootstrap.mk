f4551f1d263ca7394aedcd17fb79f48d2902acb8
ryoon <ryoon@pkgsrc.org>
2022-05-13 15:17:13 +0000

    openjdk17: Fetch bootstrap kits from ftp.NetBSD.org

--- lang/openjdk17/bootstrap.mk
+++ lang/openjdk17/bootstrap.mk
@@ -1,10 +1,9 @@
-# $NetBSD: bootstrap.mk,v 1.1 2022/05/13 14:24:19 ryoon Exp $
+# $NetBSD: bootstrap.mk,v 1.2 2022/05/13 15:17:13 ryoon Exp $
 
 
 ONLY_FOR_PLATFORM+=		NetBSD-*-i386
 BOOT.nb9-i386=			bootstrap-jdk-1.17.0.2.8-netbsd-9-i386-20220506.tar.xz
-#SITES.${BOOT.nb9-i386}=		${MASTER_SITE_LOCAL:=openjdk17/}
-SITES.${BOOT.nb9-i386}=		https://www.ryoon.net/~ryoon/
+SITES.${BOOT.nb9-i386}=		${MASTER_SITE_LOCAL:=openjdk17/}
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-i386) || make(distinfo)
 DISTFILES+=			${BOOT.nb9-i386}
 EXTRACT_ONLY+=			${BOOT.nb9-i386}
@@ -12,8 +11,7 @@ EXTRACT_ONLY+=			${BOOT.nb9-i386}
 
 ONLY_FOR_PLATFORM+=		NetBSD-9.[0-8]*-x86_64
 BOOT.nb9-amd64=			bootstrap-jdk-1.17.0.2.8-netbsd-9-amd64-20220408.tar.xz
-#SITES.${BOOT.nb9-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
-SITES.${BOOT.nb9-amd64}=	https://www.ryoon.net/~ryoon/
+SITES.${BOOT.nb9-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
 .if !empty(MACHINE_PLATFORM:MNetBSD-9.[0-8]*-x86_64) || make(distinfo)
 DISTFILES+=			${BOOT.nb9-amd64}
 EXTRACT_ONLY+=			${BOOT.nb9-amd64}
@@ -21,8 +19,7 @@ EXTRACT_ONLY+=			${BOOT.nb9-amd64}
 
 ONLY_FOR_PLATFORM+=		NetBSD-9.9*-x86_64
 BOOT.nb10-amd64=		bootstrap-jdk-1.17.0.2.8-netbsd-10-amd64-20220401.tar.xz
-#SITES.${BOOT.nb10-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
-SITES.${BOOT.nb10-amd64}=	https://www.ryoon.net/~ryoon/
+SITES.${BOOT.nb10-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
 .if !empty(MACHINE_PLATFORM:MNetBSD-9.9*-x86_64) || make(distinfo)
 DISTFILES+=			${BOOT.nb10-amd64}
 EXTRACT_ONLY+=			${BOOT.nb10-amd64}
