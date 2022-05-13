7c4fa500a3aaea9bfc1a54a78f06733f3226796d
tnn <tnn@pkgsrc.org>
2022-05-13 10:39:35 +0000

    cpu_features: update to 0.7.0
    API Change
    [x86] Embed brand_string and mark FillX86BrandString as deprecated
    New code layout - breaking change in cpu_features_macros.h
    
    New features / Enhancements
    [cmake] Use of CMAKE_POSITION_INDEPENDENT_CODE instead of BUILD_PIC
    Add support for ZHAOXIN CPU
    Fix a getauxval comment and expand the Krait idiv workaround
    Update AArch64 features to Linux 5.10
    Detect Zen 3 (K19) cpus
    Detect AVX512 on Darwin use GetDarwinSysCtlByName("hw.optional.avx512f")
    Detect Intel's Multi-Precision Add-Carry Instruction Extensions
    CPU features for AMD
    Update uarch detection for Intel processors
    Add cache info for new AMD CPUs (0x8000001D)
    [NFC] Add bazel support to cpu_features
    Add support for FreeBSD on x86
    [cmake] cmake: use CTest default

--- devel/cpu_features/patches/patch-include_cpu__features__macros.h
+++ devel/cpu_features/patches/patch-include_cpu__features__macros.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-include_cpu__features__macros.h,v 1.1 2020/12/14 15:07:49 tnn Exp $
+$NetBSD: patch-include_cpu__features__macros.h,v 1.2 2022/05/13 10:39:35 tnn Exp $
 
 NetBSD support.
 
---- include/cpu_features_macros.h.orig	2020-10-15 09:09:51.000000000 +0000
+--- include/cpu_features_macros.h.orig	2022-03-08 10:31:24.000000000 +0000
 +++ include/cpu_features_macros.h
 @@ -71,6 +71,10 @@
- #define CPU_FEATURES_OS_LINUX_OR_ANDROID
+ #define CPU_FEATURES_OS_FREEBSD
  #endif
  
 +#if defined(__NetBSD__)
