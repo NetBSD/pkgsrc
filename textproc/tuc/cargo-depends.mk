37df167295ca38c15c1082891c686cff9eec68d8
pin <pin@pkgsrc.org>
2022-05-15 06:45:09 +0000

    textproc/tuc: update to 0.6.0
    [0.6.0] - 2022-05-13
        Add split-by-byte using --bytes
        Add split-by-character using --characters
        Faster performance when -p (compress delimiters) is on
        Faster performance when reading the input
        CI now fails if the linter is not satisfied
        Reviewd CI/release actions, simpler, faster
        Release binaries for ARM too

--- textproc/tuc/cargo-depends.mk
+++ textproc/tuc/cargo-depends.mk
@@ -1,15 +1,17 @@
-# $NetBSD: cargo-depends.mk,v 1.1 2022/05/03 13:01:05 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.2 2022/05/15 06:45:09 pin Exp $
 
-CARGO_CRATE_DEPENDS+=	anyhow-1.0.41
-CARGO_CRATE_DEPENDS+=	assert_cmd-1.0.5
+CARGO_CRATE_DEPENDS+=	anyhow-1.0.57
+CARGO_CRATE_DEPENDS+=	assert_cmd-2.0.4
 CARGO_CRATE_DEPENDS+=	bstr-0.2.16
-CARGO_CRATE_DEPENDS+=	difference-2.0.0
+CARGO_CRATE_DEPENDS+=	difflib-0.4.0
 CARGO_CRATE_DEPENDS+=	doc-comment-0.3.3
+CARGO_CRATE_DEPENDS+=	either-1.6.1
+CARGO_CRATE_DEPENDS+=	itertools-0.10.3
 CARGO_CRATE_DEPENDS+=	lazy_static-1.4.0
 CARGO_CRATE_DEPENDS+=	libc-0.2.97
 CARGO_CRATE_DEPENDS+=	memchr-2.4.0
 CARGO_CRATE_DEPENDS+=	pico-args-0.4.2
-CARGO_CRATE_DEPENDS+=	predicates-1.0.8
+CARGO_CRATE_DEPENDS+=	predicates-2.1.1
 CARGO_CRATE_DEPENDS+=	predicates-core-1.0.2
 CARGO_CRATE_DEPENDS+=	predicates-tree-1.0.2
 CARGO_CRATE_DEPENDS+=	regex-automata-0.1.10
