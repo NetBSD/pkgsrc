1a4d65d6615f67ffb03d35252ad3c4d2913f2ea7
tnn <tnn@pkgsrc.org>
2022-05-15 20:29:00 +0000

    openjdk17: regen bootstrap binaries to be smaller & more portable
    - build without x11
    - bundle libstdc++.so.9 & libgcc_s.so.1 from base so it works with
      base clang as well as future NetBSD releases
    - don't ship external debug symbols (.diz), demos, src.zip
    - build in clean chroot with pkgsrc bootstrapped to /root/pkg
    - upload pgp signatures to nbftp
    
    This mimics how it was done for openjdk11 &
    takes bootstraps down from ~300MB to ~100MB.

--- lang/openjdk17/bootstrap.mk
+++ lang/openjdk17/bootstrap.mk
@@ -1,33 +1,28 @@
-# $NetBSD: bootstrap.mk,v 1.2 2022/05/13 15:17:13 ryoon Exp $
+# $NetBSD: bootstrap.mk,v 1.3 2022/05/15 20:29:00 tnn Exp $
 
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 090000
+PKG_SKIP_REASON+=		"Only supports NetBSD >= 9"
+.endif
 
 ONLY_FOR_PLATFORM+=		NetBSD-*-i386
-BOOT.nb9-i386=			bootstrap-jdk-1.17.0.2.8-netbsd-9-i386-20220506.tar.xz
+BOOT.nb9-i386=			bootstrap-jdk-1.17.0.3.7-netbsd-9-i386-20220515.tar.xz
 SITES.${BOOT.nb9-i386}=		${MASTER_SITE_LOCAL:=openjdk17/}
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-i386) || make(distinfo)
 DISTFILES+=			${BOOT.nb9-i386}
 EXTRACT_ONLY+=			${BOOT.nb9-i386}
 .endif
 
-ONLY_FOR_PLATFORM+=		NetBSD-9.[0-8]*-x86_64
-BOOT.nb9-amd64=			bootstrap-jdk-1.17.0.2.8-netbsd-9-amd64-20220408.tar.xz
+ONLY_FOR_PLATFORM+=		NetBSD-*-x86_64
+BOOT.nb9-amd64=			bootstrap-jdk-1.17.0.3.7-netbsd-9-amd64-20220515.tar.xz
 SITES.${BOOT.nb9-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
-.if !empty(MACHINE_PLATFORM:MNetBSD-9.[0-8]*-x86_64) || make(distinfo)
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-x86_64) || make(distinfo)
 DISTFILES+=			${BOOT.nb9-amd64}
 EXTRACT_ONLY+=			${BOOT.nb9-amd64}
 .endif
 
-ONLY_FOR_PLATFORM+=		NetBSD-9.9*-x86_64
-BOOT.nb10-amd64=		bootstrap-jdk-1.17.0.2.8-netbsd-10-amd64-20220401.tar.xz
-SITES.${BOOT.nb10-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
-.if !empty(MACHINE_PLATFORM:MNetBSD-9.9*-x86_64) || make(distinfo)
-DISTFILES+=			${BOOT.nb10-amd64}
-EXTRACT_ONLY+=			${BOOT.nb10-amd64}
-.endif
-
-#ONLY_FOR_PLATFORM+=		NetBSD-*-aarch64
-#BOOT.nb9-aarch64=		bootstrap-jdk-1.11.0.7.10-netbsd-9-aarch64-20200509.tar.xz
-#SITES.${BOOT.nb9-aarch64}=	${MASTER_SITE_LOCAL:=openjdk11/}
+ONLY_FOR_PLATFORM+=		NetBSD-*-aarch64
+#BOOT.nb9-aarch64=		bootstrap-jdk-1.17.0.3.7-netbsd-9-aarch64-20220515.tar.xz
+#SITES.${BOOT.nb9-aarch64}=	${MASTER_SITE_LOCAL:=openjdk17/}
 #.if !empty(MACHINE_PLATFORM:MNetBSD-*-aarch64) || make(distinfo)
 #DISTFILES+=			${BOOT.nb9-aarch64}
 #EXTRACT_ONLY+=			${BOOT.nb9-aarch64}
