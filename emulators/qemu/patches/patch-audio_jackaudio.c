17a08607fb2a3980f0fb245315892b1b5fc3b945
KFL <kludev@gmail.com>
2022-05-14 20:40:55 -0700

    emulators/qemu: fix pthread_setname_np for macos
    pthread_setname_np has yet another signature for macos.

--- emulators/qemu/patches/patch-audio_jackaudio.c
+++ emulators/qemu/patches/patch-audio_jackaudio.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-audio_jackaudio.c,v 1.1 2021/12/27 10:12:57 nia Exp $
-
-Teach it about NetBSD's pthread_setname_np.
+$NetBSD$
 
 --- audio/jackaudio.c.orig	2021-12-14 20:42:02.000000000 +0000
 +++ audio/jackaudio.c
-@@ -631,7 +631,11 @@ static int qjack_thread_creator(jack_nat
+@@ -631,7 +631,13 @@ static int qjack_thread_creator(jack_nat
      }
  
      /* set the name of the thread */
 +#ifdef __NetBSD__
 +    pthread_setname_np(*thread, "%s", (void *)"jack-client");
++#elif __APPLE__
++    pthread_setname_np("jack-client");
 +#else
      pthread_setname_np(*thread, "jack-client");
 +#endif
