6d937230073400d67da8b04a55d52fdd89448805
tnn <tnn@pkgsrc.org>
2022-05-15 12:09:49 +0000

    faudio: update to 22.05. Unknown changes.

--- audio/faudio/buildlink3.mk
+++ audio/faudio/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/05/22 09:04:29 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/15 12:09:49 tnn Exp $
 
 BUILDLINK_TREE+=	faudio
 
 .if !defined(FAUDIO_BUILDLINK3_MK)
 FAUDIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.faudio+=	faudio>=20.05
+BUILDLINK_API_DEPENDS.faudio+=	faudio>=22.05
 BUILDLINK_PKGSRCDIR.faudio?=	../../audio/faudio
 .endif	# FAUDIO_BUILDLINK3_MK
 
