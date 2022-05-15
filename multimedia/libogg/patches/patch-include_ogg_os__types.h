4d93e2a4ab7093378e30430376071e3d91904f30
dholland <dholland@pkgsrc.org>
2022-05-15 05:50:49 +0000

    PR 56708 John Klos: audio/libvorbis fails on macOS El Capitan
    When including <stdint.h>, use the types it defines, not the legacy
    ones with extra (internal) underscores.

--- multimedia/libogg/patches/patch-include_ogg_os__types.h
+++ multimedia/libogg/patches/patch-include_ogg_os__types.h
@@ -1,17 +1,27 @@
-$NetBSD: patch-include_ogg_os__types.h,v 1.2 2021/06/07 09:12:32 adam Exp $
+$NetBSD: patch-include_ogg_os__types.h,v 1.3 2022/05/15 05:50:49 dholland Exp $
 
 use stdint.h on macos
-This was broken by a recent change.  See comments in:
-https://gitlab.xiph.org/xiph/ogg/issues/2294
+also use the types that are defined by stdint.h, not the legacy ones.
 
---- include/ogg/os_types.h.orig	2021-06-07 08:37:18.000000000 +0000
+See https://gitlab.xiph.org/xiph/ogg/issues/2294
+
+--- include/ogg/os_types.h.orig	2020-05-04 17:14:04.000000000 +0000
 +++ include/ogg/os_types.h
-@@ -70,7 +70,7 @@
+@@ -70,13 +70,13 @@
  
  #elif (defined(__APPLE__) && defined(__MACH__)) /* MacOS X Framework build */
  
 -#  include <sys/types.h>
 +#  include <stdint.h>
     typedef int16_t ogg_int16_t;
-    typedef u_int16_t ogg_uint16_t;
+-   typedef u_int16_t ogg_uint16_t;
++   typedef uint16_t ogg_uint16_t;
     typedef int32_t ogg_int32_t;
+-   typedef u_int32_t ogg_uint32_t;
++   typedef uint32_t ogg_uint32_t;
+    typedef int64_t ogg_int64_t;
+-   typedef u_int64_t ogg_uint64_t;
++   typedef uint64_t ogg_uint64_t;
+ 
+ #elif defined(__HAIKU__)
+ 
