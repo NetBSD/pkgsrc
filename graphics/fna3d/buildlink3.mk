4ab58f9f58372458017e5b659b56078a4a7aa018
tnn <tnn@pkgsrc.org>
2022-05-15 12:08:07 +0000

    fna3d: reset bl3. Assume this is API breaking.

--- graphics/fna3d/buildlink3.mk
+++ graphics/fna3d/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/03/28 10:44:21 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/05/15 12:08:07 tnn Exp $
 
 BUILDLINK_TREE+=	fna3d
 
 .if !defined(FNA3D_BUILDLINK3_MK)
 FNA3D_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fna3d+=	fna3d>=21.01
-BUILDLINK_ABI_DEPENDS.fna3d?=	fna3d>=21.01nb3
+BUILDLINK_API_DEPENDS.fna3d+=	fna3d>=22.05
 BUILDLINK_PKGSRCDIR.fna3d?=	../../graphics/fna3d
 
 .include "../../devel/SDL2/buildlink3.mk"
