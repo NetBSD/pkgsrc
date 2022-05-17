00106336f8ae56bb1be1b1a206375ba3bdd8b7f6
jperkin <jperkin@pkgsrc.org>
2022-05-17 13:35:47 +0000

    kopete: Support SunOS.

--- net/kopete/patches/patch-libkopete_avdevice_videodevice.h
+++ net/kopete/patches/patch-libkopete_avdevice_videodevice.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-libkopete_avdevice_videodevice.h,v 1.1 2013/11/07 13:03:07 markd Exp $
+$NetBSD: patch-libkopete_avdevice_videodevice.h,v 1.2 2022/05/17 13:35:47 jperkin Exp $
 
---- libkopete/avdevice/videodevice.h.orig	2011-04-01 10:59:27.000000000 +0000
+--- libkopete/avdevice/videodevice.h.orig	2016-12-13 01:23:22.000000000 +0000
 +++ libkopete/avdevice/videodevice.h
-@@ -75,6 +75,13 @@
+@@ -73,6 +73,13 @@
  #endif // HAVE_V4L2
  
  #endif // __linux__ __FreeBSD__ ENABLE_AV
-+#if (defined(__NetBSD__) || defined(__DragonFly__)) && defined(ENABLE_AV)
++#if (defined(__NetBSD__) || defined(__DragonFly__) || defined(__sun)) && defined(ENABLE_AV)
 +
 +#ifdef HAVE_LIBV4L2
 +#include <libv4l2.h>
