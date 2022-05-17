84a1d023136e217372c1d84171fea2af7dc807e0
jperkin <jperkin@pkgsrc.org>
2022-05-17 11:55:23 +0000

    libv4l: SunOS support.
    This also updates the Linux headers so may improve compatibility on other OS.

--- graphics/libv4l/patches/patch-lib_libv4l2_libv4l2.c
+++ graphics/libv4l/patches/patch-lib_libv4l2_libv4l2.c
@@ -1,8 +1,22 @@
-$NetBSD: patch-lib_libv4l2_libv4l2.c,v 1.1 2020/09/02 09:54:33 ryoon Exp $
+$NetBSD: patch-lib_libv4l2_libv4l2.c,v 1.2 2022/05/17 11:55:23 jperkin Exp $
 
---- lib/libv4l2/libv4l2.c.orig	2017-12-27 13:50:55.000000000 +0000
+Support SunOS.
+
+--- lib/libv4l2/libv4l2.c.orig	2020-05-02 12:16:20.000000000 +0000
 +++ lib/libv4l2/libv4l2.c
-@@ -1144,7 +1144,9 @@ int v4l2_ioctl(int fd, unsigned long int
+@@ -880,7 +880,11 @@ int v4l2_dup(int fd)
+ 	int index = v4l2_get_index(fd);
+ 
+ 	if (index == -1)
++#ifdef __sun
++		return syscall(SYS_fcntl, fd, F_DUPFD, 0);
++#else
+ 		return syscall(SYS_dup, fd);
++#endif
+ 
+ 	devices[index].open_count++;
+ 
+@@ -1144,7 +1148,9 @@ int v4l2_ioctl(int fd, unsigned long int
  		break;
  	case VIDIOC_S_STD:
  	case VIDIOC_S_INPUT:
@@ -12,7 +26,7 @@ $NetBSD: patch-lib_libv4l2_libv4l2.c,v 1.1 2020/09/02 09:54:33 ryoon Exp $
  		is_capture_request = 1;
  		stream_needs_locking = 1;
  		break;		
-@@ -1255,7 +1257,10 @@ no_capture_request:
+@@ -1255,7 +1261,10 @@ no_capture_request:
  
  	case VIDIOC_S_STD:
  	case VIDIOC_S_INPUT:
