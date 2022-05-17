81c31b1f7001e5f56be16e078cbf2ad42056aab6
khorben <khorben@pkgsrc.org>
2022-05-17 17:31:18 +0000

    deforaos-configure.mk: explicitly support configuration modes

--- devel/deforaos-configure/deforaos-configure.mk
+++ devel/deforaos-configure/deforaos-configure.mk
@@ -1,4 +1,4 @@
-# $NetBSD: deforaos-configure.mk,v 1.5 2022/04/30 21:53:23 khorben Exp $
+# $NetBSD: deforaos-configure.mk,v 1.6 2022/05/17 17:31:18 khorben Exp $
 #
 # used by audio/deforaos-mixer/Makefile
 # used by devel/deforaos-asm/Makefile
@@ -11,11 +11,23 @@ BUILD_DEPENDS+=	deforaos-configure-[0-9]*:../../devel/deforaos-configure
 DEFORAOS_CONFIGURE?=		${PREFIX}/bin/configure
 DEFORAOS_CONFIGURE_ARGS?=	-v
 DEFORAOS_CONFIGURE_DIRS?=	.
-_BUILD_DEFS+=			DEFORAOS_CONFIGURE DEFORAOS_CONFIGURE_ARGS DEFORAOS_CONFIGURE_DIRS
+DEFORAOS_CONFIGURE_MODE?=
+_BUILD_DEFS+=			DEFORAOS_CONFIGURE \
+				DEFORAOS_CONFIGURE_ARGS \
+				DEFORAOS_CONFIGURE_DIRS \
+				DEFORAOS_CONFIGURE_MODE
 _DO_CONFIGURE_TARGETS+=		do-configure-deforaos
 
 do-configure-deforaos:
-.for dir in ${DEFORAOS_CONFIGURE_DIRS}
-	@${ECHO_MSG} "Running DeforaOS configure in ${dir}"
-	@${RUN} cd ${WRKSRC} && ${DEFORAOS_CONFIGURE} ${DEFORAOS_CONFIGURE_ARGS} ${dir}
-.endfor
+.if !empty(DEFORAOS_CONFIGURE_MODE)
+	@${ECHO_MSG} "Running DeforaOS configure (${DEFORAOS_CONFIGURE_MODE})"
+	@${RUN} cd ${WRKSRC} && ${DEFORAOS_CONFIGURE} \
+		-M ${DEFORAOS_CONFIGURE_MODE} \
+		${DEFORAOS_CONFIGURE_ARGS} \
+		${DEFORAOS_CONFIGURE_DIRS}
+.else
+	@${ECHO_MSG} "Running DeforaOS configure"
+	@${RUN} cd ${WRKSRC} && ${DEFORAOS_CONFIGURE} \
+		${DEFORAOS_CONFIGURE_ARGS} \
+		${DEFORAOS_CONFIGURE_DIRS}
+.endif
