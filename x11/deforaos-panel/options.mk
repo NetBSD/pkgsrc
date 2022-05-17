ff1d222191cb416f136972a07913aad70bc4a5db
khorben <khorben@pkgsrc.org>
2022-05-17 18:01:28 +0000

    deforaos-panel: update to 0.4.2
    Changes since 0.4.1:
    
    * Renaming of the .desktop files
    * Moved the configuration file to
      ~/.config/DeforaOS/Desktop/Panel/Panel.conf
    * Build fixes and improved support for macOS
    * Support for compilation modes with DeforaOS configure
    * Improved compilation scripts (from DeforaOS configure)
    
    This also makes use of the helper for DeforaOS configure, found in
    devel/deforaos-configure/deforaos-configure.mk.

--- x11/deforaos-panel/options.mk
+++ x11/deforaos-panel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2012/12/17 00:32:49 khorben Exp $
+# $NetBSD: options.mk,v 1.2 2022/05/17 18:01:28 khorben Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.deforaos-panel
 PKG_SUPPORTED_OPTIONS=	embedded
@@ -6,5 +6,5 @@ PKG_SUPPORTED_OPTIONS=	embedded
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Membedded)
-MAKE_FLAGS+=	CPPFLAGS=-DEMBEDDED
+DEFORAOS_CONFIGURE_MODE=embedded-debug
 .endif
