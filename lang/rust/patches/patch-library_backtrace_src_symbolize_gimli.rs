101c3aca4dc7f5585d2dba76b57ec4fb78e1ed22
he <he@pkgsrc.org>
2022-05-18 20:57:28 +0000

    lang/rust: add patches so that RUST_BACKTRACE works.
    Add NetBSD to the systems which need dl_iterate_phdr(),
    and make NetBSD find the debug libraries if present.
    Also make the execinfo functions visible, but not sure
    that's used by rust.
    
    Back-ported from wip / 1.60.0, but we can't do != in
    conditionals here.

---
+++ lang/rust/patches/patch-library_backtrace_src_symbolize_gimli.rs
@@ -0,0 +1,22 @@
+$NetBSD: patch-library_backtrace_src_symbolize_gimli.rs,v 1.1 2022/05/18 20:57:28 he Exp $
+
+Do mmap also on NetBSD, and also do dl_iterate_phdr.
+
+--- library/backtrace/src/symbolize/gimli.rs.orig	2022-02-23 04:07:36.000000000 +0000
++++ library/backtrace/src/symbolize/gimli.rs
+@@ -38,6 +38,7 @@ cfg_if::cfg_if! {
+         target_os = "ios",
+         target_os = "linux",
+         target_os = "macos",
++        target_os = "netbsd",
+         target_os = "openbsd",
+         target_os = "solaris",
+         target_os = "illumos",
+@@ -177,6 +178,7 @@ cfg_if::cfg_if! {
+             target_os = "linux",
+             target_os = "fuchsia",
+             target_os = "freebsd",
++            target_os = "netbsd",
+             target_os = "openbsd",
+             all(target_os = "android", feature = "dl_iterate_phdr"),
+         ),
