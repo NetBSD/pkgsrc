2b474ae0d14ebf2ba3cd40f91756e51b00571c1d
jaapb <jaapb@pkgsrc.org>
2022-05-19 08:22:33 +0000

    Fixed build problem in textproc/ocaml-csv, revbump.
    There was a problem with modules not being built correctly according to
    options set; I've now corrected this. Thanks to mef@ for reporting this.

--- textproc/ocaml-csv/options.mk
+++ textproc/ocaml-csv/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2022/05/05 12:00:47 jaapb Exp $
+# $NetBSD: options.mk,v 1.5 2022/05/19 08:22:33 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml-csv
 PKG_SUPPORTED_OPTIONS=	lwt
@@ -10,11 +10,12 @@ PLIST_VARS+=		lwt
 .include "../../mk/bsd.options.mk"
 
 ###
-### Camlp4 support
+### Lwt support
 ###
 .if !empty(PKG_OPTIONS:Mlwt)
 .include "../../devel/ocaml-lwt/buildlink3.mk"
 PLIST.lwt=	yes
+DUNE_BUILD_PACKAGES+=	csv-lwt
 OPAM_INSTALL_FILES+=	csv-lwt
 OCAML_FINDLIB_DIRS+=	csv-lwt
 .else
