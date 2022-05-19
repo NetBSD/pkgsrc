98470209877c508cd1d24f9d71c6aa55aa131a13
jperkin <jperkin@pkgsrc.org>
2022-05-19 09:18:02 +0000

    nspr: Remove BUILDLINK_FILES.
    These were entirely redundant due to already having the correct
    BUILDLINK_LIBDIRS and BUILDLINK_INCDIRS configured, and the globs
    would break the "ls -1" used in the wrapper code.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2018/01/07 13:04:08 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2022/05/19 09:18:02 jperkin Exp $
 
 BUILDLINK_TREE+=	nspr
 
@@ -8,13 +8,8 @@ NSPR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.3.2
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
-BUILDLINK_FILES.nspr+=	lib/nspr/*
-BUILDLINK_FILES.nspr+=	include/nspr/*
-BUILDLINK_FILES.nspr+=	include/nspr/obsolete/*
-
 BUILDLINK_LIBDIRS.nspr+=	lib/nspr
 BUILDLINK_RPATHDIRS.nspr+=	lib/nspr
-
 BUILDLINK_INCDIRS.nspr+=	include/nspr
 
 .endif	# NSPR_BUILDLINK3_MK
