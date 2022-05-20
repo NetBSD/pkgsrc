29b8050f1466e467b0d1625d6a5be66d245073a6
bsiegert <bsiegert@pkgsrc.org>
2022-05-20 13:05:56 +0000

    Pullup ticket #6637 - requested by nia multimedia/libaom: security fix
    Revisions pulled up:
    - multimedia/libaom/Makefile                                    1.22
    - multimedia/libaom/distinfo                                    1.16
    - multimedia/libaom/patches/patch-aom__ports_ppc__cpudetect.c   1.3
    - multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake 1.5
    - multimedia/libaom/patches/patch-build_cmake_version.cmake     1.3
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Tue May 17 21:44:11 UTC 2022
    
       Modified Files:
       	pkgsrc/multimedia/libaom: Makefile distinfo
       	pkgsrc/multimedia/libaom/patches: patch-aom__ports_ppc__cpudetect.c
       	    patch-build_cmake_aom__configure.cmake
       	    patch-build_cmake_version.cmake
    
       Log Message:
       libaom: Update to 3.3.0
    
       2022-01-28 v3.3.0
         This release includes compression efficiency and perceptual quality
         improvements, speedup and memory optimizations, some new features, and
         several bug fixes.
    
         - New Features
           * AV1 RT: Introducing CDEF search level 5
           * Changed real time speed 4 to behave the same as real time speed 5
           * Add --deltaq-strength
           * rtc: Allow scene-change and overshoot detection for svc
           * rtc: Intra-only frame for svc
           * AV1 RT: Option 2 for codec control AV1E_SET_ENABLE_CDEF to disable
             CDEF on non-ref frames
           * New codec controls AV1E_SET_LOOPFILTER_CONTROL and
             AOME_GET_LOOPFILTER_LEVEL
           * Improvements to three pass encoding
    
         - Compression Efficiency Improvements
           * Overall compression gains: 0.6%
    
         - Perceptual Quality Improvements
           * Improves the perceptual quality of high QP encoding for delta-q mode 4
           * Auto select noise synthesis level for all intra
    
         - Speedup and Memory Optimizations
           * Added many SSE2 optimizations.
           * Good quality 2-pass encoder speedups:
             o Speed 2: 9%
             o Speed 3: 12.5%
             o Speed 4: 8%
             o Speed 5: 3%
             o Speed 6: 4%
           * Real time mode encoder speedups:
             o Speed 5: 2.6% BDRate gain, 4% speedup
             o Speed 6: 3.5% BDRate gain, 4% speedup
             o Speed 9: 1% BDRate gain, 3% speedup
             o Speed 10: 3% BDRate gain, neutral speedup
           * All intra encoding speedups (AVIF):
             o Single thread - speed 6: 8%
             o Single thread - speed 9: 15%
             o Multi thread(8) - speed 6: 14%
             o Multi thread(8) - speed 9: 34%
    
         - Bug Fixes
           * Issue 3163: Segmentation fault when using --enable-keyframe-filtering=2
           * Issue 2436: Integer overflow in av1_warp_affine_c()
           * Issue 3226: armv7 build failure due to gcc-11
           * Issue 3195: Bug report on libaom (AddressSanitizer: heap-buffer-overflow)
           * Issue 3191: Bug report on libaom (AddressSanitizer: SEGV on unknown
             address)
           * Issue 3176: Some SSE2/SADx4AvgTest.* tests fail on Windows
           * Issue 3175: Some SSE2/SADSkipTest.* tests fail on Windows

--- multimedia/libaom/patches/patch-aom__ports_ppc__cpudetect.c
+++ multimedia/libaom/patches/patch-aom__ports_ppc__cpudetect.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-aom__ports_ppc__cpudetect.c,v 1.2 2021/12/28 17:35:52 gutteridge Exp $
+$NetBSD: patch-aom__ports_ppc__cpudetect.c,v 1.2.4.1 2022/05/20 13:05:56 bsiegert Exp $
 
 Make this at least build on NetBSD.
 So far no dynamic CPU feature detection on NetBSD.
 
---- aom_ports/ppc_cpudetect.c.orig	2021-10-14 18:05:43.000000000 +0000
+--- aom_ports/ppc_cpudetect.c.orig	2022-02-15 07:20:42.000000000 +0000
 +++ aom_ports/ppc_cpudetect.c
 @@ -12,14 +12,16 @@
  #include <fcntl.h>
  #include <unistd.h>
  #include <stdint.h>
-+#if !defined(__NetBSD__)
++#ifdef __linux
  #include <asm/cputable.h>
  #include <linux/auxvec.h>
 +#endif
@@ -19,7 +19,7 @@ So far no dynamic CPU feature detection on NetBSD.
  #include "aom_ports/ppc.h"
  
 -#if CONFIG_RUNTIME_CPU_DETECT
-+#if (CONFIG_RUNTIME_CPU_DETECT && !defined(__NetBSD__))
++#if (CONFIG_RUNTIME_CPU_DETECT && defined(__linux))
  static int cpu_env_flags(int *flags) {
    char *env;
    env = getenv("AOM_SIMD_CAPS");
--- multimedia/libaom/patches/patch-build_cmake_version.cmake
+++ multimedia/libaom/patches/patch-build_cmake_version.cmake
@@ -1,19 +1,21 @@
-$NetBSD: patch-build_cmake_version.cmake,v 1.2 2021/10/09 14:47:24 nia Exp $
+$NetBSD: patch-build_cmake_version.cmake,v 1.2.4.1 2022/05/20 13:05:56 bsiegert Exp $
 
 Write the correct version number in the aom.pc file.
 
---- build/cmake/version.cmake.orig	2021-10-09 07:54:04.000000000 +0000
+--- build/cmake/version.cmake.orig	2022-02-15 07:20:42.000000000 +0000
 +++ build/cmake/version.cmake
-@@ -23,28 +23,8 @@ include("${AOM_ROOT}/build/cmake/util.cm
+@@ -23,30 +23,7 @@ include("${AOM_ROOT}/build/cmake/util.cm
  
  # Generate the version string for this run.
  unset(aom_version)
 -if(EXISTS "${GIT_EXECUTABLE}")
--  execute_process(COMMAND ${GIT_EXECUTABLE} --git-dir=${AOM_ROOT}/.git describe
+-  execute_process(COMMAND ${GIT_EXECUTABLE}
+-                          --git-dir=${AOM_ROOT}/.git describe
+-                          --match=v[0-9]*
 -                  OUTPUT_VARIABLE aom_version
 -                  ERROR_QUIET
 -                  RESULT_VARIABLE version_check_result)
- 
+-
 -  if(${version_check_result} EQUAL 0)
 -    string(STRIP "${aom_version}" aom_version)
 -
