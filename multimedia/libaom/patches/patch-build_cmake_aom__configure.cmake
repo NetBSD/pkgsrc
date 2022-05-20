20b4b7e4f12b79d940b43d83da9443f9d323fd32
bsiegert <bsiegert@pkgsrc.org>
2022-05-20 13:05:56 +0000

    Pullup ticket #6637 - requested by nia multimedia/libaom: security fix
    Revisions pulled up:
    - multimedia/libaom/Makefile                                    1.22
    - multimedia/libaom/distinfo                                    1.16
    - multimedia/libaom/patches/patch-aom__ports_ppc__cpudetect.c   1.3
    - multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake 1.5
    - multimedia/libaom/patches/patch-build_cmake_version.cmake     1.3
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Tue May 17 21:44:11 UTC 2022
    
       Modified Files:
       	pkgsrc/multimedia/libaom: Makefile distinfo
       	pkgsrc/multimedia/libaom/patches: patch-aom__ports_ppc__cpudetect.c
       	    patch-build_cmake_aom__configure.cmake
       	    patch-build_cmake_version.cmake
    
       Log Message:
       libaom: Update to 3.3.0
    
       2022-01-28 v3.3.0
         This release includes compression efficiency and perceptual quality
         improvements, speedup and memory optimizations, some new features, and
         several bug fixes.
    
         - New Features
           * AV1 RT: Introducing CDEF search level 5
           * Changed real time speed 4 to behave the same as real time speed 5
           * Add --deltaq-strength
           * rtc: Allow scene-change and overshoot detection for svc
           * rtc: Intra-only frame for svc
           * AV1 RT: Option 2 for codec control AV1E_SET_ENABLE_CDEF to disable
             CDEF on non-ref frames
           * New codec controls AV1E_SET_LOOPFILTER_CONTROL and
             AOME_GET_LOOPFILTER_LEVEL
           * Improvements to three pass encoding
    
         - Compression Efficiency Improvements
           * Overall compression gains: 0.6%
    
         - Perceptual Quality Improvements
           * Improves the perceptual quality of high QP encoding for delta-q mode 4
           * Auto select noise synthesis level for all intra
    
         - Speedup and Memory Optimizations
           * Added many SSE2 optimizations.
           * Good quality 2-pass encoder speedups:
             o Speed 2: 9%
             o Speed 3: 12.5%
             o Speed 4: 8%
             o Speed 5: 3%
             o Speed 6: 4%
           * Real time mode encoder speedups:
             o Speed 5: 2.6% BDRate gain, 4% speedup
             o Speed 6: 3.5% BDRate gain, 4% speedup
             o Speed 9: 1% BDRate gain, 3% speedup
             o Speed 10: 3% BDRate gain, neutral speedup
           * All intra encoding speedups (AVIF):
             o Single thread - speed 6: 8%
             o Single thread - speed 9: 15%
             o Multi thread(8) - speed 6: 14%
             o Multi thread(8) - speed 9: 34%
    
         - Bug Fixes
           * Issue 3163: Segmentation fault when using --enable-keyframe-filtering=2
           * Issue 2436: Integer overflow in av1_warp_affine_c()
           * Issue 3226: armv7 build failure due to gcc-11
           * Issue 3195: Bug report on libaom (AddressSanitizer: heap-buffer-overflow)
           * Issue 3191: Bug report on libaom (AddressSanitizer: SEGV on unknown
             address)
           * Issue 3176: Some SSE2/SADx4AvgTest.* tests fail on Windows
           * Issue 3175: Some SSE2/SADSkipTest.* tests fail on Windows

--- multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake
+++ multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake
@@ -1,16 +1,15 @@
-$NetBSD: patch-build_cmake_aom__configure.cmake,v 1.4 2021/10/09 14:47:24 nia Exp $
+$NetBSD: patch-build_cmake_aom__configure.cmake,v 1.4.4.1 2022/05/20 13:05:56 bsiegert Exp $
 
 - Don't disable fortify just because it's a release build.
 - Set CPU correctly on SunOS.
 - Add support for NetBSD/*arm*
 
---- build/cmake/aom_configure.cmake.orig	2021-10-09 07:54:04.000000000 +0000
+--- build/cmake/aom_configure.cmake.orig	2022-02-15 07:20:42.000000000 +0000
 +++ build/cmake/aom_configure.cmake
-@@ -66,12 +66,16 @@ if(NOT AOM_TARGET_CPU)
-                     "      CMAKE_GENERATOR=${CMAKE_GENERATOR}\n")
+@@ -67,11 +67,16 @@ if(NOT AOM_TARGET_CPU)
      endif()
    elseif(cpu_lowercase STREQUAL "i386" OR cpu_lowercase STREQUAL "x86")
--    set(AOM_TARGET_CPU "x86")
+     set(AOM_TARGET_CPU "x86")
 -  elseif(cpu_lowercase MATCHES "^arm" OR cpu_lowercase MATCHES "^mips")
 +    if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
 +      set(AOM_TARGET_CPU "x86")
@@ -26,7 +25,7 @@ $NetBSD: patch-build_cmake_aom__configure.cmake,v 1.4 2021/10/09 14:47:24 nia Ex
      set(AOM_TARGET_CPU "ppc")
    else()
      message(WARNING "The architecture ${CMAKE_SYSTEM_PROCESSOR} is not "
-@@ -321,9 +325,6 @@ else()
+@@ -322,9 +327,6 @@ else()
      add_compiler_flag_if_supported("-Werror")
    endif()
  
