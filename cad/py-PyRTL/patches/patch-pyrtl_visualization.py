5117aad2bca49ebc0e4e6a13b7616544b1bfce0f
ryoon <ryoon@pkgsrc.org>
2022-05-21 15:01:00 +0000

    py-PyRTL: Add upstream pull request URL to a patch

--- cad/py-PyRTL/patches/patch-pyrtl_visualization.py
+++ cad/py-PyRTL/patches/patch-pyrtl_visualization.py
@@ -1,6 +1,7 @@
-$NetBSD: patch-pyrtl_visualization.py,v 1.1 2022/05/19 15:59:04 ryoon Exp $
+$NetBSD: patch-pyrtl_visualization.py,v 1.2 2022/05/21 15:01:00 ryoon Exp $
 
 * Support SVG output with py-graphviz 0.19 or later too.
+  https://github.com/UCSBarchlab/PyRTL/pull/416/
 
 --- pyrtl/visualization.py.orig	2021-09-02 19:11:31.000000000 +0000
 +++ pyrtl/visualization.py
