8ae6855f880b5396acd1818ffad6be512c726467
adam <adam@pkgsrc.org>
2022-05-22 19:14:00 +0000

    py-numpy: updated to 1.22.4
    1.22.4:
    TYP, BUG: Fix ``np.lib.stride_tricks`` re-exported under the...
    TST: Bump mypy from 0.931 to 0.940
    MAINT: Explicitly re-export the types in ``numpy._typing``
    MAINT: Specify sphinx, numpydoc versions for CI doc builds
    BUG: Fix typos
    ENH, BLD: Fix math feature detection for wasm
    MAINT: Fix failing simd and cygwin tests.
    MAINT: Fix failing Python 3.8 32-bit Windows test.
    BUG: add linux guard
    BUG: Allow legacy dtypes to cast to datetime again
    BUG: Make mmap handling safer in frombuffer
    BUG: Stop using PyBytesObject.ob_shash deprecated in Python 3.11.
    ENH: Introduce numpy.core.setup_common.NPY_CXX_FLAGS
    BUG: Ensure compile errors are raised correclty
    BUG: Fix segmentation fault
    MAINT: Update doc requirements
    MAINT: Mark ``npy_memchr`` with ``no_sanitize("alignment")`` on clang
    DOC: Proposal - make the doc landing page cards more similar...
    MAINT: Update Cython version to 0.29.30.
    BUG: Fix GCC error during build configuration
    REL: Prepare for the NumPy 1.22.4 release.
    MAINT: Skip tests that fail on PyPy.

--- math/py-numpy/patches/patch-numpy_core_setup.py
+++ math/py-numpy/patches/patch-numpy_core_setup.py
@@ -1,14 +1,10 @@
-$NetBSD: patch-numpy_core_setup.py,v 1.6 2022/04/12 11:50:34 tnn Exp $
+$NetBSD: patch-numpy_core_setup.py,v 1.7 2022/05/22 19:14:00 adam Exp $
 
 Use C language (not F77 from blas_info); fixes library name on Darwin.
 
-Don't redefine -D__STDC_VERSION__=0 on the command line. It is almost
-certainly the wrong thing to do and breaks the build completely on
-modern SunOS.
-
---- numpy/core/setup.py.orig	2022-03-06 17:04:34.000000000 +0000
+--- numpy/core/setup.py.orig	2022-05-20 05:46:10.000000000 +0000
 +++ numpy/core/setup.py
-@@ -816,6 +816,7 @@ def configuration(parent_package='',top_
+@@ -868,6 +868,7 @@ def configuration(parent_package='',top_
  
      if have_blas:
          extra_info = blas_info
@@ -16,11 +12,3 @@ modern SunOS.
          # These files are also in MANIFEST.in so that they are always in
          # the source distribution independently of HAVE_CBLAS.
          common_src.extend([join('src', 'common', 'cblasfuncs.c'),
-@@ -1038,7 +1039,6 @@ def configuration(parent_package='',top_
-                          extra_objects=svml_objs,
-                          extra_info=extra_info,
-                          extra_cxx_compile_args=['-std=c++11',
--                                                 '-D__STDC_VERSION__=0',
-                                                  '-fno-exceptions',
-                                                  '-fno-rtti'])
- 
