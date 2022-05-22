287561da93f7f5523af824da6b50a5b528ca1030
adam <adam@pkgsrc.org>
2022-05-22 12:17:38 +0000

    py-gnupg: updated to 0.4.9
    0.4.9
    
    Added a status attribute to the returned object from gen_key() which is set to 'ok' if a key was successfully created, or 'key not created' if that was reported by gpg, or None in any other case.
    Provided the ability to add subkeys. Thanks to Daniel Kilimnik for the feature request and patch.
    Added keygrip values to the information collected when keys are listed. Thanks to Daniel Kilimnik for the feature request and patch.
    Added extra_args to send_keys(), recv_keys() and search_keys() to allow passing options relating to key servers.

--- security/py-gnupg/patches/patch-gnupg.py
+++ security/py-gnupg/patches/patch-gnupg.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-gnupg.py,v 1.1 2022/01/11 08:45:11 adam Exp $
+$NetBSD: patch-gnupg.py,v 1.2 2022/05/22 12:17:38 adam Exp $
 
 Look for gpg2 executable.
 
---- gnupg.py.orig	2022-01-11 08:31:14.000000000 +0000
+--- gnupg.py.orig	2022-05-20 08:01:43.000000000 +0000
 +++ gnupg.py
-@@ -832,7 +832,7 @@ class GPG(object):
-     }
- 
+@@ -874,7 +874,7 @@ class GPG(object):
      "Encapsulate access to the gpg executable"
--    def __init__(self, gpgbinary='gpg', gnupghome=None, verbose=False,
-+    def __init__(self, gpgbinary='gpg2', gnupghome=None, verbose=False,
-                  use_agent=False, keyring=None, options=None,
-                  secret_keyring=None):
-         """Initialize a GPG process wrapper.  Options are:
+ 
+     def __init__(self,
+-                 gpgbinary='gpg',
++                 gpgbinary='gpg2',
+                  gnupghome=None,
+                  verbose=False,
+                  use_agent=False,
