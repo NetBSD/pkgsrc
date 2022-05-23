0b8814d36e02e11b9a8fedc41ba3181b61551f12
jperkin <jperkin@pkgsrc.org>
2022-05-23 15:13:00 +0000

    rust: Attempt to unbreak patches.

--- lang/rust/patches/patch-library_backtrace_src_symbolize_gimli_elf.rs
+++ lang/rust/patches/patch-library_backtrace_src_symbolize_gimli_elf.rs
@@ -1,4 +1,4 @@
-$NetBSD: patch-library_backtrace_src_symbolize_gimli_elf.rs,v 1.1 2022/05/18 20:57:28 he Exp $
+$NetBSD: patch-library_backtrace_src_symbolize_gimli_elf.rs,v 1.2 2022/05/23 15:13:00 jperkin Exp $
 
 Make NetBSD also find its debug libraries, if present.
 
@@ -10,13 +10,13 @@ Make NetBSD also find its debug libraries, if present.
  
 +#[cfg(target_os = "netbsd")]
 +const DEBUG_PATH: &[u8] = b"/usr/libdata/debug";
-+#[cfg(any(target_os = "freebsd", target_os = "linux"))]
++#[cfg(not(target_os = "netbsd"))]
  const DEBUG_PATH: &[u8] = b"/usr/lib/debug";
  
  fn debug_path_exists() -> bool {
      cfg_if::cfg_if! {
 -        if #[cfg(any(target_os = "freebsd", target_os = "linux"))] {
-+        if #[cfg(any(target_os = "freebsd", 
++        if #[cfg(any(target_os = "freebsd",
 +                     target_os = "netbsd",
 +                     target_os = "linux"))] {
              use core::sync::atomic::{AtomicU8, Ordering};
