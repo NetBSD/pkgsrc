a27716cbea2fd283643e903fc1cc136a80b058c9
jaapb <jaapb@pkgsrc.org>
2022-05-23 15:27:15 +0000

    Updated devel/js_of_ocaml-ocamlbuild to be a build dependency only

--- devel/js_of_ocaml-ocamlbuild/buildlink3.mk
+++ devel/js_of_ocaml-ocamlbuild/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/05/23 15:09:44 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/23 15:27:15 jaapb Exp $
 
 BUILDLINK_TREE+=	js_of_ocaml-ocamlbuild
 
@@ -7,6 +7,9 @@ JS_OF_OCAML_OCAMLBUILD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.js_of_ocaml-ocamlbuild+=	js_of_ocaml-ocamlbuild>=0.0.0.3693
 BUILDLINK_PKGSRCDIR.js_of_ocaml-ocamlbuild?=	../../devel/js_of_ocaml-ocamlbuild
+
+# We only need this to install
+BUILDLINK_DEPMETHOD.js_of_ocaml-ocamlbuild?=	build
 
 .include "../../devel/ocamlbuild/buildlink3.mk"
 .endif	# JS_OF_OCAML_OCAMLBUILD_BUILDLINK3_MK
