56d72b6562c0ee02598c76e8fefb4722f8887ca1
ryoon <ryoon@pkgsrc.org>
2022-05-23 17:28:41 +0000

    py-numpy: Workaround undefined-symbol errors under NetBSD at least
    py-numpy-1.22.4's symbol check mechanism does not work for NetBSD
    and the other platforms, because gcc and clang calculate trivial math
    functions, for example, log1pl() and expm1l(), with immediate arguments,
    for example 0. So compiling 'log1pl(0)' is always successful
    even if -lm is not added to the compiler's commandline arguments.
    Use GNU autotools style check with ugly workaround.
    I have no idea about WASM case. However current mechanism will
    not work as expected.
    Bump PKGREVISION.

--- math/py-numpy/patches/patch-numpy_core_setup.py
+++ math/py-numpy/patches/patch-numpy_core_setup.py
@@ -1,9 +1,18 @@
-$NetBSD: patch-numpy_core_setup.py,v 1.7 2022/05/22 19:14:00 adam Exp $
+$NetBSD: patch-numpy_core_setup.py,v 1.8 2022/05/23 17:28:41 ryoon Exp $
 
 Use C language (not F77 from blas_info); fixes library name on Darwin.
 
---- numpy/core/setup.py.orig	2022-05-20 05:46:10.000000000 +0000
+--- numpy/core/setup.py.orig	2022-05-19 22:46:10.000000000 +0000
 +++ numpy/core/setup.py
+@@ -217,7 +217,7 @@ def check_math_capabilities(config, ext,
+             f, args, headers, m = tup[0], tup[1], [tup[2]], fname2def(tup[0])
+         else:
+             f, args, headers, m = tup[0], tup[1], [tup[2]], fname2def(tup[3])
+-        if config.check_func(f, decl=False, call=True, call_args=args,
++        if config.check_func(f, decl=False, call=False, call_args=args,
+                              headers=headers):
+             moredefs.append((m, 1))
+ 
 @@ -868,6 +868,7 @@ def configuration(parent_package='',top_
  
      if have_blas:
