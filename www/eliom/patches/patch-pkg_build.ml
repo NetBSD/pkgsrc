c8d6526e3f22e69679c3a3bff35590eddcdd5a62
jaapb <jaapb@pkgsrc.org>
2022-05-23 15:31:34 +0000

    Updated www/eliom to version 10.0.0.
    There are not as many changes as might be expected from the large
    version increase; mostly compatibility updates and minor fixes.

--- www/eliom/patches/patch-pkg_build.ml
+++ www/eliom/patches/patch-pkg_build.ml
@@ -1,14 +1,16 @@
-$NetBSD: patch-pkg_build.ml,v 1.4 2020/04/21 12:52:06 jaapb Exp $
+$NetBSD: patch-pkg_build.ml,v 1.5 2022/05/23 15:31:34 jaapb Exp $
 
 Do not install mime types
 
---- pkg/build.ml.orig	2020-03-11 10:55:00.000000000 +0000
+--- pkg/build.ml.orig	2022-03-10 16:39:39.000000000 +0000
 +++ pkg/build.ml
-@@ -104,7 +104,6 @@ let () =
- 
-     Pkg.doc "README.md";
-     Pkg.doc "CHANGES";
--    Pkg.etc "pkg/etc/mime.types"
-   ] @ (
-     List.flatten (
-       List.map (fun (name,files) ->
+@@ -116,8 +116,7 @@ let () =
+            server_extra
+     @ [ (* MISC *)
+         Pkg.doc "README.md"
+-      ; Pkg.doc "CHANGES"
+-      ; Pkg.etc "pkg/etc/mime.types" ]
++      ; Pkg.doc "CHANGES" ]
+     @ List.flatten
+         (List.map
+            (fun (name, files) ->
