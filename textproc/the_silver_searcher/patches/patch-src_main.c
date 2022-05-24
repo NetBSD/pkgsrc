fd49530cab43d460b4ce889d2de021503f4655c4
charlotte <charlotte@pkgsrc.org>
2022-05-24 06:08:35 +0000

    the_silver_searcher: Handle cpuset_t quirk with DragonFly

---
+++ textproc/the_silver_searcher/patches/patch-src_main.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_main.c,v 1.1 2022/05/24 06:08:35 charlotte Exp $
+
+DragonFly also has a particular 'cpuset' type.
+--- src/main.c.orig	2022-05-23 22:57:49.749747000 -0700
++++ src/main.c	2022-05-23 22:58:26.439723000 -0700
+@@ -156,7 +156,7 @@
+             if (opts.use_thread_affinity) {
+ #ifdef __linux__
+                 cpu_set_t cpu_set;
+-#elif __FreeBSD__
++#elif __FreeBSD__ || __DragonFly__
+                 cpuset_t cpu_set;
+ #endif
+                 CPU_ZERO(&cpu_set);
