26e834e25aae7786daba44da3ad21e404569df04
jaapb <jaapb@pkgsrc.org>
2022-05-24 18:51:47 +0000

    Recursive revbump associated with update of ocaml.
    Also change of mk/ocaml.mk to lang/ocaml/ocaml.mk.

--- devel/ocaml-ppx_fields_conv/buildlink3.mk
+++ devel/ocaml-ppx_fields_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/12/11 08:06:15 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/05/24 18:51:50 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_fields_conv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_fields_conv
 OCAML_PPX_FIELDS_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_fields_conv+=	ocaml-ppx_fields_conv>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_fields_conv+=	ocaml-ppx_fields_conv>=0.14.2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_fields_conv+=	ocaml-ppx_fields_conv>=0.14.2nb2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_fields_conv?=	../../devel/ocaml-ppx_fields_conv
 
 .include "../../devel/ocaml-fieldslib/buildlink3.mk"
