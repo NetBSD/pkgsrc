48a3f8ccf167aea854ff8f6e976c5b82733bc19b
jaapb <jaapb@pkgsrc.org>
2022-05-24 17:55:23 +0000

    Updated math/ocaml-zarith to version 1.12.
    Changes are mostly minor, see the Changes file inside the distribution
    for more details.

--- math/ocaml-zarith/buildlink3.mk
+++ math/ocaml-zarith/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/11/28 12:04:35 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/05/24 17:55:23 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-zarith
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-zarith
 OCAML_ZARITH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-zarith+=	ocaml-zarith>=1.2.1
-BUILDLINK_ABI_DEPENDS.ocaml-zarith+=	ocaml-zarith>=1.7
+BUILDLINK_ABI_DEPENDS.ocaml-zarith+=	ocaml-zarith>=1.12
 BUILDLINK_PKGSRCDIR.ocaml-zarith?=	../../math/ocaml-zarith
 
 .include "../../devel/gmp/buildlink3.mk"
