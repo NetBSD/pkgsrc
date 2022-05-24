e2bca5b51f171a4e6e6b34d235edd92847bedeef
jaapb <jaapb@pkgsrc.org>
2022-05-24 17:37:50 +0000

    Corrected mistake in buildlink3.mk for archivers/ocaml-decompress

--- archivers/ocaml-decompress/buildlink3.mk
+++ archivers/ocaml-decompress/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/05/03 19:45:12 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/24 17:37:50 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-decompress
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-decompress
 OCAML_DECOMPRESS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-decompress+=	ocaml-decompress>=1.4.1
-BUILDLINK_API_DEPENDS.ocaml-decompress+=	ocaml-decompress>=1.4.3
+BUILDLINK_ABI_DEPENDS.ocaml-decompress+=	ocaml-decompress>=1.4.3
 BUILDLINK_PKGSRCDIR.ocaml-decompress?=		../../archivers/ocaml-decompress
 
 .endif	# OCAML_DECOMPRESS_BUILDLINK3_MK
