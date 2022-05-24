dc07979001b8601e987b67d5b97b4a775c9c55d8
nia <nia@pkgsrc.org>
2022-05-24 09:53:02 +0000

    various hacks.mk: use OPSYS_VERSION

--- textproc/CRF++/hacks.mk
+++ textproc/CRF++/hacks.mk
@@ -1,10 +1,10 @@
-# $NetBSD: hacks.mk,v 1.2 2019/11/04 21:43:32 rillig Exp $
+# $NetBSD: hacks.mk,v 1.3 2022/05/24 09:53:02 nia Exp $
 
 .if !defined(CRFPP_HACKS_MK)
 CRFPP_HACKS_MK=	# defined
 
 # No TLS support on NetBSD<6
-.if !empty(MACHINE_PLATFORM:MNetBSD-[0-5].*-*)
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 060000
 PKG_HACKS+=			netbsd5-disable_tls
 SUBST_CLASSES+=			disable_tls
 SUBST_STAGE.disable_tls=	pre-configure
