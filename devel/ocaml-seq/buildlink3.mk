e73ad29f53692a3b9fb188d476f90f4ab614b302
jaapb <jaapb@pkgsrc.org>
2022-05-24 17:48:00 +0000

    Fixed error in buildlink3.mk for devel/ocaml-seq

--- devel/ocaml-seq/buildlink3.mk
+++ devel/ocaml-seq/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/05/02 13:53:44 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/05/24 17:48:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-seq
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-seq
 OCAML_SEQ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-seq+=	ocaml-seq>=0.1
-BUILDLINK_API_DEPENDS.ocaml-seq+=	ocaml-seq>=0.2.2
+BUILDLINK_ABI_DEPENDS.ocaml-seq+=	ocaml-seq>=0.2.2
 BUILDLINK_PKGSRCDIR.ocaml-seq?=		../../devel/ocaml-seq
 
 .endif	# OCAML_SEQ_BUILDLINK3_MK
