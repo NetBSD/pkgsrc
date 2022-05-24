dfd51346dfe139174e091fb5e568b73a1a6e6c74
jaapb <jaapb@pkgsrc.org>
2022-05-24 17:39:15 +0000

    Small correction in devel/ocaml-core_unix; revbump.
    Findlib usage was not correctly specified.

--- devel/ocaml-core_unix/buildlink3.mk
+++ devel/ocaml-core_unix/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/05/05 09:26:58 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/05/24 17:39:15 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-core_unix
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-core_unix
 OCAML_CORE_UNIX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-core_unix+=	ocaml-core_unix>=0.15.0
+BUILDLINK_ABI_DEPENDS.ocaml-core_unix+=	ocaml-core_unix>=0.15.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-core_unix?=	../../devel/ocaml-core_unix
 
 .endif	# OCAML_CORE_UNIX_BUILDLINK3_MK
