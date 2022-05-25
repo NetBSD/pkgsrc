47e6c192cba721eb7d86f8954046b264c2a4d572
osa <osa@pkgsrc.org>
2022-05-25 10:48:47 +0000

    www/nginx: update to the recent stable version, 1.22.0
    Also, update third-party modules:
    o) http_push from 1.2.12 to 1.2.15
    o) lua from 0.10.20 to 0.10.21
    o) njs from 0.7.3 to 0.7.4

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.79 2022/05/10 18:08:52 nia Exp $
+# $NetBSD: options.mk,v 1.80 2022/05/25 10:48:47 osa Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nginx
 PKG_SUPPORTED_OPTIONS=	array-var auth-request cache-purge dav debug
@@ -143,7 +143,7 @@ DISTFILES+=		${NDK_DISTFILE}
 .endif
 
 .if !empty(PKG_OPTIONS:Mluajit) || make(makesum) || make(mdi) || make(distclean)
-LUA_VERSION=		0.10.20
+LUA_VERSION=		0.10.21
 LUA_DISTNAME=		lua-nginx-module-${LUA_VERSION}
 LUA_DISTFILE=		${LUA_DISTNAME}.tar.gz
 SITES.${LUA_DISTFILE}=	-${MASTER_SITE_GITHUB:=openresty/lua-nginx-module/archive/}v${LUA_VERSION}.tar.gz
@@ -235,7 +235,7 @@ CONFIGURE_ARGS+=	--without-http_uwsgi_module
 .endif
 
 .if !empty(PKG_OPTIONS:Mpush) || make(makesum) || make(mdi) || make(distclean)
-PUSH_VERSION=		1.2.12
+PUSH_VERSION=		1.2.15
 PUSH_DISTNAME=		nchan-${PUSH_VERSION}
 PUSH_DISTFILE=		${PUSH_DISTNAME}.tar.gz
 SITES.${PUSH_DISTFILE}=	-${MASTER_SITE_GITHUB:=slact/nchan/archive/}v${PUSH_VERSION}.tar.gz
@@ -311,7 +311,7 @@ PLIST.rtmp=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mnjs) || make(makesum) || make(mdi) || make(distclean)
-NJS_VERSION=		0.7.3
+NJS_VERSION=		0.7.4
 NJS_DISTNAME=		njs-${NJS_VERSION}
 NJS_DISTFILE=		${NJS_DISTNAME}.tar.gz
 NJS_CONFIGURE_ARGS=	--no-pcre2
