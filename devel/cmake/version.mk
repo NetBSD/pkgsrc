3fa7c7ebaf0917e6f92677afb339d50ab7bcd4d9
adam <adam@pkgsrc.org>
2022-05-26 08:53:59 +0000

    cmake: updated to 3.23.2
    CMake 3.23.2
    * CPack/NuGet: Restore component packaging on Windows
    * cmInstallCommand: tweak error message
    * cmExportBuildFileGenerator: handle genex-wrapped source paths
    * ci: use CMake 3.23.1
    * CheckLinkerFlag: Catch linker warning when the checked flag is ignored
    * CheckCompilerFlags: Revert "Catch linker warning about ignored flags"
    * Tests: Teach RunCMake to ignore Xcode extension point warnings
    * gitlab-ci: update macOS jobs to use Xcode 13.3
    * FindBoost: Add support for Boost 1.79
    * Utilities/Sphinx: Clarify names of variables listing post-sphinx commands
    * Utilities/Sphinx: Add variables listing pre-sphinx commands
    * Tutorial: Provide a source archive when published on cmake.org
    * cmGeneratedFileStream: Do not remove empty path
    * FindPkgConfig: Fix preservation of ENV{PKG_CONFIG_ALLOW_SYSTEM_LIBS}
    * gitlab-ci: Update Windows builds to MSVC 19.32 toolset
    * VS: Fix CUDA CodeGeneration field for CMAKE_CUDA_ARCHITECTURES=OFF
    * FortranCInterface: Fix failure with gfortran 12 and Clang
    * Clang: Use -imsvc flag without space to support Clang tools
    * file(GET_RUNTIME_DEPENDENCIES): Support VS 2022 without VS 2019
    * libarchive: Update script to get 3.5.3
    * libarchive: include archive_platform.h first in blake2s sources
    * libarchive: Update build within CMake after changes in 3.5.3
    * productbuild: Restore CPACK_PACKAGEMAKER_CHOICES variable
    * Help: Document when HIP_COMPILER generator expressions were added
    * Tests: Ignore all classes in Xcode internal objc warnings
    * CPackIFW: Fix regression in icon file names
    * FILE_SET: Make INTERFACE libraries with HEADER_SETS participate in buildsystem
    * Help: Remove references to MODULE in $ doc
    * LibArchive 2022-02-08 (673c1eae)
    * FindPython: Add support for pypy v7.3.9 and uppers
    * FindPython: fix typo error
    * FindJava, FindJNI: ensure correct handling of versions on Windows
    * Help/guide: Fix wrong header file reference
    * ccmake: Move Initialization of Fields Inline
    * ccmake: Fix crash when deleting all cache entries
    * ccmake: Fix infinite loop during invalid search
    * FindPython: add support for Python 3.12
    * CMakePackageConfigHelpers: Fix note about version files supporting ranges

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.26 2022/04/18 11:01:33 adam Exp $
+# $NetBSD: version.mk,v 1.27 2022/05/26 08:53:59 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.23.1
+CMAKE_VERSION=	3.23.2
 CMAKE_API=	${CMAKE_VERSION:R}
