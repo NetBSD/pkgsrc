f225db3ead80706943371199dbaa7e81db3024a5
triaxx <triaxx@pkgsrc.org>
2022-05-26 21:08:37 +0000

    dotclear: Update to 2.22
    upstream changes:
    -----------------
      * PHP 7.4+ is required, PHP 8.0/8.1 compliance
      * Bug fixes

--- www/php-dotclear/patches/patch-inc_prepend.php
+++ www/php-dotclear/patches/patch-inc_prepend.php
@@ -1,14 +1,14 @@
-$NetBSD: patch-inc_prepend.php,v 1.2 2022/01/25 08:31:38 triaxx Exp $
+$NetBSD: patch-inc_prepend.php,v 1.3 2022/05/26 21:08:38 triaxx Exp $
 
 Honor PKG_SYSCONFDIR.
 
---- inc/prepend.php.orig	2021-02-13 10:32:13.000000000 +0000
+--- inc/prepend.php.orig	2022-05-13 06:26:03.000000000 +0000
 +++ inc/prepend.php
-@@ -127,7 +127,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
+@@ -128,7 +128,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
  } elseif (isset($_SERVER['REDIRECT_DC_RC_PATH'])) {
      define('DC_RC_PATH', $_SERVER['REDIRECT_DC_RC_PATH']);
  } else {
--    define('DC_RC_PATH', dirname(__FILE__) . '/config.php');
+-    define('DC_RC_PATH', __DIR__ . '/config.php');
 +    define('DC_RC_PATH', '@PKG_SYSCONFDIR@/config.php');
  }
  
