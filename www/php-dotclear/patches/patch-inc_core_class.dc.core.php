d798aafb78e64e6928249ef8bcfeaaf0535aeda5
triaxx <triaxx@pkgsrc.org>
2022-05-26 21:08:37 +0000

    dotclear: Update to 2.22
    upstream changes:
    -----------------
      * PHP 7.4+ is required, PHP 8.0/8.1 compliance
      * Bug fixes

--- www/php-dotclear/patches/patch-inc_core_class.dc.core.php
+++ www/php-dotclear/patches/patch-inc_core_class.dc.core.php
@@ -1,15 +1,15 @@
-$NetBSD: patch-inc_core_class.dc.core.php,v 1.2 2022/01/25 08:31:38 triaxx Exp $
+$NetBSD: patch-inc_core_class.dc.core.php,v 1.3 2022/05/26 21:08:38 triaxx Exp $
 
 Set pkgsrc compliant directory for /public.
 
---- inc/core/class.dc.core.php.orig	2021-02-13 10:31:06.000000000 +0000
+--- inc/core/class.dc.core.php.orig	2022-05-13 06:25:40.000000000 +0000
 +++ inc/core/class.dc.core.php
-@@ -1564,7 +1564,7 @@ class dcCore
-                     'Number of comments on feeds'],
+@@ -1570,7 +1570,7 @@ class dcCore
+                     'Number of comments on feeds', ],
                  ['post_url_format', 'string', '{y}/{m}/{d}/{t}',
-                     'Post URL format. {y}: year, {m}: month, {d}: day, {id}: post id, {t}: entry title'],
+                     'Post URL format. {y}: year, {m}: month, {d}: day, {id}: post id, {t}: entry title', ],
 -                ['public_path', 'string', 'public',
 +                ['public_path', 'string', '@DOTCLEAR_PUBDIR@',
-                     'Path to public directory, begins with a / for a full system path'],
+                     'Path to public directory, begins with a / for a full system path', ],
                  ['public_url', 'string', '/public',
-                     'URL to public directory'],
+                     'URL to public directory', ],
--- www/php-dotclear/patches/patch-inc_load__plugin__file.php
+++ www/php-dotclear/patches/patch-inc_load__plugin__file.php
@@ -1,15 +1,15 @@
-$NetBSD: patch-inc_load__plugin__file.php,v 1.2 2022/01/25 08:31:38 triaxx Exp $
+$NetBSD: patch-inc_load__plugin__file.php,v 1.3 2022/05/26 21:08:38 triaxx Exp $
 
 Honor PKG_SYSCONFDIR.
 
---- inc/load_plugin_file.php.orig	2021-02-13 10:31:06.000000000 +0000
+--- inc/load_plugin_file.php.orig	2022-05-13 06:25:40.000000000 +0000
 +++ inc/load_plugin_file.php
 @@ -25,7 +25,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
  } elseif (isset($_SERVER['REDIRECT_DC_RC_PATH'])) {
      define('DC_RC_PATH', $_SERVER['REDIRECT_DC_RC_PATH']);
  } else {
--    define('DC_RC_PATH', dirname(__FILE__) . '/config.php');
-+    define('DC_RC_PATH', '@PKG_SYSCONFDIR@/config.php');
+-    define('DC_RC_PATH', __DIR__ . '/config.php');
++    define('DC_RC_PATH', __DIR__ . '@PKG_SYSCONFDIR@/config.php');
  }
  
  if (!is_file(DC_RC_PATH)) {
