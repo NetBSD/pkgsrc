48471b1b684116ef9434574a740d9f7a68e16b49
triaxx <triaxx@pkgsrc.org>
2022-05-26 21:08:37 +0000

    dotclear: Update to 2.22
    upstream changes:
    -----------------
      * PHP 7.4+ is required, PHP 8.0/8.1 compliance
      * Bug fixes

--- www/php-dotclear/patches/patch-admin_install_index.php
+++ www/php-dotclear/patches/patch-admin_install_index.php
@@ -1,15 +1,15 @@
-$NetBSD: patch-admin_install_index.php,v 1.2 2022/01/25 08:31:38 triaxx Exp $
+$NetBSD: patch-admin_install_index.php,v 1.3 2022/05/26 21:08:38 triaxx Exp $
 
 Honor PKG_SYSCONFDIR.
 
---- admin/install/index.php.orig	2021-02-13 10:31:06.000000000 +0000
+--- admin/install/index.php.orig	2022-05-13 06:25:40.000000000 +0000
 +++ admin/install/index.php
 @@ -11,7 +11,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
  } elseif (isset($_SERVER['REDIRECT_DC_RC_PATH'])) {
      $rc_path = $_SERVER['REDIRECT_DC_RC_PATH'];
  } else {
--    $rc_path = dirname(__FILE__) . '/../../inc/config.php';
+-    $rc_path = __DIR__ . '/../../inc/config.php';
 +    $rc_path = '@PKG_SYSCONFDIR@/config.php';
  }
  
- require dirname(__FILE__) . '/../../inc/prepend.php';
+ require __DIR__ . '/../../inc/prepend.php';
