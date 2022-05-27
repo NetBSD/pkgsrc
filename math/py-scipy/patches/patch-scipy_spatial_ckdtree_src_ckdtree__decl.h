17aad2f6fafb3d05cea9776f5a19e891f12c7191
tnn <tnn@pkgsrc.org>
2022-05-27 16:59:50 +0000

    py-scipy: disable __builtin_prefetch completely for now
    It failed with GCC too. There is some bad interaction with py-numpy,
    probably related to patch-numpy_core_include_numpy_npy__common.h.
    Unbreak the build until I have time to investigate this.

--- math/py-scipy/patches/patch-scipy_spatial_ckdtree_src_ckdtree__decl.h
+++ math/py-scipy/patches/patch-scipy_spatial_ckdtree_src_ckdtree__decl.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-scipy_spatial_ckdtree_src_ckdtree__decl.h,v 1.1 2022/05/26 14:49:59 tnn Exp $
+$NetBSD: patch-scipy_spatial_ckdtree_src_ckdtree__decl.h,v 1.2 2022/05/27 16:59:50 tnn Exp $
 
 Many errors like this:
 scipy/spatial/ckdtree/src/query_pairs.cxx:110:16: error: argument to '__builtin_prefetch' must be a constant integer
 
---- scipy/spatial/ckdtree/src/ckdtree_decl.h.orig	2022-04-29 21:14:38.000000000 +0000
+--- scipy/spatial/ckdtree/src/ckdtree_decl.h.orig	2022-04-29 21:14:38.041529700 +0000
 +++ scipy/spatial/ckdtree/src/ckdtree_decl.h
 @@ -10,7 +10,11 @@
  #include <numpy/npy_common.h>
  #define CKDTREE_LIKELY(x) NPY_LIKELY(x)
  #define CKDTREE_UNLIKELY(x)  NPY_UNLIKELY(x)
-+#if !defined(__clang__)
++#if 0
  #define CKDTREE_PREFETCH(x, rw, loc)  NPY_PREFETCH(x, rw, loc)
 +#else
 +#define CKDTREE_PREFETCH(x, rw, loc)
