efa4dd6556daf914f10314d90f362d4df1293497
jaapb <jaapb@pkgsrc.org>
2022-05-30 09:11:17 +0000

    Updated devel/ocaml-fpath to v0.7.3.
    This version drops the ocaml-result dependency and adds support for
    OCaml 4.12 (and higher).

--- devel/ocaml-fpath/buildlink3.mk
+++ devel/ocaml-fpath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/05/24 18:59:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/05/30 09:11:17 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-fpath
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-fpath
 OCAML_FPATH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-fpath+=	ocaml-fpath>=0.7.2
-BUILDLINK_ABI_DEPENDS.ocaml-fpath+=	ocaml-fpath>=0.7.2nb2
+BUILDLINK_ABI_DEPENDS.ocaml-fpath+=	ocaml-fpath>=0.7.3
 BUILDLINK_PKGSRCDIR.ocaml-fpath?=	../../devel/ocaml-fpath
 
 .endif	# OCAML_FPATH_BUILDLINK3_MK
