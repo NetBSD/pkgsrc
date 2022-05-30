6bfd22b5c042da06ca16f39339a4a1160829cba3
jaapb <jaapb@pkgsrc.org>
2022-05-30 09:52:47 +0000

    Updated devel/ocaml-batteries to version 3.5.1.
    Most updates are minor, but version 3.0.0 introduces a fair amount of
    API-breaking changes. Version 3.3.0 also includes support for OCaml 4.12.
    Full details in the ChangeLog file included in the distribution.

--- devel/ocaml-batteries/buildlink3.mk
+++ devel/ocaml-batteries/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/05/24 18:59:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/05/30 09:52:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-batteries
 
 .if !defined(OCAML_BATTERIES_BUILDLINK3_MK)
 OCAML_BATTERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.0
-BUILDLINK_ABI_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.11.0nb2
+BUILDLINK_API_DEPENDS.ocaml-batteries+=	ocaml-batteries>=3.0
+BUILDLINK_ABI_DEPENDS.ocaml-batteries+=	ocaml-batteries>=3.5.1
 BUILDLINK_PKGSRCDIR.ocaml-batteries?=	../../devel/ocaml-batteries
 
 .endif	# OCAML_BATTERIES_BUILDLINK3_MK
