12d1bd556fef46f25bf37b0cbe8c6a6f9efc5723
osa <osa@pkgsrc.org>
2022-06-02 17:04:31 +0000

    www/unit*: update to 1.27.0
    <ChangeLog>
    
    *) Feature: ability to specify a custom index file name when serving
       static files.
    
    *) Feature: variables support in the "location" option of the "return"
       action.
    
    *) Feature: support empty strings in the "location" option of the
       "return" action.
    
    *) Feature: added a new variable, $request_uri, that includes both the
       path and the query parts as per RFC 3986, sections 3-4.
    
    *) Feature: Ruby Rack environment parameter "SCRIPT_NAME" support.
    
    *) Feature: compatibility with GCC 12.
    
    *) Bugfix: Ruby Sinatra applications don't work without custom logging.
    
    *) Bugfix: the controller process could crash when a chain of more than
       four certificates was uploaded.
    
    *) Bugfix: some Perl applications failed to process the request body,
       notably with Plack.
    
    *) Bugfix: some Spring Boot applications failed to start, notably with
       Grails.
    
    *) Bugfix: incorrect Python protocol auto detection (ASGI or WSGI) for
       native callable object, notably with Falcon.
    
    *) Bugfix: ECMAScript modules did not work with the recent Node.js
       versions.
    
    </ChangeLog>

--- www/unit/version.mk
+++ www/unit/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.4 2021/12/03 01:05:21 gutteridge Exp $
+# $NetBSD: version.mk,v 1.5 2022/06/02 17:04:31 osa Exp $
 #
 # UNIT_EXTENSION_DIR
 #	Relative path to ${PREFIX} for NGINX Unit's modules.
@@ -12,7 +12,7 @@
 UNITVERSION_MK=	defined
 
 # Define NGINX Unit's version.
-UNIT_VERSION=	1.26.1
+UNIT_VERSION=	1.27.0
 
 # Define NGINX Unit's modules directory
 UNIT_EXTENSION_DIR=	libexec/unit/modules
