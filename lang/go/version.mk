b72b007d12349aae7f3e41c87d7bee0ded18c624
bsiegert <bsiegert@pkgsrc.org>
2022-06-02 18:19:26 +0000

    Update go117 to 1.17.11
    This minor release includes 4 security fixes following the security policy
    <https://go.dev/security>:
    
       - crypto/rand: rand.Read hangs with extremely large buffers
    
       On Windows, rand.Read will hang indefinitely if passed a buffer larger
       than 1 << 32 - 1 bytes.
    
       Thanks to Davis Goodin and Quim Muntal, working at Microsoft on the Go
       toolset, for reporting this issue.
    
       This is CVE-2022-30634 and Go issue https://go.dev/issue/52561.
    
    
       - crypto/tls: session tickets lack random ticket_age_add
    
       Session tickets generated by crypto/tls did not contain a randomly
       generated ticket_age_add. This allows an attacker that can observe TLS
       handshakes to correlate successive connections by comparing ticket ages
       during session resumption.
    
       Thanks to GitHub user @nervuri for reporting this.
    
       This is CVE-2022-30629 and Go issue https://go.dev/issue/52814.
    
    
       - os/exec: empty Cmd.Path can result in running unintended binary on
       Windows
    
       If, on Windows, Cmd.Run, cmd.Start, cmd.Output, or cmd.CombinedOutput
       are executed when Cmd.Path is unset and, in the working directory, there
       are binaries named either "..com" or "..exe", they will be executed.
    
       Thanks to Chris Darroch (chrisd8088@github.com), brian m. carlson (
       bk2204@github.com), and Mikhail Shcherbakov (https://twitter.com/yu5k3)
       for reporting this.
    
       This is CVE-2022-30580 and Go issue https://go.dev/issue/52574.
    
    
       - path/filepath: Clean(`.\c:`) returns `c:` on Windows
    
       On Windows, the filepath.Clean function could convert an invalid path to
       a valid, absolute path. For example, Clean(`.\c:`) returned `c:`.
    
       Thanks to Unrud for reporting this issue.
    
       This is CVE-2022-29804 and Go issue https://go.dev/issue/52476.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.149 2022/05/11 19:00:55 bsiegert Exp $
+# $NetBSD: version.mk,v 1.150 2022/06/02 18:19:26 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO118_VERSION=	1.18.2
-GO117_VERSION=	1.17.10
+GO117_VERSION=	1.17.11
 GO116_VERSION=	1.16.15
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
