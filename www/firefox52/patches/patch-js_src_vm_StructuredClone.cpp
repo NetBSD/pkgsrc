19aa81572b9b4524bf0f25703aa23b59cfe2fda2
martin <martin@pkgsrc.org>
2022-06-03 16:30:00 +0000

    One of the bigendian fixes accidently was 64bit only - make it work on 32bit powerpc too.

--- www/firefox52/patches/patch-js_src_vm_StructuredClone.cpp
+++ www/firefox52/patches/patch-js_src_vm_StructuredClone.cpp
@@ -1,15 +1,17 @@
-$NetBSD: patch-js_src_vm_StructuredClone.cpp,v 1.1 2018/08/08 08:49:58 martin Exp $
+$NetBSD: patch-js_src_vm_StructuredClone.cpp,v 1.2 2022/06/03 16:30:00 martin Exp $
 
 Fix a crash on big endian machines. Reported in bugzilla
 as #1481740.
 
 --- js/src/vm/StructuredClone.cpp.orig	2018-06-21 20:53:54.000000000 +0200
 +++ js/src/vm/StructuredClone.cpp	2018-08-08 10:15:04.949370197 +0200
-@@ -925,6 +925,7 @@ JSStructuredCloneData::discardTransferab
+@@ -923,8 +923,7 @@ JSStructuredCloneData::discardTransferab
+         if (!point.canPeek())
+             return;
  
-         void* content;
-         SCInput::getPtr(point.peek(), &content);
-+	content = (void*)NativeEndian::swapFromLittleEndian(reinterpret_cast<uint64_t>(content));
+-        void* content;
+-        SCInput::getPtr(point.peek(), &content);
++        void* content = (void*)NativeEndian::swapFromLittleEndian(point.peek());
          point.next();
          if (!point.canPeek())
              return;
