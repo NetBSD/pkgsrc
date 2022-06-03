55162d8882e2d9043f2b0f1f2eefa3d930a64ef8
jaapb <jaapb@pkgsrc.org>
2022-06-03 12:46:25 +0000

    Update www/ocsigen-i18n to version 4.0.0.
    Fixes are minor, but the package has switched to dune for building.

--- www/ocsigen-i18n/buildlink3.mk
+++ www/ocsigen-i18n/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/12/11 10:58:20 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/06/03 12:46:25 jaapb Exp $
 
 BUILDLINK_TREE+=	ocsigen-i18n
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocsigen-i18n
 OCSIGEN_I18N_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.7.0
-BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.7.0
+BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=4.0.0
 BUILDLINK_PKGSRCDIR.ocsigen-i18n?=	../../www/ocsigen-i18n
 .endif	# OCSIGEN_I18N_BUILDLINK3_MK
 
