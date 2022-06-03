6c6d52570a62fe5931938ce1882e8fbfc09da62c
jaapb <jaapb@pkgsrc.org>
2022-06-03 13:17:35 +0000

    Fixed dependencies for databases/pgocaml. Revbump.
    devel/ocaml-rresult is not just a build dependency, and is in fact needed
    by packages that depend on pgocaml.

--- databases/pgocaml/buildlink3.mk
+++ databases/pgocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/05/05 12:11:18 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2022/06/03 13:17:35 jaapb Exp $
 
 BUILDLINK_TREE+=	pgocaml
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	pgocaml
 PGOCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pgocaml+=	pgocaml>=4.0
-BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=4.3.0
+BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=4.3.0nb1
 BUILDLINK_PKGSRCDIR.pgocaml?=	../../databases/pgocaml
 
 .include "../../devel/ocaml-re/buildlink3.mk"
+.include "../../devel/ocaml-rresult/buildlink3.mk"
 .include "../../textproc/ocaml-csv/buildlink3.mk"
 .include "../../time/ocaml-calendar/buildlink3.mk"
 .endif	# PGOCAML_BUILDLINK3_MK
