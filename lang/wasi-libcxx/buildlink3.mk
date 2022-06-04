6a16d473fe28d8556bcc76527578855a0243f1fe
gutteridge <gutteridge@pkgsrc.org>
2022-06-04 00:34:30 +0000

    wasi-compiler-rt & wasi-libcxx: build fix for firefox
    BUILDLINK_API_DEPENDS must reflect the exact llvm version, as this is
    necessary to force the wasi packages to match, or build failures will
    occur, because various commands invoked in the Firefox build process
    supply the exact clang version in their path. For instance:
    
    wasm-ld: error: cannot open /usr/pkg/lib/clang/13.0.1/lib/wasi/libclang_rt.builtins-wasm32.a: No such file or directory
    clang-13: error: linker command failed with exit code 1 (use -v to see invocation)
    
    (No PKGREVISION bump of www/firefox required, as this is a build-only
    dependency, and no other packages presently depend on these.)

--- lang/wasi-libcxx/buildlink3.mk
+++ lang/wasi-libcxx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/12/19 14:12:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/06/04 00:34:31 gutteridge Exp $
 
 BUILDLINK_TREE+=	wasi-libcxx
 
@@ -7,7 +7,9 @@ BUILDLINK_DEPMETHOD.wasi-libcxx?=	build
 .if !defined(WASI_LIBCXX_BUILDLINK3_MK)
 WASI_LIBCXX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=13.0.0
+# This exact version must be kept in sync with llvm, or firefox
+# minimum build dependencies will be incorrect.
+BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=13.0.1
 BUILDLINK_PKGSRCDIR.wasi-libcxx?=	../../lang/wasi-libcxx
 
 .endif	# WASI_LIBCXX_BUILDLINK3_MK
