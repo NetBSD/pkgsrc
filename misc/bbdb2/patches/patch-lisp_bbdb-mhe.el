ef31529fb086a2b345251bb331eae3df5f4e332d
mef <mef@pkgsrc.org>
2022-06-05 02:27:31 +0000

    misc/bbdb2: Moved from misc/bbdb version 2.35
    (Moved from misc/bbdb, meaning rename directory and PKGBASE, see the
    thread starting at
       http://mail-index.netbsd.org/pkgsrc-users/2022/05/31/msg035631.html
    ) Thank you,
    
    The Insidious Big Brother DataBase is an emacs-based contact manager
    that integrates itself into your mail and news clients.

---
+++ misc/bbdb2/patches/patch-lisp_bbdb-mhe.el
@@ -0,0 +1,26 @@
+$NetBSD: patch-lisp_bbdb-mhe.el,v 1.1 2022/06/05 02:27:31 mef Exp $
+
+Make bbdb build with emacs-24.
+(emacs-27):Warning: !! The file uses old-style backquotes !!
+This functionality has been obsolete for more than 10 years already
+
+--- lisp/bbdb-mhe.el.orig	2007-01-31 07:08:33.000000000 +0900
++++ lisp/bbdb-mhe.el	2018-02-01 13:22:21.336378903 +0900
+@@ -37,7 +37,7 @@
+ 
+ (defmacro bbdb/mh-cache-key (message)
+   "Return a (numeric) key for MESSAGE"
+-  (`(let* ((attrs (file-attributes (, message)))
++   `(let* ((attrs (file-attributes (, message)))
+            (status-time (nth 6 attrs))
+            (status-time-2 (cdr status-time))
+            (inode (nth 10 attrs)))
+@@ -50,7 +50,7 @@
+               ;; return it as list.
+               (if (integerp status-time-2)
+                   status-time-2
+-                (car status-time-2))))))
++                (car status-time-2)))))
+ 
+ ;;;###autoload
+ (defun bbdb/mh-update-record (&optional offer-to-create)
