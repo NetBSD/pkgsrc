82f3fd77f7b40b739f92d9295c042e0cf7c7f3c7
mef <mef@pkgsrc.org>
2022-06-08 11:59:45 +0000

    (lang/nqp) Updated 2022.03 to 2022.06, ChangeLog not known

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2021/09/30 14:18:14 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2022/06/08 11:59:45 mef Exp $
 
 BUILDLINK_TREE+=	nqp
 
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2021.09
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2021.09
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2022.06
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2022.06
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
 .include "../../devel/MoarVM/buildlink3.mk"
