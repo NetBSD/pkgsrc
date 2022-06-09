1e056f0606d1f6d876a6dbe6fe491a954dc49802
taca <taca@pkgsrc.org>
2022-06-09 15:10:50 +0000

    lang/php81: update to 8.1.7
    09 Jun 2022, PHP 8.1.7
    
    - CLI:
      . Fixed bug GH-8575 (CLI closes standard streams too early). (Levi Morrison)
    
    - Date:
      . Fixed bug #51934 (strtotime plurals / incorrect time). (Derick)
      . Fixed bug #51987 (Datetime fails to parse an ISO 8601 ordinal date
        (extended format)). (Derick)
      . Fixed bug #66019 (DateTime object does not support short ISO 8601 time
        format - YYYY-MM-DDTHH) (cmb, Derick)
      . Fixed bug #68549 (Timezones and offsets are not properly used when working
        with dates) (Derick, Roel Harbers)
      . Fixed bug #81565 (date parsing fails when provided with timezones including
        seconds). (Derick)
      . Fixed bug GH-7758 (Problems with negative timestamps and fractions).
        (Derick, Ilija)
    
    - FPM:
      . Fixed ACL build check on MacOS. (David Carlier)
      . Fixed bug #72185: php-fpm writes empty fcgi record causing nginx 502.
        (Jakub Zelenka, loveharmful)
    
    - mysqlnd:
      . Fixed bug #81719: mysqlnd/pdo password buffer overflow. (CVE-2022-31626)
        (c dot fol at ambionics dot io)
    
    - OPcache:
      . Fixed bug GH-8461 (tracing JIT crash after function/method change).
        (Arnaud, Dmitry)
    
    - OpenSSL:
      . Fixed bug #79589 (error:14095126:SSL routines:ssl3_read_n:unexpected eof
        while reading). (Jakub Zelenka)
    
    - Pcntl:
      . Fixed Haiku build. (David Carlier)
    
    - pgsql
      . Fixed bug #81720: Uninitialized array in pg_query_params().
        (CVE-2022-31625) (cmb)
    
    - Soap:
      . Fixed bug GH-8578 (Error on wrong parameter on SoapHeader constructor).
        (robertnisipeanu)
      . Fixed bug GH-8538 (SoapClient may strip parts of nmtokens). (cmb)
    
    - SPL:
      . Fixed bug GH-8235 (iterator_count() may run indefinitely). (cmb)
    
    - Standard:
      . Fixed bug GH-8185 (Crash during unloading of extension after dl() in ZTS).
        (Arnaud)
    
    - Zip:
      . Fixed type for index in ZipArchive::replaceFile. (Martin Rehberger)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.366 2022/06/09 15:00:38 taca Exp $
+# $NetBSD: phpversion.mk,v 1.367 2022/06/09 15:10:50 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.30
 PHP80_VERSION=	8.0.19
-PHP81_VERSION=	8.1.6
+PHP81_VERSION=	8.1.7
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
