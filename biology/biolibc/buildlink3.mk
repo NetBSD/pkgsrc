06543e0ccb65dd451c3373c8a6f204e24c9187ba
bacon <bacon@pkgsrc.org>
2022-06-11 19:42:39 +0000

    biology/biolibc: Update to 0.2.3
    Expand use of tsv_read_field_malloc() to improve memory efficiency
    Add SAM bit flag constants
    Import SAM-GFF compare functions from diffanal
    Updates for libxtend DSV API changes
    Numerous minor bug fixes and enhancements
    Changes: https://github.com/auerlab/biolibc/releases

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/03/15 21:06:32 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/06/11 19:42:39 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.2
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.3
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
