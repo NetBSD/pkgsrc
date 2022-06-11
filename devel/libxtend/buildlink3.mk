33201c4b1607d2634ecb6fcadb892f62b2497ca4
bacon <bacon@pkgsrc.org>
2022-06-11 19:39:33 +0000

    devel/libxtend: Update to 0.1.6
    Add dsv_line_init(), strptrcasecmp()
    A few API improvements and bug fixes
    Changes: https://github.com/outpaddling/libxtend/releases

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/03/15 21:03:27 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/06/11 19:39:33 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.5
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.6
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
