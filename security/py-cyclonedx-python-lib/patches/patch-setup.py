4433aec6c3af012d3c81c55356fc2123419da911
wiz <wiz@pkgsrc.org>
2022-06-13 09:59:01 +0000

    py-cyclonedx-python-lib: update to 2.5.1.
    2.5.1
    
    Fix
    
        Add missing Vulnerability comparator for sorting (#246) (c3f3d0d)
    
    2.5.0
    
    Feature
    
        Use SortedSet in model to improve reproducibility - this will provide predictable ordering of various items in generated CycloneDX documents - thanks to @RodneyRichardson (8a1c404)
    
    Documentation
    
        Fix typo "This is out" -> "This is our" (ef0278a)
    
    2.4.0
    
    Feature
    
        deps: Remove unused typing-extensions constraints (2ce358a)

--- security/py-cyclonedx-python-lib/patches/patch-setup.py
+++ security/py-cyclonedx-python-lib/patches/patch-setup.py
@@ -1,18 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2022/02/20 21:23:44 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2022/06/13 09:59:01 wiz Exp $
 
 Be less restrictive in dependencies.
 
---- setup.py.orig	2022-01-24 13:30:26.931830600 +0000
+--- setup.py.orig	2022-06-10 20:01:07.074393300 +0000
 +++ setup.py
-@@ -15,9 +15,9 @@ package_data = \
- install_requires = \
+@@ -16,7 +16,7 @@ install_requires = \
  ['packageurl-python>=0.9',
   'setuptools>=47.0.0',
-- 'toml>=0.10.0,<0.11.0',
-+ 'toml>=0.10.0',
-  'types-setuptools>=57.0.0',
-- 'types-toml>=0.10.0,<0.11.0']
-+ 'types-toml>=0.10.0']
+  'sortedcontainers>=2.4.0,<3.0.0',
+- 'toml>=0.10.0,<0.11.0']
++ 'toml>=0.10.0']
  
  extras_require = \
- {':python_version < "3.8"': ['importlib-metadata>=3.4',
+ {':python_version < "3.8"': ['importlib-metadata>=3.4']}
