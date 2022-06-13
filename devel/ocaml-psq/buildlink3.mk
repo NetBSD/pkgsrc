58e3ebd4cb5e36ebe7d7d8d4523972a2c08ff412
jaapb <jaapb@pkgsrc.org>
2022-06-13 09:10:05 +0000

    Updated devel/ocaml-psq to version 0.2.0.
    This version includes a semantics cleanup, with breaking API changes,
    and is somewhat faster.

--- devel/ocaml-psq/buildlink3.mk
+++ devel/ocaml-psq/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/05/24 18:59:31 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/06/13 09:10:05 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-psq
 
 .if !defined(OCAML_PSQ_BUILDLINK3_MK)
 OCAML_PSQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-psq+=	ocaml-psq>=0.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-psq+=	ocaml-psq>=0.1.0nb2
+BUILDLINK_API_DEPENDS.ocaml-psq+=	ocaml-psq>=0.2.0
+BUILDLINK_ABI_DEPENDS.ocaml-psq+=	ocaml-psq>=0.2.0
 BUILDLINK_PKGSRCDIR.ocaml-psq?=		../../devel/ocaml-psq
 .endif	# OCAML_PSQ_BUILDLINK3_MK
 
