8556e5e5389dd83da6978d7485f72f793b95b2eb
wiz <wiz@pkgsrc.org>
2022-06-13 07:11:33 +0000

    deforaos-panel: fix PLIST

--- x11/deforaos-panel/options.mk
+++ x11/deforaos-panel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2022/05/17 18:01:28 khorben Exp $
+# $NetBSD: options.mk,v 1.3 2022/06/13 07:11:33 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.deforaos-panel
 PKG_SUPPORTED_OPTIONS=	embedded
@@ -6,5 +6,5 @@ PKG_SUPPORTED_OPTIONS=	embedded
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Membedded)
-DEFORAOS_CONFIGURE_MODE=embedded-debug
+DEFORAOS_CONFIGURE_MODE=	embedded-debug
 .endif
