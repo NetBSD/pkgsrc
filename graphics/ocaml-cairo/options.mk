b4517c2a5748b4bb55a3d6758a26f4d19c53af07
gdt <gdt@pkgsrc.org>
2022-06-15 11:09:26 +0000

    graphics/ocaml-cairo: Remove gtk and pango from default options
    The gtk and pango options depend on ocaml-lablgtk (which is gtk2), but
    ocaml-lablgtk3 depends on ocaml-cairo.  For now, don't suggest the
    options, as the only pkgsrc user is ocaml-lablgtk3.
    
    Discussed with $MAINTAINER long ago.  Perhaps these need a split
    package, and perhaps packages that depend on ocaml-cairo might need
    updating -- but gtk2 is obsolete and those packages should be moving
    (perhaps upstream) to lablgtk3.
    
    (Committed separately from 0.6.3 update for ease of bisecting and
    reverting if necessary.)

--- graphics/ocaml-cairo/options.mk
+++ graphics/ocaml-cairo/options.mk
@@ -1,8 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2022/02/28 12:55:08 jaapb Exp $
+# $NetBSD: options.mk,v 1.2 2022/06/15 11:09:26 gdt Exp $
 
+# The gtk and pango options depend on ocaml-lablgtk (which is gtk2),
+# but ocaml-lablgtk3 depends on ocaml-cairo.  For now, don't suggest
+# the options, as the only pkgsrc user is ocaml-lablgtk3.
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml-cairo
 PKG_SUPPORTED_OPTIONS=	gtk pango
-PKG_SUGGESTED_OPTIONS=	gtk pango
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.prefs.mk"
 
