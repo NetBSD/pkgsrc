395e7aea8f884f88c7961203858e828468bac4e5
adam <adam@pkgsrc.org>
2022-06-15 08:54:58 +0000

    rhash: updated to 1.4.3
    RHash v1.4.3
    
    Allow simultaneous usage of --update and --check options
    Honor --lowercase option while --embed-crc is in action
    Improve ripemd160 performance
    Support --brief option to show verification report without header and footer
    Support --ignore-missing option, to ignore missing files in --check mode
    Add --bt-transmission option to create Transmission compatible torrent files
    Honor hash file extension in --update mode
    LibRHash: add rhash_import() and rhash_export() functions
    Support runtime loading of OpenSSL 3.0
    Bugfix: Show total speed in the update mode
    Bugfix: fix building of the LibRHash static library on Solaris
    Bugfix: hash options were sometimes ignored in the --check mode
    Bugfix: star character at the start of filepath must be ignored

--- misc/rhash/buildlink3.mk
+++ misc/rhash/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/10/03 17:34:11 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/06/15 08:54:58 adam Exp $
 
 BUILDLINK_TREE+=	rhash
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	rhash
 RHASH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rhash+=	rhash>=1.3.1
-BUILDLINK_ABI_DEPENDS.rhash?=	rhash>=1.3.9nb1
+BUILDLINK_ABI_DEPENDS.rhash+=	rhash>=1.3.9nb1
 BUILDLINK_PKGSRCDIR.rhash?=	../../misc/rhash
 
 .include "../../security/openssl/buildlink3.mk"
