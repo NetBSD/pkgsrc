a8cad43d0cde2e601ed355be16a0738a60cda9e0
taca <taca@pkgsrc.org>
2022-06-15 14:02:36 +0000

    net/bind916: update to 9.16.30
    	--- 9.16.30 released ---
    
    5899.	[func]		Don't try to process DNSSEC-related and ZONEMD records
    			in catz. [GL #3380]
    
    5890.	[bug]		When the fetches-per-server quota was adjusted
    			because of an authoritative server timing out more
    			or less frequently, it was incorrectly set to 1
    			rather than the intended value.  This has been
    			fixed. [GL #3327]
    
    5888.	[bug]		Only write key files if the dnssec-policy keymgr has
    			changed the metadata. [GL #3302]
    
    5823.	[func]		Replace hazard pointers based lock-free list with
    			locked-list based queue that's simpler and has no or
    			little performance impact. [GL #3180]

--- net/bind916/patches/patch-lib_isc_netmgr_netmgr-int.h
+++ net/bind916/patches/patch-lib_isc_netmgr_netmgr-int.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.1 2021/10/24 06:40:28 taca Exp $
+$NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.2 2022/06/15 14:02:36 taca Exp $
 
 * Take from NetBSD base.
 
---- lib/isc/netmgr/netmgr-int.h.orig	2021-09-07 09:37:05.000000000 +0000
+--- lib/isc/netmgr/netmgr-int.h.orig	2022-06-02 19:34:02.000000000 +0000
 +++ lib/isc/netmgr/netmgr-int.h
-@@ -377,13 +377,13 @@ typedef struct isc__netievent__socket {
+@@ -408,13 +408,13 @@ typedef struct isc__netievent__socket {
  } isc__netievent__socket_t;
  
  #define NETIEVENT_SOCKET_TYPE(type) \
@@ -20,7 +20,7 @@ $NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.1 2021/10/24 06:40:28 taca Exp $
  
  #define NETIEVENT_SOCKET_DEF(type)                                             \
  	isc__netievent_##type##_t *isc__nm_get_netievent_##type(               \
-@@ -407,13 +407,13 @@ typedef struct isc__netievent__socket_re
+@@ -438,13 +438,13 @@ typedef struct isc__netievent__socket_re
  } isc__netievent__socket_req_t;
  
  #define NETIEVENT_SOCKET_REQ_TYPE(type) \
@@ -36,7 +36,7 @@ $NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.1 2021/10/24 06:40:28 taca Exp $
  
  #define NETIEVENT_SOCKET_REQ_DEF(type)                                         \
  	isc__netievent_##type##_t *isc__nm_get_netievent_##type(               \
-@@ -440,14 +440,14 @@ typedef struct isc__netievent__socket_re
+@@ -470,14 +470,14 @@ typedef struct isc__netievent__socket_re
  } isc__netievent__socket_req_result_t;
  
  #define NETIEVENT_SOCKET_REQ_RESULT_TYPE(type) \
@@ -53,7 +53,7 @@ $NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.1 2021/10/24 06:40:28 taca Exp $
  
  #define NETIEVENT_SOCKET_REQ_RESULT_DEF(type)                                  \
  	isc__netievent_##type##_t *isc__nm_get_netievent_##type(               \
-@@ -474,13 +474,13 @@ typedef struct isc__netievent__socket_ha
+@@ -504,13 +504,13 @@ typedef struct isc__netievent__socket_ha
  } isc__netievent__socket_handle_t;
  
  #define NETIEVENT_SOCKET_HANDLE_TYPE(type) \
@@ -69,7 +69,7 @@ $NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.1 2021/10/24 06:40:28 taca Exp $
  
  #define NETIEVENT_SOCKET_HANDLE_DEF(type)                                      \
  	isc__netievent_##type##_t *isc__nm_get_netievent_##type(               \
-@@ -506,13 +506,13 @@ typedef struct isc__netievent__socket_qu
+@@ -536,13 +536,13 @@ typedef struct isc__netievent__socket_qu
  } isc__netievent__socket_quota_t;
  
  #define NETIEVENT_SOCKET_QUOTA_TYPE(type) \
@@ -85,8 +85,8 @@ $NetBSD: patch-lib_isc_netmgr_netmgr-int.h,v 1.1 2021/10/24 06:40:28 taca Exp $
  
  #define NETIEVENT_SOCKET_QUOTA_DEF(type)                                       \
  	isc__netievent_##type##_t *isc__nm_get_netievent_##type(               \
-@@ -571,12 +571,12 @@ typedef struct isc__netievent {
- 	isc__netievent_type type;
+@@ -603,12 +603,12 @@ typedef struct isc__netievent {
+ 	ISC_LINK(isc__netievent_t) link;
  } isc__netievent_t;
  
 -#define NETIEVENT_TYPE(type) typedef isc__netievent_t isc__netievent_##type##_t;
