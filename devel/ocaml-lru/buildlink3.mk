7647b6a1dde8014584a0e74f3daa7f36e9124eb7
jaapb <jaapb@pkgsrc.org>
2022-06-17 13:01:24 +0000

    Updated devel/ocaml-lru to version 0.3.0.
    This introduces several API-breaking changes, not to mention support for
    OCaml 4.14.

--- devel/ocaml-lru/buildlink3.mk
+++ devel/ocaml-lru/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/05/24 18:59:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/06/17 13:01:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lru
 
 .if !defined(OCAML_LRU_BUILDLINK3_MK)
 OCAML_LRU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-lru+=	ocaml-lru>=0.2.0
-BUILDLINK_ABI_DEPENDS.ocaml-lru+=	ocaml-lru>=0.2.0nb2
+BUILDLINK_API_DEPENDS.ocaml-lru+=	ocaml-lru>=0.3.0
+BUILDLINK_ABI_DEPENDS.ocaml-lru+=	ocaml-lru>=0.3.0
 BUILDLINK_PKGSRCDIR.ocaml-lru?=		../../devel/ocaml-lru
 
 .endif	# OCAML_LRU_BUILDLINK3_MK
