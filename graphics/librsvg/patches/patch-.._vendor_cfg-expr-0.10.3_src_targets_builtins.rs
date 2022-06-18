12b80bfd9a5327ecbf2346b66aba5a55750c8ad1
gutteridge <gutteridge@pkgsrc.org>
2022-06-18 01:56:22 +0000

    librsvg: update to 2.54.4
    Version 2.54.4
    ==============
    
    - #817 - Support CSS Color 4 syntax for `<alpha-value>`.  Opacities
      can be specified as numbers or percentages now, e.g. `0.5` or `50%`
      (Michael Howell).
    
    - #870 - Roll back minimum required version of Pango to 1.46.0
      (Kleis Auke Wolthuizen).
    
    - #867 - Fix Windows NMake install when documentation is not built.
      (Peter Williams).

--- graphics/librsvg/patches/patch-.._vendor_cfg-expr-0.10.3_src_targets_builtins.rs
+++ graphics/librsvg/patches/patch-.._vendor_cfg-expr-0.10.3_src_targets_builtins.rs
@@ -1,14 +1,14 @@
-$NetBSD: patch-.._vendor_cfg-expr-0.10.2_src_targets_builtins.rs,v 1.1 2022/03/26 15:24:29 tnn Exp $
+$NetBSD: patch-.._vendor_cfg-expr-0.10.3_src_targets_builtins.rs,v 1.1 2022/06/18 01:56:22 gutteridge Exp $
 
 Add i586-unknown-netbsd.
 Needed because of lang/rust/patches/patch-compiler_rustc__target_src_spec_mod.rs
 Note that the implementation does a binary search of the target list.
 It must be sorted lexically!
 
---- ../vendor/cfg-expr-0.10.2/src/targets/builtins.rs.orig	1973-11-29 21:33:09.000000000 +0000
-+++ ../vendor/cfg-expr-0.10.2/src/targets/builtins.rs
-@@ -654,6 +654,16 @@ pub const ALL_BUILTINS: &[TargetInfo] = 
-         endian: Endian::little,
+--- ../vendor/cfg-expr-0.10.3/src/targets/builtins.rs.orig	1973-11-29 16:33:09.000000000 -0500
++++ ../vendor/cfg-expr-0.10.3/src/targets/builtins.rs
+@@ -806,6 +806,18 @@
+         panic: Panic::unwind,
      },
      TargetInfo {
 +        triple: Triple::new_const("i586-unknown-netbsd"),
@@ -19,6 +19,8 @@ It must be sorted lexically!
 +        families: Families::unix,
 +        pointer_width: 32,
 +        endian: Endian::little,
++        has_atomics: HasAtomics::atomic_8_16_32_64_ptr,
++        panic: Panic::unwind,
 +    },
 +    TargetInfo {
          triple: Triple::new_const("i686-apple-darwin"),
