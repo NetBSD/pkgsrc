c5715f4e741e549647b84d9ca33810b7d6154bfe
wiz <wiz@pkgsrc.org>
2022-06-18 21:44:34 +0000

    devel/py-lama7: re-import py-lama-7.7.1nb2
    For python 2.7 support. DESCR:
    
    Code audit tool for Python and JavaScript. Pylama wraps these tools:
    * pycodestyle (formerly pep8)
    * pydocstyle (formerly pep257)
    * PyFlakes
    * Mccabe
    * Pylint
    * Radon
    * gjslin
    * eradicate
    
    This package contains the last version supporting python 2.7.

---
+++ devel/py-lama7/patches/patch-setup.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-setup.py,v 1.1 2022/06/18 21:44:34 wiz Exp $
+
+Do not install tests.
+
+--- setup.py.orig	2018-11-20 13:13:48.000000000 +0000
++++ setup.py
+@@ -35,7 +35,7 @@ meta = dict(
+     author_email='horneds@gmail.com',
+     url=' https://github.com/klen/pylama',
+ 
+-    packages=find_packages(exclude=['plugins']),
++    packages=find_packages(exclude=['tests']),
+ 
+     entry_points={
+         'console_scripts': [
