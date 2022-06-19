578526abea0c2675186eceff550538fda77763fa
gdt <gdt@pkgsrc.org>
2022-06-19 22:22:21 +0000

    lang/tcl-expect: Limit deadlock workaround to affected platforms
    This caused a build failure on OpenServer.  As the comment hinted
    would be necessary, limit the workaround to linux and sunos, which are
    known to have poll.  (expect does not have a configure test for poll
    already.)

--- lang/tcl-expect/patches/patch-exp__chan.c
+++ lang/tcl-expect/patches/patch-exp__chan.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-exp__chan.c,v 1.2 2022/06/19 14:43:28 gdt Exp $
+$NetBSD: patch-exp__chan.c,v 1.3 2022/06/19 22:22:22 gdt Exp $
 
 Add hack to work around deadlock situation that occurs on Solaris and
 Linux. See PR 34442.
@@ -13,27 +13,32 @@ cause problems, we can wrap it in ifdefs.
 
 --- exp_chan.c.orig	2010-07-01 00:53:49.000000000 +0000
 +++ exp_chan.c
-@@ -7,6 +7,7 @@
+@@ -7,6 +7,9 @@
   */
  
  #include <sys/types.h>
++#if defined(linux) || defined(sunos)
 +#include <sys/poll.h>
++#endif
  #include <stdio.h>
  #include <signal.h>
  #include <errno.h>
-@@ -205,6 +206,8 @@ ExpInputProc(instanceData, buf, toRead, 
+@@ -205,6 +208,10 @@ ExpInputProc(instanceData, buf, toRead, 
      ExpState *esPtr = (ExpState *) instanceData;
      int bytesRead;			/* How many bytes were actually
                                           * read from the input device? */
++#if defined(linux) || defined(sunos)
 +    struct pollfd fds[1];
 +    int pollResult;
++#endif
  
      *errorCodePtr = 0;
      
-@@ -215,6 +218,18 @@ ExpInputProc(instanceData, buf, toRead, 
+@@ -215,6 +222,20 @@ ExpInputProc(instanceData, buf, toRead, 
       * nonblocking, the read will never block.
       */
  
++#if defined(linux) || defined(sunos)
 +    /* Update: there isn't always, which can lead to hangs. See PR 34442. */
 +    fds[0].fd = esPtr->fdin;
 +    fds[0].events = POLLIN | POLLERR | POLLHUP | POLLNVAL;
@@ -45,6 +50,7 @@ cause problems, we can wrap it in ifdefs.
 +        *errorCodePtr = EBADF;
 +	return -1;
 +    }
++#endif
 +
      bytesRead = read(esPtr->fdin, buf, (size_t) toRead);
      /*printf("ExpInputProc: read(%d,,) = %d\r\n",esPtr->fdin,bytesRead);*/
