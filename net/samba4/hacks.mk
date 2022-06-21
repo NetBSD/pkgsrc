47d8c2009f18b810f075417709af6726d0c23981
he <he@pkgsrc.org>
2022-06-21 09:47:49 +0000

    net/samba4: needs --without-pie on NetBSD/powerpc.
    This is in contrast to ../net/samba which needs --disable-pie.

--- net/samba4/hacks.mk
+++ net/samba4/hacks.mk
@@ -1,3 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1 2015/05/12 12:19:52 ryoon Exp $
+# $NetBSD: hacks.mk,v 1.2 2022/06/21 09:47:49 he Exp $
 
-.include "../../net/samba/hacks.mk"
+# Enabling PIE results in a 'Unsupported relocation type 6 in non-PLT
+# relocations' error under NetBSD/macppc.
+.if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "powerpc"
+PKG_HACKS+=		pie
+CONFIGURE_ARGS+=	--without-pie
+.endif
