f378ffd620bb2c8caa9fc840572538319046bb36
nia <nia@pkgsrc.org>
2022-06-22 10:12:30 +0000

    jack: fix sense of condition. jack works on netbsd-9.

--- audio/jack/platform.mk
+++ audio/jack/platform.mk
@@ -1,4 +1,4 @@
-# $NetBSD: platform.mk,v 1.2 2022/05/08 10:23:33 nia Exp $
+# $NetBSD: platform.mk,v 1.3 2022/06/22 10:12:30 nia Exp $
 
 .if !defined(PLATFORM_SUPPORTS_JACK)
 .  include "../../mk/bsd.fast.prefs.mk"
@@ -6,7 +6,7 @@
 JACK_PLATFORMS+=	Darwin-*-*
 JACK_PLATFORMS+=	Linux-*-*
 
-.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} > 090000
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} >= 090000
 PLATFORM_SUPPORTS_JACK=	yes
 .endif
 
