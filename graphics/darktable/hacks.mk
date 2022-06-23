0e3e1856bd7aa8683cde9a969875f7c478b8bb80
nia <nia@pkgsrc.org>
2022-06-23 07:08:36 +0000

    darktable: Fix building with GCC on NetBSD.

---
+++ graphics/darktable/hacks.mk
@@ -0,0 +1,23 @@
+# $NetBSD: hacks.mk,v 1.1 2022/06/23 07:08:36 nia Exp $
+
+.if !defined(DARKTABLE_HACKS_MK)
+DARKTABLE_HACKS_MK:=
+
+.  include "../../mk/bsd.fast.prefs.mk"
+.  if ${OPSYS} == "NetBSD"
+PKG_HACKS+=		disable-graphite-opt
+# Disable some of the more ridiculous compiler flags being used, that are
+# creating build failures with the system compiler on NetBSD due to the
+# assumption that the isl/graphite optimizer is available.
+CFLAGS+=		-Wno-unknown-pragmas
+SUBST_CLASSES+=		opt
+SUBST_MESSAGE.opt=	Disabling excessive forced optimizations.
+SUBST_STAGE.opt+=	pre-configure
+SUBST_SED.opt+=		-e "s,pragma GCC optimize,pragma GCC nooptimize,g"
+SUBST_FILES.opt+=	src/iop/*.c
+SUBST_FILES.opt+=	src/iop/*.h
+SUBST_FILES.opt+=	src/develop/blends/*.c
+SUBST_FILES.opt+=	src/common/*.c
+SUBST_FILES.opt+=	src/common/*.h
+.  endif
+.endif
