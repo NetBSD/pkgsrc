3b59d2f9373f43025e598f361bdb1824514ec13f
bouyer <bouyer@pkgsrc.org>
2022-06-28 16:33:25 +0000

    Remove xenkernel411 and xenkernel411, they're EOL upstream

--- sysutils/xentools411/version.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: version.mk,v 1.2 2020/08/24 10:33:27 bouyer Exp $
-#
-# Version number is used by xentools411 and xenstoretools
-
-VERSION=	4.11.4
-VERSION_IPXE=	356f6c1b64d7a97746d1816cef8ca22bdd8d0b5d
