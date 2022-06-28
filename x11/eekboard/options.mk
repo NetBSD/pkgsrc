ba8dfc6994eb273a17bc8b157c73d59e1f4531e7
nia <nia@pkgsrc.org>
2022-06-28 11:40:47 +0000

    eekboard: Remove introspection option.
    https://marc.info/?l=pkgsrc-users&m=165444158600671&w=2

--- x11/eekboard/options.mk
+++ x11/eekboard/options.mk
@@ -1,22 +1,13 @@
-# $NetBSD: options.mk,v 1.6 2014/11/25 09:59:11 wiz Exp $
+# $NetBSD: options.mk,v 1.7 2022/06/28 11:40:47 nia Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.eekboard
-PKG_SUPPORTED_OPTIONS=	introspection vala xtest libcanberra
-PKG_SUGGESTED_OPTIONS=	introspection vala xtest
+PKG_SUPPORTED_OPTIONS=	vala xtest libcanberra
+PKG_SUGGESTED_OPTIONS=	vala xtest
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	introspection vala
-
-.if !empty(PKG_OPTIONS:Mintrospection)
-PLIST.introspection=	yes
-BUILDLINK_DEPMETHOD.gobject-introspection=	build
-.include "../../devel/gobject-introspection/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-introspection=yes
-.else
-CONFIGURE_ARGS+=	--enable-introspection=no
-.endif
+PLIST_VARS+=	vala
 
 .if !empty(PKG_OPTIONS:Mvala)
 PLIST.vala=	yes
