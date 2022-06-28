c1e5eb7e154add26a7c63a11874edafedf0aafb2
pin <pin@pkgsrc.org>
2022-06-28 09:29:17 +0000

    editors/ced: update to 0.2.1
    0.2.2
        BugFix : Limit command loop was not interruptable
        BugFix : Limit command one-liner value didn't respect quote rules
        BugFix : Limit command loop input trimmed all whitespaces
        BugFix : Undo and redo mechanics were inconsistent
        Ergono : Line number prompt for edit row multiple
        Ergono : Limit command show columns before prompt
        Ergono : Print-column now allows no argument case
        Featur : New command history
    
    0.2.1
        Hotfix : Ced panicked when given invalid command type
    
    Known issues
        Loop variants are not properly parsing quoted values
            Add row
            Edit row
            Limit

--- editors/ced/cargo-depends.mk
+++ editors/ced/cargo-depends.mk
@@ -1,7 +1,7 @@
-# $NetBSD: cargo-depends.mk,v 1.3 2022/06/04 21:24:58 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.4 2022/06/28 09:29:17 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	aho-corasick-0.7.18
-CARGO_CRATE_DEPENDS+=	dcsv-0.1.3
+CARGO_CRATE_DEPENDS+=	dcsv-0.3.0
 CARGO_CRATE_DEPENDS+=	memchr-2.4.1
 CARGO_CRATE_DEPENDS+=	regex-1.5.5
 CARGO_CRATE_DEPENDS+=	regex-syntax-0.6.25
