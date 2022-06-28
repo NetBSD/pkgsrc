1663f35dc8c12038a0cfa3c29d2368bd4f755581
nia <nia@pkgsrc.org>
2022-06-28 10:40:39 +0000

    cogl: Remove introspection option. Darwin !x11 case appears broken, anyone want to investigate?
    https://marc.info/?l=pkgsrc-users&m=165444158600671&w=2

--- graphics/cogl/options.mk
+++ graphics/cogl/options.mk
@@ -1,26 +1,13 @@
-# $NetBSD: options.mk,v 1.5 2021/04/16 06:49:24 nia Exp $
+# $NetBSD: options.mk,v 1.6 2022/06/28 10:40:39 nia Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cogl
-PKG_SUPPORTED_OPTIONS=	introspection x11
-PKG_SUGGESTED_OPTIONS=	introspection x11
+PKG_SUPPORTED_OPTIONS=	x11
+PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	introspection x11
-
-.if !empty(PKG_OPTIONS:Mintrospection)
-PLIST.introspection=		yes
-BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.9.5
-BUILDLINK_DEPMETHOD.gobject-introspection+=	build
-.include "../../devel/gobject-introspection/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-introspection=yes
-.else
-CONFIGURE_ARGS+=	--enable-introspection=no
-.endif
-
 .if !empty(PKG_OPTIONS:Mx11)
-PLIST.x11=		yes
 CONFIGURE_ARGS+=	--with-x
 CONFIGURE_ARGS+=	--enable-gdk-pixbuf
 CONFIGURE_ARGS+=	--with-gl-libname=libGL
