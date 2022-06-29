df34557de8da11e994645542088972e435abe176
wiz <wiz@pkgsrc.org>
2022-06-29 07:54:38 +0000

    camlp4: remove
    camlp4 is an outdated way to build packages with caml and does not work
    with pkgsrc ocaml versions for more than a year.
    
    Remove camlp4 and all packages using it.
    
    Ok jaapb@

--- devel/monotone-viz/patches/patch-unidiff.mli
+++
@@ -1,8 +0,0 @@
-$NetBSD: patch-unidiff.mli,v 1.1 2015/08/09 13:21:42 jaapb Exp $
-
-Moved classes to separate module for ocaml 4.02.
---- unidiff.mli.orig	2007-03-07 21:53:54.000000000 +0000
-+++ unidiff.mli
-@@ -1 +1 @@
--val show : #App.t -> string -> string -> unit
-+val show : #App_types.t -> string -> string -> unit
