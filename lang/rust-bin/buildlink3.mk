c81306131f1f92066fc9c6212d8a415372ad8509
pin <pin@pkgsrc.org>
2022-07-01 07:00:28 +0000

    lang/rust-bin: Bump minimum rust version
    By now a large number of Rust projects have migrated to edition = 2021, which
    requires Rust >= 1.56.
    
    Some projects still using edition = 2018 could, technically build with 1.20.
    But, since several libs have also adopted edition = 2021, many would fail while
    building dependencies.

--- lang/rust-bin/buildlink3.mk
+++ lang/rust-bin/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/06/14 15:33:28 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/07/01 07:00:28 pin Exp $
 
 BUILDLINK_TREE+=	rust-bin
 
 .if !defined(RUST_BIN_BUILDLINK3_MK)
 RUST_BIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rust-bin+=	rust-bin>=1.20.0
+BUILDLINK_API_DEPENDS.rust-bin+=	rust-bin>=1.56.1
 BUILDLINK_PKGSRCDIR.rust-bin?=		../../lang/rust-bin
 BUILDLINK_PASSTHRU_DIRS+=		${PREFIX}/lib/rustlib
 .endif
