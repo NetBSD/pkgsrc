be477493082caedaca89f06a9a2573ce264f8c5e
adam <adam@pkgsrc.org>
2022-07-01 19:35:45 +0000

    ugrep: updated to 3.8.3
    ugrep v3.8.3
    
    New option combination -o (--only-matching) with -ABC context to show the match within NUM columns, with context before and/or after, which visually aids in searching files with long lines; option -o with -b or -k now shows results on separate lines; new option --width to truncate long lines to the specified width or the terminal width by default; short ug command now sorts by name by default; query TUI ALT-] [ (increase/decrease context) and ALT-} { (increase/decrease fuzziness) keys switched meaning (quicker and more intuitive); new %+ format field used by updated --only-line-number option; new --help fuzzy help page; updated man pages; fixed bz2 issue cutting off decompression too soon, fixed tiny lz4 file search error, and other improvements.

--- textproc/ugrep/options.mk
+++ textproc/ugrep/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2020/08/01 15:13:59 scole Exp $
+# $NetBSD: options.mk,v 1.2 2022/07/01 19:35:45 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ugrep
-PKG_SUPPORTED_OPTIONS=	cpu-optimization boost-libs pcre2
-PKG_SUGGESTED_OPTIONS=	cpu-optimization boost-libs pcre2
+PKG_SUPPORTED_OPTIONS=	cpu-optimization boost-libs
+PKG_SUGGESTED_OPTIONS=	cpu-optimization boost-libs
 
 .include "../../mk/bsd.options.mk"
 
@@ -16,11 +16,4 @@ CONFIGURE_ARGS+=	--disable-neon
 .if !empty(PKG_OPTIONS:Mboost-libs)
 CONFIGURE_ARGS+=	--with-boost-regex
 .include "../../devel/boost-libs/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mpcre2)
-CONFIGURE_ARGS+=	--with-pcre2
-.include "../../devel/pcre2/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-pcre2
 .endif
