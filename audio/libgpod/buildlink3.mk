44d859fcb4ce7eec9e5a9a5115c1e8d6301cb781
wiz <wiz@pkgsrc.org>
2022-07-03 10:13:49 +0000

    *: bump PKGREVISION for libplist shlib name change

--- audio/libgpod/buildlink3.mk
+++ audio/libgpod/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2022/04/18 19:09:40 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2022/07/03 10:13:49 wiz Exp $
 
 BUILDLINK_TREE+=	libgpod
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgpod
 LIBGPOD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgpod+=	libgpod>=0.3.2
-BUILDLINK_ABI_DEPENDS.libgpod+=	libgpod>=0.8.3nb3
+BUILDLINK_ABI_DEPENDS.libgpod+=	libgpod>=0.8.3nb4
 BUILDLINK_PKGSRCDIR.libgpod?=	../../audio/libgpod
 
 .include "../../devel/glib2/buildlink3.mk"
