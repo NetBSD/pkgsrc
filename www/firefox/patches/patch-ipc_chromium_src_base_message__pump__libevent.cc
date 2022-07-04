b2bc076e0c20968675e993efd4d5706eefab2b6c
ryoon <ryoon@pkgsrc.org>
2022-07-04 14:34:07 +0000

    firefox: Update to 102.0
    Changelog:
    New
    
      * Tired of too many windows crowding your screen? You can now disable
        automatic opening of the download panel every time a new download starts.
        Read more.
    
      * Firefox now mitigates query parameter tracking when navigating sites in ETP
        strict mode.
    
    Fixed
    
      * When using a screen reader on Windows, pressing enter to activate an
        element no longer fails or clicks the wrong element and/or another
        application window. For those blind or with very limited vision, this
        technology reads out loud what is on the screen, and users can adapt them
        to their needs (now, on our platform, without errors).
    
      * Various security fixes.
    
    Changed
    
      * Improved security by moving audio decoding into a separate process with
        stricter sandboxing, thus improving process isolation.
    
    Enterprise
    
      * Various bug fixes and new policies have been implemented in the latest
        version of Firefox. You can find more information in the Firefox for
        Enterprise 102 Release Notes.
    
      * Firefox 102 is the new Extended Support Release (ESR). Firefox 91 ESR goes
        out of support on September 20, 2022. (See the 102 ESR release notes for
        more information)
    
    Developer
    
      * Developer Information
      * You can now filter style sheets in the Style Editor tab of our developer
        tools
    
    Web Platform
    
      * TransformStream and ReadableStream.pipeThrough have landed, allowing you to
        pipe from a ReadableStream to a WritableStream, executing a transformation
        on each chunk.
    
      * ReadableStream, TransformStream, and WritableStream are all transferable
        now.
    
      * Firefox now supports Content-Security-Policy (CSP) integration with
        WebAssembly. A document with a CSP that restricts scripts will no longer
        execute WebAssembly unless the policy uses 'unsafe-eval' or the new
        'wasm-unsafe-eval' keyword.
    
    Security fixes:
    #CVE-2022-34479: A popup window could be resized in a way to overlay the
     address bar with web content
    #CVE-2022-34470: Use-after-free in nsSHistory
    #CVE-2022-34468: CSP sandbox header without `allow-scripts` can be bypassed via
     retargeted javascript: URI
    #CVE-2022-34482: Drag and drop of malicious image could have led to malicious
     executable and potential code execution
    #CVE-2022-34483: Drag and drop of malicious image could have led to malicious
     executable and potential code execution
    #CVE-2022-34476: ASN.1 parser could have been tricked into accepting malformed
     ASN.1
    #CVE-2022-34481: Potential integer overflow in ReplaceElementsAt
    #CVE-2022-34474: Sandboxed iframes could redirect to external schemes
    #CVE-2022-34469: TLS certificate errors on HSTS-protected domains could be
     bypassed by the user on Firefox for Android
    #CVE-2022-34471: Compromised server could trick a browser into an addon
     downgrade
    #CVE-2022-34472: Unavailable PAC file resulted in OCSP requests being blocked
    #CVE-2022-34478: Microsoft protocols can be attacked if a user accepts a prompt
    #CVE-2022-2200: Undesired attributes could be set as part of prototype
     pollution
    #CVE-2022-34480: Free of uninitialized pointer in lg_init
    #CVE-2022-34477: MediaError message property leaked information on cross-origin
     same-site pages
    #CVE-2022-34475: HTML Sanitizer could have been bypassed via same-origin script
     via use tags
    #CVE-2022-34473: HTML Sanitizer could have been bypassed via use tags
    #CVE-2022-34484: Memory safety bugs fixed in Firefox 102 and Firefox ESR 91.11
    #CVE-2022-34485: Memory safety bugs fixed in Firefox 102

--- www/firefox/patches/patch-ipc_chromium_src_base_message__pump__libevent.cc
+++ www/firefox/patches/patch-ipc_chromium_src_base_message__pump__libevent.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-ipc_chromium_src_base_message__pump__libevent.cc,v 1.11 2019/03/19 16:11:28 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_message__pump__libevent.cc,v 1.12 2022/07/04 14:34:08 ryoon Exp $
 
 Allow older libevent
 
---- ipc/chromium/src/base/message_pump_libevent.cc.orig	2019-03-07 16:53:35.000000000 +0000
+--- ipc/chromium/src/base/message_pump_libevent.cc.orig	2022-06-16 21:32:07.000000000 +0000
 +++ ipc/chromium/src/base/message_pump_libevent.cc
-@@ -22,6 +22,7 @@
+@@ -24,6 +24,7 @@
  
  // This macro checks that the _EVENT_SIZEOF_* constants defined in
  // ipc/chromiume/src/third_party/<platform>/event2/event-config.h are correct.
@@ -12,9 +12,9 @@ Allow older libevent
  #if defined(_EVENT_SIZEOF_SHORT)
  #  define CHECK_EVENT_SIZEOF(TYPE, type)                \
      static_assert(_EVENT_SIZEOF_##TYPE == sizeof(type), \
-@@ -41,6 +42,7 @@ CHECK_EVENT_SIZEOF(PTHREAD_T, pthread_t)
- CHECK_EVENT_SIZEOF(SHORT, short);
+@@ -44,6 +45,7 @@ CHECK_EVENT_SIZEOF(SHORT, short);
  CHECK_EVENT_SIZEOF(SIZE_T, size_t);
+ CHECK_EVENT_SIZEOF(TIME_T, time_t);
  CHECK_EVENT_SIZEOF(VOID_P, void*);
 +#endif
  
