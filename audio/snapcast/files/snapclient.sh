c94bebed2a22012d820116094edfc533a29564f8
nia <nia@pkgsrc.org>
2022-07-06 10:02:13 +0000

    snapcast: /var/run is cleared on boot so needs to be created by the rc script

--- audio/snapcast/files/snapclient.sh
+++ audio/snapcast/files/snapclient.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: snapclient.sh,v 1.1 2022/07/03 16:09:15 nia Exp $
+# $NetBSD: snapclient.sh,v 1.2 2022/07/06 10:02:14 nia Exp $
 #
 # PROVIDE: snapclient
 # KEYWORD: shutdown
@@ -16,8 +16,17 @@ rcvar=${name}
 command="@PREFIX@/bin/snapclient"
 command_args="-d"
 pidfile="@VARBASE@/run/snapclient/pid"
-snapclient_user=@SNAPCLIENT_USER@
-snapclient_group=@SNAPCLIENT_GROUP@
+snapclient_user="@SNAPCLIENT_USER@"
+snapclient_group="@SNAPCLIENT_GROUP@"
+start_precmd="snapclient_precmd"
+
+snapclient_precmd()
+{
+	if [ ! -d "@VARBASE@/run/snapclient" ]; then
+		mkdir -p -m 755 "@VARBASE@/run/snapclient"
+		chown @SNAPCLIENT_USER@:@SNAPCLIENT_GROUP@ "@VARBASE@/run/snapclient"
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
