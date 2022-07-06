f741022127fc38e8164369918d51c1acf81cff5b
adam <adam@pkgsrc.org>
2022-07-06 15:16:59 +0000

    abseil: updated to 20220623.0
    Abseil LTS 20220623
    
    What's New:
    
    Added absl::AnyInvocable, a move-only function type.
    Added absl::CordBuffer, a type for buffering data for eventual inclusion an absl::Cord, which is useful for writing zero-copy code.
    Added support for command-line flags of type absl::optional<T>.
    
    Breaking Changes:
    
    CMake builds now use the flag ABSL_BUILD_TESTING (default: OFF) to control whether or not unit tests are built.
    The ABSL_DEPRECATED macro now works with the GCC compiler. GCC users that are experiencing new warnings can use -Wno-deprecated-declatations silence the warnings or use -Wno-error=deprecated-declarations to see warnings but not fail the build.
    ABSL_CONST_INIT uses the C++20 keyword constinit when available. Some compilers are more strict about where this keyword must appear compared to the pre-C++20 implementation.
    Bazel builds now depend on the bazelbuild/bazel-skylib repository. See Abseil's WORKSPACE file for an example of how to add this dependency.
    
    Other:
    
    This will be the last release to support C++11. Future releases will require at least C++14.

--- devel/abseil/patches/patch-absl_debugging_internal_vdso__support.cc
+++ devel/abseil/patches/patch-absl_debugging_internal_vdso__support.cc
@@ -1,23 +1,12 @@
-$NetBSD: patch-absl_debugging_internal_vdso__support.cc,v 1.3 2022/03/26 22:50:18 tnn Exp $
+$NetBSD: patch-absl_debugging_internal_vdso__support.cc,v 1.4 2022/07/06 15:16:59 adam Exp $
 
 Support NetBSD.
 https://github.com/abseil/abseil-cpp/pull/1121
 No support for SunOS yet, so disable for the time being.
 
---- absl/debugging/internal/vdso_support.cc.orig	2021-11-03 15:26:14.000000000 +0000
+--- absl/debugging/internal/vdso_support.cc.orig	2022-06-23 18:22:47.000000000 +0000
 +++ absl/debugging/internal/vdso_support.cc
-@@ -50,6 +50,10 @@
- #define AT_SYSINFO_EHDR 33  // for crosstoolv10
- #endif
- 
-+#if defined(__NetBSD__)
-+using Elf32_auxv_t = Aux32Info;
-+using Elf64_auxv_t = Aux64Info;
-+#endif
- #if defined(__FreeBSD__)
- using Elf64_auxv_t = Elf64_Auxinfo;
- using Elf32_auxv_t = Elf32_Auxinfo;
-@@ -93,6 +97,9 @@ const void *VDSOSupport::Init() {
+@@ -101,6 +101,9 @@ const void *VDSOSupport::Init() {
      }
    }
  #endif  // ABSL_HAVE_GETAUXVAL
@@ -27,21 +16,7 @@ No support for SunOS yet, so disable for the time being.
    if (vdso_base_.load(std::memory_order_relaxed) == kInvalidBase) {
      int fd = open("/proc/self/auxv", O_RDONLY);
      if (fd == -1) {
-@@ -104,8 +111,13 @@ const void *VDSOSupport::Init() {
-     ElfW(auxv_t) aux;
-     while (read(fd, &aux, sizeof(aux)) == sizeof(aux)) {
-       if (aux.a_type == AT_SYSINFO_EHDR) {
-+#if defined(__NetBSD__)
-+        vdso_base_.store(reinterpret_cast<void *>(aux.a_v),
-+                         std::memory_order_relaxed);
-+#else
-         vdso_base_.store(reinterpret_cast<void *>(aux.a_un.a_val),
-                          std::memory_order_relaxed);
-+#endif
-         break;
-       }
-     }
-@@ -127,6 +139,7 @@ const void *VDSOSupport::Init() {
+@@ -140,6 +143,7 @@ const void *VDSOSupport::Init() {
    // from assigning to getcpu_fn_ more than once.
    getcpu_fn_.store(fn, std::memory_order_relaxed);
    return vdso_base_.load(std::memory_order_relaxed);
