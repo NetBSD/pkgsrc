3bed73c43b9f9fd149f06cbebfeb75ecd1aa31aa
khorben <khorben@pkgsrc.org>
2022-07-07 02:31:45 +0000

    prosody: make sure pidfile always matches PROSODY_RUN in the RC script
    This concludes my investigation on the correct path for the PID file.
    No changes to the final binary if PROSODY_RUN is set to its default
    value.
    
    Tested on NetBSD/amd64.
    
    XXX pull-up to pkgsrc-2022Q2

--- chat/prosody/files/prosody.sh
+++ chat/prosody/files/prosody.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: prosody.sh,v 1.3 2022/07/07 01:53:00 khorben Exp $
+# $NetBSD: prosody.sh,v 1.4 2022/07/07 02:31:46 khorben Exp $
 #
 # PROVIDE: prosody
 # REQUIRE: DAEMON
@@ -13,7 +13,7 @@ name="prosody"
 rcvar=${name}
 ctl_command="@PREFIX@/bin/${name}ctl"
 required_files="@PKG_SYSCONFDIR@/${name}.cfg.lua"
-pidfile="@VARBASE@/run/prosody/${name}.pid"
+pidfile="@PROSODY_RUN@/${name}.pid"
 #start_precmd="ulimit -n 2048"
 extra_commands="reload status"
 
