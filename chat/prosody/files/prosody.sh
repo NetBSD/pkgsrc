41283ce12f35062a7223161ddae6c398fa63553c
khorben <khorben@pkgsrc.org>
2022-07-07 01:53:00 +0000

    prosody: fix the path to the PID file in the RC script
    PROSODY_RUN is set to eg /var/run/prosody/prosody.pid instead of just
    /var/run/prosody.pid, which is a good thing (tm) since prosody's user
    needs the access rights to write to the corresponding directory.
    
    Unfortunately, the directory is not automatically created nor the right
    permissions set yet, but this is progress.
    
    While there, appease pkglint(1).
    
    Bumps PKGREVISION.
    
    Tested on NetBSD/amd64.
    
    XXX pull-up to pkgsrc-2022Q2 once the complete solution is in place

--- chat/prosody/files/prosody.sh
+++ chat/prosody/files/prosody.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: prosody.sh,v 1.2 2021/08/29 18:32:29 khorben Exp $
+# $NetBSD: prosody.sh,v 1.3 2022/07/07 01:53:00 khorben Exp $
 #
 # PROVIDE: prosody
 # REQUIRE: DAEMON
@@ -13,7 +13,7 @@ name="prosody"
 rcvar=${name}
 ctl_command="@PREFIX@/bin/${name}ctl"
 required_files="@PKG_SYSCONFDIR@/${name}.cfg.lua"
-pidfile="@VARBASE@/run/${name}.pid"
+pidfile="@VARBASE@/run/prosody/${name}.pid"
 #start_precmd="ulimit -n 2048"
 extra_commands="reload status"
 
