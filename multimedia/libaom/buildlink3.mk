e332cd1f21788cfc8863314ba5b63df3606bf57a
adam <adam@pkgsrc.org>
2022-07-07 10:13:56 +0000

    libaom: updated to 3.4.0
    v3.4.0
    This release includes compression efficiency and perceptual quality
    improvements, speedup and memory optimizations, and some new features.
    There are no ABI or API breaking changes in this release.
    
    - New Features
      * New --dist-metric flag with "qm-psnr" value to use quantization
        matrices in the distortion computation for RD search. The default
        value is "psnr".
      * New command line option "--auto-intra-tools-off=1" to make
        all-intra encoding faster for high bit rate under
        "--deltaq-mode=3" mode.
      * New rate control library aom_av1_rc for real-time hardware
        encoders. Supports CBR for both one spatial layer and SVC.
      * New image format AOM_IMG_FMT_NV12 can be used as input to the
        encoder. The presence of AOM_IMG_FMT_NV12 can be detected at
        compile time by checking if the macro AOM_HAVE_IMG_FMT_NV12 is
        defined.
      * New codec controls for the encoder:
        o AV1E_SET_AUTO_INTRA_TOOLS_OFF. Only in effect if
          --deltaq-mode=3.
        o AV1E_SET_RTC_EXTERNAL_RC
        o AV1E_SET_FP_MT. Only supported if libaom is built with
          -DCONFIG_FRAME_PARALLEL_ENCODE=1.
        o AV1E_GET_TARGET_SEQ_LEVEL_IDX
      * New key-value pairs for the key-value API:
        o --auto-intra-tools-off=0 (default) or 1. Only in effect if
          --deltaq-mode=3.
        o --strict-level-conformance=0 (default) or 1
        o --fp-mt=0 (default) or 1. Only supported if libaom is built
          with -DCONFIG_FRAME_PARALLEL_ENCODE=1.
      * New aomenc options (not supported by the key-value API):
        o --nv12
    
    - Compression Efficiency Improvements
      * Correctly calculate SSE for high bitdepth in skip mode, 0.2% to
        0.6% coding gain.
      * RTC at speed 9/10: BD-rate gain of ~4/5%
      * RTC screen content coding: many improvements for real-time screen
        at speed 10 (quality, speedup, and rate control), up to high
        resolutions (1080p).
      * RTC-SVC: fixes to make intra-only frames work for spatial layers.
      * RTC-SVC: quality improvements for temporal layers.
      * AV1 RT: A new passive rate control strategy for screen content, an
        average of 7.5% coding gain, with some clips of 20+%. The feature
        is turned off by default due to higher bit rate variation.
    
    - Perceptual Quality Improvements
      * RTC: Visual quality improvements for high speeds (9/10)
      * Improvements in coding quality for all intra mode
    
    - Speedup and Memory Optimizations
      * ~10% speedup in good quality mode encoding.
      * ~7% heap memory reduction in good quality encoding mode for speed
        5 and 6.
      * Ongoing improvements to intra-frame encoding performance on Arm
      * Faster encoding speed for "--deltaq-mode=3" mode.
      * ~10% speedup for speed 5/6, ~15% speedup for speed 7/8, and
        ~10% speedup for speed 9/10 in real time encoding mode
      * ~20% heap memory reduction in still-picture encoding mode for
        360p-720p resolutions with multiple threads
      * ~13% speedup for speed 6 and ~12% speedup for speed 9 in
        still-picture encoding mode.
      * Optimizations to improve multi-thread efficiency for still-picture
        encoding mode.
    
    - Bug Fixes

--- multimedia/libaom/buildlink3.mk
+++ multimedia/libaom/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/06/28 11:34:56 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/07/07 10:13:56 adam Exp $
 
 BUILDLINK_TREE+=	libaom
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libaom
 LIBAOM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libaom+=	libaom>=1.0.0
-BUILDLINK_ABI_DEPENDS.libaom?=	libaom>=3.3.0nb1
+BUILDLINK_ABI_DEPENDS.libaom+=	libaom>=3.3.0nb1
 BUILDLINK_PKGSRCDIR.libaom?=	../../multimedia/libaom
 .endif	# LIBAOM_BUILDLINK3_MK
 
