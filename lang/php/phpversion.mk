3148b4d0be4360cb278c092f0c6105f433b5dc38
taca <taca@pkgsrc.org>
2022-07-08 13:51:56 +0000

    lang/php81: update to 8.1.8
    07 Jul 2022, PHP 8.1.8
    
    - Core:
      . Fixed bug GH-8338 (Intel CET is disabled unintentionally). (Chen, Hu)
      . Fixed leak in Enum::from/tryFrom for internal enums when using JIT (ilutov)
      . Fixed calling internal methods with a static return type from
        extension code. (Sara)
      . Fixed bug GH-8655 (Casting an object to array does not unwrap refcount=1
        references). (Nicolas Grekas)
      . Fixed potential use after free in php_binary_init(). (Heiko Weber)
    
    - CLI:
      . Fixed GH-8827 (Intentionally closing std handles no longer possible). (cmb)
    
    - COM:
      . Fixed bug GH-8778 (Integer arithmethic with large number variants fails).
        (cmb)
    
    - Curl:
      . Fixed CURLOPT_TLSAUTH_TYPE is not treated as a string option. (Pierrick)
    
    - Date:
      . Fixed bug #72963 (Null-byte injection in CreateFromFormat and related
        functions). (Derick)
      . Fixed bug #74671 (DST timezone abbreviation has incorrect offset). (Derick)
      . Fixed bug #77243 (Weekdays are calculated incorrectly for negative years).
        (Derick)
      . Fixed bug #78139 (timezone_open accepts invalid timezone string argument).
        (Derick)
    
    - Fileinfo:
      . Fixed bug #81723 (Heap buffer overflow in finfo_buffer). (CVE-2022-31627)
        (cmb)
    
    - FPM:
      . Fixed bug #67764 (fpm: syslog.ident don't work). (Jakub Zelenka)
    
    - GD:
      . Fixed imagecreatefromavif() memory leak. (cmb)
    
    - MBString:
      . mb_detect_encoding recognizes all letters in Czech alphabet (alexdowad)
      . mb_detect_encoding recognizes all letters in Hungarian alphabet (alexdowad)
      . Fixed bug GH-8685 (pcre not ready at mbstring startup). (Remi)
      . Backwards-compatible mappings for 0x5C/0x7E in Shift-JIS are restored,
        after they had been changed in 8.1.0. (Alex Dowad)
    
    - ODBC:
      . Fixed handling of single-key connection strings. (Calvin Buckley)
    
    - OPcache:
      . Fixed bug GH-8591 (tracing JIT crash after private instance method change).
        (Arnaud, Dmitry, Oleg Stepanischev)
    
    - OpenSSL:
      . Fixed bug #50293 (Several openssl functions ignore the VCWD).
        (Jakub Zelenka, cmb)
      . Fixed bug #81713 (NULL byte injection in several OpenSSL functions working
        with certificates). (Jakub Zelenka)
    
    - PDO_ODBC:
      . Fixed handling of single-key connection strings. (Calvin Buckley)
    
    - SPL:
      . Fixed bug GH-8563 (Different results for seek() on SplFileObject and SplTempFileObject). (Girgias)
    
    - Zip:
      . Fixed bug GH-8781 (ZipArchive::close deletes zip file without updating stat
        cache). (Remi)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.369 2022/07/08 13:50:32 taca Exp $
+# $NetBSD: phpversion.mk,v 1.370 2022/07/08 13:51:56 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.30
 PHP80_VERSION=	8.0.21
-PHP81_VERSION=	8.1.7
+PHP81_VERSION=	8.1.8
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
