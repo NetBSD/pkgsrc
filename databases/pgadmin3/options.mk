36ffd6e898934e2db8a344f5bec076d1d423a42a
wiz <wiz@pkgsrc.org>
2022-07-10 18:43:19 +0000

    pgadmin3: does not build against postgres14
    Somebody(TM) should really package pgadmin4
    
    Fix doc option, and restrict to python 3.
    
    Bump PKGREVISION for PLIST change for doc option.

--- databases/pgadmin3/options.mk
+++ databases/pgadmin3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2019/10/21 21:19:35 adam Exp $
+# $NetBSD: options.mk,v 1.5 2022/07/10 18:43:19 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pgadmin3
 PKG_SUPPORTED_OPTIONS=	doc
@@ -10,9 +10,9 @@ PLIST_VARS=	doc
 .if !empty(PKG_OPTIONS:Mdoc)
 PLIST.doc=	yes
 PYTHON_FOR_BUILD_ONLY=yes
+PYTHON_VERSIONS_INCOMPATIBLE=	27
+BUILD_DEPENDS+=	${PYPKGPREFIX}-sphinx-[0-9]*:../../textproc/py-sphinx
 .include "../../lang/python/pyversion.mk"
-.include "../../lang/python/versioned_dependencies.mk"
-PYTHON_VERSIONED_DEPENDENCIES=	sphinx:build
 CONFIGURE_ARGS+=	--with-sphinx-build=${PREFIX}/bin/sphinx-build-${PYVERSSUFFIX}
 .else
 CONFIGURE_ENV+=		ac_cv_path_SPHINX_BUILD=
