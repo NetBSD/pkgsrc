1f1e2c92560404edeaf0cd65cad9580ec0f5160b
adam <adam@pkgsrc.org>
2022-07-11 11:49:48 +0000

    gerbera: updated to 1.11.0
    Gerbera v1.11.0
    
    NEW Features
    
    Database: Clients and statistics are stored database so restart does not empty client list. Client grouping for play statistics.
    Search: Support searching playlists containers
    Search: Respect ContainerID when performing search
    Import: item class filtering and mapping by file properties allows more sophisticated virtual structure
    Transcoding: Support filtering transcoding profiles by resource properties (like codecs) avoids transcoding if client can play files
    DLNA: Detect DNLA profiles by resource attributes to specify more detailled profile for handling in client
    File type support for WavPack improved: More metadata read with special library if compiled in.
    Support Ubuntu 22.04
    
    FIXES
    
    Playlist: Fix parser error
    Playlist: Handle end of file properly
    Browsing: Sort containers first
    Search: search result is sort by title now
    Import: Timestamps in future are not stored for containers

--- multimedia/gerbera/patches/patch-src_main.cc
+++ multimedia/gerbera/patches/patch-src_main.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_main.cc,v 1.1 2022/02/08 06:26:49 khorben Exp $
+$NetBSD: patch-src_main.cc,v 1.2 2022/07/11 11:49:48 adam Exp $
 
 Fix build on NetBSD
 
---- src/main.cc.orig	2021-09-30 19:23:27.000000000 +0000
+--- src/main.cc.orig	2022-05-03 17:39:25.000000000 +0000
 +++ src/main.cc
-@@ -266,7 +266,7 @@ int main(int argc, char** argv, char** e
- // mac os x does this differently, setgid and setuid are basically doing the same
+@@ -287,7 +287,7 @@ int main(int argc, char** argv, char** e
+ // macOS does this differently, setgid and setuid are basically doing the same
  // as setresuid and setresgid on linux: setting all of real{u,g}id, effective{u,g}id and saved-set{u,g}id
  // Solaroid systems are likewise missing setresgid and setresuid
 -#if defined(__APPLE__) || defined(SOLARIS) || defined(__CYGWIN__) || defined(__HAIKU__)
 +#if defined(__APPLE__) || defined(SOLARIS) || defined(__CYGWIN__) || defined(__HAIKU__) || defined(__NetBSD__)
              // set group-ids, then add. groups, last user-ids, all need to succeed
-             if (0 != setgid(user_id->pw_gid) || 0 != initgroups(user_id->pw_name, user_id->pw_gid) || 0 != setuid(user_id->pw_uid)) {
+             if (0 != setgid(userId->pw_gid) || 0 != initgroups(userId->pw_name, userId->pw_gid) || 0 != setuid(userId->pw_uid)) {
  #else
