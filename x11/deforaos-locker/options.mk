8e65dddced3004f37f846746ae30a54d218055c4
khorben <khorben@pkgsrc.org>
2022-07-11 01:36:50 +0000

    deforaos-locker: update to version 0.4.2
    This update brings:
    - re-licensing to the BSD license (2 clause)
    - renaming of the desktop files
    - documentation for plug-ins with Gtk-Doc
    - re-generation of Makefiles with DeforaOS configure

--- x11/deforaos-locker/options.mk
+++ x11/deforaos-locker/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2012/12/17 00:47:38 khorben Exp $
+# $NetBSD: options.mk,v 1.2 2022/07/11 01:36:50 khorben Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.deforaos-locker
 PKG_SUPPORTED_OPTIONS=	embedded
@@ -6,5 +6,5 @@ PKG_SUPPORTED_OPTIONS=	embedded
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Membedded)
-MAKE_FLAGS+=	CPPFLAGS=-DEMBEDDED
+DEFORAOS_CONFIGURE_MODE=	embedded-debug
 .endif
