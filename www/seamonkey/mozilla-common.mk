5bae3f8ca13ed5981b903fe3eaf34c783c9d9402
dholland <dholland@pkgsrc.org>
2022-07-11 22:36:36 +0000

    www/seamonkey: accept python310 as well as python3[6789]

--- www/seamonkey/mozilla-common.mk
+++ www/seamonkey/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.12 2022/05/15 00:37:26 dholland Exp $
+# $NetBSD: mozilla-common.mk,v 1.13 2022/07/11 22:36:36 dholland Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -20,7 +20,7 @@ PYTHON_FOR_BUILD_ONLY=		tool
 TOOL_DEPENDS+=			${PYPKGPREFIX}-expat-[0-9]*:../../textproc/py-expat
 # Include pyversion.mk after setting PYTHON_* but before testing the default.
 .include "../../lang/python/pyversion.mk"
-.if !empty(PYTHON_VERSION_DEFAULT:M3[6789])
+.if !empty(PYTHON_VERSION_DEFAULT:M3[6789]) || !empty(PYTHON_VERSION_DEFAULT:M310)
 TOOL_DEPENDS+=			python${PYTHON_VERSION_DEFAULT}-[0-9]*:../../lang/python${PYTHON_VERSION_DEFAULT}
 ALL_ENV+=			PYTHON3=${PREFIX}/bin/python${PYTHON_VERSION_DEFAULT:S/3/3./}
 .else
