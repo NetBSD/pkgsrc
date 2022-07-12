fec2a62cb0b9182a87e86ec9fc57c15427c62fab
adam <adam@pkgsrc.org>
2022-07-12 07:28:27 +0000

    py-rlp: updated to 3.0.0
    3.0.0:
    Unknown changes

--- devel/py-rlp/patches/patch-setup.py
+++ devel/py-rlp/patches/patch-setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-setup.py,v 1.1 2021/04/11 16:59:36 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2022/07/12 07:28:27 adam Exp $
 
 setuptools-markdown is deprecated for functionality included in setuptools.
 
---- setup.py.orig	2020-11-23 15:09:47.000000000 +0000
+--- setup.py.orig	2021-12-06 20:00:00.000000000 +0000
 +++ setup.py
 @@ -52,7 +52,7 @@ setup(
      url='https://github.com/ethereum/pyrlp',
@@ -11,5 +11,5 @@ setuptools-markdown is deprecated for functionality included in setuptools.
 -    setup_requires=['setuptools-markdown'],
 +    setup_requires=[],
      install_requires=[
-         "eth-utils>=1.0.2,<2",
+         "eth-utils>=2.0.0,<3",
      ],
