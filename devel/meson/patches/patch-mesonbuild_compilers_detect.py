50bb4a06bc8007be531d112bf6ec4b6f3a7a5f34
triaxx <triaxx@pkgsrc.org>
2022-07-13 03:57:22 +0000

    commit.msg

--- devel/meson/patches/patch-mesonbuild_compilers_detect.py
+++ devel/meson/patches/patch-mesonbuild_compilers_detect.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-mesonbuild_compilers_detect.py,v 1.1 2021/09/27 18:50:06 adam Exp $
+$NetBSD: patch-mesonbuild_compilers_detect.py,v 1.2 2022/07/13 03:57:22 triaxx Exp $
 
 Specify c++ language when making CXX parse standard input.
 Fixes problem where graphics/MesaLib fails to build due to:
@@ -8,7 +8,7 @@ Fixes problem where graphics/MesaLib fails to build due to:
 
 --- mesonbuild/compilers/detect.py.orig	2021-09-10 05:30:31.000000000 +0000
 +++ mesonbuild/compilers/detect.py
-@@ -1162,7 +1162,10 @@ def _get_gnu_compiler_defines(compiler: 
+@@ -1224,7 +1224,10 @@ def _get_gnu_compiler_defines(compiler: 
      """
      # Arguments to output compiler pre-processor defines to stdout
      # gcc, g++, and gfortran all support these arguments
@@ -20,7 +20,7 @@ Fixes problem where graphics/MesaLib fails to build due to:
      p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
      if p.returncode != 0:
          raise EnvironmentException('Unable to detect GNU compiler type:\n' + output + error)
-@@ -1186,7 +1189,10 @@ def _get_clang_compiler_defines(compiler
+@@ -1248,7 +1251,10 @@ def _get_clang_compiler_defines(compiler
      """
      Get the list of Clang pre-processor defines
      """
