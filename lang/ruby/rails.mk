df30aad9d82af5a5025a968da00421dd8d573b5c
taca <taca@pkgsrc.org>
2022-07-13 14:41:07 +0000

    www/ruby-rails52: update to 5.2.8.1
    Rails 5.2.8.1 (2022-07-12) updates databases/ruby-activerecord52 only.
    
    databases/ruby-activerecord52
    
    * Change ActiveRecord::Coders::YAMLColumn default to safe_load
    
      This adds two new configuration options The configuration options are as
      follows:
    
    	o config.active_storage.use_yaml_unsafe_load
    
      When set to true, this configuration option tells Rails to use the old
      "unsafe" YAML loading strategy, maintaining the existing behavior but
      leaving the possible escalation vulnerability in place.  Setting this
      option to true is *not* recommended, but can aid in upgrading.
    
    	o config.active_record.yaml_column_permitted_classes
    
      The "safe YAML" loading method does not allow all classes to be
      deserialized by default.  This option allows you to specify classes deemed
      "safe" in your application.  For example, if your application uses Symbol
      and Time in serialized data, you can add Symbol and Time to the allowed
      list as follows:
    
    
    	config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time]
    
    
      [CVE-2022-32224]

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.128 2022/06/07 15:12:58 taca Exp $
+# $NetBSD: rails.mk,v 1.129 2022/07/13 14:41:07 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -47,7 +47,7 @@ _RUBY_RAILS_MK=	# defined
 #
 # current Ruby on Rails versions.
 #
-RUBY_RAILS52_VERSION?=	5.2.8
+RUBY_RAILS52_VERSION?=	5.2.8.1
 RUBY_RAILS60_VERSION?=	6.0.5
 RUBY_RAILS61_VERSION?=	6.1.6
 RUBY_RAILS70_VERSION?=	7.0.3
