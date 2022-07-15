72a21fb77b8d24161a6133df82e1f447818d2b8b
adam <adam@pkgsrc.org>
2022-07-15 13:16:36 +0000

    libuv: updated to 1.44.2
    Version 1.44.2 (Stable)
    
    Important bugs fixed
    
    loop: better align order-of-events behavior between platforms
    zos: fix fs event not fired if the watched file is moved/removed/recreated
    win: Fix pipe resource leak if closed during connect (and other bugs)
    zos: don't error when killing a zombie process
    
    Regressions fixed
    
    macos: avoid posix_spawnp() cwd bug
    kqueue: skip EVFILT_PROC events when invalidating events for an fd.

--- devel/libuv/patches/patch-autogen.sh
+++ devel/libuv/patches/patch-autogen.sh
@@ -1,17 +1,24 @@
-$NetBSD: patch-autogen.sh,v 1.1 2015/06/11 13:32:35 fhajny Exp $
+$NetBSD: patch-autogen.sh,v 1.2 2022/07/15 13:16:36 adam Exp $
 
+Portability fix.
 Use libtoolize on Darwin instead of glibtoolize
 
---- autogen.sh.orig	2015-06-05 17:45:58.000000000 +0000
+--- autogen.sh.orig	2022-07-12 16:16:33.000000000 +0000
 +++ autogen.sh
-@@ -16,10 +16,6 @@
- 
+@@ -17,15 +17,12 @@
+ set -eu
  cd `dirname "$0"`
  
--if [ "$LIBTOOLIZE" = "" ] && [ "`uname`" = "Darwin" ]; then
+-if [ "${1:-dev}" == "release" ]; then
++if [ "${1:-dev}" = "release" ]; then
+     export LIBUV_RELEASE=true
+ else
+     export LIBUV_RELEASE=false
+ fi
+ 
+-if [ "${LIBTOOLIZE:-}" = "" ] && [ "`uname`" = "Darwin" ]; then
 -  LIBTOOLIZE=glibtoolize
 -fi
--
+ 
  ACLOCAL=${ACLOCAL:-aclocal}
  AUTOCONF=${AUTOCONF:-autoconf}
- AUTOMAKE=${AUTOMAKE:-automake}
