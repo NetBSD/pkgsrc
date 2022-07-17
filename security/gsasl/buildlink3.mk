6025f5a95dd8832b99b46935d0c9de24ba7c8b10
wiz <wiz@pkgsrc.org>
2022-07-17 08:24:53 +0000

    gsasl: update to 2.0.1.
    * Noteworthy changes in release 2.0.1 (2022-07-15) [stable]
    
    ** Support for the libgssglue GSS-API library were added.
    We encourage you to build with libgssglue, as that allows system
    administrators and end-users to chose between MIT Kerberos, Heimdal
    and GNU GSS during run-time.  Read about the background here:
    https://blog.josefsson.org/2022/07/14/towards-pluggable-gss-api-modules/
    
    ** GSSAPI client: don't use AUTHID as fallback for AUTHZID.
    The code historically used the AUTHID as authorization identity, but
    in 2012 we changed it to first query for AUTHZID, and only if that is
    not available, fall back to using AUTHID as the authorization
    identity.  The change was not released until version 1.8.1 on
    2019-08-02, when it was properly documented to be removed 'after the
    year 2012'.  While documented behaviour, this seems like just
    surprising behaviour and we now finally make the change.
    
    ** GSSAPI server: don't set AUTHZID to empty string when absent.
    The GSS-API SASL protocol does not differentiate between an absent
    authorization identity and an authorization identity that is the empty
    string.  Previously libgsasl would set it to the empty string but now
    it is set to NULL.  The manual explains that this is a protocol
    limitation.
    
    ** The examples/smtp-server.c now supports GSSAPI/GS2-KRB5.
    The example is used during CI/CD testing of GNU SASL and thus it made
    sense to extend it.  Some bugs related to getline error conditions
    were also fixed.
    
    ** GSSAPI server: Fix out-of-bounds read.
    A malicious client can after it has authenticated with Kerberos send a
    specially crafted message that causes Libgsasl to read out of bounds
    and cause a crash in the server.
    
    * Noteworthy changes in release 2.0.0 (2022-06-20) [stable]
    
    ** Compared to last stable branch 1.10.x the 2.0.0 release
    ** drops all obsolete APIs, drops the abandoned KERBEROS_V5 mechanism,
    ** stops shipping a separate tarball for only the library, adds new APIs
    ** gsasl_mechanism_name_p() and gsasl_property_free().
    Numerous other translation improvements, code cleanups, bug fixes,
    documentation additions, build improvements and portability
    enhancements were made as well.

--- security/gsasl/buildlink3.mk
+++ security/gsasl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2021/10/21 07:46:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2022/07/17 08:24:53 wiz Exp $
 
 BUILDLINK_TREE+=	gsasl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gsasl
 GSASL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gsasl+=	gsasl>=0.2.15
-BUILDLINK_ABI_DEPENDS.gsasl+=	gsasl>=1.10.0nb3
+BUILDLINK_ABI_DEPENDS.gsasl+=	gsasl>=2.0
 BUILDLINK_PKGSRCDIR.gsasl?=	../../security/gsasl
 
 .include "../../devel/gettext-lib/buildlink3.mk"
