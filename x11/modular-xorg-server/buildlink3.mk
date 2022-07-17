194d508b5d249965e272b457214c5ef362a9bdf1
tnn <tnn@pkgsrc.org>
2022-07-17 02:29:41 +0000

    modular-xorg-server: add libxcvt to bl3 as required by xorg-server.pc

--- x11/modular-xorg-server/buildlink3.mk
+++ x11/modular-xorg-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2022/05/03 17:07:10 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2022/07/17 02:29:41 tnn Exp $
 
 BUILDLINK_TREE+=	modular-xorg-server
 
@@ -7,7 +7,7 @@ MODULAR_XORG_SERVER_BUILDLINK3_MK:=
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.18
+BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=21.1.4nb1
 BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
 BUILDLINK_INCDIRS.modular-xorg-server+=		include/xorg
 
@@ -16,6 +16,7 @@ BUILDLINK_DEPMETHOD.libpciaccess=	full
 .include "../../x11/pixman/buildlink3.mk"
 
 .include "../../x11/xorgproto/buildlink3.mk"
+.include "../../x11/libxcvt/buildlink3.mk"
 .include "../../x11/libXfont2/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk" # XXX should not be needed for non-dri build
 .endif # MODULAR_XORG_SERVER_BUILDLINK3_MK
