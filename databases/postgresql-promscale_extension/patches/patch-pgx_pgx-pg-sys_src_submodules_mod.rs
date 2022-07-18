c3b937452b526cf59be3a092b683d32acfad9e69
tnn <tnn@pkgsrc.org>
2022-07-18 19:47:26 +0000

    postgresql-promscale_extension: update to 0.5.2
    0.5.x is a major release of the Promscale Database Extension.
    The Promscale extension is mandatory for the connector deployment starting
    with this release, as all the SQL codebase has been migrated to the
    Promscale extension.
    This has been done to improve performance and the query experience.
    As such this version should only be used with promscale 0.11 or higher.

--- databases/postgresql-promscale_extension/patches/patch-pgx_pgx-pg-sys_src_submodules_mod.rs
+++ databases/postgresql-promscale_extension/patches/patch-pgx_pgx-pg-sys_src_submodules_mod.rs
@@ -1,9 +1,9 @@
-$NetBSD: patch-pgx_pgx-pg-sys_src_submodules_mod.rs,v 1.3 2022/03/27 14:05:36 tnn Exp $
+$NetBSD: patch-pgx_pgx-pg-sys_src_submodules_mod.rs,v 1.4 2022/07/18 19:47:26 tnn Exp $
 
 NetBSD, SunOS support
 
---- ../vendor/pgx-pg-sys-0.2.0/src/submodules/mod.rs.orig	1973-11-29 21:33:09.000000000 +0000
-+++ ../vendor/pgx-pg-sys-0.2.0/src/submodules/mod.rs
+--- ../pgx/pgx-pg-sys/src/submodules/mod.rs.orig	1973-11-29 21:33:09.000000000 +0000
++++ ../pgx/pgx-pg-sys/src/submodules/mod.rs
 @@ -20,7 +20,16 @@ extern "C" {
      ) -> std::os::raw::c_int;
  }
