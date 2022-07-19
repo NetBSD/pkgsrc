b725d8f5c4d3a0afc98dff2e722c2cf83a327010
bsiegert <bsiegert@pkgsrc.org>
2022-07-19 20:37:27 +0000

    New package, go-impl-1.1.0.
    Needed by the new gopls.
    
    impl generates method stubs for implementing an interface.
    
    Sample usage:
    
    $ impl 'f *File' io.ReadWriteCloser
    func (f *File) Read(p []byte) (n int, err error) {
    	panic("not implemented")
    }
    
    func (f *File) Write(p []byte) (n int, err error) {
    	panic("not implemented")
    }
    
    func (f *File) Close() error {
    	panic("not implemented")
    }

---
+++ devel/go-impl/go-modules.mk
@@ -0,0 +1,28 @@
+# $NetBSD: go-modules.mk,v 1.1 2022/07/19 20:37:27 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.1.27.mod
+GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.1.27.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191011191535-87dc89f01550.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191011191535-87dc89f01550.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.2.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.2.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190620200207-3b0461eec859.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20200226121028-0de0cce0169b.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20200226121028-0de0cce0169b.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190423024810-112230192c58.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190911185100-cd5d95a43a6e.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190911185100-cd5d95a43a6e.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20191119224855-298f0cb1881e.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20200522201501-cb1345f3a375.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20200522201501-cb1345f3a375.zip
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20190717185122-a985d3407aa7.mod
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191011141410-1b5146add898.mod
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191204190536-9bdfabe68543.mod
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191204190536-9bdfabe68543.zip
