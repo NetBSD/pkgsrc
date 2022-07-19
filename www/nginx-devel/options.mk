56b4e32eb47fc006379e4880fb76ca4b39ce68a2
osa <osa@pkgsrc.org>
2022-07-19 18:08:29 +0000

    www/nginx-devel: update 1.23.0 -> 1.23.1
    Also, update NGINX JavaScript module: 0.7.5 -> 0.7.6.
    
    <Changelog for NGINX 1.23.1>
    
    *) Feature: memory usage optimization in configurations with SSL
       proxying.
    
    *) Feature: looking up of IPv4 addresses while resolving now can be
       disabled with the "ipv4=off" parameter of the "resolver" directive.
    
    *) Change: the logging level of the "bad key share", "bad extension",
       "bad cipher", and "bad ecpoint" SSL errors has been lowered from
       "crit" to "info".
    
    *) Bugfix: while returning byte ranges nginx did not remove the
       "Content-Range" header line if it was present in the original backend
       response.
    
    *) Bugfix: a proxied response might be truncated during reconfiguration
       on Linux; the bug had appeared in 1.17.5.
    
    </Changelog>
    
    <ChangeLog for NGINX JavaScript 0.7.6>
    
    nginx modules:
    
    *) Feature: improved r.args object. Added support for multiple
       arguments with the same key. Added case sensitivity for
       keys. Keys and values are percent-decoded now.
    
    *) Bugfix: fixed r.headersOut setter for special headers.
    
    Core:
    
    *) Feature: added Symbol.for() and Symbol.keyfor().
    
    *) Feature: added btoa() and atob() from WHATWG spec.
    
    *) Bugfix: fixed large non-decimal literals.
    
    *) Bugfix: fixed unicode argument trimming in parseInt().
    
    *) Bugfix: fixed break instruction in a try-catch block.
    
    *) Bugfix: fixed async function declaration in CLI.
    
    </ChangeLog>

--- www/nginx-devel/options.mk
+++ www/nginx-devel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2022/07/14 15:13:22 osa Exp $
+# $NetBSD: options.mk,v 1.11 2022/07/19 18:08:29 osa Exp $
 
 CODELOAD_SITE_GITHUB=		https://codeload.github.com/
 
@@ -334,7 +334,7 @@ PLIST.rtmp=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mnjs) || make(makesum) || make(mdi) || make(distclean)
-NJS_VERSION=		0.7.5
+NJS_VERSION=		0.7.6
 NJS_DISTNAME=		njs-${NJS_VERSION}
 NJS_DISTFILE=		${NJS_DISTNAME}.tar.gz
 NJS_CONFIGURE_ARGS=	--no-pcre2
