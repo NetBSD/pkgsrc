c9e94fd3d922c1b066119bef01916cfc321caf5f
bsiegert <bsiegert@pkgsrc.org>
2022-07-19 20:26:07 +0000

    New package, gomodifytags-1.16.0.
    This is needed by the new gopls.
    
    gomodifytags is a Go tool to modify/update field tags in structs. It makes it
    easy to update, add or delete the tags in a struct field. You can easily add
    new tags, update existing tags (such as appending a new key, i.e. db, xml,
    etc.) or remove existing tags. It also allows you to add and remove tag
    options. It's intended to be used by an editor, but also has modes to run it
    from the terminal.

---
+++ devel/gomodifytags/go-modules.mk
@@ -0,0 +1,8 @@
+# $NetBSD: go-modules.mk,v 1.1 2022/07/19 20:26:07 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/fatih/camelcase/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/fatih/camelcase/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/fatih/structtag/@v/v1.2.0.mod
+GO_MODULE_FILES+=	github.com/fatih/structtag/@v/v1.2.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180824175216-6c1c5e93cdc1.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180824175216-6c1c5e93cdc1.zip
