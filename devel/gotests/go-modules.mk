2b128e157c647ec1dd9695ebc6dee392acf87013
bsiegert <bsiegert@pkgsrc.org>
2022-07-19 20:14:27 +0000

    New package for gotests-1.6.0.
    This is needed by the current gopls version.
    
    gotests makes writing Go tests easy. It is a Go command-line tool that
    generates table driven tests based on its target source files' function and
    method signatures. Any new dependencies in the test files are automatically
    imported.

---
+++ devel/gotests/go-modules.mk
@@ -0,0 +1,16 @@
+# $NetBSD: go-modules.mk,v 1.1 2022/07/19 20:14:27 bsiegert Exp $
+
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190620200207-3b0461eec859.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190620200207-3b0461eec859.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190423024810-112230192c58.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190423024810-112230192c58.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20191109212701-97ad0ed33101.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20191109212701-97ad0ed33101.zip
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20190717185122-a985d3407aa7.mod
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20190717185122-a985d3407aa7.zip
