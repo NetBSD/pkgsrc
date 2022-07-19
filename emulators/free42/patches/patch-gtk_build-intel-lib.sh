22f6530fb00fc7830b762bbc763e46bebdd0f471
dbj <dbj@pkgsrc.org>
2022-07-19 08:49:43 +0000

    emulators/free42: fix fenv.h inclusion detection on NetBSD

--- emulators/free42/patches/patch-gtk_build-intel-lib.sh
+++ emulators/free42/patches/patch-gtk_build-intel-lib.sh
@@ -1,4 +1,4 @@
-$NetBSD: patch-gtk_build-intel-lib.sh,v 1.1 2022/07/14 05:09:46 dbj Exp $
+$NetBSD: patch-gtk_build-intel-lib.sh,v 1.2 2022/07/19 08:49:43 dbj Exp $
 
 Avoid pkgsrc libtool on Darwin.
 
@@ -6,7 +6,7 @@ On NetBSD, build intel lib as if it was FreeBSD
 
 --- gtk/build-intel-lib.sh.orig	2022-06-16 09:39:50.000000000 +0000
 +++ gtk/build-intel-lib.sh
-@@ -45,6 +45,19 @@ tar xvfz ../inteldecimal/IntelRDFPMathLi
+@@ -45,6 +45,24 @@ tar xvfz ../inteldecimal/IntelRDFPMathLi
  cd IntelRDFPMathLib20U1
  patch -p0 <../intel-lib-linux.patch
  
@@ -14,6 +14,11 @@ On NetBSD, build intel lib as if it was FreeBSD
 +mv LIBRARY/makefile.iml_head LIBRARY/makefile.iml_head.orig && \
 +    sed -e 's/libtool/xcrun libtool/' \
 +        LIBRARY/makefile.iml_head.orig > LIBRARY/makefile.iml_head
++
++# Fixup fenv.h inclusion detection. NetBSD uses _FENV_H_
++mv LIBRARY/src/bid_functions.h LIBRARY/src/bid_functions.h.orig && \
++    sed -e 's/!defined (_FENV_H)/& \&\& !defined(_FENV_H_)/' \
++        LIBRARY/src/bid_functions.h.orig > LIBRARY/src/bid_functions.h
 +
 +# make NetBSD look like FreeBSD to the Makefiles, this is a little hackish
 +if [ `uname -s` = "NetBSD" ]; then
