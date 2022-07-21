b160ed38336072f8ef6a7f2be25f9b55fb9b43f4
adam <adam@pkgsrc.org>
2022-07-21 09:35:19 +0000

    samba4: updated to 4.16.3
    Changes since 4.16.2
    --------------------
    * BUG 15099: Using vfs_streams_xattr and deleting a file causes a panic.
    * BUG 14986: Add support for bind 9.18.
    * BUG 15076: logging dsdb audit to specific files does not work.
    * BUG 14979: Problem when winbind renews Kerberos.
    * BUG 15095: Samba with new lorikeet-heimdal fails to build on gcc 12.1 in
      developer mode.
    * BUG 15105: Crash in streams_xattr because fsp->base_fsp->fsp_name is NULL.
    * BUG 15118: Crash in rpcd_classic - NULL pointer deference in
      mangle_is_mangled().
    * BUG 15100: smbclient commands del & deltree fail with
      NT_STATUS_OBJECT_PATH_NOT_FOUND with DFS.
    * BUG 15120: Fix check for chown when processing NFSv4 ACL.
    * BUG 15082: The pcap background queue process should not be stopped.
    * BUG 15097: testparm: Fix typo in idmap rangesize check.
    * BUG 15106: net ads info returns LDAP server and LDAP server name as null.
    * BUG 15108: ldconfig: /lib64/libsmbconf.so.0 is not a symbolic link.
    * BUG 15090: CTDB child process logging does not work as expected.
    
    Changes since 4.16.1
    --------------------
    * BUG 15042: Use pathref fd instead of io fd in vfs_default_durable_cookie.
    * BUG 15069: vfs_gpfs with vfs_shadowcopy2 fail to restore file if original
      file had been deleted.
    * BUG 15087: netgroups support removed.
    * BUG 14674: net ads info shows LDAP Server: 0.0.0.0 depending on contacted
      server.
    * BUG 15062: Update from 4.15  to 4.16 breaks discovery of [homes] on
      standalone server from Win and IOS.
    * BUG 15071: waf produces incorrect names for python extensions with Python
      3.11.
    * BUG 15075: smbclient -E doesn't work as advertised.
    * BUG 15071: waf produces incorrect names for python extensions with Python
      3.11.
    * BUG 15081: The samba background daemon doesn't refresh the printcap cache
      on startup.
    * BUG 14443: Out-by-4 error in smbd read reply max_send clamp..
    
    Changes since 4.16.0
    --------------------
    * BUG 14831: Share and server swapped in smbget password prompt.
    * BUG 15022: Durable handles won't reconnect if the leased file is written
      to.
    * BUG 15023: rmdir silently fails if directory contains unreadable files and
      hide unreadable is yes.
    * BUG 15038: SMB2_CLOSE_FLAGS_FULL_INFORMATION fails to return information on
      renamed file handle.
    * BUG 8731: Need to describe --builtin-libraries= better (compare with
     --bundled-libraries).
    * BUG 14957: vfs_shadow_copy2 breaks "smbd async dosmode" sync fallback.
    * BUG 15035: shadow_copy2 fails listing snapshotted dirs with
      shadow:fixinodes.
    * BUG 15046: PAM Kerberos authentication incorrectly fails with a clock skew
      error.
    * BUG 15041: Username map - samba erroneously applies unix group memberships
      to user account entries.
    * BUG 14951: KVNO off by 100000.
    * BUG 15027: Uninitialized litemask in variable in vfs_gpfs module.
    * BUG 15055: vfs_gpfs recalls=no option prevents listing files.
    * BUG 15054: smbd doesn't handle UPNs for looking up names.

--- net/samba4/patches/patch-source3_modules_vfs__solarisacl.c
+++ net/samba4/patches/patch-source3_modules_vfs__solarisacl.c
@@ -1,18 +1,9 @@
-$NetBSD: patch-source3_modules_vfs__solarisacl.c,v 1.1 2022/04/25 16:25:02 jperkin Exp $
+$NetBSD: patch-source3_modules_vfs__solarisacl.c,v 1.2 2022/07/21 09:35:19 adam Exp $
 
 https://bugzilla.samba.org/attachment.cgi?id=16710
 
 --- source3/modules/vfs_solarisacl.c.orig	2021-08-09 13:38:36.435381200 +0000
 +++ source3/modules/vfs_solarisacl.c
-@@ -204,7 +204,7 @@ int solarisacl_sys_acl_set_fd(vfs_handle
- 	DEBUG(10, ("solarisacl_sys_acl_set_fd %s.\n",
- 		   ((ret == 0) ? "succeeded" : "failed" )));
- 	SAFE_FREE(solaris_acl);
--	SAFE_FREE(default_acl);
-+	SAFE_FREE(other_acl);
- 	return ret;
- }
- 
 @@ -252,7 +252,7 @@ int solarisacl_sys_acl_delete_def_fd(vfs
  	}
  	ret = acl(fsp->fsp_name->base_name, SETACL, count, solaris_acl);
