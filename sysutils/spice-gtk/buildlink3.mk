300a1affdd8583be4ceaa1ed186c7b1d4011613a
wiz <wiz@pkgsrc.org>
2022-07-21 10:19:32 +0000

    spice-gtk: add missing include of gobject-introspection to bl3.mk

--- sysutils/spice-gtk/buildlink3.mk
+++ sysutils/spice-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/07/11 07:52:56 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/07/21 10:19:32 wiz Exp $
 
 BUILDLINK_TREE+=	spice-gtk
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.spice-gtk+=	spice-gtk>=0.39
 BUILDLINK_PKGSRCDIR.spice-gtk?=		../../sysutils/spice-gtk
 
 .include "../../audio/libopus/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../multimedia/gst-plugins1-base/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
