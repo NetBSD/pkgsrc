fd93931bd6d327d18ad202096808a38605a54648
wiz <wiz@pkgsrc.org>
2022-07-22 14:52:24 +0000

    graphviz: revert buildlink3.mk changes for now

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.29 2022/07/22 12:04:46 micha Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2022/07/22 14:52:24 wiz Exp $
 
 BUILDLINK_TREE+=	graphviz
 
 .if !defined(GRAPHVIZ_BUILDLINK3_MK)
 GRAPHVIZ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=5.0.0
+BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=2.26.3                                                                                                                                                 
+BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.50.0nb6                                                                                                                                              
 BUILDLINK_PKGSRCDIR.graphviz?=		../../graphics/graphviz
-
-DEPENDS+=	urw-fonts-[0-9]*:../../fonts/urw-fonts
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
