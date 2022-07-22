fe6622dda9e3c6c3c3c3099b996360637674a82f
micha <micha@pkgsrc.org>
2022-07-22 17:07:10 +0000

    *: recursive bump for graphics/graphviz 5.0.0

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.30 2022/07/22 14:52:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2022/07/22 17:07:10 micha Exp $
 
 BUILDLINK_TREE+=	graphviz
 
 .if !defined(GRAPHVIZ_BUILDLINK3_MK)
 GRAPHVIZ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=2.26.3                                                                                                                                                 
-BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.50.0nb6                                                                                                                                              
+BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=2.26.3
+BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=5.0.0nb1
 BUILDLINK_PKGSRCDIR.graphviz?=		../../graphics/graphviz
 
 .include "../../converters/libiconv/buildlink3.mk"
--- x11/libdesktop-agnostic/buildlink3.mk
+++ x11/libdesktop-agnostic/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.45 2022/04/18 19:10:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.46 2022/07/22 17:07:10 micha Exp $
 
 BUILDLINK_TREE+=	libdesktop-agnostic
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdesktop-agnostic
 LIBDESKTOP_AGNOSTIC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92
-BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92nb48
+BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92nb50
 BUILDLINK_PKGSRCDIR.libdesktop-agnostic?=	../../x11/libdesktop-agnostic
 
 .include "../../lang/vala/buildlink3.mk"
