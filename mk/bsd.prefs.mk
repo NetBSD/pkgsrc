3d8a6bbe5cd5be43235683d635363999297b7aa3
nia <nia@pkgsrc.org>
2022-07-22 09:27:01 +0000

    mk: Also use /etc/release for OS_VERSION on NetBSD.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.prefs.mk,v 1.423 2022/07/22 09:14:43 nia Exp $
+# $NetBSD: bsd.prefs.mk,v 1.424 2022/07/22 09:27:01 nia Exp $
 #
 # This file includes the mk.conf file, which contains the user settings.
 #
@@ -101,7 +101,11 @@ OS_VARIANT?=		# empty
 # without executing the commands at all.  Later, recursed make
 # invocations will skip these blocks entirely thanks to MAKEFLAGS.
 .if !defined(OS_VERSION)
+.  if ${OPSYS} == "NetBSD" && exists(/etc/release)
+_OS_VERSION_CMD=	head -1 /etc/release | sed -e "s,^NetBSD ,,g" -e "s,/.*$,,g"
+.  else
 _OS_VERSION_CMD=	${UNAME} -r
+.  endif
 OS_VERSION=		${_OS_VERSION_CMD:sh}
 MAKEFLAGS+=		OS_VERSION=${OS_VERSION:Q}
 .endif
