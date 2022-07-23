aa8a9fa9bb0dd3bb2974865da7294d11bafdc038
wiz <wiz@pkgsrc.org>
2022-07-23 06:40:08 +0000

    gimp-devel: make PKGNAME match directory name
    Requested by David H. Gutteridge

--- graphics/gimp-devel/buildlink3.mk
+++ graphics/gimp-devel/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/07/22 20:18:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/07/23 06:40:08 wiz Exp $
 
-BUILDLINK_TREE+=	gimp
+BUILDLINK_TREE+=	gimp-devel
 
-.if !defined(GIMP_BUILDLINK3_MK)
-GIMP_BUILDLINK3_MK:=
+.if !defined(GIMP_DEVEL_BUILDLINK3_MK)
+GIMP_DEVEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gimp+=	gimp>=2.99
-BUILDLINK_PKGSRCDIR.gimp?=	../../graphics/gimp-devel
+BUILDLINK_API_DEPENDS.gimp-devel+=	gimp-devel>=2.99.10
+BUILDLINK_PKGSRCDIR.gimp-devel?=	../../graphics/gimp-devel
 
 .include "../../graphics/gegl/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
-.endif # GIMP_BUILDLINK3_MK
+.endif	# GIMP_DEVEL_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-gimp
+BUILDLINK_TREE+=	-gimp-devel
