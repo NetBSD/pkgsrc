82cb59cb5407857e047312df003615ae8819a613
wiz <wiz@pkgsrc.org>
2022-07-25 11:04:45 +0000

    adaptagrams: add dependency on cairomm, bump PKGREVISION

--- graphics/adaptagrams/buildlink3.mk
+++ graphics/adaptagrams/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/09/18 23:36:00 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/07/25 11:04:45 wiz Exp $
 
 BUILDLINK_TREE+=	adaptagrams
 
 .if !defined(ADAPTAGRAMS_BUILDLINK3_MK)
 ADAPTAGRAMS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.adaptagrams+=	adaptagrams>=0.20160901
+BUILDLINK_API_DEPENDS.adaptagrams+=	adaptagrams>=0.20160901nb1
 BUILDLINK_PKGSRCDIR.adaptagrams?=	../../graphics/adaptagrams
+
+.include "../../graphics/cairomm/buildlink3.mk"
 .endif	# ADAPTAGRAMS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-adaptagrams
