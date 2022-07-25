1626eaf25cdd112838fd35aacbd3979198f6e8b1
tnn <tnn@pkgsrc.org>
2022-07-25 13:57:01 +0000

    firefox: --disable-necko-wifi is mandated with --disable-dbus
    At least on platforms where the geolocation API is implemented (i.e. Linux)

--- www/firefox/options.mk
+++ www/firefox/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.69 2022/07/24 16:33:09 rjs Exp $
+# $NetBSD: options.mk,v 1.70 2022/07/25 13:57:01 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox
 
@@ -63,6 +63,7 @@ AUDIO_BACKENDS+=	pulseaudio
 CONFIGURE_ARGS+=	--enable-dbus
 .else
 CONFIGURE_ARGS+=	--disable-dbus
+CONFIGURE_ARGS+=	--disable-necko-wifi
 .endif
 
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
