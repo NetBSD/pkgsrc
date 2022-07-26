3e2fc896df321cf292dcf25ab9a43a36cf03e181
spz <spz@pkgsrc.org>
2022-07-26 19:08:02 +0000

    Pullup ticket #6649 - requested by khorben chat/prosody: integration fix
    Revisions pulled up:
    - chat/prosody/Makefile                                         1.38-1.40
    - chat/prosody/files/prosody.sh                                 1.3-1.5
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   khorben
       Date:           Thu Jul  7 01:53:00 UTC 2022
    
       Modified Files:
               pkgsrc/chat/prosody: Makefile
               pkgsrc/chat/prosody/files: prosody.sh
    
       Log Message:
       prosody: fix the path to the PID file in the RC script
    
       PROSODY_RUN is set to eg /var/run/prosody/prosody.pid instead of just
       /var/run/prosody.pid, which is a good thing (tm) since prosody's user
       needs the access rights to write to the corresponding directory.
    
       Unfortunately, the directory is not automatically created nor the right
       permissions set yet, but this is progress.
    
       While there, appease pkglint(1).
    
       Bumps PKGREVISION.
    
       Tested on NetBSD/amd64.
    
       XXX pull-up to pkgsrc-2022Q2 once the complete solution is in place
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.37 -r1.38 pkgsrc/chat/prosody/Makefile
       cvs rdiff -u -r1.2 -r1.3 pkgsrc/chat/prosody/files/prosody.sh
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   khorben
       Date:           Thu Jul  7 02:31:46 UTC 2022
    
       Modified Files:
               pkgsrc/chat/prosody: Makefile
               pkgsrc/chat/prosody/files: prosody.sh
    
       Log Message:
       prosody: make sure pidfile always matches PROSODY_RUN in the RC script
    
       This concludes my investigation on the correct path for the PID file.
       No changes to the final binary if PROSODY_RUN is set to its default
       value.
    
       Tested on NetBSD/amd64.
    
       XXX pull-up to pkgsrc-2022Q2
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.38 -r1.39 pkgsrc/chat/prosody/Makefile
       cvs rdiff -u -r1.3 -r1.4 pkgsrc/chat/prosody/files/prosody.sh
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   khorben
       Date:           Mon Jul 25 04:47:07 UTC 2022
    
       Modified Files:
               pkgsrc/chat/prosody: Makefile
               pkgsrc/chat/prosody/files: prosody.sh
    
       Log Message:
       chat/prosody: always create the directory for the PID file
    
       The RC script for prosody now always creates the corresponding
       sub-directory for prosody's PID file. This is inspired by the RC script
       for mdnsd in NetBSD, and for dbus in pkgsrc; thanks spz@ for the
       suggestion!
    
       Bumps PKGREVISION.
    
       Tested on NetBSD/amd64.
    
       XXX pull-up to pkgsrc-2022Q2 (completes request 6649)
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.39 -r1.40 pkgsrc/chat/prosody/Makefile
       cvs rdiff -u -r1.4 -r1.5 pkgsrc/chat/prosody/files/prosody.sh

--- chat/prosody/files/prosody.sh
+++ chat/prosody/files/prosody.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: prosody.sh,v 1.2 2021/08/29 18:32:29 khorben Exp $
+# $NetBSD: prosody.sh,v 1.2.8.1 2022/07/26 19:08:02 spz Exp $
 #
 # PROVIDE: prosody
 # REQUIRE: DAEMON
@@ -13,9 +13,18 @@ name="prosody"
 rcvar=${name}
 ctl_command="@PREFIX@/bin/${name}ctl"
 required_files="@PKG_SYSCONFDIR@/${name}.cfg.lua"
-pidfile="@VARBASE@/run/${name}.pid"
-#start_precmd="ulimit -n 2048"
+pidfile="@PROSODY_RUN@/${name}.pid"
+start_precmd="prosody_precmd"
 extra_commands="reload status"
+
+prosody_precmd()
+{
+    if [ ! -d @PROSODY_RUN@ ]; then
+        @MKDIR@ -m 0755 @PROSODY_RUN@
+    fi
+    @CHOWN@ @PROSODY_USER@:@PROSODY_GROUP@ @PROSODY_RUN@
+    #ulimit -n 2048
+}
 
 start_cmd="${ctl_command} start"
 stop_cmd="${ctl_command} stop"
