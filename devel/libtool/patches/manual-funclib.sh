494685258a28ef2c97a794c8f8884ee3c464e785
wiz <wiz@pkgsrc.org>
2022-07-26 16:15:26 +0000

    libtool*: update to 2.4.7
    * Noteworthy changes in release 2.4.7 (2022-03-16) [stable]
    
    ** New features:
    
      - Libtool script now supports (configure-time and runtime) ARFLAGS
        variable, which obsoletes AR_FLAGS.  This is due to naming conventions
        among other *FLAGS and to be consistent with Automake's ARFLAGS.
    
      - Gnulib testsuite is enabled and run during 'make check'.
    
      - Support the Windows version of the Intel C Compiler (icl) in
        libtool script.
    
      - Pass '-fsanitize=*' flags for GCC and LLVM, and '-specs=*' for GCC
        to linker.
    
      - Pass '-Xassembler=*' and '-Wa,*' flag to compilers and linkers.
    
      - The variable 'FILECMD' with default value of '/usr/bin/file' was used to
        replace existing hard coded references to '/usr/bin/file'.
    
      - Add MidnightBSD support.
    
    ** Important incompatible changes:
    
      - Libtool changed ARFLAGS/AR_FLAGS default from 'cru' to 'cr'.
    
      - Do not pass '-pthread' to Solaris linker.
    
      - 'libtool' and 'libtoolize' scripts now use '#! /usr/bin/env sh' shebang.
        Previously '#! /bin/sh' was used, which presents challenges for
        containerized environments.
    
    ** Bug fixes:
    
      - Fix significant slowdown of libtoolize for certain projects (regression
        introduced in 2.4.3 release) caused by infinite m4 macro recursion.
    
      - Mitigate the slowdown of libtool script (introduced in v2.4.3) caused by
        increased number of calls to '$SED $sed_quote_subst' (bug#20006).
    
      - Properly parse and export TLS symbols on AIX.
    
      - Various bug fixes surrounding use of 'sed'.
    
      - Darwin systems set proper "allow undefined" flag on OSX 11, and
        PowerPC 10.5.
    
      - Removed some deprecated tests related to 'Makefile.inc' files.

--- devel/libtool/patches/manual-funclib.sh
+++ devel/libtool/patches/manual-funclib.sh
@@ -1,10 +1,10 @@
-$NetBSD: manual-funclib.sh,v 1.1 2017/03/06 16:14:07 jperkin Exp $
+$NetBSD: manual-funclib.sh,v 1.2 2022/07/26 16:15:26 wiz Exp $
 
 Avoid pkgsrc variables which are not suitable here.
 
---- build-aux/funclib.sh.orig	2015-01-20 19:18:37.000000000 +0000
+--- build-aux/funclib.sh.orig	2022-03-17 02:44:32.000000000 +0000
 +++ build-aux/funclib.sh
-@@ -244,6 +244,9 @@ test -z "$GREP" && {
+@@ -258,6 +258,9 @@ test -z "$GREP" && {
  # uses them if a suitable command of that name is not already available
  # in the command search PATH.
  
--- devel/libtool/patches/manual-ltdl.m4
+++ devel/libtool/patches/manual-ltdl.m4
@@ -1,10 +1,10 @@
-$NetBSD: manual-ltdl.m4,v 1.7 2018/11/13 21:44:42 sevan Exp $
+$NetBSD: manual-ltdl.m4,v 1.8 2022/07/26 16:15:26 wiz Exp $
 
 Support mirbsd & minix
 
---- m4/ltdl.m4.orig	Fri Jan 16 18:52:04 2015
+--- m4/ltdl.m4.orig	2022-03-17 02:43:39.000000000 +0000
 +++ m4/ltdl.m4
-@@ -496,7 +496,10 @@ AC_CACHE_CHECK([whether deplibs are loaded by dlopen],
+@@ -497,7 +497,10 @@ AC_CACHE_CHECK([whether deplibs are load
      # at 6.2 and later dlopen does load deplibs.
      lt_cv_sys_dlopen_deplibs=yes
      ;;
