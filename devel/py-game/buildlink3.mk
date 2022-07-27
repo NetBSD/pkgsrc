56d2ae79e464e9e6a237d1a619aa92b671d92964
wiz <wiz@pkgsrc.org>
2022-07-27 19:05:58 +0000

    py-game: fix png library name.
    Some pkglint cleanup while here.
    
    Bump PKGREVISION.

--- devel/py-game/buildlink3.mk
+++ devel/py-game/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.30 2017/06/20 22:37:53 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2022/07/27 19:05:58 wiz Exp $
 
-BUILDLINK_TREE+=	${PYPKGPREFIX}-game
+BUILDLINK_TREE+=	py-game
 
 .if !defined(PY_GAME_BUILDLINK3_MK)
 PY_GAME_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.9.1
-BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.9.1nb2
-BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../devel/py-game
+BUILDLINK_API_DEPENDS.py-game+=	${PYPKGPREFIX}-game>=1.9.1
+BUILDLINK_ABI_DEPENDS.py-game+=	${PYPKGPREFIX}-game>=1.9.1nb2
+BUILDLINK_PKGSRCDIR.py-game?=	../../devel/py-game
 
 .include "../../audio/SDL_mixer/buildlink3.mk"
 .include "../../devel/SDL/buildlink3.mk"
@@ -22,4 +22,4 @@ BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../devel/py-game
 .endif
 .endif # PY_GAME_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-${PYPKGPREFIX}-game
+BUILDLINK_TREE+=	-py-game
