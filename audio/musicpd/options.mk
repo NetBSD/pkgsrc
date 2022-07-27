2884d94ccadbcc0c05f4d6fbaba0b8579ebb4575
wiz <wiz@pkgsrc.org>
2022-07-27 09:42:45 +0000

    musicpd: switch back to ffmpeg5, using an upstream patch.
    Bump PKGREVISION

--- audio/musicpd/options.mk
+++ audio/musicpd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.43 2022/07/26 07:38:42 wiz Exp $
+# $NetBSD: options.mk,v 1.44 2022/07/27 09:42:45 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.musicpd
 
@@ -83,10 +83,7 @@ MESON_ARGS+=	-Dfaad=disabled
 .endif
 
 .if !empty(PKG_OPTIONS:Mffmpeg)
-# needs 5.1 support
-# https://github.com/MusicPlayerDaemon/MPD/issues/1582
-#.  include "../../multimedia/ffmpeg5/buildlink3.mk"
-.  include "../../multimedia/ffmpeg4/buildlink3.mk"
+.  include "../../multimedia/ffmpeg5/buildlink3.mk"
 .else
 MESON_ARGS+=	-Dffmpeg=disabled
 .endif
