218cdff2d1230b2a0ba92465d5c0a32a35ca21b3
dbj <dbj@pkgsrc.org>
2022-07-28 05:25:22 +0000

    x11/wxGTK32: for cmake builds, install binaries into libexec/wx-3.2
    this avoids conflicts with wxGTK28 and wxGTK30
    take upstream patches for DESTDIR support
    also respect CMAKE_INSTALL_BINDIR instead of assuming "bin"

---
+++ x11/wxGTK32/patches/patch-build_cmake_install.cmake
@@ -0,0 +1,34 @@
+$NetBSD: patch-build_cmake_install.cmake,v 1.1 2022/07/28 05:25:22 dbj Exp $
+
+Fix use of DESTDIR
+https://github.com/wxWidgets/wxWidgets/issues/22610
+
+Respect ${CMAKE_INSTALL_BINDIR} instead of assuming bin
+
+--- build/cmake/install.cmake.orig	2022-07-28 04:31:27.000000000 +0000
++++ build/cmake/install.cmake
+@@ -39,11 +39,11 @@ else()
+                     WORLD_EXECUTE WORLD_READ
+         )
+ 
+-    install(DIRECTORY DESTINATION "bin")
++    install(DIRECTORY DESTINATION "${CMAKE_INSTALL_BINDIR}")
+     install(CODE "execute_process( \
+         COMMAND ${CMAKE_COMMAND} -E create_symlink \
+         ${CMAKE_INSTALL_PREFIX}/lib/wx/config/${wxBUILD_FILE_ID} \
+-        ${CMAKE_INSTALL_PREFIX}/bin/wx-config \
++        \"\$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/wx-config\" \
+         )"
+     )
+ endif()
+@@ -96,8 +96,8 @@ if(NOT TARGET ${UNINST_NAME})
+         endif()
+ 
+         set(WX_EXTRA_UNINSTALL_FILES
+-            "${CMAKE_INSTALL_PREFIX}/bin/wx-config"
+-            "${CMAKE_INSTALL_PREFIX}/bin/wxrc${EXE_SUFFIX}"
++            "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/wx-config"
++            "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/wxrc${EXE_SUFFIX}"
+         )
+     endif()
+ 
