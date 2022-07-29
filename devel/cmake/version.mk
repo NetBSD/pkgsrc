bba40060ff20e4d3e27c091510522e981bdb8f20
adam <adam@pkgsrc.org>
2022-07-29 10:40:21 +0000

    cmake: updated to 3.23.3
    CMake 3.23.3
    * cmake-gui: Restore support for internationalization with Qt5 on Windows
    * Tutorial: Simplify logic checking for cmath functions
    * ci: update to use ninja 1.11.0
    * Xcode: Use ad-hoc signing during compiler id on macOS
    * Xcode: Suppress "Run Script" build phase warning during compiler id
    * Tests: Teach RunCMake to ignore Xcode DVTSDK warnings
    * ExternalProject: Fix regression in stamp creation for Xcode+iOS
    * Utilities/Release: Update macOS notarization script to use notarytool
    * gitlab-ci: update macOS jobs to use Xcode 13.4
    * FindLAPACK: Add '-fortranlibs' flag only with NVHPC/PGI compilers
    * LCC: link with -lgfortran instead of -llfortran since 1.26.03
    * install(EXPORT): Check for missing file sets at generate time
    * cmGlobalGenerator: Only compute build files for all targets
    * cmake: simplify to a single source of truth of working mode
    * cmake: In -P mode ignore extra paths on the command line

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.27 2022/05/26 08:53:59 adam Exp $
+# $NetBSD: version.mk,v 1.28 2022/07/29 10:40:21 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.23.2
+CMAKE_VERSION=	3.23.3
 CMAKE_API=	${CMAKE_VERSION:R}
