1dea1e0f613a2e42fbcf54f36dfe08b8bd54b174
wiz <wiz@pkgsrc.org>
2022-07-29 16:34:58 +0000

    devel/libadwaita: import libadwaita-1.0.5
    Libadwaita is a GTK 4 library implementing the GNOME HIG, complementing
    GTK. For GTK 3 this role has increasingly been played by Libhandy,
    and so Libadwaita is a direct Libhandy successor.

---
+++ devel/libadwaita/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/07/29 16:34:58 wiz Exp $
+
+BUILDLINK_TREE+=	libadwaita
+
+.if !defined(LIBADWAITA_BUILDLINK3_MK)
+LIBADWAITA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libadwaita+=	libadwaita>=1.0.5
+BUILDLINK_PKGSRCDIR.libadwaita?=	../../devel/libadwaita
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk4/buildlink3.mk"
+.endif	# LIBADWAITA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libadwaita
