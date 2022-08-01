ca8a323254059514ca702353518f237378088e3c
ast <ast@pkgsrc.org>
2022-08-01 15:00:13 +0000

    multimedia/handbrake: update from 1.3.3 to 1.5.1; fix build when python>3.8

--- multimedia/handbrake/options.mk
+++ multimedia/handbrake/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2022/07/03 21:38:48 dbj Exp $
+# $NetBSD: options.mk,v 1.2 2022/08/01 15:00:13 ast Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.handbrake
 PKG_SUPPORTED_OPTIONS=	gtk3
@@ -7,11 +7,11 @@ PKG_SUGGESTED_OPTIONS=	gtk3
 
 .include "../../mk/bsd.options.mk"
 
-.if empty(PKG_OPTIONS:Mgtk3)
-CONFIGURE_ARGS+=	--disable-gtk
-.else
+.if !empty(PKG_OPTIONS:Mgtk3)
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .include "../../sysutils/desktop-file-utils/desktopdb.mk"
 PLIST_SRC+=	${PKGDIR}/PLIST.gtk3
+.else
+CONFIGURE_ARGS+=	--disable-gtk
 .endif
