ae8caf1ebb8167ac1ad369ed551eae46d26c0311
pin <pin@pkgsrc.org>
2022-08-01 08:07:13 +0000

    sysutils/dust: update to 0.8.1
    -Improve help text
    -Fix archive/directory check in platform.rs
    -Bump regex
    -Refactor: tweak utils function
    -Fix: Allow -n to be used with -d

--- sysutils/dust/cargo-depends.mk
+++ sysutils/dust/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.5 2022/02/27 20:33:27 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.6 2022/08/01 08:07:13 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	aho-corasick-0.7.18
 CARGO_CRATE_DEPENDS+=	ansi_term-0.12.1
@@ -35,7 +35,7 @@ CARGO_CRATE_DEPENDS+=	predicates-tree-1.0.5
 CARGO_CRATE_DEPENDS+=	rayon-1.5.1
 CARGO_CRATE_DEPENDS+=	rayon-core-1.9.1
 CARGO_CRATE_DEPENDS+=	redox_syscall-0.2.10
-CARGO_CRATE_DEPENDS+=	regex-1.5.4
+CARGO_CRATE_DEPENDS+=	regex-1.5.5
 CARGO_CRATE_DEPENDS+=	regex-automata-0.1.10
 CARGO_CRATE_DEPENDS+=	regex-syntax-0.6.25
 CARGO_CRATE_DEPENDS+=	remove_dir_all-0.5.3
