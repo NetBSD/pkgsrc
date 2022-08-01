49f98c002c05811c128c3cc25102acd4c72ff67c
wiz <wiz@pkgsrc.org>
2022-08-01 14:51:15 +0000

    py-Pillow: improve zlib detection workaround
    It started failing for me, I don't understand why - improve the existing
    workaround.

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,15 +1,17 @@
-$NetBSD: patch-setup.py,v 1.23 2021/03/10 18:43:30 adam Exp $
+$NetBSD: patch-setup.py,v 1.24 2022/08/01 14:51:15 wiz Exp $
 
 Fix finding zlib.h.
 
---- setup.py.orig	2021-03-06 05:17:25.000000000 +0000
+--- setup.py.orig	2022-07-01 13:14:51.000000000 +0000
 +++ setup.py
-@@ -561,7 +561,7 @@ class pil_build_ext(build_ext):
+@@ -636,7 +636,9 @@ class pil_build_ext(build_ext):
  
          if feature.want("zlib"):
              _dbg("Looking for zlib")
 -            if _find_include_file(self, "zlib.h"):
-+            if True or _find_include_file(self, "zlib.h"):
++            if True:
++                feature.zlib = "z"
++            elif _find_include_file(self, "zlib.h"):
                  if _find_library_file(self, "z"):
                      feature.zlib = "z"
                  elif sys.platform == "win32" and _find_library_file(self, "zlib"):
