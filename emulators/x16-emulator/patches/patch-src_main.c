6af41dd06b058e59be0b4c27d1578e57a13e49a9
thorpej <thorpej@pkgsrc.org>
2022-08-07 17:38:08 +0000

    Update to x16-emulator r41.

--- emulators/x16-emulator/patches/patch-src_main.c
+++ emulators/x16-emulator/patches/patch-src_main.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-main.c,v 1.1 2019/12/28 19:32:18 thorpej Exp $
+$NetBSD: patch-src_main.c,v 1.1 2022/08/07 17:38:08 thorpej Exp $
 
 Allow the default location of the X16 ROM to be overridden.
 
---- main.c.orig	2019-12-28 19:00:35.000000000 +0000
-+++ main.c
-@@ -436,9 +436,13 @@ main(int argc, char **argv)
+--- src/main.c.orig	2022-08-07 02:13:20.524132103 +0000
++++ src/main.c	2022-08-07 02:17:36.561192332 +0000
+@@ -491,9 +491,13 @@ main(int argc, char **argv)
  
  	char *base_path = SDL_GetBasePath();
  
 -	// This causes the emulator to load ROM data from the executable's directory when
 -	// no ROM file is specified on the command line.
 +	// This causes the emulator to load ROM data from the executable's
-+	// directory (or somewhere nearby if ROM_RELATIVE_PATH is specified)
++	// directory ((or somewhere nearby if ROM_RELATIVE_PATH is specified)
 +	// when no ROM file is specified on the command line.
  	memcpy(rom_path, base_path, strlen(base_path) + 1);
 +#ifdef ROM_RELATIVE_PATH
