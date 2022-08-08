ca76614e67281d31c0e23b2dd95d124cb37770bd
fox <fox@pkgsrc.org>
2022-08-08 09:28:06 +0000

    sysutils/py-Glances: Update to 3.2.7
    Changes since 3.2.6.4:
    
    ===============
    Version 3.2.7
    ===============
    
    Enhancements:
    
        * Config to disable all plugins by default (or enable an exclusive list) #2089
        * Keybind(s) for modifying nice level #2081
        * [WEBUI] Reorganize help screen #2037
        * Add a Json stdout option #2060
        * Improve error message when export error occures
        * Improve error message when MQTT error occures
        * Change the way core are displayed
        * Remove unused key in the process list
        * Refactor top menu of the curse interface
        * Improve Irix display for the load plugin
    
    Bug corrected:
    
        * In the sensor plugin thresholds in the configuration file should overwrite system ones #2058
        * Drive names truncated in Web UI #2055
        * Correct issue with CPU label
    
    Documentation and CI:
    
        * Improve makefile help #2078
        * Add quote to the update command line (already ok for the installation). Related to #2073
        * Make Glances (almost) compliant with REUSE #2042
        * Update README for Debian package users
        * Update documentation for Docker
        * Update docs for new shortcut
        * Disable Pyright on the Git actions pipeline
        * Refactor comments
        * Except datutil import error
        * Another dep issue solved in the Alpine Docker + issue in the outdated method
    
    Contributors for this version:
    
        * Nicolargo
        * Sylvain MOUQUET
        * FastThenLeft
        * Jiajie Chen
        * dbrennand
        * ewuerger

--- sysutils/py-Glances/patches/patch-glances_plugins_glances__cpu.py
+++ sysutils/py-Glances/patches/patch-glances_plugins_glances__cpu.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-glances_plugins_glances__cpu.py,v 1.1 2021/12/05 11:46:40 fox Exp $
+$NetBSD: patch-glances_plugins_glances__cpu.py,v 1.2 2022/08/08 09:28:06 fox Exp $
 
 Disable system call counts in BSD.
---- glances/plugins/glances_cpu.py.orig	2021-11-20 09:22:55.000000000 +0000
+--- glances/plugins/glances_cpu.py.orig	2022-07-28 16:12:29.000000000 +0000
 +++ glances/plugins/glances_cpu.py
-@@ -23,7 +23,7 @@ from glances.logger import logger
+@@ -13,7 +13,7 @@ from glances.logger import logger
  from glances.timer import getTimeSinceLastUpdate
  from glances.compat import iterkeys
  from glances.cpu_percent import cpu_percent
@@ -12,9 +12,9 @@ Disable system call counts in BSD.
  from glances.plugins.glances_core import Plugin as CorePlugin
  from glances.plugins.glances_plugin import GlancesPlugin
  
-@@ -347,7 +347,7 @@ class Plugin(GlancesPlugin):
+@@ -366,7 +366,7 @@ class Plugin(GlancesPlugin):
          # Steal CPU usage
-         ret.extend(self.curse_add_stat('steal', width=15, header='  '))
+         ret.extend(self.curse_add_stat('steal', width=14, header='  '))
          # syscalls: number of system calls since boot. Always set to 0 on Linux. (do not display)
 -        if not LINUX:
 +        if not LINUX and not BSD:
--- sysutils/py-Glances/patches/patch-glances_plugins_glances__quicklook.py
+++ sysutils/py-Glances/patches/patch-glances_plugins_glances__quicklook.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-glances_plugins_glances__quicklook.py,v 1.2 2022/05/07 08:06:51 fox Exp $
+$NetBSD: patch-glances_plugins_glances__quicklook.py,v 1.3 2022/08/08 09:28:06 fox Exp $
 
 It seems cpu_hz_current and cpu_hz is not available in NetBSD, so add additional
 check to see if the key exists.
---- glances/plugins/glances_quicklook.py.orig	2022-04-10 14:15:33.000000000 +0000
+--- glances/plugins/glances_quicklook.py.orig	2022-07-28 16:12:29.000000000 +0000
 +++ glances/plugins/glances_quicklook.py
-@@ -74,10 +74,12 @@ class Plugin(GlancesPlugin):
+@@ -64,10 +64,12 @@ class Plugin(GlancesPlugin):
              # Get additional information
              cpu_info = cpu_percent.get_info()
              stats['cpu_name'] = cpu_info['cpu_name']
