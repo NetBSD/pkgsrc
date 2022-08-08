cd7073d0e66b2077eea36630941fc3b27f65ac5b
nia <nia@pkgsrc.org>
2022-08-08 13:53:57 +0000

    xf86-input-ws: stable releases of NetBSD lack WSCONS_EVENT_HSCROLL and WSCONS_EVENT_VSCROLL (PR pkg/56958)

--- x11/xf86-input-ws/patches/patch-src_ws.c
+++ x11/xf86-input-ws/patches/patch-src_ws.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_ws.c,v 1.2 2022/05/27 02:35:43 tnn Exp $
+$NetBSD: patch-src_ws.c,v 1.3 2022/08/08 13:53:57 nia Exp $
 
 ws.c:407:3: warning: attempt to free a non-heap object
 Reported to upstream maintainers.
@@ -13,7 +13,7 @@ Reported to upstream maintainers.
  		return !Success;
  	}
  
-@@ -611,9 +610,11 @@ wsReadHwState(InputInfoPtr pInfo, wsHwSt
+@@ -611,17 +610,23 @@ wsReadHwState(InputInfoPtr pInfo, wsHwSt
  		case WSCONS_EVENT_MOUSE_ABSOLUTE_W:
  			/* ignore those */
  			continue;
@@ -22,10 +22,22 @@ Reported to upstream maintainers.
  			DBG(4, ErrorF("Sync\n"));
  			return TRUE;
 +#endif
++#ifdef WSCONS_EVENT_HSCROLL
  		case WSCONS_EVENT_HSCROLL:
  			hw->hscroll = event->value;
  			DBG(4, ErrorF("Horiz. Scrolling %d\n", event->value));
-@@ -876,17 +877,23 @@ wsSetCalibProperty(DeviceIntPtr device, 
+ 			return TRUE;
++#endif
++#ifdef WSCONS_EVENT_VSCROLL
+ 		case WSCONS_EVENT_VSCROLL:
+ 			hw->vscroll = event->value;
+ 			DBG(4, ErrorF("Vert. Scrolling %d\n", event->value));
+ 			return TRUE;
++#endif
+ 		default:
+ 			xf86IDrvMsg(pInfo, X_WARNING,
+ 			    "bad wsmouse event type=%d\n", event->type);
+@@ -876,17 +881,23 @@ wsSetCalibProperty(DeviceIntPtr device, 
  		priv->coords.maxx = priv->max_x;
  		priv->coords.miny = priv->min_y;
  		priv->coords.maxy = priv->max_y;
