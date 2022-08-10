4eb61e9c98ca551c950f8d74d1e90a389d398503
dbj <dbj@pkgsrc.org>
2022-08-10 05:19:57 +0000

    x11/wxGTK: have cmake include(GNUInstallDirs) when using ${CMAKE_INSTALL_BINDIR}

--- x11/wxGTK32/patches/patch-build_cmake_config.cmake
+++ x11/wxGTK32/patches/patch-build_cmake_config.cmake
@@ -1,10 +1,16 @@
-$NetBSD: patch-build_cmake_config.cmake,v 1.1 2022/07/28 05:25:22 dbj Exp $
+$NetBSD: patch-build_cmake_config.cmake,v 1.2 2022/08/10 05:19:57 dbj Exp $
 
 Respect ${CMAKE_INSTALL_BINDIR} instead of assuming bin
 
 --- build/cmake/config.cmake.orig	2022-07-06 14:19:50.000000000 +0000
 +++ build/cmake/config.cmake
-@@ -87,7 +87,7 @@ function(wx_write_config)
+@@ -83,11 +83,13 @@ endfunction()
+ 
+ function(wx_write_config)
+ 
++    include(GNUInstallDirs)
++
+     set(prefix ${CMAKE_INSTALL_PREFIX})
      set(exec_prefix "\${prefix}")
      set(includedir "\${prefix}/include")
      set(libdir "\${exec_prefix}/lib")
