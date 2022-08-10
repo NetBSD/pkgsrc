6b1bf0a52f0df3906d217ea9f7074d5871ba4151
adam <adam@pkgsrc.org>
2022-08-10 09:17:47 +0000

    py-pip: updated to 22.2.2
    22.2.2 (2022-08-03)
    ===================
    
    Bug Fixes
    - Avoid  ``AttributeError`` when removing the setuptools-provided ``_distutils_hack`` and it is missing its implementation.
    - Fix import error when reinstalling pip in user site.
    - Show pip deprecation warnings by default.

--- devel/py-pip/patches/patch-src_pip___internal_build__env.py
+++ devel/py-pip/patches/patch-src_pip___internal_build__env.py
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_pip___internal_build__env.py,v 1.3 2022/07/07 11:48:51 adam Exp $
+$NetBSD: patch-src_pip___internal_build__env.py,v 1.4 2022/08/10 09:17:48 adam Exp $
 
 Copy distutils distro config file to overlay.
 https://github.com/pypa/pip/issues/10949
 
---- src/pip/_internal/build_env.py.orig	2022-05-31 11:18:59.000000000 +0000
+--- src/pip/_internal/build_env.py.orig	2022-08-03 18:55:14.000000000 +0000
 +++ src/pip/_internal/build_env.py
-@@ -9,6 +9,7 @@ import sys
+@@ -7,6 +7,7 @@ import pathlib
+ import sys
  import textwrap
- import zipfile
  from collections import OrderedDict
 +from shutil import copy
  from sysconfig import get_paths
  from types import TracebackType
- from typing import TYPE_CHECKING, Generator, Iterable, List, Optional, Set, Tuple, Type
-@@ -93,6 +94,9 @@ class BuildEnvironment:
+ from typing import TYPE_CHECKING, Iterable, List, Optional, Set, Tuple, Type
+@@ -81,6 +82,9 @@ class BuildEnvironment:
          self._site_dir = os.path.join(temp_dir.path, "site")
          if not os.path.exists(self._site_dir):
              os.mkdir(self._site_dir)
