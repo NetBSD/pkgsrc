d03fb90bb4fd03a16ebf475463aff726171e1d62
pin <pin@pkgsrc.org>
2022-08-12 08:54:30 +0000

    devel/lldb: llvm update

--- devel/lldb/patches/patch-cmake_modules_LLDBStandalone.cmake
+++ devel/lldb/patches/patch-cmake_modules_LLDBStandalone.cmake
@@ -1,8 +1,8 @@
-$NetBSD: patch-cmake_modules_LLDBStandalone.cmake,v 1.4 2019/10/19 14:01:37 adam Exp $
+$NetBSD: patch-cmake_modules_LLDBStandalone.cmake,v 1.5 2022/08/12 08:54:31 pin Exp $
 
---- cmake/modules/LLDBStandalone.cmake.orig	2019-07-17 19:36:20.000000000 +0000
+--- cmake/modules/LLDBStandalone.cmake.orig	2022-06-22 16:46:24.000000000 +0000
 +++ cmake/modules/LLDBStandalone.cmake
-@@ -74,7 +74,6 @@ list(APPEND CMAKE_MODULE_PATH "${LLVM_DI
+@@ -85,7 +85,6 @@ list(APPEND CMAKE_MODULE_PATH "${LLVM_DI
  include(AddLLVM)
  include(TableGen)
  include(HandleLLVMOptions)
