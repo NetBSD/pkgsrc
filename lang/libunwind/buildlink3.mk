41f4d1dbb6f6dbc1c930f42395eac4d247341ad8
pin <pin@pkgsrc.org>
2022-08-12 08:39:56 +0000

    lang/libunwind: llvm update

--- lang/libunwind/buildlink3.mk
+++ lang/libunwind/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2021/11/23 21:42:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/08/12 08:39:56 pin Exp $
 
 BUILDLINK_TREE+=	libunwind
 
 .if !defined(LIBUNWIND_BUILDLINK3_MK)
 LIBUNWIND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=12.0.0
+BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=14.0.0
 BUILDLINK_PKGSRCDIR.libunwind?=		../../lang/libunwind
 
 .endif	# LIBUNWIND_BUILDLINK3_MK
