6b615075cd98096af5a6bffcd777e3bddbf3a7cd
pin <pin@pkgsrc.org>
2022-08-12 08:58:41 +0000

    lang/compiler-rt: llvm update

--- lang/compiler-rt/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp
+++ lang/compiler-rt/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp,v 1.3 2021/07/12 18:46:07 adam Exp $
+$NetBSD: patch-lib_sanitizer__common_sanitizer__platform__limits__netbsd.cpp,v 1.4 2022/08/12 08:58:41 pin Exp $
 
 urio and smb have been removed from NetBSD.
 
---- lib/sanitizer_common/sanitizer_platform_limits_netbsd.cpp.orig	2020-10-07 10:10:48.000000000 +0000
+--- lib/sanitizer_common/sanitizer_platform_limits_netbsd.cpp.orig	2022-06-22 16:46:24.000000000 +0000
 +++ lib/sanitizer_common/sanitizer_platform_limits_netbsd.cpp
-@@ -803,12 +803,14 @@ unsigned struct_session_op_sz = sizeof(s
+@@ -957,12 +957,14 @@ unsigned struct_session_op_sz = sizeof(s
  unsigned struct_sgttyb_sz = sizeof(sgttyb);
  unsigned struct_sioc_sg_req_sz = sizeof(sioc_sg_req);
  unsigned struct_sioc_vif_req_sz = sizeof(sioc_vif_req);
@@ -19,7 +19,7 @@ urio and smb have been removed from NetBSD.
  unsigned struct_spppauthcfg_sz = sizeof(spppauthcfg);
  unsigned struct_spppauthfailuresettings_sz = sizeof(spppauthfailuresettings);
  unsigned struct_spppauthfailurestats_sz = sizeof(spppauthfailurestats);
-@@ -833,7 +835,9 @@ unsigned struct_twe_drivecommand_sz = si
+@@ -987,7 +989,9 @@ unsigned struct_twe_drivecommand_sz = si
  unsigned struct_twe_paramcommand_sz = sizeof(twe_paramcommand);
  unsigned struct_twe_usercommand_sz = sizeof(twe_usercommand);
  unsigned struct_ukyopon_identify_sz = sizeof(ukyopon_identify);
@@ -29,7 +29,7 @@ urio and smb have been removed from NetBSD.
  unsigned struct_usb_alt_interface_sz = sizeof(usb_alt_interface);
  unsigned struct_usb_bulk_ra_wb_opt_sz = sizeof(usb_bulk_ra_wb_opt);
  unsigned struct_usb_config_desc_sz = sizeof(usb_config_desc);
-@@ -1422,8 +1426,10 @@ unsigned IOCTL_STICIO_RESET = STICIO_RES
+@@ -1576,8 +1580,10 @@ unsigned IOCTL_STICIO_RESET = STICIO_RES
  unsigned IOCTL_STICIO_STARTQ = STICIO_STARTQ;
  unsigned IOCTL_STICIO_STOPQ = STICIO_STOPQ;
  unsigned IOCTL_UKYOPON_IDENTIFY = UKYOPON_IDENTIFY;
@@ -40,7 +40,7 @@ urio and smb have been removed from NetBSD.
  unsigned IOCTL_USB_REQUEST = USB_REQUEST;
  unsigned IOCTL_USB_SETDEBUG = USB_SETDEBUG;
  unsigned IOCTL_USB_DISCOVER = USB_DISCOVER;
-@@ -1808,6 +1814,7 @@ unsigned IOCTL_SIOCCONNECTXDEL = SIOCCON
+@@ -1962,6 +1968,7 @@ unsigned IOCTL_SIOCCONNECTXDEL = SIOCCON
  unsigned IOCTL_SIOCSIFINFO_FLAGS = SIOCSIFINFO_FLAGS;
  unsigned IOCTL_SIOCAADDRCTL_POLICY = SIOCAADDRCTL_POLICY;
  unsigned IOCTL_SIOCDADDRCTL_POLICY = SIOCDADDRCTL_POLICY;
@@ -48,7 +48,7 @@ urio and smb have been removed from NetBSD.
  unsigned IOCTL_SMBIOC_OPENSESSION = SMBIOC_OPENSESSION;
  unsigned IOCTL_SMBIOC_OPENSHARE = SMBIOC_OPENSHARE;
  unsigned IOCTL_SMBIOC_REQUEST = SMBIOC_REQUEST;
-@@ -1815,6 +1822,7 @@ unsigned IOCTL_SMBIOC_SETFLAGS = SMBIOC_
+@@ -1969,6 +1976,7 @@ unsigned IOCTL_SMBIOC_SETFLAGS = SMBIOC_
  unsigned IOCTL_SMBIOC_LOOKUP = SMBIOC_LOOKUP;
  unsigned IOCTL_SMBIOC_READ = SMBIOC_READ;
  unsigned IOCTL_SMBIOC_WRITE = SMBIOC_WRITE;
