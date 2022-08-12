58ac064f6fe12a7bf72c8ab31a57d0e771699bec
pin <pin@pkgsrc.org>
2022-08-12 08:54:30 +0000

    devel/lldb: llvm update

--- devel/lldb/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h
+++ devel/lldb/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h,v 1.3 2020/09/23 06:58:46 adam Exp $
+$NetBSD: patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.h,v 1.4 2022/08/12 08:54:31 pin Exp $
 
 Declare StopProcess.
 
---- source/Plugins/Process/NetBSD/NativeProcessNetBSD.h.orig	2020-07-07 16:21:37.000000000 +0000
+--- source/Plugins/Process/NetBSD/NativeProcessNetBSD.h.orig	2022-06-22 16:46:24.000000000 +0000
 +++ source/Plugins/Process/NetBSD/NativeProcessNetBSD.h
-@@ -86,6 +86,7 @@ public:
+@@ -87,6 +87,7 @@ public:
    // Interface used by NativeRegisterContext-derived classes.
    static Status PtraceWrapper(int req, lldb::pid_t pid, void *addr = nullptr,
                                int data = 0, int *result = nullptr);
 +  static Status StopProcess(lldb::pid_t pid);
  
- private:
-   MainLoop::SignalHandleUP m_sigchld_handle;
+   llvm::Expected<std::string> SaveCore(llvm::StringRef path_hint) override;
+ 
