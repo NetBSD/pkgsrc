b51b3dc91f5e77dd95c223f580ed576539575f43
pin <pin@pkgsrc.org>
2022-08-12 08:41:58 +0000

    lang/libcxxabi: llvm update

--- lang/libcxxabi/buildlink3.mk
+++ lang/libcxxabi/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/07/12 18:46:07 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/08/12 08:41:58 pin Exp $
 
 BUILDLINK_TREE+=	libcxxabi
 
 .if !defined(LIBCXXABI_BUILDLINK3_MK)
 LIBCXXABI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcxxabi+=	libcxxabi>=12.0.0
+BUILDLINK_API_DEPENDS.libcxxabi+=	libcxxabi>=14.0.0
 BUILDLINK_PKGSRCDIR.libcxxabi?=		../../lang/libcxxabi
 
 .include "../../lang/llvm/buildlink3.mk"
