bd84ffa1dc8678c9bd019f14e95ecf5175eae693
adam <adam@pkgsrc.org>
2022-08-14 20:57:40 +0000

    libjpeg-turbo: updated to 2.1.4
    2.1.4
    
    Significant changes relative to 2.1.3
    
    Fixed a regression introduced in 2.1.3 that caused build failures with Visual Studio 2010.
    
    The tjDecompressHeader3() function in the TurboJPEG C API and the TJDecompressor.setSourceImage() method in the TurboJPEG Java API now accept "abbreviated table specification" (AKA "tables-only") datastreams, which can be used to prime the decompressor with quantization and Huffman tables that can be used when decompressing subsequent "abbreviated image" datastreams.
    
    libjpeg-turbo now performs run-time detection of AltiVec instructions on OS X/PowerPC systems if AltiVec instructions are not enabled at compile time. This allows both AltiVec-equipped (PowerPC G4 and G5) and non-AltiVec-equipped (PowerPC G3) CPUs to be supported using the same build of libjpeg-turbo.
    
    Fixed an error ("Bogus virtual array access") that occurred when attempting to decompress a progressive JPEG image with a height less than or equal to one iMCU (8 * the vertical sampling factor) using buffered-image mode with interblock smoothing enabled. This was a regression introduced by 2.1 beta1[6(b)].
    
    Fixed two issues that prevented partial image decompression from working properly with buffered-image mode:
    
    Attempting to call jpeg_crop_scanline() after jpeg_start_decompress() but before jpeg_start_output() resulted in an error ("Improper call to JPEG library in state 207".)
    Attempting to use jpeg_skip_scanlines() resulted in an error ("Bogus virtual array access") under certain circumstances.

--- graphics/libjpeg-turbo/patches/patch-simd_arm__aarch32_jsimd.c
+++ graphics/libjpeg-turbo/patches/patch-simd_arm__aarch32_jsimd.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-simd_arm__aarch32_jsimd.c,v 1.1 2021/04/26 08:18:48 adam Exp $
+$NetBSD: patch-simd_arm__aarch32_jsimd.c,v 1.2 2022/08/14 20:57:40 adam Exp $
 
---- simd/arm/aarch32/jsimd.c.orig	2021-04-23 16:42:40.000000000 +0000
+--- simd/arm/aarch32/jsimd.c.orig	2022-08-08 21:03:55.000000000 +0000
 +++ simd/arm/aarch32/jsimd.c
-@@ -29,6 +29,10 @@
- #include <string.h>
+@@ -27,6 +27,10 @@
+ 
  #include <ctype.h>
  
 +#if defined(__NetBSD__)
@@ -13,7 +13,7 @@ $NetBSD: patch-simd_arm__aarch32_jsimd.c,v 1.1 2021/04/26 08:18:48 adam Exp $
  static unsigned int simd_support = ~0;
  static unsigned int simd_huffman = 1;
  
-@@ -109,6 +113,9 @@ init_simd(void)
+@@ -107,6 +111,9 @@ init_simd(void)
  #endif
  #if !defined(__ARM_NEON__) && (defined(__linux__) || defined(ANDROID) || defined(__ANDROID__))
    int bufsize = 1024; /* an initial guess for the line buffer size limit */
@@ -23,7 +23,7 @@ $NetBSD: patch-simd_arm__aarch32_jsimd.c,v 1.1 2021/04/26 08:18:48 adam Exp $
  #endif
  
    if (simd_support != ~0U)
-@@ -127,6 +134,10 @@ init_simd(void)
+@@ -125,6 +132,10 @@ init_simd(void)
      if (bufsize > SOMEWHAT_SANE_PROC_CPUINFO_SIZE_LIMIT)
        break;
    }
