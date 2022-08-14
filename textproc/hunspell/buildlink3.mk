5703bbbbf78fe4f20bb28291a7435414d37b0781
tnn <tnn@pkgsrc.org>
2022-08-14 20:44:00 +0000

    hunspell: propagate iconv and intl dependencies in bl3.mk

--- textproc/hunspell/buildlink3.mk
+++ textproc/hunspell/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/06/28 11:36:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/08/14 20:44:00 tnn Exp $
 
 BUILDLINK_TREE+=	hunspell
 
@@ -8,6 +8,9 @@ HUNSPELL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hunspell+=	hunspell>=1.2.2
 BUILDLINK_ABI_DEPENDS.hunspell+=	hunspell>=1.7.0nb7
 BUILDLINK_PKGSRCDIR.hunspell?=		../../textproc/hunspell
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
 .endif # HUNSPELL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hunspell
