a3987cdf1e486e36a049181b188344da29525283
tnn <tnn@pkgsrc.org>
2022-08-15 20:39:21 +0000

    spirv-llvm-translator: update to 14.0.0 fix the build
    Changes unknown.

--- parallel/spirv-llvm-translator/buildlink3.mk
+++ parallel/spirv-llvm-translator/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/06/09 14:56:51 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/08/15 20:39:21 tnn Exp $
 
 BUILDLINK_TREE+=	spirv-llvm-translator
 
 .if !defined(SPIRV_LLVM_TRANSLATOR_BUILDLINK3_MK)
 SPIRV_LLVM_TRANSLATOR_BUILDLINK3_MK:=
 
-GCC_REQD+=	5 # C++14
+GCC_REQD+=	7 # sync with llvm
 
-BUILDLINK_API_DEPENDS.spirv-llvm-translator+=	spirv-llvm-translator>=10.0.0
+BUILDLINK_API_DEPENDS.spirv-llvm-translator+=	spirv-llvm-translator>=14.0.0
 BUILDLINK_PKGSRCDIR.spirv-llvm-translator?=	../../parallel/spirv-llvm-translator
 
 .include "../../lang/llvm/buildlink3.mk"
