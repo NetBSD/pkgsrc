9d05e39f308bcc2b94bba857c82a89f7a6ac36d9
adam <adam@pkgsrc.org>
2022-08-15 08:19:39 +0000

    meson: updated to 0.63.1
    0.63.1
    Bug fixes

--- devel/meson/patches/patch-mesonbuild_compilers_detect.py
+++ devel/meson/patches/patch-mesonbuild_compilers_detect.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-mesonbuild_compilers_detect.py,v 1.2 2022/07/13 03:57:22 triaxx Exp $
+$NetBSD: patch-mesonbuild_compilers_detect.py,v 1.3 2022/08/15 08:19:39 adam Exp $
 
 Specify c++ language when making CXX parse standard input.
 Fixes problem where graphics/MesaLib fails to build due to:
@@ -6,9 +6,9 @@ Fixes problem where graphics/MesaLib fails to build due to:
 > clang++ -std=c++14 -dM -E -
 > error: invalid argument '-std=c++14' not allowed with 'C'
 
---- mesonbuild/compilers/detect.py.orig	2021-09-10 05:30:31.000000000 +0000
+--- mesonbuild/compilers/detect.py.orig	2022-08-13 08:48:46.000000000 +0000
 +++ mesonbuild/compilers/detect.py
-@@ -1224,7 +1224,10 @@ def _get_gnu_compiler_defines(compiler: 
+@@ -1231,7 +1231,10 @@ def _get_gnu_compiler_defines(compiler: 
      """
      # Arguments to output compiler pre-processor defines to stdout
      # gcc, g++, and gfortran all support these arguments
@@ -17,10 +17,10 @@ Fixes problem where graphics/MesaLib fails to build due to:
 +    if compiler[0].endswith('++'):
 +        args += ['-x', 'c++']
 +    args += ['-']
+     mlog.debug(f'Running command: {join_args(args)}')
      p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
      if p.returncode != 0:
-         raise EnvironmentException('Unable to detect GNU compiler type:\n' + output + error)
-@@ -1248,7 +1251,10 @@ def _get_clang_compiler_defines(compiler
+@@ -1258,7 +1261,10 @@ def _get_clang_compiler_defines(compiler
      """
      Get the list of Clang pre-processor defines
      """
@@ -29,6 +29,6 @@ Fixes problem where graphics/MesaLib fails to build due to:
 +    if compiler[0].endswith('++'):
 +        args += ['-x', 'c++']
 +    args += ['-']
+     mlog.debug(f'Running command: {join_args(args)}')
      p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
      if p.returncode != 0:
-         raise EnvironmentException('Unable to get clang pre-processor defines:\n' + output + error)
