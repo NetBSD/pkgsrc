87304222be4a2a744291906badd3ff16774addd6
wiz <wiz@pkgsrc.org>
2022-08-15 19:19:16 +0000

    xine-lib: fix bl3.mk for ffmpeg5 -> ffmpeg4 change
    Depend on latest xine-lib.
    Bump all packages using it.

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2022/06/28 11:34:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2022/08/15 19:19:16 wiz Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -6,10 +6,10 @@ BUILDLINK_TREE+=	xine-lib
 XINE_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
-BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.12nb2
+BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.12nb6
 BUILDLINK_PKGSRCDIR.xine-lib?=		../../multimedia/xine-lib
 
-.include "../../multimedia/ffmpeg5/buildlink3.mk"
+.include "../../multimedia/ffmpeg4/buildlink3.mk"
 .endif # XINE_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xine-lib
