3b6f0e5091eef0c99f596050b56efe2a75594f80
gdt <gdt@pkgsrc.org>
2022-08-16 23:08:39 +0000

    net/mosquitto: Update to 2.0.15
    Upstream changes: bugfixes

--- net/mosquitto/patches/patch-lib_strings__mosq.c
+++ net/mosquitto/patches/patch-lib_strings__mosq.c
@@ -1,16 +1,17 @@
-$NetBSD: patch-lib_strings__mosq.c,v 1.1 2021/12/31 23:59:17 triaxx Exp $
+$NetBSD: patch-lib_strings__mosq.c,v 1.2 2022/08/16 23:08:39 gdt Exp $
 
 Suppress warning by including missing header.
 
---- lib/strings_mosq.c.orig	2021-11-17 00:28:35.000000000 +0000
+--- lib/strings_mosq.c.orig	2022-08-16 13:34:02.000000000 +0000
 +++ lib/strings_mosq.c
-@@ -20,6 +20,9 @@ Contributors:
- 
+@@ -21,6 +21,10 @@ Contributors:
  #include <errno.h>
  #include <string.h>
+ 
 +#if defined(__NetBSD__)
 +#include <strings.h>
 +#endif
- 
- #include "mosquitto.h"
- #include "mqtt_protocol.h"
++
+ #ifndef WIN32
+ #  include <strings.h>
+ #endif
