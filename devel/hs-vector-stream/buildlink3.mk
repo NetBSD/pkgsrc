3f4dd5671e7e5984f81e2650a686297b997e3975
wiz <wiz@pkgsrc.org>
2022-08-19 12:22:57 +0000

    devel/hs-vector-stream: import hs-vector-stream-0.1.0.0
    Simple yet powerful monadic streams that are used as a backbone
    for vector package fusion functionality.

---
+++ devel/hs-vector-stream/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/08/19 12:22:57 wiz Exp $
+
+BUILDLINK_TREE+=	hs-vector-stream
+
+.if !defined(HS_VECTOR_STREAM_BUILDLINK3_MK)
+HS_VECTOR_STREAM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-vector-stream+=	hs-vector-stream>=0.1.0.0
+BUILDLINK_PKGSRCDIR.hs-vector-stream?=		../../devel/hs-vector-stream
+.endif	# HS_VECTOR_STREAM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-vector-stream
