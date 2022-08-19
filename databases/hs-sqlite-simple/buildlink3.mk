7aece6e0c10043c6c329ca8add8d1b11da2b196d
tnn <tnn@pkgsrc.org>
2022-08-19 11:43:50 +0000

    hs-*sqlite*: propagate sqlite3 dependency in bl3.mk
    seems to be needed for devel/hs-hiedb

--- databases/hs-sqlite-simple/buildlink3.mk
+++ databases/hs-sqlite-simple/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/08/19 11:43:50 tnn Exp $
 
 BUILDLINK_TREE+=	hs-sqlite-simple
 
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.hs-sqlite-simple?=		../../databases/hs-sqlite-simple
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
 .include "../../devel/hs-blaze-textual/buildlink3.mk"
 .include "../../databases/hs-direct-sqlite/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/hs-Only/buildlink3.mk"
 .include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_SQLITE_SIMPLE_BUILDLINK3_MK
