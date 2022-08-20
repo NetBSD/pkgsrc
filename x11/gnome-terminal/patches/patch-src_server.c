e0655b905b0f1f9aafaaf990e0e363c6d833f176
gutteridge <gutteridge@pkgsrc.org>
2022-08-20 21:41:43 +0000

    gnome-terminal: note upstream commit that merges our patches
    These will be in the next stable release.

--- x11/gnome-terminal/patches/patch-src_server.c
+++ x11/gnome-terminal/patches/patch-src_server.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_server.c,v 1.1 2020/06/20 15:05:48 nia Exp $
+$NetBSD: patch-src_server.c,v 1.2 2022/08/20 21:41:44 gutteridge Exp $
 
 error: %m is only allowed in syslog(3) like functions [-Werror=format=]
+https://gitlab.gnome.org/GNOME/gnome-terminal/-/commit/fd27fa449c8a494c41fcb0a8c127ffa74725b8be
 
 --- src/server.c.orig	2020-03-05 21:43:41.000000000 +0000
 +++ src/server.c
