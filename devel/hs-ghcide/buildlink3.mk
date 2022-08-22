66c0b55a23cfc0ef320be3d6acaf1814afc7ca31
wiz <wiz@pkgsrc.org>
2022-08-22 10:13:21 +0000

    *: bump for hs-vector-algorithms buildlink3.mk change

--- devel/hs-ghcide/buildlink3.mk
+++ devel/hs-ghcide/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/08/22 10:13:22 wiz Exp $
 
 BUILDLINK_TREE+=	hs-ghcide
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-ghcide
 HS_GHCIDE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-ghcide+=	hs-ghcide>=1.6.0
-BUILDLINK_ABI_DEPENDS.hs-ghcide+=	hs-ghcide>=1.6.0.0nb3
+BUILDLINK_ABI_DEPENDS.hs-ghcide+=	hs-ghcide>=1.6.0.0nb4
 BUILDLINK_PKGSRCDIR.hs-ghcide?=		../../devel/hs-ghcide
 
 .include "../../converters/hs-aeson/buildlink3.mk"
--- devel/hs-implicit-hie/buildlink3.mk
+++ devel/hs-implicit-hie/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:16 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/08/22 10:13:24 wiz Exp $
 
 BUILDLINK_TREE+=	hs-implicit-hie
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-implicit-hie
 HS_IMPLICIT_HIE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2.6nb2
+BUILDLINK_ABI_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2.6nb3
 BUILDLINK_PKGSRCDIR.hs-implicit-hie?=	../../devel/hs-implicit-hie
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
