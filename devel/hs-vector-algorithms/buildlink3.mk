877791d4acf4b34ff4697103864e1c54871d705b
wiz <wiz@pkgsrc.org>
2022-08-22 10:11:27 +0000

    hs-vector-algorithms: update to 0.9.0.1.
    ## Version 0.9.0.1 (2022-07-28)
    
    - Allow building with vector-0.13.*.
    
    ## Version 0.9.0.0 (2022-05-19)
    
    - Add nub related functions.
    - Add sortUniq related functions (sorts, then removes duplicates).

--- devel/hs-vector-algorithms/buildlink3.mk
+++ devel/hs-vector-algorithms/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2022/08/20 08:08:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2022/08/22 10:11:27 wiz Exp $
 
 BUILDLINK_TREE+=	hs-vector-algorithms
 
@@ -6,9 +6,10 @@ BUILDLINK_TREE+=	hs-vector-algorithms
 HS_VECTOR_ALGORITHMS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0
-BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0.4nb5
+BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.9.0.1
 BUILDLINK_PKGSRCDIR.hs-vector-algorithms?=	../../devel/hs-vector-algorithms
 
+.include "../../devel/hs-bitvec/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_VECTOR_ALGORITHMS_BUILDLINK3_MK
