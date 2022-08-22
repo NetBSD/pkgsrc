49e035fc4daeeccb0a153f342e7635ab97b45fb2
wiz <wiz@pkgsrc.org>
2022-08-22 10:52:46 +0000

    SDL2_net: update to 2.2.0.
    2.2.0:
     * Added support for building with CMake

--- net/SDL2_net/buildlink3.mk
+++ net/SDL2_net/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/08/11 05:09:08 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/08/22 10:52:46 wiz Exp $
 
 BUILDLINK_TREE+=	SDL2_net
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	SDL2_net
 SDL2_NET_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_net+=	SDL2_net>=2.0.0nb2
-BUILDLINK_ABI_DEPENDS.SDL2_net?=		SDL2_net>=2.0.1nb6
+BUILDLINK_ABI_DEPENDS.SDL2_net?=	SDL2_net>=2.0.1nb6
 BUILDLINK_PKGSRCDIR.SDL2_net?=		../../net/SDL2_net
 
 .include "../../devel/SDL2/buildlink3.mk"
