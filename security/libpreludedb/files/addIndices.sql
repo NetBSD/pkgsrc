cf9a49c0d11b84395b55dec0118d8ea1162a37f4
tnn <tnn@pkgsrc.org>
2022-08-22 22:59:07 +0000

    prelude: remove from pkgsrc
    prelude is unmaintained in pkgsrc since 2009 and is several major
    versions behind. This makes it useless for it's intended purpose as an
    intrusion detection system. Can be revived if/when there is an interested
    maintainer. But it's probably easier to start from scratch given how stale
    these packages were.

--- security/libpreludedb/files/addIndices.sql
+++
@@ -1,4 +0,0 @@
-alter table Prelude_Service add index (_message_ident,_parent_type);
-alter table Prelude_Service add index (_message_ident);
-alter table Prelude_Node add index (_message_ident,_parent_type);
-alter table Prelude_Process add index (_message_ident,_parent_type);
