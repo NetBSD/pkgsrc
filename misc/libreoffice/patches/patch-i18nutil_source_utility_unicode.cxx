ff92223ea1f779ffaea91eccbcb255ffdfa942f5
ryoon <ryoon@pkgsrc.org>
2022-08-22 13:58:32 +0000

    libreoffice: Update to 7.4.0.3
    Changelog:
    Most Significant New Features
    
    GENERAL
        * Support for WebP images and EMZ/WMZ files
        * Help pages for the ScriptForge scripting library
        * Search field for the Extension Manager
        * Performance and compatibility improvements
    
    WRITER
        * Better change tracking in the footnote area
        * Edited lists show original numbers in change tracking
        * New typographic settings for hyphenation
    
    CALC
        * Support for 16,384 columns in spreadsheets
        * Extra functions in drop-down AutoSum widget
        * New menu item to search for sheet names
    
    IMPRESS
        * New support for document themes

--- misc/libreoffice/patches/patch-i18nutil_source_utility_unicode.cxx
+++ misc/libreoffice/patches/patch-i18nutil_source_utility_unicode.cxx
@@ -1,12 +1,12 @@
-$NetBSD: patch-i18nutil_source_utility_unicode.cxx,v 1.5 2020/02/05 14:10:35 ryoon Exp $
+$NetBSD: patch-i18nutil_source_utility_unicode.cxx,v 1.6 2022/08/22 13:58:32 ryoon Exp $
 
---- i18nutil/source/utility/unicode.cxx.orig	2019-01-23 19:35:25.000000000 +0000
+--- i18nutil/source/utility/unicode.cxx.orig	2022-08-10 14:14:32.000000000 +0000
 +++ i18nutil/source/utility/unicode.cxx
-@@ -25,6 +25,7 @@
+@@ -24,6 +24,7 @@
  #include <i18nutil/unicode.hxx>
  #include <sal/log.hxx>
  #include <unicode/numfmt.h>
 +#include <unicode/regex.h>
  #include "unicode_data.h"
  #include <rtl/character.hxx>
- #include <memory>
+ #include <o3tl/string_view.hxx>
