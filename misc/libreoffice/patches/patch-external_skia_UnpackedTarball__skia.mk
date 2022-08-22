fcb7a7c4d2e1c0d24c52c06c867509df3129d595
ryoon <ryoon@pkgsrc.org>
2022-08-22 13:58:32 +0000

    libreoffice: Update to 7.4.0.3
    Changelog:
    Most Significant New Features
    
    GENERAL
        * Support for WebP images and EMZ/WMZ files
        * Help pages for the ScriptForge scripting library
        * Search field for the Extension Manager
        * Performance and compatibility improvements
    
    WRITER
        * Better change tracking in the footnote area
        * Edited lists show original numbers in change tracking
        * New typographic settings for hyphenation
    
    CALC
        * Support for 16,384 columns in spreadsheets
        * Extra functions in drop-down AutoSum widget
        * New menu item to search for sheet names
    
    IMPRESS
        * New support for document themes

--- misc/libreoffice/patches/patch-external_skia_UnpackedTarball__skia.mk
+++ misc/libreoffice/patches/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,12 +1,12 @@
-$NetBSD: patch-external_skia_UnpackedTarball__skia.mk,v 1.6 2022/02/05 04:53:43 ryoon Exp $
+$NetBSD: patch-external_skia_UnpackedTarball__skia.mk,v 1.7 2022/08/22 13:58:32 ryoon Exp $
 
---- external/skia/UnpackedTarball_skia.mk.orig	2022-01-26 14:35:29.000000000 +0000
+--- external/skia/UnpackedTarball_skia.mk.orig	2022-08-10 14:14:32.000000000 +0000
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -37,6 +37,7 @@ skia_patches := \
-     disable-freetype-colrv1.1 \
-     windows-libraries-system32.patch.1 \
-     fix-graphite-ifdef.patch.1 \
-+    netbsd.patch.0
+@@ -38,6 +38,7 @@ skia_patches := \
+     allow-no-es2restrictions.patch.1 \
+     vk_mem_alloc.patch.1 \
+     tdf148624.patch.1 \
++    netbsd.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
  
