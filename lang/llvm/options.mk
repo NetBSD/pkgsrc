1d9470bebde0960ed140cb251813cd305f3a01c1
ryoon <ryoon@pkgsrc.org>
2022-08-24 14:38:56 +0000

    llvm: Add option for experimental targets, arc, csky, m68k and ve
    * Enable the experimental targets for faster architectures only.
      And bump PKGREVISION.
    * Add some helper for PLIST generation.

--- lang/llvm/options.mk
+++ lang/llvm/options.mk
@@ -1,13 +1,17 @@
-# $NetBSD: options.mk,v 1.15 2022/08/13 09:11:12 tnn Exp $
+# $NetBSD: options.mk,v 1.16 2022/08/24 14:38:56 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.llvm
 
 LLVM_TARGETS=	AArch64 AMDGPU ARM AVR BPF Hexagon Lanai Mips MSP430 NVPTX PowerPC RISCV Sparc SystemZ WebAssembly X86 XCore
+LLVM_EXPERIMENTAL_TARGETS=	ARC CSKY M68k VE
 
-.for tgt in ${LLVM_TARGETS}
+.for tgt in ${LLVM_TARGETS} ${LLVM_EXPERIMENTAL_TARGETS}
 PLIST_VARS+=			${tgt}
 PKG_SUPPORTED_OPTIONS+=		llvm-target-${tgt:tl}
+PRINT_PLIST_AWK+=		{if ($$0 ~ /libLLVM${tgt}/) {$$0 = "$${PLIST.${tgt}}" $$0;}}
+PRINT_PLIST_AWK+=		{if ($$0 ~ /libLLVMExegesis${tgt}/) {$$0 = "$${PLIST.${tgt}}" $$0;}}
 .endfor
+
 PKG_SUPPORTED_OPTIONS+=		terminfo z3
 
 # Terminfo is used for colour output, only enable it by default if terminfo
@@ -35,7 +39,7 @@ PKG_SUGGESTED_OPTIONS+=	llvm-target-arm
 PKG_SUGGESTED_OPTIONS+=	llvm-target-mips
 .else
 # X86 and everyone else get all targets by default.
-.  for tgt in ${LLVM_TARGETS}
+.  for tgt in ${LLVM_TARGETS} ${LLVM_EXPERIMENTAL_TARGETS}
 PKG_SUGGESTED_OPTIONS+=	llvm-target-${tgt:tl}
 .  endfor
 .endif
@@ -48,6 +52,16 @@ PLIST.${tgt}=		yes
 LLVM_TARGETS_TO_BUILD+=	${tgt}
 .  endif
 .endfor
+
+.for tgt in ${LLVM_EXPERIMENTAL_TARGETS}
+.  if !empty(PKG_OPTIONS:Mllvm-target-${tgt:tl})
+PLIST.${tgt}=	yes
+LLVM_EXPERIMENTAL_TARGETS_TO_BUILD+=	${tgt}
+.  endif
+.endfor
+.if !empty(LLVM_EXPERIMENTAL_TARGETS_TO_BUILD)
+CMAKE_ARGS+=	-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="${LLVM_EXPERIMENTAL_TARGETS_TO_BUILD:ts;}"
+.endif
 
 .if !empty(PKG_OPTIONS:Mterminfo)
 .include "../../mk/terminfo.buildlink3.mk"
