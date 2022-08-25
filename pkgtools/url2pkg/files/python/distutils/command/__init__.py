81050baf167493f2687546893553015084e48fc3
rillig <rillig@pkgsrc.org>
2022-08-25 20:10:42 +0000

    pkgtools/url2pkg: add support for distutils.errors
    Needed for geography/py-maxmindb.

---
+++ pkgtools/url2pkg/files/python/distutils/command/__init__.py
@@ -0,0 +1,5 @@
+# $NetBSD: __init__.py,v 1.1 2022/08/25 20:10:43 rillig Exp $
+#
+# Fake implementation of distutils.command.
+#
+# https://docs.python.org/3/distutils/apiref.html#module-distutils.command
