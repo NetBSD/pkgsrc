6fdc8705ac3b033060b8e899da5cbb85d51aca67
thor <thor@pkgsrc.org>
2022-08-25 22:52:07 +0000

    parallel/openmpi: fix buildlink path from wip
    Stupid! Redundant! Sorry.

--- parallel/openmpi/buildlink3.mk
+++ parallel/openmpi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/08/25 22:09:14 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/08/25 22:52:07 thor Exp $
 
 BUILDLINK_TREE+=	openmpi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openmpi
 OPENMPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=1.5.1
-BUILDLINK_PKGSRCDIR.openmpi?=	../../wip/openmpi
+BUILDLINK_PKGSRCDIR.openmpi?=	../../parallel/openmpi
 
 .endif # OPENMPI_BUILDLINK3_MK
 
