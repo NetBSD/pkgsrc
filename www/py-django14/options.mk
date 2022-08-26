071b4638be8568c8e88e110cbd7a96b278d901b4
wiz <wiz@pkgsrc.org>
2022-08-26 18:39:43 +0000

    py-django14: remove mysql option
    Latest py-mysqldb does not support python 2.x

--- www/py-django14/options.mk
+++ www/py-django14/options.mk
@@ -1,18 +1,18 @@
-# $NetBSD: options.mk,v 1.11 2019/11/04 22:10:12 rillig Exp $
+# $NetBSD: options.mk,v 1.12 2022/08/26 18:39:43 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.django
-PKG_SUPPORTED_OPTIONS=	mysql oracle pgsql sqlite
+PKG_SUPPORTED_OPTIONS=	oracle pgsql sqlite # mysql
 PKG_SUGGESTED_OPTIONS=	pgsql
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
 
-.if !empty(PKG_OPTIONS:Mmysql)
-DEPENDS+=	${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
-PYTHON_VERSIONS_ACCEPTED= 27 # py-mysqldb
-PLIST.mysql=	yes
-.endif
+#.if !empty(PKG_OPTIONS:Mmysql)
+#DEPENDS+=	${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
+#PYTHON_VERSIONS_ACCEPTED= 27 # py-mysqldb
+#PLIST.mysql=	yes
+#.endif
 
 .if !empty(PKG_OPTIONS:Moracle)
 DEPENDS+=	${PYPKGPREFIX}-cx_Oracle-[0-9]*:../../databases/py-cx_Oracle
