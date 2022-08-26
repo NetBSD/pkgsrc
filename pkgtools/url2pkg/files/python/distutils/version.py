0653d94f2f8dfb3a6ba10f4bf496a33b997fbc37
rillig <rillig@pkgsrc.org>
2022-08-26 20:14:58 +0000

    pkgtools/url2pkg: make distutils.version.LooseVersion more complete
    For the benefit of wip/py-torch.

--- pkgtools/url2pkg/files/python/distutils/version.py
+++ pkgtools/url2pkg/files/python/distutils/version.py
@@ -1,9 +1,21 @@
-# $NetBSD: version.py,v 1.2 2022/08/25 19:56:07 rillig Exp $
+# $NetBSD: version.py,v 1.3 2022/08/26 20:14:58 rillig Exp $
 #
 # Fake implementation of distutils.version.
 #
 # https://docs.python.org/3/distutils/apiref.html#module-distutils.version
 
-# used by pkgsrc package wip/py-torch 1.12.0
-class LooseVersion:
+# indirectly used by pkgsrc package wip/py-torch 1.12.0
+class Version:
+    def __init__(self, vstring=None):
+        pass
+
+
+# just for completeness
+class StrictVersion(Version):
     pass
+
+
+# used by pkgsrc package wip/py-torch 1.12.0
+class LooseVersion(Version):
+    def __init__(self, vstring=None):
+        super().__init__(vstring)
