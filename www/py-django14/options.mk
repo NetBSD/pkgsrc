57824af15db09c5981b621849db60b29d107d03a
wiz <wiz@pkgsrc.org>
2022-08-29 10:33:05 +0000

    py-django14: finish mysql option removal

--- www/py-django14/options.mk
+++ www/py-django14/options.mk
@@ -1,18 +1,12 @@
-# $NetBSD: options.mk,v 1.12 2022/08/26 18:39:43 wiz Exp $
+# $NetBSD: options.mk,v 1.13 2022/08/29 10:33:05 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.django
-PKG_SUPPORTED_OPTIONS=	oracle pgsql sqlite # mysql
+PKG_SUPPORTED_OPTIONS=	oracle pgsql sqlite
 PKG_SUGGESTED_OPTIONS=	pgsql
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
-
-#.if !empty(PKG_OPTIONS:Mmysql)
-#DEPENDS+=	${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
-#PYTHON_VERSIONS_ACCEPTED= 27 # py-mysqldb
-#PLIST.mysql=	yes
-#.endif
 
 .if !empty(PKG_OPTIONS:Moracle)
 DEPENDS+=	${PYPKGPREFIX}-cx_Oracle-[0-9]*:../../databases/py-cx_Oracle
