3c66fcd361e98897b282fcb32d3dd186c391c221
nia <nia@pkgsrc.org>
2022-08-29 10:36:51 +0000

    freerdp2: Fix eventfd detection when epoll-shim is used.
    Fixes building on NetBSD 9.

--- net/freerdp2/patches/patch-cmake_InstallFreeRDPMan.cmake
+++ net/freerdp2/patches/patch-cmake_InstallFreeRDPMan.cmake
@@ -1,6 +1,8 @@
-$NetBSD: patch-cmake_InstallFreeRDPMan.cmake,v 1.1 2022/08/06 08:02:27 nia Exp $
+$NetBSD: patch-cmake_InstallFreeRDPMan.cmake,v 1.2 2022/08/29 10:36:51 nia Exp $
 
 Install man pages to CMAKE_INSTALL_MANDIR on all OSes, per pkgsrc policy.
+
+https://github.com/FreeRDP/FreeRDP/commit/8f44b9cf87d85de5cc2db0cf1d61771748f3249b
 
 --- cmake/InstallFreeRDPMan.cmake.orig	2021-03-15 12:29:35.000000000 +0000
 +++ cmake/InstallFreeRDPMan.cmake
