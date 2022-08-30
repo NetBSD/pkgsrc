35f0470e1cd4635f9317a198ee4d58c47b7d56a9
jperkin <jperkin@pkgsrc.org>
2022-08-30 15:45:28 +0000

    lame: Pull in iconv when the option is enabled.

--- audio/lame/buildlink3.mk
+++ audio/lame/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:23:54 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2022/08/30 15:45:28 jperkin Exp $
 
 BUILDLINK_TREE+=	lame
 
@@ -8,6 +8,14 @@ LAME_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.lame+=	lame>=3.93.1nb2
 BUILDLINK_ABI_DEPENDS.lame+=	lame>=3.96.1nb4
 BUILDLINK_PKGSRCDIR.lame?=	../../audio/lame
+
+pkgbase:= lame
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.lame:Municode}
+.  include "../../converters/libiconv/buildlink3.mk"
+.endif
+
 .endif # LAME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-lame
