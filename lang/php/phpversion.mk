3c7d1bed560111b3b91491bbf9d2017a498b9752
taca <taca@pkgsrc.org>
2022-09-01 16:34:21 +0000

    lang/php80: update to 8.0.23
    01 Sep 2022, PHP 8.0.23
    
    - Core:
      . Fixed incorrect double to long casting in latest clang. (zeriyoshi)
    
    - DBA:
      . Fixed LMDB driver memory leak on DB creation failure (Girgias)
      . Fixed bug GH-9155 (dba_open("non-existing", "c-", "flatfile") segfaults).
        (cmb)
    
    - Intl:
      . Fixed IntlDateFormatter::formatObject() parameter type. (Gert de Pagter)
    
    - OPcache:
      . Fixed bug GH-9033 (Loading blacklist file can fail due to negative length).
        (cmb)
    
    - PDO_SQLite:
      . Fixed bug GH-9032 (SQLite3 authorizer crashes on NULL values). (cmb)
    
    - SQLite3:
      . Fixed bug GH-9032 (SQLite3 authorizer crashes on NULL values). (cmb)
    
    - Standard:
      . Fixed bug GH-9017 (php_stream_sock_open_from_socket could return NULL).
        (Heiko Weber)
    
    - Streams:
      . Fixed bug GH-8472 (The resource returned by stream_socket_accept may have
        incorrect metadata). (Jakub Zelenka)
      . Fixed bug GH-8409 (SSL handshake timeout leaves persistent connections
        hanging). (Jakub Zelenka, Twosee)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.373 2022/09/01 16:32:35 taca Exp $
+# $NetBSD: phpversion.mk,v 1.374 2022/09/01 16:34:21 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.30
-PHP80_VERSION=	8.0.22
+PHP80_VERSION=	8.0.23
 PHP81_VERSION=	8.1.10
 
 # Define initial release of major version.
