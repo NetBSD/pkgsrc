f0744c291d025a36d6270fd01030e6fbbc371b58
jperkin <jperkin@pkgsrc.org>
2022-09-01 09:59:46 +0000

    rust: Cleanups and fixes from wip.
    No effective functional change to normal builds, primarily whitespace changes,
    but does include a fix to the stage0-bootstrap target.

--- lang/rust/patches/patch-src_bootstrap_lib.rs
+++ lang/rust/patches/patch-src_bootstrap_lib.rs
@@ -1,10 +1,8 @@
-$NetBSD: patch-src_bootstrap_lib.rs,v 1.15 2022/08/30 19:22:17 he Exp $
+$NetBSD: patch-src_bootstrap_lib.rs,v 1.16 2022/09/01 09:59:46 jperkin Exp $
 
 Don't filter out optimization flags.
 FreeBSD has a particular C++ runtime library name
 
---- src/bootstrap/lib.rs.orig	2021-09-06 18:42:35.000000000 +0000
-+++ src/bootstrap/lib.rs
 --- src/bootstrap/lib.rs.orig	2022-07-16 08:49:08.000000000 +0000
 +++ src/bootstrap/lib.rs
 @@ -1029,14 +1029,13 @@ impl Build {
