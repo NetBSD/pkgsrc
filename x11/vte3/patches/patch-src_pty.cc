c0e231ae9d810ee4a9c68df7dba75543e0af726f
gutteridge <gutteridge@pkgsrc.org>
2022-09-01 01:17:27 +0000

    vte3: amend previous NetBSD fix patch
    The original version of the NetBSD fix in the patch was added back in
    2018, then lost in churn in 2022, and put back by me. (There was no
    technical explanation of the patch in the original commit, and it looks
    kind of odd.) In fact, it was missing the fact CLOEXEC wasn't getting
    set, either.

--- x11/vte3/patches/patch-src_pty.cc
+++ x11/vte3/patches/patch-src_pty.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_pty.cc,v 1.7 2022/04/10 22:14:50 gutteridge Exp $
+$NetBSD: patch-src_pty.cc,v 1.8 2022/09/01 01:17:27 gutteridge Exp $
 
 Use correct includes on SunOS.
 Functional fix of posix_openpt() on NetBSD.
@@ -16,15 +16,21 @@ Functional fix of posix_openpt() on NetBSD.
  #include <stropts.h>
  #endif
  #include <glib.h>
-@@ -426,7 +428,13 @@ _vte_pty_open_posix(void)
+@@ -426,7 +428,19 @@ _vte_pty_open_posix(void)
          auto fd = vte::libc::FD{posix_openpt(O_RDWR | O_NOCTTY | O_NONBLOCK | O_CLOEXEC)};
  #ifndef __linux__
          /* Other kernels may not support CLOEXEC or NONBLOCK above, so try to fall back */
 -        bool need_cloexec = false, need_nonblocking = false;
-+        bool need_cloexec = false;
 +#ifdef __NetBSD__
++        /*
++         * NetBSD's posix_openpt() will pass along flags that don't actually get applied,
++         * so it will return an FD with only O_RDWR actually set, and we need to force the
++         * fallbacks below.
++         */
++        bool need_cloexec = true;
 +        bool need_nonblocking = true;
 +#else
++        bool need_cloexec = false;
 +        bool need_nonblocking = false;
 +#endif /* __NetBSD__ */
 +
