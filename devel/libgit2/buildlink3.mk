daccb69083cad26dff99562242094a8bd10440a4
taca <taca@pkgsrc.org>
2022-09-03 14:53:35 +0000

    devel/libgit2: update to 1.5.0
    Update from 1.2.0 to 1.5.0 are too many write here, please refer:
    <https://github.com/libgit2/libgit2/releases>.

--- devel/libgit2/buildlink3.mk
+++ devel/libgit2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2021/12/21 14:01:26 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/09/03 14:53:35 taca Exp $
 
 BUILDLINK_TREE+=	libgit2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgit2
 LIBGIT2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.28.0
-BUILDLINK_ABI_DEPENDS.libgit2+=	libgit2>=1.0
+BUILDLINK_ABI_DEPENDS.libgit2+=	libgit2>=1.5
 BUILDLINK_PKGSRCDIR.libgit2?=	../../devel/libgit2
 
 .include "../../devel/pcre2/buildlink3.mk"
