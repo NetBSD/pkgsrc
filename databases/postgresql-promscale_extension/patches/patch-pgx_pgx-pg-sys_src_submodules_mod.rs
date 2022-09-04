39e350d7ba5b9a5c541d7cab868fc562af83fe66
tnn <tnn@pkgsrc.org>
2022-09-04 11:55:41 +0000

    postgresql-promscale_extension: update to 0.6.0
    Added
      prom_api.get_default_chunk_interval()
      prom_api.get_metric_chunk_interval(TEXT)
      _ps_trace.text_matches()
      _ps_trace.tag_v_text_eq_matching_tags()
    Fixed
      Don't fail metric deletion if some tables or views are missing
      Incorrect type coercion when using tag_map with = operator
      During upgrade from 0.3.x only alter relations which actually exist
    Changed
      ps_trace.delete_all_traces() can only be executed when no Promscale
      connectors are running

--- databases/postgresql-promscale_extension/patches/patch-pgx_pgx-pg-sys_src_submodules_mod.rs
+++ databases/postgresql-promscale_extension/patches/patch-pgx_pgx-pg-sys_src_submodules_mod.rs
@@ -1,14 +1,22 @@
-$NetBSD: patch-pgx_pgx-pg-sys_src_submodules_mod.rs,v 1.4 2022/07/18 19:47:26 tnn Exp $
+$NetBSD: patch-pgx_pgx-pg-sys_src_submodules_mod.rs,v 1.5 2022/09/04 11:55:41 tnn Exp $
 
 NetBSD, SunOS support
 
---- ../pgx/pgx-pg-sys/src/submodules/mod.rs.orig	1973-11-29 21:33:09.000000000 +0000
+--- ../pgx/pgx-pg-sys/src/submodules/mod.rs.orig	2022-07-26 13:03:15.000000000 +0000
 +++ ../pgx/pgx-pg-sys/src/submodules/mod.rs
-@@ -20,7 +20,16 @@ extern "C" {
+@@ -27,10 +27,19 @@ extern "C" {
      ) -> std::os::raw::c_int;
  }
  
--#[cfg(any(target_os = "macos", target_os = "freebsd"))]
+-#[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd"))]
++#[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd", target_os = "solaris", target_os = "illumos"))]
+ extern "C" {
+     pub(crate) fn sigsetjmp(
+         env: *mut crate::sigjmp_buf,
+         savemask: std::os::raw::c_int,
+     ) -> std::os::raw::c_int;
+ }
++
 +#[cfg(target_os = "netbsd")]
 +extern "C" {
 +    #[link_name = "__sigsetjmp14"]
@@ -17,8 +25,3 @@ NetBSD, SunOS support
 +        savemask: std::os::raw::c_int,
 +    ) -> std::os::raw::c_int;
 +}
-+
-+#[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "solaris", target_os = "illumos"))]
- extern "C" {
-     pub(crate) fn sigsetjmp(
-         env: *mut crate::sigjmp_buf,
