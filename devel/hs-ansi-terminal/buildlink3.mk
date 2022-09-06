6ec3e3f4e4df246becf214b19672a54f92a34175
pho <pho@pkgsrc.org>
2022-09-06 18:41:45 +0000

    Update to ansi-terminal-0.11.3
    Version 0.11.3
    * Add "hyperlink", "hyperlinkWithId" and "hyperlinkWithParams", and support
      for clicable hyperlinks.
    
    Version 0.11.2
    * On Windows, fix compatability with the Windows I/O Manager (WinIO) when
      GHC >= 9.0.1 but Win32 < 2.9.0.0.
    * Improvements to Haddock documentation.

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2022/02/26 03:57:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/09/06 18:41:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.1
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.1nb2
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.3
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.3
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
 
 .include "../../devel/hs-colour/buildlink3.mk"
