910b0c25c6d19dfcbb2f0d19c293600f6986b717
pho <pho@pkgsrc.org>
2022-09-06 19:03:33 +0000

    Update to bower-json-1.1.0.0
    No release notes have been provided by the upstream.

--- www/hs-bower-json/buildlink3.mk
+++ www/hs-bower-json/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/09/06 19:03:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-bower-json
 
 .if !defined(HS_BOWER_JSON_BUILDLINK3_MK)
 HS_BOWER_JSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bower-json+=	hs-bower-json>=1.0.0
-BUILDLINK_ABI_DEPENDS.hs-bower-json+=	hs-bower-json>=1.0.0.1nb2
+BUILDLINK_API_DEPENDS.hs-bower-json+=	hs-bower-json>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-bower-json+=	hs-bower-json>=1.1.0.0
 BUILDLINK_PKGSRCDIR.hs-bower-json?=	../../www/hs-bower-json
 
 .include "../../converters/hs-aeson/buildlink3.mk"
