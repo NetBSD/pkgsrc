06e877fa9ff2e3282afe9fe6a8734295de367148
pho <pho@pkgsrc.org>
2022-09-06 19:18:33 +0000

    Update to clock-0.8.3
    * Dropped support for GHC < 7.8.
    * Tested with GHC 7.8 - 9.2.
    * TODO: new module System.Clock.Seconds
    * TODO: new functions
    * TODO: other changes

--- time/hs-clock/buildlink3.mk
+++ time/hs-clock/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/09/06 19:18:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-clock
 
 .if !defined(HS_CLOCK_BUILDLINK3_MK)
 HS_CLOCK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-clock+=	hs-clock>=0.8.2
-BUILDLINK_ABI_DEPENDS.hs-clock+=	hs-clock>=0.8.2nb4
+BUILDLINK_API_DEPENDS.hs-clock+=	hs-clock>=0.8.3
+BUILDLINK_ABI_DEPENDS.hs-clock+=	hs-clock>=0.8.3
 BUILDLINK_PKGSRCDIR.hs-clock?=		../../time/hs-clock
 .endif	# HS_CLOCK_BUILDLINK3_MK
 
