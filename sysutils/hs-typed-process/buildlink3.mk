4e95ab41d741e65c0c3266fbccd5b9fc1a31ebfd
pho <pho@pkgsrc.org>
2022-09-07 02:18:04 +0000

    sysutils/typed-process: Update to 0.2.10.1
    0.2.10.0
    * Add mkPipeStreamSpec
    
    0.2.9.0
    * Re-export StdStream

--- sysutils/hs-typed-process/buildlink3.mk
+++ sysutils/hs-typed-process/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:24 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/09/07 02:18:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-typed-process
 
 .if !defined(HS_TYPED_PROCESS_BUILDLINK3_MK)
 HS_TYPED_PROCESS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.8
-BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.8.0nb2
+BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.10
+BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.10.1
 BUILDLINK_PKGSRCDIR.hs-typed-process?=		../../sysutils/hs-typed-process
 
 .include "../../devel/hs-async/buildlink3.mk"
