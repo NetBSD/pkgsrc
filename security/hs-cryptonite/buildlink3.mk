6d8a0f90f0ae54c08406493f8fa79145c7e227cb
pho <pho@pkgsrc.org>
2022-09-07 00:58:45 +0000

    security/hs-cryptonite: Update to 0.30
    * Fix some C symbol blake2b prefix to be cryptonite_ prefix (fix mixing
      with other C library)
    * add hmac-lazy
    * Fix compilation with GHC 9.2
    * Drop support for GHC8.0, GHC8.2, GHC8.4, GHC8.6

--- security/hs-cryptonite/buildlink3.mk
+++ security/hs-cryptonite/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/09/07 00:58:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-cryptonite
 
 .if !defined(HS_CRYPTONITE_BUILDLINK3_MK)
 HS_CRYPTONITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.29
-BUILDLINK_ABI_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.29nb2
+BUILDLINK_API_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.30
+BUILDLINK_ABI_DEPENDS.hs-cryptonite+=	hs-cryptonite>=0.30
 BUILDLINK_PKGSRCDIR.hs-cryptonite?=	../../security/hs-cryptonite
 
 .include "../../devel/hs-basement/buildlink3.mk"
