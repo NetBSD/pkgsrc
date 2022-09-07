7047cc4d80618b238212e60b723eff8c03fe4272
pho <pho@pkgsrc.org>
2022-09-07 02:07:01 +0000

    devel/hs-memory: Forgot to update buildlink3.mk

--- devel/hs-memory/buildlink3.mk
+++ devel/hs-memory/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/09/07 02:07:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-memory
 
 .if !defined(HS_MEMORY_BUILDLINK3_MK)
 HS_MEMORY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-memory+=	hs-memory>=0.16.0
-BUILDLINK_ABI_DEPENDS.hs-memory+=	hs-memory>=0.16.0nb2
+BUILDLINK_API_DEPENDS.hs-memory+=	hs-memory>=0.18.0
+BUILDLINK_ABI_DEPENDS.hs-memory+=	hs-memory>=0.18.0
 BUILDLINK_PKGSRCDIR.hs-memory?=		../../devel/hs-memory
 
 .include "../../devel/hs-basement/buildlink3.mk"
