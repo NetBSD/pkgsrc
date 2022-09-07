7a1468a24a950fb605771fc8f93d500265a8b7ab
pho <pho@pkgsrc.org>
2022-09-07 06:43:41 +0000

    devel/hs-protolude: Update to 0.3.2
    0.3.2
    * GHC 9.2.2 support
    * Drop export executable
    
    0.3.1
    * GHC 9.0.1 and 9.2.1 support
    * Add HasCallStack to unsafe* functions.
    * Banish String on readMaybe and readEither.

--- devel/hs-protolude/buildlink3.mk
+++ devel/hs-protolude/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/09/07 06:43:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-protolude
 
 .if !defined(HS_PROTOLUDE_BUILDLINK3_MK)
 HS_PROTOLUDE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-protolude+=	hs-protolude>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-protolude+=	hs-protolude>=0.3.0nb1
+BUILDLINK_API_DEPENDS.hs-protolude+=	hs-protolude>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-protolude+=	hs-protolude>=0.3.2nb1
 BUILDLINK_PKGSRCDIR.hs-protolude?=	../../devel/hs-protolude
 
 .include "../../devel/hs-async/buildlink3.mk"
