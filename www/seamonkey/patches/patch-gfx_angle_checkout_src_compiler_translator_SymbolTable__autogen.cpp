6d5b3e1b7d3dd1ebace8950dddba902a8a499285
ryoon <ryoon@pkgsrc.org>
2022-09-08 20:27:32 +0000

    seamonkey: Update to 2.53.13
    * Use libwebp from pkgsrc/graphics/libwebp.
    * Drop gtk2 dependency.
    
    Changelog:
    What's New in SeaMonkey 2.53.13
    
    SeaMonkey 2.53.13 contains (among other changes) the following major changes
    relative to SeaMonkey 2.53.12:
    
      * Updates to devtools.
      * Updates to build configuration.
      * Starting the switch from Python 2 to Python 3 in the build system.
      * Removal of array comprehensions, legacy iterators and generators bug
        1414340 and bug 1098412.
      * Adding initial optional chaining and Promise.allSettled() support.
    
    SeaMonkey 2.53.13 contains (among other changes) the following major changes
    relative to SeaMonkey 2.49.5:
    
      * The Bookmarks Manager has switched its name to Library, and now also
        includes the History list. When History is invoked, the Library will be
        shown with the History list selected. The extensive modifications were
        needed because of Mozilla Gecko platform API changes.
      * Download Manager has been migrated to a new API. Although it looks pretty
        much the same as before, the search option is missing and some other minor
        details work differently. The previous downloads history is removed during
        the upgrade.
      * The layout panel was added to the CSS Grid tools.
      * TLS 1.3 is the default SSL version now.
      * Support for all NPAPI plugins like Flash, Java and Silverlight has been
        removed. For displaying pdf files in the browser you can use
        pdf.js-seamonkey from Isaac Schemm.
      * SeaMonkey now uses a new api for formatting regional data like time and
        date. Default is to use the application locale of the current SeaMonkey
        build. If you use a language pack or a different OS formatting this is
        usually not desired. You can change the formatting from the application
        locale to the regional settings locale (OS) in the preferences dialog under
        "Appearance".
    
    SeaMonkey 2.53.13 uses the same backend as Firefox and contains the relevant
    Firefox 60.8 security fixes.
    
    SeaMonkey 2.53.13 shares most parts of the mail and news code with Thunderbird.
    Please read the Thunderbird 60.8.0 release notes for specific security fixes in
    this release.
    
    Additional important security fixes up to Current Firefox 91.11 and Thunderbird
    91.11 ESR plus many enhancements have been backported. We will continue to
    enhance SeaMonkey security in subsequent 2.53.x beta and release versions as
    fast as we are able to.
    
    SeaMonkey-specific changes
    
      * SeaMonkey now uses gtk3 on Linux. If you experience a problem because of
        this please file a bug and link it to bug 1367257. Please try another OS
        theme first. Some of them are buggy and cause problems with SeaMonkey,
        Thunderbird and Firefox.

--- www/seamonkey/patches/patch-gfx_angle_checkout_src_compiler_translator_SymbolTable__autogen.cpp
+++ www/seamonkey/patches/patch-gfx_angle_checkout_src_compiler_translator_SymbolTable__autogen.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-gfx_angle_checkout_src_compiler_translator_SymbolTable__autogen.cpp,v 1.1 2021/09/16 14:59:32 nia Exp $
+$NetBSD: patch-gfx_angle_checkout_src_compiler_translator_SymbolTable__autogen.cpp,v 1.2 2022/09/08 20:27:32 ryoon Exp $
 
 https://bugzilla.mozilla.org/show_bug.cgi?id=1490673
 
---- gfx/angle/checkout/src/compiler/translator/SymbolTable_autogen.cpp.orig	2021-05-25 18:33:36.000000000 +0000
+--- gfx/angle/checkout/src/compiler/translator/SymbolTable_autogen.cpp.orig	2022-06-08 19:10:28.000000000 +0000
 +++ gfx/angle/checkout/src/compiler/translator/SymbolTable_autogen.cpp
-@@ -9,6 +9,8 @@
- // SymbolTable_autogen.cpp:
- //   Compile-time initialized built-ins.
+@@ -11,6 +11,8 @@
+ 
+ #ifndef ANGLE_TRANSLATOR_ESSL_ONLY
  
 +#include <cmath>
 +
- #include "compiler/translator/SymbolTable.h"
+ #    include "compiler/translator/SymbolTable.h"
  
- #include "angle_gl.h"
+ #    include "angle_gl.h"
