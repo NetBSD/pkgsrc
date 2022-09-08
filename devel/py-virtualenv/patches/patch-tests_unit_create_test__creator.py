de6e305158b2904288e574272960beb97d62abba
adam <adam@pkgsrc.org>
2022-09-08 07:46:09 +0000

    py-virtualenv: updated to 20.16.5
    v20.16.5 (2022-09-07)
    ---------------------
    
    Bugfixes - 20.16.5
    ~~~~~~~~~~~~~~~~~~
    - Do not turn echo off for subsequent commands in batch activators
      (``activate.bat`` and ``deactivate.bat``) - by :user:`pawelszramowski`.
    
    
    v20.16.4 (2022-08-29)
    ---------------------
    
    Bugfixes - 20.16.4
    ~~~~~~~~~~~~~~~~~~
    - Bump embed setuptools to ``65.3`` - by :user:`gaborbernat`.
    
    
    v20.16.3 (2022-08-04)
    ---------------------
    
    Bugfixes - 20.16.3
    ~~~~~~~~~~~~~~~~~~
    - Upgrade embedded pip to ``22.2.2`` from ``22.2.1`` and setuptools to ``63.4.1`` from ``63.2.0`` - by :user:`gaborbernat`.
    
    
    v20.16.2 (2022-07-27)
    ---------------------
    
    Bugfixes - 20.16.2
    ~~~~~~~~~~~~~~~~~~
    - Bump embeded pip from ``22.2`` to ``22.2.1`` - by :user:`gaborbernat`.
    
    
    v20.16.1 (2022-07-26)
    ---------------------
    
    Features - 20.16.1
    ~~~~~~~~~~~~~~~~~~
    - Update Nushell activation scripts to version 0.67 - by :user:`kubouch`.
    
    
    v20.16.0 (2022-07-25)
    ---------------------
    
    Features - 20.16.0
    ~~~~~~~~~~~~~~~~~~
    - Drop support for running under Python 2 (still can generate Python 2 environments) - by :user:`gaborbernat`.
    - Upgrade embedded pip to ``22.2`` from ``22.1.2`` and  setuptools to ``63.2.0`` from ``62.6.0`` -
      by :user:`gaborbernat`.

--- devel/py-virtualenv/patches/patch-tests_unit_create_test__creator.py
+++ devel/py-virtualenv/patches/patch-tests_unit_create_test__creator.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-tests_unit_create_test__creator.py,v 1.1 2022/05/09 22:08:10 wiz Exp $
+$NetBSD: patch-tests_unit_create_test__creator.py,v 1.2 2022/09/08 07:46:09 adam Exp $
 
 Fix testing, related to patch-src_virtualenv_create_via__global__ref_api.py.
 
---- tests/unit/create/test_creator.py.orig	2022-05-04 12:29:21.000000000 +0000
+--- tests/unit/create/test_creator.py.orig	2022-09-08 02:18:47.000000000 +0000
 +++ tests/unit/create/test_creator.py
-@@ -156,6 +156,7 @@ def test_create_no_seed(python, creator,
+@@ -153,6 +153,7 @@ def test_create_no_seed(python, creator,
      purelib = creator.purelib
-     patch_files = {purelib / "{}.{}".format("_virtualenv", i) for i in ("py", "pyc", "pth")}
+     patch_files = {purelib / f"{'_virtualenv'}.{i}" for i in ("py", "pyc", "pth")}
      patch_files.add(purelib / "__pycache__")
 +    patch_files.add(purelib / "_distutils_system_mod.py")
      content = set(creator.purelib.iterdir()) - patch_files
-     assert not content, "\n".join(ensure_text(str(i)) for i in content)
-     assert creator.env_name == ensure_text(dest.name)
+     assert not content, "\n".join(str(i) for i in content)
+     assert creator.env_name == str(dest.name)
