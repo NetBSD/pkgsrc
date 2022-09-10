bceb45f685e579bffc5b7335786e652d31835961
taca <taca@pkgsrc.org>
2022-09-10 08:24:40 +0000

    www/ruby-rails61: update to 6.1.7
    Ruby on Rails 6.1.7 release on 9th September 2022.
    Active Record and Active Storage are updated:
    
    
    Active Record
    
    * Symbol is allowed by default for YAML columns
    
      Étienne Barrié
    
    * Fix ActiveRecord::Store to serialize as a regular Hash
    
      Previously it would serialize as an
      ActiveSupport::HashWithIndifferentAccess which is wasteful and cause
      problem with YAML safe_load.
    
      Jean Boussier
    
    * Fix PG.connect keyword arguments deprecation warning on ruby 2.7
    
      Fixes .
    
      Nikita Vasilevsky
    
    
    Active Storage
    
    * Respect Active Record's primary_key_type in Active Storage
      migrations. Backported from 7.0.
    
      fatkodima

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.135 2022/09/10 08:18:59 taca Exp $
+# $NetBSD: rails.mk,v 1.136 2022/09/10 08:24:40 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -49,7 +49,7 @@ _RUBY_RAILS_MK=	# defined
 #
 RUBY_RAILS52_VERSION?=	5.2.8.1
 RUBY_RAILS60_VERSION?=	6.0.6
-RUBY_RAILS61_VERSION?=	6.1.6.1
+RUBY_RAILS61_VERSION?=	6.1.7
 RUBY_RAILS70_VERSION?=	7.0.3.1
 
 RUBY_RAILS_ACCEPTED?=	# empty
