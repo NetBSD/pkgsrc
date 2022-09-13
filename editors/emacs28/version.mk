023abec2342ec0495f5c93e2e41afa5840f70753
wiz <wiz@pkgsrc.org>
2022-09-13 23:36:13 +0000

    emacs28: update to 28.2.
    Emacs 28.2 is a bug-fix release, with no new features with respect to
    Emacs 28.1.

--- editors/emacs28/version.mk
+++ editors/emacs28/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.1 2022/04/06 22:40:38 wiz Exp $
+# $NetBSD: version.mk,v 1.2 2022/09/13 23:36:13 wiz Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs28>=28.1<29
 
 _EMACS_VERSION_MAJOR=	28
-_EMACS_VERSION_MINOR=	1
+_EMACS_VERSION_MINOR=	2
