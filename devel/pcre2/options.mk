1ae86c4ca00ff55c6651aee352ef4ac8835cf65c
wiz <wiz@pkgsrc.org>
2022-09-14 09:35:01 +0000

    pcre2: disable JIT by default on NetBSD
    See
    https://mail-index.netbsd.org/pkgsrc-users/2022/09/11/msg036184.html
    for more details.
    
    Bump PKGREVISION.

--- devel/pcre2/options.mk
+++ devel/pcre2/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2017/09/07 14:50:44 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2022/09/14 09:35:01 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pcre2
 PKG_SUPPORTED_OPTIONS=	pcre2-jit
+
+.include "../../mk/bsd.options.mk"
 
 .if (${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "x86_64" || \
@@ -9,10 +11,12 @@ PKG_SUPPORTED_OPTIONS=	pcre2-jit
     ${MACHINE_ARCH} == "powerpc" || \
     ${MACHINE_ARCH} == "sparc" || \
     (!empty(MACHINE_ARCH:M*arm*) && empty(MACHINE_ARCH:M*armv4*)))
+.if ${OPSYS} != "NetBSD"
+# causes core dumps in rspamd
+# https://mail-index.netbsd.org/pkgsrc-users/2022/09/11/msg036184.html
 PKG_SUGGESTED_OPTIONS=	pcre2-jit
 .endif
-
-.include "../../mk/bsd.options.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mpcre2-jit)
 CONFIGURE_ARGS+=	--enable-jit
