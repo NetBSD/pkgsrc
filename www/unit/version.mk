36213bc469bf4ae4fd87d8d88e1c27f68e469801
osa <osa@pkgsrc.org>
2022-09-14 05:31:41 +0000

    */*unit*: update NGINX Unit from 1.27.0 to 1.28.0
    <ChangeLog>
    
    *) Change: increased the applications' startup timeout.
    
    *) Change: disallowed abstract Unix domain socket syntax in non-Linux
       systems.
    
    *) Feature: basic statistics API.
    
    *) Feature: customizable access log format.
    
    *) Feature: more HTTP variables support.
    
    *) Feature: forwarded header to replace client address and protocol.
    
    *) Feature: ability to get dynamic variables.
    
    *) Feature: support for abstract Unix sockets.
    
    *) Feature: support for Unix sockets in address matching.
    
    *) Feature: the $dollar variable translates to a literal "$" during
       variable substitution.
    
    *) Bugfix: router process could crash if index file didn't contain an
       extension.
    
    *) Bugfix: force SCRIPT_NAME in Ruby to always be an empty string.
    
    *) Bugfix: when isolated PID numbers reach the prototype process host
       PID, the prototype crashed.
    
    *) Bugfix: the Ruby application process could crash on SIGTERM.
    
    *) Bugfix: the Ruby application process could crash on SIGINT.
    
    *) Bugfix: mutex leak in the C API.
    
    </ChangeLog>

--- www/unit/version.mk
+++ www/unit/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.5 2022/06/02 17:04:31 osa Exp $
+# $NetBSD: version.mk,v 1.6 2022/09/14 05:31:41 osa Exp $
 #
 # UNIT_EXTENSION_DIR
 #	Relative path to ${PREFIX} for NGINX Unit's modules.
@@ -12,7 +12,7 @@
 UNITVERSION_MK=	defined
 
 # Define NGINX Unit's version.
-UNIT_VERSION=	1.27.0
+UNIT_VERSION=	1.28.0
 
 # Define NGINX Unit's modules directory
 UNIT_EXTENSION_DIR=	libexec/unit/modules
