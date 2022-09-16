7d7a6f488607382b083fb0459bd7e1d68d7ce17c
adam <adam@pkgsrc.org>
2022-09-16 13:00:15 +0000

    py-uvloop: updated to 0.17.0
    v0.17.0
    
    This release adds Python 3.11 support, updates bundled libuv to 1.43.0
    and fixes a handful of issues.
    
    Changes
    
    Expose uv_loop_t pointer for integration with other C-extensions
    Support python 3.11+
    Expose libuv uv_fs_event functionality
    Activate debug mode when -X dev is used
    Expose uv_version() for libuv API compatibility
    Fix loop.getaddrinfo() and tests
    Bump to libuv 1.43.0
    
    Fixes
    
    _TransProtPair is no longer defined in asyncio.events
    use a TypeVar for asyncio.BaseProtocol
    Fix segfault in TimerHandle.when() after cleared
    Avoid self._errpipe_write double close
    Fix typo in test
    Fix potential infinite loop
    use a stack of self._fds_to_close to prevent double closes
    Fix incorrect main thread id value forking from a thread
    create_subprocess_exec should treat env={} as empty environment
    Queue write only after processing all buffers
    Drop Python 3.6 support for thread ident
    bugfix: write to another transport in resume_writing() fails
    
    Build
    
    Upgrade GitHub Actions
    typo same as same
    setup.py: allow to override extra_compile_args
    Drop hack in setup.py in finalize_options (492)
    Fix tests invocation on release CI worklow
    
    Documentation
    
    use asyncio.Runner loop_factory on 3.11+
    Fix CI badge in docs, remove remaining Travis CI references from docs
    Fix typo in README

--- devel/py-uvloop/patches/patch-setup.py
+++ devel/py-uvloop/patches/patch-setup.py
@@ -1,27 +1,29 @@
-$NetBSD: patch-setup.py,v 1.5 2022/01/27 10:06:38 wiz Exp $
+$NetBSD: patch-setup.py,v 1.6 2022/09/16 13:00:15 adam Exp $
 
 Relax restrictions.
 Use system libuv (--use-system-libuv option does not work for 'install').
 
---- setup.py.orig	2021-08-10 19:05:35.000000000 +0000
+--- setup.py.orig	2022-09-14 14:53:41.000000000 +0000
 +++ setup.py
-@@ -25,13 +25,9 @@ CYTHON_DEPENDENCY = 'Cython(>=0.29.24,<0
+@@ -25,15 +25,11 @@ CYTHON_DEPENDENCY = 'Cython(>=0.29.32,<0
  
  # Minimal dependencies required to test uvloop.
  TEST_DEPENDENCIES = [
 -    # pycodestyle is a dependency of flake8, but it must be frozen because
 -    # their combination breaks too often
 -    # (example breakage: https://gitlab.com/pycqa/flake8/issues/427)
-     'aiohttp',
+     # aiohttp doesn't support 3.11 yet,
+     # see https://github.com/aio-libs/aiohttp/issues/6600
+     'aiohttp ; python_version < "3.11"',
      'flake8~=3.9.2',
      'psutil',
 -    'pycodestyle~=2.7.0',
-     'pyOpenSSL~=19.0.0',
+     'pyOpenSSL~=22.0.0',
      'mypy>=0.800',
- ]
-@@ -121,7 +117,7 @@ class uvloop_build_ext(build_ext):
-             return
+     CYTHON_DEPENDENCY,
+@@ -118,7 +114,7 @@ class uvloop_build_ext(build_ext):
  
+     def initialize_options(self):
          super().initialize_options()
 -        self.use_system_libuv = False
 +        self.use_system_libuv = True
