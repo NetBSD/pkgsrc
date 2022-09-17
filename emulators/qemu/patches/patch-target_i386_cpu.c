fc0b661d063dfb0077f98201055963d9d0143540
tnn <tnn@pkgsrc.org>
2022-09-17 09:54:56 +0000

    qemu: update to 7.1.0
    This release includes initial support for LoongArch64 as well as CPU
    architecture and driver emulation improvements for ARM, HPPA, Nios2,
    OpenRISC, PowerPC, Renesas, s390, x86 and Xtensa.
    
    Incompatible changes
      The -enable-fips option to QEMU system emulators has been removed
      The -writeconfig option to QEMU system emulators has been removed
      The deprecated x86 CPU model Icelake-Client has been removed
      The deprecated properties loaded (for crypto objects) and opened
        (for RNG backends) are now read-only
      The deprecated -soundhw option has been replaced by -audio
        (e.g. -audio pa,model=hda)
      The deprecated -sdl and -curses have been removed, use -display sdl
        and -display curses instead.
      The -display sdl,window_close=... suboption has been removed, use
       -display sdl,window-close=... instead, i.e. with a minus instead of an
       underscore between “window” and “close”
      The -alt-grab option and the -display sdl,alt_grab=on suboption have been
        removed, use -display sdl,grab-mod=lshift-lctrl-lalt instead.
      The -ctrl-grab option and the -display sdl,ctrl_grab=on suboption have
        been removed, use -display sdl,grab-mod=rctrl instead.
      The x86 Icelake-Client CPU does not exist in the real world and has
        therefore been removed.

--- emulators/qemu/patches/patch-target_i386_cpu.c
+++ emulators/qemu/patches/patch-target_i386_cpu.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-target_i386_cpu.c,v 1.1 2022/06/12 13:49:28 leot Exp $
+$NetBSD: patch-target_i386_cpu.c,v 1.2 2022/09/17 09:54:56 tnn Exp $
 
 Permit to use `-cpu host' with NVMM.
 
---- target/i386/cpu.c.orig	2022-04-19 19:10:27.000000000 +0000
+--- target/i386/cpu.c.orig	2022-08-30 16:41:57.000000000 +0000
 +++ target/i386/cpu.c
 @@ -26,6 +26,7 @@
  #include "tcg/helper-tcg.h"
@@ -12,7 +12,7 @@ Permit to use `-cpu host' with NVMM.
  #include "kvm/kvm_i386.h"
  #include "sev.h"
  #include "qapi/error.h"
-@@ -1440,7 +1441,7 @@ static uint32_t xsave_area_size(uint64_t
+@@ -1486,7 +1487,7 @@ static uint32_t xsave_area_size(uint64_t
  
  static inline bool accel_uses_host_cpuid(void)
  {
@@ -20,8 +20,8 @@ Permit to use `-cpu host' with NVMM.
 +    return kvm_enabled() || hvf_enabled() || nvmm_enabled();
  }
  
- static inline uint64_t x86_cpu_xsave_components(X86CPU *cpu)
-@@ -6418,7 +6419,8 @@ static void x86_cpu_realizefn(DeviceStat
+ static inline uint64_t x86_cpu_xsave_xcr0_components(X86CPU *cpu)
+@@ -6476,7 +6477,8 @@ static void x86_cpu_realizefn(DeviceStat
  
      if (xcc->host_cpuid_required && !accel_uses_host_cpuid()) {
          g_autofree char *name = x86_cpu_class_get_model_name(xcc);
