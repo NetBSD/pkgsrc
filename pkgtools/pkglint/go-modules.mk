7142250dbf5e5de122957732f317e54070b11fc4
bsiegert <bsiegert@pkgsrc.org>
2022-09-17 15:50:39 +0000

    pkglint: use Go module build, simplify Makefile
    Add a go.mod file to the sources and pull in goyacc through a TOOL_DEPENDS.
    Bump version to 22.2.7.

---
+++ pkgtools/pkglint/go-modules.mk
@@ -0,0 +1,13 @@
+# $NetBSD: go-modules.mk,v 1.1 2022/09/17 15:50:39 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.mod
+GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.zip
+GO_MODULE_FILES+=	github.com/kr/pty/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220829220503-c86fa9a7ed90.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220829220503-c86fa9a7ed90.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.mod
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.zip
