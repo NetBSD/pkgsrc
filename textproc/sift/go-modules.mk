631fcef3d2002dd09c7cd6c9c237adfda176478b
bsiegert <bsiegert@pkgsrc.org>
2022-09-18 11:54:51 +0000

    sift: update to 0.9.0
    Also turn this into a Go module build because it's 2022.
    
    Version 0.9.0 introduces several new options/features:
    
    * --conf
    * --field-sep
    * --byte-offset
    * Support for custom types
    
    A detailed description can be found in this blog post:
    https://svent.github.io/2016/10/23/sift-version-0.9-released/
    
    This version also removes the cgo dependency while improving the
    performance for line counting and case conversions.

---
+++ textproc/sift/go-modules.mk
@@ -0,0 +1,13 @@
+# $NetBSD: go-modules.mk,v 1.1 2022/09/18 11:54:51 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/svent/go-flags/@v/v0.0.0-20141123140740-4bcbad344f03.mod
+GO_MODULE_FILES+=	github.com/svent/go-flags/@v/v0.0.0-20141123140740-4bcbad344f03.zip
+GO_MODULE_FILES+=	github.com/svent/go-nbreader/@v/v0.0.0-20150201200112-7cef48da76dc.mod
+GO_MODULE_FILES+=	github.com/svent/go-nbreader/@v/v0.0.0-20150201200112-7cef48da76dc.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220829220503-c86fa9a7ed90.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220829220503-c86fa9a7ed90.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20201119102817-f84b799fce68.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.zip
