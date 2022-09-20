0230de92ac7c3a05be6e050b543aa5e852f3860c
he <he@pkgsrc.org>
2022-09-20 15:59:33 +0000

    lang/mono: fix NetBSD/powerpc support
     * Add a cast which is needed
     * Simplify one other macro
    
    Fixes build on NetBSD/macppc.

--- lang/mono/patches/patch-mono_utils_mono-sigcontext.h
+++ lang/mono/patches/patch-mono_utils_mono-sigcontext.h
@@ -1,10 +1,22 @@
-$NetBSD: patch-mono_utils_mono-sigcontext.h,v 1.2 2021/03/03 12:05:41 nia Exp $
+$NetBSD: patch-mono_utils_mono-sigcontext.h,v 1.3 2022/09/20 15:59:33 he Exp $
 
 NetBSD/aarch64 support.
+Fix NetBSD/powerpc support.
 
 --- mono/utils/mono-sigcontext.h.orig	2019-09-19 07:46:07.000000000 +0000
 +++ mono/utils/mono-sigcontext.h
-@@ -474,6 +474,12 @@ typedef struct ucontext {
+@@ -356,8 +356,8 @@ typedef struct ucontext {
+ 
+ 	#define UCONTEXT_REG_Rn(ctx, n)   (((os_ucontext*)(ctx))->uc_mcontext.__gregs [(n)])
+ 	#define UCONTEXT_REG_FPRn(ctx, n) (((os_ucontext*)(ctx))->uc_mcontext.__fpregs.__fpu_regs [(n)])
+-	#define UCONTEXT_REG_NIP(ctx)     _UC_MACHINE_PC(ctx)
+-	#define UCONTEXT_REG_LNK(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.__gregs [_REG_LR])
++	#define UCONTEXT_REG_NIP(ctx)     _UC_MACHINE_PC((os_ucontext*)(ctx))
++	#define UCONTEXT_REG_LNK(ctx)     UCONTEXT_REG_Rn(ctx, _REG_LR)
+ #elif defined(__FreeBSD__)
+ 	typedef ucontext_t os_ucontext;
+ 
+@@ -472,6 +472,12 @@ typedef struct ucontext {
  	#define UCONTEXT_REG_SP(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_sp)
  	#define UCONTEXT_REG_R0(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x [ARMREG_R0])
  	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x))
