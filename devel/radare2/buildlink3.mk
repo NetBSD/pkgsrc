bc4d3fe7f003c48b91a07525522a6656f5872d41
ast <ast@pkgsrc.org>
2022-09-21 13:49:39 +0000

    devel/radare2: Update to 5.7.8 (codename "boredom") from 5.1.1 ("lasagna") and fix build on pkgsrc / NetBSD 9.x.
    5.1.1->5.7.8 skips a handful of bug-fixing and refinement releases
    that happened between Feb 2021 and Sep 2022. Way too many changes
    to list here - see https://github.com/radareorg/radare2/releases
    
    OK-ed by wiz during freeze.

--- devel/radare2/buildlink3.mk
+++ devel/radare2/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2022/06/28 11:33:42 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2022/09/21 13:49:39 ast Exp $
 
 BUILDLINK_TREE+=	radare2
 
 .if !defined(RADARE2_BUILDLINK3_MK)
 RADARE2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.radare2+=	radare2>=5.1.0
-BUILDLINK_ABI_DEPENDS.radare2+=	radare2>=5.1.1nb5
+BUILDLINK_API_DEPENDS.radare2+=	radare2>=5.7.8
 BUILDLINK_PKGSRCDIR.radare2?=	../../devel/radare2
 
 .include "../../devel/capstone/buildlink3.mk"
