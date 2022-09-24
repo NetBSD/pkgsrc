0cd2f90cd990b3132109fd6ad689e52984349e1d
wiz <wiz@pkgsrc.org>
2022-09-24 06:30:43 +0000

    gelemental: update to 2.0.1.
    Fixes build.
    
    Version 2.0.1 [stable]
    ======================
    
    This is a maintenance release of gElemental which is C++17 compatible and
    forces button colours for several modern themes.
    
    Version 2.0.0 [stable]
    ----------------------
    
    This release of gElemental reflects the transfer to a new maintainer.
    It includes several bugfixes from the Debichem Team, some minor interface
    improvements, and the addition of new element names.

--- misc/gelemental/patches/patch-libelemental_value.cc
+++ misc/gelemental/patches/patch-libelemental_value.cc
@@ -1,32 +1,16 @@
-$NetBSD: patch-libelemental_value.cc,v 1.1 2015/07/03 12:46:10 joerg Exp $
+$NetBSD: patch-libelemental_value.cc,v 1.2 2022/09/24 06:30:44 wiz Exp $
 
---- libelemental/value.cc.orig	2015-06-27 14:24:42.000000000 +0000
+Fix build with pango 1.50.
+https://github.com/ginggs/gelemental/commit/689d343086ca151ecc4c4a407c1650bc64c7c931
+
+--- libelemental/value.cc.orig	2021-09-26 09:22:33.000000000 +0000
 +++ libelemental/value.cc
-@@ -23,7 +23,6 @@
- #include <libelemental/value.hh>
- #include <libelemental/value-types.hh>
+@@ -25,7 +25,7 @@
  
--#include <ext/stdio_filebuf.h>
+ #include <ext/stdio_filebuf.h>
  #include <glibmm/utility.h>
- #include <pango/pango-attributes.h>
+-#include <pango/pango-attributes.h>
++#include <pango/pango.h>
  
-@@ -130,19 +129,6 @@ EntriesView::max_name_length = 0;
- //******************************************************************************
- // class EntriesStream
+ namespace Elemental {
  
--
--EntriesStream::EntriesStream (FILE* file) throw ()
--:	buf (new __gnu_cxx::stdio_filebuf<char> (file, std::ios_base::out)),
--	buf_owned (true), os (*new std::ostream (buf)), os_owned (true)
--{}
--
--
--EntriesStream::EntriesStream (int fd) throw ()
--:	buf (new __gnu_cxx::stdio_filebuf<char> (fd, std::ios_base::out)),
--	buf_owned (true), os (*new std::ostream (buf)), os_owned (true)
--{}
--
--
- EntriesStream::EntriesStream (std::streambuf& buf_) throw ()
- :	buf (&buf_), buf_owned (false),
- 	os (*new std::ostream (buf)), os_owned (true)
