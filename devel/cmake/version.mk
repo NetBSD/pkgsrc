d880d551b9c22e4ab60603af2c113f892b0461ab
adam <adam@pkgsrc.org>
2022-09-26 13:37:32 +0000

    cmake: updated to 3.24.2
    CMake 3.24.2
    * automoc: avoid more compiler warnings in linker-warning-silencing code
    * ExternalProject: note the default of `GIT_TAG` being `master`
    * Help: Clarify that variable references may use cache entries
    * ci: use CMake 3.24.1
    * gitlab-ci: Use separate MSVC toolset specification for packaging jobs
    * gitlab-ci: Update non-packaging Windows builds to MSVC 14.33 toolset
    * Tests: Teach RunCMake.SymlinkTrees to tolerate CCACHE_BASEDIR
    * automoc: revert attempts to silence linker warning on macos
    * Tests: Add cases covering bad ctest output truncation types
    * Help: Add missing closing quote on C++ example
    * Help: Add crossrefs to ctest output control options
    * Help: Fix typos, grammar and formatting in CMP0134 policy docs
    * Help: Fix wrong casing of GTest in FetchContent integration example
    * FetchContent: Fix unsetting wrong variable name after provider returns
    * FetchContent: Ignore EXACT for redirected find_package() calls
    * FindZLIB: fix CMAKE_FIND_LIBRARY_PREFIXES being unset when it was empty
    * RunCMakeTest: fix Truncation test definition
    * ctest: print error for invalid CTEST_CUSTOM_TEST_OUTPUT_TRUNCATION
    * FortranCInterface: Add support for LLVMFlang mangling
    * FindBoost: Add support for Boost 1.80
    * Tests: Fix VS10Project SourceGroupTreeCMakeLists check
    * FILE_SET: Fix source group detection
    * Xcode: Fix erroneous MACOSX_BUNDLE link
    * Help: cmake-developer: Add section for Windows registry access.
    * renames method FinalizeTargetCompileInfo() in FinalizeTargetConfiguration().
    * Check link libraries properties: fix performances regression
    * Help: Fix typo in FetchContent example, extras should read extra
    * FindCUDAToolkit: Search the cuda toolkit include path for cupti
    * cmFindBase: Refactor CMAKE_FIND_USE_INSTALL_PREFIX handling
    * CMAKE_FIND_USE_INSTALL_PREFIX considers CMAKE_STAGING_PREFIX
    * FindCUDAToolkit: Correctly state cusolver and cublas dependencies
    * FindPostgreSQL: Add brew-style directories to search path

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.29 2022/08/22 19:08:10 adam Exp $
+# $NetBSD: version.mk,v 1.30 2022/09/26 13:37:32 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.24.1
+CMAKE_VERSION=	3.24.2
 CMAKE_API=	${CMAKE_VERSION:R}
