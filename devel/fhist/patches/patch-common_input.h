4c84aea9be556014dbede020aa31246ad847ffe2
nros <nros@pkgsrc.org>
2022-09-26 10:21:32 +0000

    fhist: unbrake build and fix a bug in usage of iswctype. Set LICENSE.

--- devel/fhist/patches/patch-common_input.h
+++ devel/fhist/patches/patch-common_input.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-common_input.h,v 1.1 2013/02/26 10:25:38 joerg Exp $
+$NetBSD: patch-common_input.h,v 1.2 2022/09/26 10:21:32 nros Exp $
+
+* remove redefinitions of functions since it conflicts with c99
 
 --- common/input.h.orig	2013-02-25 17:15:19.000000000 +0000
 +++ common/input.h
@@ -7,7 +9,7 @@ $NetBSD: patch-common_input.h,v 1.1 2013/02/26 10:25:38 joerg Exp $
  struct string_ty *input_one_line(input_ty *);
  
 -#ifdef __GNUC__
-+#if defined(__GNUC__) && !defined(__clang__)
++#if 0
  extern __inline long input_read(input_ty *fp, void *data, long len)
  	{ if (len <= 0) return 0; if (fp->pushback_len > 0) {
  	fp->pushback_len--; *(char *)data = fp->pushback_buf[
