57b21bf5dee4265bf6b928c1f5b4f0012cc2f9d2
gdt <gdt@pkgsrc.org>
2022-09-27 01:20:39 +0000

    zoneminder: Update to 1.30.5
    Welcome to 2016.
    
    MESSAGE content has been moved to an installed README and improved.
    
    Prepared in wip by dsainty@ with some help from gdt@.
    
    Upstream NEWS
    
    Onvif discovery is now enabled by default. This features increases
    build time, but the benefit is auto-discovery of any Onvif compliant
    camera. Please note that this feature is still experimental. It has
    not been tested in a large environment or with a large variety of
    cameras. Using this tool is largely self explanatory, but if you are
    looking for documentation it can be found on the @altaroca ’s blog
    site. As time allows, we will bring the documentation over to our
    readthedocs site. Volunteers to do this work are welcome.
    
    ZoneMinder now has a telemetry deamon which collects information about
    your machine. This was done to learn how our user base uses
    ZoneMinder, the most common system size, choice of distro, cpu,
    memory, and a couple of other things. This will help us focus our
    efforts on what features get used the most. Since this was written in
    Perl, you can easily see for yourself what information is being
    sent. Note that this feature defaults to ON. If this is not what you
    want, then you can turn it off under Options. In a future release, we
    plan to include an install wizard, which will prompt the user about
    this feature.
    
    Have you ever wished for the motion detection zones to be live, rather
    than a static image? Well they are now! Creating or modifying zones
    works exactly the same as before, but with the live stream running in
    the background. If you need to freeze the stream, there is a Pause
    button at the bottom of the window to do just that.
    
    A new feature called Montage Review has been added. This can be
    thought of as a modified version of the original montage feature with
    features such as "scrubbing" through video. The Montage Review screen
    has received sufficient testing to be part of the release, but you
    should still treat this feature as experimental. We expect there to be
    additional fixes and refinements.

--- security/zoneminder/patches/patch-src_zmu.cpp
+++ security/zoneminder/patches/patch-src_zmu.cpp
@@ -1,54 +1,54 @@
-$NetBSD: patch-src_zmu.cpp,v 1.3 2020/08/25 16:42:21 gdt Exp $
+$NetBSD: patch-src_zmu.cpp,v 1.4 2022/09/27 01:20:39 gdt Exp $
 
 time types
 
---- src/zmu.cpp.orig	2016-02-03 18:40:30.000000000 +0000
+--- src/zmu.cpp.orig	2019-02-22 15:38:47.000000000 +0000
 +++ src/zmu.cpp
 @@ -519,14 +519,14 @@ int main( int argc, char *argv[] )
- 					if ( timestamp.tv_sec )
- 						strftime( timestamp_str, sizeof(timestamp_str), "%Y-%m-%d %H:%M:%S", localtime( &timestamp.tv_sec ) );
- 					if ( image_idx == -1 )
--						printf( "Time of last image capture: %s.%02ld\n", timestamp_str, timestamp.tv_usec/10000 );
-+						printf( "Time of last image capture: %s.%02ld\n", timestamp_str, (long) timestamp.tv_usec/10000 );
- 					else
--						printf( "Time of image %d capture: %s.%02ld\n", image_idx, timestamp_str, timestamp.tv_usec/10000 );
-+						printf( "Time of image %d capture: %s.%02ld\n", image_idx, timestamp_str, (long) timestamp.tv_usec/10000 );
- 				}
- 				else
- 				{
- 					if ( have_output ) printf( "%c", separator );
--					printf( "%ld.%02ld", timestamp.tv_sec, timestamp.tv_usec/10000 );
-+					printf( "%jd.%02ld", (intmax_t) timestamp.tv_sec, (long) timestamp.tv_usec/10000 );
- 					have_output = true;
- 				}
- 			}
-@@ -793,12 +793,12 @@ int main( int argc, char *argv[] )
- 						if ( monitor && monitor->connect() )
- 						{
- 							struct timeval tv = monitor->GetTimestamp();
--							printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",
-+							printf( "%4d%5d%6d%9d%11jd.%02ld%6d%6d%8d%8.2f\n",
- 								monitor->Id(),
- 								function,
- 								monitor->GetState(),
- 								monitor->GetTriggerState(),
--								tv.tv_sec, tv.tv_usec/10000,
-+								(intmax_t) tv.tv_sec, (long) tv.tv_usec/10000,
- 								monitor->GetLastReadIndex(),
- 								monitor->GetLastWriteIndex(),
- 								monitor->GetLastEvent(),
-@@ -810,12 +810,12 @@ int main( int argc, char *argv[] )
- 					else
- 					{
- 						struct timeval tv = { 0, 0 };
--						printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",
-+						printf( "%4d%5d%6d%9d%11jd.%02ld%6d%6d%8d%8.2f\n",
- 							mon_id,
- 							function,
- 							0,
- 							0,
--							tv.tv_sec, tv.tv_usec/10000,
-+							(intmax_t) tv.tv_sec, (long) tv.tv_usec/10000,
- 							0,
- 							0,
- 							0,
+           if ( timestamp.tv_sec )
+             strftime( timestamp_str, sizeof(timestamp_str), "%Y-%m-%d %H:%M:%S", localtime( &timestamp.tv_sec ) );
+           if ( image_idx == -1 )
+-            printf( "Time of last image capture: %s.%02ld\n", timestamp_str, timestamp.tv_usec/10000 );
++            printf( "Time of last image capture: %s.%02ld\n", timestamp_str, (long) timestamp.tv_usec/10000 );
+           else
+-            printf( "Time of image %d capture: %s.%02ld\n", image_idx, timestamp_str, timestamp.tv_usec/10000 );
++            printf( "Time of image %d capture: %s.%02ld\n", image_idx, timestamp_str, (long) timestamp.tv_usec/10000 );
+         }
+         else
+         {
+           if ( have_output ) printf( "%c", separator );
+-          printf( "%ld.%02ld", timestamp.tv_sec, timestamp.tv_usec/10000 );
++          printf( "%jd.%02ld", (intmax_t) timestamp.tv_sec, (long) timestamp.tv_usec/10000 );
+           have_output = true;
+         }
+       }
+@@ -792,12 +792,12 @@ int main( int argc, char *argv[] )
+             if ( monitor && monitor->connect() )
+             {
+               struct timeval tv = monitor->GetTimestamp();
+-              printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",
++              printf( "%4d%5d%6d%9d%11jd.%02ld%6d%6d%8d%8.2f\n",
+                 monitor->Id(),
+                 function,
+                 monitor->GetState(),
+                 monitor->GetTriggerState(),
+-                tv.tv_sec, tv.tv_usec/10000,
++		(intmax_t) tv.tv_sec, (long) tv.tv_usec/10000,
+                 monitor->GetLastReadIndex(),
+                 monitor->GetLastWriteIndex(),
+                 monitor->GetLastEvent(),
+@@ -809,12 +809,12 @@ int main( int argc, char *argv[] )
+           else
+           {
+             struct timeval tv = { 0, 0 };
+-            printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",
++            printf( "%4d%5d%6d%9d%11jd.%02ld%6d%6d%8d%8.2f\n",
+               mon_id,
+               function,
+               0,
+               0,
+-              tv.tv_sec, tv.tv_usec/10000,
++	      (intmax_t) tv.tv_sec, (long) tv.tv_usec/10000,
+               0,
+               0,
+               0,
