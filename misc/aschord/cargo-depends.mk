f584e5058ee703c9d9b6d65f1c0e0a1df3ac1869
pin <pin@pkgsrc.org>
2022-09-27 07:48:43 +0000

    misc/aschord: import package
    Command line tool for showing how to play guitar chords.

---
+++ misc/aschord/cargo-depends.mk
@@ -0,0 +1,29 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2022/09/27 07:48:43 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	atty-0.2.14
+CARGO_CRATE_DEPENDS+=	autocfg-1.1.0
+CARGO_CRATE_DEPENDS+=	bitflags-1.3.2
+CARGO_CRATE_DEPENDS+=	clap-3.2.21
+CARGO_CRATE_DEPENDS+=	clap_derive-3.2.18
+CARGO_CRATE_DEPENDS+=	clap_lex-0.2.4
+CARGO_CRATE_DEPENDS+=	hashbrown-0.12.3
+CARGO_CRATE_DEPENDS+=	heck-0.4.0
+CARGO_CRATE_DEPENDS+=	hermit-abi-0.1.19
+CARGO_CRATE_DEPENDS+=	indexmap-1.9.1
+CARGO_CRATE_DEPENDS+=	libc-0.2.132
+CARGO_CRATE_DEPENDS+=	once_cell-1.14.0
+CARGO_CRATE_DEPENDS+=	os_str_bytes-6.3.0
+CARGO_CRATE_DEPENDS+=	proc-macro-error-1.0.4
+CARGO_CRATE_DEPENDS+=	proc-macro-error-attr-1.0.4
+CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.43
+CARGO_CRATE_DEPENDS+=	quote-1.0.21
+CARGO_CRATE_DEPENDS+=	strsim-0.10.0
+CARGO_CRATE_DEPENDS+=	syn-1.0.99
+CARGO_CRATE_DEPENDS+=	termcolor-1.1.3
+CARGO_CRATE_DEPENDS+=	textwrap-0.15.1
+CARGO_CRATE_DEPENDS+=	unicode-ident-1.0.4
+CARGO_CRATE_DEPENDS+=	version_check-0.9.4
+CARGO_CRATE_DEPENDS+=	winapi-0.3.9
+CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
+CARGO_CRATE_DEPENDS+=	winapi-util-0.1.5
+CARGO_CRATE_DEPENDS+=	winapi-x86_64-pc-windows-gnu-0.4.0
