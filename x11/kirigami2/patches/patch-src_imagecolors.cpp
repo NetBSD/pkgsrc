31bb2e3b771997af9dd7f7366ecbdd762f4f6492
markd <markd@pkgsrc.org>
2022-09-29 19:46:40 +0000

    kirigami2: and the modified patch to go along with previous

--- x11/kirigami2/patches/patch-src_imagecolors.cpp
+++ x11/kirigami2/patches/patch-src_imagecolors.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_imagecolors.cpp,v 1.4 2022/04/25 17:58:29 tnn Exp $
+$NetBSD: patch-src_imagecolors.cpp,v 1.5 2022/09/29 19:46:40 markd Exp $
 
 Explicitly use std::pow.
 
---- src/imagecolors.cpp.orig	2022-04-02 10:23:09.000000000 +0000
+--- src/imagecolors.cpp.orig	2022-09-05 12:09:26.000000000 +0000
 +++ src/imagecolors.cpp
-@@ -26,6 +26,8 @@
- #include "loggingcategory.h"
+@@ -27,6 +27,8 @@
  #include <cmath>
+ #include <vector>
  
 +using std::pow;
 +
