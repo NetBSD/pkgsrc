0a4556a0b91c4648f7647f77791d0238f6d535bf
markd <markd@pkgsrc.org>
2022-09-29 00:53:10 +0000

    KDE Gear update to 22.08.1
    16 months of development

---
+++ graphics/ksanecore/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/09/29 00:53:12 markd Exp $
+
+BUILDLINK_TREE+=	ksanecore
+
+.if !defined(KSANECORE_BUILDLINK3_MK)
+KSANECORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ksanecore+=	ksanecore>=22.08.0
+BUILDLINK_PKGSRCDIR.ksanecore?=	../../graphics/ksanecore
+
+.include "../../devel/ki18n/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KSANECORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ksanecore
