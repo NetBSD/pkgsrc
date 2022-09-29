5ca858ebb7fcbfd679681078043f3388ac5b29c8
markd <markd@pkgsrc.org>
2022-09-29 00:06:48 +0000

    mlt: update to 7.8.0
    7.8.0
    This highlight of this version is a new glaxnimate producer to render 2D vector
    art and animation.
    
    7.6.0
    This version adds image slice-threading to many filters and full support for
    full range color. All inputs are normalized to and processed at the range
    specified by the consumer property color_range that defaults to tv/mpeg (limited).
    
    7.4.0
    This main highlight of this version is property animation for avfilter!
    
    7.2.0
    This is the first major maintenance release for the new major version 7
    rendering it much more production ready. Plus there are a few nice new features.

--- multimedia/mlt/buildlink3.mk
+++ multimedia/mlt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/03/30 19:42:53 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/09/29 00:06:48 markd Exp $
 
 BUILDLINK_TREE+=	mlt
 
 .if !defined(MLT_BUILDLINK3_MK)
 MLT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mlt+=	mlt>=0.9.0
-BUILDLINK_ABI_DEPENDS.mlt+=	mlt>=0.9.0
+BUILDLINK_API_DEPENDS.mlt+=	mlt>=7.8.0
+BUILDLINK_ABI_DEPENDS.mlt+=	mlt>=7.8.0
 BUILDLINK_PKGSRCDIR.mlt?=	../../multimedia/mlt
 .endif # MLT_BUILDLINK3_MK
 
