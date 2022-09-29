6770cc139f819bd2a211b435adfc02059ef39bce
adam <adam@pkgsrc.org>
2022-09-29 09:26:15 +0000

    fs-uae-launcher: updated to 3.1.68
    3.1.64
    Fix Linux distribution by removing unwanted stuff from PyQt5
    … fontconfig/freetype
    * Deleted other workflows
    * Removed unwanted stuff from PyQt5
    * Remove libfontconfig and libfreetype also
    * Revert "Deleted other workflows"
    
    3.1.63
    Removed cacert.pem (should be handled by certifi package now)
    
    3.1.26
    Updated fsbuild, added macOS10.9 target
    * Updated fsbuild, added macOS10.9 target
    * Correct bundle.sh for stable branch
    * Install appdmg
    * macOS: Sign with both SHA-1 and SHA-256 hashes
    * macOS10.9 - add pyobjc
    * Updated workflow
    * More build fixes
    * Separate workflows
    * Don't specify Python 3.9
    * Updated version

--- emulators/fs-uae-launcher/patches/patch-setup.py
+++ emulators/fs-uae-launcher/patches/patch-setup.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-setup.py,v 1.5 2020/05/23 09:12:58 adam Exp $
+$NetBSD: patch-setup.py,v 1.6 2022/09/29 09:26:15 adam Exp $
 
 Fix installation on Darwin.
 
---- setup.py.orig	2017-01-08 19:48:56.000000000 +0000
+--- setup.py.orig	2022-01-26 18:26:58.000000000 +0000
 +++ setup.py
-@@ -180,10 +180,6 @@ if sys.argv[1] == "build_exe":
+@@ -178,10 +178,6 @@ if sys.argv[1] == "build_exe":
  if sys.platform == "win32" and False:
      setup_kwargs["windows"] = scripts
  
 -if sys.platform == "darwin":
 -    setup_kwargs["name"] = title
--    setup_kwargs["version"] = "3.0.5"
+-    setup_kwargs["version"] = "3.1.68"
 -else:
 -    setup_kwargs["scripts"] = scripts
 +setup_kwargs["scripts"] = scripts
