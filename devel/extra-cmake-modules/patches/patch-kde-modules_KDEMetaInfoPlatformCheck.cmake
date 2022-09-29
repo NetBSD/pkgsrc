85690c4851cfb6f1120defab4e141995e25c3c42
markd <markd@pkgsrc.org>
2022-09-29 00:36:30 +0000

    KDE frameworks update to 5.98.0
    5 months of development.

--- devel/extra-cmake-modules/patches/patch-kde-modules_KDEMetaInfoPlatformCheck.cmake
+++ devel/extra-cmake-modules/patches/patch-kde-modules_KDEMetaInfoPlatformCheck.cmake
@@ -1,8 +1,8 @@
-$NetBSD: patch-kde-modules_KDEMetaInfoPlatformCheck.cmake,v 1.1 2022/04/25 03:04:59 markd Exp $
+$NetBSD: patch-kde-modules_KDEMetaInfoPlatformCheck.cmake,v 1.2 2022/09/29 00:36:30 markd Exp $
 
 Add NetBSD to supported platforms
 
---- kde-modules/KDEMetaInfoPlatformCheck.cmake.orig	2022-03-30 13:04:06.000000000 +0000
+--- kde-modules/KDEMetaInfoPlatformCheck.cmake.orig	2022-08-27 09:14:28.000000000 +0000
 +++ kde-modules/KDEMetaInfoPlatformCheck.cmake
 @@ -38,6 +38,10 @@ if (NOT "${KF_IGNORE_PLATFORM_CHECK}")
                  if (CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
@@ -12,6 +12,6 @@ Add NetBSD to supported platforms
 +                if (CMAKE_SYSTEM_NAME MATCHES "NetBSD")
 +                    set(_MetainfoFoundSupportedPlatform true)
 +                endif()
-             elseif (${CMAKE_MATCH_1} STREQUAL "Windows")
-                 if (WIN32)
+             elseif (${CMAKE_MATCH_1} STREQUAL "OpenBSD")
+                 if (CMAKE_SYSTEM_NAME MATCHES "OpenBSD")
                      set(_MetainfoFoundSupportedPlatform true)
