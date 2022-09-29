7fc6506a3ed3c740a7fe490b036209ffa02761c5
markd <markd@pkgsrc.org>
2022-09-29 00:47:27 +0000

    KDE Gear update to 22.08.1
    16 months of development

--- games/ksudoku/patches/patch-src_gui_views_ArcBall.h
+++ games/ksudoku/patches/patch-src_gui_views_ArcBall.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_gui_views_ArcBall.h,v 1.1 2021/04/07 12:28:22 markd Exp $
+$NetBSD: patch-src_gui_views_ArcBall.h,v 1.2 2022/09/29 00:47:31 markd Exp $
 
 Not finding NULL on NetBSD9
 
---- src/gui/views/ArcBall.h.orig	2021-02-28 22:44:32.000000000 +0000
+--- src/gui/views/ArcBall.h.orig	2022-08-17 10:30:44.000000000 +0000
 +++ src/gui/views/ArcBall.h
-@@ -20,6 +20,10 @@
- #ifndef _ArcBall_h
- #define _ArcBall_h
+@@ -34,6 +34,10 @@
+ #include <GL/glu.h>                                                                                            // Header File For The GLu32 Library
+ #endif
  
 +#ifdef __NetBSD__
 +#include <sys/null.h>
 +#endif
 +
- #include "math.h"                                               // Needed for sqrtf
+ #include <math.h>                                               // Needed for sqrtf
  
  // 8<--Snip here if you have your own math types/funcs-->8 
