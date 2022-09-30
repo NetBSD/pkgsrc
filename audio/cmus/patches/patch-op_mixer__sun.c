8d2d00281e3b818aa83ab33138478369906ef13a
triaxx <triaxx@pkgsrc.org>
2022-09-30 13:07:09 +0000

    cmus: Update to 2.10.0
    upstream changes:
    -----------------
      * https://github.com/cmus/cmus/releases/tag/v2.10.0

--- audio/cmus/patches/patch-op_mixer__sun.c
+++ audio/cmus/patches/patch-op_mixer__sun.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-op_mixer__sun.c,v 1.1 2019/02/11 10:07:37 nia Exp $
+$NetBSD: patch-op_mixer__sun.c,v 1.2 2022/09/30 13:07:09 triaxx Exp $
 
 Fix build on SunOS.
 
 --- op/mixer_sun.c.orig	2011-11-30 09:57:20.000000000 +0100
 +++ op/mixer_sun.c	2012-02-15 21:12:05.691072986 +0100
-@@ -55,6 +55,7 @@ static int sun_mixer_get_option(int, cha
+@@ -52,6 +52,7 @@ static int sun_mixer_get_option(int, cha
  
  static int mixer_open(const char *dev)
  {
@@ -12,7 +12,7 @@ Fix build on SunOS.
  	struct mixer_devinfo minf;
  	int output_class;
  
-@@ -100,6 +101,7 @@ static int mixer_open(const char *dev)
+@@ -97,6 +98,7 @@ static int mixer_open(const char *dev)
  
  	if (sun_mixer_volume_delta == 0)
  		sun_mixer_volume_delta = 1;
@@ -20,7 +20,7 @@ Fix build on SunOS.
  
  	return 0;
  
-@@ -192,6 +194,7 @@ static int sun_mixer_close(void)
+@@ -189,6 +191,7 @@ static int sun_mixer_close(void)
  
  static int sun_mixer_set_volume(int l, int r)
  {
@@ -28,7 +28,7 @@ Fix build on SunOS.
  	struct mixer_ctrl minf;
  	int ovall, ovalr;
  
-@@ -215,12 +218,14 @@ static int sun_mixer_set_volume(int l, i
+@@ -212,12 +215,14 @@ static int sun_mixer_set_volume(int l, i
  
  	if (ioctl(mixer_fd, AUDIO_MIXER_WRITE, &minf) == -1)
  		return -1;
@@ -43,7 +43,7 @@ Fix build on SunOS.
  	struct mixer_ctrl minf;
  
  	minf.dev = sun_mixer_device_id;
-@@ -237,6 +242,7 @@ static int sun_mixer_get_volume(int *l, 
+@@ -234,6 +239,7 @@ static int sun_mixer_get_volume(int *l, 
  		*l = minf.un.value.level[AUDIO_MIXER_LEVEL_LEFT];
  		*r = minf.un.value.level[AUDIO_MIXER_LEVEL_RIGHT];
  	}
