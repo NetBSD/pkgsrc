7b72a68046b753c7db5182aea8a763b72b7f39ed
wiz <wiz@pkgsrc.org>
2022-10-01 15:08:26 +0000

    suse131_gtk3: fix comment, pkglint

--- emulators/suse131_gtk3/suse.x86_64.mk
+++ emulators/suse131_gtk3/suse.x86_64.mk
@@ -1,5 +1,5 @@
-# $NetBSD: suse.x86_64.mk,v 1.1 2021/11/25 15:39:48 ryoon Exp $
+# $NetBSD: suse.x86_64.mk,v 1.2 2022/10/01 15:08:26 wiz Exp $
 
-# Suffix on the GTK+-2.x/Pango binaries used in the INSTALL script
+# Suffix on the GTK+-3.x binaries used in the INSTALL script
 ABISUFFIX=	64
 BINSUFFIX=	-${ABISUFFIX}
