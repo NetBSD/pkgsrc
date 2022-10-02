5e00009cfd00cb787acffa402f5661341b612953
wiz <wiz@pkgsrc.org>
2022-10-02 17:17:16 +0000

    suse15_*: add many packages based on OpenSUSE 15.5

---
+++ emulators/suse15_glib2/suse.x86_64.mk
@@ -0,0 +1,5 @@
+# $NetBSD: suse.x86_64.mk,v 1.1 2022/10/02 17:17:18 wiz Exp $
+
+# Suffix on the GLib2 binaries used in the INSTALL script
+ABISUFFIX=	64
+BINSUFFIX=	-${ABISUFFIX}
---
+++ emulators/suse15_gtk3/suse.x86_64.mk
@@ -0,0 +1,5 @@
+# $NetBSD: suse.x86_64.mk,v 1.1 2022/10/02 17:17:18 wiz Exp $
+
+# Suffix on the GTK+-2.x/Pango binaries used in the INSTALL script
+ABISUFFIX=	64
+BINSUFFIX=	-${ABISUFFIX}
