ad27167f770f9b9f406e7e9bfe89bea722fc394c
adam <adam@pkgsrc.org>
2022-10-03 09:07:03 +0000

    stellarium: updated to 1.0
    1.0 [2022-10-01]
    The Stellarium team did it. Stellarium 1.0 is here!
    
    What makes this 1.0?
    
    - After more than 20 years of development the program is finally accurate
      enough for historical application.
    - This is the first release based on the Qt6 framework, so the project is
      safe for future development. These packages are tagged with a version
      number like 1.22.3.
    - We keep releasing Qt5-based packages for legacy or obsolete systems.
      This may include many Windows users who have to run ANGLE mode.
      These packages are tagged with a version number like 0.22.3.
    
    What is new for users?
    
    - A new, much better skylight model (Not for MacOS and OpenGL ES2 systems,
      sorry.)
    - Many details around eclipses
    - Updated Observation List features
    - New features in AstroCalc tool
    - HiDPI improvements
    - Better dithering
    - Able to use Windows location service (Windows)
    - New skyculture: Samoan
    - Updated several skycultures
    - Updates in Angle Measure, Satellites, Oculars, Remote Control, Pulsars
      plugins
    - Many more bugfixes.

--- misc/stellarium/patches/patch-src_core_StelFileMgr.cpp
+++ misc/stellarium/patches/patch-src_core_StelFileMgr.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_core_StelFileMgr.cpp,v 1.2 2019/03/26 11:15:37 adam Exp $
+$NetBSD: patch-src_core_StelFileMgr.cpp,v 1.3 2022/10/03 09:07:03 adam Exp $
 
 On Darwin, do not use bundles.
 
---- src/core/StelFileMgr.cpp.orig	2019-03-23 14:43:26.000000000 +0000
+--- src/core/StelFileMgr.cpp.orig	2022-09-21 12:07:19.000000000 +0000
 +++ src/core/StelFileMgr.cpp
-@@ -101,7 +101,7 @@ void StelFileMgr::init()
+@@ -103,7 +103,7 @@ void StelFileMgr::init()
  	}	
  	else
  	{
--	#if defined(Q_OS_MAC)
+-	#if defined(Q_OS_MACOS)
 +	#if 0
  		QString relativePath = "/../Resources";
  		if (QCoreApplication::applicationDirPath().contains("src")) {
