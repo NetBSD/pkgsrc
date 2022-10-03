ef5b689b542767ba98ce4a397dede6d96204190c
adam <adam@pkgsrc.org>
2022-10-03 10:46:05 +0000

    handbrake: switch to stable x264; fix build on Darwin; fix PLIST when gtk3 option is disabled; pkglint fixes; bump revision

--- multimedia/handbrake/options.mk
+++ multimedia/handbrake/options.mk
@@ -1,9 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2022/08/01 15:00:13 ast Exp $
+# $NetBSD: options.mk,v 1.3 2022/10/03 10:46:05 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.handbrake
 PKG_SUPPORTED_OPTIONS=	gtk3
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} != "Darwin"
 PKG_SUGGESTED_OPTIONS=	gtk3
+.endif
 
 .include "../../mk/bsd.options.mk"
 
