8057362ecb20416e6342bc8f80616df670af85cc
bsiegert <bsiegert@pkgsrc.org>
2022-10-03 14:45:52 +0000

    Pullup ticket #6677 - requested by taca lang/php80: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.377
    - lang/php80/distinfo                                           1.25
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Oct  1 00:28:12 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php80: distinfo
    
       Log Message:
       lang/php80: update to 8.0.24
    
       29 Sep 2022, PHP 8.0.24
    
       - Core:
         . Fixed bug GH-9323 (Crash in ZEND_RETURN/GC/zend_call_function)
           (Tim Starling)
         . Fixed bug GH-9361 (Segmentation fault on script exit #9379). (cmb,
           Christian Schneider)
         . Fixed bug GH-9407 (LSP error in eval'd code refers to wrong class for static
           type). (ilutov)
         . Fix #81727 (Don't mangle semantically meaningful HTTP var names). (derick)
    
       - DOM:
         . Fixed bug #79451 (DOMDocument->replaceChild on doctype causes double free).
           (Nathan Freeman)
    
       - FPM:
         . Fixed bug GH-8885 (FPM access.log with stderr begins to write logs to
           error_log after daemon reload). (Dmitry Menshikov)
         . Fixed bug #77780 ("Headers already sent..." when previous connection was
           aborted). (Jakub Zelenka)
    
       - GMP
         . Fixed bug GH-9308 (GMP throws the wrong error when a GMP object is passed
           to gmp_init()). (Girgias)
    
       - Intl
         . Fixed bug GH-9421 (Incorrect argument number for ValueError in NumberFormatter).
           (Girgias)
    
       - Phar:
         . Fix #81726 (DOS when using quine gzip file). (cmb)
    
       - PDO_PGSQL:
         . Fixed bug GH-9411 (PgSQL large object resource is incorrectly closed).
           (Yurunsoft)
    
       - Reflection:
         . Fixed bug GH-8932 (ReflectionFunction provides no way to get the called
           class of a Closure). (cmb, Nicolas Grekas)
         . Fixed bug GH-9409 (Private method is incorrectly dumped as "overwrites").
           (ilutov)
    
       - Streams:
         . Fixed bug GH-9316 ($http_response_header is wrong for long status line).
           (cmb, timwolla)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.374.2.2 2022/10/03 14:29:14 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.374.2.3 2022/10/03 14:45:52 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.32
-PHP80_VERSION=	8.0.23
+PHP80_VERSION=	8.0.24
 PHP81_VERSION=	8.1.11
 
 # Define initial release of major version.
