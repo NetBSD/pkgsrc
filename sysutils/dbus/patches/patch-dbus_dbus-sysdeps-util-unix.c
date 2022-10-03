c950e81ab087c82d6d05e112cd3085908e5cc6f2
wiz <wiz@pkgsrc.org>
2022-10-03 12:44:00 +0000

    dbus: update to 1.14.2.
    dbus 1.14.2 (2022-09-26)
    ========================
    
    Fixes:
    
    • Fix build failure on FreeBSD (dbus!277, Alex Richardson)
    
    • Fix build failure on macOS with launchd enabled
      (dbus!287, Dawid Wróbel)
    
    • Preserve errno on failure to open /proc/self/oom_score_adj
      (dbus!285, Gentoo#834725; Mike Gilbert)
    
    • On Linux, don't log warnings if oom_score_adj is read-only but does not
      need to be changed (dbus!291, Simon McVittie)
    
    • Slightly improve error-handling for inotify
      (dbus!235, Simon McVittie)
    
    • Don't crash if dbus-daemon is asked to watch more than 128 directories
      for changes (dbus!302, Jan Tojnar)
    
    • Autotools build system fixes:
      · Don't treat --with-x or --with-x=yes as a request to disable X11,
        fixing a regression in 1.13.20. Instead, require X11 libraries and
        fail if they cannot be detected. (dbus!263, Lars Wendler)
      · When a CMake project uses an Autotools-built libdbus in a
        non-standard prefix, find dbus-arch-deps.h successfully
        (dbus#314, Simon McVittie)
      · Don't include generated XML catalog in source releases
        (dbus!317, Jan Tojnar)
      · Improve robustness of detecting gcc __sync atomic builtins
        (dbus!320, Alex Richardson)
    
    • CMake build system fixes:
      · Detect endianness correctly, fixing interoperability with other D-Bus
        implementations on big-endian systems (dbus#375, Ralf Habacker)
      · When building for Unix, install session and system bus setup
        in the intended locations
        (dbus!267, dbus!297; Ralf Habacker, Alex Richardson)
      · Detect setresuid() and getresuid() (dbus!319, Alex Richardson)
      · Detect backtrace() on FreeBSD (dbus!281, Alex Richardson)
      · Don't include headers from parent directory (dbus!282, Alex Richardson)
      · Distinguish between host and target TMPDIR when cross-compiling
        (dbus!279, Alex Richardson)
      · Fix detection of atomic operations (dbus!306, Alex Richardson)
    
    Tests and CI enhancements:
    
    • On Unix, skip tests that switch uid if run in a container that is
      unable to do so, instead of failing (dbus#407, Simon McVittie)
    
    • Use the latest MSYS2 packages for CI
      (Ralf Habacker, Simon McVittie)

--- sysutils/dbus/patches/patch-dbus_dbus-sysdeps-util-unix.c
+++ sysutils/dbus/patches/patch-dbus_dbus-sysdeps-util-unix.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-dbus_dbus-sysdeps-util-unix.c,v 1.5 2022/03/08 11:16:00 wiz Exp $
+$NetBSD: patch-dbus_dbus-sysdeps-util-unix.c,v 1.6 2022/10/03 12:44:00 wiz Exp $
 
 add solaris specific console_user test
 
---- dbus/dbus-sysdeps-util-unix.c.orig	2022-02-23 14:39:11.000000000 +0000
+--- dbus/dbus-sysdeps-util-unix.c.orig	2022-09-14 12:12:21.000000000 +0000
 +++ dbus/dbus-sysdeps-util-unix.c
 @@ -56,6 +56,10 @@
+ #ifdef HAVE_SYS_PRCTL_H
  #include <sys/prctl.h>
  #endif
- 
 +#if defined(__sun) && defined(__SVR4)
 +#include <pwd.h>
 +#endif
 +
- #ifdef HAVE_SYS_SYSLIMITS_H
- #include <sys/syslimits.h>
- #endif
-@@ -581,8 +585,31 @@ _dbus_user_at_console (const char *usern
+ 
+ #ifdef HAVE_SYSTEMD
+ #include <systemd/sd-daemon.h>
+@@ -578,8 +582,31 @@ _dbus_user_at_console (const char *usern
  #ifdef DBUS_CONSOLE_AUTH_DIR
    DBusString u, f;
    dbus_bool_t result;
@@ -47,7 +47,7 @@ add solaris specific console_user test
    if (!_dbus_string_init (&f))
      {
        _DBUS_SET_OOM (error);
-@@ -607,6 +634,7 @@ _dbus_user_at_console (const char *usern
+@@ -604,6 +631,7 @@ _dbus_user_at_console (const char *usern
  
   out:
    _dbus_string_free (&f);
