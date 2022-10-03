603353173a7f453950c9f3bc502bcc5cd6182880
bsiegert <bsiegert@pkgsrc.org>
2022-10-03 14:17:25 +0000

    Pullup ticket #6675 - requested by taca lang/php74: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.375
    - lang/php74/distinfo                                           1.38
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Oct  1 00:25:22 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php74: distinfo
    
       Log Message:
       29 Sep 2022, PHP 7.4.32
    
       - Core:
         . Fixed bug #81726: phar wrapper: DOS when using quine gzip file.
           (CVE-2022-31628). (cmb)
         . Fixed bug #81727: Don't mangle HTTP variable names that clash with ones
           that have a specific semantic meaning. (CVE-2022-31629). (Derick)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.374 2022/09/01 16:34:21 taca Exp $
+# $NetBSD: phpversion.mk,v 1.374.2.1 2022/10/03 14:17:25 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP74_VERSION=	7.4.30
+PHP74_VERSION=	7.4.32
 PHP80_VERSION=	8.0.23
 PHP81_VERSION=	8.1.10
 
