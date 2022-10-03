ec19cd4a6132c50e0c89e9ea26efdca57f18dba7
bsiegert <bsiegert@pkgsrc.org>
2022-10-03 14:29:14 +0000

    Pullup ticket #6676 - requested by taca lang/php81: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.376
    - lang/php81/distinfo                                           1.13
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Oct  1 00:27:05 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php81: distinfo
    
       Log Message:
       lang/php81: update to 8.1.11
    
       29 Sep 2022, PHP 8.1.11
    
       - Core:
         . Fixed bug #81726: phar wrapper: DOS when using quine gzip file.
           (CVE-2022-31628). (cmb)
         . Fixed bug #81727: Don't mangle HTTP variable names that clash with ones
           that have a specific semantic meaning. (CVE-2022-31629). (Derick)
         . Fixed bug GH-9323 (Crash in ZEND_RETURN/GC/zend_call_function)
           (Tim Starling)
         . Fixed bug GH-9361 (Segmentation fault on script exit #9379). (cmb,
           Christian Schneider)
         . Fixed bug GH-9447 (Invalid class FQN emitted by AST dump for new and class
           constants in constant expressions). (ilutov)
    
       - DOM:
         . Fixed bug #79451 (DOMDocument->replaceChild on doctype causes double free).
           (Nathan Freeman)
    
       - FPM:
         . Fixed bug GH-8885 (FPM access.log with stderr begins to write logs to
           error_log after daemon reload). (Dmitry Menshikov)
         . Fixed bug #77780 ("Headers already sent..." when previous connection was
           aborted). (Jakub Zelenka)
    
       - GMP
         . Fixed bug GH-9308 (GMP throws the wrong error when a GMP object is passed
           to gmp_init()). (Girgias)
    
       - Intl
         . Fixed bug GH-9421 (Incorrect argument number for ValueError in NumberFormatter).
           (Girgias)
    
       - PCRE:
         . Fixed pcre.jit on Apple Silicon. (Niklas Keller)
    
       - PDO_PGSQL:
         . Fixed bug GH-9411 (PgSQL large object resource is incorrectly closed).
           (Yurunsoft)
    
       - Reflection:
         . Fixed bug GH-8932 (ReflectionFunction provides no way to get the called
           class of a Closure). (cmb, Nicolas Grekas)
    
       - Streams:
         . Fixed bug GH-9316 ($http_response_header is wrong for long status line).
           (cmb, timwolla)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.374.2.1 2022/10/03 14:17:25 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.374.2.2 2022/10/03 14:29:14 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.32
 PHP80_VERSION=	8.0.23
-PHP81_VERSION=	8.1.10
+PHP81_VERSION=	8.1.11
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
