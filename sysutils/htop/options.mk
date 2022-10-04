efe48eaa5d23b8dc0463e7b0e32d452619cd4e07
kim <kim@pkgsrc.org>
2022-10-04 07:14:57 +0000

    htop: Fix options for selecting curses type
    Users can now select ncurses for enabling unicode characters on NetBSD.
    It was not sufficient (or even correct, most likely) to use "wide-curses"
    for this.

--- sysutils/htop/options.mk
+++ sysutils/htop/options.mk
@@ -1,10 +1,20 @@
-# $NetBSD: options.mk,v 1.1 2022/09/25 09:53:28 kim Exp $
+# $NetBSD: options.mk,v 1.2 2022/10/04 07:14:57 kim Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.htop
+PKG_OPTIONS_VAR=		PKG_OPTIONS.htop
 
 PKG_OPTIONS_REQUIRED_GROUPS+=	curses
-PKG_OPTIONS_GROUP.curses=	curses wide-curses
+PKG_OPTIONS_GROUP.curses=	curses ncurses
 
 PKG_SUGGESTED_OPTIONS=		curses
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mncurses)
+. include "../../devel/ncursesw/buildlink3.mk"
+.else
+. include "../../mk/curses.buildlink3.mk"
+. if ${CURSES_TYPE} != "ncursesw"
+# See netbsd/README.md
+CONFIGURE_ARGS+=		--disable-unicode
+. endif
+.endif
