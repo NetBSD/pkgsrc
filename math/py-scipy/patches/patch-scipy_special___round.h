c4c772b20ac784ae9d80c9bda862eea7a321ab0c
adam <adam@pkgsrc.org>
2022-10-05 20:54:31 +0000

    py-scipy: updated to 1.9.1
    SciPy 1.9.1 is a bug-fix release with no new features
    compared to 1.9.0. Notably, some important meson build
    fixes are included.
    
    
    SciPy 1.9.0 is the culmination of 6 months of hard work. It contains
    many new features, numerous bug-fixes, improved test coverage and better
    documentation. There have been a number of deprecations and API changes
    in this release, which are documented below. All users are encouraged to
    upgrade to this release, as there are a large number of bug-fixes and
    optimizations. Before upgrading, we recommend that users check that
    their own code does not use deprecated SciPy functionality (to do so,
    run your code with python -Wd and check for DeprecationWarning s).
    Our development attention will now shift to bug-fix releases on the
    1.9.x branch, and on adding new features on the main branch.
    
    This release requires Python 3.8-3.11 and NumPy 1.18.5 or greater.
    
    For running on PyPy, PyPy3 6.0+ is required.

--- math/py-scipy/patches/patch-scipy_special___round.h
+++ math/py-scipy/patches/patch-scipy_special___round.h
@@ -1,13 +1,7 @@
-$NetBSD: patch-scipy_special___round.h,v 1.3 2018/09/03 09:04:24 wiz Exp $
+$NetBSD: patch-scipy_special___round.h,v 1.4 2022/10/05 20:54:31 adam Exp $
 
-Chunk 1: SunOS fix
+SunOS fix
 https://github.com/scipy/scipy/issues/9214
-
-Chunk 2:
-This conditional is just wrong, when built with c++, __STDC_VERSION__
-is not defined.  Make a cheezy fix which insists on <fenv.h> when
-built with C++.
-https://github.com/scipy/scipy/issues/9213
 
 --- scipy/special/_round.h.orig	2018-05-05 17:10:11.000000000 +0000
 +++ scipy/special/_round.h
@@ -22,12 +16,3 @@ https://github.com/scipy/scipy/issues/9213
  
  double add_round_up(double a, double b)
  {
-@@ -49,7 +53,7 @@ double add_round_down(double a, double b
- 
- 
- /* Helper code for testing _round.h. */
--#if __STDC_VERSION__ >= 199901L
-+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__cplusplus)
- /* We have C99 */
- #include <fenv.h>
- #else
