fa74f29833d3c54c56e7c27926254dae0691ea60
nros <nros@pkgsrc.org>
2022-10-07 11:15:54 +0000

    Update libhighway to version 1.0.1
    Pkgsrc changes:
    Build shared libs.
    Use gcc 8 and over to fix build.
    Include devel/googletest in
    buildlink3.mk since one of the
    installed header files include
    gtest/gtest.h .
    
    Changes from changelog:
    
    * Add Eq128, i64 Mul, unsigned->float ConvertTo
    * Faster sort for few unique keys, more robust pivot selection
    * Fix: floating-point generator for sort tests, Min/MaxOfLanes for i16
    * Fix: avoid always_inline in debug, link atomic
    * GCC warnings: string.h, maybe-uninitialized, ignored-attributes
    * GCC warnings: preprocessor int overflow, spurious use-after-free/overflow
    * Doc: <=HWY_AVX3, Full32/64/128, how to use generic-inl
    * ABI change: 64-bit target values, more room for expansion
    * Add CompressBlocksNot, CompressNot, Lt128Upper, Min/Max128Upper, TruncateTo
    * Add HWY_SVE2_128 target
    * Sort speedups especially for 128-bit
    * Documentation clarifications
    * Faster NEON CountTrue/FindFirstTrue/AllFalse/AllTrue
    * Improved SVE codegen
    * Fix u16x8 ConcatEven/Odd, SSSE3 i64 Lt
    * MSVC 2017 workarounds
    * Support for runtime dispatch on Arm/GCC/Linux

--- graphics/libhighway/buildlink3.mk
+++ graphics/libhighway/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/07/17 19:32:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/10/07 11:15:54 nros Exp $
 
 BUILDLINK_TREE+=	libhighway
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	libhighway
 LIBHIGHWAY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libhighway+=	libhighway>=0.12.2
+BUILDLINK_ABI_DEPENDS.libhighway+=	libhighway>=1.0.1
 BUILDLINK_PKGSRCDIR.libhighway?=	../../graphics/libhighway
 
+.include "../../devel/googletest/buildlink3.mk"
 .endif	# LIBHIGHWAY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libhighway
