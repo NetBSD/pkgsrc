b8569fafb045b331be9920951823c32adf004b5e
bsiegert <bsiegert@pkgsrc.org>
2022-10-07 18:19:31 +0000

    Pullup ticket #6680 - requested by taca lang/go119: security fix
    Revisions pulled up:
    - lang/go/version.mk                                            1.163
    - lang/go119/PLIST                                              1.3
    - lang/go119/distinfo                                           1.3
    
    ---
       Module Name:	pkgsrc
       Committed By:	bsiegert
       Date:		Wed Oct  5 11:20:24 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/go: version.mk
       	pkgsrc/lang/go119: PLIST distinfo
    
       Log Message:
       Update go119 to 1.19.2
    
       This minor release includes 3 security fixes following the security policy:
    
       - archive/tar: unbounded memory consumption when reading headers
    
         Reader.Read did not set a limit on the maximum size of file headers.
         A maliciously crafted archive could cause Read to allocate unbounded
         amounts of memory, potentially causing resource exhaustion or panics.
         Reader.Read now limits the maximum size of header blocks to 1 MiB.
    
         Thanks to Adam Korczynski (ADA Logics) and OSS-Fuzz for reporting this issue.
    
         This is CVE-2022-2879 and Go issue https://go.dev/issue/54853.
    
       - net/http/httputil: ReverseProxy should not forward unparseable query parameters
    
         Requests forwarded by ReverseProxy included the raw query parameters from the
         inbound request, including unparseable parameters rejected by net/http. This
         could permit query parameter smuggling when a Go proxy forwards a parameter
         with an unparseable value.
    
         ReverseProxy will now sanitize the query parameters in the forwarded query
         when the outbound request's Form field is set after the ReverseProxy.Director
         function returns, indicating that the proxy has parsed the query parameters.
         Proxies which do not parse query parameters continue to forward the original
         query parameters unchanged.
    
         Thanks to Gal Goldstein (Security Researcher, Oxeye) and
         Daniel Abeles (Head of Research, Oxeye) for reporting this issue.
    
         This is CVE-2022-2880 and Go issue https://go.dev/issue/54663.
    
       - regexp/syntax: limit memory used by parsing regexps
    
         The parsed regexp representation is linear in the size of the input,
         but in some cases the constant factor can be as high as 40,000,
         making relatively small regexps consume much larger amounts of memory.
    
         Each regexp being parsed is now limited to a 256 MB memory footprint.
         Regular expressions whose representation would use more space than that
         are now rejected. Normal use of regular expressions is unaffected.
    
         Thanks to Adam Korczynski (ADA Logics) and OSS-Fuzz for reporting this issue.
    
         This is CVE-2022-41715 and Go issue https://go.dev/issue/55949.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.159.2.1 2022/10/07 16:34:02 bsiegert Exp $
+# $NetBSD: version.mk,v 1.159.2.2 2022/10/07 18:19:31 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,7 +6,7 @@
 #
 .include "go-vars.mk"
 
-GO119_VERSION=	1.19.1
+GO119_VERSION=	1.19.2
 GO118_VERSION=	1.18.7
 GO117_VERSION=	1.17.13
 GO116_VERSION=	1.16.15
