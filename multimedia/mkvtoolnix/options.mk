9be0c75ae3f842ffb3f1b2e08602299d9ead0c8b
adam <adam@pkgsrc.org>
2022-10-07 04:29:07 +0000

    One PLIST for all options

--- multimedia/mkvtoolnix/options.mk
+++ multimedia/mkvtoolnix/options.mk
@@ -1,15 +1,15 @@
-# $NetBSD: options.mk,v 1.7 2022/10/02 18:39:42 adam Exp $
+# $NetBSD: options.mk,v 1.8 2022/10/07 04:29:07 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mkvtoolnix
-PKG_SUPPORTED_OPTIONS=	gui doc
+PKG_SUPPORTED_OPTIONS=	doc gui
 PKG_SUGGESTED_OPTIONS=	doc
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_SRC=		${PLIST_SRC_DFLT}
+PLIST_VARS+=	doc gui
 
 .if !empty(PKG_OPTIONS:Mgui)
-PLIST_SRC+=		PLIST.gui
+PLIST.gui=		yes
 CONFIGURE_ARGS+=	--enable-gui
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../x11/qt5-qtsvg/buildlink3.mk"
@@ -18,7 +18,7 @@ CONFIGURE_ARGS+=	--disable-gui
 .endif
 
 .if !empty(PKG_OPTIONS:Mdoc)
-PLIST_SRC+=		PLIST.po4a
+PLIST.doc=		yes
 CONFIGURE_ARGS+=	--with-po4a
 BUILD_DEPENDS+=		po4a-[0-9]*:../../textproc/po4a
 .else
