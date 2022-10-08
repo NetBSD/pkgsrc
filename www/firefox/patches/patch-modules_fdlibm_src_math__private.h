0717ce97c26904a279bf9895449fe0e32e0ce827
ryoon <ryoon@pkgsrc.org>
2022-10-08 21:18:55 +0000

    firefox: Update to 105.0.3
    * Add --enable-new-pass-manager.
    * Disable sysutils/dbus dependency for non-Linux platforms by default.
    
    Changelog:
    105.0.3:
    Fixed
      * Mitigated frequent crashes for Windows users with Avast or AVG Antivirus
        software installed (bug 1794064)
    
    105.0.2:
    Fixed
      * Fixed poor contrast on various menu items with certain themes on Linux
        systems (bug 1792063)
    
      * Fixed the scrollbar appearing on the wrong side of select elements in
        right-to-left locales (bug 1791219)
    
      * Fixed a possible deadlock when loading some sites in Troubleshoot Mode (bug
        1786259)
    
      * Fixed a bug causing some dynamic appearance changes to not appear when
        expected (bug 1786521)
    
      * Fixed a bug causing theme styling to not be properly applied to sidebars
        for some add-ons in Private Browsing Mode (bug 1787543)
    
    105.0.1:
    Fixed
      * Reverted focus behavior for new windows back to the content area
        instead of the address bar (bug 1784692)
    
    105.0:
    New
      * Added an option to print only the current page from the print preview
        dialog.
    
      * Firefox now supports partitioned service workers in third-party contexts.
        You can register service workers in a third-party iframe and it will be
        partitioned under the top-level domain.
    
      * Swipe to navigate (two fingers on a touchpad swiped left or right to
        perform history back or forward) on Windows is now enabled.
    
      * Firefox is now compliant with the User Timing L3 specification, which adds
        additional optional arguments to the performance.mark and
        performance.measure methods to provide custom start times, end times,
        duration, and attached details.
    
      * Searching in large lists for individual items is now 2x faster. This
        performance enhancement replaces array.includes and array.indexOf with an
        optimized SIMD version.
    
    Fixed
      * Stability on Windows is significantly improved as Firefox handles
        low-memory situations much better.
    
      * Touchpad scrolling on macOS was made more accessible by reducing unintended
        diagonal scrolling opposite of the intended scroll axis.
    
      * Firefox is less likely to run out of memory on Linux and performs more
        efficiently for the rest of the system when memory runs low.
    
      * Various security fixes.
    
    Web Platform
      * Support for the Offscreen Canvas DOM API with full context and font
        support. The OffscreenCanvas API provides a canvas that can be rendered
        off-screen in both Window and Web Worker contexts.
    
    Security fixes:
    #CVE-2022-40959: Bypassing FeaturePolicy restrictions on transient pages
    #CVE-2022-40960: Data-race when parsing non-UTF-8 URLs in threads
    #CVE-2022-40958: Bypassing Secure Context restriction for cookies with __Host
     and __Secure prefix
    #CVE-2022-40961: Stack-buffer overflow when initializing Graphics
    #CVE-2022-40956: Content-Security-Policy base-uri bypass
    #CVE-2022-40957: Incoherent instruction cache when building WASM on ARM64
    #CVE-2022-40962: Memory safety bugs fixed in Firefox 105 and Firefox ESR 102.3

--- www/firefox/patches/patch-modules_fdlibm_src_math__private.h
+++ www/firefox/patches/patch-modules_fdlibm_src_math__private.h
@@ -1,18 +1,21 @@
-$NetBSD: patch-modules_fdlibm_src_math__private.h,v 1.1 2021/10/29 16:46:04 ryoon Exp $
+$NetBSD: patch-modules_fdlibm_src_math__private.h,v 1.2 2022/10/08 21:18:55 ryoon Exp $
 
 * Fix PR pkg/56457, build failure on NetBSD/i386.
   Do not use FreeBSD-specific definition.
 
---- modules/fdlibm/src/math_private.h.orig	2021-09-27 22:47:42.000000000 +0000
+--- modules/fdlibm/src/math_private.h.orig	2022-10-07 15:25:41.000000000 +0000
 +++ modules/fdlibm/src/math_private.h
-@@ -30,8 +30,8 @@
+@@ -30,10 +30,9 @@
   * Adapted from https://github.com/freebsd/freebsd-src/search?q=__double_t
   */
  
 -typedef double      __double_t;
 -typedef __double_t  double_t;
+-typedef float       __float_t;
+-typedef __float_t   float_t;
 +#include <math.h>
 +typedef double_t __double_t;
++typedef float_t  __float_t;
  
  /*
   * The original fdlibm code used statements like:
