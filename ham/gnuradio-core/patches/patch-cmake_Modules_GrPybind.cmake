9726ee22c9fcd9e009d8c8883ae66a6f19c5e725
adam <adam@pkgsrc.org>
2022-10-09 07:56:09 +0000

    gnuradio: updated to 3.10.4.0
    3.10.4.0
    
    Project Scope
    
    Replace get_initial_sptr() calls with make_block_sptr() calls. There were a number of places the incorrect function was being used.
    
    Runtime
    
    Use correctly typed arguments to log messages to prevent build errors.
    
    GRC
    
    Add xfce4-terminal and urxvt to the list of terminal emulators discovered during the build process.
    Suppress GUI hint errors that were being shown in the terminal window.
    Use integers for screenshot size (floats were causing Cairo errors).
    
    Build system and packaging
    
    Reformat cmake files and make cmake formatting part of the workflow.
    Allow GNU Radio to be a part of other cmake-based projects.
    Correct linking to libiio and libad9361 on macOS.
    Update method for determining Python installation directory. This should work correctly now on (all?) distro releases.
    
    gr-blocks
    
    New Block Interleaver/Deinterleaver interleaves blocks of symbols
    Correct calculation of items_remaining in File Source, which allows seek() to work correctly.
    Add an example for Wavefile Sink
    
    gr-digital
    
    Deprecate the CRC32 and CRC16 blocks, which will be removed in the future. There are more general CRC blocks which do the same thing (and more).
    
    gr-filter
    
    Fix demo for PFB channelizer
    
    gr-iio
    
    FMCOMMS2 Sink assumes CS16 data is scaled to 32768, rather than 2048.
    FMCOMMS2 returns the correct samples for the second channel in 2-channel mode.
    
    gr-trellis
    
    Correct Python bindings for trellis::metrics.
    
    gr-qtgui
    
    Range widget can now output messages when value changes.
    Add C++ code generation for Time Sink
    Regenerate Python bindings for some blocks when necessary.
    Waterfall Sink correctly uses half spectrum for float input.
    
    gr-uhd
    
    Add Python bindings for the UHD find() functino.
    
    gr-zeromq
    
    Support newer get() and older/deprecated getsockopt() functions in cppzmq depending on availability.
    
    Modtool
    
    Parse IO signatures with or without gr:: prefix.
    
    Documentation
    
    Update certain file lists to keep build paths out of documentation.
    
    Testing
    
    Update Conda recipe for Qt 5.15 and re-render CI support files.
    Add testing on Ubuntu 22.04.
    Link tests directly against spdlog with not linking to GR runtime.
    Ignore Python "missing whitespace after keywork" formatting error.

--- ham/gnuradio-core/patches/patch-cmake_Modules_GrPybind.cmake
+++ ham/gnuradio-core/patches/patch-cmake_Modules_GrPybind.cmake
@@ -1,15 +1,15 @@
-$NetBSD: patch-cmake_Modules_GrPybind.cmake,v 1.1 2022/01/16 18:06:25 tnn Exp $
+$NetBSD: patch-cmake_Modules_GrPybind.cmake,v 1.2 2022/10/09 07:56:09 adam Exp $
 
 Disable use of precompiled headers; it doesn't play nice with pkgsrc nor ccache.
 
---- cmake/Modules/GrPybind.cmake.orig	2022-01-14 15:27:04.000000000 +0000
+--- cmake/Modules/GrPybind.cmake.orig	2022-09-16 15:51:17.000000000 +0000
 +++ cmake/Modules/GrPybind.cmake
-@@ -158,7 +158,7 @@ target_include_directories(${name}_pytho
- # Precompile the pybind11 header
- # This should speed up building of the python bindings at least in larger modules
- # This functionality is only available in CMake >= 3.16
--if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16.0")
-+if(false AND ${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16.0")
-   target_precompile_headers(
-     ${name}_python
-     PRIVATE
+@@ -176,7 +176,7 @@ macro(GR_PYBIND_MAKE_CHECK_HASH name upd
+     # Precompile the pybind11 header
+     # This should speed up building of the python bindings at least in larger modules
+     # This functionality is only available in CMake >= 3.16
+-    if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16.0")
++    if(FALSE AND ${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16.0")
+         target_precompile_headers(
+             ${name}_python
+             PRIVATE
