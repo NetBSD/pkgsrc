d5ed686369a0d5219fc730bbdc2f6dbf720fdc34
tonio <tonio@pkgsrc.org>
2022-10-09 07:02:47 +0000

    Update devel/frama-c to 25.0
    -   Kernel    [2022-10-05] Support for ghost VLA and calls to builtins with
                  ghost arguments.
    -   Eva       [2022-09-16] Numerors now needs MLMPFR 4.1.0+bugfix2
    -   Kernel    [2022-09-07] Improve error message for invalid options -D/-I/-U.
    o!  Configure [2022-07-28] Removed autoconf and configure
    o!  Makefile  [2022-07-11] Removed Makefile, Frama-C is now built using Dune 3.x
    o!  Pdg       [2022-07-01] Removed from Db. Use proper Pdg API instead.
    -!  Kernel    [2022-06-06] Remove journalisation.
    
    ####################################
    Open Source Release 25.0 (Manganese)
    ####################################
    
    o   Kernel    [2022-05-03] Prototype of AST comparison between two versions
                  of the same program, via the new option -ast-diff.
    -   Eva       [2022-05-02] Fixes stack overflow errors on very large C functions.
    -   Eva       [2022-04-25] Improve the multidim abstract domain: it is now
                  more precise and more robust, it is able to infer simple array
                  invariants on some loops without unrolling, and has a new option
                  -eva-multidim-disjunctive-invariants to infer structures
                  disjunctive invariants.
    o   Kernel    [2022-03-07] Known compiler builtins are no longer hardcoded in
                  OCaml, but defined via JSON files (in share/compliance).
    o   Kernel    [2022-02-23] New visitor functions visitFramacFileFunctions
                  and visitCilFileFunctions to visit only function definitions,
                  for better performance.
    o!  Kernel    [2022-02-23] Remove State_selection.Static (deprecated since
                  10 years, use directly State_selection instead)
    -*  Kernel    [2022-02-22] Fix list of potential types for decimal
                  integer literal constants
    *   Kernel    [2022-02-17] Reject programs using unsupported
                  vector_size attribute (fixes ##2573)
    o   Eva       [2022-02-15] New API to run the analysis and access its results,
                  intended to replace the old API in Db.Value. It is more precise
                  as it uses all available domains to evaluate values and locations.
                  See file Eva.mli for more details.
    *   Kernel    [2022-02-08] Reject array whose size is too big with a proper
                  error message instead of a crash (fixes ##2590)
    o!  Kernel    [2022-02-19] Removed obsolete AST nodes IndexPI and Info
    *   Kernel    [2022-02-08] Reject array whose size is too big with a proper
                  error message instead of a crash (fixes ##2590)
    o!  Kernel    [2021-12-03] Remove unused AST node Dcustom_annot and field
                  fpadding_in_bits. Do not cache size of types in the AST but in
                  a dedicated table.
    -*  Logic     [2021-11-30] Fix type of expressions whose value are functions
    o!  Kernel    [2021-11-29] Integer.pretty does not have the optional argument
                  hexa anymore. Use Integer.pretty_hex or Integer.pp_hex if you
                  want to print integers in hexadecimal format.

--- devel/frama-c/patches/patch-src_libraries_utils_c__bindings.c
+++ devel/frama-c/patches/patch-src_libraries_utils_c__bindings.c
@@ -1,24 +1,26 @@
-$NetBSD: patch-src_libraries_utils_c__bindings.c,v 1.1 2017/09/05 07:30:00 dholland Exp $
+$NetBSD: patch-src_libraries_utils_c__bindings.c,v 1.2 2022/10/09 07:02:47 tonio Exp $
 
 sync ifdefs with reality
 
---- src/libraries/utils/c_bindings.c~	2017-06-01 08:02:15.000000000 +0000
+--- src/libraries/utils/c_bindings.c.orig	2022-06-21 00:00:00.000000000 +0000
 +++ src/libraries/utils/c_bindings.c
-@@ -35,7 +35,7 @@
+@@ -34,18 +34,7 @@
+ #include <stdint.h>
  #include <unistd.h>
  
- // Some BSD flavors do not implement all of C99
--#if defined(__OpenBSD__) || defined(__NetBSD__) 
-+#if defined(__OpenBSD__)
- # include <ieeefp.h>
- # define FE_DOWNWARD FP_RM
- # define FE_UPWARD FP_RP
-@@ -218,7 +218,7 @@ value single_precision_of_string(value s
- value terminate_process(value v) 
- {
-   long pid = Long_val(v);
--#if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _POSIX_SOURCE || __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ 
-+#if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _POSIX_SOURCE || __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined (__Bitrig__)
-   kill(pid,9);
- #else
-  #ifdef _WIN32
+-// Some BSD flavors do not implement all of C99
+-#if defined(__NetBSD__)
+-# include <ieeefp.h>
+-# define FE_DOWNWARD FP_RM
+-# define FE_UPWARD FP_RP
+-# define FE_TONEAREST FP_RN
+-# define FE_TOWARDZERO FP_RZ
+-# define fegetround()	fpgetround()
+-# define fesetround(RM)	fpsetround(RM)
+-#else 
+-# include <fenv.h>
+-#endif
++#include <fenv.h>
+ 
+ #include <float.h>
+ #include <math.h>
