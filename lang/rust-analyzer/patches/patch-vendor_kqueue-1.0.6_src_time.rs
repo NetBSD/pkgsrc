6d766116fba0b94f15cb48fe3b27ce3f15b9ea5f
he <he@pkgsrc.org>
2022-10-10 19:04:52 +0000

    lang/rust-analyzer: do the previous ... properly.
    Linux x32 has a quirk in the libc crate, so that gets duplicated here.
    Also, fix the path to the file to be patched.  Ride previous revision bump.

--- lang/rust-analyzer/patches/patch-vendor_kqueue-1.0.6_src_time.rs
+++ lang/rust-analyzer/patches/patch-vendor_kqueue-1.0.6_src_time.rs
@@ -1,20 +1,20 @@
-$NetBSD: patch-vendor_kqueue-1.0.6_src_time.rs,v 1.1 2022/10/10 08:08:28 he Exp $
+$NetBSD: patch-vendor_kqueue-1.0.6_src_time.rs,v 1.2 2022/10/10 19:04:52 he Exp $
 
 Use the timespec component types instead of hardcoding int lengths.
+...except Linux has a quirk.
 
---- vendor/kqueue-1.0.6/src/time.rs.orig	1973-11-29 21:33:09.000000000 +0000
-+++ vendor/kqueue-1.0.6/src/time.rs
-@@ -1,32 +1,11 @@
-+use libc::time_t;
-+use libc::c_long;
- use libc::timespec;
+--- ../vendor/kqueue-1.0.6/src/time.rs.orig	1973-11-29 21:33:09.000000000 +0000
++++ ../vendor/kqueue-1.0.6/src/time.rs
+@@ -1,32 +1,15 @@
+-use libc::timespec;
++use libc::{timespec, time_t, c_long};
  use std::time::Duration;
  
 -#[cfg(not(all(
 -    any(target_os = "freebsd", target_os = "macos"),
 -    any(target_arch = "x86", target_arch = "powerpc")
 -)))]
- pub(crate) fn duration_to_timespec(d: Duration) -> timespec {
+-pub(crate) fn duration_to_timespec(d: Duration) -> timespec {
 -    let tv_sec = d.as_secs() as i64;
 -    let tv_nsec = d.subsec_nanos() as i64;
 -
@@ -28,16 +28,21 @@ Use the timespec component types instead of hardcoding int lengths.
 -
 -    timespec { tv_sec, tv_nsec }
 -}
--
++// Linux x32 compatibility, ref. libc/src/unix/mod.rs
++#[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
++type NSec = i64;
++#[cfg(not(all(target_arch = "x86_64", target_pointer_width = "32")))]
++type NSec = c_long;
+ 
 -#[cfg(all(
 -    any(target_os = "freebsd", target_os = "macos"),
 -    any(target_arch = "x86", target_arch = "powerpc")
 -))]
--pub(crate) fn duration_to_timespec(d: Duration) -> timespec {
+ pub(crate) fn duration_to_timespec(d: Duration) -> timespec {
 -    let tv_sec = d.as_secs() as i32;
 -    let tv_nsec = d.subsec_nanos() as i32;
 +    let tv_sec = d.as_secs() as time_t;
-+    let tv_nsec = d.subsec_nanos() as c_long;
++    let tv_nsec = d.subsec_nanos() as NSec;
  
      if tv_sec.is_negative() {
          panic!("Duration seconds is negative");
