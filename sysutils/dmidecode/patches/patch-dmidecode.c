2da7c33e0169d133186b84f63d8d32e0f906d28b
msaitoh <msaitoh@pkgsrc.org>
2022-10-11 09:12:18 +0000

    Update dmidecode to 3.4.
    CHANGES from 3.3:
    
    Version 3.4 (Mon Jun 27 2022)
      - Support for SMBIOS 3.4.0. This includes new memory device types, new
        processor upgrades, new slot types and characteristics, decoding of memory
        module extended speed, new system slot types, new processor characteristics
        and new format of Processor ID.
      - Support for SMBIOS 3.5.0. This includes new processor upgrades, BIOS
        characteristics, new slot characteristics, new on-board device types, new
        pointing device interface types, and a new record type (type 45 -
        Firmware Inventory Information).
      - Decode HPE OEM records 194, 199, 203, 236, 237, 238 ans 240.
      - Bug fixes:
        Fix OEM vendor name matching
        Fix ASCII filtering of strings
        Fix crash with option -u
      - Minor improvements:
        Skip details of uninstalled memory modules
        Don't display the raw CPU ID in quiet mode
        Improve the formatting of the manual pages

--- sysutils/dmidecode/patches/patch-dmidecode.c
+++ sysutils/dmidecode/patches/patch-dmidecode.c
@@ -1,14 +1,10 @@
-$NetBSD: patch-dmidecode.c,v 1.3 2022/04/23 20:18:30 tnn Exp $
+$NetBSD: patch-dmidecode.c,v 1.4 2022/10/11 09:12:18 msaitoh Exp $
 
 * Find SMBIOS table address on NetBSD ARM64.
 * Add missing header for FreeBSD.
-* Subject: dmidecode: Fix the condition error in ascii_filter
-  https://git.savannah.gnu.org/cgit/dmidecode.git/patch/?id=1117390ccd9cea139638db6f460bb6de70e28f94
-* Subject: dmidecode: Fix crash with -u option
-  https://git.savannah.gnu.org/cgit/dmidecode.git/patch/?id=11e134e54d15e67a64c39a623f492a28df922517
 
---- dmidecode.c.orig	2020-10-14 12:51:11.000000000 +0000
-+++ dmidecode.c
+--- dmidecode.c.orig	2022-06-28 00:06:32.000000000 +0900
++++ dmidecode.c	2022-10-11 18:08:14.158248758 +0900
 @@ -71,6 +71,13 @@
  #ifdef __FreeBSD__
  #include <errno.h>
@@ -23,27 +19,7 @@ $NetBSD: patch-dmidecode.c,v 1.3 2022/04/23 20:18:30 tnn Exp $
  #endif
  
  #include "version.h"
-@@ -116,7 +123,7 @@ static void ascii_filter(char *bp, size_
- 	size_t i;
- 
- 	for (i = 0; i < len; i++)
--		if (bp[i] < 32 || bp[i] == 127)
-+		if (bp[i] < 32 || bp[i] >= 127)
- 			bp[i] = '.';
- }
- 
-@@ -248,9 +255,9 @@ static void dmi_dump(const struct dmi_he
- 			{
- 				int j, l = strlen(s) + 1;
- 
--				off = 0;
- 				for (row = 0; row < ((l - 1) >> 4) + 1; row++)
- 				{
-+					off = 0;
- 					for (j = 0; j < 16 && j < l - (row << 4); j++)
- 						off += sprintf(raw_data + off,
- 						       j ? " %02X" : "%02X",
-@@ -5499,6 +5506,9 @@ static int address_from_efi(off_t *addre
+@@ -5837,6 +5844,9 @@ static int address_from_efi(off_t *addre
  	char linebuf[64];
  #elif defined(__FreeBSD__)
  	char addrstr[KENV_MVALLEN + 1];
@@ -53,7 +29,7 @@ $NetBSD: patch-dmidecode.c,v 1.3 2022/04/23 20:18:30 tnn Exp $
  #endif
  	const char *eptype;
  	int ret;
-@@ -5552,6 +5562,23 @@ static int address_from_efi(off_t *addre
+@@ -5890,6 +5900,23 @@ static int address_from_efi(off_t *addre
  	*address = strtoull(addrstr, NULL, 0);
  	eptype = "SMBIOS";
  	ret = 0;
