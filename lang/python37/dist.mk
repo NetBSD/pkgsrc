9a4a966da1d3fccf72361c66c9ce8ed3d9f3207d
adam <adam@pkgsrc.org>
2022-10-12 08:38:36 +0000

    python37 py37-html-docs: updated to 3.7.15
    Python 3.7.15
    
    Security
    
    gh-97616: Fix multiplying a list by an integer (list *= int): detect the integer overflow when the new allocated length is close to the maximum size. Issue reported by Jordan Limor. Patch by Victor Stinner.
    gh-97612: Fix a shell code injection vulnerability in the get-remote-certificate.py example script. The script no longer uses a shell to run openssl commands. Issue reported and initial fix by Caleb Shortt. Patch by Victor Stinner.
    
    Core and Builtins
    
    gh-96848: Fix command line parsing: reject -X int_max_str_digits option with no value (invalid) when the PYTHONINTMAXSTRDIGITS environment variable is set to a valid limit. Patch by Victor Stinner.
    gh-95778: When ValueError is raised if an integer is larger than the limit, mention the sys.set_int_max_str_digits() function in the error message. Patch by Victor Stinner.
    
    Library
    
    gh-97005: Update bundled libexpat to 2.4.9
    
    Windows
    
    gh-96577: Fixes a potential buffer overrun in msilib.

--- lang/python37/dist.mk
+++ lang/python37/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.15 2022/09/12 07:58:55 adam Exp $
+# $NetBSD: dist.mk,v 1.16 2022/10/12 08:38:36 adam Exp $
 
-PY_DISTVERSION=	3.7.14
+PY_DISTVERSION=	3.7.15
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python37/distinfo
