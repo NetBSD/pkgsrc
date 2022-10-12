fe0447fd33249265e0e6aee6239b30c5a28c4562
adam <adam@pkgsrc.org>
2022-10-12 08:37:52 +0000

    python38 py38-html-docs: updated to 3.8.15
    Python 3.8.15
    
    Security
    
    gh-97616: Fix multiplying a list by an integer (list *= int): detect the integer overflow when the new allocated length is close to the maximum size. Issue reported by Jordan Limor. Patch by Victor Stinner.
    gh-97612: Fix a shell code injection vulnerability in the get-remote-certificate.py example script. The script no longer uses a shell to run openssl commands. Issue reported and initial fix by Caleb Shortt. Patch by Victor Stinner.
    
    Core and Builtins
    
    gh-96848: Fix command line parsing: reject -X int_max_str_digits option with no value (invalid) when the PYTHONINTMAXSTRDIGITS environment variable is set to a valid limit. Patch by Victor Stinner.
    gh-95778: When ValueError is raised if an integer is larger than the limit, mention the sys.set_int_max_str_digits() function in the error message. Patch by Victor Stinner.
    
    Library
    
    gh-97005: Update bundled libexpat to 2.4.9
    
    Windows
    
    gh-96577: Fixes a potential buffer overrun in msilib.

--- lang/python38/dist.mk
+++ lang/python38/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.15 2022/09/07 15:32:34 adam Exp $
+# $NetBSD: dist.mk,v 1.16 2022/10/12 08:37:52 adam Exp $
 
-PY_DISTVERSION=	3.8.14
+PY_DISTVERSION=	3.8.15
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python38/distinfo
