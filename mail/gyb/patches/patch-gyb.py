ec91ecfd7e8d280bbc5e5ec55f61db2c4b42e6bb
sborrill <sborrill@pkgsrc.org>
2022-10-14 12:26:56 +0000

    gyb: update to 1.71
    Disable automatic update check
    
    Changes:
    - improved build process should reduce false positive AV detection of GYB
    - fix --action restore-group and .eml files as a type of mbox.

--- mail/gyb/patches/patch-gyb.py
+++ mail/gyb/patches/patch-gyb.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-gyb.py,v 1.1 2022/08/30 12:20:15 sborrill Exp $
+$NetBSD: patch-gyb.py,v 1.2 2022/10/14 12:26:56 sborrill Exp $
 
 Set default config path to PKG_SYSCONFDIR
+Disable automatic update check
 
---- gyb.py.orig	2022-08-25 10:03:41.833502873 +0100
-+++ gyb.py	2022-08-25 10:06:19.651616395 +0100
-@@ -241,7 +241,7 @@
+--- gyb.py.orig	2022-09-26 19:18:58.000000000 +0100
++++ gyb.py	2022-10-14 13:19:17.325498099 +0100
+@@ -242,7 +242,7 @@
    parser.add_argument('--config-folder',
      dest='config_folder',
      help='Optional: Alternate folder to store config and credentials',
@@ -13,3 +14,12 @@ Set default config path to PKG_SYSCONFDIR
    parser.add_argument('--cleanup',
            action='store_true',
            dest='cleanup',
+@@ -1964,7 +1964,7 @@
+   options = SetupOptionParser(argv)
+   if options.debug:
+     httplib2.debuglevel = 4
+-  doGYBCheckForUpdates(debug=options.debug)
++#  doGYBCheckForUpdates(debug=options.debug)
+   if options.version:
+     print(getGYBVersion())
+     print('Path: %s' % getProgPath())
