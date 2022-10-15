db5c64f9f802d727d70cde21afd69ecf66361c42
triaxx <triaxx@pkgsrc.org>
2022-10-15 18:26:27 +0000

    rabbitmq: Update to 3.11.1
    upstream changes:
    -----------------
    3.11.1
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.11.1.md
    3.11.0
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.11.0.md
    3.10.9
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.9.md
    3.10.8
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.8.md
    3.10.7
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.7.md
    3.10.6
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.6.md
    3.10.5
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.5.md
    3.10.4
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.4.md
    3.10.3
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.3.md
    3.10.2
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.2.md
    3.10.1
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.1.md
    3.10.0
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.10.0.md
    3.9.23
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.23.md
    3.9.22
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.22.md
    3.9.21
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.21.md
    3.9.20
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.20.md
    3.9.19
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.19.md
    3.9.18
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.18.md
    3.9.17
      o https://github.com/rabbitmq/rabbitmq-server/blob/main/release-notes/3.9.17.md

--- net/rabbitmq/patches/patch-deps_rabbit__common_src_rabbit__env.erl
+++ net/rabbitmq/patches/patch-deps_rabbit__common_src_rabbit__env.erl
@@ -1,10 +1,10 @@
-$NetBSD: patch-deps_rabbit__common_src_rabbit__env.erl,v 1.1 2021/01/28 13:18:15 adam Exp $
+$NetBSD: patch-deps_rabbit__common_src_rabbit__env.erl,v 1.2 2022/10/15 18:26:27 triaxx Exp $
 
 Use PkgSrc prefix.
 
 --- deps/rabbit_common/src/rabbit_env.erl.orig	2021-01-28 09:54:36.000000000 +0000
 +++ deps/rabbit_common/src/rabbit_env.erl
-@@ -1187,7 +1187,7 @@ erlang_dist_tcp_port(#{amqp_tcp_port := 
+@@ -1272,7 +1272,7 @@ erlang_dist_tcp_port(#{amqp_tcp_port := 
  sys_prefix(#{os_type := {unix, _}} = Context) ->
      case get_env_var("SYS_PREFIX") of
          false ->
