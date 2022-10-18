94eb874fcc31f50a72b4d9eeb6aa169c985a2814
adam <adam@pkgsrc.org>
2022-10-18 12:01:52 +0000

    net-snmp py-netsnmp: updated to 5.9.3
    *5.9.3*:
        security:
          - These two CVEs can be exploited by a user with read-only credentials:
              - CVE-2022-24805 A buffer overflow in the handling of the INDEX of
                NET-SNMP-VACM-MIB can cause an out-of-bounds memory access.
              - CVE-2022-24809 A malformed OID in a GET-NEXT to the nsVacmAccessTable
                can cause a NULL pointer dereference.
          - These CVEs can be exploited by a user with read-write credentials:
              - CVE-2022-24806 Improper Input Validation when SETing malformed
                OIDs in master agent and subagent simultaneously
              - CVE-2022-24807 A malformed OID in a SET request to
                SNMP-VIEW-BASED-ACM-MIB::vacmAccessTable can cause an
                out-of-bounds memory access.
              - CVE-2022-24808 A malformed OID in a SET request to
                NET-SNMP-AGENT-MIB::nsLogTable can cause a NULL pointer dereference
              - CVE-2022-24810 A malformed OID in a SET to the nsVacmAccessTable
                can cause a NULL pointer dereference.
          - To avoid these flaws, use strong SNMPv3 credentials and do not share them.
            If you must use SNMPv1 or SNMPv2c, use a complex community string
            and enhance the protection by restricting access to a given IP address range.
          - Thanks are due to Yu Zhang of VARAS@IIE and Nanyu Zhong of VARAS@IIE for
            reporting the following CVEs that have been fixed in this release, and
            to Arista Networks for providing fixes.
    
        misc:
          - Snmp-create-v3-user: Fix the snmpd.conf path   @datadir@ is
            expanded in ${datarootdir} so datarootdir must be set before
            @datadir@ is used.
    
        general: Many bug fixes
    
    *5.9.2*:
        skipped due to a last minute library versioning found bug -- use 5.9.3 instead
    
    *5.9.1*:
        General: Many bug fixes
    
    *5.9*
        snmplib:
          - Add IPv6 support to DTLSUDP transport CHANGES: snmplib: use new
            netsnmp_sockaddr_storage in netsnmp_addr_pair CHANGES: snmplib: add
            base_transport ptr for tunneled transports
    
        snmpd:
          - Security vulnerabilty in the ping MIB reported by Christopher Ertl
            from Microsoft fixed
          - Changing to a different uid/gid can only be done once
          - The extend mib is now read-only by default
    
        snmptrap:
          - BUG: 2899: Patch from Drew Roedersheimer to set library
            engineboots/time values before sending
    
        unspecified:
          - Add pkg-config support for building applications and sub-agents Use
            the netsnmp package when building Net-SNMP applications. Use the
            netsnmp-agent package when building Net-SNMP subagents.

--- net/net-snmp/patches/patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c
+++ net/net-snmp/patches/patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.7 2017/10/06 02:39:38 gavan Exp $
+$NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.8 2022/10/18 12:01:52 adam Exp $
 
---- agent/mibgroup/hardware/cpu/cpu_sysctl.c.orig	2014-12-08 20:23:22.000000000 +0000
+--- agent/mibgroup/hardware/cpu/cpu_sysctl.c.orig	2022-07-13 21:14:14.000000000 +0000
 +++ agent/mibgroup/hardware/cpu/cpu_sysctl.c
 @@ -10,6 +10,7 @@
  
@@ -9,7 +9,7 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.7 2017/10/06 02:39:
 +#include <errno.h>
  #include <sys/types.h>
  
- #if defined(__FreeBSD__)
+ #if defined(freebsd3)
 @@ -24,6 +25,9 @@
  #include <sys/param.h>
  #include <sys/sysctl.h>
@@ -20,7 +20,7 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.7 2017/10/06 02:39:
  #ifdef HAVE_VM_VM_PARAM_H
  #include <vm/vm_param.h>
  #endif
-@@ -43,7 +47,7 @@ void init_cpu_sysctl( void ) {
+@@ -46,7 +50,7 @@ void init_cpu_sysctl( void ) {
      int               i, n;
      size_t            siz;
      int               ncpu_mib[]  = { CTL_HW, HW_NCPU };
@@ -29,29 +29,7 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.7 2017/10/06 02:39:
      int               model_mib[] = { CTL_HW, HW_MODEL };
  #endif
      char              descr[ SNMP_MAXBUF ];
-@@ -51,14 +55,17 @@ void init_cpu_sysctl( void ) {
-     strcpy(cpu->name, "Overall CPU statistics");
- 
-     siz = sizeof(n);
--    sysctl(ncpu_mib, 2, &n, &siz, NULL, 0);
-+    if (sysctl(ncpu_mib, 2, &n, &siz, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl hw.ncpu failed (errno %d)\n", errno);
-     if ( n <= 0 )
-         n = 1;   /* Single CPU system */
-     siz = sizeof(descr);
--#if defined(__NetBSD__) && ( defined(__i386__) || defined(__x86_64__) )
--    sysctlbyname("machdep.cpu_brand", descr, &siz, NULL, 0);
-+#if defined(__FreeBSD__) && ( defined(__i386__) || defined(__x86_64__) )
-+    if (sysctlbyname("machdep.cpu_brand", descr, &siz, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl machdep.cpu_brand failed (errno %d)\n", errno)
- #else
--    sysctl(model_mib, 2, descr, &siz, NULL, 0);
-+    if (sysctl(model_mib, 2, descr, &siz, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl hw.model failed (errno %d)\n", errno);
- #endif
-     for ( i = 0; i < n; i++ ) {
-         cpu = netsnmp_cpu_get_byIdx( i, 1 );
-@@ -158,7 +165,7 @@
+@@ -174,7 +178,7 @@ struct __vmmeter {
      /*
       * Load the latest CPU usage statistics
       */
@@ -60,31 +38,7 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.7 2017/10/06 02:39:
  
      /*
       * Strictly speaking, BSDi ought to use
-@@ -190,9 +197,11 @@ int netsnmp_cpu_arch_load( netsnmp_cache
-     netsnmp_cpu_info *cpu = netsnmp_cpu_get_byIdx( -1, 0 );
- 
- #if defined(__FreeBSD__) || defined(__NetBSD__)
--    sysctlbyname("kern.cp_time", cpu_stats, &cpu_size, NULL, 0);
-+    if (sysctlbyname("kern.cp_time", cpu_stats, &cpu_size, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl kern.cp_time failed (errno %d)\n", errno);
- #else
--    sysctl(cpu_mib, 2,  cpu_stats, &cpu_size, NULL, 0);
-+    if (sysctl(cpu_mib, 2,  cpu_stats, &cpu_size, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl kern.cpu failed (errno %d)\n", errno);
- #endif
-     cpu->user_ticks = cpu_stats[CP_USER];
-     cpu->nice_ticks = cpu_stats[CP_NICE];
-@@ -206,7 +215,8 @@ int netsnmp_cpu_arch_load( netsnmp_cache
-          * Interrupt/Context Switch statistics
-          *   XXX - Do these really belong here ?
-          */
--    sysctl(mem_mib, 2, &mem_stats, &mem_size, NULL, 0);
-+    if (sysctl(mem_mib, 2, &mem_stats, &mem_size, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl vm.vm_meter failed (errno %d)\n", errno);
-     cpu->nInterrupts  = mem_stats.NS_VM_INTR;
-     cpu->nCtxSwitches = mem_stats.NS_VM_SWTCH;
-     cpu->swapIn       = mem_stats.NS_VM_SWAPIN;
-@@ -218,6 +228,32 @@ int netsnmp_cpu_arch_load( netsnmp_cache
+@@ -255,6 +259,32 @@ int netsnmp_cpu_arch_load( netsnmp_cache
      cpu->pageOut      = mem_stats.NS_VM_PAGEOUT;
  #endif
  
@@ -117,13 +71,3 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.7 2017/10/06 02:39:
  #ifdef NETSNMP_KERN_MCPU
  #if defined(KERN_CPTIME2)
      mcpu_size  = cpu_num*sizeof(cpu_stats);
-@@ -225,7 +261,8 @@ int netsnmp_cpu_arch_load( netsnmp_cache
- #elif defined(__NetBSD__)
-     mcpu_size  = cpu_num*sizeof(cpu_stats);
-     mcpu_stats = malloc(mcpu_size);
--    sysctlbyname("kern.cp_time", mcpu_stats, &mcpu_size, NULL, 0);
-+    if (sysctlbyname("kern.cp_time", mcpu_stats, &mcpu_size, NULL, 0) == -1)
-+	snmp_log(LOG_ERR, "sysctl kern.mcpu failed (errno %d)\n", errno);
- #elif defined(__FreeBSD__)
-     if (cp_times == -1) {
- 	int ret = sysctlbyname("kern.cp_times", NULL, &mcpu_size, NULL, 0);
