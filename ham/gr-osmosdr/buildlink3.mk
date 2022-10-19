e607d52473abf52002d8d9b45696ee81aeae5c53
adam <adam@pkgsrc.org>
2022-10-19 18:38:29 +0000

    gr-osmosdr: fix building + cleanups

--- ham/gr-osmosdr/buildlink3.mk
+++ ham/gr-osmosdr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/12/11 14:57:52 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/10/19 18:38:29 adam Exp $
 
 BUILDLINK_TREE+=	gr-osmosdr
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	gr-osmosdr
 GR_OSMOSDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gr-osmosdr+=	gr-osmosdr>=0.2.2
-BUILDLINK_ABI_DEPENDS.gr-osmosdr?=	gr-osmosdr>=0.2.3.20210128nb4
+BUILDLINK_ABI_DEPENDS.gr-osmosdr+=	gr-osmosdr>=0.2.3.20210128nb4
 BUILDLINK_PKGSRCDIR.gr-osmosdr?=	../../ham/gr-osmosdr
 
-.include	"../../devel/boost-headers/buildlink3.mk"
-.include	"../../ham/gnuradio-core/buildlink3.mk"
+.include "../../devel/boost-headers/buildlink3.mk"
+.include "../../ham/gnuradio-core/buildlink3.mk"
 .endif	# GR_OSMOSDR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gr-osmosdr
