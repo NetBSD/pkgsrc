029ff1f770f4cef43faef738dc27d73ebc75b284
adam <adam@pkgsrc.org>
2022-10-19 13:16:45 +0000

    postgresql15: added version 15.0
    PostgreSQL 15 contains many new features and enhancements, including:
    
    Support for the SQL MERGE command.
    
    Selective publication of tables' contents within logical replication publications, through the ability to specify column lists and row filter conditions.
    
    More options for compression, including support for Zstandard (zstd) compression. This includes support for performing compression on the server side during pg_basebackup.
    
    Support for structured server log output using the JSON format.
    
    Performance improvements, particularly for in-memory and on-disk sorting.

---
+++ databases/postgresql15-client/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/10/19 13:16:45 adam Exp $
+
+BUILDLINK_TREE+=	postgresql15-client
+
+.if !defined(POSTGRESQL15_CLIENT_BUILDLINK3_MK)
+POSTGRESQL15_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql15-client+=	postgresql15-client>=15.0
+BUILDLINK_ABI_DEPENDS.postgresql15-client+=	postgresql15-client>=15.0
+BUILDLINK_PKGSRCDIR.postgresql15-client?=	../../databases/postgresql15-client
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql15-client+=	-lpq ${BUILDLINK_LDADD.gettext}
+BUILDLINK_FILES.postgresql15-client+=	bin/pg_config
+
+pkgbase := postgresql15-client
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql15-client:Mlz4}
+.include "../../archivers/lz4/buildlink3.mk"
+.endif
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "SunOS"
+.include "../../devel/ossp-uuid/buildlink3.mk"
+.endif
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif # POSTGRESQL15_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql15-client
