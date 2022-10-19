e2df9869a9d5546ea8e1cf518fbc1f99a7381ce2
he <he@pkgsrc.org>
2022-10-19 17:14:32 +0000

    x11/pixman: fix a couple more altivec-related bugs for powerpc.
    The patch fixes a couple of bugs related to int32 / vector
    initialization, pointed out by the compiler.  Now builds without
    warnings.

--- x11/pixman/patches/patch-pixman_pixman-vmx.c
+++ x11/pixman/patches/patch-pixman_pixman-vmx.c
@@ -1,12 +1,46 @@
-$NetBSD: patch-pixman_pixman-vmx.c,v 1.2 2022/10/18 12:53:10 wiz Exp $
+$NetBSD: patch-pixman_pixman-vmx.c,v 1.3 2022/10/19 17:14:32 he Exp $
 
 Add a fix so that this builds on NetBSD/powerpc.  Otherwise we get
 pixman-vmx.c:2936:5: error: subscripted value is neither array nor pointer
+Also fix something which clearly must be bugs with loading
+vector registers from single 32-bit ints.
+
 https://gitlab.freedesktop.org/pixman/pixman/-/merge_requests/64/
 
 --- pixman/pixman-vmx.c.orig	2016-01-04 09:13:54.000000000 +0000
 +++ pixman/pixman-vmx.c
 @@ -2913,32 +2913,29 @@ scaled_nearest_scanline_vmx_8888_8888_OV
+@@ -292,7 +292,9 @@ create_mask_1x32_128 (const uint32_t *sr
+ static force_inline vector unsigned int
+ create_mask_32_128 (uint32_t mask)
+ {
+-    return create_mask_1x32_128(&mask);
++    uint32_t nmask[4]; /* vector sized array */
++    nmask[3] = nmask[2] = nmask[1] = nmask[0] = mask;
++    return create_mask_1x32_128(nmask);
+ }
+ 
+ static force_inline vector unsigned int
+@@ -2435,6 +2437,9 @@ vmx_fill (pixman_implementation_t *imp,
+     uint8_t *byte_line;
+ 
+     vector unsigned int vfiller;
++    uint32_t fillers[4];
++
++    fillers[3] = fillers[2] = fillers[1] = fillers[0] = filler;
+ 
+     if (bpp == 8)
+     {
+@@ -2471,7 +2476,7 @@ vmx_fill (pixman_implementation_t *imp,
+ 	return FALSE;
+     }
+ 
+-    vfiller = create_mask_1x32_128(&filler);
++    vfiller = create_mask_1x32_128(fillers);
+ 
+     while (height--)
+     {
+@@ -2913,32 +2918,29 @@ scaled_nearest_scanline_vmx_8888_8888_OV
  
      while (w >= 4)
      {
