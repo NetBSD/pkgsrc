5d5aa419148f5cfccd725e1c388576b812921890
wiz <wiz@pkgsrc.org>
2022-10-22 16:54:53 +0000

    libXrender: update to 0.9.11.
    Alan Coopersmith (10):
          Update README for gitlab migration
          Update configure.ac bug URL for gitlab migration
          Build xz tarballs instead of bzip2
          Fix spelling/wording issues
          gitlab CI: add a basic build test
          Remove unnecessary casts from malloc & free calls
          Reduce variable scopes as recommended by cppcheck
          Resolve -Wsign-compare warnings
          Rename xDepth to xPDepth to quiet -Wshadow warnings
          libXrender 0.9.11
    
    Dmitry Karasik (1):
          fix coredumps in XRenderComputeTrapezoids (issue #1)
    
    Emil Velikov (1):
          autogen.sh: use quoted string variables
    
    Mihail Konev (1):
          autogen: add default patch prefix
    
    Oliver Hattshire (1):
          WIP: Documentation
    
    Peter Hutterer (1):
          autogen.sh: use exec instead of waiting for configure to finish
    
    Robin Linden (1):
          Add missing HAVE_CONFIG_H guard to Xrenderint.h
    
    Thomas E. Dickey (8):
          amend cppcheck-scope change, fixing c89 build
          additional cppcheck-scope warning
          cppcheck (removing unused assignment lets variable scope reduction)
          use casts to reduce compiler warnings (no object change)
          use _Xconst with DataInt32/DataInt16/memcpy to reduce strict compiler warnings
          use _X_UNUSED for compiler-warnings
          whitespace fix
          fix regression

--- x11/libXrender/buildlink3.mk
+++ x11/libXrender/buildlink3.mk
@@ -1,8 +1,8 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/09/27 16:32:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/10/22 16:54:53 wiz Exp $
 
 BUILDLINK_TREE+=	libXrender
 
-.  if !defined(LIBXRENDER_BUILDLINK3_MK)
+.if !defined(LIBXRENDER_BUILDLINK3_MK)
 LIBXRENDER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libXrender+=	libXrender>=0.9.2
@@ -10,6 +10,6 @@ BUILDLINK_PKGSRCDIR.libXrender?=	../../x11/libXrender
 
 .include "../../x11/xorgproto/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
-.  endif # LIBXRENDER_BUILDLINK3_MK
+.endif # LIBXRENDER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libXrender
