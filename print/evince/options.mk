ec7ad70cc9f787a2ad0e3c41c9bb8a08f525cd68
prlw1 <prlw1@pkgsrc.org>
2022-10-25 08:38:09 +0000

    Import evince 43.0 as print/evince with a view to removing print/evince3
    - nearly three years of development since 3.34.2
    - autotools -> meson
    - tiff and ps are now options, off by default
    - no longer calls gs, but uses libspectre which depends on gs
    - no need for a buildink3.mk file - not used

---
+++ print/evince/options.mk
@@ -0,0 +1,64 @@
+# $NetBSD: options.mk,v 1.8 2022/10/25 08:38:09 prlw1 Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.evince
+PKG_SUPPORTED_OPTIONS=	dbus djvu dvi gstreamer ps tiff xps
+PKG_SUGGESTED_OPTIONS=	dbus
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
+
+.if !empty(PKG_OPTIONS:Mdbus)
+PLIST.dbus=	yes
+USE_TOOLS+=	gdbus-codegen
+.include "../../sysutils/dbus/buildlink3.mk"
+BUILDLINK_API_DEPENDS.dbus-glib+=	dbus-glib>=0.71
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.else
+MESON_ARGS+=	-Ddbus=false
+.endif
+
+.if !empty(PKG_OPTIONS:Mdjvu)
+PLIST.djvu=	yes
+BUILDLINK_API_DEPENDS.djvulibre-lib+=	djvulibre-lib>=3.5.22
+.include "../../graphics/djvulibre-lib/buildlink3.mk"
+.else
+MESON_ARGS+=	-Ddjvu=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mdvi)
+PLIST.dvi=	yes
+.include "../../print/kpathsea/buildlink3.mk"
+.else
+MESON_ARGS+=	-Ddvi=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mgstreamer)
+PLIST.gstreamer=	yes
+.include "../../multimedia/gst-plugins1-base/buildlink3.mk"
+.include "../../multimedia/gstreamer1/buildlink3.mk"
+.else
+MESON_ARGS+=	-Dmultimedia=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mps)
+PLIST.ps=	yes
+.include "../../print/libspectre/buildlink3.mk"
+MESON_ARGS+=	-Dps=enabled
+.else
+MESON_ARGS+=	-Dps=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mtiff)
+PLIST.tiff=		yes
+.include "../../graphics/tiff/buildlink3.mk"
+.else
+MESON_ARGS+=	-Dtiff=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mxps)
+PLIST.xps=		yes
+.include "../../print/libgxps/buildlink3.mk"
+.else
+MESON_ARGS+=	-Dxps=disabled
+.endif
