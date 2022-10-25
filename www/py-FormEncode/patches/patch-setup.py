eb180a19c3d809d061485f38b726d92d5edc0ba6
adam <adam@pkgsrc.org>
2022-10-25 08:30:14 +0000

    py-FormEncode: remove setuptools_scm_git_archive from requirements

--- www/py-FormEncode/patches/patch-setup.py
+++ www/py-FormEncode/patches/patch-setup.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-setup.py,v 1.2 2021/12/06 14:41:21 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2022/10/25 08:30:14 adam Exp $
 
 Remove hardcoded version limits.
+Remove setuptools_scm_git_archive from requirements.
 
 --- setup.py.orig	2021-10-05 12:14:00.000000000 +0000
 +++ setup.py
-@@ -16,7 +16,7 @@ if not (2,7) <= sys.version_info[:2] < (
+@@ -16,12 +16,11 @@ if not (2,7) <= sys.version_info[:2] < (
  
  tests_require = [
      'pytest<4.7' if sys.version_info[:2] < (3,0) else 'pytest',
@@ -13,3 +14,8 @@ Remove hardcoded version limits.
      'pycountry<19' if sys.version_info < (3,0) else 'pycountry']
  
  setup_requires = [
+     'setuptools_scm<6.0' if sys.version_info[:2] < (3,0) else 'setuptools_scm',
+-    'setuptools_scm_git_archive',
+ ]
+ 
+ doctests = ['docs/htmlfill.txt', 'docs/Validator.txt',
