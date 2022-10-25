2d680fd51dc32a2cc0d2c1b2c30f44d269cef983
adam <adam@pkgsrc.org>
2022-10-25 15:56:21 +0000

    py-landslide: updated to 1.1.9
    v1.1.9
    - Pin `MarkupSafe` dependency to `v1.1.1`

--- www/py-landslide/patches/patch-setup.py
+++ www/py-landslide/patches/patch-setup.py
@@ -1,18 +1,20 @@
-$NetBSD: patch-setup.py,v 1.2 2020/12/15 09:07:41 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2022/10/25 15:56:21 adam Exp $
 
 Relax dependencies.
 
---- setup.py.orig	2020-08-20 22:45:13.000000000 +0000
+--- setup.py.orig	2022-06-12 03:45:27.000000000 +0000
 +++ setup.py
-@@ -23,11 +23,11 @@ setup(
+@@ -23,12 +23,12 @@ setup(
          'textile'
      ],
      install_requires=[
+-        'MarkupSafe==1.1.1',
 -        'Jinja2==2.10.1',
 -        'Markdown==2.6.11',
 -        'Pygments==2.2.0',
 -        'docutils==0.14',
 -        'six==1.11.0'
++        'MarkupSafe>=1.1.1',
 +        'Jinja2>=2.10.1',
 +        'Markdown>=2.6.11',
 +        'Pygments>=2.2.0',
