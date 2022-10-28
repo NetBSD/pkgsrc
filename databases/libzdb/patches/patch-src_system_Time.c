28122a8eb913702d2ef1304f9766059a0ab4b0f8
he <he@pkgsrc.org>
2022-10-28 18:39:24 +0000

    Update databases/libzdb to version 3.2.3.
    Pkgsrc changes:
     * More patching of isxxxx() uses to u_char.
    
    Upstream changes:
    
    Version 3.2.3
    -------------
    * New: Provide better error reporting if a Connection cannot be
      obtained from the Connection Pool by introducing the method
      ConnectionPool_getConnectionOrException(). In C++ the exception
      thrown now contains the actual cause instead of a generic error.
    * Fix: In C++ guard against stopping the Connection Pool with active
      Connections. I.e. all Connections should be returned to the pool
      before explicitly attempting to stop the pool. This is due to how
      C++ destructors are called at scope end; if there is a reference
      to a live Connection object after the pool is stopped it might cause
      a dealloc sequence problem.
    * Fix: Issue #50 where unit test 5 could fail for MariaDB because
      mysql_stmt_affected_rows() only returns actual affected rows, not
      matched rows which MySQL does.
    * Fix: plus other minor improvements and fixes

--- databases/libzdb/patches/patch-src_system_Time.c
+++ databases/libzdb/patches/patch-src_system_Time.c
@@ -1,10 +1,19 @@
-$NetBSD: patch-src_system_Time.c,v 1.1 2022/10/28 18:25:01 he Exp $
+$NetBSD: patch-src_system_Time.c,v 1.2 2022/10/28 18:39:25 he Exp $
 
-Fix one more pair of isxxxx() uses by casting arg to u_char.
+Fix uses of isxxxx() by casting arg to u_char.
 
 --- src/system/Time.c.orig	2020-03-31 22:57:32.000000000 +0000
 +++ src/system/Time.c
-@@ -290,9 +290,9 @@ yy10:
+@@ -147,7 +147,7 @@ static inline int _a2i(const char *a, in
+ }
+ 
+ static inline int _m2i(const char m[static 3]) {
+-        char month[3] = {[0] = tolower(m[0]), [1] = tolower(m[1]), [2] = tolower(m[2])};
++        char month[3] = {[0] = tolower((u_char)m[0]), [1] = tolower((u_char)m[1]), [2] = tolower((u_char)m[2])};
+         static char *months = "janfebmaraprmayjunjulaugsepoctnovdec";
+         for (int i = 0; i < 34; i += 3) {
+                 if (memcmp(months + i, month, 3) == 0)
+@@ -318,9 +318,9 @@ yy9:
  	{ // Timezone: +-HH:MM, +-HH or +-HHMM is offset from UTC in seconds
                          if (have_time) { // Only set timezone if we have parsed time
                                  tm.TM_GMTOFF = _a2i(token + 1, 2) * 3600;
