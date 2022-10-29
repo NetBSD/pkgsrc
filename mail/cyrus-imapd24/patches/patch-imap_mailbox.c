e3af5cffc069e4a51a2e0b7a1d01729b78d0b98a
he <he@pkgsrc.org>
2022-10-29 14:44:07 +0000

    mail/cyrus-imapd24: fix issue related to own assert() definition.
    The C standard says assert() must be a void expression, so change
    the definition from being a block to an expression, loosely patterned
    after our <assert.h> header.  This makes it compatible with the
    various perl headers this program uses.  Also, add a missing semicolon
    in a sequence of assert() uses, as this error was now exposed.
    
    Bump PKGREVISION.

--- mail/cyrus-imapd24/patches/patch-imap_mailbox.c
+++ mail/cyrus-imapd24/patches/patch-imap_mailbox.c
@@ -1,11 +1,12 @@
-$NetBSD: patch-imap_mailbox.c,v 1.3 2014/06/17 01:08:59 obache Exp $
+$NetBSD: patch-imap_mailbox.c,v 1.4 2022/10/29 14:44:07 he Exp $
 
 * cast to unsigned long for platform sizeof(time_t) > sizeof(unsigned long).
   https://bugzilla.cyrusimap.org/show_bug.cgi?id=3376
+* Fix an issue related to use of the assert() macro (statement or block?)
 
 --- imap/mailbox.c.orig	2012-12-01 19:57:54.000000000 +0000
 +++ imap/mailbox.c
-@@ -1960,10 +1960,10 @@ bit32 make_sync_crc(struct mailbox *mail
+@@ -1961,10 +1961,10 @@ bit32 make_sync_crc(struct mailbox *mail
  	flagcrc ^= crc32_cstring(buf);
      }
  
@@ -19,3 +20,12 @@ $NetBSD: patch-imap_mailbox.c,v 1.3 2014/06/17 01:08:59 obache Exp $
  	    message_guid_encode(&record->guid));
  
      return crc32_cstring(buf);
+@@ -2179,7 +2179,7 @@ int mailbox_append_index_record(struct m
+     assert(mailbox_index_islocked(mailbox, 1));
+ 
+     /* Append MUST be a higher UID than any we've yet seen */
+-    assert(record->uid > mailbox->i.last_uid)
++    assert(record->uid > mailbox->i.last_uid);
+ 
+     /* Append MUST have a message with data */
+     assert(record->size);
