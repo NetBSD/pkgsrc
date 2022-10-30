ce2d4d385234c00b1f11840b113403ef95f6baab
triaxx <triaxx@pkgsrc.org>
2022-10-30 06:41:56 +0000

    awesome: Fix manpages build for Ruby 3.1
    pkgsrc changes:
    ---------------
      * Update patch to find executable provided by ruby31-asciidoctor.
      * Bump revision.

--- wm/awesome/patches/patch-awesomeConfig.cmake
+++ wm/awesome/patches/patch-awesomeConfig.cmake
@@ -1,17 +1,18 @@
-$NetBSD: patch-awesomeConfig.cmake,v 1.7 2021/05/15 16:42:43 triaxx Exp $
+$NetBSD: patch-awesomeConfig.cmake,v 1.8 2022/10/30 06:41:57 triaxx Exp $
 
 If pkgtools/pkg_alternatives is not installed, the asciidoctor
 binary is suffixed by the ruby version.
 
 --- awesomeConfig.cmake.orig	2019-01-28 06:53:24.000000000 +0000
 +++ awesomeConfig.cmake
-@@ -38,6 +38,9 @@ endmacro()
+@@ -38,6 +38,10 @@ endmacro()
  a_find_program(GIT_EXECUTABLE git FALSE)
  # programs needed for man pages
  a_find_program(ASCIIDOCTOR_EXECUTABLE asciidoctor FALSE)
 +a_find_program(ASCIIDOCTOR_EXECUTABLE asciidoctor26 FALSE)
 +a_find_program(ASCIIDOCTOR_EXECUTABLE asciidoctor27 FALSE)
 +a_find_program(ASCIIDOCTOR_EXECUTABLE asciidoctor30 FALSE)
++a_find_program(ASCIIDOCTOR_EXECUTABLE asciidoctor31 FALSE)
  a_find_program(GZIP_EXECUTABLE gzip FALSE)
  # Lua documentation
  if(GENERATE_DOC)
