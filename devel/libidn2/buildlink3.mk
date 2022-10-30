393284e8ae049c3fdab50bcaa3d382ae995a6383
adam <adam@pkgsrc.org>
2022-10-30 11:19:21 +0000

    libidn2: add TEST_TARGET; pkglint fix

--- devel/libidn2/buildlink3.mk
+++ devel/libidn2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/10/26 10:31:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/10/30 11:19:21 adam Exp $
 
 BUILDLINK_TREE+=	libidn2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libidn2
 LIBIDN2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libidn2+=	libidn2>=2.0.0
-BUILDLINK_ABI_DEPENDS.libidn2?=	libidn2>=2.3.3nb1
+BUILDLINK_ABI_DEPENDS.libidn2+=	libidn2>=2.3.3nb1
 BUILDLINK_PKGSRCDIR.libidn2?=	../../devel/libidn2
 
 .include "../../converters/libiconv/buildlink3.mk"
