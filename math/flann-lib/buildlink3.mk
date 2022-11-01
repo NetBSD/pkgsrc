204dd613b3c1d7f0921362134bc5b314fddd46b6
adam <adam@pkgsrc.org>
2022-11-01 21:29:07 +0000

    flann-lib: updated to 1.9.2
    Version 1.9.2
    * Removed redundant assignment
    * Removed unnecessary null checks before delete
    * Reverted PR 424 due to lack of portability
    * fscanf fix
    * Out of bounds check
    * Fixed MacOS build
    * Fixed build system with dummy.c hack
    * Fixed misleading indentation in util/any.h
    * Included datasets in repo
    * Correct typo in definition
    * Fix typos
    * CMakefile CUDA sources fix
    * Documentation fix
    * Scoping issue fix
    * Documentation fixes
    * Changed return value
    * Fixed CUDA crash - guarantee prealloc > 0
    * Fixed wrong variable use
    * Fixed RNG initialization
    * Updated link to PDF

--- math/flann-lib/buildlink3.mk
+++ math/flann-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/04/27 15:26:34 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/11/01 21:29:07 adam Exp $
 
 BUILDLINK_TREE+=	flann-lib
 
@@ -7,7 +7,7 @@ FLANN_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.flann-lib+=	flann-lib>=1.9.0
 BUILDLINK_ABI_DEPENDS.flann-lib+=	flann-lib>=1.9.0
-BUILDLINK_PKGSRCDIR.flann-lib?=	../../math/flann-lib
+BUILDLINK_PKGSRCDIR.flann-lib?=		../../math/flann-lib
 
 .include "../../devel/hdf5-c++/buildlink3.mk"
 
