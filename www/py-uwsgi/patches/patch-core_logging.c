0fc21dfdbe433f7260b1e92deba9d4f7b5663917
wiz <wiz@pkgsrc.org>
2022-11-02 07:26:47 +0000

    py-uwsgi: add links to upstream pull request to patches

--- www/py-uwsgi/patches/patch-core_logging.c
+++ www/py-uwsgi/patches/patch-core_logging.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-core_logging.c,v 1.4 2022/01/25 10:30:51 wiz Exp $
+$NetBSD: patch-core_logging.c,v 1.5 2022/11/02 07:26:47 wiz Exp $
 
 Fix build for NetBSD.
+https://github.com/unbit/uwsgi/pull/2506
 
 --- core/logging.c.orig	2021-10-06 05:22:45.000000000 +0000
 +++ core/logging.c
