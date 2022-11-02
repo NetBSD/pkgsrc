74395c66b6c046e039f1592b52bef6309bf73bd2
jperkin <jperkin@pkgsrc.org>
2022-11-02 17:02:10 +0000

    mysql80: Update to 8.0.31.
    Includes a number of pkgsrc cleanups, removing nonsense that has been
    cargo-culted since the MySQL 3.23.x days(!).  Stop shipping mysql-test, it's
    absolutely huge and unnecessary.  Add XXX to undocumented patches that we
    should probably just drop, and make others easier to maintain.
    
    Upstream changes are far too numerous to include here, so here are the links to
    the corresponding release notes for each since 8.0.24:
    
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-31.html
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-30.html
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-29.html
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-28.html
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-27.html
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-26.html
      https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-25.html
    
    Tested on SmartOS, I'll monitor bulk build reports for any fallout on other
    platforms (it takes ages to build).

--- databases/mysql80-server/buildlink3.mk
+++ databases/mysql80-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/06/28 11:31:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/11/02 17:02:11 jperkin Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
@@ -7,7 +7,7 @@ MYSQL_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.7.12
 BUILDLINK_ABI_DEPENDS.mysql-server?=	mysql-server>=8.0.24nb7
-BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql57-server
+BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql80-server
 BUILDLINK_LIBDIRS.mysql-server?=	lib
 
 .endif	# MYSQL_SERVER_BUILDLINK3_MK
