77f0aae09f5c946f443bf5703bbf1275275e9c49
wiz <wiz@pkgsrc.org>
2022-11-02 07:26:47 +0000

    py-uwsgi: add links to upstream pull request to patches

--- www/py-uwsgi/patches/patch-uwsgi.h
+++ www/py-uwsgi/patches/patch-uwsgi.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-uwsgi.h,v 1.9 2022/01/25 10:30:51 wiz Exp $
+$NetBSD: patch-uwsgi.h,v 1.10 2022/11/02 07:26:47 wiz Exp $
 
 Avoid using "sun" which breaks on SunOS
+https://github.com/unbit/uwsgi/pull/2506
 
 --- uwsgi.h.orig	2020-06-17 09:03:34.000000000 +0000
 +++ uwsgi.h
