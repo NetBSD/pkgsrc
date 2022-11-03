78130c2d737d2368866de8c2b56aa567d734bc8b
nros <nros@pkgsrc.org>
2022-11-03 12:35:00 +0000

    fcitx-libthai: fix building with const **char in second argument to iconv

--- inputmethod/fcitx5-libthai/patches/patch-src_iconvwrapper.cpp
+++ inputmethod/fcitx5-libthai/patches/patch-src_iconvwrapper.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_iconvwrapper.cpp,v 1.1 2022/04/10 07:59:29 nia Exp $
+$NetBSD: patch-src_iconvwrapper.cpp,v 1.2 2022/11/03 12:35:00 nros Exp $
 
 Fix building on NetBSD <=9 with non-POSIX iconv.
 
@@ -20,18 +20,17 @@ Fix building on NetBSD <=9 with non-POSIX iconv.
  class IconvWrapperPrivate {
  public:
      IconvWrapperPrivate(iconv_t conv) : conv_(conv) {}
-@@ -37,9 +46,13 @@ std::vector<uint8_t> IconvWrapper::tryCo
+@@ -37,8 +46,12 @@ std::vector<uint8_t> IconvWrapper::tryCo
          result.resize(s.size() * 10);
          size_t byteLength = s.size();
          size_t byteRemains = result.size();
 -        char *data = const_cast<char *>(s.data());
          char *outData = reinterpret_cast<char *>(result.data());
 +#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
-+        auto err = iconv(conv, s.data(), &byteLength, &outData, &byteRemains);
++        const char *data = s.data();
 +#else
 +        char *data = const_cast<char *>(s.data());
-         auto err = iconv(conv, &data, &byteLength, &outData, &byteRemains);
 +#endif
+         auto err = iconv(conv, &data, &byteLength, &outData, &byteRemains);
          if (err == static_cast<size_t>(-1)) {
              continue;
-         }
