71e618aa81e7ee38975a04451ca7ee92832d0e39
schmonz <schmonz@pkgsrc.org>
2022-11-03 00:00:08 +0000

    Fix macOS build with the 13.0 SDK, which has its own definition of __deprecated__.

--- devel/libowfat/patches/patch-scan_scan__iso8601.c
+++ devel/libowfat/patches/patch-scan_scan__iso8601.c
@@ -1,10 +1,21 @@
-$NetBSD: patch-scan_scan__iso8601.c,v 1.1 2020/06/26 19:21:52 schmonz Exp $
+$NetBSD: patch-scan_scan__iso8601.c,v 1.2 2022/11/03 00:00:08 schmonz Exp $
 
-Fix Illumos build.
+Fix Illumos build. Then fix macOS build with the 13.0 SDK, which has its
+own definition of __deprecated__.
 
---- scan/scan_iso8601.c.orig	2016-04-27 14:07:50.000000000 +0000
+--- scan/scan_iso8601.c.orig	2016-04-27 14:07:50
 +++ scan/scan_iso8601.c
-@@ -44,7 +44,7 @@ size_t scan_iso8601(const char* in,struc
+@@ -4,6 +4,9 @@
+ #include "byte.h"
+ #include "case.h"
+ #include <time.h>
++#ifdef __APPLE__
++#undef __deprecated__
++#endif
+ #include <stdlib.h>
+ 
+ #ifdef sgi
+@@ -44,7 +47,7 @@ size_t scan_iso8601(const char* in,struct timespec* t)
      }
    }
  
