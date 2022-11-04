bd83b2966b10c5ffc6d2275cf8ed6c84e3d67a12
adam <adam@pkgsrc.org>
2022-11-04 19:46:48 +0000

    cmake cmake-gui: updated to 3.24.3
    CMake 3.24.3
    * cmStringAlgorithms: Add functions to parse strings to long long integers
    * file: Avoid strange istringstream crash in cmake.org binaries on Alpine Linux
    * Xcode: Drop CMAKE_INTDIR= definition in Swift targets
    * Tests: Update RunCMake.XcodeProject iOS cases for Xcode 14.0
    * gitlab-ci: update macOS jobs to use Xcode 14.0
    * COMPILE_WARNING_AS_ERROR: Fix internal formatting of options table
    * LLVMFlang: Add support for mixed-language linking with Fortran
    * Help: Add versionadded for string(TIMESTAMP) %f specifier
    * try_compile: Honor CMP0128 setting in test project
    * Tests: Explicitly allow usage of git file-based protocol in test cases
    * cm/filesystem: Fix crash with pre-C++11 std::string GNU ABI in C++17
    * Tests: Avoid running C++11 test on GNU < 4.7
    * VS: Fix crash finding vswhere on 32-bit Windows
    * Ninja: Restore support for non-ascii paths on Windows with ninja<=1.10
    * Tests: Add case for ninja with non-ascii chars
    * CPack/IFW: Add support for QtIFW 4.4
    * CPack: Require no argument for --trace and --trace-expand
    * Apple: Fix regression when linking a framework with postfix
    * FindMatlab: Add MEX binary file suffix for Apple Silicon native Matlab
    * FindMatlab: add R2022b 9.13 version map
    * Intel/IntelLLVM: Fortran has distinct "-Werror"-like flag
    * CPack/NSIS: Fix installer not waiting for uninstaller to finish
    * CUDA: Add support for the two new architectures in 11.8
    * FindJNI: replace CMAKE_ANDROID_API by CMAKE_SYSTEM_VERSION
    * Android: Avoid searching API level directories matching architecture bitness
    * ctest: only report make-level errors when no others are found
    * LLVMFlang: Add support for Windows
    * LLVMFlang: Add a required line to define linking rules on Windows

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.30 2022/09/26 13:37:32 adam Exp $
+# $NetBSD: version.mk,v 1.31 2022/11/04 19:46:48 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.24.2
+CMAKE_VERSION=	3.24.3
 CMAKE_API=	${CMAKE_VERSION:R}
