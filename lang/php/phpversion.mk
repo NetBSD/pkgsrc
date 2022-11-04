9aaa956943e1bd7ca84db91bf0bd515bbd3c4880
taca <taca@pkgsrc.org>
2022-11-04 00:40:58 +0000

    lang/php74: update to 7.4.33
    7.4.33 (2022-11-03)
    
    - GD:
      . Fixed bug #81739: OOB read due to insufficient input validation in
        imageloadfont(). (CVE-2022-31630) (cmb)
    
    - Hash:
      . Fixed bug #81738: buffer overflow in hash_update() on long parameter.
        (CVE-2022-37454) (nicky at mouha dot be)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.379 2022/10/30 10:18:35 taca Exp $
+# $NetBSD: phpversion.mk,v 1.380 2022/11/04 00:40:58 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP74_VERSION=	7.4.32
+PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.25
 PHP81_VERSION=	8.1.12
 
