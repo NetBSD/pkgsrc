3475e7731da3464eb5856b4651e95f73e89267ca
bsiegert <bsiegert@pkgsrc.org>
2022-11-05 19:21:29 +0000

    Pullup ticket #6700 - requested by taca lang/php80: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.379
    - lang/php80/distinfo                                           1.26
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Oct 30 10:18:35 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php80: distinfo
    
       Log Message:
       lang/php80: update to 8.0.25
    
       8.0.25 (2022-10-27)
    
       - GD:
         . Fixed bug #81739: OOB read due to insufficient input validation in
           imageloadfont(). (CVE-2022-31630) (cmb)
    
       - Hash:
         . Fixed bug #81738: buffer overflow in hash_update() on long parameter.
           (CVE-2022-37454) (nicky at mouha dot be)
    
       - Session:
         . Fixed bug GH-9583 (session_create_id() fails with user defined save handler
           that doesn't have a validateId() method). (Girgias)
    
       - Streams:
         . Fixed bug GH-9590 (stream_select does not abort upon exception or empty
           valid fd set). (Arnaud)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.374.2.4 2022/11/05 19:15:09 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.374.2.5 2022/11/05 19:21:29 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.32
-PHP80_VERSION=	8.0.24
+PHP80_VERSION=	8.0.25
 PHP81_VERSION=	8.1.12
 
 # Define initial release of major version.
