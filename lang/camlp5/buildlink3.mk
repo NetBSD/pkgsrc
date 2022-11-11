1273d9d058b1e2a18a689d8babbc068f8f55c3e9
jaapb <jaapb@pkgsrc.org>
2022-11-11 10:33:00 +0000

    Updated textproc/camlp5 to the latest version, 8.00.03.
    This includes several changes, including breaking ones; for full details,
    see the changelog in the distribution.
    
    This release adds support for the newest OCaml versions up to 4.14.0.

--- lang/camlp5/buildlink3.mk
+++ lang/camlp5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2022/05/24 18:51:53 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2022/11/11 10:33:00 jaapb Exp $
 #
 
 BUILDLINK_TREE+=	camlp5
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	camlp5
 .if !defined(CAMLP5_BUILDLINK3_MK)
 CAMLP5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=7.00
-BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=7.13nb2
+BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=8.00
+BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=8.00.03
 BUILDLINK_PKGSRCDIR.camlp5?=	../../lang/camlp5
 
 .include "../../lang/ocaml/buildlink3.mk"
