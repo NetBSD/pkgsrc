eb4f1b743c9b30715a5460e3f14958d1f5a395c8
gdt <gdt@pkgsrc.org>
2022-11-12 13:24:09 +0000

    chat/matrix-synapse: Update to 1.71.0
    Synapse 1.71.0 (2022-11-08)
    ===========================
    
    Please note that, as announced in the release notes for Synapse
    1.69.0, legacy Prometheus metric names are now disabled by default.
    They will be removed altogether in Synapse 1.73.0.  If not already
    done, server administrators should update their dashboards and
    alerting rules to avoid using the deprecated metric names.  See the
    [upgrade
    notes](https://matrix-org.github.io/synapse/v1.71/upgrade.html#upgrading-to-v1710)
    for more details.
    
    **Note:** in line with our [deprecation
      policy](https://matrix-org.github.io/synapse/latest/deprecation_policy.html)
      for platform dependencies, this will be the last release to support
      PostgreSQL 10, which reaches upstream end-of-life on November 10th,
      2022. Future releases of Synapse will require PostgreSQL 11+.
    
    Features
    --------
    
    - Support back-channel logouts from OpenID Connect
      providers. ([\#11414](https://github.com/matrix-org/synapse/issues/11414))
    
    - Allow use of Postgres and SQLlite full-text search operators in
      search
      queries. ([\#11635](https://github.com/matrix-org/synapse/issues/11635),
      [\#14310](https://github.com/matrix-org/synapse/issues/14310),
      [\#14311](https://github.com/matrix-org/synapse/issues/14311))
    
    - Implement
      [MSC3664](https://github.com/matrix-org/matrix-doc/pull/3664),
      Pushrules for relations. Contributed by
      Nico. ([\#11804](https://github.com/matrix-org/synapse/issues/11804))
    
    - Improve aesthetics of HTML templates. Note that these changes do not
      retroactively apply to templates which have been
      [customised](https://matrix-org.github.io/synapse/latest/templates.html#templates)
      by server
      admins. ([\#13652](https://github.com/matrix-org/synapse/issues/13652))
    
    - Enable write-ahead logging for SQLite installations. Contributed by
      [@asymmetric](https://github.com/asymmetric). ([\#13897](https://github.com/matrix-org/synapse/issues/13897))
    
    - Show erasure status when [listing
      users](https://matrix-org.github.io/synapse/latest/admin_api/user_admin_api.html#query-user-account)
      in the Admin
      API. ([\#14205](https://github.com/matrix-org/synapse/issues/14205))
    
    - Provide a specific error code when a `/sync` request provides a
      filter which doesn't represent a JSON
      object. ([\#14262](https://github.com/matrix-org/synapse/issues/14262))

--- chat/matrix-synapse/patches/patch-synapse_handlers_room.py
+++ chat/matrix-synapse/patches/patch-synapse_handlers_room.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-synapse_handlers_room.py,v 1.3 2022/10/29 00:56:40 gdt Exp $
+$NetBSD: patch-synapse_handlers_room.py,v 1.4 2022/11/12 13:24:09 gdt Exp $
 
 This patch allows changing the default power level for the creator of a new
 room. It's useful to set this higher than 100 so that the creator of the room
@@ -6,10 +6,10 @@ can give admin to others while still retaining the ability to revoke admin from
 someone. In the past, this turned out to be the only thing that could recover a
 room in which a bridge with admin permissions went wild and kicked everybody.
 
---- synapse/handlers/room.py.orig	2022-10-17 10:31:15.000000000 +0000
+--- synapse/handlers/room.py.orig	2022-11-08 10:59:36.000000000 +0000
 +++ synapse/handlers/room.py
-@@ -1170,7 +1170,7 @@ class RoomCreationHandler:
-             await send(power_event, power_context, creator)
+@@ -1151,7 +1151,7 @@ class RoomCreationHandler:
+             events_to_send.append((power_event, power_context))
          else:
              power_level_content: JsonDict = {
 -                "users": {creator_id: 100},
