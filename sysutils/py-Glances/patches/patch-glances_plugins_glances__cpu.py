d51bbc930197017dcf1281d55a9e75c78aa9c110
fox <fox@pkgsrc.org>
2022-11-12 06:19:26 +0000

    sysutils/py-Glances: Update to 3.3.0.4
    Changes since 3.2.7:
    
    ===============
    Version 3.3.0.4
    ===============
    
    Refactor the Docker images factory, from now, only Alpine image wll be
    provided.
    
    The following Docker images (nicolargo/glances) are availables:
    
    - *latest-full* for a full Alpine Glances image (latest release) with all
       dependencies
    - *latest* for a basic Alpine Glances (latest release) version with minimal
       dependencies (Bottle and Docker)
    - *dev* for a basic Alpine Glances image (based on development branch) with
      all dependencies (Warning: may be instable)
    
    ===============
    Version 3.3.0.2
    ===============
    
    Bug corrected:
        * Password files in same configuration dir in effect #2143
        * Fail to load config file on Python 3.10 #2176
    
    ===============
    Version 3.3.0.1
    ===============
    
    Just a version to rebuild the Docker images.
    
    ===============
    Version 3.3.0
    ===============
    
    Enhancements:
    
        * Migration from AngularJS to Angular/React/Vue #2100 (many thanks to
          @fr4nc0is)
        * Improve the IP module with a link to Censys #2105
        * Add the public IP information to the WebUI #2105
        * Add an option to show a configurable clock/time module to display #2150
        * Add sort information on Docker plugin (console mode). Related to #2138
        * Password files in same configuration dir in effect #2143
        * If the container name is long, then display the start, not the end -
          Related to #1732
        * Make the Web UI same than Console for CPU plugin
        * [WINDOWS] Reorganise CPU stats display #2131
        * Remove the static exportable_plugins list from glances_export.py #1556
        * Limiting data exported for economic storage #1443
    
    Bug corrected:
    
        * glances.conf FS hide not applying #1666
        * AMP: regex with special chars #2152
        * fix(help-screen): add missing shortcuts and columnize algorithmically
          #2135
        * Correct issue with the regexp filter (use fullmatch instead of match)
        * Errors when running Glances as web service #1702
        * Apply alias to Duplicate sensor name #1686
        * Make the hide function in sensors section compliant with lower/upercase
          #1590
        * Web UI truncates the days part of CPU time counter of the process list
          #2108
        * Correct alignement issue with the diskio plugin (Console UI)
    
    Documentation and CI:
    
        * Refactor Docker file CI
        * Add Codespell to the CI pipeline #2148
        * Please add docker-compose example and document example. #2151
        * [DOC] Glances failed to start and some other issues - BSD #2106
        * [REQUEST Docker image] Output log to stdout #2128 (for debian)
        * Fix code scanning alert - Clear-text logging of sensitive information
          #2124
        * Improve makefile (with online documentation)
        * buildx failed with: ERROR: failed to solve: python:3.10-slim-buster: no
          match for platform in manifest #2120
        * [Update docs] Can I export only the fields I need in csv report？ #2113
        * Windows Python 3 installation fails on dependency package "future" #2109
    
    Contributors for this version:
    
        * fr4nc0is : a very special thanks to @fr4nc0is for his huge work on the
          Glances v3.3.0 WebUI !!!
        * Kostis Anagnostopoulos
        * Kian-Meng Ang
        * dependabot[bot]
        * matthewaaronthacker
        * and your servant Nicolargo

--- sysutils/py-Glances/patches/patch-glances_plugins_glances__cpu.py
+++ sysutils/py-Glances/patches/patch-glances_plugins_glances__cpu.py
@@ -1,18 +1,18 @@
-$NetBSD: patch-glances_plugins_glances__cpu.py,v 1.2 2022/08/08 09:28:06 fox Exp $
+$NetBSD: patch-glances_plugins_glances__cpu.py,v 1.3 2022/11/12 06:19:26 fox Exp $
 
 Disable system call counts in BSD.
---- glances/plugins/glances_cpu.py.orig	2022-07-28 16:12:29.000000000 +0000
+--- glances/plugins/glances_cpu.py.orig	2022-11-03 14:30:13.000000000 +0000
 +++ glances/plugins/glances_cpu.py
-@@ -13,7 +13,7 @@ from glances.logger import logger
+@@ -12,7 +12,7 @@
  from glances.timer import getTimeSinceLastUpdate
  from glances.compat import iterkeys
  from glances.cpu_percent import cpu_percent
--from glances.globals import LINUX
-+from glances.globals import LINUX, BSD
+-from glances.globals import LINUX, WINDOWS, SUNOS
++from glances.globals import LINUX, WINDOWS, SUNOS, BSD
  from glances.plugins.glances_core import Plugin as CorePlugin
  from glances.plugins.glances_plugin import GlancesPlugin
  
-@@ -366,7 +366,7 @@ class Plugin(GlancesPlugin):
+@@ -386,7 +386,7 @@ class Plugin(GlancesPlugin):
          # Steal CPU usage
          ret.extend(self.curse_add_stat('steal', width=14, header='  '))
          # syscalls: number of system calls since boot. Always set to 0 on Linux. (do not display)
