4db1988361af3cb7f5b479e67a6438ee10dfe1ba
gutteridge <gutteridge@pkgsrc.org>
2022-11-12 05:03:33 +0000

    libfm-qt: update to 1.2.0
    Change log:
    
    libfm-qt-1.2.0 / 2022-11-05
    ============================
     * Support adding of pattern lists to entries of search dialog (such lists are used by `pcmanfm-qt` for having a search history).
     * Fixed crash with empty URI scheme of folder path.
     * Silenced compilation warnings about deprecated GLib functions.
     * Don't show a deletion prompt without a selection.
     * Fixed infinite loop if a symlink that is created by DND overwrites a non-empty directory.
     * Allow remembering the result of execution prompt for multiple files.
     * Added `Ctrl+D` to LXQt file dialog for deselecting all items.
     * Allow selection by dragging mouse cursor inside non-name columns in the detailed list mode.
     * Corrected the position of drop menu under Wayland.
     * Fixed crash with DND into side-pane under Wayland.
     * Added nullity checks to `XdndWorkaround`.

--- x11/libfm-qt/buildlink3.mk
+++ x11/libfm-qt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2022/06/28 11:37:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2022/11/12 05:03:33 gutteridge Exp $
 
 BUILDLINK_TREE+=	libfm-qt
 
 .if !defined(LIBFM_QT_BUILDLINK3_MK)
 LIBFM_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.1.0
-BUILDLINK_ABI_DEPENDS.libfm-qt?=	libfm-qt>=1.1.0nb1
+BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.2.0
+BUILDLINK_ABI_DEPENDS.libfm-qt?=	libfm-qt>=1.2.0
 BUILDLINK_PKGSRCDIR.libfm-qt?=		../../x11/libfm-qt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
