0cdf5d30f9be25b95727dc66b18d0ccf14d3ccbc
adam <adam@pkgsrc.org>
2022-11-13 09:57:24 +0000

    meson: updated to 0.64.0
    0.64.0
    Add optimization plain option
    New languages: nasm and masm
    Pager and colors for meson configure output
    various install_* functions no longer handle the sticky bit
    fs.copyfile to replace configure_file(copy : true)
    Added update_mime_database to gnome.post_install()
    Added preserve_path arg to install_data
    BSD support for the jni dependency
    Credentials from ~/.netrc for https URLs
    Basic support for oneAPI compilers on Linux and Windows
    New method to preprocess source files
    python.find_installation() now accepts pure argument
    Generates rust-project.json when there are Rust targets
    summary() accepts disablers
    Option to allow meson test to fail fast after the first failing testcase
    Incremental ThinLTO with b_thinlto_cache
    Update all wraps from WrapDB with meson wrap update command
    Added include_core_only arg to wayland.scan_xml.
    Automatic fallback using WrapDB

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,22 +1,16 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.5 2022/07/13 03:57:22 triaxx Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.6 2022/11/13 09:57:24 adam Exp $
 
 Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
 a condition to match FreeBSD path norm for pkg-config files.
 
---- mesonbuild/modules/pkgconfig.py.orig	2022-06-19 19:10:14.000000000 +0000
+--- mesonbuild/modules/pkgconfig.py.orig	2022-10-24 19:25:24.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -593,12 +593,8 @@ class PkgConfigModule(ExtensionModule):
+@@ -696,7 +696,7 @@ class PkgConfigModule(NewExtensionModule
          pcfile = filebase + '.pc'
-         pkgroot = pkgroot_name = kwargs.get('install_dir', default_install_dir)
+         pkgroot = pkgroot_name = kwargs['install_dir'] or default_install_dir
          if pkgroot is None:
 -            if mesonlib.is_freebsd():
--                pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('prefix')), 'libdata', 'pkgconfig')
--                pkgroot_name = os.path.join('{prefix}', 'libdata', 'pkgconfig')
--            else:
--                pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
--                pkgroot_name = os.path.join('{libdir}', 'pkgconfig')
-+            pkgroot = os.path.join(state.environment.coredata.get_option(mesonlib.OptionKey('libdir')), 'pkgconfig')
-+            pkgroot_name = os.path.join('{libdir}', 'pkgconfig')
-         if not isinstance(pkgroot, str):
-             raise mesonlib.MesonException('Install_dir must be a string.')
-         relocatable = state.get_option('relocatable', module='pkgconfig')
++            if False:
+                 pkgroot = os.path.join(_as_str(state.environment.coredata.get_option(mesonlib.OptionKey('prefix'))), 'libdata', 'pkgconfig')
+                 pkgroot_name = os.path.join('{prefix}', 'libdata', 'pkgconfig')
+             else:
