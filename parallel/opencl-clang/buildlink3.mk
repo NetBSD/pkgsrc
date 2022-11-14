ebd2586cad6a50c524ab4beb9a8cbf660383f7e7
adam <adam@pkgsrc.org>
2022-11-14 19:02:04 +0000

    opencl-clang: updated to 15.0.0
    15.0.0
    This release is linked against LLVM 15.0.0 and SPIR-V LLVM translator v15.0.0.

--- parallel/opencl-clang/buildlink3.mk
+++ parallel/opencl-clang/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/04/18 19:10:04 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/11/14 19:02:04 adam Exp $
 
 BUILDLINK_TREE+=	opencl-clang
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	opencl-clang
 OPENCL_CLANG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencl-clang+=	opencl-clang>=10.0.0.1
-BUILDLINK_ABI_DEPENDS.opencl-clang?=	opencl-clang>=13.0.0nb2
+BUILDLINK_ABI_DEPENDS.opencl-clang+=	opencl-clang>=13.0.0nb2
 BUILDLINK_PKGSRCDIR.opencl-clang?=	../../parallel/opencl-clang
 
 .include "../../parallel/spirv-llvm-translator/buildlink3.mk"
