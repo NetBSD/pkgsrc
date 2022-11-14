4ca99e277fdf05f23b42e01c23d5f2327a76f91e
wiz <wiz@pkgsrc.org>
2022-11-14 18:58:50 +0000

    py-gssapi: update to 1.8.2.
    Release v1.8.2: Klipspringer (Patch 2) Latest
    
        Added Python 3.11 wheel - #303
    
    Release v1.8.1: Klipspringer (Patch 1)
    
        Bumped minimum Python version to Python 3.7 - #299
    
    Release v1.8.0: Klipspringer
    
        Created PEP 517 compliant sdist
        No longer include cythonised files in sdist as they can be built on install time with Cython
        The Cython build requirement will be automatically resolved by pip and other tools due to PEP 517 metadata

--- security/py-gssapi/patches/patch-setup.py
+++ security/py-gssapi/patches/patch-setup.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-setup.py,v 1.1 2021/12/06 14:45:22 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2022/11/14 18:58:50 wiz Exp $
 
 Match this setting with py-setuptools.
 
---- setup.py.orig	2021-10-10 21:13:06.000000000 +0000
+--- setup.py.orig	2022-10-25 18:55:45.000000000 +0000
 +++ setup.py
-@@ -13,7 +13,7 @@ import shlex
+@@ -11,7 +11,7 @@ import shlex
  # the deprecation warning. Must be done before importing setuptools,
  # setuptools also must be imported before distutils.
  # https://github.com/pypa/setuptools/blob/main/docs/deprecated/distutils-legacy.rst
@@ -12,4 +12,4 @@ Match this setting with py-setuptools.
 +#os.environ['SETUPTOOLS_USE_DISTUTILS'] = 'local'
  
  from setuptools import setup  # noqa: E402
- from setuptools import Distribution  # noqa: E402
+ from setuptools.extension import Extension  # noqa: E402
