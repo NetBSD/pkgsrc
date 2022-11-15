134b63c1afbd045f6dc87d1c9b2bc61134f34f3d
he <he@pkgsrc.org>
2022-11-15 17:14:48 +0000

    Also buildlink libatomic for mipsel and mipseb (32-bit ports)..

--- mk/atomic64.mk
+++ mk/atomic64.mk
@@ -1,4 +1,4 @@
-# $NetBSD: atomic64.mk,v 1.6 2021/12/19 15:12:28 ryoon Exp $
+# $NetBSD: atomic64.mk,v 1.7 2022/11/15 17:14:48 he Exp $
 #
 # This Makefile fragment is included by packages that require 64-bit atomic
 # operations, and will attempt to set the appropriate architecture-specific
@@ -17,6 +17,8 @@ CFLAGS+=	-march=i586
 .endif
 
 .if ${MACHINE_ARCH} == "m68k" || \
+    ${MACHINE_ARCH} == "mipsel" || \
+    ${MACHINE_ARCH} == "mipseb" || \
     ${MACHINE_ARCH} == "powerpc" || \
     ${MACHINE_ARCH} == "sparc" || \
     ${MACHINE_ARCH} == "vax"
