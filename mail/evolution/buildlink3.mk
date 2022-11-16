e80b533a7b05f09cd5bfd0fdaed759db108111b3
bsiegert <bsiegert@pkgsrc.org>
2022-11-16 17:12:24 +0000

    Pullup ticket #6703 - requested by wiz mail/evolution: build fix
    Revisions pulled up:
    - mail/evolution/Makefile                                       1.263
    - mail/evolution/buildlink3.mk                                  1.103
    - mail/evolution/distinfo                                       1.90
    - mail/evolution/patches/patch-src-modules-book-config-carddav-evolution-book-config-carddav-c 1.1
    - mail/evolution/patches/patch-src-modules-cal-config-caldav-evolution-cal-config-caldav-c 1.1
    - mail/evolution/patches/patch-src-modules-cal-config-google-e-google-chooser-button-c 1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Wed Nov  9 13:00:58 UTC 2022
    
       Modified Files:
       	pkgsrc/mail/evolution: Makefile buildlink3.mk distinfo
       Added Files:
       	pkgsrc/mail/evolution/patches:
       	    patch-src-modules-book-config-carddav-evolution-book-config-carddav-c
       	    patch-src-modules-cal-config-caldav-evolution-cal-config-caldav-c
       	    patch-src-modules-cal-config-google-e-google-chooser-button-c
    
       Log Message:
       evolution: fix build with latest evolution-data-server
    
       Patches from Dave Tyson on tech-pkg.
    
       Remove BROKEN tag, bump PKGREVISION.

--- mail/evolution/buildlink3.mk
+++ mail/evolution/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.102 2022/08/11 05:08:58 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.102.2.1 2022/11/16 17:12:24 bsiegert Exp $
 
 BUILDLINK_TREE+=	evolution
 
@@ -9,8 +9,6 @@ BUILDLINK_API_DEPENDS.evolution+=	evolution>=2.8.0
 BUILDLINK_ABI_DEPENDS.evolution+=	evolution>=3.34.1nb16
 BUILDLINK_PKGSRCDIR.evolution?=		../../mail/evolution
 
-.include "../../devel/nspr/buildlink3.mk"
-.include "../../devel/nss/buildlink3.mk"
 .include "../../mail/evolution-data-server/buildlink3.mk"
 .include "../../audio/libcanberra/buildlink3.mk"
 .endif # EVOLUTION_BUILDLINK3_MK
