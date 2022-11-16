20f124a785309f08cb40eee2d9f146a3a5756ced
jperkin <jperkin@pkgsrc.org>
2022-11-16 16:08:34 +0000

    gobject-introspection: Avoid -Wl,--no-as-needed on SunOS.
    We currently prune this out in mk/platform but this ensures the installed files
    do not contain it either.  Bump PKGREVISION.

--- devel/gobject-introspection/patches/patch-giscanner_ccompiler.py
+++ devel/gobject-introspection/patches/patch-giscanner_ccompiler.py
@@ -1,9 +1,19 @@
-$NetBSD: patch-giscanner_ccompiler.py,v 1.4 2021/01/11 19:15:24 jperkin Exp $
+$NetBSD: patch-giscanner_ccompiler.py,v 1.5 2022/11/16 16:08:34 jperkin Exp $
 
 Exclude system paths from LD_LIBRARY_PATH.
+Do not use -Wl,--no-as-needed on SunOS.
 
---- giscanner/ccompiler.py.orig	2020-10-03 10:23:41.768748500 +0000
+--- giscanner/ccompiler.py.orig	2021-09-17 16:02:42.520052200 +0000
 +++ giscanner/ccompiler.py
+@@ -197,7 +197,7 @@ class CCompiler(object):
+ 
+                 # Ensure libraries are always linked as we are going to use ldd to work
+                 # out their names later
+-                if sys.platform != 'darwin':
++                if sys.platform != 'darwin' and sys.platform != 'sunos5':
+                     args.append('-Wl,--no-as-needed')
+ 
+         for library_path in libpaths:
 @@ -215,7 +215,8 @@ class CCompiler(object):
                      else:
                          args.append('-Wl,-rpath,' + library_path)
