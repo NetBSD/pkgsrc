037923c073f8c5f04083beecbc28e95f22c10e3f
gutteridge <gutteridge@pkgsrc.org>
2022-11-17 01:35:31 +0000

    xfce4-exo: slightly (and belatedly) bump BUILDLINK_API_DEPENDS
    Move us to present general reality (still more generous than the recency
    required of some other Xfce components).

--- x11/xfce4-exo/buildlink3.mk
+++ x11/xfce4-exo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.36 2022/06/28 11:37:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2022/11/17 01:35:31 gutteridge Exp $
 
 BUILDLINK_TREE+=	xfce4-exo
 
 .if !defined(XFCE4_EXO_BUILDLINK3_MK)
 XFCE4_EXO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=0.10.4
+BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=0.12
 BUILDLINK_ABI_DEPENDS.xfce4-exo+=	xfce4-exo>=4.16.4nb1
 BUILDLINK_PKGSRCDIR.xfce4-exo?=		../../x11/xfce4-exo
 
