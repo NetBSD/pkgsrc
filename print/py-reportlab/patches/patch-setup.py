d0f252e174a82d64b6686f49837a5c8031d19522
gdt <gdt@pkgsrc.org>
2022-11-17 01:49:17 +0000

    print/py-reportlab: Note that patch has been sent upstream

--- print/py-reportlab/patches/patch-setup.py
+++ print/py-reportlab/patches/patch-setup.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-setup.py,v 1.1 2022/11/16 12:18:46 gdt Exp $
+$NetBSD: patch-setup.py,v 1.2 2022/11/17 01:49:18 gdt Exp $
 
 Add /usr/X11R7 to the set of places setup.py looks for.  The existing
 setup.py is problematic, because this behavior escapes from buildlink,
@@ -6,7 +6,7 @@ and because it is not just trying a test compile with the provided
 CPPFLAGS.  Given that there is already a list of questionable places
 (/usr/local), it is not incrementally worse to add in X11R7.
 
-Not yet sent upstream.
+Sent to reportlab-users@reportlab.com on 17 November 2022.
 
 --- setup.py.orig	2022-10-17 12:06:28.000000000 +0000
 +++ setup.py
