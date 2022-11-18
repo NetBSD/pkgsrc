d71e20343d798494e012fc84b669b91cb01eb712
gutteridge <gutteridge@pkgsrc.org>
2022-11-18 04:26:00 +0000

    liblxqt: belatedly bump BUILDLINK_API_DEPENDS
    Though this library really didn't change significantly, the LXQt CMake
    build infrastructure insists component versions match at compile time.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.18 2022/06/28 11:37:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2022/11/18 04:26:00 gutteridge Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.1.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=1.1.0nb2
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.2.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
