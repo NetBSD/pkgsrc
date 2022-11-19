b422eb54cddbce881fc66e06f179510f5768aa03
wiz <wiz@pkgsrc.org>
2022-11-19 08:10:45 +0000

    libXdmcp: update to 1.1.4.
    Alan Coopersmith (11):
          Update configure.ac bug URL for gitlab migration
          gitlab CI: add a basic build test
          Fix build on Solaris 11.3.0 - 11.3.8
          Build xz tarballs instead of bzip2
          Fix spelling/wording issues
          gitlab CI: enable doc builds in basic build test
          Use memcpy() instead of memmove() when buffers are known not to overlap
          arc4random_buf: Only declare ret if HAVE_GETENTROPY is defined
          XdmcpDisposeARRAYofARRAY8: Reduce scope of i
          Only link with libbsd if needed for arc4random_buf()
          libXdmcp 1.1.4
    
    Guillem Jover (1):
          Switch from libbsd to libbsd-overlay

--- x11/libXdmcp/builtin.mk
+++ x11/libXdmcp/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.7 2019/11/03 09:14:11 rillig Exp $
+# $NetBSD: builtin.mk,v 1.8 2022/11/19 08:10:45 wiz Exp $
 
 BUILTIN_PKG:=	libXdmcp
 
@@ -31,10 +31,10 @@ USE_BUILTIN.libXdmcp=	no
 .  else
 USE_BUILTIN.libXdmcp=	${IS_BUILTIN.libXdmcp}
 .    if defined(BUILTIN_PKG.libXdmcp) && \
-        !empty(IS_BUILTIN.libXdmcp:M[yY][eE][sS])
+        ${IS_BUILTIN.libXdmcp:M[yY][eE][sS]}
 USE_BUILTIN.libXdmcp=	yes
 .      for _dep_ in ${BUILDLINK_API_DEPENDS.libXdmcp}
-.        if !empty(USE_BUILTIN.libXdmcp:M[yY][eE][sS])
+.        if ${USE_BUILTIN.libXdmcp:M[yY][eE][sS]}
 USE_BUILTIN.libXdmcp!=							\
 	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.libXdmcp:Q}; then \
 		${ECHO} yes;						\
@@ -51,11 +51,11 @@ MAKEVARS+=		USE_BUILTIN.libXdmcp
 .include "../../mk/x11.builtin.mk"
 
 CHECK_BUILTIN.libXdmcp?=	no
-.if !empty(CHECK_BUILTIN.libXdmcp:M[nN][oO])
+.if ${CHECK_BUILTIN.libXdmcp:M[nN][oO]}
 
 # If we are using the builtin version, check whether it has a xdmcp.pc
 # file or not.  If the latter, generate a fake one.
-.  if !empty(USE_BUILTIN.libXdmcp:M[Yy][Ee][Ss])
+.  if ${USE_BUILTIN.libXdmcp:M[Yy][Ee][Ss]}
 BUILDLINK_TARGETS+=	xdmcp-fake-pc
 
 xdmcp-fake-pc:
