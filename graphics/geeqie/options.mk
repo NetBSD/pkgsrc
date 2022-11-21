5fd55e9533f4b3a142db9627a493f5228a666107
wiz <wiz@pkgsrc.org>
2022-11-21 11:58:47 +0000

    geeqie: update to 2.0.1.
    Geeqie 2.0.1
    ============
    
    - Exclude build directories from release tar
    
    Geeqie 2.0
    ============
    
    - GTK2 no longer supported
    - Configuration system moved from Autotools to Meson
    - Spelling checks for metadata
    - When navigation folders, on return to a folder select last image viewed
    - Search facility in Exif window
    - Display keyboard shortcuts in popup menus
    - LIRC deleted
    - renderer-clutter() deleted
    
    And as usual:
    - Many bug fixes
    - Updated language support

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,29 +1,17 @@
-# $NetBSD: options.mk,v 1.8 2021/01/11 18:15:13 gdt Exp $
+# $NetBSD: options.mk,v 1.9 2022/11/21 11:58:47 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.geeqie
-PKG_OPTIONS_OPTIONAL_GROUPS=	gtk
-PKG_OPTIONS_GROUP.gtk=		gtk2 gtk3
-PKG_SUPPORTED_OPTIONS=		libchamplain
+PKG_SUPPORTED_OPTIONS=		gtk3 libchamplain
 PKG_SUGGESTED_OPTIONS=		gtk3
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlibchamplain)
 # While clutter is optional, the map requires it.
-CONFIGURE_ARGS+=	--enable-clutter
 .include "../../graphics/clutter/buildlink3.mk"
-
-CONFIGURE_ARGS+=	--enable-map
 .include "../../geography/libchamplain012/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-map
-.endif
-
-.if !empty(PKG_OPTIONS:Mgtk2)
-.include "../../x11/gtk2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtk3)
-CONFIGURE_ARGS+=	--enable-gtk3
 .include "../../x11/gtk3/buildlink3.mk"
 .endif
