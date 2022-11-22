6c537890f92b95c901db7b00ca9ded8940b8f9a0
adam <adam@pkgsrc.org>
2022-11-22 10:18:06 +0000

    py-jupyter_core: updated to 5.0.0
    5.0.0
    
    Major Changes
    
    Prefer Environment Level Configuration
    
    We now make the assumption that if we are running in a virtual environment, we should prioritize the environment-level `sys.prefix` over the user-level paths. Users can opt out of this behavior by setting `JUPYTER_PREFER_ENV_PATH`, which takes precedence over our autodetection.
    
    Migrate to Standard Platform Directories
    
    In version 5, we introduce a `JUPYTER_PLATFORM_DIRS` environment variable to opt in to using more appropriate platform-specific directories.  We raise a deprecation warning if the variable is not set.  In version 6,  `JUPYTER_PLATFORM_DIRS` will be opt-out.  In version 7, we will remove the environment variable checks and old directory logic.
    
    Drop Support for Python 3.7
    
    We are dropping support for Python 3.7 ahead of its official end of life, to reduce maintenance burden as we add support for Python 3.11.
    
    Enhancements made
    
    - Use platformdirs for path locations
    - Try to detect if we are in a virtual environment and change path precedence accordingly
    
    Bugs fixed
    
    - Add current working directory as first config path
    - Fix inclusion of jupyter file and check in CI
    
    Maintenance and upkeep improvements
    
    - Bump github/codeql-action from 1 to 2
    - Bump actions/checkout from 2 to 3
    - Add dependabot
    - Adopt jupyter releaser
    - Add more typing
    - Require Python 3.8+
    - Use hatch backend
    - Switch to flit build backend
    - is_hidden: Use normalized paths
    
    Documentation
    
    - Update broken link to `Contributing` guide

--- devel/py-jupyter_core/patches/patch-jupyter__core_paths.py
+++ devel/py-jupyter_core/patches/patch-jupyter__core_paths.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-jupyter__core_paths.py,v 1.2 2022/04/19 11:34:27 adam Exp $
+$NetBSD: patch-jupyter__core_paths.py,v 1.3 2022/11/22 10:18:06 adam Exp $
 
 Enable Jupyter to discover kernels installed by pkgsrc packages.
 
---- jupyter_core/paths.py.orig	2022-04-18 10:53:22.000000000 +0000
+--- jupyter_core/paths.py.orig	2020-02-02 00:00:00.000000000 +0000
 +++ jupyter_core/paths.py
-@@ -131,6 +131,7 @@ if os.name == "nt":
-         SYSTEM_JUPYTER_PATH = [os.path.join(sys.prefix, "share", "jupyter")]
- else:
-     SYSTEM_JUPYTER_PATH = [
-+        "@PREFIX@/share/jupyter",
-         "/usr/local/share/jupyter",
-         "/usr/share/jupyter",
-     ]
+@@ -188,6 +188,7 @@ else:
+             SYSTEM_JUPYTER_PATH = [os.path.join(sys.prefix, "share", "jupyter")]
+     else:
+         SYSTEM_JUPYTER_PATH = [
++            "@PREFIX@/share/jupyter",
+             "/usr/local/share/jupyter",
+             "/usr/share/jupyter",
+         ]
