beddd1643556cb14d71a4687f76dce46e2a44163
adam <adam@pkgsrc.org>
2022-11-23 10:56:39 +0000

    gobject-introspection: updated to 1.74.0
    1.74.0 - 2022-09-17
    -------------------
    * Update the GIR data for GLib, GObject, GModule, and GIO
    
    1.72.1 - 2022-09-21
    -------------------
    * Improve scanner support for C99 and pre-processor macros
    * Build fixes
    * Documentation fixes
    * Update the GIR data for GLib, GObject, GModule, and GIO
    
    1.72.0 - 2022-03-18
    -------------------
    * Add new utility API to libgirepository for bindings implementing an argument
      cache [Philip Chimento]
    * Update the GIR data for GLib, GObject, GModule, and GIO

--- devel/gobject-introspection/patches/patch-giscanner_ast.py
+++ devel/gobject-introspection/patches/patch-giscanner_ast.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-giscanner_ast.py,v 1.1 2019/06/03 10:47:20 prlw1 Exp $
+$NetBSD: patch-giscanner_ast.py,v 1.2 2022/11/23 10:56:40 adam Exp $
 
 - add double underscore versions
 - 32/64-bit time_t fix
 
---- giscanner/ast.py.orig	2019-01-06 10:32:10.000000000 +0000
+--- giscanner/ast.py.orig	2022-09-17 18:52:38.000000000 +0000
 +++ giscanner/ast.py
 @@ -307,6 +307,15 @@ type_names['uint32_t'] = TYPE_UINT32
  type_names['int64_t'] = TYPE_INT64
@@ -21,10 +21,10 @@ $NetBSD: patch-giscanner_ast.py,v 1.1 2019/06/03 10:47:20 prlw1 Exp $
  # A few additional GLib type aliases
  type_names['guchar'] = TYPE_UINT8
  type_names['gchararray'] = TYPE_STRING
-@@ -344,7 +353,7 @@ type_names['FILE*'] = TYPE_ANY
- # integers it's easy enough to expand them.
- type_names['size_t'] = type_names['gsize']
+@@ -346,7 +355,7 @@ type_names['size_t'] = type_names['gsize
  type_names['ssize_t'] = type_names['gssize']
+ type_names['uintptr_t'] = type_names['guintptr']
+ type_names['intptr_t'] = type_names['gintptr']
 -type_names['time_t'] = TYPE_LONG
 +type_names['time_t'] = @TIMET_TYPENAME@
  type_names['off_t'] = type_names['gsize']
