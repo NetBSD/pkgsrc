50c589a46d614294f03cc043c8c7a183c27923b7
adam <adam@pkgsrc.org>
2022-11-23 22:18:47 +0000

    py-ICU: updated to 2.10.2
    Version 2.10.1 -> 2.10.2
    ------------------------
    - fixed bug in logic computing max_char for PyUnicode_New()
    
    Version 2.10 -> 2.10.1
    ----------------------
    - fixed build issues with ICU < 72
    
    Version 2.9 -> 2.10
    -------------------
    - fixed bug with building on MacOS when pkg-config is present
    - added link to note about building everything from sources
    - added wrapper for u_cleanup/u_init sequence as ResourceBundle.resetICU()
    - added wrapper for TimeZone.getRegion()
    - added support for ICU 72.1
    - added wrapper for UnicodeSetIterator::skipToStrings()
    - added wrapper for DisplayOptions, DisplayOptions::Builder and related enums
    - added wrapper for FormattedNumber::getNounClass()
    - added wrapper for NumberFormatter.displayOptions()

--- textproc/py-ICU/patches/patch-setup.py
+++ textproc/py-ICU/patches/patch-setup.py
@@ -1,10 +1,19 @@
-$NetBSD: patch-setup.py,v 1.3 2020/11/05 18:11:55 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2022/11/23 22:18:47 adam Exp $
 
 Add NetBSD support.
 
---- setup.py.orig	2020-11-02 21:26:44.000000000 +0000
+--- setup.py.orig	2022-10-25 04:31:36.000000000 +0000
 +++ setup.py
-@@ -86,6 +86,7 @@ CONFIGURE_WITH_ICU_CONFIG = {
+@@ -64,6 +64,8 @@ if platform.startswith(('linux', 'gnu'))
+     platform = 'linux'
+ elif platform.startswith('freebsd'):
+     platform = 'freebsd'
++elif platform.startswith('netbsd'):
++    platform = 'netbsd'
+ 
+ 
+ CONFIGURE_WITH_ICU_CONFIG = {
+@@ -73,6 +75,7 @@ CONFIGURE_WITH_ICU_CONFIG = {
      'win32': False,   # no icu-config
      'sunos5': False,  # not tested
      'cygwin': False,  # not tested
@@ -12,15 +21,15 @@ Add NetBSD support.
  }
  
  CONFIGURE_WITH_PKG_CONFIG = {
-@@ -95,6 +96,7 @@ CONFIGURE_WITH_PKG_CONFIG = {
+@@ -82,6 +85,7 @@ CONFIGURE_WITH_PKG_CONFIG = {
      'win32': False,   # no pkg-config ?
      'sunos5': False,  # not tested
      'cygwin': False,  # not tested
 +    'netbsd': True,
  }
  
- INCLUDES = {
-@@ -104,6 +106,7 @@ INCLUDES = {
+ 
+@@ -114,6 +118,7 @@ INCLUDES = {
      'win32': ['c:/icu/include'],
      'sunos5': [],
      'cygwin': [],
@@ -28,7 +37,7 @@ Add NetBSD support.
  }
  
  if sys.platform == 'win32' and sys.version_info < (3,9):
-@@ -121,6 +124,7 @@ PEDANTIC_FLAGS = {
+@@ -132,6 +137,7 @@ PEDANTIC_FLAGS = {
      'win32': [],
      'sunos5': [],
      'cygwin': ['-pedantic'],
@@ -36,7 +45,7 @@ Add NetBSD support.
  }
  
  CFLAGS = {
-@@ -130,6 +134,7 @@ CFLAGS = {
+@@ -141,6 +147,7 @@ CFLAGS = {
      'win32': ['/Zc:wchar_t', '/EHsc'],
      'sunos5': ['-std=c++11'],
      'cygwin': ['-D_GNU_SOURCE=1', '-std=c++11'],
@@ -44,7 +53,7 @@ Add NetBSD support.
  }
  
  # added to CFLAGS when setup is invoked with --debug
-@@ -140,6 +145,7 @@ DEBUG_CFLAGS = {
+@@ -151,6 +158,7 @@ DEBUG_CFLAGS = {
      'win32': ['/Od', '/DDEBUG'],
      'sunos5': ['-DDEBUG'],
      'cygwin': ['-Og', '-g', '-DDEBUG'],
@@ -52,7 +61,7 @@ Add NetBSD support.
  }
  
  LFLAGS = {
-@@ -149,6 +155,7 @@ LFLAGS = {
+@@ -160,6 +168,7 @@ LFLAGS = {
      'win32': ['/LIBPATH:c:/icu/lib'],
      'sunos5': [],
      'cygwin': [],
@@ -60,20 +69,11 @@ Add NetBSD support.
  }
  
  LIBRARIES = {
-@@ -158,6 +165,7 @@ LIBRARIES = {
+@@ -169,6 +178,7 @@ LIBRARIES = {
      'win32': ['icuin', 'icuuc', 'icudt'],
      'sunos5': ['icui18n', 'icuuc', 'icudata'],
      'cygwin': ['icui18n', 'icuuc', 'icudata'],
 +    'netbsd': [],
  }
  
- platform = sys.platform
-@@ -165,6 +173,8 @@ if platform.startswith(('linux', 'gnu'))
-     platform = 'linux'
- elif platform.startswith('freebsd'):
-     platform = 'freebsd'
-+elif platform.startswith('netbsd'):
-+    platform = 'netbsd'
- 
  if 'PYICU_INCLUDES' in os.environ:
-     _includes = os.environ['PYICU_INCLUDES'].split(os.pathsep)
