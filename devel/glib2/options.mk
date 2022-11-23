447f1d0460330f5afc2d0fe76257184740f90840
adam <adam@pkgsrc.org>
2022-11-23 10:41:41 +0000

    glib2 glib2-tools gdbus-codegen: updated to 2.74.1
    Overview of changes in GLib 2.74.1
    ==================================
    
    * Update Unicode data to version 15 (work by Marco Trevisan)
    
    * Fix various build failures in different situations
      (work by Kjell Ahlstedt, Marco Trevisan (TreviĂąo))
    
    * Fix over-eager deprecated property warnings for construct properties
      (work by Simon McVittie)
    
    * Fix a crash calling `g_param_value_is_valid()` on a `GParamSpecParam` (work by
      Emmanuele Bassi)
    
    * Fix floating `GVariant` leaks with GObject properties (work by Matthias Clasen)
    
    * Add inline optimised version of `g_str_equal()` (work by Xavier Claessens)
    
    * Fix `GVariant` type depths checks on text format variants (work by Philip Withnall)
    
    * Fix regression with int64 and double hashing functions on big-endian architectures
      (work by Simon McVittie)
    
    * Build the API documentation only when building GLib as a shared library (work
      by Emmanuele Bassi)
    
    * Ignore weird `/etc/localtime` configurations generated by toolbx (work by Ray Strode)
    
    * Avoid `EINTR` races when closing FDs in `g_spawn_*()` (work by Thomas Haller)
    
    * Bugs fixed.
    
    
    Overview of changes in GLib 2.74.0, 17-09-2022
    ==============================================
    
    * Use EPOLL_CLOEXEC by default (Colin Walters)
    
    * Fixed various regression on GRegex as per the PCRE2 porting (Marco Trevisan)
    
    * Fixed various memory leaks (Sebastian Keller, Marco Trevisan)
    
    * Bugs fixed:
     - GNOME/gtksourceview-278 Failed to load html.lang after GLib switched to PCRE2
       (Marco Trevisan (TreviĂąo))
     - GNOME/gtksourceview-283 GRegex-on-PCRE2 regressions (Marco Trevisan)
     - -2688 Follow-up from "replace pcre1 with pcre2" (Marco Trevisan (TreviĂąo))
     - -2713 static_assert is undefined in C99 (wen Rafferty)
     - -2719 GParamSpec constructors should have nullable annotation on nick/blurb
       (Andy Holmes)
     - -2729 GRegex/GMatchInfo related crashes after upgrade to 2.73.2 (Marco
       Trevisan)
     - -2733 gio: GVariantBuilder builder leaked in g_document_portal_add_documents
       when URI list is empty (Sebastian Keller)
     - -2737 gio mount coredumps on prompt exit since 2.73 (Marco Trevisan)
     - -2741 Memory errors on GRegex (Marco Trevisan (TreviĂąo))
     - GNOME/gtk-4400 gtk_show_uri can't open browser on MSYS2 on Windows (Luca
       Bacci)
     - !2820 ci: Expose the coverage report and docs as links in the MR's (Marco)
     - !2855 gtlscertificate: Update reference to G_TLS_CERTIFICATE_NO_FLAGS
     - !2861 mem: Document OOM behavior for allocations (Matthias Clasen)
     - !2868 gio-unix: Use EPOLL_CLOEXEC by default (Colin Walters)
     - !2873 Fix array-bounds compiler warnings with GCC 12 (Emmanuele Bassi)
     - !2874 tests, GDesktopAppInfo: Fix various GVariant leaks (Marco Trevisan)
     - !2875 g_strsplit: Use a pre-allocated GArray when max_tokens is provided
       (Marco Trevisan)
     - !2876 GActionGroup: Fix ownership transfer annotation for query_action()
       (Andy Holmes)
     - !2879 gioenumtypes: Only define GLIB_DISABLE_DEPRECATION_WARNINGS if needed
       (Marco Trevisan)
     - !2881 GWin32AppInfo: Add missing g_spawn_close_pid (Luca Bacci)
     - !2882 meson: Set install_tag on all tools (Xavier Claessens)
     - !2883 ci: Improve style check script (Matthias Clasen)
     - !2900 docs: Improve wording in documentation of g_unix_signal_source_new()
       (Tom Levy)

--- devel/glib2/options.mk
+++ devel/glib2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.22 2019/10/29 09:22:24 adam Exp $
+# $NetBSD: options.mk,v 1.23 2022/11/23 10:41:41 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.glib2
 PKG_SUPPORTED_OPTIONS=	fam
@@ -15,6 +15,9 @@ PLIST_VARS+=	cocoa nococoa
 # if this options is enabled, use quartz for gtk{2,3}
 .if !empty(PKG_OPTIONS:Mcocoa)
 MESON_ARGS+=	-Duse_cocoa=true
+# Broken as of 2.74.1: gio/tests/giotypefuncs.inc:168:9:
+# error: implicit declaration of function 'g_osx_app_info_get_type'
+MESON_ARGS+=	-Dtests=false
 PLIST.cocoa=	yes
 .else
 PLIST.nococoa=	yes
