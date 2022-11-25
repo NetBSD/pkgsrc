aaa661c37a91b0f0b3180ffc6c02ee8afc3a685d
nia <nia@pkgsrc.org>
2022-11-25 14:20:45 +0000

    gstreamer1: Clean up buildlink3. Add BUILDLINK_INCDIRS.

--- multimedia/gstreamer1/buildlink3.mk
+++ multimedia/gstreamer1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/06/28 11:34:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/11/25 14:20:45 nia Exp $
 
 BUILDLINK_TREE+=	gstreamer1
 
@@ -8,9 +8,7 @@ GSTREAMER1_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gstreamer1+=	gstreamer1>=1.18.4
 BUILDLINK_ABI_DEPENDS.gstreamer1?=	gstreamer1>=1.20.2nb2
 BUILDLINK_PKGSRCDIR.gstreamer1?=	../../multimedia/gstreamer1
-
-pkgbase := gstreamer1
-.include "../../mk/pkg-build-options.mk"
+BUILDLINK_INCDIRS.gstreamer1?=		include/gstreamer-1.0
 
 .include "../../devel/glib2/buildlink3.mk"
 .endif # GSTREAMER1_BUILDLINK3_MK
