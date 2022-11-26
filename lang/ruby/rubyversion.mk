db1cb6d29767f0b66d829df59dec85dcc9789eac
taca <taca@pkgsrc.org>
2022-11-26 13:09:58 +0000

    lang/ruby27-base: update to 2.7.7
    Ruby 2.7.7 Released (2022-11-24)
    
    Ruby 2.7.7 has been released.
    
    This release includes a security fix.  Please check the topics below for
    details.
    
    * CVE-2021-33621: HTTP response splitting in CGI
    
    This release also includes some build problem fixes.  They are not
    considered to affect compatibility with previous versions.  See the commit
    logs for further details.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.257 2022/11/26 13:07:51 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.258 2022/11/26 13:09:58 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -213,7 +213,7 @@ RUBY_VERSION_REQD?=	${PKGNAME_REQD:C/ruby([0-9][0-9])-.*/\1/}
 .endif
 
 # current supported Ruby's version
-RUBY27_VERSION=		2.7.6
+RUBY27_VERSION=		2.7.7
 RUBY30_VERSION=		3.0.5
 RUBY31_VERSION=		3.1.3
 
@@ -277,7 +277,7 @@ RUBY_RUBYGEMS_VER=		3.1.6
 RUBY_BENCHMARK_VER=		0.1.0
 RUBY_BIGDECIMAL_VER=		2.0.0
 RUBY_BUNDLER_VER=		2.1.4
-RUBY_CGI_VER=			0.1.0.1
+RUBY_CGI_VER=			0.1.0.2
 RUBY_CSV_VER=			3.1.2
 RUBY_DATE_VER=			3.0.3
 RUBY_DBM_VER=			1.1.0
@@ -302,7 +302,7 @@ RUBY_NET_SMTP_VER=		0.1.0
 RUBY_NET_TELNET_VER=		0.2.0
 RUBY_OBSERVER_VER=		0.1.0
 RUBY_OPEN3_VER=			0.1.0
-RUBY_OPENSSL_VER=		2.1.3
+RUBY_OPENSSL_VER=		2.1.4
 RUBY_OSTRUCT_VER=		0.2.0
 RUBY_POWER_ASSERT_VER=		1.1.7
 RUBY_PRIME_VER=			0.1.1
