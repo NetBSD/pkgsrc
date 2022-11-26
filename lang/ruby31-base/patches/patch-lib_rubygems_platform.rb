f4bece47cfca551dae12ca5b5c7bfc573c14165d
taca <taca@pkgsrc.org>
2022-11-26 13:02:49 +0000

    lang/ruby31-base: update to 3.1.3
    Ruby 3.1.3 Released (2022-11-24)
    
    Ruby 3.1.3 has been released.
    
    This release includes a security fix.  Please check the topics below for
    details.
    
    * CVE-2021-33621: HTTP response splitting in CGI
    
    This release also includes a fix for build failure with Xcode 14 and macOS
    13 (Ventura).  See the related ticket for more details.

--- lang/ruby31-base/patches/patch-lib_rubygems_platform.rb
+++ lang/ruby31-base/patches/patch-lib_rubygems_platform.rb
@@ -1,29 +1,29 @@
-$NetBSD: patch-lib_rubygems_platform.rb,v 1.2 2022/02/20 15:59:09 taca Exp $
+$NetBSD: patch-lib_rubygems_platform.rb,v 1.3 2022/11/26 13:02:49 taca Exp $
 
 * Don't replace "i486" to "x86".
 * Allow simple "netbsd" as Gem::Platform.
 
---- lib/rubygems/platform.rb.orig	2022-02-18 09:42:25.000000000 +0000
+--- lib/rubygems/platform.rb.orig	2022-11-24 10:20:31.000000000 +0000
 +++ lib/rubygems/platform.rb
-@@ -75,12 +75,7 @@ class Gem::Platform
+@@ -76,12 +76,7 @@ class Gem::Platform
          arch.last << "-#{extra}"
        end
  
 -      cpu = arch.shift
 -
 -      @cpu = case cpu
--      when /i\d86/ then 'x86'
+-      when /i\d86/ then "x86"
 -      else cpu
 -      end
 +      @cpu = arch.shift
  
-       if arch.length == 2 and arch.last =~ /^\d+(\.\d+)?$/ # for command-line
+       if arch.length == 2 && arch.last =~ /^\d+(\.\d+)?$/ # for command-line
          @os, @version = arch
-@@ -110,6 +105,7 @@ class Gem::Platform
-         @cpu = 'x86' if @cpu.nil? and os =~ /32$/
+@@ -111,6 +106,7 @@ class Gem::Platform
+         @cpu = "x86" if @cpu.nil? && os =~ /32$/
          [os, version]
-       when /netbsdelf/ then             [ 'netbsdelf', nil ]
-+      when /netbsd/ then                [ 'netbsd',    nil ]
-       when /openbsd(\d+\.\d+)?/ then    [ 'openbsd',   $1  ]
-       when /bitrig(\d+\.\d+)?/ then     [ 'bitrig',    $1  ]
-       when /solaris(\d+\.\d+)?/ then    [ 'solaris',   $1  ]
+       when /netbsdelf/ then             [ "netbsdelf", nil ]
++      when /netbsd/ then                [ "netbsd",    nil ]
+       when /openbsd(\d+\.\d+)?/ then    [ "openbsd",   $1  ]
+       when /bitrig(\d+\.\d+)?/ then     [ "bitrig",    $1  ]
+       when /solaris(\d+\.\d+)?/ then    [ "solaris",   $1  ]
