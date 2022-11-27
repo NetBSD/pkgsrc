418d99ddd84dbecc938a3f2dd86046aaf12c0bfb
gdt <gdt@pkgsrc.org>
2022-11-27 13:55:49 +0000

    geography/proj: recursive revbump

--- graphics/pcl/buildlink3.mk
+++ graphics/pcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2022/11/23 16:18:48 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/11/27 13:55:50 gdt Exp $
 
 BUILDLINK_TREE+=	pcl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pcl
 PCL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pcl+=	pcl>=1.10.0
-BUILDLINK_ABI_DEPENDS.pcl+=	pcl>=1.11.1nb10
+BUILDLINK_ABI_DEPENDS.pcl+=	pcl>=1.11.1nb11
 BUILDLINK_PKGSRCDIR.pcl?=	../../graphics/pcl
 
 .include "../../devel/boost-libs/buildlink3.mk"
