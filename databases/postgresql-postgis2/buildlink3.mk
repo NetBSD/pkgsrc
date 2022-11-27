8f15ebe233ea775209bcf288b4e24a1940af531f
gdt <gdt@pkgsrc.org>
2022-11-27 13:55:49 +0000

    geography/proj: recursive revbump

--- databases/postgresql-postgis2/buildlink3.mk
+++ databases/postgresql-postgis2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2022/11/23 16:18:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2022/11/27 13:55:49 gdt Exp $
 
 BUILDLINK_TREE+=	postgresql-postgis
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	postgresql-postgis
 POSTGRESQL_POSTGIS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=2.0
-BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql14-postgis>=3.3.2nb1
+BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql12-postgis>=3.3.2nb2
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis2
 
 .include "../../geography/geos/buildlink3.mk"
