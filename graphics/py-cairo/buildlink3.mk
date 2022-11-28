badad65153a911c8c6e02dcb8c0fb597cd993c9d
adam <adam@pkgsrc.org>
2022-11-28 18:12:15 +0000

    py-cairo: updated to 1.23.0
    1.23.0 - 2022-11-28
    -------------------
    
    * git: changed default branch from "master" to "main"
    * Windows: Update the cairo version included in the wheels from 1.17.2 to 1.17.6
    * docs: Document how to look up pycairo headers without loading the module
    * tests: don't error out if cairo wasn't built with all features
    * New APIs:
    
      * :attr:`Status.DWRITE_ERROR`
      * :attr:`Format.RGB96F`, :attr:`Format.RGBA128F`
      * :attr:`PDFVersion.VERSION_1_6`, :attr:`PDFVersion.VERSION_1_7`
      * :attr:`HAS_DWRITE_FONT`
      * :meth:`Context.set_hairline`, :meth:`Context.get_hairline`
      * :meth:`PDFSurface.set_custom_metadata`

--- graphics/py-cairo/buildlink3.mk
+++ graphics/py-cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2020/09/03 15:45:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2022/11/28 18:12:15 adam Exp $
 
 BUILDLINK_TREE+=	py-cairo
 
@@ -8,7 +8,7 @@ PY_CAIRO_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-cairo+=	${PYPKGPREFIX}-cairo>=1.10.0
-BUILDLINK_ABI_DEPENDS.py-cairo?=	${PYPKGPREFIX}-cairo>=1.18.2nb1
+BUILDLINK_ABI_DEPENDS.py-cairo+=	${PYPKGPREFIX}-cairo>=1.18.2nb1
 .if ${PYVERSSUFFIX} == "2.7"
 BUILDLINK_PKGSRCDIR.py-cairo?=		../../graphics/py-cairo118
 .else
