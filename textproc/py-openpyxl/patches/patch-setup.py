4ba15c1b9211be1b10d38a298d85dbf9142e1ce5
adam <adam@pkgsrc.org>
2022-11-29 10:20:57 +0000

    py-openpyxl: updated to 3.0.10
    3.0.10 (2021-05-13)
    
    Bugfixes
    
    * Image files not closed when workbooks are saved
    * Problem with missing scope attribute in Pivot Table formats
    * Excel unhappy when multiple sorts are defined
    
    3.0.9 (2021-09-22)
    
    Bugfixes
    
    * Ignore blank ignored in existing Data Validations
    * Add support for cell protection for merged cell ranges
    * Timezone-aware datetimes raise an Exception
    * Improved normalisation of chart series
    * Catch OverflowError for out of range datetimes
    * Alignment.relativeIndent can be negative
    * Incorrect default value groupBy attribute
    
    3.0.8 (brown bag)
    
    Deleted because it contained breaking changes from 3.1
    
    3.0.7 (2021-03-09)
    
    Bugfixes
    
    * Problems with zero time values
    * Not possible to correctly convert excel dates to timedelta
    * Exception raised when merging cells which do not have borders all the way round.
    * Python 2 print statement in the tutorial
    
    Pull Requests
    
    * Add documentation on datetime handling
    * Drop dependency on jdcal
    * Datetime rounding
    * Unify handling of 1900 epoch
    * Add explicit support for reading datetime deltas
    * Millisecond precision for datetimes
    
    3.0.6 (2021-01-14)
    
    Bugfixes
    
    * Borders in differential styles are incorrect
    * Error when opening some pivot tables
    * Resave breaks the border format in conditional formatting rules
    * Read-only workbook not closed properly if generator interrupted
    * Pandas.Multiindex.labels deprecated
    * Pandas.Multiinex not expanded correctly
    * Cannot read rows with exponents
    * numpy.float is deprecated
    * Cells without coordinate attributes not always correctly handled
    
    Pull Requests
    
    * Improved handling of borders for differential styles
    * Support subclasses of datetime objects
    * Improved handling of cells without coordinates
    
    3.0.5 (2020-08-21)
    
    Bugfixes
    
    * Incorrectly consider currency format as datetime
    * Cannot copy worksheets with merged cells
    * Empty worksheets do not return generators when looping.
    * Hyperlinks duplicated on multiple saves
    * Incorrectly literal format as datetime
    * Links set to range of cells not preserved
    * Exception when opening workbook with chartsheets and tables
    
    3.0.4 (2020-06-24)
    
    Bugfixes
    
    * Find tables by name
    * Worksheet protection missing in existing files
    * Exception when reading files with external images
    * Reading lots of merged cells is very slow.
    * Read support for Bubble Charts.
    * Preserve any indexed colours
    * Reading many thousand of merged cells is really slow.
    * Adding tables in write-only mode raises an exception.
    
    Pull Requests
    
    * Add support for finding tables by name or range.
    
    3.0.3 (2020-01-20)
    
    Bugfixes
    
    * Exception when handling merged cells with hyperlinks
    * Problems when both lxml and defusedxml are installed
    * CFVO with incorrect values cannot be processed
    
    3.0.2 (2019-11-25)
    
    Bug fixes
    
    * DeprecationError if both defusedxml and lxml are installed
    * ws._current_row is higher than ws.max_row
    * Border bottom style is not optional when it should be
    * Empty cells in read-only, values-only mode are sometimes returned as ReadOnlyCells
    * Cannot add page breaks to existing worksheets if none exist already
    
    Pull Requests
    
    * Improvements to the documentation
    
    3.0.1 (2019-11-14)
    
    Bugfixes
    
    * Cannot read empty charts.
    
    Pull Requests
    
    * Fix for 1250
    * TableStyleElement is a sequence
    
    3.0.0 (2019-09-25)
    
    Python 3.6+ only release

--- textproc/py-openpyxl/patches/patch-setup.py
+++ textproc/py-openpyxl/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2019/02/08 10:48:38 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2022/11/29 10:20:57 adam Exp $
 
-Prefer lxml to et_xmlfile.
+Prefer lxml to et_xmlfile; the latter is a subset of the former.
 
---- setup.py.orig	2019-02-06 14:12:51.000000000 +0000
+--- setup.py.orig	2022-03-08 15:06:53.000000000 +0000
 +++ setup.py
-@@ -59,7 +59,7 @@ setup(
+@@ -76,7 +76,7 @@ setup(
      license=__license__,
-     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+     python_requires=f">={__python__}",
      install_requires=[
--        'jdcal', 'et_xmlfile',
-+        'jdcal', 'lxml',
+-        'et_xmlfile',
++        'lxml',
          ],
      project_urls={
          'Documentation': 'https://openpyxl.readthedocs.io/en/stable/',
