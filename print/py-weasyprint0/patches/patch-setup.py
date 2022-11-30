617705e7a23ad01777cbece73c006d2cadbe7b24
adam <adam@pkgsrc.org>
2022-11-30 09:24:13 +0000

    py-weasyprint0: exclude pytest-flake8 and pytest-isort from testing

--- print/py-weasyprint0/patches/patch-setup.py
+++ print/py-weasyprint0/patches/patch-setup.py
@@ -1,10 +1,23 @@
-$NetBSD: patch-setup.py,v 1.3 2021/04/29 13:15:32 joerg Exp $
+$NetBSD: patch-setup.py,v 1.4 2022/11/30 09:24:13 adam Exp $
 
 Adjust script path to allow multiple Python version installs.
 
---- setup.py.orig	2017-12-22 23:26:45.000000000 +0000
+Do not require pytest-flake8 and pytest-isort as PkgSrc does have Python 2.7
+versions anymore.
+
+--- setup.py.orig	2018-03-27 20:48:21.000000000 +0000
 +++ setup.py
-@@ -83,7 +83,7 @@ setup(
+@@ -75,15 +75,15 @@ setup(
+     setup_requires=pytest_runner,
+     test_suite='weasyprint.tests',
+     tests_require=[
+-        'pytest-runner', 'pytest-cov', 'pytest-flake8', 'pytest-isort'],
++        'pytest-runner', 'pytest-cov'],
+     extras_require={
+         'test': [
+-            'pytest-runner', 'pytest-cov', 'pytest-flake8', 'pytest-isort'],
++            'pytest-runner', 'pytest-cov'],
+         ':python_version < "3.0"': ['CairoSVG >= 1.0.20, < 2.0.0'],
          ':python_version >= "3.0"': ['CairoSVG >= 1.0.20']},
      entry_points={
          'console_scripts': [
