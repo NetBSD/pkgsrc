70a3b5e445d70d24ff0b3514d9e8583df71e36de
bsiegert <bsiegert@pkgsrc.org>
2022-11-30 17:19:02 +0000

    Pullup ticket #6706 - requested by taca lang/ruby31-base: security fix
    Revisions pulled up:
    - lang/ruby/rubyversion.mk                                      1.256
    - lang/ruby31-base/Makefile                                     1.7
    - lang/ruby31-base/PLIST                                        1.3
    - lang/ruby31-base/distinfo                                     1.8
    - lang/ruby31-base/patches/patch-configure                      1.3
    - lang/ruby31-base/patches/patch-lib_rubygems.rb                1.2
    - lang/ruby31-base/patches/patch-lib_rubygems_commands_setup__command.rb 1.2
    - lang/ruby31-base/patches/patch-lib_rubygems_install__update__options.rb 1.2
    - lang/ruby31-base/patches/patch-lib_rubygems_installer.rb      1.2
    - lang/ruby31-base/patches/patch-lib_rubygems_platform.rb       1.3
    - lang/ruby31-base/patches/patch-test_rubygems_test__gem.rb     1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Nov 26 13:02:49 UTC 2022
    
       Modified Files:
       	pkgsrc/lang/ruby: rubyversion.mk
       	pkgsrc/lang/ruby31-base: Makefile PLIST distinfo
       	pkgsrc/lang/ruby31-base/patches: patch-configure patch-lib_rubygems.rb
       	    patch-lib_rubygems_commands_setup__command.rb
       	    patch-lib_rubygems_install__update__options.rb
       	    patch-lib_rubygems_installer.rb patch-lib_rubygems_platform.rb
       	    patch-test_rubygems_test__gem.rb
    
       Log Message:
       lang/ruby31-base: update to 3.1.3
    
       Ruby 3.1.3 Released (2022-11-24)
    
       Ruby 3.1.3 has been released.
    
       This release includes a security fix.  Please check the topics below for
       details.
    
       * CVE-2021-33621: HTTP response splitting in CGI
    
       This release also includes a fix for build failure with Xcode 14 and macOS
       13 (Ventura).  See the related ticket for more details.

--- lang/ruby31-base/patches/patch-lib_rubygems.rb
+++ lang/ruby31-base/patches/patch-lib_rubygems.rb
@@ -1,22 +1,22 @@
-$NetBSD: patch-lib_rubygems.rb,v 1.1 2022/01/16 13:57:10 taca Exp $
+$NetBSD: patch-lib_rubygems.rb,v 1.1.6.1 2022/11/30 17:19:03 bsiegert Exp $
 
 * Add install_root option for pkgsrc's rubygems support.
 
---- lib/rubygems.rb.orig	2020-12-08 09:02:31.000000000 +0000
+--- lib/rubygems.rb.orig	2022-11-24 10:20:31.000000000 +0000
 +++ lib/rubygems.rb
-@@ -315,10 +315,16 @@ module Gem
+@@ -299,10 +299,16 @@ module Gem
    ##
    # The path where gem executables are to be installed.
  
 -  def self.bindir(install_dir=Gem.dir)
--    return File.join install_dir, 'bin' unless
+-    return File.join install_dir, "bin" unless
 -      install_dir.to_s == Gem.default_dir.to_s
 -    Gem.default_bindir
 +  def self.bindir(install_dir=Gem.dir, install_root=nil)
 +    if install_dir.to_s == Gem.default_dir.to_s
 +      install_dir = Gem.default_bindir
 +    else
-+      install_dir = File.join install_dir, 'bin'
++      install_dir = File.join install_dir, "bin"
 +    end
 +    unless install_root.nil? or install_root.empty?
 +      install_dir = File.join install_root, install_dir
@@ -25,20 +25,20 @@ $NetBSD: patch-lib_rubygems.rb,v 1.1 2022/01/16 13:57:10 taca Exp $
    end
  
    ##
-@@ -591,7 +597,7 @@ An Array (#{env.inspect}) was passed in 
+@@ -576,7 +582,7 @@ An Array (#{env.inspect}) was passed in 
        return i if path.instance_variable_defined?(:@gem_prelude_index)
      end
  
--    index = $LOAD_PATH.index RbConfig::CONFIG['sitelibdir']
-+    index = $LOAD_PATH.index RbConfig::CONFIG['vendorlibdir']
+-    index = $LOAD_PATH.index RbConfig::CONFIG["sitelibdir"]
++    index = $LOAD_PATH.index RbConfig::CONFIG["vendorlibdir"]
  
      index || 0
    end
-@@ -775,6 +781,7 @@ An Array (#{env.inspect}) was passed in 
+@@ -742,6 +748,7 @@ An Array (#{env.inspect}) was passed in 
      prefix = File.dirname RUBYGEMS_DIR
  
-     if prefix != File.expand_path(RbConfig::CONFIG['sitelibdir']) and
-+       prefix != File.expand_path(RbConfig::CONFIG['vendorlibdir']) and
-        prefix != File.expand_path(RbConfig::CONFIG['libdir']) and
-        'lib' == File.basename(RUBYGEMS_DIR)
+     if prefix != File.expand_path(RbConfig::CONFIG["sitelibdir"]) &&
++       prefix != File.expand_path(RbConfig::CONFIG["vendorlibdir"]) and
+        prefix != File.expand_path(RbConfig::CONFIG["libdir"]) &&
+        "lib" == File.basename(RUBYGEMS_DIR)
        prefix
