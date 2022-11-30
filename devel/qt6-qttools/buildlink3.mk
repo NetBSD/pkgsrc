976a8e4e5e8913521829eb714fb881792606ccb5
nros <nros@pkgsrc.org>
2022-11-30 20:57:35 +0000

    Add qt6-qttools version 6.4.1 to pkgsrc-current
    Qt6 development tools for translations, gui design and more.

---
+++ devel/qt6-qttools/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2022/11/30 20:57:35 nros Exp $
+
+BUILDLINK_TREE+=	qt6-qttools
+
+.if !defined(QT6_QTTOOLS_BUILDLINK3_MK)
+QT6_QTTOOLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt6-qttools+=	qt6-qttools>=6.4.1
+BUILDLINK_PKGSRCDIR.qt6-qttools?=	../../devel/qt6-qttools
+
+
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# QT6_QTTOOLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt6-qttools
