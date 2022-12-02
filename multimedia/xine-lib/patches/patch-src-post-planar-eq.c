e9893029cd4eb3d2881836d3e7db78d1d9b7e3f2
jperkin <jperkin@pkgsrc.org>
2022-12-02 15:06:22 +0000

    xine-lib: Restore dummy patch.
    While here remove obsolete patches, including disabling MMX on SunOS, as it
    appears to work fine now.

--- multimedia/xine-lib/patches/patch-src-post-planar-eq.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-eq.c
@@ -1,20 +1,9 @@
-$NetBSD: patch-src-post-planar-eq.c,v 1.5 2020/04/02 15:52:46 nia Exp $
+$NetBSD: patch-src-post-planar-eq.c,v 1.6 2022/12/02 15:06:22 jperkin Exp $
 
 https://bugs.xine-project.org/show_bug.cgi?id=524
 
-Disable MMX sections on SunOS.
-
 --- src/post/planar/eq.c.orig	2019-12-13 20:47:50.000000000 +0000
 +++ src/post/planar/eq.c
-@@ -33,7 +33,7 @@
- #include <pthread.h>
- 
- 
--#if defined(ARCH_X86)
-+#if defined(ARCH_X86) && !defined(__sun)
- 
- #if defined(ARCH_X86_64)
- #  define MEM1(reg) "(%"reg")"
 @@ -155,9 +155,11 @@ typedef struct eq_parameters_s {
   * description of params struct
   */
@@ -29,12 +18,3 @@ Disable MMX sections on SunOS.
              "contrast" )
  END_PARAM_DESCR( param_descr )
  
-@@ -360,7 +362,7 @@ static post_plugin_t *eq_open_plugin(pos
-   (void)audio_target;
- 
-   process = process_C;
--#if defined(ARCH_X86)
-+#if defined(ARCH_X86) && !defined(__sun)
-   if( xine_mm_accel() & MM_ACCEL_X86_MMX )
-     process = process_MMX;
- #endif
