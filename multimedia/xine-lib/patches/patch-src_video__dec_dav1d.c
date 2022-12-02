26d34135f72ad1168bfff245fe124af800167453
jperkin <jperkin@pkgsrc.org>
2022-12-02 15:06:22 +0000

    xine-lib: Restore dummy patch.
    While here remove obsolete patches, including disabling MMX on SunOS, as it
    appears to work fine now.

--- multimedia/xine-lib/patches/patch-src_video__dec_dav1d.c
+++ multimedia/xine-lib/patches/patch-src_video__dec_dav1d.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_video__dec_dav1d.c,v 1.1 2022/04/03 14:32:31 wiz Exp $
+$NetBSD: patch-src_video__dec_dav1d.c,v 1.2 2022/12/02 15:06:22 jperkin Exp $
 
 dav1d 1.0.0 compatibility from upstream.
 
---- src/video_dec/dav1d.c.orig	2020-11-19 10:30:37.000000000 +0000
+--- src/video_dec/dav1d.c.orig	2022-03-06 16:31:24.000000000 +0000
 +++ src/video_dec/dav1d.c
-@@ -537,11 +544,17 @@ static video_decoder_t *_open_plugin(vid
+@@ -544,11 +544,17 @@ static video_decoder_t *_open_plugin(vid
  
    /* multithreading */
    ncpu = xine_cpu_count();
