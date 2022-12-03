94dd0b2935953a4de00117644799aeab278365f8
adam <adam@pkgsrc.org>
2022-12-03 17:07:13 +0000

    nodejs: updated to 19.2.0
    Version 19.2.0 (Current)
    
    Notable changes
    
    Time zone update
    
    Time zone data has been updated to 2022f. This includes changes to Daylight Savings Time (DST) for Fiji and Mexico. For more information, see https://mm.icann.org/pipermail/tz-announce/2022-October/000075.html.
    
    
    Version 19.1.0 (Current)
    
    Notable changes
    
    Support function mocking on Node.js test runner
    fs.watch recursive support on Linux
    
    
    Version 19.0.1 (Current)
    
    This is a security release.
    
    Notable changes
    
    The following CVEs are fixed in this release:
    
    CVE-2022-3602: X.509 Email Address 4-byte Buffer Overflow (High)
    CVE-2022-3786: X.509 Email Address Variable Length Buffer Overflow (High)
    CVE-2022-43548: DNS rebinding in --inspect via invalid octal IP address (Medium)
    
    
    Version 19.0.0 (Current)
    
    Node.js 19 is here! Highlights include the update of the V8 JavaScript engine to 10.7, HTTP(s)/1.1 KeepAlive enabled by default, and ESM Resolution adjusts.
    
    Node.js 19 will replace Node.js 18 as our ‘Current’ release line when Node.js 18 enters long-term support (LTS) later this month. As per the release schedule, Node.js 19 will be ‘Current' release for the next 6 months, until April 2023.

--- lang/nodejs/patches/patch-deps_v8_tools_profiling_run-llprof.sh
+++ lang/nodejs/patches/patch-deps_v8_tools_profiling_run-llprof.sh
@@ -1,9 +1,9 @@
-$NetBSD: patch-deps_v8_tools_run-llprof.sh,v 1.1 2015/04/30 15:04:56 ryoon Exp $
+$NetBSD: patch-deps_v8_tools_profiling_run-llprof.sh,v 1.1 2022/12/03 17:07:13 adam Exp $
 
 POSIX shell portability.
 
---- deps/v8/tools/run-llprof.sh.orig	2015-03-31 22:13:01.000000000 +0000
-+++ deps/v8/tools/run-llprof.sh
+--- deps/v8/tools/profiling/run-llprof.sh.orig	2015-03-31 22:13:01.000000000 +0000
++++ deps/v8/tools/profiling/run-llprof.sh
 @@ -46,7 +46,7 @@ framework, then calls the low level tick
  EOF
  }
