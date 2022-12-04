3f00cf527511c2c8b68f31aba262099ffd24d639
rhialto <rhialto@pkgsrc.org>
2022-12-04 17:11:13 +0000

    lang/algol68g: update to version 3.1.0
    Most pkgsrc patches were incorporated.
    
    From the NEWS file:
    
    Version 3.1.0, November 2022
    * Fix several bugs reported for 3.0.0-8.
    * Apply several OS dependent patches reported for 3.0.0-8.
    * READ and INCLUDE relative to the source file, not the present working directory.
      This conforms to the C preprocessor standard.
    
    Versions 3.0.1-8, January/September 2022
    * Fix several bugs reported for 3.0.0-7.
    * Apply several OS dependent patches reported for 3.0.0-7.
    * Update a68g.exe to GSL 2.7.1 and R mathlib 4.1.2.

--- lang/algol68g/patches/patch-src_a68g_a68g.c
+++ lang/algol68g/patches/patch-src_a68g_a68g.c
@@ -1,15 +1,22 @@
-$NetBSD: patch-src_a68g_a68g.c,v 1.1 2022/03/02 01:41:34 ryoon Exp $
+$NetBSD: patch-src_a68g_a68g.c,v 1.2 2022/12/04 17:11:13 rhialto Exp $
 
-* Use header files from ${PREFIX}.
+* Add -rpath=. to the linker command line, so that --compile works.
+  Otherwise, you get errors like
 
---- src/a68g/a68g.c.orig	2022-02-01 20:45:41.000000000 +0000
+  test-set$ a68g --compile 20-quicksort.a68 
+  [20-quicksort.a68]
+  test-set$ ./20-quicksort
+  [.a68g.tmp.20-quicksort]
+  a68g: exiting: ./src/a68g/a68g.c: 497: cannot resolve symbol, /usr/pkg/bin/a68g: Shared object ".a68g.tmp.20-quicksort.so" not found
+
+--- src/a68g/a68g.c.orig	2022-11-06 16:02:39.000000000 +0000
 +++ src/a68g/a68g.c
-@@ -448,7 +448,7 @@ static void compiler_interpreter (void)
-       bufcat (options, " ", BUFFER_SIZE);
-       bufcat (options, HAVE_PIC, BUFFER_SIZE);
+@@ -437,7 +437,7 @@ void compiler_interpreter (void)
  #endif
--      ASSERT (snprintf (cmd, SNPRINTF_SIZE, "gcc %s -c -o \"%s\" \"%s\"", options, FILE_BINARY_NAME (&A68_JOB), FILE_OBJECT_NAME (&A68_JOB)) >= 0);
-+      ASSERT (snprintf (cmd, SNPRINTF_SIZE, "gcc %s -I@PREFIX@/include -c -o \"%s\" \"%s\"", options, FILE_BINARY_NAME (&A68_JOB), FILE_OBJECT_NAME (&A68_JOB)) >= 0);
+       ASSERT (snprintf (cmd, SNPRINTF_SIZE, "%s -I%s %s -c -o \"%s\" \"%s\"", C_COMPILER, INCLUDEDIR, options, FILE_BINARY_NAME (&A68_JOB), FILE_OBJECT_NAME (&A68_JOB)) >= 0);
        ABEND (system (cmd) != 0, ERROR_ACTION, cmd);
-       ASSERT (snprintf (cmd, SNPRINTF_SIZE, "ld -export-dynamic -shared -o \"%s\" \"%s\"", FILE_LIBRARY_NAME (&A68_JOB), FILE_BINARY_NAME (&A68_JOB)) >= 0);
+-      ASSERT (snprintf (cmd, SNPRINTF_SIZE, "ld -export-dynamic -shared -o \"%s\" \"%s\"", FILE_LIBRARY_NAME (&A68_JOB), FILE_BINARY_NAME (&A68_JOB)) >= 0);
++      ASSERT (snprintf (cmd, SNPRINTF_SIZE, "ld -rpath=. -export-dynamic -shared -o \"%s\" \"%s\"", FILE_LIBRARY_NAME (&A68_JOB), FILE_BINARY_NAME (&A68_JOB)) >= 0);
        ABEND (system (cmd) != 0, ERROR_ACTION, cmd);
+       a68_rm (FILE_BINARY_NAME (&A68_JOB));
+     }
