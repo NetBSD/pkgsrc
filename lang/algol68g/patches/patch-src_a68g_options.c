0e3d2d7cdcdaf97b84b7be601598f9745af9ce27
rhialto <rhialto@pkgsrc.org>
2022-12-04 17:11:13 +0000

    lang/algol68g: update to version 3.1.0
    Most pkgsrc patches were incorporated.
    
    From the NEWS file:
    
    Version 3.1.0, November 2022
    * Fix several bugs reported for 3.0.0-8.
    * Apply several OS dependent patches reported for 3.0.0-8.
    * READ and INCLUDE relative to the source file, not the present working directory.
      This conforms to the C preprocessor standard.
    
    Versions 3.0.1-8, January/September 2022
    * Fix several bugs reported for 3.0.0-7.
    * Apply several OS dependent patches reported for 3.0.0-7.
    * Update a68g.exe to GSL 2.7.1 and R mathlib 4.1.2.

--- lang/algol68g/patches/patch-src_a68g_options.c
+++ lang/algol68g/patches/patch-src_a68g_options.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_a68g_options.c,v 1.1 2022/03/02 01:41:34 ryoon Exp $
+$NetBSD: patch-src_a68g_options.c,v 1.2 2022/12/04 17:11:13 rhialto Exp $
 
 * Enable compiler for *BSD.
 
 --- src/a68g/options.c.orig	2022-02-01 20:45:41.000000000 +0000
 +++ src/a68g/options.c
-@@ -544,7 +544,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
+@@ -548,7 +548,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
  #endif
              }
              if (eq (q, "linux")) {
@@ -13,30 +13,3 @@ $NetBSD: patch-src_a68g_options.c,v 1.1 2022/03/02 01:41:34 ryoon Exp $
                io_close_tty_line ();
                WRITE (STDOUT_FILENO, "linux required - exiting graciously");
                a68_exit (EXIT_SUCCESS);
-@@ -701,7 +701,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
-         }
- // COMPILE and NOCOMPILE switch on/off compilation.
-         else if (eq (p, "Compile")) {
--#if defined (BUILD_LINUX)
-+#if defined (BUILD_LINUX) || defined(BUILD_BSD)
-           OPTION_COMPILE (&A68_JOB) = A68_TRUE;
-           OPTION_COMPILE_CHECK (&A68_JOB) = A68_TRUE;
-           if (OPTION_OPT_LEVEL (&A68_JOB) < OPTIMISE_1) {
-@@ -745,7 +745,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
-         }
- // RUN-SCRIPT runs a compiled .sh script.
-         else if (eq (p, "RUN-SCRIPT")) {
--#if defined (BUILD_LINUX)
-+#if defined (BUILD_LINUX) || defined(BUILD_BSD)
-           FORWARD (i);
-           if (i != NO_OPTION_LIST) {
-             if (!name_set) {
-@@ -766,7 +766,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
-         }
- // RUN-QUOTE-SCRIPT runs a compiled .sh script.
-         else if (eq (p, "RUN-QUOTE-SCRIPT")) {
--#if defined (BUILD_LINUX)
-+#if defined (BUILD_LINUX) || defined(BUILD_BSD)
-           FORWARD (i);
-           if (i != NO_OPTION_LIST) {
-             if (!name_set) {
