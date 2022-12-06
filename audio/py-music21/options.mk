52ab01d347401f04267707257f197e421f583ed8
gutteridge <gutteridge@pkgsrc.org>
2022-12-06 01:45:17 +0000

    py-music21: update a comment

--- audio/py-music21/options.mk
+++ audio/py-music21/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2022/04/09 21:57:28 gutteridge Exp $
+# $NetBSD: options.mk,v 1.6 2022/12/06 01:45:17 gutteridge Exp $
 
 # print/lilypond and devel/py-game are optional because they pull in
-# large and complex dependency chains.
+# large and complex dependency chains. (lilypond also has a tendency
+# to fail to build.)
 
 # audio/musescore is also an application dependency that gets called
 # by music21's show() method, but it's a pretty heavy dependency for
