d3441311bbb8512b86688e86a5a312d98389f2b9
tsutsui <tsutsui@pkgsrc.org>
2022-12-07 15:37:12 +0000

    ruby-gdk3: fix typo.

--- graphics/ruby-gdk3/buildlink3.mk
+++ graphics/ruby-gdk3/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/08/11 05:08:47 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2022/12/07 15:37:12 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gdk3
 
 .if !defined(RUBY_GDK3_BUILDLINK3_MK)
 RUBY_GDK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gtk3>=3.3.8
+BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=3.3.8
 BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=3.5.1nb1
 BUILDLINK_PKGSRCDIR.ruby-gdk3?=		../../graphics/ruby-gdk3
 
