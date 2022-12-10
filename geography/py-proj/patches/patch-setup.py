bcf9efa7b1c98baa46865c8f3313d4ce56078586
gdt <gdt@pkgsrc.org>
2022-12-10 13:19:38 +0000

    geography/py-proj: Update to 3.2.1
    This is the most recent verison which works with the version of proj i
    pkgsrc, 7.
    
    Upstream NEWS, less bug fixes, minor improvements, doc changes,
    internal changes, and things not relevant to pkgsrc usage,
    e.g. statements about binary builds and dependency bumps satisfied in
    pkgsrc.  But note that minor enhancements add up to supporting much
    more of what is in proj.
    
    3.2.0
    
        CLN: Remove ignore_axis_order kwarg from pyproj.crs.CRS.is_exact_same() as it was added by accident (pull #904)
    
        CLN: remove numeric/numarrays support (pull #908)
    
    3.0.0
    
        ENH: Added PROJ Network Settings (#675, #691, #695)
    
        ENH: Added transformation grid sync API/CLI (issue #572)

--- geography/py-proj/patches/patch-setup.py
+++ geography/py-proj/patches/patch-setup.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-setup.py,v 1.1 2022/01/27 08:24:51 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2022/12/10 13:19:38 gdt Exp $
 
 Force running cythonize, to fix build with python 3.10.
 
---- setup.py.orig	2020-05-04 02:03:06.000000000 +0000
+--- setup.py.orig	2021-03-05 04:25:28.000000000 +0000
 +++ setup.py
-@@ -168,6 +168,7 @@ def get_extension_modules():
-             Extension("pyproj._list", ["pyproj/_list.pyx"], **ext_options),
+@@ -179,6 +179,7 @@ def get_extension_modules():
+             Extension("pyproj._sync", ["pyproj/_sync.pyx"], **ext_options),
          ],
          quiet=True,
 +        force=True,
-         **get_cythonize_options()
-     )
- 
+         compile_time_env={
+             "CTE_PROJ_VERSION_MAJOR": int(proj_version_major),
+             "CTE_PROJ_VERSION_MINOR": int(proj_version_minor),
