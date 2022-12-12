4215e75fe1b6215909741741e8ce431284df0fe1
jperkin <jperkin@pkgsrc.org>
2022-12-12 08:26:46 +0000

    php81: Fix version upper bound.

--- lang/php81/buildlink3.mk
+++ lang/php81/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/11/23 16:18:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2022/12/12 08:26:46 jperkin Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=8.1.0<9.0
+BUILDLINK_API_DEPENDS.php+=	php>=8.1.0<8.2
 BUILDLINK_ABI_DEPENDS.php+=	php>=8.1.12nb1
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php81
 
