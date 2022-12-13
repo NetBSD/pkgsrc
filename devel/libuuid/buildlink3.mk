bd142417f619b57e04b84aba0049597abe7a6773
jperkin <jperkin@pkgsrc.org>
2022-12-13 12:46:24 +0000

    libuuid: Set INCDIRS, fixes some dependent packages.

--- devel/libuuid/buildlink3.mk
+++ devel/libuuid/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/08 20:40:33 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/12/13 12:46:24 jperkin Exp $
 
 BUILDLINK_TREE+=	libuuid
 
@@ -7,6 +7,7 @@ LIBUUID_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libuuid+=	libuuid>=2.18
 BUILDLINK_PKGSRCDIR.libuuid?=	../../devel/libuuid
+BUILDLINK_INCDIRS.libuuid+=	include/uuid
 .endif	# LIBUUID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libuuid
