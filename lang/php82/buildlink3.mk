3d1b58145cf171af02bb9a786df0e04d801a640e
taca <taca@pkgsrc.org>
2022-12-13 00:05:53 +0000

    lang/php82: fix version upper bound

--- lang/php82/buildlink3.mk
+++ lang/php82/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/12/11 14:12:29 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/12/13 00:05:53 taca Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=8.2.0<9.0
+BUILDLINK_API_DEPENDS.php+=	php>=8.2.0<8.3
 BUILDLINK_ABI_DEPENDS.php+=	php>=8.2.0
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php82
 
