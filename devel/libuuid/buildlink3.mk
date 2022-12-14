7c4ba29f6b04f962d76d72b1f96ee4c292b54579
wiz <wiz@pkgsrc.org>
2022-12-14 00:27:04 +0000

    libuuid: revert setting INCDIRS in buildlink3.mk
    This breaks python311 on at least NetBSD-current.

--- devel/libuuid/buildlink3.mk
+++ devel/libuuid/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/12/13 12:46:24 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2022/12/14 00:27:04 wiz Exp $
 
 BUILDLINK_TREE+=	libuuid
 
@@ -7,7 +7,6 @@ LIBUUID_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libuuid+=	libuuid>=2.18
 BUILDLINK_PKGSRCDIR.libuuid?=	../../devel/libuuid
-BUILDLINK_INCDIRS.libuuid+=	include/uuid
 .endif	# LIBUUID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libuuid
