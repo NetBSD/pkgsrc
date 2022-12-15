4885cd33769c6791172ec9e46bdd267681537153
gutteridge <gutteridge@pkgsrc.org>
2022-12-15 01:11:17 +0000

    wasi-libcxx: catch up with LLVM bump

--- lang/wasi-libcxx/buildlink3.mk
+++ lang/wasi-libcxx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/08/12 13:55:48 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/12/15 01:11:17 gutteridge Exp $
 
 BUILDLINK_TREE+=	wasi-libcxx
 
@@ -7,7 +7,7 @@ BUILDLINK_DEPMETHOD.wasi-libcxx?=	build
 .if !defined(WASI_LIBCXX_BUILDLINK3_MK)
 WASI_LIBCXX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=14.0.6
+BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=15.0.6
 BUILDLINK_PKGSRCDIR.wasi-libcxx?=	../../lang/wasi-libcxx
 
 .endif	# WASI_LIBCXX_BUILDLINK3_MK
