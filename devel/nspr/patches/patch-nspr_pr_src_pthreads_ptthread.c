025d9b7e6a0b65992362174d6b6b6de95c28be68
adam <adam@pkgsrc.org>
2022-12-15 23:01:17 +0000

    nspr: properly fix nspr/pr/src/pthreads/ptthread.c; pkglint fixes

--- devel/nspr/patches/patch-nspr_pr_src_pthreads_ptthread.c
+++ devel/nspr/patches/patch-nspr_pr_src_pthreads_ptthread.c
@@ -1,16 +1,19 @@
-$NetBSD: patch-nspr_pr_src_pthreads_ptthread.c,v 1.2 2021/07/01 07:23:00 wiz Exp $
+$NetBSD: patch-nspr_pr_src_pthreads_ptthread.c,v 1.3 2022/12/15 23:01:17 adam Exp $
 
 pthread_t may be 64-bit, avoid casting it.
 https://bugzilla.mozilla.org/show_bug.cgi?id=1718835
 
---- nspr/pr/src/pthreads/ptthread.c.orig	2018-01-18 12:37:14.000000000 +0000
+--- nspr/pr/src/pthreads/ptthread.c.orig	2022-09-12 11:40:15.000000000 +0000
 +++ nspr/pr/src/pthreads/ptthread.c
-@@ -1122,7 +1122,7 @@ PR_IMPLEMENT(void) PR_ProcessExit(PRIntn
+@@ -1183,9 +1183,9 @@ PR_IMPLEMENT(void) PR_ProcessExit(PRIntn
      _exit(status);
  }
  
 -PR_IMPLEMENT(PRUint32) PR_GetThreadID(PRThread *thred)
 +PR_IMPLEMENT(pthread_t) PR_GetThreadID(PRThread *thred)
  {
-     return (PRUint32)thred->id;  /* and I don't know what they will do with it */
+-    return (PRUint32)thred->id;  /* and I don't know what they will do with it */
++    return (pthread_t)thred->id;  /* and I don't know what they will do with it */
  }
+ 
+ /*
