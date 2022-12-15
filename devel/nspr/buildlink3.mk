faa301d191f6caa79986b5b07c07f660d26b03fb
adam <adam@pkgsrc.org>
2022-12-15 23:01:17 +0000

    nspr: properly fix nspr/pr/src/pthreads/ptthread.c; pkglint fixes

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2022/06/28 11:31:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2022/12/15 23:01:17 adam Exp $
 
 BUILDLINK_TREE+=	nspr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nspr
 NSPR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.3.2
-BUILDLINK_ABI_DEPENDS.nspr?=	nspr>=4.34nb1
+BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.34nb1
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
 BUILDLINK_LIBDIRS.nspr+=	lib/nspr
