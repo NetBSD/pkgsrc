da3e2b3a17c066cc334c4890d075295fa889c910
jperkin <jperkin@pkgsrc.org>
2022-12-15 10:37:46 +0000

    fcitx5: Fix SunOS linker args.

--- inputmethod/fcitx5/buildlink3.mk
+++ inputmethod/fcitx5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/02/17 15:16:25 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/12/15 10:37:46 jperkin Exp $
 
 BUILDLINK_TREE+=	fcitx5
 
@@ -8,6 +8,7 @@ FCITX5_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.fcitx5+=	fcitx5>=5.0.1
 BUILDLINK_PKGSRCDIR.fcitx5?=	../../inputmethod/fcitx5
 
+BUILDLINK_TRANSFORM.SunOS+=	rm:-Wl,--as-needed
 .endif	# FCITX5_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fcitx5
