d3479bd67dbc1d6427f0ae660e89bb7175fe6363
gutteridge <gutteridge@pkgsrc.org>
2022-12-15 01:10:02 +0000

    wasi-compiler-rt: catch up with LLVM bump

--- lang/wasi-compiler-rt/buildlink3.mk
+++ lang/wasi-compiler-rt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/08/12 13:59:52 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/12/15 01:10:02 gutteridge Exp $
 
 BUILDLINK_TREE+=	wasi-compiler-rt
 
@@ -9,7 +9,7 @@ WASI_COMPILER_RT_BUILDLINK3_MK:=
 
 # This exact version must be kept in sync with llvm, or firefox
 # minimum build dependencies will be incorrect.
-BUILDLINK_API_DEPENDS.wasi-compiler-rt+=	wasi-compiler-rt>=14.0.6
+BUILDLINK_API_DEPENDS.wasi-compiler-rt+=	wasi-compiler-rt>=15.0.6
 BUILDLINK_PKGSRCDIR.wasi-compiler-rt?=		../../lang/wasi-compiler-rt
 
 .endif	# WASI_COMPILER_RT_BUILDLINK3_MK
