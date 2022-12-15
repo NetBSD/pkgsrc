1fa9bb03a0b9177ae7f0b20793c8dfa559d5f99a
bouyer <bouyer@pkgsrc.org>
2022-12-15 13:24:43 +0000

    Update xenkernel415, xentools415 and xenstoretools to 4.15.4 Main changes from 4.15.3: includes all security fixes up to and including XSA-422 other changes are minor improvements and fixes. Complete changelog can be found at: https://xenproject.org/downloads/xen-project-archives/xen-project-4-15-series/xen-project-4-15-4/

--- sysutils/xenkernel415/patches/patch-xen_arch_x86_Rules.mk
+++ sysutils/xenkernel415/patches/patch-xen_arch_x86_Rules.mk
@@ -1,10 +1,10 @@
-$NetBSD: patch-xen_arch_x86_Rules.mk,v 1.1 2021/04/18 12:31:26 bouyer Exp $
+$NetBSD: patch-xen_arch_x86_Rules.mk,v 1.2 2022/12/15 13:24:44 bouyer Exp $
 
---- xen/arch/x86/Rules.mk.orig	2020-12-08 15:28:32.000000000 +0100
-+++ xen/arch/x86/Rules.mk	2020-12-08 17:27:58.740162095 +0100
+--- xen/arch/x86/Rules.mk.orig	2022-11-15 09:03:34.000000000 +0100
++++ xen/arch/x86/Rules.mk	2022-12-14 21:22:01.302860156 +0100
 @@ -8,3 +8,5 @@
  endif
- c_flags += $(object_label_flags) $(CFLAGS-stack-boundary)
- a_flags += $(object_label_flags) $(CFLAGS-stack-boundary)
+ c_flags += $(object_label_flags) $(CFLAGS_stack_boundary)
+ a_flags += $(object_label_flags) $(CFLAGS_stack_boundary)
 +
 +c_flags += $(EXTRA_CFLAGS)
