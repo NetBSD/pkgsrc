5c091c1afae87e4aa1ca588bf744427719b01967
adam <adam@pkgsrc.org>
2022-12-16 21:08:07 +0000

    gstreamer1: updated to 1.20.4
    Highlighted bugfixes in 1.20.4
    
    avaudiodec: fix playback issue with WMA files, would throw an error at EOS with FFmpeg 5.x
    Fix deadlock when loading gst-editing-services plugin
    Fix input buffering capacity in live mode for aggregator, video/audio aggregator subclasses, muxers
    glimagesink: fix crash on Android
    subtitle handling and subtitle overlay fixes
    matroska-mux: allow width + height changes for avc3|hev1|vp8|vp9
    rtspsrc: fix control url handling for spec compliant servers and add fallback for incompliant servers
    WebRTC fixes
    RTP retransmission fixes
    video: fixes for formats with 4x subsampling and horizontal co-sited chroma (Y41B, YUV9, YVU9 and IYU9)
    macOS build and packaging fixes, in particular fix finding of gio modules on macOS for https/TLS support
    Fix consuming of the macOS package as a framework in XCode
    Performance improvements
    Miscellaneous bug fixes, memory leak fixes, and other stability and reliability improvements

--- graphics/gst-plugins1-gdk_pixbuf/buildlink3.mk
+++ graphics/gst-plugins1-gdk_pixbuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/12/08 16:02:10 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2022/12/16 21:08:11 adam Exp $
 
 BUILDLINK_TREE+=	gst-plugins1-gdk_pixbuf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gst-plugins1-gdk_pixbuf
 GST_PLUGINS1_GDK_PIXBUF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gst-plugins1-gdk_pixbuf+=	gst-plugins1-gdk_pixbuf>=1.18.0
-BUILDLINK_ABI_DEPENDS.gst-plugins1-gdk_pixbuf?=	gst-plugins1-gdk_pixbuf>=1.18.5
+BUILDLINK_ABI_DEPENDS.gst-plugins1-gdk_pixbuf+=	gst-plugins1-gdk_pixbuf>=1.18.5
 BUILDLINK_PKGSRCDIR.gst-plugins1-gdk_pixbuf?=	../../graphics/gst-plugins1-gdk_pixbuf
 
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
--- multimedia/gst-plugins1-good/buildlink3.mk
+++ multimedia/gst-plugins1-good/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/11/15 22:53:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2022/12/16 21:08:08 adam Exp $
 
 BUILDLINK_TREE+=	gst-plugins1-good
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gst-plugins1-good
 GST_PLUGINS1_GOOD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gst-plugins1-good+=	gst-plugins1-good>=1.18.4
-BUILDLINK_ABI_DEPENDS.gst-plugins1-good?=		gst-plugins1-good>=1.18.5
+BUILDLINK_ABI_DEPENDS.gst-plugins1-good+=	gst-plugins1-good>=1.18.5
 BUILDLINK_PKGSRCDIR.gst-plugins1-good?=		../../multimedia/gst-plugins1-good
 
 .include "../../multimedia/gstreamer1/buildlink3.mk"
