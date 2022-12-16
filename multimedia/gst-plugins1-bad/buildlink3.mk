b9585078c4aa3a5f0050e3969c855860e4f647e7
adam <adam@pkgsrc.org>
2022-12-16 21:08:07 +0000

    gstreamer1: updated to 1.20.4
    Highlighted bugfixes in 1.20.4
    
    avaudiodec: fix playback issue with WMA files, would throw an error at EOS with FFmpeg 5.x
    Fix deadlock when loading gst-editing-services plugin
    Fix input buffering capacity in live mode for aggregator, video/audio aggregator subclasses, muxers
    glimagesink: fix crash on Android
    subtitle handling and subtitle overlay fixes
    matroska-mux: allow width + height changes for avc3|hev1|vp8|vp9
    rtspsrc: fix control url handling for spec compliant servers and add fallback for incompliant servers
    WebRTC fixes
    RTP retransmission fixes
    video: fixes for formats with 4x subsampling and horizontal co-sited chroma (Y41B, YUV9, YVU9 and IYU9)
    macOS build and packaging fixes, in particular fix finding of gio modules on macOS for https/TLS support
    Fix consuming of the macOS package as a framework in XCode
    Performance improvements
    Miscellaneous bug fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gst-plugins1-bad/buildlink3.mk
+++ multimedia/gst-plugins1-bad/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/07/29 15:14:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2022/12/16 21:08:08 adam Exp $
 
 BUILDLINK_TREE+=	gst-plugins1-bad
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gst-plugins1-bad
 GST_PLUGINS1_BAD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gst-plugins1-bad+=	gst-plugins1-bad>=1.18.4
-BUILDLINK_ABI_DEPENDS.gst-plugins1-bad?=	gst-plugins1-bad>=1.18.5
+BUILDLINK_ABI_DEPENDS.gst-plugins1-bad+=	gst-plugins1-bad>=1.18.5
 BUILDLINK_PKGSRCDIR.gst-plugins1-bad?=		../../multimedia/gst-plugins1-bad
 
 .include "../../multimedia/gst-plugins1-base/buildlink3.mk"
