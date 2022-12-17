f724e6110b201ba08ca18d4a69e061fcf799ed7a
osa <osa@pkgsrc.org>
2022-12-17 04:37:42 +0000

    */*unit*: update NGINX Unit from 1.28.0 to 1.29.0
    Remove python 3.11 patch.
    Adopt Solaris/Illumos/SmartOS patch.
    
    <ChangeLog>
    
    *) Change: removed $uri auto-append for "share" when loading
       configuration.
    
    *) Change: prefer system crypto policy instead of hardcoding a default.
    
    *) Feature: njs support with the basic syntax of JS template literals.
    
    *) Feature: support per-application cgroups on Linux.
    
    *) Feature: the $request_time variable contains the request processing
       time.
    
    *) Feature: "prefix" option in Python applications to set WSGI
       "SCRIPT_NAME" and ASGI root-path variables.
    
    *) Feature: compatibility with Python 3.11.
    
    *) Feature: compatibility with OpenSSL 3.
    
    *) Feature: compatibility with PHP 8.2.
    
    *) Feature: compatibility with Node.js 19.0.
    
    *) Feature: Ruby Rack v3 support.
    
    *) Bugfix: fix error in connection statistics when using proxy.
    
    *) Bugfix: fix HTTP cookie parsing when the value contains an equals
       sign.
    
    *) Bugfix: PHP directory URLs without a trailing '/' would give a 503
       error (fixed with a 301 re-direct).
    
    *) Bugfix: missing error checks in the C API.
    
    *) Bugfix: report the regex status in configure summary.
    
    </ChangeLog>

--- www/unit/version.mk
+++ www/unit/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.6 2022/09/14 05:31:41 osa Exp $
+# $NetBSD: version.mk,v 1.7 2022/12/17 04:37:42 osa Exp $
 #
 # UNIT_EXTENSION_DIR
 #	Relative path to ${PREFIX} for NGINX Unit's modules.
@@ -12,7 +12,7 @@
 UNITVERSION_MK=	defined
 
 # Define NGINX Unit's version.
-UNIT_VERSION=	1.28.0
+UNIT_VERSION=	1.29.0
 
 # Define NGINX Unit's modules directory
 UNIT_EXTENSION_DIR=	libexec/unit/modules
