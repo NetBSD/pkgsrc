b62db692282be85972469870ed9868f44f18909b
adam <adam@pkgsrc.org>
2022-12-17 06:22:02 +0000

    py-curl: updated to 7.45.2
    Version 7.45.2
    
    * Python 3.9 compatibility for Py_TRASHCAN_SAFE_BEGIN
      (patch by Scott Talbert).
    * Add support for CURL_HTTP_VERSION_3 (patch by Scott Talbert).
    * Add CURLOPT_TLS13_CIPHERS and CURLOPT_PROXY_TLS13_CIPHERS options
      (patch by Scott Talbert).
    * Added HTTP09_ALLOWED option (patch by Scott Talbert).
    * Removed use of distutils (patch by Scott Talbert).

--- www/py-curl/patches/patch-setup.py
+++ www/py-curl/patches/patch-setup.py
@@ -1,23 +1,10 @@
-$NetBSD: patch-setup.py,v 1.4 2021/11/07 11:32:51 adam Exp $
+$NetBSD: patch-setup.py,v 1.5 2022/12/17 06:22:02 adam Exp $
 
-Stick to distutils.
 Add multi Python variant support.
 
---- setup.py.orig	2021-08-15 19:30:48.000000000 +0000
+--- setup.py.orig	2022-12-16 19:31:55.000000000 +0000
 +++ setup.py
-@@ -10,10 +10,7 @@ VERSION = "7.44.1"
- 
- import glob, os, re, sys, subprocess
- import distutils
--try:
--    from setuptools import setup
--except ImportError:
--    from distutils.core import setup
-+from distutils.core import setup
- from distutils.extension import Extension
- from distutils.util import split_quoted
- from distutils.version import LooseVersion
-@@ -686,10 +683,11 @@ def get_extension(argv, split_extension_
+@@ -662,10 +662,11 @@ def get_extension(argv, split_extension_
  def get_data_files():
      # a list of tuples with (path to install to, a list of local files)
      data_files = []
