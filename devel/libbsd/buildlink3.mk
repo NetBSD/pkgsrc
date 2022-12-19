81e4eb9a1de64e647493f26c15299106e9a592e9
vins <vins@pkgsrc.org>
2022-12-19 16:05:43 +0000

    devel/libbsd: update to version 0.11.7
     # Changes (local):
     * Add new dependency on devel/libmd.
     * Rework buildlink file to use overly linking mode.
     * Add a test target for regression tests.
     * Update licese.
     * Fix bug with Makefile caused by lack of an implicit
       $(RM) variable in bmake.
    
    # Changes (upstream):
    
    * libbsd 0.11.7
      - Portability fixes for the Hurd.
      - Fix ELF support for big endian SH.
      - Sync the arc4random(3) implementation from OpenBSD.
      - Adjust declaration shadowing to match new glibc additions.
      - Manual pages and documentation cleanups.
      - Document non-overlay mode as discouraged, future releases might emit
        warnings when using it.
    
    * libbsd 0.11.6
      - Build system and test suite fixes for musl.
      - Remove arc4random() support for OpenBSD.
      - LoongArch support for nlist().
    
    * libbsd-0.11.5
      - Build system and test suite regression fixes.
      - Documentation on how to build the project.
    
    * libbsd 0.11.4
      - Fix build on GNU/kFreeBSD due to missing linking against libmd.
      - Fix build with LTO.
      - Add link-time warnings to MD5 wrapper functions
      - Various build system fixes.
      - Various portability fixes.
      - Various documentation fixes.
    
    * libbsd 0.11.3
      - Rework of the libmd wrapping code to not require users to explicitly
        link against libmd.
      - Switch libmd wrapper to use dlsym()
      - Various build system fixes.
      - Various portability fixes.
    
    * libbsd 0.11.2
      - Update <sys/queue.h> from FreeBSD.
      - Import some closefrom() changes from sudo.
      - Make closefrom() use close_range() syscall on Linux when available.
      - Update libbsd(7) man page with updates in 0.11.0.
    
    * libbsd 0.11.1
      - Export provided function strnvisx().
      - New recallocarray() and freezero() from OpenBSD.
      - New pwcache module from OpenBSD.
      - New timespec(3bsd) man page alias to timeval(3bsd).
      - New progname implementation for Windows.
      - New LIBBSD_VIS_OPENBSD selection macro.
      - Switch from embedded hashing function implementations to use libmd.
      - Various man pages cleanups.
      - Various portability fixes.
      - Various memory leak fixes.
    
    * libbsd 0.10.0
      - Several security related fixes for nlist()
      - Fix for a leak in the vis family of functions.
      - Fix for a configure check to not unnecessarily link against librt.
      - General portability fixes for musl, uClibc, macOS and GNU/kFreeBSD.
      - New architectures support for nlist().
      - Switch the <err.h> *c() functions to be standalone and add err(),
        warn(), errx() and warnx() familiy of functions in case the system
        lacks them.
      - Several man page fixes.

--- devel/libbsd/buildlink3.mk
+++ devel/libbsd/buildlink3.mk
@@ -1,12 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/12/09 11:21:46 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2022/12/19 16:05:43 vins Exp $
 
 BUILDLINK_TREE+=	libbsd
 
 .if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.10.0
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.7
 BUILDLINK_PKGSRCDIR.libbsd?=	../../devel/libbsd
+
+BUILDLINK_CFLAGS.libbsd+=	-I${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
+BUILDLINK_LDFLAGS.libbsd+=	-lbsd -lmd
+
+.include "../../devel/libmd/buildlink3.mk"
 .endif	# LIBBSD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbsd
