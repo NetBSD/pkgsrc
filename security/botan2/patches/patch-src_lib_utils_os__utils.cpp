f734786fa8435f30cbf8d919f3a1141068c16f7d
nros <nros@pkgsrc.org>
2022-12-19 17:39:40 +0000

    botan2: fix build of os_utils.cpp on SunOS
    os_utils.cpp needs priv.h to be included on
    SunOS for the patch to build.
    Use Botans own BOTAN_TARGET_OS_IS_SOLARIS
    instead of __sun as indicator for building
    on SunOS.

--- security/botan2/patches/patch-src_lib_utils_os__utils.cpp
+++ security/botan2/patches/patch-src_lib_utils_os__utils.cpp
@@ -1,15 +1,26 @@
-$NetBSD: patch-src_lib_utils_os__utils.cpp,v 1.1 2022/04/01 08:01:12 wiz Exp $
+$NetBSD: patch-src_lib_utils_os__utils.cpp,v 1.2 2022/12/19 17:39:40 nros Exp $
 
 Add check for ability to do mlock() on SunOS.
 
---- src/lib/utils/os_utils.cpp.orig	2019-07-15 18:14:39.932394200 +0000
+--- src/lib/utils/os_utils.cpp.orig	2022-11-16 11:19:19.000000000 +0000
 +++ src/lib/utils/os_utils.cpp
-@@ -325,7 +325,21 @@ size_t OS::system_page_size()
+@@ -60,6 +60,10 @@
+   #include <mach/vm_statistics.h>
+ #endif
+ 
++#if defined(BOTAN_TARGET_OS_IS_SOLARIS)
++#  include <priv.h>
++#endif
++
+ namespace Botan {
+ 
+ // Not defined in OS namespace for historical reasons
+@@ -326,7 +330,21 @@ size_t OS::system_page_size()
  
  size_t OS::get_memory_locking_limit()
     {
 -#if defined(BOTAN_TARGET_OS_HAS_POSIX1) && defined(BOTAN_TARGET_OS_HAS_POSIX_MLOCK) && defined(RLIMIT_MEMLOCK)
-+#if defined(__sun)
++#if defined(BOTAN_TARGET_OS_IS_SOLARIS)
 +   priv_set_t *priv_set = priv_allocset();
 +   if (priv_set == nullptr)
 +     return 0;
