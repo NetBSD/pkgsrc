b530b382b144ffe0998d59fa88e1e29c8b4e9d91
nia <nia@pkgsrc.org>
2022-12-21 10:10:37 +0000

    firefox102: Disable service worker support until the root cause of the crashes is investigated.

--- www/firefox102/patches/patch-gfx_webrender__bindings_webrender__ffi.h
+++ www/firefox102/patches/patch-gfx_webrender__bindings_webrender__ffi.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-gfx_webrender__bindings_webrender__ffi.h,v 1.1 2022/08/07 18:08:08 nia Exp $
+$NetBSD: patch-gfx_webrender__bindings_webrender__ffi.h,v 1.2 2022/12/21 10:10:37 nia Exp $
 
 https://bugzilla.mozilla.org/show_bug.cgi?id=1773259
 
---- gfx/webrender_bindings/webrender_ffi.h.orig	2022-07-18 18:14:25.000000000 +0000
+--- gfx/webrender_bindings/webrender_ffi.h.orig	2022-11-07 21:06:45.000000000 +0000
 +++ gfx/webrender_bindings/webrender_ffi.h
 @@ -73,8 +73,6 @@ struct WrPipelineInfo;
  struct WrPipelineIdAndEpoch;
