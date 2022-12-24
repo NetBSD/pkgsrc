4870dcf77383303eeb83c72890323444f0c87a64
wiz <wiz@pkgsrc.org>
2022-12-24 15:23:41 +0000

    dosbox-x: fix build on non-Linux & non-FreeBSD

--- emulators/dosbox-x/patches/patch-src_output_output__surface.cpp
+++ emulators/dosbox-x/patches/patch-src_output_output__surface.cpp
@@ -1,16 +1,17 @@
-$NetBSD: patch-src_output_output__surface.cpp,v 1.1 2022/11/06 21:31:57 triaxx Exp $
+$NetBSD: patch-src_output_output__surface.cpp,v 1.2 2022/12/24 15:23:42 wiz Exp $
 
-Fix linking error on FreeBSD (from FreeBSD ports).
+Match #ifdef with the one for the code definition.
 
 --- src/output/output_surface.cpp.orig	2022-09-01 22:55:29.000000000 +0000
 +++ src/output/output_surface.cpp
-@@ -24,7 +24,9 @@ void OUTPUT_SURFACE_Initialize()
+@@ -24,8 +24,10 @@ void OUTPUT_SURFACE_Initialize()
      // blitting the wrong dimensions to the window and trigger an X11 BadValue error.
      // Set up an error handler that prints the error to STDERR and then returns,
      // instead of the default handler which prints an error and exit()s this program.
-+#if !defined(__FreeBSD__)
++#ifdef LINUX
      X11_ErrorHandlerInstall();
-+#endif
  #endif
++#endif
  }
  
+ void OUTPUT_SURFACE_Select()
