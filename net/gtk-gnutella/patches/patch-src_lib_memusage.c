dceb4d1af329e8bb70b60de1d3cab9706a385e3a
wiz <wiz@pkgsrc.org>
2022-12-25 18:03:42 +0000

    gtk-gnutella: add upstream pull request URL

--- net/gtk-gnutella/patches/patch-src_lib_memusage.c
+++ net/gtk-gnutella/patches/patch-src_lib_memusage.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_lib_memusage.c,v 1.1 2022/09/18 20:04:30 he Exp $
+$NetBSD: patch-src_lib_memusage.c,v 1.2 2022/12/25 18:03:42 wiz Exp $
 
 Avoid naming collision with powerpc/frame.h definition of callframe.
+https://github.com/gtk-gnutella/gtk-gnutella/pull/34
 
 --- src/lib/memusage.c.orig	2016-11-11 14:33:11.000000000 +0000
 +++ src/lib/memusage.c
