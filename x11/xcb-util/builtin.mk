0c5dc9c7232c610ab7b6132f190c5a1dd650be9c
wiz <wiz@pkgsrc.org>
2022-12-26 22:10:43 +0000

    xcb-util: update to 0.4.1.
    Alan Coopersmith (10):
          Update configure.ac bug URL for gitlab migration
          Update README for gitlab migration
          Add README.md to EXTRA_DIST
          Build xz tarballs instead of bzip2
          Update m4 to xorg/util/xcb-util-m4@c617eee22ae5c285e79e81
          gitlab CI: add a basic build test
          atoms: add format attribute to makenames() function
          atoms: Fix type mismatch in xcb_atom_name_unique()
          xcb_aux: handle -Wimplicit-int-conversion warnings from clang
          xcb-util 0.4.1
    
    Emil Velikov (1):
          autogen.sh: use quoted string variables
    
    Mihail Konev (1):
          autogen: add default patch prefix
    
    Peter Hutterer (1):
          autogen.sh: use exec instead of waiting for configure to finish

--- x11/xcb-util/builtin.mk
+++ x11/xcb-util/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.6 2019/11/03 09:14:19 rillig Exp $
+# $NetBSD: builtin.mk,v 1.7 2022/12/26 22:10:43 wiz Exp $
 
 BUILTIN_PKG:=			xcb-util
 PKGCONFIG_FILE.xcb-util=	${X11BASE}/lib/pkgconfig/xcb-atom.pc
@@ -7,7 +7,7 @@ PKGCONFIG_FILE.xcb-util+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xcb-atom.pc
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
 
-.if !empty(USE_BUILTIN.xcb-util:M[yY][eE][sS])
+.if ${USE_BUILTIN.xcb-util:tl} == yes
 BUILDLINK_FILES.xcb-util+=	lib/pkgconfig/xcb-atom.pc
 BUILDLINK_FILES.xcb-util+=	lib/pkgconfig/xcb-aux.pc
 BUILDLINK_FILES.xcb-util+=	lib/pkgconfig/xcb-event.pc
