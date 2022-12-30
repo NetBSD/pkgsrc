455597582eadac41edce4e91bb14df63249cef3c
bsiegert <bsiegert@pkgsrc.org>
2022-12-30 17:32:35 +0000

    Update XFCE4 to 4.18.0, part 2.
    xfce4-appfinder, xfce4-power-manager, xfce4-thunar, xfce4-wm,
    xfce4-desktop, xfce4-garcon, xfce4-panel, xfce4-settings
    
    The full changelog (long) can be found at
    https://www.xfce.org/download/changelogs/4.18.

--- x11/xfce4-garcon/buildlink3.mk
+++ x11/xfce4-garcon/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.8 2021/12/08 16:03:03 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2022/12/30 17:32:35 bsiegert Exp $
 
 BUILDLINK_TREE+=	xfce4-garcon
 
 .if !defined(XFCE4_GARCON_BUILDLINK3_MK)
 XFCE4_GARCON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-garcon+=	xfce4-garcon>=0.4.0
-BUILDLINK_ABI_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.16.1
+BUILDLINK_API_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.18.0
+BUILDLINK_ABI_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.18.0
 BUILDLINK_PKGSRCDIR.xfce4-garcon?=	../../x11/xfce4-garcon
 
 .include "../../x11/libxfce4ui/buildlink3.mk"
 .endif	# XFCE4_GARCON_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-xfce4-garcon
+BUILDLINK_TREE+=	-xfce4-garconp
