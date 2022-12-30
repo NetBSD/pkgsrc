4f8bba0805956c67c58dd0cd6e373e9cedc3d3ff
bsiegert <bsiegert@pkgsrc.org>
2022-12-30 17:16:56 +0000

    Update XFCE4 to 4.18.0, part 1.
    libxfce4util, libxfce4ui, xfce4-conf, xfce4-exo, xfce4-tumbler
    
    The full changelog (long) can be found at
    https://www.xfce.org/download/changelogs/4.18.

--- x11/xfce4-exo/buildlink3.mk
+++ x11/xfce4-exo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.37 2022/11/17 01:35:31 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2022/12/30 17:16:56 bsiegert Exp $
 
 BUILDLINK_TREE+=	xfce4-exo
 
 .if !defined(XFCE4_EXO_BUILDLINK3_MK)
 XFCE4_EXO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=0.12
-BUILDLINK_ABI_DEPENDS.xfce4-exo+=	xfce4-exo>=4.16.4nb1
+BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=4.18.0
+BUILDLINK_ABI_DEPENDS.xfce4-exo+=	xfce4-exo>=4.18.0
 BUILDLINK_PKGSRCDIR.xfce4-exo?=		../../x11/xfce4-exo
 
 .include "../../x11/libxfce4ui/buildlink3.mk"
