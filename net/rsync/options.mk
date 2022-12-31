f294a38d17fb07e7238c828f4969cc370d37bee5
sekiya <sekiya@pkgsrc.org>
2022-12-31 09:10:15 +0000

    Restore acl option, which was inadvertantly nuked during the pkgsrc branch.
    Sorry, adam@

--- net/rsync/options.mk
+++ net/rsync/options.mk
@@ -1,11 +1,15 @@
-# $NetBSD: options.mk,v 1.4 2022/12/29 00:47:40 sekiya Exp $
+# $NetBSD: options.mk,v 1.5 2022/12/31 09:10:15 sekiya Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rsync
 
-PKG_SUPPORTED_OPTIONS=	zstd
-PKG_SUGGESTED_OPTIONS=	zstd
+PKG_SUPPORTED_OPTIONS=	acl zstd
+PKG_SUGGESTED_OPTIONS=	acl zstd
 
 .include "../../mk/bsd.options.mk"
+
+.if empty(PKG_OPTIONS:Macl)
+CONFIGURE_ARGS+=       --disable-acl-support
+.endif
 
 .if !empty(PKG_OPTIONS:Mzstd)
 .include "../../archivers/zstd/buildlink3.mk"
