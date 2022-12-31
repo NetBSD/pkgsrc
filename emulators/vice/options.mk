4e09abe65466c8c8ee28836d55afb092761171e2
rhialto <rhialto@pkgsrc.org>
2022-12-31 18:30:00 +0000

    emulators/vice: updated to 3.7.1
    Small extract from the release notes:
    
    - Added Debian packages of development builds of the SDL2 and Gtk3 UIs and the
      headless build (amd64 only).
    
      You can find bleeding edge development builds on github:
    
      https://github.com/VICE-Team/svn-mirror/releases
    
    - Renamed all included ROM files to use a more elaborate naming scheme, for
      example as in "kernal-901227-01.bin".
    
    - CRT emulation improvements:
      - uses measured (different) color coordinates for odd/even lines when possible
      - added option for U-only delay line (1084 style)
    
    - More realistic handling of the case when two paddles are selected at the same
      time.
    
    - Keyboard handling was rewritten / fixed
      - The case when two host keys are mapped to different CBM keys with different
        modifiers is handled differently (correctly) now, so some old keymap(s) may
        expose detail errors as a result - please report them!
      - Fixed Shiftlock/Capslock mapping
      - All extra keys that are not in the regular matrix are handled in generic
        code now.
        - used by C128 40/80 and CAPS keys, Restore
        - extra keys can operate in "non locking" mode
    
    - More realistic behaviour of the virtual drive
      - always return 'files scratched' (instead of 'file not found'), even if the
        file does not exist
    
    - More realistic behaviour when mounting images:
      - When attaching a d64 image to a 1571 drive, create an 'unformatted' image for
        the second side.
      - Create empty tracks for non existing tracks also when mounting gcr images
    
    - SID fixes:
      - use a few more bits for better accuracy when calculating the 8580 filter
        DAC values.
      - fix filter ringing/self oscillating with 6581 and 8580
    
    - CIA fixes:
      - Improved the CIA shift register.
      - Fix the case when pb changes value after changing the control bits for
        pb/pb7 output. Fixes the turn disk space detection of the 'Love' demo.
    
    - Add SPT (Stupid Pet Tricks) userport joystick adapter emulation.
    
    - Fixed use of SO_REUSEPORT on server sockets - this allowed multiple VICE
      instances to bind to the same port, with an indeterminant one receiving each
      incoming connection. Now uses SO_REUSEADDR instead, which allows use of a
      port that remains in the TIME_WAIT state after a previous process closed,
      but does not allow use of a port that is still in active use.
    
    - Add DragonFly BSD support.
    
    - Remove Android support.
    
    and much more, for the full list see the included NEWS file.

--- emulators/vice/options.mk
+++ emulators/vice/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.15 2022/01/27 18:23:39 rhialto Exp $
+# $NetBSD: options.mk,v 1.16 2022/12/31 18:30:00 rhialto Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.vice
 PKG_SUPPORTED_OPTIONS=		ffmpeg vice-x64 vice-cpuhistory pulseaudio alsa
@@ -8,12 +8,11 @@ PKG_SUGGESTED_OPTIONS=		gtk3 ffmpeg
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	gtk sdl x64 desktop gh
+PLIST_VARS+=	gtk sdl x64 desktop
 
 .if !empty(PKG_OPTIONS:Mgtk3)
-CONFIGURE_ARGS+=	--enable-native-gtk3ui
+CONFIGURE_ARGS+=	--enable-gtk3ui
 PLIST.gtk=		yes
-PLIST.gh=		yes	# gtk-or-headless
 TOOL_DEPENDS+=		glib2-tools>=2.56:../../devel/glib2-tools
 
 CONFIGURE_ARGS+=	--enable-desktop-files
@@ -44,14 +43,14 @@ BUILD_DEFS+=	PKG_SYSCONFBASE
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl)
-CONFIGURE_ARGS+=	--enable-sdlui
+CONFIGURE_ARGS+=	--enable-sdl1ui
 PLIST.sdl=		yes
 .  include "../../devel/SDL/buildlink3.mk"
 .  include "../../graphics/SDL_image/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl2)
-CONFIGURE_ARGS+=	--enable-sdlui2
+CONFIGURE_ARGS+=	--enable-sdl2ui
 PLIST.sdl=		yes
 .  include "../../devel/SDL2/buildlink3.mk"
 .  include "../../graphics/SDL2_image/buildlink3.mk"
@@ -59,14 +58,13 @@ PLIST.sdl=		yes
 
 .if !empty(PKG_OPTIONS:Mvice-headless)
 CONFIGURE_ARGS+=	--enable-headlessui
-PLIST.gh=		yes	# gtk-or-headless
 .endif
 
 # If desired, ffmpeg and lame can be build-time-only dependencies,
 # since they are loaded dynamically only.
 .if !empty(PKG_OPTIONS:Mffmpeg)
 #BUILDLINK_DEPMETHOD.ffmpeg?=	build
-CONFIGURE_ARGS+=	--enable-external-ffmpeg
+CONFIGURE_ARGS+=	--enable-ffmpeg
 .  include "../../multimedia/ffmpeg4/buildlink3.mk"
 .endif
 
