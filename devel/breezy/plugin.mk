913d4a7aff60b2251df3c90e355a5a07fb2eeed8
rhialto <rhialto@pkgsrc.org>
2023-01-01 14:12:05 +0000

    devel/breezy-qbrz: Successor of qbzr, but with Python 3.x and Qt 5.
    A GUI for the Breezy version control system.
    
    No tagged releases, distfiles, nor release notes available.
    Version 0.4.1 from version.txt.

---
+++ devel/breezy/plugin.mk
@@ -0,0 +1,17 @@
+# $NetBSD: plugin.mk,v 1.1 2023/01/01 14:12:05 rhialto Exp $
+
+BREEZY_PLUGIN_NAME?=	${PKGBASE:S/^breezy-//}
+BREEZY_PLUGIN_TEST?=	yes
+BREEZY_PLUGIN_TEST_DIR?=	${WRKSRC}/build/lib/breezylib/plugins
+
+DEPENDS+=		breezy>=3.0:../../devel/breezy
+
+# stolen from graphics/hicolor-icon-theme
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !defined(TEST_TARGET) && !empty(BREEZY_PLUGIN_TEST:M[Yy][Ee][Ss])
+# stolen from breezy-svn
+do-test:
+	BREEZY_PLUGIN_PATH=${BREEZY_PLUGIN_TEST_DIR} brz selftest -v \
+	  --starting-with=breezylib.plugins.${BREEZY_PLUGIN_NAME}
+.endif
