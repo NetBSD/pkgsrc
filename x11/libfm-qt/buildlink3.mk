9aa3c92d6a0ffc8980af1d0cc9b13f11a82057e3
gutteridge <gutteridge@pkgsrc.org>
2023-01-06 00:34:31 +0000

    libfm-qt: update to 1.2.1
    Change log:
    
    libfm-qt-1.2.1 / 2023-01-02
    ============================
     * Specified the parents of context menus for use on Wayland.
     * Fixed launching XWayland apps under Wayland.
     * appchooserdialog: do not disable the OK button when Custom Command page is shown.
     * Increased the minimum width of name column in detailed list view.
     * Fixed the DND menu position under Wayland.
     * Fixed crash with DND from outside app under Wayland.
     * Fixed unresponsive DND menu under Wayland.
     * Workaround for DND keyboard modifiers under Wayland.
     * Check mime data of clipboard for nullity (for Wayland).
     * Fixed drawing of selection rectangle under Wayland.

--- x11/libfm-qt/buildlink3.mk
+++ x11/libfm-qt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2022/11/23 16:19:09 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2023/01/06 00:34:31 gutteridge Exp $
 
 BUILDLINK_TREE+=	libfm-qt
 
 .if !defined(LIBFM_QT_BUILDLINK3_MK)
 LIBFM_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.2.0
-BUILDLINK_ABI_DEPENDS.libfm-qt?=	libfm-qt>=1.2.0nb1
+BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.2.1
+BUILDLINK_ABI_DEPENDS.libfm-qt?=	libfm-qt>=1.2.1
 BUILDLINK_PKGSRCDIR.libfm-qt?=		../../x11/libfm-qt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
