0c88c0b7bbc0915920d3523ae6e12b62b273ba9c
gdt <gdt@pkgsrc.org>
2023-01-07 16:07:58 +0000

    sysutils/ups-nut: Rototill rc.d scripts
      - Fix strange sh style: don't have then on a new line
      - Ensure main daemons started before SERVERS
      - Slot upsmon after SERVERS and before LOGIN
      - Rewrite upsdriver instructions to make upsdrvctl the standard approach
      - Drop compat for ancient (<1.6) NetBSD

--- sysutils/ups-nut/files/upsdriver.sh
+++ sysutils/ups-nut/files/upsdriver.sh
@@ -1,56 +1,53 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: upsdriver.sh,v 1.9 2018/09/21 00:59:53 jym Exp $
+# $NetBSD: upsdriver.sh,v 1.9.34.1 2023/01/07 16:07:59 gdt Exp $
 #
 # PROVIDE: upsdriver
 # REQUIRE: NETWORK syslogd mountcritremote
 #
-# You will need to set some variables in /etc/rc.conf to start the UPS
-# driver(s) properly, e.g.
+# The standard approach is to leave "upsdriver_type" unset, so that
+# upsdrvctl is used to start and stop the driver(s).  (Note that a
+# system might have more than one UPS, and upsdrvctl should support
+# this.)  See nutupsdrv(8), but upsdrvctl will invoke the driver
+# properly.
+#
+# An alternative is to set the driver type and flags.  This only works
+# for a single driver.  This approach is deprecated and may be removed.
 #
 # upsdriver=YES
 #	upsdriver_type="newapc"
 #	upsdriver_flags="-a smartups"
-#
-# If "upsdriver_type" is unset, then use "upsdrvctl" to control the UPS
-# drivers.  Please refer to nutupsdrv(8) for more information about the
-# arguments to pass to the UPS drivers.
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 name="upsdriver"
 rcvar=$name
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
 fi
 
-if [ "${upsdriver_type:-upsdrvctl}" = "upsdrvctl" ]
-then
+if [ "${upsdriver_type:-upsdrvctl}" = "upsdrvctl" ]; then
 	ctl_command="@PREFIX@/sbin/upsdrvctl"
 	required_files="@NUT_CONFDIR@/ups.conf"
 	start_cmd="${ctl_command} start"
 	stop_cmd="${ctl_command} stop"
+	# XXX implement status after upsdrvctl does
 else
 	command="@PREFIX@/libexec/nut/${upsdriver_type}"
-	#
-	# XXX - We need a way to easily determine the pidfile, which is of
-	# XXX - the form @NUT_STATEDIR@/${updriver_type}-${tty}.pid
-	#
-	#pidfile="@NUT_STATEDIR@/${upsdriver_type}-${tty}.pid"
+	# XXX Restructure upsname into variable, but really just
+	# delete this option.
+	#upsname=`echo $upsdriver_flags | sed -e 's/^-a //'
+	#pidfile="@NUT_STATEDIR@/${upsdriver_type}-${upsname}.pid"
 fi
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	run_rc_command "$1"
 else
 	@ECHO@ -n " ${name}"
-	if [ -n "${start_cmd}" ]
-	then
+	if [ -n "${start_cmd}" ]; then
 		${start_cmd}
 	else
 		${command} ${upsdriver_flags} ${command_args}
--- sysutils/ups-nut/files/upsmon.sh
+++ sysutils/ups-nut/files/upsmon.sh
@@ -1,12 +1,12 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: upsmon.sh,v 1.7 2006/08/15 12:07:15 ghen Exp $
+# $NetBSD: upsmon.sh,v 1.7.134.1 2023/01/07 16:07:59 gdt Exp $
 #
 # PROVIDE: upsmon
+# BEFORE: SERVERS
 # KEYWORD: shutdown
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
@@ -16,8 +16,7 @@ command="@PREFIX@/sbin/${name}"
 pidfile="@NUT_STATEDIR@/${name}.pid"
 required_files="@NUT_CONFDIR@/${name}.conf"
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
