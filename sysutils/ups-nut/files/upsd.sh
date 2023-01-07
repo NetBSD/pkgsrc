a57015426f28f7558fbd2f9962cf566fd026c174
gdt <gdt@pkgsrc.org>
2023-01-07 16:16:10 +0000

    sysutils/ups-nut: Rototill rc.d scripts
      - Fix strange sh style: don't have then on a new line
      - Ensure main daemons started before SERVERS
      - Slot upsmon after SERVERS and before LOGIN
      - Rewrite upsdriver instructions to make upsdrvctl the standard approach
      - Drop compat for ancient (<1.6) NetBSD

--- sysutils/ups-nut/files/upsd.sh
+++ sysutils/ups-nut/files/upsd.sh
@@ -1,14 +1,13 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: upsd.sh,v 1.7 2006/08/15 12:07:15 ghen Exp $
+# $NetBSD: upsd.sh,v 1.8 2023/01/07 16:16:10 gdt Exp $
 #
 # PROVIDE: upsd
 # REQUIRE: upsdriver
 # BEFORE: upsmon
 # KEYWORD: shutdown
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
@@ -18,8 +17,7 @@ command="@PREFIX@/sbin/${name}"
 required_files="@NUT_CONFDIR@/${name}.conf"
 pidfile="@NUT_STATEDIR@/${name}.pid"
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
