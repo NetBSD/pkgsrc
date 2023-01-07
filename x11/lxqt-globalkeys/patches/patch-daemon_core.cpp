e7d9d6e7ef4418badc5fa0c07e7c7430ed337f05
gutteridge <gutteridge@pkgsrc.org>
2023-01-07 01:26:14 +0000

    lxqt-globalkeys: adjust previous LOG_MAKEPRI patch
    This is also actually relevant to NetBSD, which defines it incorrectly
    (PR lib/57172).

--- x11/lxqt-globalkeys/patches/patch-daemon_core.cpp
+++ x11/lxqt-globalkeys/patches/patch-daemon_core.cpp
@@ -1,14 +1,20 @@
-$NetBSD: patch-daemon_core.cpp,v 1.3 2023/01/06 02:35:47 gutteridge Exp $
+$NetBSD: patch-daemon_core.cpp,v 1.4 2023/01/07 01:26:14 gutteridge Exp $
 
-Fix SunOS builds.
+Fix SunOS and OpenBSD builds, and work around a bug in NetBSD with the same
+macro (PR lib/57172).
 
 --- daemon/core.cpp.orig	2022-11-05 08:17:16.000000000 +0000
 +++ daemon/core.cpp
-@@ -59,6 +59,10 @@
+@@ -59,6 +59,15 @@
  
  #include "core.h"
- 
-+#ifndef LOG_MAKEPRI
+
++/* 
++ * SunOS and modern OpenBSD don't define this, and NetBSD's version has an
++ * incorrect shift applied (log facilities are already defined with "<<3", it
++ * shouldn't be done a second time).
++ */
++#if !defined(LOG_MAKEPRI) || defined(__NetBSD__)
 +#define LOG_MAKEPRI(fac, pri)    ((fac) | (pri))
 +#endif
 +
