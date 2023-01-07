73984aabdb3c0517cd350ee631242818a4dcd4e7
triaxx <triaxx@pkgsrc.org>
2023-01-07 21:20:19 +0000

    jack: Update to 1.9.21
    upstream changes:
    -----------------
    1.9.21 Latest
    =============
      o Add shell mode to jack_control (executes commands from stdin until EOF)
      o The waf autooption --example-tools introduced in 1.9.20 is now off by
        default,
      o To get the previous behavior back pass --example-tools during build.
      o This flag (and the related tools and their code) are going to be removed in
        the next release.
      o Fix alignas() on non-packed architectures
      o Fix build of jack-example-tools man pages (1.9.20 regression)
      o Fix compatibility with macOS 12
      o Fix missing symbols from jack client library (error and info callback
        pointers)
      o Fix potential memory corruption in midi_latency_test tool
      o Fix JackWeakAPI on Windows
      o Use predefined variables in pkg-config file (required for mingw)
    External changes, related to macOS/Windows installer:
      o Fix application style in QJackCtl (qwindowsvistastyle.dll was missing)
      o Update QjackCtl used in macOS/Windows installers to 0.9.7
    
    1.9.20
    ======
      o Add waf autooption --example-tools to allow optional build of executables,
        libraries and man pages provided by jack-example-tools (the files are built
        by default).
      o Building and installing the additional files can be disabled by using
        --example-tools=no or --no-example-tools.
      o Fix 32-bit support in ALSA driver
      o Fix incomplete ASIO support on Windows
      o Fix metadata usage with multiple users
      o Fix netsource tool missing on Windows
      o Fix semaphore usage on macOS
      o Official FreeBSD support
    External changes, related to macOS/Windows installer:
      o Update Qt5 used in macOS/Windows installers to 5.12.12
      o Update QjackCtl used in macOS/Windows installers to 0.9.6

--- audio/jack/patches/patch-common_JackAudioAdapterFactory.cpp
+++ audio/jack/patches/patch-common_JackAudioAdapterFactory.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-common_JackAudioAdapterFactory.cpp,v 1.1 2021/09/08 19:19:20 nia Exp $
+$NetBSD: patch-common_JackAudioAdapterFactory.cpp,v 1.2 2023/01/07 21:20:19 triaxx Exp $
 
 Add NetBSD support.
 
---- common/JackAudioAdapterFactory.cpp.orig	2021-07-15 06:18:26.000000000 +0000
+--- common/JackAudioAdapterFactory.cpp.orig	2022-04-15 19:14:06.000000000 +0000
 +++ common/JackAudioAdapterFactory.cpp
 @@ -35,6 +35,11 @@ Foundation, Inc., 675 Mass Ave, Cambridg
  #define JackPlatformAdapter JackAlsaAdapter
@@ -13,6 +13,6 @@ Add NetBSD support.
 +#define JackPlatformAdapter JackSunAdapter
 +#endif
 +
- #if defined(__sun__) || defined(sun)
+ #if defined(__sun__) || defined(sun) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
  #include "JackOSSAdapter.h"
  #define JackPlatformAdapter JackOSSAdapter
--- audio/jack/patches/patch-posix_JackPosixSemaphore.cpp
+++ audio/jack/patches/patch-posix_JackPosixSemaphore.cpp
@@ -1,24 +1,16 @@
-$NetBSD: patch-posix_JackPosixSemaphore.cpp,v 1.1 2021/09/08 19:19:20 nia Exp $
+$NetBSD: patch-posix_JackPosixSemaphore.cpp,v 1.2 2023/01/07 21:20:19 triaxx Exp $
 
 NetBSD requires POSIX semaphores to be prefixed with
 a slash, see sem_open(3).
 
---- posix/JackPosixSemaphore.cpp.orig	2021-07-15 06:18:26.000000000 +0000
+--- posix/JackPosixSemaphore.cpp.orig	2022-04-15 19:14:06.000000000 +0000
 +++ posix/JackPosixSemaphore.cpp
-@@ -46,13 +46,13 @@ void JackPosixSemaphore::BuildName(const
- {
-     char ext_client_name[SYNC_MAX_NAME_SIZE + 1];
-     JackTools::RewriteName(client_name, ext_client_name);
--#if __APPLE__  // POSIX semaphore names are limited to 32 characters... 
-+#if __APPLE__  // POSIX semaphore names are limited to 32 characters...
-     snprintf(res, 32, "js_%s", ext_client_name); 
- #else
-     if (fPromiscuous) {
--        snprintf(res, size, "jack_sem.%s_%s", server_name, ext_client_name);
-+        snprintf(res, size, "/jack_sem.%s_%s", server_name, ext_client_name);
-     } else {
--        snprintf(res, size, "jack_sem.%d_%s_%s", JackTools::GetUID(), server_name, ext_client_name);
-+        snprintf(res, size, "/jack_sem.%d_%s_%s", JackTools::GetUID(), server_name, ext_client_name);
-     }
+@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suit
+ #include "promiscuous.h"
  #endif
- }
+ 
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
+ #define JACK_SEM_PREFIX "/jack_sem"
+ #define SEM_DEFAULT_O 0
+ #else
