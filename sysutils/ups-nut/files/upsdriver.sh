0cf6d67b90eef5867f7d6915f8eb0e1b7fde20ea
gdt <gdt@pkgsrc.org>
2023-01-08 01:32:54 +0000

    sysutils/ups-nut: Simplify upsdriver
    Drop option to specify driver and args manually.  upsdrvctl is now the
    only approach.
    
    (Raised to 3 usual suspects who have helped with nut, with 2
    supporting the simplification.)

--- sysutils/ups-nut/files/upsdriver.sh
+++ sysutils/ups-nut/files/upsdriver.sh
@@ -1,23 +1,15 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: upsdriver.sh,v 1.11 2023/01/08 01:14:23 gdt Exp $
+# $NetBSD: upsdriver.sh,v 1.12 2023/01/08 01:32:55 gdt Exp $
 #
 # PROVIDE: upsdriver
 # REQUIRE: NETWORK syslogd mountcritremote
 # KEYWORD: shutdown
 #
-# The standard approach is to leave "upsdriver_type" unset, so that
-# upsdrvctl is used to start and stop the driver(s).  (Note that a
-# system might have more than one UPS, and upsdrvctl should support
-# this.)  See nutupsdrv(8), but upsdrvctl will invoke the driver
-# properly.
-#
-# An alternative is to set the driver type and flags.  This only works
-# for a single driver.  This approach is deprecated and may be removed.
-#
-# upsdriver=YES
-#	upsdriver_type="newapc"
-#	upsdriver_flags="-a smartups"
+# This script calls upsdrvctl to start and stop the driver(s).  (Note
+# that a system might have more than one UPS, and upsdrvctl should
+# support this.)  See nutupsdrv(8), but upsdrvctl will invoke the
+# driver properly.
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
@@ -30,19 +22,11 @@ if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
 fi
 
-if [ "${upsdriver_type:-upsdrvctl}" = "upsdrvctl" ]; then
-	ctl_command="@PREFIX@/sbin/upsdrvctl"
-	required_files="@NUT_CONFDIR@/ups.conf"
-	start_cmd="${ctl_command} start"
-	stop_cmd="${ctl_command} stop"
-	# XXX implement status after upsdrvctl does
-else
-	command="@PREFIX@/libexec/nut/${upsdriver_type}"
-	# XXX Restructure upsname into variable, but really just
-	# delete this option.
-	#upsname=`echo $upsdriver_flags | sed -e 's/^-a //'
-	#pidfile="@NUT_STATEDIR@/${upsdriver_type}-${upsname}.pid"
-fi
+ctl_command="@PREFIX@/sbin/upsdrvctl"
+required_files="@NUT_CONFDIR@/ups.conf"
+start_cmd="${ctl_command} start"
+stop_cmd="${ctl_command} stop"
+# XXX implement status after upsdrvctl does
 
 if [ -f /etc/rc.subr ]; then
 	run_rc_command "$1"
