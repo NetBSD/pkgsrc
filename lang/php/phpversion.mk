49c4fe8d7f2f654e928c00ae0b3592d478cff938
bsiegert <bsiegert@pkgsrc.org>
2023-01-08 19:18:28 +0000

    Pullup ticket #6719 - requested by taca lang/php80: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.386
    - lang/php80/distinfo                                           1.28
    - lang/php80/patches/patch-sapi_fpm_fpm_events_port.c           deleted
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  7 07:43:34 UTC 2023
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php80: distinfo
       Removed Files:
       	pkgsrc/lang/php80/patches: patch-sapi_fpm_fpm_events_port.c
    
       Log Message:
       lang/php80: update to 8.0.27
    
       (2023-01-05)
    
       - PDO/SQLite:
         . Fixed bug #81740 (PDO::quote() may return unquoted string). (CVE-2022-31631)
           (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.383.2.2 2023/01/08 17:57:21 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.383.2.3 2023/01/08 19:18:28 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
-PHP80_VERSION=	8.0.26
+PHP80_VERSION=	8.0.27
 PHP81_VERSION=	8.1.14
 PHP82_VERSION=	8.2.1
 
