ffb74bb0e9610a486006c2e2cd8c64f990ffb1a3
sekiya <sekiya@pkgsrc.org>
2023-01-09 04:29:12 +0000

    Document known issue with pkglint versus a patchfile versus CVS.

--- net/nagios-base/patches/patch-base_logging.c
+++ net/nagios-base/patches/patch-base_logging.c
@@ -2,6 +2,9 @@ $NetBSD$
 
 64-bit time_t workaround
 
+Note: this file will fail the pkglint CVS ID check.  Fixing it in the repo will
+cause other issues, so we just leave it as-is.  [sekiya@ 2022-01-09]
+
 --- base/logging.c.orig	2022-11-17 05:52:51.000000000 +0900
 +++ base/logging.c	2023-01-09 06:59:17.162183952 +0900
 @@ -533,7 +533,7 @@
