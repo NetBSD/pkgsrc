0503a3dc272bbca3af2115b465832d5f61cc5a07
sekiya <sekiya@pkgsrc.org>
2023-01-09 00:28:41 +0000

    Update nagios-plugins to 2.3.3

--- net/nagios-plugins/patches/patch-plugins_check_nagios.c
+++ net/nagios-plugins/patches/patch-plugins_check_nagios.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-plugins_check_nagios.c,v 1.2 2022/04/02 10:35:35 nia Exp $
+$NetBSD: patch-plugins_check_nagios.c,v 1.3 2023/01/09 00:28:41 sekiya Exp $
 
 Crash fix: initialize variable, check bounds on string copy.
 
---- plugins/check_nagios.c.orig	2017-01-19 16:01:31.000000000 +0000
-+++ plugins/check_nagios.c
-@@ -72,7 +72,7 @@ main (int argc, char **argv)
+--- plugins/check_nagios.c.orig	2019-12-05 06:53:08.000000000 +0900
++++ plugins/check_nagios.c	2023-01-09 08:53:37.895632188 +0900
+@@ -73,7 +73,7 @@
  #ifdef PS_USES_PROCETIME
  	char procetime[MAX_INPUT_BUFFER];
  #endif /* PS_USES_PROCETIME */
@@ -13,7 +13,7 @@ Crash fix: initialize variable, check bounds on string copy.
  	char *procargs;
  	int pos, cols;
  	int expected_cols = PS_COLS - 1;
-@@ -144,7 +144,8 @@ main (int argc, char **argv)
+@@ -145,7 +145,8 @@
  			/* Some ps return full pathname for command. This removes path */
  			temp_string = strtok ((char *)procprog, "/");
  			while (temp_string) {
--- net/nagios-plugins/patches/patch-plugins_check_radius.c
+++ net/nagios-plugins/patches/patch-plugins_check_radius.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-plugins_check__radius.c,v 1.1 2017/07/04 19:45:18 jperkin Exp $
+$NetBSD: patch-plugins_check_radius.c,v 1.1 2023/01/09 00:28:41 sekiya Exp $
 
 Support radius config.
 
---- plugins/check_radius.c.orig	2017-01-16 17:24:03.000000000 +0000
-+++ plugins/check_radius.c
-@@ -86,7 +86,7 @@ char *password = NULL;
- char *nasid = NULL;
+--- plugins/check_radius.c.orig	2019-12-05 06:53:08.000000000 +0900
++++ plugins/check_radius.c	2023-01-09 08:53:32.981542458 +0900
+@@ -99,7 +99,7 @@
  char *nasipaddress = NULL;
+ char *castid = NULL;
  char *expect = NULL;
 -char *config_file = NULL;
 +char *config_file = "@SYSCONFDIR@/radiusclient-ng/radiusclient.conf";
