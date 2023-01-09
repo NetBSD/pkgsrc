fa890bd702fb64892c405fdf28107bb7ff729adb
sekiya <sekiya@pkgsrc.org>
2023-01-09 23:29:24 +0000

    Minor fixes to appease pkglint.

--- print/a2ps/patches/patch-lib_path-concat.c
+++ print/a2ps/patches/patch-lib_path-concat.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-lib_path-concat.c,v 1.1 2012/03/23 18:44:23 marino Exp $
+$NetBSD: patch-lib_path-concat.c,v 1.2 2023/01/09 23:29:25 sekiya Exp $
+
+Portability fix.
 
 --- lib/path-concat.c.orig	2012-03-23 17:50:24.184967000 +0000
 +++ lib/path-concat.c
