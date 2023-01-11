5367e73c83bffc638d994943cfa43dae0525aa6d
sekiya <sekiya@pkgsrc.org>
2023-01-11 09:27:27 +0000

    Linux-flavored includes should not be the default.

--- net/nagios-plugins/patches/patch-plugins_check_radius.c
+++ net/nagios-plugins/patches/patch-plugins_check_radius.c
@@ -1,9 +1,28 @@
-$NetBSD: patch-plugins_check_radius.c,v 1.1 2023/01/09 00:28:41 sekiya Exp $
+$NetBSD: patch-plugins_check_radius.c,v 1.2 2023/01/11 09:27:28 sekiya Exp $
+
+Do not assume that we are compiling under Linux.
 
 Support radius config.
 
---- plugins/check_radius.c.orig	2019-12-05 06:53:08.000000000 +0900
-+++ plugins/check_radius.c	2023-01-09 08:53:32.981542458 +0900
+--- plugins/check_radius.c.orig	2023-01-11 18:07:22.189005504 +0900
++++ plugins/check_radius.c	2023-01-11 18:07:11.857369955 +0900
+@@ -38,13 +38,13 @@
+ 
+ #include <ifaddrs.h>
+ 
+-#ifdef __FreeBSD__
+-#include <net/if_dl.h>
+-#else
++#if defined(__linux__)
+ #include <linux/if_packet.h>
+ #define AF_LINK AF_PACKET
+ #define LLADDR(l) ((l)->sll_addr)
+ #define sockaddr_dl sockaddr_ll
++#else
++#include <net/if_dl.h>
+ #endif
+ 
+ #if defined(HAVE_LIBFREERADIUS_CLIENT)
 @@ -99,7 +99,7 @@
  char *nasipaddress = NULL;
  char *castid = NULL;
