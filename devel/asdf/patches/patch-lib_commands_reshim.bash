7c45e2e740722d80835b442ec6948c92e498131d
schmonz <schmonz@pkgsrc.org>
2023-01-13 22:18:11 +0000

    Update to 0.11.1. From the changelog:
    # Bug Fixes
    
    * `reshim` did not rewrite executable path (#1311)
    * Add test for nushell integration and fix some bugs (#1415)
    * Allow `path:` versions to use `~` (#1403)
    * Ban use of 'test' (#1383)
    * correct order of checks in conditional for adding a missing newline
      (#1418, closes #1417)
    * Do not use `pwd`
    * Do not use type not exported on older Python versions (#1409)
    * force lwrcase plugin name fix capitalization mismatch errs (#1400)
    * lint errors from `scripts/checkstyle.py` (#1385)
    * mv dev dep from repo root to subdir to avoid clash (#1408)
    * Remove unecessary backslashes (#1384)
    * Remove usage of `$(pwd)` in favor of `$PWD`

--- devel/asdf/patches/patch-lib_commands_reshim.bash
+++ devel/asdf/patches/patch-lib_commands_reshim.bash
@@ -1,15 +1,18 @@
-$NetBSD: patch-lib_commands_reshim.bash,v 1.1 2022/12/27 16:11:01 schmonz Exp $
+$NetBSD: patch-lib_commands_reshim.bash,v 1.2 2023/01/13 22:18:11 schmonz Exp $
 
 Use pkgsrc paths.
 
---- lib/commands/reshim.bash.orig	2022-12-14 20:53:20.000000000 +0000
+--- lib/commands/reshim.bash.orig	2023-01-13 14:45:18.000000000 +0000
 +++ lib/commands/reshim.bash
-@@ -97,7 +97,7 @@ write_shim_script() {
-     cat <<EOF >"$shim_path"
- #!/usr/bin/env bash
- # asdf-plugin: ${plugin_name} ${version}
+@@ -102,9 +102,9 @@ EOF
+   fi
+ 
+   cat <<EOF >"$shim_path"
+-#!/usr/bin/env bash
++#!@BASH@
+ $(sort -u <"$temp_versions_path")
 -exec $(asdf_dir)/bin/asdf exec "${executable_name}" "\$@" # asdf_allow: ' asdf '
 +exec @PREFIX@/bin/asdf exec "${executable_name}" "\$@" # asdf_allow: ' asdf '
  EOF
-   fi
  
+   rm "$temp_versions_path"
