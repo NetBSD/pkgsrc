e76705606bd8893ba5562f3371357c944ffb9113
jperkin <jperkin@pkgsrc.org>
2023-01-13 11:50:37 +0000

    bareos: Fix build on SunOS.
    Remove ONLY_FOR_PLATFORM.  We don't deliberately break platforms just because
    they haven't been tested.  Fix PKG_SHELL to use ${SH}, there is no guarantee
    that /bin/sh is the right shell on all platforms.
    
    There are some other cleanups necessary in this package (e.g. bsd.pkg.mk is
    included way too early), but they can wait for the 22.0.1 update.

--- sysutils/bareos/patches/patch-core_cmake_BareosFindAllLibraries.cmake
+++ sysutils/bareos/patches/patch-core_cmake_BareosFindAllLibraries.cmake
@@ -1,11 +1,23 @@
-$NetBSD: patch-core_cmake_BareosFindAllLibraries.cmake,v 1.2 2021/02/01 09:08:43 kardel Exp $
+$NetBSD: patch-core_cmake_BareosFindAllLibraries.cmake,v 1.3 2023/01/13 11:50:37 jperkin Exp $
 
-	cmake build gets confused when both versions of python 2 and 3 are
-	availabe. as python 2.7 is eol prefer python 3 in this case.
+Prefer python3, remove broken SunOS section.
 
---- core/cmake/BareosFindAllLibraries.cmake.orig	2020-12-16 07:46:16.000000000 +0000
+--- core/cmake/BareosFindAllLibraries.cmake.orig	2021-06-10 15:37:18.000000000 +0000
 +++ core/cmake/BareosFindAllLibraries.cmake
-@@ -44,7 +44,7 @@ else()
+@@ -30,12 +30,6 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Windows
+   set(Python3_FOUND 1)
+   set(Python3_EXT_SUFFIX ".pyd")
+ 
+-  # Python Plugins currently cannot be built for Solaris
+-elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
+-  set(HAVE_PYTHON 0)
+-  set(Python2_FOUND 0)
+-  set(Python3_FOUND 0)
+-
+ else()
+   find_package(Python2 COMPONENTS Interpreter Development)
+   find_package(Python3 COMPONENTS Interpreter Development)
+@@ -54,7 +48,7 @@ else()
      set(HAVE_PYTHON 1)
    endif()
  
@@ -14,7 +26,7 @@ $NetBSD: patch-core_cmake_BareosFindAllLibraries.cmake,v 1.2 2021/02/01 09:08:43
      set(PYTHON_EXECUTABLE
          ${Python2_EXECUTABLE}
          PARENT_SCOPE
-@@ -62,6 +62,7 @@ else()
+@@ -72,6 +66,7 @@ else()
    endif()
  
    if(${Python3_FOUND})
