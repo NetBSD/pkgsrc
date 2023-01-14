eeda09dfe92b508ee642da5cf8208c43f2ae138a
wiz <wiz@pkgsrc.org>
2023-01-14 17:23:26 +0000

    python311: remove superfluous chunk

--- lang/python311/patches/patch-Modules_socketmodule.c
+++ lang/python311/patches/patch-Modules_socketmodule.c
@@ -1,18 +1,7 @@
-$NetBSD: patch-Modules_socketmodule.c,v 1.1 2022/10/31 09:50:40 adam Exp $
-
-Support NetBSD's socketcan implementation
+$NetBSD: patch-Modules_socketmodule.c,v 1.2 2023/01/14 17:23:26 wiz Exp $
 
 --- Modules/socketmodule.c.orig	2022-01-13 18:52:14.000000000 +0000
 +++ Modules/socketmodule.c
-@@ -2112,7 +2112,7 @@ getsockaddrarg(PySocketSockObject *s, Py
-             PyObject *interfaceName;
-             struct ifreq ifr;
-             Py_ssize_t len;
--            struct sockaddr_can *addr = &addrbuf->can;
-+            struct sockaddr_can *addr = (struct sockaddr_can *)addrbuf;
- 
-             if (!PyTuple_Check(args)) {
-                 PyErr_Format(PyExc_TypeError,
 @@ -5440,7 +5440,7 @@ socket_sethostname(PyObject *self, PyObj
      Py_buffer buf;
      int res, flag = 0;
