2e016acaf71299371e8779f3b5cbbd7030c8a293
nikita <nikita@pkgsrc.org>
2023-01-15 18:32:50 +0000

    Import devel/shfmt as shfmt version 3.6.0
    Packaged in wip by leot@
    
    Shfmt is a shell parser, formatter, and interpreter.
    It suports POSIX Shell, Bash, and mksh.

---
+++ devel/shfmt/go-modules.mk
@@ -0,0 +1,27 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/01/15 18:32:50 nikita Exp $
+
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.18.mod
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.18.zip
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.9.mod
+GO_MODULE_FILES+=	github.com/frankban/quicktest/@v/v1.14.4.mod
+GO_MODULE_FILES+=	github.com/frankban/quicktest/@v/v1.14.4.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.zip
+GO_MODULE_FILES+=	github.com/google/renameio/v2/@v/v2.0.0.mod
+GO_MODULE_FILES+=	github.com/google/renameio/v2/@v/v2.0.0.zip
+GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.3.1.mod
+GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.3.1.zip
+GO_MODULE_FILES+=	github.com/kr/text/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/kr/text/@v/v0.2.0.zip
+GO_MODULE_FILES+=	github.com/pkg/diff/@v/v0.0.0-20210226163009-20ebb0f2a09e.mod
+GO_MODULE_FILES+=	github.com/pkg/diff/@v/v0.0.0-20210226163009-20ebb0f2a09e.zip
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.9.0.mod
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.9.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.1.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.1.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.3.0.zip
+GO_MODULE_FILES+=	mvdan.cc/editorconfig/@v/v0.2.0.mod
+GO_MODULE_FILES+=	mvdan.cc/editorconfig/@v/v0.2.0.zip
