8242a5373f13318a12bbe271f650cb06f97e3212
thorpej <thorpej@pkgsrc.org>
2023-01-15 15:41:46 +0000

    Update cpmtools to cpmtools-2.23.
    Changes since 2.22:
    
    o  Use 16 bit block pointers for file systems > 256 blocks, not >= 256
    o  Translate CP/M file name character '/' to ',' for the host and back
    o  dirblks in Kaypro format fixed
    o  Misc fixes for directory listing
    o  Added bootsec diskdefs option
    o  Check Device_close return value
    o  Check for too small block number when reading file data
    o  Replaced obsolete macros in configure.in
    o  Cygwin/Windows support did not build any more and likely not for quite
       some time, so it was removed.
    
    (We're coming from 2.13, so there's quite a few more since then.)

--- sysutils/cpmtools/patches/patch-cpmcp.c
+++ sysutils/cpmtools/patches/patch-cpmcp.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-ac,v 1.2 2012/10/11 18:46:11 shattered Exp $
+$NetBSD: patch-cpmcp.c,v 1.1 2023/01/15 15:41:47 thorpej Exp $
+
+Some systems require <sys/types.h> before <sys/stat.h>.
 
 --- cpmcp.c.orig	2012-10-07 10:46:25.000000000 +0000
 +++ cpmcp.c
