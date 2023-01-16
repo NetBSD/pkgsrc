f8abf295240e2214eca4838e2bd9ba9f86646dcd
dholland <dholland@pkgsrc.org>
2023-01-16 03:07:39 +0000

    misc/celestia: regen patch pkglint doesn't like. No functional change

--- misc/celestia/patches/patch-src_celengine_command.cpp
+++ misc/celestia/patches/patch-src_celengine_command.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_celengine_command.cpp,v 1.1 2013/10/27 21:21:26 dholland Exp $
+$NetBSD: patch-src_celengine_command.cpp,v 1.2 2023/01/16 03:07:39 dholland Exp $
 
 Fix build with lua switched off.
 (This is probably not the right place to switch it off, but it does
 build this way.)
 
---- src/celengine/command.cpp~	2013-10-27 20:32:16.000000000 +0000
+--- src/celengine/command.cpp.orig	2011-06-05 16:11:09.000000000 +0000
 +++ src/celengine/command.cpp
 @@ -11,7 +11,9 @@
  #include <celutil/util.h>
@@ -24,7 +24,7 @@ build this way.)
      vector<Observer*> observer_list;
      getObservers(env.getCelestiaCore(), observer_list);
  
-@@ -785,6 +786,7 @@ void CommandSplitView::process(Execution
+@@ -783,6 +786,7 @@ void CommandSplitView::process(Execution
          View::Type type = (compareIgnoringCase(splitType, "h") == 0) ? View::HorizontalSplit : View::VerticalSplit;
          env.getCelestiaCore()->splitView(type, view, (float)splitPos);
      }
@@ -40,7 +40,7 @@ build this way.)
      vector<Observer*> observer_list;
      getObservers(env.getCelestiaCore(), observer_list);
  
-@@ -807,6 +810,7 @@ void CommandDeleteView::process(Executio
+@@ -805,6 +810,7 @@ void CommandDeleteView::process(Executio
          View* view = getViewByObserver(env.getCelestiaCore(), obs);
          env.getCelestiaCore()->deleteView(view);
      }
@@ -59,7 +59,7 @@ build this way.)
  }
  
  
-@@ -834,6 +840,7 @@ CommandSetActiveView::CommandSetActiveVi
+@@ -832,6 +840,7 @@ CommandSetActiveView::CommandSetActiveVi
  
  void CommandSetActiveView::process(ExecutionEnvironment& env)
  {
@@ -67,7 +67,7 @@ build this way.)
      vector<Observer*> observer_list;
      getObservers(env.getCelestiaCore(), observer_list);
  
-@@ -843,6 +850,7 @@ void CommandSetActiveView::process(Execu
+@@ -841,6 +850,7 @@ void CommandSetActiveView::process(Execu
          View* view = getViewByObserver(env.getCelestiaCore(), obs);
          env.getCelestiaCore()->setActiveView(view);
      }
@@ -75,7 +75,7 @@ build this way.)
  }
  
  
-@@ -892,6 +900,7 @@ CommandSetLineColor::CommandSetLineColor
+@@ -890,6 +900,7 @@ CommandSetLineColor::CommandSetLineColor
  
  void CommandSetLineColor::process(ExecutionEnvironment& /* env */)
  {
@@ -83,7 +83,7 @@ build this way.)
      if (CelxLua::LineColorMap.count(item) == 0)
      {
          cerr << "Unknown line style: " << item << "\n";
-@@ -900,6 +909,7 @@ void CommandSetLineColor::process(Execut
+@@ -898,6 +909,7 @@ void CommandSetLineColor::process(Execut
      {
          *CelxLua::LineColorMap[item] = color;
      }
@@ -91,7 +91,7 @@ build this way.)
  }
  
  
-@@ -914,6 +924,7 @@ CommandSetLabelColor::CommandSetLabelCol
+@@ -912,6 +924,7 @@ CommandSetLabelColor::CommandSetLabelCol
  
  void CommandSetLabelColor::process(ExecutionEnvironment& /* env */)
  {
@@ -99,7 +99,7 @@ build this way.)
      if (CelxLua::LabelColorMap.count(item) == 0)
      {
          cerr << "Unknown label style: " << item << "\n";
-@@ -922,6 +933,7 @@ void CommandSetLabelColor::process(Execu
+@@ -920,6 +933,7 @@ void CommandSetLabelColor::process(Execu
      {
          *CelxLua::LabelColorMap[item] = color;
      }
