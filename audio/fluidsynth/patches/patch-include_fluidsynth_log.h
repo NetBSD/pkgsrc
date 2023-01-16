d50299ecb90c1d5deb48853bee1b24792631794b
adam <adam@pkgsrc.org>
2023-01-16 20:48:41 +0000

    fluidsynth: updated to 2.3.1
    fluidsynth 2.3.1
    
    Prevent MIDI player from finishing prematurely
    Fix a crash when enumerating rawmidi ALSA devices
    Restore systemd sandboxing options and make it work with user units
    Handle conflict with pipewire systemd daemon

--- audio/fluidsynth/patches/patch-include_fluidsynth_log.h
+++ audio/fluidsynth/patches/patch-include_fluidsynth_log.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-include_fluidsynth_log.h,v 1.2 2021/04/05 10:54:16 wiz Exp $
+$NetBSD: patch-include_fluidsynth_log.h,v 1.3 2023/01/16 20:48:41 adam Exp $
 
 Use the protected form to avoid conflicts with printf macros.
 
---- include/fluidsynth/log.h.orig	2021-03-27 17:35:00.000000000 +0000
+--- include/fluidsynth/log.h.orig	2022-12-28 11:43:59.000000000 +0000
 +++ include/fluidsynth/log.h
 @@ -85,7 +85,7 @@ FLUIDSYNTH_API void fluid_default_log_fu
  
@@ -12,4 +12,4 @@ Use the protected form to avoid conflicts with printf macros.
 +__attribute__ ((__format__ (__printf__, 2, 3)))
  #endif
  ;
- /* @} */
+ /** @} */
