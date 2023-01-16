a996c1d4a6b72acfe3726dcd02eeb7f43c6cffa8
nikita <nikita@pkgsrc.org>
2023-01-16 16:07:49 +0000

    age: update to version 1.1.1
    ChangeLog:
    
    age v1.1.1
      age v1.1.1 is a patch release to fix go install filippo.io/age/...@latest.
    
    age v1.1.0
      Breaking changes
        If -i is used, passphrase-encrypted files are now rejected. Previously,
        a passphrase-encrypted file was auto-detected and the identity file was
        ignored. This could lead to unexpected behavior, such as a script blocking
        for user interaction, based on potentially untrusted input files. Now,
        age -d must be invoked without -i arguments to decrypt passphrase-encrypted
        files. A helpful error is printed otherwise. This should not break any
        automated system as passphrase decryption was always interactive.
    
        Empty final chunks are now rejected. If a payload was a multiple of 64KiB
        long, there were two valid encryptions for it: with a "full" last chunk
        encrypting 64KiB, or with an additional "empty" chunk encrypting 0 bytes.
        age, rage, and all other known implementations only ever produced the
        former. (Note that age will forever decrypt files it generated.) The latter
        is now rejected. The specification has been updated (C2SP/C2SP#13) and test
        cases are included in the test suite.
      Minor changes
        PKCS#8-encoded Ed25519 private keys (such as 1Password exports) are now
        supported as SSH identities.
    
        If an armored file is pasted into the terminal, age will now attempt to
        wait until the end of the file before prompting for a password.
    
        Some invalid files are now correctly rejected, in particular encrypted files
        with trailing data. (Yay for the test suite!)
    
        If /dev/tty is present but can't be opened, age will now fallback to trying
        to treat stdin as a terminal as if /dev/tty wasn't present.
        (Thanks @brandsimon!)
    
        Input prompts now go to the terminal, even if standard error is redirected.
    
        Values of the new armor.Error type are now returned wrapped in decryption
        errors when appropriate.
    
        Windows binary releases are now signed. (Thanks @technion!)
    
        Documentation and error messages were improved.

--- security/age/go-modules.mk
+++ security/age/go-modules.mk
@@ -1,14 +1,10 @@
-# $NetBSD: go-modules.mk,v 1.3 2021/09/12 09:08:32 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.4 2023/01/16 16:07:49 nikita Exp $
 
-GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-rc.1.mod
-GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0-rc.1.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210817164053-32db794688a5.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210817164053-32db794688a5.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210903071746-97244b99971b.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210903071746-97244b99971b.zip
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20210615171337-6886f2dfbf5b.mod
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20210615171337-6886f2dfbf5b.zip
-GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.mod
-GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.zip
+GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0.mod
+GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.4.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.4.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.3.0.zip
