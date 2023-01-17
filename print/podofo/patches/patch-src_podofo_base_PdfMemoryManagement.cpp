75b0fd93193ca3d1a8e30ea9d530973a70aacd53
spz <spz@pkgsrc.org>
2023-01-17 14:01:57 +0000

    Update print/podofo to version 0.9.8 Fixes many CVE.
    Note upstream pushed their src/ contents to src/podofo so patches
    had to move that didn't otherwise change.
    
    upstream changelog:
    
      PoDoFo 0.9.8 released - last release on current code base
    
      May 3rd 2022
    
      The PoDoFo developers are happy to announce the release of PoDoFo 0.9.8.
      This release contains over 25 patches submitted by various contributors
      (see SVN Log for details). We encourage all users to upgrade to this
      release.
    
      Also, this will be the final release of PoDoFo based on the current
      codebase.
      After the release we plan to introduce two major changes to PoDoFo
      development.
    
      First of all, we will lock/close the current SVN trunk and switch PoDoFo
      development to a more modern development platform, where we can leverage
      state of the art development features such as Continuous Integration or
      Pull Requests. The mailing list and webpage will stay on SourceForge
      as well as the issue tracker. Still, we will open a new issue tracker
      for the new development environment and gradually migrate open issues.
      We will share more news on this, once the new development environment
      was set up.
    
      Secondly and most importantly, we will replace the current codebase
      of PoDoFo with the amazing work Francesco Pretto has done with pdfmm.
      pdfmm is based on PoDoFo but with an improved and reworked API based
      on C++17 which we consider more suitable for future development of PoDoFo.
      After rebasing PoDoFo on pdfmm, we plan to release PoDoFo 1.0.0.
    
      Please note, PoDoFo 1.0.0 will be API incompatible (binary and in
      source code) with PoDoFo 0.9.8. We expect migration steps to be necessary.
      PoDoFo Tools are currently being ported to pdfmm as a showcase for
      the migration.
    
      PoDoFo 0.9.7 released
    
      January 9th 2021
    
      PoDoFo 0.9.7 was released today, after over 2 year of development
      and with the help of many new contributors.
    
      As there are so many improvements, patches and fixes that made it
      into this release, we are not able to list them, so please stick with
      the svn log.
    
      This release also includes a release of podofobrowser which was
      ported to Qt5.

--- print/podofo/patches/patch-src_podofo_base_PdfMemoryManagement.cpp
+++ print/podofo/patches/patch-src_podofo_base_PdfMemoryManagement.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_base_PdfMemoryManagement.cpp,v 1.2 2017/02/11 22:27:00 adam Exp $
+$NetBSD: patch-src_podofo_base_PdfMemoryManagement.cpp,v 1.1 2023/01/17 14:01:57 spz Exp $
 
 Make sure SIZE_MAX is defined.
 
---- src/base/PdfMemoryManagement.cpp.orig	2017-01-29 12:26:55.000000000 +0000
-+++ src/base/PdfMemoryManagement.cpp
+--- src/podofo/base/PdfMemoryManagement.cpp.orig	2017-01-29 12:26:55.000000000 +0000
++++ src/podofo/base/PdfMemoryManagement.cpp
 @@ -31,6 +31,9 @@
   *   files in the program, then also delete it here.                       *
   ***************************************************************************/
