56a0fa59a3eb0d6d7eefb34aed13074b7ac9642f
gutteridge <gutteridge@pkgsrc.org>
2023-01-17 01:33:17 +0000

    scapy: update to 2.5.0
    Changelog
    
    Scapy v2.5.0 is the last version to support Python 2.7
    Main Changes
    
        Type hinting of Scapy core: Scapy now provides type hintings for all of its core
        Python 3.9 and 3.10 support
        macOS 10.15 support
        update built-in dependencies (six) + and our cryptography imports that created warnings
        fix sniffing performance issues with 2.4.4+ on Windows
        greatly improve BPF (macOS) support (timestamps...)
        enhanced loopback interface support on Linux, *BSD, and Windows
        SPDX License identifiers added
        several major CLI improvements, especially in autocompletion: you can now auto-complete the names for all Scapy fields, automatons, answering machines thanks to signature injection (and patches in IPython/bpython)
    
    Core
    
        improved support of BPF
        support pcapng writing, comments, TLS secrets decryption block
        Re-work how sent_time is shared across packets iterators
        support new LINUX_SLL2 packet type (new tcpdump versions)
        pipes: performances issues fixed
        tools: fixes to hexdiff, lhex...
        [breaking] FlagsField in dict mode now uses values instead of offsets
    
    Layers
    
        new layers related to Windows: DCERPC/NTLM/KERBEROS/GSSAPI/SPNEGO/(C)LDAP.
        new contrib layers: ESMC/RTPS/RTPC/metawatch
        rework Netbios/SMB1/SMB2, basic SMB clients & server, ntlm relay
        several fixes to the TLS implementation
        major zigbee/6lowpan improvements
        bug fixes in TLS: properly support FFDH, fix the TLS 1.3 notebooks... among other things
        p0f module update
        IPsec: fixes, x25519 support...
        various updates to the ASN.1 engine
        IKEv2: fixes, UDP encapsulation
        STUN support
        Postgres line protocol 3.0 support
        EDNS0 client subnet support
        ESMC protocol added
        support TCP-MD5 and TCP-AO options
        ERF Ethernet Support
        many fixes: modbus, 802.11, BTLE, SCTP, DNS, LLDP, Kerberos, RTPS, DHCP, MQTT, BGP, L2TP...
    
    Automotive
    
        ISOTPSoftSocket: Bug fixes and performance improvements
        Documentation and API-Doc improvements
        Unit-Test speedups for Scanners
        IPv6 support for DoIP
        Bugfixes for DoIP
        Unit-Test cleanups
        UDS-, GMLAN- and OBD-Scanner refactoring
        CANFD support
    
    Misc
    
        new sanity rules prevent fields from having the same name in all packets
            Currently displays a warning but will become a SyntaxError in the future!
        archives of the Scapy repo should now have more consistent hashes

--- net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
+++ net/scapy/patches/patch-scapy_arch_bpf_supersocket.py
@@ -1,83 +1,32 @@
-$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.5 2021/08/18 01:44:21 gutteridge Exp $
+$NetBSD: patch-scapy_arch_bpf_supersocket.py,v 1.6 2023/01/17 01:33:17 gutteridge Exp $
 
 Add DragonFly support.
 
-Fix NetBSD 32-bit architecture alignment.
-
-Fix send() on loopback and utun devices for Darwin and FreeBSD.
-https://github.com/secdev/scapy/pull/3257
-
---- scapy/arch/bpf/supersocket.py.orig	2021-04-18 18:36:15.000000000 +0000
+--- scapy/arch/bpf/supersocket.py.orig	2022-12-24 14:06:26.000000000 +0000
 +++ scapy/arch/bpf/supersocket.py
-@@ -18,7 +18,7 @@ from scapy.arch.bpf.consts import BIOCGB
-     BIOCIMMEDIATE, BIOCPROMISC, BIOCSBLEN, BIOCSETIF, BIOCSHDRCMPLT, \
-     BPF_BUFFER_LENGTH, BIOCSDLT, DLT_IEEE802_11_RADIO
+@@ -33,7 +33,7 @@ from scapy.arch.bpf.consts import (
+     BPF_T_NANOTIME,
+ )
  from scapy.config import conf
--from scapy.consts import FREEBSD, NETBSD, DARWIN
-+from scapy.consts import FREEBSD, NETBSD, DARWIN, DRAGONFLY
- from scapy.data import ETH_P_ALL
+-from scapy.consts import DARWIN, FREEBSD, NETBSD
++from scapy.consts import DARWIN, FREEBSD, NETBSD, DRAGONFLY
+ from scapy.data import ETH_P_ALL, DLT_IEEE802_11_RADIO
  from scapy.error import Scapy_Exception, warning
  from scapy.interfaces import network_name
-@@ -27,10 +27,10 @@ from scapy.compat import raw
- from scapy.layers.l2 import Loopback
- 
- 
--if FREEBSD:
-+if FREEBSD or NETBSD:
+@@ -45,6 +45,6 @@ from scapy.compat import raw
+-if FREEBSD or NETBSD:
++if FREEBSD or NETBSD or DRAGONFLY:
      # On 32bit architectures long might be 32bit.
-     BPF_ALIGNMENT = sizeof(c_long)
--elif NETBSD:
-+elif DRAGONFLY:
-     BPF_ALIGNMENT = 8  # sizeof(long)
+     BPF_ALIGNMENT = ctypes.sizeof(ctypes.c_long)
  else:
-     BPF_ALIGNMENT = 4  # sizeof(int32_t)
-@@ -295,6 +295,9 @@ class L2bpfListenSocket(_L2bpfSocket):
-                 bh_tstamp_offset = 16
-         elif NETBSD:
-             # struct bpf_hdr or struct bpf_hdr32
-+            bh_tstamp_offset = sizeof(c_long) * 2
-+        elif DRAGONFLY:
-+            # struct bpf_hdr
-             bh_tstamp_offset = 16
-         else:
-             # struct bpf_hdr
-@@ -390,14 +393,31 @@ class L3bpfSocket(L2bpfSocket):
-             self.assigned_interface = iff
- 
-         # Build the frame
--        if self.guessed_cls == Loopback:
--            # bpf(4) man page (from macOS, but also for BSD):
--            # "A packet can be sent out on the network by writing to a bpf
--            # file descriptor. [...] Currently only writes to Ethernets and
--            # SLIP links are supported"
--            #
--            # Headers are only mentioned for reads, not writes. tuntaposx's tun
--            # device reports as a "loopback" device, but it does IP.
-+        #
-+        # LINKTYPE_NULL / DLT_NULL (Loopback) is a special case. From the
-+        # bpf(4) man page (from macOS/Darwin, but also for BSD):
-+        #
-+        # "A packet can be sent out on the network by writing to a bpf file
-+        # descriptor. [...] Currently only writes to Ethernets and SLIP links
-+        # are supported."
-+        #
-+        # Headers are only mentioned for reads, not writes, and it has the
-+        # name "NULL" and id=0.
-+        #
-+        # The _correct_ behaviour appears to be that one should add a BSD
-+        # Loopback header to every sent packet. This is needed by FreeBSD's
-+        # if_lo, and Darwin's if_lo & if_utun.
-+        #
-+        # tuntaposx appears to have interpreted "NULL" as "no headers".
-+        # Thankfully its interfaces have a different name (tunX) to Darwin's
-+        # if_utun interfaces (utunX).
-+        #
-+        # There might be other drivers which make the same mistake as
-+        # tuntaposx, but these are typically provided with VPN software, and
-+        # Apple are breaking these kexts in a future version of macOS... so
-+        # the problem will eventually go away. They already don't work on Macs
-+        # with Apple Silicon (M1).
-+        if DARWIN and iff.startswith('tun') and self.guessed_cls == Loopback:
-             frame = raw(pkt)
-         else:
-             frame = raw(self.guessed_cls() / pkt)
+     # DARWIN, OPENBSD
+     BPF_ALIGNMENT = ctypes.sizeof(ctypes.c_int32)
+@@ -56,7 +56,7 @@ if _NANOTIME:
+         # actually a bpf_timespec
+         _fields_ = [("tv_sec", ctypes.c_ulong),
+                     ("tv_nsec", ctypes.c_ulong)]
+-elif NETBSD:
++elif NETBSD or DRAGONFLY:
+     class bpf_timeval(ctypes.Structure):
+         _fields_ = [("tv_sec", ctypes.c_ulong),
+                     ("tv_usec", ctypes.c_ulong)]
