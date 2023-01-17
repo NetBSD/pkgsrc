490a49b350b7aa34f449ff81380a8ebe6f73e02f
vins <vins@pkgsrc.org>
2023-01-17 13:52:54 +0000

    x11/libXaw3dXft: import package from wip.
    Packaged by @triaxx.
    
    libXaw3dXft is an extended version of libXaw3d, developed as part of
    xpaint with support for UTF-8 input and UTF-8 encoding of text, and
    rendering text with the FreeType library and TrueType fonts.

---
+++ x11/libXaw3dXft/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/17 13:52:54 vins Exp $
+
+BUILDLINK_TREE+=	libXaw3dXft
+
+.if !defined(LIBXAW3DXFT_BUILDLINK3_MK)
+LIBXAW3DXFT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libXaw3dXft+=	libXaw3dXft>=1.6.2h
+BUILDLINK_PKGSRCDIR.libXaw3dXft?=	../../x11/libXaw3dXft
+
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXft/buildlink3.mk"
+.include "../../x11/libXmu/buildlink3.mk"
+.include "../../x11/libXpm/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.endif	# LIBXAW3DXFT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libXaw3dXft
