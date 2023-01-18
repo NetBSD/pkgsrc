7968113f9f311a824c33fd5fde92f1ee4fe2bcde
wiz <wiz@pkgsrc.org>
2023-01-18 16:32:26 +0000

    tree-sitter: support grammars where sources are not in ${WRKSRC}/src

--- textproc/tree-sitter/grammar.mk
+++ textproc/tree-sitter/grammar.mk
@@ -1,4 +1,4 @@
-# $NetBSD: grammar.mk,v 1.3 2023/01/18 13:28:15 wiz Exp $
+# $NetBSD: grammar.mk,v 1.4 2023/01/18 16:32:26 wiz Exp $
 #
 # Common logic to build and install tree-sitter grammars.
 #
@@ -7,20 +7,21 @@
 #
 
 GRAMMAR_LIBRARY_NAME?=	lib${PKGBASE}
+GRAMMAR_SOURCE_DIR?=	src
 
 USE_LANGUAGES+=		c c++
 
 do-build:
-	cd ${WRKSRC}/src && \
+	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
 	${CC} ${CFLAGS} -std=c99 -I. -c -fPIC parser.c
-	cd ${WRKSRC}/src && \
+	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
 	if [ -f scanner.c ]; then ${CC} ${CFLAGS} -std=c99 -I. -c -fPIC scanner.c; fi
-	cd ${WRKSRC}/src && \
+	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
 	if [ -f scanner.cc ]; then ${CXX} ${CXXFLAGS} -I. -c -fPIC scanner.cc; fi
-	cd ${WRKSRC}/src && \
+	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
 	${CXX} -shared -o ${GRAMMAR_LIBRARY_NAME}.so *.o
 
 INSTALLATION_DIRS+=	lib
 
 do-install:
-	${INSTALL_LIB} ${WRKSRC}/src/${GRAMMAR_LIBRARY_NAME}.so ${DESTDIR}${PREFIX}/lib
+	${INSTALL_LIB} ${WRKSRC}/${GRAMMAR_SOURCE_DIR}/${GRAMMAR_LIBRARY_NAME}.so ${DESTDIR}${PREFIX}/lib
