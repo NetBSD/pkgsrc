62f245ac3cee660f5f7636d8a768d8477abdc5f0
wiz <wiz@pkgsrc.org>
2023-01-19 13:58:16 +0000

    sysutils/tmux-mem-cpu-load: import tmux-mem-cpu-load-3.6.0
    A simple, lightweight program provided for system monitoring in
    the status line of tmux.
    
    The memory monitor displays the used and available memory.
    
    The CPU usage monitor outputs a percent CPU usage over all processors.
    It also displays a textual bar graph of the current percent usage.
    
    The system load average is also displayed.

---
+++ sysutils/tmux-mem-cpu-load/patches/patch-netbsd_cpu.cc
@@ -0,0 +1,21 @@
+$NetBSD: patch-netbsd_cpu.cc,v 1.1 2023/01/19 13:58:16 wiz Exp $
+
+Adapt to signature change.
+
+--- netbsd/cpu.cc.orig	2022-03-03 16:23:14.000000000 +0000
++++ netbsd/cpu.cc
+@@ -26,12 +26,12 @@
+ #include "getsysctl.h"
+ #include "cpu.h"
+ 
+-uint8_t get_cpu_count()
++uint32_t get_cpu_count()
+ {
+   int cpu_count = 0;
+   GETSYSCTL( "hw.ncpu", cpu_count );
+ 
+-  return static_cast<uint8_t>( cpu_count );
++  return static_cast<uint32_t>( cpu_count );
+ }
+ 
+ float cpu_percentage( unsigned int cpu_usage_delay )
