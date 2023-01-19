0d1fde2be758960b4bc26ff8d8a411164efb8f73
wiz <wiz@pkgsrc.org>
2023-01-19 00:53:42 +0000

    hunspell: update to 1.7.2
    * Crash fixes, code clean-up in ~200 commits
    * tdf#136306 don't accept/suggest typos as 3-or-more-word compound
      words
    * Prepare optional spelling mode of LibreOffice to not accept/suggest
      not dictionary-based words as compound words (#517)
    * Merge in weblate translations
    
    While here:
    Fix libcurses detection on NetBSD.
    However, switch to ncursesw to get Unicode support, hunspell
    doesn't recognize wide character support in NetBSD curses.

--- textproc/hunspell/options.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: options.mk,v 1.9 2021/11/28 19:24:19 nia Exp $
-
-PKG_OPTIONS_VAR=		PKG_OPTIONS.hunspell
-
-# handled by curses framework
-PKG_SUPPORTED_OPTIONS=		wide-curses
-PKG_SUGGESTED_OPTIONS=		wide-curses
-
-.include "../../mk/bsd.options.mk"
