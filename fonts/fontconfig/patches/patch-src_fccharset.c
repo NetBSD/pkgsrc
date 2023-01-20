e686040d400df836265363470cf7e91710b6595b
wiz <wiz@pkgsrc.org>
2023-01-20 13:10:08 +0000

    fontconfig: update to 2.14.1.
    Changes not found.

--- fonts/fontconfig/patches/patch-src_fccharset.c
+++ fonts/fontconfig/patches/patch-src_fccharset.c
@@ -1,8 +1,6 @@
-$NetBSD: patch-src_fccharset.c,v 1.3 2022/04/07 11:47:48 riastradh Exp $
+$NetBSD: patch-src_fccharset.c,v 1.4 2023/01/20 13:10:09 wiz Exp $
 
 - Fix build on SCO OpenServer 5.0.7/3.2.
-- Fix ctype(3) abuse.
-  https://gitlab.freedesktop.org/fontconfig/fontconfig/-/merge_requests/224
 
 --- src/fccharset.c.orig	2018-06-05 10:36:38.000000000 +0000
 +++ src/fccharset.c
@@ -15,20 +13,3 @@ $NetBSD: patch-src_fccharset.c,v 1.3 2022/04/07 11:47:48 riastradh Exp $
      return __builtin_popcount (c1);
  #else
      /* hackmem 169 */
-@@ -838,14 +838,14 @@ FcNameParseRange (FcChar8 **string, FcCh
- 	char *t;
- 	long first, last;
- 
--	while (isspace(*s))
-+	while (isspace((unsigned char)*s))
- 	    s++;
- 	t = s;
- 	errno = 0;
- 	first = last = strtol (s, &s, 16);
- 	if (errno)
- 	    return FcFalse;
--	while (isspace(*s))
-+	while (isspace((unsigned char)*s))
- 	    s++;
- 	if (*s == '-')
- 	{
