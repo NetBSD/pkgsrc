604906f537532a91cd7d750db903c8f317997116
adam <adam@pkgsrc.org>
2023-01-20 13:08:07 +0000

    cmake: updated to 3.25.2
    CMake 3.25.2
    * CheckSymbolExists: Restore newline at end of test source
    * Utilities/Release: Use explicit digest for Win7-compatible signature
    * Help: Clarify SYSTEM property default for imported targets
    * gitlab-ci: replace '$os' tags with '$os-x86_64' on 3.25 release branch
    * gitlab-ci: drop unnecessary linux kernel version tag on 3.25 release branch
    * ccmake: Restore compilation with AIX curses.h
    * ASM_MASM: Populate MSVC debug information format abstraction table
    * VS: Do not enable ASM_MASM debug information unless requested
    * gitlab-ci: update macOS jobs to use Xcode 14.2
    * Tests: Fix CTest.UpdateGIT under repo-local defaultBranch config
    * try_run: Avoid crash in keyword-dispatched signature when cross-compiling
    * Restore implicit include directory extraction for adaptive relative paths
    * IntelLLVM: Avoid unnecessary -Qstd=c++11 flag on Windows
    * Help: Restore cmake-buildsystem(7) header-only library example
    * FetchContent: Don't pass SYSTEM through to sub-build
    * Help: Clarify and update SYSTEM-related docs
    * Code comments: Fix trivial typos
    * Help: Add version information for SYSTEM option of add_subdirectory
    * Help: string(JSON): avoid duplicate labels
    * IntelLLVM: Avoid finding not-yet-supported icpx on Windows
    * Help: Clarify compiler id distinction between Intel Classic and IntelLLVM
    * CUDA: Add support for cuda_std_20 for nvcc 12.0+
    * FindCUDAToolkit: Handle CUDA::nvToolsExt not existing
    * zlib: Fix typo in mangling the crc32() function
    * FindBoost: Add Boost 1.81 support

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.33 2022/12/02 09:20:10 adam Exp $
+# $NetBSD: version.mk,v 1.34 2023/01/20 13:08:07 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.25.1
+CMAKE_VERSION=	3.25.2
 CMAKE_API=	${CMAKE_VERSION:R}
