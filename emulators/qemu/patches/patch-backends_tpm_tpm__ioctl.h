d07be185cd451734a77ef4678945d9a0423f2710
adam <adam@pkgsrc.org>
2023-01-20 08:36:24 +0000

    qemu: updated to 7.2.0
    https://wiki.qemu.org/ChangeLog/7.2

--- emulators/qemu/patches/patch-backends_tpm_tpm__ioctl.h
+++ emulators/qemu/patches/patch-backends_tpm_tpm__ioctl.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-backends_tpm_tpm__ioctl.h,v 1.1 2020/08/12 18:31:27 ryoon Exp $
+$NetBSD: patch-backends_tpm_tpm__ioctl.h,v 1.2 2023/01/20 08:36:24 adam Exp $
 
 SunOS needs filio.h for _IO*() macros.
 
---- backends/tpm/tpm_ioctl.h.orig	2019-08-15 19:01:42.000000000 +0000
+--- backends/tpm/tpm_ioctl.h.orig	2022-12-14 16:28:45.000000000 +0000
 +++ backends/tpm/tpm_ioctl.h
-@@ -9,6 +9,9 @@
- #ifndef TPM_IOCTL_H
- #define TPM_IOCTL_H
+@@ -14,6 +14,9 @@
  
+ #include <stdint.h>
+ #include <sys/types.h>
 +#ifdef __sun
 +#include <sys/filio.h>
 +#endif
+ #ifndef _WIN32
  #include <sys/uio.h>
  #include <sys/ioctl.h>
- 
--- emulators/qemu/patches/patch-hw_display_omap__dss.c
+++ emulators/qemu/patches/patch-hw_display_omap__dss.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-hw_display_omap__dss.c,v 1.2 2014/01/15 18:26:20 wiz Exp $
+$NetBSD: patch-hw_display_omap__dss.c,v 1.3 2023/01/20 08:36:24 adam Exp $
 
 Avoid conflicts with SSP read() macro in NetBSD's <ssp/unistd.h>
 (PR lib/43832: ssp causes common names to be defines)
 
---- hw/display/omap_dss.c.orig	2013-11-27 22:15:55.000000000 +0000
+--- hw/display/omap_dss.c.orig	2022-12-14 16:28:45.000000000 +0000
 +++ hw/display/omap_dss.c
-@@ -791,18 +791,18 @@ static void omap_rfbi_write(void *opaque
+@@ -798,18 +798,18 @@ static void omap_rfbi_write(void *opaque
          break;
-     case 0x58:	/* RFBI_READ */
+     case 0x58:  /* RFBI_READ */
          if ((s->rfbi.control & (1 << 2)) && s->rfbi.chip[0])
 -            s->rfbi.rxbuf = s->rfbi.chip[0]->read(s->rfbi.chip[0]->opaque, 1);
 +            s->rfbi.rxbuf = (*s->rfbi.chip[0]->read)(s->rfbi.chip[0]->opaque, 1);
@@ -18,7 +18,7 @@ Avoid conflicts with SSP read() macro in NetBSD's <ssp/unistd.h>
              omap_rfbi_transfer_stop(s);
          break;
  
-     case 0x5c:	/* RFBI_STATUS */
+     case 0x5c:  /* RFBI_STATUS */
          if ((s->rfbi.control & (1 << 2)) && s->rfbi.chip[0])
 -            s->rfbi.rxbuf = s->rfbi.chip[0]->read(s->rfbi.chip[0]->opaque, 0);
 +            s->rfbi.rxbuf = (*s->rfbi.chip[0]->read)(s->rfbi.chip[0]->opaque, 0);
