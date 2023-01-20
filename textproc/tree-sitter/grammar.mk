d8a66b9850309086e717bd573c3ea9b14168b5fc
wiz <wiz@pkgsrc.org>
2023-01-20 11:14:58 +0000

    treesitter: use libtool for generating grammars
    Hopefully fixes use on macOS

--- textproc/tree-sitter/grammar.mk
+++ textproc/tree-sitter/grammar.mk
@@ -1,4 +1,4 @@
-# $NetBSD: grammar.mk,v 1.4 2023/01/18 16:32:26 wiz Exp $
+# $NetBSD: grammar.mk,v 1.5 2023/01/20 11:14:58 wiz Exp $
 #
 # Common logic to build and install tree-sitter grammars.
 #
@@ -10,18 +10,19 @@ GRAMMAR_LIBRARY_NAME?=	lib${PKGBASE}
 GRAMMAR_SOURCE_DIR?=	src
 
 USE_LANGUAGES+=		c c++
+USE_LIBTOOL=		yes
 
 do-build:
 	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
-	${CC} ${CFLAGS} -std=c99 -I. -c -fPIC parser.c
+	libtool --mode=compile ${CC} ${CFLAGS} -std=c99 -I. -c -fPIC parser.c
 	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
-	if [ -f scanner.c ]; then ${CC} ${CFLAGS} -std=c99 -I. -c -fPIC scanner.c; fi
+	if [ -f scanner.c ]; then libtool --mode=compile ${CC} ${CFLAGS} -std=c99 -I. -c -fPIC scanner.c; fi
 	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
-	if [ -f scanner.cc ]; then ${CXX} ${CXXFLAGS} -I. -c -fPIC scanner.cc; fi
+	if [ -f scanner.cc ]; then libtool --mode=compile ${CXX} ${CXXFLAGS} -I. -c -fPIC scanner.cc; fi
 	cd ${WRKSRC}/${GRAMMAR_SOURCE_DIR} && \
-	${CXX} -shared -o ${GRAMMAR_LIBRARY_NAME}.so *.o
+	libtool --mode=link ${CXX} ${LDFLAGS} -module -rpath ${PREFIX}/lib -shared -avoid-version -o ${GRAMMAR_LIBRARY_NAME}.la *.lo
 
 INSTALLATION_DIRS+=	lib
 
 do-install:
-	${INSTALL_LIB} ${WRKSRC}/${GRAMMAR_SOURCE_DIR}/${GRAMMAR_LIBRARY_NAME}.so ${DESTDIR}${PREFIX}/lib
+	libtool --mode=install ${INSTALL_LIB} ${WRKSRC}/${GRAMMAR_SOURCE_DIR}/${GRAMMAR_LIBRARY_NAME}.la ${DESTDIR}${PREFIX}/lib
