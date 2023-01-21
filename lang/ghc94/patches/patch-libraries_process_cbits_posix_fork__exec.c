15422e9c83d0d3c0f6c52afd3b6c81dfab06a661
pho <pho@pkgsrc.org>
2023-01-21 13:20:33 +0000

    Add a merge request URL to a patch

--- lang/ghc94/patches/patch-libraries_process_cbits_posix_fork__exec.c
+++ lang/ghc94/patches/patch-libraries_process_cbits_posix_fork__exec.c
@@ -1,6 +1,9 @@
-$NetBSD: patch-libraries_process_cbits_posix_fork__exec.c,v 1.1 2023/01/21 04:49:25 pho Exp $
+$NetBSD: patch-libraries_process_cbits_posix_fork__exec.c,v 1.2 2023/01/21 13:20:34 pho Exp $
 
 Ensure _POSIX_PTHREAD_SEMANTICS is defined on SunOS.
+
+Pull request:
+https://github.com/haskell/process/pull/274
 
 --- libraries/process/cbits/posix/fork_exec.c.orig	2021-10-28 20:42:10.000000000 +0000
 +++ libraries/process/cbits/posix/fork_exec.c
