141a238017b9d5799eaafe27ba723a2515244883
wiz <wiz@pkgsrc.org>
2023-01-23 16:57:06 +0000

    gstreamer1: update to 1.20.5
    GStreamer 1.20.5 stable bug fix release
    
    The GStreamer team is pleased to announce another bug fix release
    in the stable 1.20 release series of your favourite cross-platform
    multimedia framework!
    
    This release only contains bug fixes. It should be safe to update from 1.20.x.
    
    Highlighted bugfixes:
    
        systemclock waiting fixes for certain 32-bit platforms/libcs
        alphacombine: robustness improvements for corner case scenarios
        avfvideosrc: Report latency when doing screen capture
        d3d11videosink: various thread-safety and stability fixes
        decklink: fix performance issue when HDMI signal has been lost for a long time
        flacparse: Fix handling of headers advertising 32 bits per sample
        mpegts: Handle when iconv doesn't support ISO 6937 (e.g. musl libc)
        opengl: fix automatic dispmanx detection for rpi4 and fix usage of eglCreate/DestroyImage
        opusdec: Various channel-related fixes
        textrender: event handling fixes, esp. for GAP event
        subparse: Fix non-closed tag handling
        videoscale: fix handling of unknown buffer metas
        videosink: reverse playback handling fixes
        qtmux: Prefill mode fixes, especially for raw audio
        multiudpsink: allow binding to IPv6 address
        rtspsrc: Fix usage of IPv6 connections in SETUP
        rtspsrc: Only EOS on timeout if all streams are timed out/EOS
        splitmuxsrc: fix playback stall if there are unlinked pads
        v4l2: Fix SIGSEGV on state change during format changes
        wavparse robustness fixes
        Fix static linking on macOS (opengl, vulkan)
        gstreamer-vaapi: fix headless build against mesa >= 22.3.0
        GStreamer Editing Services library: Fix build with tools disabled
        webrtc example/demo fixes
        unit test fixes for aesdec and rtpjitterbuffer
        Cerbero: Fix ios cross-compile with cmake on M1; some recipe updates and other build fixes
        Binary packages: pkg-config file fixes for various recipes (ffmpeg, taglib, gstreamer)
        Binary packages: Enable high bitdepth support for libvpx (VP8/VP9 encoding/decoding)
        Binary packages: ship aes plugin
        Performance improvements
        Miscellaneous bug fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.1 2022/12/16 21:08:08 adam Exp $
+# $NetBSD: version.mk,v 1.2 2023/01/23 16:57:08 wiz Exp $
 
-GST_VERSION=	1.20.4
+GST_VERSION=	1.20.5
 
 MAINTAINER?=	pkgsrc-users@NetBSD.org
 HOMEPAGE?=	https://gstreamer.freedesktop.org/
