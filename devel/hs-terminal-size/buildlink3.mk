489ecfbe23d1da04f313d6102a3660510dbdfd35
pho <pho@pkgsrc.org>
2023-01-24 17:07:08 +0000

    devel/hs-terminal-size: Update to 0.3.3
    0.3.3
    * Fixed ioctl foreign import. (https://github.com/biegunka/terminal-size/pull/16)
    * #alignment is a hsc2hs built-in since some unspecified GHC
       7.x. (https://github.com/biegunka/terminal-size/pull/12)
    * Captured possible stty stderr output with a
      pipe. (https://github.com/biegunka/terminal-size/pull/13)

--- devel/hs-terminal-size/buildlink3.mk
+++ devel/hs-terminal-size/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:58:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/24 17:07:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-terminal-size
 
 .if !defined(HS_TERMINAL_SIZE_BUILDLINK3_MK)
 HS_TERMINAL_SIZE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.2
-BUILDLINK_ABI_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.2.1nb4
-BUILDLINK_PKGSRCDIR.hs-terminal-size?=	../../devel/hs-terminal-size
+BUILDLINK_API_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.3
+BUILDLINK_ABI_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.3
+BUILDLINK_PKGSRCDIR.hs-terminal-size?=		../../devel/hs-terminal-size
 .endif	# HS_TERMINAL_SIZE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-terminal-size
