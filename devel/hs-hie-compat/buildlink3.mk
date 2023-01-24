4eb0e70a4720a6b62a11203388f0d09b5ed94077
pho <pho@pkgsrc.org>
2023-01-24 17:26:07 +0000

    devel/hs-hie-compat: Update to 0.3.1.0
    No release notes have been provided by upstream.

--- devel/hs-hie-compat/buildlink3.mk
+++ devel/hs-hie-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 17:26:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-hie-compat
 
 .if !defined(HS_HIE_COMPAT_BUILDLINK3_MK)
 HS_HIE_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hie-compat+=	hs-hie-compat>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-hie-compat+=	hs-hie-compat>=0.2.1.1nb1
+BUILDLINK_API_DEPENDS.hs-hie-compat+=	hs-hie-compat>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-hie-compat+=	hs-hie-compat>=0.3.1.0
 BUILDLINK_PKGSRCDIR.hs-hie-compat?=	../../devel/hs-hie-compat
 .endif	# HS_HIE_COMPAT_BUILDLINK3_MK
 
