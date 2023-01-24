60396a2381d1cfec330423288ed07d6a7357ec60
pho <pho@pkgsrc.org>
2023-01-24 13:21:43 +0000

    devel/hs-vector-stream: Fix build with GHC 9.4

--- devel/hs-vector-stream/buildlink3.mk
+++ devel/hs-vector-stream/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/08/20 08:08:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 13:21:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-stream
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-vector-stream
 HS_VECTOR_STREAM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vector-stream+=	hs-vector-stream>=0.1.0.0
-BUILDLINK_ABI_DEPENDS.hs-vector-stream?=		hs-vector-stream>=0.1.0.0
+BUILDLINK_ABI_DEPENDS.hs-vector-stream?=	hs-vector-stream>=0.1.0.0
 BUILDLINK_PKGSRCDIR.hs-vector-stream?=		../../devel/hs-vector-stream
 .endif	# HS_VECTOR_STREAM_BUILDLINK3_MK
 
