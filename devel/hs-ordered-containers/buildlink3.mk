744b32e41c76efc857657c5e2037dfbfc17c0090
pho <pho@pkgsrc.org>
2023-01-24 18:19:17 +0000

    devel/hs-ordered-containers: Update to 0.2.3
    No release notes have been provided by upstream.

--- devel/hs-ordered-containers/buildlink3.mk
+++ devel/hs-ordered-containers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 18:19:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-ordered-containers
 
 .if !defined(HS_ORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_ORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.2nb1
+BUILDLINK_API_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.3
 BUILDLINK_PKGSRCDIR.hs-ordered-containers?=	../../devel/hs-ordered-containers
 .endif	# HS_ORDERED_CONTAINERS_BUILDLINK3_MK
 
