02813e73be1a8aa4d22ed37f44bcc24f9c884aaf
pho <pho@pkgsrc.org>
2023-01-24 15:02:50 +0000

    security/hs-entropy: Update to 0.4.1.10
    No release notes have been provided by upstream.

--- security/hs-entropy/buildlink3.mk
+++ security/hs-entropy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 15:02:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-entropy
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-entropy
 HS_ENTROPY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-entropy+=	hs-entropy>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-entropy+=	hs-entropy>=0.4.1.7nb1
+BUILDLINK_ABI_DEPENDS.hs-entropy+=	hs-entropy>=0.4.1.10
 BUILDLINK_PKGSRCDIR.hs-entropy?=	../../security/hs-entropy
 .endif	# HS_ENTROPY_BUILDLINK3_MK
 
