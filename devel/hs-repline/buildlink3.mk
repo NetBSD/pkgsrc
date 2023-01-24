a66eb03e8887199ad5a7dac9b3827da346a44616
pho <pho@pkgsrc.org>
2023-01-24 15:44:30 +0000

    devel/hs-repline: Update to 0.4.2.0
    0.4.2.0
    * Add support for ghc 9.0, 9.2
    * CI: Drop Travis and use Github Actions instead
    
    0.4.1.0
    * Fix up documentation.
    * Export MonadHaskeline from System.Control.Repline.

--- devel/hs-repline/buildlink3.mk
+++ devel/hs-repline/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 15:44:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-repline
 
 .if !defined(HS_REPLINE_BUILDLINK3_MK)
 HS_REPLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-repline+=	hs-repline>=0.4.0
-BUILDLINK_ABI_DEPENDS.hs-repline+=	hs-repline>=0.4.0.0nb1
+BUILDLINK_API_DEPENDS.hs-repline+=	hs-repline>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-repline+=	hs-repline>=0.4.2.0
 BUILDLINK_PKGSRCDIR.hs-repline?=	../../devel/hs-repline
 .endif	# HS_REPLINE_BUILDLINK3_MK
 
