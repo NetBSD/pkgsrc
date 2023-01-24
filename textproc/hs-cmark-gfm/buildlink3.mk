8d0cd9869a66168d4aa26e53ac52eeba7f8a096e
pho <pho@pkgsrc.org>
2023-01-24 12:22:14 +0000

    textproc/hs-cmark-gfm: Update to 0.2.5
    cmark-gfm 0.2.5 (27 Sep 2022)
    * Add support for enabling footnotes (#26, Anton Sorokin).
    
    cmark-gfm 0.2.4 (22 Sep 2022)
    * Pull in upstream changes (includes security fix).
    
    cmark-gfm 0.2.3 (9 Mar 2022)
    * Pull in upstream changes.

--- textproc/hs-cmark-gfm/buildlink3.mk
+++ textproc/hs-cmark-gfm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/24 12:22:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-cmark-gfm
 
 .if !defined(HS_CMARK_GFM_BUILDLINK3_MK)
 HS_CMARK_GFM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.2nb4
+BUILDLINK_API_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.5
+BUILDLINK_ABI_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.5
 BUILDLINK_PKGSRCDIR.hs-cmark-gfm?=	../../textproc/hs-cmark-gfm
 
 .include "../../textproc/cmark-gfm/buildlink3.mk"
