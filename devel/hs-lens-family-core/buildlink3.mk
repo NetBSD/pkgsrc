73806c0cf65df71f6e5b4df1c631aa71add96b46
pho <pho@pkgsrc.org>
2023-01-24 16:55:05 +0000

    devel/hs-lens-family-core: Update to 2.1.2
    2.1.2 (Changes from 2.1.0)
    ==========================
    * Bump dependency on transformers.

--- devel/hs-lens-family-core/buildlink3.mk
+++ devel/hs-lens-family-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:00 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 16:55:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-lens-family-core
 
 .if !defined(HS_LENS_FAMILY_CORE_BUILDLINK3_MK)
 HS_LENS_FAMILY_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lens-family-core+=	hs-lens-family-core>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-lens-family-core+=	hs-lens-family-core>=2.1.0nb1
+BUILDLINK_API_DEPENDS.hs-lens-family-core+=	hs-lens-family-core>=2.1.2
+BUILDLINK_ABI_DEPENDS.hs-lens-family-core+=	hs-lens-family-core>=2.1.2
 BUILDLINK_PKGSRCDIR.hs-lens-family-core?=	../../devel/hs-lens-family-core
 .endif	# HS_LENS_FAMILY_CORE_BUILDLINK3_MK
 
