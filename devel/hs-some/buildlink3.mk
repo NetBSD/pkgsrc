c23e1b711a5c3402b64cdf6e82cb685557959edf
pho <pho@pkgsrc.org>
2023-01-24 18:34:14 +0000

    devel/hs-some: Update to 1.0.4.1
    1.0.4.1
    * Drop support for GHC before 8.6
    
    1.0.4
    * Add instances for (:~~:)
    * Add instances for :+: and :*:
    * Add defaultGeq :: GCompare f => f a -> f b -> Maybe (a :~: b)
    * Add defaultGshowsPrec :: Show (t a) => Int -> t a -> ShowS

--- devel/hs-some/buildlink3.mk
+++ devel/hs-some/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 18:34:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-some
 
 .if !defined(HS_SOME_BUILDLINK3_MK)
 HS_SOME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-some+=	hs-some>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-some+=	hs-some>=1.0.3nb1
+BUILDLINK_API_DEPENDS.hs-some+=	hs-some>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-some+=	hs-some>=1.0.4.1
 BUILDLINK_PKGSRCDIR.hs-some?=	../../devel/hs-some
 .endif	# HS_SOME_BUILDLINK3_MK
 
