4ced9ac98385eb60a8a577bfcd1d98865528dc68
pho <pho@pkgsrc.org>
2023-01-24 16:39:26 +0000

    lang/hs-lua: Update to 2.2.1
    lua-2.2.1 -- Released 2022-06-19
    * Make string-peeking independent of the current foreign encoding: always
      use char8 encoding to retrieve values of LUA_PRELOAD_TABLE and
      LUA_LOADED_TABLE.
    
    lua-2.2.0 -- Released 2022-02-19
    * Update to Lua 5.4, include Lua 5.4.4 by default.
    * Removed hardcode-reg-keys flag: this is no longer required for Lua 5.4.
    * Support arbitrary number of uservalues: Lua 5.4 allows an arbitrary
      number of uservalues to be associated with userdata objects. The
      functions lua_newuserdata, lua_getuservalue, and lua_setuservalue are
      replaced with the new functions lua_newuserdatauv, lua_getiuservalue, and
      lua_setiuservalue, respectively.
    * The function hslua_newhsuserdata is renamed to hslua_newhsuserdatauv and
      takes the number of associated uservalues as an additional argument.
    * Support for the new warnings system: export binding to lua_warning.
    * The function lua_gc now takes three data arguments of type CInt. This is
      a workaround for the fact that the C function has become variadic.
    * The new patterns LUA_GCGEN and LUA_GCINC are usable with lua_gc to switch
      to generational and incremental garbage collection, respectively.

--- lang/hs-lua/buildlink3.mk
+++ lang/hs-lua/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/24 16:39:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-lua
 
 .if !defined(HS_LUA_BUILDLINK3_MK)
 HS_LUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.2.1
 BUILDLINK_PKGSRCDIR.hs-lua?=	../../lang/hs-lua
 
-.include "../../lang/lua53/buildlink3.mk"
+.include "../../lang/lua54/buildlink3.mk"
 .endif	# HS_LUA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-lua
