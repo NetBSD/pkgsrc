eee6fee12279a81bdf5e2ff91dfd1b438ee12206
pho <pho@pkgsrc.org>
2023-01-24 12:29:23 +0000

    textproc/hs-unicode-show: Update to 0.1.1.1
    No release notes are provided by upstream

--- textproc/hs-unicode-show/buildlink3.mk
+++ textproc/hs-unicode-show/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/24 12:29:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-show
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	hs-unicode-show
 HS_UNICODE_SHOW_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.1.0nb2
+BUILDLINK_ABI_DEPENDS.hs-unicode-show+=	hs-unicode-show>=0.1.1.1
 BUILDLINK_PKGSRCDIR.hs-unicode-show?=	../../textproc/hs-unicode-show
+
+.include "../../devel/hs-safe/buildlink3.mk"
 .endif	# HS_UNICODE_SHOW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-unicode-show
