cd29220d0134371d5c4ae9c178c6204b3c1f6701
pho <pho@pkgsrc.org>
2023-01-24 12:12:11 +0000

    textproc/hs-unicode-data: Update to 0.4.0.1
    0.4.0.1 (December 2022)
    * Fix Unicode blocks handling on big-endian architectures.
    
    0.4.0 (September 2022)
    * Update to Unicode 15.0.0.
    
    0.3.1 (September 2022)
    * Added full case conversions to Unicode.Char.Case:
      - Case folding: caseFoldMapping and toCaseFoldString.
      - Lower case: lowerCaseMapping and toLowerString.
      - Upper case: upperCaseMapping and toUpperString.
      - Title case: titleCaseMapping and toTitleString.
      - Stream mechanism: Unfold and Step.
      - Added isNumeric, numericValue and integerValue to Unicode.Char.Numeric.
      - Added the module Unicode.Char.General.Blocks.
      - Add compatibility module:
        * Unicode.Char.Numeric.Compat
    * Deprecations
      - Unicode.Char.Numeric.isNumber: it will be replaced by isNumeric in a
        future version of this package. Use the function in
        Unicode.Char.Numeric.Compat instead.

--- textproc/hs-unicode-data/buildlink3.mk
+++ textproc/hs-unicode-data/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/24 12:12:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-data
 
 .if !defined(HS_UNICODE_DATA_BUILDLINK3_MK)
 HS_UNICODE_DATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.3.0nb2
+BUILDLINK_API_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.4
+BUILDLINK_ABI_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.4.0.1
 BUILDLINK_PKGSRCDIR.hs-unicode-data?=	../../textproc/hs-unicode-data
 .endif	# HS_UNICODE_DATA_BUILDLINK3_MK
 
