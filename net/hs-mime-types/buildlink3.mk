a879eaee32df92b3f62dafa5f536375957dd06e5
pho <pho@pkgsrc.org>
2023-01-24 12:41:00 +0000

    net/hs-mime-types: Update to 0.1.1.0
    0.1.1.0
    * Replace audio/x-mpegurl with IANA registered type
      application/vnd.apple.mpegurl.
    * Add TeX-related types; this includes bib, tex, sty, and cls.
    * Use type image/x-xcf for .xcf files.
    * Use type "audio/opus" for .opus files.
    * Add type text/vtt for .vtt text track files.
    * Use IANA registered type "application/vnd.rar" for .rar files.
    * Use font types defined in RFC 8081.
    * Replace audio/x-m4a with audio/mp4.
    * Change mime type for .exe files; use IANA registered type
      application/vnd.microsoft.portable-executable.
    * Add video/dv for files with extension dv.
    * Use 'application/xml' instead of 'text/xml'.
    * Change type for .pcx files to image/vnd.zbrush.pcx.
    * Use text/markdown type for .md and .markdown files.
    * Replace application/x-gzip with type application/gzip.
    See PR #906.
    
    0.1.0.9
    * Add mjs mime type

--- net/hs-mime-types/buildlink3.mk
+++ net/hs-mime-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:58:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/24 12:41:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-mime-types
 
 .if !defined(HS_MIME_TYPES_BUILDLINK3_MK)
 HS_MIME_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.0.9nb4
+BUILDLINK_API_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.1.0
 BUILDLINK_PKGSRCDIR.hs-mime-types?=	../../net/hs-mime-types
 .endif	# HS_MIME_TYPES_BUILDLINK3_MK
 
