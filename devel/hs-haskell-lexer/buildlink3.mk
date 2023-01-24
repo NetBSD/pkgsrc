33dcfc8d0d2990c6d78e8362da123e4bda58606b
pho <pho@pkgsrc.org>
2023-01-24 17:11:08 +0000

    devel/hs-haskell-lexer: Update to 1.1.1
    No release notes have been provided by upstream.

--- devel/hs-haskell-lexer/buildlink3.mk
+++ devel/hs-haskell-lexer/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:57:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/24 17:11:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-haskell-lexer
 
 .if !defined(HS_HASKELL_LEXER_BUILDLINK3_MK)
 HS_HASKELL_LEXER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.1
-BUILDLINK_ABI_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.1nb4
+BUILDLINK_API_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-haskell-lexer?=		../../devel/hs-haskell-lexer
 .endif	# HS_HASKELL_LEXER_BUILDLINK3_MK
 
