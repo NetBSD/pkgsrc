bdcfead9440ca66314a841a12c1ded4c07d3bdde
pho <pho@pkgsrc.org>
2023-01-24 13:04:44 +0000

    textproc/hs-text-manipulate: Update to 0.3.1.0
    No release notes have been provided by upstream.

--- textproc/hs-text-manipulate/buildlink3.mk
+++ textproc/hs-text-manipulate/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 13:04:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-manipulate
 
 .if !defined(HS_TEXT_MANIPULATE_BUILDLINK3_MK)
 HS_TEXT_MANIPULATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-manipulate+=	hs-text-manipulate>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-text-manipulate+=	hs-text-manipulate>=0.3.0.0nb1
+BUILDLINK_API_DEPENDS.hs-text-manipulate+=	hs-text-manipulate>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-text-manipulate+=	hs-text-manipulate>=0.3.1.0
 BUILDLINK_PKGSRCDIR.hs-text-manipulate?=	../../textproc/hs-text-manipulate
 .endif	# HS_TEXT_MANIPULATE_BUILDLINK3_MK
 
