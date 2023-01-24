6a78af19e3ac2aaeb0384e2174644fc0a0cc42c9
pho <pho@pkgsrc.org>
2023-01-24 18:21:23 +0000

    devel/hs-th-compat: Update to 0.1.4
    0.1.4 [2022.07.23]
    * Backport the getPackageRoot and makeRelativeToProject functions
      introduced in template-haskell-2.19.0.0 (GHC 9.4).
    * Implement qGetPackageRoot in the Quasi instance for QuoteToQuasi when
      building with template-haskell-2.19.0.0 (GHC 9.4) or later.

--- devel/hs-th-compat/buildlink3.mk
+++ devel/hs-th-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/24 18:21:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-compat
 
 .if !defined(HS_TH_COMPAT_BUILDLINK3_MK)
 HS_TH_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.3nb2
+BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-th-compat?=	../../devel/hs-th-compat
 .endif	# HS_TH_COMPAT_BUILDLINK3_MK
 
