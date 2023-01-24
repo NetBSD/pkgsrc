1afcfd5a6eda14efe9a4341b0b82b4cca6c452c1
pho <pho@pkgsrc.org>
2023-01-24 17:01:46 +0000

    math/hs-algebraic-graphs: Update to 0.7
    0.7
    * #294: Change the argument order of bfs*, dfs* and reachable algorithms.
    * #293: Fix the ToGraph instance of symmetric relations.
    
    0.6.1
    * Drop dependency on mtl.

--- math/hs-algebraic-graphs/buildlink3.mk
+++ math/hs-algebraic-graphs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 17:01:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-algebraic-graphs
 
 .if !defined(HS_ALGEBRAIC_GRAPHS_BUILDLINK3_MK)
 HS_ALGEBRAIC_GRAPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-algebraic-graphs+=	hs-algebraic-graphs>=0.6
-BUILDLINK_ABI_DEPENDS.hs-algebraic-graphs+=	hs-algebraic-graphs>=0.6nb1
+BUILDLINK_API_DEPENDS.hs-algebraic-graphs+=	hs-algebraic-graphs>=0.7
+BUILDLINK_ABI_DEPENDS.hs-algebraic-graphs+=	hs-algebraic-graphs>=0.7
 BUILDLINK_PKGSRCDIR.hs-algebraic-graphs?=	../../math/hs-algebraic-graphs
 .endif	# HS_ALGEBRAIC_GRAPHS_BUILDLINK3_MK
 
