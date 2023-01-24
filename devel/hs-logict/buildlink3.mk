7a678c34f98360c9c3d7dc96ae129a29fa3fcaaf
pho <pho@pkgsrc.org>
2023-01-24 16:43:04 +0000

    devel/hs-logict: Updateto 0.8.0.0
    0.8.0.0
    * Breaking change: do not re-export Control.Monad and Control.Monad.Trans
      from Control.Monad.Logic.
    * Generalize instance Traversable (LogicT Identity) to instance
      (Traversable m, Monad m) => Traversable (LogicT m).
    * Add conversion functions fromLogicT and fromLogicTWith to facilitate
      interoperation with list-t and logict-sequence packages.
    * Add hoistLogicT and embedLogicT to convert LogicT computations from one
      underlying monad to another.

--- devel/hs-logict/buildlink3.mk
+++ devel/hs-logict/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:00 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 16:43:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-logict
 
 .if !defined(HS_LOGICT_BUILDLINK3_MK)
 HS_LOGICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-logict+=	hs-logict>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-logict+=	hs-logict>=0.7.1.0nb1
+BUILDLINK_API_DEPENDS.hs-logict+=	hs-logict>=0.8.0
+BUILDLINK_ABI_DEPENDS.hs-logict+=	hs-logict>=0.8.0.0
 BUILDLINK_PKGSRCDIR.hs-logict?=		../../devel/hs-logict
 .endif	# HS_LOGICT_BUILDLINK3_MK
 
