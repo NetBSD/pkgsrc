8b86d6e87c9f55dd966a8aa7dc4a615d9dd3b62b
wiz <wiz@pkgsrc.org>
2023-01-24 23:34:36 +0000

    libLLVM: pkglint cleanups

--- lang/libLLVM/options.mk
+++ lang/libLLVM/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2022/10/31 14:54:38 triaxx Exp $
+# $NetBSD: options.mk,v 1.7 2023/01/24 23:34:36 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libLLVM
 
@@ -18,21 +18,21 @@ PKG_SUPPORTED_OPTIONS+=		terminfo
 CHECK_BUILTIN.terminfo:=	yes
 .include "../../mk/terminfo.builtin.mk"
 CHECK_BUILTIN.terminfo:=	no
-.if !empty(USE_BUILTIN.terminfo:M[yY][eE][sS]) && ${OPSYS} != "FreeBSD"
+.if ${USE_BUILTIN.terminfo:tl} == yes && ${OPSYS} != "FreeBSD"
 PKG_SUGGESTED_OPTIONS+=		terminfo
 .endif
 
 # Probably safe to assume that only x86 users are interested in
 # cross-compilation for now. This saves some build time for everyone else.
-.if !empty(MACHINE_ARCH:Msparc*)
+.if ${MACHINE_ARCH:Msparc*}
 PKG_SUGGESTED_OPTIONS+=	llvm-target-sparc
-.elif !empty(MACHINE_ARCH:Mpowerpc*)
+.elif ${MACHINE_ARCH:Mpowerpc*}
 PKG_SUGGESTED_OPTIONS+=	llvm-target-powerpc
-.elif !empty(MACHINE_ARCH:Maarch64)
+.elif ${MACHINE_ARCH} == aarch64
 PKG_SUGGESTED_OPTIONS+=	llvm-target-aarch64
-.elif !empty(MACHINE_ARCH:Mearm*)
+.elif ${MACHINE_ARCH:Mearm*}
 PKG_SUGGESTED_OPTIONS+=	llvm-target-arm
-.elif !empty(MACHINE_ARCH:M*mips*)
+.elif ${MACHINE_ARCH:M*mips*}
 PKG_SUGGESTED_OPTIONS+=	llvm-target-mips
 .else
 # X86 and everyone else get all targets by default.
