ede46304c75265550d97a3c674669b61478612be
pho <pho@pkgsrc.org>
2023-01-24 17:14:46 +0000

    textproc/hs-rope-utf16-splay: Update to 0.4.0.0
    0.4.0.0
    * Support text-2.0 and up without backward compatibility

--- textproc/hs-rope-utf16-splay/buildlink3.mk
+++ textproc/hs-rope-utf16-splay/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/24 17:14:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-rope-utf16-splay
 
 .if !defined(HS_ROPE_UTF16_SPLAY_BUILDLINK3_MK)
 HS_ROPE_UTF16_SPLAY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-rope-utf16-splay+=	hs-rope-utf16-splay>=0.3.2
-BUILDLINK_ABI_DEPENDS.hs-rope-utf16-splay+=	hs-rope-utf16-splay>=0.3.2.0nb1
+BUILDLINK_API_DEPENDS.hs-rope-utf16-splay+=	hs-rope-utf16-splay>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-rope-utf16-splay+=	hs-rope-utf16-splay>=0.4.0.0
 BUILDLINK_PKGSRCDIR.hs-rope-utf16-splay?=	../../textproc/hs-rope-utf16-splay
 .endif	# HS_ROPE_UTF16_SPLAY_BUILDLINK3_MK
 
