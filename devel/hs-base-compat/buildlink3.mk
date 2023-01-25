d53994312d82bb2b2e0a5944906c64c8840ef175
pho <pho@pkgsrc.org>
2023-01-25 09:32:19 +0000

    devel/hs-base-compat: Update BUILDLINK_{API,ABI}_DEPENDS

--- devel/hs-base-compat/buildlink3.mk
+++ devel/hs-base-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:57:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/25 09:32:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat
 
 .if !defined(HS_BASE_COMPAT_BUILDLINK3_MK)
 HS_BASE_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.1
-BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.1nb2
+BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.2
+BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.2
 BUILDLINK_PKGSRCDIR.hs-base-compat?=	../../devel/hs-base-compat
 .endif	# HS_BASE_COMPAT_BUILDLINK3_MK
 
