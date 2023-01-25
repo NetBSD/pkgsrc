e8cc6d3432c0035ba65ef1f2ed49e2413bfd7b57
pho <pho@pkgsrc.org>
2023-01-25 01:52:55 +0000

    devel/hs-unix-compat: Update to 0.6
    Version 0.6 (2022-05-22)
    * Better support for symbolic links

--- devel/hs-unix-compat/buildlink3.mk
+++ devel/hs-unix-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/25 01:52:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-compat
 
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.4
-BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.4nb4
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.6
+BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.6
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../devel/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
