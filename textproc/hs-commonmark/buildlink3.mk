2fbf429bcae2d0ec976d9f156798819a13ac1cc1
pho <pho@pkgsrc.org>
2023-01-25 03:43:28 +0000

    textproc/hs-commonmark: 0.2.2
    0.2.2
    * Blocks: export getParentListType [API change].
    * Require unicode-data >= 0.3.
    * Change mkFormattingSpecMap so it integrates different FormattingSpecs
      that use the same character (#87). Otherwise we have problems if you have
      one formatting spec that reacts to single delimiters and another that
      reacts to pairs; if the first fails to match, the fallback behavior is
      produced and the second never matches.
    * Use unicode-data's faster versions of Data.Char functions. This speeds up
      benchmarks for tokenize considerably; little difference in other
      benchmarks. unicode-data is already a transitive dependency, via
      unicode-transforms.
    * Increase strictness in tokenize/go.
    * Remove legacy cpp needed to support ghc < 8.4.

--- textproc/hs-commonmark/buildlink3.mk
+++ textproc/hs-commonmark/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/25 03:43:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark
 
 .if !defined(HS_COMMONMARK_BUILDLINK3_MK)
 HS_COMMONMARK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.1.1nb2
+BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-commonmark?=	../../textproc/hs-commonmark
 
+.include "../../textproc/hs-unicode-data/buildlink3.mk"
 .include "../../textproc/hs-unicode-transforms/buildlink3.mk"
 .endif	# HS_COMMONMARK_BUILDLINK3_MK
 
