4da8da4551c989867fd0128ce631b69926f3073a
pho <pho@pkgsrc.org>
2023-01-25 09:33:44 +0000

    devel/hs-base-compat-batteries: Update BUILDLINK_{API,ABI}_DEPENDS

--- devel/hs-base-compat-batteries/buildlink3.mk
+++ devel/hs-base-compat-batteries/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:57:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/25 09:33:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat-batteries
 
 .if !defined(HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK)
 HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.12.1
-BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.12.1nb2
+BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.12.2
+BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.12.2
 BUILDLINK_PKGSRCDIR.hs-base-compat-batteries?=		../../devel/hs-base-compat-batteries
 
 .include "../../devel/hs-base-compat/buildlink3.mk"
