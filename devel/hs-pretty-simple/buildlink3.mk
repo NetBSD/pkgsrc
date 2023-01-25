49dc959a4cc6c08367485fab35dcc260d5e3dbd6
pho <pho@pkgsrc.org>
2023-01-25 19:19:27 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- devel/hs-pretty-simple/buildlink3.mk
+++ devel/hs-pretty-simple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/09/07 06:50:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/25 19:19:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-pretty-simple
 
 .if !defined(HS_PRETTY_SIMPLE_BUILDLINK3_MK)
 HS_PRETTY_SIMPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.0.0
-BUILDLINK_ABI_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.0.0.0nb5
+BUILDLINK_API_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.1.2
+BUILDLINK_ABI_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.1.2.0
 BUILDLINK_PKGSRCDIR.hs-pretty-simple?=		../../devel/hs-pretty-simple
 
 .include "../../textproc/hs-prettyprinter/buildlink3.mk"
