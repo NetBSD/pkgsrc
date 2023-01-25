b4ef5ae5f6d8096cf9aed9faec3436c099ece19d
pho <pho@pkgsrc.org>
2023-01-25 16:23:22 +0000

    lang/hs-hslua-marshalling: Update to 2.2.1
    hslua-marshalling-2.2.1 - Released 2022-06-19
    * Require hslua-core-2.2.1.
    * Relax upper bound for mtl, allow mtl-2.3.
    
    hslua-marshalling-2.2.0 - Released 2022-02-19
    * Result is now an instance of Foldable and Traversable. Both Result and
      Peek are made instances of MonadPlus.
    * Require hslua-core-2.2.0.

--- lang/hs-hslua-marshalling/buildlink3.mk
+++ lang/hs-hslua-marshalling/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/25 16:23:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-marshalling
 
 .if !defined(HS_HSLUA_MARSHALLING_BUILDLINK3_MK)
 HS_HSLUA_MARSHALLING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.2.1
 BUILDLINK_PKGSRCDIR.hs-hslua-marshalling?=	../../lang/hs-hslua-marshalling
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
