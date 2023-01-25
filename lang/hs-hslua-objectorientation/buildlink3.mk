a16578ef81800b6ff2118bce6ff14c557c7a20be
pho <pho@pkgsrc.org>
2023-01-25 17:38:00 +0000

    lang/hs-hslua-objectorientation: Update to 2.2.1
    hslua-objectorientation-2.2.1 - Released 2022-06-19
    * Require hslua-core-2.2.1.
    * Require hslua-marshalling-2.2.1.
    
    hslua-objectorientation-2.2.0.1 - Released 2022-05-20
    * Relax upper bound for mtl, allow mtl-2.3.
    
    hslua-objectorientation-2.2.0 - Released 2022-02-19
    * Require version 2.2 of hslua-core and hslua-marshalling.

--- lang/hs-hslua-objectorientation/buildlink3.mk
+++ lang/hs-hslua-objectorientation/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/25 17:38:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-objectorientation
 
 .if !defined(HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK)
 HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.2.1
 BUILDLINK_PKGSRCDIR.hs-hslua-objectorientation?=	../../lang/hs-hslua-objectorientation
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
