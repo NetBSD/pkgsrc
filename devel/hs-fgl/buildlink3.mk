7157a1437ba5bb612429854fef4f3dfddcf1cfc5
pho <pho@pkgsrc.org>
2023-01-25 01:58:56 +0000

    devel/hs-fgl: Update to 5.8
    5.8.0.0
    -------
    * Breaking change: MonadFail is no longer a superclass of GraphM.
      This is to support GHC 9.4.  This has no effect on the IO and ST
      instances of GraphM, but may affect users.

--- devel/hs-fgl/buildlink3.mk
+++ devel/hs-fgl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:57:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/25 01:58:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-fgl
 
 .if !defined(HS_FGL_BUILDLINK3_MK)
 HS_FGL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.7.0
-BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.7.0.3nb4
+BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.8.0
+BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.8.0.0
 BUILDLINK_PKGSRCDIR.hs-fgl?=	../../devel/hs-fgl
 .endif	# HS_FGL_BUILDLINK3_MK
 
