8714bbebdfd129369e41a3bb3c829acd85075f9b
pho <pho@pkgsrc.org>
2023-01-25 04:30:49 +0000

    devel/hs-lpeg: Update to 1.0.3
    lpeg-1.0.3 - Released 2022-01-29
    * Allow lua-2.2.*.

--- devel/hs-lpeg/buildlink3.mk
+++ devel/hs-lpeg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/25 04:30:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-lpeg
 
 .if !defined(HS_LPEG_BUILDLINK3_MK)
 HS_LPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lpeg+=	hs-lpeg>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-lpeg+=	hs-lpeg>=1.0.2nb2
+BUILDLINK_API_DEPENDS.hs-lpeg+=	hs-lpeg>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-lpeg+=	hs-lpeg>=1.0.3
 BUILDLINK_PKGSRCDIR.hs-lpeg?=	../../devel/hs-lpeg
 
 .include "../../lang/hs-lua/buildlink3.mk"
