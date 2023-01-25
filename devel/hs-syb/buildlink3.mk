821b22dd3e42d9a3c2828d3675bb2da1b8ae9ce8
pho <pho@pkgsrc.org>
2023-01-25 04:25:11 +0000

    devel/hs-syb: Update to 0.7.2.2
    * 0.7.2.2: Compatibility with GHC 9.4

--- devel/hs-syb/buildlink3.mk
+++ devel/hs-syb/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/02/26 03:58:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/01/25 04:25:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-syb
 
 .if !defined(HS_SYB_BUILDLINK3_MK)
 HS_SYB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.7.2.1
-BUILDLINK_ABI_DEPENDS.hs-syb+=	hs-syb>=0.7.2.1nb4
+BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.7.2
+BUILDLINK_ABI_DEPENDS.hs-syb+=	hs-syb>=0.7.2.2
 BUILDLINK_PKGSRCDIR.hs-syb?=	../../devel/hs-syb
 .endif	# HS_SYB_BUILDLINK3_MK
 
