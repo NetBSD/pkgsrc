6ea63cdd4f44602c72d06e7d9b6c7f57540d57b2
pho <pho@pkgsrc.org>
2023-01-25 14:20:17 +0000

    devel/hs-microlens-th: Update to 0.4.3.11
    No release notes have been provided by upstream.

--- devel/hs-microlens-th/buildlink3.mk
+++ devel/hs-microlens-th/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/25 14:20:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-microlens-th
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-microlens-th
 HS_MICROLENS_TH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3
-BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.10nb2
+BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.11
 BUILDLINK_PKGSRCDIR.hs-microlens-th?=	../../devel/hs-microlens-th
 
 .include "../../devel/hs-microlens/buildlink3.mk"
