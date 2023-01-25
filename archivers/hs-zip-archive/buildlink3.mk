2b711d21998ec5b1830c9135f8629b08448a34f8
pho <pho@pkgsrc.org>
2023-01-25 09:53:52 +0000

    archivers/hs-zip-archive: Update to 0.4.2.2
    zip-archive 0.4.2.2
    
      * Use `command -v` before trying `which` in the test suite (#62).
        `command` is a bash builtin, but for busybox we'll need `which`.
    
    zip-archive 0.4.2.1
    
      * Fix Windows build regression (#61).
    
    zip-archive 0.4.2
    
      * Fix problem with files with colon (#89).
      * Remove build-tools.  This was used to indicate that the 'unzip'
        executable was needed for testing, but it was never intended to be used
        this way and now the field is deprecated.  The current test suite
        simply skips the test using the unzip executable (with a warning) if
        'unzip' is not in the path.
      * Remove existing symlinks when extracting zip files with symlinks (#60,
        Vikrem).  Previously, writeEntry would raise an error if it tried to
        create a symlink and a symlink already existed at that path.  This
        behavior was inconsistent with its behavior for regular files, which
        it overwrote without comment.  This commit causes symlinks to be replaced
        by writeEntry instead of an error being raised.
      * Remove binary < 0.6 CPP.  It's no longer needed because we don't support
        binary < 0.6.  Also use manySig instead of many, to get better error
        messages.
      * Add type annotation for printf.
      * Better checking for unsafe paths (#55).  This method allows things like
        `foo/bar/../../baz`.
      * Require base >= 4.5 (#56)
      * Add GitHub CI.

--- archivers/hs-zip-archive/buildlink3.mk
+++ archivers/hs-zip-archive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:57:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/25 09:53:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-zip-archive
 
 .if !defined(HS_ZIP_ARCHIVE_BUILDLINK3_MK)
 HS_ZIP_ARCHIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.1nb4
+BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.2.2
 BUILDLINK_PKGSRCDIR.hs-zip-archive?=	../../archivers/hs-zip-archive
 
 .include "../../security/hs-digest/buildlink3.mk"
