d7104bebfdd196d6c2662a59da417a84c35b4e52
pho <pho@pkgsrc.org>
2023-01-25 13:59:39 +0000

    devel/hs-retry: Update to 0.9.3.0
    0.9.3.0
    * Add UnliftIO.Retry PR 81
    
    0.9.2.1
    * Use explicit import for lift which allows for mtl-2.3 compatibility PR 80
    
    0.9.2.0
    * Add retryOnError PR 44

--- devel/hs-retry/buildlink3.mk
+++ devel/hs-retry/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/25 13:59:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-retry
 
 .if !defined(HS_RETRY_BUILDLINK3_MK)
 HS_RETRY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-retry+=	hs-retry>=0.9.1
-BUILDLINK_ABI_DEPENDS.hs-retry+=	hs-retry>=0.9.1.0nb1
+BUILDLINK_API_DEPENDS.hs-retry+=	hs-retry>=0.9.3
+BUILDLINK_ABI_DEPENDS.hs-retry+=	hs-retry>=0.9.3.0
 BUILDLINK_PKGSRCDIR.hs-retry?=		../../devel/hs-retry
 
+.include "../../devel/hs-mtl-compat/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
 .endif	# HS_RETRY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-retry
