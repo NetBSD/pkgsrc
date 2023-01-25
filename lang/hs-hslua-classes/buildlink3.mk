6d28c5b547da4161d6d2ad52e19a91c50b5e616d
pho <pho@pkgsrc.org>
2023-01-25 17:40:05 +0000

    lang/hs-hslua-classes: Update to 2.2.0
    hslua-classes-2.2.0 - Released 2022-02-19
    * Relaxed upper bounds, allowing hslua-core-2.2.0 and
      hslua-marshalling-2.2.0.

--- lang/hs-hslua-classes/buildlink3.mk
+++ lang/hs-hslua-classes/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/25 17:40:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-classes
 
 .if !defined(HS_HSLUA_CLASSES_BUILDLINK3_MK)
 HS_HSLUA_CLASSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.2.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.2.0
 BUILDLINK_PKGSRCDIR.hs-hslua-classes?=		../../lang/hs-hslua-classes
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
