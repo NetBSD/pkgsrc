a152b26bfbe2f60d2d1fb4b373235edf550e0121
pho <pho@pkgsrc.org>
2023-01-25 16:52:53 +0000

    wm/xmonad: Update to 0.17.1
    0.17.1 (September 3, 2021)
    
    Enhancements
    * Added custom cursor shapes for resizing and moving windows.
    * Exported cacheNumlockMask and mkGrabs from XMonad.Operations.
    
    Bug Fixes
    * Fixed border color of windows with alpha channel. Now all windows have
      the same opaque border color.
    * Change the main loop to try to avoid GHC bug 21708 on systems running GHC
      9.2 up to version 9.2.3. The issue has been fixed in GHC 9.2.4 and all
      later releases.

--- wm/xmonad/buildlink3.mk
+++ wm/xmonad/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.12 2022/02/26 03:58:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/01/25 16:52:53 pho Exp $
 
 BUILDLINK_TREE+=	xmonad
 
 .if !defined(XMONAD_BUILDLINK3_MK)
 XMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.17.0
-BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.17.0nb2
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.17.1
+BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.17.1
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wm/xmonad
 
-.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
 .include "../../devel/hs-setlocale/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
