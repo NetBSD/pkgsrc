e895d7a12ea445561dd36a61473d353b2cc712c1
pho <pho@pkgsrc.org>
2023-01-25 14:32:57 +0000

    devel/hs-foundation: Update to 0.0.29
    No release notes have been provided by upstream.

--- devel/hs-foundation/buildlink3.mk
+++ devel/hs-foundation/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:57:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/25 14:32:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-foundation
 
 .if !defined(HS_FOUNDATION_BUILDLINK3_MK)
 HS_FOUNDATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26
-BUILDLINK_ABI_DEPENDS.hs-foundation+=	hs-foundation>=0.0.26.1nb2
+BUILDLINK_API_DEPENDS.hs-foundation+=	hs-foundation>=0.0.29
+BUILDLINK_ABI_DEPENDS.hs-foundation+=	hs-foundation>=0.0.29
 BUILDLINK_PKGSRCDIR.hs-foundation?=	../../devel/hs-foundation
 
 .include "../../devel/hs-basement/buildlink3.mk"
