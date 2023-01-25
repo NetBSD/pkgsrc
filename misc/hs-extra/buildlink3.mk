545433b7a0ceb9c44eeb33357f4f1d56951a896b
pho <pho@pkgsrc.org>
2023-01-25 01:22:35 +0000

    misc/hs-extra: Update to 1.7.12
    1.7.12, released 2022-09-02
    * #98, make both lazy in its argument
    * #98, make first3,second3,third3 lazy in their argument
    * #98, make firstM,secondM lazy in their argument
    * #86, add notNull for Foldable
    
    1.7.11, released 2022-08-21
    * #95, add List.repeatedlyNE and NonEmpty.repeatedly
    * #94, add groupOnKey
    * #91, add foldl1' to NonEmpty

--- misc/hs-extra/buildlink3.mk
+++ misc/hs-extra/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/09/07 06:50:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/25 01:22:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-extra
 
 .if !defined(HS_EXTRA_BUILDLINK3_MK)
 HS_EXTRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.7.10
-BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.7.10nb3
+BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.7.12
+BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.7.12
 BUILDLINK_PKGSRCDIR.hs-extra?=		../../misc/hs-extra
 
 .include "../../time/hs-clock/buildlink3.mk"
