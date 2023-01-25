f62f74c93b4c3a38c9c77de74a14c52c6f06f442
pho <pho@pkgsrc.org>
2023-01-25 14:38:09 +0000

    devel/hs-resourcet: Update to 1.3.0
    1.3.0
    * Include the exception in ReleaseTypes indicating exceptional exit.
    * Only backwards-incompatible in code relying on instances of ReleaseType
      other than Show, or constructing ReleaseException directly.
    
    1.2.6
    * Add allocateU #490
    
    1.2.5
    * Support transformers-0.6 / mtl-2.3

--- devel/hs-resourcet/buildlink3.mk
+++ devel/hs-resourcet/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2022/02/26 03:58:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/01/25 14:38:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4
-BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.4.3nb2
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.3.0
+BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.3.0
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 
 .include "../../devel/hs-primitive/buildlink3.mk"
