22f85ddd32a9611691900c3886240fe0eb53e9a7
pho <pho@pkgsrc.org>
2023-01-25 16:48:33 +0000

    x11/hs-X11: Update to 1.10.3
    1.10.3 (2022-09-14)
    * Added cWX, cWY, cWWidth, cWHeight constants (AttributeMask) (#82)
    * Added FocusChangeEvent to data Event (#81)
    * Added setWMNormalHints (#83)

--- x11/hs-X11/buildlink3.mk
+++ x11/hs-X11/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.13 2022/02/26 03:58:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/01/25 16:48:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-X11
 
 .if !defined(HS_X11_BUILDLINK3_MK)
 HS_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-X11+=	hs-X11>=1.10.2
-BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.10.2nb2
+BUILDLINK_API_DEPENDS.hs-X11+=	hs-X11>=1.10.3
+BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.10.3
 BUILDLINK_PKGSRCDIR.hs-X11?=	../../x11/hs-X11
 
-.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXrandr/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
