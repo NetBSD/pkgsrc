84ad71512903cf68a33beae26925c52e5be815a5
pho <pho@pkgsrc.org>
2023-01-25 13:53:30 +0000

    devel/hs-th-env: Update to 0.1.1
    0.1.1 - 2022-05-23
    
    Added
    * #6 Add envQ' variant that requires the environment variable to be set at
       compilation time

--- devel/hs-th-env/buildlink3.mk
+++ devel/hs-th-env/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/25 13:53:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-env
 
 .if !defined(HS_TH_ENV_BUILDLINK3_MK)
 HS_TH_ENV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-env+=	hs-th-env>=0.1.0.3
-BUILDLINK_ABI_DEPENDS.hs-th-env+=	hs-th-env>=0.1.0.3nb1
+BUILDLINK_API_DEPENDS.hs-th-env+=	hs-th-env>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-th-env+=	hs-th-env>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-th-env?=		../../devel/hs-th-env
 
 .include "../../devel/hs-th-compat/buildlink3.mk"
