a1f3602e1131e4fc6a6d56f1d3837e973c33dd97
pho <pho@pkgsrc.org>
2023-01-25 13:36:37 +0000

    devel/hs-split: Update to 0.2.3.5
    * 0.2.3.5 (11 August 2022)
    
      - Change base upper bound to < 5.
      - Test with GHC 9.2 and 9.4.

--- devel/hs-split/buildlink3.mk
+++ devel/hs-split/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/02/26 03:58:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/01/25 13:36:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.3.4
-BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.3.4nb4
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.3.5
 BUILDLINK_PKGSRCDIR.hs-split?=		../../devel/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
