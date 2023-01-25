7600f8aacc331b52c33d328a76bb85249e4fe07e
pho <pho@pkgsrc.org>
2023-01-25 09:43:43 +0000

    security/hs-digest: Update to 0.0.1.5
    0.0.1.5 — 24th Jan 2023
    * Correct license field in .cabal file (Thanks to @juhp):
      https://github.com/TeofilC/digest/pull/9
    * Use https for git source repository (Thanks to @felixonmars):
      https://github.com/TeofilC/digest/pull/8
    
    0.0.1.4 — 22th Dec 2022
    * Use pkgconfig to find zlib (Thanks to @bgamari):
      https://github.com/jkff/digest/pull/5

--- security/hs-digest/buildlink3.mk
+++ security/hs-digest/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/02/26 03:58:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/01/25 09:43:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-digest
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-digest
 HS_DIGEST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.1
-BUILDLINK_ABI_DEPENDS.hs-digest+=	hs-digest>=0.0.1.3nb2
+BUILDLINK_ABI_DEPENDS.hs-digest+=	hs-digest>=0.0.1.5
 BUILDLINK_PKGSRCDIR.hs-digest?=		../../security/hs-digest
 
 .include "../../devel/zlib/buildlink3.mk"
