6ecd5296f19d47eb62542102db4c7d2be3ed4955
pho <pho@pkgsrc.org>
2023-01-25 09:24:45 +0000

    devel/hs-hashable: Update to 1.4.2.0
    Version 1.4.2.0
    * Fix the foreign signature of getThreadId
      https://github.com/haskell-unordered-containers/hashable/pull/263
    * Drop support for GHCs prior GHC-8.2 The recent unordered-containers
      releases support only GHC-8.2+
    * Add instance for OsString, PosixString, WindowsString from
      filepath-1.4.100.1
    * Add Hashable ByteArray instance using data-array-byte compat package

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2022/02/26 03:57:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2023/01/25 09:24:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.0
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.0.2nb2
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.2
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.2.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
