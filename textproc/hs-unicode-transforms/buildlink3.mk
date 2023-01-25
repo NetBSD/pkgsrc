32566ebbbc5b1540feb347382c53e40af594d3a6
pho <pho@pkgsrc.org>
2023-01-25 02:26:37 +0000

    textproc/hs-unicode-transforms: Update to 0.4.0.1
    0.4.0.1 (March 2022)
    * Support text-icu == 0.8.* in tests
    * Support unicode-data == 0.3.*

--- textproc/hs-unicode-transforms/buildlink3.mk
+++ textproc/hs-unicode-transforms/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/25 02:26:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-transforms
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-unicode-transforms
 HS_UNICODE_TRANSFORMS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.4.0
-BUILDLINK_ABI_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.4.0nb2
+BUILDLINK_ABI_DEPENDS.hs-unicode-transforms+=	hs-unicode-transforms>=0.4.0.1
 BUILDLINK_PKGSRCDIR.hs-unicode-transforms?=	../../textproc/hs-unicode-transforms
 
 .include "../../textproc/hs-unicode-data/buildlink3.mk"
