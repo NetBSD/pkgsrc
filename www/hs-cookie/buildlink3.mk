5a18612832ad2ee6e0631aa3ff6fe835b4729f96
pho <pho@pkgsrc.org>
2023-01-25 10:00:29 +0000

    www/hs-cookie: Update to 0.4.6
    0.4.6
    * Resolve redundant import of Data.Monoid #26
    * Added renderSetCookieBS and renderCookiesBS

--- www/hs-cookie/buildlink3.mk
+++ www/hs-cookie/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:58:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/25 10:00:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-cookie
 
 .if !defined(HS_COOKIE_BUILDLINK3_MK)
 HS_COOKIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cookie+=	hs-cookie>=0.4.5
-BUILDLINK_ABI_DEPENDS.hs-cookie+=	hs-cookie>=0.4.5nb4
+BUILDLINK_API_DEPENDS.hs-cookie+=	hs-cookie>=0.4.6
+BUILDLINK_ABI_DEPENDS.hs-cookie+=	hs-cookie>=0.4.6
 BUILDLINK_PKGSRCDIR.hs-cookie?=		../../www/hs-cookie
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
