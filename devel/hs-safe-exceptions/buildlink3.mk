7b4d11b30e5282c2ff977421155b251117fdb368
pho <pho@pkgsrc.org>
2023-01-25 01:26:27 +0000

    devel/hs-safe-exceptions: Update to 0.1.7.3
    0.1.7.3
    * Allow transformers 0.6 #39

--- devel/hs-safe-exceptions/buildlink3.mk
+++ devel/hs-safe-exceptions/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/25 01:26:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-safe-exceptions
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-safe-exceptions
 HS_SAFE_EXCEPTIONS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-safe-exceptions+=	hs-safe-exceptions>=0.1.7
-BUILDLINK_ABI_DEPENDS.hs-safe-exceptions+=	hs-safe-exceptions>=0.1.7.2nb1
+BUILDLINK_ABI_DEPENDS.hs-safe-exceptions+=	hs-safe-exceptions>=0.1.7.3
 BUILDLINK_PKGSRCDIR.hs-safe-exceptions?=	../../devel/hs-safe-exceptions
 .endif	# HS_SAFE_EXCEPTIONS_BUILDLINK3_MK
 
