191c0d5dd9be165de136668f73be80d92fde6ac9
pho <pho@pkgsrc.org>
2023-01-25 18:58:07 +0000

    devel/hs-th-lift-instances: Update to 0.1.20
    No release notes have been provided by upstream.

--- devel/hs-th-lift-instances/buildlink3.mk
+++ devel/hs-th-lift-instances/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/08/20 08:08:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/25 18:58:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-lift-instances
 
 .if !defined(HS_TH_LIFT_INSTANCES_BUILDLINK3_MK)
 HS_TH_LIFT_INSTANCES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-lift-instances+=	hs-th-lift-instances>=0.1.19
-BUILDLINK_ABI_DEPENDS.hs-th-lift-instances+=	hs-th-lift-instances>=0.1.19nb3
+BUILDLINK_API_DEPENDS.hs-th-lift-instances+=	hs-th-lift-instances>=0.1.20
+BUILDLINK_ABI_DEPENDS.hs-th-lift-instances+=	hs-th-lift-instances>=0.1.20
 BUILDLINK_PKGSRCDIR.hs-th-lift-instances?=	../../devel/hs-th-lift-instances
 
 .include "../../devel/hs-th-lift/buildlink3.mk"
