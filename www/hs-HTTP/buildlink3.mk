94d45475f168a5d971b7af37abc6876e51cffd14
pho <pho@pkgsrc.org>
2023-01-25 15:19:11 +0000

    www/hs-HTTP: Update to 4000.4.1
    Version 4000.4.0: release 2022-02-22
     * Restrict to GHC >=7.6 and associated cleanups (Andreas Abel)
     * Drop deprecated instance Error (Andreas Abel)
     * Preparation for mtl-2.3 (Andreas Abel)
     * General tidying (Andreas Abel)
     * Support GHC 9.2 (Bodigrim)

--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2022/02/26 03:58:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2023/01/25 15:19:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.16
-BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.16nb4
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.4.1
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.4.1
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
 .include "../../net/hs-network/buildlink3.mk"
