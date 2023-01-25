4a917daf916467dec48c75191c4df1fcf9447323
pho <pho@pkgsrc.org>
2023-01-25 09:12:18 +0000

    devel/hs-th-abstraction: Update to 0.4.5.0
    0.4.5.0 -- 2022.09.12
    * Fix a bug in which data family declarations with interesting return kinds
      (e.g., data family F :: Type -> Type) would be reified incorrectly when
      using reifyDatatype.

--- devel/hs-th-abstraction/buildlink3.mk
+++ devel/hs-th-abstraction/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/25 09:12:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-abstraction
 
 .if !defined(HS_TH_ABSTRACTION_BUILDLINK3_MK)
 HS_TH_ABSTRACTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.3
-BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.3.0nb2
+BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.5
+BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.5.0
 BUILDLINK_PKGSRCDIR.hs-th-abstraction?=		../../devel/hs-th-abstraction
 .endif	# HS_TH_ABSTRACTION_BUILDLINK3_MK
 
