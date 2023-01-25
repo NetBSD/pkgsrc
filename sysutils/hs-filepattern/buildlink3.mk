b8a0993f785d27630d47fa66b1bc7203950d2bb0
pho <pho@pkgsrc.org>
2023-01-25 14:41:27 +0000

    sysutils/hs-filepattern: Update to 0.1.3
    0.1.3, released 2022-08-21
    * #5, remove invalid optimisation in the presence of symlinks

--- sysutils/hs-filepattern/buildlink3.mk
+++ sysutils/hs-filepattern/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/09/07 06:50:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/25 14:41:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-filepattern
 
 .if !defined(HS_FILEPATTERN_BUILDLINK3_MK)
 HS_FILEPATTERN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-filepattern+=	hs-filepattern>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-filepattern+=	hs-filepattern>=0.1.2nb4
+BUILDLINK_API_DEPENDS.hs-filepattern+=	hs-filepattern>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-filepattern+=	hs-filepattern>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-filepattern?=	../../sysutils/hs-filepattern
 
 .include "../../misc/hs-extra/buildlink3.mk"
