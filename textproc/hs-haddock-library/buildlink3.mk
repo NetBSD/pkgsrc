46b677451d62da3bb34f0b2cfd786f5434e2a049
pho <pho@pkgsrc.org>
2023-01-25 03:35:57 +0000

    textproc/hs-haddock-library: Update to 1.11.0
    Changes in version 1.11.0
    * Add support for linking identifiers with a quote between backticks (#1408)

--- textproc/hs-haddock-library/buildlink3.mk
+++ textproc/hs-haddock-library/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/25 03:35:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-haddock-library
 
 .if !defined(HS_HADDOCK_LIBRARY_BUILDLINK3_MK)
 HS_HADDOCK_LIBRARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.10.0
-BUILDLINK_ABI_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.10.0nb4
+BUILDLINK_API_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.11.0
+BUILDLINK_ABI_DEPENDS.hs-haddock-library+=	hs-haddock-library>=1.11.0
 BUILDLINK_PKGSRCDIR.hs-haddock-library?=	../../textproc/hs-haddock-library
 .endif	# HS_HADDOCK_LIBRARY_BUILDLINK3_MK
 
