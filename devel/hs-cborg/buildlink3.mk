8e236eb77e78978aeafcd869489abd463f483a2d
pho <pho@pkgsrc.org>
2023-01-25 16:27:12 +0000

    devel/hs-cborg: Update to 0.2.8.0
    0.2.8.0 -- 2022-09-24
    * Support GHC 9.4
    * Fix compatibility with primitive 0.7.4.0
    * Drop GHC 8.0 and 8.2 support
    * Support aeson 2.1

--- devel/hs-cborg/buildlink3.mk
+++ devel/hs-cborg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/25 16:27:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-cborg
 
 .if !defined(HS_CBORG_BUILDLINK3_MK)
 HS_CBORG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cborg+=	hs-cborg>=0.2.6
-BUILDLINK_ABI_DEPENDS.hs-cborg+=	hs-cborg>=0.2.6.0nb1
+BUILDLINK_API_DEPENDS.hs-cborg+=	hs-cborg>=0.2.8
+BUILDLINK_ABI_DEPENDS.hs-cborg+=	hs-cborg>=0.2.8.0
 BUILDLINK_PKGSRCDIR.hs-cborg?=		../../devel/hs-cborg
 
 .include "../../devel/hs-half/buildlink3.mk"
