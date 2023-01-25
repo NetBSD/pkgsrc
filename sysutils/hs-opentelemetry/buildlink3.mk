4ef4c1060c8426d8060c4016e4ea1297e4849984
pho <pho@pkgsrc.org>
2023-01-25 14:25:20 +0000

    sysutils/hs-opentelemetry: Update to 0.8.0
    No release notes have been provided by upstream.

--- sysutils/hs-opentelemetry/buildlink3.mk
+++ sysutils/hs-opentelemetry/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/25 14:25:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-opentelemetry
 
 .if !defined(HS_OPENTELEMETRY_BUILDLINK3_MK)
 HS_OPENTELEMETRY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-opentelemetry+=	hs-opentelemetry>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-opentelemetry+=	hs-opentelemetry>=0.7.0nb1
+BUILDLINK_API_DEPENDS.hs-opentelemetry+=	hs-opentelemetry>=0.8.0
+BUILDLINK_ABI_DEPENDS.hs-opentelemetry+=	hs-opentelemetry>=0.8.0
 BUILDLINK_PKGSRCDIR.hs-opentelemetry?=		../../sysutils/hs-opentelemetry
 
 .include "../../devel/hs-ghc-trace-events/buildlink3.mk"
