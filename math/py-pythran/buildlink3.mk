e9f9e3570baf419d46c1863b563b122f03179a5e
adam <adam@pkgsrc.org>
2023-01-25 09:06:54 +0000

    py-pythran: fix building scipy

--- math/py-pythran/buildlink3.mk
+++ math/py-pythran/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/10/13 19:32:00 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/01/25 09:06:54 adam Exp $
 
 BUILDLINK_TREE+=	py-pythran
 
@@ -10,7 +10,7 @@ BUILDLINK_API_DEPENDS.py-pythran+=	${PYPKGPREFIX}-pythran>=0.10.0
 BUILDLINK_PKGSRCDIR.py-pythran?=	../../math/py-pythran
 BUILDLINK_DEPMETHOD.py-pythran?=	build
 
-BUILDLINK_CONTENTS_FILTER.py-pythran+=	${EGREP} '(\.h$$|\.hpp$$)'
+BUILDLINK_CONTENTS_FILTER.py-pythran=	${EGREP} '(\.h$$|\.hpp$$)'
 .endif	# PY_PYTHRAN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-pythran
