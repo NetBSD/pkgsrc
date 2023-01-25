515fd58fd637371aeeb58da0621bf9cd61aec4ed
pho <pho@pkgsrc.org>
2023-01-25 19:32:07 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- lang/hs-hslua-packaging/buildlink3.mk
+++ lang/hs-hslua-packaging/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:58:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/25 19:32:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-packaging
 
 .if !defined(HS_HSLUA_PACKAGING_BUILDLINK3_MK)
 HS_HSLUA_PACKAGING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.2.1
 BUILDLINK_PKGSRCDIR.hs-hslua-packaging?=	../../lang/hs-hslua-packaging
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
