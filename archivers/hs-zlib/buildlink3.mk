a71a41bd35f2d0f2083dd5e39106f3bb44db96bf
pho <pho@pkgsrc.org>
2023-01-25 09:48:10 +0000

    archivers/hs-zlib: Update to 0.6.3.0
    0.6.3.0 - Andrew Lelechenko - May 2022
     * Bump bundled zlib to 1.2.12, #48
     * Support base-4.17

--- archivers/hs-zlib/buildlink3.mk
+++ archivers/hs-zlib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/02/26 03:57:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/25 09:48:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-zlib
 
 .if !defined(HS_ZLIB_BUILDLINK3_MK)
 HS_ZLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.6.2
-BUILDLINK_ABI_DEPENDS.hs-zlib+=	hs-zlib>=0.6.2.3nb4
+BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.6.3
+BUILDLINK_ABI_DEPENDS.hs-zlib+=	hs-zlib>=0.6.3.0
 BUILDLINK_PKGSRCDIR.hs-zlib?=	../../archivers/hs-zlib
 
 .include "../../devel/zlib/buildlink3.mk"
