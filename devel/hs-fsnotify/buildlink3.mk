0e3acd32060610f309653d84c3058ff7c9d8ab1f
pho <pho@pkgsrc.org>
2023-01-26 15:08:26 +0000

    devel/hs-fsnotify: Update to 0.4.1.0
    Version 0.4.1.0
    * Add unliftio lower bound (#106)
    * Change the tests back to a test-suite to avoid building for library
      users. (#107)
    * Fix up Windows compatibility.
    * Export WatchConfig type (#108)
    
    Version 0.4.0.1
    * Fix compatibility with *BSD.
    
    Version 0.4.0.0
    API breaking update.
    * New options for threading control (single-threaded, thread-per-watch, and
      thread-per-manager)
    * Revamp WatchConfig options to be less confusing and reduce boolean
      blindness.
    * Pull out debouncing stuff, since it was never correct as it simply took
      the last event affecting a given file in the debounce period. Debouncing
      is currently not included, and should be handled as an orthogonal
      concern. I'd like to include some debouncing logic, but didn't want to
      delay this release any longer.
      - We now expose type DebounceFn = Action -> IO Action, which represents
        an arbitrary debouncer. All debouncers should be in the form of one of
        these functions.
      - A robust state machine debouncer is in progress but not fully
        implemented yet; see the state-machine branch.
      - Contributions are welcome! We can potentially add multiple debouncers
        of different complexity as modules under System.FSNotify.Debounce.*.
    * Don't silently fall back to polling on failure of native
      watcher. Instead, throw an exception which the user can recover from by
      switching to polling.
    * Add ModifiedAttributes event type + Linux support
    * Add confOnHandlerException to be able to control what happens when a
      handler throws an exception.
    * WatchConfig constructor is no longer exposed. Instead use defaultConfig
      {...} with the accessors.

--- devel/hs-fsnotify/buildlink3.mk
+++ devel/hs-fsnotify/buildlink3.mk
@@ -1,21 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 12:33:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/26 15:08:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-fsnotify
 
 .if !defined(HS_FSNOTIFY_BUILDLINK3_MK)
 HS_FSNOTIFY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.3.0.1nb5
+BUILDLINK_API_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.4.1
+BUILDLINK_ABI_DEPENDS.hs-fsnotify+=	hs-fsnotify>=0.4.1.0
 BUILDLINK_PKGSRCDIR.hs-fsnotify?=	../../devel/hs-fsnotify
 
 .include "../../mk/bsd.fast.prefs.mk"
-.if ${OPSYS} == "Linux" || !empty(OPSYS:M*BSD)
+.if ${OPSYS} == "Linux" || ${OPSYS:M*BSD}
 .  include "../../devel/hs-hinotify/buildlink3.mk"
 .  include "../../devel/hs-shelly/buildlink3.mk"
 .endif
 
 .include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../devel/hs-safe-exceptions/buildlink3.mk"
 .include "../../devel/hs-unix-compat/buildlink3.mk"
 .endif	# HS_FSNOTIFY_BUILDLINK3_MK
 
