bab5bb908a500e1b5c8f49bba12a8a0ced6626bf
pho <pho@pkgsrc.org>
2023-01-26 11:30:52 +0000

    devel/hs-strict: Fix build with GHC 9.4

--- devel/hs-strict/buildlink3.mk
+++ devel/hs-strict/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:58:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/26 11:30:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-strict
 
@@ -9,9 +9,9 @@ BUILDLINK_API_DEPENDS.hs-strict+=	hs-strict>=0.4.0
 BUILDLINK_ABI_DEPENDS.hs-strict+=	hs-strict>=0.4.0.1nb4
 BUILDLINK_PKGSRCDIR.hs-strict?=		../../devel/hs-strict
 
-.include "../../devel/hs-assoc/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-these/buildlink3.mk"
+.include "../../devel/hs-assoc/buildlink3.mk"
 .endif	# HS_STRICT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-strict
