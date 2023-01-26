ecbde8c47da748ee3059440e849aad73734882fa
pho <pho@pkgsrc.org>
2023-01-26 10:25:46 +0000

    devel/hs-tasty: Update to 1.4.3
    Version 1.4.3 - 2022-12-18
    * Drop unbounded-delays dependency on 64-bit machines (#344).
    * Drop clock dependency, decomission clock flag (#345).
    * Drop wcwidth dependency (#346)
    * Tested with GHC 8.0 - 9.4.
    
    Version 1.4.2.3 - 2022-05-10
    * Drop mtl dependency
    * Warning-free under GHC 8.0 - 9.2
    
    Version 1.4.2.2 - 2022-05-10
    * Fix compilation with mtl-2.3
    * Tested with GHC 8.0 - 9.2; dropped support for GHC 7.x

--- devel/hs-tasty/buildlink3.mk
+++ devel/hs-tasty/buildlink3.mk
@@ -1,20 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/09/07 06:50:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/26 10:25:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty
 
 .if !defined(HS_TASTY_BUILDLINK3_MK)
 HS_TASTY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.4.2
-BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.4.2.1nb3
+BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.4.3
+BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.4.3
 BUILDLINK_PKGSRCDIR.hs-tasty?=		../../devel/hs-tasty
 
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../devel/hs-optparse-applicative/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-unbounded-delays/buildlink3.mk"
-.include "../../time/hs-clock/buildlink3.mk"
-.include "../../textproc/hs-wcwidth/buildlink3.mk"
 .endif	# HS_TASTY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tasty
