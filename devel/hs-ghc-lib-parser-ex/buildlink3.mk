9de25f6b93185573157f4bff5db9f610df113f9b
pho <pho@pkgsrc.org>
2023-01-26 08:42:45 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- devel/hs-ghc-lib-parser-ex/buildlink3.mk
+++ devel/hs-ghc-lib-parser-ex/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/02/26 03:57:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/26 08:42:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-lib-parser-ex
 
 .if !defined(HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK)
 HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.0.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.2.0.1nb2
+BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.4.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.4.0.0
 BUILDLINK_PKGSRCDIR.hs-ghc-lib-parser-ex?=	../../devel/hs-ghc-lib-parser-ex
 
+.include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
 .include "../../devel/hs-uniplate/buildlink3.mk"
 .endif	# HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK
 
