4d902f289484917a3ce5c5b95019a65d48ec4454
pho <pho@pkgsrc.org>
2023-01-26 11:16:50 +0000

    math/hs-bifunctors: Update to 5.5.14
    5.5.14 [2022.12.07]
    * Define Functor, Foldable, and Traversable instances for Sum and Product.
    
    5.5.13 [2022.09.12]
    * Make the Biapplicative instances for tuples lazy, to match their
      Bifunctor instances.

--- math/hs-bifunctors/buildlink3.mk
+++ math/hs-bifunctors/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/26 11:16:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-bifunctors
 
 .if !defined(HS_BIFUNCTORS_BUILDLINK3_MK)
 HS_BIFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.11
-BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.11nb2
+BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.14
+BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.14
 BUILDLINK_PKGSRCDIR.hs-bifunctors?=	../../math/hs-bifunctors
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
 .include "../../math/hs-comonad/buildlink3.mk"
-.include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
 .endif	# HS_BIFUNCTORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-bifunctors
