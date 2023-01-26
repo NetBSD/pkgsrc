86fded94a61c4f2e52d67363b51d25567b7425bf
pho <pho@pkgsrc.org>
2023-01-26 09:27:44 +0000

    devel/hs-hslua-module-version: Update to 1.0.2
    hslua-module-system-1.0.2 - Released 2022-02-19
    * Adjusted package bounds, for hslua-core and hslua-packaging.

--- devel/hs-hslua-module-system/buildlink3.mk
+++ devel/hs-hslua-module-system/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:57:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/26 09:27:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-system
 
 .if !defined(HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK)
 HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.2
 BUILDLINK_PKGSRCDIR.hs-hslua-module-system?=	../../devel/hs-hslua-module-system
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
