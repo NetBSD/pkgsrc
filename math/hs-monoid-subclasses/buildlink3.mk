26c55d206852f5f2a0de765a8b1083d28dca288a
pho <pho@pkgsrc.org>
2023-01-26 03:38:37 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- math/hs-monoid-subclasses/buildlink3.mk
+++ math/hs-monoid-subclasses/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/26 03:38:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-monoid-subclasses
 
 .if !defined(HS_MONOID_SUBCLASSES_BUILDLINK3_MK)
 HS_MONOID_SUBCLASSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.1.3
-BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.1.3nb2
+BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.1
 BUILDLINK_PKGSRCDIR.hs-monoid-subclasses?=	../../math/hs-monoid-subclasses
 
+.include "../../math/hs-commutative-semigroups/buildlink3.mk"
 .include "../../math/hs-primes/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_MONOID_SUBCLASSES_BUILDLINK3_MK
