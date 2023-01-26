db0ba945458bcfdae597ddf71549ea2c4739b127
pho <pho@pkgsrc.org>
2023-01-26 11:11:15 +0000

    devel/hs-constraints-extras: Update to 0.4.0.0
    0.4.0.0 - 2022-11-18
    * Make Has from a type synonym into the class upon which everything else is
      based. Instances may define either has or argDict which are now both
      methods of Has. This should hopefully improve the readability of type
      errors involving the library a fair bit, as everything wanted to use Has,
      but it was defined in terms of the less commonly appearing ArgDict and
      ConstraintsFor.
    * The ConstraintsFor type family has been removed as it is now unnecessary,
      as instances of Has can simply be constrained directly. This has the
      added benefit of allowing QuantifiedConstraints in those instance heads
      that formerly would not have been allowed as part of the result of a type
      family.
    * The ArgDict class has also been removed, as it was also basically never
      used on its own.

--- devel/hs-constraints-extras/buildlink3.mk
+++ devel/hs-constraints-extras/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/26 11:11:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-constraints-extras
 
 .if !defined(HS_CONSTRAINTS_EXTRAS_BUILDLINK3_MK)
 HS_CONSTRAINTS_EXTRAS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-constraints-extras+=	hs-constraints-extras>=0.3.2
-BUILDLINK_ABI_DEPENDS.hs-constraints-extras+=	hs-constraints-extras>=0.3.2.1nb1
+BUILDLINK_API_DEPENDS.hs-constraints-extras+=	hs-constraints-extras>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-constraints-extras+=	hs-constraints-extras>=0.4.0.0
 BUILDLINK_PKGSRCDIR.hs-constraints-extras?=	../../devel/hs-constraints-extras
 
 .include "../../devel/hs-constraints/buildlink3.mk"
