87fcfc84dcfd561b7f6454328898985139f1b252
pho <pho@pkgsrc.org>
2023-01-26 03:26:34 +0000

    devel/hs-hashable: Fix build with GHC 9.2

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2023/01/25 09:24:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2023/01/26 03:26:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
@@ -8,6 +8,8 @@ HS_HASHABLE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.2
 BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.2.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
+
+.include "../../devel/hs-data-array-byte/buildlink3.mk"
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hashable
