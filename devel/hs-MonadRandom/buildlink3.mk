7e6fa7f58dcd125086c5df9d8a13d83e86e39a4d
pho <pho@pkgsrc.org>
2023-01-26 03:58:43 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- devel/hs-MonadRandom/buildlink3.mk
+++ devel/hs-MonadRandom/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/02/26 03:57:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/01/26 04:02:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-MonadRandom
 
 .if !defined(HS_MONADRANDOM_BUILDLINK3_MK)
 HS_MONADRANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.3
-BUILDLINK_ABI_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.3nb4
+BUILDLINK_API_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.6
+BUILDLINK_ABI_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.6
 BUILDLINK_PKGSRCDIR.hs-MonadRandom?=	../../devel/hs-MonadRandom
 
 .include "../../devel/hs-primitive/buildlink3.mk"
