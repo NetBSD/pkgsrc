25eb42788e72b66784be7576569cd2ce548e4891
pho <pho@pkgsrc.org>
2023-01-26 11:26:32 +0000

    databases/hs-sqlite-simple: Update to 0.4.18.2
    0.4.18.2
    * Depend on semigroups only for old GHC versions
    
    0.4.18.1
    * Add generic implementation of 'FromRow' and 'ToRow'.

--- databases/hs-sqlite-simple/buildlink3.mk
+++ databases/hs-sqlite-simple/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/11/23 16:18:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/26 11:26:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-sqlite-simple
 
@@ -6,16 +6,14 @@ BUILDLINK_TREE+=	hs-sqlite-simple
 HS_SQLITE_SIMPLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18
-BUILDLINK_ABI_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18.0nb5
+BUILDLINK_ABI_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18.2
 BUILDLINK_PKGSRCDIR.hs-sqlite-simple?=		../../databases/hs-sqlite-simple
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
 .include "../../devel/hs-blaze-textual/buildlink3.mk"
 .include "../../databases/hs-direct-sqlite/buildlink3.mk"
-.include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/hs-Only/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_SQLITE_SIMPLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-sqlite-simple
