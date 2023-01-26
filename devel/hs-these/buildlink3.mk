82ae43aab8f93f4c69dc7d32cec7f2da484bde68
pho <pho@pkgsrc.org>
2023-01-26 11:28:33 +0000

    devel/hs-these: Fix build with GHC 9.4

--- devel/hs-these/buildlink3.mk
+++ devel/hs-these/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/02/26 03:58:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/26 11:28:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-these
 
@@ -9,8 +9,8 @@ BUILDLINK_API_DEPENDS.hs-these+=	hs-these>=1.1.1
 BUILDLINK_ABI_DEPENDS.hs-these+=	hs-these>=1.1.1.1nb4
 BUILDLINK_PKGSRCDIR.hs-these?=		../../devel/hs-these
 
-.include "../../devel/hs-assoc/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-assoc/buildlink3.mk"
 .endif	# HS_THESE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-these
