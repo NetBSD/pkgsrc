e42af0ff3de9de7ebbe3a123e7e5d53fc84a5733
pho <pho@pkgsrc.org>
2023-01-26 09:03:02 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- www/hs-http-client/buildlink3.mk
+++ www/hs-http-client/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/02/26 03:58:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/26 09:03:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-http-client
 
 .if !defined(HS_HTTP_CLIENT_BUILDLINK3_MK)
 HS_HTTP_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-client+=	hs-http-client>=0.7.11
-BUILDLINK_ABI_DEPENDS.hs-http-client+=	hs-http-client>=0.7.11nb2
+BUILDLINK_API_DEPENDS.hs-http-client+=	hs-http-client>=0.7.13
+BUILDLINK_ABI_DEPENDS.hs-http-client+=	hs-http-client>=0.7.13.1
 BUILDLINK_PKGSRCDIR.hs-http-client?=	../../www/hs-http-client
 
+.include "../../devel/hs-async/buildlink3.mk"
 .include "../../converters/hs-base64-bytestring/buildlink3.mk"
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
