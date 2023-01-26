69e8146b6b152e840bb361afc68bd8dc6b6b86b9
pho <pho@pkgsrc.org>
2023-01-26 10:35:14 +0000

    math/hs-distributive: Remove an unnecessary dependency on math/hs-semigroups

--- math/hs-distributive/buildlink3.mk
+++ math/hs-distributive/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/02/26 03:58:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/01/26 10:35:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-distributive
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.hs-distributive+=	hs-distributive>=0.6.2.1nb4
 BUILDLINK_PKGSRCDIR.hs-distributive?=	../../math/hs-distributive
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .endif	# HS_DISTRIBUTIVE_BUILDLINK3_MK
 
