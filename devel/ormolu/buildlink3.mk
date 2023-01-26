531f605aedab5c165d12368750efc1e5b76d3b29
pho <pho@pkgsrc.org>
2023-01-26 10:58:24 +0000

    devel/ormolu: Update to 0.5.2.0
    Ormolu 0.5.2.0
    * Eliminated the fixity-th Cabal flag because it caused issues on GHC 9.4
      as well as on aarch64. See issue 941 and issue 927.
    * Now operators without explicitly specified fixity default to left
      associativity and highest precedence. Issue 907.
    
    Ormolu 0.5.1.0
    * Imports are now sorted by package qualifier, if one is present. Issue
      905.
    * Extension packs like GHC2021 and Haskell2010 are now bumped to the top of
      the list of language pragmas. Issue 922.
    * Fix formatting of SCC pragmas in do blocks. Issue 925.
    * Support type applications in patterns. Issue 930.
    * Handle UnicodeSyntax variants more consistently. Issue 934.
    * Fix an inconsistency in formatting of types in GADT declarations in
      certain cases. PR 932.
    * Switched to ghc-lib-parser-9.4, which brings support for the following
      new syntactic features:
      - \cases via LambdaCase
      - OPAQUE pragmas
      - Unboxed sum type constructors like (# | #).
    * Updated to Cabal-syntax-3.8, supporting cabal-version: 3.8.
    
    Ormolu 0.5.0.1
    * Fixed a bug in the diff printing functionality. Issue 886.
    * Indent closing bracket for list comprehensions in do blocks. Issue 893.
    * Fix hs-source-dirs: . resulting in failing to find a .cabal file for a
      Haskell source file. Issue 909.
    * Comments in closed type family declarations are now indented
      correctly. Issue 913.
    * Cache .cabal file parsing and processing when given multiple input files
      in the same project. This results in dramatic speedups on projects which
      have both huge .cabal files and a large number of individual
      modules. Issue 897.
    
    Ormolu 0.5.0.0
    * Changed the way operator fixities and precedences are inferred.
      - Ormolu now tries to locate .cabal files of source files by default and
        in addition to default extensions it also infers the list of
        dependencies.
      - Ormolu comes equipped with extensive knowledge of all packages on
        Hackage and operators that those packages define. Knowing the names of
        the dependencies it can select the right fixity and precedence info
        from its knowledge base.
      - You can ask Ormolu not to look for .cabal files by using the --no-cabal
        switch.
      - Dependencies can be selected manually by using the -p / --package
        option (can be repeated many times).
      - The default heuristic algorithm will still try to guess the right
        fixities and precedence.
      - Fixity overrides can be provided by the user in the familiar Haskell
        notation (e.g. infixr 9 ., one declaration per line). They are loaded
        by default from the .ormolu file that is expected to be in the same
        directory as the .cabal file of a given source file. However, if
        --no-cabal is supplied, the .ormolu file will not be looked for
        either. Fixity declarations can be also provided by using the -f /
        --fixity command line option, which see.
      - This resolves the following issues: Issue 826, Issue 785, Issue 690,
        Issue 825.
    * Invalid haddock comments are formatted in a more consistent way. Leading
      haddock triggers (|, ^) in an invalid haddock comment block are now
      escaped with a backslash \. Issue 816.
    * Type synonyms and families are now formatted correctly when the equals
      sign is preceded by a comment. Issue 829.
    * Bidirectional pattern synonyms are formatted nicer in certain
      cases. Issue 843.
    * Magic comments (like {- ORMOLU_DISABLED -}) now allow arbitrary
      succeeding text. This fixes use cases like Issue 856.
    * Remove discrepancies between unboxed types and terms. Issue 856 .
      - Unboxed sum types are now formatted with a space before each |.
      - Unboxed unit tuples on type and value levels are formatted as (# #).
    * Errors caused by AST differences now print before/after diffs. Issue 877.
    * Improved formatting of data declarations in the case of
      single-constructor record with a Haddock. Issue 881.

--- devel/ormolu/buildlink3.mk
+++ devel/ormolu/buildlink3.mk
@@ -1,18 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/09/07 06:50:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/26 10:58:24 pho Exp $
 
 BUILDLINK_TREE+=	ormolu
 
 .if !defined(ORMOLU_BUILDLINK3_MK)
 ORMOLU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.4.0
-BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.4.0.0nb3
+BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.5.2
+BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.5.2.0
 BUILDLINK_PKGSRCDIR.ormolu?=	../../devel/ormolu
 
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../textproc/hs-Diff/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-file-embed/buildlink3.mk"
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
+.include "../../textproc/hs-megaparsec/buildlink3.mk"
+.include "../../devel/hs-memotrie/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .include "../../devel/hs-gitrev/buildlink3.mk"
 .include "../../devel/hs-optparse-applicative/buildlink3.mk"
