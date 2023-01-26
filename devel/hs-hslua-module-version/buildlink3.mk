a0bc1a960fb703483d7b9f7ca3b95fc59a1f1517
pho <pho@pkgsrc.org>
2023-01-26 09:30:36 +0000

    devel/hs-hslua-module-version: Update to 1.0.3
    hslua-module-version-1.0.3 - Released 2022-09-01
    * Allow equality checks with non-version values: A Version value can now be
      compared with any value. Previously, comparing a version with a value
      that cannot be interpreted as a Version would result in an error,
      violating the principle of least surprise.
    
    hslua-module-version-1.0.2 - Released 2022-02-19
    * Relax upper bounds, allowing hslua-core-2.2.*, hslua-marshalling-2.2.*
      and hslua-packaging-2.2.*.

--- devel/hs-hslua-module-version/buildlink3.mk
+++ devel/hs-hslua-module-version/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/02/26 03:57:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/26 09:30:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-version
 
 .if !defined(HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK)
 HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.3
 BUILDLINK_PKGSRCDIR.hs-hslua-module-version?=	../../devel/hs-hslua-module-version
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
