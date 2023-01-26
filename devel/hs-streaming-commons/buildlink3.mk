75b59dbeac0d7a1f2c9ed1e18c7370bb538a77cb
pho <pho@pkgsrc.org>
2023-01-26 03:43:50 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- devel/hs-streaming-commons/buildlink3.mk
+++ devel/hs-streaming-commons/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/26 03:43:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-streaming-commons
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-streaming-commons
 HS_STREAMING_COMMONS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2.4nb2
+BUILDLINK_ABI_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2.5
 BUILDLINK_PKGSRCDIR.hs-streaming-commons?=	../../devel/hs-streaming-commons
 
 .include "../../devel/hs-async/buildlink3.mk"
