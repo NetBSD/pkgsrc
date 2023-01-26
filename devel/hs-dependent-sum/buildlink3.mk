a9dacaafc03c3cad71f0b7ecfc1f4ed67eb624b4
pho <pho@pkgsrc.org>
2023-01-26 16:03:39 +0000

    devel/hs-dependent-sum: Update to 0.7.2.0
    0.7.2.0 - 2022-12-22
    * Update to some-1.0.4.*
    
    0.7.1.1 - 2022-12-12
    * Support constraints-extras 0.4

--- devel/hs-dependent-sum/buildlink3.mk
+++ devel/hs-dependent-sum/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/26 16:03:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-dependent-sum
 
 .if !defined(HS_DEPENDENT_SUM_BUILDLINK3_MK)
 HS_DEPENDENT_SUM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dependent-sum+=	hs-dependent-sum>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-dependent-sum+=	hs-dependent-sum>=0.7.1.0nb1
+BUILDLINK_API_DEPENDS.hs-dependent-sum+=	hs-dependent-sum>=0.7.2
+BUILDLINK_ABI_DEPENDS.hs-dependent-sum+=	hs-dependent-sum>=0.7.2.0
 BUILDLINK_PKGSRCDIR.hs-dependent-sum?=		../../devel/hs-dependent-sum
 
 .include "../../devel/hs-constraints-extras/buildlink3.mk"
