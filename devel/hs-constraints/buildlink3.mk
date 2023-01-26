e81779287eb19d122df263fdc41ee4462ab9f452
pho <pho@pkgsrc.org>
2023-01-26 08:46:54 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- devel/hs-constraints/buildlink3.mk
+++ devel/hs-constraints/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/26 08:46:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-constraints
 
 .if !defined(HS_CONSTRAINTS_BUILDLINK3_MK)
 HS_CONSTRAINTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-constraints+=	hs-constraints>=0.13.3
-BUILDLINK_ABI_DEPENDS.hs-constraints+=	hs-constraints>=0.13.3nb1
+BUILDLINK_API_DEPENDS.hs-constraints+=	hs-constraints>=0.13.4
+BUILDLINK_ABI_DEPENDS.hs-constraints+=	hs-constraints>=0.13.4
 BUILDLINK_PKGSRCDIR.hs-constraints?=	../../devel/hs-constraints
 
 .include "../../devel/hs-hashable/buildlink3.mk"
