9590f019adc5a9a4d47f04427d1557149b2dc68b
pho <pho@pkgsrc.org>
2023-01-26 10:06:26 +0000

    www/hs-xss-sanitize: Update to 0.3.7.1
    0.3.7.1
    * add max height and max width as valid style attributes

--- www/hs-xss-sanitize/buildlink3.mk
+++ www/hs-xss-sanitize/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:58:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/26 10:06:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-xss-sanitize
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-xss-sanitize
 HS_XSS_SANITIZE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.7
-BUILDLINK_ABI_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.7nb2
+BUILDLINK_ABI_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.7.1
 BUILDLINK_PKGSRCDIR.hs-xss-sanitize?=	../../www/hs-xss-sanitize
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
