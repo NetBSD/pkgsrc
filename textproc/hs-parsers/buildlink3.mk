db4f2abf123c685349a79e176323c9b57686ebc6
pho <pho@pkgsrc.org>
2023-01-26 10:19:20 +0000

    textproc/hs-parsers: Update to 0.12.11
    0.12.11 [2022.05.07]
    * Allow building with transformers-0.6.* and mtl-2.3.*.

--- textproc/hs-parsers/buildlink3.mk
+++ textproc/hs-parsers/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/26 10:19:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-parsers
 
 .if !defined(HS_PARSERS_BUILDLINK3_MK)
 HS_PARSERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsers+=	hs-parsers>=0.12.10
-BUILDLINK_ABI_DEPENDS.hs-parsers+=	hs-parsers>=0.12.10nb1
+BUILDLINK_API_DEPENDS.hs-parsers+=	hs-parsers>=0.12.11
+BUILDLINK_ABI_DEPENDS.hs-parsers+=	hs-parsers>=0.12.11
 BUILDLINK_PKGSRCDIR.hs-parsers?=	../../textproc/hs-parsers
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
 .include "../../textproc/hs-charset/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
 .endif	# HS_PARSERS_BUILDLINK3_MK
