25b3d90f33419ec67c925454967488bfd99cc650
pho <pho@pkgsrc.org>
2023-01-26 15:51:09 +0000

    devel/hs-either: Update to 5.0.2
    5.0.2 [2022.05.07]
    * Tweak error messages of fromRight' and fromLeft' for clarity.
    * Allow building with mtl-2.3.*.

--- devel/hs-either/buildlink3.mk
+++ devel/hs-either/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:57:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/26 15:51:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-either
 
 .if !defined(HS_EITHER_BUILDLINK3_MK)
 HS_EITHER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-either+=	hs-either>=5.0.1
-BUILDLINK_ABI_DEPENDS.hs-either+=	hs-either>=5.0.1.1nb4
+BUILDLINK_API_DEPENDS.hs-either+=	hs-either>=5.0.2
+BUILDLINK_ABI_DEPENDS.hs-either+=	hs-either>=5.0.2
 BUILDLINK_PKGSRCDIR.hs-either?=		../../devel/hs-either
 
 .include "../../math/hs-bifunctors/buildlink3.mk"
