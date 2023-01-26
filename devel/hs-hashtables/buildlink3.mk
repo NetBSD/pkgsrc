ee7d567762b2318cb4b9aa4c3e1699e8982f5cd7
pho <pho@pkgsrc.org>
2023-01-26 03:28:56 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- devel/hs-hashtables/buildlink3.mk
+++ devel/hs-hashtables/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/08/20 08:08:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/26 03:28:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashtables
 
 .if !defined(HS_HASHTABLES_BUILDLINK3_MK)
 HS_HASHTABLES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashtables+=	hs-hashtables>=1.3
-BUILDLINK_ABI_DEPENDS.hs-hashtables+=	hs-hashtables>=1.3nb3
+BUILDLINK_API_DEPENDS.hs-hashtables+=	hs-hashtables>=1.3.1
+BUILDLINK_ABI_DEPENDS.hs-hashtables+=	hs-hashtables>=1.3.1
 BUILDLINK_PKGSRCDIR.hs-hashtables?=	../../devel/hs-hashtables
 
 .include "../../devel/hs-hashable/buildlink3.mk"
