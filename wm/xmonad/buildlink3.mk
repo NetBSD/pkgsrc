e9ab3e4abc49c422f408abf39ceb54d02786c785
pho <pho@pkgsrc.org>
2023-01-26 09:53:59 +0000

    wm/xmonad: Remove unnecessary dependencies

--- wm/xmonad/buildlink3.mk
+++ wm/xmonad/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/01/25 16:52:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/01/26 09:53:59 pho Exp $
 
 BUILDLINK_TREE+=	xmonad
 
@@ -10,9 +10,7 @@ BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.17.1
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wm/xmonad
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../devel/hs-extensible-exceptions/buildlink3.mk"
 .include "../../devel/hs-setlocale/buildlink3.mk"
-.include "../../devel/hs-utf8-string/buildlink3.mk"
 .include "../../x11/hs-X11/buildlink3.mk"
 .endif	# XMONAD_BUILDLINK3_MK
 
