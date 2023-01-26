5195a8633e7d675d78d39882fc1d609726226791
pho <pho@pkgsrc.org>
2023-01-26 18:08:11 +0000

    lang/hs-hslua: Update to 2.2.1
    hslua-2.2.1 - Released 2022-06-19
    * Require hslua-core-2.2.1.
    * Require hslua-marshalling-2.2.0.1.
    * Relax upper bound for mtl, allow mtl-2.3.
    
    hslua-2.2.0 - Released 2022-02-19
    * Require version 2.2.* of all hslua-* packages.
    * Include hslua-aeson, and re-export HsLua.Aeson from HsLua.

--- lang/hs-hslua/buildlink3.mk
+++ lang/hs-hslua/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/02/26 03:58:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/26 18:08:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua
 
 .if !defined(HS_HSLUA_BUILDLINK3_MK)
 HS_HSLUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua+=	hs-hslua>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua+=	hs-hslua>=2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua+=	hs-hslua>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-hslua+=	hs-hslua>=2.2.1
 BUILDLINK_PKGSRCDIR.hs-hslua?=		../../lang/hs-hslua
 
+.include "../../converters/hs-hslua-aeson/buildlink3.mk"
 .include "../../lang/hs-hslua-classes/buildlink3.mk"
 .include "../../lang/hs-hslua-core/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
