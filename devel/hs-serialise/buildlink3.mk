1f92922d1716d31d9cc744d286c43ee02cf50ff5
pho <pho@pkgsrc.org>
2023-01-26 11:43:02 +0000

    devel/hs-serialise: Update to 0.2.6.0
    0.2.6.0 -- 2022-09-24
    * Support GHC 9.4
    * Drop GHC 8.0 and 8.2 support

--- devel/hs-serialise/buildlink3.mk
+++ devel/hs-serialise/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/26 11:43:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-serialise
 
 .if !defined(HS_SERIALISE_BUILDLINK3_MK)
 HS_SERIALISE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-serialise+=	hs-serialise>=0.2.4
-BUILDLINK_ABI_DEPENDS.hs-serialise+=	hs-serialise>=0.2.4.0nb2
+BUILDLINK_API_DEPENDS.hs-serialise+=	hs-serialise>=0.2.6
+BUILDLINK_ABI_DEPENDS.hs-serialise+=	hs-serialise>=0.2.6.0
 BUILDLINK_PKGSRCDIR.hs-serialise?=	../../devel/hs-serialise
 
 .include "../../devel/hs-cborg/buildlink3.mk"
 .include "../../devel/hs-half/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-strict/buildlink3.mk"
 .include "../../devel/hs-these/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
