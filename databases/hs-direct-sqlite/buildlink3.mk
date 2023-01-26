ebd2087018c6ae0142a99b7f1f4e54d82f72e715
pho <pho@pkgsrc.org>
2023-01-26 08:53:08 +0000

    devel/hs-pretty-simple: Update to 4.1.2.0
    4.1.2.0
    * Fix a problem with the pHPrint function incorrectly outputting a trailing
      newline to stdout, instead of the handle you pass it. #118
    * Add a web app where you can play around with pretty-simple in your
      browser. #116. This took a lot of hard work by @georgefst!
    
    4.1.1.0
    * Make the pretty-printed output with outputOptionsCompact enabled a little
      more compact. #110. Thanks @juhp!
    * Add a --compact / -C flag to the pretty-simple executable that enables
      outputOptionsCompact. #111. Thanks again @juhp!
    * Add pTraceWith and pTraceShowWith to Debug.Pretty.Simple. #104. Thanks
      @LeviButcher!
    
    4.1.0.0
    * Fix a regression which arose in 4.0, whereby excess spaces would be
      inserted for unusual strings like dates and IP addresses. #105
    * Attach warnings to debugging functions, so that they're easy to find and
      remove. #103
    * Some minor improvements to the CLI tool:
      - Add a --version/-v flag. #83
      - Add a trailing newline. #87
      - Install by default, without requiring a flag. #94

--- databases/hs-direct-sqlite/buildlink3.mk
+++ databases/hs-direct-sqlite/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/11/23 16:18:34 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/26 08:53:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-direct-sqlite
 
 .if !defined(HS_DIRECT_SQLITE_BUILDLINK3_MK)
 HS_DIRECT_SQLITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.26
-BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.26nb3
+BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.27
+BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.27
 BUILDLINK_PKGSRCDIR.hs-direct-sqlite?=		../../databases/hs-direct-sqlite
 
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_DIRECT_SQLITE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-direct-sqlite
