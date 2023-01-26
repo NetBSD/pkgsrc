bfc94b4e76215f280edc2834d8b27337d69f0367
pho <pho@pkgsrc.org>
2023-01-26 16:17:31 +0000

    devel/hs-foldl: Update to 1.4.14
    1.4.14
    * Add Control.Foldl.NonEmpty.nonEmpty
    * Add Control.Foldl.NonEmpty.toFold
    * Generalize fold1 to work with Foldable1
    
    1.4.13
    * New "Control.Foldl.NonEmpty" module for folding non-empty containers

--- devel/hs-foldl/buildlink3.mk
+++ devel/hs-foldl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/26 16:17:31 pho Exp $
 
 BUILDLINK_TREE+=	hs-foldl
 
 .if !defined(HS_FOLDL_BUILDLINK3_MK)
 HS_FOLDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.12
-BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.12nb2
+BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.14
+BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.14
 BUILDLINK_PKGSRCDIR.hs-foldl?=		../../devel/hs-foldl
 
 .include "../../math/hs-comonad/buildlink3.mk"
