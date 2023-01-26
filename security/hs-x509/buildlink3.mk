857ab9ea7e8c22015d6864fdd9077e3ddee33ea8
pho <pho@pkgsrc.org>
2023-01-26 10:08:48 +0000

    security/hs-x509: Update to 1.7.7
    2022-05-31 v1.7.7
    * Bump requirements to GHC 7.8 and transformers 0.4 series #130

--- security/hs-x509/buildlink3.mk
+++ security/hs-x509/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/09/07 06:50:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/26 10:08:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-x509
 
 .if !defined(HS_X509_BUILDLINK3_MK)
 HS_X509_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-x509+=	hs-x509>=1.7.6
-BUILDLINK_ABI_DEPENDS.hs-x509+=	hs-x509>=1.7.6nb3
+BUILDLINK_API_DEPENDS.hs-x509+=	hs-x509>=1.7.7
+BUILDLINK_ABI_DEPENDS.hs-x509+=	hs-x509>=1.7.7
 BUILDLINK_PKGSRCDIR.hs-x509?=	../../security/hs-x509
 
 .include "../../devel/hs-asn1-encoding/buildlink3.mk"
