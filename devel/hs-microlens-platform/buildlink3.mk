202b40104ea43414f958f89970b901ffe9f67d6a
pho <pho@pkgsrc.org>
2023-01-26 10:32:44 +0000

    devel/hs-microlens-platform: Update to 0.4.3.3
    0.4.3.1
    * Bumped text upper bound to support 2.0.
    
    0.4.3
    * New minor release (microlens-0.4.13.0, microlens-ghc-0.4.14).

--- devel/hs-microlens-platform/buildlink3.mk
+++ devel/hs-microlens-platform/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/26 10:32:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-microlens-platform
 
 .if !defined(HS_MICROLENS_PLATFORM_BUILDLINK3_MK)
 HS_MICROLENS_PLATFORM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-microlens-platform+=	hs-microlens-platform>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-microlens-platform+=	hs-microlens-platform>=0.4.2.1nb2
+BUILDLINK_API_DEPENDS.hs-microlens-platform+=	hs-microlens-platform>=0.4.3
+BUILDLINK_ABI_DEPENDS.hs-microlens-platform+=	hs-microlens-platform>=0.4.3.3
 BUILDLINK_PKGSRCDIR.hs-microlens-platform?=	../../devel/hs-microlens-platform
 
 .include "../../devel/hs-hashable/buildlink3.mk"
