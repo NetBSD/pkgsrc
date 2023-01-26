07c476b4d5b5b1776288a74e978000816297a34a
pho <pho@pkgsrc.org>
2023-01-26 09:33:08 +0000

    devel/hs-errors: Fix build with GHC 9.4

--- devel/hs-errors/buildlink3.mk
+++ devel/hs-errors/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/26 03:57:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/26 09:33:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-errors
 
@@ -9,8 +9,8 @@ BUILDLINK_API_DEPENDS.hs-errors+=	hs-errors>=2.3.0
 BUILDLINK_ABI_DEPENDS.hs-errors+=	hs-errors>=2.3.0nb4
 BUILDLINK_PKGSRCDIR.hs-errors?=		../../devel/hs-errors
 
-.include "../../devel/hs-safe/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-safe/buildlink3.mk"
 .endif	# HS_ERRORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-errors
