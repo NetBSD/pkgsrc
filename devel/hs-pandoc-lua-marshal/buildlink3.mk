a7005d601591fedbb702212a9f32d9ffd1fc7a79
pho <pho@pkgsrc.org>
2023-01-27 16:01:04 +0000

    devel/hs-pandoc-lua-marshal: Update to 0.2.0
    0.2.0
    * Depend on pandoc-types 1.23: the Null Block constructor has been removed,
      and a Figure constructor has been added.
    * Support for Lua 5.3 has been dropped; the package now requires hslua 2.2
      or later.
    * The implementation for List has been moved to the separate hslua-list
      module. This module no longer contains C code.
    
    0.1.7 - Released 2022-07-16
    * Allow Blocks to be passed as Caption value. The resulting caption has the
      Blocks as its long version and no short version.
    * Add clone method to Pandoc elements.
    
    0.1.6.1 - Released 2022-06-10
    * Provide better error messages when fuzzy retrieval of Inlines or Blocks
      fails.
    * Relax upper bound for text, allow text-2.0.
    
    0.1.6 - Released 2022-06-03
    * Fix applyFully: the function always traversed the document type-wise,
      never topdown.
    * Avoid shadowing of a function name that was added in hslua-2.2.
    * Support concatenating of Pandoc values with the .. operator.
    
    0.1.5.1 - Released 2022-02-19
    * Relax upper bound for lua and hslua.
    
    0.1.5 - Released 2022-02-17
    * Allow any type of callable object as argument to List functions filter,
      map, and find_if. These previously required the argument to be of type
      function, which was too restrictive.
    * Inline: the type of Image captions is now Inlines instead of List.

--- devel/hs-pandoc-lua-marshal/buildlink3.mk
+++ devel/hs-pandoc-lua-marshal/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/08/20 08:08:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/27 16:01:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-lua-marshal
 
 .if !defined(HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK)
 HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.1.4
-BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.1.4nb3
+BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-pandoc-lua-marshal?=	../../devel/hs-pandoc-lua-marshal
 
 .include "../../lang/hs-hslua/buildlink3.mk"
+.include "../../lang/hs-hslua-list/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
 .include "../../lang/hs-lua/buildlink3.mk"
 .include "../../textproc/hs-pandoc-types/buildlink3.mk"
