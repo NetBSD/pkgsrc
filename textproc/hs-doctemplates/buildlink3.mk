7da83adbbf7714fe7ed3f9d5112ab7f92d9c2738
pho <pho@pkgsrc.org>
2023-01-27 15:23:37 +0000

    textproc/hs-doctemplates: Update to 0.11
    No release notes have been provided by upstream.

--- textproc/hs-doctemplates/buildlink3.mk
+++ textproc/hs-doctemplates/buildlink3.mk
@@ -1,21 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/08/20 08:08:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/27 15:23:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-doctemplates
 
 .if !defined(HS_DOCTEMPLATES_BUILDLINK3_MK)
 HS_DOCTEMPLATES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.10.0
-BUILDLINK_ABI_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.10.0.1nb3
+BUILDLINK_API_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.11
+BUILDLINK_ABI_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.11
 BUILDLINK_PKGSRCDIR.hs-doctemplates?=	../../textproc/hs-doctemplates
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../textproc/hs-doclayout/buildlink3.mk"
-.include "../../textproc/hs-HsYAML/buildlink3.mk"
 .include "../../devel/hs-safe/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../converters/hs-text-conversions/buildlink3.mk"
-.include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_DOCTEMPLATES_BUILDLINK3_MK
 
