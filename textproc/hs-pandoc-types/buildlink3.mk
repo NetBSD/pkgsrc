720c7bfb323916e93386e83294ae378e9940f1e8
pho <pho@pkgsrc.org>
2023-01-27 15:51:06 +0000

    textproc/hs-pandoc-types: Update to 1.23
    [1.23]
    * Remove Null constructor from Block (#91) [API change].
    * ToJSONFilter: Add instance for MonadIO (#105, Willem Van Onsem) [API
      change].
    * Add `Figure` block constructor (Albert Krewinkel, Aner Lucero, and
      Christian Despres) [API change]. The new Figure block represents a figure
      with attributes, caption, and arbitrary block content.

--- textproc/hs-pandoc-types/buildlink3.mk
+++ textproc/hs-pandoc-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.26 2022/08/20 08:08:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2023/01/27 15:51:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-types
 
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.22.1
-BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.22.1nb3
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.23
+BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.23
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../textproc/hs-pandoc-types
 
 .include "../../converters/hs-aeson/buildlink3.mk"
