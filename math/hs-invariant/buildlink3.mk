34218a01554a5984e3983f268d43c6da018c7fed
pho <pho@pkgsrc.org>
2023-01-27 02:31:23 +0000

    math/hs-invariant: Update to 0.6
    0.6 [2022.07.03]
    * Loosen the Monad constraint in the Invariant(2) instances for Kleisli to
      an Invariant constraint.
    * Loosen the Comonad constraint in the Invariant2 instance for Cokleisli to
      an Invariant constraint.
    * Add Invariant instances for PastroSum, CopastroSum, Environment,
      FreeMapping, Pastro, and FreeTraversing from the profunctors library.
    * Add Invariant(2) instances for Copastro and Coyoneda from the profunctors
      library.
    
    0.5.6 [2022.05.07]
    * Add InvariantProfunctor and InvariantArrow newtypes that admit
      implementations of invmap that only require Profunctor or Arrow
      constraints, respectively. Also add top-level invmapProfunctor and
      invmapArrow functions.

--- math/hs-invariant/buildlink3.mk
+++ math/hs-invariant/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/27 02:31:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-invariant
 
 .if !defined(HS_INVARIANT_BUILDLINK3_MK)
 HS_INVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-invariant+=	hs-invariant>=0.5.5
-BUILDLINK_ABI_DEPENDS.hs-invariant+=	hs-invariant>=0.5.5nb1
+BUILDLINK_API_DEPENDS.hs-invariant+=	hs-invariant>=0.5.6
+BUILDLINK_ABI_DEPENDS.hs-invariant+=	hs-invariant>=0.5.6
 BUILDLINK_PKGSRCDIR.hs-invariant?=	../../math/hs-invariant
 
 .include "../../math/hs-bifunctors/buildlink3.mk"
