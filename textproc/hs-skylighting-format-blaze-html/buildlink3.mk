fae2bd09fba536fbfdcd6ca177f7947336e62023
pho <pho@pkgsrc.org>
2023-01-27 14:53:30 +0000

    textproc/hs-skylighting-format-blaze-html: import hs-skylighting-format-blaze-html-0.1.1
    This package provides functions to render syntax-highlighting as HTML.

---
+++ textproc/hs-skylighting-format-blaze-html/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/27 14:53:30 pho Exp $
+
+BUILDLINK_TREE+=	hs-skylighting-format-blaze-html
+
+.if !defined(HS_SKYLIGHTING_FORMAT_BLAZE_HTML_BUILDLINK3_MK)
+HS_SKYLIGHTING_FORMAT_BLAZE_HTML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-skylighting-format-blaze-html+=	hs-skylighting-format-blaze-html>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-skylighting-format-blaze-html+=	hs-skylighting-format-blaze-html>=0.1.1
+BUILDLINK_PKGSRCDIR.hs-skylighting-format-blaze-html?=		../../textproc/hs-skylighting-format-blaze-html
+
+.include "../../textproc/hs-blaze-html/buildlink3.mk"
+.include "../../textproc/hs-skylighting-core/buildlink3.mk"
+.endif	# HS_SKYLIGHTING_FORMAT_BLAZE_HTML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-skylighting-format-blaze-html
