6aef3addb016ad55a989816208ed6bb62b050b71
pho <pho@pkgsrc.org>
2023-01-27 13:59:34 +0000

    devel/hs-ghc-exactprint: Update to 1.6.1.1
    2022-11-08 v1.6.1
    * Add GHC 9.4.4 to CI
    * Add support for exact printing HsDocTy. #121
    
    2022-11-08 v1.6.1
    * Support GHC 9.4.3
    
    2022-09-30 v1.6.0
    * Support GHC 9.4.1 and GHC 9.4.2

--- devel/hs-ghc-exactprint/buildlink3.mk
+++ devel/hs-ghc-exactprint/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:57:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/27 13:59:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-exactprint
 
 .if !defined(HS_GHC_EXACTPRINT_BUILDLINK3_MK)
 HS_GHC_EXACTPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.5.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.5.0nb1
+BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.6.1
+BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.6.1.1
 BUILDLINK_PKGSRCDIR.hs-ghc-exactprint?=		../../devel/hs-ghc-exactprint
 
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../devel/hs-fail/buildlink3.mk"
 .include "../../math/hs-free/buildlink3.mk"
-.include "../../devel/hs-ghc-paths/buildlink3.mk"
 .include "../../devel/hs-ordered-containers/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .endif	# HS_GHC_EXACTPRINT_BUILDLINK3_MK
