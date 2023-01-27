abf86f002919aac16aec80731c7fd4d1fe84caf3
pho <pho@pkgsrc.org>
2023-01-27 16:16:16 +0000

    www/hs-warp-tls: Update to 3.3.4
    3.3.4
    * Integrated customizable accept hook from
      Network.Wai.Handler.Warp.Settings (cf. setAccept) #912
    * Adjusted httpOverTls because of the factoring out of
      Network.Wai.Handler.Warp.Recv to its own package recv in the warp
      package. #899
    
    3.3.3
    * Creating a bigger buffer when the current one is too small to fit the
      Builder #895
    * Expose TLS.supportedHashSignatures via TLSSettings #872

--- www/hs-warp-tls/buildlink3.mk
+++ www/hs-warp-tls/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/09/07 06:50:52 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/27 16:16:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-warp-tls
 
 .if !defined(HS_WARP_TLS_BUILDLINK3_MK)
 HS_WARP_TLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.2
-BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.2nb5
+BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.4
+BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.3.4
 BUILDLINK_PKGSRCDIR.hs-warp-tls?=	../../www/hs-warp-tls
 
 .include "../../security/hs-cryptonite/buildlink3.mk"
@@ -15,6 +15,7 @@ BUILDLINK_PKGSRCDIR.hs-warp-tls?=	../../www/hs-warp-tls
 .include "../../devel/hs-streaming-commons/buildlink3.mk"
 .include "../../security/hs-tls/buildlink3.mk"
 .include "../../security/hs-tls-session-manager/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
 .include "../../www/hs-wai/buildlink3.mk"
 .include "../../www/hs-warp/buildlink3.mk"
 .endif	# HS_WARP_TLS_BUILDLINK3_MK
