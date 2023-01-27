74655af3ef31ce6f475bb75e080059faa3cb10be
pho <pho@pkgsrc.org>
2023-01-27 16:19:17 +0000

    math/hs-adjunctions: Update to 4.4.2
    4.4.2 [2022.08.15]
    * Fix the build with mtl-2.3.1.
    
    4.4.1 [2022.05.07]
    * Allow building with transformers-0.6.* and mtl-2.3.*.

--- math/hs-adjunctions/buildlink3.mk
+++ math/hs-adjunctions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/27 16:19:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-adjunctions
 
 .if !defined(HS_ADJUNCTIONS_BUILDLINK3_MK)
 HS_ADJUNCTIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-adjunctions+=	hs-adjunctions>=4.4
-BUILDLINK_ABI_DEPENDS.hs-adjunctions+=	hs-adjunctions>=4.4nb1
+BUILDLINK_API_DEPENDS.hs-adjunctions+=	hs-adjunctions>=4.4.2
+BUILDLINK_ABI_DEPENDS.hs-adjunctions+=	hs-adjunctions>=4.4.2
 BUILDLINK_PKGSRCDIR.hs-adjunctions?=	../../math/hs-adjunctions
 
 .include "../../math/hs-comonad/buildlink3.mk"
