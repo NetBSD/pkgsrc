586cbfd51afd3c5181bb7b969130518d1fd47e97
pho <pho@pkgsrc.org>
2023-01-27 14:44:11 +0000

    textproc/hs-skylighting-core: Update to 0.13.2
    0.13.2
    * Support ConTeXt output via skylighting-format-context (Albert Krewinkel).
    * Update syntax definitions for markdown, nim, javascript.
    
    0.13.1.2
    * Update syntax definitions for c, cmake, commonmlisp, css, go, ini,
      isocpp, javascript, json, powershell, python, rust, sass, scss, toml,
      xml.
    * Associate regex captures with a context. This is necessary for proper
      implementation of the KDE syntax highlighting algorithm. Captures are
      only available within the context to which the match creating the capture
      switches. (Closes #160.)
    
    0.13.1.1
    * Add dart, purescript syntax definitions.
    * Update syntax definitions for isocpp, latex, alert, bash, cmake, nix,
      sql-postgresql, vhdl, zsh.
    * Remove obsoleote patch for lua.xml.
    * Add patches for latex.xml and isocpp.xml.
    
    0.13.1
    * getCapture: fail instead of throwing error if dynamic match not found. I
      believe this is the intended behavior for StringDetect, judging from
      examples in the KDE documentation.
    * Update xml syntax definitions: asn1, bash, c, cmake, cpp, cs, d, elixir,
      fortran-fixed, gcc, glsl, go, html, java, javascript, lex, lua, markdown,
      mediawiki, noweb, ocaml, orgmode, php, powershell, prolog, python, r,
      ruby, rust, scheme, sql-postgresql, typescript, vhdl, xml, yacc, yaml,
      zsh
    * Replace a lambda with pointfree notation (shaving off some RAM usage)
      (0xd34df00d).
    * Use newtype for TokenizerM, giving about 5-10% boost on benchmark
      (0xd34df00d).
    
    0.13
    * Update syntax definitions from upstream: bash, cmake, diff, ini, perl,
      php, sgml, xml.
    * Split out formatters into separate packages (#152). API changes:
      - Skylighting.Core no longer exports Skylighting.Format.ANSI,
        Skylighting.Format.HTML, Skylighting.Format.LaTeX. These are now
        provided by separate packages, skylighting-format-ansi,
        skylighting-format-blaze-html, skylighting-format-latex.
      - Skylighting.Types no longer exports XTerm256ColorCode. This has been
        moved to skylighting-format-ansi.
    * Change Makefile to use cabal instead of stack.
    
    0.12.3.1
    
    * Allow mtl 2.3.
    * Update syntax defs from upstream: bash, cmake, go, haxe, lua, zsh.
    * Add nix.xml (#149).
    * Add Pygments styles for Import and BuiltIn token types (#147, Bryan
      A. Danielak).
    * Use StrictData.
    * Remove unused dependencies (silences cabal warnings) (Andreas Abel).
    
    0.12.3
    * Add scss, sass, systemverilog, orgmode.
    * Update xml definitions from upstream: bash, css, python, r, zsh.

--- textproc/hs-skylighting-core/buildlink3.mk
+++ textproc/hs-skylighting-core/buildlink3.mk
@@ -1,19 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/09/07 06:50:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/01/27 14:44:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-skylighting-core
 
 .if !defined(HS_SKYLIGHTING_CORE_BUILDLINK3_MK)
 HS_SKYLIGHTING_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.12.2
-BUILDLINK_ABI_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.12.2nb5
+BUILDLINK_API_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.13.2
+BUILDLINK_ABI_DEPENDS.hs-skylighting-core+=	hs-skylighting-core>=0.13.2
 BUILDLINK_PKGSRCDIR.hs-skylighting-core?=	../../textproc/hs-skylighting-core
 
 .include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
 .include "../../converters/hs-base64-bytestring/buildlink3.mk"
-.include "../../textproc/hs-blaze-html/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../devel/hs-colour/buildlink3.mk"
 .include "../../devel/hs-safe/buildlink3.mk"
