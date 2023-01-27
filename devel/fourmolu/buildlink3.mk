6fdcfb156f0e9c43b91c70abd6845b09ee8b3749
pho <pho@pkgsrc.org>
2023-01-27 14:35:00 +0000

    devel/fourmolu: Update to 0.10.1.0
    The release note is too long to paste here. See
    https://hackage.haskell.org/package/fourmolu-0.10.1.0/changelog

--- devel/fourmolu/buildlink3.mk
+++ devel/fourmolu/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/09/07 06:50:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/27 14:35:00 pho Exp $
 
 BUILDLINK_TREE+=	fourmolu
 
 .if !defined(FOURMOLU_BUILDLINK3_MK)
 FOURMOLU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fourmolu+=	fourmolu>=0.5.0
-BUILDLINK_ABI_DEPENDS.fourmolu+=	fourmolu>=0.5.0.1nb4
+BUILDLINK_API_DEPENDS.fourmolu+=	fourmolu>=0.10.1
+BUILDLINK_ABI_DEPENDS.fourmolu+=	fourmolu>=0.10.1.0
 BUILDLINK_PKGSRCDIR.fourmolu?=		../../devel/fourmolu
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -14,11 +14,12 @@ BUILDLINK_PKGSRCDIR.fourmolu?=		../../devel/fourmolu
 .include "../../textproc/hs-Diff/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
-.include "../../textproc/hs-HsYAML/buildlink3.mk"
-.include "../../textproc/hs-HsYAML-aeson/buildlink3.mk"
+.include "../../textproc/hs-megaparsec/buildlink3.mk"
+.include "../../devel/hs-memotrie/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-th-lift-instances/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
 .include "../../devel/hs-gitrev/buildlink3.mk"
-.include "../../devel/hs-optparse-applicative/buildlink3.mk"
 .endif	# FOURMOLU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fourmolu
