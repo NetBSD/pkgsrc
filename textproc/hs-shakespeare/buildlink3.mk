5636f625fd54199d2d6c84b91025277764ec84b5
pho <pho@pkgsrc.org>
2023-01-27 08:50:18 +0000

    textproc/hs-shakespeare: Update to 2.0.30
    2.0.30
    * Add Text.Cassius.Ordered and Text.Lucius.Ordered modules with parsers to
      maintain order between attributes and mixin blocks.
    
    2.0.29
    * Support the upcoming template-haskell release with GHC 9.4 #267
    
    2.0.28
    * Add support for sub-blocks in mixins #264
    
    2.0.27
    * Change how embedded templates are located by the compiler. Relative files
      are now resolved using the Cabal project root, to fix builds of
      multi-project codebases. #266
    * Change how messages are located by the compiler. The message directory is
      now resolved using the Cabal project root, to fix builds of multi-project
      codebases. #266
    
    2.0.26
    * Support @supports #263

--- textproc/hs-shakespeare/buildlink3.mk
+++ textproc/hs-shakespeare/buildlink3.mk
@@ -1,16 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/08/20 08:08:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/01/27 08:50:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-shakespeare
 
 .if !defined(HS_SHAKESPEARE_BUILDLINK3_MK)
 HS_SHAKESPEARE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.25
-BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.25.1nb3
+BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.30
+BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.30
 BUILDLINK_PKGSRCDIR.hs-shakespeare?=	../../textproc/hs-shakespeare
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../textproc/hs-blaze-html/buildlink3.mk"
+.include "../../textproc/hs-blaze-markup/buildlink3.mk"
+.include "../../devel/hs-file-embed/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-th-lift/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
