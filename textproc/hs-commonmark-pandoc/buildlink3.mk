422b87781fc655d0a846700cee9ad664eb2ec6da
pho <pho@pkgsrc.org>
2023-01-27 15:52:56 +0000

    textproc/hs-commonmark-pandoc: Update to 0.2.1.3
    0.2.1.3
    * Allow pandoc-types 1.23.

--- textproc/hs-commonmark-pandoc/buildlink3.mk
+++ textproc/hs-commonmark-pandoc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/08/20 08:08:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/27 15:52:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark-pandoc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-commonmark-pandoc
 HS_COMMONMARK_PANDOC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.1.2nb3
+BUILDLINK_ABI_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.1.3
 BUILDLINK_PKGSRCDIR.hs-commonmark-pandoc?=	../../textproc/hs-commonmark-pandoc
 
 .include "../../textproc/hs-commonmark/buildlink3.mk"
