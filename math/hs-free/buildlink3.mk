eb9cfa2f41d4e4b784d5bbb07064bb2a2428ea95
pho <pho@pkgsrc.org>
2023-01-27 13:50:46 +0000

    math/hs-free: Update to 5.1.10
    5.1.10 [2022.11.30]
    * Add a MonadFail instance for FT.
    
    5.1.9 [2022.06.26]
    * Simplify the Eq and Ord instances for FT to avoid the use of overlapping
      instances.
    
    5.1.8 [2022.05.07]
    * Generalize the Monad constraint in the type signatures for hoistFreeT in
      Control.Monad.Trans.Free and Control.Monad.Trans.Free.Ap to a Functor
      constraint.
    * Allow building with transformers-0.6.* and mtl-2.3.*.

--- math/hs-free/buildlink3.mk
+++ math/hs-free/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/27 13:50:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-free
 
 .if !defined(HS_FREE_BUILDLINK3_MK)
 HS_FREE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-free+=	hs-free>=5.1.7
-BUILDLINK_ABI_DEPENDS.hs-free+=	hs-free>=5.1.7nb1
+BUILDLINK_API_DEPENDS.hs-free+=	hs-free>=5.1.10
+BUILDLINK_ABI_DEPENDS.hs-free+=	hs-free>=5.1.10
 BUILDLINK_PKGSRCDIR.hs-free?=	../../math/hs-free
 
 .include "../../math/hs-comonad/buildlink3.mk"
