195368d1bcd0a5f51222c942d36ec2d767aced61
pho <pho@pkgsrc.org>
2023-01-27 13:47:44 +0000

    devel/hs-list-t: Update to 1.0.5.6
    No release notes have been provided by upstream.

--- devel/hs-list-t/buildlink3.mk
+++ devel/hs-list-t/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/27 13:47:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-list-t
 
@@ -6,14 +6,13 @@ BUILDLINK_TREE+=	hs-list-t
 HS_LIST_T_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-list-t+=	hs-list-t>=1.0.5
-BUILDLINK_ABI_DEPENDS.hs-list-t+=	hs-list-t>=1.0.5.1nb2
+BUILDLINK_ABI_DEPENDS.hs-list-t+=	hs-list-t>=1.0.5.6
 BUILDLINK_PKGSRCDIR.hs-list-t?=		../../devel/hs-list-t
 
 .include "../../devel/hs-foldl/buildlink3.mk"
 .include "../../devel/hs-logict/buildlink3.mk"
 .include "../../devel/hs-mmorph/buildlink3.mk"
 .include "../../devel/hs-monad-control/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
 .endif	# HS_LIST_T_BUILDLINK3_MK
 
