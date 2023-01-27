9ff392f8bb94e0b32c1d2f399989076aca2e4eb5
pho <pho@pkgsrc.org>
2023-01-27 16:06:25 +0000

    converters/hs-aeson-pretty: Fix build with aeson-2.1

--- converters/hs-aeson-pretty/buildlink3.mk
+++ converters/hs-aeson-pretty/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/08/20 08:08:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/01/27 16:06:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-aeson-pretty
 
@@ -14,8 +14,6 @@ BUILDLINK_PKGSRCDIR.hs-aeson-pretty?=	../../converters/hs-aeson-pretty
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
-.include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../devel/hs-cmdargs/buildlink3.mk"
 .endif	# HS_AESON_PRETTY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-aeson-pretty
