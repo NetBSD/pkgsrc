54522c5261b724594f8a806fda2ed97d029aceb2
pho <pho@pkgsrc.org>
2023-01-28 08:45:46 +0000

    textproc/hs-citeproc: Update to 0.8.1
    0.8.1
    * In Pandoc and CslJson CaseTransform, group punctuation in clusters
      (#127).
    * Fix sorting on dates (#126). Previously this broke for some styles,
      e.g. apa.csl, which styles dates as MM/DD/YYYY, and would lead to
      incorrect sorting of dates with months and/or days.
    * Add citation-key variable from citeId. This is a new addition in CSL
      1.0.2.
    * Update locales from upstream.
    * Raise an error if multiple layout elements are present (#120).
    * Fix two test cases. They had illegal bibliography elements with no layout
      children.
    * If there are multiple layout elements, only use the last one. This can
      happen with CSL-M styles. The last layout should be
      locale-unspecific. This change will prevent us from emitting doubled
      citations or bibliographic references (see #120), allowing more graceful
      handling of CSL-M styles, even though we don't support CSL-M.
    
    0.8.0.2
    * Fix missing locator after collapsing and grouping with year suffix (#96).
    
    0.8.0.1
    * Fix disambiguation edge case (#116). We weren't properly disambiguating
      when only one of two ambiguous names had a subsequent citation.
    * Chicago page numbering fixes.
    * Update test suite form upstream.
    * Handle whole-citation links differently in secondFieldAlign (#113,
      Benjamin Bray).
    * Require data-default >= 0.5.2 (#114, Bodigrim).
    
    0.8
    * Add SubstitutedVal constructor for Val [API change] (#101, #108). This is
      used to track variables that are repressed due to substitution. (We can't
      just delete them, because they still count when we have "if" elements
      that check for a variable.)
    * Fix logic for including a group. A group with a text node and an empty
      variable should count as empty.
    * CaseTransform: don't change words that are a mix of uppercase and
      nonletters, like CRT1000.
    * Fix label with "page" variable (#107).
    * Fix error in test suite. We stripped indentation in the expected result
      in some cases.
    * Update fr-FR locale from upstream.
    
    0.7
    * Handle old term form sub verbo as if it is sub-verbo (the new form).
    * Update to latest locales in CSL repository.
    * Makefile: Fix update-locales target.
    * Keep explicit "et al." (#102, Albert Krewinkel).
    * Factor out deleteSubstitutedVariables.
    * Add any references in citationItemData to references.
    * Add citationItemData field to CitationItem [API change]. This corresponds
      to the itemData that can appear in the JSON representation of a citation
      item.
    * Add Ord, Eq instances for Reference, DisambiguationData, Val [API
      change].

--- textproc/hs-citeproc/buildlink3.mk
+++ textproc/hs-citeproc/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/09/07 06:50:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/01/28 08:45:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-citeproc
 
 .if !defined(HS_CITEPROC_BUILDLINK3_MK)
 HS_CITEPROC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-citeproc+=	hs-citeproc>=0.6.0
-BUILDLINK_ABI_DEPENDS.hs-citeproc+=	hs-citeproc>=0.6.0.1nb5
+BUILDLINK_API_DEPENDS.hs-citeproc+=	hs-citeproc>=0.8.1
+BUILDLINK_ABI_DEPENDS.hs-citeproc+=	hs-citeproc>=0.8.1
 BUILDLINK_PKGSRCDIR.hs-citeproc?=	../../textproc/hs-citeproc
 
 .include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../converters/hs-aeson-pretty/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
