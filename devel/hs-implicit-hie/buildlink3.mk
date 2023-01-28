346c7c0e3763742654214a15cb92df76e8787b75
pho <pho@pkgsrc.org>
2023-01-28 02:32:44 +0000

    devel/hs-implicit-hie: Update to 0.1.4.0
    No release notes have been provided by upstream.

--- devel/hs-implicit-hie/buildlink3.mk
+++ devel/hs-implicit-hie/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/09/07 06:50:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/28 02:32:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-implicit-hie
 
 .if !defined(HS_IMPLICIT_HIE_BUILDLINK3_MK)
 HS_IMPLICIT_HIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.2.6nb4
+BUILDLINK_API_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-implicit-hie+=	hs-implicit-hie>=0.1.4.0
 BUILDLINK_PKGSRCDIR.hs-implicit-hie?=	../../devel/hs-implicit-hie
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
