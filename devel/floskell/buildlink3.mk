c819842e99977f77b1899a5ca25588044f154328
pho <pho@pkgsrc.org>
2023-01-28 07:33:32 +0000

    devel/floskell: Update to 0.10.7
    Floskell 0.10.7 (2022-12-10)
    * Add support for building with GHC-9.4.3

--- devel/floskell/buildlink3.mk
+++ devel/floskell/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/09/07 06:50:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/28 07:33:32 pho Exp $
 
 BUILDLINK_TREE+=	floskell
 
 .if !defined(FLOSKELL_BUILDLINK3_MK)
 FLOSKELL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.floskell+=	floskell>=0.10.6
-BUILDLINK_ABI_DEPENDS.floskell+=	floskell>=0.10.6nb4
+BUILDLINK_API_DEPENDS.floskell+=	floskell>=0.10.7
+BUILDLINK_ABI_DEPENDS.floskell+=	floskell>=0.10.7
 BUILDLINK_PKGSRCDIR.floskell?=		../../devel/floskell
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -14,9 +14,8 @@ BUILDLINK_PKGSRCDIR.floskell?=		../../devel/floskell
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../devel/hs-haskell-src-exts/buildlink3.mk"
 .include "../../devel/hs-monad-dijkstra/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
-.include "../../converters/hs-aeson-pretty/buildlink3.mk"
-.include "../../devel/hs-optparse-applicative/buildlink3.mk"
 .endif	# FLOSKELL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-floskell
