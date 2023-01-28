4387a400580ba4a2eea9e52fa40be55664846299
pho <pho@pkgsrc.org>
2023-01-28 02:55:43 +0000

    converters/hs-deriving-aeson: import hs-deriving-aeson-0.2.9
    This package provides a newtype wrapper with FromJSON/ToJSON instances
    customisable via a phantom type parameter. The instances can be rendered to
    the original type using DerivingVia.

---
+++ converters/hs-deriving-aeson/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/28 02:55:43 pho Exp $
+
+BUILDLINK_TREE+=	hs-deriving-aeson
+
+.if !defined(HS_DERIVING_AESON_BUILDLINK3_MK)
+HS_DERIVING_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-deriving-aeson+=	hs-deriving-aeson>=0.2.9
+BUILDLINK_ABI_DEPENDS.hs-deriving-aeson+=	hs-deriving-aeson>=0.2.9
+BUILDLINK_PKGSRCDIR.hs-deriving-aeson?=		../../converters/hs-deriving-aeson
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.endif	# HS_DERIVING_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-deriving-aeson
