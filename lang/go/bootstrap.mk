af59565a3c579be60c2ff15e683a1bbc7a6efed7
jperkin <jperkin@pkgsrc.org>
2023-01-28 12:34:30 +0000

    go: Switch illumos platforms to native GOOS=illumos.
    Ensures that native interfaces such as flock(3C) that aren't available on
    Solaris can be used.

--- lang/go/bootstrap.mk
+++ lang/go/bootstrap.mk
@@ -1,7 +1,9 @@
-# $NetBSD: bootstrap.mk,v 1.8 2022/03/09 15:15:59 bacon Exp $
+# $NetBSD: bootstrap.mk,v 1.9 2023/01/28 12:34:30 jperkin Exp $
 
 .if !defined(GOROOT_BOOTSTRAP) || !exists(${GOROOT_BOOTSTRAP}/bin/go)
-.  if ${MACHINE_ARCH} == "aarch64" || ${OPSYS} == "Darwin" && ${OPSYS_VERSION} >= 120000
+.  if ${MACHINE_ARCH} == "aarch64" || \
+    (${OPSYS} == "Darwin" && ${OPSYS_VERSION} >= 120000) || \
+    (${OPSYS} == "SunOS" && ${OS_VARIANT} != "Solaris")
 BUILD_DEPENDS+=		go-bin-[0-9]*:../../lang/go-bin
 GOROOT_BOOTSTRAP=	${PREFIX}/go-bin
 .  else
