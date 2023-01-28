5076397e1f0b97b98918006a68b944aef0988f0b
pho <pho@pkgsrc.org>
2023-01-28 05:46:06 +0000

    devel/hs-hie-bios: Update to 0.11.0
    2022-09-13 - 0.11.0
    * Compatibility with aeson 1.5 #368
    * Add GHC 9.4 support #366
    * Actually run the bios-tests when tool-deps are ignored #365
      - They have been accidentally disabled since 0.9.0.
    * Completely overhaul test-suite #356
    
    2022-07-26 - 0.10.0
    * Apply Hlint suggestions #354
    * Cabal cradle: change error message on failure #353
    * Refactor parsing of hie.yaml files #329
    * Make sure we test the same versions as HLS #346
    * Move logging from hslogger to co-log #347
      - Demote process output to Debug severity #348
    * Fix typos #342
    
    2022-03-07 - 0.9.1
    * Ignore .ghci files while querying project GHC #337
      - Fixes a bug where hie-bios fails to load cabal cradles with .ghci files
    * Improve error messages if cabal invocation fails #338
    * Allow text-2.0 #335
    
    2022-02-25 - 0.9.0
    * Use the proper GHC version given by cabal #282
      - In particular, honour the with-compiler field in cabal.project
    * Drop support for GHC 8.4 #331

--- devel/hs-hie-bios/buildlink3.mk
+++ devel/hs-hie-bios/buildlink3.mk
@@ -1,22 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/09/07 06:50:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/01/28 05:46:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-hie-bios
 
 .if !defined(HS_HIE_BIOS_BUILDLINK3_MK)
 HS_HIE_BIOS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hie-bios+=	hs-hie-bios>=0.8.1
-BUILDLINK_ABI_DEPENDS.hs-hie-bios+=	hs-hie-bios>=0.8.1nb5
+BUILDLINK_API_DEPENDS.hs-hie-bios+=	hs-hie-bios>=0.11.0
+BUILDLINK_ABI_DEPENDS.hs-hie-bios+=	hs-hie-bios>=0.11.0
 BUILDLINK_PKGSRCDIR.hs-hie-bios?=	../../devel/hs-hie-bios
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../converters/hs-base16-bytestring/buildlink3.mk"
+.include "../../sysutils/hs-co-log-core/buildlink3.mk"
 .include "../../devel/hs-conduit/buildlink3.mk"
 .include "../../devel/hs-conduit-extra/buildlink3.mk"
 .include "../../security/hs-cryptohash-sha1/buildlink3.mk"
 .include "../../misc/hs-extra/buildlink3.mk"
 .include "../../devel/hs-file-embed/buildlink3.mk"
-.include "../../sysutils/hs-hslogger/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter/buildlink3.mk"
 .include "../../sysutils/hs-temporary/buildlink3.mk"
 .include "../../devel/hs-unix-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
