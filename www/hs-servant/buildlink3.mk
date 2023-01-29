f969a5cd8f9c0e33890e860e4bf1fefecfc0c1cf
pho <pho@pkgsrc.org>
2023-01-29 04:56:23 +0000

    www/hs-servant: import hs-servant-0.19.1
    A family of combinators for defining webservices APIs and serving them.

---
+++ www/hs-servant/buildlink3.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/29 04:56:23 pho Exp $
+
+BUILDLINK_TREE+=	hs-servant
+
+.if !defined(HS_SERVANT_BUILDLINK3_MK)
+HS_SERVANT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-servant+=	hs-servant>=0.19.1
+BUILDLINK_ABI_DEPENDS.hs-servant+=	hs-servant>=0.19.1
+BUILDLINK_PKGSRCDIR.hs-servant?=	../../www/hs-servant
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../math/hs-bifunctors/buildlink3.mk"
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../devel/hs-constraints/buildlink3.mk"
+.include "../../www/hs-http-api-data/buildlink3.mk"
+.include "../../www/hs-http-media/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-mmorph/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../devel/hs-QuickCheck/buildlink3.mk"
+.include "../../devel/hs-singleton-bool/buildlink3.mk"
+.include "../../devel/hs-sop-core/buildlink3.mk"
+.include "../../converters/hs-string-conversions/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-vault/buildlink3.mk"
+.endif	# HS_SERVANT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-servant
