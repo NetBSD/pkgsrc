00fe727e7191ae9079e7f6e9aa21d3ddd59071ff
pho <pho@pkgsrc.org>
2023-01-29 15:56:11 +0000

    devel/hs-pointed: Update to 5.0.4
    5.0.4 [2022.05.07]
    * Allow building with transformers-0.6.*.

--- devel/hs-pointed/buildlink3.mk
+++ devel/hs-pointed/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/02/26 03:58:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/01/29 15:56:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-pointed
 
 .if !defined(HS_POINTED_BUILDLINK3_MK)
 HS_POINTED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pointed+=	hs-pointed>=5.0.3
-BUILDLINK_ABI_DEPENDS.hs-pointed+=	hs-pointed>=5.0.3nb1
+BUILDLINK_API_DEPENDS.hs-pointed+=	hs-pointed>=5.0.4
+BUILDLINK_ABI_DEPENDS.hs-pointed+=	hs-pointed>=5.0.4
 BUILDLINK_PKGSRCDIR.hs-pointed?=	../../devel/hs-pointed
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
