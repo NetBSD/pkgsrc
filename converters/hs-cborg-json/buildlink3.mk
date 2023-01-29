d8231b43570f6d51fbbf9279934a8cc4751e0eb1
pho <pho@pkgsrc.org>
2023-01-29 11:56:04 +0000

    converters/hs-cborg-json: Update to 0.2.5.0
    0.2.5.0 -- 2022-09-24
    * Support GHC 9.4
    * Drop GHC 8.0 and 8.2 support
    * Support aeson 2.1

--- converters/hs-cborg-json/buildlink3.mk
+++ converters/hs-cborg-json/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/29 11:56:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-cborg-json
 
 .if !defined(HS_CBORG_JSON_BUILDLINK3_MK)
 HS_CBORG_JSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.3
-BUILDLINK_ABI_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.3.0nb2
+BUILDLINK_API_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.5
+BUILDLINK_ABI_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.5.0
 BUILDLINK_PKGSRCDIR.hs-cborg-json?=	../../converters/hs-cborg-json
 
 .include "../../converters/hs-aeson/buildlink3.mk"
