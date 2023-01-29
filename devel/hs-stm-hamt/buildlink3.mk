b282900843792c80187ed989b3882492bcb47dc2
pho <pho@pkgsrc.org>
2023-01-29 15:50:10 +0000

    devel/hs-stm-hamt: Update to 1.2.0.8
    No release notes have been provided by upstream.

--- devel/hs-stm-hamt/buildlink3.mk
+++ devel/hs-stm-hamt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/29 15:50:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-stm-hamt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-stm-hamt
 HS_STM_HAMT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-stm-hamt+=	hs-stm-hamt>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-stm-hamt+=	hs-stm-hamt>=1.2.0.7nb2
+BUILDLINK_ABI_DEPENDS.hs-stm-hamt+=	hs-stm-hamt>=1.2.0.8
 BUILDLINK_PKGSRCDIR.hs-stm-hamt?=	../../devel/hs-stm-hamt
 
 .include "../../devel/hs-deferred-folds/buildlink3.mk"
