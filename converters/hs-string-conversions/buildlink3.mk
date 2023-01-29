59126ddfad096105281c02f8ed3f6889a5226ee6
pho <pho@pkgsrc.org>
2023-01-29 04:54:44 +0000

    converters/hs-string-conversions: import hs-string-conversions-0.4.0.1
    Provides a simple type class for converting values of different string
    types into values of other string types.

---
+++ converters/hs-string-conversions/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/29 04:54:44 pho Exp $
+
+BUILDLINK_TREE+=	hs-string-conversions
+
+.if !defined(HS_STRING_CONVERSIONS_BUILDLINK3_MK)
+HS_STRING_CONVERSIONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-string-conversions+=	hs-string-conversions>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-string-conversions+=	hs-string-conversions>=0.4.0.1
+BUILDLINK_PKGSRCDIR.hs-string-conversions?=	../../converters/hs-string-conversions
+
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.endif	# HS_STRING_CONVERSIONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-string-conversions
