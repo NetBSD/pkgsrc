e29f686b70ff55439326d890f34807b12f19cedc
pho <pho@pkgsrc.org>
2023-01-29 16:02:54 +0000

    devel/hs-monoidal-containers: Update to 0.6.3.0
    0.6.3.0
    * Bump upper bounds of aeson, base, and lens

--- devel/hs-monoidal-containers/buildlink3.mk
+++ devel/hs-monoidal-containers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/09/07 02:57:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/01/29 16:02:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-monoidal-containers
 
 .if !defined(HS_MONOIDAL_CONTAINERS_BUILDLINK3_MK)
 HS_MONOIDAL_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monoidal-containers+=	hs-monoidal-containers>=0.6.2
-BUILDLINK_ABI_DEPENDS.hs-monoidal-containers+=	hs-monoidal-containers>=0.6.2.0
+BUILDLINK_API_DEPENDS.hs-monoidal-containers+=	hs-monoidal-containers>=0.6.3
+BUILDLINK_ABI_DEPENDS.hs-monoidal-containers+=	hs-monoidal-containers>=0.6.3.0
 BUILDLINK_PKGSRCDIR.hs-monoidal-containers?=	../../devel/hs-monoidal-containers
 
 .include "../../converters/hs-aeson/buildlink3.mk"
