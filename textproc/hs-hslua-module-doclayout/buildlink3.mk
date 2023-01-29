fd068d4f3d99f9c89fb644c3b5809720299f61cb
pho <pho@pkgsrc.org>
2023-01-29 08:26:41 +0000

    textproc/hs-hslua-module-doclayout: import hs-hslua-module-doclayout-1.0.4
    Lua module wrapping the doclayout Haskell package.

---
+++ textproc/hs-hslua-module-doclayout/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/29 08:26:41 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-module-doclayout
+
+.if !defined(HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK)
+HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.0.4
+BUILDLINK_PKGSRCDIR.hs-hslua-module-doclayout?=		../../textproc/hs-hslua-module-doclayout
+
+.include "../../textproc/hs-doclayout/buildlink3.mk"
+.include "../../lang/hs-hslua/buildlink3.mk"
+.endif	# HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-module-doclayout
