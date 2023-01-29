e718be687b02c0d7e02ec558cceb1df1a97819fa
pho <pho@pkgsrc.org>
2023-01-29 05:24:22 +0000

    www/hs-servant-server: import hs-servant-server-0.19.2
    This library lets you implement an HTTP server with handlers for each
    endpoint of a servant API, handling most of the boilerplate for you.

---
+++ www/hs-servant-server/buildlink3.mk
@@ -0,0 +1,32 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/29 05:24:22 pho Exp $
+
+BUILDLINK_TREE+=	hs-servant-server
+
+.if !defined(HS_SERVANT_SERVER_BUILDLINK3_MK)
+HS_SERVANT_SERVER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-servant-server+=	hs-servant-server>=0.19.2
+BUILDLINK_ABI_DEPENDS.hs-servant-server+=	hs-servant-server>=0.19.2
+BUILDLINK_PKGSRCDIR.hs-servant-server?=		../../www/hs-servant-server
+
+.include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
+.include "../../devel/hs-constraints/buildlink3.mk"
+.include "../../www/hs-http-api-data/buildlink3.mk"
+.include "../../www/hs-http-media/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../www/hs-servant/buildlink3.mk"
+.include "../../devel/hs-sop-core/buildlink3.mk"
+.include "../../converters/hs-string-conversions/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../www/hs-wai/buildlink3.mk"
+.include "../../www/hs-wai-app-static/buildlink3.mk"
+.include "../../devel/hs-word8/buildlink3.mk"
+.endif	# HS_SERVANT_SERVER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-servant-server
