259aaeb7137db5727c9db0ce9a7af2d6997f5364
pho <pho@pkgsrc.org>
2023-01-29 08:30:32 +0000

    archivers/hs-hslua-module-zip: import hs-hslua-module-zip-1.0.0
    Module with function for creating, modifying, and extracting files from zip
    archives.

---
+++ archivers/hs-hslua-module-zip/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/29 08:30:32 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-module-zip
+
+.if !defined(HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK)
+HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.0.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.0.0
+BUILDLINK_PKGSRCDIR.hs-hslua-module-zip?=	../../archivers/hs-hslua-module-zip
+
+.include "../../lang/hs-hslua-core/buildlink3.mk"
+.include "../../lang/hs-hslua-list/buildlink3.mk"
+.include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-packaging/buildlink3.mk"
+.include "../../archivers/hs-zip-archive/buildlink3.mk"
+.endif	# HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-module-zip
