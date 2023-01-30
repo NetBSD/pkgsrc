a56974b2f9a9762c181a673b5970d65789504f5a
pho <pho@pkgsrc.org>
2023-01-30 09:21:11 +0000

    devel/hs-hls-qualify-imported-names-plugin: Update to 1.0.2.0
    No release notes have been provided by upstream.

--- devel/hs-hls-qualify-imported-names-plugin/buildlink3.mk
+++ devel/hs-hls-qualify-imported-names-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/11/23 16:18:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/30 09:21:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-qualify-imported-names-plugin
 
 .if !defined(HS_HLS_QUALIFY_IMPORTED_NAMES_PLUGIN_BUILDLINK3_MK)
 HS_HLS_QUALIFY_IMPORTED_NAMES_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-qualify-imported-names-plugin+=	hs-hls-qualify-imported-names-plugin>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-hls-qualify-imported-names-plugin+=	hs-hls-qualify-imported-names-plugin>=1.0.1.0nb5
+BUILDLINK_API_DEPENDS.hs-hls-qualify-imported-names-plugin+=	hs-hls-qualify-imported-names-plugin>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-hls-qualify-imported-names-plugin+=	hs-hls-qualify-imported-names-plugin>=1.0.2.0
 BUILDLINK_PKGSRCDIR.hs-hls-qualify-imported-names-plugin?=	../../devel/hs-hls-qualify-imported-names-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
