7fac28d43f6b1f6287ac29110f261287129eaf25
adam <adam@pkgsrc.org>
2023-01-30 07:57:28 +0000

    tesseract: updated to 5.3.0
    5.3.0
    
    Fix memory issues in ScrollView::MessageReceiver
    autotools: Add rule for svpaint executable
    Replace call of exit function by return statement in main function
    Fix the build on CodeQL/Analyze
    CI: Remove Ubuntu 18.04
    configure.ac: fix build on aarch64_be
    SW CI: Add paths filter
    Create .mailmap
    Fix tesseract.pc from cmake to match autotools
    Update README.md
    Fixed 2 errors
    fix issue 3940 - remove colormap before thresholding
    Update upload-artifact action
    Update checkout action to version 3
    Fix Markdownlint
    Fix broken links in CONTRIBUTING.md
    pdfrenderer.cpp: Ignore non-text blocks
    lstm.train: allow .box from .raw.png too
    Fix a number of performance issues
    Fix training tools for legacy engine
    Fix function tesseract::WriteFeature
    Modernize function ObjectCache::DeleteUnusedObjects
    More fixes for issue

--- graphics/tesseract/buildlink3.mk
+++ graphics/tesseract/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/01/03 17:36:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/30 07:57:28 adam Exp $
 
 BUILDLINK_TREE+=	tesseract
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	tesseract
 TESSERACT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tesseract+=	tesseract>=3.05.01nb6
-BUILDLINK_ABI_DEPENDS.tesseract?=	tesseract>=4.1.1nb12
+BUILDLINK_ABI_DEPENDS.tesseract+=	tesseract>=4.1.1nb12
 BUILDLINK_PKGSRCDIR.tesseract?=		../../graphics/tesseract
 
 .include "../../graphics/leptonica/buildlink3.mk"
