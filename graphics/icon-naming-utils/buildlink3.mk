ff82ea154d862316ed841e2a2b905a205ba459f1
wiz <wiz@pkgsrc.org>
2023-01-30 17:15:52 +0000

    icon-naming-utils: remove unused intltool dependency

--- graphics/icon-naming-utils/buildlink3.mk
+++ graphics/icon-naming-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/06/28 11:34:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/30 17:15:52 wiz Exp $
 
 BUILDLINK_TREE+=	icon-naming-utils
 
@@ -7,7 +7,7 @@ ICON_NAMING_UTILS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.icon-naming-utils+=	icon-naming-utils>=0.8.7
 BUILDLINK_DEPMETHOD.icon-naming-utils?=		build
-BUILDLINK_ABI_DEPENDS.icon-naming-utils?=		icon-naming-utils>=0.8.90nb10
+BUILDLINK_ABI_DEPENDS.icon-naming-utils?=	icon-naming-utils>=0.8.90nb10
 BUILDLINK_PKGSRCDIR.icon-naming-utils?=		../../graphics/icon-naming-utils
 .endif # ICON_NAMING_UTILS_BUILDLINK3_MK
 
