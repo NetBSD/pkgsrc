85962cb7764639ad8c1d9d5f28e9ce303496b0ea
pho <pho@pkgsrc.org>
2023-01-30 09:29:12 +0000

    devel/hs-hls-pragmas-plugin: Update to 1.0.4.0
    No release notes have been provided by upstream.

--- devel/hs-hls-pragmas-plugin/buildlink3.mk
+++ devel/hs-hls-pragmas-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/11/23 16:18:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/30 09:29:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-pragmas-plugin
 
 .if !defined(HS_HLS_PRAGMAS_PLUGIN_BUILDLINK3_MK)
 HS_HLS_PRAGMAS_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-pragmas-plugin+=	hs-hls-pragmas-plugin>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-hls-pragmas-plugin+=	hs-hls-pragmas-plugin>=1.0.2.0nb5
+BUILDLINK_API_DEPENDS.hs-hls-pragmas-plugin+=	hs-hls-pragmas-plugin>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-hls-pragmas-plugin+=	hs-hls-pragmas-plugin>=1.0.4.0
 BUILDLINK_PKGSRCDIR.hs-hls-pragmas-plugin?=	../../devel/hs-hls-pragmas-plugin
 
 .include "../../misc/hs-extra/buildlink3.mk"
