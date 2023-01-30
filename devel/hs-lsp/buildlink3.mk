e9a1ee532d58fc88c7fe681d535447d6444b78b9
pho <pho@pkgsrc.org>
2023-01-30 03:56:55 +0000

    devel/hs-lsp: Update to 1.6.0.0
    1.6.0.0
    * Pinned to lsp-types 1.6
    * Tests moved to lsp-types
    
    1.5.0.0
    * VFS module moved to lsp from lsp-types.
    * Logging reworked to use co-log-core instead of hslogger.

--- devel/hs-lsp/buildlink3.mk
+++ devel/hs-lsp/buildlink3.mk
@@ -1,26 +1,27 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/30 03:56:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-lsp
 
 .if !defined(HS_LSP_BUILDLINK3_MK)
 HS_LSP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lsp+=	hs-lsp>=1.4.0
-BUILDLINK_ABI_DEPENDS.hs-lsp+=	hs-lsp>=1.4.0.0nb2
+BUILDLINK_API_DEPENDS.hs-lsp+=	hs-lsp>=1.6.0
+BUILDLINK_ABI_DEPENDS.hs-lsp+=	hs-lsp>=1.6.0.0
 BUILDLINK_PKGSRCDIR.hs-lsp?=	../../devel/hs-lsp
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../devel/hs-async/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../sysutils/hs-co-log-core/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../sysutils/hs-hslogger/buildlink3.mk"
 .include "../../devel/hs-lens/buildlink3.mk"
 .include "../../devel/hs-lsp-types/buildlink3.mk"
-.include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
-.include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-sorted-list/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../textproc/hs-text-rope/buildlink3.mk"
 .include "../../devel/hs-unliftio-core/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-uuid/buildlink3.mk"
