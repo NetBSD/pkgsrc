d8307af0d1793060f886bfe1f55c61023e2bfece
pho <pho@pkgsrc.org>
2023-01-30 03:01:38 +0000

    devel/hs-lsp-types: Update to 1.6.0.0
    1.6.0.0
    * Add isSubRangeOf and positionInRange helper functions
    * Add ServerCancelled, RequestFailed and ErrorCodeCustom server error types
    * Fix "workspace/semanticTokens/refresh" to be a server method instead of a
      client method
    * Use a packed representation for NormalizedFilePath
    * Add converions from OsPath to NormalizedFilePath in
      Language.LSP.Types.Uri.OsPath when using new enough filepath
    
    1.5.0.0
    * VFS module moved from lsp-types to lsp, as it relates to the actual
      implementation of a LSP server.

--- devel/hs-lsp-types/buildlink3.mk
+++ devel/hs-lsp-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/08/20 08:08:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/01/30 03:01:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-lsp-types
 
 .if !defined(HS_LSP_TYPES_BUILDLINK3_MK)
 HS_LSP_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lsp-types+=	hs-lsp-types>=1.4.0
-BUILDLINK_ABI_DEPENDS.hs-lsp-types+=	hs-lsp-types>=1.4.0.1nb2
+BUILDLINK_API_DEPENDS.hs-lsp-types+=	hs-lsp-types>=1.6.0
+BUILDLINK_ABI_DEPENDS.hs-lsp-types+=	hs-lsp-types>=1.6.0.0
 BUILDLINK_PKGSRCDIR.hs-lsp-types?=	../../devel/hs-lsp-types
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -14,14 +14,12 @@ BUILDLINK_PKGSRCDIR.hs-lsp-types?=	../../devel/hs-lsp-types
 .include "../../textproc/hs-Diff/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../sysutils/hs-hslogger/buildlink3.mk"
 .include "../../devel/hs-lens/buildlink3.mk"
 .include "../../math/hs-mod/buildlink3.mk"
 .include "../../net/hs-network-uri/buildlink3.mk"
-.include "../../textproc/hs-rope-utf16-splay/buildlink3.mk"
+.include "../../devel/hs-safe/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-some/buildlink3.mk"
-.include "../../sysutils/hs-temporary/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_LSP_TYPES_BUILDLINK3_MK
 
