12c1aa679d1412aa019306dd6233fc45594953e5
pho <pho@pkgsrc.org>
2023-01-30 10:10:03 +0000

    devel/hs-hls-ormolu-plugin: import hs-hls-ormolu-plugin-1.0.3.0
    Integration with the Ormolu code formatter
    
    --
    I will soon remove devel/hs-hls-oumolu-plugin. It had a typo in the directory name and CVS doesn't let me rename anything.

---
+++ devel/hs-hls-ormolu-plugin/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/30 10:10:03 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-ormolu-plugin
+
+.if !defined(HS_HLS_ORMOLU_PLUGIN_BUILDLINK3_MK)
+HS_HLS_ORMOLU_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-ormolu-plugin+=	hs-hls-ormolu-plugin>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-hls-ormolu-plugin+=	hs-hls-ormolu-plugin>=1.0.3.0
+BUILDLINK_PKGSRCDIR.hs-hls-ormolu-plugin?=	../../devel/hs-hls-ormolu-plugin
+
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/ormolu/buildlink3.mk"
+.endif	# HS_HLS_ORMOLU_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-ormolu-plugin
