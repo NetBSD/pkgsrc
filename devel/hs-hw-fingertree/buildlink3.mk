66702c539dee11d4e0774f82c0951a670fe97e1b
pho <pho@pkgsrc.org>
2023-01-30 07:01:12 +0000

    devel/hs-hw-fingertree: import hs-hw-fingertree-0.1.2.1
    A general sequence representation with arbitrary annotations, for use as a
    base for implementations of various collection types, with examples, as
    described in section 4 of
    
        Ralf Hinze and Ross Paterson, "Finger trees: a simple general-purpose
        data structure", Journal of Functional Programming 16:2 (2006) pp
        197-217. http://staff.city.ac.uk/~ross/papers/FingerTree.html
    
    For a tuned sequence type, see Data.Sequence in the containers package,
    which is a specialization of this structure.

---
+++ devel/hs-hw-fingertree/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/30 07:01:12 pho Exp $
+
+BUILDLINK_TREE+=	hs-hw-fingertree
+
+.if !defined(HS_HW_FINGERTREE_BUILDLINK3_MK)
+HS_HW_FINGERTREE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hw-fingertree+=	hs-hw-fingertree>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-hw-fingertree+=	hs-hw-fingertree>=0.1.2.1
+BUILDLINK_PKGSRCDIR.hs-hw-fingertree?=		../../devel/hs-hw-fingertree
+
+.include "../../devel/hs-hw-prim/buildlink3.mk"
+.endif	# HS_HW_FINGERTREE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hw-fingertree
