c08b72e58ccd00de82bf3e45e76caa5d5e6b9398
pho <pho@pkgsrc.org>
2023-01-30 04:01:17 +0000

    devel/hs-hls-graph: Update to 1.9.0.0
    No release notes have been provided by upstream.

--- devel/hs-hls-graph/buildlink3.mk
+++ devel/hs-hls-graph/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/09/07 06:50:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/01/30 04:01:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-graph
 
 .if !defined(HS_HLS_GRAPH_BUILDLINK3_MK)
 HS_HLS_GRAPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-graph+=	hs-hls-graph>=1.6.0.0
-BUILDLINK_ABI_DEPENDS.hs-hls-graph+=	hs-hls-graph>=1.6.0.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-graph+=	hs-hls-graph>=1.9.0
+BUILDLINK_ABI_DEPENDS.hs-hls-graph+=	hs-hls-graph>=1.9.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-graph?=	../../devel/hs-hls-graph
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -18,8 +18,8 @@ BUILDLINK_PKGSRCDIR.hs-hls-graph?=	../../devel/hs-hls-graph
 .include "../../www/hs-js-flot/buildlink3.mk"
 .include "../../www/hs-js-jquery/buildlink3.mk"
 .include "../../devel/hs-list-t/buildlink3.mk"
-.include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-stm-containers/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_HLS_GRAPH_BUILDLINK3_MK
 
