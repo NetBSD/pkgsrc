ee869f3a5aadfc815d1cf6d80908d62e65c13c55
pho <pho@pkgsrc.org>
2023-01-30 10:15:16 +0000

    devel/hs-hls-module-name-plugin: Update to 1.1.1.0
    No release notes have been provided by upstream.

--- devel/hs-hls-module-name-plugin/buildlink3.mk
+++ devel/hs-hls-module-name-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/11/23 16:18:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/30 10:15:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-module-name-plugin
 
 .if !defined(HS_HLS_MODULE_NAME_PLUGIN_BUILDLINK3_MK)
 HS_HLS_MODULE_NAME_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=1.0.1.0nb5
+BUILDLINK_API_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=1.1.1.0
 BUILDLINK_PKGSRCDIR.hs-hls-module-name-plugin?=		../../devel/hs-hls-module-name-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
