e902794fedeb9f5ea0e87b18561cf6fd6e95dd92
pho <pho@pkgsrc.org>
2023-01-30 07:19:33 +0000

    devel/hs-enummapset: import hs-enummapset-0.7.1.0
    This package contains simple wrappers around Data.IntMap and Data.IntSet
    with Enum keys and elements respectively. Especially useful for Ints
    wrapped with newtype.

---
+++ devel/hs-enummapset/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/30 07:19:33 pho Exp $
+
+BUILDLINK_TREE+=	hs-enummapset
+
+.if !defined(HS_ENUMMAPSET_BUILDLINK3_MK)
+HS_ENUMMAPSET_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-enummapset+=	hs-enummapset>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-enummapset+=	hs-enummapset>=0.7.1.0
+BUILDLINK_PKGSRCDIR.hs-enummapset?=	../../devel/hs-enummapset
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.endif	# HS_ENUMMAPSET_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-enummapset
