808eaebe45ae949fb1d5ddafe0b61ba42bf566f7
pho <pho@pkgsrc.org>
2023-01-31 08:06:48 +0000

    devel/hs-hls-refine-imports-plugin: Update to 1.0.4.0
    No release notes have been provided by upstream.

--- devel/hs-hls-refine-imports-plugin/buildlink3.mk
+++ devel/hs-hls-refine-imports-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/11/23 16:18:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/31 08:06:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-refine-imports-plugin
 
 .if !defined(HS_HLS_REFINE_IMPORTS_PLUGIN_BUILDLINK3_MK)
 HS_HLS_REFINE_IMPORTS_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-refine-imports-plugin+=	hs-hls-refine-imports-plugin>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-hls-refine-imports-plugin+=	hs-hls-refine-imports-plugin>=1.0.1.0nb5
+BUILDLINK_API_DEPENDS.hs-hls-refine-imports-plugin+=	hs-hls-refine-imports-plugin>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-hls-refine-imports-plugin+=	hs-hls-refine-imports-plugin>=1.0.4.0
 BUILDLINK_PKGSRCDIR.hs-hls-refine-imports-plugin?=	../../devel/hs-hls-refine-imports-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
