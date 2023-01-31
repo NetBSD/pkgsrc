35c2573817b71d491bf1703c157ced5cb766e4a3
pho <pho@pkgsrc.org>
2023-01-31 19:16:13 +0000

    devel/hs-hls-splice-plugin: import hs-hls-splice-plugin-1.0.3.0
    HLS Plugin to expand TemplateHaskell Splices and QuasiQuotes.

---
+++ devel/hs-hls-splice-plugin/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/01/31 19:16:13 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-splice-plugin
+
+.if !defined(HS_HLS_SPLICE_PLUGIN_BUILDLINK3_MK)
+HS_HLS_SPLICE_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-splice-plugin+=	hs-hls-splice-plugin>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-hls-splice-plugin+=	hs-hls-splice-plugin>=1.0.3.0
+BUILDLINK_PKGSRCDIR.hs-hls-splice-plugin?=	../../devel/hs-hls-splice-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-foldl/buildlink3.mk"
+.include "../../devel/hs-ghc-exactprint/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-hls-refactor-plugin/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/retrie/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_SPLICE_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-splice-plugin
