ea88548e5b7468390f6cbc38362b4b40cc6d544c
pho <pho@pkgsrc.org>
2023-01-31 01:37:28 +0000

    Forgot to update buildlink3.mk

--- devel/hs-hls-explicit-imports-plugin/buildlink3.mk
+++ devel/hs-hls-explicit-imports-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/11/23 16:18:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/31 01:37:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-explicit-imports-plugin
 
 .if !defined(HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK)
 HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.0.2.0nb5
+BUILDLINK_API_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.2.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-explicit-imports-plugin?=	../../devel/hs-hls-explicit-imports-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
