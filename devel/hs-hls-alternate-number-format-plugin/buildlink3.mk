244279801642818dd611ae85bb19ab9faaac2f52
pho <pho@pkgsrc.org>
2023-01-31 08:03:51 +0000

    devel/hs-hls-alternate-number-format-plugin: Update to 1.3.0.0
    No release notes have been provided by upstream.

--- devel/hs-hls-alternate-number-format-plugin/buildlink3.mk
+++ devel/hs-hls-alternate-number-format-plugin/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/11/23 16:18:37 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/01/31 08:03:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-alternate-number-format-plugin
 
 .if !defined(HS_HLS_ALTERNATE_NUMBER_FORMAT_PLUGIN_BUILDLINK3_MK)
 HS_HLS_ALTERNATE_NUMBER_FORMAT_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-alternate-number-format-plugin+=	hs-hls-alternate-number-format-plugin>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-hls-alternate-number-format-plugin+=	hs-hls-alternate-number-format-plugin>=1.0.1.0nb5
+BUILDLINK_API_DEPENDS.hs-hls-alternate-number-format-plugin+=	hs-hls-alternate-number-format-plugin>=1.3.0
+BUILDLINK_ABI_DEPENDS.hs-hls-alternate-number-format-plugin+=	hs-hls-alternate-number-format-plugin>=1.3.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-alternate-number-format-plugin?=	../../devel/hs-hls-alternate-number-format-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
 .include "../../devel/hs-ghcide/buildlink3.mk"
 .include "../../devel/hs-hie-compat/buildlink3.mk"
 .include "../../devel/hs-hls-graph/buildlink3.mk"
