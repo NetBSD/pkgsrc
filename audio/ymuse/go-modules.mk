78a3eada6bc241b686585b8a47712b5c5ef45089
wiz <wiz@pkgsrc.org>
2023-02-01 14:54:22 +0000

    audio/ymuse: import ymuse-0.21
    Ymuse is an easy, functional, and snappy GTK front-end (client)
    for Music Player Daemon written in Go. It supports both light and
    dark desktop theme.
    
    It supports library browsing and search, playlists, streams etc.

---
+++ audio/ymuse/go-modules.mk
@@ -0,0 +1,10 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/02/01 14:54:22 wiz Exp $
+
+GO_MODULE_FILES+=	github.com/fhs/gompd/v2/@v/v2.2.1-0.20220620205817-bbf835995263.mod
+GO_MODULE_FILES+=	github.com/fhs/gompd/v2/@v/v2.2.1-0.20220620205817-bbf835995263.zip
+GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.6.1.mod
+GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.6.1.zip
+GO_MODULE_FILES+=	github.com/op/go-logging/@v/v0.0.0-20160315200505-970db520ece7.mod
+GO_MODULE_FILES+=	github.com/op/go-logging/@v/v0.0.0-20160315200505-970db520ece7.zip
+GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.9.1.mod
+GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.9.1.zip
