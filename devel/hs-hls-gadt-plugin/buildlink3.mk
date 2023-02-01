5d16dc3efad20d07cb88dcbb722591027a984c66
pho <pho@pkgsrc.org>
2023-02-01 02:00:15 +0000

    devel/hs-hls-gadt-plugin: import hs-hls-gadt-plugin-1.0.1.0
    Convert to GADT syntax plugin.

---
+++ devel/hs-hls-gadt-plugin/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/02/01 02:00:15 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-gadt-plugin
+
+.if !defined(HS_HLS_GADT_PLUGIN_BUILDLINK3_MK)
+HS_HLS_GADT_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-gadt-plugin+=	hs-hls-gadt-plugin>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-hls-gadt-plugin+=	hs-hls-gadt-plugin>=1.0.1.0
+BUILDLINK_PKGSRCDIR.hs-hls-gadt-plugin?=	../../devel/hs-hls-gadt-plugin
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-ghc-exactprint/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-hls-refactor-plugin/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_HLS_GADT_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-gadt-plugin
