04c56eeba0d24b44c28238a658f48d50d0a64c77
pho <pho@pkgsrc.org>
2023-02-01 02:04:51 +0000

    devel/hs-hls-explicit-fixity-plugin: import hs-hls-explicit-fixity-plugin-1.1.0.0
    The hls-explicit-fixity-plugin will show available fixity explicitly while
    hovering.

---
+++ devel/hs-hls-explicit-fixity-plugin/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/02/01 02:04:51 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-explicit-fixity-plugin
+
+.if !defined(HS_HLS_EXPLICIT_FIXITY_PLUGIN_BUILDLINK3_MK)
+HS_HLS_EXPLICIT_FIXITY_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-explicit-fixity-plugin+=	hs-hls-explicit-fixity-plugin>=1.1.0
+BUILDLINK_API_DEPENDS.hs-hls-explicit-fixity-plugin+=	hs-hls-explicit-fixity-plugin>=1.1.0.0
+BUILDLINK_PKGSRCDIR.hs-hls-explicit-fixity-plugin?=	../../devel/hs-hls-explicit-fixity-plugin
+
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.endif	# HS_HLS_EXPLICIT_FIXITY_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-explicit-fixity-plugin
