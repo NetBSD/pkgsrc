9034a9a1336ce5487d15000f309ef63223c8dbc7
tnn <tnn@pkgsrc.org>
2023-02-02 09:23:41 +0000

    go/bootstrap.mk: support setting GO_BOOTSTRAP_REQD=118
    for wip/go120

--- lang/go/bootstrap.mk
+++ lang/go/bootstrap.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bootstrap.mk,v 1.9 2023/01/28 12:34:30 jperkin Exp $
+# $NetBSD: bootstrap.mk,v 1.10 2023/02/02 09:23:41 tnn Exp $
 
 .if !defined(GOROOT_BOOTSTRAP) || !exists(${GOROOT_BOOTSTRAP}/bin/go)
 .  if ${MACHINE_ARCH} == "aarch64" || \
@@ -6,6 +6,9 @@
     (${OPSYS} == "SunOS" && ${OS_VARIANT} != "Solaris")
 BUILD_DEPENDS+=		go-bin-[0-9]*:../../lang/go-bin
 GOROOT_BOOTSTRAP=	${PREFIX}/go-bin
+.  elif defined(GO_BOOTSTRAP_REQD)
+BUILD_DEPENDS+=		go${GO_BOOTSTRAP_REQD}-[0-9]*:../../lang/go${GO_BOOTSTRAP_REQD}
+GOROOT_BOOTSTRAP=	${PREFIX}/${GO_BOOTSTRAP_REQD}
 .  else
 BUILD_DEPENDS+=		go14-1.4*:../../lang/go14
 GOROOT_BOOTSTRAP=	${PREFIX}/go14
