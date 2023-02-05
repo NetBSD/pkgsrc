e6572f7d5f73d4001afa09fee3f2a3682914ec8e
he <he@pkgsrc.org>
2023-02-05 09:05:28 +0000

    mail/thunderbird: Update to version 102.6.1.
    This is copied from wip/thunderbird, which builds with
    more recent versions of rust.
    
    This is the version just before this change was implemented:
    
    https://blog.thunderbird.net/2023/01/important-message-for-microsoft-office-365-enterprise-users/
    
    and this version will stay here for a while before we update
    again.
    
    Call me lazy, but I don't think it's worth anyone's time to paste
    in the change log from version 78 to 102.6.1 here, and it doesn't
    appear to be readily accessible in the source distribution.  That
    said, the list of security fixes implemented between version 78.12
    and 102.6.1 is available at
    
    https://www.mozilla.org/en-US/security/known-vulnerabilities/thunderbird/#thunderbird102.6.1
    
    and the release notes are available at
    
    https://www.thunderbird.net/en-US/thunderbird/releases/

--- mail/thunderbird/patches/patch-config_gcc-stl-wrapper.template.h
+++ mail/thunderbird/patches/patch-config_gcc-stl-wrapper.template.h
@@ -1,19 +1,19 @@
-$NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.2 2020/09/03 15:26:22 ryoon Exp $
+$NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.3 2023/02/05 09:05:29 he Exp $
 
 --- config/gcc-stl-wrapper.template.h.orig	2020-08-28 21:32:29.000000000 +0000
 +++ config/gcc-stl-wrapper.template.h
-@@ -28,8 +28,8 @@
- #endif
- 
- // Don't include mozalloc for cstdlib. See bug 1245076.
+@@ -32,8 +32,8 @@
+ // limits) and bug 1694575 (iosfwd).
+ // Please be careful when adding more exceptions, especially regarding
+ // the header not directly or indirectly including <new>.
 -#ifndef moz_dont_include_mozalloc_for_cstdlib
 -#  define moz_dont_include_mozalloc_for_cstdlib
 +#ifndef moz_dont_include_mozalloc_for_${HEADER}
 +#  define moz_dont_include_mozalloc_for_${HEADER}
  #endif
  
- #ifndef moz_dont_include_mozalloc_for_type_traits
-@@ -43,7 +43,7 @@
+ #ifndef moz_dont_include_mozalloc_for_cmath
+@@ -55,7 +55,7 @@
  // Include mozalloc after the STL header and all other headers it includes
  // have been preprocessed.
  #if !defined(MOZ_INCLUDE_MOZALLOC_H) && \
@@ -22,7 +22,7 @@ $NetBSD: patch-config_gcc-stl-wrapper.template.h,v 1.2 2020/09/03 15:26:22 ryoon
  #  define MOZ_INCLUDE_MOZALLOC_H
  #  define MOZ_INCLUDE_MOZALLOC_H_FROM_${HEADER}
  #endif
-@@ -72,4 +72,6 @@
+@@ -84,4 +84,6 @@
  #  include "mozilla/throw_gcc.h"
  #endif
  
