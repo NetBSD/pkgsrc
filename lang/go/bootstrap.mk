bcc729e70aced4d90a5d5f8c7399b9d2c0bdaa89
ryoon <ryoon@pkgsrc.org>
2023-02-06 20:53:02 +0000

    go: Fix path for bootstrap kit
    * Fix lang/go120 build.

--- lang/go/bootstrap.mk
+++ lang/go/bootstrap.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bootstrap.mk,v 1.10 2023/02/02 09:23:41 tnn Exp $
+# $NetBSD: bootstrap.mk,v 1.11 2023/02/06 20:53:02 ryoon Exp $
 
 .if !defined(GOROOT_BOOTSTRAP) || !exists(${GOROOT_BOOTSTRAP}/bin/go)
 .  if ${MACHINE_ARCH} == "aarch64" || \
@@ -8,7 +8,7 @@ BUILD_DEPENDS+=		go-bin-[0-9]*:../../lang/go-bin
 GOROOT_BOOTSTRAP=	${PREFIX}/go-bin
 .  elif defined(GO_BOOTSTRAP_REQD)
 BUILD_DEPENDS+=		go${GO_BOOTSTRAP_REQD}-[0-9]*:../../lang/go${GO_BOOTSTRAP_REQD}
-GOROOT_BOOTSTRAP=	${PREFIX}/${GO_BOOTSTRAP_REQD}
+GOROOT_BOOTSTRAP=	${PREFIX}/go${GO_BOOTSTRAP_REQD}
 .  else
 BUILD_DEPENDS+=		go14-1.4*:../../lang/go14
 GOROOT_BOOTSTRAP=	${PREFIX}/go14
