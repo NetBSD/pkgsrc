87d3cf4448db89315bc70866cb5088f023ad9813
pho <pho@pkgsrc.org>
2023-02-08 08:41:26 +0000

    devel/hs-microlens-ghc: Update to 0.4.14.1
    (I even did "make cce" but I somehow forgot to commit this.)
    
    0.4.14
    * New minor release (microlens-0.4.13.0).

--- devel/hs-microlens-ghc/buildlink3.mk
+++ devel/hs-microlens-ghc/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/02/07 01:40:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/02/08 08:41:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-microlens-ghc
 
 .if !defined(HS_MICROLENS_GHC_BUILDLINK3_MK)
 HS_MICROLENS_GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-microlens-ghc+=	hs-microlens-ghc>=0.4.13
-BUILDLINK_ABI_DEPENDS.hs-microlens-ghc+=	hs-microlens-ghc>=0.4.13.1nb2
+BUILDLINK_API_DEPENDS.hs-microlens-ghc+=	hs-microlens-ghc>=0.4.14
+BUILDLINK_ABI_DEPENDS.hs-microlens-ghc+=	hs-microlens-ghc>=0.4.14.1
 BUILDLINK_PKGSRCDIR.hs-microlens-ghc?=		../../devel/hs-microlens-ghc
 
 .include "../../devel/hs-microlens/buildlink3.mk"
