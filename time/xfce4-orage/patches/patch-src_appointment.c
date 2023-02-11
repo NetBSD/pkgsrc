e1d44cb983f172f08a430df859f6677591889bcf
gutteridge <gutteridge@pkgsrc.org>
2023-02-11 01:26:12 +0000

    xfce4-orage: update to 4.18.0
    Change log:
    
    4.18.0 (2023-02-08)
    ======
    - Changed Orage internal time and data strings to GDateTime (MR !20)
    - In event list, fixed event hiding when event start at 00:00 and end 00:00 in
      same date (this kind event can be created when creating full day event, save
      event and then unset full day option). (MR !20)
    - Removed bundled libical (Issue #5)
    - Removed panel plugin (Issue #11)
    - Removed GlobalTime (Issue #12)
    - Replaced old Orage icons (Issue #16) and removed 160x160 icons
    - Replaced deprecated dbus-glib with GDBus (Issue #4)
    - Changed event attach order in day view window (Issue #15)
    - Added Wayland support (Issue #2)
    - Fixed orage_persistent_alarms.txt not found messages (MR !35)
    - Fixed after wakeup tray icon updating (Issue #14)
    - Changed alarm_structure to reference counted structure. This fixes warnings
      about memory leaks (MR !38, replaces MR !8)
    - Changed libnotify minimum version to 0.7.0 (MR !39)
    - Disabled sound alarm when sound command is not set (MR !40)
    - Fixed large ical file import (Issue #8)
    - Translation Updates:
      Albanian, Amharic, Arabic, Armenian (Armenia), Asturian, Basque,
      Belarusian, Bulgarian, Catalan, Chinese (China), Chinese (Taiwan),
      Croatian, Czech, Danish, Dutch, English (Australia), English (United
      Kingdom), Esperanto, Estonian, Finnish, French, Galician, German,
      Greek, Hebrew, Hungarian, Icelandic, Indonesian, Italian, Japanese,
      Kazakh, Korean, Latvian, Lithuanian, Malay, Norwegian Bokml,
      Norwegian Nynorsk, Occitan (post 1500), Polish, Portuguese,
      Portuguese (Brazil), Romanian, Russian, Serbian, Slovak, Spanish,
      Swedish, Thai, Turkish, Ukrainian, Urdu, Urdu (Pakistan), Uyghur,
      Vietnamese

--- time/xfce4-orage/patches/patch-src_appointment.c
+++ time/xfce4-orage/patches/patch-src_appointment.c
@@ -1,16 +1,15 @@
-$NetBSD: patch-src_appointment.c,v 1.1 2018/05/17 09:13:02 jperkin Exp $
+$NetBSD: patch-src_appointment.c,v 1.2 2023/02/11 01:26:12 gutteridge Exp $
 
 Set _XOPEN_SOURCE correctly on SunOS.
 
---- src/appointment.c.orig	2015-04-10 10:26:26.000000000 +0000
+--- src/appointment.c.orig	2023-01-27 10:02:10.000000000 +0000
 +++ src/appointment.c
-@@ -25,8 +25,10 @@
+@@ -25,7 +25,9 @@
  #  include <config.h>
  #endif
  
 +#if !defined(__sun) || (!defined(_XOPEN_SOURCE) && (__STDC_VERSION__-0 < 199901L))
  #define _XOPEN_SOURCE /* glibc2 needs this */
- #define _XOPEN_SOURCE_EXTENDED 1 /* strptime needs this in posix systems */
 +#endif
  
  #include <sys/types.h>
