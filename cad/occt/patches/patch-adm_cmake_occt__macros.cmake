6cf76cc732828390e7c91e18f5a4eda4ccbbc23d
tnn <tnn@pkgsrc.org>
2023-02-12 16:03:28 +0000

    occt: fix DESTDIR issues during make replace

---
+++ cad/occt/patches/patch-adm_cmake_occt__macros.cmake
@@ -0,0 +1,15 @@
+$NetBSD: patch-adm_cmake_occt__macros.cmake,v 1.1 2023/02/12 16:03:28 tnn Exp $
+
+Fix DESTDIR issues.
+
+--- adm/cmake/occt_macros.cmake.orig	2022-11-11 22:19:44.000000000 +0000
++++ adm/cmake/occt_macros.cmake
+@@ -592,7 +592,7 @@ macro (OCCT_UPDATE_TARGET_FILE)
+ 
+   install (CODE
+   "string (TOLOWER \"\${CMAKE_INSTALL_CONFIG_NAME}\" CMAKE_INSTALL_CONFIG_NAME_LOWERCASE)
+-  file (GLOB ALL_OCCT_TARGET_FILES \"${INSTALL_DIR}/${INSTALL_DIR_CMAKE}/OpenCASCADE*Targets-\${CMAKE_INSTALL_CONFIG_NAME_LOWERCASE}.cmake\")
++  file (GLOB ALL_OCCT_TARGET_FILES \"\$ENV{DESTDIR}${INSTALL_DIR}/${INSTALL_DIR_CMAKE}/OpenCASCADE*Targets-\${CMAKE_INSTALL_CONFIG_NAME_LOWERCASE}.cmake\")
+   foreach(TARGET_FILENAME \${ALL_OCCT_TARGET_FILES})
+     file (STRINGS \"\${TARGET_FILENAME}\" TARGET_FILE_CONTENT)
+     file (REMOVE \"\${TARGET_FILENAME}\")
