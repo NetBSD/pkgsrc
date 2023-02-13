dd173db4a1f2f4a878577812fc915503fbf5a974
wiz <wiz@pkgsrc.org>
2023-02-13 08:40:34 +0000

    libdeflate: has a shared library too, so do not default to build dependency

--- devel/libdeflate/buildlink3.mk
+++ devel/libdeflate/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/02/13 08:28:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/02/13 08:40:34 wiz Exp $
 
 BUILDLINK_TREE+=	libdeflate
 
 .if !defined(LIBDEFLATE_BUILDLINK3_MK)
 LIBDEFLATE_BUILDLINK3_MK:=
-
-BUILDLINK_DEPMETHOD.libdeflate?=	build
 
 BUILDLINK_API_DEPENDS.libdeflate+=	libdeflate>=1.10
 BUILDLINK_PKGSRCDIR.libdeflate?=	../../devel/libdeflate
