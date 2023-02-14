0e333d5d63e8054e8b574447467b9e01d2845889
tnn <tnn@pkgsrc.org>
2023-02-14 00:33:12 +0000

    podman: update to 4.4.1
    Changelog too long and pertains mostly to Linux.
    Importantly this update makes 9pfs volume mounts work on macOS
    and we can start to work on getting the QEMU side to support
    this on NetBSD.
    
    Note: because 9pfs volume mounts are not yet supported on NetBSD,
    I have patched this podman version to have a "none" volume driver
    and to initialize the machine with "--volume-driver none" by default
    instead of virtfs.
    
    To get volumes on macOS with this package you therefore must pass
    "--volume-driver virtfs" explicitly.

--- sysutils/podman/patches/patch-vendor_github.com_containers_storage_pkg_homedir_homedir__others.go
+++ sysutils/podman/patches/patch-vendor_github.com_containers_storage_pkg_homedir_homedir__others.go
@@ -1,11 +1,13 @@
-$NetBSD: patch-vendor_github.com_containers_storage_pkg_homedir_homedir__others.go,v 1.1 2021/09/04 20:28:42 maya Exp $
+$NetBSD: patch-vendor_github.com_containers_storage_pkg_homedir_homedir__others.go,v 1.2 2023/02/14 00:33:13 tnn Exp $
 
 Add netbsd support (vendor code)
 
---- vendor/github.com/containers/storage/pkg/homedir/homedir_others.go.orig	2021-08-30 19:15:26.000000000 +0000
+--- vendor/github.com/containers/storage/pkg/homedir/homedir_others.go.orig	2023-02-08 19:03:18.000000000 +0000
 +++ vendor/github.com/containers/storage/pkg/homedir/homedir_others.go
-@@ -1,4 +1,4 @@
+@@ -1,5 +1,5 @@
+-//go:build !linux && !darwin && !freebsd
 -// +build !linux,!darwin,!freebsd
++//go:build !linux && !darwin && !freebsd && !netbsd
 +// +build !linux,!darwin,!freebsd,!netbsd
  
  package homedir
