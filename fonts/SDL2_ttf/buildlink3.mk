05196c0f2345fc858a715a8a9aa8dba32da2f505
wiz <wiz@pkgsrc.org>
2023-02-14 16:04:12 +0000

    SDL2_ttf: update to 2.20.2.
    Force usage of freetype2 and harfbuzz from pkgsrc,
    instead of using included copies
    
    Changes:
    
    This is a stable bugfix release, with the following changes:
    
        Fixed building from source without running autogen.sh

--- fonts/SDL2_ttf/buildlink3.mk
+++ fonts/SDL2_ttf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/01/29 21:14:34 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/02/14 16:04:12 wiz Exp $
 
 BUILDLINK_TREE+=	SDL2_ttf
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	SDL2_ttf
 SDL2_TTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.0.12
-BUILDLINK_ABI_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.20.1nb2
+BUILDLINK_ABI_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.20.2
 BUILDLINK_PKGSRCDIR.SDL2_ttf?=		../../fonts/SDL2_ttf
 
 .include "../../devel/SDL2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../fonts/harfbuzz/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .endif # SDL2_TTF_BUILDLINK3_MK
 
