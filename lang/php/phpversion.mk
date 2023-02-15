5569579c6ebf3b11a473dc16368a626ef8365b2c
taca <taca@pkgsrc.org>
2023-02-15 14:15:11 +0000

    lang/php82: update to 8.2.3
    14 Feb 2023, PHP 8.2.3
    
    - Core:
      . Fixed bug #81744 (Password_verify() always return true with some hash).
        (CVE-2023-0567) (Tim Düsterhus)
      . Fixed bug #81746 (1-byte array overrun in common path resolve code).
        (CVE-2023-0568) (Niels Dossche)
    
    - FPM:
      . Fixed bug GHSA-54hq-v5wp-fqgv (DOS vulnerability when parsing multipart
        request body). (CVE-2023-0662) (Jakub Zelenka)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.389 2023/02/15 14:13:06 taca Exp $
+# $NetBSD: phpversion.mk,v 1.390 2023/02/15 14:15:11 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -91,7 +91,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.28
 PHP81_VERSION=	8.1.15
-PHP82_VERSION=	8.2.2
+PHP82_VERSION=	8.2.3
 
 # Define API version or initial release of major version.
 PHP56_RELDATE=	20140828
