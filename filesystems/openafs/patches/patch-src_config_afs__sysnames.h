e6da601dd0fdcf3be6070d2cd09768d3c26abbf0
he <he@pkgsrc.org>
2023-02-16 18:56:44 +0000

    openafs: bring the ability to build & install to NetBSD/macppc 10.0_BETA.
    I see fertile ground for similar fixes for other archs in this
    package.  The 10.0-specific files were copied from the 9.0 files
    and minimally modified (actually, the ppc_nbsd100 file was from
    the ppc_nbsd20 file).  New ID assigned for NetBSD/macppc 10.0.
    
    No functional testing done: "it builds; ship it!" :)

--- filesystems/openafs/patches/patch-src_config_afs__sysnames.h
+++ filesystems/openafs/patches/patch-src_config_afs__sysnames.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_config_afs__sysnames.h,v 1.5 2022/03/19 16:19:02 jakllsch Exp $
+$NetBSD: patch-src_config_afs__sysnames.h,v 1.6 2023/02/16 18:56:44 he Exp $
 
 - Add SYS_NAME_IDs for NetBSD 8 to 9 on arm/i386/amd64
 
 --- src/config/afs_sysnames.h.orig	2021-12-09 17:07:41.000000000 +0000
 +++ src/config/afs_sysnames.h
-@@ -297,6 +297,11 @@
+@@ -297,6 +297,12 @@
  #define SYS_NAME_ID_i386_nbsd60         2543
  #define SYS_NAME_ID_amd64_nbsd70        2544
  #define SYS_NAME_ID_i386_nbsd70         2545
@@ -13,6 +13,7 @@ $NetBSD: patch-src_config_afs__sysnames.h,v 1.5 2022/03/19 16:19:02 jakllsch Exp
 +#define SYS_NAME_ID_i386_nbsd80         2548
 +#define SYS_NAME_ID_amd64_nbsd90        2549
 +#define SYS_NAME_ID_i386_nbsd90         2550
++#define SYS_NAME_ID_macppc_nbsd100      2551
  
  #define SYS_NAME_ID_i386_obsd31		2600
  #define SYS_NAME_ID_i386_obsd32		2601
