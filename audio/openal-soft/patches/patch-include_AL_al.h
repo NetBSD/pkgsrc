7dc52ae859419e13d8f6f8b0be5ac5d51903283e
wiz <wiz@pkgsrc.org>
2023-02-16 15:15:55 +0000

    openal-soft: update to 1.23.0.
    openal-soft-1.23.0:
    
        Fixed CoreAudio capture support.
    
        Fixed handling per-version EAX properties.
    
        Fixed interpolating changes to the Super Stereo width source property.
    
        Fixed detection of the update and buffer size from PipeWire.
    
        Fixed resuming playback devices with OpenSL.
    
        Fixed support for certain OpenAL implementations with the router.
    
        Improved reverb environment transitions.
    
        Improved performance of convolution reverb.
    
        Improved quality and performance of the pitch shifter effect slightly.
    
        Improved sub-sample precision for resampled sources.
    
        Improved blending spatialized multi-channel sources that use the source
        radius property.
    
        Improved mixing 2D ambisonic sources for higher-order 3D ambisonic mixing.
    
        Improved quadraphonic and 7.1 surround sound output slightly.
    
        Added config options for UHJ encoding/decoding quality. Including Super
        Stereo processing.
    
        Added a config option for specifying the speaker distance.
    
        Added a compatibility config option for specifying the NFC distance
        scaling.
    
        Added a config option for mixing on PipeWire's non-real-time thread.
    
        Added support for virtual source nodes with PipeWire capture.
    
        Added the ability for the WASAPI backend to use different playback rates.
    
        Added support for SOFA files that define per-response delays in makemhr.
    
        Changed the default fallback playback sample rate to 48khz. This doesn't
        affect most backends, which can detect a default rate from the system.
    
        Changed the default resampler to cubic.
    
        Changed the default HRTF size from 32 to 64 points.

--- audio/openal-soft/patches/patch-include_AL_al.h
+++ audio/openal-soft/patches/patch-include_AL_al.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-include_AL_al.h,v 1.1 2020/04/09 13:35:21 jperkin Exp $
+$NetBSD: patch-include_AL_al.h,v 1.2 2023/02/16 15:15:55 wiz Exp $
 
 Fix SunOS build, int8_t is not always the same as signed char.
 
---- include/AL/al.h.orig	2020-01-24 07:28:45.000000000 +0000
+--- include/AL/al.h.orig	2023-02-03 14:48:18.000000000 +0000
 +++ include/AL/al.h
 @@ -41,7 +41,12 @@ typedef char ALboolean;
  typedef char ALchar;
  
- /** signed 8-bit 2's complement integer */
+ /** signed 8-bit integer */
 +#ifdef __sun
 +#include <inttypes.h>
 +typedef int8_t ALbyte;
