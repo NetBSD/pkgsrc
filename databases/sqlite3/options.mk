29312f56e7cce3978f2de8e5bf2bdc317fc56705
wiz <wiz@pkgsrc.org>
2023-02-20 07:28:31 +0000

    sqlite3: remove rtree option, this has been turned-on by default for some time

--- databases/sqlite3/options.mk
+++ databases/sqlite3/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2020/03/23 19:37:54 rmind Exp $
+# $NetBSD: options.mk,v 1.7 2023/02/20 07:28:31 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sqlite3
-PKG_SUPPORTED_OPTIONS=		fts icu rtree deserialize
+PKG_SUPPORTED_OPTIONS=		fts icu deserialize
 PKG_SUGGESTED_OPTIONS=		fts # enabled in NetBSD builtin version
 
 .include "../../mk/bsd.options.mk"
@@ -14,10 +14,6 @@ CFLAGS+=	-DSQLITE_ENABLE_FTS4=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS=1
 CFLAGS+=	-DSQLITE_ENABLE_ICU=1
 LDFLAGS+=	-licui18n -licuuc -licudata
 .include "../../textproc/icu/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mrtree)
-CFLAGS+=	-DSQLITE_ENABLE_RTREE=1
 .endif
 
 .if !empty(PKG_OPTIONS:Mdeserialize)
