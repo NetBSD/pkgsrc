18fedafca680e2aaf8327f1770551d8d1a90c6c6
adam <adam@pkgsrc.org>
2023-02-21 06:31:44 +0000

    py-pip: updated to 23.0.1
    23.0.1 (2023-02-17)
    ===================
    
    Features
    --------
    
    - Ignore PIP_REQUIRE_VIRTUALENV for ``pip index``
    - Implement ``--break-system-packages`` to permit installing packages into
      ``EXTERNALLY-MANAGED`` Python installations.
    
    Bug Fixes
    ---------
    
    - Improve handling of isolated build environments on platforms that
      customize the Python's installation schemes, such as Debian and
      Homebrew.
    - Do not crash in presence of misformatted hash field in ``direct_url.json``.
    
    
    23.0 (2023-01-30)
    =================
    
    Features
    --------
    
    - Change the hashes in the installation report to be a mapping. Emit the
      ``archive_info.hashes`` dictionary in ``direct_url.json``.
    - Implement logic to read the ``EXTERNALLY-MANAGED`` file as specified in PEP 668.
      This allows a downstream Python distributor to prevent users from using pip to
      modify the externally managed environment.
    - Enable the use of ``keyring`` found on ``PATH``. This allows ``keyring``
      installed using ``pipx`` to be used by ``pip``.
    - The inspect and installation report formats are now declared stabled, and their version
      has been bumped from ``0`` to ``1``.
    
    Bug Fixes
    ---------
    
    - Wheel cache behavior is restored to match previous versions, allowing the
      cache to find existing entries.
    - Use the "venv" scheme if available to obtain prefixed lib paths.
    - Deprecated a historical ambiguity in how ``egg`` fragments in URL-style
      requirements are formatted and handled. ``egg`` fragments that do not look
      like PEP 508 names now produce a deprecation warning.
    - Fix scripts path in isolated build environment on Debian.
    - Make ``pip show`` show the editable location if package is editable
    - Stop checking that ``wheel`` is present when ``build-system.requires``
      is provided without ``build-system.build-backend`` as ``setuptools``
      (which we still check for) will inject it anyway.
    - Fix an issue when an already existing in-memory distribution would cause
      exceptions in ``pip install``
    
    Vendored Libraries
    ------------------
    
    - Upgrade certifi to 2022.12.7
    - Upgrade chardet to 5.1.0
    - Upgrade colorama to 0.4.6
    - Upgrade distro to 1.8.0
    - Remove pep517 from vendored packages
    - Upgrade platformdirs to 2.6.2
    - Add pyproject-hooks 1.0.0
    - Upgrade requests to 2.28.2
    - Upgrade rich to 12.6.0
    - Upgrade urllib3 to 1.26.14
    
    Improved Documentation
    ----------------------
    
    - Fixed the description of the option "--install-options" in the documentation
    - Remove mention that editable installs are necessary for pip freeze to report the VCS
      URL.
    - Clarify that the egg URL fragment is only necessary for editable VCS installs, and
      otherwise not necessary anymore.

--- devel/py-pip/patches/patch-src_pip___internal_build__env.py
+++ devel/py-pip/patches/patch-src_pip___internal_build__env.py
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_pip___internal_build__env.py,v 1.4 2022/08/10 09:17:48 adam Exp $
+$NetBSD: patch-src_pip___internal_build__env.py,v 1.5 2023/02/21 06:31:44 adam Exp $
 
 Copy distutils distro config file to overlay.
 https://github.com/pypa/pip/issues/10949
 
---- src/pip/_internal/build_env.py.orig	2022-08-03 18:55:14.000000000 +0000
+--- src/pip/_internal/build_env.py.orig	2023-02-17 18:31:10.000000000 +0000
 +++ src/pip/_internal/build_env.py
-@@ -7,6 +7,7 @@ import pathlib
+@@ -8,6 +8,7 @@ import site
  import sys
  import textwrap
  from collections import OrderedDict
 +from shutil import copy
- from sysconfig import get_paths
  from types import TracebackType
- from typing import TYPE_CHECKING, Iterable, List, Optional, Set, Tuple, Type
-@@ -81,6 +82,9 @@ class BuildEnvironment:
+ from typing import TYPE_CHECKING, Iterable, List, Optional, Set, Tuple, Type, Union
+ 
+@@ -102,6 +103,9 @@ class BuildEnvironment:
          self._site_dir = os.path.join(temp_dir.path, "site")
          if not os.path.exists(self._site_dir):
              os.mkdir(self._site_dir)
