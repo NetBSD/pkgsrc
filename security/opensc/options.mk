1a360d74f82d7cfee92b2a0bbdac9b43bec88b41
adam <adam@pkgsrc.org>
2023-02-21 17:23:07 +0000

    opensc: updated to 0.23.0
    New in 0.23.0; 2022-11-29
    
    General improvements
    
    Support signing of data with a length of more than 512 bytes
    By default, disable support for old card drivers
    Bump minimal required OpenSSL version to 1.1.1 and add support for OpenSSL 3.0
    Compatibility with LibreSSL
    Remove support for DSA
    Extend p11test to support symmetric keys
    Notice detached reader on macOS
    Support for OAEP padding
    Fix for PSS salt length
    Improve fuzzing by adding new tests
    Fixed various issues reported by OSS-Fuzz and Coverity regarding card drivers, PKCS#11 and PKCS#15 init
    Fix issues with OpenPACE
    Containers support for local testing
    Add support for encryption and decryption using symmetric keys
    Stop building support for Gost algorithms with OpenSSL 3.0 as they require deprecated API
    Fix detection of disconnected readers in PCSC
    Add configuration option for on-disk caching of private data
    Skip building empty binaries when dependencies are missing and remove needless linking
    Define arm64 as a supported architecture in the Installer package
    PKCS#11
    
    Implement C_CreateObject for EC keys and fix signature verification for CKM_ECDSA_SHAx cards
    pkcs11-tool
    
    Add more elliptic curves
    Add support for symmetric encrypt and decrypt, wrap and unwrap operations, and initialization vector
    Fix consistent handling of secret key attributes
    Add support for signing and verifying with HMAC
    Add support for SHA3
    Make object selectable via label
    Do not require an R/W session for some operations and add --session-rw option
    Print more information: CKA_UNIQUE_ID attribute, SHA3 HMACs and serial number for certificates
    Add new option --undestroyable to create keys with CKA_DESTROYABLE=FALSE
    sc-hsm-tool
    
    Add options for public key authentication
    Minidriver
    
    Fix reinit of the card
    Add an entry for Italian CNS (e)
    Fix detection of ECC mechanisms
    Fix ATRs before adding them to the windows registry
    NQ-Applet
    
    Add support for the JCOP4 Cards with NQ-Applet
    ItaCNS
    
    Add support for ItaCMS v1.1 (key length 2048)
    Belpic
    
    Add support for applet v1.8
    Starcos
    
    Add ATR for V3.4
    Add PKCS#15 emulator for 3.x cards with eSign app
    ePass2003
    
    Fix PKCS#15 initialization
    Add support for FIPS
    Fix matching with newer versions and tokens initialized with OpenSC
    MyEID
    
    Support logout operation
    Support for symmetric encryption and decryption
    GIDS
    
    Fix decipher for TPM
    OpenPGP
    
    Get the list of supported algorithms from algorithm information on the card
    Support for 3 certificates with OpenPGP 3+
    nPA
    
    Fix card detection
    Rutoken
    
    Fix formatting rtecp cards
    PIV
    
    Add new PIVKey ATRs for current cards

--- security/opensc/options.mk
+++ security/opensc/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2019/11/04 21:12:56 rillig Exp $
+# $NetBSD: options.mk,v 1.6 2023/02/21 17:23:07 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensc
 
@@ -10,12 +10,24 @@ PKG_SUGGESTED_OPTIONS=		pcsc-lite
 
 .include "../../mk/bsd.options.mk"
 
+.if ${OPSYS} == "Darwin"
+SOEXT=	dylib
+.else
+SOEXT=	so
+.endif
+
 .if !empty(PKG_OPTIONS:Mpcsc-lite)
 .include "../../security/pcsc-lite/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-pcsc
+.  if ${OPSYS} == "Darwin" && exists(/System/Library/Frameworks/PCSC.framework)
+# On Darwin, prefer system-provided PCSC.
+.  else
 # While one might expect the libtool .la, ltdl support has been
 # removed from opensc.
-CONFIGURE_ARGS+=	--with-pcsc-provider=${BUILDLINK_PREFIX.pcsc-lite}/lib/libpcsclite.so
+CONFIGURE_ARGS+=	--with-pcsc-provider=${BUILDLINK_PREFIX.pcsc-lite}/lib/libpcsclite.${SOEXT}
+.  endif
+# This is perhaps an upstream bug.
+CONFIGURE_ENV+=		PCSC_CFLAGS=-I${BUILDLINK_PREFIX.pcsc-lite}/include/PCSC
 .else
 CONFIGURE_ARGS+=	--disable-pcsc
 .endif
