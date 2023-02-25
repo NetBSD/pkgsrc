ed298a31b7f52be1a5ca500489643705b8d96d00
bacon <bacon@pkgsrc.org>
2023-02-25 13:50:38 +0000

    biology/htslib: Update to 1.17
    Numerous enhancements and fixes
    
    Changes: https://github.com/samtools/htslib/releases

--- biology/htslib/buildlink3.mk
+++ biology/htslib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/06/28 11:31:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/02/25 13:50:38 bacon Exp $
 
 BUILDLINK_TREE+=	htslib
 
 .if !defined(HTSLIB_BUILDLINK3_MK)
 HTSLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.14
-BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.15nb1
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.17
+BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.17
 BUILDLINK_PKGSRCDIR.htslib?=	../../biology/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
