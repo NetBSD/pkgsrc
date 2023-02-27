e950e0db80e22e737a0e460d053eaf36d6351c1a
jperkin <jperkin@pkgsrc.org>
2023-02-27 13:07:46 +0000

    py-pythran: Add SunOS to exclusion list.
    Bump PKGREVISION.

--- math/py-pythran/patches/patch-third__party_xsimd_arch_xsimd__scalar.hpp
+++ math/py-pythran/patches/patch-third__party_xsimd_arch_xsimd__scalar.hpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-third__party_xsimd_arch_xsimd__scalar.hpp,v 1.1 2023/01/25 09:06:54 adam Exp $
+$NetBSD: patch-third__party_xsimd_arch_xsimd__scalar.hpp,v 1.2 2023/02/27 13:07:47 jperkin Exp $
 
 https://github.com/serge-sans-paille/pythran/issues/2070
 
@@ -9,7 +9,7 @@ https://github.com/serge-sans-paille/pythran/issues/2070
      }
  
 -#if defined(_GNU_SOURCE) && !defined(__APPLE__) && !defined(__MINGW32__) && !defined(__ANDROID__)
-+#if defined(_GNU_SOURCE) && !defined(__APPLE__) && !defined(__MINGW32__) && !defined(__ANDROID__) && !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__)
++#if defined(_GNU_SOURCE) && !defined(__APPLE__) && !defined(__MINGW32__) && !defined(__ANDROID__) && !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__) && !defined(__sun)
      inline float exp10(const float& x) noexcept
      {
          return ::exp10f(x);
