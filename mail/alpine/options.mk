60709ac007cc184721de823d7b10d8dad7bf9cdf
vins <vins@pkgsrc.org>
2023-02-27 20:08:37 +0000

    mail/alpine: package revision.
    * Add kerberos and pam build options (enabled by default).
    * Add inet6 and pthread build options (enabled by default if supported).
    * Remove tcl support from options.mk until an install target for web alpine
      files isn't implemented.
    * Pull upstream patch providing additional compose subcommands.
    * Bump revision.

--- mail/alpine/options.mk
+++ mail/alpine/options.mk
@@ -1,9 +1,20 @@
-# $NetBSD: options.mk,v 1.6 2023/02/26 20:39:49 vins Exp $
+# $NetBSD: options.mk,v 1.7 2023/02/27 20:08:37 vins Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.alpine
 
-PKG_SUPPORTED_OPTIONS+=	aspell ldap tcl
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS+=	aspell kerberos inet6 ldap pam threads
+PKG_SUGGESTED_OPTIONS=	kerberos pam
+
+CHECK_BUILTIN.pthread:= yes
+.include "../../mk/pthread.builtin.mk"
+CHECK_BUILTIN.pthread:= no
+.if ${BUILTIN_LIB_FOUND.pthread:U:tl} == yes
+PKG_SUGGESTED_OPTIONS+=	threads
+.endif
+
+.if ${IPV6_READY:tl} == "yes"
+PKG_SUGGESTED_OPTIONS+=	inet6
+.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -19,6 +30,36 @@ CONFIGURE_ENV+=		ac_cv_path_alpine_interactive_spellcheck=${PREFIX}/bin/aspell
 .endif
 
 ###
+### IPv6 support
+###
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--with-ipv6
+.else
+CONFIGURE_ARGS+=	--without-ipv6
+.endif
+
+###
+### Support POSIX threads
+###
+.if !empty(PKG_OPTIONS:Mthreads)
+.  include "../../mk/pthread.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pthread
+.else
+CONFIGURE_ARGS+=	--without-pthread
+.endif
+
+###
+### Support GSSAPI authentication via Kerberos 5.
+###
+.if !empty(PKG_OPTIONS:Mkerberos)
+.  include "../../mk/krb5.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-krb5-dir=${KRB5BASE:Q}
+CONFIGURE_ENV+=	alpine_GSSTYPE=${KRB5_TYPE}
+.else
+CONFIGURE_ARGS+=	 --without-krb5
+.endif
+
+###
 ### LDAP server support
 ###
 .if !empty(PKG_OPTIONS:Mldap)
@@ -29,11 +70,11 @@ CONFIGURE_ARGS+=    	--without-ldap
 .endif
 
 ###
-###  Web Alpine browser
+### PAM support
 ###
-.if !empty(PKG_OPTIONS:Mtcl)
-.include "../../lang/tcl/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-tcl-lib-dir=${BUILDLINK_PREFIX.tcl}/lib
+.if !empty(PKG_OPTIONS:Mpam)
+.  include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ENV+=		ac_cv_lib_pam_pam_start=yes
 .else
-CONFIGURE_ARGS+=	--without-tcl
+CONFIGURE_ENV+=		ac_cv_lib_pam_pam_start=no
 .endif
