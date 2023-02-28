01e7795b503a005e50373eb0bd53bac08a4cbf19
bouyer <bouyer@pkgsrc.org>
2023-02-28 11:33:03 +0000

    Update mimedefang to 3.3. Main changes from 2.78:         * add is_public_ip6_address to check if an ipv6 address 	  is local         * add md_authres method to generate a basic Authentication-Results           header for the message         * add md_arc_sign method to sign email messages           with DKIM ARC signatures         * add md_dkim_verify method to verify DKIM signatures         * add md_dkim_sign method to sign email messages           with DKIM signatures         * add anonymize_uri to remove utm_* parameters           from uris.         * split mimedefang.pl code in Perl modules         * add re_match_in_7zip_directory to check for files           inside 7zip archives         * fallback to plaintext when md_check_against_smtp_server           fails SSL connection for unknown reasons         * add experimental support to scan emails with Rspamd antispam         * Obtain the Queue-ID as early as possible in the SMTP           session. Requires the "-y" command-line option to mimedefang.         * mimedefang.pl: Add support for a configuration file           to separate data from code         * mimedefang.pl: Add support to scan messages for viruses on a remote           Clamav server using clamdscan client.         * mimedefang.pl: Add re_match_in_rar_directory function to match           unwanted file names extensions inside a rar archive file.         * mimedefang.pl: Added TLS support to md_check_against_smtp_server         * mimedefang-multiplexor: Make "workerinfo nnn" show how long ago           the last state change was for a given worker.         * mimedefang.pl: Do not add a Message-ID: header when handing a           message to SpamAssassin if the original message lacks such a           header.         * Add a new -V maxLifetime option to mimedefang-multiplexor that           terminates worker processes after maxLifetime seconds (approximately).           This is in addition to the -r maxRequests option.         * Log the lifetime and number of requests processed when we terminate           a worker process.         * Make mimedefang and mimedefang-multiplexor write their PID files           as root to avoid an unprivileged user tampering with the pidfiles.           Thanks to Michael Orlitzky for pointing this issue out.         * mimedefang.pl: Add an extra level of subdirectories in the quarantine           to avoid 32K subdirectory limit on ext3.  Idea by Kevin McGrail.         * Add the --data-dump option to scripts/mimedefang-util
    And various bug fixes and minor improvements.
    pkgsrc changes: make the rc.d script use the new -o option and move the pid
    files to $VARBASE/run/, keeping the lock and socket files in
    $VARBASE/spool/MIMEdefang/

--- mail/mimedefang/files/mimedefang.sh
+++ mail/mimedefang/files/mimedefang.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: mimedefang.sh,v 1.1 2004/03/07 11:18:33 markd Exp $
+# $NetBSD: mimedefang.sh,v 1.2 2023/02/28 11:33:03 bouyer Exp $
 #
 # mimedefang does "in-stream" spam and virus checking using 
 # sendmail's MailFilter interfaces.
@@ -25,9 +25,10 @@ defangdir="@DEFANG_SPOOLDIR@"
 name="mimedefang"
 rcvar=$name
 command="@PREFIX@/bin/mimedefang"
-pidfile="${defangdir}/${name}.pid"
-mimedefang_user=${mimedefang_user-"@DEFANG_USER@"}
-command_args="-P ${pidfile}"
+pidfile="@VARBASE@/run/${name}.pid"
+lockfile="${defangdir}/${name}.lock"
+defang_user=${defang_user-"@DEFANG_USER@"}
+command_args="-P ${pidfile} -o ${lockfile} -U ${defang_user}"
 
 # default values, may be overridden on NetBSD by setting them in /etc/rc.conf
 mimedefang_flags=${mimedefang_flags-"-p ${defangdir}/mimedefang.sock \
