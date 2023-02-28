a081f71e4d380a3981a1ec9a020e9801426a82c2
pin <pin@pkgsrc.org>
2023-02-28 22:38:42 +0000

    textproc/tuc: update to 1.0.0
    [0.11.0] - 2022-06-20
    
    - fix: --lines could throw out of bounds with -f 1: in some situations
    - chore: dependency updates
    - doc: fixed typos
    - doc: new section about community-managed install methods (macports)
    - doc: man page generated using the mode modern pandoc 2.5

--- textproc/tuc/cargo-depends.mk
+++ textproc/tuc/cargo-depends.mk
@@ -1,22 +1,22 @@
-# $NetBSD: cargo-depends.mk,v 1.5 2023/01/02 12:54:00 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.6 2023/02/28 22:38:42 pin Exp $
 
-CARGO_CRATE_DEPENDS+=	aho-corasick-0.7.18
-CARGO_CRATE_DEPENDS+=	anyhow-1.0.58
-CARGO_CRATE_DEPENDS+=	assert_cmd-2.0.4
-CARGO_CRATE_DEPENDS+=	bstr-0.2.16
+CARGO_CRATE_DEPENDS+=	anyhow-1.0.69
+CARGO_CRATE_DEPENDS+=	assert_cmd-2.0.8
+CARGO_CRATE_DEPENDS+=	bstr-1.1.0
 CARGO_CRATE_DEPENDS+=	difflib-0.4.0
 CARGO_CRATE_DEPENDS+=	doc-comment-0.3.3
 CARGO_CRATE_DEPENDS+=	either-1.6.1
 CARGO_CRATE_DEPENDS+=	itertools-0.10.3
-CARGO_CRATE_DEPENDS+=	lazy_static-1.4.0
 CARGO_CRATE_DEPENDS+=	libc-0.2.97
 CARGO_CRATE_DEPENDS+=	memchr-2.4.0
+CARGO_CRATE_DEPENDS+=	once_cell-1.17.0
 CARGO_CRATE_DEPENDS+=	pico-args-0.5.0
 CARGO_CRATE_DEPENDS+=	predicates-2.1.1
 CARGO_CRATE_DEPENDS+=	predicates-core-1.0.2
 CARGO_CRATE_DEPENDS+=	predicates-tree-1.0.2
-CARGO_CRATE_DEPENDS+=	regex-1.5.6
+CARGO_CRATE_DEPENDS+=	regex-1.7.1
 CARGO_CRATE_DEPENDS+=	regex-automata-0.1.10
-CARGO_CRATE_DEPENDS+=	regex-syntax-0.6.26
+CARGO_CRATE_DEPENDS+=	regex-syntax-0.6.27
+CARGO_CRATE_DEPENDS+=	serde-1.0.152
 CARGO_CRATE_DEPENDS+=	treeline-0.1.0
 CARGO_CRATE_DEPENDS+=	wait-timeout-0.2.0
