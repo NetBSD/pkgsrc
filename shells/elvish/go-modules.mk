c66137ea5d6eb0241fb59d21f8daf101cad5185b
pin <pin@pkgsrc.org>
2023-03-05 07:37:37 +0000

    shells/elvish: update to 0.19.1
    Note: The 0.19.0 version was tagged prematurely by mistake, but it has been
    picked up by some package managers. As a result, the 0.19.0 version is
    considered to be "skipped" officially. If your package manager provides a
    0.19.0 version, it is probably identical to 0.19.1 in functionalities.
    
    Packagers who have already packaged 0.19.0 are still advised to "upgrade" to
    0.19.1.
    
    Notable new features
     - A new doc module provides access to the documentation of builtin modules.
     - A new conj command “appends” values to a list, and has a guaranteed time
       complexity independent of the size of the list.
     - A new inexact-num converts its argument to an inexact number.
     - It is functionally identical to the now deprecated float64 command since the
       Go float64 type is the only underlying inexact number type for now. Its
       behavior may change in future if there are more underlying types for inexact
       numbers.
     - A new type of interactive abbreviation: edit:command-abbr (#1472).
     - The order and compare commands now support boolean values (#1585).
     - A new path:join command and path:separator and path:list-separator variables
       (#1562).
     - A new runtime: module that contains paths important for the Elvish runtime
       (#1385, #1423).
     - A new compact command that replaces consecutive runs of equal values with a
       single copy, similar to the Unix uniq command.
     - The order command has a new &key option (#1570).
     - A new benchmark command has been added (#1586).
     - When checking compilation errors, Elvish no longer stops after the first
       error found. For example, if $a and $b are both not defined, echo $a $b now
       yields two errors. This applies to both the interactive REPL and
       elvish -compile-only.
     - When using an unimported builtin modules from the REPL, the REPL now shows
       the use command needed to import it, which can be executed from a key
       binding. This functionality is bound to Ctrl-A by default.
     - New variables exposing the terminal and null device in an OS-agnostic
       fashion: $path:dev-tty and $path:dev-null. They are /dev/tty and /dev/null
       on Unix, and CON and NUL on Windows (#1633).
    
    Breaking changes
     - When a styled or styled-segment is printed to terminal, the resulting
       sequence will now always ignore any existing SGR state.
     - Symbolic links are now always treated as ordinary files by the global
       modifiers type:dir and type:regular in wildcard expansions.
     - Support for shared vars has been removed, along with its API
       (store:shared-var, store:set-shared-var and store:del-shared-var).
     - The try command no longer supports the except keyword. It has been
       superseded by the catch keyword.
    
    Deprecated features
    Deprecated features will be removed in 0.20.0.
    
    The following deprecated features trigger a warning whenever the code is parsed
    and compiled, even if it is not executed:
     - The float64 command is now deprecated. Use num for constructing a typed
       number, or inexact-num for constructing an inexact number.
     - The documentation has advertised it as deprecated since the 0.16.0 release,
       but deprecation warnings were never added.
    
    Notable bugfixes
     - Temporary assignment on an unset environment variables no longer leave it
       set to an empty string (#1448).
     - Broken symbolic links no longer terminate a wildcard expansion prematurely
       (#1240).
     - On Windows, command completion for executables now also works for local
       files.

--- shells/elvish/go-modules.mk
+++ shells/elvish/go-modules.mk
@@ -1,21 +1,18 @@
-# $NetBSD: go-modules.mk,v 1.4 2023/02/28 22:31:53 pin Exp $
+# $NetBSD: go-modules.mk,v 1.5 2023/03/05 07:37:37 pin Exp $
 
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.15.mod
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.15.zip
-GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.7.mod
-GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.7.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.zip
 GO_MODULE_FILES+=	github.com/gorilla/websocket/@v/v1.4.1.mod
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.mod
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.17.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.17.zip
 GO_MODULE_FILES+=	github.com/sourcegraph/go-lsp/@v/v0.0.0-20200429204803-219e11d77f5d.mod
 GO_MODULE_FILES+=	github.com/sourcegraph/go-lsp/@v/v0.0.0-20200429204803-219e11d77f5d.zip
-GO_MODULE_FILES+=	github.com/sourcegraph/jsonrpc2/@v/v0.1.0.mod
-GO_MODULE_FILES+=	github.com/sourcegraph/jsonrpc2/@v/v0.1.0.zip
-GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.6.mod
-GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.6.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200923182605-d9f96fdee20d.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210630005230-0f9fa26af87c.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220227234510-4e6760a101f9.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220227234510-4e6760a101f9.zip
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191204190536-9bdfabe68543.mod
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191204190536-9bdfabe68543.zip
+GO_MODULE_FILES+=	github.com/sourcegraph/jsonrpc2/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/sourcegraph/jsonrpc2/@v/v0.2.0.zip
+GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.7.mod
+GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.7.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220811171246-fbc7d0a398ab.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.zip
