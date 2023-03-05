f46a16fd2d6f1605306d303ba8ad851ba93362ea
triaxx <triaxx@pkgsrc.org>
2023-03-05 19:21:23 +0000

    php-dotclear: Update to 2.25.3
    pkgsrc changes:
    * Add 82 as PHP_VERSIONS_ACCEPTED
    
    upstream changes:
    -----------------
    * Dotclear 2.25.3
      https://dotclear.org/blog/post/2023/02/25/Dotclear-2.25.3
    * Dotclear 2.25.2
      https://dotclear.org/blog/post/2023/02/19/Dotclear-2.25.2
    * Dotclear 2.25.1
      https://dotclear.org/blog/post/2023/02/18/Dotclear-2.25.1
    * Dotclear 2.25
      https://dotclear.org/blog/post/2023/02/13/Dotclear-2.25
    * Dotclear 2.24.1
      https://dotclear.org/blog/post/2023/01/18/Dotclear-2.24.1
    * Dotclear 2.24
      https://dotclear.org/blog/post/2022/12/24/Dotclear-2.24

--- www/php-dotclear/patches/patch-admin_install_wizard.php
+++ www/php-dotclear/patches/patch-admin_install_wizard.php
@@ -1,16 +1,16 @@
-$NetBSD: patch-admin_install_wizard.php,v 1.3 2022/05/26 21:08:38 triaxx Exp $
+$NetBSD: patch-admin_install_wizard.php,v 1.4 2023/03/05 19:21:24 triaxx Exp $
 
 Honor PKG_SYSCONFDIR.
 Set pkgsrc compliant directories for SQLite database.
 
---- admin/install/wizard.php.orig	2022-05-13 06:25:40.000000000 +0000
+--- admin/install/wizard.php.orig	2023-02-25 08:19:11.000000000 +0000
 +++ admin/install/wizard.php
-@@ -61,7 +61,7 @@ if (!empty($_POST)) {
+@@ -61,7 +61,7 @@ $ADMINMAILFROM = !empty($_POST['ADMINMAI
+ if (!empty($_POST)) {
      try {
-         if ($DBDRIVER == 'sqlite') {
-             if (strpos($DBNAME, '/') === false) {
--                $sqlite_db_directory = dirname(DC_RC_PATH) . '/../db/';
-+                $sqlite_db_directory = '@DOTCLEAR_DBDIR@';
-                 files::makeDir($sqlite_db_directory, true);
+         if ($DBDRIVER == 'sqlite' && strpos($DBNAME, '/') === false) {
+-            $sqlite_db_directory = dirname(DC_RC_PATH) . '/../db/';
++            $sqlite_db_directory = '@DOTCLEAR_DBDIR@';
+             files::makeDir($sqlite_db_directory, true);
  
-                 # Can we write sqlite_db_directory ?
+             # Can we write sqlite_db_directory ?
