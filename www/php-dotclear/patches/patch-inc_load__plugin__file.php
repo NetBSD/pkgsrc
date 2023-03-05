40194067cc47bb03597eff6edd88d237c2a59dbc
triaxx <triaxx@pkgsrc.org>
2023-03-05 19:21:23 +0000

    php-dotclear: Update to 2.25.3
    pkgsrc changes:
    * Add 82 as PHP_VERSIONS_ACCEPTED
    
    upstream changes:
    -----------------
    * Dotclear 2.25.3
      https://dotclear.org/blog/post/2023/02/25/Dotclear-2.25.3
    * Dotclear 2.25.2
      https://dotclear.org/blog/post/2023/02/19/Dotclear-2.25.2
    * Dotclear 2.25.1
      https://dotclear.org/blog/post/2023/02/18/Dotclear-2.25.1
    * Dotclear 2.25
      https://dotclear.org/blog/post/2023/02/13/Dotclear-2.25
    * Dotclear 2.24.1
      https://dotclear.org/blog/post/2023/01/18/Dotclear-2.24.1
    * Dotclear 2.24
      https://dotclear.org/blog/post/2022/12/24/Dotclear-2.24

--- www/php-dotclear/patches/patch-inc_load__plugin__file.php
+++ www/php-dotclear/patches/patch-inc_load__plugin__file.php
@@ -1,10 +1,10 @@
-$NetBSD: patch-inc_load__plugin__file.php,v 1.4 2022/08/02 20:09:00 triaxx Exp $
+$NetBSD: patch-inc_load__plugin__file.php,v 1.5 2023/03/05 19:21:24 triaxx Exp $
 
 Honor PKG_SYSCONFDIR.
 
 --- inc/load_plugin_file.php.orig	2022-05-13 06:25:40.000000000 +0000
 +++ inc/load_plugin_file.php
-@@ -25,7 +25,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
+@@ -27,7 +27,7 @@ if (isset($_SERVER['DC_RC_PATH'])) {
  } elseif (isset($_SERVER['REDIRECT_DC_RC_PATH'])) {
      define('DC_RC_PATH', $_SERVER['REDIRECT_DC_RC_PATH']);
  } else {
