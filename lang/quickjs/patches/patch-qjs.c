89491abacaadf3651c8f1c9c39731deb4b0c3be3
he <he@pkgsrc.org>
2023-03-07 14:38:03 +0000

    quickjs: make it build on macppc again, or more generally...
    on non-CONFIG_BIGNUM platforms.
    Bump PKGREVISION.

--- lang/quickjs/patches/patch-qjs.c
+++ lang/quickjs/patches/patch-qjs.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-qjs.c,v 1.1 2019/11/14 21:20:04 rillig Exp $
+$NetBSD: patch-qjs.c,v 1.2 2023/03/07 14:38:03 he Exp $
 
 Portability patch for NetBSD.
 
 --- qjs.c.orig	2019-10-27 10:55:35.000000000 +0000
 +++ qjs.c
-@@ -124,7 +124,7 @@ static inline size_t js_trace_malloc_usa
+@@ -146,7 +146,7 @@ static inline size_t js_trace_malloc_usa
      return malloc_size(ptr);
  #elif defined(_WIN32)
      return _msize(ptr);
@@ -13,7 +13,7 @@ Portability patch for NetBSD.
      return 0;
  #elif defined(__linux__)
      return malloc_usable_size(ptr);
-@@ -240,7 +240,7 @@ static const JSMallocFunctions trace_mf 
+@@ -268,7 +268,7 @@ static const JSMallocFunctions trace_mf 
      malloc_size,
  #elif defined(_WIN32)
      (size_t (*)(const void *))_msize,
@@ -22,3 +22,14 @@ Portability patch for NetBSD.
      NULL,
  #elif defined(__linux__)
      (size_t (*)(const void *))malloc_usable_size,
+@@ -454,8 +454,10 @@ int main(int argc, char **argv)
+         }
+     }
+ 
++#ifdef CONFIG_BIGNUM
+     if (load_jscalc)
+         bignum_ext = 1;
++#endif
+ 
+     if (trace_memory) {
+         js_trace_malloc_init(&trace_data);
