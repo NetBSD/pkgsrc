027a9e53a9707264e6df9f9cc2d34b6c9ed98b64
adam <adam@pkgsrc.org>
2023-03-08 09:58:16 +0000

    abseil: updated to 20230125.1
    Abseil LTS 20230125.1
    
    What's New:
    
    The Abseil logging library has been released. This library provides facilities for writing short text messages about the status of a program to stderr, disk files, or other sinks (via an extension API). See the logging library documentation for more information.
    An extension point, AbslStringify(), allows user-defined types to seamlessly work with Abseil's string formatting functions like absl::StrCat() and absl::StrFormat().
    A library for computing CRC32C checksums has been added.
    Floating-point parsing now uses the Eisel-Lemire algorithm, which provides a significant speed improvement.
    The flags library now provides suggestions for the closest flag(s) in the case of misspelled flags.
    Using CMake to install Abseil now makes the installed artifacts (in particular absl/base/options.h) reflect the compiled ABI.
    
    Breaking Changes:
    
    Abseil now requires at least C++14 and follows Google's Foundational C++ Support Policy. See this table for a list of currently supported versions compilers, platforms, and build tools.
    The legacy spellings of the thread annotation macros/functions (e.g. GUARDED_BY()) have been removed by default in favor of the ABSL_ prefixed versions (e.g. ABSL_GUARDED_BY()) due to clashes with other libraries. The compatibility macro ABSL_LEGACY_THREAD_ANNOTATIONS can be defined on the compile command-line to temporarily restore these spellings, but this compatibility macro will be removed in the future.
    
    Known Issues
    
    The Abseil logging library in this release is not a feature-complete replacement for glog yet. VLOG and DFATAL are examples of features that have not yet been released.

--- devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
+++ devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
@@ -1,18 +1,15 @@
-$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.1 2020/03/15 22:25:56 adam Exp $
+$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.2 2023/03/08 09:58:16 adam Exp $
 
 Avoid _XOPEN_SOURCE.
 
---- absl/time/internal/cctz/src/time_zone_format.cc.orig	2020-03-15 11:19:38.157894380 +0000
+--- absl/time/internal/cctz/src/time_zone_format.cc.orig	2023-02-18 16:23:34.000000000 +0000
 +++ absl/time/internal/cctz/src/time_zone_format.cc
-@@ -18,11 +18,6 @@
- #endif
+@@ -19,7 +19,7 @@
  #endif
  
--#if defined(HAS_STRPTIME) && HAS_STRPTIME
--#if !defined(_XOPEN_SOURCE)
--#define _XOPEN_SOURCE  // Definedness suffices for strptime.
--#endif
--#endif
- 
- #include "absl/base/config.h"
- #include "absl/time/internal/cctz/include/cctz/time_zone.h"
+ #if defined(HAS_STRPTIME) && HAS_STRPTIME
+-#if !defined(_XOPEN_SOURCE) && !defined(__OpenBSD__)
++#if !defined(_XOPEN_SOURCE) && !defined(__NetBSD__) && !defined(__OpenBSD__)
+ #define _XOPEN_SOURCE  // Definedness suffices for strptime.
+ #endif
+ #endif
