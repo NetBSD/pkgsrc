cea5c3d7628aa4b22597e19c04d7f6d2c0e99bd7
ryoon <ryoon@pkgsrc.org>
2023-03-10 21:56:18 +0000

    bvi: Update to 1.4.2
    Changelog:
    New in release 1.4.2
    ====================
    This release is dedicated to Sven Guckes (*1967-04-06 +2022-02-20).
    Sven was an enthusiastic Linux explainer, his particular passion was the console
     tools.
    I met Sven a couple of times. He always showed interest in the further developme
    nt of
    the bvi and liked to discuss new ideas.
    R.I.P. Sven
    
    * :wq bug fixed
    * Error-message if input no terminal
    * Mixed licenses (SF bug #10)
    * Segmentation fault occurs on undo (SF bug #11)
    * Segmentation fault occurs on substitution in 32-bit systems (SF bug #12)
    * ncursesw support
    * Handling inputs larger than 2^31 bytes (SF bug #13)
    * Some incorrect function prototypes in ANSI mode fixed (SF bug #14)
    * Minor fixes

--- editors/bvi/patches/patch-comm.c
+++ editors/bvi/patches/patch-comm.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-comm.c,v 1.1 2016/06/19 16:55:28 tsutsui Exp $
+$NetBSD: patch-comm.c,v 1.2 2023/03/10 21:56:18 ryoon Exp $
 
 - use <errno.h> for errno.
 
---- comm.c.orig	2014-01-29 14:54:53.000000000 +0000
+--- comm.c.orig	2023-03-06 16:40:20.000000000 +0000
 +++ comm.c
-@@ -35,6 +35,8 @@
+@@ -37,6 +37,8 @@
  #include "bvi.h"
  #include "set.h"
  
@@ -13,11 +13,11 @@ $NetBSD: patch-comm.c,v 1.1 2016/06/19 16:55:28 tsutsui Exp $
  #ifdef HAVE_UNISTD_H
  #	include <unistd.h>
  #endif
-@@ -84,7 +86,6 @@ PTR		end_addr;
+@@ -86,7 +88,6 @@ PTR		end_addr;
  extern	char	*name;			/* actual filename */
  extern	char	**files;		/* used for "next" and "rewind" */
  extern	int		numfiles, curfile;
 -extern	int		errno;
  
- static	char	oldbuf[CMDSZ];		/** for :!! command **/
+ static	char	oldbuf[CMDSZ + 3];		/** for :!! command **/
  
