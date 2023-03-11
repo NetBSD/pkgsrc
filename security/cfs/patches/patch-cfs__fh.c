04710e3bb7ac237006a1f9c6fd5608e5fd485e28
gdt <gdt@pkgsrc.org>
2023-03-11 01:54:01 +0000

    security/cfs: Improve statvfs handling and add patch comments
    It is necessary to use statvfs instead of statfs on NetBSD; simplify
    how that is done.
    
    Explain most patches much better and add notes to file most upstream.

--- security/cfs/patches/patch-cfs__fh.c
+++ security/cfs/patches/patch-cfs__fh.c
@@ -1,4 +1,13 @@
-$NetBSD: patch-cfs__fh.c,v 1.1 2022/12/19 23:18:37 gdt Exp $
+$NetBSD: patch-cfs__fh.c,v 1.2 2023/03/11 01:54:01 gdt Exp $
+
+If a file was opened for reading, do not open the ciphertext in RW
+mode.  While in many filesystems, opening a file for writing and not
+writing does not result in a modification, coda does a writeback in
+that case.  Plus, as a general matter, reads should only lead to
+reads.  Details available at
+  https://gnats.netbsd.org/28479
+
+\todo File and discuss upstream.
 
 --- cfs_fh.c.orig	2013-05-15 16:50:30.000000000 +0000
 +++ cfs_fh.c
