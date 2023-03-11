43c708857d971cdad03e40bd4465b244318d58e4
schmonz <schmonz@pkgsrc.org>
2023-03-11 15:47:34 +0000

    Update to 2.9.2.1. From the changelog:
    - New multicall binary: execline.
    - Bugfixes.

--- lang/execline/buildlink3.mk
+++ lang/execline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/09/29 09:53:57 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/03/11 15:47:34 schmonz Exp $
 
 BUILDLINK_TREE+=	execline
 
 .if !defined(EXECLINE_BUILDLINK3_MK)
 EXECLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.execline+=	execline>=2.8.1.0
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.2.0
 BUILDLINK_PKGSRCDIR.execline?=		../../lang/execline
 BUILDLINK_DEPMETHOD.execline?=		build
 .endif # EXECLINE_BUILDLINK3_MK
