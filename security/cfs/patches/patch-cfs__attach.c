d2a233d099cfbe753f129a542933fc9a01d4b6c8
gdt <gdt@pkgsrc.org>
2023-03-11 01:54:01 +0000

    security/cfs: Improve statvfs handling and add patch comments
    It is necessary to use statvfs instead of statfs on NetBSD; simplify
    how that is done.
    
    Explain most patches much better and add notes to file most upstream.

--- security/cfs/patches/patch-cfs__attach.c
+++ security/cfs/patches/patch-cfs__attach.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-cfs__attach.c,v 1.1 2022/12/19 23:18:37 gdt Exp $
+$NetBSD: patch-cfs__attach.c,v 1.2 2023/03/11 01:54:01 gdt Exp $
 
 For NetBSD, use statvfs.
 
 \todo Send upstream.
 
---- cattach.c.orig	2022-12-19 22:34:48.224422733 +0000
+--- cattach.c.orig	2013-05-15 16:50:30.000000000 +0000
 +++ cattach.c
-@@ -83,8 +83,12 @@ main(int argc, char *argv[])
- 	struct fs_data sfb;
- #define f_blocks  fd_req.btot
- #else
-+#if defined(__NetBSD_Version__) && __NetBSD_Version__ >= 299000900
-+	struct statvfs sfb;
-+#else
- 	struct statfs sfb;
+@@ -59,6 +59,12 @@
+   your solaris configuration */
+ #define statfs		statvfs
  #endif
++#ifdef NetBSD
++/*
++ * Strictly, NetBSD >= 3.0.
++ */
++#define statfs         statvfs
 +#endif
- 	char *flg;
- 	int ciph;
- 	FILE *fp;
+ 
+ #ifndef TMOUT		/* default timeout; override in makefile */
+ #define TMOUT 0
