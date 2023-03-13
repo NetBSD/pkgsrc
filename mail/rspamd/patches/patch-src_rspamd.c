a3b191d72397df42bb2b02beff005af6d1960a9a
triaxx <triaxx@pkgsrc.org>
2023-03-13 11:41:57 +0000

    rspamd: Update to 3.4
    pkgsrc changes:
    ---------------
      * Remove LUA_VERSIONS_ACCEPTED since it is redundant with
        lang/lua/luaversion.mk.
    
    upstream changes:
    -----------------
    3.4: 01 Nov 2022
      * [CritFix] Restore compatibility with the integrations and headers alterations
      * [Feature] Milter_headers: Add `x-rspamd-action` routine
      * [Feature] Share hyperscan database among processes
      * [Fix] Another corner case in url parsing
      * [Fix] Another fix for the enable password
      * [Fix] Another try to fix close method in lua_tcp
      * [Fix] Fix additional fields in the Redis schema
      * [Fix] Fix emoji joiner FP
      * [Fix] Fix favicon.ico Content-Type header
      * [Fix] Fix hang when close is used
      * [Fix] Lua_tcp: Sigh, another try to fix `close` invocation
      * [Fix] Mx_check: Cache the fact of a missing MX record
      * [Fix] Try to fix parsing of the unencoded `>` characters in html attributes
      * [Fix] Try to fix the case where password == enable_password
      * [Project] (Re)implement hyperscan caching
      * [Project] Rework cleanup
      * [Project] Synchronize hyperscan caches via the main process
      * [Rework] Convert multipattern to use hyperscan tools
      * [Rework] Make http normalize path function a generic function
      * [Rework] Split locked and unlocked files, as mmap does not need flock normally
      * [Rework] Start movement of the hyperscan related routines into a single unit
      * [Rework] Store the current worker, so other libraries could use this information
      * [Rework] Use blocking socket for IPC between main and workers
      * [Rework] Use more predictable size for commands buffers
      * [Rules] Do not insert ONCE_RECEIVED_STRICT on RDNS missing
      * [Rules] Reduce score of HTTP_TO_HTTPS - subject to remove completely

--- mail/rspamd/patches/patch-src_rspamd.c
+++ mail/rspamd/patches/patch-src_rspamd.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_rspamd.c,v 1.1 2022/10/30 15:38:54 jperkin Exp $
+$NetBSD: patch-src_rspamd.c,v 1.2 2023/03/13 11:41:57 triaxx Exp $
 
 Avoid sun define.
 
 --- src/rspamd.c.orig	2022-10-01 14:19:21.000000000 +0000
 +++ src/rspamd.c
-@@ -451,7 +451,7 @@ systemd_get_socket (struct rspamd_main *
+@@ -455,7 +455,7 @@ systemd_get_socket (struct rspamd_main *
  	union {
  		struct sockaddr_storage ss;
  		struct sockaddr sa;
