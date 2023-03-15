9457109db91e376a87abb6a2bb0e411a5f1b4c00
schmonz <schmonz@pkgsrc.org>
2023-03-15 18:20:51 +0000

    Update to 1.4.4. From the changelog:
    1.4.4:
    - Firefox packaged in a Snap, which is the default browser of Ubuntu
      22.04, is now supported
    - Fixed a crash when a CSR doesn't have SANs
    - Calling mkcert with no arguments only prints the help text
    
    1.4.3:
    - The EKU logic is now simpler, and it follows the following rules
        - if an IP address, DNS name, or URI SAN is present, serverAuth
          is included
        - if -client is used, clientAuth is included
        - if an email address SAN in present, emailProtection is included
    - Certificate generation based on CSRs is now consistent with standard
      certificate generation
    
    1.4.2:
    - Reduce certificate lifetime to 2 years and 3 months
    - Detect various flavors of Firefox (#225, #280)
    
    pkgsrc changes:
    
    - Simplify packaging
    - Take MAINTAINER

---
+++ security/go-mkcert/go-modules.mk
@@ -0,0 +1,22 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/03/15 18:20:51 schmonz Exp $
+
+GO_MODULE_FILES+=	github.com/jessevdk/go-flags/@v/v1.4.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220331220935-ae2d96664a29.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220331220935-ae2d96664a29.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20211112202133-69e39bad7dc2.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220421235706-1d1ef9303861.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220421235706-1d1ef9303861.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20201119102817-f84b799fce68.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210423082822-04245dca01da.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.7.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.7.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v1/@v/v1.0.0-20140924161607-9f9df34309c0.mod
+GO_MODULE_FILES+=	howett.net/plist/@v/v1.0.0.mod
+GO_MODULE_FILES+=	howett.net/plist/@v/v1.0.0.zip
+GO_MODULE_FILES+=	software.sslmate.com/src/go-pkcs12/@v/v0.2.0.mod
+GO_MODULE_FILES+=	software.sslmate.com/src/go-pkcs12/@v/v0.2.0.zip
