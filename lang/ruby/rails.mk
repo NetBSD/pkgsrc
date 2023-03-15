71c47383decbc48e6b909ff011ebfa920b5248db
taca <taca@pkgsrc.org>
2023-03-15 13:31:47 +0000

    www/ruby-rails61: update to 6.1.7.3
    6.1.7.3 (2023-03-13)
    
    Active Support
    
    * Implement SafeBuffer#bytesplice
    
      [CVE-2023-28120]
    
    Action View
    
    * Ignore certain data-* attributes in rails-ujs when element is
      contenteditable
    
      [CVE-2023-23913]

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.142 2023/01/25 13:31:15 taca Exp $
+# $NetBSD: rails.mk,v 1.143 2023/03/15 13:31:47 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -49,7 +49,7 @@ _RUBY_RAILS_MK=	# defined
 #
 RUBY_RAILS52_VERSION?=	5.2.8.1
 RUBY_RAILS60_VERSION?=	6.0.6.1
-RUBY_RAILS61_VERSION?=	6.1.7.2
+RUBY_RAILS61_VERSION?=	6.1.7.3
 RUBY_RAILS70_VERSION?=	7.0.4.2
 
 RUBY_RAILS_ACCEPTED?=	# empty
