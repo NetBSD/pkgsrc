d02130c6879583ea45e7f7f1c33782dbc2a5945d
adam <adam@pkgsrc.org>
2023-03-16 08:06:13 +0000

    rinutils: updated to 0.10.1
    0.10.1 ( 19 May 2022 )
    
    Add GitHub actions' CI.
    
    Fix ( "pkg-config file is broken when CMAKE_INSTALL_INCLUDEDIR is absolute")
    
    
    0.10.0 ( 06 Sep 2021 )
    
    Fix the filenames in the comments of **.h and add SPDX identifiers.
    
    Add -DINSTALL_TO_DATADIR=…​ in CMakeLists.txt "Install libdir files to the architecture independent datadir (e.g for Debian)"
    
    Minor README.asciidoc reformatting.
    
    Thanks to pert from #debian-mentoring .

--- devel/rinutils/buildlink3.mk
+++ devel/rinutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/06/28 11:33:42 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/03/16 08:06:13 adam Exp $
 
 BUILDLINK_TREE+=	rinutils
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	rinutils
 RINUTILS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rinutils+=	rinutils>=0.8.0
-BUILDLINK_ABI_DEPENDS.rinutils?=		rinutils>=0.8.0nb2
+BUILDLINK_ABI_DEPENDS.rinutils+=	rinutils>=0.8.0nb2
 BUILDLINK_PKGSRCDIR.rinutils?=		../../devel/rinutils
 
 BUILDLINK_DEPMETHOD.rinutils?=		build
