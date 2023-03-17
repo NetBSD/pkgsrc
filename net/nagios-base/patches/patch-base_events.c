650bdb3caab13386e7272572496d3e78898314d6
jperkin <jperkin@pkgsrc.org>
2023-03-17 11:51:34 +0000

    nagios-base: Update to 4.4.10.
    Primarily came here to fix builds on systems where SSL is not in base, as the
    configure script aborts if it can't find SSL, however we might as well update
    to the latest version while here.  SSL support is now properly optional.
    
    4.4.10 - 2023-01-17
    -------------------
    FIXES
    * Fix memory leak in nagiostats.c when using -c, -s, -d, or -D multiple times (#888)
    * Improve service scheduling at startup (#887)
    * Improve compilation/reduce warnings when using Clang 16 (#889)
    * Improve compilation on other, stricter C99 compilers (#890)

--- net/nagios-base/patches/patch-base_events.c
+++ net/nagios-base/patches/patch-base_events.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-base_events.c,v 1.3 2023/01/08 23:33:59 sekiya Exp $
+$NetBSD: patch-base_events.c,v 1.4 2023/03/17 11:51:34 jperkin Exp $
 
 64-bit time_t workaround
 
---- base/events.c.orig	2022-11-17 05:52:51.000000000 +0900
-+++ base/events.c	2023-01-09 06:59:11.773902761 +0900
-@@ -354,7 +354,7 @@
+--- base/events.c.orig	2023-01-17 19:26:16.000000000 +0000
++++ base/events.c
+@@ -354,7 +354,7 @@ void init_timing_loop(void) {
  			if(check_delay > check_window(temp_service)) {
  				log_debug_info(DEBUGL_EVENTS, 0,
  						"  Fixing check time %lu secs too far away\n",
 -						check_delay - check_window(temp_service));
 +						(unsigned long)(check_delay - check_window(temp_service)));
  				fixed_services++;
- 				check_delay = check_window(temp_service);
+ 				check_delay = ranged_urand(0, check_window(temp_service));
  				log_debug_info(DEBUGL_EVENTS, 0, "  New check offset: %d\n",
-@@ -506,7 +506,7 @@
+@@ -506,7 +506,7 @@ void init_timing_loop(void) {
  		check_delay = mult_factor * scheduling_info.host_inter_check_delay;
  		if(check_delay > check_window(temp_host)) {
  			log_debug_info(DEBUGL_EVENTS, 1, "Fixing check time (off by %lu)\n",
