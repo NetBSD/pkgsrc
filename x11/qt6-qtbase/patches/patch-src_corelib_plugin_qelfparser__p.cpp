c91b7ddee5f31d64c594883de0d6e0c308f04f84
nros <nros@pkgsrc.org>
2023-03-25 07:29:32 +0000

    qt6-qtbase: add patches to build on systems with Alpha cpus

--- x11/qt6-qtbase/patches/patch-src_corelib_plugin_qelfparser__p.cpp
+++ x11/qt6-qtbase/patches/patch-src_corelib_plugin_qelfparser__p.cpp
@@ -1,7 +1,11 @@
-$NetBSD: patch-src_corelib_plugin_qelfparser__p.cpp,v 1.1 2022/11/24 11:11:38 nros Exp $
+$NetBSD: patch-src_corelib_plugin_qelfparser__p.cpp,v 1.2 2023/03/25 07:29:32 nros Exp $
 
 * Support older ELFOSABI_LINUX that has been replaced with ELFOSABI_GNU.
   Needed on NetBSD 9.
+
+* Enable Alpha support
+  From upstream:
+  https://codereview.qt-project.org/c/qt/qtbase/+/437349
 
 --- src/corelib/plugin/qelfparser_p.cpp.orig	2022-11-03 12:40:18.000000000 +0000
 +++ src/corelib/plugin/qelfparser_p.cpp
@@ -17,3 +21,20 @@ $NetBSD: patch-src_corelib_plugin_qelfparser__p.cpp,v 1.1 2022/11/24 11:11:38 nr
  QT_BEGIN_NAMESPACE
  
  using namespace Qt::StringLiterals;
+@@ -107,6 +112,8 @@ struct ElfMachineCheck
+     static const Elf32_Half ExpectedMachine =
+ #if 0
+             // nothing
++#elif defined(Q_PROCESSOR_ALPHA)
++            EM_ALPHA
+ #elif defined(Q_PROCESSOR_ARM_32)
+             EM_ARM
+ #elif defined(Q_PROCESSOR_ARM_64)
+@@ -371,6 +378,7 @@ Q_DECL_UNUSED Q_DECL_COLD_FUNCTION stati
+     switch (r.machine) {
+     // list definitely not exhaustive!
+     case EM_NONE:       d << ", no machine"; break;
++    case EM_ALPHA:      d << ", ALPHA"; break;
+     case EM_ARM:        d << ", ARM"; break;
+     case EM_AARCH64:    d << ", AArch64"; break;
+ #ifdef EM_BLACKFIN
