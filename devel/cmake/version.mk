de7c52eaeb8b0f7da65a7355a4fec53d1bb3df0c
adam <adam@pkgsrc.org>
2023-03-28 13:30:41 +0000

    cmake cmake-gui: updated to 3.26.1
    CMake 3.26.1
    * WriteBasicConfigVersionFile: Fix regression in ARCH_INDEPENDENT check
    * cmCxxModuleMapper: Remove redundant path conversion callbacks
    * cmCxxModuleMapper: Use value semantics in path conversion callback
    * cmGlobalNinjaGenerator: Factor out GNU-like command-line detection on Windows
    * cmGlobalNinjaGenerator: Detect GNU-like command-line for dyndep collator
    * cmLocalNinjaGenerator: De-duplicate condition for using 'cmd /C' on Windows
    * cmDyndepCollation: Drop outdated mentions of CXX_MODULE_INTERNAL_PARTITIONS
    * cmGlobalNinjaGenerator: Use forward slashes in clang modmap format on Windows
    * Tests: Comment RunCMake.Ninja ShowIncludes sample input languages
    * Clang: Record Clang 16.0 C++ modules flags only for GNU-like front-end
    * Ninja: Wrap rules using '>' shell redirection with 'cmd /C' on Windows
    * FindHDF5: Restore parallel HDF5 detection with only CXX enabled
    * FindPython: ensure Stable ABI is correctly handled
    * Ninja: Restore detection of clang-cl showIncludes prefix
    * FindMatlab: add version/release map for R2023a

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.35 2023/03/15 13:30:35 adam Exp $
+# $NetBSD: version.mk,v 1.36 2023/03/28 13:30:41 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.26.0
+CMAKE_VERSION=	3.26.1
 CMAKE_API=	${CMAKE_VERSION:R}
