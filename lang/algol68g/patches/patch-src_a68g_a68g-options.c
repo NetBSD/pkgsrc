9c600cbcfa0aa496537699902e56b9390b92a8b7
rhialto <rhialto@pkgsrc.org>
2023-03-29 10:11:12 +0000

    lang/algol68g: update to version 3.1.4.
    From the NEWS file:
    
    Version 3.1.1-3.1.4, January/March 2023
    * Fix configuration bug preventing building on Cygwin.
    * Fix undefined errno upon succesful relpath call on some systems.
    * Distribution update.
    * Minor bug fixes.

--- lang/algol68g/patches/patch-src_a68g_a68g-options.c
+++ lang/algol68g/patches/patch-src_a68g_a68g-options.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_a68g_options.c,v 1.2 2022/12/04 17:11:13 rhialto Exp $
+$NetBSD: patch-src_a68g_a68g-options.c,v 1.1 2023/03/29 10:11:13 rhialto Exp $
 
 * Enable compiler for *BSD.
 
---- src/a68g/options.c.orig	2022-02-01 20:45:41.000000000 +0000
-+++ src/a68g/options.c
-@@ -548,7 +548,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
+--- src/a68g/a68g-options.c.orig	2023-03-19 20:10:12.000000000 +0000
++++ src/a68g/a68g-options.c
+@@ -552,7 +552,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
  #endif
              }
              if (eq (q, "linux")) {
