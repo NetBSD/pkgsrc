be5e01bc3633db2887368de83b262c345c2deb95
nikita <nikita@pkgsrc.org>
2023-03-30 18:41:00 +0000

    lua-language-server: update to version 3.6.18
    Import from wip, packaged by wiz/nikita
    
    Changelog:
    
    
    3.6.18
    What's Changed
    
        更新 by @CppCXY in #1995
        Make functions in string api accept numbers by @C3pa in #2016
        3rd: meta add bee by @fesily in #2021
    
    
    3.6.17
    What's Changed
    
        fix #1969 by @CppCXY in #1970
        Don't override lm.bindir and lm.EXE_DIR in make.lua by @neeshy in #1977
        fix issues by @CppCXY in #1987
        add: description for config.addonManager.enable by @carsakiller in #1989
    
    
    3.6.14
    What's Changed
    
        fix #1969 by @CppCXY in #1970
        Don't override lm.bindir and lm.EXE_DIR in make.lua by @neeshy in #1977
        fix issues by @CppCXY in #1987
        add: description for config.addonManager.enable by @carsakiller in #1989
    
    
    3.6.13
    
    
    3.6.12
    What's Changed
    
        更新 by @CppCXY in #1918
        fix #1919 by @CppCXY in #1920
        fix #1921 by @CppCXY in #1923
        add: addon manager setting to template by @carsakiller in #1931
        README updates by @carsakiller in #1937
        更新 by @CppCXY in #1939
        更新 by @CppCXY in #1954
        Array of a class type does not get completion #1940 by @jharajeev55 in #1962
        Improve math.abs annotation by @C3pa in #1961
        meta: newproxy can accept a userdata value by @Bilal2453 in #1959
    
    
    3.6.11
    What's Changed
    
        Small improvements in make.sh by @atk91 in #1890
        更新 by @CppCXY in #1894
        更新修复一些issue by @CppCXY in #1908
    
    
    3.6.10
    What's Changed
    
        chore: update links to point to organization by @carsakiller in #1874
        更新编译 by @CppCXY in #1877

--- devel/lua-language-server/patches/patch-make.lua
+++ devel/lua-language-server/patches/patch-make.lua
@@ -1,14 +1,13 @@
-$NetBSD: patch-make.lua,v 1.1 2023/03/29 20:27:09 nikita Exp $
+$NetBSD: patch-make.lua,v 1.2 2023/03/30 18:41:00 nikita Exp $
 
 Do not generate directories with spaces in the file names.
 
---- make.lua.orig	2022-09-06 18:48:12 UTC
-+++ make.lua
-@@ -96,6 +96,5 @@ lm:build 'unit-test' {
+--- make.lua.orig	2023-03-23 07:23:30.000000000 +0100
++++ make.lua	2023-03-29 22:59:59.816110949 +0200
+@@ -117,5 +117,5 @@
  }
  
  lm:default {
--    "bee-test",
 -    "unit-test",
-+    "all"
++    "all",
  }
