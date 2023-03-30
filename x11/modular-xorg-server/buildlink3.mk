dceeba12beb56c860ad9d0fd478f3e61d134768b
wiz <wiz@pkgsrc.org>
2023-03-30 08:25:06 +0000

    modular-xorg-server: update to 21.1.8.
    Clean lots of pkglint while here.
    
    Benno Schulenberg (1):
         xkbUtils: use existing symbol names instead of deleted deprecated ones
    
    Olivier Fourdan (2):
         composite: Fix use-after-free of the COW
         xserver 21.1.8

--- x11/modular-xorg-server/buildlink3.mk
+++ x11/modular-xorg-server/buildlink3.mk
@@ -1,25 +1,29 @@
-# $NetBSD: buildlink3.mk,v 1.26 2023/01/29 21:15:12 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2023/03/30 08:25:06 wiz Exp $
 
 BUILDLINK_TREE+=	modular-xorg-server
 
 .if !defined(MODULAR_XORG_SERVER_BUILDLINK3_MK)
 MODULAR_XORG_SERVER_BUILDLINK3_MK:=
 
-.include "../../mk/bsd.fast.prefs.mk"
-
 BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=21.1.4nb1
 BUILDLINK_ABI_DEPENDS.modular-xorg-server?=	modular-xorg-server>=21.1.6nb1
 BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
 BUILDLINK_INCDIRS.modular-xorg-server+=		include/xorg
 
-BUILDLINK_DEPMETHOD.libpciaccess=	full
+.include "../../mk/bsd.fast.prefs.mk"
+
+pkgbase := modular-xorg-server
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../sysutils/libpciaccess/buildlink3.mk"
 .include "../../x11/pixman/buildlink3.mk"
 
 .include "../../x11/xorgproto/buildlink3.mk"
 .include "../../x11/libxcvt/buildlink3.mk"
 .include "../../x11/libXfont2/buildlink3.mk"
-.include "../../graphics/MesaLib/buildlink3.mk" # XXX should not be needed for non-dri build
+.if ${PKG_BUILD_OPTIONS.modular-xorg-server:Mdri}
+.include "../../graphics/MesaLib/buildlink3.mk"
+.endif
 .endif # MODULAR_XORG_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-modular-xorg-server
