ee1cddd8b6213c0ff59ae22fabd86f5869256bda
adam <adam@pkgsrc.org>
2023-03-31 08:06:46 +0000

    cmake: updated to 3.26.2
    CMake 3.26.2
    * cmGlobalGenerator: Factor out helper to check target ordering
    * Ninja,Makefile: Restore Fortran module scanning in static library cycle
    * Ninja: Restore slash style for MinGW tools when extra languages are enabled
    * Swift: Restore compatibility with old C++ driver

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.36 2023/03/28 13:30:41 adam Exp $
+# $NetBSD: version.mk,v 1.37 2023/03/31 08:06:46 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.26.1
+CMAKE_VERSION=	3.26.2
 CMAKE_API=	${CMAKE_VERSION:R}
