3bd06af9486276eec7e8deecce5c50ff5acc054a
wiz <wiz@pkgsrc.org>
2023-03-31 20:52:12 +0000

    libheif: add dav1d to bl3.mk file
    pkg-config fails otherwise
    
    Bump all dependencies

--- graphics/libheif/buildlink3.mk
+++ graphics/libheif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2021/10/09 15:35:06 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/03/31 20:52:12 wiz Exp $
 
 BUILDLINK_TREE+=	libheif
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	libheif
 LIBHEIF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libheif+=	libheif>=1.8.0
-BUILDLINK_ABI_DEPENDS.libheif+=	libheif>=1.12.0nb1
+BUILDLINK_ABI_DEPENDS.libheif+=	libheif>=1.15.2nb1
 BUILDLINK_PKGSRCDIR.libheif?=	../../graphics/libheif
 
 .include "../../graphics/png/buildlink3.mk"
+.include "../../multimedia/dav1d/buildlink3.mk"
 .include "../../multimedia/libaom/buildlink3.mk"
 .include "../../multimedia/libde265/buildlink3.mk"
 .include "../../multimedia/x265/buildlink3.mk"
