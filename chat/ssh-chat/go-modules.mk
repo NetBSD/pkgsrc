7a724aafa725707d2786b34f0b39a58d4a8b1a19
bsiegert <bsiegert@pkgsrc.org>
2023-04-01 10:13:44 +0000

    New package, ssh-chat-1.10
    Custom SSH server written in Go. Instead of a shell, you get a chat
    prompt.

---
+++ chat/ssh-chat/go-modules.mk
@@ -0,0 +1,24 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/04/01 10:13:44 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/alexcesaro/log/@v/v0.0.0-20150915221235-61e686294e58.mod
+GO_MODULE_FILES+=	github.com/alexcesaro/log/@v/v0.0.0-20150915221235-61e686294e58.zip
+GO_MODULE_FILES+=	github.com/howeyc/gopass/@v/v0.0.0-20190910152052-7cb4b85ec19c.mod
+GO_MODULE_FILES+=	github.com/howeyc/gopass/@v/v0.0.0-20190910152052-7cb4b85ec19c.zip
+GO_MODULE_FILES+=	github.com/jessevdk/go-flags/@v/v1.4.0.mod
+GO_MODULE_FILES+=	github.com/jessevdk/go-flags/@v/v1.4.0.zip
+GO_MODULE_FILES+=	github.com/shazow/rateio/@v/v0.0.0-20200113175441-4461efc8bdc4.mod
+GO_MODULE_FILES+=	github.com/shazow/rateio/@v/v0.0.0-20200113175441-4461efc8bdc4.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20200420104511-884d27f42877.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20200420104511-884d27f42877.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200420163511-1957bb5e6d1f.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200420163511-1957bb5e6d1f.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.2.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.2.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.zip
