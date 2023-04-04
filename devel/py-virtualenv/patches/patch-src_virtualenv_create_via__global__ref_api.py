a9cc927421256457543176d903d220ef49e208aa
adam <adam@pkgsrc.org>
2023-04-04 09:52:28 +0000

    py-virtualenv: updated to 20.21.0
    v20.21.0 (2023-03-12)
    
    Features - 20.21.0
    
    Make closure syntax explicitly starts with {||.
    
    Bugfixes - 20.21.0
    
    Add print command to nushell print_prompt to ensure compatibility with future release of nushell, where intermediate commands no longer print their result to stdout.
    Do not assume the default encoding.
    Make ReentrantFileLock thread-safe and, thereby, fix race condition in virtualenv.cli_run

--- devel/py-virtualenv/patches/patch-src_virtualenv_create_via__global__ref_api.py
+++ devel/py-virtualenv/patches/patch-src_virtualenv_create_via__global__ref_api.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_virtualenv_create_via__global__ref_api.py,v 1.2 2022/09/08 07:46:09 adam Exp $
+$NetBSD: patch-src_virtualenv_create_via__global__ref_api.py,v 1.3 2023/04/04 09:52:28 adam Exp $
 
 Copy distutils distro config file to virtualenv.
 https://github.com/pypa/virtualenv/issues/2313
 
---- src/virtualenv/create/via_global_ref/api.py.orig	2022-09-08 02:18:47.000000000 +0000
+--- src/virtualenv/create/via_global_ref/api.py.orig	2020-02-02 00:00:00.000000000 +0000
 +++ src/virtualenv/create/via_global_ref/api.py
 @@ -2,6 +2,8 @@ import logging
  import os
@@ -16,7 +16,7 @@ https://github.com/pypa/virtualenv/issues/2313
  
 @@ -92,6 +94,10 @@ class ViaGlobalRefApi(Creator, metaclass
              logging.debug("create %s", dest_path)
-             dest_path.write_text(text)
+             dest_path.write_text(text, encoding="utf-8")
  
 +        distutils_distro_config = get_paths()["platlib"] + "/_distutils_system_mod.py"
 +        if os.path.exists(distutils_distro_config):
