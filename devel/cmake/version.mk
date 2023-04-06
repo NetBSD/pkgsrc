854076ab6208d9cb1f8734fbf4c5cf6f3302a354
adam <adam@pkgsrc.org>
2023-04-06 11:01:18 +0000

    cmake: updated to 3.26.3
    CMake 3.26.3
    * VS: Remove extra try_compile argument in Intel Fortran check
    * Xcode: Fix detection of Swift compiler location for Xcode 14.3
    * Tests: Teach RunCMake to ignore Xcode DVTCoreDeviceEnabledState warnings

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.37 2023/03/31 08:06:46 adam Exp $
+# $NetBSD: version.mk,v 1.38 2023/04/06 11:01:18 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.26.2
+CMAKE_VERSION=	3.26.3
 CMAKE_API=	${CMAKE_VERSION:R}
