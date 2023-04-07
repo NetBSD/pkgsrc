9e647d80cd95add3a6d2496e4a72983dcff53000
nros <nros@pkgsrc.org>
2023-04-07 21:26:00 +0000

    buildlink the plugin binaries of grpc so that using the cmake package files work

--- net/grpc/buildlink3.mk
+++ net/grpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2022/08/19 06:25:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/04/07 21:26:00 nros Exp $
 
 BUILDLINK_TREE+=	grpc
 
@@ -8,6 +8,14 @@ GRPC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.grpc+=	grpc>=1.11.0
 BUILDLINK_ABI_DEPENDS.grpc+=	grpc>=1.48.0
 BUILDLINK_PKGSRCDIR.grpc?=	../../net/grpc
+
+BUILDLINK_FILES.grpc+=	bin/grpc_cpp_plugin
+BUILDLINK_FILES.grpc+=	bin/grpc_csharp_plugin
+BUILDLINK_FILES.grpc+=	bin/grpc_node_plugin
+BUILDLINK_FILES.grpc+=	bin/grpc_objective_c_plugin
+BUILDLINK_FILES.grpc+=	bin/grpc_php_plugin
+BUILDLINK_FILES.grpc+=	bin/grpc_python_plugin
+BUILDLINK_FILES.grpc+=	bin/grpc_ruby_plugin
 
 .include "../../devel/protobuf/buildlink3.mk"
 .endif	# GRPC_BUILDLINK3_MK
