76e0c50f4dc514966933c478e0c0c6c6f0584491
nros <nros@pkgsrc.org>
2023-04-07 18:28:02 +0000

    Update qt6 to version 6.5.0
    Remove alot of the patches taken from or sent upstream.
    Add qt6-qtserialport to qt6 meta-pkg.
    This release has more PLIST files since header files
    for features for different OS:s are not installed
    on the ones not having them.
    
    Changes:
    More than a thousand bug fixes including some pkgsrc
    patches.

--- x11/qt6-qtbase/patches/patch-src_corelib_qt__cmdline.cmake
+++ x11/qt6-qtbase/patches/patch-src_corelib_qt__cmdline.cmake
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_corelib_qt__cmdline.cmake,v 1.1 2022/11/24 11:11:38 nros Exp $
+$NetBSD: patch-src_corelib_qt__cmdline.cmake,v 1.2 2023/04/07 18:28:05 nros Exp $
 
 * support avoiding the cmake archiving api using configure
   cmakes archiving api needs to be avoided since pkgsrc libarchive 
   that cmake uses is built without zstd support
 
---- src/corelib/qt_cmdline.cmake.orig	2021-02-11 10:23:05.000000000 +0000
+--- src/corelib/qt_cmdline.cmake.orig	2023-03-14 08:29:43.000000000 +0000
 +++ src/corelib/qt_cmdline.cmake
-@@ -12,3 +12,4 @@ qt_commandline_option(pps TYPE boolean N
+@@ -16,3 +16,4 @@ qt_commandline_option(pps TYPE boolean N
  qt_commandline_option(slog2 TYPE boolean)
  qt_commandline_option(syslog TYPE boolean)
- qt_commandline_option(trace TYPE optionalString VALUES etw lttng no yes)
+ qt_commandline_option(trace TYPE optionalString VALUES etw lttng ctf no yes)
 +qt_commandline_option(avoid_cmake_archiving_api TYPE boolean)
