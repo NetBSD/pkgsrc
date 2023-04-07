002b069d3f331e0dc223e20e6c141b4e238bbae0
nros <nros@pkgsrc.org>
2023-04-07 18:28:02 +0000

    Update qt6 to version 6.5.0
    Remove alot of the patches taken from or sent upstream.
    Add qt6-qtserialport to qt6 meta-pkg.
    This release has more PLIST files since header files
    for features for different OS:s are not installed
    on the ones not having them.
    
    Changes:
    More than a thousand bug fixes including some pkgsrc
    patches.

--- lang/qt6-qtdeclarative/patches/patch-src_qml_common_qv4alloca__p.h
+++ lang/qt6-qtdeclarative/patches/patch-src_qml_common_qv4alloca__p.h
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_qml_common_qv4alloca__p.h,v 1.1 2022/11/25 11:50:59 nros Exp $
+$NetBSD: patch-src_qml_common_qv4alloca__p.h,v 1.2 2023/04/07 18:28:04 nros Exp $
 
 Make sure we pick up gcc's builtin alloca on NetBSD
 
---- src/qml/common/qv4alloca_p.h.orig	2022-09-23 07:21:41.000000000 +0000
+--- src/qml/common/qv4alloca_p.h.orig	2023-03-23 08:11:12.000000000 +0000
 +++ src/qml/common/qv4alloca_p.h
 @@ -42,9 +42,13 @@
  #define Q_ALLOCA_DECLARE(type, name) \
      type *name = 0
  
 +#if defined(__GNUC__)
- #define Q_ALLOCA_ASSIGN(type, name, size) \
++#define Q_ALLOCA_ASSIGN(type, name, size) \
 +    name = static_cast<type*>(__builtin_alloca(size))
 +#else
-+#define Q_ALLOCA_ASSIGN(type, name, size) \
+ #define Q_ALLOCA_ASSIGN(type, name, size) \
      name = static_cast<type*>(alloca(size))
 -
 +#endif
--- lang/qt6-qtdeclarative/patches/patch-src_qml_jit_qv4assemblercommon__p.h
+++ lang/qt6-qtdeclarative/patches/patch-src_qml_jit_qv4assemblercommon__p.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_qml_jit_qv4assemblercommon__p.h,v 1.1 2022/11/25 11:50:59 nros Exp $
+$NetBSD: patch-src_qml_jit_qv4assemblercommon__p.h,v 1.2 2023/04/07 18:28:04 nros Exp $
 
-Add NetBSD & Solaris support.
+* Add NetBSD support
 
---- src/qml/jit/qv4assemblercommon_p.h.orig	2020-01-21 16:34:07.735772770 +0000
+--- src/qml/jit/qv4assemblercommon_p.h.orig	2023-04-05 23:40:17.393684190 +0000
 +++ src/qml/jit/qv4assemblercommon_p.h
-@@ -66,7 +66,7 @@ namespace QV4 {
+@@ -30,7 +30,7 @@ namespace QV4 {
  namespace JIT {
  
  #if defined(Q_PROCESSOR_X86_64) || defined(ENABLE_ALL_ASSEMBLERS_FOR_REFACTORING_PURPOSES)
--#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN)
-+#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN) || defined(Q_OS_NETBSD) || defined(Q_OS_SOLARIS)
+-#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN) || defined(Q_OS_SOLARIS)
++#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN) || defined(Q_OS_SOLARIS) || defined(Q_OS_NETBSD)
  
  class PlatformAssembler_X86_64_SysV : public JSC::MacroAssembler<JSC::MacroAssemblerX86_64>
  {
