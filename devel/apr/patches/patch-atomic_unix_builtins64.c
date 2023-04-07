a1b951ac575e505e7c3011733dd4d35f4bfa1f53
adam <adam@pkgsrc.org>
2023-04-07 09:37:00 +0000

    apr: updated to 1.7.3
    Changes for APR 1.7.3
    
    *) apr-1-config: Fix crosscompiling detection in apr-1-config.
    
    *) configure: Add --enable-sysv-shm to use SysV shared memory (shmget) if
       available.
    
    *) apr_socket_sendfile: Use WSAIoctl() to get TransmitFile function
       pointer on Windows.
    
    *) apr_dir_read: Do not request short file names on Windows 7
       and later.
    
    *) apr_file_gets: Optimize for buffered files on Windows.
    
    *) Fix a deadlock when writing to locked files opened with APR_FOPEN_APPEND
       on Windows.
    
    *) Don't seek to the end when opening files with APR_FOPEN_APPEND on Windows.
    
    *) apr_file_write: Optimize large writes to buffered files on Windows.
    
    *) apr_file_write: Optimize large reads from buffered files on Windows.

--- devel/apr/patches/patch-atomic_unix_builtins64.c
+++ devel/apr/patches/patch-atomic_unix_builtins64.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-atomic_unix_builtins64.c,v 1.2 2023/01/31 18:41:49 wiz Exp $
+$NetBSD: patch-atomic_unix_builtins64.c,v 1.3 2023/04/07 09:37:01 adam Exp $
 
 Work around missing 64bit atomic builtins for non-x86 ILP32 platforms.
 
---- atomic/unix/builtins64.c.orig	2022-06-27 21:55:09.000000000 +0000
+--- atomic/unix/builtins64.c.orig	2023-03-02 22:36:42.000000000 +0000
 +++ atomic/unix/builtins64.c
 @@ -16,7 +16,7 @@
  
@@ -11,9 +11,9 @@ Work around missing 64bit atomic builtins for non-x86 ILP32 platforms.
 -#ifdef USE_ATOMICS_BUILTINS64
 +#if defined (USE_ATOMICS_BUILTINS) && !defined (NEED_ATOMICS_GENERIC64)
  
- #if defined(__arm__) || defined(__powerpc__) || defined(__powerpc64__)
- #define WEAK_MEMORY_ORDERING 1
-@@ -106,4 +106,4 @@ APR_DECLARE(apr_uint64_t) apr_atomic_xch
+ #if defined(__i386__) || defined(__x86_64__) \
+     || defined(__s390__) || defined(__s390x__)
+@@ -107,4 +107,4 @@ APR_DECLARE(apr_uint64_t) apr_atomic_xch
  #endif
  }
  
