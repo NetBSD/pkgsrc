ef4e9aa9b26438fb3dc8a9ac94b4dd3af1845982
wiz <wiz@pkgsrc.org>
2023-04-09 21:44:13 +0000

    libXt: update to 1.3.0.
    Alan Coopersmith (19):
          gitlab CI: add a basic build test
          Build xz tarballs instead of bzip2
          Fix spelling/wording issues
          gitlab CI: stop requiring Signed-off-by in commits
          Use memcpy() instead of memmove() when buffers are known not to overlap
          Use memcpy() instead of XtMemmove() when buffers are known to differ
          tests: update g_test_bug_base url from bugzilla to gitlab
          tests: Use XORG_MEMORY_CHECK_FLAGS from xorg-macros 1.16
          tests: Replace g_assert() calls with g_assert_*() calls
          configure.ac: Replace HAVE_LIBRARY with AC_CHECK_LIB
          Add xfilesearchpath to xt.pc
          TMstate.c: Handle -Wduplicated-branches warnings
          Remove "All rights reserved" from Oracle copyright notices
          configure: Use LT_INIT from libtool 2 instead of deprecated AC_PROG_LIBTOOL
          test: Add unit tests for XtMalloc, XtCalloc, & XtRealloc
          Add XtReallocArray() for overflow checking of multiplied args
          Replace XtRealloc() calls with XtReallocArray()
          Replace XtMalloc() calls with XtMallocArray()
          libXt 1.3.0
    
    Alex Richardson (6):
          Define LONG64 if __SIZEOF_LONG__ indicates 64-bit long
          XtArgVal: Support architectures where pointers are bigger than long
          Use XtUIntPtr for integer types that can hold pointers
          Cast via intptr_t when converting integers to pointers
          Fix XrmResource layout if pointers are bigger than long
          Fix InternalCallbackRec layout if pointers are bigger than 64 bits
    
    Keith Packard (1):
          Support buttons > 5 in translation tables [v2]
    
    Thomas E. Dickey (7):
          add _X_NORETURN to agree with header-file
          codespell-fixes
          cppcheck and clang --analyze fixes
          fixes for gcc13 warnings
          cppcheck fixes (const, null dereferencing, uninitialized, scope)
          cppcheck (revise IsDescendant() to fix possible null-dereference)
          update copyright-date

--- x11/libXt/hacks.mk
+++ x11/libXt/hacks.mk
@@ -1,11 +1,11 @@
-# $NetBSD: hacks.mk,v 1.1 2008/01/29 03:37:02 bjs Exp $
+# $NetBSD: hacks.mk,v 1.2 2023/04/09 21:44:13 wiz Exp $
 
 .if !defined(LIBXT_HACKS_MK)
 LIBXT_HACKS_MK=	# defined
 
 .include "../../mk/compiler.mk"
 
-.if !empty(CC_VERSION:Mgcc-[34]*)
+.if ${CC_VERSION:Mgcc-[34]*}
 CFLAGS+=	-fno-strict-aliasing
 PKG_HACKS+=	no-strict-aliasing
 .endif
