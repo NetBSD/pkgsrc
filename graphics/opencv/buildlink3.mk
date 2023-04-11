7c0de3b9a21c4e4ca9b0a390a5e159b63750fd7e
adam <adam@pkgsrc.org>
2023-04-11 06:10:54 +0000

    revbump for jasper 4.0.0

--- graphics/opencv/buildlink3.mk
+++ graphics/opencv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2023/01/03 17:36:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2023/04/11 06:10:54 adam Exp $
 
 BUILDLINK_TREE+=	opencv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	opencv
 OPENCV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencv+=	opencv>=3.0.0
-BUILDLINK_ABI_DEPENDS.opencv+=	opencv>=3.4.16nb10
+BUILDLINK_ABI_DEPENDS.opencv+=	opencv>=3.4.16nb12
 BUILDLINK_PKGSRCDIR.opencv?=	../../graphics/opencv
 
 pkgbase := opencv
