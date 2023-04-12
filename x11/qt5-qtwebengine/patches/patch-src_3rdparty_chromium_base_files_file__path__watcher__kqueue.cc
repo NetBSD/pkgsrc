7898087c680b5680f544cf10386b8fc9b9b2ed9d
adam <adam@pkgsrc.org>
2023-04-12 19:24:03 +0000

    qt5: updated to 5.15.9
    5.15.9
    Bug fix release

--- x11/qt5-qtwebengine/patches/patch-src_3rdparty_chromium_base_files_file__path__watcher__kqueue.cc
+++ x11/qt5-qtwebengine/patches/patch-src_3rdparty_chromium_base_files_file__path__watcher__kqueue.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_3rdparty_chromium_base_files_file__path__watcher__kqueue.cc,v 1.1 2021/08/03 21:04:34 markd Exp $
+$NetBSD: patch-src_3rdparty_chromium_base_files_file__path__watcher__kqueue.cc,v 1.2 2023/04/12 19:24:06 adam Exp $
 
 data was intptr_t until NetBSD 9, then it switched to void * like every other operating system.
 
---- src/3rdparty/chromium/base/files/file_path_watcher_kqueue.cc.orig	2020-11-07 01:22:36.000000000 +0000
+--- src/3rdparty/chromium/base/files/file_path_watcher_kqueue.cc.orig	2022-03-30 09:48:18.000000000 +0000
 +++ src/3rdparty/chromium/base/files/file_path_watcher_kqueue.cc
 @@ -4,6 +4,15 @@
  
@@ -20,12 +20,19 @@ data was intptr_t until NetBSD 9, then it switched to void * like every other op
  #include <fcntl.h>
  #include <stddef.h>
  #include <sys/param.h>
-@@ -74,7 +83,7 @@ int FilePathWatcherKQueue::EventsForPath
+@@ -72,9 +81,15 @@ int FilePathWatcherKQueue::EventsForPath
+     FilePath::StringType subdir = (i != (components.end() - 1)) ? *(i + 1) : "";
+     EventData* data = new EventData(built_path, subdir);
      struct kevent event;
++#ifdef __NetBSD__
++    EV_SET(&event, fd, EVFILT_VNODE, (EV_ADD | EV_CLEAR | EV_RECEIPT),
++           (NOTE_DELETE | NOTE_WRITE | NOTE_ATTRIB |
++            NOTE_RENAME | NOTE_REVOKE | NOTE_EXTEND), 0, reinterpret_cast<KEVENT_TYPE>(data));
++#else
      EV_SET(&event, fd, EVFILT_VNODE, (EV_ADD | EV_CLEAR | EV_RECEIPT),
             (NOTE_DELETE | NOTE_WRITE | NOTE_ATTRIB |
--            NOTE_RENAME | NOTE_REVOKE | NOTE_EXTEND), 0, data);
-+            NOTE_RENAME | NOTE_REVOKE | NOTE_EXTEND), 0, reinterpret_cast<KEVENT_TYPE>(data));
+             NOTE_RENAME | NOTE_REVOKE | NOTE_EXTEND), 0, data);
++#endif
      events->push_back(event);
    }
    return last_existing_entry;
