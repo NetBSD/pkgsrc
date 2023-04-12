3349ab7b7de7f6c2323a0ee19e6d1f2f07e7d5e5
wiz <wiz@pkgsrc.org>
2023-04-12 15:55:18 +0000

    libpaper: fix bl3.mk too

--- print/libpaper/buildlink3.mk
+++ print/libpaper/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/04/12 15:52:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/04/12 15:55:18 wiz Exp $
 
 BUILDLINK_TREE+=	libpaper
 
 .if !defined(LIBPAPER_BUILDLINK3_MK)
 LIBPAPER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpaper+=	libpaper>=2.0.10
-BUILDLINK_PKGSRCDIR.libpaper?=		../../wip/libpaper2
-
-.endif	# LIBPAPER_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.libpaper+=	libpaper>=1.1.22
+BUILDLINK_PKGSRCDIR.libpaper?=		../../print/libpaper
+.endif # LIBPAPER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libpaper
