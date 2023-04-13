e474955435e293df5b9caac2acea2d46362444bd
wiz <wiz@pkgsrc.org>
2023-04-13 12:14:29 +0000

    *: bump for rasqal dependency change

--- textproc/redland/buildlink3.mk
+++ textproc/redland/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.40 2022/11/23 16:19:04 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.41 2023/04/13 12:14:29 wiz Exp $
 
 BUILDLINK_TREE+=	redland
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	redland
 REDLAND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.redland+=	redland>=1.0.7
-BUILDLINK_ABI_DEPENDS.redland+=	redland>=1.0.17nb35
+BUILDLINK_ABI_DEPENDS.redland+=	redland>=1.0.17nb36
 BUILDLINK_PKGSRCDIR.redland?=	../../textproc/redland
 
 .include "../../security/openssl/buildlink3.mk"
