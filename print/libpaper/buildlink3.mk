f7a89c3ca0d64f60ca74264421d7c4798a08527b
wiz <wiz@pkgsrc.org>
2023-04-13 14:13:56 +0000

    *: recursive PKGREVISION bump for libpaper shlib major bump

--- print/libpaper/buildlink3.mk
+++ print/libpaper/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/04/12 15:55:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/04/13 14:13:56 wiz Exp $
 
 BUILDLINK_TREE+=	libpaper
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libpaper
 LIBPAPER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpaper+=	libpaper>=1.1.22
+BUILDLINK_ABI_DEPENDS.libpaper?=		libpaper>=2.1.0
 BUILDLINK_PKGSRCDIR.libpaper?=		../../print/libpaper
 .endif # LIBPAPER_BUILDLINK3_MK
 
