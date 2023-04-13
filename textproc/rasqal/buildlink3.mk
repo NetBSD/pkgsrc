48a1a89583c292fc265ad7d7f5a790a2ae3dba64
wiz <wiz@pkgsrc.org>
2023-04-13 12:14:29 +0000

    *: bump for rasqal dependency change

--- textproc/rasqal/buildlink3.mk
+++ textproc/rasqal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2023/04/13 12:13:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2023/04/13 12:14:29 wiz Exp $
 
 BUILDLINK_TREE+=	rasqal
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	rasqal
 RASQAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.17
-BUILDLINK_ABI_DEPENDS.rasqal+=	rasqal>=0.9.33nb27
+BUILDLINK_ABI_DEPENDS.rasqal+=	rasqal>=0.9.33nb28
 BUILDLINK_PKGSRCDIR.rasqal?=	../../textproc/rasqal
 
 .include "../../devel/gmp/buildlink3.mk"
