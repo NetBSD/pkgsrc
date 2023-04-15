d9f225ebc7bc68763e625b1e19c398b67379e111
taca <taca@pkgsrc.org>
2023-04-15 02:19:13 +0000

    lang/php81: update to 8.1.18
    13 Apr 2023, PHP 8.1.18
    
    - Core:
      . Added optional support for max_execution_time in ZTS/Linux builds
        (Kévin Dunglas)
      . Fixed use-after-free in recursive AST evaluation. (ilutov)
      . Fixed bug GH-8646 (Memory leak PHP FPM 8.1). (nielsdos)
      . Fixed bug GH-10801 (Named arguments in CTE functions cause a segfault).
        (nielsdos)
      . Fixed bug GH-8789 (PHP 8.0.20 (ZTS) zend_signal_handler_defer crashes on
        apache). (nielsdos)
      . Fixed bug GH-10015 (zend_signal_handler_defer crashes on apache shutdown).
        (nielsdos)
      . Fixed bug GH-10810 (Fix NUL byte terminating Exception::__toString()).
        (ilutov)
      . Fix potential memory corruption when mixing __callStatic() and FFI. (ilutov)
    
    - Date:
      . Fixed bug GH-10583 (DateTime modify with tz pattern should not update
        linked timezone). (Derick)
    
    - FPM:
      . Fixed bug GH-10611 (fpm_env_init_main leaks environ). (nielsdos)
      . Destroy file_handle in fpm_main. (Jakub Zelenka, nielsdos)
      . Fixed bug #74129 (Incorrect SCRIPT_NAME with apache ProxyPassMatch when
        spaces are in path). (Jakub Zelenka)
    
    - FTP:
      . Propagate success status of ftp_close(). (nielsdos)
      . Fixed bug GH-10521 (ftp_get/ftp_nb_get resumepos offset is maximum 10GB).
        (nielsdos)
    
    - IMAP:
      . Fix build failure with Clang 16. (orlitzky)
    
    - MySQLnd:
      . Fixed bug GH-8979 (Possible Memory Leak with SSL-enabled MySQL
        connections). (nielsdos)
    
    - Opcache:
      . Fixed build for macOS to cater with pkg-config settings. (David Carlier)
      . Fixed bug GH-8065 (opcache.consistency_checks > 0 causes segfaults in
        PHP >= 8.1.5 in fpm context). (nielsdos)
    
    - OpenSSL:
      . Add missing error checks on file writing functions. (nielsdos)
    
    - PDO Firebird:
      . Fixed bug GH-10908 (Bus error with PDO Firebird on RPI with 64 bit kernel
        and 32 bit userland). (nielsdos)
    
    - PDO ODBC:
      . Fixed missing and inconsistent error checks on SQLAllocHandle. (nielsdos)
    
    - Phar:
      . Fixed bug GH-10766 (PharData archive created with Phar::Zip format does
        not keep files metadata (datetime)). (nielsdos)
      . Add missing error checks on EVP_MD_CTX_create() and EVP_VerifyInit().
        (nielsdos)
    
    - PGSQL:
      . Fixed typo in the array returned from pg_meta_data (extended mode).
        (David Carlier)
    
    - SPL:
      . Fixed bug GH-10519 (Array Data Address Reference Issue). (Nathan Freeman)
      . Fixed bug GH-10844 (ArrayIterator allows modification of readonly props).
        (ilutov)
    
    - Standard:
      . Fixed bug GH-10885 (stream_socket_server context leaks). (ilutov)
      . Fixed bug GH-10052 (Browscap crashes PHP 8.1.12 on request shutdown
        (apache2)). (nielsdos)
      . Fixed oss-fuzz #57392 (Buffer-overflow in php_fgetcsv() with \0 delimiter
        and enclosure). (ilutov)
      . Fixed undefined behaviour in unpack(). (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.394 2023/04/15 02:17:13 taca Exp $
+# $NetBSD: phpversion.mk,v 1.395 2023/04/15 02:19:13 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.28
-PHP81_VERSION=	8.1.17
+PHP81_VERSION=	8.1.18
 PHP82_VERSION=	8.2.5
 
 # Define API version or initial release of major version.
