87a9f47b2e4b99e96600879258634921ef60f7b5
adam <adam@pkgsrc.org>
2023-04-17 17:43:48 +0000

    lv2: updated to 1.18.10
    lv2 (1.18.10) stable; urgency=medium
    
    * Fix includedir in pkg-config file
    * Fix missing example plugin UI binaries
    * Only install lv2specgen when required Python modules are found
    * Replace change history data with a plain text NEWS file
    
    
    lv2 (1.18.8) stable; urgency=medium
    
    * Fix documentation build with Python 3.7
    * Fix documentation build with meson 0.56.2
    * Fix lv2.h missing from installation
    * eg-midigate: Fix output timing
    * eg-sampler: Add resampling via libsamplerate
    * eg-sampler: Fix potentially corrupt notification events
    * lv2core: Fix inconsistent plugin class labels
    * lv2specgen: Fix installed data paths
    
    
    lv2 (1.18.6) stable; urgency=medium
    
    * Add dark mode style for documentation
    * Clean up and modernize Python support code
    * Fix or avoid new compiler and tool warnings
    * Rearrange source tree to be directly usable by dependants
    * Remove archaic properties from foaf vocabulary
    * Replace canonical dcs ontology with a minimal version for LV2
    * Separate API headers from data
    * Switch to Meson build system

--- audio/lv2/options.mk
+++ audio/lv2/options.mk
@@ -1,36 +1,33 @@
-# $NetBSD: options.mk,v 1.3 2020/05/19 17:05:44 nia Exp $
+# $NetBSD: options.mk,v 1.4 2023/04/17 17:43:48 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lv2
-PKG_SUPPORTED_OPTIONS=	debug doc tests valgrind
-PKG_SUGGESTED_OPTIONS+=	# blank
+PKG_SUPPORTED_OPTIONS=	doc tests
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mdebug)
-BUILD_DEBUG=		--debug
-.else
-BUILD_DEBUG=		# blank
-.endif
-
 .if !empty(PKG_OPTIONS:Mdoc)
-PLIST_SRC+=		PLIST.doc
-BUILD_DOC=		--docs
-BUILD_DEPENDS+=		asciidoc-[0-9]*:../../textproc/asciidoc
-BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
-BUILD_DEPENDS+=		${PYPKGPREFIX}-rdflib-[0-9]*:../../textproc/py-rdflib
+TOOL_DEPENDS+=	asciidoc-[0-9]*:../../textproc/asciidoc
+TOOL_DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
+TOOL_DEPENDS+=	${PYPKGPREFIX}-lxml-[0-9]*:../../textproc/py-lxml
+TOOL_DEPENDS+=	${PYPKGPREFIX}-markdown-[0-9]*:../../textproc/py-markdown
+TOOL_DEPENDS+=	${PYPKGPREFIX}-pygments-[0-9]*:../../textproc/py-pygments
+TOOL_DEPENDS+=	${PYPKGPREFIX}-rdflib-[0-9]*:../../textproc/py-rdflib
+MESON_ARGS+=	-Ddocs=enabled
+
+PLIST_SRC=	${PLIST_SRC_DFLT} PLIST.doc ${WRKDIR}/PLIST.STATIC
+
+post-install: doxygen-plist
+.PHONY: doxygen-plist
+doxygen-plist:
+	cd ${DESTDIR}${PREFIX} && \
+	${FIND} share/doc/lv2 -type f -print > ${WRKDIR}/PLIST.STATIC
 .else
-BUILD_DOC=		# blank
+MESON_ARGS+=	-Ddocs=disabled
 .endif
 
 .if !empty(PKG_OPTIONS:Mtests)
-BUILD_TESTS=		--test
+MESON_ARGS+=	-Dtests=enabled
+USE_LANGUAGES+=	c c++
 .else
-BUILD_TESTS=		# blank
-.endif
-
-.if !empty(PKG_OPTIONS:Mvalgrind)
-BUILD_VALGRIND=		--grind
-BUILD_DEPENDS+=		valgrind-[0-9]*:../../devel/valgrind
-.else
-BUILD_VALGRIND=		# blank
+MESON_ARGS+=	-Dtests=disabled
 .endif
