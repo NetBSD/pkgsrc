f1c753d8bcc58f77f7a3ddaea3b1ac473512f612
ryoon <ryoon@pkgsrc.org>
2023-04-18 14:31:59 +0000

    blender: Update to 3.5.0
    Changelog:
    Blender 3.5 is feature packed, touching almost every corner to make
    Blender better in every way:
    
        GPU accelerated viewport compositor
        Support for sculpting with Vector Displacement Maps (VDM) textures
        26 built-in node groups assets for Hair grooming
        Faster rendering of scenes with many lights in Cycles
        Import and export USDZ

--- graphics/blender/patches/patch-source_blender_blenkernel_intern_mesh__validate.cc
+++ graphics/blender/patches/patch-source_blender_blenkernel_intern_mesh__validate.cc
@@ -1,17 +1,17 @@
-$NetBSD: patch-source_blender_blenkernel_intern_mesh__validate.cc,v 1.1 2023/01/22 21:24:37 ryoon Exp $
+$NetBSD: patch-source_blender_blenkernel_intern_mesh__validate.cc,v 1.2 2023/04/18 14:31:59 ryoon Exp $
 
---- source/blender/blenkernel/intern/mesh_validate.cc.orig	2022-11-04 00:33:06.000000000 +0000
+--- source/blender/blenkernel/intern/mesh_validate.cc.orig	2023-02-15 22:58:24.000000000 +0000
 +++ source/blender/blenkernel/intern/mesh_validate.cc
-@@ -307,7 +307,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
+@@ -306,7 +306,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
      bool fix_normal = true;
  
      for (j = 0; j < 3; j++) {
--      if (!isfinite(mv->co[j])) {
-+      if (!std::isfinite(mv->co[j])) {
+-      if (!isfinite(vert_positions[i][j])) {
++      if (!std::isfinite(vert_positions[i][j])) {
          PRINT_ERR("\tVertex %u: has invalid coordinate", i);
  
          if (do_fixes) {
-@@ -814,7 +814,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
+@@ -813,7 +813,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
  
        for (j = 0, dw = dv->dw; j < dv->totweight; j++, dw++) {
          /* NOTE: greater than max defgroups is accounted for in our code, but not < 0. */
