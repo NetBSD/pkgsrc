888fd0b2d72502d29f47dcb137c3ce087bed087e
adam <adam@pkgsrc.org>
2023-04-18 07:37:42 +0000

    py-psutil: updated to 5.9.5
    5.9.5
    =====
    
    2023-04-17
    
    **Enhancements**
    
    - 2196_: in case of exception, display a cleaner error traceback by hiding the
      `KeyError` bit deriving from a missed cache hit.
    - 2217_: print the full traceback when a `DeprecationWarning` or `UserWarning`
      is raised.
    - 2230_, [OpenBSD]: `psutil.net_connections`_ implementation was rewritten from
      scratch:
      - We're now able to retrieve the path of AF_UNIX sockets (before it was an
        empty string)
      - The function is faster since it no longer iterates over all processes.
      - No longer produces duplicate connection entries.
    - 2238_: there are cases where `Process.cwd()`_ cannot be determined
      (e.g. directory no longer exists), in which case we returned either ``None``
      or an empty string. This was consolidated and we now return ``""`` on all
      platforms.
    - 2239_, [UNIX]: if process is a zombie, and we can only determine part of the
      its truncated `Process.name()`_ (15 chars), don't fail with `ZombieProcess`_
      when we try to guess the full name from the `Process.cmdline()`_. Just
      return the truncated name.
    - 2240_, [NetBSD], [OpenBSD]: add CI testing on every commit for NetBSD and
      OpenBSD platforms (python 3 only).
    
    **Bug fixes**
    
    - 1043_, [OpenBSD] `psutil.net_connections`_ returns duplicate entries.
    - 1915_, [Linux]: on certain kernels, ``"MemAvailable"`` field from
      ``/proc/meminfo`` returns ``0`` (possibly a kernel bug), in which case we
      calculate an approximation for ``available`` memory which matches "free"
      CLI utility.
    - 2164_, [Linux]: compilation fails on kernels < 2.6.27 (e.g. CentOS 5).
    - 2186_, [FreeBSD]: compilation fails with Clang 15.  (patch by Po-Chuan Hsieh)
    - 2191_, [Linux]: `disk_partitions()`_: do not unnecessarily read
      /proc/filesystems and raise `AccessDenied`_ unless user specified `all=False`
      argument.
    - 2216_, [Windows]: fix tests when running in a virtual environment (patch by
      Matthieu Darbois)
    - 2225_, [POSIX]: `users()`_ loses precision for ``started`` attribute (off by
      1 minute).
    - 2229_, [OpenBSD]: unable to properly recognize zombie processes.
      `NoSuchProcess`_ may be raised instead of `ZombieProcess`_.
    - 2231_, [NetBSD]: *available*  `virtual_memory()`_ is higher than *total*.
    - 2234_, [NetBSD]: `virtual_memory()`_ metrics are wrong: *available* and
      *used* are too high. We now match values shown by *htop* CLI utility.
    - 2236_, [NetBSD]: `Process.num_threads()`_ and `Process.threads()`_ return
      threads that are already terminated.
    - 2237_, [OpenBSD], [NetBSD]: `Process.cwd()`_ may raise ``FileNotFoundError``
      if cwd no longer exists. Return an empty string instead.

--- sysutils/py-psutil/patches/patch-setup.py
+++ sysutils/py-psutil/patches/patch-setup.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-setup.py,v 1.6 2022/11/08 08:50:20 adam Exp $
+$NetBSD: patch-setup.py,v 1.7 2023/04/18 07:37:42 adam Exp $
 
 Enable limited API on all platforms.
 
---- setup.py.orig	2022-11-08 08:46:24.000000000 +0000
+--- setup.py.orig	2023-04-17 15:01:41.000000000 +0000
 +++ setup.py
-@@ -109,7 +109,7 @@ def get_version():
- VERSION = get_version()
- macros.append(('PSUTIL_VERSION', int(VERSION.replace('.', ''))))
+@@ -111,7 +111,7 @@ macros.append(('PSUTIL_VERSION', int(VER
  
+ # Py_LIMITED_API lets us create a single wheel which works with multiple
+ # python versions, including unreleased ones.
 -if bdist_wheel and CP36_PLUS and (MACOS or LINUX or WINDOWS):
 +if bdist_wheel and CP36_PLUS:
      py_limited_api = {"py_limited_api": True}
