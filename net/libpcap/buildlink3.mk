2fbdd059c15c37ac678d579b410ebd15da4ffe1e
adam <adam@pkgsrc.org>
2023-04-18 07:59:11 +0000

    libpcap: updated to 1.10.4
    Summary for 1.10.4 libpcap release
      Source code:
        Fix spaces before tabs in indentation.
      rpcap:
        Fix name of launchd service.
      Documentation:
        Document use of rpcapd with systemd, launchd, inetd, and xinetd.
      Building and testing:
        Require at least pkg-config 0.17.0, as we use --static.
        Get rid of the remains of gnuc.h.
        Require at least autoconf 2.69.
        Update config.{guess,sub}, timestamps 2023-01-01,2023-01-21.

--- net/libpcap/buildlink3.mk
+++ net/libpcap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2013/02/06 19:30:54 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2023/04/18 07:59:11 adam Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -7,9 +7,8 @@ BUILDLINK_TREE+=	libpcap
 .if !defined(LIBPCAP_BUILDLINK3_MK)
 LIBPCAP_BUILDLINK3_MK:=
 
-# XXX?
 BUILDLINK_API_DEPENDS.libpcap+=	libpcap>=0.5.0
-BUILDLINK_ABI_DEPENDS.libpcap?=	libpcap>=1.3.0nb1
+BUILDLINK_ABI_DEPENDS.libpcap+=	libpcap>=1.3.0nb1
 BUILDLINK_PKGSRCDIR.libpcap?=	../../net/libpcap
 
 # on Solaris, we always need libnsl and libsocket because libpcap.a
