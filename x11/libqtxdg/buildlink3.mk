2b322b90493e012f8c13820b5c9d892bb6ac42f9
pin <pin@pkgsrc.org>
2023-04-18 09:35:29 +0000

    x11/libqtxdg: update to 3.11.0
    libqtxdg-3.11.0 / 2023-04-15
    ============================
     * Fixed file association with symlink desktop entries.
     * Fixed typos.

--- x11/libqtxdg/buildlink3.mk
+++ x11/libqtxdg/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.20 2023/01/29 21:15:12 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2023/04/18 09:35:29 pin Exp $
 
 BUILDLINK_TREE+=	libqtxdg
 
 .if !defined(LIBQTXDG_BUILDLINK3_MK)
 LIBQTXDG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.9.1
-BUILDLINK_ABI_DEPENDS.libqtxdg?=	libqtxdg>=3.10.0nb2
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.11.0
 BUILDLINK_PKGSRCDIR.libqtxdg?=		../../x11/libqtxdg
 
 .include "../../devel/glib2/buildlink3.mk"
