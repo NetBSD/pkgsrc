2fb753ec3adaf6b1ae25ba933dbb6bf19c39ff7c
ryoon <ryoon@pkgsrc.org>
2023-04-18 14:31:59 +0000

    blender: Update to 3.5.0
    Changelog:
    Blender 3.5 is feature packed, touching almost every corner to make
    Blender better in every way:
    
        GPU accelerated viewport compositor
        Support for sculpting with Vector Displacement Maps (VDM) textures
        26 built-in node groups assets for Hair grooming
        Faster rendering of scenes with many lights in Cycles
        Import and export USDZ

--- graphics/blender/patches/patch-intern_locale_boost__locale__wrapper.cpp
+++ graphics/blender/patches/patch-intern_locale_boost__locale__wrapper.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-intern_locale_boost__locale__wrapper.cpp,v 1.1 2023/01/22 21:24:37 ryoon Exp $
+$NetBSD: patch-intern_locale_boost__locale__wrapper.cpp,v 1.2 2023/04/18 14:31:59 ryoon Exp $
 
---- intern/locale/boost_locale_wrapper.cpp.orig	2022-11-04 00:33:06.000000000 +0000
+--- intern/locale/boost_locale_wrapper.cpp.orig	2023-02-15 22:58:23.000000000 +0000
 +++ intern/locale/boost_locale_wrapper.cpp
 @@ -5,6 +5,7 @@
   * \ingroup intern_locale
@@ -8,5 +8,5 @@ $NetBSD: patch-intern_locale_boost__locale__wrapper.cpp,v 1.1 2023/01/22 21:24:3
  
 +#include <iostream>
  #include <boost/locale.hpp>
+ #include <iostream>
  #include <stdio.h>
- 
