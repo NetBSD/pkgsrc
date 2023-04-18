42f6f0c9511c164b2ada9f1f01885222d8cc1842
ryoon <ryoon@pkgsrc.org>
2023-04-18 14:00:11 +0000

    firefox: Update to 112.0.1
    Changelog:
    112.0.1
    Fixed
    
      * Fixed a bug where cookie dates appear to be set in the far future after
        updating Firefox. This may have caused cookies to be unintentionally
        purged. (bug 1827669).
    
    112.0
    New
    
      * Right-clicking on password fields now shows an option to reveal the
        password.
    
      * Ubuntu Linux users can now import their browser data from the Chromium Snap
        package. Currently, this will only work if Firefox is not also installed as
        a Snap package, but work is underway to address this!
    
      * Do you use the tab list panel in the tab bar? If so, you can now close tabs
        by middle-clicking items in that list.
    
      * You've always been able to un-close a tab by using (Cmd/Ctrl)-Shift-T. Now,
        that same shortcut will restore the previous session if there are no more
        closed tabs from the same session to re-open.
    
      * For all ETP Strict users, we extended the list of known tracking parameters
        that are removed from URLs to further protect our users from cross-site
        tracking.
    
      * Enables overlay of software-decoded video on Intel GPUs in Windows.
        Improves video down scaling quality and reduces GPU usage.
    
      * Private windows and ETP set to strict will now include email tracking
        protection. This will make it harder for email trackers to learn the
        browsing habits of Firefox users. You can check the Tracking Content in the
        sub-panel on the shield icon panel.
    
    Fixed
    
      * Various security fixes.
    
    Changed
    
      * The deprecated U2F Javascript API is now disabled by default. The U2F
        protocol remains usable through the WebAuthn API. The U2F API can be
        re-enabled using the security.webauth.u2f preference.
    
    Security fixes:
    #CVE-2023-29531: Out-of-bound memory access in WebGL on macOS
    #CVE-2023-29532: Mozilla Maintenance Service Write-lock bypass
    #CVE-2023-29533: Fullscreen notification obscured
    #CVE-2023-29534: Fullscreen notification could have been obscured on Firefox
     for Android
    #CVE-2023-1999: Double-free in libwebp
    #CVE-2023-29535: Potential Memory Corruption following Garbage Collector
     compaction
    #CVE-2023-29536: Invalid free from JavaScript code
    #CVE-2023-29537: Data Races in font initialization code
    #CVE-2023-29538: Directory information could have been leaked to WebExtensions
    #CVE-2023-29539: Content-Disposition filename truncation leads to Reflected
     File Download
    #CVE-2023-29540: Iframe sandbox bypass using redirects and sourceMappingUrls
    #CVE-2023-29541: Files with malicious extensions could have been downloaded
     unsafely on Linux
    #CVE-2023-29542: Bypass of file download extension restrictions
    #CVE-2023-29543: Use-after-free in debugging APIs
    #CVE-2023-29544: Memory Corruption in garbage collector
    #CVE-2023-29545: Windows Save As dialog resolved environment variables
    #CVE-2023-29546: Screen recording in Private Browsing included address bar on
     Android
    #CVE-2023-29547: Secure document cookie could be spoofed with insecure cookie
    #CVE-2023-29548: Incorrect optimization result on ARM64
    #CVE-2023-29549: Javascript's bind function may have failed
    #CVE-2023-29550: Memory safety bugs fixed in Firefox 112 and Firefox ESR 102.10
    #CVE-2023-29551: Memory safety bugs fixed in Firefox 112

--- www/firefox/patches/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland_egl__dmabuf.cc
+++ www/firefox/patches/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland_egl__dmabuf.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland_egl__dmabuf.cc,v 1.1 2023/04/05 14:22:36 ryoon Exp $
+$NetBSD: patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland_egl__dmabuf.cc,v 1.2 2023/04/18 14:00:11 ryoon Exp $
 
---- third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc.orig	2023-03-02 21:15:55.000000000 +0000
+--- third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc.orig	2023-03-30 21:16:09.000000000 +0000
 +++ third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
-@@ -10,11 +10,15 @@
+@@ -10,12 +10,16 @@
  
  #include "modules/desktop_capture/linux/wayland/egl_dmabuf.h"
  
@@ -11,6 +11,7 @@ $NetBSD: patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland_egl
 +#endif
  #include <dlfcn.h>
  #include <fcntl.h>
+ #include <gdk/gdk.h>
  #include <libdrm/drm_fourcc.h>
 +#if defined(__linux__)
  #include <linux/types.h>
