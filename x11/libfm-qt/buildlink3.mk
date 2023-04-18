aa04fe4ed881f7bc0378fac2858be61ff1a241c6
pin <pin@pkgsrc.org>
2023-04-18 09:39:27 +0000

    x11/libfm-qt: update to 1.3.0
    libfm-qt-1.3.0 / 2023-04-15
    ============================
     * Fixed the thumbnail/icon rectangle in list views.
     * Allowed typing of text tab with inline renaming.
     * Open non-executable files that have executable file type
       (like non-executable scripts with root executable flag).
     * Allowed disabling of smooth scrolling in all view modes.
     * File dialog: Add extension on saving only if it makes sense.
     * Go to name start/end with Home/End keys during inline renaming.
     * Keep selection and tree expansion in app chooser dialog when menu-cached
       reloads the apps tree.
     * Fixed pressing of ENTER in the file dialog, especially in the file-saving
       dialog.
     * Ensure `Escape` closes the popup of path-edit completer.
     * Treat zero-sized files based on their extensions.
     * Use "New file" as the default name for new files
       (especially after GLib 2.75.1).
     * Ensure toolbar background is shown behind path-bar.
     * Prevent long content search texts from widening search dialog.

--- x11/libfm-qt/buildlink3.mk
+++ x11/libfm-qt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.21 2023/01/29 21:15:11 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2023/04/18 09:39:27 pin Exp $
 
 BUILDLINK_TREE+=	libfm-qt
 
 .if !defined(LIBFM_QT_BUILDLINK3_MK)
 LIBFM_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.2.1
-BUILDLINK_ABI_DEPENDS.libfm-qt?=	libfm-qt>=1.2.1nb1
+BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.3.0
 BUILDLINK_PKGSRCDIR.libfm-qt?=		../../x11/libfm-qt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
