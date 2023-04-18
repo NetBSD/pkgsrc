ed78e36308ffcdf1f828a99f3a4a3e289c319d27
pin <pin@pkgsrc.org>
2023-04-18 11:39:47 +0000

    x11/lxqt-globalkeys: update to 1.3.0
    lxqt-globalkeys-1.3.0 / 2023-04-15
    ===================================
     * Updated translations and bumped the version.

--- x11/lxqt-globalkeys/buildlink3.mk
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.18 2023/01/29 21:15:12 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2023/04/18 11:39:47 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=1.1.0
-BUILDLINK_ABI_DEPENDS.lxqt-globalkeys?=	lxqt-globalkeys>=1.2.0nb4
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=1.3.0
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
