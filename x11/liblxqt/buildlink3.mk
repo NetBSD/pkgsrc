34c4d1452be595b980b2933180eb02a4a816536a
pin <pin@pkgsrc.org>
2023-04-18 09:38:08 +0000

    x11/liblxqt: update to 1.3.0
    liblxqt-1.3.0 / 2023-04-15
    ===========================
     * Updated translations and bumped version to 1.3.0.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.21 2023/01/29 21:15:12 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2023/04/18 09:38:08 pin Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.2.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=1.2.0nb2
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.3.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
