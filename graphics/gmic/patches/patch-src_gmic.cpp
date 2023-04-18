1defe072e29a7939e1b9abba8a801f766825248a
wiz <wiz@pkgsrc.org>
2023-04-18 16:00:08 +0000

    gmic: update to 3.2.3.
    Three years of development.

--- graphics/gmic/patches/patch-src_gmic.cpp
+++ graphics/gmic/patches/patch-src_gmic.cpp
@@ -1,22 +1,10 @@
-$NetBSD: patch-src_gmic.cpp,v 1.1 2020/05/23 10:44:02 ryoon Exp $
+$NetBSD: patch-src_gmic.cpp,v 1.2 2023/04/18 16:00:08 wiz Exp $
 
 * Support NetBSD.
 
---- src/gmic.cpp.orig	2020-03-28 13:25:12.000000000 +0000
+--- src/gmic.cpp.orig	2023-04-04 16:20:28.000000000 +0000
 +++ src/gmic.cpp
-@@ -2324,7 +2324,11 @@ bool *gmic::abort_ptr(bool *const p_is_a
- #if defined(__MACOSX__) || defined(__APPLE__)
-   void* tid = (void*)(cimg_ulong)getpid();
- #elif cimg_OS==1
-+#if defined(__NetBSD__)
-+  void* tid = (void*)(cimg_ulong)pthread_self();
-+#else
-   void* tid = (void*)(cimg_ulong)syscall(SYS_gettid);
-+#endif
- #elif cimg_OS==2
-   void* tid = (void*)(cimg_ulong)GetCurrentThreadId();
- #else
-@@ -2677,7 +2681,11 @@ gmic::~gmic() {
+@@ -2205,7 +2205,11 @@ inline void* get_tid() {
  #if defined(__MACOSX__) || defined(__APPLE__)
    void* tid = (void*)(cimg_ulong)getpid();
  #elif cimg_OS==1
