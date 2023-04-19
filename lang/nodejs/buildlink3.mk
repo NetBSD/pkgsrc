fd453d55c1f6a869aba71f84a31f48eaafbdc3a2
adam <adam@pkgsrc.org>
2023-04-19 17:38:35 +0000

    nodejs: updated to 19.9.0
    Version 19.9.0 (Current)
    
    Notable Changes
    
    Tracing Channel in diagnostic_channel
    New URL.canParse API
    (SEMVER-MINOR) add getMaxListeners method (Khafra)
    (SEMVER-MINOR) migrate to WiX4 (Stefan Stojanovic)
    (SEMVER-MINOR) deprecate napi_module_register (Vladimir Morozov)
    (SEMVER-MINOR) add setter & getter for default highWaterMark (Robert Nagy)
    (SEMVER-MINOR) expose reporter for use in run api (Chemi Atlow)

--- lang/nodejs/buildlink3.mk
+++ lang/nodejs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2022/05/05 07:08:06 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2023/04/19 17:38:35 adam Exp $
 
 BUILDLINK_TREE+=	nodejs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nodejs
 NODEJS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nodejs+=	nodejs>=10
-BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=18.1.0
+BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=19.9.0nb1
 BUILDLINK_PKGSRCDIR.nodejs?=	../../lang/nodejs
 
 .include "../../mk/bsd.fast.prefs.mk"
