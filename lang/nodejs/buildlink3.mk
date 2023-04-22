e296557c6212acd2d2312f27f212410114582b67
gutteridge <gutteridge@pkgsrc.org>
2023-04-22 23:54:10 +0000

    nodejs: fix BUILDLINK_ABI_DEPENDS pattern (fix bulk builds)

--- lang/nodejs/buildlink3.mk
+++ lang/nodejs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2023/04/19 17:38:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2023/04/22 23:54:10 gutteridge Exp $
 
 BUILDLINK_TREE+=	nodejs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nodejs
 NODEJS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nodejs+=	nodejs>=10
-BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=19.9.0nb1
+BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=19.9.0
 BUILDLINK_PKGSRCDIR.nodejs?=	../../lang/nodejs
 
 .include "../../mk/bsd.fast.prefs.mk"
