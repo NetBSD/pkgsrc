48c035163e83fcc5b03b29c310e6de327b0cf6af
rillig <rillig@pkgsrc.org>
2023-04-22 14:32:48 +0000

    pkgtools/pkglint: update to 23.1.7
    Changes since 23.1.0:
    
    No functional changes.
    
    Instead of having two copies of the code, install the package by
    downloading the releases from GitHub.
    
    Reduce the build-time dependencies by delivering pre-generated parser
    code.

--- pkgtools/pkglint/go-modules.mk
+++ pkgtools/pkglint/go-modules.mk
@@ -1,13 +1,13 @@
-# $NetBSD: go-modules.mk,v 1.1 2022/09/17 15:50:39 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.2 2023/04/22 14:32:48 rillig Exp $
 
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.mod
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.zip
 GO_MODULE_FILES+=	github.com/kr/pty/@v/v1.1.1.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220829220503-c86fa9a7ed90.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220829220503-c86fa9a7ed90.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210615035016-665e8c7367d1.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.6.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.6.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.mod
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.zip
