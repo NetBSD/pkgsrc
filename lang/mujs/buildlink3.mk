4beb9e470e042e045a76c78cd9e3c42bce064d73
wiz <wiz@pkgsrc.org>
2023-04-24 07:50:40 +0000

    mujs: update to 1.3.3.
    Changes not found.

--- lang/mujs/buildlink3.mk
+++ lang/mujs/buildlink3.mk
@@ -1,11 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/08/30 12:47:02 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/04/24 07:50:40 wiz Exp $
 
 BUILDLINK_TREE+=	mujs
 
 .if !defined(MUJS_BUILDLINK3_MK)
 MUJS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mujs+=	mujs>=1.2.0
+BUILDLINK_DEPMETHOD.mujs?=	build
+
+BUILDLINK_API_DEPENDS.mujs+=	mujs>=1.3.3
 BUILDLINK_PKGSRCDIR.mujs?=	../../lang/mujs
 .endif	# MUJS_BUILDLINK3_MK
 
