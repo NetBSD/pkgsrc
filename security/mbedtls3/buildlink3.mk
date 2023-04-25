d01d63d91c23c9111ccb7bb8ffa118fe52e7046b
wiz <wiz@pkgsrc.org>
2023-04-25 21:42:17 +0000

    mbedtls3: remove zlib option from bl3.mk as well

--- security/mbedtls3/buildlink3.mk
+++ security/mbedtls3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/04/25 21:39:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/04/25 21:42:17 wiz Exp $
 
 BUILDLINK_TREE+=	mbedtls3
 
@@ -8,12 +8,6 @@ MBEDTLS3_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mbedtls3+=	mbedtls3>=3.4.0
 BUILDLINK_PKGSRCDIR.mbedtls3?=		../../security/mbedtls3
 
-pkgbase := mbedtls3
-.include "../../mk/pkg-build-options.mk"
-
-.if ${PKG_BUILD_OPTIONS.mbedtls3:Mzlib}
-.include "../../devel/zlib/buildlink3.mk"
-.endif
 .endif	# MBEDTLS3_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mbedtls3
