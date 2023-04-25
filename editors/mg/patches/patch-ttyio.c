aacbe633d8f0213004268ac6b677fb0f66b9e755
nikita <nikita@pkgsrc.org>
2023-04-25 21:53:01 +0000

    mg: update to version 20230406
    upstream URL changed.

--- editors/mg/patches/patch-ttyio.c
+++ editors/mg/patches/patch-ttyio.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-ttyio.c,v 1.1 2014/07/07 18:06:58 jperkin Exp $
+$NetBSD: patch-ttyio.c,v 1.2 2023/04/25 21:53:01 nikita Exp $
 
 SunOS needs sys/filio.h for FIONREAD.
 
---- ttyio.c.orig	2008-12-11 08:59:08.000000000 +0000
-+++ ttyio.c
+--- ttyio.c.orig	2023-04-25 22:27:51.465437426 +0200
++++ ttyio.c	2023-04-25 22:28:25.938802102 +0200
 @@ -14,6 +14,9 @@
- #include <sys/types.h>
+ #include <sys/queue.h>
  #include <sys/time.h>
- #include <sys/ioctl.h>
+ #include <sys/types.h>
 +#ifdef __sun
 +#include <sys/filio.h>
 +#endif
+ #include <errno.h>
  #include <fcntl.h>
- #include <termios.h>
- #include <term.h>
+ #include <poll.h>
