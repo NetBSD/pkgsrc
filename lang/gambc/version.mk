236149efde8221572f6ec3337674ad2895289c11
nikita <nikita@pkgsrc.org>
2023-04-26 20:53:23 +0000

    gambc: update to version 4.9.4
    No Changelog to be found anywhere.
    Upstream homepage location changed.

--- lang/gambc/version.mk
+++ lang/gambc/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.4 2012/04/16 10:19:34 asau Exp $
+# $NetBSD: version.mk,v 1.5 2023/04/26 20:53:23 nikita Exp $
 ###
 .if !defined(GAMBC_VERSION_MK)
 GAMBC_VERSION_MK=	# empty
 ###
 ### The package version sets the distribution version.
 ###
-GAMBC_VERSION=		4.6.5
+GAMBC_VERSION=		4.9.4
 GAMBC_DIST_VERSION=	${GAMBC_VERSION:C|^.*|v&|:S|.|_|g}
 .endif
