3a3f006ae6a3a97c983801192be081479320ca13
nikita <nikita@pkgsrc.org>
2023-04-26 20:53:23 +0000

    gambc: update to version 4.9.4
    No Changelog to be found anywhere.
    Upstream homepage location changed.

--- lang/gambc/buildlink3.mk
+++ lang/gambc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:49 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/04/26 20:53:23 nikita Exp $
 
 BUILDLINK_TREE+=	gambc
 
 .if !defined(GAMBC_BUILDLINK3_MK)
 GAMBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gambc+=	gambc>=4.2.6
+BUILDLINK_API_DEPENDS.gambc+=	gambc>=4.9.4
 BUILDLINK_PKGSRCDIR.gambc?=	../../lang/gambc
 
 .include "../../mk/dlopen.buildlink3.mk"
