178c7b1bc24fb94b7f6fce94f08457eb316e4e82
adam <adam@pkgsrc.org>
2023-04-26 12:38:26 +0000

    py-pygit2: updated to 1.12.0
    1.12.0 (2023-04-01)
    - Upgrade to libgit2 v1.6.3
    - Update Linux wheels to bundle OpenSSL 3.0.8
    - Downgrade Linux wheels to manylinux2014
    - New ``ConflictCollection.__contains__``
    - New ``Repository.references.iterator(...)``
    - New ``favor``, ``flags`` and ``file_flags`` optional arguments for
      ``Repository.merge(...)``
    - New ``keep_all`` and ``paths`` optional arguments for ``Repository.stash(...)``
    - New ``Respository.state()``
    - Improve ``Repository.write_archive(...)`` performance
    - Sync type annotations

--- devel/py-pygit2/patches/patch-build.sh
+++ devel/py-pygit2/patches/patch-build.sh
@@ -1,14 +1,13 @@
-$NetBSD: patch-build.sh,v 1.1 2022/09/06 09:26:28 wiz Exp $
+$NetBSD: patch-build.sh,v 1.2 2023/04/26 12:38:26 adam Exp $
 
 Fix unportable test(1) operator.
 
---- build.sh.orig	2022-08-28 09:11:54.262747800 +0000
+--- build.sh.orig	2023-03-22 14:38:33.000000000 +0000
 +++ build.sh
-@@ -249,6 +249,6 @@ if [ "$1" = "stubtest" ]; then
+@@ -269,5 +269,5 @@ if [ "$1" = "stubtest" ]; then
      shift
      $PREFIX/bin/pip install mypy
      PYTHONPATH=. $PREFIX/bin/stubtest --mypy-config-file mypy-stubtest.ini pygit2._pygit2
 -    [ $? == 0 ] && echo "stubtest OK"
 +    [ $? = 0 ] && echo "stubtest OK"
  fi
- 
