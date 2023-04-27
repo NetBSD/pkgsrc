00d646bf0091ee9a753618bdd51f10889d074ce3
yhardy <yhardy@pkgsrc.org>
2023-04-27 13:16:35 +0000

    finance/ledger: update to ledger-3.3.2
    3.3.2
    
      * Fix divide by zero (bugs #777 and #2207)
    
      * Increase string size limit in src/unistring.h assert (bug #2174)
    
      * Require tzdata for Nix flake build (bug #2213)
    
    3.3.1
    
      * Fix regression leading to incorrect error about format directives (bug #
        2205)
    
      * Add information about compile features to --version
    
      * Fix compiler warnings by minimizing the use of deprecated APIs
    
      * Update flake.nix to match nixpkgs ledger/default.nix
    
      * Remove unused Python server related code
    
      * Various documentation improvements
    
    3.3.0
    
      * Use $PAGER when environment variable is set (bug #1674)
    
      * Make --depth correctly fold postings to accounts of greater depth into
        the parent at the specified level (bug #987)
    
      * When using wild-cards in the include directive, include matched files in
        sorted order (bug #1659)
    
      * Ensure absolute path for include (bug #2075)
    
      * Try to use $XDG_HOME_CONFIG/ledger/ledgerrc or ~/.config/ledger/ledgerrc
        first
    
      * Improve Python 3 support and drop support for Python 2
    
      * Add support for automatically reading files encrypted with GPG (bug #1949
        )
    
      * Add support for a "debit" column in the convert command (bug #1120)
    
      * Fix parsing of files without end of line (bug #516)
    
      * Fix incorrect parsing of expressions containing a - without spaces (bug #
        2001)
    
      * Fix payee metadata on postings not being validated and payee aliases not
        being honored (bug #556 & bug #1892)
    
      * Fix ledger interpreting a posting with 0 difference as a null-posting,
        which leads to it auto-balancing the posting (bug #1942)
    
      * Correctly escape all string values in lisp report (bug #2034)
    
      * Fix a regression where empty commodities were shown (bug #1969)
    
      * Fix a regression where using multiple commodities in one transaction
        triggers an assertion (bug #1998)
    
      * Fix --time-colon for negative time amounts
    
      * Use correct int return type for stream input operations (bug #2058)
    
      * Use amount_width for balance report
    
      * Remove some UTF-8 code that was having no effect (bug #2061)
    
      * Fix unrounding for equity
    
      * Fix SIGABRT when python subcommand raises an exception
    
      * Improve XML reports
    
      * Support building on older versions of CMAKE (less than 3.7)
    
      * Fix compilation with Boost 1.76 (bug #2030)
    
      * Fix Msys2 MinGW build (bug #1905)
    
      * Fix unicode problems on Windows (bug #1986)
    
      * Fix the issue that with Boost >= 1.77 include directive cannot find the
        file to include for stdin (-f -). Also for -f - when include cannot find
        the file it reports the error with full path now. (bug #2057 & bug #2092)
    
      * Fix Nix build
    
      * Rename quoted_rfc4180 to quoted_rfc, as numbers used in function names
        confuses the parser (#2007).
    
      * Numbers are no longer permitted in value expression function names.
    
      * Various documentation improvements

--- finance/ledger/patches/patch-src_main.cc
+++ finance/ledger/patches/patch-src_main.cc
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_main.cc,v 1.1 2019/02/17 11:55:03 wiz Exp $
+$NetBSD: patch-src_main.cc,v 1.2 2023/04/27 13:16:35 yhardy Exp $
 
 Canonicalize pkgsrc readline.h filename.  This is seen both by
 consumers of devel/readline and by consumers of devel/editline, via
 buildlink3 magic.  The only reason editline/readline.h may exist is to
 allow both devel/readline and devel/editline to coexist.
 
---- src/main.cc.orig	2019-02-05 13:30:45.000000000 +0000
+--- src/main.cc.orig	2023-03-03 10:49:39.000000000 +0000
 +++ src/main.cc
-@@ -36,7 +36,7 @@
- #include "session.h"
+@@ -40,7 +40,7 @@
+ #endif
  
  #ifdef HAVE_EDIT
 -#include <editline/readline.h>
