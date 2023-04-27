13cf7f534a10ed791ea4e00cabc065a0bc5e0678
wiz <wiz@pkgsrc.org>
2023-04-27 06:02:54 +0000

    py-yubikey-manager: update to 5.1.0
    * Version 5.1.0 (released 2023-04-17)
     ** Add OpenPGP functionality to supported API.
     ** Add PIV key info command to CLI.
     ** PIV: Support signing prehashed data via API.
     ** Bugfix: Fix signing PIV certificates/CSRs with key that always requires PIN.
     ** Bugfix: Fix incorrect display name detection for certain keys over NFC.

--- security/ykman/version.mk
+++ security/ykman/version.mk
@@ -1,6 +1,6 @@
-#	$NetBSD: version.mk,v 1.2 2023/03/12 14:05:22 wiz Exp $
+#	$NetBSD: version.mk,v 1.3 2023/04/27 06:02:54 wiz Exp $
 
 # used by security/py-yubikey-manager/Makefile
 # used by security/ykman/Makefile
 
-VERSION=	5.0.1
+VERSION=	5.1.0
