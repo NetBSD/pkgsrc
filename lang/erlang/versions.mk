29d2bde1910383a8706d0d6390097bef5d254522
wiz <wiz@pkgsrc.org>
2023-04-27 13:15:55 +0000

    erlang: update to 25.3.1.
    Support for fully asynchronous distributed signaling where send
    operations never block. This functionality is by default disabled
    and can be enabled per process. For more information see the
    documentation of process_flag(async_dist, Bool).

--- lang/erlang/versions.mk
+++ lang/erlang/versions.mk
@@ -1,3 +1,3 @@
-# $NetBSD: versions.mk,v 1.30 2022/12/27 16:47:52 schmonz Exp $
+# $NetBSD: versions.mk,v 1.31 2023/04/27 13:15:55 wiz Exp $
 # Do not edit, regenerate using 'make versions'
-_VERSIONS=	common_test 1.23.2 compiler 8.2.2 dialyzer 5.0.4 erts 13.1.3 ftp 1.1.2 inets 8.2 kernel 8.5.2 megaco 4.4.2 mnesia 4.21.3 observer 2.14 os_mon 2.8 public_key 1.13.2 snmp 5.13.2 ssh 4.15.1 ssl 10.8.6 stdlib 4.2 wx 2.2.1 asn1 5.0.21 crypto 5.1.2 debugger 5.3 diameter 2.2.7 edoc 1.2 eldap 1.2.10 erl_docgen 1.4 erl_interface 5.3 et 1.6.5 eunit 2.8.1 jinterface 1.13.1 odbc 2.14 parsetools 2.4.1 reltool 0.9 runtime_tools 1.19 sasl 4.2 syntax_tools 3.0 tftp 1.0.3 tools 3.5.3 xmerl 1.3.30
+_VERSIONS=	compiler 8.2.5 crypto 5.1.4 eldap 1.2.11 erl_interface 5.3.2 erts 13.2.1 inets 8.3.1 snmp 5.13.5 ssl 10.9.1 stdlib 4.3.1 wx 2.2.2 asn1 5.0.21 common_test 1.24 debugger 5.3.1 dialyzer 5.0.5 diameter 2.2.7 edoc 1.2 erl_docgen 1.4 et 1.6.5 eunit 2.8.2 ftp 1.1.4 jinterface 1.13.2 kernel 8.5.4 megaco 4.4.3 mnesia 4.21.4 observer 2.14 odbc 2.14 os_mon 2.8.1 parsetools 2.4.1 public_key 1.13.3 reltool 0.9.1 runtime_tools 1.19 sasl 4.2 ssh 4.15.3 syntax_tools 3.0.1 tftp 1.0.4 tools 3.5.3 xmerl 1.3.31
