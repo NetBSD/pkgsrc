69850e1e101a8010400de1ee8af150c21dcdf6d0
wiz <wiz@pkgsrc.org>
2023-04-27 08:19:58 +0000

    tinyxml2: bump depends for so -> a change

--- textproc/tinyxml2/buildlink3.mk
+++ textproc/tinyxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/04/27 08:19:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/04/27 08:19:58 wiz Exp $
 
 BUILDLINK_TREE+=	tinyxml2
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	tinyxml2
 TINYXML2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tinyxml2+=	tinyxml2>=2.1.0
+BUILDLINK_ABI_DEPENDS.tinyxml2+=	tinyxml2>=9.0.0
 BUILDLINK_PKGSRCDIR.tinyxml2?=		../../textproc/tinyxml2
 
 BUILDLINK_DEPMETHOD.tinyxml2?=		build
