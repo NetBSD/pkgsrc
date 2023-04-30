022083e8e58ae8c3eb323bf20a8ac9aa2200ed10
martin <martin@pkgsrc.org>
2023-04-30 22:09:38 +0000

    Fix compilation on architectures where gulong is not 64bit.

---
+++ sysutils/xfce4-systemload-plugin/patches/patch-panel-plugin_network.cc
@@ -0,0 +1,26 @@
+$NetBSD: patch-panel-plugin_network.cc,v 1.1 2023/04/30 22:09:38 martin Exp $
+
+Fix type error.
+
+--- ./panel-plugin/network.cc.orig	2022-02-16 19:57:05.000000000 +0100
++++ ./panel-plugin/network.cc	2023-05-01 00:01:34.366940861 +0200
+@@ -116,15 +116,17 @@ read_netload (gulong *net, gulong *NTota
+ {
+     static guint64 bytes[2];
+     static gint64 time[2];
++    gulong val;
+ 
+     *net = 0;
+     *NTotal = 0;
+ 
+     time[1] = g_get_monotonic_time ();
+ 
+-    if (read_netload_proc (&bytes[1]) != 0)
+-        if (read_netload_libgtop (&bytes[1]) != 0)
++    if (read_netload_proc (&val) != 0)
++        if (read_netload_libgtop (&val) != 0)
+             return -1;
++    bytes[1] = val;
+ 
+     if (time[0] != 0 && G_LIKELY (time[1] > time[0]) && G_LIKELY (bytes[1] >= bytes[0]))
+     {
