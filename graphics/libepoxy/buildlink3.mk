067cf5dcccb3a25889e1b360a8bb32cf4e691f0e
schmonz <schmonz@pkgsrc.org>
2023-04-30 17:44:57 +0000

    libepoxy: if we were built with 'x11', buildlink what we were buildlunk with. Fixes gtk3's configure not finding a suitable libepoxy (at least on macOS), then building a vendored copy instead and failing PLIST check.

--- graphics/libepoxy/buildlink3.mk
+++ graphics/libepoxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/09/13 16:01:04 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/04/30 17:44:57 schmonz Exp $
 
 BUILDLINK_TREE+=	libepoxy
 
@@ -7,6 +7,13 @@ LIBEPOXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libepoxy+=	libepoxy>=1.2
 BUILDLINK_PKGSRCDIR.libepoxy?=		../../graphics/libepoxy
+
+pkgbase:=		libepoxy
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.libepoxy:Mx11}
+.include "../../x11/libX11/buildlink3.mk"
+.endif
 
 .include "../../mk/bsd.fast.prefs.mk"
 
