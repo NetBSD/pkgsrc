bcace3ac711e62f352223e1baf807555151f2f91
nikita <nikita@pkgsrc.org>
2023-05-02 18:19:23 +0000

    compiler-rt, wasi-libcxx, wasi-compiler-rt, libunwind: restrict bl3 while 16 is in wip

--- lang/compiler-rt/buildlink3.mk
+++ lang/compiler-rt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 15:31:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/05/02 18:19:23 nikita Exp $
 
 BUILDLINK_TREE+=	compiler-rt
 
 .if !defined(COMPILER_RT_BUILDLINK3_MK)
 COMPILER_RT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.compiler-rt+=	compiler-rt>=9.0.1
+BUILDLINK_API_DEPENDS.compiler-rt+=	compiler-rt>=9.0.1<16
 BUILDLINK_PKGSRCDIR.compiler-rt?=	../../lang/compiler-rt
 
 .include "../../lang/llvm/buildlink3.mk"
