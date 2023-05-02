3dd8306dca0f8666ba0c4d0817e8fd3e2a5d5db1
nikita <nikita@pkgsrc.org>
2023-05-02 18:10:53 +0000

    lld, lldb, polly: restrict version in bl3 while we have 16 in wip

--- devel/lld/buildlink3.mk
+++ devel/lld/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 15:31:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/05/02 18:10:53 nikita Exp $
 
 BUILDLINK_TREE+=	lld
 
 .if !defined(LLD_BUILDLINK3_MK)
 LLD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lld+=	lld>=9.0.1
+BUILDLINK_API_DEPENDS.lld+=	lld>=9.0.1<16
 BUILDLINK_PKGSRCDIR.lld?=	../../devel/lld
 
 .include "../../lang/llvm/buildlink3.mk"
