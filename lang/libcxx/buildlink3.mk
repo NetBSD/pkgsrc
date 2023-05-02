b7653049cfbfa8a6749753f40c8f90290a0bed41
nikita <nikita@pkgsrc.org>
2023-05-02 18:07:46 +0000

    libcxx, libcxxabi: restrict version while we have 16 in wip

--- lang/libcxx/buildlink3.mk
+++ lang/libcxx/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2022/08/13 09:38:09 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/05/02 18:07:46 nikita Exp $
 
 BUILDLINK_TREE+=	libcxx
 
 .if !defined(LIBCXX_BUILDLINK3_MK)
 LIBCXX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcxx+=	libcxx>=14.0.0
+BUILDLINK_API_DEPENDS.libcxx+=	libcxx>=14.0.0<16
 BUILDLINK_PKGSRCDIR.libcxx?=	../../lang/libcxx
 
 .endif	# LIBCXX_BUILDLINK3_MK
--- lang/libcxxabi/buildlink3.mk
+++ lang/libcxxabi/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/08/12 08:41:58 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/05/02 18:07:46 nikita Exp $
 
 BUILDLINK_TREE+=	libcxxabi
 
 .if !defined(LIBCXXABI_BUILDLINK3_MK)
 LIBCXXABI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcxxabi+=	libcxxabi>=14.0.0
+BUILDLINK_API_DEPENDS.libcxxabi+=	libcxxabi>=14.0.0<16
 BUILDLINK_PKGSRCDIR.libcxxabi?=		../../lang/libcxxabi
 
 .include "../../lang/llvm/buildlink3.mk"
