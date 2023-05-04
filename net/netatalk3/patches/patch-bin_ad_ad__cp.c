0bc41c0082888954695d013def660939425ce0f8
hauke <hauke@pkgsrc.org>
2023-05-04 16:53:07 +0000

    Update net/netatalk3 to 3.1.15
    What's Changed
    
    FIX: CVE-2022-43634
    FIX: CVE-2022-45188
    NEW: Support for macOS hosts, Intel and Apple silicon, GitHub#281
    FIX: configure.ac: update deprecated autoconf syntax
    UPD: configure.ac: Support linking with system shared libraries
    Introduces the --with-talloc option
    FIX: macros: largefile-check macro for largefile (clang 16)
    UPD: macros: Update pthread macro to the latest from gnu.org
    FIX: initscripts: Modernize Systemd service file.
    FIX: libatalk/conf: include sys/file.h for LOCK_EX
    FIX: libatalk: Change log level for realpath() error, SF bug#666
    FIX: libatalk: Change log level for real_name error, SF bug#596
    FIX: libatalk: The my_bool type is deprecated as of MySQL 8.0.1, GitHub#129
    UPD: libatalk: allow afpd to read read-protected afp.conf, SF bug#546
    UPD: libatalk: Make the "valid users" option work in the Homes
    section, SF bug#449
    UPD: libatalk: Check that FPDisconnectOldSession is successful, SF bug#634
    UPD: libatalk: Bring iniparser library codebase in line with current version 4.1
    FIX: afpd: Provide MNTTYPE_NFS on OmniOS to make quota work, GitHub#117
    FIX: afpd: Avoid triggering realpath() lookups with empty path, GitHub#277
    FIX: spotlight: Spotlight searches can cause afpd to segfault, GitHub#56
    UPD: spotlight: add support for tracker3, SF patch#147
    FIX: macusers: Fix output for long usernames
    FIX: macusers: account for usernames with non-word characters
    FIX: macusers: Support NetBSD
    FIX: Fix all function declarations without a prototype
    FIX: Fix C99 compliance issues
    FIX: Fix gcc10 compiler warnings
    UPD: Remove acsiidocs sources and release notes script
    FIX: manpages: afp.conf: Parameters are not quoted, SF bug#617
    FIX: manpages: afp.conf: Document $u in home name, GitHub#123
    FIX: manpages: afp.conf: Document the usage of guest user, GitHub#298
    FIX: Document how the mysql cnid backend is configured, GitHub#69
    FIX: Fix user-visible typos in log output and man pages.
    FIX: Fix spelling, syntax, and dead URLs in html manual.
    NEW: Create README.md
    NEW: Set up GitHub workflow and static analysis with Sonarcloud

--- net/netatalk3/patches/patch-bin_ad_ad__cp.c
+++ net/netatalk3/patches/patch-bin_ad_ad__cp.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-bin_ad_ad__cp.c,v 1.2 2016/11/07 12:46:52 christos Exp $
+$NetBSD: patch-bin_ad_ad__cp.c,v 1.3 2023/05/04 16:53:07 hauke Exp $
 
 Treat NetBSD as FreeBSD for timeval to timespec conversion
 
@@ -8,8 +8,8 @@ Treat NetBSD as FreeBSD for timeval to timespec conversion
      islink = !fdval && S_ISLNK(fs->st_mode);
      mode = fs->st_mode & (S_ISUID | S_ISGID | S_ISVTX | S_IRWXU | S_IRWXG | S_IRWXO);
  
--#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined(__NetBSD__)
+-#if defined(__FreeBSD__) || defined(__APPLE__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__APPLE__)
      TIMESPEC_TO_TIMEVAL(&tv[0], &fs->st_atimespec);
      TIMESPEC_TO_TIMEVAL(&tv[1], &fs->st_mtimespec);
  #else
