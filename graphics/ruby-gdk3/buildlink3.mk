feebacd456e73bae8749ae99b48e4f4b1f3250fd
tsutsui <tsutsui@pkgsrc.org>
2023-05-04 21:55:45 +0000

    ruby-gnome: update to 4.1.4.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.4
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.3
    
    Ruby-GNOME 4.1.4: 2023-05-04
    
    This is a bug fix release for 4.1.3.
    
    Changes
    
    Ruby/GObjectIntrospection
    
      * Improvements
    
          + Hid internal INVOKERS constant to improve backward compatibility.
            GH-1551
            [Reported by Mamoru TASAKA]
    
    Thanks
    
      * Mamoru TASAKA
    
    
    Ruby-GNOME 4.1.3: 2023-04-28
    
    This is a bug fix release for 4.1.2.
    
    Changes
    
    Ruby/GObjectIntrospection
    
      * Improvements
    
          + Added support for methods and functions for enum and flags.
    
          + Improved internal GObjectIntrospection::Loader APIs but some
            libraries may use these APIs. So this may be a backward
            incompatible change.
    
      * Fixes
    
          + Fixed a bug for nullable argument support.
    
    Ruby/GTK4
    
      * Improvements
    
          + Added examples:
              o GH-1551
              o GH-1552
              o GH-1553
              o GH-1554
              o GH-1555
              o GH-1556
              o GH-1557
              o GH-1558
                [Patch by Toshio Sekiya]
    
    Thanks
    
      * Toshio Sekiya

--- graphics/ruby-gdk3/buildlink3.mk
+++ graphics/ruby-gdk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/01/29 21:14:45 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/05/04 21:55:46 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gdk3
 
 .if !defined(RUBY_GDK3_BUILDLINK3_MK)
 RUBY_GDK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.0.3
-BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.0.5nb2
+BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.1.4
+BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.1.4
 BUILDLINK_PKGSRCDIR.ruby-gdk3?=		../../graphics/ruby-gdk3
 
 .include "../../devel/ruby-pango/buildlink3.mk"
