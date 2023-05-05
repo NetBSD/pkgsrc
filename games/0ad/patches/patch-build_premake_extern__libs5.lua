65fea0928d518189416751123413ac056e8886dd
nikita <nikita@pkgsrc.org>
2023-05-05 14:55:18 +0000

    0ad: fix part of the rpath issues + use pkgconfig for miniupnpc

--- games/0ad/patches/patch-build_premake_extern__libs5.lua
+++ games/0ad/patches/patch-build_premake_extern__libs5.lua
@@ -1,13 +1,35 @@
-$NetBSD: patch-build_premake_extern__libs5.lua,v 1.1 2023/05/04 15:20:52 nikita Exp $
+$NetBSD: patch-build_premake_extern__libs5.lua,v 1.2 2023/05/05 14:55:18 nikita Exp $
 
---- build/premake/extern_libs5.lua.orig	2021-07-27 21:57:08.000000000 +0000
-+++ build/premake/extern_libs5.lua
-@@ -350,7 +350,7 @@ extern_lib_defs = {
+pkgsrc miniupnpc is new enough to have pkg-config files.
+get iconv from libc on FreeBSD and NetBSD.
+
+--- build/premake/extern_libs5.lua.orig	2022-08-21 14:45:49.000000000 +0200
++++ build/premake/extern_libs5.lua	2023-05-05 14:04:01.320479087 +0200
+@@ -372,7 +372,7 @@
  			elseif os.istarget("macosx") then
  				add_default_include_paths("iconv")
  				defines { "LIBICONV_STATIC" }
 -			elseif os.getversion().description == "FreeBSD" then
-+			elseif os.getversion().description == "NetBSD" then
++			elseif os.getversion().description == "NetBSD" or os.getversion().description == "FreeBSD" then
  				-- On FreeBSD you need this flag to tell it to use the BSD libc iconv
  				defines { "LIBICONV_PLUG" }
  			end
+@@ -525,14 +525,13 @@
+ 				add_default_links({
+ 					win_names  = { "miniupnpc" },
+ 				})
+-			elseif os.istarget("macosx") then
+-				pkgconfig.add_links("miniupnpc")
+ 			else
++				pkgconfig.add_links("miniupnpc")
+ 				-- Once miniupnpc v2.1 or better becomes near-universal (see above comment),
+ 				-- we can use pkg-config for Linux and BSD.
+-				add_default_links({
+-					unix_names = { "miniupnpc" },
+-				})
++				--add_default_links({
++				--	unix_names = { "miniupnpc" },
++				--})
+ 			end
+ 		end,
+ 	},
