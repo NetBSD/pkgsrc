09f871f281092f64e20ec5318dc91de74cf34bce
ryoon <ryoon@pkgsrc.org>
2023-05-06 18:57:10 +0000

    libopus: Update buildlink3.mk for 1.4 shared library bump

--- audio/libopus/buildlink3.mk
+++ audio/libopus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/01/02 14:43:21 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/05/06 18:57:10 ryoon Exp $
 
 BUILDLINK_TREE+=	libopus
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libopus
 LIBOPUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libopus+=	libopus>=1.1
+BUILDLINK_ABI_DEPENDS.libopus+=	libopus>=1.4
 BUILDLINK_PKGSRCDIR.libopus?=	../../audio/libopus
 .endif	# LIBOPUS_BUILDLINK3_MK
 
