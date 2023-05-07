1edf8c4dbba016224f69a39fd05c27c51200d5c5
wiz <wiz@pkgsrc.org>
2023-05-07 12:24:08 +0000

    libimagequant: update to 4.2.0.
    Re-written in rust.

--- graphics/libimagequant/buildlink3.mk
+++ graphics/libimagequant/buildlink3.mk
@@ -1,11 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/01/29 11:57:03 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/05/07 12:24:08 wiz Exp $
 
 BUILDLINK_TREE+=	libimagequant
 
 .if !defined(LIBIMAGEQUANT_BUILDLINK3_MK)
 LIBIMAGEQUANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libimagequant+=	libimagequant>=2.8.2
+BUILDLINK_DEPMETHOD.libimagequant?=	build
+
+BUILDLINK_API_DEPENDS.libimagequant+=	libimagequant>=4.2.0
 BUILDLINK_PKGSRCDIR.libimagequant?=	../../graphics/libimagequant
 .endif	# LIBIMAGEQUANT_BUILDLINK3_MK
 
