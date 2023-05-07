375fee064dc0bcba80a4409f370c179112a39181
wiz <wiz@pkgsrc.org>
2023-05-07 16:55:20 +0000

    libimagequant: downgrade to 2.18
    4.x only provides a static library but graphviz needs a shared gd library
    PR 57395 and https://github.com/ImageOptim/libimagequant/issues/109

--- graphics/libimagequant/buildlink3.mk
+++ graphics/libimagequant/buildlink3.mk
@@ -1,13 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/05/07 12:24:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/05/07 16:55:20 wiz Exp $
 
 BUILDLINK_TREE+=	libimagequant
 
 .if !defined(LIBIMAGEQUANT_BUILDLINK3_MK)
 LIBIMAGEQUANT_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.libimagequant?=	build
-
-BUILDLINK_API_DEPENDS.libimagequant+=	libimagequant>=4.2.0
+BUILDLINK_API_DEPENDS.libimagequant+=	libimagequant>=2.8.2<4
 BUILDLINK_PKGSRCDIR.libimagequant?=	../../graphics/libimagequant
 .endif	# LIBIMAGEQUANT_BUILDLINK3_MK
 
