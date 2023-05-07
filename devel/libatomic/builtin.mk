1d005eb5d24012fb1a9588a3406bd6685768df52
wiz <wiz@pkgsrc.org>
2023-05-07 10:08:48 +0000

    libatomic: update to 13.1.0.
    Use code from gcc 13.1.0.

--- devel/libatomic/builtin.mk
+++ devel/libatomic/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.1 2020/06/02 21:26:03 maya Exp $
+# $NetBSD: builtin.mk,v 1.2 2023/05/07 10:08:48 wiz Exp $
 
 BUILTIN_PKG:=		libatomic
 
@@ -8,7 +8,7 @@ BUILTIN_FIND_LIBS:=	atomic
 
 # Use builtin libatomic if the compiler isn't GCC
 # This package is broken with non-GCC compilers.
-.if !empty(BUILTIN_LIB_FOUND.atomic:M[yY][eE][sS]) && \
+.if ${BUILTIN_LIB_FOUND.atomic:U:tl} == yes && \
     empty(PKGSRC_COMPILER:Mgcc*)
 BUILTIN_LIBNAME.libatomic=	atomic
 USE_BUILTIN.libatomic=		yes
