ac925b5785504bbbc08dc941440c64aeada59cf4
wiz <wiz@pkgsrc.org>
2023-05-08 09:33:24 +0000

    *: recursive bump for poppler 23.05.0 shlib bump

--- print/poppler-data/buildlink3.mk
+++ print/poppler-data/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/05/20 11:55:56 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/05/08 09:33:25 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-data
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-data
 POPPLER_DATA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-data+=	poppler-data>=0.4.8
-BUILDLINK_ABI_DEPENDS.poppler-data?=	poppler-data>=0.4.10
+BUILDLINK_ABI_DEPENDS.poppler-data?=	poppler-data>=0.4.12
 BUILDLINK_PKGSRCDIR.poppler-data?=	../../print/poppler-data
 .endif	# POPPLER_DATA_BUILDLINK3_MK
 
--- print/poppler-qt6/buildlink3.mk
+++ print/poppler-qt6/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/04/19 08:08:40 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/05/08 09:33:25 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt6
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt6
 POPPLER_QT6_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt6+=	poppler-qt6>=22.01.0
-BUILDLINK_ABI_DEPENDS.poppler-qt6?=	poppler-qt6>=23.03.0nb2
+BUILDLINK_ABI_DEPENDS.poppler-qt6?=	poppler-qt6>=23.05.0
 BUILDLINK_PKGSRCDIR.poppler-qt6?=	../../print/poppler-qt6
 
 .include "../../print/poppler/buildlink3.mk"
