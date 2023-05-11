699211d6881b03ee6d73acbb379926f385b31df8
adam <adam@pkgsrc.org>
2023-05-11 09:53:06 +0000

    py-manticore: updated to 0.3.7
    0.3.7
    
    Ethereum
    
    Use crytic-compile 0.2.2
    Multi-transaction analysis now uses fixed attacker and owner contracts
    
    Native
    
    [Added API] Retrieve list of unimplemented syscalls
    Add FXSAVE/FXRSTOR concrete support on x86
    Add last_executed_pc property to CPU
    Support LD_LIBRARY_PATH
    Optional will/did_read_memory events
    Fixed base address handling on Linux
    Add ENDBR-style NOPs
    Support epoll-related syscalls
    
    Other
    
    [Added API] Add fork policy for providing explicit values
    Fixed Constant Folding
    Add simplifications for subtraction
    Parent state ID, last PC now available in state descriptors
    States now contain a reference to the current manticore instance
    fast_fail config to exit after first state exception
    Scaling bugfix for large solver queries
    Don't fork when only one solution is found for Concretize

--- devel/py-manticore/patches/patch-setup.py
+++ devel/py-manticore/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1 2020/09/29 03:27:32 khorben Exp $
+$NetBSD: patch-setup.py,v 1.2 2023/05/11 09:53:06 adam Exp $
 
-Use the ptable2 Python module to implement prettytable
+Allow newer crytic-compile.
 
---- setup.py.orig	2020-06-27 01:54:55.000000000 +0000
+--- setup.py.orig	2023-05-11 09:50:04.000000000 +0000
 +++ setup.py
-@@ -54,7 +54,7 @@ setup(
-     install_requires=[
-         "pyyaml",
-         "pysha3",
--        "prettytable",
-+        "PTable",
+@@ -71,7 +71,7 @@ setup(
          "ply",
          "rlp",
-         "crytic-compile>=0.1.1",
+         "intervaltree",
+-        "crytic-compile==0.2.2",
++        "crytic-compile>=0.2.2",
+         "wasm",
+         "dataclasses; python_version < '3.7'",
+         "pyevmasm>=0.2.3",
