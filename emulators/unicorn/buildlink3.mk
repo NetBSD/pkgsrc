8de3564ab056d2b2614c3d817ef8fd0bbbadef2c
adam <adam@pkgsrc.org>
2023-05-11 06:47:11 +0000

    unicorn py-unicorn: updated to 2.0.1.post1
    Version 2.0.1
    
    Features:
    
    - Support building & running on BE hosts.
    - Fix and support `clang-cl` on Windows.
    - Fix python `sdist` and add aarch64 Linux wheels. Note `pip` can build Unicorn2 on M1 now!
    - C# binding is refined and upgraded to .Net 6.
    
    Fix/Improvements:
    
    - Various bindings improvements.
    - Improvements for tests.
    - Fail explicitly when VEX.L is set.
    - Fix endianness when writing PPC32 CR register.
    - Fix a bug in `uc_ctl_set_cpu_model` check.
    - Fix Tricore PC not updating.
    - Fix the mapping not updated if users modify the mappings in the hooks.
    - Handle pathological cases consistently.
    - Fix memory leaks in PPC target.
    - Fix memory leaks in Tricore target.
    - Fix MSVC handling in cmake.
    - Fix PC sync-ing problems for `UC_HOOK_BLOCK` hooks.
    - Fix PC sync-ed twice when users request a soft restart.
    - Prevent overflow with pre-allocated RAM blocks.
    - Add FPCR and FPSR registers
    - Fix ARM CPU state not deep copied.
    - Fix PC not sync-ed for memory operation on aarch64.
    - Exit invalid store operations early to avoid the target registers being overwritten.
    - Improve the support for ARM BE32.
    
    
    Version 2.0.0
    
    Features:
    
    - TriCore Support
    
    Fixes/Improvements:
    
    - Build both shared library and static archive as unicorn1 does.
    - Misc bindings improvements.
    - Make sure setjmp-setjmp-wrapper-win32 participates in the build.
    - Improve Rust bindings build logic.
    - Fix wrong python binding for UC_CTL_TB_REMOVE_CACHE
    - Flush translation blocks when the count hook is removed.
    - Fix unicorn crash when nested `uc_emu_start` deletes a hook
    - Fix CPU not fully resumed when writing PC.
    - Don't quit TB if `uc_mem_protect` doesn't change the protection of current TB memory.
    - Add type annotations for python bindings.
    - Add CPUID hook for python bindings.
    - Don't repeat memory hooks if there is already an unhandled error.
    - Support reads and writes over all Arm SIMD registers
    - Fix wrong registers range in python bindings.
    - Fix uc_mem_protect on mmio regions
    - Fix a UAF caused by hook cache.
    - Fix the value collision between UC_MODE_ARMBE8 and UC_MODE_ARM926

--- emulators/unicorn/buildlink3.mk
+++ emulators/unicorn/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 19:24:34 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/05/11 06:47:11 adam Exp $
 
 BUILDLINK_TREE+=	unicorn
 
 .if !defined(UNICORN_BUILDLINK3_MK)
 UNICORN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.unicorn+=	unicorn>=1.0.1
+BUILDLINK_API_DEPENDS.unicorn+=	unicorn>=2.0.0
 BUILDLINK_PKGSRCDIR.unicorn?=	../../emulators/unicorn
 .endif	# UNICORN_BUILDLINK3_MK
 
