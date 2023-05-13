b0da8d64cbcbe9e45901c630dfeb889c5d7d5fde
tsutsui <tsutsui@pkgsrc.org>
2023-05-13 02:44:04 +0000

    ruby-gnome: update to 4.1.5.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.5
    
    Ruby-GNOME 4.1.5: 2023-05-11
    
    This is a bug fix release for 4.1.4.
    
    Changes
    
    Ruby/GLib2
    
      * Improvements
    
          + Ignored gmodule.h explicitly for MacPorts.
            [Reported by Akira Ouchi]
    
    Thanks
    
      * Akira Ouchi

--- devel/ruby-gio2/buildlink3.mk
+++ devel/ruby-gio2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/05/04 21:55:45 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/05/13 02:44:05 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gio2
 
 .if !defined(RUBY_GIO2_BUILDLINK3_MK)
 RUBY_GIO2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.1.4
-BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.1.4
+BUILDLINK_API_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.1.5
+BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.1.5
 BUILDLINK_PKGSRCDIR.ruby-gio2?=		../../devel/ruby-gio2
 
 .include "../../devel/ruby-gobject-introspection/buildlink3.mk"
--- x11/ruby-gtk3/buildlink3.mk
+++ x11/ruby-gtk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/05/04 21:55:47 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/05/13 02:44:05 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gtk3
 
 .if !defined(RUBY_GTK3_BUILDLINK3_MK)
 RUBY_GTK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.4
-BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.4
+BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.5
+BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.5
 BUILDLINK_PKGSRCDIR.ruby-gtk3?=		../../x11/ruby-gtk3
 
 .include "../../graphics/ruby-gdk3/buildlink3.mk"
