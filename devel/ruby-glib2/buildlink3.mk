c2e66708b9a78d9bb8f7d80f731def5adf4588ca
tsutsui <tsutsui@pkgsrc.org>
2023-05-13 02:44:04 +0000

    ruby-gnome: update to 4.1.5.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.5
    
    Ruby-GNOME 4.1.5: 2023-05-11
    
    This is a bug fix release for 4.1.4.
    
    Changes
    
    Ruby/GLib2
    
      * Improvements
    
          + Ignored gmodule.h explicitly for MacPorts.
            [Reported by Akira Ouchi]
    
    Thanks
    
      * Akira Ouchi

--- devel/ruby-glib2/buildlink3.mk
+++ devel/ruby-glib2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/05/04 21:55:45 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/05/13 02:44:04 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-glib2
 
 .if !defined(RUBY_GLIB2_BUILDLINK3_MK)
 RUBY_GLIB2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.4
-BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.4
+BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.5
+BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.5
 BUILDLINK_PKGSRCDIR.ruby-glib2?=	../../devel/ruby-glib2
 
 .include "../../devel/glib2/buildlink3.mk"
