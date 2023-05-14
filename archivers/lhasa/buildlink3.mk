f33e8db7ee8d8daac8571187afa734b7b96ec192
tsutsui <tsutsui@pkgsrc.org>
2023-05-14 16:11:22 +0000

    lhasa: update to 0.4.0.
    Upstream changes:
     https://github.com/fragglet/lhasa/blob/v0.4.0/NEWS
    
    v0.4.0 (2023-05-14):
    
         * The manpage was expanded, with more information about different
           compression formats, example commands, a BUGS section and some
           additional references in the SEE ALSO section.
         * A verbose mode was added to the list output that shows the full file
           timestamp (thanks Dan Fandrich).
         * Support for archives generated by the DOS LHARK tool was added; big
           thanks go to Jason Summers for his tool DEARK and his comprehensive
           writeup on the LHARK format.
         * Some changes were made for compatibility with old versions of
           autotools (thanks Ozkan Sezer).
         * Some tweaks were made to tests to make them work on BSD systems.
         * Segmentation fault on NetBSD/amd64 was fixed when decoding 8-bit
           character filenames (thanks Izumi Tsutsui).
         * Some minor tweaks and fixes were made to the help text and manpage
           (thanks Stefan)
         * Some spelling mistakes were fixed, excess whitespace was trimmed, and
           the website link in the RPC spec file was fixed (thanks a1346054 and
           Jakub Wilk).
         * A potential integer overflow in the lha_decode_*uint32 functions was
           fixed (thanks Jacub Wilk).

--- archivers/lhasa/buildlink3.mk
+++ archivers/lhasa/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/09/21 11:14:23 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/05/14 16:11:22 tsutsui Exp $
 
 BUILDLINK_TREE+=	lhasa
 
 .if !defined(LHASA_BUILDLINK3_MK)
 LHASA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lhasa+=	lhasa>=0.3.1
+BUILDLINK_API_DEPENDS.lhasa+=	lhasa>=0.4.0
 BUILDLINK_PKGSRCDIR.lhasa?=	../../archivers/lhasa
 .endif	# LHASA_BUILDLINK3_MK
 
