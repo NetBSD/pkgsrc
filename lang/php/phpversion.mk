ab2676cc11568342c4199e51a20c0ca525c5f636
taca <taca@pkgsrc.org>
2023-05-14 14:46:15 +0000

    lang/php82: update to 8.2.6
    11 May 2023, PHP 8.2.6
    
    - Core:
      . Fix inconsistent float negation in constant expressions. (ilutov)
      . Fixed bug GH-8841 (php-cli core dump calling a badly formed function).
        (nielsdos)
      . Fixed bug GH-10085 (Assertion when adding two arrays with += where the first
        array is contained in the second). (ilutov)
      . Fixed bug GH-10737 (PHP 8.1.16 segfaults on line 597 of
        sapi/apache2handler/sapi_apache2.c). (nielsdos, ElliotNB)
      . Fixed bug GH-11028 (Heap Buffer Overflow in zval_undefined_cv.). (nielsdos)
      . Fixed bug GH-11108 (Incorrect CG(memoize_mode) state after bailout in ??=).
        (ilutov)
    
    - Date:
      . Fixed bug where the diff() method would not return the right result around
        DST changeover for date/times associated with a timezone identifier. (Derick)
      . Fixed out-of-range bug when converting to/from around the LONG_MIN unix
        timestamp. (Derick)
    
    - DOM:
      . Fixed bug #80602 (Segfault when using DOMChildNode::before()).
        (Nathan Freeman)
      . Fixed incorrect error handling in dom_zvals_to_fragment(). (nielsdos)
    
    - Exif:
      . Fixed bug GH-9397 (exif read : warnings and errors : Potentially invalid
        endianess, Illegal IFD size and Undefined index). (nielsdos)
    
    - Intl:
      . Fixed bug GH-11071 (TZData version not displayed anymore). (Remi)
    
    - PCRE:
      . Fixed bug GH-10968 (Segfault in preg_replace_callback_array()). (ilutov)
    
    - Reflection:
      . Fixed bug GH-10983 (State-dependant segfault in
        ReflectionObject::getProperties). (nielsdos)
    
    - SPL:
      . Handle indirect zvals and use up-to-date properties in
        SplFixedArray::__serialize. (nielsdos)
    
    - Standard:
      . Fixed bug GH-10990 (mail() throws TypeError after iterating over
        $additional_headers array by reference). (nielsdos)
      . Fixed bug GH-9775 (Duplicates returned by array_unique when using enums).
        (ilutov)
    
    - Streams:
      . Fixed bug GH-10406 (feof() behavior change for UNIX based socket
        resources). (Jakub Zelenka)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.395 2023/04/15 02:19:13 taca Exp $
+# $NetBSD: phpversion.mk,v 1.396 2023/05/14 14:46:15 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -91,7 +91,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.28
 PHP81_VERSION=	8.1.18
-PHP82_VERSION=	8.2.5
+PHP82_VERSION=	8.2.6
 
 # Define API version or initial release of major version.
 PHP56_RELDATE=	20140828
