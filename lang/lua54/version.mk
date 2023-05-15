24d4dd260b35f5107a52ae564ffc5eac18badf1f
nikita <nikita@pkgsrc.org>
2023-05-15 08:17:11 +0000

    lua54: update to version 5.4.6
    Changelog:
    
    "Emergency" new version 5.4.6
    'lua_resetthread' is back to its original signature, to avoid
    incompatibilities in the ABI between releases of the same version.
    New function 'lua_closethread' added with the 'correct' signature.

--- lang/lua54/version.mk
+++ lang/lua54/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.5 2022/03/06 07:46:10 nia Exp $
+# $NetBSD: version.mk,v 1.6 2023/05/15 08:17:11 nikita Exp $
 
-LUA_VERSION=	5.4.4
+LUA_VERSION=	5.4.6
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
