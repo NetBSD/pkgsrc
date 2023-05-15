a1d0f38b8dec7187061a8f106d20df9f326d9a78
adam <adam@pkgsrc.org>
2023-05-15 20:57:36 +0000

    py-scikit-build: updated to 0.17.5
    Scikit-build 0.17.5
    ===================
    
    A small fix release fixing the passing on of generator specific arguments. This
    fixes some cases where the Ninja generator was found but then was unable to
    build. NetBSD was reported to work, so was added to the BSD's supported.
    
    Bug fixes
    ---------
    
    * Generator args were missing for actual compile
    * Add support for netbsd & pyodide (future)

--- devel/py-scikit-build/patches/patch-skbuild_platform__specifics_platform__factory.py
+++ devel/py-scikit-build/patches/patch-skbuild_platform__specifics_platform__factory.py
@@ -1,15 +1,14 @@
-$NetBSD: patch-skbuild_platform__specifics_platform__factory.py,v 1.2 2023/05/15 09:19:04 jperkin Exp $
+$NetBSD: patch-skbuild_platform__specifics_platform__factory.py,v 1.3 2023/05/15 20:57:36 adam Exp $
 
-Add NetBSD support
-https://github.com/scikit-build/scikit-build/issues/971
+Add SunOS support.
 
---- skbuild/platform_specifics/platform_factory.py.orig	2023-05-05 18:58:16.000000000 +0000
+--- skbuild/platform_specifics/platform_factory.py.orig	2020-02-02 00:00:00.000000000 +0000
 +++ skbuild/platform_specifics/platform_factory.py
-@@ -34,7 +34,7 @@ def get_platform() -> abstract.CMakePlat
+@@ -35,7 +35,7 @@ def get_platform() -> abstract.CMakePlat
  
          return osx.OSXPlatform()
  
--    if this_platform in {"freebsd", "os400", "openbsd"}:
+-    if this_platform in {"freebsd", "netbsd", "os400", "openbsd"}:
 +    if this_platform in {"freebsd", "netbsd", "os400", "openbsd", "sunos"}:
          from . import bsd
  
