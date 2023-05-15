fa79ebf7fcba98ccdf09818c135aa3e80f304595
nikita <nikita@pkgsrc.org>
2023-05-15 17:16:58 +0000

    mk: fix repro.mk, add initial SOURCE_DATE_EPOCH support.

--- mk/repro/repro.mk
+++ mk/repro/repro.mk
@@ -1,15 +1,20 @@
-# $NetBSD: repro.mk,v 1.1 2017/11/12 13:34:14 khorben Exp $
+# $NetBSD: repro.mk,v 1.2 2023/05/15 17:16:58 nikita Exp $
 #
 # Infrastructure support for PKGSRC_MKREPRO.
 #
 # Keywords: reproducible
 #
 
-.if ${_PKGSRC_MKREPRO} == "yes"
+.if ${PKGSRC_MKREPRO} == "yes"
 
 # force ar(1) to be deterministic
 TOOLS_CREATE+=	ar
 TOOLS_PATH.ar?=	${PKGSRCDIR}/mk/repro/ar
 TOOLS_ARGS.ar?=	D
+
+# force SOURCE_DATE_EPOCH
+# https://reproducible-builds.org/docs/source-date-epoch/
+# TODO: is this exported in all necessary envs?
+PKGSRC_MAKE_ENV+= SOURCE_DATE_EPOCH=1
 
 .endif
