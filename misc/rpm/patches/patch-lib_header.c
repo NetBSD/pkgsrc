f993ba0ff6a69ebbfe1d248d55c7505d0e02c5e2
jperkin <jperkin@pkgsrc.org>
2023-05-15 13:52:37 +0000

    rpm: SunOS build fixes.

--- misc/rpm/patches/patch-lib_header.c
+++ misc/rpm/patches/patch-lib_header.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-lib_header.c,v 1.3 2017/04/23 08:18:15 adam Exp $
+$NetBSD: patch-lib_header.c,v 1.4 2023/05/15 13:52:38 jperkin Exp $
 
 * Detect htonll in configure script.
 
---- lib/header.c.orig	2014-06-30 08:47:13.000000000 +0000
+--- lib/header.c.orig	2017-02-16 09:54:14.797025194 +0000
 +++ lib/header.c
 @@ -108,6 +108,7 @@ static const size_t headerMaxbytes = (32
  #define	ENTRY_IN_REGION(_e)	((_e)->info.offset < 0)
  
  /* Convert a 64bit value to network byte order. */
-+#if !defined(htonll)
++#if !defined(htonll) && !defined(__sun)
  RPM_GNUC_CONST
  static uint64_t htonll(uint64_t n)
  {
