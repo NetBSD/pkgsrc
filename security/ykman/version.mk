b7887699fe1876686a50897d9a0c2bf3f4c25543
wiz <wiz@pkgsrc.org>
2023-05-16 22:51:02 +0000

    ykman, py-yubikey-manager: update to 5.1.1
    * Version 5.1.1 (released 2023-04-27)
     ** Bugfix: PIV: string representation of SLOT caused infinite loop on Python <3.11.
     ** Bugfix: Fix errors in 'ykman config nfc' on YubiKeys without NFC capability.
     ** Bugfix: Fix error message shown when invalid modhex input length given for YubiOTP.

--- security/ykman/version.mk
+++ security/ykman/version.mk
@@ -1,6 +1,6 @@
-#	$NetBSD: version.mk,v 1.3 2023/04/27 06:02:54 wiz Exp $
+#	$NetBSD: version.mk,v 1.4 2023/05/16 22:51:02 wiz Exp $
 
 # used by security/py-yubikey-manager/Makefile
 # used by security/ykman/Makefile
 
-VERSION=	5.1.0
+VERSION=	5.1.1
