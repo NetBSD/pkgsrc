a39aeea4aaf9bb823e7bd66bc63adf16441c0e96
wiz <wiz@pkgsrc.org>
2023-05-16 08:56:02 +0000

    libheif: add upstream pull request

--- graphics/libheif/patches/patch-libheif_hevc.cc
+++ graphics/libheif/patches/patch-libheif_hevc.cc
@@ -1,6 +1,7 @@
-$NetBSD: patch-libheif_hevc.cc,v 1.1 2023/05/15 09:23:18 jperkin Exp $
+$NetBSD: patch-libheif_hevc.cc,v 1.2 2023/05/16 08:56:02 wiz Exp $
 
 Explicitly use std::pow.
+https://github.com/strukturag/libheif/pull/866
 
 --- libheif/hevc.cc.orig	2023-05-03 07:03:54.000000000 +0000
 +++ libheif/hevc.cc
