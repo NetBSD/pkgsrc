29d9fc204e5d64b0cbff9af3daae9283285ddbfe
wiz <wiz@pkgsrc.org>
2023-05-16 16:54:50 +0000

    fish: fix build on NetBSD-current
    fish uses symbols from libterminfo, so it needs to link against it.

---
+++ shells/fish/patches/patch-cmake_ConfigureChecks.cmake
@@ -0,0 +1,18 @@
+$NetBSD: patch-cmake_ConfigureChecks.cmake,v 1.3 2023/05/16 16:54:50 wiz Exp $
+
+On some systems, libterminfo has a longer name.
+
+--- cmake/ConfigureChecks.cmake.orig	2023-03-25 06:50:41.000000000 +0000
++++ cmake/ConfigureChecks.cmake
+@@ -80,6 +80,11 @@ find_library(CURSES_TINFO tinfo)
+ if (CURSES_TINFO)
+     set(CURSES_LIBRARY ${CURSES_LIBRARY} ${CURSES_TINFO})
+ endif()
++# on some systems, libterminfo has a longer name
++find_library(CURSES_TINFO terminfo)
++if (CURSES_TINFO)
++    set(CURSES_LIBRARY ${CURSES_LIBRARY} ${CURSES_TINFO})
++endif()
+ 
+ # Get threads.
+ set(THREADS_PREFER_PTHREAD_FLAG ON)
