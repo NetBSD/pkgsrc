b15a0932eccbc8faa8ea0ebcfc3851577dfed231
nikita <nikita@pkgsrc.org>
2023-05-17 14:58:30 +0000

    libhidapi: increase bl3 abi+api version

--- comms/libhidapi/buildlink3.mk
+++ comms/libhidapi/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/05/10 20:15:10 nikita Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/05/17 14:58:30 nikita Exp $
 
 BUILDLINK_TREE+=	libhidapi
 
 .if !defined(LIBHIDAPI_BUILDLINK3_MK)
 LIBHIDAPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libhidapi+=	libhidapi>=0.7.0
+BUILDLINK_API_DEPENDS.libhidapi+=	libhidapi>=0.13.1
+BUILDLINK_ABPI_DEPENDS.libhidapi+=	libhidapi>=0.13.1
 BUILDLINK_PKGSRCDIR.libhidapi?=	../../comms/libhidapi
 .include "../../devel/libusb1/buildlink3.mk"
 .if ${OPSYS} == "Linux"
