76d61dadd6b2b77fb4f9d5e244a33212ad274acb
wiz <wiz@pkgsrc.org>
2023-05-17 10:49:15 +0000

    exiv2: downgrade
    Too much breakage:
    https://github.com/Exiv2/exiv2/issues/2630

--- graphics/exiv2/buildlink3.mk
+++ graphics/exiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2023/05/16 20:28:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2023/05/17 10:49:15 wiz Exp $
 
 BUILDLINK_TREE+=	exiv2
 
@@ -6,13 +6,12 @@ BUILDLINK_TREE+=	exiv2
 EXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.exiv2+=	exiv2>=0.27
-BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.28.0
+BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.27
 BUILDLINK_PKGSRCDIR.exiv2?=	../../graphics/exiv2
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
-.include "../../textproc/inih/buildlink3.mk"
 .endif # EXIV2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-exiv2
