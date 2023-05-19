d39006606a1d20a53dd9c9e0cc2236a630b423c5
adam <adam@pkgsrc.org>
2023-05-19 10:34:23 +0000

    cmake cmake-gui: updated to 3.26.4
    CMake 3.26.4
    * FindPython: fix interpreter launcher variable spelling
    * Help: Remove duplicated word in COMPILE_OPTIONS target property docs
    * Ninja: Restore detection of msvc-wine showIncludes prefix
    * FindJNI: add Ubuntu specific paths for more recent JDK versions
    * Help: CMP0105 policy: clarifications
    * FindCUDAToolkit: nvptxcompiler_static correctly specify dependencies
    * FindCUDAToolkit: Add dependency between cusparse and nvJitLink
    * FindCUDAToolkit: Fix nvrtc_static dependencies on Windows
    * FindCUDAToolkit: Add missing static library dependencies on pthread and libdl
    * FindCUDAToolkit: Support CUDA version extraction from version.json

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.38 2023/04/06 11:01:18 adam Exp $
+# $NetBSD: version.mk,v 1.39 2023/05/19 10:34:23 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.26.3
+CMAKE_VERSION=	3.26.4
 CMAKE_API=	${CMAKE_VERSION:R}
