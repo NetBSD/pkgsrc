69f33fa1c7e0463d19130ec8aad4ee44beef90b4
wiz <wiz@pkgsrc.org>
2023-05-20 07:18:56 +0000

    p5-File-BaseDir: update to 0.09.
    0.09      2021-07-08 14:04:14 -0600
     - Tests can now be run in parallel or isolation safely (rt#199256)
     - Fallback on getpwuid if environment variable not set (rt#127138)
     - Test suite works with or without environment variables (rt#127138)

--- devel/p5-File-BaseDir/patches/patch-lib_File_BaseDir.pm
+++ devel/p5-File-BaseDir/patches/patch-lib_File_BaseDir.pm
@@ -1,14 +1,15 @@
-$NetBSD: patch-lib_File_BaseDir.pm,v 1.1 2013/02/16 22:14:41 schmonz Exp $
+$NetBSD: patch-lib_File_BaseDir.pm,v 1.2 2023/05/20 07:18:56 wiz Exp $
 
 Look in pkgsrc's sharedir, too.
 
---- lib/File/BaseDir.pm.orig	2007-10-24 19:57:22.000000000 +0000
+--- lib/File/BaseDir.pm.orig	2021-07-08 20:04:16.000000000 +0000
 +++ lib/File/BaseDir.pm
-@@ -41,6 +41,7 @@ unless ($home) {
- # Set defaults
- our $xdg_data_home = File::Spec->catdir($home, qw/.local share/);
- our @xdg_data_dirs = (
-+	File::Spec->catdir($rootdir, qw/@SHAREDIR_SPLIT@/),
- 	File::Spec->catdir($rootdir, qw/usr local share/),
- 	File::Spec->catdir($rootdir, qw/usr share/),
- );
+@@ -46,7 +46,7 @@ sub xdg_data_home { $ENV{XDG_DATA_HOME} 
+ sub xdg_data_dirs {
+   ( $ENV{XDG_DATA_DIRS}
+     ? _adapt($ENV{XDG_DATA_DIRS})
+-    : (File::Spec->catdir(_rootdir(), qw/usr local share/), File::Spec->catdir(_rootdir(), qw/usr share/))
++    : (File::Spec->catdir(_rootdir(), qw/@SHAREDIR_SPLIT@/), File::Spec->catdir(_rootdir(), qw/usr local share/), File::Spec->catdir(_rootdir(), qw/usr share/))
+   )
+ }
+ 
