77f00cd131d12e2209297674ef04b58915371942
wiz <wiz@pkgsrc.org>
2023-05-22 11:38:58 +0000

    *: recursive bump for libimagequant 4.x

--- graphics/gd/buildlink3.mk
+++ graphics/gd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.49 2023/01/29 21:14:39 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.50 2023/05/22 11:38:58 wiz Exp $
 
 BUILDLINK_TREE+=	gd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gd
 GD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gd+=	gd>=2.0.15nb1
-BUILDLINK_ABI_DEPENDS.gd+=	gd>=2.3.3nb6
+BUILDLINK_ABI_DEPENDS.gd+=	gd>=2.3.3nb9
 BUILDLINK_PKGSRCDIR.gd?=	../../graphics/gd
 
 .include "../../mk/bsd.fast.prefs.mk"
--- inputmethod/m17n-im-config/buildlink3.mk
+++ inputmethod/m17n-im-config/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2023/04/19 08:08:27 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2023/05/22 11:38:59 wiz Exp $
 
 BUILDLINK_TREE+=	m17n-im-config
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	m17n-im-config
 M17N_IM_CONFIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.m17n-im-config+=	m17n-im-config>=0.9.0
-BUILDLINK_ABI_DEPENDS.m17n-im-config+=	m17n-im-config>=0.9.0nb48
+BUILDLINK_ABI_DEPENDS.m17n-im-config+=	m17n-im-config>=0.9.0nb49
 BUILDLINK_PKGSRCDIR.m17n-im-config?=	../../inputmethod/m17n-im-config
 
 .include "../../devel/gettext-lib/buildlink3.mk"
