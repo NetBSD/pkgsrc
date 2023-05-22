208e45c4f6355e926f312b898ca2479f87408861
wiz <wiz@pkgsrc.org>
2023-05-22 20:28:28 +0000

    libproxy: update to 0.5.0.
    What's Changed
    
        Fix building without duktape by @mattst88 in #180
        Fix WITH_GNOME2 documentation by @mattst88 in #181
        bindings: perl: Use ccflags from %Config for libproxy module compilation by @Vogtinator in #183
        Use XDG_CURRENT_DESKTOP to detect GNOME desktop like systems by @janbrummer in #192
        Enable the use of ducktape in Windows systems by @illera88 in #194
        Enable static builds for libproxy by @illera88 in #195
        Fix build on macOS by @wegank in #189
        Complete rewrite by @janbrummer in #201
        kde: Remove line breaks in values by @janbrummer in #203
        Add man page for proxy tool by @janbrummer in #204
        Add PX_FORCE_CONFIG env by @janbrummer in #205
        add alert function to pacrunners by @multiSnow in #196
        Update architecture documentation by @janbrummer in #206
        Check for valid uri_host in ignore_ip() by @janbrummer in #210
        Remove line break in debug output by @janbrummer in #211
        Improve GNOME/KDE get_config() execution by @janbrummer in #212

--- www/libproxy/buildlink3.mk
+++ www/libproxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/06/08 11:27:05 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/05/22 20:28:28 wiz Exp $
 
 BUILDLINK_TREE+=	libproxy
 
@@ -8,6 +8,11 @@ LIBPROXY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libproxy+=	libproxy>=0.2.3
 BUILDLINK_ABI_DEPENDS.libproxy+=	libproxy>=0.4.11
 BUILDLINK_PKGSRCDIR.libproxy?=		../../www/libproxy
+
+# before version 0.5, the header file was directly in include/proxy.h
+BUILDLINK_FNAME_TRANSFORM.libproxy+=	-e s,include/libproxy,include,
+
+.include "../../devel/glib2/buildlink3.mk"
 .endif # LIBPROXY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libproxy
