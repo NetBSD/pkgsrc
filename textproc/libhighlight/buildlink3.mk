e840cfbcb1a3d59e361b6edbf437ed3990b244e5
schmonz <schmonz@pkgsrc.org>
2023-05-24 15:53:35 +0000

    Update to 4.6. From the changelog:
    - added `--service-mode` option to run highlight as long running
      process (#140)
    - added extras/highlight-service.py as an example for the service mode
    - updated astyle lib to version 3.3

--- textproc/libhighlight/buildlink3.mk
+++ textproc/libhighlight/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/07/13 00:52:30 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/05/24 15:53:35 schmonz Exp $
 
 BUILDLINK_TREE+=	libhighlight
 
 .if !defined(LIBHIGHLIGHT_BUILDLINK3_MK)
 LIBHIGHLIGHT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libhighlight+=	libhighlight>=4.2
+BUILDLINK_API_DEPENDS.libhighlight+=	libhighlight>=4.6
 BUILDLINK_PKGSRCDIR.libhighlight?=	../../textproc/libhighlight
 .endif	# LIBHIGHLIGHT_BUILDLINK3_MK
 
