9bd8b838bc211669235d22e8ffc6f163883c7f38
wiz <wiz@pkgsrc.org>
2023-05-24 17:14:52 +0000

    gd: make libimagequant optional
    Bump PKGREVISION.
    
    Based on a patch by osa@, thanks!

--- graphics/gd/options.mk
+++ graphics/gd/options.mk
@@ -1,10 +1,17 @@
-# $NetBSD: options.mk,v 1.8 2018/09/10 13:34:16 kim Exp $
+# $NetBSD: options.mk,v 1.9 2023/05/24 17:14:52 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gd
-PKG_SUPPORTED_OPTIONS=	tiff x11
-PKG_SUGGESTED_OPTIONS=	tiff
+PKG_SUPPORTED_OPTIONS=	libimagequant tiff x11
+PKG_SUGGESTED_OPTIONS=	libimagequant tiff
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlibimagequant)
+.include "../../graphics/libimagequant/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-liq=${BUILDLINK_PREFIX.libimagequant}
+.else
+CONFIGURE_ARGS+=	--without-liq
+.endif
 
 .if !empty(PKG_OPTIONS:Mtiff)
 .include "../../graphics/tiff/buildlink3.mk"
