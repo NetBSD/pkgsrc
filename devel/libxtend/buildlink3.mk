188760cd97c4da4e1ea3d72fee78f9d83b66100b
bacon <bacon@pkgsrc.org>
2023-05-24 15:32:52 +0000

    devel/libxtend: Update to 0.1.7.12
    Fix strptrcasecmp() to use strcasecmp()
    Add romantoi(), convert Roman numeral text to integer
    Add XT_SIGNUM() macro
    A few minor fixes

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/12/11 21:12:22 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/05/24 15:32:52 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.7
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.7.12
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
