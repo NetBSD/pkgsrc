f1e224e0535cd0238cde8811de885cfcb79936ef
schmonz <schmonz@pkgsrc.org>
2023-05-26 20:38:47 +0000

    Update to 1.0.2. From the changelog:
    1.0.2:
    
    Accept DOS CRLF newlines in all situations. Prior to this, CRLF newlines
    would cause problems in some contexts.
    
    Add support for pandoc title blocks as a metadata block syntax choice.
    Pandoc and MMD syntaxes are mutually exclusive, so only one may be used
    in any given document. The syntax is documented in lowdown(5).
    
    While there, support starting and ending an MMD metadata block with
    "---" on its own line, as in the MMD specification.
    
    Add the --html-titleblock option. If specified, output a Pandoc-style
    title header.
    
    Pandoc has a poorly-executed feature where -stman outputs have the title
    metadata line additionally parsed into a manpage title, section, and
    optional source and volume. This allows titles like `TITLE(1) Source |
    Volume` to be split into their respective manpage parts -- but only when
    in -stman mode, so an HTML document generated from the same page will
    not inherit the additional parsing. In lowdown(5), introduce this
    parsing for all outputs if --parse-mantitle is specified, which is now
    the default.
    
    Behaviour change: do not use "Untitled article" as the default title for
    media supporting (or requiring) titles when in standalone -s mode.
    Instead, just the empty string if a title is required. This is a more
    logical choice: not only is "Untitled article" language-specific, it
    also assumes the output is an article. For the previous behaviour,
    `--mtitle="Untitled article"` may always be used.
    
    Fix support for GFM tables. Specifically, don't arbitrarily require a
    certain three or more dashes/colons per cell in the layout row. Also
    allow for escaped vertical bars in the header row.
    
    1.0.1:
    
    Numerous build and configuration improvements (via oconfigure) for
    building into WASI targets and on Mac OS X.
    
    Add -L for listing all metadata keys.
    
    Some fixes to smart typography around hard linebreaks. These aren't
    likely to affect normal documents.
    
    Recognise void HTML elements as block elements.
    
    Switch between types (e.g., unordered to ordered) instead of continuing
    with the current list type. Fix parsing of block versus non-block lists
    when switching between list types. Fix parsing of block versus non-block
    lists when sublists contain block elements, but top-level lists don't.
    The Markdown spec isn't clear on how to accept these instances, so use
    pandoc as a guiding tool.
    
    For -tman output, normalise list marker widths (unordered or ordered)
    to three spaces by default, increasing for ordered lists depending on
    the number of entries. For -tms, it's five. Make other small cleanups
    to -tms, making sure that paragraph indentation looks the same as
    with -tlatex.
    
    Fix that escaped text (e.g., `\-\-foo`) is not changed by the smart
    typography formatter.
    
    Use `TP` in -tman for definition lists. To align with existing
    lists, use a subsequent paragraph offset of three. Similarly, use
    `XP` for -tms.
    
    pkgsrc changes:
    
    - Libtoolize for portable shared libraries.

--- textproc/lowdown/buildlink3.mk
+++ textproc/lowdown/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/02/23 08:20:29 fcambus Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/05/26 20:38:47 schmonz Exp $
 
 BUILDLINK_TREE+=	lowdown
 
 .if !defined(LOWDOWN_BUILDLINK3_MK)
 LOWDOWN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lowdown+=	lowdown>=0.11.0
+BUILDLINK_API_DEPENDS.lowdown+=	lowdown>=1.0.2
 BUILDLINK_PKGSRCDIR.lowdown?=	../../textproc/lowdown
 .endif	# LOWDOWN_BUILDLINK3_MK
 
