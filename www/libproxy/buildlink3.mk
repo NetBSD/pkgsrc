cb76eb787430b3ff17cd86c261257638eb87fa4a
wiz <wiz@pkgsrc.org>
2023-05-26 07:09:39 +0000

    libproxy: do not provide header file in old location
    instead adapt qt5-qtbase to look in the new location

--- www/libproxy/buildlink3.mk
+++ www/libproxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/05/22 21:00:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/05/26 07:09:39 wiz Exp $
 
 BUILDLINK_TREE+=	libproxy
 
@@ -9,8 +9,7 @@ BUILDLINK_API_DEPENDS.libproxy+=	libproxy>=0.2.3
 BUILDLINK_ABI_DEPENDS.libproxy+=	libproxy>=0.5.0
 BUILDLINK_PKGSRCDIR.libproxy?=		../../www/libproxy
 
-# before version 0.5, the header file was directly in include/proxy.h
-BUILDLINK_FNAME_TRANSFORM.libproxy+=	-e s,include/libproxy,include,
+BUILDLINK_INCDIRS.libproxy+=		include/libroxy
 
 .include "../../devel/glib2/buildlink3.mk"
 .endif # LIBPROXY_BUILDLINK3_MK
