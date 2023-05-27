fc13d9e18a03129992923543b943f679f5d3c5b8
charlotte <charlotte@pkgsrc.org>
2023-05-27 07:09:09 +0000

    divecmd: Fix bl3 path

--- misc/libdivecomputer/buildlink3.mk
+++ misc/libdivecomputer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/05/27 07:00:07 charlotte Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/05/27 07:09:09 charlotte Exp $
 
 BUILDLINK_TREE+=	libdivecomputer
 
@@ -7,7 +7,7 @@ LIBDIVECOMPUTER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdivecomputer+=	libdivecomputer>=0.8.0
 BUILDLINK_ABI_DEPENDS.libdivecomputer+=	libdivecomputer>=0.8.0
-BUILDLINK_PKGSRCDIR.libdivecomputer?=	../../wip/libdivecomputer
+BUILDLINK_PKGSRCDIR.libdivecomputer?=	../../misc/libdivecomputer
 
 .endif # LIBDIVECOMPUTER_BUILDLINK3_MK
 
