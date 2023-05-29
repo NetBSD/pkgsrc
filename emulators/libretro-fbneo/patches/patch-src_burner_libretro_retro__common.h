cba0d91781f87f6555db2d06075b6c07fc87e9c5
nia <nia@pkgsrc.org>
2023-05-29 14:33:14 +0000

    libretro-fbneo: Update to 20230524
    Lots of support for new exciting arcade systems, like Cave CV1000 and
    Sega System 32.

--- emulators/libretro-fbneo/patches/patch-src_burner_libretro_retro__common.h
+++ emulators/libretro-fbneo/patches/patch-src_burner_libretro_retro__common.h
@@ -1,15 +1,16 @@
-$NetBSD: patch-src_burner_libretro_retro__common.h,v 1.2 2020/09/10 10:10:18 nia Exp $
+$NetBSD: patch-src_burner_libretro_retro__common.h,v 1.3 2023/05/29 14:33:14 nia Exp $
 
 Don't cast lvalues.
 
---- src/burner/libretro/retro_common.h.orig	2020-05-30 15:55:11.000000000 +0000
+--- src/burner/libretro/retro_common.h.orig	2023-05-24 08:09:39.000000000 +0000
 +++ src/burner/libretro/retro_common.h
 @@ -7,7 +7,11 @@
  #include <vector>
  #include "burner.h"
  
+-#define SSTR( x ) static_cast< const std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
 +#ifndef __clang__
- #define SSTR( x ) static_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
++#define SSTR( x ) static_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
 +#else
 +#define SSTR( x ) ((( std::ostringstream() << std::dec << x ) ).str())
 +#endif
