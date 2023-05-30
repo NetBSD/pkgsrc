6d2b689de820dedf1a27fa470bec4419b5a6dedc
wiz <wiz@pkgsrc.org>
2023-05-30 08:06:02 +0000

    imath: really bump ABI depends

--- math/imath/buildlink3.mk
+++ math/imath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/05/07 12:33:43 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/05/30 08:06:02 wiz Exp $
 
 BUILDLINK_TREE+=	imath
 
@@ -9,7 +9,7 @@ IMATH_BUILDLINK3_MK:=
 GCC_REQD+=	6
 
 BUILDLINK_API_DEPENDS.imath+=	imath>=3.0.4
-BUILDLINK_ABI_DEPENDS.imath+=	imath>=3.1.7
+BUILDLINK_ABI_DEPENDS.imath+=	imath>=3.1.8
 BUILDLINK_PKGSRCDIR.imath?=	../../math/imath
 .endif	# IMATH_BUILDLINK3_MK
 
