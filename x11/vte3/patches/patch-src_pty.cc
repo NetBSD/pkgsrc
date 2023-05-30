4076837e356b10c607b2b09255546c2cf85fe183
gutteridge <gutteridge@pkgsrc.org>
2023-05-30 23:04:53 +0000

    vte3: improve patch to work around NetBSD posix_openpt(3) defect
    Now that as of roughly 9.99.101 we have a posix_openpt(3) that applies
    O_NONBLOCK and O_CLOEXEC, we don't need the fallback code. Now we
    dynamically detect whether we need the fallback using the best
    approximation we can (an assumption backed by a test case I recently
    added). Tested on 9.3_STABLE, 10.0_BETA, and 10.99.4.

--- x11/vte3/patches/patch-src_pty.cc
+++ x11/vte3/patches/patch-src_pty.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_pty.cc,v 1.9 2022/09/04 19:26:10 gutteridge Exp $
+$NetBSD: patch-src_pty.cc,v 1.10 2023/05/30 23:04:53 gutteridge Exp $
 
 Use correct includes on SunOS.
 Functional fix of posix_openpt() on NetBSD.
@@ -6,7 +6,7 @@ https://gitlab.gnome.org/GNOME/vte/-/issues/2575
 
 --- src/pty.cc.orig	2022-03-27 17:52:19.000000000 +0000
 +++ src/pty.cc
-@@ -62,7 +62,9 @@
+@@ -62,9 +62,15 @@
  #ifdef HAVE_PTY_H
  #include <pty.h>
  #endif
@@ -16,25 +16,46 @@ https://gitlab.gnome.org/GNOME/vte/-/issues/2575
 +#include <sys/ptyvar.h>
  #include <stropts.h>
  #endif
++#ifdef __NetBSD__
++#include <sys/param.h>
++#include <sys/sysctl.h>
++#endif
  #include <glib.h>
-@@ -426,7 +428,19 @@ _vte_pty_open_posix(void)
-         auto fd = vte::libc::FD{posix_openpt(O_RDWR | O_NOCTTY | O_NONBLOCK | O_CLOEXEC)};
+ #include <gio/gio.h>
+ #include "debug.h"
+@@ -427,6 +433,27 @@ _vte_pty_open_posix(void)
  #ifndef __linux__
          /* Other kernels may not support CLOEXEC or NONBLOCK above, so try to fall back */
--        bool need_cloexec = false, need_nonblocking = false;
+         bool need_cloexec = false, need_nonblocking = false;
++
 +#ifdef __NetBSD__
-+        /*
-+         * NetBSD's posix_openpt() will accept flags that don't actually get applied, so it
-+         * will return an FD with only O_RDWR actually set. Unlike other BSDs, it doesn't
-+         * return EINVAL when given inapplicable flags. We need to force the fallbacks below.
-+         */
-+        bool need_cloexec = true;
-+        bool need_nonblocking = true;
++         // NetBSD is a special case: prior to 9.99.101, posix_openpt() will not return
++         // EINVAL for unknown/unsupported flags but instead silently ignore these flags
++         // and just return a valid PTY but without the NONBLOCK | CLOEXEC flags set.
++         // So we need to manually apply these flags there. See issue #2575.
++        int mib[2], osrev;
++        size_t len;
++
++        mib[0] = CTL_KERN;
++        mib[1] = KERN_OSREV;
++        len = sizeof(osrev);
++        sysctl(mib, 2, &osrev, &len, NULL, 0);
++        if (osrev < 999010100) {
++                need_cloexec = need_nonblocking = true;
++                _vte_debug_print(VTE_DEBUG_PTY,
++                                 "NetBSD < 9.99.101, forcing fallback "
++                                 "for NONBLOCK and CLOEXEC.\n");
++        }
 +#else
-+        bool need_cloexec = false;
-+        bool need_nonblocking = false;
-+#endif /* __NetBSD__ */
 +
          if (!fd && errno == EINVAL) {
                  /* Try without NONBLOCK and apply the flag afterward */
                  need_nonblocking = true;
+@@ -437,6 +464,7 @@ _vte_pty_open_posix(void)
+                         fd = posix_openpt(O_RDWR | O_NOCTTY);
+                 }
+         }
++#endif /* __NetBSD__ */
+ #endif /* !linux */
+ 
+         if (!fd) {
