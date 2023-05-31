1745094d29bb984ba59d14aecf2ec36e4d01b669
nikita <nikita@pkgsrc.org>
2023-05-31 18:37:52 +0000

    import libcurl-gnutls as www/libcurl-gnutls version 8.1.1

---
+++ www/libcurl-gnutls/options.mk
@@ -0,0 +1,63 @@
+# $NetBSD: options.mk,v 1.1 2023/05/31 18:37:52 nikita Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.libcurl-gnutls
+PKG_SUPPORTED_OPTIONS=		inet6 libssh2 gssapi ldap rtmp idn http2
+PKG_SUGGESTED_OPTIONS=		http2 inet6 idn
+PKG_OPTIONS_LEGACY_OPTS=	libidn:idn
+
+# Kerberos is built in - no additional dependency
+PKG_SUGGESTED_OPTIONS.NetBSD+=	gssapi
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.else
+CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibssh2)
+CONFIGURE_ARGS+=	--with-libssh2=${BUILDLINK_PREFIX.libssh2}
+.  include "../../security/libssh2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-libssh2
+.endif
+
+.if !empty(PKG_OPTIONS:Mgssapi)
+.include "../../mk/krb5.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gssapi=${KRB5BASE}
+CONFIGURE_ARGS+=	--with-gssapi-includes=${KRB5BASE}/include/gssapi
+.else
+CONFIGURE_ARGS+=	--without-gssapi
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap-client/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-ldap
+CONFIGURE_ARGS+=	--enable-ldaps
+.else
+CONFIGURE_ARGS+=	--disable-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mrtmp)
+.include "../../net/rtmpdump/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-librtmp
+.else
+CONFIGURE_ARGS+=	--without-librtmp
+.endif
+
+.if !empty(PKG_OPTIONS:Midn)
+.include "../../devel/libidn2/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libidn2
+.else
+CONFIGURE_ARGS+=	--without-libidn2
+.endif
+
+.if !empty(PKG_OPTIONS:Mhttp2)
+USE_TOOLS+=		pkg-config
+CONFIGURE_ARGS+=	--with-nghttp2=${BUILDLINK_PREFIX.nghttp2}
+TEST_DEPENDS+=		nghttp2-tools-[0-9]*:../../www/nghttp2-tools
+.include "../../www/nghttp2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-nghttp2
+.endif
