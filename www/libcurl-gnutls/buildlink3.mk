8c4aada3ff8decf5fdd32d0b33f4dde1717d386f
nikita <nikita@pkgsrc.org>
2023-05-31 18:37:52 +0000

    import libcurl-gnutls as www/libcurl-gnutls version 8.1.1

---
+++ www/libcurl-gnutls/buildlink3.mk
@@ -0,0 +1,38 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/05/31 18:37:52 nikita Exp $
+
+BUILDLINK_TREE+=	libcurl-gnutls
+
+.if !defined(LIBCURL_GNUTLS_BUILDLINK3_MK)
+LIBCURL_GNUTLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libcurl-gnutls+=	libcurl-gnutls>=7.12.3
+BUILDLINK_ABI_DEPENDS.libcurl-gnutls+=	libcurl-gnutls>=8.0.1nb1
+BUILDLINK_PKGSRCDIR.libcurl-gnutls?=	../../www/libcurl-gnutls
+
+pkgbase:= libcurl-gnutls
+.include "../../mk/pkg-build-options.mk"
+.if ${PKG_BUILD_OPTIONS.libcurl-gnutls:Mlibssh2}
+.  include "../../security/libssh2/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libcurl-gnutls:Mgssapi}
+.  include "../../mk/krb5.buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libcurl-gnutls:Mldap}
+.  include "../../databases/openldap-client/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libcurl-gnutls:Mrtmp}
+.  include "../../net/rtmpdump/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libcurl-gnutls:Midn}
+.  include "../../devel/libidn2/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libcurl-gnutls:Mhttp2}
+.  include "../../www/nghttp2/buildlink3.mk"
+.endif
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.endif # LIBCURL_GNUTLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libcurl-gnutls
