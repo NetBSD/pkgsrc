55735f7ef6ee65bcd9b99b376b93006c01b37094
wiz <wiz@pkgsrc.org>
2023-06-02 15:30:09 +0000

    vim*: update to 9.0.1598
    Changelog here:
    http://ftp.vim.org/pub/vim/patches/9.0/
    too long to quote, no summary found.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.140 2022/11/28 10:00:33 morr Exp $
+# $NetBSD: version.mk,v 1.141 2023/06/02 15:30:10 wiz Exp $
 
 VIM_VERSION=		9.0
-VIM_PATCHLEVEL=		0960
+VIM_PATCHLEVEL=		1598
 # Changelog: see http://ftp.vim.org/pub/vim/patches/9.0/
 VIM_SUBDIR=		vim90
 
