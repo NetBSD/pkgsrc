0fa330fce4744cd1e49832819813bf3d3f7877e8
wiz <wiz@pkgsrc.org>
2023-06-02 16:44:06 +0000

    multimedia/ffmpeg6: import ffmpeg6-6.0
    FFmpeg is a complete, cross-platform solution to record, convert
    and stream audio and video.  It includes libavcodec - the leading
    audio/video codec library.
    
    This package contains major version 6.
    
    Changes compared to 5:
    
    A new major release, FFmpeg 6.0 "Von Neumann", is now available
    for download. This release has many new encoders and decoders,
    filters, ffmpeg CLI tool improvements, and also, changes the way
    releases are done. All major releases will now bump the version of
    the ABI. We plan to have a new major release each year. Another
    release-specific change is that deprecated APIs will be removed
    after 3 releases, upon the next major bump. This means that releases
    will be done more often and will be more organized.
    
    New decoders featured are Bonk, RKA, Radiance, SC-4, APAC, VQC,
    WavArc and a few ADPCM formats. QSV and NVenc now support AV1
    encoding. The FFmpeg CLI (we usually reffer to it as ffmpeg.c to
    avoid confusion) has speed-up improvements due to threading, as
    well as statistics options, and the ability to pass option values
    for filters from a file. There are quite a few new audio and video
    filters, such as adrc, showcwt, backgroundkey and ssim360, with a
    few hardware ones too. Finally, the release features many
    behind-the-scenes changes, including a new FFT and MDCT implementation
    used in codecs (expect a blog post about this soon), numerous
    bugfixes, better ICC profile handling and colorspace signalling
    improvement, introduction of a number of RISC-V vector and scalar
    assembly optimized routines, and a few new improved APIs, which
    can be viewed in the doc/APIchanges file in our tree. A few submitted
    features, such as the Vulkan improvements and more FFT optimizations
    will be in the next minor release, 6.1, which we plan to release
    soon, in line with our new release schedule. Some highlights are:
    
        Radiance HDR image support
        ddagrab (Desktop Duplication) video capture filter
        ffmpeg -shortest_buf_duration option
        ffmpeg now requires threading to be built
        ffmpeg now runs every muxer in a separate thread
        Add new mode to cropdetect filter to detect crop-area based on motion vectors and edges
        VAAPI decoding and encoding for 10/12bit 422, 10/12bit 444 HEVC and VP9
        WBMP (Wireless Application Protocol Bitmap) image format
        a3dscope filter
        bonk decoder and demuxer
        Micronas SC-4 audio decoder
        LAF demuxer
        APAC decoder and demuxer
        Media 100i decoders
        DTS to PTS reorder bsf
        ViewQuest VQC decoder
        backgroundkey filter
        nvenc AV1 encoding support
        MediaCodec decoder via NDKMediaCodec
        MediaCodec encoder
        oneVPL support for QSV
        QSV AV1 encoder
        QSV decoding and encoding for 10/12bit 422, 10/12bit 444 HEVC and VP9
        showcwt multimedia filter
        corr video filter
        adrc audio filter
        afdelaysrc audio filter
        WADY DPCM decoder and demuxer
        CBD2 DPCM decoder
        ssim360 video filter
        ffmpeg CLI new options: -stats_enc_pre[_fmt], -stats_enc_post[_fmt], -stats_mux_pre[_fmt]
        hstack_vaapi, vstack_vaapi and xstack_vaapi filters
        XMD ADPCM decoder and demuxer
        media100 to mjpegb bsf
        ffmpeg CLI new option: -fix_sub_duration_heartbeat
        WavArc decoder and demuxer
        CrystalHD decoders deprecated
        SDNS demuxer
        RKA decoder and demuxer
        filtergraph syntax in ffmpeg CLI now supports passing file contents as option values
        hstack_qsv, vstack_qsv and xstack_qsv filters

---
+++ multimedia/ffmpeg6/buildlink3.mk
@@ -0,0 +1,62 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/06/02 16:44:06 wiz Exp $
+
+BUILDLINK_TREE+=	ffmpeg6
+
+.if !defined(FFMPEG6_BUILDLINK3_MK)
+FFMPEG6_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ffmpeg6+=	ffmpeg6>=6.0
+BUILDLINK_PKGSRCDIR.ffmpeg6?=	../../multimedia/ffmpeg6
+
+pkgbase := ffmpeg6
+.include "../../mk/pkg-build-options.mk"
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mav1}
+.  include "../../multimedia/libaom/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mfreetype}
+.  include "../../graphics/freetype2/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mfontconfig}
+.  include "../../fonts/fontconfig/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mtheora}
+.  include "../../multimedia/libtheora/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mxvid}
+.  include "../../multimedia/xvidcore/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mx264}
+.  include "../../multimedia/x264/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mlibvpx}
+.  include "../../multimedia/libvpx/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mvaapi}
+.  include "../../multimedia/libva/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.ffmpeg6:Mvdpau}
+.  include "../../multimedia/libvdpau/buildlink3.mk"
+.endif
+
+BUILDLINK_INCDIRS.ffmpeg6+=		include/ffmpeg6
+BUILDLINK_LIBDIRS.ffmpeg6+=		lib/ffmpeg6
+BUILDLINK_FNAME_TRANSFORM.ffmpeg6+=	-e 's|lib/ffmpeg6/pkgconfig/|lib/pkgconfig/|'
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../archivers/xz/buildlink3.mk"
+.include "../../devel/libgetopt/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif # FFMPEG6_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ffmpeg6
