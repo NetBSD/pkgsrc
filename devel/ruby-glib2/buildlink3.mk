ee570fcdaf8b6d17e9a4ccab2982e5c540af8ef3
tsutsui <tsutsui@pkgsrc.org>
2023-06-02 16:43:28 +0000

    ruby-gnome: update to 4.1.7.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.7
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.6
    
    Ruby-GNOME 4.1.7: 2023-05-31
    
    This is a minor improvement release for 4.1.6.
    
    Changes
    
    Ruby/GLib2
    
      * Improvements
    
          + Added support for writer barrier.
    
    Ruby/CairoGObject
    
      * Improvements
    
          + Added support for specifying rcairo's source directory by
            RCAIRO_SOURCE_DIR.
    
    Ruby-GNOME 4.1.6: 2023-05-21
    
    This is a minor improvement release for 4.1.5.
    
    Changes
    
    Ruby/GLib2
    
      * Improvements
    
          + Avoided closure related TypeError in GC at exit.
            [Reported by Matijs van Zuijlen]
    
    Thanks
    
      * Matijs van Zuijlen

--- devel/ruby-glib2/buildlink3.mk
+++ devel/ruby-glib2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/05/13 02:44:04 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/06/02 16:43:28 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-glib2
 
 .if !defined(RUBY_GLIB2_BUILDLINK3_MK)
 RUBY_GLIB2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.5
-BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.5
+BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.7
+BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.7
 BUILDLINK_PKGSRCDIR.ruby-glib2?=	../../devel/ruby-glib2
 
 .include "../../devel/glib2/buildlink3.mk"
--- x11/ruby-gtk3/buildlink3.mk
+++ x11/ruby-gtk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/05/13 02:44:05 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/06/02 16:43:30 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gtk3
 
 .if !defined(RUBY_GTK3_BUILDLINK3_MK)
 RUBY_GTK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.5
-BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.5
+BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.7
+BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.7
 BUILDLINK_PKGSRCDIR.ruby-gtk3?=		../../x11/ruby-gtk3
 
 .include "../../graphics/ruby-gdk3/buildlink3.mk"
