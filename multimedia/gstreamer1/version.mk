9cf72ab1d75e198f90fa873b9619d96e7da9ebb3
wiz <wiz@pkgsrc.org>
2023-06-02 16:09:45 +0000

    gstreamer1*: update to 1.22.3
    Highlighted bugfixes:
    
        avdec: fix occasional video decoder deadlock on seeking with FFmpeg 6.0
        decodebin3: fix regression handling input streams without CAPS or TIME segment such as e.g. udpsrc or pushfilesrc
        bluez: a2dpsink: fix Bluetooth SIG Certification test failures
        osxvideosink: fix deadlock upon closing output window
        qtdemux: fix edit list handling regression and AV1 codec box parsing
        qtmux: fix extraction of CEA608 closed caption data from S334-1A packets
        rtspsrc: Fix handling of * control path
        splitmux: timestamp handling improvements
        v4l2videodec: Rework dynamic resolution change handling (needed for IMX6 mainline codec)
        videoflip: fix regression with automatically rotating video based on tags
        d3d11: many d3d11videosink and d3d11compositor fixes
        webrtc, rtp: numerous data race fixes and stability fixes
        cerbero: Add support for RHEL9 and Rocky Linux; build timecodestamper plugin with libltc support
        various bug fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,8 @@
-# $NetBSD: version.mk,v 1.4 2023/04/29 07:12:14 wiz Exp $
+# $NetBSD: version.mk,v 1.5 2023/06/02 16:09:47 wiz Exp $
 
-GST_VERSION=	1.22.2
+GST_VERSION=	1.22.3
+SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
+PLIST_SUBST+=	SHLIBVER=${SHLIBVER}
 
 MAINTAINER?=	pkgsrc-users@NetBSD.org
 HOMEPAGE?=	https://gstreamer.freedesktop.org/
