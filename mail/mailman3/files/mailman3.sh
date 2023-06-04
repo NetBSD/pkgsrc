15670524a17d08f80e9f4eb706711504001ba6aa
markd <markd@pkgsrc.org>
2023-06-04 03:27:43 +0000

    mailman3: add version 3.3.8
    Mailman is free software for managing electronic mail discussion and
    e-newsletter lists. Mailman is integrated with the web, making it easy
    for users to manage their accounts and for list owners to administer
    their lists. Mailman supports built-in archiving, automatic bounce
    processing, content filtering, digest delivery, spam filters, and
    more.

---
+++ mail/mailman3/files/mailman3.sh
@@ -0,0 +1,45 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: mailman3.sh,v 1.1 2023/06/04 03:27:43 markd Exp $
+#
+# PROVIDE: mailman3
+# REQUIRE: mail
+# KEYWORD: shutdown
+
+if [ -f /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
+
+name="mailman3"
+rcvar=$name
+command="@PREFIX@/bin/mailman"
+#command_args="-C ${mailman_config}"
+start_cmd="mailman_doit start"
+stop_cmd="mailman_doit stop"
+restart_cmd="mailman_doit restart"
+status_cmd="mailman_doit status"
+info_cmd="mailman_doit info"
+extra_commands=info
+start_precmd=mailman_prestart
+
+mailman_prestart() {
+	dir="@VARBASE@/run/mailman"
+	if @TEST@ ! -d $dir; then
+		@MKDIR@ -p $dir
+		@CHMOD@ 0755 $dir
+		@CHOWN@ @MAILMAN_USER@:@MAILMAN_GROUP@ $dir
+	fi
+}
+
+mailman_doit()
+{
+	LANG=en_US.UTF-8 @SU@ -m @MAILMAN_USER@ -c "${command} ${command_args} ${rc_arg}"
+}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	mailman_doit "$1"
+fi
