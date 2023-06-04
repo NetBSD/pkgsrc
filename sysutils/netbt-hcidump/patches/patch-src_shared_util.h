a28d5f9ea914dd2d1075fedc6231028c33f11e14
plunky <plunky@pkgsrc.org>
2023-06-04 09:06:16 +0000

    update to hcidump from BlueZ-5.66
    changes to the parser, with newer profiles catered for

--- sysutils/netbt-hcidump/patches/patch-src_shared_util.h
+++ sysutils/netbt-hcidump/patches/patch-src_shared_util.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_shared_util.h,v 1.1 2014/12/30 08:39:13 plunky Exp $
+$NetBSD: patch-src_shared_util.h,v 1.2 2023/06/04 09:06:16 plunky Exp $
 
 Linux vs BSD incompatibilities
 
---- src/shared/util.h.orig	2014-12-12 13:38:33.000000000 +0000
+--- src/shared/util.h.orig	2022-03-16 15:06:20.000000000 +0000
 +++ src/shared/util.h
-@@ -23,8 +23,7 @@
- 
+@@ -11,8 +11,7 @@
  #include <stdint.h>
  #include <stdlib.h>
+ #include <stdarg.h>
 -#include <alloca.h>
 -#include <byteswap.h>
-+#include <machine/bswap.h>
++#include <endian.h>
+ #include <string.h>
+ #include <sys/types.h>
  
- #if __BYTE_ORDER == __LITTLE_ENDIAN
- #define le16_to_cpu(val) (val)
--- sysutils/netbt-hcidump/patches/patch-tools_parser_parser.h
+++ sysutils/netbt-hcidump/patches/patch-tools_parser_parser.h
@@ -1,19 +1,19 @@
-$NetBSD: patch-tools_parser_parser.h,v 1.2 2015/03/19 19:16:31 joerg Exp $
+$NetBSD: patch-tools_parser_parser.h,v 1.3 2023/06/04 09:06:16 plunky Exp $
 
 the device ID is a string on netbt stack
 
---- tools/parser/parser.h.orig	2014-03-25 20:53:42.000000000 +0000
+--- tools/parser/parser.h.orig	2021-10-13 18:38:35.000000000 +0000
 +++ tools/parser/parser.h
-@@ -37,7 +37,7 @@ struct frame {
+@@ -24,7 +24,7 @@ struct frame {
  	uint32_t	data_len;
  	void		*ptr;
  	uint32_t	len;
 -	uint16_t	dev_id;
 +	char		*dev_id;
  	uint8_t		in;
- 	uint8_t		master;
+ 	uint8_t		central;
  	uint16_t	handle;
-@@ -135,9 +135,9 @@ static inline void p_indent(int level, s
+@@ -122,9 +122,9 @@ static inline void p_indent(int level, s
  				localtime_r(&t, &tm);
  				printf("%04d-%02d-%02d %02d:%02d:%02d.%06lu ",
  					tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
