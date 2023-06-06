49768b15d57aa1fc338fd1d23f4669c866f4a963
wiz <wiz@pkgsrc.org>
2023-06-06 08:02:07 +0000

    *: recursive bump for math/imath shlib version change

--- graphics/nvidia-texture-tools/buildlink3.mk
+++ graphics/nvidia-texture-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/05/28 17:04:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/06/06 08:02:12 wiz Exp $
 
 BUILDLINK_TREE+=	nvidia-texture-tools
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nvidia-texture-tools
 NVIDIA_TEXTURE_TOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nvidia-texture-tools+=	nvidia-texture-tools>=2.0.8
-BUILDLINK_ABI_DEPENDS.nvidia-texture-tools?=	nvidia-texture-tools>=2.1.0nb6
+BUILDLINK_ABI_DEPENDS.nvidia-texture-tools?=	nvidia-texture-tools>=2.1.0nb7
 BUILDLINK_PKGSRCDIR.nvidia-texture-tools?=	../../graphics/nvidia-texture-tools
 
 .include "../../graphics/openexr/buildlink3.mk"
