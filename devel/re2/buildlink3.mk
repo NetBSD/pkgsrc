b75d92f3ef5956fcb5fab96e53b7297c24f292df
wiz <wiz@pkgsrc.org>
2023-06-06 10:50:48 +0000

    re2: update to 20230602.
    Changes not found, but now depends on abseil.

--- devel/re2/buildlink3.mk
+++ devel/re2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/08 05:46:23 agc Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/06/06 10:50:48 wiz Exp $
 
 BUILDLINK_TREE+=	re2
 
@@ -6,7 +6,10 @@ BUILDLINK_TREE+=	re2
 RE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.re2+=	re2>=20160201
+BUILDLINK_ABI_DEPENDS.re2+=	re2>=20230602
 BUILDLINK_PKGSRCDIR.re2?=	../../devel/re2
+
+.include "../../devel/abseil/buildlink3.mk"
 .endif	# RE2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-re2
