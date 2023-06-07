b68b9d34dec309e0123a13ea418b523d0bd81c76
wiz <wiz@pkgsrc.org>
2023-06-07 15:36:40 +0000

    polkit: update to 121.
    ## polkit 121
    
    ### Highlights:
    - new versioning
    - duktape added as backend JS engine
    - autotools build system removed
    - new translations
    
    ### Changes since polkit 0.120:
    
    * Simon McVittie
        * meson and testsuite fixes
    * Bastien Nocera
        * add ability to create policyconfig-1.dtd file
    * Xi Ruoyao
        * port to newer mozjs-91, jsauthority tweaks
    * Wu Xiaotian and Gustavo Lima Chaves
        * Add duktape as javascript engine
    * Nathan Follens
        * Dutch translation
    * Daniel E
        * duktape fixup
    * Fabrice Fontaine
        * fix build without C++
    * Dan Nicholson
        * fixup in group permision checking (backend)
    * Phaedrus Leeds
        * typo fix
    * Adrian Vovk
        * add option (--keep-cwd) for pkexec
    * Matt Turner
        * Allow --version and --help even if not setuid root
    * Benedikt Ames
        * fixes in polkitagent
    * Vincent Mihalkovic
        * development support

--- security/polkit/patches/patch-src_polkitbackend_polkitbackendinteractiveauthority.c
+++ security/polkit/patches/patch-src_polkitbackend_polkitbackendinteractiveauthority.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_polkitbackend_polkitbackendinteractiveauthority.c,v 1.2 2018/04/29 05:14:36 wiz Exp $
+$NetBSD: patch-src_polkitbackend_polkitbackendinteractiveauthority.c,v 1.3 2023/06/07 15:36:40 wiz Exp $
 
 * for *BSD netgroup functions
 
---- src/polkitbackend/polkitbackendinteractiveauthority.c.orig	2018-04-03 18:16:04.000000000 +0000
+--- src/polkitbackend/polkitbackendinteractiveauthority.c.orig	2022-07-11 08:46:06.000000000 +0000
 +++ src/polkitbackend/polkitbackendinteractiveauthority.c
 @@ -26,8 +26,13 @@
  #ifdef HAVE_NETGROUP_H
@@ -18,12 +18,12 @@ $NetBSD: patch-src_polkitbackend_polkitbackendinteractiveauthority.c,v 1.2 2018/
  #include <string.h>
  #include <glib/gstdio.h>
  #include <locale.h>
-@@ -2228,7 +2233,7 @@ get_users_in_net_group (PolkitIdentity  
+@@ -2250,7 +2255,7 @@ get_users_in_net_group (PolkitIdentity  
    ret = NULL;
    name = polkit_unix_netgroup_get_name (POLKIT_UNIX_NETGROUP (group));
  
 -#ifdef HAVE_SETNETGRENT_RETURN
-+#if HAVE_SETNETGRENT_RETURN && !defined(__NetBSD__)
++#if defined(HAVE_SETNETGRENT_RETURN) && !defined(__NetBSD__)
    if (setnetgrent (name) == 0)
      {
        g_warning ("Error looking up net group with name %s: %s", name, g_strerror (errno));
