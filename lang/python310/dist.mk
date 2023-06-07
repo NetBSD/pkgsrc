b5f8c80ad1de6b448d8de8f9527da1678ff98f91
adam <adam@pkgsrc.org>
2023-06-07 13:26:54 +0000

    python310 py310-html-docs: updated to 3.10.12
    Python 3.10.12
    
    Security
    gh-103142: The version of OpenSSL used in our binary builds has been upgraded to 1.1.1u to address several CVEs.
    gh-99889: Fixed a security in flaw in uu.decode() that could allow for directory traversal based on the input if no out_file was specified.
    gh-104049: Do not expose the local on-disk location in directory indexes produced by http.client.SimpleHTTPRequestHandler.
    gh-102153: urllib.parse.urlsplit() now strips leading C0 control and space characters following the specification for URLs defined by WHATWG in response to CVE-2023-24329. Patch by Illia Volochii.
    
    Library
    gh-103935: Use io.open_code() for files to be executed instead of raw open()
    gh-102953: The extraction methods in tarfile, and shutil.unpack_archive(), have a new a filter argument that allows limiting tar features than may be surprising or dangerous, such as creating files outside the destination directory. See Extraction filters for details.
    
    Documentation
    gh-89412: Add missing documentation for the end_lineno and end_offset attributes of the traceback.TracebackException class.
    
    Build
    gh-103262: Fixes Windows installer build to work with latest compilers.

--- lang/python310/dist.mk
+++ lang/python310/dist.mk
@@ -1,8 +1,8 @@
-# $NetBSD: dist.mk,v 1.12 2023/04/06 11:16:52 adam Exp $
+# $NetBSD: dist.mk,v 1.13 2023/06/07 13:26:54 adam Exp $
 
-PY_DISTVERSION=	3.10.11
+PY_DISTVERSION=	3.10.12
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python310/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python310/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
+MASTER_SITES=	https://www.python.org/ftp/python/${PY_DISTVERSION}/
