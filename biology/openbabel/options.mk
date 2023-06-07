5f695bc2611a68000562f2af829ec1793d28ed53
wiz <wiz@pkgsrc.org>
2023-06-07 09:25:22 +0000

    openbabel: switch to wxGTK32
    This only affects the default-off GUI option.

--- biology/openbabel/options.mk
+++ biology/openbabel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2020/06/29 12:10:34 ryoon Exp $
+# $NetBSD: options.mk,v 1.3 2023/06/07 09:25:22 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openbabel
 PKG_SUPPORTED_OPTIONS=	wxwidgets
@@ -9,7 +9,7 @@ PLIST_VARS+=		wx-gui
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mwxwidgets)
-.  include "../../x11/wxGTK28/buildlink3.mk"
+.  include "../../x11/wxGTK32/buildlink3.mk"
 CMAKE_ARGS+=	-DBUILD_GUI=ON
 PLIST.wx-gui=	yes
 .else
