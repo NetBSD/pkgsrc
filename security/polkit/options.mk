4f362478db6fbead4542167e4e1fdaf686c993b8
wiz <wiz@pkgsrc.org>
2023-06-07 15:36:40 +0000

    polkit: update to 121.
    ## polkit 121
    
    ### Highlights:
    - new versioning
    - duktape added as backend JS engine
    - autotools build system removed
    - new translations
    
    ### Changes since polkit 0.120:
    
    * Simon McVittie
        * meson and testsuite fixes
    * Bastien Nocera
        * add ability to create policyconfig-1.dtd file
    * Xi Ruoyao
        * port to newer mozjs-91, jsauthority tweaks
    * Wu Xiaotian and Gustavo Lima Chaves
        * Add duktape as javascript engine
    * Nathan Follens
        * Dutch translation
    * Daniel E
        * duktape fixup
    * Fabrice Fontaine
        * fix build without C++
    * Dan Nicholson
        * fixup in group permision checking (backend)
    * Phaedrus Leeds
        * typo fix
    * Adrian Vovk
        * add option (--keep-cwd) for pkexec
    * Matt Turner
        * Allow --version and --help even if not setuid root
    * Benedikt Ames
        * fixes in polkitagent
    * Vincent Mihalkovic
        * development support

--- security/polkit/options.mk
+++ security/polkit/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2022/06/07 18:03:55 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2023/06/07 15:36:40 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.polkit
 PKG_SUPPORTED_OPTIONS=	pam
@@ -10,16 +10,14 @@ PLIST_VARS+=	pam
 
 .if !empty(PKG_OPTIONS:Mpam)
 .include "../../mk/pam.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-authfw=pam
-CONFIGURE_ARGS+=	--with-pam-module-dir=${PREFIX}/lib/security
 PLIST.pam=	yes
-.  if ${OPSYS} == "NetBSD"
-CONFIGURE_ARGS+=	--with-pam-include=system
-.  endif
 MAKE_DIRS+=	/etc/pam.d
 CONF_FILES+=	${PREFIX}/share/examples/pam.d/polkit-1 \
 		/etc/pam.d/polkit-1
-.else
-CONFIGURE_ARGS+=	--with-authfw=shadow
-CONFIGURE_ARGS+=	--disable-pam-module
+INSTALLATION_DIRS+=	share/examples/pam.d
+post-install: pam-move
+.PHONY: pam-move
+pam-move:
+	${MV} ${DESTDIR}${PREFIX}/etc/pam.d/polkit-1 \
+		${DESTDIR}${PREFIX}/share/examples/pam.d/polkit-1
 .endif
