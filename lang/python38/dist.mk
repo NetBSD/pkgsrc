961a83aae434c5540c17bfe1d116f4195e31be3b
adam <adam@pkgsrc.org>
2023-06-07 13:25:08 +0000

    python38 py38-html-docs: updated to 3.8.17
    Python 3.8.17
    
    Security
    gh-103142: The version of OpenSSL used in our binary builds has been upgraded to 1.1.1u to address several CVEs.
    gh-99889: Fixed a security in flaw in uu.decode() that could allow for directory traversal based on the input if no out_file was specified.
    gh-104049: Do not expose the local on-disk location in directory indexes produced by http.client.SimpleHTTPRequestHandler.
    gh-102153: urllib.parse.urlsplit() now strips leading C0 control and space characters following the specification for URLs defined by WHATWG in response to CVE-2023-24329. Patch by Illia Volochii.
    gh-101727: Updated the OpenSSL version used in Windows and macOS binary release builds to 1.1.1t to address CVE-2023-0286, CVE-2022-4303, and CVE-2022-4303 per the OpenSSL 2023-02-07 security advisory.
    gh-101283: subprocess.Popen now uses a safer approach to find cmd.exe when launching with shell=True. Patch by Eryk Sun, based on a patch by Oleg Iarygin.
    
    Library
    gh-103935: Use io.open_code() for files to be executed instead of raw open()
    gh-102953: The extraction methods in tarfile, and shutil.unpack_archive(), have a new a filter argument that allows limiting tar features than may be surprising or dangerous, such as creating files outside the destination directory. See Extraction filters for details.
    gh-101997: Upgrade pip wheel bundled with ensurepip (pip 23.0.1)
    
    Build
    gh-102306: [3.8] Avoid GHA CI macOS test_posix failure by using the appropriate macOS SDK.
    
    Windows
    gh-100180: Update Windows installer to OpenSSL 1.1.1s
    
    macOS
    gh-103142: Update macOS installer to use OpenSSL 1.1.1u.

--- lang/python38/dist.mk
+++ lang/python38/dist.mk
@@ -1,8 +1,8 @@
-# $NetBSD: dist.mk,v 1.17 2022/12/07 11:52:00 adam Exp $
+# $NetBSD: dist.mk,v 1.18 2023/06/07 13:25:08 adam Exp $
 
-PY_DISTVERSION=	3.8.16
+PY_DISTVERSION=	3.8.17
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python38/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python38/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
+MASTER_SITES=	https://www.python.org/ftp/python/${PY_DISTVERSION}/
