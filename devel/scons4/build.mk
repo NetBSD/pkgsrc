f505eb9a2fa155aab081067ef8892efdd3626a40
wiz <wiz@pkgsrc.org>
2023-06-08 05:28:43 +0000

    scons4: carry over rpath hack from devel/scons
    Improve build.mk, add test target.

--- devel/scons4/build.mk
+++ devel/scons4/build.mk
@@ -1,4 +1,4 @@
-# $NetBSD: build.mk,v 1.1 2023/06/07 18:25:03 wiz Exp $
+# $NetBSD: build.mk,v 1.2 2023/06/08 05:28:43 wiz Exp $
 #
 # This Makefile fragment supports building using the SCons build tool.
 #
@@ -6,6 +6,9 @@
 #
 # SCONS_BUILD_ARGS
 #	Arguments to pass to SCons during build. Default: empty
+#
+# SCONS_TEST_ARGS
+#	Arguments to pass to SCons during testing. Default: empty
 #
 # SCONS_INSTALL_ARGS
 #	Arguments to pass to SCons during installation: Default: empty
@@ -20,28 +23,37 @@ TOOL_DEPENDS+=	${PYPKGPREFIX}-scons>=4:../../devel/scons4
 SCONSBIN=	scons-${PYVERSSUFFIX}
 
 _SCONS_BUILD_ARGS+=	PREFIX=${PREFIX}
+_SCONS_BUILD_ARGS+=	-j${MAKE_JOBS:U1}
 _SCONS_BUILD_ARGS+=	${SCONS_BUILD_ARGS}
 _SCONS_INSTALL_ARGS+=	${_SCONS_BUILD_ARGS}
 _SCONS_INSTALL_ARGS+=	${SCONS_INSTALL_ARGS}
+_SCONS_TEST_ARGS+=	${_SCONS_BUILD_ARGS}
+_SCONS_TEST_ARGS+=	${SCONS_TEST_ARGS}
 
-.PHONY: scons-build scons-install
+.PHONY: scons-build scons-test scons-install
 
 do-build: scons-build
 scons-build:
 	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${SCONSBIN} ${_SCONS_BUILD_ARGS}
 
+do-test: scons-test
+scons-test:
+	cd ${WRKSRC} && ${SETENV} ${TEST_ENV} ${SCONSBIN} ${_SCONS_TEST_ARGS} check
+
 do-install: scons-install
 scons-install:
-	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${SCONSBIN} ${_SCONS_INSTALL_ARGS} install
+	cd ${WRKSRC} && ${SETENV} ${INSTALL_ENV} ${SCONSBIN} ${_SCONS_INSTALL_ARGS} install
 
 _VARGROUPS+=		scons
 _PKG_VARS.scons+=	PYPKGPREFIX
 _PKG_VARS.scons+=	SCONS_BUILD_ARGS
+_PKG_VARS.scons+=	SCONS_TEST_ARGS
 _PKG_VARS.scons+=	SCONS_INSTALL_ARGS
 _PKG_VARS.scons+=	SCONSBIN
 _PKG_VARS.scons+=	_SCONS_BUILD_ARGS
+_PKG_VARS.scons+=	_SCONS_TEST_ARGS
 _PKG_VARS.scons+=	_SCONS_INSTALL_ARGS
-_IGN_VARS.scons+=	MAKE_ENV PREFIX WRKSRC PYVERSSUFFIX PYTHON_FOR_BUILD_ONLY
+_IGN_VARS.scons+=	MAKE_ENV MAKE_JOBS PREFIX WRKSRC PYVERSSUFFIX PYTHON_FOR_BUILD_ONLY
 _IGN_VARS.scons+=	SETENV TOOL_DEPENDS
 #_LISTED_VARS.scons+=	*_ARGS
 #_SORTED_VARS.scons+=	*_ENV
