d5394f21e4bb9b792f10fa5c0210eca96d912f6d
micha <micha@pkgsrc.org>
2023-06-09 09:34:16 +0000

    games/woof: Modify patches to apply cleanly

--- games/woof/patches/patch-src_d__iwad.c
+++ games/woof/patches/patch-src_d__iwad.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_d__iwad.c,v 1.1 2022/06/29 11:31:54 micha Exp $
+$NetBSD: patch-src_d__iwad.c,v 1.2 2023/06/09 09:34:16 micha Exp $
 
 Add pkgsrc ${PREFIX}/share/doom to WAD file search path.
 Patch is not suitable for upstream.
 
 --- src/d_iwad.c.orig	2020-04-14 06:39:44.000000000 +0000
 +++ src/d_iwad.c
-@@ -463,6 +463,9 @@ static void AddXdgDirs(void)
+@@ -482,6 +482,9 @@ static void AddXdgDirs(void)
          env = "/usr/local/share:/usr/share";
      }
  
