789e4b6eaf95d6151da233547e67cc2a27c7c5eb
adam <adam@pkgsrc.org>
2023-06-10 18:23:03 +0000

    qt5*: updated to 5.15.10
    Compared to Qt 5.15.9, Qt 5.15.10 contains about 110 bug fixes and security updates. The list of fixes and the overview of all important changes in Qt 5.15.10 can be found in the release note.

--- x11/qt5-qtwebengine/patches/patch-src_3rdparty_gn_tools_gn_args.cc
+++ x11/qt5-qtwebengine/patches/patch-src_3rdparty_gn_tools_gn_args.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_3rdparty_gn_tools_gn_args.cc,v 1.2 2022/04/18 11:18:19 adam Exp $
+$NetBSD: patch-src_3rdparty_gn_tools_gn_args.cc,v 1.3 2023/06/10 18:23:07 adam Exp $
 
---- src/3rdparty/gn/tools/gn/args.cc.orig
+--- src/3rdparty/gn/tools/gn/args.cc.orig	2022-04-06 13:32:17.000000000 +0000
 +++ src/3rdparty/gn/tools/gn/args.cc
 @@ -314,6 +314,8 @@ void Args::SetSystemVarsLocked(Scope* de
    os = "aix";
@@ -11,12 +11,3 @@ $NetBSD: patch-src_3rdparty_gn_tools_gn_args.cc,v 1.2 2022/04/18 11:18:19 adam E
  #else
  #error Unknown OS type.
  #endif
-@@ -338,6 +340,8 @@ void Args::SetSystemVarsLocked(Scope* de
-     arch = kX86;
-   else if (os_arch == "x86_64")
-     arch = kX64;
-+  else if (os_arch == "arm64")
-+    arch = kArm64;
-   else if (os_arch.substr(0, 3) == "arm")
-     arch = kArm;
-   else if (os_arch == "aarch64")
