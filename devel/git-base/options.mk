878cc7098b268bb077c329b196b53ce301124511
schmonz <schmonz@pkgsrc.org>
2023-06-12 21:41:59 +0000

    git-base: fix build on Solaris 11.4. Quell pkglint while here.

--- devel/git-base/options.mk
+++ devel/git-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2018/04/11 19:39:34 adam Exp $
+# $NetBSD: options.mk,v 1.6 2023/06/12 21:41:59 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.git
 PKG_SUPPORTED_OPTIONS=	apple-common-crypto
@@ -7,7 +7,7 @@ PKG_SUGGESTED_OPTIONS=
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mapple-common-crypto)
-.  if !empty(MACHINE_PLATFORM:MDarwin-[0-8].*-*)
+.  if ${MACHINE_PLATFORM:MDarwin-[0-8].*-*}
 PKG_FAIL_REASON+=	"apple-common-crypto not available on this system"
 .  endif
 CONFIGURE_ARGS+=	--without-openssl
