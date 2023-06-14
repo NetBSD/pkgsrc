1b92b9b636b1a6cf35db488011e0b4a3aead49d1
ryoon <ryoon@pkgsrc.org>
2023-06-14 16:22:18 +0000

    firefox: Update to 114.0.1
    * mprotect support for firefox and firefox-bin is insufficient now.
    
    Changelog:
    114.0.1
    
    Fixed
    
      * Fix a startup crash (bug 1837201).
    
    114.0
    
    New
    
      * Added UI to manage the DNS over HTTPS exception list.
    
      * Bookmarks can now be searched from the Bookmarks menu. The Bookmarks menu
        is accessible by adding the Bookmarks menu button to the toolbar.
    
      * Restrict searches to your local browsing history by selecting Search
        history from the History, Library or Application menu buttons.
    
      * Mac users can now capture video from their cameras in all supported native
        resolutions. This enables resolutions higher than 1280x720.
    
      * It is now possible to reorder the extensions listed in the extensions
        panel.
    
      * Users on macOS, Linux, and Windows 7 can now use FIDO2 / WebAuthn
        authenticators over USB. Some advanced features, such as fully passwordless
        logins, require a PIN to be set on the authenticator.
    
      * Pocket Recommended content can now be seen in France, Italy, and Spain.
    
    Fixed
    
      * Various security fixes.
    
    Changed
    
      * DNS over HTTPS settings are now part of the Privacy & Security section of
        the Settings page and allow the user to choose from all the supported
        modes.
    
    Enterprise
    
      * You can find information about policy updates and enterprise specific bug
        fixes in the Firefox for Enterprise 114 Release Notes.
    
    Developer
    
      * Developer Information
      * The Copy as cURL feature, available in the Network panel, has been
        enhanced. It now supports the --compressed argument.
    
      * The Accessibility Inspector has been improved to accurately recognize all
        the ARIA roles like banner, main, navigation, and contentinfo, etc. This
        enhancement is particularly beneficial for web developers working with ARIA
        roles to improve web accessibility.
    
      * Firefox now provides support for the CSS Cascading Level 4 supports()
        syntax for @import rules. This allows for the importation of other
        stylesheets based on support-dependency. In addition, the Inspector panel
        now accurately displays the conditions at the top of the imported rule.
    
        developer tools screenshot of the new @import syntax rule
    
    Web Platform
    
      * DOM: Added support for ES Modules on DedicatedWorker and SharedWorker
    
      * WebTransport is now enabled by default and will be going to release with
        114. As the original Explainer notes, it enables multiple use-cases that
        are hard or impossible to handle without it, especially for Gaming and live
        streaming. It covers cases that are problematic for alternative mechanisms,
        such as WebSockets.
    
        Built on top of HTTP3 (HTTP2 support will be coming later). The current
        implementation in Firefox is passing 505 out of 565 Web-Platform Tests.
    
      * CSS: The infinity and NaN constants are now supported inside the calc()
        function.
    
    Security fixes
    #CVE-2023-34414: Click-jacking certificate exceptions through rendering lag
    #CVE-2023-34415: Site-isolation bypass on sites that allow open redirects to
     data: urls
    #CVE-2023-34416: Memory safety bugs fixed in Firefox 114 and Firefox ESR 102.12
    #CVE-2023-34417: Memory safety bugs fixed in Firefox 114

--- www/firefox/patches/patch-gfx_skia_skia_src_core_SkVM.cpp
+++ www/firefox/patches/patch-gfx_skia_skia_src_core_SkVM.cpp
@@ -1,21 +1,21 @@
-$NetBSD: patch-gfx_skia_skia_src_core_SkVM.cpp,v 1.4 2023/01/24 17:57:09 nia Exp $
+$NetBSD: patch-gfx_skia_skia_src_core_SkVM.cpp,v 1.5 2023/06/14 16:22:18 ryoon Exp $
 
 Work with PaX MPROTECT on NetBSD by stating that we will later make this memory
 block executable.
 
 https://bugzilla.mozilla.org/show_bug.cgi?id=1811913
 
---- gfx/skia/skia/src/core/SkVM.cpp.orig	2022-11-28 16:47:38.000000000 +0000
+--- gfx/skia/skia/src/core/SkVM.cpp.orig	2023-05-25 21:19:29.000000000 +0000
 +++ gfx/skia/skia/src/core/SkVM.cpp
-@@ -2076,7 +2076,11 @@ namespace skvm {
-         // Allocate space that we can remap as executable.
-         const size_t page = sysconf(_SC_PAGESIZE);
-         fJITSize = ((a.size() + page - 1) / page) * page;  // mprotect works at page granularity.
+@@ -57,7 +57,11 @@ bool gSkVMJITViaDylib{false};
+             // mprotect doesn't round up for you, and instead requires *len is at page granularity.
+             const size_t page = sysconf(_SC_PAGESIZE);
+             *len = ((*len + page - 1) / page) * page;
 +#ifdef PROT_MPROTECT
-+        fJITBuf = mmap(nullptr,fJITSize, PROT_READ|PROT_WRITE|PROT_MPROTECT(PROT_EXEC), MAP_ANONYMOUS|MAP_PRIVATE, -1,0);
++            return mmap(nullptr,*len, PROT_READ|PROT_WRITE|PROT_MPROTECT(PROT_EXEC), MAP_ANONYMOUS|MAP_PRIVATE, -1,0);
 +#else
-         fJITBuf = mmap(nullptr,fJITSize, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1,0);
+             return mmap(nullptr,*len, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1,0);
 +#endif
- 
-         // Assemble the program for real.
-         a = Assembler{fJITBuf};
+         }
+         static void remap_as_executable(void* ptr, size_t len) {
+             mprotect(ptr, len, PROT_READ|PROT_EXEC);
