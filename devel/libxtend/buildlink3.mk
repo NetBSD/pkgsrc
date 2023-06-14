041aacd612d272c81ca6deb5677e807c94871c5f
bacon <bacon@pkgsrc.org>
2023-06-14 18:39:00 +0000

    devel/libxtend: Update to 0.1.8
    Several new functions, macros, and minor enhancements
    Changes: https://github.com/outpaddling/libxtend/releases

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/05/24 15:32:52 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/06/14 18:39:00 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.7.12
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.8
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
