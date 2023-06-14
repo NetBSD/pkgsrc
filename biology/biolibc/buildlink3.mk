9aff29e23836991a99e4f21ebab4ed78fb92ef2e
bacon <bacon@pkgsrc.org>
2023-06-14 18:41:24 +0000

    biology/biolibc: Update to 0.2.4.3
    Add extern "C" to support use from C++ apps

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/12/11 21:14:37 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/06/14 18:41:24 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.4
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.4.3
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
