b736a09b72fe14aa1292e5ada03ae14e3d755a15
bacon <bacon@pkgsrc.org>
2023-06-17 14:57:40 +0000

    devel/libxtend: Update to 0.1.8.2
    xt_fopen(): Add file existence and creation checks
    popen() does not flag nonexistent and uncreatable files

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/06/14 18:39:00 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/06/17 14:57:40 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.8
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.8.2
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
