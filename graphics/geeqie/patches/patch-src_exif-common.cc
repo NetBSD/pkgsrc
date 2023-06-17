3a2b543d1f6b9ebe0b64b062a9e4abe67256d047
wiz <wiz@pkgsrc.org>
2023-06-17 16:45:43 +0000

    geeqie: update to 2.1.
    Geeqie 2.1
    ============
    
    - Additional --selection remote commands
    - Split view additional 3-image layout
    - Many bug fixes
    - Some progress in GTK4 migration
    - Change source code from c to c++
    - Much progress in making source code warning-free
    - Updated language support

---
+++ graphics/geeqie/patches/patch-src_exif-common.cc
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_exif-common.cc,v 1.1 2023/06/17 16:45:43 wiz Exp $
+
+../src/exif-common.cc:780:3: error: 'setenv' was not declared in this scope; did you mean 'getenv'?                                                                                                                
+
+--- src/exif-common.cc.orig	2023-06-17 12:12:30.000000000 +0000
++++ src/exif-common.cc
+@@ -19,6 +19,8 @@
+ 
+ #include <config.h>
+ 
++#include <stdlib.h>
++
+ #define _XOPEN_SOURCE
+ 
+ #include <string.h>
