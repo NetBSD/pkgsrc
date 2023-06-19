8128ee7f186f7da7b542f3822edf5e91a95bb94f
bsiegert <bsiegert@pkgsrc.org>
2023-06-19 14:46:47 +0000

    rust: fix build on Darwin if Homebrew is installed
    The openssl-sys crate has a misguided piece of logic to prefer any
    Homebrew tree to the usual place for finding OpenSSL. Remove this code
    so that Rust actually uses pkgsrc OpenSSL as it should.
    
    In particular, if you have Apple Game Porting toolkit installed, you get
    a Homebrew installation for x86_64 on an ARM machine, which can never work.
    
    OK during freeze adam@, wiz@

---
+++ lang/rust/patches/patch-vendor_openssl-sys_build_find__normal.rs
@@ -0,0 +1,28 @@
+$NetBSD: patch-vendor_openssl-sys_build_find__normal.rs,v 1.1 2023/06/19 14:46:47 bsiegert Exp $
+
+--- vendor/openssl-sys/build/find_normal.rs.orig	2023-05-31 21:44:49.000000000 +0000
++++ vendor/openssl-sys/build/find_normal.rs
+@@ -72,23 +72,6 @@ fn resolve_with_wellknown_location(dir:
+ fn find_openssl_dir(target: &str) -> OsString {
+     let host = env::var("HOST").unwrap();
+ 
+-    if host == target && target.ends_with("-apple-darwin") {
+-        let homebrew_dir = match target {
+-            "aarch64-apple-darwin" => "/opt/homebrew",
+-            _ => "/usr/local",
+-        };
+-
+-        if let Some(dir) = resolve_with_wellknown_homebrew_location(homebrew_dir) {
+-            return dir.into();
+-        } else if let Some(dir) = resolve_with_wellknown_location("/opt/pkg") {
+-            // pkgsrc
+-            return dir.into();
+-        } else if let Some(dir) = resolve_with_wellknown_location("/opt/local") {
+-            // MacPorts
+-            return dir.into();
+-        }
+-    }
+-
+     try_pkg_config();
+     try_vcpkg();
+ 
