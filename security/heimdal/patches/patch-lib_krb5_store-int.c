74cad9b9ba9d4c887aa89b3ebc21baab2d6f98bb
bsiegert <bsiegert@pkgsrc.org>
2023-06-20 17:57:33 +0000

    Pullup ticket #6762 - requested by riastradh security/heimdal: security fix
    Revisions pulled up:
    - security/heimdal/Makefile                                     1.160
    - security/heimdal/distinfo                                     1.57
    - security/heimdal/patches/patch-lib_krb5_store-int.c           1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   riastradh
       Date:           Mon Jun 19 19:13:03 UTC 2023
    
       Modified Files:
               pkgsrc/security/heimdal: Makefile distinfo
       Added Files:
               pkgsrc/security/heimdal/patches: patch-lib_krb5_store-int.c
    
       Log Message:
       security/heimdal: Patch CVE-2022-42898 away.

---
+++ security/heimdal/patches/patch-lib_krb5_store-int.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-lib_krb5_store-int.c,v 1.1.2.2 2023/06/20 17:57:33 bsiegert Exp $
+
+Fix CVE-2022-42898 for 32-bit systems.
+https://github.com/heimdal/heimdal/issues/1161
+
+--- lib/krb5/store-int.c.orig	2022-09-15 22:59:25.000000000 +0000
++++ lib/krb5/store-int.c
+@@ -49,7 +49,7 @@ KRB5_LIB_FUNCTION krb5_ssize_t KRB5_LIB_
+ _krb5_get_int64(void *buffer, uint64_t *value, size_t size)
+ {
+     unsigned char *p = buffer;
+-    unsigned long v = 0;
++    uint64_t v = 0;
+     size_t i;
+     for (i = 0; i < size; i++)
+ 	v = (v << 8) + p[i];
