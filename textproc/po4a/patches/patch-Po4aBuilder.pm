ca7ce4957609c7f2f350771af636bf267ecfec55
schmonz <schmonz@pkgsrc.org>
2023-06-21 19:08:06 +0000

    po4a: fix build on Solarish.
    - Conditionally depend on diffutils (borrowed from pkgdiff)
    - On "SunOS", resolve /dev/null symlink (if any) during pre-configure
    - When invoking gettext-tools, make sure they're the pkgsrc ones
    - Add 'sgml' option (also off by default) as suggested by upstream
    
    Fixes build (and passes nearly all tests, with both options enabled) on
    Tribblix and Solaris 11, which is exactly as good as things were -- and
    are -- on NetBSD.
    
    Bump PKGREVISION.

--- textproc/po4a/patches/patch-Po4aBuilder.pm
+++ textproc/po4a/patches/patch-Po4aBuilder.pm
@@ -1,10 +1,10 @@
-$NetBSD: patch-Po4aBuilder.pm,v 1.9 2023/06/20 15:30:12 schmonz Exp $
+$NetBSD: patch-Po4aBuilder.pm,v 1.10 2023/06/21 19:08:06 schmonz Exp $
 
-(1) ryoon: remove gzip at initial import
-(2) mef: Add PerlIO::F_UTF8 () macro
-(3) mef: to avoid --previous option is said unknown to msgmerge
-    (but sounds strange)
-(4) schmonz: Use a sufficiently capable diff
+(1) ryoon: remove gzip at initial import.
+(2) mef: Add PerlIO::F_UTF8 () macro.
+(3) schmonz: Use a sufficiently capable diff.
+(4) schmonz: Invoke pkgsrc's gettext-tools.
+(5) schmonz: Write to the full physical null device.
 
 --- Po4aBuilder.pm.orig	2023-01-01 00:30:43.000000000 +0000
 +++ Po4aBuilder.pm
@@ -31,7 +31,7 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.9 2023/06/20 15:30:12 schmonz Exp $
          unless ($self->up_to_date("po/bin/po4a.pot", $_)) {
              print "XX Sync $_: ";
 -            system("msgmerge --previous $_ po/bin/po4a.pot -o $_.new") && die;
-+            system("msgmerge $_ po/bin/po4a.pot -o $_.new") && die;
++            system("@PREFIX@/bin/msgmerge --previous $_ po/bin/po4a.pot -o $_.new") && die;
              # Typically all that changes was a date. I'd
              # prefer not to commit such changes, so detect
              # and ignore them.
@@ -40,6 +40,28 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.9 2023/06/20 15:30:12 schmonz Exp $
              if ($diff eq "") {
                  unlink "$_.new" || die;
                  # touch it
+@@ -101,7 +103,7 @@ sub ACTION_binpo {
+       }
+         unless ($self->up_to_date($_,"blib/po/$lang/LC_MESSAGES/po4a.mo")) {
+             mkpath( File::Spec->catdir( 'blib', 'po', $lang, "LC_MESSAGES" ), 0, oct(755) );
+-            system("msgfmt -o blib/po/$lang/LC_MESSAGES/po4a.mo $_") && die;
++            system("@PREFIX@/bin/msgfmt -o blib/po/$lang/LC_MESSAGES/po4a.mo $_") && die;
+         }
+     }
+ }
+@@ -222,9 +224,9 @@ sub ACTION_man {
+                 $parser->{release} = $parser->{center} = "Po4a Tools";
+         } else {
+                 my $command;
+-                $command = "msggrep -K -E -e \"Po4a Tools\" po/pod/$lang.po |";
+-                $command .= "msgconv -t UTF-8 | ";
+-                $command .= "msgexec /bin/sh -c '[ -n \"\$MSGEXEC_MSGID\" ] ";
++                $command = "@PREFIX@/bin/msggrep -K -E -e \"Po4a Tools\" po/pod/$lang.po |";
++                $command .= "@PREFIX@/bin/msgconv -t UTF-8 | ";
++                $command .= "@PREFIX@/bin/msgexec @SH@ -c '[ -n \"\$MSGEXEC_MSGID\" ] ";
+                 $command .= "&& cat || cat > /dev/null'";
+ 
+                 my $title = `$command 2> /dev/null`;
 @@ -234,8 +236,6 @@ sub ACTION_man {
          }
          $parser->parse_from_file ($file, $out);
@@ -59,3 +81,12 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.9 2023/06/20 15:30:12 schmonz Exp $
          }
      }
  }
+@@ -284,7 +282,7 @@ sub postats {
+     foreach (sort @files) {
+         $file = $_;
+         my $lang = fileparse($file, qw{.po});
+-        my $stat = `msgfmt -o /dev/null -c --statistics $file 2>&1`;
++        my $stat = `@PREFIX@/bin/msgfmt -o @PO4A_DEVNULL@ -c --statistics $file 2>&1`;
+ 	my ($trans, $fuzz, $untr) = (0,0,0);
+ 	if ($stat =~ /(\d+)\D+?(\d+)\D+?(\d+)/) {
+ 	  ($trans, $fuzz, $untr) = ($1,$2,$3);
--- textproc/po4a/patches/patch-lib_Locale_Po4a_Po.pm
+++ textproc/po4a/patches/patch-lib_Locale_Po4a_Po.pm
@@ -1,9 +1,19 @@
-$NetBSD: patch-lib_Locale_Po4a_Po.pm,v 1.1 2023/06/20 15:30:12 schmonz Exp $
+$NetBSD: patch-lib_Locale_Po4a_Po.pm,v 1.2 2023/06/21 19:08:06 schmonz Exp $
 
 Use a sufficiently capable diff.
+Write to the full physical null device.
 
 --- lib/Locale/Po4a/Po.pm.orig	2023-01-01 00:30:43.000000000 +0000
 +++ lib/Locale/Po4a/Po.pm
+@@ -325,7 +325,7 @@ sub read {
+     $lang =~ s/\.po$//;
+     $self->{lang} = $lang;
+ 
+-    my $cmd = "msgfmt" . $Config{_exe} . " --check-format --check-domain -o /dev/null " . $filename;
++    my $cmd = "@PREFIX@/bin/msgfmt" . $Config{_exe} . " --check-format --check-domain -o @PO4A_DEVNULL@ " . $filename;
+ 
+     my $locale = $ENV{'LC_ALL'};
+     $ENV{'LC_ALL'} = "C";
 @@ -621,7 +621,7 @@ sub move_po_if_needed {
      my $diff;
  
