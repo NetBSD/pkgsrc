eb2b72100f830b7e111b5ed9df7653e9e7108158
schmonz <schmonz@pkgsrc.org>
2023-06-21 19:08:06 +0000

    po4a: fix build on Solarish.
    - Conditionally depend on diffutils (borrowed from pkgdiff)
    - On "SunOS", resolve /dev/null symlink (if any) during pre-configure
    - When invoking gettext-tools, make sure they're the pkgsrc ones
    - Add 'sgml' option (also off by default) as suggested by upstream
    
    Fixes build (and passes nearly all tests, with both options enabled) on
    Tribblix and Solaris 11, which is exactly as good as things were -- and
    are -- on NetBSD.
    
    Bump PKGREVISION.

--- textproc/po4a/options.mk
+++ textproc/po4a/options.mk
@@ -1,13 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2023/04/29 14:31:29 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2023/06/21 19:08:06 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.po4a
-PKG_SUPPORTED_OPTIONS+=		latex
+PKG_SUPPORTED_OPTIONS+=		latex sgml
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlatex)
-# kpsewhich
-DEPENDS+=	kpathsea-[0-9]*:../../print/kpathsea
-# article.cls
-DEPENDS+=	tex-latex-[0-9]*:../../print/tex-latex
+DEPENDS+=	kpathsea-[0-9]*:../../print/kpathsea	# kpsewhich
+DEPENDS+=	tex-latex-[0-9]*:../../print/tex-latex	# article.cls
+.endif
+
+.if !empty(PKG_OPTIONS:Msgml)
+DEPENDS+=	docbook-[0-9]*:../../textproc/docbook	# DTDs
+DEPENDS+=	opensp-[0-9]*:../../textproc/opensp	# onsgmls
 .endif
