9702e001bbb504e2ccfd90fe9e904cdde8613173
jperkin <jperkin@pkgsrc.org>
2023-06-23 17:12:39 +0000

    netcdf: Add bzip2 to buildlink3.mk.
    Required by various dependencies.  Fixes builds where bzip2 comes from pkgsrc.

--- devel/netcdf/buildlink3.mk
+++ devel/netcdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2023/04/19 08:08:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2023/06/23 17:12:39 jperkin Exp $
 
 BUILDLINK_TREE+=	netcdf
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.netcdf+=	netcdf>=3.5
 BUILDLINK_ABI_DEPENDS.netcdf+=	netcdf>=4.6.1nb16
 BUILDLINK_PKGSRCDIR.netcdf?=	../../devel/netcdf
 
+.include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/hdf5/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .endif # NETCDF_BUILDLINK3_MK
