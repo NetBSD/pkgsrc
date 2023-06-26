1dfaf48e4102b24692cd1354b5555730f141570e
adam <adam@pkgsrc.org>
2023-06-26 16:13:22 +0000

    py-virtualenv: updated to 20.23.1
    v20.23.1
    update and simplify nushell activation script, fixes an issue on Windows resulting in consecutive command not found
    Upgrade embedded wheels:
    setuptools to 67.8.0 from 67.7.2

--- devel/py-virtualenv/patches/patch-src_virtualenv_create_via__global__ref_api.py
+++ devel/py-virtualenv/patches/patch-src_virtualenv_create_via__global__ref_api.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-src_virtualenv_create_via__global__ref_api.py,v 1.3 2023/04/04 09:52:28 adam Exp $
+$NetBSD: patch-src_virtualenv_create_via__global__ref_api.py,v 1.4 2023/06/26 16:13:22 adam Exp $
 
 Copy distutils distro config file to virtualenv.
 https://github.com/pypa/virtualenv/issues/2313
 
 --- src/virtualenv/create/via_global_ref/api.py.orig	2020-02-02 00:00:00.000000000 +0000
 +++ src/virtualenv/create/via_global_ref/api.py
-@@ -2,6 +2,8 @@ import logging
+@@ -4,6 +4,8 @@ import logging
  import os
  from abc import ABCMeta
  from pathlib import Path
 +from shutil import copy
 +from sysconfig import get_paths
  
+ from virtualenv.create.creator import Creator, CreatorMeta
  from virtualenv.info import fs_supports_symlink
- 
-@@ -92,6 +94,10 @@ class ViaGlobalRefApi(Creator, metaclass
+@@ -94,6 +96,10 @@ class ViaGlobalRefApi(Creator, metaclass
              logging.debug("create %s", dest_path)
              dest_path.write_text(text, encoding="utf-8")
  
@@ -23,5 +23,5 @@ https://github.com/pypa/virtualenv/issues/2313
 +            copy(distutils_distro_config, self.platlib)
 +
      def env_patch_text(self):
-         """Patch the distutils package to not be derailed by its configuration files"""
+         """Patch the distutils package to not be derailed by its configuration files."""
          with self.app_data.ensure_extracted(Path(__file__).parent / "_virtualenv.py") as resolved_path:
