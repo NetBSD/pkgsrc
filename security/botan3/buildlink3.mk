2cf3512a08102e560816e8f230485de04fd87539
wiz <wiz@pkgsrc.org>
2023-06-26 11:40:39 +0000

    security/botan3: import botan3-3.0.0
    Botan is a crypto library written in C++. It provides a variety of
    cryptographic algorithms, including common ones such as AES, MD5, SHA,
    HMAC, RSA, Diffie-Hellman, DSA, and ECDSA, as well as many others that
    are more obscure or specialized. It also offers X.509v3 certificates
    and CRLs, and PKCS #10 certificate requests. A message processing
    system that uses a filter/pipeline metaphor allows for many common
    cryptographic tasks to be completed with just a few lines of code.
    Assembly optimizations for common CPUs, including x86, x86-64, and
    PowerPC, offers further speedups for critical tasks such as SHA-1
    hashing and multiple precision integer operations.
    
    This package contains major version 3 of the library.

---
+++ security/botan3/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/06/26 11:40:39 wiz Exp $
+
+BUILDLINK_TREE+=	botan3
+
+.if !defined(BOTAN3_BUILDLINK3_MK)
+BOTAN3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.botan3+=	botan3>=3.0.0
+BUILDLINK_PKGSRCDIR.botan3?=	../../security/botan3
+
+.include "../../devel/boost-libs/buildlink3.mk"
+.endif	# BOTAN3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-botan3
