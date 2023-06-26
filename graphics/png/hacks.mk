2934c5f39125e6efdb743aa1d407413488ccd549
wiz <wiz@pkgsrc.org>
2023-06-26 13:10:12 +0000

    png: update to 1.6.40.
    Changes from version 1.6.39 to version 1.6.40
    ---------------------------------------------
    
     * Fixed the eXIf chunk multiplicity checks.
     * Fixed a memory leak in pCAL processing.
     * Corrected the validity report about tRNS inside png_get_valid().
     * Fixed various build issues on *BSD, Mac and Windows.
     * Updated the configurations and the scripts for continuous integration.
     * Cleaned up the code, the build scripts, and the documentation.

--- graphics/png/hacks.mk
+++ graphics/png/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.5 2019/11/02 22:38:01 rillig Exp $
+# $NetBSD: hacks.mk,v 1.6 2023/06/26 13:10:12 wiz Exp $
 
 .if !defined(PNG_HACKS_MK)
 PNG_HACKS_MK=	defined
@@ -8,7 +8,7 @@ PNG_HACKS_MK=	defined
 ### gcc-4.5.3 in NetBSD/arm 6.1 causes SEGV in some png functions.
 ### -O works around, and there is a report that -O2 -fno-ipa-sra is enough.
 ### See PR/47798 for details.
-.if !empty(MACHINE_PLATFORM:MNetBSD-*-arm) && !empty(CC_VERSION:Mgcc-4.5.*)
+.if ${MACHINE_PLATFORM:MNetBSD-*-arm} && ${CC_VERSION:Mgcc-4.5.*}
 # XXX: is there any good way to replace the default -O2 with multiple args?
 #PKG_HACKS+=		optimisation
 #BUILDLINK_TRANSFORM+=	opt:-O[2-9]*:-O2 -fno-ipa-sra
