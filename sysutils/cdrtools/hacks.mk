cb35eeb3c79b7c632e605aac26a3c361c25eb644
bsiegert <bsiegert@pkgsrc.org>
2023-06-27 20:40:34 +0000

    cdrtools: remove ABI check on Darwin hack
    The ABI==64 check is redundant, since the problem that is being worked
    around is not specific to the exact ABI. This fixes the build on systems
    such as mine, where the bootstrap is not run with  "--abi 64".
    
    Update PR pkg/51257

--- sysutils/cdrtools/hacks.mk
+++ sysutils/cdrtools/hacks.mk
@@ -1,9 +1,8 @@
-# $NetBSD: hacks.mk,v 1.5 2016/10/07 13:22:10 joerg Exp $
+# $NetBSD: hacks.mk,v 1.6 2023/06/27 20:40:34 bsiegert Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
-.if ${OPSYS} == "Darwin" && defined(ABI) && ${ABI} == "64" && \
-    ${_USE_CWRAPPERS} == "yes"
+.if ${OPSYS} == "Darwin" && ${_USE_CWRAPPERS} == "yes"
 #
 # PR pkg/51257: "sysutils/cdrtools" fails to build under Mac OS X 64bit
 #               when cwrappers are used
