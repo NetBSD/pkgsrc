7c78ca80d2d090ee1f7ad5654554ff9469845db1
adam <adam@pkgsrc.org>
2023-06-27 08:33:10 +0000

    erlang: updated to 26.0.1
    26.0.1
    Bug-fix release

--- lang/erlang/versions.mk
+++ lang/erlang/versions.mk
@@ -1,3 +1,3 @@
-# $NetBSD: versions.mk,v 1.33 2023/05/23 21:55:50 wiz Exp $
+# $NetBSD: versions.mk,v 1.34 2023/06/27 08:33:10 adam Exp $
 # Do not edit, regenerate using 'make versions'
-_VERSIONS=	asn1 5.1 common_test 1.25 compiler 8.3 crypto 5.2 dialyzer 5.1 diameter 2.3 erl_docgen 1.5 erl_interface 5.4 erts 14.0 et 1.7 ftp 1.2 inets 9.0 jinterface 1.14 kernel 9.0 megaco 4.4.4 mnesia 4.22 observer 2.15 odbc 2.14.1 os_mon 2.9 parsetools 2.5 public_key 1.14 reltool 1.0 runtime_tools 2.0 sasl 4.2.1 snmp 5.14 ssh 5.0 ssl 11.0 stdlib 5.0 syntax_tools 3.1 tftp 1.1 tools 3.6 wx 2.3 debugger 5.3.1 edoc 1.2 eldap 1.2.11 eunit 2.8.2 xmerl 1.3.31
+_VERSIONS=	compiler 8.3.1 erts 14.0.1 inets 9.0.1 kernel 9.0.1 ssl 11.0.1 stdlib 5.0.1 xmerl 1.3.32 asn1 5.1 common_test 1.25 crypto 5.2 debugger 5.3.1 dialyzer 5.1 diameter 2.3 edoc 1.2 eldap 1.2.11 erl_docgen 1.5 erl_interface 5.4 et 1.7 eunit 2.8.2 ftp 1.2 jinterface 1.14 megaco 4.4.4 mnesia 4.22 observer 2.15 odbc 2.14.1 os_mon 2.9 parsetools 2.5 public_key 1.14 reltool 1.0 runtime_tools 2.0 sasl 4.2.1 snmp 5.14 ssh 5.0 syntax_tools 3.1 tftp 1.1 tools 3.6 wx 2.3
