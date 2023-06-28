75417cd20ceea76880b366ccbc25df664dc3ae8c
wiz <wiz@pkgsrc.org>
2023-06-28 08:39:38 +0000

    py-google-api-python-client: update to 2.90.0.
    No changelog found.

--- www/py-google-api-python-client/patches/patch-setup.py
+++ www/py-google-api-python-client/patches/patch-setup.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-setup.py,v 1.2 2022/08/30 11:33:50 sborrill Exp $
+$NetBSD: patch-setup.py,v 1.3 2023/06/28 08:39:38 wiz Exp $
 
 Relax restrictions.
 
---- setup.py.orig	2022-08-23 15:04:32.000000000 +0100
-+++ setup.py	2022-08-25 12:31:43.711533878 +0100
-@@ -43,7 +43,7 @@
+--- setup.py.orig	2023-06-20 16:26:45.000000000 +0000
++++ setup.py
+@@ -43,7 +43,7 @@ install_requires = [
      # Until this issue is closed
      # https://github.com/googleapis/google-cloud-python/issues/10566
-     "google-api-core >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
+     "google-api-core >= 1.31.5, <3.0.0.dev0,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
 -    "uritemplate>=3.0.1,<5",
 +    "uritemplate>=3.0.1",
  ]
