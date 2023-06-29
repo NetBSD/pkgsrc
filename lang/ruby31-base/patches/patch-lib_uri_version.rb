4af528eb34ac2466824219814534656884d86875
taca <taca@pkgsrc.org>
2023-06-29 15:39:12 +0000

    lang/ruby31-base: update bundled gem uri to 0.12.2
    Fix CVE-2023-36617: ReDoS vulnerability in URI.
    
    Bump PKGREVISION.

---
+++ lang/ruby31-base/patches/patch-lib_uri_version.rb
@@ -0,0 +1,14 @@
+$NetBSD: patch-lib_uri_version.rb,v 1.1 2023/06/29 15:39:12 taca Exp $
+
+Fix for CVE-2023-36617 updating uri to 0.12.2.
+
+--- lib/uri/version.rb.orig	2023-03-30 10:53:51.000000000 +0000
++++ lib/uri/version.rb
+@@ -1,6 +1,6 @@
+ module URI
+   # :stopdoc:
+-  VERSION_CODE = '001201'.freeze
++  VERSION_CODE = '001202'.freeze
+   VERSION = VERSION_CODE.scan(/../).collect{|n| n.to_i}.join('.').freeze
+   # :startdoc:
+ end
