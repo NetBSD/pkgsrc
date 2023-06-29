93259a821ca36b96174672a5b3a8888047b38df8
taca <taca@pkgsrc.org>
2023-06-29 15:42:07 +0000

    lang/ruby32-base: update bundled gem uri to 0.12.2
    Fix CVE-2023-36617: ReDoS vulnerability in URI.
    
    Bump PKGREVISION.

---
+++ lang/ruby32-base/patches/patch-lib_uri_rfc3986__parser.rb
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_uri_rfc3986__parser.rb,v 1.1 2023/06/29 15:42:07 taca Exp $
+
+Fix for CVE-2023-36617 updating uri to 0.12.2.
+
+--- lib/uri/rfc3986_parser.rb.orig	2023-03-30 11:06:29.000000000 +0000
++++ lib/uri/rfc3986_parser.rb
+@@ -100,7 +100,7 @@ module URI
+         QUERY: /\A(?:%\h\h|[!$&-.0-;=@-Z_a-z~\/?])*\z/,
+         FRAGMENT: /\A(?:%\h\h|[!$&-.0-;=@-Z_a-z~\/?])*\z/,
+         OPAQUE: /\A(?:[^\/].*)?\z/,
+-        PORT: /\A[\x09\x0a\x0c\x0d ]*\d*[\x09\x0a\x0c\x0d ]*\z/,
++        PORT: /\A[\x09\x0a\x0c\x0d ]*+\d*[\x09\x0a\x0c\x0d ]*\z/,
+       }
+     end
+ 
