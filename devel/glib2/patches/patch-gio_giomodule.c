1f979b519b170d8671fa54386e58d9dc6664a042
riastradh <riastradh@pkgsrc.org>
2023-06-30 13:08:37 +0000

    devel/glib2: Fix build on macOS without cocoa option.

--- devel/glib2/patches/patch-gio_giomodule.c
+++ devel/glib2/patches/patch-gio_giomodule.c
@@ -1,10 +1,20 @@
-$NetBSD: patch-gio_giomodule.c,v 1.7 2021/10/11 10:03:47 cirnatdan Exp $
+$NetBSD: patch-gio_giomodule.c,v 1.8 2023/06/30 13:08:37 riastradh Exp $
 
+Make Cocoa notification backend conditional.
 Disable inotify on SunOS.
 
---- gio/giomodule.c.orig	2021-09-17 10:17:56.682960300 +0000
+--- gio/giomodule.c.orig	2023-05-23 10:55:59.000000000 +0000
 +++ gio/giomodule.c
-@@ -1280,7 +1280,7 @@ _g_io_modules_ensure_loaded (void)
+@@ -1105,7 +1105,7 @@ extern GType g_proxy_resolver_portal_get
+ extern GType g_network_monitor_portal_get_type (void);
+ #endif
+ 
+-#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
++#if defined(HAVE_COCOA) && MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+ extern GType g_cocoa_notification_backend_get_type (void);
+ #endif
+ 
+@@ -1346,7 +1346,7 @@ _g_io_modules_ensure_loaded (void)
        g_type_ensure (g_memory_settings_backend_get_type ());
        g_type_ensure (g_keyfile_settings_backend_get_type ());
        g_type_ensure (g_power_profile_monitor_dbus_get_type ());
@@ -13,3 +23,12 @@ Disable inotify on SunOS.
        g_type_ensure (g_inotify_file_monitor_get_type ());
  #endif
  #if defined(HAVE_KQUEUE)
+@@ -1373,7 +1373,7 @@ _g_io_modules_ensure_loaded (void)
+       g_type_ensure (g_power_profile_monitor_portal_get_type ());
+       g_type_ensure (g_proxy_resolver_portal_get_type ());
+ #endif
+-#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
++#if defined(HAVE_COCOA) && MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+       g_type_ensure (g_cocoa_notification_backend_get_type ());
+ #endif
+ #ifdef G_OS_WIN32
