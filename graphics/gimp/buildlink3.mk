271371e62155cde19ffd86c6ba854f96f463d2f9
wiz <wiz@pkgsrc.org>
2023-07-01 22:16:34 +0000

    *: recursive bump for exiv2 0.28.0

--- graphics/gimp/buildlink3.mk
+++ graphics/gimp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.84 2023/05/16 20:28:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.85 2023/07/01 22:16:35 wiz Exp $
 
 BUILDLINK_TREE+=	gimp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gimp
 GIMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gimp+=	gimp>=2.4.0<2.99
-BUILDLINK_ABI_DEPENDS.gimp+=	gimp>=2.10.32nb11
+BUILDLINK_ABI_DEPENDS.gimp+=	gimp>=2.10.32nb14
 BUILDLINK_PKGSRCDIR.gimp?=	../../graphics/gimp
 
 pkgbase := gimp
