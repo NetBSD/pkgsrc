a711a71345a8a4906ea6db8716a7b31367092e70
wiz <wiz@pkgsrc.org>
2023-07-01 22:13:14 +0000

    exiv2: update to 0.28.0, again.
    geeqie already supports it, patches for two others incoming.

--- graphics/exiv2/buildlink3.mk
+++ graphics/exiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2023/05/17 10:49:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2023/07/01 22:13:14 wiz Exp $
 
 BUILDLINK_TREE+=	exiv2
 
@@ -6,12 +6,13 @@ BUILDLINK_TREE+=	exiv2
 EXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.exiv2+=	exiv2>=0.27
-BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.27
+BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.28.0
 BUILDLINK_PKGSRCDIR.exiv2?=	../../graphics/exiv2
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
+.include "../../textproc/inih/buildlink3.mk"
 .endif # EXIV2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-exiv2
