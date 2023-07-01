d65719ff1f9996710be7c7295f44d1fb19439b64
wiz <wiz@pkgsrc.org>
2023-07-01 22:16:34 +0000

    *: recursive bump for exiv2 0.28.0

--- graphics/gexiv2/buildlink3.mk
+++ graphics/gexiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/05/16 20:28:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/07/01 22:16:35 wiz Exp $
 
 BUILDLINK_TREE+=	gexiv2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gexiv2
 GEXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gexiv2+=	gexiv2>=0.12.0
-BUILDLINK_ABI_DEPENDS.gexiv2?=	gexiv2>=0.14.0nb3
+BUILDLINK_ABI_DEPENDS.gexiv2?=	gexiv2>=0.14.1nb2
 BUILDLINK_PKGSRCDIR.gexiv2?=	../../graphics/gexiv2
 
 .include "../../devel/glib2/buildlink3.mk"
