bcf207d1a08e8cb993e275f739086fed3713bf4d
nia <nia@pkgsrc.org>
2023-07-02 17:32:30 +0000

    Import emulators/flycast. Rejig libretro-flycast package around it.
    Flycast is a multi-platform Sega Dreamcast, Naomi, Naomi 2, and Atomiswave
    emulator derived from reicast.

--- emulators/flycast/options.mk
+++ emulators/flycast/options.mk
@@ -1,6 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2023/07/02 17:32:30 nia Exp $
+# $NetBSD: options.mk,v 1.1 2023/07/02 17:32:30 nia Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.libretro-flycast
+PKG_OPTIONS_VAR=		PKG_OPTIONS.flycast
+
+PKG_SUPPORTED_OPTIONS+=		alsa pulseaudio
+PKG_SUGGESTED_OPTIONS.Linux+=	alsa
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -12,6 +15,16 @@ PKG_SUGGESTED_OPTIONS+=		rpi
 .endif
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Malsa)
+.  include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CMAKE_ARGS+=		-DCMAKE_DISABLE_FIND_PACKAGE_ALSA=ON
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.  include "../../audio/pulseaudio/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mrpi)
 CMAKE_ARGS+=	-DUSE_VIDEOCORE=ON
