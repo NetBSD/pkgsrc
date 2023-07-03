e7f42024b2d9ceef0aab5d87fe75eb5136d93920
wiz <wiz@pkgsrc.org>
2023-07-03 12:57:52 +0000

    nodejs*: add upper bounds so only the requested version is pulled in

--- lang/nodejs16/buildlink3.mk
+++ lang/nodejs16/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/04/19 17:41:29 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/07/03 12:57:52 wiz Exp $
 
 BUILDLINK_TREE+=	nodejs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nodejs
 NODEJS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nodejs+=	nodejs>=10
-BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=16.20.0nb1
+BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=16.20.0nb1<18
 BUILDLINK_PKGSRCDIR.nodejs?=	../../lang/nodejs16
 
 .include "../../mk/bsd.fast.prefs.mk"
