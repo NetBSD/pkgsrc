704af1e47566b0dd5b6a9fab496c9e5f280d7248
wiz <wiz@pkgsrc.org>
2023-07-03 17:33:06 +0000

    pyt-trytond: move PYTHON_VERSIONS_INCOMPATIBLE into Makefile.common

--- devel/py-trytond/options.mk
+++ devel/py-trytond/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.19 2023/07/03 17:18:05 wiz Exp $
+# $NetBSD: options.mk,v 1.20 2023/07/03 17:33:06 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-trytond
 PKG_SUPPORTED_OPTIONS=	levenshtein pydot psycopg
@@ -7,7 +7,6 @@ PKG_SUGGESTED_OPTIONS+=	levenshtein pydot psycopg
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlevenshtein)
-PYTHON_VERSIONS_INCOMPATIBLE+= 27 37 38
 DEPENDS+=		${PYPKGPREFIX}-Levenshtein-[0-9]*:../../textproc/py-Levenshtein
 .endif
 
