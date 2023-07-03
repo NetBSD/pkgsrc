00598599c1c71e250165dfe90a0f4af4861dd074
vins <vins@pkgsrc.org>
2023-07-03 10:51:40 +0000

    devel/libmd: update to release 1.1.0
    CHANGES:
    * Code synced from the various BSDs, with the noticeable addition of
      the SHA224 and SHA512-256 digest variants.
    * New libmd(7) man page.
    * Portability fixes for macOS.
    * Various build system fixes.

--- devel/libmd/buildlink3.mk
+++ devel/libmd/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/12/19 15:08:04 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/03 10:51:40 vins Exp $
 
 BUILDLINK_TREE+=	libmd
 
 .if !defined(LIBMD_BUILDLINK3_MK)
 LIBMD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmd+=	libmd>=1.0.4
+BUILDLINK_API_DEPENDS.libmd+=	libmd>=1.1.0
 BUILDLINK_PKGSRCDIR.libmd?=	../../devel/libmd
 .endif	# LIBMD_BUILDLINK3_MK
 
