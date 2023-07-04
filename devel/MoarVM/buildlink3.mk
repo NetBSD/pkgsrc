a2abb2aa9df4bb2a73e49be72112c8d07885140a
mef <mef@pkgsrc.org>
2023-07-04 13:49:50 +0000

    (devel/MoarVM) Update buildlink3.mk as well, sorry being late

--- devel/MoarVM/buildlink3.mk
+++ devel/MoarVM/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/05/24 14:38:35 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/07/04 13:49:50 mef Exp $
 
 BUILDLINK_TREE+=	MoarVM
 
 .if !defined(MOARVM_BUILDLINK3_MK)
 MOARVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2023.04
-BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2023.04
+BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2023.06
+BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2023.06
 BUILDLINK_PKGSRCDIR.MoarVM?=	../../devel/MoarVM
 
 .include "../../devel/libatomic_ops/buildlink3.mk"
