0e792f1287f391a9931840adaeca5f02a29fcb89
nia <nia@pkgsrc.org>
2023-07-07 12:45:25 +0000

    xdiary: Further reduce number of implicit and own declarations of libc functions

--- time/xdiary/patches/patch-ndbm_xdndbm.c
+++ time/xdiary/patches/patch-ndbm_xdndbm.c
@@ -1,11 +1,19 @@
-$NetBSD: patch-ndbm_xdndbm.c,v 1.1 2023/05/27 15:22:36 he Exp $
+$NetBSD: patch-ndbm_xdndbm.c,v 1.2 2023/07/07 12:45:25 nia Exp $
 
-Make `int` types explicit, don't own-declare lseek() on NetBSD
+Make `int` types explicit, don't own-declare lseek()
 (gives type mismatch).
 
 --- ndbm/xdndbm.c.orig	1997-05-04 22:05:18.000000000 +0000
 +++ ndbm/xdndbm.c
-@@ -28,8 +28,8 @@
+@@ -12,6 +12,7 @@
+ 
+ #include <sys/types.h>
+ #include <sys/stat.h>
++#include <errno.h>
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include <stdio.h>
+@@ -28,8 +29,8 @@
  #ifdef NET_BYTE_ORDER
  #include <netinet/in.h>
  
@@ -16,17 +24,17 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
  #undef DEBUG
  #endif
-@@ -53,7 +53,9 @@ static  int delitem();
+@@ -52,9 +53,6 @@ static  int finddatum();
+ static  int delitem();
  static  int additem();
  
- extern  int errno;
-+#ifndef __NetBSD__
- extern  long lseek();
-+#endif
- 
+-extern  int errno;
+-extern  long lseek();
+-
  #ifdef DEBUG
  static chkblk(char buf[PBLKSIZ]);
-@@ -142,7 +144,7 @@ dbm_fetch(db, key)
+ #endif
+@@ -142,7 +140,7 @@ dbm_fetch(db, key)
  register DBM *db;
  datum key;
  {
@@ -35,7 +43,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
    datum item;
  
    if (dbm_error(db))
-@@ -168,7 +170,7 @@ dbm_delete(db, key)
+@@ -168,7 +166,7 @@ dbm_delete(db, key)
  register DBM *db;
  datum key;
  {
@@ -44,7 +52,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
    if (dbm_error(db))
      return (-1);
-@@ -216,7 +218,7 @@ register DBM *db;
+@@ -216,7 +214,7 @@ register DBM *db;
  datum key, dat;
  int replace;
  {
@@ -53,7 +61,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
    datum item, item1;
    char ovfbuf[PBLKSIZ];
  
-@@ -438,7 +440,7 @@ getbit(db)
+@@ -438,7 +436,7 @@ getbit(db)
  register DBM *db;
  {
    long bn;
@@ -62,7 +70,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
    if (db->dbm_bitno > db->dbm_maxbno)
      return (0);
-@@ -496,7 +498,7 @@ makdatum(buf, n)
+@@ -496,7 +494,7 @@ makdatum(buf, n)
  char buf[PBLKSIZ];
  {
    register short *sp;
@@ -71,7 +79,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
    datum item;
  
    sp = (short *)buf;
-@@ -624,7 +626,7 @@ delitem(buf, n)
+@@ -624,7 +622,7 @@ delitem(buf, n)
  char buf[PBLKSIZ];
  {
    register short *sp, *sp1;
@@ -80,7 +88,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
    sp = (short *)buf;
    i2 = sp[0];
-@@ -667,7 +669,7 @@ char buf[PBLKSIZ];
+@@ -667,7 +665,7 @@ char buf[PBLKSIZ];
  datum item, item1;
  {
    register short *sp;
@@ -89,7 +97,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
    sp = (short *)buf;
    i1 = PBLKSIZ;
-@@ -695,7 +697,7 @@ static
+@@ -695,7 +693,7 @@ static
  chkblk(char buf[PBLKSIZ])
  {
    register short *sp;
@@ -98,7 +106,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
    sp = (short *)buf;
    t = PBLKSIZ;
-@@ -719,7 +721,7 @@ int
+@@ -719,7 +717,7 @@ int
  convert_after_read(char buf[PBLKSIZ])
  {
    register short *sp;
@@ -107,7 +115,7 @@ Make `int` types explicit, don't own-declare lseek() on NetBSD
  
    sp = (short *)buf;
    sp[0] = ntohs(sp[0]);
-@@ -745,7 +747,7 @@ int
+@@ -745,7 +743,7 @@ int
  convert_before_write(char buf[PBLKSIZ])
  {
    register short *sp;
