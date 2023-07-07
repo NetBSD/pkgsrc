de3f045ad21ec3d4ed198a03e3f7fada53656123
nia <nia@pkgsrc.org>
2023-07-07 12:45:25 +0000

    xdiary: Further reduce number of implicit and own declarations of libc functions

--- time/xdiary/patches/patch-dtools_xtmDbTools.c
+++ time/xdiary/patches/patch-dtools_xtmDbTools.c
@@ -1,19 +1,20 @@
-$NetBSD: patch-dtools_xtmDbTools.c,v 1.1 2023/01/22 20:08:17 vins Exp $
+$NetBSD: patch-dtools_xtmDbTools.c,v 1.2 2023/07/07 12:45:25 nia Exp $
 
 Avoid int to pointer conversion warnings.
 Add a mode to open() call with O_CREAT.
 
 --- dtools/xtmDbTools.c.orig	1997-05-04 22:27:52.000000000 +0000
 +++ dtools/xtmDbTools.c
-@@ -47,6 +47,7 @@ static char SCCSID[] = "@(#) Module: xtm
+@@ -47,6 +47,8 @@ static char SCCSID[] = "@(#) Module: xtm
  
  #include <fcntl.h>
  #include <stdio.h>
 +#include <stdint.h>
++#include <stdlib.h>
  #include <unistd.h>
  #include <ctype.h>
  #include <sys/stat.h>
-@@ -1813,7 +1814,7 @@ XTM_DB_STATUS
+@@ -1813,7 +1815,7 @@ XTM_DB_STATUS
  
      /* Insert the date in the sorted list. */
      lst_status = LstLinkSearchFirst( *list_ref, 
@@ -22,7 +23,7 @@ Add a mode to open() call with O_CREAT.
                                       (EQUALS_FUNC_TYPE) dateSortFunc );
  
      if( lst_status == LST_OK )
-@@ -1896,7 +1897,7 @@ XTM_DB_STATUS
+@@ -1896,7 +1898,7 @@ XTM_DB_STATUS
            case XTM_DB_DAY_NOTE:
              lst_status = LstLinkSearchFirst( 
                             *note_list_ref, 
@@ -31,7 +32,7 @@ Add a mode to open() call with O_CREAT.
                             (EQUALS_FUNC_TYPE) entryIdSortFunc );
  
              if( lst_status == LST_OK )
-@@ -1910,7 +1911,7 @@ XTM_DB_STATUS
+@@ -1910,7 +1912,7 @@ XTM_DB_STATUS
            case XTM_DB_DAY_ENTRY:
              lst_status = LstLinkSearchFirst( 
                             *entry_list_ref, 
@@ -40,7 +41,7 @@ Add a mode to open() call with O_CREAT.
                             (EQUALS_FUNC_TYPE) entryTimeSortFunc );
  
              if( lst_status == LST_OK )
-@@ -1984,7 +1985,7 @@ XTM_DB_STATUS
+@@ -1984,7 +1986,7 @@ XTM_DB_STATUS
          case XTM_DB_DAY_NOTE:
            lst_status = LstLinkSearchFirst( 
                           *note_list_ref, 
@@ -49,7 +50,7 @@ Add a mode to open() call with O_CREAT.
                           (EQUALS_FUNC_TYPE) entryIdSortFunc );
  
            if( lst_status == LST_OK )
-@@ -1998,7 +1999,7 @@ XTM_DB_STATUS
+@@ -1998,7 +2000,7 @@ XTM_DB_STATUS
          case XTM_DB_DAY_ENTRY:
            lst_status = LstLinkSearchFirst( 
                           *entry_list_ref, 
@@ -58,7 +59,7 @@ Add a mode to open() call with O_CREAT.
                           (EQUALS_FUNC_TYPE) entryTimeSortFunc );
  
            if( lst_status == LST_OK )
-@@ -2072,7 +2073,7 @@ XTM_DB_STATUS
+@@ -2072,7 +2074,7 @@ XTM_DB_STATUS
        case XTM_DB_DAY_NOTE:
          lst_status = LstLinkSearchFirst( 
                         *note_list_ref, 
@@ -67,7 +68,7 @@ Add a mode to open() call with O_CREAT.
                         (EQUALS_FUNC_TYPE) entryIdSortFunc );
  
          if( lst_status == LST_OK )
-@@ -2086,7 +2087,7 @@ XTM_DB_STATUS
+@@ -2086,7 +2088,7 @@ XTM_DB_STATUS
        case XTM_DB_DAY_ENTRY:
          lst_status = LstLinkSearchFirst( 
                         *entry_list_ref, 
@@ -76,7 +77,7 @@ Add a mode to open() call with O_CREAT.
                         (EQUALS_FUNC_TYPE) entryTimeSortFunc );
  
          if( lst_status == LST_OK )
-@@ -2332,7 +2333,7 @@ XTM_DB_STATUS
+@@ -2332,7 +2334,7 @@ XTM_DB_STATUS
    sprintf( filename, "%s/%s_%s_%d",
             message_dir, XTM_DB_MESSAGE_FILE, msg_info -> from, new_id );
  
