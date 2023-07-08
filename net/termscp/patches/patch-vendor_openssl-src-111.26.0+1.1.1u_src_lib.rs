97c9bde1cbc1e39a0709057fcaceb2530ab72a44
pin <pin@pkgsrc.org>
2023-07-08 20:16:41 +0000

    net/termscp: update to 0.12.1
    0.12.1 - Released on 06/07/2023
     - Issue 169: Local working directory can now be specified in connection form
       and be saved into bookmarks.
     - Issue 188: The breadcrumbs path is not fallbacked after a failed enter into
       the directory
     - SMB support is now a feature (you can build rust without default features to
       disable smb).
     - If SSH connection timeout is 0, the connection won't timeout.
    
    0.12.0 - Released on 16/05/2023
     - Change file permissions: you can now change file permissions easily with the
       permissions popup pressing Z in the explorer.
        - Issue 172
     - SMB protocol: Support for SMB protocol has been added thanks to the
       remotefs-smb library and the pavao project. You may notice that the
       interface is quiet different between Windows and Linux/MacOs/BSD due to the
       fact that SMB is natively supported on Windows systems.
        - Issue 182
     - Issue 153: show a loading message when loading directory's content
     - Issue 176: debug log is now written to CACHE_DIR
     - Issue 173: allow unknown fields in ssh2 configuration file
     - Issue 175: don't prompt for password if a ssh key is set for that host
     - Fixed an issue that didn't use the User specified in ssh2-config

--- net/termscp/patches/patch-vendor_openssl-src-111.26.0+1.1.1u_src_lib.rs
+++ net/termscp/patches/patch-vendor_openssl-src-111.26.0+1.1.1u_src_lib.rs
@@ -1,9 +1,9 @@
-$NetBSD: patch-vendor_openssl-src-111.25.1+1.1.1t_src_lib.rs,v 1.2 2023/04/18 18:51:48 pin Exp $
+$NetBSD: patch-vendor_openssl-src-111.26.0+1.1.1u_src_lib.rs,v 1.1 2023/07/08 20:16:41 pin Exp $
 
 Allow building on NetBSD and provide defaults for the various targets.
 
---- ../vendor/openssl-src-111.25.2+1.1.1t/src/lib.rs.orig	2006-07-24 01:21:28.000000000 +0000
-+++ ../vendor/openssl-src-111.25.2+1.1.1t/src/lib.rs
+--- ../vendor/openssl-src-111.26.0+1.1.1u/src/lib.rs.orig	2006-07-24 01:21:28.000000000 +0000
++++ ../vendor/openssl-src-111.26.0+1.1.1u/src/lib.rs
 @@ -239,6 +239,8 @@ impl Build {
              "aarch64-unknown-freebsd" => "BSD-generic64",
              "aarch64-unknown-linux-gnu" => "linux-aarch64",
@@ -34,10 +34,10 @@ Allow building on NetBSD and provide defaults for the various targets.
              "i686-unknown-linux-gnu" => "linux-elf",
              "i686-unknown-linux-musl" => "linux-elf",
 +            "i686-unknown-netbsd" => "BSD-x86-elf",
+             "loongarch64-unknown-linux-gnu" => "linux64-loongarch64",
              "mips-unknown-linux-gnu" => "linux-mips32",
              "mips-unknown-linux-musl" => "linux-mips32",
-             "mips64-unknown-linux-gnuabi64" => "linux64-mips64",
-@@ -275,6 +280,7 @@ impl Build {
+@@ -276,6 +281,7 @@ impl Build {
              "powerpc-unknown-freebsd" => "BSD-generic32",
              "powerpc-unknown-linux-gnu" => "linux-ppc",
              "powerpc-unknown-linux-gnuspe" => "linux-ppc",
@@ -45,7 +45,7 @@ Allow building on NetBSD and provide defaults for the various targets.
              "powerpc64-unknown-freebsd" => "BSD-generic64",
              "powerpc64-unknown-linux-gnu" => "linux-ppc64",
              "powerpc64-unknown-linux-musl" => "linux-ppc64",
-@@ -284,6 +290,7 @@ impl Build {
+@@ -285,6 +291,7 @@ impl Build {
              "riscv64gc-unknown-linux-gnu" => "linux-generic64",
              "s390x-unknown-linux-gnu" => "linux64-s390x",
              "s390x-unknown-linux-musl" => "linux64-s390x",
