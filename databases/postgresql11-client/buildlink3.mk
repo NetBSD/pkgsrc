9fd2fcc0756c4a971e5a0ed3669a5548e3a76d41
nia <nia@pkgsrc.org>
2023-07-09 13:57:19 +0000

    postgresql-client: When dependening modules include libpq server headers, they can then attempt to include the gssapi headers if postgres was compiled with the gssapi option. In this case, include gssapi in the buildlink3 tree.
    Should fix the build of various modules on various platforms.

--- databases/postgresql11-client/buildlink3.mk
+++ databases/postgresql11-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/06/28 11:31:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/07/09 13:57:19 nia Exp $
 
 BUILDLINK_TREE+=	postgresql11-client
 
@@ -21,6 +21,13 @@ BUILDLINK_FILES.postgresql11-client+=	bin/pg_config
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
+
+pkgbase := postgresql11-client
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql11-client:Mgssapi}
+.  include "../../mk/krb5.buildlink3.mk"
+.endif
 .endif # POSTGRESQL11_CLIENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-postgresql11-client
