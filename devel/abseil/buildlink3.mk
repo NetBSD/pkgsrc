45d83a3f893fb137d973de90df3f84a2963e278d
nia <nia@pkgsrc.org>
2023-07-09 08:16:47 +0000

    abseil: Requires a minimum of gcc7 through its own headers.

--- devel/abseil/buildlink3.mk
+++ devel/abseil/buildlink3.mk
@@ -1,9 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/07/06 15:16:59 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/07/09 08:16:47 nia Exp $
 
 BUILDLINK_TREE+=	abseil
 
 .if !defined(ABSEIL_BUILDLINK3_MK)
 ABSEIL_BUILDLINK3_MK:=
+
+# .buildlink/include/absl/base/policy_checks.h:57:2:
+# error: #error "This package requires GCC 7 or higher."
+GCC_REQD+=	7
 
 BUILDLINK_API_DEPENDS.abseil+=	abseil>=20220623.0
 BUILDLINK_PKGSRCDIR.abseil?=	../../devel/abseil
