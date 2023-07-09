5993598e96c14f0de2e24584374925d0d05451ff
bsiegert <bsiegert@pkgsrc.org>
2023-07-09 19:10:21 +0000

    go-staticcheck: update to 2023.1.3
    Staticcheck 2023.1.3
    
    This release fixes the following bugs:
    
    *   A crash when embedding type aliases of unnamed types
    *   A false positive in U1000, claiming that type aliases are unused
    *   A bug in the binary formatter that prevented correct merging behavior for
        some checks
    
    
    Staticcheck 2023.1.2 (v0.4.2)
    
    This release fixes a bug that prevented the binary formatter from working.
    
    Staticcheck 2023.1.1 (v0.4.1)
    
    This release fixes a crash, a false positive in U1000 and improves the way
    deprecated API is flagged.
    
    When targeting a Go version that is older than the version that deprecated an
    API, SA1019 will no longer flag the use even if there is already an alternative
    available in the targeted Go version.
    
    For example, math/rand.Seed has been deprecated in Go 1.20, but an alternative
    has existed since Go 1.0. In the past, we would flag uses of Seed even if
    targeting e.g. Go 1.19, to encourage better forwards compatibility. This can
    lead to unnecessary churn, however, because the correct change may depend on the
    Go version in use. For example, for Seed before Go 1.20, the alternative is to
    use a separate instance of math/rand.Rand, whereas in Go 1.20, a possible
    alternative is to simply drop the call to Seed.
    
    Staticcheck 2023.1 (v0.4.0)
    
    Staticcheck 2023.1 adds support for Go 1.20, brings minor improvements to
    various checks, and replaces U1000 with a new implementation.
    
    The following checks have been improved:
    
    *   The wording of S1001 has been made clearer for cases involving arrays.
        Furthermore, it no longer suggests using copy when the function has been
        shadowed.
    *   S1011 now recognizes index-based loops.
    *   SA1019 no longer flags tests (internal or external) that use deprecated API
        from the package under test. Furthermore, entire declaration groups (such as
        groups of constants) can now be marked as deprecated.
    *   SA4017 now detects more functions, including those in the time package.
        Additionally, its wording has been made clearer.
    *   SA5010 no longer gets confused by type assertions involving generic types.
    *   ST1005 no longer flags errors that start with alpha-numeric acronyms such as
        P384.
    *   Improvements to our intermediate representation may allow various checks to
        find more problems.
    
    Staticcheck now knows about version 2 of the k8s.io/klog package, in particular
    which functions abort control flow.
    
    In addition to these minor improvements, U1000 has been rewritten from the
    ground up, operating on a program representation more suited to the task. In
    practice this means that there will be fewer false positives and more true
    positives.
    
    Overall, the rewrite fixes at least eight known bugs, both ones that have been a
    nuisance for a while, as well as ones newly introduced by generics.

--- devel/go-staticcheck/go-modules.mk
+++ devel/go-staticcheck/go-modules.mk
@@ -1,14 +1,12 @@
-# $NetBSD: go-modules.mk,v 1.2 2022/04/17 19:09:40 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.3 2023/07/09 19:10:21 bsiegert Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.4.1.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.4.1.zip
-GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20220218215828-6cf2b201936e.mod
-GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20220218215828-6cf2b201936e.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.6.0-dev.0.20220106191415-9b9b3d81d5e3.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.6.0-dev.0.20220106191415-9b9b3d81d5e3.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20211019181941-9d821ace8654.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20211019181941-9d821ace8654.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.1.11-0.20220316014157-77aa08bb151a.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.1.11-0.20220316014157-77aa08bb151a.zip
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20200804184101-5ec99f83aff1.mod
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20200804184101-5ec99f83aff1.zip
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.zip
+GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20221208152030-732eee02a75a.mod
+GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20221208152030-732eee02a75a.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.7.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.7.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.4.1-0.20221208213631-3f74d914ae6d.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.4.1-0.20221208213631-3f74d914ae6d.zip
