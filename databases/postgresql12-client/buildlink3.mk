3aea093dbf96aad94f68b57c9479e5a03a996c33
nia <nia@pkgsrc.org>
2023-07-09 13:57:19 +0000

    postgresql-client: When dependening modules include libpq server headers, they can then attempt to include the gssapi headers if postgres was compiled with the gssapi option. In this case, include gssapi in the buildlink3 tree.
    Should fix the build of various modules on various platforms.

--- databases/postgresql12-client/buildlink3.mk
+++ databases/postgresql12-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/06/28 11:31:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/07/09 13:57:20 nia Exp $
 
 BUILDLINK_TREE+=	postgresql12-client
 
@@ -21,6 +21,13 @@ BUILDLINK_FILES.postgresql12-client+=	bin/pg_config
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
+
+pkgbase := postgresql12-client
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql12-client:Mgssapi}
+.  include "../../mk/krb5.buildlink3.mk"
+.endif
 .endif # POSTGRESQL12_CLIENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-postgresql12-client
