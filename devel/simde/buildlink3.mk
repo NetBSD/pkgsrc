ba14386c58e52de8afdf7231e2c1a2388244b332
nia <nia@pkgsrc.org>
2023-07-09 08:28:37 +0000

    simde: Depending packages must be built with a c99 compiler.

--- devel/simde/buildlink3.mk
+++ devel/simde/buildlink3.mk
@@ -1,9 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/03/28 19:46:09 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/09 08:28:37 nia Exp $
 
 BUILDLINK_TREE+=	simde
 
 .if !defined(SIMDE_BUILDLINK3_MK)
 SIMDE_BUILDLINK3_MK:=
+
+# .buildlink/include/simde/x86/sse2.h:5293:3:
+# error: 'for' loop initial declarations are only allowed in C99 mode
+USE_LANGUAGES+=	c99
 
 BUILDLINK_API_DEPENDS.simde+=	simde>=0.7.2
 BUILDLINK_PKGSRCDIR.simde?=	../../devel/simde
