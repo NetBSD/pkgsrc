6445c241ab1225b6242a8ee58df1340aa4b794e0
nia <nia@pkgsrc.org>
2023-07-09 15:39:45 +0000

    libretro-flycast: graphics library is a required option, pkgsrc wants at least one option to be available

--- emulators/libretro-flycast/options.mk
+++ emulators/libretro-flycast/options.mk
@@ -1,14 +1,20 @@
-# $NetBSD: options.mk,v 1.2 2023/07/02 17:32:30 nia Exp $
+# $NetBSD: options.mk,v 1.3 2023/07/09 15:39:45 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libretro-flycast
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if !empty(MACHINE_ARCH:M*arm*)
-PKG_SUPPORTED_OPTIONS+=		rpi
+PKG_OPTIONS_REQUIRED_GROUPS+=	gl
+PKG_OPTIONS_GROUP.gl+=		opengl
+
+.if ${OPSYS} == "NetBSD" && !empty(MACHINE_ARCH:M*arm*)
+PKG_OPTIONS_GROUP.gl+=		rpi
 .endif
+
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-earmv6hf)
 PKG_SUGGESTED_OPTIONS+=		rpi
+.elif ${OPSYS} != "Darwin"
+PKG_SUGGESTED_OPTIONS+=		opengl
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -18,7 +24,8 @@ CMAKE_ARGS+=	-DUSE_VIDEOCORE=ON
 CMAKE_ARGS+=	-DUSE_GLES2=ON
 CMAKE_ARGS+=	-DUSE_OPENGL=OFF
 .  include "../../misc/raspberrypi-userland/buildlink3.mk"
-.else
+.elif !empty(PKG_OPTIONS:Mopengl)
+CMAKE_ARGS+=	-DUSE_OPENGL=ON
 .  if ${OPSYS} != "Darwin"
 .    include "../../graphics/MesaLib/buildlink3.mk"
 .  endif
