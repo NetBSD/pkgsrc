2df174cf7f33ba55de30780c6906d310f411ee87
nia <nia@pkgsrc.org>
2023-07-10 08:12:15 +0000

    skkfep: Bring package a bit closer to the 21st century.
    - Add missing termcap dependency.
    - Honour CFLAGS/LDFLAGS.
    - Don't prototype own libc/libtermcap functions.

--- inputmethod/skkfep/patches/patch-terms.c
+++ inputmethod/skkfep/patches/patch-terms.c
@@ -1,8 +1,30 @@
-$NetBSD: patch-terms.c,v 1.1 2012/12/20 21:50:47 joerg Exp $
+$NetBSD: patch-terms.c,v 1.2 2023/07/10 08:12:15 nia Exp $
 
---- terms.c.orig	2012-12-20 14:38:09.000000000 +0000
+--- terms.c.orig	1998-09-21 16:45:15.000000000 +0000
 +++ terms.c
-@@ -124,7 +124,7 @@ getTCstr()
+@@ -1,5 +1,7 @@
+ #include	"config.h"
+ #include	"fep.h"
++#include	<termcap.h>
++#include	<stdlib.h>
+ 
+ /*#define SHOW_ESCSEQ		/* show escape sequences for debugging */
+ 
+@@ -17,13 +19,8 @@ char	*T_TS, *T_FS, *T_SS,*T_HS,*T_ES;
+ int     columns, lines;
+ StatusType status;
+ 
+-extern char *getenv();
+ extern char ReverseStatus;
+ 
+-/* termcap functions */
+-extern char *tgetstr();
+-extern char *tgoto();
+-
+ #ifdef SHOW_ESCSEQ
+ printe(s,v)
+ char *s,*v;
+@@ -124,7 +121,7 @@ getTCstr()
  	}
  }
  
