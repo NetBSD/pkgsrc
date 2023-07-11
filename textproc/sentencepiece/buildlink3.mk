eb26eb3c8467356d72adf40720136b6aac5b4235
nia <nia@pkgsrc.org>
2023-07-11 06:09:29 +0000

    sentencepiece: Require a C++17 compiler the proper way.

--- textproc/sentencepiece/buildlink3.mk
+++ textproc/sentencepiece/buildlink3.mk
@@ -1,9 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/03/13 14:17:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/11 06:09:29 nia Exp $
 
 BUILDLINK_TREE+=	sentencepiece
 
 .if !defined(SENTENCEPIECE_BUILDLINK3_MK)
 SENTENCEPIECE_BUILDLINK3_MK:=
+
+# C++17
+GCC_REQD+=	7
 
 BUILDLINK_API_DEPENDS.sentencepiece+=	sentencepiece>=0.1.97
 BUILDLINK_PKGSRCDIR.sentencepiece?=	../../textproc/sentencepiece
