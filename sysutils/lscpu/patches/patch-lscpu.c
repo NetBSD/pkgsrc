9ccf0ac36f8af671561637b05b37fb4dd75eec45
vins <vins@pkgsrc.org>
2023-07-11 19:32:00 +0000

    sysutils/lscpu: fix CPU model detection on NetBSD.
    # CHANGES
    
    * Improve CPU model detection on NetBSD, by relying on machdep.cpu_brand
      sysctl identifier.
    * Mark package as x86/amd64 -only as it leverages on CPUID
      instruction.
    * Clarify description.
    * Bump revision.

--- sysutils/lscpu/patches/patch-lscpu.c
+++ sysutils/lscpu/patches/patch-lscpu.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-lscpu.c,v 1.1 2023/07/08 21:29:33 vins Exp $
+$NetBSD: patch-lscpu.c,v 1.2 2023/07/11 19:32:00 vins Exp $
 
-Fix architecture detection on FreeBSD.
+* Fix architecture detection on FreeBSD.
+* Fix CPU model detection on NetBSD.
 
 --- lscpu.c.orig	2018-05-05 02:25:31.000000000 +0000
 +++ lscpu.c
@@ -16,3 +17,18 @@ Fix architecture detection on FreeBSD.
          {HW_BYTEORDER, &(gen_info.byte_order), sizeof(gen_info.byte_order), "HW_BYTEORDER"},
          {HW_MODEL, gen_info.model, sizeof(gen_info.model), "HW_MODEL"},
          {HW_NCPU, &(gen_info.active_cpu_num), sizeof(gen_info.active_cpu_num), "HW_NCPU"},
+@@ -972,6 +976,14 @@ int main(int argc, char **argv) 
+         }
+     }
+ 
++#if defined(__NetBSD__) && (defined(__amd64__) || defined(__i386__))
++{
++       size_t sz = sizeof (gen_info.model);
++       if (sysctlbyname("machdep.cpu_brand", gen_info.model, &sz, NULL, 0) == -1)
++               err(1, "MACHDEP.CPU_BRAND");
++}
++#endif
++
+ #if defined(__amd64__) || defined(__i386__)
+     get_x86_cpu_info(&x86_info);
+ #endif
