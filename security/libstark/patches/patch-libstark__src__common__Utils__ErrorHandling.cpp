33b53189115777084fb9479926e897aa8045ccca
nia <nia@pkgsrc.org>
2023-07-12 21:19:27 +0000

    libstark: Include stdexcept for std::runtime_error.

--- security/libstark/patches/patch-libstark__src__common__Utils__ErrorHandling.cpp
+++ security/libstark/patches/patch-libstark__src__common__Utils__ErrorHandling.cpp
@@ -1,10 +1,20 @@
-$NetBSD: patch-libstark__src__common__Utils__ErrorHandling.cpp,v 1.1 2019/10/28 17:46:58 agc Exp $
+$NetBSD: patch-libstark__src__common__Utils__ErrorHandling.cpp,v 1.2 2023/07/12 21:19:27 nia Exp $
+
+Include stdexcept for std::runtime_error.
 
 backtrace() available on linux
 
---- libstark/src/common/Utils/ErrorHandling.cpp	2019/10/19 04:10:03	1.1
-+++ libstark/src/common/Utils/ErrorHandling.cpp	2019/10/19 04:10:31
-@@ -53,7 +53,7 @@
+--- libstark/src/common/Utils/ErrorHandling.cpp.orig	2018-12-11 08:57:02.000000000 +0000
++++ libstark/src/common/Utils/ErrorHandling.cpp
+@@ -9,6 +9,7 @@
+  */
+   /************************************************************************************************************/
+ #include <iostream>
++#include <stdexcept>
+ #ifndef WIN32
+ #include <unistd.h>
+ #endif
+@@ -53,7 +54,7 @@ void ErrorHandling::info(const stringstr
  }
  
  void ErrorHandling::printStacktrace() {
