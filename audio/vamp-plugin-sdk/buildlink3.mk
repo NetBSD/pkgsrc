55393b496fe6953cfb50855d602ac90a6952d61d
nia <nia@pkgsrc.org>
2023-07-13 13:45:41 +0000

    *: remove all instances of GCC_REQD where my name is the most recent in "cvs annotate" (part 1)
    Hopefully this commit can be reviewed later if a better replacement
    for GCC_REQD is committed.

--- audio/vamp-plugin-sdk/buildlink3.mk
+++ audio/vamp-plugin-sdk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/05/06 19:08:47 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/07/13 13:45:41 nia Exp $
 
 BUILDLINK_TREE+=	vamp-plugin-sdk
 
@@ -8,9 +8,6 @@ VAMP_PLUGIN_SDK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.vamp-plugin-sdk+=	vamp-plugin-sdk>=2.6
 BUILDLINK_ABI_DEPENDS.vamp-plugin-sdk?=	vamp-plugin-sdk>=2.10.0nb3
 BUILDLINK_PKGSRCDIR.vamp-plugin-sdk?=	../../audio/vamp-plugin-sdk
-
-# C++11
-GCC_REQD+=	4.7
 
 .include "../../audio/libsndfile/buildlink3.mk"
 .endif	# VAMP_PLUGIN_SDK_BUILDLINK3_MK
