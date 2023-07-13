ca06ab299689dca29fe909518a413b8bef16f583
nia <nia@pkgsrc.org>
2023-07-13 13:45:41 +0000

    *: remove all instances of GCC_REQD where my name is the most recent in "cvs annotate" (part 1)
    Hopefully this commit can be reviewed later if a better replacement
    for GCC_REQD is committed.

--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2021/11/11 12:02:46 kim Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/07/13 13:45:42 nia Exp $
 
 BUILDLINK_TREE+=	protobuf
 
@@ -8,8 +8,6 @@ PROTOBUF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
 BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=3.19.0nb1
 BUILDLINK_PKGSRCDIR.protobuf?=		../../devel/protobuf
-
-GCC_REQD+=	4.8
 
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# PROTOBUF_BUILDLINK3_MK
