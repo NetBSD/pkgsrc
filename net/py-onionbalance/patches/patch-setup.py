824084c07979125915e22888359334939e64aaec
adam <adam@pkgsrc.org>
2023-07-13 07:30:17 +0000

    py-onionbalance: updated to 0.2.1
    0.2.1
    v2 codebase now uses Cryptodome instead of the deprecated PyCrypto library.
    v3 codebase is now more flexible when it comes to requiring a live consensus. This should increase the reachability of Onionbalance in scenarios where the network is having trouble establishing a new consensus.
    v3 support for connecting to the control port through a Unix socket. Patch by Peter Tripp.
    Introduce status socket support for v3 onions. Patch by vporton.
    Sending a SIGHUP signal now reloads the v3 config. Patch by Peter Chung.

--- net/py-onionbalance/patches/patch-setup.py
+++ net/py-onionbalance/patches/patch-setup.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-setup.py,v 1.2 2020/05/12 18:40:35 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2023/07/13 07:30:18 adam Exp $
 
-Replace pycrypto with pycryptodome.
+Replace pycryptodomex with pycryptodome.
 
---- setup.py.orig	2020-04-14 22:49:04.000000000 +0000
+--- setup.py.orig	2021-01-22 15:01:09.000000000 +0000
 +++ setup.py
 @@ -46,7 +46,7 @@ setup(
          'setuptools',
          'stem>=1.8',
          'PyYAML>=4.2b1',
--        'pycrypto>=2.6.1',
+-        'pycryptodomex',
 +        'pycryptodome',
          'future>=0.14.0',
          'setproctitle',
