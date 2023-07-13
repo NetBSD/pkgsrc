67548ea87579e4bfc42ad3b4034a50c9c07c46eb
nia <nia@pkgsrc.org>
2023-07-13 13:45:41 +0000

    *: remove all instances of GCC_REQD where my name is the most recent in "cvs annotate" (part 1)
    Hopefully this commit can be reviewed later if a better replacement
    for GCC_REQD is committed.

--- devel/cppunit/buildlink3.mk
+++ devel/cppunit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/10/25 12:56:55 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/07/13 13:45:41 nia Exp $
 
 BUILDLINK_TREE+=	cppunit
 
@@ -9,8 +9,6 @@ BUILDLINK_API_DEPENDS.cppunit+=	cppunit>=1.10.2
 BUILDLINK_ABI_DEPENDS.cppunit+=	cppunit>=1.10.2nb5
 BUILDLINK_PKGSRCDIR.cppunit?=	../../devel/cppunit
 BUILDLINK_CPPFLAGS.cppunit+=	-I${BUILDLINK_PREFIX.cppunit}/include/cppunit
-
-GCC_REQD+=	4.8
 .endif # CPPUNIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cppunit
