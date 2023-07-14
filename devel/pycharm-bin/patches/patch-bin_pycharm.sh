507fae83da2dc8a1ee9182b79ae1d07a1d603525
hauke <hauke@pkgsrc.org>
2023-07-14 12:36:12 +0000

    Update devel/pycharm-bin to 2022.1.4
    Newer versions are built with a java version newer than what we ship,
    leading to binary incompatibility.
    
    This version breaks with java 17, see
    <https://intellij-support.jetbrains.com/hc/en-us/community/posts/5961640280210-Can-t-start-IntelliJ-Idea-Error-LinkageError-occurred-while-loading-main-class-com-intellij-idea-Main>,
    and unfortunately the pkgsrc USE_JAVA2 is "version N or newer", which
    does not help here.
    
    Changes are too numerous to list, see
    <https://youtrack.jetbrains.com/articles/PY-A-233537986/PyCharm-2022.1.4-221.6008.17-build-Release-Notes> etc.
    ("Neue Schraube! Ganz neue Schraube!")

--- devel/pycharm-bin/patches/patch-bin_pycharm.sh
+++ devel/pycharm-bin/patches/patch-bin_pycharm.sh
@@ -1,24 +1,16 @@
-$NetBSD: patch-bin_pycharm.sh,v 1.1 2019/03/03 18:02:47 youri Exp $
+$NetBSD: patch-bin_pycharm.sh,v 1.2 2023/07/14 12:36:13 hauke Exp $
 
-Add JVM options for a typical NetBSD setup.
+Run IDE with pkgsrc choice of JVM
 
---- bin/pycharm.sh.orig	2019-02-26 09:00:10.000000000 +0000
+--- bin/pycharm.sh.orig	2022-07-20 20:50:22.000000000 +0000
 +++ bin/pycharm.sh
-@@ -21,6 +21,7 @@ message()
-   fi
- }
+@@ -49,6 +49,9 @@ CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/
+ # ---------------------------------------------------------------------
+ JRE=""
  
-+PYCHARM_JDK=@PKG_JAVA_HOME@
- UNAME=`which uname`
- GREP=`which egrep`
- GREP_OPTIONS=""
-@@ -202,6 +203,9 @@ fi
- IFS="$(printf '\n\t')"
- "$JAVA_BIN" \
-   -classpath "$CLASSPATH" \
-+  -Xmx4096 -Xms4096 \
-+  -XX:-UseCompressedClassPointers \
-+  -XX:-UseCompressedOops \
-   ${VM_OPTIONS} \
-   "-XX:ErrorFile=$HOME/java_error_in_PYCHARM_%p.log" \
-   "-XX:HeapDumpPath=$HOME/java_error_in_PYCHARM.hprof" \
++# pkgsrc choice
++PYCHARM_JDK="@PKG_JAVA_HOME@"
++
+ # shellcheck disable=SC2154
+ if [ -n "$PYCHARM_JDK" ] && [ -x "$PYCHARM_JDK/bin/java" ]; then
+   JRE="$PYCHARM_JDK"
