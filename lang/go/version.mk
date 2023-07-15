ab9f637aa97bf8afd8ba2083d816c4167e06966f
bsiegert <bsiegert@pkgsrc.org>
2023-07-15 10:40:37 +0000

    go120: update to 1.20.6 (security)
    This minor release includes 1 security fix following the security policy:
    
    net/http: insufficient sanitization of Host header
    
    The HTTP/1 client did not fully validate the contents of the Host header. A
    maliciously crafted Host header could inject additional headers or entire
    requests. The HTTP/1 client now refuses to send requests containing an invalid
    Request.Host or Request.URL.Host value.
    
    Thanks to Bartek Nowotarski for reporting this issue.
    
    Includes security fixes for CVE-2023-29406 and Go issue
    https://go.dev/issue/60374

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.182 2023/07/15 10:35:14 bsiegert Exp $
+# $NetBSD: version.mk,v 1.183 2023/07/15 10:40:37 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,7 +6,7 @@
 #
 .include "go-vars.mk"
 
-GO120_VERSION=	1.20.5
+GO120_VERSION=	1.20.6
 GO119_VERSION=	1.19.11
 GO118_VERSION=	1.18.10
 GO14_VERSION=	1.4.3
