9327012f72b3cd157dbfe2dd954e046177ae0627
tnn <tnn@pkgsrc.org>
2023-07-17 19:39:56 +0000

    py-scipy: update patch comment

--- math/py-scipy/patches/patch-scipy_spatial___ckdtree.pyx
+++ math/py-scipy/patches/patch-scipy_spatial___ckdtree.pyx
@@ -1,7 +1,7 @@
-$NetBSD: patch-scipy_spatial___ckdtree.pyx,v 1.3 2023/07/17 19:26:37 tnn Exp $
+$NetBSD: patch-scipy_spatial___ckdtree.pyx,v 1.4 2023/07/17 19:39:56 tnn Exp $
 
 Fix missing isnan/isinf.
-Build breaks on at least NetBSD/aarch64 without this:
+Build breaks on at least NetBSD-10.99.4-aarch64 without this:
 
 scipy/spatial/_ckdtree.cxx: In function 'PyObject* __pyx_pf_5scipy_7spatial_8_ckdtree_7cKDTree_16count_neighbors(__pyx_obj_5scipy_7spatial_8_ckdtree_cKDTree*, __pyx_obj_5scipy_7spatial_8_ckdtree_cKDTree*, PyObject*, __pyx_t_5numpy_float64_t, PyObject*, int)':
 scipy/spatial/_ckdtree.cxx:13392:19: error: 'isinf' was not declared in this scope; did you mean 'std::isinf'?
