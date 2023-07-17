98943173a68b86bdb1fcf67b73edd7ce37827836
triaxx <triaxx@pkgsrc.org>
2023-07-17 19:41:15 +0000

    openrct2: Update to 0.2.5
    pkgsrc changes:
    ---------------
      * Remove patches which have been merged upstream.
      * Update versions incrementally since the last one does not work.
    
    upstream changes:
    -----------------
    0.2.5 (2020-03-24)
    ------------------------------------------------------------------------
    - Feature: [#3154] Use a random title sequence each time it is shown.
    - Feature: [#6553] Android version now runs in full screen.
    - Feature: [#7865] Transport rides can now be synchronised.
    - Feature: [#9073] Shortcut keys for the Tile Inspector.
    - Feature: [#10305] Add two shortcuts for increasing and decreasing the scaling factor.
    - Feature: [#10189] Make Track Designs work in multiplayer.
    - Feature: [#10357] Added window for scenery scatter tool, allowing for area and density selection.
    - Feature: [#10637] Console command to remove all floating objects.
    - Change: [#1164] Use available translations for shortcut key bindings.
    - Change: [#10997] Speed is automatically reset to normal upon scenario completion.
    - Fix: [#2485] Hide Vertical Faces not applied to the edges of water.
    - Fix: [#5249] No collision detection when building ride entrance at heights > 85.5m.
    - Fix: [#6766] Changelog window doesn't open on some platforms.
    - Fix: [#7784] Vehicle tab takes 1st car colour instead of tab_vehicle's colour.
    - Fix: [#7854] Cannot build a custom spiral roller coaster design.
    - Fix: [#7854] Empty entries in spiral roller coaster designs list.
    - Fix: [#8151] Game freezes upon demolishing mazes at odd heights.
    - Fix: [#8875] RCT1 competition scenarios are classified incorrectly.
    - Fix: [#10176] Mistake in the sprite for the land tool's 6x6 grid.
    - Fix: [#10196] Doors unable to be placed at end of track corners.
    - Fix: [#10228] Can't import RCT1 Deluxe from Steam.
    - Fix: [#10313] Path furniture can be placed on level crossings.
    - Fix: [#10325] Crash when banners have no text.
    - Fix: [#10376] No ratings generated when a shop and track intersect.
    - Fix: [#10420] Money effect causing false positive desync.
    - Fix: [#10477] Large Scenery cannot be placed higher using SHIFT.
    - Fix: [#10489] Hosts last player action not being synchronized.
    - Fix: [#10543] Secondary shop item prices are not imported correctly from RCT1 saves.
    - Fix: [#10547] RCT1 parks have too many rides available.
    - Fix: [#10587] Update last action coordinates on correct player.
    - Fix: [#10631] Game bugs out and crashes if you get too many stations via copying stations with the tile inspector.
    - Fix: [#10662] Duck cheat tooltips look odd and do not explain anything.
    - Fix: [#10694] The lift hill speed of the flying roller coaster cannot be changed (original bug).
    - Fix: [#10705] Apply multithreaded rendering to all viewports.
    - Fix: [#10739] Mountain tool overlay for even-numbered selections.
    - Fix: [#10752] Mute button state not correctly set at startup.
    - Fix: [#10822] Can place too many peep spawns.
    - Fix: [#10898] Banner text has an offset in tile inspector window.
    - Fix: [#10904] RCT1/LL-scenarios with red water won't open.
    - Fix: [#10941] The Clear Scenery tool gives refunds for ghost elements.
    - Fix: [#10963] Light effects are drawn off-centre in some rotations.
    - Fix: [#10993] Bottom toolbar not refreshing when a guest leaves the park.
    - Fix: [#11001] Rides list does not use natural sorting.
    - Fix: [objects#54] Stage Coach cars are not considered covered by the game.
    - Fix: [objects#56] Handymen cut grass incorrectly.
    - Improved: [#682] The staff patrol area is now drawn on the water, instead of on the surface under water.
    - Improved: [#10858] Added horizontal grid lines to finance charts.
    - Improved: [#10884] Added y-axes and labels to park window charts.
    - Improved: [#10970] Introduced optional light effects for vehicles at night.
    - Removed: [#6898] LOADMM and LOADRCT1 title sequence commands (use LOADSC instead).

--- games/openrct2/patches/patch-src_openrct2_common.h
+++ games/openrct2/patches/patch-src_openrct2_common.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_openrct2_common.h,v 1.1 2022/12/15 19:26:35 he Exp $
+$NetBSD: patch-src_openrct2_common.h,v 1.2 2023/07/17 19:41:15 triaxx Exp $
 
 Take a stab at supporting big-endian ports.
 
 --- src/openrct2/common.h.orig	2019-10-28 20:18:47.000000000 +0000
 +++ src/openrct2/common.h
-@@ -73,6 +73,12 @@ const constexpr auto ror64 = ror<uint64_
+@@ -92,6 +92,12 @@ const constexpr auto ror64 = ror<uint64_
  #        define HIBYTE(w) ((uint8_t)(((uint16_t)(w) >> 8) & 0xFF))
  #    endif // __BYTE_ORDER__
  
 +#    if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-+#	 define RCT2_ENDIANESS __ORDER_BIG_ENDIAN__
-+#	 define LOBYTE(w) ((uint8_t)(((uint16_t)(w) >> 8) & 0xFF))
-+#	 define HIBYTE(w) ((uint8_t)(w))
++#        define RCT2_ENDIANESS __ORDER_BIG_ENDIAN__
++#        define LOBYTE(w) ((uint8_t)(((uint16_t)(w) >> 8) & 0xFF))
++#        define HIBYTE(w) ((uint8_t)(w))
 +#    endif
 +
  #    ifndef RCT2_ENDIANESS
