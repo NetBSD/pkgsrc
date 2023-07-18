f177a7c166f21807ab2f9f84455e313a4444f144
ryoon <ryoon@pkgsrc.org>
2023-07-18 19:33:31 +0000

    MesaLib: Restrict elf-tls option for NetBSD 10.99.4 or later
    * elf-tls option is disable by default for NetBSD.
    * Remove problematic workaround for older NetBSD.
      Already removed in xsrc.

--- graphics/MesaLib/patches/patch-src_glx_glxcurrent.c
+++ graphics/MesaLib/patches/patch-src_glx_glxcurrent.c
@@ -1,33 +1,10 @@
-$NetBSD: patch-src_glx_glxcurrent.c,v 1.5 2022/03/13 15:50:05 tnn Exp $
-
-Interim fix for toolchain/50277.
+$NetBSD: patch-src_glx_glxcurrent.c,v 1.6 2023/07/18 19:33:31 ryoon Exp $
 
 NetBSD only supports zero-initialized initial-exec tls variables in conjuction
 with dlopen(3) at the moment.
 
 --- src/glx/glxcurrent.c.orig	2021-08-04 18:49:29.150474000 +0000
 +++ src/glx/glxcurrent.c
-@@ -40,6 +40,20 @@
- #include "glx_error.h"
- 
- /*
-+ * MASSIVE KLUDGE!
-+ * We need these to not be extern in libGL.so because of
-+ * PR toolchain/50277
-+ */
-+#if defined(USE_ELF_TLS) && defined(__NetBSD__)
-+#define u_current_table _glapi_tls_Dispatch
-+#define u_current_context _glapi_tls_Context
-+_X_EXPORT __thread struct _glapi_table *u_current_table
-+    __attribute__((tls_model("initial-exec"))) = NULL;
-+_X_EXPORT __thread void * u_current_context
-+    __attribute__((tls_model("initial-exec")));
-+#endif
-+
-+/*
- ** We setup some dummy structures here so that the API can be used
- ** even if no context is current.
- */
 @@ -76,7 +90,11 @@ _X_HIDDEN pthread_mutex_t __glXmutex = P
   * \b never be \c NULL.  This is important!  Because of this
   * \c __glXGetCurrentContext can be implemented as trivial macro.
