d91ca63f11ffebf763d25088e8617482c7b23bd5
nia <nia@pkgsrc.org>
2023-07-18 14:11:14 +0000

    devel: Adapt packages to use USE_(CC|CXX)_FEATURES

--- devel/libebml/buildlink3.mk
+++ devel/libebml/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.21 2020/06/29 15:08:26 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2023/07/18 14:11:16 nia Exp $
 
 BUILDLINK_TREE+=	libebml
 
 .if !defined(LIBEBML_BUILDLINK3_MK)
 LIBEBML_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=		c++14
 
 BUILDLINK_API_DEPENDS.libebml+=	libebml>=0.8
 BUILDLINK_ABI_DEPENDS.libebml+=	libebml>=1.4.0
