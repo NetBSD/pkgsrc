abf55671225e1e481d630c73229ad738297f3dae
nia <nia@pkgsrc.org>
2023-07-18 14:11:14 +0000

    devel: Adapt packages to use USE_(CC|CXX)_FEATURES

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2022/06/28 11:31:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2023/07/18 14:11:15 nia Exp $
 
 BUILDLINK_TREE+=	glibmm
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.24.2
 BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.66.2nb2
 BUILDLINK_PKGSRCDIR.glibmm?=	../../devel/glibmm
 
-GCC_REQD+=	4.9
+USE_CXX_FEATURES+=	regex c++11
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libsigc++/buildlink3.mk"
--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/07/13 13:45:42 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/07/18 14:11:17 nia Exp $
 
 BUILDLINK_TREE+=	protobuf
 
 .if !defined(PROTOBUF_BUILDLINK3_MK)
 PROTOBUF_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++11
 
 BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
 BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=3.19.0nb1
