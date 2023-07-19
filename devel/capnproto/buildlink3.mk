4603b279068023c515701be2106686052fb262e6
nia <nia@pkgsrc.org>
2023-07-19 14:49:45 +0000

    capnproto: Require a C++14 compiler, as the configure script does.

--- devel/capnproto/buildlink3.mk
+++ devel/capnproto/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/06/29 08:25:35 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/07/19 14:49:45 nia Exp $
 
 BUILDLINK_TREE+=	capnproto
 
 .if !defined(CAPNPROTO_BUILDLINK3_MK)
 CAPNPROTO_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++14
 
 BUILDLINK_API_DEPENDS.capnproto+=	capnproto>=0.8.0
 BUILDLINK_PKGSRCDIR.capnproto?=		../../devel/capnproto
