fa70e250cf8eab659dc11d3a89e989920f0d8085
adam <adam@pkgsrc.org>
2023-07-19 07:16:58 +0000

    libadwaita: updated to 1.2.4
    Version 1.2.4
    =============
    
    - AdwAboutWindow
      - Add a missing Since annotation
    - AdwCarousel
      - Fix allocation
    - AdwFlap
      - Add a missing setter annotation for :fold-policy
      - Correctly measure separator
      - Avoid notify emissions in dispose()
    - AdwTabBar
      - Fix clipped labels
    - Demo
      - Open primary menu with F10
    
    =============
    Version 1.2.3
    =============
    
    - AdwEntryRow
      - Fix :disabled styles
    - AdwExpanderRow
      - Expose expanded state for a11y
    - Memory leak fixes
    
    =============
    Version 1.2.2
    =============
    
    - AboutWindow
      - Prevent double clicks from opening troubleshooting page
    - AdwLeaflet
      - Fix can-unfold=false
    - AdwSpringAnimation
      - Fix critical damping velocity
    - AdwSwipeable
      - Fix get_swipe_area() fallback
    - AdwToastOverlay
      - Don't focus buttons on click
      - Prevent toast labels from wrapping
    - Demo
      - Fix the switch on the avatar page
    - Stylesheet
      - Fix GtkLevelBar fill colors
    - Fix dependency names in docs
    - Translation updates
      - French
    
    =============
    Version 1.2.1
    =============
    
    - AdwActionRow
      - Fix spacing after removing all prefixes/suffixes
    - AdwAvatar
      - Correctly redraw on custom image changes
    - AdwEntryRow
      - Respect use-markup property
      - Fix error/warning/success styles
      - Fix spacing after removing all prefixes/suffixes
    - AdwExpanderRow
      - Fix spacing after removing all prefixes/suffixes
    - AdwFlap
      - Fix natural width with fold-policy=never
    - AdwMessageDialog
      - Fix focus styles in RTL
    - AdwPreferencesWindow
      - Fix a memory leak
    - AdwSplitButton
      - Don't make dropdown insensitive when the button is
    - AdwTabBar
      - Fix focus handling
      - Fix autoscroll for non-local drags
    - AdwToastOverlay
      - Clarify documentation
    - AdwViewSwitcherBar
      - Fix typos in examples
    - AdwViewSwitcherTitle
      - Fix typos in examples
    - Stylesheet
      - Fix GtkSpinButton inside toolbars
    - Translation updates
      - Abkhazian
      - British English
      - Croatian
      - Friulian
      - German
      - Greek
      - Hebrew
      - Indonesian
      - Persian
      - Slovak
      - Turkish
    
    =============
    Version 1.2.0
    =============
    
    - Docs
      - Fix @card_shade_color description
      - Fix a typo in AdwTabView docs
      - Fix AdwMessageDialog example
    - Translation updates
      - Bulgarian
      - Croatian
      - Czech
      - German
      - Hungarian
      - Occitan
      - Slovenian

--- devel/libadwaita/buildlink3.mk
+++ devel/libadwaita/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/01/29 21:14:29 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/07/19 07:16:58 adam Exp $
 
 BUILDLINK_TREE+=	libadwaita
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libadwaita
 LIBADWAITA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libadwaita+=	libadwaita>=1.0.5
-BUILDLINK_ABI_DEPENDS.libadwaita?=	libadwaita>=1.1.3nb2
+BUILDLINK_ABI_DEPENDS.libadwaita+=	libadwaita>=1.1.3nb2
 BUILDLINK_PKGSRCDIR.libadwaita?=	../../devel/libadwaita
 
 .include "../../devel/glib2/buildlink3.mk"
