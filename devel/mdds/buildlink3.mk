23f02b74c80ee81b4f04f538889b235970ca0e93
nia <nia@pkgsrc.org>
2023-07-19 14:53:20 +0000

    mdds: Require a C++17 compiler, as the configure script does.

--- devel/mdds/buildlink3.mk
+++ devel/mdds/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/02/05 04:00:52 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/07/19 14:53:20 nia Exp $
 
 BUILDLINK_TREE+=	mdds
 
 .if !defined(MDDS_BUILDLINK3_MK)
 MDDS_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++17
 
 BUILDLINK_API_DEPENDS.mdds+=	mdds>=2.0.1
 BUILDLINK_PKGSRCDIR.mdds?=	../../devel/mdds
