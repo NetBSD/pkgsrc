7ee637572e35a2d624e59a70a33eb14fa7496489
nia <nia@pkgsrc.org>
2023-07-20 09:46:33 +0000

    *: Convert some remaining usage of GCC_REQD.

--- devel/googletest/buildlink3.mk
+++ devel/googletest/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/10/25 12:59:26 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/07/20 09:46:33 nia Exp $
 
 BUILDLINK_TREE+=	googletest
 
 .if !defined(GOOGLETEST_BUILDLINK3_MK)
 GOOGLETEST_BUILDLINK3_MK:=
 
+USE_CXX_FEATURES+=		c++14
+
 BUILDLINK_API_DEPENDS.googletest+=	googletest>=1.4.0
 BUILDLINK_DEPMETHOD.googletest?=	build
 BUILDLINK_PKGSRCDIR.googletest?=	../../devel/googletest
 
-GCC_REQD+=	4.8
 .endif	# GOOGLETEST_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-googletest
