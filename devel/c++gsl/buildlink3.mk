3aa0193d5a4375ded666e178358c48a13e826dd3
nia <nia@pkgsrc.org>
2023-07-20 21:45:26 +0000

    c++gsl: Require a C++14 compiler.

--- devel/c++gsl/buildlink3.mk
+++ devel/c++gsl/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/01/23 20:34:46 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/20 21:45:26 nia Exp $
 
 BUILDLINK_TREE+=	c++gsl
 
 .if !defined(C++GSL_BUILDLINK3_MK)
 C++GSL_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++14
 
 BUILDLINK_API_DEPENDS.c++gsl+=	c++gsl>=3.1.0
 BUILDLINK_PKGSRCDIR.c++gsl?=	../../devel/c++gsl
