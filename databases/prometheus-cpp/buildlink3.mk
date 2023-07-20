2e6411f267d77910744ad62d2840a014952a35f7
nia <nia@pkgsrc.org>
2023-07-20 21:40:53 +0000

    prometheus-cpp: Require a C++14 compiler.
    While here, option-ize building tests. They're not installed anyway.

--- databases/prometheus-cpp/buildlink3.mk
+++ databases/prometheus-cpp/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/06/26 11:39:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/20 21:40:53 nia Exp $
 
 BUILDLINK_TREE+=	prometheus-cpp
 
 .if !defined(PROMETHEUS_CPP_BUILDLINK3_MK)
 PROMETHEUS_CPP_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=		c++14
 
 BUILDLINK_DEPMETHOD.prometheus-cpp?=	build
 
