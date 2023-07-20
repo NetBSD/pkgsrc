f3382e3c34aaa8892eadcef02b5b2e0d33df68c4
nia <nia@pkgsrc.org>
2023-07-20 09:46:33 +0000

    *: Convert some remaining usage of GCC_REQD.

--- textproc/yaml-cpp/buildlink3.mk
+++ textproc/yaml-cpp/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/30 07:22:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/07/20 09:46:34 nia Exp $
 
 BUILDLINK_TREE+=	yaml-cpp
 
 .if !defined(YAML_CPP_BUILDLINK3_MK)
 YAML_CPP_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=			c++11
 
 BUILDLINK_API_DEPENDS.yaml-cpp+=	yaml-cpp>=0.5.0
 BUILDLINK_PKGSRCDIR.yaml-cpp?=		../../textproc/yaml-cpp
