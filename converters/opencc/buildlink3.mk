601dff68e180d3fd868eabeecc9a8f6283c4709b
nia <nia@pkgsrc.org>
2023-07-20 21:29:00 +0000

    opencc: Requires a C++14 compiler.

--- converters/opencc/buildlink3.mk
+++ converters/opencc/buildlink3.mk
@@ -1,10 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/02/20 13:56:07 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/07/20 21:29:00 nia Exp $
 #
 
 BUILDLINK_TREE+=	opencc
 
 .if !defined(OPENCC_BUILDLINK3_MK)
 OPENCC_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++14
 
 BUILDLINK_API_DEPENDS.opencc+=	opencc>=1.1.1
 BUILDLINK_PKGSRCDIR.opencc?=	../../converters/opencc
