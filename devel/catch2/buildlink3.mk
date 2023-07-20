3135b30fd590261f0426c3e706eec228da787d1d
nia <nia@pkgsrc.org>
2023-07-20 21:47:47 +0000

    catch2: Require a C++11 compiler.

--- devel/catch2/buildlink3.mk
+++ devel/catch2/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/02/03 12:35:13 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/20 21:47:47 nia Exp $
 
 BUILDLINK_TREE+=	catch2
 
 .if !defined(CATCH2_BUILDLINK3_MK)
 CATCH2_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++11
 
 BUILDLINK_DEPMETHOD.catch2?=	build
 BUILDLINK_API_DEPENDS.catch2+=	catch2>=2.11.1
