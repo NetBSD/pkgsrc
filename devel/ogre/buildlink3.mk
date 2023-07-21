1656b8fc32b82307bbcd337190b69b260c477b19
nia <nia@pkgsrc.org>
2023-07-21 08:30:48 +0000

    ogre: Require a compiler that supports put_time.

--- devel/ogre/buildlink3.mk
+++ devel/ogre/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.31 2021/03/01 12:12:14 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2023/07/21 08:30:48 nia Exp $
 
 BUILDLINK_TREE+=	ogre
 
 .if !defined(OGRE_BUILDLINK3_MK)
 OGRE_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++11 put_time
 
 BUILDLINK_API_DEPENDS.ogre+=	ogre>=1.12
 BUILDLINK_ABI_DEPENDS.ogre+=	ogre>=1.12
