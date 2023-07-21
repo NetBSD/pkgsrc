0deb4296e5507ce375689699de96baaa275f5e65
hauke <hauke@pkgsrc.org>
2023-07-21 16:03:38 +0000

    Hook up the graphics/libimagequant{,-c} alternative mechanism, modelled after graphics/librsvg{,-c}.

--- graphics/libimagequant/buildlink3.mk
+++ graphics/libimagequant/buildlink3.mk
@@ -1,12 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/05/22 11:37:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/07/21 16:03:38 hauke Exp $
 
 BUILDLINK_TREE+=	libimagequant
 
 .if !defined(LIBIMAGEQUANT_BUILDLINK3_MK)
 LIBIMAGEQUANT_BUILDLINK3_MK:=
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.include "../../graphics/libimagequant/available.mk"
+
+.if ${LIBIMAGEQUANT_TYPE:tl} == "rust"
 BUILDLINK_API_DEPENDS.libimagequant+=	libimagequant>=4.2.0
 BUILDLINK_PKGSRCDIR.libimagequant?=	../../graphics/libimagequant
+.elif ${LIBIMAGEQUANT_TYPE:tl} == "c"
+BUILDLINK_API_DEPENDS.libimagequant+=	libimagequant>=2.17.0<3.0.0
+BUILDLINK_PKGSRCDIR.libimagequant?=	../../graphics/libimagequant-c
+.else
+PKG_FAIL_REASON+=	"[graphics/libimagequant/buildlink3.mk] Invalid value ${LIBIMAGEQUANT_TYPE} for LIBIMAGEQUANT_TYPE."
+.endif
+
 .endif	# LIBIMAGEQUANT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libimagequant
