c1d445ae09af1804e73577a99abdc9b56c608386
nia <nia@pkgsrc.org>
2023-07-21 08:40:04 +0000

    libcutl: Add boost to buildlink3 so that depending packages are built with the same ABI.

--- devel/libcutl/buildlink3.mk
+++ devel/libcutl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/03/19 21:48:27 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/07/21 08:40:04 nia Exp $
 
 BUILDLINK_TREE+=	libcutl
 
@@ -7,6 +7,9 @@ LIBCUTL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcutl+=	libcutl>=1.10.0
 BUILDLINK_PKGSRCDIR.libcutl?=	../../devel/libcutl
+
+.include "../../devel/boost-headers/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
 .endif	# LIBCUTL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libcutl
