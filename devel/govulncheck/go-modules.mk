d39127a4a0aec1686a13fa7d8ee3246caed3bb3d
bsiegert <bsiegert@pkgsrc.org>
2023-07-23 09:58:36 +0000

    New package, govulncheck-1.0.0.
    Govulncheck is a low-noise tool that helps you find and fix vulnerable
    dependencies in your Go projects. It does this by scanning your
    project's dependencies for known vulnerabilities and then identifying
    any direct or indirect calls to those vulnerabilities in your code.
    
    To run govulncheck inside your module, run:
    
    govulncheck ./...

---
+++ devel/govulncheck/go-modules.mk
@@ -0,0 +1,19 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/07/23 09:58:36 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/google/go-cmdtest/@v/v0.4.1-0.20220921163831-55ab3332a786.mod
+GO_MODULE_FILES+=	github.com/google/go-cmdtest/@v/v0.4.1-0.20220921163831-55ab3332a786.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.3.1.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.8.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.8.zip
+GO_MODULE_FILES+=	github.com/google/renameio/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/google/renameio/@v/v0.1.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.10.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.10.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.1.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.1.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.7.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.7.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.8.1-0.20230421161920-b9619ee54b47.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.8.1-0.20230421161920-b9619ee54b47.zip
+GO_MODULE_FILES+=	mvdan.cc/unparam/@v/v0.0.0-20230312165513-e84e2d14e3b8.mod
+GO_MODULE_FILES+=	mvdan.cc/unparam/@v/v0.0.0-20230312165513-e84e2d14e3b8.zip
