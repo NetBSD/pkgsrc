826027199a5ee40982c39f36c2d606cd27576588
msaitoh <msaitoh@pkgsrc.org>
2023-07-24 15:07:57 +0000

    sysutils/dmidecode: update to 3.5.
    Changes from 3.4:
    
    Version 3.5 (Tue Mar 14 2023)
      - Decode HPE OEM records 216, 224, 230, 238 and 242.
      - Fortify entry point length checks.
      - Add a --no-quirks option.
      - Drop the CPUID exception list.
      - Do not let --dump-bin overwrite an existing file.
      - Ensure /dev/mem is a character device file.
      - Bug fixes:
        Fix segmentation fault in HPE OEM record 240
      - Minor improvements:
        Typo fixes
        Write the whole dump file at once
        Fix a build warning when USE_MMAP isn't set

--- sysutils/dmidecode/patches/patch-dmiopt.c
+++ sysutils/dmidecode/patches/patch-dmiopt.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-dmiopt.c,v 1.1 2022/11/28 04:26:08 mrg Exp $
+$NetBSD: patch-dmiopt.c,v 1.2 2023/07/24 15:07:57 msaitoh Exp $
 
 * Use /dev/smbios instead of /dev/mem on NetBSD if available.
 
---- dmiopt.c.orig	2022-06-27 08:06:32.000000000 -0700
-+++ dmiopt.c	2022-11-27 14:28:45.656425673 -0800
-@@ -368,7 +368,7 @@
+--- dmiopt.c.orig	2023-03-15 01:32:17.000000000 +0900
++++ dmiopt.c	2023-07-24 13:39:41.169153661 +0900
+@@ -372,7 +372,7 @@ void print_help(void)
  	static const char *help =
  		"Usage: dmidecode [OPTIONS]\n"
  		"Options are:\n"
@@ -12,8 +12,8 @@ $NetBSD: patch-dmiopt.c,v 1.1 2022/11/28 04:26:08 mrg Exp $
 +		" -d, --dev-mem FILE     Read memory from device FILE (default: %s)\n"
  		" -h, --help             Display this help text and exit\n"
  		" -q, --quiet            Less verbose output\n"
- 		" -s, --string KEYWORD   Only display the value of the given DMI string\n"
-@@ -381,5 +381,5 @@
+ 		"     --no-quirks        Decode everything without quirks\n"
+@@ -386,5 +386,5 @@ void print_help(void)
  		"     --oem-string N     Only display the value of the given OEM string\n"
  		" -V, --version          Display the version and exit\n";
  
