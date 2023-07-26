123e9744f2b07a11b56dc7255a8b554c43e62643
adam <adam@pkgsrc.org>
2023-07-26 08:46:53 +0000

    cmake cmake-gui: updated to 3.27.1
    CMake 3.27.1
    * Tests/FortranOnly: add a test case for issue
    * Tests/RunCMake/CXXModules: add a test for issue
    * cmComputeLinkInformation: track OBJECT library dependencies
    * cmTarget: Restore Fortran_PREPROCESS property initialization
    * FindOpenSSL: Prefer OPENSSL_ROOT_DIR only if non-empty
    * CTest: Fix regression in calculating test timeout
    * FindCUDAToolkit: Fix nvrtc_builtins library name
    * ctest: Update ctest_submit for CDash behavior change

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.40 2023/07/20 06:12:13 adam Exp $
+# $NetBSD: version.mk,v 1.41 2023/07/26 08:46:53 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.0
+CMAKE_VERSION=	3.27.1
 CMAKE_API=	${CMAKE_VERSION:R}
