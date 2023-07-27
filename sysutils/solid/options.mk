cdde9a8be3f5e956aa5c23736cecec8ca931474d
nia <nia@pkgsrc.org>
2023-07-27 03:08:20 +0000

    solid: udev must be enabled on linux, and it builds now, so enable it

--- sysutils/solid/options.mk
+++ sysutils/solid/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2023/07/22 08:32:34 nia Exp $
+# $NetBSD: options.mk,v 1.2 2023/07/27 03:08:20 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.solid
 PKG_SUPPORTED_OPTIONS=		udev
+PKG_SUGGESTED_OPTIONS.Linux=	udev
 
 .include "../../mk/bsd.options.mk"
 
