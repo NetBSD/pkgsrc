6f99be62410f31023f358cb578747333e70fe9f6
adam <adam@pkgsrc.org>
2023-07-29 11:45:43 +0000

    mongodb: updated to 6.0.8
    https://www.mongodb.com/docs/manual/release-notes/6.0/

--- databases/mongodb/patches/patch-src_third__party_wiredtiger_src_os__posix_os__fs.c
+++ databases/mongodb/patches/patch-src_third__party_wiredtiger_src_os__posix_os__fs.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_third__party_wiredtiger_src_os__posix_os__fs.c,v 1.2 2020/03/24 14:34:06 adam Exp $
+$NetBSD: patch-src_third__party_wiredtiger_src_os__posix_os__fs.c,v 1.3 2023/07/29 11:45:43 adam Exp $
 
 The fdatasync call is specified by POSIX, and the definition has changed:
   https://pubs.opengroup.org/onlinepubs/009695399/functions/fdatasync.html
@@ -9,14 +9,14 @@ crashing on EBADF, at the possible expense of sync safety.
 
 \todo Change NetBSD to follow the current standard.
 
---- src/third_party/wiredtiger/src/os_posix/os_fs.c.orig	2020-03-19 16:16:51.000000000 +0000
+--- src/third_party/wiredtiger/src/os_posix/os_fs.c.orig	2023-06-29 13:37:52.000000000 +0000
 +++ src/third_party/wiredtiger/src/os_posix/os_fs.c
-@@ -94,7 +94,7 @@ __posix_sync(WT_SESSION_IMPL *session, i
+@@ -89,7 +89,7 @@ __posix_sync(WT_SESSION_IMPL *session, i
  #if defined(HAVE_FDATASYNC)
      /* See comment in __posix_sync(): sync cannot be retried or fail. */
      WT_SYSCALL(fdatasync(fd), ret);
 -    if (ret == 0)
 +    if (ret == 0 || errno == EBADF)
          return (0);
-     WT_PANIC_RET(session, ret, "%s: %s: fdatasync", name, func);
+     WT_RET_PANIC(session, ret, "%s: %s: fdatasync", name, func);
  #else
