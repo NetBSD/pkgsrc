9f6a02ff40144234264eb65a601821596639223e
adam <adam@pkgsrc.org>
2023-07-30 14:41:05 +0000

    python37, py37-html-docs: removed; end of life; use Python 3.8, 3.9, 3.10 or 3.11

--- lang/python37/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.18 2023/06/07 13:23:58 adam Exp $
-
-PY_DISTVERSION=	3.7.17
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.xz
-DISTINFO_FILE=	${.CURDIR}/../../lang/python37/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python37/patches
-MASTER_SITES=	https://www.python.org/ftp/python/${PY_DISTVERSION}/
