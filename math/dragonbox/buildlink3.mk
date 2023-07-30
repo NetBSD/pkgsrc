c71d79ce3cfbd9d1aca092c5013b0875ac61fa68
nia <nia@pkgsrc.org>
2023-07-30 08:24:21 +0000

    dragonbox: Do not use USE_LANGUAGES to require a newer compiler but rather USE_CXX_FEATURES. It sets -std=gnu++17 on its own.
    Append USE_CXX_FEATURES, don't set it.

--- math/dragonbox/buildlink3.mk
+++ math/dragonbox/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/07/20 19:36:00 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/07/30 08:24:21 nia Exp $
 
 BUILDLINK_TREE+=	dragonbox
 
 .if !defined(DRAGONBOX_BUILDLINK3_MK)
 DRAGONBOX_BUILDLINK3_MK:=
 
-USE_CXX_FEATURES=			c++17 # is_unsigned_v
+USE_CXX_FEATURES+=			c++17 # is_unsigned_v
 
 BUILDLINK_API_DEPENDS.dragonbox+=	dragonbox>=1.1.3
 BUILDLINK_PKGSRCDIR.dragonbox?=		../../math/dragonbox
