42ba7701e7afa61bc192ee1e97ac8345833985ac
bacon <bacon@pkgsrc.org>
2023-07-31 14:34:18 +0000

    biology/rna-star: Spliced Transcripts Align to a Ref
    STAR (Spliced Transcripts Alignment to a Reference) aims to achieve
    accurate alignment of high-throughput RNA-seq data.  STAR is based on
    a previously undescribed RNA-seq alignment algorithm that uses
    sequential maximum mappable seed search in uncompressed suffix arrays
    followed by seed clustering and stitching procedure. STAR outperforms
    other aligners by a factor of >50 in mapping speed, aligning to the
    human genome 550 million 2 x 76 bp paired-end reads per hour on a modest
    12-core server, while at the same time improving alignment sensitivity
    and precision.

---
+++ biology/rna-star/patches/patch-SharedMemory.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-SharedMemory.cpp,v 1.1 2023/07/31 14:34:18 bacon Exp $
+
+# Portability
+
+--- SharedMemory.cpp.orig	2022-11-01 14:50:35.000000000 +0000
++++ SharedMemory.cpp
+@@ -12,7 +12,7 @@
+ #include <errno.h>
+ #include <sys/types.h>
+ 
+-#if defined(COMPILE_FOR_MAC) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
+   //some Mac's idiosyncrasies: standard SHM libraries are very old and missing some definitions
+   #define SHM_NORESERVE 0
+ #endif
