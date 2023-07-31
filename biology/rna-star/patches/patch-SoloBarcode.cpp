bbe537f6c98f11e81b64aee201e56cbd1417d7ad
bacon <bacon@pkgsrc.org>
2023-07-31 14:34:18 +0000

    biology/rna-star: Spliced Transcripts Align to a Ref
    STAR (Spliced Transcripts Alignment to a Reference) aims to achieve
    accurate alignment of high-throughput RNA-seq data.  STAR is based on
    a previously undescribed RNA-seq alignment algorithm that uses
    sequential maximum mappable seed search in uncompressed suffix arrays
    followed by seed clustering and stitching procedure. STAR outperforms
    other aligners by a factor of >50 in mapping speed, aligning to the
    human genome 550 million 2 x 76 bp paired-end reads per hour on a modest
    12-core server, while at the same time improving alignment sensitivity
    and precision.

---
+++ biology/rna-star/patches/patch-SoloBarcode.cpp
@@ -0,0 +1,21 @@
+$NetBSD: patch-SoloBarcode.cpp,v 1.1 2023/07/31 14:34:18 bacon Exp $
+
+# Portability
+
+--- SoloBarcode.cpp.orig	2023-07-23 17:22:42.613062682 +0000
++++ SoloBarcode.cpp
+@@ -3,6 +3,7 @@
+ #include "SoloCommon.h"
+ #include "SequenceFuns.h"
+ #include "ParametersSolo.h"
++#include <cmath>
+ 
+ void wlAddMismatches(uint32 nMM, uint32 cbLen, vector<uintCB> &wl, vector<uintCB> &wlEd1, vector<uint32> &wlInd1);
+ 
+@@ -149,4 +150,4 @@ void wlAddMismatches(uint32 nMM, uint32 
+             ++icb;
+         };
+     };
+-};
+\ No newline at end of file
++};
