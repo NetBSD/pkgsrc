c8ab82c7ca0f1b8c97c9f5a36b9376e083984d73
adam <adam@pkgsrc.org>
2023-08-01 12:33:17 +0000

    py-uwsgi: updated to 2.0.22
    uWSGI 2.0.22
    
    Changes
    
    Add graceful harakiri to give more slack to workers in order to do cleanup actions (Filipe Felisbino) The following options have been added:
    harakiri-graceful-timeout to control the timeout for the worker to attempt a graceful shutdown
    harakiri-graceful-signal, to choose which signal to use for graceful harakiri (default: SIGTERM)
    harakiri-graceful-queue-threshold in order to trigger harakiri only when the listen queue crosses a threshold
    plugins/php: Fix PHP 8.2 compilation (Alexandre Rossi)
    plugins/python: Use “backslashreplace” on stderr initialization (Nicolas Evrard)
    Fix typo (Young Ziyi)
    Fix use after free with DEBUG (Alexandre Rossi)
    apache2/mod_proxy_uwsgi: stricter backend HTTP response parsing/validation (Eric Covener, via Freexian)
    ci: update to ubuntu 20.04 since 18.04 is unsupported (Riccardo Magliocchetti)
    plugins/rack: fix compilation with Ruby 3.1, this breaks compilation for Ruby < 2.x (Lalufu, Riccardo Magliocchetti)

--- www/py-uwsgi/patches/patch-uwsgi.h
+++ www/py-uwsgi/patches/patch-uwsgi.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-uwsgi.h,v 1.10 2022/11/02 07:26:47 wiz Exp $
+$NetBSD: patch-uwsgi.h,v 1.11 2023/08/01 12:33:17 adam Exp $
 
 Avoid using "sun" which breaks on SunOS
 https://github.com/unbit/uwsgi/pull/2506
 
---- uwsgi.h.orig	2020-06-17 09:03:34.000000000 +0000
+--- uwsgi.h.orig	2023-07-27 15:34:12.000000000 +0000
 +++ uwsgi.h
-@@ -1631,7 +1635,7 @@ struct wsgi_request {
+@@ -1634,7 +1634,7 @@ struct wsgi_request {
  	union address {
  		struct sockaddr_in sin;
  		struct sockaddr_in6 sin6;
