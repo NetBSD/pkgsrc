3fd0a9007711ca77733a0ba0d8fac7b392bc9aab
adam <adam@pkgsrc.org>
2023-08-02 07:57:36 +0000

    GraphicsMagick: fix for libjpeg-turbo 3.0.0

--- graphics/GraphicsMagick/hacks.mk
+++ graphics/GraphicsMagick/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2014/08/14 15:23:59 jperkin Exp $
+# $NetBSD: hacks.mk,v 1.2 2023/08/02 07:57:36 adam Exp $
 
 .if !defined(GRAPHICSMAGICK_HACKS_MK)
 GRAPHICSMAGICK_HACKS_MK=	# defined
@@ -6,7 +6,7 @@ GRAPHICSMAGICK_HACKS_MK=	# defined
 # Gain 2-3X speedup by working around GCC optimisation bug
 # http://gcc.gnu.org/bugzilla/show_bug.cgi?id=53967
 .include "../../mk/compiler.mk"
-.if !empty(CC_VERSION:Mgcc-4.*) && ${MACHINE_ARCH} == "x86_64"
+.if ${CC_VERSION:Mgcc-4.*} && ${MACHINE_ARCH} == "x86_64"
 PKG_HACKS+=	frename-registers
 CFLAGS+=	-frename-registers
 .endif
