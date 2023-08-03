f074550459f98c21f254359d568614995564daca
ryoon <ryoon@pkgsrc.org>
2023-08-03 22:02:33 +0000

    firefox: Update to 116.0
    * speech-dispatcher is not tested yet.
    
    Changelog:
    New
    
      * Sidebar switcher allows users to access Bookmarks, History and Synced Tabs
        panels easily, quickly switch between them, move the sidebar to another
        side of the browser window, or close the sidebar. Now, keyboard users would
        be able to do it all with ease too, with or without any assistive
        technology running, without needing to memorize keyboard shortcuts to
        access these panels.
    
      * When an update is available in English locales, users will now have access
        to the release notes in the update notification prompt in the form of a
        "Learn More" link.
    
      * It is now possible to copy any file from your operating system and paste it
        into Firefox.
    
      * You asked, and we listened! The volume slider is now available in
        Picture-in-Picture.
    
      * We added the possibility to edit existing text annotations.
    
    Fixed
    
      * The upload performance of HTTP/2 has been significantly improved starting
        with Firefox 115.0, particularly on those with a higher bandwidth delay
        product (i.e., networks characterized by both high bandwidth and high
        latency).
    
      * Various security fixes.
    
    Changed
    
      * The keyboard shortcut to reopen closed tabs (command + shift + t) now
        reopens last closed tab or last closed window, in the order items were
        closed. If there aren't any tabs or windows to reopen, this command
        restores the previous session. This change is in anticipation of upcoming
        changes to recently closed tabs.
    
    Security fixes:
    #CVE-2023-4045: Offscreen Canvas could have bypassed cross-origin restrictions
    #CVE-2023-4046: Incorrect value used during WASM compilation
    #CVE-2023-4047: Potential permissions request bypass via clickjacking
    #CVE-2023-4048: Crash in DOMParser due to out-of-memory conditions
    #CVE-2023-4049: Fix potential race conditions when releasing platform objects
    #CVE-2023-4050: Stack buffer overflow in StorageManager
    #CVE-2023-4051: Full screen notification obscured by file open dialog
    #CVE-2023-4052: File deletion and privilege escalation through Firefox
     uninstaller
    #CVE-2023-4053: Full screen notification obscured by external program
    #CVE-2023-4054: Lack of warning when opening appref-ms files
    #CVE-2023-4055: Cookie jar overflow caused unexpected cookie jar state
    #CVE-2023-4056: Memory safety bugs fixed in Firefox 116, Firefox ESR 115.1,
     Firefox ESR 102.14, Thunderbird 115.1, and Thunderbird 102.14
    #CVE-2023-4057: Memory safety bugs fixed in Firefox 116, Firefox ESR 115.1, and
     Thunderbird 115.1
    #CVE-2023-4058: Memory safety bugs fixed in Firefox 116

--- www/firefox/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
+++ www/firefox/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
@@ -1,18 +1,17 @@
-$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.12 2019/03/19 16:11:28 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.13 2023/08/03 22:02:33 ryoon Exp $
 
 * Support NetBSD
 * Support Solaris (we can't rely on pthread_setname_np so ignore it).
 
---- ipc/chromium/src/base/platform_thread_posix.cc.orig	2019-03-07 16:53:35.000000000 +0000
+--- ipc/chromium/src/base/platform_thread_posix.cc.orig	2023-07-20 18:06:23.000000000 +0000
 +++ ipc/chromium/src/base/platform_thread_posix.cc
 @@ -12,7 +12,9 @@
- #if defined(OS_MACOSX)
+ #if defined(XP_DARWIN)
  #  include <mach/mach.h>
- #elif defined(OS_NETBSD)
--#  include <lwp.h>
+ #elif defined(XP_NETBSD)
 +_Pragma("GCC visibility push(default)")
-+#include <lwp.h>
+ #  include <lwp.h>
 +_Pragma("GCC visibility pop")
- #elif defined(OS_LINUX)
+ #elif defined(XP_LINUX)
  #  include <sys/syscall.h>
  #  include <sys/prctl.h>
