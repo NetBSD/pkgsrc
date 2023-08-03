06048fd4f8e59fd0ce9dbc5e15b6b9360298bf3e
markd <markd@pkgsrc.org>
2023-08-03 05:32:04 +0000

    ktextaddons: add version 1.3.2
    Various text handling addons

---
+++ textproc/ktextaddons/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/03 05:32:04 markd Exp $
+
+BUILDLINK_TREE+=	ktextaddons
+
+.if !defined(KTEXTADDONS_BUILDLINK3_MK)
+KTEXTADDONS_BUILDLINK3_MK:=
+
+
+BUILDLINK_API_DEPENDS.ktextaddons+=	ktextaddons>=1.3.2
+BUILDLINK_PKGSRCDIR.ktextaddons?=	../../wip/ktextaddons
+
+.include "../../archivers/karchive/buildlink3.mk"
+.include "../../devel/kcoreaddons/buildlink3.mk"
+.include "../../devel/ki18n/buildlink3.mk"
+.include "../../security/qt5-qtkeychain/buildlink3.mk"
+.include "../../x11/kconfigwidgets/buildlink3.mk"
+.include "../../x11/kxmlgui/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KTEXTADDONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ktextaddons
