af36b046651ecee2eb2aa32730aa110dc1ba3075
adam <adam@pkgsrc.org>
2023-08-04 19:23:30 +0000

    qt6-qtvirtualkeyboard: added version 6.5.2
    Qt Virtual Keyboard is a virtual keyboard framework that consists of a C++
    backend supporting custom input methods as well as a UI frontend implemented
    in QML.

---
+++ inputmethod/qt6-qtvirtualkeyboard/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/04 19:23:30 adam Exp $
+
+BUILDLINK_TREE+=	qt6-qtvirtualkeyboard
+
+.if !defined(QT6_QTVIRTUALKEYBOARD_BUILDLINK3_MK)
+QT6_QTVIRTUALKEYBOARD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt6-qtvirtualkeyboard+=	qt6-qtvirtualkeyboard>=6.5.2
+BUILDLINK_ABI_DEPENDS.qt6-qtvirtualkeyboard+=	qt6-qtvirtualkeyboard>=6.5.2
+BUILDLINK_PKGSRCDIR.qt6-qtvirtualkeyboard?=	../../inputmethod/qt6-qtvirtualkeyboard
+
+.include "../../graphics/qt6-qtsvg/buildlink3.mk"
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# QT6_QTVIRTUALKEYBOARD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt6-qtvirtualkeyboard
