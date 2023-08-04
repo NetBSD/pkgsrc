11043026f77848924bb79ae5609e6dff0f536994
adam <adam@pkgsrc.org>
2023-08-04 19:40:42 +0000

    qt6-qtsensors: added version 6.5.2
    The Qt Sensors module provides access to sensor hardware via QML and C++
    interfaces.

---
+++ misc/qt6-qtsensors/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/04 19:40:42 adam Exp $
+
+BUILDLINK_TREE+=	qt6-qtsensors
+
+.if !defined(QT6_QTSENSORS_BUILDLINK3_MK)
+QT6_QTSENSORS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt6-qtsensors+=	qt6-qtsensors>=6.5.2
+BUILDLINK_ABI_DEPENDS.qt6-qtsensors+=	qt6-qtsensors>=6.5.2
+BUILDLINK_PKGSRCDIR.qt6-qtsensors?=	../../misc/qt6-qtsensors
+
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# QT6_QTSENSORS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt6-qtsensors
