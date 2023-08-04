4746fd9b0dd84418c576c7b1d4d3694499463d8a
adam <adam@pkgsrc.org>
2023-08-04 19:37:00 +0000

    qt6-qtconnectivity: added version 6.5.2
    Qt6 Connectivity supports a range of connectivity options, with a focus on Qt
    Serial Bus, Qt Serial Port, Qt Bluetooth and Qt NFC.

---
+++ comms/qt6-qtconnectivity/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/04 19:37:01 adam Exp $
+
+BUILDLINK_TREE+=	qt6-qtconnectivity
+
+.if !defined(QT6_QTCONNECTIVITY_BUILDLINK3_MK)
+QT6_QTCONNECTIVITY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt6-qtconnectivity+=	qt6-qtconnectivity>=6.5.2
+BUILDLINK_ABI_DEPENDS.qt6-qtconnectivity+=	qt6-qtconnectivity>=6.5.2
+BUILDLINK_PKGSRCDIR.qt6-qtconnectivity?=	../../comms/qt6-qtconnectivity
+
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# QT6_QTCONNECTIVITY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt6-qtconnectivity
