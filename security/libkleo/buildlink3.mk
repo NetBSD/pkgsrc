fe61cb9825b7e68c0cb0bbb771905f4a46998a35
markd <markd@pkgsrc.org>
2023-08-05 00:29:38 +0000

    libkleo: add version 23.04.3
    KDE PIM cryptographic library

---
+++ security/libkleo/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/05 00:29:38 markd Exp $
+
+BUILDLINK_TREE+=	libkleo
+
+.if !defined(LIBKLEO_BUILDLINK3_MK)
+LIBKLEO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libkleo+=	libkleo>=20.04.1
+BUILDLINK_PKGSRCDIR.libkleo?=	../../security/libkleo
+
+.include "../../devel/kitemmodels/buildlink3.mk"
+.include "../../security/qgpgme/buildlink3.mk"
+.include "../../textproc/kpimtextedit/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# LIBKLEO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libkleo
