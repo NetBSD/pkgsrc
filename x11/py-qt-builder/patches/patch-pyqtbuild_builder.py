f22357bf36dc98a250ecdb3702086c4d6bf7dbcc
adam <adam@pkgsrc.org>
2023-08-05 06:16:27 +0000

    py-qt-builder: refine Darwin support

--- x11/py-qt-builder/patches/patch-pyqtbuild_builder.py
+++ x11/py-qt-builder/patches/patch-pyqtbuild_builder.py
@@ -1,9 +1,27 @@
-$NetBSD: patch-pyqtbuild_builder.py,v 1.1 2023/08/04 17:10:08 adam Exp $
+$NetBSD: patch-pyqtbuild_builder.py,v 1.2 2023/08/05 06:16:27 adam Exp $
 
 On Darwin, do not support application bundles.
 
---- pyqtbuild/builder.py.orig	2023-08-04 17:04:58.000000000 +0000
+--- pyqtbuild/builder.py.orig	2023-07-21 13:05:11.000000000 +0000
 +++ pyqtbuild/builder.py
+@@ -379,7 +379,7 @@ class QmakeBuilder(Builder):
+         else:
+             # Note some version of Qt5 (probably incorrectly) implements
+             # 'plugin_bundle' instead of 'plugin' so we specify both.
+-            pro_lines.append('CONFIG += plugin plugin_bundle no_default_rpath')
++            pro_lines.append('CONFIG += plugin no_default_rpath')
+ 
+         if project.qml_debug:
+             pro_lines.append('CONFIG += qml_debug')
+@@ -419,7 +419,7 @@ win32 {
+     PY_MODULE_SRC = $(DESTDIR_TARGET)
+ } else {
+     macx {
+-        PY_MODULE_SRC = $(TARGET).plugin/Contents/MacOS/$(TARGET)
++        PY_MODULE_SRC = $(TARGET)
+         QMAKE_LFLAGS += "-undefined dynamic_lookup"
+     } else {
+         PY_MODULE_SRC = $(TARGET)
 @@ -609,11 +609,7 @@ macx {
          else:
              makefile_target = None
