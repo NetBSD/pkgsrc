0c92601e589eb6f3ac78ed9ec89f8972d477a8c7
markd <markd@pkgsrc.org>
2023-08-05 06:05:13 +0000

    kontactinterface: add version 23.04.3
    This library provides the glue necessary for application "Parts"
    to be embedded as a Kontact component (or plugin).

---
+++ misc/kontactinterface/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/05 06:05:13 markd Exp $
+
+BUILDLINK_TREE+=	kontactinterface
+
+.if !defined(KONTACTINTERFACE_BUILDLINK3_MK)
+KONTACTINTERFACE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kontactinterface+=	kontactinterface>=20.04.1
+BUILDLINK_PKGSRCDIR.kontactinterface?=		../../misc/kontactinterface
+
+.include "../../devel/kparts/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KONTACTINTERFACE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kontactinterface
