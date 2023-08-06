01f87413d430ae9daea00a42e742233b95ac7e83
nia <nia@pkgsrc.org>
2023-08-06 10:41:42 +0000

    libreoffice: Fix stale patches. The libreoffice build system no longer sets DLOPEN_LIBS.

--- misc/libreoffice/patches/patch-external_skia_Library__skia.mk
+++ misc/libreoffice/patches/patch-external_skia_Library__skia.mk
@@ -1,17 +1,25 @@
-$NetBSD: patch-external_skia_Library__skia.mk,v 1.1 2020/08/11 16:07:39 ryoon Exp $
+$NetBSD: patch-external_skia_Library__skia.mk,v 1.2 2023/08/06 10:41:42 nia Exp $
 
---- external/skia/Library_skia.mk.orig	2020-07-29 19:29:17.000000000 +0000
+We want to link against libm and libX11 on every Unix, but libdl only on
+Linux.
+
+--- external/skia/Library_skia.mk.orig	2023-07-12 17:23:19.000000000 +0000
 +++ external/skia/Library_skia.mk
-@@ -66,10 +66,10 @@ $(eval $(call gb_Library_use_externals,s
+@@ -96,10 +96,15 @@ $(eval $(call gb_Library_use_externals,s
      libpng \
  ))
  
 -ifeq ($(OS),LINUX)
-+ifeq ($(OS), $(filter LINUX %BSD SOLARIS, $(OS)))
++ifeq ($(OS), $(filter LINUX, $(OS)))
  $(eval $(call gb_Library_add_libs,skia,\
-     -lm \
--    -ldl \
-+    $(DLOPEN_LIBS) \
+-    -lm \
+     -ldl \
++))
++endif
++
++ifeq ($(OS), $(filter LINUX %BSD SOLARIS, $(OS)))
++$(eval $(call gb_Library_add_libs,skia,\
++    -lm \
      -lX11-xcb \
      -lX11 \
  ))
