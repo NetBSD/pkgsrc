cc961f870dd848cfe447a34466f1095674db6ac3
abs <abs@pkgsrc.org>
2023-08-07 13:28:04 +0000

    Fix firefox91 build with python 3.{10,11}
    Bump PKGREVISION
    
    Maintainer pinged 17/7

--- www/firefox91/mozilla-common.mk
+++ www/firefox91/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.7 2022/12/09 21:28:04 wiz Exp $
+# $NetBSD: mozilla-common.mk,v 1.8 2023/08/07 13:28:04 abs Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -6,7 +6,7 @@
 
 .include "../../mk/bsd.prefs.mk"
 
-PYTHON_VERSIONS_INCOMPATIBLE=	27 311
+PYTHON_VERSIONS_INCOMPATIBLE=	27
 PYTHON_FOR_BUILD_ONLY=		tool
 ALL_ENV+=			PYTHON3=${PYTHONBIN}
 
