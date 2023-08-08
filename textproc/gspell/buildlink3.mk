58cc6ee90b142ac01e4feac2078b6b8d1ec3b324
adam <adam@pkgsrc.org>
2023-08-08 08:31:55 +0000

    gspell: updated to 1.8.4
    News in 1.8.4, 2020-09-04
    -------------------------
    * Translation updates.

--- textproc/gspell/buildlink3.mk
+++ textproc/gspell/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2023/01/29 21:15:00 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2023/08/08 08:31:55 adam Exp $
 
 BUILDLINK_TREE+=	gspell
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gspell
 GSPELL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gspell+=	gspell>=1.0.1
-BUILDLINK_ABI_DEPENDS.gspell?=	gspell>=1.8.3nb13
+BUILDLINK_ABI_DEPENDS.gspell+=	gspell>=1.8.3nb13
 BUILDLINK_PKGSRCDIR.gspell?=	../../textproc/gspell
 
 .include "../../devel/glib2/buildlink3.mk"
