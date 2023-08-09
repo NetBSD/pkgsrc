9817e2797511eb08498c79d4218a628dba45da9d
markd <markd@pkgsrc.org>
2023-08-09 20:48:12 +0000

    libkcddb: note kcmutils dependency

--- multimedia/libkcddb/buildlink3.mk
+++ multimedia/libkcddb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2023/05/22 21:00:04 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2023/08/09 20:48:12 markd Exp $
 
 BUILDLINK_TREE+=	libkcddb
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.libkcddb?=	libkcddb>=22.08.1nb7
 BUILDLINK_PKGSRCDIR.libkcddb?=	../../multimedia/libkcddb
 
 .include "../../audio/libmusicbrainz5/buildlink3.mk"
+.include "../../devel/kcmutils/buildlink3.mk"
 .include "../../devel/kio/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKCDDB_BUILDLINK3_MK
