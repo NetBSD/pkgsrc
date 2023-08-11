0fc1cf34f44f5ab5c2f56af2b85cd73477c6271a
mef <mef@pkgsrc.org>
2023-08-11 08:31:20 +0000

    (math/nlopt) Updated 2.6.2 to 2.7.1
    NLopt 2.7.1
    * Various minor bugfixes (#268, #409, #420) and build
    * improvements (support Octave 6.x, Guile 3.x, Cmake 3.2).
    
    NLopt 2.7.0
    * New nlopt_set_param API for setting internal algorithm parameters
      ([#365]).
    * Avoid library-symbol conflicts ([#355], [#361])

--- math/nlopt/buildlink3.mk
+++ math/nlopt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/09/18 05:06:06 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/08/11 08:31:20 mef Exp $
 
 BUILDLINK_TREE+=	nlopt
 
 .if !defined(NLOPT_BUILDLINK3_MK)
 NLOPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.6.2
-BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.6.2
+BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.7.1
+BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.7.1
 BUILDLINK_PKGSRCDIR.nlopt?=	../../math/nlopt
 .endif # NLOPT_BUILDLINK3_MK
 
