d168e83047d2a6e215c67c80528c6d113e287efe
bsiegert <bsiegert@pkgsrc.org>
2023-08-12 13:02:54 +0000

    Update go119 to 1.19.12 (security).
    crypto/tls: restrict RSA keys in certificates to <= 8192 bits
    
    Extremely large RSA keys in certificate chains can cause a client/server
    to expend significant CPU time verifying signatures. Limit this by
    restricting the size of RSA keys transmitted during handshakes to <=
    8192 bits.
    
    Based on a survey of publicly trusted RSA keys, there are currently only
    three certificates in circulation with keys larger than this, and all
    three appear to be test certificates that are not actively deployed. It
    is possible there are larger keys in use in private PKIs, but we target
    the web PKI, so causing breakage here in the interests of increasing the
    default safety of users of crypto/tls seems reasonable.
    
    Thanks to Mateusz Poliwczak for reporting this issue.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.183 2023/07/15 10:40:37 bsiegert Exp $
+# $NetBSD: version.mk,v 1.184 2023/08/12 13:02:54 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO120_VERSION=	1.20.6
-GO119_VERSION=	1.19.11
+GO119_VERSION=	1.19.12
 GO118_VERSION=	1.18.10
 GO14_VERSION=	1.4.3
 
