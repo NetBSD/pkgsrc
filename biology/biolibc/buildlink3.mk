3459720ea6548577fe61991593dbfbbfd89ef475
bacon <bacon@pkgsrc.org>
2023-08-12 14:49:18 +0000

    biology/biolibc: Update to 0.2.5
    Make header files C++ compatible (extern C)
    bl_chrom_name_cmp(): Support names with embedded digits, e.g. chr2p
    bl_gff_extract_attribute(): Improve performance
    gff -> gff3 to distinguish from past and future GFF specs
    Silence some compiler warnings
    
    Changes: https://github.com/auerlab/biolibc/releases

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/06/14 18:41:24 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/08/12 14:49:18 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.4.3
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.5
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
