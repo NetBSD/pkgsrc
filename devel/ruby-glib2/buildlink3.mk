fcf7155103ac8b49ac3fe7ee804a7f0f2f3366c0
tsutsui <tsutsui@pkgsrc.org>
2023-08-12 08:37:19 +0000

    ruby-gnome: update to 4.1.9.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.8
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.1.9
    
    Ruby-GNOME 4.1.9: 2023-08-11
    
    This is a vte4 added release.
    
    Changes
    
    Ruby/GObjectIntrospection
    
      * Improvements
    
          + Changed to use GLib memory allocation functions instead of
            Ruby's one when calling callbacks. Because callbacks may be
            called in out of the main Ruby thread.
    
    Ruby/VTE3
    
      * Improvements
    
          + Ensured using GTK3.
    
    Ruby/VTE4
    
      * Improvements
    
          + Added.
    
    
    Ruby-GNOME 4.1.8: 2023-06-25
    
    This is a conda support release.
    
    Changes
    
    All
    
      * Improvements
    
          + Added support for installing conda packages automatically.

--- devel/ruby-glib2/buildlink3.mk
+++ devel/ruby-glib2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/06/02 16:43:28 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/08/12 08:37:19 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-glib2
 
 .if !defined(RUBY_GLIB2_BUILDLINK3_MK)
 RUBY_GLIB2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.7
-BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.7
+BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.9
+BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.1.9
 BUILDLINK_PKGSRCDIR.ruby-glib2?=	../../devel/ruby-glib2
 
 .include "../../devel/glib2/buildlink3.mk"
