867f843901c2374d331198f5c5a1b29380c6e1fd
tsutsui <tsutsui@pkgsrc.org>
2023-08-13 00:34:47 +0000

    webp-pixbuf-loader: update fix to make geeqie show webp properly.
    Bump PKGREVISION.

--- graphics/webp-pixbuf-loader/patches/patch-io-webp.c
+++ graphics/webp-pixbuf-loader/patches/patch-io-webp.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-io-webp.c,v 1.2 2023/08/05 00:37:23 tsutsui Exp $
+$NetBSD: patch-io-webp.c,v 1.3 2023/08/13 00:34:47 tsutsui Exp $
 
 - update_func() shall be called for proper signal delivery
   https://github.com/aruiz/webp-pixbuf-loader/pull/73
@@ -14,12 +14,35 @@ $NetBSD: patch-io-webp.c,v 1.2 2023/08/05 00:37:23 tsutsui Exp $
            ret = TRUE;
          }
  
-@@ -189,6 +191,8 @@ stop_load (gpointer data, GError **error
+@@ -174,6 +176,9 @@ stop_load (gpointer data, GError **error
+           return FALSE;
+         }
+ 
++      if (context->prepare_func)
++        context->prepare_func (pb, NULL, context->user_data);
++
+       if (icc_data)
          {
-           if (context->prepare_func)
-             context->prepare_func (pb, NULL, context->user_data);
+           gdk_pixbuf_set_option (pb, "icc-profile", icc_data);
+@@ -187,10 +192,8 @@ stop_load (gpointer data, GError **error
+                                          context->buffer->len, &config);
+       if (status == VP8_STATUS_OK)
+         {
+-          if (context->prepare_func)
+-            context->prepare_func (pb, NULL, context->user_data);
+-
+-          g_clear_object (&pb);
 +          if (context->update_func)
 +            context->update_func (pb, 0, 0, context->width, context->height, context->user_data);
  
-           g_clear_object (&pb);
+           ret = TRUE;
+         }
+@@ -198,6 +201,8 @@ stop_load (gpointer data, GError **error
+         g_set_error (error, GDK_PIXBUF_ERROR, GDK_PIXBUF_ERROR_FAILED,
+                           "WebP decoder failed with VP8 status code: %d", status);
+       }
++
++      g_clear_object (&pb);
+     }
  
+   if (context->buffer)
