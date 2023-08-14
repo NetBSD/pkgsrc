01a62575fd99ee48aa37a38e0536a2217261cfe3
tnn <tnn@pkgsrc.org>
2023-08-14 18:01:26 +0000

    libuuid: revert bump of BUIDLINK_ABI_DEPENDS
    This was a false positive from the python revbump.
    Keep the PKGREVISION bumped though so it doesn't go backwards.

--- devel/libuuid/buildlink3.mk
+++ devel/libuuid/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/08/14 05:24:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/08/14 18:01:26 tnn Exp $
 
 BUILDLINK_TREE+=	libuuid
 
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	libuuid
 LIBUUID_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libuuid+=	libuuid>=2.18
-BUILDLINK_ABI_DEPENDS.libuuid?=	libuuid>=2.32.1nb2
 BUILDLINK_PKGSRCDIR.libuuid?=	../../devel/libuuid
 .endif	# LIBUUID_BUILDLINK3_MK
 
