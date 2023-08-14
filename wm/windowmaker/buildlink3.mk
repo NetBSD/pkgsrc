455bddd54e366d901a360093ded5f4f97fcd58c9
nia <nia@pkgsrc.org>
2023-08-14 19:10:08 +0000

    windowmaker: Add dependencies to buildlink3 that are used inside installed headers.

--- wm/windowmaker/buildlink3.mk
+++ wm/windowmaker/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2023/01/29 21:15:02 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2023/08/14 19:10:08 nia Exp $
 
 BUILDLINK_TREE+=	windowmaker
 
@@ -10,10 +10,12 @@ BUILDLINK_ABI_DEPENDS.windowmaker+=	windowmaker>=0.95.9nb9
 BUILDLINK_PKGSRCDIR.windowmaker?=	../../wm/windowmaker
 
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/giflib/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXft/buildlink3.mk"
 .include "../../x11/libXpm/buildlink3.mk"
 .endif # WINDOWMAKER_BUILDLINK3_MK
