bb8f9178960ef4dfc50483ea2f6d49c1a45a8b25
nia <nia@pkgsrc.org>
2023-08-16 15:20:49 +0000

    libhighway: Option-ize googletest dependency for ABI reasons (same as other packages, it's a dependency that might require a much newer compiler)
    While here, add missing libatomic dependency

--- graphics/libhighway/buildlink3.mk
+++ graphics/libhighway/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/10/07 11:15:54 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/08/16 15:20:49 nia Exp $
 
 BUILDLINK_TREE+=	libhighway
 
@@ -8,8 +8,6 @@ LIBHIGHWAY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libhighway+=	libhighway>=0.12.2
 BUILDLINK_ABI_DEPENDS.libhighway+=	libhighway>=1.0.1
 BUILDLINK_PKGSRCDIR.libhighway?=	../../graphics/libhighway
-
-.include "../../devel/googletest/buildlink3.mk"
 .endif	# LIBHIGHWAY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libhighway
