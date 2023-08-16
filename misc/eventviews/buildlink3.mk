61c842e926f8c5015e9f87b3806d591e68e9d5b0
markd <markd@pkgsrc.org>
2023-08-16 07:36:56 +0000

    eventviews: add version 23.04.3
    KDE PIM library for creating events.

---
+++ misc/eventviews/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/16 07:36:56 markd Exp $
+
+BUILDLINK_TREE+=	eventviews
+
+.if !defined(EVENTVIEWS_BUILDLINK3_MK)
+EVENTVIEWS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.eventviews+=	eventviews>=20.04.1
+BUILDLINK_PKGSRCDIR.eventviews?=	../../misc/eventviews
+
+.include "../../graphics/kdiagram/buildlink3.mk"
+.include "../../time/calendarsupport/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# EVENTVIEWS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-eventviews
