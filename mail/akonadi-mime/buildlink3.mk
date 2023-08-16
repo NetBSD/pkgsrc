7cfcab7371d07349a17e68d39542bee78f1b56cf
markd <markd@pkgsrc.org>
2023-08-16 03:27:05 +0000

    akonadi-mime: add version 23.04.3
    Libraries and daemons to implement basic email handling

---
+++ mail/akonadi-mime/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/16 03:27:05 markd Exp $
+
+BUILDLINK_TREE+=	akonadi-mime
+
+.if !defined(AKONADI_MIME_BUILDLINK3_MK)
+AKONADI_MIME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.akonadi-mime+=	akonadi-mime>=17.12.1
+BUILDLINK_PKGSRCDIR.akonadi-mime?=	../../mail/akonadi-mime
+
+.include "../../mail/akonadi/buildlink3.mk"
+.include "../../mail/kmime/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# AKONADI_MIME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-akonadi-mime
