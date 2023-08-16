6996b1df9af95f794a84f545fd946453e5df9ec4
markd <markd@pkgsrc.org>
2023-08-16 03:58:59 +0000

    libkgapi: update to 23.04.3
    qt5/kf5 version.

--- devel/libkgapi/buildlink3.mk
+++ devel/libkgapi/buildlink3.mk
@@ -1,14 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/06/28 11:31:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/08/16 03:58:59 markd Exp $
 
 BUILDLINK_TREE+=	libkgapi
 
 .if !defined(LIBKGAPI_BUILDLINK3_MK)
 LIBKGAPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkgapi+=	libkgapi>=2.2.0
-BUILDLINK_ABI_DEPENDS.libkgapi?=		libkgapi>=2.2.0nb33
+BUILDLINK_API_DEPENDS.libkgapi+=	libkgapi>=20.04.1
 BUILDLINK_PKGSRCDIR.libkgapi?=		../../devel/libkgapi
 
+.include "../../devel/kio/buildlink3.mk"
+.include "../../misc/kcontacts/buildlink3.mk"
+.include "../../security/cyrus-sasl/buildlink3.mk"
+.include "../../time/kcalendarcore/buildlink3.mk"
+.include "../../x11/qt5-qtwebengine/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKGAPI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkgapi
