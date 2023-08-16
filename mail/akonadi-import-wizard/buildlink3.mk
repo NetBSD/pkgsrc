76204d7541379d960b59b2989ddb4ec75bbf2d78
markd <markd@pkgsrc.org>
2023-08-16 05:43:10 +0000

    akonadi-import-wizard: add version 23.04.3
    akonadiimportwizard allows to import emails, settings, addressbook and
    calendar data detected in your user account from the following mailers:
    
    Trojit, Evolution 1.x - 3.x, OS X Mail, Opera, Sylpheed,
    Thunderbird/Mozilla, The Bat!, Outlook Express, Balsa, Pegasus-Mail,
    and Claws Mail.
    
    Additionally you can use the manual import for KMail Archive,
    mbox (Unix, Evolution), kmail Maildirs, Icedove, Lotus Notes,
    Plain Text and Mailman Archives.

---
+++ mail/akonadi-import-wizard/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/16 05:43:10 markd Exp $
+
+BUILDLINK_TREE+=	akonadi-import-wizard
+
+.if !defined(AKONADI_IMPORT_WIZARD_BUILDLINK3_MK)
+AKONADI_IMPORT_WIZARD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.akonadi-import-wizard+=	akonadi-import-wizard>=20.04.1
+BUILDLINK_PKGSRCDIR.akonadi-import-wizard?=	../../mail/akonadi-import-wizard
+
+.include "../../mail/mailcommon/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# AKONADI_IMPORT_WIZARD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-akonadi-import-wizard
