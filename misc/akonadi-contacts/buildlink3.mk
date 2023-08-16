87e7331534153cfc4c647962d13beb7373655cfe
markd <markd@pkgsrc.org>
2023-08-16 03:40:23 +0000

    akonadi-contacts: add version 23.04.3
    Libraries and daemons to implement Contact Management in Akonadi

---
+++ misc/akonadi-contacts/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/08/16 03:40:23 markd Exp $
+
+BUILDLINK_TREE+=	akonadi-contacts
+
+.if !defined(AKONADI_CONTACTS_BUILDLINK3_MK)
+AKONADI_CONTACTS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.akonadi-contacts+=	akonadi-contacts>=17.12.1
+BUILDLINK_PKGSRCDIR.akonadi-contacts?=		../../misc/akonadi-contacts
+
+.include "../../graphics/prison/buildlink3.mk"
+.include "../../mail/akonadi-mime/buildlink3.mk"
+.include "../../mail/kmime/buildlink3.mk"
+.include "../../misc/grantleetheme/buildlink3.mk"
+.include "../../misc/kcontacts/buildlink3.mk"
+.include "../../security/libkleo/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# AKONADI_CONTACTS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-akonadi-contacts
