43760f3cfca59d63dedb86a7609958e43074d790
adam <adam@pkgsrc.org>
2023-08-20 12:59:26 +0000

    cmake cmake-gui: updated to 3.27.3
    CMake 3.27.3
    * FindJNI: Restore support for macOS without Xcode
    * Utilities/Release: Remove unused documentation build settings
    * Apple Text Stubs (.tbd): various fixes
    * Clang: use -std=c23 for Clang 18+
    * Unity: use C-style comments to work both with C and C++
    * FindPython: add support for Python 3.13
    * GHS: Escape custom command comments
    * testDebuggerNamedPipe: fix for cppdap with nlohmann_json

--- devel/cmake/patches/patch-Modules_FindPython_Support.cmake
+++ devel/cmake/patches/patch-Modules_FindPython_Support.cmake
@@ -1,10 +1,10 @@
-$NetBSD: patch-Modules_FindPython_Support.cmake,v 1.4 2022/05/26 08:53:59 adam Exp $
+$NetBSD: patch-Modules_FindPython_Support.cmake,v 1.5 2023/08/20 12:59:26 adam Exp $
 
 Insist on select Python version for pkgsrc.
 
---- Modules/FindPython/Support.cmake.orig	2022-05-25 13:42:51.000000000 +0000
+--- Modules/FindPython/Support.cmake.orig	2023-08-16 17:46:35.000000000 +0000
 +++ Modules/FindPython/Support.cmake
-@@ -21,7 +21,9 @@ endif()
+@@ -27,7 +27,9 @@ endif()
  if (NOT DEFINED _${_PYTHON_PREFIX}_REQUIRED_VERSION_MAJOR)
    message (FATAL_ERROR "FindPython: INTERNAL ERROR")
  endif()
@@ -12,6 +12,6 @@ Insist on select Python version for pkgsrc.
 +if (DEFINED PYVERSSUFFIX)
 +  set(_${_PYTHON_PREFIX}_VERSIONS ${PYVERSSUFFIX})
 +elseif (_${_PYTHON_PREFIX}_REQUIRED_VERSION_MAJOR EQUAL "3")
-   set(_${_PYTHON_PREFIX}_VERSIONS 3.12 3.11 3.10 3.9 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0)
+   set(_${_PYTHON_PREFIX}_VERSIONS 3.13 3.12 3.11 3.10 3.9 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0)
  elseif (_${_PYTHON_PREFIX}_REQUIRED_VERSION_MAJOR EQUAL "2")
    set(_${_PYTHON_PREFIX}_VERSIONS 2.7 2.6 2.5 2.4 2.3 2.2 2.1 2.0)
