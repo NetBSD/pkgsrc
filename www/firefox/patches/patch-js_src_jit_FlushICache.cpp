c61bdb677aed7d9f0e00d5b4ceb668b717fc4a05
tnn <tnn@pkgsrc.org>
2023-08-20 09:31:24 +0000

    firefox: fix incomplete aarch64 patch
    Linux membarrier(2) is used by Firefox on ARM to trigger an IPI-induced
    ICache flush from the WASM compiler thread during tiered compilation.
    
    When the syscall is not available, it is unsafe to do tiered compilation
    and it must be disabled, but this check didn't work properly.
    
    Unfortunately there are still frequent tab crashes on aarch64, but likely
    the cause is elsewhere.

--- www/firefox/patches/patch-js_src_jit_FlushICache.cpp
+++ www/firefox/patches/patch-js_src_jit_FlushICache.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-js_src_jit_FlushICache.cpp,v 1.1 2022/10/08 21:18:55 ryoon Exp $
+$NetBSD: patch-js_src_jit_FlushICache.cpp,v 1.2 2023/08/20 09:31:24 tnn Exp $
 
 NetBSD does not have the Linux-specific membarrier(2) syscall.
 
---- js/src/jit/FlushICache.cpp.orig	2022-09-08 19:35:33.000000000 +0000
+--- js/src/jit/FlushICache.cpp.orig	2023-08-15 20:31:19.000000000 +0000
 +++ js/src/jit/FlushICache.cpp
 @@ -31,12 +31,18 @@
  #    elif defined(__android__)
@@ -23,3 +23,12 @@ NetBSD does not have the Linux-specific membarrier(2) syscall.
  }
  
  // These definitions come from the Linux kernel source, for kernels before 4.16
+@@ -92,6 +98,8 @@ bool CanFlushExecutionContextForAllThrea
+ 
+   computed = true;
+   return kernelHasMembarrier;
++#  elif defined(__NetBSD__)
++  return false;
+ #  else
+   // On other platforms, we assume that the syscall for flushing the icache
+   // will flush the execution context for other cores.
