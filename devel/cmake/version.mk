803d35b637e1babcf04125accda53929d93945e5
adam <adam@pkgsrc.org>
2023-08-20 12:59:26 +0000

    cmake cmake-gui: updated to 3.27.3
    CMake 3.27.3
    * FindJNI: Restore support for macOS without Xcode
    * Utilities/Release: Remove unused documentation build settings
    * Apple Text Stubs (.tbd): various fixes
    * Clang: use -std=c23 for Clang 18+
    * Unity: use C-style comments to work both with C and C++
    * FindPython: add support for Python 3.13
    * GHS: Escape custom command comments
    * testDebuggerNamedPipe: fix for cppdap with nlohmann_json

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.42 2023/08/12 12:50:12 adam Exp $
+# $NetBSD: version.mk,v 1.43 2023/08/20 12:59:26 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.2
+CMAKE_VERSION=	3.27.3
 CMAKE_API=	${CMAKE_VERSION:R}
