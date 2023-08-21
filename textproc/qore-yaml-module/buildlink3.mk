0e38eb6887e6a41ac099e0a8c5fe3a91501f4eab
nros <nros@pkgsrc.org>
2023-08-21 19:31:20 +0000

    Revbump after lang/qore update

--- textproc/qore-yaml-module/buildlink3.mk
+++ textproc/qore-yaml-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/08/13 10:38:03 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/08/21 19:31:22 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-yaml-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-yaml-module
 QORE_YAML_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-yaml-module+=	qore-yaml-module>=0.4
-BUILDLINK_ABI_DEPENDS.qore-yaml-module?=	qore-yaml-module>=0.7.3nb2
+BUILDLINK_ABI_DEPENDS.qore-yaml-module?=	qore-yaml-module>=0.7.3nb3
 BUILDLINK_PKGSRCDIR.qore-yaml-module?=		../../textproc/qore-yaml-module
 
 
