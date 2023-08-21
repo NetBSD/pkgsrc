bac9443c68c70e7d1b734b53d808e8ce7b84cfde
micha <micha@pkgsrc.org>
2023-08-21 13:29:44 +0000

    news/libcanlock: Use new variables to request a C99 compiler
    Bump, because now the mode should always be set (if default is not C99).

--- news/libcanlock/buildlink3.mk
+++ news/libcanlock/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/08/27 15:14:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/08/21 13:29:44 micha Exp $
 
 BUILDLINK_TREE+=	libcanlock
 
 .if !defined(LIBCANLOCK_BUILDLINK3_MK)
 LIBCANLOCK_BUILDLINK3_MK:=
+
+USE_CC_FEATURES+=	c99
 
 BUILDLINK_API_DEPENDS.libcanlock+=	libcanlock>=3.0.0
 BUILDLINK_PKGSRCDIR.libcanlock?=	../../news/libcanlock
