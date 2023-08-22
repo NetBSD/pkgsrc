a3c96d7bb379c9b5dac86401a48252af455e6a1e
adam <adam@pkgsrc.org>
2023-08-22 20:31:36 +0000

    dnsdist: updated to 1.8.0
    1.8.0
    
    Bug Fixes
    
    Fix ‘Unknown key’ issue for actions and rules parameters
    Fix a dnsheader unaligned case
    secpoll: explicitly include necessary ctime header for time_t

--- net/dnsdist/patches/patch-dnsdist-console.cc
+++ net/dnsdist/patches/patch-dnsdist-console.cc
@@ -1,13 +1,13 @@
-$NetBSD: patch-dnsdist-console.cc,v 1.5 2020/08/06 16:28:03 adam Exp $
+$NetBSD: patch-dnsdist-console.cc,v 1.6 2023/08/22 20:31:36 adam Exp $
 
 Always use the pkgsrc editline layout.
 
---- dnsdist-console.cc.orig	2020-07-28 07:39:28.000000000 +0000
+--- dnsdist-console.cc.orig	2023-03-28 11:06:46.000000000 +0000
 +++ dnsdist-console.cc
-@@ -24,7 +24,7 @@
- #include <pwd.h>
+@@ -27,7 +27,7 @@
  #include <thread>
  
+ #ifdef HAVE_LIBEDIT
 -#if defined (__OpenBSD__) || defined(__NetBSD__)
 +#if 1
  // If this is not undeffed, __attribute__ wil be redefined by /usr/include/readline/rlstdc.h
