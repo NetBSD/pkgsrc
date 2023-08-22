6ef5f19af3d8761c8c6ec9fb55c5b855e6696720
adam <adam@pkgsrc.org>
2023-08-22 20:26:29 +0000

    strawberry: updated to 1.0.18
    Pkgsrc:
    - Qt6 as an option
    - Builds on Darwin
    
    Strawberry 1.0.18
    
    Bugfixes:
    
    Fixed reading disc from QObuz songs
    Fixed volume being reset on playback with PulseAudio
    Fixed <br> tags in SQL query error message.
    Fixed compile with Qt 6 without XCB (QX11Application).
    Fixed smart playlist editor not properly loading search terms
    Fixed use of fixed icon for playlist favorite star icon
    Possible fix for collection thumbnails using disk cache having identical covers for albums with hashtag
    Fixed listenbrainz scrobbling for songs with multiple artist mbids.
    Fixed listenbrainz scrobbling for songs without duration.
    Fixed gapless playback sometimes not working.
    Fixed writing PNG images as embedded covers
    Fixed greyscale album covers not working in OSD D-Bus
    Fixed collection thumbnail disk cache with Qt 6.5.1 and newer.
    Fixed moodbar disk cache with Qt 6.5.1 and newer.
    Fixed playlist edit tag F2 shortcut only working for title tag
    Append number to filename if the destination file already exist when transcoding audio
    Fixed abseil linking issues with protobuf 1.22.0 and newer.
    (macOS) Fixed "Show this message" checkbox having no affect on Rosetta warning dialog
    (macOS) Disable unused D-Bus.
    (Windows) Fixed command line options not working with diacritics
    (Windows) Fixed issue with saving album covers in album directory being saved in temp directory instead.
    (Windows) Fixed crash when trying a play a song which doesn't exist
    
    Enhancements:
    
    Reduce memory overhead with album cover handling
    Improved listenbrainz error handling.
    Show error dialog for listenbrainz errors similar to last.fm/libre.fm.
    Reduce NetworkAccessManager instances.
    Replace SingleApplication with KDSingleApplication.
    Require Qt 5.12 or higher.
    Add new database fields for art_embedded and art_unset.
    Rewrite album cover loader.
    Move cover filename settings from collection to covers settings.
    Add setting to set priorities for album cover types.
    Add rating filtering to playlist search
    (Windows|MSVC) Add WSAPI2 plugin.

--- audio/strawberry/options.mk
+++ audio/strawberry/options.mk
@@ -1,7 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2020/05/15 17:38:35 nia Exp $
+# $NetBSD: options.mk,v 1.2 2023/08/22 20:26:29 adam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.strawberry
 PKG_SUPPORTED_OPTIONS=		pulseaudio
+PKG_OPTIONS_OPTIONAL_GROUPS=	gui
+PKG_OPTIONS_GROUP.gui=		qt5 qt6
+PKG_SUGGESTED_OPTIONS=		qt5
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +13,23 @@ CMAKE_ARGS+=	-DLIBPULSE=ON
 .  include "../../audio/pulseaudio/buildlink3.mk"
 .else
 CMAKE_ARGS+=	-DLIBPULSE=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt5) || !empty(PKG_OPTIONS:Mqt6)
+.  if !empty(PKG_OPTIONS:Mqt5)
+TOOL_DEPENDS+=	qt5-qttools-[0-9]*:../../x11/qt5-qttools
+CMAKE_ARGS+=	-DBUILD_WITH_QT5=ON
+CMAKE_ARGS+=	-DBUILD_WITH_QT6=OFF
+.    include "../../x11/qt5-qtbase/buildlink3.mk"
+.    if ${OPSYS} != "Darwin"
+.      include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.    else
+.      include "../../x11/qt5-qtmacextras/buildlink3.mk"
+.    endif
+.  elif !empty(PKG_OPTIONS:Mqt6)
+TOOL_DEPENDS+=	qt6-qttools-[0-9]*:../../devel/qt6-qttools
+CMAKE_ARGS+=	-DBUILD_WITH_QT5=OFF
+CMAKE_ARGS+=	-DBUILD_WITH_QT6=ON
+.    include "../../x11/qt6-qtbase/buildlink3.mk"
+.  endif
 .endif
