77e6c91a7e6b1eca99d37d7e8665d7bc630c9a32
adam <adam@pkgsrc.org>
2023-08-23 08:01:06 +0000

    wesnoth: updated to 1.16.10
    Version 1.16.10
     Campaigns
       * Heir to the Throne
         * S03: Rewrite intro text, including a hint about training troops
     Editor
       * Scrolling the map north or south can move the text that overlays the map, so it doesn't always obscure the southmost hexes of the map.
     Multiplayer
       * Allied units’ orbs no longer look like the player’s own units’ orbs.
           * By default, they are now two-color during the ally’s turn.
           * Added an advanced setting to always show them as single-color (the ally color).
       * "Back to Turn" menu no longer crashes the game when used
         After the game has ended.
       * World Conquest: Fixed rare lua error when a unit is killed.
       * World Conquest: Fixed Bezoar Item.
       * World Conquest: Fixed some items losing their effects in
         later scenarios.
       * World Conquest: Fixed units losing hp wheh picking up an
         artifact
       * World Conquest: Fixed "Promote to commander"
     Translations
       * Updated translations: Arabic, Czech, French, Italian, Spanish, Portuguese (Brazil)
     User interface
       * Fix file dialogs (e.g. Save As dialog in the Map Editor) not listing /run/media/USER
         as a possible file location on modern Linux distributions.
       * Update the preferences window’s Display resolution list if the game window is resized.
       * Improved the layout of the orb colors dialog, and added tooltips.
     Miscellaneous and Bug Fixes
       * Fixed Possible Crash when using planning mode
       * Fixed Possible OOS when using planning mode
       * Fixed game hanging when an enemy attacks a unit
         with planned moves
       * Fixed custom effects implemented via wesnoth.effects
         losing their effects in later scenarios.
       * Fix Crash undo+redo error after reloading
       * Fix Crash when loading old replays
       * Fix Crash when a unit has bad animations replays
       * Postponed the removal of the `SPECIAL_NOTES` macro, which means there will be fewer log messages.

--- games/wesnoth/patches/patch-src_color__range.cpp
+++ games/wesnoth/patches/patch-src_color__range.cpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_color__range.cpp,v 1.1 2020/04/15 22:52:09 joerg Exp $
+$NetBSD: patch-src_color__range.cpp,v 1.2 2023/08/23 08:01:06 adam Exp $
+
+Add missing include.
 
 --- src/color_range.cpp.orig	2020-04-14 20:36:13.908481410 +0000
 +++ src/color_range.cpp
