4b4e0610aea88f18f7fe2410151ba3da6856ea1e
adam <adam@pkgsrc.org>
2023-08-25 08:26:49 +0000

    python310 py310-html-docs: updated to 3.10.13
    Python 3.10.13
    
    Security
    
    gh-108310: Fixed an issue where instances of ssl.SSLSocket were vulnerable to a bypass of the TLS handshake and included protections (like certificate verification) and treating sent unencrypted data as if it were post-handshake TLS encrypted data. Security issue reported as CVE-2023-40217 by Aapo Oksman. Patch by Gregory P. Smith.
    
    Library
    
    gh-107845: tarfile.data_filter() now takes the location of symlinks into account when determining their target, so it will no longer reject some valid tarballs with LinkOutsideDestinationError.
    
    Tools/Demos
    
    gh-107565: Update multissltests and GitHub CI workflows to use OpenSSL 1.1.1v, 3.0.10, and 3.1.2.
    
    C API
    
    gh-99612: Fix PyUnicode_DecodeUTF8Stateful() for ASCII-only data: *consumed was not set.

--- lang/python310/dist.mk
+++ lang/python310/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.13 2023/06/07 13:26:54 adam Exp $
+# $NetBSD: dist.mk,v 1.14 2023/08/25 08:26:49 adam Exp $
 
-PY_DISTVERSION=	3.10.12
+PY_DISTVERSION=	3.10.13
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python310/distinfo
