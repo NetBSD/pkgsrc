687eb4f3062160b52d6d9ee3f1f6957293b59cb2
adam <adam@pkgsrc.org>
2023-08-25 08:26:13 +0000

    python39 py39-html-docs: updated to 3.9.18
    Python 3.9.18
    
    Security
    
    gh-108310: Fixed an issue where instances of ssl.SSLSocket were vulnerable to a bypass of the TLS handshake and included protections (like certificate verification) and treating sent unencrypted data as if it were post-handshake TLS encrypted data. Security issue reported as CVE-2023-40217 by Aapo Oksman. Patch by Gregory P. Smith.
    
    Library
    
    gh-107845: tarfile.data_filter() now takes the location of symlinks into account when determining their target, so it will no longer reject some valid tarballs with LinkOutsideDestinationError.
    
    Tools/Demos
    
    gh-107565: Update multissltests and GitHub CI workflows to use OpenSSL 1.1.1v, 3.0.10, and 3.1.2.
    
    C API
    
    gh-99612: Fix PyUnicode_DecodeUTF8Stateful() for ASCII-only data: *consumed was not set.

--- lang/python39/dist.mk
+++ lang/python39/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.18 2023/06/07 13:25:52 adam Exp $
+# $NetBSD: dist.mk,v 1.19 2023/08/25 08:26:13 adam Exp $
 
-PY_DISTVERSION=	3.9.17
+PY_DISTVERSION=	3.9.18
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python39/distinfo
