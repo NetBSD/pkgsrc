850f66c31f822a5f617ec90e4ac0e4e7ba0c90fa
bsiegert <bsiegert@pkgsrc.org>
2023-08-26 14:57:22 +0000

    Pullup ticket #6794 - requested by bouyer textproc/cjose: build fix for OpenSSL 3
    Revisions pulled up:
    - textproc/cjose/Makefile                                       1.4-1.5
    - textproc/cjose/distinfo                                       1.4-1.5
    - textproc/cjose/patches/patch-src_concatkdf.c                  1.1-1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	bouyer
       Date:		Tue Aug 22 10:45:03 UTC 2023
    
       Added Files:
       	pkgsrc/textproc/cjose/patches: patch-src_concatkdf.c
    
       Log Message:
       Switch from unmaintained github repo cisco/cjose/ to OpenIDC/cjose/ and
       update to 0.6.2.2, fixing build on netbsd-10 (and probably HEAD) after
       openssl upgrade.
       Changes are mostly build and bug fixes; support for newer openssl version
       and ciphers.
    
    ---
       Module Name:	pkgsrc
       Committed By:	bouyer
       Date:		Tue Aug 22 10:50:00 UTC 2023
    
       Modified Files:
       	pkgsrc/textproc/cjose: Makefile distinfo
    
       Log Message:
       Switch from unmaintained github repo cisco/cjose/ to OpenIDC/cjose/ and
       update to 0.6.2.2, fixing build on netbsd-10 (and probably HEAD) after
       openssl upgrade.
       Changes are mostly build and bug fixes; support for newer openssl version
       and ciphers.
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Tue Aug 22 11:50:03 UTC 2023
    
       Modified Files:
       	pkgsrc/textproc/cjose: Makefile distinfo
       	pkgsrc/textproc/cjose/patches: patch-src_concatkdf.c
    
       Log Message:
       cjose: clean up

---
+++ textproc/cjose/patches/patch-src_concatkdf.c
@@ -0,0 +1,19 @@
+$NetBSD$
+
+Fix build on NetBSD.
+
+--- src/concatkdf.c.orig	2023-08-22 12:18:03.202602223 +0200
++++ src/concatkdf.c	2023-08-22 12:17:49.008381386 +0200
+@@ -12,8 +12,12 @@
+ #include <malloc.h>
+ #else
+ #include <arpa/inet.h>
++#ifdef __NetBSD__
++#include <stdlib.h>
++#else
+ #include <alloca.h>
+ #endif
++#endif
+ #include <openssl/evp.h>
+ #include <string.h>
+ #include <cjose/base64.h>
