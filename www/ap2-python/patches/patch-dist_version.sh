cf0f51d101a21bb3fc02074eb6167f853a3c86fa
gdt <gdt@pkgsrc.org>
2023-08-28 09:47:46 +0000

    www/ap2-python: Update to 3.5.0.1
    Upstream has taggeed a pseudo-release at our request!
    
    packaging changes:
    
      - switch to github for distfiles and HOMEPAGE
    
      - remove explanation about non-threaded apache from MESSAGE, as
        there is no longer any such thing in pkgsrc
    
      - remove 3 hunks from MESSAGE that duplicate upstream documentation
    
      - remove empty MESSAGE
    
    Upstream NEWS:
    
    Aug  18 2023 -  3.5.0.1 released. It addresses compatibility issues with
                    Python 3.11.

--- www/ap2-python/patches/patch-dist_version.sh
+++ www/ap2-python/patches/patch-dist_version.sh
@@ -1,14 +1,22 @@
-$NetBSD: patch-dist_version.sh,v 1.1 2014/02/17 17:20:28 adam Exp $
+$NetBSD: patch-dist_version.sh,v 1.2 2023/08/28 09:47:46 gdt Exp $
 
 Don't depend on 'git'.
 
---- dist/version.sh.orig	2014-02-17 13:35:44.000000000 +0000
+--- dist/version.sh.orig	2023-08-18 17:28:08.000000000 +0000
 +++ dist/version.sh
-@@ -5,6 +5,5 @@ MPV_PATH="`dirname $0`/../src/include/mp
- MAJ=`awk '/MP_VERSION_MAJOR/ {print $3}' $MPV_PATH`
+@@ -6,6 +6,8 @@ MAJ=`awk '/MP_VERSION_MAJOR/ {print $3}'
  MIN=`awk '/MP_VERSION_MINOR/ {print $3}' $MPV_PATH`
  PCH=`awk '/MP_VERSION_PATCH/ {print $3}' $MPV_PATH`
--GIT=`git describe --always`
  
--echo $MAJ.$MIN.$PCH-$GIT
-+echo $MAJ.$MIN.$PCH
++if false; then
++
+ # if git exists in path
+ if type git >/dev/null 2>&1; then
+     # and we are in a checkout
+@@ -18,4 +20,6 @@ if type git >/dev/null 2>&1; then
+     fi
+ fi
+ 
++fi
++
+ echo $MAJ.$MIN.$PCH$GIT
