0e2c3a7ac547af6082e632073099dd1863db9c81
abs <abs@pkgsrc.org>
2023-08-28 19:34:24 +0000

    Fix mongodb4 option to install v4, and add mongodb6 option
    Not bumping PKGREVISION as default build unaffected.
    
    OKed by maintainer

--- net/unifi/options.mk
+++ net/unifi/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2023/07/03 12:53:32 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2023/08/28 19:34:24 abs Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.unifi
-PKG_OPTIONS_GROUP.mongodb=	mongodb3 mongodb4
+PKG_OPTIONS_GROUP.mongodb=	mongodb3 mongodb4 mongodb6
 PKG_OPTIONS_REQUIRED_GROUPS=	mongodb
 PKG_SUGGESTED_OPTIONS=		mongodb3
 
@@ -16,5 +16,11 @@ DEPENDS+=		mongodb>=3.4.4<4:../../databases/mongodb3
 # For those happy with server-side-public-license
 # Note a backup/restore is required when switching versions
 .if !empty(PKG_OPTIONS:Mmongodb4)
-DEPENDS+=		mongodb>=4.0:../../databases/mongodb
+DEPENDS+=		mongodb>=4.0<6:../../databases/mongodb4
+.endif
+
+# For those happy with server-side-public-license
+# Note a backup/restore is required when switching versions
+.if !empty(PKG_OPTIONS:Mmongodb6)
+DEPENDS+=		mongodb>=6.0:../../databases/mongodb
 .endif
