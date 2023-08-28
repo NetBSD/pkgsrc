31bdc41023d9a2d9a196f0a89ca70a7378db6157
gdt <gdt@pkgsrc.org>
2023-08-28 10:37:40 +0000

    ham/chirp: Update to 20230823 and switch to py3 branch
    Upstream has had a python3 branch for a long time, which uses
    wxWidgets.  The "chirp-daily" py2 branch with py-gtk2 is unmaintained,
    and the "chirp-next" py3 branch is the recommended version.  This
    commit switches to that branch, but there is no major version change
    because upstream uses just dates for all tarballs.  Besides the python
    version and toolkit change, installed program names change.
    
    As always there are bugfixes, minor improvements and support for
    additional radios.

--- ham/chirp/patches/patch-chirp_platform.py
+++ ham/chirp/patches/patch-chirp_platform.py
@@ -1,23 +1,19 @@
-$NetBSD: patch-chirp_platform.py,v 1.2 2020/08/25 13:45:53 gdt Exp $
+$NetBSD: patch-chirp_platform.py,v 1.3 2023/08/28 10:37:40 gdt Exp $
 
-This software contains darwin specific tweaks which do not apply to pkgsrc
+Don't look for share other than in the right place.
+\todo File upstream.
+\todo Make upstream respect a configured share dir.
 
---- chirp/platform.py.orig	2017-10-12 18:01:04.000000000 +1100
-+++ chirp/platform.py	2020-07-03 15:33:43.225703607 +1000
-@@ -283,16 +283,6 @@
- 
-         Platform.__init__(self, basepath)
- 
--        # This is a hack that needs to be properly fixed by importing the
--        # latest changes to this module from d-rats.  In the interest of
--        # time, however, I'll throw it here
--        if sys.platform == "darwin":
--            if "DISPLAY" not in os.environ:
--                LOG.info("Forcing DISPLAY for MacOS")
--                os.environ["DISPLAY"] = ":0"
--
--            os.environ["PANGO_RC_FILE"] = "../Resources/etc/pango/pangorc"
--
-     def default_dir(self):
-         return os.path.abspath(os.getenv("HOME"))
- 
+--- chirp/platform.py.orig	2023-02-23 10:01:01.000000000 +0000
++++ chirp/platform.py
+@@ -117,10 +117,7 @@ class Platform:
+         """Searches for files installed to a share/ prefix."""
+         execpath = self.executable_path()
+         share_candidates = [
+-            os.path.join(execpath, "share"),
+             os.path.join(sys.prefix, "share"),
+-            "/usr/local/share",
+-            "/usr/share",
+         ]
+         pkgshare_candidates = [os.path.join(i, "chirp")
+                                for i in share_candidates]
