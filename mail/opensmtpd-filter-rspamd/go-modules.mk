51794220b268e020ec0a3b05015318fb03a04bc2
vins <vins@pkgsrc.org>
2023-08-28 16:26:10 +0000

    mail/opensmtpd-filter-rspamd: import opensmtpd-filter-rspamd-0.1.8
    filter-rspamd is an opensmtpd filter that passes mail through
    rspamd to flag or reject spam messages at session time.

---
+++ mail/opensmtpd-filter-rspamd/go-modules.mk
@@ -0,0 +1,4 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/08/28 16:26:10 vins Exp $
+
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200821140526-fda516888d29.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200821140526-fda516888d29.zip
