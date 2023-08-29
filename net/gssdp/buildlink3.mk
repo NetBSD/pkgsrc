dcd04c5c156c914b591e35dcf852a18a384ee0c5
adam <adam@pkgsrc.org>
2023-08-29 19:28:14 +0000

    gssdp: updated to 1.6.2
    1.6.2 (stable)
    ==============
     - Propagate random port up to client, partial fix for GUPnP/81
     - Add manpage for sniffer
    
    1.6.1 (stable)
    ==============
     - Potential fix for sending discovery responses with the wrong
       location
     - Properly parse netlink messages in neighbour discovery
     - Do not leak the host mask if it was alreay provided on object client
       creation
     - Fix install path for generated documentation
     - Fix warning message for link-local v4 addresses
    
    Bugs fixed in this release:
     - https://gitlab.gnome.org/GNOME/gssdp/issues/22
     - https://gitlab.gnome.org/GNOME/gssdp/issues/24
    
    1.6.0 (stable)
    ==============
     - Re-release of 1.5.2 as stable version. No functional changes

--- net/gssdp/buildlink3.mk
+++ net/gssdp/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.42 2023/04/19 08:08:34 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2023/08/29 19:28:14 adam Exp $
 
 BUILDLINK_TREE+=	gssdp
 
 .if !defined(GSSDP_BUILDLINK3_MK)
 GSSDP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=0.12.2.1
-BUILDLINK_ABI_DEPENDS.gssdp+=	gssdp>=1.0.3nb25
+BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=1.6
+BUILDLINK_ABI_DEPENDS.gssdp+=	gssdp>=1.6
 BUILDLINK_PKGSRCDIR.gssdp?=	../../net/gssdp
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
+.include "../../net/libsoup3/buildlink3.mk"
 .endif	# GSSDP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gssdp
