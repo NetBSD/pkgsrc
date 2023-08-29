4ed8db592f958f97dadf0785469d211743110446
manu <manu@pkgsrc.org>
2023-08-29 14:15:01 +0000

    Added www/py-sydent version 2.5.6
    Sydent is an identity server for the Matrix communications protocol.
    It allows Matrix users to prove that they own an email address or
    phone number, and allows _other_ Matrix users to look them up using
    that email address or phone number.
    
    Imported from wip/py-sydent, contributed by kethzer.dr@gmail.com

---
+++ www/py-sydent/patches/patch-sydent_threepid_bind.py
@@ -0,0 +1,14 @@
+$NetBSD: patch-sydent_threepid_bind.py,v 1.1 2023/08/29 14:15:01 manu Exp $
+--- sydent/threepid/bind.py.orig
++++ sydent/threepid/bind.py
+@@ -30,9 +30,9 @@
+ from sydent.threepid import ThreepidAssociation
+ 
+ from OpenSSL import SSL
+ from OpenSSL.SSL import VERIFY_NONE
+-from StringIO import StringIO
++from io import StringIO
+ from twisted.internet import reactor, defer, ssl
+ from twisted.names import client, dns
+ from twisted.names.error import DNSNameError
+ from twisted.web.client import FileBodyProducer, Agent
