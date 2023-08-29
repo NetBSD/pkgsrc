565edf7e0dc12bb6d40a2d376bd76d71011c7979
adam <adam@pkgsrc.org>
2023-08-29 19:51:59 +0000

    gupnp: updated to 1.6.5
    1.6.5 (stable)
     - Fix build with meson 1.2
    
    1.6.4 (stable)
    ==============
     - Keep a weak reference to proxy in action
     - Add API to provide HTTP credentials for simple authentication
     - Remove xmlRecoverMemory usage
    
    Bugs fixed in this release:
     - Fixes: https://gitlab.gnome.org/GNOME/gupnp/-/issues/85
     - https://gitlab.gnome.org/GNOME/gupnp/issues/86
    
    1.6.3 (stable)
    ==============
     - Fix handling of deprecated and tentative v6 addresses
     - Bump GSSDP minimjal version to 1.6.2
    
    Bugs fixed in this release:
     - https://gitlab.gnome.org/GNOME/gupnp/issues/82
     - https://gitlab.gnome.org/GNOME/gupnp/issues/83
    
    1.6.2 (stable)
    ==============
     - Add test for issue 81
    
    Bugs fixed in this release:
     - https://gitlab.gnome.org/GNOME/gupnp/issues/81
    
    1.6.1 (stable)
    ==============
     - ControlPoint: Fix error handling if description download fails
     - Use proper method for detecting IFA_FLAGS availability
     - ContextManager: Do not leak filtered contexts
     - Network ContextManager: Do not leak list parts of context lists
     - Introspection: Properly chain up to parent class
     - ContextManager: Fix freeing unavailable contexts
     - ControlPoint: Do not leak cancellable
     - Service: Fix crash if subscription callback is points unreachable host
    
    Bugs fixed in this release:
     - https://gitlab.gnome.org/GNOME/gupnp/issues/80
     - https://gitlab.gnome.org/GNOME/gupnp/issues/78
    
    1.6.0 (stable)
    ==============
     - Re-release of 1.5.4, no functional change

--- net/gupnp/buildlink3.mk
+++ net/gupnp/buildlink3.mk
@@ -1,26 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.53 2023/08/14 05:25:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.54 2023/08/29 19:51:59 adam Exp $
 
 BUILDLINK_TREE+=	gupnp
 
 .if !defined(GUPNP_BUILDLINK3_MK)
 GUPNP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gupnp+=	gupnp>=0.20.3
-BUILDLINK_ABI_DEPENDS.gupnp+=	gupnp>=1.0.3nb24
+BUILDLINK_API_DEPENDS.gupnp+=	gupnp>=1.6
+BUILDLINK_ABI_DEPENDS.gupnp?=	gupnp>=1.6
 BUILDLINK_PKGSRCDIR.gupnp?=	../../net/gupnp
 
 .include "../../mk/bsd.fast.prefs.mk"
-.if (!empty(OPSYS:M*BSD) || ${OPSYS} == "DragonFly" || ${OPSYS} == "Darwin") && (!defined(USE_INTERNAL_UUID) || empty(USE_INTERNAL_UUID:M[Yy][Ee][Ss]))
-pre-configure: gupnp-pre-configure
-.PHONY: gupnp-pre-configure
-gupnp-pre-configure:
+.if (${OPSYS:M*BSD} || ${OPSYS} == "DragonFly" || ${OPSYS} == "Darwin") && (!defined(USE_INTERNAL_UUID) || empty(USE_INTERNAL_UUID:M[Yy][Ee][Ss]))
+pre-configure:
 	if ! ${TEST} -e ${BUILDLINK_DIR}/lib/pkgconfig/uuid.pc; then cp ${BUILDLINK_PKGSRCDIR.gupnp}/files/uuid.pc ${BUILDLINK_DIR}/lib/pkgconfig/uuid.pc; fi
 .else
 .include "../../devel/libuuid/buildlink3.mk"
 .endif
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
+.include "../../net/libsoup3/buildlink3.mk"
 .include "../../net/gssdp/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif	# GUPNP_BUILDLINK3_MK
