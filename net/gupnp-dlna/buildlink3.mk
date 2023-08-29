4d8f25f1816168fe8a2c25788fb9327a47239c9a
adam <adam@pkgsrc.org>
2023-08-29 20:06:53 +0000

    gupnp-dlna: updated to 0.12.0
    0.12.0 (stable)
    - Re-tag of 0.11 as 0.12. No functional change
    
    0.11.0 (development)
    - Drop GStreamer 0.10 back-end
    - Drop old compatibility code
    - Add meson support
    - Port to more modern GObject
    
    Bugs fixed in this release:
     - https://gitlab.gnome.org/GNOME/gupnp-dlna/issues/1
     - https://gitlab.gnome.org/GNOME/gupnp-dlna/issues/7
    
    Merge requests included in this release:
     - https://gitlab.gnome.org/GNOME/gupnp-dlna/merge_requests/1

--- net/gupnp-dlna/buildlink3.mk
+++ net/gupnp-dlna/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.32 2023/04/19 08:08:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2023/08/29 20:06:53 adam Exp $
 
 BUILDLINK_TREE+=	gupnp-dlna
 
 .if !defined(GUPNP_DLNA_BUILDLINK3_MK)
 GUPNP_DLNA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gupnp-dlna+=	gupnp-dlna>=0.10.2
-BUILDLINK_ABI_DEPENDS.gupnp-dlna?=	gupnp-dlna>=0.10.3nb27
+BUILDLINK_API_DEPENDS.gupnp-dlna+=	gupnp-dlna>=0.12
+BUILDLINK_ABI_DEPENDS.gupnp-dlna+=	gupnp-dlna>=0.12
 BUILDLINK_PKGSRCDIR.gupnp-dlna?=	../../net/gupnp-dlna
 
 .include "../../net/gupnp/buildlink3.mk"
-#.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../multimedia/gstreamer1/buildlink3.mk"
 .include "../../multimedia/gst-plugins1-base/buildlink3.mk"
 .endif	# GUPNP_DLNA_BUILDLINK3_MK
