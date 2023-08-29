3d639a2ce8a0cfcddada294a2ae71eb76fce73af
adam <adam@pkgsrc.org>
2023-08-29 11:12:13 +0000

    gtksourceview4: updated to 4.8.4
    News in 4.8.4, 2022-11-03
    -------------------------
    * Style updates to kate, classic, tango
    * Language updates to vala, python3, c, cuda, latex
    * Add unit tests for language specs
    * Translation updates
    
    News in 4.8.3, 2022-03-18
    -------------------------
    
    * Adds support for more Pango font-variants
    * Style updates to solarized-dark, solarized-light
    * Language updates to lean, rst, c, gtk-doc, javascript, and json
    * Translation updates
    
    News in 4.8.2, 2021-09-03
    -------------------------
    * New syntax highlighting definition file for: STAR
    * Improved syntax highlighting definition file for: YARA, LLVM
    * GtkSourceMap now avoids animation while scrolling in response
      to clicks. This should help prevent some hysteresis.
    * GObject Introspection fixes
    * Fixed decrement of negative numbers in GtkSourceView
    * Improvements to style schemes: kate
    * Fixed build-time macros for version checking in some scenarios
    * Translation updates
    
    News in 4.8.1, 2021-03-02
    -------------------------
    * Fix highlighting in presence of byte-order-marks (BOM)
    * meson.lang: Update keywords to 0.55.3 syntax
    * cmake.lang: Update keywords to 3.18.2 syntax
    * go.lang: make "0i" syntax highlight as integer
    * Translation updates

--- x11/gtksourceview4/buildlink3.mk
+++ x11/gtksourceview4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2023/08/14 05:25:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2023/08/29 11:12:13 adam Exp $
 
 BUILDLINK_TREE+=	gtksourceview4
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtksourceview4
 GTKSOURCEVIEW4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtksourceview4+=	gtksourceview4>=4.0.0
-BUILDLINK_ABI_DEPENDS.gtksourceview4?=	gtksourceview4>=4.8.0nb11
+BUILDLINK_ABI_DEPENDS.gtksourceview4+=	gtksourceview4>=4.8.0nb11
 BUILDLINK_PKGSRCDIR.gtksourceview4?=	../../x11/gtksourceview4
 
 .include "../../textproc/libxml2/buildlink3.mk"
