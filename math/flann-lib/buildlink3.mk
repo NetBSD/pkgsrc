3a672154f05f9337b58952d107b0658731bc01d1
adam <adam@pkgsrc.org>
2023-08-31 10:22:29 +0000

    flann-lib: buildlinking needs lz4; switch to cmake/build.mk

--- math/flann-lib/buildlink3.mk
+++ math/flann-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/08/14 05:24:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/08/31 10:22:29 adam Exp $
 
 BUILDLINK_TREE+=	flann-lib
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.flann-lib+=	flann-lib>=1.9.0
 BUILDLINK_ABI_DEPENDS.flann-lib+=	flann-lib>=1.9.2nb1
 BUILDLINK_PKGSRCDIR.flann-lib?=		../../math/flann-lib
 
+.include "../../archivers/lz4/buildlink3.mk"
 .include "../../devel/hdf5-c++/buildlink3.mk"
 
 .endif # FLANN_LIB_BUILDLINK3_MK
