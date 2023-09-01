8cf993f5adffa2d5fd888c8d06013dd5d091f06e
osa <osa@pkgsrc.org>
2023-09-01 17:28:28 +0000

    */*unit*: update NGINX Unit: 1.30.0 -> 1.31.0
    <ChangeLog>
    
    *) Change: if building with njs, version 0.8.0 or later is now required.
    
    *) Feature: technology preview of WebAssembly application module.
    
    *) Feature: "response_headers" option to manage headers in the action
       and fallback.
    
    *) Feature: HTTP response header variables.
    
    *) Feature: ASGI lifespan state support. Thanks to synodriver.
    
    *) Bugfix: ensure that $uri variable is not cached.
    
    *) Bugfix: deprecated options were unavailable.
    
    *) Bugfix: ASGI applications inaccessible over IPv6.
    
    </ChangeLog>

--- www/unit/version.mk
+++ www/unit/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.9 2023/05/15 22:24:38 osa Exp $
+# $NetBSD: version.mk,v 1.10 2023/09/01 17:28:28 osa Exp $
 #
 # UNIT_EXTENSION_DIR
 #	Relative path to ${PREFIX} for NGINX Unit's modules.
@@ -12,7 +12,7 @@
 UNITVERSION_MK=	defined
 
 # Define NGINX Unit's version.
-UNIT_VERSION=	1.30.0
+UNIT_VERSION=	1.31.0
 
 # Define NGINX Unit's modules directory
 UNIT_EXTENSION_DIR=	libexec/unit/modules
