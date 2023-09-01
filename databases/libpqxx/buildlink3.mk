996b4249a7b8f0c134292f33f49db9e3706e688b
nia <nia@pkgsrc.org>
2023-09-01 10:49:07 +0000

    libpqxx: Convert to USE_CXX_FEATURES.

--- databases/libpqxx/buildlink3.mk
+++ databases/libpqxx/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/08/14 05:23:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/09/01 10:49:07 nia Exp $
 
 BUILDLINK_TREE+=	libpqxx
 
 .if !defined(LIBPQXX_BUILDLINK3_MK)
 LIBPQXX_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++17
 
 BUILDLINK_API_DEPENDS.libpqxx+=	libpqxx>=7.1.2
 BUILDLINK_ABI_DEPENDS.libpqxx?=	libpqxx>=7.7.4nb1
