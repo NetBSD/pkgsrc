2658c3d1b73ab3f88b0761f8ec34a5daa240ec5d
osa <osa@pkgsrc.org>
2023-09-01 17:06:02 +0000

    devel/libnjs: update libnjs API version to 0.8.0

--- devel/libnjs/buildlink3.mk
+++ devel/libnjs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/05/17 16:44:51 osa Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/09/01 17:06:02 osa Exp $
 
 BUILDLINK_TREE+=	libnjs
 
 .if !defined(LIBNJS_BUILDLINK3_MK)
 LIBNJS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libnjs+=	libnjs>=0.7.12
+BUILDLINK_API_DEPENDS.libnjs+=	libnjs>=0.8.0
 BUILDLINK_PKGSRCDIR.libnjs?=	../../devel/libnjs
 
 .endif # LIBNJS_BUILDLINK3_MK
