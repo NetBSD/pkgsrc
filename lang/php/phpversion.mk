68a42d86cfdbf57d80b0fc9125061adebf7be8cd
taca <taca@pkgsrc.org>
2023-09-02 14:49:39 +0000

    lang/php81: update to 8.1.23
    31 Aug 2023, PHP 8.1.23
    
    - CLI:
      . Fixed bug GH-11716 (cli server crashes on SIGINT when compiled with
        ZEND_RC_DEBUG=1). (nielsdos)
      . Fixed bug GH-10964 (Improve man page about the built-in server).
        (Alexandre Daubois)
    
    - Core:
      . Fixed strerror_r detection at configuration time. (Kévin Dunglas)
    
    - Date:
      . Fixed bug GH-11416: Crash with DatePeriod when uninitialised objects
        are passed in. (Derick)
    
    - DOM:
      . Fix DOMEntity field getter bugs. (nielsdos)
      . Fix incorrect attribute existence check in DOMElement::setAttributeNodeNS.
        (nielsdos)
      . Fix DOMCharacterData::replaceWith() with itself. (nielsdos)
      . Fix empty argument cases for DOMParentNode methods. (nielsdos)
      . Fixed bug GH-11791 (Wrong default value of DOMDocument::xmlStandalone).
        (nielsdos)
      . Fix json_encode result on DOMDocument. (nielsdos)
      . Fix manually calling __construct() on DOM classes. (nielsdos)
      . Fixed bug GH-11830 (ParentNode methods should perform their checks
        upfront). (nielsdos)
      . Fix segfault when DOMParentNode::prepend() is called when the child
        disappears. (nielsdos)
    
    - FFI:
      . Fix leaking definitions when using FFI::cdef()->new(...). (ilutov)
    
    - MySQLnd:
      . Fixed bug GH-11440 (authentication to a sha256_password account fails over
        SSL). (nielsdos)
      . Fixed bug GH-11438 (mysqlnd fails to authenticate with sha256_password
        accounts using passwords longer than 19 characters).
        (nielsdos, Kamil Tekiela)
      . Fixed bug GH-11550 (MySQL Statement has a empty query result when
        the response field has changed, also Segmentation fault).
        (Yurunsoft)
      . Fixed invalid error message "Malformed packet" when connection is dropped.
        (Kamil Tekiela)
    
    - Opcache:
      . Fixed bug GH-11715 (opcache.interned_strings_buffer either has no effect or
        opcache_get_status() / phpinfo() is wrong). (nielsdos)
      . Avoid adding an unnecessary read-lock when loading script from shm if
        restart is in progress. (mikhainin)
    
    - PCNTL:
      . Revert behaviour of receiving SIGCHLD signals back to the behaviour
        before 8.1.22. (nielsdos)
    
    - SPL:
      . Fixed bug #81992 (SplFixedArray::setSize() causes use-after-free).
        (nielsdos)
    
    - Standard:
      . Prevent int overflow on $decimals in number_format. (Marc Bennewitz)
      . Fixed bug GH-11870 (Fix off-by-one bug when truncating tempnam prefix)
        (athos-ribeiro)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.407 2023/09/02 14:47:35 taca Exp $
+# $NetBSD: phpversion.mk,v 1.408 2023/09/02 14:49:39 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.30
-PHP81_VERSION=	8.1.22
+PHP81_VERSION=	8.1.23
 PHP82_VERSION=	8.2.10
 
 # Define API version or initial release of major version.
