8bca5959a596513488752312a2c3e3d36651529f
rhialto <rhialto@pkgsrc.org>
2023-09-02 13:45:14 +0000

    games/nethack*: enable curses user interface
    The "nethack" wrapper script that papers over the existence of nethack-tty
    and nethack-x11 has been made more like the original way to select UIs
    (since there are now 3, and each game binary supports more than one of them).

--- games/nethack-lib/patches/patch-sys_unix_nethack.sh
+++ games/nethack-lib/patches/patch-sys_unix_nethack.sh
@@ -1,13 +1,11 @@
-$NetBSD: patch-sys_unix_nethack.sh,v 1.2 2023/08/27 15:59:19 rhialto Exp $
+$NetBSD: patch-sys_unix_nethack.sh,v 1.3 2023/09/02 13:45:14 rhialto Exp $
 
-Modify the wrapper script so users can choose the UI type on the
-command line.
+Modify the wrapper script to find the app-defaults, the font path,
+the nethack binary with the highest number of UIs.
 
-(Perhaps this should be handled by the pkg_alternatives framework.)
-
---- sys/unix/nethack.sh.orig	2019-05-08 07:00:16.000000000 +0000
+--- sys/unix/nethack.sh.orig	2023-02-15 21:52:57.000000000 +0000
 +++ sys/unix/nethack.sh
-@@ -3,9 +3,32 @@
+@@ -3,23 +3,56 @@
  # Copyright (c) 2015 by Kenneth Lorber, Kensington, Maryland
  # NetHack may be freely redistributed.  See license for details.
  
@@ -15,30 +13,60 @@ command line.
 +HACKDIR="@HACKDIR@"
  export HACKDIR
 -HACK=$HACKDIR/nethack
-+
-+UILIST="tty curses x11 qt" 
-+findbin ()
-+{
-+	for x in $UILIST; do
-+		if [ -x "@PREFIX@/bin/nethack-$x" ]; then
-+			echo $x
-+			return
-+		fi
-+	done
-+}
-+
-+if [ -z "$1" -o -z "${1%%-*}" ]; then
-+	UI=`findbin`
-+else
-+	UI=$1
-+    shift
+ 
+-# Since Nethack.ad is installed in HACKDIR, add it to XUSERFILESEARCHPATH
++UILIST="curses tty" 
++if [ -n "$DISPLAY" ]
++then
++    UILIST="qt x11 $UILIST" 
 +fi
 +
-+if [ -z "$UI" ]; then
-+	echo "No UI ($UILIST) found"
++case "$1" in
++	qt|x11|curses|tty)
++	    echo "Please select the UI by putting 'windowtype:$1' in your \$NETHACKOPTIONS"
++	    echo "or 'OPTIONS=windowtype:$1' at the top of your \$HOME/.nethackrc file."
++	    exit 1
++	    ;;
++esac
++
++HACK=""
++
++for x in $UILIST
++do
++	if [ -x "@PREFIX@/bin/nethack-$x" ]
++	then
++		HACK="@PREFIX@/bin/nethack-$x"
++		break
++	fi
++done
++
++if [ -z "$HACK" ]
++then
++	echo "No UI found (nethack-$UILIST)"
 +	exit 1
 +fi
-+HACK="@PREFIX@/bin/nethack-${UI}"
- 
- # Since Nethack.ad is installed in HACKDIR, add it to XUSERFILESEARCHPATH
++
++# Since app-defaults/NetHack is installed in @PREFIX@/lib/X11,
++# add it to XUSERFILESEARCHPATH.
  case "x$XUSERFILESEARCHPATH" in
+-x)	XUSERFILESEARCHPATH="$HACKDIR/%N.ad"
++x)	XUSERFILESEARCHPATH="@PREFIX@/lib/X11/app-defaults/%N"
+ 	;;
+-*)	XUSERFILESEARCHPATH="$XUSERFILESEARCHPATH:$HACKDIR/%N.ad"
++*)	XUSERFILESEARCHPATH="$XUSERFILESEARCHPATH:@PREFIX@/lib/X11/app-defaults/%N"
+ 	;;
+ esac
+ export XUSERFILESEARCHPATH
+ 
+ # Get font dir added, but only once (and only if there's an xset to be found).
+-test -n "$DISPLAY" -a -f $HACKDIR/fonts.dir && xset p >/dev/null 2>&1 && (
+-	xset fp- $HACKDIR >/dev/null 2>&1;
+-	xset fp+ $HACKDIR
++test -n "$DISPLAY" -a -f @PREFIX@/share/fonts/X11/misc/fonts.dir && xset p >/dev/null 2>&1 && (
++	if ! xset q | grep @PREFIX@/share/fonts/X11/misc >/dev/null 2>&1
++	then
++	    xset fp+ @PREFIX@/share/fonts/X11/misc
++	fi
+ )
+ 
+ # see if we can find the full path name of PAGER, so help files work properly
