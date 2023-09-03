a6628e71c75043eff96f45a4b41f83b578471b36
tsutsui <tsutsui@pkgsrc.org>
2023-09-03 10:22:12 +0000

    ruby-gnome: update to 4.2.0.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.2.0
    
    Ruby-GNOME 4.2.0: 2023-08-19
    
    This is a minor bug fix release.
    
    Changes
    
    Ruby/GTK3
    
      * Fixes
    
          + Suppressed a build type warning.

--- devel/ruby-pango/buildlink3.mk
+++ devel/ruby-pango/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/08/12 08:37:20 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/09/03 10:22:12 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-pango
 
 .if !defined(RUBY_PANGO_BUILDLINK3_MK)
 RUBY_PANGO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.1.9
-BUILDLINK_ABI_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.1.9
+BUILDLINK_API_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.2.0
+BUILDLINK_ABI_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.2.0
 BUILDLINK_PKGSRCDIR.ruby-pango?=	../../devel/ruby-pango
 
 .include "../../devel/pango/buildlink3.mk"
--- x11/ruby-gtk3/buildlink3.mk
+++ x11/ruby-gtk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/08/12 08:37:21 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/09/03 10:22:14 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gtk3
 
 .if !defined(RUBY_GTK3_BUILDLINK3_MK)
 RUBY_GTK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.9
-BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.1.9
+BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.2.0
+BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.2.0
 BUILDLINK_PKGSRCDIR.ruby-gtk3?=		../../x11/ruby-gtk3
 
 .include "../../graphics/ruby-gdk3/buildlink3.mk"
