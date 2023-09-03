e05e540aadcc6f98c3cb6c9b9b6a86c045bb93e3
adam <adam@pkgsrc.org>
2023-09-03 11:58:54 +0000

    gupnp: needs xsltproc to build; pkglint fix while here

--- net/gupnp/buildlink3.mk
+++ net/gupnp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.55 2023/09/01 19:02:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.56 2023/09/03 11:58:54 adam Exp $
 
 BUILDLINK_TREE+=	gupnp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gupnp
 GUPNP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gupnp+=	gupnp>=1.6
-BUILDLINK_ABI_DEPENDS.gupnp?=	gupnp>=1.6
+BUILDLINK_ABI_DEPENDS.gupnp+=	gupnp>=1.6
 BUILDLINK_PKGSRCDIR.gupnp?=	../../net/gupnp
 
 .include "../../mk/bsd.fast.prefs.mk"
