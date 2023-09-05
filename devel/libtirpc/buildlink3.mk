46aff3a249eee9e209a0f1cc5b3a00acb0f5b309
vins <vins@pkgsrc.org>
2023-09-05 08:05:52 +0000

    libtirpc: s/CFLAGS/CPPFLAGS in buildlink file.

--- devel/libtirpc/buildlink3.mk
+++ devel/libtirpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/09/03 12:35:59 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/09/05 08:05:52 vins Exp $
 
 BUILDLINK_TREE+=		libtirpc
 
@@ -8,7 +8,7 @@ LIBTIRPC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libtirpc+=	libtirpc>=1.3.3
 BUILDLINK_PKGSRCDIR.libtirpc?=		../../devel/libtirpc
 
-BUILDLINK_CFLAGS.libtirpc+=	-I${BUILDLINK_PREFIX.libtirpc}/include/tirpc
+BUILDLINK_CPPFLAGS.libtirpc+=	-I${BUILDLINK_PREFIX.libtirpc}/include/tirpc
 BUILDLINK_LDFLAGS.libtirpc+=	-ltirpc
 
 .endif	# LIBTIRPC_BUILDLINK3_MK
