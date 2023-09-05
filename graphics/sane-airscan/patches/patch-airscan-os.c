cbe5f4546ce9f2db79265dae409960dd761dc78d
ryoon <ryoon@pkgsrc.org>
2023-09-05 14:42:17 +0000

    sane-airscan: Update to 0.99.27
    * Changelog is not provided in concise form. Changes are mostly bugfix.

--- graphics/sane-airscan/patches/patch-airscan-os.c
+++ graphics/sane-airscan/patches/patch-airscan-os.c
@@ -1,22 +1,22 @@
-$NetBSD: patch-airscan-os.c,v 1.1 2020/12/09 12:05:58 ryoon Exp $
+$NetBSD: patch-airscan-os.c,v 1.2 2023/09/05 14:42:18 ryoon Exp $
 
 * Support NetBSD.
 
---- airscan-os.c.orig	2020-11-23 19:47:02.000000000 +0000
+--- airscan-os.c.orig	2021-10-04 18:36:05.000000000 +0000
 +++ airscan-os.c
 @@ -17,7 +17,7 @@
  #include <unistd.h>
  #include <sys/stat.h>
  
--#ifdef __OpenBSD__
-+#if defined(__OpenBSD__) || defined(__NetBSD__)
+-#if defined(__OpenBSD__) || defined(__FreeBSD__)
++#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__)
  #   include <sys/types.h>
  #   include <sys/sysctl.h>
  #endif
-@@ -92,6 +92,14 @@ os_progname_init (void)
-         return;
+@@ -101,6 +101,14 @@ os_progname_init (void)
+     if (rc < 0) {
+         os_progname_buf[0] = '\0'; /* Just a paranoia */
      }
-     memmove(os_progname_buf, kp.p_comm, KI_MAXCOMLEN);
 +#elif defined(__NetBSD__)
 +    struct kinfo_proc2 kp;
 +    int mib[] = {CTL_KERN, KERN_PROC2, KERN_PROC_PID, getpid(), sizeof(kp), 1};
