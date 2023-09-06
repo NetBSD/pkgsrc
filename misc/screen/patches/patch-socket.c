688143c7b0039d554aa375b6cf7b6aa9772207e7
vins <vins@pkgsrc.org>
2023-09-06 16:00:15 +0000

    misc/screen: update to 4.9.1.
    # Changes
    Version 4.9.1 (20/08/2023):
      * Support stop/parity bits on serial port (#23952)
      * Add needed system headers in checks and return values
        for implicit function declarations
      * Fixes:
    	- Avoid zombies after shell exit (#25089)
    	- Missed signal sending permission check on failed
    	  query messages (CVE-2023-24626)
    	- manpage fixes
    	- source code fixes during cleanup
    	- UTF-8 encoding can emit invalid UTF-8 sequences
    	  for out of range unicode values (#62097)

--- misc/screen/patches/patch-socket.c
+++ misc/screen/patches/patch-socket.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-socket.c,v 1.3 2017/07/18 23:02:18 kim Exp $
+$NetBSD: patch-socket.c,v 1.4 2023/09/06 16:00:15 vins Exp $
+
+Include <uio.h> for iovec. 
 
 --- socket.c.orig	2017-07-10 19:26:25.000000000 +0000
 +++ socket.c	2017-07-18 22:35:40.000000000 +0000
