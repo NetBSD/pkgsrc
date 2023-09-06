d4b8656fa5e12131c2eeb787e2162ea7d952a489
vins <vins@pkgsrc.org>
2023-09-06 10:42:51 +0000

    devel/libtirpc: further buildlink file revision.
    * use INCDIRS to provide a language-agnostic include path.
    * fix linking for TI-RPC.

--- devel/libtirpc/buildlink3.mk
+++ devel/libtirpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/09/05 08:05:52 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/09/06 10:42:51 vins Exp $
 
 BUILDLINK_TREE+=		libtirpc
 
@@ -8,8 +8,9 @@ LIBTIRPC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libtirpc+=	libtirpc>=1.3.3
 BUILDLINK_PKGSRCDIR.libtirpc?=		../../devel/libtirpc
 
-BUILDLINK_CPPFLAGS.libtirpc+=	-I${BUILDLINK_PREFIX.libtirpc}/include/tirpc
-BUILDLINK_LDFLAGS.libtirpc+=	-ltirpc
+BUILDLINK_INCDIRS.libtirpc?=	include/tirpc
+BUILDLINK_LIBNAME.libtirpc=	tirpc
+BUILDLINK_LIBS.libtirpc=	${BUILDLINK_LIBNAME.libtirpc:S/^/-l/:S/^-l$//}
 
 .endif	# LIBTIRPC_BUILDLINK3_MK
 
