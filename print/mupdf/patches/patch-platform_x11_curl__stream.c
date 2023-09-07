db25f6df0fe69fceff3593297a1399dcfa0e074b
vins <vins@pkgsrc.org>
2023-09-07 14:36:54 +0000

    print/mupdf: update to MuPDF 1.23.3
    # Changes (local)
    
      * Import patch from OpenBSD ports, adding support for fine-grained zoom.
      * Fix scrolling with vi-like bindings (from FreeBSD ports).
      * Use REPLACE_INTERPRETER instead of an ad-hoc patch.
      * Don't install html documentation (turn it into an option in future?)
      * Provide .desktop application file and pixmap for mupdf.
      * Avoid executable bit being set on static libs.
      * Other minor fixes.
      * Package linted.
    
    # Changes (upstream)
    
    ## 1.23.3 (2023-09-05)
    
      * Fix bug where an inconsistent xref caused problems when saving PDF.
    
    ## 1.23.2 (2023-08-31)
    
      * Fixes needed to allow tesseract builds in C++/Python bindings.
    
    ## 1.23.1 (2023-08-29)
    
      * Fix bug where implicit operations where not ended when creating
        signature widget annotations.
      * Fix bug where saving an encrypted PDF requiring encryption could
        cause an invalid saved file.
      * Fix bug where ink annotation line caps adhered to rendering in older
        Adobe Acrobat Reader.
      * Fix compilation issue of shared library used in pythong bindings.
    
    ## 1.23.0 (2023-08-22)
    
     New features:
    
      * New WASM library with same API as mutool run and Java, for both
        browser and Node environments.
      * Support CropBox, TrimBox, BleedBox, and ArtBox in PDF tools and
        viewers.
      * PhotoShop PSD image support.
      * mupdf-gl: Custom ICC display profile support.
      * mutool poster: Option to split in RTL direction.
    
     Miscellaneous improvements and notable bug fixes:
    
      * EPUB: Fix table cell height calculations.
      * EPUB: Inherit table cell background color from table row.
      * EPUB: Support files with partial encryption (only read unencrypted
        parts).
      * EPUB: Support files with incorrect directory prefixes.
      * TIFF: Stability improvements.
      * MOBI: Stability improvements.
      * PDF: Support old style border dash patterns.
      * PDF: Support GoToR links to remote PDF documents.
      * PDF: Improve link parsing and creation.
      * ZIP: Improve unicode file name handling.
      * Fall back to unhinted fonts if hints are broken.
      * Recognize document types by sniffing contents (don't need to rely on
        mimetype or file extension).
    
     New APIs:
    
      * More PDF document permission flags.
      * Tweaked exception error logging.
      * pdf_minimize_document to squeeze amount of memory used by an open
        document.
    
     Incompatible API changes:
    
      * pdf_field_name renamed to pdf_load_field_name.
      * mutool run: Changed many methods to match Java and new WASM library.
    
     New and improved documentation.

---
+++ print/mupdf/patches/patch-platform_x11_curl__stream.c
@@ -0,0 +1,51 @@
+$NetBSD: patch-platform_x11_curl__stream.c,v 1.1 2023/09/07 14:36:54 vins Exp $
+
+offset is ia 64-bit integer.
+
+--- platform/x11/curl_stream.c.orig	2023-09-05 11:51:19.000000000 +0000
++++ platform/x11/curl_stream.c
+@@ -397,7 +397,7 @@ static int cs_next(fz_context *ctx, fz_s
+ 	{
+ 		unlock(state);
+ 		if (state->data_arrived == 0)
+-			fz_throw(ctx, err_type, "read of a block we don't have (A) (offset=%ld)", read_point);
++			fz_throw(ctx, err_type, "read of a block we don't have (A) (offset=%lld)", read_point);
+ 		return EOF;
+ 	}
+ 
+@@ -411,7 +411,7 @@ static int cs_next(fz_context *ctx, fz_s
+ 		if (read_point + len > state->current_fill_start)
+ 		{
+ 			unlock(state);
+-			fz_throw(ctx, err_type, "read of a block we don't have (B) (offset=%ld)", read_point);
++			fz_throw(ctx, err_type, "read of a block we don't have (B) (offset=%lld)", read_point);
+ 		}
+ 		memcpy(buf, state->buffer + read_point, len);
+ 		unlock(state);
+@@ -434,7 +434,7 @@ static int cs_next(fz_context *ctx, fz_s
+ 		{
+ 			state->next_fill_start = block<<BLOCK_SHIFT;
+ 			unlock(state);
+-			fz_throw(ctx, err_type, "read of a block we don't have (C) (offset=%ld)", read_point);
++			fz_throw(ctx, err_type, "read of a block we don't have (C) (offset=%lld)", read_point);
+ 		}
+ 		block++;
+ 		memcpy(buf, state->buffer + read_point, left_over);
+@@ -457,7 +457,7 @@ static int cs_next(fz_context *ctx, fz_s
+ 			stream->pos += len_read;
+ 			/* If we haven't fetched anything, throw. */
+ 			if (len_read == 0)
+-				fz_throw(ctx, err_type, "read of a block we don't have (D) (offset=%ld)", read_point);
++				fz_throw(ctx, err_type, "read of a block we don't have (D) (offset=%lld)", read_point);
+ 			/* Otherwise, we got at least one byte, so we can safely return that. */
+ 			return *stream->rp++;
+ 		}
+@@ -482,7 +482,7 @@ static int cs_next(fz_context *ctx, fz_s
+ 			stream->pos += len_read;
+ 			/* If we haven't fetched anything, throw. */
+ 			if (len_read == 0)
+-				fz_throw(ctx, err_type, "read of a block we don't have (E) (offset=%ld)", read_point);
++				fz_throw(ctx, err_type, "read of a block we don't have (E) (offset=%lld)", read_point);
+ 			/* Otherwise, we got at least one byte, so we can safely return that. */
+ 			return *stream->rp++;
+ 		}
