1a49b23af6dc53e67266970421dce069567b2546
vins <vins@pkgsrc.org>
2023-09-07 19:38:14 +0000

    filesystems/fuse: update to fuse-2.9.9.
    This is the last and most recent maintainance release for the 2.x
    branch.
    
    # Changes (since version 2.9.3)
    
    FUSE 2.9.9 (2019-01-04)
    =======================
    
    * Added OpenAFS to whitelist (so users can now mount FUSE filesystems
      on mountpoints within OpenAFS filesystems).
    * Added a test of `seekdir` to test_syscalls.
    * Fixed `readdir` bug when non-zero offsets are given to filler and the
      filesystem client, after reading a whole directory, re-reads it from a
      non-zero offset e. g. by calling `seekdir` followed by `readdir`.
    
    FUSE 2.9.8 (2018-07-24)
    =======================
    
    * SECURITY UPDATE: In previous versions of libfuse it was possible to
      for unprivileged users to specify the `allow_other` option even when
      this was forbidden in `/etc/fuse.conf`.  The vulnerability is
      present only on systems where SELinux is active (including in
      permissive mode).
    * libfuse no longer segfaults when fuse_interrupted() is called outside
      the event loop.
    * The fusermount binary has been hardened in several ways to reduce
      potential attack surface. Most importantly, mountpoints and mount
      options must now match a hard-coded whitelist. It is expected that
      this whitelist covers all regular use-cases.
    * Fixed rename deadlock on FreeBSD.
    
    FUSE 2.9.7 (2016-06-20)
    =======================
    
    * Added SELinux support.
    * Fixed race-condition when session is terminated right after starting
      a FUSE file system.
    
    FUSE 2.9.6 (2016-04-23)
    =======================
    
    * Tarball now includes documentation.
    * Shared-object version has now been bumped correctly.
    
    FUSE 2.9.5 (2016-01-14)
    =======================
    
    * New maintainer: Nikolaus Rath <Nikolaus@rath.org>. Many thanks to
      Miklos Szeredi <miklos@szeredi.hu> for bringing FUSE to where it is
      now!
    
    * fix warning in mount.c:receive_fd().  Reported by Albert Berger
    
    * fix possible memory leak.  Reported by Jose R. Guzman
    
    FUSE 2.9.4 (2015-05-22)
    =======================
    
    * fix exec environment for mount and umount.  Found by Tavis Ormandy
      (CVE-2015-3202).
    
    * fix fuse_remove_signal_handlers() to properly restore the default
      signal handler.  Reported by: Chris Johnson
    
    * highlevel API: fix directory file handle passed to ioctl() method.
      Reported by Eric Biggers
    
    * libfuse: document deadlock avoidance for fuse_notify_inval_entry()
      and fuse_notify_delete()
    
    * fusermount, libfuse: send value as unsigned in "user_id=" and
      "group_id=" options.  Uids/gids larger than 2147483647 would result
      in EINVAL when mounting the filesystem.  This also needs a fix in
      the kernel.
    
    * Initilaize stat buffer passed to ->getattr() and ->fgetattr() to
      zero in all cases.  Reported by Daniel Iwan
    
    * libfuse: Add missing includes.  This allows compiling fuse with
      musl.  Patch by Daniel Thau

--- filesystems/fuse/buildlink3.mk
+++ filesystems/fuse/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/09/06 12:13:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/09/07 19:38:14 vins Exp $
 
 BUILDLINK_TREE+=	fuse
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	fuse
 FUSE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fuse+=	fuse>=2.5.2
+BUILDLINK_ABI_DEPENDS.fuse+=	fuse>=2.9.9
 BUILDLINK_PKGSRCDIR.fuse?=	../../filesystems/fuse
 
 .include "../../mk/bsd.fast.prefs.mk"
--- filesystems/fuse/builtin.mk
+++ filesystems/fuse/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.5 2019/11/03 19:26:20 rillig Exp $
+# $NetBSD: builtin.mk,v 1.6 2023/09/07 19:38:14 vins Exp $
 
 BUILTIN_PKG:=		fuse
 PKGCONFIG_BASE.fuse=	/usr
@@ -7,7 +7,7 @@ PKGCONFIG_FILE.fuse+=	/usr/lib${LIBABISUFFIX}/pkgconfig/fuse.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 
-.if !empty(USE_BUILTIN.fuse:M[yY][eE][sS])
+.if ${USE_BUILTIN.fuse:tl} == yes
 BUILDLINK_FILES.fuse+=	lib/pkgconfig/fuse.pc
 BUILDLINK_FILES.fuse+=	lib${LIBABISUFFIX}/pkgconfig/fuse.pc
 .endif
