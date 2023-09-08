af6e87b91bbde9e5ed7d052e36c01e394f674068
bsiegert <bsiegert@pkgsrc.org>
2023-09-08 18:49:45 +0000

    go120: update to 1.20.8 (security)
    This minor releases includes 4 security fixes following the security policy:
    
    - cmd/go: go.mod toolchain directive allows arbitrary execution
    
      The go.mod toolchain directive, introduced in Go 1.21, could be leveraged to
      execute scripts and binaries relative to the root of the module when the
      "go" command was executed within the module. This applies to modules
      downloaded using the "go" command from the module proxy, as well as modules
      downloaded directly using VCS software.
    
      Thanks to Juho Nurminen of Mattermost for reporting this issue.
    
      This is CVE-2023-39320 and Go issue https://go.dev/issue/62198.
    
    - html/template: improper handling of HTML-like comments within script
      contexts
    
      The html/template package did not properly handle HMTL-like "<!--" and "-->"
      comment tokens, nor hashbang "#!" comment tokens, in <script> contexts. This
      may cause the template parser to improperly interpret the contents of
      <script> contexts, causing actions to be improperly escaped. This could be
      leveraged to perform an XSS attack.
    
      Thanks to Takeshi Kaneko (GMO Cybersecurity by Ierae, Inc.) for reporting
      this issue.
    
      This is CVE-2023-39318 and Go issue https://go.dev/issue/62196.
    
    - html/template: improper handling of special tags within script contexts
    
      The html/template package did not apply the proper rules for handling
      occurrences of "<script", "<!--", and "</script" within JS literals in
      <script> contexts.  This may cause the template parser to improperly
      consider script contexts to be terminated early, causing actions to be
      improperly escaped. This could be leveraged to perform an XSS attack.
    
      Thanks to Takeshi Kaneko (GMO Cybersecurity by Ierae, Inc.) for reporting
      this issue.
    
      This is CVE-2023-39319 and Go issue https://go.dev/issue/62197.
    
    - crypto/tls: panic when processing post-handshake message on QUIC connections
    
      Processing an incomplete post-handshake message for a QUIC connection caused
      a panic.
    
      Thanks to Marten Seemann for reporting this issue.
    
      This is CVE-2023-39321 and CVE-2023-39322 and Go issue
      https://go.dev/issue/62266.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.187 2023/09/08 13:06:29 bsiegert Exp $
+# $NetBSD: version.mk,v 1.188 2023/09/08 18:49:45 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO121_VERSION=	1.21.1
-GO120_VERSION=	1.20.7
+GO120_VERSION=	1.20.8
 GO119_VERSION=	1.19.12
 GO118_VERSION=	1.18.10
 GO14_VERSION=	1.4.3
