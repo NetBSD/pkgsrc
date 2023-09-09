1bd455b6f91caebe6bc122e30784ff4e8836fa2e
wiz <wiz@pkgsrc.org>
2023-09-09 20:43:42 +0000

    librsvg: update to 2.56.3.
    Version 2.56.3
    ==============
    
    This is a security release for bug #996.
    
    - #996 - Fix arbitrary file read when href has special characters.
    
    - #998 - Fix cascade for symbol elements being referenced from use elements.
    
    Version 2.56.2
    ==============
    
    - Windows build fixes (Chun-wei Fan).
    
    Version 2.56.1
    ==============
    
    - The minimum supported Rust version (MSRV) is 1.65.  Unfortunately
      the assert_cmd crate, used in the test suite, bumped its MSRV and is
      forcing us to do the same.
    
    - #965 - Shrink the shared library by telling the linker to omit
      unused code (Sebastian Dröge).
    
    - Updates to dependencies (Lukáš Tyrychtr, Kalev Lember).

--- graphics/librsvg/patches/patch-.._vendor_cfg-expr-0.15.3_src_targets_builtins.rs
+++ graphics/librsvg/patches/patch-.._vendor_cfg-expr-0.15.3_src_targets_builtins.rs
@@ -1,18 +1,19 @@
-$NetBSD: patch-.._vendor_cfg-expr-0.11.0_src_targets_builtins.rs,v 1.1 2023/04/11 05:45:45 adam Exp $
+$NetBSD: patch-.._vendor_cfg-expr-0.15.3_src_targets_builtins.rs,v 1.1 2023/09/09 20:43:42 wiz Exp $
 
 Add i586-unknown-netbsd.
 Needed because of lang/rust/patches/patch-compiler_rustc__target_src_spec_mod.rs
 Note that the implementation does a binary search of the target list.
 It must be sorted lexically!
 
---- ../vendor/cfg-expr-0.11.0/src/targets/builtins.rs.orig	2006-07-24 01:21:28.000000000 +0000
-+++ ../vendor/cfg-expr-0.11.0/src/targets/builtins.rs
-@@ -854,6 +854,18 @@ pub const ALL_BUILTINS: &[TargetInfo] = 
+--- ../vendor/cfg-expr-0.15.3/src/targets/builtins.rs.orig	2006-07-24 01:21:28.000000000 +0000
++++ ../vendor/cfg-expr-0.15.3/src/targets/builtins.rs
+@@ -1041,6 +1041,19 @@ pub const ALL_BUILTINS: &[TargetInfo] = 
          panic: Panic::unwind,
      },
      TargetInfo {
 +        triple: Triple::new_const("i586-unknown-netbsd"),
 +        os: Some(Os::netbsd),
++        abi: None,
 +        arch: Arch::x86,
 +        env: None,
 +        vendor: Some(Vendor::unknown),
@@ -25,4 +26,4 @@ It must be sorted lexically!
 +    TargetInfo {
          triple: Triple::new_const("i686-apple-darwin"),
          os: Some(Os::macos),
-         arch: Arch::x86,
+         abi: None,
