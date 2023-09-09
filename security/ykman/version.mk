a4ffb4e0036e3b38dac61f1912c20f150cafc7d4
wiz <wiz@pkgsrc.org>
2023-09-09 21:37:49 +0000

    py-yubikey-manager, ykman: update to 5.2.0
    * Version 5.2.0 (released 2023-08-21)
     * PIV: Support for compressed certificates.
     * OpenPGP: Use InvalidPinError for wrong PIN.
     * Add YubiHSM Auth application support.
     * Improved API documentation.
     * Scripting: Add name attribute to device.
     * Bugfix: PIV: don't throw InvalidPasswordError on malformed PEM private key.

--- security/ykman/version.mk
+++ security/ykman/version.mk
@@ -1,6 +1,6 @@
-#	$NetBSD: version.mk,v 1.4 2023/05/16 22:51:02 wiz Exp $
+#	$NetBSD: version.mk,v 1.5 2023/09/09 21:37:49 wiz Exp $
 
 # used by security/py-yubikey-manager/Makefile
 # used by security/ykman/Makefile
 
-VERSION=	5.1.1
+VERSION=	5.2.0
