fab479bf76dfa0f00034ba767c7331a713a1c4e5
ryoon <ryoon@pkgsrc.org>
2023-09-11 13:15:34 +0000

    www/php-nextcloud26: import pphp80-nextcloud-26.0.5
    A safe home for all your data
    
    Access, share and protect your files, calendars, contacts,
    communication & more at home and in your enterprise.
    
    This package tracks 26 release branch.

---
+++ www/php-nextcloud26/options.mk
@@ -0,0 +1,21 @@
+# $NetBSD: options.mk,v 1.1 2023/09/11 13:15:34 ryoon Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.php-nextcloud
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql sqlite3 pgsql
+
+PKG_SUGGESTED_OPTIONS=	sqlite3
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Use mysql, pgsql, or sqlite backend
+###
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PHP_PKG_PREFIX}-pdo_mysql>=5.2.0:../../databases/php-pdo_mysql
+.elif !empty(PKG_OPTIONS:Msqlite3)
+DEPENDS+=	${PHP_PKG_PREFIX}-pdo_sqlite>=5.4.0:../../databases/php-pdo_sqlite
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${PHP_PKG_PREFIX}-pdo_pgsql>=5.4.0:../../databases/php-pdo_pgsql
+.endif
