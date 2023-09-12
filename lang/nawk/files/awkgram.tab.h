12f162f5cf5edd6d3850f4506e47bf34b6e48e7a
vins <vins@pkgsrc.org>
2023-09-12 19:16:52 +0000

    lang/nawk: update to release 20230911
    This release marks the official 2nd edition of the AWK programming
    language!
    
    # CHANGES (since 20220122)
    
    Sep 11, 2023:
    	Added --csv option to enable processing of comma-separated
    	values inputs.  When --csv is enabled, fields are separated
    	by commas, fields may be quoted with " double quotes, fields
    	may contain embedded newlines.
    
    	If no explicit separator argument is provided, split() uses
    	the setting of --csv to determine how fields are split.
    
    	Strings may now contain UTF-8 code points (not necessarily
    	characters).  Functions that operate on characters, like
    	length, substr, index, match, etc., use UTF-8, so the length
    	of a string of 3 emojis is 3, not 12 as it would be if bytes
    	were counted.
    
    	Regular expressions are processes as UTF-8.
    
    	Unicode literals can be written as \u followed by one
    	to eight hexadecimal digits.  These may appear in strings and
    	regular expressions.
    
    Sep 06, 2023:
    	Fix edge case where FS is changed on commandline. Thanks to
    	Gordon Shephard and Miguel Pineiro Jr.
    
    	Fix regular expression clobbering in the lexer, where lexer does
    	not make a copy of regexp literals. also makedfa memory leaks have
    	been plugged. Thanks to Miguel Pineiro Jr.
    
    Dec 15, 2022:
    	Force hex escapes in strings to be no more than two characters,
    	as they already are in regular expressions. This brings internal
    	consistency, as well as consistency with gawk. Thanks to
    	Arnold Robbins.
    
    Sep 12, 2022:
    	adjbuf minlen error (cannot be 0) in cat, resulting in NULL pbuf.
    	discovered by todd miller. also use-after-free issue with
    	tempfree in cat, thanks to Miguel Pineiro Jr and valgrind.
    
    Aug 30, 2022:
    	Various leaks and use-after-free issues plugged/fixed.
    	Thanks to Miguel Pineiro Jr. <mpj@pineiro.cc>.
    
    May 23, 2022:
    	Memory leak when assigning a string to some of the built-in
    	variables. allocated string erroneously marked DONTFREE.
    	Thanks to Miguel Pineiro Jr. <mpj@pineiro.cc>.
    
    Mar 14, 2022:
    	Historic bug: command-line "name=value" assignment had been
    	truncating its entry in ARGV. (circa 1989) Thanks to
    	Miguel Pineiro Jr. <mpj@pineiro.cc>.
    
    Mar 3, 2022:
    	Fixed file management memory leak that appears to have been
    	there since the files array was first initialized with stdin,
    	stdout, and stderr (circa 1992). Thanks to Miguel Pineiro Jr.
    	<mpj@pineiro.cc>.

--- lang/nawk/files/awkgram.tab.h
+++ lang/nawk/files/awkgram.tab.h
@@ -1,182 +1,114 @@
-/* A Bison parser, made by GNU Bison 3.8.2.  */
+#ifndef _yy_defines_h_
+#define _yy_defines_h_
 
-/* Bison interface for Yacc-like parsers in C
-
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
-   Inc.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
-
-/* As a special exception, you may create a larger work that contains
-   part or all of the Bison parser skeleton and distribute that work
-   under terms of your choice, so long as that work isn't itself a
-   parser generator using the skeleton or a modified version thereof
-   as a parser skeleton.  Alternatively, if you modify or redistribute
-   the parser skeleton itself, you may (at your option) remove this
-   special exception, which will cause the skeleton and the resulting
-   Bison output files to be licensed under the GNU General Public
-   License without this special exception.
-
-   This special exception was added by the Free Software Foundation in
-   version 2.2 of Bison.  */
-
-/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
-   especially those whose name start with YY_ or yy_.  They are
-   private implementation details that can be changed or removed.  */
-
-#ifndef YY_YY_AWKGRAM_TAB_H_INCLUDED
-# define YY_YY_AWKGRAM_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
+#define FIRSTTOKEN 257
+#define PROGRAM 258
+#define PASTAT 259
+#define PASTAT2 260
+#define XBEGIN 261
+#define XEND 262
+#define NL 263
+#define ARRAY 264
+#define MATCH 265
+#define NOTMATCH 266
+#define MATCHOP 267
+#define FINAL 268
+#define DOT 269
+#define ALL 270
+#define CCL 271
+#define NCCL 272
+#define CHAR 273
+#define OR 274
+#define STAR 275
+#define QUEST 276
+#define PLUS 277
+#define EMPTYRE 278
+#define ZERO 279
+#define AND 280
+#define BOR 281
+#define APPEND 282
+#define EQ 283
+#define GE 284
+#define GT 285
+#define LE 286
+#define LT 287
+#define NE 288
+#define IN 289
+#define ARG 290
+#define BLTIN 291
+#define BREAK 292
+#define CLOSE 293
+#define CONTINUE 294
+#define DELETE 295
+#define DO 296
+#define EXIT 297
+#define FOR 298
+#define FUNC 299
+#define SUB 300
+#define GSUB 301
+#define IF 302
+#define INDEX 303
+#define LSUBSTR 304
+#define MATCHFCN 305
+#define NEXT 306
+#define NEXTFILE 307
+#define ADD 308
+#define MINUS 309
+#define MULT 310
+#define DIVIDE 311
+#define MOD 312
+#define ASSIGN 313
+#define ASGNOP 314
+#define ADDEQ 315
+#define SUBEQ 316
+#define MULTEQ 317
+#define DIVEQ 318
+#define MODEQ 319
+#define POWEQ 320
+#define PRINT 321
+#define PRINTF 322
+#define SPRINTF 323
+#define ELSE 324
+#define INTEST 325
+#define CONDEXPR 326
+#define POSTINCR 327
+#define PREINCR 328
+#define POSTDECR 329
+#define PREDECR 330
+#define VAR 331
+#define IVAR 332
+#define VARNF 333
+#define CALL 334
+#define NUMBER 335
+#define STRING 336
+#define REGEXPR 337
+#define GETLINE 338
+#define RETURN 339
+#define SPLIT 340
+#define SUBSTR 341
+#define WHILE 342
+#define CAT 343
+#define NOT 344
+#define UMINUS 345
+#define UPLUS 346
+#define POWER 347
+#define DECR 348
+#define INCR 349
+#define INDIRECT 350
+#define LASTTOKEN 351
+#ifdef YYSTYPE
+#undef  YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
 #endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token kinds.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-  enum yytokentype
-  {
-    YYEMPTY = -2,
-    YYEOF = 0,                     /* "end of file"  */
-    YYerror = 256,                 /* error  */
-    YYUNDEF = 257,                 /* "invalid token"  */
-    FIRSTTOKEN = 258,              /* FIRSTTOKEN  */
-    PROGRAM = 259,                 /* PROGRAM  */
-    PASTAT = 260,                  /* PASTAT  */
-    PASTAT2 = 261,                 /* PASTAT2  */
-    XBEGIN = 262,                  /* XBEGIN  */
-    XEND = 263,                    /* XEND  */
-    NL = 264,                      /* NL  */
-    ARRAY = 265,                   /* ARRAY  */
-    MATCH = 266,                   /* MATCH  */
-    NOTMATCH = 267,                /* NOTMATCH  */
-    MATCHOP = 268,                 /* MATCHOP  */
-    FINAL = 269,                   /* FINAL  */
-    DOT = 270,                     /* DOT  */
-    ALL = 271,                     /* ALL  */
-    CCL = 272,                     /* CCL  */
-    NCCL = 273,                    /* NCCL  */
-    CHAR = 274,                    /* CHAR  */
-    OR = 275,                      /* OR  */
-    STAR = 276,                    /* STAR  */
-    QUEST = 277,                   /* QUEST  */
-    PLUS = 278,                    /* PLUS  */
-    EMPTYRE = 279,                 /* EMPTYRE  */
-    ZERO = 280,                    /* ZERO  */
-    AND = 281,                     /* AND  */
-    BOR = 282,                     /* BOR  */
-    APPEND = 283,                  /* APPEND  */
-    EQ = 284,                      /* EQ  */
-    GE = 285,                      /* GE  */
-    GT = 286,                      /* GT  */
-    LE = 287,                      /* LE  */
-    LT = 288,                      /* LT  */
-    NE = 289,                      /* NE  */
-    IN = 290,                      /* IN  */
-    ARG = 291,                     /* ARG  */
-    BLTIN = 292,                   /* BLTIN  */
-    BREAK = 293,                   /* BREAK  */
-    CLOSE = 294,                   /* CLOSE  */
-    CONTINUE = 295,                /* CONTINUE  */
-    DELETE = 296,                  /* DELETE  */
-    DO = 297,                      /* DO  */
-    EXIT = 298,                    /* EXIT  */
-    FOR = 299,                     /* FOR  */
-    FUNC = 300,                    /* FUNC  */
-    SUB = 301,                     /* SUB  */
-    GSUB = 302,                    /* GSUB  */
-    IF = 303,                      /* IF  */
-    INDEX = 304,                   /* INDEX  */
-    LSUBSTR = 305,                 /* LSUBSTR  */
-    MATCHFCN = 306,                /* MATCHFCN  */
-    NEXT = 307,                    /* NEXT  */
-    NEXTFILE = 308,                /* NEXTFILE  */
-    ADD = 309,                     /* ADD  */
-    MINUS = 310,                   /* MINUS  */
-    MULT = 311,                    /* MULT  */
-    DIVIDE = 312,                  /* DIVIDE  */
-    MOD = 313,                     /* MOD  */
-    ASSIGN = 314,                  /* ASSIGN  */
-    ASGNOP = 315,                  /* ASGNOP  */
-    ADDEQ = 316,                   /* ADDEQ  */
-    SUBEQ = 317,                   /* SUBEQ  */
-    MULTEQ = 318,                  /* MULTEQ  */
-    DIVEQ = 319,                   /* DIVEQ  */
-    MODEQ = 320,                   /* MODEQ  */
-    POWEQ = 321,                   /* POWEQ  */
-    PRINT = 322,                   /* PRINT  */
-    PRINTF = 323,                  /* PRINTF  */
-    SPRINTF = 324,                 /* SPRINTF  */
-    ELSE = 325,                    /* ELSE  */
-    INTEST = 326,                  /* INTEST  */
-    CONDEXPR = 327,                /* CONDEXPR  */
-    POSTINCR = 328,                /* POSTINCR  */
-    PREINCR = 329,                 /* PREINCR  */
-    POSTDECR = 330,                /* POSTDECR  */
-    PREDECR = 331,                 /* PREDECR  */
-    VAR = 332,                     /* VAR  */
-    IVAR = 333,                    /* IVAR  */
-    VARNF = 334,                   /* VARNF  */
-    CALL = 335,                    /* CALL  */
-    NUMBER = 336,                  /* NUMBER  */
-    STRING = 337,                  /* STRING  */
-    REGEXPR = 338,                 /* REGEXPR  */
-    GETLINE = 339,                 /* GETLINE  */
-    RETURN = 340,                  /* RETURN  */
-    SPLIT = 341,                   /* SPLIT  */
-    SUBSTR = 342,                  /* SUBSTR  */
-    WHILE = 343,                   /* WHILE  */
-    CAT = 344,                     /* CAT  */
-    NOT = 345,                     /* NOT  */
-    UMINUS = 346,                  /* UMINUS  */
-    UPLUS = 347,                   /* UPLUS  */
-    POWER = 348,                   /* POWER  */
-    DECR = 349,                    /* DECR  */
-    INCR = 350,                    /* INCR  */
-    INDIRECT = 351,                /* INDIRECT  */
-    LASTTOKEN = 352                /* LASTTOKEN  */
-  };
-  typedef enum yytokentype yytoken_kind_t;
-#endif
-
-/* Value type.  */
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-union YYSTYPE
-{
-#line 41 "awkgram.y"
-
+#ifndef YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
+typedef union {
 	Node	*p;
 	Cell	*cp;
 	int	i;
 	char	*s;
-
-#line 168 "awkgram.tab.h"
-
-};
-typedef union YYSTYPE YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
-# define YYSTYPE_IS_DECLARED 1
-#endif
-
-
+} YYSTYPE;
+#endif /* !YYSTYPE_IS_DECLARED */
 extern YYSTYPE yylval;
 
-
-int yyparse (void);
-
-
-#endif /* !YY_YY_AWKGRAM_TAB_H_INCLUDED  */
+#endif /* _yy_defines_h_ */
