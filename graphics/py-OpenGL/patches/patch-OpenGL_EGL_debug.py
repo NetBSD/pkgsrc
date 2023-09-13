a88470b77786082b28b9b65f8451d45fbf0ddcbb
adam <adam@pkgsrc.org>
2023-09-13 17:18:23 +0000

    py-OpenGL py-OpenGL-accelerate: updated to 3.1.7
    3.1.7
    Unknown changes

--- graphics/py-OpenGL/patches/patch-OpenGL_EGL_debug.py
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-OpenGL_EGL_debug.py,v 1.1 2020/02/23 23:59:45 joerg Exp $
-
---- OpenGL/EGL/debug.py.orig	2020-02-22 21:14:12.920237693 +0000
-+++ OpenGL/EGL/debug.py
-@@ -1,3 +1,4 @@
-+from __future__ import print_function
- """Debug utilities for EGL operations"""
- from OpenGL.EGL import *
- import itertools
