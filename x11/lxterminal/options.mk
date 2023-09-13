1f349b27e6e8396bf14e76d86d6d4ca2e7594081
gutteridge <gutteridge@pkgsrc.org>
2023-09-13 01:43:11 +0000

    lxterminal: switch (non-default) GTK3 support to use vte3
    This actually works better now with vte3 than vte029.

--- x11/lxterminal/options.mk
+++ x11/lxterminal/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2022/04/09 00:37:02 gutteridge Exp $
+# $NetBSD: options.mk,v 1.3 2023/09/13 01:43:11 gutteridge Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.lxterminal
 PKG_OPTIONS_REQUIRED_GROUPS+=	toolkit
@@ -7,12 +7,9 @@ PKG_SUGGESTED_OPTIONS=		gtk2
 
 .include "../../mk/bsd.options.mk"
 
-# XXX there are issues using the GTK3 toolkit that don't manifest with
-# GTK2, so this needs investigation and reporting upstream. Basic
-# functionality works, but some specific menu options cause crashes.
 .if !empty(PKG_OPTIONS:Mgtk3)
 .include "../../x11/gtk3/buildlink3.mk"
-.include "../../x11/vte029/buildlink3.mk"
+.include "../../x11/vte3/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-gtk3
 .else
 .include "../../x11/gtk2/buildlink3.mk"
