f8a2d900282f172af68ad52f33f51827fe6b374a
gutteridge <gutteridge@pkgsrc.org>
2023-09-13 00:37:46 +0000

    trusted-firmware-a-rk3399: fix some pkglint nits

--- sysutils/trusted-firmware-a-rk3399/buildlink3.mk
+++ sysutils/trusted-firmware-a-rk3399/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/02/09 01:57:57 mrg Exp $
-
-BUILDLINK_DEPMETHOD.trusted-firmware-a-rk3399?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2023/09/13 00:37:46 gutteridge Exp $
 
 BUILDLINK_TREE+=	trusted-firmware-a-rk3399
 
@@ -8,8 +6,8 @@ BUILDLINK_TREE+=	trusted-firmware-a-rk3399
 TRUSTED_FIRMWARE_A_RK3399_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.trusted-firmware-a-rk3399+=	trusted-firmware-a-rk3399>=2.6
-BUILDLINK_PKGSRCDIR.trusted-firmware-a-rk3399?=	../../sysutils/trusted-firmware-a-rk3399
+BUILDLINK_PKGSRCDIR.trusted-firmware-a-rk3399?=		../../sysutils/trusted-firmware-a-rk3399
+BUILDLINK_DEPMETHOD.trusted-firmware-a-rk3399?=		build
 .endif	# TRUSTED_FIRMWARE_A_RK3399_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-trusted-firmware-a-rk3399
-
