aedae1c912d91d4f4f122bf3fc2e80c2890caa5d
charlotte <charlotte@pkgsrc.org>
2023-09-15 22:59:46 +0000

    games/redeclipse16: Explicitly depend on GNU ed(1)

---
+++ games/redeclipse16/patches/patch-src_system-install.mk
@@ -0,0 +1,33 @@
+$NetBSD: patch-src_system-install.mk,v 1.1 2023/09/15 22:59:46 charlotte Exp $
+
+Parameterize ed(1).
+
+--- src/system-install.mk.orig	2019-12-19 18:00:28.000000000 -0800
++++ src/system-install.mk	2023-09-15 15:24:25.538177725 -0700
+@@ -19,6 +19,8 @@
+ pixmapdir=$(DESTDIR)$(prefix)/share/pixmaps
+ appdatadir=$(DESTDIR)$(prefix)/share/appdata
+ 
++ED?=	ed
++
+ ICONS= \
+ 	install/nix/$(appsrcname)_x16.png \
+ 	install/nix/$(appsrcname)_x32.png \
+@@ -69,7 +71,7 @@
+ 	s,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g\n\
+ 	g,@APPNAME@,\
+ 	s,@APPNAME@,$(appname),g\n\
+-	w\n" | ed -s $(gamesbindir)/$(appname)
++	w\n" | $(ED) -s $(gamesbindir)/$(appname)
+ 
+ system-install-server: server
+ 	$(MKDIR) $(libexecdir)/$(appname)
+@@ -88,7 +90,7 @@
+ 	s,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g\n\
+ 	g,@APPNAME@,\
+ 	s,@APPNAME@,$(appname),g\n\
+-	w\n" | ed -s $(gamesbindir)/$(appname)-server
++	w\n" | $(ED) -s $(gamesbindir)/$(appname)-server
+ 
+ system-install-common:
+ 	$(MKDIR) $(libexecdir)/$(appname)
