f011337a1dca6e6396b13192b0ec93c6f9323c4e
adam <adam@pkgsrc.org>
2023-09-16 02:30:07 +0000

    cmake cmake-gui: updated to 3.27.5
    3.27.5
    Unknown changes

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.44 2023/08/25 08:05:17 adam Exp $
+# $NetBSD: version.mk,v 1.45 2023/09/16 02:30:07 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.4
+CMAKE_VERSION=	3.27.5
 CMAKE_API=	${CMAKE_VERSION:R}
