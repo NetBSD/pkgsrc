b11964055666ffb05595cee7479c2ebdc2408294
rhialto <rhialto@pkgsrc.org>
2023-09-16 12:21:18 +0000

    editors/vim*: update to 9.0.1900
    Upstream changes: see http://ftp.vim.org/pub/vim/patches/9.0/
    (from .1599 to .1678; I didn't find an overview for the rest, apart from
    the git repository https://github.com/vim/vim/commits/master)
    Package changes: the :help command works again.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.141 2023/06/02 15:30:10 wiz Exp $
+# $NetBSD: version.mk,v 1.142 2023/09/16 12:21:18 rhialto Exp $
 
 VIM_VERSION=		9.0
-VIM_PATCHLEVEL=		1598
+VIM_PATCHLEVEL=		1900
 # Changelog: see http://ftp.vim.org/pub/vim/patches/9.0/
 VIM_SUBDIR=		vim90
 
