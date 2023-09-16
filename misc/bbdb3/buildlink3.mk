c12cfcfbb26d5a669bd9e6a0070914fb767673b3
mef <mef@pkgsrc.org>
2023-09-16 07:52:00 +0000

    (misc/bbdb) correction PKGNAME on buildlink3.mk

--- misc/bbdb3/buildlink3.mk
+++ misc/bbdb3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/06/05 02:33:28 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/09/16 07:52:00 mef Exp $
 
 .include "../../editors/emacs/modules.mk"
 .if ${EMACS_FLAVOR} == "emacs"
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	bbdb3
 .  if !defined(BBDB3_BUILDLINK3_MK)
 BBDB3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.bbdb3+=	${EMACS_PKGNAME_PREFIX}bbdb3>=3.2
+BUILDLINK_API_DEPENDS.bbdb3+=	${EMACS_PKGNAME_PREFIX}bbdb>=3.2
 BUILDLINK_PKGSRCDIR.bbdb3?=	../../misc/bbdb3
 
 BUILDLINK_CONTENTS_FILTER.bbdb3=	${EGREP} '.*\.el$$|.*\.elc$$'
