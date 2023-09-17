8d74ed807532068c8f83e09bf7fefdc7e6db7f27
adam <adam@pkgsrc.org>
2023-09-17 08:53:20 +0000

    postgresql16: added version 16.0
    PostgreSQL 16 raises its performance, with notable improvements to query
    parallelism, bulk data loading, and logical replication. There are many
    features in this release for developers and administrators alike, including
    more SQL/JSON syntax, new monitoring stats for your workloads, and greater
    flexibility in defining access control rules for management of policies across
    large fleets.

---
+++ databases/postgresql16/options.mk
@@ -0,0 +1,80 @@
+# $NetBSD: options.mk,v 1.1 2023/09/17 08:53:21 adam Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.postgresql16
+PKG_SUPPORTED_OPTIONS=	bonjour dtrace icu llvm gssapi ldap nls pam lz4
+PKG_SUGGESTED_OPTIONS=	gssapi nls lz4
+
+PLIST_VARS+=		gssapi llvm nls
+
+.include "../../mk/bsd.options.mk"
+
+# Bonjour support
+.if !empty(PKG_OPTIONS:Mbonjour)
+CONFIGURE_ARGS+=	--with-bonjour
+.  if ${OPSYS} != "Darwin"
+LIBS+=			-ldns_sd
+.  endif
+.  include "../../net/mDNSResponder/buildlink3.mk"
+.endif
+
+# Dtrace support
+.if !empty(PKG_OPTIONS:Mdtrace)
+CONFIGURE_ARGS+=	--enable-dtrace
+.endif
+
+# ICU collatium support
+.if !empty(PKG_OPTIONS:Micu)
+USE_TOOLS+=		pkg-config
+CONFIGURE_ARGS+=	--with-icu
+.  include "../../textproc/icu/buildlink3.mk"
+.endif
+
+# GSSAPI (Kerberos5) authentication for the PostgreSQL backend
+.if !empty(PKG_OPTIONS:Mgssapi)
+.  include "../../mk/krb5.buildlink3.mk"
+PLIST.gssapi=		yes
+CONFIGURE_ARGS+=       --with-gssapi
+.else
+CONFIGURE_ARGS+=       --without-gssapi
+.endif
+
+# LDAP authentication for the PostgreSQL backend
+.if !empty(PKG_OPTIONS:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ldap
+.endif
+
+# LLVM based JIT support
+.if !empty(PKG_OPTIONS:Mllvm)
+.  include "../../lang/llvm/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-llvm
+CONFIGURE_ENV+=		CLANG=${CC}	# XXX: make it be better
+PLIST.llvm=		yes
+.endif
+
+# lz4 support
+.if !empty(PKG_OPTIONS:Mlz4)
+.  include "../../archivers/lz4/buildlink3.mk"
+USE_TOOLS+=		pkg-config
+CONFIGURE_ARGS+=	--with-lz4
+.endif
+
+# NLS support
+.if !empty(PKG_OPTIONS:Mnls)
+USE_PKGLOCALEDIR=	yes
+CONFIGURE_ARGS+=	--enable-nls
+PLIST.nls=		yes
+BROKEN_GETTEXT_DETECTION=	yes
+.  include "../../devel/gettext-lib/buildlink3.mk"
+.  if !empty(USE_BUILTIN.gettext:M[nN][oO])
+CPPFLAGS+=		-I${PREFIX}/include/gettext
+.  endif
+.else
+CONFIGURE_ARGS+=	--disable-nls
+.endif
+
+# PAM authentication for the PostgreSQL backend
+.if !empty(PKG_OPTIONS:Mpam)
+.  include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pam
+.endif
