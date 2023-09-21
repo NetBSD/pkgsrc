5ab17ed057b06ba91adfd45261782c085a90c298
gdt <gdt@pkgsrc.org>
2023-09-21 13:54:15 +0000

    devel/cmake: Update to 3.27.6
    Upstream changes from https://discourse.cmake.org/t/cmake-3-27-6-available-for-download/9024
    
      - add_custom_target: Fix regression with Fortran sources
      - Tests/FortranModules: move issue 25112 fix from FortranOnly
      - Tests/FortranModules: add a test case for #25223
      - Tests/FortranModules: also test INTERFACE targets with Fortran sources
      - Tests/FortranModules: add a test for iface Fortran sources
    
    commited during freeze because it fixes a regression that breaks at
    least math/blas.

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.45 2023/09/16 02:30:07 adam Exp $
+# $NetBSD: version.mk,v 1.46 2023/09/21 13:54:15 gdt Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.5
+CMAKE_VERSION=	3.27.6
 CMAKE_API=	${CMAKE_VERSION:R}
