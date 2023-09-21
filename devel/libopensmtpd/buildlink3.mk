66edb908b46cb4dafdb99d6c5ed05be6544665ea
vins <vins@pkgsrc.org>
2023-09-21 10:57:39 +0000

    devel/libopensmtpd: fix build on SunOS and bump revision.

--- devel/libopensmtpd/buildlink3.mk
+++ devel/libopensmtpd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/09/02 21:32:11 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/09/21 10:57:39 vins Exp $
 
 BUILDLINK_TREE+=	libopensmtpd
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libopensmtpd
 LIBOPENSMTPD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libopensmtpd+=	libopensmtpd>=0.7
+BUILDLINK_ABI_DEPENDS.libopensmtpd+=	libopensmtpd>=0.7nb1
 BUILDLINK_PKGSRCDIR.libopensmtpd?=	../../devel/libopensmtpd
 
 .endif	# LIBOPENSMTPD_BUILDLINK3_MK
