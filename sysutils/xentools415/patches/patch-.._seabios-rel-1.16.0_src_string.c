a5314fd2ff6449a83008592b889a0c5a69755b65
bsiegert <bsiegert@pkgsrc.org>
2023-09-23 17:55:51 +0000

    Pullup ticket #6795 - requested by bouyer sysutils/xenkernel415: security fix sysutils/xenstoretools: security fix sysutils/xentools415: security fix
    Revisions pulled up:
    - sysutils/xenkernel415/Makefile                                1.10
    - sysutils/xenkernel415/distinfo                                1.9
    - sysutils/xenstoretools/Makefile                               1.28
    - sysutils/xentools415/Makefile                                 1.26
    - sysutils/xentools415/distinfo                                 1.13
    - sysutils/xentools415/patches/patch-.._seabios-rel-1.14.0_src_string.c deleted
    - sysutils/xentools415/patches/patch-.._seabios-rel-1.16.0_src_string.c 1.1
    - sysutils/xentools415/patches/patch-tools_firmware_Makefile    1.2
    - sysutils/xentools415/version.mk                               1.4
    
    ---
       Module Name:	pkgsrc
       Committed By:	bouyer
       Date:		Thu Aug 24 10:27:09 UTC 2023
    
       Modified Files:
       	pkgsrc/sysutils/xenkernel415: Makefile distinfo
       	pkgsrc/sysutils/xenstoretools: Makefile
       	pkgsrc/sysutils/xentools415: Makefile distinfo version.mk
       	pkgsrc/sysutils/xentools415/patches: patch-tools_firmware_Makefile
       Added Files:
       	pkgsrc/sysutils/xentools415/patches:
       	    patch-.._seabios-rel-1.16.0_src_string.c
       Removed Files:
       	pkgsrc/sysutils/xentools415/patches:
       	    patch-.._seabios-rel-1.14.0_src_string.c
    
       Log Message:
       Update xenkernel415, xentools415 and xenstoretools to Xen 4.15.5
       Chnages since 4.15.4:
       - includes patches for Xen Security Advisory up to XSA-436 (inclued)
       - update seabios to 1.16.0
       - better support on some hardware
    
       Complete changes here:
       https://xenproject.org/downloads/xen-project-archives/xen-project-4-15-series/xen-project-4-15-5/

--- sysutils/xentools415/patches/patch-.._seabios-rel-1.16.0_src_string.c
+++ sysutils/xentools415/patches/patch-.._seabios-rel-1.16.0_src_string.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-.._seabios-rel-1.14.0_src_string.c,v 1.1 2021/04/18 12:31:26 bouyer Exp $
+$NetBSD: patch-.._seabios-rel-1.16.0_src_string.c,v 1.1.2.2 2023/09/23 17:55:51 bsiegert Exp $
 
 when the code is compiled -fwhole-program, memcpy() will be ommitted by gcc
 because nothing in the file references it. Later the link fails with
 undefined references to memcpy().
 Force the inclusion with (externally_visible) attribute
 
---- ../seabios-rel-1.14.0/src/string.c.orig	2021-04-17 22:32:25.409876075 +0200
-+++ ../seabios-rel-1.14.0/src/string.c	2021-04-17 22:32:39.873615679 +0200
+--- ../seabios-rel-1.16.0/src/string.c.orig	2021-04-17 22:32:25.409876075 +0200
++++ ../seabios-rel-1.16.0/src/string.c	2021-04-17 22:32:39.873615679 +0200
 @@ -149,7 +149,7 @@
          memcpy(d_fl, s_fl, len);
  }
