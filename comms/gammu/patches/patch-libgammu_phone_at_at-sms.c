da89310908edb7276da1d755a6f45708edd0ca8d
manu <manu@pkgsrc.org>
2023-09-24 00:50:48 +0000

    Skip garbage reply on AT+CMGL in PDU mode as in text mode
    From upstream https://github.com/gammu/gammu/pull/823
    
    THe D-Link DWM222 4G USB modem sends a garbage line before the reply
    to AT+CMGL. This case was handled in text mode but not in PDU mode,
    and this device exhibit the bad behavior in PDU mode too.
    
    This change brings garbage skip behavior in PDU mode on par with text mode.

--- comms/gammu/patches/patch-libgammu_phone_at_at-sms.c
+++ comms/gammu/patches/patch-libgammu_phone_at_at-sms.c
@@ -1,11 +1,22 @@
-$NetBSD: patch-libgammu_phone_at_at-sms.c,v 1.3 2015/08/17 16:42:53 leot Exp $
+$NetBSD: patch-libgammu_phone_at_at-sms.c,v 1.4 2023/09/24 00:50:48 manu Exp $
 
-Don't use strcharnul from the helper library, it doesn't end up as PIC
-when compiled with Clang.
+1) Don't use strcharnul from the helper library, it doesn't end 
+up as PIC when compiled with Clang.
 
---- libgammu/phone/at/at-sms.c.orig	2015-08-14 12:32:29.000000000 +0000
-+++ libgammu/phone/at/at-sms.c
-@@ -112,6 +112,9 @@ GSM_Error ATGEN_ReplyGetSMSMemories(GSM_
+2) Skip garbage reply on AT+CMGL in PDU mode as in text mode
+
+From upstream https://github.com/gammu/gammu/pull/823
+
+THe D-Link DWM222 4G USB modem sends a garbage line before the reply
+to AT+CMGL. This case was handled in text mode but not in PDU mode,
+and this device exhibit the bad behavior in PDU mode too.
+
+This change brings garbage skip behavior in PDU mode on par with text mode.
+
+--- libgammu/phone/at/at-sms.c.orig	2019-09-27 10:55:52.000000000 +0200
++++ libgammu/phone/at/at-sms.c	2023-09-24 02:44:57.292835696 +0200
+@@ -113,8 +113,11 @@
+ 		}
  		if (pos_start != NULL) {
  			/* Detect which memories we can use for saving */
  			pos_end = strchrnul(pos_start + 1, ')');
@@ -15,3 +26,19 @@ when compiled with Clang.
  			pos_tmp = strstr(pos_start, "\"SM\"");
  
  			if (pos_tmp != NULL && pos_tmp < pos_end) {
+ 				Priv->SIMSaveSMS = AT_AVAILABLE;
+@@ -1144,14 +1147,9 @@
+ 		if (str == NULL) {
+ 			/*
+ 			 * Sometimes an SMS message will contain a line break. In SMS text
+                          * mode we skip to the next line and try again to find +CMGL.
+-			 * FIXME: Can we do the same for SMS PDU mode?
+ 			 */
+-			if (Priv->SMSMode == SMS_AT_PDU) {
+-				smprintf(s, "Can not find +CMGL:!\n");
+-				return ERR_UNKNOWN;
+-			}
+ 			continue;
+ 		}
+ 
+ 		/* Parse reply */
