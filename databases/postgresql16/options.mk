bbbca76f6a56be8e9ab2dee0033646fa5f51ca20
adam <adam@pkgsrc.org>
2023-09-25 07:35:18 +0000

    postgresql16: use mit-krb5 for gssapi

--- databases/postgresql16/options.mk
+++ databases/postgresql16/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2023/09/20 11:39:40 adam Exp $
+# $NetBSD: options.mk,v 1.3 2023/09/25 07:35:18 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.postgresql16
 PKG_SUPPORTED_OPTIONS=	bonjour dtrace llvm gssapi ldap nls pam lz4
@@ -24,7 +24,7 @@ CONFIGURE_ARGS+=	--enable-dtrace
 
 # GSSAPI (Kerberos5) authentication for the PostgreSQL backend
 .if !empty(PKG_OPTIONS:Mgssapi)
-.  include "../../mk/krb5.buildlink3.mk"
+.  include "../../security/mit-krb5/buildlink3.mk"
 PLIST.gssapi=		yes
 CONFIGURE_ARGS+=       --with-gssapi
 .else
