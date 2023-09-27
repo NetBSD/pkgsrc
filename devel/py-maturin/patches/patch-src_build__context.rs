de3783841839e5c1b2eb5d2010e63462de3b1a22
wiz <wiz@pkgsrc.org>
2023-09-27 12:49:10 +0000

    py-maturin: update to 1.2.3nb2.
    Fix wheel name on macOS>=11 using upstream patch.

--- devel/py-maturin/patches/patch-src_build__context.rs
+++ devel/py-maturin/patches/patch-src_build__context.rs
@@ -1,6 +1,9 @@
-$NetBSD: patch-src_build__context.rs,v 1.1 2023/08/26 18:20:32 tnn Exp $
+$NetBSD: patch-src_build__context.rs,v 1.2 2023/09/27 12:49:10 wiz Exp $
 
 Fix wrong python wheel tag on NetBSD/evbarm.
+
+Fix wrong python wheel tag on macOS.
+https://github.com/PyO3/maturin/pull/1778
 
 --- src/build_context.rs.orig	2023-08-17 05:08:34.000000000 +0000
 +++ src/build_context.rs
@@ -22,3 +25,35 @@ Fix wrong python wheel tag on NetBSD/evbarm.
              // FreeBSD
              (Os::FreeBsd, _)
              // NetBSD
+@@ -1150,7 +1161,21 @@ fn macosx_deployment_target(
+             arm64_ver = (major, minor);
+         }
+     }
+-    Ok((x86_64_ver, arm64_ver))
++    Ok((
++        python_macosx_target_version(x86_64_ver),
++        python_macosx_target_version(arm64_ver),
++    ))
++}
++
++#[inline]
++fn python_macosx_target_version(version: (u16, u16)) -> (u16, u16) {
++    let (major, minor) = version;
++    if major >= 11 {
++        // pip only supports (major, 0) for macOS 11+
++        (major, 0)
++    } else {
++        (major, minor)
++    }
+ }
+ 
+ pub(crate) fn rustc_macosx_target_version(target: &str) -> (u16, u16) {
+@@ -1277,7 +1302,7 @@ mod test {
+         );
+         assert_eq!(
+             macosx_deployment_target(Some("11.1"), false).unwrap(),
+-            ((11, 1), (11, 1))
++            ((11, 0), (11, 0))
+         );
+     }
+ }
