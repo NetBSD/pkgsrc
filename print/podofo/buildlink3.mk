b6cbfe29ed86b2f8e5beafe5b7975e3cda744263
joerg <joerg@pkgsrc.org>
2023-09-28 21:35:41 +0000

    Buildlink OpenSSL

--- print/podofo/buildlink3.mk
+++ print/podofo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/01/03 17:36:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/09/28 21:35:41 joerg Exp $
 
 BUILDLINK_TREE+=	podofo
 
@@ -12,6 +12,7 @@ BUILDLINK_PKGSRCDIR.podofo?=	../../print/podofo
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif	# PODOFO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-podofo
