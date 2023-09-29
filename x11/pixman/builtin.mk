4c7788898e3e146efe73650b71c135ac25aeb3ba
adam <adam@pkgsrc.org>
2023-09-29 09:27:07 +0000

    pixman: fix build for aarch64 machines with clang; apply pkglint suggestions while here

--- x11/pixman/builtin.mk
+++ x11/pixman/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.10 2022/10/18 11:06:46 wiz Exp $
+# $NetBSD: builtin.mk,v 1.11 2023/09/29 09:27:07 adam Exp $
 
 BUILTIN_PKG:=		pixman
 PKGCONFIG_FILE.pixman=	${X11BASE}/lib/pkgconfig/pixman-1.pc
@@ -11,8 +11,8 @@ PKGCONFIG_FILE.pixman+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/pixman-1.pc
 # Mac OS 10.5.4 or newer which references a non-existing version of the
 # PNG shared library.
 CHECK_BUILTIN.pixman?=	no
-.if ${CHECK_BUILTIN.pixman:M[nN][oO]} && \
-    ${USE_BUILTIN.pixman:M[Yy][Ee][Ss]} && \
-    !empty(MACHINE_PLATFORM:MDarwin-9.*-*)
+.if ${CHECK_BUILTIN.pixman:tl} == no && \
+    ${USE_BUILTIN.pixman:tl} == yes && \
+    ${MACHINE_PLATFORM:MDarwin-9.*-*}
 BUILDLINK_TRANSFORM+=	opt:-lpixman-1.0.10.0:-lpixman-1
 .endif
