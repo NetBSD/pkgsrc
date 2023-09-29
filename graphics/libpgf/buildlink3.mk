caaba75bbb023c436c5dd6996d049573e4f9e8b8
nros <nros@pkgsrc.org>
2023-09-29 18:29:29 +0000

    Update libpgf to version 7.21.7
    Pkgsrc changes:
    Don't need dos2unix, the line endings have been fixed in this version.
    Removed doc/Makefile.am patch, it has been applied upstream.
    This version builds on compilers defaulting to C++17.
    BUILDLINK_API_DEPENDS has been bumped to the new version since nothing
    in pkgsrc depends on libpgf.
    
    
    Changelog from readme:
    
    Version 7.21.7, (Thu, 18 Feb 2021)
    ------------------------------------
    
    1. The new version is a minor bug fix of versions 7.15.25 to 7.21.2.
    
    2. This bug fix for ROI decoding only. This bug fix is necessary if
       several Read() and ResetStreamPos() operations are called with
       the same PGFimage object after a single Open() call.
    
    
    Version 7.21.2, (Sun, 17 Jan 2021)
    ------------------------------------
    
    1. The new version is a documentation and build tool update of
       version 7.19.3.
    
    
    Version 7.19.3, (Tue, 15 Jan 2019)
    ------------------------------------
    
    1. The new version is a minor update of version 7.15.25.
    
    2. This version fixes a compilation bug seen when ROI support is
       disabled.
    
    
    Version 7.15.32, (Thu, 6 Aug 2015)
    ------------------------------------
    
    1. The new version is a minor update of version 7.15.25.
    
    2. This version improves the reuse of CPGFImage objects for several
       decoding operations. It clarifies the usage of CPGFImage::Close()
       and CPGFImage::Destroy() by deletion of Close(). Several reading
       operations can be performed in the following way:
       Open(), Read(), GetBitmap(), ResetStreamPos(), Read(),
       GetBitmap(), ResetStreamPos(), ...
       Calling Destroy() frees all allocated ressources and reinitializes
       the object to the same state as the constructor does. This allows
       the reuse of the CPGFImage object for encoding and decoding:
       SetHeader(), ImportBitmap(), Write(), ResetStreamPos(), Destroy(),
       Open(), Read(), GetBitmap()
    
    3. Caching or skipping of user data (meta data) while opening a PGF
       image can be controlled by a new UserdataPolicy in
       ConfigureDecoder().
    
    
    Version 7.15.25, (Sat, 20 June 2015)
    ------------------------------------
    
    1. This new version is a replacement of version 6.14.12.
       In case you use the ROI decoding, we strongly encourage using
       version 6.15.25 instead of an older version.
    
    2. This version fixes some decoder bugs only seen in ROI decoding.
       ROI decoding is now also supported for Bitmap and RGB12 image modes.
    
    3. This version introduces a new and more efficient data format for
       binary images (bitmaps).
       The new format allows ROI decoding.
       The decoder supports both the old and the new format, but ROI
       decoding works only with the new format.
    
    4. The two reserverd bytes in PGFHeader are now used for a more
       detailled PGF version number.
    
    5. The Visual Studio project files are in the VS12 format.

--- graphics/libpgf/buildlink3.mk
+++ graphics/libpgf/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/07/22 17:36:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/09/29 18:29:29 nros Exp $
 
 BUILDLINK_TREE+=	libpgf
 
 .if !defined(LIBPGF_BUILDLINK3_MK)
 LIBPGF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpgf+=	libpgf>=6.14.12
+BUILDLINK_API_DEPENDS.libpgf+=	libpgf>=7.21.7
 BUILDLINK_PKGSRCDIR.libpgf?=	../../graphics/libpgf
 .endif	# LIBPGF_BUILDLINK3_MK
 
