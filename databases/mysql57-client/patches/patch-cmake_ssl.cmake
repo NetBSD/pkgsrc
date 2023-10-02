3da88e40156c58f13860ee5b600858c63b504e6e
adam <adam@pkgsrc.org>
2023-10-02 10:31:58 +0000

    mysql57: updated to 5.7.43
    Changes in MySQL 5.7.43
    
    Compilation Notes
    
    Improved Clang 15 usage, although it's not officially supported in v5.7.
    
    Functionality Added or Changed
    
    Important Change: The linked OpenSSL library for MySQL Server has been upgraded from OpenSSL 1.1.1 to OpenSSL 3.0. The exact version is now 3.0.9. More information on changes from 1.1.1 to 3.0 can be found at https://www.openssl.org/docs/man3.0/man7/migration_guide.html.
    
    The linked curl library for MySQL Server (Enterprise Edition) has been updated to version 8.1.1.
    
    Bugs Fixed
    
    Group Replication: After one machine halted and restarted in a three-node MySQL InnoDB Cluster, one node failed to start; after restarting all nodes, the cluster shut down unexpectedly.
    
    Our thanks to Zetang Zeng for the contribution.
    
    Fortified parsing of the network packet data sent by the server to the client.
    
    Some floating-point literals were not always handled correctly.
    
    Executing a query with an implicit aggregation should return exactly one row, unless the query has a HAVING clause that filters out the row, but a query with a HAVING clause which evaluated to FALSE sometimes ignored this, and returned a row regardless.
    
    During optimization, range-select tree creation uses logic which differs based on the left-hand side of the IN() predicate. For a field item, each value on the right-hand side is added to an OR tree to create the necessary expression. In the case of a row item comparison (example: WHERE (a,b) IN ((n1,m1), (n2, m2), ...)), an expression in disjunctive normal form (DNF) is needed. A DNF expression is created by adding an AND tree with column values to an OR tree for each set of RHS values, but instead the OR tree was added to the AND tree causing the tree merge to require exponential time due to O(n2) runtime complexity.

--- databases/mysql57-client/patches/patch-cmake_ssl.cmake
+++ databases/mysql57-client/patches/patch-cmake_ssl.cmake
@@ -1,10 +1,48 @@
-$NetBSD: patch-cmake_ssl.cmake,v 1.5 2021/11/10 11:03:52 jperkin Exp $
+$NetBSD: patch-cmake_ssl.cmake,v 1.6 2023/10/02 10:31:59 adam Exp $
 
 Do not prefer static libraries (or linking will fail).
 
---- cmake/ssl.cmake.orig	2021-09-07 05:26:47.000000000 +0000
+--- cmake/ssl.cmake.orig	2023-06-21 13:02:02.000000000 +0000
 +++ cmake/ssl.cmake
-@@ -153,7 +153,6 @@ MACRO (MYSQL_CHECK_SSL)
+@@ -128,6 +128,29 @@ MACRO(FIND_OPENSSL_VERSION)
+       OPENSSL_VERSION_NUMBER
+       REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x[0-9].*"
+       )
++    IF (NOT OPENSSL_VERSION_NUMBER)
++      FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
++        OPENSSL_VERSION_NUMBER
++        REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_MAJOR[\t ]+[0-9].*$"
++      )
++      STRING(REGEX REPLACE "^.*MAJOR[\t ]+([0-9]).*$" "\\1"
++        OPENSSL_MAJOR_VERSION "${OPENSSL_VERSION_NUMBER}"
++      )
++      FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
++        OPENSSL_VERSION_NUMBER
++        REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_MINOR[\t ]+[0-9].*"
++      )
++      STRING(REGEX REPLACE "^.*MINOR[\t ]+([0-9]).*$" "\\1"
++        OPENSSL_MINOR_VERSION "${OPENSSL_VERSION_NUMBER}"
++      )
++      FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
++        OPENSSL_VERSION_NUMBER
++        REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_PATCH[\t ]+[0-9].*"
++      )
++      STRING(REGEX REPLACE "^.*PATCH[\t ]+([0-9]).*$" "\\1"
++        OPENSSL_FIX_VERSION "${OPENSSL_VERSION_NUMBER}"
++      )
++    ELSE()
+     STRING(REGEX REPLACE
+       "^.*OPENSSL_VERSION_NUMBER[\t ]+0x([0-9]).*$" "\\1"
+       OPENSSL_MAJOR_VERSION "${OPENSSL_VERSION_NUMBER}"
+@@ -140,6 +163,7 @@ MACRO(FIND_OPENSSL_VERSION)
+       "^.*OPENSSL_VERSION_NUMBER[\t ]+0x[0-9][0-9][0-9]([0-9][0-9]).*$" "\\1"
+       OPENSSL_FIX_VERSION "${OPENSSL_VERSION_NUMBER}"
+       )
++    ENDIF()
+   ENDIF()
+   SET(OPENSSL_VERSION
+     "${OPENSSL_MAJOR_VERSION}.${OPENSSL_MINOR_VERSION}.${OPENSSL_FIX_VERSION}"
+@@ -235,7 +259,6 @@ MACRO (MYSQL_CHECK_SSL)
      # On mac this list is <.dylib;.so;.a>
      # We prefer static libraries, so we reverse it here.
      IF (WITH_SSL_PATH)
@@ -12,7 +50,7 @@ Do not prefer static libraries (or linking will fail).
        MESSAGE(STATUS "suffixes <${CMAKE_FIND_LIBRARY_SUFFIXES}>")
      ENDIF()
  
-@@ -163,9 +162,6 @@ MACRO (MYSQL_CHECK_SSL)
+@@ -245,9 +268,6 @@ MACRO (MYSQL_CHECK_SSL)
      FIND_LIBRARY(CRYPTO_LIBRARY
                   NAMES crypto libcrypto libeay32
                   HINTS ${OPENSSL_ROOT_DIR}/lib)
@@ -21,46 +59,8 @@ Do not prefer static libraries (or linking will fail).
 -    ENDIF()
  
      IF(OPENSSL_INCLUDE_DIR)
-       # Verify version number. Version information looks like:
-@@ -175,6 +171,29 @@ MACRO (MYSQL_CHECK_SSL)
-         OPENSSL_VERSION_NUMBER
-         REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x[0-9].*"
-         )
-+      IF (NOT OPENSSL_VERSION_NUMBER)
-+        FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
-+          OPENSSL_VERSION_NUMBER
-+          REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_MAJOR[\t ]+[0-9].*$"
-+        )
-+        STRING(REGEX REPLACE "^.*MAJOR[\t ]+([0-9]).*$" "\\1"
-+          OPENSSL_MAJOR_VERSION "${OPENSSL_VERSION_NUMBER}"
-+        )
-+        FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
-+          OPENSSL_VERSION_NUMBER
-+          REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_MINOR[\t ]+[0-9].*"
-+        )
-+        STRING(REGEX REPLACE "^.*MINOR[\t ]+([0-9]).*$" "\\1"
-+          OPENSSL_MINOR_VERSION "${OPENSSL_VERSION_NUMBER}"
-+        )
-+        FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
-+          OPENSSL_VERSION_NUMBER
-+          REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_PATCH[\t ]+[0-9].*"
-+        )
-+        STRING(REGEX REPLACE "^.*PATCH[\t ]+([0-9]).*$" "\\1"
-+          OPENSSL_FIX_VERSION "${OPENSSL_VERSION_NUMBER}"
-+        )
-+      ELSE()
-       STRING(REGEX REPLACE
-         "^.*OPENSSL_VERSION_NUMBER[\t ]+0x([0-9]).*$" "\\1"
-         OPENSSL_MAJOR_VERSION "${OPENSSL_VERSION_NUMBER}"
-@@ -187,6 +206,7 @@ MACRO (MYSQL_CHECK_SSL)
-         "^.*OPENSSL_VERSION_NUMBER[\t ]+0x[0-9][0-9][0-9]([0-9][0-9]).*$" "\\1"
-         OPENSSL_FIX_VERSION "${OPENSSL_VERSION_NUMBER}"
-         )
-+      ENDIF()
-     ENDIF()
-     SET(OPENSSL_VERSION
-       "${OPENSSL_MAJOR_VERSION}.${OPENSSL_MINOR_VERSION}.${OPENSSL_FIX_VERSION}"
-@@ -196,14 +216,14 @@ MACRO (MYSQL_CHECK_SSL)
+       FIND_OPENSSL_VERSION()
+@@ -256,7 +276,7 @@ MACRO (MYSQL_CHECK_SSL)
      IF("${OPENSSL_VERSION}" VERSION_GREATER "1.1.0")
         ADD_DEFINITIONS(-DHAVE_TLSv13)
         SET(HAVE_TLSv13 1)
@@ -69,11 +69,3 @@ Do not prefer static libraries (or linking will fail).
           SET(FORCE_SSL_SOLARIS "-Wl,--undefined,address_of_sk_new_null")
         ENDIF()
      ENDIF()
-     IF(OPENSSL_INCLUDE_DIR AND
-        OPENSSL_LIBRARY   AND
-        CRYPTO_LIBRARY      AND
--       OPENSSL_MAJOR_VERSION STREQUAL "1"
-+       (OPENSSL_MAJOR_VERSION STREQUAL "1" OR OPENSSL_MAJOR_VERSION STREQUAL "3")
-       )
-       SET(OPENSSL_FOUND TRUE)
-     ELSE()
