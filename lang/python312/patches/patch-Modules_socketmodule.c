06a0459c03ffeae26ee338b5e4b766c68c5d6e36
jperkin <jperkin@pkgsrc.org>
2023-10-04 13:19:49 +0000

    python312: Fix socket module on SunOS.

--- lang/python312/patches/patch-Modules_socketmodule.c
+++ lang/python312/patches/patch-Modules_socketmodule.c
@@ -1,8 +1,20 @@
-$NetBSD: patch-Modules_socketmodule.c,v 1.1 2023/10/02 20:07:15 adam Exp $
+$NetBSD: patch-Modules_socketmodule.c,v 1.2 2023/10/04 13:19:49 jperkin Exp $
 
---- Modules/socketmodule.c.orig	2022-01-13 18:52:14.000000000 +0000
+SunOS needs _XOPEN_SOURCE for sendmsg bits that are now required.
+
+--- Modules/socketmodule.c.orig	2023-10-02 11:48:14.000000000 +0000
 +++ Modules/socketmodule.c
-@@ -5440,7 +5440,7 @@ socket_sethostname(PyObject *self, PyObj
+@@ -89,6 +89,9 @@ Local naming conventions:
+ #  define Py_BUILD_CORE_MODULE 1
+ #endif
+ 
++#ifdef __sun
++#define _XOPEN_SOURCE	600
++#endif
+ #ifdef __APPLE__
+ // Issue #35569: Expose RFC 3542 socket options.
+ #define __APPLE_USE_RFC_3542 1
+@@ -5655,7 +5658,7 @@ socket_sethostname(PyObject *self, PyObj
      Py_buffer buf;
      int res, flag = 0;
  
