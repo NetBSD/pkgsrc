b8ed10bd1310376f6ab1185274d4a08b5d102921
pho <pho@pkgsrc.org>
2023-10-09 02:49:52 +0000

    lang/ghc94: Update to GHC 9.4.7
    Release notes:
    9.4.5: https://downloads.haskell.org/~ghc/9.4.5/docs/users_guide/9.4.5-notes.html
    9.4.6: https://downloads.haskell.org/~ghc/9.4.6/docs/users_guide/9.4.6-notes.html
    9.4.7: https://downloads.haskell.org/~ghc/9.4.7/docs/users_guide/9.4.7-notes.html

--- lang/ghc94/buildlink3.mk
+++ lang/ghc94/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/08/14 05:24:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 02:49:52 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.4.4
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.4.4nb3
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.4.7
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.4.7
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc94
 
 .include "../../converters/libiconv/buildlink3.mk"
