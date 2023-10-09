e31df0625943106e989da7b56eec39a78140a530
pho <pho@pkgsrc.org>
2023-10-09 02:49:52 +0000

    lang/ghc94: Update to GHC 9.4.7
    Release notes:
    9.4.5: https://downloads.haskell.org/~ghc/9.4.5/docs/users_guide/9.4.5-notes.html
    9.4.6: https://downloads.haskell.org/~ghc/9.4.6/docs/users_guide/9.4.6-notes.html
    9.4.7: https://downloads.haskell.org/~ghc/9.4.7/docs/users_guide/9.4.7-notes.html

--- lang/ghc94/patches/patch-libraries_process_cbits_posix_fork__exec.c
+++ lang/ghc94/patches/patch-libraries_process_cbits_posix_fork__exec.c
@@ -1,17 +1,20 @@
-$NetBSD: patch-libraries_process_cbits_posix_fork__exec.c,v 1.2 2023/01/21 13:20:34 pho Exp $
+$NetBSD: patch-libraries_process_cbits_posix_fork__exec.c,v 1.3 2023/10/09 02:49:52 pho Exp $
 
 Ensure _POSIX_PTHREAD_SEMANTICS is defined on SunOS.
 
 Pull request:
 https://github.com/haskell/process/pull/274
 
---- libraries/process/cbits/posix/fork_exec.c.orig	2021-10-28 20:42:10.000000000 +0000
+--- libraries/process/cbits/posix/fork_exec.c.orig	2023-08-21 03:50:41.000000000 +0000
 +++ libraries/process/cbits/posix/fork_exec.c
-@@ -1,3 +1,7 @@
+@@ -1,6 +1,10 @@
+ /* ensure that execvpe is provided if possible */
+ #define _GNU_SOURCE 1
+ 
 +#ifdef __sun
 +#define _POSIX_PTHREAD_SEMANTICS
 +#endif
 +
  #include "common.h"
  
- #include <sys/types.h>
+ #if defined(HAVE_FORK)
