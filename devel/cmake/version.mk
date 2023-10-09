78627d0bbc2cf348c26eab574c3c4414fb7dd9b8
adam <adam@pkgsrc.org>
2023-10-09 17:19:30 +0000

    cmake: updated to 3.27.7
    CMake 3.27.7
    * Android: Require Clang 18 for -std=c++23
    * Tests: Clarify RunCMake.CTestTimeout case name
    * ctest: Restore support for --timeout values higher than default test timeout
    * Help: Document CMP0124 behavior on already-set variables
    * FindPostgreSQL: Add support for version 16

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.46 2023/09/21 13:54:15 gdt Exp $
+# $NetBSD: version.mk,v 1.47 2023/10/09 17:19:30 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.6
+CMAKE_VERSION=	3.27.7
 CMAKE_API=	${CMAKE_VERSION:R}
