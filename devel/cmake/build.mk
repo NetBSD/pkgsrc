90309f486b3683d966d775e56b0623d9cf1cc578
adam <adam@pkgsrc.org>
2023-10-09 17:19:30 +0000

    cmake: updated to 3.27.7
    CMake 3.27.7
    * Android: Require Clang 18 for -std=c++23
    * Tests: Clarify RunCMake.CTestTimeout case name
    * ctest: Restore support for --timeout values higher than default test timeout
    * Help: Document CMP0124 behavior on already-set variables
    * FindPostgreSQL: Add support for version 16

--- devel/cmake/build.mk
+++ devel/cmake/build.mk
@@ -1,4 +1,4 @@
-# $NetBSD: build.mk,v 1.9 2023/07/17 06:43:59 ryoon Exp $
+# $NetBSD: build.mk,v 1.10 2023/10/09 17:19:30 adam Exp $
 #
 # This Makefile fragment supports building using the CMake build tool.
 #
@@ -22,21 +22,21 @@
 # CMAKE_INSTALL_ARGS
 #	Arguments to pass to CMake during installation: Default: empty
 #
-# CONFIGURE_DIRS
-#	Directories relative to WRKSRC in which to run CMake. Usually
-#	only one, the toplevel.
+# CONFIGURE_DIR
+#	Directory relative to WRKSRC in which to run CMake. Usually
+#	the top-level one.
 #
 # BUILD_DIRS
-#	Directories relative to WRKSRC in which to build. Defaults
-#	to CONFIGURE_DIRS.
+#	Directories relative to WRKSRC/CMAKE_BUILD_DIR in which to build.
+#	Defaults to WRKSRC/CMAKE_BUILD_DIR.
 #
 # TEST_DIRS
-#	Directories relative to WRKSRC in which to run the tests. Defaults
-#	to CONFIGURE_DIRS.
+#	Directories relative to WRKSRC/CMAKE_BUILD_DIR in which to run the
+#	tests. Defaults to WRKSRC/CMAKE_BUILD_DIR.
 #
 # INSTALL_DIRS
-#	Directories relative to WRKSRC in which to run the 'install'
-#	step. Defaults to CONFIGURE_DIRS.
+#	Directories relative to WRKSRC/CMAKE_BUILD_DIR in which to run the
+#	'install' step. Defaults to WRKSRC/CMAKE_BUILD_DIR.
 
 CMAKE_REQD?=	0
 .for version in ${CMAKE_REQD}
@@ -64,62 +64,70 @@ _CMAKE_BUILD_SYSTEM?=	Unix Makefiles
 _CMAKE_BUILD_TOOL?=	${MAKE_PROGRAM}
 .endif
 
-CONFIGURE_DIRS?=	.
-BUILD_DIRS?=		${CONFIGURE_DIRS}
-TEST_DIRS?=		${CONFIGURE_DIRS}
-INSTALL_DIRS?=		${CONFIGURE_DIRS}
+CONFIGURE_DIR?=		.
+BUILD_DIRS?=		${CONFIGURE_DIR}
+TEST_DIRS?=		${CONFIGURE_DIR}
+INSTALL_DIRS?=		${CONFIGURE_DIR}
 
 _CMAKE_CONFIGURE_SETTINGS=	yes
 
 .PHONY: cmake-configure cmake-build cmake-test cmake-install
 
+.if !target(do-configure)
 do-configure: cmake-configure
 cmake-configure:
-.for d in ${CONFIGURE_DIRS}
-	${RUN} cd ${WRKSRC}/${d} && ${SETENV} ${CONFIGURE_ENV} cmake \
+	${RUN} cd ${WRKSRC}/${CONFIGURE_DIR} && \
+		${SETENV} ${CONFIGURE_ENV} cmake \
 		--install-prefix ${PREFIX} \
 		-B ${CMAKE_BUILD_DIR} \
 		-G ${_CMAKE_BUILD_SYSTEM:Q} \
 		${CMAKE_CONFIGURE_ARGS} \
 		${WRKSRC}
-.endfor
+.endif
 
+.if !target(do-build)
 do-build: cmake-build
 cmake-build:
-.for d in ${BUILD_DIRS}
-	${RUN} cd ${WRKSRC}/${d}/${CMAKE_BUILD_DIR} && \
+.  for d in ${BUILD_DIRS}
+	${RUN} cd ${WRKSRC}/${CMAKE_BUILD_DIR}/${d} && \
 		${SETENV} ${MAKE_ENV} \
 		${_CMAKE_BUILD_TOOL} ${CMAKE_BUILD_ARGS} ${BUILD_TARGET}
-.endfor
+.  endfor
+.endif
 
+.if !target(do-test)
 do-test: cmake-test
 cmake-test:
-.for d in ${TEST_DIRS}
-	${RUN} cd ${WRKSRC}/${d}/${CMAKE_BUILD_DIR} && \
+.  for d in ${TEST_DIRS}
+	${RUN} cd ${WRKSRC}/${CMAKE_BUILD_DIR}/${d} && \
 		${SETENV} ${TEST_ENV} \
 		${_CMAKE_BUILD_TOOL} ${CMAKE_BUILD_ARGS} ${TEST_TARGET}
-.endfor
+.  endfor
+.endif
 
+.if !target(do-install)
 do-install: cmake-install
 cmake-install:
-.for d in ${INSTALL_DIRS}
-	${RUN} cd ${WRKSRC}/${d}/${CMAKE_BUILD_DIR} && \
+.  for d in ${INSTALL_DIRS}
+	${RUN} cd ${WRKSRC}/${CMAKE_BUILD_DIR}/${d} && \
 		${SETENV} ${INSTALL_ENV} \
 		${_CMAKE_BUILD_TOOL} ${CMAKE_INSTALL_ARGS} ${INSTALL_TARGET}
-.endfor
+.  endfor
+.endif
 
 _VARGROUPS+=		cmake
 _USER_VARS.cmake+=	CMAKE_GENERATOR
 _PKG_VARS.cmake+=	CMAKE_REQD
-_PKG_VARS.cmake+=	CMAKE_CONFIGURE_ARGS CONFIGURE_DIRS
-_PKG_VARS.cmake+=	CMAKE_BUILD_ARGS BUILD_DIRS
+_PKG_VARS.cmake+=	CMAKE_CONFIGURE_ARGS CONFIGURE_DIR
+_PKG_VARS.cmake+=	CMAKE_BUILD_ARGS BUILD_DIRS BUILD_TARGET
 _PKG_VARS.cmake+=	TEST_DIRS TEST_TARGET
 _PKG_VARS.cmake+=	CMAKE_INSTALL_ARGS INSTALL_DIRS INSTALL_TARGET
 _SYS_VARS.cmake+=	CMAKE_BUILD_DIR
 _USE_VARS.cmake+=	CMAKE_ARGS
 _USE_VARS.cmake+=	CONFIGURE_ENV MAKE_ENV TEST_ENV INSTALL_ENV
 _IGN_VARS.cmake+=	BUILDLINK_DIR WRKSRC PREFIX
-_IGN_VARS.cmake+=	SETENV TOOL_DEPENDS
+_IGN_VARS.cmake+=	BUILD_USES_CMAKE SETENV TOOL_DEPENDS
 _IGN_VARS.cmake+=	_CMAKE_BUILD_SYSTEM _CMAKE_BUILD_TOOL _MAKE_JOBS_N
+_IGN_VARS.cmake+=	_CMAKE_CONFIGURE_SETTINGS
 _LISTED_VARS.cmake+=	*_ARGS
 _SORTED_VARS.cmake+=	*_ENV
