0937468fba3bb404e45e9252d934fb349102e0cf
adam <adam@pkgsrc.org>
2023-10-10 17:01:31 +0000

    py-djangocms-text-ckeditor: updated to 5.1.4
    5.1.4 (2023-10-03)
    
    fix: Issue 641 which lead to disappearing content after adding child plugins and cancelling the edit
    Feature/issue 648 unable to unlink
    fix: Remove legacy code from Django pre 1.4
    fix: test suite for Django 2.2
    
    5.1.3 (2022-06-21)
    
    Upgrade Ckeditor to 4.21.0 via `640 https://github.com/django-cms/djangocms-text-ckeditor/pull/640`_
    
    5.1.2 (2022-12-27)
    
    Add suport for django CMS 4.1
    Fix 468 via 637: Delay importing models.CMSPlugin in utils to allow adding an HTMLField to a custom user model.
    
    5.1.1 (2022-06-22)
    
    Removed inline editing toggle button from touch devices (where it is inactive anyways) and collapsed menus
    Add workaround for toolbar bug in django CMS < 3.10: Reload page after change in inline editor
    Fix 578: If parent plugins to not set body css class the default from CKEDITOR_SETTINGS is used
    
    5.1.0 (2022-03-24)
    
    Added inline editing
    fix: Update skin patch to avoid @media query
    
    5.0.1 (2022-03-24)
    
    Generated new static bundle.
    
    5.0.0 (2022-03-24)
    
    Drop support for Python 3.5 and 3.6
    Drop support for django 3.0
    Added support for Python 3.9 and 3.10
    Added support for django 3.2
    Added support for django-cms 3.9 and 3.10

--- www/py-djangocms-text-ckeditor/patches/patch-setup.py
+++ www/py-djangocms-text-ckeditor/patches/patch-setup.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-setup.py,v 1.1 2021/01/16 10:13:54 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2023/10/10 17:01:31 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2021-01-16 10:08:40.000000000 +0000
+--- setup.py.orig	2023-10-03 16:31:00.000000000 +0000
 +++ setup.py
-@@ -47,7 +47,7 @@ setup(
-     license="BSD-3-Clause",
-     description="Text Plugin for django CMS with CKEditor support",
-     long_description=open("README.rst").read(),
--    packages=find_packages(),
+@@ -63,7 +63,7 @@ setup(
+     description='Text Plugin for django CMS with CKEditor support',
+     long_description=long_description,
+     long_description_content_type='text/x-rst',
+-    packages=find_packages(exclude=['tests']),
 +    packages=find_packages(exclude=['tests*']),
      include_package_data=True,
      zip_safe=False,
