ac660a97d0c91d3c8cd9192200a80ff304be61b7
adam <adam@pkgsrc.org>
2023-10-10 16:50:55 +0000

    py-djangocms-file: updated to 3.0.1
    3.0.1 (2023-07-03)
    
    Added support for Django 4.2
    Fixed search_field to point to file_name

--- www/py-djangocms-file/patches/patch-setup.py
+++ www/py-djangocms-file/patches/patch-setup.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-setup.py,v 1.3 2021/01/16 09:43:02 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2023/10/10 16:50:55 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2021-01-16 09:37:11.000000000 +0000
+--- setup.py.orig	2023-07-03 23:34:23.000000000 +0000
 +++ setup.py
-@@ -46,7 +46,7 @@ setup(
-     license='BSD-3-Clause',
+@@ -53,7 +53,7 @@ setup(
      description='Adds file plugin to django CMS',
      long_description=open('README.rst').read(),
+     long_description_content_type='text/x-rst',
 -    packages=find_packages(),
 +    packages=find_packages(exclude=['tests']),
      include_package_data=True,
