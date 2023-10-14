7e310ce483fab01e06955ee37f5037c7ad35320f
markd <markd@pkgsrc.org>
2023-10-14 03:39:54 +0000

    okteta: update to 0.26.13
    QT5/KF5 version.

--- devel/okteta/buildlink3.mk
+++ devel/okteta/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/07 10:42:26 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/10/14 03:39:54 markd Exp $
 
 BUILDLINK_TREE+=	okteta
 
 .if !defined(OKTETA_BUILDLINK3_MK)
 OKTETA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.okteta+=	okteta>=4.11.3
+BUILDLINK_API_DEPENDS.okteta+=	okteta>=0.26.6
 BUILDLINK_PKGSRCDIR.okteta?=	../../devel/okteta
 
-#.include "../../security/qca2/buildlink3.mk"
-#.include "../../x11/kdelibs4/buildlink3.mk"
 .endif	# OKTETA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-okteta
