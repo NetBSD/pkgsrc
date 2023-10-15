28ffdbd4bbf02c7b11db93f8cbd49a60e05e643f
ryoon <ryoon@pkgsrc.org>
2023-10-15 07:08:09 +0000

    libixion, py-libixion: Update to 0.18.1
    Changelog:
    0.81.1
    * fixed a 32-bit Linux build issue as discovered on Debian, due to
      a clash on two 32-bit unsigned integer types being used with std::variant.
    
    0.18.0
    C++ API
    * removed the formula_model_access interface from model_context, and
      switched to using model_context directly everywhere.
    * revised formula_tokens_t type to remove use of std::unique_ptr for each
      formula_token instance. This should improve memory locality when iterating
      through an array of formula token values. A similar change has also been
      made to lexer_tokens_t and lexer_token types.
    
    formula interpreter
    * added the following 41 built-in functions:
        ABS
        AND
        COLUMN
        COLUMNS
        COUNT
        COUNTBLANK
        EXACT
        FALSE
        FIND
        ISBLANK
        ISERROR
        ISEVEN
        ISFORMULA
        ISLOGICAL
        ISNA
        ISNONTEXT
        ISNUMBER
        ISODD
        ISREF
        ISTEXT
        LEFT
        MEDIAN
        MID
        MODE
        N
        NA
        NOT
        OR
        REPLACE
        REPT
        RIGHT
        ROW
        ROWS
        SHEET
        SHEETS
        SUBSTITUTE
        T
        TEXTJOIN
        TRIM
        TRUE
        TYPE
    
    * added support for multi-sheet references in Excel A1 and Excel R1C1
      grammers.

--- math/libixion/buildlink3.mk
+++ math/libixion/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/08/14 05:24:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/15 07:08:09 ryoon Exp $
 
 BUILDLINK_TREE+=	libixion
 
 .if !defined(LIBIXION_BUILDLINK3_MK)
 LIBIXION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.17.0
-BUILDLINK_ABI_DEPENDS.libixion?=		libixion>=0.17.0nb4
+BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.18.1
 BUILDLINK_PKGSRCDIR.libixion?=		../../math/libixion
 
 .include "../../devel/mdds/buildlink3.mk"
