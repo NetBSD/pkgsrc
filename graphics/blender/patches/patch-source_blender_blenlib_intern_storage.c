0de0daf66cffc821d3217b3856b7506f0b9dea9e
ryoon <ryoon@pkgsrc.org>
2023-10-15 19:53:20 +0000

    blender: Updatet to 3.6.4
    Changelog:
    New features and bugfixes.
    See: https://wiki.blender.org/wiki/Reference/Release_Notes/3.6

--- graphics/blender/patches/patch-source_blender_blenlib_intern_storage.c
+++ graphics/blender/patches/patch-source_blender_blenlib_intern_storage.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-source_blender_blenlib_intern_storage.c,v 1.1 2022/05/23 05:21:25 dholland Exp $
+$NetBSD: patch-source_blender_blenlib_intern_storage.c,v 1.2 2023/10/15 19:53:20 ryoon Exp $
 
 Use statvfs on Solaris. (From PR 35928)
 
---- source/blender/blenlib/intern/storage.c~	2021-08-10 01:31:50.000000000 +0000
+--- source/blender/blenlib/intern/storage.c.orig	2023-05-17 23:38:29.000000000 +0000
 +++ source/blender/blenlib/intern/storage.c
-@@ -29,7 +29,7 @@
+@@ -13,7 +13,7 @@
  
  #include <sys/stat.h>
  
--#if defined(__NetBSD__) || defined(__DragonFly__) || defined(__HAIKU__)
-+#if defined(__NetBSD__) || defined(__DragonFly__) || defined(__HAIKU__) || defined(__sun)
+-#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__HAIKU__)
++#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__HAIKU__) || defined(__sun)
  /* Other modern unix OS's should probably use this also. */
  #  include <sys/statvfs.h>
  #  define USE_STATFS_STATVFS
--- graphics/blender/patches/patch-source_blender_imbuf_intern_anim_movie.c
+++ graphics/blender/patches/patch-source_blender_imbuf_intern_anim_movie.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-source_blender_imbuf_intern_anim_movie.c,v 1.1 2021/07/26 05:15:15 mrg Exp $
+$NetBSD: patch-source_blender_imbuf_intern_anim_movie.c,v 1.2 2023/10/15 19:53:20 ryoon Exp $
 
 Avoid a crash when playing mp4 files.
 
---- source/blender/imbuf/intern/anim_movie.c.orig	2021-05-01 19:00:25.000000000 -0700
-+++ source/blender/imbuf/intern/anim_movie.c	2021-07-24 01:16:54.625074391 -0700
-@@ -1262,7 +1262,7 @@ static void ffmpeg_seek_and_decode(struc
+--- source/blender/imbuf/intern/anim_movie.cc.orig	2023-06-19 18:45:48.000000000 +0000
++++ source/blender/imbuf/intern/anim_movie.cc
+@@ -453,7 +453,7 @@ static ImBuf *avi_fetchibuf(struct anim 
+   int *tmp;
+   int y;
  
- static ImBuf *ffmpeg_fetchibuf(struct anim *anim, int position, IMB_Timecode_Type tc)
- {
--  if (anim == NULL) {
-+  if (anim == NULL || anim->pFormatCtx == NULL) {
-     return NULL;
+-  if (anim == nullptr) {
++  if (anim == nullptr || anim->pFormatCtx == nullptr) {
+     return nullptr;
    }
  
