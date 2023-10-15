82abf14164de6f010f187d631f46c62cf89d8038
ryoon <ryoon@pkgsrc.org>
2023-10-15 19:53:20 +0000

    blender: Updatet to 3.6.4
    Changelog:
    New features and bugfixes.
    See: https://wiki.blender.org/wiki/Reference/Release_Notes/3.6

--- graphics/blender/patches/patch-source_blender_blenkernel_intern_mesh__validate.cc
+++ graphics/blender/patches/patch-source_blender_blenkernel_intern_mesh__validate.cc
@@ -1,17 +1,17 @@
-$NetBSD: patch-source_blender_blenkernel_intern_mesh__validate.cc,v 1.2 2023/04/18 14:31:59 ryoon Exp $
+$NetBSD: patch-source_blender_blenkernel_intern_mesh__validate.cc,v 1.3 2023/10/15 19:53:20 ryoon Exp $
 
---- source/blender/blenkernel/intern/mesh_validate.cc.orig	2023-02-15 22:58:24.000000000 +0000
+--- source/blender/blenkernel/intern/mesh_validate.cc.orig	2023-05-24 15:42:42.000000000 +0000
 +++ source/blender/blenkernel/intern/mesh_validate.cc
-@@ -306,7 +306,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
-     bool fix_normal = true;
+@@ -302,7 +302,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
  
+   for (i = 0; i < totvert; i++) {
      for (j = 0; j < 3; j++) {
 -      if (!isfinite(vert_positions[i][j])) {
 +      if (!std::isfinite(vert_positions[i][j])) {
          PRINT_ERR("\tVertex %u: has invalid coordinate", i);
  
          if (do_fixes) {
-@@ -813,7 +813,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
+@@ -797,7 +797,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
  
        for (j = 0, dw = dv->dw; j < dv->totweight; j++, dw++) {
          /* NOTE: greater than max defgroups is accounted for in our code, but not < 0. */
