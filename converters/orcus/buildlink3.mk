4cd4cda21b17c92fa98f00a18873d23b2e19411f
ryoon <ryoon@pkgsrc.org>
2023-10-15 07:09:50 +0000

    orcus: Update to 0.18.1
    Changelog:
    0.18.1
    sax parser
     * added support for optionally skipping multiple BOM's in the beginning of
       XML stream. This affects all XML-based file format filters such as
       xls-xml (aka Excel 2003 XML).
    xml-map
     * fixed a bug where XML documents consisting of simple single-column
       records were not properly converted to sheet data.
    xls-xml
     * fixed a bug where the filter would always pass border color even when
       it was not set.
    buildsystem
     * added new configure switches --without-benchmark and --without-doc-example
       to optinally skip building of these two directories.
    
    0.18.0
    general
     * fixed the flat output mode to properly calculate the lengths of UTF-8
       encoded strings.
     * replaced all uses of std::strtol() to parse_integer() to properly
       parse strings that are not necessarily null-terminated.
     * added a new output format type 'debug-state' which dumps the internal
       state of the populated document model in detail. This can be useful
       during debugging.
     * separated the import_shared_string interface implementation from the
       backend shared strings store per separation of responsibility.
     * merged the foo_t and foo_active_t struct pair, such as font_t and
       font_active_t, in the styles store into a single type using std::optional.
     * revised the documentation and public API and cleaned things up where
       necessary.
    ods
     * re-implemented the number format styles import to correctly keep
       track of element stacks and correctly perform structure checks to
       detect malformed documents.
       * added new interface to import named styles applied to columns.
       * added new interface to import attributes for asian and complex
         scripts for the folloiwng font attributes:
         * font name
         * font size
         * font style
         * font weight
       * re-designed the styles import interface to make it multi-level.
       * re-worked the import of the style:text-underline-width attribute
         to make its handling more in line with the specifications.
    xls-xml
       * added support for importing wrap-text and shrink-to-fit cell format
         attributes.
       * added support for importing cell-hidden and locked attributes.
       * added support for importing direct and named cell formats applied
         to columns and rows.
    xlsx
       * added support for importing wrap-text and shrink-to-fit cell format
         attributes.
       * added support for importing direct and named cell formats applied to
         columns and rows.
    xml-map
       * added a new interface to pass the encoding information to the document
         model so that it can correctly decode non-UTF-8-encoded string values.

--- converters/orcus/buildlink3.mk
+++ converters/orcus/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/08/14 05:23:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/10/15 07:09:50 ryoon Exp $
 
 BUILDLINK_TREE+=	orcus
 
 .if !defined(ORCUS_BUILDLINK3_MK)
 ORCUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.17.0
-BUILDLINK_ABI_DEPENDS.orcus?=	orcus>=0.17.2nb3
+BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.18.1
 BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
 
 .endif	# ORCUS_BUILDLINK3_MK
