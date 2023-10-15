f6e49e97a06158fecff5ca662fe29712f93d049e
wiz <wiz@pkgsrc.org>
2023-10-15 08:00:14 +0000

    devel/appstream: import appstream-0.16.3
    AppStream is a collaborative effort for making machine-readable
    software metadata easily available to programs that need it.  It
    is part of the Freedesktop ecosystem and provides a convenient way
    to retrieve information about available software, making it one of
    the building blocks for modern software centers.
    
    AppStream consists of a specification to describe individual software
    component metadata in XML (so-called MetaInfo files), as well as
    a derived specification for a metadata-collection format to provide
    a list of these metadata entries in XML or YAML for easy consumption
    by software centers and other tools which need to know about
    available software in a repository.  In addition to the metadata
    specification, AppStream specifies a set of related features to
    help providing better metadata for software repositories (primarily
    in Linux distributions).  This reference implementation of AppStream
    provides a shared library to work with these metadata files, features
    to index and query their data quickly, as well as other useful
    related functionality to make building programs which work with
    software metadata very easy.

---
+++ devel/appstream/patches/patch-src_as-system-info.c
@@ -0,0 +1,24 @@
+$NetBSD: patch-src_as-system-info.c,v 1.1 2023/10/15 08:00:14 wiz Exp $
+
+Add NetBSD support.
+
+--- src/as-system-info.c.orig	2023-08-22 21:51:55.000000000 +0000
++++ src/as-system-info.c
+@@ -45,7 +45,7 @@
+ 
+ #if defined(__linux__)
+ #include <sys/sysinfo.h>
+-#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) || defined(__NetBSD__)
+ #include <sys/types.h>
+ #include <sys/sysctl.h>
+ #endif
+@@ -405,7 +405,7 @@ as_get_physical_memory_total (void)
+ 	if (si.mem_unit > 0)
+ 		return (si.totalram * si.mem_unit) / MB_IN_BYTES;
+ 	return 0;
+-#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) || defined(__NetBSD__)
+ 	unsigned long physmem;
+ 	sysctl ((int[]){ CTL_HW, HW_PHYSMEM }, 2, &physmem, &(size_t){ sizeof (physmem) }, NULL, 0);
+ 	return physmem / MB_IN_BYTES;
