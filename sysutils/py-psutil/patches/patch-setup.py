cf7a11965c000dafadae47ae3362d9e72ecc5e68
adam <adam@pkgsrc.org>
2023-10-18 08:53:08 +0000

    py-psutil: updated to 5.9.6
    5.9.6
    =====
    
    2023-10-14
    
    **Enhancements**
    
    - 1703_: `cpu_percent()`_ and `cpu_times_percent()`_ are now thread safe,
      meaning they can be called from different threads and still return
      meaningful and independent results. Before, if (say) 10 threads called
      ``cpu_percent(interval=None)`` at the same time, only 1 thread out of 10
      would get the right result.
    - 2266_: if `Process`_ class is passed a very high PID, raise `NoSuchProcess`_
      instead of OverflowError.  (patch by Xuehai Pan)
    - 2246_: drop python 3.4 & 3.5 support.  (patch by Matthieu Darbois)
    - 2290_: PID reuse is now pre-emptively checked for `Process.ppid()`_  and
      `Process.parents()`_.
    - 2312_: use ``ruff`` Python linter instead of ``flake8 + isort``. It's an
      order of magnitude faster + it adds a ton of new code quality checks.
    
    **Bug fixes**
    
    - 2195_, [Linux]: no longer print exception at import time in case /proc/stat
      can't be read due to permission error. Redirect it to ``PSUTIL_DEBUG``
      instead.
    - 2241_, [NetBSD]: can't compile On NetBSD 10.99.3/amd64.  (patch by Thomas
      Klausner)
    - 2245_, [Windows]: fix var unbound error on possibly in `swap_memory()`_
      (patch by student_2333)
    - 2268_: ``bytes2human()`` utility function was unable to properly represent
      negative values.
    - 2252_, [Windows]: `disk_usage()`_ fails on Python 3.12+.  (patch by
      Matthieu Darbois)
    - 2284_, [Linux]: `Process.memory_full_info()`_ may incorrectly raise
      `ZombieProcess`_ if it's determined via ``/proc/pid/smaps_rollup``. Instead
      we now fallback on reading ``/proc/pid/smaps``.
    - 2287_, [OpenBSD], [NetBSD]: `Process.is_running()`_ erroneously return
      ``False`` for zombie processes, because creation time cannot be determined.
    - 2288_, [Linux]: correctly raise `ZombieProcess`_ on `Process.exe()`_,
      `Process.cmdline()`_ and `Process.memory_maps()`_ instead of returning a
      "null" value.
    - 2290_: differently from what stated in the doc, PID reuse is not
      pre-emptively checked for `Process.nice()`_ (set), `Process.ionice()`_,
      (set), `Process.cpu_affinity()`_ (set), `Process.rlimit()`_
      (set), `Process.parent()`_.
    - 2308_, [OpenBSD]: `Process.threads()`_ always fail with AccessDenied (also as
      root).

--- sysutils/py-psutil/patches/patch-setup.py
+++ sysutils/py-psutil/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.7 2023/04/18 07:37:42 adam Exp $
+$NetBSD: patch-setup.py,v 1.8 2023/10/18 08:53:08 adam Exp $
 
 Enable limited API on all platforms.
 
---- setup.py.orig	2023-04-17 15:01:41.000000000 +0000
+--- setup.py.orig	2023-10-04 12:46:27.000000000 +0000
 +++ setup.py
-@@ -111,7 +111,7 @@ macros.append(('PSUTIL_VERSION', int(VER
+@@ -115,7 +115,7 @@ macros.append(('PSUTIL_VERSION', int(VER
  
  # Py_LIMITED_API lets us create a single wheel which works with multiple
  # python versions, including unreleased ones.
--if bdist_wheel and CP36_PLUS and (MACOS or LINUX or WINDOWS):
+-if bdist_wheel and CP36_PLUS and (MACOS or LINUX):
 +if bdist_wheel and CP36_PLUS:
      py_limited_api = {"py_limited_api": True}
      macros.append(('Py_LIMITED_API', '0x03060000'))
- else:
+ elif bdist_wheel and CP37_PLUS and WINDOWS:
