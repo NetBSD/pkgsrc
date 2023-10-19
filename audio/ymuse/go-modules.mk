3d94ae204348d3eee4e17626f945527bb3e826e1
wiz <wiz@pkgsrc.org>
2023-10-19 12:54:55 +0000

    ymuse: update to 0.22.
    Changelog
    
        f9d9ab2 Add drag-and-drop queue reordering, closes #34 (#80)
        981861c Add german translation. (#68)
        4d4325e Add missing launchable element. (#65)
        f482541 Allow adding/replacing of all tracks in Library by Files context menu (resolves #69)
        d7c40b5 Fix drag-n-drop beyond list end (#34), other optimisations
        d1dcc10 Fix dragging an item down (#34)
        abd7f2e New icon (#79)
        8f8e02c Remove warnings about non-existent/empty Ymuse config (resolves #70)
        30853bf Support for single-track repeat (resolves #76)
        adf6e7b Use remote icon in AppStream metadata (resolves #66)
        95c0d15 chore: release 0.22

--- audio/ymuse/go-modules.mk
+++ audio/ymuse/go-modules.mk
@@ -1,9 +1,9 @@
-# $NetBSD: go-modules.mk,v 1.1 2023/02/01 14:54:22 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.2 2023/10/19 12:54:55 wiz Exp $
 
-GO_MODULE_FILES+=	github.com/fhs/gompd/v2/@v/v2.2.1-0.20220620205817-bbf835995263.mod
-GO_MODULE_FILES+=	github.com/fhs/gompd/v2/@v/v2.2.1-0.20220620205817-bbf835995263.zip
-GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.6.1.mod
-GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.6.1.zip
+GO_MODULE_FILES+=	github.com/fhs/gompd/v2/@v/v2.3.0.mod
+GO_MODULE_FILES+=	github.com/fhs/gompd/v2/@v/v2.3.0.zip
+GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.6.2.mod
+GO_MODULE_FILES+=	github.com/gotk3/gotk3/@v/v0.6.2.zip
 GO_MODULE_FILES+=	github.com/op/go-logging/@v/v0.0.0-20160315200505-970db520ece7.mod
 GO_MODULE_FILES+=	github.com/op/go-logging/@v/v0.0.0-20160315200505-970db520ece7.zip
 GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.9.1.mod
