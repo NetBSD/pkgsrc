1b85dff3886032eb348de9d340aedbc6f62826a0
wiz <wiz@pkgsrc.org>
2023-10-19 15:21:40 +0000

    botan3: add upstream bug report URL

--- security/botan3/buildlink3.mk
+++ security/botan3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/08/14 05:25:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/10/19 15:21:40 wiz Exp $
 
 BUILDLINK_TREE+=	botan3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	botan3
 BOTAN3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.botan3+=	botan3>=3.0.0
-BUILDLINK_ABI_DEPENDS.botan3?=	botan3>=3.0.0nb1
+BUILDLINK_ABI_DEPENDS.botan3?=	botan3>=3.2.0
 BUILDLINK_PKGSRCDIR.botan3?=	../../security/botan3
 
 .include "../../devel/boost-libs/buildlink3.mk"
