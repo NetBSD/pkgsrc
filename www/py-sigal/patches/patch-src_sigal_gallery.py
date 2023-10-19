abc8599ca1bb0889680b7fba17a3f7d85d3eb61e
wiz <wiz@pkgsrc.org>
2023-10-19 15:39:44 +0000

    py-sigal: update to 2.4.
    Version 2.4
    ~~~~~~~~~~~
    
    Released on 2023-09-29.
    
    Sigal now requires Pillow>=8.0.0
    
    - Avoid IndexError with empty source dir [:issue:`498`].
    - New cli option ``--force-album`` to reprocess only specific albums
      [:issue:`491`].
    - Added support for black detection in video thumbnail generation
      [:issue:`485`].
    - Don't show the galleria container if there are no medias [:issue:`484`].
    - Force encrypt's password form to be on top [:issue:`483`].
    - Add mkv to ``video_extensions`` [:issue:`481`].
    - Make sure ``zip_gallery`` is set correctly [:issue:`477`].
    - Updates for recent Pillow changes, requires Pillow>=8.0.0 [:issue:`479`].
    - Make theme dir writable after copying to it [:issue:`471`].
    - New plugin: Titleregexp - modify titles of albums with regular expressions
      [:issue:`469`].
    - List of sort-properties as fallback instead of zero-length string
      [:issue:`468`].

--- www/py-sigal/patches/patch-src_sigal_gallery.py
+++ www/py-sigal/patches/patch-src_sigal_gallery.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-sigal_gallery.py,v 1.1 2023/03/08 21:05:58 wiz Exp $
+$NetBSD: patch-src_sigal_gallery.py,v 1.1 2023/10/19 15:39:44 wiz Exp $
 
 Fix bug in img_format configuration parameter.
 https://github.com/saimn/sigal/issues/436
 
---- sigal/gallery.py.orig	2022-04-08 18:19:56.000000000 +0000
-+++ sigal/gallery.py
-@@ -235,6 +235,7 @@ class Image(Media):
-         super().__init__(filename, path, settings)
-         imgformat = settings.get('img_format')
+--- src/sigal/gallery.py.orig	2023-09-29 07:03:55.000000000 +0000
++++ src/sigal/gallery.py
+@@ -243,6 +243,7 @@ class Image(Media):
+         # Register all formats
+         PILImage.init()
  
 +        PILImage.registered_extensions()
          if imgformat and PILImage.EXTENSION[self.src_ext] != imgformat.upper():
