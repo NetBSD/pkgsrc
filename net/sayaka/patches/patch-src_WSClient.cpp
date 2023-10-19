f0aba1d4c7144bcb11228c7a64b51ec17c5504c5
tsutsui <tsutsui@pkgsrc.org>
2023-10-19 15:11:26 +0000

    sayaka: update to 3.7.2.
    pkgsrc changes:
    - update DESCR (forgot to commit in previous)
    
    Upstream changes:
    * 3.7.2 (2023/10/19)
     - fix a problem that transparent webp images are not drawn
     - add --show-cw and --shownsfw options
     - support NSFW images
     - format messages a bit
     - show external instance names
     - implement reconnection on network errors

--- net/sayaka/patches/patch-src_WSClient.cpp
+++ net/sayaka/patches/patch-src_WSClient.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_WSClient.cpp,v 1.1 2023/10/10 14:20:53 tsutsui Exp $
+$NetBSD: patch-src_WSClient.cpp,v 1.2 2023/10/19 15:11:26 tsutsui Exp $
 
 - workaround errors of gcc7 on netbsd-9:
-> WSClient.cpp:71:2: sorry, unimplemented: non-trivial designated initializers not supported
+> WSClient.cpp:73:2: sorry, unimplemented: non-trivial designated initializers not supported
 
---- src/WSClient.cpp.orig	2023-10-09 11:22:42.000000000 +0000
+--- src/WSClient.cpp.orig	2023-10-19 10:40:17.000000000 +0000
 +++ src/WSClient.cpp
-@@ -67,6 +67,9 @@ WSClient::Init()
+@@ -69,6 +69,9 @@ WSClient::Init(wsclient_onmsg_callback_t
  		.recv_callback			= wsclient_recv_callback,
  		.send_callback			= wsclient_send_callback,
  		.genmask_callback		= wsclient_genmask_callback,
@@ -14,4 +14,4 @@ $NetBSD: patch-src_WSClient.cpp,v 1.1 2023/10/10 14:20:53 tsutsui Exp $
 +		.on_frame_recv_end_callback	= NULL,
  		.on_msg_recv_callback	= wsclient_on_msg_recv_callback,
  	};
- 	if (wslay_event_context_client_init(&wsctx, &callbacks, this) != 0) {
+ 	int r = wslay_event_context_client_init(&wsctx, &callbacks, this);
