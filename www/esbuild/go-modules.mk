98df39f46847a6035f7b058ea72bc79724b94323
bsiegert <bsiegert@pkgsrc.org>
2023-10-19 10:22:28 +0000

    New package for esbuild, from wip
    The main goal of the esbuild bundler project is to bring about a new era
    of build tool performance, and create an easy-to-use modern bundler
    along the way.
    
    Major features:
    
    * Extreme speed without needing a cache
    * ES6 and CommonJS modules
    * Tree shaking of ES6 modules
    * An API for JavaScript and Go
    * TypeScript and JSX syntax
    * Source maps
    * Minification
    * Plugins

---
+++ www/esbuild/go-modules.mk
@@ -0,0 +1,4 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/10/19 10:22:28 bsiegert Exp $
+
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
