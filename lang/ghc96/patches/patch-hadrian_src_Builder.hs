d7de91dc6f785c9dc170d90f969ec24984e510ee
pho <pho@pkgsrc.org>
2023-10-23 08:06:48 +0000

    lang/ghc96: import ghc-9.6.3
    GHC: The Glasgow Haskell Compiler.
    
    The Glasgow Haskell Compiler is a robust, fully-featured, optimising
    compiler for the functional programming language Haskell 98
    (http://www.haskell.org). GHC compiles Haskell to either native code
    or C. It implements numerous experimental language extensions to
    Haskell, including concurrency, a foreign language interface, several
    type-system extensions, exceptions, and so on. GHC comes with a
    generational garbage collector, a space and time profiler, and a
    comprehensive set of libraries.
    
    This package provides the 9.6.x release series.

---
+++ lang/ghc96/patches/patch-hadrian_src_Builder.hs
@@ -0,0 +1,52 @@
+$NetBSD: patch-hadrian_src_Builder.hs,v 1.1 2023/10/23 08:06:48 pho Exp $
+
+Hunk #0, #2:
+    This is pkgsrc-specific. We build haddock as a dynamically-linked
+    executable but it doesn't contain an rpath to our not-yet-installed
+    shared libraries. This is usually not a problem because Hadrian embeds
+    relative rpaths (i.e. $ORIGIN) in dynamically-linked objects but we
+    patch Hadrian not to do that.
+
+Hunk #1:
+
+    primop.txt contains Unicode characters. Don't assume the current locale
+    is "*.UTF-8". TODO: Upstream this.
+
+    > _build/stage0/compiler/build/primops.txt: hGetContents: invalid argument (cannot decode byte sequence starting from 226)
+
+--- hadrian/src/Builder.hs.orig	2023-09-21 11:30:31.000000000 +0000
++++ hadrian/src/Builder.hs
+@@ -31,6 +31,7 @@ import Hadrian.Utilities
+ import Oracles.Setting (bashPath)
+ import System.Exit
+ import System.IO (stderr)
++import System.Directory (makeAbsolute)
+ 
+ import Base
+ import Context
+@@ -333,8 +334,8 @@ instance H.Builder Builder where
+                 GenApply -> captureStdout
+ 
+                 GenPrimopCode -> do
+-                    stdin <- readFile' input
+-                    Stdout stdout <- cmd' (Stdin stdin) [path] buildArgs
++                    need [input]
++                    Stdout stdout <- cmd' (FileStdin input) [path] buildArgs
+                     -- see Note [Capture stdout as a ByteString]
+                     writeFileChangedBS output stdout
+ 
+@@ -383,6 +384,14 @@ instance H.Builder Builder where
+                   when (code /= ExitSuccess) $ do
+                     fail "tests failed"
+ 
++                Haddock _ -> do
++                  libPath'  <- libPath (vanillaContext Stage1 haddock)
++                  distDir'  <- distDir Stage1
++                  shlibPath <- liftIO $ makeAbsolute (libPath' -/- distDir')
++                  let envs = [ AddEnv "LD_LIBRARY_PATH" shlibPath
++                             , AddEnv "DYLD_LIBRARY_PATH" shlibPath ] -- Darwin
++                  cmd' envs [path] buildArgs
++
+                 _  -> cmd' [path] buildArgs
+ 
+ -- TODO: Some builders are required only on certain platforms. For example,
