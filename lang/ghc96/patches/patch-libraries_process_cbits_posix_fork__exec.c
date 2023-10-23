2aaf52fe61232884a218fee4570e57438a62ee32
pho <pho@pkgsrc.org>
2023-10-23 08:06:48 +0000

    lang/ghc96: import ghc-9.6.3
    GHC: The Glasgow Haskell Compiler.
    
    The Glasgow Haskell Compiler is a robust, fully-featured, optimising
    compiler for the functional programming language Haskell 98
    (http://www.haskell.org). GHC compiles Haskell to either native code
    or C. It implements numerous experimental language extensions to
    Haskell, including concurrency, a foreign language interface, several
    type-system extensions, exceptions, and so on. GHC comes with a
    generational garbage collector, a space and time profiler, and a
    comprehensive set of libraries.
    
    This package provides the 9.6.x release series.

---
+++ lang/ghc96/patches/patch-libraries_process_cbits_posix_fork__exec.c
@@ -0,0 +1,20 @@
+$NetBSD: patch-libraries_process_cbits_posix_fork__exec.c,v 1.1 2023/10/23 08:06:49 pho Exp $
+
+Ensure _POSIX_PTHREAD_SEMANTICS is defined on SunOS.
+
+Pull request:
+https://github.com/haskell/process/pull/274
+
+--- libraries/process/cbits/posix/fork_exec.c.orig	2023-08-21 03:50:41.000000000 +0000
++++ libraries/process/cbits/posix/fork_exec.c
+@@ -1,6 +1,10 @@
+ /* ensure that execvpe is provided if possible */
+ #define _GNU_SOURCE 1
+ 
++#ifdef __sun
++#define _POSIX_PTHREAD_SEMANTICS
++#endif
++
+ #include "common.h"
+ 
+ #if defined(HAVE_FORK)
