8cd1ef5d0639ec9cc2bb39c3a8af76f8c97ebe36
micha <micha@pkgsrc.org>
2023-10-23 14:04:18 +0000

    converters/utf8proc: Update to 2.9.0
    pkgsrc changes:
    - Replace USE_LANGUAGES with USE_CC_FEATURES to declare required
      language version
    - Add FORCE_C_STD to switch the compiler to that version
    - Add BUILDLINK_ABI_DEPENDS for new ABI major version
    
    Changelog:
    - Unicode 15.1 support (#253).

--- converters/utf8proc/buildlink3.mk
+++ converters/utf8proc/buildlink3.mk
@@ -1,11 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/30 19:31:47 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/10/23 14:04:18 micha Exp $
 
 BUILDLINK_TREE+=	utf8proc
 
 .if !defined(UTF8PROC_BUILDLINK3_MK)
 UTF8PROC_BUILDLINK3_MK:=
 
+USE_CC_FEATURES+=	c99
+
 BUILDLINK_API_DEPENDS.utf8proc+=	utf8proc>=2.1.1
+BUILDLINK_ABI_DEPENDS.utf8proc+=	utf8proc>=2.9.0
 BUILDLINK_PKGSRCDIR.utf8proc?=		../../converters/utf8proc
 .endif	# UTF8PROC_BUILDLINK3_MK
 
