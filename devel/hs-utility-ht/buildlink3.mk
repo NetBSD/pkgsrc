75c83cdec798aa883b04a960a07dfde8fb2e7007
pho <pho@pkgsrc.org>
2023-10-24 11:11:34 +0000

    devel/hs-utility-ht: Update to 0.0.17
    No change log is provided by the upstream.

--- devel/hs-utility-ht/buildlink3.mk
+++ devel/hs-utility-ht/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/24 11:11:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-utility-ht
 
 .if !defined(HS_UTILITY_HT_BUILDLINK3_MK)
 HS_UTILITY_HT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.16
-BUILDLINK_ABI_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.16nb6
+BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.17
+BUILDLINK_ABI_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.17
 BUILDLINK_PKGSRCDIR.hs-utility-ht?=	../../devel/hs-utility-ht
 .endif	# HS_UTILITY_HT_BUILDLINK3_MK
 
