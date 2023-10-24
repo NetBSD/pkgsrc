8819a29b03a5cf1ea0ce0a161b1e4d17d2be095e
pho <pho@pkgsrc.org>
2023-10-24 14:06:24 +0000

    devel/hs-reflection: Update to 2.1.7
    2.1.7 [2023.02.28]
    
    * When building with base-4.18 (GHC 9.6) or later, implement reifyNat and
      reifySymbol using the API provided by GHC.TypeLits instead of resorting
      to unsafeCoerce.

--- devel/hs-reflection/buildlink3.mk
+++ devel/hs-reflection/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/24 14:06:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-reflection
 
 .if !defined(HS_REFLECTION_BUILDLINK3_MK)
 HS_REFLECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.6
-BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.6nb6
+BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.7
+BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.7
 BUILDLINK_PKGSRCDIR.hs-reflection?=	../../devel/hs-reflection
 .endif	# HS_REFLECTION_BUILDLINK3_MK
 
