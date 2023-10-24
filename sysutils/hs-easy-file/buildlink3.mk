8fd316057d4b382aa698bc83d4b37aa46010ccab
pho <pho@pkgsrc.org>
2023-10-24 08:14:10 +0000

    sysutils/hs-easy-file: Update to 0.2.5
    No change log is provided by the upstream.

--- sysutils/hs-easy-file/buildlink3.mk
+++ sysutils/hs-easy-file/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/24 08:14:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-easy-file
 
 .if !defined(HS_EASY_FILE_BUILDLINK3_MK)
 HS_EASY_FILE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-easy-file+=	hs-easy-file>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-easy-file+=	hs-easy-file>=0.2.2nb5
+BUILDLINK_API_DEPENDS.hs-easy-file+=	hs-easy-file>=0.2.5
+BUILDLINK_ABI_DEPENDS.hs-easy-file+=	hs-easy-file>=0.2.5
 BUILDLINK_PKGSRCDIR.hs-easy-file?=	../../sysutils/hs-easy-file
 .endif	# HS_EASY_FILE_BUILDLINK3_MK
 
