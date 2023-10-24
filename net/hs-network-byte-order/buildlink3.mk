129703fa42a2a3cb2b3da21df722e4579e0dbfc9
pho <pho@pkgsrc.org>
2023-10-24 08:28:16 +0000

    net/hs-network-byte-order: Update to 0.1.7
    No change log is provided by the upstream.

--- net/hs-network-byte-order/buildlink3.mk
+++ net/hs-network-byte-order/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:42 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/24 08:28:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-network-byte-order
 
 .if !defined(HS_NETWORK_BYTE_ORDER_BUILDLINK3_MK)
 HS_NETWORK_BYTE_ORDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network-byte-order+=	hs-network-byte-order>=0.1.6
-BUILDLINK_ABI_DEPENDS.hs-network-byte-order+=	hs-network-byte-order>=0.1.6nb5
+BUILDLINK_API_DEPENDS.hs-network-byte-order+=	hs-network-byte-order>=0.1.7
+BUILDLINK_ABI_DEPENDS.hs-network-byte-order+=	hs-network-byte-order>=0.1.7
 BUILDLINK_PKGSRCDIR.hs-network-byte-order?=	../../net/hs-network-byte-order
 .endif	# HS_NETWORK_BYTE_ORDER_BUILDLINK3_MK
 
