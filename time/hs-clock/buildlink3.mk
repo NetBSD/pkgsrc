6ec800aaa7ea4fd17348cd2cf64e806b73d9a456
pho <pho@pkgsrc.org>
2023-10-24 10:42:22 +0000

    time/hs-clock: Update to 0.8.4
    No change log is provided by the upstream.

--- time/hs-clock/buildlink3.mk
+++ time/hs-clock/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/24 10:42:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-clock
 
 .if !defined(HS_CLOCK_BUILDLINK3_MK)
 HS_CLOCK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-clock+=	hs-clock>=0.8.3
-BUILDLINK_ABI_DEPENDS.hs-clock+=	hs-clock>=0.8.3nb3
+BUILDLINK_API_DEPENDS.hs-clock+=	hs-clock>=0.8.4
+BUILDLINK_ABI_DEPENDS.hs-clock+=	hs-clock>=0.8.4
 BUILDLINK_PKGSRCDIR.hs-clock?=		../../time/hs-clock
 .endif	# HS_CLOCK_BUILDLINK3_MK
 
