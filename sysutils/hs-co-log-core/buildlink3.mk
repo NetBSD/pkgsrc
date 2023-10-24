c5a71da7a6a009d34dd2dcc46ef5dc64caf06f8f
pho <pho@pkgsrc.org>
2023-10-24 17:33:24 +0000

    sysutils/hs-co-log-core: Update to 0.3.2.1
    0.3.2.1 — Oct 20, 2023
    What's Changed
    
        Relax doctest boundaries. by @alaendle in #32
        GA(deps): Bump actions/checkout from 3 to 4 by @dependabot in #35
        Allow doctest-0.22 by @Vekhir in #36
        [#29] Support GHC 9.6 by @vrom911 in #33
        Support ghc-9.8 by @alaendle in #37
        Publish to hackage directly from GitHub by @alaendle in #38
    
    New Contributors
    
        @Vekhir made their first contribution in https://github.com/co-log/co-log-core/pull/36
    
    Full Changelog: https://github.com/co-log/co-log-core/compare/v0.3.2.0...v0.3.2.1

--- sysutils/hs-co-log-core/buildlink3.mk
+++ sysutils/hs-co-log-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/24 17:33:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-co-log-core
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-co-log-core
 HS_CO_LOG_CORE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-co-log-core+=	hs-co-log-core>=0.3.2
-BUILDLINK_ABI_DEPENDS.hs-co-log-core+=	hs-co-log-core>=0.3.2.0nb2
+BUILDLINK_ABI_DEPENDS.hs-co-log-core+=	hs-co-log-core>=0.3.2.1
 BUILDLINK_PKGSRCDIR.hs-co-log-core?=	../../sysutils/hs-co-log-core
 .endif	# HS_CO_LOG_CORE_BUILDLINK3_MK
 
