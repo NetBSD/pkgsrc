4ea88038c7a561303c30d121623e4840d2781414
pho <pho@pkgsrc.org>
2023-10-24 12:31:41 +0000

    devel/hs-logict: Update to 0.8.1.0
    * Add instance MonadLogic (Control.Monad.Writer.CPS.WriterT w m).

--- devel/hs-logict/buildlink3.mk
+++ devel/hs-logict/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/24 12:31:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-logict
 
 .if !defined(HS_LOGICT_BUILDLINK3_MK)
 HS_LOGICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-logict+=	hs-logict>=0.8.0
-BUILDLINK_ABI_DEPENDS.hs-logict+=	hs-logict>=0.8.0.0nb2
+BUILDLINK_API_DEPENDS.hs-logict+=	hs-logict>=0.8.1
+BUILDLINK_ABI_DEPENDS.hs-logict+=	hs-logict>=0.8.1.0
 BUILDLINK_PKGSRCDIR.hs-logict?=		../../devel/hs-logict
 .endif	# HS_LOGICT_BUILDLINK3_MK
 
