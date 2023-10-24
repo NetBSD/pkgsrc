653d1c26f55d0b5c35c41b1cc233afabf7e27fe2
pho <pho@pkgsrc.org>
2023-10-24 17:37:56 +0000

    devel/hs-sorted-list: Update to 0.2.2.0
    0.2.2.0
    * Add deleteAll.

--- devel/hs-sorted-list/buildlink3.mk
+++ devel/hs-sorted-list/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/24 17:37:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-sorted-list
 
 .if !defined(HS_SORTED_LIST_BUILDLINK3_MK)
 HS_SORTED_LIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-sorted-list+=	hs-sorted-list>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-sorted-list+=	hs-sorted-list>=0.2.1.0nb3
+BUILDLINK_API_DEPENDS.hs-sorted-list+=	hs-sorted-list>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-sorted-list+=	hs-sorted-list>=0.2.2.0
 BUILDLINK_PKGSRCDIR.hs-sorted-list?=	../../devel/hs-sorted-list
 .endif	# HS_SORTED_LIST_BUILDLINK3_MK
 
