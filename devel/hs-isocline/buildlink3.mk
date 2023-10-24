367db83681e57581c7fd7ae95a69973d0d257e83
pho <pho@pkgsrc.org>
2023-10-24 14:08:06 +0000

    devel/hs-isocline: import hs-isocline-1.0.9
    A Haskell wrapper around the Isocline C library which can provide an
    alternative to GNU Readline.

---
+++ devel/hs-isocline/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/24 14:08:06 pho Exp $
+
+BUILDLINK_TREE+=	hs-isocline
+
+.if !defined(HS_ISOCLINE_BUILDLINK3_MK)
+HS_ISOCLINE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-isocline+=	hs-isocline>=1.0.9
+BUILDLINK_ABI_DEPENDS.hs-isocline+=	hs-isocline>=1.0.9
+BUILDLINK_PKGSRCDIR.hs-isocline?=	../../devel/hs-isocline
+.endif	# HS_ISOCLINE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-isocline
