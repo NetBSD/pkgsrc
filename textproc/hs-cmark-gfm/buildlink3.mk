09b867eebf462d462c88ab6fdc24ae3bf27a1417
pho <pho@pkgsrc.org>
2023-10-24 07:58:40 +0000

    textproc/hs-cmark-gfm: Update to 0.2.6
    cmark-gfm 0.2.6 (28 Aug 2023)
    * Pull in upstream changes (includes security fixes).

--- textproc/hs-cmark-gfm/buildlink3.mk
+++ textproc/hs-cmark-gfm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/24 07:58:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-cmark-gfm
 
 .if !defined(HS_CMARK_GFM_BUILDLINK3_MK)
 HS_CMARK_GFM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.5
-BUILDLINK_ABI_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.5nb2
+BUILDLINK_API_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.6
+BUILDLINK_ABI_DEPENDS.hs-cmark-gfm+=	hs-cmark-gfm>=0.2.6
 BUILDLINK_PKGSRCDIR.hs-cmark-gfm?=	../../textproc/hs-cmark-gfm
 
 .include "../../textproc/cmark-gfm/buildlink3.mk"
