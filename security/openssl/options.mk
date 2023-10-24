288a5c1f0caeb87c9c2e7fee840404de99e2d4d9
wiz <wiz@pkgsrc.org>
2023-10-24 21:30:35 +0000

    openssl: update to 3.1.4
    This is the latest version of the currently newest supported branch.
    The 1.1.x branch was de-supported in September.
    
    Based on wip/openssl3 by gdt, tnn, and myself.

--- security/openssl/options.mk
+++ security/openssl/options.mk
@@ -1,29 +1,12 @@
-# $NetBSD: options.mk,v 1.12 2020/01/20 17:42:53 jperkin Exp $
+# $NetBSD: options.mk,v 1.13 2023/10/24 21:30:35 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openssl
-PKG_SUPPORTED_OPTIONS=	idea md2 mdc2 rc5 zlib threads
-PKG_SUGGESTED_OPTIONS=	idea md2 mdc2 rc5 threads
+PKG_SUPPORTED_OPTIONS=	zlib threads
+PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
-
-#
-# Support optional algorithms
-#
-.for alg in idea md2 mdc2 rc5
-.  if !empty(PKG_OPTIONS:M${alg})
-CONFIGURE_ARGS+=	enable-${alg}
-PLIST.${alg}=		yes
-.  else
-CONFIGURE_ARGS+=	no-${alg}
-.  endif
-.endfor
-#
-# Only supported in certain Linux configurations, needs proper handling if
-# it is to be supported as an option.
-#
-CONFIGURE_ARGS+=	no-afalgeng
 
 .if !empty(PKG_OPTIONS:Mzlib)
 CONFIGURE_ARGS+=	zlib
