e7f20b8faf42637e3349d022187a17cae59a11b1
pho <pho@pkgsrc.org>
2023-10-24 12:16:09 +0000

    devel/hs-managed: Update to 1.0.10
    No change log is provided by the upstream.

--- devel/hs-managed/buildlink3.mk
+++ devel/hs-managed/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/24 12:16:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-managed
 
 .if !defined(HS_MANAGED_BUILDLINK3_MK)
 HS_MANAGED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-managed+=	hs-managed>=1.0.9
-BUILDLINK_ABI_DEPENDS.hs-managed+=	hs-managed>=1.0.9nb3
+BUILDLINK_API_DEPENDS.hs-managed+=	hs-managed>=1.0.10
+BUILDLINK_ABI_DEPENDS.hs-managed+=	hs-managed>=1.0.10
 BUILDLINK_PKGSRCDIR.hs-managed?=	../../devel/hs-managed
 .endif	# HS_MANAGED_BUILDLINK3_MK
 
