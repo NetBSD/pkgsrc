165d35d7c1181d666cfd947692f551b765b8ee96
pho <pho@pkgsrc.org>
2023-10-24 06:38:45 +0000

    lang/ghc96: Fix a linkage issue in the unix package

--- lang/ghc96/buildlink3.mk
+++ lang/ghc96/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/10/23 17:00:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/10/24 06:38:45 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.6.3nb1
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.6.3nb1
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.6.3nb2
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.6.3nb2
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc96
 
 .include "../../converters/libiconv/buildlink3.mk"
