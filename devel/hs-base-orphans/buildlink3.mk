bc3a16e9ebd228686365d9bc5a6f744c085981b5
pho <pho@pkgsrc.org>
2023-10-25 13:49:51 +0000

    devel/hs-base-orphans: Update to 0.9.1
    Changes in 0.9.1 [2023.10.11]
    * Backport new instances from GHC 9.8/base-4.19:
      - Eq and Ord instances for SChar, SNat, and SSymbol
      - Enum, Bounded, Num, Real, and Integral instances for Compose
    
    Changes in 0.9.0 [2023.03.05]
    * Adapt to recent changes to Down instances:
      - The Bounded instance for Down was changed in base-4.15.0.0 to swap the
        values of minBound and maxBound for the underlying type. This change
        has now been propagated to base-orphans.
      - The Enum instance for Down was removed in base-4.15.0.0, but a
        different version of the instance was added back in base-4.18.0.0,
        where succ and pred are swapped. We have changed the backported version
        of this instance in base-orphans to match the behavior of the instance
        added in base-4.18.0.0.
      - The Integral instance for Down was removed from base entirely in
        base-4.15.0.0. We have finally removed it from base-orphans in this
        release, as it actively makes it more difficult to define the
        aforementioned Enum instance.
    
    Changes in 0.8.8.1 [2023.03.05]
    * Fix GHC 9.4 build error that was accidentally introduced in
      base-orphans-0.8.8.
    
    Changes in 0.8.8 [2023.03.05]
    * Backport new instances from GHC 9.6.1/base-4.18.0.0:
      - Functor instances for the (,,,,) a b c d, (,,,,,) a b c d e, and
        (,,,,,) a b c d e f tuple types.
      - Eq and Ord instances for Generically1

--- devel/hs-base-orphans/buildlink3.mk
+++ devel/hs-base-orphans/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/25 13:49:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-orphans
 
 .if !defined(HS_BASE_ORPHANS_BUILDLINK3_MK)
 HS_BASE_ORPHANS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.6
-BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.7nb2
+BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.9.1
+BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.9.1
 BUILDLINK_PKGSRCDIR.hs-base-orphans?=	../../devel/hs-base-orphans
 .endif	# HS_BASE_ORPHANS_BUILDLINK3_MK
 
