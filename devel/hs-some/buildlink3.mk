5a5f1863595e4e708069cb8a1b43870ff43ae0e1
pho <pho@pkgsrc.org>
2023-10-25 13:52:16 +0000

    devel/hs-some: Update to 1.0.6
    1.0.6
    * Add instances for SSymbol, SNat and SChar from `base >=4.18.0.0'
    
    1.0.5
    * Add EqP and OrdP classes. These are strong versions of Eq1 and Ord1, and
      on the other hand weaker versions of GEq and GCompare. They are exactly
      what's needed for Eq and Ord instances of Some.
    
      The naming is unfortunate: GShow would be better named ShowP, as it's
      similar version of Show1.
    
      Note: we could add ReadP with readsPrecP :: Int -> ReadS (t a) method,
      but it will barely have any instances. GRead is different, as it can
      reify the type index for many types, e.g. for the singletons.
    
      In some future there will be major version of some with following
      breaking changes:
      - EqP and OrdP will become superclasses of GEq and GCompare
      - Eq (Some t) will require EqP t, similarly for Ord and OrdP.
      - GShow will get forall a. Show (f a) superclass. (This will cause
        removal of Product and Sum instances for base <4.18).
    
      To ease future transition you may
      - Define EqP and OrdP instances for your types. The defaultEq and
        defaultCompare methods can be used to define eqp and comparep from GEq
        and GCompare instances respectively.
      - Move to use GHC.Generics.:*: and :+: instead of Data.Functor.Product
        and Sum, as these have better Eq and Ord instances.

--- devel/hs-some/buildlink3.mk
+++ devel/hs-some/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/25 13:52:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-some
 
 .if !defined(HS_SOME_BUILDLINK3_MK)
 HS_SOME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-some+=	hs-some>=1.0.4
-BUILDLINK_ABI_DEPENDS.hs-some+=	hs-some>=1.0.4.1nb2
+BUILDLINK_API_DEPENDS.hs-some+=	hs-some>=1.0.6
+BUILDLINK_ABI_DEPENDS.hs-some+=	hs-some>=1.0.6
 BUILDLINK_PKGSRCDIR.hs-some?=	../../devel/hs-some
+
+.include "../../devel/hs-base-orphans/buildlink3.mk"
 .endif	# HS_SOME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-some
