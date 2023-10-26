56febbb034bd41c07c47ab9af3ca139889507216
pho <pho@pkgsrc.org>
2023-10-26 05:15:19 +0000

    devel/hs-githash: Update to 0.1.7.0
    0.1.7.0
        Expose giFiles to users.

--- devel/hs-githash/buildlink3.mk
+++ devel/hs-githash/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/26 05:15:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-githash
 
 .if !defined(HS_GITHASH_BUILDLINK3_MK)
 HS_GITHASH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-githash+=	hs-githash>=0.1.6
-BUILDLINK_ABI_DEPENDS.hs-githash+=	hs-githash>=0.1.6.3nb2
+BUILDLINK_API_DEPENDS.hs-githash+=	hs-githash>=0.1.7
+BUILDLINK_ABI_DEPENDS.hs-githash+=	hs-githash>=0.1.7.0
 BUILDLINK_PKGSRCDIR.hs-githash?=	../../devel/hs-githash
 
 .include "../../devel/hs-th-compat/buildlink3.mk"
