e0b6197f9f8749a7e76a2bb54bfdb02aa37d8001
adam <adam@pkgsrc.org>
2023-10-26 06:32:57 +0000

    py-OpenSSL: updated to 23.3.0
    23.3.0 (2023-10-25)
    -------------------
    
    Backward-incompatible changes:
    
    - Dropped support for Python 3.6.
    - The minimum ``cryptography`` version is now 41.0.5.
    - Removed ``OpenSSL.crypto.loads_pkcs7`` and ``OpenSSL.crypto.loads_pkcs12`` which had been deprecated for 3 years.
    - Added ``OpenSSL.SSL.OP_LEGACY_SERVER_CONNECT`` to allow legacy insecure renegotiation between OpenSSL and unpatched servers.
    
    Deprecations:
    
    - Deprecated ``OpenSSL.crypto.PKCS12`` (which was intended to have been deprecated at the same time as ``OpenSSL.crypto.load_pkcs12``).
    - Deprecated ``OpenSSL.crypto.NetscapeSPKI``.
    - Deprecated ``OpenSSL.crypto.CRL``
    - Deprecated ``OpenSSL.crypto.Revoked``
    - Deprecated ``OpenSSL.crypto.load_crl`` and ``OpenSSL.crypto.dump_crl``
    - Deprecated ``OpenSSL.crypto.sign`` and ``OpenSSL.crypto.verify``
    - Deprecated ``OpenSSL.crypto.X509Extension``
    
    Changes:
    
    - Changed ``OpenSSL.crypto.X509Store.add_crl`` to also accept
      ``cryptography``'s ``x509.CertificateRevocationList`` arguments in addition
      to the now deprecated ``OpenSSL.crypto.CRL`` arguments.
    - Fixed ``test_set_default_verify_paths`` test so that it is skipped if no
      network connection is available.

--- security/py-OpenSSL/patches/patch-tests_memdbg.py
+++ security/py-OpenSSL/patches/patch-tests_memdbg.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-tests_memdbg.py,v 1.3 2020/12/19 11:52:26 leot Exp $
+$NetBSD: patch-tests_memdbg.py,v 1.4 2023/10/26 06:32:57 adam Exp $
 
 Fix running test on NetBSD.
 https://github.com/pyca/pyopenssl/issues/595
 
---- tests/memdbg.py.orig	2020-12-15 15:30:54.000000000 +0000
+--- tests/memdbg.py.orig	2023-10-19 22:18:27.000000000 +0000
 +++ tests/memdbg.py
-@@ -19,9 +19,9 @@ _ffi.cdef(
+@@ -17,9 +17,9 @@ _ffi.cdef(
      int  CRYPTO_set_mem_functions(
          void *(*m)(size_t),void *(*r)(void *,size_t), void (*f)(void *));
  
@@ -16,9 +16,9 @@ https://github.com/pyca/pyopenssl/issues/595
 +    char **backtrace_symbols(void *const *buffer, size_t size);
 +    void backtrace_symbols_fd(void *const *buffer, size_t size, int fd);
      """
- )  # noqa
+ )
  _api = _ffi.verify(
-@@ -30,7 +30,7 @@ _api = _ffi.verify(
+@@ -28,7 +28,7 @@ _api = _ffi.verify(
      #include <stdlib.h>
      #include <execinfo.h>
      """,
