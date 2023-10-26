5de76c6009826a1f7485a2cdc75aa3c578fd21c1
pho <pho@pkgsrc.org>
2023-10-26 06:55:35 +0000

    devel/hs-memotrie: Update to 0.6.11
    No change log is provided by the upstream.

--- devel/hs-memotrie/buildlink3.mk
+++ devel/hs-memotrie/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/26 06:55:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-memotrie
 
 .if !defined(HS_MEMOTRIE_BUILDLINK3_MK)
 HS_MEMOTRIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-memotrie+=	hs-memotrie>=0.6.10
-BUILDLINK_ABI_DEPENDS.hs-memotrie+=	hs-memotrie>=0.6.10nb2
+BUILDLINK_API_DEPENDS.hs-memotrie+=	hs-memotrie>=0.6.11
+BUILDLINK_ABI_DEPENDS.hs-memotrie+=	hs-memotrie>=0.6.11
 BUILDLINK_PKGSRCDIR.hs-memotrie?=	../../devel/hs-memotrie
 
 .include "../../devel/hs-newtype-generics/buildlink3.mk"
