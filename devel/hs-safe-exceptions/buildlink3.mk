9b2260923fd301057d6fb93d52156e9e7c17e49b
pho <pho@pkgsrc.org>
2023-10-26 05:13:37 +0000

    devel/hs-safe-exceptions: Update to 0.1.7.4
    0.1.7.4
        Add HasCallStack when exceptions >= 0.10.6 (#41)

--- devel/hs-safe-exceptions/buildlink3.mk
+++ devel/hs-safe-exceptions/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/26 05:13:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-safe-exceptions
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-safe-exceptions
 HS_SAFE_EXCEPTIONS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-safe-exceptions+=	hs-safe-exceptions>=0.1.7
-BUILDLINK_ABI_DEPENDS.hs-safe-exceptions+=	hs-safe-exceptions>=0.1.7.3nb2
+BUILDLINK_ABI_DEPENDS.hs-safe-exceptions+=	hs-safe-exceptions>=0.1.7.4
 BUILDLINK_PKGSRCDIR.hs-safe-exceptions?=	../../devel/hs-safe-exceptions
 .endif	# HS_SAFE_EXCEPTIONS_BUILDLINK3_MK
 
