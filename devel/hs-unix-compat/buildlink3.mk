401c4d696fd587d83aa5f837fe42376c931770bc
pho <pho@pkgsrc.org>
2023-10-26 12:06:39 +0000

    devel/hs-unix-compat: Update to 0.7
    Version 0.7 (2023-03-15)
    * Remote System.PosixCompat.User module
      (sic, probably they meant remove?)

--- devel/hs-unix-compat/buildlink3.mk
+++ devel/hs-unix-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/26 12:06:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-compat
 
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.6
-BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.6nb2
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7
+BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../devel/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
