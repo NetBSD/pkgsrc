bcd08b5c179896d0ec97b36af431edc6c8d9397b
pho <pho@pkgsrc.org>
2023-10-26 04:43:55 +0000

    devel/hs-hscolour: Fix build with GHC 9.6

--- textproc/hs-hscolour/buildlink3.mk
+++ textproc/hs-hscolour/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 04:43:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-hscolour
 
 .if !defined(HS_HSCOLOUR_BUILDLINK3_MK)
 HS_HSCOLOUR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.24.4
-BUILDLINK_ABI_DEPENDS.hs-hscolour+=	hs-hscolour>=1.24.4nb6
+BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.25
+BUILDLINK_ABI_DEPENDS.hs-hscolour+=	hs-hscolour>=1.25
 BUILDLINK_PKGSRCDIR.hs-hscolour?=	../../textproc/hs-hscolour
 .endif	# HS_HSCOLOUR_BUILDLINK3_MK
 
