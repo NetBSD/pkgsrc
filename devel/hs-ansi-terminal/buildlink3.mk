a9332064cf2db028bcae832ca95c725efcaa3447
pho <pho@pkgsrc.org>
2023-10-26 07:31:18 +0000

    devel/hs-ansi-terminal: Update to 1.0
    Version 1.0
    * On Windows, drop support for legacy Windows requiring emulation.
    * On Windows, the package no longer depends (directly or indirectly) on the
      Win32, array, containers, deepseq, filepath, ghc-boot-th, mintty, pretty
      or template-haskell packages.
    * hSupportsANSI no longer assumes that the given handle is writeable.
    * hSupportsANSIWithoutEmulation is deprecated.
    
    Version 0.11.5
    * Module System.Console.ANSI.Types spun out to new dependency package
      ansi-terminal-types-0.11.5.
    * Drop support for GHC versions before GHC 7.10.1 (released March 2015).
    * Improvements to Haddock documentation.

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/10/09 04:54:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2023/10/26 07:31:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.3
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.11.4nb2
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.0
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.0
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
 
+.include "../../devel/hs-ansi-terminal-types/buildlink3.mk"
 .include "../../devel/hs-colour/buildlink3.mk"
 .endif	# HS_ANSI_TERMINAL_BUILDLINK3_MK
 
