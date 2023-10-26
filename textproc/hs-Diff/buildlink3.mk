c2b7a53242d3cb091dc8ed14d6f60b3862547e31
pho <pho@pkgsrc.org>
2023-10-26 06:53:51 +0000

    textproc/hs-Diff: Update to 0.5
    No change log is provided by the upstream.

--- textproc/hs-Diff/buildlink3.mk
+++ textproc/hs-Diff/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/26 06:53:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-Diff
 
 .if !defined(HS_DIFF_BUILDLINK3_MK)
 HS_DIFF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-Diff+=	hs-Diff>=0.4.1nb4
+BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.5
+BUILDLINK_ABI_DEPENDS.hs-Diff+=	hs-Diff>=0.5
 BUILDLINK_PKGSRCDIR.hs-Diff?=	../../textproc/hs-Diff
 .endif	# HS_DIFF_BUILDLINK3_MK
 
