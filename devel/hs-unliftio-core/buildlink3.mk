5e9aa13bd5cdb2c6aa0c38791ab5d51bf9c41ed1
pho <pho@pkgsrc.org>
2023-10-26 05:11:20 +0000

    devel/hs-unliftio-core: Update to 0.2.1.0
    0.2.1.0
        Added Control.Monad.IO.Unlift.liftIOOp
    
    0.2.0.2
        Widen base upperbound to < 4.17 to support ghc-9.2.

--- devel/hs-unliftio-core/buildlink3.mk
+++ devel/hs-unliftio-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 05:11:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-unliftio-core
 
 .if !defined(HS_UNLIFTIO_CORE_BUILDLINK3_MK)
 HS_UNLIFTIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.2.0.1
-BUILDLINK_ABI_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.2.0.1nb6
+BUILDLINK_API_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.2.1.0
 BUILDLINK_PKGSRCDIR.hs-unliftio-core?=		../../devel/hs-unliftio-core
 .endif	# HS_UNLIFTIO_CORE_BUILDLINK3_MK
 
