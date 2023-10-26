633f260097825e2d8813c6ffcba8b0deb14e6fdb
pho <pho@pkgsrc.org>
2023-10-26 12:46:27 +0000

    devel/hs-fgl: Update to 5.8.2.0
    5.8.2.0
    -------
    
    * Data.Graph.Inductive.Graph now only requires Graph, not DynGraph
      (issue #100).
    
    * Documented that some functions are partial (issue #98).
    
    * Add `insert` function as synonym for `&` (issue #90).
    
    5.8.1.1
    -------
    
    * Data.Graph.Inductive.Query.Dominators.{dom,iDom} could fail for some
      graphs (issue #109, regression in 5.8.1.0).
    
    5.8.1.0
    -------
    
    * Data.Graph.Inductive.PatriciaTree.Gr and
      Data.Graph.Inductive.Tree.Gr now have Functor instances.
    
    * 'Gr a' is now an instance of Functor.

--- devel/hs-fgl/buildlink3.mk
+++ devel/hs-fgl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 12:46:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-fgl
 
 .if !defined(HS_FGL_BUILDLINK3_MK)
 HS_FGL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.8.0
-BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.8.0.0nb2
+BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.8.2
+BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.8.2.0
 BUILDLINK_PKGSRCDIR.hs-fgl?=	../../devel/hs-fgl
 .endif	# HS_FGL_BUILDLINK3_MK
 
