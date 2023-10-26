5ee3344f4289872bfbced3f447ed460ee5e7e764
pho <pho@pkgsrc.org>
2023-10-26 16:34:49 +0000

    textproc/hs-jira-wiki-markup: Update to 1.5.1
    1.5.1 - Released 2023-03-13.
    * Parser: Disallow image paths that start with a space.
    
    1.5.0 - Released 2023-01-11.
    * Relaxed upper bounds for mtl, allow mtl 2.3.
    * Fixed handling of character sequences that would be interpreted as icons
      if they weren't followed by an alphanumeric character. See also pandoc
      issue #8511.

--- textproc/hs-jira-wiki-markup/buildlink3.mk
+++ textproc/hs-jira-wiki-markup/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:52 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 16:34:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-jira-wiki-markup
 
 .if !defined(HS_JIRA_WIKI_MARKUP_BUILDLINK3_MK)
 HS_JIRA_WIKI_MARKUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.4.0
-BUILDLINK_ABI_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.4.0nb4
+BUILDLINK_API_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.5.1
+BUILDLINK_ABI_DEPENDS.hs-jira-wiki-markup+=	hs-jira-wiki-markup>=1.5.1
 BUILDLINK_PKGSRCDIR.hs-jira-wiki-markup?=	../../textproc/hs-jira-wiki-markup
 .endif	# HS_JIRA_WIKI_MARKUP_BUILDLINK3_MK
 
