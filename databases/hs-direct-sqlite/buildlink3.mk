4f9f9864373a501b06f9f686610762e2f4fb3cc3
pho <pho@pkgsrc.org>
2023-10-26 13:15:58 +0000

    databases/hs-direct-sqlite: Update to 2.3.28
    v2.3.28:
    * Upgrade embedded sqlite library to 3.41.0.

--- databases/hs-direct-sqlite/buildlink3.mk
+++ databases/hs-direct-sqlite/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/26 13:15:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-direct-sqlite
 
 .if !defined(HS_DIRECT_SQLITE_BUILDLINK3_MK)
 HS_DIRECT_SQLITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.27
-BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.27nb3
+BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.28
+BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.28
 BUILDLINK_PKGSRCDIR.hs-direct-sqlite?=		../../databases/hs-direct-sqlite
 
 .include "../../databases/sqlite3/buildlink3.mk"
