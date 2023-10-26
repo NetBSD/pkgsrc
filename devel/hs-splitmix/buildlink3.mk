d2383fe8c37133c2d12801e50fe92fe60fbf0a48
pho <pho@pkgsrc.org>
2023-10-26 03:38:26 +0000

    devel/hs-splitmix: Update to 0.1.0.5
    No change log is provided by the upstream.

--- devel/hs-splitmix/buildlink3.mk
+++ devel/hs-splitmix/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 03:38:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-splitmix
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-splitmix
 HS_SPLITMIX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0.4nb4
+BUILDLINK_ABI_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0.5
 BUILDLINK_PKGSRCDIR.hs-splitmix?=	../../devel/hs-splitmix
 .endif	# HS_SPLITMIX_BUILDLINK3_MK
 
