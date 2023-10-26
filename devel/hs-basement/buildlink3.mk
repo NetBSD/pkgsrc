fba131f1e044a516b18bb8b62a6a01aa0b6e5906
pho <pho@pkgsrc.org>
2023-10-26 02:53:48 +0000

    devel/hs-basement: Update to 0.0.16
    No change log is provided by the upstream.

--- devel/hs-basement/buildlink3.mk
+++ devel/hs-basement/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 02:53:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-basement
 
 .if !defined(HS_BASEMENT_BUILDLINK3_MK)
 HS_BASEMENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-basement+=	hs-basement>=0.0.15
-BUILDLINK_ABI_DEPENDS.hs-basement+=	hs-basement>=0.0.15nb2
+BUILDLINK_API_DEPENDS.hs-basement+=	hs-basement>=0.0.16
+BUILDLINK_ABI_DEPENDS.hs-basement+=	hs-basement>=0.0.16
 BUILDLINK_PKGSRCDIR.hs-basement?=	../../devel/hs-basement
 .endif	# HS_BASEMENT_BUILDLINK3_MK
 
