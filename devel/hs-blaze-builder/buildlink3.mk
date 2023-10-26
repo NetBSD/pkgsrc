96f84b34abafda7fae32903027149597788dd380
pho <pho@pkgsrc.org>
2023-10-26 13:09:34 +0000

    devel/hs-blaze-builder: 0.4.2.3
    * 0.4.2.3 2023-08-27
      - Fix compilation warnings concerning non-canonical mappend
      - Support bytestring-0.12
      - Support text-2.1
      - Tested with GHC 7.0.4 to 9.8.1 alpha3

--- devel/hs-blaze-builder/buildlink3.mk
+++ devel/hs-blaze-builder/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2023/10/09 04:54:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2023/10/26 13:09:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-builder
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-blaze-builder
 HS_BLAZE_BUILDER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.2.2nb4
+BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.2.3
 BUILDLINK_PKGSRCDIR.hs-blaze-builder?=		../../devel/hs-blaze-builder
 .endif	# HS_BLAZE_BUILDER_BUILDLINK3_MK
 
