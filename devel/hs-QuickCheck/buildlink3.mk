bf46826930948931409d630b87c17d5262894f91
pho <pho@pkgsrc.org>
2023-10-27 07:26:45 +0000

    devel/hs-QuickCheck: Update to 2.14.3
    QuickCheck 2.14.3 (released 2023-05-31)
    * Add shrinkBoundedEnum (thanks to Jonathan Knowles)
    * Add discardAfter for discarding tests on timeout (thanks to Justus Sagemüller)
    * Add assertWith for monadic testing (thanks to KtorZ)
    * Add functionElements to Test.QuickCheck.Function (thanks to Oleg Grenrus)
    * Add Arbitrary instance for Newline (thanks to Daniel Bramucci)
    * Improve Arbitrary instances for Float and Double (thanks to Oleg Grenrus)
    * Improve arbitrarySizedFractional (thanks to Bodigrim)
    * Fix shrinkRealFrac and shrinkDecimal, which were broken
    * Speed up printing of progress messages (thanks to Bodigrim)
    * Add COMPLETE pragmas for Fn and family (thanks to ilkecan)
    * Make templateHaskell flag manual (thanks to Oleg Grenrus)

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/10/09 04:54:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/10/27 07:26:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.2
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.2nb6
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.3
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.3
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
