567c7d75b13f2de2f18bfa0b127b3aa2f3b12392
pho <pho@pkgsrc.org>
2023-10-27 12:22:24 +0000

    devel/hs-th-lift: Update to 0.8.4
    [0.8.4] - 2023.08.01
    * Add support for the FldName NameSpace, which was introduced in
      template-haskell-2.21.0.0 (GHC 9.8).
    
    [0.8.3] - 2023.02.27
    * Support building with th-abstraction-0.5.*.
    * Require th-abstraction-0.3.0.0 or later.

--- devel/hs-th-lift/buildlink3.mk
+++ devel/hs-th-lift/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/27 12:22:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-lift
 
 .if !defined(HS_TH_LIFT_BUILDLINK3_MK)
 HS_TH_LIFT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.2
-BUILDLINK_ABI_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.2nb6
+BUILDLINK_API_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.4
+BUILDLINK_ABI_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.4
 BUILDLINK_PKGSRCDIR.hs-th-lift?=	../../devel/hs-th-lift
 
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
