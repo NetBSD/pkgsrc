b2ccb4fda4e161c494582ec58d65f148f3246b57
pho <pho@pkgsrc.org>
2023-10-27 11:58:38 +0000

    math/hs-digits: import hs-digits-0.3.1
    Converts integers to lists of digits and back.

---
+++ math/hs-digits/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/27 11:58:38 pho Exp $
+
+BUILDLINK_TREE+=	hs-digits
+
+.if !defined(HS_DIGITS_BUILDLINK3_MK)
+HS_DIGITS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-digits+=	hs-digits>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-digits+=	hs-digits>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-digits?=		../../math/hs-digits
+
+.include "../../devel/hs-QuickCheck/buildlink3.mk"
+.endif	# HS_DIGITS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-digits
