1b83a90c47749bb82d1f8f48fcf201fc637c64fc
pho <pho@pkgsrc.org>
2023-10-27 02:08:49 +0000

    net/hs-network: Update to 3.1.4.0
    Version 3.1.4.0
        Install and use afunix_compat.h header. #556
        Supporting SO_SNDTIMEO and SO_RCVTIMEO. #555
        Emulating socketPair on Windows. #554
    
    Version 3.1.3.0
        Supporting AF_UNIX on Windows #553
    
    Version 3.1.2.9
        Resolving the runtime linker problem on Windows. #552
    
    Version 3.1.2.8
        Ignoring error from shutdown in gracefulClose
        Fix bitsize of some msghdr and cmsghdr fields on Linux #535
        Add SO_ACCEPTCONN SocketOption #524

--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.20 2023/10/09 04:54:42 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2023/10/27 02:08:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-network
 
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.1.2
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.1.2.7nb4
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.1.4
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.1.4.0
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 .endif	# HS_NETWORK_BUILDLINK3_MK
 
