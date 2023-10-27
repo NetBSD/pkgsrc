39d21b33fef343a48e33c52bcc62f75496359198
pho <pho@pkgsrc.org>
2023-10-27 12:53:00 +0000

    devel/hs-microlens-th: Update to 0.4.3.14
    0.4.3.14
        #170, #171 Support GHC 9.8.
    
    0.4.3.13
        #167 Support th-abstraction 0.5.
    
    0.4.3.12
        #164, #165 Support template-haskell 2.19 (and GHC 9.6 by extension).

--- devel/hs-microlens-th/buildlink3.mk
+++ devel/hs-microlens-th/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/27 12:53:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-microlens-th
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-microlens-th
 HS_MICROLENS_TH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3
-BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.11nb2
+BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.14
 BUILDLINK_PKGSRCDIR.hs-microlens-th?=	../../devel/hs-microlens-th
 
 .include "../../devel/hs-microlens/buildlink3.mk"
