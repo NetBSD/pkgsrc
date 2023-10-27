c144cd0d6ff64af7227b6096971010079a98144f
pho <pho@pkgsrc.org>
2023-10-27 14:00:55 +0000

    devel/hs-generic-deriving: Update to 1.14.5
    1.14.5 [2023.08.06]
    * Support building with template-haskell-2.21.* (GHC 9.8).
    * The Template Haskell machinery now uses TemplateHaskellQuotes when
      building with GHC 8.0+ instead of manually constructing each Template
      Haskell Name. A consequence of this is that generic-deriving will now
      build with GHC 9.8, as TemplateHaskellQuotes abstracts over some internal
      Template Haskell changes introduced in 9.8.
    
    1.14.4 [2023.04.30]
    * Allow building with GHC backends where HTYPE_SIG_ATOMIC_T is not defined,
      such as the WASM backend.
    * Place INLINE [1] pragmas on from and to implementations when types don't
      have too many constructors or fields, following the heuristics that GHC
      9.2+ uses for Generic deriving.
    
    1.14.3 [2023.02.27]
    * Support th-abstraction-0.5.*.

--- devel/hs-generic-deriving/buildlink3.mk
+++ devel/hs-generic-deriving/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/27 14:00:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-generic-deriving
 
 .if !defined(HS_GENERIC_DERIVING_BUILDLINK3_MK)
 HS_GENERIC_DERIVING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-generic-deriving+=	hs-generic-deriving>=1.14.1
-BUILDLINK_ABI_DEPENDS.hs-generic-deriving+=	hs-generic-deriving>=1.14.2nb2
+BUILDLINK_API_DEPENDS.hs-generic-deriving+=	hs-generic-deriving>=1.14.5
+BUILDLINK_ABI_DEPENDS.hs-generic-deriving+=	hs-generic-deriving>=1.14.5
 BUILDLINK_PKGSRCDIR.hs-generic-deriving?=	../../devel/hs-generic-deriving
 
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
