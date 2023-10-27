84d3be1303b21813e8e805625b21d15480b82a4c
pho <pho@pkgsrc.org>
2023-10-27 11:53:14 +0000

    net/hs-resolv: Update to 0.2.0.2
    0.2.0.2 - 2023-06-12, Alexey Radkov and Andreas Abel
    * Support Haiku OS by including libnetwork in configure script. (PR #23.)
    
    0.2.0.1 - 2023-03-31, Alexey Radkov and Andreas Abel
    * Fix 0.2.0.0: Ship updated configure script.
    
    0.2.0.0 - 2023-03-31, Alexey Radkov and Andreas Abel
    * Bump bytestring to >= 0.10 for correct IsString ByteString instance. (PR
      #16.)
    * Fix memory leaks due to missing res_nclose() after each res_ninit()
      call. (PR #12.)
    * Check the value of h_errno on failures of res_nquery() and throw an
      appropriate exception of type DnsException built with one of new
      constructors DnsHostNotFound, DnsNoData, DnsNoRecovery, or
      DnsTryAgain. Note that previously such exceptions were thrown by fail and
      had type IOError. (PR #17.)
    * Suppress configure warning on option --with-compiler passed by Cabal. (PR
      #21.)
    * Tested with GHC 8.0 - 9.6.

--- net/hs-resolv/buildlink3.mk
+++ net/hs-resolv/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/27 11:53:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-resolv
 
 .if !defined(HS_RESOLV_BUILDLINK3_MK)
 HS_RESOLV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resolv+=	hs-resolv>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-resolv+=	hs-resolv>=0.1.2.0nb6
+BUILDLINK_API_DEPENDS.hs-resolv+=	hs-resolv>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-resolv+=	hs-resolv>=0.2.0.2
 BUILDLINK_PKGSRCDIR.hs-resolv?=		../../net/hs-resolv
 
 .include "../../converters/hs-base16-bytestring/buildlink3.mk"
