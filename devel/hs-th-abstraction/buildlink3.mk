656fce63cc80c5a7ceca5693bb74c754349a6622
pho <pho@pkgsrc.org>
2023-10-27 06:20:28 +0000

    devel/hs-th-abstraction: Update to 0.6.0.0
    The release note is too long to paste here. See:
    https://hackage.haskell.org/package/th-abstraction-0.6.0.0/changelog

--- devel/hs-th-abstraction/buildlink3.mk
+++ devel/hs-th-abstraction/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/27 06:20:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-abstraction
 
 .if !defined(HS_TH_ABSTRACTION_BUILDLINK3_MK)
 HS_TH_ABSTRACTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.5
-BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.4.5.0nb2
+BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.6.0
+BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.6.0.0
 BUILDLINK_PKGSRCDIR.hs-th-abstraction?=		../../devel/hs-th-abstraction
 .endif	# HS_TH_ABSTRACTION_BUILDLINK3_MK
 
