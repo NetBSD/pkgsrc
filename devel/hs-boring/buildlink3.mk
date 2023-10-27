e102c44cdfa3c41b1f1e76e6dacf8bdb5f24fb37
pho <pho@pkgsrc.org>
2023-10-27 15:09:44 +0000

    devel/hs-boring: Update to 0.2.1
    0.2.1
    * Add instances for SNat, SSymbol, and SChar (singletons introduced in
      base-4.18.0.0)

--- devel/hs-boring/buildlink3.mk
+++ devel/hs-boring/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/27 15:09:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-boring
 
 .if !defined(HS_BORING_BUILDLINK3_MK)
 HS_BORING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-boring+=	hs-boring>=0.2
-BUILDLINK_ABI_DEPENDS.hs-boring+=	hs-boring>=0.2nb2
+BUILDLINK_API_DEPENDS.hs-boring+=	hs-boring>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-boring+=	hs-boring>=0.2.1
 BUILDLINK_PKGSRCDIR.hs-boring?=		../../devel/hs-boring
 
 .include "../../devel/hs-tagged/buildlink3.mk"
