9286004c603a6da59dd36380e42eb2bb603635f8
pho <pho@pkgsrc.org>
2023-10-27 04:44:02 +0000

    devel/hs-cmdargs: Update to 0.10.22
    0.10.22, released 2023-03-13
    * #68, support GHC 9.6

--- devel/hs-cmdargs/buildlink3.mk
+++ devel/hs-cmdargs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/10/09 04:54:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2023/10/27 04:44:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-cmdargs
 
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.20
-BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.21nb6
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.22
+BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.22
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../devel/hs-cmdargs
 .endif	# HS_CMDARGS_BUILDLINK3_MK
 
