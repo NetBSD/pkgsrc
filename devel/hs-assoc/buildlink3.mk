5b336ffe83eaef4e6549ab21719ed604178668e9
pho <pho@pkgsrc.org>
2023-10-27 15:15:24 +0000

    devel/hs-assoc: Update to 1.1
    1.1
    * Depend on bifunctor-classes-compat only. Instances for types defined in
      bifunctors package are moved there. With this change assoc only depends
      on base and tagged.
    * Mark modules as explicitly Safe

--- devel/hs-assoc/buildlink3.mk
+++ devel/hs-assoc/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/27 15:15:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-assoc
 
 .if !defined(HS_ASSOC_BUILDLINK3_MK)
 HS_ASSOC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-assoc+=	hs-assoc>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-assoc+=	hs-assoc>=1.0.2nb6
+BUILDLINK_API_DEPENDS.hs-assoc+=	hs-assoc>=1.1
+BUILDLINK_ABI_DEPENDS.hs-assoc+=	hs-assoc>=1.1
 BUILDLINK_PKGSRCDIR.hs-assoc?=		../../devel/hs-assoc
 
-.include "../../math/hs-bifunctors/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .endif	# HS_ASSOC_BUILDLINK3_MK
 
