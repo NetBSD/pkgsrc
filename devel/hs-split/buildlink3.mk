320f50b7271960063a5ee30f7e0beb211ebfb6ca
pho <pho@pkgsrc.org>
2023-10-27 02:21:05 +0000

    devel/hs-split: Update to 0.2.4
    * 0.2.4 (9 October 2023)
      - Test with GHC 9.6.
      - Change examples to doctests (+ add a few more), and check them as part of CI.
      - Fix up a bunch of style hints.

--- devel/hs-split/buildlink3.mk
+++ devel/hs-split/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/10/09 04:54:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/10/27 02:21:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.3
-BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.3.5nb2
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.4
+BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.4
 BUILDLINK_PKGSRCDIR.hs-split?=		../../devel/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
