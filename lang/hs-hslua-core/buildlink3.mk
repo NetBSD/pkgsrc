85fb7147851419905a979a69a3a526e4a21166db
pho <pho@pkgsrc.org>
2023-10-27 08:31:06 +0000

    lang/hs-hslua-core: Update to 2.3.1
    hslua-core-2.3.1 - Released 2023-03-17.
    * New module HsLua.Core.Debug: the module provides bindings to a subset of
      functions of the Lua debug interface. Currently the module only exports
      getupvalue and setupvalue, both of which are also re-exported from
      HsLua.Core.
    
    hslua-core-2.3.0 - Released 2023-03-13.
    * The functions loadfile, dofile, and dofileTrace now expect the argument
      to be of type Maybe FilePath. The functions load from stdin when the
      argument is Nothing.
    * Added setwarnf' for simple warning messgae handling: The built-in method
      of setting a warn function is flexible but not straight-forward to
      use. The new setwarnf' function allows to set a Haskell action as a
      warning hook: the default warning behavior is kept in place, but, in
      addition to the default action, the hook is called on the concatenated
      warning messages. This can be used to plug Lua warnings into an
      application specific Haskell reporting system.
    * Export GCManagedState, newGCManagedState, closeGCManagedState, and
      withGCManagedState from HsLua.Core.

--- lang/hs-hslua-core/buildlink3.mk
+++ lang/hs-hslua-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/27 08:31:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-core
 
 .if !defined(HS_HSLUA_CORE_BUILDLINK3_MK)
 HS_HSLUA_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.3.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.3.1
 BUILDLINK_PKGSRCDIR.hs-hslua-core?=	../../lang/hs-hslua-core
 
 .include "../../lang/hs-lua/buildlink3.mk"
