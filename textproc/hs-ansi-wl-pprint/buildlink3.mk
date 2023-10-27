1b4a58dc8d9c4eb3132cb2c34ffeb2af79ba26fd
pho <pho@pkgsrc.org>
2023-10-27 13:20:48 +0000

    textproc/hs-ansi-wl-pprint: Update to 1.0.2
    1.0.2
    
    * Change the implementation to the prettyprinter package, using the
      prettyprinter-compat-ansi-wl-pprint compatibility package.
    
      Migrate to prettyprinter.
    
      Note: Using show won't preserve formatting anymore, as prettyprinters
      Show Doc instance is annotation invariant. You can convert to String
      using Data.Text.Lazy.unpack . Prettyprinter.Render.Terminal.renderLazy
      . Prettyprinter.layoutPretty Prettyprinter.defaultLayoutOptions, from the
      text, prettyprinter-ansi-terminal, prettyprinter packages respectively.
    
    * The Text.PrettyPrint.ANSI.Leijen.Internal module is gone.

--- textproc/hs-ansi-wl-pprint/buildlink3.mk
+++ textproc/hs-ansi-wl-pprint/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/27 13:20:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-wl-pprint
 
 .if !defined(HS_ANSI_WL_PPRINT_BUILDLINK3_MK)
 HS_ANSI_WL_PPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.9
-BUILDLINK_ABI_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.9nb7
+BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=1.0.2
 BUILDLINK_PKGSRCDIR.hs-ansi-wl-pprint?=		../../textproc/hs-ansi-wl-pprint
 
-.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter-compat-ansi-wl-pprint/buildlink3.mk"
 .endif	# HS_ANSI_WL_PPRINT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-ansi-wl-pprint
