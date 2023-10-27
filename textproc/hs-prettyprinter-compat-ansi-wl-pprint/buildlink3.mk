15153563430823ea2a4f33c7dd0139087cef2530
pho <pho@pkgsrc.org>
2023-10-27 13:15:59 +0000

    textproc/hs-prettyprinter-compat-ansi-wl-pprint: import hs-prettyprinter-compat-ansi-wl-pprint-1.0.2
    This package defines a compatibility layer between the old ansi-wl-pprint
    package, and the new prettyprinter/prettyprinter-ansi-terminal ones.
    
    This allows easily transitioning dependent packages from the old to the new
    package, by simply replacing ansi-wl-pprint with
    prettyprinter-ansi-terminal in the .cabal file. For adapting third party
    plugins that output ansi-wl-pprint data, use the proper converter from the
    prettyprinter-convert-ansi-wl-pprint module.
    
    Note that this package is only for transitional purposes, and therefore
    deprecated and wholly undocumented. For new development, use the current
    version of prettyprinter, and the ANSI terminal backend provided in
    prettyprinter-ansi-terminal.

---
+++ textproc/hs-prettyprinter-compat-ansi-wl-pprint/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/27 13:15:59 pho Exp $
+
+BUILDLINK_TREE+=	hs-prettyprinter-compat-ansi-wl-pprint
+
+.if !defined(HS_PRETTYPRINTER_COMPAT_ANSI_WL_PPRINT_BUILDLINK3_MK)
+HS_PRETTYPRINTER_COMPAT_ANSI_WL_PPRINT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-prettyprinter-compat-ansi-wl-pprint+=	hs-prettyprinter-compat-ansi-wl-pprint>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-prettyprinter-compat-ansi-wl-pprint+=	hs-prettyprinter-compat-ansi-wl-pprint>=1.0.2
+BUILDLINK_PKGSRCDIR.hs-prettyprinter-compat-ansi-wl-pprint?=	../../textproc/hs-prettyprinter-compat-ansi-wl-pprint
+
+.include "../../textproc/hs-prettyprinter/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter-ansi-terminal/buildlink3.mk"
+.endif	# HS_PRETTYPRINTER_COMPAT_ANSI_WL_PPRINT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-prettyprinter-compat-ansi-wl-pprint
