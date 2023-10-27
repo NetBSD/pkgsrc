de9ee18b9142e5833e316f7dcc743f8a34ca02cf
pho <pho@pkgsrc.org>
2023-10-27 19:16:49 +0000

    textproc/hs-regex-tdfa: Update to 1.3.2.2
    1.3.2.2 - 2023-08-02, Andreas Abel
    * Fix return type in memcpy FFI signature (#52)
    * Refactor regexec to avoid partial functions tail and (!0)
    * Tested with GHC 7.4 - 9.8.1-alpha1
    
    1.3.2.1 - 2023-05-19, Andreas Abel
    * Fix haddock rendering of code examples in top-level documentation (#50)
    * Tested with GHC 7.4 - 9.6

--- textproc/hs-regex-tdfa/buildlink3.mk
+++ textproc/hs-regex-tdfa/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/27 19:16:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-tdfa
 
 .if !defined(HS_REGEX_TDFA_BUILDLINK3_MK)
 HS_REGEX_TDFA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-tdfa+=	hs-regex-tdfa>=1.3.1
-BUILDLINK_ABI_DEPENDS.hs-regex-tdfa+=	hs-regex-tdfa>=1.3.2nb2
+BUILDLINK_API_DEPENDS.hs-regex-tdfa+=	hs-regex-tdfa>=1.3.2
+BUILDLINK_ABI_DEPENDS.hs-regex-tdfa+=	hs-regex-tdfa>=1.3.2.2
 BUILDLINK_PKGSRCDIR.hs-regex-tdfa?=	../../textproc/hs-regex-tdfa
 
 .include "../../textproc/hs-regex-base/buildlink3.mk"
