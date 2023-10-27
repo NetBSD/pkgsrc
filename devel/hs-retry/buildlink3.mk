f355f4e6b23be59d593f1c0c282c7a2729db46e4
pho <pho@pkgsrc.org>
2023-10-27 12:29:19 +0000

    devel/hs-retry: Update to 0.9.3.1
    0.9.3.1
        Resolve warnings in test suite PR 83

--- devel/hs-retry/buildlink3.mk
+++ devel/hs-retry/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/27 12:29:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-retry
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-retry
 HS_RETRY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-retry+=	hs-retry>=0.9.3
-BUILDLINK_ABI_DEPENDS.hs-retry+=	hs-retry>=0.9.3.0nb2
+BUILDLINK_ABI_DEPENDS.hs-retry+=	hs-retry>=0.9.3.1
 BUILDLINK_PKGSRCDIR.hs-retry?=		../../devel/hs-retry
 
 .include "../../devel/hs-mtl-compat/buildlink3.mk"
