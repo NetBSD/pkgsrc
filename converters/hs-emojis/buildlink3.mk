8c8b3c02bb008a391e3a197e007e09b2f943c703
pho <pho@pkgsrc.org>
2023-10-27 04:39:30 +0000

    converters/hs-emojis: Update to 0.1.3
    0.1.3
        Update emoji data to Unicode Emoji 15.0 and aliases from gemoji.

--- converters/hs-emojis/buildlink3.mk
+++ converters/hs-emojis/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/27 04:39:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-emojis
 
 .if !defined(HS_EMOJIS_BUILDLINK3_MK)
 HS_EMOJIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1.2nb4
+BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-emojis?=		../../converters/hs-emojis
 .endif	# HS_EMOJIS_BUILDLINK3_MK
 
