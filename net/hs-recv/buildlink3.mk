0d6579456328cad5295b1226d8f1a2f9535c0e62
pho <pho@pkgsrc.org>
2023-10-27 11:03:52 +0000

    net/hs-recv: Update to 0.1.0
    No change log is provided by the upstream.

--- net/hs-recv/buildlink3.mk
+++ net/hs-recv/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/27 11:03:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-recv
 
 .if !defined(HS_RECV_BUILDLINK3_MK)
 HS_RECV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-recv+=	hs-recv>=0.0.0
-BUILDLINK_ABI_DEPENDS.hs-recv+=	hs-recv>=0.0.0nb2
+BUILDLINK_API_DEPENDS.hs-recv+=	hs-recv>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-recv+=	hs-recv>=0.1.0
 BUILDLINK_PKGSRCDIR.hs-recv?=	../../net/hs-recv
 
 .include "../../net/hs-network/buildlink3.mk"
