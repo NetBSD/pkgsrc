45ca00007886d7bb105063e6fb0fcd530b5a3922
pho <pho@pkgsrc.org>
2023-10-27 10:58:52 +0000

    security/hs-crypton: import hs-crypton-0.33
    A repository of cryptographic primitives.
    
    * Symmetric ciphers: AES, DES, 3DES, CAST5, Blowfish, Twofish, Camellia,
      RC4, Salsa, XSalsa, ChaCha.
    
    * Hash: SHA1, SHA2, SHA3, SHAKE, MD2, MD4, MD5, Keccak, Skein, Ripemd,
      Tiger, Whirlpool, Blake2
    
    * MAC: HMAC, KMAC, Poly1305
    
    * Asymmetric crypto: DSA, RSA, DH, ECDH, ECDSA, ECC, Curve25519, Curve448,
      Ed25519, Ed448
    
    * Key Derivation Function: PBKDF2, Scrypt, HKDF, Argon2, BCrypt,
      BCryptPBKDF
    
    * Cryptographic Random generation: System Entropy, Deterministic Random
      Generator
    
    * Data related: Anti-Forensic Information Splitter (AFIS)
    
    If anything cryptographic related is missing from here, submit a pull
    request to have it added. This package strives to be a cryptographic
    kitchen sink that provides cryptography for everyone.

---
+++ security/hs-crypton/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/27 10:58:52 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypton
+
+.if !defined(HS_CRYPTON_BUILDLINK3_MK)
+HS_CRYPTON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypton+=	hs-crypton>=0.33
+BUILDLINK_ABI_DEPENDS.hs-crypton+=	hs-crypton>=0.33
+BUILDLINK_PKGSRCDIR.hs-crypton?=	../../security/hs-crypton
+
+.include "../../devel/hs-basement/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.endif	# HS_CRYPTON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypton
