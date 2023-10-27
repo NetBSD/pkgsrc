03d60ec0a35453a5c4e0c287bbe7aaa46d14cb02
pho <pho@pkgsrc.org>
2023-10-27 04:57:03 +0000

    devel/hs-base-compat: Update to 0.13.1
    Changes in 0.13.1 [2023.10.11]
    * Sync with base-4.19/GHC 9.8
    * Backport unzip to Data.Functor.Compat
    * Backport (!?) and unsnoc to Data.List.Compat
    * Backport getSolo to Data.Tuple.Compat when building against
      ghc-prim-0.8.0 (GHC 9.2) or later. To backport getSolo to older versions
      of GHC, import Data.Tuple.Compat from base-compat-batteries instead.
    * Backport decT and hdecT to Data.Typeable.Compat
    * Backport decTypeRep to Type.Reflection.Compat
    
    Changes in 0.13.0 [2023.03.10]
    * Sync with base-4.18/GHC 9.6
    * Backport liftA2 being re-exported from Prelude.Compat.
    * Data.Tuple.Compat's Solo API now matches what is present in Data.Tuple in
      base-4.18. In particular, we now re-export both the MkSolo and Solo data
      constructors when building with ghc-prim-0.10.0 or later, with MkSolo
      being preferred over Solo. If you want to backport MkSolo to earlier
      versions of GHC, import Data.Tuple.Compat from base-compat-batteries
      instead.
    * Backport traceWith, traceShowWith, and traceEventWith to
      Debug.Trace. Note that traceEventWith is only provided when building with
      base-4.5 or later, as that is the first version of base to provide the
      traceEvent primitive on which traceEventWith is defined.
    * Backport inits1 and tails1 to Data.List.NonEmpty.Compat.
    * Backport minusNaturalMaybe to Numeric.Natural.Compat.
    * Backport applyWhen to Data.Function.Compat.
    * Backport mapAccumM and forAccumM to Data.Traversable.Compat.
    * Backport heqT to Data.Typeable.Compat. Note that heqT is only defined
      when building with base-4.10 or later, as that is the first version of
      base to provide the primitives needed to define heqT.
    * Introduce Data.Foldable1.Compat and Data.Bifoldable1.Compat modules,
      which correspond to changes made in base-4.18.0.0. You may consider using
      base-compat-batteries instead if you want increase the range of base
      versions that are supported.
    
    Changes in 0.12.3 [2023.07.12]
    * This coincides with the base-compat-batteries-0.12.3 release. Refer to
      the base-compat-batteries changelog for more details.

--- devel/hs-base-compat/buildlink3.mk
+++ devel/hs-base-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/27 04:57:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat
 
 .if !defined(HS_BASE_COMPAT_BUILDLINK3_MK)
 HS_BASE_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.2
-BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.12.2nb2
+BUILDLINK_API_DEPENDS.hs-base-compat+=	hs-base-compat>=0.13.1
+BUILDLINK_ABI_DEPENDS.hs-base-compat+=	hs-base-compat>=0.13.1
 BUILDLINK_PKGSRCDIR.hs-base-compat?=	../../devel/hs-base-compat
 .endif	# HS_BASE_COMPAT_BUILDLINK3_MK
 
