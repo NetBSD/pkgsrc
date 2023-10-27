77c9d4235ec0540c52a8e4d7b6ccb3ef4911c4cd
pho <pho@pkgsrc.org>
2023-10-27 12:34:40 +0000

    sysutils/hs-fast-logger: Update to 3.2.2
    3.2.2
    * Corrected handling of messages at the buffer boundary in the SingleLogger
      #211
    
    3.2.1
    * Fixing a bug where a single logger is not killed
    
    3.2.0
    * newFastLogger1 ensures the ordering of logs #207
    
    3.1.2
    * Require unix-compat >= 0.2 #206
    * Remove Safe if directory >= 1.3.8 #199

--- sysutils/hs-fast-logger/buildlink3.mk
+++ sysutils/hs-fast-logger/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/27 12:34:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-fast-logger
 
 .if !defined(HS_FAST_LOGGER_BUILDLINK3_MK)
 HS_FAST_LOGGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.1.1
-BUILDLINK_ABI_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.1.1nb4
+BUILDLINK_API_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.2.2
+BUILDLINK_ABI_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.2.2
 BUILDLINK_PKGSRCDIR.hs-fast-logger?=	../../sysutils/hs-fast-logger
 
 .include "../../devel/hs-auto-update/buildlink3.mk"
