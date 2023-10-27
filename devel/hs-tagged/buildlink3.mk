85f90b718f562fa61245a0144872c3d21e49e876
pho <pho@pkgsrc.org>
2023-10-27 06:23:31 +0000

    devel/hs-tagged: Update to 0.8.8
    0.8.8 [2023.08.08]
    * Allow building with GHC 9.8.
    
    0.8.7 [2023.02.18]
    * Define Foldable1 and Bifoldable1 instances for Tagged. These instances
      were originally defined in the semigroupoids library, and they have now
      been migrated to tagged as a side effect of adapting to this Core
      Libraries Proposal
      [https://github.com/haskell/core-libraries-committee/issues/9], which
      adds Foldable1 and Bifoldable1 to base.

--- devel/hs-tagged/buildlink3.mk
+++ devel/hs-tagged/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/10/09 04:54:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/10/27 06:23:31 pho Exp $
 
 BUILDLINK_TREE+=	hs-tagged
 
 .if !defined(HS_TAGGED_BUILDLINK3_MK)
 HS_TAGGED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.8.6
-BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.8.6.1nb6
+BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.8.8
+BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.8.8
 BUILDLINK_PKGSRCDIR.hs-tagged?=		../../devel/hs-tagged
 .endif	# HS_TAGGED_BUILDLINK3_MK
 
