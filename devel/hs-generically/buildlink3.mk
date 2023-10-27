e8e97ac4587fd31f808bc8ea8754db5efc584491
pho <pho@pkgsrc.org>
2023-10-27 07:17:34 +0000

    devel/hs-generically: Update to 0.1.1
    0.1.1 -- 2023-03-10
    * Backport Eq and Ord instances for Generically1, which were introduced in
      base-4.18.0.0. To ensure that these instances are in scope when building
      with base-4.17.0.0 (in which Generically1 is defined in base, not this
      library), we also add a dependency on base-orphans.

--- devel/hs-generically/buildlink3.mk
+++ devel/hs-generically/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/27 07:17:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-generically
 
 .if !defined(HS_GENERICALLY_BUILDLINK3_MK)
 HS_GENERICALLY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-generically+=	hs-generically>=0.1
-BUILDLINK_ABI_DEPENDS.hs-generically?=	hs-generically>=0.1nb2
+BUILDLINK_API_DEPENDS.hs-generically+=	hs-generically>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-generically?=	hs-generically>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-generically?=	../../devel/hs-generically
 .endif	# HS_GENERICALLY_BUILDLINK3_MK
 
