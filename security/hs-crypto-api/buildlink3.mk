bc56090028794449e11823ac43df7773f31f0b94
pho <pho@pkgsrc.org>
2023-10-27 11:01:30 +0000

    security/hs-crypto-api: import hs-crypto-api-0.13.3
    A generic interface for cryptographic operations (hashes, ciphers,
    randomness). Maintainers of hash and cipher implementations are encouraged
    to add instances for the classes defined in Crypto.Classes. Crypto users
    are similarly encouraged to use the interfaces defined in the Classes
    module. Any concepts or functions of general use to more than one
    cryptographic algorithm (ex: padding) is within scope of this package.

---
+++ security/hs-crypto-api/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/27 11:01:30 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypto-api
+
+.if !defined(HS_CRYPTO_API_BUILDLINK3_MK)
+HS_CRYPTO_API_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.13.3
+BUILDLINK_ABI_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.13.3
+BUILDLINK_PKGSRCDIR.hs-crypto-api?=	../../security/hs-crypto-api
+
+.include "../../devel/hs-cereal/buildlink3.mk"
+.include "../../security/hs-entropy/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.endif	# HS_CRYPTO_API_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypto-api
