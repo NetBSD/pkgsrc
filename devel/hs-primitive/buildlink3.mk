7acd7fb4373008ede1ee873df443999cba8f4f80
pho <pho@pkgsrc.org>
2023-10-27 06:58:36 +0000

    devel/hs-primitive: Update to 0.9.0.0
    Changes in version 0.9.0.0
    * Add withByteArrayContents, withMutableByteArrayContents,
      withPrimArrayContents, withMutablePrimArrayContents.
    * Fix signature of keepAlive.
    * Remove re-export of fromList and fromListN from Data.Primitive.Array.
    * Use mutableByteArrayContents# in GHC 9.2+
    * Add Prim instance for Complex.
    * Add getSizeofSmallMutableArray that wraps getSizeofSmallMutableArray#
      from GHC.Exts.
    * Add default definitions for the setByteArray# and setOffAddr# methods, so
      they don't need to be defined explicitly anymore.
    * Add standalone sizeOfType/alignmentOfType (recommended over
      sizeOf/alignment) and Prim class methods sizeOfType#/alignmentOfType#
      (recommended over sizeOf#/alignment#)
    * Change Show instances of PrimArray, Array, and SmallArray. These
      previously used the fromListN n [...] form, but they now used the more
      terse [...] form.
    * Correct the Read instances of Array and SmallArray. These instances are
      supposed to be able to handle all three of these forms: fromList [...],
      fromListN n [...], and [...]. They had been rejected the last form, but
      this mistake was discovered by the test suite when the Show instances
      were changed.
    
    Changes in version 0.8.0.0
    * Add resizeSmallMutableArray that wraps resizeSmallMutableArray# from
      GHC.Exts.
    * New module Data.Primitive.PrimVar. This is essentially PrimArray with
      element length 1. For types with Prim instances, this is a drop-in
      replacement for MutVar with fewer indirections.
    * PrimArray's type argument has been given a nominal role instead of a
      phantom role. This is a breaking change.
    * Add readCharArray, writeCharArray, indexCharArray for operating on 8-bit
      characters in a byte array.
    * When building with base-4.17 and newer, re-export the ByteArray and
      MutableByteArray types from base instead of defining them in this
      library. This does not change the user-facing interface of
      Data.Primitive.ByteArray.
    * Add keepAlive that wraps keepAlive# for GHC 9.2 and newer. It falls back
      to using touch for older GHCs.

--- devel/hs-primitive/buildlink3.mk
+++ devel/hs-primitive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/10/09 04:54:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2023/10/27 06:58:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.7.3
-BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.7.4.0nb2
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.9.0
+BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.9.0.0
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../devel/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
