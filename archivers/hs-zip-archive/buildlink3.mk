665bb3405b54cb4fc360551be462253853fecf47
pho <pho@pkgsrc.org>
2023-10-27 08:33:32 +0000

    archivers/hs-zip-archive: Update to 0.4.3
      * Improve code for retrieving compressed data of unknown length (#63).
        Do not assume we'll have the signature 0x08074b50 that is
        sometimes used for the data description, because it is not
        in the spec and is not always used.
      * Make some record fields strict.
      * Require binary >= 0.7.2, remove some CPP

--- archivers/hs-zip-archive/buildlink3.mk
+++ archivers/hs-zip-archive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/27 08:33:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-zip-archive
 
 .if !defined(HS_ZIP_ARCHIVE_BUILDLINK3_MK)
 HS_ZIP_ARCHIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.2.2nb2
+BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.3
+BUILDLINK_ABI_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.3
 BUILDLINK_PKGSRCDIR.hs-zip-archive?=	../../archivers/hs-zip-archive
 
 .include "../../security/hs-digest/buildlink3.mk"
