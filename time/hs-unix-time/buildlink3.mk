0a9cc22c3ee637e5fdb48fdcfb9c2f413f9cd212
pho <pho@pkgsrc.org>
2023-10-27 10:54:00 +0000

    time/hs-unix-time: Update to 0.4.11
    No change log is provided by the upstream.

--- time/hs-unix-time/buildlink3.mk
+++ time/hs-unix-time/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/27 10:54:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-time
 
 .if !defined(HS_UNIX_TIME_BUILDLINK3_MK)
 HS_UNIX_TIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.8
-BUILDLINK_ABI_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.8nb2
+BUILDLINK_API_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.11
+BUILDLINK_ABI_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.11
 BUILDLINK_PKGSRCDIR.hs-unix-time?=	../../time/hs-unix-time
 
 .include "../../time/hs-old-time/buildlink3.mk"
