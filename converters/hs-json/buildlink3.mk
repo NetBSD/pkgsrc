820a921ee8fc787933f99858baf67bd23f4d0855
pho <pho@pkgsrc.org>
2023-10-27 03:52:58 +0000

    converters/hs-json: Update to 0.11
    Version 0.11
      * Limit floating-point range to that of Double to avoid allocation
        overflows when constructing Rationals

--- converters/hs-json/buildlink3.mk
+++ converters/hs-json/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/27 03:52:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-json
 
 .if !defined(HS_JSON_BUILDLINK3_MK)
 HS_JSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-json+=	hs-json>=0.10
-BUILDLINK_ABI_DEPENDS.hs-json+=	hs-json>=0.10nb2
+BUILDLINK_API_DEPENDS.hs-json+=	hs-json>=0.11
+BUILDLINK_ABI_DEPENDS.hs-json+=	hs-json>=0.11
 BUILDLINK_PKGSRCDIR.hs-json?=	../../converters/hs-json
 
 .include "../../devel/hs-syb/buildlink3.mk"
