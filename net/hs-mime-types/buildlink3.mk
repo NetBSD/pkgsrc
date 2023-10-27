0b3458a2d1d674663395d665a3f953571ca0cf55
pho <pho@pkgsrc.org>
2023-10-27 02:11:26 +0000

    net/hs-mime-types: Update to 0.1.2.0
    0.1.2.0
        Added defaultExtensionMap to provide the inverse of defaultMimeMap.
        See PR #930 and #948.

--- net/hs-mime-types/buildlink3.mk
+++ net/hs-mime-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:42 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/27 02:11:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-mime-types
 
 .if !defined(HS_MIME_TYPES_BUILDLINK3_MK)
 HS_MIME_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.1.0nb2
+BUILDLINK_API_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.2.0
 BUILDLINK_PKGSRCDIR.hs-mime-types?=	../../net/hs-mime-types
 .endif	# HS_MIME_TYPES_BUILDLINK3_MK
 
