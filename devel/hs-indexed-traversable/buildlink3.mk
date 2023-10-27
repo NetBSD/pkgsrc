39e449ffbc82789449a861f3af4bdf990a0ab4c6
pho <pho@pkgsrc.org>
2023-10-27 07:01:56 +0000

    devel/hs-indexed-traversable: Update to 0.1.3
    0.1.3 [2023-10-04]
        Add Foldable1WithIndex1 type-class, an indexed version of Foldable1.

--- devel/hs-indexed-traversable/buildlink3.mk
+++ devel/hs-indexed-traversable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/27 07:01:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-indexed-traversable
 
 .if !defined(HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK)
 HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.2nb4
+BUILDLINK_API_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-indexed-traversable?=	../../devel/hs-indexed-traversable
 .endif	# HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK
 
