1e543760f8baa87f260140f73d54ff096ae9ec7d
pho <pho@pkgsrc.org>
2023-10-27 08:00:17 +0000

    devel/hs-OneTuple: Update to 0.4.1.1
    0.4.1.1
    * Support GHC-7.2 and GHC-7.0.
    
    0.4.1
    * Mark MkSolo pattern synonym as COMPLETE
    
    0.4
    * Rename constructor to MkSolo as in base-4.17. The compatibility pattern
      synonym is provided.
    * Add Foldable1 Solo instance

--- devel/hs-OneTuple/buildlink3.mk
+++ devel/hs-OneTuple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/27 08:00:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-OneTuple
 
 .if !defined(HS_ONETUPLE_BUILDLINK3_MK)
 HS_ONETUPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.3.1nb4
+BUILDLINK_API_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.4.1
+BUILDLINK_ABI_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.4.1.1
 BUILDLINK_PKGSRCDIR.hs-OneTuple?=	../../devel/hs-OneTuple
 .endif	# HS_ONETUPLE_BUILDLINK3_MK
 
