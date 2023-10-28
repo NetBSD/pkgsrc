42a94d8bf100da02d049d443350c078cdacf634a
wiz <wiz@pkgsrc.org>
2023-10-28 19:56:54 +0000

    python/wheel.mk: simplify a lot, and switch to 'installer' for installation
    This follows the recommended bootstrap method (flit_core, build, installer).
    
    However, installer installs different files than pip, so update PLISTs
    for all packages using wheel.mk and bump their PKGREVISIONs.

--- lang/python/wheel.mk
+++ lang/python/wheel.mk
@@ -1,83 +1,36 @@
-# $NetBSD: wheel.mk,v 1.9 2023/07/02 13:01:21 riastradh Exp $
+# $NetBSD: wheel.mk,v 1.10 2023/10/28 19:57:10 wiz Exp $
 #
-# Initial mk for building and installing python wheels
+# Build and install Python wheels
 #
 # Variables:
-# 
+#
 # WHEELFILE:		path to the wheelfile to be installed
 #			only needs to be set if do-build is redefined
 #
-# MASTER_SITE_WHEEL:	master site to grab wheels directly 
-#			use like ${MASTER_SITE_WHEEL:=project/}
-#			uses debian pypi redirector so that there
-#			is no need for hashes in urls.
-#
-# TODO: 		fix BUILDDIR support
-#			
-# Feel free to contribute to this file
-#			
 
-.include "../../lang/python/pyversion.mk"
+PY_PATCHPLIST?=	yes
 
-TOOL_DEPENDS+= ${PYPKGPREFIX}-pip>=23.1.2nb1:../../devel/py-pip
+WHEELFILE?=	${WRKSRC}/dist/*.whl
+WHEEL_NAME?=	${DISTNAME:C/-([^0-9])/_\1/g}
+_WHEEL_INFODIR=	${WHEEL_NAME}.dist-info
+PLIST_SUBST+=	WHEEL_INFODIR=${_WHEEL_INFODIR}
 
-WHEELFILE?= ${WRKSRC}/dist/*.whl
-
-.if defined(NO_BUILD)
-
-MASTER_SITE_WHEEL= https://pypi.debian.net/
-
-WHEELFILE= ${DISTFILES}
-
-.for i in 2 3
-.if !empty(_PYTHON_VERSIONS_ACCEPTED:M${i}*)
-PYMAJORVERSIONS+= py${i}
-.endif
-.endfor
-
-EXTRACT_SUFX= -${PYMAJORVERSIONS:ts.}-none-any.whl
-
-.else
-#we need to build a wheel
-
-TOOL_DEPENDS+= ${PYPKGPREFIX}-build>=0:../../devel/py-build
+PRINT_PLIST_AWK+=	{ gsub(/${_WHEEL_INFODIR:S,.,\.,g}/, "$${WHEEL_INFODIR}") }
 
 .if !target(do-build)
+TOOL_DEPENDS+= ${PYPKGPREFIX}-build>=0:../../devel/py-build
 do-build:
 	${RUN} cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${TOOL_PYTHONBIN} \
 		-m build --wheel --skip-dependency-check --no-isolation
 .endif
 
-.endif
-
-WHEEL_NAME?=	${DISTNAME:C/-([^0-9])/_\1/g}
-_WHEEL_INFODIR=	${WHEEL_NAME}.dist-info
-PLIST_SUBST+=	PYSITELIB=${PYSITELIB}
-PLIST_SUBST+=	PYVERSSUFFIX=${PYVERSSUFFIX}
-PLIST_SUBST+=	WHEEL_INFODIR=${_WHEEL_INFODIR}
-
-# mostly for ALTERNATIVES files
-FILES_SUBST+=	PYVERSSUFFIX=${PYVERSSUFFIX}
-
-# from extension.mk
-
-# Python>=3.2 bytecode file location change
-# http://www.python.org/dev/peps/pep-3147/
-.if empty(_PYTHON_VERSION:M2?)
-PLIST_AWK+=		-f ${PKGSRCDIR}/lang/python/plist-python.awk
-PLIST_AWK_ENV+=		PYVERS="${PYVERSSUFFIX:S/.//}"
-EARLY_PRINT_PLIST_AWK+=	/^[^@]/ && /[^\/]+\.pyc$$/ {
-EARLY_PRINT_PLIST_AWK+=	gsub(/__pycache__\//, "")
-EARLY_PRINT_PLIST_AWK+=	gsub(/\.cpython-${_PYTHON_VERSION}/, "")}
-.endif
-
-PRINT_PLIST_AWK+=	{ gsub(/${_WHEEL_INFODIR:S,.,\.,g}/, "$${WHEEL_INFODIR}") }
-
-INSTALL_ENV+= PIP_NO_CACHEDIR=1
-
+.if !target(do-install)
+TOOL_DEPENDS+= ${PYPKGPREFIX}-installer>=0.7.0nb1:../../misc/py-installer
 do-install:
-	${RUN} cd ${WRKDIR} && \
+	${RUN} cd ${WRKSRC} && \
 	${SETENV} ${INSTALL_ENV} \
-	${TOOL_PYTHONBIN} -m pip install --no-cache-dir --no-deps \
-		--root ${DESTDIR:Q} --prefix ${PREFIX:Q} --compile \
-		--force-reinstall -I --executable=${PYTHONBIN:Q} ${WHEELFILE}
+	${TOOL_PYTHONBIN} -m installer --destdir ${DESTDIR:Q} \
+		--prefix ${PREFIX:Q} ${WHEELFILE}
+.endif
+
+.include "../../lang/python/extension.mk"
