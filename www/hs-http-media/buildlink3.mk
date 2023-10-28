f9a267029da6cd9a548b8deb306cac908ee53ece
pho <pho@pkgsrc.org>
2023-10-28 20:14:59 +0000

    www/hs-http-media: Update to 0.8.1.1
    Version 0.8.1.1
    * Fixed a bug when mapping againt a client-side content header where a
      match would be overridden by a more specific key earlier in the map.
    * The bounds for tasty have been updated to include the latest version.
    
    Version 0.8.1.0
    * Exposed qualityData accessor.
    * Added isAcceptable to allow filtering out unacceptable items.

--- www/hs-http-media/buildlink3.mk
+++ www/hs-http-media/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/28 20:14:59 pho Exp $
 
 BUILDLINK_TREE+=	hs-http-media
 
 .if !defined(HS_HTTP_MEDIA_BUILDLINK3_MK)
 HS_HTTP_MEDIA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-media+=	hs-http-media>=0.8.0
-BUILDLINK_ABI_DEPENDS.hs-http-media+=	hs-http-media>=0.8.0.0nb2
+BUILDLINK_API_DEPENDS.hs-http-media+=	hs-http-media>=0.8.1.1
+BUILDLINK_ABI_DEPENDS.hs-http-media+=	hs-http-media>=0.8.1.1
 BUILDLINK_PKGSRCDIR.hs-http-media?=	../../www/hs-http-media
 
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
