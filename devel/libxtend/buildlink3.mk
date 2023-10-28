cb138e809332599753ccc26920daf0488dfcb81e
bacon <bacon@pkgsrc.org>
2023-10-28 20:28:20 +0000

    devel/libxtend: Update to 0.2.0
    Add xt_ prefix to all remaining functions
    Switch fast-file class to opaque structure
    Improve test scripts
    Add libxtend(3) man page listing all functions (generated by auto-man2man)
    
    Changes: https://github.com/outpaddling/libxtend/releases

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/08/12 14:46:26 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/10/28 20:28:20 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.9
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
