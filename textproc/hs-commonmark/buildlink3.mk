84e206d5f3ca740b31245b4881b80d56cbb58690
pho <pho@pkgsrc.org>
2023-10-28 09:52:23 +0000

    textproc/hs-commonmark: Update to 0.2.4
    0.2.4
    * Do not parse hard line breaks in fenced codeblock info (#116, Michael
      Howell). This change makes commonmark-hs conform to the spec and behave
      like other implementations when an info string in a code block ends with
      a backslash.
    * [API change] Commonmark.Inlines now exports pEscapedSymbol (#116, Michael
      Howell).
    * Tokenize combining marks as WordChars not Symbol (#114).

--- textproc/hs-commonmark/buildlink3.mk
+++ textproc/hs-commonmark/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/26 17:18:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/28 09:52:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark
 
 .if !defined(HS_COMMONMARK_BUILDLINK3_MK)
 HS_COMMONMARK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.3
-BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.3
+BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.4
+BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.4
 BUILDLINK_PKGSRCDIR.hs-commonmark?=	../../textproc/hs-commonmark
 
 .include "../../textproc/hs-unicode-data/buildlink3.mk"
