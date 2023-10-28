e419f1577eda2f8e009c834171eb43c685d0913b
pho <pho@pkgsrc.org>
2023-10-28 17:01:01 +0000

    devel/hs-vector: Update to 0.13.1.0
    Changes in version 0.13.1.0
    * Specialized variants of findIndexR are reexported for all vector
      types. #469
    * UnboxViaPrim could be used for deriving Unbox instances (V_UnboxViaPrim
      constructor is exported) #450
    * Fields of Data.Vector.Fusion.Bundle.Size are now strict #456
    * Compatibility with future GHC 9.10 release #462
    * Test suite no longer fails when built with QuickCheck-2.14 #461
    * Doctests now work with current versions of GHC #465
    * Various documentation improvements

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2023/10/09 04:54:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2023/10/28 17:01:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.12.3
-BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.13.0.0nb2
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.13.1
+BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.13.1.0
 BUILDLINK_PKGSRCDIR.hs-vector?=		../../devel/hs-vector
 
 .include "../../devel/hs-primitive/buildlink3.mk"
