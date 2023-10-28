fa18526e2607147807cf6d37b8ae16123d21607c
pho <pho@pkgsrc.org>
2023-10-28 12:45:37 +0000

    devel/hs-cborg: Update to 0.2.9.0
    0.2.9.0
        Fix build with base >= 4.17 on platforms without unaligned memory operations.
        Fix Eq, Ord, Show and IsList instances for SlicedByteArray when offset is present
        Fix toBuilder and encodeByteArray for SlicedByteArray when offset is present

--- devel/hs-cborg/buildlink3.mk
+++ devel/hs-cborg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/28 12:45:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-cborg
 
 .if !defined(HS_CBORG_BUILDLINK3_MK)
 HS_CBORG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cborg+=	hs-cborg>=0.2.8
-BUILDLINK_ABI_DEPENDS.hs-cborg+=	hs-cborg>=0.2.8.0nb2
+BUILDLINK_API_DEPENDS.hs-cborg+=	hs-cborg>=0.2.9
+BUILDLINK_ABI_DEPENDS.hs-cborg+=	hs-cborg>=0.2.9.0
 BUILDLINK_PKGSRCDIR.hs-cborg?=		../../devel/hs-cborg
 
 .include "../../devel/hs-half/buildlink3.mk"
