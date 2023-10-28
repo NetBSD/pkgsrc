e22b4b4ea91e9352f476bc7a443ad0df19a3acba
pho <pho@pkgsrc.org>
2023-10-28 20:00:47 +0000

    devel/hs-these: Update to 1.2
    * Depend on bifunctor-classes-compat instead of bifunctors See changelog
      note in bifunctors-5.6:
      https://hackage.haskell.org/package/bifunctors-5.6/changelog This is
      breaking change, but affects only GHC-8.0 and older users. In that case
      you should check various combinations of newer/older bifunctors, these
      (and e.g. semialign) packages.
    * Depend on assoc-1.1. Since version 1.1 assoc has an almost trivial
      dependency footprint, so these depends on it unconditionally.
    * Add Bifoldable1 These instance
    * Add Foldable1 (Data.Functor.These1 f g) instance
    * Change Eq (These1 f g a), Ord, Read, Show, NFData instances similarly to
      how they are changed for Product and Sum in base-4.18.0.0.

--- devel/hs-these/buildlink3.mk
+++ devel/hs-these/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/28 20:00:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-these
 
 .if !defined(HS_THESE_BUILDLINK3_MK)
 HS_THESE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-these+=	hs-these>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-these+=	hs-these>=1.1.1.1nb6
+BUILDLINK_API_DEPENDS.hs-these+=	hs-these>=1.2
+BUILDLINK_ABI_DEPENDS.hs-these+=	hs-these>=1.2
 BUILDLINK_PKGSRCDIR.hs-these?=		../../devel/hs-these
 
-.include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-assoc/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
 .endif	# HS_THESE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-these
