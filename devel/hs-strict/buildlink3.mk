923e2578297d7ddbbf6345777850355a2d01c2ae
pho <pho@pkgsrc.org>
2023-10-28 20:03:46 +0000

    devel/hs-strict: Update to 0.5
    Depend on bifunctor-classes-compat instead of bifunctors See changelog note
    in bifunctors-5.6:
    https://hackage.haskell.org/package/bifunctors-5.6/changelog This is
    breaking change, but affects only GHC-8.0 and older users.

--- devel/hs-strict/buildlink3.mk
+++ devel/hs-strict/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/28 20:03:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-strict
 
 .if !defined(HS_STRICT_BUILDLINK3_MK)
 HS_STRICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-strict+=	hs-strict>=0.4.0
-BUILDLINK_ABI_DEPENDS.hs-strict+=	hs-strict>=0.4.0.1nb6
+BUILDLINK_API_DEPENDS.hs-strict+=	hs-strict>=0.5
+BUILDLINK_ABI_DEPENDS.hs-strict+=	hs-strict>=0.5
 BUILDLINK_PKGSRCDIR.hs-strict?=		../../devel/hs-strict
 
 .include "../../devel/hs-hashable/buildlink3.mk"
