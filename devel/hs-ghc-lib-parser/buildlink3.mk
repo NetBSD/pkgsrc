8ceb61f131a4ec3f1e71766389e5559db81b770e
pho <pho@pkgsrc.org>
2023-10-28 04:26:46 +0000

    devel/hs-ghc-lib-parser: Update to 9.6.3.20231014
    No change log is provided by the upstream.

--- devel/hs-ghc-lib-parser/buildlink3.mk
+++ devel/hs-ghc-lib-parser/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/28 04:26:46 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-lib-parser
 
 .if !defined(HS_GHC_LIB_PARSER_BUILDLINK3_MK)
 HS_GHC_LIB_PARSER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-lib-parser+=	hs-ghc-lib-parser>=9.4.4
-BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser+=	hs-ghc-lib-parser>=9.4.4.20221225nb2
+BUILDLINK_API_DEPENDS.hs-ghc-lib-parser+=	hs-ghc-lib-parser>=9.6.3
+BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser+=	hs-ghc-lib-parser>=9.6.3.20231014
 BUILDLINK_PKGSRCDIR.hs-ghc-lib-parser?=		../../devel/hs-ghc-lib-parser
 .endif	# HS_GHC_LIB_PARSER_BUILDLINK3_MK
 
