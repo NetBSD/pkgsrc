137c8648fe97bb68f39eb33328a05fe0797b6eaa
pho <pho@pkgsrc.org>
2023-10-28 20:12:37 +0000

    lang/hs-hslua-list: Update to 1.1.1
    hslua-list-1.1.1 - Released 2023-03-17.
    * Conversion to strings: added a __tostring that lists all elements
      separated by commas and a space, surrounded by braces and prefixed with
      the metatable's name.

--- lang/hs-hslua-list/buildlink3.mk
+++ lang/hs-hslua-list/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/28 20:12:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-list
 
 .if !defined(HS_HSLUA_LIST_BUILDLINK3_MK)
 HS_HSLUA_LIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-hslua-list?=	../../lang/hs-hslua-list
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
