83d041aba986707e1d82bc72f83cf2dd947c587a
pho <pho@pkgsrc.org>
2023-10-28 20:32:37 +0000

    textproc/hs-charset: Update to 0.3.10
    0.3.10 [2023.08.06]
    * Allow building with bytestring-0.12.*.
    * Replace a use of bytestring's memset function (which is now deprecated as
      of bytestring-0.12.*) with base's fillBytes function.

--- textproc/hs-charset/buildlink3.mk
+++ textproc/hs-charset/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/28 20:32:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-charset
 
 .if !defined(HS_CHARSET_BUILDLINK3_MK)
 HS_CHARSET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-charset+=	hs-charset>=0.3.9
-BUILDLINK_ABI_DEPENDS.hs-charset+=	hs-charset>=0.3.9nb3
+BUILDLINK_API_DEPENDS.hs-charset+=	hs-charset>=0.3.10
+BUILDLINK_ABI_DEPENDS.hs-charset+=	hs-charset>=0.3.10
 BUILDLINK_PKGSRCDIR.hs-charset?=	../../textproc/hs-charset
 
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
