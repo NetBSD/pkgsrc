d21b24cf99239ab1476c39d7e612785cc6c18fd0
bacon <bacon@pkgsrc.org>
2023-10-28 20:31:40 +0000

    biology/biolibc: Update to 0.2.6
    Updates for libxtend 0.2.0 API
    Add full list of functions to biolibc(3) man page
    Other documentation improvements
    
    Changes: https://github.com/auerlab/biolibc/releases

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/08/12 14:49:18 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/10/28 20:31:40 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.5
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.6
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
