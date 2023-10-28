ae7aee06993ab0892797ccda10d7e748cfde4f10
pho <pho@pkgsrc.org>
2023-10-28 09:45:22 +0000

    textproc/hs-doclayout: Update to 0.4.0.1
    0.4.0.1
        Add clause for Empty to renderList (#22).
        Remove upper bound for criterion

--- textproc/hs-doclayout/buildlink3.mk
+++ textproc/hs-doclayout/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/28 09:45:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-doclayout
 
 .if !defined(HS_DOCLAYOUT_BUILDLINK3_MK)
 HS_DOCLAYOUT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.4
-BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.4nb2
+BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.4.0
+BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.4.0.1
 BUILDLINK_PKGSRCDIR.hs-doclayout?=	../../textproc/hs-doclayout
 
 .include "../../converters/hs-emojis/buildlink3.mk"
