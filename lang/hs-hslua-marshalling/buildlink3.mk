b2837d408545cc9f97b79373864ed85633eaa247
pho <pho@pkgsrc.org>
2023-10-28 20:10:44 +0000

    lang/hs-hslua-marshalling: Update to 2.3.0
    hslua-marshalling-2.3.0 - Released 2023-03-13.
    * Result is now an instance of MonadFail.
    * New peeker and pusher functions for NonEmpty.
    * Peeker combinators for optional values: The new combinators peekNilOr,
      peekNoneOr, and peekNoneOrNil can be used to retrieve optional values.

--- lang/hs-hslua-marshalling/buildlink3.mk
+++ lang/hs-hslua-marshalling/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/28 20:10:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-marshalling
 
 .if !defined(HS_HSLUA_MARSHALLING_BUILDLINK3_MK)
 HS_HSLUA_MARSHALLING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.3.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.3.0
 BUILDLINK_PKGSRCDIR.hs-hslua-marshalling?=	../../lang/hs-hslua-marshalling
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
