9fc24bb310a3a25f0591ad25ce0e5848a6b7b6d4
pho <pho@pkgsrc.org>
2023-10-29 07:07:04 +0000

    math/hs-monoid-subclasses: Update to 1.2.4.1
    Version 1.2.4.1
    * Bumped the text dependency upper bounds
    
    Version 1.2.4
    * Added Data.Monoid.Instances.PrefixMemory.Shadowed monoid transformer
    
    Version 1.2.3
    * Added DistributiveGCDMonoid and DistributiveLCMMonoid type class by
      Jonathan Knowles
    
    Version 1.2.2
    * Added Data.Monoid.LCM module with LCMMonoid type class by Jonathan
      Knowles
    * Repaired links to Hackage within README.md by Jonathan Knowles

--- math/hs-monoid-subclasses/buildlink3.mk
+++ math/hs-monoid-subclasses/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/29 07:07:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-monoid-subclasses
 
 .if !defined(HS_MONOID_SUBCLASSES_BUILDLINK3_MK)
 HS_MONOID_SUBCLASSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.1
-BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.1nb2
+BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.4
+BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.4.1
 BUILDLINK_PKGSRCDIR.hs-monoid-subclasses?=	../../math/hs-monoid-subclasses
 
 .include "../../math/hs-commutative-semigroups/buildlink3.mk"
