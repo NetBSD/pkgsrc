545f159d6b9b22bc687ebdeee15651698ae63973
pho <pho@pkgsrc.org>
2023-10-29 11:26:07 +0000

    devel/hs-constraints: Update to 0.14
    0.14 [2023.10.11]
    * Drop support for GHCs older than 8.6.
    * The forall function in Data.Constraint.Forall has been renamed to
      forall_, since a future version of GHC will make the use of forall as an
      identifier an error.
    * Implement Data.Constraint.Forall using QuantifiedConstraints.
    * Remove Lifting instances for ErrorT and ListT, which were removed in
      transformers-0.6.*.
    * Add a c => Boring (Dict c) instance.
    * Add the Data.Constraint.Char module, which contains utilities for working
      with KnownChar constraints. This module is only available on GHC 9.2 or
      later.
    * Add unsafeAxiom to Data.Constraint.Unsafe.
    * Add unsafeSChar, unsafeSNat, and unsafeSSymbol to Data.Constraint.Unsafe
      when building with base-4.18 (GHC 9.6) or later.

--- devel/hs-constraints/buildlink3.mk
+++ devel/hs-constraints/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/29 11:26:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-constraints
 
@@ -9,8 +9,8 @@ BUILDLINK_API_DEPENDS.hs-constraints+=	hs-constraints>=0.13.4
 BUILDLINK_ABI_DEPENDS.hs-constraints+=	hs-constraints>=0.13.4nb2
 BUILDLINK_PKGSRCDIR.hs-constraints?=	../../devel/hs-constraints
 
+.include "../../devel/hs-boring/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-type-equality/buildlink3.mk"
 .endif	# HS_CONSTRAINTS_BUILDLINK3_MK
 
