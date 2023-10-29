4001c8ae40c2119e1afc05a3c6ae6fab5ec10f6b
pho <pho@pkgsrc.org>
2023-10-29 07:18:44 +0000

    devel/hs-with-utf8: Update to 1.0.2.4
    1.0.2.4
    * Allow base 4.17, 4.18 (GHC 9.4, 9.6).
    * Allow text<2.1

--- devel/hs-with-utf8/buildlink3.mk
+++ devel/hs-with-utf8/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:35 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/29 07:18:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-with-utf8
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-with-utf8
 HS_WITH_UTF8_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.0.2.3nb3
+BUILDLINK_ABI_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.0.2.4
 BUILDLINK_PKGSRCDIR.hs-with-utf8?=	../../devel/hs-with-utf8
 
 .include "../../devel/hs-safe-exceptions/buildlink3.mk"
