ca3b6854a8e6d0023f946081dc2eeb93e76a1356
bsiegert <bsiegert@pkgsrc.org>
2023-10-29 17:51:23 +0000

    New package, gotelemetry-0.8.0.
    This package contains the gotelemetry tool, which allows opting in or
    out from Go telemetry reporting. It will also let you see local
    telemetry data in a web interface.
    
    Note that by default, uploading telemetry data is always disabled. Run
    "gotelemetry on" to enable collection and upload of telemetry data.

---
+++ devel/gotelemetry/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/10/29 17:51:24 bsiegert Exp $
+
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.13.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.13.0.zip
