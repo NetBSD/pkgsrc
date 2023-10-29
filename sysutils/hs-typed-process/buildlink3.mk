2b9e5454dff7bf359f2dc8833226d44de51aeeeb
pho <pho@pkgsrc.org>
2023-10-29 09:39:14 +0000

    sysutils/hs-typed-process: Update to 0.2.11.1
    0.2.11.1
        No user-visible changes
    
    0.2.11.0
        Expose more from System.Process.Typed.Internal

--- sysutils/hs-typed-process/buildlink3.mk
+++ sysutils/hs-typed-process/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/29 09:39:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-typed-process
 
 .if !defined(HS_TYPED_PROCESS_BUILDLINK3_MK)
 HS_TYPED_PROCESS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.10
-BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.10.1nb3
+BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.11
+BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.11.1
 BUILDLINK_PKGSRCDIR.hs-typed-process?=		../../sysutils/hs-typed-process
 
 .include "../../devel/hs-async/buildlink3.mk"
