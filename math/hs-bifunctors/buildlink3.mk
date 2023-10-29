2641f6bd937229aca0f08749b36254ea12b71732
pho <pho@pkgsrc.org>
2023-10-29 09:48:51 +0000

    math/hs-bifunctors: Update to 5.6.1
    The release note is too long to paste here. See
    https://hackage.haskell.org/package/bifunctors-5.6.1/changelog

--- math/hs-bifunctors/buildlink3.mk
+++ math/hs-bifunctors/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/29 09:48:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-bifunctors
 
 .if !defined(HS_BIFUNCTORS_BUILDLINK3_MK)
 HS_BIFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.14
-BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.5.14nb2
+BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.6.1
+BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.6.1
 BUILDLINK_PKGSRCDIR.hs-bifunctors?=	../../math/hs-bifunctors
 
-.include "../../devel/hs-base-orphans/buildlink3.mk"
+.include "../../devel/hs-assoc/buildlink3.mk"
 .include "../../math/hs-comonad/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
