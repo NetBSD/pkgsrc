45feed45cf50c643b691aac2eda5d49038c23935
pho <pho@pkgsrc.org>
2023-10-29 10:38:47 +0000

    devel/hs-async-pool: Update to 0.9.2
    No change log is provided by the upstream.

--- devel/hs-async-pool/buildlink3.mk
+++ devel/hs-async-pool/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/29 10:38:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-async-pool
 
 .if !defined(HS_ASYNC_POOL_BUILDLINK3_MK)
 HS_ASYNC_POOL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-async-pool+=	hs-async-pool>=0.9.1
-BUILDLINK_ABI_DEPENDS.hs-async-pool+=	hs-async-pool>=0.9.1nb3
+BUILDLINK_API_DEPENDS.hs-async-pool+=	hs-async-pool>=0.9.2
+BUILDLINK_ABI_DEPENDS.hs-async-pool+=	hs-async-pool>=0.9.2
 BUILDLINK_PKGSRCDIR.hs-async-pool?=	../../devel/hs-async-pool
 
 .include "../../devel/hs-async/buildlink3.mk"
