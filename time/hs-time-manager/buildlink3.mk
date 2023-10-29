802b2280077007dbb6d2ea62923c36413b6ef99c
pho <pho@pkgsrc.org>
2023-10-29 10:00:56 +0000

    time/hs-time-manager: Update to 0.0.1
    No change log is provided by the upstream.

--- time/hs-time-manager/buildlink3.mk
+++ time/hs-time-manager/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/29 10:00:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-time-manager
 
 .if !defined(HS_TIME_MANAGER_BUILDLINK3_MK)
 HS_TIME_MANAGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.0
-BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.0nb5
+BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.1
+BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.1
 BUILDLINK_PKGSRCDIR.hs-time-manager?=	../../time/hs-time-manager
 
 .include "../../devel/hs-auto-update/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
 .endif	# HS_TIME_MANAGER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-time-manager
