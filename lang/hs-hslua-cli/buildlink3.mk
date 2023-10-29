2d8d66c452cbe2c1dba0d6db75b92185107e5946
pho <pho@pkgsrc.org>
2023-10-29 11:45:07 +0000

    lang/hs-hslua-cli: Update to 1.4.1
    hslua-cli-1.4.1 - Released 2023-03-18.
    * Always start the REPL if the -i parameter is given on the command
      line. This fixes a bug where the REPL would not start if -v, -e or -l
      where given.
    
    hslua-cli-1.4.0.1 - Released 2023-03-17.
    * Fix building on Windows.
    
    hslua-cli-1.4.0 - Released 2023-03-16.
    * Isocline-based REPL: interactive mode is now supported with the help of a
      new repl built with the isocline library.
    
    hslua-cli-1.3.0 - Released 2023-03-13.
    * Require hslua-core 2.3.

--- lang/hs-hslua-cli/buildlink3.mk
+++ lang/hs-hslua-cli/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/29 11:45:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-cli
 
 .if !defined(HS_HSLUA_CLI_BUILDLINK3_MK)
 HS_HSLUA_CLI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.2.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.4.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.4.1
 BUILDLINK_PKGSRCDIR.hs-hslua-cli?=	../../lang/hs-hslua-cli
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-repl/buildlink3.mk"
 .include "../../lang/hs-lua/buildlink3.mk"
 .endif	# HS_HSLUA_CLI_BUILDLINK3_MK
 
