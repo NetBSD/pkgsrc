d0b3977fc9d51396e21eedfb44857af8fe8fd471
pho <pho@pkgsrc.org>
2023-10-29 11:41:29 +0000

    devel/hs-blaze-textual: Update to 0.2.3.1
    0.2.3.1 2023-03-02
    * Drop redundant dependency on time (see #19).
    
    0.2.3 2023-03-02
    * Fix incorrect minBound detection and -128 :: Int8 (see #12).

--- devel/hs-blaze-textual/buildlink3.mk
+++ devel/hs-blaze-textual/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/29 11:41:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-textual
 
 .if !defined(HS_BLAZE_TEXTUAL_BUILDLINK3_MK)
 HS_BLAZE_TEXTUAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.2.1nb4
+BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.3.1
 BUILDLINK_PKGSRCDIR.hs-blaze-textual?=		../../devel/hs-blaze-textual
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
