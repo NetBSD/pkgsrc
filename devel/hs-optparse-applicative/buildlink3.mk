313d5fe46f6bb46efd791f554554f8b23d0f3503
pho <pho@pkgsrc.org>
2023-10-29 11:31:29 +0000

    devel/hs-optparse-applicative: Update to 0.18.1.0
    Version 0.18.1.0 (29 May 2023)
    
    * Change pretty printer layout algorithm used.
    
      The layoutSmart algorithm appears to be extremely slow with some command
      line sets, to the point where the program appears to hang.
    
      Fixes issues:
      # 476 - Stack executable 'hangs' with 0.17.1 and 0.18.0.
    
    * Render help text with AnsiStyle aware rendering functions.
    
    Version 0.18.0.0 (22 May 2023)
    
    * Move to 'prettyprinter` library for pretty printing.
    
    * This is a potentially breaking change when one uses the '*Doc' family of
      functions (like headerDoc) from Options.Applicative. However, as versions
      of 'ansi-wl-pprint > 1.0' export a compatible Doc type, this can be
      mitigated by using a recent version.
    
    * One can also either import directly from Options.Applicative.Help or from
      the Prettyprinter module of 'prettyprinter'.
    
    * Allow commands to be disambiguated in a similar manner to flags when the
      disambiguate modifier is used.
    
    * This is a potentially breaking change as the internal CmdReader
      constructor has been adapted so it is able to be inspected to a greater
      degree to support finding prefix matches.
    
    Version 0.17.1.0 (22 May 2023)
    
    * Widen bounds for ansi-wl-pprint. This supports the use of prettyprinter
      in a non-breaking way, as the ansi-wl-pprint > 1.0 support the newer
      library.
    
    * Export helpIndent from Options.Applicative.
    
    * Export completion script generators from
      Options.Applicative.BashCompletion.
    
    * Add simpleVersioner utility for adding a '--version' option to a parser.
    
    * Improve documentation.
    
    * Drop support for GHC 7.0 and 7.2.

--- devel/hs-optparse-applicative/buildlink3.mk
+++ devel/hs-optparse-applicative/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/29 11:31:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-optparse-applicative
 
 .if !defined(HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK)
 HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.17.0
-BUILDLINK_ABI_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.17.0.0nb5
+BUILDLINK_API_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.18.1
+BUILDLINK_ABI_DEPENDS.hs-optparse-applicative+=	hs-optparse-applicative>=0.18.1.0
 BUILDLINK_PKGSRCDIR.hs-optparse-applicative?=	../../devel/hs-optparse-applicative
 
-.include "../../textproc/hs-ansi-wl-pprint/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter/buildlink3.mk"
+.include "../../textproc/hs-prettyprinter-ansi-terminal/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_OPTPARSE_APPLICATIVE_BUILDLINK3_MK
 
