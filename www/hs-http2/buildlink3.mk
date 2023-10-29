fda1135467d2bfebcbc498c0dcc2911142bd11b3
pho <pho@pkgsrc.org>
2023-10-29 18:38:03 +0000

    www/hs-http2: Update to 4.2.2
    4.2.2
    * Adding rate limit for RST_STREAM to work around CVE-2023-44487. #94
    
    4.2.1
    * This version is identical to v4.2.0 by accident.
    
    4.2.0
    * Treating HALF_CLOSED_LOCAL correctly. #90
    * Ensuring that GOAWAY is sent after DATA in the client side. #89
    * Test uses a random port instead of 8080.
    * Breaking change: adding two optional SockAddrs to Config to be copied
      into Aux.
    * Close all streams on termination. #83
    * Introducing OutBodyStreamingUnmask #80
    * Introducing KilledByHttp2ThreadManager instead of ThreadKilled. #79 #81
      #82
    * Handle RST_STREAM with NO_ERROR. #78
    * Internal changes: #74
    * Breaking change: Client is generalized into (forall b. Request ->
      (Response -> IO b) -> IO b) -> IO a. The RankNTypes language extension is
      required. #72
    
    4.1.3
    * Using crypton instead of cryptonite.
    
    4.1.2
    * Removing the race of frameSender and frameReceiver in the server
      side. This fixes the loss of RST_Stream and TLS bad MAC error. #67
    
    4.1.1
    * Fixing memory-blow-up due to no flow control. #62 #66
    
    4.1.0
    * Implementing streaming from the client side. #41
    * Making use of SettingsMaxFrameSize #44 #57
    * Disabling flow control #55
    * Fixing buffer overrun by trailers #52
    * Proper use of settings
    * Breaking change: the data structure of Next was changed. The http3
      package is influenced.
    
    4.0.0
    * Breaking change: HTTP2Error is redefined.
    * Breaking change: FrameTypeId, SettingsKeyId and ErrorCodeId are
      removed. Use FrameType, SettingsKey and ErrorCode instead.
    * A client can receive a concrete HTTP2Error.
    * Catching up RFC 9113. Host: and :authority cannot disagree.
    * Breaking change: Network.HTTP2 and Network.HTTP2.Priority are removed.
    * Breaking change: obsoleted stuff are removed.

--- www/hs-http2/buildlink3.mk
+++ www/hs-http2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/29 18:38:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-http2
 
 .if !defined(HS_HTTP2_BUILDLINK3_MK)
 HS_HTTP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=3.0.3
-BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=3.0.3nb4
+BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=4.2.2
+BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=4.2.2
 BUILDLINK_PKGSRCDIR.hs-http2?=		../../www/hs-http2
 
 .include "../../devel/hs-async/buildlink3.mk"
@@ -17,6 +17,7 @@ BUILDLINK_PKGSRCDIR.hs-http2?=		../../www/hs-http2
 .include "../../devel/hs-psqueues/buildlink3.mk"
 .include "../../time/hs-time-manager/buildlink3.mk"
 .include "../../time/hs-unix-time/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
 .endif	# HS_HTTP2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-http2
