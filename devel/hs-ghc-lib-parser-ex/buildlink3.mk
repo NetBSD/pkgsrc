126fa0b578ac21181aacaea8e66d42ce436db45c
pho <pho@pkgsrc.org>
2023-10-29 11:01:14 +0000

    devel/hs-ghc-lib-parser-ex: Update to 9.6.0.2
    9.6.0.2 released
        New functions isTypedSplice, isUntypedSpice
    
    9.6.0.1 released
        Add & to baseFixities
        Add support for ghc-9.8 series: GH_9_8
        New functions isLetStmt and isDo
    
    9.6.0.0 released
        Add support for ghc-9.6 series: GHC_9_6

--- devel/hs-ghc-lib-parser-ex/buildlink3.mk
+++ devel/hs-ghc-lib-parser-ex/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/29 11:01:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-lib-parser-ex
 
 .if !defined(HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK)
 HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.4.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.4.0.0nb2
+BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.6.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.6.0.2
 BUILDLINK_PKGSRCDIR.hs-ghc-lib-parser-ex?=	../../devel/hs-ghc-lib-parser-ex
 
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
