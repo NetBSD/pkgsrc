c8329883fecf364a2f83a6d6519eb68fb3d7ab63
wiz <wiz@pkgsrc.org>
2023-10-29 17:53:19 +0000

    devel/py27-game: import py27-game-1.9.6nb3
    Pygame is a set of Python modules designed for writing games. It is written on
    top of the excellent SDL library. This allows you to create fully featured
    games and multimedia programs in the python language. Pygame is highly
    portable and runs on nearly every platform and operating system.

---
+++ devel/py27-game/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/29 17:53:19 wiz Exp $
+
+BUILDLINK_TREE+=	py-game
+
+.if !defined(PY_GAME_BUILDLINK3_MK)
+PY_GAME_BUILDLINK3_MK:=
+
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_API_DEPENDS.py-game+=	${PYPKGPREFIX}-game>=1.9.1
+BUILDLINK_ABI_DEPENDS.py-game+=	${PYPKGPREFIX}-game>=1.9.6nb3
+BUILDLINK_PKGSRCDIR.py-game?=	../../devel/py27-game
+
+.include "../../audio/SDL_mixer/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
+.include "../../devel/SDL_ttf/buildlink3.mk"
+.include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../multimedia/smpeg/buildlink3.mk"
+.if ${OPSYS} != "Darwin"
+.  include "../../x11/libX11/buildlink3.mk"
+.endif
+.endif # PY_GAME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-game
