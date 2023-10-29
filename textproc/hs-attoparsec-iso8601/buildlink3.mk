b8414b70fb9ef022a18b817919daf5d7adbab8db
pho <pho@pkgsrc.org>
2023-10-29 18:44:44 +0000

    textproc/hs-attoparsec-iso8601: Update to 1.1.0.1
    No change log is provided by the upstream.

--- textproc/hs-attoparsec-iso8601/buildlink3.mk
+++ textproc/hs-attoparsec-iso8601/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/29 18:44:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec-iso8601
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	hs-attoparsec-iso8601
 HS_ATTOPARSEC_ISO8601_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.0.0nb2
+BUILDLINK_ABI_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.0.1
 BUILDLINK_PKGSRCDIR.hs-attoparsec-iso8601?=	../../textproc/hs-attoparsec-iso8601
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../devel/hs-base-compat-batteries/buildlink3.mk"
+.include "../../math/hs-integer-conversion/buildlink3.mk"
 .include "../../time/hs-time-compat/buildlink3.mk"
 .endif	# HS_ATTOPARSEC_ISO8601_BUILDLINK3_MK
 
