5f9437816de394ef3b857201865cef9f3b0d27f0
pho <pho@pkgsrc.org>
2023-10-29 07:35:45 +0000

    devel/hs-primitive-unlifted: Update to 2.1.0.0
    2.1.0.0 -- 2023-06-28
    * Change the order of the type arguments to UnliftedArray_ and
      MutableUnliftedArray_. This makes the library work better with the
      typeclasses in the contiguous library.
    
    2.0.0.0 -- 2023-06-27
    * Use legitimate unlifted primitive types and operations, only supporting
      GHC 9.4 and newer.
    
    1.0.0.0 -- 2020-11-02
    * Redo everything. This uses unsafeCoerce# a lot to coerce between lifted
      and unlifted types. Stay on the 0.1.x.x series unless you need something
      from this newer version.

--- devel/hs-primitive-unlifted/buildlink3.mk
+++ devel/hs-primitive-unlifted/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/29 07:35:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive-unlifted
 
 .if !defined(HS_PRIMITIVE_UNLIFTED_BUILDLINK3_MK)
 HS_PRIMITIVE_UNLIFTED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=0.1.3.1nb3
+BUILDLINK_API_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=2.1.0.0
 BUILDLINK_PKGSRCDIR.hs-primitive-unlifted?=	../../devel/hs-primitive-unlifted
 
 .include "../../devel/hs-primitive/buildlink3.mk"
