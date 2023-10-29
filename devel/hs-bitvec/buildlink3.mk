8842740278ec4a11d61bd40712a18922d3c152e1
pho <pho@pkgsrc.org>
2023-10-29 10:07:00 +0000

    devel/hs-bitvec: Update to 1.1.5.0
    1.1.5.0
    * Make zipBits unconditionally strict in its second bit vector argument
      (thanks to @treeowl).
    * Add simd flag (enabled by default) to use a C SIMD implementation for
      zipBits, invertBits, countBits, bitIndex, nthBitIndex, selectBits,
      excludeBits, reverseBits (thanks to @konsumlamm).
    * Decomission libgmp flag.
    
    1.1.4.0
    * Include Data.Bit.Gmp only if libgmp flag is set.
    * Tweak inlining pragmas to inline less aggressively.

--- devel/hs-bitvec/buildlink3.mk
+++ devel/hs-bitvec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/29 10:07:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-bitvec
 
 .if !defined(HS_BITVEC_BUILDLINK3_MK)
 HS_BITVEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bitvec+=	hs-bitvec>=1.1.3.0
-BUILDLINK_ABI_DEPENDS.hs-bitvec?=		hs-bitvec>=1.1.3.0nb2
+BUILDLINK_API_DEPENDS.hs-bitvec+=	hs-bitvec>=1.1.5.0
+BUILDLINK_ABI_DEPENDS.hs-bitvec?=	hs-bitvec>=1.1.5.0
 BUILDLINK_PKGSRCDIR.hs-bitvec?=		../../devel/hs-bitvec
 
 .include "../../devel/hs-primitive/buildlink3.mk"
