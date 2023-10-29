1b544c6ca075a08fdb5ff12e387466d5c358159f
pho <pho@pkgsrc.org>
2023-10-29 18:28:37 +0000

    time/hs-text-iso8601: import hs-text-iso8601-0.1
    Converting time to and from IS0 8601 text. Specifically the RFC3339
    profile.

---
+++ time/hs-text-iso8601/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/29 18:28:37 pho Exp $
+
+BUILDLINK_TREE+=	hs-text-iso8601
+
+.if !defined(HS_TEXT_ISO8601_BUILDLINK3_MK)
+HS_TEXT_ISO8601_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-text-iso8601+=	hs-text-iso8601>=0.1
+BUILDLINK_ABI_DEPENDS.hs-text-iso8601+=	hs-text-iso8601>=0.1
+BUILDLINK_PKGSRCDIR.hs-text-iso8601?=	../../time/hs-text-iso8601
+
+.include "../../math/hs-integer-conversion/buildlink3.mk"
+.include "../../time/hs-time-compat/buildlink3.mk"
+.endif	# HS_TEXT_ISO8601_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-text-iso8601
