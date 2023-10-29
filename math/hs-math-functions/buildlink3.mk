7a4b7329adcf0dddeac99afa0ce4370ab3af43ae
pho <pho@pkgsrc.org>
2023-10-29 07:16:24 +0000

    math/hs-math-functions: Update to 0.3.4.3
    Changes in 0.3.4.3
    * Support for QuickCheck >= 2.14. Test no longer fail
    * Support for GHC<8.0 dropped

--- math/hs-math-functions/buildlink3.mk
+++ math/hs-math-functions/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/29 07:16:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-math-functions
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-math-functions
 HS_MATH_FUNCTIONS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.4
-BUILDLINK_ABI_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.4.2nb7
+BUILDLINK_ABI_DEPENDS.hs-math-functions+=	hs-math-functions>=0.3.4.3
 BUILDLINK_PKGSRCDIR.hs-math-functions?=		../../math/hs-math-functions
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
