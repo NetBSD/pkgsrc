bb769672aeff73cb56a42e7afdd9629ddd95f211
pho <pho@pkgsrc.org>
2023-10-29 09:52:45 +0000

    devel/hs-unliftio: Update to 0.2.25.0
    0.2.25.0
        Add UnliftIO.Exception.Lens
    
    0.2.24.0
        Add UnliftIO.STM.writeTMVar
        Add UnliftIO.STM.stateTVar

--- devel/hs-unliftio/buildlink3.mk
+++ devel/hs-unliftio/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/29 09:52:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-unliftio
 
 .if !defined(HS_UNLIFTIO_BUILDLINK3_MK)
 HS_UNLIFTIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unliftio+=	hs-unliftio>=0.2.23
-BUILDLINK_ABI_DEPENDS.hs-unliftio+=	hs-unliftio>=0.2.23.0nb2
+BUILDLINK_API_DEPENDS.hs-unliftio+=	hs-unliftio>=0.2.25
+BUILDLINK_ABI_DEPENDS.hs-unliftio+=	hs-unliftio>=0.2.25.0
 BUILDLINK_PKGSRCDIR.hs-unliftio?=	../../devel/hs-unliftio
 
 .include "../../devel/hs-async/buildlink3.mk"
