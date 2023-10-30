ff3ead7c9f121c3e8396a25fdf0e57d87d1b2b21
pho <pho@pkgsrc.org>
2023-10-30 09:11:41 +0000

    converters/hs-aeson: Update to 2.2.1.0
    The change log is too long to paste here. See
    https://hackage.haskell.org/package/aeson-2.2.1.0/changelog

--- converters/hs-aeson/buildlink3.mk
+++ converters/hs-aeson/buildlink3.mk
@@ -1,21 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.30 2023/10/09 04:54:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2023/10/30 09:11:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-aeson
 
 .if !defined(HS_AESON_BUILDLINK3_MK)
 HS_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=2.1.1
-BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=2.1.1.0nb2
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=2.2.1
+BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=2.2.1.0
 BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 
-.include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../devel/hs-base-compat-batteries/buildlink3.mk"
 .include "../../devel/hs-data-fix/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-generically/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../math/hs-integer-conversion/buildlink3.mk"
+.include "../../math/hs-integer-logarithms/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
 .include "../../devel/hs-OneTuple/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-QuickCheck/buildlink3.mk"
@@ -23,6 +24,7 @@ BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 .include "../../devel/hs-semialign/buildlink3.mk"
 .include "../../devel/hs-strict/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../time/hs-text-iso8601/buildlink3.mk"
 .include "../../textproc/hs-text-short/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
 .include "../../devel/hs-these/buildlink3.mk"
