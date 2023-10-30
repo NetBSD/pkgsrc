69888ad9a731c26ebfda61bf0603ca19af2d366b
pho <pho@pkgsrc.org>
2023-10-30 18:12:34 +0000

    devel/hs-pandoc-lua-marshal: Update to 0.2.2
    0.2.2 - Released 2023-03-15.
    * Add __tostring metamethods to Blocks and Inlines.
    
    0.2.1.1 - Released 2023-03-13.
    * The version constraints for hslua packages have been relaxed; other
      changes in hslua 2.3.* do not affect this package.
    
    0.2.1 - Released 2023-02-11.
    * All userdata types have been given a __tojson metamethod. The methods
      return the default JSON representations of AST objects.

--- devel/hs-pandoc-lua-marshal/buildlink3.mk
+++ devel/hs-pandoc-lua-marshal/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/09 04:54:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/10/30 18:12:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-lua-marshal
 
 .if !defined(HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK)
 HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.0
-BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.0nb2
+BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-pandoc-lua-marshal?=	../../devel/hs-pandoc-lua-marshal
 
+.include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../lang/hs-hslua/buildlink3.mk"
 .include "../../lang/hs-hslua-list/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
