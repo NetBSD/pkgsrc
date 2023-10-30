3d48f0fa4563fa1eecffe21443b1fedc9aefb039
pho <pho@pkgsrc.org>
2023-10-30 09:45:10 +0000

    security/hs-crypton-x509-store: import hs-crypton-x509-store-1.6.9
    X.509 collection accessing and storing methods for certificate, crl,
    exception list

---
+++ security/hs-crypton-x509-store/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 09:45:10 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypton-x509-store
+
+.if !defined(HS_CRYPTON_X509_STORE_BUILDLINK3_MK)
+HS_CRYPTON_X509_STORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypton-x509-store+=	hs-crypton-x509-store>=1.6.9
+BUILDLINK_ABI_DEPENDS.hs-crypton-x509-store+=	hs-crypton-x509-store>=1.6.9
+BUILDLINK_PKGSRCDIR.hs-crypton-x509-store?=	../../security/hs-crypton-x509-store
+
+.include "../../devel/hs-asn1-encoding/buildlink3.mk"
+.include "../../devel/hs-asn1-types/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../security/hs-crypton-x509/buildlink3.mk"
+.include "../../security/hs-pem/buildlink3.mk"
+.endif	# HS_CRYPTON_X509_STORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypton-x509-store
