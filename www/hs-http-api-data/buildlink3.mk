aed04254a9616d8598005eb1bffba4b881ddd521
pho <pho@pkgsrc.org>
2023-10-30 09:24:44 +0000

    www/hs-http-api-data: Update to 0.6
    0.6
    * Use text-iso8601 to parse and serialise time types. (Instead of
      attoparsec-iso8601). Due this change some formats are slightly changed:
      - Space between timezone is not accepted
      - Timezone offset can be between -23:59..23:59
      - Timezone offset is output with colon between hours and minutes
    * Require at least GHC-8.2

--- www/hs-http-api-data/buildlink3.mk
+++ www/hs-http-api-data/buildlink3.mk
@@ -1,21 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:58 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/30 09:24:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-http-api-data
 
 .if !defined(HS_HTTP_API_DATA_BUILDLINK3_MK)
 HS_HTTP_API_DATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.5
-BUILDLINK_ABI_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.5nb2
+BUILDLINK_API_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.6
+BUILDLINK_ABI_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.6
 BUILDLINK_PKGSRCDIR.hs-http-api-data?=		../../www/hs-http-api-data
 
-.include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../textproc/hs-attoparsec-iso8601/buildlink3.mk"
-.include "../../devel/hs-base-compat/buildlink3.mk"
 .include "../../www/hs-cookie/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../time/hs-text-iso8601/buildlink3.mk"
 .include "../../time/hs-time-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-uuid-types/buildlink3.mk"
