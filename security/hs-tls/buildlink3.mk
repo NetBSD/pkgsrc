c5a159901bcb51f6cb9f82b53db0f186a310fa39
pho <pho@pkgsrc.org>
2023-10-30 14:50:27 +0000

    security/hs-tls: Update to 1.9.0
    Version 1.9.0
    * BREAKING CHANGE: The type of the Error_Protocol constructor of TLSError
      has changed. The "warning" case has been split off into a new
      Error_Protocol_Warning constructor. #460
    
    Version 1.8.0
    * BREAKING CHANGE: Remove Exception instance for TLSError. The library now
      throws TLSException only. If you need to change your code, please refer
      to this example first. #457
    
    Version 1.7.1
    * NOP on UserCanceled event #454
    
    Version 1.7.0
    * Major version up because "crypton" is used instead of "cryptonite"

--- security/hs-tls/buildlink3.mk
+++ security/hs-tls/buildlink3.mk
@@ -1,25 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/10/09 04:54:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/10/30 14:50:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-tls
 
 .if !defined(HS_TLS_BUILDLINK3_MK)
 HS_TLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tls+=	hs-tls>=1.6.0
-BUILDLINK_ABI_DEPENDS.hs-tls+=	hs-tls>=1.6.0nb2
+BUILDLINK_API_DEPENDS.hs-tls+=	hs-tls>=1.9.0
+BUILDLINK_ABI_DEPENDS.hs-tls+=	hs-tls>=1.9.0
 BUILDLINK_PKGSRCDIR.hs-tls?=	../../security/hs-tls
 
 .include "../../devel/hs-asn1-encoding/buildlink3.mk"
 .include "../../devel/hs-asn1-types/buildlink3.mk"
 .include "../../devel/hs-async/buildlink3.mk"
 .include "../../devel/hs-cereal/buildlink3.mk"
-.include "../../security/hs-cryptonite/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../security/hs-crypton-x509/buildlink3.mk"
+.include "../../security/hs-crypton-x509-store/buildlink3.mk"
+.include "../../security/hs-crypton-x509-validation/buildlink3.mk"
 .include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../time/hs-hourglass/buildlink3.mk"
 .include "../../devel/hs-memory/buildlink3.mk"
-.include "../../security/hs-x509/buildlink3.mk"
-.include "../../security/hs-x509-store/buildlink3.mk"
-.include "../../security/hs-x509-validation/buildlink3.mk"
+.include "../../time/hs-unix-time/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
 .endif	# HS_TLS_BUILDLINK3_MK
 
