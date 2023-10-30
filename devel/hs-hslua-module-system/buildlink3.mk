a1ea2e09e2e5192286f33807b926448111415b6e
pho <pho@pkgsrc.org>
2023-10-30 12:43:04 +0000

    devel/hs-hslua-module-system: Update to 1.1.0.1
    hslua-module-system-1.1.0.1 - Released 2023-03-26.
    * Improve doc strings.
    
    hslua-module-system-1.1.0
    * Update to hslua-2.3; this includes the addition of type initializers to
      the module and type specifiers to the fields.
    
    hslua-module-system-1.0.3 - Released 2023-02-14.
    * Added new function cputime and field cputime_precision, e.g. for
      benchmarking.

--- devel/hs-hslua-module-system/buildlink3.mk
+++ devel/hs-hslua-module-system/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/30 12:43:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-system
 
 .if !defined(HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK)
 HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.0.2nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.1.0.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-system?=	../../devel/hs-hslua-module-system
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
