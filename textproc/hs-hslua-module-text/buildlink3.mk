4ded5a3346dd99c9cf42025ce1bce2374bdf3cc3
pho <pho@pkgsrc.org>
2023-10-30 12:37:27 +0000

    textproc/hs-hslua-module-text: Update to 1.1.0.1
    hslua-module-text-1.1.0.1 - Released 2023-03-26.
    * Improved doc strings.
    
    hslua-module-text-1.1.0 - Released 2023-03-13.
    * Update to hslua-2.3; this includes the addition of type initializers to
      the module and type specifiers to the fields.
    
    hslua-module-text-1.0.3.1 - Released 2023-01-06.
    * Generalized a test to avoid failures with stack nightly.
    * Added GHC 9.4 to test matrix.

--- textproc/hs-hslua-module-text/buildlink3.mk
+++ textproc/hs-hslua-module-text/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/30 12:37:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-text
 
 .if !defined(HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK)
 HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=1.0.3nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=1.1.0.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-text?=	../../textproc/hs-hslua-module-text
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
