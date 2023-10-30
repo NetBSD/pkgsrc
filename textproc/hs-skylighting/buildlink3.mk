38af44b3dd70f0d233dbe19231e5a07e3eabd9e7
pho <pho@pkgsrc.org>
2023-10-30 18:29:39 +0000

    textproc/hs-skylighting: Update to 0.14
    0.14
    * Add rWeakDeliminators field to Rule. [API change]
    * Make WordDetect sensitive to weakDeliminator. This fixes parsing of
      floats beginning with '0.' in C (#174).
    * Add debiancontrol syntax (#173).
    
    0.13.4.1
    * Update syntax definitions: ada, bash, cmake, css, html, isocpp, java,
      javascript, kotlin, latex, makefile, markdown, php, python, qml, r, sass,
      scss, typescript, zsh.
    * Don't require word boundary at end of Int, Float, HlCHex, HlCOct
      (#170). KDE does not. This fixes things like 7L in R.
    
    0.13.4
    * Add dosbat syntax (MS DOS batch file) (#169).
    * Derive Bounded Instance for TokenType (#168, Pavan Pikhi). Add Bounded to
      the derived instances for the TokenType type. This allows consumers to
      use [minBound .. maxBound] to generate a list of all token types when
      writing a Style.
    * Require xml-conduit >= 1.9.1.3. This fixes a bug that prevents parsing
      certain DOCTYPE declarations, e.g. in agda.xml.
    * Updated cmake syntax definition.
    
    0.13.3
    * Add gap language (#167).
    * Update syntax definitions.
    * Add patches for agda.xml and dtd.xml, to wor around a bug in xml-conduit:
      https://github.com/snoyberg/xml/pull/187
    * Store compiled regexes in RE (#166, Jonathan Coates). This changes the RE
      type to (lazily) compile the regex when constructed, rather than in the
      tokenizer. This allows us to avoid re-compiling regexes for each separate
      tokenize call, instead sharing them globally. We try to hide the
      internals of this, exposing the previous interface (RE { reString,
      reCaseSensitive }) with pattern synonyms.
    * ConTeXt: fix handling of spaces in non-normal tokens (Albert
      Krewinkel). This ensures that multiple spaces won't be collapsed into a
      single space.
    
    0.13.2.1
    * Update tango style for new token types (#164). The original tango style
      didn't have colors defined for many token types that have been added
      since it was added. This commit updates the style to support them. Thanks
      to @danbraswell for providing the values needed.

--- textproc/hs-skylighting/buildlink3.mk
+++ textproc/hs-skylighting/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/10/09 04:54:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/10/30 18:29:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-skylighting
 
 .if !defined(HS_SKYLIGHTING_BUILDLINK3_MK)
 HS_SKYLIGHTING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.13.2
-BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.13.2nb2
+BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.14
+BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.14
 BUILDLINK_PKGSRCDIR.hs-skylighting?=	../../textproc/hs-skylighting
 
 .include "../../textproc/hs-skylighting-core/buildlink3.mk"
