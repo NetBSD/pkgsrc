15bbd6147546b27167ed761afbadbf23ed938dd9
pho <pho@pkgsrc.org>
2023-10-30 07:45:17 +0000

    math/hs-semigroupoids: Update to 6.0.0.1
    The change log is too long to paste here. See
    https://hackage.haskell.org/package/semigroupoids-6.0.0.1/changelog

--- math/hs-semigroupoids/buildlink3.mk
+++ math/hs-semigroupoids/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:41 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/30 07:45:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.7
-BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=5.3.7nb4
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=6.0.0
+BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=6.0.0.1
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=		../../math/hs-semigroupoids
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
