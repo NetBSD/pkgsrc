38d5ff98a88ab61341f6b1ee9c1e25621cce0287
pho <pho@pkgsrc.org>
2023-10-30 14:46:29 +0000

    security/hs-crypton-x509-system: import hs-crypton-x509-system-1.6.7
    System X.509 handling for accessing operating system dependents store and
    other storage methods

---
+++ security/hs-crypton-x509-system/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 14:46:29 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypton-x509-system
+
+.if !defined(HS_CRYPTON_X509_SYSTEM_BUILDLINK3_MK)
+HS_CRYPTON_X509_SYSTEM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypton-x509-system+=	hs-crypton-x509-system>=1.6.7
+BUILDLINK_ABI_DEPENDS.hs-crypton-x509-system+=	hs-crypton-x509-system>=1.6.7
+BUILDLINK_PKGSRCDIR.hs-crypton-x509-system?=	../../security/hs-crypton-x509-system
+
+.include "../../security/hs-crypton-x509/buildlink3.mk"
+.include "../../security/hs-crypton-x509-store/buildlink3.mk"
+.include "../../security/hs-pem/buildlink3.mk"
+.endif	# HS_CRYPTON_X509_SYSTEM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypton-x509-system
