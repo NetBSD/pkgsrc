552874c446b78a97707929b639d20c61cc652b5c
pho <pho@pkgsrc.org>
2023-10-30 17:31:20 +0000

    lang/hs-hslua: Update to 2.3.0
    hslua-2.3.0 -Released 2023-03-13.
    * Require version 2.3.* of HsLua packages: hslua-core, hslua-marshalling,
      hslua-objectorientation, hslua-packaging, hslua-aeson, hslua-classes.
    * Include hslua-typing, re-export HsLua.Typing.

--- lang/hs-hslua/buildlink3.mk
+++ lang/hs-hslua/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/30 17:31:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua
 
 .if !defined(HS_HSLUA_BUILDLINK3_MK)
 HS_HSLUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua+=	hs-hslua>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua+=	hs-hslua>=2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua+=	hs-hslua>=2.3.0
+BUILDLINK_ABI_DEPENDS.hs-hslua+=	hs-hslua>=2.3.0
 BUILDLINK_PKGSRCDIR.hs-hslua?=		../../lang/hs-hslua
 
 .include "../../converters/hs-hslua-aeson/buildlink3.mk"
@@ -15,6 +15,7 @@ BUILDLINK_PKGSRCDIR.hs-hslua?=		../../lang/hs-hslua
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
 .include "../../lang/hs-hslua-objectorientation/buildlink3.mk"
 .include "../../lang/hs-hslua-packaging/buildlink3.mk"
+.include "../../lang/hs-hslua-typing/buildlink3.mk"
 .endif	# HS_HSLUA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hslua
