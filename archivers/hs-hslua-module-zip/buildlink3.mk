677deb43edc0ec7b6e8ae39fc9505c6159707546
pho <pho@pkgsrc.org>
2023-10-30 09:17:31 +0000

    archivers/hs-hslua-module-zip: Update to 1.1.0
    No change log is provided by the upstream.

--- archivers/hs-hslua-module-zip/buildlink3.mk
+++ archivers/hs-hslua-module-zip/buildlink3.mk
@@ -1,18 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/30 09:17:31 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-zip
 
 .if !defined(HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK)
 HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.0.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.0.0nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.1.0
 BUILDLINK_PKGSRCDIR.hs-hslua-module-zip?=	../../archivers/hs-hslua-module-zip
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
 .include "../../lang/hs-hslua-list/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
 .include "../../lang/hs-hslua-packaging/buildlink3.mk"
+.include "../../lang/hs-hslua-typing/buildlink3.mk"
 .include "../../archivers/hs-zip-archive/buildlink3.mk"
 .endif	# HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK
 
