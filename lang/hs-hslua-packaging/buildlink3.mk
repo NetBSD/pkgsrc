5aa99f4bb7907c5b64a82a9336fec259962bffed
pho <pho@pkgsrc.org>
2023-10-30 09:15:06 +0000

    lang/hs-hslua-packaging: Update to 2.3.0
    hslua-packaging-2.3.0 - Released 2023-03-13.
    * Type initializers as part of Module records. This allows to associate
      types with a module. For performance reasons, the types are not
      initialized when the module is pushed, but only on first use. However,
      the documentation Lua object for each module now has an additional field
      types. The new field contains a function that returns the names of all
      associated types. Calling the function will also initialize these types,
      thereby making the respective metatables available in the registry.
    * Field records now have an additional fieldType entry. [API change]
    * The pushUD function is now specialized to documented types.
    * Export initType. The function ensures that the metatable of a type has
      been fully initialized. This can be helpful when the default method of
      lazy initialization is not desired, e.g. when the type object is to be
      inspected or extended.
    * Re-export udDocs, udTypeSpec, allowing to generate typing info for
      userdata classes.

--- lang/hs-hslua-packaging/buildlink3.mk
+++ lang/hs-hslua-packaging/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:38 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 09:15:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-packaging
 
 .if !defined(HS_HSLUA_PACKAGING_BUILDLINK3_MK)
 HS_HSLUA_PACKAGING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.3.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.3.0
 BUILDLINK_PKGSRCDIR.hs-hslua-packaging?=	../../lang/hs-hslua-packaging
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
 .include "../../lang/hs-hslua-objectorientation/buildlink3.mk"
+.include "../../lang/hs-hslua-typing/buildlink3.mk"
 .endif	# HS_HSLUA_PACKAGING_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hslua-packaging
