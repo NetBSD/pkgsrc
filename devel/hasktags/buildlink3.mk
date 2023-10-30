b9c6ac4fc8797646434b68579a5943d4bde357a1
pho <pho@pkgsrc.org>
2023-10-30 08:09:57 +0000

    devel/hasktags: Update to 0.73.0
    No change log is provided by the upstream.

--- devel/hasktags/buildlink3.mk
+++ devel/hasktags/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/30 08:09:57 pho Exp $
 
 BUILDLINK_TREE+=	hasktags
 
 .if !defined(HASKTAGS_BUILDLINK3_MK)
 HASKTAGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hasktags+=	hasktags>=0.72.0
-BUILDLINK_ABI_DEPENDS.hasktags+=	hasktags>=0.72.0nb2
+BUILDLINK_API_DEPENDS.hasktags+=	hasktags>=0.73.0
+BUILDLINK_ABI_DEPENDS.hasktags+=	hasktags>=0.73.0nb2
 BUILDLINK_PKGSRCDIR.hasktags?=		../../devel/hasktags
 
 .include "../../converters/hs-json/buildlink3.mk"
