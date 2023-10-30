9d3cb273520bc37f600b379d8498a5f4a7c9312d
pho <pho@pkgsrc.org>
2023-10-30 10:23:47 +0000

    devel/hs-tasty-hunit: Update to 0.10.1
    Version 0.10.1
    * Provide an explicit implementation of displayException in instance
      Exception HUnitFailure (#330).

--- devel/hs-tasty-hunit/buildlink3.mk
+++ devel/hs-tasty-hunit/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/09 04:54:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2023/10/30 10:23:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty-hunit
 
 .if !defined(HS_TASTY_HUNIT_BUILDLINK3_MK)
 HS_TASTY_HUNIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.0
-BUILDLINK_ABI_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.0.3nb7
+BUILDLINK_API_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.1
+BUILDLINK_ABI_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.1
 BUILDLINK_PKGSRCDIR.hs-tasty-hunit?=	../../devel/hs-tasty-hunit
 .endif	# HS_TASTY_HUNIT_BUILDLINK3_MK
 
