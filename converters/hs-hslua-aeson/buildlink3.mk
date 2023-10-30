e762b2be56f56625c29810252513cd253496dcab
pho <pho@pkgsrc.org>
2023-10-30 13:24:57 +0000

    converters/hs-hslua-aeson: Update to 2.3.0.1
    hslua-aeson-2.3.0.1 - Released 2023-03-13.
    
    * Relax upper bound for hslua-marshalling, allow version 2.3.
    
    hslua-aeson-2.3.0 - Released 2023-02-21.
    
    * The peekValue peeker now checks for a __toaeson metafield or __tojson
      metamethod and uses them to compute the Value of an object:
    
    * The __toaeson metafield, if set, must be a function pushed via
      pushToAeson. That function is called on a given object, and the returned
      Value becomes the result of calling peekValue.
    
      Likewise, the __tojson metamethod must be a function that returns a valid
      JSON string. The result in that case is the decoded string.
    
      If both, __toaeson and __tojson are set, then __toaeson takes precedent.
    
    * The test suite now has tasty-hunit as an additional dependency.

--- converters/hs-hslua-aeson/buildlink3.mk
+++ converters/hs-hslua-aeson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 13:24:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-aeson
 
 .if !defined(HS_HSLUA_AESON_BUILDLINK3_MK)
 HS_HSLUA_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-aeson+=	hs-hslua-aeson>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-aeson+=	hs-hslua-aeson>=2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-aeson+=	hs-hslua-aeson>=2.3.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-aeson+=	hs-hslua-aeson>=2.3.0.1
 BUILDLINK_PKGSRCDIR.hs-hslua-aeson?=	../../converters/hs-hslua-aeson
 
 .include "../../converters/hs-aeson/buildlink3.mk"
