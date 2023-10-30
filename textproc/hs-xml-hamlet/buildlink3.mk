38de657816c6ae73a3d8b74a858d8fd20b8222ad
pho <pho@pkgsrc.org>
2023-10-30 13:36:59 +0000

    textproc/hs-xml-hamlet: import hs-xml-hamlet-0.5.0.2
    Hamlet-style quasiquoter for XML content

---
+++ textproc/hs-xml-hamlet/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 13:36:59 pho Exp $
+
+BUILDLINK_TREE+=	hs-xml-hamlet
+
+.if !defined(HS_XML_HAMLET_BUILDLINK3_MK)
+HS_XML_HAMLET_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-xml-hamlet+=	hs-xml-hamlet>=0.5.0
+BUILDLINK_ABI_DEPENDS.hs-xml-hamlet+=	hs-xml-hamlet>=0.5.0.2
+BUILDLINK_PKGSRCDIR.hs-xml-hamlet?=	../../textproc/hs-xml-hamlet
+
+.include "../../textproc/hs-shakespeare/buildlink3.mk"
+.include "../../textproc/hs-xml-conduit/buildlink3.mk"
+.endif	# HS_XML_HAMLET_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-xml-hamlet
