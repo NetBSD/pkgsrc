c5d8ad0c2369211ea1373251a5d1771dbde48813
pho <pho@pkgsrc.org>
2023-10-30 10:53:51 +0000

    math/hs-free: Update to 5.2
    5.2 [2023.03.12]
    * Drop support for GHC 7.10 and earlier.
    * Drop redundant Monad constraints on many functions and instances. These
      constraints were only present for compatibility with pre-7.10 versions of
      GHC, which free no longer supports.
    * Add Eq, Eq1, Ord, Ord1, and Foldable instances for Ap in
      Control.Applicative.Free.
    * Switch out bifunctors dependency for bifunctor-classes-compat.

--- math/hs-free/buildlink3.mk
+++ math/hs-free/buildlink3.mk
@@ -1,21 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/30 10:53:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-free
 
 .if !defined(HS_FREE_BUILDLINK3_MK)
 HS_FREE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-free+=	hs-free>=5.1.10
-BUILDLINK_ABI_DEPENDS.hs-free+=	hs-free>=5.1.10nb2
+BUILDLINK_API_DEPENDS.hs-free+=	hs-free>=5.2
+BUILDLINK_ABI_DEPENDS.hs-free+=	hs-free>=5.2
 BUILDLINK_PKGSRCDIR.hs-free?=	../../math/hs-free
 
 .include "../../math/hs-comonad/buildlink3.mk"
 .include "../../math/hs-distributive/buildlink3.mk"
 .include "../../devel/hs-indexed-traversable/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
 .include "../../math/hs-semigroupoids/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
-.include "../../math/hs-profunctors/buildlink3.mk"
 .endif	# HS_FREE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-free
