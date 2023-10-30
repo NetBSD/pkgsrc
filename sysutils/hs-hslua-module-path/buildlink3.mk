e29be58b05e5e0f76613f40ff3d99cbf9d35fe00
pho <pho@pkgsrc.org>
2023-10-30 12:39:21 +0000

    sysutils/hs-hslua-module-path: Update to 1.1.0
    hslua-module-path-1.1.0 - Released 2023-03-13.
    * Update to hslua-2.3; this includes the addition of type initializers to
      the module and type specifiers to the fields.
    * Fixed tests for make_relative on Windows.

--- sysutils/hs-hslua-module-path/buildlink3.mk
+++ sysutils/hs-hslua-module-path/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/10/09 04:54:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/10/30 12:39:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-path
 
 .if !defined(HS_HSLUA_MODULE_PATH_BUILDLINK3_MK)
 HS_HSLUA_MODULE_PATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-path+=	hs-hslua-module-path>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-path+=	hs-hslua-module-path>=1.0.3nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-path+=	hs-hslua-module-path>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-path+=	hs-hslua-module-path>=1.1.0
 BUILDLINK_PKGSRCDIR.hs-hslua-module-path?=	../../sysutils/hs-hslua-module-path
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
