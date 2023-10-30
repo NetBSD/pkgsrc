11cc8bc090973658e17bfabe06ef2c33da1841d1
bsiegert <bsiegert@pkgsrc.org>
2023-10-30 19:04:35 +0000

    go-tools: update to 0.14.0
    New tool: gonew -- starts a new Go module by copying a template module.

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,12 +1,12 @@
-# $NetBSD: go-modules.mk,v 1.2 2023/04/16 18:23:00 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.3 2023/10/30 19:04:35 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.10.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.10.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.9.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.9.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.1.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.1.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.7.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.7.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.16.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.16.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.4.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.4.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.13.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.13.0.zip
