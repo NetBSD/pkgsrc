c4e4c3628efb0e3fb3d96d307941bd029623f9c7
pho <pho@pkgsrc.org>
2023-10-30 10:29:45 +0000

    textproc/hs-attoparsec-aeson: import hs-attoparsec-aeson-2.2.0.1
    Parsing of aeson's Value with attoparsec, originally from aeson.

---
+++ textproc/hs-attoparsec-aeson/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 10:29:45 pho Exp $
+
+BUILDLINK_TREE+=	hs-attoparsec-aeson
+
+.if !defined(HS_ATTOPARSEC_AESON_BUILDLINK3_MK)
+HS_ATTOPARSEC_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-attoparsec-aeson+=	hs-attoparsec-aeson>=2.2.0
+BUILDLINK_ABI_DEPENDS.hs-attoparsec-aeson+=	hs-attoparsec-aeson>=2.2.0.1
+BUILDLINK_PKGSRCDIR.hs-attoparsec-aeson?=	../../textproc/hs-attoparsec-aeson
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../math/hs-integer-conversion/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_ATTOPARSEC_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-attoparsec-aeson
