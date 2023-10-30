142dd10600fed29d8043272139e6374ca47ee5e3
pho <pho@pkgsrc.org>
2023-10-30 14:30:57 +0000

    security/hs-crypton-x509-validation: import hs-crypton-x509-validation-1.6.12
    X.509 Certificate and CRL validation.

---
+++ security/hs-crypton-x509-validation/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 14:30:57 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypton-x509-validation
+
+.if !defined(HS_CRYPTON_X509_VALIDATION_BUILDLINK3_MK)
+HS_CRYPTON_X509_VALIDATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypton-x509-validation+=	hs-crypton-x509-validation>=1.6.12
+BUILDLINK_ABI_DEPENDS.hs-crypton-x509-validation+=	hs-crypton-x509-validation>=1.6.12
+BUILDLINK_PKGSRCDIR.hs-crypton-x509-validation?=	../../security/hs-crypton-x509-validation
+
+.include "../../devel/hs-asn1-encoding/buildlink3.mk"
+.include "../../devel/hs-asn1-types/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../security/hs-crypton-x509/buildlink3.mk"
+.include "../../security/hs-crypton-x509-store/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../time/hs-hourglass/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../security/hs-pem/buildlink3.mk"
+.endif	# HS_CRYPTON_X509_VALIDATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypton-x509-validation
