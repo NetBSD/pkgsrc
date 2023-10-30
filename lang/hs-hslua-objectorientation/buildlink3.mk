ad983ee9a2d59b1883dd3b6410ecf8309e5ea238
pho <pho@pkgsrc.org>
2023-10-30 03:00:21 +0000

    lang/hs-hslua-objectorientation: Update to 2.3.0
    hslua-objectorientation-2.3.0 - Released 2023-03-13.
    
    * Export all constructors and functions of type Property.
    
    * Renamed peekUD to peekUDGeneric and pushUD to pushUDGeneric. Functions
      with the old names are now now defined hslua-packaging.
    
    * Hook for udtype metatable initializer. The function pushUDGeneric takes
      an additional hook parameter. The hook operation can be used to perform
      additional setup operations, e.g., for documentation.
    
      The old pushUD function can be recovered with
    
        pushUD = pushUDGeneric (\_ -> pure ())
    
      The hslua-packaging now exports a pushUD functions that is specialized to documented types.
    
    * Export new function initTypeGeneric: The function ensures that a type's
      metatable is initialized and available from the registry. Just like with
      pushUDGeneric, a hook can be used to augment the initialization.
    
    * Type info for properties: Properties are amended with information on the
      property's type. The functions property, possibleProperty, and readonly
      each now come with typed version property', possibleProperty', and
      readonly'. This allows to specify the type of a property value.
    
    * Functions for object typing info: The functions udDocs and udTypeSpec are
      added, enabling the generation of typing information for UDType objects.

--- lang/hs-hslua-objectorientation/buildlink3.mk
+++ lang/hs-hslua-objectorientation/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 03:00:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-objectorientation
 
 .if !defined(HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK)
 HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.2.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.3.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.3.0
 BUILDLINK_PKGSRCDIR.hs-hslua-objectorientation?=	../../lang/hs-hslua-objectorientation
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
 .include "../../lang/hs-hslua-marshalling/buildlink3.mk"
+.include "../../lang/hs-hslua-typing/buildlink3.mk"
 .endif	# HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hslua-objectorientation
