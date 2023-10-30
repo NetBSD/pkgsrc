8eef414754b485b9b5904b446fbd19811b6b55f0
pho <pho@pkgsrc.org>
2023-10-30 06:28:51 +0000

    devel/ormolu: Update to 0.7.3.0
    Ormolu 0.7.3.0
    * Switched to ghc-lib-parser-9.8, with the following new syntactic features:
      - ExtendedLiterals: 123#Int8 is a literal of type Int8#. (disabled by
        default)
      - TypeAbstractions: @k-binders in data type declarations (enabled by
        default)
      - GHC proposal #134: deprecating/warning about exports
      - GHC proposal #541: warning categories
    
    Ormolu 0.7.2.0
    * Preserve necessary braces for final function arguments. Issue 1044.
    * Put "this" PackageImports at the end. Issue 1048.
    * Format parenthesized operators starting with a # correctly in the
      presence of UnboxedSums. Issue 1062.
    * Fix false positives in AST diffing related to empty Haddock comments in
      data declarations. Issue 1065.
    
    Ormolu 0.7.1.0
    * Include base fixity information when formatting a Haskell file that's not
      mentioned in an existing cabal file. Issue 1032
    * Update displayException for OrmoluException to pretty print the
      exception. PR 1031.
    * Ormolu is now aware of more common module re-exports by default.
    * Support explicit mention of target package name in module
      re-exports. Even if the exported package is not specified as a direct
      dependency of the component being formatted it will still be taken into
      account correctly. Issue 1037.
    * Ormolu no longer fails when CPP directly follows the import section (a
      regression introduced in 0.7.0.0). Issue 1040.
    
    Ormolu 0.7.0.0
    * Inference of operator fixity information is now more precise and takes
      into account the import section of the module being formatted. Issue 892
      and issue 929.
    * Ormolu can now be made aware of module re-exports through either special
      declarations in .ormolu files (see the readme for a description of the
      syntax), or on the command line with the --reexport/-r option. Issue
      1017.
    * Ormolu now looks for .ormolu files independently of .cabal files. This
      means that it is now possible to have one .ormolu file for multiple Cabal
      packages. Issue 1019.
    * Consistently format do blocks/cases/MultiWayIfs with 4 spaces if and only
      if they occur as the applicand. Issue 1002 and issue 730.
    * Support the (deprecated) DatatypeContexts extension to avoid
      surprises. Issue 1012.
    * Don't let comments escape from empty export lists. Issue 906.
    * Format \cases with multiple patterns across multiple lines
      correctly. Issue 1025.
    
    Ormolu 0.6.0.1
    * Fix false positives in AST diffing related to UnicodeSyntax. PR 1009.
    
    Ormolu 0.6.0.0
    * Haddocks attached to arguments of a data constructor are now formatted in
      the pipe style (rather than the caret style), consistent with everything
      else. As a consequence, now Ormolu's output will be deemed invalid by the
      Haddock shipped with GHC <9.0. Issue 844 and issue 828.
    * Insert space before char literals in ticked promoted constructs when
      necessary. Issue 1000.
    * Switched to ghc-lib-parser-9.6:
      - Extended OverloadedLabels: #Foo, #3, #"Hello there".
      - Also, it is now disabled by default, as it causes e.g. a#b to be parsed
        differently.
      - New extension: TypeData, enabled by default.
      - Parse errors now include error codes, cf. https://errors.haskell.org.
    * Updated to Cabal-syntax-3.10.
    * Now whenever Ormolu fails to parse a .cabal file it also explains why. PR
      999.
    
    Ormolu 0.5.3.0
    * Stop making empty lets move comments. Issue 917.
    * Now .ormolu fixity override files can use both LF and CRLF line
      endings. PR 969.
    * Normalize parentheses around constraints. Issue 264.
    * The ormolu function now consumes Text instead of String due to an
      internal refactoring.
    * Exposed a more complete public API in the Ormolu module. The API is
      supposed to be stable and change according to PVP.
    * Now warnings regarding Ormolu not being able to find .cabal files or
      finding such files but them not mentioning the source file in question
      are only displayed when --debug is used. Printing the warnings by default
      seems to have been confusing, see e.g. Issue 971 and issue 924.

--- devel/ormolu/buildlink3.mk
+++ devel/ormolu/buildlink3.mk
@@ -1,24 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 06:28:51 pho Exp $
 
 BUILDLINK_TREE+=	ormolu
 
 .if !defined(ORMOLU_BUILDLINK3_MK)
 ORMOLU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.5.2
-BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.5.2.0nb2
+BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.7.3
+BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.7.3.0
 BUILDLINK_PKGSRCDIR.ormolu?=	../../devel/ormolu
 
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../textproc/hs-Diff/buildlink3.mk"
-.include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-file-embed/buildlink3.mk"
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
 .include "../../textproc/hs-megaparsec/buildlink3.mk"
 .include "../../devel/hs-memotrie/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
-.include "../../devel/hs-gitrev/buildlink3.mk"
-.include "../../devel/hs-optparse-applicative/buildlink3.mk"
 .endif	# ORMOLU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ormolu
