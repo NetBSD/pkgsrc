89e1bc76e30a2f64e24bc56a50d33d0c86411f49
jperkin <jperkin@pkgsrc.org>
2023-10-30 16:15:32 +0000

    libmysofa: Fix build on SunOS too.

--- audio/libmysofa/patches/patch-src_hrtf_portable__endian.h
+++ audio/libmysofa/patches/patch-src_hrtf_portable__endian.h
@@ -1,10 +1,19 @@
-$NetBSD: patch-src_hrtf_portable__endian.h,v 1.1 2023/10/15 19:33:54 adam Exp $
+$NetBSD: patch-src_hrtf_portable__endian.h,v 1.2 2023/10/30 16:15:32 jperkin Exp $
 
 Fix build on NetBSD.
 https://github.com/hoene/libmysofa/issues/211
 
---- src/hrtf/portable_endian.h.orig	2023-10-15 19:23:14.526442578 +0000
+--- src/hrtf/portable_endian.h.orig	2023-10-15 10:30:14.000000000 +0000
 +++ src/hrtf/portable_endian.h
+@@ -13,7 +13,7 @@
+ 
+ #endif
+ 
+-#if defined(__linux__) || defined(__CYGWIN__)
++#if defined(__linux__) || defined(__CYGWIN__) || defined(__sun)
+ 
+ #	include <endian.h>
+ 
 @@ -41,11 +41,11 @@
  #	define __LITTLE_ENDIAN LITTLE_ENDIAN
  #	define __PDP_ENDIAN    PDP_ENDIAN
