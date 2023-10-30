194ed569d7290e1cad1f1876191092b8cdf2d160
pho <pho@pkgsrc.org>
2023-10-30 13:39:58 +0000

    textproc/hs-skylighting-format-context: Update to 0.1.0.2
    No change log is provided by the upstream.

--- textproc/hs-skylighting-format-context/buildlink3.mk
+++ textproc/hs-skylighting-format-context/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/30 13:39:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-skylighting-format-context
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-skylighting-format-context
 HS_SKYLIGHTING_FORMAT_CONTEXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-skylighting-format-context+=	hs-skylighting-format-context>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-skylighting-format-context+=	hs-skylighting-format-context>=0.1.0.1nb2
+BUILDLINK_ABI_DEPENDS.hs-skylighting-format-context+=	hs-skylighting-format-context>=0.1.0.2
 BUILDLINK_PKGSRCDIR.hs-skylighting-format-context?=	../../textproc/hs-skylighting-format-context
 
 .include "../../textproc/hs-skylighting-core/buildlink3.mk"
