46f5bea01d32a1232ed9fe8e02a509b987c14c1c
pho <pho@pkgsrc.org>
2023-10-30 10:27:01 +0000

    devel/hs-storable-tuple: Update to 0.1
    0.1
        Tuple: implement instances using Record.Tuple

--- devel/hs-storable-tuple/buildlink3.mk
+++ devel/hs-storable-tuple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 10:27:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-storable-tuple
 
 .if !defined(HS_STORABLE_TUPLE_BUILDLINK3_MK)
 HS_STORABLE_TUPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-storable-tuple+=	hs-storable-tuple>=0.0.3
-BUILDLINK_ABI_DEPENDS.hs-storable-tuple+=	hs-storable-tuple>=0.0.3.3nb5
+BUILDLINK_API_DEPENDS.hs-storable-tuple+=	hs-storable-tuple>=0.1
+BUILDLINK_ABI_DEPENDS.hs-storable-tuple+=	hs-storable-tuple>=0.1
 BUILDLINK_PKGSRCDIR.hs-storable-tuple?=		../../devel/hs-storable-tuple
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
