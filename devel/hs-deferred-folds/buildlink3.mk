d4f17131d77f4a958ca68a4384476cf3a4a162cf
pho <pho@pkgsrc.org>
2023-10-30 07:59:16 +0000

    devel/hs-deferred-folds: Update to 0.9.18.5
    No change log is provided by the upstream.

--- devel/hs-deferred-folds/buildlink3.mk
+++ devel/hs-deferred-folds/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:12 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/30 07:59:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-deferred-folds
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-deferred-folds
 HS_DEFERRED_FOLDS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-deferred-folds+=	hs-deferred-folds>=0.9.18
-BUILDLINK_ABI_DEPENDS.hs-deferred-folds+=	hs-deferred-folds>=0.9.18.2nb2
+BUILDLINK_ABI_DEPENDS.hs-deferred-folds+=	hs-deferred-folds>=0.9.18.5
 BUILDLINK_PKGSRCDIR.hs-deferred-folds?=		../../devel/hs-deferred-folds
 
 .include "../../devel/hs-foldl/buildlink3.mk"
