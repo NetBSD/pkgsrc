2c5633ec15bf74257d4c5965d0f723b42f3f48b1
pho <pho@pkgsrc.org>
2023-10-30 12:41:12 +0000

    devel/hs-hslua-module-version: Update to 1.1.0
    hslua-module-version-1.1.0 - Released 2023-03-13.
    * Update to hslua-2.3; this includes the addition of type initializers to
      the module.

--- devel/hs-hslua-module-version/buildlink3.mk
+++ devel/hs-hslua-module-version/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 12:41:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-version
 
 .if !defined(HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK)
 HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.0.3nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.1.0
 BUILDLINK_PKGSRCDIR.hs-hslua-module-version?=	../../devel/hs-hslua-module-version
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
