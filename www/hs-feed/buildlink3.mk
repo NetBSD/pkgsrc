8404e329ef204b00f477780332ebcdec3a5b6e5b
pho <pho@pkgsrc.org>
2023-10-30 13:51:46 +0000

    www/hs-feed: import hs-feed-1.3.2.1
    Interfacing with RSS (v 0.9x, 2.x, 1.0) + Atom feeds.
    
    * Parsers
    * Constructors
    * Rendering
    * Querying
    
    To help working with the multiple feed formats we've ended up with this set
    of modules providing parsers, printers and some utility code for querying
    and just generally working with a concrete representation of feeds in
    Haskell.
    
    For basic reading and editing of feeds, consult the documentation of the
    Text.Feed.* hierarchy.

---
+++ www/hs-feed/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 13:51:46 pho Exp $
+
+BUILDLINK_TREE+=	hs-feed
+
+.if !defined(HS_FEED_BUILDLINK3_MK)
+HS_FEED_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-feed+=	hs-feed>=1.3.2
+BUILDLINK_ABI_DEPENDS.hs-feed+=	hs-feed>=1.3.2.1
+BUILDLINK_PKGSRCDIR.hs-feed?=	../../www/hs-feed
+
+.include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../devel/hs-old-locale/buildlink3.mk"
+.include "../../time/hs-old-time/buildlink3.mk"
+.include "../../devel/hs-safe/buildlink3.mk"
+.include "../../time/hs-time-locale-compat/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../textproc/hs-xml-conduit/buildlink3.mk"
+.include "../../textproc/hs-xml-types/buildlink3.mk"
+.endif	# HS_FEED_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-feed
