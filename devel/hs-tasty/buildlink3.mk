e1ef4ca9a0cffd06653ffcf0874bb70c4d0b5f72
pho <pho@pkgsrc.org>
2023-10-30 03:17:47 +0000

    devel/hs-tasty: Update to 1.5
    Version 1.5 - 2023-09-10
    * Progress reporting is no longer ignored. PrintTest constructor of
      TestOutput now has an extra field used to report progress. Supply const
      (pure ()) as this extra field value if you want to skip progress
      reporting (#311).
    * foldGroup now takes [b] instead of b as its last argument to allow for
      custom fold strategies. This is a backwards incompatible change, but you
      can get the old behavior by applying mconcat (#364).
    * Dependency loop error now lists all test cases that formed a cycle
      (#340).
    * Dependencies can now be defined pattern-free with sequentialTestGroup
      (#343).
    * Added --min-duration-to-report flag that specifies the time a test must
      take before tasty outputs timing information (#341).
    * When a test failed with an exception, print it using displayException
      instead of show (#330).
    * The -p / --pattern option can be specified multiple times; only tests
      that match all patterns are run (#380).
    * Fix color scheme to make info messages visible in terminals with white
      background (#369).
    * When parsing of a command-line option failed, report received option
      (#368).
    * Support WASM (#365).
    * Tested with GHC 8.0 - 9.8.

--- devel/hs-tasty/buildlink3.mk
+++ devel/hs-tasty/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/10/09 04:54:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/10/30 03:17:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty
 
 .if !defined(HS_TASTY_BUILDLINK3_MK)
 HS_TASTY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.4.3
-BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.4.3nb2
+BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.5
+BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.5
 BUILDLINK_PKGSRCDIR.hs-tasty?=		../../devel/hs-tasty
 
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
