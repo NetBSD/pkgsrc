ab13f1f1d272522bfe49cd5bf4f6b8ba847e82c1
pho <pho@pkgsrc.org>
2023-10-30 03:13:10 +0000

    devel/hs-versions: Update to 6.0.3
    6.0.3 (2023-10-23)
    Added
    * Data instances for the various data types.
    * Simple conversion types between the main version types.
    * Compile-time constructors via Template Haskell, like versioningQ.
    
    6.0.2 (2023-10-12)
    Added
    * Lift instances for the various types, which allows parsing version
      numbers at compile time within Template Haskell quotes. Currently there
      is no exported function that supports this directly, but you could write
      one like:
    
    -- | Parse a `Versioning` at compile time.
    thVer :: Text -> Q Exp
    thVer nm =
      case versioning nm of
        Left err -> fail (errorBundlePretty err)
        Right v  -> lift v
    
    Changed
    * Due to the new dependency on template-haskell, GHC 8.8 is now the lowest
      supported compiler version.
    
    6.0.1 (2023-05-08)
    Fixed
    * Restored the ability to compile with GHC versions earlier than 9.
    
    6.0.0 (2023-04-29)
    
    A number of type changes have been made to improve parsing and comparison
    logic. Doing so fixed several bugs and made the code cleaner overall.
    
    If you're just doing basic parsing and comparisons and not actually
    inspecting the types themselves, you shouldn't notice a difference.
    
    Added
    * New types Release, Chunks, and Chunk.
    
    Changed
    * Both SemVer and Version now contain a better-behaving Release type for
      their prerelease info.
    * Similarly, Version now also has a better-behaving Chunks type for its
      main version number sections.
    * The release traversal now yields a Maybe Release.
    * Versions with ~ in their metadata will now parse as a Mess. Example:
      12.0.0-3ubuntu1~20.04.5
    
    Removed
    * The various Semigroup instances. Adding version numbers together is a
      nonsensical operation and should never have been added in the first
      place.
    * The VChunk and VUnit types and their associated functions.
    
    Fixed
    * Leading zeroes are handled a little better in SemVer pre-release data.
    
    5.0.5 (2023-03-23)
    Changed
    * Bumped base bound to support GHC 9.6.

--- devel/hs-versions/buildlink3.mk
+++ devel/hs-versions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:35 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/30 03:13:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-versions
 
 .if !defined(HS_VERSIONS_BUILDLINK3_MK)
 HS_VERSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-versions+=	hs-versions>=5.0.4
-BUILDLINK_ABI_DEPENDS.hs-versions+=	hs-versions>=5.0.4nb2
+BUILDLINK_API_DEPENDS.hs-versions+=	hs-versions>=6.0.3
+BUILDLINK_ABI_DEPENDS.hs-versions+=	hs-versions>=6.0.3
 BUILDLINK_PKGSRCDIR.hs-versions?=	../../devel/hs-versions
 
 .include "../../devel/hs-hashable/buildlink3.mk"
