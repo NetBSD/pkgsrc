cbfe9f0dbcbc0ab0daad6ff81bc9906b7e61d0f9
pho <pho@pkgsrc.org>
2023-10-30 18:55:53 +0000

    converters/hs-lens-aeson: Update to 1.2.3
    1.2.3 [2023.06.27]
    * Use aeson's decode function in the AsJSON instance for ByteStrings. This
      allows removing a direct dependency on attoparsec.
    * Allow building with aeson-2.2.

--- converters/hs-lens-aeson/buildlink3.mk
+++ converters/hs-lens-aeson/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:03 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/30 18:55:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-lens-aeson
 
 .if !defined(HS_LENS_AESON_BUILDLINK3_MK)
 HS_LENS_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lens-aeson+=	hs-lens-aeson>=1.1.3
-BUILDLINK_ABI_DEPENDS.hs-lens-aeson+=	hs-lens-aeson>=1.2.2nb2
+BUILDLINK_API_DEPENDS.hs-lens-aeson+=	hs-lens-aeson>=1.2.3
+BUILDLINK_ABI_DEPENDS.hs-lens-aeson+=	hs-lens-aeson>=1.2.3
 BUILDLINK_PKGSRCDIR.hs-lens-aeson?=	../../converters/hs-lens-aeson
 
 .include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../textproc/hs-attoparsec/buildlink3.mk"
 .include "../../devel/hs-lens/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
+.include "../../textproc/hs-text-short/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_LENS_AESON_BUILDLINK3_MK
