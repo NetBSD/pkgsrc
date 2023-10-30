0c69ae57586a5368863a1f0c612b73f853ef3db8
pho <pho@pkgsrc.org>
2023-10-30 18:35:48 +0000

    net/hs-crypton-connection: import hs-crypton-connection-0.3.1
    Simple network library for all your connection need.
    
    Features: Really simple to use, SSL/TLS, SOCKS.
    
    This library provides a very simple api to create sockets to a destination
    with the choice of SSL/TLS, and SOCKS.

---
+++ net/hs-crypton-connection/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 18:35:48 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypton-connection
+
+.if !defined(HS_CRYPTON_CONNECTION_BUILDLINK3_MK)
+HS_CRYPTON_CONNECTION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-crypton-connection?=	../../net/hs-crypton-connection
+
+.include "../../devel/hs-basement/buildlink3.mk"
+.include "../../security/hs-crypton-x509/buildlink3.mk"
+.include "../../security/hs-crypton-x509-store/buildlink3.mk"
+.include "../../security/hs-crypton-x509-system/buildlink3.mk"
+.include "../../security/hs-crypton-x509-validation/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-socks/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.endif	# HS_CRYPTON_CONNECTION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypton-connection
