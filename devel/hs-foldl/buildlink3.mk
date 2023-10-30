fbd4c918765f8fcfd3e40686729f8e4a8c5e3f98
pho <pho@pkgsrc.org>
2023-10-30 07:54:42 +0000

    devel/hs-foldl: Update to 1.4.15
    No change log is provided by the upstream.

--- devel/hs-foldl/buildlink3.mk
+++ devel/hs-foldl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/30 07:54:42 pho Exp $
 
 BUILDLINK_TREE+=	hs-foldl
 
 .if !defined(HS_FOLDL_BUILDLINK3_MK)
 HS_FOLDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.14
-BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.14nb2
+BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.15
+BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.15
 BUILDLINK_PKGSRCDIR.hs-foldl?=		../../devel/hs-foldl
 
 .include "../../math/hs-comonad/buildlink3.mk"
