8e4744299271f52a1bc1e7e67e1c9eb9a750053f
pho <pho@pkgsrc.org>
2023-10-30 10:15:06 +0000

    textproc/hs-shakespeare: Update to 2.1.0.1
    2.1.0.1
    * Fix 100⨯ slowdown on parsec-3.1.15 #277
    
    2.1.0
    * Add OverloadedRecordDot-style record access in expressions

--- textproc/hs-shakespeare/buildlink3.mk
+++ textproc/hs-shakespeare/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/10/09 04:54:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2023/10/30 10:15:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-shakespeare
 
 .if !defined(HS_SHAKESPEARE_BUILDLINK3_MK)
 HS_SHAKESPEARE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.30
-BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.0.30nb2
+BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.1.0
+BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.1.0.1
 BUILDLINK_PKGSRCDIR.hs-shakespeare?=	../../textproc/hs-shakespeare
 
 .include "../../converters/hs-aeson/buildlink3.mk"
