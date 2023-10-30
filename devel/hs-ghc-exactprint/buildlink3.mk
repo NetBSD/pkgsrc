df78bf80043026e9f917bc8c208328fc4aa4aaf3
pho <pho@pkgsrc.org>
2023-10-30 11:55:15 +0000

    devel/hs-ghc-exactprint: Update to 1.7.0.1
    2022-11-08 v1.7.0.1
    * Add CI for GHC 9.6.2
    * Correctly deal with comments when using GHC flag -haddock
    
    2022-11-08 v1.7
    * Support GHC 9.6.1

--- devel/hs-ghc-exactprint/buildlink3.mk
+++ devel/hs-ghc-exactprint/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/30 11:55:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-exactprint
 
 .if !defined(HS_GHC_EXACTPRINT_BUILDLINK3_MK)
 HS_GHC_EXACTPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.6.1
-BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.6.1.1nb2
+BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.7.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.7.0.1
 BUILDLINK_PKGSRCDIR.hs-ghc-exactprint?=		../../devel/hs-ghc-exactprint
 
 .include "../../devel/hs-data-default/buildlink3.mk"
