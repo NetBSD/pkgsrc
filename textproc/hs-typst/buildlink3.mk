6d84df0eb0448d4b1e9fe73e4eb3a09a620669cb
pho <pho@pkgsrc.org>
2023-10-30 12:54:19 +0000

    textproc/hs-typst: import hs-typst-0.3.2.1
    A library for parsing and evaluating typst syntax. Typst
    (https://typst.app) is a document layout and formatting language. This
    library targets typst 0.7 and currently offers only partial support.

---
+++ textproc/hs-typst/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/30 12:54:19 pho Exp $
+
+BUILDLINK_TREE+=	hs-typst
+
+.if !defined(HS_TYPST_BUILDLINK3_MK)
+HS_TYPST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-typst+=	hs-typst>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-typst+=	hs-typst>=0.3.2.1
+BUILDLINK_PKGSRCDIR.hs-typst?=		../../textproc/hs-typst
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-cassava/buildlink3.mk"
+.include "../../math/hs-digits/buildlink3.mk"
+.include "../../devel/hs-ordered-containers/buildlink3.mk"
+.include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../textproc/hs-toml-parser/buildlink3.mk"
+.include "../../textproc/hs-typst-symbols/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-xml-conduit/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
+.endif	# HS_TYPST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-typst
