bbd5341e1f6326a7a535a86ef281721e3ee69373
pho <pho@pkgsrc.org>
2023-10-30 10:56:53 +0000

    devel/hs-generic-lens: Update to 2.2.2.0
    generic-lens-2.2.2.0 (2023-04-15)
    * Support unprefixed constructor prisms on GHC 9.6 (#152)

--- devel/hs-generic-lens/buildlink3.mk
+++ devel/hs-generic-lens/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/30 10:56:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-generic-lens
 
 .if !defined(HS_GENERIC_LENS_BUILDLINK3_MK)
 HS_GENERIC_LENS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-generic-lens+=	hs-generic-lens>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-generic-lens+=	hs-generic-lens>=2.2.1.0nb3
+BUILDLINK_API_DEPENDS.hs-generic-lens+=	hs-generic-lens>=2.2.2
+BUILDLINK_ABI_DEPENDS.hs-generic-lens+=	hs-generic-lens>=2.2.2.0
 BUILDLINK_PKGSRCDIR.hs-generic-lens?=	../../devel/hs-generic-lens
 
 .include "../../devel/hs-generic-lens-core/buildlink3.mk"
