657f8dfa6baa10e96b0bdfdc8aaab6bbd49770df
pho <pho@pkgsrc.org>
2023-10-31 15:51:42 +0000

    finance/hledger-lib: Update to 1.31
    The change log is too long to paste here. See
    https://hackage.haskell.org/package/hledger-lib-1.31/changelog

--- finance/hledger-lib/buildlink3.mk
+++ finance/hledger-lib/buildlink3.mk
@@ -1,22 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/10/09 04:54:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2023/10/31 15:51:42 pho Exp $
 
 BUILDLINK_TREE+=	hledger-lib
 
 .if !defined(HLEDGER_LIB_BUILDLINK3_MK)
 HLEDGER_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hledger-lib+=	hledger-lib>=1.28
-BUILDLINK_ABI_DEPENDS.hledger-lib+=	hledger-lib>=1.28nb2
+BUILDLINK_API_DEPENDS.hledger-lib+=	hledger-lib>=1.31
+BUILDLINK_ABI_DEPENDS.hledger-lib+=	hledger-lib>=1.31
 BUILDLINK_PKGSRCDIR.hledger-lib?=	../../finance/hledger-lib
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../converters/hs-aeson-pretty/buildlink3.mk"
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-base-compat/buildlink3.mk"
 .include "../../textproc/hs-blaze-markup/buildlink3.mk"
 .include "../../devel/hs-call-stack/buildlink3.mk"
 .include "../../textproc/hs-cassava/buildlink3.mk"
 .include "../../textproc/hs-cassava-megaparsec/buildlink3.mk"
 .include "../../devel/hs-cmdargs/buildlink3.mk"
+.include "../../devel/hs-colour/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../finance/hs-Decimal/buildlink3.mk"
 .include "../../textproc/hs-doclayout/buildlink3.mk"
@@ -34,10 +36,13 @@ BUILDLINK_PKGSRCDIR.hledger-lib?=	../../finance/hledger-lib
 .include "../../devel/hs-tabular/buildlink3.mk"
 .include "../../devel/hs-tasty/buildlink3.mk"
 .include "../../devel/hs-tasty-hunit/buildlink3.mk"
+.include "../../devel/hs-terminal-size/buildlink3.mk"
+.include "../../textproc/hs-text-ansi/buildlink3.mk"
 .include "../../devel/hs-timeit/buildlink3.mk"
 .include "../../devel/hs-uglymemo/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../misc/hs-pager/buildlink3.mk"
 .endif	# HLEDGER_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hledger-lib
