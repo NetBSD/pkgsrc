8762d855a03e0f69b0b328ce249ba78fe858c8be
pho <pho@pkgsrc.org>
2023-10-31 14:02:15 +0000

    devel/hs-hls-explicit-record-fields-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-call-hierarchy-plugin/buildlink3.mk
+++ devel/hs-hls-call-hierarchy-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/31 14:02:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-call-hierarchy-plugin
 
 .if !defined(HS_HLS_CALL_HIERARCHY_PLUGIN_BUILDLINK3_MK)
 HS_HLS_CALL_HIERARCHY_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-call-hierarchy-plugin+=	hs-hls-call-hierarchy-plugin>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-hls-call-hierarchy-plugin+=	hs-hls-call-hierarchy-plugin>=1.2.0.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-call-hierarchy-plugin+=	hs-hls-call-hierarchy-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-call-hierarchy-plugin+=	hs-hls-call-hierarchy-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-call-hierarchy-plugin?=	../../devel/hs-hls-call-hierarchy-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
