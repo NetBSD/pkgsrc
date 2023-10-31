35d2ba6e09944d77d4650c24e5cc6d7439aaecf2
adam <adam@pkgsrc.org>
2023-10-31 19:56:48 +0000

    qt6: updated to 6.6.0
    Qt 6.6
    https://www.qt.io/blog/qt-6.6-released

--- x11/qt6-qtbase/patches/patch-src_corelib_thread_qwaitcondition__unix.cpp
+++ x11/qt6-qtbase/patches/patch-src_corelib_thread_qwaitcondition__unix.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_corelib_thread_qwaitcondition__unix.cpp,v 1.2 2023/08/03 20:01:30 adam Exp $
+$NetBSD: patch-src_corelib_thread_qwaitcondition__unix.cpp,v 1.3 2023/10/31 19:56:48 adam Exp $
 
 On NetBSD before 6.1, do not use pthread_condattr_setclock().
 
---- src/corelib/thread/qwaitcondition_unix.cpp.orig	2023-07-06 10:02:03.000000000 +0000
+--- src/corelib/thread/qwaitcondition_unix.cpp.orig	2023-09-13 16:02:48.000000000 +0000
 +++ src/corelib/thread/qwaitcondition_unix.cpp
-@@ -18,6 +18,7 @@
+@@ -17,6 +17,7 @@
  
  #include <errno.h>
  #include <sys/time.h>
@@ -12,12 +12,12 @@ On NetBSD before 6.1, do not use pthread_condattr_setclock().
  #include <time.h>
  
  QT_BEGIN_NAMESPACE
-@@ -33,7 +34,7 @@ void qt_initialize_pthread_cond(pthread_
-     pthread_condattr_t condattr;
+@@ -56,7 +57,7 @@ static void qt_initialize_pthread_cond(p
+ {
+     pthread_condattr_t *attrp = nullptr;
  
-     pthread_condattr_init(&condattr);
 -#if defined(CLOCK_MONOTONIC) && !defined(Q_OS_DARWIN)
 +#if defined(CLOCK_MONOTONIC) && !defined(Q_OS_DARWIN) && !(defined(Q_OS_NETBSD) && (__NetBSD_Version__ < 600010000))
-     if (QElapsedTimer::clockType() == QElapsedTimer::MonotonicClock)
-         pthread_condattr_setclock(&condattr, CLOCK_MONOTONIC);
- #endif
+     pthread_condattr_t condattr;
+     attrp = &condattr;
+ 
