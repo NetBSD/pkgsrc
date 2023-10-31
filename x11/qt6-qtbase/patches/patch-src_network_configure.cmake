c964012f75cac7badff3a547eaca59b8dd5dad36
adam <adam@pkgsrc.org>
2023-10-31 19:56:48 +0000

    qt6: updated to 6.6.0
    Qt 6.6
    https://www.qt.io/blog/qt-6.6-released

--- x11/qt6-qtbase/patches/patch-src_network_configure.cmake
+++ x11/qt6-qtbase/patches/patch-src_network_configure.cmake
@@ -1,18 +1,17 @@
-$NetBSD: patch-src_network_configure.cmake,v 1.1 2022/11/24 11:11:38 nros Exp $
+$NetBSD: patch-src_network_configure.cmake,v 1.2 2023/10/31 19:56:48 adam Exp $
 
 * add libraries needed for networking on solaris
 
---- src/network/configure.cmake.orig	2022-03-27 12:50:42.000000000 +0000
+--- src/network/configure.cmake.orig	2023-09-13 16:02:48.000000000 +0000
 +++ src/network/configure.cmake
-@@ -56,6 +56,7 @@ SSL_free(SSL_new(0));
- qt_find_package(GSSAPI PROVIDED_TARGETS GSSAPI::GSSAPI MODULE_NAME network QMAKE_LIB gssapi)
+@@ -13,12 +13,14 @@ qt_find_package(GSSAPI PROVIDED_TARGETS
  qt_find_package(GLIB2 OPTIONAL_COMPONENTS GOBJECT PROVIDED_TARGETS GLIB2::GOBJECT MODULE_NAME core QMAKE_LIB gobject)
  qt_find_package(GLIB2 OPTIONAL_COMPONENTS GIO PROVIDED_TARGETS GLIB2::GIO MODULE_NAME core QMAKE_LIB gio)
+ qt_find_package(WrapResolv PROVIDED_TARGETS WrapResolv::WrapResolv MODULE_NAME network QMAKE_LIB libresolv)
 +qt_test_lib_target(TARGET QtTEST::needed_network_libs LIBS socket nsl)
  
- 
  #### Tests
-@@ -63,6 +64,7 @@ qt_find_package(GLIB2 OPTIONAL_COMPONENT
+ 
  # getifaddrs
  qt_config_compile_test(getifaddrs
      LABEL "getifaddrs()"
@@ -20,7 +19,7 @@ $NetBSD: patch-src_network_configure.cmake,v 1.1 2022/11/24 11:11:38 nros Exp $
      CODE
  "#include <sys/types.h>
  #include <sys/socket.h>
-@@ -100,6 +102,7 @@ req.ifr_index = 0;
+@@ -56,6 +58,7 @@ req.ifr_index = 0;
  # ipv6ifname
  qt_config_compile_test(ipv6ifname
      LABEL "IPv6 ifname"
