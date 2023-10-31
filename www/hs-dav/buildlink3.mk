22f2c12c3fb4b9354e61a9db9749feb4d882c761
pho <pho@pkgsrc.org>
2023-10-31 06:20:11 +0000

    www/hs-dav: import hs-dav-1.3.4
    This is a library for the Web Distributed Authoring and Versioning (WebDAV)
    extensions to HTTP. At present it supports a very small subset of client
    functionality.
    
    In addition, there is an executable, hdav, which can be used for
    command-line operation.

---
+++ www/hs-dav/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/31 06:20:11 pho Exp $
+
+BUILDLINK_TREE+=	hs-dav
+
+.if !defined(HS_DAV_BUILDLINK3_MK)
+HS_DAV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-dav+=	hs-dav>=1.3.4
+BUILDLINK_ABI_DEPENDS.hs-dav+=	hs-dav>=1.3.4
+BUILDLINK_PKGSRCDIR.hs-dav?=	../../www/hs-dav
+
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../www/hs-http-client/buildlink3.mk"
+.include "../../www/hs-http-client-tls/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../textproc/hs-xml-conduit/buildlink3.mk"
+.include "../../textproc/hs-xml-hamlet/buildlink3.mk"
+.endif	# HS_DAV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-dav
