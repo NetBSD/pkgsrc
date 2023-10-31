b0f88d53ba4a65801bf43bf44e94e2be8be0c988
pho <pho@pkgsrc.org>
2023-10-31 14:07:12 +0000

    devel/hs-hls-explicit-record-fields-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-cabal-plugin/buildlink3.mk
+++ devel/hs-hls-cabal-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/31 14:07:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-cabal-plugin
 
 .if !defined(HS_HLS_CABAL_PLUGIN_BUILDLINK3_MK)
 HS_HLS_CABAL_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-cabal-plugin+=	hs-hls-cabal-plugin>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-hls-cabal-plugin+=	hs-hls-cabal-plugin>=0.1.0.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-cabal-plugin+=	hs-hls-cabal-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-cabal-plugin+=	hs-hls-cabal-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-cabal-plugin?=	../../devel/hs-hls-cabal-plugin
 
 .include "../../misc/hs-extra/buildlink3.mk"
@@ -14,9 +14,11 @@ BUILDLINK_PKGSRCDIR.hs-hls-cabal-plugin?=	../../devel/hs-hls-cabal-plugin
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-hls-graph/buildlink3.mk"
 .include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
 .include "../../devel/hs-lsp/buildlink3.mk"
 .include "../../devel/hs-lsp-types/buildlink3.mk"
 .include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.include "../../textproc/hs-text-rope/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_HLS_CABAL_PLUGIN_BUILDLINK3_MK
 
