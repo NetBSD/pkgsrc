9234107b527456cddaeec99a18671beccd632bb8
pho <pho@pkgsrc.org>
2023-10-31 13:48:45 +0000

    devel/hs-hls-explicit-record-fields-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-explicit-imports-plugin/buildlink3.mk
+++ devel/hs-hls-explicit-imports-plugin/buildlink3.mk
@@ -1,18 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/31 13:48:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-explicit-imports-plugin
 
 .if !defined(HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK)
 HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=1.2.0.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-explicit-imports-plugin+=	hs-hls-explicit-imports-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-explicit-imports-plugin?=	../../devel/hs-hls-explicit-imports-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../devel/hs-ghcide/buildlink3.mk"
 .include "../../devel/hs-hls-graph/buildlink3.mk"
 .include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
 .include "../../devel/hs-lsp/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_HLS_EXPLICIT_IMPORTS_PLUGIN_BUILDLINK3_MK
