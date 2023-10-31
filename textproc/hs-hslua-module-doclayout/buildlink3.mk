23216724320f27b98af37ff5e7d0d1f20633464f
pho <pho@pkgsrc.org>
2023-10-31 04:55:36 +0000

    textproc/hs-hslua-module-doclayout: Update to 1.1.0
    1.1.0
    * Require hslua-2.3 and tasty-lua-1.1.
    * Type info is added to all fields.
    * The Doc type is now associated with this module's docs, making it easier
      to generate documentation.

--- textproc/hs-hslua-module-doclayout/buildlink3.mk
+++ textproc/hs-hslua-module-doclayout/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/09 04:54:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/10/31 04:55:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-doclayout
 
 .if !defined(HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK)
 HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.0.4
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.0.4nb2
+BUILDLINK_API_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.1.0
 BUILDLINK_PKGSRCDIR.hs-hslua-module-doclayout?=		../../textproc/hs-hslua-module-doclayout
 
 .include "../../textproc/hs-doclayout/buildlink3.mk"
