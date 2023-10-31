d50175d2755b55c9bccdc65d4a98f0e9997c623b
pho <pho@pkgsrc.org>
2023-10-31 17:00:26 +0000

    databases/hs-persistent-sqlite: import hs-persistent-sqlite-2.13.2.0
    This package includes a thin sqlite3 wrapper based on the direct-sqlite
    package.

---
+++ databases/hs-persistent-sqlite/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/31 17:00:26 pho Exp $
+
+BUILDLINK_TREE+=	hs-persistent-sqlite
+
+.if !defined(HS_PERSISTENT_SQLITE_BUILDLINK3_MK)
+HS_PERSISTENT_SQLITE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-persistent-sqlite+=	hs-persistent-sqlite>=2.13.2
+BUILDLINK_ABI_DEPENDS.hs-persistent-sqlite+=	hs-persistent-sqlite>=2.13.2.0
+BUILDLINK_PKGSRCDIR.hs-persistent-sqlite?=	../../databases/hs-persistent-sqlite
+
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-microlens-th/buildlink3.mk"
+.include "../../devel/hs-monad-logger/buildlink3.mk"
+.include "../../devel/hs-persistent/buildlink3.mk"
+.include "../../devel/hs-resource-pool/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_PERSISTENT_SQLITE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-persistent-sqlite
