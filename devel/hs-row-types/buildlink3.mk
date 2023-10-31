dde5c4fecf50a1bb69e89232a5523ba59c6c8940
pho <pho@pkgsrc.org>
2023-10-31 03:44:03 +0000

    devel/hs-row-types: import hs-row-types-1.0.1.2
    This package uses closed type families and type literals to implement open
    records and variants. The core is based off of the CTRex package, but it
    also includes polymorphic variants and a number of additional
    functions. That said, it is not a proper superset of CTRex as it
    specifically forbids records from having more than one element of the same
    label.

---
+++ devel/hs-row-types/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/31 03:44:03 pho Exp $
+
+BUILDLINK_TREE+=	hs-row-types
+
+.if !defined(HS_ROW_TYPES_BUILDLINK3_MK)
+HS_ROW_TYPES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-row-types+=	hs-row-types>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-row-types+=	hs-row-types>=1.0.1.2
+BUILDLINK_PKGSRCDIR.hs-row-types?=	../../devel/hs-row-types
+
+.include "../../devel/hs-constraints/buildlink3.mk"
+.include "../../devel/hs-generic-lens/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_ROW_TYPES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-row-types
