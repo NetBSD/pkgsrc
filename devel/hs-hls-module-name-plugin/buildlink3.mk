c0e30513284d15a95d28bf3727413aef74b6572d
pho <pho@pkgsrc.org>
2023-10-31 13:13:03 +0000

    devel/hs-hls-pragmas-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-module-name-plugin/buildlink3.mk
+++ devel/hs-hls-module-name-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/31 13:13:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-module-name-plugin
 
 .if !defined(HS_HLS_MODULE_NAME_PLUGIN_BUILDLINK3_MK)
 HS_HLS_MODULE_NAME_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=1.1.1.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-module-name-plugin+=	hs-hls-module-name-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-module-name-plugin?=		../../devel/hs-hls-module-name-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
