37bf0467d09ba624872d55e376dda70188946215
pho <pho@pkgsrc.org>
2023-10-31 18:24:52 +0000

    www/hs-clientsession: import hs-clientsession-0.9.2.0
    Achieves security through AES-CTR encryption and Skein-MAC-512-256
    authentication. Uses Base64 encoding to avoid any issues with characters.

---
+++ www/hs-clientsession/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/31 18:24:52 pho Exp $
+
+BUILDLINK_TREE+=	hs-clientsession
+
+.if !defined(HS_CLIENTSESSION_BUILDLINK3_MK)
+HS_CLIENTSESSION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-clientsession+=	hs-clientsession>=0.9.2
+BUILDLINK_ABI_DEPENDS.hs-clientsession+=	hs-clientsession>=0.9.2.0
+BUILDLINK_PKGSRCDIR.hs-clientsession?=		../../www/hs-clientsession
+
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
+.include "../../devel/hs-cereal/buildlink3.mk"
+.include "../../security/hs-crypto-api/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../security/hs-entropy/buildlink3.mk"
+.include "../../devel/hs-setenv/buildlink3.mk"
+.include "../../security/hs-skein/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.endif	# HS_CLIENTSESSION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-clientsession
