24721d22cab4649669287a95d2a8c7964f022ee5
pho <pho@pkgsrc.org>
2023-10-31 14:18:30 +0000

    devel/hs-hls-cabal-fmt-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-cabal-fmt-plugin/buildlink3.mk
+++ devel/hs-hls-cabal-fmt-plugin/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/31 14:18:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-cabal-fmt-plugin
 
 .if !defined(HS_HLS_CABAL_FMT_PLUGIN_BUILDLINK3_MK)
 HS_HLS_CABAL_FMT_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-cabal-fmt-plugin+=	hs-hls-cabal-fmt-plugin>=0.1.0
-BUILDLINK_API_DEPENDS.hs-hls-cabal-fmt-plugin+=	hs-hls-cabal-fmt-plugin>=0.1.0.0
-BUILDLINK_ABI_DEPENDS.hs-hls-cabal-fmt-plugin?=	hs-hls-cabal-fmt-plugin>=0.1.0.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-cabal-fmt-plugin+=	hs-hls-cabal-fmt-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-cabal-fmt-plugin?=	hs-hls-cabal-fmt-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-cabal-fmt-plugin?=	../../devel/hs-hls-cabal-fmt-plugin
 
 .include "../../devel/hs-ghcide/buildlink3.mk"
 .include "../../devel/hs-hls-plugin-api/buildlink3.mk"
 .include "../../devel/hs-lens/buildlink3.mk"
 .include "../../devel/hs-lsp-types/buildlink3.mk"
+.include "../../sysutils/hs-process-extras/buildlink3.mk"
 .endif	# HS_HLS_CABAL_FMT_PLUGIN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hls-cabal-fmt-plugin
