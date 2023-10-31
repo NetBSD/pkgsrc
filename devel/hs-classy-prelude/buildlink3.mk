09ea2dfd5bfe5b8f41664b82915c0817521bb31b
pho <pho@pkgsrc.org>
2023-10-31 03:32:06 +0000

    devel/hs-classy-prelude: Update to 1.5.0.3
    1.5.0.3
        Don't import Data.Functor.unzip #215

--- devel/hs-classy-prelude/buildlink3.mk
+++ devel/hs-classy-prelude/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/09 04:54:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/10/31 03:32:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-classy-prelude
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-classy-prelude
 HS_CLASSY_PRELUDE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-classy-prelude+=	hs-classy-prelude>=1.5.0
-BUILDLINK_ABI_DEPENDS.hs-classy-prelude+=	hs-classy-prelude>=1.5.0.2nb5
+BUILDLINK_ABI_DEPENDS.hs-classy-prelude+=	hs-classy-prelude>=1.5.0.3
 BUILDLINK_PKGSRCDIR.hs-classy-prelude?=		../../devel/hs-classy-prelude
 
 .include "../../devel/hs-async/buildlink3.mk"
