52dfebed9420224f8f4372a6913f8d55785f6c77
pho <pho@pkgsrc.org>
2023-10-31 06:14:09 +0000

    www/hs-http-client-restricted: import hs-http-client-restricted-0.1.0
    Addition to the http-client and http-client-tls libraries, that restricts
    the HTTP servers that can be used.
    
    This is useful when a security policy needs to eg, prevent connections to
    HTTP servers on localhost or a local network, or only allow connections to
    a specific HTTP server.
    
    It handles restricting redirects as well as the initial HTTP connection,
    and it also guards against DNS poisoning attacks.

---
+++ www/hs-http-client-restricted/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/31 06:14:09 pho Exp $
+
+BUILDLINK_TREE+=	hs-http-client-restricted
+
+.if !defined(HS_HTTP_CLIENT_RESTRICTED_BUILDLINK3_MK)
+HS_HTTP_CLIENT_RESTRICTED_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-http-client-restricted+=	hs-http-client-restricted>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-http-client-restricted+=	hs-http-client-restricted>=0.1.0
+BUILDLINK_PKGSRCDIR.hs-http-client-restricted?=		../../www/hs-http-client-restricted
+
+.include "../../net/hs-crypton-connection/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../www/hs-http-client/buildlink3.mk"
+.include "../../www/hs-http-client-tls/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-bsd/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.endif	# HS_HTTP_CLIENT_RESTRICTED_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-http-client-restricted
