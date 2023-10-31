fe37dadc2bc36c7f3a9aec1029551225b9501209
pho <pho@pkgsrc.org>
2023-10-31 13:20:09 +0000

    devel/hs-hls-hlint-plugin: Mark the package as BROKEN

--- devel/hs-hls-hlint-plugin/buildlink3.mk
+++ devel/hs-hls-hlint-plugin/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/31 13:20:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-hlint-plugin
 
 .if !defined(HS_HLS_HLINT_PLUGIN_BUILDLINK3_MK)
 HS_HLS_HLINT_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-hlint-plugin+=	hs-hls-hlint-plugin>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-hls-hlint-plugin+=	hs-hls-hlint-plugin>=1.1.1.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-hlint-plugin+=	hs-hls-hlint-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-hlint-plugin+=	hs-hls-hlint-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-hlint-plugin?=	../../devel/hs-hls-hlint-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-apply-refact/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../textproc/hs-Diff/buildlink3.mk"
 .include "../../misc/hs-extra/buildlink3.mk"
@@ -20,14 +21,12 @@ BUILDLINK_PKGSRCDIR.hs-hls-hlint-plugin?=	../../devel/hs-hls-hlint-plugin
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hlint/buildlink3.mk"
 .include "../../devel/hs-hls-plugin-api/buildlink3.mk"
-.include "../../sysutils/hs-hslogger/buildlink3.mk"
 .include "../../devel/hs-lens/buildlink3.mk"
 .include "../../devel/hs-lsp/buildlink3.mk"
 .include "../../devel/hs-refact/buildlink3.mk"
 .include "../../textproc/hs-regex-tdfa/buildlink3.mk"
 .include "../../sysutils/hs-temporary/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
-.include "../../devel/hs-apply-refact/buildlink3.mk"
 .endif	# HS_HLS_HLINT_PLUGIN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hls-hlint-plugin
