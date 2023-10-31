44ae88c58702f71425acd2654506623946e0b6db
pho <pho@pkgsrc.org>
2023-10-31 04:52:24 +0000

    textproc/hs-texmath: Update to 0.12.8.4
    texmath (0.12.8.4)
      * TeX reader: ignore `\allowbreak` (#230).
      * TeX reader: handle `*{5}{lr}` in array column specifier (#229).
      * OMML reader: allow `m:e` to be missing in `m:nary` (#228).
        Technically this is not allowed, according to the spec, but
        Word and LibreOffice seem to tolerate it.
    
    texmath (0.12.8.3)
      * OMML writer: use "on" and "off" instead of "1" and "0" for
        m:CT_OnOff type.  It is said that "1" and "0" work in Word
        but not Powerpoint.
    
    texmath (0.12.8.2)
      * Typst writer: use binom instead of a fraction (jgm/pandoc#9063).
    
    texmath (0.12.8.1)
      * Typst writer: several fixes (#223, Lleu Yang).
        + Escape quotes (") in inQuotes
        + Accent `\8407` corresponds to `arrow()`
        + Write `#none`'s for matrices with blanks at the beginning of a row
    
    texmath (0.12.8)
      * Expose Text.TeXMath.Shared [API change]
      * Typst writer: Fix bug where 's' turned into 'space' (#219).
      * Typst writer: Fix handling of overline (#214).
      * Typst writer: Fix underbrace (#217).
      * Typst writer: Improve some accents (#216).
      * TeX writer: don't include \ on last line of matrix.
      * TeX writer: Remove escaping of spaces inside \text{}.
        It isn't needed, and it causes problems in MathJax rendering.
      * TeX reader: allow empty matrices.
      * MathML writer: Fix rendering of vectors (#218).
      * Depend on external typst-symbols package.
    
    texmath (0.12.7.1)
     * Typst writer:
       + Improve under/overbrace/bracket/line.
       + Fix bugs with super/subscript grouping (#212).
       + Fix case where super/subscript is on an empty element,
         by inserting a zws.
    
    texmath (0.12.7)
      * Add typst writer. New module: Text.TeXMath.Writers.Typst.
      * TeX reader: Support multilined environment. Closes #210.

--- textproc/hs-texmath/buildlink3.mk
+++ textproc/hs-texmath/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.31 2023/10/09 04:54:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2023/10/31 04:52:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
 .if !defined(HS_TEXMATH_BUILDLINK3_MK)
 HS_TEXMATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.12.6
-BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.12.6nb2
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.12.8
+BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.12.8.4
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../textproc/hs-texmath
 
 .include "../../textproc/hs-pandoc-types/buildlink3.mk"
 .include "../../devel/hs-split/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
+.include "../../textproc/hs-typst-symbols/buildlink3.mk"
 .include "../../textproc/hs-xml/buildlink3.mk"
 .endif	# HS_TEXMATH_BUILDLINK3_MK
 
