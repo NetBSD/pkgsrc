6ceb3c6a12b803613c158f97c59595225effe00d
pho <pho@pkgsrc.org>
2023-10-31 03:20:47 +0000

    devel/hs-vector-instances: Update to 3.4.2
    3.4.2 [2023.04.17]
        Remove unused dependency on semigroups.
    
    3.4.1 [2023.04.14]
        Add a Hashable1 Vector instance.
        Require vector-0.12+ and hashable-1.2.5.0+.
        Drop support for GHC 7.10 and earlier.

--- devel/hs-vector-instances/buildlink3.mk
+++ devel/hs-vector-instances/buildlink3.mk
@@ -1,19 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/10/09 04:54:35 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/10/31 03:20:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-instances
 
 .if !defined(HS_VECTOR_INSTANCES_BUILDLINK3_MK)
 HS_VECTOR_INSTANCES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector-instances+=	hs-vector-instances>=3.4
-BUILDLINK_ABI_DEPENDS.hs-vector-instances+=	hs-vector-instances>=3.4nb4
+BUILDLINK_API_DEPENDS.hs-vector-instances+=	hs-vector-instances>=3.4.2
+BUILDLINK_ABI_DEPENDS.hs-vector-instances+=	hs-vector-instances>=3.4.2
 BUILDLINK_PKGSRCDIR.hs-vector-instances?=	../../devel/hs-vector-instances
 
 .include "../../math/hs-comonad/buildlink3.mk"
 .include "../../devel/hs-keys/buildlink3.mk"
 .include "../../devel/hs-pointed/buildlink3.mk"
 .include "../../math/hs-semigroupoids/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .endif	# HS_VECTOR_INSTANCES_BUILDLINK3_MK
