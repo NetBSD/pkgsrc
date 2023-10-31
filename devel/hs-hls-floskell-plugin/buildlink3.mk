d62ad49c3874f8c3bbb720b2c5e7f150e039ad1f
pho <pho@pkgsrc.org>
2023-10-31 12:42:03 +0000

    devel/hs-hls-floskell-plugin: Mark the package as BROKEN

--- devel/hs-hls-floskell-plugin/buildlink3.mk
+++ devel/hs-hls-floskell-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/10/09 04:54:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2023/10/31 12:42:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-floskell-plugin
 
 .if !defined(HS_HLS_FLOSKELL_PLUGIN_BUILDLINK3_MK)
 HS_HLS_FLOSKELL_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-floskell-plugin+=	hs-hls-floskell-plugin>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-hls-floskell-plugin+=	hs-hls-floskell-plugin>=1.0.2.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-floskell-plugin+=	hs-hls-floskell-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-floskell-plugin+=	hs-hls-floskell-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-floskell-plugin?=	../../devel/hs-hls-floskell-plugin
 
 .include "../../devel/floskell/buildlink3.mk"
