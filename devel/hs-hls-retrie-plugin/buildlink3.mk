6adc287f65186937ce31ecd905313fd2b67473a5
pho <pho@pkgsrc.org>
2023-10-31 12:55:10 +0000

    devel/hs-hls-retrie-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-retrie-plugin/buildlink3.mk
+++ devel/hs-hls-retrie-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/31 12:55:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-retrie-plugin
 
 .if !defined(HS_HLS_RETRIE_PLUGIN_BUILDLINK3_MK)
 HS_HLS_RETRIE_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-retrie-plugin+=	hs-hls-retrie-plugin>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-hls-retrie-plugin+=	hs-hls-retrie-plugin>=1.0.3.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-retrie-plugin+=	hs-hls-retrie-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-retrie-plugin+=	hs-hls-retrie-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-retrie-plugin?=	../../devel/hs-hls-retrie-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -14,6 +14,8 @@ BUILDLINK_PKGSRCDIR.hs-hls-retrie-plugin?=	../../devel/hs-hls-retrie-plugin
 .include "../../devel/hs-ghcide/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-hls-refactor-plugin/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
 .include "../../devel/hs-lsp/buildlink3.mk"
 .include "../../devel/hs-lsp-types/buildlink3.mk"
 .include "../../devel/retrie/buildlink3.mk"
