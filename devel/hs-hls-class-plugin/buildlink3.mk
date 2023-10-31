8c2a3d05bd2cee753795f5149693a855f4ab8630
pho <pho@pkgsrc.org>
2023-10-31 13:57:37 +0000

    devel/hs-hls-explicit-record-fields-plugin: Update to 2.4.0.0
    No change log is provided by the upstream.

--- devel/hs-hls-class-plugin/buildlink3.mk
+++ devel/hs-hls-class-plugin/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/09 04:54:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/10/31 13:57:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-hls-class-plugin
 
 .if !defined(HS_HLS_CLASS_PLUGIN_BUILDLINK3_MK)
 HS_HLS_CLASS_PLUGIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hls-class-plugin+=	hs-hls-class-plugin>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-hls-class-plugin+=	hs-hls-class-plugin>=1.1.1.0nb3
+BUILDLINK_API_DEPENDS.hs-hls-class-plugin+=	hs-hls-class-plugin>=2.4.0
+BUILDLINK_ABI_DEPENDS.hs-hls-class-plugin+=	hs-hls-class-plugin>=2.4.0.0
 BUILDLINK_PKGSRCDIR.hs-hls-class-plugin?=	../../devel/hs-hls-class-plugin
 
 .include "../../converters/hs-aeson/buildlink3.mk"
