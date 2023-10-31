1d0f0087d08887792e18a4ff9568be6bc042b502
schmonz <schmonz@pkgsrc.org>
2023-10-31 14:55:36 +0000

    getmail6: avoid conflicts with getmail. Bump PKGREVISION.

--- mail/getmail6/patches/patch-setup.py
+++ mail/getmail6/patches/patch-setup.py
@@ -1,15 +1,59 @@
-$NetBSD: patch-setup.py,v 1.1 2023/10/31 01:54:45 sekiya Exp $
+$NetBSD: patch-setup.py,v 1.2 2023/10/31 14:55:36 schmonz Exp $
 
-Fix mandir per pkgsrc
+Use standard pkgsrc paths. Avoid conflicting with getmail.
 
---- setup.py~	2023-10-31 10:46:37.492574240 +0900
-+++ setup.py	2023-10-31 10:46:56.670550748 +0900
-@@ -39,7 +39,7 @@
- DOCDIR = os.path.join('share','doc','getmail-%s' % __version__)
+--- setup.py.orig	2023-08-22 15:38:27.000000000 +0000
++++ setup.py
+@@ -36,10 +36,10 @@ for (pos, arg) in enumerate(args):
+         # hack hack hack hack hack hack hack
+         datadir = args[pos + 1]
+ 
+-DOCDIR = os.path.join('share','doc','getmail-%s' % __version__)
++DOCDIR = os.path.join('share','doc','getmail6')
  GETMAILDOCDIR = os.path.join(datadir or prefix, DOCDIR)
  
 -MANDIR = os.path.join('share','man','man1')
-+MANDIR = os.path.join('man','man1')
++MANDIR = os.path.join(os.environ["PKGMANDIR"],'man1')
  GETMAILMANDIR = os.path.join( datadir or prefix, MANDIR)
  
  if '--show-default-install-dirs' in args:
+@@ -79,15 +79,15 @@ setup(
+         'Topic :: Utilities',
+     ],
+     packages=[
+-        'getmailcore'
++        'getmail6core'
+     ],
+     scripts=[
+-        'getmail',
+-        'getmails',
+-        'getmail_fetch',
+-        'getmail_maildir',
+-        'getmail_mbox',
+-        'getmail-gmail-xoauth-tokens',
++        'getmail6',
++        'getmail6s',
++        'getmail6_fetch',
++        'getmail6_maildir',
++        'getmail6_mbox',
++        'getmail6-gmail-xoauth-tokens',
+     ],
+     data_files=[
+         (DOCDIR, [
+@@ -108,11 +108,11 @@ setup(
+             'docs/troubleshooting.txt',
+         ]),
+         (MANDIR, [
+-            'docs/getmails.1',
+-            'docs/getmail.1',
+-            'docs/getmail_fetch.1',
+-            'docs/getmail_maildir.1',
+-            'docs/getmail_mbox.1',
++            'docs/getmail6s.1',
++            'docs/getmail6.1',
++            'docs/getmail6_fetch.1',
++            'docs/getmail6_maildir.1',
++            'docs/getmail6_mbox.1',
+         ]),
+     ],
+ )
