d732932dc3fa9b743296a7ce3469c43cfe27ae31
pho <pho@pkgsrc.org>
2023-10-31 17:05:49 +0000

    devel/hs-safesemaphore: import hs-safesemaphore-0.10.1
    This provides a much safer semaphore than the QSem, QSemN, SampleVar in
    base. Those base modules are not exception safe and can be broken by
    killThread.

---
+++ devel/hs-safesemaphore/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/10/31 17:05:49 pho Exp $
+
+BUILDLINK_TREE+=	hs-safesemaphore
+
+.if !defined(HS_SAFESEMAPHORE_BUILDLINK3_MK)
+HS_SAFESEMAPHORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-safesemaphore+=	hs-safesemaphore>=0.10.1
+BUILDLINK_ABI_DEPENDS.hs-safesemaphore+=	hs-safesemaphore>=0.10.1
+BUILDLINK_PKGSRCDIR.hs-safesemaphore?=		../../devel/hs-safesemaphore
+.endif	# HS_SAFESEMAPHORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-safesemaphore
