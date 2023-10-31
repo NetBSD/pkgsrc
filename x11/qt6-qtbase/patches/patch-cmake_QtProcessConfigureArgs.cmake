b5baf8f613900eecd0cb81cca210cb596dfdef5a
adam <adam@pkgsrc.org>
2023-10-31 19:56:48 +0000

    qt6: updated to 6.6.0
    Qt 6.6
    https://www.qt.io/blog/qt-6.6-released

--- x11/qt6-qtbase/patches/patch-cmake_QtProcessConfigureArgs.cmake
+++ x11/qt6-qtbase/patches/patch-cmake_QtProcessConfigureArgs.cmake
@@ -1,4 +1,4 @@
-$NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.1 2022/11/24 11:11:38 nros Exp $
+$NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.2 2023/10/31 19:56:48 adam Exp $
 
 * add function so that libraries such as socket and nsl 
   can be added to tests
@@ -6,9 +6,9 @@ $NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.1 2022/11/24 11:11:38 nros
 * add support for avoiding the cmake archiving api using configure since
   libarchive in pkgsrc that cmake uses is built without zstd support
 
---- cmake/QtProcessConfigureArgs.cmake.orig	2022-03-27 12:50:42.000000000 +0000
+--- cmake/QtProcessConfigureArgs.cmake.orig	2023-09-13 16:02:48.000000000 +0000
 +++ cmake/QtProcessConfigureArgs.cmake
-@@ -177,6 +177,7 @@ defstub(set_package_properties)
+@@ -240,6 +240,7 @@ defstub(set_package_properties)
  defstub(qt_qml_find_python)
  defstub(qt_set01)
  defstub(qt_internal_check_if_linker_is_available)
@@ -16,8 +16,8 @@ $NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.1 2022/11/24 11:11:38 nros
  
  ####################################################################################################
  # Define functions/macros that are called in qt_cmdline.cmake files
-@@ -798,6 +799,7 @@ translate_boolean_input(precompile_heade
- translate_boolean_input(ccache QT_USE_CCACHE)
+@@ -864,6 +865,7 @@ translate_boolean_input(ccache QT_USE_CC
+ translate_boolean_input(vcpkg QT_USE_VCPKG)
  translate_boolean_input(shared BUILD_SHARED_LIBS)
  translate_boolean_input(warnings_are_errors WARNINGS_ARE_ERRORS)
 +translate_boolean_input(avoid_cmake_archiving_api QT_AVOID_CMAKE_ARCHIVING_API)
--- x11/qt6-qtbase/patches/patch-cmake_QtSyncQtHelpers.cmake
+++ x11/qt6-qtbase/patches/patch-cmake_QtSyncQtHelpers.cmake
@@ -1,18 +1,10 @@
-$NetBSD: patch-cmake_QtSyncQtHelpers.cmake,v 1.1 2023/04/07 18:28:05 nros Exp $
+$NetBSD: patch-cmake_QtSyncQtHelpers.cmake,v 1.2 2023/10/31 19:56:48 adam Exp $
 
 * make sure syncqt is built before being used when building with make
 
---- cmake/QtSyncQtHelpers.cmake.orig	2023-04-06 21:34:52.245729498 +0000
+--- cmake/QtSyncQtHelpers.cmake.orig	2023-09-13 16:02:48.000000000 +0000
 +++ cmake/QtSyncQtHelpers.cmake
-@@ -163,6 +163,7 @@ function(qt_internal_target_sync_headers
-             ${syncqt_args_rsp}
-             ${module_headers}
-             ${QT_CMAKE_EXPORT_NAMESPACE}::syncqt
-+            $<$<STREQUAL:${PROJECT_NAME},QtBase>:syncqt_build>
-         COMMENT
-             "Running syncqt.cpp for module: ${module}"
-         VERBATIM
-@@ -190,6 +191,7 @@ function(qt_internal_target_sync_headers
+@@ -228,6 +228,7 @@ function(qt_internal_target_sync_headers
              ${syncqt_all_args_rsp}
              ${QT_CMAKE_EXPORT_NAMESPACE}::syncqt
              ${target}_sync_headers
