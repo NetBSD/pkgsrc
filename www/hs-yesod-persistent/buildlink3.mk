c8667d40221c40bbcf5f768d06ac2b1ccef4de36
pho <pho@pkgsrc.org>
2023-11-01 02:54:57 +0000

    www/hs-yesod-persistent: import hs-yesod-persistent-1.6.0.8
    Some helpers for using Persistent from Yesod.

---
+++ www/hs-yesod-persistent/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/01 02:54:57 pho Exp $
+
+BUILDLINK_TREE+=	hs-yesod-persistent
+
+.if !defined(HS_YESOD_PERSISTENT_BUILDLINK3_MK)
+HS_YESOD_PERSISTENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-yesod-persistent+=	hs-yesod-persistent>=1.6.0
+BUILDLINK_ABI_DEPENDS.hs-yesod-persistent+=	hs-yesod-persistent>=1.6.0.8
+BUILDLINK_PKGSRCDIR.hs-yesod-persistent?=	../../www/hs-yesod-persistent
+
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-persistent/buildlink3.mk"
+.include "../../devel/hs-persistent-template/buildlink3.mk"
+.include "../../devel/hs-resource-pool/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../www/hs-yesod-core/buildlink3.mk"
+.endif	# HS_YESOD_PERSISTENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-yesod-persistent
