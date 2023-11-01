a6d101131ae954e043626dc371a408a09c89f3e4
gutteridge <gutteridge@pkgsrc.org>
2023-11-01 01:05:54 +0000

    caja: restore introspection support (no default change)
    This was evidently lost many updates ago. Use a potentially more
    resilient and obvious means of recording these components.

--- sysutils/caja/options.mk
+++ sysutils/caja/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2016/06/13 22:31:25 youri Exp $
+# $NetBSD: options.mk,v 1.3 2023/11/01 01:05:54 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.caja
 PKG_SUPPORTED_OPTIONS=	introspection
@@ -6,14 +6,14 @@ PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	introspection
+PLIST_SRC+=	PLIST
 
 .if !empty(PKG_OPTIONS:Mintrospection)
 BUILDLINK_DEPMETHOD.gobject-introspection+=	build
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../graphics/cairo-gobject/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-introspection
-PLIST.introspection=	yes
+PLIST_SRC+=	PLIST.introspection
 .else
 CONFIGURE_ARGS+=	--disable-introspection
 .endif
