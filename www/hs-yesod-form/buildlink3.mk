d5d55e4429dfdb0efe2c38483b799f819e44e681
pho <pho@pkgsrc.org>
2023-11-01 02:57:11 +0000

    www/hs-yesod-form: import hs-yesod-form-1.7.6
    Form handling for Yesod, in the same style as formlets. See the forms
    chapter of the Yesod book: http://www.yesodweb.com/book/forms
    
    This package provides a set of basic form inputs such as text, number,
    time, checkbox, select, textarea, and etc. via Yesod.Form.Fields
    module. Also, there is Yesod.Form.Nic module providing richtext field using
    Nic editor. However, this module is grandfathered now and Nic editor is not
    actively maintained since June 2012. You can find additional richtext
    editor fields in yesod-form-richtext package (currently in provides support
    of Summernote editor only).

---
+++ www/hs-yesod-form/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/01 02:57:11 pho Exp $
+
+BUILDLINK_TREE+=	hs-yesod-form
+
+.if !defined(HS_YESOD_FORM_BUILDLINK3_MK)
+HS_YESOD_FORM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-yesod-form+=	hs-yesod-form>=1.7.6
+BUILDLINK_ABI_DEPENDS.hs-yesod-form+=	hs-yesod-form>=1.7.6
+BUILDLINK_PKGSRCDIR.hs-yesod-form?=	../../www/hs-yesod-form
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../textproc/hs-blaze-html/buildlink3.mk"
+.include "../../textproc/hs-blaze-markup/buildlink3.mk"
+.include "../../devel/hs-byteable/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../mail/hs-email-validate/buildlink3.mk"
+.include "../../devel/hs-persistent/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.include "../../textproc/hs-shakespeare/buildlink3.mk"
+.include "../../www/hs-wai/buildlink3.mk"
+.include "../../www/hs-xss-sanitize/buildlink3.mk"
+.include "../../www/hs-yesod-core/buildlink3.mk"
+.include "../../www/hs-yesod-persistent/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.endif	# HS_YESOD_FORM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-yesod-form
