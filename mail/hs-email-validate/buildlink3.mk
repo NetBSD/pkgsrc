c0ff29bd0470deb6b15896a0a000b90d464f516c
pho <pho@pkgsrc.org>
2023-11-01 02:42:28 +0000

    mail/hs-email-validate: import hs-email-validate-2.3.2.19
    Validating an email address string against RFC 5322.

---
+++ mail/hs-email-validate/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/01 02:42:28 pho Exp $
+
+BUILDLINK_TREE+=	hs-email-validate
+
+.if !defined(HS_EMAIL_VALIDATE_BUILDLINK3_MK)
+HS_EMAIL_VALIDATE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-email-validate+=	hs-email-validate>=2.3.2
+BUILDLINK_ABI_DEPENDS.hs-email-validate+=	hs-email-validate>=2.3.2.19
+BUILDLINK_PKGSRCDIR.hs-email-validate?=		../../mail/hs-email-validate
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.endif	# HS_EMAIL_VALIDATE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-email-validate
