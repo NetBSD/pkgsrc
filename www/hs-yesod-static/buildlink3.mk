cc127ce1dc20b5f822077a2b1b8c02717c1b7aa8
pho <pho@pkgsrc.org>
2023-11-01 03:18:15 +0000

    www/hs-yesod-static: import hs-yesod-static-1.6.1.0
    Static file serving subsite for Yesod Web Framework.

---
+++ www/hs-yesod-static/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/01 03:18:15 pho Exp $
+
+BUILDLINK_TREE+=	hs-yesod-static
+
+.if !defined(HS_YESOD_STATIC_BUILDLINK3_MK)
+HS_YESOD_STATIC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-yesod-static+=	hs-yesod-static>=1.6.1
+BUILDLINK_ABI_DEPENDS.hs-yesod-static+=	hs-yesod-static>=1.6.1.0
+BUILDLINK_PKGSRCDIR.hs-yesod-static?=	../../www/hs-yesod-static
+
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../security/hs-crypton-conduit/buildlink3.mk"
+.include "../../textproc/hs-css-text/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../devel/hs-file-embed/buildlink3.mk"
+.include "../../lang/hjsmin/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../net/hs-mime-types/buildlink3.mk"
+.include "../../devel/hs-unix-compat/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../www/hs-wai/buildlink3.mk"
+.include "../../www/hs-wai-app-static/buildlink3.mk"
+.include "../../www/hs-yesod-core/buildlink3.mk"
+.endif	# HS_YESOD_STATIC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-yesod-static
