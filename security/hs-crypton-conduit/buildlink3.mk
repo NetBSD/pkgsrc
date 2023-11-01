476fb232c39946a9b64728e09a10aac47caf26b7
pho <pho@pkgsrc.org>
2023-11-01 03:08:32 +0000

    security/hs-crypton-conduit: import hs-crypton-conduit-0.2.3
    Conduit bridge for crypton.
    
    For now only provide a conduit version for hash and hmac, but with
    contribution, this could provide cipher conduits too, and probably other
    things.

---
+++ security/hs-crypton-conduit/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/01 03:08:32 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypton-conduit
+
+.if !defined(HS_CRYPTON_CONDUIT_BUILDLINK3_MK)
+HS_CRYPTON_CONDUIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypton-conduit+=	hs-crypton-conduit>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-crypton-conduit+=	hs-crypton-conduit>=0.2.3
+BUILDLINK_PKGSRCDIR.hs-crypton-conduit?=	../../security/hs-crypton-conduit
+
+.include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../devel/hs-conduit-extra/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../devel/hs-resourcet/buildlink3.mk"
+.endif	# HS_CRYPTON_CONDUIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypton-conduit
