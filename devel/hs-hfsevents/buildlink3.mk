853fc5e6aa593c8a55c4bb0a23b2b66b33f98955
pho <pho@pkgsrc.org>
2023-11-01 17:17:00 +0000

    devel/hs-hfsevents: import hs-hfsevents-0.1.6
    File/folder watching for OS X.

---
+++ devel/hs-hfsevents/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/01 17:17:00 pho Exp $
+
+BUILDLINK_TREE+=	hs-hfsevents
+
+.if !defined(HS_HFSEVENTS_BUILDLINK3_MK)
+HS_HFSEVENTS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hfsevents+=	hs-hfsevents>=0.1.6
+BUILDLINK_ABI_DEPENDS.hs-hfsevents+=	hs-hfsevents>=0.1.6
+BUILDLINK_PKGSRCDIR.hs-hfsevents?=	../../devel/hs-hfsevents
+
+.include "../../devel/hs-cereal/buildlink3.mk"
+.endif	# HS_HFSEVENTS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hfsevents
