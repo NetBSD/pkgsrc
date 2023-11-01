eeac0d6a2e319ac6e2f782786e2cbe10b12d5d80
adam <adam@pkgsrc.org>
2023-11-01 18:20:50 +0000

    py-djangocms-picture: updated to 4.1.1
    4.1.1 (2023-10-19)
    
    Updated release workflows
    
    4.1.0
    
    Added support for Django 3.2, 4.0, 4.1, and 4.2
    Added support for Django CMS 3.11
    Added support for Python 3.11
    Dropped support for Django <= 3.1
    Dropped support for Python 3.7

--- www/py-djangocms-picture/patches/patch-setup.py
+++ www/py-djangocms-picture/patches/patch-setup.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-setup.py,v 1.3 2021/01/16 09:59:07 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2023/11/01 18:20:50 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2021-01-16 09:55:16.000000000 +0000
+--- setup.py.orig	2023-11-01 18:18:34.277473378 +0000
 +++ setup.py
-@@ -47,7 +47,7 @@ setup(
-     license='BSD-3-Clause',
+@@ -59,7 +59,7 @@ setup(
      description='Adds an image plugin to django CMS',
-     long_description=open('README.rst').read(),
+     long_description=long_description,
+     long_description_content_type='text/x-rst',
 -    packages=find_packages(),
 +    packages=find_packages(exclude=['tests']),
      include_package_data=True,
