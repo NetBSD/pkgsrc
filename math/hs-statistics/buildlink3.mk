6428e91df6209e28c82ad015aec8c97612c52288
pho <pho@pkgsrc.org>
2023-11-02 02:47:40 +0000

    math/hs-statistics: import hs-statistics-0.16.2.1
    This library provides a number of common functions and types useful in
    statistics. We focus on high performance, numerical robustness, and use of
    good algorithms. Where possible, we provide references to the statistical
    literature.
    
    The library's facilities can be divided into four broad categories:
    
    * Working with widely used discrete and continuous probability
      distributions. (There are dozens of exotic distributions in use; we focus
      on the most common.)
    
    * Computing with sample data: quantile estimation, kernel density
      estimation, histograms, bootstrap methods, significance testing, and
      regression and autocorrelation analysis.
    
    * Random variate generation under several different distributions.
    
    * Common statistical tests for significant differences between samples.

---
+++ math/hs-statistics/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/02 02:47:40 pho Exp $
+
+BUILDLINK_TREE+=	hs-statistics
+
+.if !defined(HS_STATISTICS_BUILDLINK3_MK)
+HS_STATISTICS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-statistics+=	hs-statistics>=0.16.2
+BUILDLINK_ABI_DEPENDS.hs-statistics+=	hs-statistics>=0.16.2.1
+BUILDLINK_PKGSRCDIR.hs-statistics?=	../../math/hs-statistics
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../math/hs-dense-linear-algebra/buildlink3.mk"
+.include "../../math/hs-math-functions/buildlink3.mk"
+.include "../../math/hs-mwc-random/buildlink3.mk"
+.include "../../devel/hs-parallel/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../devel/hs-vector-algorithms/buildlink3.mk"
+.include "../../devel/hs-vector-binary-instances/buildlink3.mk"
+.include "../../devel/hs-vector-th-unbox/buildlink3.mk"
+.endif	# HS_STATISTICS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-statistics
