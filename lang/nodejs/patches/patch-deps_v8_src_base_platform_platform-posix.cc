f38589e11475d2a6bdec91082ea5763c21585236
adam <adam@pkgsrc.org>
2023-11-02 13:18:15 +0000

    nodejs: updated to 21.1.0
    Version 21.1.0 (Current)
    
    Notable Changes
    
    Automatically detect and run ESM syntax
    
    The new flag --experimental-detect-module can be used to automatically run ES modules when their syntax can be detected. For “ambiguous” files, which are .js or extensionless files with no package.json with a type field, Node.js will parse the file to detect ES module syntax; if found, it will run the file as an ES module, otherwise it will run the file as a CommonJS module. The same applies to string input via --eval or STDIN.
    
    We hope to make detection enabled by default in a future version of Node.js. Detection increases startup time, so we encourage everyone — especially package authors — to add a type field to package.json, even for the default "type": "commonjs". The presence of a type field, or explicit extensions such as .mjs or .cjs, will opt out of detection.
    
    
    vm: fix V8 compilation cache support for vm.Script
    
    Previously repeated compilation of the same source code using vm.Script stopped hitting the V8 compilation cache after v16.x when support for importModuleDynamically was added to vm.Script, resulting in a performance regression that blocked users (in particular Jest users) from upgrading from v16.x.
    
    The recent fixes landed in v21.1.0 allow the compilation cache to be hit again for vm.Script when --experimental-vm-modules is not used even in the presence of the importModuleDynamically option, so that users affected by the performance regression can now upgrade. Ongoing work is also being done to enable compilation cache support for vm.CompileFunction.

--- lang/nodejs/patches/patch-deps_v8_src_base_platform_platform-posix.cc
+++ lang/nodejs/patches/patch-deps_v8_src_base_platform_platform-posix.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.12 2023/05/03 14:03:09 nikita Exp $
+$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.13 2023/11/02 13:18:15 adam Exp $
 
 Use sysconf(_SC_THREAD_STACK_MIN) instead of PTHREAD_STACK_MIN.
 Cast explicitly.
@@ -7,9 +7,9 @@ Remove legacy madvise(2) prototypes, prefer posix_madvise(2) if available.
 Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
 (PR port-arm/55533)
 
---- deps/v8/src/base/platform/platform-posix.cc.orig	2023-04-17 23:15:54.000000000 +0200
-+++ deps/v8/src/base/platform/platform-posix.cc	2023-05-03 14:19:05.405513040 +0200
-@@ -75,14 +75,6 @@
+--- deps/v8/src/base/platform/platform-posix.cc.orig	2023-10-24 10:04:41.000000000 +0000
++++ deps/v8/src/base/platform/platform-posix.cc
+@@ -77,14 +77,6 @@
  #define MAP_ANONYMOUS MAP_ANON
  #endif
  
@@ -24,7 +24,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
  #ifndef MADV_FREE
  #define MADV_FREE MADV_DONTNEED
  #endif
-@@ -391,6 +383,10 @@
+@@ -399,6 +391,10 @@ void* OS::GetRandomMmapAddr() {
  #endif
  #endif
  #endif
@@ -35,7 +35,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
    return reinterpret_cast<void*>(raw_addr);
  }
  
-@@ -553,14 +549,11 @@
+@@ -565,14 +561,11 @@ bool OS::DiscardSystemPages(void* addres
      // MADV_FREE_REUSABLE sometimes fails, so fall back to MADV_DONTNEED.
      ret = madvise(address, size, MADV_DONTNEED);
    }
@@ -52,7 +52,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
  #else
    int ret = madvise(address, size, MADV_DONTNEED);
  #endif
-@@ -802,6 +795,8 @@
+@@ -815,6 +808,8 @@ int OS::GetCurrentThreadId() {
    return static_cast<int>(syscall(__NR_gettid));
  #elif V8_OS_ANDROID
    return static_cast<int>(gettid());
@@ -61,9 +61,9 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
  #elif V8_OS_AIX
    return static_cast<int>(thread_self());
  #elif V8_OS_FUCHSIA
-@@ -1075,7 +1070,11 @@
-     : data_(new PlatformData),
+@@ -1091,7 +1086,11 @@ Thread::Thread(const Options& options)
        stack_size_(options.stack_size()),
+       priority_(options.priority()),
        start_semaphore_(nullptr) {
 +#if V8_OS_NETBSD
 +  const int min_stack_size = sysconf(_SC_THREAD_STACK_MIN);
@@ -73,7 +73,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
    if (stack_size_ > 0) stack_size_ = std::max(stack_size_, min_stack_size);
    set_name(options.name());
  }
-@@ -1091,7 +1090,7 @@
+@@ -1106,7 +1105,7 @@ static void SetThreadName(const char* na
    pthread_set_name_np(pthread_self(), name);
  #elif V8_OS_NETBSD
    static_assert(Thread::kMaxThreadNameLength <= PTHREAD_MAX_NAMELEN_NP);
