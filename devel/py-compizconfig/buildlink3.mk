037331215908df3a0888fc19fbaed1194d358370
wiz <wiz@pkgsrc.org>
2023-11-02 12:20:01 +0000

    *: recursive bump for protobuf

--- devel/py-compizconfig/buildlink3.mk
+++ devel/py-compizconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/04/19 08:08:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2023/11/02 12:20:03 wiz Exp $
 
 BUILDLINK_TREE+=	${PYPKGPREFIX}-compizconfig
 
@@ -8,7 +8,7 @@ PY_COMPIZCONFIG_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-compizconfig+=	${PYPKGPREFIX}-compizconfig>=0.6.0
-BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-compizconfig+=	${PYPKGPREFIX}-compizconfig>=0.8.4nb71
+BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-compizconfig+=	${PYPKGPREFIX}-compizconfig>=0.8.4nb72
 BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-compizconfig?=	../../devel/py-compizconfig
 
 .include "../../devel/libcompizconfig/buildlink3.mk"
--- graphics/opencv-contrib-face/buildlink3.mk
+++ graphics/opencv-contrib-face/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/10/21 17:10:37 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 12:20:03 wiz Exp $
 
 BUILDLINK_TREE+=	opencv-contrib-face
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	opencv-contrib-face
 OPENCV_CONTRIB_FACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencv-contrib-face+=	opencv-contrib-face>=3.0.0
-BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.16nb10
+BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.16nb11
 BUILDLINK_PKGSRCDIR.opencv-contrib-face?=	../../graphics/opencv-contrib-face
 
 .include "../../graphics/opencv/buildlink3.mk"
