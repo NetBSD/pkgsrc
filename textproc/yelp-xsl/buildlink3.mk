a0c188afefc63636081b13773391296f8c3d0b00
wiz <wiz@pkgsrc.org>
2023-11-02 10:23:35 +0000

    yelp-xsl: update to 42.1.
    42.1
    ====
    * Updated translations:
      bg    (Alexander Shopov)
      hr    (Goran Vidović)
      ka    (Zurab Kargareteli)
      sr    (Марко Костић)
      sv    (Anders Jonsson)
    
    42.0
    ====
    * No changes since beta, stable release only
    
    42.beta
    =======
    * Fix topic and section links in Mallard stack files
    * Avoid use of global l10n.locale parameter
    * Updated translations:
      ab    (Naala Nanba)
      hr    (Goran Vidović)
      is    (Sveinn í Felli)
    
    41.0
    ====
    * No changes since beta, stable release only
    
    41.beta
    =======
    * Loosen CSS match for title/desc in linkdiv
    * Use better target contrasts for white-on-black
    
    40.2
    ====
    * Updated translations:
      he    (Yaron Shahrabani)
      ne    (Pawan Chitrakar)
      oc    (Quentin PAGÈS)
      vi    (Ngọc Quân Trần)
    
    40.0
    ====
    * Stable release. No changes since release candidate.
    
    40.rc
    =====
    * Updated translations:
      gl    (Fran Dieguez)
      sl    (Matej Urbančič)
    
    40.beta
    =======
    * Added new text templates for easier CSS editing
    * Updated bundled highlight.js and added SML highlighter
    * Added experimental links styles for the GNOME 40 help
    * Added html.grid.size parameter to templatize CSS grid size
    * Hide sidebars by default at 720px
    * Updated COPYING file, which was pretty out of date
    * Some documentation updates and improvements
    * Updated translations:
      be    (Źmicier Turok)
      ca    (Jordi Mas)
      nb    (Kjartan Maraas)
      pt    (Juliano de Souza Camargo)
    
    3.38.1
    ======
    * Updated translations:
      lv    (Rūdolfs Mazurs)
      sk    (Dušan Kazik)
    
    3.38.0
    ======
    * Updated translations:
      ja    (sicklylife)
    
    3.37.90
    =======
    * Various styling changes, especially around media and ttml
    * Updated translations:
      ckb      (Jwtiyar Nariman)
      nl       (Hannie Dumoleyn)
      ro       (Florentina Mușat)
      uk       (Daniel Korostil)
      zh_CN    (Aron Xu)
    
    3.36.0
    ======
    * Added a syntax highlighter for Ducktype
    * Support multiple desc elements for links (MEP-0008)
    * Fixed missing link when group specified twice on topic links
    * Typo fix in docs
    * Updated translations:
      dz    (Andre Klapper)
      en_GB (Zander Brown)
      ms    (Umarzuki Bin Mochlis Moktar)
      tr    (Emin Tufan Çetin)
    
    3.34.2
    ======
    * Updated translations:
      fa      (Danial Behzadi)
    
    3.34.0
    ======
    * Updated translations:
      it    (Milo Casagrande)
    
    3.33.92
    =======
    * Updated translations:
      ca    (Jordi Mas)
      de    (Tim Sabsch)
      eu    (Asier Sarasua Garmendia)
      fr    (Claude Paroz)
      lt    (Aurimas Černius)
      ru    (Stas Solovey)
    
    3.33.4
    ======
    * Update highlight.pack.js to 9.15.8, added awk and shell (Shaun McCance)
    * Removed intltool and other build updates (Jeremy Bicha, Javier Jardón)
    * Updated translations:
      es    (Daniel Mustieles)
      hu    (Balázs Úr)
      id    (Kukuh Syafaat)
      pl    (Piotr Drąg)
      pt_BR (Rafael Fontenelle)
      zh_CN (Mingcong Bai)

--- textproc/yelp-xsl/buildlink3.mk
+++ textproc/yelp-xsl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/08/14 05:25:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/11/02 10:23:35 wiz Exp $
 
 BUILDLINK_TREE+=	yelp-xsl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	yelp-xsl
 YELP_XSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.yelp-xsl+=	yelp-xsl>=3.3.1
-BUILDLINK_ABI_DEPENDS.yelp-xsl?=		yelp-xsl>=3.32.1nb2
+BUILDLINK_ABI_DEPENDS.yelp-xsl?=	yelp-xsl>=3.32.1nb2
 BUILDLINK_PKGSRCDIR.yelp-xsl?=		../../textproc/yelp-xsl
 .endif	# YELP_XSL_BUILDLINK3_MK
 
