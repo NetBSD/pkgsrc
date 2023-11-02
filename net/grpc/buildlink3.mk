6d07d5270e35bd0b418bf5a8cb8821718bb4d123
wiz <wiz@pkgsrc.org>
2023-11-02 12:47:37 +0000

    *: recursive bump for grpc

--- net/grpc/buildlink3.mk
+++ net/grpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/11/02 12:20:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2023/11/02 12:47:37 wiz Exp $
 
 BUILDLINK_TREE+=	grpc
 
@@ -8,7 +8,7 @@ GRPC_BUILDLINK3_MK:=
 USE_CXX_FEATURES+=	c++14
 
 BUILDLINK_API_DEPENDS.grpc+=	grpc>=1.11.0
-BUILDLINK_ABI_DEPENDS.grpc+=	grpc>=1.54.3nb3
+BUILDLINK_ABI_DEPENDS.grpc+=	grpc>=1.59.2
 BUILDLINK_PKGSRCDIR.grpc?=	../../net/grpc
 
 BUILDLINK_FILES.grpc+=	bin/grpc_cpp_plugin
