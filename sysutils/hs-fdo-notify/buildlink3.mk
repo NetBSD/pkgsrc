89fbb0d3f91805b358837f2d95ae9cf29a1276a1
pho <pho@pkgsrc.org>
2023-11-02 01:30:08 +0000

    sysutils/hs-fdo-notify: import hs-fdo-notify-0.3.1
    A library for issuing notifications using FreeDesktop.org's Desktop
    Notifications protcol. This protocol is supported by services such as
    Ubuntu's NotifyOSD.

---
+++ sysutils/hs-fdo-notify/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/02 01:30:08 pho Exp $
+
+BUILDLINK_TREE+=	hs-fdo-notify
+
+.if !defined(HS_FDO_NOTIFY_BUILDLINK3_MK)
+HS_FDO_NOTIFY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-fdo-notify+=	hs-fdo-notify>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-fdo-notify+=	hs-fdo-notify>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-fdo-notify?=	../../sysutils/hs-fdo-notify
+
+.include "../../sysutils/hs-dbus/buildlink3.mk"
+.endif	# HS_FDO_NOTIFY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-fdo-notify
