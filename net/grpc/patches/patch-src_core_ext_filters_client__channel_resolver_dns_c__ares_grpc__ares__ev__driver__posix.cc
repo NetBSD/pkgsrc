ed79d457959946122ba68c0f41e679c9591976d8
wiz <wiz@pkgsrc.org>
2023-11-02 12:46:44 +0000

    *grpc*: update to 1.59.2
    Lots of changes.

--- net/grpc/patches/patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc
+++ net/grpc/patches/patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc,v 1.4 2021/10/06 10:58:30 adam Exp $
+$NetBSD: patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc,v 1.5 2023/11/02 12:46:44 wiz Exp $
 
 Add SunOS support.
 
---- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc.orig	2021-09-25 02:33:41.000000000 +0000
+--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc.orig	2023-10-30 17:33:54.000000000 +0000
 +++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc
 @@ -22,6 +22,11 @@
+ #if GRPC_ARES == 1 && defined(GRPC_POSIX_SOCKET_ARES_EV_DRIVER)
  
- #include <string.h>
- #include <sys/ioctl.h>
+ // IWYU pragma: no_include <ares_build.h>
 +#ifdef __sun
 +#include <sys/filio.h>
 +#include <unistd.h>
 +#include <stropts.h>
 +#endif
  
- #include <ares.h>
- 
+ #include <sys/ioctl.h>
+ #include <sys/socket.h>
--- net/py-grpcio/patches/patch-setup.py
+++ net/py-grpcio/patches/patch-setup.py
@@ -1,60 +1,60 @@
-$NetBSD: patch-setup.py,v 1.15 2023/08/03 18:58:48 adam Exp $
+$NetBSD: patch-setup.py,v 1.16 2023/11/02 12:46:44 wiz Exp $
 
 Fix libatomic detection.
 Use dependencies in pkgsrc.
 
---- setup.py.orig	2023-08-01 19:15:12.000000000 +0000
+--- setup.py.orig	2023-10-30 18:02:41.000000000 +0000
 +++ setup.py
-@@ -315,32 +315,7 @@ EXTENSION_INCLUDE_DIRECTORIES = ((PYTHON
-                                  UPBDEFS_GRPC_GENERATED_INCLUDE +
-                                  XXHASH_INCLUDE + ZLIB_INCLUDE)
+@@ -350,33 +350,7 @@ EXTENSION_INCLUDE_DIRECTORIES = (
+     + ZLIB_INCLUDE
+ )
  
 -EXTENSION_LIBRARIES = ()
 -if "linux" in sys.platform:
--    EXTENSION_LIBRARIES += ('rt',)
+-    EXTENSION_LIBRARIES += ("rt",)
 -if not "win32" in sys.platform:
--    EXTENSION_LIBRARIES += ('m',)
+-    EXTENSION_LIBRARIES += ("m",)
 -if "win32" in sys.platform:
 -    EXTENSION_LIBRARIES += (
--        'advapi32',
--        'bcrypt',
--        'dbghelp',
--        'ws2_32',
+-        "advapi32",
+-        "bcrypt",
+-        "dbghelp",
+-        "ws2_32",
 -    )
 -if BUILD_WITH_SYSTEM_OPENSSL:
 -    EXTENSION_LIBRARIES += (
--        'ssl',
--        'crypto',
+-        "ssl",
+-        "crypto",
 -    )
 -if BUILD_WITH_SYSTEM_ZLIB:
--    EXTENSION_LIBRARIES += ('z',)
+-    EXTENSION_LIBRARIES += ("z",)
 -if BUILD_WITH_SYSTEM_CARES:
--    EXTENSION_LIBRARIES += ('cares',)
+-    EXTENSION_LIBRARIES += ("cares",)
 -if BUILD_WITH_SYSTEM_RE2:
--    EXTENSION_LIBRARIES += ('re2',)
+-    EXTENSION_LIBRARIES += ("re2",)
 -if BUILD_WITH_SYSTEM_ABSL:
 -    EXTENSION_LIBRARIES += tuple(
--        lib.stem[3:] for lib in pathlib.Path('/usr').glob('lib*/libabsl_*.so'))
+-        lib.stem[3:] for lib in pathlib.Path("/usr").glob("lib*/libabsl_*.so")
+-    )
 +EXTENSION_LIBRARIES = ('grpc',)
  
- DEFINE_MACROS = (('_WIN32_WINNT', 0x600),)
+ DEFINE_MACROS = (("_WIN32_WINNT", 0x600),)
  asm_files = []
-@@ -443,7 +418,7 @@ def cython_extensions_and_necessity():
+@@ -494,7 +468,7 @@ def cython_extensions_and_necessity():
      ]
-     config = os.environ.get('CONFIG', 'opt')
-     prefix = 'libs/' + config + '/'
+     config = os.environ.get("CONFIG", "opt")
+     prefix = "libs/" + config + "/"
 -    if USE_PREBUILT_GRPC_CORE:
 +    if False:
          extra_objects = [
-             prefix + 'libares.a', prefix + 'libboringssl.a',
-             prefix + 'libgpr.a', prefix + 'libgrpc.a'
-@@ -455,8 +430,7 @@ def cython_extensions_and_necessity():
-     extensions = [
-         _extension.Extension(
-             name=module_name,
--            sources=([module_file] + list(CYTHON_HELPER_C_FILES) +
--                     core_c_files + asm_files),
-+            sources=[module_file] + list(CYTHON_HELPER_C_FILES),
+             prefix + "libares.a",
+             prefix + "libboringssl.a",
+@@ -511,8 +485,6 @@ def cython_extensions_and_necessity():
+             sources=(
+                 [module_file]
+                 + list(CYTHON_HELPER_C_FILES)
+-                + core_c_files
+-                + asm_files
+             ),
              include_dirs=list(EXTENSION_INCLUDE_DIRECTORIES),
              libraries=list(EXTENSION_LIBRARIES),
-             define_macros=list(DEFINE_MACROS),
