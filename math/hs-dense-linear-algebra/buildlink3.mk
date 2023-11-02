56152d633759608f8d0ee810c3b0bcce9e16c6dc
pho <pho@pkgsrc.org>
2023-11-02 02:44:38 +0000

    math/hs-dense-linear-algebra: import hs-dense-linear-algebra-0.1.0.0
    This library is simply collection of linear-algebra related modules split
    from statistics library.

---
+++ math/hs-dense-linear-algebra/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/02 02:44:38 pho Exp $
+
+BUILDLINK_TREE+=	hs-dense-linear-algebra
+
+.if !defined(HS_DENSE_LINEAR_ALGEBRA_BUILDLINK3_MK)
+HS_DENSE_LINEAR_ALGEBRA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-dense-linear-algebra+=	hs-dense-linear-algebra>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-dense-linear-algebra+=	hs-dense-linear-algebra>=0.1.0.0
+BUILDLINK_PKGSRCDIR.hs-dense-linear-algebra?=	../../math/hs-dense-linear-algebra
+
+.include "../../math/hs-math-functions/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../devel/hs-vector-algorithms/buildlink3.mk"
+.include "../../devel/hs-vector-binary-instances/buildlink3.mk"
+.include "../../devel/hs-vector-th-unbox/buildlink3.mk"
+.endif	# HS_DENSE_LINEAR_ALGEBRA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-dense-linear-algebra
