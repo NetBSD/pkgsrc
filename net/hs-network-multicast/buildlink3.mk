94c4f999182989c671965ac40169a1420120ab2c
pho <pho@pkgsrc.org>
2023-11-02 01:39:28 +0000

    net/hs-network-multicast: import hs-network-multicast-0.3.2
    The Network.Multicast module is for sending UDP datagrams over multicast
    (class D) addresses.

---
+++ net/hs-network-multicast/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/02 01:39:28 pho Exp $
+
+BUILDLINK_TREE+=	hs-network-multicast
+
+.if !defined(HS_NETWORK_MULTICAST_BUILDLINK3_MK)
+HS_NETWORK_MULTICAST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-network-multicast+=	hs-network-multicast>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-network-multicast+=	hs-network-multicast>=0.3.2
+BUILDLINK_PKGSRCDIR.hs-network-multicast?=	../../net/hs-network-multicast
+
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-bsd/buildlink3.mk"
+.endif	# HS_NETWORK_MULTICAST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-network-multicast
