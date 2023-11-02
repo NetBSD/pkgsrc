7c52950691c031885e5082c2ef42c98678f722e9
adam <adam@pkgsrc.org>
2023-11-02 13:22:01 +0000

    nodejs20: cleanup

--- lang/nodejs20/buildlink3.mk
+++ lang/nodejs20/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/11/02 13:16:54 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 13:22:01 adam Exp $
 
 BUILDLINK_TREE+=	nodejs
 
@@ -7,7 +7,7 @@ NODEJS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nodejs+=	nodejs>=10
 BUILDLINK_ABI_DEPENDS.nodejs+=	nodejs>=20.8.1nb2
-BUILDLINK_PKGSRCDIR.nodejs?=	../../lang/nodejs
+BUILDLINK_PKGSRCDIR.nodejs?=	../../lang/nodejs20
 
 .include "../../mk/bsd.fast.prefs.mk"
 
