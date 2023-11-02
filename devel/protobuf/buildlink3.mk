a7e03976427d257cb25afad3372a0e9a75a8625f
wiz <wiz@pkgsrc.org>
2023-11-02 12:17:25 +0000

    {,py-}protobuf: update to 24.4
    Based on wip package by bsiegert.
    
    Ten months of heavy development.

--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,14 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/07/18 14:11:17 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 12:17:25 wiz Exp $
 
 BUILDLINK_TREE+=	protobuf
 
 .if !defined(PROTOBUF_BUILDLINK3_MK)
 PROTOBUF_BUILDLINK3_MK:=
 
-USE_CXX_FEATURES+=	c++11
-
-BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
-BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=3.19.0nb1
+BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=24.4
+BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=24.4
 BUILDLINK_PKGSRCDIR.protobuf?=		../../devel/protobuf
 
 .include "../../devel/zlib/buildlink3.mk"
