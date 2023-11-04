dbdc26c38adc0b8ef771fbb47351f13488ca3bfc
wiz <wiz@pkgsrc.org>
2023-11-04 00:12:55 +0000

    x11/gtksourceview5: import gtksourceview5-5.10.0
    GtkSourceView is a text widget that extends the standard GTK+ 4.x text
    widget GtkTextView.  It improves GtkTextView by implementing syntax
    highlighting and other features typical of a source code editor.
    
    This package contains major version 5 of the widget.

---
+++ x11/gtksourceview5/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2023/11/04 00:12:55 wiz Exp $
+
+BUILDLINK_TREE+=	gtksourceview5
+
+.if !defined(GTKSOURCEVIEW5_BUILDLINK3_MK)
+GTKSOURCEVIEW5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtksourceview5+=	gtksourceview5>=5.0.0
+BUILDLINK_PKGSRCDIR.gtksourceview5?=	../../x11/gtksourceview5
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk4/buildlink3.mk"
+.endif	# GTKSOURCEVIEW5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtksourceview5
