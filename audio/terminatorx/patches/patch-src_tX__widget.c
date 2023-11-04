8d2f8d696baf37979f456a1463b96732b266ac5a
wiz <wiz@pkgsrc.org>
2023-11-04 10:02:57 +0000

    terminatorx: update to 4.2.0.
    4.2.0
    
    A new sinc based interpolator that improves resampling quality
    quite a bit, if you prefer the old aliasing artifacts a new
    configuration option allows to switch back to the old linear
    interpolation.
    
    Updated the artwork a bit with a “Tux under threat” theme (yes,
    that is another Public Enemy reference) – the source code based
    label was really dated.
    
    And the usual minor bug fixes.

--- audio/terminatorx/patches/patch-src_tX__widget.c
+++ audio/terminatorx/patches/patch-src_tX__widget.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_tX__widget.c,v 1.1 2019/10/05 12:09:26 nia Exp $
+$NetBSD: patch-src_tX__widget.c,v 1.2 2023/11/04 10:02:57 wiz Exp $
 
 Use standard stdlib.h instead of non-standard malloc.h.
 
---- src/tX_widget.c.orig	2016-07-24 14:24:08.000000000 +0000
+--- src/tX_widget.c.orig	2022-08-25 14:40:07.000000000 +0000
 +++ src/tX_widget.c
 @@ -28,7 +28,6 @@
- #include "tX_widget.h"
  #include "tX_types.h"
- #include "tX_global.h"
+ #include "tX_widget.h"
+ #include <gtk/gtk.h>
 -#include <malloc.h>
  #include <stdlib.h>
  #include <string.h>
