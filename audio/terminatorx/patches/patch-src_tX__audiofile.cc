f9f5c6ba54d794c5d64cfddb0c14de90b51c38c6
wiz <wiz@pkgsrc.org>
2023-11-04 10:02:57 +0000

    terminatorx: update to 4.2.0.
    4.2.0
    
    A new sinc based interpolator that improves resampling quality
    quite a bit, if you prefer the old aliasing artifacts a new
    configuration option allows to switch back to the old linear
    interpolation.
    
    Updated the artwork a bit with a “Tux under threat” theme (yes,
    that is another Public Enemy reference) – the source code based
    label was really dated.
    
    And the usual minor bug fixes.

--- audio/terminatorx/patches/patch-src_tX__audiofile.cc
+++ audio/terminatorx/patches/patch-src_tX__audiofile.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_tX__audiofile.cc,v 1.1 2019/10/05 12:09:26 nia Exp $
+$NetBSD: patch-src_tX__audiofile.cc,v 1.2 2023/11/04 10:02:57 wiz Exp $
 
 Use standard stdlib.h instead of non-standard malloc.h.
 
---- src/tX_audiofile.cc.orig	2016-07-24 14:24:08.000000000 +0000
+--- src/tX_audiofile.cc.orig	2022-08-25 14:40:07.000000000 +0000
 +++ src/tX_audiofile.cc
-@@ -30,7 +30,7 @@
- #include "tX_audiofile.h"
- 
- #include <string.h>
+@@ -32,7 +32,7 @@
+ #include "tX_endian.h"
+ #include "tX_loaddlg.h"
+ #include "wav_file.h"
 -#include <malloc.h>
 +#include <stdlib.h>
- #include "wav_file.h"
- #include "tX_loaddlg.h"
- #include "tX_endian.h"
+ #include <string.h>
+ 
+ #define __STDC_FORMAT_MACROS
