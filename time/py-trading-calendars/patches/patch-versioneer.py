461fcc63897a01742d19af946ed146e8a54ece15
wiz <wiz@pkgsrc.org>
2023-11-05 20:31:16 +0000

    py-trading-calendars: update to 2.1.1.
    Two years of development.

---
+++ time/py-trading-calendars/patches/patch-versioneer.py
@@ -0,0 +1,18 @@
+$NetBSD: patch-versioneer.py,v 1.1 2023/11/05 20:31:17 wiz Exp $
+
+Fix build with Python 3.12.
+
+--- versioneer.py.orig	2021-01-05 01:45:29.000000000 +0000
++++ versioneer.py
+@@ -339,9 +339,9 @@ def get_config_from_root(root):
+     # configparser.NoOptionError (if it lacks "VCS="). See the docstring at
+     # the top of versioneer.py for instructions on writing your setup.cfg .
+     setup_cfg = os.path.join(root, "setup.cfg")
+-    parser = configparser.SafeConfigParser()
++    parser = configparser.ConfigParser()
+     with open(setup_cfg, "r") as f:
+-        parser.readfp(f)
++        parser.read_file(f)
+     VCS = parser.get("versioneer", "VCS")  # mandatory
+ 
+     def get(parser, name):
