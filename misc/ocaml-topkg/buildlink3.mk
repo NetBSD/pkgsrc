9cb5f485151afc94725b3d0ab8bdac452a5bf9ee
jaapb <jaapb@pkgsrc.org>
2023-11-05 21:08:20 +0000

    Updated misc/ocaml-topkg to version 1.0.7.
    Changes:
    * Fix install of C stubs in byte code only installations.
    * Fix native dynlink detection on OCaml 5.0 and thus cmxs file
      installation.

--- misc/ocaml-topkg/buildlink3.mk
+++ misc/ocaml-topkg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/05/24 18:51:54 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2023/11/05 21:08:20 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-topkg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-topkg
 OCAML_TOPKG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.5nb1
+BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.7
 BUILDLINK_PKGSRCDIR.ocaml-topkg?=	../../misc/ocaml-topkg
 
 .endif	# OCAML_TOPKG_BUILDLINK3_MK
