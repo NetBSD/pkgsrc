0d5330ff8b8c938c3204d04f4f18757d7593712e
jaapb <jaapb@pkgsrc.org>
2023-11-05 21:28:13 +0000

    Updated devel/ocaml-dune-configurator to version 3.11.0.
    This package comes with the main dune distribution, but it needed some
    massaging (post-extract) to make it work properly.

--- devel/ocaml-dune-configurator/buildlink3.mk
+++ devel/ocaml-dune-configurator/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/05/24 18:51:48 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/11/05 21:28:13 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune-configurator
 
 .if !defined(OCAML_DUNE_CONFIGURATOR_BUILDLINK3_MK)
 OCAML_DUNE_CONFIGURATOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=2.7.1
-BUILDLINK_ABI_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=2.9.3nb3
+BUILDLINK_API_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.11.1
 BUILDLINK_PKGSRCDIR.ocaml-dune-configurator?=	../../devel/ocaml-dune-configurator
 
 .include "../../devel/ocaml-dune/buildlink3.mk"
