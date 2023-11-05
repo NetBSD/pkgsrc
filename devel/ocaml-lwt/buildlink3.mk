fdec401f945bb0b35a76f334df6f95fad9e3d17e
jaapb <jaapb@pkgsrc.org>
2023-11-05 21:31:38 +0000

    Updated devel/ocaml-lwt to version 5.7.0.
    I've removed superfluous dependencies from the package. Upstream changes
    are many, so I'll add only the ones that break API (for the rest, see
    the CHANGES file in the distribution):
    
    * Lwt_result.catch now takes a function (unit -> 'a t)
      rather than a promise ('a t).
    * Remove the deprecated Lwt.result type (use Stdlib.result instead).
    * Remove the deprecated Lwt.make_value and Lwt.make_result functions
      (use Ok and Error instead).
    * Remove the deprecated and unsafe waiter_of_wakener
      (keep the waiter around when you create the wakener instead).
    * Remove the deprecated Lwt_stream.on_termination and
      Lwt_stream.on_terminate (bind to Lwt_stream.closed instead).
    * Remove the deprecated Lwt_stream.result type (use Stdlib.result instead).
    * Remove the deprecated Lwt_stream.map_exn function (use wrap_exn instead).

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2022/05/02 14:02:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2023/11/05 21:31:38 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt
 OCAML_LWT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=5.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=5.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=5.7.0
 BUILDLINK_PKGSRCDIR.ocaml-lwt?=		../../devel/ocaml-lwt
 
 #.include "../../devel/libev/buildlink3.mk"
