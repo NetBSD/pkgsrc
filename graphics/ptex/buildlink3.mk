13e87f34bd6e777fc9f2a3d7cf12eca874111750
nros <nros@pkgsrc.org>
2023-11-07 17:56:55 +0000

    Update graphics/ptex to version 2.4.2
    Pkgsrc changes:
    Use CMake to check for alloca.h .
    Include zlib and pthread in
    buildlink3.mk since the are
    interface linked in the exported
    cmake targets.
    
    Changes:
    Export cmake targets to cmake users.
    Use newer spinlock API:s on MacOSX.
    Use soname versioning.

--- graphics/ptex/buildlink3.mk
+++ graphics/ptex/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/10/18 02:47:16 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/11/07 17:56:55 nros Exp $
 
 BUILDLINK_TREE+=	ptex
 
@@ -6,7 +6,11 @@ BUILDLINK_TREE+=	ptex
 PTEX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ptex+=	ptex>=2.1.28
+BUILDLINK_ABI_DEPENDS.ptex+=	ptex>=2.4.2
 BUILDLINK_PKGSRCDIR.ptex?=	../../graphics/ptex
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 .endif	# PTEX_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ptex
--- graphics/ptex/patches/patch-src_ptex_PtexPlatform.h
+++ graphics/ptex/patches/patch-src_ptex_PtexPlatform.h
@@ -1,16 +1,17 @@
-$NetBSD: patch-src_ptex_PtexPlatform.h,v 1.1 2016/10/18 02:47:17 kamil Exp $
+$NetBSD: patch-src_ptex_PtexPlatform.h,v 1.2 2023/11/07 17:56:56 nros Exp $
 
 The alloca(3) function is an extension and place of its definition varies.
 The <alloca.h> header is required on SunOS and acceptable on other systems, but
-unavailable on NetBSD.
+unavailable on NetBSD so use CMake to test for it.
+<alloca.h> is not needed when using Ptex. 
 
---- src/ptex/PtexPlatform.h.orig	2016-04-04 17:14:05.000000000 +0000
+--- src/ptex/PtexPlatform.h.orig	2022-08-05 02:02:51.000000000 +0000
 +++ src/ptex/PtexPlatform.h
-@@ -62,7 +62,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+@@ -70,7 +70,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  
  // linux/unix/posix
  #include <stdlib.h>
-+#if !defined(__NetBSD__)
++#if defined(PTEX_HAVE_ALLOCA_H)
  #include <alloca.h>
 +#endif
  #include <string.h>
