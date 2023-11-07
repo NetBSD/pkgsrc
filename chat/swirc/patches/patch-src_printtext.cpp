9eec51bc58f8f759caada04728afe80ab07602e8
fcambus <fcambus@pkgsrc.org>
2023-11-07 07:58:36 +0000

    swirc: update to 3.4.6.
    ## [3.4.6] - 2023-11-05 ##
    - **Added** adoption of SPDX-tags for the distributed manual pages
    - **Added** better detection of server Wallops
    - **Added** command-line option `-S` (Force TLS)
    - **Added** event 249 (`RPL_STATSDEBUG`)
    - **Added** extensive use of the `CPPFLAGS` make macro
    - **Added** handling of empty user@host combinations in Wallops messages
    - **Added** handling of znc messages. (Made them look like notices and
      appear in the active window.)
    - **Added** protection for bad filenames for IRC logs
    - **Added** translations
    - **Changed** the way of recognizing a user mode versus channel mode
    - **Did** code improvements
    - **Fixed** occurrences of hardcoded function names in messages
    - **Fixed** typos in manual pages
    - **Improved** C++ exception handling
    - **Limited** notification messages in length
    
    ## [3.4.5] - 2023-09-14 ##
    - **Added** command-line option `-W`
    - **Added** tab completion for:
      - `/cs`, `/chanserv`
      - `/ns`, `/nickserv`
      - `/squery`
    - **Added** the following commands:
      - `/admin`
      - `/die`
      - `/gline`
      - `/info`
      - `/ison`
      - `/kline`
      - `/rehash`
      - `/restart`
      - `/servstats`
      - `/wallops`
    - **Added** the following events:
      - 211 (`RPL_STATSLINKINFO`)
      - 212 (`RPL_STATSCOMMANDS`)
      - 216 (`RPL_STATSKLINE`)
      - 219 (`RPL_ENDOFSTATS`)
      - 242 (`RPL_STATSUPTIME`)
      - 256 (`RPL_ADMINME`)
      - 257 (`RPL_ADMINLOC1`)
      - 258 (`RPL_ADMINLOC2`)
      - 259 (`RPL_ADMINEMAIL`)
      - 303 (`RPL_ISON`)
      - 371 (`RPL_INFO`)
      - 374 (`RPL_ENDOFINFO`)
    - **Added** translations
    - **Did** code improvements

--- chat/swirc/patches/patch-src_printtext.cpp
+++ chat/swirc/patches/patch-src_printtext.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_printtext.cpp,v 1.2 2023/01/07 09:37:37 fcambus Exp $
+$NetBSD: patch-src_printtext.cpp,v 1.3 2023/11/07 07:58:36 fcambus Exp $
 
 Support iconv prototype on NetBSD 9 and earlier.
 
---- src/printtext.cpp.orig	2023-01-07 02:57:47.000000000 +0000
+--- src/printtext.cpp.orig	2023-11-05 13:36:56.000000000 +0000
 +++ src/printtext.cpp
 @@ -39,6 +39,15 @@
  #include <iconv.h>
@@ -20,7 +20,7 @@ Support iconv prototype on NetBSD 9 and earlier.
  #include "assertAPI.h"
  #include "atomicops.h"
  #ifdef UNIT_TESTING
-@@ -1364,8 +1373,13 @@ get_buffer(const char *orig)
+@@ -1411,8 +1420,13 @@ get_buffer(CSTRING orig)
  		out[outbytes] = '\0';
  		out_p = addrof(out[0]);
  		errno = 0;
