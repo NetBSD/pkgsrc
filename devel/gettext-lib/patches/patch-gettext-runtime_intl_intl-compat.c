619f5a488acef183151b6f93053f9326e3a60143
wiz <wiz@pkgsrc.org>
2023-11-07 10:51:10 +0000

    gettext*: update to 0.22.3
    Version 0.22.3 - October 2023
    
    * Portability:
      - The libintl library now works on macOS 14.  (Older versions of libintl
        crash on macOS 14, due to an incompatible change in macOS.)
    
    Version 0.22.2 - September 2023
    
    * Bug fixes:
      - The libintl shared library now exports again some symbols that were
        accidentally missing.
        <https://savannah.gnu.org/bugs/index.php?64323>
        This bug was introduced in version 0.22.
    
    Version 0.22.1 - September 2023
    
    * Bug fixes:
      - xgettext's processing of large Perl files may have led to errors
        <https://savannah.gnu.org/bugs/index.php?64552>
      - "xgettext --join-existing" could encounter errors.
        <https://savannah.gnu.org/bugs/index.php?64490>
      These bugs were introduced in version 0.22.
    
    * Portability:
      - Building on Android is now supported.

--- devel/gettext-lib/patches/patch-gettext-runtime_intl_intl-compat.c
+++ devel/gettext-lib/patches/patch-gettext-runtime_intl_intl-compat.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-gettext-runtime_intl_intl-compat.c,v 1.1 2023/06/30 08:31:04 tnn Exp $
+$NetBSD: patch-gettext-runtime_intl_intl-compat.c,v 1.2 2023/11/07 10:51:11 wiz Exp $
 
 Make sure bindtextdomain(3) is visible on macOS, as it cannot be exported otherwise:
 
 ld: warning: cannot export hidden symbol _bindtextdomain from .libs/intl-compat.o
 
---- gettext-runtime/intl/intl-compat.c.orig	2023-06-30 08:04:29.086186639 +0000
+--- gettext-runtime/intl/intl-compat.c.orig	2023-09-19 20:11:19.000000000 +0000
 +++ gettext-runtime/intl/intl-compat.c
-@@ -47,7 +47,7 @@
+@@ -54,7 +54,7 @@
  /* When building a DLL, we must export some functions.  Note that because
     the functions are only defined for binary backward compatibility, we
     don't need to use __declspec(dllimport) in any case.  */
@@ -14,4 +14,4 @@ ld: warning: cannot export hidden symbol _bindtextdomain from .libs/intl-compat.
 +#if HAVE_VISIBILITY && BUILDING_DLL || defined(__APPLE__)
  # define DLL_EXPORTED __attribute__((__visibility__("default")))
  #elif defined _MSC_VER && BUILDING_DLL
- # define DLL_EXPORTED __declspec(dllexport)
+ /* When building with MSVC, exporting a symbol means that the object file
