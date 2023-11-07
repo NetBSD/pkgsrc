0b0b7f0668c47f1b5c09ec9bf6c0901c8d9bc6a8
adam <adam@pkgsrc.org>
2023-11-07 19:55:21 +0000

    libuv: updated to 1.47.0
    Version 1.47.0 (Stable)
    
    linux: don't use io_uring on pre-5.10.186 kernels
    linux: disable io_uring on 32 bits arm systems
    unix: disable io_uring close on selected kernels
    unix: TCP_KEEPINTVL and TCP_KEEPCNT are available on OS X 10.8+
    unix: add missing syscall numbers for RISC-V arch
    unix: get mainline kernel version in Ubuntu
    win,process: avoid kernel bug with starting Windows Store apps
    export WTF8 conversion utilities
    fs: fix WTF-8 decoding issue

--- devel/libuv/patches/patch-test_test-fs.c
+++ devel/libuv/patches/patch-test_test-fs.c
@@ -1,24 +1,24 @@
-$NetBSD: patch-test_test-fs.c,v 1.3 2022/01/28 21:13:10 schmonz Exp $
+$NetBSD: patch-test_test-fs.c,v 1.4 2023/11/07 19:55:22 adam Exp $
 
 Apply MacPorts patch-libuv-legacy.diff for pre-10.7 platforms.
 
---- test/test-fs.c.orig	2022-01-04 14:18:00.000000000 +0000
+--- test/test-fs.c.orig	2023-11-06 18:17:17.000000000 +0000
 +++ test/test-fs.c
-@@ -1405,7 +1405,7 @@ TEST_IMPL(fs_fstat) {
-   ASSERT(0 == uv_fs_fstat(NULL, &req, file, NULL));
-   ASSERT(req.result == 0);
+@@ -1423,7 +1423,7 @@ TEST_IMPL(fs_fstat) {
+   ASSERT_OK(uv_fs_fstat(NULL, &req, file, NULL));
+   ASSERT_OK(req.result);
    s = req.ptr;
 -# if defined(__APPLE__)
 +# if defined(__APPLE__) && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1050
-   ASSERT(s->st_birthtim.tv_sec == t.st_birthtimespec.tv_sec);
-   ASSERT(s->st_birthtim.tv_nsec == t.st_birthtimespec.tv_nsec);
+   ASSERT_EQ(s->st_birthtim.tv_sec, t.st_birthtimespec.tv_sec);
+   ASSERT_EQ(s->st_birthtim.tv_nsec, t.st_birthtimespec.tv_nsec);
  # elif defined(__linux__)
-@@ -1446,7 +1446,7 @@ TEST_IMPL(fs_fstat) {
-   ASSERT(s->st_size == (uint64_t) t.st_size);
-   ASSERT(s->st_blksize == (uint64_t) t.st_blksize);
-   ASSERT(s->st_blocks == (uint64_t) t.st_blocks);
+@@ -1464,7 +1464,7 @@ TEST_IMPL(fs_fstat) {
+   ASSERT_EQ(s->st_size, (uint64_t) t.st_size);
+   ASSERT_EQ(s->st_blksize, (uint64_t) t.st_blksize);
+   ASSERT_EQ(s->st_blocks, (uint64_t) t.st_blocks);
 -#if defined(__APPLE__)
 +#if defined(__APPLE__) && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1050
-   ASSERT(s->st_atim.tv_sec == t.st_atimespec.tv_sec);
-   ASSERT(s->st_atim.tv_nsec == t.st_atimespec.tv_nsec);
-   ASSERT(s->st_mtim.tv_sec == t.st_mtimespec.tv_sec);
+   ASSERT_EQ(s->st_atim.tv_sec, t.st_atimespec.tv_sec);
+   ASSERT_EQ(s->st_atim.tv_nsec, t.st_atimespec.tv_nsec);
+   ASSERT_EQ(s->st_mtim.tv_sec, t.st_mtimespec.tv_sec);
