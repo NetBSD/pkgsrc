ba62b43be0343321d7b804efc6cce087991f6710
nia <nia@pkgsrc.org>
2023-11-09 20:10:32 +0000

    freetype2: Make brotli support optional, but enabled by default.
    brotli is quite a heavy dependency compared to freetype itself on
    weaker systems, especially those using old C++ compilers.
    
    And it's only used for certain web fonts.

--- graphics/freetype2/options.mk
+++ graphics/freetype2/options.mk
@@ -1,11 +1,20 @@
-# $NetBSD: options.mk,v 1.5 2022/04/12 23:38:13 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2023/11/09 20:10:32 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.freetype2
-PKG_SUPPORTED_OPTIONS=	png
-PKG_SUGGESTED_OPTIONS=	png
+PKG_SUPPORTED_OPTIONS=	brotli png
+PKG_SUGGESTED_OPTIONS=	brotli png
 
 .include "../../mk/bsd.options.mk"
 
+# Support decompression of WOFF2 streams.
+.if !empty(PKG_OPTIONS:Mbrotli)
+CONFIGURE_ARGS+=	--with-brotli=yes
+.include "../../archivers/brotli/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--with-brotli=no
+.endif
+
+# Support bitmaps in OpenType fonts.
 .if !empty(PKG_OPTIONS:Mpng)
 CONFIGURE_ARGS+=	--with-png=yes
 .include "../../graphics/png/buildlink3.mk"
