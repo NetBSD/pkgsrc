83fbb58369007593c1e52be7aae1854431eb4306
gdt <gdt@pkgsrc.org>
2023-11-09 21:02:48 +0000

    sysutils/ups-nut: Re-enable openssl option and default to it
    The option was disabled because old nut could only used ancient
    openssl, but 2.8.1 works with 3.

--- sysutils/ups-nut/options.mk
+++ sysutils/ups-nut/options.mk
@@ -1,18 +1,18 @@
-# $NetBSD: options.mk,v 1.2 2021/03/24 11:01:55 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2023/11/09 21:02:48 gdt Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ups-nut
 PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
-PKG_OPTIONS_GROUP.ssl=		nss # openssl # only has openssl 1.0 support
-PKG_SUGGESTED_OPTIONS=		nss
+PKG_OPTIONS_GROUP.ssl=		nss openssl
+PKG_SUGGESTED_OPTIONS=		openssl
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mnss)
 CONFIGURE_ARGS+=	--with-ssl=nss --with-nss --without-openssl
 .  include "../../devel/nss/buildlink3.mk"
-#.elif !empty(PKG_OPTIONS:Mopenssl)
-#CONFIGURE_ARGS+=	--with-ssl=openssl --with-openssl --without-nss
-#.  include "../../security/openssl/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mopenssl)
+CONFIGURE_ARGS+=	--with-ssl=openssl --without-nss --with-openssl
+.  include "../../security/openssl/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-ssl
 .endif
