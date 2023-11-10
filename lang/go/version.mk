4bbf6aab1c907baeb102ba7cda189c263c53e36a
bsiegert <bsiegert@pkgsrc.org>
2023-11-10 15:39:34 +0000

    Update go120 to 1.20.11 and go121 to 1.21.4 (security).
    These minor releases include 2 security fixes following the security policy:
    
    - path/filepath: recognize \??\ as a Root Local Device path prefix.
    
      On Windows, a path beginning with \??\ is a Root Local Device path equivalent
      to a path beginning with \\?\. Paths with a \??\ prefix may be used to access
      arbitrary locations on the system. For example, the path \??\c:\x is
      equivalent to the more common path c:\x.
    
      The filepath package did not recognize paths with a \??\ prefix as special.
    
      Clean could convert a rooted path such as \a\..\??\b into
      the root local device path \??\b. It will now convert this
      path into .\??\b.
    
      IsAbs did not report paths beginning with \??\ as absolute.
      It now does so.
    
      VolumeName now reports the \??\ prefix as a volume name.
    
      Join(`\`, `??`, `b`) could convert a seemingly innocent
      sequence of path elements into the root local device path
      \??\b. It will now convert this to \.\??\b.
    
      This is CVE-2023-45283 and https://go.dev/issue/63713.
    
    - path/filepath: recognize device names with trailing spaces and superscripts
    
      The IsLocal function did not correctly detect reserved names in some cases:
    
      reserved names followed by spaces, such as "COM1 ".
      "COM" or "LPT" followed by a superscript 1, 2, or 3.
      IsLocal now correctly reports these names as non-local.
    
      This is CVE-2023-45284 and https://go.dev/issue/63713.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.193 2023/10/29 14:40:35 bsiegert Exp $
+# $NetBSD: version.mk,v 1.194 2023/11/10 15:39:34 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,8 +6,8 @@
 #
 .include "go-vars.mk"
 
-GO121_VERSION=	1.21.3
-GO120_VERSION=	1.20.10
+GO121_VERSION=	1.21.4
+GO120_VERSION=	1.20.11
 GO119_VERSION=	1.19.13
 GO118_VERSION=	1.18.10
 GO14_VERSION=	1.4.3
