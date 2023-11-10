e27e46a7ae11a0902dc20877ea5b552ba37a762f
nia <nia@pkgsrc.org>
2023-11-10 12:32:51 +0000

    fvwm: Disable svg by default so this fits into bulk-small.
    The included themes only use png and xpm icons.

--- wm/fvwm3/options.mk
+++ wm/fvwm3/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2020/09/29 12:22:35 nia Exp $
+# $NetBSD: options.mk,v 1.2 2023/11/10 12:32:51 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fvwm3
 PKG_SUPPORTED_OPTIONS=	svg
-PKG_SUGGESTED_OPTIONS=	svg
 
 .include "../../mk/bsd.options.mk"
 
