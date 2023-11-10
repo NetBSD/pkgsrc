64670d8fc8ca35c362b0e33a42b885ab720a362e
nia <nia@pkgsrc.org>
2023-11-10 09:48:39 +0000

    python39, python310: Fixes for Mac OS X 10.4 via MacPorts.

---
+++ lang/python39/patches/patch-Modules_clinic_posixmodule.c.h
@@ -0,0 +1,25 @@
+$NetBSD: patch-Modules_clinic_posixmodule.c.h,v 1.1 2023/11/10 09:48:40 nia Exp $
+
+posix.copyfile does not exist on Tiger. 
+
+Python 3.8's posix._fcopyfile implementation unconditionally uses <copyfile.h>, 
+which only exists on Leopard ane newer. This patch removes posix._fcopyfile 
+on Tiger - this is okay because the rest of the stdlib uses posix._fcopyfile 
+only conditionally after checking that the function exists 
+(non-Apple systems don't have posix._fcopyfile either).
+
+
+thanks to @dgelessus
+https://github.com/macports/macports-ports/pull/5987
+
+--- Modules/clinic/posixmodule.c.h.orig	2023-08-24 17:59:28.000000000 +0000
++++ Modules/clinic/posixmodule.c.h
+@@ -5546,7 +5546,7 @@ exit:
+ 
+ #endif /* defined(HAVE_SENDFILE) && !defined(__APPLE__) && !(defined(__FreeBSD__) || defined(__DragonFly__)) */
+ 
+-#if defined(__APPLE__)
++#if defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050
+ 
+ PyDoc_STRVAR(os__fcopyfile__doc__,
+ "_fcopyfile($module, in_fd, out_fd, flags, /)\n"
--- lang/python39/patches/patch-Python_thread__pthread.h
+++ lang/python39/patches/patch-Python_thread__pthread.h
@@ -1,8 +1,11 @@
-$NetBSD: patch-Python_thread__pthread.h,v 1.1 2020/10/10 20:20:12 adam Exp $
+$NetBSD: patch-Python_thread__pthread.h,v 1.2 2023/11/10 09:48:40 nia Exp $
 
---- Python/thread_pthread.h.orig	2013-05-15 16:33:00.000000000 +0000
+https://bugs.python.org/issue46045
+https://trac.macports.org/ticket/59772
+
+--- Python/thread_pthread.h.orig	2023-08-24 17:59:28.000000000 +0000
 +++ Python/thread_pthread.h
-@@ -50,6 +50,9 @@
+@@ -78,6 +78,9 @@
     we need to add 0 to make it work there as well. */
  #if (_POSIX_SEMAPHORES+0) == -1
  #define HAVE_BROKEN_POSIX_SEMAPHORES
@@ -12,3 +15,21 @@ $NetBSD: patch-Python_thread__pthread.h,v 1.1 2020/10/10 20:20:12 adam Exp $
  #else
  #include <semaphore.h>
  #include <errno.h>
+@@ -340,7 +343,17 @@ PyThread_get_thread_native_id(void)
+         PyThread_init_thread();
+ #ifdef __APPLE__
+     uint64_t native_id;
++#if MAC_OS_X_VERSION_MAX_ALLOWED < 1060
++    native_id = pthread_mach_thread_np(pthread_self());
++#elif MAC_OS_X_VERSION_MIN_REQUIRED < 1060
++    if (&pthread_threadid_np != NULL) {
++        (void) pthread_threadid_np(NULL, &native_id);
++    } else {
++        native_id = pthread_mach_thread_np(pthread_self());
++    }
++#else
+     (void) pthread_threadid_np(NULL, &native_id);
++#endif
+ #elif defined(__linux__)
+     pid_t native_id;
+     native_id = syscall(SYS_gettid);
