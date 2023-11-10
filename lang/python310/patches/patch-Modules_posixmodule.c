678a719bc2e53d45718bbef8b905f6efc78635bc
nia <nia@pkgsrc.org>
2023-11-10 09:48:39 +0000

    python39, python310: Fixes for Mac OS X 10.4 via MacPorts.

---
+++ lang/python310/patches/patch-Modules_posixmodule.c
@@ -0,0 +1,52 @@
+$NetBSD: patch-Modules_posixmodule.c,v 1.1 2023/11/10 09:48:40 nia Exp $
+
+posix.copyfile does not exist on Tiger. 
+
+Python 3.8's posix._fcopyfile implementation unconditionally uses <copyfile.h>, 
+which only exists on Leopard ane newer. This patch removes posix._fcopyfile 
+on Tiger - this is okay because the rest of the stdlib uses posix._fcopyfile 
+only conditionally after checking that the function exists 
+(non-Apple systems don't have posix._fcopyfile either).
+
+
+thanks to @dgelessus
+https://github.com/macports/macports-ports/pull/5987
+
+--- Modules/posixmodule.c.orig	2023-08-24 12:46:25.000000000 +0000
++++ Modules/posixmodule.c
+@@ -66,6 +66,8 @@
+  */
+ #if defined(__APPLE__)
+ 
++#include <AvailabilityMacros.h>
++
+ #if defined(__has_builtin)
+ #if __has_builtin(__builtin_available)
+ #define HAVE_BUILTIN_AVAILABLE 1
+@@ -238,7 +240,7 @@ corresponding Unix manual entries for mo
+ #  include <sys/sendfile.h>
+ #endif
+ 
+-#if defined(__APPLE__)
++#if defined(__APPLE__) && MAC_OS_X_VERSION_MAX_ALLOWED >= 1050
+ #  include <copyfile.h>
+ #endif
+ 
+@@ -9997,7 +9999,7 @@ done:
+ #endif /* HAVE_SENDFILE */
+ 
+ 
+-#if defined(__APPLE__)
++#if defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050
+ /*[clinic input]
+ os._fcopyfile
+ 
+@@ -15440,7 +15442,7 @@ all_ins(PyObject *m)
+ #endif
+ #endif  /* HAVE_EVENTFD && EFD_CLOEXEC */
+ 
+-#if defined(__APPLE__)
++#if defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050
+     if (PyModule_AddIntConstant(m, "_COPYFILE_DATA", COPYFILE_DATA)) return -1;
+ #endif
+ 
--- lang/python310/patches/patch-Python_thread__pthread.h
+++ lang/python310/patches/patch-Python_thread__pthread.h
@@ -1,10 +1,11 @@
-$NetBSD: patch-Python_thread__pthread.h,v 1.2 2021/12/11 10:40:58 wiz Exp $
+$NetBSD: patch-Python_thread__pthread.h,v 1.3 2023/11/10 09:48:40 nia Exp $
 
 https://bugs.python.org/issue46045
+https://trac.macports.org/ticket/59772
 
---- Python/thread_pthread.h.orig	2013-05-15 16:33:00.000000000 +0000
+--- Python/thread_pthread.h.orig	2023-08-24 12:46:25.000000000 +0000
 +++ Python/thread_pthread.h
-@@ -82,6 +82,9 @@
+@@ -81,6 +81,9 @@
     we need to add 0 to make it work there as well. */
  #if (_POSIX_SEMAPHORES+0) == -1
  #define HAVE_BROKEN_POSIX_SEMAPHORES
@@ -14,3 +15,21 @@ https://bugs.python.org/issue46045
  #else
  #include <semaphore.h>
  #include <errno.h>
+@@ -343,7 +346,17 @@ PyThread_get_thread_native_id(void)
+         PyThread_init_thread();
+ #ifdef __APPLE__
+     uint64_t native_id;
++#if MAC_OS_X_VERSION_MAX_ALLOWED < 1060
++    native_id = pthread_mach_thread_np(pthread_self());
++#elif MAC_OS_X_VERSION_MIN_REQUIRED < 1060
++    if (&pthread_threadid_np != NULL) {
++        (void) pthread_threadid_np(NULL, &native_id);
++    } else {
++        native_id = pthread_mach_thread_np(pthread_self());
++    }
++#else
+     (void) pthread_threadid_np(NULL, &native_id);
++#endif
+ #elif defined(__linux__)
+     pid_t native_id;
+     native_id = syscall(SYS_gettid);
