e48562925270d70a2ce0eb4154f8e46bc95aa821
schmonz <schmonz@pkgsrc.org>
2023-11-11 11:57:20 +0000

    s6-networking: update to 2.6.0.0. Changes:
    - Bugfixes.
    - s6-tcpserver has been unified! no ipv4 and ipv6 separation anymore.
       * The only programs in the superserver chain are now s6-tcpserver,
         s6-tcpserver-socketbinder, and s6-tcpserverd.
       * s6-tcpserver-access still exists, should now run under s6-tcpserverd,
         still invoked once per connection. Doesn't spam the log anymore when
         invoked with no ruleset.
       * Options -4 and -6 removed from s6-tcpserver and s6-tlsserver.
         Protocol detection happens when the cmdline address is scanned.
       * Option -e removed from s6-tlsserver. It should now always invoke
         s6-tcpserver-access when needed (and only then).
    - Major performance improvements. s6-tcpserverd does not fork on systems
      that support posix_spawn. Also, its lookups are now logarithmic
      instead of linear (which only matters on *heavy* loads).

--- net/s6-networking/buildlink3.mk
+++ net/s6-networking/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/09/29 09:56:36 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/11/11 11:57:20 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-networking
 
 .if !defined(S6_NETWORKING_BUILDLINK3_MK)
 S6_NETWORKING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-networking+=	s6-networking>=2.5.0.0
+BUILDLINK_API_DEPENDS.s6-networking+=	s6-networking>=2.6.0.0
 BUILDLINK_PKGSRCDIR.s6-networking?=	../../net/s6-networking
 BUILDLINK_INCDIRS.s6-networking+=	include/s6-networking
 BUILDLINK_LIBDIRS.s6-networking+=	lib/s6-networking
