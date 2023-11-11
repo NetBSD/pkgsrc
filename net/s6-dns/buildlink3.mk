9f202303719b8f340d7722fdd5ca90939df80eb7
schmonz <schmonz@pkgsrc.org>
2023-11-11 11:53:34 +0000

    s6-dns: update to 2.3.6.0. Changes:
    - Bugfixes.
    - New s6dns_hosts functions.
    - New command: s6-dns-hosts-compile
    - s6-dnsip* and s6-dnsname now support a -h option, to make use of
      /etc/hosts data.

--- net/s6-dns/buildlink3.mk
+++ net/s6-dns/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/09/29 09:55:43 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2023/11/11 11:53:34 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-dns
 
 .if !defined(S6_DNS_BUILDLINK3_MK)
 S6_DNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.5.2
+BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.6.0
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../net/s6-dns
 BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
 BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
