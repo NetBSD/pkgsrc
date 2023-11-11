5e7276ea06e2051687ff025370c4d071cc1cfc03
schmonz <schmonz@pkgsrc.org>
2023-11-11 11:52:41 +0000

    execline: update to 2.9.4.0. Changes:
    - Adaptation to skalibs-2.14.0.0.
    - New dummy -e option in execlineb.
    - Slightly better error reporting in execlineb.

--- lang/execline/buildlink3.mk
+++ lang/execline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/03/11 15:47:34 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/11/11 11:52:41 schmonz Exp $
 
 BUILDLINK_TREE+=	execline
 
 .if !defined(EXECLINE_BUILDLINK3_MK)
 EXECLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.2.0
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.4.0
 BUILDLINK_PKGSRCDIR.execline?=		../../lang/execline
 BUILDLINK_DEPMETHOD.execline?=		build
 .endif # EXECLINE_BUILDLINK3_MK
