de7d4e3f96705ec196e423f5d9b6b4d356448d60
adam <adam@pkgsrc.org>
2023-11-13 20:22:03 +0000

    postgresql: updated to 16.1, 15.5, 14.10, 13.13, 12.17, and 11.22
    Security Issues
    
    CVE-2023-5868: Memory disclosure in aggregate function calls
    CVE-2023-5869: Buffer overrun from integer overflow in array modification
    CVE-2023-5870: Role pg_signal_backend can signal certain superuser processes
    
    Bug Fixes and Improvements
    
    This update fixes over 55 bugs that were reported in the last several months. The issues listed below affect PostgreSQL 16. Some of these issues may also affect other supported versions of PostgreSQL.
    
    Fix issue where GiST indexes had an incorrect behavior during a "page split" operation that could lead to incorrect results in subsequent index searches. Please reindex GiST indexes after installing this update.
    Fix issue where B-tree indexes would incorrectly de-duplicate interval columns. Please reindex any B-tree index that includes an interval column after installing this update.
    Provide more efficient indexing of date, timestamptz, and timestamp values in BRIN indexes when using a minmax_multi opsclass. While not required, we recommend reindexing BRIN indexes that include these data types after installing this update.
    Fix for bulk table insertion into partitioned tables.
    Fix for hash-partitioned tables with multiple partition keys during step generation and runtime pruning that could lead to crashes in some cases.
    Throw the correct error if pgrowlocks() is applied to a partitioned table
    
    Fix inconsistent rechecking of concurrently-updated rows during MERGE when using READ COMMITTED mode.
    
    Correctly identify the target table in an inherited UPDATE/DELETE/MERGE even when the parent table is excluded by constraints.
    Fix over-allocation of a constructed tsvector.
    Fix ALTER SUBSCRIPTION to apply changes in the run_as_owner option.
    Several fixes for COPY FROM,
    Several fixes for handling torn reads with pg_control.
    Fix "could not find pathkey item to sort" errors occurring while planning aggregate functions with ORDER BY or DISTINCT options.
    When track_io_timing is enabled, include the time taken by relation extension operations as write time.
    Track the dependencies of cached CALL statements, and re-plan them when needed.
    Treat out-of-memory failures as FATAL while reading WAL.
    Fix pg_dump to dump the new run_as_owner option of subscriptions.
    Fix pg_restore so that selective restores will include both table-level and column-level ACLs for selected tables.
    Add logic to pg_upgrade to check for use of obsolete data types abstime, reltime, and tinterval.
    Fix vacuumdb to have multiple -N switches actually exclude tables in multiple schemas.
    amcheck will no longer report interrupted page deletion as corruption.
    Fix btree_gin indexes on interval columns to properly return data when using the < and <= operators.

--- databases/postgresql15/options.mk
+++ databases/postgresql15/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2022/10/19 13:16:45 adam Exp $
+# $NetBSD: options.mk,v 1.2 2023/11/13 20:22:07 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.postgresql15
 PKG_SUPPORTED_OPTIONS=	bonjour dtrace icu llvm gssapi ldap nls pam lz4
@@ -46,7 +46,7 @@ CONFIGURE_ARGS+=	--with-ldap
 
 # LLVM based JIT support
 .if !empty(PKG_OPTIONS:Mllvm)
-.  include "../../lang/llvm/buildlink3.mk"
+.  include "../../wip/llvm/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-llvm
 CONFIGURE_ENV+=		CLANG=${CC}	# XXX: make it be better
 PLIST.llvm=		yes
