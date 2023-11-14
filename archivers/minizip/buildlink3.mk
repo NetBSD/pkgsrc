0f8fe59875410d6acc4b5233320917efea9356c7
nia <nia@pkgsrc.org>
2023-11-14 16:16:56 +0000

    minizip: Set BUILDLINK_INCDIRS

--- archivers/minizip/buildlink3.mk
+++ archivers/minizip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/31 10:25:28 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/11/14 16:16:56 nia Exp $
 
 BUILDLINK_TREE+=	minizip
 
@@ -7,6 +7,7 @@ MINIZIP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.minizip+=	minizip>=1.2.11
 BUILDLINK_PKGSRCDIR.minizip?=	../../archivers/minizip
+BUILDLINK_INCDIRS.minizip+=	include/minizip
 
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# MINIZIP_BUILDLINK3_MK
