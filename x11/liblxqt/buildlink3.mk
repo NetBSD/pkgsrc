405e11734521327e4667a4a385c51454070df85a
gutteridge <gutteridge@pkgsrc.org>
2023-11-14 04:18:17 +0000

    liblxqt: bump API_DEPENDS
    The first batch of LXQt package updates committed didn't expect the
    newer version of liblxqt, but some in the second batch do expect 1.4.0.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.27 2023/11/12 13:24:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2023/11/14 04:18:17 gutteridge Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.3.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=1.3.0nb5
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.4.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
