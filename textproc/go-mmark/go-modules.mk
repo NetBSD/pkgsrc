8f3bb866a0eafa93bc65a6841b2c2f2c4c568fd8
bsiegert <bsiegert@pkgsrc.org>
2023-11-14 21:28:38 +0000

    mmark: update to 2.2.43
    - New github org
    - otherwise no changelog from upstream

--- textproc/go-mmark/go-modules.mk
+++ textproc/go-mmark/go-modules.mk
@@ -1,10 +1,8 @@
-# $NetBSD: go-modules.mk,v 1.1 2021/05/15 17:39:05 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.2 2023/11/14 21:28:38 bsiegert Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.zip
-GO_MODULE_FILES+=	github.com/gomarkdown/markdown/@v/v0.0.0-20200817091206-6efd7696db82.mod
-GO_MODULE_FILES+=	github.com/gomarkdown/markdown/@v/v0.0.0-20200817091206-6efd7696db82.zip
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.zip
+GO_MODULE_FILES+=	github.com/gomarkdown/markdown/@v/v0.0.0-20230922112808-5421fefb8386.mod
+GO_MODULE_FILES+=	github.com/gomarkdown/markdown/@v/v0.0.0-20230922112808-5421fefb8386.zip
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.2.0.mod
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.2.0.zip
-GO_MODULE_FILES+=	golang.org/dl/@v/v0.0.0-20190829154251-82a15e2f2ead.mod
-GO_MODULE_FILES+=	golang.org/dl/@v/v0.0.0-20190829154251-82a15e2f2ead.zip
