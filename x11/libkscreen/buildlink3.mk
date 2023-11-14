794dd38b0f06df95a8953ec6e608c6611e7133cf
gutteridge <gutteridge@pkgsrc.org>
2023-11-14 03:53:46 +0000

    libkscreen: update to 5.27.9
    No convenient condensed change log to summarize all the versions covered
    in this update. This update is required because lxqt-config expects a
    minimum 5.27.0 version.

--- x11/libkscreen/buildlink3.mk
+++ x11/libkscreen/buildlink3.mk
@@ -1,14 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/11/12 13:24:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2023/11/14 03:53:46 gutteridge Exp $
 
 BUILDLINK_TREE+=	libkscreen
 
 .if !defined(LIBKSCREEN_BUILDLINK3_MK)
 LIBKSCREEN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkscreen+=	libkscreen>=5.6.3
-BUILDLINK_ABI_DEPENDS.libkscreen?=	libkscreen>=5.24.6nb8
+BUILDLINK_API_DEPENDS.libkscreen+=	libkscreen>=5.27.9
 BUILDLINK_PKGSRCDIR.libkscreen?=	../../x11/libkscreen
 
+BUILDLINK_API_DEPENDS.kconfig+= kconfig>=5.102.0
+.include "../../devel/kconfig/buildlink3.mk"
+BUILDLINK_API_DEPENDS.kwayland+=        kwayland>=5.102.0
 .include "../../devel/kwayland/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .include "../../x11/qt5-qtx11extras/buildlink3.mk"
