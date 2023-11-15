11b66fe71d22ca8996a47f3984a7c525b70eb599
wiz <wiz@pkgsrc.org>
2023-11-15 21:03:57 +0000

    py-cairo: de-support Python 2 in buildlink3.mk
    Too fragile, broke twice in the last week, and no users left that need it

--- graphics/py-cairo/buildlink3.mk
+++ graphics/py-cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2023/11/15 13:00:02 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2023/11/15 21:03:57 wiz Exp $
 
 BUILDLINK_TREE+=	py-cairo
 
@@ -8,14 +8,9 @@ PY_CAIRO_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-cairo+=	${PYPKGPREFIX}-cairo>=1.10.0
-.if ${PYVERSSUFFIX} == "2.7"
-BUILDLINK_ABI_DEPENDS.py-cairo+=	${PYPKGPREFIX}-cairo>=1.18.2nb5
-BUILDLINK_PKGSRCDIR.py-cairo?=		../../graphics/py-cairo118
-.else
 BUILDLINK_ABI_DEPENDS.py-cairo+=	${PYPKGPREFIX}-cairo>=1.24.0nb1
 BUILDLINK_PKGSRCDIR.py-cairo?=		../../graphics/py-cairo
 BUILDLINK_INCDIRS.py-cairo+=		${PYSITELIB}/cairo/include
-.endif
 
 .include "../../graphics/cairo/buildlink3.mk"
 .endif	# PY_CAIRO_BUILDLINK3_MK
