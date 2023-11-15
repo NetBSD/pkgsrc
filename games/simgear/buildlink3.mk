3efb2c624ad9a8379e0f44c3b0d8024eb6b08155
wiz <wiz@pkgsrc.org>
2023-11-15 20:00:35 +0000

    *: bump for openexr 3.2.1

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.89 2023/11/15 09:25:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.90 2023/11/15 20:00:36 wiz Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2nb7
-BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.11nb29
+BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.11nb30
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
