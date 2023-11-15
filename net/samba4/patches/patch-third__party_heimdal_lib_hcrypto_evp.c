0a08fa99d7674042f656652509377919ff789f50
wiz <wiz@pkgsrc.org>
2023-11-15 18:54:43 +0000

    samba: update to 4.19.2.
    This is the first stable release of the Samba 4.19 release series.

--- net/samba4/patches/patch-third__party_heimdal_lib_hcrypto_evp.c
+++ net/samba4/patches/patch-third__party_heimdal_lib_hcrypto_evp.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-third__party_heimdal_lib_hcrypto_evp.c,v 1.1 2022/08/30 17:47:50 jperkin Exp $
+$NetBSD: patch-third__party_heimdal_lib_hcrypto_evp.c,v 1.2 2023/11/15 18:54:43 wiz Exp $
 
 Disable pkcs11_hcrypto, only available on Oracle Solaris 11.x
 
---- third_party/heimdal/lib/hcrypto/evp.c.orig	2022-03-08 14:53:05.754089600 +0000
+--- third_party/heimdal/lib/hcrypto/evp.c.orig	2023-07-18 08:14:56.858105700 +0000
 +++ third_party/heimdal/lib/hcrypto/evp.c
 @@ -55,8 +55,6 @@
  #ifndef HCRYPTO_DEF_PROVIDER
@@ -12,4 +12,4 @@ Disable pkcs11_hcrypto, only available on Oracle Solaris 11.x
 -#  define HCRYPTO_DEF_PROVIDER pkcs11_hcrypto
  # elif HAVE_HCRYPTO_W_OPENSSL
  #  define HCRYPTO_DEF_PROVIDER ossl
- # else
+ #  define HCRYPTO_DEF_PROVIDER_IS_OPENSSL
