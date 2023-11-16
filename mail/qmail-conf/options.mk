886cd772a8a57458240748e7b28fa9a6de77ba58
schmonz <schmonz@pkgsrc.org>
2023-11-16 23:09:13 +0000

    qmail-conf: bump PKGREVISION. Changes:
    - Remove inet6 option; simply depend on dual-stack ucspi-tcp6 (or
      v4-only original DJB ucspi-tcp, if that's already installed)
    - Set LICENSE to public-domain, as this is derived from djbdns-1.05
      which has long been public-domain.

--- mail/qmail-conf/options.mk
+++ mail/qmail-conf/options.mk
@@ -1,15 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2019/01/10 13:23:16 schmonz Exp $
+# $NetBSD: options.mk,v 1.3 2023/11/16 23:09:13 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qmail-conf
-PKG_SUPPORTED_OPTIONS+=	inet6 pam
+PKG_SUPPORTED_OPTIONS+=	pam
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Minet6)
-DEPENDS+=		ucspi-tcp6-[0-9]*:../../net/ucspi-tcp6
-.else
-DEPENDS+=		{ucspi-tcp6-[0-9]*,ucspi-tcp-[0-9]*}:../../net/ucspi-tcp
-.endif
 
 .if !empty(PKG_OPTIONS:Mpam)
 DEPENDS+=		checkpassword-pam-[0-9]*:../../sysutils/checkpassword-pam
