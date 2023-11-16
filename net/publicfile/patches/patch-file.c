b2335da11556d9521d145eeb987a4e564001ed27
schmonz <schmonz@pkgsrc.org>
2023-11-16 22:55:37 +0000

    publicfile: bump PKGREVISION. Changes:
    - Remove inet6 option; simply depend on dual-stack ucspi-tcp6 (or
      v4-only original DJB ucspi-tcp, if that's already installed)
    - Apply upstream patch to support https URLs under e.g. sslserver
    - Add patch comments

--- net/publicfile/patches/patch-file.c
+++ net/publicfile/patches/patch-file.c
@@ -1,6 +1,12 @@
-$NetBSD: patch-file.c,v 1.1 2013/05/23 14:57:32 joerg Exp $
+$NetBSD: patch-file.c,v 1.2 2023/11/16 22:55:37 schmonz Exp $
 
---- file.c.orig	1999-11-09 07:23:46.000000000 +0000
+Handle request URLs with an https scheme. Look for the client IP in
+SSLREMOTEIP if TCPREMOTEIP is not set.
+<http://www.superscript.com/patches/publicfile.sslserver>
+
+Add missing includes.
+
+--- file.c.orig	2023-11-16 22:52:07.122614429 +0000
 +++ file.c
 @@ -7,6 +7,8 @@
  #include "str.h"
@@ -11,3 +17,16 @@ $NetBSD: patch-file.c,v 1.1 2013/05/23 14:57:32 joerg Exp $
  
  static void log(char *fn,char *result1,char *result2,int flagread)
  {
+@@ -15,7 +17,11 @@ static void log(char *fn,char *result1,c
+   char *x;
+ 
+   x = env_get("TCPREMOTEIP");
+-  if (!x) x = "0";
++  if (!x) {
++    x = env_get("SSLREMOTEIP");
++    if (!x)
++      x = "0";
++  }
+   substdio_puts(subfderr,x);
+   substdio_puts(subfderr,flagread ? " read ": " dir ");
+ 
