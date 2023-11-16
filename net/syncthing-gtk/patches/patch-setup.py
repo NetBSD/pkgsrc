fa8cd44c4c9f681c13664e7d8ee8bd581b98b7a6
wiz <wiz@pkgsrc.org>
2023-11-16 12:16:04 +0000

    syncthing-gtk: update to 0.9.4.5.
    Switch to a maintained fork.
    
    Changes:
    
    This is the first version of Syncthing-GTK maintained by a new team
    at https://github.com/syncthing-gtk.
    
    Changes:
    
        Port to Python3. Python2 is not supported anymore.
        Miscelaneous fixes about icons
        Miscelaneous fixes about app indicators on KDE, Gnome.
        Translation updates

--- net/syncthing-gtk/patches/patch-setup.py
+++ net/syncthing-gtk/patches/patch-setup.py
@@ -1,30 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2016/09/01 17:30:21 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2023/11/16 12:16:04 wiz Exp $
 
-Avoid calling git.
-Install man page in proper directory for pkgsrc.
+Accept a missing git binary.
 
---- setup.py.orig	2016-08-29 04:05:55.000000000 +0000
+--- setup.py.orig	2023-11-16 12:08:01.063349216 +0000
 +++ setup.py
-@@ -12,13 +12,6 @@ def get_version():
- 	Returns current package version using git-describe or examining
- 	path. If both methods fails, returns 'unknown'.
- 	"""
--	try:
--		p = Popen(['git', 'describe', '--tags', '--match', 'v*'], stdout=PIPE)
--		version = p.communicate()[0].strip("\n\r \t")
--		if p.returncode != 0:
--			raise Exception("git-describe failed")
--		return version
--	except: pass
- 	# Git-describe method failed, try to guess from working directory name
- 	path = os.getcwd().split(os.path.sep)
- 	version = 'unknown'
-@@ -82,7 +75,7 @@ if __name__ == "__main__" : 
- 				"icons/%s.png" % x for x in (
- 					'restart', 'settings', 'shutdown', "st-gtk-logo"
- 			)]),
--		('share/man/man1', glob.glob("doc/*") ),
-+		(os.environ['PKGMANDIR'] + '/man1', glob.glob("doc/*") ),
- 		('share/icons/hicolor/64x64/emblems', glob.glob("icons/emblem-*.png") ),
- 		('share/pixmaps', ["icons/syncthing-gtk.png"]),
- 		('share/applications', ['syncthing-gtk.desktop'] ),
+@@ -19,7 +19,7 @@ def get_version():
+     try:
+         version = subprocess.check_output(["git", "describe", "--tags"])
+         version = version.decode("utf-8").strip("\n\r \t")
+-    except subprocess.CalledProcessError:
++    except (subprocess.CalledProcessError, FileNotFoundError):
+         # Git-describe method failed, try to guess from working directory name
+         project_dir_name = Path(__file__).parent.name
+         result = re.match(r"syncthing-gtk-(.*)", project_dir_name)
