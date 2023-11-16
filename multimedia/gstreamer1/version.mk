733c0a6a005ff1040c62620762b576734b0a8106
wiz <wiz@pkgsrc.org>
2023-11-16 09:31:35 +0000

    gstreamer: update to 1.22.7
     Highlighted bugfixes:
    
        Security fixes for the MXF demuxer and AV1 codec parser
        glfilter: Memory leak fix for OpenGL filter elements
        d3d11videosink: Fix toggling between fullscreen and maximized, and window switching in fullscreen mode
        DASH / HLS adaptive streaming fixes
        Decklink card device provider device name string handling fixes
        interaudiosrc: handle non-interleaved audio properly
        openh264: Fail gracefully if openh264 encoder/decoder creation fails
        rtspsrc: improved whitespace handling in response headers by certain cameras
        v4l2codecs: avoid wrap-around after 1000000 frames; tiled formats handling fixes
        video-scaler, audio-resampler: downgraded "Can't find exact taps" debug log messages
        wasapi2: Don't use global volume control object
        Rust plugins: various improvements in aws, fmp4mux, hlssink3, livesync, ndisrc, rtpav1depay, rsfilesink, s3sink, sccparse
        WebRTC: various webrtchttp, webrtcsrc, and webrtcsink improvements and fixes
        Cerbero build tools: recognise Windows 11; restrict parallelism of gst-plugins-rs build on small systems
        Packages: ca-certificates update; fix gio module loading and TLS support on macOS

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.7 2023/11/16 09:22:51 wiz Exp $
+# $NetBSD: version.mk,v 1.8 2023/11/16 09:31:35 wiz Exp $
 
-GST_VERSION=	1.22.6
+GST_VERSION=	1.22.7
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 PLIST_SUBST+=	SHLIBVER=${SHLIBVER}
 
