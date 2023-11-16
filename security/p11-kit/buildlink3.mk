b38050cad72c32df2e041b6f78181bba678062fe
adam <adam@pkgsrc.org>
2023-11-16 12:11:27 +0000

    p11-kit: updated to 0.25.3
    0.25.3 (stable)
    * rpc: fix serialization of NULL mechanism pointer
    * fix meson build failure in macOS (appleframeworks not found)

--- security/p11-kit/buildlink3.mk
+++ security/p11-kit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2021/12/08 16:02:34 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/11/16 12:11:27 adam Exp $
 
 BUILDLINK_TREE+=	p11-kit
 
@@ -7,7 +7,7 @@ P11_KIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p11-kit+=	p11-kit>=0.10
 BUILDLINK_API_DEPENDS.p11-kit+=	p11-kit>=0.23
-BUILDLINK_ABI_DEPENDS.p11-kit?=	p11-kit>=0.24.0nb3
+BUILDLINK_ABI_DEPENDS.p11-kit+=	p11-kit>=0.24.0nb3
 BUILDLINK_PKGSRCDIR.p11-kit?=	../../security/p11-kit
 .include "../../devel/libffi/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
