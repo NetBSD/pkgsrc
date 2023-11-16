5190f1b238f5065a05ee63e477b3b424b8ad7372
wiz <wiz@pkgsrc.org>
2023-11-16 09:54:10 +0000

    vim*: update to latest patchlevel
    About 200 bugfixes.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.142 2023/09/16 12:21:18 rhialto Exp $
+# $NetBSD: version.mk,v 1.143 2023/11/16 09:54:10 wiz Exp $
 
 VIM_VERSION=		9.0
-VIM_PATCHLEVEL=		1900
+VIM_PATCHLEVEL=		2105
 # Changelog: see http://ftp.vim.org/pub/vim/patches/9.0/
 VIM_SUBDIR=		vim90
 
