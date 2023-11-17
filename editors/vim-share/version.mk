13d3e5b831a1d766b5ff1b12aaf6f12a0432fa87
wiz <wiz@pkgsrc.org>
2023-11-17 09:32:54 +0000

    vim*: update to patchlevel 2112 for security fixes

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.143 2023/11/16 09:54:10 wiz Exp $
+# $NetBSD: version.mk,v 1.144 2023/11/17 09:32:54 wiz Exp $
 
 VIM_VERSION=		9.0
-VIM_PATCHLEVEL=		2105
+VIM_PATCHLEVEL=		2112
 # Changelog: see http://ftp.vim.org/pub/vim/patches/9.0/
 VIM_SUBDIR=		vim90
 
