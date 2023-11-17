9c3c52468a0cc671f7e68f3b9a2aa56373ad8935
nros <nros@pkgsrc.org>
2023-11-17 12:07:49 +0000

    liblbfgs: only use sse2 on supported platforms Make the sse2 option available only on i386 and x86_64 where sse2 is possibly available. Only make it the default option on x86_64 where see2 is guaranteed to be available. Should fix build on NetBSD arm.

--- math/liblbfgs/options.mk
+++ math/liblbfgs/options.mk
@@ -1,8 +1,15 @@
-# $NetBSD: options.mk,v 1.1.1.1 2014/10/29 23:10:29 cheusov Exp $
+# $NetBSD: options.mk,v 1.2 2023/11/17 12:07:49 nros Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.liblbfgs
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${MACHINE_ARCH} =="i386" || ${MACHINE_ARCH} == "x86_64"
 PKG_SUPPORTED_OPTIONS=	liblbfgs-sse2
+.  if ${MACHINE_ARCH} == "x86_64"
 PKG_SUGGESTED_OPTIONS=	liblbfgs-sse2
+.  endif
+.endif
 
 .include "../../mk/bsd.options.mk"
 
