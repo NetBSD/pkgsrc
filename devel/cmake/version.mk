9bbc58da8c06a2f064f8a532fc03f932ea1bde5e
adam <adam@pkgsrc.org>
2023-11-19 17:16:27 +0000

    cmake cmake-gui: updated to 3.27.8
    CMake 3.27.8
    * Tests/ObjectLibrary: fix comment
    * cmComputeLinkDepends: also copy the target from object link items
    * cmComputeLinkInformation: skip over linking to items for object purposes
    * Fortran: Restore support for TARGET_OBJECTS providing modules
    * curl: Backport SOCKS5 heap buffer overflow fix from curl 8.4.0
    * GetPrerequisites: Revert "Ignore relative paths printed by ldd"
    * cmCommonTargetGenerator: Factor out GetLinkedTargetDirectories loop body
    * cmComputeLinkInformation: Track targets named by TARGET_OBJECTS sources
    * FindOpenMP: Add support for openmp 5.1 (llvm 17) and 5.2

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.47 2023/10/09 17:19:30 adam Exp $
+# $NetBSD: version.mk,v 1.48 2023/11/19 17:16:27 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.7
+CMAKE_VERSION=	3.27.8
 CMAKE_API=	${CMAKE_VERSION:R}
