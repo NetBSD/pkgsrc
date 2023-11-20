319f416a83aad6f97391b82dbe908923953d2751
wiz <wiz@pkgsrc.org>
2023-11-20 06:36:47 +0000

    py-kivy: update to 2.2.1.
    Add upstream patch for cython 3.
    
    2.2.1
    
    Highlights
    
        [#8283]: backport (#8276): Limit stencil to inner instructions on Image widget
    
    Tests/ci
    
        [#8288]: backport (#8263): Increase timeout of httpbin tests to reduce risk of failures on CI runs
    
    Documentation
    
        [#8252]: backport (#8251): Ensures that jQuery is always installed (on newer sphinx versions is not the default)
    
    2.2.0
    
    Highlights
    
        [#7876]: Line/SmoothLine: Fixes rendering issues related to corner radius and updates its order (rounded_rectangle) + add getter methods for rounded_rectangle, rectangle, ellipse, circle.
        [#7882]: Re-implements the Bubble widget.
        [#7908]: Speed up SmoothLine creation by ~2.5x
        [#7942]: Config unicode support on Windows
        [#7988]: Added support for KIVY_LOG_MODE
        [#8044]: Add support for Python 3.11
        [#8056]: New Feature: Add BoxShadow graphic instruction 🎉
        [#8115]: Use font_direction and font_script_name from SDL2_ttf
        [#8144]: Added property for mouse draggable tab scrollbar to TabbedPanel
        [#8162]: Label: allow different values of left, top, right and bottom for padding.
        [#8169]: Image: add fit_mode feature
        [#8096]: Introduce build script for SDL dependencies and KIVY_DEPS_ROOT
    
    2.1.0
    
    Highlights
    
        [#7658]: Feature: Added EventManagerBase. We can now register managers that selectively process inputs e.g. touch and decide how to dispatch it to the widget tree. on_motion and motion_filter was added to Widget to handle generic motion inputs from the managers.
        [#7270]: Graphics: We now check whether the user called GL instructions from an external thread and raise an error.
        [#7293]: Properties: Added dynamic screen density/dpi support. KV code referencing density e.g. width: "5dp" will now update if the window dpi changes.
        [#7371]: KV: Allow using f-strings in KV-lang.
        [#7424]: Properties: Speed up bare widget creation (3X) and property dispatching/setting.
        [#7587]: Fix PermissionError when reconnecting mtdev input devices.
        [#7637]: Added Custom titlebar support.
        [#7642]: TextInput loading time optimization for large texts.
        [#7663]: Add python3.10 support.
        [#7678]: Add support for Apple Silicon.

---
+++ devel/py-kivy/patches/patch-setup.py
@@ -0,0 +1,17 @@
+$NetBSD: patch-setup.py,v 1.1 2023/11/20 06:36:47 wiz Exp $
+
+https://github.com/kivy/kivy/pull/8326/files
+
+--- setup.py.orig	2023-11-20 06:26:46.811168742 +0000
++++ setup.py
+@@ -597,10 +597,6 @@ class CythonExtension(Extension):
+             'language_level': 3,
+             'unraisable_tracebacks': True,
+         }
+-        # XXX with pip, setuptools is imported before distutils, and change
+-        # our pyx to c, then, cythonize doesn't happen. So force again our
+-        # sources
+-        self.sources = args[1]
+ 
+ 
+ def merge(d1, *args):
