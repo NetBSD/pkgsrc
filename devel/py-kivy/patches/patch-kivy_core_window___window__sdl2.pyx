7ddfd4bd42b418097f0d425748943cef5dfac555
wiz <wiz@pkgsrc.org>
2023-11-20 06:36:47 +0000

    py-kivy: update to 2.2.1.
    Add upstream patch for cython 3.
    
    2.2.1
    
    Highlights
    
        [#8283]: backport (#8276): Limit stencil to inner instructions on Image widget
    
    Tests/ci
    
        [#8288]: backport (#8263): Increase timeout of httpbin tests to reduce risk of failures on CI runs
    
    Documentation
    
        [#8252]: backport (#8251): Ensures that jQuery is always installed (on newer sphinx versions is not the default)
    
    2.2.0
    
    Highlights
    
        [#7876]: Line/SmoothLine: Fixes rendering issues related to corner radius and updates its order (rounded_rectangle) + add getter methods for rounded_rectangle, rectangle, ellipse, circle.
        [#7882]: Re-implements the Bubble widget.
        [#7908]: Speed up SmoothLine creation by ~2.5x
        [#7942]: Config unicode support on Windows
        [#7988]: Added support for KIVY_LOG_MODE
        [#8044]: Add support for Python 3.11
        [#8056]: New Feature: Add BoxShadow graphic instruction 🎉
        [#8115]: Use font_direction and font_script_name from SDL2_ttf
        [#8144]: Added property for mouse draggable tab scrollbar to TabbedPanel
        [#8162]: Label: allow different values of left, top, right and bottom for padding.
        [#8169]: Image: add fit_mode feature
        [#8096]: Introduce build script for SDL dependencies and KIVY_DEPS_ROOT
    
    2.1.0
    
    Highlights
    
        [#7658]: Feature: Added EventManagerBase. We can now register managers that selectively process inputs e.g. touch and decide how to dispatch it to the widget tree. on_motion and motion_filter was added to Widget to handle generic motion inputs from the managers.
        [#7270]: Graphics: We now check whether the user called GL instructions from an external thread and raise an error.
        [#7293]: Properties: Added dynamic screen density/dpi support. KV code referencing density e.g. width: "5dp" will now update if the window dpi changes.
        [#7371]: KV: Allow using f-strings in KV-lang.
        [#7424]: Properties: Speed up bare widget creation (3X) and property dispatching/setting.
        [#7587]: Fix PermissionError when reconnecting mtdev input devices.
        [#7637]: Added Custom titlebar support.
        [#7642]: TextInput loading time optimization for large texts.
        [#7663]: Add python3.10 support.
        [#7678]: Add support for Apple Silicon.

---
+++ devel/py-kivy/patches/patch-kivy_core_window___window__sdl2.pyx
@@ -0,0 +1,33 @@
+$NetBSD: patch-kivy_core_window___window__sdl2.pyx,v 1.1 2023/11/20 06:36:47 wiz Exp $
+
+https://github.com/kivy/kivy/pull/8326/files
+
+--- kivy/core/window/_window_sdl2.pyx.orig	2023-11-20 06:24:57.914793792 +0000
++++ kivy/core/window/_window_sdl2.pyx
+@@ -263,7 +263,7 @@ cdef class _WindowSDL2Storage:
+         for joy_i in range(SDL_NumJoysticks()):
+             SDL_JoystickOpen(joy_i)
+ 
+-        SDL_SetEventFilter(_event_filter, <void *>self)
++        SDL_SetEventFilter(<SDL_EventFilter *>_event_filter, <void *>self)
+ 
+         SDL_EventState(SDL_DROPFILE, SDL_ENABLE)
+         SDL_EventState(SDL_DROPTEXT, SDL_ENABLE)
+@@ -437,7 +437,7 @@ cdef class _WindowSDL2Storage:
+     def set_shape(self, shape, mode, cutoff, color_key):
+         cdef SDL_Surface * sdl_shape
+ 
+-        cpdef SDL_WindowShapeMode sdl_window_mode
++        cdef SDL_WindowShapeMode sdl_window_mode
+         cdef SDL_WindowShapeParams parameters
+         cdef SDL_Color color
+         cdef int result
+@@ -793,7 +793,7 @@ cdef class _WindowSDL2Storage:
+ 
+     def set_custom_titlebar(self, titlebar_widget):
+         SDL_SetWindowBordered(self.win, SDL_FALSE)
+-        return SDL_SetWindowHitTest(self.win,custom_titlebar_handler_callback,<void *>titlebar_widget)
++        return SDL_SetWindowHitTest(self.win,<SDL_HitTest>custom_titlebar_handler_callback,<void *>titlebar_widget)
+ 
+     @property
+     def window_size(self):
