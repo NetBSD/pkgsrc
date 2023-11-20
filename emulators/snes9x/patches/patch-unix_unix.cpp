b0934b5723e9c5d2fd5f4501edc12d6cc01616cf
nia <nia@pkgsrc.org>
2023-11-20 10:19:00 +0000

    [libretro-]snes9x[-gtk]: Update to 1.62.3
    Snes9x 1.62
    - Fixed SA1 division with negative dividend again. (Atari2)
    - Fixed timing on several instructions. (pi1541)
    - MSU1 audio with no loop point will not repeat from start.
    - Modernized some old memory-related code.
    - Fixed a desynchronization in interlaced modes and reporting.
    - Remove SA1 access to WRAM.
    - Added second set of registers between S-DSP and S-SMP. Fixes
      PAL version of Virtual Bart.
    
    Gtk + Windows:
    - Added a Vulkan graphics output method. Similar to OpenGL, this
      can use libretro's slang shaders.
    - Changed the automatic frame skip throttle to take place immediately
      before showing the frame, making VRR work better.
    - Add menu item to clear recent files entries.
    - Add fixed window size changes up to 10x.
    
    Gtk:
    - Switched to CMake for build system. Removed meson.
    - Fixed some keys not working with modifiers. (Gutawer)
    - Removed graphics options most people won't understand or need.
    - Improved wayland support. Handle fractional DPI properly.

--- emulators/snes9x/patches/patch-unix_unix.cpp
+++ emulators/snes9x/patches/patch-unix_unix.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-unix_unix.cpp,v 1.4 2022/05/03 17:09:19 nia Exp $
+$NetBSD: patch-unix_unix.cpp,v 1.5 2023/11/20 10:19:00 nia Exp $
 
 Avoid colliding with libc functions.
+https://github.com/snes9xgit/snes9x/pull/887
 
 --- unix/unix.cpp.orig	2022-03-03 16:41:25.000000000 +0000
 +++ unix/unix.cpp
