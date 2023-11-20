338ba14ae11f308af6b01b3f242e6d532e3d3956
nia <nia@pkgsrc.org>
2023-11-20 00:58:09 +0000

    strawberry: Update to 1.0.21
    Strawberry Music Player
    =======================
    ChangeLog
    
    Version 1.0.21 (2023.10.21):
    
      Bugfixes:
        * Fixed seekbar position resetting to zero before showing actual position when seeking.
        * Fixed compressed files showing up in collection
        * Fixed connecting devices
        * Fixed device schema missing ebur128 fields.
        * Fixed collection search by tag not working with space between colon and search term
        * Fixed seeking when 5 seconds is remaining of the song resetting position to beginning
        * Fixed intermittent crash when seeking with Auto as output
    
      Enhancements:
        * Support using system KDSingleApplication when available.
        * Improved lyrics matching.
        * (macOS) Fully codesign binaries and DMG.
    
    Version 1.0.20 (2023.09.24):
    
      Bugfixes:
        * Fixed appdata validation.
    
    Version 1.0.19 (2023.09.24):
    
      Bugfixes:
        * Use shared pointers for objects to fix potential crashes on exit
        * Fixed smart playlist search not matching unrated songs
        * Fixed reading FMPS_Playcount for MP3 ID3v2 tags
        * Always stop playing after 100 errors to prevent flooding the error dialog
        * Fixed volume going to 100% when decreasing volume beyond zero
        * Fixed error dialog sometimes showing empty.
        * (macOS) Fixed missing search bars
    
      Enhancements:
        * Add Mpris2 property to read/write rating
        * Capitalize playlist column names
        * Added lyrics from songlyrics.com, azlyrics.com, elyrics.net and lyricsmode.com.
    
      New features
        * Support performing song loudness analysis using `libebur128`
        * Support song playback loudness normalization, as per EBU R 128
    
      Other:
        * Removed last.fm HTTPS workaround and GnuTLS dependency
        * Removed broken lyrics.com lyrics provider.

--- audio/strawberry/patches/patch-src_utilities_transliterate.cpp
+++ audio/strawberry/patches/patch-src_utilities_transliterate.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_utilities_transliterate.cpp,v 1.1 2023/02/04 15:38:39 nia Exp $
+$NetBSD: patch-src_utilities_transliterate.cpp,v 1.2 2023/11/20 00:58:10 nia Exp $
 
 Support non-POSIX iconv prototype in NetBSD 9/earlier and SunOS.
 
---- src/utilities/transliterate.cpp.orig	2022-12-28 21:54:04.427431913 +0000
+--- src/utilities/transliterate.cpp.orig	2023-09-25 17:20:58.428898151 +0000
 +++ src/utilities/transliterate.cpp
-@@ -34,6 +34,15 @@
- 
- #include "transliterate.h"
+@@ -28,6 +28,15 @@
+ #  include <iconv.h>
+ #endif
  
 +#if defined(__NetBSD__)
 +#include <sys/param.h>
@@ -17,9 +17,9 @@ Support non-POSIX iconv prototype in NetBSD 9/earlier and SunOS.
 +#endif
 +#endif
 +
- namespace Utilities {
+ #include <QByteArray>
+ #include <QString>
  
- QString Transliterate(const QString &accented_str) {
 @@ -76,7 +85,11 @@ QString Transliterate(const QString &acc
  
    snprintf(input, input_len, "%s", utf8.constData());
