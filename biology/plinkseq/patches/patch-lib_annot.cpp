ef8c5045d58bfa04b3408ef80b4e331128a2b921
wiz <wiz@pkgsrc.org>
2023-11-23 09:04:24 +0000

    plinkseq: fix some pkglint

--- biology/plinkseq/patches/patch-lib_annot.cpp
+++ biology/plinkseq/patches/patch-lib_annot.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-lib_annot.cpp,v 1.1 2014/02/08 12:27:14 wiedi Exp $
+$NetBSD: patch-lib_annot.cpp,v 1.2 2023/11/23 09:04:24 wiz Exp $
 
 rename to avoid collision on sunos
+
 --- lib/annot.cpp.orig	2012-03-19 20:00:45.000000000 +0000
 +++ lib/annot.cpp
 @@ -34,7 +34,7 @@ std::map<seq_annot_t,std::string> popula
--- biology/plinkseq/patches/patch-lib_annot.h
+++ biology/plinkseq/patches/patch-lib_annot.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-lib_annot.h,v 1.1 2014/02/08 12:27:14 wiedi Exp $
+$NetBSD: patch-lib_annot.h,v 1.2 2023/11/23 09:04:24 wiz Exp $
 
 rename to avoid collision on sunos
+
 --- lib/annot.h.orig	2012-03-19 11:35:59.000000000 +0000
 +++ lib/annot.h
 @@ -35,7 +35,7 @@ enum seq_annot_t { UNDEF   =  0 ,     //
