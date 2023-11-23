b9046f2474ebdb25b8bddc8386780b33da4a7581
schmonz <schmonz@pkgsrc.org>
2023-11-23 18:51:33 +0000

    s6-dns: update to 2.3.7.0. Changes:
    - New s6dns_init_options() function, to choose whether
      to support /etc/hosts or not.
    - Properly ignore link-local addresses in hosts files
      instead of erroring out on them.
    - New s6dns_hosts_name46() macro.

--- net/s6-dns/buildlink3.mk
+++ net/s6-dns/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/11/11 11:53:34 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2023/11/23 18:51:33 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-dns
 
 .if !defined(S6_DNS_BUILDLINK3_MK)
 S6_DNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.6.0
+BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.7.0
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../net/s6-dns
 BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
 BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
