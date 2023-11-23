b5a9881d91db3efefd7878dd4c5cf56c4341845d
jperkin <jperkin@pkgsrc.org>
2023-11-23 11:12:33 +0000

    gettext-tools: Add termcap to the buildlink includes.
    This is required for dependencies that use libtextstyle.

--- devel/gettext-tools/buildlink3.mk
+++ devel/gettext-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/03 13:07:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/11/23 11:12:33 jperkin Exp $
 
 BUILDLINK_TREE+=	gettext-tools
 
@@ -8,6 +8,7 @@ GETTEXT_TOOLS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gettext-tools+=	gettext-tools>=0.18.1.1nb3
 BUILDLINK_PKGSRCDIR.gettext-tools?=	../../devel/gettext-tools
 
+.include "../../mk/termcap.buildlink3.mk"
 .endif	# GETTEXT_TOOLS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gettext-tools
