db6755df2883ff0c7449f09d4c552206ce8df1cd
wiz <wiz@pkgsrc.org>
2023-11-23 09:04:24 +0000

    plinkseq: fix some pkglint

--- biology/plinkseq/patches/patch-browser_main.c
+++ biology/plinkseq/patches/patch-browser_main.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-browser_main.c,v 1.1 2014/02/08 12:27:14 wiedi Exp $
+$NetBSD: patch-browser_main.c,v 1.2 2023/11/23 09:04:24 wiz Exp $
 
 not needed on sunos
+
 --- browser/main.c.orig	2011-11-17 17:17:36.000000000 +0000
 +++ browser/main.c
 @@ -21,8 +21,10 @@
