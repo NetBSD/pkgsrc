bd1eb8242710378bc723718e408e9f51ac8807a6
adam <adam@pkgsrc.org>
2023-11-26 16:45:02 +0000

    pysolfc: updated to 2.21.0
    v2.21.0
    
    Ten new games.
    Compatibility with Pillow 10.0.
    Several new display options.
    Bugfixes and enhancements.
    
    v2.20.1
    
    The release packages have been updated to use the latest version of Python, and all the dependent libraries.
    New alternate release packages for Windows and Mac that include the full cardsets library (these are larger and may take longer to download/install).
    Bugfixes and other enhancements.
    
    v2.20.0
    
    Over 40 new games, bringing the total to 1,200!
    Memory games can now use any type of cardset, including new Matching type cardsets.
    New options in the Solitaire Wizard.
    Additional documentation for a number of PySol's features.
    Bugfixes and other enhancements.

--- games/pysolfc/patches/patch-pysollib_settings.py
+++ games/pysolfc/patches/patch-pysollib_settings.py
@@ -1,13 +1,14 @@
-$NetBSD: patch-pysollib_settings.py,v 1.2 2022/01/03 10:55:26 wiz Exp $
+$NetBSD: patch-pysollib_settings.py,v 1.3 2023/11/26 16:45:02 adam Exp $
 
 Use custom data dir.
 
---- pysollib/settings.py.orig	2021-12-11 00:44:30.000000000 +0000
+--- pysollib/settings.py.orig	2023-07-11 01:24:10.000000000 +0000
 +++ pysollib/settings.py
-@@ -61,10 +61,7 @@ DATA_DIRS = []
+@@ -61,11 +61,7 @@ DATA_DIRS = []
  # you can add your extra directories here
  if os.name == 'posix':
      DATA_DIRS = [
+-        '/app/share/PySolFC',
 -        '/usr/share/PySolFC',
 -        '/usr/local/share/PySolFC',
 -        '/usr/share/games/PySolFC',
