b7fa4b041536338dc4140bc1f2aa281a61df45b9
adam <adam@pkgsrc.org>
2023-11-26 14:47:09 +0000

    SDL2_sound: updated to 2.0.2
    SDL_sound 2.0.2
    
    This is the release of version 2.0.2 of SDL_sound. This is an official, stable release, and everyone is encouraged to upgrade. This release remains API and ABI compatible with previous releases of SDL_sound 2.0.x.
    
    Significant changes
    
    Tons of work on build fixes and improvements to the CMake project.
    Sound_Sample::buffer is now guaranteed to be aligned and padded for SIMD access.
    Audio conversion is now done via SDL_AudioStream, instead of SDL_AudioCVT, for better results.
    Added MP3 duration support.
    Fixes for WAV, Ogg Vorbis, and Midi decoding.
    WAV decoder now supports 24-bit int PCM data.
    Updated dr_mp3 and dr_flac.
    Release tarball is manually built with a name/directory structure that matches other SDL2 satellite libraries.
    Other bug fixes and improvements.

--- audio/SDL2_sound/buildlink3.mk
+++ audio/SDL2_sound/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/05/06 19:08:45 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/11/26 14:47:09 adam Exp $
 
 BUILDLINK_TREE+=	SDL2_sound
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	SDL2_sound
 SDL2_SOUND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_sound+=	SDL2_sound>=2.0.1
-BUILDLINK_ABI_DEPENDS.SDL2_sound?=	SDL2_sound>=2.0.1nb1
+BUILDLINK_ABI_DEPENDS.SDL2_sound+=	SDL2_sound>=2.0.1nb1
 BUILDLINK_PKGSRCDIR.SDL2_sound?=	../../audio/SDL2_sound
 
 .include "../../devel/SDL2/buildlink3.mk"
