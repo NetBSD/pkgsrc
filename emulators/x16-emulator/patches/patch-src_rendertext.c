a5c5cc5d4c60ccbb7a6874ed113ab463c1836e83
thorpej <thorpej@pkgsrc.org>
2023-11-27 04:17:30 +0000

    Safely pass chars to ctype(3) functions in cartridge.c, too.

--- emulators/x16-emulator/patches/patch-src_rendertext.c
+++ emulators/x16-emulator/patches/patch-src_rendertext.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_rendertext.c,v 1.1 2022/08/07 17:38:08 thorpej Exp $
+$NetBSD: patch-src_rendertext.c,v 1.2 2023/11/27 04:17:30 thorpej Exp $
+
+Safely pass a char to ctype(3) functions.
 
 --- src/rendertext.c.orig	2022-08-07 02:20:35.066620319 +0000
 +++ src/rendertext.c	2022-08-07 02:22:17.485680282 +0000
