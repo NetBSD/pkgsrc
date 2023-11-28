e54fd5f33725eb7b7ce073f5ef688b3d0d8ba845
jperkin <jperkin@pkgsrc.org>
2023-11-28 11:14:21 +0000

    gmpc: Remove default BUILDLINK_DEPMETHOD.gmpc=build.
    It doesn't make any sense, plugins are useless without the main package, and
    it causes indirect depends issues.

--- audio/gmpc/buildlink3.mk
+++ audio/gmpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.43 2023/11/14 14:01:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.44 2023/11/28 11:14:21 jperkin Exp $
 
 BUILDLINK_TREE+=	gmpc
 
@@ -8,7 +8,6 @@ GMPC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gmpc+=	gmpc>=0.20.0
 BUILDLINK_ABI_DEPENDS.gmpc+=	gmpc>=11.8.16nb46
 BUILDLINK_PKGSRCDIR.gmpc?=	../../audio/gmpc
-BUILDLINK_DEPMETHOD.gmpc?=	build
 
 .include "../../audio/libmpd/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
