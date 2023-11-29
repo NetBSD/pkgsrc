04bd6719801a917bf95e0799553332f380a330b7
jperkin <jperkin@pkgsrc.org>
2023-11-29 11:02:40 +0000

    python: Exclude dependencies for build.
    When python bl3 files are included with PYTHON_FOR_BUILD_ONLY=yes, their
    DEPMETHOD is set to "build", in which case we do not want to include
    indirect dependencies as they will then be tagged as "build" also.  Fixes
    potential runtime issues exposed by indirect dependency checks.

--- lang/python310/buildlink3.mk
+++ lang/python310/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/10/05 19:07:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2023/11/29 11:02:41 jperkin Exp $
 
 BUILDLINK_TREE+=	python310
 
@@ -16,9 +16,12 @@ BUILDLINK_INCDIRS.python310+=	include/python3.10
 BUILDLINK_LIBDIRS.python310+=	lib/python3.10/config
 BUILDLINK_TRANSFORM+=		l:python:python3.10
 
+.if !${BUILDLINK_DEPMETHOD.python310:U:Mbuild}
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.endif
+
 .endif # PYTHON310_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-python310
