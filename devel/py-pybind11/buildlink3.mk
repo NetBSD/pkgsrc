1cc4807010bdccc2dcf7c0de777c4e7179fc11c5
jperkin <jperkin@pkgsrc.org>
2023-11-29 10:53:11 +0000

    py-pybind11: Default to python for build only.
    This buildlink3.mk has a default DEPMETHOD of "build", so this helps avoid
    unwanted indirect buildlink3 dependencies from python.

--- devel/py-pybind11/buildlink3.mk
+++ devel/py-pybind11/buildlink3.mk
@@ -1,14 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/05/01 07:54:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/11/29 10:53:11 jperkin Exp $
 
 BUILDLINK_TREE+=	py-pybind11
 
 .if !defined(PY_PYBIND11_BUILDLINK3_MK)
 PY_PYBIND11_BUILDLINK3_MK:=
 
+# As this package defaults to DEPMETHOD=build we also set python to default
+# to build, and avoid pulling in unwanted indirect buildlink3 dependencies.
+PYTHON_FOR_BUILD_ONLY?=		yes
 .include "../../lang/python/pyversion.mk"
+
 BUILDLINK_API_DEPENDS.py-pybind11+=	${PYPKGPREFIX}-pybind11>=2.5.0
 BUILDLINK_PKGSRCDIR.py-pybind11?=	../../devel/py-pybind11
 BUILDLINK_DEPMETHOD.py-pybind11?=	build
+
 .endif	# PY_PYBIND11_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-pybind11
