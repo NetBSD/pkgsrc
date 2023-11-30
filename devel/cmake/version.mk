365fbda5b31a3d448d47d2e79e0287c9086cb261
adam <adam@pkgsrc.org>
2023-11-30 20:18:54 +0000

    cmake cmake-gui: updated to 3.27.9
    CMake 3.27.9
    * Tests/FortranModules: add test for TARGET_OBJECTS-as-sources module usage
    * Tests/FortranModules: add test for TARGET_OBJECTS-as-linked-items module usage
    * Ninja: Revert exact collation dependencies for 3.27
    * Help: Document that CMP0149 should be set before project()
    * Help: Fix ctest(1) manual links to www.cdash.org
    * fileapi: Fix file sets' base directories relative to top source
    * Ninja: depfile: keep rules without dependencies

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.48 2023/11/19 17:16:27 adam Exp $
+# $NetBSD: version.mk,v 1.49 2023/11/30 20:18:54 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.27.8
+CMAKE_VERSION=	3.27.9
 CMAKE_API=	${CMAKE_VERSION:R}
