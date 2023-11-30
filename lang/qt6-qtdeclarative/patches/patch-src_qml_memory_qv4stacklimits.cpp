e937f941fef0ed34a40c9adcba4a03bb843b2e54
adam <adam@pkgsrc.org>
2023-11-30 20:11:26 +0000

    qt6: updated to 6.6.1
    As a patch release, Qt 6.6.1 does not introduce any new features but contains more than 400 bug fixes, security updates, and other improvements to the top of the Qt 6.6.0 release. See more information about the most important changes and bug fixes from Qt 6.6.1 release note.

--- lang/qt6-qtdeclarative/patches/patch-src_qml_memory_qv4stacklimits.cpp
+++ lang/qt6-qtdeclarative/patches/patch-src_qml_memory_qv4stacklimits.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_qml_memory_qv4stacklimits.cpp,v 1.2 2023/09/29 21:12:15 adam Exp $
+$NetBSD: patch-src_qml_memory_qv4stacklimits.cpp,v 1.3 2023/11/30 20:11:27 adam Exp $
 
 Solaris uses pthread_attr_get_np for stack info.
 
---- src/qml/memory/qv4stacklimits.cpp.orig	2023-09-24 07:37:49.000000000 +0000
+--- src/qml/memory/qv4stacklimits.cpp.orig	2023-11-20 05:53:14.000000000 +0000
 +++ src/qml/memory/qv4stacklimits.cpp
 @@ -234,7 +234,7 @@ StackProperties stackPropertiesGeneric(q
  
@@ -10,6 +10,6 @@ Solaris uses pthread_attr_get_np for stack info.
      pthread_attr_t sattr;
 -#  if defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(Q_OS_NETBSD)
 +#  if defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(Q_OS_NETBSD) || defined(Q_OS_SOLARIS)
+     pthread_attr_init(&sattr);
      pthread_attr_get_np(thread, &sattr);
  #  else
-     pthread_getattr_np(thread, &sattr);
