f2587a4ece1848050be1c22461840a2bd74c2e6d
rhialto <rhialto@pkgsrc.org>
2023-12-01 18:35:11 +0000

    lang/algol68g: update to 3.4.4.
    Version 3.4.0-4, October 2023
    * Fix Darwin configuration issue.
    * Enable plugin compiler on Apple Silicon Macs.
    * C17 compliant code.
    * Procedure "http content" now requires libcurl.
    * Procedure "tcp request" is deprecated.
    * Minor fixes.
    
    Version 3.3.1-24, August/September 2023
    * Fix configuration issue for 32-bit systems.
    * Minor fixes.
    * Distribution updates.

--- lang/algol68g/options.mk
+++ lang/algol68g/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2020/05/21 07:02:56 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2023/12/01 18:35:11 rhialto Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.a68g
-PKG_SUPPORTED_OPTIONS=	plotutils pgsql
+PKG_SUPPORTED_OPTIONS=	plotutils pgsql longtypes
 PKG_SUGGESTED_OPTIONS=	plotutils
 
 .include "../../mk/bsd.options.mk"
@@ -13,12 +13,22 @@ CONFIGURE_ARGS+=	--enable-plotutils
 CONFIGURE_ARGS+=	--disable-plotutils
 .endif
 
+# The implementation of this is somewhat Linux-centric so
+# it may not fully work as expected when enabled elsewhere.
+.if !empty(PKG_OPTIONS:Mlongtypes)
+.  include "../../math/mpfr/buildlink3.mk" # fails some configure checks
+.  include "../../math/libquadmath/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-long-types
+.else
+CONFIGURE_ARGS+=	--disable-long-types
+.endif
+
 # It seems that when including PostgreSQL support it doesn't actually
 # need to have it installed at build time, just at run time.
 # So maybe this DEPENDS is undesirable.
 .if !empty(PKG_OPTIONS:Mpgsql)
 CONFIGURE_ARGS+=	--enable-postgresql
-.include "../../mk/pgsql.buildlink3.mk"
+.  include "../../mk/pgsql.buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-postgresql
 .endif
--- lang/algol68g/patches/patch-src_a68g_a68g-options.c
+++ lang/algol68g/patches/patch-src_a68g_a68g-options.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_a68g_a68g-options.c,v 1.1 2023/03/29 10:11:13 rhialto Exp $
+$NetBSD: patch-src_a68g_a68g-options.c,v 1.2 2023/12/01 18:35:12 rhialto Exp $
 
 * Enable compiler for *BSD.
 
---- src/a68g/a68g-options.c.orig	2023-03-19 20:10:12.000000000 +0000
+--- src/a68g/a68g-options.c.orig	2023-10-27 20:14:34.000000000 +0000
 +++ src/a68g/a68g-options.c
-@@ -552,7 +552,7 @@ BOOL_T set_options (OPTION_LIST_T * i, B
+@@ -478,7 +478,7 @@ BOOL_T need_library (OPTION_LIST_T *i)
  #endif
-             }
-             if (eq (q, "linux")) {
--#if !defined (BUILD_LINUX)
-+#if (!defined (BUILD_LINUX) && !defined(BUILD_BSD))
-               io_close_tty_line ();
-               WRITE (STDOUT_FILENO, "linux required - exiting graciously");
-               a68_exit (EXIT_SUCCESS);
+   }
+   if (eq (q, "linux")) {
+-#if defined (BUILD_LINUX)
++#if defined (BUILD_LINUX) || defined(BUILD_BSD)
+     return (A68_TRUE);
+ #else
+     io_close_tty_line ();
