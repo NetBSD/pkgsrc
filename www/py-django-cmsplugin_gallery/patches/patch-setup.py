7d2c5ccf64b037cadf321e716fc1814ed3473a8e
adam <adam@pkgsrc.org>
2023-12-07 17:43:51 +0000

    py-django-cmsplugin_gallery: updated to 2.0.3
    2.0.3 / 2022-04-20
    ==================
    
      * fix: issue with the history.md file not included in MANIFEST.in file
    
    2.0.0 / 2022-04-20
    ==================
    
      * feat: add make file for release automation and generate python3 wheels only
      * correct readme
      * fix: correct metadata
      * feat:add more metadata to the setup.py and add a setup.cfg too
      * feat: update ci setup
      * feat: set default auto field
      * feat: get package building for python 3.10 too
      * feat: python3+ only
      * fix: upgrade deps and also add more settins
      * feat: try updated package for djangocms-helper
      * fix: upgrade to a newer version of djangocms-helper
      * feat: add github actions
      * feat: make python code python3.7+
      * fix: make code django 3.2+ compatible
      * Merge pull request 50 from centralniak/fix/failing-migration-tofiler
      * wrap migration in try except
      * bump version
      * fix yet another typo
      * make it backward compatible
      * add module name in settings name
      * bump version
      * fix typo
      * add configurable module name

--- www/py-django-cmsplugin_gallery/patches/patch-setup.py
+++ www/py-django-cmsplugin_gallery/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2020/02/19 11:09:52 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2023/12/07 17:43:51 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2018-05-03 10:53:37.000000000 +0000
+--- setup.py.orig	2022-04-20 15:48:34.000000000 +0000
 +++ setup.py
-@@ -11,7 +11,7 @@ setup(
-     description = 'DjangoCMS image gallery plugin with drag&drop '
-                   'reordering in admin, support for thumbnails and '
+@@ -48,7 +48,7 @@ setup(
                    'jQueryTOOLS overlay.',
+     long_description=readme + "\n\n" + history,
+     long_description_content_type="text/markdown",
 -    packages=find_packages(),
 +    packages=find_packages(exclude=['tests']),
      provides=['cmsplugin_gallery', ],
      include_package_data=True,
-     install_requires = ['django-cms>=3.2.0', 'django-inline-ordering>=0.1.1',
+     install_requires = ['django-cms>=3.9.0', 'django-inline-ordering==1.0.2',
