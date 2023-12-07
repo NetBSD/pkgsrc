dd4919a945246059e7fd2221ad4eff23ac5e67a2
gdt <gdt@pkgsrc.org>
2023-12-07 17:05:10 +0000

    misc/py-anita: Repair xen option and simplify
    Restore the xen option and point to xen{kernel,tools}418.  Note that
    either 415 or 418 should work.
    
    Stop making qemu be an options group, because it only has one member
    and this is likely to continue.  Remove comments about qemu0 and
    needing 2.0.0nb4, because that's well more than 5 years in the past.
    Pick qemu>=3 as the dependency pattern, to be comfortably past
    2.0.0nb4, while not unreasonably requiring last week's qemu.  Likely
    newer is needed, but advancing the requirement is left for future
    work.
    
    This commit results in no change by default, because all options were
    and are default off.  It merely provides better documentation for
    emulation/hypervisor choices.

--- misc/py-anita/options.mk
+++ misc/py-anita/options.mk
@@ -1,30 +1,26 @@
-# $NetBSD: options.mk,v 1.12 2023/12/07 09:33:05 wiz Exp $
+# $NetBSD: options.mk,v 1.13 2023/12/07 17:05:10 gdt Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.py-anita
-PKG_SUPPORTED_OPTIONS=		# xen
-PKG_OPTIONS_OPTIONAL_GROUPS=	qemu
-PKG_OPTIONS_GROUP.qemu=		qemu
+PKG_SUPPORTED_OPTIONS=		qemu xen
 PKG_SUGGESTED_OPTIONS=
 
 # To run (but not to build), anita needs some way to run a virtual
-# machine.  The standard approach is qemu-0, because it is known to
-# work.  Newer qemu is problematic.  xen also works, but is less well
-# tested.  See anita(1) for details of the above.
+# machine.  The standard approach is qemu.  An alternate approach is
+# xen (when running anita on a dom0).
 
 # By default, py-anita does not depend on any of these, because a
-# hypervisor one doesn't want to use is a large burden (qemu in
-# particular is very large).  While these options can be enabled, they
+# hypervisor one doesn't want to use is a large burden.  While these
+# options can be enabled to cause anita to depend on qemu or xen, they
 # also serve to document ways to fulfill the hypervisor requirement.
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqemu)
-# Some versions of qemu1/2 do not work reliably, see the anita(1)
-# man page.  Version 2.0.0nb4 or newer should work.
-DEPENDS+=	qemu>=1.0:../../emulators/qemu
+DEPENDS+=	qemu>=3:../../emulators/qemu
 .endif
 
-#.if !empty(PKG_OPTIONS:Mxen)
-#DEPENDS+=	xenkernel413>=4.2:../../sysutils/xenkernel413
-#DEPENDS+=	xentools413>=4.2:../../sysutils/xentools413
-#.endif
+# Either 415 or 418 should work; we choose 418 because it's newer.
+.if !empty(PKG_OPTIONS:Mxen)
+DEPENDS+=	xenkernel418>=4.2:../../sysutils/xenkernel418
+DEPENDS+=	xentools418>=4.2:../../sysutils/xentools418
+.endif
