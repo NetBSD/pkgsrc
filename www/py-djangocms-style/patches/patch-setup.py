224303d7abd9c7793f10d7173a5a94b24882ac28
adam <adam@pkgsrc.org>
2023-12-07 17:57:26 +0000

    py-djangocms-style: updated to 3.1.0
    3.1.0
    
    Add GitHub Actions
    FEATURE: adjust setup metadata
    FEATURE: add github pr template
    Removed pinned django-treebeard
    Feat: Django 4.2 support
    chore: Improve ruff config

--- www/py-djangocms-style/patches/patch-setup.py
+++ www/py-djangocms-style/patches/patch-setup.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-setup.py,v 1.3 2021/01/16 10:12:50 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2023/12/07 17:57:26 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2021-01-16 10:02:35.000000000 +0000
+--- setup.py.orig	2023-12-06 22:08:53.000000000 +0000
 +++ setup.py
-@@ -45,7 +45,7 @@ setup(
-     license='BSD-3-Clause',
-     description='Adds style plugin to django CMS',
-     long_description=open('README.rst').read(),
+@@ -51,7 +51,7 @@ setup(
+     license="BSD-3-Clause",
+     description="Adds style plugin to django CMS",
+     long_description=open("README.rst").read(),
 -    packages=find_packages(),
 +    packages=find_packages(exclude=['tests']),
      include_package_data=True,
