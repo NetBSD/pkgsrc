1564c5833a72dcb751eb8d311b5a2c03f46fb4ca
zafer <zafer@pkgsrc.org>
2023-12-08 04:21:44 +0000

    httpsify

--- mk/fetch/sites.mk
+++ mk/fetch/sites.mk
@@ -1,4 +1,4 @@
-# $NetBSD: sites.mk,v 1.187 2023/12/06 10:56:55 zafer Exp $
+# $NetBSD: sites.mk,v 1.188 2023/12/08 04:21:44 zafer Exp $
 #
 # This Makefile fragment defines read-only MASTER_SITE_* variables
 # representing some well-known master distribution sites for software.
@@ -13,8 +13,8 @@ MASTER_SITE_XCONTRIB+=  \
 MASTER_SITE_GNU+=       \
 	https://mirrors.dotsrc.org/gnu/ \
 	https://ftp.nluug.nl/pub/gnu/ \
-	http://mirrors.kernel.org/gnu/ \
-	http://ftp.funet.fi/pub/gnu/prep/ \
+	https://mirrors.kernel.org/gnu/ \
+	https://ftp.funet.fi/pub/gnu/prep/ \
 	https://ftp.gnu.org/pub/gnu/
 
 MASTER_SITE_GITHUB+=    \
@@ -29,32 +29,27 @@ MASTER_SITE_GNUSTEP+=   \
 MASTER_SITE_OSDN+=	\
 	http://jaist.dl.osdn.jp/ \
 	http://iij.dl.osdn.jp/ \
-	http://tcpdiag.dl.osdn.jp/ \
 	http://c3sl.dl.osdn.jp/ \
 	http://onet.dl.osdn.jp/ \
 	http://nchc.dl.osdn.jp/ \
 	http://osdn.dl.osdn.jp/
 
 MASTER_SITE_PERL_CPAN+= \
-	https://cpan.perl.org/CPAN/modules/by-module/ \
-	http://cpan.pair.com/modules/by-module/ \
-	http://ftp.funet.fi/pub/languages/perl/CPAN/modules/by-module/ \
+	https://www.cpan.org/modules/by-module/ \
+	https://cpan.pair.com/modules/by-module/ \
+	https://ftp.funet.fi/pub/languages/perl/CPAN/modules/by-module/ \
 	ftp://ftp.fi.muni.cz/pub/CPAN/modules/by-module/
 
 MASTER_SITE_R_CRAN+=    \
 	https://cran.r-project.org/src/ \
-	http://cran.r-project.org/src/ \
 	ftp://cran.r-project.org/pub/R/src/ \
-	http://cran.at.r-project.org/src/ \
 	ftp://cran.at.r-project.org/pub/R/src/ \
-	https://cran.ch.r-project.org/src/ \
-	https://cran.uk.r-project.org/src/ \
 	http://lib.stat.cmu.edu/R/CRAN/src/ \
 	https://stat.ethz.ch/CRAN/src/ \
 	https://www.stats.bris.ac.uk/R/src/
 
 MASTER_SITE_TEX_CTAN+= \
-	http://ftp.funet.fi/pub/TeX/CTAN/ \
+	https://ftp.funet.fi/pub/TeX/CTAN/ \
 	ftp://www.dnsbalance.ring.gr.jp/pub/text/CTAN/ \
 	http://www.dnsbalance.ring.gr.jp/archives/text/CTAN/
 
@@ -79,7 +74,7 @@ MASTER_SITE_KDE+=	\
 	ftp://ftp.fi.muni.cz/pub/kde/stable/
 
 MASTER_SITE_SOURCEFORGE+=	\
-	http://downloads.sourceforge.net/sourceforge/
+	https://downloads.sourceforge.net/sourceforge/
 
 MASTER_SITE_SUSE+=	\
 	ftp://rpmfind.net/linux/opensuse/update/${SUSE_VERSION}/${MACHINE_ARCH}/ \
@@ -87,13 +82,13 @@ MASTER_SITE_SUSE+=	\
 	ftp://fr2.rpmfind.net/linux/opensuse/update/${SUSE_VERSION}/${MACHINE_ARCH}/
 
 MASTER_SITE_MOZILLA_ALL+=   \
-	http://download-origin.cdn.mozilla.net/pub/ \
-	http://ftp.mozilla.org/pub/
+	https://download-origin.cdn.mozilla.net/pub/ \
+	https://ftp.mozilla.org/pub/
 
 # see http://www.mozilla.org/mirrors.html
 MASTER_SITE_MOZILLA+=	\
-	http://releases.mozilla.org/pub/ \
-	http://download-origin.cdn.mozilla.net/pub/
+	https://releases.mozilla.org/pub/ \
+	https://download-origin.cdn.mozilla.net/pub/
 
 MASTER_SITE_XEMACS+=    \
 	ftp://ftp.xemacs.org/pub/xemacs/ \
@@ -107,22 +102,22 @@ MASTER_SITE_APACHE+=    \
 	https://archive.apache.org/dist/
 
 MASTER_SITE_MYSQL+=	\
-	http://mysql.linux.cz/Downloads/ \
+	https://mysql.linux.cz/Downloads/ \
 	ftp://ftp.fi.muni.cz/pub/mysql/Downloads/ \
-	http://mirrors.dotsrc.org/mysql/Downloads/ \
+	https://mirrors.dotsrc.org/mysql/Downloads/ \
 	ftp://mirrors.dotsrc.org/mysql/Downloads/ \
-	http://ftp.gwdg.de/pub/misc/mysql/Downloads/ \
+	https://ftp.gwdg.de/pub/misc/mysql/Downloads/ \
 	ftp://ftp.gwdg.de/pub/misc/mysql/Downloads/ \
 	ftp://ftp.fu-berlin.de/unix/databases/mysql/Downloads/ \
-	http://www.mirrorservice.org/sites/ftp.mysql.com/Downloads/ \
+	https://www.mirrorservice.org/sites/ftp.mysql.com/Downloads/ \
 	ftp://ftp.mirrorservice.org/sites/ftp.mysql.com/Downloads/ \
-	http://mirror.csclub.uwaterloo.ca/mysql/Downloads/ \
+	https://mirror.csclub.uwaterloo.ca/mysql/Downloads/ \
 	ftp://mirror.csclub.uwaterloo.ca/mysql/Downloads/ \
-	http://ftp.iij.ad.jp/pub/db/mysql/Downloads/ \
+	https://ftp.iij.ad.jp/pub/db/mysql/Downloads/ \
 	ftp://ftp.iij.ad.jp/pub/db/mysql/Downloads/
 
 MASTER_SITE_DEBIAN+= \
-	http://ftp.debian.org/debian/ \
+	https://ftp.debian.org/debian/ \
 	http://ftp.at.debian.org/debian/ \
 	http://ftp.au.debian.org/debian/ \
 	http://ftp.wa.au.debian.org/debian/ \
@@ -154,27 +149,27 @@ MASTER_SITE_OPENOFFICE+=	\
 	ftp://ftp-stud.fht-esslingen.de/pub/Mirrors/ftp.openoffice.org/
 
 MASTER_SITE_CYGWIN+= \
-	http://mirrors.kernel.org/sources.redhat.com/cygwin/ \
-	http://mirrors.xmission.com/cygwin/ \
+	https://mirrors.kernel.org/sources.redhat.com/cygwin/ \
+	https://mirrors.xmission.com/cygwin/ \
 	ftp://ftp.dnsbalance.ring.gr.jp/pub/pc/gnu-win32/ \
-	http://ftp.funet.fi/pub/mirrors/cygwin.com/pub/cygwin/
+	https://ftp.funet.fi/pub/mirrors/cygwin.com/pub/cygwin/
 
 MASTER_SITE_IFARCHIVE+= \
 	http://ifarchive.flavorplex.com/if-archive/
 
 MASTER_SITE_PGSQL+=	\
-	http://ftp.postgresql.org/pub/
+	https://ftp.postgresql.org/pub/
 
 MASTER_SITE_GENTOO+= \
 	http://ftp-stud.fht-esslingen.de/pub/Mirrors/gentoo/ \
-	http://mirror.bytemark.co.uk/gentoo/ \
+	https://mirror.bytemark.co.uk/gentoo/ \
 	ftp://ftp.free.fr/mirrors/ftp.gentoo.org/ \
 	http://ftp.ntua.gr/pub/linux/gentoo/ \
-	http://mirror.yandex.ru/gentoo-distfiles/ \
-	http://ftp.swin.edu.au/gentoo/ \
-	http://ftp.iij.ad.jp/pub/linux/gentoo/ \
-	http://ftp.jaist.ac.jp/pub/Linux/Gentoo/ \
-	http://distfiles.gentoo.org/
+	https://mirror.yandex.ru/gentoo-distfiles/ \
+	https://ftp.swin.edu.au/gentoo/ \
+	https://ftp.iij.ad.jp/pub/linux/gentoo/ \
+	https://ftp.jaist.ac.jp/pub/Linux/Gentoo/ \
+	https://distfiles.gentoo.org/
 
 MASTER_SITE_XORG+= \
 	https://xorg.freedesktop.org/releases/individual/ \
@@ -187,12 +182,11 @@ MASTER_SITE_FREEBSD_LOCAL+= \
 	http://distcache.FreeBSD.org/local-distfiles/
 
 MASTER_SITE_NETLIB+= \
-	http://netlib.org/ \
-	http://netlib.sandia.gov/ \
-	http://www.mirrorservice.org/sites/netlib.bell-labs.com/netlib/
+	https://netlib.org/ \
+	https://netlib.sandia.gov/
 
 MASTER_SITE_OPENBSD+= \
-	http://ftp.OpenBSD.org/pub/OpenBSD/ \
+	https://ftp.OpenBSD.org/pub/OpenBSD/ \
 	ftp://ftp3.usa.openbsd.org/pub/OpenBSD/ \
 	ftp://ftp.jaist.ac.jp/pub/OpenBSD/
 
@@ -203,13 +197,13 @@ MASTER_SITE_PYPI+= \
 	https://files.pythonhosted.org/packages/source/
 
 MASTER_SITE_HASKELL_HACKAGE+=	\
-	http://hackage.haskell.org/package/
+	https://hackage.haskell.org/package/
 
 MASTER_SITE_CRATESIO+=	\
 	https://crates.io/api/v1/crates/
 
 # The primary backup site.
 MASTER_SITE_BACKUP?=	\
-	http://cdn.NetBSD.org/pub/pkgsrc/distfiles/ \
+	https://cdn.NetBSD.org/pub/pkgsrc/distfiles/ \
 	http://ftp.fr.NetBSD.org/pub/pkgsrc/distfiles/ \
-	http://ftp.NetBSD.org/pub/pkgsrc/distfiles/
+	https://ftp.NetBSD.org/pub/pkgsrc/distfiles/
