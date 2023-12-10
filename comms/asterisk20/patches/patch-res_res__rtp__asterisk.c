a34327aae8a0599e3891d0265180b9950069f5d7
ryoon <ryoon@pkgsrc.org>
2023-12-10 06:40:20 +0000

    comms/asterisk20: import asterisk-20.5.0
    Asterisk is a complete PBX in software.  It provides all of the
    features you would expect from a PBX and more. Asterisk does voice
    over IP in three protocols, and can interoperate with almost all
    standards-based telephony equipment using relatively inexpensive
    hardware.
    
    Asterisk provides Voicemail services with Directory, Call Conferencing,
    Interactive Voice Response, Call Queuing. It has support for
    three-way calling, caller ID services, ADSI, SIP and H.323 (as both
    client and gateway).
    
    This is a long term support version, 20. It is scheduled to go to
    security fixes only on 2026-10-19, and EOL on 2027-10-19. For more
    information about Asterisk versions, see
    https://docs.asterisk.org/About-the-Project/Asterisk-Versions/

---
+++ comms/asterisk20/patches/patch-res_res__rtp__asterisk.c
@@ -0,0 +1,24 @@
+$NetBSD: patch-res_res__rtp__asterisk.c,v 1.1 2023/12/10 06:40:20 ryoon Exp $
+
+--- res/res_rtp_asterisk.c.orig	2022-02-10 11:53:37.000000000 +0000
++++ res/res_rtp_asterisk.c
+@@ -69,6 +69,10 @@
+ #include <ifaddrs.h>
+ #endif
+ 
++static int is_zero_address(struct in_addr *addr) {
++  return !addr->s_addr;
++}
++
+ #include "asterisk/conversions.h"
+ #include "asterisk/options.h"
+ #include "asterisk/logger_category.h"
+@@ -3654,7 +3658,7 @@ static void rtp_add_candidates_to_ice(st
+ 	ast_rwlock_unlock(&stunaddr_lock);
+ 
+ 	/* If configured to use a STUN server to get our external mapped address do so */
+-	if (stunaddr_copy.sin_addr.s_addr && !stun_address_is_blacklisted(addr) &&
++        if (!is_zero_address(&stunaddr.sin_addr) && !stun_address_is_blacklisted(addr) &&
+ 		(ast_sockaddr_is_ipv4(addr) || ast_sockaddr_is_any(addr)) &&
+ 		count < PJ_ICE_MAX_CAND) {
+ 		struct sockaddr_in answer;
