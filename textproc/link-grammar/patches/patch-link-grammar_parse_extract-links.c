4d2aedcf9fb7ae94c5ef1e37db278d0bcf348b6d
jperkin <jperkin@pkgsrc.org>
2023-12-11 16:32:19 +0000

    link-grammar: SunOS build fixes.

--- textproc/link-grammar/patches/patch-link-grammar_parse_extract-links.c
+++ textproc/link-grammar/patches/patch-link-grammar_parse_extract-links.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-link-grammar_parse_extract-links.c,v 1.1 2023/10/15 06:39:25 ryoon Exp $
+$NetBSD: patch-link-grammar_parse_extract-links.c,v 1.2 2023/12/11 16:32:19 jperkin Exp $
+
+__GNUC__ does not imply malloc_trim() support.
 
 --- link-grammar/parse/extract-links.c.orig	2023-03-16 18:49:51.000000000 +0000
 +++ link-grammar/parse/extract-links.c
@@ -7,7 +9,7 @@ $NetBSD: patch-link-grammar_parse_extract-links.c,v 1.1 2023/10/15 06:39:25 ryoo
  	xfree((void *) pex, sizeof(extractor_t));
  
 -#if defined __GNUC__
-+#if defined(__GNUC__) && !defined(__NetBSD__)
++#if defined(__GNUC__) && !defined(__NetBSD__) && !defined(__sun)
  	// malloc_trim() is a gnu extension.  An alternative would be
  	// to call madvise(MADV_DONTNEED) but this is more complicated.
  	if (trim) malloc_trim(0);
