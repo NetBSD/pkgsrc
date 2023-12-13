35c21602b76931adbe13d17b815cd96dd88ca17e
nros <nros@pkgsrc.org>
2023-12-13 11:44:26 +0000

    Update devel/c++gsl to version 4.0.0
    Pkgsrc changes:
    Use devel/cmake/build.mk
    CMake only looks for the c++ compiler so remove c
    from USE_LANGAGES.
    Due to a breaking change, that gsl/multi_span is not
    installed anymore and gsl::narrow is in gsl/narrow not
    gsl/util, update BUILDLINK_API_DEPENDS.
    Change maintainer, ok'ed by minskim.
    
    Changes from Release Notes:
    
    * Deprecation of gsl::string_span
    * Removal of <gsl/multi_span>
    * Header files dropped the gsl_ prefix
    * Changes to not_null
    * gsl::span and std::span now use the correct specialization of gsl::at
    * The zstring family no longer requires empty brackets to be used
      - for example, void foo(zstring<> str); should now be
        void foo(zstring str);
    * gsl::narrowing_error now has a helpful what() message
    * finally and final_action are now [[nodiscard]]
    * GSL will work in environments where exceptions are disabled, with
      some caveats
    * GSL will work in environments which do not support ios, via the
      addition of the GSL_NO_IOSTREAMS flag
    * CMake and build improvements

--- devel/c++gsl/buildlink3.mk
+++ devel/c++gsl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/07/20 21:45:26 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2023/12/13 11:44:26 nros Exp $
 
 BUILDLINK_TREE+=	c++gsl
 
@@ -7,7 +7,7 @@ C++GSL_BUILDLINK3_MK:=
 
 USE_CXX_FEATURES+=	c++14
 
-BUILDLINK_API_DEPENDS.c++gsl+=	c++gsl>=3.1.0
+BUILDLINK_API_DEPENDS.c++gsl+=	c++gsl>=4.0.0
 BUILDLINK_PKGSRCDIR.c++gsl?=	../../devel/c++gsl
 BUILDLINK_DEPMETHOD.c++gsl?=	build
 .endif	# C++GSL_BUILDLINK3_MK
