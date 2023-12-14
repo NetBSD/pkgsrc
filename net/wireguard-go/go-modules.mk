7c128f107bbac4322a34d7f231317db24b937585
adam <adam@pkgsrc.org>
2023-12-14 13:56:23 +0000

    wireguard-go: added version 0.0.20230223
    WireGuard is an extremely simple yet fast and modern VPN that utilizes
    state-of-the-art cryptography. It aims to be faster, simpler, leaner, and more
    useful than IPsec, while avoiding the massive headache. It intends to be
    considerably more performant than OpenVPN. WireGuard is designed as a general
    purpose VPN for running on embedded interfaces and super computers alike, fit
    for many different circumstances.
    
    This is an implementation of WireGuard in Go.

---
+++ net/wireguard-go/go-modules.mk
@@ -0,0 +1,16 @@
+# $NetBSD: go-modules.mk,v 1.1 2023/12/14 13:56:23 adam Exp $
+
+GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.mod
+GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.2.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.2.0.zip
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.mod
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.zip
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.mod
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.zip
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20221203005347-703fd9b7fbc0.mod
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20221203005347-703fd9b7fbc0.zip
