416cdf931835a5b28ba9b65c24df7229992e911b
nros <nros@pkgsrc.org>
2023-12-17 13:59:38 +0000

    libtecla: Update to version 1.6.3, fix install on solaris
    pkgsrc changes:
    Install the man pages in the same catalogs
    as other systems in pkgsrc.
    Also install .so.${PKG_VERSION} on solaris.
    Readd PLIST.SunOS for the reentrant versions
    of libtecla.
    patch-Makefile.in removed since it has been
    applied upstream.
    Set LICENSE.
    
    Changes from release notes:
    Version 1.6.3:
    
      This release corrects some problems in the build process,
      including one that was preventing libtecla from being compiled
      on Mac OS X.
    
    Version 1.6.2:
    
      This release updates the configuration script to ensure that the
      enhance utility program is compiled correctly on systems that have
      system V psuedo-terminal allocation but not system V streams.
    
      There are no new features.

--- devel/libtecla/buildlink3.mk
+++ devel/libtecla/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/01/07 13:04:07 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2023/12/17 13:59:38 nros Exp $
 
 BUILDLINK_TREE+=	libtecla
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libtecla
 LIBTECLA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libtecla+=	libtecla>=1.5.0
-BUILDLINK_ABI_DEPENDS.libtecla+=	libtecla>=1.6.1nb1
+BUILDLINK_ABI_DEPENDS.libtecla+=	libtecla>=1.6.3
 BUILDLINK_PKGSRCDIR.libtecla?=		../../devel/libtecla
 .endif # LIBTECLA_BUILDLINK3_MK
 
