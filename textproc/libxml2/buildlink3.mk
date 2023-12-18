8580f6d235775a90fcaeda605cce783508dd4a86
wiz <wiz@pkgsrc.org>
2023-12-18 16:09:16 +0000

    libxml2: revert previous

--- textproc/libxml2/buildlink3.mk
+++ textproc/libxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2023/12/18 16:08:32 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2023/12/18 16:09:16 wiz Exp $
 
 BUILDLINK_TREE+=	libxml2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libxml2
 LIBXML2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxml2+=	libxml2>=2.6.2
-BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.10.4nb6
+BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.10.4nb3
 BUILDLINK_PKGSRCDIR.libxml2?=	../../textproc/libxml2
 
 BUILDLINK_FILES.libxml2+=	bin/xml2-config
