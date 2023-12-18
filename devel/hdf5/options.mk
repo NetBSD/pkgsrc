7a9957d5960118f58b5c3d8f65c00695dd5a097b
thor <thor@pkgsrc.org>
2023-12-18 10:33:28 +0000

    devel/hdf5: adding unsafe-threads option
    This is an option certain scientific users request for building their code.
    They explicitly need the unsupported configuration, that somehow seems to
    work for them.

--- devel/hdf5/options.mk
+++ devel/hdf5/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.10 2022/07/22 14:50:32 thor Exp $
+# $NetBSD: options.mk,v 1.11 2023/12/18 10:33:28 thor Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.hdf5
-PKG_SUPPORTED_OPTIONS=	szip threads
+PKG_SUPPORTED_OPTIONS=	szip threads unsafe-threads
 PKG_SUGGESTED_OPTIONS=	szip
 
 .include "../../mk/bsd.options.mk"
@@ -18,4 +18,11 @@ CONFIGURE_ARGS+=	--enable-threadsafe --disable-hl
 .include "../../mk/pthread.buildlink3.mk"
 .else
 PLIST.hl=	yes
+.endif
+
+# You can still enable threads if you are adventurous. There
+# are scientists like that. Not supported, but possible.
+.if !empty(PKG_OPTIONS:Munsafe-threads)
+CONFIGURE_ARGS+=       --enable-threadsafe --enable-unsupported
+.include "../../mk/pthread.buildlink3.mk"
 .endif
