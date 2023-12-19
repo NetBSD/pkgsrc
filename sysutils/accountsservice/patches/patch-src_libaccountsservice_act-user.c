e76d0d314b5fab23c5566a1e1397259ecc791270
gutteridge <gutteridge@pkgsrc.org>
2023-12-19 00:49:52 +0000

    accountsservice: minor patch cleanup
    Improve library inclusion and explicit linking. Also fix some code
    formatting, plus spelling and grammar tweaks. (We still don't
    correctly include util.h, because the upstream project uses its
    own header file by that name, which is woven all over the place.
    That should be fixed later.) Noted while reviewing the NetBSD support
    patch set.
    
    The (now rather dated) 0.6.42 release is (co-incidentally?) the last
    version before various function signature changes that assume a glibc-
    specific structure exists that's passed around. This of course doesn't
    work on NetBSD. There's more than one way to fix this: OpenBSD added a
    whole bunch more #ifdefs; a more elegant method would be to abstract
    these expectations. The upstream project has recently landed a patch
    set that sort of does the latter (simply copies the glibc struct if it
    isn't found), but that hasn't made it into a release yet.

--- sysutils/accountsservice/patches/patch-src_libaccountsservice_act-user.c
+++ sysutils/accountsservice/patches/patch-src_libaccountsservice_act-user.c
@@ -1,18 +1,27 @@
-$NetBSD: patch-src_libaccountsservice_act-user.c,v 1.1 2018/06/11 23:55:50 youri Exp $
+$NetBSD: patch-src_libaccountsservice_act-user.c,v 1.2 2023/12/19 00:49:52 gutteridge Exp $
 
 Add NetBSD backend.
 
---- src/libaccountsservice/act-user.c.orig	2018-05-25 23:09:35.000000000 +0000
+--- src/libaccountsservice/act-user.c.orig	2016-01-11 15:42:43.000000000 +0000
 +++ src/libaccountsservice/act-user.c
-@@ -25,6 +25,7 @@
- #include <sys/types.h>
+@@ -20,9 +20,15 @@
+ 
+ #include <config.h>
+ 
++#include <sys/types.h>
++#ifdef __NetBSD__
++#include <err.h>
++#include <errno.h>
++#include <pwd.h>
++/* #include <util.h> XXX should be included, but this project has its own util.h! */
++#endif
+ #include <float.h>
+ #include <string.h>
+-#include <sys/types.h>
  #include <sys/stat.h>
  #include <unistd.h>
-+#include <errno.h>
  
- #include <glib.h>
- #include <glib/gi18n.h>
-@@ -1818,8 +1819,29 @@ salt_char (GRand *rand)
+@@ -1818,8 +1824,29 @@ salt_char (GRand *rand)
  }
  
  static gchar *
@@ -22,9 +31,9 @@ Add NetBSD backend.
 +#ifdef __NetBSD__
 +        char salt[_PASSWORD_LEN+1];
 +        char option[LINE_MAX], *key, *opt;
-+	struct passwd *pw;
++        struct passwd *pw;
 +
-+	if (!(pw = getpwnam(username))) {
++        if (!(pw = getpwnam(username))) {
 +                errx(EXIT_FAILURE, "can't get pw for %s", username);
 +        }
 +
@@ -43,7 +52,7 @@ Add NetBSD backend.
          GString *salt;
          gchar *result;
          GRand *rand;
-@@ -1830,6 +1852,7 @@ make_crypted (const gchar *plain)
+@@ -1830,6 +1857,7 @@ make_crypted (const gchar *plain)
  
          /* SHA 256 */
          g_string_append (salt, "$6$");
@@ -51,7 +60,7 @@ Add NetBSD backend.
          for (i = 0; i < 16; i++) {
                  g_string_append_c (salt, salt_char (rand));
          }
-@@ -1839,6 +1862,7 @@ make_crypted (const gchar *plain)
+@@ -1839,6 +1867,7 @@ make_crypted (const gchar *plain)
  
          g_string_free (salt, TRUE);
          g_rand_free (rand);
@@ -59,7 +68,7 @@ Add NetBSD backend.
  
          return result;
  }
-@@ -1866,7 +1890,7 @@ act_user_set_password (ActUser          
+@@ -1866,7 +1895,7 @@ act_user_set_password (ActUser          
          g_return_if_fail (password != NULL);
          g_return_if_fail (ACCOUNTS_IS_USER (user->accounts_proxy));
  
