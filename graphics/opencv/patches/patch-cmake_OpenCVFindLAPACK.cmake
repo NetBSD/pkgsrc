a7b0d4756f33840622e9ec49f667eb106554d3c6
thor <thor@pkgsrc.org>
2023-12-22 15:35:44 +0000

    graphics/opencv: avoid name-clash of complex macro for gcc<4.8
    This adds #undef complex explicitly, to fix the build for older gccs that
    don't have that in their headers for C++ code.

--- graphics/opencv/patches/patch-cmake_OpenCVFindLAPACK.cmake
+++ graphics/opencv/patches/patch-cmake_OpenCVFindLAPACK.cmake
@@ -1,8 +1,18 @@
-$NetBSD: patch-cmake_OpenCVFindLAPACK.cmake,v 1.1 2023/12/22 07:35:07 thor Exp $
+$NetBSD: patch-cmake_OpenCVFindLAPACK.cmake,v 1.2 2023/12/22 15:35:44 thor Exp $
 
 --- cmake/OpenCVFindLAPACK.cmake.orig	2021-12-24 16:39:15.000000000 +0000
 +++ cmake/OpenCVFindLAPACK.cmake
-@@ -84,7 +84,7 @@ macro(ocv_lapack_check)
+@@ -52,6 +52,9 @@ macro(ocv_lapack_check)
+       list(APPEND _lapack_content "#include \"${OPENCV_LAPACKE_H_PATH_${_lapack_impl}}\"")
+     endif()
+     list(APPEND _lapack_content "
++/* avoid name clashes with older gcc, alias to _Complex breaking std::complex */
++#undef complex
++
+ #if defined(LAPACK_GLOBAL) || defined(LAPACK_NAME)
+ /*
+  * Using netlib's reference LAPACK implementation version >= 3.4.0 (first with C interface).
+@@ -84,7 +87,7 @@ macro(ocv_lapack_check)
          OUTPUT_VARIABLE TRY_OUT
      )
      if(NOT __VALID_LAPACK)
@@ -11,7 +21,7 @@ $NetBSD: patch-cmake_OpenCVFindLAPACK.cmake,v 1.1 2023/12/22 07:35:07 thor Exp $
        message(STATUS "LAPACK(${LAPACK_IMPL}): Can't build LAPACK check code. This LAPACK version is not supported.")
        unset(LAPACK_LIBRARIES)
      else()
-@@ -98,6 +98,22 @@ endmacro()
+@@ -98,6 +101,22 @@ endmacro()
  if(WITH_LAPACK)
    ocv_update(LAPACK_IMPL "Unknown")
    if(NOT OPENCV_LAPACK_FIND_PACKAGE_ONLY)
