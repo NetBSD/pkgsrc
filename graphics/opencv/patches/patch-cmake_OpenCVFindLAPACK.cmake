5c981da33b266e3afd1313935adc3f6694881838
thor <thor@pkgsrc.org>
2023-12-22 07:35:07 +0000

    opencv: version 3.4.17 with proper explicit BLAS (CBLAS + LAPACKE) usage
    This does the small bugfix update from 3.4.16 to 3.4.17 and adds proper usage
    of BLAS stuff. There was linkage to BLAS before via numpy, but the internal
    explicit BLAS-using code was not built, as the CMake machinery did not find it.
    
    This commit drops an actually counterproductive patch and adds pkg-config
    calls to find the BLAS-related libraries. Note that vor opencv-contrib-face,
    the BLAS choice during its build doesn't really enter the result, apparently,
    but the build process does use it and it would not help to confuse matters there.
    
    I am not sure if the buildlink3.mk should also add blas.buildlink3.mk now. It
    does not feature numpy right now. Should it?
    
    Next step should be move towards 4.x at least.
    
    Upstream:
    
    December, 2021
    
    OpenCV 3.4.17 has been released. Bug fixes, optimizations and other enhancements
    are propagated into OpenCV 4.5.5.
    
    Long-lived OpenCV 3.x release series is here since 2015. We are going to reduce
    support of 3.x branch in the future to move forward to OpenCV 5.0.

---
+++ graphics/opencv/patches/patch-cmake_OpenCVFindLAPACK.cmake
@@ -0,0 +1,36 @@
+$NetBSD: patch-cmake_OpenCVFindLAPACK.cmake,v 1.1 2023/12/22 07:35:07 thor Exp $
+
+--- cmake/OpenCVFindLAPACK.cmake.orig	2021-12-24 16:39:15.000000000 +0000
++++ cmake/OpenCVFindLAPACK.cmake
+@@ -84,7 +84,7 @@ macro(ocv_lapack_check)
+         OUTPUT_VARIABLE TRY_OUT
+     )
+     if(NOT __VALID_LAPACK)
+-      #message(FATAL_ERROR "LAPACK: check build log:\n${TRY_OUT}")
++      message(FATAL_ERROR "LAPACK: check build log:\n${TRY_OUT}")
+       message(STATUS "LAPACK(${LAPACK_IMPL}): Can't build LAPACK check code. This LAPACK version is not supported.")
+       unset(LAPACK_LIBRARIES)
+     else()
+@@ -98,6 +98,22 @@ endmacro()
+ if(WITH_LAPACK)
+   ocv_update(LAPACK_IMPL "Unknown")
+   if(NOT OPENCV_LAPACK_FIND_PACKAGE_ONLY)
++    # Mimicking cmake's FindBLAS with BLA_PREFER_PKGCONFIG
++    if(NOT LAPACK_LIBRARIES AND BLA_PREFER_PKGCONFIG)
++       find_package(PkgConfig)
++       pkg_search_module(PKG_BLAS ${BLA_PKGCONFIG_BLAS} blas)
++       pkg_search_module(PKG_LAPACK ${BLA_PKGCONFIG_LAPACK} lapack)
++       pkg_search_module(PKG_CBLAS ${BLA_PKGCONFIG_CBLAS} cblas)
++       pkg_search_module(PKG_LAPACKE ${BLA_PKGCONFIG_LAPACKE} lapacke)
++       if(PKG_BLAS_FOUND AND PKG_LAPACK_FOUND AND PKG_CBLAS_FOUND AND PKG_LAPACKE_FOUND)
++          set(LAPACK_INCLUDE_DIR ${PKG_CBLAS_INCLUDE_DIRS} ${PKG_LAPACKE_INCLUDE_DIRS} ${PKG_BLAS_INCLUDE_DIRS} ${PKG_LAPACK_INCLUDE_DIRS})
++          set(LAPACK_LIBRARIES ${PKG_CBLAS_LIBRARIES} ${PKG_LAPACKE_LIBRARIES} ${PKG_BLAS_LIBRARIES} ${PKG_LAPACK_LIBRARIES})
++          set(LAPACK_CBLAS_H   "cblas.h")
++          set(LAPACK_LAPACKE_H "lapacke.h")
++          set(LAPACK_IMPL      "pkg")
++          ocv_lapack_check()
++       endif()
++    endif()
+     if(NOT LAPACK_LIBRARIES AND NOT OPENCV_LAPACK_DISABLE_MKL)
+       include(cmake/OpenCVFindMKL.cmake)
+       if(HAVE_MKL)
