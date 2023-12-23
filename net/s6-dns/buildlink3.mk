28b95ddef08195a766eee47a01542ffaff6b18cd
schmonz <schmonz@pkgsrc.org>
2023-12-23 20:10:49 +0000

    s6-dns: update to 2.3.7.1. Changes:
    - Bugfixes.

--- net/s6-dns/buildlink3.mk
+++ net/s6-dns/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/11/23 18:51:33 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2023/12/23 20:10:49 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-dns
 
 .if !defined(S6_DNS_BUILDLINK3_MK)
 S6_DNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.7.0
+BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.7.1
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../net/s6-dns
 BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
 BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
