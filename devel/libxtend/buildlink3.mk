ed5a3d84f84450113fad6230200848499cc318c9
bacon <bacon@pkgsrc.org>
2023-12-28 15:02:17 +0000

    devel/libxtend: Update to 0.2.0.2
    Fix outdated function list man page
    No changes to library

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/10/28 20:28:20 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2023/12/28 15:02:17 bacon Exp $
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0.2
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
