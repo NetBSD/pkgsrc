47a5b49bde2ebce416ee0df2721ee1643faccd98
adam <adam@pkgsrc.org>
2023-12-29 09:16:25 +0000

    boost: updated to 1.84.0
    https://www.boost.org/users/history/version_1_84_0.html

--- devel/boost-libs/buildlink3.mk
+++ devel/boost-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.63 2023/08/22 20:16:10 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.64 2023/12/29 09:16:26 adam Exp $
 
 BUILDLINK_TREE+=	boost-libs
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	boost-libs
 BOOST_LIBS_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.83.*
-BUILDLINK_ABI_DEPENDS.boost-libs?=	boost-libs>=1.83.0
+BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.84.*
+BUILDLINK_ABI_DEPENDS.boost-libs?=	boost-libs>=1.84.0
 BUILDLINK_PKGSRCDIR.boost-libs?=	../../devel/boost-libs
 
 .include "../../mk/bsd.fast.prefs.mk"
