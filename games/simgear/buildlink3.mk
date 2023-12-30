e3daf00f77a16d83e985396a3d389779d84c9570
wiz <wiz@pkgsrc.org>
2023-12-30 12:57:50 +0000

    *: switch from poppler-glib to poppler
    Bump PKGREVISION for all poppler users to be on the safe side

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.91 2023/12/29 18:24:45 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.92 2023/12/30 12:57:51 wiz Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2nb7
-BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.11nb31
+BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2020.3.11nb32
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
