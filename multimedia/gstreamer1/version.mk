234d3e9890e0c031cf711c9ee852edf0370dcbc3
wiz <wiz@pkgsrc.org>
2023-12-31 22:03:00 +0000

    gstreamer1: update to 1.22.8
     Highlighted bugfixes:
    
        Security fixes for the AV1 codec parser
        [Security fixes](https://gstreamer.freedesktop.org/security/) for the AV1 video codec parser
        avdec video decoder: fix another possible deadlock with FFmpeg 6.1
        qtdemux: reverse playback and seeking fixes for files with raw audio streams
        v4l2: fix "newly allocated buffer ... is not free" warning log flood
        GstPlay + GstPlayer library fixes
        dtls: Fix build failure on Windows when compiling against OpenSSL 3.2.0
        d3d11screencapturesrc: Fix wrong color with HDR enabled
        Cerbero build tool: More python 3.12 string escape warning fixes; make sure to bundle build tools as well
        various bug fixes, build fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.8 2023/11/16 09:31:35 wiz Exp $
+# $NetBSD: version.mk,v 1.9 2023/12/31 22:03:01 wiz Exp $
 
-GST_VERSION=	1.22.7
+GST_VERSION=	1.22.8
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 PLIST_SUBST+=	SHLIBVER=${SHLIBVER}
 
