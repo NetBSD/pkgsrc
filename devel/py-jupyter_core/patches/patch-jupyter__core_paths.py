f7b84bf7eafa16bc41b392135a7aa6e4a07a7a81
adam <adam@pkgsrc.org>
2023-12-31 09:01:20 +0000

    py-jupyter_core: updated to 5.5.1
    5.5.1
    
    Bugs fixed
    
    - Account for read only file systems in migration
    
    Maintenance and upkeep improvements
    
    - Update ruff and typings
    - chore: update pre-commit hooks
    - Clean up lint and add downstream checks

--- devel/py-jupyter_core/patches/patch-jupyter__core_paths.py
+++ devel/py-jupyter_core/patches/patch-jupyter__core_paths.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-jupyter__core_paths.py,v 1.3 2022/11/22 10:18:06 adam Exp $
+$NetBSD: patch-jupyter__core_paths.py,v 1.4 2023/12/31 09:01:21 adam Exp $
 
 Enable Jupyter to discover kernels installed by pkgsrc packages.
 
 --- jupyter_core/paths.py.orig	2020-02-02 00:00:00.000000000 +0000
 +++ jupyter_core/paths.py
-@@ -188,6 +188,7 @@ else:
-             SYSTEM_JUPYTER_PATH = [os.path.join(sys.prefix, "share", "jupyter")]
+@@ -222,6 +222,7 @@ else:
+             SYSTEM_JUPYTER_PATH = [str(Path(sys.prefix, "share", "jupyter"))]
      else:
          SYSTEM_JUPYTER_PATH = [
 +            "@PREFIX@/share/jupyter",
