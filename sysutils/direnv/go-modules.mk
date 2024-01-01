faa435a6495d305c94c60e28303febd6f29ee88f
leot <leot@pkgsrc.org>
2024-01-01 14:28:49 +0000

    direnv: Update to 2.33.0
    Changes:
    v2.33.0
    -------
    - Documentation update and improvements
    - Add PowerShell Support
    - Minor improvements and bug fixes

--- sysutils/direnv/go-modules.mk
+++ sysutils/direnv/go-modules.mk
@@ -1,28 +1,10 @@
-# $NetBSD: go-modules.mk,v 1.4 2023/07/05 19:39:40 schmonz Exp $
+# $NetBSD: go-modules.mk,v 1.5 2024/01/01 14:28:49 leot Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.0.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.0.zip
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.mod
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191011191535-87dc89f01550.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191011191535-87dc89f01550.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.5.1.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.5.1.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190620200207-3b0461eec859.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190620200207-3b0461eec859.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190423024810-112230192c58.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20190423024810-112230192c58.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210630005230-0f9fa26af87c.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220520151302-bc2c85ada10a.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220520151302-bc2c85ada10a.zip
-GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
-GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20191119224855-298f0cb1881e.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20191119224855-298f0cb1881e.zip
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20190717185122-a985d3407aa7.mod
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191011141410-1b5146add898.mod
-GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191011141410-1b5146add898.zip
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.zip
