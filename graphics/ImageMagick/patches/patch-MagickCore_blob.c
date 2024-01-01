a6099ea94dba21831e524c3607e500d0b088d0be
bsiegert <bsiegert@pkgsrc.org>
2024-01-01 10:29:55 +0000

    Pullup ticket #6828 - requested by schmonz graphics/ImageMagick: bugfix
    Revisions pulled up:
    - graphics/ImageMagick/Makefile                                 1.334
    - graphics/ImageMagick/distinfo                                 1.296
    - graphics/ImageMagick/patches/patch-MagickCore_blob.c          1.1
    - graphics/p5-PerlMagick/Makefile                               1.201
    
    ---
       Module Name:	pkgsrc
       Committed By:	schmonz
       Date:		Sat Dec 30 14:24:29 UTC 2023
    
       Modified Files:
       	pkgsrc/graphics/ImageMagick: Makefile distinfo
       	pkgsrc/graphics/p5-PerlMagick: Makefile
       Added Files:
       	pkgsrc/graphics/ImageMagick/patches: patch-MagickCore_blob.c
    
       Log Message:
       {Image,p5-Perl}Magick: apply upstream fix for NetBSD test regressions.

---
+++ graphics/ImageMagick/patches/patch-MagickCore_blob.c
@@ -0,0 +1,25 @@
+$NetBSD: patch-MagickCore_blob.c,v 1.2.2.2 2024/01/01 10:29:56 bsiegert Exp $
+
+Apply upstream commit 30274e5 to fix test failures on NetBSD.
+
+--- MagickCore/blob.c.orig	2023-12-10 20:15:36.000000000 +0000
++++ MagickCore/blob.c
+@@ -618,8 +618,7 @@ MagickExport MagickBooleanType CloseBlob
+   blob_info=image->blob;
+   if ((blob_info == (BlobInfo *) NULL) || (blob_info->type == UndefinedStream))
+     return(MagickTrue);
+-  if (SyncBlob(image) != 0)
+-    ThrowBlobException(blob_info);
++  (void) SyncBlob(image);
+   status=blob_info->status;
+   switch (blob_info->type)
+   {
+@@ -5518,6 +5517,8 @@ static int SyncBlob(const Image *image)
+   assert(image->blob != (BlobInfo *) NULL);
+   if (IsEventLogging() != MagickFalse)
+     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",image->filename);
++  if (EOFBlob(image) != 0)
++    return(0);
+   blob_info=image->blob;
+   status=0;
+   switch (blob_info->type)
