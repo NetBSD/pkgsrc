78243be09a16443356c84dc268e559cf26664ef5
pin <pin@pkgsrc.org>
2024-01-02 16:02:44 +0000

    textproc/tuc: update to 1.2.0
    [1.2.0] - 2024-01-01
     - feat: new option --json to format output as JSON array
     - feat: -r can be used when cutting --characters. It replaces the (empty)
       delimiter between characters with whatever you provided
     - feat: exit early when some combinations of fields cannot be used together
     - feat: updated dependencies. In particular the regex crate which offers new
       functionalities (word boundary assertions)
     - fix: field formatting is now applied to field 1 even if no delimiters are
       found (similar to how we print the unformatted field 1)
     - doc: many error messages have been rewritten for better clarity
     - doc: new svg demo in the README

--- textproc/tuc/cargo-depends.mk
+++ textproc/tuc/cargo-depends.mk
@@ -1,7 +1,8 @@
-# $NetBSD: cargo-depends.mk,v 1.7 2023/12/02 22:12:46 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.8 2024/01/02 16:02:44 pin Exp $
 
+CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.2
 CARGO_CRATE_DEPENDS+=	anstyle-1.0.0
-CARGO_CRATE_DEPENDS+=	anyhow-1.0.75
+CARGO_CRATE_DEPENDS+=	anyhow-1.0.78
 CARGO_CRATE_DEPENDS+=	assert_cmd-2.0.12
 CARGO_CRATE_DEPENDS+=	bstr-1.1.0
 CARGO_CRATE_DEPENDS+=	difflib-0.4.0
@@ -15,9 +16,10 @@ CARGO_CRATE_DEPENDS+=	pico-args-0.5.0
 CARGO_CRATE_DEPENDS+=	predicates-3.0.4
 CARGO_CRATE_DEPENDS+=	predicates-core-1.0.6
 CARGO_CRATE_DEPENDS+=	predicates-tree-1.0.2
-CARGO_CRATE_DEPENDS+=	regex-1.8.4
+CARGO_CRATE_DEPENDS+=	regex-1.10.2
 CARGO_CRATE_DEPENDS+=	regex-automata-0.1.10
-CARGO_CRATE_DEPENDS+=	regex-syntax-0.7.2
+CARGO_CRATE_DEPENDS+=	regex-automata-0.4.3
+CARGO_CRATE_DEPENDS+=	regex-syntax-0.8.2
 CARGO_CRATE_DEPENDS+=	serde-1.0.152
 CARGO_CRATE_DEPENDS+=	treeline-0.1.0
 CARGO_CRATE_DEPENDS+=	wait-timeout-0.2.0
