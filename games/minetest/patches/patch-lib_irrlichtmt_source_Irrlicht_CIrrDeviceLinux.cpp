be8ccad3184b4413aaf63fac8f621cce50ad8e38
gdt <gdt@pkgsrc.org>
2024-01-02 14:18:07 +0000

    games/minetest: update minetest to 5.8.0
    Update prepared in wip by Kevin Bloom
    
    Added buildlink3 for textproc/jsoncpp to avoid used of vendored copy.
    Also prevents build error when building on a system that has jsoncpp
    installed.
    
    Deprecations and compatibility notes
    - Minetest Game is no longer the default game and will no longer be
      shipped by Minetest. If you want it back, install it by using the
      "Content" tab
    - C++17 support is now required
    
    Client / Audiovisuals
    - Redesign main menu and unify settings interface
    - better main menu prompt to install a game when none is installed
    - various main menu fixes
    - Load package list asynchronously
    - Option to invert direction or disable mouse wheel for hotbar item
      selection
    - Inventory mouse shortcut improvements
    - Holding down Sneak+click while moving the mouse over item slots now
      continously moves items to other inventory (if available)
    - Press Sneak+click on the crafting output slot to craft and move
      result to inventory
    - While crafting:
      - Left mouse button: Craft as many as possible
      - Mouse wheel: Craft 10 times
      - Right mouse button: Craft once
      - Drag an item stack on the crafting grid to split stacks evenly
      - Hold down Left mouse button while holding an item stack and move
        the cursor over the slots to pick up items of the same type
    - Implement check_offset for decorations
    - Touchscreen input improvements
    - Rendering-related performance improvements and fixes
    - Add antialiasing filters
    - Reverse eye-offset Z-coordinate in 3rd person front view
    - DPI-aware crosshair
    - Prevent early respawns caused by up/down button in the death screen
    - Sounds and animations are now paused in pause menu in singleplayer
    - X11: Add primary selection (copy & paste via select & middleclick)
      support
    
    World / Server / Environment
    - Major speedup for crafting shapeless craft recipes
    - Fix crash on handling wallmounted nodes with invalid param2
     Fix biomes not repecting their Y limits
    - Especially thin biomes will now be generated as intended.
    - Saner (HTTP) timeout limits and log messages
    
    Script API / Modding
    - Add ability to override item images using ItemMetaData
    - Add node pos to node damage HP change reason
    - Add vector.in_area() utility function
    - Add focused styling to buttons
    - Add min/max protocol version to minetest.get_version()
    - Add additional texture modifiers
    - Add node group disable_descend to disable actively descending down
      climbable nodes and nodes with liquid move physics
    - Add VoxelArea::intersect()
    - Allow nodes to have their post_effect_color affected by lighting
    - Fix potential freeze in core.check_for_falling
    - Send everlasting particle spawners to all players
    - Allow place_param2 = 0 node placement predictions
    - New player physics overrides for climb speed, sneak speed,
      acceleration, liquid fluidity and liquid sink speed
    - Allow to set custom third person front view camera offset
    - Add script to update/generate mod translations:
      util/mod_translation_updater.py
    - Add start_time to sound parameter tables
    
    Misc / Maintenance
    - Entity/Object fixes and unittests
    - Lua environment cleanups and improvements
    - Various documentation improvements
    - Inventory code fixes
    - Many various code fixes
    - Opt-out option for Doxygen generation on build
    - Sound code cleanups and improvements
      - Long sounds in sound packs, or sent via dynamic media, no longer
        cause client freezes on load
      - Positional sounds can be faded now
      - Documentation
      - Other improvements listed elsewhere
    - Faster client load times (#12764 and irr#233)

--- games/minetest/patches/patch-lib_irrlichtmt_source_Irrlicht_CIrrDeviceLinux.cpp
+++ games/minetest/patches/patch-lib_irrlichtmt_source_Irrlicht_CIrrDeviceLinux.cpp
@@ -1,19 +1,29 @@
-$NetBSD: patch-lib_irrlichtmt_source_Irrlicht_CIrrDeviceLinux.cpp,v 1.1 2023/08/18 10:51:52 pin Exp $
+$NetBSD: patch-lib_irrlichtmt_source_Irrlicht_CIrrDeviceLinux.cpp,v 1.2 2024/01/02 14:18:07 gdt Exp $
 
 Use FreeBSD's support for NetBSD
 
---- lib/irrlichtmt/source/Irrlicht/CIrrDeviceLinux.cpp.orig	2023-03-05 20:45:09.000000000 +0000
+--- lib/irrlichtmt/source/Irrlicht/CIrrDeviceLinux.cpp.orig	2023-10-27 15:48:29.000000000 +0000
 +++ lib/irrlichtmt/source/Irrlicht/CIrrDeviceLinux.cpp
-@@ -47,7 +47,7 @@
+@@ -28,6 +28,7 @@
+ #include "IFileSystem.h"
+ #include <X11/XKBlib.h>
+ #include <X11/Xatom.h>
++#include <unistd.h>
+ 
+ #if defined(_IRR_LINUX_X11_XINPUT2_)
+ #include <X11/extensions/XInput2.h>
+@@ -47,9 +48,8 @@
+ 
+ #if defined _IRR_COMPILE_WITH_JOYSTICK_EVENTS_
  #include <fcntl.h>
- #include <unistd.h>
+-#include <unistd.h>
  
 -#ifdef __FreeBSD__
 +#if defined(__NetBSD__) || defined(__FreeBSD__)
  #include <sys/joystick.h>
  #else
  
-@@ -1578,7 +1578,7 @@ bool CIrrDeviceLinux::activateJoysticks(
+@@ -1690,7 +1690,7 @@ bool CIrrDeviceLinux::activateJoysticks(
  		if (-1 == info.fd)
  			continue;
  
@@ -22,7 +32,7 @@ Use FreeBSD's support for NetBSD
  		info.axes=2;
  		info.buttons=2;
  #else
-@@ -1602,7 +1602,7 @@ bool CIrrDeviceLinux::activateJoysticks(
+@@ -1714,7 +1714,7 @@ bool CIrrDeviceLinux::activateJoysticks(
  		returnInfo.Axes = info.axes;
  		returnInfo.Buttons = info.buttons;
  
@@ -31,7 +41,7 @@ Use FreeBSD's support for NetBSD
  		char name[80];
  		ioctl( info.fd, JSIOCGNAME(80), name);
  		returnInfo.Name = name;
-@@ -1637,7 +1637,7 @@ void CIrrDeviceLinux::pollJoysticks()
+@@ -1749,7 +1749,7 @@ void CIrrDeviceLinux::pollJoysticks()
  	{
  		JoystickInfo & info =  ActiveJoysticks[j];
  
