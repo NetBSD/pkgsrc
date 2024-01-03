cf974fde119897578385b2d73fca03f8649f146f
nros <nros@pkgsrc.org>
2024-01-03 08:16:36 +0000

    bzip3: avoid redefinition of bswap16 on NetBSD

--- archivers/bzip3/patches/patch-include_common.h
+++ archivers/bzip3/patches/patch-include_common.h
@@ -1,10 +1,12 @@
-$NetBSD: patch-include_common.h,v 1.1 2023/12/16 11:40:53 nros Exp $
+$NetBSD: patch-include_common.h,v 1.2 2024/01/03 08:16:36 nros Exp $
 
 * _*_ENDIAN is defined to nothing on solaris-like systems leading to preprocessor errors
   when == try to compare to nothing. Use if _*_ENDIAN are defined as set in sys/isa_defs.h
   on solaris-like systems to check for endianess instead of _BYTE_ORDER.
 
---- include/common.h.orig	2023-12-16 12:17:00.391225325 +0000
+* Avoid redefinition of bswap16 on NetBSD
+
+--- include/common.h.orig	2023-08-06 08:22:20.000000000 +0000
 +++ include/common.h
 @@ -93,6 +93,7 @@ static void write_neutral_s32(u8 * data,
      #error Your compiler, configuration or platform is not supported.
@@ -14,7 +16,7 @@ $NetBSD: patch-include_common.h,v 1.1 2023/12/16 11:40:53 nros Exp $
  #if !defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)
      #if defined(_LITTLE_ENDIAN) || (defined(BYTE_ORDER) && defined(LITTLE_ENDIAN) && BYTE_ORDER == LITTLE_ENDIAN) || \
          (defined(_BYTE_ORDER) && defined(_LITTLE_ENDIAN) && _BYTE_ORDER == _LITTLE_ENDIAN) ||                        \
-@@ -108,6 +109,14 @@ static void write_neutral_s32(u8 * data,
+@@ -108,17 +109,25 @@ static void write_neutral_s32(u8 * data,
          #define __LITTLE_ENDIAN__
      #endif
  #endif
@@ -29,3 +31,18 @@ $NetBSD: patch-include_common.h,v 1.1 2023/12/16 11:40:53 nros Exp $
  
  #if defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)
      #if defined(HAS_BUILTIN_BSWAP16)
+-        #define bswap16(x) (__builtin_bswap16(x))
++        #define bzip3_beton16(x) (__builtin_bswap16(x))
+     #elif defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+-        #define bswap16(x) (_byteswap_ushort(x))
++        #define bzip3_beton16(x) (_byteswap_ushort(x))
+     #else
+-        #define bswap16(x) ((u16)(x >> 8) | (u16)(x << 8))
++        #define bzip3_beton16(x) ((u16)(x >> 8) | (u16)(x << 8))
+     #endif
+ #elif !defined(__LITTLE_ENDIAN__) && defined(__BIG_ENDIAN__)
+-    #define bswap16(x) (x)
++    #define bzip3_beton16(x) (x)
+ #else
+     #error Your compiler, configuration or platform is not supported.
+ #endif
