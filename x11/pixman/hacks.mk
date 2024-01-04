07b68788c8e2384170ff661309ba652c166a0d07
wiz <wiz@pkgsrc.org>
2024-01-04 11:33:38 +0000

    pixman: update to 0.43.0.
    	Benjamin Gilbert (3):
    	      meson: don't dllexport when built as static library
    	      mmx: use xmmintrin.h if building with SSE2
    	      meson: avoid linking with -pthread if we don't have pthreads
    
    	Claude Heiland-Allen (2):
    	      implement r8g8b8 sRGB (without alpha)
    	      add r8g8b8 sRGB to test suite
    
    	Dylan Baker (2):
    	      autotools: remove autotools
    	      delete win32 make files
    
    	Emanuel Schmidt (3):
    	      Separate meson build options for demos and tests
    	      Changed name of the config-header to "pixman-config.h"
    	      Fixed missing dependency in libdemo
    
    	Gauthier Östervall (1):
    	      Update build instructions to meson and ninja
    
    	Havard Eidnes (2):
    	      vmx: Simplify scaled_nearest_scanline_vmx_8888_8888_OVER
    	      vmx: Reimplement create_mask_32_128 and use it in vmx_fill
    
    	Heiko Lewin (2):
    	      Fix signed-unsigned semantics in reduce_32
    	      pixman-arma64: Adjustments to build with llvm integrated assembler
    
    	Matt Turner (9):
    	      Post-release version bump to 0.42.3
    	      test: Revert to including pixman-private.h
    	      Remove generic build system information
    	      Use more Markdown-friendly syntax
    	      meson: Fix syntax
    	      pixman-sse2: Remove unused functions
    	      pixman-mmx: Don't redefine _MM_SHUFFLE
    	      pixman-access: Mark __dummy__ variables with MAYBE_UNUSED
    	      test: Use fabsl on float128
    
    	Pavel Labath (1):
    	      Fix alignment problem in pixman-fast-path.c
    
    	Sam James (1):
    	      pixman-bits-image: fix -Walloc-size
    
    	Simon Ser (5):
    	      Add pixman_region{,32}_empty()
    	      Constify pixman_image_set_clip_region()
    	      Fix const warnings in pixman_image_set_clip_region()
    	      ci: upgrade to Fedora 39
    	      Pre-release version bump to 0.43.0

--- x11/pixman/hacks.mk
+++ x11/pixman/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.15 2023/09/29 09:27:07 adam Exp $
+# $NetBSD: hacks.mk,v 1.16 2024/01/04 11:33:38 wiz Exp $
 #
 .if !defined(PIXMAN_HACKS_MK)
 PIXMAN_HACKS_MK=	# empty
@@ -11,17 +11,13 @@ PIXMAN_HACKS_MK=	# empty
 .  if !${CC_VERSION:Mgcc-4.[2-9]*} && \
     !${CC_VERSION:Mgcc-[5-9].*} && \
     !${CC_VERSION:Mgcc-[1-9][0-9]*}
-CONFIGURE_ARGS+=	--disable-sse2 --disable-ssse3
+MESON_ARGS+=	-Dsse2=disabled
+MESON_ARGS+=	-Dssse3=disabled
 .  endif
 .endif
 
 .if !empty(PKGSRC_COMPILER:Msunpro)
-CONFIGURE_ARGS+=	--disable-sse2 --disable-mmx --disable-ssse3
-.endif
-
-CONFIGURE_ARGS.MirBSD+=	PTHREAD_LIBS=-pthread
-
-.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 060000
-CONFIGURE_ENV+=		ac_cv_tls=none
-CFLAGS+=		-DPIXMAN_NO_TLS
+MESON_ARGS+=	-Dmmx=disabled
+MESON_ARGS+=	-Dsse2=disabled
+MESON_ARGS+=	-Dssse3=disabled
 .endif
