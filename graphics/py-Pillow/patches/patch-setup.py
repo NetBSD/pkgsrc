3fc0bd9d2625024e9ded71c33f448f3e6dfb0064
adam <adam@pkgsrc.org>
2024-01-05 23:47:53 +0000

    py-Pillow: updated to 10.2.0
    10.2.0 (2024-01-02)
    
    - Add ``keep_rgb`` option when saving JPEG to prevent conversion of RGB colorspace
    - Trim glyph size in ImageFont.getmask()
    - Deprecate IptcImagePlugin helpers
    - Allow uncompressed TIFF images to be saved in chunks
    - Concatenate multiple JPEG EXIF markers
    - Changed IPTC tile tuple to match other plugins
    - Do not assign new fp attribute when exiting context manager
    - Support arbitrary masks for uncompressed RGB DDS images
    - Support setting ROWSPERSTRIP tag
    - Apply ImageFont.MAX_STRING_LENGTH to ImageFont.getmask()
    - Optimise ``ImageColor`` using ``functools.lru_cache``
    - Restricted environment keys for ImageMath.eval()
    - Optimise ``ImageMode.getmode`` using ``functools.lru_cache``
    - Fix incorrect color blending for overlapping glyphs
    - Attempt memory mapping when tile args is a string
    - Fill identical pixels with transparency in subsequent frames when saving GIF
    - Corrected duration when combining multiple GIF frames into single frame
    - Handle disposing GIF background from outside palette
    - Seek past the data when skipping a PSD layer
    - Import plugins relative to the module
    - Translate encoder error codes to strings; deprecate ``ImageFile.raise_oserror()``
    - Support reading BC4U and DX10 BC1 images
    - Optimize ImageStat.Stat.extrema
    - Handle pathlib.Path in FreeTypeFont
    - Added support for reading DX10 BC4 DDS images
    - Optimized ImageStat.Stat.count
    - Correct PDF palette size when saving
    - Fixed closing file pointer with olefile 0.47
    - Raise ValueError when TrueType font size is not greater than zero
    - If absent, do not try to close fp when closing image
    - Allow configuring JPEG restart marker interval on save
    - Decrement reference count for PyObject
    - Implement ``streamtype=1`` option for tables-only JPEG encoding
    - If save_all PNG only has one frame, do not create animated image
    - Fixed frombytes() for images with a zero dimension

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-setup.py,v 1.24 2022/08/01 14:51:15 wiz Exp $
+$NetBSD: patch-setup.py,v 1.25 2024/01/05 23:47:53 adam Exp $
 
 Fix finding zlib.h.
+Do not look for xcb.
 
---- setup.py.orig	2022-07-01 13:14:51.000000000 +0000
+--- setup.py.orig	2024-01-02 08:28:43.000000000 +0000
 +++ setup.py
-@@ -636,7 +636,9 @@ class pil_build_ext(build_ext):
+@@ -642,7 +642,9 @@ class pil_build_ext(build_ext):
  
          if feature.want("zlib"):
              _dbg("Looking for zlib")
@@ -15,3 +16,15 @@ Fix finding zlib.h.
                  if _find_library_file(self, "z"):
                      feature.zlib = "z"
                  elif sys.platform == "win32" and _find_library_file(self, "zlib"):
+@@ -800,11 +802,6 @@ class pil_build_ext(build_ext):
+                 ):
+                     feature.webpmux = "libwebpmux"
+ 
+-        if feature.want("xcb"):
+-            _dbg("Looking for xcb")
+-            if _find_include_file(self, "xcb/xcb.h"):
+-                if _find_library_file(self, "xcb"):
+-                    feature.xcb = "xcb"
+ 
+         for f in feature:
+             if not getattr(feature, f) and feature.require(f):
