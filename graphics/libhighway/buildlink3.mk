156167e64298402bcb589581189586615a79ff5a
nros <nros@pkgsrc.org>
2024-01-05 11:09:53 +0000

    Update libhighway to version 1.0.7 The reason for updating is that the only libhighway user (wip/libjxl) needs 1.0.7 to build its latests version 0.9.0. Due to that nothing in pkgsrc-current depends on libhighway and the only thing that needs it needs 1.0.7, bump BUILDLINK_API_DEPENDS to 1.0.7 .

--- graphics/libhighway/buildlink3.mk
+++ graphics/libhighway/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/08/16 15:20:49 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/01/05 11:09:53 nros Exp $
 
 BUILDLINK_TREE+=	libhighway
 
 .if !defined(LIBHIGHWAY_BUILDLINK3_MK)
 LIBHIGHWAY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libhighway+=	libhighway>=0.12.2
-BUILDLINK_ABI_DEPENDS.libhighway+=	libhighway>=1.0.1
+BUILDLINK_API_DEPENDS.libhighway+=	libhighway>=1.0.7
 BUILDLINK_PKGSRCDIR.libhighway?=	../../graphics/libhighway
 .endif	# LIBHIGHWAY_BUILDLINK3_MK
 
