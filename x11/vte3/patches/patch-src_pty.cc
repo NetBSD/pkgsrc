f1dc5b37da107c358c364247a64d77922e94de16
gutteridge <gutteridge@pkgsrc.org>
2024-01-07 02:14:16 +0000

    vte3: update to 0.74.2
    (No published change log found.)

--- x11/vte3/patches/patch-src_pty.cc
+++ x11/vte3/patches/patch-src_pty.cc
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_pty.cc,v 1.12 2023/10/30 23:02:02 gutteridge Exp $
+$NetBSD: patch-src_pty.cc,v 1.13 2024/01/07 02:14:16 gutteridge Exp $
 
 Use correct includes on SunOS.
 
---- src/pty.cc.orig	2023-04-13 09:57:27.000000000 +0000
+--- src/pty.cc.orig	2024-01-06 16:00:59.800608016 +0000
 +++ src/pty.cc
 @@ -57,7 +57,9 @@
- #ifdef HAVE_PTY_H
+ #if __has_include(<pty.h>)
  #include <pty.h>
  #endif
--#if defined(__sun) && defined(HAVE_STROPTS_H)
+-#if defined(__sun) && __has_include(<stropts.h>)
 +#if defined(__sun)
 +#include <sys/ioccom.h>
 +#include <sys/ptyvar.h>
  #include <stropts.h>
+ #define HAVE_STROPTS_H
  #endif
- #ifdef __NetBSD__
