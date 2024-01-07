2a9b65988204116b1b53870138418b1d32b1fdca
bsiegert <bsiegert@pkgsrc.org>
2024-01-07 17:26:47 +0000

    go-staticcheck: update to 2023.1.6
    2023.1.4
    
    This release adds support for Go 1.21 and fixes the following bugs:
    
    - Three crashes when encountering unnecessarily parenthesized statements
    - Unnecessarily high memory usage when analyzing composite literals such as
      []int{1<<31: 1}
    - A false positive in S1011 when appending to a dynamic left-hand side
    - A crash involving generics
    - A false positive in SA9001 involving control flow statements
    - A false positive in ST1003, complaining about the names of fuzz functions
    
    2023.1.5
    
    This release fixes the following bug:
    
    - A crash involving methods named _
    
    2023.1.6
    
    This release fixes the following bugs:
    
    - A crash when using the upcoming Go 1.22
    - A false positive in SA9005 when embedding basic types

--- devel/go-staticcheck/go-modules.mk
+++ devel/go-staticcheck/go-modules.mk
@@ -1,12 +1,12 @@
-# $NetBSD: go-modules.mk,v 1.3 2023/07/09 19:10:21 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.4 2024/01/07 17:26:47 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.mod
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.zip
 GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20221208152030-732eee02a75a.mod
 GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20221208152030-732eee02a75a.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.7.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.7.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.4.1-0.20221208213631-3f74d914ae6d.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.4.1-0.20221208213631-3f74d914ae6d.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.12.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.12.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.12.1-0.20230825192346-2191a27a6dc5.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.12.1-0.20230825192346-2191a27a6dc5.zip
