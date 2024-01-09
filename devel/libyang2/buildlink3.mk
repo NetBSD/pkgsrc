dbbea47a3c7df367e457fd81624a8c8288c8cfa5
chopps <chopps@pkgsrc.org>
2024-01-09 15:53:59 +0000

    devel/libyang2: update to version 2.1.128
    In addition add support for a run the test suite when testing is enabled.

--- devel/libyang2/buildlink3.mk
+++ devel/libyang2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/08/11 09:53:36 kardel Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/01/09 15:53:59 chopps Exp $
 
 BUILDLINK_TREE+=	libyang2
 
 .if !defined(LIBYANG2_BUILDLINK3_MK)
 LIBYANG2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libyang2+=	libyang2>=2.0.7
+BUILDLINK_API_DEPENDS.libyang2+=	libyang2>=2.1.128
 BUILDLINK_PKGSRCDIR.libyang2?=		../../devel/libyang2
 
 TOOL_DEPENDS+=			cmocka>=1.1.5:../../devel/cmocka
