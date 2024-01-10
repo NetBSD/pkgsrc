595c14845d2b1f6404acc59046720b31cc480197
tnn <tnn@pkgsrc.org>
2024-01-10 08:20:10 +0000

    jimctl: needs zlib

--- lang/jimtcl/buildlink3.mk
+++ lang/jimtcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/05/06 15:19:39 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/01/10 08:20:10 tnn Exp $
 
 BUILDLINK_TREE+=	jimtcl
 
@@ -7,6 +7,8 @@ JIMTCL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jimtcl+=	jimtcl>=0.80nb1
 BUILDLINK_PKGSRCDIR.jimtcl?=	../../lang/jimtcl
+
+.include "../../devel/zlib/buildlink3.mk"
 .endif	# JIMTCL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-jimtcl
