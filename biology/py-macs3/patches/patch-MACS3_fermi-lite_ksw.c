836ebd754e6d299606ed080ae9283a79583c36bf
bacon <bacon@pkgsrc.org>
2024-01-10 15:17:33 +0000

    biology/py-macs3: Peak caller for TF binding sites
    MACS (Model-based Analysis of ChIP-Seq) is a tool for identifying
    transcription factor (TF) binding sites.   Such sites are generated
    by CHiP-Seq (CHromatin immuno-Precipitation sequencing) and ATAC-Seq
    (Assay for Transposase Accessible Chromatin Sequencing).  MACS
    identifies "peaks" in the genome sequence, which are areas enriched
    in bound TFs or accessible chromatin.

---
+++ biology/py-macs3/patches/patch-MACS3_fermi-lite_ksw.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-MACS3_fermi-lite_ksw.c,v 1.1 2024/01/10 15:17:33 bacon Exp $
+
+# Use SIMDE sse2.h
+
+--- MACS3/fermi-lite/ksw.c.orig	2023-12-21 22:20:16.716124047 +0000
++++ MACS3/fermi-lite/ksw.c
+@@ -28,7 +28,7 @@
+ 
+ /* I modified this according to https://github.com/lh3/bwa/pull/283/*/
+ #ifdef USE_SIMDE
+-#include "lib/x86/sse2.h"
++#include <simde/x86/sse2.h>
+ #else
+ #include <emmintrin.h>
+ #endif
