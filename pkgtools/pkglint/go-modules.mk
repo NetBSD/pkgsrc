e9dfafe7415bc5f9c6fe4eef8f528599177e0a2b
rillig <rillig@pkgsrc.org>
2024-01-12 23:37:54 +0000

    pkgtools/pkglint: update to 23.3.0
    Changes since 23.2.5:
    
    Do not warn about Meson packages that only use Python as a build-time
    tool.
    
    Warn about pkgsrc-wip packages that are missing COMMIT_MSG.
    
    Ignore errors when resolving the username of the current user. This can
    happen on systems where the user database uses NIS. In case of an error,
    assume that the user is neither maintainer nor owner of any package.
    
    Mark variables from expressions in .info, .warning and .error as used.
    
    Remove the word 'please' from the diagnostics, as the diagnostics are
    intended to help the package author, not some unrelated third-party
    person.

--- pkgtools/pkglint/go-modules.mk
+++ pkgtools/pkglint/go-modules.mk
@@ -1,13 +1,13 @@
-# $NetBSD: go-modules.mk,v 1.2 2023/04/22 14:32:48 rillig Exp $
+# $NetBSD: go-modules.mk,v 1.3 2024/01/12 23:37:54 rillig Exp $
 
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.mod
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.zip
 GO_MODULE_FILES+=	github.com/kr/pty/@v/v1.1.1.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.6.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.6.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.17.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.17.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.15.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.15.0.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.mod
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.zip
