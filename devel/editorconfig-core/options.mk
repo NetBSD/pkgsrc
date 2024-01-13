e34699e6fac4fbe6b171d309fc2efdaff6ab638d
nros <nros@pkgsrc.org>
2024-01-13 14:32:36 +0000

    Update editorconfig-core to version 0.12.6
    Pkgsrc changes:
    Use cmake/build.mk instead of USE_CMAKE.
    
    Changes from changelog:
    Fix memory leak in editorconfig_parse() that would
    occur if no .editorconfig files were found in the
    file's ancestry if an error occured while ingesting
    value.
    Fix potential buffer overflow in ec_glob.

--- devel/editorconfig-core/options.mk
+++ devel/editorconfig-core/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2023/06/06 12:40:37 riastradh Exp $
+# $NetBSD: options.mk,v 1.5 2024/01/13 14:32:36 nros Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.editorconfig-core
 PKG_SUPPORTED_OPTIONS=	doc
 
@@ -7,12 +7,12 @@ PKG_SUPPORTED_OPTIONS=	doc
 ###
 ### Build and install html documentation and man pages
 ###
-PLIST_VARS+=	doc
+PLIST_VARS+=		doc
 .if !empty(PKG_OPTIONS:Mdoc)
-TOOL_DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
-CMAKE_ARGS+=	-DBUILD_DOCUMENTATION=ON
-CMAKE_ARGS+=	-DINSTALL_HTML_DOC=ON
-PLIST.doc=	yes
+TOOL_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
+CMAKE_CONFIGURE_ARGS+=	-DBUILD_DOCUMENTATION=ON
+CMAKE_CONFIGURE_ARGS+=	-DINSTALL_HTML_DOC=ON
+PLIST.doc=		yes
 .else
-CMAKE_ARGS+=	-DBUILD_DOCUMENTATION=OFF
+CMAKE_CONFIGURE_ARGS+=	-DBUILD_DOCUMENTATION=OFF
 .endif
