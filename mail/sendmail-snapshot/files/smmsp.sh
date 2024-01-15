996b7a8edb79fb22f844208a33d75b96b4a4d839
jnemeth <jnemeth@pkgsrc.org>
2024-01-15 04:50:54 +0000

    mail/sendmail-snapshot: import sendmail-8.18.0.2
    Sendmail acts as a unified "post office" to which all mail can be
    submitted.  Address interpretation is controlled by a production
    system, which can parse both domain-based addressing and old-style
    "ad hoc" addresses.  The production system is powerful enough to
    rewrite addresses in the message header to conform to the standards
    of a number of common target networks, including old (NCP/RFC733)
    Arpanet, new (TCP/RFC822) Arpanet, UUCP, and Phonenet. Sendmail
    also implements an SMTP server, message queueing, and aliasing.
    
    This package is for snapshot (alpha/beta) releases, which aren't
    considered to be release/production quality.  It may be used to
    test new features.  Some features / bug fixes may be backported to
    the regular sendmail package.

---
+++ mail/sendmail-snapshot/files/smmsp.sh
@@ -0,0 +1,30 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: smmsp.sh,v 1.1 2024/01/15 04:50:55 jnemeth Exp $
+#
+
+# REQUIRE: mail
+
+#	The only function of the smmsp process is to push locally
+#	collected mail (which got stuck in the local client queue)
+#	to a more fully functional smtp relay.  It is not a fully
+#	functional smtp daemon.
+#
+#	It can, however, be used as a completely unprivileged
+#	replacement for hosts that were previously using a "null
+#	client" configuration and a smart relay.
+
+. /etc/rc.subr
+
+name="smmsp"
+rcvar=$name
+pidfile="@VARBASE@/spool/clientmqueue/sm-client.pid"
+required_files="/etc/mail/submit.cf"
+extra_commands="reload"
+
+smbin="@PREFIX@/libexec/sendmail/sendmail"
+smmsp_flags="-Lsm-msp-queue -Ac -q30m"
+command=$smbin
+
+load_rc_config $name
+run_rc_command "$1"
