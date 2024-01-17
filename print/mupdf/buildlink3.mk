d75dd4fbd48df3d1d9a538c1ef644cac528e6969
gdt <gdt@pkgsrc.org>
2024-01-17 23:57:50 +0000

    revbump.py www/gumbo-parser

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.53 2023/11/12 13:23:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.54 2024/01/17 23:57:50 gdt Exp $
 
 BUILDLINK_TREE+=	mupdf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mupdf
 MUPDF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.14.0
-BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.23.4nb3
+BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.23.7nb1
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
 BUILDLINK_DEPMETHOD.mupdf?=	build
--- www/gumbo-parser/buildlink3.mk
+++ www/gumbo-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/01/17 23:49:50 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/01/17 23:57:50 gdt Exp $
 
 BUILDLINK_TREE+=	gumbo-parser
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	gumbo-parser
 GUMBO_PARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gumbo-parser+=	gumbo-parser>=0.11.0
+BUILDLINK_ABI_DEPENDS.gumbo-parser?=	gumbo-parser>=0.12.1
 BUILDLINK_PKGSRCDIR.gumbo-parser?=	../../www/gumbo-parser
 .endif	# GUMBO_PARSER_BUILDLINK3_MK
 
