14ff8552bb1d704253a519b4cd4661b1e088e523
gdt <gdt@pkgsrc.org>
2024-01-17 23:49:50 +0000

    gumbo-parser: Update to 0.12.1 and change to continuation fork
    Update prepared in wip by Kevin Bloom.
    
    0.12.0 -> 0.12.1:
    - Fix empty the end of the tag <form>
    - Correctly handle text in <form> elements
    - Fix tag name filtering that could result in XSS
    - Return visualc/include/strings.h file
    
    0.11.1 -> 0.12.0:
    - Add support for <dialog> tag
    - Fix TAGSET_INCLUDES macro to work properly with multiple bit flags
    
    0.10.1 -> 0.11.0:
    - Add support for <picture> tag
    - Make genperf.py script compatible with Python 3
    - Change maintainer to Grigory Kirillov

--- www/gumbo-parser/buildlink3.mk
+++ www/gumbo-parser/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/10/08 22:36:20 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/01/17 23:49:50 gdt Exp $
 
 BUILDLINK_TREE+=	gumbo-parser
 
 .if !defined(GUMBO_PARSER_BUILDLINK3_MK)
 GUMBO_PARSER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gumbo-parser+=	gumbo-parser>=0.10.1
+BUILDLINK_API_DEPENDS.gumbo-parser+=	gumbo-parser>=0.11.0
 BUILDLINK_PKGSRCDIR.gumbo-parser?=	../../www/gumbo-parser
 .endif	# GUMBO_PARSER_BUILDLINK3_MK
 
