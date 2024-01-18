d3a18428d484ced13ed32dffc17cd2cd92df537f
vins <vins@pkgsrc.org>
2024-01-18 20:57:03 +0000

    wm/cde: update to 2.5.2
    This is mainly a bugfix release addressing various issues.
    Noticeably, this release included a fix to a bug reported on #netbsd
    irc by a couple of of users, resulting in a frozen display on saved
    sessions.
    
    # pkgsrc changes
    
      * Add session file for CDE to use with other display managers.
      * Update dtlogin service script and README.
      * Explcitely set utmp/wtmp file paths.
      * Use a PHONY target instead of pre-install for PAM files.
      * Remove redundant dependencies.
    
    # upstream changes (since 2.5.1)
    
    ### 2.5.2 (stable) 11/18/2023
    
    Shortlog:
    
    Cy Schubert (1):
           * Fix build under LLVM15
    
    Jon Trulson (9):
           * Apply various patches from Giacomo Comes <co...@na...>
           * Patch from Giacomo Comes: rename ksh manpage to ksh-cde
           * Add DesktopNames=CDE to cde.desktop
           * pgadmin.dt: set icon from pgadmin to pgadmin3
           * dtfile/dterror.ds: fix script defines typo
           * dtksh: enable SHOPT_ECHOPRINT
           * dticon, dtpad, dtterm: fix session save issues (sprintf bogosity)
             lib/DtHelp: strmove(): return memmove() result
           * .gitignore: add new locations of dtsession/dtlogin PAM files
    
    Peter Howkins (3):
           * (Pascal Stumpf) Makefile.am change several places where ${prefix}
             Dshould be $(CDE_INSTALLATION_TOP)
           * (Pascal Stumpf) CDE doesn't provide the ksh binary, don't install
    	     the manpage for it
           * (Pascal Stumpf) dtlogin: On OpenBSD start X as root (it drops
    	     privileges later)
    
    hyousatsu (15):
           * DtTerm: fix a segfault by allocating a string dynamically.
           * dtwm: fix a title bar resizing issue.
           * dtwm: fix compiler warnings.
           * dtwm: add support for _NET_WM_VISIBLE_NAME and
    	     _NET_WM_VISIBLE_ICON_NAME.
           * dtwm: optimize EWMH processing.
           * localized: fix the character encoding errors in zh_TW.UTF-8.
           * dtwm: add a new feature -- window rename.
           * dtwm: optimize EWMH processing.
           * dtwm: support _NET_WM_STATE_ABOVE and _NET_WM_STATE_BELOW.
           * dtsession: change the maximum size of cover dialog to fullscreen.
           * dtlogin: use sessreg to manage utmp/wtmp.
           * dtwm: fix a segfault.
           * dtstyle: make the style manager recognize wheel mouse correctly.
           * tt: make the ttserver process events properly.
           * dtsession: fix a crash.

--- wm/cde/files/dtlogin.sh
+++ wm/cde/files/dtlogin.sh
@@ -1,9 +1,9 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dtlogin.sh,v 1.1 2023/02/12 16:34:15 vins Exp $
+# $NetBSD: dtlogin.sh,v 1.2 2024/01/18 20:57:03 vins Exp $
 #
 # PROVIDE: dtlogin
-# REQUIRE: DAEMON LOGIN
+# REQUIRE: DAEMON LOGIN wscons
 # KEYWORD: shutdown
 #
 
@@ -11,22 +11,10 @@
 
 name="dtlogin"
 rcvar=$name
-command="@PREFIX@/dt/bin/dtlogin"
+command="@PREFIX@/dt/bin/${name}"
 command_args="-daemon"
 pidfile="@VARBASE@/dt/Xpid"
 extra_commands="reload"
 
 load_rc_config $name
-
-
-start_precmd=dtlogin_prestart
-
-dtlogin_prestart()
-{
-	if ! [ -f "@PREFIX@/etc/X11/Xwrapper.config" ]; then
-			echo "If dtlogin fails to start:"
-			echo "# echo allowed_users=anybody >> @PREFIX@/etc/X11/Xwrapper.config"
-	fi
-}
-
 run_rc_command "$1"
