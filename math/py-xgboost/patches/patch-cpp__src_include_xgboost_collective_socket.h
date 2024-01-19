d3d9e4ccf99532b1d8e0f27d47a3b5093d694bf1
adam <adam@pkgsrc.org>
2024-01-19 14:36:17 +0000

    py-xgboost: updated to 2.0.3
    2.0.3
    
    [backport][sklearn] Fix loading model attributes.
    [backport][py] Use the first found native library.
    [backport] [CI] Upload libxgboost4j.dylib (M1) to S3 bucket
    [jvm-packages] Fix POM for xgboost-jvm metapackage

--- math/py-xgboost/patches/patch-cpp__src_include_xgboost_collective_socket.h
+++ math/py-xgboost/patches/patch-cpp__src_include_xgboost_collective_socket.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-xgboost_include_xgboost_collective_socket.h,v 1.1 2023/06/13 17:36:58 adam Exp $
+$NetBSD: patch-cpp__src_include_xgboost_collective_socket.h,v 1.1 2024/01/19 14:36:17 adam Exp $
 
 NetBSD doesn't provide SO_DOMAIN socket option.
 
---- xgboost/include/xgboost/collective/socket.h.orig	2023-06-13 16:57:30.244346651 +0000
-+++ xgboost/include/xgboost/collective/socket.h
+--- cpp_src/include/xgboost/collective/socket.h.orig	2023-06-13 16:57:30.244346651 +0000
++++ cpp_src/include/xgboost/collective/socket.h
 @@ -250,7 +250,7 @@ class TCPSocket {
    HandleT handle_{InvalidSocket()};
    // There's reliable no way to extract domain from a socket without first binding that
