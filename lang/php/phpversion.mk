9f548183bd45acb3c3b77e54c875fce6f880a95b
taca <taca@pkgsrc.org>
2024-01-21 07:56:15 +0000

    lang/php82: update to 8.2.15
    8.2.15 (2024-01-18)
    
    - Core:
      . Fixed bug GH-12953 (false positive SSA integrity verification failed when
        loading composer classmaps with more than 11k elements). (nielsdos)
      . Fixed bug GH-12966 (missing cross-compiling 3rd argument so Autoconf doesn't
        emit warnings). (Peter Kokot)
    
    - Cli:
      . Fix incorrect timeout in built-in web server when using router script and
        max_input_time. (ilutov)
    
    - FFI:
      . Fixed bug GH-9698 (stream_wrapper_register crashes with FFI\CData).
        (Jakub Zelenka)
      . Fixed bug GH-12905 (FFI::new interacts badly with observers). (nielsdos)
    
    - Intl:
      . Fixed GH-12943 (IntlDateFormatter::__construct accepts 'C' as valid locale).
        (David Carlier)
    
    - Hash:
      . Fixed bug GH-12936 (hash() function hangs endlessly if using sha512 on
        strings >= 4GiB). (nielsdos)
    
    - ODBC:
      . Fix crash on Apache shutdown with persistent connections. (nielsdos)
    
    - Opcache:
      . Fixed oss-fuzz #64727 (JIT undefined array key warning may overwrite DIM
        with NULL when DIM is the same var as result). (ilutov)
      . Added workaround for SELinux mprotect execheap issue.
        See https://bugzilla.kernel.org/show_bug.cgi?id=218258. (ilutov)
    
    - OpenSSL:
      . Fixed bug GH-12987 (openssl_csr_sign might leak new cert on error).
        (Jakub Zelenka)
    
    - PDO:
      . Fix GH-12969 (Fixed PDO::getAttribute() to get PDO::ATTR_STRINGIFY_FETCHES).
        (SakiTakamachi)
    
    - PDO_ODBC:
      . Fixed bug GH-12767 (Unable to turn on autocommit mode with setAttribute()).
        (SakiTakamachi)
    
    - PGSQL:
      . Fixed auto_reset_persistent handling and allow_persistent type. (David Carlier)
      . Fixed bug GH-12974 (Apache crashes on shutdown when using pg_pconnect()).
        (nielsdos)
    
    - Phar:
      . Fixed bug #77432 (Segmentation fault on including phar file). (nielsdos)
    
    - PHPDBG:
      . Fixed bug GH-12962 (Double free of init_file in phpdbg_prompt.c). (nielsdos)
    
    - SimpleXML:
      . Fix getting the address of an uninitialized property of a SimpleXMLElement
        resulting in a crash. (nielsdos)
    
    - Tidy:
      . Fixed bug GH-12980 (tidynode.props.attribute is missing
        "Boolean Attributes" and empty attributes). (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.420 2024/01/21 07:53:53 taca Exp $
+# $NetBSD: phpversion.mk,v 1.421 2024/01/21 07:56:15 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -92,7 +92,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.30
 PHP81_VERSION=	8.1.27
-PHP82_VERSION=	8.2.14
+PHP82_VERSION=	8.2.15
 PHP83_VERSION=	8.3.2
 
 # Define API version or initial release of major version.
