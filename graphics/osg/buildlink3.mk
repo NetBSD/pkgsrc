83f7438e4e3e09ecd8e313157c5fc02621018245
ryoon <ryoon@pkgsrc.org>
2024-01-22 13:16:54 +0000

    *: Recursive revbump from multimedia/libvpx

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.131 2023/12/30 12:57:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.132 2024/01/22 13:16:59 ryoon Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.6.5nb45
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.6.5nb46
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../mk/bsd.fast.prefs.mk"
