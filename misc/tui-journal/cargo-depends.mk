712942cd681b77eaace1f062aece97d5ce53c345
pin <pin@pkgsrc.org>
2024-01-22 07:32:57 +0000

    misc/tui-journal: update to 0.8.1
    What's Changed
     - Fixed: Aeruginous: migrate from deprecated comment-changes option -T by @kevinmatthes in #308
     - Added: Rust minimum supported version to cargo manifest by @AmmarAbouZor in #310
     - Enhancement: Remove explicit inline annotations by @AmmarAbouZor in #311

--- misc/tui-journal/cargo-depends.mk
+++ misc/tui-journal/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.16 2024/01/17 07:39:59 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.17 2024/01/22 07:32:57 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.21.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
