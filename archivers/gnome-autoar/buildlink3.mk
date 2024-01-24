054f722944e55eec32792a1d368e14ea037f188d
adam <adam@pkgsrc.org>
2024-01-24 23:22:34 +0000

    gnome-autoar: updated to 0.4.4
    Major changes in 0.4.4:
    * Add application/zstd to supported mime-types (Ondrej Holy)
    * Fix test suite failure with recent GLib version (Ondrej Holy)
    * Add autoar_extractor_set_passphrase function (Sergio Costas Rodriguez)
    
    Major changes in 0.4.3:
    * Prevent NULL dereference when extracting to root (Ondrej Holy)
    * Do not include basename in error messages (Ondrej Holy)
    * Do not request password when encryption is unsupported (Ignacy Kuchciński)
    * Propagate libarchive errors when extraction failed (Ignacy Kuchciński)
    
    Major changes in 0.4.2:
    * Fix extraction to root directory (Ondrej Holy)
    * Fix extraction of raw format archives (Ondrej Holy)
    * Do not leave read-only directory in the tree after running tests (Ondrej Holy)

--- archivers/gnome-autoar/buildlink3.mk
+++ archivers/gnome-autoar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/14 14:01:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/01/24 23:22:34 adam Exp $
 
 BUILDLINK_TREE+=	gnome-autoar
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-autoar
 GNOME_AUTOAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-autoar+=	gnome-autoar>=0.2.4
-BUILDLINK_ABI_DEPENDS.gnome-autoar?=	gnome-autoar>=0.4.1nb10
+BUILDLINK_ABI_DEPENDS.gnome-autoar+=	gnome-autoar>=0.4.1nb10
 BUILDLINK_PKGSRCDIR.gnome-autoar?=	../../archivers/gnome-autoar
 
 .include "../../archivers/libarchive/buildlink3.mk"
