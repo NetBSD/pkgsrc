707b116c796b33009f45771b4cf7d9ff67b6111d
wiz <wiz@pkgsrc.org>
2024-01-24 16:36:38 +0000

    libppl: fix buildlink3 file

--- biology/libpll/buildlink3.mk
+++ biology/libpll/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/12/18 01:41:49 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/01/24 16:36:38 wiz Exp $
 
 BUILDLINK_TREE+=	libpll
 
 .if !defined(LIBPLL_BUILDLINK3_MK)
-BIOLIBC_BUILDLINK3_MK:=
+LIBPLL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpll+=	libpll>=0.3.2
 BUILDLINK_PKGSRCDIR.libpll?=	../../biology/libpll
-
 .endif	# LIBPLL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libpll
