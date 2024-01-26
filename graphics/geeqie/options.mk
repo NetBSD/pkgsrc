108c0c90f95790e988ab1e706addb7cd5024ff1e
wiz <wiz@pkgsrc.org>
2024-01-26 14:13:01 +0000

    geeqie: update to 2.2.
    Add more options (libarchive, libraw, pdf, gspell, libwebp), default off for now.
    
    - Enhanced debug output with -Ddevel=enabled option
    - GitHub workflows for code tests and AppImage building
    - Modify Collections user interface
    - Remote keyboard actions
    - Allow .desktop files to be linked to programmable mouse buttons
    - Option to show-hide selectable bars
    - On exit additional check for multiple windows open
    - Plugin to resize an image
    - Include a separator as a toolbar option
    - Change all .desktop files to RDNS style
    - GTK "Open With" feature menu option
    - Reduce the height of toolbars
    - Option to select "hamburger" menu style
    - More progress in GTK4 migration
    - Many bug fixes

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,13 +1,33 @@
-# $NetBSD: options.mk,v 1.10 2022/11/21 22:28:41 wiz Exp $
+# $NetBSD: options.mk,v 1.11 2024/01/26 14:13:01 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.geeqie
-PKG_SUPPORTED_OPTIONS=		libchamplain
+PKG_SUPPORTED_OPTIONS=		gspell libarchive libchamplain libraw libwebp pdf
 PKG_SUGGESTED_OPTIONS=		# none
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgspell)
+.include "../../textproc/gspell/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibarchive)
+.include "../../archivers/libarchive/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mlibchamplain)
 # While clutter is optional, the map requires it.
 .include "../../graphics/clutter/buildlink3.mk"
 .include "../../geography/libchamplain012/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibraw)
+.include "../../graphics/libraw/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibwebp)
+.include "../../graphics/libwebp/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpdf)
+.include "../../print/poppler/buildlink3.mk"
 .endif
