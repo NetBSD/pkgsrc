ab080436198a801210ddbab481e3773342e2523b
wiz <wiz@pkgsrc.org>
2024-01-26 14:13:01 +0000

    geeqie: update to 2.2.
    Add more options (libarchive, libraw, pdf, gspell, libwebp), default off for now.
    
    - Enhanced debug output with -Ddevel=enabled option
    - GitHub workflows for code tests and AppImage building
    - Modify Collections user interface
    - Remote keyboard actions
    - Allow .desktop files to be linked to programmable mouse buttons
    - Option to show-hide selectable bars
    - On exit additional check for multiple windows open
    - Plugin to resize an image
    - Include a separator as a toolbar option
    - Change all .desktop files to RDNS style
    - GTK "Open With" feature menu option
    - Reduce the height of toolbars
    - Option to select "hamburger" menu style
    - More progress in GTK4 migration
    - Many bug fixes

--- graphics/geeqie/patches/patch-src_exif-common.cc
+++ graphics/geeqie/patches/patch-src_exif-common.cc
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_exif-common.cc,v 1.2 2023/06/17 16:50:53 wiz Exp $
+$NetBSD: patch-src_exif-common.cc,v 1.3 2024/01/26 14:13:01 wiz Exp $
 
-../src/exif-common.cc:780:3: error: 'setenv' was not declared in this scope; did you mean 'getenv'?                                                                                                                
-https://github.com/BestImageViewer/geeqie/pull/1110
+With _XOPEN_SOURCE defined without value, NetBSD headers don't provide setenv().
+https://github.com/BestImageViewer/geeqie/issues/1230
 
---- src/exif-common.cc.orig	2023-06-17 12:12:30.000000000 +0000
+--- src/exif-common.cc.orig	2024-01-26 13:57:52.740080922 +0000
 +++ src/exif-common.cc
-@@ -19,6 +19,8 @@
+@@ -19,7 +19,7 @@
  
  #include <config.h>
  
-+#include <stdlib.h>
-+
- #define _XOPEN_SOURCE
+-#define _XOPEN_SOURCE
++//#define _XOPEN_SOURCE
  
- #include <string.h>
+ #include <cmath>
+ #include <cstdlib>
