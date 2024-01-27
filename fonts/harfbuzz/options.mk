f670327b642bab36a428bf1668d8685426e57434
riastradh <riastradh@pkgsrc.org>
2024-01-27 02:09:19 +0000

    fonts/harfbuzz: Make introspection optional.
    Currently this gets in the way of cross-compiling, and will continue
    to do so until gobject-introspection's obstinate refusal to
    cross-build is overcome.

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.13 2024/01/27 02:08:36 riastradh Exp $
+# $NetBSD: options.mk,v 1.14 2024/01/27 02:09:19 riastradh Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
-PKG_SUPPORTED_OPTIONS+=	doc
-PKG_SUGGESTED_OPTIONS=	doc
+PKG_SUPPORTED_OPTIONS+=	doc introspection
+PKG_SUGGESTED_OPTIONS=	doc introspection
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		doc
+PLIST_VARS+=		doc introspection
 
 .if !empty(PKG_OPTIONS:Mdoc)
 MESON_ARGS+=	-Ddocs=enabled
@@ -14,4 +14,14 @@ TOOL_DEPENDS+=	gtk-doc>=1.32nb9:../../textproc/gtk-doc
 PLIST.doc=	yes
 .else
 MESON_ARGS+=	-Ddocs=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mintrospection)
+MESON_ARGS+=	-Dintrospection=enabled
+.include "../../devel/glib2/buildlink3.mk"
+# XXX This should be TOOL_DEPENDS, no?
+BUILDLINK_DEPMETHOD.gobject-introspection+=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.else
+MESON_ARGS+=	-Dintrospection=disabled
 .endif
