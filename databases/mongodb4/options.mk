b2eca795e0a36c11ac8701205413883821d005a8
adam <adam@pkgsrc.org>
2024-01-27 08:32:31 +0000

    mongodb4: updated to 4.4.28
    4.4.28 - Jan 18, 2024
    SERVER-77506
     Sharded multi-document transactions can mismatch data and ShardVersion
    
    SERVER-82365
     Optimize the construction of the balancer's collection distribution status histogram (2nd attempt)
    
    SERVER-82883
     Recovering TransactionCoordinator on stepup may block acquiring read/write tickets while participants are in the prepared state
    
    WT-7929
     Investigate a solution to avoid FTDC stalls during checkpoint
    
    All JIRA issues closed in 4.4.28
    
    
    4.4.27 - Jan 3, 2024
    SERVER-63865
     Handle missing index idents during standalone startup recovery after unclean shutdown
    
    SERVER-81106
     Recipient shard doesn't wait for the collection version to be locally persisted before starting the cloning phase
    
    SERVER-81878
     startupRecoveryForRestore may not play nicely with collection drop applied during startup recovery
    
    SERVER-82325
     Config server could invariant during balancer round
    
    WT-11564
     Fix RTS to read the newest transaction value only when it exists in the checkpoint
    
    All JIRA issues closed in 4.4.27
    
    
    4.4.26 - Nov 27, 2023
    Issues fixed:
    
    SERVER-50792
     Return more useful errors when a shard key index can't be found for shardCollection or refineCollectionShardKey
    
    SERVER-80021
     Make $convert round-trip correctly between double and string
    
    SERVER-81106
     Recipient shard doesn't wait for the collection version to be locally persisted before starting the cloning phase
    
    SERVER-81966
     Avoid modification of previous ChunkMap instances during refresh
    
    WT-10424
     cursor::search_near slow performance if many deleted items are present
    
    All JIRA issues closed in 4.4.26
    
    
    4.4.25 - Sep 29, 2023
    Issues fixed:
    
    SERVER-76299
     Report writeConflicts in serverStatus on secondaries
    
    SERVER-78828
     LDAP host timing data can be inconsistent during sorting
    
    WT-11031
     Fix RTS to skip tables with no time window information in the checkpoint
    
    SERVER-70973
     Balancer should stop iterating collections when there are no more available shards
    
    SERVER-71627
     Refreshed cached collection route info will severely block all client request when a cluster with 1 million chunks
    
    SERVER-78813
     Commit point propagation fails indefinitely with exhaust cursors with null lastCommitted optime
    
    WT-8570
     Do not increase oldest ID during recovery
    
    WT-10449
     Do not save update chain when there are no updates to be written to the history store
    
    All JIRA issues closed in 4.4.25
    
    
    4.4.24 - Aug 23, 2023
    Issues fixed:
    
    SERVER-76299
     Report writeConflicts in serverStatus on secondaries
    
    SERVER-78828
     LDAP host timing data can be inconsistent during sorting
    
    WT-11031
     Fix RTS to skip tables with no time window information in the checkpoint

--- databases/mongodb4/options.mk
+++ databases/mongodb4/options.mk
@@ -1,14 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2023/07/29 11:51:15 adam Exp $
+# $NetBSD: options.mk,v 1.2 2024/01/27 08:32:31 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mongodb
 PKG_SUPPORTED_OPTIONS=	ssl
+PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mssl)
 .include "../../security/openssl/buildlink3.mk"
 SCONS_BUILD_ARGS+=		--ssl=on
-SCONS_BUILD_ARGS+=		--ssl-provider=openssl
+#SCONS_BUILD_ARGS+=		--ssl-provider=openssl
 .else
 SCONS_BUILD_ARGS+=		--ssl=off
 .endif
