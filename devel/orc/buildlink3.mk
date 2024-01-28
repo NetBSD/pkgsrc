75bad2ba5f6db67c073a8cc7a88c2b6aaeb5f773
adam <adam@pkgsrc.org>
2024-01-28 07:14:16 +0000

    orc: updated to 0.4.36
    0.4.36
    - Only use AVX / AVX2 instructions on CPUs that support both AVX and AVX2
      (fixes crash on machines that only support AVX) (L. E. Segovia)
    
    0.4.35
    - Add support for AVX / AVX2 (L. E. Segovia)
    - SSE backend improvements (L. E. Segovia)
    - New `orf` and `andf` opcodes for bitwise AND and OR for single precision floats (Jorge Zapata)
    - Add support for `convwf`, int16 to float conversion (Jorge Zapata)
    - Allow backend selection through ORC_TARGET environment variable (L. E. Segovia)
    - Documentation improvements (Jorge Zapata, L. E. Segovia, Tim-Philipp Müller)
    - orconce: Use Win32 once implementation with MSVC (Seungha Yang, L. E. Segovia)
    - orcc: add --binary option to output raw machine code for functions (L. E. Segovia)
    - orcprofile: Implement Windows high-resolution timestamp for MSVC
      to allow benchmarking on MSVC builds (L. E. Segovia)

--- devel/orc/buildlink3.mk
+++ devel/orc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/08/14 05:24:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/01/28 07:14:17 adam Exp $
 
 BUILDLINK_TREE+=	orc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	orc
 ORC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.orc+=	orc>=0.4.6
-BUILDLINK_ABI_DEPENDS.orc?=	orc>=0.4.34nb1
+BUILDLINK_ABI_DEPENDS.orc+=	orc>=0.4.34nb1
 BUILDLINK_PKGSRCDIR.orc?=	../../devel/orc
 .endif	# ORC_BUILDLINK3_MK
 
