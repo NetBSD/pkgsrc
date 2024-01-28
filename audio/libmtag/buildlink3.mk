97a764e5ae61c00ca8eb8facd6f39b1e427303de
wiz <wiz@pkgsrc.org>
2024-01-28 13:24:32 +0000

    *: recursive bump for taglib 2

--- audio/libmtag/buildlink3.mk
+++ audio/libmtag/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/10/17 07:56:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/01/28 13:24:33 wiz Exp $
 
 BUILDLINK_TREE+=	libmtag
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmtag
 LIBMTAG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmtag+=	libmtag>=0.2.0
-BUILDLINK_ABI_DEPENDS.libmtag?=	libmtag>=0.3.2nb1
+BUILDLINK_ABI_DEPENDS.libmtag?=	libmtag>=0.3.2nb2
 BUILDLINK_PKGSRCDIR.libmtag?=	../../audio/libmtag
 
 .include "../../audio/taglib/buildlink3.mk"
