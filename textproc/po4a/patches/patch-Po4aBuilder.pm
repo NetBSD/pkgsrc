076a5186ac3114a78536417658c6938bb2cdfd34
adam <adam@pkgsrc.org>
2024-01-29 16:08:20 +0000

    po4a: updated to 0.70
    0.70
    The PerlIO cleanup release
    
    Major cleanup:
    
    Greatly simplify the code by using PerlIO instead of messing up with encodings manually.
    This is a very intrusive change, and even if all tests of our comprehensive suite pass, I still expect issues with this on some corner cases, such as projects not using UTF-8 but a mixture of encodings. Please report any issue, and accept my apologies.
    This greatly simplifies the code, enabling future maintenance.
    Asciidoc:
    
    Correctly handle lines that start with a dot in a paragraph. Thanks Jean-Noël Avila for the pull request.
    Labels of cross-references were not translatable. Thanks Jean-Noël Avila for the pull request.
    Fix a bug in the handling of the 'id' attribute. Thanks Jean-Noël Avila for the pull request.
    Process multi-line attributes in legacy and asciidoctor mode. Thanks Jean-Noël Avila for the pull request.
    Process transparent index entries in their own segmentst. Thanks Jean-Noël Avila for the pull request.
    Keep original formating. Thanks suddenfall for the feedback, and Jean-Noël Avila for the fixes.
    Fix a bug in handling of bold text starting a list item. Thanks Jean-Noël Avila for the pull request.
    Fix handling of linebreaks in lists). Thanks Jean-Noël Avila for the pull request.
    Tex:
    
    Allow verbatim environment declaration without trailing space. Thanks Mayeul Cantan for the report & the fix.
    Informative error message when file not found for \input, indicating how to skip this include [mquinson].
    Texinfo:
    
    Translate partial menu node names [Apteryks].
    Markdown:
    
    Treat link reference definitions as no-wrap [gemmaro].
    Don't emit a warning when provided an empty tag list in YFM [Martin].
    YAML:
    
    Verbosely fail on inline lists and dictionaries, as YAML::Tiny does not handle them as lists or dicts anyway. Do quote list-looking strings [Martin & gemmaro].
    General:
    
    Don't fail on empty po files, e.g. adoc files containing only includes. Thanks Suddenfall for the report & fix.
    Fix an encoding error in one file which resulted in build errors with new gettext versions. Thanks to Jan Palus for the issue report and the fix.
    Use the semantic of Perl v5.12 (released in 2012) instead of the previously requested one (v5.6 release in 2000). This is to request the unicode_strings feature, making the utf strings more predictable.
    Accept BOM markers at the beginning of files [Mt]
    Specify in the README.md and in all source files that the license is GPL2+, not GPL2-only [Mt]
    Documentation:
    
    Improve the description of the internals, in the hope that potential contributors will become proficient quickly and help us. [Mt]
    Revamp the po4a(7) document now that the po4a-* scripts are deprecated. Explain why they are, and don't mention them in the described workflow. [Mt]
    po4a main script (the deprecated po4a-* scripts are still buggy):
    
    Deal properly with CRLF files coming from windows.
    Allow to escape spaces in file names, or to quote them.
    Do not pass empty PO files to msgmerge, as it stumbles on such things when the POT file has UTF chars in the msgids. [Mt]
    po4a-translate:
    
    Remove the wrap-po option that was not used internally.
    po4a-updatepo:
    
    Do not pass empty PO files to msgmerge, as it stumbles on such things when the POT file has UTF chars in the msgids. [Mt]
    Build scripts:
    
    Use xmlcatalog(1) to find DocBook XSL file.
    Translations:
    
    Updated: Chinese (traditional), thanks Haowei Hsu.
    Updated: French, thanks Jérémie Tarot, brandelune and Jean-Baptiste Holcroft.
    Updated: Georgian, thanks Temuri Doghonadze.
    Updated: German, thanks Helge Kreutzmann.
    Updated: Italian, thanks Marco Ciampa.
    Updated: Japanese, thanks gemmaro.
    Updated: Norwegian Bokmål, thanks Allan Nordhøy and Petter Reinholdtsen.
    Updated: Portuguese, thanks Silvério Santos.
    Updated: Russian, thanks vrbtm, Виталий Наумов and Golubev Alexander.
    Updated: Serbian (cyrillic), thanks Ivan Pesic.
    Updated: Spanish, thanks Francisco Serrador, ignotus and gallegonovato.

--- textproc/po4a/patches/patch-Po4aBuilder.pm
+++ textproc/po4a/patches/patch-Po4aBuilder.pm
@@ -1,4 +1,4 @@
-$NetBSD: patch-Po4aBuilder.pm,v 1.10 2023/06/21 19:08:06 schmonz Exp $
+$NetBSD: patch-Po4aBuilder.pm,v 1.11 2024/01/29 16:08:20 adam Exp $
 
 (1) ryoon: remove gzip at initial import.
 (2) mef: Add PerlIO::F_UTF8 () macro.
@@ -6,27 +6,27 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.10 2023/06/21 19:08:06 schmonz Exp $
 (4) schmonz: Invoke pkgsrc's gettext-tools.
 (5) schmonz: Write to the full physical null device.
 
---- Po4aBuilder.pm.orig	2023-01-01 00:30:43.000000000 +0000
+--- Po4aBuilder.pm.orig	2024-01-28 23:39:44.000000000 +0000
 +++ Po4aBuilder.pm
-@@ -9,6 +9,8 @@ use File::stat;
+@@ -16,6 +16,8 @@ use File::stat;
  
- @ISA = qw(Module::Build);
+ our @ISA = qw(Module::Build);
  
 +sub PerlIO::F_UTF8 () { 0x00008000 } # from perliol.h
 +
  sub ACTION_build {
      my $self = shift;
      $self->depends_on('code');
-@@ -60,7 +62,7 @@ sub ACTION_binpo {
+@@ -67,7 +69,7 @@ sub ACTION_binpo {
          chdir "../..";
  	
          if ( -e "po/bin/po4a.pot") {
--            $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
-+            $diff = qx(@DIFF@ -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
+-            my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
++            my $diff = qx(@DIFF@ -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
              if ( $diff eq "" ) {
                  unlink "po/bin/po4a.pot.new" || die;
                  # touch it
-@@ -82,11 +84,11 @@ sub ACTION_binpo {
+@@ -89,11 +91,11 @@ sub ACTION_binpo {
        if (0) {
          unless ($self->up_to_date("po/bin/po4a.pot", $_)) {
              print "XX Sync $_: ";
@@ -35,12 +35,12 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.10 2023/06/21 19:08:06 schmonz Exp $
              # Typically all that changes was a date. I'd
              # prefer not to commit such changes, so detect
              # and ignore them.
--            $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
-+            $diff = qx(@DIFF@ -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
+-            my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
++            my $diff = qx(@DIFF@ -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
              if ($diff eq "") {
                  unlink "$_.new" || die;
                  # touch it
-@@ -101,7 +103,7 @@ sub ACTION_binpo {
+@@ -108,7 +110,7 @@ sub ACTION_binpo {
        }
          unless ($self->up_to_date($_,"blib/po/$lang/LC_MESSAGES/po4a.mo")) {
              mkpath( File::Spec->catdir( 'blib', 'po', $lang, "LC_MESSAGES" ), 0, oct(755) );
@@ -49,41 +49,41 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.10 2023/06/21 19:08:06 schmonz Exp $
          }
      }
  }
-@@ -222,9 +224,9 @@ sub ACTION_man {
-                 $parser->{release} = $parser->{center} = "Po4a Tools";
+@@ -230,9 +232,9 @@ sub ACTION_man {
+             $parser->{release} = $parser->{center} = "Po4a Tools";
          } else {
-                 my $command;
--                $command = "msggrep -K -E -e \"Po4a Tools\" po/pod/$lang.po |";
--                $command .= "msgconv -t UTF-8 | ";
--                $command .= "msgexec /bin/sh -c '[ -n \"\$MSGEXEC_MSGID\" ] ";
-+                $command = "@PREFIX@/bin/msggrep -K -E -e \"Po4a Tools\" po/pod/$lang.po |";
-+                $command .= "@PREFIX@/bin/msgconv -t UTF-8 | ";
-+                $command .= "@PREFIX@/bin/msgexec @SH@ -c '[ -n \"\$MSGEXEC_MSGID\" ] ";
-                 $command .= "&& cat || cat > /dev/null'";
+             my $command;
+-            $command = "msggrep -K -E -e \"Po4a Tools\" po/pod/$lang.po |";
+-            $command .= "msgconv -t UTF-8 | ";
+-            $command .= "msgexec /bin/sh -c '[ -n \"\$MSGEXEC_MSGID\" ] ";
++            $command = "@PREFIX@/bin/msggrep -K -E -e \"Po4a Tools\" po/pod/$lang.po |";
++            $command .= "@PREFIX@/bin/msgconv -t UTF-8 | ";
++            $command .= "@PREFIX@/bin/msgexec @SH@ -c '[ -n \"\$MSGEXEC_MSGID\" ] ";
+             $command .= "&& cat || cat > /dev/null'";
  
-                 my $title = `$command 2> /dev/null`;
-@@ -234,8 +236,6 @@ sub ACTION_man {
+             my $title = `$command 2> /dev/null`;
+@@ -242,8 +244,6 @@ sub ACTION_man {
          }
-         $parser->parse_from_file ($file, $out);
+         $parser->parse_from_file( $file, $out );
  
 -        system("gzip -9 -n -f $out") and die;
 -        unlink "$file" || die;
      }
  
-     if ($^O ne 'MSWin32') {
-@@ -253,9 +253,7 @@ sub ACTION_man {
-             print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
-             system("xsltproc -o $outdir/$outfile.$section --nonet http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $file") and die;
-             }
--                system ("gzip -9 -n -f $outdir/$outfile.$section") and die;
+     if ( $^O ne 'MSWin32' ) {
+@@ -264,9 +264,7 @@ sub ACTION_man {
+                     print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
+                     system("xsltproc -o $outdir/$outfile.$section --nonet $docbook_xsl_url $file") and die;
+                 }
+-                system("gzip -9 -n -f $outdir/$outfile.$section") and die;
              }
 -            unlink "$file" || die;
          }
      }
  }
-@@ -284,7 +282,7 @@ sub postats {
-     foreach (sort @files) {
-         $file = $_;
+@@ -294,7 +292,7 @@ sub postats {
+     my (@t100,@t95,@t90,@t80,@t70,@t50,@t33,@t20,@starting);
+     foreach my $file (sort @files) {
          my $lang = fileparse($file, qw{.po});
 -        my $stat = `msgfmt -o /dev/null -c --statistics $file 2>&1`;
 +        my $stat = `@PREFIX@/bin/msgfmt -o @PO4A_DEVNULL@ -c --statistics $file 2>&1`;
