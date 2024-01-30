67c9651aebe9ab5119e3af4c48907535c453c680
adam <adam@pkgsrc.org>
2024-01-30 18:22:53 +0000

    abseil: updated 20240116.0
    Abseil LTS 20240116.0
    
    What's New:
    
    Added absl::NoDestructor<T> to simplify defining static types that do not need to be destructed upon program exit.
    Added configurable verbose logging (also known as VLOG).
    Added absl::Overload(), which returns a functor that provides overloads based on the functors passed to it. Note that this functionality requires C++17 or newer.
    Bzlmod is now officially supported (previously it was supported by the community). Note that it may take several days after the release for it to become available in the Bazel Central Registry.
    
    Breaking Changes:
    
    AbslHashValue() no longer accepts C-style arrays as a parameter. Previously the array would decay to a pointer type, which could lead to subtle, unintended bugs. The most common potential error is passing a C-string literal. After this change, these call-sites require wrapping the literal in absl::string_view.
    absl::weak_equality and absl::strong_equality have been removed. The corresponding std types were removed before C++20 was finalized (https://wg21.link/P1959R0).

--- devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
+++ devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.3 2023/08/21 14:52:26 adam Exp $
+$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.4 2024/01/30 18:22:53 adam Exp $
 
 Avoid _XOPEN_SOURCE.
 
---- absl/time/internal/cctz/src/time_zone_format.cc.orig	2023-08-07 18:40:00.000000000 +0000
+--- absl/time/internal/cctz/src/time_zone_format.cc.orig	2024-01-22 17:59:25.000000000 +0000
 +++ absl/time/internal/cctz/src/time_zone_format.cc
 @@ -19,7 +19,7 @@
  #endif
  
  #if defined(HAS_STRPTIME) && HAS_STRPTIME
--#if !defined(_XOPEN_SOURCE) && !defined(__OpenBSD__)
-+#if !defined(_XOPEN_SOURCE) && !defined(__NetBSD__) && !defined(__OpenBSD__)
+-#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
++#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
  #define _XOPEN_SOURCE 500  // Exposes definitions for SUSv2 (UNIX 98).
  #endif
  #endif
