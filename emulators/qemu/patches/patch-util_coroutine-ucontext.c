eff07547a4e97b347d2c204e733e67d0fc7838c7
adam <adam@pkgsrc.org>
2024-01-30 18:20:09 +0000

    qemu: updated to 8.2.1
    8.2.1
    Bug fixes

--- emulators/qemu/patches/patch-util_coroutine-ucontext.c
+++ emulators/qemu/patches/patch-util_coroutine-ucontext.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-util_coroutine-ucontext.c,v 1.1 2023/03/15 11:49:20 jperkin Exp $
+$NetBSD: patch-util_coroutine-ucontext.c,v 1.2 2024/01/30 18:20:09 adam Exp $
 
 https://github.com/omniosorg/omnios-extra/blob/master/build/qemu/patches/0008-Temporary-coroutine-patch-pending-illumos-15206.patch
 
---- util/coroutine-ucontext.c.orig	2022-12-14 16:28:45.000000000 +0000
+--- util/coroutine-ucontext.c.orig	2024-01-29 19:13:22.000000000 +0000
 +++ util/coroutine-ucontext.c
-@@ -305,6 +305,12 @@ qemu_coroutine_switch(Coroutine *from_,
+@@ -320,6 +320,12 @@ qemu_coroutine_switch(Coroutine *from_,
  
      ret = sigsetjmp(from->env, 0);
      if (ret == 0) {
@@ -14,6 +14,6 @@ https://github.com/omniosorg/omnios-extra/blob/master/build/qemu/patches/0008-Te
 +        ucontext_t *t = SIGJMP2UCONTEXT(to->env);
 +        t->uc_mcontext.gregs[REG_FSBASE] = f->uc_mcontext.gregs[REG_FSBASE];
 +#endif
-         start_switch_fiber_asan(action, &fake_stack_save, to->stack,
-                                 to->stack_size);
-         start_switch_fiber_tsan(&fake_stack_save,
+         start_switch_fiber_asan(IS_ENABLED(CONFIG_COROUTINE_POOL) ||
+                                 action != COROUTINE_TERMINATE ?
+                                     &fake_stack_save : NULL,
