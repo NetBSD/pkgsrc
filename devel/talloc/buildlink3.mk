bd9490f523de92622de2ba46ec614b614af583de
adam <adam@pkgsrc.org>
2024-01-31 17:40:00 +0000

    talloc: updated to 2.4.2
    2.4.2
    Unknown changes

--- devel/talloc/buildlink3.mk
+++ devel/talloc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/08/14 05:24:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/01/31 17:40:00 adam Exp $
 
 BUILDLINK_TREE+=	talloc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	talloc
 TALLOC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.talloc+=	talloc>=2.1.0
-BUILDLINK_ABI_DEPENDS.talloc?=	talloc>=2.4.1nb1
+BUILDLINK_ABI_DEPENDS.talloc+=	talloc>=2.4.1nb1
 BUILDLINK_PKGSRCDIR.talloc?=	../../devel/talloc
 .endif	# TALLOC_BUILDLINK3_MK
 
