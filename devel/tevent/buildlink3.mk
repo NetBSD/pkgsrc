5c3c091be7a1eaf5c548ae47a765be376b60d527
adam <adam@pkgsrc.org>
2024-01-31 17:43:16 +0000

    tevent: updated to 0.16.1
    0.16.1
    Unknown changes

--- devel/tevent/buildlink3.mk
+++ devel/tevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/08/14 05:24:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/01/31 17:43:16 adam Exp $
 
 BUILDLINK_TREE+=	tevent
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	tevent
 TEVENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tevent+=	tevent>=0.9.37
-BUILDLINK_ABI_DEPENDS.tevent?=	tevent>=0.14.1nb1
+BUILDLINK_ABI_DEPENDS.tevent+=	tevent>=0.14.1nb1
 BUILDLINK_PKGSRCDIR.tevent?=	../../devel/tevent
 
 .include "../../devel/talloc/buildlink3.mk"
