37042b99dcb5388a9b31e1eb19b9bcee93284522
adam <adam@pkgsrc.org>
2024-02-01 10:25:02 +0000

    cmake cmake-gui: updated to 3.28.2
    CMake 3.28.2
    * cmNinjaTargetGenerator: use scan flag for modmap usage inexported commands
    * cmDyndepCollation: clarify CXX_MODULES-without-BMI error message
    * Help: update lua.org links to be HTTPS
    * ExternalProject: revert `BYPRODUCTS` for download outputs
    * Tests/CXXModules: add a test with unity build support
    * cmGlobalGenerator: add unity sources after computing target compile features
    * cmLocalGenerator: ignore scanned sources for unity builds
    * cmVisualStudio10TargetGenerator: fix typo in flag name
    * Tests/CXXModules: add a test case for VS generation without flags
    * cmVisualStudio10TargetGenerator: always specify scanning
    * Tests/CXXModules/scan_properties: use `ixx` extension
    * cxxmodules: make export trampoline script files unique
    * FindBoost: Add support for Boost 1.84
    * zlib: Update script to get zlib 1.2.13
    * CUDA/Clang: Update architectures supported by CUDA 12
    * CUDA/Clang: Record architectures supported by Clang 16
    * Source: Simplify hasher object construction
    * Tests: Add missing include in testUVProcessChainHelper on Windows
    * cmUVProcessChain: Simplify builder initialization
    * cmUVProcessChain: Simplify SetExternalStream usage
    * cmUVProcessChain: Tolerate fileno() of invalid FILE stream
    * Revert use of libuv for process execution for 3.28
    * FindFreetype: Revert use of upstream freetype cmake package
    * Help: Fix typo not accounting for addition of visionOS
    * Help: Note CMAKE_IOS_INSTALL_COMBINED deprecation in toolchains example
    * Help: Update missed text for dynamically generated resource spec files
    * Help: Use  for non-keyword arguments in property command signatures
    * Help: Use  consistently in property getter commands
    * Help: Add missing DIRECTORY keyword to set_tests_properties() signature
    * Help: Fix wrong return values for unset inherited properties
    * Tests: Improve C compatibility of LoadCommand tests
    * HIP: Really forward CMAKE_HIP_HOST_COMPILER in check_language(HIP)
    * HIP: Propagate CMAKE_HIP_PLATFORM from/to the test project in check_language
    * FindMatlab: Restore support for versions without VersionInfo.xml
    * FindMatlab: use correct registry view when extracting versions
    * FindMatlab: Restore support for finding EXACT major.minor version
    * FindMatlab: matlab_get_release_name_from_version is a function now
    * FindMatlab: Document all registry paths for version lookup
    * FindMatlab: Fix error message from function
    * FindMatlab: Some versions use major.minor.patch in the registry
    * FindMatlab: Document that version mappings do not apply to MCR
    * UsePkgConfig: Restore removal of trailing whitespace from pkg-config output
    * Xcode: Restore support for standalone IMPORTED_LOCATION_
    * Ninja: LINK_OPTIONS property should support newlines
    * Help: string(LENGTH): Fix synopsis link to command description
    * cxxmodules: compute link information for C++ module-consuming targets
    * Makefile: Reduce string copies
    * Makefile: Fix double escaping when DEPFILE is used
    * cmQtAutoGenInitializer: Improve Const-correctness
    * Autogen: Forward dependencies when both Makefile and DEPFILE are used
    * Source: Restore compilation on OpenBSD
    * CUDAToolkit: Correctly search all include paths from compiler
    * Tests: Verify CUDA::cupti existence on a subset of machines
    * FindMatlab: Fix major.minor version lookups in Windows Registry
    * FindMatlab: Accept long version in matlab_get_release_name_from_version
    * zlib 2022-10-12 (04f42cec)

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.51 2023/12/18 05:37:35 adam Exp $
+# $NetBSD: version.mk,v 1.52 2024/02/01 10:25:02 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.28.1
+CMAKE_VERSION=	3.28.2
 CMAKE_API=	${CMAKE_VERSION:R}
