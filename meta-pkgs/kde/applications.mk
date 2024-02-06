592a47bb6da7ae872fc4e0f005d1127852eb3a21
markd <markd@pkgsrc.org>
2024-02-06 10:30:14 +0000

    kde: bump frameworks to 5.114.0, kde gear to 23.08.4. plasma to 5.27.10

--- meta-pkgs/kde/applications.mk
+++ meta-pkgs/kde/applications.mk
@@ -1,6 +1,6 @@
-# $NetBSD: applications.mk,v 1.9 2023/08/03 02:10:31 markd Exp $
+# $NetBSD: applications.mk,v 1.10 2024/02/06 10:30:14 markd Exp $
 
-KAPPSVER?=	23.04.3
+KAPPSVER?=	23.08.4
 CATEGORIES+=	kde
 MASTER_SITES?=	${MASTER_SITE_KDE:=release-service/${KAPPSVER}/src/}
 EXTRACT_SUFX?=	.tar.xz
--- meta-pkgs/kde/plasma5.mk
+++ meta-pkgs/kde/plasma5.mk
@@ -1,6 +1,6 @@
-# $NetBSD: plasma5.mk,v 1.5 2022/09/29 00:22:13 markd Exp $
+# $NetBSD: plasma5.mk,v 1.6 2024/02/06 10:30:14 markd Exp $
 
-PLASMAVER?=	5.24.6
+PLASMAVER?=	5.27.10
 CATEGORIES+=	kde
 MASTER_SITES=	${MASTER_SITE_KDE:=plasma/${PLASMAVER}/}
 EXTRACT_SUFX=	.tar.xz
