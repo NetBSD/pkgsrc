7975d73e18549f2fc00bb6a3e543749314518c3c
markd <markd@pkgsrc.org>
2024-02-06 10:46:24 +0000

    *: update KDE Frameworks to 5.114.0
    6 months of development

--- sysutils/baloo5/patches/patch-src_file_timeestimator.cpp
+++ sysutils/baloo5/patches/patch-src_file_timeestimator.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_file_timeestimator.cpp,v 1.1 2022/05/17 17:17:57 jperkin Exp $
+$NetBSD: patch-src_file_timeestimator.cpp,v 1.2 2024/02/06 10:46:30 markd Exp $
 
 Avoid ambiguous function call.
 
---- src/file/timeestimator.cpp.orig	2022-04-02 10:13:54.000000000 +0000
+--- src/file/timeestimator.cpp.orig	2024-01-06 08:49:42.000000000 +0000
 +++ src/file/timeestimator.cpp
-@@ -10,6 +10,8 @@
+@@ -9,6 +9,8 @@
+ 
  #include "timeestimator.h"
- #include "filecontentindexerprovider.h"
  
 +using std::sqrt;
 +
  using namespace Baloo;
  
- TimeEstimator::TimeEstimator(QObject* parent)
+ TimeEstimator::TimeEstimator() = default;
