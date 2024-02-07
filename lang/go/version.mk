ee9f4cfb591480d1226b032700e259d3b39753fd
bsiegert <bsiegert@pkgsrc.org>
2024-02-07 14:44:16 +0000

    go121: update 1.21.7
    go1.21.7 (released 2024-02-06) includes fixes to the compiler, the go command,
    the runtime, and the crypto/x509 package. See the Go 1.21.7 milestone on the Go
    issue tracker for details.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.200 2024/02/07 14:37:50 bsiegert Exp $
+# $NetBSD: version.mk,v 1.201 2024/02/07 14:44:16 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,7 +6,7 @@
 #
 .include "go-vars.mk"
 
-GO121_VERSION=	1.21.6
+GO121_VERSION=	1.21.7
 GO120_VERSION=	1.20.14
 GO119_VERSION=	1.19.13
 GO118_VERSION=	1.18.10
