cf5774659c94c6659adbaec1f858a2f5056f4512
wiz <wiz@pkgsrc.org>
2024-02-07 18:42:45 +0000

    atkmm2.36: update to 2.36.3.
    atkmm 2.36.3 (stable):
    
    Documentation:
    * atkmm.h: Update link to ATK
      (Kjell Ahlstedt)
    * Add README.win32.md with Win32 build documentation
      (Chun-wei Fan)
    * Remove AUTHORS and add general information to README.md
      (Kjell Ahlstedt) Issue gtkmm#140
    
    Meson build:
    * Detect if we build from a git subtree
      (William Roy) Merge request gtkmm!72
    * Don't copy files with configure_file()
      (Kjell Ahlstedt)
    * Fix the evaluation of is_git_build on Windows
      (Kjell Ahlstedt) Issue gtkmm#131 (William Roy)
    * Don't fail if warning_level=everything
      (Daniel Boles, Kjell Ahlstedt) Merge request gtkmm!87
    
    
    atkmm 2.36.2 (stable):
    
    Build:
    * Support building with Visual Studio 2022
      (Chun-wei Fan)
    * Meson build: Specify 'check' option in run_command()
      Will be necessary with future versions of Meson.
    * Meson build: Perl is not required by new versions of mm-common
    * Meson build: Avoid unnecessary configuration warnings
    * Meson build: Require meson >= 0.55.0
      (Kjell Ahlstedt)
    * Require atk >= 2.33.3
      Not a new requirement, but previously it was not specified in
      configure.ac and meson.build.
      (Kjell Ahlstedt)

--- devel/atkmm2.36/buildlink3.mk
+++ devel/atkmm2.36/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/06/28 11:31:39 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/02/07 18:42:45 wiz Exp $
 
 BUILDLINK_TREE+=	atkmm2.36
 
@@ -7,7 +7,7 @@ ATKMM2.36_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.atkmm2.36+=	atkmm2.36>=2.36.1
 BUILDLINK_ABI_DEPENDS.atkmm2.36?=	atkmm2.36>=2.36.1nb2
-BUILDLINK_PKGSRCDIR.atkmm2.36?=	../../devel/atkmm2.36
+BUILDLINK_PKGSRCDIR.atkmm2.36?=		../../devel/atkmm2.36
 
 GCC_REQD+=	4.9
 
