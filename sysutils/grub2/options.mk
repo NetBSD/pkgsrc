d365dda4ad4941c406dad8ea987559573310178d
wiz <wiz@pkgsrc.org>
2024-02-07 17:50:45 +0000

    grub2: update to 2.12.
    With help from Anon.
    
    New in 2.12:
    
    * GCC 13 support.
    * clang 14 support.
    * binutils 2.38 support.
    * Unification of EFI Linux kernel loader across architectures.
    * Transition to EFI Linux kernel stub loader for x86 architecture.
    * Initial support for Boot Loader Interface.
    * Support for dynamic GRUB runtime memory addition using firmware calls.
    * PCI and MMIO UARTs support.
    * SDL2 support.
    * LoongArch support.
    * TPM driver fixes.
    * Many filesystems fixes.
    * Many CVE and Coverity fixes.
    * Debugging support improvements.
    * Tests improvements.
    * Documentation improvements.
    * ...and tons of other fixes and cleanups...
    
    New in 2.06:
    
    * GCC 10 support.
    * clang 10 support.
    * SBAT support.
    * LUKS2 support.
    * Drop small MBR gap support.
    * Xen Security Modules (XSM/FLASK) support.
    * The lockdown mechanism similar to the Linux kernel one.
    * Disable the os-prober by default.
    * Many backports of GRUB distros specific patches.
    * BootHole and BootHole2 fixes.
    * ...and tons of other fixes and cleanups...

--- sysutils/grub2/options.mk
+++ sysutils/grub2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2023/06/06 12:42:19 riastradh Exp $
+# $NetBSD: options.mk,v 1.6 2024/02/07 17:50:45 wiz Exp $
 #
 
 #
@@ -9,7 +9,7 @@
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.grub2
-PKG_SUPPORTED_OPTIONS=	debug freetype fuse
+PKG_SUPPORTED_OPTIONS=	debug freetype fuse grub-efi
 PKG_SUGGESTED_OPTIONS=	freetype
 
 .include "../../mk/bsd.options.mk"
@@ -48,3 +48,25 @@ PLIST.fuse=		yes
 .else
 CONFIGURE_ARGS+=	--disable-grub-mount
 .endif
+
+#
+# The configure script sets the variables `target_cpu' and `platform', which
+# are used for the location of the library directory.  The following mimics
+# the configure script's behavior for i386 and x86_64.
+#
+GRUB_TARGET_CPU=	${MACHINE_ARCH}
+.if ${LOWER_VENDOR} == "apple" || !empty(PKG_OPTIONS:Mgrub-efi)
+GRUB_PLATFORM=		efi
+.else
+.  if !defined(GRUB_PLATFORM)
+GRUB_PLATFORM=		pc
+.  endif
+.endif
+.if ${GRUB_TARGET_CPU} == "x86_64" && ${GRUB_PLATFORM} == "pc"
+GRUB_TARGET_CPU=	i386
+.endif
+PLIST_SUBST+=		GRUB_TARGET_CPU=${GRUB_TARGET_CPU}
+PLIST_SUBST+=		GRUB_PLATFORM=${GRUB_PLATFORM}
+PLIST_VARS+=		efi
+PLIST_VARS+=		pc
+PLIST.${GRUB_PLATFORM}=	yes
