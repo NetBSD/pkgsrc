7b96b0d584a48a178b9c059b741719aa571a043f
adam <adam@pkgsrc.org>
2024-02-07 09:46:36 +0000

    capstone py-capstone: updated to 5.0.1
    Version 5.0.1
    
    [ARM] Fix VFP feature check
    Restore the ARM register naming from v4.
    Use OS independent printf formatting.
    Cherry-pick from next for v5.0.1
    Add Python bindings for WASM
    Sync Python bindings for x86, m68k, and mos65xx
    Add Python bindings for SH
    Update Python binding constants
    Fixing TriCore disasm instructions
    allow absolute CMAKE_INSTALL_*DIR
    
    Version 5.0
    
    [workflows] Remove deprecated image versions
    Fix capstone_test.c
    Support repz prefix on X86 ret instructions
    fix for msvc static lib, dll build error and warnings
    use calloc for cs_insn instead of malloc
    Fix access bug for TEST op
    Update Cython bindings
    Use Intel register syntax for Tricore
    Add # prefix to tricore imm operand
    Revert "Mark cs_* as thread local to avoid race condition in multithreads"
    Replace strncpy with memcpy to fix compiler warning.
    Remove outdated and irrelevant TODO

--- devel/capstone/patches/patch-bindings_python_setup.py
+++ devel/capstone/patches/patch-bindings_python_setup.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-bindings_python_setup.py,v 1.2 2020/08/18 20:20:46 riastradh Exp $
+$NetBSD: patch-bindings_python_setup.py,v 1.3 2024/02/07 09:46:36 adam Exp $
 
---- bindings/python/setup.py.orig	2020-05-08 10:03:30.000000000 +0000
+--- bindings/python/setup.py.orig	2023-08-22 16:03:55.000000000 +0000
 +++ bindings/python/setup.py
-@@ -140,14 +140,6 @@ def build_libraries():
-         # Do not build tests & static library
-         os.system('cmake -DCMAKE_BUILD_TYPE=RELEASE -DCAPSTONE_BUILD_TESTS=0 -DCAPSTONE_BUILD_STATIC=0 -G "NMake Makefiles" ..')
-         os.system("nmake")
--    else:   # Unix incl. cygwin
+@@ -144,14 +144,6 @@ def build_libraries():
+         # Only build capstone.dll
+         os.system('cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCAPSTONE_BUILD_TESTS=OFF -DCAPSTONE_BUILD_CSTOOL=OFF -G "NMake Makefiles" ..')
+         os.system("cmake --build .")
+-    else:  # Unix incl. cygwin
 -        os.system("CAPSTONE_BUILD_CORE_ONLY=yes bash ./make.sh")
 -
 -    shutil.copy(VERSIONED_LIBRARY_FILE, os.path.join(LIBS_DIR, LIBRARY_FILE))
--- devel/capstone/patches/patch-suite_fuzz_driverbin.c
+++ devel/capstone/patches/patch-suite_fuzz_driverbin.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-suite_fuzz_driverbin.c,v 1.1 2022/03/26 19:25:51 tnn Exp $
+$NetBSD: patch-suite_fuzz_driverbin.c,v 1.2 2024/02/07 09:46:36 adam Exp $
 
 no d_type / DT_REG on SunOS, skip check.
 
---- suite/fuzz/driverbin.c.orig	2020-05-08 10:03:30.000000000 +0000
+--- suite/fuzz/driverbin.c.orig	2023-08-22 16:03:55.000000000 +0000
 +++ suite/fuzz/driverbin.c
-@@ -34,9 +34,11 @@ int main(int argc, char** argv)
+@@ -35,9 +35,11 @@ int main(int argc, char** argv)
  
      while((dir = readdir(d)) != NULL) {
          //opens the file, get its size, and reads it into a buffer
@@ -13,6 +13,6 @@ no d_type / DT_REG on SunOS, skip check.
              continue;
          }
 +#endif
- 
-         printf("Running %s\n", dir->d_name);
+         printf("Running file %s ", dir->d_name);
          fflush(stdout);
+         fp = fopen(dir->d_name, "rb");
