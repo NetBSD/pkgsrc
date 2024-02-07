fd05f34bd3f1034cf32cd51616fff456d1bfb095
adam <adam@pkgsrc.org>
2024-02-07 09:56:51 +0000

    py-angr: updated to 9.2.89
    9.2.89
    Bug fixes

--- devel/py-angr/version.mk
+++ devel/py-angr/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.7 2024/01/30 19:24:35 adam Exp $
+# $NetBSD: version.mk,v 1.8 2024/02/07 09:56:52 adam Exp $
 # used by devel/py-ailment/Makefile
 # used by devel/py-angr/Makefile
 # used by devel/py-archinfo/Makefile
@@ -7,4 +7,4 @@
 # used by devel/py-pyvex/buildlink3.mk
 # used by math/py-claripy/Makefile
 
-ANGR_VERSION=	9.2.87
+ANGR_VERSION=	9.2.89
