02e1dc4c31ab39c858546c6322b5dc4555c37740
adam <adam@pkgsrc.org>
2024-02-08 21:33:34 +0000

    zlib: updated to 1.3.1
    Changes in 1.3.1 (22 Jan 2024)
    - Reject overflows of zip header fields in minizip
    - Fix bug in inflateSync() for data held in bit buffer
    - Add LIT_MEM define to use more memory for a small deflate speedup
    - Fix decision on the emission of Zip64 end records in minizip
    - Add bounds checking to ERR_MSG() macro, used by zError()
    - Neutralize zip file traversal attacks in miniunz
    - Fix a bug in ZLIB_DEBUG compiles in check_match()
    - Various portability and appearance improvements
    
    Changes in 1.3 (18 Aug 2023)
    - Remove K&R function definitions and zlib2ansi
    - Fix bug in deflateBound() for level 0 and memLevel 9
    - Fix bug when gzungetc() is used immediately after gzopen()
    - Fix bug when using gzflush() with a very small buffer
    - Fix crash when gzsetparams() attempted for transparent write
    - Fix test/example.c to work with FORCE_STORED
    - Rewrite of zran in examples (see zran.c version history)
    - Fix minizip to allow it to open an empty zip file
    - Fix reading disk number start on zip64 files in minizip
    - Fix logic error in minizip argument processing
    - Add minizip testing to Makefile
    - Read multiple bytes instead of byte-by-byte in minizip unzip.c
    - Add memory sanitizer to configure (--memory)
    - Various portability improvements
    - Various documentation improvements
    - Various spelling and typo corrections

--- devel/zlib/files/zconf.h
+++ devel/zlib/files/zconf.h
@@ -1,9 +1,9 @@
 /* zconf.h -- configuration of the zlib compression library
- * Copyright (C) 1995-2016 Jean-loup Gailly, Mark Adler
+ * Copyright (C) 1995-2024 Jean-loup Gailly, Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-/* @(#) $Id: zconf.h,v 1.7 2022/10/14 07:43:57 wiz Exp $ */
+/* @(#) $Id: zconf.h,v 1.8 2024/02/08 21:33:34 adam Exp $ */
 
 #ifndef ZCONF_H
 #define ZCONF_H
@@ -241,7 +241,11 @@
 #endif
 
 #ifdef Z_SOLO
-   typedef unsigned long z_size_t;
+#  ifdef _WIN64
+     typedef unsigned long long z_size_t;
+#  else
+     typedef unsigned long z_size_t;
+#  endif
 #else
 #  define z_longlong long long
 #  if defined(NO_SIZE_T)
@@ -293,14 +297,6 @@
 #    define OF(args)  args
 #  else
 #    define OF(args)  ()
-#  endif
-#endif
-
-#ifndef Z_ARG /* function prototypes for stdarg */
-#  if defined(STDC) || defined(Z_HAVE_STDARG_H)
-#    define Z_ARG(args)  args
-#  else
-#    define Z_ARG(args)  ()
 #  endif
 #endif
 
@@ -520,7 +516,7 @@ typedef uLong FAR uLongf;
 #if !defined(_WIN32) && defined(Z_LARGE64)
 #  define z_off64_t off64_t
 #else
-#  if defined(_WIN32) && !defined(__GNUC__) && !defined(Z_SOLO)
+#  if defined(_WIN32) && !defined(__GNUC__)
 #    define z_off64_t __int64
 #  else
 #    define z_off64_t z_off_t
