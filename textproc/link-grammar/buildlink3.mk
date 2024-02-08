0ae2fa42f6755e994dae5ffdef782a55705e10d8
gutteridge <gutteridge@pkgsrc.org>
2024-02-08 23:23:36 +0000

    link-grammar: drop argp dependency in bl3 (fix builds)
    argp was dropped as a dependency in 5.10.3. The reference in the
    buildlink3.mk file was causing editors/abiword-plugins to fail to build.

--- textproc/link-grammar/buildlink3.mk
+++ textproc/link-grammar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/08/14 05:25:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/02/08 23:23:36 gutteridge Exp $
 
 BUILDLINK_TREE+=	link-grammar
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.link-grammar+=	link-grammar>=4.3.4
 BUILDLINK_ABI_DEPENDS.link-grammar+=	link-grammar>=5.10.2nb1
 BUILDLINK_PKGSRCDIR.link-grammar?=	../../textproc/link-grammar
 
-.include "../../devel/argp/buildlink3.mk"
 .endif # LINK_GRAMMAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-link-grammar
