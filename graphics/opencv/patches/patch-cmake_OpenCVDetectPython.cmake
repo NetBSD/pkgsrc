ba0d4e0bef78752175a4ce600eb80792860cd791
adam <adam@pkgsrc.org>
2024-02-09 11:50:30 +0000

    opencv: fix for Numpy 1.26 and Pyton 3.12; fix testing

--- graphics/opencv/patches/patch-cmake_OpenCVDetectPython.cmake
+++ graphics/opencv/patches/patch-cmake_OpenCVDetectPython.cmake
@@ -1,9 +1,21 @@
-$NetBSD: patch-cmake_OpenCVDetectPython.cmake,v 1.1 2019/12/16 08:24:39 adam Exp $
+$NetBSD: patch-cmake_OpenCVDetectPython.cmake,v 1.2 2024/02/09 11:50:30 adam Exp $
+
+numpy.distutils is removed in numpy 1.26 on Python 3.12
+https://github.com/opencv/opencv/pull/24351
 
 Do not look for Python 2.x.
 
---- cmake/OpenCVDetectPython.cmake.orig	2019-12-16 06:04:25.000000000 +0000
+--- cmake/OpenCVDetectPython.cmake.orig	2023-06-27 11:29:13.000000000 +0000
 +++ cmake/OpenCVDetectPython.cmake
+@@ -216,7 +216,7 @@ if(NOT ${found})
+           message(STATUS "  PYTHON3_NUMPY_INCLUDE_DIRS")
+         else()
+           # Attempt to discover the NumPy include directory. If this succeeds, then build python API with NumPy
+-          execute_process(COMMAND "${_executable}" -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))"
++          execute_process(COMMAND "${_executable}" -c "import numpy; print(numpy.get_include())"
+                           RESULT_VARIABLE _numpy_process
+                           OUTPUT_VARIABLE _numpy_include_dirs
+                           OUTPUT_STRIP_TRAILING_WHITESPACE)
 @@ -268,13 +268,6 @@ if(OPENCV_PYTHON_SKIP_DETECTION)
    return()
  endif()
