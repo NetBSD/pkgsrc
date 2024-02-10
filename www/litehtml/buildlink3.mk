e00e9a0a51432158237d12f6cb4318e5fdebee20
wiz <wiz@pkgsrc.org>
2024-02-10 11:03:14 +0000

    litehtml: update to 0.9.
    0.9
    
    Improved blocks rendering by @tordex in #268
    Refactor render_item based classes by @tordex in #269
    Discard invalid color declarations by @stasoid in #274
    Add litehtml-specific testing option in cmake by @charliewolf in #282
    Basic implementation for css counters by @MicCalo in #281
    Media_query: Treat unknown media type as none by @imzyxwvu in #283
    Flex layout by @tordex in #285
    
    0.8
    
    fix: render_test._/"acid1.htm" test fails on x86 architecture
    
    0.7
    
    Many changes to improve rendering of inline and block elements.
    Improved rendering performance
    litehtml now passes Acid1 test
    Removed some legacy code
    Refactoring to make code more clean
    Added additional rendering tests

--- www/litehtml/buildlink3.mk
+++ www/litehtml/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/11/30 10:24:35 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/02/10 11:03:14 wiz Exp $
 
 BUILDLINK_TREE+=	litehtml
 
 .if !defined(LITEHTML_BUILDLINK3_MK)
 LITEHTML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.litehtml+=	litehtml>=0.6
+BUILDLINK_API_DEPENDS.litehtml+=	litehtml>=0.9
 BUILDLINK_PKGSRCDIR.litehtml?=		../../www/litehtml
 .endif	# LITEHTML_BUILDLINK3_MK
 
