a45c6e321d9c24d5139321d46e74ab19127e85c3
adam <adam@pkgsrc.org>
2024-02-10 11:01:38 +0000

    libwebsockets: updated to 4.3.3
    4.3.3
    Bug fixes

--- www/libwebsockets/buildlink3.mk
+++ www/libwebsockets/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/10/24 22:11:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/02/10 11:01:38 adam Exp $
 
 BUILDLINK_TREE+=	libwebsockets
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libwebsockets
 LIBWEBSOCKETS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libwebsockets+=	libwebsockets>=1.7.2
-BUILDLINK_ABI_DEPENDS.libwebsockets?=	libwebsockets>=4.3.0nb1
+BUILDLINK_ABI_DEPENDS.libwebsockets+=	libwebsockets>=4.3.0nb1
 BUILDLINK_PKGSRCDIR.libwebsockets?=	../../www/libwebsockets
 
 .include "../../security/openssl/buildlink3.mk"
