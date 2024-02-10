e790e5f74920c80de7be9bdc98d1668fc86d8e53
pin <pin@pkgsrc.org>
2024-02-10 21:46:03 +0000

    devel/cargo-c: update to 0.9.30
     - tvOS support
     - cargo 0.77

--- devel/cargo-c/patches/patch-.._vendor_faster-hex-0.9.0_src_lib.rs
+++ devel/cargo-c/patches/patch-.._vendor_faster-hex-0.9.0_src_lib.rs
@@ -1,9 +1,9 @@
-$NetBSD: patch-.._vendor_faster-hex-0.8.1_src_lib.rs,v 1.1 2023/10/13 07:03:24 pin Exp $
+$NetBSD: patch-.._vendor_faster-hex-0.9.0_src_lib.rs,v 1.1 2024/02/10 21:46:04 pin Exp $
 
 Unify conditions for vectorization_support() use and definition.
 
---- ../vendor/faster-hex-0.8.1/src/lib.rs.orig	2006-07-24 01:21:28.000000000 +0000
-+++ ../vendor/faster-hex-0.8.1/src/lib.rs
+--- ../vendor/faster-hex-0.9.0/src/lib.rs.orig	2024-02-10 21:16:02.053719873 +0000
++++ ../vendor/faster-hex-0.9.0/src/lib.rs
 @@ -43,7 +43,7 @@ pub(crate) enum Vectorization {
  
  #[inline(always)]
