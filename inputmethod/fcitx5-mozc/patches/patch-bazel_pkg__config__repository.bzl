1de4a0e7dbb546ae36054b80da5766146d4b2d51
ryoon <ryoon@pkgsrc.org>
2024-02-10 12:26:00 +0000

    inputmethod/fcitx5-mozc: Update to 2.29.5268.102
    * Sync with Mozc 2.29.5268.102.

---
+++ inputmethod/fcitx5-mozc/patches/patch-bazel_pkg__config__repository.bzl
@@ -0,0 +1,13 @@
+$NetBSD: patch-bazel_pkg__config__repository.bzl,v 1.1 2024/02/10 12:26:01 ryoon Exp $
+
+--- bazel/pkg_config_repository.bzl.orig	2023-12-14 08:51:15.941327119 +0000
++++ bazel/pkg_config_repository.bzl
+@@ -103,7 +103,7 @@ def _pkg_config_repository_impl(repo_ctx
+         "hdrs": _make_strlist([item + "/**" for item in includes]),
+         "copts": _make_strlist(_exec_pkg_config(repo_ctx, "--cflags-only-other")),
+         "includes": _make_strlist(includes),
+-        "linkopts": _make_strlist(_exec_pkg_config(repo_ctx, "--libs-only-l")),
++        "linkopts": _make_strlist(_exec_pkg_config(repo_ctx, "--libs")),
+     }
+     build_file_data = BUILD_TEMPLATE.format(**data)
+ 
--- inputmethod/fcitx5-mozc/patches/patch-session_session__test.cc
+++ inputmethod/fcitx5-mozc/patches/patch-session_session__test.cc
@@ -1,71 +1,31 @@
-$NetBSD: patch-session_session__test.cc,v 1.1 2021/02/17 15:29:51 ryoon Exp $
+$NetBSD: patch-session_session__test.cc,v 1.2 2024/02/10 12:26:02 ryoon Exp $
 
-* NetBSD support
-
---- session/session_test.cc.orig	2021-02-15 03:48:53.000000000 +0000
+--- session/session_test.cc.orig	2023-12-13 09:36:18.413874507 +0000
 +++ session/session_test.cc
-@@ -1974,11 +1974,11 @@ TEST_F(SessionTest, UpdatePreferences) {
+@@ -2011,7 +2011,7 @@ TEST_F(SessionTest, UpdatePreferences) {
    const size_t cascading_cand_size =
        command.output().candidates().candidate_size();
  
--#if defined(OS_LINUX) || defined(OS_ANDROID) || OS_WASM
-+#if defined(OS_LINUX) || defined(OS_ANDROID) || OS_WASM || defined(OS_NETBSD)
-   EXPECT_EQ(no_cascading_cand_size, cascading_cand_size);
--#else   // defined(OS_LINUX) || defined(OS_ANDROID) || OS_WASM
-+#else   // defined(OS_LINUX) || defined(OS_ANDROID) || OS_WASM || defined(OS_NETBSD)
+-#if defined(__linux__) || defined(__wasm__)
++#if defined(__linux__) || defined(__wasm__) || defined(__NetBSD__)
+   EXPECT_EQ(cascading_cand_size, no_cascading_cand_size);
+ #else   // __linux__ || __wasm__
    EXPECT_GT(no_cascading_cand_size, cascading_cand_size);
--#endif  // defined(OS_LINUX) || defined(OS_ANDROID) || OS_WASM
-+#endif  // defined(OS_LINUX) || defined(OS_ANDROID) || OS_WASM || defined(OS_NETBSD)
+@@ -2200,7 +2200,7 @@ TEST_F(SessionTest, OutputAllCandidateWo
  
-   command.Clear();
-   session->ConvertCancel(&command);
-@@ -2149,7 +2149,7 @@ TEST_F(SessionTest, OutputAllCandidateWo
- 
-     EXPECT_EQ(0, output.all_candidate_words().focused_index());
-     EXPECT_EQ(commands::CONVERSION, output.all_candidate_words().category());
--#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_WASM)
-+#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_WASM) || defined(OS_NETBSD)
+     EXPECT_EQ(output.all_candidate_words().focused_index(), 0);
+     EXPECT_EQ(output.all_candidate_words().category(), commands::CONVERSION);
+-#if defined(__linux__) || defined(__wasm__)
++#if defined(__linux__) || defined(__wasm__) || defined(__NetBSD__)
      // Cascading window is not supported on Linux, so the size of
      // candidate words is different from other platform.
      // TODO(komatsu): Modify the client for Linux to explicitly change
-@@ -2159,13 +2159,13 @@ TEST_F(SessionTest, OutputAllCandidateWo
-     //   "ａｉｕｅｏ"  (t13n), "ＡＩＵＥＯ" (t13n), "Ａｉｅｕｏ" (t13n),
-     //   "ｱｲｳｴｵ" (t13n) ]
-     EXPECT_EQ(9, output.all_candidate_words().candidates_size());
--#else   // OS_LINUX || OS_ANDROID || OS_WASM
-+#else   // OS_LINUX || OS_ANDROID || OS_WASM || OS_NETBSD
-     // [ "あいうえお", "アイウエオ", "アイウエオ" (t13n), "あいうえお" (t13n),
-     //   "aiueo" (t13n), "AIUEO" (t13n), "Aieuo" (t13n),
-     //   "ａｉｕｅｏ"  (t13n), "ＡＩＵＥＯ" (t13n), "Ａｉｅｕｏ" (t13n),
-     //   "ｱｲｳｴｵ" (t13n) ]
-     EXPECT_EQ(11, output.all_candidate_words().candidates_size());
--#endif  // OS_LINUX || OS_ANDROID || OS_WASM
-+#endif  // OS_LINUX || OS_ANDROID || OS_WASM || OS_NETBSD
-   }
+@@ -2228,7 +2228,7 @@ TEST_F(SessionTest, OutputAllCandidateWo
  
-   command.Clear();
-@@ -2177,7 +2177,7 @@ TEST_F(SessionTest, OutputAllCandidateWo
- 
-     EXPECT_EQ(1, output.all_candidate_words().focused_index());
-     EXPECT_EQ(commands::CONVERSION, output.all_candidate_words().category());
--#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_WASM)
-+#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_WASM) || defined(OS_NETBSD)
+     EXPECT_EQ(output.all_candidate_words().focused_index(), 1);
+     EXPECT_EQ(output.all_candidate_words().category(), commands::CONVERSION);
+-#if defined(__linux__) || defined(__wasm__)
++#if defined(__linux__) || defined(__wasm__) || defined(__NetBSD__)
      // Cascading window is not supported on Linux, so the size of
      // candidate words is different from other platform.
      // TODO(komatsu): Modify the client for Linux to explicitly change
-@@ -2187,13 +2187,13 @@ TEST_F(SessionTest, OutputAllCandidateWo
-     //   "ａｉｕｅｏ"  (t13n), "ＡＩＵＥＯ" (t13n), "Ａｉｅｕｏ" (t13n),
-     //   "ｱｲｳｴｵ" (t13n) ]
-     EXPECT_EQ(9, output.all_candidate_words().candidates_size());
--#else   // OS_LINUX || OS_ANDROID || OS_WASM
-+#else   // OS_LINUX || OS_ANDROID || OS_WASM || OS_NETBSD
-     // [ "あいうえお", "アイウエオ",
-     //   "aiueo" (t13n), "AIUEO" (t13n), "Aieuo" (t13n),
-     //   "ａｉｕｅｏ"  (t13n), "ＡＩＵＥＯ" (t13n), "Ａｉｅｕｏ" (t13n),
-     //   "ｱｲｳｴｵ" (t13n) ]
-     EXPECT_EQ(11, output.all_candidate_words().candidates_size());
--#endif  // OS_LINUX || OS_ANDROID || OS_WASM
-+#endif  // OS_LINUX || OS_ANDROID || OS_WASM || OS_NETBSD
-   }
- }
- 
