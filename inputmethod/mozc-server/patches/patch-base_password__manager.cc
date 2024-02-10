bba9b737d35b7cf5b40f50a3eca01d3c90f1b9c0
ryoon <ryoon@pkgsrc.org>
2024-02-10 01:17:26 +0000

    mozc: Update to 2.29.5268.102
    * Convert to use devel/bazel as build automation tool.
    * Bazel downloads distfiles during build. It is difficult to avoid
      the downloading because it is performed recursively.
    
    Changelog:
    Not Available.

--- inputmethod/mozc-server/patches/patch-base_password__manager.cc
+++ inputmethod/mozc-server/patches/patch-base_password__manager.cc
@@ -1,15 +1,13 @@
-$NetBSD: patch-base_password__manager.cc,v 1.5 2021/02/15 14:50:23 ryoon Exp $
+$NetBSD: patch-base_password__manager.cc,v 1.6 2024/02/10 01:17:27 ryoon Exp $
 
-* NetBSD support
-
---- base/password_manager.cc.orig	2021-02-15 03:48:53.000000000 +0000
+--- base/password_manager.cc.orig	2023-10-26 12:00:50.000000000 +0000
 +++ base/password_manager.cc
-@@ -264,7 +264,7 @@ bool WinMacPasswordManager::RemovePasswo
+@@ -265,7 +265,7 @@ bool WinMacPasswordManager::RemovePasswo
  // We use plain text file for password storage on Linux. If you port this module
  // to other Linux distro, you might want to implement a new password manager
  // which adopts some secure mechanism such like gnome-keyring.
--#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_WASM)
-+#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_WASM) || defined(OS_NETBSD)
+-#if defined(__linux__) || defined(__wasm__)
++#if defined(__linux__) || defined(__wasm__) || defined(__NetBSD__)
  typedef PlainPasswordManager DefaultPasswordManager;
- #endif  // OS_LINUX || OS_ANDROID || OS_WASM
+ #endif  // __linux__ || __wasm__
  
--- inputmethod/mozc-server/patches/patch-base_port.h
+++ inputmethod/mozc-server/patches/patch-base_port.h
@@ -1,15 +1,21 @@
-$NetBSD: patch-base_port.h,v 1.5 2021/02/15 14:50:23 ryoon Exp $
+$NetBSD: patch-base_port.h,v 1.6 2024/02/10 01:17:27 ryoon Exp $
 
---- base/port.h.orig	2021-02-15 05:04:33.000000000 +0000
+--- base/port.h.orig	2023-10-26 12:00:50.000000000 +0000
 +++ base/port.h
-@@ -69,6 +69,10 @@ using std::string;
- #define MOZC_OS_DEFINED
- #endif  // OS_WASM
+@@ -45,6 +45,7 @@ enum class PlatformType {
+   kIPhone,    // Darwin-based firmware, devices, or simulator
+   kWASM,      // WASM
+   kChromeOS,  // ChromeOS
++  kNetBSD,    // NetBSD
+ };
  
-+#ifdef OS_NETBSD
-+#define MOZC_OS_DEFINED
-+#endif  // OS_NETBSD
-+
- #ifndef MOZC_OS_DEFINED
- #error "OS_XXX (e.g., OS_WIN) must be defined."
- #endif  // !MOZC_OS_DEFINED
+ // kTargetPlatform is the current build target platform.
+@@ -68,6 +69,8 @@ inline constexpr PlatformType kTargetPla
+ #endif                   // !TARGET_OS_IPHONE
+ #elif defined(__wasm__)  // __APPLE__
+ inline constexpr PlatformType kTargetPlatform = PlatformType::kWASM;
++#elif defined(__NetBSD__)
++inline constexpr PlatformType kTargetPlatform = PlatformType::kNetBSD;
+ #else                    // __wasm__
+ #error "Unsupported target platform."
+ #endif  // !__wasm__
