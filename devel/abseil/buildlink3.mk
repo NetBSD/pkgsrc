6337e32fd4485d60007f701bc31e730b79a9ae39
wiz <wiz@pkgsrc.org>
2024-02-10 10:16:05 +0000

    abseil: now requires filesystem, propagate

--- devel/abseil/buildlink3.mk
+++ devel/abseil/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2024/01/30 18:29:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/02/10 10:16:05 wiz Exp $
 
 BUILDLINK_TREE+=	abseil
 
@@ -9,7 +9,7 @@ ABSEIL_BUILDLINK3_MK:=
 # error: #error "This package requires GCC 7 or higher."
 GCC_REQD+=	7
 
-USE_CXX_FEATURES+=	c++14
+USE_CXX_FEATURES+=	c++14 filesystem
 
 BUILDLINK_API_DEPENDS.abseil+=	abseil>=20220623.0
 BUILDLINK_ABI_DEPENDS.abseil?=	abseil>=20240116.0
