c9f7255487d1b5cf2d90ff01ebb91d99d6fc6be3
adam <adam@pkgsrc.org>
2024-02-11 15:42:39 +0000

    cmake: updated to 3.28.3
    CMake 3.28.3
    * Unity: Clarify source comments on unity build transformation
    * zlib: Fix 1.2.13 version and date notice
    * Tests: Add case covering PCH in a unity build
    * cmGlobalGenerator: Add unity/pch sources after computing compile features

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.52 2024/02/01 10:25:02 adam Exp $
+# $NetBSD: version.mk,v 1.53 2024/02/11 15:42:39 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.28.2
+CMAKE_VERSION=	3.28.3
 CMAKE_API=	${CMAKE_VERSION:R}
