3561c6c47cce934144ddb5f44720a0aa83c19e51
ryoon <ryoon@pkgsrc.org>
2024-02-11 23:30:39 +0000

    emulators/bochs: Update to 2.7
    Changelog:
    Brief summary of changes in 2.7:
      - Bugfixes for CPU emulation correctness (CPUID/VMX/SVM fixes to support
        Windows Hyper-V as guest in Bochs)
      - Improvements for the plugin handling to simplify Bochs extensions
      - Added "multiple NICs" support to the NE2000 and E1000 devices
      - Added experimental FTP service for networking modules 'vnet' and 'socket'
      - Fixes and improvements for all supported Voodoo graphics adapters
      - Added USB keyboard emulation with most of the keys supported
      - GUI "cmdmode": create a headerbar event with key combo starting with F7
      - LGPL'd VGABIOS updated to version 0.8a (new VGABIOS for Voodoo Banshee)

--- emulators/bochs/patches/patch-iodev_network_eth__socket.cc
+++ emulators/bochs/patches/patch-iodev_network_eth__socket.cc
@@ -1,22 +1,16 @@
-$NetBSD: patch-iodev_network_eth__socket.cc,v 1.1 2019/01/13 11:59:38 ryoon Exp $
+$NetBSD: patch-iodev_network_eth__socket.cc,v 1.2 2024/02/11 23:30:39 ryoon Exp $
 
---- iodev/network/eth_socket.cc.orig	2017-03-30 18:08:15.000000000 +0000
+--- iodev/network/eth_socket.cc.orig	2021-03-12 21:31:51.747171000 +0000
 +++ iodev/network/eth_socket.cc
-@@ -83,12 +83,14 @@ extern "C" {
- #include <sys/types.h>
+@@ -93,10 +93,10 @@ extern "C" {
  #include <sys/socket.h>
  #include <sys/ioctl.h>
--#include <netpacket/packet.h>
  #include <netinet/in.h>
 -#include <net/ethernet.h>
  #include <net/if.h>
--#include <linux/types.h>
- #include <netdb.h>
-+#if defined(__linux__)
-+#include <netpacket/packet.h>
+ #ifdef __linux__
+ #include <linux/types.h>
 +#include <net/ethernet.h>
-+#include <linux/types.h>
-+#endif
+ #endif
+ #include <netdb.h>
  #define closesocket(s) close(s)
- typedef int SOCKET;
- #ifndef INVALID_SOCKET
