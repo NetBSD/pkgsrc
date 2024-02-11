240b6092d3ebc4efefe059e19ec18cad730ae57e
vins <vins@pkgsrc.org>
2024-02-11 14:47:06 +0000

    got: use devel/got-portable to update to v0.95
    Bring this package in sync with upstream by relying on its portable
    counterpart found at devel/got-portable.
    
    Practically speaking, we're renaming devel/got-portable to devel/got and
    making it supersede the former of the two. The previous devel/got
    package, an independent port of Game of Trees to NetBSD by @ryoon,
    predating the official portable release, gets hereby silently deleted
    along with this commit.
    
    For reference, see:
    https://mail-index.netbsd.org/pkgsrc-users/2024/01/18/msg038795.html

---
+++ devel/got/patches/patch-gotd_log.h
@@ -0,0 +1,14 @@
+$NetBSD: patch-gotd_log.h,v 1.1 2024/02/11 14:47:07 vins Exp $
+
+Provide a declaration of va_list
+
+--- gotd/log.h.orig	2024-01-14 16:07:41.039068602 +0000
++++ gotd/log.h
+@@ -14,6 +14,7 @@
+  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  */
+ 
++#include <stdarg.h>
+ #include "got_compat.h"
+ 
+ void	log_init(int, int);
