569bcf8ec9a10399d5097b81384088c1b62c24ba
vins <vins@pkgsrc.org>
2024-02-11 19:18:09 +0000

    devel/libtirpc: update to release 1.3.4
    # noteworthy changes (since 1.3.3):
    * honor ip_local_reserved_ports
    * allow TCP-only portmapper
    * avoid potential DoS issue by removing unnecessary sleep
    * fix a potential resource leakage
    * fix a possible null pointer dereference

--- devel/libtirpc/buildlink3.mk
+++ devel/libtirpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/09/06 10:42:51 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/02/11 19:18:09 vins Exp $
 
 BUILDLINK_TREE+=		libtirpc
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=		libtirpc
 LIBTIRPC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libtirpc+=	libtirpc>=1.3.3
+BUILDLINK_ABI_DEPENDS.libtirpc+=	libtirpc>=1.3.4
 BUILDLINK_PKGSRCDIR.libtirpc?=		../../devel/libtirpc
 
 BUILDLINK_INCDIRS.libtirpc?=	include/tirpc
