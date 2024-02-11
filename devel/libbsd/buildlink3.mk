19ea548ed27bada2da4a02fea04ac1911a2d2ae6
vins <vins@pkgsrc.org>
2024-02-11 18:58:02 +0000

    devel/libbsd: update to 0.11.8
    # noteworthy changes (since 0.11.7)
    
    - Portability fixes for Solaris, macOS and AIX [needs to be tested on
      pkgsrc, to see wether we can unbreak the package on these platforms]
    - Build fixes for the modern C effort.
    - Adjust declaration shadowing to match new glibc additions.
    - Manual page sync to get rid of the last BSD-4-Clause
      license.
    - Manual pages, documentation and build system
      cleanups.

--- devel/libbsd/buildlink3.mk
+++ devel/libbsd/buildlink3.mk
@@ -1,17 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/12/19 16:05:43 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/02/11 18:58:02 vins Exp $
 
 BUILDLINK_TREE+=	libbsd
 
 .if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.7
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.6
+BUILDLINK_ABI_DEPENDS.libbsd+=	libbsd>=0.11.8
 BUILDLINK_PKGSRCDIR.libbsd?=	../../devel/libbsd
 
+#BUILDLINK_INCDIRS.libbsd?=	include/bsd
 BUILDLINK_CFLAGS.libbsd+=	-I${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
-BUILDLINK_LDFLAGS.libbsd+=	-lbsd -lmd
+BUILDLINK_LIBNAME.libbsd=	bsd
+BUILDLINK_LDADD.libbsd=		${BUILDLINK_LIBNAME.libbsd:S/^/-l/:S/^-l$//}
 
 .include "../../devel/libmd/buildlink3.mk"
+BUILDLINK_LDADD.libbsd+=	${BUILDLINK_LDADD.libmd}
+BUILDLINK_LDFLAGS.libbsd+=	${BUILDLINK_LDADD.libbsd}
+
 .endif	# LIBBSD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbsd
