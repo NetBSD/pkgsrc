496ae7973e1a9bacb345d8de2c2bc020210b761a
mef <mef@pkgsrc.org>
2024-02-12 14:38:27 +0000

    (lang/nqp) Add  two componet on PLIST, update buildlink3.mk
    lang/rakudo wants two componet on @INC path from this package

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2024/01/14 06:13:32 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2024/02/12 14:38:27 mef Exp $
 
 BUILDLINK_TREE+=	nqp
 
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2023.12
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2023.12
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2024.01
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2024.01
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
 .include "../../devel/MoarVM/buildlink3.mk"
