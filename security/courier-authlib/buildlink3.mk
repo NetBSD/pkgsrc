7859dbe48230ae63c9468b78d287e33fef155881
hans <hans@pkgsrc.org>
2024-02-12 17:24:07 +0000

    Update courier-authlib to 0.72.1
    Changes since 0.72.0:
    * Use strncmp instead of memcmp.
    * Various fixes for calls to getgrnam_r/getpwnam_r

--- security/courier-authlib/buildlink3.mk
+++ security/courier-authlib/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.11 2022/06/28 11:35:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/02/12 17:24:07 hans Exp $
 
 BUILDLINK_TREE+=	courier-authlib
 
 .if !defined(COURIER_AUTHLIB_BUILDLINK3_MK)
 COURIER_AUTHLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.courier-authlib+=	courier-authlib>=0.53
-BUILDLINK_ABI_DEPENDS.courier-authlib+=	courier-authlib>=0.69.1nb5
+BUILDLINK_API_DEPENDS.courier-authlib+=	courier-authlib>=0.72
+BUILDLINK_ABI_DEPENDS.courier-authlib+=	courier-authlib>=0.72.1
 BUILDLINK_PKGSRCDIR.courier-authlib?=	../../security/courier-authlib
+
 .endif # COURIER_AUTHLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-courier-authlib
