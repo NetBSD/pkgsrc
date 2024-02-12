e0bc13a9b5dad91e83e46030357eb744e93b3867
hans <hans@pkgsrc.org>
2024-02-12 17:10:22 +0000

    Update courier-unicode to 2.3.0
    Changes since 2.1:
    * Unicode 15 update. Added unicode_derived_<name>() lookup for derived core properties. Added unicode_general_category_lookup() for general category lookups. Fix GB11 rule.
    * configure.ac: gcc 12, autotool updates.
    * Fix compilation error on CentOS 7.
    * unicode_bidi.c: Implement unicode_bidi_combining(). Implement canonical and compatibility decomposition and canonical composition.
    * unicode_bidi.c (unicode_bidi_needs_embed): Make its return code what its documentation says its return code is.
    * Implement unicode_bidi_needs_embed(), unicode_bidi_cleaned_size(), unicode::bidi_override, unicode_bidi_calc and unicode_bidi_calc_levels return a unicode_bidi_direction object, to indicate whether the computer paragraph embedding level was explicitly computed or defaulted.
    * courier-unicode-version.m4: split version checking macro into its own m4 file.
    * unicode::bidi_calc - an empty string with an explicit embedding level should return the requested embedding level.
    * unicode::iconvert::convert, fix uninitialized variable.
    * Implement bidirectional algorithm, canonical or equivalent character mappings.
    * Unicode 13 update. Adds lookup of EastAsianWidth property, emoji property lookup, adds an alternative sequence-based grapheme API that handles multi-character emoji rules.
    * Add bidi property lookups.
    * Makefile.am: Regenerate docs due to broken man page links.
    * unicodebuf.c (unicode_buf_remove): Bug fix.

--- mail/courier-unicode/buildlink3.mk
+++ mail/courier-unicode/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/05/24 08:38:28 hauke Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/02/12 17:10:22 hans Exp $
 
 BUILDLINK_TREE+=	courier-unicode
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	courier-unicode
 COURIER_UNICODE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.courier-unicode+=	courier-unicode>=2.0
-BUILDLINK_ABI_DEPENDS.courier-unicode+=	courier-unicode>=2.1
+BUILDLINK_ABI_DEPENDS.courier-unicode+=	courier-unicode>=2.3
 BUILDLINK_PKGSRCDIR.courier-unicode?=	../../mail/courier-unicode
 
 .endif # COURIER_UNICODE_BUILDLINK3_MK
