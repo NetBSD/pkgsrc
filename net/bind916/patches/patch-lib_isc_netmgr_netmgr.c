e6f56f2d447ea4c511ce753e56afad8f1dc4ee2f
bsiegert <bsiegert@pkgsrc.org>
2024-02-13 16:56:43 +0000

    Pullup ticket #6836 - requested by taca net/bind916: security fix
    Revisions pulled up:
    - net/bind916/Makefile                                          1.67-1.68
    - net/bind916/distinfo                                          1.53
    - net/bind916/patches/patch-lib_dns_rbtdb.c                     1.3
    - net/bind916/patches/patch-lib_isc_netmgr_netmgr.c             1.3
    
    ---
       Module Name:	pkgsrc
       Committed By:	adam
       Date:		Tue Jan 30 18:29:21 UTC 2024
    
       Modified Files:
       	pkgsrc/audio/libilbc: Makefile
       	pkgsrc/audio/termusic: Makefile
       	pkgsrc/biology/plinkseq: Makefile
       	pkgsrc/chat/ekg: Makefile
       	pkgsrc/chat/libgadu: Makefile buildlink3.mk
       	pkgsrc/databases/postgresql-postgis2: Makefile
       	pkgsrc/devel/abseil: buildlink3.mk
       	pkgsrc/devel/compizconfig-backend-gconf: Makefile buildlink3.mk
       	pkgsrc/devel/cre2: Makefile
       	pkgsrc/devel/libcompizconfig: Makefile buildlink3.mk
       	pkgsrc/devel/protobuf: Makefile buildlink3.mk
       	pkgsrc/devel/protobuf-c: Makefile buildlink3.mk
       	pkgsrc/devel/py-compizconfig: Makefile buildlink3.mk
       	pkgsrc/devel/re2: Makefile buildlink3.mk
       	pkgsrc/graphics/opencv: Makefile buildlink3.mk
       	pkgsrc/graphics/opencv-contrib-face: Makefile buildlink3.mk
       	pkgsrc/graphics/py-Willow: Makefile
       	pkgsrc/inputmethod/fcitx5-mozc: Makefile
       	pkgsrc/misc/libreoffice: Makefile
       	pkgsrc/net/bind916: Makefile
       	pkgsrc/net/bind918: Makefile
       	pkgsrc/net/dnsdist: Makefile
       	pkgsrc/net/frr: Makefile
       	pkgsrc/net/grpc: Makefile buildlink3.mk
       	pkgsrc/net/mosh: Makefile
       	pkgsrc/net/py-grpcio: Makefile
       	pkgsrc/net/py-grpcio-tools: Makefile
       	pkgsrc/net/qt6-qtgrpc: Makefile buildlink3.mk
       	pkgsrc/net/ratman: Makefile
       	pkgsrc/net/unbound: Makefile
       	pkgsrc/sysutils/collectd-grpc: Makefile
       	pkgsrc/sysutils/collectd-pinba: Makefile
       	pkgsrc/sysutils/collectd-riemann: Makefile
       	pkgsrc/sysutils/collectd-write_prometheus: Makefile
       	pkgsrc/sysutils/riemann-client: Makefile
       	pkgsrc/wm/ccsm: Makefile
    
       Log Message:
       revbump for devel/abseil
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Tue Feb 13 15:25:33 UTC 2024
    
       Modified Files:
       	pkgsrc/net/bind916: Makefile distinfo
       	pkgsrc/net/bind916/patches: patch-lib_dns_rbtdb.c
       	    patch-lib_isc_netmgr_netmgr.c
    
       Log Message:
       net/bind916: update to 9.16.48
    
       	--- 9.16.48 released ---
    
       6343.	[bug]		Fix case insensitive setting for isc_ht hashtable.
       			[GL #4568]
    
       	--- 9.16.47 released ---
    
       6322.	[security]	Specific DNS answers could cause a denial-of-service
       			condition due to DNS validation taking a long time.
       			(CVE-2023-50387) [GL #4424]
    
       6321.	[security]	Change 6315 inadvertently introduced regressions that
       			could cause named to crash. [GL #4234]
    
       	--- 9.16.46 released ---
    
       6319.	[security]	Query patterns that continuously triggered cache
       			database maintenance could exhaust all available memory
       			on the host running named. (CVE-2023-6516) [GL #4383]
    
       6317.	[security]	Restore DNS64 state when handling a serve-stale timeout.
       			(CVE-2023-5679) [GL #4334]
    
       6316.	[security]	Specific queries could trigger an assertion check with
       			nxdomain-redirect enabled. (CVE-2023-5517) [GL #4281]
    
       6315.	[security]	Speed up parsing of DNS messages with many different
       			names. (CVE-2023-4408) [GL #4234]
    
       6314.	[bug]		Address race conditions in dns_tsigkey_find().
       			[GL #4182]
    
       6304.	[bug]		The wrong time was being used to determine what RRSIGs
       			where to be generated when dnssec-policy was in use.
       			[GL #4494]
    
       6282.	[func]		Deprecate AES-based DNS cookies. [GL #4421]

--- net/bind916/patches/patch-lib_isc_netmgr_netmgr.c
+++ net/bind916/patches/patch-lib_isc_netmgr_netmgr.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-lib_isc_netmgr_netmgr.c,v 1.2 2021/10/24 06:40:28 taca Exp $
+$NetBSD: patch-lib_isc_netmgr_netmgr.c,v 1.2.18.1 2024/02/13 16:56:43 bsiegert Exp $
 
 * Take from NetBSD base.
 
---- lib/isc/netmgr/netmgr.c.orig	2021-09-07 09:37:05.000000000 +0000
+--- lib/isc/netmgr/netmgr.c.orig	2024-02-11 11:31:39.000000000 +0000
 +++ lib/isc/netmgr/netmgr.c
-@@ -373,7 +373,7 @@ isc__netmgr_create(isc_mem_t *mctx, uint
+@@ -358,7 +358,7 @@ isc__netmgr_create(isc_mem_t *mctx, uint
  		mgr->workers_running++;
  		isc_thread_create(nm_thread, &mgr->workers[i], &worker->thread);
  
--		snprintf(name, sizeof(name), "isc-net-%04zu", i);
-+		snprintf(name, sizeof(name), "net-%04zu", i);
+-		snprintf(name, sizeof(name), "isc-net-%04d", i);
++		snprintf(name, sizeof(name), "net-%04d", i);
  		isc_thread_setname(worker->thread, name);
  	}
  
-@@ -1828,6 +1828,13 @@ isc_nmhandle_getdata(isc_nmhandle_t *han
+@@ -1794,6 +1794,13 @@ isc_nmhandle_getdata(isc_nmhandle_t *han
  	return (handle->opaque);
  }
  
