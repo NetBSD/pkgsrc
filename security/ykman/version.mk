7434fe73bdd5c8784fe5e6c0c34c74679c343f13
wiz <wiz@pkgsrc.org>
2024-02-13 17:55:20 +0000

    py-yubikey, ykman: update to 5.3.0
    Version 5.3.0 (released 2024-01-31)
    
    FIDO: Add new CLI commands for PIN management and authenticator config (force-change, set-min-length, toggle-always-uv, enable-ep-attestation).
    PIV: Improve handling of legacy "PUK blocked" flag.
    PIV: Improve handling of malformed certificates.
    PIV: Display key information in "piv info" output on supported devices.
    OTP: Fix some commands incorrectly showing errors when used over NFC/CCID.
    Add tab-completion for YubiKey serial numbers and NFC readers.

--- security/ykman/version.mk
+++ security/ykman/version.mk
@@ -1,6 +1,6 @@
-#	$NetBSD: version.mk,v 1.6 2023/11/05 23:55:59 wiz Exp $
+#	$NetBSD: version.mk,v 1.7 2024/02/13 17:55:20 wiz Exp $
 
 # used by security/py-yubikey-manager/Makefile
 # used by security/ykman/Makefile
 
-VERSION=	5.2.1
+VERSION=	5.3.0
