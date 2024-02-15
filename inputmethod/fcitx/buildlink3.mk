b5365858c87fccfbe28f5486dc3d0e208a13d73d
nia <nia@pkgsrc.org>
2024-02-15 18:19:45 +0000

    fcitx: requires a C++ implementation new enough to have charconv

--- inputmethod/fcitx/buildlink3.mk
+++ inputmethod/fcitx/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/08/18 01:11:16 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/02/15 18:19:45 nia Exp $
 
 BUILDLINK_TREE+=	fcitx
 
 .if !defined(FCITX_BUILDLINK3_MK)
 FCITX_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	charconv
 
 BUILDLINK_API_DEPENDS.fcitx+=	fcitx>=4.2.9.6
 BUILDLINK_PKGSRCDIR.fcitx?=	../../inputmethod/fcitx
