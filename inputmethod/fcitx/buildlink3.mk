627d38bd58bbe949153a99a20769a7a4e3fe4110
nia <nia@pkgsrc.org>
2024-02-15 18:20:15 +0000

    revert previous, committed to wrong package

--- inputmethod/fcitx/buildlink3.mk
+++ inputmethod/fcitx/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/02/15 18:19:45 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/02/15 18:20:15 nia Exp $
 
 BUILDLINK_TREE+=	fcitx
 
 .if !defined(FCITX_BUILDLINK3_MK)
 FCITX_BUILDLINK3_MK:=
-
-USE_CXX_FEATURES+=	charconv
 
 BUILDLINK_API_DEPENDS.fcitx+=	fcitx>=4.2.9.6
 BUILDLINK_PKGSRCDIR.fcitx?=	../../inputmethod/fcitx
