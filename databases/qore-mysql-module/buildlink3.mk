560008594b6c5ed7b19d5ffd793d376d69df0861
nros <nros@pkgsrc.org>
2024-02-16 20:31:12 +0000

    revbump due tp lang/qore update

--- databases/qore-mysql-module/buildlink3.mk
+++ databases/qore-mysql-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/10/24 22:08:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2024/02/16 20:31:12 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-mysql-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-mysql-module
 QORE_MYSQL_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-mysql-module+=	qore-mysql-module>=2.0.1
-BUILDLINK_ABI_DEPENDS.qore-mysql-module?=	qore-mysql-module>=2.1nb7
+BUILDLINK_ABI_DEPENDS.qore-mysql-module?=	qore-mysql-module>=2.1nb8
 BUILDLINK_PKGSRCDIR.qore-mysql-module?=		../../databases/qore-mysql-module
 
 .include "../../lang/qore/buildlink3.mk"
