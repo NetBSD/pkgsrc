a3dd9554b9cf76c2df116624959a7170df2861ee
taca <taca@pkgsrc.org>
2024-02-16 13:15:19 +0000

    lang/php83: update to 8.3.3
    15 Feb 2024, PHP 8.3.3
    
    - Core:
      . Fixed timer leak in zend-max-execution-timers builds. (withinboredom)
      . Fixed bug GH-12349 (linking failure on ARM with mold). (Jan Palus)
      . Fixed bug GH-13097 (Anonymous class reference in trigger_error / thrown
        Exception). (nielsdos)
      . Fixed bug GH-13177 (PHP 8.3.2: final private constructor not allowed
        when used in trait). (nielsdos)
      . Fixed bug GH-13215 (GCC 14 build failure). (Remi)
    
    - Curl:
      . Fix missing error check in curl_multi_init(). (divinity76)
    
    - FPM:
      . Fixed bug GH-12996 (Incorrect SCRIPT_NAME with Apache ProxyPassMatch when
        plus in path). (Jakub Zelenka)
    
    - GD:
      . Fixed bug GH-10344 (imagettfbbox(): Could not find/open font UNC path).
        (nielsdos)
      . Fixed bug GH-10614 (imagerotate will turn the picture all black, when
        rotated 90). (nielsdos)
    
    - LibXML:
      . Fix crashes with entity references and predefined entities. (nielsdos)
    
    - MySQLnd:
      . Fixed bug GH-12107 (When running a stored procedure (that returns a result
        set) twice, PHP crashes). (nielsdos)
    
    - Opcache:
      . Fixed bug GH-13145 (strtok() is not comptime). (ilutov)
      . Fixed type inference of range(). (ilutov)
      . Fixed bug GH-13232 (Segmentation fault will be reported when JIT is off but
        JIT_debug is still on). (nielsdos)
    
    - OpenSSL:
      . Fixed LibreSSL undefined reference when OPENSSL_NO_ENGINE not set.
       (David Carlier).
    
    - PDO_Firebird:
      . Fix GH-13119 (Changed to convert float and double values ​​into strings using
        `H` format). (SakiTakamachi)
    
    - Phar:
      . Fixed bug #71465 (PHAR doesn't know about litespeed). (nielsdos)
      . Fixed bug GH-13037 (PharData incorrectly extracts zip file). (nielsdos)
    
    - Random:
      . Fixed bug GH-13138 (Randomizer::pickArrayKeys() does not detect broken
        engines). (timwolla)
    
    - Session:
      . Fixed bug GH-12504 (Corrupted session written when there's a fatal error
        in autoloader). (nielsdos)
    
    - Standard:
      . Fixed bug GH-13094 (range(9.9, '0') causes segmentation fault). (nielsdos)
    
    - Streams:
      . Fixed bug GH-13071 (Copying large files using mmap-able source streams may
        exhaust available memory and fail). (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.421 2024/01/21 07:56:15 taca Exp $
+# $NetBSD: phpversion.mk,v 1.422 2024/02/16 13:15:19 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -93,7 +93,7 @@ PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.30
 PHP81_VERSION=	8.1.27
 PHP82_VERSION=	8.2.15
-PHP83_VERSION=	8.3.2
+PHP83_VERSION=	8.3.3
 
 # Define API version or initial release of major version.
 PHP56_RELDATE=	20140828
