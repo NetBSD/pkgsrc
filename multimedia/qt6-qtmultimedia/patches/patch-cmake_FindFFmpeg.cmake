50a040b1414d2f2f8650270c4ef2f2bcab1b05e1
adam <adam@pkgsrc.org>
2024-02-16 17:39:03 +0000

    qt6: updated to 6.6.2
    We have released Qt 6.6.2 today. As a patch release, Qt 6.6.2 does not introduce any new features but contains more than 400 bug fixes, security updates, and other improvements to the top of the Qt 6.6.1 release. See more information about the most important changes and bug fixes from Qt 6.6.2 release note.
    
    https://www.qt.io/blog/qt-6.6.2-released

--- multimedia/qt6-qtmultimedia/patches/patch-cmake_FindFFmpeg.cmake
+++ multimedia/qt6-qtmultimedia/patches/patch-cmake_FindFFmpeg.cmake
@@ -1,17 +1,17 @@
-$NetBSD: patch-cmake_FindFFmpeg.cmake,v 1.1 2023/03/18 13:14:16 nros Exp $
+$NetBSD: patch-cmake_FindFFmpeg.cmake,v 1.2 2024/02/16 17:39:05 adam Exp $
 
 * --exclude-libs is not available on Solaris/Illumos, according to
   the git commit messages for this file about exlude-libs it was
   added relocation problems on Linux so only use it on Linux
 
---- cmake/FindFFmpeg.cmake.orig	2023-03-18 11:53:01.741033419 +0000
+--- cmake/FindFFmpeg.cmake.orig	2024-02-10 00:08:32.000000000 +0000
 +++ cmake/FindFFmpeg.cmake
-@@ -247,7 +247,7 @@ endfunction()
-             __ffmpeg_internal_set_dependencies(${_lowerComponent})
+@@ -288,7 +288,7 @@ endfunction()
+           __ffmpeg_internal_set_dependencies(${_lowerComponent})
          endif()
-         target_link_libraries(FFmpeg::${_lowerComponent} INTERFACE "${${_component}_LIBRARY}")
+         target_link_libraries(FFmpeg::${_lowerComponent} INTERFACE "${${_component}_LIBRARY_NAME}")
 -        if (UNIX AND NOT APPLE)
 +        if (LINUX)
            target_link_options(FFmpeg::${_lowerComponent} INTERFACE  "-Wl,--exclude-libs=lib${_lowerComponent}")
          endif ()
-     endif()
+       endif()
