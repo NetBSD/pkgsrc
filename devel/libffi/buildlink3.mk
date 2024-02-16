c9d0b641120b3c1316624f0cceab3ecb79a5449f
mef <mef@pkgsrc.org>
2024-02-16 23:50:13 +0000

    (devel/libffi) Revert buildlink3.mk to 2022 version
     - This change was said wrong location, should change python311/Makefile
          http://mail-index.netbsd.org/tech-pkg/2024/02/03/msg028893.html
     - Even above change may not be necessary
          http://mail-index.netbsd.org/tech-pkg/2024/02/12/msg028919.html
     - I connfirmed that on 10.99.10 it does not need this change
     - I don't remember why I had this problem (pyton 3.11.7 requires this change)
    
      Sorry and thanks
    : ----------------------------------------------------------------------

--- devel/libffi/buildlink3.mk
+++ devel/libffi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/02/03 12:17:16 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2024/02/16 23:50:13 mef Exp $
 
 BUILDLINK_TREE+=	libffi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libffi
 LIBFFI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libffi+=	libffi>=1.20
-BUILDLINK_ABI_DEPENDS.libffi+=	libffi>=3.4.4
+BUILDLINK_ABI_DEPENDS.libffi+=	libffi>=3.4.2nb3
 BUILDLINK_PKGSRCDIR.libffi?=	../../devel/libffi
 .endif # LIBFFI_BUILDLINK3_MK
 
