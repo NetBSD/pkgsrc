d6862b1e521cfd531b175727db00973dba2dd31d
adam <adam@pkgsrc.org>
2024-02-17 10:13:19 +0000

    openblas: updated to 0.3.26
    OpenBLAS 0.3.26
    
    general:
    
    improved the version of openblas.pc that is created by the CMAKE build
    fixed a CMAKE-specific build problems on older versions of MacOS
    worked around linking problems on old versions of MacOS
    corrected installation location of the lapacke_mangling header in CMAKE builds
    added type declarations for complex variables to the MSVC-specific parts of the LAPACK header
    significantly sped up ?GESV for small problem sizes by introducing a lower bound for multithreading
    imported additions and corrections from the Reference-LAPACK project:
    added new LAPACK functions for truncated QR with pivoting
    handle miscalculation of minimum work array size in corner cases
    fixed use of uninitialized variables in ?GEDMD and improved inline documentation
    fixed use of uninitialized variables (and consequential failures) in ?BBCSD
    added tests for the recently introduced Dynamic Mode Decomposition functions
    fixed several memory leaks in the LAPACK testsuite
    fixed counting of testsuite results by the Python script
    
    x86-64:
    
    fixed computation of CASUM on SkylakeX and newer targets in the special
    case that AVX512 is not supported by the compiler or operating environment
    fixed potential undefined behaviour in the CASUM/ZASUM kernels for AVX512 targets
    worked around a problem in the pre-AVX kernel for GEMV
    sped up the thread management code on MS Windows
    
    arm64:
    
    fixed building of the LAPACK testsuite with Xcode 15 on Apple M1 and newer
    sped up the thread management code on MS Windows
    sped up SGEMM and DGEMM on Neoverse V1
    sped up ?DOT on SVE-capable targets
    reduced the number of targets in DYNAMIC_ARCH builds by eliminating functionally equivalent ones
    included support for Apple M1 and newer targets in DYNAMIC_ARCH builds
    
    power:
    
    improved the SGEMM kernel for POWER10
    fixed compilation with (very) old versions of gcc
    fixed detection of old 32bit PPC targets in CMAKE-based builds
    added autodetection of the POWERPC 7400 subtype
    fixed CMAKE-based compilation for PPCG4 and PPC970 targets
    
    loongarch64:
    
    added and improved optimized kernels for almost all BLAS functions

--- math/openblas_openmp/buildlink3.mk
+++ math/openblas_openmp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/06/15 04:41:53 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/02/17 10:13:20 adam Exp $
 
 BUILDLINK_TREE+=	openblas_openmp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openblas_openmp
 OPENBLAS_OPENMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openblas_openmp+=	openblas_openmp>=0.3.5
-BUILDLINK_ABI_DEPENDS.openblas_openmp?=	openblas_openmp>=0.3.15nb1
+BUILDLINK_ABI_DEPENDS.openblas_openmp+=	openblas_openmp>=0.3.15nb1
 BUILDLINK_PKGSRCDIR.openblas_openmp?=	../../math/openblas_openmp
 .endif	# OPENBLAS_OPENMP_BUILDLINK3_MK
 
