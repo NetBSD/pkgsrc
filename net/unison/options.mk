3df98987f3c3b2e422383824848b3f5d5087165b
gdt <gdt@pkgsrc.org>
2024-02-19 19:39:00 +0000

    net/unison: Update to 2.53.4
    packaging changes:
      use upstream's 'make install'
      bin/unison is always TUI
      bin/unison-gui is GUI, but installed only if built with lablgtk
    
    ## Changes in 2.53.4
    
    Released 2024-02-17
    
      * Improved and simplified makefiles. Please review your build scripts
        for changed build goals, input variables and build artifact names.
        See INSTALL.md for details.
      * opam build uses the makefile build system.
      * Various fixes in Windows and Cygwin builds.
      * Preferences "force", "prefer" and related "partial" preferences now
        work slightly differently with values "newer" and "older". Previously,
        if mtimes in both replicas were equal then always the second root
        propagated to the first root (possibly reverting user changes). It
        is now made explicit that "newer" and "older" only work when mtimes
        are different.
      * Cleanups in documentation.
      * Bugfixes, minor improvements, cleanups.

--- net/unison/options.mk
+++ net/unison/options.mk
@@ -1,16 +1,17 @@
-# $NetBSD: options.mk,v 1.7 2022/11/08 01:07:09 gdt Exp $
+# $NetBSD: options.mk,v 1.8 2024/02/19 19:39:00 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unison
 PKG_SUPPORTED_OPTIONS=	lablgtk
+# ocaml-cairo is broken
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mlablgtk)
-MAKE_FLAGS+=	UISTYLE=gtk3
+PLIST_VARS+=	gtk3
 
+.if !empty(PKG_OPTIONS:Mlablgtk)
+PLIST.gtk3=	yes
 DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
 
 .include "../../x11/ocaml-lablgtk3/buildlink3.mk"
-.else
-MAKE_FLAGS+=	UISTYLE=text
 .endif
