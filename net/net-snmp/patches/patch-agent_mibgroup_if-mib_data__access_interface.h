40bd5da41d65cbd609f758d0e6c433a8af7d9cc7
wiz <wiz@pkgsrc.org>
2024-02-21 10:21:00 +0000

    *net-snmp: update to 5.9.4
    *5.9.4*:
    
        IMPORTANT: SNMP over TLS and/or DTLS are not functioning properly
        in this release with various versions of OpenSSL and will be fixed
        in a future release.
    
        libsnmp:
          - Remove the SNMP_SWIPE_MEM() macro Remove this macro since it is not
    	used in the Net-SNMP code base.
          - DISPLAY-HINT fixes
          - Miscellanious improvements to the transports
          - Handle multiple oldEngineID configuration lines
          - fixes for DNS names longer than 63 characters
    
        agent:
          - Added a ignoremount configuration option for the HOST-MIB
          - disallow SETs with a NULL varbind
          - fix the --enable-minimalist build
    
        apps:
          - snmpset: allow SET with NULL varbind for testing
          - snmptrapd: improved MySQL logging code
    
        general:
          - configure: Remove -Wno-deprecated as it is no longer needed
          - miscellanious ther bug fixes, build fixes and cleanups

--- net/net-snmp/patches/patch-agent_mibgroup_if-mib_data__access_interface.h
+++ net/net-snmp/patches/patch-agent_mibgroup_if-mib_data__access_interface.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-agent_mibgroup_if-mib_data__access_interface.h,v 1.1 2022/10/18 12:01:52 adam Exp $
+$NetBSD: patch-agent_mibgroup_if-mib_data__access_interface.h,v 1.2 2024/02/21 10:21:01 wiz Exp $
 
 Improve portability to newer NetBSD versions.
 
---- agent/mibgroup/if-mib/data_access/interface.h.orig	2022-07-13 21:14:14.000000000 +0000
+--- agent/mibgroup/if-mib/data_access/interface.h.orig	2023-08-15 20:32:01.000000000 +0000
 +++ agent/mibgroup/if-mib/data_access/interface.h
 @@ -34,7 +34,8 @@ config_require(if-mib/data_access/interf
- config_require(if-mib/data_access/interface_ioctl)
+ config_require(if-mib/data_access/interface_ioctl);
  #elif defined( openbsd3 ) ||                                         \
      defined( freebsd4 ) || defined( freebsd5 ) || defined( freebsd6 ) || \
 -    defined( darwin )   || defined( dragonfly ) || defined( netbsd1 )
 +    defined( darwin )   || defined( dragonfly ) || defined( netbsd1 ) || \
 +    defined(netbsd) || defined(netbsdelf) || defined(netbsdelf2) || defined(netbsdelf3)
- config_require(if-mib/data_access/interface_sysctl)
+ config_require(if-mib/data_access/interface_sysctl);
  #elif defined(solaris2)
- config_require(if-mib/data_access/interface_solaris2)
+ config_require(if-mib/data_access/interface_solaris2);
--- net/net-snmp/patches/patch-agent_mibgroup_mibII_var__route.c
+++ net/net-snmp/patches/patch-agent_mibgroup_mibII_var__route.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-agent_mibgroup_mibII_var__route.c,v 1.1 2022/10/18 12:01:52 adam Exp $
+$NetBSD: patch-agent_mibgroup_mibII_var__route.c,v 1.2 2024/02/21 10:21:01 wiz Exp $
 
 Fix for systems with RTF_LLDATA in place of RTF_LLINFO.
 
---- agent/mibgroup/mibII/var_route.c.orig	2016-04-29 19:35:23.000000000 +0000
+--- agent/mibgroup/mibII/var_route.c.orig	2023-08-15 20:32:01.000000000 +0000
 +++ agent/mibgroup/mibII/var_route.c
-@@ -954,8 +954,12 @@ load_rtentries(struct radix_node *pt)
+@@ -956,8 +956,12 @@ load_rtentries(struct radix_node *pt)
          }
- #if CHECK_RT_FLAGS
+ #ifdef CHECK_RT_FLAGS
          if (((rt.rt_flags & RTF_CLONING) != RTF_CLONING)
 +#ifdef RTF_LLDATA
 +            && ((rt.rt_flags & RTF_LLDATA) != RTF_LLDATA)) {
