ff4640a6159e09ebc542b751103a72900ffdbef0
wiz <wiz@pkgsrc.org>
2024-02-21 10:26:25 +0000

    erlang: update to 26.2.2.
    Erlang/OTP 26.2 is the second maintenance patch package for OTP
    26, with mostly bug fixes as well as improvements.
    
    Highlights #
    
        process_info/2 now supports lookup of values for specific keys in the process dictionary.
    
    Potential incompatibilities: #
    
        common_test now returns an error when a suite with a badly defined group is executed.

--- lang/erlang/versions.mk
+++ lang/erlang/versions.mk
@@ -1,3 +1,3 @@
-# $NetBSD: versions.mk,v 1.36 2023/12/20 18:46:53 wiz Exp $
+# $NetBSD: versions.mk,v 1.37 2024/02/21 10:26:25 wiz Exp $
 # Do not edit, regenerate using 'make versions'
-_VERSIONS=	erts 14.2.1 ssh 5.1.1 asn1 5.2.1 common_test 1.26 compiler 8.4.1 crypto 5.4 debugger 5.3.3 dialyzer 5.1.2 diameter 2.3.1 edoc 1.2.1 eldap 1.2.12 erl_docgen 1.5.2 erl_interface 5.5 et 1.7 eunit 2.9 ftp 1.2.1 inets 9.1 jinterface 1.14 kernel 9.2 megaco 4.5 mnesia 4.23 observer 2.15.1 odbc 2.14.1 os_mon 2.9.1 parsetools 2.5 public_key 1.15 reltool 1.0 runtime_tools 2.0.1 sasl 4.2.1 snmp 5.15 ssl 11.1 stdlib 5.2 syntax_tools 3.1 tftp 1.1.1 tools 3.6 wx 2.4 xmerl 1.3.34
+_VERSIONS=	common_test 1.26.1 erl_interface 5.5.1 erts 14.2.2 kernel 9.2.1 ssh 5.1.2 ssl 11.1.1 asn1 5.2.1 compiler 8.4.1 crypto 5.4 debugger 5.3.3 dialyzer 5.1.2 diameter 2.3.1 edoc 1.2.1 eldap 1.2.12 erl_docgen 1.5.2 et 1.7 eunit 2.9 ftp 1.2.1 inets 9.1 jinterface 1.14 megaco 4.5 mnesia 4.23 observer 2.15.1 odbc 2.14.1 os_mon 2.9.1 parsetools 2.5 public_key 1.15 reltool 1.0 runtime_tools 2.0.1 sasl 4.2.1 snmp 5.15 stdlib 5.2 syntax_tools 3.1 tftp 1.1.1 tools 3.6 wx 2.4 xmerl 1.3.34
