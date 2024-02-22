b13b883a9043cd1291d638b72b21dac2380a9a1a
ryoon <ryoon@pkgsrc.org>
2024-02-22 15:12:49 +0000

    www/firefox: Update to 123.0
    * For NetBSD, user-agent string reports as Linux.
    
    Changelog:
    New
    
      * We've integrated search into Firefox View. You can now search through all
        of the tabs on each of the section subpages - Recent Browsing, Open Tabs,
        Recently Closed Tabs, Tabs from other devices, or History.
    
      * Having any issues with a website on Firefox, yet the site seems to be
        working as expected on another browser? You can now let us know via the Web
        Compatibility Reporting Tool! By filing a web compatibility issue, you're
        directly helping us detect, target, and fix the most impacted sites to make
        your browsing experience on Firefox smoother.
    
    Fixed
    
      * When translating web pages, we are now also translating text in tooltips
        (i.e. titles) and text displayed in form controls (i.e. placeholder).
    
      * Various security fixes.
    
    Changed
    
      * Address bar settings can now be found in the Firefox Settings' Search
        section.
    
      * Improved PGO optimizations for macOS ARM64 and Android
    
      * Off-main-thread canvas on macOS, Linux and Android
    
    Enterprise
    
      * You can find information about policy updates and enterprise specific bug
        fixes in the Firefox for Enterprise 123 Release Notes.
    
    Developer
    
      * Developer Information
      * The Network Monitor now supports saving a response body to disk through the
        Save Response As context menu item. This replaces the Save Image context
        menu item, which only supported saving images.
    
    Web Platform
    
      * We now implement linearRGB interpolation for SVG gradients, as well as the
        existing sRGB interpolation. You can choose which to use via the
        color-interpolation property.
    
      * Previously, SVG feImage elements that pointed to SVG content required the
        SVG document to have a root element that had non-percentage width and
        height values. Such content will now fall back to use the default replaced
        element values of 300px width and 150px height and the feImage will no
        longer fail to render.
    
      * By enabling Preload and Modulepreload support, Early Hints are now fully
        supported.
    
      * Audio echo cancellation can now be applied to microphone inputs when the
        audio output is redirected to another device with setSinkId().
    
      * Firefox now supports declarative ShadowDOM, providing developers greater
        flexibility and improved ergonomics when working with ShadowDOMs directly
        within HTML.
    
    Security fixes:
    Mozilla Foundation Security Advisory 2024-05
    #CVE-2024-1546: Out-of-bounds memory read in networking channels
    #CVE-2024-1547: Alert dialog could have been spoofed on another site
    #CVE-2024-1554: fetch could be used to effect cache poisoning
    #CVE-2024-1548: Fullscreen Notification could have been hidden by select
     element
    #CVE-2024-1549: Custom cursor could obscure the permission dialog
    #CVE-2024-1550: Mouse cursor re-positioned unexpectedly could have led to
     unintended permission grants
    #CVE-2024-1551: Multipart HTTP Responses would accept the Set-Cookie header in
     response parts
    #CVE-2024-1555: SameSite cookies were not properly respected when opening a
     website from an external browser
    #CVE-2024-1556: Invalid memory access in the built-in profiler
    #CVE-2024-1552: Incorrect code generation on 32-bit ARM devices
    #CVE-2024-1553: Memory safety bugs fixed in Firefox 123, Firefox ESR 115.8, and
     Thunderbird 115.8
    #CVE-2024-1557: Memory safety bugs fixed in Firefox 123

--- www/firefox/patches/patch-js-src-jit-arm64-vixl-MozCpu-vixl.cpp
+++ www/firefox/patches/patch-js-src-jit-arm64-vixl-MozCpu-vixl.cpp
@@ -1,24 +1,15 @@
-$NetBSD: patch-js-src-jit-arm64-vixl-MozCpu-vixl.cpp,v 1.1 2023/08/20 21:41:54 tnn Exp $
+$NetBSD: patch-js-src-jit-arm64-vixl-MozCpu-vixl.cpp,v 1.2 2024/02/22 15:12:50 ryoon Exp $
 
 Add NetBSD support.
 
---- js/src/jit/arm64/vixl/MozCpu-vixl.cpp.orig	2023-08-15 20:31:20.000000000 +0000
+--- js/src/jit/arm64/vixl/MozCpu-vixl.cpp.orig	2024-02-13 14:50:57.324257195 +0000
 +++ js/src/jit/arm64/vixl/MozCpu-vixl.cpp
-@@ -68,7 +68,7 @@ void CPU::SetUp() {
- 
- 
- uint32_t CPU::GetCacheType() {
--#if defined(__aarch64__) && (defined(__linux__) || defined(__android__))
-+#if defined(__aarch64__) && (defined(__linux__) || defined(__android__) || defined(__NetBSD__))
-   uint64_t cache_type_register;
-   // Copy the content of the cache type register to a core register.
-   __asm__ __volatile__ ("mrs %[ctr], ctr_el0"  // NOLINT
 @@ -110,7 +110,7 @@ void CPU::EnsureIAndDCacheCoherency(void
    FlushInstructionCache(GetCurrentProcess(), address, length);
  #elif defined(XP_DARWIN)
    sys_icache_invalidate(address, length);
--#elif defined(__aarch64__) && (defined(__linux__) || defined(__android__))
-+#elif defined(__aarch64__) && (defined(__linux__) || defined(__android__) || defined(__NetBSD__))
+-#elif defined(__aarch64__) && (defined(__linux__) || defined(__android__) || defined(__FreeBSD__))
++#elif defined(__aarch64__) && (defined(__linux__) || defined(__android__) || defined(__FreeBSD__) || defined(__NetBSD__))
    // Implement the cache synchronisation for all targets where AArch64 is the
    // host, even if we're building the simulator for an AAarch64 host. This
    // allows for cases where the user wants to simulate code as well as run it
