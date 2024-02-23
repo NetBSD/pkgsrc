922ff0e84393708c0979ecec427f0d18ba87a204
tsutsui <tsutsui@pkgsrc.org>
2024-02-23 11:25:28 +0000

    ruby-gnome: update to 4.2.1.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.2.1
    
    Ruby-GNOME 4.2.1: 2024-02-23
    
    This is a minor bug fix release.
    
    Changes
    
    Ruby/GLib2
    
      * Improvements
    
          + Added support for showing GType name for anonymous GType
            based classes.
              o GH-1577
              o Reported by Sami Sieranoja
    
          + Added RG_DEF_SALIAS().
    
          + Stopped providing unusable signal related methods to not signal
            ready objects such as GLib::Instantiatable.
              o GH-1589
              o Reported by Sami Sieranoja
    
          + Added GLib::Type::INVALID. GLib::Type[nil] is also accepted.
              o GH-1594
              o Reported by Sami Sieranoja
    
          + Added GLib::TimeZone#identifier.
    
          + Added GLib::TimeZone.try_convert.
    
    Ruby/GObjectIntrospection
    
      * Improvements
    
          + Ignored .hash(some_arguments) methods. It may break something.
    
          + Ignored fundamental GType.
              o GH-1580
              o Reported by Sami Sieranoja
    
          + Added support for returning sized GType array.
    
    Ruby/GDK4
    
      * Fixes
    
          + Fixed a GC related bug of Gdk::MemoryTexture.new.
              o GH-1596
              o Reported by HuBandiT
    
          + Fixed a GC related bug of Gdk::Picture.
              o GH-1596
              o Reported by HuBandiT
    
    Ruby/GTK4
    
      * Improvements
    
          + Added a menu bar sample.
              o GH-1581
              o GH-1582
              o GH-1584
              o Patch by Sami Sieranoja
    
          + Added support for Gtk::DropTarget.new([type, ...], actions).
              o GH-1594
              o Reported by Sami Sieranoja
    
      * Fixes
    
          + Fixed deprecation message of Gtk::Button.new.
              o GH-1587
              o Reported by Sami Sieranoja
    
          + Fixed a GC related bug of Gtk::Image.
    
          + Fixed a GC related bug of Gtk::ColumnView.
              o GH-1600
              o Reported by HuBandiT
    
          + Fixed a GC related bug of Gtk::SingleSelection.
              o GH-1600
              o Reported by HuBandiT
    
    Ruby/GStreamer
    
      * Improvements
    
          + Added a missing GC guard for Gst::Element#bus.
              o GH-1586
              o Reported by Sami Sieranoja
    
    Thanks
    
      * Sami Sieranoja
      * HuBandiT

--- devel/ruby-glib2/buildlink3.mk
+++ devel/ruby-glib2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/09/03 10:22:12 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2024/02/23 11:25:28 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-glib2
 
 .if !defined(RUBY_GLIB2_BUILDLINK3_MK)
 RUBY_GLIB2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.0
-BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.0
+BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.1
+BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.1
 BUILDLINK_PKGSRCDIR.ruby-glib2?=	../../devel/ruby-glib2
 
 .include "../../devel/glib2/buildlink3.mk"
