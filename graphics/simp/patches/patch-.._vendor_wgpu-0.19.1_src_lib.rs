155b1933c87cab7bc870b06ee3dd8056b5fe3d17
pin <pin@pkgsrc.org>
2024-02-23 10:09:12 +0000

    graphics/simp: update to 3.5.1
     - Update wgpu to v0.19
     - Update egui to v0.26
     - Update resvg to v0.40
     - Enable mip maps on OpenGL
    
    NOTE: As of version 3.5.0, we need to set the environment variable
    WGPU_BACKEND to 'gl', else simp will segfault when launching.

---
+++ graphics/simp/patches/patch-.._vendor_wgpu-0.19.1_src_lib.rs
@@ -0,0 +1,26 @@
+$NetBSD: patch-.._vendor_wgpu-0.19.1_src_lib.rs,v 1.1 2024/02/23 10:09:12 pin Exp $
+
+Apply https://github.com/gfx-rs/wgpu/pull/5166
+
+--- ../vendor/wgpu-0.19.1/src/lib.rs.orig	2024-02-21 19:24:28.317001076 +0000
++++ ../vendor/wgpu-0.19.1/src/lib.rs
+@@ -2812,7 +2812,7 @@ pub struct CreateSurfaceError {
+ enum CreateSurfaceErrorKind {
+     /// Error from [`wgpu_hal`].
+     #[cfg(wgpu_core)]
+-    Hal(hal::InstanceError),
++    Hal(wgc::instance::CreateSurfaceError),
+ 
+     /// Error from WebGPU surface creation.
+     #[allow(dead_code)] // may be unused depending on target and features
+@@ -2847,8 +2847,8 @@ impl error::Error for CreateSurfaceError
+ }
+ 
+ #[cfg(wgpu_core)]
+-impl From<hal::InstanceError> for CreateSurfaceError {
+-    fn from(e: hal::InstanceError) -> Self {
++impl From<wgc::instance::CreateSurfaceError> for CreateSurfaceError {
++    fn from(e: wgc::instance::CreateSurfaceError) -> Self {
+         Self {
+             inner: CreateSurfaceErrorKind::Hal(e),
+         }
