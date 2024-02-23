d13a3e12a92f51ac1be056f5368260cdf2507c06
adam <adam@pkgsrc.org>
2024-02-23 07:56:02 +0000

    py-virtualenv: updated to 20.25.1
    Bugfixes - 20.25.1
    Upgrade embedded wheels:
    setuptools to 69.0.3 from 69.0.2
    pip to 23.3.2 from 23.3.1
    Upgrade embedded wheels:
    pip 23.3.2 to 24.0`,
    setuptools 69.0.3 to 69.1.0.

--- devel/py-virtualenv/patches/patch-src_virtualenv_create_via__global__ref_api.py
+++ devel/py-virtualenv/patches/patch-src_virtualenv_create_via__global__ref_api.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_virtualenv_create_via__global__ref_api.py,v 1.4 2023/06/26 16:13:22 adam Exp $
+$NetBSD: patch-src_virtualenv_create_via__global__ref_api.py,v 1.5 2024/02/23 07:56:03 adam Exp $
 
 Copy distutils distro config file to virtualenv.
 https://github.com/pypa/virtualenv/issues/2313
@@ -7,14 +7,14 @@ https://github.com/pypa/virtualenv/issues/2313
 +++ src/virtualenv/create/via_global_ref/api.py
 @@ -4,6 +4,8 @@ import logging
  import os
- from abc import ABCMeta
+ from abc import ABC
  from pathlib import Path
 +from shutil import copy
 +from sysconfig import get_paths
  
  from virtualenv.create.creator import Creator, CreatorMeta
  from virtualenv.info import fs_supports_symlink
-@@ -94,6 +96,10 @@ class ViaGlobalRefApi(Creator, metaclass
+@@ -94,6 +96,10 @@ class ViaGlobalRefApi(Creator, ABC):
              logging.debug("create %s", dest_path)
              dest_path.write_text(text, encoding="utf-8")
  
