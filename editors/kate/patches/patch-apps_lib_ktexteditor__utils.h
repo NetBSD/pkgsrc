9da7572be08c1861f2fa0a709ab9b0b28ba346c2
gutteridge <gutteridge@pkgsrc.org>
2024-02-23 04:59:00 +0000

    kate: add upstream PR to patches
    It turns out this was already reported and patches merged (that are the
    same as mine), so on the next update this will be resolved.

--- editors/kate/patches/patch-apps_lib_ktexteditor__utils.h
+++ editors/kate/patches/patch-apps_lib_ktexteditor__utils.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-apps_lib_ktexteditor__utils.h,v 1.1 2024/02/23 04:00:03 gutteridge Exp $
+$NetBSD: patch-apps_lib_ktexteditor__utils.h,v 1.2 2024/02/23 04:59:00 gutteridge Exp $
 
 Fix non-PCH-enabled build by adding missing includes.
+https://invent.kde.org/utilities/kate/-/issues/116
 
 --- apps/lib/ktexteditor_utils.h.orig	2023-12-04 03:09:43.000000000 +0000
 +++ apps/lib/ktexteditor_utils.h
