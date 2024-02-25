1efdaccec9e5982c17deeefecbfe76bbc84a0db7
wiz <wiz@pkgsrc.org>
2024-02-25 15:08:50 +0000

    pciutils: update to 3.11.1.
    2024-02-25  Martin Mares <mj@ucw.cz>
    
    	* Released as 3.11.1.
    
    	* Fixed wrong API version in lib/pci.h.
    
    	* Updated README.Windows.
    
    	* Fix compilation on Windows.
    
    2024-02-24  Martin Mares <mj@ucw.cz>
    
    	* Released as 3.11.0.
    
    	* update-pciids now supports XZ compression. If libpci is configured
    	  with support for compression, all downloaded files are recompressed
    	  as gzip. Otherwise they are stored as plain text.
    
    	* update-pciids now sends itself as the User-Agent.
    
    	* Added a pcilmr utility for PCIe lane margining. Thanks to Nikita
    	  Proshkin for contributing it.
    
    	* Re-factored access to i386 ports on all relevant platforms.
    
    	* Added i386 port access on OpenBSD.
    
    	* Back-ends for Windows received many bug fixes and improvements.
    
    	* ECAM back-end now scans ACPI and BIOS memory faster.
    
    	* Linux systems without pread/pwrite are no longer supported
    	  as they are hopefully long gone. This helps avoid the tricky check
    	  for presence of pread which was found to fail on musl libc.
    
    	* Improved decoding of PCIe control and status registers.
    
    	* Decoding of CXL capabilities now supports up to CXL 3.0.
    
    	* lspci now displays interrupt message numbers consistently across
    	  different capabilities.
    
    	* Cache of IDs resolved via DNS, which was located in ~/.pci-ids
    	  by default, is now stored according to the XDG base directory
    	  specification in $XDG_CACHE_HOME/pci-ids.
    
    	* All source files now have SPDX license identifiers.
    
    	* Internal: The "aux" fields of structs pci_access and pci_dev
    	  reserved for use by back-ends were renamed to backend_data to better
    	  reflect their meaning.
    
    	* As usually, various minor bug fixes and updated pci.ids.

--- sysutils/pciutils/patches/patch-lib_internal.h
+++ sysutils/pciutils/patches/patch-lib_internal.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-lib_internal.h,v 1.3 2023/05/07 10:20:44 wiz Exp $
+$NetBSD: patch-lib_internal.h,v 1.4 2024/02/25 15:08:50 wiz Exp $
 
 no symbol versioning available on SunOS
 
---- lib/internal.h.orig	2023-03-05 13:45:24.000000000 +0000
+--- lib/internal.h.orig	2024-02-24 19:47:05.000000000 +0000
 +++ lib/internal.h
-@@ -18,6 +18,17 @@
- #define STATIC_ALIAS(_decl, _for) _decl VERSIONED_ABI { return _for; }
+@@ -23,6 +23,17 @@
+ #define STATIC_ALIAS(_decl, _for) VERSIONED_ABI _decl { return _for; }
  #define DEFINE_ALIAS(_decl, _for)
  #define SYMBOL_VERSION(_int, _ext)
 +#elif defined(__sun__)
@@ -20,5 +20,5 @@ no symbol versioning available on SunOS
 +#define pci_filter_parse_id_v38 pci_filter_parse_id
 +#define pci_filter_match_v38 pci_filter_match
  #else
- #define STATIC_ALIAS(_decl, _for)
  #define DEFINE_ALIAS(_decl, _for) extern _decl __attribute__((alias(#_for))) VERSIONED_ABI
+ #ifdef _WIN32
