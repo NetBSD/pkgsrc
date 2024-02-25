b0e43ca9cbb3bb7f1d53e0cdb29ebb9d9a0c6ea6
wiz <wiz@pkgsrc.org>
2024-02-25 15:02:42 +0000

    *: recursive bump for botan3

--- security/botan3/buildlink3.mk
+++ security/botan3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/12/29 18:24:57 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/02/25 15:02:42 wiz Exp $
 
 BUILDLINK_TREE+=	botan3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	botan3
 BOTAN3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.botan3+=	botan3>=3.0.0
-BUILDLINK_ABI_DEPENDS.botan3?=	botan3>=3.2.0nb2
+BUILDLINK_ABI_DEPENDS.botan3?=	botan3>=3.3.0
 BUILDLINK_PKGSRCDIR.botan3?=	../../security/botan3
 
 .include "../../devel/boost-libs/buildlink3.mk"
