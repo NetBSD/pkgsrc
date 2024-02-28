ae4e2a33009f2d3a940a6fcca4f7a7155bcb973a
taca <taca@pkgsrc.org>
2024-02-28 16:41:42 +0000

    time/php-mrbs: update to 1.11.4
    * Reset MAINTAINER by requested from previous MAINTAINER.
    * Overhaul package.
    
    Changes from 1.4.11 to 1.11.4 are too many to write here, please refer NEWS
    file.

--- time/php-mrbs/options.mk
+++ time/php-mrbs/options.mk
@@ -1,22 +1,18 @@
-# $NetBSD: options.mk,v 1.4 2019/11/02 16:28:41 rillig Exp $
+# $NetBSD: options.mk,v 1.5 2024/02/28 16:41:42 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.php-mrbs
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
 PKG_OPTIONS_GROUP.db=		mysql pgsql
-
-PKG_SUGGESTED_OPTIONS=	mysql
+PKG_SUGGESTED_OPTIONS=		mysql
 
 .include "../../mk/bsd.options.mk"
 
 ###
 ### Use mysql, or pgsql backend
 ###
-.if !empty(PKG_OPTIONS:Mmysql)
-DEPENDS+=		${PHP_PKG_PREFIX}-mysql>=5.2.0:../../databases/php-mysql
-PHP_VERSIONS_ACCEPTED=	56
-.elif !empty(PKG_OPTIONS:Mpgsql)
-DEPENDS+=		${PHP_PKG_PREFIX}-pgsql>=5.2.0:../../databases/php-pgsql
-.else
-PKG_FAIL_REASON+=	${PKGNAME} needs either mysql or pgsql
+.if ${PKG_OPTIONS:Mmysql}
+DEPENDS+=		${PHP_PKG_PREFIX}-pdo_mysql>=7.4.0:../../databases/php-pdo_mysql
+.elif ${PKG_OPTIONS:Mpgsql}
+DEPENDS+=		${PHP_PKG_PREFIX}-pdo_pgsql>=7.4.0:../../databases/php-pdo_pgsql
 .endif
