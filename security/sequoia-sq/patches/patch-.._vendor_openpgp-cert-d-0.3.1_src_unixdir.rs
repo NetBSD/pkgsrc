20bd3343c2e04ed01bd2219eba4b6d11a6a35924
wiz <wiz@pkgsrc.org>
2024-02-28 16:39:07 +0000

    sequoia-sq: add upstream bug report as patch comment

--- security/sequoia-sq/patches/patch-.._vendor_openpgp-cert-d-0.3.1_src_unixdir.rs
+++ security/sequoia-sq/patches/patch-.._vendor_openpgp-cert-d-0.3.1_src_unixdir.rs
@@ -1,6 +1,7 @@
-$NetBSD: patch-.._vendor_openpgp-cert-d-0.3.1_src_unixdir.rs,v 1.1 2024/02/28 16:33:38 wiz Exp $
+$NetBSD: patch-.._vendor_openpgp-cert-d-0.3.1_src_unixdir.rs,v 1.2 2024/02/28 16:39:07 wiz Exp $
 
 NetBSD has st_mtimensec
+https://gitlab.com/sequoia-pgp/pgp-cert-d/-/issues/45
 
 --- ../vendor/openpgp-cert-d-0.3.1/src/unixdir.rs.orig	2024-02-28 14:58:05.502259036 +0000
 +++ ../vendor/openpgp-cert-d-0.3.1/src/unixdir.rs
