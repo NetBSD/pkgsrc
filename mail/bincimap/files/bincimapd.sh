9fda8b46082d547251edc451cdc637a262db691d
schmonz <schmonz@pkgsrc.org>
2024-02-28 04:14:16 +0000

    bincimap: new upstream, update to 2.0.15. Changes:
    2.0.15  Included logging for Operator LOGIN and display of the real auth
            user rather than the virtual user in the logs. Streamlined log
            output to be close to s/qmail's.
    2.0.14  Removed gethostbyname and replaced by environment variable
            'TCPLOCALHOST'. Changed default logtype to 'multilog'.
    2.0.13  Logging is working now for each session at logout/disconnect/timeout.
    2.0.12  CRAM-MD5 Auth should work now (given the backend allows this).
            Modularized auth operator for potential new methods.
            Improved man pages to reflect current capabilities.
    2.0.11  BincInfo renamed to BincLog, added s/qmail like tags for logging.
            Added a LICENSE file (under ./doc).
            StartTLS working now after reordering reply command sequence.
    2.0.10  Added CRAM-MD5 authentication. 'logged in' log includes now remote IP.
    2.0.9   Added doc + man pages and finished slashpackage installation.
    2.0.8   Final cleaning and integration tests.
    2.0.7   Moved StartTLS support into operator-starttls.cc.
    2.0.6   Added StartTLS into sesssion-initialisation-bincimap-up.cc.
    2.0.5   Added operator-id.cc (RFC 2971) since Evolution requires this.
    2.0.4   Removed JAIL option -> sslserver can do that as well. Trimmed log
            output.
    2.0.3   Trimming of *.cc and *.h files. Removed StartTLS driver.
    2.0.2   Integration tests successful for TLS and with new qmail-authuser.
    2.0.1   Makefile working. First minor fixes. Initial working version.
    2.0.0   Initial restart of Bincimap 1.3.4 without Autoconf tools.

--- mail/bincimap/files/bincimapd.sh
+++ mail/bincimap/files/bincimapd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: bincimapd.sh,v 1.1 2021/01/15 19:50:25 schmonz Exp $
+# $NetBSD: bincimapd.sh,v 1.2 2024/02/28 04:14:16 schmonz Exp $
 #
 
 # PROVIDE: bincimapd mail
@@ -8,30 +8,72 @@
 name="bincimapd"
 
 # User-settable rc.conf variables and their default values:
-: ${bincimapd_postenv:=""}
+: ${bincimapd_postenv:="VERBOSE_GREETING=yes"}
 : ${bincimapd_datalimit:="180000000"}
 : ${bincimapd_pretcpserver:=""}
-: ${bincimapd_tcpserver:="@PREFIX@/bin/tcpserver"}
-: ${bincimapd_tcpflags:="-R -H"}
+: ${bincimapd_tcpserver:="@PREFIX@/bin/sslserver"}
+: ${bincimapd_tcpflags:="-ne -vRl0"}
 : ${bincimapd_tcphost:=":0"}
-: ${bincimapd_tcpport:="143"}
-: ${bincimapd_precheckpassword:="@PREFIX@/bin/bincimap-up --conf=@PKG_SYSCONFDIR@/bincimap.conf --"}
+: ${bincimapd_tcpport:="993"}
+: ${bincimapd_precheckpassword:="@PREFIX@/bin/bincimap-up --"}
 : ${bincimapd_checkpassword:="@PREFIX@/bin/nbcheckpassword"}
 : ${bincimapd_preimapd:="@PREFIX@/bin/checknotroot"}
 : ${bincimapd_imapdcmd:="@PREFIX@/bin/bincimapd"}
-: ${bincimapd_postimapd:=""}
+: ${bincimapd_postimapd:="Maildir"}
+: ${bincimapd_log:="YES"}
+: ${bincimapd_logcmd:="logger -t bincimap -p mail.info"}
+: ${bincimapd_nologcmd:="@PREFIX@/bin/multilog -*"}
+: ${bincimapd_tls:="YES"}
+: ${bincimapd_tls_dhparams:="@PKG_SYSCONFDIR@/certs/dh2048.pem"}
+: ${bincimapd_tls_cert:="@PKG_SYSCONFDIR@/certs/servercert.pem"}
+: ${bincimapd_tls_key:="@PKG_SYSCONFDIR@/certs/serverkey.pem"}
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 rcvar=${name}
-required_files="@PKG_SYSCONFDIR@/bincimap.conf"
 command="${bincimapd_tcpserver}"
 procname=nb${name}
 start_precmd="bincimapd_precmd"
 
+bincimapd_configure_tls() {
+	if [ "auto" = "${bincimapd_tls}" ]; then
+		if [ -f "${bincimapd_tls_cert}" ]; then
+			bincimapd_enable_tls
+		else
+			bincimapd_disable_tls
+		fi
+	elif [ -f /etc/rc.subr ] && checkyesno bincimapd_tls; then
+		bincimapd_enable_tls
+	else
+		bincimapd_disable_tls
+	fi
+}
+
+bincimapd_disable_tls() {
+	bincimapd_postenv="ALLOW_NONSSL_PLAINTEXT_LOGINS=yes ${bincimapd_postenv}"
+}
+
+bincimapd_enable_tls() {
+	bincimapd_postenv="CADIR=@SSLDIR@/certs ${bincimapd_postenv}"
+	bincimapd_postenv="SSL_UID=$(@ID@ -u @UCSPI_SSL_USER@) ${bincimapd_postenv}"
+	bincimapd_postenv="SSL_GID=$(@ID@ -g @UCSPI_SSL_GROUP@) ${bincimapd_postenv}"
+	bincimapd_postenv="DHFILE=${bincimapd_tls_dhparams} ${bincimapd_postenv}"
+	bincimapd_postenv="CERTFILE=${bincimapd_tls_cert} ${bincimapd_postenv}"
+	if [ -n "${bincimapd_tls_key}" -a ! -f "${bincimapd_tls_key}" ]; then
+		@OPENSSL@ rsa -in ${bincimapd_tls_cert} -out ${bincimapd_tls_key}
+		@CHMOD@ 640 ${bincimapd_tls_key}
+	fi
+	bincimapd_postenv="KEYFILE=${bincimapd_tls_key} ${bincimapd_postenv}"
+}
+
+
 bincimapd_precmd() {
+	if [ -f /etc/rc.subr ] && ! checkyesno bincimapd_log; then
+		bincimapd_logcmd=${bincimapd_nologcmd}
+	fi
+	bincimapd_configure_tls
 	# tcpserver(1) is akin to inetd(8), but runs one service per process.
 	# We want to signal only the tcpserver process responsible for this
 	# service. Use argv0(1) to set procname to "nbbincimapd".
@@ -39,9 +81,12 @@ bincimapd_precmd() {
 @PREFIX@/bin/softlimit -m ${bincimapd_datalimit} ${bincimapd_pretcpserver} \
 @PREFIX@/bin/argv0 ${bincimapd_tcpserver} ${procname} \
 ${bincimapd_tcpflags} \
+-u @BINCIMAP_USER@ -g @BINCIMAP_GROUP@ \
 ${bincimapd_tcphost} ${bincimapd_tcpport} \
 ${bincimapd_precheckpassword} ${bincimapd_checkpassword} \
-${bincimapd_preimapd} ${bincimapd_imapdcmd} ${bincimapd_postimapd}"
+${bincimapd_preimapd} ${bincimapd_imapdcmd} ${bincimapd_postimapd} \
+2>&1 | \
+@PREFIX@/bin/pgrphack @PREFIX@/bin/setuidgid @BINCIMAP_LOG_USER@ ${bincimapd_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
