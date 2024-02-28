e042478fe951fb3b504d37db0ae7f0d0c42dcf11
schmonz <schmonz@pkgsrc.org>
2024-02-28 04:14:16 +0000

    bincimap: new upstream, update to 2.0.15. Changes:
    2.0.15  Included logging for Operator LOGIN and display of the real auth
            user rather than the virtual user in the logs. Streamlined log
            output to be close to s/qmail's.
    2.0.14  Removed gethostbyname and replaced by environment variable
            'TCPLOCALHOST'. Changed default logtype to 'multilog'.
    2.0.13  Logging is working now for each session at logout/disconnect/timeout.
    2.0.12  CRAM-MD5 Auth should work now (given the backend allows this).
            Modularized auth operator for potential new methods.
            Improved man pages to reflect current capabilities.
    2.0.11  BincInfo renamed to BincLog, added s/qmail like tags for logging.
            Added a LICENSE file (under ./doc).
            StartTLS working now after reordering reply command sequence.
    2.0.10  Added CRAM-MD5 authentication. 'logged in' log includes now remote IP.
    2.0.9   Added doc + man pages and finished slashpackage installation.
    2.0.8   Final cleaning and integration tests.
    2.0.7   Moved StartTLS support into operator-starttls.cc.
    2.0.6   Added StartTLS into sesssion-initialisation-bincimap-up.cc.
    2.0.5   Added operator-id.cc (RFC 2971) since Evolution requires this.
    2.0.4   Removed JAIL option -> sslserver can do that as well. Trimmed log
            output.
    2.0.3   Trimming of *.cc and *.h files. Removed StartTLS driver.
    2.0.2   Integration tests successful for TLS and with new qmail-authuser.
    2.0.1   Makefile working. First minor fixes. Initial working version.
    2.0.0   Initial restart of Bincimap 1.3.4 without Autoconf tools.

--- mail/bincimap/options.mk
+++ mail/bincimap/options.mk
@@ -1,30 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2021/01/15 19:50:25 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2024/02/28 04:14:16 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bincimap
-PKG_SUPPORTED_OPTIONS+=		inet6 pam tls
-PKG_SUGGESTED_OPTIONS+=		inet6 tls
+PKG_SUPPORTED_OPTIONS+=		pam
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Minet6)
-DEPENDS+=			ucspi-tcp6>=1.10.7nb1:../../net/ucspi-tcp6
-.else
-DEPENDS+=			{ucspi-tcp6>=1.10.7nb1,ucspi-tcp-[0-9]*}:../../net/ucspi-tcp
-.endif
 
 .if !empty(PKG_OPTIONS:Mpam)
 DEPENDS+=			checkpassword-pam>=0.99nb1:../../sysutils/checkpassword-pam
 .else
 DEPENDS+=			checkpassword>=0.90nb1:../../sysutils/checkpassword
-.endif
-
-.if !empty(PKG_OPTIONS:Mtls)
-.  include "../../security/openssl/buildlink3.mk"
-SUBST_CLASSES+=		paths
-SUBST_STAGE.paths=	pre-configure
-SUBST_FILES.paths=	src/io-ssl.cc doc/bincimap-faq.html \
-			doc/manual/bincimap-manual.ps conf/bincimap.conf.in
-SUBST_SED.paths=	-e 's|/usr/share/ssl/certs|${SSLDIR}/certs|g'
-.else
-CONFIGURE_ARGS+=	--without-ssl
 .endif
