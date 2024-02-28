9da5730bf608153170d5b155ca6fab5cfcbfd237
wiz <wiz@pkgsrc.org>
2024-02-28 07:19:22 +0000

    openjpeg: builldlink some binaries required by cmake files

--- graphics/openjpeg/buildlink3.mk
+++ graphics/openjpeg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/07 13:04:15 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/02/28 07:19:22 wiz Exp $
 
 BUILDLINK_TREE+=	openjpeg
 
@@ -8,6 +8,11 @@ OPENJPEG_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.openjpeg+=	openjpeg>=2.1.0
 BUILDLINK_ABI_DEPENDS.openjpeg+=	openjpeg>=2.1.0
 BUILDLINK_PKGSRCDIR.openjpeg?=		../../graphics/openjpeg
+
+BUILDLINK_FILES.openjpeg+=	bin/opj_compress
+BUILDLINK_FILES.openjpeg+=	bin/opj_decompress
+BUILDLINK_FILES.openjpeg+=	bin/opj_dump
+
 .endif	# OPENJPEG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openjpeg
