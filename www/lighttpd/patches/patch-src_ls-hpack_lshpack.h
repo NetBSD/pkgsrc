5f55e2d4a5b000a6018d2f2e4ccce77bf76dc220
schmonz <schmonz@pkgsrc.org>
2024-02-29 18:42:29 +0000

    lighttpd: use upstream sys/queue.h portability patch. NFCI.

--- www/lighttpd/patches/patch-src_ls-hpack_lshpack.h
+++ www/lighttpd/patches/patch-src_ls-hpack_lshpack.h
@@ -1,22 +1,24 @@
-$NetBSD: patch-src_ls-hpack_lshpack.h,v 1.1 2024/02/21 21:32:13 schmonz Exp $
+$NetBSD: patch-src_ls-hpack_lshpack.h,v 1.2 2024/02/29 18:42:29 schmonz Exp $
 
-The compat file isn't present; unconditionally include.
+Apply upstream (106c4b147b) macOS sys/queue.h portability patch.
 
---- src/ls-hpack/lshpack.h.orig	2024-02-21 21:28:48.938352829 +0000
+--- src/ls-hpack/lshpack.h.orig	2024-02-20 04:42:50.000000000 +0000
 +++ src/ls-hpack/lshpack.h
-@@ -219,15 +219,7 @@ lshpack_dec_set_max_capacity (struct lsh
-  * These structures are not very complicated.
-  */
+@@ -225,6 +225,8 @@ lshpack_dec_set_max_capacity (struct lsh
+ #endif
+ #endif
  
--#ifdef __has_include
--#if __has_include(<sys/queue.h>)
- #include <sys/queue.h>
--#endif
--#endif
--
--#ifndef SIMPLEQ_FOREACH
--#include "../compat/sys/queue.h"
--#endif
++#ifndef STAILQ_FOREACH
++
+ #ifndef SIMPLEQ_FOREACH
+ #include "../compat/sys/queue.h"
+ #endif
+@@ -243,6 +245,8 @@ lshpack_dec_set_max_capacity (struct lsh
+ #define STAILQ_FOREACH          SIMPLEQ_FOREACH
+ #endif
  
- /* OpenBSD and older MacOSX might not define STAILQ,
-  * but ls-hpack usage could use SIMPLEQ as alternative */
++#endif
++
+ #if defined(STAILQ_FIRST) && defined(STAILQ_NEXT) && !defined(STAILQ_FOREACH)
+ #define STAILQ_FOREACH(var, head, field)                                \
+         for((var) = STAILQ_FIRST((head));                               \
