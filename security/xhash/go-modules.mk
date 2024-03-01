c91be8e4f9733a00fe6bccfd9bb979d00cb4fc83
bsiegert <bsiegert@pkgsrc.org>
2024-03-01 15:51:24 +0000

    New package, xhash-3.3.2.
    From Ricardo Branco in PR pkg/57963.
    Closes NetBSD/pkgsrc#129.
    
    This Go program uses goroutines to calculate multiple hashes on strings, files
    and directories. The output format is fully configurable.

---
+++ security/xhash/go-modules.mk
@@ -0,0 +1,14 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/03/01 15:51:25 bsiegert Exp $
+
+GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.mod
+GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.zip
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.mod
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.zip
+GO_MODULE_FILES+=	github.com/zeebo/assert/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.3.mod
+GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.3.zip
+GO_MODULE_FILES+=	github.com/zeebo/pcg/@v/v1.0.1.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.19.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.19.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.zip
