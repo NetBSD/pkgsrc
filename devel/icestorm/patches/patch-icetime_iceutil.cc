cdd1e51cf4b6d7250c242d97240ee4a370c9d5da
thorpej <thorpej@pkgsrc.org>
2024-03-02 23:07:59 +0000

    KERN_PROC_PATHNAME is slightly different on NetBSD vs FreeBSD; handle this difference correctly.  Bump package revision to 1.

--- devel/icestorm/patches/patch-icetime_iceutil.cc
+++ devel/icestorm/patches/patch-icetime_iceutil.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-icetime_iceutil.cc,v 1.1 2024/02/28 06:02:39 thorpej Exp $
+$NetBSD: patch-icetime_iceutil.cc,v 1.2 2024/03/02 23:07:59 thorpej Exp $
 
 On NetBSD, also use KERN_PROC_PATHNAME to get the path to the executable.
 
---- icetime/iceutil.cc.orig	2024-02-27 14:44:02.760248201 +0000
-+++ icetime/iceutil.cc	2024-02-27 14:45:11.872738091 +0000
+--- icetime/iceutil.cc.orig	2023-12-12 12:01:19.000000000 +0000
++++ icetime/iceutil.cc	2024-03-02 22:37:27.137509507 +0000
 @@ -32,7 +32,7 @@
  #  include <unistd.h>
  #endif
@@ -13,7 +13,7 @@ On NetBSD, also use KERN_PROC_PATHNAME to get the path to the executable.
  #  include <sys/sysctl.h>
  #endif
  
-@@ -51,7 +51,7 @@ std::string proc_self_dirname()
+@@ -51,10 +51,14 @@ std::string proc_self_dirname()
  		buflen--;
  	return std::string(path, buflen);
  }
@@ -21,4 +21,11 @@ On NetBSD, also use KERN_PROC_PATHNAME to get the path to the executable.
 +#elif defined(__FreeBSD__) || defined(__NetBSD__)
  std::string proc_self_dirname()
  {
++#ifdef __NetBSD__
++	int mib[4] = {CTL_KERN, KERN_PROC_ARGS, getpid(), KERN_PROC_PATHNAME};
++#else
  	int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
++#endif
+ 	size_t buflen;
+ 	char *buffer;
+ 	std::string path;
