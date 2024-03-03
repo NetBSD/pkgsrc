2a5ebdacc9e090a8c3e184b443b96c72a8cc2b11
riastradh <riastradh@pkgsrc.org>
2024-03-03 16:12:50 +0000

    finance/ledger32: restore ledger-3.2.1
    This avoids the following regression:
    
    https://github.com/ledger/ledger/issues/2318
    
    Eventually I would like to see this either fixed upstream or worked
    around, but keeping an older version is easier for now.

---
+++ finance/ledger32/options.mk
@@ -0,0 +1,30 @@
+# $NetBSD: options.mk,v 1.1 2024/03/03 16:12:50 riastradh Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ledger
+PKG_SUPPORTED_OPTIONS+=	python
+PKG_SUGGESTED_OPTIONS+=	python
+
+PLIST_VARS+=		python
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpython)
+CMAKE_ARGS+=		-DUSE_PYTHON:BOOL=TRUE
+PLIST.python=		yes
+PLIST_SUBST+=		PYSITELIB=${PYSITELIB:Q}
+.include "../../devel/py-boost/buildlink3.mk"
+
+.  include "../../mk/bsd.prefs.mk"
+.  if ${OPSYS} == "Darwin"
+SOEXT=dylib
+.  else
+SOEXT=so
+.  endif
+
+# Remove when src/CMakeLists.txt (see FIXME there) stops copying
+# uninstalled library as Python module. Make symlink here to
+# avoid problem with install_name on Darwin and save disk-space.
+post-install:
+	cd ${DESTDIR}${PREFIX}/${PYSITELIB} && \
+	${LN} -s -f ${DESTDIR}${PREFIX}/lib/libledger.${SOEXT} ledger.so
+.endif
---
+++ finance/ledger32/patches/patch-src_main.cc
@@ -0,0 +1,18 @@
+$NetBSD: patch-src_main.cc,v 1.1 2024/03/03 16:12:51 riastradh Exp $
+
+Canonicalize pkgsrc readline.h filename.  This is seen both by
+consumers of devel/readline and by consumers of devel/editline, via
+buildlink3 magic.  The only reason editline/readline.h may exist is to
+allow both devel/readline and devel/editline to coexist.
+
+--- src/main.cc.orig	2019-02-05 13:30:45.000000000 +0000
++++ src/main.cc
+@@ -36,7 +36,7 @@
+ #include "session.h"
+ 
+ #ifdef HAVE_EDIT
+-#include <editline/readline.h>
++#include <readline/readline.h>
+ #endif
+ 
+ using namespace ledger;
