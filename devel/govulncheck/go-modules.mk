bc269fe3ed5476810707de4350c43ddd91dd5c14
bsiegert <bsiegert@pkgsrc.org>
2024-03-04 18:24:35 +0000

    govulncheck: update to 1.0.4
    No changelog from upstream :(

--- devel/govulncheck/go-modules.mk
+++ devel/govulncheck/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.2 2023/08/31 18:39:13 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.3 2024/03/04 18:24:35 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/google/go-cmdtest/@v/v0.4.1-0.20220921163831-55ab3332a786.mod
 GO_MODULE_FILES+=	github.com/google/go-cmdtest/@v/v0.4.1-0.20220921163831-55ab3332a786.zip
@@ -7,13 +7,11 @@ GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.8.mod
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.8.zip
 GO_MODULE_FILES+=	github.com/google/renameio/@v/v0.1.0.mod
 GO_MODULE_FILES+=	github.com/google/renameio/@v/v0.1.0.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.12.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.12.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.3.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.3.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.12.1-0.20230815132531-74c255bcf846.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.12.1-0.20230815132531-74c255bcf846.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.14.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.14.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.17.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.17.0.zip
 GO_MODULE_FILES+=	mvdan.cc/unparam/@v/v0.0.0-20230312165513-e84e2d14e3b8.mod
 GO_MODULE_FILES+=	mvdan.cc/unparam/@v/v0.0.0-20230312165513-e84e2d14e3b8.zip
