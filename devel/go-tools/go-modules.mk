b793c6650a50bff5db4abe33a888636469cf6301
bsiegert <bsiegert@pkgsrc.org>
2024-03-04 18:14:09 +0000

    go-tools: update to 0.18.0
    Now includes the "deadcode" utility to find dead (unreachable) code in
    Go programs.

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,12 +1,14 @@
-# $NetBSD: go-modules.mk,v 1.3 2023/10/30 19:04:35 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.4 2024/03/04 18:14:09 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.16.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.16.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.4.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.4.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.13.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.13.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.21.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.zip
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240208230135-b75ee8823808.mod
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240208230135-b75ee8823808.zip
