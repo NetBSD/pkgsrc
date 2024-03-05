f3bb5d6d4c504587b851d493a5ff4866271db6c5
vins <vins@pkgsrc.org>
2024-03-05 19:44:36 +0000

    devel/libbsd: update to 0.12.1
    # Noteworthy changes (since 0.11.8)
    
    - Fix missing installation of err.h. Closes: #1064909
    - Support for Solaris, macOS and AIX (now build and pass tests).
    - Improve ABI selection handling for a more controlled ports
      ABI.
    - Use -isystem instead of -I due the overlay via #include_next usage.
    - Sync strtoi()/strtou() implementations from NetBSD to get the new
      error handling behavior.
    - Add explicit time32 and time64 support.
    - Rework ABI selection logic.
    - Add closefrom(), strlcpy() and strlcat() as superseded functions
    - Manual pages, documentation and build system cleanups.

--- devel/libbsd/buildlink3.mk
+++ devel/libbsd/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/02/11 18:58:02 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/03/05 19:44:36 vins Exp $
 
 BUILDLINK_TREE+=	libbsd
 
 .if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.6
-BUILDLINK_ABI_DEPENDS.libbsd+=	libbsd>=0.11.8
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.12.1
+BUILDLINK_ABI_DEPENDS.libbsd+=	libbsd>=0.12.1
 BUILDLINK_PKGSRCDIR.libbsd?=	../../devel/libbsd
 
 #BUILDLINK_INCDIRS.libbsd?=	include/bsd
-BUILDLINK_CFLAGS.libbsd+=	-I${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
+BUILDLINK_CFLAGS.libbsd+=	-isystem${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
 BUILDLINK_LIBNAME.libbsd=	bsd
 BUILDLINK_LDADD.libbsd=		${BUILDLINK_LIBNAME.libbsd:S/^/-l/:S/^-l$//}
 
