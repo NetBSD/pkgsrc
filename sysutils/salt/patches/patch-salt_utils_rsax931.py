d0d351a4e066a57ed69a89492a53d498051decd6
adam <adam@pkgsrc.org>
2024-03-06 19:11:53 +0000

    salt salt-docs: updated to 3006.7
    SALT 3006.7 RELEASE NOTES
    
    CHANGELOG
    DEPRECATED
    
    Deprecate and stop using salt.features
    
    CHANGED
    
    Change module search path priority, so Salt extensions can be overridden by syncable modules and module_dirs. You can switch back to the old logic by setting features.enable_deprecated_module_search_path_priority to true, but it will be removed in Salt 3008.
    
    FIXED
    
    Fix an issue with mac_shadow that was causing a command execution error when retrieving values that were not yet set. For example, retrieving last login before the user had logged in.
    
    Fixed an issue when keys didn't match because of line endings
    
    Corrected encoding of credentials for use with Artifactory
    
    Use send_multipart instead of send when sending multipart message.
    
    Fix an issue where the minion would crash on Windows if some of the grains failed to resolve
    
    Fix issue with openscap when the error was outside the expected scope. It now returns failed with the error code and the error
    
    Upgrade relenv to 0.15.0 to fix namespaced packages installed by salt-pip
    
    Fix regression of fileclient re-use when rendering sls pillars and states
    
    Fixes the s3fs backend computing the local cache's files with the wrong hash type
    
    Fixed Salt-SSH pillar rendering and state rendering with nested SSH calls when called via saltutil.cmd or in an orchestration
    
    Fix boto execution module loading
    
    Removed PR 65185 changes since incomplete solution
    
    catch only ret/ events not all returning events.
    
    Fix nonsensical time in fileclient timeout error.
    
    Fixes an issue when reading/modifying ini files that contain unicode characters
    
    added https proxy to the list of proxies so that requests knows what to do with https based proxies
    
    Ensure minion channels are closed on any master connection error.
    
    Fixed issue where Salt can't find libcrypto when pip installed from a cloned repo
    
    Fix RPM package systemd scriptlets to make RPM packages more universal
    
    Fixed an issue where fileclient requests during Pillar rendering cause fileserver backends to be needlessly refreshed.
    
    Fix exceptions being set on futures that are already done in ZeroMQ transport
    
    Use hmac compare_digest method in hashutil module to mitigate potential timing attacks
    
    Fix request channel default timeout regression. In 3006.5 it was changed from 60 to 30 and is now set back to 60 by default.
    
    Upgrade relenv to 0.15.1 to fix debugpy support.
    
    SECURITY
    
    Bump to cryptography==42.0.0 due to https://github.com/advisories/GHSA-3ww4-gg4f-jr7f
    
    In the process, we were also required to update to pyOpenSSL==24.0.0
    
    Bump to cryptography==42.0.3 due to https://github.com/advisories/GHSA-3ww4-gg4f-jr7f

--- sysutils/salt/patches/patch-salt_utils_rsax931.py
+++ sysutils/salt/patches/patch-salt_utils_rsax931.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-salt_utils_rsax931.py,v 1.2 2024/01/22 16:35:25 adam Exp $
+$NetBSD: patch-salt_utils_rsax931.py,v 1.3 2024/03/06 19:11:53 adam Exp $
 
 Skip hacks for finding libcrypto on Darwin.
 
---- salt/utils/rsax931.py.orig	2023-12-12 18:03:35.000000000 +0000
+--- salt/utils/rsax931.py.orig	2024-02-20 22:04:07.000000000 +0000
 +++ salt/utils/rsax931.py
-@@ -31,7 +31,7 @@ def _find_libcrypto():
+@@ -30,7 +30,7 @@ def _find_libcrypto():
              if lib:
                  break
  
 -    elif salt.utils.platform.is_darwin():
 +    elif False:
          # will look for several different location on the system,
-         # Search in the following order. salts pkg, homebrew, macports, finnally
-         # system.
+         # Search in the following order:
+         # - salt's pkg install location
