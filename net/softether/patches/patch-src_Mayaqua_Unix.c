2c6c47b6e797dc28bccfbd2155df10ce39c499c8
hauke <hauke@pkgsrc.org>
2024-03-07 11:48:34 +0000

    Add net/softether v5.02.5181
    SoftEther VPN is an open-source multi-protocol VPN server and client.

---
+++ net/softether/patches/patch-src_Mayaqua_Unix.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-src_Mayaqua_Unix.c,v 1.1 2024/03/07 11:48:35 hauke Exp $
+
+*BSD have statvfs
+
+--- src/Mayaqua/Unix.c.orig	2024-03-05 12:27:18.757261333 +0000
++++ src/Mayaqua/Unix.c
+@@ -51,6 +51,11 @@
+ #include <sys/statvfs.h>
+ #endif
+ 
++#ifdef UNIX_BSD
++#define USE_STATVFS
++#include <sys/statvfs.h>
++#endif
++
+ #ifdef	UNIX_MACOS
+ #ifdef	NO_VLAN
+ // Struct statfs for MacOS X
