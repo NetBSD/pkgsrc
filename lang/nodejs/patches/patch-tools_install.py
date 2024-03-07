a317e4b6dd7d584055624a09e7cf06b660fa1d83
adam <adam@pkgsrc.org>
2024-03-07 17:07:43 +0000

    nodejs: updated to 21.7.0
    Version 21.7.0 (Current)
    
    Text Styling
    Loading and parsing environment variables
    Support for multi-line values for .env file
    sea: support embedding assets
    vm: support using the default loader to handle dynamic import()
    crypto: implement crypto.hash()

--- lang/nodejs/patches/patch-tools_install.py
+++ lang/nodejs/patches/patch-tools_install.py
@@ -1,18 +1,18 @@
-$NetBSD: patch-tools_install.py,v 1.8 2022/03/30 06:52:33 adam Exp $
+$NetBSD: patch-tools_install.py,v 1.9 2024/03/07 17:07:43 adam Exp $
 
 Install man pages under the right directory.
-
---- tools/install.py.orig	2021-09-10 17:25:14.000000000 +0000
+ 
+--- tools/install.py.orig	2024-03-06 15:49:47.000000000 +0000
 +++ tools/install.py
-@@ -156,10 +156,7 @@ def files(action):
-   action(['deps/v8/tools/gdbinit'], 'share/doc/node/')
-   action(['deps/v8/tools/lldb_commands.py'], 'share/doc/node/')
+@@ -182,10 +182,7 @@ def files(options, action):
+   action(options, [os.path.join(options.v8_dir, 'tools/gdbinit')], 'share/doc/node/')
+   action(options, [os.path.join(options.v8_dir, 'tools/lldb_commands.py')], 'share/doc/node/')
  
 -  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
--    action(['doc/node.1'], 'man/man1/')
+-    action(options, ['doc/node.1'], 'man/man1/')
 -  else:
--    action(['doc/node.1'], 'share/man/man1/')
-+  action(['doc/node.1'], os.environ.get('PKGMANDIR') + '/man1/')
+-    action(options, ['doc/node.1'], 'share/man/man1/')
++  action(options, ['doc/node.1'], os.environ.get('PKGMANDIR') + '/man1/')
  
-   if 'true' == variables.get('node_install_npm'):
-     npm_files(action)
+   if 'true' == options.variables.get('node_install_npm'):
+     npm_files(options, action)
