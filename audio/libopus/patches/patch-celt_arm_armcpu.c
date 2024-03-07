c644108489a5e68c8eaaa418ea2008273219c736
adam <adam@pkgsrc.org>
2024-03-07 17:26:28 +0000

    libopus: updated to 1.5.1
    libopus 1.5.1
    
    Opus 1.5 is the first release to make extended use of ML in the encoder and decoder. You can read all the details in this release demo page. In summary, major changes since 1.4 include:
    
    Significant improvement to packet loss robustness using Deep Redundancy (DRED)
    Improved packet loss concealment through Deep PLC
    Low-bitrate speech quality enhancement down to 6 kb/s wideband
    Improved x86 (AVX2) and Arm (Neon) optimizations
    Support for 4th and 5th order ambisonics
    In addition to the improvements above, this release includes many minor bug fixes. Opus 1.5.1 fixes the meson build that was broken in 1.5.
    
    
    libopus 1.5
    
    Opus 1.5 is the first release to make extended use of ML in the encoder and decoder. You can read all the details in this release demo page. In summary, major changes since 1.4 include:
    
    Significant improvement to packet loss robustness using Deep Redundancy (DRED)
    Improved packet loss concealment through Deep PLC
    Low-bitrate speech quality enhancement down to 6 kb/s wideband
    Improved x86 (AVX2) and Arm (Neon) optimizations
    Support for 4th and 5th order ambisonics
    In addition to the improvements above, this release includes many minor bug fixes.

--- audio/libopus/patches/patch-celt_arm_armcpu.c
+++ audio/libopus/patches/patch-celt_arm_armcpu.c
@@ -1,28 +1,22 @@
-$NetBSD: patch-celt_arm_armcpu.c,v 1.2 2023/05/06 18:51:15 ryoon Exp $
+$NetBSD: patch-celt_arm_armcpu.c,v 1.3 2024/03/07 17:26:28 adam Exp $
 
-Don't error on ARM if we're not on linux or MSVC.
+Don't error on ARM if we're not on Darwin, Linux, or MSVC.
 Currently the code attempts to open /proc/cpuinfo, and tests for
 failure - if that doesn't contain useful info or doesn't exist,
 it still behaves correctly.
 
---- celt/arm/armcpu.c.orig	2022-08-04 21:54:39.000000000 +0000
+--- celt/arm/armcpu.c.orig	2024-03-01 07:55:09.000000000 +0000
 +++ celt/arm/armcpu.c
-@@ -91,7 +91,7 @@ static OPUS_INLINE opus_uint32 opus_cpu_
+@@ -92,7 +92,7 @@ static OPUS_INLINE opus_uint32 opus_cpu_
    return flags;
  }
  
 -#elif defined(__linux__)
-+#else
++#elif !defined(__APPLE__)
  /* Linux based */
  #include <stdio.h>
  
-@@ -148,11 +148,12 @@ opus_uint32 opus_cpu_capabilities(void)
-   }
-   return flags;
- }
--#else
-+#endif
-+#if !(defined(__linux__) || defined(_MSC_VER))
+@@ -195,7 +195,7 @@ opus_uint32 opus_cpu_capabilities(void)
  /* The feature registers which can tell us what the processor supports are
   * accessible in priveleged modes only, so we can't have a general user-space
   * detection method like on x86.*/
