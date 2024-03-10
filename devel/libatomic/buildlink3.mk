20c58af66f52bf0e734c0c5ce09a207f8b4e3c9f
he <he@pkgsrc.org>
2024-03-10 10:18:21 +0000

    devel/libatomic: Add support for rust-based programs.

--- devel/libatomic/buildlink3.mk
+++ devel/libatomic/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/06/28 11:31:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/03/10 10:18:21 he Exp $
 
 BUILDLINK_TREE+=	libatomic
 
@@ -11,6 +11,11 @@ BUILDLINK_PKGSRCDIR.libatomic?=		../../devel/libatomic
 
 BUILDLINK_LDADD.libatomic+=	-latomic
 BUILDLINK_LDFLAGS.libatomic?=	${BUILDLINK_LDADD.libatomic}
+
+# Make this work for rust as well:
+RUSTFLAGS+=	-C link-arg=${BUILDLINK_LDADD.libatomic}
+RUSTFLAGS+=	-C link-arg=-L${BUILDLINK_PREFIX.libatomic}/lib
+RUSTFLAGS+=	-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX}/lib
 
 .include "../../mk/pthread.buildlink3.mk"
 .endif
