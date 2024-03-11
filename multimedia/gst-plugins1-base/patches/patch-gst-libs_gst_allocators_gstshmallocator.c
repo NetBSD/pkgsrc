4f1928b976d8ea1bae34c0db28aab85f3daf9eb2
wiz <wiz@pkgsrc.org>
2024-03-11 09:37:10 +0000

    gst-plugins1-base: add upstream bug report URL

--- multimedia/gst-plugins1-base/patches/patch-gst-libs_gst_allocators_gstshmallocator.c
+++ multimedia/gst-plugins1-base/patches/patch-gst-libs_gst_allocators_gstshmallocator.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-gst-libs_gst_allocators_gstshmallocator.c,v 1.1 2024/03/11 09:13:41 jperkin Exp $
+$NetBSD: patch-gst-libs_gst_allocators_gstshmallocator.c,v 1.2 2024/03/11 09:37:10 wiz Exp $
 
 Include sys/stat.h for S_*.
+https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/3370
 
 --- gst-libs/gst/allocators/gstshmallocator.c.orig	2024-03-11 09:02:38.876091503 +0000
 +++ gst-libs/gst/allocators/gstshmallocator.c
