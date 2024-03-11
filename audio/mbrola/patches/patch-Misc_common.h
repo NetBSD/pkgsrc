b3ef19cc3f894736772c0f5b42c4627d401bca59
jperkin <jperkin@pkgsrc.org>
2024-03-11 11:35:58 +0000

    mbrola: Support x86_64, not MAKE_JOBS safe, fix perms.

--- audio/mbrola/patches/patch-Misc_common.h
+++ audio/mbrola/patches/patch-Misc_common.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-Misc_common.h,v 1.1 2024/01/26 12:58:05 wiz Exp $
+$NetBSD: patch-Misc_common.h,v 1.2 2024/03/11 11:35:58 jperkin Exp $
 
 Add NetBSD support and check for _LITTLE_ENDIAN
 
@@ -18,3 +18,12 @@ Add NetBSD support and check for _LITTLE_ENDIAN
  #  define LITTLE_ENDIAN
  #else
  #  define BIG_ENDIAN
+@@ -82,7 +82,7 @@ void swab( const char *from, char *to, i
+ #else
+ 
+ /* Intel based machine ? */
+-#if defined(__i386) || defined(_M_X86) || defined(TARGET_OS_VMS)
++#if defined(__i386) || defined(_M_X86) || defined(TARGET_OS_VMS) || defined(__x86_64__)
+ #undef BIG_ENDIAN
+ #undef LITTLE_ENDIAN
+ # define LITTLE_ENDIAN
