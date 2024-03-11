f0a41a4e935f8fc53c3af7888c854d1fdac73d3f
ryoon <ryoon@pkgsrc.org>
2024-03-11 14:59:27 +0000

    openexr: Add devel/libdeflate to buildlink3.mk
    * Fix openexr detection in graphics/gegl, for example.

--- graphics/openexr/buildlink3.mk
+++ graphics/openexr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2023/11/15 20:00:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2024/03/11 14:59:27 ryoon Exp $
 
 BUILDLINK_TREE+=	openexr
 
@@ -13,6 +13,7 @@ PTHREAD_OPTS+=	require
 
 USE_CXX_FEATURES+=		c++14
 
+.include "../../devel/libdeflate/buildlink3.mk"
 .include "../../math/imath/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # OPENEXR_BUILDLINK3_MK
