2b16a7c34eaca85d58806590cb48159e24680b35
wiz <wiz@pkgsrc.org>
2024-03-11 08:59:16 +0000

    libXaw: update to 1.0.16.
    Code-cleanup and build-fixes...
    
    Alan Coopersmith (13):
          Set close-on-exec when opening files
          Variable scope reductions as suggested by cppcheck
          Handle redundantInitialization warnings from cppcheck
          Use C99 designated initializers where appropriate
          gitlab CI: add xz-utils to container for "make distcheck"
          unifdef NCR
          unifdef sony
          unifdef QNX4
          unifdef AIXV3 || __SCO__
          unifdef __osf__
          unifdef __UNIXOS2__
          Remove unused sharedlib.c
          unifdef ISC
    
    Thomas E. Dickey (27):
          build-fix
          fix missing-initializer warnings
          quiet conversion-warnings with casts
          provide fallback for deprecated isascii/toascii
          reduce compiler-warnings using casts
          cppcheck scope and assignment warnings
          manpage: adjust line-breaks to avoid embedded full-stops
          manpage: assume .EX/.EE macros
          manpage: improve formatting consistency of bold/italics
          fix references to uninitialized data when constructing argument-lists
          reduce variable-length argument lists to match the existing code
          delete unused assignments
          add check for null pointer, avoid dereferencing
          add check for null pointer, avoid dereferencing
          add check for null pointer, avoid dereferencing
          add check for null pointer, avoid dereferencing
          add check for null pointer, avoid dereferencing
          add check for null pointer, avoid dereferencing
          add check for null pointer, avoid dereferencing
          split-up assignments which give unnecessary compiler-warnings
          scan-build(clang-17) reports possible null dereference, add checks
          fix clang/gcc warnings for undefined behavior, also fix a bug
          clang/gcc warnings about undefined behavior (minor bug-fix)
          amend change to XawListChange()
          make the examples configurable as (interactive) unit-tests
          delete code which is never used
          libXaw 1.0.16:  amends XawListChange() prototype, plus code-cleanup
    
    Walter Harms (4):
          add examples
          add xawhisto
          add simple widget demo
          expand for simple widget demo

--- x11/libXaw/hacks.mk
+++ x11/libXaw/hacks.mk
@@ -1,11 +1,11 @@
-# $NetBSD: hacks.mk,v 1.1 2008/01/29 03:57:36 bjs Exp $
+# $NetBSD: hacks.mk,v 1.2 2024/03/11 08:59:16 wiz Exp $
 
 .if !defined(LIBXAW_HACKS_MK)
 LIBXAW_HACKS_MK=	# defined
 
 .include "../../mk/compiler.mk"
 
-.if !empty(CC_VERSION:Mgcc-[34]*)
+.if ${CC_VERSION:Mgcc-[34]*}
 CFLAGS+=	-fno-strict-aliasing
 PKG_HACKS+=	no-strict-aliasing
 .endif
