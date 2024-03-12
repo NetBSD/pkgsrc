edbaf1b4f7147df2fee130493acda6b07eec5e0a
wiz <wiz@pkgsrc.org>
2024-03-12 08:53:19 +0000

    speech-dispatcher: update to 0.11.5.
    Updated provided by adr and myself via wip.
    
    Version 0.11.5
    
        Update CLDR to version 43 and symbols from NVDA.
        Fix parsing and emitting space character.
        Reduce espeak buffer size to avoid ssml issues.
        Add --disable-doc.
        Fix spd-conf not being able to set the default module.
        Fix pausing without index.
        ibmtts: Fix parameter reset.
        ibmtts: Disable using the module punctuation by default.

--- audio/speech-dispatcher/buildlink3.mk
+++ audio/speech-dispatcher/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/08/14 05:23:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/03/12 08:53:19 wiz Exp $
 
 BUILDLINK_TREE+=	speech-dispatcher
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	speech-dispatcher
 SPEECH_DISPATCHER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.speech-dispatcher+=	speech-dispatcher>=0.8.4
-BUILDLINK_ABI_DEPENDS.speech-dispatcher?=		speech-dispatcher>=0.10.2nb8
+BUILDLINK_ABI_DEPENDS.speech-dispatcher?=	speech-dispatcher>=0.10.2nb8
 BUILDLINK_PKGSRCDIR.speech-dispatcher?=		../../audio/speech-dispatcher
 
 .include "../../devel/glib2/buildlink3.mk"
