ece2cf57082a95d6d3c625c09e86d28c5029b401
wiz <wiz@pkgsrc.org>
2024-03-13 14:38:12 +0000

    aeneas: switch to espeak-ng.
    From adr.
    
    While here, switch to wheel.mk and ffmpeg6 instead of ffmpeg3.
    Also, make it a plain dependency because it's not linked against.
    
    Bump PKGREVISION.

--- audio/aeneas/patches/patch-setup.py
+++ audio/aeneas/patches/patch-setup.py
@@ -1,9 +1,18 @@
-$NetBSD: patch-setup.py,v 1.1 2018/02/28 22:10:13 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2024/03/13 14:38:12 wiz Exp $
 
 Always build C extension.
 
 --- setup.py.orig	2017-03-16 12:48:53.000000000 +0000
 +++ setup.py
+@@ -234,7 +234,7 @@ EXTENSION_CEW = Extension(
+         "aeneas/cew/cew_func.c"
+     ],
+     libraries=[
+-        "espeak"
++        "espeak-ng"
+     ]
+ )
+ EXTENSION_CFW = Extension(
 @@ -297,7 +297,7 @@ elif FORCE_CEW:
      print("[INFO] ")
      EXTENSIONS.append(EXTENSION_CEW)
