dffdc214e895fd13f24d17ce01e1aa8f012588b9
tsutsui <tsutsui@pkgsrc.org>
2024-03-16 07:01:26 +0000

    mikutter: update to 5.0.6.
    Upstream changes:
     https://mikutter.hatenablog.com/entry/2024/03/16/150527
    
    mikutter 5.0.6
    
    * lint by rubocop against newer sources and reorganize sources
    * remove twitter_api_keys plugin
     * thanks Izumi Tsutsui
    * use gtk3 4.2.1
     * thanks Izumi Tsutsui
    * thumbnails of niconico-video were not shown
     * thanks Izumi Tsutsui
    * remove unused oauth gem from Gemfile
     * thanks Izumi Tsutsui

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,9 +1,9 @@
-$NetBSD: patch-mikutter.rb,v 1.10 2022/01/08 15:17:54 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.11 2024/03/16 07:01:26 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
 
---- mikutter.rb.orig	2021-12-31 15:11:57.000000000 +0000
+--- mikutter.rb.orig	2024-03-16 05:31:35.000000000 +0000
 +++ mikutter.rb
 @@ -1,7 +1,5 @@
 -#!/bin/sh
@@ -13,4 +13,4 @@ $NetBSD: patch-mikutter.rb,v 1.10 2022/01/08 15:17:54 tsutsui Exp $
 -#!ruby
  =begin rdoc
  = mikutter - simple, powerful and moeful Mastodon client
- Copyright (C) 2009-2022 Toshiaki Asai
+ Copyright (C) 2009-2024 Toshiaki Asai
