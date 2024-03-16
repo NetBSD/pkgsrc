5415a58ecc5de1109766689c3c0c6694566481ee
bsiegert <bsiegert@pkgsrc.org>
2024-03-16 17:17:58 +0000

    go-tools: update to 0.19.0
    No changelog from upstream.
    
    The getgo tool has been removed.

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,14 +1,14 @@
-# $NetBSD: go-modules.mk,v 1.4 2024/03/04 18:14:09 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.5 2024/03/16 17:17:58 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.16.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.16.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.22.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.22.0.zip
 GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.mod
 GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.zip
-GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240208230135-b75ee8823808.mod
-GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240208230135-b75ee8823808.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.18.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.18.0.zip
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240228155512-f48c80bd79b2.mod
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240228155512-f48c80bd79b2.zip
