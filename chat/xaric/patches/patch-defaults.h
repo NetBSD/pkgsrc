011329741caea274e32b4e99c7959706f55dbb71
nia <nia@pkgsrc.org>
2024-03-17 17:51:22 +0000

    xaric: Update to 0.13.9
    Changes seem to be mostly code quality fixes, memory leaks, undefined
    behaviour, etc etc.

---
+++ chat/xaric/patches/patch-defaults.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-defaults.h,v 1.1 2024/03/17 17:51:22 nia Exp $
+
+Freenode - it's dead, Jim.
+
+--- defaults.h.orig	2024-03-17 17:46:39.704947010 +0000
++++ defaults.h
+@@ -27,7 +27,7 @@
+  */
+ 
+ #ifndef DEFAULT_SERVER
+-#define DEFAULT_SERVER	    "irc.freenode.net"
++#define DEFAULT_SERVER	    "irc.libera.chat"
+ #endif
+ 
+ /*
