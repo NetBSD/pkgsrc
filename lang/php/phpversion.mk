3a956e454be91f149d73b0b1badb162e7b25bcb9
taca <taca@pkgsrc.org>
2024-03-17 16:46:05 +0000

    lang/php82: update to 8.2.17
    PHP 8.2.17 (2024-03-14)
    
    - Core:
      . Fix ZTS persistent resource crashes on shutdown. (nielsdos)
    
    - Curl:
      . Fix failing tests due to string changes in libcurl 8.6.0. (Ayesh)
    
    - DOM:
      . Fix reference access in dimensions for DOMNodeList and DOMNodeMap.
        (nielsdos)
    
    - Fileinfo:
      . Fixed bug GH-13344 (finfo::buffer(): Failed identify data 0:(null),
        backport). (nielsdos)
    
    - FPM:
      . Fixed bug #75712 (getenv in php-fpm should not read $_ENV, $_SERVER).
        (Jakub Zelenka)
    
    - GD:
      . Fixed bug GH-12019 (detection of image formats in system gd library).
        (Michael Orlitzky)
    
    - MySQLnd:
      . Fixed bug GH-11950 ([mysqlnd] Fixed not to set CR_MALFORMED_PACKET to error
        if CR_SERVER_GONE_ERROR is already set). (Saki Takamachi)
    
    - PGSQL:
      . Fixed bug GH-13354 (pg_execute/pg_send_query_params/pg_send_execute
        with null value passed by reference). (George Barbarosie)
    
    - Standard:
      . Fixed array key as hash to string (case insensitive) comparison typo
        for the second operand buffer size (albeit unused for now). (A. Slepykh)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.424 2024/02/19 15:42:29 taca Exp $
+# $NetBSD: phpversion.mk,v 1.425 2024/03/17 16:46:05 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -92,7 +92,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.30
 PHP81_VERSION=	8.1.27
-PHP82_VERSION=	8.2.16
+PHP82_VERSION=	8.2.17
 PHP83_VERSION=	8.3.3
 
 # Define API version or initial release of major version.
