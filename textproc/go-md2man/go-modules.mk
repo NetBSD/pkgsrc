d3e789fa288125e3cba2e52ac14cb2674e1bae91
pin <pin@pkgsrc.org>
2024-03-19 10:12:12 +0000

    textproc/go-md2man: update to 2.0.4
    What's Changed
        Clean up build stuff by @cpuguy83 in #77
        Bump actions/setup-go from 4 to 5 by @dependabot in #108
        Bump golangci/golangci-lint-action from 3.7.0 to 4.0.0 by @dependabot in #114
        Prepend table preprocessor by @cpuguy83 in #111
        Fix trailing newline in code blocks by @cpuguy83 in #113
        Fix escape characters for content with newline by @cpuguy83 in #112

--- textproc/go-md2man/go-modules.mk
+++ textproc/go-md2man/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.2 2023/09/09 20:14:17 pin Exp $
+# $NetBSD: go-modules.mk,v 1.3 2024/03/19 10:12:12 pin Exp $
 
 GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.mod
 GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.zip
