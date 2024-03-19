54db984b4bdc1e99ec54cc4230f39e5f6f70d4c9
nia <nia@pkgsrc.org>
2024-03-19 11:18:49 +0000

    rust: More realistic list of supported platforms.

--- lang/rust/platform.mk
+++ lang/rust/platform.mk
@@ -1,20 +1,33 @@
-# $NetBSD: platform.mk,v 1.21 2024/03/03 14:53:32 he Exp $
+# $NetBSD: platform.mk,v 1.22 2024/03/19 11:18:49 nia Exp $
 
 # This file encodes whether a given platform has support for rust.
 
-# Platforms where rust ought to work but does not require a link to an
-# open PR.
-
 .if !defined(PLATFORM_SUPPORTS_RUST)
 
-# Rust needs NetBSD>8
-.for rust_arch in aarch64 earmv7hf i386 powerpc riscv64 sparc64 x86_64
-.  for rust_os in Darwin FreeBSD Linux NetBSD SunOS
-.    if ${OPSYS} != "NetBSD" || empty(OS_VERSION:M[0-8].*)
-RUST_PLATFORMS+=	${rust_os}-*-${rust_arch}
-.    endif
-.  endfor
-.endfor
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} >= 090000
+RUST_PLATFORMS+=	NetBSD-*-aarch64
+RUST_PLATFORMS+=	NetBSD-*-aarch64eb
+RUST_PLATFORMS+=	NetBSD-*-earmv6hf
+RUST_PLATFORMS+=	NetBSD-*-earmv7hf
+RUST_PLATFORMS+=	NetBSD-*-i386
+RUST_PLATFORMS+=	NetBSD-*-mipsel
+RUST_PLATFORMS+=	NetBSD-*-powerpc
+RUST_PLATFORMS+=	NetBSD-*-riscv64
+RUST_PLATFORMS+=	NetBSD-*-sparc64
+RUST_PLATFORMS+=	NetBSD-*-x86_64
+.endif
+
+RUST_PLATFORMS+=	Darwin-*-aarch64
+RUST_PLATFORMS+=	Darwin-*-x86_64
+RUST_PLATFORMS+=	FreeBSD-*-x86_64
+RUST_PLATFORMS+=	Linux-*-aarch64
+RUST_PLATFORMS+=	Linux-*-earmv6hf
+RUST_PLATFORMS+=	Linux-*-earmv7hf
+RUST_PLATFORMS+=	Linux-*-i386
+RUST_PLATFORMS+=	Linux-*-x86_64
+RUST_PLATFORMS+=	SunOS-*-x86_64
 
 .for rust_platform in ${RUST_PLATFORMS}
 .  if !empty(MACHINE_PLATFORM:M${rust_platform})
