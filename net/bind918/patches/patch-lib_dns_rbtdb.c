be21e5828f391eacedf5e32501646d3386929423
taca <taca@pkgsrc.org>
2024-03-20 14:59:15 +0000

    net/bind918: update to 9.18.25
    9.18.25 (2024-03-20)
    
    6356.	[bug]		Create the pruning task in the dns_cache_flush(), so
    			the cache pruning still works after the flush.
    			[GL #4621]
    
    6353.	[bug]		Improve the TTL-based cleaning by removing the expired
    			headers from the heap, so they don't block the next
    			cleaning round and clean more than a single item for
    			each new addition to the RBTDB. [GL #4591]
    
    6352.	[bug]		Revert change 6319 and decrease lock contention during
    			RBTDB tree pruning by not cleaning up nodes recursively
    			within a single prune_tree() call. [GL #4596]
    
    6350.	[bug]		Address use after free in expire_lru_headers. [GL #4495]

--- net/bind918/patches/patch-lib_dns_rbtdb.c
+++ net/bind918/patches/patch-lib_dns_rbtdb.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-lib_dns_rbtdb.c,v 1.2 2024/02/13 13:50:39 taca Exp $
+$NetBSD: patch-lib_dns_rbtdb.c,v 1.3 2024/03/20 14:59:15 taca Exp $
 
 * Take from NetBSD base.
 
---- lib/dns/rbtdb.c.orig	2024-02-11 10:42:55.656990155 +0000
+--- lib/dns/rbtdb.c.orig	2024-03-12 08:51:08.618355570 +0000
 +++ lib/dns/rbtdb.c
-@@ -2234,7 +2234,7 @@ prune_tree(isc_task_t *task, isc_event_t
- 	}
+@@ -2222,7 +2222,7 @@ prune_tree(isc_task_t *task, isc_event_t
+ 	NODE_UNLOCK(&rbtdb->node_locks[locknum].lock, isc_rwlocktype_write);
  	RWUNLOCK(&rbtdb->tree_lock, isc_rwlocktype_write);
  
 -	detach((dns_db_t **)&rbtdb);
