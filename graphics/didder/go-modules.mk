b93dadd20fd1dbc51bf30cfb97d9017e620c341d
charlotte <charlotte@pkgsrc.org>
2024-03-21 05:43:24 +0000

    graphics/didder: Import package
    Didder is an extensive, fast, and accurate command-line image dithering tool.
    It is designed to work well for both power users as well as pipeline scripting.
    It is unique in its correctness and variety of dithering algorithms.

---
+++ graphics/didder/go-modules.mk
@@ -0,0 +1,35 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/03/21 05:43:24 charlotte Exp $
+
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v0.3.1.zip
+GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.0-20190314233015-f79a8a8ca69d.mod
+GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.0-20190314233015-f79a8a8ca69d.zip
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.zip
+GO_MODULE_FILES+=	github.com/disintegration/imaging/@v/v1.6.2.mod
+GO_MODULE_FILES+=	github.com/disintegration/imaging/@v/v1.6.2.zip
+GO_MODULE_FILES+=	github.com/makeworld-the-better-one/dither/v2/@v/v2.4.0.mod
+GO_MODULE_FILES+=	github.com/makeworld-the-better-one/dither/v2/@v/v2.4.0.zip
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.0.1.mod
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.0.1.zip
+GO_MODULE_FILES+=	github.com/shurcoo!l/sanitized_anchor_name/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/shurcoo!l/sanitized_anchor_name/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.6.1.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.6.1.zip
+GO_MODULE_FILES+=	github.com/urfave/cli/v2/@v/v2.3.0.mod
+GO_MODULE_FILES+=	github.com/urfave/cli/v2/@v/v2.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/image/@v/v0.0.0-20191009234506-e7c1f5e7dbb8.mod
+GO_MODULE_FILES+=	golang.org/x/image/@v/v0.0.0-20210220032944-ac19c3e999fb.mod
+GO_MODULE_FILES+=	golang.org/x/image/@v/v0.0.0-20210220032944-ac19c3e999fb.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.zip
+GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.2.3.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.2.3.zip
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.zip
