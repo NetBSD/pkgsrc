b0ca112652352899aa7597fe6475bf25ca6e76fa
taca <taca@pkgsrc.org>
2024-03-24 14:04:18 +0000

    devel/ruby-ffi-compiler: update to 1.3.2
    1.2.0 (2023-03-03)
    
    * Correctly format compile arguments for shell
    * Respect user's environment FLAGS
    
    1.2.1 (2023-06-08)
    
    * Stay backwards compatible
    
    1.2.2 (2024-02-11)
    
    * Implement some escaping for Windows shells
    * Escape all arguments for shell commands
    
    1.3.0 (2024-03-13)
    
    * Reuse shellescape for Windows
    * Reuse prebuilt binaries
    
    1.3.1 (2024-03-13)
    
    * Bump ffi version requirement to 1.15.5
    * Add version file
    
    1.3.2 (2024-03-14)
    
    * Provide namespace-free task name even when using namespaces

--- devel/ruby-ffi-compiler/patches/patch-lib_ffi-compiler_compile__task.rb
+++ devel/ruby-ffi-compiler/patches/patch-lib_ffi-compiler_compile__task.rb
@@ -1,13 +1,13 @@
-$NetBSD: patch-lib_ffi-compiler_compile__task.rb,v 1.1 2020/11/02 15:13:27 taca Exp $
+$NetBSD: patch-lib_ffi-compiler_compile__task.rb,v 1.2 2024/03/24 14:04:18 taca Exp $
 
 Do not use FFI::Compiler::Platform own definition and use
 RbConfig::CONFIG['arch'] instead.
 
---- lib/ffi-compiler/compile_task.rb.orig	2020-11-02 14:39:47.900421091 +0000
+--- lib/ffi-compiler/compile_task.rb.orig	2024-03-23 12:59:05.934181248 +0000
 +++ lib/ffi-compiler/compile_task.rb
-@@ -95,7 +95,7 @@ module FFI
+@@ -122,7 +122,7 @@ module FFI
          end
-         so_flags = so_flags.join(' ')
+         so_flags = shelljoin(so_flags)
  
 -        out_dir = "#{@platform.arch}-#{@platform.os}"
 +        out_dir = RbConfig::CONFIG['arch']
