efa8744bda73faf7e09cb35bbb767f3ca022f7e5
leot <leot@pkgsrc.org>
2024-03-24 20:03:50 +0000

    direnv: Update to 2.34.0
    Changes:
    2.34.0
    ======
      * doc: README.md, man pages: it's typos
      * doc: add shell setup instructions for oh-my-zsh
      * doc: added fetchurl manpage link to README.md
      * doc: document XDG_DATA_HOME
      * feat: add Murex support
      * feat: added systemd shell for export
      * feat: allow to disable warn timeouts
      * feat: hide env diff
      * feat: made 'direnv export' non private
      * fix: `use_julia` should not set LD_LIBRARY_PATH
      * fix: add missing deps for release in go.mod
      * fix: avoid use of regex in bash hook output
      * fix: direnv.toml.1.md: add examples for $HOME expansion
      * fix: stdlib: use_flake: don't keep old generations around
      * fix: stdlib: use_node: strip leading v from version
      * fix: support Bash 5.1 array PROMPT_COMMAND
      * fix: update stdlib.sh to avoid deprecation warning
      * fix: update zsh syntax in internal/cmd/shell_zsh.go

--- sysutils/direnv/go-modules.mk
+++ sysutils/direnv/go-modules.mk
@@ -1,10 +1,10 @@
-# $NetBSD: go-modules.mk,v 1.5 2024/01/01 14:28:49 leot Exp $
+# $NetBSD: go-modules.mk,v 1.6 2024/03/24 20:03:50 leot Exp $
 
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.mod
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.zip
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.mod
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.13.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.zip
