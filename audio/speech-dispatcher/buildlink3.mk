29bc153ea964a1a80b838d2d67918a7764869897
adam <adam@pkgsrc.org>
2024-03-24 10:28:14 +0000

    speech-dispatcher: fix build on Darwin

--- audio/speech-dispatcher/buildlink3.mk
+++ audio/speech-dispatcher/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/03/12 08:53:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/03/24 10:28:14 adam Exp $
 
 BUILDLINK_TREE+=	speech-dispatcher
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	speech-dispatcher
 SPEECH_DISPATCHER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.speech-dispatcher+=	speech-dispatcher>=0.8.4
-BUILDLINK_ABI_DEPENDS.speech-dispatcher?=	speech-dispatcher>=0.10.2nb8
+BUILDLINK_ABI_DEPENDS.speech-dispatcher+=	speech-dispatcher>=0.10.2nb8
 BUILDLINK_PKGSRCDIR.speech-dispatcher?=		../../audio/speech-dispatcher
 
 .include "../../devel/glib2/buildlink3.mk"
