5be1c86d52fdf33a057f632db7630e55df3125db
charlotte <charlotte@pkgsrc.org>
2024-04-02 01:28:00 +0000

    textproc/the_silver_searcher: Lint.

--- textproc/the_silver_searcher/patches/patch-src_main.c
+++ textproc/the_silver_searcher/patches/patch-src_main.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_main.c,v 1.1 2022/05/24 06:08:35 charlotte Exp $
+$NetBSD: patch-src_main.c,v 1.2 2024/04/02 01:28:00 charlotte Exp $
 
 DragonFly also has a particular 'cpuset' type.
+
 --- src/main.c.orig	2022-05-23 22:57:49.749747000 -0700
 +++ src/main.c	2022-05-23 22:58:26.439723000 -0700
 @@ -156,7 +156,7 @@
