71c915e652e3e8c8b5d9712e793c088f04d0088f
charlotte <charlotte@pkgsrc.org>
2024-04-03 07:14:32 +0000

    x11/libdrm: Fix DragonFly build

--- x11/libdrm/patches/patch-tests_nouveau_threaded.c
+++ x11/libdrm/patches/patch-tests_nouveau_threaded.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-tests_nouveau_threaded.c,v 1.1 2022/03/13 15:20:01 tnn Exp $
+$NetBSD: patch-tests_nouveau_threaded.c,v 1.2 2024/04/03 07:14:32 charlotte Exp $
+
+Use the correct form of ioctl(2) for as many systems as possible.
 
 --- tests/nouveau/threaded.c.orig	2021-07-02 12:49:05.455772000 +0000
 +++ tests/nouveau/threaded.c
@@ -7,7 +9,7 @@ $NetBSD: patch-tests_nouveau_threaded.c,v 1.1 2022/03/13 15:20:01 tnn Exp $
  static int import_fd;
  
 -#if defined(__GLIBC__) || defined(__FreeBSD__)
-+#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__NetBSD__)
++#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  int ioctl(int fd, unsigned long request, ...)
  #else
  int ioctl(int fd, int request, ...)
