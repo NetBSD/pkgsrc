a4e61224fd5a8251d91d088fbf61d9b9bbcbd57b
adam <adam@pkgsrc.org>
2024-04-05 08:50:25 +0000

    cmake: updated to 3.29.1
    CMake 3.29.1
    * ExternalProject: add an example of `LIST_SEPARATOR` usage
    * ExternalProject: reword `LIST_SEPARATOR` to indicate what it *does*
    * IntelLLVM: Revert accidental use of -external:I with Fortran compilers
    * libuv: Avoid posix_spawn on macOS < 10.8
    * FindPkgConfig: Avoid finding Strawberry Perl's pkg-config
    * libuv: process: de-duplicate conditions for using kqueue
    * libuv: darwin: group kqueue-specific definitions together
    * libuv: macos: use posix poll instead of kqueue
    * FindMPI: Fix finding IntelLLVM wrappers on Windows
    * ctest: Restore enforcement of RESOURCE_LOCK test property
    * ARTOS: Add support for finding library files named with 'lib' prefix
    * IntelLLVM: Fix link flags with Visual Studio generators
    * cmSystemTools: Fix compilation on DragonFly BSD
    * Help: Remove stray formatting for try_compile() LINKER_LANGUAGE keyword
    * try_compile(): Include languages with _ or - when passing linker vars
    * Help: Grammar, typos and wording improvements for linker variables
    * Help: Add custom linker example for CMAKE_<LANG>_USING_LINKER_<TYPE>
    * Help: CMAKE_<LANG>_USING_LINKER_<TYPE> can be a list of flags
    * Help: Restructure and improve LINKER_TYPE docs
    * CMakePackageConfigHelpers: Remove unused variable
    * CMakePackageConfigHelpers: Use unique variable name for pkg prefix
    * Swift: Add CMAKE_LINKER_TYPE support
    * Help: Fix location of install() note about superseded commands
    * FindMatlab: R2024a version map
    * Help: emphasize need for VS Integrations with oneAPI Fortran

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.55 2024/04/02 17:27:41 adam Exp $
+# $NetBSD: version.mk,v 1.56 2024/04/05 08:50:25 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.29.0
+CMAKE_VERSION=	3.29.1
 CMAKE_API=	${CMAKE_VERSION:R}
