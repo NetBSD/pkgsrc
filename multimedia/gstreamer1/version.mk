3932e2e32e07c63a82d30a11b376333daf54bfe7
wiz <wiz@pkgsrc.org>
2024-04-05 17:26:42 +0000

    gstreamer1: update to 1.24.1
    This release only contains bugfixes and security fixes and it should
    be safe to update from 1.24.0.

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.11 2024/03/05 14:45:49 wiz Exp $
+# $NetBSD: version.mk,v 1.12 2024/04/05 17:26:43 wiz Exp $
 
-GST_VERSION=	1.24.0
+GST_VERSION=	1.24.1
 # if ${PATCH} is < 10
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 #else
