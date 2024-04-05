b56c0c4a00fba77a1afd47f512d5af188507ad49
bsiegert <bsiegert@pkgsrc.org>
2024-04-05 19:07:55 +0000

    go121: Update to 1.21.9.
    This minor release includes 1 security fix following the security policy:
    
    http2: close connections when receiving too many headers
    
    Maintaining HPACK state requires that we parse and process all HEADERS and
    CONTINUATION frames on a connection. When a request's headers exceed
    MaxHeaderBytes, we don't allocate memory to store the excess headers but we do
    parse them. This permits an attacker to cause an HTTP/2 endpoint to read
    arbitrary amounts of header data, all associated with a request which is going
    to be rejected. These headers can include Huffman-encoded data which is
    significantly more expensive for the receiver to decode than for an attacker to
    send.
    
    Set a limit on the amount of excess header frames we will process before
    closing a connection.
    
    Thanks to Bartek Nowotarski (https://nowotarski.info/) for reporting this
    issue.
    
    This is CVE-2023-45288 and Go issue https://go.dev/issue/65051.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.205 2024/04/05 18:51:52 bsiegert Exp $
+# $NetBSD: version.mk,v 1.206 2024/04/05 19:07:55 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO122_VERSION=	1.22.2
-GO121_VERSION=	1.21.8
+GO121_VERSION=	1.21.9
 GO120_VERSION=	1.20.14
 GO119_VERSION=	1.19.13
 GO118_VERSION=	1.18.10
