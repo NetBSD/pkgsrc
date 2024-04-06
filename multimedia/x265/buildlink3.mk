e0ffc1dbff4632a1c58926ff48eebaba8721bc1b
wiz <wiz@pkgsrc.org>
2024-04-06 09:32:55 +0000

    *: bump for x265 API change
    Reported by Marc Baudoin

--- multimedia/x265/buildlink3.mk
+++ multimedia/x265/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/07/02 11:46:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/04/06 09:32:55 wiz Exp $
 
 BUILDLINK_TREE+=	x265
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	x265
 X265_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.x265+=	x265>=1.1
+BUILDLINK_ABI_DEPENDS.x265?=	x265>=3.6
 BUILDLINK_PKGSRCDIR.x265?=	../../multimedia/x265
 .endif	# X265_BUILDLINK3_MK
 
