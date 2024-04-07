27652aed9891d3ab2ebccb05c6a1b5e1f2e2b494
wiz <wiz@pkgsrc.org>
2024-04-07 21:38:35 +0000

    py-yubikey-manager,ykman: update to 5.4.0
    * Version 5.4.0 (released)
     * Support for YubiKey Bio Multi-protocol Edition.
     * CLI: Improve error messages for several failures.
     * Attempt to send SIGHUP to yubikey-agent if it is blocking the connection.
     * Bugfix: Allow "fido config" to work when no PIN is set on the YubiKey.
     * Bugfix: MacOS - Fix race condition resulting in unneeded delay in fido commands over
       USB.
     * Bugfix: Linux - Fix error when listing OTP devices when no YubiKeys are attached.
     * Bugfix: OpenPGP - Fix RSA key generation on YubiKey NEO.

--- security/ykman/version.mk
+++ security/ykman/version.mk
@@ -1,6 +1,6 @@
-#	$NetBSD: version.mk,v 1.7 2024/02/13 17:55:20 wiz Exp $
+#	$NetBSD: version.mk,v 1.8 2024/04/07 21:38:35 wiz Exp $
 
 # used by security/py-yubikey-manager/Makefile
 # used by security/ykman/Makefile
 
-VERSION=	5.3.0
+VERSION=	5.4.0
