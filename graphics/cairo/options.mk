829b653e600af3c2cb4adbc0c6c8bd357399ff12
wiz <wiz@pkgsrc.org>
2024-04-07 07:31:19 +0000

    cairo: make lzo optional, keep it default-on
    Bump PKGREVISION.

--- graphics/cairo/options.mk
+++ graphics/cairo/options.mk
@@ -1,17 +1,22 @@
-# $NetBSD: options.mk,v 1.26 2023/11/26 18:45:03 schmonz Exp $
+# $NetBSD: options.mk,v 1.27 2024/04/07 07:31:19 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cairo
-PKG_SUPPORTED_OPTIONS=	x11 xcb
+PKG_SUPPORTED_OPTIONS=	lzo x11 xcb
 .if exists(/System/Library/Frameworks/Quartz.framework)
 PKG_SUPPORTED_OPTIONS+=	quartz
 PKG_SUGGESTED_OPTIONS+=	quartz
 .else
 PKG_SUGGESTED_OPTIONS=	x11 xcb
 .endif
+PKG_SUGGESTED_OPTIONS+=	lzo
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	x11 xcb quartz
+
+.if !empty(PKG_OPTIONS:Mlzo)
+.include "../../archivers/lzo/buildlink3.mk"
+.endif
 
 ###
 ### X11 and XCB support (XCB implies X11)
