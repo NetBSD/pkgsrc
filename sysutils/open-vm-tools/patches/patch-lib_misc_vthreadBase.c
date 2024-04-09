7b7c772ae0f2d9f7d99150f1952dd2cc47a7fd1d
pho <pho@pkgsrc.org>
2024-04-09 01:12:36 +0000

    sysutils/open-vm-tools: Update to 12.4.0
    open-vm-tools now supports NetBSD/aarch64. At least vmhgfs-fuse has been
    confirmed to be working on this platform. vmware-user is still useless
    though, because it requires either Wayland running on top of recent DRM/KMS
    supporting SVGA v3 (which hasn't been ported to NetBSD as of writing) or
    xf86-video-vmware, which only supports SVGA v1 and v2. VMware only provides
    SVGA v3 interface on AArch64 platforms so nothing can interact with VMware
    graphics aside from the standard framebuffer (via xf86-video-wsfb) at the
    moment.
    
    Release notes:
    * https://github.com/vmware/open-vm-tools/blob/stable-12.4.0/ReleaseNotes.md
    * https://github.com/vmware/open-vm-tools/blob/stable-12.3.5/ReleaseNotes.md
    * https://github.com/vmware/open-vm-tools/blob/stable-12.3.0/ReleaseNotes.md
    * https://github.com/vmware/open-vm-tools/blob/stable-12.2.5/ReleaseNotes.md

--- sysutils/open-vm-tools/patches/patch-lib_misc_vthreadBase.c
+++ sysutils/open-vm-tools/patches/patch-lib_misc_vthreadBase.c
@@ -1,13 +1,16 @@
-$NetBSD: patch-lib_misc_vthreadBase.c,v 1.1 2018/01/01 06:56:01 ryoon Exp $
+$NetBSD: patch-lib_misc_vthreadBase.c,v 1.2 2024/04/09 01:12:36 pho Exp $
 
---- lib/misc/vthreadBase.c.orig	2017-12-15 19:09:51.000000000 +0000
+NetBSD implementation for VThreadBase_GetKernelID(). Use pthread_self() and
+cast the resulting pthread_t into uint64.
+
+--- lib/misc/vthreadBase.c.orig	2024-02-06 15:08:41.000000000 +0000
 +++ lib/misc/vthreadBase.c
 @@ -352,6 +352,8 @@ VThreadBase_GetKernelID(void)
  #  endif
     // Best effort until FreeBSD header update
     return (uint64)(uintptr_t)(void *)pthread_self();
-+#elif defined(__NetBSD__)
++#elif defined __NetBSD__
 +   return (uint64)(uintptr_t)(void *)pthread_self();
+ #elif defined __EMSCRIPTEN__
+    return (uint64)(uintptr_t)(void *)pthread_self();
  #else
- #  error "Unknown platform"
- #endif
