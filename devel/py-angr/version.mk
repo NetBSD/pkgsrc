8c71bd6605f9b2b3990bb9d65922df0fb13b46fa
adam <adam@pkgsrc.org>
2024-04-10 16:49:09 +0000

    py-angr family: updated to 9.2.98
    9.2.98
    Bug fixes

--- devel/py-angr/version.mk
+++ devel/py-angr/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.8 2024/02/07 09:56:52 adam Exp $
+# $NetBSD: version.mk,v 1.9 2024/04/10 16:49:09 adam Exp $
 # used by devel/py-ailment/Makefile
 # used by devel/py-angr/Makefile
 # used by devel/py-archinfo/Makefile
@@ -7,4 +7,4 @@
 # used by devel/py-pyvex/buildlink3.mk
 # used by math/py-claripy/Makefile
 
-ANGR_VERSION=	9.2.89
+ANGR_VERSION=	9.2.98
