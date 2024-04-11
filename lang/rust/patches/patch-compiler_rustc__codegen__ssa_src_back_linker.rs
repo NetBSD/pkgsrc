2055197aedfedb5778e901d2645609a74bbe91bd
tnn <tnn@pkgsrc.org>
2024-04-11 19:53:50 +0000

    rust: allow linking with external libunwind on Linux

--- lang/rust/patches/patch-compiler_rustc__codegen__ssa_src_back_linker.rs
+++ lang/rust/patches/patch-compiler_rustc__codegen__ssa_src_back_linker.rs
@@ -1,10 +1,11 @@
-$NetBSD: patch-compiler_rustc__codegen__ssa_src_back_linker.rs,v 1.12 2023/10/25 05:50:43 pin Exp $
+$NetBSD: patch-compiler_rustc__codegen__ssa_src_back_linker.rs,v 1.13 2024/04/11 19:53:50 tnn Exp $
 
 Do not use @rpath on Darwin.
+Find external linunwind on Linux.
 
---- compiler/rustc_codegen_ssa/src/back/linker.rs.orig	2022-12-12 16:02:12.000000000 +0000
+--- compiler/rustc_codegen_ssa/src/back/linker.rs.orig	2023-12-21 16:55:28.000000000 +0000
 +++ compiler/rustc_codegen_ssa/src/back/linker.rs
-@@ -325,7 +325,7 @@ impl<'a> GccLinker<'a> {
+@@ -329,7 +329,7 @@ impl<'a> GccLinker<'a> {
              // principled solution at some point to force the compiler to pass
              // the right `-Wl,-install_name` with an `@rpath` in it.
              if self.sess.opts.cg.rpath || self.sess.opts.unstable_opts.osx_rpath_install_name {
@@ -13,3 +14,14 @@ Do not use @rpath on Darwin.
                  rpath.push(out_filename.file_name().unwrap());
                  self.linker_args(&[OsString::from("-install_name"), rpath]);
              }
+@@ -455,6 +455,10 @@ impl<'a> Linker for GccLinker<'a> {
+                 self.linker_arg("--as-needed");
+             }
+         }
++        if self.sess.target.os.contains("linux") && lib == "unwind" {
++            self.linker_arg("-rpath,@PREFIX@/lib");
++            self.linker_arg("-L@PREFIX@/lib");
++        }
+     }
+     fn link_staticlib(&mut self, lib: &str, verbatim: bool) {
+         self.hint_static();
