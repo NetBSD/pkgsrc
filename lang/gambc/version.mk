b46610777fb980b22c093dca69687fa9528c6a4a
ktnb <ktnb@pkgsrc.org>
2024-04-11 15:19:10 +0000

    gambc: update to 4.9.5
    - Avoid leading 0 in timestamp that can be interpreted as an octal
      number
    - Fix various SRFI 19 issues and update leap-seconds table
    - Check for Autoconf 2.69 and give a warning if another version is
      used

--- lang/gambc/version.mk
+++ lang/gambc/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.5 2023/04/26 20:53:23 nikita Exp $
+# $NetBSD: version.mk,v 1.6 2024/04/11 15:19:11 ktnb Exp $
 ###
 .if !defined(GAMBC_VERSION_MK)
 GAMBC_VERSION_MK=	# empty
 ###
 ### The package version sets the distribution version.
 ###
-GAMBC_VERSION=		4.9.4
+GAMBC_VERSION=		4.9.5
 GAMBC_DIST_VERSION=	${GAMBC_VERSION:C|^.*|v&|:S|.|_|g}
 .endif
