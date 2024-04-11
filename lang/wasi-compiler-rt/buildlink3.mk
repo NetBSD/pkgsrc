f7d4b96075a7c5a57b3e1b8bf6fd49f44c45b136
gutteridge <gutteridge@pkgsrc.org>
2024-04-11 22:19:25 +0000

    wasi-compiler-rt: drop comment that's no longer relevant

--- lang/wasi-compiler-rt/buildlink3.mk
+++ lang/wasi-compiler-rt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/06 19:18:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/11 22:19:25 gutteridge Exp $
 
 BUILDLINK_TREE+=	wasi-compiler-rt
 
@@ -7,8 +7,6 @@ BUILDLINK_DEPMETHOD.wasi-compiler-rt?=	build
 .if !defined(WASI_COMPILER_RT_BUILDLINK3_MK)
 WASI_COMPILER_RT_BUILDLINK3_MK:=
 
-# This exact version must be kept in sync with llvm, or firefox
-# minimum build dependencies will be incorrect.
 BUILDLINK_API_DEPENDS.wasi-compiler-rt+=	wasi-compiler-rt>=16.0.0<17
 BUILDLINK_PKGSRCDIR.wasi-compiler-rt?=		../../lang/wasi-compiler-rt
 
