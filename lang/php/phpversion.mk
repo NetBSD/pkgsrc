9bbea5a8b9730ba199cef4e6fa2320a532d4c47c
taca <taca@pkgsrc.org>
2024-04-13 02:51:54 +0000

    lang/php83: update to 8.3.5
    This release includes security fixes.
    
    11 Apr 2024, PHP 8.3.5
    
    - Core:
      . Fixed GH-13569 (GC buffer unnecessarily grows up to GC_MAX_BUF_SIZE when
        scanning WeakMaps). (Arnaud)
      . Fixed bug GH-13612 (Corrupted memory in destructor with weak references).
        (nielsdos)
      . Fixed bug GH-13446 (Restore exception handler after it finishes). (ilutov)
      . Fixed bug GH-13784 (AX_GCC_FUNC_ATTRIBUTE failure). (Remi)
      . Fixed bug GH-13670 (GC does not scale well with a lot of objects created in
        destructor). (Arnaud)
    
    - DOM:
      . Add some missing ZPP checks. (nielsdos)
      . Fix potential memory leak in XPath evaluation results. (nielsdos)
    
    - FPM:
      . Fixed GH-11086 (FPM: config test runs twice in daemonised mode).
        (Jakub Zelenka)
      . Fix incorrect check in fpm_shm_free(). (nielsdos)
    
    - GD:
      . Fixed bug GH-12019 (add GDLIB_CFLAGS in feature tests). (Michael Orlitzky)
    
    - Gettext:
      . Fixed sigabrt raised with dcgettext/dcngettext calls with gettext 0.22.5
        with category set to LC_ALL. (David Carlier)
    
    - MySQLnd:
      . Fix GH-13452 (Fixed handshake response [mysqlnd]). (Saki Takamachi)
      . Fix incorrect charset length in check_mb_eucjpms(). (nielsdos)
    
    - Opcache:
      . Fixed GH-13508 (JITed QM_ASSIGN may be optimized out when op1 is null).
        (Arnaud, Dmitry)
      . Fixed GH-13712 (Segmentation fault for enabled observers when calling trait
        method of internal trait when opcache is loaded). (Bob)
    
    - Random:
      . Fixed bug GH-13544 (Pre-PHP 8.2 compatibility for mt_srand with unknown
        modes). (timwolla)
      . Fixed bug GH-13690 (Global Mt19937 is not properly reset in-between
        requests when MT_RAND_PHP is used). (timwolla)
    
    - Session:
      . Fixed bug GH-13680 (Segfault with session_decode and compilation error).
        (nielsdos)
    
    - SPL:
      . Fixed bug GH-13685 (Unexpected null pointer in zend_string.h). (nielsdos)
    
    - Standard:
      . Fixed bug GH-11808 (Live filesystem modified by tests). (nielsdos)
      . Fixed GH-13402 (Added validation of `\n` in $additional_headers of mail()).
        (SakiTakamachi)
      . Fixed bug GH-13203 (file_put_contents fail on strings over 4GB on Windows).
        (divinity76)
      . Fixed bug GHSA-pc52-254m-w9w7 (Command injection via array-ish $command
        parameter of proc_open). (CVE-2024-1874) (Jakub Zelenka)
      . Fixed bug GHSA-wpj3-hf5j-x4v4 (__Host-/__Secure- cookie bypass due to
        partial CVE-2022-31629 fix). (CVE-2024-2756) (nielsdos)
      . Fixed bug GHSA-h746-cjrr-wfmr (password_verify can erroneously return true,
        opening ATO risk). (CVE-2024-3096) (Jakub Zelenka)
        Fixed bug GHSA-fjp9-9hwx-59fq (mb_encode_mimeheader runs endlessly for some
        inputs). (CVE-2024-2757) (Alex Dowad)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.427 2024/04/13 02:49:41 taca Exp $
+# $NetBSD: phpversion.mk,v 1.428 2024/04/13 02:51:54 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -93,7 +93,7 @@ PHP74_VERSION=	7.4.33
 PHP80_VERSION=	8.0.30
 PHP81_VERSION=	8.1.27
 PHP82_VERSION=	8.2.18
-PHP83_VERSION=	8.3.4
+PHP83_VERSION=	8.3.5
 
 # Define API version or initial release of major version.
 PHP56_RELDATE=	20140828
