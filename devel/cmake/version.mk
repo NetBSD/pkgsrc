673aa5c08646115e17f42fb39ffe58fcf4140dea
adam <adam@pkgsrc.org>
2024-04-13 15:44:17 +0000

    cmake cmake-gui: updated to 3.29.2
    CMake 3.29.2
    * Tests/RunCMake/CompilerChange: Simplify test cases
    * Fix regression on reconfigure with unnormalized -DCMAKE_<LANG>_COMPILER=
    * CMakePackageConfigHelpers: Restore undocumented PACKAGE_PREFIX_DIR variable
    * Swift: Ninja: Remove module dependency for executables
    * FindJasper: Fix Jasper::Jasper IMPORTED_CONFIGURATIONS
    * FindOpenSSL: Update layout for Shining Light Productions 32-bit package
    * FindOpenSSL: Update layout for Shining Light Productions debug libraries

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.56 2024/04/05 08:50:25 adam Exp $
+# $NetBSD: version.mk,v 1.57 2024/04/13 15:44:17 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.29.1
+CMAKE_VERSION=	3.29.2
 CMAKE_API=	${CMAKE_VERSION:R}
