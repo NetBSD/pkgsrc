21bba33f139f25183f4662d1e025466a948ab0cc
adam <adam@pkgsrc.org>
2024-04-14 14:20:28 +0000

    gsound: updated to 1.0.3
    1.0.3
    Unknown changes

--- audio/gsound/buildlink3.mk
+++ audio/gsound/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2021/12/08 16:01:43 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/14 14:20:28 adam Exp $
 
 BUILDLINK_TREE+=	gsound
 
@@ -6,9 +6,10 @@ BUILDLINK_TREE+=	gsound
 GSOUND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gsound+=	gsound>=1.0.2
-BUILDLINK_ABI_DEPENDS.gsound?=	gsound>=1.0.2nb3
+BUILDLINK_ABI_DEPENDS.gsound+=	gsound>=1.0.2nb3
 BUILDLINK_PKGSRCDIR.gsound?=	../../audio/gsound
 
+.include "../../audio/libcanberra/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .endif	# GSOUND_BUILDLINK3_MK
 
