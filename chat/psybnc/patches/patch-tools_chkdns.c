dec418f9397aff691263989fa667ce96810d9ea7
wiz <wiz@pkgsrc.org>
2024-04-14 08:23:22 +0000

    psybnc: update to 2.4.3.
    Version 2.4.3:
    -------------------------------------------------------------------
    08-04-21   - minor fixes

---
+++ chat/psybnc/patches/patch-tools_chkdns.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-tools_chkdns.c,v 1.1 2024/04/14 08:23:22 wiz Exp $
+
+Fix libcares detection (not found because of deprecated symbol).
+
+--- tools/chkdns.c.orig	2024-04-14 08:20:59.195730495 +0000
++++ tools/chkdns.c
+@@ -30,6 +30,6 @@
+ int main(int argc, char **argv)
+ {
+     ares_channel resolver;
+-    ares_init(&resolver);
++    ares_init_options(&resolver, NULL, 0);
+     return 0;
+ }
