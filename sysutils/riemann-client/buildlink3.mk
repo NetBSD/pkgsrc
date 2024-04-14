ad7b37d21f7a1b2ab1dae2727ae15da4e011f9b2
wiz <wiz@pkgsrc.org>
2024-04-14 09:15:34 +0000

    riemann-client: moved off github
    fix some pkglint while here

--- sysutils/riemann-client/buildlink3.mk
+++ sysutils/riemann-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/08 13:21:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/04/14 09:15:34 wiz Exp $
 
 BUILDLINK_TREE+=	riemann-client
 
@@ -12,7 +12,7 @@ BUILDLINK_PKGSRCDIR.riemann-client?=	../../sysutils/riemann-client
 pkgbase := riemann-client
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.riemann-client:Mtls)
+.if ${PKG_BUILD_OPTIONS.riemann-client:Mtls}
 .  include "../../security/gnutls/buildlink3.mk"
 .endif
 
