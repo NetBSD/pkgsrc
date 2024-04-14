ef515ecd60e3d89b3812967af27994519a873c86
adam <adam@pkgsrc.org>
2024-04-14 17:42:09 +0000

    gexiv2: updated to 0.14.2
    gexiv2 0.14.2 - 24 Jul 2023 (stable)
    
    * Make compatible with exiv2 0.28 or later
    * Fix double free if creation of meta-data fails
    * Fix floating point compare in tests

--- graphics/gexiv2/buildlink3.mk
+++ graphics/gexiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/08/14 05:24:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/04/14 17:42:09 adam Exp $
 
 BUILDLINK_TREE+=	gexiv2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gexiv2
 GEXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gexiv2+=	gexiv2>=0.12.0
-BUILDLINK_ABI_DEPENDS.gexiv2?=	gexiv2>=0.14.1nb3
+BUILDLINK_ABI_DEPENDS.gexiv2+=	gexiv2>=0.14.1nb3
 BUILDLINK_PKGSRCDIR.gexiv2?=	../../graphics/gexiv2
 
 .include "../../devel/glib2/buildlink3.mk"
