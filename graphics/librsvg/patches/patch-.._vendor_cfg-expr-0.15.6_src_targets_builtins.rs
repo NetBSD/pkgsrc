13157362def8e161f7b2eaa329639c31226e62dc
adam <adam@pkgsrc.org>
2024-04-14 17:25:28 +0000

    librsvg: updated to 2.58.0
    Version 2.58.0
    
    librsvg crate version 2.58.0
    
    The most significant change in this release is that librsvg no longer
    uses gdk-pixbuf for loading raster images (for eample, when there is
    an element <image href="foo.jpg"/>.  All image loading uses Rust
    codecs now.  Please see the release notes for 2.57.90 below for more
    details.
    
    Librsvg's gdk-pixbuf loader for SVG documents is unchanged; you can
    still load SVG files from gdk-pixbuf with it as usual.
    
    New features since 2.57.0 (for fine-grained bug fixes, please see the
    release notes for development releases):
    
    - Add basic support for "ch" length units in CSS (Mackenzie Morgan).
    
    - Support "var(--foo, #aabbcc)" just for colors.  This is the
      minimum required to render color SVG emoji fonts that provide color
      fallbacks, but it is not yet full support for CSS var().
    
    Fixes since the last development release:
    
    - Limit numOctaves in feTurbulence to avoid unbounded CPU consumption.
    
    - Don't panic when a :lang selector has an argument that is
      not a language-range (Lukáš Tyrychtr).
    
    Special thanks for this release cycle:
    
    - Thanks to correctmost for doing several rounds of fuzzing, and then
      found some interesting bugs!
    
    - Thanks to Jordan Petridis for adding CI jobs for testing on
      gnome-os's regular builds.
    
    - As usual, thanks to Chun-wei Fan for keeping the Windows builds
      going, and Bilal Elmoussaoui for updating gtk-rs crates.

--- graphics/librsvg/patches/patch-.._vendor_cfg-expr-0.15.6_src_targets_builtins.rs
+++ graphics/librsvg/patches/patch-.._vendor_cfg-expr-0.15.6_src_targets_builtins.rs
@@ -1,13 +1,13 @@
-$NetBSD: patch-.._vendor_cfg-expr-0.15.3_src_targets_builtins.rs,v 1.1 2023/09/09 20:43:42 wiz Exp $
+$NetBSD: patch-.._vendor_cfg-expr-0.15.6_src_targets_builtins.rs,v 1.1 2024/04/14 17:25:28 adam Exp $
 
 Add i586-unknown-netbsd.
 Needed because of lang/rust/patches/patch-compiler_rustc__target_src_spec_mod.rs
 Note that the implementation does a binary search of the target list.
 It must be sorted lexically!
 
---- ../vendor/cfg-expr-0.15.3/src/targets/builtins.rs.orig	2006-07-24 01:21:28.000000000 +0000
-+++ ../vendor/cfg-expr-0.15.3/src/targets/builtins.rs
-@@ -1041,6 +1041,19 @@ pub const ALL_BUILTINS: &[TargetInfo] = 
+--- ../vendor/cfg-expr-0.15.6/src/targets/builtins.rs.orig	2006-07-24 01:21:28.000000000 +0000
++++ ../vendor/cfg-expr-0.15.6/src/targets/builtins.rs
+@@ -1106,6 +1106,19 @@ pub const ALL_BUILTINS: &[TargetInfo] =
          panic: Panic::unwind,
      },
      TargetInfo {
