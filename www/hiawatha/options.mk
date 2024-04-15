a76d7e7611c5ff7562ff9382ab8c484ae102892f
hauke <hauke@pkgsrc.org>
2024-04-15 14:52:33 +0000

    Update www/hiawatha to v11.5
    Given a version jump of *two* orders of magnitude, changes are too
    many to enumerate.
    
    This package integrates some of the ideas in ktnb@'s wip/hiawatha, in
    particular statically linking the included mbedtls.
    
    See the discussion at
    <http://mail-index.netbsd.org/tech-pkg/2024/03/23/msg029036.html> --
    built-in letsencrypt with its php dependency is an option.
    
    Also: Upstream is not going away, but just changing its domain name.

---
+++ www/hiawatha/options.mk
@@ -0,0 +1,88 @@
+# $NetBSD: options.mk,v 1.1 2024/04/15 14:52:33 hauke Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.hiawatha
+PKG_SUPPORTED_OPTIONS=		cache letsencrypt monitor rproxy tomahawk
+PKG_SUPPORTED_OPTIONS+=		urltoolkit xslt
+
+PKG_OPTIONS_OPTIONAL_GROUPS=	tls
+PKG_OPTIONS_GROUP.tls=		mbedtls mbedtls-private
+
+PKG_SUGGESTED_OPTIONS=		cache rproxy urltoolkit mbedtls-private xslt
+
+PLIST_VARS+=			letsencrypt urltoolkit xslt
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcache)
+CMAKE_ARGS+=	-DENABLE_CACHE=on
+.else
+CMAKE_ARGS+=	-DENABLE_CACHE=off
+.endif
+
+.if !empty(PKG_OPTIONS:Mletsencrypt)
+PKG_OPTIONS_REQUIRED_GROUPS=	tls
+PLIST.letsencrypt=		yes
+REPLACE_PHP+=			extra/letsencrypt/lefh.in
+
+DEPENDS+=	php-[0-9]*:${PHPPKGSRCDIR}
+.include "../../lang/php/replace.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mmonitor)
+CMAKE_ARGS+=	-DENABLE_MONITOR=on
+.else
+CMAKE_ARGS+=	-DENABLE_MONITOR=off
+.endif
+
+.if !empty(PKG_OPTIONS:Mrproxy)
+CMAKE_ARGS+=	-DENABLE_RPROXY=on
+.else
+CMAKE_ARGS+=	-DENABLE_RPROXY=off
+.endif
+
+.if !empty(PKG_OPTIONS:Mtomahawk)
+CMAKE_ARGS+=	-DENABLE_TOMAHAWK=on
+.else
+CMAKE_ARGS+=	-DENABLE_TOMAHAWK=off
+.endif
+
+# TLS support options
+
+.if !empty(PKG_OPTIONS:Mmbedtls)
+CMAKE_ARGS+=	-DENABLE_TLS=on
+CMAKE_ARGS+=	-DUSE_SYSTEM_MBEDTLS=on
+CONF_FILES+=	${EGDIR}/letsencrypt.conf ${PKG_SYSCONFDIR}/letsencrypt.conf
+.include "../../security/mbedtls3/buildlink3.mk"
+.endif
+.if !empty(PKG_OPTIONS:Mmbedtls-private)
+CMAKE_ARGS+=		-DENABLE_TLS=on
+CMAKE_ARGS+=		-DUSE_SYSTEM_MBEDTLS=off
+CMAKE_ARGS+=		-DUSE_SHARED_MBEDTLS_LIBRARY=OFF
+CMAKE_ARGS+=		-DUSE_STATIC_MBEDTLS_LIBRARY=ON
+##LDFLAGS+=		${COMPILER_RPATH_FLAG}${PREFIX}/lib/hiawatha
+##CONFIGURE_ENV+=		LDFLAGS=${LDFLAGS:Q}
+##INSTALLATION_DIRS+=	lib/hiawatha
+.endif
+.if empty(PKG_OPTIONS:Mmbedtls) && empty(PKG_OPTIONS:Mmbedtls-private)
+CMAKE_ARGS+=	-DENABLE_TLS=off
+.endif
+
+.if !empty(PKG_OPTIONS:Murltoolkit)
+PLIST.urltoolkit=	yes
+CONF_FILES+=		${EGDIR}/toolkit.conf ${PKG_SYSCONFDIR}/toolkit.conf
+
+CMAKE_ARGS+=	-DENABLE_TOOLKIT=on
+.else
+CMAKE_ARGS+=	-DENABLE_TOOLKIT=off
+.endif
+
+.if !empty(PKG_OPTIONS:Mxslt)
+PLIST.xslt=	yes
+CONF_FILES+=	${EGDIR}/error.xslt ${PKG_SYSCONFDIR}/error.xslt
+CONF_FILES+=	${EGDIR}/index.xslt ${PKG_SYSCONFDIR}/index.xslt
+
+CMAKE_ARGS+=	-DENABLE_XSLT=ON
+.include "../../textproc/libxslt/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DENABLE_XSLT=OFF
+.endif
