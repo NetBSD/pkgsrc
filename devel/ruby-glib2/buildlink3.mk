f44a6d135f03c68c507e31037fe24f7e0d267c1f
tsutsui <tsutsui@pkgsrc.org>
2024-04-20 10:16:52 +0000

    ruby-gnome: update to 4.2.2.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.2.2
    
    Ruby-GNOME 4.2.2: 2024-04-02
    
    This is a GLib 2.80 support improvement release.
    
    Changes
    
    Ruby/GIO2
    
      * Improvements
          + Fixed a load error with GLib 2.80.
              o GH-1610
              o Reported by Dmitry Vorotilin
    
    Ruby/GTK4
    
      * Improvements
          + Gtk::CssProvider#load(string:): Added.
              o GH-1608
              o Reported by Sami Sieranoja
          + Gtk::CssProvider#load(bytes:): Added.
              o GH-1608
              o Reported by Sami Sieranoja
    
    Thanks
    
      * Sami Sieranoja
      * Dmitry Vorotilin

--- devel/ruby-glib2/buildlink3.mk
+++ devel/ruby-glib2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/02/23 11:25:28 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/04/20 10:16:53 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-glib2
 
 .if !defined(RUBY_GLIB2_BUILDLINK3_MK)
 RUBY_GLIB2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.1
-BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.1
+BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.2
+BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.2
 BUILDLINK_PKGSRCDIR.ruby-glib2?=	../../devel/ruby-glib2
 
 .include "../../devel/glib2/buildlink3.mk"
