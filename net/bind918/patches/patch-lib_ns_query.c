55d1ccc53a851358367cca022104682e2938437c
taca <taca@pkgsrc.org>
2024-04-20 14:02:39 +0000

    net/bind918: fix blocklist handling
    Apply change of revision 1.21 in NetBSD base which fixed PR bin/58170.
    
    Bump PKGREVISION.

--- net/bind918/patches/patch-lib_ns_query.c
+++ net/bind918/patches/patch-lib_ns_query.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_ns_query.c,v 1.1 2022/12/11 01:57:55 sekiya Exp $
+$NetBSD: patch-lib_ns_query.c,v 1.2 2024/04/20 14:02:40 taca Exp $
 
 * Based on NetBSD, add support for blocklist(blacklist).
 
---- lib/ns/query.c.orig	2022-11-08 07:17:42.056257919 +0900
-+++ lib/ns/query.c	2022-12-10 11:00:47.549957781 +0900
+--- lib/ns/query.c.orig	2024-04-03 09:39:01.839989165 +0000
++++ lib/ns/query.c
 @@ -74,6 +74,10 @@
  #include <ns/stats.h>
  #include <ns/xfrout.h>
@@ -15,17 +15,7 @@ $NetBSD: patch-lib_ns_query.c,v 1.1 2022/12/11 01:57:55 sekiya Exp $
  #if 0
  /*
   * It has been recommended that DNS64 be changed to return excluded
-@@ -915,6 +919,9 @@
- 						NULL);
- 
- 			if (log) {
-+#if defined(HAVE_BLACKLIST_H) || defined(HAVE_BLOCKLIST_H)
-+				pfilter_notify(result, client, "checkcacheaccess");
-+#endif
- 				ns_client_aclmsg("query (cache)", name, qtype,
- 						 client->view->rdclass, msg,
- 						 sizeof(msg));
-@@ -1043,6 +1050,9 @@
+@@ -1047,6 +1051,9 @@ query_validatezonedb(ns_client_t *client
  					      msg);
  			}
  		} else {
