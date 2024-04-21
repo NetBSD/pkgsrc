2ee9b32bb9a50eb776c464ae4c3edc79222c9866
wiz <wiz@pkgsrc.org>
2024-04-21 15:31:33 +0000

    gopls: update to 0.15.3.
    This release fixes the following regressions in gopls@v0.15.0+:
    
        golang/go#66490: occasional crashes when the imports cache is refreshed.
        golang/go#66425: spurious import errors in multi-root workspaces that have go.work replace directives.
        golang/go#66636: a crash in analysis when the go.mod contains a patch version and gopls was built with Go 1.20 or earlier.
        golang/go#66677: silent breakage when the go.mod file contains Go 1.22.x, and gopls was built with Go 1.21.x.
        golang/go#66731: a rare crash when diagnostics are erroneously positioned outside the file due to malformed syntax.
        golang/go#66647: a performance regression due to unnecessary reloading following "workspace/didChangeConfiguration" notifications. Under some not-yet-understood conditions, an apparent VS Code bug causes didChangeConfiguration notifications on every keystroke. With the zero-config logic of gopls@v0.15.0+, any didChangeConfiguration notification causes gopls to re-evaluate (and reload) the set of builds it tracks. With the v0.15.3 release, gopls verifies that configuration actually changed. Special thanks to @gordallott for working with us to track down this bug.

--- devel/gopls/go-modules.mk
+++ devel/gopls/go-modules.mk
@@ -1,5 +1,4 @@
-===> Extracting for gopls-0.15.2
-# $NetBSD: go-modules.mk,v 1.38 2024/03/16 17:13:20 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.39 2024/04/21 15:31:33 wiz Exp $
 
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.mod
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.zip
@@ -26,8 +25,8 @@ GO_MODULE_FILES+=	golang.org/x/text/@v/v0.14.0.mod
 GO_MODULE_FILES+=	golang.org/x/text/@v/v0.14.0.zip
 GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.mod
 GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.18.1-0.20240311201521-78fbdeb61842.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.18.1-0.20240311201521-78fbdeb61842.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.18.1-0.20240412183611-d92ae0781217.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.18.1-0.20240412183611-d92ae0781217.zip
 GO_MODULE_FILES+=	golang.org/x/vuln/@v/v1.0.1.mod
 GO_MODULE_FILES+=	golang.org/x/vuln/@v/v1.0.1.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
