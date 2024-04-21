8d62d74e382fb0f2b7a908f833d0a9389e766346
thorpej <thorpej@pkgsrc.org>
2024-04-21 20:37:52 +0000

    Update to minipro-0.7.  minipro now supports the Xgecu T48 programmer.

--- sysutils/minipro/patches/patch-main.c
+++ sysutils/minipro/patches/patch-main.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-main.c,v 1.1 2022/01/22 17:52:53 thorpej Exp $
+$NetBSD: patch-main.c,v 1.2 2024/04/21 20:37:52 thorpej Exp $
 
 Use ctype(3) functions safely.
 
---- main.c.orig	2022-01-22 15:56:02.000000000 +0000
-+++ main.c
-@@ -922,7 +922,7 @@ int get_config_value(const char *buffer,
-     val = num;
-     cur += 2;  // Advances the pointer to the first numeric character
-     while (cur < eol) {
--      if (isxdigit(*cur++))  // check for hex digit
-+      if (isxdigit((unsigned char)*cur++))  // check for hex digit
-       {
-         *val++ = *(cur - 1);  // put it in the buffer
-       }
+--- main.c.orig	2024-04-21 20:31:05.892824868 +0000
++++ main.c	2024-04-21 20:31:14.668888370 +0000
+@@ -987,7 +987,7 @@ int get_config_value(const char *buffer,
+ 		val = num;
+ 		cur += 2; /* Advances the pointer to the first numeric character */
+ 		while (cur < eol) {
+-			if (isxdigit((int)*cur++)) /* check for hex digit */
++			if (isxdigit((unsigned char)*cur++)) /* check for hex digit */
+ 			{
+ 				*val++ = *(cur - 1); /* put it in the buffer */
+ 			}
