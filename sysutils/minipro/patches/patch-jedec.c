0b66126b2af41bfbc5c7c3805ba9b507937eb2cb
thorpej <thorpej@pkgsrc.org>
2024-04-21 20:37:52 +0000

    Update to minipro-0.7.  minipro now supports the Xgecu T48 programmer.

--- sysutils/minipro/patches/patch-jedec.c
+++ sysutils/minipro/patches/patch-jedec.c
@@ -1,24 +1,24 @@
-$NetBSD: patch-jedec.c,v 1.1 2022/01/22 17:52:53 thorpej Exp $
+$NetBSD: patch-jedec.c,v 1.2 2024/04/21 20:37:52 thorpej Exp $
 
 Use ctype(3) functions safely.
 
---- jedec.c.orig	2022-01-22 15:54:35.000000000 +0000
-+++ jedec.c
-@@ -125,7 +125,7 @@ static int parse_tokens(char *buffer, si
-     if (!*p_token) continue;
+--- jedec.c.orig	2024-04-21 20:28:47.620230367 +0000
++++ jedec.c	2024-04-21 20:30:15.920088540 +0000
+@@ -146,7 +146,7 @@ static int parse_tokens(char *buffer, si
+ 			continue;
  
-     // Skip non printable characters but ETX
--    while (!isalpha(*p_token) && *p_token != ETX) p_token++;
-+    while (!isalpha((unsigned char)*p_token) && *p_token != ETX) p_token++;
+ 		/* Skip non printable characters but ETX */
+-		while (!isalpha((int)*p_token) && *p_token != ETX)
++		while (!isalpha((unsigned char)*p_token) && *p_token != ETX)
+ 			p_token++;
  
-     // Exit the loop if the ETX character is found
-     if (*p_token == ETX) break;
-@@ -237,7 +237,7 @@ static int parse_tokens(char *buffer, si
-          We need to parse each line to get the entire 120 bits row.
-          */
-         while (*p_next != DELIMITER) {
--          if (!iscntrl(*p_next) && *p_next != ' ' && *p_next != '0' &&
-+          if (!iscntrl((unsigned char)*p_next) && *p_next != ' ' && *p_next != '0' &&
-               *p_next != '1')
-             return BAD_FORMAT;
+ 		/* Exit the loop if the ETX character is found */
+@@ -274,7 +274,7 @@ static int parse_tokens(char *buffer, si
+ 			 * 120 bits row.
+ 			 */
+ 			while (*p_next != DELIMITER) {
+-				if (!iscntrl((int)*p_next) && *p_next != ' ' &&
++				if (!iscntrl((unsigned char)*p_next) && *p_next != ' ' &&
+ 				    *p_next != '0' && *p_next != '1')
+ 					return BAD_FORMAT;
  
