6a235e303ae22712587f93223f2eaa6dbb753155
nikita <nikita@pkgsrc.org>
2024-04-22 13:23:55 +0000

    nim: update to version 2.0.4
    Changelog (from https://nim-lang.org/blog/2024/04/16/versions-1620-204-released.html)
    
     Versions 2.0.4 and 1.6.20 released
    16 April 2024 The Nim Team
    
    The Nim team is happy to announce two releases:
    
        the latest Nim, version 2.0.4
        LTS release, version 1.6.20
    
    For the majority of our users, v2.0.4 contains 23 commits and brings bugfixes and improvements to Nim 2.0.2, released four months ago.
    
    For those users who haven’t switched to Nim v2.0 yet, we have released the ninth patch release for Nim 1.6.x. It is a small release with 13 commits.
    Installing Nim 2.0.4
    New users
    
    Check out if the package manager of your OS already ships version 2.0.4 or install it as described here.
    Existing users
    
    If you have installed a previous version of Nim using choosenim, getting Nim 2.0.4 is as easy as:
    
    $ choosenim 2.0.4
    
    If you are on OSX ARM, choosenim will not work for you. Please choose an alternative method of installing or updating.
    
    Alternatively, you can download Nim 2.0.4 from our nightlies builds.
    Donating to Nim
    
    We would like to encourage you to donate to Nim. The donated money will be used to further improve Nim by creating bounties for the most important bugfixes and features.
    
    You can donate via:
    
        Open Collective
        Bitcoin: 1BXfuKM2uvoD6mbx4g5xM3eQhLzkCK77tJ
    
    If you are a company, we also offer commercial support.
    Bugfixes
    
    These reported issues were fixed:
    
        Fixed “import sth from mymodule is compiles and trying to see module sthfrommymodule” (#23148)
        Fixed “Operations on inline toOpenArray len return a wrong result” (#23280)
        Fixed “Nimsuggest segfault with invalid assignment to table” (#22753)
    
    The complete list of changes is available here.
    
    Installing Nim 1.6.20
    New users
    
    If you are a new user, we highly recommend that you start with Nim 2.0.x — see the sections above on how to install it.
    Existing users
    
    If you have installed a previous version of Nim using choosenim, getting Nim 1.6.20 is as easy as:
    
    $ choosenim 1.6.20
    
    If you are on OSX ARM, choosenim will not work for you. Please choose an alternative method of installing or updating.
    
    Alternatively, you can download Nim 1.6.20 from our nightlies builds.
    Bugfixes
    
    These reported issues were fixed:
    
        Fixed “import sth from mymodule is compiles and trying to see module sthfrommymodule” (#23148)
        Fixed “Operations on inline toOpenArray len return a wrong result” (#23280)
        Fixed “[Backport 2.0] Itanium mangling scheme” (#23301)
        Fixed “Nimsuggest segfault with invalid assignment to table” (#22753)
    
    The complete list of changes is available here.

--- lang/nim/buildlink3.mk
+++ lang/nim/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/04/25 15:34:38 nikita Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/04/22 13:23:55 nikita Exp $
 
 BUILDLINK_TREE+=	nim
 
 .if !defined(NIM_BUILDLINK3_MK)
 NIM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nim+=	nim>=1.2.6
-BUILDLINK_ABI_DEPENDS.nim+=	nim>=1.2.6
+BUILDLINK_API_DEPENDS.nim+=	nim>=2.0.4
+BUILDLINK_ABI_DEPENDS.nim+=	nim>=2.0.4
 BUILDLINK_PKGSRCDIR.nim?=	../../lang/nim
 
 .endif # NIM_BUILDLINK3_MK
