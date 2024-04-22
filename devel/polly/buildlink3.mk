a0b3a7313c1b9f73ff23fe8c17efbe1217e4c7ca
adam <adam@pkgsrc.org>
2024-04-22 07:25:56 +0000

    polly openmp: updated to 17.0.6
    17.0.6
    https://releases.llvm.org/17.0.1/tools/polly/docs/ReleaseNotes.html

--- devel/polly/buildlink3.mk
+++ devel/polly/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/10/06 19:15:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/22 07:25:56 adam Exp $
 
 BUILDLINK_TREE+=	polly
 
 .if !defined(POLLY_BUILDLINK3_MK)
 POLLY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.polly+=	polly>=16.0.0<17
+BUILDLINK_API_DEPENDS.polly+=	polly>=17.0.0<18
 BUILDLINK_PKGSRCDIR.polly?=	../../devel/polly
 
 .include "../../lang/llvm/buildlink3.mk"
--- parallel/openmp/buildlink3.mk
+++ parallel/openmp/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/10/06 19:15:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/04/22 07:25:56 adam Exp $
 
 BUILDLINK_TREE+=	openmp
 
 .if !defined(OPENMP_BUILDLINK3_MK)
 OPENMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openmp+=	openmp>=16.0.0<17
+BUILDLINK_API_DEPENDS.openmp+=	openmp>=17.0.0<18
+BUILDLINK_ABI_DEPENDS.openmp+=	openmp>=17.0.0
 BUILDLINK_PKGSRCDIR.openmp?=	../../parallel/openmp
 .endif	# OPENMP_BUILDLINK3_MK
 
