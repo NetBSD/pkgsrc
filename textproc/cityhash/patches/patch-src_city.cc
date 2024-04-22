bc7625b8196d74e6b5cb4dad6d239b671f254583
he <he@pkgsrc.org>
2024-04-22 10:21:52 +0000

    textproc/cityhash: Fix NetBSD implementation.
    Don't probe for implementation-specific symbols, there's no need.

--- textproc/cityhash/patches/patch-src_city.cc
+++ textproc/cityhash/patches/patch-src_city.cc
@@ -1,13 +1,20 @@
-$NetBSD: patch-src_city.cc,v 1.2 2014/01/19 02:54:56 rodent Exp $
+$NetBSD: patch-src_city.cc,v 1.3 2024/04/22 10:21:53 he Exp $
 
 Add support for SunOS.
+Fix support for NetBSD, submitted as
+  https://github.com/google/cityhash/pull/40
 
 --- src/city.cc.orig	2013-06-17 19:45:57.000000000 +0000
 +++ src/city.cc
-@@ -69,6 +69,16 @@ static uint32 UNALIGNED_LOAD32(const cha
- #define bswap_64(x) bswap64(x)
- #endif
+@@ -64,10 +64,18 @@ static uint32 UNALIGNED_LOAD32(const cha
  
+ #include <sys/types.h>
+ #include <machine/bswap.h>
+-#if defined(__BSWAP_RENAME) && !defined(__bswap_32)
+ #define bswap_32(x) bswap32(x)
+ #define bswap_64(x) bswap64(x)
+-#endif
++
 +#elif defined(__sun)
 +#include <sys/byteorder.h>
 +#define bswap_32(x) htonl(x)
@@ -17,7 +24,6 @@ Add support for SunOS.
 +#include <sys/endian.h>
 +#define bswap_32(x) bswap32(x)
 +#define bswap_64(x) bswap64(x)
-+
+ 
  #else
  
- #include <byteswap.h>
