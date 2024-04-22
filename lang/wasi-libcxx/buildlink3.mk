d2d7d680c163a6eaab875519f094f34fef073d24
adam <adam@pkgsrc.org>
2024-04-22 07:26:42 +0000

    wasi-compiler-rt wasi-libcxx: updated to 17.0.6
    17.0.6
    Match LLVM version

--- lang/wasi-libcxx/buildlink3.mk
+++ lang/wasi-libcxx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/10/06 19:18:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/04/22 07:26:42 adam Exp $
 
 BUILDLINK_TREE+=	wasi-libcxx
 
@@ -7,7 +7,7 @@ BUILDLINK_DEPMETHOD.wasi-libcxx?=	build
 .if !defined(WASI_LIBCXX_BUILDLINK3_MK)
 WASI_LIBCXX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=16.0.0<17
+BUILDLINK_API_DEPENDS.wasi-libcxx+=	wasi-libcxx>=17.0.0<18
 BUILDLINK_PKGSRCDIR.wasi-libcxx?=	../../lang/wasi-libcxx
 
 .endif	# WASI_LIBCXX_BUILDLINK3_MK
