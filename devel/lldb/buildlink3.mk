0514490250342b4e5a29c61ef5743701ce055f81
adam <adam@pkgsrc.org>
2024-04-22 07:06:38 +0000

    lld lldb: updated to 17.0.6
    17.0.6
    https://releases.llvm.org/17.0.1/tools/lld/docs/ReleaseNotes.html

--- devel/lldb/buildlink3.mk
+++ devel/lldb/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2023/11/08 13:18:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2024/04/22 07:06:39 adam Exp $
 
 BUILDLINK_TREE+=	lldb
 
 .if !defined(LLDB_BUILDLINK3_MK)
 LLDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lldb+=	lldb>=16.0.0<17
-BUILDLINK_ABI_DEPENDS.lldb?=	lldb>=16.0.6nb1
+BUILDLINK_API_DEPENDS.lldb+=	lldb>=17.0.0<18
+BUILDLINK_ABI_DEPENDS.lldb+=	lldb>=17.0.0
 BUILDLINK_PKGSRCDIR.lldb?=	../../devel/lldb
 
 .include "../../devel/zlib/buildlink3.mk"
