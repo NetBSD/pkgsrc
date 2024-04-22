17dd1b371eb95dbe87b309d4d4a01d59345aa2e5
adam <adam@pkgsrc.org>
2024-04-22 07:28:21 +0000

    mlir flang: updated to 17.0.6
    17.0.6
    https://releases.llvm.org/17.0.1/tools/flang/docs/ReleaseNotes.html

--- lang/mlir/buildlink3.mk
+++ lang/mlir/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/10/06 19:15:21 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/04/22 07:28:21 adam Exp $
 
 BUILDLINK_TREE+=	mlir
 
 .if !defined(MLIR_BUILDLINK3_MK)
 MLIR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mlir+=	mlir>=16.0.0<17
+BUILDLINK_API_DEPENDS.mlir+=	mlir>=17.0.0<18
 BUILDLINK_PKGSRCDIR.mlir?=	../../lang/mlir
 
 BUILDLINK_FILES.mlir+=		bin/mlir-tblgen
