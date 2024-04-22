15a642b6eea04913bd6f842d0664671e35149546
adam <adam@pkgsrc.org>
2024-04-22 07:05:14 +0000

    libunwind compiler-rt libcxx libcxxabi: updated to 17.0.6
    17.0.6
    https://releases.llvm.org/17.0.1/projects/libcxx/docs/ReleaseNotes/17.html

--- lang/compiler-rt/buildlink3.mk
+++ lang/compiler-rt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/06 19:15:20 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/04/22 07:05:15 adam Exp $
 
 BUILDLINK_TREE+=	compiler-rt
 
 .if !defined(COMPILER_RT_BUILDLINK3_MK)
 COMPILER_RT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.compiler-rt+=	compiler-rt>=16.0.0<17
+BUILDLINK_API_DEPENDS.compiler-rt+=	compiler-rt>=17.0.0<18
 BUILDLINK_PKGSRCDIR.compiler-rt?=	../../lang/compiler-rt
 
 .include "../../lang/llvm/buildlink3.mk"
