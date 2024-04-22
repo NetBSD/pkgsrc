5ad3016b01e4c12df719a29de7802d42d29ce697
adam <adam@pkgsrc.org>
2024-04-22 07:06:38 +0000

    lld lldb: updated to 17.0.6
    17.0.6
    https://releases.llvm.org/17.0.1/tools/lld/docs/ReleaseNotes.html

--- devel/lld/buildlink3.mk
+++ devel/lld/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/10/06 19:15:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/04/22 07:06:39 adam Exp $
 
 BUILDLINK_TREE+=	lld
 
 .if !defined(LLD_BUILDLINK3_MK)
 LLD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lld+=	lld>=16.0.0<17
+BUILDLINK_API_DEPENDS.lld+=	lld>=17.0.0<18
 BUILDLINK_PKGSRCDIR.lld?=	../../devel/lld
 
 .include "../../lang/llvm/buildlink3.mk"
