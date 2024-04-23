574a84dc9c0cb14c139c38e39276321ea1dfa1e3
wiz <wiz@pkgsrc.org>
2024-04-23 22:12:04 +0000

    darktable: add build fix from upstream
    Via Dave Tyson on pkgsrc-users.

--- graphics/darktable/hacks.mk
+++ graphics/darktable/hacks.mk
@@ -1,10 +1,10 @@
-# $NetBSD: hacks.mk,v 1.3 2023/09/01 16:23:09 nia Exp $
+# $NetBSD: hacks.mk,v 1.4 2024/04/23 22:12:04 wiz Exp $
 
 .if !defined(DARKTABLE_HACKS_MK)
 DARKTABLE_HACKS_MK:=
 
-.  include "../../mk/bsd.fast.prefs.mk"
-.  if ${OPSYS} == "NetBSD" && !empty(PKGSRC_COMPILER:M*gcc*)
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "NetBSD" && !empty(PKGSRC_COMPILER:M*gcc*)
 PKG_HACKS+=		disable-graphite-opt
 # Disable some of the more ridiculous compiler flags being used, that are
 # creating build failures with the system compiler on NetBSD due to the
@@ -12,12 +12,12 @@ PKG_HACKS+=		disable-graphite-opt
 CFLAGS+=		-Wno-unknown-pragmas
 SUBST_CLASSES+=		opt
 SUBST_MESSAGE.opt=	Disabling excessive forced optimizations.
-SUBST_STAGE.opt+=	pre-configure
+SUBST_STAGE.opt=	pre-configure
 SUBST_SED.opt+=		-e "s,pragma GCC optimize,pragma GCC nooptimize,g"
 SUBST_FILES.opt+=	src/iop/*.c
 SUBST_FILES.opt+=	src/iop/demosaicing/*.c
 SUBST_FILES.opt+=	src/develop/blends/*.c
 SUBST_FILES.opt+=	src/common/*.c
 SUBST_FILES.opt+=	src/common/*.h
-.  endif
+.endif
 .endif
