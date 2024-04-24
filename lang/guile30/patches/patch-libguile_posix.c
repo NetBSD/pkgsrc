5e0219949beb54f8605d2ce4fbe833d6f0263770
wiz <wiz@pkgsrc.org>
2024-04-24 06:47:55 +0000

    guile30: compilation fix for previous
    (posix.c:1573:7: error: label at end of compound statement)

--- lang/guile30/patches/patch-libguile_posix.c
+++ lang/guile30/patches/patch-libguile_posix.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-libguile_posix.c,v 1.1 2024/04/23 16:34:09 jperkin Exp $
+$NetBSD: patch-libguile_posix.c,v 1.2 2024/04/24 06:47:55 wiz Exp $
 
 Wrap dprintf() in HAVE_DPRINTF.
 
---- libguile/posix.c.orig	2024-04-23 15:53:58.536124795 +0000
+--- libguile/posix.c.orig	2023-01-23 10:47:35.000000000 +0000
 +++ libguile/posix.c
-@@ -1573,8 +1573,10 @@ piped_process (pid_t *pid, SCM prog, SCM
+@@ -1573,8 +1573,11 @@ piped_process (pid_t *pid, SCM prog, SCM
        default:    /* ENOENT, etc. */
          /* Report the error on the console (before switching to
             'posix_spawn', the child process would do exactly that.)  */
@@ -12,6 +12,7 @@ Wrap dprintf() in HAVE_DPRINTF.
          dprintf (err, "In execvp of %s: %s\n", exec_file,
                   strerror (errno_save));
 +#endif
++	break;
        }
  
    free (exec_file);
