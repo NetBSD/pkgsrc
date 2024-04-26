df975641ab180779dd078e3779099678561df020
rillig <rillig@pkgsrc.org>
2024-04-26 18:50:21 +0000

    pkgtools/pkglint: update to 23.4.1
    Changes since 23.4.0:
    
    Add riscv32 as a known hardware architecture.
    
    Don't suggest a runtime dependency instead of TOOL_DEPENDS.

--- pkgtools/pkglint/go-modules.mk
+++ pkgtools/pkglint/go-modules.mk
@@ -1,13 +1,13 @@
-# $NetBSD: go-modules.mk,v 1.3 2024/01/12 23:37:54 rillig Exp $
+# $NetBSD: go-modules.mk,v 1.4 2024/04/26 18:50:21 rillig Exp $
 
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.mod
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.2.1.zip
 GO_MODULE_FILES+=	github.com/kr/pty/@v/v1.1.1.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.1.0.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.17.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.17.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.15.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.15.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.22.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.22.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.19.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.19.0.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.mod
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v1.0.0-20201130134442-10cb98267c6c.zip
