b4f9846b23585431b6c04e0b156a29f7066fedaf
tnn <tnn@pkgsrc.org>
2024-04-26 16:46:29 +0000

    kicad-*: update to 8.0.1
    No changelog available; those interested in what's new in 8.x may refer
    to the forum thread:
    https://forum.kicad.info/t/post-v7-new-features-and-development-news/40144/10

--- cad/kicad/patches/patch-eeschema_sim_sim_property.cpp
+++ cad/kicad/patches/patch-eeschema_sim_sim_property.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-eeschema_sim_sim_property.cpp,v 1.1 2023/06/06 10:20:58 bouyer Exp $
+$NetBSD: patch-eeschema_sim_sim_property.cpp,v 1.2 2024/04/26 16:46:29 tnn Exp $
 
 Needs isnan in scope on NetBSD.
 
---- eeschema/sim/sim_property.cpp.orig	2023-06-03 11:54:31.739102496 +0200
-+++ eeschema/sim/sim_property.cpp	2023-06-03 11:55:13.347928053 +0200
+--- eeschema/sim/sim_property.cpp.orig	2024-03-13 15:55:00.000000000 +0000
++++ eeschema/sim/sim_property.cpp
 @@ -30,6 +30,8 @@
  #include <sim/sim_value.h>
  #include <ki_exception.h>
@@ -11,5 +11,5 @@ Needs isnan in scope on NetBSD.
 +using std::isnan;
 +
  
- SIM_PROPERTY::SIM_PROPERTY( SIM_MODEL& aModel, int aParamIndex ) :
-         m_model( aModel ),
+ /**
+  *
