5f87d10e7fefbbdc5f1a45f6b8bb3e78d1bda42a
micha <micha@pkgsrc.org>
2024-04-26 13:05:01 +0000

    cross/avr-gcc: Update to 13.2.0
    Patch for NetBSD support removed (does no longer apply, it seems that it
    is no longer required).
    Patch for Darwin/aarch64 removed (as requested in patch comment by tnn@).
    Patches for build system added (nonportable "==" shell syntax).
    Patch for libgcc added (system header file "sys/mman.h" not found).
    Disable PKGSRC_MKPIE (build failed on AMD64).
    Needs a C++11 compiler for bootstrap.
    
    Upstream changelog2:
    <https://gcc.gnu.org/gcc-13/changes.html>
    <https://gcc.gnu.org/gcc-12/changes.html>
    <https://gcc.gnu.org/gcc-11/changes.html>

--- cross/avr-gcc/buildlink3.mk
+++ cross/avr-gcc/buildlink3.mk
@@ -1,11 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/09/08 12:07:06 micha Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2024/04/26 13:05:01 micha Exp $
 
 BUILDLINK_TREE+=	avr-gcc
 
 .if !defined(AVR_GCC_BUILDLINK3_MK)
 AVR_GCC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.avr-gcc+=	avr-gcc>=10.3.0
+USE_CXX_FEATURES+=	c++11
+
+BUILDLINK_API_DEPENDS.avr-gcc+=	avr-gcc>=13.2.0
 BUILDLINK_PKGSRCDIR.avr-gcc?=	../../cross/avr-gcc
 .endif	# AVR_GCC_BUILDLINK3_MK
 
