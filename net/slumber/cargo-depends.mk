3f9c03b43845e08cefeffd341df83f124730c8b8
pin <pin@pkgsrc.org>
2024-04-28 16:31:13 +0000

    net/slumber: update to 1.0.1
    [1.0.1] - 2024-04-27
    Added
        Add two new build targets to releases: x86_64-pc-windows-msvc and x86_64-unknown-linux-musl
    
    Fixed
        Fix build on Windows (#180)
            I can't guarantee it works on Windows since I don't have a machine to test on, but it at least compiles now

--- net/slumber/cargo-depends.mk
+++ net/slumber/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.11 2024/04/26 09:43:29 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.12 2024/04/28 16:31:13 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.21.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
