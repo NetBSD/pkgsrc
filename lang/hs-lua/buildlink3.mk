0f3bf3732481472c200f772ca50e9dc36916ee85
pho <pho@pkgsrc.org>
2024-04-28 16:46:29 +0000

    lang/hs-lua: Update to 2.3.2
    lua-2.3.2
        Update to Lua 5.4.6.

--- lang/hs-lua/buildlink3.mk
+++ lang/hs-lua/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/28 16:46:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-lua
 
 .if !defined(HS_LUA_BUILDLINK3_MK)
 HS_LUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.3.1
-BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.3.1nb1
+BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.3.2
+BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.3.2
 BUILDLINK_PKGSRCDIR.hs-lua?=	../../lang/hs-lua
 
 .include "../../lang/lua54/buildlink3.mk"
