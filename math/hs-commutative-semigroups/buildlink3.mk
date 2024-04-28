b185a9d72f657242f84c5c41da5c6ca1ae9a286d
pho <pho@pkgsrc.org>
2024-04-28 11:54:38 +0000

    math/hs-commutative-semigroups: Update to 0.2
    0.2 -- 2024-04-09
        Removed the instances for the Event and Lifetime types unavailable on Windows
    
    0.1.1.0 -- 2024-03-24
        Added more trivial instances for semigroups from base
    
    0.1.0.2 -- 2023-12-22
        Support GHC 9.8.1

--- math/hs-commutative-semigroups/buildlink3.mk
+++ math/hs-commutative-semigroups/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/11/02 06:37:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/04/28 11:54:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-commutative-semigroups
 
 .if !defined(HS_COMMUTATIVE_SEMIGROUPS_BUILDLINK3_MK)
 HS_COMMUTATIVE_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.1.0.1nb1
+BUILDLINK_API_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.2
+BUILDLINK_ABI_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.2
 BUILDLINK_PKGSRCDIR.hs-commutative-semigroups?=		../../math/hs-commutative-semigroups
 .endif	# HS_COMMUTATIVE_SEMIGROUPS_BUILDLINK3_MK
 
