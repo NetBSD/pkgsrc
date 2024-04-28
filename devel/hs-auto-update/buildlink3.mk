4e41fa3168cc3cfbfc01407d58955fa1431ab00d
pho <pho@pkgsrc.org>
2024-04-28 20:34:00 +0000

    devel/hs-auto-update: Update to 0.2.0
    0.2.0
      * Creating Reaper.Internal to export Reaper constructor.
      * Hiding Reaper constructor.
      * Add reaperModify to the Reaper API, allowing workload modification
        outside of the main reaperAction loop. #985

--- devel/hs-auto-update/buildlink3.mk
+++ devel/hs-auto-update/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:36:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/04/28 20:34:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-auto-update
 
 .if !defined(HS_AUTO_UPDATE_BUILDLINK3_MK)
 HS_AUTO_UPDATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-auto-update+=	hs-auto-update>=0.1.6
-BUILDLINK_ABI_DEPENDS.hs-auto-update+=	hs-auto-update>=0.1.6nb6
+BUILDLINK_API_DEPENDS.hs-auto-update+=	hs-auto-update>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-auto-update+=	hs-auto-update>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-auto-update?=	../../devel/hs-auto-update
 .endif	# HS_AUTO_UPDATE_BUILDLINK3_MK
 
