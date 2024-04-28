8d0d3aa5f2b04a7c417c5451511679cde4f97ae0
pho <pho@pkgsrc.org>
2024-04-28 17:04:59 +0000

    devel/hs-th-compat: Update to 0.1.5
    0.1.5 [2024.03.19]
        Support building with template-haskell-2.22.* (GHC 9.10).

--- devel/hs-th-compat/buildlink3.mk
+++ devel/hs-th-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/11/02 06:37:00 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2024/04/28 17:04:59 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-compat
 
 .if !defined(HS_TH_COMPAT_BUILDLINK3_MK)
 HS_TH_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.4
-BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.4nb3
+BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.5
+BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.5
 BUILDLINK_PKGSRCDIR.hs-th-compat?=	../../devel/hs-th-compat
 .endif	# HS_TH_COMPAT_BUILDLINK3_MK
 
