a4ae2e79105b87b22da82d50d683daf1ba9f36ff
pho <pho@pkgsrc.org>
2024-04-28 20:19:56 +0000

    devel/hs-terminal-size: Update to 0.3.4
    0.3.4
        Provided hSize on Windows. (https://github.com/biegunka/terminal-size/pull/18)

--- devel/hs-terminal-size/buildlink3.mk
+++ devel/hs-terminal-size/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/11/02 06:36:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/04/28 20:19:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-terminal-size
 
 .if !defined(HS_TERMINAL_SIZE_BUILDLINK3_MK)
 HS_TERMINAL_SIZE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.3nb3
+BUILDLINK_API_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-terminal-size+=	hs-terminal-size>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-terminal-size?=		../../devel/hs-terminal-size
 .endif	# HS_TERMINAL_SIZE_BUILDLINK3_MK
 
