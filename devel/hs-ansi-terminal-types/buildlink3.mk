8ed40ecbd19b4d059e7a10f71a32299abe5a97c4
pho <pho@pkgsrc.org>
2024-04-28 20:37:47 +0000

    devel/hs-ansi-terminal-types: Update to 1.1
    Version 1.1
        Add Underlining constructor to type ConsoleLayer.
        Add CurlyUnderline, DottedUnderline, and DashedUnderline constructors to type Underlining.

--- devel/hs-ansi-terminal-types/buildlink3.mk
+++ devel/hs-ansi-terminal-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:36:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/28 20:37:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal-types
 
 .if !defined(HS_ANSI_TERMINAL_TYPES_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal-types+=	hs-ansi-terminal-types>=0.11.5
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal-types+=	hs-ansi-terminal-types>=0.11.5nb1
+BUILDLINK_API_DEPENDS.hs-ansi-terminal-types+=	hs-ansi-terminal-types>=1.1
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal-types+=	hs-ansi-terminal-types>=1.1
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal-types?=	../../devel/hs-ansi-terminal-types
 
 .include "../../devel/hs-colour/buildlink3.mk"
