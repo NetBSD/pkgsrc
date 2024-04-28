bebb1c502ab5c8e7a248fffbf0861527f1ad2707
pho <pho@pkgsrc.org>
2024-04-28 12:21:19 +0000

    devel/hs-vector-stream: Update to 0.1.0.1
    Changes in version 0.1.0.1
        Compatibility with future GHC 9.10 release #462

--- devel/hs-vector-stream/buildlink3.mk
+++ devel/hs-vector-stream/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/11/02 06:37:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/04/28 12:21:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-stream
 
 .if !defined(HS_VECTOR_STREAM_BUILDLINK3_MK)
 HS_VECTOR_STREAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector-stream+=	hs-vector-stream>=0.1.0.0
-BUILDLINK_ABI_DEPENDS.hs-vector-stream?=	hs-vector-stream>=0.1.0.0nb3
+BUILDLINK_API_DEPENDS.hs-vector-stream+=	hs-vector-stream>=0.1.0.1
+BUILDLINK_ABI_DEPENDS.hs-vector-stream?=	hs-vector-stream>=0.1.0.1
 BUILDLINK_PKGSRCDIR.hs-vector-stream?=		../../devel/hs-vector-stream
 .endif	# HS_VECTOR_STREAM_BUILDLINK3_MK
 
