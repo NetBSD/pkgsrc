422f29c9737d2109d51795c4c7201daa2fd80b25
pho <pho@pkgsrc.org>
2024-04-29 04:40:08 +0000

    math/hs-monoid-subclasses: Update to 1.2.5
    Version 1.2.5
    
        Data.Monoid.Null.MonoidNull now has a default implementation
        Add instances of MonoidNull and (where appropriate) PositiveMonoid for:
            (a, b, c, d, e)
            Data.Functor.Compose f g a
            Data.Functor.Const r a
            Data.Functor.Identity a
            Data.Functor.Product f g a
            Data.Ord.Down a
            Data.Proxy.Proxy a
            Data.Semigroup.Max a
            Data.Semigroup.Min a
            Data.Semigroup.WrappedMonoid a
            GHC.Generics.(:*:) f g a
            GHC.Generics.(:.:) f g a
            GHC.Generics.K1 i c p
            GHC.Generics.M1 i c f p
            GHC.Generics.Par1 p
            GHC.Generics.Rec1 f p
            GHC.Generics.U1 p
        Added instances of Reductive for Max a, Min a, Any, and All
        Bumped the commutative-semigroups dependency upper bound

--- math/hs-monoid-subclasses/buildlink3.mk
+++ math/hs-monoid-subclasses/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/29 04:40:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-monoid-subclasses
 
 .if !defined(HS_MONOID_SUBCLASSES_BUILDLINK3_MK)
 HS_MONOID_SUBCLASSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.4
-BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.4.1nb1
+BUILDLINK_API_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.5
+BUILDLINK_ABI_DEPENDS.hs-monoid-subclasses+=	hs-monoid-subclasses>=1.2.5
 BUILDLINK_PKGSRCDIR.hs-monoid-subclasses?=	../../math/hs-monoid-subclasses
 
 .include "../../math/hs-commutative-semigroups/buildlink3.mk"
