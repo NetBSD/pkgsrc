954978146d0546103c106fc847e0b6bb72cb0071
pho <pho@pkgsrc.org>
2024-04-29 23:10:55 +0000

    devel/hs-unordered-containers: Update to 0.2.20
    [0.2.20] - January 2024
    
        Allow template-haskell-2.21
        Rename confusing variables
        Deal with introduction of Prelude.foldl'
        Remove redundant Hashable constraints from intersection.* and union.*.
        Various optimizations and cleanups: #458, #469, #404, #460, #456, #433
        Add invariant tests: #444, #455
        Improve test case generation
        Improve test failure reporting

--- devel/hs-unordered-containers/buildlink3.mk
+++ devel/hs-unordered-containers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.21 2023/11/02 06:37:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2024/04/29 23:10:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.16
-BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.19.1nb3
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.20
+BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.20
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../devel/hs-unordered-containers
 
 .include "../../devel/hs-hashable/buildlink3.mk"
