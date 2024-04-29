424de43c4c5fe730baf00c219d9a4a1a2313ccc4
pho <pho@pkgsrc.org>
2024-04-29 04:12:05 +0000

    wm/xmonad: Update to 0.18.0
    0.18.0 (February 3, 2024)
    
      Breaking Changes
    
      * Dropped support for GHC 8.4.
    
      Enhancements
    
      * Exported sendRestart and sendReplace from XMonad.Operations.
      * Exported buildLaunch from XMonad.Main.
      * Tall does not draw windows with zero area.
      * XMonad.Operations.floatLocation now applies size hints. This means
        windows will snap to these hints as soon as they're floated (mouse
        move, keybinding). Previously that only happened on mouse resize.
      * Recompilation now detects flake.nix and default.nix (can be a symlink)
        and switches to using nix build as appropriate.
      * Added unGrab to XMonad.Operations; this releases XMonad's passive
        keyboard grab, so other applications (like scrot) can do their thing.
    
      Bug Fixes
    
      * Duplicated floats (e.g. from X.A.CopyToAll) no longer escape to
        inactive screens.

--- wm/xmonad/buildlink3.mk
+++ wm/xmonad/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2023/11/02 06:37:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2024/04/29 04:12:05 pho Exp $
 
 BUILDLINK_TREE+=	xmonad
 
 .if !defined(XMONAD_BUILDLINK3_MK)
 XMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.17.2
-BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.17.2nb1
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.18.0
+BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.18.0
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wm/xmonad
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
