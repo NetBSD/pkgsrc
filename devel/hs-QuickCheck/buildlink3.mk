c4eb2df542034db866c0452946d2886ffdfefb7c
pho <pho@pkgsrc.org>
2024-04-29 03:33:52 +0000

    devel/hs-QuickCheck: Update to 2.15
    QuickCheck 2.15
    	* BREAKING: properties without quantification are no longer implicitly
    	  wrapped in once
    	* Add witness property modifier and witnesses field in result on
    	compilers that support Typeable to allow conveniently exposing
    	counterexamples
    	* Add assertException, assertExceptionIO, assertDeepException, and
    	assertDeepExceptionIO (thanks to Alexey Kuleshevich)
    	* Add withMaxSize, withMaxShrinks, and withMaxDiscardRatio combinators
    	* Add recheck
    	* Add HasCallStack to partial top-level functions
    	* Updated some documentation
    	* Fix overflow bug when calculating discard ratio
    	* Fix error printing bug when shrinks are discarded
    	* Fix withMaxSuccess not working when checkCoverage is turned on
    	* Fix a bug whereby an unfortunately timed discard could unduly fail a
    	property running with checkCoverage
    	* Fix Arbitrary intance for Map breaking invariants from
    	Data.Map.Strict (thanks to Neil Mayhew)
    	* Fix non-covered classes not showing up in output as 0% covered
    	* Fix Negative's Arbitrary instance discarding an unnecessary number
    	of generated values.
    	* Fix promote doing an unnecessary seed split
    	* Fix a bug whereby maxSuccess and discardRatio would interact poorly
    	to produce too few size increases.
    	* Fix a couple of underflow bugs in generators for Small Natural and
    	Ratio Natural
    	* Fix a bug whereby the internal DISCARDED exception would show up
    	user-side in sample

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 06:36:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/04/29 03:33:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.3
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.14.3nb1
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.15
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.15
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
