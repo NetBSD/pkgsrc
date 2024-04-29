97753ca5701b8af159cf590a18b0391c58be2ec1
pho <pho@pkgsrc.org>
2024-04-29 02:52:29 +0000

    textproc/hs-blaze-html: Update to 0.9.2.0
    - 0.9.2.0 (2024-03-06)
        * Make XHTML5 doctype lowercase
        * Add new attributes to (X)HTML5:
            - `download`
            - `minlength`
            - `muted`
            - `onkeypress`
            - `poster`
            - `property`
        * Add new elements to (X)HTML5:
            - `bdi`
            - `data`
            - `dialog`
            - `picture`
            - `s`
            - `search`
            - `slot`
            - `template`

--- textproc/hs-blaze-html/buildlink3.mk
+++ textproc/hs-blaze-html/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2023/11/02 06:37:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2024/04/29 02:52:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-html
 
 .if !defined(HS_BLAZE_HTML_BUILDLINK3_MK)
 HS_BLAZE_HTML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.9.1
-BUILDLINK_ABI_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.9.1.2nb7
+BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.9.2
+BUILDLINK_ABI_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.9.2.0
 BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../textproc/hs-blaze-html
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
