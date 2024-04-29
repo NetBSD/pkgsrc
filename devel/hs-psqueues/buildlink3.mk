be8580c86b6f0d98262d9c00b9b8f4b08cfe0e4b
pho <pho@pkgsrc.org>
2024-04-29 05:01:12 +0000

    devel/hs-psqueues: Update to 0.2.8.0
    - 0.2.8.0 (2022-10-27)
        * Add a number of minor optimizations and INLINE pragmas:
            - The previous `INLINABLE` pragmas were insufficient to fully specialize
              functions. Add a bunch more. I believe they now do the job they were
              meant to.
            - Change the way we check for very short queues in `lbalance` and
              `rbalance` to avoid redundant size comparisons in the non-short
              case.
            - Make the fields of `Play` strict. I doubt this makes any practical
              difference, since `tourView` is `INLINE`, but in fact the fields are
              always in WHNF, so we might as well make that explicitly clear.
        * Fix a bug in `fromList`. It previously used the *first* occurrence
          of a duplicated key; it now uses the *last* occurrence, as documented.
        * Cleanup: refactor `binShrinkL` and `binShrinkR` into `bin`.
        * Bump deepseq upper bound to 1.6
        * Bump tasty upper bound to 1.6

--- devel/hs-psqueues/buildlink3.mk
+++ devel/hs-psqueues/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:36:52 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/29 05:01:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-psqueues
 
 .if !defined(HS_PSQUEUES_BUILDLINK3_MK)
 HS_PSQUEUES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.7
-BUILDLINK_ABI_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.7.3nb5
+BUILDLINK_API_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.8
+BUILDLINK_ABI_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.8.0
 BUILDLINK_PKGSRCDIR.hs-psqueues?=	../../devel/hs-psqueues
 
 .include "../../devel/hs-hashable/buildlink3.mk"
