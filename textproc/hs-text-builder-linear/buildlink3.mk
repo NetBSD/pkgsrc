a4af8166ffd38c549e112e91335e7a1af3f24471
pho <pho@pkgsrc.org>
2024-04-29 04:17:55 +0000

    textproc/hs-text-builder-linear: Update to 0.1.2
    0.1.2
    * Fix unsound behaviour caused by inlining of runBuffer / runBufferBS and
      CSE (common subexpression elimination).
    * Fix hexadecimal builder, looping on negative inputs.
    * Fix decimal builder for non-standard bitness of the input.
    * Add (#<|) and deprecate (|>#).
    * Add newEmptyBuffer.
    * Add prependChars and appendChars.
    * Add justifyLeft, justifyRight and center.

--- textproc/hs-text-builder-linear/buildlink3.mk
+++ textproc/hs-text-builder-linear/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:37:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/29 04:17:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-builder-linear
 
 .if !defined(HS_TEXT_BUILDER_LINEAR_BUILDLINK3_MK)
 HS_TEXT_BUILDER_LINEAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.1.1nb1
+BUILDLINK_API_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-text-builder-linear?=	../../textproc/hs-text-builder-linear
 
 .include "../../math/hs-quote-quot/buildlink3.mk"
