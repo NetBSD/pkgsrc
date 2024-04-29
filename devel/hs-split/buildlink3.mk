41ad10bdad2f39ad09ea43af747ba005b2ffeda4
pho <pho@pkgsrc.org>
2024-04-29 02:59:18 +0000

    devel/hs-split: Update to 0.2.5
    * 0.2.5 (9 January 2024)
    
      - Test with GHC 9.8.
      - New function `mapSplitter` witnessing the fact that `Splitter` is
        a contravariant functor
        ([#22](https://github.com/byorgey/split/pull/22), thanks to Ellis
        Kesterton)

--- devel/hs-split/buildlink3.mk
+++ devel/hs-split/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 06:36:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/04/29 02:59:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.4
-BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.4nb1
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.5
+BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.5
 BUILDLINK_PKGSRCDIR.hs-split?=		../../devel/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
