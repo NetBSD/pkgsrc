93a9f2f62caf92f6624b161baeef0ae7c220f1a8
pho <pho@pkgsrc.org>
2024-04-29 02:07:27 +0000

    devel/hs-ansi-terminal: Update to 1.1.1
    Version 1.1.1
    * Use of C function setenv() in one thread can cause other threads calling
      C functions that query environment variables to crash. On Unix-like
      operating systems, where such functions are used, the implementations of
      hSupportsANSI and hSupportsANSIColor now avoid unnecessary calls of
      System.Environment.lookupEnv.
    * Improvements to Haddock documentation.
    
    Version 1.1
    * Add support for console layer Underlining and underlining styles
      CurlyUnderline, DottedUnderline and DashedUnderline.
    * Add type synonyms Parameter, SubParam, and ParamWithSubs to represent SGR
      parameter values with and without following parameter substrings
      comprised of one or more parameter elements (including empty elements).
    * Add csi' and sgrToCode', corresponding to csi and sgrToCode but capable
      of handling a parameter value followed by a parameter substring.
    
    Version 1.0.2
    * On Windows, fix linker error about a duplicate symbol castUINTPtrToPtr.
    
    Version 1.0.1
    * On Windows, the processing of 'ANSI' control characters in output is
      enabled by default in Windows Terminal but is not enabled by default in
      ConHost terminals. Additions have been made to allow support of users of
      ConHost terminals.
    * Add hNowSupportsANSI. On Unix, the function is equivalent to
      hSupportsANSI. On Windows, in Windows Terminal and ConHost terminals, the
      action can try to enable the processing of 'ANSI' control characters in
      output.
    * In Windows Terminal and ConHost terminals, hSupportsANSI will yield False
      if the processing of 'ANSI' control characters in output is not enabled.
    * Deprecated hSupportsANSIWithoutEmulation is now consistent with
      hNowSupportsANSI.
    * Improvements to Haddock documentation.

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2023/11/02 06:36:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2024/04/29 02:07:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.0
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.0nb1
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
 
 .include "../../devel/hs-ansi-terminal-types/buildlink3.mk"
