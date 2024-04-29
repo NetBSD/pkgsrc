12421ffaa3c01bb56f237d3ba6c0856896b59cd5
pho <pho@pkgsrc.org>
2024-04-29 03:28:32 +0000

    devel/hs-hashable: Update to 1.4.4.0
    Version 1.4.4.0
    
        Depend on os-string-2 for GHC-9.2+
        Support filepath-1.5

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.24 2023/11/02 06:36:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2024/04/29 03:28:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.3
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.3.0nb1
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.4
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.4.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
+
+.include "../../devel/hs-os-string/buildlink3.mk"
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hashable
