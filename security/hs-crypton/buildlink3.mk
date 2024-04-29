b3f82df042767d7a2842349953309f6a467922ac
pho <pho@pkgsrc.org>
2024-04-29 09:35:01 +0000

    security/hs-crypton: Update to 1.0.0
    1.0.0
        Versions follow the standard version policy.
        Removing pthread stuff. #32
    
    0.34
        Hashing getRandomBytes before using as Seed for ChaChaDRG #24
        Add support for XChaCha and XChaChaPoly1305 #18
        Strict byteArray of IV c #16

--- security/hs-crypton/buildlink3.mk
+++ security/hs-crypton/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:37:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/29 09:35:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypton
 
 .if !defined(HS_CRYPTON_BUILDLINK3_MK)
 HS_CRYPTON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypton+=	hs-crypton>=0.33
-BUILDLINK_ABI_DEPENDS.hs-crypton+=	hs-crypton>=0.33nb1
+BUILDLINK_API_DEPENDS.hs-crypton+=	hs-crypton>=1.0.0
+BUILDLINK_ABI_DEPENDS.hs-crypton+=	hs-crypton>=1.0.0
 BUILDLINK_PKGSRCDIR.hs-crypton?=	../../security/hs-crypton
 
 .include "../../devel/hs-basement/buildlink3.mk"
