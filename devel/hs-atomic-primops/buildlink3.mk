69a0b83b430a3cfa4954ddd80d541990bc1299c4
pho <pho@pkgsrc.org>
2024-04-29 08:56:01 +0000

    devel/hs-atomic-primops: Update to 0.8.7
    0.8.7 [2024.04.20]
        Fix typos in the foreign imports introduced in atomic-primops-0.8.5 and
        atomic-primops-0.8.6, which would lead to linker errors when building
        executables with GHC 9.10.
    
    0.8.6 [2024.04.16]
        Use prim, not ccall, for the foreign imports used when building the
        library with GHC 9.10 or later. This fixes a GHC 9.10-specific build
        issue.
    
    0.8.5 [2024.02.17]
        Allow building with GHC 9.10.

--- devel/hs-atomic-primops/buildlink3.mk
+++ devel/hs-atomic-primops/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:36:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/29 08:56:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-atomic-primops
 
 .if !defined(HS_ATOMIC_PRIMOPS_BUILDLINK3_MK)
 HS_ATOMIC_PRIMOPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.4
-BUILDLINK_ABI_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.4nb1
+BUILDLINK_API_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.7
+BUILDLINK_ABI_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.7
 BUILDLINK_PKGSRCDIR.hs-atomic-primops?=		../../devel/hs-atomic-primops
 
 .include "../../devel/hs-primitive/buildlink3.mk"
