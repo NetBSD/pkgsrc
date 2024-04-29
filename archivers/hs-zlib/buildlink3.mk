2fe43a29cda213ff0c4acd3d81c52222881f30db
pho <pho@pkgsrc.org>
2024-04-29 03:48:16 +0000

    archivers/hs-zlib: Update to 0.7.1.0
    0.7.1.0 Bodigrim andrew.lelechenko@gmail.com April 2024
    
        Split zlib C sources into zlib-clib package (thanks @hasufell).
        Use zlib-clib on Windows, unless pkg-config is available.
    
    0.7.0.0 Bodigrim andrew.lelechenko@gmail.com February 2024
    
        Bump bundled zlib to 1.3.1.
        Hide deprecated constructors of CompressionLevel, Method, WindowBits, MemoryLevel, CompressionStrategy and Format.
        Make WindowBits, MemoryLevel and CompressionLevel newtypes over Int.
        Add smart constructors rleStrategy and fixedStrategy.
        Add assorted Eq, Ord, Typeable and Generic instances.
        Make flag pkg-config automatic and on by default.
        Make flag bundled-c-zlib to take priority over pkg-config.
        Do not force bundled-c-zlib on Windows, but force it for WASM.
        Strip install-includes, do not install any headers.
        Export DecompressError from non-internal modules.
        Fix compression/decompression of ByteString chunks > 4G.
        Flip flag non-blocking-ffi to be True be default.

--- archivers/hs-zlib/buildlink3.mk
+++ archivers/hs-zlib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:36:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/04/29 03:48:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-zlib
 
 .if !defined(HS_ZLIB_BUILDLINK3_MK)
 HS_ZLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.6.3
-BUILDLINK_ABI_DEPENDS.hs-zlib+=	hs-zlib>=0.6.3.0nb3
+BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-zlib+=	hs-zlib>=0.7.1.0
 BUILDLINK_PKGSRCDIR.hs-zlib?=	../../archivers/hs-zlib
 
 .include "../../devel/zlib/buildlink3.mk"
