237a3e38b9662971252915420b8881f2c6056ce6
pho <pho@pkgsrc.org>
2024-04-29 03:01:09 +0000

    devel/hs-safe: Update to 0.3.21
    0.3.21, released 2024-01-18
        #34, mark headErr/tailErr as Partial
    0.3.20, released 2024-01-14
        #34, add headErr, tailErr
        #33, avoid using head/tail to avoid x-partial

--- devel/hs-safe/buildlink3.mk
+++ devel/hs-safe/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/11/02 06:36:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/04/29 03:01:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-safe
 
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.19
-BUILDLINK_ABI_DEPENDS.hs-safe+=	hs-safe>=0.3.19nb7
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.21
+BUILDLINK_ABI_DEPENDS.hs-safe+=	hs-safe>=0.3.21
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../devel/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
