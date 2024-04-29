817c6d91ad7817c02d59214e81a0b351d5bbc8d2
pho <pho@pkgsrc.org>
2024-04-29 03:44:41 +0000

    security/hs-digest: Update to 0.0.2.1
    0.0.2.1 — 10th Febraury 2024
    
        Make pkg-config an automatic flag: https://github.com/TeofilC/digest/pull/21
    
    0.0.2.0 — 13th December 2023
    
        Add CRC32C (Thanks to @4eUep): https://github.com/TeofilC/digest/pull/18
        Support latest GHC versions: https://github.com/TeofilC/digest/pull/19

--- security/hs-digest/buildlink3.mk
+++ security/hs-digest/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 06:37:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/04/29 03:44:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-digest
 
 .if !defined(HS_DIGEST_BUILDLINK3_MK)
 HS_DIGEST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.1
-BUILDLINK_ABI_DEPENDS.hs-digest+=	hs-digest>=0.0.1.7nb1
+BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.2
+BUILDLINK_ABI_DEPENDS.hs-digest+=	hs-digest>=0.0.2.1
 BUILDLINK_PKGSRCDIR.hs-digest?=		../../security/hs-digest
 
 .include "../../devel/zlib/buildlink3.mk"
