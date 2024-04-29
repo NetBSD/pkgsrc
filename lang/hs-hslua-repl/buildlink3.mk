817ef9b5ef05b9782540b118d60717d4332c1848
pho <pho@pkgsrc.org>
2024-04-29 12:18:12 +0000

    lang/hs-hslua-repl: Update to 0.1.2
    hslua-repl-0.1.2 - Released 2024-01-18.
        Relaxed upper bound for text and bytestring, allowing text-2.1 and bytestring-0.12.

--- lang/hs-hslua-repl/buildlink3.mk
+++ lang/hs-hslua-repl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:37:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/29 12:18:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-repl
 
 .if !defined(HS_HSLUA_REPL_BUILDLINK3_MK)
 HS_HSLUA_REPL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-repl+=	hs-hslua-repl>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-repl+=	hs-hslua-repl>=0.1.1nb1
+BUILDLINK_API_DEPENDS.hs-hslua-repl+=	hs-hslua-repl>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-hslua-repl+=	hs-hslua-repl>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-hslua-repl?=	../../lang/hs-hslua-repl
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
