b574353ee629e882a1c18eb9a1878506e0a4bf8a
pho <pho@pkgsrc.org>
2024-04-29 02:33:36 +0000

    databases/hs-direct-sqlite: Update to 2.3.29
    Upstream provides no release notes.

--- databases/hs-direct-sqlite/buildlink3.mk
+++ databases/hs-direct-sqlite/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/11/08 13:18:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/04/29 02:33:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-direct-sqlite
 
 .if !defined(HS_DIRECT_SQLITE_BUILDLINK3_MK)
 HS_DIRECT_SQLITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.28
-BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.28nb2
+BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.29
+BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.29
 BUILDLINK_PKGSRCDIR.hs-direct-sqlite?=		../../databases/hs-direct-sqlite
 
 .include "../../databases/sqlite3/buildlink3.mk"
