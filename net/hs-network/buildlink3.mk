d59db9468cf05822f0d162e34567bb51dfcb11a3
pho <pho@pkgsrc.org>
2024-04-29 02:56:00 +0000

    net/hs-network: Update to 3.2.0.0
    Version 3.2.0.0
    
        Breaking change: Using Strict and StrictData.
        Breaking change: Allow a control message to contain multiple file descriptors #567
        Breaking change: CmsgIdFd -> CmsgIdFds #575
        SocketTypes: use UnsupportedSocketType instead of -1. #560
        Expose NullSockAddr, add send-with-fds functions. #562
        Allow bytestring-0.12 and replace deprecated memcpy by copyBytes. #564
        Close file descriptor in accept when exception happens #569
        Bump CI to GHC 9.8 and latest versions of actions #573
    
    Version 3.1.5.0
    
        #541
            Export Network.Socket.Internal.NullSockAddr
            Add Network.Socket.ByteString.sendManyWithFds
            Add Network.Socket.ByteString.Lazy.sendWithFds

--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.22 2023/11/02 06:37:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2024/04/29 02:56:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-network
 
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.1.4
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.1.4.0nb1
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.2.0
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.2.0.0
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 .endif	# HS_NETWORK_BUILDLINK3_MK
 
