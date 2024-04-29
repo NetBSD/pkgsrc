4b31e0c4d3eb67e06fc41bc266437081394ce99c
pho <pho@pkgsrc.org>
2024-04-29 02:47:41 +0000

    textproc/hs-commonmark: Update to 0.2.6
    0.2.6
    * Make list tightness match the reference implementation closer (#150,
      Michael Howell). This solves the problem where blank lines in the middle
      of a list are attributed to the list itself instead of the item, making
      its parent list become spuriously loose.
    * Fix bug with entities inside link destinations (#149). The bug affects
      cases like this: [link](\&#33;); the backslash escape was being ignored
      here.
    * Commonmark.Entity: export pEntity [API change].
    
    0.2.5.1
    * Replace source with search in list of block tags. This is a spec 0.31
      change that was forgotten in the last release.
    
    0.2.5
    * Fix HTML comment parser to conform to 0.31.2 spec.
    * Update spec.txt tests to commonmark-spec 0.31.2.
    * Match HTML declaration blocks with lowercase letters (Michael Howell).
    * Specifically track the position where enders end (Michael Howell).
    
    0.2.4.1
    * Commonmark.Html: Add aria-hidden, d, and viewBox to allowed attributes
      list.
    * Correctly merge list blanks with non-list blanks (#133, Michael Howell).
    * Do not look for backslashed hard breaks in link titles (#130, Michael
      Howell).
    * Work around ghc bug with -K RTS options, to set the stack space properly
      for tests (#129). See https://gitlab.haskell.org/ghc/ghc/-/issues/10445.
    * Revert block state completely if lazy line (#126). This fixes an issue
      with lazily-wrapped footnotes.
    * Avoid adding trailing newline to list block if it's already there
      (Michael Howell). This fixes tight/loose classification in a few cases.
    * Fix incorrectly parsing links with nested [] (Michael Howell).

--- textproc/hs-commonmark/buildlink3.mk
+++ textproc/hs-commonmark/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:37:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/04/29 02:47:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark
 
 .if !defined(HS_COMMONMARK_BUILDLINK3_MK)
 HS_COMMONMARK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.4
-BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.4nb1
+BUILDLINK_API_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.6
+BUILDLINK_ABI_DEPENDS.hs-commonmark+=	hs-commonmark>=0.2.6
 BUILDLINK_PKGSRCDIR.hs-commonmark?=	../../textproc/hs-commonmark
 
 .include "../../textproc/hs-unicode-data/buildlink3.mk"
