0a0afb675b2dee1dae0cae2185bf31bc0c4b31ff
pho <pho@pkgsrc.org>
2024-04-29 12:11:10 +0000

    devel/hs-async: Update to 2.2.5
    Changes in 2.2.5
    
        #117: Document that empty for Concurrently waits forever
        #120: Add ConcurrentlyE.
        #123: Fix failing concurrentlyE tests in older GHCs.
        #124: Allow hashable 1.4
        #126: Semigroup and Monoid instances for ConcurrentlyE
        #120: Add ConcurrentlyE
        #138: expose internals as Control.Concurrent.Async.Internal
        #131: Fix typos in docs
        #132: waitAny(Catch): clarify non-empty input list requirement
        #142: Add cancelMany
        #135, #145, #150: Support for GHC 9.4, 9.6, 9.8
        Document that empty for Concurrently waits forever.

--- devel/hs-async/buildlink3.mk
+++ devel/hs-async/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/11/02 06:36:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/04/29 12:11:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-async
 
 .if !defined(HS_ASYNC_BUILDLINK3_MK)
 HS_ASYNC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-async+=	hs-async>=2.2.4
-BUILDLINK_ABI_DEPENDS.hs-async+=	hs-async>=2.2.4nb5
+BUILDLINK_API_DEPENDS.hs-async+=	hs-async>=2.2.5
+BUILDLINK_ABI_DEPENDS.hs-async+=	hs-async>=2.2.5
 BUILDLINK_PKGSRCDIR.hs-async?=		../../devel/hs-async
 
 .include "../../devel/hs-hashable/buildlink3.mk"
