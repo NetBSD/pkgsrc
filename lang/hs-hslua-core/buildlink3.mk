fbd11c270734e5b6fd65d5fc8090398b3ba9dd99
pho <pho@pkgsrc.org>
2024-04-29 04:00:13 +0000

    lang/hs-hslua-core: Update to 2.3.2
    hslua-core-2.3.2 - Released 2024-01-18.
        Relaxed upper bound for text, and bytestring, allowing text-2.1, and bytestring-0.12.

--- lang/hs-hslua-core/buildlink3.mk
+++ lang/hs-hslua-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/29 04:00:13 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-core
 
 .if !defined(HS_HSLUA_CORE_BUILDLINK3_MK)
 HS_HSLUA_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.3.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.3.1nb1
+BUILDLINK_API_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.3.2
+BUILDLINK_ABI_DEPENDS.hs-hslua-core+=	hs-hslua-core>=2.3.2
 BUILDLINK_PKGSRCDIR.hs-hslua-core?=	../../lang/hs-hslua-core
 
 .include "../../lang/hs-lua/buildlink3.mk"
