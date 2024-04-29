26ad78bc22c93d4ba77acdbb82d5dd6d90f61330
pho <pho@pkgsrc.org>
2024-04-29 04:15:50 +0000

    textproc/hs-toml-parser: Update to 2.0.0.0
    2.0.0.0
    * Pervasive annotations on the values added to allow for detailed
      positional error reporting throughout parsing and validation.
    * Replace uses of String with Text in the Value type and throughout the API
    * Reorganized almost all of the modules to minimize imports that upstream
      packages will actually need.
    
    1.3.3.0
    * Added IsString Value instance.
    * Addded helpers for runMatcher for ignoring and failing on warning
      runMatcherIgnoreWarn and runMatcherFatalWarn
    
    1.3.2.0
    * Added Toml.Generic to make instances easily derivable via DerivingVia.
    * Added GHC.Generics support for switching between product types and TOML
      arrays.
    
    1.3.1.3
    * Bugfix: Previous fix admitted some invalid inline tables - these are now
      rejected
    
    1.3.1.2
    * Bugfix: In some cases overlapping keys in inline tables could throw an
      exception instead instead of returning the proper semantic error value.
    
    1.3.1.1
    * Ensure years are rendered zero-padded
    
    1.3.1.0
    * Added Toml.Semantics.Ordered for preserving input TOML orderings
    * Added support for pretty-printing multi-line strings

--- textproc/hs-toml-parser/buildlink3.mk
+++ textproc/hs-toml-parser/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:37:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/04/29 04:15:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-toml-parser
 
 .if !defined(HS_TOML_PARSER_BUILDLINK3_MK)
 HS_TOML_PARSER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-toml-parser+=	hs-toml-parser>=1.3.0
-BUILDLINK_ABI_DEPENDS.hs-toml-parser+=	hs-toml-parser>=1.3.0.0nb1
+BUILDLINK_API_DEPENDS.hs-toml-parser+=	hs-toml-parser>=2.0.0
+BUILDLINK_ABI_DEPENDS.hs-toml-parser+=	hs-toml-parser>=2.0.0.0
 BUILDLINK_PKGSRCDIR.hs-toml-parser?=	../../textproc/hs-toml-parser
 
 .include "../../textproc/hs-prettyprinter/buildlink3.mk"
