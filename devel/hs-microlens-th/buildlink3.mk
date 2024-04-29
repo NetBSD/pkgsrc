773322de054b723016134373786a143eace4b7b5
pho <pho@pkgsrc.org>
2024-04-29 12:15:53 +0000

    devel/hs-microlens-th: Update to 0.4.3.15
    0.4.3.15
        #180 #181 Support GHC 9.10.

--- devel/hs-microlens-th/buildlink3.mk
+++ devel/hs-microlens-th/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:36:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/29 12:15:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-microlens-th
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-microlens-th
 HS_MICROLENS_TH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3
-BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.14nb1
+BUILDLINK_ABI_DEPENDS.hs-microlens-th+=	hs-microlens-th>=0.4.3.15
 BUILDLINK_PKGSRCDIR.hs-microlens-th?=	../../devel/hs-microlens-th
 
 .include "../../devel/hs-microlens/buildlink3.mk"
