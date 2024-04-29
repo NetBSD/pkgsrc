b9f3df99c30a1a74890309fb5a5dc7324206467a
nikita <nikita@pkgsrc.org>
2024-04-29 13:27:52 +0000

    devel/zls: import zls version 0.12.0
    The Zig Language Server (ZLS) is a tool that implements
    Microsoft's Language Server Protocol for Zig in Zig.
    In simpler terms: it'll provide you with completions,
    go-to definition, etc. when you write Zig code!

---
+++ devel/zls/zig-depends.mk
@@ -0,0 +1,4 @@
+# $NetBSD: zig-depends.mk,v 1.1 2024/04/29 13:27:52 nikita Exp $
+
+ZIG_PACKAGE_DEPENDS+=	https://github.com/ziglibs/known-folders/archive/bf79988adcfce166f848e4b11e718c1966365329.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://github.com/ziglibs/diffz/archive/e10bf15962e45affb3fcd7d9a950977a69c901b3.tar.gz
