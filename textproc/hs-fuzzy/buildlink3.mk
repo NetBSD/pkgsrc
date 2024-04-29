dba5f5698c60a6d780e408ec912591d57ba31003
pho <pho@pkgsrc.org>
2024-04-29 05:07:54 +0000

    textproc/hs-fuzzy: Update to 0.1.1.0
    Upstream provides no release notes.

--- textproc/hs-fuzzy/buildlink3.mk
+++ textproc/hs-fuzzy/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/11/02 06:37:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/04/29 05:07:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-fuzzy
 
 .if !defined(HS_FUZZY_BUILDLINK3_MK)
 HS_FUZZY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fuzzy+=	hs-fuzzy>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-fuzzy+=	hs-fuzzy>=0.1.0.1nb5
+BUILDLINK_API_DEPENDS.hs-fuzzy+=	hs-fuzzy>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-fuzzy+=	hs-fuzzy>=0.1.1.0
 BUILDLINK_PKGSRCDIR.hs-fuzzy?=		../../textproc/hs-fuzzy
 
 .include "../../math/hs-monoid-subclasses/buildlink3.mk"
