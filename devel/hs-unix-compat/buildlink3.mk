c46b43c09e076171dbc25a0633617c2e1286c10d
pho <pho@pkgsrc.org>
2024-04-29 02:25:48 +0000

    devel/hs-unix-compat: Update to 0.7.1
    Version 0.7.1 (2023-12-06) Santa Clause edition
    
        Add System.PosixCompat.Process module, exporting getProcessID

--- devel/hs-unix-compat/buildlink3.mk
+++ devel/hs-unix-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:37:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/04/29 02:25:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-compat
 
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7
-BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7nb1
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7.1
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../devel/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
