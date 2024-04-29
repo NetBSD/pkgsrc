2fc87500aac48bb6286c59b721f8b0851854953d
adam <adam@pkgsrc.org>
2024-04-29 10:53:03 +0000

    py-pypiserver: updated to 2.1.1
    2.1.1 (2024-04-24)
    - 31c9cf1 FIX: deprecated `setuptools.py` when building in `package.sh`
    - 2619c17 FIX: use the right env variables in `release-tag` workflow
    
    2.1.0 (2024-04-24)
    - d588913 ENH: Bump github action versions and add multiarch support
    - a558dbc ENH: Handle tar.xz archives
    - 2f0a56c FIX: support Python 3.12
    - 84bf12c MAINT: make the last supported python version explicit in `ci.yaml`
    - 946fbfe MAINT: Update setuptools requirement from <62.0.0,>=40.0 to >=40.0,<70.0.0 in /requirements
    - 50c7a78 MAINT: add tar xz test case

--- net/py-pypiserver/patches/patch-setup.py
+++ net/py-pypiserver/patches/patch-setup.py
@@ -1,15 +1,14 @@
-$NetBSD: patch-setup.py,v 1.4 2023/08/04 08:17:36 adam Exp $
+$NetBSD: patch-setup.py,v 1.5 2024/04/29 10:53:03 adam Exp $
 
 Do not require setuptools-git for building.
 
---- setup.py.orig	2023-07-30 19:33:20.000000000 +0000
+--- setup.py.orig	2024-04-24 23:59:20.000000000 +0000
 +++ setup.py
-@@ -13,7 +13,7 @@ tests_require = [
-     "webtest",
+@@ -16,7 +16,6 @@ tests_require = [
+ 
+ setup_requires = [
+     "setuptools",
+-    "setuptools-git>=0.3",
+     "wheel>=0.25.0",
  ]
- 
--setup_requires = ["setuptools", "setuptools-git >= 0.3", "wheel >= 0.25.0"]
-+setup_requires = ["setuptools", "wheel >= 0.25.0"]
- install_requires = ["pip>=7"]
- 
- 
+ install_requires = [
