7cda2144248b1539fac9f5c464af1c631256f5c7
pho <pho@pkgsrc.org>
2024-04-29 04:06:39 +0000

    www/hs-cookie: Update to 0.5.0
    0.5.0
    
      Remove surrounding double quotes from cookie values when parsing #31
    
      This is a breaking change, as it changes the behavior of parseCookies and
      parseSetCookie to no longer include the surrounding double quotes in the
      cookie value. This is the correct behavior according to the RFC.

--- www/hs-cookie/buildlink3.mk
+++ www/hs-cookie/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/11/02 06:37:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/04/29 04:06:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-cookie
 
 .if !defined(HS_COOKIE_BUILDLINK3_MK)
 HS_COOKIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cookie+=	hs-cookie>=0.4.6
-BUILDLINK_ABI_DEPENDS.hs-cookie+=	hs-cookie>=0.4.6nb3
+BUILDLINK_API_DEPENDS.hs-cookie+=	hs-cookie>=0.5.0
+BUILDLINK_ABI_DEPENDS.hs-cookie+=	hs-cookie>=0.5.0
 BUILDLINK_PKGSRCDIR.hs-cookie?=		../../www/hs-cookie
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
