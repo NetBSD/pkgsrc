45e322f14029ceb3399bf5f86f85feff17003ffd
pho <pho@pkgsrc.org>
2024-04-29 03:05:29 +0000

    devel/hs-file-embed: Update to 0.0.16.0
    0.0.16.0
        Add embedFileRelative

--- devel/hs-file-embed/buildlink3.mk
+++ devel/hs-file-embed/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/11/02 06:36:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2024/04/29 03:05:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-file-embed
 
 .if !defined(HS_FILE_EMBED_BUILDLINK3_MK)
 HS_FILE_EMBED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.15
-BUILDLINK_ABI_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.15.0nb5
+BUILDLINK_API_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.16
+BUILDLINK_ABI_DEPENDS.hs-file-embed+=	hs-file-embed>=0.0.16.0
 BUILDLINK_PKGSRCDIR.hs-file-embed?=	../../devel/hs-file-embed
 .endif	# HS_FILE_EMBED_BUILDLINK3_MK
 
