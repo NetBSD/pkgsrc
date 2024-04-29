6e14526ec6cc690dc198b73cf1d9544ea7bbb83b
nikita <nikita@pkgsrc.org>
2024-04-29 20:25:08 +0000

    passphrase: zig dependencies

---
+++ security/passphrase/zig-depends.mk
@@ -0,0 +1,4 @@
+# $NetBSD: zig-depends.mk,v 1.1 2024/04/29 20:25:08 nikita Exp $
+
+ZIG_PACKAGE_DEPENDS+=	https://github.com/Hejsil/zig-clap/archive/8c98e64.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://codeberg.org/dude_the_builder/ziglyph/archive/56cd50c8eb.tar.gz
