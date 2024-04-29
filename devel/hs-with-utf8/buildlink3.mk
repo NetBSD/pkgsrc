818d0ba72ad39f3fa4857ee6c0434e23e45f265f
pho <pho@pkgsrc.org>
2024-04-29 04:53:20 +0000

    devel/hs-with-utf8: Update to 1.1.0.0
    1.1.0.0
    
      Changed
    
        Allow newer versions of base and text to support GHC up to 9.8.
        Deprecate Data.Text.IO.Utf8.

--- devel/hs-with-utf8/buildlink3.mk
+++ devel/hs-with-utf8/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/11/02 06:37:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/04/29 04:53:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-with-utf8
 
 .if !defined(HS_WITH_UTF8_BUILDLINK3_MK)
 HS_WITH_UTF8_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.0.2.4nb1
+BUILDLINK_API_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.1.0
+BUILDLINK_ABI_DEPENDS.hs-with-utf8+=	hs-with-utf8>=1.1.0.0
 BUILDLINK_PKGSRCDIR.hs-with-utf8?=	../../devel/hs-with-utf8
 
 .include "../../devel/hs-safe-exceptions/buildlink3.mk"
