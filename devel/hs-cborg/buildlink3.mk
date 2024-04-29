49474e5e02858e64b2306c21bec582210c6aa237
pho <pho@pkgsrc.org>
2024-04-29 12:44:07 +0000

    devel/hs-cborg: Update to 0.2.10.0
    0.2.10.0
    
        Return TypeUInt64 and TypeNInt64 from tokenType when appropriate, fixing #324
        Don't rely on MonadFail Gen
        Support for GHC 9.8

--- devel/hs-cborg/buildlink3.mk
+++ devel/hs-cborg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:36:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/04/29 12:44:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-cborg
 
 .if !defined(HS_CBORG_BUILDLINK3_MK)
 HS_CBORG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cborg+=	hs-cborg>=0.2.9
-BUILDLINK_ABI_DEPENDS.hs-cborg+=	hs-cborg>=0.2.9.0nb1
+BUILDLINK_API_DEPENDS.hs-cborg+=	hs-cborg>=0.2.10
+BUILDLINK_ABI_DEPENDS.hs-cborg+=	hs-cborg>=0.2.10.0
 BUILDLINK_PKGSRCDIR.hs-cborg?=		../../devel/hs-cborg
 
 .include "../../devel/hs-half/buildlink3.mk"
