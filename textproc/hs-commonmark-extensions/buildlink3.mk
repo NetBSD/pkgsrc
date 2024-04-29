bf090ec1dd0c6ea49701ebfe12de6e4349ddfc16
pho <pho@pkgsrc.org>
2024-04-29 12:24:32 +0000

    textproc/hs-commonmark-extensions: Update to 0.2.5.4
    0.2.5.4
    * Fix autolink parsing regression (#151). This affects autolinks with
      doubled internal line-ending punctuation characters.
    
    0.2.5.3
    * Fix rebase_relative_paths extension so it works with URLs with non-ASCII
      characters (#148). Previously these would not be properly detected as
      absolute URIs.
    
    0.2.5.2
    * Improve autolinks extension (#147). The autolinks extension was
      interacting badly with explicit links, To fix this we had to make
      autolink parsing a bit stricter than the GFM spec does. They allow
      unbalanced ) except at the end of a URL (which is defined as: followed by
      optional final punctuation then whitespace or eof). With this change, we
      don't allow unbalanced ) at all in raw URLs. This should not be a big
      problem in practice.
    * Protect against quadratic generated table size explosion (Michael
      Howell). This commit adds a limit to the number of auto-completed cells
      around 200,000. The result is, in these original samples:
    
    0.2.5.1
    * Add test/alerts.md to extra-source-files in cabal file.
    
    0.2.5
    * Add support for alerts extension, supporting GitHub-style alerts
      (#132). https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#alerts
      New module Commonmark.Extensions.Alerts [API change].
    * Do not accept footnote labels with line breaks (Michael Howell).
    * Parse [^ ] and [^] as links (Michael Howell). This is consistent with
      most other CommonMark parsers, even when they have support for footnotes
      turned on.

--- textproc/hs-commonmark-extensions/buildlink3.mk
+++ textproc/hs-commonmark-extensions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/11/02 06:37:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/04/29 12:24:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark-extensions
 
 .if !defined(HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK)
 HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.4
-BUILDLINK_ABI_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.4nb1
+BUILDLINK_API_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.5
+BUILDLINK_ABI_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.5.4
 BUILDLINK_PKGSRCDIR.hs-commonmark-extensions?=		../../textproc/hs-commonmark-extensions
 
 .include "../../textproc/hs-commonmark/buildlink3.mk"
