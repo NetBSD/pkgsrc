26ec51a47c47771d0e9b0cb2d0fc718e55ff525b
pho <pho@pkgsrc.org>
2024-04-29 05:05:09 +0000

    sysutils/hs-hslogger: Update to 1.3.1.1
    1.3.1.1 (patch)
    
        Drop support for GHC 7
        Tested with GHC 8.0 - 9.10

--- sysutils/hs-hslogger/buildlink3.mk
+++ sysutils/hs-hslogger/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/11/02 06:37:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/04/29 05:05:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslogger
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-hslogger
 HS_HSLOGGER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-hslogger+=	hs-hslogger>=1.3.1
-BUILDLINK_ABI_DEPENDS.hs-hslogger+=	hs-hslogger>=1.3.1.0nb4
+BUILDLINK_ABI_DEPENDS.hs-hslogger+=	hs-hslogger>=1.3.1.1
 BUILDLINK_PKGSRCDIR.hs-hslogger?=	../../sysutils/hs-hslogger
 
 .include "../../devel/hs-old-locale/buildlink3.mk"
