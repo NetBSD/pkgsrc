9e2de27dd4c4d6736ac7118df1fd320331815af7
pho <pho@pkgsrc.org>
2024-04-30 13:38:14 +0000

    devel/hs-protolude: Update to 0.3.4
    Upstream provides no release notes.

--- devel/hs-protolude/buildlink3.mk
+++ devel/hs-protolude/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:36:52 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/04/30 13:38:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-protolude
 
 .if !defined(HS_PROTOLUDE_BUILDLINK3_MK)
 HS_PROTOLUDE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-protolude+=	hs-protolude>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-protolude+=	hs-protolude>=0.3.3nb1
+BUILDLINK_API_DEPENDS.hs-protolude+=	hs-protolude>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-protolude+=	hs-protolude>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-protolude?=	../../devel/hs-protolude
 
 .include "../../devel/hs-async/buildlink3.mk"
