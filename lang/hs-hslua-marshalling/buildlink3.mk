4d98532f3bbe56c06c9d75f85d1b3c28373d2b69
pho <pho@pkgsrc.org>
2024-04-30 02:56:41 +0000

    lang/hs-hslua-marshalling: Update to 2.3.1
    hslua-marshalling-2.3.1 - Released 2024-01-18.
    
        Relaxed upper bound for text, containers, and bytestring, allowing
        text-2.1, containers-0.7, and bytestring-0.12.

--- lang/hs-hslua-marshalling/buildlink3.mk
+++ lang/hs-hslua-marshalling/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/30 02:56:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-marshalling
 
 .if !defined(HS_HSLUA_MARSHALLING_BUILDLINK3_MK)
 HS_HSLUA_MARSHALLING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.3.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.3.0nb1
+BUILDLINK_API_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.3.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-marshalling+=	hs-hslua-marshalling>=2.3.1
 BUILDLINK_PKGSRCDIR.hs-hslua-marshalling?=	../../lang/hs-hslua-marshalling
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
