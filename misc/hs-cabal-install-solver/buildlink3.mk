7bc32e6666274c9e17744614426c685ff8bf9f69
pho <pho@pkgsrc.org>
2024-04-30 12:55:44 +0000

    misc/hs-cabal-install-solver: Update to 3.10.2.1
    The release note is too long to paste here:
    https://github.com/haskell/cabal/blob/master/release-notes/cabal-install-3.10.2.0.md

--- misc/hs-cabal-install-solver/buildlink3.mk
+++ misc/hs-cabal-install-solver/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/11/02 06:37:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/04/30 12:55:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-cabal-install-solver
 
 .if !defined(HS_CABAL_INSTALL_SOLVER_BUILDLINK3_MK)
 HS_CABAL_INSTALL_SOLVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.10.1
-BUILDLINK_ABI_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.10.1.0nb1
+BUILDLINK_API_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.10.2
+BUILDLINK_ABI_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.10.2.1
 BUILDLINK_PKGSRCDIR.hs-cabal-install-solver?=	../../misc/hs-cabal-install-solver
 
 .include "../../textproc/hs-edit-distance/buildlink3.mk"
