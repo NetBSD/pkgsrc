af399f570f438d393340c86e542be5cf157b2590
pho <pho@pkgsrc.org>
2024-04-30 13:30:03 +0000

    lang/hs-hslua-classes: Update to 2.3.1
    hslua-classes-2.3.1 - Released 2024-01-18.
    
        Relaxed upper bound for text, containers, and bytestring, allowing
        text-2.1, containers-0.7, and bytestring-0.12.

--- lang/hs-hslua-classes/buildlink3.mk
+++ lang/hs-hslua-classes/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/04/30 13:30:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-classes
 
 .if !defined(HS_HSLUA_CLASSES_BUILDLINK3_MK)
 HS_HSLUA_CLASSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.3.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.3.0nb1
+BUILDLINK_API_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.3.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-classes+=	hs-hslua-classes>=2.3.1
 BUILDLINK_PKGSRCDIR.hs-hslua-classes?=		../../lang/hs-hslua-classes
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
