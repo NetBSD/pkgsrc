7f578c652a61b7a3282a77b34dd4fd4c6ebd1031
cheusov <cheusov@pkgsrc.org>
2024-05-01 22:32:15 +0000

    Add option "nls" enabled by default

---
+++ devel/libidn/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD: options.mk,v 1.1 2024/05/01 22:32:15 cheusov Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.libidn
+
+PKG_SUPPORTED_OPTIONS=		nls
+PKG_SUGGESTED_OPTIONS=		nls
+
+PLIST_VARS+=	nls
+
+.include "../../mk/bsd.options.mk"
+
+# nls
+.if !empty(PKG_OPTIONS:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+PLIST.nls=	yes
+.else
+CONFIGURE_ARGS+= --disable-nls
+.endif
