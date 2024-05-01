d4cb8971ad5d77e9e887200afc900e98422752b0
pho <pho@pkgsrc.org>
2024-05-01 14:26:59 +0000

    www/hs-websockets: Update to 0.13.0.0
    - 0.13.0.0 (2023-12-30)
        * **BREAKING**: Remove `serverRequirePong` option in favor of the new
          implementation.
        * **BREAKING**: Client: Rejecting request raises
          `RequestRejected RequestHead ResponseHead`
        * Timeout initial socket connection after 30s.
        * If the socket is closed unexpectedly, raise `ConnectionClosed`.
        * Added a way to manually send a Pong message.
        * `runServer` now cleans up threads correctly.
        * Remove redundant bytestring-builder dependency.
        * Introduce `Network.WebSockets.Connection.PingPong` to
          handle ping pong for any Connection, be it Client or Server.
        * Bump `text `dependency upper bound to 2.2
        * Bump `random `dependency lower bound to 1.0.1

--- www/hs-websockets/buildlink3.mk
+++ www/hs-websockets/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/11/02 06:37:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/05/01 14:26:59 pho Exp $
 
 BUILDLINK_TREE+=	hs-websockets
 
 .if !defined(HS_WEBSOCKETS_BUILDLINK3_MK)
 HS_WEBSOCKETS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-websockets+=	hs-websockets>=0.12.7
-BUILDLINK_ABI_DEPENDS.hs-websockets+=	hs-websockets>=0.12.7.3nb5
+BUILDLINK_API_DEPENDS.hs-websockets+=	hs-websockets>=0.13.0
+BUILDLINK_ABI_DEPENDS.hs-websockets+=	hs-websockets>=0.13.0.0
 BUILDLINK_PKGSRCDIR.hs-websockets?=	../../www/hs-websockets
 
 .include "../../devel/hs-async/buildlink3.mk"
