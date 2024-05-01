1d3886bb7ce36dbaae3c4b378bffe62c213d0b9a
pho <pho@pkgsrc.org>
2024-05-01 07:26:30 +0000

    devel/hs-mono-traversable: Update to 1.0.17.0
    1.0.17.0
    
        Added inits, tails, initTails to class IsSequence with tests and benchmarks for initTails.
        Improved ghc benchmark flags.
        Removed extraneous constraint IsSequence from initMay.
    
    1.0.16.0
    
        Added MonoPointed instance for bytestring Builder #219

--- devel/hs-mono-traversable/buildlink3.mk
+++ devel/hs-mono-traversable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/11/02 06:36:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2024/05/01 07:26:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-mono-traversable
 
 .if !defined(HS_MONO_TRAVERSABLE_BUILDLINK3_MK)
 HS_MONO_TRAVERSABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.15
-BUILDLINK_ABI_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.15.3nb7
+BUILDLINK_API_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.17.0
+BUILDLINK_ABI_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.17.0
 BUILDLINK_PKGSRCDIR.hs-mono-traversable?=	../../devel/hs-mono-traversable
 
 .include "../../devel/hs-hashable/buildlink3.mk"
