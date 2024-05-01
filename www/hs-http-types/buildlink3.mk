c9e9a4300b7843df0bad02d227d801fc40c56d60
pho <pho@pkgsrc.org>
2024-05-01 10:45:52 +0000

    www/hs-http-types: Update to 0.12.4
    ## 0.12.4 [2023-11-29]
    
    * Add `Data` and `Generic` instances to `ByteRange`, `StdMethod`, `Status` and `HttpVersion`.
    * Rework of all the documentation, with the addition of `@since` notations.

--- www/hs-http-types/buildlink3.mk
+++ www/hs-http-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/01 10:45:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-http-types
 
 .if !defined(HS_HTTP_TYPES_BUILDLINK3_MK)
 HS_HTTP_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-types+=	hs-http-types>=0.12.3
-BUILDLINK_ABI_DEPENDS.hs-http-types+=	hs-http-types>=0.12.3nb7
+BUILDLINK_API_DEPENDS.hs-http-types+=	hs-http-types>=0.12.4
+BUILDLINK_ABI_DEPENDS.hs-http-types+=	hs-http-types>=0.12.4
 BUILDLINK_PKGSRCDIR.hs-http-types?=	../../www/hs-http-types
 
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
