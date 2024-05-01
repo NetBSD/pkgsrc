6b39ce0d1a60e9d0b351443a2c23a4333987a74f
pho <pho@pkgsrc.org>
2024-05-01 08:16:52 +0000

    textproc/hs-megaparsec: Update to 9.6.1
    Megaparsec 9.6.1
    
        Exposed Text.Megaparsec.State, so that the new functions (initialState
        and initialPosState) can be actually imported from it. PR 549.

--- textproc/hs-megaparsec/buildlink3.mk
+++ textproc/hs-megaparsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:37:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/05/01 08:16:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-megaparsec
 
 .if !defined(HS_MEGAPARSEC_BUILDLINK3_MK)
 HS_MEGAPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.6.0
-BUILDLINK_ABI_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.6.0nb1
+BUILDLINK_API_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.6.1
+BUILDLINK_ABI_DEPENDS.hs-megaparsec+=	hs-megaparsec>=9.6.1
 BUILDLINK_PKGSRCDIR.hs-megaparsec?=	../../textproc/hs-megaparsec
 
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
