fd59ae1cc70e21fb69fba048425bd702305271ca
pho <pho@pkgsrc.org>
2024-05-01 14:16:03 +0000

    converters/hs-base64: Update to 1.0
    1.0.0.0
    
    New epoch time! lots of changes to the API. Please see MIGRATION.md for more info:
    
        Add support for GHC 9.x
        Bumped bounds to more modern dependency set
        Introduced Base64 newtype, located in Data.Base64.Types, as well as ADT for associated alphabets.
        Signature for encodeBase64 has changed so that it produces a Base64 wrapped value.
        decodeBase64 and its URLsafe variants now consume a value of type Base64 and produces a decoded value of the underlying type.
        The old decodeBase64 and its URLsafe variants is now renamed to decodeBase64Untyped*.
        Fix for 32-bit systems in which memory barriers were not respected in the encoe loop (see: #56)
        Optimize encode and decode loops to achieve significant speedup across the board.
    
    Thanks to everyone who contributed (especially @sofia-m-a!).

--- converters/hs-base64/buildlink3.mk
+++ converters/hs-base64/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/11/02 06:36:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/05/01 14:16:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-base64
 
 .if !defined(HS_BASE64_BUILDLINK3_MK)
 HS_BASE64_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base64+=	hs-base64>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-base64+=	hs-base64>=0.4.2.4nb3
+BUILDLINK_API_DEPENDS.hs-base64+=	hs-base64>=1.0
+BUILDLINK_ABI_DEPENDS.hs-base64+=	hs-base64>=1.0
 BUILDLINK_PKGSRCDIR.hs-base64?=		../../converters/hs-base64
 
 .include "../../textproc/hs-text-short/buildlink3.mk"
