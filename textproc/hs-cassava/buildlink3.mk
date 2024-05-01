61f11c37443240599e7495144c5ccde102b2b114
pho <pho@pkgsrc.org>
2024-05-01 14:22:35 +0000

    textproc/hs-cassava: Update to 0.5.3.1
    Version 0.5.3.1
    
        Remove support for GHC 7.
        Remove cabal flag bytestring--LT-0_10_4 and support for bytestring < 0.10.4.
        Tested with GHC 8.0 - 9.10 alpha3

--- textproc/hs-cassava/buildlink3.mk
+++ textproc/hs-cassava/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/11/02 06:37:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2024/05/01 14:22:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-cassava
 
 .if !defined(HS_CASSAVA_BUILDLINK3_MK)
 HS_CASSAVA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cassava+=	hs-cassava>=0.5.2
-BUILDLINK_ABI_DEPENDS.hs-cassava+=	hs-cassava>=0.5.3.0nb3
+BUILDLINK_API_DEPENDS.hs-cassava+=	hs-cassava>=0.5.3
+BUILDLINK_ABI_DEPENDS.hs-cassava+=	hs-cassava>=0.5.3.1
 BUILDLINK_PKGSRCDIR.hs-cassava?=	../../textproc/hs-cassava
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
