d679988d783bba7fdc473c2da9103df4f4fe5c28
pho <pho@pkgsrc.org>
2024-05-01 17:01:35 +0000

    lang/hs-hslua-objectorientation: Update to 2.3.1
    hslua-objectorientation-2.3.1 - Released 2024-01-18.
    
        Relaxed upper bound for text, containers, and bytestring, allowing
        text-2.1, containers-0.7, and bytestring-0.12.

--- lang/hs-hslua-objectorientation/buildlink3.mk
+++ lang/hs-hslua-objectorientation/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/01 17:01:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-objectorientation
 
 .if !defined(HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK)
 HS_HSLUA_OBJECTORIENTATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.3.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.3.0nb1
+BUILDLINK_API_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.3.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-objectorientation+=	hs-hslua-objectorientation>=2.3.1
 BUILDLINK_PKGSRCDIR.hs-hslua-objectorientation?=	../../lang/hs-hslua-objectorientation
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
