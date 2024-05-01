27b40f46495ca4935cdc5d9e35ff3436554aaf7b
cheusov <cheusov@pkgsrc.org>
2024-05-01 20:02:14 +0000

    Add option "nls" enabled by default

--- lang/gawk/options.mk
+++ lang/gawk/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.3 2013/01/31 22:01:43 adam Exp $
+# $NetBSD: options.mk,v 1.4 2024/05/01 20:02:14 cheusov Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gawk
-PKG_SUPPORTED_OPTIONS=	portals readline
-PKG_SUGGESTED_OPTIONS=	readline
+PKG_SUPPORTED_OPTIONS=	portals readline nls
+PKG_SUGGESTED_OPTIONS=	readline nls
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		nls
 
 .if !empty(PKG_OPTIONS:Mportals)
 CONFIGURE_ARGS+=	--enable-portals
@@ -14,4 +16,12 @@ CONFIGURE_ARGS+=	--enable-portals
 .include "../../devel/readline/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-readline
+.endif
+
+# nls
+.if !empty(PKG_OPTIONS:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+PLIST.nls=	yes
+.else
+CONFIGURE_ARGS+= --disable-nls
 .endif
