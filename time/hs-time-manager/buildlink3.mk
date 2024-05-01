a5d00a01f0c6296eff2d2c531a6731568fbcef4c
pho <pho@pkgsrc.org>
2024-05-01 14:29:33 +0000

    time/hs-time-manager: Update to 0.1.0
    0.1.0
    
        #986
    
        * Change behavior of cancel to immediately remove the Handle from
          the reaper's workload, rather than waiting for timeout.
    
        * Using auto-update v0.2.0.

--- time/hs-time-manager/buildlink3.mk
+++ time/hs-time-manager/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/01 14:29:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-time-manager
 
 .if !defined(HS_TIME_MANAGER_BUILDLINK3_MK)
 HS_TIME_MANAGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.1
-BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.0.1nb1
+BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.1.0
 BUILDLINK_PKGSRCDIR.hs-time-manager?=	../../time/hs-time-manager
 
 .include "../../devel/hs-auto-update/buildlink3.mk"
