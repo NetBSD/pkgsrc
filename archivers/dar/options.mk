a342fc0e203a965d0de48151f4b2697291496328
cheusov <cheusov@pkgsrc.org>
2024-05-01 19:00:19 +0000

    Add support for option "nls"

--- archivers/dar/options.mk
+++ archivers/dar/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2022/10/05 09:36:52 nros Exp $
+# $NetBSD: options.mk,v 1.2 2024/05/01 19:00:19 cheusov Exp $
 
 ## Our threading check here is the same as the one used in devel/perl5.
 #
 CHECK_BUILTIN.pthread:=yes
 .include "../../mk/pthread.builtin.mk"
 CHECK_BUILTIN.pthread:=no
+
+PLIST_VARS+=		nls
 
 ## XXX I'm following the USE_FEATURES semantics used in security/openssl.
 #
@@ -18,7 +20,7 @@ PKG_OPTIONS_VAR=		PKG_OPTIONS.dar
 PKG_OPTIONS_OPTIONAL_GROUPS=	int
 PKG_OPTIONS_GROUP.int=		dar-int32 dar-int64
 PKG_SUGGESTED_OPTIONS=		dar-int64 ${USE_FEATURES.dar}
-PKG_SUPPORTED_OPTIONS=		threads
+PKG_SUPPORTED_OPTIONS=		threads nls
 
 .include "../../mk/bsd.options.mk"
 
@@ -47,4 +49,12 @@ BUILDLINK_TRANSFORM+=	rm:-lpthread
 .include "../../devel/libthreadar/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-thread-safe
+.endif
+
+# readline/editline
+.if !empty(PKG_OPTIONS:Mnls)
+CONFIGURE_ARGS+= --enable-nls
+PLIST.nls=	yes
+.else
+CONFIGURE_ARGS+= --disable-nls
 .endif
