41dadb96c6a9c9509f777bd535666fb041faaf70
pho <pho@pkgsrc.org>
2024-05-01 07:19:57 +0000

    math/hs-bifunctors: Update to 5.6.2
    5.6.2 [2024.03.19]
    
        Support building with template-haskell-2.22.* (GHC 9.10).

--- math/hs-bifunctors/buildlink3.mk
+++ math/hs-bifunctors/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:37:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/05/01 07:19:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-bifunctors
 
 .if !defined(HS_BIFUNCTORS_BUILDLINK3_MK)
 HS_BIFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.6.1
-BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.6.1nb1
+BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.6.2
+BUILDLINK_ABI_DEPENDS.hs-bifunctors+=	hs-bifunctors>=5.6.2
 BUILDLINK_PKGSRCDIR.hs-bifunctors?=	../../math/hs-bifunctors
 
 .include "../../devel/hs-assoc/buildlink3.mk"
