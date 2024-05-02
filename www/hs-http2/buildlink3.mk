479658a8b822ddcd7058571fba10b65a5d3bc1b2
pho <pho@pkgsrc.org>
2024-05-02 06:59:49 +0000

    www/hs-http2: Update to 5.2.1
    5.2.1
        Using time-manager v0.1.0. #115
    
    5.2.0
        Using http-semantics #114
        Header of http-types should be used as high-level header.
        TokenHeader of http-semantics should be used as low-level header.
        Breaking change: encodeHeader takes Header of http-types.
        Breaking change: decodeHeader returns Header of http-types.
        Breaking change: HeaderName as ByteString is removed.
    
    5.1.4
        Using network-control v0.1.
    
    5.1.3
        Defining SendRequest type synonym. #111
    
    5.1.2
        Make ping rate limit configurable #108
    
    5.1.1
        Deal with RST_STREAM in HalfClosedLocal state #107
    
    5.1.0
        Drop frames after reset #106
        BREAKING CHANGE: Use String for Authority #105
        Properly close streams #104
    
    5.0.1
        Allowing bytestring 0.12.
    
    5.0.0
        Using the network-control package.
        The limits of resources can be specified in ServerConfig and ClientConfig.
        Open streams based on peer's MaxStreams.
        Rejecting Data if it is over the receiving limit.
        Informing MaxStreams properly.
        Informing WindowUpdate properly.
        New API: Server.Internal.runIO and Client.Internal.runIO.

--- www/hs-http2/buildlink3.mk
+++ www/hs-http2/buildlink3.mk
@@ -1,23 +1,26 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/11/02 06:37:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/05/02 06:59:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-http2
 
 .if !defined(HS_HTTP2_BUILDLINK3_MK)
 HS_HTTP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=4.2.2
-BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=4.2.2nb1
+BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=5.2.1
+BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=5.2.1
 BUILDLINK_PKGSRCDIR.hs-http2?=		../../www/hs-http2
 
 .include "../../devel/hs-async/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../www/hs-http-semantics/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
 .include "../../net/hs-network-byte-order/buildlink3.mk"
+.include "../../net/hs-network-control/buildlink3.mk"
 .include "../../devel/hs-psqueues/buildlink3.mk"
 .include "../../time/hs-time-manager/buildlink3.mk"
 .include "../../time/hs-unix-time/buildlink3.mk"
 .include "../../devel/hs-unliftio/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HTTP2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-http2
