a357ab8bc810f2f5054ebd31b4606c7f9e3e4d82
pho <pho@pkgsrc.org>
2024-05-02 04:25:39 +0000

    databases/hs-sqlite-simple: Update to 0.4.19.0
    Upstream provides no release notes.

--- databases/hs-sqlite-simple/buildlink3.mk
+++ databases/hs-sqlite-simple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/11/08 13:18:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/05/02 04:25:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-sqlite-simple
 
 .if !defined(HS_SQLITE_SIMPLE_BUILDLINK3_MK)
 HS_SQLITE_SIMPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18
-BUILDLINK_ABI_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.18.2nb5
+BUILDLINK_API_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.19
+BUILDLINK_ABI_DEPENDS.hs-sqlite-simple+=	hs-sqlite-simple>=0.4.19.0
 BUILDLINK_PKGSRCDIR.hs-sqlite-simple?=		../../databases/hs-sqlite-simple
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
