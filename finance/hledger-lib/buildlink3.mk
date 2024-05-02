460f0fdcb98fe25ef7f0bb1c8a4dca6a479c8c57
pho <pho@pkgsrc.org>
2024-05-02 11:59:34 +0000

    finance/hledger-lib: Update to 1.33.1
    The release note is too long to paste here:
    https://hackage.haskell.org/package/hledger-lib-1.33.1/changelog

--- finance/hledger-lib/buildlink3.mk
+++ finance/hledger-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 06:37:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/05/02 11:59:34 pho Exp $
 
 BUILDLINK_TREE+=	hledger-lib
 
 .if !defined(HLEDGER_LIB_BUILDLINK3_MK)
 HLEDGER_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hledger-lib+=	hledger-lib>=1.31
-BUILDLINK_ABI_DEPENDS.hledger-lib+=	hledger-lib>=1.31nb1
+BUILDLINK_API_DEPENDS.hledger-lib+=	hledger-lib>=1.33.1
+BUILDLINK_ABI_DEPENDS.hledger-lib+=	hledger-lib>=1.33.1
 BUILDLINK_PKGSRCDIR.hledger-lib?=	../../finance/hledger-lib
 
 .include "../../converters/hs-aeson/buildlink3.mk"
