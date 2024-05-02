a4c85d4f9a598570df18748d9858743a9e3e3a9f
pho <pho@pkgsrc.org>
2024-05-02 09:54:43 +0000

    devel/hs-ghc-exactprint: Update to 1.8.0.0
    2022-12-19 v1.8.0.0
    	* Support GHC 9.8.1
    2022-12-19 v1.7.1.0
    	* Add CI for GHC 9.6.3
    	* Reinstate MonadTrans on TransFormT (via @Vekhir)

--- devel/hs-ghc-exactprint/buildlink3.mk
+++ devel/hs-ghc-exactprint/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:36:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/05/02 09:54:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-exactprint
 
 .if !defined(HS_GHC_EXACTPRINT_BUILDLINK3_MK)
 HS_GHC_EXACTPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.7.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.7.0.1nb1
+BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.8.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.8.0.0
 BUILDLINK_PKGSRCDIR.hs-ghc-exactprint?=		../../devel/hs-ghc-exactprint
 
 .include "../../devel/hs-data-default/buildlink3.mk"
-.include "../../devel/hs-fail/buildlink3.mk"
 .include "../../math/hs-free/buildlink3.mk"
 .include "../../devel/hs-ordered-containers/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
