d7062946ab8c892161abc9401e9254ba16dc33fa
pho <pho@pkgsrc.org>
2024-05-02 06:41:01 +0000

    lang/hs-hslua-packaging: Update to 2.3.1
    hslua-packaging-2.3.1 - Released 2024-01-18.
    
        Relaxed upper bound for text and containers, allowing text-2.1, and
        containers-0.7.

--- lang/hs-hslua-packaging/buildlink3.mk
+++ lang/hs-hslua-packaging/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/02 06:41:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-packaging
 
 .if !defined(HS_HSLUA_PACKAGING_BUILDLINK3_MK)
 HS_HSLUA_PACKAGING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.3.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.3.0nb1
+BUILDLINK_API_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.3.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-packaging+=	hs-hslua-packaging>=2.3.1
 BUILDLINK_PKGSRCDIR.hs-hslua-packaging?=	../../lang/hs-hslua-packaging
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
