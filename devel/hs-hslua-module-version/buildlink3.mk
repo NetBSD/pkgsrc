75066be459959b44791b0b6cc655c1dcb22b37b2
pho <pho@pkgsrc.org>
2024-05-02 10:25:54 +0000

    devel/hs-hslua-module-version: Update to 1.1.1
    hslua-module-version-1.1.1 - Released 2024-01-18.
    
        Relaxed upper bound for text, and filepath, allowing text-2.1,
        filepath-1.5.

--- devel/hs-hslua-module-version/buildlink3.mk
+++ devel/hs-hslua-module-version/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:36:41 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/02 10:25:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-version
 
 .if !defined(HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK)
 HS_HSLUA_MODULE_VERSION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.1.0nb1
+BUILDLINK_API_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-version+=	hs-hslua-module-version>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-version?=	../../devel/hs-hslua-module-version
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
