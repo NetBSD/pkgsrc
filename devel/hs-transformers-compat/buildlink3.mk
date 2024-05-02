7fb600bae0f02562cb02edc41c30d90e6465ad5d
pho <pho@pkgsrc.org>
2024-05-02 10:00:01 +0000

    devel/hs-transformers-compat: Turned out neither hs-fail nor hs-generic-deriving were required for building it any longer

--- devel/hs-transformers-compat/buildlink3.mk
+++ devel/hs-transformers-compat/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/11/02 06:37:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/05/02 10:00:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers-compat
 
 .if !defined(HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK)
 HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.7.2nb3
+BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.7.2
+BUILDLINK_ABI_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.7.2nb4
 BUILDLINK_PKGSRCDIR.hs-transformers-compat?=	../../devel/hs-transformers-compat
 
-.include "../../devel/hs-fail/buildlink3.mk"
-.include "../../devel/hs-generic-deriving/buildlink3.mk"
 .endif	# HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-transformers-compat
