33179bc39db81259278df71bb0a95d8e45d0d5cc
pho <pho@pkgsrc.org>
2024-05-02 06:45:32 +0000

    archivers/hs-hslua-module-zip: Update to 1.1.1
    hslua-module-zip-1.1.1 - Released 2024-01-18.
    
        Relaxed upper bound for text, and filepath, allowing text-2.1,
        filepath-1.5.

--- archivers/hs-hslua-module-zip/buildlink3.mk
+++ archivers/hs-hslua-module-zip/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/11/02 06:36:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/05/02 06:45:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-zip
 
 .if !defined(HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK)
 HS_HSLUA_MODULE_ZIP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.1.0nb1
+BUILDLINK_API_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-zip+=	hs-hslua-module-zip>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-zip?=	../../archivers/hs-hslua-module-zip
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
