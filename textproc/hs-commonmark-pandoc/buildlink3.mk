3a8b3b3fc69445007bbcb8f1dac05160166203d3
pho <pho@pkgsrc.org>
2024-05-02 11:06:10 +0000

    textproc/hs-commonmark-pandoc: Update to 0.2.2.1
    0.2.2.1
        Remove nested footnotes (#138).
    
    0.2.2
        Add support for alerts extension (#132).

--- textproc/hs-commonmark-pandoc/buildlink3.mk
+++ textproc/hs-commonmark-pandoc/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2023/11/02 06:37:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/05/02 11:06:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-commonmark-pandoc
 
 .if !defined(HS_COMMONMARK_PANDOC_BUILDLINK3_MK)
 HS_COMMONMARK_PANDOC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.1.3nb3
+BUILDLINK_API_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-commonmark-pandoc+=	hs-commonmark-pandoc>=0.2.2.1
 BUILDLINK_PKGSRCDIR.hs-commonmark-pandoc?=	../../textproc/hs-commonmark-pandoc
 
 .include "../../textproc/hs-commonmark/buildlink3.mk"
