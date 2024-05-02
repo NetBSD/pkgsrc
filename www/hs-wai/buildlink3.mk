097ccdbe981e1349756963da241fcc9d0f856877
pho <pho@pkgsrc.org>
2024-05-02 04:27:50 +0000

    www/hs-wai: Update to 3.2.4
    3.2.4
    
        Add helpers for modifying request headers: modifyRequest and mapRequestHeaders. #710 #952
        Small documentation adjustments like adding more @since markers. #952
        Add setRequestBodyChunks to mirror getRequestBodyChunk and avoid deprecation warnings when using requestBody as a setter. #949
        Overhaul documentation of Middleware. #858

--- www/hs-wai/buildlink3.mk
+++ www/hs-wai/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:37:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/05/02 04:27:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-wai
 
 .if !defined(HS_WAI_BUILDLINK3_MK)
 HS_WAI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-wai+=	hs-wai>=3.2.3
-BUILDLINK_ABI_DEPENDS.hs-wai+=	hs-wai>=3.2.3nb6
+BUILDLINK_API_DEPENDS.hs-wai+=	hs-wai>=3.2.4
+BUILDLINK_ABI_DEPENDS.hs-wai+=	hs-wai>=3.2.4
 BUILDLINK_PKGSRCDIR.hs-wai?=	../../www/hs-wai
 
 .include "../../www/hs-http-types/buildlink3.mk"
