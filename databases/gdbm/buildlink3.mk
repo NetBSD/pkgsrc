72c67dfe81e61bfb621bb0c900610c8e8de14464
cheusov <cheusov@pkgsrc.org>
2024-05-02 08:10:18 +0000

    Sync including devel/gettext-lib/buildlink3.mk in buildlink3.mk and Makefile

--- databases/gdbm/buildlink3.mk
+++ databases/gdbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2020/11/19 02:57:11 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/05/02 08:10:18 cheusov Exp $
 
 BUILDLINK_TREE+=	gdbm
 
@@ -9,8 +9,13 @@ BUILDLINK_API_DEPENDS.gdbm+=	gdbm>=1.8.3
 BUILDLINK_ABI_DEPENDS.gdbm+=	gdbm>=1.18.1
 BUILDLINK_PKGSRCDIR.gdbm?=	../../databases/gdbm
 
+pkgbase:= gdbm
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../converters/libiconv/buildlink3.mk"
+.if ${PKG_BUILD_OPTIONS.gdbm:Mnls}
 .include "../../devel/gettext-lib/buildlink3.mk"
+.endif
 .endif # GDBM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gdbm
