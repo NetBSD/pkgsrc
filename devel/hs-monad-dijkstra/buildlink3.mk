e015dc208f02770763c4ec942ea142155952a493
pho <pho@pkgsrc.org>
2024-05-02 10:09:00 +0000

    devel/hs-monad-dijkstra: Update to 0.1.1.5
    monad-dijkstra 0.1.1.5
    
        Support for containers-0.7.0.0

--- devel/hs-monad-dijkstra/buildlink3.mk
+++ devel/hs-monad-dijkstra/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/11/02 06:36:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/05/02 10:09:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-monad-dijkstra
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-monad-dijkstra
 HS_MONAD_DIJKSTRA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-monad-dijkstra+=	hs-monad-dijkstra>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-monad-dijkstra+=	hs-monad-dijkstra>=0.1.1.4nb1
+BUILDLINK_ABI_DEPENDS.hs-monad-dijkstra+=	hs-monad-dijkstra>=0.1.1.5
 BUILDLINK_PKGSRCDIR.hs-monad-dijkstra?=		../../devel/hs-monad-dijkstra
 
 .include "../../math/hs-free/buildlink3.mk"
