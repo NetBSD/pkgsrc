c3fec905e53efea2ae00e7f7a5b0d8785679f161
cheusov <cheusov@pkgsrc.org>
2024-05-02 14:50:49 +0000

    buildlink3.mk: sync logic of loading ../../devel/gettext-lib/buildlink3.mk with options.mk

--- devel/libidn/buildlink3.mk
+++ devel/libidn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2022/06/28 11:31:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/05/02 14:50:49 cheusov Exp $
 
 BUILDLINK_TREE+=	libidn
 
@@ -9,8 +9,13 @@ BUILDLINK_API_DEPENDS.libidn+=	libidn>=0.4.6
 BUILDLINK_ABI_DEPENDS.libidn+=	libidn>=1.38nb1
 BUILDLINK_PKGSRCDIR.libidn?=	../../devel/libidn
 
+pkgbase:= libidn
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../converters/libiconv/buildlink3.mk"
+.if ${PKG_BUILD_OPTIONS.libidn:Mnls}
 .include "../../devel/gettext-lib/buildlink3.mk"
+.endif
 .include "../../devel/libgetopt/buildlink3.mk"
 .endif # LIBIDN_BUILDLINK3_MK
 
