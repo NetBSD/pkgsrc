b2eae28d5a1c6520e2a6b21a042145c0b5036fb1
pho <pho@pkgsrc.org>
2024-05-02 03:37:54 +0000

    devel/hs-versions: Update to 6.0.6
    6.0.6 (2024-03-08)
    Fixed
        Account for large numbers when parsing on 32-bit (or smaller) systems.
    
    6.0.5 (2024-01-24)
    Fixed
        Certain illegal versions were parsing as PVP.
    
    6.0.4 (2023-12-29)
    Changed
        Bump dependencies to support GHC 9.8.

--- devel/hs-versions/buildlink3.mk
+++ devel/hs-versions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:37:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/05/02 03:37:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-versions
 
 .if !defined(HS_VERSIONS_BUILDLINK3_MK)
 HS_VERSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-versions+=	hs-versions>=6.0.3
-BUILDLINK_ABI_DEPENDS.hs-versions+=	hs-versions>=6.0.3nb1
+BUILDLINK_API_DEPENDS.hs-versions+=	hs-versions>=6.0.6
+BUILDLINK_ABI_DEPENDS.hs-versions+=	hs-versions>=6.0.6
 BUILDLINK_PKGSRCDIR.hs-versions?=	../../devel/hs-versions
 
 .include "../../devel/hs-hashable/buildlink3.mk"
