ed7433b7f9a9ae6358f606872f2994c3fd085d74
pho <pho@pkgsrc.org>
2024-05-02 10:27:16 +0000

    devel/hs-hslua-module-system: Update to 1.1.1
    hslua-module-system-1.1.1 - Released 2024-01-18.
    
        Relaxed upper bound for text, allowing text-2.1.

--- devel/hs-hslua-module-system/buildlink3.mk
+++ devel/hs-hslua-module-system/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:36:41 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/05/02 10:27:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-system
 
 .if !defined(HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK)
 HS_HSLUA_MODULE_SYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.1.0.1nb1
+BUILDLINK_API_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-system+=	hs-hslua-module-system>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-hslua-module-system?=	../../devel/hs-hslua-module-system
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
