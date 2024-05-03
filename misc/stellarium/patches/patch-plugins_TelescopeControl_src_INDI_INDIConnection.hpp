bb0325de64c1f325beeeebf53c62ff8e8089a4db
wiz <wiz@pkgsrc.org>
2024-05-03 12:07:41 +0000

    stellarium: use upstream PR for indi 2+ support
    https://github.com/Stellarium/stellarium/pull/3269
    
    Ride bump.

--- misc/stellarium/patches/patch-plugins_TelescopeControl_src_INDI_INDIConnection.hpp
+++ misc/stellarium/patches/patch-plugins_TelescopeControl_src_INDI_INDIConnection.hpp
@@ -1,10 +1,25 @@
-$NetBSD: patch-plugins_TelescopeControl_src_INDI_INDIConnection.hpp,v 1.1 2024/05/03 11:28:27 wiz Exp $
+$NetBSD: patch-plugins_TelescopeControl_src_INDI_INDIConnection.hpp,v 1.2 2024/05/03 12:07:41 wiz Exp $
 
-Compatibility with indi 2.0.7.
+Port code to INDI 2.0.2
+https://github.com/Stellarium/stellarium/pull/3269/files
 
---- plugins/TelescopeControl/src/INDI/INDIConnection.hpp.orig	2024-05-03 11:24:20.270404353 +0000
+--- plugins/TelescopeControl/src/INDI/INDIConnection.hpp.orig	2024-02-04 13:27:52.000000000 +0000
 +++ plugins/TelescopeControl/src/INDI/INDIConnection.hpp
-@@ -69,18 +69,18 @@ private:
+@@ -21,6 +21,7 @@
+ 
+ #include <QObject>
+ #include "libindi/baseclient.h"
++#include <libindi/basedevice.h>
+ 
+ #include <mutex>
+ #include <QStringList>
+@@ -64,21 +65,16 @@ private:
+ 	void setSpeed(int speed);
+ 
+ 	mutable std::mutex mMutex;
+-	INDI::BaseDevice* mTelescope = nullptr;
++	INDI::BaseDevice mTelescope;
+ 	Coordinates mCoordinates;
  	QStringList mDevices;
  
  public: // from INDI::BaseClient
@@ -18,20 +33,11 @@ Compatibility with indi 2.0.7.
 -	void newText(ITextVectorProperty *tvp) override;
 -	void newLight(ILightVectorProperty *lvp) override;
 -	void newMessage(INDI::BaseDevice *dp, int messageID) override;
--	void serverConnected() override;
--	void serverDisconnected(int exit_code) override;
-+	void newDevice(INDI::BaseDevice *dp);
-+	void removeDevice(INDI::BaseDevice *dp);
-+	void newProperty(INDI::Property *property);
-+	void removeProperty(INDI::Property *property);
-+	void newBLOB(IBLOB *bp);
-+	void newSwitch(ISwitchVectorProperty *svp);
-+	void newNumber(INumberVectorProperty *nvp);
-+	void newText(ITextVectorProperty *tvp);
-+	void newLight(ILightVectorProperty *lvp);
-+	void newMessage(INDI::BaseDevice *dp, int messageID);
-+	void serverConnected();
-+	void serverDisconnected(int exit_code);
++	void newDevice(INDI::BaseDevice dp) override;
++	void removeDevice(INDI::BaseDevice dp) override;
++	void newProperty(INDI::Property property) override;
++	void removeProperty(INDI::Property property) override;
++	void newMessage(INDI::BaseDevice dp, int messageID) override;
+ 	void serverConnected() override;
+ 	void serverDisconnected(int exit_code) override;
  	void unParkTelescope();
- 	//void parkTelescope();
- };
