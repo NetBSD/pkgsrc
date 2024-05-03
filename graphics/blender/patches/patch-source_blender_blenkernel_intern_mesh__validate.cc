41bf49b1014ec1dc89c3d898fcd27c78f91fc137
ryoon <ryoon@pkgsrc.org>
2024-05-03 20:34:36 +0000

    graphics/blender: Update to 4.1.1
    Changelog:
    Blender 4.1.1 includes many bugfixes and improvements in the following
    catogories:
    
        Animation & Rigging
        Compositor
        Cycles
        EEVEE
        Geometry Nodes
        Import & Export
        Modeling
        Python API
        Rendering
        Sculpting
        Sequencer
        User Interface
        Add-ons

--- graphics/blender/patches/patch-source_blender_blenkernel_intern_mesh__validate.cc
+++ graphics/blender/patches/patch-source_blender_blenkernel_intern_mesh__validate.cc
@@ -1,17 +1,17 @@
-$NetBSD: patch-source_blender_blenkernel_intern_mesh__validate.cc,v 1.3 2023/10/15 19:53:20 ryoon Exp $
+$NetBSD: patch-source_blender_blenkernel_intern_mesh__validate.cc,v 1.4 2024/05/03 20:34:37 ryoon Exp $
 
---- source/blender/blenkernel/intern/mesh_validate.cc.orig	2023-05-24 15:42:42.000000000 +0000
+--- source/blender/blenkernel/intern/mesh_validate.cc.orig	2024-02-07 15:39:40.000000000 +0000
 +++ source/blender/blenkernel/intern/mesh_validate.cc
-@@ -302,7 +302,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
+@@ -311,7 +311,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
  
-   for (i = 0; i < totvert; i++) {
+   for (i = 0; i < verts_num; i++) {
      for (j = 0; j < 3; j++) {
 -      if (!isfinite(vert_positions[i][j])) {
 +      if (!std::isfinite(vert_positions[i][j])) {
          PRINT_ERR("\tVertex %u: has invalid coordinate", i);
  
          if (do_fixes) {
-@@ -797,7 +797,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
+@@ -805,7 +805,7 @@ bool BKE_mesh_validate_arrays(Mesh *mesh
  
        for (j = 0, dw = dv->dw; j < dv->totweight; j++, dw++) {
          /* NOTE: greater than max defgroups is accounted for in our code, but not < 0. */
