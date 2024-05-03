22ab32593e32c6763d6d5de666573d2879b8c23f
jperkin <jperkin@pkgsrc.org>
2024-05-03 19:03:21 +0000

    python312: Remove gettext-lib from buildlink3.mk.
    Unbreaks dependencies since gettext-lib was removed from the main package.

--- lang/python312/buildlink3.mk
+++ lang/python312/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/29 11:02:41 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/05/03 19:03:21 jperkin Exp $
 
 BUILDLINK_TREE+=	python312
 
@@ -19,7 +19,6 @@ BUILDLINK_TRANSFORM+=		l:python:python3.12
 .if !${BUILDLINK_DEPMETHOD.python312:U:Mbuild}
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
-.include "../../devel/gettext-lib/buildlink3.mk"
 .endif
 
 .endif # PYTHON312_BUILDLINK3_MK
