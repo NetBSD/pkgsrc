c00c80f7bb4eba1d7ea388f0329e00241ec2258b
pho <pho@pkgsrc.org>
2024-05-03 18:22:48 +0000

    converters/hs-cborg-json: Update to 0.2.6.0
    0.2.6.0
        Use Base62Url encoding for bytes
        Support GHC 9.8

--- converters/hs-cborg-json/buildlink3.mk
+++ converters/hs-cborg-json/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/11/02 06:36:12 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/05/03 18:22:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-cborg-json
 
 .if !defined(HS_CBORG_JSON_BUILDLINK3_MK)
 HS_CBORG_JSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.5
-BUILDLINK_ABI_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.5.0nb3
+BUILDLINK_API_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.6
+BUILDLINK_ABI_DEPENDS.hs-cborg-json+=	hs-cborg-json>=0.2.6.0
 BUILDLINK_PKGSRCDIR.hs-cborg-json?=	../../converters/hs-cborg-json
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../converters/hs-aeson-pretty/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
 .include "../../devel/hs-cborg/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
