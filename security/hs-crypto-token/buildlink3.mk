a5590d98a7d431a43b5b4215c2e94c11a7e913dd
pho <pho@pkgsrc.org>
2024-05-04 08:21:56 +0000

    security/hs-crypto-token: import hs-crypto-token-0.1.1
    Encrypted tokens/tickets to keep state in the client side.

---
+++ security/hs-crypto-token/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/04 08:21:56 pho Exp $
+
+BUILDLINK_TREE+=	hs-crypto-token
+
+.if !defined(HS_CRYPTO_TOKEN_BUILDLINK3_MK)
+HS_CRYPTO_TOKEN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-crypto-token+=	hs-crypto-token>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-crypto-token+=	hs-crypto-token>=0.1.1
+BUILDLINK_PKGSRCDIR.hs-crypto-token?=	../../security/hs-crypto-token
+
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../net/hs-network-byte-order/buildlink3.mk"
+.endif	# HS_CRYPTO_TOKEN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-crypto-token
