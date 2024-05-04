1f7a8c1f3b950e16cf745e57ed749c51a1a5c482
pho <pho@pkgsrc.org>
2024-05-04 08:09:34 +0000

    net/hs-crypton-connection: Update to 0.3.2
    Version 0.3.2
        Supporting tls v2.0.0.

--- net/hs-crypton-connection/buildlink3.mk
+++ net/hs-crypton-connection/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:37:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/05/04 08:09:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypton-connection
 
 .if !defined(HS_CRYPTON_CONNECTION_BUILDLINK3_MK)
 HS_CRYPTON_CONNECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.1nb1
+BUILDLINK_API_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.2
 BUILDLINK_PKGSRCDIR.hs-crypton-connection?=	../../net/hs-crypton-connection
 
 .include "../../devel/hs-basement/buildlink3.mk"
