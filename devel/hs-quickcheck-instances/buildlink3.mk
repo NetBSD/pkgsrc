a00b3005772ea1264486a925e7c2f68d542d9845
pho <pho@pkgsrc.org>
2024-05-04 06:47:41 +0000

    devel/hs-quickcheck-instances: import hs-quickcheck-instances-0.3.30
    QuickCheck instances.
    
    The goal is to supply QuickCheck instances for types provided by the
    Haskell Platform.
    
    Since all of these instances are provided as orphans, I recommend that you
    do not use this library within another library module, so that you don't
    impose these instances on down-stream consumers of your code.

---
+++ devel/hs-quickcheck-instances/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/04 06:47:41 pho Exp $
+
+BUILDLINK_TREE+=	hs-quickcheck-instances
+
+.if !defined(HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK)
+HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-quickcheck-instances+=	hs-quickcheck-instances>=0.3.30
+BUILDLINK_ABI_DEPENDS.hs-quickcheck-instances+=	hs-quickcheck-instances>=0.3.30
+BUILDLINK_PKGSRCDIR.hs-quickcheck-instances?=	../../devel/hs-quickcheck-instances
+
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../devel/hs-data-fix/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-integer-logarithms/buildlink3.mk"
+.include "../../time/hs-old-time/buildlink3.mk"
+.include "../../devel/hs-OneTuple/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-QuickCheck/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-splitmix/buildlink3.mk"
+.include "../../devel/hs-strict/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-these/buildlink3.mk"
+.include "../../time/hs-time-compat/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-uuid-types/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-text-short/buildlink3.mk"
+.endif	# HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-quickcheck-instances
