ac190241a03e5f4906e33ad1e9c6e340bab4f1cc
pho <pho@pkgsrc.org>
2024-05-04 07:04:15 +0000

    devel/hs-lsp: Update to 2.5.0.0
    2.5.0.0
    
        The server will now reject messages sent after shutdown has been received.
        There is a shutdownBarrier member in the server state which can be used to conveniently run actions when shutdown is triggered.
    
    2.4.0.0
    
        Server-created progress now will not send reports until and unless the client confirms the progress token creation.
        Progress helper functions now can take a progress token provided by the client, so client-initiated progress can now be supported properly.
        The server options now allow the user to say whether the server should advertise support for client-initiated progress or not.
        The server now dynamically registers for workspace/didChangeConfiguration notifications, to ensure that newer clients continue to send them.
        Removed getCompletionPrefix from the VFS module. This is specific to completing Haskell identifiers and doesn't belong here. It has already been moved to ghcide some time ago.
    
    2.3.0.0
    
        Fix inference of server capabilities for newer methods (except notebook methods).
        VFS no longer requires IO to initialize, functions that wrote to a temporary directory now take the directory as an argument.

--- devel/hs-lsp/buildlink3.mk
+++ devel/hs-lsp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:36:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/04 07:04:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-lsp
 
 .if !defined(HS_LSP_BUILDLINK3_MK)
 HS_LSP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lsp+=	hs-lsp>=2.2.0
-BUILDLINK_ABI_DEPENDS.hs-lsp+=	hs-lsp>=2.2.0.0nb1
+BUILDLINK_API_DEPENDS.hs-lsp+=	hs-lsp>=2.5.0
+BUILDLINK_ABI_DEPENDS.hs-lsp+=	hs-lsp>=2.5.0.0
 BUILDLINK_PKGSRCDIR.hs-lsp?=	../../devel/hs-lsp
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -14,15 +14,14 @@ BUILDLINK_PKGSRCDIR.hs-lsp?=	../../devel/hs-lsp
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
 .include "../../sysutils/hs-co-log-core/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-lens/buildlink3.mk"
 .include "../../converters/hs-lens-aeson/buildlink3.mk"
 .include "../../devel/hs-lsp-types/buildlink3.mk"
 .include "../../textproc/hs-prettyprinter/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
-.include "../../devel/hs-row-types/buildlink3.mk"
 .include "../../devel/hs-sorted-list/buildlink3.mk"
-.include "../../sysutils/hs-temporary/buildlink3.mk"
 .include "../../textproc/hs-text-rope/buildlink3.mk"
 .include "../../devel/hs-unliftio-core/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
