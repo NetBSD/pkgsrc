4bb2d984e140ad750a62bfacc25d94518e062c86
pho <pho@pkgsrc.org>
2024-05-04 08:22:50 +0000

    security/hs-tls-session-manager: Update to 0.0.5
    Upstream provides no release notes.

--- security/hs-tls-session-manager/buildlink3.mk
+++ security/hs-tls-session-manager/buildlink3.mk
@@ -1,19 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/11/02 06:37:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2024/05/04 08:22:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-tls-session-manager
 
 .if !defined(HS_TLS_SESSION_MANAGER_BUILDLINK3_MK)
 HS_TLS_SESSION_MANAGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tls-session-manager+=	hs-tls-session-manager>=0.0.4
-BUILDLINK_ABI_DEPENDS.hs-tls-session-manager+=	hs-tls-session-manager>=0.0.4nb7
+BUILDLINK_API_DEPENDS.hs-tls-session-manager+=	hs-tls-session-manager>=0.0.5
+BUILDLINK_ABI_DEPENDS.hs-tls-session-manager+=	hs-tls-session-manager>=0.0.5
 BUILDLINK_PKGSRCDIR.hs-tls-session-manager?=	../../security/hs-tls-session-manager
 
 .include "../../devel/hs-auto-update/buildlink3.mk"
 .include "../../devel/hs-basement/buildlink3.mk"
 .include "../../time/hs-clock/buildlink3.mk"
+.include "../../security/hs-crypto-token/buildlink3.mk"
 .include "../../devel/hs-memory/buildlink3.mk"
 .include "../../devel/hs-psqueues/buildlink3.mk"
+.include "../../devel/hs-serialise/buildlink3.mk"
 .include "../../security/hs-tls/buildlink3.mk"
 .endif	# HS_TLS_SESSION_MANAGER_BUILDLINK3_MK
 
