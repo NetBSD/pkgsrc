58b315156ab1382606beb15994ca38bf31536478
pho <pho@pkgsrc.org>
2024-05-04 13:34:00 +0000

    converters/hs-pandoc-server: Update to 0.1.0.5
    Upstream provides no release notes.

--- converters/hs-pandoc-server/buildlink3.mk
+++ converters/hs-pandoc-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/11/02 06:36:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/05/04 13:34:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-server
 
@@ -6,14 +6,14 @@ BUILDLINK_TREE+=	hs-pandoc-server
 HS_PANDOC_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-pandoc-server+=	hs-pandoc-server>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-pandoc-server+=	hs-pandoc-server>=0.1.0.3nb1
+BUILDLINK_ABI_DEPENDS.hs-pandoc-server+=	hs-pandoc-server>=0.1.0.5
 BUILDLINK_PKGSRCDIR.hs-pandoc-server?=		../../converters/hs-pandoc-server
 
 .include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../converters/hs-base64/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../textproc/hs-doctemplates/buildlink3.mk"
-.include "../../converters/hs-pandoc-base/buildlink3.mk"
+.include "../../converters/hs-pandoc/buildlink3.mk"
 .include "../../textproc/hs-pandoc-types/buildlink3.mk"
 .include "../../www/hs-servant-server/buildlink3.mk"
 .include "../../textproc/hs-skylighting/buildlink3.mk"
