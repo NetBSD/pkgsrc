85fa5825f5bce584f83cea040ff2d146a130cc87
pho <pho@pkgsrc.org>
2024-05-05 09:01:23 +0000

    databases/hs-direct-sqlite: Update the version constraint on sqlite3

--- databases/hs-direct-sqlite/buildlink3.mk
+++ databases/hs-direct-sqlite/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/04/29 02:33:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/05/05 09:01:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-direct-sqlite
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.29
 BUILDLINK_ABI_DEPENDS.hs-direct-sqlite+=	hs-direct-sqlite>=2.3.29
 BUILDLINK_PKGSRCDIR.hs-direct-sqlite?=		../../databases/hs-direct-sqlite
 
+BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.45.3
 .include "../../databases/sqlite3/buildlink3.mk"
 .endif	# HS_DIRECT_SQLITE_BUILDLINK3_MK
 
