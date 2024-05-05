fcf31bcac48263f73984de136ae27c6c14b2843b
pho <pho@pkgsrc.org>
2024-05-05 13:05:54 +0000

    devel/hs-th-abstraction: Update buildlink3.mk

--- devel/hs-th-abstraction/buildlink3.mk
+++ devel/hs-th-abstraction/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2023/11/02 06:36:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/05/05 13:05:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-abstraction
 
 .if !defined(HS_TH_ABSTRACTION_BUILDLINK3_MK)
 HS_TH_ABSTRACTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.6.0
-BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.6.0.0nb1
+BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.7.0
+BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.7.0.0
 BUILDLINK_PKGSRCDIR.hs-th-abstraction?=		../../devel/hs-th-abstraction
 .endif	# HS_TH_ABSTRACTION_BUILDLINK3_MK
 
