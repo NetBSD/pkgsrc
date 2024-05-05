c62eb7d7f1ee4a21c373957fe94007fcfc7295f0
pho <pho@pkgsrc.org>
2024-05-05 08:37:53 +0000

    devel/stan: import stan-0.1.2.1
    Stan is a command-line tool for analysing Haskell projects. It discovers
    which parts of the code can potentially be improved, and offers suggestions
    on how to do so. Stan is searching for not only performance or error-prone
    code pieces, but it also can help with establishing and applying
    best-practices from the whole Haskell ecosystem.
    
    Although Haskell is a statically typed language, not all properties can be
    encoded in types. Even though GHC is quite a powerful compiler, it tries to
    be library-agnostic and provide only language-specific suggestions, while
    Stan uses the knowledge about the current state of the ecosystem and
    commonly used libraries.
    
    You will find Stan helpful if you enjoy writing in Haskell, but want more
    guarantees from your code, not provided by the Haskell type system or GHC.

---
+++ devel/stan/buildlink3.mk
@@ -0,0 +1,36 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/05 08:37:53 pho Exp $
+
+BUILDLINK_TREE+=	stan
+
+.if !defined(STAN_BUILDLINK3_MK)
+STAN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.stan+=	stan>=0.1.2
+BUILDLINK_ABI_DEPENDS.stan+=	stan>=0.1.2.1
+BUILDLINK_PKGSRCDIR.stan?=	../../devel/stan
+
+# lib:stan
+.include "../../converters/hs-base64/buildlink3.mk"
+.include "../../textproc/hs-blaze-html/buildlink3.mk"
+.include "../../textproc/hs-clay/buildlink3.mk"
+.include "../../devel/hs-colourista/buildlink3.mk"
+.include "../../security/hs-cryptohash-sha1/buildlink3.mk"
+.include "../../sysutils/hs-dir-traverse/buildlink3.mk"
+.include "../../devel/hs-extensions/buildlink3.mk"
+.include "../../devel/hs-gitrev/buildlink3.mk"
+.include "../../converters/hs-microaeson/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../devel/hs-pretty-simple/buildlink3.mk"
+.include "../../devel/hs-relude/buildlink3.mk"
+.include "../../devel/hs-slist/buildlink3.mk"
+.include "../../textproc/hs-tomland/buildlink3.mk"
+.include "../../devel/hs-trial/buildlink3.mk"
+.include "../../devel/hs-trial-optparse-applicative/buildlink3.mk"
+.include "../../textproc/hs-trial-tomland/buildlink3.mk"
+
+# lib:target
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+
+.endif	# STAN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-stan
