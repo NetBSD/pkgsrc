ee0b39d082a82ac4f543010e422e40aad898a0e4
adam <adam@pkgsrc.org>
2024-05-05 20:58:06 +0000

    pcl: updated to 1.14.1
    PCL 1.14.1
    
    One big change in this release is that the dependency boost-filesystem is now optional if you compile PCL and your own project that uses PCL as C++17 (e.g. by adding -DCMAKE_CXX_STANDARD=17 -DCMAKE_CUDA_STANDARD=17 to the cmake call). The default C++ standard for PCL is currently still C++14, where boost-filesystem is a hard requirement. Some modules will not be built if boost-filesystem is not available (currently recognition, outofcore, some apps). Big thanks to @cybaol for implementing this, and of course also thanks to everyone else who contributed to this release!
    
    
    PCL 1.14.0
    
    This release brings for example:
    
    a faster and more robust GeneralizedIterativeClosestPoint (GICP) registration
    better compatibility with the latest compilers and 3rd party libraries (Eigen, Boost, etc.)
    and many more bug fixes and performance enhancements

--- graphics/pcl/options.mk
+++ graphics/pcl/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2021/05/08 22:03:14 thor Exp $
+# $NetBSD: options.mk,v 1.3 2024/05/05 20:58:06 adam Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pcl
 
 # Qt could be an option, but relies on vtk's qt support, too.
@@ -14,15 +14,15 @@ PLIST_VARS+=	vtk qt
 
 CMAKE_ARGS+=	-DWITH_VTK=TRUE
 PLIST.vtk=	yes
-.include "../../graphics/vtk/buildlink3.mk"
 .include "../../graphics/glew/buildlink3.mk"
+.include "../../graphics/vtk/buildlink3.mk"
 .  if ${PKG_BUILD_OPTIONS.vtk:Mqt}
-CMAKE_ARGS+=	-DWITH_QT=TRUE
-CMAKE_ARGS+=	-DBUILD_apps=TRUE
+CMAKE_ARGS+=	-DWITH_QT=QT5
+CMAKE_ARGS+=	-DBUILD_apps=ON
 PLIST.qt=	yes
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .  endif
 .else
 CMAKE_ARGS+=	-DWITH_VTK=FALSE
-CMAKE_ARGS+=    -DWITH_QT=FALSE
+CMAKE_ARGS+=    -DWITH_QT=NO
 .endif
