d5a9e0cb6e990fd968a0914d2cadbb2165c0958c
pho <pho@pkgsrc.org>
2024-05-05 08:49:34 +0000

    devel/stylish-haskell: import stylish-haskell-0.14.6.0
    A simple Haskell code prettifier. The goal is not to format all of the code
    in a file, since I find those kind of tools often "get in the
    way". However, manually cleaning up import statements etc. gets tedious
    very quickly.
    
    This tool tries to help where necessary without getting in the way.
    
    Features:
    * Aligns and sorts import statements
    * Groups and wraps {-# LANGUAGE #-} pragmas, can remove (some) redundant
      pragmas
    * Removes trailing whitespace
    * Aligns branches in case and fields in records
    * Converts line endings (customizable)
    * Replaces tabs by four spaces (turned off by default)
    * Replaces some ASCII sequences by their Unicode equivalents (turned off by
      default)
    * Format data constructors and fields in records.

---
+++ devel/stylish-haskell/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/05 08:49:34 pho Exp $
+
+BUILDLINK_TREE+=	stylish-haskell
+
+.if !defined(STYLISH_HASKELL_BUILDLINK3_MK)
+STYLISH_HASKELL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.stylish-haskell+=	stylish-haskell>=0.14.6
+BUILDLINK_ABI_DEPENDS.stylish-haskell+=	stylish-haskell>=0.14.6.0
+BUILDLINK_PKGSRCDIR.stylish-haskell?=	../../devel/stylish-haskell
+
+# lib:stylish-haskell
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-file-embed/buildlink3.mk"
+.include "../../devel/hs-ghc-lib-parser-ex/buildlink3.mk"
+.include "../../textproc/hs-HsYAML/buildlink3.mk"
+.include "../../textproc/hs-HsYAML-aeson/buildlink3.mk"
+.include "../../textproc/hs-regex-tdfa/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
+
+.endif	# STYLISH_HASKELL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-stylish-haskell
