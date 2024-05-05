1d1cf23abfc77e260aea25669266c51f75276b00
pho <pho@pkgsrc.org>
2024-05-05 08:16:16 +0000

    devel/hs-validation-selective: import hs-validation-selective-0.2.0.0
    Lightweight pure data validation based on Applicative and Selective
    functors.
    
    validation-selective is built around the following data type:
    
        data Validation e a
            = Failure e
            | Success a
    
    This data type is similar to Either but allows accumulating all errors
    instead of short-circuiting on the first one.

---
+++ devel/hs-validation-selective/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/05 08:16:16 pho Exp $
+
+BUILDLINK_TREE+=	hs-validation-selective
+
+.if !defined(HS_VALIDATION_SELECTIVE_BUILDLINK3_MK)
+HS_VALIDATION_SELECTIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-validation-selective+=	hs-validation-selective>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-validation-selective+=	hs-validation-selective>=0.2.0.0
+BUILDLINK_PKGSRCDIR.hs-validation-selective?=	../../devel/hs-validation-selective
+
+.include "../../devel/hs-selective/buildlink3.mk"
+.endif	# HS_VALIDATION_SELECTIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-validation-selective
