15eea74213684e0a970879934577af38142e82c6
pho <pho@pkgsrc.org>
2024-05-05 05:28:36 +0000

    devel/hs-colourista: import hs-colourista-0.1.0.2
    colourista is the library that provides a convenient interface for printing
    colourful messages to the terminal. It is based on ansi-terminal, however,
    in contradistinction to this Haskell library, colourista is a high-level
    wrapper focused on easily achieved output modification without low-level
    setup.

---
+++ devel/hs-colourista/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/05 05:28:36 pho Exp $
+
+BUILDLINK_TREE+=	hs-colourista
+
+.if !defined(HS_COLOURISTA_BUILDLINK3_MK)
+HS_COLOURISTA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-colourista+=	hs-colourista>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-colourista+=	hs-colourista>=0.1.0.2
+BUILDLINK_PKGSRCDIR.hs-colourista?=	../../devel/hs-colourista
+
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.endif	# HS_COLOURISTA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-colourista
