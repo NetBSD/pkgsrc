d10c1d4f1b63ca5bda82183b62990200b73b1f9e
ktnb <ktnb@pkgsrc.org>
2024-05-05 14:31:31 +0000

    nudoku: update to 4.0.0
    Packaging changes:
    Remove FAKE_NCURSES and add ncursesw because ncursesw is now required
    and leaving FAKE_NCURSES enabled causes an issue with the alternate
    character set (acs_map specifically).
    
    Upstream changes:
    3.0.0 -> 4.0.0
    - Add Turkish translation
    - Add undo feature
    
    2.1.0 -> 3.0.0
    - Fix build failure with ncurses 6.3
    - Add call to PKG_PROG_PKG_CONFIG and pkg-check ncurses libs
    - Fix build with musl
    - Add highlighting of user input
    - Fix toggling marks removing solution

--- games/nudoku/patches/patch-src_main.c
+++ games/nudoku/patches/patch-src_main.c
@@ -1,35 +1,24 @@
-$NetBSD: patch-src_main.c,v 1.1 2020/03/16 12:59:02 fcambus Exp $
+$NetBSD: patch-src_main.c,v 1.2 2024/05/05 14:31:32 ktnb Exp $
 
-- Add missing <locale.h> header, for setlocale.
-- Use wgetch(grid) instead of getch(), otherwise wmove() doesn't
-  move the cursor in the grid window.
+Fixed upstream bug where variable was defined without braces in
+switch. Reported upstream: https://github.com/jubalh/nudoku/issues/61
 
---- src/main.c.orig	2019-07-30 09:50:49.000000000 +0000
+--- src/main.c.orig	2024-05-05 01:37:03.380017435 +0000
 +++ src/main.c
-@@ -19,7 +19,8 @@ along with this program. If not, see <ht
- */
- 
- /* INCLUDES */
--#include "gettext.h"			/* gettext */
-+#include <libintl.h>			/* gettext */
-+#include <locale.h>				/* setlocale */
- #include <stdlib.h>				/* rand, srand */
- #include <unistd.h>				/* getopt */
- #include <ncurses.h>			/* ncurses */
-@@ -294,6 +295,7 @@ static void init_windows(void)
- 	status = newwin(STATUS_LINES, STATUS_COLS, STATUS_Y, STATUS_X);
- 
- 	grid = newwin(GRID_LINES, GRID_COLS, GRID_Y, GRID_X);
-+	keypad(grid, true);
- 	_draw_grid();
- 
- 	infobox = newwin(INFO_LINES, INFO_COLS, INFO_Y, INFO_X);
-@@ -497,7 +499,7 @@ int main(int argc, char *argv[])
- #endif // DEBUG
- 		refresh();
- 		wrefresh(grid);
--		key = getch();
-+		key = wgetch(grid);
- 		// clear status window
- 		werase(status);
- 		switch(key)
+@@ -708,6 +708,7 @@ int main(int argc, char *argv[])
+ 				}
+ 				break;
+ 			case 'u': // Undo
++			{
+ 				move_t old_move;
+ 				if (undo_stack_pop(&old_move))
+ 				{	// Stack empty
+@@ -720,7 +721,7 @@ int main(int argc, char *argv[])
+ 				user_board[posy*9+posx] = old_move.prev_val;
+ 				fill_grid(user_board, plain_board, x, y);
+ 				break;
+-
++			}
+ 			default:
+ 				break;
+ 		}
