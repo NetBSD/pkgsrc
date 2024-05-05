5aef8f80f9abb6d0e0b11abc39fb059bec3799b7
pho <pho@pkgsrc.org>
2024-05-05 10:23:58 +0000

    converters/hs-pandoc-base: The package has been renamed to converters/hs-pandoc

--- converters/hs-pandoc-base/version.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: version.mk,v 1.2 2023/10/31 05:14:53 pho Exp $
-#
-# Included by ../../converters/hs-pandoc-base/Makefile
-# Included by ../../converters/pandoc/Makefile
-
-PANDOC_BASE_VERSION=	3.1.9
