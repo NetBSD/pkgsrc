16016f6070ba92c68120ed308ee68d429818e1b5
pho <pho@pkgsrc.org>
2024-05-05 08:21:18 +0000

    textproc/hs-tomland: import hs-tomland-1.3.3.2
    tomland is a Haskell library for Bidirectional TOML Serialization. It
    provides the composable interface for implementing TOML codecs. If you want
    to use TOML as a configuration for your tool or application, you can use
    tomland to easily convert in both ways between textual TOML representation
    and Haskell types.
    
    tomland supports TOML spec version 0.5.0.
    
    The following blog post has more details about the library design and
    internal implementation details:
    
        tomland: Bidirectional TOML Serialization
        https://kowainik.github.io/posts/2019-01-14-tomland

---
+++ textproc/hs-tomland/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/05/05 08:21:18 pho Exp $
+
+BUILDLINK_TREE+=	hs-tomland
+
+.if !defined(HS_TOMLAND_BUILDLINK3_MK)
+HS_TOMLAND_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tomland+=	hs-tomland>=1.3.3
+BUILDLINK_ABI_DEPENDS.hs-tomland+=	hs-tomland>=1.3.3.2
+BUILDLINK_PKGSRCDIR.hs-tomland?=	../../textproc/hs-tomland
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../textproc/hs-megaparsec/buildlink3.mk"
+.include "../../devel/hs-parser-combinators/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-validation-selective/buildlink3.mk"
+.endif	# HS_TOMLAND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tomland
