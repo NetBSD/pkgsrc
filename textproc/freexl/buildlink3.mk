3a14a32af8419cbc78d70e60a20d2bd3cdebcaa5
adam <adam@pkgsrc.org>
2024-05-06 10:13:01 +0000

    freexl: updated to 2.0.0
    The following document formats are supported starting since version 2.0:
    
    MS Excel legacy BIFF binary format (.xls)
    MS Excel new XML-based format (.xlsx)
    Free/OpenOffice Calc XML-based format (.ods)

--- textproc/freexl/buildlink3.mk
+++ textproc/freexl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/11/27 14:00:26 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/05/06 10:13:01 adam Exp $
 
 BUILDLINK_TREE+=	freexl
 
@@ -8,7 +8,9 @@ FREEXL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.freexl+=	freexl>=1.0.2
 BUILDLINK_PKGSRCDIR.freexl?=	../../textproc/freexl
 
+.include "../../archivers/minizip/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
 .endif	# FREEXL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-freexl
