75b6a4eab78c4b4d622cf83fea40f0de79082f9c
nia <nia@pkgsrc.org>
2024-05-06 18:01:30 +0000

    import a legacy version of SDL2_mixer to go with the legacy SDL2.

--- audio/SDL2_mixer/buildlink3.mk
+++ audio/SDL2_mixer/buildlink3.mk
@@ -1,12 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.15 2024/04/06 08:04:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2024/05/06 18:01:31 nia Exp $
 
 BUILDLINK_TREE+=	SDL2_mixer
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${MACHINE_PLATFORM:MDarwin-*-powerpc*}
+# This defines SDL2_MIXER_BUILDLINK3_MK.
+.  include "../../audio/SDL2_mixer-legacy/buildlink3.mk"
+.endif
 
 .if !defined(SDL2_MIXER_BUILDLINK3_MK)
 SDL2_MIXER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_mixer+=	SDL2_mixer>=2.0.0
-BUILDLINK_ABI_DEPENDS.SDL2_mixer+=	SDL2_mixer>=2.0.4nb15
 BUILDLINK_PKGSRCDIR.SDL2_mixer?=	../../audio/SDL2_mixer
 
 .include "../../devel/SDL2/buildlink3.mk"
