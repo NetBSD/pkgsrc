46d87f2445b5f6c825b8892ac120578a11627bb8
pho <pho@pkgsrc.org>
2024-05-06 09:58:13 +0000

    devel/hs-hfsevents: Update to 0.1.7
    Upstream provides no release notes.

--- devel/hs-hfsevents/buildlink3.mk
+++ devel/hs-hfsevents/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/11/02 06:36:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/05/06 09:58:13 pho Exp $
 
 BUILDLINK_TREE+=	hs-hfsevents
 
 .if !defined(HS_HFSEVENTS_BUILDLINK3_MK)
 HS_HFSEVENTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hfsevents+=	hs-hfsevents>=0.1.6
-BUILDLINK_ABI_DEPENDS.hs-hfsevents+=	hs-hfsevents>=0.1.6nb1
+BUILDLINK_API_DEPENDS.hs-hfsevents+=	hs-hfsevents>=0.1.7
+BUILDLINK_ABI_DEPENDS.hs-hfsevents+=	hs-hfsevents>=0.1.7
 BUILDLINK_PKGSRCDIR.hs-hfsevents?=	../../devel/hs-hfsevents
 
 .include "../../devel/hs-cereal/buildlink3.mk"
