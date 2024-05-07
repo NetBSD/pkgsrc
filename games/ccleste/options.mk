33280f3f75c7a5bd05fe21773d61d6a1b330cdf6
nia <nia@pkgsrc.org>
2024-05-07 10:13:14 +0000

    ccleste: fix build on darwin

--- games/ccleste/options.mk
+++ games/ccleste/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2024/05/07 08:40:19 nia Exp $
+# $NetBSD: options.mk,v 1.2 2024/05/07 10:13:14 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ccleste
 
@@ -17,12 +17,14 @@ PKG_SUGGESTED_OPTIONS=		sdl2
 
 .if !empty(PKG_OPTIONS:Msdl2)
 MAKE_FLAGS+=	SDL_VER=2
+LDFLAGS.Darwin+=	-lSDL2main
 .  include "../../audio/SDL2_mixer/buildlink3.mk"
 .  include "../../devel/SDL2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl)
-MAKE_FLAGS+=	SDL_VER=1
+MAKE_FLAGS+=		SDL_VER=1
+LDFLAGS.Darwin+=	-lSDLmain
 .  include "../../audio/SDL_mixer/buildlink3.mk"
 .  include "../../devel/SDL/buildlink3.mk"
 .endif
