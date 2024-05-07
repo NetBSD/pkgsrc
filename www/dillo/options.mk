77d8a954d66abffe55046f7e389b512c86ad7c57
abs <abs@pkgsrc.org>
2024-05-07 18:06:35 +0000

    Updated www/dillo to 3.1.0
    Since the last release 3.0.5 from 2015 a lot of things have happened
    to the Dillo project. A plot of number of commits over time colored
    by authors. There is a missing gap from mid 2002 to mid 2008. And
    from 2017 to end of 2023 there is almost no commits. Here is a
    short timeline to put things into perspective:
    
     - On 2016, the main developer of the layout engine, Sebastian
       Geerken, passed away
     - On 2017, the development stopped
     - On 2019, the last email from Jorge Arellano Cid, the lead
       developer of Dillo, was recorded by the mailing list
     - On 2022, the domain dillo.org was lost
     - On 2024, an attempt to resurrect the project began by Rodrigo
       Arias Mallo
    
    This release contains a lot of changes accumulated from the 2015-2017
    period, as well as fixes and small features introduced in 2024.
    Here is a summary:
    
     - Add support for floating HTML elements, which involved a big
       redesign
     - Add support for OpenSSL, LibreSSL and mbed TLS for HTTPS, which
       is now enabled by default
     - Add a CI pipeline to build Dillo on Ubuntu, MacOS, FreeBSD and
       Windows (via cygwin)
     - Add automatic HTML rendering tests
     - Improve and extend the Dillo manual

--- www/dillo/options.mk
+++ www/dillo/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2020/09/20 08:55:48 bsiegert Exp $
+# $NetBSD: options.mk,v 1.7 2024/05/07 18:06:35 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dillo
 PKG_SUPPORTED_OPTIONS=	inet6 ssl
@@ -11,13 +11,13 @@ CONFIGURE_ARGS+=	--enable-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
-CONFIGURE_ARGS+=	--enable-ssl
+CONFIGURE_ARGS+=	--enable-tls
 LIBS+=			-lssl
 .include "../../security/openssl/buildlink3.mk"
 
 SUBST_CLASSES+=		sslcerts
 SUBST_MESSAGE.sslcerts=	Fixing SSL certificate directory.
-SUBST_FILES.sslcerts=	dpi/https.c
+SUBST_FILES.sslcerts=	src/IO/tls_openssl.c src/IO/tls_mbedtls.c
 SUBST_STAGE.sslcerts=	post-extract
 SUBST_SED.sslcerts=	-e 's,"/etc/ssl/certs,"${SSLCERTS},'
 
