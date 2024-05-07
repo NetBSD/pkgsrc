1f82a4f3b9d25bc8fec1bc3eea731e54db31c2e0
adam <adam@pkgsrc.org>
2024-05-07 17:54:10 +0000

    nodejs: updated to 22.1.0
    Node.js v22.1.0
    
    module: implement NODE_COMPILE_CACHE for automatic on-disk code caching
    buffer: improve base64 and base64url performance
    (SEMVER-MINOR) dns: add order option and support ipv6first
    events,doc: mark CustomEvent as stable
    (SEMVER-MINOR) lib, url: add a windows option to path parsing
    (SEMVER-MINOR) net: add CLI option for autoSelectFamilyAttemptTimeout
    (SEMVER-MINOR) src: add string_view overload to snapshot FromBlob
    src,permission: throw async errors on async APIs
    (SEMVER-MINOR) test_runner: add --test-skip-pattern cli option
    (SEMVER-MINOR) url: implement parse method for safer URL parsing

--- lang/nodejs/patches/patch-tools_install.py
+++ lang/nodejs/patches/patch-tools_install.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-tools_install.py,v 1.9 2024/03/07 17:07:43 adam Exp $
+$NetBSD: patch-tools_install.py,v 1.10 2024/05/07 17:54:10 adam Exp $
 
 Install man pages under the right directory.
  
---- tools/install.py.orig	2024-03-06 15:49:47.000000000 +0000
+--- tools/install.py.orig	2024-05-02 12:49:16.000000000 +0000
 +++ tools/install.py
 @@ -182,10 +182,7 @@ def files(options, action):
    action(options, [os.path.join(options.v8_dir, 'tools/gdbinit')], 'share/doc/node/')
    action(options, [os.path.join(options.v8_dir, 'tools/lldb_commands.py')], 'share/doc/node/')
  
--  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
+-  if 'openbsd' in sys.platform:
 -    action(options, ['doc/node.1'], 'man/man1/')
 -  else:
 -    action(options, ['doc/node.1'], 'share/man/man1/')
