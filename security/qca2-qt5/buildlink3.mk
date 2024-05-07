e9bee16dabbc8964c2890e780ed02b4184e6da3e
adam <adam@pkgsrc.org>
2024-05-07 04:11:45 +0000

    qca2-qt5*: updated to 2.3.8
    2.3.8
    Bug fixes

--- security/qca2-qt5/buildlink3.mk
+++ security/qca2-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2024/04/06 08:06:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2024/05/07 04:11:45 adam Exp $
 
 BUILDLINK_TREE+=	qca2-qt5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qca2-qt5
 QCA2_QT5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qca2-qt5+=	qca2-qt5>=2.1.3
-BUILDLINK_ABI_DEPENDS.qca2-qt5?=	qca2-qt5>=2.3.5nb8
+BUILDLINK_ABI_DEPENDS.qca2-qt5+=	qca2-qt5>=2.3.5nb8
 BUILDLINK_PKGSRCDIR.qca2-qt5?=		../../security/qca2-qt5
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
