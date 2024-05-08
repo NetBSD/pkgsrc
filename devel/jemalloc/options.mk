7c8764d5849a36afd4a503375e677b3008021661
cheusov <cheusov@pkgsrc.org>
2024-05-08 18:16:39 +0000

    Add option "doc" for disabling heavy dependency on docbook-xsl and libxslt.   Approved by gson@

--- devel/jemalloc/options.mk
+++ devel/jemalloc/options.mk
@@ -1,10 +1,21 @@
-# $NetBSD: options.mk,v 1.1 2019/03/17 13:28:22 rin Exp $
+# $NetBSD: options.mk,v 1.2 2024/05/08 18:16:39 cheusov Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.jemalloc
-PKG_SUPPORTED_OPTIONS=		debug
+PKG_SUPPORTED_OPTIONS=		debug doc
+PKG_SUGGESTED_OPTIONS=		doc
+
+PLIST_VARS+=			doc
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Mdoc)
+TOOL_DEPENDS+=		docbook-xsl-[0-9]*:../../textproc/docbook-xsl
+TOOL_DEPENDS+=		libxslt-[0-9]*:../../textproc/libxslt
+PLIST.doc=		yes
+.else
+CONFIGURE_ARGS+=	--disable-doc
 .endif
