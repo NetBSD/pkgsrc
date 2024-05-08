3c521617ba566548f5acc1e8fc0059ac1e81f60d
adam <adam@pkgsrc.org>
2024-05-08 15:44:20 +0000

    glib2 glib2-tools gdbus-codegen: updated to 2.80.1
    Overview of changes in GLib 2.80.1, 2024-05-07
    ==============================================
    
    * Fix CVE-2024-34397: GDBus signal subscriptions for well-known names are
      vulnerable to unicast spoofing
    
    * Bugs fixed:
      - GNOME/tracker-miners-315 3.7.0 - GLib-GIO-WARNING **: 09:27:12.186: Error
        creating IO channel for /proc/self/mountinfo: Invalid argument (g-io-error-
        quark, 13) (Ondrej Holy)
      - CVE-2024-34397: GDBus signal subscriptions for well-known names are
        vulnerable to unicast spoofing (Simon McVittie)
      - Devhelp does not show indexes for GLib, GIO, or GObject (Philip
        Withnall)
      - readlink -f fails in CI on macOS (Simon McVittie)
      - Crash in gdbus schedule_callbacks() due to missing NULL check before
        g_str_equal() (Philip Withnall)
      - [th/performance] add script for combining performance results
      - [th/gobject-toggle-refs-check] Fix critical warning for toggle
        notifications in g_object_ref()/g_object_unref()
      - meson: Fix a needless recompilation of some gdbus tests
      - girparser: Don't assume sizeof(size_t) == sizeof(void *)
      - girparser: Allow time_t, off_t, etc. to appear in GIR XML
      - Ported the first few documentation comments in `gio/gaction.c` to gi-
        docgen
      - girparser: Make sizes in integer_aliases more obviously correct
      - girparser: Adjust signedness() macro
      - glib/gvariant: fix compile error with GCC 14.0.1
      - tests: Mark several additional tests as can_fail on GNU Hurd
      - Backport !3979 "Use CPU_COUNT to get the number of set CPUs" to
        glib-2-80
      - Backport !3989 “completion: make gsettings work in nounset mode” to
        glib-2-80
      - Backport "Remove unused cmph files" for glib-2-80
      - Backport !4019 “gunixmounts: Use fallback if libmount monitoring
        fails” to glib-2-80
      - Backport !4005 “Fix various bugs found by scan-build“ to glib-2-80
      - Backport !4028 “gdbusconnection: Fix a crash on arg0 matching” to
        glib-2-80
      - Backport !4031 “gdocumentportal: Handle EROFS and similar errors more
        gracefully” to glib-2-80
      - Backport !4038 “gdbusconnection: Don't deliver signals if the sender
        doesn't match” to glib-2-80
    
    * Translation updates:
      - Czech (Daniel Rusek)
      - Danish (Ask Hjorth Larsen)
      - French (Vincent Chatelain)
      - Galician (Fran Dieguez)
      - Georgian (Ekaterine Papava)
      - German (Christian Kirbach)
      - Indonesian (Andika Triwidada)
      - Italian (Milo Casagrande)
      - Kabyle (Athmane MOKRAOUI, Rachida SACI)
      - Korean (Changwoo Ryu)
      - Latvian (Rūdolfs Mazurs)
      - Persian (Danial Behzadi)
      - Polish (Piotr Drąg)
      - Portuguese (Hugo Carvalho)
      - Portuguese (Brazil) (Leônidas Araújo, Juliano de Souza Camargo)

--- devel/glib2/patches/patch-glib_gthread.c
+++ devel/glib2/patches/patch-glib_gthread.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-glib_gthread.c,v 1.1 2024/04/09 16:57:18 wiz Exp $
+$NetBSD: patch-glib_gthread.c,v 1.2 2024/05/08 15:44:20 adam Exp $
 
 Fix build on NetBSD.
 
---- glib/gthread.c.orig	2024-04-09 16:24:39.087896412 +0000
+--- glib/gthread.c.orig	2024-05-07 14:50:30.000000000 +0000
 +++ glib/gthread.c
 @@ -1090,7 +1090,7 @@ g_get_num_processors (void)
  
@@ -11,5 +11,5 @@ Fix build on NetBSD.
 -#elif defined(_SC_NPROCESSORS_ONLN) && defined(THREADS_POSIX) && defined(HAVE_PTHREAD_GETAFFINITY_NP)
 +#elif defined(_SC_NPROCESSORS_ONLN) && defined(THREADS_POSIX) && defined(HAVE_PTHREAD_GETAFFINITY_NP) && defined(CPU_ZERO)
    {
-     int idx;
      int ncores = MIN (sysconf (_SC_NPROCESSORS_ONLN), CPU_SETSIZE);
+     cpu_set_t cpu_mask;
