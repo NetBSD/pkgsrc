500ffdf10da8241c1c44b5519d114c10a43248b3
adam <adam@pkgsrc.org>
2024-05-10 09:18:20 +0000

    cmake: updated to 3.29.3
    CMake 3.29.3
    * Tests/CXXModules: test exporting modules which include headers
    * cmGeneratorTarget: add the original target as a COMPILE_ONLY link
    * cmGeneratorTarget: add method to get a filesystem-safe export name
    * cxxmodules: use filesystem-safe export names in filenames
    * cmGeneratorTarget: add a target-level query for "needs dyndep"
    * VS: set ScanSourceForModuleDependencies at vcxproj level
    * cmDyndepCollation: look at all `export(TARGETS)` calls
    * Tests/RunCMake/CMakePackage: Define variable closer to where it is used
    * FindBoost: Add support for Boost 1.85
    * cxxmodules: Do not scan sources while testing the compiler
    * Help: Fix CMakePackageConfigHelpers typos, grammar and formatting
    * Help: Add missing section heading for apple architecture selection
    * generate_apple_architecture_selection_file: Avoid early returns
    * generate_apple_*_selection_file: Save/restore PACKAGE_PREFIX_DIR
    * Help/OBJC_STANDARD: Document Objective C17 and C23 support
    * FindHDF5: Fix detection of versions with more than three components

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.57 2024/04/13 15:44:17 adam Exp $
+# $NetBSD: version.mk,v 1.58 2024/05/10 09:18:20 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.29.2
+CMAKE_VERSION=	3.29.3
 CMAKE_API=	${CMAKE_VERSION:R}
