66384d358289a30685b703053bae7400f2cfe9ad
jperkin <jperkin@pkgsrc.org>
2024-05-10 08:23:15 +0000

    mozilla: Match rust triple on illumos.

--- www/firefox115/mozilla-common.mk
+++ www/firefox115/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.7 2024/04/11 22:21:39 gutteridge Exp $
+# $NetBSD: mozilla-common.mk,v 1.8 2024/05/10 08:23:15 jperkin Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -47,6 +47,9 @@ CFLAGS+=		-msse2
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-i386)
 CONFIGURE_ARGS+=	--target=i586-unknown-netbsd
 CONFIGURE_ARGS+=	--host=i586-unknown-netbsd
+.elif ${MACHINE_PLATFORM:MSunOS-*-x86_64}
+CONFIGURE_ARGS+=	--host=x86_64-unknown-illumos
+CONFIGURE_ARGS+=	--target=x86_64-unknown-illumos
 .else
 CONFIGURE_ARGS+=	--target=${MACHINE_GNU_PLATFORM}
 CONFIGURE_ARGS+=	--host=${MACHINE_GNU_PLATFORM}
