15212d6ddbb0a56353d96911f1293102c107cffa
pho <pho@pkgsrc.org>
2024-05-10 07:31:46 +0000

    sysutils/open-vm-tools: kern/58195: Detach NetBSD vmt(4) before starting vmtoolsd
    On NetBSD/aarch64 this currently only works on -CURRENT, but doing this on
    older kernels is at least harmless because drvctl requests are simply
    rejected. On any other OSes this should change nothing.

--- sysutils/open-vm-tools/files/vmtools.sh
+++ sysutils/open-vm-tools/files/vmtools.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: vmtools.sh,v 1.3 2023/02/17 21:58:09 khorben Exp $
+# $NetBSD: vmtools.sh,v 1.4 2024/05/10 07:31:46 pho Exp $
 #
 
 # PROVIDE: vmtools
@@ -13,6 +13,21 @@ rcvar="vmtools"
 pidfile="/var/run/vmware-guestd.pid"
 command="@PREFIX@/bin/vmtoolsd"
 command_args="--background ${pidfile}"
+
+start_precmd="disable_vmt"
+disable_vmt()
+{
+	# Attempt to detach NetBSD vmt(4) driver so that it won't conflict
+	# with vmtoolsd.
+	drvctl -d vmt0 >/dev/null 2>&1 || true
+}
+
+stop_precmd="enable_vmt"
+enable_vmt()
+{
+	# Attempt to re-attach NetBSD vmt(4) driver.
+	drvctl -r -a cpufeaturebus cpu0 >/dev/null 2>&1 || true
+}
 
 load_rc_config $name
 run_rc_command "$1"
