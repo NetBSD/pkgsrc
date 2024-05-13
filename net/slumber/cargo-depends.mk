619dbf9a7ed401bfdb9bb00fdec2dd4fdcb06c85
pin <pin@pkgsrc.org>
2024-05-13 06:56:46 +0000

    net/slumber: update to 1.2.1
    [1.2.1] - 2024-05-11
    Fixed
     - Fix profile not being selected on initial startup

--- net/slumber/cargo-depends.mk
+++ net/slumber/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.14 2024/05/12 08:39:09 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.15 2024/05/13 06:56:46 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.21.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
