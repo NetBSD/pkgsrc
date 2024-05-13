8b0c726502241b51caea256668777de99f63bbed
adam <adam@pkgsrc.org>
2024-05-13 09:05:54 +0000

    gstreamer1: updated to 1.24.3
    1.24.3
    
    This release only contains bugfixes and it should be safe to update from 1.24.x.
    
    Highlighted bugfixes in 1.24.3
    
    EXIF image tag parsing security fixes
    Subtitle handling improvements in parsebin
    Fix issues with HLS streams that contain VTT subtitles
    Qt6 QML sink re-render and re-sizing fixes
    unixfd ipc plugin timestamp and segment handling fixes
    vah264enc, vah265enc: Do not touch the PTS of the output frame
    vah264dec and vapostproc fixes and improvements
    v4l2: multiple fixes and improvements, incl. for mediatek JPEG decoder and v4l2 loopback
    v4l2: fix hang after seek with some v4l2 decoders
    Wayland sink fixes
    ximagesink: fix regression on RPi/aarch64
    fmp4mux, mp4mux gained FLAC audio support
    D3D11, D3D12: reliablity improvements and memory leak fixes
    Media Foundation device provider fixes
    GTK4 paintable sink improvements including support for directly importing dmabufs with GTK 4.14
    WebRTC sink/source fixes and improvements
    AWS s3sink, s3src, s3hlssink now support path-style addressing
    MPEG-TS demuxer fixes
    Python bindings fixes
    various bug fixes, memory leak fixes, and other stability and reliability improvements
    
    
    
    1.24.2
    
    This release only contains bugfixes and it should be safe to update from 1.24.x.
    
    Highlighted bugfixes in 1.24.2
    
    H.264 parsing regression fixes
    WavPack typefinding improvements
    Video4linux fixes and improvements
    Android build and runtime fixes
    macOS OpenGL memory leak and robustness fixes
    Qt/QML video sink fixes
    Package new analytics and mse libraries in binary packages
    Windows MSVC binary packages: fix libvpx avx/avx2/avx512 instruction set detection
    various bug fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.12 2024/04/05 17:26:43 wiz Exp $
+# $NetBSD: version.mk,v 1.13 2024/05/13 09:05:54 adam Exp $
 
-GST_VERSION=	1.24.1
+GST_VERSION=	1.24.3
 # if ${PATCH} is < 10
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 #else
