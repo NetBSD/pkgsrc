19b3b8f4e073e13e5aa710cef2e0cd9e510bc734
wiz <wiz@pkgsrc.org>
2024-05-14 09:30:38 +0000

    vte3: update to 0.76.1.
    Improve HOMEPAGE.
    
    No changelog provided.

--- x11/vte3/hacks.mk
+++ x11/vte3/hacks.mk
@@ -1,8 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1 2022/09/24 07:46:31 nia Exp $
+# $NetBSD: hacks.mk,v 1.2 2024/05/14 09:30:38 wiz Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if !empty(MACHINE_PLATFORM:MNetBSD-*-aarch64*)
+.if ${MACHINE_PLATFORM:MNetBSD-*-aarch64*}
 # pty.cc:(.text+0x18): undefined reference to `__aarch64_ldadd4_acq_rel'
 # https://bugzilla.redhat.com/show_bug.cgi?id=1830472
 PKG_HACKS+=		no-outline-atomics
