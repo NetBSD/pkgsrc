f301768bd4c259926f7a74fc79426ff665a4dc54
bacon <bacon@pkgsrc.org>
2024-05-14 21:00:27 +0000

    security/munge: Update to 0.5.16
    Major package improvements:
    
    - Install init scripts for Linux, macOS, and NetBSD
    - Create runtime directories with proper ownership and permissions
    - Support use under non-priveleged pkgsrc trees
    - Run daemon as munge rather than root, as recommended by upstream
    - Catch up from several releases behind
    
    Changes: https://github.com/dun/munge/releases

--- security/munge/files/munge.sh
+++ security/munge/files/munge.sh
@@ -1,5 +1,5 @@
 #!@RCD_SCRIPTS_SHELL@
-# $NetBSD: munge.sh,v 1.1 2014/09/11 16:11:46 asau Exp $
+# $NetBSD: munge.sh,v 1.2 2024/05/14 21:00:27 bacon Exp $
 #
 # PROVIDE: munge
 # REQUIRE: DAEMON
@@ -10,6 +10,8 @@
 name="munge"
 rcvar=${name}
 command="@PREFIX@/sbin/munged"
+munge_user="munge"
+munge_group="munge"
 
 load_rc_config $name
 run_rc_command "$1"
