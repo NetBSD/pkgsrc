5cdb26247a8acafd7c5318b39c78fea762af1df6
nia <nia@pkgsrc.org>
2024-05-15 08:13:25 +0000

    gnutls: Make pkcs11 optional.
    pkcs11 and smart card support is rather unrelated to gnutls's core
    goal of providing TLS support to applications, and may be unsuitable
    e.g. on embedded devices

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2024/04/10 10:13:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2024/05/15 08:13:25 nia Exp $
 
 BUILDLINK_TREE+=	gnutls
 
@@ -14,7 +14,9 @@ BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 .include "../../security/libtasn1/buildlink3.mk"
 BUILDLINK_API_DEPENDS.nettle+=		nettle>=3.4.1
 .include "../../security/nettle/buildlink3.mk"
+.if ${PKG_BUILD_OPTIONS.gnutls:Mpkcs11}
 .include "../../security/p11-kit/buildlink3.mk"
+.endif
 .include "../../textproc/libunistring/buildlink3.mk"
 pkgbase := gnutls
 .include "../../mk/pkg-build-options.mk"
