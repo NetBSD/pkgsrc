8266b21194056daaca0801f9f3ad1a77a1babc84
wiz <wiz@pkgsrc.org>
2024-05-15 21:15:26 +0000

    *: recursive bump for grpc 1.63

--- net/qt6-qtgrpc/buildlink3.mk
+++ net/qt6-qtgrpc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/04/06 08:06:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/05/15 21:15:26 wiz Exp $
 
 BUILDLINK_TREE+=	qt6-qtgrpc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt6-qtgrpc
 QT6_QTGRPC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt6-qtgrpc+=	qt6-qtgrpc>=6.5.0
-BUILDLINK_ABI_DEPENDS.qt6-qtgrpc+=	qt6-qtgrpc>=6.6.3nb2
+BUILDLINK_ABI_DEPENDS.qt6-qtgrpc+=	qt6-qtgrpc>=6.7.0nb1
 BUILDLINK_PKGSRCDIR.qt6-qtgrpc?=	../../net/qt6-qtgrpc
 
 BUILDLINK_FILES.qt6-qtgrpc+=	libexec/qtgrpcgen
