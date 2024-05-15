daa588f9ad0c30c27334cba160e555e5ace4b200
nia <nia@pkgsrc.org>
2024-05-15 08:13:25 +0000

    gnutls: Make pkcs11 optional.
    pkcs11 and smart card support is rather unrelated to gnutls's core
    goal of providing TLS support to applications, and may be unsuitable
    e.g. on embedded devices

--- security/gnutls/options.mk
+++ security/gnutls/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.6 2023/02/15 07:40:52 nikita Exp $
+# $NetBSD: options.mk,v 1.7 2024/05/15 08:13:25 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnutls
-PKG_SUPPORTED_OPTIONS=	dane
+PKG_SUPPORTED_OPTIONS=	dane pkcs11
+PKG_SUGGESTED_OPTIONS=	pkcs11
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +12,13 @@ CONFIGURE_ARGS+=	--enable-libdane
 PLIST_SRC+=		PLIST.dane
 .else
 CONFIGURE_ARGS+=	--disable-libdane
+.endif
+
+PLIST_VARS+=	pkcs11
+.if !empty(PKG_OPTIONS:Mpkcs11)
+PLIST.pkcs11=	yes
+BUILDLINK_API_DEPENDS.p11-kit+=	p11-kit>=0.23.1
+.include "../../security/p11-kit/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-p11-kit
 .endif
