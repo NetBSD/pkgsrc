76ccc38235d8cacc7757bc97c60d098c06d8e9a0
wiz <wiz@pkgsrc.org>
2024-05-16 13:38:42 +0000

    fifengine: fix build with boost 1.85
    From an upstream problem report - it builds, but if you use it,
    please verify it works.

---
+++ devel/fifengine/patches/patch-engine_core_loaders_native_map_objectloader.cpp
@@ -0,0 +1,18 @@
+$NetBSD: patch-engine_core_loaders_native_map_objectloader.cpp,v 1.1 2024/05/16 13:38:42 wiz Exp $
+
+boost 1.85 support.
+https://github.com/fifengine/fifengine/issues/1085#issuecomment-2115210942
+
+--- engine/core/loaders/native/map/objectloader.cpp.orig	2019-01-11 17:24:38.000000000 +0000
++++ engine/core/loaders/native/map/objectloader.cpp
+@@ -702,7 +702,9 @@ namespace FIFE {
+ 			for (iter = files.begin(); iter != files.end(); ++iter) {
+ 				// TODO - vtchill - may need a way to allow clients to load things other
+ 				// than .xml and .zip files
+-				std::string ext = bfs::extension(*iter);
++                                    bfs::path p = *iter;
++                                    std::string ext = p.extension().string();
++				
+ 				if (ext == ".xml" || ext == ".zip") {
+ 					loadImportFile(*iter, importDirectoryString);
+ 				}
