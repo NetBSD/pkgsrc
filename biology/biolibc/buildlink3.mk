43ddba0c59004e99397632b5e34a8e647b6adcce
bacon <bacon@pkgsrc.org>
2024-05-19 12:54:59 +0000

    biology/biolibc: Update to 0.2.6.4
    bl_sam_copy(): Make sure all fields are properly initialized

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2023/10/28 20:31:40 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2024/05/19 12:54:59 bacon Exp $
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.6
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.6.4
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
