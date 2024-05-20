ac9b07afde4de9209c7938432ee3deffd175ee7d
wiz <wiz@pkgsrc.org>
2024-05-20 10:38:37 +0000

    harfbuzz*: update to 8.5.0
    pkgsrc: Add lots of PRINT_PLIST_AWK to simplify updates. Put coretext
    file(s) into their own PLIST.
    
    Overview of changes leading to 8.5.0
    Monday, May 13, 2024
    ====================================
    - API for partial instancing is now stable and have been promoted out of
      experimental APIs.
    - Support instancing “BASE” table.
    - Speedup AAT shaping by 13–30%.
    - Various build fixes.
    - Various subsetter and instancer fixes.
    
    - New API
    +HB_SUBSET_FLAGS_OPTIMIZE_IUP_DELTAS
    +hb_subset_input_get_axis_range()
    +hb_subset_input_pin_axis_location()

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.16 2024/04/13 00:48:57 riastradh Exp $
+# $NetBSD: options.mk,v 1.17 2024/05/20 10:38:37 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
 PKG_SUPPORTED_OPTIONS+=	doc introspection
@@ -10,6 +10,7 @@ PKG_SUGGESTED_OPTIONS+=	${${USE_CROSS_COMPILE:tl} == "yes":?:introspection}
 
 PLIST_VARS+=		doc introspection
 
+PRINT_PLIST_AWK+=	{ if (/^share\/gtk-doc/) $$0 = "$${PLIST.doc}" $$0 }
 .if !empty(PKG_OPTIONS:Mdoc)
 MESON_ARGS+=	-Ddocs=enabled
 TOOL_DEPENDS+=	gtk-doc>=1.32nb9:../../textproc/gtk-doc
@@ -18,6 +19,16 @@ PLIST.doc=	yes
 MESON_ARGS+=	-Ddocs=disabled
 .endif
 
+PRINT_PLIST_AWK+=	{ if (/^bin\/hb-info/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^bin\/hb-ot-shape-closure/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^bin\/hb-shape/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^bin\/hb-subset/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^include\/harfbuzz\/hb-glib.h/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^include\/harfbuzz\/hb-gobject/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^lib\/girepository/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^lib\/libharfbuzz-gobject/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^lib\/pkgconfig\/harfbuzz-gobject.pc/) $$0 = "$${PLIST.introspection}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^share\/gir-1.0/) $$0 = "$${PLIST.introspection}" $$0 }
 .if !empty(PKG_OPTIONS:Mintrospection)
 MESON_ARGS+=	-Dintrospection=enabled
 .include "../../devel/glib2/buildlink3.mk"
