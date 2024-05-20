36f0bc0b056fa3ff867aa4860e889a91c2ef4591
adam <adam@pkgsrc.org>
2024-05-20 19:38:24 +0000

    qt6: updated to 6.7.1
    6.7.1
    Bug fix release

--- graphics/qt6-qt3d/buildlink3.mk
+++ graphics/qt6-qt3d/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/04/06 08:05:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/05/20 19:38:24 adam Exp $
 
 BUILDLINK_TREE+=	qt6-qt3d
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.qt6-qt3d+=	qt6-qt3d>=6.6.3nb1
 BUILDLINK_PKGSRCDIR.qt6-qt3d?=		../../graphics/qt6-qt3d
 
 .include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../multimedia/qt6-qtmultimedia/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# QT6_QT3D_BUILDLINK3_MK
 
