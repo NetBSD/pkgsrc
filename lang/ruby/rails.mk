b32fe17653171065b48ff0658de334cc9bdf1f4b
taca <taca@pkgsrc.org>
2024-05-22 13:23:00 +0000

    www/ruby-rails70: update to 7.0.8.2
    Fix CVE-2024-34341 of textproc/ruby-actiontext.
    
    
    7.0.8.2 (2024-05-16)
    
    Active Support
    
    * No changes.
    
    Active Model
    
    * No changes.
    
    Active Record
    
    * No changes.
    
    Action View
    
    * No changes.
    
    Action Pack
    
    * No changes.
    
    Active Job
    
    * No changes.
    
    Action Mailer
    
    * No changes.
    
    Action Cable
    
    * No changes.
    
    Active Storage
    
    * No changes.
    
    Action Mailbox
    
    * No changes.
    
    Action Text
    
    * Upgrade Trix to 1.3.2 to fix CVE-2024-34341.
    
      Rafael Mendonça França
    
    Railties
    
    * No changes.

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.159 2024/02/24 14:55:25 taca Exp $
+# $NetBSD: rails.mk,v 1.160 2024/05/22 13:23:00 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -48,7 +48,7 @@ _RUBY_RAILS_MK=	# defined
 # current Ruby on Rails versions.
 #
 RUBY_RAILS61_VERSION?=	6.1.7.7
-RUBY_RAILS70_VERSION?=	7.0.8.1
+RUBY_RAILS70_VERSION?=	7.0.8.2
 RUBY_RAILS71_VERSION?=	7.1.3.2
 
 RUBY_RAILS_ACCEPTED?=	# empty
