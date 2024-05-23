b3367519528f0360b5e527051bcbc415c1fa431a
gdt <gdt@pkgsrc.org>
2024-05-23 13:35:58 +0000

    geography/proj: Update to 8.2.1
    This version withdraws proj_api.h.
    
    Upstream NEWS, less normal updates (e.g. EPSG database) and other
    housekeeping, bugfixes, minor improvements follows.
    
    8.2.1 Release Notes
    -------------------
    
    [pruned items only]
    
    8.2.0 Release Notes
    -------------------
    
     Announcements
     -------------
    
     From PROJ 9.0.0 and onwards CMake will be the only build system bundled
     with the PROJ package. As a consequence support for Autotools builds will
     stop when the 8.2 branch of PROJ reaches end of life. We encourage
     everyone to adjust their build workflows as soon as possible and report
     any discrepancies discovered between Autotools and CMake builds.
    
     Updates
     -------
    
     o Database layout modified to include "anchor" field to geodetic_datum and
       vertical_datum tables, consequently database layout version is increased
       to 1.2 (#2859)
    
     o Add IAU_2015 CRS definitions (#2876)
    
     o Added proj_trans_bounds() (#2882)
    
    
    8.1.1 Release Notes
    -------------------
    
    [pruned items only]
    
    
    8.1.0 Release Notes
    -------------------
    
     Updates
     -------
    
     + Database
    
       o Added a PROJ.VERSION metadata entry (#2646)
    
    
     + API additions
    
       o Added proj_get_geoid_models_from_database() function that returns a list of
         geoid models available for a given CRS (#2681)
    
       o Added proj_get_celestial_body_list_from_database that returns a list
         of celestial bodies in the PROJ database (#2667)
    
       o Added proj_get_celestial_body_name() (#2662)
    
     + Various improvements
    
       o projsync: make it filter out files not intended for the current version (#2725)
    
       o Added projinfo option --list-crs (supports --area) (#2663)
    
       o Added capability to get SQL statements to add custom CRS in the database (#2577)
    
    
    8.0.1 Release Notes
    -------------------
    
    [pruned items only]
    
    
    8.0.0 Release Notes
    -------------------
    
    With the release of PROJ 8 the proj_api.h API is finally removed. See
    https://proj.org/development/migration.html for more info on how to migrate
    from the old to the proj.h API.
    
    With the removal of proj_api.h it has been possible to simplify error codes
    and messages given by the software. The error codes are exposed in the API.
    
    Several improvements has been made to the command line utilities as well as
    tweaks in the underlying API.
    
     Updates
     -------
    
     o Public header file proj_api.h removed (#837)
    
     o Added geocentric->topocentric conversion (+proj=topocentric) (#2444)
    
     o Added --authority switch in cs2cs to control where coordinate operations
       are looked for. C API function proj_create_crs_to_crs_from_pj() updated
       accordingly (#2477)
    
     o Added proj_crs_is_derived() function to C API (#2496)

--- geography/proj/buildlink3.mk
+++ geography/proj/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2024/05/16 06:14:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2024/05/23 13:35:58 gdt Exp $
 
 BUILDLINK_TREE+=	proj
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	proj
 PROJ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.proj+=	proj>=5.0.0
-BUILDLINK_ABI_DEPENDS.proj+=	proj>=7.2.1nb7
-BUILDLINK_PKGSRCDIR.proj?=	../../geography/proj
+BUILDLINK_ABI_DEPENDS.proj+=	proj>=8.0.0
+BUILDLINK_PKGSRCDIR.proj?=	../../wip/proj
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
