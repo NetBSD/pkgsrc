48f3067ba00af358106b451f66ee181f35c93586
gdt <gdt@pkgsrc.org>
2024-05-23 16:24:37 +0000

    geography/libgeotiff: Fix stray nb1 in ABI_DEPENDS

--- geography/libgeotiff/buildlink3.mk
+++ geography/libgeotiff/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2024/05/23 14:18:00 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2024/05/23 16:24:37 gdt Exp $
 
 BUILDLINK_TREE+=	libgeotiff
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgeotiff
 LIBGEOTIFF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgeotiff+=	libgeotiff>=1.5.1
-BUILDLINK_ABI_DEPENDS.libgeotiff?=	libgeotiff>=1.7.2nb1
+BUILDLINK_ABI_DEPENDS.libgeotiff?=	libgeotiff>=1.7.2
 BUILDLINK_PKGSRCDIR.libgeotiff?=	../../geography/libgeotiff
 
 .include "../../geography/proj/buildlink3.mk"
