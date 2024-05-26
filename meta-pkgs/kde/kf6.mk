71abd8edc8b9f28da52a526c90db02f4ab00081e
markd <markd@pkgsrc.org>
2024-05-26 11:02:12 +0000

    kde: update kf6 version to 6.2.0. Default PKGNAME has kf6- prefix.

--- meta-pkgs/kde/kf6.mk
+++ meta-pkgs/kde/kf6.mk
@@ -1,10 +1,11 @@
-# $NetBSD: kf6.mk,v 1.1 2024/03/19 11:21:15 markd Exp $
+# $NetBSD: kf6.mk,v 1.2 2024/05/26 11:02:12 markd Exp $
 # used by x11/kf6-kwindowsystem/Makefile
 
-KF6VER=		6.0.0
+KF6VER=		6.2.0
 CATEGORIES+=	kde
 MASTER_SITES=	${MASTER_SITE_KDE:=frameworks/${KF6VER:R}/}
 EXTRACT_SUFX=	.tar.xz
+PKGNAME?=	kf6-${DISTNAME}
 
 TOOL_DEPENDS+= extra-cmake-modules>=${KF6VER}:../../devel/extra-cmake-modules
 TOOLS_DEPENDS.cmake= cmake>=3.0:../../devel/cmake
