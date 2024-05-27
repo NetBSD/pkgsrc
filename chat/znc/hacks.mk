03fd7696868b178d6e336dd25d2c8a6d5ba6d77e
nia <nia@pkgsrc.org>
2024-05-27 08:56:30 +0000

    *: Sprinkle some workarounds for libstdc++ in NetBSD 9.x.
    It needs _NETBSD_SOURCE to make some ISO C++ functions visible.

---
+++ chat/znc/hacks.mk
@@ -0,0 +1,15 @@
+# $NetBSD: hacks.mk,v 1.1 2024/05/27 08:56:30 nia Exp $
+
+.if !defined(ZNC_HACKS_MK)
+ZNC_HACKS_MK=		# defined
+
+### [Mon May 27 10:49:39 CEST 2024 : nia]
+### NetBSD 9.x does not expose all standard C++ functions with default
+### source visibility.
+###
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 100000
+PKG_HACKS+=		netbsd-source
+CXXFLAGS+=		-D_NETBSD_SOURCE
+.endif
+
+.endif
