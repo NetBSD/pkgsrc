850ade87ba5686e94a9ded705fbe84ea7f797a92
vins <vins@pkgsrc.org>
2024-05-29 17:28:29 +0000

    devel/got: update to got 0.99
    # local changes
    * Fix build following devel/ncursesw removal
    * Package now requires LibreTLS
    * Add required tools and dependencies for tests
    * Regression tests are interactive
    
    # upstream changes (since version  0.97)
    
    * got 0.99; 2024-05-05
      see git repository history for per-change authorship information
    - make 'got fetch' work with URLs which refer to $HOME via a tilde: ~user
    - replace strftime %G-%m-%d with %F to prevent 2024-12-30 -> 2025-12-30
    - fix spurious errors from got-fetch-http when server has no more data to send
    - prevent gotd notification process from exiting due to EPIPE
    - fix I/O hangs with TLS in got-notify-http
    - document http and https protocol support in got.conf(5), too
    - fix an fd leak in gotd's notify process causing endless CPU spin
    - back out got stage -R option addition; deemed too inconvenient in practice
    - fix got-fetch-http GET request URL; add leading slash and avoid double slashes
    - allow custom GOT_TEST_HTTP_PORT when running regression tests
    - gotwebd: add magic ".git" handling; try foo.git if repository foo is not found
    - expose authenticated gotd user account in HTTP notifications
    - gotd.conf(5) HTTP/JSON documentation fixes
    - fix endless loop upon Ctrl-D (EOF) input during got stage/unstage/revert -p
    - make gotd notifications work when 'git push' is used instead of 'got send'
    - make got stage -p behave the same way in interactive and -F modes for 'q'
    - fix lingering gotd processes from clients closing connections early
    - regress: prevent spurious failure of gotd test_clone_basic_access_denied
    - fix an issue where 'git fetch' would error or hang against gotd
    - use polling read in got_pkt_readn() to avoid endless hangs in gotsh
    
    * got 0.98; 2024-04-23
      see git repository history for per-change authorship information
    - speed up got tag -l by caching timestamps in got_ref_cmp_tags()
    - provide a macro for vi(1) path for use by -portable at compile time
    - avoid a rename/stat race when gotd installs a new pack and then uses it
    - make 'got ref -l' output consistent when packed references exist
    - make 'got ref -l' work consistently when a reference argument is given
    - add initial support for notifications to gotd(8), via email and http/json
    - display process title in syslog when a gotd child process exits
    - hide a pointless end-of-file error on imsg pipe in libexec helpers
    - plug a memory leak in 'got blame'
    - add support for topological sorting to the commit graph
    - add log -t option which enables topological sorting of commits
    - make 'got rebase' find a merge base with topological sorting if needed
    - call unveil(2) earlier during import, commit, histedit, and tag commands
    - make 'got status' display interrupted rebase, histedit, and merge operations
    - got.1: escape Eq since it's a GNU roff macro, to fix rendering in -portable
    - regress: use seq instead of jot, for portability reasons
    - get rid of unnecessary "dns inet" pledge promises while fetching via git://
    - add http clone/fetch support using a new got-fetch-http helper
    - drop git+ssh protocol name from documentation; Git has done the same
    - require -R option for staging or unstaging directory contents
    - got patch: fix applying on empty files

--- devel/got/patches/patch-compat_sha2.h
+++ devel/got/patches/patch-compat_sha2.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-compat_sha2.h,v 1.1 2024/02/11 14:47:07 vins Exp $
+$NetBSD: patch-compat_sha2.h,v 1.2 2024/05/29 17:28:29 vins Exp $
 
 attribute __bounded__ is only defined in OpenBSD's GCC.
 
---- compat/sha2.h.orig	2023-03-13 14:37:38.000000000 +0000
+--- compat/sha2.h.orig	2024-05-29 15:52:53.668746638 +0000
 +++ compat/sha2.h
 @@ -39,6 +39,10 @@
  #ifndef _SSHSHA2_H
  #define _SSHSHA2_H
-
+ 
 +#if !defined(HAVE_ATTRIBUTE__BOUNDED__) && !defined(__OpenBSD__)
 +#define __bounded__(x, y, z)
 +#endif
 +
- #if !defined(HAVE_SHA256UPDATE) || !defined(HAVE_SHA384UPDATE) || \
-     !defined(HAVE_SHA512UPDATE)
+ #if !defined(HAVE_SHA256UPDATE)
  
+ /*** SHA-256/384/512 Various Length Definitions ***********************/
