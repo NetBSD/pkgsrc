72d23350503787d8f543943a6552e095ff48115c
vins <vins@pkgsrc.org>
2024-05-31 10:59:24 +0000

    wm/fvwm3: update to 1.1.0
    # pkgsrc changes
    
    * Introduce a 'golang' package option to address optional build of fvwm
      modules written in Go (namely FvwmPrompt), and handle installation such
      modules locally, by mimicking the functionality of
      lang/go/go-module.mk
    
    # upstream changes
    
    Breaking Changes
    
    * build: fix Xft/Freetype/Fontconfig check by @ThomasAdam in #956
    * Goto{Desk,Page,DeskAndPage}: use 'screen' keyword by @ThomasAdam in #967
    * FvwmPager: present overview of all monitors if "Monitor:" is not used by @ThomasAdam in #978
    * Remove single screen support, -s. by @somiaj in #977
    
    Enhancements
    
    * Add CurrentAtEnd to Alt-Tab WindowList by @somiaj in #938
    * Add DesktopNames to xdg desktop file. by @somiaj in #939
    * randr: allow geometry strings to include @n by @ThomasAdam in #940
    
    Other Changes
    
    * Don't use google fonts when building html doc. by @somiaj in #932
    * Add index page for html manpages. by @somiaj in #934
    * randr: fix primary flag check by @ThomasAdam in #936
    * monitor: fix geometry parsing for window assignment by @ThomasAdam in #889
    * GotoPage: inject current screen if missing by @ThomasAdam in #942
    * Fix bug with fvwm-menu-directory --install-prefix option. by @somiaj in #943
    * FvwmPrompt: accept commands on stdin by @ThomasAdam in #951
    * Update default-config to check for FvwmPrompt. by @somiaj in #950
    * RandR: handle monitor {dis,}connection better by @ThomasAdam in #953
    * logging: minor tweaks by @ThomasAdam in #955
    * FvwmPager: honour screen focus rules by @ThomasAdam in #958
    * FvwmPager: use monitors from fvwm3lib by @ThomasAdam in #961
    * FvwmPager: remove debug by @ThomasAdam in #962
    * FvwmPager: remove list_focus handling by @ThomasAdam in #964
    * GotoDesk: fallback to current monitor if not given by @ThomasAdam in #966
    * Improve RandR Detection by @ThomasAdam in #963
    * RandR: don't unset a new monitor's enabled state by @ThomasAdam in #971
    * Stop fvwm-menu-desktop from informing users it cannot find icon %s. by @somiaj in #973
    * shared mode: fix internal commands by @ThomasAdam in #976
    * A few FvwmPager fixes by @ThomasAdam in #980

--- wm/fvwm3/options.mk
+++ wm/fvwm3/options.mk
@@ -1,23 +1,104 @@
-# $NetBSD: options.mk,v 1.3 2024/03/22 13:54:54 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2024/05/31 10:59:24 vins Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fvwm3
-PKG_SUPPORTED_OPTIONS=	svg doc
+PKG_SUPPORTED_OPTIONS=	doc golang svg
 PKG_SUGGESTED_OPTIONS=	doc
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		doc
+PLIST_VARS+=		doc go
 
+#
+# SVG icon support
+#
 .if !empty(PKG_OPTIONS:Msvg)
 .  include "../../graphics/librsvg/buildlink3.mk"
 .endif
 
+#
+# Build documentation (requires asciidoctor)
+#
 .if !empty(PKG_OPTIONS:Mdoc)
 CONFIGURE_ARGS+=	--enable-mandoc
-TOOL_DEPENDS+=	${RUBY_PKGPREFIX}-asciidoctor-[0-9]*:../../textproc/ruby-asciidoctor	# asciidoctor
-CONFIGURE_ENV+=	ac_cv_prog_ASCIIDOC=${PREFIX}/bin/asciidoctor${RUBY_VER}
+TOOL_DEPENDS+=		${RUBY_PKGPREFIX}-asciidoctor-[0-9]*:../../textproc/ruby-asciidoctor
+ASCIIDOC=               ${PREFIX}/bin/asciidoctor${RUBY_VER}
+CONFIGURE_ENV+=		ac_cv_prog_ASCIIDOC=${ASCIIDOC:Q}
 .  include "../../lang/ruby/rubyversion.mk"
 PLIST.doc=		yes
 .else
 CONFIGURE_ARGS+=	--disable-mandoc
+.endif
+
+#
+# Fvwm modules written in Go
+#
+.if !empty(PKG_OPTIONS:Mgolang)
+
+.  include "../../lang/go/version.mk"
+.  include "go-modules.mk"
+
+GO_BUILD_PATTERN?=	./...
+GO_MOD_DIRS?=		bin/FvwmPrompt
+GO_MOD_DOCS?=		FvwmPrompt
+
+MAKE_JOBS_SAFE=		no
+INSTALLATION_DIRS+=	bin
+USE_TOOLS+=		pax
+
+TOOL_DEPENDS+=		${GO_PACKAGE_DEP}
+PRINT_PLIST_AWK+=	/^@pkgdir bin$$/ { next; }
+
+MAKE_ENV+=	GO111MODULE=on GOPATH=${WRKDIR}/.gopath
+MAKE_ENV+=	GOFLAGS=-ldflags="-s -w"
+MAKE_ENV+=	GOPROXY=file://${WRKDIR}/.goproxy
+MAKE_ENV+=	GOCACHE=${WRKDIR}/.cache/go-build
+MAKE_ENV+=	GOTOOLCHAIN=local
+
+.PHONY: show-go-modules
+show-go-modules: ${WRKDIR}/.extract_done
+.  for d in ${GO_MOD_DIRS}
+	${RUN} cd ${WRKSRC}/${d} &&	\
+		${PKGSRC_SETENV} ${MAKE_ENV} https_proxy= GOPROXY= ${GO} mod download -x
+.  endfor
+	${RUN} ${PRINTF} '# $$%s$$\n\n' NetBSD
+	${RUN} cd ${WRKDIR}/.gopath/pkg/mod/cache/download &&	\
+		${FIND} . -type f -a \( -name "*.mod" -o -name "*.zip" \)	\
+	| ${SED} -e 's/\.\//GO_MODULE_FILES+=	/' | ${SORT}
+
+DISTFILES?=		${DEFAULT_DISTFILES}
+EXTRACT_ONLY?=		${DEFAULT_DISTFILES} ${GO_MODULE_EXTRACT}
+.  for i in ${GO_MODULE_FILES}
+DISTFILES+=		${i:C/[\/!]/_/g}
+SITES.${i:C/[\/!]/_/g}=	-https://proxy.golang.org/${i}
+.  endfor
+
+post-extract:
+.  for i in ${GO_MODULE_FILES}
+	${RUN} ${MKDIR} ${WRKDIR}/.goproxy/${i:H}
+	${RUN} cp ${DISTDIR}/${DIST_SUBDIR}/${i:C/[\/!]/_/g} ${WRKDIR}/.goproxy/${i}
+.  endfor
+
+post-build:
+.  for d in ${GO_MOD_DIRS}
+	${RUN} ${PRINTF} '%s\n' "Making fvwm Go modules in ${d} ... "
+	${RUN} cd ${WRKSRC}/${d} && ${_ULIMIT_CMD} ${PKGSRC_SETENV} ${MAKE_ENV}	\
+		${GO} ${GOFLAGS} install -v ${GO_BUILD_PATTERN}
+.  endfor
+.  if !empty(PKG_OPTIONS:Mdoc)
+.    for i in ${GO_MOD_DOCS}
+	${RUN} ${PRINTF} '%s\n' "Building documentation for ${i} ... "
+	${RUN} cd ${WRKSRC}/doc && ${ASCIIDOC} -b manpage -a ${i} ${i}.adoc -o ${i}.1
+.    endfor
+.  endif
+
+post-install:
+	${RUN} cd ${WRKDIR}/.gopath && [ ! -d bin ] || ${PAX} -rw bin ${DESTDIR}${PREFIX}
+.  for i in ${GO_MOD_DOCS}
+	${INSTALL_MAN} ${WRKSRC}/doc/${i}.1 ${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
+.  endfor
+
+pre-clean:
+	${RUN} [ -d ${WRKDIR}/.gopath ] && chmod -R +w ${WRKDIR}/.gopath ||  ${TRUE}
+
+PLIST.go=		yes
 .endif
