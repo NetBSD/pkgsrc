64c7dbfbf8a1e6436f17c1f537675a9703c8b7dc
nia <nia@pkgsrc.org>
2024-06-05 06:40:59 +0000

    gc old unbuildable thunderbirds

--- mail/thunderbird52/patches/patch-mozilla_config_system__wrappers_unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-mozilla_config_system__wrappers_unwind.h,v 1.1 2018/12/16 08:29:48 ryoon Exp $
-
---- mozilla/config/system_wrappers/unwind.h.orig	2017-04-25 12:22:45.864950181 +0000
-+++ mozilla/config/system_wrappers/unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
