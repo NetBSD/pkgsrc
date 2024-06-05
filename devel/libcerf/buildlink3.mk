f92d5821f7e568f14bea1a4b49623bd7cc0dde0b
adam <adam@pkgsrc.org>
2024-06-05 13:51:28 +0000

    libcerf: updated to 2.4
    libcerf-2.4, released 2aug23:
      - CMake: runtime destination corrected (bin, not lib)
      - CMake: removed option PORTABLE and compiler flag -march (unavailable on some architectures)
    
    libcerf-2.3, released 10jan23:
      - Fix compiler flags; use -O3 for Release
        - Introduce CERF_COMPILE_OPTIONS to let users overwrite the hard-coded compile options
      - Under gcc, use -march=native
        - PACKAGE MAINTAINERS: use -DPORTABLE=ON to override this !!
      - Add test coverage for erfcx, dawson, im_w_of_x (against nearby complex and status quo)
      - Avoid some divisions in function voigt (suggested by Alexander Kleinsorge)
      - Accelerate lookup in erfcx, im_w_of_x (avoid long switch, contributed by Alexander Kleinsorge)
      - Work around clang bug https://github.com/llvm/llvm-project/issues/59681 (0+I*Inf->NaN)
    
    libcerf-2.2, released 7nov22:
      - No longer raise underflow error in w_of_z for large real arg (contributed by Ethan Merritt)
      - In test suite, confound zero with values close to underflow
      - Remove some invisible files that had slipped in accidentally (noticed by Christoph Junghans)
      - Windows binary now built with Visual Studio 17 2022
    
    libcerf-2.1, released 8apr22:
      - Provide cerfConfig.cmake (contributed by Lori A. Burns and Christoph Junghans)
      - libcerf.pc: Remove hard-coded lib/
      - Version number incremented to overcome upstream problems with withdrawn release 2.0
    
    libcerf-1.17, released 29jun21:
      - Correct pkgconfig installation directory (use GNU install dirs, install to LIBDIR/pkgconfig)
    
    libcerf-1.16, released 23jun21:
      - voigt_hwhm: replaced Newton's method (which failed for rare parameter combinations)
          by Illinois regula falsi (contributed by Ethan A Merritt)
      - Some internal cleanup, especially of include directives
    
    libcerf-1.15, released 10jun21:
      - Use assertion in voigt_hwhm to check for impossible situations
          (to avoid fprintf and exit, as suggested by Ethan A Merritt)
      - Added widthtest, which tests voigt_hwhm for numerous ratios gamma/sigma
      - libcerf.pc.in: correct prefix (as suggested by Alexander Grund)
      - CMake install
        - make full use of CMAKE_INSTALL_<dir> variables
        - DESTINATION now given by LIBDIR from GNUInstallDirs
      - Support CPack for all platforms
    
    libcerf-1.14, released 19oct20:
      - Simplified test code
      - Homepage moved to https://jugit.fz-juelich.de/mlz/libcerf, 17mar19

--- devel/libcerf/buildlink3.mk
+++ devel/libcerf/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/10/28 19:00:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/06/05 13:51:28 adam Exp $
 
 BUILDLINK_TREE+=	libcerf
 
 .if !defined(LIBCERF_BUILDLINK3_MK)
 LIBCERF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcerf+=	libcerf>=1.4
+BUILDLINK_API_DEPENDS.libcerf+=	libcerf>=2.4
 BUILDLINK_PKGSRCDIR.libcerf?=	../../devel/libcerf
 .endif	# LIBCERF_BUILDLINK3_MK
 
