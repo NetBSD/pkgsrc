336db2595a9b27028f81a2190e02cecc0a7b9576
jperkin <jperkin@pkgsrc.org>
2024-06-06 11:03:28 +0000

    irssi: Update to 1.4.5.
    pkgsrc fixes:
    
     * Remove useless MESSAGE
     * Fold Makefile.common back into Makefile
     * Remove obsolete variables
     * pkglint -Wall cleanup
    
    v1.4.5 2023-10-03  The Irssi team <staff@irssi.org>
    	+ Add workaround for Perl 5.38.0 bug that breaks the Irssi
              locale and glyph rendering (scripts.irssi.org#857, #1498)
    	- Fix Perl scripts broken by Perl 5.38 (scripts.irssi.org#851,
              #1474). With input from Leon Timmermans
    	- Document workaround to fix linker errors when building with
              meson on Apple (#1435, #1492)
    	- Fix meson using wrong filenames on Apple, leading to broken
              Perl support (#1483, #1493)
    	- Fix /upgrade not accepting `~' (#1460, #1462). By Lukas Mai
    	- Improve compatibility with Perl5-IDEA (#1465, #1467). By
              Charlie Daffern
    	- Fix logic in how own actions are printed for other protocols
              (codeberg!5, #1471). By Andrej Kacian
    	- Fix crash on old PowerPC Mac (#1482, #1484)
    	- Fix wrong prototype of library function used in terminal
              handling (#1495). By Emil Engler
    	- Minor cleanups (#1488, #1497). Includes work by Emil Engler
    	- Minor help and documentation fixes (#1458, #1494, #1477,
              #1478). Includes work by KindOne

--- chat/irssi/buildlink3.mk
+++ chat/irssi/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/07/24 22:21:42 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/06/06 11:03:28 jperkin Exp $
 
-BUILDLINK_TREE+=        irssi
+BUILDLINK_TREE+=	irssi
 
 .if !defined(IRSSI_BUILDLINK3_MK)
 IRSSI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.irssi+=    irssi>=1.4.4
-BUILDLINK_ABI_DEPENDS.irssi+=    irssi>=1.4.4
-BUILDLINK_PKGSRCDIR.irssi?=      ../../chat/irssi
+BUILDLINK_API_DEPENDS.irssi+=	irssi>=1.4.5
+BUILDLINK_ABI_DEPENDS.irssi+=	irssi>=1.4.5
+BUILDLINK_PKGSRCDIR.irssi?=	../../chat/irssi
 
 .endif # IRSSI_BUILDLINK3_MK
 
-BUILDLINK_TREE+=        -irssi
+BUILDLINK_TREE+=	-irssi
