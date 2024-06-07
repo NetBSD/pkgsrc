f5b0fd80cff0ae653cb5343341ace7d8f3827ba4
abs <abs@pkgsrc.org>
2024-06-07 17:39:01 +0000

    Updated databases/mongodb4 to 4.4.29
    4.4.29 Changelog
    
    Replication
    
     - SERVER-70155 Add duration of how long an oplog slot is kept open
       to mongod "Slow query" log lines
    
    Query
    
     - WT-11064 Skip globally visible tombstones as part of update obsolete check
    
    Storage
    
    WiredTiger
    
     - WT-12036 Workaround for lock contention on Windows
    
    Build and Packaging
    
     - SERVER-85156 dbCheck throws unexpected "invalidate" change stream
       event [5.0]
    
    Internals
    
     - SERVER-72839 Server skips peer certificate validation if neither
       CAFile nor clusterCAFile is provided
    
     - SERVER-74344 Ban use of sparse indexes on internal comparison
       expression unless explicitly hinted
    
     - SERVER-80279 Commit on non-existing transaction then proceed to
       continue can trigger an invariant
    
     - SERVER-80310 Update sysperf to allow running individual genny
       tasks on waterfall
    
     - SERVER-82353 Multi-document transactions can miss documents when
       movePrimary runs concurrently
    
     - SERVER-82815 Expose server’s index key creation via aggregation
    
     - SERVER-83564 Make sure the process field is indexed in config.locks
    
     - SERVER-84722 Create undocumented server parameter to skip document
       validation on insert code path for internal usage
    
     - SERVER-84732 Fix typo in mongo-perf standalone inMemory ARM AWS
       test
    
     - SERVER-85305 Fix sys-perf-4.4 clone issue
    
     - SERVER-85306 Update sys-perf config to use HTTPs github links
       rather than SSH
    
     - SERVER-85419 Balancer pollutes logs in case no suitable recipient
       is found during draining
    
     - SERVER-85530 Refresh Test Certificates
    
     - SERVER-85536 [4.4] removing unindexed unique partial index
       entries generates write conflicts
    
     - SERVER-85652 Update DSI atlas azure tasks to use an AL2 compile
       artifact.
    
     - SERVER-85771 Make $bucketAuto more robust in the case of an
       empty string for the groupBy field
    
     - SERVER-85984 The test for inserting docs larger than the user
       max is flaky
    
     - SERVER-86027 Tag insert_docs_larger_than_max_user_size_standalone.js
       with requires_persistence and requires_replication
    
     - SERVER-86081 Sys-perf missing required parameters due to Evergreen
       Redaction
    
     - SERVER-86322 [v4.4] Add high value workloads to the 4.4 branch
    
     - SERVER-86351 Investigate failed copybara sync operation
    
     - WT-11280 Generation tracking might not be properly synchronized
    
     - WT-12272 Remove unnecessary module in evergreen.yml

--- databases/mongodb4/patches/patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_src_bid__functions.h
+++ databases/mongodb4/patches/patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_src_bid__functions.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_src_bid__functions.h,v 1.1 2023/07/29 11:51:15 adam Exp $
+$NetBSD: patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_src_bid__functions.h,v 1.2 2024/06/07 17:39:01 abs Exp $
 
 Fix buidling.
 
