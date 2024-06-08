69931ef2bed990929a74a394f1650188c95fef8e
wiz <wiz@pkgsrc.org>
2024-06-08 16:06:26 +0000

    mame: adapt patch after discussion

--- emulators/mame/patches/patch-src_osd_modules_input_input__sdl.cpp
+++ emulators/mame/patches/patch-src_osd_modules_input_input__sdl.cpp
@@ -1,29 +1,15 @@
-$NetBSD: patch-src_osd_modules_input_input__sdl.cpp,v 1.3 2024/06/08 14:47:48 wiz Exp $
+$NetBSD: patch-src_osd_modules_input_input__sdl.cpp,v 1.4 2024/06/08 16:06:26 wiz Exp $
 
 Fix "lround is not a member of std::" on NetBSD.
 https://github.com/mamedev/mame/issues/12458
 
 --- src/osd/modules/input/input_sdl.cpp.orig	2024-05-29 17:42:54.000000000 +0000
 +++ src/osd/modules/input/input_sdl.cpp
-@@ -786,8 +786,8 @@ public:
- 		case SDL_MOUSEWHEEL:
- 			// adjust SDL 1-per-click to match Win32 120-per-click
- #if SDL_VERSION_ATLEAST(2, 0, 18)
--			m_v += std::lround(event.wheel.preciseY * 120 * input_device::RELATIVE_PER_PIXEL);
--			m_h += std::lround(event.wheel.preciseX * 120 * input_device::RELATIVE_PER_PIXEL);
-+			m_v += lround(event.wheel.preciseY * 120 * input_device::RELATIVE_PER_PIXEL);
-+			m_h += lround(event.wheel.preciseX * 120 * input_device::RELATIVE_PER_PIXEL);
- #else
- 			m_v += event.wheel.y * 120 * input_device::RELATIVE_PER_PIXEL;
- 			m_h += event.wheel.x * 120 * input_device::RELATIVE_PER_PIXEL;
-@@ -888,8 +888,8 @@ public:
- 		case SDL_MOUSEWHEEL:
- 			// adjust SDL 1-per-click to match Win32 120-per-click
- #if SDL_VERSION_ATLEAST(2, 0, 18)
--			m_v += std::lround(event.wheel.preciseY * 120 * input_device::RELATIVE_PER_PIXEL);
--			m_h += std::lround(event.wheel.preciseX * 120 * input_device::RELATIVE_PER_PIXEL);
-+			m_v += lround(event.wheel.preciseY * 120 * input_device::RELATIVE_PER_PIXEL);
-+			m_h += lround(event.wheel.preciseX * 120 * input_device::RELATIVE_PER_PIXEL);
- #else
- 			m_v += event.wheel.y * 120 * input_device::RELATIVE_PER_PIXEL;
- 			m_h += event.wheel.x * 120 * input_device::RELATIVE_PER_PIXEL;
+@@ -31,6 +31,7 @@
+ #include <SDL2/SDL.h>
+ 
+ #include <algorithm>
++#include <cmath>
+ #include <cctype>
+ #include <chrono>
+ #include <cstddef>
