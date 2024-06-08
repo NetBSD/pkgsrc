10cdf72d2767b883fcf4119170357ccbeb434439
wiz <wiz@pkgsrc.org>
2024-06-08 14:43:26 +0000

    mame: update to 0.266.
    MAME 0.266 is ready just in time for the end of May. The first
    thing you’ll notice is that Taito F3 video emulation has been
    reimplemented, fixing numerous long-standing graphical issues. The
    dynamic range compression used by several Yamaha synthesisers seems
    to finally be understood, fixing audible distortion. Speaking of
    sound, a previously missing Game Boy Advance sound channel is now
    emulated.
    
    Several additional members of Nokia’s MikroMikko 1 line of 8-bit
    business computers from the 1980s are now supported. In PC emulation,
    S3 ViRGE video accelerator emulation has been simplified and
    improved. A couple more cartridge types have been added for MSX
    home computers.
    
    This release also adds plenty of software list items and arcade
    bootlegs. Lots of code has been cleaned up and modernised, with
    various bugs fixed along the way. We’ve also added a workaround
    for the regression in clang 18 that was causing build failures.

--- emulators/mame/patches/patch-src_osd_modules_input_input__sdl.cpp
+++ emulators/mame/patches/patch-src_osd_modules_input_input__sdl.cpp
@@ -1,10 +1,21 @@
-$NetBSD: patch-src_osd_modules_input_input__sdl.cpp,v 1.1 2024/04/25 09:51:16 wiz Exp $
+$NetBSD: patch-src_osd_modules_input_input__sdl.cpp,v 1.2 2024/06/08 14:43:26 wiz Exp $
 
 Fix "lround is not a member of std::" on NetBSD.
 
---- src/osd/modules/input/input_sdl.cpp.orig	2024-04-25 09:49:02.743315265 +0000
+--- src/osd/modules/input/input_sdl.cpp.orig	2024-05-29 17:42:54.000000000 +0000
 +++ src/osd/modules/input/input_sdl.cpp
-@@ -738,8 +738,8 @@ public:
+@@ -786,8 +786,8 @@ public:
+ 		case SDL_MOUSEWHEEL:
+ 			// adjust SDL 1-per-click to match Win32 120-per-click
+ #if SDL_VERSION_ATLEAST(2, 0, 18)
+-			m_v += std::lround(event.wheel.preciseY * 120 * input_device::RELATIVE_PER_PIXEL);
+-			m_h += std::lround(event.wheel.preciseX * 120 * input_device::RELATIVE_PER_PIXEL);
++			m_v += lround(event.wheel.preciseY * 120 * input_device::RELATIVE_PER_PIXEL);
++			m_h += lround(event.wheel.preciseX * 120 * input_device::RELATIVE_PER_PIXEL);
+ #else
+ 			m_v += event.wheel.y * 120 * input_device::RELATIVE_PER_PIXEL;
+ 			m_h += event.wheel.x * 120 * input_device::RELATIVE_PER_PIXEL;
+@@ -888,8 +888,8 @@ public:
  		case SDL_MOUSEWHEEL:
  			// adjust SDL 1-per-click to match Win32 120-per-click
  #if SDL_VERSION_ATLEAST(2, 0, 18)
