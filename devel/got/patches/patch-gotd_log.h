24facc15a98a9f71170d64e87af37445bbb6dbbf
vins <vins@pkgsrc.org>
2024-06-09 13:25:05 +0000

    devel/got: update to 0.100
    * got 0.100; 2024-06-03
      - abort if we see a sha2 hash to quiet a potential compiler warning
      - switch to using readdir because readdir_r is deprecated
      - show hint about update -b if the user attempts to rebase a branch
        onto itself
      - in got.1 EXAMPLES, mention how files can be moved or renamed
      - fix running gotd regress in release tarball sources
      - in gotd.8 add an example which illustrates how to create
        repositories for gotd
      - don't leak the existence of gotd repositories to unrelated user
        accounts
      - fix empty notification messages with multiple gotd notification
        targets
      - display abbreviated commit/tag IDs in email notification subject
        lines
      - fix Date header generated by got-notify-email
      - regress: make server tests more robust against race hazard
      - regress: replace userinfo(8) with the more common getent(1)
      - fix wrong errno check in bufio_close_sync(), i.e. got-fetch-http
      - bufio: crank BIO_CHUNK up to 64k for improved http fetch performance
      - gotwebd.conf: remove `listen on socket off' and `unix_socket off'
        options
      - gotwebd.conf: make `listen' a top-level statement
      - gotwebd.conf: remove unix_socket_name option
      - gotwebd.conf: allow changing the user ID which gotwebd runs as
      - gotd/gotwebd: unify log.c
      - gotwebd: use less temporary files in /tmp
      - gotd/gotwebd: hide log_info() behind -v and log_debug behind -vv
        options
      - fix confusing error message from 'got commit' upon uncommitable
        paths
      - gotwebd: use the last matching fastcgi parameter if multiple
        parameters match
      - gotwebd: remove previd and prevset query string parameter, they were
        unused
      - plug 'got diff obj1 obj2' line metadata memory leak
      - fix interop with servers that do not use Git protocol sidebands,
        such as git9
      - reintroduce the 'got init' command as an alternative to 'gotadmin
        init'

--- devel/got/patches/patch-gotd_log.h
+++ devel/got/patches/patch-gotd_log.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-gotd_log.h,v 1.1 2024/02/11 14:47:07 vins Exp $
+$NetBSD: patch-gotd_log.h,v 1.2 2024/06/09 13:25:05 vins Exp $
 
 Provide a declaration of va_list
 
---- gotd/log.h.orig	2024-01-14 16:07:41.039068602 +0000
-+++ gotd/log.h
+--- lib/log.h.orig	2024-01-14 16:07:41.039068602 +0000
++++ lib/log.h
 @@ -14,6 +14,7 @@
   * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
   */
