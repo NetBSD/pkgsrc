7e6491bf89a3c56fceb510eb848f18851db0818b
markd <markd@pkgsrc.org>
2024-06-09 00:42:14 +0000

    kde: add plasma6- prefix to name by default (for plasma6 packages)

--- meta-pkgs/kde/plasma6.mk
+++ meta-pkgs/kde/plasma6.mk
@@ -1,8 +1,9 @@
-# $NetBSD: plasma6.mk,v 1.1 2024/06/08 00:00:40 markd Exp $
+# $NetBSD: plasma6.mk,v 1.2 2024/06/09 00:42:14 markd Exp $
 
 PLASMAVER?=	6.0.5
 CATEGORIES+=	kde
 MASTER_SITES=	${MASTER_SITE_KDE:=plasma/${PLASMAVER}/}
 EXTRACT_SUFX=	.tar.xz
+PKGNAME?=	plasma6-${DISTNAME}
 
 .include "../../meta-pkgs/kde/Makefile.common"
