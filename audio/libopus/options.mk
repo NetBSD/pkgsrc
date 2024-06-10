aed39ebabb4d62fd10150aeec46ca2a3a004d627
wiz <wiz@pkgsrc.org>
2024-06-10 15:46:07 +0000

    libopus: fix PLIST for doc option
    From Robert Whitlock in PR 58333.

--- audio/libopus/options.mk
+++ audio/libopus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2024/04/26 15:43:05 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2024/06/10 15:46:07 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libopus
 PKG_SUPPORTED_OPTIONS=	doc
@@ -10,6 +10,8 @@ PLIST_VARS+=	doc
 .if !empty(PKG_OPTIONS:Mdoc)
 TOOL_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
 PLIST.doc=		yes
+PRINT_PLIST_AWK+=	{ if (/^share\/doc/) $$0 = "$${PLIST.doc}" $$0 }
+PRINT_PLIST_AWK+=	{ if (/^man/) $$0 = "$${PLIST.doc}" $$0 }
 .else
 CONFIGURE_ARGS+=	--disable-doc
 .endif
