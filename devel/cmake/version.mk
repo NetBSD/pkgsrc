a416cabceeb1769ea1052967d098532cc04c3926
adam <adam@pkgsrc.org>
2024-06-10 07:17:31 +0000

    cmake cmake-gui: updated to 3.29.5
    CMake 3.29.5
    * CPack/WiX: Restore default (missing) InstallScope from 3.28 and lower
    * ctest: Set close-on-exec for jobserver FIFO

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.59 2024/06/04 08:33:04 adam Exp $
+# $NetBSD: version.mk,v 1.60 2024/06/10 07:17:31 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.29.4
+CMAKE_VERSION=	3.29.5
 CMAKE_API=	${CMAKE_VERSION:R}
