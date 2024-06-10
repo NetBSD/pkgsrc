67757b701314c7cf9541ac96694e5bf15491f8df
wiz <wiz@pkgsrc.org>
2024-06-10 06:16:04 +0000

    json-c: fix PLIST for doc option
    From Robert Whitlock in PR 58332.

--- textproc/json-c/options.mk
+++ textproc/json-c/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2023/01/09 18:16:14 khorben Exp $
+# $NetBSD: options.mk,v 1.2 2024/06/10 06:16:04 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.json-c
 PKG_SUPPORTED_OPTIONS=	doc
+
 PLIST_VARS+=		doc
 
 .include "../../mk/bsd.options.mk"
@@ -11,6 +12,7 @@ USE_TOOLS+=		pax:build
 DEPENDS+=		doxygen>=1.8.13:../../devel/doxygen
 BUILD_TARGET+=		doc
 PLIST.doc=		yes
+PRINT_PLIST_AWK+=	{ if (/^share\/doc/) $$0 = "$${PLIST.doc}" $$0 }
 
 INSTALLATION_DIRS+=	share/doc/json-c
 
