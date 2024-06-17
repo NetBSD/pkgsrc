2a837072c11a0904040dca8e2c7999c06f9cf14a
gutteridge <gutteridge@pkgsrc.org>
2024-06-17 21:53:57 +0000

    opensbi: mark PYTHON_VERSIONS_INCOMPATIBLE and de-lint

--- sysutils/opensbi/opensbi.mk
+++ sysutils/opensbi/opensbi.mk
@@ -1,4 +1,4 @@
-# $NetBSD: opensbi.mk,v 1.1 2024/06/17 11:20:32 skrll Exp $
+# $NetBSD: opensbi.mk,v 1.2 2024/06/17 21:53:57 gutteridge Exp $
 
 .include "../../sysutils/opensbi/opensbi-version.mk"
 
@@ -13,19 +13,18 @@ GITHUB_TAG?=		v1.4
 MASTER_SITES?=		${MASTER_SITE_GITHUB:=riscv-software-src/}
 DIST_SUBDIR?=		${GITHUB_PROJECT}
 
-TOOL_DEPENDS+=		dtc>=1.4.2:../../sysutils/dtc
-
 COMMENT=		RISC-V Open Source Supervisor Binary Interface (OpenSBI)
 LICENSE=		2-clause-bsd
 
 USE_TOOLS+=		gmake
-
+TOOL_DEPENDS+=		dtc>=1.4.2:../../sysutils/dtc
 BUILD_DEPENDS+=		cross-riscv64-none-elf-gcc-[0-9]*:../../cross/riscv64-none-elf-gcc
 
 OPENSBI_ENV+=		CROSS_COMPILE=${PREFIX}/cross-riscv64-none-elf/bin/riscv64-none-elf-
 OPENSBI_ENV+=		PLATFORM=${OPENSBI_PLATFORM}
 
-PYTHON_FOR_BUILD_ONLY=	yes
+PYTHON_VERSIONS_INCOMPATIBLE=	27
+PYTHON_FOR_BUILD_ONLY=		yes
 .include "../../lang/python/tool.mk"
 
 MAKE_ENV+=		${OPENSBI_ENV}
