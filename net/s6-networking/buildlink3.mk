ad0d0634606173255003bb32c28ed2f8143b9fac
schmonz <schmonz@pkgsrc.org>
2024-06-19 19:00:07 +0000

    s6-networking: update to 2.7.0.3. Changes:
    - Bugfixes.

--- net/s6-networking/buildlink3.mk
+++ net/s6-networking/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/12/23 20:11:48 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2024/06/19 19:00:07 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-networking
 
 .if !defined(S6_NETWORKING_BUILDLINK3_MK)
 S6_NETWORKING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-networking+=	s6-networking>=2.7.0.1
+BUILDLINK_API_DEPENDS.s6-networking+=	s6-networking>=2.7.0.3
 BUILDLINK_PKGSRCDIR.s6-networking?=	../../net/s6-networking
 BUILDLINK_INCDIRS.s6-networking+=	include/s6-networking
 BUILDLINK_LIBDIRS.s6-networking+=	lib/s6-networking
