e19367d00509de1e13aefe4c736859ff69ddaea3
schmonz <schmonz@pkgsrc.org>
2024-06-19 18:59:28 +0000

    s6: update to 2.13.0.0. Changes:
    - Bugfixes.
    - s6-svstat -o pgid (or -g) prints the service's pgid
    - s6-supervise now passes the service's pgid to ./finish
    - New s6-svc -P/-C/-K sends SIGSTOP, SIGCONT or SIGKILL to
      the service's process group.

--- sysutils/s6/buildlink3.mk
+++ sysutils/s6/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/12/23 20:10:14 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/06/19 18:59:28 schmonz Exp $
 
 BUILDLINK_TREE+=	s6
 
 .if !defined(S6_BUILDLINK3_MK)
 S6_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6+=	s6>=2.12.0.3
+BUILDLINK_API_DEPENDS.s6+=	s6>=2.13.0.0
 BUILDLINK_PKGSRCDIR.s6?=	../../sysutils/s6
 BUILDLINK_INCDIRS.s6+=		include/s6
 BUILDLINK_LIBDIRS.s6+=		lib/s6
