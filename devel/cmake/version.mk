475a728a6c6a486f88b0c6066ae493f973baba81
adam <adam@pkgsrc.org>
2024-06-19 08:47:42 +0000

    cmake cmake-gui: updated to 3.29.6
    CMake 3.29.6
    * Clang: Drop non-existent -fno-ansi-escape-codes flag
    * VS: Quote custom command arguments containing semicolons

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.60 2024/06/10 07:17:31 adam Exp $
+# $NetBSD: version.mk,v 1.61 2024/06/19 08:47:42 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.29.5
+CMAKE_VERSION=	3.29.6
 CMAKE_API=	${CMAKE_VERSION:R}
