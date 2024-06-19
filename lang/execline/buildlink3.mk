00092fb3fce6b2f11eee7297ecf5ef4587e53d3e
schmonz <schmonz@pkgsrc.org>
2024-06-19 18:58:58 +0000

    execline: update to 2.9.6.0. Changes:
    - elglob can now provide a single word, where the results of the
      globbing are separated by a given delimiter (or encoded into a
      netstring).
    - New option: importas -S, with the same functionality as the old
      "import" command: import a variable with the same name.

--- lang/execline/buildlink3.mk
+++ lang/execline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/11/11 11:52:41 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2024/06/19 18:58:58 schmonz Exp $
 
 BUILDLINK_TREE+=	execline
 
 .if !defined(EXECLINE_BUILDLINK3_MK)
 EXECLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.4.0
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.6.0
 BUILDLINK_PKGSRCDIR.execline?=		../../lang/execline
 BUILDLINK_DEPMETHOD.execline?=		build
 .endif # EXECLINE_BUILDLINK3_MK
