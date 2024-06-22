1c222c22a7f0f7f3fa7a4ee8e9c6ccace21c9a85
jperkin <jperkin@pkgsrc.org>
2024-06-22 12:16:40 +0000

    ghc98: Attempt to fix dependencies on Darwin.
    With libiconv being included but overridden with PREFER=native in the main
    package, this ends up breaking dependencies as they expect the libiconv package
    to be installed.
    
    For now just exclude the libiconv include in the bl3, but this could really do
    with a cleaner fix.

--- lang/ghc98/buildlink3.mk
+++ lang/ghc98/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/04/28 16:23:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/06/22 12:16:40 jperkin Exp $
 
 BUILDLINK_TREE+=	ghc
 
@@ -9,7 +9,13 @@ BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.8.2
 BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.8.2nb1
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc98
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+# A regrettable hack.  See Makefile for background.
+.if ${OPSYS} != "Darwin"
 .include "../../converters/libiconv/buildlink3.mk"
+.endif
+
 .include "../../devel/libffi/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
 .include "../../mk/curses.buildlink3.mk"
