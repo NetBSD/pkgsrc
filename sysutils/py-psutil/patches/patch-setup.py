e940cb0556d88b17e62e0a545b558db19a4b0159
wiz <wiz@pkgsrc.org>
2024-06-26 16:51:33 +0000

    py-psutil: update to 6.0.0.
    6.0.0 2024-06-18
    ================
    
    **Enhancements**
    
    - 2109_: ``maxfile`` and ``maxpath`` fields were removed from the namedtuple
      returned by `disk_partitions()`_. Reason: on network filesystems (NFS) this
      can potentially take a very long time to complete.
    - 2366_, [Windows]: log debug message when using slower process APIs.
    - 2375_, [macOS]: provide arm64 wheels.  (patch by Matthieu Darbois)
    - 2396_: `process_iter()`_ no longer pre-emptively checks whether PIDs have
      been reused. This makes `process_iter()`_ around 20x times faster.
    - 2396_: a new ``psutil.process_iter.cache_clear()`` API can be used the clear
      `process_iter()`_ internal cache.
    - 2401_, Support building with free-threaded CPython 3.13.
    - 2407_: `Process.connections()`_ was renamed to `Process.net_connections()`_.
      The old name is still available, but it's deprecated (triggers a
      ``DeprecationWarning``) and will be removed in the future.
    - 2425_: [Linux]: provide aarch64 wheels.  (patch by Matthieu Darbois / Ben Raz)
    
    **Bug fixes**
    
    - 2250_, [NetBSD]: `Process.cmdline()`_ sometimes fail with EBUSY. It usually
      happens for long cmdlines with lots of arguments. In this case retry getting
      the cmdline for up to 50 times, and return an empty list as last resort.
    - 2254_, [Linux]: offline cpus raise NotImplementedError in cpu_freq() (patch
      by Shade Gladden)
    - 2272_: Add pickle support to psutil Exceptions.
    - 2359_, [Windows], [CRITICAL]: `pid_exists()`_ disagrees with `Process`_ on
      whether a pid exists when ERROR_ACCESS_DENIED.
    - 2360_, [macOS]: can't compile on macOS < 10.13.  (patch by Ryan Schmidt)
    - 2362_, [macOS]: can't compile on macOS 10.11.  (patch by Ryan Schmidt)
    - 2365_, [macOS]: can't compile on macOS < 10.9.  (patch by Ryan Schmidt)
    - 2395_, [OpenBSD]: `pid_exists()`_ erroneously return True if the argument is
      a thread ID (TID) instead of a PID (process ID).
    - 2412_, [macOS]: can't compile on macOS 10.4 PowerPC due to missing `MNT_`
      constants.
    
    **Porting notes**
    
    Version 6.0.0 introduces some changes which affect backward compatibility:
    
    - 2109_: the namedtuple returned by `disk_partitions()`_' no longer has
      ``maxfile`` and ``maxpath`` fields.
    - 2396_: `process_iter()`_ no longer pre-emptively checks whether PIDs have
      been reused. If you want to check for PID reusage you are supposed to use
      `Process.is_running()`_ against the yielded `Process`_ instances. That will
      also automatically remove reused PIDs from `process_iter()`_ internal cache.
    - 2407_: `Process.connections()`_ was renamed to `Process.net_connections()`_.
      The old name is still available, but it's deprecated (triggers a
      ``DeprecationWarning``) and will be removed in the future.

--- sysutils/py-psutil/patches/patch-setup.py
+++ sysutils/py-psutil/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.8 2023/10/18 08:53:08 adam Exp $
+$NetBSD: patch-setup.py,v 1.9 2024/06/26 16:51:33 wiz Exp $
 
 Enable limited API on all platforms.
 
---- setup.py.orig	2023-10-04 12:46:27.000000000 +0000
+--- setup.py.orig	2024-06-02 23:32:26.000000000 +0000
 +++ setup.py
-@@ -115,7 +115,7 @@ macros.append(('PSUTIL_VERSION', int(VER
+@@ -120,7 +120,7 @@ macros.append(('PSUTIL_VERSION', int(VER
  
  # Py_LIMITED_API lets us create a single wheel which works with multiple
  # python versions, including unreleased ones.
--if bdist_wheel and CP36_PLUS and (MACOS or LINUX):
+-if bdist_wheel and CP36_PLUS and (MACOS or LINUX) and not Py_GIL_DISABLED:
 +if bdist_wheel and CP36_PLUS:
      py_limited_api = {"py_limited_api": True}
      macros.append(('Py_LIMITED_API', '0x03060000'))
- elif bdist_wheel and CP37_PLUS and WINDOWS:
+ elif bdist_wheel and CP37_PLUS and WINDOWS and not Py_GIL_DISABLED:
