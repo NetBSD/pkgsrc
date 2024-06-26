1043f55e9a7c7ae87c56fc1a2381a6fd9954c839
adam <adam@pkgsrc.org>
2024-06-26 16:20:55 +0000

    opencolorio: updated to 2.3.2
    v2.3.2
    
    This is an ABI-compatible update for the 2.3.x series that includes all fixes since the 2.3.1 release.
    
    In particular, this release fixes two crashes:
    
    Certain older (generally 7-12 year old) CPUs including Intel x86-64 (Sandy Bridge and Ivy Bridge) and Intel Atom (Goldmont/Apollo Lake) microarchitectures that support SSE 4.2 SIMD instructions but do not support the initial AVX set and XSAVE, or other CPUs where the OS has disabled AVX and XSAVE support, will crash when trying to evaluate a Lut1D or Lut3D in OCIO libraries compiled with SSE enabled (which is the default). More recent CPUs (e.g., those supporting AVX2 or higher) should not be affected.
    
    Users of the Linux Python bindings via PyPI will crash when trying to use OCIO from within another application that has been compiled with the new libstdc++ ABI, per the requirements of the VFX Platform for CY2023 and CY2024.
    
    Bug fixes and minor enhancements:
    
    Prevent trying to use AVX instructions on certain CPUs that don't support them
    Add manylinux_2_28 Python wheels for compliance with the current VFX Platform libstdc++ ABI
    Fix ssse3 detection typo
    Fix build break with custom PyString installs by always using system include pystring.h
    Fix narrowing conversion error in unit test on riscv64
    Enhance the ociochecklut command-line tool to print the output after each step

--- graphics/opencolorio/patches/patch-share_cmake_modules_Findyaml-cpp.cmake
+++ graphics/opencolorio/patches/patch-share_cmake_modules_Findyaml-cpp.cmake
@@ -1,15 +1,15 @@
-$NetBSD: patch-share_cmake_modules_Findyaml-cpp.cmake,v 1.1 2023/10/15 19:45:33 ryoon Exp $
+$NetBSD: patch-share_cmake_modules_Findyaml-cpp.cmake,v 1.2 2024/06/26 16:20:55 adam Exp $
 
-* Fix yaml-cpp-0.8.8 detection.
+Fix yaml-cpp-0.8.8 detection.
 
---- share/cmake/modules/Findyaml-cpp.cmake.orig	2023-01-06 02:05:59.000000000 +0000
+--- share/cmake/modules/Findyaml-cpp.cmake.orig	2024-01-30 07:11:35.000000000 +0000
 +++ share/cmake/modules/Findyaml-cpp.cmake
-@@ -39,7 +39,7 @@ if(NOT OCIO_INSTALL_EXT_PACKAGES STREQUA
+@@ -49,7 +49,7 @@ if(NOT OCIO_INSTALL_EXT_PACKAGES STREQUA
  
+     # Search for yaml-cpp-config.cmake
      if(NOT DEFINED yaml-cpp_ROOT)
-         # Search for yaml-cpp-config.cmake
--        find_package(yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG QUIET)
-+        find_package(yaml-cpp::yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG QUIET)
+-        find_package(yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG ${quiet})
++        find_package(yaml-cpp::yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG ${quiet})
      endif()
  
      if(yaml-cpp_FOUND)
