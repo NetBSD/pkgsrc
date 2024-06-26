63e147b148ea24e39e92ba341718f1fad67b399b
adam <adam@pkgsrc.org>
2024-06-26 16:40:58 +0000

    qt6: updated to 6.7.2
    6.7.2
    
    As a patch release, Qt 6.7.2 does not introduce new features but contains more
    than 200 bug fixes, security updates, and other improvements to the top of the
    Qt 6.7.1 release. See more information about the most important changes and bug
    fixes from Qt 6.7.2 release note:
    https://code.qt.io/cgit/qt/qtreleasenotes.git/about/qt/6.7.2/release-note.md

--- multimedia/qt6-qtmultimedia/patches/patch-cmake_FindFFmpeg.cmake
+++ multimedia/qt6-qtmultimedia/patches/patch-cmake_FindFFmpeg.cmake
@@ -1,14 +1,14 @@
-$NetBSD: patch-cmake_FindFFmpeg.cmake,v 1.2 2024/02/16 17:39:05 adam Exp $
+$NetBSD: patch-cmake_FindFFmpeg.cmake,v 1.3 2024/06/26 16:41:00 adam Exp $
 
 * --exclude-libs is not available on Solaris/Illumos, according to
   the git commit messages for this file about exlude-libs it was
   added relocation problems on Linux so only use it on Linux
 
---- cmake/FindFFmpeg.cmake.orig	2024-02-10 00:08:32.000000000 +0000
+--- cmake/FindFFmpeg.cmake.orig	2024-06-08 01:28:28.000000000 +0000
 +++ cmake/FindFFmpeg.cmake
-@@ -288,7 +288,7 @@ endfunction()
-           __ffmpeg_internal_set_dependencies(${_lowerComponent})
-         endif()
+@@ -311,7 +311,7 @@ endfunction()
+ 
+         __ffmpeg_internal_set_dependencies(${_component})
          target_link_libraries(FFmpeg::${_lowerComponent} INTERFACE "${${_component}_LIBRARY_NAME}")
 -        if (UNIX AND NOT APPLE)
 +        if (LINUX)
