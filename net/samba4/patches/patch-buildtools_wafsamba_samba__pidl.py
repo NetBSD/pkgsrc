6a0e8c750088b4c492f57fbe0bb4a47fe0b40d56
adam <adam@pkgsrc.org>
2024-06-27 09:50:27 +0000

    samba4: updated to 4.20.2
    Changes since 4.20.1
    --------------------
    
    o  Jeremy Allison <jra@samba.org>
       * BUG 15662: vfs_widelinks with DFS shares breaks case insensitivity.
    
    o  Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
       * BUG 13213: Samba build is not reproducible.
       * BUG 15569: ldb qsort might r/w out of bounds with an intransitive compare
         function.
       * BUG 15625: Many qsort() comparison functions are non-transitive, which can
         lead to out-of-bounds access in some circumstances.
    
    o  Andrew Bartlett <abartlet@samba.org>
       * BUG 15638: Need to change gitlab-ci.yml tags in all branches to avoid CI
         bill.
       * BUG 15654: We have added new options --vendor-name and --vendor-patch-
         revision arguments to ./configure to allow distributions and packagers to
         put their name in the Samba version string so that when debugging Samba the
         source of the binary is obvious.
    
    o  Günther Deschner <gd@samba.org>
       * BUG 15665: CTDB RADOS mutex helper misses namespace support.
    
    o  Stefan Metzmacher <metze@samba.org>
       * BUG 13019: Dynamic DNS updates with the internal DNS are not working.
       * BUG 14981: netr_LogonSamLogonEx returns NR_STATUS_ACCESS_DENIED with
         SysvolReady=0.
       * BUG 15412: Anonymous smb3 signing/encryption should be allowed (similar to
         Windows Server 2022).
       * BUG 15573: Panic in dreplsrv_op_pull_source_apply_changes_trigger.
       * BUG 15620: s4:nbt_server: does not provide unexpected handling, so winbindd
         can't use nmb requests instead cldap.
       * BUG 15642: winbindd, net ads join and other things don't work on an ipv6
         only host.
       * BUG 15659: Segmentation fault when deleting files in vfs_recycle.
       * BUG 15664: Panic in vfs_offload_token_db_fetch_fsp().
       * BUG 15666: "client use kerberos" and --use-kerberos is ignored for the
         machine account.
    
    o  Noel Power <noel.power@suse.com>
       * BUG 15435: Regression DFS not working with widelinks = true.
    
    o  Andreas Schneider <asn@samba.org>
       * BUG 15633: samba-gpupdate - Invalid NtVer in netlogon_samlogon_response.
       * BUG 15653: idmap_ad creates an incorrect local krb5.conf in case of trusted
         domain lookups.
       * BUG 15660: The images don't build after the git security release and CentOS
         8 Stream is EOL.

--- net/samba4/patches/patch-buildtools_wafsamba_samba__pidl.py
+++ net/samba4/patches/patch-buildtools_wafsamba_samba__pidl.py
@@ -1,16 +1,19 @@
-$NetBSD: patch-buildtools_wafsamba_samba__pidl.py,v 1.3 2020/04/02 11:21:42 adam Exp $
+$NetBSD: patch-buildtools_wafsamba_samba__pidl.py,v 1.4 2024/06/27 09:50:27 adam Exp $
 
 Do not set CPP, as it fails when using cwrappers.
 If not provided, it will default to $CC -E (see pidl/idl.yp).
 
---- buildtools/wafsamba/samba_pidl.py.orig	2020-01-31 10:25:36.000000000 +0000
+--- buildtools/wafsamba/samba_pidl.py.orig	2024-06-19 14:28:56.379517300 +0000
 +++ buildtools/wafsamba/samba_pidl.py
-@@ -80,7 +80,7 @@ def SAMBA_PIDL(bld, pname, source,
+@@ -81,9 +81,9 @@ def SAMBA_PIDL(bld, pname, source,
          else:
              cc = 'CC="%s"' % bld.CONFIG_GET("CC")
  
--    t = bld(rule='cd ${PIDL_LAUNCH_DIR} && %s%s %s ${PERL} ${PIDL} --quiet ${OPTIONS} --outputdir ${OUTPUTDIR} -- "${IDLSRC}"' % (pidl_dev, cpp, cc),
-+    t = bld(rule='cd ${PIDL_LAUNCH_DIR} && %s %s ${PERL} ${PIDL} --quiet ${OPTIONS} --outputdir ${OUTPUTDIR} -- "${IDLSRC}"' % (pidl_dev, cc),
+-    t = bld(rule=('cd ${PIDL_LAUNCH_DIR} && PERL_HASH_SEED=0 %s%s %s ${PERL} '
++    t = bld(rule=('cd ${PIDL_LAUNCH_DIR} && PERL_HASH_SEED=0 %s %s ${PERL} '
+                   '${PIDL} --quiet ${OPTIONS} --outputdir ${OUTPUTDIR} -- "${IDLSRC}"' %
+-                  (pidl_dev, cpp, cc)),
++                  (pidl_dev, cc)),
              ext_out    = '.c',
              before     = 'c',
              update_outputs = True,
