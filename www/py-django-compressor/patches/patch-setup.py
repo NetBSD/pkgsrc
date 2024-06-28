1f6deaee42d884aaa25fa6e7e541f081ce1458c1
adam <adam@pkgsrc.org>
2024-06-28 09:58:10 +0000

    py-django-compressor: updated to 4.5
    v4.5 (2024-05-17)
    
    - Officially support Python 3.12 (requires lxml 4.9.3 or higher)
    - Drop support for Django versions before 4.2.
    - Added support for Django 5.1
    - Added new ``COMPRESS_STORAGE_ALIAS`` and ``COMPRESS_OFFLINE_MANIFEST_STORAGE_ALIAS``
      settings. These point to keys in Django's ``storages`` handler. If set by the
      user, Compressor will use these storages, or otherwise will set a default
      storage, given by ``COMPRESS_STORAGE`` and
      ``COMPRESS_OFFLINE_MANIFEST_STORAGE`` respectively on first access.

--- www/py-django-compressor/patches/patch-setup.py
+++ www/py-django-compressor/patches/patch-setup.py
@@ -1,17 +1,17 @@
-$NetBSD: patch-setup.py,v 1.1 2023/10/25 19:40:36 markd Exp $
+$NetBSD: patch-setup.py,v 1.2 2024/06/28 09:58:11 adam Exp $
 
 Don't pin to a single version
 https://github.com/django-compressor/django-compressor/issues/1195
 
---- setup.py.orig	2023-01-22 12:40:08.000000000 +0000
+--- setup.py.orig	2024-06-16 17:24:41.000000000 +0000
 +++ setup.py
-@@ -163,7 +163,7 @@ setup(
-     zip_safe=False,
+@@ -164,7 +164,7 @@ setup(
      install_requires=[
+         "Django >= 4.2",
          "django-appconf >= 1.0.3",
--        "rcssmin == 1.1.1",
--        "rjsmin == 1.2.1",
-+        "rcssmin >= 1.1.1",
-+        "rjsmin >= 1.2.1",
+-        "rcssmin == 1.1.2",
+-        "rjsmin == 1.2.2",
++        "rcssmin >= 1.1.2",
++        "rjsmin >= 1.2.2",
      ],
  )
