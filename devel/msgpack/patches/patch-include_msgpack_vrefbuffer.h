a1c6cbbfd40ccc3036977e4ebb7c7b6b3cfc65c1
adam <adam@pkgsrc.org>
2024-06-28 17:35:09 +0000

    msgpack: updated to 6.0.2
    version 6.0.2
    
    Fix header file installation to respect CMAKE_INSTALL_INCLUDEDIR
    Support absolute path for CMAKE_INSTALL_*DIR
    Removed invalid ctest option.
    Support relative path for CMAKE_INSTALL_*DIR
    
    version 6.0.1 for C
    
    Improve CI environment
    Improve build system
    
    version 6.0.0
    
    Remove C++ requirement if test is disabled
    << breaking changes >>
    
    Change CMake package name of C library to msgpack-c
    Unified all C package, library, cmake, tarball name become msgpack-c.

--- devel/msgpack/patches/patch-include_msgpack_vrefbuffer.h
+++ devel/msgpack/patches/patch-include_msgpack_vrefbuffer.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-include_msgpack_vrefbuffer.h,v 1.3 2020/04/11 13:39:29 adam Exp $
+$NetBSD: patch-include_msgpack_vrefbuffer.h,v 1.4 2024/06/28 17:35:09 adam Exp $
 
 FreeBSD fix.
 
---- include/msgpack/vrefbuffer.h.orig	2019-12-10 03:33:32.000000000 +0000
+--- include/msgpack/vrefbuffer.h.orig	2023-04-26 14:20:34.000000000 +0000
 +++ include/msgpack/vrefbuffer.h
-@@ -13,7 +13,7 @@
- #include "zone.h"
+@@ -14,7 +14,7 @@
  #include <stdlib.h>
+ #include <assert.h>
  
 -#if defined(unix) || defined(__unix) || defined(__linux__) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__HAIKU__)
-+#if defined(unix) || defined(__unix) || defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__HAIKU__)
++#if defined(unix) || defined(__unix) || defined(__linux__) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__HAIKU__) || defined(__FreeBSD__)
  #include <sys/uio.h>
+ typedef struct iovec msgpack_iovec;
  #else
- struct iovec {
