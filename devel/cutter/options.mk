f8a647c2a36998cbb3973a9546400ddb666c11c1
adam <adam@pkgsrc.org>
2024-06-28 08:36:17 +0000

    cutter: updated to 1.2.7
    1.2.7: 2019-09-13
    
    Improvements
    
    * [deb][ubuntu] Dropped support for Ubuntu 14.04/17.04/17.10/18.10.
    * [gstreamer] Restricted GStreamer 1.14 or later as non supported version explicitly.
    * [deb][ubuntu] Added support for Ubuntu 18.04/19.04
    * [homebrew] Added support for custom Homebrew path. In the previous versions,
      /usr/local/share/aclocal was expected as Homebrew's aclocal path.
    * [travis] Added macOS as CI target.
    * [doc][tutorial] Added a description about OS that need DYLD_LIBRARY_PATH
    * [doc][tutorial] Updated an explanation about loading shared library.
    
    cppcutter
    
    Fixes
    
    * Fixed returning invalid address because of wrong c_str() usage in Message class.

--- devel/cutter/options.mk
+++ devel/cutter/options.mk
@@ -1,12 +1,11 @@
-# $NetBSD: options.mk,v 1.7 2023/06/26 12:17:40 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2024/06/28 08:36:17 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cutter
-PKG_SUPPORTED_OPTIONS=	gtk2 libsoup pdf
-PKG_SUGGESTED_OPTIONS=	#
+PKG_SUPPORTED_OPTIONS=	libsoup pdf
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	gtk pdf soup
+PLIST_VARS+=	pdf soup
 
 ###
 ### PDF support
@@ -16,31 +15,6 @@ PLIST_VARS+=	gtk pdf soup
 PLIST.pdf=		yes
 .endif
 
-###
-### goffice support
-###
-#.if !empty(PKG_OPTIONS:Mgoffice)
-#.include "../../misc/goffice0.8/buildlink3.mk"
-#CONFIGURE_ARGS+=	--enable-goffice
-#PLIST.gtk=		yes
-#PLIST.pdf=		yes
-#.else
-#CONFIGURE_ARGS+=	--disable-goffice
-#.endif
-
-###
-### gtk2 support
-###
-.if !empty(PKG_OPTIONS:Mgtk2)
-.include "../../x11/gtk2/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gtktest
-PLIST.gtk=		yes
-PLIST.pdf=		yes
-.else
-CONFIGURE_ARGS+=	--disable-gtktest
-.endif
-
-###
 ###
 ### libsoup support
 ###
