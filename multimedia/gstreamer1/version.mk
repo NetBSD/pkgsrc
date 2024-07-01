7e0d3c83ad00bb16911aec614ca106dca8c986cd
adam <adam@pkgsrc.org>
2024-07-01 14:04:46 +0000

    gstreamer1: updated to 1.24.5
    GStreamer 1.24.5
    
    Highlighted bugfixes:
    
    webrtcsink: Support for AV1 via nvav1enc, av1enc or rav1enc encoders
    AV1 RTP payloader/depayloader fixes to work correctly with Chrome and Pion WebRTC
    av1parse, av1dec error handling/robustness improvements
    av1enc: Handle force-keyunit events properly for WebRTC
    decodebin3: selection and collection handling improvements
    hlsdemux2: Various fixes for discontinuities, variant switching, playlist updates
    qml6glsink: fix RGB format support
    rtspsrc: more control URL handling fixes
    v4l2src: Interpret V4L2 report of sync loss as video signal loss
    d3d12 encoder, memory and videosink fixes
    vtdec: more robust error handling, fix regression
    ndi: support for NDI SDK v6
    Various bug fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.14 2024/06/04 15:09:50 adam Exp $
+# $NetBSD: version.mk,v 1.15 2024/07/01 14:04:46 adam Exp $
 
-GST_VERSION=	1.24.4
+GST_VERSION=	1.24.5
 # if ${PATCH} is < 10
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 #else
