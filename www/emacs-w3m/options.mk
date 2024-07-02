e69b4423d400442538eb02237fec98a563f72db0
mef <mef@pkgsrc.org>
2024-07-02 13:13:25 +0000

    (www/emacs-w3m) EMACS_VERSIONS_ACCEPTED+= emacs29. Adjust PLIST for w3m-load

--- www/emacs-w3m/options.mk
+++ www/emacs-w3m/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2016/12/12 13:53:10 hauke Exp $
+# $NetBSD: options.mk,v 1.4 2024/07/02 13:13:25 mef Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.emacs-w3m
 PKG_SUPPORTED_OPTIONS=	emacs-w3m-mew
@@ -9,7 +9,7 @@ PLIST_VARS+=	mew
 
 # xemacs-packages contain mew (but not mew-nntp)
 .if (${EMACS_FLAVOR} == "xemacs")
-PLIST.mew =	yes
+PLIST.mew=	yes
 .elif !empty(PKG_OPTIONS:Memacs-w3m-mew)
 .include "../../mail/mew/buildlink3.mk"
 PLIST.mew=	yes
