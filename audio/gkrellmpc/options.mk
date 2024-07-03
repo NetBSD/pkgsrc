572aee709c238c310126bc19dee4f7a5524059b6
cheusov <cheusov@pkgsrc.org>
2024-07-03 19:18:39 +0000

    Fix option "nls"

--- audio/gkrellmpc/options.mk
+++ audio/gkrellmpc/options.mk
@@ -1,13 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2021/11/27 10:37:24 pho Exp $
+# $NetBSD: options.mk,v 1.2 2024/07/03 19:18:39 cheusov Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gkrellmpc
 PKG_SUPPORTED_OPTIONS=	nls
 PKG_SUGGESTED_OPTIONS=	nls
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	nls
 .if !empty(PKG_OPTIONS:Mnls)
 USE_TOOLS+=	gmake msgfmt
 MAKE_FLAGS+=	enable_nls=1
+PLIST.nls=	yes
 post-install:
 	${GMAKE} -C "${WRKSRC}/po" \
 		${MAKE_FLAGS} \
