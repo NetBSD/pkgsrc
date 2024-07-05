723deaeb9eb4e6b40944371dc161c7a9c92611f6
riastradh <riastradh@pkgsrc.org>
2024-07-05 02:21:34 +0000

    filesystems/glusterfs: Fix paths in rc script.
    PR pkg/58400

--- filesystems/glusterfs/files/glusterd.sh
+++ filesystems/glusterfs/files/glusterd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: glusterd.sh,v 1.12 2014/11/18 14:38:15 manu Exp $
+# $NetBSD: glusterd.sh,v 1.13 2024/07/05 02:21:34 riastradh Exp $
 #
 
 # PROVIDE: glusterd
@@ -12,9 +12,9 @@ glusterd_precmd()
 {
 	fatal=0
 
-	for d in /var/lib/glusterd /usr/pkg/etc/glusterd ; do
-        	if test -d /var/lib/glusterd ; then
-			echo "Please migrate $d to /var/db/glusterd" >&2
+	for d in @VARBASE@/lib/glusterd @PKG_SYSCONFDIR@/glusterd ; do
+		if test -d "$d" ; then
+			echo "Please migrate $d to @VARBASE@/db/glusterd" >&2
 			fatal=1
 		fi
 	done
@@ -26,11 +26,11 @@ glusterd_precmd()
 
 name="glusterd"
 rcvar=$name
-command="/usr/pkg/sbin/${name}"
+command="@PREFIX@/sbin/${name}"
 #start_precmd="glusterd_precmd"
 pidfile="/var/run/${name}.pid"
 command_args="-p ${pidfile}"
-required_files="/usr/pkg/etc/glusterfs/${name}.vol"
+required_files="@PKG_SYSCONFDIR@/glusterfs/${name}.vol"
 
 load_rc_config $name
 run_rc_command "$1"
