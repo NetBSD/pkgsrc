d43de9b9df4fef1ee45c3862f9af34ae5650e104
maya <maya@pkgsrc.org>
2024-07-05 20:25:24 +0000

    Add protobuf-go version 1.34.1
    Go support for Protocol Buffers
    
    This project hosts the Go implementation for
    [protocol buffers](https://protobuf.dev), which is a
    language-neutral, platform-neutral, extensible mechanism for serializing
    structured data. The protocol buffer language is a language for specifying the
    schema for structured data. This schema is compiled into language specific
    bindings. This project provides both a tool to generate Go code for the
    protocol buffer language, and also the runtime implementation to handle
    serialization of messages in Go. See the
    [protocol buffer developer guide](https://protobuf.dev/overview)
    for more information about protocol buffers themselves.

---
+++ devel/protobuf-go/go-modules.mk
@@ -0,0 +1,9 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/07/05 20:25:24 maya Exp $
+
+GO_MODULE_FILES+=	github.com/golang/protobuf/@v/v1.5.0.mod
+GO_MODULE_FILES+=	github.com/golang/protobuf/@v/v1.5.0.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.5.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.5.zip
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191204190536-9bdfabe68543.mod
+GO_MODULE_FILES+=	golang.org/x/xerrors/@v/v0.0.0-20191204190536-9bdfabe68543.zip
+GO_MODULE_FILES+=	google.golang.org/protobuf/@v/v1.26.0-rc.1.mod
