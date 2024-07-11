e035af04da2e44865646674c38d165770ef62cc5
wiz <wiz@pkgsrc.org>
2024-07-11 19:53:33 +0000

    *: bump for raqm option in gd & ImageMagick
    to be on the safe side

--- graphics/gdchart/buildlink3.mk
+++ graphics/gdchart/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2023/11/12 13:21:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2024/07/11 19:53:36 wiz Exp $
 
 BUILDLINK_TREE+=	gdchart
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdchart
 GDCHART_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdchart+=	gdchart>=0.11.4nb1
-BUILDLINK_ABI_DEPENDS.gdchart+=	gdchart>=0.11.5nb25
+BUILDLINK_ABI_DEPENDS.gdchart+=	gdchart>=0.11.5nb26
 BUILDLINK_PKGSRCDIR.gdchart?=	../../graphics/gdchart
 
 .include "../../graphics/gd/buildlink3.mk"
