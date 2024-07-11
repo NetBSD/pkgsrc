9f2d869d515652cb0a1a12e2daacc93bb686867b
wiz <wiz@pkgsrc.org>
2024-07-11 19:43:10 +0000

    raqm: fix buildlink3.mk

--- graphics/raqm/buildlink3.mk
+++ graphics/raqm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/07/11 17:18:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/07/11 19:43:10 wiz Exp $
 
 BUILDLINK_TREE+=	raqm
 
@@ -8,6 +8,9 @@ RAQM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.raqm+=	raqm>=0.10.1
 BUILDLINK_PKGSRCDIR.raqm?=	../../graphics/raqm
 
+.include "../../converters/fribidi/buildlink3.mk"
+.include "../../fonts/harfbuzz/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
 .endif	# RAQM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-raqm
