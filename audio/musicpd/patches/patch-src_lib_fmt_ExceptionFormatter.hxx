689c20e28c76bb38a8ddd1a7c58ecbe700d39969
mrg <mrg@pkgsrc.org>
2024-07-14 23:20:51 +0000

    fix the build since fmtlib 11 import.
    taken from upstream commit:
       https://github.com/MusicPlayerDaemon/MPD/commit/1402869715e3efca87942d79c3173a6b21a6925d
    
    lib/fmt: support build with libfmt-11.0.0
    
    Upstream libfmt commit fmtlib/fmt@d707292
    now requires the format function to be const.
    
    Adjust the function prototype so it is const and can compile.
    
    Signed-off-by: Rudi Heitbaum <rudi@heitbaum.com>

---
+++ audio/musicpd/patches/patch-src_lib_fmt_ExceptionFormatter.hxx
@@ -0,0 +1,11 @@
+--- src/lib/fmt/ExceptionFormatter.hxx.orig	2023-12-20 07:21:57.000000000 -0800
++++ src/lib/fmt/ExceptionFormatter.hxx	2024-07-14 16:13:04.360506262 -0700
+@@ -38,7 +38,7 @@
+ struct fmt::formatter<std::exception_ptr> : formatter<string_view>
+ {
+ 	template<typename FormatContext>
+-	auto format(std::exception_ptr e, FormatContext &ctx) {
++	auto format(std::exception_ptr e, FormatContext &ctx) const {
+ 		return formatter<string_view>::format(GetFullMessage(e), ctx);
+ 	}
+ };
