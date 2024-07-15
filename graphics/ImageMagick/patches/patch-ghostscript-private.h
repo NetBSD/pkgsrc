1a90e440c8b3c096698d405e9b8fd430f7dc34fa
triaxx <triaxx@pkgsrc.org>
2024-07-15 06:12:17 +0000

    ImageMagick: Update to 7.1.1.35

--- graphics/ImageMagick/patches/patch-ghostscript-private.h
+++ graphics/ImageMagick/patches/patch-ghostscript-private.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-ghostscript-private.h,v 1.1 2022/06/20 20:49:26 dholland Exp $
+$NetBSD: patch-ghostscript-private.h,v 1.2 2024/07/15 06:12:17 triaxx Exp $
 
 Add a workaround to allow building with ghostscript-gpl.
 
 --- coders/ghostscript-private.h~	2022-06-05 10:26:38.000000000 +0000
 +++ coders/ghostscript-private.h
-@@ -76,6 +76,15 @@ static inline MagickBooleanType ExecuteG
+@@ -77,6 +77,15 @@ static inline MagickBooleanType ExecuteG
    return(MagickFalse);
  }
  
