df36e71910c163fe3ead02ecf89daa83f4d8768a
schwarz <schwarz@pkgsrc.org>
2024-07-15 21:14:50 +0000

    Updated editors/nano to 8.1
    See ChangeLog for list of changes from 8.0 to 8.1

--- editors/nano/patches/patch-src_winio.c
+++ editors/nano/patches/patch-src_winio.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_winio.c,v 1.3 2024/05/11 20:14:27 schwarz Exp $
+$NetBSD: patch-src_winio.c,v 1.4 2024/07/15 21:14:50 schwarz Exp $
 
 NetBSD curses fix.
 
 https://gnats.netbsd.org/57749
 https://savannah.gnu.org/patch/index.php?10438
 
---- src/winio.c.orig	2024-05-01 10:25:05.000000000 +0200
-+++ src/winio.c	2024-05-11 22:01:46.000000000 +0200
-@@ -2534,10 +2534,11 @@
+--- src/winio.c.orig	2024-07-01 11:15:11.000000000 +0200
++++ src/winio.c	2024-07-15 23:03:13.000000000 +0200
+@@ -2531,10 +2531,11 @@
  		column -= get_page_start(column);
  	}
  
