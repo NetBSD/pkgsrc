6125ebbe7eeb5753f0011b51981c4c9824607b05
sjmulder <sjmulder@pkgsrc.org>
2024-07-15 22:36:52 +0000

    sysutils/nnn: Fix build on CentOS 7
    In glibc <2.23, fts.h doesn't have large file support, so don't enable it
    there.

--- sysutils/nnn/patches/patch-src_nnn.c
+++ sysutils/nnn/patches/patch-src_nnn.c
@@ -1,10 +1,31 @@
-$NetBSD: patch-src_nnn.c,v 1.5 2021/10/25 22:26:53 sjmulder Exp $
+$NetBSD: patch-src_nnn.c,v 1.6 2024/07/15 22:36:52 sjmulder Exp $
 
-dprintf() polyfill for Solaris
+ - dprintf() polyfill for Solaris.
+ - Disable large file support on old glibc where unsupported in fts.h.
+   https://github.com/jarun/nnn/pull/1910
 
---- src/nnn.c.orig	2021-09-28 23:10:13.183444540 +0000
+--- src/nnn.c.orig	2023-08-27 04:25:13.000000000 +0000
 +++ src/nnn.c
-@@ -117,6 +117,10 @@
+@@ -28,13 +28,16 @@
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  */
+ 
+-#define _FILE_OFFSET_BITS 64 /* Support large files on 32-bit glibc */
+-
+ #if defined(__linux__) || defined(MINGW) || defined(__MINGW32__) \
+ 	|| defined(__MINGW64__) || defined(__CYGWIN__)
+ #ifndef _GNU_SOURCE
+ #define _GNU_SOURCE
+ #endif
++#include <features.h>		/* __GLIBC__ etc */
++/* large file support on 32-bit glibc >= 2.23 where fts.h supports it */
++#if !defined(__GLIBC__) || __GLIBC__ > 2 || __GLIBC_MINOR__ >= 23
++#define _FILE_OFFSET_BITS 64
++#endif
+ #if defined(__linux__)
+ #include <sys/inotify.h>
+ #define LINUX_INOTIFY
+@@ -121,6 +124,10 @@
  #define alloca(size) __builtin_alloca(size)
  #endif
  
@@ -15,7 +36,7 @@ dprintf() polyfill for Solaris
  #include "nnn.h"
  #include "dbg.h"
  
-@@ -834,6 +838,25 @@ static void notify_fifo(bool force);
+@@ -854,6 +861,25 @@ static void notify_fifo(bool force);
  
  /* Functions */
  
