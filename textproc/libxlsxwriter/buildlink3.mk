0166909d9e27cfe068a6070d7a06c047346dece6
sjmulder <sjmulder@pkgsrc.org>
2024-07-15 19:39:43 +0000

    textproc/libxlsxwriter: Update to 1.1.7
    In addition to the below, a pkglint warning that we should += to
    BUILDLINK_ABI_DEPENDS instead of ?= was addressed.
    
    Changes:
    
    1.1.7
    - Bump `.so` version to 6. This was missed in the previous release.
    
    1.1.6
    - Updated vendored version of minizip to v 1.3.0 to include upstream fixes.
      Feature request [#419].
    - Added `quote_prefix` format property.
      Feature request [#385].
    - Added support for signed VBA projects.
    - Fix worksheet password hashing algorithm for long passwords. Replace/fix
      the worksheet protection password algorithm so that is works correctly
      for strings over 24 chars.
    - Fix cmake minizip version check.
      Feature request [#405].
    - Fix for buffer overflow with utf-8 strings in data validation.
      Feature request [#394].
    - Fix for range in text type conditional format.
      Feature request [#395].
    - Fix 32bit multiply with overflow issue for images.
      Fix multiply with overflow issue when image locations in the worksheet
      were greater than the u32 max value.
    - Added Swift package manager support.
    - Added support for building Zig library.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2023/10/24 22:11:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2024/07/15 19:39:43 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.4
-BUILDLINK_ABI_DEPENDS.libxlsxwriter?=	libxlsxwriter>=1.1.5nb2
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.7
+BUILDLINK_ABI_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.7
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../archivers/minizip/buildlink3.mk"
