7df96a0fa883524f7be544e75aaf60ebc830ba5e
adam <adam@pkgsrc.org>
2024-07-16 11:40:16 +0000

    portaudio: updated to 190700.20210406
    v19.7.0
    
    Stable release with:
    
    support for latest Mac OS APIs
    Jack - fixes for device names that allows use on Linux systems with PipeWire
    WASAPI - numerous improvements
    OSS - better format support
    ALSA - various fixes for hangs and crashes
    WDMKS - improved performance and fixes
    updated build scripts

--- audio/portaudio/buildlink3.mk
+++ audio/portaudio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2023/05/06 19:08:47 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2024/07/16 11:40:16 adam Exp $
 
 BUILDLINK_TREE+=	portaudio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	portaudio
 PORTAUDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.portaudio+=	portaudio>=19
-BUILDLINK_ABI_DEPENDS.portaudio?=		portaudio>=190600.20161030nb13
+BUILDLINK_ABI_DEPENDS.portaudio+=	portaudio>=190600.20161030nb13
 BUILDLINK_PKGSRCDIR.portaudio?=		../../audio/portaudio
 BUILDLINK_INCDIRS.portaudio?=		include/portaudio2
 BUILDLINK_LIBDIRS.portaudio+=		lib/portaudio2
