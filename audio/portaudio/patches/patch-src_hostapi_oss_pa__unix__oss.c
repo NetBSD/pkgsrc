1f48739ea61dde0e74f46ab771babe66f18ef772
adam <adam@pkgsrc.org>
2024-07-16 11:40:16 +0000

    portaudio: updated to 190700.20210406
    v19.7.0
    
    Stable release with:
    
    support for latest Mac OS APIs
    Jack - fixes for device names that allows use on Linux systems with PipeWire
    WASAPI - numerous improvements
    OSS - better format support
    ALSA - various fixes for hangs and crashes
    WDMKS - improved performance and fixes
    updated build scripts

--- audio/portaudio/patches/patch-src_hostapi_oss_pa__unix__oss.c
+++ audio/portaudio/patches/patch-src_hostapi_oss_pa__unix__oss.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp $
+$NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.6 2024/07/16 11:40:16 adam Exp $
 
 - Add 24-bit and 32-bit sample formats.
 - NetBSD-specific changes to make this map better to the emulation layer:
@@ -6,7 +6,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
   PaOssStream_WaitForFrames hangs forever unless disabled, this may be a bug.
 - Add features needed to make portmixer work with audacity.
 
---- src/hostapi/oss/pa_unix_oss.c.orig	2016-10-30 01:23:04.000000000 +0000
+--- src/hostapi/oss/pa_unix_oss.c.orig	2021-04-01 00:35:42.000000000 +0000
 +++ src/hostapi/oss/pa_unix_oss.c
 @@ -65,7 +65,7 @@
  
@@ -30,7 +30,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
          ENSURE_( ioctl( devHandle, SNDCTL_DSP_SPEED, &sr ), paUnanticipatedHostError );
  
          *defaultSampleRate = sr;
-@@ -756,7 +761,7 @@ static PaError PaOssStreamComponent_Init
+@@ -755,7 +760,7 @@ static PaError PaOssStreamComponent_Init
      component->devName = deviceName;
      component->userChannelCount = parameters->channelCount;
      component->userFormat = parameters->sampleFormat;
@@ -39,7 +39,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
      component->userInterleaved = !(parameters->sampleFormat & paNonInterleaved);
  
      if( !callbackMode && !component->userInterleaved )
-@@ -938,6 +943,16 @@ static PaError Pa2OssFormat( PaSampleFor
+@@ -937,6 +942,11 @@ static PaError Pa2OssFormat( PaSampleFor
          case paInt16:
              *ossFormat = AFMT_S16_NE;
              break;
@@ -48,15 +48,10 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
 +            *ossFormat = AFMT_S24_NE;
 +            break;
 +#endif
-+#ifdef AFMT_S32_NE
-+        case paInt32:
-+            *ossFormat = AFMT_S32_NE;
-+            break;
-+#endif
-         default:
-             return paInternalError;     /* This shouldn't happen */
-     }
-@@ -961,6 +976,14 @@ static PaError GetAvailableFormats( PaOs
+ #ifdef AFMT_S32_NE
+         case paInt32:
+             *ossFormat = AFMT_S32_NE;
+@@ -965,6 +975,10 @@ static PaError GetAvailableFormats( PaOs
          frmts |= paInt8;
      if( mask & AFMT_S16_NE )
          frmts |= paInt16;
@@ -64,14 +59,10 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
 +    if( mask & AFMT_S24_NE )
 +        frmts |= paInt24;
 +#endif
-+#ifdef AFMT_S32_NE
-+    if( mask & AFMT_S32_NE )
-+        frmts |= paInt32;
-+#endif
-     else
-         result = paSampleFormatNotSupported;
- 
-@@ -1040,12 +1063,23 @@ static PaError PaOssStreamComponent_Conf
+ #ifdef AFMT_S32_NE
+     if( mask & AFMT_S32_NE )
+         frmts |= paInt32;
+@@ -1048,12 +1062,23 @@ static PaError PaOssStreamComponent_Conf
          PA_ENSURE( Pa2OssFormat( hostFormat, &temp ) );
          nativeFormat = temp;
          ENSURE_( ioctl( component->fd, SNDCTL_DSP_SETFMT, &temp ), paUnanticipatedHostError );
@@ -95,7 +86,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
  
          /* try to set the sample rate */
          ENSURE_( ioctl( component->fd, SNDCTL_DSP_SPEED, &sr ), paInvalidSampleRate );
-@@ -1477,6 +1511,7 @@ static PaError PaOssStream_Prepare( PaOs
+@@ -1485,6 +1510,7 @@ static PaError PaOssStream_Prepare( PaOs
          size_t bufSz = PaOssStreamComponent_BufferSize( stream->playback );
          memset( stream->playback->buffer, 0, bufSz );
  
@@ -103,7 +94,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
          /* Looks like we have to turn off blocking before we try this, but if we don't fill the buffer
           * OSS will complain. */
          PA_ENSURE( ModifyBlocking( stream->playback->fd, 0 ) );
-@@ -1486,6 +1521,7 @@ static PaError PaOssStream_Prepare( PaOs
+@@ -1494,6 +1520,7 @@ static PaError PaOssStream_Prepare( PaOs
                  break;
          }
          PA_ENSURE( ModifyBlocking( stream->playback->fd, 1 ) );
@@ -111,7 +102,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
      }
  
      if( stream->sharedDevice )
-@@ -1652,6 +1688,7 @@ static void *PaOSS_AudioThreadProc( void
+@@ -1660,6 +1687,7 @@ static void *PaOSS_AudioThreadProc( void
              callbackResult = paComplete;
          }
  
@@ -119,7 +110,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
          /* Aspect StreamState: Because of the messy OSS scheme we can't explicitly trigger device start unless
           * the stream has been recently started, we will have to go right ahead and read/write in blocking
           * fashion to trigger operation. Therefore we begin with processing one host buffer before we switch
-@@ -1667,6 +1704,9 @@ static void *PaOSS_AudioThreadProc( void
+@@ -1675,6 +1703,9 @@ static void *PaOSS_AudioThreadProc( void
          {
              framesAvail = stream->framesPerHostBuffer;
          }
@@ -129,7 +120,7 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
  
          while( framesAvail > 0 )
          {
-@@ -1749,11 +1789,13 @@ static void *PaOSS_AudioThreadProc( void
+@@ -1757,11 +1788,13 @@ static void *PaOSS_AudioThreadProc( void
  
          if( initiateProcessing || !triggered )
          {
@@ -143,8 +134,8 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
  
              initiateProcessing = 0;
              sem_post( &stream->semaphore );
-@@ -2043,3 +2085,26 @@ error:
- #endif
+@@ -2032,6 +2065,29 @@ error:
+     return result;
  }
  
 +const char *PaOSS_GetStreamInputDevice( PaStream* s )
@@ -170,3 +161,6 @@ $NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.5 2022/06/14 14:18:45 nia Exp
 +
 +   return NULL;
 +}
+ 
+ /* TODO: Compute number of allocated bytes somewhere else, can we use ODELAY with capture */
+ static signed long GetStreamWriteAvailable( PaStream* s )
