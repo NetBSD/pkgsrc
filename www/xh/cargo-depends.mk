e4a2d6909946483fa424fb4dd66151edb632e45a
pin <pin@pkgsrc.org>
2024-07-19 09:58:32 +0000

    www/xh: update to 0.22.2
    [0.22.2] - 2024-07-08
    Security fixes
     - Prevent directory traversal in server-supplied filenames, see #379 (@blyxxyz)

--- www/xh/cargo-depends.mk
+++ www/xh/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.7 2024/04/15 07:48:49 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.8 2024/07/19 09:58:32 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.21.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
