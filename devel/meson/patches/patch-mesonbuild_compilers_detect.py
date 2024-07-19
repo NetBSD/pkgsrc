29e7421562e0006299d783c86710f0f1de9d3e31
adam <adam@pkgsrc.org>
2024-07-19 07:26:37 +0000

    meson: updates to 1.5.0
    1.5.0
    
    Support for bztar in meson dist
    Cargo dependencies names now include the API version
    Added support Cargo.lock file
    Meson now propagates its build type to CMake
    compiler.run() method is now available for all languages
    dependencies created by compiler.find_library implement the name() method
    New version_argument kwarg for find_program
    Meson configure handles changes to options in more cases
    New meson format command
    Added support for GCC's null_terminated_string_arg function attribute
    A new dependency for ObjFW is now supported
    Support of indexed @PLAINNAME@ and @BASENAME@
    Required kwarg on more compiler methods
    The Meson test program supports a new "--interactive" argument
    meson test now sets the MESON_TEST_ITERATION environment variable
    The Meson test program supports a new "--max-lines" argument
    Basic support for TI Arm Clang (tiarmclang)
    Support for Texas Instruments C6000 C/C++ compiler
    Wayland stable protocols can be versioned

--- devel/meson/patches/patch-mesonbuild_compilers_detect.py
+++ devel/meson/patches/patch-mesonbuild_compilers_detect.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-mesonbuild_compilers_detect.py,v 1.5 2023/10/03 01:21:36 gutteridge Exp $
+$NetBSD: patch-mesonbuild_compilers_detect.py,v 1.6 2024/07/19 07:26:38 adam Exp $
 
 Specify c++ language when making CXX parse standard input.
 Fixes problem where graphics/MesaLib fails to build due to:
@@ -8,9 +8,9 @@ Fixes problem where graphics/MesaLib fails to build due to:
 
 Also account for multiple versions of cython in pkgsrc.
 
---- mesonbuild/compilers/detect.py.orig	2023-07-21 20:36:13.000000000 +0000
+--- mesonbuild/compilers/detect.py.orig	2024-06-24 16:26:41.000000000 +0000
 +++ mesonbuild/compilers/detect.py
-@@ -80,7 +80,7 @@ defaults['cuda'] = ['nvcc']
+@@ -68,7 +68,7 @@ defaults['cuda'] = ['nvcc']
  defaults['rust'] = ['rustc']
  defaults['swift'] = ['swiftc']
  defaults['vala'] = ['valac']
@@ -19,27 +19,27 @@ Also account for multiple versions of cython in pkgsrc.
  defaults['static_linker'] = ['ar', 'gar']
  defaults['strip'] = ['strip']
  defaults['vs_static_linker'] = ['lib']
-@@ -1318,7 +1318,10 @@ def _get_gnu_compiler_defines(compiler: 
-     """
+@@ -1346,7 +1346,10 @@ def _get_gnu_compiler_defines(compiler:
+ 
      # Arguments to output compiler pre-processor defines to stdout
      # gcc, g++, and gfortran all support these arguments
--    args = compiler + ['-E', '-dM', '-']
-+    args = compiler + ['-E', '-dM']
+-    baseline_test_args = ['-E', '-dM', '-']
++    baseline_test_args = ['-E', '-dM']
 +    if compiler[0].endswith('++'):
-+        args += ['-x', 'c++']
-+    args += ['-']
-     mlog.debug(f'Running command: {join_args(args)}')
-     p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
-     if p.returncode != 0:
-@@ -1345,7 +1348,10 @@ def _get_clang_compiler_defines(compiler
-     """
-     Get the list of Clang pre-processor defines
-     """
--    args = compiler + ['-E', '-dM', '-']
-+    args = compiler + ['-E', '-dM']
++        baseline_test_args += ['-x', 'c++']
++    baseline_test_args += ['-']
+     try:
+         # We assume that when _get_gnu_compiler_defines is called, it's
+         # close enough to a GCCish compiler so we reuse the _LANG_MAP
+@@ -1398,7 +1401,10 @@ def _get_clang_compiler_defines(compiler
+         return output
+ 
+     # Arguments to output compiler pre-processor defines to stdout
+-    baseline_test_args = ['-E', '-dM', '-']
++    baseline_test_args = ['-E', '-dM']
 +    if compiler[0].endswith('++'):
-+        args += ['-x', 'c++']
-+    args += ['-']
-     mlog.debug(f'Running command: {join_args(args)}')
-     p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
-     if p.returncode != 0:
++        baseline_test_args += ['-x', 'c++']
++    baseline_test_args += ['-']
+     try:
+         # We assume that when _get_clang_compiler_defines is called, it's
+         # close enough to a Clangish compiler so we reuse the _LANG_MAP
