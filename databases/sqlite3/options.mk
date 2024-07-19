88372744310974c4e48245688d45edb4fd87b725
riastradh <riastradh@pkgsrc.org>
2024-07-19 17:11:54 +0000

    databases/sqlite3: New sqlite3-dbstat option.
    Enables the DBSTAT virtual table:
    
    https://sqlite.org/dbstat.html
    
    No revbump because option is default-off for now.
    
    PR pkg/58448

--- databases/sqlite3/options.mk
+++ databases/sqlite3/options.mk
@@ -1,10 +1,14 @@
-# $NetBSD: options.mk,v 1.7 2023/02/20 07:28:31 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2024/07/19 17:11:54 riastradh Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sqlite3
-PKG_SUPPORTED_OPTIONS=		fts icu deserialize
+PKG_SUPPORTED_OPTIONS=		sqlite3-dbstat fts icu deserialize
 PKG_SUGGESTED_OPTIONS=		fts # enabled in NetBSD builtin version
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Msqlite3-dbstat)
+CFLAGS+=	-DSQLITE_ENABLE_DBSTAT_VTAB=1
+.endif
 
 .if !empty(PKG_OPTIONS:Mfts)
 CFLAGS+=	-DSQLITE_ENABLE_FTS4=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS=1
