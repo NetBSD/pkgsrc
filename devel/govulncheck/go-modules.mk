3ca44a96ae3c9343fce4d456589a5d9625007103
wiz <wiz@pkgsrc.org>
2024-07-21 08:57:20 +0000

    govulncheck: update to 1.1.3.
    Changes not found.

--- devel/govulncheck/go-modules.mk
+++ devel/govulncheck/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.4 2024/06/17 09:21:35 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.5 2024/07/21 08:57:20 wiz Exp $
 
 GO_MODULE_FILES+=	github.com/google/go-cmdtest/@v/v0.4.1-0.20220921163831-55ab3332a786.mod
 GO_MODULE_FILES+=	github.com/google/go-cmdtest/@v/v0.4.1-0.20220921163831-55ab3332a786.zip
@@ -7,13 +7,13 @@ GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
 GO_MODULE_FILES+=	github.com/google/renameio/@v/v0.1.0.mod
 GO_MODULE_FILES+=	github.com/google/renameio/@v/v0.1.0.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.18.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.18.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.19.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.19.0.zip
 GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.7.0.mod
 GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.7.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.22.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.22.0.zip
 GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240522233618-39ace7a40ae7.mod
 GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240522233618-39ace7a40ae7.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.23.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.23.0.zip
