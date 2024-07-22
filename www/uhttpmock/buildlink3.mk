c932191ef93e7712208efd7b102cd3dbd1c4a075
adam <adam@pkgsrc.org>
2024-07-22 20:38:12 +0000

    uhttpmock: updated to 0.11.0
    Overview of changes from uhttpmock 0.10.0 to uhttpmock 0.11.0
    
    * Bugs fixed:
      - Dump hosts during record (Jan-Michael Brummer)
      - Add Jan-Michael Brummer to doap file
      - Add HTTP/2 support
      - Add Location field support
      - Fix memory leak in uhm_message_finalize
    
    
    Overview of changes from uhttpmock 0.9.0 to uhttpmock 0.10.0
    
    * Bugs fixed:
      - Add PATCH request method
      - Add CONNECT request method

--- www/uhttpmock/buildlink3.mk
+++ www/uhttpmock/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2023/11/08 13:21:25 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2024/07/22 20:38:12 adam Exp $
 
 BUILDLINK_TREE+=	uhttpmock
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	uhttpmock
 UHTTPMOCK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.uhttpmock+=	uhttpmock>=0.4.0
-BUILDLINK_ABI_DEPENDS.uhttpmock?=	uhttpmock>=0.5.3nb4
+BUILDLINK_ABI_DEPENDS.uhttpmock+=	uhttpmock>=0.5.3nb4
 BUILDLINK_PKGSRCDIR.uhttpmock?=		../../www/uhttpmock
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
+.include "../../net/libsoup3/buildlink3.mk"
 .endif	# UHTTPMOCK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-uhttpmock
