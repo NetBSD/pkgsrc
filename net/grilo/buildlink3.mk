2428f8361b1ee921f59317cb3b34a07dedd380ad
adam <adam@pkgsrc.org>
2024-07-22 20:49:18 +0000

    grilo: updated to 0.3.16
    NEW in 0.3.16
    
    * Fix usage of depcreated GSimpleAsyncResult
    * Build updates. Meson requirement is now 0.62.
    * Fix build on Darwin
    
    * Updated translations
      * Turkish
      * Georgian
    
    
    NEW in 0.3.15
    
    * Increase glib requirement to 2.66 so we can use GUri
    * net: add libsoup3 support with compile-time option
    * net: Add documentation on throttling property in libsoup3
    * Build fixes and cleanups
    
    * New translations
      * Abkhazian
      * Georgian
      * Persian
    
    * Updated translations
      * Hebrew
      * Bulgarian
      * Nepali
    
    
    NEW in 0.3.14
    
    * CVE-2016-20011: Fix TLS cert validation not being done for any network call
    * Fix double-free when using GrlNet in Python
    * Load config from GRL_CONFIG_PATH if set
    * Clarify LGPLv2.1 or later license
    * Handle numeric limits for GrlOperationOptions
    
    * New translations
      * Belarusian
    
    * Updated translations
      * Portuguese
      * Norwegian Bokmål
      * Galician
      * Russian
      * Finnish
    
    
    NEW in 0.3.13
    
    * Increase glib requirement to 2.58 or greater
    * API addition: grl_related_keys_set_for_id()
    * Fix trying to load .so.p files as plugins
    * Fix crash using get_plugins() from bindings
    * Several other fixes and cleanups
    
    * Updated translations
      * Chinese (Taiwan)
      * Japanese
      * Ukrainian
    
    
    NEW in 0.3.12
    
    * grl-source: Fix reference count in the bindings
    
    * Updated translations
      * Japanese
      * British English

--- net/grilo/buildlink3.mk
+++ net/grilo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.40 2024/05/16 06:15:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.41 2024/07/22 20:49:18 adam Exp $
 
 BUILDLINK_TREE+=	grilo
 
@@ -6,14 +6,13 @@ BUILDLINK_TREE+=	grilo
 GRILO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.grilo+=	grilo>=0.3
-BUILDLINK_ABI_DEPENDS.grilo?=	grilo>=0.3.11nb29
+BUILDLINK_ABI_DEPENDS.grilo+=	grilo>=0.3.11nb29
 BUILDLINK_PKGSRCDIR.grilo?=	../../net/grilo
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../multimedia/totem-pl-parser/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
-.include "../../security/liboauth/buildlink3.mk"
+.include "../../net/libsoup3/buildlink3.mk"
 .endif	# GRILO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-grilo
