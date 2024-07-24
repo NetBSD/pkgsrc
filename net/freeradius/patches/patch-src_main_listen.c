52919564cbf28521fe5bddf23da979375f8cb1d5
manu <manu@pkgsrc.org>
2024-07-24 14:28:05 +0000

    Update freeradius to 3.2.5, with mitigations for BlastRADIUS
    FreeRADIUS 3.2.5 Tue 09 Jul 2024 12:00:00 UTC urgency=high
            Configuration changes
            * BlastRADIUS mitigations have been added to the "security"
              section.  See "require_message_authenticator" and also
              "limit_proxy_state".
            * BlastRADIUS mitigations have been added to radclient.
              See "man radclient", and the "-b" option.
    
            Feature improvements
            * TOTP now supports TOTP-Time-Offset for tokens with times that
              are out of sync.  See mods-available/totp
            * radclient now supports forcing the Request Authenticator and ID
              for Access-Request packets.
            * Update dictionary.3gpp.
            * Update advice on shared secrets, including suggesting a secure
              method for generating useful secrets.
    
            Bug fixes
            * Allow proxying by pool / home server name to work with auth+acct servers
            * Fix OpenSSL API usage which sometimes caused crash in MS-CHAP
              Previously it would either always crash immediately, or never crash.
            * Fix packet statistics.  Stop double counting some packets,
              and track packet statistics even if a socket is closed.
            * Reverted patch in TTLS which broke compatibility with some systems.
            * Don't crash in debug mode when multiple intermediate certs are used
              Patch from Alexander Chernikov.

--- net/freeradius/patches/patch-src_main_listen.c
+++ net/freeradius/patches/patch-src_main_listen.c
@@ -1,15 +1,17 @@
-$NetBSD: patch-src_main_listen.c,v 1.2 2024/06/19 08:46:33 adam Exp $
+$NetBSD: patch-src_main_listen.c,v 1.3 2024/07/24 14:28:05 manu Exp $
 
 Fix missing SOL_TCP for other platforms.
 
---- src/main/listen.c.orig	2024-05-29 16:18:18.000000000 +0000
-+++ src/main/listen.c
-@@ -55,7 +55,7 @@ RCSID("$Id: dbb2167e28189720df35cbf677ff
+--- src/main/listen.c.orig	2024-07-08 22:29:24.000000000 +0200
++++ src/main/listen.c	2024-07-19 15:39:10.917760544 +0200
+@@ -54,9 +54,9 @@
+ 
  #ifdef WITH_TLS
  #include <netinet/tcp.h>
  
 -#  if defined(__APPLE__) || defined(__FreeBSD__) || defined(__illumos__) || defined(__sun__)
-+#  if defined(__APPLE__) || defined(__FreeBSD__) || defined(__illumos__) || defined(__sun__) || defined(__NetBSD__)
++#  if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__illumos__) || defined(__sun__)
  #    if !defined(SOL_TCP) && defined(IPPROTO_TCP)
  #      define SOL_TCP IPPROTO_TCP
  #    endif
+ #  endif
