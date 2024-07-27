b7f94230ade7342b6a0275bab4b74814a61a25ff
tnn <tnn@pkgsrc.org>
2024-07-27 02:35:24 +0000

    rust: restore patch for pkgsrc libunwind on Linux

--- lang/rust/patches/patch-compiler_rustc__codegen__ssa_src_back_linker.rs
+++ lang/rust/patches/patch-compiler_rustc__codegen__ssa_src_back_linker.rs
@@ -1,6 +1,7 @@
-$NetBSD: patch-compiler_rustc__codegen__ssa_src_back_linker.rs,v 1.16 2024/07/15 03:32:23 wiz Exp $
+$NetBSD: patch-compiler_rustc__codegen__ssa_src_back_linker.rs,v 1.17 2024/07/27 02:35:24 tnn Exp $
 
 Do not use @rpath on Darwin.
+Find external libunwind on Linux.
 
 --- compiler/rustc_codegen_ssa/src/back/linker.rs.orig	2006-07-24 01:21:28.000000000 +0000
 +++ compiler/rustc_codegen_ssa/src/back/linker.rs
@@ -13,3 +14,14 @@ Do not use @rpath on Darwin.
                  rpath.push(out_filename.file_name().unwrap());
                  self.linker_args(&[OsString::from("-install_name"), rpath]);
              }
+@@ -462,6 +462,10 @@ impl<'a> Linker for GccLinker<'a> {
+                 self.linker_arg("--as-needed");
+             }
+         }
++        if self.sess.target.os.contains("linux") && name == "unwind" {
++            self.linker_arg("-rpath,@PREFIX@/lib");
++            self.linker_arg("-L@PREFIX@/lib");
++        }
+     }
+ 
+     fn link_framework_by_name(&mut self, name: &str, _verbatim: bool, as_needed: bool) {
--- lang/rust/patches/patch-src_bootstrap_src_core_builder.rs
+++ lang/rust/patches/patch-src_bootstrap_src_core_builder.rs
@@ -1,9 +1,22 @@
-$NetBSD: patch-src_bootstrap_src_core_builder.rs,v 1.4 2024/07/15 03:32:23 wiz Exp $
+$NetBSD: patch-src_bootstrap_src_core_builder.rs,v 1.5 2024/07/27 02:35:24 tnn Exp $
 
+Find external libunwind on Linux.
 Use @PREFIX@, not $ORIGIN in rpath.
 
---- src/bootstrap/src/core/builder.rs.orig	2024-07-07 12:16:58.347642901 +0000
+--- src/bootstrap/src/core/builder.rs.orig	2006-07-24 01:21:28.000000000 +0000
 +++ src/bootstrap/src/core/builder.rs
+@@ -2149,6 +2149,11 @@ impl<'a> Builder<'a> {
+             rustflags.arg(v);
+         });
+ 
++        // added for pkgsrc libunwind
++        if target.contains("linux") {
++            rustflags.arg("-Clink-args=-Wl,-rpath,@PREFIX@/lib,-L@PREFIX@/lib");
++        }
++
+         Cargo {
+             command: cargo,
+             compiler,
 @@ -2513,7 +2518,7 @@ impl Cargo {
                  Some(format!("-Wl,-rpath,@loader_path/../{libdir}"))
              } else if !target.is_windows() && !target.contains("aix") && !target.contains("xous") {
