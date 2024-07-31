cfaf002a007d53963e87a08cf3d0373687bd4c31
wiz <wiz@pkgsrc.org>
2024-07-31 17:10:10 +0000

    ecl: fix ffi option handling in buildlink3.mk

--- lang/ecl/buildlink3.mk
+++ lang/ecl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2015/12/09 21:00:47 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/07/31 17:10:10 wiz Exp $
 
 BUILDLINK_TREE+=	ecl
 
@@ -8,6 +8,13 @@ ECL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ecl+=	ecl>=16.0.0
 BUILDLINK_ABI_DEPENDS.ecl+=	ecl>=16.0.0
 BUILDLINK_PKGSRCDIR.ecl?=	../../lang/ecl
+
+pkgbase := ecl
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.ecl:Mffi}
+.include "../../devel/libffi/buildlink3.mk"
+.endif
 
 .include "../../devel/boehm-gc/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
