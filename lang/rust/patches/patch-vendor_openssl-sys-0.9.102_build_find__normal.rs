50565ecf40f8868d625c7148c4d58ec1fe55a086
he <he@pkgsrc.org>
2024-07-31 08:05:50 +0000

    lang/rust: pull over fixes to rust 1.79 from pkgsrc-wip:
    1) Turn off the use ot thread local storage on NetBSD/aarch64,
       due to NetBSD PR#58154 and our desire to support systems
       built before the fix was applied.
    2) Use the fixed 1.78.0 bootstrap for NetBSD/aarch64 (which
       is now built with the workaround from #1 applied).
    3) Reinstate the patches to the openssl-sys and openssl-src
       vendor crates, and their checksum fixes, taken from pkgsrc-wip
       rust 1.78.0 and adapted.
    4) Make the NetBSD/sparc64 target spec more similar to the other
       sparc64 targets.  Does not resolve the cross-build problem, sadly.

---
+++ lang/rust/patches/patch-vendor_openssl-sys-0.9.102_build_find__normal.rs
@@ -0,0 +1,30 @@
+$NetBSD: patch-vendor_openssl-sys-0.9.102_build_find__normal.rs,v 1.1 2024/07/31 08:05:51 he Exp $
+
+Add hack for Darwin when homebrew is installed.
+
+--- vendor/openssl-sys-0.9.102/build/find_normal.rs.orig	2006-07-24 01:21:28.000000000 +0000
++++ vendor/openssl-sys-0.9.102/build/find_normal.rs
+@@ -72,23 +72,6 @@ fn resolve_with_wellknown_location(dir: 
+ fn find_openssl_dir(target: &str) -> OsString {
+     let host = env::var("HOST").unwrap();
+ 
+-    if host == target && target.ends_with("-apple-darwin") {
+-        let homebrew_dir = match target {
+-            "aarch64-apple-darwin" => "/opt/homebrew",
+-            _ => "/usr/local",
+-        };
+-
+-        if let Some(dir) = resolve_with_wellknown_homebrew_location(homebrew_dir) {
+-            return dir.into();
+-        } else if let Some(dir) = resolve_with_wellknown_location("/opt/pkg") {
+-            // pkgsrc
+-            return dir.into();
+-        } else if let Some(dir) = resolve_with_wellknown_location("/opt/local") {
+-            // MacPorts
+-            return dir.into();
+-        }
+-    }
+-
+     try_pkg_config();
+     try_vcpkg();
+ 
