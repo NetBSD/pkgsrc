f6816304884c5ac1487bd285e26a1647da651b54
ryoon <ryoon@pkgsrc.org>
2024-08-01 15:41:09 +0000

    www/firefox: Update to 128.0.3
    * Fix build errors under NetBSD/i386 10 at least.
    
    Changelog:
    128.0.3:
    Fixed
    
      * Fixed an issue causing some sites to not load when connecting via HTTP/2. (
        Bug 1908161, Bug 1909666)
    
      * Fixed collapsed table rows not appearing when expected in some situations.
        (Bug 1907789)
    
      * Fixed the Windows on-screen keyboard potentially concealing the webpage
        when displayed. (Bug 1907766)
    
    128.0.2:
    Fixed
    
      * Fixed an audio echo in video calls on macOS under certain conditions. (Bug
        1908539)
    
      * Fixed an issue where the Adguard extension popup was not displaying. (Bug
        1906132)
    
      * Fixed an issue causing some screen readers to fail to read when navigating
        by character in rich text editors. (Bug 1905021)
    
      * Fixed visual glitches when dark mode is enabled in Windows ARM devices. (
        Bug 1897444)
    
      * Fixed an issue causing NTLM authentication failure. (Bug 1908115)
    
      * Fixed an issue where content displayed on mouseover was not captured in a
        screenshot. (Bug 1905468)
    
      * Various stability fixes.

--- www/firefox/patches/patch-config_makefiles_rust.mk
+++ www/firefox/patches/patch-config_makefiles_rust.mk
@@ -1,4 +1,4 @@
-$NetBSD: patch-config_makefiles_rust.mk,v 1.12 2023/07/07 13:47:10 ryoon Exp $
+$NetBSD: patch-config_makefiles_rust.mk,v 1.13 2024/08/01 15:41:09 ryoon Exp $
 
 NetBSD doesn't get along with parallel rust builds (it causes issues
 with ld.so) which are the default. Force -j1.
@@ -6,7 +6,7 @@ with ld.so) which are the default. Force -j1.
 Use less memory to fix build under NetBSD/i386.
 From: https://bugzilla.mozilla.org/show_bug.cgi?id=1644409
 
---- config/makefiles/rust.mk.orig	2023-06-22 21:19:03.000000000 +0000
+--- config/makefiles/rust.mk.orig	2024-07-04 17:04:12.000000000 +0000
 +++ config/makefiles/rust.mk
 @@ -60,6 +60,9 @@ ifdef MOZ_TSAN
  cargo_build_flags += -Zbuild-std=std,panic_abort
@@ -23,7 +23,7 @@ From: https://bugzilla.mozilla.org/show_bug.cgi?id=1644409
  ifndef MOZ_CODE_COVERAGE
  ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
 -cargo_rustc_flags += -Clto$(if $(filter full,$(MOZ_LTO_RUST_CROSS)),=fat)
-+cargo_rustc_flags += -Clto$(if $(filter full,$(MOZ_LTO_RUST_CROSS)),=thin)
++cargo_rustc_flags += -Clto=thin
  endif
  # We need -Cembed-bitcode=yes for all crates when using -Clto.
  RUSTFLAGS += -Cembed-bitcode=yes
