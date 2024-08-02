245228604aad3da84df5779f70eafe79b314795b
pin <pin@pkgsrc.org>
2024-08-02 16:57:16 +0000

    x11/liblxqt: update to 2.0.0
     - Ported to Qt6.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,20 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.29 2024/04/06 08:07:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2024/08/02 16:57:16 pin Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=1.4.0
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=1.4.0nb1
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=2.0.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../x11/qt5-qtx11extras/buildlink3.mk"
-.include "../../x11/kwindowsystem/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.include "../../x11/kf6-kwindowsystem/buildlink3.mk"
 .include "../../x11/libqtxdg/buildlink3.mk"
 .include "../../x11/libXScrnSaver/buildlink3.mk"
-.include "../../security/polkit-qt5/buildlink3.mk"
+.include "../../security/polkit-qt6/buildlink3.mk"
 .endif	# LIBLXQT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-liblxqt
