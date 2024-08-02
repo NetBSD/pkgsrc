fac8fb1fd090955e61218945708a5c66f84e9248
pin <pin@pkgsrc.org>
2024-08-02 17:04:18 +0000

    x11/qtermwidget: update to 2.0.1
    qtermwidget-2.0.1 / 2024-06-27
     - Fixed visual artifacts due to a mistake about conversion to std::wstring in Qt6 port.
    
    qtermwidget-2.0.0 (2024-05-17)
     - Ported to Qt6.
     - Used "new" signal/slot syntax.
     - Dropped `#define` for bulk timeouts.
     - Removed KDE `#include`s.
     - Dropped XKB related conditional compilation.
     - Assigned a parent to bulkTimers.
     - Removed unused Screen artifacts.
     - Added shortcuts for appscreen `default.keytab`.
     - Exposed wordCharacters property to QTermWidget API.
     - Fixed cursor positioning issues.
     - Removed `ChildProcessSetup` and directly called `onsetupChildProcess`.
     - Used PyQt6.
     - Fixed deprecated function warning.
     - Removed anchored pattern for email and url.
     - Adapted to Qt6 `QProcess::setChildProcessModifier()`.
     - Used std library find algorithm.
     - Fixed comparison of pointer addition with NULL.

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.10 2023/11/10 17:02:28 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2024/08/02 17:04:18 pin Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=1.4.0
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=2.0.0
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
-#.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# QTERMWIDGET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-qtermwidget
