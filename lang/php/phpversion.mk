2a73b053a69fd1fc6352cb487172ca9d5eff90d7
taca <taca@pkgsrc.org>
2024-08-02 15:33:10 +0000

    lang/php82: update to 8.2.22
    8.2.22 (2024-08-01)
    
    - Core:
      . Fixed bug GH-13922 (Fixed support for systems with
        sysconf(_SC_GETPW_R_SIZE_MAX) == -1). (Arnaud)
      . Fixed bug GH-14626 (Fix is_zend_ptr() for huge blocks). (Arnaud)
      . Fixed bug GH-14590 (Memory leak in FPM test gh13563-conf-bool-env.phpt.
        (nielsdos)
      . Fixed OSS-Fuzz #69765. (nielsdos)
      . Fixed bug GH-14741 (Segmentation fault in Zend/zend_types.h). (nielsdos)
      . Fixed bug GH-14969 (Use-after-free in property coercion with __toString()).
        (ilutov)
    
    - Dom:
      . Fixed bug GH-14702 (DOMDocument::xinclude() crash). (nielsdos)
    
    - Gd:
      . ext/gd/tests/gh10614.phpt: skip if no PNG support. (orlitzky)
      . restored warning instead of fata error. (dryabov)
    
    - LibXML:
      . Fixed bug GH-14563 (Build failure with libxml2 v2.13.0). (nielsdos)
    
    - Opcache:
      . Fixed bug GH-14550 (No warning message when Zend DTrace is enabled that
        opcache.jit is implictly disabled). (nielsdos)
    
    - Output:
      . Fixed bug GH-14808 (Unexpected null pointer in Zend/zend_string.h with
        empty output buffer). (nielsdos)
    
    - PDO:
      . Fixed bug GH-14712 (Crash with PDORow access to null property).
        (David Carlier)
    
    - Phar:
      . Fixed bug GH-14603 (null string from zip entry).
        (David Carlier)
    
    - PHPDBG:
      . Fixed bug GH-14596 (crashes with ASAN and ZEND_RC_DEBUG=1).
        (David Carlier)
      . Fixed bug GH-14553 (echo output trimmed at NULL byte). (nielsdos)
    
    - Shmop:
       . Fixed bug GH-14537 (shmop Windows 11 crashes the process). (nielsdos)
    
    - SimpleXML:
       . Fixed bug GH-14638 (null dereference after XML parsing failure).
         (David Carlier)
    
    - SPL:
      . Fixed bug GH-14639 (Member access within null pointer in
        ext/spl/spl_observer.c). (nielsdos)
    
    - Standard:
      . Fix 32-bit wordwrap test failures. (orlitzky)
      . Fixed bug GH-14774 (time_sleep_until overflow). (David Carlier)
    
    - Tidy:
      . Fix memory leak in tidy_repair_file(). (nielsdos)
    
    - Treewide:
      . Fix compatibility with libxml2 2.13.2. (nielsdos)
    
    - XML:
      . Move away from to-be-deprecated libxml fields. (nielsdos)
      . Fixed bug GH-14834 (Error installing PHP when --with-pear is used).
        (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.438 2024/08/02 15:29:30 taca Exp $
+# $NetBSD: phpversion.mk,v 1.439 2024/08/02 15:33:10 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.29
-PHP82_VERSION=	8.2.21
+PHP82_VERSION=	8.2.22
 PHP83_VERSION=	8.3.10
 
 # Define API version or initial release of major version.
