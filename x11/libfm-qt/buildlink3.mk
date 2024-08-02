da00ee92e6c5350d1292f0d9399afbdc422b06d5
pin <pin@pkgsrc.org>
2024-08-02 17:00:25 +0000

    x11/libfm-qt: update to 2.0.2
    libfm-qt-2.0.2 (2024-05-08)
     - Fixed a regression in running executable files in terminal.
    
    libfm-qt-2.0.1 (2024-05-04)
     - Removed a redundant child-parent relation in pathbar to prevent a rare crash
       with Qt ≥ 6.7.
    
    libfm-qt-2.0.0 (2024-04-17)
     - Ported to Qt6.
     - Added lxqt-menu-data dependency to `README.md`.
     - Updated mime types of lxqt-archiver and arqiver.
     - Added 4 menu icons.
     - Workaround for Qt bug in a context menu invoked by Menu key.

--- x11/libfm-qt/buildlink3.mk
+++ x11/libfm-qt/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.29 2024/04/06 08:07:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2024/08/02 17:00:25 pin Exp $
 
 BUILDLINK_TREE+=	libfm-qt
 
 .if !defined(LIBFM_QT_BUILDLINK3_MK)
 LIBFM_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=1.4.0
-BUILDLINK_ABI_DEPENDS.libfm-qt?=		libfm-qt>=1.4.0nb1
+BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=2.0.2
 BUILDLINK_PKGSRCDIR.libfm-qt?=		../../x11/libfm-qt
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .include "../../graphics/libexif/buildlink3.mk"
 .include "../../sysutils/menu-cache/buildlink3.mk"
 .endif	# LIBFM_QT_BUILDLINK3_MK
