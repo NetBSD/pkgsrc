1cf2ed5b19358364d30b278e0a887808d2614853
adam <adam@pkgsrc.org>
2024-08-03 05:53:56 +0000

    cmake: updated to 3.30.2
    CMake 3.30.2
    * ci: Update CMake version to 3.30.1
    * Xcode: Restore support for CMAKE_OSX_ARCHITECTURES=$(ARCHS_STANDARD)
    * Help: Fix typo in add_test documentation
    * Tests/RunCMake/file-DOWNLOAD: Update bad hostname message for curl 8.9
    * Tests: Restore suppression of xcodebuild error with Xcode Command-Line Tools
    * ExternalProject: Don't add SVN auth args if not set
    * ExternalProject: Fix bad indenting for uses_terminal
    * FetchContent: Ensure can_fetch_default is set early enough
    * Swift: Fix INSTALL_NAME_DIR under CMP0157 NEW behavior
    * FindPython: Fix usage of cached variable _Python_EXECUTABLE_DEBUG
    * bootstrap: Restore support for system jsoncpp and uv without pkg-config

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.63 2024/07/19 16:37:14 adam Exp $
+# $NetBSD: version.mk,v 1.64 2024/08/03 05:53:56 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.30.1
+CMAKE_VERSION=	3.30.2
 CMAKE_API=	${CMAKE_VERSION:R}
