6b6bd6f913e3f1e338f9d83af7466b58f4420ef4
wiz <wiz@pkgsrc.org>
2024-08-04 15:56:01 +0000

    libkate: fix PLIST when doc option is enabled
    From Robert Whitlock in PR 58451.
    
    While here, add doxygen to tool dependencies for doc option so it works
    correctly.

--- multimedia/libkate/options.mk
+++ multimedia/libkate/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.12 2023/06/26 11:56:22 wiz Exp $
+# $NetBSD: options.mk,v 1.13 2024/08/04 15:56:01 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libkate
 PKG_SUPPORTED_OPTIONS=	debug doc oggz valgrind vorbis
@@ -13,6 +13,7 @@ CONFIGURE_ARGS+=	--enable-debug
 
 .if !empty(PKG_OPTIONS:Mdoc)
 PLIST.doc=		yes
+TOOL_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
 .else
 CONFIGURE_ARGS+=	--disable-doc
 .endif
