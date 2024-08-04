7a442dd51e5c449a5fee54b85f1daf5851a5685d
pin <pin@pkgsrc.org>
2024-08-04 14:49:42 +0000

    net/yaydl: update to 0.15.0
     - Supports setting the preferred Invidious instance via environment variables now. Closes #19.

--- net/yaydl/cargo-depends.mk
+++ net/yaydl/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.14 2024/07/29 07:31:32 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.15 2024/08/04 14:49:42 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.22.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
