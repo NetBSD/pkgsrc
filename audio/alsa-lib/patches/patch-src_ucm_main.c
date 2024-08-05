b551058cabffc0f8f81c32e1aff433c1ceb7a1cb
tnn <tnn@pkgsrc.org>
2024-08-05 11:55:10 +0000

    alsa-lib: fix pkgsrc patch; don't override fstat64 on Linux

--- audio/alsa-lib/patches/patch-src_ucm_main.c
+++ audio/alsa-lib/patches/patch-src_ucm_main.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_ucm_main.c,v 1.1 2022/07/15 21:17:25 wiz Exp $
+$NetBSD: patch-src_ucm_main.c,v 1.2 2024/08/05 11:55:10 tnn Exp $
 
 fstat64 does not exist everywhere, fall back to fstat
 
---- src/ucm/main.c.orig	2022-06-17 09:42:05.000000000 +0000
+--- src/ucm/main.c.orig	2024-06-10 09:18:34.000000000 +0000
 +++ src/ucm/main.c
 @@ -176,6 +176,9 @@ static int read_tlv_file(unsigned int **
  		err = -errno;
  		return err;
  	}
-+#ifndef fstat64
++#if !defined(fstat64) && !defined(__linux__)
 +#define fstat64 fstat
 +#endif
  	if (fstat64(fd, &st) == -1) {
