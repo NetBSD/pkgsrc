bdc6272523feb6eded644b9c9dfcff96c0d6a325
tnn <tnn@pkgsrc.org>
2024-08-06 01:34:40 +0000

    varnish: update to 7.5.0
    - fixes for DoS CVEs: CVE-2023-44487, CVE-2024-30156
    - new timeout options

--- www/varnish/patches/patch-include_tbl_params.h
+++ www/varnish/patches/patch-include_tbl_params.h
@@ -1,21 +1,21 @@
-$NetBSD: patch-include_tbl_params.h,v 1.1 2021/06/25 12:21:08 tnn Exp $
+$NetBSD: patch-include_tbl_params.h,v 1.2 2024/08/06 01:34:41 tnn Exp $
 
 Change some default limits to cover NetBSD defaults.
 
---- include/tbl/params.h.orig	2021-03-12 15:02:41.000000000 +0000
+--- include/tbl/params.h.orig	2024-03-18 13:37:59.000000000 +0000
 +++ include/tbl/params.h
-@@ -856,7 +856,7 @@ PARAM_SIMPLE(
+@@ -897,7 +897,7 @@ PARAM_SIMPLE(
  	/* name */	tcp_keepalive_intvl,
- 	/* type */	timeout,
+ 	/* type */	duration,
  	/* min */	"1",
 -	/* max */	"100",
 +	/* max */	"150",
  	/* def */	NULL,
  	/* units */	"seconds",
  	/* descr */
-@@ -889,7 +889,7 @@ PARAM_SIMPLE(
+@@ -930,7 +930,7 @@ PARAM_SIMPLE(
  	/* name */	tcp_keepalive_time,
- 	/* type */	timeout,
+ 	/* type */	duration,
  	/* min */	"1",
 -	/* max */	"7200",
 +	/* max */	"14400",
