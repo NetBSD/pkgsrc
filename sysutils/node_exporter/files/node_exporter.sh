428de59856b03149ecb4d80827b7770f8ead0c84
tnn <tnn@pkgsrc.org>
2024-08-06 02:27:53 +0000

    node_exporter: connect stdin/out/err to /dev/null in rc script

--- sysutils/node_exporter/files/node_exporter.sh
+++ sysutils/node_exporter/files/node_exporter.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: node_exporter.sh,v 1.1 2022/12/25 17:46:15 bsiegert Exp $
+# $NetBSD: node_exporter.sh,v 1.2 2024/08/06 02:27:53 tnn Exp $
 #
 # PROVIDE: node_exporter
 # REQUIRE: DAEMON
@@ -10,7 +10,7 @@
 name="node_exporter"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
-command_args=" &"
+command_args=" < /dev/null > /dev/null 2>&1 &"
 node_exporter_flags=${node_exporter_flags:-"--web.listen-address=:9100 --log.level=error"}
 
 load_rc_config $name
