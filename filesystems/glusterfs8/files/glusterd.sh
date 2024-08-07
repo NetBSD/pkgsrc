27c2110b7a0e5a5caf027c1fe8ffa46bf1d0673a
bsiegert <bsiegert@pkgsrc.org>
2024-08-07 18:47:39 +0000

    Pullup ticket #6885 - requested by riastradh filesystems/glusterfs: build fix -- Use @VARBASE@, @PREFIX@, &c., instead of /var, /usr/pkg, &c., in the rc script
    Revisions pulled up:
    - filesystems/glusterfs/Makefile                                1.123
    - filesystems/glusterfs/files/glusterd.sh                       1.13
    - filesystems/glusterfs8/Makefile                               1.7
    - filesystems/glusterfs8/files/glusterd.sh                      1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   riastradh
       Date:           Fri Jul  5 02:21:34 UTC 2024
    
       Modified Files:
               pkgsrc/filesystems/glusterfs: Makefile
               pkgsrc/filesystems/glusterfs/files: glusterd.sh
               pkgsrc/filesystems/glusterfs8: Makefile
               pkgsrc/filesystems/glusterfs8/files: glusterd.sh
    
       Log Message:
       filesystems/glusterfs: Fix paths in rc script.
    
       PR pkg/58400

--- filesystems/glusterfs8/files/glusterd.sh
+++ filesystems/glusterfs8/files/glusterd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: glusterd.sh,v 1.1 2023/03/12 11:55:59 js Exp $
+# $NetBSD: glusterd.sh,v 1.1.12.1 2024/08/07 18:47:39 bsiegert Exp $
 #
 
 # PROVIDE: glusterd
@@ -12,9 +12,9 @@ glusterd_precmd()
 {
 	fatal=0
 
-	for d in /var/lib/glusterd /usr/pkg/etc/glusterd ; do
-        	if test -d /var/lib/glusterd ; then
-			echo "Please migrate $d to /var/db/glusterd" >&2
+	for d in @VARBASE@/lib/glusterd @PKG_SYSCONFDIR@/glusterd ; do
+		if test -d "$d" ; then
+			echo "Please migrate $d to @VARBASE@/db/glusterd" >&2
 			fatal=1
 		fi
 	done
@@ -26,11 +26,11 @@ glusterd_precmd()
 
 name="glusterd"
 rcvar=$name
-command="/usr/pkg/sbin/${name}"
+command="@PREFIX@/sbin/${name}"
 #start_precmd="glusterd_precmd"
 pidfile="/var/run/${name}.pid"
 command_args="-p ${pidfile}"
-required_files="/usr/pkg/etc/glusterfs/${name}.vol"
+required_files="@PKG_SYSCONFDIR@/glusterfs/${name}.vol"
 
 load_rc_config $name
 run_rc_command "$1"
