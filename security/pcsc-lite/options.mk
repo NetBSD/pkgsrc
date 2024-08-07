8793a9a01aaf864d432a51f9e3c78293a9e7a6f6
adam <adam@pkgsrc.org>
2024-08-07 09:39:06 +0000

    pcsc-lite: updated to 2.3.0
    2.3.0
    
    - SCardGetStatusChange(): add the number of reader events
    - Add Appstream metainfo announcing HW support
    - meson: specify minimum meson version to use
    - fix formats under musl libc
    - Send libpcsclite.so logs to stderr instead of stdout
    - Some other minor improvements

--- security/pcsc-lite/options.mk
+++ security/pcsc-lite/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2024/07/21 01:02:26 manu Exp $
+# $NetBSD: options.mk,v 1.4 2024/08/07 09:39:06 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pcsc-lite
 
@@ -7,12 +7,12 @@ PKG_SUGGESTED_OPTIONS=		polkit
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	polkit
+
 .if !empty(PKG_OPTIONS:Mpolkit)
 .include "../../security/polkit/buildlink3.mk"
 MESON_ARGS+=		-Dpolkit=true
-PLIST_SUBST+=		POLKIT=""
+PLIST.polkit=		yes
 .else
 MESON_ARGS+=		-Dpolkit=false
-PLIST_SUBST+=		POLKIT="@comment "
 .endif
-
