a27696b3498bf664c517e7b8c8196ed08b766d35
bsiegert <bsiegert@pkgsrc.org>
2024-08-07 18:47:44 +0000

    Pullup ticket #6886 - requested by nia archivers/unzip: security fix
    Revisions pulled up:
    - archivers/unzip/Makefile                                      1.98
    - archivers/unzip/distinfo                                      1.35
    - archivers/unzip/patches/patch-fileio.c                        1.2
    - archivers/unzip/patches/patch-process.c                       1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Tue Aug  6 14:40:13 UTC 2024
    
       Modified Files:
       	pkgsrc/archivers/unzip: Makefile distinfo
       	pkgsrc/archivers/unzip/patches: patch-fileio.c patch-process.c
    
       Log Message:
       unzip: Add patches for CVE-2022-0529 and CVE-2022-0530

--- archivers/unzip/patches/patch-process.c
+++ archivers/unzip/patches/patch-process.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-process.c,v 1.1 2014/12/25 16:48:33 wiz Exp $
+$NetBSD: patch-process.c,v 1.1.78.1 2024/08/07 18:47:45 bsiegert Exp $
 
 https://bugzilla.redhat.com/show_bug.cgi?id=CVE-2014-8141
+https://sources.debian.org/patches/unzip/6.0-28/28-cve-2022-0529-and-cve-2022-0530.patch/
 
 --- process.c.orig	2009-03-06 01:25:10.000000000 +0000
 +++ process.c
@@ -11,12 +12,21 @@ https://bugzilla.redhat.com/show_bug.cgi?id=CVE-2014-8141
  
    See the accompanying file LICENSE, version 2009-Jan-02 or later
    (the contents of which are also included in unzip.h) for terms of use.
-@@ -1888,48 +1888,82 @@ int getZip64Data(__G__ ef_buf, ef_len)
+@@ -222,6 +222,8 @@ static ZCONST char Far ZipfileCommTrunc1
+      "\nwarning:  Unicode Path version > 1\n";
+    static ZCONST char Far UnicodeMismatchError[] =
+      "\nwarning:  Unicode Path checksum invalid\n";
++   static ZCONST char Far UFilenameTooLongTrunc[] =
++     "warning:  filename too long (P1) -- truncating.\n";
+ #endif
+ 
+ 
+@@ -1888,48 +1890,83 @@ int getZip64Data(__G__ ef_buf, ef_len)
      and a 4-byte version of disk start number.
      Sets both local header and central header fields.  Not terribly clever,
      but it means that this procedure is only called in one place.
 +
-+    2014-12-05 SMS.
++    2014-12-05 SMS.  (oCERT.org report.)  CVE-2014-8141.
 +    Added checks to ensure that enough data are available before calling
 +    makeint64() or makelong().  Replaced various sizeof() values with
 +    simple ("4" or "8") constants.  (The Zip64 structures do not depend
@@ -50,19 +60,20 @@ https://bugzilla.redhat.com/show_bug.cgi?id=CVE-2014-8141
              break;
          }
 -        if (eb_id == EF_PKSZ64) {
--
+ 
+-          int offset = EB_HEADSIZE;
 +        if (eb_id == EF_PKSZ64)
 +        {
-           int offset = EB_HEADSIZE;
- 
--          if (G.crec.ucsize == 0xffffffff || G.lrec.ucsize == 0xffffffff){
--            G.lrec.ucsize = G.crec.ucsize = makeint64(offset + ef_buf);
--            offset += sizeof(G.crec.ucsize);
++          unsigned offset = EB_HEADSIZE;
++
 +          if ((G.crec.ucsize == Z64FLGL) || (G.lrec.ucsize == Z64FLGL))
 +          {
 +            if (offset+ 8 > ef_len)
 +              return PK_ERR;
-+
+ 
+-          if (G.crec.ucsize == 0xffffffff || G.lrec.ucsize == 0xffffffff){
+-            G.lrec.ucsize = G.crec.ucsize = makeint64(offset + ef_buf);
+-            offset += sizeof(G.crec.ucsize);
 +            G.crec.ucsize = G.lrec.ucsize = makeint64(offset + ef_buf);
 +            offset += 8;
            }
@@ -110,3 +121,86 @@ https://bugzilla.redhat.com/show_bug.cgi?id=CVE-2014-8141
          ef_buf += (eb_len + EB_HEADSIZE);
          ef_len -= (eb_len + EB_HEADSIZE);
      }
+@@ -1984,7 +2021,7 @@ int getUnicodeData(__G__ ef_buf, ef_len)
+         }
+         if (eb_id == EF_UNIPATH) {
+ 
+-          int offset = EB_HEADSIZE;
++          unsigned offset = EB_HEADSIZE;
+           ush ULen = eb_len - 5;
+           ulg chksum = CRCVAL_INITIAL;
+ 
+@@ -2440,16 +2477,17 @@ char *wide_to_local_string(wide_string, 
+   int state_dependent;
+   int wsize = 0;
+   int max_bytes = MB_CUR_MAX;
+-  char buf[9];
++  char buf[ MB_CUR_MAX+ 1];             /* ("+1" not really needed?) */
+   char *buffer = NULL;
+   char *local_string = NULL;
++  size_t buffer_size;                   /* CVE-2022-0529 */
+ 
+   for (wsize = 0; wide_string[wsize]; wsize++) ;
+ 
+   if (max_bytes < MAX_ESCAPE_BYTES)
+     max_bytes = MAX_ESCAPE_BYTES;
+-
+-  if ((buffer = (char *)malloc(wsize * max_bytes + 1)) == NULL) {
++  buffer_size = wsize * max_bytes + 1;          /* Reused below. */
++  if ((buffer = (char *)malloc( buffer_size)) == NULL) {
+     return NULL;
+   }
+ 
+@@ -2487,8 +2525,28 @@ char *wide_to_local_string(wide_string, 
+     } else {
+       /* no MB for this wide */
+         /* use escape for wide character */
+-        char *escape_string = wide_to_escape_string(wide_string[i]);
+-        strcat(buffer, escape_string);
++        size_t buffer_len;
++        size_t escape_string_len;
++        char *escape_string;
++        int err_msg = 0;
++
++        escape_string = wide_to_escape_string(wide_string[i]);
++        buffer_len = strlen( buffer);
++        escape_string_len = strlen( escape_string);
++
++        /* Append escape string, as space allows. */
++        /* 2022-07-18 SMS, et al.  CVE-2022-0529 */
++        if (escape_string_len > buffer_size- buffer_len- 1)
++        {
++            escape_string_len = buffer_size- buffer_len- 1;
++            if (err_msg == 0)
++            {
++                err_msg = 1;
++                Info(slide, 0x401, ((char *)slide,
++                 LoadFarString( UFilenameTooLongTrunc)));
++            }
++        }
++        strncat( buffer, escape_string, escape_string_len);
+         free(escape_string);
+     }
+   }
+@@ -2540,9 +2598,18 @@ char *utf8_to_local_string(utf8_string, 
+   ZCONST char *utf8_string;
+   int escape_all;
+ {
+-  zwchar *wide = utf8_to_wide_string(utf8_string);
+-  char *loc = wide_to_local_string(wide, escape_all);
+-  free(wide);
++  zwchar *wide;
++  char *loc = NULL;
++
++  wide = utf8_to_wide_string( utf8_string);
++
++  /* 2022-07-25 SMS, et al.  CVE-2022-0530 */
++  if (wide != NULL)
++  {
++    loc = wide_to_local_string( wide, escape_all);
++    free( wide);
++  }
++
+   return loc;
+ }
+ 
