81850f74f45d7033d8f80c3b9c4d1afe6fd05c7c
ryoon <ryoon@pkgsrc.org>
2024-08-09 03:53:22 +0000

    security/libassuan2: Update to 3.0.1
    Changelog:
    Noteworthy changes in version 3.0.1 (2024-06-24) [C9/A0/R1]
    ------------------------------------------------
    
     * Change Unix symbol versioning to help the Debian transitioning
       process.
    
     Release-info: https://dev.gnupg.org/T7163
    
    
    Noteworthy changes in version 3.0.0 (2024-06-18) [C9/A0/R0]
    ------------------------------------------------
    
     * API change: For new code, which uses libassuan with nPTH, please
       use gpgrt_get_syscall_clamp and assuan_control, instead of the
       system_hooks API.  Use of ASSUAN_SYSTEM_NPTH is deprecated with new
       API version 3.  If it's really needed to keep using old
       implementation of ASSUAN_SYSTEM_NPTH, you need to change your your
       application code, to define
       ASSUAN_REALLY_REQUIRE_V2_NPTH_SYSTEM_HOOKS before including
       <assuan.h>.  For an application which uses version 2 API
       (NEED_LIBASSUAN_API=2 in its configure.ac), use of
       ASSUAN_SYSTEM_NPTH is still supported.  [T5914]
    
     * New function assuan_control.  [T6625]
    
     * New function assuan_sock_accept.  [T5925]
    
     * New functions assuan_pipe_wait_server_termination and
       assuan_pipe_kill_server to support abstraction of process.  [T6487]
    
     * Windows support for sendfd/recvfd.  [T6236]
    
     * Implement timeout in assuan_sock_connect_byname.  [T3302]
    
     * No support for WindowsCE, any more.  [T6170]
    
     * New socket flags "linger" and "reuseaddr".  [rA87f92fe962]
    
     * Interface changes relative to the 2.5.0 release:
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     assuan_sock_accept                  NEW.
     assuan_pipe_wait_server_termination NEW.
     assuan_pipe_kill_server             NEW.
     assuan_sock_set_flag                EXTENDED.
     assuan_sock_get_flag                EXTENDED.

--- security/libassuan2/patches/patch-src_assuan-socket.c
+++ security/libassuan2/patches/patch-src_assuan-socket.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_assuan-socket.c,v 1.1 2023/11/15 16:12:05 nia Exp $
+$NetBSD: patch-src_assuan-socket.c,v 1.2 2024/08/09 03:53:22 ryoon Exp $
 
 Add missing header for select(2). Fixes build on Mac OS X 10.4.
 
---- src/assuan-socket.c.orig	2023-06-16 08:33:14.000000000 +0000
+--- src/assuan-socket.c.orig	2024-06-27 04:17:34.537130917 +0000
 +++ src/assuan-socket.c
-@@ -34,6 +34,7 @@
- #endif
+@@ -32,6 +32,7 @@
+ # include <io.h>
  #else
  # include <sys/types.h>
 +# include <sys/select.h>
