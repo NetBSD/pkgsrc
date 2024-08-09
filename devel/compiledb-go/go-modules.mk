5c78babd4c644582b55344c09e9ca06b433e5657
wiz <wiz@pkgsrc.org>
2024-08-09 18:51:28 +0000

    devel/compiledb-go: import compiledb-go-1.3.0
    Tool for generating Clang's JSON Compilation Database file for GNU
    `make`-based build systems.
    
    It's aimed mainly at non-cmake (cmake already generates compilation
    database) large codebases. Inspired by projects like YCM-Generator
    and Bear, but faster (mainly with large projects), since in most
    cases it doesn't need a clean build (as the mentioned tools do) to
    generate the compilation database file, to achieve this it uses
    the make options such as -n/--dry-run and -k/--keep-going to extract
    the compile commands. Also, it's more cross-compiling friendly than
    YCM-generator's fake-toolchanin approach.

---
+++ devel/compiledb-go/go-modules.mk
@@ -0,0 +1,21 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/08/09 18:51:28 wiz Exp $
+
+GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.4.mod
+GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.4.zip
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.mod
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.zip
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.9.3.mod
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.9.3.zip
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.7.0.mod
+GO_MODULE_FILES+=	github.com/urfave/cli/v2/@v/v2.27.3.mod
+GO_MODULE_FILES+=	github.com/urfave/cli/v2/@v/v2.27.3.zip
+GO_MODULE_FILES+=	github.com/xrash/smetrics/@v/v0.0.0-20240521201337-686a1a2994c1.mod
+GO_MODULE_FILES+=	github.com/xrash/smetrics/@v/v0.0.0-20240521201337-686a1a2994c1.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.mod
