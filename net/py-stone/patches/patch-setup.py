37373277387193b3ef346cb560be5e24045b2fd1
wiz <wiz@pkgsrc.org>
2024-08-10 16:13:41 +0000

    py-stone: remove unused test dependency

--- net/py-stone/patches/patch-setup.py
+++ net/py-stone/patches/patch-setup.py
@@ -1,15 +1,14 @@
-$NetBSD: patch-setup.py,v 1.2 2024/04/26 17:02:07 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2024/08/10 16:13:41 wiz Exp $
 
-Relax requirement.
+Remove unused depdendency.
 
---- setup.py.orig	2024-03-28 16:48:27.000000000 +0000
+--- setup.py.orig	2024-07-19 18:05:56.000000000 +0000
 +++ setup.py
-@@ -21,7 +21,7 @@ with open('requirements.txt') as f:  # p
+@@ -21,7 +21,6 @@ with open('requirements.txt') as f:  # p
      install_reqs += f.read().splitlines()
  
  setup_requires = [
 -    'pytest-runner == 5.3.2',
-+    'pytest-runner',
  ]
  
  # WARNING: This imposes limitations on test/requirements.txt such that the
