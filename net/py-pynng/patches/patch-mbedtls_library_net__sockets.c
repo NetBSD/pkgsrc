d29d53c17ad087a4fa0138ceb249a39e6033be21
wiz <wiz@pkgsrc.org>
2024-08-10 07:34:38 +0000

    py-pynng: update to 0.8.0.
    Changes not found.

--- net/py-pynng/patches/patch-mbedtls_library_net__sockets.c
+++ net/py-pynng/patches/patch-mbedtls_library_net__sockets.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-mbedtls_library_net__sockets.c,v 1.1 2021/05/13 19:35:54 khorben Exp $
+$NetBSD: patch-mbedtls_library_net__sockets.c,v 1.2 2024/08/10 07:34:38 wiz Exp $
 
 Fix build on NetBSD
 
---- mbedtls/library/net_sockets.c.orig	2020-08-27 02:34:52.000000000 +0000
+--- mbedtls/library/net_sockets.c.orig	2024-01-16 17:26:58.000000000 +0000
 +++ mbedtls/library/net_sockets.c
 @@ -24,6 +24,10 @@
-  * Harmless on other platforms. */
- #define _POSIX_C_SOURCE 200112L
- 
+     !defined(__HAIKU__) && !defined(__midipix__)
+ #error "This module only works on Unix and Windows, see MBEDTLS_NET_C in mbedtls_config.h"
+ #endif
 +#if defined(__NetBSD__)
 +# define _NETBSD_SOURCE
 +#endif
 +
- #if !defined(MBEDTLS_CONFIG_FILE)
- #include "mbedtls/config.h"
- #else
+ 
+ #include "mbedtls/platform.h"
+ 
--- net/py-pynng/patches/patch-nng_src_platform_posix_posix__thread.c
+++ net/py-pynng/patches/patch-nng_src_platform_posix_posix__thread.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-nng_src_platform_posix_posix__thread.c,v 1.1 2021/05/13 19:35:54 khorben Exp $
+$NetBSD: patch-nng_src_platform_posix_posix__thread.c,v 1.2 2024/08/10 07:34:38 wiz Exp $
 
 Fix build on NetBSD
 
---- nng/src/platform/posix/posix_thread.c.orig	2020-08-27 02:35:02.000000000 +0000
+--- nng/src/platform/posix/posix_thread.c.orig	2024-01-16 17:27:13.000000000 +0000
 +++ nng/src/platform/posix/posix_thread.c
-@@ -268,6 +268,12 @@ nni_plat_thr_set_name(nni_plat_thr *thr,
- 	if ((thr == NULL) || (pthread_self() == thr->tid)) {
-         	pthread_setname_np(name);
+@@ -316,6 +316,12 @@ nni_plat_thr_set_name(nni_plat_thr *thr,
+ 	} else {
+ 		pthread_setname_np(thr->tid, "%s", name);
  	}
 +#elif defined(__NetBSD__)
 +	if (thr == NULL) {
@@ -16,4 +16,4 @@ Fix build on NetBSD
 +	}
  #else
  	if (thr == NULL) {
-                 pthread_setname_np(pthread_self(), name);
+ 		pthread_setname_np(pthread_self(), name);
