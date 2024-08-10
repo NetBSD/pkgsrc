14d21f30765a07b8f6d2742876f2a0108f04b122
adam <adam@pkgsrc.org>
2024-08-10 13:18:58 +0000

    srain: updated to 1.7.0
    2024-04-26 Version 1.7.0
    
    - Changes:
    
      - Remove GIMPNet (:pull:`408`)
      - Migrate to libsoup3 (:issue:`409`)
    
    
    2024-04-26 Version 1.6.1
    
    - Bug fixes:
    
      - Add developer name to metainfo
    
    
    2024-02-11 Version 1.6.0
    
    - Features:
    
      - Use new app indicator provided by libayatana-appindicator_, which has better
        compatibility than ``GtkStatusIcon`` (:pull:`374`, :pull:`401`)
    
        .. note:: User can still use ``GtkStatusIcon`` if they have trouble installing
           libayatana-appindicator, see :doc:`./install` for more details.
    
      - Add conf item ``scroll-on-new-message`` to automatically scroll down the
        message list when a new message is incoming even if the application is
        inactive (:issue:`382`)
    
    - Bug fixes:
    
      - Let conf item ``chat.log`` works as expected (:issue:`402`)
    
    
    2023-03-26 Version 1.5.1
    
    - Bug fixes:
    
      - Fix the problem that the URL cannot be opened on MS Windows (:issue:`391`)
    
    
    2022-10-02 Version 1.5.0
    
    - Features:
    
      - Able to identify `Nordic GTK Theme`__ as dark theme (:pull:`372`)
      - Activate the corresponding buffer to a response user query (:issue:`381`)
    
    - Changes:
    
      - No additional sphinx extensions are required to build documentation (:issue:`368`)
      - Cleanup deprecated APIs (:pull:`369`)
      - Suppress error message when connection keeps failing (:pull:`386`)
      - Enable debian release build , thanks to @tomac4t (:issue:`387`)
    
    - Bug fixes:
    
      - Fix SASL authentication error when connecting to Soju

--- chat/srain/patches/patch-src_lib_path.c
+++ chat/srain/patches/patch-src_lib_path.c
@@ -1,44 +1,43 @@
-$NetBSD: patch-src_lib_path.c,v 1.2 2021/03/17 15:32:24 nia Exp $
+$NetBSD: patch-src_lib_path.c,v 1.3 2024/08/10 13:18:58 adam Exp $
+
+Use FreeBSD code for NetBSD.
 
 Use the Linux procfs code path for finding the executable
 name as a portable fallback.
 
-The code for other platforms is wrong.
-
---- src/lib/path.c.orig	2021-02-28 09:19:09.000000000 +0000
+--- src/lib/path.c.orig	2024-04-26 06:04:27.000000000 +0000
 +++ src/lib/path.c
-@@ -50,21 +50,20 @@
+@@ -50,21 +50,21 @@
  #include <mach-o/dyld.h>
  #include <unistd.h>
  
 -#elif defined __linux__
-+#elif defined __FreeBSD__
-+#include <sys/types.h>
-+#include <sys/sysctl.h>
-+
-+#else
- #include <limits.h>
- #include <libgen.h>
- #include <unistd.h>
- 
+-#include <limits.h>
+-#include <libgen.h>
+-#include <unistd.h>
+-
 -#define PROC_SELF_EXE "/proc/self/exe"
 -
--#elif defined __sun
--
-+#if defined __sun
+ #elif defined __sun
+ 
  #define PROC_SELF_EXE "/proc/self/path/a.out"
--
+ 
 -#elif defined __FreeBSD__
--#include <sys/types.h>
--#include <sys/sysctl.h>
--
++#elif defined(__FreeBSD__) || defined(__NetBSD__)
+ #include <sys/types.h>
+ #include <sys/sysctl.h>
+ 
 +#else
++#include <limits.h>
++#include <libgen.h>
++#include <unistd.h>
++
 +#define PROC_SELF_EXE "/proc/self/exe"
-+#endif
++
  #endif
  
  #define DEFAULT_FILE_MODE   (S_IRUSR | S_IWUSR)
-@@ -371,20 +370,6 @@ char *srn_get_executable_dir() {
+@@ -371,20 +371,6 @@ char *srn_get_executable_dir() {
      return executableDir;
  }
  
@@ -59,16 +58,16 @@ The code for other platforms is wrong.
  #elif defined __APPLE__
  char *srn_get_executable_path() {
      char rawPathName[PATH_MAX];
-@@ -404,7 +389,7 @@ char *srn_get_executable_dir() {
+@@ -404,7 +390,7 @@ char *srn_get_executable_dir() {
      return executableDir;
  }
  
 -#elif defined __FreeBSD__
-+
++#elif defined(__FreeBSD__) || defined(__NetBSD__)
  char *srn_get_executable_path() {
      int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
      char rawPath[PATH_MAX];
-@@ -424,4 +409,18 @@ char *srn_get_executable_dir() {
+@@ -424,4 +410,18 @@ char *srn_get_executable_dir() {
      g_free(executablePath);
      return executableDir;
  }
