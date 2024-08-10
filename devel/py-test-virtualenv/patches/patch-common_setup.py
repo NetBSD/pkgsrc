fb251d37af0d9c6c2561b1ba5a6b8debedb4c221
wiz <wiz@pkgsrc.org>
2024-08-10 06:24:24 +0000

    *: fix build with latest setuptools

--- devel/py-test-virtualenv/patches/patch-common_setup.py
+++ devel/py-test-virtualenv/patches/patch-common_setup.py
@@ -1,10 +1,19 @@
-$NetBSD: patch-common_setup.py,v 1.1 2018/03/24 07:14:59 adam Exp $
+$NetBSD: patch-common_setup.py,v 1.2 2024/08/10 06:24:25 wiz Exp $
 
+Fix build with latest setuptools.
 Do not depend on setuptools-git.
 
---- common_setup.py.orig	2018-03-24 07:10:19.000000000 +0000
+--- common_setup.py.orig	2019-05-28 06:32:19.000000000 +0000
 +++ common_setup.py
-@@ -72,6 +72,5 @@ def common_setup(src_dir):
+@@ -38,7 +38,6 @@ class EggInfo(EggInfoCommand):
+     def run(self):
+         if self.distribution.extras_require is None:
+             self.distribution.extras_require = {}
+-        self.distribution.extras_require['tests'] = self.distribution.tests_require
+         EggInfoCommand.run(self)
+ 
+ 
+@@ -72,6 +71,5 @@ def common_setup(src_dir):
              license='MIT license',
              platforms=['unix', 'linux'],
              cmdclass={'test': PyTest, 'egg_info': EggInfo},
