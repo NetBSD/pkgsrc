b3fb29a81523878dee1a9b90cabc3c31b098a493
wiz <wiz@pkgsrc.org>
2024-08-10 07:14:15 +0000

    py-dropbox: update to 12.0.2.
    v12.0.2
    
    Remove the pin for urllib3 (#507)
    
    v12.0.1
    
    Fix incorrect pin of requests (#505)
    
    
    v12.0.0
    
    Fixes to Restore CI (#492, #501)
    Fixes to doc generation (#500, #503)
    Manual Spec Update (#498)
    Stop providing a hardcoded CA bundle (#489, #499)

--- net/py-dropbox/patches/patch-setup.py
+++ net/py-dropbox/patches/patch-setup.py
@@ -1,23 +1,22 @@
-$NetBSD: patch-setup.py,v 1.2 2023/02/10 14:39:38 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2024/08/10 07:14:15 wiz Exp $
 
-Fix invalid pattern.
-Unpin pytest-runner
+Unpin stone.
+Remove unused pytest-runner dependency.
 
---- setup.py.orig	2022-06-15 23:27:44.000000000 +0000
+--- setup.py.orig	2024-06-03 16:45:16.000000000 +0000
 +++ setup.py
-@@ -27,13 +27,13 @@ version = eval(line.split('=', 1)[1].str
+@@ -27,13 +27,10 @@ version = eval(line.split('=', 1)[1].str
  install_reqs = [
-     'requests >= 2.16.2',
+     'requests>=2.16.2',
      'six >= 1.12.0',
--    'stone >= 2.*',
-+    'stone >= 2',
+-    'stone>=2,<3.3.3',
++    'stone>=2',
  ]
  
  setup_requires = [
-     # Pin pytest-runner to 5.2.0, since 5.3.0 uses `find_namespaces` directive, not supported in
-     # Python 2.7
--    'pytest-runner == 5.2.0',
-+    'pytest-runner',
+-    # Pin pytest-runner to 5.2.0, since 5.3.0 uses `find_namespaces` directive, not supported in
+-    # Python 2.7
+-    'pytest-runner==5.2.0',
  ]
  
  # WARNING: This imposes limitations on test/requirements.txt such that the
