c00d9cd6101c68b9d34aa5a7a6d59886aee4d6a4
wiz <wiz@pkgsrc.org>
2024-08-10 15:14:37 +0000

    py-setuptools_trial: convert to wheel.mk.
    Remove unused test dependency.
    Document test status.
    
    Bump PKGREVISION.

--- devel/py-setuptools_trial/patches/patch-setup.py
+++ devel/py-setuptools_trial/patches/patch-setup.py
@@ -1,10 +1,23 @@
-$NetBSD: patch-setup.py,v 1.1 2018/04/03 20:19:47 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2024/08/10 15:14:37 wiz Exp $
+
+Test-runner is not really used.
+https://github.com/rutsky/setuptools-trial/issues/272
 
 Do not install docs and avoid conflict between Python versions.
 
---- setup.py.orig	2018-04-03 20:08:51.000000000 +0000
+--- setup.py.orig	2016-03-21 03:17:06.000000000 +0000
 +++ setup.py
-@@ -86,7 +86,6 @@ setup(
+@@ -54,9 +54,6 @@ PKG = 'setuptools_trial'
+ doc_loc = "share/doc/python-" + PKG
+ data_files = [(doc_loc, data_fnames)]
+ 
+-if {'pytest', 'test'}.intersection(sys.argv):
+-    setup_requires.append('pytest_runner')
+-
+ setup(
+     name=PKG,
+     version=verstr,
+@@ -86,7 +83,6 @@ setup(
      license="BSD",
      packages=find_packages(exclude=("tests",)),
      include_package_data=True,
