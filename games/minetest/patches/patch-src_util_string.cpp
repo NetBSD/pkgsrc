f17d897a429bf0d667d518601a55da524091f01d
ktnb <ktnb@pkgsrc.org>
2024-08-13 14:45:28 +0000

    Fixed issue with patch and luaJIT

--- games/minetest/patches/patch-src_util_string.cpp
+++ games/minetest/patches/patch-src_util_string.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_util_string.cpp,v 1.6 2023/08/28 07:01:29 nia Exp $
+$NetBSD: patch-src_util_string.cpp,v 1.7 2024/08/13 14:45:28 ktnb Exp $
 
 Support non-POSIX iconv on NetBSD before 10 and SunOS.
 
---- src/util/string.cpp.orig	2023-04-08 16:04:52.000000000 +0000
+--- src/util/string.cpp.orig	2024-08-11 15:54:55.000000000 +0000
 +++ src/util/string.cpp
 @@ -39,6 +39,15 @@ with this program; if not, write to the 
  	#include <windows.h>
@@ -19,16 +19,4 @@ Support non-POSIX iconv on NetBSD before 10 and SunOS.
 +
  #ifndef _WIN32
  
- static bool convert(const char *to, const char *from, char *outbuf,
-@@ -54,7 +63,11 @@ static bool convert(const char *to, cons
- 	const size_t old_outbuf_size = *outbuf_size;
- 	size_t old_size = inbuf_size;
- 	while (inbuf_size > 0) {
-+#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
-+		iconv(cd, (const char **)&inbuf_ptr, inbuf_left_ptr, &outbuf_ptr, outbuf_size);
-+#else
- 		iconv(cd, &inbuf_ptr, inbuf_left_ptr, &outbuf_ptr, outbuf_size);
-+#endif
- 		if (inbuf_size == old_size) {
- 			iconv_close(cd);
- 			return false;
+ namespace {
