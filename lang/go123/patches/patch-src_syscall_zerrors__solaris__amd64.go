90dc1637516f34a1da0e4ddb5faaa5cd657ce783
bsiegert <bsiegert@pkgsrc.org>
2024-08-14 10:04:04 +0000

    go123: add Go version 1.23.0. Not the default yet.
    The latest Go release, version 1.23, arrives six months after Go 1.22. Most of
    its changes are in the implementation of the toolchain, runtime, and libraries.
    As always, the release maintains the Go 1 promise of compatibility. We expect
    almost all Go programs to continue to compile and run as before.
    
    Go 1.23 makes the (Go 1.22) “range-over-func” experiment a part of the
    language.
    
    Starting in Go 1.23, the Go toolchain can collect usage and breakage statistics
    that help the Go team understand how the Go toolchain is used and how well it
    is working. We refer to these statistics as Go telemetry.
    
    Go telemetry is an opt-in system, controlled by the go telemetry command. By
    default, the toolchain programs collect statistics in counter files that can be
    inspected locally but are otherwise unused (go telemetry local).
    
    Setting the GOROOT_FINAL environment variable no longer has an effect.
    Distributions that install the go command to a location other than
    $GOROOT/bin/go should install a symlink instead of relocating or copying the go
    binary.

---
+++ lang/go123/patches/patch-src_syscall_zerrors__solaris__amd64.go
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_syscall_zerrors__solaris__amd64.go,v 1.1 2024/08/14 10:04:05 bsiegert Exp $
+
+Support O_DIRECT on illumos.
+
+--- src/syscall/zerrors_solaris_amd64.go.orig	2024-06-20 19:20:56.000000000 +0000
++++ src/syscall/zerrors_solaris_amd64.go
+@@ -634,6 +634,7 @@ const (
+ 	O_APPEND                      = 0x8
+ 	O_CLOEXEC                     = 0x800000
+ 	O_CREAT                       = 0x100
++	O_DIRECT                      = 0x2000000
+ 	O_DIRECTORY                   = 0x1000000
+ 	O_DSYNC                       = 0x40
+ 	O_EXCL                        = 0x400
