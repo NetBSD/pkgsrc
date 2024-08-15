6fd68cdad311dc38e8a896b5d4d912563eb1aab3
tnn <tnn@pkgsrc.org>
2024-08-15 18:05:05 +0000

    jack: fix build w/ Python 3.12 (cherry-picked from upstream)

--- audio/jack/patches/patch-waflib_Context.py
+++ audio/jack/patches/patch-waflib_Context.py
@@ -1,11 +1,31 @@
-$NetBSD: patch-waflib_Context.py,v 1.1 2022/12/16 07:36:41 wiz Exp $
+$NetBSD: patch-waflib_Context.py,v 1.2 2024/08/15 18:05:05 tnn Exp $
 
-Fix build with Python 3.11.
+Fix build with Python 3.11, 3.12.
 https://github.com/jackaudio/jack2/issues/898
+https://github.com/jackaudio/jack2/commit/250420381b1a6974798939ad7104ab1a4b9a9994#diff-5cdadb9cd9b71f7c6dda6ca75241b1968009f71cd7c046242929974b61cff061L12
 
---- waflib/Context.py.orig	2021-07-15 06:18:26.000000000 +0000
+--- waflib/Context.py.orig	2022-04-15 19:14:06.000000000 +0000
 +++ waflib/Context.py
-@@ -266,7 +266,7 @@ class Context(ctx):
+@@ -6,10 +6,17 @@
+ Classes and functions enabling the command system
+ """
+ 
+-import os, re, imp, sys
++import os, re, sys
+ from waflib import Utils, Errors, Logs
+ import waflib.Node
+ 
++if sys.hexversion > 0x3040000:
++	import types
++	class imp(object):
++		new_module = lambda x: types.ModuleType(x)
++else:
++	import imp
++
+ # the following 3 constants are updated on each new release (do not touch)
+ HEXVERSION=0x2000c00
+ """Constant updated on new releases"""
+@@ -266,7 +273,7 @@ class Context(ctx):
  				cache[node] = True
  				self.pre_recurse(node)
  				try:
@@ -14,7 +34,7 @@ https://github.com/jackaudio/jack2/issues/898
  					exec(compile(function_code, node.abspath(), 'exec'), self.exec_dict)
  				finally:
  					self.post_recurse(node)
-@@ -662,7 +662,7 @@ def load_module(path, encoding=None):
+@@ -662,7 +669,7 @@ def load_module(path, encoding=None):
  
  	module = imp.new_module(WSCRIPT_FILE)
  	try:
