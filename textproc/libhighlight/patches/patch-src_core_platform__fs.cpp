7560d38bc4ccf2b4c1abe32312a8991783310fc5
schmonz <schmonz@pkgsrc.org>
2024-08-16 15:27:59 +0000

    libhighlight: update to 4.13. Changes:
    - updated astyle lib to version 3.6
    - added Gleam and V definitions
    - added Meson and CMake build files
    - improved LSP compatibility with bash-language-server and
      haskell-language-server
    - applied several clang-tidy modernize fixes
    - GUI: refactored settings retore code

--- textproc/libhighlight/patches/patch-src_core_platform__fs.cpp
+++ textproc/libhighlight/patches/patch-src_core_platform__fs.cpp
@@ -1,25 +1,28 @@
-$NetBSD: patch-src_core_platform__fs.cpp,v 1.4 2020/12/18 18:45:08 schmonz Exp $
+$NetBSD: patch-src_core_platform__fs.cpp,v 1.5 2024/08/16 15:27:59 schmonz Exp $
 
 Fix build on (at least) SmartOS.
 
---- src/core/platform_fs.cpp.orig	2020-11-13 20:00:38.000000000 +0000
+--- src/core/platform_fs.cpp.orig	2024-08-11 13:05:37.000000000 +0000
 +++ src/core/platform_fs.cpp
-@@ -95,12 +95,12 @@ std::string getTempFilePath(){
+@@ -65,7 +65,7 @@ namespace Platform
+ const char pathSeparator = '\\';
  
- const char pathSeparator = '/';
- 
+ #ifndef QT
 -std::string getAppPath()
 +::std::string getAppPath()
  {
-     return "";
+     char pathAndName[MAX_PATH], path[MAX_PATH], drive[3];
+     GetModuleFileName(NULL, pathAndName, MAX_PATH);
+@@ -74,7 +74,7 @@ std::string getAppPath()
  }
+ #endif
  
 -std::string getHomePath()
 +::std::string getHomePath()
  {
-     struct passwd *pw = getpwuid(getuid());
-     return pw != NULL ? string(pw->pw_dir) : "";
-@@ -140,8 +140,8 @@ int isDarkTerminal() {
+     return "";
+ }
+@@ -149,8 +149,8 @@ int isDarkTerminal() {
      return 1;
  }
  
