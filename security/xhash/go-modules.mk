9744e4c64a356b021335041869c4c58d98e1fb35
bsiegert <bsiegert@pkgsrc.org>
2024-08-19 15:14:35 +0000

    xhash: update to 3.4.4
    - add --base64 and -b options
    - re-enable 224 and 384 sized hashes
    
    Patch from Ricardo Branco in PR pkg/58267.

--- security/xhash/go-modules.mk
+++ security/xhash/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.1 2024/03/01 15:51:25 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.2 2024/08/19 15:14:35 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.mod
 GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.zip
@@ -8,7 +8,7 @@ GO_MODULE_FILES+=	github.com/zeebo/assert/@v/v1.1.0.mod
 GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.3.mod
 GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.3.zip
 GO_MODULE_FILES+=	github.com/zeebo/pcg/@v/v1.0.1.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.19.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.19.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.17.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.zip
