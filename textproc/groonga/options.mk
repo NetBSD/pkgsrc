b3bde4b381c324bd1726a60f4b32e2f795df6828
adam <adam@pkgsrc.org>
2024-08-19 11:55:25 +0000

    groonga: updated to 14.0.6
    14.0.6
    https://groonga.org/en/blog/2024/07/29/groonga-14.0.6.html

--- textproc/groonga/options.mk
+++ textproc/groonga/options.mk
@@ -1,14 +1,13 @@
-# $NetBSD: options.mk,v 1.19 2020/05/31 14:43:14 rillig Exp $
-#
+# $NetBSD: options.mk,v 1.20 2024/08/19 11:55:25 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.groonga
-PKG_SUPPORTED_OPTIONS=	mecab tests zlib lz4
-PKG_SUPPORTED_OPTIONS+=	groonga-suggest-learner groonga-httpd
-PKG_SUGGESTED_OPTIONS=	mecab groonga-suggest-learner groonga-httpd
+PKG_SUPPORTED_OPTIONS=	lz4 mecab tests zstd
+PKG_SUPPORTED_OPTIONS+=	groonga-suggest-learner
+PKG_SUGGESTED_OPTIONS=	mecab groonga-suggest-learner
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		mecab learner httpd
+PLIST_VARS+=		mecab learner
 
 .if !empty(PKG_OPTIONS:Mmecab)
 CONFIGURE_ARGS+=	--with-mecab
@@ -25,11 +24,11 @@ BUILDLINK_API_DEPENDS.cutter+=		cutter>=1.1.6
 .include "../../devel/cutter/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mzlib)
-CONFIGURE_ARGS+=	--with-zlib
-.include "../../devel/zlib/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mzstd)
+CONFIGURE_ARGS+=	--with-zstd
+.include "../../archivers/zstd/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--without-zlib
+CONFIGURE_ARGS+=	--without-zstd
 .endif
 
 .if !empty(PKG_OPTIONS:Mlz4)
@@ -51,49 +50,4 @@ PLIST.learner=		yes
 CONFIGURE_ARGS+=	--without-libevent
 CONFIGURE_ARGS+=	--without-message-pack
 CONFIGURE_ARGS+=	--disable-zeromq
-.endif
-
-.if !empty(PKG_OPTIONS:Mgroonga-httpd)
-.include "../../devel/pcre/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-groonga-httpd
-PLIST.httpd=	yes
-OWN_DIRS+=	${PKG_SYSCONFDIR}/httpd/html
-OWN_DIRS+=	${PKG_SYSCONFDIR}/httpd
-OWN_DIRS+=	${VARBASE}/run/${PKGBASE}
-OWN_DIRS+=	${VARBASE}/log/${PKGBASE}/httpd
-BUILD_DEFS+=	VARBASE
-
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/fastcgi.conf \
-		${PKG_SYSCONFDIR}/httpd/fastcgi.conf
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/fastcgi_params \
-		${PKG_SYSCONFDIR}/httpd/fastcgi_params
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/groonga-httpd.conf \
-		${PKG_SYSCONFDIR}/httpd/groonga-httpd.conf
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/html/50x.html \
-		${PKG_SYSCONFDIR}/httpd/html/50x.html
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/html/index.html \
-		${PKG_SYSCONFDIR}/httpd/html/index.html
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/koi-utf \
-		${PKG_SYSCONFDIR}/httpd/koi-utf
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/koi-win \
-		${PKG_SYSCONFDIR}/httpd/koi-win
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/mime.types \
-		${PKG_SYSCONFDIR}/httpd/mime.types
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/scgi_params \
-		${PKG_SYSCONFDIR}/httpd/scgi_params
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/uwsgi_params \
-		${PKG_SYSCONFDIR}/httpd/uwsgi_params
-CONF_FILES+=	${EXAMPLE_CONF_DIR}/httpd/win-utf \
-		${PKG_SYSCONFDIR}/httpd/win-utf
-
-SUBST_CLASSES+=		confpath
-SUBST_STAGE.confpath=	pre-build
-SUBST_FILES.confpath=	vendor/nginx-*/objs/Makefile
-SUBST_SED.confpath=	-e 's,\$$(DESTDIR)${PKG_SYSCONFDIR}/httpd,\$$(DESTDIR)${PREFIX}/share/examples/${PKGBASE}/httpd,g'
-
-post-install:
-	${RM} -f ${DESTDIR}${PREFIX}/share/examples/groonga/httpd/*.default
-
-.else
-CONFIGURE_ARGS+=	--disable-groonga-httpd
 .endif
