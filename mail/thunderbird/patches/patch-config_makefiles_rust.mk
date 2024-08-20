1b16aa4c98ed8dafdf085253278b94c4eab71c70
ryoon <ryoon@pkgsrc.org>
2024-08-20 14:11:52 +0000

    mail/thunderbird: Fix build with lang/rust-1.79.0
    * Fix build with patches from FreeBSD Ports.
    * This package cannot be build under NetBSD/i386 for me because
      of out-of-memory.

--- mail/thunderbird/patches/patch-config_makefiles_rust.mk
+++ mail/thunderbird/patches/patch-config_makefiles_rust.mk
@@ -1,12 +1,11 @@
-$NetBSD: patch-config_makefiles_rust.mk,v 1.4 2024/08/01 15:27:50 ryoon Exp $
+$NetBSD: patch-config_makefiles_rust.mk,v 1.5 2024/08/20 14:11:52 ryoon Exp $
 
 NetBSD doesn't get along with parallel rust builds (it causes issues
 with ld.so) which are the default. Force -j1.
 
-Use less memory to fix build under NetBSD/i386.
-From: https://bugzilla.mozilla.org/show_bug.cgi?id=1644409
+Expand -Clto=thin for all to avoid out-of-memory errors.
 
---- config/makefiles/rust.mk.orig	2023-06-22 21:19:03.000000000 +0000
+--- config/makefiles/rust.mk.orig	2024-08-01 19:52:10.000000000 +0000
 +++ config/makefiles/rust.mk
 @@ -60,6 +60,9 @@ ifdef MOZ_TSAN
  cargo_build_flags += -Zbuild-std=std,panic_abort
@@ -18,12 +17,14 @@ From: https://bugzilla.mozilla.org/show_bug.cgi?id=1644409
  
  rustflags_sancov =
  ifdef LIBFUZZER
-@@ -92,7 +95,7 @@ ifndef rustflags_sancov
+@@ -91,9 +94,7 @@ ifndef MOZ_LTO_RUST_CROSS
+ ifndef rustflags_sancov
  # Never enable when coverage is enabled to work around https://github.com/rust-lang/rust/issues/90045.
  ifndef MOZ_CODE_COVERAGE
- ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
+-ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
 -cargo_rustc_flags += -Clto$(if $(filter full,$(MOZ_LTO_RUST_CROSS)),=fat)
+-endif
 +cargo_rustc_flags += -Clto=thin
- endif
  # We need -Cembed-bitcode=yes for all crates when using -Clto.
  RUSTFLAGS += -Cembed-bitcode=yes
+ endif
