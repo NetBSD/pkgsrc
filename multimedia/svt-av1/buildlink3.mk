c704e9c461cba69879de95b3e24659e4305cc4b3
wiz <wiz@pkgsrc.org>
2024-08-21 17:00:41 +0000

    *: revbump for svt-av1 shlib bump

--- multimedia/svt-av1/buildlink3.mk
+++ multimedia/svt-av1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/04/09 11:13:51 plunky Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/08/21 17:00:41 wiz Exp $
 
 BUILDLINK_TREE+=	svt-av1
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	svt-av1
 SVT_AV1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.svt-av1+=	svt-av1>=1.4.1
+BUILDLINK_ABI_DEPENDS.svt-av1?=	svt-av1>=2.1.0
 BUILDLINK_PKGSRCDIR.svt-av1?=	../../multimedia/svt-av1
 
 .endif	# SVT_AV1_BUILDLINK3_MK
