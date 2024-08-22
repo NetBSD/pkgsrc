8052c976847973f6d3fdc7dc17df016382eb9197
gdt <gdt@pkgsrc.org>
2024-08-22 12:59:14 +0000

    geography/mapserver: Disable optional things harder
    Explicitly disable postgis if the option isn't enabled, and adjust
    comments/todos surrounding options.

--- geography/mapserver/options.mk
+++ geography/mapserver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2024/08/17 12:54:20 gdt Exp $
+# $NetBSD: options.mk,v 1.8 2024/08/22 12:59:14 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mapserver
 PKG_SUPPORTED_OPTIONS=	fastcgi pgsql mysql x11
@@ -6,15 +6,13 @@ PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
 
-# \todo cope with cmake
-
 #
 # FastCGI support
 #
 .if !empty(PKG_OPTIONS:Mfastcgi)
 .include "../../www/fcgi/buildlink3.mk"
 .else
-# Remediate cmake looking for things not found with configure tests.
+# Remediate cmake looking out of the bl3 tree.
 CMAKE_CONFIGURE_ARGS+=		-DWITH_FCGI=OFF
 .endif
 
@@ -25,7 +23,8 @@ CMAKE_CONFIGURE_ARGS+=		-DWITH_FCGI=OFF
 # \todo Explain why this is bl3 rather than DEPENDS.  It doesn't make
 # sense, given how postgis works.
 .include "../../databases/postgresql-postgis2/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-postgis
+.else
+CMAKE_CONFIGURE_ARGS+=		-D-DWITH_POSTGIS=OFF
 .endif
 
 #
@@ -33,7 +32,7 @@ CONFIGURE_ARGS+=	--with-postgis
 #
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../mk/mysql.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-mygis
+# \todo Enable?
 .endif
 
 #
@@ -41,6 +40,5 @@ CONFIGURE_ARGS+=	--with-mygis
 #
 .if !empty(PKG_OPTIONS:Mx11)
 .include "../../x11/libXpm/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-xpm
+# \todo Enable/disable?
 .endif
