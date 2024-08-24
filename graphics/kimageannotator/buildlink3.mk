5ffcd58b5ba96c4e894532871b46075e09c8dff1
markd <markd@pkgsrc.org>
2024-08-24 04:24:47 +0000

    kimageannotator: update to 0.7.1
    Release 0.7.1
    * Fixed: Crash after pressing key when no tab exists or closing last tab.
    
    Release 0.7.0
    * New: Allow copying items between tabs.
    * New: CTRL + A does not select all text typed.
    * New: Open text edit mode when double-click on textbox figure in Text tool.
    * New: Add reflowing capability to the text tool.
    * New: Editing text, no mouse cursor edit functions.
    * New: Mouse click within a text box for setting specific editing position
      and selecting text.
    * Fixed: Text isn't reflowed the next line within the box and text overlaps
      when resizing box.
    * Fixed: Can't wrap long text line when I resize Text box area.
    * Fixed: Key press operations affect items across different tabs.
    * Fixed: Clipboard cleared when new tab added.
    
    Release 0.6.1
    * Fixed: Fix for unnecessary scrollbars when a screenshot has a smaller size
      than the previous one.
    * Fixed: Add KDE support for scale factor.
    * Fixed: Show tab tooltips on initial tabs.
    * Fixed: Sticker resizing is broken when bounding rect flipped.

--- graphics/kimageannotator/buildlink3.mk
+++ graphics/kimageannotator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2022/09/29 00:09:28 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/08/24 04:24:47 markd Exp $
 
 BUILDLINK_TREE+=	kimageannotator
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	kimageannotator
 KIMAGEANNOTATOR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kimageannotator+=	kimageannotator>=0.6.0
+BUILDLINK_ABI_DEPENDS.kimageannotator?=	kimageannotator>=0.7.1
 BUILDLINK_PKGSRCDIR.kimageannotator?=	../../graphics/kimageannotator
 
 .include "../../graphics/kcolorpicker/buildlink3.mk"
