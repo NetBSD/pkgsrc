d31d33bb7ee032e3b5ea5456be3133b61a334a46
pin <pin@pkgsrc.org>
2024-08-24 15:57:19 +0000

    textproc/git-delta: update to 0.18.1
    This patch release fixes an issue with delta --version that was causing problems downstream (#1818).
    What's Changed
     - Add hyperlinks section to manual by @dandavison in #1816
     - Added platypus theme by @sarpuser in #1819
     - Don't use pager for --version by @dandavison in #1824

--- textproc/git-delta/cargo-depends.mk
+++ textproc/git-delta/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.11 2024/08/18 11:58:53 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.12 2024/08/24 15:57:19 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.2
