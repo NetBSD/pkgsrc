3462f9f2578ee9cccbdc7b7b466e733b7c8977f9
morr <morr@pkgsrc.org>
2024-08-26 19:26:31 +0000

    Security update to version 9.1.698.
    Changelog:
    
    - patch 9.1.0698: tests: "Untitled" file not removed when running Test_crash1_3 alone
    - patch 9.1.0697: [security]: heap-buffer-overflow in ins_typebuf
    - patch 9.1.0696: installing runtime files fails when using SHADOWDIR
    - runtime(doc): fix typo
    - patch 9.1.0695: tests: test_crash leaves Untitled file around
    - translation(br): Update Brazilian translation
    - translation(pt): Update menu_pt_br
    - patch 9.1.0694: matchparen is slow on a long line
    - patch 9.1.0693: Configure doesn't show result when not using python3 stable abi
    - patch 9.1.0692: Wrong patlen value in ex_substitute()
    - patch 9.1.0691: python3: stable-abi may cause segfault on Python 3.11
    - runtime(vim): Update base-syntax, match :loadkeymap after colon and bar
    - runtime(mane): Improve <Plug>ManBS mapping
    - patch 9.1.0690: cannot set special highlight kind in popupmenu
    - translation(pt): Revert and fix wrong Portuguese menu translation files
    - translation(pt): revert Portuguese menu translation
    - translation(br): Update Brazilian translations
    - runtime(vim): Update base-syntax, improve :let-heredoc highlighting
    - patch 9.1.0689: [security]: buffer-overflow in do_search() with 'rightleft'
    - runtime(vim): Improve heredoc handling for all embedded scripts
    - patch 9.1.0688: Vim9: dereferences NULL pointer in check_type_is_value()
    - patch 9.1.0687: Makefile may not install desktop files
    - runtime(man): Fix <Plug>ManBS
    - runtime(java): Make the bundled &foldtext function optional
    - runtime(netrw): Change line on `mx` if command output exists
    - runtime(netrw): Fix `mf`-selected entry highlighting
    - runtime(htmlangular): add html syntax highlighting
    - translation(it): Fix filemode of Italian manpages
    - runtime(doc): Update outdated man.vim plugin information
    - runtime(zip): simplify condition to detect MS-Windows
    - patch 9.1.0686: zip-plugin has problems with special characters
    - runtime(pandoc): escape quotes in &errorformat for pandoc
    - translation(it): updated Italian manpage
    - patch 9.1.0685: too many strlen() calls in usercmd.c
    - runtime(doc): fix grammar in :h :keeppatterns
    - runtime(pandoc): refine pandoc compiler settings
    - patch 9.1.0684: completion is inserted on Enter with "noselect"
    - translation(ru): update man pages
    - patch 9.1.0683: mode() returns wrong value with <Cmd> mapping
    - runtime(doc): remove trailing whitespace in cmdline.txt
    - patch 9.1.0682: Vim9: Segfault with uninitialized funcref
    - patch 9.1.0681: tests: Analyzing failed screendumps is hard
    - runtime(doc): more clarification for the :keeppatterns needed
    - patch 9.1.0680: VMS does not have defined uintptr_t
    - runtime(doc): improve typedchar documentation for KeyInputPre autocmd
    - runtime(dist): verify that executable is in $PATH
    - translation(it): update Italian manpages
    - runtime(doc): clarify the effect of :keeppatterns after v9.1.0677
    - runtime(doc): update Makefile and make it portable between GNU and BSD
    - patch 9.1.0679: Rename from w_closing to w_locked is incomplete
    - runtime(colors): update colorschemes
    - runtime(vim): Update base-syntax, improve :let-heredoc highlighting
    - runtime(doc): Updating the examples in the xxd manpage
    - translation(ru): Updated uganda.rux
    - runtime(yaml): do not re-indent when commenting out lines

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.148 2024/08/17 22:17:14 morr Exp $
+# $NetBSD: version.mk,v 1.149 2024/08/26 19:26:31 morr Exp $
 
 VIM_VERSION=		9.1
-VIM_PATCHLEVEL=		0678
+VIM_PATCHLEVEL=		0698
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s"
 VIM_SUBDIR=		vim91
