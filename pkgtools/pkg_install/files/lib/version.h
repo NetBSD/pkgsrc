10f1d9159cb5d221b41cea3bf4c9e3d766950018
wiz <wiz@pkgsrc.org>
2024-08-26 22:37:19 +0000

    pkg_install: update to 20240826
    Fix previous: Only check REQUIRES after dependencies are installed.
    
    (My testing seems to only have happened for updates.)

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.194 2024/08/25 06:08:01 wiz Exp $	*/
+/*	$NetBSD: version.h,v 1.195 2024/08/26 22:37:19 wiz Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20240821
+#define PKGTOOLS_VERSION 20240826
 
 #endif /* _INST_LIB_VERSION_H_ */
