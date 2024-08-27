0f8f9d9b1e37b6cb43afecbe8f45900806a2dbde
sborrill <sborrill@pkgsrc.org>
2024-08-27 16:04:44 +0000

    gam: update to 6.58
    Fix COMMENT
    Allow certificates to be validated as needed for authorisation workflow
    
    Changes since 6.25:
    
    GAM 6.58
        workaround #1625 where Google is not handling max page size for print licenses correctly.
        other minor fixes and improvements.
    
    GAM 6.57
    
        "certain Enterprise SKUs" can assign a delegated admin role to a group. gam create admin helpdesk-team@acme.com  _HELP_DESK_ADMIN_ROLE customer
        Fix Linux -legacy builds were missing.
        gam version extended now includes version information for dependency Python libraries.
    
    GAM 6.56
    
        Improved support for running GAM on Google Compute Engine and other Workload Identity Federation setups.
        GAM now requires Python 3.8 or newer, 3.7 is deprecated.
        various fixes and improvements by Jay and Ross
    
    GAM 6.53
    
        afce21a: Add steps to trust GAM client_ID (Jay Lee)
        27461b0: Update var.py (Jay Lee)
        bd0bb15: AppSheet licenses (Jay Lee)
    
    GAM 6.52
    
        Ross: fix crash on gam create project
    
    GAM 6.51
    
        Upgrade OpenSSL to 3.1.0 release and completely disable insecure TLS 1.0 / 1.1 protocols.
        When rotating service account keys you can now set key expiration time with validity_hours <number>
        Ross: Improvements to Yubikey Service Account setup and usage.
        Ross: gam version checkrc command will exit with a non-zero status if there is a newer version of GAM available.
    
    GAM 6.50
    
        Run GAM securely on Google Compute Engine (GCP) VMs with no service account private key that can be stolen.
    
    GAM 6.42
    
        Allow/block content managers ability to share folders on shared drives: gam user <email> update shareddrive <id> sharingFoldersRequiresOrganizerPermission true|false. Note that Google is still rolling this feature out and the GAM command won't actually work until rollout is complete.
    
    GAM 6.41
    
        fixed #1600 unable to create new projects
    
    GAM 6.40
    
        gam copy vaultexport and gam copy storagebucket commands to copy Google Vault and Organization Takeout data to your own GCS bucket.
        Ross: gam print chromeaues command to see when your Chrome OS devices will reach auto-update expiration and gam print chromeneedsattn command to see devices that require attention.
        Upgrade to latest OpenSSL 3.0.8 and Python 3.11.2
        The usual bug fixes and improvements by Ross and Jay.
    
    GAM 6.31
    
        NEW: manage Inbound SSO profiles, credentials and assignments with GAM.
        NEW: set the display name of a user. Display name overrides the first/last name displayed to other users. gam create|update user <email> displayname "Dave Smith"
        The usual fixes by Ross and Jay
    
    GAM 6.30
    
        Upgrade to OpenSSL 3.0.7 to fix a high risk vulnerability (low chance of impact to GAM)
        Upgrade to Python 3.11.0 which offers performance improvements
        Fix: GAM was ignoring nobrowser.txt and trying to open a browser on WSL which caused a hang. #1516
        gam info domain can now be run as a delegated admin assuming proper role.
        The usual bug fixes and improvements by Ross and Jay

--- sysutils/gam/patches/patch-src_gam___init.py
+++ sysutils/gam/patches/patch-src_gam___init.py
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_gam___init.py,v 1.1 2022/10/14 12:01:18 sborrill Exp $
+$NetBSD: patch-src_gam___init.py,v 1.2 2024/08/27 16:04:44 sborrill Exp $
 
 Don't check for updates
 
---- gam/__init__.py.orig	2022-10-14 12:42:57.307510055 +0100
-+++ gam/__init__.py	2022-10-14 12:43:41.196030867 +0100
-@@ -583,8 +583,8 @@
+--- gam/__init__.py.orig	2024-08-27 15:01:56.000000000 +0100
++++ gam/__init__.py	2024-08-27 15:03:43.916076241 +0100
+@@ -585,8 +585,8 @@
          GC_Values[GC_CONFIG_DIR], FN_LAST_UPDATE_CHECK_TXT)
      GM_Globals[GM_ENABLEDASA_TXT] = os.path.join(
          GC_Values[GC_CONFIG_DIR], FN_ENABLEDASA_TXT)
 -    if not GC_Values[GC_NO_UPDATE_CHECK]:
--        doGAMCheckForUpdates()
+-        doGAMCheckForUpdates(forceCheck=0)
 +#    if not GC_Values[GC_NO_UPDATE_CHECK]:
-+#        doGAMCheckForUpdates()
++#        doGAMCheckForUpdates(forceCheck=0)
  
  # domain must be set and customer_id must be set and != my_customer when enable_dasa = true
      if GC_Values[GC_ENABLE_DASA]:
