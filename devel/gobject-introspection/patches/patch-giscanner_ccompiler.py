d326cb93c8836676e2613633c074174a0e91dab8
ryoon <ryoon@pkgsrc.org>
2024-08-30 16:31:19 +0000

    devel/gobject-introspection: Fix build with py-setuptools-74.0.0

--- devel/gobject-introspection/patches/patch-giscanner_ccompiler.py
+++ devel/gobject-introspection/patches/patch-giscanner_ccompiler.py
@@ -1,10 +1,21 @@
-$NetBSD: patch-giscanner_ccompiler.py,v 1.6 2024/04/09 17:55:46 wiz Exp $
+$NetBSD: patch-giscanner_ccompiler.py,v 1.7 2024/08/30 16:31:19 ryoon Exp $
+
+Fix build with py-setuptools-74.0.0.
 
 Exclude system paths from LD_LIBRARY_PATH.
 Do not use -Wl,--no-as-needed on SunOS.
 
 --- giscanner/ccompiler.py.orig	2024-04-01 13:21:25.000000000 +0000
 +++ giscanner/ccompiler.py
+@@ -26,7 +26,7 @@ import tempfile
+ import sys
+ import distutils
+ 
+-from distutils.msvccompiler import MSVCCompiler
++from distutils._msvccompiler import MSVCCompiler
+ from distutils.unixccompiler import UnixCCompiler
+ from distutils.cygwinccompiler import Mingw32CCompiler
+ from distutils.sysconfig import get_config_vars
 @@ -232,7 +232,7 @@ class CCompiler(object):
  
                  # Ensure libraries are always linked as we are going to use ldd to work
