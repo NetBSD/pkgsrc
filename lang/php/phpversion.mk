ba280f21996c5f0a41ac968a10b6b707d5aed919
taca <taca@pkgsrc.org>
2024-08-31 04:35:00 +0000

    lang/php82: update to 8.2.23
    PHP 8.2.23 (2024-08-29)
    
    - Core:
      . Fixed bug GH-15020 (Memory leak in Zend/Optimizer/escape_analysis.c).
        (nielsdos)
      . Fixed bug GH-15023 (Memory leak in Zend/zend_ini.c). (nielsdos)
      . Fixed bug GH-13330 (Append -Wno-implicit-fallthrough flag conditionally).
        (Peter Kokot)
      . Fix uninitialized memory in network.c. (nielsdos)
      . Fixed bug GH-15108 (Segfault when destroying generator during shutdown).
        (Arnaud)
      . Fixed bug GH-15275 (Crash during GC of suspended generator delegate).
        (Arnaud)
    
    - Curl:
      . Fixed case when curl_error returns an empty string.
        (David Carlier)
    
    - DOM:
      . Fix UAF when removing doctype and using foreach iteration. (nielsdos)
    
    - FFI:
      . Fixed bug GH-14286 (ffi enum type (when enum has no name) make memory
        leak). (nielsdos, dstogov)
    
    - Hash:
      . Fix crash when converting array data for array in shm in xxh3. (nielsdos)
    
    - Intl:
      . Fixed bug GH-15087 (IntlChar::foldCase()'s $option is not optional). (cmb)
    
    - Opcache:
      . Fixed bug GH-13817 (Segmentation fault for enabled observers after pass 4).
        (Bob)
      . Fixed bug GH-13775 (Memory leak possibly related to opcache SHM placement).
        (Arnaud, nielsdos)
    
    - Output:
      . Fixed bug GH-15179 (Segmentation fault (null pointer dereference) in
        ext/standard/url_scanner_ex.re). (nielsdos)
    
    - PDO_Firebird:
      . Fix bogus fallthrough path in firebird_handle_get_attribute(). (nielsdos)
    
    - PHPDBG:
      . Fixed bug GH-13199 (EOF emits redundant prompt in phpdbg local console mode
        with libedit/readline). (Peter Kokot)
      . Fixed bug GH-15268 (heap buffer overflow in phpdbg
        (zend_hash_num_elements() Zend/zend_hash.h)). (nielsdos)
      . Fixed bug GH-15210 use-after-free on watchpoint allocations. (nielsdos)
    
    - Soap:
      . Fixed bug #55639 (Digest autentication dont work). (nielsdos)
      . Fix SoapFault property destruction. (nielsdos)
      . Fixed bug GH-15252 (SOAP XML broken since PHP 8.3.9 when using classmap
        constructor option). (nielsdos)
    
    - Standard:
      . Fix passing non-finite timeout values in stream functions. (nielsdos)
      . Fixed GH-14780 p(f)sockopen timeout overflow. (David Carlier)
    
    - Streams:
      . Fixed bug GH-15028 (Memory leak in ext/phar/stream.c). (nielsdos)
      . Fixed bug GH-15034 (Integer overflow on stream_notification_callback
        byte_max parameter with files bigger than 2GB). (nielsdos)
    
    - Tidy:
      . Fix memory leaks in ext/tidy basedir restriction code. (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.439 2024/08/02 15:33:10 taca Exp $
+# $NetBSD: phpversion.mk,v 1.440 2024/08/31 04:35:00 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.29
-PHP82_VERSION=	8.2.22
+PHP82_VERSION=	8.2.23
 PHP83_VERSION=	8.3.10
 
 # Define API version or initial release of major version.
