776ce2a886c0d7f57ef7c5eca6f3d08c870e34c2
gdt <gdt@pkgsrc.org>
2024-09-01 22:28:58 +0000

    geography/proj: Fix buildlink path (from wip update)

--- geography/proj/buildlink3.mk
+++ geography/proj/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.24 2024/09/01 14:56:33 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2024/09/01 22:28:58 gdt Exp $
 
 BUILDLINK_TREE+=	proj
 
 .if !defined(PROJ_BUILDLINK3_MK)
-
 PROJ_BUILDLINK3_MK:=
 
 # Proj withdrew the old API with 8.0.0.
 BUILDLINK_API_DEPENDS.proj+=	proj>=8.0.0
 BUILDLINK_ABI_DEPENDS.proj+=	proj>=9.4.1
-BUILDLINK_PKGSRCDIR.proj?=	../../wip/proj
+BUILDLINK_PKGSRCDIR.proj?=	../../geography/proj
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
