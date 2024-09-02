2aa211a205e51faa4e85e42066535f92fc4503bb
maya <maya@pkgsrc.org>
2024-09-02 17:13:24 +0000

    Add grpc-go 1.66.0
    The Go implementation of gRPC: A high performance, open source, general
    RPC framework that puts mobile and HTTP/2 first. For more information see the
    Go gRPC docs.

---
+++ net/grpc-go/go-modules.mk
@@ -0,0 +1,14 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/09/02 17:13:24 maya Exp $
+
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.26.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.26.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.16.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.16.0.zip
+GO_MODULE_FILES+=	google.golang.org/genproto/googleapis/rpc/@v/v0.0.0-20240604185151-ef581f913117.mod
+GO_MODULE_FILES+=	google.golang.org/genproto/googleapis/rpc/@v/v0.0.0-20240604185151-ef581f913117.zip
+GO_MODULE_FILES+=	google.golang.org/grpc/@v/v1.65.0.mod
+GO_MODULE_FILES+=	google.golang.org/grpc/@v/v1.65.0.zip
+GO_MODULE_FILES+=	google.golang.org/protobuf/@v/v1.34.1.mod
+GO_MODULE_FILES+=	google.golang.org/protobuf/@v/v1.34.1.zip
