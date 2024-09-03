8d2ea2a893872c6a19f71a9b360b9aab38ab4fd8
morr <morr@pkgsrc.org>
2024-09-03 18:31:21 +0000

    update to patchlevel 9.1.0708.
    Changelog:
    
    - patch 9.1.0709: GUIEnter event not found in Turkish locale
    - runtime(sudoers): improve recognized Runas_Spec and Tag_Spec items
    - patch 9.1.0708: Recursive window update does not account for reset skipcol
    - runtime(nu): include filetype plugin
    - patch 9.1.0707: [security]: invalid cursor position may cause a crash
    - patch 9.1.0706: tests: test_gettext fails when using shadow dir
    - CI: Install locales-all package
    - patch 9.1.0705: Sorting of fuzzy filename completion is not stable
    - translation(pt): update Portuguese/Brazilian menu translation
    - runtime(vim): Update base-syntax, match bracket mark ranges
    - runtime(doc): Update :help :command-complete list
    - patch 9.1.0704: inserting with a count is inefficient
    - runtime(doc): use mkdir -p to save a command
    - patch 9.1.0703: crash with 2byte encoding and glob2regpat()
    - runtime(hollywood): update syn highlight for If-Then statements and For-In-Loops
    - patch 9.1.0702: Patch 9.1.0700 broke CI
    - patch 9.1.0701: crash with NFA regex engine when searching for composing chars
    - patch 9.1.0700: crash with 2byte encoding and glob2regpat()
    - patch 9.1.0699: "dvgo" is not always an inclusive motion
    - runtime(java): Provide support for syntax preview features

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.149 2024/08/26 19:26:31 morr Exp $
+# $NetBSD: version.mk,v 1.150 2024/09/03 18:31:21 morr Exp $
 
 VIM_VERSION=		9.1
-VIM_PATCHLEVEL=		0698
+VIM_PATCHLEVEL=		0709
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s"
 VIM_SUBDIR=		vim91
