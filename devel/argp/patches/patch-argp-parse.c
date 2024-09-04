2ab0472790e152b0f44be959730c847deea48b62
jperkin <jperkin@pkgsrc.org>
2024-09-04 11:52:38 +0000

    argp: Fix build on SunOS.

--- devel/argp/patches/patch-argp-parse.c
+++ devel/argp/patches/patch-argp-parse.c
@@ -1,11 +1,23 @@
-$NetBSD: patch-argp-parse.c,v 1.1 2023/03/12 12:38:56 js Exp $
+$NetBSD: patch-argp-parse.c,v 1.2 2024/09/04 11:52:38 jperkin Exp $
 
 Remove __THROW that might be defined to __attribute__((nothrow)) and would thus
 cause the compilation to fail.
+SunOS needs alloca.h.
 
 --- argp-parse.c.orig	2003-12-11 08:37:05.000000000 +0000
 +++ argp-parse.c
-@@ -1277,13 +1277,13 @@ weak_alias (__argp_input, _argp_input)
+@@ -26,6 +26,10 @@
+ #include <config.h>
+ #endif
+ 
++#ifdef __sun
++#include <alloca.h>
++#endif
++
+ /* AIX requires this to be the first thing in the file.  */
+ #ifndef __GNUC__
+ # if HAVE_ALLOCA_H
+@@ -1277,13 +1281,13 @@ weak_alias (__argp_input, _argp_input)
  /* Defined here, in case a user is not inlining the definitions in
   * argp.h */
  void
@@ -21,7 +33,7 @@ cause the compilation to fail.
  {
    if (__opt->flags & OPTION_DOC)
      return 0;
-@@ -1297,7 +1297,7 @@ __option_is_short (__const struct argp_o
+@@ -1297,7 +1301,7 @@ __option_is_short (__const struct argp_o
  }
  
  int
