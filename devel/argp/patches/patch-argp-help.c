b38ce05fb20b50915f321a1d026b9b5385478b0e
jperkin <jperkin@pkgsrc.org>
2024-09-04 11:52:38 +0000

    argp: Fix build on SunOS.

--- devel/argp/patches/patch-argp-help.c
+++ devel/argp/patches/patch-argp-help.c
@@ -1,10 +1,22 @@
-$NetBSD: patch-argp-help.c,v 1.1 2024/08/06 18:50:08 riastradh Exp $
+$NetBSD: patch-argp-help.c,v 1.2 2024/09/04 11:52:38 jperkin Exp $
 
 Use getprogname() as short program name if available.
+SunOS needs alloca.h.
 
 --- argp-help.c.orig	2003-12-11 08:37:05.000000000 +0000
 +++ argp-help.c
-@@ -1725,6 +1725,8 @@ __argp_short_program_name(const struct a
+@@ -26,6 +26,10 @@
+ #include <config.h>
+ #endif
+ 
++#ifdef __sun
++#include <alloca.h>
++#endif
++
+ /* AIX requires this to be the first thing in the file.  */
+ #ifndef __GNUC__
+ # if HAVE_ALLOCA_H
+@@ -1725,6 +1729,8 @@ __argp_short_program_name(const struct a
    return program_invocation_short_name;
  #elif HAVE_DECL_PROGRAM_INVOCATION_NAME
    return __argp_basename(program_invocation_name);
