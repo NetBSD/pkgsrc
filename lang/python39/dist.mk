08841759651a9be4c32822c1676dcb3b64b17487
adam <adam@pkgsrc.org>
2024-09-09 15:48:22 +0000

    python39 py39-html-docs: updated to 3.9.20
    Python 3.9.20 final
    
    Windows
    
    gh-119690: Fixes data type confusion in audit events raised by _winapi.CreateFile and _winapi.CreateNamedPipe.
    gh-116773: Fix instances of <_overlapped.Overlapped object at 0xXXX> still has pending operation at deallocation, the process may crash.
    
    Tests
    
    gh-112769: The tests now correctly compare zlib version when zlib.ZLIB_RUNTIME_VERSION contains non-integer suffixes. For example zlib-ng defines the version as 1.3.0.zlib-ng.
    gh-117187: Fix XML tests for vanilla Expat <2.6.0.
    
    Security
    
    gh-123678: Upgrade libexpat to 2.6.3
    gh-121957: Fixed missing audit events around interactive use of Python, now also properly firing for python -i, as well as for python -m asyncio. The event in question is cpython.run_stdin.
    gh-122133: Authenticate the socket connection for the socket.socketpair() fallback on platforms where AF_UNIX is not available like Windows.
    
    Patch by Gregory P. Smith <greg@krypto.org> and Seth Larson <seth@python.org>. Reported by Ellie <el@horse64.org>
    gh-121285: Remove backtracking from tarfile header parsing for hdrcharset, PAX, and GNU sparse headers.
    gh-118486: os.mkdir() on Windows now accepts mode of 0o700 to restrict the new directory to the current user. This fixes CVE-2024-4030 affecting tempfile.mkdtemp() in scenarios where the base temporary directory is more permissive than the default.
    gh-114572: ssl.SSLContext.cert_store_stats() and ssl.SSLContext.get_ca_certs() now correctly lock access to the certificate store, when the ssl.SSLContext is shared across multiple threads.
    gh-116741: Update bundled libexpat to 2.6.2
    
    Library
    
    gh-123270: Applied a more surgical fix for malformed payloads in zipfile.Path causing infinite loops (gh-122905) without breaking contents using legitimate characters.
    gh-123067: Fix quadratic complexity in parsing "-quoted cookie values with backslashes by http.cookies.
    gh-121650: email headers with embedded newlines are now quoted on output. The generator will now refuse to serialize (write) headers that are unsafely folded or delimited; see verify_generated_headers. (Contributed by Bas Bloemsaat and Petr Viktorin in gh-121650.)
    gh-113171: Fixed various false positives and false negatives in
    
    ipaddress.IPv4Address.is_private (see these docs for details)
    ipaddress.IPv4Address.is_global
    ipaddress.IPv6Address.is_private
    ipaddress.IPv6Address.is_global
    Also in the corresponding ipaddress.IPv4Network and ipaddress.IPv6Network attributes.
    gh-102988: email.utils.getaddresses() and email.utils.parseaddr() now return ('', '') 2-tuples in more situations where invalid email addresses are encountered instead of potentially inaccurate values. Add optional strict parameter to these two functions: use strict=False to get the old behavior, accept malformed inputs. getattr(email.utils, 'supports_strict_parsing', False) can be use to check if the strict paramater is available. Patch by Thomas Dwyer and Victor Stinner to improve the CVE-2023-27043 fix.
    gh-67693: Fix urllib.parse.urlunparse() and urllib.parse.urlunsplit() for URIs with path starting with multiple slashes and no authority. Based on patch by Ashwin Ramaswami.
    
    Core and Builtins
    
    gh-112275: A deadlock involving pystate.c’s HEAD_LOCK in posixmodule.c at fork is now fixed. Patch by ChuBoning based on previous Python 3.12 fix by Victor Stinner.

--- lang/python39/dist.mk
+++ lang/python39/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.20 2024/03/20 15:41:01 adam Exp $
+# $NetBSD: dist.mk,v 1.21 2024/09/09 15:48:22 adam Exp $
 
-PY_DISTVERSION=	3.9.19
+PY_DISTVERSION=	3.9.20
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python39/distinfo
