62c565244375d173a24503616cac1faeb0a28296
schmonz <schmonz@pkgsrc.org>
2024-09-10 20:08:29 +0000

    fehqlibs: update to 25a. Changes:
    - Removed 'register' in functions. Added 'const' for input strings.
    - Compatibility with current gcc and clang compilers:
    - (Internal) Unrolling for() loops removed. Internal functions are
      static now.
    - (Internal) buffer_init uses now 'buffer_unixread' and
      'buffer_unixwrite' with matching signatures.
    - (External) Added getopb() as external and thus removing getop()
      compatibility.
    - (Installation) Moved C source files to ./src. Added INSTALL.md to
      describe changes.
    - (Compilation) Added -fwrapv flag to the compiler (correctly handling
      two's complement).
    - (Internal) alloc_re()'s first argument is now a void pointer to a list
      [gcc 14.2].

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2021/10/21 22:28:42 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/09/10 20:08:29 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
 .if !defined(FEHQLIBS_BUILDLINK3_MK)
 FEHQLIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fehqlibs+=	fehqlibs>=19
+BUILDLINK_API_DEPENDS.fehqlibs+=	fehqlibs>=25
 BUILDLINK_PKGSRCDIR.fehqlibs?=		../../net/fehqlibs
 BUILDLINK_INCDIRS.fehqlibs+=		include/qlibs
 BUILDLINK_LIBDIRS.fehqlibs+=		lib/qlibs
