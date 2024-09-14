cb2f572810c10db75fb1aecd7c1665d3f6f019d9
adam <adam@pkgsrc.org>
2024-09-14 08:00:13 +0000

    haproxy: updated to 3.0.4
    3.0.4
    - MINOR: proto: extend connection thread rebind API
    - BUILD: listener: silence a build warning about unused value without threads
    - BUG/MEDIUM: quic: prevent crash on accept queue full
    - CLEANUP: proto: rename TID affinity callbacks
    - CLEANUP: quic: rename TID affinity elements
    - BUG/MINOR: session: Eval L4/L5 rules defined in the default section
    - BUG/MEDIUM: debug/cli: fix "show threads" crashing with low thread counts
    - DOC: install: don't reference removed CPU arg
    - BUG/MEDIUM: ssl_sock: fix deadlock in ssl_sock_load_ocsp() on error path
    - BUG/MAJOR: mux-h2: force a hard error upon short read with pending error
    - DOC: configuration: issuers-chain-path not compatible with OCSP
    - DOC: config: improve the http-keep-alive section
    - BUG/MINOR: stick-table: fix crash for src_inc_gpc() without stkcounter
    - BUG/MINOR: server: Don't warn fallback IP is used during init-addr resolution
    - BUG/MINOR: cli: Atomically inc the global request counter between CLI commands
    - BUG/MINOR: quic: Non optimal first datagram.
    - MEDIUM: sink: don't set NOLINGER flag on the outgoing stream interface
    - BUG/MINOR: quic: Lack of precision when computing K (cubic only cc)
    - BUG/MEDIUM: jwt: Clear SSL error queue on error when checking the signature
    - MINOR: quic: Dump TX in flight bytes vs window values ratio.
    - MINOR: quic: Add information to "show quic" for CUBIC cc.
    - MEDIUM: h1: allow to preserve keep-alive on T-E + C-L
    - MINOR: queue: add a function to check for TOCTOU after queueing
    - BUG/MEDIUM: queue: deal with a rare TOCTOU in assign_server_and_queue()
    - MEDIUM: init: set default for fd_hard_limit via DEFAULT_MAXFD (take 2)
    - BUG/MEDIUM: init: fix fd_hard_limit default in compute_ideal_maxconn
    - Revert "MEDIUM: sink: don't set NOLINGER flag on the outgoing stream interface"
    - MEDIUM: log: relax some checks and emit diag warnings instead in lf_expr_postcheck()
    - DOC: quic: fix default minimal value for max window size
    - MINOR: proxy: Add support of 429-Too-Many-Requests in retry-on status
    - BUG/MEDIUM: mux-h2: Set ES flag when necessary on 0-copy data forwarding
    - BUG/MEDIUM: stream: Prevent mux upgrades if client connection is no longer ready
    - BUG/MINIR: proxy: Match on 429 status when trying to perform a L7 retry
    - BUG/MEDIUM: mux-pt: Never fully close the connection on shutdown
    - BUG/MEDIUM: cli: Always release back endpoint between two commands on the mcli
    - BUG/MINOR: quic: unexploited retransmission cases for Initial pktns.
    - BUG/MEDIUM: mux-h1: Properly handle empty message when an error is triggered
    - MINOR: mux-h2: try to clear DEM_MROOM and MUX_MFULL at more places
    - BUG/MAJOR: mux-h2: always clear MUX_MFULL and DEM_MROOM when clearing the mbuf
    - BUG/MINOR: quic: Too shord datagram during O-RTT handshakes (aws-lc only)
    - BUG/MINOR: Crash on O-RTT RX packet after dropping Initial pktns
    - BUG/MEDIUM: mux-pt: Fix condition to perform a shutdown for writes in mux_pt_shut()

--- net/haproxy/patches/patch-src_queue.c
+++ net/haproxy/patches/patch-src_queue.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_queue.c,v 1.1 2021/12/09 10:36:07 jperkin Exp $
+$NetBSD: patch-src_queue.c,v 1.2 2024/09/14 08:00:13 adam Exp $
 
 Avoid 'struct queue' conflict.
 
---- src/queue.c.orig	2021-11-23 14:40:21.000000000 +0000
+--- src/queue.c.orig	2024-09-03 13:37:09.000000000 +0000
 +++ src/queue.c
-@@ -178,7 +178,7 @@ static inline void pendconn_queue_unlock
+@@ -177,7 +177,7 @@ static inline void pendconn_queue_unlock
   */
  void pendconn_unlink(struct pendconn *p)
  {
@@ -13,7 +13,7 @@ Avoid 'struct queue' conflict.
  	struct proxy  *px = q->px;
  	struct server *sv = q->sv;
  	uint oldidx;
-@@ -423,7 +423,7 @@ struct pendconn *pendconn_add(struct str
+@@ -430,7 +430,7 @@ struct pendconn *pendconn_add(struct str
  	struct pendconn *p;
  	struct proxy    *px;
  	struct server   *srv;
@@ -22,3 +22,12 @@ Avoid 'struct queue' conflict.
  	unsigned int *max_ptr;
  	unsigned int old_max, new_max;
  
+@@ -630,7 +630,7 @@ int pendconn_dequeue(struct stream *strm
+  */
+ int pendconn_must_try_again(struct pendconn *p)
+ {
+-	struct queue  *q  = p->queue;
++	struct haqueue *q  = p->queue;
+ 	struct proxy  *px = q->px;
+ 	struct server *sv = q->sv;
+ 	int ret = 0;
