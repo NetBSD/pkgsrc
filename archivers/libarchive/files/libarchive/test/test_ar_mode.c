27cc4d0936ad8bcc1e0b34ad64b976bbca25f3fc
adam <adam@pkgsrc.org>
2024-09-15 06:45:18 +0000

    libarchive: imported version 3.7.5
    Libarchive 3.7.5
    
    Security fixes:
    
    fix multiple vulnerabilities identified by SAST
    cpio: ignore out-of-range gid/uid/size/ino and harden AFIO parsing
    lzop: prevent integer overflow
    rar4: protect copy_from_lzss_window_to_unp()
    rar4: fix CVE-2024-26256
    rar4: fix OOB in delta and audio filter
    rar4: fix out of boundary access with large files
    rar4: add boundary checks to rgb filter
    rar4: fix OOB access with unicode filenames
    rar5: clear 'data ready' cache on window buffer reallocs
    rpm: calculate huge header sizes correctly
    unzip: unify EOF handling
    util: fix out of boundary access in mktemp functions
    uu: stop processing if lines are too long
    
    Important bugfixes:
    
    7zip: fix issue when skipping first file in 7zip archive that is a multiple of 65536 bytes
    ar: fix archive entries having no type
    lha: do not allow negative file sizes
    lha: fix integer truncation on 32-bit systems
    shar: check strdup return value
    rar5: don't try to read rediculously long names
    xar: fix another infinite loop and expat error handling
    many Windows fixes, cleanups and improvements

--- archivers/libarchive/files/libarchive/test/test_ar_mode.c
+++ archivers/libarchive/files/libarchive/test/test_ar_mode.c
@@ -1,13 +1,11 @@
-/*
- * Copyright (c) 2023 Adrian Vovk
- * All rights reserved.
+/*-SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (C) 2024 by наб <nabijaczleweli@nabijaczleweli.xyz>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer
- *    in this position and unchanged.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -25,23 +23,18 @@
  */
 #include "test.h"
 
-/* Test o arg - overwrite existing files */
-DEFINE_TEST(test_o)
+static const char data[] = "!<arch>\narchivemount.1/ 0           0     0     644     0         `\n";
+
+
+DEFINE_TEST(test_ar_mode)
 {
-	const char *reffile = "test_basic.zip";
-	int r;
+	struct archive * ar = archive_read_new();
+	assertEqualInt(archive_read_support_format_all(ar), ARCHIVE_OK);
+	assertEqualInt(archive_read_open_memory(ar, data, sizeof(data) - 1), ARCHIVE_OK);
 
-	assertMakeDir("test_basic", 0755);
-	assertMakeFile("test_basic/a", 0644, "orig a\n");
-	assertMakeFile("test_basic/b", 0644, "orig b\n");
+	struct archive_entry * entry;
+	assertEqualIntA(ar, archive_read_next_header(ar, &entry), ARCHIVE_OK);
+	assertEqualIntA(ar, archive_entry_mode(entry), S_IFREG | 0644);
 
-	extract_reference_file(reffile);
-	r = systemf("%s -o %s >test.out 2>test.err", testprog, reffile);
-	assertEqualInt(0, r);
-	assertEmptyFile("test.err");
-
-	assertTextFileContents("contents a\n", "test_basic/a");
-	assertTextFileContents("contents b\n", "test_basic/b");
-	assertTextFileContents("contents c\n", "test_basic/c");
-	assertTextFileContents("contents CAPS\n", "test_basic/CAPS");
+	archive_read_free(ar);
 }
