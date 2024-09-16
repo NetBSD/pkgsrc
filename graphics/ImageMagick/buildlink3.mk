1a941f5ac717b314a4305ec7520c3a83c79d64f6
adam <adam@pkgsrc.org>
2024-09-16 07:20:13 +0000

    Revbump for x265

--- graphics/ImageMagick/buildlink3.mk
+++ graphics/ImageMagick/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.113 2024/07/11 19:53:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.114 2024/09/16 07:20:14 adam Exp $
 
 BUILDLINK_TREE+=	ImageMagick
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ImageMagick
 IMAGEMAGICK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ImageMagick+=	ImageMagick>=5.5.7.11nb1
-BUILDLINK_ABI_DEPENDS.ImageMagick+=	ImageMagick>=7.1.1.29nb4
+BUILDLINK_ABI_DEPENDS.ImageMagick+=	ImageMagick>=7.1.1.35nb1
 BUILDLINK_PKGSRCDIR.ImageMagick?=	../../graphics/ImageMagick
 BUILDLINK_INCDIRS.ImageMagick+=		include/ImageMagick-7
 
