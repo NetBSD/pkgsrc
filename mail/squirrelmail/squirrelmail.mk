6006608535902bab6f5321104466972a088e3d83
taca <taca@pkgsrc.org>
2024-09-18 14:25:50 +0000

    mail/squirrelmail: update to 1.4.23pre15028
    pkgsrc change: remove a pkglint warning in squirrelmail.mk.
    
    1.4.23pre15028 is latest snapshot of squirrelmail 1.4 stable branch.
    
    * Reduce warnings especially PHP 8.3.

--- mail/squirrelmail/squirrelmail.mk
+++ mail/squirrelmail/squirrelmail.mk
@@ -1,7 +1,7 @@
-# $NetBSD: squirrelmail.mk,v 1.1 2023/11/13 15:59:03 taca Exp $
+# $NetBSD: squirrelmail.mk,v 1.2 2024/09/18 14:25:50 taca Exp $
 
-.if !defined(_SQUIRRELMAIL_MK)
-_SQUIRRELMAIL_MK=	# defined
+.if !defined(SQUIRRELMAIL_MK)
+SQUIRRELMAIL_MK=	# defined
 
 EGDIR=			share/examples/${PHP_PKG_PREFIX}/squirrelmail
 SMDIR=			share/${PHP_PKG_PREFIX}/squirrelmail
@@ -15,4 +15,4 @@ PLIST_SUBST+=		EGDIR=${EGDIR} SMDIR=${SMDIR}
 PRINT_PLIST_AWK+=	/^${EGDIR:S|/|\\/|g}/ { sub(/${EGDIR:S|/|\\/|g}/, "$${EGDIR}") }
 PRINT_PLIST_AWK+=	/^${SMDIR:S|/|\\/|g}/ { sub(/${SMDIR:S|/|\\/|g}/, "$${SMDIR}") }
 
-.endif # _SQUIRRELMAIL_MK
+.endif # SQUIRRELMAIL_MK
