6e0594f12c5736b2444e593be3d1dff5c5c798a6
tsutsui <tsutsui@pkgsrc.org>
2024-09-21 06:39:33 +0000

    ruby-gnome: update to 4.2.3.
    Ruby-GNOME 4.2.3: 2024-09-19
    
    This is a GTK 4 support improvement release.
    
    Changes
    
    All
    
      * Improvements
          + Update project website URL.
              o GH-1611
              o GH-1615
              o Patch by Kazuhiro NISHIYAMA
          + Dropped support for CentOS 7.
    
    Ruby/GLib2
    
      * Improvements
          + Added GLib::VariantDict.
          + Added GLib::OptionArg.
          + Added GLib::OptionFlags.
          + Added GLib.get_os_info.
          + GLib::Param::*.new: Added support for flag names.
          + GLib::Param::*.new: Added support for nil name and blurb.
    
    Ruby/GObjectIntrospection
    
      * Improvements
          + Added support auto setter generation for class methods.
          + Added support auto setter generation for base module methods.
          + Added support for nullable of callback return value.
          + Added support for nullable GHashTable.
    
    Ruby/GIO2
    
      * Improvements
          + Made Gio::ListModel enumerable.
          + Added Gio::ListModel#[].
    
    Ruby/GdkPixbuf2
    
      * Improvements
          + Removed tests that use XPM.
              o GH-1614
              o Reported by Mamoru TASAKA
    
    Ruby/GTK4
    
      * Improvements
          + Renamed Gtk::License::0BSD to Gtk::License::BSD_0.
              o GH-1618
              o Reported by rubyFeedback
              o Reported by kojix2
          + Gtk::TextBuffer#begin_irreversible_action: Added support for
            block.
    
    Thanks
    
      * Mamoru TASAKA
      * Kazuhiro NISHIYAMA
      * rubyFeedback
      * kojix2

--- devel/ruby-gio2/buildlink3.mk
+++ devel/ruby-gio2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/04/20 10:16:53 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/09/21 06:39:33 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gio2
 
 .if !defined(RUBY_GIO2_BUILDLINK3_MK)
 RUBY_GIO2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.2
-BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.2
+BUILDLINK_API_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.3
+BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.3
 BUILDLINK_PKGSRCDIR.ruby-gio2?=		../../devel/ruby-gio2
 
 .include "../../devel/ruby-gobject-introspection/buildlink3.mk"
