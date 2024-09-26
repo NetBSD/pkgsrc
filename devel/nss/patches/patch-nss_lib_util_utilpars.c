83b818d577928b696209d8599a102aad8491845c
wiz <wiz@pkgsrc.org>
2024-09-26 21:00:22 +0000

    nss: update to 3.105.
    Bug 1915792 - Allow importing PKCS#8 private EC keys missing public key
    Bug 1909768 - UBSAN fix: applying zero offset to null pointer in sslsnce.c
    Bug 1919577 - set KRML_MUSTINLINE=inline in makefile builds
    Bug 1918965 - Don't set CKA_SIGN for CKK_EC_MONTGOMERY private keys
    Bug 1918767 - override default definition of KRML_MUSTINLINE
    Bug 1916525 - libssl support for mlkem768x25519
    Bug 1916524 - support for ML-KEM-768 in softoken and pk11wrap
    Bug 1866841 - Add Libcrux implementation of ML-KEM 768 to FreeBL
    Bug 1911912 - Avoid misuse of ctype(3) functions
    Bug 1917311 - part 2: run clang-format
    Bug 1917311 - part 1: upgrade to clang-format 13
    Bug 1916953 - clang-format fuzz
    Bug 1910370 - DTLS client message buffer may not empty be on retransmit
    Bug 1916413 - Optionally print config for TLS client and server fuzz target
    Bug 1916059 - Fix some simple documentation issues in NSS.
    Bug 1915439 - improve performance of NSC_FindObjectsInit when template has CKA_TOKEN attr
    Bug 1912828 - define CKM_NSS_ECDHE_NO_PAIRWISE_CHECK_KEY_PAIR_GEN

--- devel/nss/patches/patch-nss_lib_util_utilpars.c
+++ devel/nss/patches/patch-nss_lib_util_utilpars.c
@@ -1,20 +1,9 @@
-$NetBSD: patch-nss_lib_util_utilpars.c,v 1.2 2022/04/07 19:08:40 riastradh Exp $
+$NetBSD: patch-nss_lib_util_utilpars.c,v 1.3 2024/09/26 21:00:23 wiz Exp $
 
 Revert Bug 1377940 to fix misc/libreoffice
 
-Fix ctype(3) abuse: https://bugzilla.mozilla.org/show_bug.cgi?id=1246768
-
 --- nss/lib/util/utilpars.c.orig	2022-03-03 10:18:53.000000000 +0000
 +++ nss/lib/util/utilpars.c
-@@ -274,7 +274,7 @@ NSSUTIL_ArgDecodeNumber(const char *num)
-     }
- 
-     for (; *num; num++) {
--        if (isdigit(*num)) {
-+        if (isdigit((unsigned char)*num)) {
-             digit = *num - '0';
-         } else if ((*num >= 'a') && (*num <= 'f')) {
-             digit = *num - 'a' + 10;
 @@ -1197,8 +1197,12 @@ _NSSUTIL_EvaluateConfigDir(const char *c
      NSSDBType dbType;
      PRBool checkEnvDefaultDB = PR_FALSE;
