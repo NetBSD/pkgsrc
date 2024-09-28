ff424b5c4cc2386078cf6970ae1170f9688d46e6
taca <taca@pkgsrc.org>
2024-09-28 15:08:01 +0000

    lang/php81: update to 8.1.30
    PHP 8.1.30 (2024-09-26)
    
    - CGI:
      . Fixed bug GHSA-p99j-rfp4-xqvq (Bypass of CVE-2024-4577, Parameter Injection
        Vulnerability). (CVE-2024-8926) (nielsdos)
      . Fixed bug GHSA-94p6-54jq-9mwp (cgi.force_redirect configuration is
        bypassable due to the environment variable collision). (CVE-2024-8927)
        (nielsdos)
    
    - FPM:
      . Fixed bug GHSA-865w-9rf3-2wh5 (Logs from childrens may be altered).
        (CVE-2024-9026) (Jakub Zelenka)
    
    - SAPI:
      . Fixed bug GHSA-9pqp-7h25-4f32 (Erroneous parsing of multipart form data).
        (CVE-2024-8925) (Arnaud)
    
    06 Jun 2024, PHP 8.1.29
    
    - CGI:
      . Fixed bug GHSA-3qgc-jrrr-25jv (Bypass of CVE-2012-1823, Argument Injection
        in PHP-CGI). (CVE-2024-4577) (nielsdos)
    
    - Filter:
      . Fixed bug GHSA-w8qr-v226-r27w (Filter bypass in filter_var FILTER_VALIDATE_URL).
        (CVE-2024-5458) (nielsdos)
    
    - OpenSSL:
      . The openssl_private_decrypt function in PHP, when using PKCS1 padding
        (OPENSSL_PKCS1_PADDING, which is the default), is vulnerable to the Marvin Attack
        unless it is used with an OpenSSL version that includes the changes from this pull
        request: https://github.com/openssl/openssl/pull/13817 (rsa_pkcs1_implicit_rejection).
        These changes are part of OpenSSL 3.2 and have also been backported to stable
        versions of various Linux distributions, as well as to the PHP builds provided for
        Windows since the previous release. All distributors and builders should ensure that
        this version is used to prevent PHP from being vulnerable. (CVE-2024-2408)
    
    - Standard:
      . Fixed bug GHSA-9fcc-425m-g385 (Bypass of CVE-2024-1874).
        (CVE-2024-5585) (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.443 2024/09/28 15:05:35 taca Exp $
+# $NetBSD: phpversion.mk,v 1.444 2024/09/28 15:08:01 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
-PHP81_VERSION=	8.1.29
+PHP81_VERSION=	8.1.30
 PHP82_VERSION=	8.2.24
 PHP83_VERSION=	8.3.12
 
