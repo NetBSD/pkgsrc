43df882ece4c37616d493f13315863acf4d10f92
schmonz <schmonz@pkgsrc.org>
2024-09-29 18:58:37 +0000

    libgpg-error: Avoid nonportable environ global.
    Fixes Tiger build, doesn't break Sequoia or NetBSD 10.

--- security/libgpg-error/patches/patch-src_spawn-posix.c
+++ security/libgpg-error/patches/patch-src_spawn-posix.c
@@ -1,15 +1,30 @@
-$NetBSD: patch-src_spawn-posix.c,v 1.1 2024/06/26 19:31:56 adam Exp $
+$NetBSD: patch-src_spawn-posix.c,v 1.2 2024/09/29 18:58:37 schmonz Exp $
 
-environ must be declared.
+Avoid nonportable environ global.
+Via <https://dev.gnupg.org/T7169#190989>
 
 --- src/spawn-posix.c.orig	2024-06-19 07:33:41.000000000 +0000
 +++ src/spawn-posix.c
-@@ -53,6 +53,8 @@
- #if __linux__
- # include <sys/types.h>
- # include <dirent.h>
-+#else
-+extern char **environ;
- #endif /*__linux__ */
+@@ -341,9 +341,6 @@ my_exec (const char *pgmname, const char
+   /* Close all other files.  */
+   _gpgrt_close_all_fds (3, act->except_fds);
  
- #include "gpgrt-int.h"
+-  if (act->environ)
+-    environ = act->environ;
+-
+   if (act->atfork)
+     act->atfork (act->atfork_arg);
+ 
+@@ -351,7 +348,11 @@ my_exec (const char *pgmname, const char
+   if (pgmname == NULL)
+     return 0;
+ 
+-  execv (pgmname, (char *const *)argv);
++  if (act->environ)
++    execve (pgmname, (char *const *)argv, act->environ);
++  else
++    execv (pgmname, (char *const *)argv);
++
+   /* No way to print anything, as we have may have closed all streams. */
+   _exit (127);
+   return -1;
