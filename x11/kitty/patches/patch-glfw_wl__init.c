7732061969c7337f95b76752a6d19fa55f6e79a1
ktnb <ktnb@pkgsrc.org>
2024-09-30 17:51:45 +0000

    kitty: update to 0.36.4
    Packaging Changes:
    - new go version
    - now requires Nerd Font symbols to build
    
    Upstream Changes:
    Many bug fixes from versions 0.35.2 -> 0.36.4. 0.36.x now has
    variable font support.

--- x11/kitty/patches/patch-glfw_wl__init.c
+++ x11/kitty/patches/patch-glfw_wl__init.c
@@ -1,30 +1,20 @@
-$NetBSD: patch-glfw_wl__init.c,v 1.2 2024/05/30 15:00:54 ktnb Exp $
+$NetBSD: patch-glfw_wl__init.c,v 1.3 2024/09/30 17:51:45 ktnb Exp $
 
 Support non-evdev platforms.
 
---- glfw/wl_init.c.orig	2024-05-25 03:37:36.000000000 +0000
+--- glfw/wl_init.c.orig	2024-09-25 04:04:28.000000000 +0000
 +++ glfw/wl_init.c
-@@ -44,6 +44,7 @@
- #include <sys/socket.h>
- #include <wayland-client.h>
- #include <stdio.h>
-+#include <errno.h>
- // Needed for the BTN_* defines
- #ifdef __has_include
- #if __has_include(<linux/input.h>)
-@@ -51,9 +52,13 @@
+@@ -53,8 +53,11 @@
  #elif __has_include(<dev/evdev/input.h>)
  #include <dev/evdev/input.h>
  #endif
 -#else
 -#include <linux/input.h>
- #endif
++#endif
 +#ifndef BTN_LEFT
 +#define BTN_LEFT	(0x110)
 +#define BTN_RIGHT	(0x111)
 +#define BTN_MIDDLE	(0x112)
-+#endif
-+
+ #endif
  
  #define debug debug_rendering
- 
