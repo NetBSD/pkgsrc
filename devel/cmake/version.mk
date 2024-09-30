8ec82bbfe73e6d90ec15ed70e0fc6ea623855ee0
adam <adam@pkgsrc.org>
2024-09-30 17:07:17 +0000

    cmake: updated to 3.30.4
    CMake 3.30.4
    * gitlab-ci: update macOS jobs to use Xcode 16.0
    * ci: Run RunCMake.ExternalProject serially in macos-x86_64 Xcode job
    * Help: Add 3.30 release notes for project variable updates
    * Help: file(CONFIGURE) only updates output file timestamp if it changes
    * FetchContent: Don't update verify script timestamp if it doesn't change
    * Help: State valid scopes for using proj_SOURCE_DIR and proj_BINARY_DIR
    * project: Only define non-cache vars if already defined
    * CPack/NuGet: Add native0.0 to build dependencies in groups
    * librhash: aligned_alloc is not available with glibc < 2.16
    * FindMatlab: Only consider versioned MCR subdirectory if it exists
    * FindMatlab: Add support for MCR distributions without mex.h
    * FindMatlab: Cache whether MATLAB root contains MCR or MATLAB
    * FindMatlab: Search mex and mx libraries for MCR
    * FindPython: ensure all ABI are searched by default
    * FindMatlab: Add R2024b version map

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.65 2024/08/30 08:43:37 adam Exp $
+# $NetBSD: version.mk,v 1.66 2024/09/30 17:07:17 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.30.3
+CMAKE_VERSION=	3.30.4
 CMAKE_API=	${CMAKE_VERSION:R}
