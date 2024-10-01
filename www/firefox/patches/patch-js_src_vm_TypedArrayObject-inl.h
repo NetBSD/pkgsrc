3b0bf81055f7ec84c139948de00b05c81a7749d3
ryoon <ryoon@pkgsrc.org>
2024-10-01 12:47:18 +0000

    www/firefox: Update to 130.0.1
    Changelog:
    130.0.1:
    Fixed
    
      * Fixed a recent regression causing some UI elements to be rendered as
        left-to-right instead of right-to-left for users of our Saraiki
        localization. (Bug 1917175)
    
      * Linux: Fixed black rendering of AVIF images when Firefox is built with GCC.
        (Bug 1916038)
    
    
    130.0:
    New
    
      * Firefox now allows translating selected text portions to different
        languages after a full-page translation.
    
      * Firefox now offers an easy way to try experimental features with a new
        Firefox Labs page in Settings.
    
          + AI Chatbot feature lets you add the chatbot of your choice to the
            sidebar, for quick access as you browse.
          + Picture-in-Picture auto-open experiment enables PiP on active videos
            when switching tabs.
    
      * Overscroll animations are now enabled as the default behavior for
        scrollable areas on Linux.
    
      * Users in the United States and Canada can view local weather directly on
        the new tab page. Additionally, they have the option to select a specific
        location to see current weather conditions.
    
    Fixed
    
      * Various security fixes.
    
      * Fixed an issue where Copy and Paste context menu items intermittently were
        not enabled when expected.
    
    Mozilla Foundation Security Advisory 2024-39
    #CVE-2024-8385: WASM type confusion involving ArrayTypes
    #CVE-2024-8381: Type confusion when looking up a property name in a "with"
     block
    #CVE-2024-8388: Fullscreen notice on Android could be hidden under various
     panels and OS prompts
    #CVE-2024-8382: Internal event interfaces were exposed to web content when
     browser EventHandler listener callbacks ran
    #CVE-2024-8383: Firefox did not ask before openings news: links in an external
     application
    #CVE-2024-8384: Garbage collection could mis-color cross-compartment objects in
     OOM conditions
    #CVE-2024-8386: SelectElements could be shown over another site if popups are
     allowed
    #CVE-2024-8387: Memory safety bugs fixed in Firefox 130, Firefox ESR 128.2, and
     Thunderbird 128.2
    #CVE-2024-8389: Memory safety bugs fixed in Firefox 130

--- www/firefox/patches/patch-js_src_vm_TypedArrayObject-inl.h
+++ www/firefox/patches/patch-js_src_vm_TypedArrayObject-inl.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-js_src_vm_TypedArrayObject-inl.h,v 1.1 2024/08/03 02:06:31 tsutsui Exp $
+$NetBSD: patch-js_src_vm_TypedArrayObject-inl.h,v 1.2 2024/10/01 12:47:19 ryoon Exp $
 
 - kludge to build on NetBSD/i386 10.0
   https://mail-index.netbsd.org/pkgsrc-users/2024/07/16/msg039900.html
 
---- js/src/vm/TypedArrayObject-inl.h.orig	2024-07-25 20:34:47.000000000 +0000
+--- js/src/vm/TypedArrayObject-inl.h.orig	2024-08-23 12:51:13.000000000 +0000
 +++ js/src/vm/TypedArrayObject-inl.h
-@@ -404,7 +404,9 @@ class ElementSpecific {
+@@ -435,7 +435,9 @@ class ElementSpecific {
      // `malloc` returns memory at least as strictly aligned as for max_align_t
      // and the alignment of max_align_t is a multiple of the size of `T`,
      // so `SharedMem::cast` will be called with properly aligned memory.
@@ -13,5 +13,5 @@ $NetBSD: patch-js_src_vm_TypedArrayObject-inl.h,v 1.1 2024/08/03 02:06:31 tsutsu
      static_assert(alignof(std::max_align_t) % sizeof(T) == 0);
 +#endif
  
-     SharedMem<T*> dest =
-         target->dataPointerEither().template cast<T*>() + offset;
+     SharedMem<T*> dest = Ops::extract(target).template cast<T*>() + offset;
+     SharedMem<void*> data = Ops::extract(source);
