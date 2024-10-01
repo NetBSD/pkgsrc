c7a11a0de6853de3902760b1f3f439380d427f11
ryoon <ryoon@pkgsrc.org>
2024-10-01 12:47:18 +0000

    www/firefox: Update to 130.0.1
    Changelog:
    130.0.1:
    Fixed
    
      * Fixed a recent regression causing some UI elements to be rendered as
        left-to-right instead of right-to-left for users of our Saraiki
        localization. (Bug 1917175)
    
      * Linux: Fixed black rendering of AVIF images when Firefox is built with GCC.
        (Bug 1916038)
    
    
    130.0:
    New
    
      * Firefox now allows translating selected text portions to different
        languages after a full-page translation.
    
      * Firefox now offers an easy way to try experimental features with a new
        Firefox Labs page in Settings.
    
          + AI Chatbot feature lets you add the chatbot of your choice to the
            sidebar, for quick access as you browse.
          + Picture-in-Picture auto-open experiment enables PiP on active videos
            when switching tabs.
    
      * Overscroll animations are now enabled as the default behavior for
        scrollable areas on Linux.
    
      * Users in the United States and Canada can view local weather directly on
        the new tab page. Additionally, they have the option to select a specific
        location to see current weather conditions.
    
    Fixed
    
      * Various security fixes.
    
      * Fixed an issue where Copy and Paste context menu items intermittently were
        not enabled when expected.
    
    Mozilla Foundation Security Advisory 2024-39
    #CVE-2024-8385: WASM type confusion involving ArrayTypes
    #CVE-2024-8381: Type confusion when looking up a property name in a "with"
     block
    #CVE-2024-8388: Fullscreen notice on Android could be hidden under various
     panels and OS prompts
    #CVE-2024-8382: Internal event interfaces were exposed to web content when
     browser EventHandler listener callbacks ran
    #CVE-2024-8383: Firefox did not ask before openings news: links in an external
     application
    #CVE-2024-8384: Garbage collection could mis-color cross-compartment objects in
     OOM conditions
    #CVE-2024-8386: SelectElements could be shown over another site if popups are
     allowed
    #CVE-2024-8387: Memory safety bugs fixed in Firefox 130, Firefox ESR 128.2, and
     Thunderbird 128.2
    #CVE-2024-8389: Memory safety bugs fixed in Firefox 130

--- www/firefox/patches/patch-python_mozbuild_mozbuild_backend_recursivemake.py
+++ www/firefox/patches/patch-python_mozbuild_mozbuild_backend_recursivemake.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-python_mozbuild_mozbuild_backend_recursivemake.py,v 1.1 2024/08/12 14:03:32 ryoon Exp $
+$NetBSD: patch-python_mozbuild_mozbuild_backend_recursivemake.py,v 1.2 2024/10/01 12:47:19 ryoon Exp $
 
 * As of 129.0, libxul.so is built with -Wl,-z,defs (no undefined references).
   Supply memalign() from fallback.cpp (Unified_cpp_memory_build0) for NetBSD.
   This approach is completely incorrect, however I do not find the correct
   way to include Unified_cpp_memory_build0.o to objs yet.
 
---- python/mozbuild/mozbuild/backend/recursivemake.py.orig	2024-08-01 17:32:03.000000000 +0000
+--- python/mozbuild/mozbuild/backend/recursivemake.py.orig	2024-08-23 12:51:15.000000000 +0000
 +++ python/mozbuild/mozbuild/backend/recursivemake.py
 @@ -73,6 +73,8 @@ from ..util import FileAvoidWrite, ensur
  from .common import CommonBackend
@@ -16,9 +16,9 @@ $NetBSD: patch-python_mozbuild_mozbuild_backend_recursivemake.py,v 1.1 2024/08/1
  # To protect against accidentally adding logic to Makefiles that belong in moz.build,
  # we check if moz.build-like variables are defined in Makefiles. If they are, we throw
  # an error to encourage the usage of moz.build instead.
-@@ -1427,6 +1429,8 @@ class RecursiveMakeBackend(MakeBackend):
-             return os.path.normpath(mozpath.relpath(path, obj.objdir))
+@@ -1426,6 +1428,8 @@ class RecursiveMakeBackend(MakeBackend):
  
+     def _process_linked_libraries(self, obj, backend_file):
          objs, shared_libs, os_libs, static_libs = self._expand_libs(obj)
 +        if buildconfig.substs["OS_ARCH"] == "NetBSD" and obj.name == "libxul.so":
 +            objs.append("memory/build/Unified_cpp_memory_build0.o")
