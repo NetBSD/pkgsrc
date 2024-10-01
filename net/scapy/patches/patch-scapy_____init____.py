cedae6342dd6b1f2b011b1ca650a66561dc00a9d
gutteridge <gutteridge@pkgsrc.org>
2024-10-01 23:49:29 +0000

    scapy: fix the versioning issue more cleanly
    While here, also note the issue filed for scapy.1 not getting installed.

--- net/scapy/patches/patch-scapy_____init____.py
+++ net/scapy/patches/patch-scapy_____init____.py
@@ -1,15 +1,21 @@
-$NetBSD: patch-scapy_____init____.py,v 1.1 2024/10/01 01:08:29 gutteridge Exp $
+$NetBSD: patch-scapy_____init____.py,v 1.2 2024/10/01 23:49:29 gutteridge Exp $
 
 Supply expected version, not inconsistent date value set by upstream.
+https://github.com/secdev/scapy/pull/4548
 
 --- scapy/__init__.py.orig	2024-09-28 13:11:32.000000000 +0000
 +++ scapy/__init__.py
-@@ -133,7 +133,7 @@ def _version():
-     # Method 0: from external packaging
-     try:
-         # possibly forced by external packaging
--        return os.environ['SCAPY_VERSION']
-+        return "@PKGVERSION_NOREV@"
-     except KeyError:
-         pass
+@@ -37,7 +37,12 @@ def _parse_tag(tag):
+         # remove the 'v' prefix and add a '.devN' suffix
+         return '%s.dev%s' % (match.group(1), match.group(2))
+     else:
+-        raise ValueError('tag has invalid format')
++        match = re.match('^v?([\\d\\.]+)$', tag)
++        if match:
++            # tagged release version
++            return '%s' % (match.group(1))
++        else:
++            raise ValueError('tag has invalid format')
  
+ 
+ def _version_from_git_archive():
