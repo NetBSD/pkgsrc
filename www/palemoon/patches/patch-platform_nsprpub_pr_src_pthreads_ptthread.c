0f4b95cb3ff5677e8fbfb79c9ccd8223f73b12a1
nia <nia@pkgsrc.org>
2024-10-01 12:20:29 +0000

    palemoon: Import the rest of our nsprpub patches
    It's unclear to me if these are strictly necessary with Pale Moon.
    They appear to not affect browser stability for me.  However, since
    they're bugs in the nsprpub library that Mozilla doesn't want to fix
    (likely due to API compatibility?), and previously triggered crashes on
    NetBSD, better safe than sorry.

---
+++ www/palemoon/patches/patch-platform_nsprpub_pr_src_pthreads_ptthread.c
@@ -0,0 +1,19 @@
+$NetBSD: patch-platform_nsprpub_pr_src_pthreads_ptthread.c,v 1.1 2024/10/01 12:20:29 nia Exp $
+
+pthread_t may be 64-bit, avoid integer truncation.
+https://bugzilla.mozilla.org/show_bug.cgi?id=1718835
+
+--- platform/nsprpub/pr/src/pthreads/ptthread.c.orig	2024-09-05 08:13:09.000000000 +0000
++++ platform/nsprpub/pr/src/pthreads/ptthread.c
+@@ -1183,9 +1183,9 @@ PR_IMPLEMENT(void) PR_ProcessExit(PRIntn
+     _exit(status);
+ }
+ 
+-PR_IMPLEMENT(PRUint32) PR_GetThreadID(PRThread *thred)
++PR_IMPLEMENT(pthread_t) PR_GetThreadID(PRThread *thred)
+ {
+-    return (PRUint32)thred->id;  /* and I don't know what they will do with it */
++    return (pthread_t)thred->id;  /* and I don't know what they will do with it */
+ }
+ 
+ /*
