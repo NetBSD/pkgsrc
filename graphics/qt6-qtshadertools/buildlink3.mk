3188ff867e049df95bc99f7e50eff94a744fafa3
adam <adam@pkgsrc.org>
2024-10-01 05:54:19 +0000

    qt6: updated to 6.7.3
    Qt 6.7.3 does not introduce new features but contains more than 400 bug fixes,
    security updates, and other improvements to the top of the Qt 6.7.2 release.

--- graphics/qt6-qtshadertools/buildlink3.mk
+++ graphics/qt6-qtshadertools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/04/06 08:05:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/10/01 05:54:22 adam Exp $
 
 BUILDLINK_TREE+=	qt6-qtshadertools
 
@@ -8,6 +8,8 @@ QT6_QTSHADERTOOLS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.qt6-qtshadertools+=	qt6-qtshadertools>=6.4.1
 BUILDLINK_ABI_DEPENDS.qt6-qtshadertools+=	qt6-qtshadertools>=6.6.3nb1
 BUILDLINK_PKGSRCDIR.qt6-qtshadertools?=		../../graphics/qt6-qtshadertools
+
+BUILDLINK_FILES.qt6-qtshadertools+=	bin/qsb
 
 .include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# QT6_QTSHADERTOOLS_BUILDLINK3_MK
