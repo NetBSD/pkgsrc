06bb13c4e88433ea8201d15b0f4a00f09dceb7f0
adam <adam@pkgsrc.org>
2024-10-04 09:00:05 +0000

    libtorrent: updated to 0.14.0
    0.14.0
    Unknown changes

--- net/libtorrent/patches/patch-src_torrent_net_socket__address__key.h
+++ net/libtorrent/patches/patch-src_torrent_net_socket__address__key.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_torrent_net_socket__address__key.h,v 1.1 2023/11/09 19:09:19 nia Exp $
+$NetBSD: patch-src_torrent_net_socket__address__key.h,v 1.2 2024/10/04 09:00:06 adam Exp $
 
 Add missing header for AF_INET and friends.
 Needed for SunOS but also "POSIX sez so".
 
---- src/torrent/net/socket_address_key.h.orig	2019-07-19 11:44:08.000000000 +0000
+--- src/torrent/net/socket_address_key.h.orig	2024-10-03 12:59:31.247837979 +0000
 +++ src/torrent/net/socket_address_key.h
 @@ -6,6 +6,7 @@
  
  #include <cstring>
- #include <inttypes.h>
+ #include <cinttypes>
 +#include <sys/socket.h>
  #include <netinet/in.h>
  
