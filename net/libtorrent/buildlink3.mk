d500c95eaca17d46f858880649eb0653c4682ebc
adam <adam@pkgsrc.org>
2024-10-04 09:00:05 +0000

    libtorrent: updated to 0.14.0
    0.14.0
    Unknown changes

--- net/libtorrent/buildlink3.mk
+++ net/libtorrent/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.26 2023/10/24 22:10:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2024/10/04 09:00:05 adam Exp $
 
 BUILDLINK_TREE+=	libtorrent
 
 .if !defined(LIBTORRENT_BUILDLINK3_MK)
 LIBTORRENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.13.6
-BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.13.8nb2
+BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.14.0
+BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.14.0
 BUILDLINK_PKGSRCDIR.libtorrent?=	../../net/libtorrent
 
-.include "../../devel/libsigc++/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif # LIBTORRENT_BUILDLINK3_MK
 
