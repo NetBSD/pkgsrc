61c8400ac5a1dec68f6b918b70ae3a7251fd3031
nia <nia@pkgsrc.org>
2024-10-06 16:24:45 +0000

    elinks: correct PKG_OPTIONS_VAR to match PKGNAME
    pointed out by mayuresh

--- www/elinks/options.mk
+++ www/elinks/options.mk
@@ -1,6 +1,6 @@
-# $NetBSD: options.mk,v 1.19 2023/04/14 21:35:06 nia Exp $
+# $NetBSD: options.mk,v 1.20 2024/10/06 16:24:45 nia Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.felinks
+PKG_OPTIONS_VAR=		PKG_OPTIONS.elinks
 PKG_OPTIONS_REQUIRED_GROUPS=	tls
 PKG_OPTIONS_GROUP.tls=		gnutls openssl
 PKG_SUGGESTED_OPTIONS=		gnutls
