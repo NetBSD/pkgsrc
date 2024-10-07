a6eac92712a994dc85979f99820e70eb7875c5ff
jperkin <jperkin@pkgsrc.org>
2024-10-07 14:27:45 +0000

    boost-build: Bump DEPENDS requirement.
    Almost certainly missed during meta-pkgs/boost update.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.54 2024/04/17 16:14:01 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.55 2024/10/07 14:27:45 jperkin Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.85.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.86.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
-BUILDLINK_ABI_DEPENDS.boost-build+=	boost-build>=1.85.0
+BUILDLINK_ABI_DEPENDS.boost-build+=	boost-build>=1.86.0
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
 
