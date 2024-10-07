b1c55fe13bfe213ef68ed9783d4f586169b74733
jperkin <jperkin@pkgsrc.org>
2024-10-07 14:28:30 +0000

    py-boost: Bump DEPENDS requirement.
    Almost certainly missed during meta-pkgs/boost update.

--- devel/py-boost/buildlink3.mk
+++ devel/py-boost/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2024/04/17 16:14:01 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2024/10/07 14:28:30 jperkin Exp $
 
 BUILDLINK_TREE+=	py-boost
 
@@ -8,8 +8,8 @@ PY_BOOST_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.85.*
-BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.85.*
+BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.86.*
+BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.86.*
 BUILDLINK_PKGSRCDIR.py-boost?=		../../devel/py-boost
 
 .include "../../devel/boost-headers/buildlink3.mk"
