5acc556395460af89a88bacaaf1482bbea19b7a0
wiz <wiz@pkgsrc.org>
2024-10-08 21:13:13 +0000

    simage: undo bl3 bumps for now

--- graphics/simage/buildlink3.mk
+++ graphics/simage/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.31 2024/10/08 20:43:17 rjs Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2024/10/08 21:13:13 wiz Exp $
 
 BUILDLINK_TREE+=	simage
 
 .if !defined(SIMAGE_BUILDLINK3_MK)
 SIMAGE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.simage+=	simage>=1.8.3
-BUILDLINK_ABI_DEPENDS.simage+=	simage>=1.8.3
+BUILDLINK_API_DEPENDS.simage+=	simage>=1.6.1
+BUILDLINK_ABI_DEPENDS.simage+=	simage>=1.7.0nb11
 BUILDLINK_PKGSRCDIR.simage?=	../../graphics/simage
 
 .include "../../audio/libsndfile/buildlink3.mk"
