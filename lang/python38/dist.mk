1a3f12a14b6431bdacf7c9662ab67f3d6377ccda
adam <adam@pkgsrc.org>
2024-10-09 20:00:28 +0000

    python38 py38-html-docs: removed; end of life as of 2024-10-07

--- lang/python38/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.21 2024/09/09 15:47:30 adam Exp $
-
-PY_DISTVERSION=	3.8.20
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.xz
-DISTINFO_FILE=	${.CURDIR}/../../lang/python38/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python38/patches
-MASTER_SITES=	https://www.python.org/ftp/python/${PY_DISTVERSION}/
