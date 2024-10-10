4bfd2e67f1404d406e5ddc9c5c9ce65d9a81e842
jakllsch <jakllsch@pkgsrc.org>
2024-10-10 15:19:04 +0000

    openafs: update to 1.8.9
                           User-Visible OpenAFS Changes
    
    OpenAFS 1.8.9
    
      All platforms
    
        * Support for building with more recent compilers (14970 14982..14987
          14990 14991 15056 15057 15061..15064)
    
        * Build fixes and improvements around the test suite (14879 14880
          14909..14911 15133)
    
        * Removed a vestigial autoconf check for GSSAPI support that could
          cause unnecessary configure errors (15137)
    
        * Documentation improvements (14980 15047)
    
        * Improved diagnostics and error messages. In particular, warn when
          server processes are started without keys and properly print the
          volume transaction flags in "vos status" output (14594 14968)
    
      All server platforms
    
        * Avoid several second delays in some situations when the file server
          is breaking callbacks on clients with alternate addresses. Avoid
          unnecessary several second delays in some cases during database
          quorum processing (14815)
    
        * Detect invalid (negative) inputs to FetchData RPCs and reject them
          early.  The previous behavior only detected the error when actually
          attempting to read from storage, which resulted in the volume being
          taken offline since errors were assumed to originate from the
          underlying storage (15224)
    
      All UNIX/Linux client platforms
    
        * Do not perform DNS SRV/AFSDB record queries when running "fs
          getcellstatus", "fs checkservers", and "fs setcell". The DNS
          lookups incur network delays and were not needed to process these
          commands. (14814)
    
        * Avoid possible string buffer overflows with long cell names (15151)
    
      FreeBSD
    
        * Added support for release 12.3 and further improvements (14878
          14920 14921)
    
      Linux clients
    
        * Support mainline kernels up to 6.0 (14942..14944 14989 14945
          14946 15058 15065 15094 15095 15148)
    
        * Fixed a potential memory leak (15096)
    
        * Avoid a possible performance penalty during file reads when
          the file was opened for both reading and writing (15129)
    
        * Fixed a type cast which could make builds fail against older kernels
          (15134)
    
        * In Red Hat packaging, systemd will no longer load the openafs module
          during boot (15128)
    
      Most client platforms
    
        * Handle certain failure conditions rather than panicking the system
          (14927 15052)

--- filesystems/openafs/patches/patch-src_lwp_lwp.c
+++ filesystems/openafs/patches/patch-src_lwp_lwp.c
@@ -1,33 +1,33 @@
-$NetBSD: patch-src_lwp_lwp.c,v 1.2 2022/03/19 16:19:02 jakllsch Exp $
+$NetBSD: patch-src_lwp_lwp.c,v 1.3 2024/10/10 15:19:04 jakllsch Exp $
 
 - Like Linux & Darwin, exclude *BSD on 32-bit ARM from the bespoke LWP handling mechanism.
 
---- src/lwp/lwp.c.orig	2021-12-09 17:07:41.000000000 +0000
+--- src/lwp/lwp.c.orig	2022-12-15 20:10:23.000000000 +0000
 +++ src/lwp/lwp.c
-@@ -365,7 +365,7 @@ LWP_CreateProcess(void *(*ep) (void *), 
+@@ -365,7 +365,7 @@ LWP_CreateProcess(void *(*ep) (void *),
  	Initialize_PCB(temp, priority, stackmemory, stacksize, ep, parm, name);
  	insert(temp, &runnable[priority]);
  	temp2 = lwp_cpptr;
--#if !defined(AFS_ARM_LINUX20_ENV) && !defined(AFS_ARM_DARWIN_ENV)
-+#if !defined(AFS_ARM_LINUX20_ENV) && !defined(AFS_ARM_DARWIN_ENV) && !defined(AFS_ARM32_XBSD_ENV)
+-#if !defined(AFS_ARM_LINUX_ENV) && !defined(AFS_ARM_DARWIN_ENV)
++#if !defined(AFS_ARM_LINUX_ENV) && !defined(AFS_ARM_DARWIN_ENV) && !defined(AFS_ARM32_XBSD_ENV)
  	if (PRE_Block != 0)
  	    Abort_LWP("PRE_Block not 0");
  
-@@ -461,7 +461,7 @@ LWP_CreateProcess2(void *(*ep) (void *),
+@@ -456,7 +456,7 @@ LWP_CreateProcess2(void *(*ep) (void *),
  	Initialize_PCB(temp, priority, stackptr, stacksize, ep, parm, name);
  	insert(temp, &runnable[priority]);
  	temp2 = lwp_cpptr;
--#if !defined(AFS_ARM_LINUX20_ENV) && !defined(AFS_ARM_DARWIN_ENV)
-+#if !defined(AFS_ARM_LINUX20_ENV) && !defined(AFS_ARM_DARWIN_ENV) && !defined(AFS_ARM32_XBSD_ENV)
+-#if !defined(AFS_ARM_LINUX_ENV) && !defined(AFS_ARM_DARWIN_ENV)
++#if !defined(AFS_ARM_LINUX_ENV) && !defined(AFS_ARM_DARWIN_ENV) && !defined(AFS_ARM32_XBSD_ENV)
  	if (PRE_Block != 0)
  	    Abort_LWP("PRE_Block not 0");
  
-@@ -961,7 +961,7 @@ Dispatcher(void)
+@@ -956,7 +956,7 @@ Dispatcher(void)
  	printf("Dispatch %d [PCB at 0x%x] \"%s\"\n", ++dispatch_count,
  	       runnable[i].head, runnable[i].head->name);
  #endif
--#if !defined(AFS_ARM_LINUX20_ENV) && !defined(AFS_ARM_DARWIN_ENV)
-+#if !defined(AFS_ARM_LINUX20_ENV) && !defined(AFS_ARM_DARWIN_ENV) && !defined(AFS_ARM32_XBSD_ENV)
+-#if !defined(AFS_ARM_LINUX_ENV) && !defined(AFS_ARM_DARWIN_ENV)
++#if !defined(AFS_ARM_LINUX_ENV) && !defined(AFS_ARM_DARWIN_ENV) && !defined(AFS_ARM32_XBSD_ENV)
      if (PRE_Block != 1)
  	Abort_LWP("PRE_Block not 1");
  #endif
