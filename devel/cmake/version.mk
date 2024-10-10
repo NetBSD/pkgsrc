5eadae72fabbd3a202ea8eaf03dd35636cab7628
adam <adam@pkgsrc.org>
2024-10-10 08:56:32 +0000

    cmake: updated to 3.30.5
    CMake 3.30.5
    * project: Only check non-cache vars when setting project vars

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.66 2024/09/30 17:07:17 adam Exp $
+# $NetBSD: version.mk,v 1.67 2024/10/10 08:56:32 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.30.4
+CMAKE_VERSION=	3.30.5
 CMAKE_API=	${CMAKE_VERSION:R}
