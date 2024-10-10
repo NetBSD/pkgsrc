8d451b3869778b84dde983e91fac604b00771cc0
nia <nia@pkgsrc.org>
2024-10-10 17:45:38 +0000

    mtscan: Eliminate implicit function declarations.

--- sysutils/mtscan/files/mtscan.c
+++ sysutils/mtscan/files/mtscan.c
@@ -7,15 +7,18 @@
 #include <stdio.h>
 #include <signal.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <strings.h>
 #include <sys/file.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
 
 #if	MACH
 #include <mach.h>
 #endif
 
 #ifdef	MTIO
-#include <sys/types.h>
-#include <sys/ioctl.h>
 #include <sys/mtio.h>
 #endif
 
