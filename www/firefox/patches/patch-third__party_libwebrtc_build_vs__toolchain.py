b598335a5faab5c3ac76edafb4e346cdfc9b4fd3
wiz <wiz@pkgsrc.org>
2024-10-11 13:55:47 +0000

    firefox: fix build with Python 3.13
    pipes.quote was an alias for shlex.quote, but was finally removed in
    Python 3.13. Directly use shlex.quote.

---
+++ www/firefox/patches/patch-third__party_libwebrtc_build_vs__toolchain.py
@@ -0,0 +1,14 @@
+$NetBSD: patch-third__party_libwebrtc_build_vs__toolchain.py,v 1.1 2024/10/11 13:55:48 wiz Exp $
+
+Fix build with Python 3.13.
+
+--- third_party/libwebrtc/build/vs_toolchain.py.orig	2024-10-11 13:43:08.737671418 +0000
++++ third_party/libwebrtc/build/vs_toolchain.py
+@@ -9,7 +9,6 @@ import collections
+ import glob
+ import json
+ import os
+-import pipes
+ import platform
+ import re
+ import shutil
---
+++ www/firefox/patches/patch-third__party_python_dlmanager_check.py
@@ -0,0 +1,24 @@
+$NetBSD: patch-third__party_python_dlmanager_check.py,v 1.1 2024/10/11 13:55:48 wiz Exp $
+
+Fix build with Python 3.13.
+
+--- third_party/python/dlmanager/check.py.orig	2024-10-11 13:43:43.746252969 +0000
++++ third_party/python/dlmanager/check.py
+@@ -5,7 +5,7 @@ Run flake8 checks and tests.
+ 
+ import os
+ import argparse
+-import pipes
++import shlex
+ import shutil
+ import tempfile
+ 
+@@ -23,7 +23,7 @@ def parse_args():
+ 
+ 
+ def run(cmd, **kwargs):
+-    msg = 'Running: |%s|' % ' '.join(pipes.quote(c) for c in cmd)
++    msg = 'Running: |%s|' % ' '.join(shlex.quote(c) for c in cmd)
+     if kwargs.get('cwd'):
+         msg += ' in %s' % kwargs['cwd']
+     print(msg)
