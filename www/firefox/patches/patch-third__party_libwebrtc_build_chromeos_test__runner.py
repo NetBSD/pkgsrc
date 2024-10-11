b5a74a0c2c274ea87ea4da46e90ef433e00d0975
wiz <wiz@pkgsrc.org>
2024-10-11 13:55:47 +0000

    firefox: fix build with Python 3.13
    pipes.quote was an alias for shlex.quote, but was finally removed in
    Python 3.13. Directly use shlex.quote.

---
+++ www/firefox/patches/patch-third__party_libwebrtc_build_chromeos_test__runner.py
@@ -0,0 +1,33 @@
+$NetBSD: patch-third__party_libwebrtc_build_chromeos_test__runner.py,v 1.1 2024/10/11 13:55:48 wiz Exp $
+
+Fix build with Python 3.13.
+
+--- third_party/libwebrtc/build/chromeos/test_runner.py.orig	2024-10-11 13:42:59.456381299 +0000
++++ third_party/libwebrtc/build/chromeos/test_runner.py
+@@ -9,7 +9,7 @@ import collections
+ import json
+ import logging
+ import os
+-import pipes
++import shlex
+ import re
+ import shutil
+ import signal
+@@ -342,7 +342,7 @@ class TastTest(RemoteTest):
+         # aren't compatible.
+         local_test_runner_cmd.append('-extrauseflags=tast_vm')
+       if self._attr_expr:
+-        local_test_runner_cmd.append(pipes.quote(self._attr_expr))
++        local_test_runner_cmd.append(shlex.quote(self._attr_expr))
+       else:
+         local_test_runner_cmd.extend(self._tests)
+       device_test_script_contents.append(' '.join(local_test_runner_cmd))
+@@ -380,7 +380,7 @@ class TastTest(RemoteTest):
+         self._attr_expr = '(' + ' || '.join(names) + ')'
+ 
+       if self._attr_expr:
+-        # Don't use pipes.quote() here. Something funky happens with the arg
++        # Don't use shlex.quote() here. Something funky happens with the arg
+         # as it gets passed down from cros_run_test to tast. (Tast picks up the
+         # escaping single quotes and complains that the attribute expression
+         # "must be within parentheses".)
---
+++ www/firefox/patches/patch-third__party_libwebrtc_build_print__python__deps.py
@@ -0,0 +1,24 @@
+$NetBSD: patch-third__party_libwebrtc_build_print__python__deps.py,v 1.1 2024/10/11 13:55:48 wiz Exp $
+
+Fix build with Python 3.13.
+
+--- third_party/libwebrtc/build/print_python_deps.py.orig	2024-10-11 13:43:27.070262533 +0000
++++ third_party/libwebrtc/build/print_python_deps.py
+@@ -14,7 +14,7 @@ This script should be compatible with Py
+ import argparse
+ import fnmatch
+ import os
+-import pipes
++import shlex
+ import sys
+ 
+ # Don't use any helper modules, or else they will end up in the results.
+@@ -61,7 +61,7 @@ def _NormalizeCommandLine(options):
+   for allowlist in sorted(options.allowlists):
+     args.extend(('--allowlist', os.path.relpath(allowlist, _SRC_ROOT)))
+   args.append(os.path.relpath(options.module, _SRC_ROOT))
+-  return ' '.join(pipes.quote(x) for x in args)
++  return ' '.join(shlex.quote(x) for x in args)
+ 
+ 
+ def _FindPythonInDirectory(directory, allow_test):
