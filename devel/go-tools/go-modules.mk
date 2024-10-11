48622eeefae1758d8b8a810d13abf8949604e61d
bsiegert <bsiegert@pkgsrc.org>
2024-10-11 09:30:16 +0000

    go-tools: update to 0.26.0
    - fix compilation with Go 1.23
    - gorename, guru and getgo have been removed. The former is now integrated
      in gopls. getgo was never useful with pkgsrc.

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,14 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.5 2024/03/16 17:17:58 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.6 2024/10/11 09:30:16 bsiegert Exp $
 
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.16.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.16.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.22.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.22.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.6.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.18.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.18.0.zip
-GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240228155512-f48c80bd79b2.mod
-GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240228155512-f48c80bd79b2.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.21.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.30.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.zip
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.mod
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.zip
