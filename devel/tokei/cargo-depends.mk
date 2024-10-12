7b2e9aa0bacec213845bf8cf4a310cc94972b737
pin <pin@pkgsrc.org>
2024-10-12 22:59:11 +0000

    devel/tokei: update to v13.0.0-alpha.6
    Added
        add mbti extension for MoonBit (#1168)
    
    Other
        Add language definition for Lauterbach PRACTICE Script (#1162)
        Add support for justfiles (#1175)
        Add Virgil (#1178)
        Add templ support (#1122)
        Update README.md with HiCAD from d4a1814 (#1143)
        Add BQN support (#1151)
        add more extensions for Hlsl (#1164)
        Add Phix (#1167)
        Add APL support (#1152)
        Add support for SIL (#1153)
        Use OR operator in Cargo.toml license field (#1165)
        Disable legacy Cargo features (#1158)
        add slint language support (#1054)
        Add Pyret support (#1032)
        Recognize GNUmakefile (#1021)

--- devel/tokei/cargo-depends.mk
+++ devel/tokei/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.7 2024/08/23 15:26:17 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.8 2024/10/12 22:59:11 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.3
 CARGO_CRATE_DEPENDS+=	android-tzdata-0.1.1
