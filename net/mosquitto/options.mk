39dcc300ad9eb3e06cbbac3ad9ad2d5ffc71534a
gdt <gdt@pkgsrc.org>
2024-10-13 14:13:22 +0000

    net/mosquitto: Update to 2.0.19
    packaging changes:
    
      Cherry-pick pending upstream fix for pthread confusion.  While
      labeled as fixing NetBSD, it is really a consistency rototill for
      using pthreads.
    
      Use "websockets" lib instead of the cmake-too-complicated
      "websockets_shared" which references an installed file which seems
      to have a bug of including -ldl on systems where it doesn't exist.
      \todo Resolve within pkgsrc.
    
    upstream changes:
    
    
    2.0.19 - 2024-10-02
    ===================
    
    Security:
    - Fix mismatched subscribe/unsubscribe with normal/shared topics.
    - Fix crash on bridge using remapped topic being sent a crafted packet.
    
    Broker:
    - Fix assert failure when loading a persistence file that contains
      subscriptions with no client id.
    - Fix local bridges being incorrectly expired when
      persistent_client_expiration is in use.
    - Fix use of CLOCK_BOOTTIME for getting time. Closes #3089.
    - Fix mismatched subscribe/unsubscribe with normal/shared topics.
    - Fix crash on bridge using remapped topic being sent a crafted packet.
    
    Client library:
    - Fix some error codes being converted to string as "unknown". Closes #2579.
    - Clear SSL error state to avoid spurious error reporting. Closes #3054.
    - Fix "payload format invalid" not being allowed as a PUBREC reason code.
    - Don't allow SUBACK with missing reason codes.
    
    Build:
    - Thread support is re-enabled on Windows.

--- net/mosquitto/options.mk
+++ net/mosquitto/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2024/08/25 06:19:07 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2024/10/13 14:13:22 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mosquitto
 PKG_SUPPORTED_OPTIONS=	websockets
@@ -6,6 +6,9 @@ PKG_SUGGESTED_OPTIONS=	websockets
 
 .include "../../mk/bsd.options.mk"
 
+# \todo With websockets, mosquitto's build system throws warnings that
+# performance will be terrible.  Figure out if this is correct or not
+# and fix or file bugs as appropriate.
 .if !empty(PKG_OPTIONS:Mwebsockets)
 CMAKE_CONFIGURE_ARGS+=	-DWITH_WEBSOCKETS=yes
 .include "../../www/libwebsockets/buildlink3.mk"
