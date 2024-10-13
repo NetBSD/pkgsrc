9b5c007635792ff8aa5089623d357154e6ecdefc
wiz <wiz@pkgsrc.org>
2024-10-13 07:44:04 +0000

    compiledb-go: update to 1.3.7.
    Bugfixes.

--- devel/compiledb-go/go-modules.mk
+++ devel/compiledb-go/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.1 2024/08/09 18:51:28 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.2 2024/10/13 07:44:04 wiz Exp $
 
 GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.4.mod
 GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.4.zip
@@ -17,5 +17,7 @@ GO_MODULE_FILES+=	github.com/xrash/smetrics/@v/v0.0.0-20240521201337-686a1a2994c
 GO_MODULE_FILES+=	github.com/xrash/smetrics/@v/v0.0.0-20240521201337-686a1a2994c1.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.18.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.18.0.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
 GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.mod
