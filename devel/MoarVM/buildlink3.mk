36e2bf40cbd4d4ecbb90c8fdd5f93cb3354576c3
mef <mef@pkgsrc.org>
2024-10-14 01:41:08 +0000

    (devel/MoarVM) Updated 2024.08 to 2024.09
    New in 2024.09
    
    Core:
    + When -latomic is needed, don't lose the libs entry [6ed6986b]
    + Hand-Apply fix to minilua for CVE-2014-5461 [570db261]
    + Fix NAN/INFINITY definitions to work on Windows 11 SDK [b7750ec2]
    + We don't have to account for "start" in size calculation ...
    
    Documentation:
    
    GC:
    + A re-imagining of the MVMROOT macros [ac82e446]
    
    JIT:
    
    Spesh:
    + When guarding conc or type and type is known, guard just conc/type [abab265d]
    
    Strings:
    + latin1.c and ascii.c can return str_consts.empty for 0-length string [2a0dc7f1]
    + Split string's "any" union field into "any" and "any_ptr" [f14712b6]

--- devel/MoarVM/buildlink3.mk
+++ devel/MoarVM/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2024/04/29 03:19:07 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2024/10/14 01:41:08 mef Exp $
 
 BUILDLINK_TREE+=	MoarVM
 
 .if !defined(MOARVM_BUILDLINK3_MK)
 MOARVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2024.04
-BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2024.04
+BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2024.09
+BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2024.09
 BUILDLINK_PKGSRCDIR.MoarVM?=	../../devel/MoarVM
 
 .include "../../devel/libatomic_ops/buildlink3.mk"
