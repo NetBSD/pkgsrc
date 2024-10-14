34033bc5a05deed2caa4b78a04035bc394d02820
wiz <wiz@pkgsrc.org>
2024-10-14 06:45:30 +0000

    *: clean-up after python38 removal

--- devel/nextpnr/nextpnr.mk
+++ devel/nextpnr/nextpnr.mk
@@ -1,4 +1,4 @@
-# $NetBSD: nextpnr.mk,v 1.5 2024/08/25 06:18:37 wiz Exp $
+# $NetBSD: nextpnr.mk,v 1.6 2024/10/14 06:45:39 wiz Exp $
 
 NEXTPNR_VERSION=0.7
 DISTNAME=	nextpnr-${NEXTPNR_VERSION}
@@ -27,10 +27,6 @@ CMAKE_CONFIGURE_ARGS+=	-DARCH=${NEXTPNR_TARGET}
 .if defined(NEXTPNR_TARGET_ARGS)
 CMAKE_CONFIGURE_ARGS+=	${NEXTPNR_TARGET_ARGS}
 .endif
-
-# Python 3.5 or later, including development libraries, Python 3.9 or
-# later required for nextpnr-himbaechel.
-PYTHON_VERSIONS_INCOMPATIBLE= 27 38
 
 .include "../../devel/boost-libs/buildlink3.mk"
 .include "../../math/eigen3/buildlink3.mk"
