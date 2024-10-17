3f9a7e052715faf82d0365084fefa8acff6e2101
bsiegert <bsiegert@pkgsrc.org>
2024-10-17 18:47:27 +0000

    age: update to 1.2.0
    A small release to build the release binaries with a more recent Go toolchain,
    and to fix a couple CLI edge cases.
    
    The Go module now exposes a plugin package that provides an age plugin client.
    That is, Recipient and Identity implementations that invoke a plugin binary,
    allowing the use of age plugins in Go programs.
    
    Finally, Recipients can now return a set of "labels" by implementing
    RecipientWithLabels. This allows replicating the special behavior of the scrypt
    Recipient in third-party Recipients, or applying policy useful for
    authenticated or post-quantum Recipients.

--- security/age/go-modules.mk
+++ security/age/go-modules.mk
@@ -1,10 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.4 2023/01/16 16:07:49 nikita Exp $
+# $NetBSD: go-modules.mk,v 1.5 2024/10/17 18:47:27 bsiegert Exp $
 
-GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0.mod
-GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.0.0.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.4.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.4.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.3.0.zip
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.3.0.mod
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.3.0.zip
+GO_MODULE_FILES+=	c2sp.org/!c!c!t!v/age/@v/v0.0.0-20240306222714-3ec4d716e805.mod
+GO_MODULE_FILES+=	c2sp.org/!c!c!t!v/age/@v/v0.0.0-20240306222714-3ec4d716e805.zip
+GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.1.0.mod
+GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.1.0.zip
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.12.0.mod
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.12.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.21.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.zip
