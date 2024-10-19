ac471b457b7ad43bf2accf7f7927cf3af271857c
wiz <wiz@pkgsrc.org>
2024-10-19 11:55:44 +0000

    openexr: add upstream bug report URL

--- graphics/openexr/patches/patch-src_bin_exrmetrics_exrmetrics.cpp
+++ graphics/openexr/patches/patch-src_bin_exrmetrics_exrmetrics.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_bin_exrmetrics_exrmetrics.cpp,v 1.1 2024/10/19 11:53:58 wiz Exp $
+$NetBSD: patch-src_bin_exrmetrics_exrmetrics.cpp,v 1.2 2024/10/19 11:55:44 wiz Exp $
 
 Fix build on NetBSD.
+https://github.com/AcademySoftwareFoundation/openexr/issues/1889
 
 --- src/bin/exrmetrics/exrmetrics.cpp.orig	2024-09-26 23:34:32.000000000 +0000
 +++ src/bin/exrmetrics/exrmetrics.cpp
