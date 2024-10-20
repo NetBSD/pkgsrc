e4e69976e64282254bfac109a943b48802a08667
leot <leot@pkgsrc.org>
2024-10-20 09:51:28 +0000

    direnv: Update to 2.35.0
    Changes:
    2.35.0
    ======
    - Add opam support
    - Add NuShell into the list of supported shells
    - Several bug fixes and documentation improvements

--- sysutils/direnv/go-modules.mk
+++ sysutils/direnv/go-modules.mk
@@ -1,10 +1,10 @@
-# $NetBSD: go-modules.mk,v 1.6 2024/03/24 20:03:50 leot Exp $
+# $NetBSD: go-modules.mk,v 1.7 2024/10/20 09:51:28 leot Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.3.2.zip
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.4.0.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.4.0.zip
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.mod
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.15.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.19.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.19.0.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.zip
