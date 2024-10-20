cf5b496715475173011fd57026f4e69b92b93419
leot <leot@pkgsrc.org>
2024-10-20 09:28:29 +0000

    mpv: Update to 0.39.0
    Changes:
    0.39.0
    ======
    We are excited to announce the release of mpv 0.39.0, with more than 5
    months of development since the last version. This release brings
    numerous improvements and fixes across nearly all components.
    
    Key highlights:
    
    * A new select.lua script, allowing users to easily choose files from
      playlists, tracks, chapters, output devices, and more.
    * A native replacement for autoload.lua has been implemented.
      See --autocreate-playlist in the manual.
    * NVIDIA RTX and Intel VSR scaling are now supported via the d3d11va filter.
    * Media controls are now supported on Windows.
    * Track selection has been improved, especially for multi-component languages
      like fr-CA.
    * A new fifth page has been added to stats.lua, displaying track information.
    * Compatibility with dmabuf-wayland has been significantly enhanced.
    * Multi-touch support has been introduced.
    
    This release requires FFmpeg 6.1 or newer and libplacebo 6.338.2 or newer.

--- multimedia/mpv/patches/patch-video_out_drm__common.c
+++ multimedia/mpv/patches/patch-video_out_drm__common.c
@@ -1,16 +1,14 @@
-$NetBSD: patch-video_out_drm__common.c,v 1.4 2020/12/19 11:27:51 leot Exp $
+$NetBSD: patch-video_out_drm__common.c,v 1.5 2024/10/20 09:28:29 leot Exp $
 
 Enable support for DRM on NetBSD.
 
---- video/out/drm_common.c.orig	2020-11-22 17:46:28.000000000 +0000
+--- video/out/drm_common.c.orig	2024-09-23 17:36:41.000000000 +0000
 +++ video/out/drm_common.c
-@@ -30,6 +30,9 @@
- 
- #if HAVE_CONSIO_H
- #include <sys/consio.h>
-+#elif HAVE_WSDISPLAY_USL_IO_H
-+#include <sys/types.h>
-+#include <dev/wscons/wsdisplay_usl_io.h>
- #else
+@@ -34,6 +34,7 @@
+ #elif HAVE_VT_H
  #include <sys/vt.h>
+ #elif HAVE_WSDISPLAY_USL_IO_H
++#include <sys/types.h>
+ #include <dev/wscons/wsdisplay_usl_io.h>
  #endif
+ 
