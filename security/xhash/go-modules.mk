8c101693f471d6e547299b61d6f6fe7cabe82e8f
wiz <wiz@pkgsrc.org>
2024-10-20 16:31:43 +0000

    xhash: update to 3.5.1.
    Changelog:
    - Update dependencies
    - Update to Go 1.23
    - Fix race condition with --all
    - Major refactor and code tidying
    
    From maintainer Ricardo Branco in PR 58765.

--- security/xhash/go-modules.mk
+++ security/xhash/go-modules.mk
@@ -1,14 +1,14 @@
-# $NetBSD: go-modules.mk,v 1.2 2024/08/19 15:14:35 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.3 2024/10/20 16:31:43 wiz Exp $
 
 GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.mod
 GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.zip
 GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.mod
 GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.zip
-GO_MODULE_FILES+=	github.com/zeebo/assert/@v/v1.1.0.mod
-GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.3.mod
-GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.3.zip
-GO_MODULE_FILES+=	github.com/zeebo/pcg/@v/v1.0.1.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.zip
+GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.4.mod
+GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.4.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.28.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.28.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.zip
