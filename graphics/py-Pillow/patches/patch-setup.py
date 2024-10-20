9fef7c9c9644c34cd6551ef748d9d5e02968eefb
adam <adam@pkgsrc.org>
2024-10-20 20:05:35 +0000

    py-Pillow: updated to 11.0.0
    11.0.0 (2024-10-15)
    
    - Update licence to MIT-CMU
    - Conditionally define ImageCms type hint to avoid requiring core
    - Support writing LONG8 offsets in AppendingTiffWriter
    - Use ImageFile.MAXBLOCK when saving TIFF images
    - Do not close provided file handles with libtiff when saving
    - Support ImageFilter.BuiltinFilter for I;16* images
    - Use ImagingCore.ptr instead of ImagingCore.id
    - Updated EPS mode when opening images without transparency
    - Use transparency when combining P frames from APNGs
    - Support all resampling filters when resizing I;16* images
    - Free memory on early return
    - Cast int before potentially exceeding INT_MAX
    - Check image value before use
    - Improved copying imagequant libraries
    - Use Capsule for WebP saving
    - Fixed writing multiple StripOffsets to TIFF
    - Fix dereference before checking for NULL in ImagingTransformAffine
    - Use transposed size after opening for TIFF images
    - Improve ImageFont error messages
    - Mention MAX_TEXT_CHUNK limit in PNG error message
    - Cast Dib handle to int
    - Accept float stroke widths
    - Deprecate ICNS (width, height, scale) sizes in favour of load(scale)
    - Improved handling of RGBA palettes when saving GIF images
    - Deprecate isImageType
    - Support converting more modes to LAB by converting to RGBA first
    - Deprecate support for FreeType 2.9.0
    - Removed unused TiffImagePlugin IFD_LEGACY_API
    - Handle duplicate EXIF header
    - Return early from BoxBlur if either width or height is zero
    - Check text is either string or bytes
    - Added writing XMP bytes to JPEG
    - Support JPEG2000 RGBA palettes
    - Expand C image to match GIF frame image size
    - Allow saving I;16 images as PPM
    - When IFD is missing, connect get_ifd() dictionary to Exif
    - Skip truncated ICO mask if LOAD_TRUNCATED_IMAGES is enabled
    - Treat unknown JPEG2000 colorspace as unspecified
    - Updated error message when saving WebP with invalid width or height
    - Remove warning if NumPy failed to raise an error during conversion
    - If left and right sides meet in ImageDraw.rounded_rectangle(), do not draw rectangle to fill gap
    - Remove WebP support without anim, mux/demux, and with buggy alpha
    - Add missing TIFF CMYK;16B reader
    - Remove all WITH_* flags from _imaging.c and other flags
    - Improve ImageDraw2 shape methods
    - Lock around usages of imaging memory arenas
    - Deprecate JpegImageFile huffman_ac and huffman_dc
    - Deprecate ImageMath lambda_eval and unsafe_eval options argument
    - Changed ContainerIO to subclass IO
    - Move away from APIs that use borrowed references under the free-threaded build
    - Allow size argument to resize() to be a NumPy array
    - Drop support for Python 3.8
    - Add support for Python 3.13
    - Fix incompatibility with NumPy 1.20
    - Remove PSFile, PyAccess and USE_CFFI_ACCESS

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,30 +1,18 @@
-$NetBSD: patch-setup.py,v 1.25 2024/01/05 23:47:53 adam Exp $
+$NetBSD: patch-setup.py,v 1.26 2024/10/20 20:05:35 adam Exp $
 
-Fix finding zlib.h.
 Do not look for xcb.
 
---- setup.py.orig	2024-01-02 08:28:43.000000000 +0000
+--- setup.py.orig	2024-10-15 05:58:32.000000000 +0000
 +++ setup.py
-@@ -642,7 +642,9 @@ class pil_build_ext(build_ext):
- 
-         if feature.want("zlib"):
-             _dbg("Looking for zlib")
--            if _find_include_file(self, "zlib.h"):
-+            if True:
-+                feature.zlib = "z"
-+            elif _find_include_file(self, "zlib.h"):
-                 if _find_library_file(self, "z"):
-                     feature.zlib = "z"
-                 elif sys.platform == "win32" and _find_library_file(self, "zlib"):
-@@ -800,11 +802,6 @@ class pil_build_ext(build_ext):
-                 ):
-                     feature.webpmux = "libwebpmux"
+@@ -833,11 +833,6 @@ class pil_build_ext(build_ext):
+                         feature.set("webp", prefix + "webp")
+                         break
  
 -        if feature.want("xcb"):
 -            _dbg("Looking for xcb")
 -            if _find_include_file(self, "xcb/xcb.h"):
 -                if _find_library_file(self, "xcb"):
--                    feature.xcb = "xcb"
+-                    feature.set("xcb", "xcb")
  
          for f in feature:
-             if not getattr(feature, f) and feature.require(f):
+             if not feature.get(f) and feature.require(f):
