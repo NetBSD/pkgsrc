8a88729272ef92cc500992f7e720c00fb799cd3f
leot <leot@pkgsrc.org>
2024-10-20 09:45:35 +0000

    kepubify: Import kepubify-4.0.4 as textproc/kepubify
    Convert EPUBs to Kobo EPUBs.
    
    Kepubify is the fastest tool for converting EPUBs to Kobo's enhanced
    KEPUB format for use on Kobo eReaders.
    
    It works with malformed e-books, doesn't modify the book's layout more
    than absolutely necessary, doesn't depend on any external software, and
    works from the command-line.
    
    Packaged in pkgsrc-wip by myself.

---
+++ textproc/kepubify/go-modules.mk
@@ -0,0 +1,26 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/10/20 09:45:35 leot Exp $
+
+GO_MODULE_FILES+=	github.com/bamiaux/rez/@v/v0.0.0-20170731184118-29f4463c688b.mod
+GO_MODULE_FILES+=	github.com/bamiaux/rez/@v/v0.0.0-20170731184118-29f4463c688b.zip
+GO_MODULE_FILES+=	github.com/beevik/etree/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/beevik/etree/@v/v1.1.0.zip
+GO_MODULE_FILES+=	github.com/hexops/gotextdiff/@v/v1.0.3.mod
+GO_MODULE_FILES+=	github.com/hexops/gotextdiff/@v/v1.0.3.zip
+GO_MODULE_FILES+=	github.com/kr/smartypants/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/kr/smartypants/@v/v0.1.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-sqlite3/@v/v1.14.12.mod
+GO_MODULE_FILES+=	github.com/mattn/go-sqlite3/@v/v1.14.12.zip
+GO_MODULE_FILES+=	github.com/pgaskin/kepubify/_/go116-zip.go117/@v/v0.0.0-20210611152744-2d89b3182523.mod
+GO_MODULE_FILES+=	github.com/pgaskin/kepubify/_/go116-zip.go117/@v/v0.0.0-20210611152744-2d89b3182523.zip
+GO_MODULE_FILES+=	github.com/pgaskin/kepubify/_/html/@v/v0.0.0-20211223234002-6ee2cc632cdc.mod
+GO_MODULE_FILES+=	github.com/pgaskin/kepubify/_/html/@v/v0.0.0-20211223234002-6ee2cc632cdc.zip
+GO_MODULE_FILES+=	github.com/pgaskin/koboutils/v2/@v/v2.1.2-0.20220306004009-a07e72ebae42.mod
+GO_MODULE_FILES+=	github.com/pgaskin/koboutils/v2/@v/v2.1.2-0.20220306004009-a07e72ebae42.zip
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.mod
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20210220032951-036812b2e83c.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20210220032951-036812b2e83c.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.zip
