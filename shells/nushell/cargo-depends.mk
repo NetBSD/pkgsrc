ae145fa03ee2f1ab70f0a70058021d497d036285
pin <pin@pkgsrc.org>
2024-10-21 14:01:24 +0000

    shells/nushell: update to 0.99.1
    A bug was introduced in 0.99.0 where return, break, and continue could cause
    $env.LAST_EXIT_CODE to be set to 1. This has been fixed in #14120.

--- shells/nushell/cargo-depends.mk
+++ shells/nushell/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.45 2024/10/16 10:15:26 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.46 2024/10/21 14:01:24 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.22.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
