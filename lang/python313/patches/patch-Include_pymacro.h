34d5348f4b9d855718c1002797010ebf9df853f0
jperkin <jperkin@pkgsrc.org>
2024-10-21 10:21:27 +0000

    python313: Extend NetBSD "fix" to SunOS too.
    It's somewhat odd that this is being seen on all pkgsrc platforms, e.g. here
    on Linux too:
    
      https://us-central.manta.mnx.io/pkgsrc/public/reports/Linux/el9/trunk/x86_64/20241019.1600/python313-3.13.0/build.log
    
    This suggests we are doing something actively harmful somewhere else in our
    package as there's no way upstream would ship a broken python 3.13 on Linux,
    but I don't have time to debug that right now, nor test the "fix" on Linux.

--- lang/python313/patches/patch-Include_pymacro.h
+++ lang/python313/patches/patch-Include_pymacro.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-Include_pymacro.h,v 1.1 2024/10/09 19:53:19 adam Exp $
+$NetBSD: patch-Include_pymacro.h,v 1.2 2024/10/21 10:21:27 jperkin Exp $
 
 Fix build on NetBSD: error: initializer element is not constant.
 
@@ -9,7 +9,7 @@ Fix build on NetBSD: error: initializer element is not constant.
  
  #if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L \
 -     && !defined(__cplusplus))
-+     && !defined(__cplusplus) && !defined(__NetBSD__))
++     && !defined(__cplusplus) && !defined(__NetBSD__) && !defined(__sun))
  #  define Py_BUILD_ASSERT_EXPR(cond) \
      ((void)sizeof(struct { int dummy; _Static_assert(cond, #cond); }), \
       0)
