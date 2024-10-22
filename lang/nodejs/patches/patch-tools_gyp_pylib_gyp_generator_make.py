cc08543524c3e236b3bd9ffab52dac323f3eafa3
adam <adam@pkgsrc.org>
2024-10-22 08:30:41 +0000

    nodejs: updated to 22.10.0
    Version 22.10.0 (Current)
    
    New "module-sync" exports condition
    node --run is now stable

--- lang/nodejs/patches/patch-tools_gyp_pylib_gyp_generator_make.py
+++ lang/nodejs/patches/patch-tools_gyp_pylib_gyp_generator_make.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_generator_make.py,v 1.5 2022/03/30 06:52:33 adam Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_generator_make.py,v 1.6 2024/10/22 08:30:42 adam Exp $
 
 Use the system libtool on Darwin.
 Add support for NetBSD and DragonFly.
 
---- tools/gyp/pylib/gyp/generator/make.py.orig	2021-08-03 05:54:27.000000000 +0000
+--- tools/gyp/pylib/gyp/generator/make.py.orig	2024-10-16 14:36:59.000000000 +0000
 +++ tools/gyp/pylib/gyp/generator/make.py
-@@ -179,7 +179,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
+@@ -208,7 +208,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
  
  LINK_COMMANDS_MAC = """\
  quiet_cmd_alink = LIBTOOL-STATIC $@
--cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
-+cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool /usr/bin/libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
+-cmd_alink = rm -f $@ && %(python)s gyp-mac-tool filter-libtool libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %%.o,$^)
++cmd_alink = rm -f $@ && %(python)s gyp-mac-tool filter-libtool /usr/bin/libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %%.o,$^)
  
  quiet_cmd_link = LINK($(TOOLSET)) $@
  cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o "$@" $(LD_INPUTS) $(LIBS)
-@@ -2335,7 +2335,7 @@ def GenerateOutput(target_list, target_d
+@@ -2552,7 +2552,7 @@ def GenerateOutput(target_list, target_d
                  "flock_index": 2,
              }
          )
