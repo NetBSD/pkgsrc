0e75d7838b16fae096ec1655e264434f6909d397
wiz <wiz@pkgsrc.org>
2024-10-22 09:42:05 +0000

    meson: update to 1.6.0.
    Support for OpenXL compiler in AIX.
    alias_target of both_libraries
    Default to printing deprecations when no minimum version is specified.
    Cargo subprojects is experimental
    Dependencies from CMake subprojects now use only PUBLIC link flags
    New built-in option for default both_libraries
    New as_static and as_shared methods on internal dependencies
    Support for DIA SDK
    Support for LLVM-based flang compiler
    nvc and nvc++ now support setting std
    Tools can be selected when calling has_tools() on the Qt modules
    Simple tool to test build reproducibility
    Support for variable in system dependencies
    test() and benchmark() functions accept new types
    Zig 0.11 can be used as a C/C++ compiler frontend

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.7 2023/02/24 08:19:00 adam Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.8 2024/10/22 09:42:05 wiz Exp $
 
 Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
 a condition to match FreeBSD path norm for pkg-config files.
 
---- mesonbuild/modules/pkgconfig.py.orig	2023-02-23 22:07:07.000000000 +0000
+--- mesonbuild/modules/pkgconfig.py.orig	2024-10-20 18:20:39.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -696,7 +696,7 @@ class PkgConfigModule(NewExtensionModule
+@@ -701,7 +701,7 @@ class PkgConfigModule(NewExtensionModule
          pcfile = filebase + '.pc'
          pkgroot = pkgroot_name = kwargs['install_dir'] or default_install_dir
          if pkgroot is None:
 -            if mesonlib.is_freebsd():
 +            if False:
-                 pkgroot = os.path.join(_as_str(state.environment.coredata.get_option(mesonlib.OptionKey('prefix'))), 'libdata', 'pkgconfig')
+                 pkgroot = os.path.join(_as_str(state.environment.coredata.get_option(OptionKey('prefix'))), 'libdata', 'pkgconfig')
                  pkgroot_name = os.path.join('{prefix}', 'libdata', 'pkgconfig')
              elif mesonlib.is_haiku():
