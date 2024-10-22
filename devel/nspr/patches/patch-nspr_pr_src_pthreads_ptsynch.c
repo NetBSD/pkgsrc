a8cb92297701e86ff7348d93dee95af60a9c87ff
ryoon <ryoon@pkgsrc.org>
2024-10-22 12:52:02 +0000

    devel/nspr: Update to 4.36
    Changelog:
    NSPR 4.36 contains the following changes:
    - remove support for OS/2
    - remove support for Unixware, Bsdi, old AIX, old HPUX9 & scoos
    - remove support for Windows 16 bit
    - renamed the prwin16.h header to prwin.h
    - configure was updated from 2.69 to 2.71
    - various build, test and automation script fixes
    - major parts of the source code were reformatted

--- devel/nspr/patches/patch-nspr_pr_src_pthreads_ptsynch.c
+++ devel/nspr/patches/patch-nspr_pr_src_pthreads_ptsynch.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-nspr_pr_src_pthreads_ptsynch.c,v 1.2 2021/07/01 07:23:00 wiz Exp $
+$NetBSD: patch-nspr_pr_src_pthreads_ptsynch.c,v 1.3 2024/10/22 12:52:02 ryoon Exp $
 
 * Do not use pthread_equal() for non-valid thread pointers.
   From kamin@ on source-changes-d@NetBSD.org.
@@ -6,11 +6,11 @@ $NetBSD: patch-nspr_pr_src_pthreads_ptsynch.c,v 1.2 2021/07/01 07:23:00 wiz Exp 
 
 https://bugzilla.mozilla.org/show_bug.cgi?id=1718838
 
---- nspr/pr/src/pthreads/ptsynch.c.orig	2020-01-31 20:37:50.000000000 +0000
+--- nspr/pr/src/pthreads/ptsynch.c.orig	2024-10-21 11:31:57.000000000 +0000
 +++ nspr/pr/src/pthreads/ptsynch.c
 @@ -25,6 +25,13 @@ static pthread_condattr_t _pt_cvar_attr;
- extern PTDebug pt_debug;  /* this is shared between several modules */
- #endif  /* defined(DEBUG) */
+ extern PTDebug pt_debug; /* this is shared between several modules */
+ #  endif                 /* defined(DEBUG) */
  
 +/* XXX, pthread_equal() is misused to compare non-valid thread pointers */
 +static int
@@ -19,92 +19,92 @@ https://bugzilla.mozilla.org/show_bug.cgi?id=1718838
 +	return t1 == t2;
 +}
 +
- #if defined(FREEBSD)
+ #  if defined(FREEBSD)
  /*
   * On older versions of FreeBSD, pthread_mutex_trylock returns EDEADLK.
-@@ -197,9 +204,9 @@ PR_IMPLEMENT(PRStatus) PR_Unlock(PRLock 
-     PR_ASSERT(lock != NULL);
-     PR_ASSERT(_PT_PTHREAD_MUTEX_IS_LOCKED(lock->mutex));
-     PR_ASSERT(PR_TRUE == lock->locked);
--    PR_ASSERT(pthread_equal(lock->owner, self));
-+    PR_ASSERT(pt_pthread_equal(lock->owner, self));
+@@ -181,9 +188,9 @@ PR_IMPLEMENT(PRStatus) PR_Unlock(PRLock*
+   PR_ASSERT(lock != NULL);
+   PR_ASSERT(_PT_PTHREAD_MUTEX_IS_LOCKED(lock->mutex));
+   PR_ASSERT(PR_TRUE == lock->locked);
+-  PR_ASSERT(pthread_equal(lock->owner, self));
++  PR_ASSERT(pt_pthread_equal(lock->owner, self));
  
--    if (!lock->locked || !pthread_equal(lock->owner, self)) {
-+    if (!lock->locked || !pt_pthread_equal(lock->owner, self)) {
-         return PR_FAILURE;
-     }
+-  if (!lock->locked || !pthread_equal(lock->owner, self)) {
++  if (!lock->locked || !pt_pthread_equal(lock->owner, self)) {
+     return PR_FAILURE;
+   }
  
-@@ -225,7 +232,7 @@ PR_IMPLEMENT(void) PR_AssertCurrentThrea
-      * to the correctness of PR_AssertCurrentThreadOwnsLock(), but
-      * this particular order makes the assertion more likely to
-      * catch errors. */
--    PR_ASSERT(lock->locked && pthread_equal(lock->owner, pthread_self()));
-+    PR_ASSERT(lock->locked && pt_pthread_equal(lock->owner, pthread_self()));
+@@ -207,7 +214,7 @@ PR_IMPLEMENT(void) PR_AssertCurrentThrea
+    * to the correctness of PR_AssertCurrentThreadOwnsLock(), but
+    * this particular order makes the assertion more likely to
+    * catch errors. */
+-  PR_ASSERT(lock->locked && pthread_equal(lock->owner, pthread_self()));
++  PR_ASSERT(lock->locked && pt_pthread_equal(lock->owner, pthread_self()));
  }
  
  /**************************************************************/
-@@ -281,7 +288,7 @@ static void pt_PostNotifyToCvar(PRCondVa
-     _PT_Notified *notified = &cvar->lock->notified;
+@@ -260,7 +267,7 @@ static void pt_PostNotifyToCvar(PRCondVa
+   _PT_Notified* notified = &cvar->lock->notified;
  
-     PR_ASSERT(PR_TRUE == cvar->lock->locked);
--    PR_ASSERT(pthread_equal(cvar->lock->owner, pthread_self()));
-+    PR_ASSERT(pt_pthread_equal(cvar->lock->owner, pthread_self()));
-     PR_ASSERT(_PT_PTHREAD_MUTEX_IS_LOCKED(cvar->lock->mutex));
+   PR_ASSERT(PR_TRUE == cvar->lock->locked);
+-  PR_ASSERT(pthread_equal(cvar->lock->owner, pthread_self()));
++  PR_ASSERT(pt_pthread_equal(cvar->lock->owner, pthread_self()));
+   PR_ASSERT(_PT_PTHREAD_MUTEX_IS_LOCKED(cvar->lock->mutex));
  
-     while (1)
-@@ -369,7 +376,7 @@ PR_IMPLEMENT(PRStatus) PR_WaitCondVar(PR
-     PR_ASSERT(_PT_PTHREAD_MUTEX_IS_LOCKED(cvar->lock->mutex));
-     PR_ASSERT(PR_TRUE == cvar->lock->locked);
-     /* and it better be by us */
--    PR_ASSERT(pthread_equal(cvar->lock->owner, pthread_self()));
-+    PR_ASSERT(pt_pthread_equal(cvar->lock->owner, pthread_self()));
+   while (1) {
+@@ -336,7 +343,7 @@ PR_IMPLEMENT(PRStatus) PR_WaitCondVar(PR
+   PR_ASSERT(_PT_PTHREAD_MUTEX_IS_LOCKED(cvar->lock->mutex));
+   PR_ASSERT(PR_TRUE == cvar->lock->locked);
+   /* and it better be by us */
+-  PR_ASSERT(pthread_equal(cvar->lock->owner, pthread_self()));
++  PR_ASSERT(pt_pthread_equal(cvar->lock->owner, pthread_self()));
  
-     if (_PT_THREAD_INTERRUPTED(thred)) {
-         goto aborted;
-@@ -582,7 +589,7 @@ PR_IMPLEMENT(PRIntn) PR_GetMonitorEntryC
+   if (_PT_THREAD_INTERRUPTED(thred)) {
+     goto aborted;
+@@ -535,7 +542,7 @@ PR_IMPLEMENT(PRIntn) PR_GetMonitorEntryC
  
-     rv = pthread_mutex_lock(&mon->lock);
-     PR_ASSERT(0 == rv);
+   rv = pthread_mutex_lock(&mon->lock);
+   PR_ASSERT(0 == rv);
+-  if (pthread_equal(mon->owner, self)) {
++  if (pt_pthread_equal(mon->owner, self)) {
+     count = mon->entryCount;
+   }
+   rv = pthread_mutex_unlock(&mon->lock);
+@@ -549,7 +556,7 @@ PR_IMPLEMENT(void) PR_AssertCurrentThrea
+ 
+   rv = pthread_mutex_lock(&mon->lock);
+   PR_ASSERT(0 == rv);
+-  PR_ASSERT(mon->entryCount != 0 && pthread_equal(mon->owner, pthread_self()));
++  PR_ASSERT(mon->entryCount != 0 && pt_pthread_equal(mon->owner, pthread_self()));
+   rv = pthread_mutex_unlock(&mon->lock);
+   PR_ASSERT(0 == rv);
+ #  endif
+@@ -563,7 +570,7 @@ PR_IMPLEMENT(void) PR_EnterMonitor(PRMon
+   rv = pthread_mutex_lock(&mon->lock);
+   PR_ASSERT(0 == rv);
+   if (mon->entryCount != 0) {
 -    if (pthread_equal(mon->owner, self)) {
 +    if (pt_pthread_equal(mon->owner, self)) {
-         count = mon->entryCount;
+       goto done;
      }
+     while (mon->entryCount != 0) {
+@@ -593,8 +600,8 @@ PR_IMPLEMENT(PRStatus) PR_ExitMonitor(PR
+   PR_ASSERT(0 == rv);
+   /* the entries should be > 0 and we'd better be the owner */
+   PR_ASSERT(mon->entryCount > 0);
+-  PR_ASSERT(pthread_equal(mon->owner, self));
+-  if (mon->entryCount == 0 || !pthread_equal(mon->owner, self)) {
++  PR_ASSERT(pt_pthread_equal(mon->owner, self));
++  if (mon->entryCount == 0 || !pt_pthread_equal(mon->owner, self)) {
      rv = pthread_mutex_unlock(&mon->lock);
-@@ -598,7 +605,7 @@ PR_IMPLEMENT(void) PR_AssertCurrentThrea
-     rv = pthread_mutex_lock(&mon->lock);
      PR_ASSERT(0 == rv);
-     PR_ASSERT(mon->entryCount != 0 &&
--              pthread_equal(mon->owner, pthread_self()));
-+              pt_pthread_equal(mon->owner, pthread_self()));
-     rv = pthread_mutex_unlock(&mon->lock);
-     PR_ASSERT(0 == rv);
- #endif
-@@ -614,7 +621,7 @@ PR_IMPLEMENT(void) PR_EnterMonitor(PRMon
-     PR_ASSERT(0 == rv);
-     if (mon->entryCount != 0)
-     {
--        if (pthread_equal(mon->owner, self)) {
-+        if (pt_pthread_equal(mon->owner, self)) {
-             goto done;
-         }
-         while (mon->entryCount != 0)
-@@ -646,8 +653,8 @@ PR_IMPLEMENT(PRStatus) PR_ExitMonitor(PR
-     PR_ASSERT(0 == rv);
-     /* the entries should be > 0 and we'd better be the owner */
-     PR_ASSERT(mon->entryCount > 0);
--    PR_ASSERT(pthread_equal(mon->owner, self));
--    if (mon->entryCount == 0 || !pthread_equal(mon->owner, self))
-+    PR_ASSERT(pt_pthread_equal(mon->owner, self));
-+    if (mon->entryCount == 0 || !pt_pthread_equal(mon->owner, self))
-     {
-         rv = pthread_mutex_unlock(&mon->lock);
-         PR_ASSERT(0 == rv);
-@@ -695,7 +702,7 @@ PR_IMPLEMENT(PRStatus) PR_Wait(PRMonitor
-     /* the entries better be positive */
-     PR_ASSERT(mon->entryCount > 0);
-     /* and it better be owned by us */
--    PR_ASSERT(pthread_equal(mon->owner, pthread_self()));
-+    PR_ASSERT(pt_pthread_equal(mon->owner, pthread_self()));
+     return PR_FAILURE;
+@@ -638,7 +645,7 @@ PR_IMPLEMENT(PRStatus) PR_Wait(PRMonitor
+   /* the entries better be positive */
+   PR_ASSERT(mon->entryCount > 0);
+   /* and it better be owned by us */
+-  PR_ASSERT(pthread_equal(mon->owner, pthread_self()));
++  PR_ASSERT(pt_pthread_equal(mon->owner, pthread_self()));
  
-     /* tuck these away 'till later */
-     saved_entries = mon->entryCount;
+   /* tuck these away 'till later */
+   saved_entries = mon->entryCount;
--- devel/nspr/patches/patch-nspr_pr_src_pthreads_ptthread.c
+++ devel/nspr/patches/patch-nspr_pr_src_pthreads_ptthread.c
@@ -1,19 +1,18 @@
-$NetBSD: patch-nspr_pr_src_pthreads_ptthread.c,v 1.3 2022/12/15 23:01:17 adam Exp $
+$NetBSD: patch-nspr_pr_src_pthreads_ptthread.c,v 1.4 2024/10/22 12:52:02 ryoon Exp $
 
 pthread_t may be 64-bit, avoid casting it.
 https://bugzilla.mozilla.org/show_bug.cgi?id=1718835
 
---- nspr/pr/src/pthreads/ptthread.c.orig	2022-09-12 11:40:15.000000000 +0000
+--- nspr/pr/src/pthreads/ptthread.c.orig	2024-10-21 11:31:57.000000000 +0000
 +++ nspr/pr/src/pthreads/ptthread.c
-@@ -1183,9 +1183,9 @@ PR_IMPLEMENT(void) PR_ProcessExit(PRIntn
-     _exit(status);
- }
+@@ -1088,8 +1088,8 @@ PR_IMPLEMENT(PRStatus) PR_Cleanup(void) 
  
--PR_IMPLEMENT(PRUint32) PR_GetThreadID(PRThread *thred)
-+PR_IMPLEMENT(pthread_t) PR_GetThreadID(PRThread *thred)
- {
--    return (PRUint32)thred->id;  /* and I don't know what they will do with it */
-+    return (pthread_t)thred->id;  /* and I don't know what they will do with it */
+ PR_IMPLEMENT(void) PR_ProcessExit(PRIntn status) { _exit(status); }
+ 
+-PR_IMPLEMENT(PRUint32) PR_GetThreadID(PRThread* thred) {
+-  return (PRUint32)thred->id; /* and I don't know what they will do with it */
++PR_IMPLEMENT(pthread_t) PR_GetThreadID(PRThread* thred) {
++  return (pthread_t)thred->id; /* and I don't know what they will do with it */
  }
  
  /*
