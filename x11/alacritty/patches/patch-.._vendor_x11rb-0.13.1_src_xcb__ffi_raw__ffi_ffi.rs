a846cc27d5d33780dd5f6d023fff578d4663af34
pin <pin@pkgsrc.org>
2024-10-23 03:37:58 +0000

    x11/alacritty: update to 0.14.0
    Packaging
    
        Minimum Rust version has been bumped to 1.74.0
    
    Added
    
        Support relative path imports from config files
        alacritty migrate support for TOML configuration changes
        Headless mode using alacritty --daemon
    
    Changed
    
        Pressing Alt with unicode input will now add ESC like for ASCII input
        Decorations use opaque style and system window background on macOS
        No longer source ~/.zshenv on macOS
        Moved config options import, working_directory, live_config_reload, and ipc_socket
        to the new general section
        Moved config option shell to terminal.shell
        ctrl+shift+u binding to open links to ctrl+shift+o to avoid collisions with IMEs
        Use Beam cursor for single char cursor inside the IME preview
    
    Fixed
    
        Crash when trying to create a new tab without decorations enabled
        New window being treated as focused when it's not on Wayland
        IME preview blending into text below it
        Dynamic title disabled for new windows when initial one has title as CLI option
        While terminal in mouse mode, mouse bindings that used the shift modifier and
        had multiple actions only performed the first action
        Leaking FDs when closing windows on Unix systems
        Config emitting errors for nonexistent import paths
        Kitty keyboard protocol reporting shifted key codes
        Broken search with words broken across line boundary on the first character
        Config import changes not being live reloaded
        Cursor color requests with default cursor colors
        Fullwidth semantic escape characters
        Windows app icon now displays properly in old alt+tab on Windows
        Alacritty not being properly activated with startup notify
        Invalid URL highlights after terminal scrolling
        Hollow block cursor not spanning multiple chars being edited inside the IME preview
        Vi inline search only working for direct key input without modifiers

--- x11/alacritty/patches/patch-.._vendor_x11rb-0.13.1_src_xcb__ffi_raw__ffi_ffi.rs
+++ x11/alacritty/patches/patch-.._vendor_x11rb-0.13.1_src_xcb__ffi_raw__ffi_ffi.rs
@@ -1,10 +1,10 @@
-NetBSD$
+$NetBSD: patch-.._vendor_x11rb-0.13.1_src_xcb__ffi_raw__ffi_ffi.rs,v 1.1 2024/10/23 03:37:58 pin Exp $
 
 Remove hard-coded libxcb.so paths. pkgsrc links with the correct rpath.
 Needed for X11_TYPE=modular.
 
---- ../vendor/x11rb-0.13.0/src/xcb_ffi/raw_ffi/ffi.rs.orig	2023-12-29 14:35:22.503901115 +0000
-+++ ../vendor/x11rb-0.13.0/src/xcb_ffi/raw_ffi/ffi.rs
+--- ../vendor/x11rb-0.13.1/src/xcb_ffi/raw_ffi/ffi.rs.orig	2024-10-23 03:22:01.876902589 +0000
++++ ../vendor/x11rb-0.13.1/src/xcb_ffi/raw_ffi/ffi.rs
 @@ -35,18 +35,7 @@ pub(crate) mod libxcb_library {
              #[cfg(not(unix))]
              compile_error!("dl-libxcb feature is not supported on non-unix");
