fecdfd9855a8b7db39dd13a431400edc083f0d2f
adam <adam@pkgsrc.org>
2024-10-25 10:41:56 +0000

    qt6: updated to 6.8.0
    6.8.0
    
    For this release we have focused on improving and stabilizing existing
    functionality. With over 500 bug fixes and performance improvements since Qt
    6.7, your existing code will run better without changing a single line. On
    macOS, Qt Quick applications now integrate with the native menu bar, and for a
    native Windows 11 look they can use the new Fluent style. Resizing Quick
    windows is snappier on macOS with Qt 6.8, and on Windows the application
    start-up time has been improved by changing the default font database to
    DirectWrite.
    
    Several modules that were under technology preview have been completed: Qt
    Graphs, Qt HttpServer, and Qt GRPC are promoted to be fully supported from this
    release on. Thanks to the feedback from our users we were able to finish those
    modules with substantial improvements since their initial introduction as
    technology previews.

--- lang/qt6-qtdeclarative/patches/patch-src_qml_common_qv4alloca__p.h
+++ lang/qt6-qtdeclarative/patches/patch-src_qml_common_qv4alloca__p.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_qml_common_qv4alloca__p.h,v 1.2 2023/04/07 18:28:04 nros Exp $
+$NetBSD: patch-src_qml_common_qv4alloca__p.h,v 1.3 2024/10/25 10:41:58 adam Exp $
 
 Make sure we pick up gcc's builtin alloca on NetBSD
 
---- src/qml/common/qv4alloca_p.h.orig	2023-03-23 08:11:12.000000000 +0000
+--- src/qml/common/qv4alloca_p.h.orig	2024-10-02 08:59:48.000000000 +0000
 +++ src/qml/common/qv4alloca_p.h
-@@ -42,9 +42,13 @@
+@@ -43,9 +43,13 @@
  #define Q_ALLOCA_DECLARE(type, name) \
      type *name = 0
  
@@ -17,5 +17,5 @@ Make sure we pick up gcc's builtin alloca on NetBSD
 -
 +#endif
  #else
- QT_BEGIN_NAMESPACE
- class Qt_AllocaWrapper
+ #  include <memory>
+ 
--- x11/qt6-qtbase/patches/patch-cmake_QtProcessConfigureArgs.cmake
+++ x11/qt6-qtbase/patches/patch-cmake_QtProcessConfigureArgs.cmake
@@ -1,4 +1,4 @@
-$NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.2 2023/10/31 19:56:48 adam Exp $
+$NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.3 2024/10/25 10:41:57 adam Exp $
 
 * add function so that libraries such as socket and nsl 
   can be added to tests
@@ -6,21 +6,21 @@ $NetBSD: patch-cmake_QtProcessConfigureArgs.cmake,v 1.2 2023/10/31 19:56:48 adam
 * add support for avoiding the cmake archiving api using configure since
   libarchive in pkgsrc that cmake uses is built without zstd support
 
---- cmake/QtProcessConfigureArgs.cmake.orig	2023-09-13 16:02:48.000000000 +0000
+--- cmake/QtProcessConfigureArgs.cmake.orig	2024-10-01 10:46:30.000000000 +0000
 +++ cmake/QtProcessConfigureArgs.cmake
-@@ -240,6 +240,7 @@ defstub(set_package_properties)
- defstub(qt_qml_find_python)
- defstub(qt_set01)
- defstub(qt_internal_check_if_linker_is_available)
+@@ -268,6 +268,7 @@ defstub(qt_internal_extend_sbom)
+ defstub(qt_internal_sbom_add_license)
+ defstub(qt_internal_extend_sbom_dependencies)
+ defstub(qt_find_package_extend_sbom)
 +defstub(qt_test_lib_target)
  
  ####################################################################################################
  # Define functions/macros that are called in qt_cmdline.cmake files
-@@ -864,6 +865,7 @@ translate_boolean_input(ccache QT_USE_CC
+@@ -928,6 +929,7 @@ translate_boolean_input(ccache QT_USE_CC
  translate_boolean_input(vcpkg QT_USE_VCPKG)
  translate_boolean_input(shared BUILD_SHARED_LIBS)
  translate_boolean_input(warnings_are_errors WARNINGS_ARE_ERRORS)
 +translate_boolean_input(avoid_cmake_archiving_api QT_AVOID_CMAKE_ARCHIVING_API)
+ translate_boolean_input(qtinlinenamespace QT_INLINE_NAMESPACE)
  translate_string_input(qt_namespace QT_NAMESPACE)
  translate_string_input(qt_libinfix QT_LIBINFIX)
- translate_string_input(qreal QT_COORD_TYPE)
