5fa542f4a6907ecd4b8e1cf9af2412e57da1abf0
nia <nia@pkgsrc.org>
2024-10-26 07:35:54 +0000

    gdk-pixbuf2: Make tests optional.

--- graphics/gdk-pixbuf2/options.mk
+++ graphics/gdk-pixbuf2/options.mk
@@ -1,8 +1,8 @@
-#	$NetBSD: options.mk,v 1.1 2024/04/12 19:41:39 riastradh Exp $
+#	$NetBSD: options.mk,v 1.2 2024/10/26 07:35:54 nia Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gdk-pixbuf2
-PKG_SUPPORTED_OPTIONS=	introspection
+PKG_SUPPORTED_OPTIONS=	introspection tests
 PKG_SUGGESTED_OPTIONS=	${${USE_CROSS_COMPILE:tl} == "yes":?:introspection}
 
 .include "../../mk/bsd.options.mk"
@@ -15,4 +15,11 @@ BUILDLINK_DEPMETHOD.gobject-introspection:= build
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .else
 MESON_ARGS+=		-Dintrospection=disabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mtests)
+MESON_ARGS+=		-Dtests=true
+MESON_ARGS+=		-Dinstalled_tests=false
+.else
+MESON_ARGS+=		-Dtests=false
 .endif
