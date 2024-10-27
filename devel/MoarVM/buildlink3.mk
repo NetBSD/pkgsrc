933fe5f36a99298fbe937e40973360ab5328fab0
mef <mef@pkgsrc.org>
2024-10-27 05:37:25 +0000

    (devel/MoarVM) Updated 2024.09 to 2024.10
    2024.10
    
    Core:
    
        Update libuv to version 1.49.0 [3ac5fa66]
        Give containers explicit uint store funcs [1b4f6d91][c331b005]
    
    Documentation:
    
        Output a very visible notice when building with asan [d2c12b97]
    
    GC:
    
    JIT:
    
    Spesh:
    
        Add debug helper to dump locals vars [6f925f25][b2844989][824436eb]

--- devel/MoarVM/buildlink3.mk
+++ devel/MoarVM/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2024/10/14 01:41:08 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2024/10/27 05:37:25 mef Exp $
 
 BUILDLINK_TREE+=	MoarVM
 
 .if !defined(MOARVM_BUILDLINK3_MK)
 MOARVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2024.09
-BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2024.09
+BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2024.10
+BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2024.10
 BUILDLINK_PKGSRCDIR.MoarVM?=	../../devel/MoarVM
 
 .include "../../devel/libatomic_ops/buildlink3.mk"
