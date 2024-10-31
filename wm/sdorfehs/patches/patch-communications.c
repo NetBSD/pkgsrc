20ba106f8c2503ca33b4df07484ad377d99e24ed
pin <pin@pkgsrc.org>
2024-10-31 10:50:54 +0000

    wm/sdorfehs: update to 1.6
    No release notes provided.

--- wm/sdorfehs/patches/patch-communications.c
+++ wm/sdorfehs/patches/patch-communications.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-communications.c,v 1.1 2024/03/10 21:02:21 vins Exp $
+$NetBSD: patch-communications.c,v 1.2 2024/10/31 10:50:55 pin Exp $
 
 Rename local variables to avoid name clash on SunOS.
 
---- communications.c.orig	2024-03-10 19:07:49.914451195 +0000
+--- communications.c.orig	2024-10-31 10:29:35.665002807 +0000
 +++ communications.c
-@@ -49,26 +49,26 @@ init_control_socket_path(void)
+@@ -49,27 +49,27 @@ init_control_socket_path(void)
  void
  listen_for_commands(void)
  {
@@ -21,10 +21,12 @@ Rename local variables to avoid name clash on SunOS.
  		    rp_glob_screen.control_socket_path);
  
 -	strncpy(sun.sun_path, rp_glob_screen.control_socket_path,
--	    sizeof(sun.sun_path));
+-	    sizeof(sun.sun_path)-1);
+-	sun.sun_path[sizeof(sun.sun_path) - 1] = '\0';
 -	sun.sun_family = AF_UNIX;
 +	strncpy(lsun.sun_path, rp_glob_screen.control_socket_path,
-+	    sizeof(lsun.sun_path));
++	    sizeof(lsun.sun_path)-1);
++	lsun.sun_path[sizeof(lsun.sun_path) - 1] = '\0';
 +	lsun.sun_family = AF_UNIX;
  
  	if (unlink(rp_glob_screen.control_socket_path) == -1 &&
@@ -38,7 +40,7 @@ Rename local variables to avoid name clash on SunOS.
  		err(1, "bind %s", rp_glob_screen.control_socket_path);
  
  	if (chmod(rp_glob_screen.control_socket_path, 0600) == -1)
-@@ -84,7 +84,7 @@ listen_for_commands(void)
+@@ -85,7 +85,7 @@ listen_for_commands(void)
  int
  send_command(int interactive, unsigned char *cmd)
  {
@@ -47,7 +49,7 @@ Rename local variables to avoid name clash on SunOS.
  	char *wcmd, *bufstart;
  	char ret[BUFSZ+1];
  	char success = 0;
-@@ -108,15 +108,15 @@ send_command(int interactive, unsigned c
+@@ -109,16 +109,16 @@ send_command(int interactive, unsigned c
  	if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
  		err(1, "socket");
  
@@ -57,10 +59,12 @@ Rename local variables to avoid name clash on SunOS.
  		    rp_glob_screen.control_socket_path);
  
 -	strncpy(sun.sun_path, rp_glob_screen.control_socket_path,
--	    sizeof(sun.sun_path));
+-	    sizeof(sun.sun_path)-1);
+-	sun.sun_path[sizeof(sun.sun_path) - 1] = '\0';
 -	sun.sun_family = AF_UNIX;
 +	strncpy(lsun.sun_path, rp_glob_screen.control_socket_path,
-+	    sizeof(lsun.sun_path));
++	    sizeof(lsun.sun_path)-1);
++	lsun.sun_path[sizeof(lsun.sun_path) - 1] = '\0';
 +	lsun.sun_family = AF_UNIX;
  
 -	if (connect(fd, (struct sockaddr *)&sun, sizeof(sun)) == -1)
