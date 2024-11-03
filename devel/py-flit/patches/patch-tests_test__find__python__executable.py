8f835e960380a2b6a195c17712b3015c1238971d
adam <adam@pkgsrc.org>
2024-11-03 07:50:30 +0000

    py-flit_core py-flit: updated to 3.10.1
    Version 3.10.1
    
    - The sdist of ``flit_core`` now includes the corresponding tests
      These were missing in 3.10.
    
    Version 3.10
    
    - ``flit publish`` can now use PyPI tokens stored in keyring
      either project tokens with a 'username' like
      :samp:`pypi_token:project:{project_name}` (use the `normalised form of the
      name <https://packaging.python.org/en/latest/specifications/name-normalization/>`_)
      or user tokens
    - The ``--python`` option can now take the path of a virtualenv folder, as an
      alternative to a Python executable
    - Flit will work with current development versions of Pythona again
    - The ``flit`` command line package now requires Python 3.8 or above
      ``flit_core`` still works with Python 3.6 or above.
    - The metadata in packages now has the names of optional dependency groups
      ("extras") normalised, complying with version 2.3 of the metadata standard
    - The ``flit`` command line package now depends on pip
    - Fix potential substitution of environment variables into passwords read from
      ``.pypirc`` files
    - A warning is now shown when building packages which specify the old
      ``flit.buildapi`` backend, which should be replaced by ``flit_core.buildapi``
      build requirement, to protect against changes in future major versions of Flit.
    - Avoid using the deprecated ``datetime.utcfromtimestamp()``
    - Flit now has a ``SECURITY.md`` file in the Github repository
    - The tests for ``flit_core`` are no longer part of the installed package,
      reducing the size of the wheels

--- devel/py-flit/patches/patch-tests_test__find__python__executable.py
+++ devel/py-flit/patches/patch-tests_test__find__python__executable.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-tests_test__find__python__executable.py,v 1.1 2020/12/04 23:29:32 riastradh Exp $
+$NetBSD: patch-tests_test__find__python__executable.py,v 1.2 2024/11/03 07:50:30 adam Exp $
 
 Disable test that expects `python' per se to appear in PATH, which it
 generally does not in pkgsrc.
 
---- tests/test_find_python_executable.py.orig	2020-09-06 10:55:07.000000000 +0000
+--- tests/test_find_python_executable.py.orig	2024-11-02 16:21:55.873429800 +0000
 +++ tests/test_find_python_executable.py
-@@ -20,6 +20,7 @@ def test_abs():
+@@ -23,6 +23,7 @@ def test_abs():
  
  
  def test_find_in_path():
 +    return
-     assert os.path.isabs(find_python_executable("python"))
+     assert isabs(find_python_executable("python"))
  
  
