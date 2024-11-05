8e56d9d0d69b8d730852ff697a1e9774a74efb24
adam <adam@pkgsrc.org>
2024-11-05 18:18:12 +0000

    sratom: updated to 0.6.16
    sratom (0.6.16) stable; urgency=medium
    
    * Clean up code
    * Constrain relative URI references to the base URI
    * Fix dependencies in pkg-config file
    * Override pkg-config dependency within meson
    * Remove junk files from documentation install
    * Replace duplicated dox_to_sphinx script with sphinxygen dependency
    
    sratom (0.6.14) stable; urgency=medium
    
    * Add project metadata
    * Adopt REUSE machine-readable licensing standard
    
    sratom (0.6.12) stable; urgency=medium
    
    * Switch to meson build system

--- converters/sratom/buildlink3.mk
+++ converters/sratom/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/08/14 05:23:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/11/05 18:18:12 adam Exp $
 
 BUILDLINK_TREE+=	sratom
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	sratom
 SRATOM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.sratom+=	sratom>=0.4.6
-BUILDLINK_ABI_DEPENDS.sratom?=	sratom>=0.6.10nb5
+BUILDLINK_ABI_DEPENDS.sratom+=	sratom>=0.6.10nb5
 BUILDLINK_PKGSRCDIR.sratom?=	../../converters/sratom
 
-.include "../../textproc/sord/buildlink3.mk"
 .include "../../textproc/serd/buildlink3.mk"
+.include "../../textproc/sord/buildlink3.mk"
 .endif	# SRATOM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-sratom
