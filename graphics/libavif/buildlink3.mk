9135450bbb90b026935cb4631e46a54a07f47ace
wiz <wiz@pkgsrc.org>
2024-11-05 22:29:20 +0000

    libavif: include aom in bl3.mk
    It's listed as requirement in the installed pkg-config file.

--- graphics/libavif/buildlink3.mk
+++ graphics/libavif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/01/05 11:14:02 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/11/05 22:29:20 wiz Exp $
 
 BUILDLINK_TREE+=	libavif
 
@@ -8,6 +8,7 @@ LIBAVIF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libavif+=	libavif>=1.0.3
 BUILDLINK_PKGSRCDIR.libavif?=	../../graphics/libavif
 
+.include "../../multimedia/libaom/buildlink3.mk"
 .endif	# LIBAVIF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libavif
