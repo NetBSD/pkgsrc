c4dd1ff27b14da223bdd56353a6878c46ea5d0a3
adam <adam@pkgsrc.org>
2024-11-05 18:22:30 +0000

    lilv: updated to 0.24.24
    lilv (0.24.24) stable; urgency=medium
    
    * Avoid overriding state features passed by the host
    * Fix null dereference when trying to load state from a missing file
    * Fix potential null dereferences and conversion warnings
    
    lilv (0.24.22) stable; urgency=medium
    
    * Allow LILV_API to be defined by the user
    * Clean up code
    * Clean up inconsistent tool command line interfaces
    * Convert man pages to mdoc
    * Fix crash when plugins pass NULL to the LV2_State_Retrieve_Function
    * Fix dependencies in pkg-config file
    * Fix potential crash when writing state files fails
    * Order plugin classes by URI
    * Override pkg-config dependency within meson
    * Remove junk files from documentation install
    * Replace duplicated dox_to_sphinx script with sphinxygen dependency
    * Switch to external zix dependency
    
    lilv (0.24.20) stable; urgency=medium
    
    * Adopt REUSE machine-readable licensing standard
    * Update project metadata
    
    lilv (0.24.18) stable; urgency=medium
    
    * Allow programs to be used from subproject
    * Fix default LV2_PATH
    
    lilv (0.24.16) stable; urgency=medium
    
    * Fix fallback flock() detection on MacOS
    * Switch to meson build system

--- audio/lilv/options.mk
+++ audio/lilv/options.mk
@@ -1,23 +1,25 @@
-# $NetBSD: options.mk,v 1.3 2023/06/06 12:40:17 riastradh Exp $
+# $NetBSD: options.mk,v 1.4 2024/11/05 18:22:30 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lilv
-PKG_SUPPORTED_OPTIONS=	debug doc tests
-PKG_SUGGESTED_OPTIONS+=	# blank
-PLIST_VARS+=		doc
+PKG_SUPPORTED_OPTIONS=	doc tests
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mdebug)
-WAF_ARGS+=		--debug
-.endif
+PLIST_VARS+=	doc
 
 .if !empty(PKG_OPTIONS:Mdoc)
-WAF_ARGS+=		--docs
-TOOL_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
-TOOL_DEPENDS+=		graphviz-[0-9]*:../../graphics/graphviz
-PLIST.doc=		yes
+MESON_ARGS+=	-Ddocs=enabled
+TOOL_DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
+TOOL_DEPENDS+=	graphviz-[0-9]*:../../graphics/graphviz
+TOOL_DEPENDS+=	${PYPKGPREFIX}-sphinx>0:../../textproc/py-sphinx
+TOOL_DEPENDS+=	${PYPKGPREFIX}-sphinxygen>0:../../textproc/py-sphinxygen
+PLIST.doc=	yes
+.else
+MESON_ARGS+=	-Ddocs=disabled
 .endif
 
 .if !empty(PKG_OPTIONS:Mtests)
-WAF_ARGS+=		--test
+MESON_ARGS+=	-Dtests=enabled
+.else
+MESON_ARGS+=	-Dtests=disabled
 .endif
