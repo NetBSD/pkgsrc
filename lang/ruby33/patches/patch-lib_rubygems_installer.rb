b404052f3e7d7687b6b44cb460c04402400cf0e4
taca <taca@pkgsrc.org>
2024-11-06 13:31:55 +0000

    lang/ruby33: update to 3.3.6
    3.3.6 (2024-11-05)
    
    What's Changed
    
    * Merge JSON 2.7.2 for Ruby 3.3 by hsbt · Pull Request #11541
    
    * Merge reline-0.5.10 by hsbt · Pull Request #11558
    
    * Bug #20718: Objects created with Data_Make_Struct and the default free
      function are not freed
    
    * Bug #20737: Accidentally changed warning target on Ruby 3.3
    
    * Bug #20723: IO#close is broken on Ruby 3.3+ when using the Fiber
      scheduler.
    
    * Use PRIuSIZE instead of %zu for size_t by nobu · Pull Request #9359
    
    * Bump REXML to 3.3.9 for Ruby 3.3 by ajmyers01 · Pull Request #11972
    
    * Bug #13831: error when try to install
    
    * Bug #20777: 3.3: RUBY_DESCRIPTION is corrupt when --yjit and
      --parser=prism
    
    * Bug #20704: Windows: -C option does not work for multibyte path name
    
    * Bug #20719: Float converts ASCII-incompatible string
    
    * Bug #20752: IO::Buffer#slice creates mutable IO::Buffer instance that
      points to readonly memory (e.g. fronzen String)
    
    * Bug #20755: IO::Buffer#transfer transfers mutable reference to String's
      memory but not lock ownership
    
    * Bug #20716: Different instance_method behavior in Ruby 2.7 and Ruby 3.x
    
    * Bug #20853: Hash key retrieval after Process.warmup

--- lang/ruby33/patches/patch-lib_rubygems_installer.rb
+++ lang/ruby33/patches/patch-lib_rubygems_installer.rb
@@ -1,12 +1,11 @@
-$NetBSD: patch-lib_rubygems_installer.rb,v 1.2 2024/09/05 15:02:42 taca Exp $
+$NetBSD: patch-lib_rubygems_installer.rb,v 1.3 2024/11/06 13:31:55 taca Exp $
 
 * Add install_root option for pkgsrc's rubygems support.
 * Tweak build_info directory with destdir to store build_args.
-* Fix from "Installer lock file cleanup #7939".
 
---- lib/rubygems/installer.rb.orig	2024-09-03 01:09:08.000000000 +0900
-+++ lib/rubygems/installer.rb	2024-09-05 23:34:15.179174905 +0900
-@@ -167,6 +167,9 @@ class Gem::Installer
+--- lib/rubygems/installer.rb.orig	2024-11-05 00:49:11.000000000 +0000
++++ lib/rubygems/installer.rb
+@@ -160,6 +160,9 @@ class Gem::Installer
    #                      foo_exec18.
    # :ignore_dependencies:: Don't raise if a dependency is missing.
    # :install_dir:: The directory to install the gem into.
@@ -16,25 +15,7 @@ $NetBSD: patch-lib_rubygems_installer.rb,v 1.2 2024/09/05 15:02:42 taca Exp $
    # :security_policy:: Use the specified security policy.  See Gem::Security
    # :user_install:: Indicate that the gem should be unpacked into the users
    #                 personal gem directory.
-@@ -538,7 +541,7 @@
-   def generate_bin_script(filename, bindir)
-     bin_script_path = File.join bindir, formatted_program_filename(filename)
- 
--    Gem.open_file_with_flock("#{bin_script_path}.lock") do
-+    Gem.open_file_with_flock("#{bin_script_path}.lock") do |lock|
-       require "fileutils"
-       FileUtils.rm_f bin_script_path # prior install may have been --no-wrappers
- 
-@@ -546,6 +549,8 @@
-         file.write app_script_text(filename)
-         file.chmod(options[:prog_mode] || 0o755)
-       end
-+    ensure
-+      FileUtils.rm_f lock.path
-     end
- 
-     verbose bin_script_path
-@@ -676,12 +679,20 @@ class Gem::Installer
+@@ -671,12 +674,20 @@ class Gem::Installer
  
      @build_args = options[:build_args]
  
@@ -56,7 +37,7 @@ $NetBSD: patch-lib_rubygems_installer.rb,v 1.2 2024/09/05 15:02:42 taca Exp $
  
      @plugins_dir = Gem.plugindir(@gem_home)
  
-@@ -937,6 +948,9 @@ TEXT
+@@ -929,6 +940,9 @@ TEXT
      return if build_args.empty?
  
      build_info_dir = File.join gem_home, "build_info"
