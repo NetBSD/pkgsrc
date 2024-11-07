bcc37a2175b8ce8f514063721ba0cbd3fa6af4bf
wiz <wiz@pkgsrc.org>
2024-11-07 07:35:58 +0000

    gegl: update to 0.4.50.
    GEGL-0.4.50 2024-11-03
    
    Core:
    
    Better Gobject Introspection annotation coverage.
    
    Improved error handling for gegl chain processing.
    
    Added accessors for pad meta-data, to be able to show names and
    descriptions in UIs.
    
    GeglColor: more lax parameters for g_arapm_values_cmp
    
    Ops:
    
    API: GEGL_OP_NAME needs to be uniqe .c filenames no longer needs to be.
    First registered op with a given name now takes precedence over later.
    
      * exr-save: store 16bpc files as half, fixing gimp#10778
    
      * ff-load, ff-save: fix build with FFmpeg 7
    
      * dither: do processing in linear when all levels==2
    
      * OpenCL: upgrade from 1.1 to 3.0, disable opencl caching system -
        not crashing is more important than caching. And fixes to the
        opencl operation of the following ops:
    
      * alien-map
    
      * color-to-alpha
    
      * edge-sobel
    
      * hue-chroma
    
      * noise-reduction
    
      * oilify
    
      * snn-mean
    
    New Ops:
    
    styles: a UI for achieving many visual effects for use on text and
    similar alpha defined shapes. bevel: simulate light and shadow on an
    alpha defined shape. inner-glow: an inner shadow glow effect
    
    New Ops in workshop:
    
    ctx-script: an experimental op for rendering ctx protocol this might be
    a future basis for ctx-based vector layers.
    
    Build:
    
    Various meson cleanups. Generate .pdb (CodeView) debug symbols for
    Windows Reproducible build fixed by getting rid of runtime values in
    .gir files.
    
    Updated vendored libraries
    
    ctx has been updated from upstream, perhaps most significantly the
    protocol/drawing API has been fuzzed with afl+

--- graphics/gegl/options.mk
+++ graphics/gegl/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2021/04/09 05:12:58 adam Exp $
+# $NetBSD: options.mk,v 1.5 2024/11/07 07:35:58 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gegl
 PKG_SUPPORTED_OPTIONS=	svg jasper
@@ -11,6 +11,7 @@ PLIST_VARS+=	jasper svg
 .if !empty(PKG_OPTIONS:Msvg)
 .include "../../graphics/librsvg/buildlink3.mk"
 PLIST.svg=	yes
+PRINT_PLIST_AWK+=	{ if (/svg-load/) $$0 = "$${PLIST.svg}" $$0 }
 .else
 MESON_ARGS+=	-Dlibrsvg=disabled
 .endif
@@ -18,6 +19,7 @@ MESON_ARGS+=	-Dlibrsvg=disabled
 .if !empty(PKG_OPTIONS:Mjasper)
 .include "../../graphics/jasper/buildlink3.mk"
 PLIST.jasper=	yes
+PRINT_PLIST_AWK+=	{ if (/jp2-load/) $$0 = "$${PLIST.jasper}" $$0 }
 .else
 MESON_ARGS+=	-Djasper=disabled
 .endif
