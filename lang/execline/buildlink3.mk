29707ed1bd945b13d9da2295a815f08b7d3a0d81
schmonz <schmonz@pkgsrc.org>
2024-11-14 14:11:55 +0000

    execline: update to 2.9.6.1. Changes:
    - Bugfixes.

--- lang/execline/buildlink3.mk
+++ lang/execline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/06/19 18:58:58 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/11/14 14:11:55 schmonz Exp $
 
 BUILDLINK_TREE+=	execline
 
 .if !defined(EXECLINE_BUILDLINK3_MK)
 EXECLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.6.0
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.9.6.1
 BUILDLINK_PKGSRCDIR.execline?=		../../lang/execline
 BUILDLINK_DEPMETHOD.execline?=		build
 .endif # EXECLINE_BUILDLINK3_MK
