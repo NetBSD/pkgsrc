1387dbb8dcbff88989e00cab83fcb1ae26bb87e8
schmonz <schmonz@pkgsrc.org>
2024-11-14 14:12:09 +0000

    s6: update to 2.13.1.0. Changes:
    - Bugfixes.
    - s6-supervise now supports a new file in the service directory:
      flag-newpidns. If this file is present at service spawning time, the
      process will be started in a new PID namespace. (Linux only; on other
      systems, the service will fail to start if the file exists.)

--- sysutils/s6/buildlink3.mk
+++ sysutils/s6/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/06/19 18:59:28 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2024/11/14 14:12:09 schmonz Exp $
 
 BUILDLINK_TREE+=	s6
 
 .if !defined(S6_BUILDLINK3_MK)
 S6_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6+=	s6>=2.13.0.0
+BUILDLINK_API_DEPENDS.s6+=	s6>=2.13.1.0
 BUILDLINK_PKGSRCDIR.s6?=	../../sysutils/s6
 BUILDLINK_INCDIRS.s6+=		include/s6
 BUILDLINK_LIBDIRS.s6+=		lib/s6
