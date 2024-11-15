b86a4d57d4498416bc04575542cda2a113c57d85
pin <pin@pkgsrc.org>
2024-11-15 12:41:15 +0000

    devel/libdbusmenu-lxqt: update to 0.2.0
    libdbusmenu-lxqt-0.2.0 / 2024-11-05
    ===================================
     * Stop checking for `QIcon::name()` existence.
     * Fix AUTOMOC related warnings.
     * Don't redefine already defined macros.
     * Remove Bazaar stuff.
     * Use CMake to set the CXX standard in use.
     * Use CMake to set the symbol visibility.
     * Remove duplicated compiler flags.
     * Move compiler flag check/set.
     * Update Qt6 include directories use.
     * Update self include directories.
     * Removes dist and distcheck targets.
     * Renamed README;compiling instructions.

--- devel/libdbusmenu-lxqt/buildlink3.mk
+++ devel/libdbusmenu-lxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/11/14 22:19:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2024/11/15 12:41:15 pin Exp $
 
 BUILDLINK_TREE+=	libdbusmenu-lxqt
 
 .if !defined(LIBDBUSMENU_LXQT_BUILDLINK3_MK)
 LIBDBUSMENU_LXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdbusmenu-lxqt+=	libdbusmenu-lxqt>=0.1.0
-BUILDLINK_ABI_DEPENDS.libdbusmenu-lxqt?=	libdbusmenu-lxqt>=0.1.0nb3
+BUILDLINK_API_DEPENDS.libdbusmenu-lxqt+=	libdbusmenu-lxqt>=0.2.0
 BUILDLINK_PKGSRCDIR.libdbusmenu-lxqt?=	../../devel/libdbusmenu-lxqt
 
 .include "../../x11/qt6-qtbase/buildlink3.mk"
