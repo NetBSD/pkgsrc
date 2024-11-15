17cc9e3eed9c5363a19cc594548b22ae38a85940
pin <pin@pkgsrc.org>
2024-11-15 12:21:33 +0000

    x11/libqtxdg: update to 4.1.0
    libqtxdg-4.1.0 / 2024-11-05
    ============================
     * Bumped the version to 4.1.0.

--- x11/libqtxdg/buildlink3.mk
+++ x11/libqtxdg/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.31 2024/11/14 22:22:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2024/11/15 12:21:33 pin Exp $
 
 BUILDLINK_TREE+=	libqtxdg
 
 .if !defined(LIBQTXDG_BUILDLINK3_MK)
 LIBQTXDG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=4.0.0
-BUILDLINK_ABI_DEPENDS.libqtxdg?=		libqtxdg>=4.0.1nb3
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=4.1.0
 BUILDLINK_PKGSRCDIR.libqtxdg?=		../../x11/libqtxdg
 
 .include "../../devel/glib2/buildlink3.mk"
