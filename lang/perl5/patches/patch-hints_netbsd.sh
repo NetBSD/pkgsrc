9b525808b0087c1a94431d2526c8e0ef8f36ab15
wiz <wiz@pkgsrc.org>
2024-11-16 11:54:24 +0000

    perl: update to 5.40.0.
    Core Enhancements
    
        New __CLASS__ Keyword
        :reader attribute for field variables
        Permit a space in -M command-line option
        Restrictions to use VERSION declarations
        New builtin::inf and builtin::nan functions (experimental)
        New ^^ logical xor operator
        try/catch feature is no longer experimental
        for iterating over multiple values at a time is no longer experimental
        builtin module is no longer experimental
        The :5.40 feature bundle adds try
        use v5.40; imports builtin functions
    
    Security
    
        CVE-2023-47038 - Write past buffer end via illegal user-defined Unicode property
        CVE-2023-47039 - Perl for Windows binary hijacking vulnerability
    
    Incompatible Changes
    
        reset EXPR now calls set-magic on scalars
        Calling the import method of an unknown package produces a warning
        return no longer allows an indirect object
        Class barewords no longer resolved as file handles in method calls under no feature "bareword_filehandles"

--- lang/perl5/patches/patch-hints_netbsd.sh
+++ lang/perl5/patches/patch-hints_netbsd.sh
@@ -1,29 +1,10 @@
-$NetBSD: patch-hints_netbsd.sh,v 1.10 2015/02/25 14:56:45 wiz Exp $
+$NetBSD: patch-hints_netbsd.sh,v 1.11 2024/11/16 11:54:24 wiz Exp $
 
---whole-archive is a linker flag, not a compiler flag
 Better defaults for paths.
-Stop grovelling for functions we don't want to provide (*host*)
 
---- hints/netbsd.sh.orig	2015-01-17 16:59:58.000000000 +0000
+--- hints/netbsd.sh.orig	2024-11-16 08:28:23.750420799 +0000
 +++ hints/netbsd.sh
-@@ -36,16 +36,6 @@ case "$osvers" in
- 		d_dlerror=$define
- 		cccdlflags="-DPIC -fPIC $cccdlflags"
- 		lddlflags="-shared $lddlflags"
--		cat >UU/cc.cbu <<'EOCBU'
--# gcc 4.6 doesn't support --whole-archive, but it's required for the
--# system gcc to build correctly, so check for it
--echo 'int f(void) { return 0; }' >try.c
--if ${cc:-cc} $cccdlflags -c try.c -otry.o 2>&1 &&
--   ${cc:-cc} --whole-archive $lddlflags try.o -otry.so 2>&1 ; then
--    lddlflags="--whole-archive $lddlflags"
--fi
--rm try.c try.o try.so 2>/dev/null
--EOCBU
- 		rpathflag="-Wl,-rpath,"
- 		case "$osvers" in
- 		1.[0-5]*)
-@@ -200,10 +190,12 @@ esac
+@@ -200,10 +200,11 @@ esac
  EOCBU
  
  # Set sensible defaults for NetBSD: look for local software in
@@ -33,9 +14,8 @@ Stop grovelling for functions we don't want to provide (*host*)
  #
 -loclibpth="/usr/pkg/lib /usr/local/lib"
 -locincpth="/usr/pkg/include /usr/local/include"
-+loclibpth="/usr/local/lib ${prefix}/lib"
-+locincpth="/usr/local/include ${prefix}/include"
-+
++loclibpth="${prefix}/lib /usr/local/lib"
++locincpth="${prefix}/include /usr/local/include"
  case "$rpathflag" in
  '')
  	ldflags=
