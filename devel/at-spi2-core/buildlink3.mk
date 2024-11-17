3c170255d03366a81354f66821b07ea97b361125
dholland <dholland@pkgsrc.org>
2024-11-17 00:16:16 +0000

    at-spi2-core: add dbus option.
    Turning off the dbus option makes this into more or less the same as
    the old atk library: that is, if you didn't have at-spi2-core
    installed before, only atk, you can turn the option off to avoid
    incurring a mandatory dbus dependency in all sorts of things that
    didn't previously have one, like gtk2.
    
    If you did have at-spi2-core installed before because things actually
    used it, turning off the dbus option off will break those things. (As
    in, they won't even build.)
    
    It would be better if turning off dbus provided the at-spi2-core
    libraries in a way that didn't depend on dbus, so things that wanted
    them could build, but that's not the hook that upstream gives us.
    (It's also not clear from very casual inspection whether at-spi2 is
    architected adequately to be transport-independent.)
    
    It also might be better to go back to having a separate package for
    the atk library, but this is also not so easy to arrange.
    
    Therefore, the dbus option is enabled by default here.

--- devel/at-spi2-core/buildlink3.mk
+++ devel/at-spi2-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/10/20 14:02:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2024/11/17 00:16:16 dholland Exp $
 
 BUILDLINK_TREE+=	at-spi2-core
 
@@ -9,8 +9,15 @@ BUILDLINK_API_DEPENDS.at-spi2-core+=	at-spi2-core>=2.3.2
 BUILDLINK_ABI_DEPENDS.at-spi2-core+=	at-spi2-core>=2.54.0
 BUILDLINK_PKGSRCDIR.at-spi2-core?=	../../devel/at-spi2-core
 
+pkgbase:= at-spi2-core
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../devel/glib2/buildlink3.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.at-spi2-core:Mdbus)
 .include "../../sysutils/dbus/buildlink3.mk"
+.endif
+
 .endif # AT_SPI2_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-at-spi2-core
