c3716d85c13f0869c858bdf18424d065034aab44
dholland <dholland@pkgsrc.org>
2024-11-17 00:16:16 +0000

    at-spi2-core: add dbus option.
    Turning off the dbus option makes this into more or less the same as
    the old atk library: that is, if you didn't have at-spi2-core
    installed before, only atk, you can turn the option off to avoid
    incurring a mandatory dbus dependency in all sorts of things that
    didn't previously have one, like gtk2.
    
    If you did have at-spi2-core installed before because things actually
    used it, turning off the dbus option off will break those things. (As
    in, they won't even build.)
    
    It would be better if turning off dbus provided the at-spi2-core
    libraries in a way that didn't depend on dbus, so things that wanted
    them could build, but that's not the hook that upstream gives us.
    (It's also not clear from very casual inspection whether at-spi2 is
    architected adequately to be transport-independent.)
    
    It also might be better to go back to having a separate package for
    the atk library, but this is also not so easy to arrange.
    
    Therefore, the dbus option is enabled by default here.

---
+++ devel/at-spi2-core/options.mk
@@ -0,0 +1,46 @@
+# $NetBSD: options.mk,v 1.1 2024/11/17 00:16:16 dholland Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.at-spi2-core
+
+#
+# Make dbus optional. Note that turning dbus off does not actually
+# make at-spi2 work without dbus; it tells it to build only atk, and
+# makes this package approximately equivalent to the standalone atk
+# package that used to exist. This in turn avoids incurring a
+# mandatory dbus dependency for a large number of applications that
+# never previously had one.
+#
+# However, anything that actually needs at-spi2 will not work with
+# dbus turned off, and probably not build either.
+#
+PKG_SUPPORTED_OPTIONS=	dbus
+PKG_SUGGESTED_OPTIONS=	dbus
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	dbus
+
+
+.if !empty(PKG_OPTIONS:Mdbus)
+PLIST.dbus=	yes
+
+#
+# These files only get installed when dbus is in use.
+#
+MAKE_DIRS+=	${PKG_SYSCONFDIR}/at-spi2
+CONF_FILES=	${EGDIR}/at-spi2/accessibility.conf \
+		${PKG_SYSCONFDIR}/at-spi2/accessibility.conf
+CONF_FILES+=	${EGDIR}/xdg/autostart/at-spi-dbus-bus.desktop \
+		${PKG_SYSCONFDIR}/xdg/autostart/at-spi-dbus-bus.desktop
+post-install:
+	${INSTALL_DATA_DIR} ${DESTDIR}${EGDIR}/xdg/autostart
+	${MV} ${DESTDIR}${PKG_SYSCONFDIR}/xdg/autostart/at-spi-dbus-bus.desktop \
+	      ${DESTDIR}${EGDIR}/xdg/autostart/at-spi-dbus-bus.desktop
+
+.include "../../sysutils/dbus/buildlink3.mk"
+
+.else
+MESON_ARGS+=	-Datk_only=true
+.endif
+
+.include "../../mk/bsd.prefs.mk"
