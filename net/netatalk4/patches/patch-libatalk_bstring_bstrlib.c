997ba68aa5bce5a4a173fd2097dc28c8cf9d8501
hauke <hauke@pkgsrc.org>
2024-11-21 22:02:11 +0000

    Completely remove custom function prototype, like we used to, see <http://cvsweb.netbsd.org/bsdweb.cgi/pkgsrc/net/netatalk30/patches/Attic/patch-libatalk_bstring_bstrlib.c?rev=1.1;content-type=text%2Fplain;hideattic=0;only_with_tag=MAIN>

--- net/netatalk4/patches/patch-libatalk_bstring_bstrlib.c
+++ net/netatalk4/patches/patch-libatalk_bstring_bstrlib.c
@@ -1,18 +1,23 @@
-$NetBSD: patch-libatalk_bstring_bstrlib.c,v 1.1 2024/11/21 15:26:08 hauke Exp $
+$NetBSD: patch-libatalk_bstring_bstrlib.c,v 1.2 2024/11/21 22:02:11 hauke Exp $
 
-See https://github.com/mvp/uhubctl/issues/586
+When a compiler complains about a missing prototype for a libc
+function, just inserting one for what you think the interface might be
+is probably not a good idea.
 
-Maybe the re-definition should just be removed, given the vague
-reasoning
+See also <https://github.com/mvp/uhubctl/issues/586>
 
---- libatalk/bstring/bstrlib.c.orig	2024-11-08 16:22:48.888569855 +0000
+--- libatalk/bstring/bstrlib.c.orig	2024-11-15 06:52:12.000000000 +0000
 +++ libatalk/bstring/bstrlib.c
-@@ -2913,7 +2913,7 @@ struct genBstrList g;
+@@ -2913,12 +2913,6 @@ struct genBstrList g;
  #define START_VSNBUFF (256)
  #else
  
 -#if defined(__GNUC__) && !defined(__APPLE__)
-+#if defined(__GNUC__) && !defined(__APPLE__) && !defined(__NetBSD__)
- /* Something is making gcc complain about this prototype not being here, so
-    I've just gone ahead and put it in. */
- extern int vsnprintf (char *buf, size_t count, const char *format, va_list arg);
+-/* Something is making gcc complain about this prototype not being here, so
+-   I've just gone ahead and put it in. */
+-extern int vsnprintf (char *buf, size_t count, const char *format, va_list arg);
+-#endif
+-
+ #define exvsnprintf(r,b,n,f,a) {r = vsnprintf (b,n,f,a);}
+ #endif
+ #endif
