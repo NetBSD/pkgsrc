a4cd6c81a240fb8738c89a9bf4679f44a754b086
gdt <gdt@pkgsrc.org>
2024-11-22 14:20:02 +0000

    chat/py-xmpppy: Enable certificate validation
    Tested on NetBSD 10.  This is really addressing an upstream issue, but
    upstream does not appear likely to do so soon.

--- chat/py-xmpppy/patches/patch-xmpp_transports.py
+++ chat/py-xmpppy/patches/patch-xmpp_transports.py
@@ -1,31 +1,28 @@
-$NetBSD: patch-xmpp_transports.py,v 1.3 2024/11/22 13:45:36 gdt Exp $
+$NetBSD: patch-xmpp_transports.py,v 1.4 2024/11/22 14:20:02 gdt Exp $
 
 Adapt to ssl usage in python 3.12 and up.
 
 Taken from:
   https://github.com/xmpppy/xmpppy/pull/70/
 
-NB: This patch disables validation entirely, matching the previous
-behavior.  This is a bug and it is \todo to to enable validation.
+This patch asks python to load the system's configured trust anchors.
+Validation and hostname matching are in theory enabled by default.
+This is a behavior change from upstream, but it is a bugfix.
+See
   https://github.com/xmpppy/xmpppy/issues/71
 
-Comment for after validation is enabled:
-
-This patch enables check_hostname, and in theory that should result in
-certificate validation using the system's configured trust anchors,
-and also hostname matching.  Perhaps this is a behavior change from
-before, but if so it is a bugfix.
 
 --- xmpp/transports.py.orig	2022-04-10 18:25:44.000000000 +0000
 +++ xmpp/transports.py
-@@ -383,8 +383,13 @@ class TLS(PlugIn):
+@@ -383,8 +383,14 @@ class TLS(PlugIn):
      def _startSSL(self):
          """ Immidiatedly switch socket to TLS mode. Used internally."""
          """ Here we should switch pending_data to hint mode."""
 +        context=ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-+        # \todo Fix this after understanding validation a bit better
-+        context.check_hostname = False
-+        context.verify_mode = ssl.CERT_NONE
++        context.set_default_verify_paths()
++        # Uncomment if you need to work around trust anchor configuration.
++        #context.check_hostname = False
++        #context.verify_mode = ssl.CERT_NONE
 +
          tcpsock=self._owner.Connection
 -        tcpsock._sslObj    = ssl.wrap_socket(tcpsock._sock, None, None)
