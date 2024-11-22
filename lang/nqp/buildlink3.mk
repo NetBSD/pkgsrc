e4c4203d245b172179a8c35540b973b57ca75c7e
mef <mef@pkgsrc.org>
2024-11-22 03:48:50 +0000

    (lang/rakudo, lang/nqp) adapt to perl-5.40, sorry for hard coding

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.22 2024/10/14 01:47:33 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2024/11/22 03:48:50 mef Exp $
 
 BUILDLINK_TREE+=	nqp
 
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2024.09
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2024.09
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2024.10
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2024.10
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
 .include "../../devel/MoarVM/buildlink3.mk"
