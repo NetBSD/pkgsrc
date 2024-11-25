9d484b6b7eb414ac06aac3aae0d85631ad99b904
leot <leot@pkgsrc.org>
2024-11-25 11:59:23 +0000

    zbar: Update to 0.23.93
    Changes:
    0.23.93:
      * Set a better dpi resolution when parsing PDF files
      * Fix memory recycle bug of empty symbols
      * Fix compilation with python 3.11 and 3.12
      * CVE-2023-40889: Fix array out-of-bounds access
      * Stop ignoring non-binary entries that follow binary ones
      * Increase allocated buffer memory for symbols
      * barcodetest.py: fix error code print logic
      * convert: Crash fixing while using camera
      * Add some pod information for additional functions
      * perl skip more tests if DISPLAY not set and set prereqs in Makefile.PL
      * Fixes rt.cpan.org 122061 - test fails when DISPLAY not set
      * Update Barcode::ZBar
      * isaac: ensure proper order of parsing expression
      * Enforce x11 backend even on wayland
      * zbarimg: add the --polygon option
      * xml output: Add polygon containing code bar.
      * configure.ac: drop support for Qt4 and prepare for Qt6 support
      * win: fix compiling error in Visual studio
      * Enforce a coding style
      * configure.ac: fix some issues with gtk parameter
      * zbargtk: fix version check macros
      * zbar: Address some header issues
      * zbar, test: fix compilation issues with FreeBSD
      * zbar: Function stdcall declaration issue.
      * symbol: make it compatible with MSC
      * zbar: change the code to make it c90 standard compatible
      * test: fix decode test
    
    0.23.92:
      * Added a --enable-static option to make easier to distribute
        Windows binaries

--- graphics/zbar/patches/patch-zbar_window_x.c
+++ graphics/zbar/patches/patch-zbar_window_x.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-zbar_window_x.c,v 1.1 2021/03/04 10:34:24 leot Exp $
+$NetBSD: patch-zbar_window_x.c,v 1.2 2024/11/25 11:59:23 leot Exp $
 
 Avoid possible ctype(3) undefined behaviours.
 
---- zbar/window/x.c.orig	2021-02-13 09:39:55.000000000 +0000
+--- zbar/window/x.c.orig	2021-03-14 16:28:01.000000000 +0000
 +++ zbar/window/x.c
-@@ -288,7 +288,7 @@ int _zbar_window_draw_text (zbar_window_
+@@ -274,7 +274,7 @@ int _zbar_window_draw_text(zbar_window_t
      XSetForeground(w->display, xs->gc, xs->colors[rgb]);
  
      int n = 0;
--    while(n < 32 && text[n] && isprint(text[n]))
-+    while(n < 32 && text[n] && isprint((unsigned char)text[n]))
-         n++;
+-    while (n < 32 && text[n] && isprint(text[n]))
++    while (n < 32 && text[n] && isprint((unsigned char)text[n]))
+ 	n++;
  
      int width = XTextWidth(xs->font, text, n);
