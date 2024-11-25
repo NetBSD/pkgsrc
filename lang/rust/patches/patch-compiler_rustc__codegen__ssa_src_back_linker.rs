7164489b61aeaf5469455ec1ee6cde0a4cce83f9
tnn <tnn@pkgsrc.org>
2024-11-25 14:37:35 +0000

    rust: fix mismerged patch (Linux)

--- lang/rust/patches/patch-compiler_rustc__codegen__ssa_src_back_linker.rs
+++ lang/rust/patches/patch-compiler_rustc__codegen__ssa_src_back_linker.rs
@@ -1,9 +1,9 @@
-$NetBSD: patch-compiler_rustc__codegen__ssa_src_back_linker.rs,v 1.18 2024/11/24 16:13:43 he Exp $
+$NetBSD: patch-compiler_rustc__codegen__ssa_src_back_linker.rs,v 1.19 2024/11/25 14:37:35 tnn Exp $
 
 Do not use @rpath on Darwin.
 Find external libunwind on Linux.
 
---- compiler/rustc_codegen_ssa/src/back/linker.rs.orig	2006-07-24 01:21:28.000000000 +0000
+--- compiler/rustc_codegen_ssa/src/back/linker.rs.orig	2024-09-04 15:07:06.000000000 +0000
 +++ compiler/rustc_codegen_ssa/src/back/linker.rs
 @@ -402,7 +402,7 @@ impl<'a> GccLinker<'a> {
              // principled solution at some point to force the compiler to pass
@@ -14,14 +14,14 @@ Find external libunwind on Linux.
                  rpath.push(out_filename.file_name().unwrap());
                  self.link_arg("-install_name").link_arg(rpath);
              }
-@@ -880,6 +880,10 @@ impl<'a> Linker for MsvcLinker<'a> {
+@@ -528,6 +528,10 @@ impl<'a> Linker for GccLinker<'a> {
+     }
  
-     fn link_dylib_by_name(&mut self, name: &str, verbatim: bool, _as_needed: bool) {
-         self.link_arg(format!("{}{}", name, if verbatim { "" } else { ".lib" }));
+     fn link_dylib_by_name(&mut self, name: &str, verbatim: bool, as_needed: bool) {
 +        if self.sess.target.os.contains("linux") && name == "unwind" {
 +            self.link_arg("-rpath,@PREFIX@/lib");
 +            self.link_arg("-L@PREFIX@/lib");
 +        }
-     }
- 
-     fn link_dylib_by_path(&mut self, path: &Path, _as_needed: bool) {
+         if self.sess.target.os == "illumos" && name == "c" {
+             // libc will be added via late_link_args on illumos so that it will
+             // appear last in the library search order.
