67f51a15429d18c176318bca54b62858de4a4a68
wiz <wiz@pkgsrc.org>
2024-11-25 07:12:11 +0000

    lv2: fix doc option
    remove non-working tests option
    
    Remove references to non-existent files.
    
    First part fixes PR 58853 by Robert Whitlock.

--- audio/lv2/options.mk
+++ audio/lv2/options.mk
@@ -1,13 +1,12 @@
-# $NetBSD: options.mk,v 1.5 2024/01/14 09:44:58 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2024/11/25 07:12:11 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lv2
-PKG_SUPPORTED_OPTIONS=	doc tests
+PKG_SUPPORTED_OPTIONS=	doc
+PKG_SUGGESTED_OPTIONS=	doc
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-PYTHON_VERSIONS_INCOMPATIBLE=	27
-
 TOOL_DEPENDS+=	asciidoc-[0-9]*:../../textproc/asciidoc
 TOOL_DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
 TOOL_DEPENDS+=	${PYPKGPREFIX}-lxml-[0-9]*:../../textproc/py-lxml
@@ -23,13 +22,7 @@ post-install: doxygen-plist
 doxygen-plist:
 	cd ${DESTDIR}${PREFIX} && \
 	${FIND} share/doc/lv2 -type f -print > ${WRKDIR}/PLIST.STATIC
+.include "../../lang/python/tool.mk"
 .else
 MESON_ARGS+=	-Ddocs=disabled
-.endif
-
-.if !empty(PKG_OPTIONS:Mtests)
-MESON_ARGS+=	-Dtests=enabled
-USE_LANGUAGES+=	c c++
-.else
-MESON_ARGS+=	-Dtests=disabled
 .endif
