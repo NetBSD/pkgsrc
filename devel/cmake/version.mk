0c600ba7a57d9f6af2ec48e046f3659e14eb1362
adam <adam@pkgsrc.org>
2024-11-27 07:02:35 +0000

    cmake cmake-gui: updated to 3.31.1
    3.31.1
    This version made no changes to documented features or interfaces. Some
    implementation updates were made to support ecosystem changes and/or fix
    regressions.

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.68 2024/11/10 19:58:30 adam Exp $
+# $NetBSD: version.mk,v 1.69 2024/11/27 07:02:35 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.0
+CMAKE_VERSION=	3.31.1
 CMAKE_API=	${CMAKE_VERSION:R}
