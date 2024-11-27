7b55c649e58e1bcabf1decfc15e6235d0b46315b
pin <pin@pkgsrc.org>
2024-11-27 10:28:36 +0000

    meta-pkgs/lxqt: revbump for lxqt-panel
    While here:
     * Re-add translations for those who want it.
       Now that we have x11/qt6-qttranslations.
     * Re-add obconf-qt for those using openbox.
       Now that it has been ported to Qt6.

--- meta-pkgs/lxqt/options.mk
+++ meta-pkgs/lxqt/options.mk
@@ -1,16 +1,15 @@
-# $NetBSD: options.mk,v 1.5 2021/11/19 11:49:53 pin Exp $
+# $NetBSD: options.mk,v 1.6 2024/11/27 10:28:36 pin Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.lxqt
 PKG_OPTIONS_OPTIONAL_GROUPS=	wm
 PKG_OPTIONS_GROUP.wm=		openbox xfce4-wm
 PKG_SUGGESTED_OPTIONS=		openbox
-# wip/kwin and wm/xfce4-wm are known to work alternate options.
-# See https://github.com/lxqt/lxqt/wiki/ConfigWindowManagers
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mopenbox)
 DEPENDS+=	openbox>=3.6.1:../../wm/openbox
+DEPENDS+=	obconf-qt>=0.16.5:../../wm/obconf-qt
 .endif
 
 .if !empty(PKG_OPTIONS:Mxfce4-wm)
