b1d47f673215d7c2054e56c4777bb4423a4bff52
ryoon <ryoon@pkgsrc.org>
2024-11-28 20:09:13 +0000

    cross/mingw-w64-x86_64-headers-ucrt: Update to 12.0.0
    Changelog:
    v12.0.0: 2024-05-29
    
    Important: UCRT is now the default CRT runtime instead of MSVCRT, check the mingw-w64-doc/howto-build/ucrt-vs-msvcrt.txt document for details. Both header set and CRT must be configured and built with the same settings consistently for proper functionality. Switching runtimes requires all libraries to be rebuilt, including GCC.
    
    Notable changes:
    
        C++ overloads for _strdate_s and _strtime_s, _makepath_s, wcsncat_s.
        gdtoa updated against netlib.org, up to January 2023.
        genlib tool removed in favor of llvm-dlltool, created by the same author.
        Make it possible to use winpthreads with MSVC.
        Updated wine header imports.
        Many other new win32 APIs.
        UCRT configurations now primarily use (faster) math functions from UCRT, rather than libmingwex

--- cross/mingw-w64-x86_64-headers/buildlink3.mk
+++ cross/mingw-w64-x86_64-headers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/06/08 10:53:28 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/11/28 20:09:13 ryoon Exp $
 
-BUILDLINK_TREE+=	mingw-w64-x86_64-headers
+BUILDLINK_TREE+=	mingw-w64-x86_64-headers-ucrt
 
-.if !defined(MINGW_W64_X86_64_HEADERS_BUILDLINK3_MK)
-MINGW_W64_X86_64_HEADERS_BUILDLINK3_MK:=
+.if !defined(MINGW_W64_X86_64_HEADERS_UCRT_BUILDLINK3_MK)
+MINGW_W64_X86_64_HEADERS_UCRT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mingw-w64-x86_64-headers+=	mingw-w64-x86_64-headers>=5.0.4
-BUILDLINK_PKGSRCDIR.mingw-w64-x86_64-headers?=		../../cross/mingw-w64-x86_64-headers
-.endif	# MINGW_W64_X86_64_HEADERS_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.mingw-w64-x86_64-headers-ucrt+=	mingw-w64-x86_64-headers-ucrt>=12.0.0
+BUILDLINK_PKGSRCDIR.mingw-w64-x86_64-headers-ucrt?=	../../cross/mingw-w64-x86_64-headers
+.endif	# MINGW_W64_X86_64_HEADERS_UCRT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mingw-w64-x86_64-headers
+BUILDLINK_TREE+=	-mingw-w64-x86_64-headers-ucrt
