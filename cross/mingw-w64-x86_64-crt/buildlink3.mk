9016f9d173a7c10cea2b478f0957589edfa75dda
ryoon <ryoon@pkgsrc.org>
2024-11-28 20:01:37 +0000

    cross/mingw-w64-x86_64-crt-ucrt: Update to 12.0.0
    Changelog:
    v12.0.0: 2024-05-29
    
    Important: UCRT is now the default CRT runtime instead of MSVCRT, check the mingw-w64-doc/howto-build/ucrt-vs-msvcrt.txt document for details. Both header set and CRT must be configured and built with the same settings consistently for proper functionality. Switching runtimes requires all libraries to be rebuilt, including GCC.
    
    Notable changes:
    
        C++ overloads for _strdate_s and _strtime_s, _makepath_s, wcsncat_s.
        gdtoa updated against netlib.org, up to January 2023.
        genlib tool removed in favor of llvm-dlltool, created by the same author.
        Make it possible to use winpthreads with MSVC.
        Updated wine header imports.
        Many other new win32 APIs.
        UCRT configurations now primarily use (faster) math functions from UCRT, rather than libmingwex

--- cross/mingw-w64-x86_64-crt/buildlink3.mk
+++ cross/mingw-w64-x86_64-crt/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/06/28 11:31:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/11/28 20:01:37 ryoon Exp $
 
-BUILDLINK_TREE+=	mingw-w64-x86_64-crt
+BUILDLINK_TREE+=	mingw-w64-x86_64-crt-ucrt
 
-.if !defined(MINGW_W64_X86_64_CRT_BUILDLINK3_MK)
-MINGW_W64_X86_64_CRT_BUILDLINK3_MK:=
+.if !defined(MINGW_W64_X86_64_CRT_UCRT_BUILDLINK3_MK)
+MINGW_W64_X86_64_CRT_UCRT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mingw-w64-x86_64-crt+=	mingw-w64-x86_64-crt>=5.0.4
-BUILDLINK_ABI_DEPENDS.mingw-w64-x86_64-crt?=	mingw-w64-x86_64-crt>=9.0.0nb1
-BUILDLINK_PKGSRCDIR.mingw-w64-x86_64-crt?=	../../cross/mingw-w64-x86_64-crt
+BUILDLINK_API_DEPENDS.mingw-w64-x86_64-crt-ucrt+=	mingw-w64-x86_64-crt-ucrt>=12.0.0
+BUILDLINK_PKGSRCDIR.mingw-w64-x86_64-crt-ucrt?=	../../cross/mingw-w64-x86_64-crt
 
-.include "../../cross/mingw-w64-x86_64-headers/buildlink3.mk"
-.endif	# MINGW_W64_X86_64_CRT_BUILDLINK3_MK
+.endif	# MINGW_W64_X86_64_CRT_UCRT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mingw-w64-x86_64-crt
+BUILDLINK_TREE+=	-mingw-w64-x86_64-crt-ucrt
