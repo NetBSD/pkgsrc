db60724ffddc3d6ae35c00beb07d198cb0402570
ryoon <ryoon@pkgsrc.org>
2024-11-28 20:02:52 +0000

    cross/mingw-w64-i686-crt-ucrt: Update to 12.0.0
    Changelog:
    v12.0.0: 2024-05-29
    
    Important: UCRT is now the default CRT runtime instead of MSVCRT, check the mingw-w64-doc/howto-build/ucrt-vs-msvcrt.txt document for details. Both header set and CRT must be configured and built with the same settings consistently for proper functionality. Switching runtimes requires all libraries to be rebuilt, including GCC.
    
    Notable changes:
    
        C++ overloads for _strdate_s and _strtime_s, _makepath_s, wcsncat_s.
        gdtoa updated against netlib.org, up to January 2023.
        genlib tool removed in favor of llvm-dlltool, created by the same author.
        Make it possible to use winpthreads with MSVC.
        Updated wine header imports.
        Many other new win32 APIs.
        UCRT configurations now primarily use (faster) math functions from UCRT, rather than libmingwex

--- cross/mingw-w64-i686-crt/buildlink3.mk
+++ cross/mingw-w64-i686-crt/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/06/07 19:32:13 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/11/28 20:02:52 ryoon Exp $
 
-BUILDLINK_TREE+=	mingw-w64-i686-crt
+BUILDLINK_TREE+=	mingw-w64-i686-crt-ucrt
 
-.if !defined(MINGW_W64_I686_CRT_BUILDLINK3_MK)
-MINGW_W64_I686_CRT_BUILDLINK3_MK:=
+.if !defined(MINGW_W64_I686_CRT_UCRT_BUILDLINK3_MK)
+MINGW_W64_I686_CRT_UCRT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mingw-w64-i686-crt+=	mingw-w64-i686-crt>=5.0.4
-BUILDLINK_PKGSRCDIR.mingw-w64-i686-crt?=	../../cross/mingw-w64-i686-crt
+BUILDLINK_API_DEPENDS.mingw-w64-i686-crt-ucrt+=	mingw-w64-i686-crt-ucrt>=12.0.0
+BUILDLINK_PKGSRCDIR.mingw-w64-i686-crt-ucrt?=	../../cross/mingw-w64-i686-crt
 
-.include "../../cross/mingw-w64-i686-headers/buildlink3.mk"
-.endif	# MINGW_W64_I686_CRT_BUILDLINK3_MK
+.endif	# MINGW_W64_I686_CRT_UCRT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mingw-w64-i686-crt
+BUILDLINK_TREE+=	-mingw-w64-i686-crt-ucrt
