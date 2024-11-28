4939a2a8169b15d05bb9af1cc251f36531ca6af4
ryoon <ryoon@pkgsrc.org>
2024-11-28 20:31:41 +0000

    cross/mingw-w64-x86_64-winpthreads-ucrt: Update to 12.0.0
    Changelog:
    v12.0.0: 2024-05-29
    
    Important: UCRT is now the default CRT runtime instead of MSVCRT, check the mingw-w64-doc/howto-build/ucrt-vs-msvcrt.txt document for details. Both header set and CRT must be configured and built with the same settings consistently for proper functionality. Switching runtimes requires all libraries to be rebuilt, including GCC.
    
    Notable changes:
    
        C++ overloads for _strdate_s and _strtime_s, _makepath_s, wcsncat_s.
        gdtoa updated against netlib.org, up to January 2023.
        genlib tool removed in favor of llvm-dlltool, created by the same author.
        Make it possible to use winpthreads with MSVC.
        Updated wine header imports.
        Many other new win32 APIs.
        UCRT configurations now primarily use (faster) math functions from UCRT, rather than libmingwex

--- cross/mingw-w64-x86_64-winpthreads/buildlink3.mk
+++ cross/mingw-w64-x86_64-winpthreads/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/06/28 11:31:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/11/28 20:31:41 ryoon Exp $
 
-BUILDLINK_TREE+=	mingw-w64-x86_64-winpthreads
+BUILDLINK_TREE+=	mingw-w64-x86_64-winpthreads-ucrt
 
-.if !defined(MINGW_W64_X86_64_WINPTHREADS_BUILDLINK3_MK)
-MINGW_W64_X86_64_WINPTHREADS_BUILDLINK3_MK:=
+.if !defined(MINGW_W64_X86_64_WINPTHREADS_UCRT_BUILDLINK3_MK)
+MINGW_W64_X86_64_WINPTHREADS_UCRT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mingw-w64-x86_64-winpthreads+=	mingw-w64-x86_64-winpthreads>=6.0.0
-BUILDLINK_ABI_DEPENDS.mingw-w64-x86_64-winpthreads?=	mingw-w64-x86_64-winpthreads>=9.0.0nb1
-BUILDLINK_PKGSRCDIR.mingw-w64-x86_64-winpthreads?=	../../cross/mingw-w64-x86_64-winpthreads
+BUILDLINK_API_DEPENDS.mingw-w64-x86_64-winpthreads-ucrt+=	mingw-w64-x86_64-winpthreads-ucrt>=12.0.0
+BUILDLINK_PKGSRCDIR.mingw-w64-x86_64-winpthreads-ucrt?=	../../cross/mingw-w64-x86_64-winpthreads
 
-.endif	# MINGW_W64_X86_64_WINPTHREADS_BUILDLINK3_MK
+.endif	# MINGW_W64_X86_64_WINPTHREADS_UCRT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mingw-w64-x86_64-winpthreads
+BUILDLINK_TREE+=	-mingw-w64-x86_64-winpthreads-ucrt
