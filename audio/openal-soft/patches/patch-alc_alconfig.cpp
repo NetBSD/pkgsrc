b5307546037192952443ec4ba563e528f8140929
ryoon <ryoon@pkgsrc.org>
2024-11-29 03:47:45 +0000

    audio/openal-soft: Update to 1.24.1
    Changelog:
    openal-soft-1.24.1:
    
        Fixed compilation on PowerPC.
    
        Fixed compilation on some targets that lack lock-free 64-bit atomics.
    
        Fixed a crash when parsing certain option values.
    
        Fixed applying noexcept in the public headers with MSVC.
    
        Fixed building for UWP with vcpkg.
    
        Improved compatibility when compiling as C++20 or later.
    
        Integrated fmtlib for some examples and utilities.

--- audio/openal-soft/patches/patch-alc_alconfig.cpp
+++ audio/openal-soft/patches/patch-alc_alconfig.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-alc_alconfig.cpp,v 1.2 2024/11/20 09:33:00 adam Exp $
+$NetBSD: patch-alc_alconfig.cpp,v 1.3 2024/11/29 03:47:45 ryoon Exp $
 
 Allow the Makefile to specify the default config file path.
 
---- alc/alconfig.cpp.orig	2024-11-16 12:20:51.000000000 +0000
+--- alc/alconfig.cpp.orig	2024-11-28 03:21:16.000000000 +0000
 +++ alc/alconfig.cpp
-@@ -383,7 +383,7 @@ void ReadALConfig()
+@@ -384,7 +384,7 @@ void ReadALConfig()
  void ReadALConfig()
  {
      namespace fs = std::filesystem;
 -    fs::path path{"/etc/openal/alsoft.conf"};
 +    fs::path path{SYS_CONF_DIR "/alsoft.conf"};
  
-     TRACE("Loading config %s...\n", path.u8string().c_str());
+     TRACE("Loading config %s...\n", reinterpret_cast<const char*>(path.u8string().c_str()));
      if(std::ifstream f{path}; f.is_open())
