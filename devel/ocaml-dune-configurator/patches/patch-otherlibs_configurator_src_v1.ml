ac532e13f2e9d27204f7f21ffca7867764d16594
dholland <dholland@pkgsrc.org>
2024-11-30 01:05:00 +0000

    devel/ocaml-dune-configurator: fix pkg-config search.
    Don't try to search $PATH for something that's already an absolute
    path. Fixes the build of lablgtk3.
    
    PKGREVISION -> 1

---
+++ devel/ocaml-dune-configurator/patches/patch-otherlibs_configurator_src_v1.ml
@@ -0,0 +1,18 @@
+$NetBSD: patch-otherlibs_configurator_src_v1.ml,v 1.1 2024/11/30 01:05:01 dholland Exp $
+
+Don't try to "which" absolute paths.
+
+--- otherlibs/configurator/src/v1.ml~	2023-10-09 11:08:29.000000000 +0000
++++ otherlibs/configurator/src/v1.ml
+@@ -635,7 +635,10 @@ end
+ 
+ let which t prog =
+   logf t "which: %s" prog;
+-  let x = Find_in_path.which prog in
++  let x =
++     if String.length prog > 0 && String.get prog 0 = '/' then Some prog
++     else Find_in_path.which prog
++  in
+   logf
+     t
+     "-> %s"
