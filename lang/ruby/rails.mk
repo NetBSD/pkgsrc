1a19100af62917fd13f23f6ca96c10f86a6c3e52
taca <taca@pkgsrc.org>
2024-12-01 15:47:45 +0000

    www/rails71: update to 7.1.5
    Ruby on Rails 7.1.5 (2024-10-31)
    
    Active Model
    
    * Fix regression in alias_attribute to work with user defined methods.
    
       alias_attribute would wrongly assume the attribute accessor was generated
       by Active Model.
    
    	class Person
    	  include ActiveModel::AttributeMethods
    
    	  define_attribute_methods :name
    	  attr_accessor :name
    
    	  alias_attribute :full_name, :name
    	end
    
        person.full_name # => NoMethodError: undefined method `attribute' for an
        instance of Person
    
        Jean Boussier
    
    Active Record
    
    * Fix marshalling of unsaved associated records in 7.1 format.
    
      The 7.1 format would only marshal associated records if the association
      was loaded.  But associations that would only contain unsaved records
      would be skipped.
    
      Jean Boussier
    
    * Fix an issue where .left_outer_joins used with multiple associations that
      have the same child association but different parents does not join all
      parents.
    
      Previously, using .left_outer_joins with the same child association would
      only join one of the parents.
    
      Now it will correctly join both parents.
    
        Fixes #41498.
    
        Garrett Blehm
    
    * Ensure ActiveRecord::Encryption.config is always ready before access.
    
      Previously, ActiveRecord::Encryption configuration was deferred until
      ActiveRecord::Base was loaded. Therefore, accessing
      ActiveRecord::Encryption.config properties before ActiveRecord::Base was
      loaded would give incorrect results.
    
      ActiveRecord::Encryption now has its own loading hook so that its
      configuration is set as soon as needed.
    
      When ActiveRecord::Base is loaded, even lazily, it in turn triggers the
      loading of ActiveRecord::Encryption, thus preserving the original behavior
      of having its config ready before any use of ActiveRecord::Base.
    
      Maxime Réty
    
    * Add TimeZoneConverter#== method, so objects will be properly compared by
      their type, scale, limit & precision.
    
      Address #52699.
    
      Ruy Rocha

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.171 2024/10/27 14:35:25 taca Exp $
+# $NetBSD: rails.mk,v 1.172 2024/12/01 15:47:45 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -49,7 +49,7 @@ _RUBY_RAILS_MK=	# defined
 #
 RUBY_RAILS61_VERSION?=	6.1.7.10
 RUBY_RAILS70_VERSION?=	7.0.8.6
-RUBY_RAILS71_VERSION?=	7.1.4.2
+RUBY_RAILS71_VERSION?=	7.1.5
 
 RUBY_RAILS_ACCEPTED?=	# empty
 RUBY_RAILS_DEFAULT?=	61
