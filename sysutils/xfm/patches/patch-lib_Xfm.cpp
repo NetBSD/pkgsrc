d40ecb7b6353b2e6b34cc5a2445ad2adcd96c137
nia <nia@pkgsrc.org>
2024-12-02 08:02:33 +0000

    xfm: More little fixes from RVP
    PR pkg/58816

--- sysutils/xfm/patches/patch-lib_Xfm.cpp
+++ sysutils/xfm/patches/patch-lib_Xfm.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-lib_Xfm.cpp,v 1.1 2024/11/07 15:29:32 nia Exp $
+$NetBSD: patch-lib_Xfm.cpp,v 1.2 2024/12/02 08:02:33 nia Exp $
 
 Add a <space> so that HIST_TRANSLATION() is recognized as a macro
 rather than as a continuation of the previous line.
@@ -29,6 +29,24 @@ https://gnats.netbsd.org/58816
    <Btn1Up>(2)         : fileRefresh()\n
  Xfm*file window*viewport.baseTranslations:#override\n\
    <Enter>: trackCursor()\n <Leave>: trackCursor()\n
+@@ -359,7 +359,7 @@ Ctrl	<Key>f			: notify(*view*filter)\n\
+ Ctrl	<Key>h			: notify("*view*hide folders")\n\
+ Ctrl	<Key>m			: notify("*view*mix folders/files")\n\
+ Ctrl	<Key>u			: notify("*view*show hidden files")\n\
+-LOG_TRANSLATION \
++ LOG_TRANSLATION \
+ 	<Key>n			: notify(*file*new)\n\
+  	<Key>m			: notify(*file*move)\n\
+  	<Key>c			: notify(*file*copy)\n\
+@@ -400,7 +400,7 @@ Ctrl	<Key>f			: notify(*view*filter)\n\
+ Ctrl	<Key>h			: notify("*view*hide folders")\n\
+ Ctrl	<Key>m			: notify("*view*mix folders/files")\n\
+ Ctrl	<Key>u			: notify("*view*show hidden files")\n\
+-LOG_TRANSLATION \
++ LOG_TRANSLATION \
+ 	<Key>n			: notify(*file*new)\n\
+  	<Key>m			: notify(*file*move)\n\
+  	<Key>c			: notify(*file*copy)\n\
 @@ -416,7 +416,7 @@ LOG_TRANSLATION \
   	<Key>q			: notify(*file*quit)\n
  #else
