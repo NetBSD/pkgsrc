519f94b7e7558a4970fce3908b247de6646b039d
adam <adam@pkgsrc.org>
2024-12-05 07:51:12 +0000

    python311 py311-html-docs: updated to 3.11.11
    Python 3.11.11
    
    Tools/Demos
    
    gh-123418: Update GitHub CI workflows to use OpenSSL 3.0.15 and multissltests to use 3.0.15, 3.1.7, and 3.2.3.
    
    Tests
    
    gh-125041: Re-enable skipped tests for zlib on the s390x architecture: only skip checks of the compressed bytes, which can be different between zlib’s software implementation and the hardware-accelerated implementation.
    
    Security
    
    gh-126623: Upgrade libexpat to 2.6.4
    gh-122792: Changed IPv4-mapped ipaddress.IPv6Address to consistently use the mapped IPv4 address value for deciding properties. Properties which have their behavior fixed are is_multicast, is_reserved, is_link_local, is_global, and is_unspecified.
    
    Library
    
    gh-124651: Properly quote template strings in venv activation scripts.

--- lang/python311/dist.mk
+++ lang/python311/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.11 2024/09/09 15:50:30 adam Exp $
+# $NetBSD: dist.mk,v 1.12 2024/12/05 07:51:12 adam Exp $
 
-PY_DISTVERSION=	3.11.10
+PY_DISTVERSION=	3.11.11
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python311/distinfo
