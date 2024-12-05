12471ba5161a5192c25bc24faf477b071582dc17
jperkin <jperkin@pkgsrc.org>
2024-12-05 16:14:06 +0000

    libfetch: Ensure -lsocket is always added on SunOS.

--- net/libfetch/buildlink3.mk
+++ net/libfetch/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2024/12/05 15:39:15 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2024/12/05 16:14:06 jperkin Exp $
 
 BUILDLINK_TREE+=	libfetch
 
@@ -14,11 +14,16 @@ BUILDLINK_PKGSRCDIR.libfetch?=		../../net/libfetch
 pkgbase := libfetch
 .include "../../mk/pkg-build-options.mk"
 
+.if ${OPSYS} == "SunOS"
+BUILDLINK_LIBS.libfetch+=	-lsocket
+.endif
+
 .if ${PKG_BUILD_OPTIONS.libfetch:Mopenssl}
 .  include "../../security/openssl/buildlink3.mk"
 BUILDLINK_DEPMETHOD.openssl=	full
 BUILDLINK_LIBS.libfetch+=	-lcrypto -lssl
 .endif
+
 .endif # LIBFETCH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfetch
