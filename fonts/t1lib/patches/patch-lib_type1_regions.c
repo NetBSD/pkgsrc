8f9e2a334b8f68aa814bbd67d79c032c99072e9b
markd <markd@pkgsrc.org>
2024-12-05 08:21:53 +0000

    t1lib: fix build with gcc14

--- fonts/t1lib/patches/patch-lib_type1_regions.c
+++ fonts/t1lib/patches/patch-lib_type1_regions.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-lib_type1_regions.c,v 1.1 2022/06/08 19:00:13 chuck Exp $
+$NetBSD: patch-lib_type1_regions.c,v 1.2 2024/12/05 08:21:53 markd Exp $
 
 Fixed incorrect calculation of ceiling that results in t1lib LONGCOPY
 writing past the end of a malloc buffer.   Corrects crash on OSX
@@ -16,3 +16,11 @@ and resolves complaints from the address sanitizer.
   
         IfTrace1((RegionDebug),"result=%p\n", r);
         return(r);
+@@ -604,6 +605,7 @@ void ChangeDirection(type, R, x, y, dy,
+        register struct region *R;  /* region in which we are changing direction */
+        fractpel x,y;         /* current beginning x,y                        */
+        fractpel dy;          /* direction and magnitude of change in y       */
++       fractpel x2,y2;
+ {
+        register fractpel ymin,ymax;  /* minimum and maximum Y since last call */
+        register fractpel x_at_ymin,x_at_ymax;  /* their respective X's       */
