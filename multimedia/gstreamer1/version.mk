3f54ea2fd61f0d43cc26b89860ceb9088aa7a4be
adam <adam@pkgsrc.org>
2024-12-05 09:50:19 +0000

    gstreamer1: updated to 1.24.10
    GStreamer 1.24.10 stable bug fix release
    
    More than 40 security fixes across a wide range of elements following an audit by the GitHub Security Lab, including the MP4, Matroska, Ogg and WAV demuxers, subtitle parsers, image decoders, audio decoders and the id3v2 tag parser
    avviddec: Fix regression that could trigger assertions about width/height mismatches
    appsink and appsrc fixes
    closed caption handling fixes
    decodebin3 and urisourcebin fixes
    glupload: dmabuf: Fix emulated tiled import
    level: fix LevelMeta values outside of the stated range
    mpegtsmux, flvmux: fix potential busy looping with high cpu usage in live mode
    pipeline dot file graph generation improvements
    qt(6): fix criticals with multiple qml(6)gl{src,sink}
    rtspsrc: Optionally timestamp RTP packets with their receive times in TCP/HTTP mode to enable clock drift handling
    splitmuxsrc: reduce number of file descriptors used
    systemclock: locking order fixes
    v4l2: fix possible v4l2videodec deadlock on shutdown; 8-bit bayer format fixes
    x265: Fix build with libx265 version >= 4.1 after masteringDisplayColorVolume API change
    macOS: fix rendering artifacts in retina displays, plus ptp clock fixes
    cargo: Default to thin lto for the release profile (for faster builds with lower memory requirements)
    Various bug fixes, build fixes, memory leak fixes, and other stability and reliability improvements
    Translation updates

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,10 +1,11 @@
-# $NetBSD: version.mk,v 1.19 2024/11/05 20:58:36 adam Exp $
+# $NetBSD: version.mk,v 1.20 2024/12/05 09:50:19 adam Exp $
 
-GST_VERSION=	1.24.9
-# if ${PATCH} is < 10
+GST_VERSION=	1.24.10
+.if ${GST_VERSION:E} < 10
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
-#else
-#SHLIBVER=	${GST_VERSION:S/1.//:S/.//}
+.else
+SHLIBVER=	${GST_VERSION:S/1.//:S/.//}
+.endif
 PLIST_SUBST+=	SHLIBVER=${SHLIBVER}
 
 MAINTAINER?=	pkgsrc-users@NetBSD.org
