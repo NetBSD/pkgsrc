5e368c525e4e2118c9be7ca207ce99392b0f6fa7
vins <vins@pkgsrc.org>
2024-12-05 09:08:29 +0000

    mail/opensmtpd-filter-spfgreylist: initial import
    This filter implements greylisting, allowing OpenSMTPD to temporarily
    reject sessions of clients it has not seen before. Unlike many
    implementations, this one is SPF-aware so it will properly handle
    greylisting for domains doing relaying through multiple MX, as long as
    they publish a valid SPF record.

---
+++ mail/opensmtpd-filter-spfgreylist/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD: go-modules.mk,v 1.1 2024/12/05 09:08:30 vins Exp $
+
+GO_MODULE_FILES+=	blitiri.com.ar/go/spf/@v/v1.5.1.mod
+GO_MODULE_FILES+=	blitiri.com.ar/go/spf/@v/v1.5.1.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.1.mod
