cb5516e34841679632fc72009f78e98ccc098736
pin <pin@pkgsrc.org>
2024-12-06 08:15:48 +0000

    devel/bacon: update to 3.5.0
        support for biome with analyzer = "biome"
        support for ruff with analyzer = "python_ruff"
        read bacon.toml in workspace/.config and package/.config - Fix #268
        read workspace.metadata.bacon and package.metadata.bacon config elements in Cargo.toml files - Fix #241
        fix locations export when launching bacon inside a rust workspace but with a non cargo tool

--- devel/bacon/cargo-depends.mk
+++ devel/bacon/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.16 2024/11/30 20:57:25 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.17 2024/12/06 08:15:48 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
 CARGO_CRATE_DEPENDS+=	ahash-0.8.11
