e853edc86dcd8dfcdf31932a4e94fad2edd85345
ryoon <ryoon@pkgsrc.org>
2024-12-09 13:54:17 +0000

    emulators/qemu: Fix NVMM for UEFI (OVMF)
    * From DragonFly BSD's Redmine's issue 3110 to fix memory error
      when UEFI boot.
    * Bump PKGREVISION.

--- emulators/qemu/patches/patch-target_i386_nvmm_nvmm-all.c
+++ emulators/qemu/patches/patch-target_i386_nvmm_nvmm-all.c
@@ -1,10 +1,23 @@
-$NetBSD: patch-target_i386_nvmm_nvmm-all.c,v 1.7 2024/07/10 06:52:48 triaxx Exp $
+$NetBSD: patch-target_i386_nvmm_nvmm-all.c,v 1.8 2024/12/09 13:54:17 ryoon Exp $
 
 Permit using older nvmm version.
 
---- target/i386/nvmm/nvmm-all.c.orig	2024-06-10 18:35:25.000000000 +0000
+--- target/i386/nvmm/nvmm-all.c.orig	2024-11-20 22:48:05.000000000 +0000
 +++ target/i386/nvmm/nvmm-all.c
-@@ -1169,14 +1169,10 @@ nvmm_accel_init(MachineState *ms)
+@@ -1057,7 +1057,11 @@ nvmm_process_section(MemoryRegionSection
+     unsigned int delta;
+     uintptr_t hva;
+ 
+-    if (!memory_region_is_ram(mr)) {
++    /*
++     * Don't exclude ROMD memory; for example, it's used to map UEFI firmware
++     * (if=pflash) and should be mapped for guest.
++     */
++    if (!memory_region_is_ram(mr) && !memory_region_is_romd(mr)) {
+         return;
+     }
+ 
+@@ -1170,14 +1174,10 @@ nvmm_accel_init(MachineState *ms)
          error_report("NVMM: Unable to fetch capability, error=%d", errno);
          return -err;
      }
