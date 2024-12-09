7a052fdabd266b468b5b57810e3da3453b4e9c14
adam <adam@pkgsrc.org>
2024-12-09 11:37:31 +0000

    appstream: updated to 1.0.4
    Version 1.0.4
    
    Features:
     * bundle: Add AS_BUNDLE_KIND_SYSUPDATE
     * desktop-style-ids: Add dark support for Plasma
     * desktop-style-ids: add pantheon:dark
     * qt: Expose branding color
     * sysinfo: Add memory size detection support for Illumos and Solaris
     * sysinfo: Add memory size detection support for GNU/Hurd
     * validator: Don't fail validation if a snapshot release is missing a time
    
    Specification:
     * docs: Note that support for some legacy path was dropped in 1.0
     * docs: Update fd.o spec URLs
     * docs: Better explain desktop applications
     * Update ESRA website URL
    
    Bugfixes:
     * validator: Make sure we read an initialised variable
     * qt: Specify utc as the dates' timezone
     * tests: Ignore some tests on Windows
     * sysinfo: make uname() usage POSIX compliant
     * Only emit legacy compat tag developer_name for catalog data
     * compose: Allow file discovery even in symlinked directories
     * Avoid a race condition when loading GResource data
     * validator: Mention length limit in summary length warning message
     * utils: Explicitly set permissions on installed metadata files
     * as-vercmp: Fix gtkdoc to not start with '>'
    
    Miscellaneous:
     * desktop-style-ids: Remove Endless
     * ci: Fix FreeBSD build (update `py-gi-docgen`)
     * ci: Update to Ubuntu 24.04 for Debian Testing builds
     * utils: Assume 64px as size when installing an icon tarball of unknown size

--- devel/appstream/patches/patch-src_as-system-info.c
+++ devel/appstream/patches/patch-src_as-system-info.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_as-system-info.c,v 1.4 2024/12/03 10:35:55 jperkin Exp $
+$NetBSD: patch-src_as-system-info.c,v 1.5 2024/12/09 11:37:31 adam Exp $
 
 Provide a reasonable fallback for retrieving total memory.
 
---- src/as-system-info.c.orig	2024-12-03 10:27:19.242982022 +0000
+--- src/as-system-info.c.orig	2024-12-08 22:34:32.000000000 +0000
 +++ src/as-system-info.c
-@@ -499,7 +499,7 @@ as_get_physical_memory_total (void)
- 	GlobalMemoryStatusEx (&statex);
- 	return statex.ullTotalPhys / (1024 * 1024);
+@@ -520,7 +520,7 @@ as_get_physical_memory_total (void)
+ 		return (physpages * pagesize) / MB_IN_BYTES;
+ 	return 0;
  #else
 -#error "Implementation of as_get_physical_memory_total() missing for this OS."
 +	return sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGESIZE);
