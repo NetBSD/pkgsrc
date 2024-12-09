d131de5742243afcd6d69d3702580d3494c6d34c
bsiegert <bsiegert@pkgsrc.org>
2024-12-09 21:20:52 +0000

    kicad: add a wayland option, default on
    If you have EGL available, kicad tries to build with Wayland support. However,
    without the wayland bl3 include, the build fails.
    
    Add an explicit option for enabling / disabling Wayland instead. From
    Robert Whitlock in PR pkg/58339.

--- cad/kicad/options.mk
+++ cad/kicad/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2024/11/20 01:29:49 mef Exp $
+# $NetBSD: options.mk,v 1.4 2024/12/09 21:20:52 bsiegert Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kicad
-PKG_SUPPORTED_OPTIONS=	ng-spice
-PKG_SUGGESTED_OPTIONS=	ng-spice
+PKG_SUPPORTED_OPTIONS=	ng-spice wayland
+PKG_SUGGESTED_OPTIONS=	ng-spice wayland
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +11,19 @@ PKG_SUGGESTED_OPTIONS=	ng-spice
 CMAKE_CONFIGURE_ARGS+=		-DKICAD_SPICE=ON
 .else
 CMAKE_CONFIGURE_ARGS+=		-DKICAD_SPICE=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mwayland)
+.  include "../../devel/wayland/buildlink3.mk"
+CMAKE_ARGS+=   -DKICAD_WAYLAND=ON
+# This conditional matches the conditional in Makefile
+# so that it's effectively a test for whether
+# KICAD_USE_EGL is enabled.
+.  if empty(MESALIB_SUPPORTS_EGL:Myes)
+PKG_FAIL_REASON+= "Kicad requires the KICAD_USE_EGL CMake option when using the "
+PKG_FAIL_REASON+= "KICAD_WAYLAND CMake option. See the cmake_dependent_option() "
+PKG_FAIL_REASON+= "for KICAD_WAYLAND in CMakeLists.txt."
+.  endif
+.else
+CMAKE_ARGS+=   -DKICAD_WAYLAND=OFF
 .endif
