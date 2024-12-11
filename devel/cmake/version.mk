60897a037ed5b6fc59dd360e8aafedd1d2ac0a6a
adam <adam@pkgsrc.org>
2024-12-11 11:34:32 +0000

    cmake cmake-gui: updated to 3.31.2
    CMake 3.31.2
    * AIX: Disallow TARGET_SONAME_* genex for AIX_SHARED_LIBRARY_ARCHIVE targets
    * AIX: Propagate AIX_SHARED_LIBRARY_ARCHIVE through exports
    * GNU linker: Use single-argument form of --dependency-file flag
    * fileapi: Do not expose flags for linker-generated dependencies
    * LINK_OPTIONS: Fix crash on empty LINKER: prefix
    * ci: Update to ROCm 6 HIP on Fedora 41
    * Tutorial: Restore TODO 4 comment in step 7
    * Intel/Classic: deactivate linker dependency file support
    * FindPython: Ensure SABIModule is found in all cases
    * Autogen: Restore <target_name>_autogen_timestamp_deps target
    * Autogen: Fix Ninja Multi-Config dependency graph regression
    * Autogen: Revert adding ui files to byproducts of the timestamp target
    * FindRuby: Add support for 3.4

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.69 2024/11/27 07:02:35 adam Exp $
+# $NetBSD: version.mk,v 1.70 2024/12/11 11:34:32 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.1
+CMAKE_VERSION=	3.31.2
 CMAKE_API=	${CMAKE_VERSION:R}
