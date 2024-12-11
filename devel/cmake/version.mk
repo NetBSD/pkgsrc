bdde4511da21270d29878a021a78968b15b9ece4
adam <adam@pkgsrc.org>
2024-12-11 13:44:31 +0000

    cmake: downgraded to 3.31.1 for the sake of 1 December

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.70 2024/12/11 11:34:32 adam Exp $
+# $NetBSD: version.mk,v 1.71 2024/12/11 13:44:31 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.2
+CMAKE_VERSION=	3.31.1
 CMAKE_API=	${CMAKE_VERSION:R}
