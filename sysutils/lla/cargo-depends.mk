42fee70cd499c5eef16a6eb352fc924359b06a20
pin <pin@pkgsrc.org>
2024-12-12 10:24:32 +0000

    sysutils/lla: update to 0.3.1
     - Small patch

--- sysutils/lla/cargo-depends.mk
+++ sysutils/lla/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.9 2024/12/11 21:44:30 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.10 2024/12/12 10:24:32 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.3
@@ -49,6 +49,7 @@ CARGO_CRATE_DEPENDS+=	hashbrown-0.14.5
 CARGO_CRATE_DEPENDS+=	hashbrown-0.15.2
 CARGO_CRATE_DEPENDS+=	heck-0.5.0
 CARGO_CRATE_DEPENDS+=	hermit-abi-0.1.19
+CARGO_CRATE_DEPENDS+=	home-0.5.9
 CARGO_CRATE_DEPENDS+=	iana-time-zone-0.1.61
 CARGO_CRATE_DEPENDS+=	iana-time-zone-haiku-0.1.2
 CARGO_CRATE_DEPENDS+=	indexmap-1.9.3
@@ -62,7 +63,7 @@ CARGO_CRATE_DEPENDS+=	libc-0.2.166
 CARGO_CRATE_DEPENDS+=	libloading-0.8.5
 CARGO_CRATE_DEPENDS+=	libredox-0.1.3
 CARGO_CRATE_DEPENDS+=	linux-raw-sys-0.4.14
-CARGO_CRATE_DEPENDS+=	lla_plugin_interface-0.3.0
+CARGO_CRATE_DEPENDS+=	lla_plugin_interface-0.3.1
 CARGO_CRATE_DEPENDS+=	lock_api-0.4.12
 CARGO_CRATE_DEPENDS+=	log-0.4.22
 CARGO_CRATE_DEPENDS+=	memchr-2.7.4
@@ -140,6 +141,7 @@ CARGO_CRATE_DEPENDS+=	wasm-bindgen-macro-0.2.95
 CARGO_CRATE_DEPENDS+=	wasm-bindgen-macro-support-0.2.95
 CARGO_CRATE_DEPENDS+=	wasm-bindgen-shared-0.2.95
 CARGO_CRATE_DEPENDS+=	web-time-1.1.0
+CARGO_CRATE_DEPENDS+=	which-4.4.2
 CARGO_CRATE_DEPENDS+=	winapi-0.3.9
 CARGO_CRATE_DEPENDS+=	winapi-i686-pc-windows-gnu-0.4.0
 CARGO_CRATE_DEPENDS+=	winapi-util-0.1.9
