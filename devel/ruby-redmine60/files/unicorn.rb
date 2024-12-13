6e6bd5b09838972433bc80ffaf6683b31770ad6f
taca <taca@pkgsrc.org>
2024-12-13 17:19:28 +0000

    devel/ruby-redmine60: add package version 6.0.2
    Redmine is a flexible project management web application using the
    Ruby on Rails framework, it is cross-platform and cross-database.
    
    Redmine 6.0 introduces these new features in these categories:
    
    * Redmine 6 has been upgraded to Rails 7.2
    * Ruby 3.3 is now supported and support for Ruby 2.7 and 3.0 has been dropped
    * Asset pipeline integration using Propshaft has been enabled
    * Support for Markdown has been removed
    * Icons have been replaced with SVG icons provided by Tabler
    * UI tweaks
    * New features and enhancements, some of them being long awaited features
    * API changes
    * Security improvement:
      User visibility changed from "all" to "member of visible projects" for new
      roles and existing builtin roles (#38853)

---
+++ devel/ruby-redmine60/files/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "@PREFIX@/@RM_DIR@"
+pid "@PREFIX@/@RM_DIR@/unicorn.pid"
+stderr_path "@PREFIX@/@RM_DIR@/unicorn.log"
+stdout_path "@PREFIX@/@RM_DIR@/unicorn.log"
+
+worker_processes 3
+listen "@PREFIX@/@RM_DIR@/unicorn.redmine.sock"
+timeout 60
