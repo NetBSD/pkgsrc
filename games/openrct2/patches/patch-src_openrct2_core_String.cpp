29d145596dc024143cfc16767f75982d13ea0b15
triaxx <triaxx@pkgsrc.org>
2024-12-14 20:47:52 +0000

    games/openrct2: Update to 0.4.17
    upstream changes:
    -----------------
    0.4.17 (2024-12-08)
    
        Feature: [#23166] Add Galician translation.
        Feature: [#23227] Add Classic Wooden Twister roller coaster, for better compatibility with RCT1.
        Improved: [#23051] Add large sloped turns and new inversions to the Twister, Vertical Drop, Hyper and Flying Roller Coasters.
        Improved: [#23123] Improve sorting of roller coasters in build new ride menu.
        Improved: [#23211] Add boosters to classic wooden roller coaster (cheats only).
        Improved: [#23229] Add debug option for making the sprite sorting algorithm stable.
        Improved: [#23233] Add diagonal booster to LSM Launched Coaster.
        Improved: [#23277] Add small, medium and large turns, diagonal track and S-bends to the Boat Hire.
        Fix: [#20070, #22972] Missing and mismatched flat and sloped footpaths on several scenarios.
        Fix: [#22726] ‘Force park rating’ cheat is not saved with the park.
        Fix: [#23064] Stand-Up Roller Coaster unbanked to banked track pieces are misaligned.
        Fix: [#23066] Stand-Up Roller Coaster has many supports that don't join up to the track.
        Fix: [#23206] Multiplayer desyncs when FPS is uncapped.
        Fix: [#23238] Updating a guest’s favourite ride works differently from vanilla RCT2.
        Fix: [objects#355] Fix colour preset settings of the Stand-Up Roller Coaster trains.
        Fix: [objects#355] Fix colour preset settings of many vehicles from Wacky Worlds and Time Twister.

--- games/openrct2/patches/patch-src_openrct2_core_String.cpp
+++ games/openrct2/patches/patch-src_openrct2_core_String.cpp
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_openrct2_core_String.cpp,v 1.4 2024/09/21 06:41:34 triaxx Exp $
+$NetBSD: patch-src_openrct2_core_String.cpp,v 1.5 2024/12/14 20:47:53 triaxx Exp $
 
 All BSD-like platforms have alloca in stdlib.h.
 
---- src/openrct2/core/String.cpp.orig	2024-09-01 20:30:08.000000000 +0000
+--- src/openrct2/core/String.cpp.orig	2024-12-08 16:50:02.000000000 +0000
 +++ src/openrct2/core/String.cpp
 @@ -17,8 +17,10 @@
  #include <vector>
  
  #ifndef _WIN32
--#    if !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__OpenBSD__)
-+#    if defined(__linux__) || defined(__sun)
- #        include <alloca.h>
-+#    else
-+#        include <stdlib.h>
- #    endif
- #    include <unicode/ucnv.h>
- #    include <unicode/unistr.h>
+-    #if !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__OpenBSD__)
++    #if defined(__linux__) || defined(__sun)
+         #include <alloca.h>
++    #else
++        #include <stdlib.h>
+     #endif
+     #include <unicode/ucnv.h>
+     #include <unicode/unistr.h>
--- games/openrct2/patches/patch-src_openrct2_platform_Platform.Posix.cpp
+++ games/openrct2/patches/patch-src_openrct2_platform_Platform.Posix.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_openrct2_platform_Platform.Posix.cpp,v 1.1 2023/09/02 07:30:35 triaxx Exp $
+$NetBSD: patch-src_openrct2_platform_Platform.Posix.cpp,v 1.2 2024/12/14 20:47:53 triaxx Exp $
 
 Support NetBSD.
 
---- src/openrct2/platform/Platform.Posix.cpp.orig	2022-04-25 17:21:38.000000000 +0000
+--- src/openrct2/platform/Platform.Posix.cpp.orig	2024-12-08 16:50:02.000000000 +0000
 +++ src/openrct2/platform/Platform.Posix.cpp
 @@ -7,7 +7,7 @@
   * OpenRCT2 is licensed under the GNU General Public License version 3.
@@ -11,5 +11,5 @@ Support NetBSD.
 -#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__)
 +#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__) || defined(__NetBSD__)
  
- #    include "Platform.h"
+     #include "Platform.h"
  
