4aed3f4c9e1078ad80baca190d5389cefa01fd37
jperkin <jperkin@pkgsrc.org>
2024-12-16 11:05:02 +0000

    blosc2: Re-fix illumos differently.
    This way is terrible, but at least won't break NetBSD.

--- devel/blosc2/patches/patch-tests_test__common.h
+++ devel/blosc2/patches/patch-tests_test__common.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-tests_test__common.h,v 1.1 2023/05/29 16:38:23 wiz Exp $
+$NetBSD: patch-tests_test__common.h,v 1.2 2024/12/16 11:05:02 jperkin Exp $
+
+XXX: this should be detected properly, but has a habit of breaking.
 
 --- tests/test_common.h.orig	2023-05-18 16:04:38.000000000 +0000
 +++ tests/test_common.h
@@ -7,7 +9,7 @@ $NetBSD: patch-tests_test__common.h,v 1.1 2023/05/29 16:38:23 wiz Exp $
    /* A (void *) cast needed for avoiding a warning with MINGW :-/ */
    block = (void *)_aligned_malloc(size, alignment);
 -#elif _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
-+#elif defined(__NetBSD__) || _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
++#elif defined(__NetBSD__) || defined(__illumos__) || _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
    /* Platform does have an implementation of posix_memalign */
    res = posix_memalign(&block, alignment, size);
  #elif defined(__APPLE__)
