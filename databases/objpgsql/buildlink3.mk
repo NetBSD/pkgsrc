5fd492ad4282e050f490cdc3ecbbffa8b211563b
wiz <wiz@pkgsrc.org>
2024-12-18 10:30:56 +0000

    objpgsq: add missing tool dependency
    while here, fix actual bugs that pkglint detected.

--- databases/objpgsql/buildlink3.mk
+++ databases/objpgsql/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/08/11 18:12:45 js Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2024/12/18 10:30:56 wiz Exp $
 
 BUILDLINK_TREE+=	objpgsql
 
 .if !defined(OBJPGSQL_BUILDLINK3_MK)
 OBJPGSQL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.objpgsqsl+=	objpgsql>=1.0
+BUILDLINK_API_DEPENDS.objpgsql+=	objpgsql>=1.0
 BUILDLINK_ABI_DEPENDS.objpgsql+=	objpgsql>=1.0
-BUILDLINK_PKGSRCDIR.objpgsql?=		../../database/objpgsql
+BUILDLINK_PKGSRCDIR.objpgsql?=		../../databases/objpgsql
 .endif
 
 BUILDLINK_TREE+=	-objpgsql
