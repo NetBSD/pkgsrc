da06c1aef72b3f775990aae1c08e6d62a014619f
gutteridge <gutteridge@pkgsrc.org>
2024-12-19 01:23:39 +0000

    p5-Alien-Build: fix build support for SunOS + pkgconf
    Don't assume pkgconf won't work on Solaris derivates and disallow its
    use. (This package being a tool that's used by other packages for
    builds.) Addresses a failure of p5-Alien-Hunspell to build on SunOS
    only. (Which in turn is required for p5-Text-Hunspell these days.)

---
+++ devel/p5-Alien-Build/patches/patch-lib_Alien_Build_Plugin_PkgConfig_Negotiate.pm
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_Alien_Build_Plugin_PkgConfig_Negotiate.pm,v 1.1 2024/12/19 01:23:39 gutteridge Exp $
+
+Don't assume pkgconfig won't work on 64-bit "Solaris" and prevent its use.
+
+--- lib/Alien/Build/Plugin/PkgConfig/Negotiate.pm.orig	2024-10-29 00:51:50.000000000 +0000
++++ lib/Alien/Build/Plugin/PkgConfig/Negotiate.pm
+@@ -48,7 +48,7 @@ sub pick
+     # on solaris 64 bit.
+     # Some advice on pkg-config and 64 bit Solaris
+     # https://docs.oracle.com/cd/E53394_01/html/E61689/gplhi.html
+-    my $is_solaris64 = (_perl_config('osname') eq 'solaris' && _perl_config('ptrsize') == 8);
++    my $is_solaris64 = 0 # (_perl_config('osname') eq 'solaris' && _perl_config('ptrsize') == 8);
+ 
+     # PkgConfig.pm is more reliable on windows
+     my $is_windows = _perl_config('osname') eq 'MSWin32';
