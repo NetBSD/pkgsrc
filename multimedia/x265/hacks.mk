19e8ae29fafbb25c018b038f1d3b091a70007213
nia <nia@pkgsrc.org>
2024-12-22 16:44:30 +0000

    svt-av1, x265: These use ARM NEON intrinsics only supported in GCC>=10

---
+++ multimedia/x265/hacks.mk
@@ -0,0 +1,16 @@
+# $NetBSD: hacks.mk,v 1.1 2024/12/22 16:44:30 nia Exp $
+
+.if !defined(X265_HACKS_MK)
+X265_HACKS_MK=		# defined
+
+### [Mon Sun Dec 22 17:37:18 CET 2024 : nia]
+### GCC 9 and earlier lack certain ARM NEON intrinsics such as vld1_s16_x2
+###
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 100000
+PKG_HACKS+=		neon-intrin
+CMAKE_CONFIGURE_ARGS+=	-DENABLE_NEON=OFF
+CMAKE_CONFIGURE_ARGS+=	-DENABLE_NEON_DOTPROD=OFF
+CMAKE_CONFIGURE_ARGS+=	-DENABLE_NEON_I8MM=OFF
+.endif
+
+.endif
