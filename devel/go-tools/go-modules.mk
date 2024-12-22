d774514c7b2f919fa00b7f038f104605de62ff40
bsiegert <bsiegert@pkgsrc.org>
2024-12-22 20:12:32 +0000

    go-tools: update to 0.28 (security)
    Contains a fix for GO-2024-3333:
    Non-linear parsing of case-insensitive content in golang.org/x/net/html

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,16 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.6 2024/10/11 09:30:16 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.7 2024/12/22 20:12:32 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.21.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.30.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.30.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.22.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.22.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.33.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.33.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.10.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.10.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.28.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.28.0.zip
 GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.mod
 GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.zip
