81f94c372dee67b83ce7b80e1cd419377aff51e7
nia <nia@pkgsrc.org>
2024-12-22 16:50:10 +0000

    I forgot about compiler.mk...

--- multimedia/x265/hacks.mk
+++ multimedia/x265/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2024/12/22 16:44:30 nia Exp $
+# $NetBSD: hacks.mk,v 1.2 2024/12/22 16:50:10 nia Exp $
 
 .if !defined(X265_HACKS_MK)
 X265_HACKS_MK=		# defined
@@ -6,7 +6,9 @@ X265_HACKS_MK=		# defined
 ### [Mon Sun Dec 22 17:37:18 CET 2024 : nia]
 ### GCC 9 and earlier lack certain ARM NEON intrinsics such as vld1_s16_x2
 ###
-.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 100000
+.include "../../mk/compiler.mk"
+
+.if ${MACHINE_ARCH:Maarch64*} && ${CC_VERSION:Mgcc-[0-9]\.*}
 PKG_HACKS+=		neon-intrin
 CMAKE_CONFIGURE_ARGS+=	-DENABLE_NEON=OFF
 CMAKE_CONFIGURE_ARGS+=	-DENABLE_NEON_DOTPROD=OFF
