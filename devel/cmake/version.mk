88346bc72c0f258bdcc669a94ec02db00eab0bd1
adam <adam@pkgsrc.org>
2024-12-24 13:54:01 +0000

    cmake cmake-gui: updated to 3.31.3
    CMake 3.31.3
    * VS: Factor out check for mixed C and C++ target
    * VS: Restore support for mixing C++23 and C in one target with clang-cl
    * cmLocalUnixMakefileGenerator3: Simplify progress command generation
    * Makefile: Fix per-directory codegen target
    * Makefile: Enable progress messages for codegen target
    * cmGlobalNinjaGenerator: Remove unused local variable
    * cmGlobalNinjaGenerator: Remove unnecessary local variable
    * cmGlobalNinjaGenerator: Fix local variable name for codegen target
    * cmGlobalNinjaGenerator: Clarify order of codegen build statement logic
    * cmGlobalNinjaGenerator: Simplify per-directory configuration list lookup
    * Ninja: Add missing top-level codegen dependencies on per-directory codegen
    * Tests/RunCMake/CXXModules: Rename FileSetModules cases to be more specific
    * target_sources: Improve error message for CXX_MODULES on INTERFACE libraries
    * Help: Update link to UTF-8 RFC 3629
    * ci: update macOS jobs to use Xcode 16.2
    * install: Restore treatment of DESTINATION as a single-valued keyword
    
    CMake 3.31.2
    * AIX: Disallow TARGET_SONAME_* genex for AIX_SHARED_LIBRARY_ARCHIVE targets
    * AIX: Propagate AIX_SHARED_LIBRARY_ARCHIVE through exports
    * GNU linker: Use single-argument form of --dependency-file flag
    * fileapi: Do not expose flags for linker-generated dependencies
    * LINK_OPTIONS: Fix crash on empty LINKER: prefix
    * ci: Update to ROCm 6 HIP on Fedora 41
    * Tutorial: Restore TODO 4 comment in step 7
    * Intel/Classic: deactivate linker dependency file support
    * FindPython: Ensure SABIModule is found in all cases
    * Autogen: Restore <target_name>_autogen_timestamp_deps target
    * Autogen: Fix Ninja Multi-Config dependency graph regression
    * Autogen: Revert adding ui files to byproducts of the timestamp target
    * FindRuby: Add support for 3.4

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.71 2024/12/11 13:44:31 adam Exp $
+# $NetBSD: version.mk,v 1.72 2024/12/24 13:54:01 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.1
+CMAKE_VERSION=	3.31.3
 CMAKE_API=	${CMAKE_VERSION:R}
