dabe89da218e14b2c51b7b58efc2fd4e4f687ef6
bsiegert <bsiegert@pkgsrc.org>
2024-12-25 11:05:57 +0000

    libxfce4util: update to 4.20.0
    No proper changelog from upstream.

--- x11/libxfce4util/buildlink3.mk
+++ x11/libxfce4util/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.36 2022/12/30 17:16:56 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2024/12/25 11:05:57 bsiegert Exp $
 
 BUILDLINK_TREE+=	libxfce4util
 
 .if !defined(LIBXFCE4UTIL_BUILDLINK3_MK)
 LIBXFCE4UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.18.0
-BUILDLINK_ABI_DEPENDS.libxfce4util+=	libxfce4util>=4.18.0
+BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.20.0
+BUILDLINK_ABI_DEPENDS.libxfce4util+=	libxfce4util>=4.20.0
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../x11/libxfce4util
 
 .endif # LIBXFCE4UTIL_BUILDLINK3_MK
