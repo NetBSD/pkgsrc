e56aaee177c820440092888313a4d1613533e8cd
bsiegert <bsiegert@pkgsrc.org>
2024-12-25 11:09:04 +0000

    libxfce4ui: update to 4.20.0

--- x11/libxfce4ui/buildlink3.mk
+++ x11/libxfce4ui/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2022/12/30 17:16:56 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2024/12/25 11:09:04 bsiegert Exp $
 
 BUILDLINK_TREE+=	libxfce4ui
 
 .if !defined(LIBXFCE4UI_BUILDLINK3_MK)
 LIBXFCE4UI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxfce4ui+=	libxfce4ui>=4.18.0
-BUILDLINK_ABI_DEPENDS.libxfce4ui+=	libxfce4ui>=4.18.0
+BUILDLINK_API_DEPENDS.libxfce4ui+=	libxfce4ui>=4.20.0
+BUILDLINK_ABI_DEPENDS.libxfce4ui+=	libxfce4ui>=4.20.0
 BUILDLINK_PKGSRCDIR.libxfce4ui?=	../../x11/libxfce4ui
 
 .include "../../devel/xfce4-conf/buildlink3.mk"
