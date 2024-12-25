0236f124c9f68608ad0049931ecb137505decbb1
bsiegert <bsiegert@pkgsrc.org>
2024-12-25 11:07:01 +0000

    xfce4-conf: update to 4.20.0
    No proper changelog from upstream.

--- devel/xfce4-conf/buildlink3.mk
+++ devel/xfce4-conf/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2022/12/30 17:16:56 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2024/12/25 11:07:01 bsiegert Exp $
 
 BUILDLINK_TREE+=	xfce4-conf
 
 .if !defined(XFCE4_CONF_BUILDLINK3_MK)
 XFCE4_CONF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-conf+=	xfce4-conf>=4.18.0
-BUILDLINK_ABI_DEPENDS.xfce4-conf?=	xfce4-conf>=4.18.0
+BUILDLINK_API_DEPENDS.xfce4-conf+=	xfce4-conf>=4.20.0
+BUILDLINK_ABI_DEPENDS.xfce4-conf?=	xfce4-conf>=4.20.0
 BUILDLINK_PKGSRCDIR.xfce4-conf?=	../../devel/xfce4-conf
 
 .include "../../sysutils/dbus/buildlink3.mk"
