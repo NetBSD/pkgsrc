f32ffa818681d5124518710f8e7eac022ebb9ef1
riastradh <riastradh@pkgsrc.org>
2024-12-26 22:04:28 +0000

    databases/sqlite3: Spruce up build options.
    1. Document reasoning for each of the build-time options available and
       set a rubric for which ones to enable in future updates.
    
    2. Enable some options that look well-justified.  I went through all
       the options at https://www.sqlite.org/compile.html, applied the
       rubric, and compared to several other general-purpose packaging
       systems.
    
    (Happy to discuss and amend the rubric and the choice of options --
    this should be viewed as a starting point for review, testing, and
    tweaking, not an inviolably final decision.)
    
    3. Nix PKG_OPTIONS that are default-on upstream -- these haven't done
       anything in a while.  If you want to add these back, then:
    
       (a) they should be in PKG_SUGGESTED_OPTIONS (because they're
           already enabled by default upstream), and
    
       (b) you'll have to set and test the appropriate SQLITE_OMIT_*
           option.
    
       Options removed:
       - deserialize (default-on upstream anyway)
       - fts (default-on upstream anyway)
       - sqlite3-dbstat (unconditionally enabled in pkgsrc now because
         needed by downstream packages)
    
       Options remaining:
       - icu
    
    4. Nix some deprecated options like SQLITE_ENABLE_LOAD_EXTENSION.
    
    Additions to CONFIGURE_ARGS:
    --enable-fts3 (paranoia, default-on upstream anyway)
    --enable-fts4 (paranoia, default-on upstream anyway)
    --enable-fts5 (paranoia, default-on upstream anyway)
    --enable-math (paranoia, default-on upstream anyway)
    --enable-rtree (paranoia, default-on upstream anyway)
    --enable-session
    
    Removals from CONFIGURE_ARGS: none
    
    Additions to CFLAGS:
    -DSQLITE_ENABLE_DBSTAT_VTAB
    -DSQLITE_ENABLE_FTS3_PARENTHESIS
    -DSQLITE_ENABLE_HIDDEN_COLUMNS
    -DSQLITE_ENABLE_STMTVTAB
    
    Removals from CFLAGS:
    -DSQLITE_ENABLE_LOAD_EXTENISON (default-on upstream now; need
      SQLITE_OMIT_LOAD_EXTENSION to reverse)
    -DSQLITE_ENABLE_MATH_FUNCTIONS (replaced by --enable-math for better
      build-time error checking)
    
    PR pkg/58448: databases/sqlite3 is missing dbstat option

--- databases/sqlite3/options.mk
+++ databases/sqlite3/options.mk
@@ -1,25 +1,12 @@
-# $NetBSD: options.mk,v 1.8 2024/07/19 17:11:54 riastradh Exp $
+# $NetBSD: options.mk,v 1.9 2024/12/26 22:04:28 riastradh Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sqlite3
-PKG_SUPPORTED_OPTIONS=		sqlite3-dbstat fts icu deserialize
-PKG_SUGGESTED_OPTIONS=		fts # enabled in NetBSD builtin version
+PKG_SUPPORTED_OPTIONS=		icu
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Msqlite3-dbstat)
-CFLAGS+=	-DSQLITE_ENABLE_DBSTAT_VTAB=1
-.endif
-
-.if !empty(PKG_OPTIONS:Mfts)
-CFLAGS+=	-DSQLITE_ENABLE_FTS4=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS=1
-.endif
-
-.if !empty(PKG_OPTIONS:Micu)
+.if ${PKG_OPTIONS:Micu}
 CFLAGS+=	-DSQLITE_ENABLE_ICU=1
 LDFLAGS+=	-licui18n -licuuc -licudata
 .include "../../textproc/icu/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mdeserialize)
-CFLAGS+=	-DSQLITE_ENABLE_DESERIALIZE=1
 .endif
