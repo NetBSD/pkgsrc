78ec2d56b728956758c842c6ef4f43fe0a673e41
adam <adam@pkgsrc.org>
2024-12-26 23:57:18 +0000

    revbump after devel/protobuf update

--- devel/libcompizconfig/buildlink3.mk
+++ devel/libcompizconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2024/11/14 22:19:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2024/12/26 23:57:20 adam Exp $
 
 BUILDLINK_TREE+=	libcompizconfig
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libcompizconfig
 LIBCOMPIZCONFIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcompizconfig+=	libcompizconfig>=0.6.0
-BUILDLINK_ABI_DEPENDS.libcompizconfig+=	libcompizconfig>=0.8.8nb76
+BUILDLINK_ABI_DEPENDS.libcompizconfig+=	libcompizconfig>=0.8.8nb77
 BUILDLINK_PKGSRCDIR.libcompizconfig?=	../../devel/libcompizconfig
 
 .include "../../devel/protobuf/buildlink3.mk"
--- graphics/opencv-contrib-face/buildlink3.mk
+++ graphics/opencv-contrib-face/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2024/10/19 11:55:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2024/12/26 23:57:21 adam Exp $
 
 BUILDLINK_TREE+=	opencv-contrib-face
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	opencv-contrib-face
 OPENCV_CONTRIB_FACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencv-contrib-face+=	opencv-contrib-face>=3.0.0
-BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.20nb5
+BUILDLINK_ABI_DEPENDS.opencv-contrib-face?=	opencv-contrib-face>=3.4.20nb6
 BUILDLINK_PKGSRCDIR.opencv-contrib-face?=	../../graphics/opencv-contrib-face
 
 .include "../../graphics/opencv/buildlink3.mk"
