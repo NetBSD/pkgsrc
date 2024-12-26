3fb96f9c3e2410bc93aa0c1df87fa6d444d359ee
bsiegert <bsiegert@pkgsrc.org>
2024-12-26 18:28:45 +0000

    xfce4-exo: update to 4.20.0
    Part of the XFCE 4.20 update

--- x11/xfce4-exo/buildlink3.mk
+++ x11/xfce4-exo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.38 2022/12/30 17:16:56 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2024/12/26 18:28:45 bsiegert Exp $
 
 BUILDLINK_TREE+=	xfce4-exo
 
 .if !defined(XFCE4_EXO_BUILDLINK3_MK)
 XFCE4_EXO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=4.18.0
-BUILDLINK_ABI_DEPENDS.xfce4-exo+=	xfce4-exo>=4.18.0
+BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=4.20.0
+BUILDLINK_ABI_DEPENDS.xfce4-exo+=	xfce4-exo>=4.20.0
 BUILDLINK_PKGSRCDIR.xfce4-exo?=		../../x11/xfce4-exo
 
 .include "../../x11/libxfce4ui/buildlink3.mk"
