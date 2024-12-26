23f96d0e82509af6c73ab3a3dc8d670370fcf091
adam <adam@pkgsrc.org>
2024-12-26 23:48:22 +0000

    grpc: updated to 1.68.2
    1.68.2
    
    C++
    
    [CI] Added a gRPC_BUILD_TESTS guard to third_party protos
    
    
    1.68.1
    
    Core
    
    [xDS RBAC] Support string_match in HeaderMatcher
    
    
    1.68.0
    
    Core
    
    [XdsClient][Backport] Add missing authority to XdsClient metrics scope
    [Release] Bump core version in preparation for 1.68 Branch Cut.
    [ConfigFetcher] Set HTTP2 error to NO_ERROR to do graceful GOAWAYs.
    [ruby] reduce INFO log for server CQ pluck registration to DEBUG.
    [EventEngine] Enable the PosixEventEngine client experiment.
    [chttp2_server] Fix race between connection starting and it being orphaned.
    [Chttp2Server] Fix race between connection manager updates and handshake.
    [xds] Fix XdsClient race between ResourceDoesNotExist timer and receiving resources.
    
    C++
    
    [Build] Minimum version of MSVC is now 2022.
    [Build] Bumped the minimum version of cmake.

--- net/grpc/patches/patch-src_core_lib_event__engine_cf__engine_cfstream__endpoint.h
+++ net/grpc/patches/patch-src_core_lib_event__engine_cf__engine_cfstream__endpoint.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_core_lib_event__engine_cf__engine_cfstream__endpoint.h,v 1.1 2024/02/03 20:02:03 adam Exp $
+$NetBSD: patch-src_core_lib_event__engine_cf__engine_cfstream__endpoint.h,v 1.2 2024/12/26 23:48:22 adam Exp $
 
 Fix error: no member named 'StrCat' in namespace 'absl'.
 
---- src/core/lib/event_engine/cf_engine/cfstream_endpoint.h.orig	2024-02-03 19:48:36.455664126 +0000
+--- src/core/lib/event_engine/cf_engine/cfstream_endpoint.h.orig	2024-11-14 22:30:21.000000000 +0000
 +++ src/core/lib/event_engine/cf_engine/cfstream_endpoint.h
-@@ -21,6 +21,7 @@
- 
+@@ -22,6 +22,7 @@
  #include <CoreFoundation/CoreFoundation.h>
+ #include <grpc/event_engine/event_engine.h>
  
 +#include "absl/strings/str_cat.h"
  #include "absl/strings/str_format.h"
- 
- #include <grpc/event_engine/event_engine.h>
+ #include "src/core/lib/address_utils/sockaddr_utils.h"
+ #include "src/core/lib/event_engine/cf_engine/cf_engine.h"
