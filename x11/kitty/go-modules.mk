71a3f11889bbc0132604e391a5cf0a09e9115d1e
ktnb <ktnb@pkgsrc.org>
2024-12-26 16:50:43 +0000

    kitty: update to 0.38.1
    0.38.1:
    - bug fixes
    
    0.38.0:
    - Allow specifying individual color themes to use so that kitty
      changes colors automatically following the OS dark/light mode
    - notify_on_cmd_finish: Automatically remove notifications when
      the window gains focus or the next notification is shown.
    - Discard OSC 9 notifications that start with 4
    - clipboard kitten: Fix a bug causing kitten to hang in filter
      mode when input data size is not divisible by 3 and larger
      than 8KB
    - Graphics protocol: Fix delete by number not deleting newest
      image with the specified number
    - Fix dashed and dotted underlines not being drawn at the same y
      position as straigh underlines at all font sizes
    - panel kitten: Allow creating floating and on-top panels with
      arbitrary placement and size on Wayland
    - hints/unicode_input kittens: Do not lose keypresses that are
      sent very rapidly via an automation tool immediately after the
      kitten is launched

--- x11/kitty/go-modules.mk
+++ x11/kitty/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.5 2024/11/07 14:18:42 ktnb Exp $
+# $NetBSD: go-modules.mk,v 1.6 2024/12/26 16:50:43 ktnb Exp $
 
 GO_MODULE_FILES+=	github.com/!a!l!tree/bigfloat/@v/v0.2.0.mod
 GO_MODULE_FILES+=	github.com/!a!l!tree/bigfloat/@v/v0.2.0.zip
@@ -49,13 +49,13 @@ GO_MODULE_FILES+=	github.com/zeebo/xxh3/@v/v1.0.2.zip
 GO_MODULE_FILES+=	golang.org/x/exp/@v/v0.0.0-20230801115018-d63ba01acd4b.mod
 GO_MODULE_FILES+=	golang.org/x/exp/@v/v0.0.0-20230801115018-d63ba01acd4b.zip
 GO_MODULE_FILES+=	golang.org/x/image/@v/v0.0.0-20191009234506-e7c1f5e7dbb8.mod
-GO_MODULE_FILES+=	golang.org/x/image/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/image/@v/v0.21.0.zip
+GO_MODULE_FILES+=	golang.org/x/image/@v/v0.23.0.mod
+GO_MODULE_FILES+=	golang.org/x/image/@v/v0.23.0.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190916202348-b4ddaad3f8a3.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20201204225414-ed752295db88.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.28.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.28.0.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.8.0.mod
 GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
--- x11/kitty/patches/patch-kitty_data-types.h
+++ x11/kitty/patches/patch-kitty_data-types.h
@@ -1,22 +1,15 @@
-$NetBSD: patch-kitty_data-types.h,v 1.1 2024/09/30 17:51:45 ktnb Exp $
+$NetBSD: patch-kitty_data-types.h,v 1.2 2024/12/26 16:50:44 ktnb Exp $
 
 Static assertions cause compile error for an unknown reason on
 NetBSD.
 
---- kitty/data-types.h.orig	2024-09-26 15:09:11.517368353 +0000
+--- kitty/data-types.h.orig	2024-12-18 14:56:38.746939917 +0000
 +++ kitty/data-types.h
-@@ -222,14 +222,12 @@ typedef struct {
-     sprite_index sprite_x, sprite_y, sprite_z;
-     CellAttrs attrs;
- } GPUCell;
--static_assert(sizeof(GPUCell) == 20, "Fix the ordering of GPUCell");
+@@ -63,7 +63,6 @@ static inline PyObject* Py_XNewRef(PyObj
  
- typedef struct {
-     char_type ch;
-     hyperlink_id_type hyperlink_id;
-     combining_type cc_idx[3];
- } CPUCell;
--static_assert(sizeof(CPUCell) == 12, "Fix the ordering of CPUCell");
- 
- typedef enum { UNKNOWN_PROMPT_KIND = 0, PROMPT_START = 1, SECONDARY_PROMPT = 2, OUTPUT_START = 3 } PromptKind;
- typedef union LineAttrs {
+ typedef unsigned long long id_type;
+ typedef uint32_t char_type;
+-static_assert(sizeof(Py_UCS4) == sizeof(char_type), "PyUCS4 and char_type dont match");
+ #define MAX_CHAR_TYPE_VALUE UINT32_MAX
+ typedef uint32_t color_type;
+ typedef uint16_t hyperlink_id_type;
