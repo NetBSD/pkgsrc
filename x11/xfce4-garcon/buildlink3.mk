3bcb5fc3c79059941ad75f5bfc6c2bac591adae2
bsiegert <bsiegert@pkgsrc.org>
2024-12-26 18:23:47 +0000

    xfce4-garcon: update to 4.20.0
    Part of the XFCE 4.20 update.

--- x11/xfce4-garcon/buildlink3.mk
+++ x11/xfce4-garcon/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2022/12/31 02:09:44 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2024/12/26 18:23:47 bsiegert Exp $
 
 BUILDLINK_TREE+=	xfce4-garcon
 
 .if !defined(XFCE4_GARCON_BUILDLINK3_MK)
 XFCE4_GARCON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.18.0
-BUILDLINK_ABI_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.18.0
+BUILDLINK_API_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.20.0
+BUILDLINK_ABI_DEPENDS.xfce4-garcon+=	xfce4-garcon>=4.20.0
 BUILDLINK_PKGSRCDIR.xfce4-garcon?=	../../x11/xfce4-garcon
 
 .include "../../x11/libxfce4ui/buildlink3.mk"
