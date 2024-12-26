9708053e58a95d5bb1481bb991cd89772d15b23e
wiz <wiz@pkgsrc.org>
2024-12-26 18:42:30 +0000

    libxfce4util: revert API bump
    If a package needs a newer version, it should set the API depends
    in its Makefile.

--- x11/libxfce4util/buildlink3.mk
+++ x11/libxfce4util/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.38 2024/12/26 18:30:26 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2024/12/26 18:42:30 wiz Exp $
 
 BUILDLINK_TREE+=	libxfce4util
 
 .if !defined(LIBXFCE4UTIL_BUILDLINK3_MK)
 LIBXFCE4UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.20.0nb1
+BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.20.0
 BUILDLINK_ABI_DEPENDS.libxfce4util+=	libxfce4util>=4.20.0
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../x11/libxfce4util
 
