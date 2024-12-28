b2c8f957d8774b354775f7a40aed74a0ca5ad590
adam <adam@pkgsrc.org>
2024-12-28 09:09:47 +0000

    jbig2enc: updated to 0.30
    0.30: (2024-12-24)
    * Reject odd input depths that pixThresholdToBinary doesn't support.
    * Fix build with Leptonica >=1.83
    * Replace vs2008 solution with CMake
    * Fix `Error heap-use-after-free`
    * Fix typos in code
    * Support python 3, drop support for python2
    * Rename pdf.py to jbig2topdf.py
    * Add a A switch to set dpi for images w/o dpi information
    * Add support for standalone .jb2
    * Suppress a chatty informational message unless requested
    * Neat symbolic threshold 0.92
    * Fix Endian issue on ARM
    * Neat symbolic threshold 0.92
    * Make the weight parameter adjustable
    * Add lobal BW threshold on 8 bpp images
    * Replace obsolete macro `AC_PROG_LIBTOOL'

--- graphics/jbig2enc/buildlink3.mk
+++ graphics/jbig2enc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/10/21 17:10:32 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2024/12/28 09:09:47 adam Exp $
 
 BUILDLINK_TREE+=	jbig2enc
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	jbig2enc
 JBIG2ENC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jbig2enc+=	jbig2enc>=0.29
-BUILDLINK_ABI_DEPENDS.jbig2enc?=	jbig2enc>=0.29nb1
-BUILDLINK_PKGSRCDIR.jbig2enc?=	../../graphics/jbig2enc
+BUILDLINK_ABI_DEPENDS.jbig2enc+=	jbig2enc>=0.29nb1
+BUILDLINK_PKGSRCDIR.jbig2enc?=		../../graphics/jbig2enc
 
 .include "../../graphics/leptonica/buildlink3.mk"
 .endif	# JBIG2ENC_BUILDLINK3_MK
