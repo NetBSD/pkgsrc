0bed1e6013e035e63588fb5f330c609cd123efa2
bsiegert <bsiegert@pkgsrc.org>
2024-12-29 19:09:46 +0000

    xfce4-panel: update to 4.20.0
    Part of the XFCE 4.20 update.

--- x11/xfce4-panel/buildlink3.mk
+++ x11/xfce4-panel/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.74 2024/12/27 08:21:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.75 2024/12/29 19:09:46 bsiegert Exp $
 
 BUILDLINK_TREE+=	xfce4-panel
 
 .if !defined(XFCE4_PANEL_BUILDLINK3_MK)
 XFCE4_PANEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.13.3
-BUILDLINK_ABI_DEPENDS.xfce4-panel+=	xfce4-panel>=4.18.6nb5
+BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.20.0
+BUILDLINK_ABI_DEPENDS.xfce4-panel+=	xfce4-panel>=4.20.0
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
 
 .include "../../x11/gtk3/buildlink3.mk"
