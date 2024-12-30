a9db010bf9c6b3e88bb27e7a62f2d3df1958ca1c
pin <pin@pkgsrc.org>
2024-12-30 21:53:47 +0000

    textproc/clicsv: import package
    wm/pekwm: update to 0.3.2
    
    Closed issues
    
        Disable SA_RESTART for SIGALRM, fixes WorkspaceIndicator getting stuck
        on OpenBSD.
    
    pekwm_panel
    
        Fix crash on client list updates, triggered by XRandr updates
    
    ggCVS: ----------------------------------------------------------------------

---
+++ textproc/clicsv/cargo-depends.mk
@@ -0,0 +1,11 @@
+# $NetBSD: cargo-depends.mk,v 1.1 2024/12/30 21:53:48 pin Exp $
+
+CARGO_CRATE_DEPENDS+=	bitflags-2.6.0
+CARGO_CRATE_DEPENDS+=	libc-0.2.169
+CARGO_CRATE_DEPENDS+=	libredox-0.1.3
+CARGO_CRATE_DEPENDS+=	numtoa-0.2.4
+CARGO_CRATE_DEPENDS+=	redox_syscall-0.5.8
+CARGO_CRATE_DEPENDS+=	redox_termios-0.1.3
+CARGO_CRATE_DEPENDS+=	termion-4.0.3
+CARGO_CRATE_DEPENDS+=	unicode-segmentation-1.12.0
+CARGO_CRATE_DEPENDS+=	unicode-width-0.1.14
