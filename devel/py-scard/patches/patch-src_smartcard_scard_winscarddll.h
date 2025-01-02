604261b948aab896b6172ca4710cca981f331a37
adam <adam@pkgsrc.org>
2025-01-02 09:58:45 +0000

    py-scard: updated to 2.2.0
    2.2.0 (October 2024)
    
    - patches from Ludovic Rousseau
       * PCSCCardRequest:
        - handle KeyboardInterrupt in waitforcard() & waitforcardevent()
            - use a local PC/SC context to avoid locks
       * smartcard.util.padd(): do NOT modify the input parameter
       * CardMonitoring: a timeout exception IS expected
       * Fix pydoctor documentation
       * wx: fix module and examples
       * Minor changes
    - patches from Kurt McKee
       * Remove Python 2 conditional code
       * Eliminate Windows Vista and Windows 7 conditionals
       * Test and improve the synchronization code
       * Test and update the `Observer.py` code
       * Remove `ClassLoader.py`
       * Migrate a `src/` layout
       * Migrate test/* from unittest to pytest
       * Add missing GSM 03.38 symbols for decoding
       * Support only Python 3.9 and higher
       * Remove the Python 2.x-only Pyro dependency
       * Migrate CI to use the official Coveralls action
       * Standardize local and CI testing to use tox
       * Build wheels in CI for all supported Python versions
       * Build the docs as a part of the test suite
       * Begin to add type annotations to the package
       * Deprecate the `HexListToBinString`, `BinStringToHexList`, `hl2bs`, and `bs2hl` utility functions
       * Support "64" as an ATR baud rate adjustment integer (ISO 7816-3 2006)

--- devel/py-scard/patches/patch-src_smartcard_scard_winscarddll.h
+++ devel/py-scard/patches/patch-src_smartcard_scard_winscarddll.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-smartcard_scard_winscarddll.h,v 1.1 2022/01/10 16:15:07 adam Exp $
+$NetBSD: patch-src_smartcard_scard_winscarddll.h,v 1.1 2025/01/02 09:58:45 adam Exp $
 
 Fix building on Darwin.
 
---- smartcard/scard/winscarddll.h.orig	2022-01-10 12:12:48.000000000 +0000
-+++ smartcard/scard/winscarddll.h
+--- src/smartcard/scard/winscarddll.h.orig	2022-01-10 12:12:48.000000000 +0000
++++ src/smartcard/scard/winscarddll.h
 @@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin St, Fifth 
      #include <PCSC/wintypes.h>
      #include <PCSC/winscard.h>
