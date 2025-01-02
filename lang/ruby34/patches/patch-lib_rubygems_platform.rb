6d6300400a142dec56d7e3b46b90a04ccf1ac3b4
taca <taca@pkgsrc.org>
2025-01-02 06:51:04 +0000

    lang/ruby34: add package version 3.4.1.
    Add latest release of Ruby Programming Language version 3.4.1.
    
    Ruby is an interpreted object-oriented programming language often used for
    web development. It also offers many scripting features to process plain
    text and serialized files, or manage system tasks.  It is simple,
    straightforward, and extensible.
    
    Features of Ruby are shown below.
      + Simple Syntax
      + *Normal* Object-Oriented features (ex. class, method calls)
      + *Advanced* Object-Oriented features (ex. Mix-in, Singleton-method)
      + Operator Overloading
      + Exception Handling
      + Iterators and Closures
      + Garbage Collection
      + Dynamic Loading of Object files (on some architecture)
      + Highly Portable (works on many UNIX machines, and on Windows, Mac, etc.)
    
    Ruby 3.4 introduces a number of new features and performance improvements,
    here are some of them:
      * Prsim parser (as a default gem).
      * Performance: YJIT: Verious improvements including performance.
      * Performance: RJIT: Experital pure-Ruby JIT compiler replace MJIT.
      * M:N thread scheduler.
      * Retirement of ext/readline.

---
+++ lang/ruby34/patches/patch-lib_rubygems_platform.rb
@@ -0,0 +1,29 @@
+$NetBSD: patch-lib_rubygems_platform.rb,v 1.1 2025/01/02 06:51:05 taca Exp $
+
+* Don't replace "i486" to "x86".
+* Allow simple "netbsd" as Gem::Platform.
+
+--- lib/rubygems/platform.rb.orig	2024-06-11 23:54:24.000000000 +0000
++++ lib/rubygems/platform.rb
+@@ -94,12 +94,7 @@ class Gem::Platform
+         arch.last << "-#{extra}"
+       end
+ 
+-      cpu = arch.shift
+-
+-      @cpu = case cpu
+-             when /i\d86/ then "x86"
+-             else cpu
+-      end
++      @cpu = arch.shift
+ 
+       if arch.length == 2 && arch.last.match?(/^\d+(\.\d+)?$/) # for command-line
+         @os, @version = arch
+@@ -132,6 +127,7 @@ class Gem::Platform
+                         @cpu = "x86" if @cpu.nil? && os =~ /32$/
+                         [os, version]
+                       when /netbsdelf/ then             ["netbsdelf", nil]
++                      when /netbsd/ then                ["netbsd", nil]
+                       when /openbsd(\d+\.\d+)?/ then    ["openbsd",   $1]
+                       when /solaris(\d+\.\d+)?/ then    ["solaris",   $1]
+                       when /wasi/ then                  ["wasi",      nil]
