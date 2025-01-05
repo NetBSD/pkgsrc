1abae5dc8e84de3f47ed95e717d17860280a9674
bsiegert <bsiegert@pkgsrc.org>
2025-01-05 13:50:37 +0000

    xfce4-power-manager: update to 4.20.0
    Part of the XFCE 4.20 update.

--- sysutils/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
+++ sysutils/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
@@ -1,44 +1,18 @@
-$NetBSD: patch-src_xfpm-pm-helper.c,v 1.2 2021/11/30 14:52:32 gutteridge Exp $
+$NetBSD: patch-src_xfpm-pm-helper.c,v 1.3 2025/01/05 13:50:37 bsiegert Exp $
 
 Add NetBSD support.
 
---- src/xfpm-pm-helper.c.orig	2021-11-26 18:17:09.701758792 +0000
+--- src/xfpm-pm-helper.c.orig	2024-11-21 06:26:21.000000000 +0000
 +++ src/xfpm-pm-helper.c
-@@ -77,6 +77,10 @@
- #define UP_BACKEND_SUSPEND_COMMAND  "/usr/sbin/zzz"
+@@ -82,6 +82,11 @@
  #define UP_BACKEND_HIBERNATE_COMMAND "/usr/sbin/ZZZ"
+ #define UP_BACKEND_HYBRID_SLEEP_COMMAND "/usr/bin/false"
  #endif
 +#ifdef BACKEND_TYPE_NETBSD
 +#define UP_BACKEND_SUSPEND_COMMAND "sysctl -w hw.acpi.sleep.state=3"
 +#define UP_BACKEND_HIBERNATE_COMMAND "sysctl -w hw.acpi.sleep.state=4"
++#define UP_BACKEND_HYBRID_SLEEP_COMMAND "/usr/bin/false"
 +#endif
  
  
  static gboolean
-@@ -178,22 +182,26 @@ main (int argc, char **argv)
-   /* run the command */
-   if(suspend)
-   {
-+#if defined(UP_BACKEND_SUSPEND_COMMAND)
-     if (run (UP_BACKEND_SUSPEND_COMMAND))
-     {
-       return EXIT_CODE_SUCCESS;
-     }
-     else
-+#endif
-     {
-       return EXIT_CODE_FAILED;
-     }
-   }
-   else if (hibernate)
-   {
-+#if defined(UP_BACKEND_HIBERNATE_COMMAND)
-     if(run (UP_BACKEND_HIBERNATE_COMMAND))
-     {
-       return EXIT_CODE_SUCCESS;
-     }
-     else
-+#endif
-     {
-       return EXIT_CODE_FAILED;
-     }
