fa8506a4fb8c29d140977fbd4a7c929a5b05819b
bsiegert <bsiegert@pkgsrc.org>
2025-01-06 19:27:40 +0000

    xfce4-session: update to 4.20.0
    Part of the XFCE 4.20 update.

--- x11/xfce4-session/patches/patch-xfce4-session_xfsm-shutdown-fallback.c
+++ x11/xfce4-session/patches/patch-xfce4-session_xfsm-shutdown-fallback.c
@@ -1,22 +1,22 @@
-$NetBSD: patch-xfce4-session_xfsm-shutdown-fallback.c,v 1.3 2022/02/06 17:42:47 gutteridge Exp $
+$NetBSD: patch-xfce4-session_xfsm-shutdown-fallback.c,v 1.4 2025/01/06 19:27:40 bsiegert Exp $
 
 Fix compilation on OSes other than Linux and the *BSDs.
 
---- xfce4-session/xfsm-shutdown-fallback.c.orig	2020-11-06 23:08:55.000000000 +0000
+--- xfce4-session/xfsm-shutdown-fallback.c.orig	2024-11-04 16:33:53.000000000 +0000
 +++ xfce4-session/xfsm-shutdown-fallback.c
-@@ -373,7 +373,9 @@ xfsm_shutdown_fallback_try_action (XfsmS
-                                    GError           **error)
+@@ -343,7 +343,9 @@ xfsm_shutdown_fallback_try_action (XfsmS
+                                    GError **error)
  {
    const gchar *xfsm_helper_action;
--  const gchar *cmd __attribute__((unused));
+-  const gchar *cmd __attribute__ ((unused));
 +#ifdef __BACKEND_TYPE_BSD__
 +  const gchar *cmd;
 +#endif
    gboolean ret = FALSE;
-   gint exit_status = 0;
  #ifdef HAVE_POLKIT
-@@ -384,29 +386,39 @@ xfsm_shutdown_fallback_try_action (XfsmS
-   {
+   gchar *command = NULL;
+@@ -353,23 +355,33 @@ xfsm_shutdown_fallback_try_action (XfsmS
+     {
      case XFSM_SHUTDOWN_SHUTDOWN:
        xfsm_helper_action = "shutdown";
 +#ifdef __BACKEND_TYPE_BSD__
@@ -34,28 +34,22 @@ Fix compilation on OSes other than Linux and the *BSDs.
 +#ifdef __BACKEND_TYPE_BSD__
        cmd = UP_BACKEND_SUSPEND_COMMAND;
 +#endif
-       /* On suspend we try to lock the screen */
-       if (!lock_screen (error))
-         return FALSE;
        break;
      case XFSM_SHUTDOWN_HIBERNATE:
        xfsm_helper_action = "hibernate";
 +#ifdef __BACKEND_TYPE_BSD__
        cmd = UP_BACKEND_HIBERNATE_COMMAND;
 +#endif
-       /* On hibernate we try to lock the screen */
-       if (!lock_screen (error))
-         return FALSE;
        break;
      case XFSM_SHUTDOWN_HYBRID_SLEEP:
        xfsm_helper_action = "hybrid-sleep";
 +#ifdef __BACKEND_TYPE_BSD__
        cmd = UP_BACKEND_HIBERNATE_COMMAND;
 +#endif
-       /* On hybrid sleep we try to lock the screen */
-       if (!lock_screen (error))
-         return FALSE;
-@@ -434,7 +446,7 @@ xfsm_shutdown_fallback_try_action (XfsmS
+       break;
+     default:
+       g_set_error (error, 1, 0, "Unknown shutdown type %d", type);
+@@ -394,7 +406,7 @@ xfsm_shutdown_fallback_try_action (XfsmS
  #endif
    if (!ret)
      {
--- x11/xfce4-session/patches/patch-xfsm-shutdown-helper_main.c
+++ x11/xfce4-session/patches/patch-xfsm-shutdown-helper_main.c
@@ -1,33 +1,33 @@
-$NetBSD: patch-xfsm-shutdown-helper_main.c,v 1.4 2019/05/23 02:56:07 gutteridge Exp $
+$NetBSD: patch-xfsm-shutdown-helper_main.c,v 1.5 2025/01/06 19:27:40 bsiegert Exp $
 
 Be careful to only SUSPEND or HIBERNATE when defined.
 
---- xfsm-shutdown-helper/main.c.orig	2019-05-05 22:00:21.000000000 +0000
+--- xfsm-shutdown-helper/main.c.orig	2024-10-11 05:36:33.000000000 +0000
 +++ xfsm-shutdown-helper/main.c
 @@ -183,22 +183,26 @@ main (int argc, char **argv)
      }
-   else if(suspend)
+   else if (suspend)
      {
 +#if defined(UP_BACKEND_SUSPEND_COMMAND)
        if (run (UP_BACKEND_SUSPEND_COMMAND))
-           {
-             return EXIT_CODE_SUCCESS;
-           }
-         else
+         {
+           return EXIT_CODE_SUCCESS;
+         }
+       else
 +#endif
-           {
-             return EXIT_CODE_FAILED;
-           }
+         {
+           return EXIT_CODE_FAILED;
+         }
      }
-   else if(hibernate)
+   else if (hibernate)
      {
 +#if defined (UP_BACKEND_HIBERNATE_COMMAND)
        if (run (UP_BACKEND_HIBERNATE_COMMAND))
-           {
-             return EXIT_CODE_SUCCESS;
-           }
-         else
+         {
+           return EXIT_CODE_SUCCESS;
+         }
+       else
 +#endif
-           {
-             return EXIT_CODE_FAILED;
-           }
+         {
+           return EXIT_CODE_FAILED;
+         }
