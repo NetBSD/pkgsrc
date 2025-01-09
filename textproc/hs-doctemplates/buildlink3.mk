324d923e469acf6a6accad9a144f139ab0970850
wiz <wiz@pkgsrc.org>
2025-01-09 23:29:14 +0000

    hs-doctemplates: update to 0.11.0.1.
    textproc/hs-doctemplates: update to doctemplates-0.11.0.1
    
    # 0.11.0.1
    
      * Bump version bounds for doclayout.
    
      * Fix typos.

--- textproc/hs-doctemplates/buildlink3.mk
+++ textproc/hs-doctemplates/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2025/01/09 23:24:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/01/09 23:29:14 wiz Exp $
 
 BUILDLINK_TREE+=	hs-doctemplates
 
 .if !defined(HS_DOCTEMPLATES_BUILDLINK3_MK)
 HS_DOCTEMPLATES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.11
-BUILDLINK_ABI_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.11nb6
+BUILDLINK_API_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.11.0
+BUILDLINK_ABI_DEPENDS.hs-doctemplates+=	hs-doctemplates>=0.11.0.1
 BUILDLINK_PKGSRCDIR.hs-doctemplates?=	../../textproc/hs-doctemplates
 
 .include "../../converters/hs-aeson/buildlink3.mk"
