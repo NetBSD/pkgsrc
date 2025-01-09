d6417c0ea747f2d3edf16c2b4562977942ada495
wiz <wiz@pkgsrc.org>
2025-01-09 23:47:29 +0000

    textproc/hs-hslua-module-doclayout: update to hslua-module-doclayout-1.2.0
    ## 1.2.0
    
    Released 2024-09-21.
    
    -   Require doclayout-0.5.
    
    -   Add new styling functions `bold`, `italic`, `underlined`,
        `strikeout`, `fg`, and `bg`.
    
    -   The `render` function now takes `style` as a third parameter.
        It must be either 'plain' or 'ansi' when given.
    
    ## 1.1.1.2
    
    Released 2024-06-10.
    
    -   Fixed another documentation bugs: `render` returns a `string`.
    
    ## 1.1.1.1
    
    Released 2024-06-09.
    
    -   Fixed minor typos in docstrings.

--- textproc/hs-hslua-module-doclayout/buildlink3.mk
+++ textproc/hs-hslua-module-doclayout/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2025/01/09 23:24:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/09 23:47:29 wiz Exp $
 
 BUILDLINK_TREE+=	hs-hslua-module-doclayout
 
 .if !defined(HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK)
 HS_HSLUA_MODULE_DOCLAYOUT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.1.1nb3
+BUILDLINK_API_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-doclayout+=	hs-hslua-module-doclayout>=1.2.0
 BUILDLINK_PKGSRCDIR.hs-hslua-module-doclayout?=		../../textproc/hs-hslua-module-doclayout
 
 .include "../../textproc/hs-doclayout/buildlink3.mk"
