78c6be6aad98b3fff1242237b3730228ac2fe6f9
ktnb <ktnb@pkgsrc.org>
2025-01-09 20:07:03 +0000

    screen: update to 5.0.0
    - Rewriten autentication mechanism
    - Add escape %T to show current tty for window
    - Add escape %O to show number of currently open windows
    - Use wcwdith() instead of UTF-8 hard-coded tables
      - New commands:
      - auth [on|off]
        Provides password protection
      - status [top|up|down|bottom] [left|right]
        The status window by default is in bottom-left corner
        This command can move status messages to any corner of the screen.
      - truecolor [on|off]
      - multiinput
        Input to multiple windows at the same time
    - Removed commands:
      - time
      - debug
      - password
      - maxwin
      - nethack
    - Fixes:
      - Screen buffers ESC keypresses indefinitely
      - Crashes after passing through a zmodem transfer
      - Fix double -U issue

--- misc/screen/patches/patch-socket.c
+++ misc/screen/patches/patch-socket.c
@@ -1,17 +1,16 @@
-$NetBSD: patch-socket.c,v 1.4 2023/09/06 16:00:15 vins Exp $
+$NetBSD: patch-socket.c,v 1.5 2025/01/09 20:07:04 ktnb Exp $
 
 Include <uio.h> for iovec. 
 
---- socket.c.orig	2017-07-10 19:26:25.000000000 +0000
-+++ socket.c	2017-07-18 22:35:40.000000000 +0000
-@@ -34,9 +34,7 @@
+--- socket.c.orig	2025-01-09 02:58:51.269455114 +0000
++++ socket.c
+@@ -35,9 +35,7 @@
  #include <sys/stat.h>
  #include <fcntl.h>
- # include <sys/socket.h>
--# ifdef _OpenBSD_
--#  include <sys/uio.h>
--# endif
-+# include <sys/uio.h>
- # include <sys/un.h>
- 
- #ifndef SIGINT
+ #include <sys/socket.h>
+-#ifdef _OpenBSD_
+ #include <sys/uio.h>
+-#endif
+ #include <sys/un.h>
+ #include <utime.h>
+ #include <stdint.h>
--- misc/screen/patches/patch-window.h
+++ misc/screen/patches/patch-window.h
@@ -1,17 +1,16 @@
-$NetBSD: patch-window.h,v 1.3 2021/02/12 22:53:14 cjep Exp $
+$NetBSD: patch-window.h,v 1.4 2025/01/09 20:07:04 ktnb Exp $
 
 OpenBSD grumbles without the socket definitions
 
---- window.h.orig	Wed Feb  5 20:09:38 2020
-+++ window.h	Thu Feb 11 21:27:43 2021
-@@ -33,6 +33,10 @@
+--- window.h.orig	2025-01-09 02:59:07.553319622 +0000
++++ window.h
+@@ -33,6 +33,9 @@
  #ifndef SCREEN_WINDOW_H
  #define SCREEN_WINDOW_H
  
 +#ifdef __OpenBSD__
 +#include <sys/socket.h>
 +#endif
-+
- /* keep this in sync with the initialisations in window.c */
- struct NewWindow
- {
+ #include "config.h"
+ 
+ #include "sched.h"
