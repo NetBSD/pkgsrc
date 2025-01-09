d181a69cc8a7584b604a27160834f128699befe3
wiz <wiz@pkgsrc.org>
2025-01-09 23:15:14 +0000

    i*: recursive bump for hs-emojis update

--- textproc/hs-djot/buildlink3.mk
+++ textproc/hs-djot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/05/09 01:32:41 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/01/09 23:15:15 wiz Exp $
 
 BUILDLINK_TREE+=	hs-djot
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-djot
 HS_DJOT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-djot+=	hs-djot>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-djot+=	hs-djot>=0.1.1.3nb1
+BUILDLINK_ABI_DEPENDS.hs-djot+=	hs-djot>=0.1.1.3nb2
 BUILDLINK_PKGSRCDIR.hs-djot?=	../../textproc/hs-djot
 
 .include "../../textproc/hs-doclayout/buildlink3.mk"
