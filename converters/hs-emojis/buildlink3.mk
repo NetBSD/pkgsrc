74410e23289c03c70f34a0cc10c83cccf527cc68
wiz <wiz@pkgsrc.org>
2025-01-09 23:13:41 +0000

    converters/hs-emojis: update to emojis-0.1.4.1
    0.1.4.1
    
        Use foldl' instead of foldr to avoid stack overflows.
    
    0.1.4
    
        Update emoji data.
        Export new function replaceEmojis, allowing replacement of emojis in a Text with alternate text.
        Drop support for ghc < 8.8.

--- converters/hs-emojis/buildlink3.mk
+++ converters/hs-emojis/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:31:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/09 23:13:41 wiz Exp $
 
 BUILDLINK_TREE+=	hs-emojis
 
 .if !defined(HS_EMOJIS_BUILDLINK3_MK)
 HS_EMOJIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1.3nb2
+BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1.4.1
 BUILDLINK_PKGSRCDIR.hs-emojis?=		../../converters/hs-emojis
 .endif	# HS_EMOJIS_BUILDLINK3_MK
 
