75418f7bb2a0443a486ed508cc9b9ff60da09901
wiz <wiz@pkgsrc.org>
2025-01-09 23:23:47 +0000

    textproc/hs-doclayout: update to doclayout-0.5
    ## 0.5
    
      * Extract existing HasChars definition into a module
        Text.DocLayout.HasChars (Evan Silberman).
    
      * Add a `build` method to the signature of HasChars [API change]
        (Evan Silberman). This has a default definition, and is only needed with
        the ANSI renderer, so existing users should not need to add anything to
        their HasChars instances.
    
      * Introduce support for ANSI-styled output (Evan Silberman) [API change].
        `renderPlain` and `renderANSI` are now exported; the old `render`
        is a synonym of `renderPlain`.  In addition, various functions are
        exported to add ANSI formatting (including bold, italics,
        underline, strikeout, links, and colors) to a Doc. The Attributed
        type is also now exported.
    
      * Change type of Block constructor, replacing `[a]` with `[Attributed a]`,
        which carries a Font along with an inner string type [API change]
        (Evan Silberman).
    
      * Introduce FlatDocs and use them for rendering (Evan Silberman).
        This is an internal concept, not part of the public API.
        FlatDoc is an "intermediate representation" for the Doc "interpreter".
        The general design is that any Doc can be turned into a list of FlatDocs
        that carry equivalent information. The main point of doing this
        is to replace the nested Styled and Linked Docs, which form a more
        complicated tree structure than previously existed in DocLayout.
    
      * Deprecate `unfoldD`.

--- textproc/hs-doclayout/buildlink3.mk
+++ textproc/hs-doclayout/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/01/09 23:15:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/09 23:23:47 wiz Exp $
 
 BUILDLINK_TREE+=	hs-doclayout
 
 .if !defined(HS_DOCLAYOUT_BUILDLINK3_MK)
 HS_DOCLAYOUT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.4.0
-BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.4.0.1nb3
+BUILDLINK_API_DEPENDS.hs-doclayout+=	hs-doclayout>=0.5
+BUILDLINK_ABI_DEPENDS.hs-doclayout+=	hs-doclayout>=0.5
 BUILDLINK_PKGSRCDIR.hs-doclayout?=	../../textproc/hs-doclayout
 
 .include "../../converters/hs-emojis/buildlink3.mk"
