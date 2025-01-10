0a83f080b69c2ca11959e7196f87f5773078a0a7
wiz <wiz@pkgsrc.org>
2025-01-10 10:12:25 +0000

    hs-pandoc-lua-marshal: update to 0.3.0.
    ## 0.3.0
    
    Released 2024-12-07.
    
      * Add method `normalize` to Pandoc objects.
        This returns a normalized document by merging adjacent spaces in inlines
        and by modifying tables.
    
      * Push Captions as userdata, move code to separate module.
    
      * Add tests for RawInline and its properties
    
      * Allow treatment of custom elements as singleton lists.
    
      * Remove `pandoc` prefix on table components (jgm/pandoc#8574).
    
    ## 0.2.9
    
    Released 2024-10-01.
    
    -   Update list module, thereby introducing a new method `iter`;
        the function returns an iterator that steps through list
        values on each call.
    
    -   Added support for `__toinline` and `__toblock` metamethods. If
        the metamethods are available on a Lua object and return an
        appropriate value, then that returned value will be used
        during unmarshalling.
    
    ## 0.2.8
    
    Released 2024-09-21.
    
    -   Update list module, thereby introducing a new method `at` and
        an extended constructor for List types.
    
    ## 0.2.7.1
    
    Released 2024-07-02.
    
    -   Relaxed the upper bound of tasty-quickcheck, used in tests.
        Now allows version 0.11.
    
    -   Update documentation on constructors; the descriptions now
        match those in the pandoc docs.
    
    ## 0.2.7
    
    Released 2024-05-06.
    
    -   Let the behavior of `content` attributes on BulletList and
        OrderedList elements match that of the constructor by treating
        a list of Block elements as a list of single-block items. The
        following assertion now holds true:
    
        ``` lua
        local content = {pandoc.Plain "one", pandoc.Plain "two"}
        local bl = pandoc.BulletList{}
        bl.content = content
        assert(bl == pandoc.BulletList(content))
        ```

--- devel/hs-pandoc-lua-marshal/buildlink3.mk
+++ devel/hs-pandoc-lua-marshal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/10 10:12:25 wiz Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-lua-marshal
 
 .if !defined(HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK)
 HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.6
-BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.2.6nb1
+BUILDLINK_API_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.3.0
+BUILDLINK_ABI_DEPENDS.hs-pandoc-lua-marshal+=	hs-pandoc-lua-marshal>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-pandoc-lua-marshal?=	../../devel/hs-pandoc-lua-marshal
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -16,6 +16,7 @@ BUILDLINK_PKGSRCDIR.hs-pandoc-lua-marshal?=	../../devel/hs-pandoc-lua-marshal
 .include "../../lang/hs-lua/buildlink3.mk"
 .include "../../textproc/hs-pandoc-types/buildlink3.mk"
 .include "../../devel/hs-safe/buildlink3.mk"
+.include "../../lang/hs-hslua-list/buildlink3.mk"
 .endif	# HS_PANDOC_LUA_MARSHAL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-pandoc-lua-marshal
