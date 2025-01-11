c201611ebab5b83f5ff7f96ce4b295f7d79f9bfe
ryoon <ryoon@pkgsrc.org>
2025-01-11 14:06:28 +0000

    misc/libreoffice: Fix build for NetBSD/i386

--- misc/libreoffice/patches/patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk
+++ misc/libreoffice/patches/patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk
@@ -1,12 +1,13 @@
-$NetBSD: patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk,v 1.3 2020/08/11 16:07:39 ryoon Exp $
+$NetBSD: patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk,v 1.4 2025/01/11 14:06:28 ryoon Exp $
 
---- external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk.orig	2020-07-29 19:29:17.000000000 +0000
+--- external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk.orig	2024-12-12 21:46:45.000000000 +0000
 +++ external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
-@@ -27,6 +27,7 @@ $(eval $(call gb_UnpackedTarball_set_pat
+@@ -27,6 +27,8 @@ $(eval $(call gb_UnpackedTarball_set_pat
  
  $(eval $(call gb_UnpackedTarball_add_patches,mariadb-connector-c,\
      external/mariadb-connector-c/clang-cl.patch.0 \
 +    external/mariadb-connector-c/netbsd.patch \
++    external/mariadb-connector-c/fix-debug-i386.patch.0 \
  ))
  
  # TODO are any "plugins" needed?
