7a1f2611b4047ecfbd0fbfb0faff9385c7b09ce7
wiz <wiz@pkgsrc.org>
2025-01-12 17:42:38 +0000

    qt6-qttools: buildlink files that are referenced from .cmake files

--- devel/qt6-qttools/buildlink3.mk
+++ devel/qt6-qttools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/12/26 22:20:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/12 17:42:38 wiz Exp $
 
 BUILDLINK_TREE+=	qt6-qttools
 
@@ -8,6 +8,13 @@ QT6_QTTOOLS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.qt6-qttools+=	qt6-qttools>=6.4.1
 BUILDLINK_ABI_DEPENDS.qt6-qttools+=	qt6-qttools>=6.8.1
 BUILDLINK_PKGSRCDIR.qt6-qttools?=	../../devel/qt6-qttools
+
+BUILDLINK_FILES.qt6-qttools+=		qt6/bin/lconvert
+BUILDLINK_FILES.qt6-qttools+=		qt6/bin/lrelease
+BUILDLINK_FILES.qt6-qttools+=		qt6/bin/lupdate
+BUILDLINK_FILES.qt6-qttools+=		qt6/libexec/lprodump
+BUILDLINK_FILES.qt6-qttools+=		qt6/libexec/lrelease-pro
+BUILDLINK_FILES.qt6-qttools+=		qt6/libexec/lupdate-pro
 
 .include "../../lang/qt6-qtdeclarative/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
