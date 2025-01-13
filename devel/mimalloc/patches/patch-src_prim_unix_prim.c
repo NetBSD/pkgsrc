522fa07dd9970ddf306f1400c9dccbcb3e8b1010
adam <adam@pkgsrc.org>
2025-01-13 10:35:58 +0000

    mimalloc: updated to 3.0.1
    3.0.1
    minor update

---
+++ devel/mimalloc/patches/patch-src_prim_unix_prim.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_prim_unix_prim.c,v 1.1 2025/01/13 10:35:59 adam Exp $
+
+Fix build.
+https://github.com/microsoft/mimalloc/issues/988
+
+--- src/prim/unix/prim.c.orig	2025-01-13 10:00:52.394506291 +0000
++++ src/prim/unix/prim.c
+@@ -201,7 +201,8 @@ static void* unix_mmap_prim(void* addr, 
+   void* p = NULL;
+   #if defined(MAP_ALIGNED)  // BSD
+   if (addr == NULL && try_alignment > 1 && (try_alignment % _mi_os_page_size()) == 0) {
+-    size_t n = mi_bsr(try_alignment);
++    size_t idx;
++    size_t n = mi_bsr(try_alignment, &idx);
+     if (((size_t)1 << n) == try_alignment && n >= 12 && n <= 30) {  // alignment is a power of 2 and 4096 <= alignment <= 1GiB
+       p = mmap(addr, size, protect_flags, flags | MAP_ALIGNED(n), fd, 0);
+       if (p==MAP_FAILED || !_mi_is_aligned(p,try_alignment)) {
