ce9cc19ea9e457f9ff7ac3a1a191cda4733f141c
wiz <wiz@pkgsrc.org>
2025-01-14 13:21:30 +0000

    cgal: since release 5.0, this is a header-only library
    Set default to build-dependency in buildlink3.mk.

--- math/cgal/buildlink3.mk
+++ math/cgal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.49 2025/01/11 03:23:02 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.50 2025/01/14 13:21:30 wiz Exp $
 
 BUILDLINK_TREE+=	cgal
 
@@ -8,6 +8,8 @@ CGAL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.cgal+=	cgal>=5.1
 BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=5.1
 BUILDLINK_PKGSRCDIR.cgal?=	../../math/cgal
+
+BUILDLINK_DEPMETHOD.cgal?=	build
 
 .include "../../devel/boost-headers/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
