213bab426c1af12b4e11876560888e7ff8de4fd4
adam <adam@pkgsrc.org>
2025-01-14 16:13:04 +0000

    cmake cmake-gui: updated to 3.31.4
    CMake 3.31.4
    * Tests/CXXModules: add a test with duplicate sources
    * target_sources: Restore toleration of duplicate CXX_MODULES sources
    * Makefile: List codegen target in help
    * FindBoost: Add support for Boost 1.87
    * GenEx: Revert "Limit TARGET_PROPERTY transitive closure optimization"
    * Help: FindPython: fix typo
    * LFortran: Remove hard-coded --generate-object-code flag

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.72 2024/12/24 13:54:01 adam Exp $
+# $NetBSD: version.mk,v 1.73 2025/01/14 16:13:04 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.3
+CMAKE_VERSION=	3.31.4
 CMAKE_API=	${CMAKE_VERSION:R}
