74415d1db4d17cc8673f83a4c84860e3e9571319
bacon <bacon@pkgsrc.org>
2025-01-16 01:06:31 +0000

    biology/ncbi-blast+: Update to 2.16.0
    Several minor improvements and bug fixes
    Changes: https://www.ncbi.nlm.nih.gov/books/NBK131777/

--- biology/ncbi-blast+/buildlink3.mk
+++ biology/ncbi-blast+/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/09/14 00:39:17 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/16 01:06:31 bacon Exp $
 
 BUILDLINK_TREE+=	ncbi-blast+
 
@@ -7,8 +7,8 @@ NCBI_BLAST+_BUILDLINK3_MK:=
 
 USE_CXX_FEATURES=			c++17
 
-BUILDLINK_API_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.14.1
-BUILDLINK_ABI_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.14.1
+BUILDLINK_API_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.16.0
+BUILDLINK_ABI_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.16.0
 BUILDLINK_PKGSRCDIR.ncbi-blast+?=	../../biology/ncbi-blast+
 
 .endif	# NCBI_BLAST+_BUILDLINK3_MK
