a2548cf16e821cc8b35f9f9c3d447e3f9335d9e2
adam <adam@pkgsrc.org>
2025-01-17 19:21:09 +0000

    bpython: updated to 0.25
    0.25
    
    General information:
    
    * The `bpython-cli` rendering backend has been removed following deprecation in
      version 0.19.
    * This release is focused on Python 3.13 support.
    
    New features:
    
    
    Fixes:
    
    * Fix __signature__ support
      Thanks to gpotter2
    * Fix handling of `SystemExit`
    * Improve order of completion results
      Thanks to gpotter2
    * Fix build of documentation and manpages with Sphinx >= 7
    * Do not fail if modules don't have __version__
    
    Changes to dependencies:
    
    * Remove use of distutils
      Thanks to Anderson Bravalheri
    
    Support for Python 3.12 and 3.13 has been added. Support for Python 3.7 and 3.8 has been dropped.

--- devel/bpython/patches/patch-setup.py
+++ devel/bpython/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.3 2021/03/20 15:14:48 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2025/01/17 19:21:09 adam Exp $
 
 Use the same man directory across all platforms.
 
---- setup.py.orig	2021-01-25 08:13:17.000000000 +0000
+--- setup.py.orig	2025-01-17 09:32:25.000000000 +0000
 +++ setup.py
-@@ -141,10 +141,7 @@ if using_sphinx:
-     build.sub_commands.insert(0, ("build_sphinx_man", None))
+@@ -344,10 +344,7 @@ if using_sphinx:
+     cmdclass["build_sphinx"] = BuildDoc
      cmdclass["build_sphinx_man"] = BuildDoc
  
 -    if platform.system() in ("FreeBSD", "OpenBSD"):
