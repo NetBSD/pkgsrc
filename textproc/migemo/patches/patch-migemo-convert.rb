96c0d3b8db77d3193a911a5011feb58a257776bd
taca <taca@pkgsrc.org>
2025-01-19 11:24:43 +0000

    textproc/migemo: overhaul the package
    Make build propery this pacakge.
    
    * Use older autoconf and automake in USE_TOOLS.
    * Drop completely "-Ke" option of Ruby.
    * Set Encoding.default_external to EUC-JP explictly.
    * Specify script encoding with magic comment.
    * Add comments in patch files.
    
    Bump PKGREVISION.

--- textproc/migemo/patches/patch-migemo-convert.rb
+++ textproc/migemo/patches/patch-migemo-convert.rb
@@ -1,13 +1,22 @@
-$NetBSD: patch-migemo-convert.rb,v 1.1 2012/08/12 11:52:26 obache Exp $
+$NetBSD: patch-migemo-convert.rb,v 1.2 2025/01/19 11:24:44 taca Exp $
+
+* Set default external encoding to EUC-JP.
+* Add specifying script encoding to EUC-JP.
+* Explictly set encoding of standard input to EUC-JP.
 
 --- migemo-convert.rb.orig	2003-05-26 06:55:22.000000000 +0000
 +++ migemo-convert.rb
 @@ -1,3 +1,4 @@
-+#coding: euc-jp
++# coding: euc-jp
  #
  # Ruby/Migemo - a library for Japanese incremental search.
  #
-@@ -14,12 +15,12 @@
+@@ -10,11 +11,11 @@
+ #
+ # NOTE: Ruby/Migemo can work only with EUC_JP encoding. ($KCODE="e")
+ #
++Encoding.default_external = Encoding::EUC_JP
+ 
  #
  # Convert a SKK's dictionary into Migemo's.
  #
@@ -15,9 +24,3 @@ $NetBSD: patch-migemo-convert.rb,v 1.1 2012/08/12 11:52:26 obache Exp $
  require 'romkan'
  
  HIRAGANA = "[¤¡-¤ó¡¼¡Á]"
- KANJI = "[°¡-ô¤]"
- 
-+$stdin.set_encoding(Encoding::EUC_JP)
- puts ";;"
- puts ";; This is Migemo's dictionary generated from SKK's."
- puts ";;"
