7656697280bf81b5511aa5a25e7666df2247f1e0
riastradh <riastradh@pkgsrc.org>
2025-01-19 19:27:37 +0000

    lang/go/bootstrap.mk: Use ${TOOLBASE}, not ${PREFIX}.
    Preparation for cross-compiling golang packages.  No change for
    native builds because in that case TOOLBASE and PREFIX are the same.

--- lang/go/bootstrap.mk
+++ lang/go/bootstrap.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bootstrap.mk,v 1.13 2024/08/03 19:28:29 bsiegert Exp $
+# $NetBSD: bootstrap.mk,v 1.14 2025/01/19 19:27:37 riastradh Exp $
 
 .if !defined(GOROOT_BOOTSTRAP) || !exists(${GOROOT_BOOTSTRAP}/bin/go)
 .  if ${MACHINE_ARCH} == "aarch64" || \
@@ -6,12 +6,12 @@
     (${OPSYS} == "FreeBSD" && ${OPSYS_VERSION} >= 140000) || \
     (${OPSYS} == "SunOS" && ${OS_VARIANT} != "Solaris")
 TOOL_DEPENDS+=		go-bin-[0-9]*:../../lang/go-bin
-GOROOT_BOOTSTRAP=	${PREFIX}/go-bin
+GOROOT_BOOTSTRAP=	${TOOLBASE}/go-bin
 .  elif defined(GO_BOOTSTRAP_REQD)
 TOOL_DEPENDS+=		go${GO_BOOTSTRAP_REQD}-[0-9]*:../../lang/go${GO_BOOTSTRAP_REQD}
-GOROOT_BOOTSTRAP=	${PREFIX}/go${GO_BOOTSTRAP_REQD}
+GOROOT_BOOTSTRAP=	${TOOLBASE}/go${GO_BOOTSTRAP_REQD}
 .  else
 TOOL_DEPENDS+=		go14-1.4*:../../lang/go14
-GOROOT_BOOTSTRAP=	${PREFIX}/go14
+GOROOT_BOOTSTRAP=	${TOOLBASE}/go14
 .  endif
 .endif
