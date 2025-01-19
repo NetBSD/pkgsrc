0ab08f911a71be7004a6c6fae68a95738244c740
wiz <wiz@pkgsrc.org>
2025-01-19 18:38:53 +0000

    devel/happy-lib: update to happy-lib-2.1.4
    ## 2.1.4
    
    Move `Paths_happy_lib` into `tabular` to prevent a Cabal bug concerning
    --libsubdir (#328). It is likely that this release fixes
    `cabal v1-install happy` as well, which was broken since happy-2.0 (#315).

--- devel/happy-lib/buildlink3.mk
+++ devel/happy-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/01/09 22:59:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/01/19 18:38:53 wiz Exp $
 
 BUILDLINK_TREE+=	happy-lib
 
 .if !defined(HAPPY_LIB_BUILDLINK3_MK)
 HAPPY_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.3
-BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.3
+BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.4
+BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.4
 BUILDLINK_PKGSRCDIR.happy-lib?=		../../devel/happy-lib
 .endif	# HAPPY_LIB_BUILDLINK3_MK
 
