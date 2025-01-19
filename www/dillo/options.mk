f30a5ef574166a151b73e21fc20d12107662e7ff
ktnb <ktnb@pkgsrc.org>
2025-01-19 15:34:43 +0000

    dillo: update to 3.2.0
    - optional support for webp images
    - new options for ignoring images
    - support for simple SVG
    - scrollbar navigation mode
    - zoom via Ctrl +/-
    - bug fixes

--- www/dillo/options.mk
+++ www/dillo/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.7 2024/05/07 18:06:35 abs Exp $
+# $NetBSD: options.mk,v 1.8 2025/01/19 15:34:43 ktnb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dillo
-PKG_SUPPORTED_OPTIONS=	inet6 ssl
-PKG_SUGGESTED_OPTIONS=	inet6 ssl
+PKG_SUPPORTED_OPTIONS=	inet6 ssl webp
+PKG_SUGGESTED_OPTIONS=	inet6 ssl webp
 
 .include "../../mk/bsd.options.mk"
 
@@ -23,4 +23,9 @@ SUBST_SED.sslcerts=	-e 's,"/etc/ssl/certs,"${SSLCERTS},'
 
 .else
 CONFIGURE_ARGS+=	--disable-ssl
+.endif
+
+.if !empty(PKG_OPTIONS:Mwebp)
+CONFIGURE_ARGS+=	--enable-webp
+.include "../../graphics/libwebp/buildlink3.mk"
 .endif
