9bf7f79738b1deb54b77b078c69628fd01e1deb5
gdt <gdt@pkgsrc.org>
2025-01-20 23:29:25 +0000

    cad/librecad: Update to 2.2.0
    This is a tiny update, from rc2 to the final release.
    
      - drop upstream patch now in upstream
      - drop fix for /usr to @PREFIX@ because upstream changed how they
        find paths, it clearly doesn't apply, and it looks like upstream's
        approach is ok

--- cad/librecad/patches/patch-scripts_postprocess-unix.sh
+++ cad/librecad/patches/patch-scripts_postprocess-unix.sh
@@ -1,13 +1,13 @@
-$NetBSD: patch-scripts_postprocess-unix.sh,v 1.4 2019/10/26 12:23:01 kamil Exp $
+$NetBSD: patch-scripts_postprocess-unix.sh,v 1.5 2025/01/20 23:29:25 gdt Exp $
 
 Fix QTDIR.
 
---- scripts/postprocess-unix.sh.orig	2018-02-15 20:54:45.000000000 +0000
+--- scripts/postprocess-unix.sh.orig	2022-12-17 11:13:13.000000000 +0000
 +++ scripts/postprocess-unix.sh
-@@ -8,7 +8,7 @@ APPDATADIR="${THISDIR}/unix/appdata"
- TSDIRLC="${LCDIR}/ts"
+@@ -9,7 +9,7 @@ TSDIRLC="${LCDIR}/ts"
  TSDIRPI="${PIDIR}/ts"
  SPTDIR="${LCDIR}/support"
+ DESKTOPDIR="${THISDIR}/desktop"
 -LRELEASE="lrelease"
 +LRELEASE="@QTDIR@/bin/lrelease"
  
