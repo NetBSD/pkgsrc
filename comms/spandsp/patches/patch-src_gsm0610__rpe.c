839b0cb6bd317e6b951f062e11eeaab3676812ba
mef <mef@pkgsrc.org>
2025-01-22 13:37:54 +0000

    (comms/spandsp) Updated 0.0.6pre21 (2012/08/04) to 0.0.6 (2014/07/09) ChangeLog not updated

--- comms/spandsp/patches/patch-src_gsm0610__rpe.c
+++ comms/spandsp/patches/patch-src_gsm0610__rpe.c
@@ -1,4 +1,12 @@
-$NetBSD: patch-src_gsm0610__rpe.c,v 1.1 2011/09/01 09:22:30 jnemeth Exp $
+$NetBSD: patch-src_gsm0610__rpe.c,v 1.2 2025/01/22 13:37:55 mef Exp $
+
+date: 2011-09-01 18:22:30 +0900;  author: jnemeth;  state: Exp;
+branches:  1.1.2;
+Add a patch for PR/44766.  The issue was that older versions of gas
+require you to use movd (instead of movq) when transferring data
+between reg32/64 and an mmx register.  No PKGREVISION bump since it
+failed to compile on amd64 meaning there was no binary package.
+----------------------------
 
 --- src/gsm0610_rpe.c.orig	2010-07-24 16:28:34.000000000 +0000
 +++ src/gsm0610_rpe.c
