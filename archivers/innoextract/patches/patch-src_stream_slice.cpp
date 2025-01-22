f07cb4e7f228a19c1337eafb756ac3393542be9c
wiz <wiz@pkgsrc.org>
2025-01-22 12:47:49 +0000

    innoextract: fix build

--- archivers/innoextract/patches/patch-src_stream_slice.cpp
+++ archivers/innoextract/patches/patch-src_stream_slice.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_stream_slice.cpp,v 1.1 2024/04/21 16:08:30 adam Exp $
+$NetBSD: patch-src_stream_slice.cpp,v 1.2 2025/01/22 12:47:49 wiz Exp $
 
 Fix build with newer boost.
+https://github.com/dscharrer/innoextract/commit/264c2fe6b84f90f6290c670e5f676660ec7b2387
 
 --- src/stream/slice.cpp.orig	2024-04-21 16:03:13.394516482 +0000
 +++ src/stream/slice.cpp
