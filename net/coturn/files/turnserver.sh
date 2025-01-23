2ebdf7d5a3574b72b1199f6d7af6651648835c39
adam <adam@pkgsrc.org>
2025-01-23 16:24:02 +0000

    coturn: cleanup, fix options, fix rc.d file, bump revision

--- net/coturn/files/turnserver.sh
+++ net/coturn/files/turnserver.sh
@@ -14,18 +14,39 @@
 # turnserver_config (path):	Set to @PREFIX@/etc/turnserver.conf
 #				by default.
 
-. /etc/rc.subr
+if [ -f @SYSCONFBASE@/rc.subr ]; then
+  . @SYSCONFBASE@/rc.subr
+fi
 
 name="turnserver"
-rcvar=$name
+rcvar=${name}
 
-load_rc_config $name
+if [ -f @SYSCONFBASE@/rc.subr -a -d @SYSCONFBASE@/rc.d -a -f @SYSCONFBASE@/rc.d/DAEMON ]; then
+	load_rc_config $name
+elif [ -f @SYSCONFBASE@/rc.conf ]; then
+	. @SYSCONFBASE@/rc.conf
+fi
 
 : ${turnserver:=no}
 : ${turnserver_config=@PREFIX@/etc/turnserver.conf}
 
+pidfile="@VARBASE@/run/${name}.pid"
 command="@PREFIX@/bin/${name}"
-command_args="--daemon -c ${turnserver_config}"
+command_args="--daemon -c ${turnserver_config} --pidfile ${pidfile}"
 required_files=${turnserver_config}
 
-run_rc_command "$1"
+if [ -f @SYSCONFBASE@/rc.subr -a -d @SYSCONFBASE@/rc.d -a -f @SYSCONFBASE@/rc.d/DAEMON ]; then
+	run_rc_command "$1"
+else
+	case "$1" in
+	stop)
+		if [ -r "${pidfile}" ]; then
+			@ECHO@ "Stopping ${name}."
+			kill `@CAT@ ${pidfile}`
+		fi
+		;;
+	*)
+		${command} ${command_args}
+		;;
+	esac
+fi
