a39d8c10c071b865a49b5c3e9e1aa2b34611baaf
adam <adam@pkgsrc.org>
2025-01-23 16:24:02 +0000

    coturn: cleanup, fix options, fix rc.d file, bump revision

--- net/coturn/options.mk
+++ net/coturn/options.mk
@@ -1,28 +1,31 @@
-# $NetBSD: options.mk,v 1.1 2025/01/15 14:13:39 manu Exp $
+# $NetBSD: options.mk,v 1.2 2025/01/23 16:24:02 adam Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.coturn
-PKG_SUPPORTED_OPTIONS=		hiredis mysql pgsql sqlite redis
-
-PKG_SUGGESTED_OPTIONS=		sqlite
+PKG_OPTIONS_VAR=	PKG_OPTIONS.coturn
+PKG_SUPPORTED_OPTIONS=	mysql pgsql sqlite redis
+PKG_SUGGESTED_OPTIONS=	sqlite
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mhiredis)
+.if !empty(PKG_OPTIONS:Mredis)
 .include "../../databases/hiredis/buildlink3.mk"
+.else
+CONFIGURE_ENV+=		TURN_NO_HIREDIS=1
 .endif
 
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../mk/mysql.buildlink3.mk"
+.else
+CONFIGURE_ENV+=		TURN_NO_MYSQL=1
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../mk/pgsql.buildlink3.mk"
+.else
+CONFIGURE_ENV+=		TURN_NO_PQ=1
 .endif
 
 .if !empty(PKG_OPTIONS:Msqlite)
 .include "../../databases/sqlite3/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mredis)
-DEPENDS+=	redis-[0-9]*:../../databases/redis
+.else
+CONFIGURE_ENV+=		TURN_NO_SQLITE=1
 .endif
