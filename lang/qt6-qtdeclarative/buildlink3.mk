9f2e3510ee5b5c125a06070a7bd00cce2104dac3
riastradh <riastradh@pkgsrc.org>
2025-01-24 07:34:06 +0000

    lang/qt6-qtdeclarative: Expose some qt6/* files to buildlink3.
    Needed by downstream packages like kf6-ki18n once we hide PREFIX from
    cmake.
    
    XXX These patterns are kinda hokey, feel free to do it another way.
    
    Will resolve various issues like:
    
    CMake Error at /tmp/pkgbuild/2024Q4/devel/kf6-ki18n/work/.buildlink/qt6/lib/cmake/Qt6Qml/Qt6QmlMacros.cmake:2610 (target_link_libraries):
      Cannot find source file:
    
        /tmp/pkgbuild/2024Q4/devel/kf6-ki18n/work/.buildlink/qt6/metatypes/qt6qml_release_metatypes.json
    
      Tried extensions .c .C .c++ .cc .cpp .cxx .cu .mpp .m .M .mm .ixx .cppm
      .ccm .cxxm .c++m .h .hh .h++ .hm .hpp .hxx .in .txx .f .F .for .f77 .f90
      .f95 .f03 .hip .ispc
    
    CMake Error at /home/riastradh/pkgsrc/current/work/devel/kf6-ki18n/work/.buildlink/qt6/lib/cmake/Qt6QmlAssetDownloader/Qt6QmlAssetDownloaderTargets.cmake:133 (message):
      The imported target "Qt6::QmlAssetDownloader_resources_1" references the
      file
    
         "/home/riastradh/pkgsrc/current/work/devel/kf6-ki18n/work/.buildlink/qt6/qml/Assets/Downloader/objects-Release/QmlAssetDownloader_resources_1/.qt/rcc/qrc_qmake_Assets_Downloader_init.cpp.o"
    
      but this file does not exist.  Possible reasons include: [...]
    
    CMake Error at /home/riastradh/pkgsrc/current/work/devel/kf6-ki18n/work/.buildlink/qt6/lib/cmake/Qt6Qml/QmlPlugins/Qt6QmlAssetDownloaderpluginTargets.cmake:108 (message):
      The imported target "Qt6::QmlAssetDownloaderplugin_init" references the
      file
    
         "/home/riastradh/pkgsrc/current/work/devel/kf6-ki18n/work/.buildlink/qt6/qml/Assets/Downloader/objects-Release/QmlAssetDownloaderplugin_init/QmlAssetDownloaderplugin_init.cpp.o"
    
      but this file does not exist.  Possible reasons include: [...]

--- lang/qt6-qtdeclarative/buildlink3.mk
+++ lang/qt6-qtdeclarative/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2024/12/26 22:20:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/01/24 07:34:06 riastradh Exp $
 
 BUILDLINK_TREE+=	qt6-qtdeclarative
 
@@ -12,6 +12,14 @@ BUILDLINK_PKGSRCDIR.qt6-qtdeclarative?=		../../lang/qt6-qtdeclarative
 BUILDLINK_INCDIRS.qt6-qtdeclarative+=	qt6/include
 BUILDLINK_LIBDIRS.qt6-qtdeclarative+=	qt6/lib
 BUILDLINK_LIBDIRS.qt6-qtdeclarative+=	qt6/plugins
+
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/metatypes/*
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/qml/*
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/qml/*/*
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/qml/*/*/*
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/qml/*/*/*/*/*
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/qml/*/*/*/*/*/*
+BUILDLINK_FILES.qt6-qtdeclarative+=	qt6/qml/*/*/*/*/.qt/*/*
 
 .include "../../graphics/qt6-qtimageformats/buildlink3.mk"
 .include "../../graphics/qt6-qtshadertools/buildlink3.mk"
