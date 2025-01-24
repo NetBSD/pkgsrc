4e46be120c941ee1e4cd1b4b8938ca923c02aa3f
riastradh <riastradh@pkgsrc.org>
2025-01-24 20:40:54 +0000

    x11/libdrm/buildlink3.mk: Pull in libpciaccess on x86.
    This matches what the Makefile does.
    
    Resolves the following annoying warnings (possibly harmful, unclear)
    when building, e.g., graphics/wld:
    
    Package pciaccess was not found in the pkg-config search path.
    Perhaps you should add the directory containing `pciaccess.pc'
    to the PKG_CONFIG_PATH environment variable
    Package 'pciaccess', required by 'libdrm_intel', not found

--- x11/libdrm/buildlink3.mk
+++ x11/libdrm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/05/16 05:43:14 richard Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/24 20:40:54 riastradh Exp $
 
 BUILDLINK_TREE+=	libdrm
 
@@ -8,6 +8,10 @@ LIBDRM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.4.15
 BUILDLINK_PKGSRCDIR.libdrm?=	../../x11/libdrm
 BUILDLINK_INCDIRS.libdrm?=	include/libdrm
+
+.  if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
+.    include "../../sysutils/libpciaccess/buildlink3.mk"
+.  endif
 .endif # LIBDRM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdrm
