b044f4076ba558a367f49cd63492a009d999cf29
wiz <wiz@pkgsrc.org>
2025-01-27 22:21:34 +0000

    fltk13: add upper bound in buildlink3.mk
    so this doesn't accidentally pick up wip/fltk14-devel binary packages

--- x11/fltk13/buildlink3.mk
+++ x11/fltk13/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.19 2023/11/12 13:24:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2025/01/27 22:21:34 wiz Exp $
 
 BUILDLINK_TREE+=	fltk
 
 .if !defined(FLTK_BUILDLINK3_MK)
 FLTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0
+BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0<1.4.0
 BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.8nb3
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
 BUILDLINK_FILES.fltk+=		include/Fl/*
