41ce6570d37223bd107fd3dad4febf2c54879278
pho <pho@pkgsrc.org>
2025-01-27 15:08:07 +0000

    lang/ghc98: Replace bootkits with GHC 9.8.2
    NFCI where they work. The resulting compiler produces the same binary as
    before so recursive revbump is not required. This is actually a rework of
    my previous changes that I had to revert.
    
    Replacing them with 9.8.x is necessary for the upcoming GHC 9.10. I'm going
    to import lang/ghc910, update every Haskell package in pkgsrc, then switch
    the default compiler after that.
    
    As a bonus this also contains a fix for one of the long-standing issues on
    Darwin: GHC was failing to build any Haskell packages that contain C++
    code. This affected security/hs-digest and therefore Pandoc too.
    
    Note that GHC 9.8.2 is going to be the last version that supports
    FreeBSD/i386. The upstream has apparently stopped caring about i386
    platforms and I'm already seeing several signs of it bitrotting away. In
    fact ghci, the REPL for GHC, has already stopped working for years, because
    it immediately segfaults by exhausting the ridiculously small stack space
    of i386. But I have received zero complaints so far which probably means we
    can just let it go.

---
+++ lang/ghc98/patches/patch-hadrian_src_Settings_Builders_Cabal.hs
@@ -0,0 +1,29 @@
+$NetBSD: patch-hadrian_src_Settings_Builders_Cabal.hs,v 1.1 2025/01/27 15:08:08 pho Exp $
+
+Under no circumstances we should build static GHCi objects because:
+
+  1. The only case they need to be built is when we build GHC
+     statically. We only do it for bootkits but not for regular
+     builds. Bootkits don't need GHCi, so there's no point building them.
+
+  2. We can't build them on Solaris when -fsplit-sections is enabled, which
+     we enable. Solaris LD doesn't support linker scripts. Nothing fancy,
+     like merging split sections back to their original form, can be done
+     with that exotic linker.
+
+This is more like pkgsrc-specific than a genuine defect of the upstream
+code, so don't upstream this. There should ideally be a switch, such as
+"ghciLibraries :: Bool" in Flavour.Type.Flavour to support our use case,
+but... does anyone aside of pkgsrc want it?
+
+--- hadrian/src/Settings/Builders/Cabal.hs.orig	2025-01-27 01:04:45.571870819 +0000
++++ hadrian/src/Settings/Builders/Cabal.hs
+@@ -143,7 +143,7 @@ libraryArgs = do
+     package     <- getPackage
+     withGhci    <- expr ghcWithInterpreter
+     dynPrograms <- expr (flavour >>= dynamicGhcPrograms)
+-    ghciObjsSupported <- expr platformSupportsGhciObjects
++    let ghciObjsSupported = False
+     let ways = Set.insert contextWay flavourWays
+         hasVanilla = vanilla `elem` ways
+         hasProfiling = any (wayUnit Profiling) ways
