195839b8cf5b1527ab685fad1f2cc26adbc34600
wiz <wiz@pkgsrc.org>
2025-01-27 14:12:17 +0000

    pchar: pkglint cleanup

--- net/pchar/patches/patch-PctestIpv6Tcp.cc
+++ net/pchar/patches/patch-PctestIpv6Tcp.cc
@@ -1,6 +1,7 @@
-$NetBSD: patch-PctestIpv6Tcp.cc,v 1.1 2014/02/24 12:49:56 wiedi Exp $
+$NetBSD: patch-PctestIpv6Tcp.cc,v 1.2 2025/01/27 14:12:17 wiz Exp $
 
 don't define _XOPEN_SOURCE and __EXTENSIONS__ at the same time
+
 --- PctestIpv6Tcp.cc.orig	2005-02-12 20:32:18.000000000 +0000
 +++ PctestIpv6Tcp.cc
 @@ -22,7 +22,6 @@ static char rcsid[] = "$Id: PctestIpv6Tc
