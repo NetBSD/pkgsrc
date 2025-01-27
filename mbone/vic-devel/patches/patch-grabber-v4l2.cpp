adff0a7b3b9c6918b48c5f284bb0c8bae5960fdd
wiz <wiz@pkgsrc.org>
2025-01-27 16:35:10 +0000

    vic-devel: fix build
    Work around
    video/grabber-v4l2.cpp:164:9: error: '__u32' does not name a type

--- mbone/vic-devel/patches/patch-grabber-v4l2.cpp
+++ mbone/vic-devel/patches/patch-grabber-v4l2.cpp
@@ -1,7 +1,7 @@
-$NetBSD: patch-grabber-v4l2.cpp,v 1.2 2014/01/14 17:07:37 prlw1 Exp $
+$NetBSD: patch-grabber-v4l2.cpp,v 1.3 2025/01/27 16:35:10 wiz Exp $
 
---- video/grabber-v4l2.cpp.orig	2008-12-13 22:12:50.000000000 +0000
-+++ video/grabber-v4l2.cpp	2008-12-13 22:14:26.000000000 +0000
+--- video/grabber-v4l2.cpp.orig	2008-12-10 19:23:08.000000000 +0000
++++ video/grabber-v4l2.cpp
 @@ -34,7 +34,9 @@
  #include <string.h>
  #include <signal.h>
@@ -25,3 +25,13 @@ $NetBSD: patch-grabber-v4l2.cpp,v 1.2 2014/01/14 17:07:37 prlw1 Exp $
  }
  
  #ifdef HAVE_LIBV4L
+@@ -108,6 +114,9 @@ static const char *devlist[] = {
+ #define V4L2_CID_FOCUS_RELATIVE       (V4L2_CID_PRIVATE_BASE + 6)
+ #endif
+ 
++#undef __u32
++#define __u32 uint32_t
++
+ typedef struct tag_vimage
+ {
+         struct v4l2_buffer      vidbuf;
