7cc034135b6fbacc79f78894c8196c1e529bdc4e
riastradh <riastradh@pkgsrc.org>
2025-01-27 13:37:06 +0000

    security/uacme: Fix cross-build and ctype(3) abuse.
    1. TOOL_DEPENDS, not BUILD_DEPENDS, for asciidoc -- we run this at
       build-time, we don't link against it.
    
    2. Use AC_CACHE_CHECK around AC_RUN_IFELSE to allow overriding
       run-time tests at build-time with pre-determined outputs.  On
       NetBSD, mmap(MAP_ANON|MAP_SHARED) works (for that matter, I have
       no idea on what broken systems this fails).
    
    3. Patch away ctype(3) abuse while here.

--- security/uacme/patches/patch-uacme.c
+++ security/uacme/patches/patch-uacme.c
@@ -1,8 +1,10 @@
-$NetBSD: patch-uacme.c,v 1.1 2024/05/02 13:34:56 hauke Exp $
+$NetBSD: patch-uacme.c,v 1.2 2025/01/27 13:37:07 riastradh Exp $
 
-Set CONFDIR default - we prefer not to write under /etc
+1. Set CONFDIR default - we prefer not to write under /etc
 
---- uacme.c.orig	2024-04-24 20:06:00.457609312 +0000
+2. Fix ctype(3) abuse.
+
+--- uacme.c.orig	2024-01-28 20:03:31.000000000 +0000
 +++ uacme.c
 @@ -46,7 +46,7 @@
  
@@ -13,3 +15,30 @@ Set CONFDIR default - we prefer not to write under /etc
  
  typedef struct acme {
      privkey_t key;
+@@ -866,7 +866,7 @@ bool authorize(acme_t *a)
+                     goto out;
+                 }
+                 for (const char *t = token; *t; t++)
+-                    if (!isalnum(*t) && *t != '-' && *t != '_') {
++                    if (!isalnum((unsigned char)*t) && *t != '-' && *t != '_') {
+                         warnx("failed to validate token");
+                         goto out;
+                     }
+@@ -1301,7 +1301,7 @@ bool validate_identifier_str(const char 
+                 }
+                 break;
+             default:
+-                if (!isupper(s[j]) && !islower(s[j]) && !isdigit(s[j])) {
++                if (!isupper((unsigned char)s[j]) && !islower((unsigned char)s[j]) && !isdigit((unsigned char)s[j])) {
+                     warnx("invalid character '%c' in %s", s[j], s);
+                     return false;
+                 }
+@@ -1359,7 +1359,7 @@ bool alt_parse(acme_t *a, char *alt)
+     size_t len = 0;
+     char *tok = strtok(alt, ":");
+     while (tok && len < sizeof(a->alt_fp)) {
+-        if (strlen(tok) != 2 || !isxdigit(tok[0]) || !isxdigit(tok[1]))
++        if (strlen(tok) != 2 || !isxdigit((unsigned char)tok[0]) || !isxdigit((unsigned char)tok[1]))
+             break;
+         a->alt_fp[len++] = strtol(tok, NULL, 16);
+         tok = strtok(NULL, ":");
