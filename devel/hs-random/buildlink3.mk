82198da87a25f0fcea6754e51950b4345c841f14
pho <pho@pkgsrc.org>
2025-01-29 16:56:33 +0000

    devel/hs-random: update to random-1.3.0
    # 1.3.0
    
    * Improve floating point value generation and avoid degenerate cases: [#172](https://github.com/haskell/random/pull/172)
    * Add `Uniform` instance for `Maybe` and `Either`: [#167](https://github.com/haskell/random/pull/167)
    * Add `Seed`, `SeedGen`, `seedSize`, `seedSizeProxy`, `mkSeed` and `unSeed`:
      [#162](https://github.com/haskell/random/pull/162)
    * Add `mkSeedFromByteString`, `unSeedToByteString`, `withSeed`, `withSeedM`, `withSeedFile`,
      `seedGenTypeName`, `nonEmptyToSeed`, `nonEmptyFromSeed`, `withSeedM`, `withSeedMutableGen` and `withSeedMutableGen_`
    * Add `SplitGen` and `splitGen`: [#160](https://github.com/haskell/random/pull/160)
    * Add `unifromShuffleList` and `unifromShuffleListM`: [#140](https://github.com/haskell/random/pull/140)
    * Add `uniformWordR`: [#140](https://github.com/haskell/random/pull/140)
    * Add `mkStdGen64`: [#155](https://github.com/haskell/random/pull/155)
    * Add `uniformListRM`, `uniformList`, `uniformListR`, `uniforms` and `uniformRs`:
      [#154](https://github.com/haskell/random/pull/154)
    * Add compatibility with recently added `ByteArray` to `base`:
      [#153](https://github.com/haskell/random/pull/153)
      * Switch to using `ByteArray` for type class implementation instead of
        `ShortByteString`
      * Add `unsafeUniformFillMutableByteArray` to `RandomGen` and a helper function
        `defaultUnsafeUniformFillMutableByteArray` that makes implementation
        for most instances easier.
      * Add `uniformByteArray`, `uniformByteString` and `uniformFillMutableByteArray`
      * Deprecate `genByteString` in favor of `uniformByteString`
      * Add `uniformByteArrayM` to `StatefulGen`
      * Add `uniformByteStringM` and `uniformShortByteStringM`
      * Deprecate `System.Random.Stateful.uniformShortByteString` in favor of `uniformShortByteStringM` for
        consistent naming and a future plan of removing it from `StatefulGen`
        type class
      * Add a pure `System.Random.uniformShortByteString` generating function.
      * Deprecate `genShortByteString` in favor of `System.Random.uniformShortByteString`
      * Expose a helper function `fillByteArrayST`, that can be used for
        defining implementation for `uniformByteArrayM`
      * Deprecate `genShortByteStringST` and `genShortByteStringIO` in favor of `fillByteArrayST`
    * Improve `FrozenGen` interface: [#149](https://github.com/haskell/random/pull/149)
      * Move `thawGen` from `FreezeGen` into the new `ThawGen` type class. Fixes an issue with
        an unlawful instance of `StateGen` for `FreezeGen`.
      * Add `modifyGen` and `overwriteGen` to the `FrozenGen` type class
      * Switch `splitGenM` to use `SplitGen` and `FrozenGen` instead of deprecated `RandomGenM`
      * Add `splitMutableGenM`
      * Switch `randomM` and `randomRM` to use `FrozenGen` instead of `RandomGenM`
      * Deprecate `RandomGenM` in favor of a more powerful `FrozenGen`
    * Add `isInRangeOrd` and `isInRangeEnum` that can be used for implementing `isInRange`:
      [#148](https://github.com/haskell/random/pull/148)
    * Add `isInRange` to `UniformRange`: [#78](https://github.com/haskell/random/pull/78)
    * Add default implementation for `uniformRM` using `Generics`:
      [#92](https://github.com/haskell/random/pull/92)

--- devel/hs-random/buildlink3.mk
+++ devel/hs-random/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2024/05/09 01:32:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2025/01/29 16:56:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-random
 
 .if !defined(HS_RANDOM_BUILDLINK3_MK)
 HS_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.2.1
-BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.2.1.2nb1
+BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.3.0
+BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.3.0
 BUILDLINK_PKGSRCDIR.hs-random?=		../../devel/hs-random
 
 .include "../../devel/hs-splitmix/buildlink3.mk"
