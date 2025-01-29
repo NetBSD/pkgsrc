b87c5869eac0139bc9bf951dbebcc54f00a28a0f
pho <pho@pkgsrc.org>
2025-01-29 17:30:27 +0000

    devel/hs-mono-traversable: update to mono-traversable-1.0.21.0
    ## 1.0.21.0
    
    * Support for vector 0.13.2.0, adding instances for [`Data.Vector.Strict`](https://hackage.haskell.org/package/vector-0.13.2.0/docs/Data-Vector-Strict.html) data structure.
      [#244](https://github.com/snoyberg/mono-traversable/issues/244)
    
    ## 1.0.20.0
    
    * Added instances for [`Reverse`](https://hackage.haskell.org/package/transformers-0.6.1.1/docs/Data-Functor-Reverse.html#t:Reverse) data structure.
    
    ## 1.0.19.1
    
    * Removed 'highly experimental' warning haddock comment from Data.Containers.
    
    ## 1.0.19.0
    
    * Added `filterWithKey` to `IsMap`.
      [#232](https://github.com/snoyberg/mono-traversable/pull/232)
    
    ## 1.0.18.0
    
    * Added MonoPointed instance for text Builder
      [#225](https://github.com/snoyberg/mono-traversable/pull/225)

--- devel/hs-mono-traversable/buildlink3.mk
+++ devel/hs-mono-traversable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2024/05/09 01:32:04 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/01/29 17:30:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-mono-traversable
 
 .if !defined(HS_MONO_TRAVERSABLE_BUILDLINK3_MK)
 HS_MONO_TRAVERSABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.17.0
-BUILDLINK_ABI_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.17.0nb1
+BUILDLINK_API_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.21
+BUILDLINK_ABI_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.21.0
 BUILDLINK_PKGSRCDIR.hs-mono-traversable?=	../../devel/hs-mono-traversable
 
 .include "../../devel/hs-hashable/buildlink3.mk"
