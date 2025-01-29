fa423c7052db3f195adb25bf31574cc7ce2af8b8
pho <pho@pkgsrc.org>
2025-01-29 16:43:05 +0000

    devel/hs-hashable: update to hashable-1.5.0.0
    ## Version 1.5.0.0
    
      * Add `QuantifiedConstraints` superclasses to `Hashable1/2`:
    
    ```haskell
    class (Eq1 t, forall a. Hashable a => Hashable (t a)) => Hashable1 t where
    class (Eq2 t, forall a. Hashable a => Hashable1 (t a)) => Hashable2 t where
    ```
    
      * Change contexts of `Compose`, `Product` and `Sum` instances.
        This and above is the similar change as [CLC proposal #10](https://github.com/haskell/core-libraries-committee/issues/10)
    
      * The above changes require `base-4.18.0.0`, so we drop support for GHC prior GHC-9.6.5
        (The `hashable-1.4` branch will be maintained for time being for older GHC users).
    
      * Make `Arg a b` instance behave as `Hashable a` instance.
    
    ## Version 1.4.7.0
    
      * Make `arch-native` disabled by default.
    
    ## Version 1.4.6.0
    
      * Use GND&DerivingVia to derive `newtype` intances (`Data.Semigroup`, `Data.Monoid`, `Identity` etc).
    
    ## Version 1.4.5.0
    
      * Drop support for GHCs prior 8.6.5
      * Use xxhash for hashing bytestrings and bytearrays.
        Note: when compiling binaries for distribution, you may need to disable
        `arch-native` flag.

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.26 2024/05/09 01:31:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2025/01/29 16:43:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.4.4
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.4.4.0nb1
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.5.0
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.5.0.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
-
-.include "../../devel/hs-os-string/buildlink3.mk"
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hashable
