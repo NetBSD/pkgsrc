8f8f09d43f0495d099c0baf40b79c3ab293b6d3a
pho <pho@pkgsrc.org>
2025-01-29 19:19:35 +0000

    devel/hs-th-abstraction: update to th-abstraction-0.7.1.0
    ## 0.7.1.0 -- 2024.12.05
    * Drop support for pre-8.0 versions of GHC.

--- devel/hs-th-abstraction/buildlink3.mk
+++ devel/hs-th-abstraction/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/29 19:19:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-abstraction
 
 .if !defined(HS_TH_ABSTRACTION_BUILDLINK3_MK)
 HS_TH_ABSTRACTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.7.0.0nb1
+BUILDLINK_API_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-th-abstraction+=	hs-th-abstraction>=0.7.1.0
 BUILDLINK_PKGSRCDIR.hs-th-abstraction?=		../../devel/hs-th-abstraction
 .endif	# HS_TH_ABSTRACTION_BUILDLINK3_MK
 
