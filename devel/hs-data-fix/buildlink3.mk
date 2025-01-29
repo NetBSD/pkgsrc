7dcc5e328aaf180720c730db8369618f79cbae7a
pho <pho@pkgsrc.org>
2025-01-29 17:38:32 +0000

    devel/hs-data-fix: update to data-fix-0.3.4
    ## 0.3.4
    
    - Use quantified constraints superclasses for `Eq`, `Ord`, `NFData` and
      `Hashable Fix` instances, when available.
    
    ## 0.3.3
    
    - Drop support for GHCs prior 8.6.5

--- devel/hs-data-fix/buildlink3.mk
+++ devel/hs-data-fix/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:31:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/29 17:38:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-fix
 
 .if !defined(HS_DATA_FIX_BUILDLINK3_MK)
 HS_DATA_FIX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.2
-BUILDLINK_ABI_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.2nb6
+BUILDLINK_API_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.4
+BUILDLINK_ABI_DEPENDS.hs-data-fix+=	hs-data-fix>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-data-fix?=	../../devel/hs-data-fix
 
 .include "../../devel/hs-hashable/buildlink3.mk"
