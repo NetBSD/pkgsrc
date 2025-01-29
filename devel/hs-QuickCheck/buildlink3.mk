0a56255fee700a0122f0771661033712c53ed06c
pho <pho@pkgsrc.org>
2025-01-29 17:36:52 +0000

    devel/hs-QuickCheck: update to QuickCheck-2.15.0.1
    No release notes have been provided by the upstream.

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2024/05/09 01:31:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2025/01/29 17:36:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.15
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.15nb1
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.15.0
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.15.0.1
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
