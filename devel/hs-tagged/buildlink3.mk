66c4d591f443013942babb2b3f6051e8af4188bf
pho <pho@pkgsrc.org>
2025-01-29 17:04:18 +0000

    devel/hs-tagged: update to tagged-0.8.9
    0.8.9 [2024.12.03]
    ------------------
    * Allow building with GHC 9.12.
    * Drop support for GHC 7.10 and earlier.

--- devel/hs-tagged/buildlink3.mk
+++ devel/hs-tagged/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/29 17:04:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-tagged
 
 .if !defined(HS_TAGGED_BUILDLINK3_MK)
 HS_TAGGED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.8.8
-BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.8.8nb2
+BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.8.9
+BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.8.9
 BUILDLINK_PKGSRCDIR.hs-tagged?=		../../devel/hs-tagged
 .endif	# HS_TAGGED_BUILDLINK3_MK
 
