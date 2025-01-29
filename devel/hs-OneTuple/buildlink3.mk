955e8589ae4ba2dad9d558b8f39b83ad0b6baaae
pho <pho@pkgsrc.org>
2025-01-29 17:34:52 +0000

    devel/hs-OneTuple: update to OneTuple-0.4.2
    No release notes have been provided by the upstream.

--- devel/hs-OneTuple/buildlink3.mk
+++ devel/hs-OneTuple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/05/09 01:31:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/01/29 17:34:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-OneTuple
 
 .if !defined(HS_ONETUPLE_BUILDLINK3_MK)
 HS_ONETUPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.4.1.1nb2
+BUILDLINK_API_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-OneTuple+=	hs-OneTuple>=0.4.2
 BUILDLINK_PKGSRCDIR.hs-OneTuple?=	../../devel/hs-OneTuple
 .endif	# HS_ONETUPLE_BUILDLINK3_MK
 
