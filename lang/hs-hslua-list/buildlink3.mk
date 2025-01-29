04fea6f9b2c455d2dae915269bb8463878972a3a
pho <pho@pkgsrc.org>
2025-01-29 15:31:33 +0000

    lang/hs-hslua-list: update to hslua-list-1.1.4
    ## hslua-list-1.1.4
    
    Released 2024-10-01.
    
    -   Added function `List.iter`; it returns an iterator function
        that returns the next list item each time it is called.
    
    -   `List.extend` now returns the extended list instead of the
        list that was appended.
    
    ## hslua-list-1.1.3
    
    Released 2024-09-26.
    
    -   Fixed a bug that prevented `List.filter` to be used on tables
        that don't have a metatable.
    
    -   Added a stub function to ensure that the module can be
        compiled against Lua 5.3.
    
    -   Lowered the lower bound for hslua-core, allowing hslua-core
        2.1.
    
    ## hslua-list-1.1.2
    
    Released 2024-09-20.
    
    -   Lists can now be constructed from iterators.
    
    -   Added method `:at` to access list elements by index; negative
        indices are counted from the end. A second value can be
        passed, which will be returned if there is no item at the
        given index.

--- lang/hs-hslua-list/buildlink3.mk
+++ lang/hs-hslua-list/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/29 15:31:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-list
 
 .if !defined(HS_HSLUA_LIST_BUILDLINK3_MK)
 HS_HSLUA_LIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.1nb2
+BUILDLINK_API_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.4
+BUILDLINK_ABI_DEPENDS.hs-hslua-list+=	hs-hslua-list>=1.1.4
 BUILDLINK_PKGSRCDIR.hs-hslua-list?=	../../lang/hs-hslua-list
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
