599138424a9cff0b0458ca70af70d9abc7ba2acd
pho <pho@pkgsrc.org>
2025-01-29 19:17:28 +0000

    devel/hs-base-orphans: update to 0.9.3
    ## Changes in 0.9.3 [2024.12.06]
     - Backport new instances from GHC 9.12/`base-4.21`:
       * `MonadFix` instance for `(,) a`
       * `Eq1`, `Ord1`, `Read1`, and `Show1` instances for basic `GHC.Generics`
         representation types
       * `Show` instance for `UAddr`
     - Drop support for pre-8.0 versions of GHC.
    
    ## Changes in 0.9.2 [2024.04.30]
     - Backport new instances from GHC 9.10/`base-4.20`:
       * `Fractional`, `RealFrac`, `Floating`, and `RealFloat` instances for `Compose`

--- devel/hs-base-orphans/buildlink3.mk
+++ devel/hs-base-orphans/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:31:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/29 19:17:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-orphans
 
 .if !defined(HS_BASE_ORPHANS_BUILDLINK3_MK)
 HS_BASE_ORPHANS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.9.1
-BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.9.1nb2
+BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.9.3
+BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.9.3
 BUILDLINK_PKGSRCDIR.hs-base-orphans?=	../../devel/hs-base-orphans
 .endif	# HS_BASE_ORPHANS_BUILDLINK3_MK
 
