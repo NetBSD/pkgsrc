6c3717ab6bd745c0bc2c52e5ca4bc347329e0098
pho <pho@pkgsrc.org>
2025-01-29 15:29:20 +0000

    lang/hs-lua: update to lua-2.3.3
    ## lua-2.3.3
    
    Released 2024-08-31.
    
    -   Add pattern constant `LUA_RIDX_GLOBALS`; that value is the
        registry index that holds the global environment.
    
    -   Update Lua to version 5.4.7.

--- lang/hs-lua/buildlink3.mk
+++ lang/hs-lua/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/29 15:29:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-lua
 
 .if !defined(HS_LUA_BUILDLINK3_MK)
 HS_LUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.3.2
-BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.3.2nb1
+BUILDLINK_API_DEPENDS.hs-lua+=	hs-lua>=2.3.3
+BUILDLINK_ABI_DEPENDS.hs-lua+=	hs-lua>=2.3.3
 BUILDLINK_PKGSRCDIR.hs-lua?=	../../lang/hs-lua
 
 .include "../../lang/lua54/buildlink3.mk"
