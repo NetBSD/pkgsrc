5ab592922887ed87ba3081f9efec6860fae43515
pho <pho@pkgsrc.org>
2025-01-29 17:21:33 +0000

    devel/hs-vector: update to vector-0.13.2.0
    # Changes in version 0.13.2.0
    
     * Strict boxed vector `Data.Vector.Strict` and `Data.Vector.Strict.Mutable` is
       added (#488). it ensures that all values in the vector are evaluated to WHNF.
     * `DoNotUnboxStrict`, `DoNotUnboxLazy`, and `DoNotUnboxNormalForm` wrapper are
       added for defining unbox instances for types that contain not unboxable fields.
       [#503](https://github.com/haskell/vector/issues/506),
       [#508](https://github.com/haskell/vector/pull/508)
     * `spanR` and `breakR` were added [#476](https://github.com/haskell/vector/pull/476).
       They allow parsing vector from the right.
     * We had some improvements on `*.Mutable.{next,prev}Permutation{,By}`
       [#498](https://github.com/haskell/vector/pull/498):
       * Add `*.Mutable.prevPermutation{,By}` and `*.Mutable.nextPermutationBy`
       * Improve time performance. We may now expect good specialization supported by inlining.
         The implementation has also been algorithmically updated: in the previous implementation
         the full enumeration of all the permutations of `[1..n]` took Omega(n*n!), but it now takes O(n!).
       * Add tests for `{next,prev}Permutation`
       * Add benchmarks for `{next,prev}Permutation`
     * Cabal >= 3.0 is now required for building package (#481).
     * `vector:benchmarks-O2` public sublibrary containing benchmarks is added (#481).
     * Type family `Mutable` provides instances for arrays from `primitive`.
     * Various documentation improvements.

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,16 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.21 2024/05/09 01:32:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2025/01/29 17:21:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.13.1
-BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.13.1.0nb2
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.13.2
+BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.13.2.0
 BUILDLINK_PKGSRCDIR.hs-vector?=		../../devel/hs-vector
 
+# lib:vector
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-vector-stream/buildlink3.mk"
+
+# lib:benchmarks-O2
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../devel/hs-tasty/buildlink3.mk"
+
 .endif	# HS_VECTOR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-vector
