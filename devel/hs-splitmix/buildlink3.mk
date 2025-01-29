bfd57c6f8dc2934fe0ab6b71b6f47c5d346fcc04
pho <pho@pkgsrc.org>
2025-01-29 16:53:53 +0000

    devel/hs-splitmix: update to splitmix-0.1.1
    # 0.1.1
    
    - Drop support for GHCs prior 8.6.5
    - Support GHC-9.12

--- devel/hs-splitmix/buildlink3.mk
+++ devel/hs-splitmix/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:32:12 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/29 16:53:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-splitmix
 
 .if !defined(HS_SPLITMIX_BUILDLINK3_MK)
 HS_SPLITMIX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.0.5nb2
+BUILDLINK_API_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-splitmix+=	hs-splitmix>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-splitmix?=	../../devel/hs-splitmix
 .endif	# HS_SPLITMIX_BUILDLINK3_MK
 
