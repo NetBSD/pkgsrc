243a004f51329e974d0cf33723ba6bcd8cf699f5
pho <pho@pkgsrc.org>
2025-01-29 17:44:17 +0000

    devel/hs-assoc: update to assoc-1.1.1
    ## 1.1.1
    
    - Drop support for GHC prior GHC-8.6.5

--- devel/hs-assoc/buildlink3.mk
+++ devel/hs-assoc/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:31:45 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/29 17:44:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-assoc
 
 .if !defined(HS_ASSOC_BUILDLINK3_MK)
 HS_ASSOC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-assoc+=	hs-assoc>=1.1
-BUILDLINK_ABI_DEPENDS.hs-assoc+=	hs-assoc>=1.1nb2
+BUILDLINK_API_DEPENDS.hs-assoc+=	hs-assoc>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-assoc+=	hs-assoc>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-assoc?=		../../devel/hs-assoc
-
-.include "../../devel/hs-tagged/buildlink3.mk"
 .endif	# HS_ASSOC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-assoc
