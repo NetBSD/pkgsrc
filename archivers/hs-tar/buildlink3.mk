9047e4c615d096f2480f1c36bc2e5cd652582d51
pho <pho@pkgsrc.org>
2025-01-29 16:39:16 +0000

    archivers/hs-tar: update to tar-0.6.4.0
    ## 0.6.4.0 Bodigrim <andrew.lelechenko@gmail.com> January 2025
    
      * Migrate internals of packing / unpacking to `OsPath`.
      * Use `getDirectoryContentsRecursive` from `directory-ospath-streaming`.
    
    ## 0.6.3.0 Bodigrim <andrew.lelechenko@gmail.com> June 2024
    
      * [Speed up `deserialize`](https://github.com/haskell/tar/pull/95).

--- archivers/hs-tar/buildlink3.mk
+++ archivers/hs-tar/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:31:38 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/29 16:39:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-tar
 
 .if !defined(HS_TAR_BUILDLINK3_MK)
 HS_TAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tar+=	hs-tar>=0.6.2
-BUILDLINK_ABI_DEPENDS.hs-tar+=	hs-tar>=0.6.2.0nb1
+BUILDLINK_API_DEPENDS.hs-tar+=	hs-tar>=0.6.4
+BUILDLINK_ABI_DEPENDS.hs-tar+=	hs-tar>=0.6.4.0
 BUILDLINK_PKGSRCDIR.hs-tar?=	../../archivers/hs-tar
 
-.include "../../devel/hs-os-string/buildlink3.mk"
+.include "../../sysutils/hs-directory-ospath-streaming/buildlink3.mk"
+.include "../../devel/hs-file-io/buildlink3.mk"
 .endif	# HS_TAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tar
