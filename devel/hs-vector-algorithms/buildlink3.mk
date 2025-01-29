5d13d68763e1d6165456e337b3a5f7436b8fbec6
pho <pho@pkgsrc.org>
2025-01-29 17:24:18 +0000

    devel/hs-vector-algorithms: update to vector-algorithms-0.9.0.3
    ## Version 0.9.0.3 (2024-11-25)
    
    - Fix an off-by-one error Heap.partialSort functions.
    - Support latest ghcs.
    
    ## Version 0.9.0.2 (2024-05-23)
    
    - Add `TypeOperators` pragma where needed.

--- devel/hs-vector-algorithms/buildlink3.mk
+++ devel/hs-vector-algorithms/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2024/05/09 01:32:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2025/01/29 17:24:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-algorithms
 
 .if !defined(HS_VECTOR_ALGORITHMS_BUILDLINK3_MK)
 HS_VECTOR_ALGORITHMS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0
-BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.9.0.1nb4
+BUILDLINK_API_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.9.0
+BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.9.0.3
 BUILDLINK_PKGSRCDIR.hs-vector-algorithms?=	../../devel/hs-vector-algorithms
 
 .include "../../devel/hs-bitvec/buildlink3.mk"
