c600034fb492490357f39b97f29da8d0bb303289
adam <adam@pkgsrc.org>
2025-01-29 14:31:30 +0000

    cmake cmake-gui: updated to 3.31.5
    CMake 3.31.5
    * Clang/CUDA: Fix language-wide flags when linking .so with compiler driver
    * Tests: Update expected results for ninja post-1.12 development
    * FindMatlab: Fix multi-signature function documentation anchors
    * WINDOWS_EXPORT_ALL_SYMBOLS: Fix vftable symbol export on ARM64EC
    * IAR: decouple linker location from toolkit's bin dir
    * install: Generate InstallScripts.json in consistent location

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.73 2025/01/14 16:13:04 adam Exp $
+# $NetBSD: version.mk,v 1.74 2025/01/29 14:31:30 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.4
+CMAKE_VERSION=	3.31.5
 CMAKE_API=	${CMAKE_VERSION:R}
