669743c1ff0e76c29b8e672c6c5849e8cfd6ff52
pho <pho@pkgsrc.org>
2025-01-29 17:40:12 +0000

    devel/hs-indexed-traversable: update to indexed-traversable-0.1.4
    # 0.1.4 [2024-05-dd]
    
    - Drop support for GHCs prior 8.6.5

--- devel/hs-indexed-traversable/buildlink3.mk
+++ devel/hs-indexed-traversable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:00 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/29 17:40:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-indexed-traversable
 
 .if !defined(HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK)
 HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.3nb2
+BUILDLINK_API_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-indexed-traversable+=	hs-indexed-traversable>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-indexed-traversable?=	../../devel/hs-indexed-traversable
 .endif	# HS_INDEXED_TRAVERSABLE_BUILDLINK3_MK
 
