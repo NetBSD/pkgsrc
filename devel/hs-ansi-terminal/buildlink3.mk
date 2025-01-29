aab8b58b1d8128c269262bf2f44dfe7a809f6c07
pho <pho@pkgsrc.org>
2025-01-29 17:00:33 +0000

    devel/hs-ansi-terminal: update to ansi-terminal-1.1.2
    Version 1.1.2
    -------------
    
    * Add `enableLineWrap` and `disableLineWrap`, and support for enabling and
     disabling automatic line wrapping.

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2024/05/09 01:31:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2025/01/29 17:00:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.1.1nb1
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.1.2
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=1.1.2
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
 
 .include "../../devel/hs-ansi-terminal-types/buildlink3.mk"
