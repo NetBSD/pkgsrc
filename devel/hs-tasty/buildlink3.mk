a850b61f5f336f2bcf3e0df55fb01d9e7d7a49b4
pho <pho@pkgsrc.org>
2025-01-29 17:12:11 +0000

    devel/hs-tasty: update to tasty-1.5.3
    Version 1.5.3
    --------------
    
    _2025-01-05_
    
    * Console reporter: disable line wrapping
      ([#433](https://github.com/UnkindPartition/tasty/pull/433)).
    * Console reporter: force flushing of stdout after `showCursor`
      ([#436](https://github.com/UnkindPartition/tasty/pull/436)).
    
    Version 1.5.2
    --------------
    
    _2024-11-03_
    
    * Partially revert [#393](https://github.com/UnkindPartition/tasty/pull/393)
      to fix progress reporting outside of Emacs.
    * Do not depend on `unbounded-delays` on `ppc64`, `s390x` and `riscv64`
      ([#371](https://github.com/UnkindPartition/tasty/pull/371),
       [#422](https://github.com/UnkindPartition/tasty/pull/422),
       [#423](https://github.com/UnkindPartition/tasty/pull/423)).
    
    
    Version 1.5.1
    --------------
    
    _2024-06-22_
    
    * Performance improvements
      ([#389](https://github.com/UnkindPartition/tasty/pull/389),
       [#390](https://github.com/UnkindPartition/tasty/pull/390)).
    * Progress reporting in Emacs: use `\r` instead of ANSI escape sequences
      ([#393](https://github.com/UnkindPartition/tasty/pull/393)).
    * Console reporter: fix unintended change to `foldHeading`
      ([#396](https://github.com/UnkindPartition/tasty/pull/396)).
    * Prune empty test subtrees from `TestTree`
      ([#403](https://github.com/UnkindPartition/tasty/pull/403)).
    * Add `instance Eq Timeout` and `instance Ord Timeout`
      ([#415](https://github.com/UnkindPartition/tasty/pull/415)).
    * Add ability to supply options for launchers and reporters at the top-level of test tree
      ([#417](https://github.com/UnkindPartition/tasty/pull/417)).

--- devel/hs-tasty/buildlink3.mk
+++ devel/hs-tasty/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/29 17:12:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty
 
 .if !defined(HS_TASTY_BUILDLINK3_MK)
 HS_TASTY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.5
-BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.5nb2
+BUILDLINK_API_DEPENDS.hs-tasty+=	hs-tasty>=1.5.3
+BUILDLINK_ABI_DEPENDS.hs-tasty+=	hs-tasty>=1.5.3
 BUILDLINK_PKGSRCDIR.hs-tasty?=		../../devel/hs-tasty
 
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
