55fca81803d20239300756c6226af65d9cd0f452
pho <pho@pkgsrc.org>
2025-01-30 05:06:40 +0000

    devel/hs-base-compat-batteries: update to base-compat-batteries-0.14.1
    ## Changes in 0.14.1 [2024.12.06]
     - This coincides with the `base-compat-batteries-0.14.1` release. Refer to the
       [`base-compat-batteries` changelog](https://github.com/haskell-compat/base-compat/blob/master/base-compat-batteries/CHANGES.markdown#changes-in-0141-20241206)
       for more details.
     - Drop support for pre-8.0 versions of GHC.
    
    ## Changes in 0.14.0 [2024.04.30]
     - This coincides with the `base-compat-batteries-0.14.0` release. Refer to the
       [`base-compat-batteries` changelog](https://github.com/haskell-compat/base-compat/blob/master/base-compat-batteries/CHANGES.markdown#changes-in-0140-20240430)
       for more details.

--- devel/hs-base-compat-batteries/buildlink3.mk
+++ devel/hs-base-compat-batteries/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:31:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/01/30 05:06:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-base-compat-batteries
 
 .if !defined(HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK)
 HS_BASE_COMPAT_BATTERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.13.1
-BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.13.1nb2
+BUILDLINK_API_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.14.1
+BUILDLINK_ABI_DEPENDS.hs-base-compat-batteries+=	hs-base-compat-batteries>=0.14.1
 BUILDLINK_PKGSRCDIR.hs-base-compat-batteries?=		../../devel/hs-base-compat-batteries
 
 .include "../../devel/hs-base-compat/buildlink3.mk"
