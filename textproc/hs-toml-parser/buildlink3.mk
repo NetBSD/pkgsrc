e3d6a8ff16959eb593d3e6db807fca0990fbcdcd
pho <pho@pkgsrc.org>
2025-01-30 13:26:08 +0000

    textproc/hs-toml-parser: update to toml-parser-2.0.1.0
    ## 2.0.1.0
    
    * Added `ToValue UTCTime` and `FromValue UTCTime`. These correspond
      to offset data-times with the timezone translated to UTC.

--- textproc/hs-toml-parser/buildlink3.mk
+++ textproc/hs-toml-parser/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:32:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/30 13:26:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-toml-parser
 
 .if !defined(HS_TOML_PARSER_BUILDLINK3_MK)
 HS_TOML_PARSER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-toml-parser+=	hs-toml-parser>=2.0.0
-BUILDLINK_ABI_DEPENDS.hs-toml-parser+=	hs-toml-parser>=2.0.0.0nb1
+BUILDLINK_API_DEPENDS.hs-toml-parser+=	hs-toml-parser>=2.0.1
+BUILDLINK_ABI_DEPENDS.hs-toml-parser+=	hs-toml-parser>=2.0.1.0
 BUILDLINK_PKGSRCDIR.hs-toml-parser?=	../../textproc/hs-toml-parser
 
 .include "../../textproc/hs-prettyprinter/buildlink3.mk"
