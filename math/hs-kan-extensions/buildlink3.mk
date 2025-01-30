66b77e676a7467d11e2b8d98bc9fb9d2628b6d66
pho <pho@pkgsrc.org>
2025-01-30 07:54:38 +0000

    math/hs-kan-extensions: update to kan-extensions-5.2.6
    5.2.6 [2024.05.04]
    ------------------
    * Drop support for GHC 8.2 and earlier.
    * Generalize instances in `Control.Monad.Codensity` to be of the form:
    
      ```hs
      instance (f ~~ f', MonadFail f') => MonadFail (Codensity (f :: k -> TYPE rep))
      ```
    
      This avoids having to constrain `k ~ Type` and `rep ~ LiftedRep`, which could potentially harm type inference.
    * Explicitly implement `liftA2` in the `Applicative` instance for `Data.Functor.Day.Curried`.
    * Add an `Adjunction` instance for `Data.Functor.Day`.
    * Add `Adjunction` and `Divisible` instances for `Data.Functor.Contravariant.Day`.
    * Add an `Apply` instance for `Data.Functor.Day.Curried`.

--- math/hs-kan-extensions/buildlink3.mk
+++ math/hs-kan-extensions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/30 07:54:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-kan-extensions
 
 .if !defined(HS_KAN_EXTENSIONS_BUILDLINK3_MK)
 HS_KAN_EXTENSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-kan-extensions+=	hs-kan-extensions>=5.2.5
-BUILDLINK_ABI_DEPENDS.hs-kan-extensions+=	hs-kan-extensions>=5.2.5nb4
+BUILDLINK_API_DEPENDS.hs-kan-extensions+=	hs-kan-extensions>=5.2.6
+BUILDLINK_ABI_DEPENDS.hs-kan-extensions+=	hs-kan-extensions>=5.2.6
 BUILDLINK_PKGSRCDIR.hs-kan-extensions?=		../../math/hs-kan-extensions
 
 .include "../../math/hs-adjunctions/buildlink3.mk"
@@ -18,7 +18,6 @@ BUILDLINK_PKGSRCDIR.hs-kan-extensions?=		../../math/hs-kan-extensions
 .include "../../math/hs-profunctors/buildlink3.mk"
 .include "../../math/hs-semigroupoids/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_KAN_EXTENSIONS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-kan-extensions
