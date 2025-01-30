68839cdb781f5f6b08e9db5e2f2b62760d74efef
pho <pho@pkgsrc.org>
2025-01-30 23:54:51 +0000

    devel/hs-lift-type: update to lift-type-0.1.2.0
    ## 0.1.2.0
    
    - Add `typeToName`, making it possible to accurately extract a `Name` from a type. [#11](https://github.com/parsonsmatt/lift-type/pull/11)

--- devel/hs-lift-type/buildlink3.mk
+++ devel/hs-lift-type/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 23:54:51 pho Exp $
 
 BUILDLINK_TREE+=	hs-lift-type
 
 .if !defined(HS_LIFT_TYPE_BUILDLINK3_MK)
 HS_LIFT_TYPE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lift-type+=	hs-lift-type>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-lift-type+=	hs-lift-type>=0.1.1.1nb2
+BUILDLINK_API_DEPENDS.hs-lift-type+=	hs-lift-type>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-lift-type+=	hs-lift-type>=0.1.2.0
 BUILDLINK_PKGSRCDIR.hs-lift-type?=	../../devel/hs-lift-type
 .endif	# HS_LIFT_TYPE_BUILDLINK3_MK
 
