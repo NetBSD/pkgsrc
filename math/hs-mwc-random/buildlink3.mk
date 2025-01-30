29c088b464493ebef3fece0ae7a63de6051e2380
pho <pho@pkgsrc.org>
2025-01-30 05:24:44 +0000

    math/hs-mwc-random: update to mwc-random-0.15.2.0
    ## Changes in 0.15.2.0
    
      * Support for `random-1.3`.
    
    
    ## Changes in 0.15.1.0
    
      * Additon of binomial sampler using the rejection sampling method in
        Kachitvichyanukul, V. and Schmeiser, B. W.  Binomial Random
        Variate Generation.  Communications of the ACM, 31, 2 (February,
        1988) 216. <https://dl.acm.org/doi/pdf/10.1145/42372.42381>. A more
        efficient basis for e.g. the beta binomial distribution:
    	`beta a b g >>= \p -> binomial n p g`.

--- math/hs-mwc-random/buildlink3.mk
+++ math/hs-mwc-random/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2024/05/09 01:32:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2025/01/30 05:24:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-mwc-random
 
 .if !defined(HS_MWC_RANDOM_BUILDLINK3_MK)
 HS_MWC_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.15.0
-BUILDLINK_ABI_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.15.0.2nb7
+BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.15.2
+BUILDLINK_ABI_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.15.2.0
 BUILDLINK_PKGSRCDIR.hs-mwc-random?=	../../math/hs-mwc-random
 
 .include "../../math/hs-math-functions/buildlink3.mk"
