f5b26cf729ce89d8f78f1d400ad2849155a05fa6
pho <pho@pkgsrc.org>
2025-01-30 12:16:09 +0000

    textproc/hs-djot: update to djot-0.1.2.2
    ## 0.1.2.2 -- 2024-10-04
    
    * Allow list items with blank lines between divs (#10).
    
    * Fix parsing of indented tables (#8).
    
    ## 0.1.2.1 -- 2024-06-24
    
    * Djot writer: include separator line in table when the table has
      non-default alignments but no header (#7).
    
    ## 0.1.2 -- 2024-05-10
    
    * Allow `_` in symbols (see jgm/djot#296).
    
    * Add Lift derivations to AST datatypes (#5, Gideon Farrell) [API change].

--- textproc/hs-djot/buildlink3.mk
+++ textproc/hs-djot/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2025/01/09 23:24:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/30 12:16:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-djot
 
 .if !defined(HS_DJOT_BUILDLINK3_MK)
 HS_DJOT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-djot+=	hs-djot>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-djot+=	hs-djot>=0.1.1.3nb3
+BUILDLINK_API_DEPENDS.hs-djot+=	hs-djot>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-djot+=	hs-djot>=0.1.2.2
 BUILDLINK_PKGSRCDIR.hs-djot?=	../../textproc/hs-djot
 
 .include "../../textproc/hs-doclayout/buildlink3.mk"
