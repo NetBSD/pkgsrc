04e445b6ff7b9cb309880d896ea06cd801093fb3
pho <pho@pkgsrc.org>
2025-01-30 07:23:33 +0000

    converters/hs-deriving-aeson: update to deriving-aeson-0.2.10
    ## 0.2.10
    
    * Added `StripSuffix`

--- converters/hs-deriving-aeson/buildlink3.mk
+++ converters/hs-deriving-aeson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2024/05/09 01:31:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/01/30 07:23:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-deriving-aeson
 
 .if !defined(HS_DERIVING_AESON_BUILDLINK3_MK)
 HS_DERIVING_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-deriving-aeson+=	hs-deriving-aeson>=0.2.9
-BUILDLINK_ABI_DEPENDS.hs-deriving-aeson+=	hs-deriving-aeson>=0.2.9nb4
+BUILDLINK_API_DEPENDS.hs-deriving-aeson+=	hs-deriving-aeson>=0.2.10
+BUILDLINK_ABI_DEPENDS.hs-deriving-aeson+=	hs-deriving-aeson>=0.2.10
 BUILDLINK_PKGSRCDIR.hs-deriving-aeson?=		../../converters/hs-deriving-aeson
 
 .include "../../converters/hs-aeson/buildlink3.mk"
