41b8d8e19846c3332b7dbfc76993c6e048df0f8d
pho <pho@pkgsrc.org>
2025-01-30 16:29:53 +0000

    www/hs-http2: update to http2-5.3.9
    # ChangeLog for http2
    
    ## 5.3.9
    
    * Using `ThreadManager` of `time-manager`.
    
    ## 5.3.8
    
    * `forkManagedTimeout` ensures that only one asynchronous exception is
      thrown. Fixing the thread leak via `Weak ThreadId` and `modifyTVar'`.
      [#156](https://github.com/kazu-yamamoto/http2/pull/156)
    
    ## 5.3.7
    
    * Using `withHandle` of time-manager.
    * Getting `Handle` for each thread.
    * Providing allocSimpleConfig' to enable customizing WAI tiemout manager.
    * Monitor option (-m) for h2c-client and h2c-server.
    
    ## 5.3.6
    
    * Making `runIO` friendly with the new synchronism mechanism.
      [#152](https://github.com/kazu-yamamoto/http2/pull/152)
    * Re-throwing asynchronous exceptions to prevent thread leak.
    * Simplifying the synchronism mechanism between workers and the sender.
      [#148](https://github.com/kazu-yamamoto/http2/pull/148)
    
    ## 5.3.5
    
    * Using `http-semantics` v0.3.
    * Deprecating `numberOfWorkers`.
    * Removing `unliftio`.
    * Avoid `undefined` in client.
      [#146](https://github.com/kazu-yamamoto/http2/pull/146)
    
    ## 5.3.4
    
    * Support stream cancellation
      [#142](https://github.com/kazu-yamamoto/http2/pull/142)
    
    ## 5.3.3
    
    * Enclosing IPv6 literal authority with square brackets.
      [#143](https://github.com/kazu-yamamoto/http2/pull/143)
    
    ## 5.3.2
    
    * Avoid unnecessary empty data frames at end of stream
      [#140](https://github.com/kazu-yamamoto/http2/pull/140)
    * Removing unnecessary API from ServerIO
    
    ## 5.3.1
    
    * Fix treatment of async exceptions
      [#138](https://github.com/kazu-yamamoto/http2/pull/138)
    * Avoid race condition
      [#137](https://github.com/kazu-yamamoto/http2/pull/137)
    
    ## 5.3.0
    
    * New server architecture: spawning worker on demand instead of the
      worker pool. This reduce huge numbers of threads for streaming into
      only 2. No API changes but workers do not terminate quicly. Rather
      workers collaborate with the sender after queuing a response and
      finish after all response data are sent.
    * All threads are labeled with `labelThread`. You can see them by
      `listThreas` if necessary.
    
    ## 5.2.6
    
    * Recover rxflow on closing.
      [#126](https://github.com/kazu-yamamoto/http2/pull/126)
    * Fixing ClientSpec for stream errors.
    * Allowing negative window. (h2spec http2/6.9.2)
    * Update for latest http-semantics
      [#122](https://github.com/kazu-yamamoto/http2/pull/124)
    
    ## 5.2.5
    
    * Setting peer initial window size properly.
      [#123](https://github.com/kazu-yamamoto/http2/pull/123)
    
    ## 5.2.4
    
    * Update for latest http-semantics
      [#122](https://github.com/kazu-yamamoto/http2/pull/122)
    * Measuring performance concurrently for h2c-client
    
    ## 5.2.3
    
    * Update for latest http-semantics
      [#120](https://github.com/kazu-yamamoto/http2/pull/120)
    * Enable containers 0.7 (ghc 9.10)
      [#117](https://github.com/kazu-yamamoto/http2/pull/117)
    
    ## 5.2.2
    
    * Mark final chunk as final
      [#116](https://github.com/kazu-yamamoto/http2/pull/116)

--- www/hs-http2/buildlink3.mk
+++ www/hs-http2/buildlink3.mk
@@ -1,25 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/30 16:29:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-http2
 
 .if !defined(HS_HTTP2_BUILDLINK3_MK)
 HS_HTTP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=5.2.1
-BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=5.2.1nb1
+BUILDLINK_API_DEPENDS.hs-http2+=	hs-http2>=5.3.9
+BUILDLINK_ABI_DEPENDS.hs-http2+=	hs-http2>=5.3.9
 BUILDLINK_PKGSRCDIR.hs-http2?=		../../www/hs-http2
 
 .include "../../devel/hs-async/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../www/hs-http-semantics/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
+.include "../../net/hs-iproute/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
 .include "../../net/hs-network-byte-order/buildlink3.mk"
 .include "../../net/hs-network-control/buildlink3.mk"
 .include "../../devel/hs-psqueues/buildlink3.mk"
 .include "../../time/hs-time-manager/buildlink3.mk"
 .include "../../time/hs-unix-time/buildlink3.mk"
-.include "../../devel/hs-unliftio/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HTTP2_BUILDLINK3_MK
 
