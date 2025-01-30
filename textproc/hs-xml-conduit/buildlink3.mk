b1cc6a629b67ef25f7434a40bd2eb6968947830f
pho <pho@pkgsrc.org>
2025-01-30 11:53:08 +0000

    textproc/hs-xml-conduit: update to xml-conduit-1.10.0.0
    No release notes have been provided by the upstream.

--- textproc/hs-xml-conduit/buildlink3.mk
+++ textproc/hs-xml-conduit/buildlink3.mk
@@ -1,22 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:32:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/01/30 11:53:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-xml-conduit
 
 .if !defined(HS_XML_CONDUIT_BUILDLINK3_MK)
 HS_XML_CONDUIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-xml-conduit+=	hs-xml-conduit>=1.9.1
-BUILDLINK_ABI_DEPENDS.hs-xml-conduit+=	hs-xml-conduit>=1.9.1.3nb2
+BUILDLINK_API_DEPENDS.hs-xml-conduit+=	hs-xml-conduit>=1.10.0
+BUILDLINK_ABI_DEPENDS.hs-xml-conduit+=	hs-xml-conduit>=1.10.0.0
 BUILDLINK_PKGSRCDIR.hs-xml-conduit?=	../../textproc/hs-xml-conduit
 
-.include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../textproc/hs-blaze-html/buildlink3.mk"
-.include "../../textproc/hs-blaze-markup/buildlink3.mk"
 .include "../../devel/hs-conduit/buildlink3.mk"
 .include "../../devel/hs-conduit-extra/buildlink3.mk"
-.include "../../devel/hs-data-default-class/buildlink3.mk"
 .include "../../devel/hs-resourcet/buildlink3.mk"
 .include "../../textproc/hs-xml-types/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../textproc/hs-blaze-markup/buildlink3.mk"
+.include "../../textproc/hs-blaze-html/buildlink3.mk"
 .endif	# HS_XML_CONDUIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-xml-conduit
