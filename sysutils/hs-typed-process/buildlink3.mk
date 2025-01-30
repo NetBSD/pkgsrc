1f1cf78b1e35b4a1d81358ff0d5c550ee3144924
pho <pho@pkgsrc.org>
2025-01-30 11:45:55 +0000

    sysutils/hs-typed-process: update to typed-process-0.2.12.0
    ## 0.2.12.0
    
    * Add `getPid`, `exitCodeExceptionWithOutput`,
      `exitCodeExceptionNoOutput`,
    
    * Re-export `System.Process.Pid`
    
    * Thanks to Rebecca Turner @9999years

--- sysutils/hs-typed-process/buildlink3.mk
+++ sysutils/hs-typed-process/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:32:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/01/30 11:45:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-typed-process
 
 .if !defined(HS_TYPED_PROCESS_BUILDLINK3_MK)
 HS_TYPED_PROCESS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.11
-BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.11.1nb2
+BUILDLINK_API_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.12
+BUILDLINK_ABI_DEPENDS.hs-typed-process+=	hs-typed-process>=0.2.12.0
 BUILDLINK_PKGSRCDIR.hs-typed-process?=		../../sysutils/hs-typed-process
 
 .include "../../devel/hs-async/buildlink3.mk"
