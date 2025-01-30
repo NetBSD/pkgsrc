ccbbaede3066a392d0b0ae7b7a2c0aca12e6a431
pho <pho@pkgsrc.org>
2025-01-30 11:42:38 +0000

    devel/hs-streaming-commons: update to streaming-commons-0.2.3.0
    ## 0.2.3.0
    
    * Allow Unix sockets on Windows (https://github.com/fpco/streaming-commons/pull/80)

--- devel/hs-streaming-commons/buildlink3.mk
+++ devel/hs-streaming-commons/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:32:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/01/30 11:42:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-streaming-commons
 
 .if !defined(HS_STREAMING_COMMONS_BUILDLINK3_MK)
 HS_STREAMING_COMMONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.2.6nb2
+BUILDLINK_API_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-streaming-commons+=	hs-streaming-commons>=0.2.3.0
 BUILDLINK_PKGSRCDIR.hs-streaming-commons?=	../../devel/hs-streaming-commons
 
 .include "../../devel/hs-async/buildlink3.mk"
