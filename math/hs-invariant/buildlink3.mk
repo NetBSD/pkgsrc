9e1e5461ee8c0aa360c9f7c55ff761846cd3c146
pho <pho@pkgsrc.org>
2025-01-30 07:52:23 +0000

    math/hs-invariant: update to invariant-0.6.4
    # 0.6.4 [2024.12.05]
    * Drop support for pre-8.0 versions of GHC.

--- math/hs-invariant/buildlink3.mk
+++ math/hs-invariant/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/05/09 01:32:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/30 07:52:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-invariant
 
 .if !defined(HS_INVARIANT_BUILDLINK3_MK)
 HS_INVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-invariant+=	hs-invariant>=0.6.3
-BUILDLINK_ABI_DEPENDS.hs-invariant+=	hs-invariant>=0.6.3nb1
+BUILDLINK_API_DEPENDS.hs-invariant+=	hs-invariant>=0.6.4
+BUILDLINK_ABI_DEPENDS.hs-invariant+=	hs-invariant>=0.6.4
 BUILDLINK_PKGSRCDIR.hs-invariant?=	../../math/hs-invariant
 
 .include "../../math/hs-bifunctors/buildlink3.mk"
