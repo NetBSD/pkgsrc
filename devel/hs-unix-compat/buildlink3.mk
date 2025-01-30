a2c0d5c1f63ffb0f0af4c8eba85133f35411b9eb
pho <pho@pkgsrc.org>
2025-01-30 15:56:25 +0000

    devel/hs-unix-compat: update to unix-compat-0.7.3
    ## Version 0.7.3 (2024-10-11)
    
    - Fix `sysmacros.h` include for GNU/Hurd
      ([PR #12](https://github.com/haskell-pkg-janitors/unix-compat/pull/12)).
    - Tested with GHC 8.0 - 9.10.
    
    ## Version 0.7.2 (2024-06-25)
    
    - Remove flag `old-time` and drop support for `old-time`.
    - Remove support for GHC 7.
    - Tested with GHC 8.0 - 9.10.

--- devel/hs-unix-compat/buildlink3.mk
+++ devel/hs-unix-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/30 15:56:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-compat
 
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7.1nb1
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7.3
+BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.7.3
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../devel/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
