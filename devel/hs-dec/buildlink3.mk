1572567ee18244b9a515f4ebe45b325eb1a8566e
pho <pho@pkgsrc.org>
2025-01-30 15:43:39 +0000

    devel/hs-dec: update to dec-0.0.6
    ## 0.0.6
    
    - Support GHC-8.6.5...9.10.1

--- devel/hs-dec/buildlink3.mk
+++ devel/hs-dec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2024/05/09 01:31:52 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/01/30 15:43:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-dec
 
 .if !defined(HS_DEC_BUILDLINK3_MK)
 HS_DEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dec+=	hs-dec>=0.0.5
-BUILDLINK_ABI_DEPENDS.hs-dec+=	hs-dec>=0.0.5nb4
+BUILDLINK_API_DEPENDS.hs-dec+=	hs-dec>=0.0.6
+BUILDLINK_ABI_DEPENDS.hs-dec+=	hs-dec>=0.0.6
 BUILDLINK_PKGSRCDIR.hs-dec?=	../../devel/hs-dec
 
 .include "../../devel/hs-boring/buildlink3.mk"
