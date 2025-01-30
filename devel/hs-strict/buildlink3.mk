d317627adf18c7a29c06d3dc74c235c9add9c3a4
pho <pho@pkgsrc.org>
2025-01-30 04:25:02 +0000

    devel/hs-strict: update to strict-0.5.1
    # 0.5.1
    
    - Support GHC-8.6.5...GHC-9.10.1

--- devel/hs-strict/buildlink3.mk
+++ devel/hs-strict/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:13 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/30 04:25:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-strict
 
 .if !defined(HS_STRICT_BUILDLINK3_MK)
 HS_STRICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-strict+=	hs-strict>=0.5
-BUILDLINK_ABI_DEPENDS.hs-strict+=	hs-strict>=0.5nb2
+BUILDLINK_API_DEPENDS.hs-strict+=	hs-strict>=0.5.1
+BUILDLINK_ABI_DEPENDS.hs-strict+=	hs-strict>=0.5.1
 BUILDLINK_PKGSRCDIR.hs-strict?=		../../devel/hs-strict
 
 .include "../../devel/hs-hashable/buildlink3.mk"
