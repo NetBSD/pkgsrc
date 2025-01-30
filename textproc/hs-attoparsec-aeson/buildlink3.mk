807bfc82c1bcf6e9dd49cd5a6a37e2745ce5cac0
pho <pho@pkgsrc.org>
2025-01-30 07:09:52 +0000

    textproc/hs-attoparsec-aeson: update to attoparsec-aeson-2.2.2.0
    No release notes have been provided by the upstream.

--- textproc/hs-attoparsec-aeson/buildlink3.mk
+++ textproc/hs-attoparsec-aeson/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 07:09:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec-aeson
 
 .if !defined(HS_ATTOPARSEC_AESON_BUILDLINK3_MK)
 HS_ATTOPARSEC_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec-aeson+=	hs-attoparsec-aeson>=2.2.0
-BUILDLINK_ABI_DEPENDS.hs-attoparsec-aeson+=	hs-attoparsec-aeson>=2.2.0.1nb2
+BUILDLINK_API_DEPENDS.hs-attoparsec-aeson+=	hs-attoparsec-aeson>=2.2.2
+BUILDLINK_ABI_DEPENDS.hs-attoparsec-aeson+=	hs-attoparsec-aeson>=2.2.2.0
 BUILDLINK_PKGSRCDIR.hs-attoparsec-aeson?=	../../textproc/hs-attoparsec-aeson
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-character-ps/buildlink3.mk"
 .include "../../math/hs-integer-conversion/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
