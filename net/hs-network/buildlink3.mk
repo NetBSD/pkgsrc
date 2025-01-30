05f511f789a8fd83d6a076bf048cfc48bb830715
pho <pho@pkgsrc.org>
2025-01-30 09:56:49 +0000

    net/hs-network: update to network-3.2.7.0
    ## Version 3.2.7.0
    
    * Using nested `bracket` for `gracefulClose`.
      [#591](https://github.com/haskell/network/issues/590)
    * Fix memory leak in getaddrinfo and make it async exception safe.
      [#591](https://github.com/haskell/network/pull/591)
    * Make call to c_free async exception safe.
      [#592](https://github.com/haskell/network/pull/592)
    
    ## Version 3.2.6.0
    
    * fixing the Show instance of IPv4-mapped IPv6 address on little endian machines
    
    ## Version 3.2.5.0
    
    * `gracefulClose` based on STM racing and `timeout`.
      [#587](https://github.com/haskell/network/issues/587)
    
    ## Version 3.2.4.0
    
    * New API: setSockOptValue.
      [#588](https://github.com/haskell/network/issues/588)
    
    ## Version 3.2.3.0
    
    * Making getAddrInfo polymorphic
      [#587](https://github.com/haskell/network/issues/587)
    
    ## Version 3.2.2.0
    
    * New API: waitReadSocketSTM, waitAndCancelReadSocketSTM,
      waitWriteSocketSTM, waitAndCancelWriteSocketSTM
      [#586](https://github.com/haskell/network/issues/586)
    * Checking the length of ASCII string allowing trailing 0.
      [#585](https://github.com/haskell/network/issues/585)
    
    ## Version 3.2.1.0
    
    * Trying to release with the latest autoreconf.
      Packing "network" in the local directory instead of CI.
    * Remove includes from .cabal-file
      [#583](https://github.com/haskell/network/issues/583)
    * making gracefulClose more graceful
      [#580](https://github.com/haskell/network/issues/580)
    * Update config.guess, config.sub to their latest versions
      [#579](https://github.com/haskell/network/issues/579)

--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.24 2024/05/09 01:32:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2025/01/30 09:56:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-network
 
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.2.0
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.2.0.0nb1
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=3.2.7
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=3.2.7.0
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 .endif	# HS_NETWORK_BUILDLINK3_MK
 
