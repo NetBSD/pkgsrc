fc57865be1e0e33a868fb06ecfe29bab61c21b9e
adam <adam@pkgsrc.org>
2025-01-30 11:08:39 +0000

    openal-soft: updated to 1.24.2
    openal-soft-1.24.2:
    
    Implemented the AL_SOFT_bformat_hoa extension.
    
    Implemented default device change events for the PulseAudio backend.
    
    Implemented an option for WASAPI exclusive mode playback.
    
    Fixed reverb being too quiet for sounds from different directions.
    
    Fixed compiling with certain versions of Clang.
    
    Fixed compiling for some older macOS versions.
    
    Fixed building alffplay on systems without pkg-config.
    
    Improved output format detection for CoreAudio.
    
    Changed the default resampler back to Cubic Spline.
    
    Added an SDL3 playback backend. Disabled by default to avoid a runtime
    dependency and for compatibility; a single process can't safely use SDL2
    and SDL3 together on some OSs, so enable with care.
    
    Converted examples from SDL2 to SDL3.
    
    Integrated fmtlib into the main library and router for logging and string
    formatting.

--- audio/openal-soft/patches/patch-alc_alconfig.cpp
+++ audio/openal-soft/patches/patch-alc_alconfig.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-alc_alconfig.cpp,v 1.3 2024/11/29 03:47:45 ryoon Exp $
+$NetBSD: patch-alc_alconfig.cpp,v 1.4 2025/01/30 11:08:40 adam Exp $
 
 Allow the Makefile to specify the default config file path.
 
---- alc/alconfig.cpp.orig	2024-11-28 03:21:16.000000000 +0000
+--- alc/alconfig.cpp.orig	2025-01-11 07:17:23.000000000 +0000
 +++ alc/alconfig.cpp
-@@ -384,7 +384,7 @@ void ReadALConfig()
+@@ -381,7 +381,7 @@ void ReadALConfig()
+ 
  void ReadALConfig()
  {
-     namespace fs = std::filesystem;
 -    fs::path path{"/etc/openal/alsoft.conf"};
 +    fs::path path{SYS_CONF_DIR "/alsoft.conf"};
  
-     TRACE("Loading config %s...\n", reinterpret_cast<const char*>(path.u8string().c_str()));
-     if(std::ifstream f{path}; f.is_open())
+     TRACE("Loading config {}...", al::u8_as_char(path.u8string()));
+     if(fs::ifstream f{path}; f.is_open())
