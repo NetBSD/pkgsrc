0325f9fbfdd5d379508eaac53ebc7cb3c3098b98
pho <pho@pkgsrc.org>
2025-01-30 16:13:35 +0000

    time/hs-time-manager: update to time-manager-0.2.2
    ## 0.2.2
    
    * `initialize` with non positive integer creates a time manager
      which does not maintain timeout.
      [#1017](https://github.com/yesodweb/wai/pull/1017)
    
    ## 0.2.1
    
    * Export KilledByThreadManager exception
      [#1016](https://github.com/yesodweb/wai/pull/1016)
    
    ## 0.2.0
    
    * Providing `System.ThreadManager`.
    * `withHandle` catches `TimeoutThread` internally.
      It returns `Nothing` on timeout.
    
    ## 0.1.3
    
    * Providing `withHandle` and `withHandleKillThread`.
    
    ## 0.1.2
    
    * Holding `Weak ThreadId` to prevent thread leak again
      [#1013](https://github.com/yesodweb/wai/pull/1013)
    
    ## 0.1.1
    
    * Removing `unliftio`.

--- time/hs-time-manager/buildlink3.mk
+++ time/hs-time-manager/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/30 16:13:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-time-manager
 
 .if !defined(HS_TIME_MANAGER_BUILDLINK3_MK)
 HS_TIME_MANAGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.1.0nb1
+BUILDLINK_API_DEPENDS.hs-time-manager+=	hs-time-manager>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-time-manager+=	hs-time-manager>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-time-manager?=	../../time/hs-time-manager
 
 .include "../../devel/hs-auto-update/buildlink3.mk"
-.include "../../devel/hs-unliftio/buildlink3.mk"
 .endif	# HS_TIME_MANAGER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-time-manager
