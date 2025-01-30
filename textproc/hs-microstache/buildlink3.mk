e6228aad69957e4b70a29eb0fb042643b6332428
pho <pho@pkgsrc.org>
2025-01-30 06:49:15 +0000

    textproc/hs-microstache: update to microstache-1.0.3
    ## microstache 1.0.3
    
    - Support GHC-8.6.5...GHC-9.10.1

--- textproc/hs-microstache/buildlink3.mk
+++ textproc/hs-microstache/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 06:49:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-microstache
 
 .if !defined(HS_MICROSTACHE_BUILDLINK3_MK)
 HS_MICROSTACHE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-microstache+=	hs-microstache>=1.0.2
-BUILDLINK_ABI_DEPENDS.hs-microstache+=	hs-microstache>=1.0.2.3nb2
+BUILDLINK_API_DEPENDS.hs-microstache+=	hs-microstache>=1.0.3
+BUILDLINK_ABI_DEPENDS.hs-microstache+=	hs-microstache>=1.0.3
 BUILDLINK_PKGSRCDIR.hs-microstache?=	../../textproc/hs-microstache
 
 .include "../../converters/hs-aeson/buildlink3.mk"
