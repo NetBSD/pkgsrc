e0ae9d7fb55f991c6a5504860d4193edca67222c
pho <pho@pkgsrc.org>
2025-01-30 13:00:49 +0000

    devel/hs-ordered-containers: update to ordered-containers-0.2.4
    ## 0.2.4 -- 2024-05-18
    
    * Misc. housekeeping -- version bumps, documentation fixes, etc. (thank you Ryan Scott and Andrew Kent!)
    * Add `IsList` and `Hashable` instances (thank you Alexis King!)
    * the strict variant of `alter`
    
    ## 0.2.3 -- 2022-11-01
    
    * `alter` (thank you Raoul Hidalgo Charman!)

--- devel/hs-ordered-containers/buildlink3.mk
+++ devel/hs-ordered-containers/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/30 13:00:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-ordered-containers
 
 .if !defined(HS_ORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_ORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.3
-BUILDLINK_ABI_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.3nb4
+BUILDLINK_API_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.4
+BUILDLINK_ABI_DEPENDS.hs-ordered-containers+=	hs-ordered-containers>=0.2.4
 BUILDLINK_PKGSRCDIR.hs-ordered-containers?=	../../devel/hs-ordered-containers
+
+.include "../../devel/hs-hashable/buildlink3.mk"
 .endif	# HS_ORDERED_CONTAINERS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-ordered-containers
