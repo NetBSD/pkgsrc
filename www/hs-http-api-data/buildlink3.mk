cce29b0fad7b2dd40ce3ffff8c56623340e41dc3
pho <pho@pkgsrc.org>
2025-01-30 14:51:59 +0000

    www/hs-http-api-data: update to http-api-data-0.6.1
    0.6.1
    
    * Require at least GHC-8.6
    * Support `cookie-0.5.0`, see [#137](https://github.com/fizruk/http-api-data/pull/137).
    
      Note that `cookie-0.5.0`'s parser now removes double quotes around cookie values.

--- www/hs-http-api-data/buildlink3.mk
+++ www/hs-http-api-data/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:52 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 14:51:59 pho Exp $
 
 BUILDLINK_TREE+=	hs-http-api-data
 
 .if !defined(HS_HTTP_API_DATA_BUILDLINK3_MK)
 HS_HTTP_API_DATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.6
-BUILDLINK_ABI_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.6nb2
+BUILDLINK_API_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.6.1
+BUILDLINK_ABI_DEPENDS.hs-http-api-data+=	hs-http-api-data>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-http-api-data?=		../../www/hs-http-api-data
 
 .include "../../www/hs-cookie/buildlink3.mk"
