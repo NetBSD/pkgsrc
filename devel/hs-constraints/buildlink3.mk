c9078d764182c6b0534f01877949f7a256e2c257
pho <pho@pkgsrc.org>
2025-01-30 14:47:35 +0000

    devel/hs-constraints: update to constraints-0.14.2
    0.14.2 [2024.05.12]
    -------------------
    * Re-export `Log2` from `Data.Constraint.Nat`.
    * Add `log2Nat` and `log2Pow` to `Data.Constraint.Nat`.
    
    0.14.1 [2024.04.29]
    -------------------
    * Remove an unused dependency on the `type-equality` library.

--- devel/hs-constraints/buildlink3.mk
+++ devel/hs-constraints/buildlink3.mk
@@ -1,17 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/05/09 01:31:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/01/30 14:47:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-constraints
 
 .if !defined(HS_CONSTRAINTS_BUILDLINK3_MK)
 HS_CONSTRAINTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-constraints+=	hs-constraints>=0.13.4
-BUILDLINK_ABI_DEPENDS.hs-constraints+=	hs-constraints>=0.14nb2
+BUILDLINK_API_DEPENDS.hs-constraints+=	hs-constraints>=0.14.2
+BUILDLINK_ABI_DEPENDS.hs-constraints+=	hs-constraints>=0.14.2
 BUILDLINK_PKGSRCDIR.hs-constraints?=	../../devel/hs-constraints
 
 .include "../../devel/hs-boring/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../devel/hs-type-equality/buildlink3.mk"
 .endif	# HS_CONSTRAINTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-constraints
