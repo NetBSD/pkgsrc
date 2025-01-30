eb8e112d411ef48a5b8252fdfa52e7ebd4e7479b
pho <pho@pkgsrc.org>
2025-01-30 04:36:49 +0000

    devel/hs-th-compat: update to th-compat-0.1.6
    ### 0.1.6 [2024.12.05]
    * Drop support for pre-8.0 versions of GHC.

--- devel/hs-th-compat/buildlink3.mk
+++ devel/hs-th-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:32:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/30 04:36:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-compat
 
 .if !defined(HS_TH_COMPAT_BUILDLINK3_MK)
 HS_TH_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.5
-BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.5nb1
+BUILDLINK_API_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.6
+BUILDLINK_ABI_DEPENDS.hs-th-compat+=	hs-th-compat>=0.1.6
 BUILDLINK_PKGSRCDIR.hs-th-compat?=	../../devel/hs-th-compat
 .endif	# HS_TH_COMPAT_BUILDLINK3_MK
 
