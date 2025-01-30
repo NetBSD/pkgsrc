98ed41e94f00d9d9d039391345c43c23488ac80a
pho <pho@pkgsrc.org>
2025-01-30 04:33:39 +0000

    math/hs-integer-logarithms: update to integer-logarithms-1.0.4
    1.0.4
    -----
    
    - Drop support for GHCs prior 8.6.5
    - GHC-9.12 support

--- math/hs-integer-logarithms/buildlink3.mk
+++ math/hs-integer-logarithms/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/30 04:33:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-integer-logarithms
 
 .if !defined(HS_INTEGER_LOGARITHMS_BUILDLINK3_MK)
 HS_INTEGER_LOGARITHMS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-integer-logarithms+=	hs-integer-logarithms>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-integer-logarithms+=	hs-integer-logarithms>=1.0.3.1nb9
+BUILDLINK_API_DEPENDS.hs-integer-logarithms+=	hs-integer-logarithms>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-integer-logarithms+=	hs-integer-logarithms>=1.0.4
 BUILDLINK_PKGSRCDIR.hs-integer-logarithms?=	../../math/hs-integer-logarithms
 .endif	# HS_INTEGER_LOGARITHMS_BUILDLINK3_MK
 
