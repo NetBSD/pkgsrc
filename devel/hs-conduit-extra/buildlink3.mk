9d50a6d13f44a26e89d3c3802c132ebaf78ac088
pho <pho@pkgsrc.org>
2025-01-30 11:47:27 +0000

    devel/hs-conduit-extra: update to conduit-extra-1.3.7
    ## 1.3.7
    
    * Allow Data.Conduit.Network.Unix on Windows [#518](https://github.com/snoyberg/conduit/pull/518)

--- devel/hs-conduit-extra/buildlink3.mk
+++ devel/hs-conduit-extra/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:31:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/01/30 11:47:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-conduit-extra
 
 .if !defined(HS_CONDUIT_EXTRA_BUILDLINK3_MK)
 HS_CONDUIT_EXTRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-conduit-extra+=	hs-conduit-extra>=1.3.6
-BUILDLINK_ABI_DEPENDS.hs-conduit-extra+=	hs-conduit-extra>=1.3.6nb4
+BUILDLINK_API_DEPENDS.hs-conduit-extra+=	hs-conduit-extra>=1.3.7
+BUILDLINK_ABI_DEPENDS.hs-conduit-extra+=	hs-conduit-extra>=1.3.7
 BUILDLINK_PKGSRCDIR.hs-conduit-extra?=		../../devel/hs-conduit-extra
 
 .include "../../devel/hs-async/buildlink3.mk"
