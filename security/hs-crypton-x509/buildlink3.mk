e5f21dca35889f70d21229e8d69d5e37da78bb8b
pho <pho@pkgsrc.org>
2025-01-30 10:05:56 +0000

    security/hs-crypton-x509: update to crypton-x509-1.7.7
    No release notes have been provided by the upstream.

--- security/hs-crypton-x509/buildlink3.mk
+++ security/hs-crypton-x509/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 10:05:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypton-x509
 
 .if !defined(HS_CRYPTON_X509_BUILDLINK3_MK)
 HS_CRYPTON_X509_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypton-x509+=	hs-crypton-x509>=1.7.6
-BUILDLINK_ABI_DEPENDS.hs-crypton-x509+=	hs-crypton-x509>=1.7.6nb2
+BUILDLINK_API_DEPENDS.hs-crypton-x509+=	hs-crypton-x509>=1.7.7
+BUILDLINK_ABI_DEPENDS.hs-crypton-x509+=	hs-crypton-x509>=1.7.7
 BUILDLINK_PKGSRCDIR.hs-crypton-x509?=	../../security/hs-crypton-x509
 
 .include "../../devel/hs-asn1-encoding/buildlink3.mk"
