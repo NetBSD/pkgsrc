70a6c724de55f1b368e8f1abfd00a21b7bbe6740
pho <pho@pkgsrc.org>
2025-01-30 16:36:41 +0000

    www/hs-wai-extra: update to wai-extra-3.1.17
    ## 3.1.17
    
    * Started deprecation of `data-default` [#1011](https://github.com/yesodweb/wai/pull/1011)
      * All `Default` instances have comments that these will be removed in a future major version.
      * `def` exported from `Network.Wai.Middleware.Gzip` now has a deprecation warning
      * All uses of `def` have been replaced with explicit `default{TYPE_NAME}` values.
    * Some additional documentation
    
    ## 3.1.16
    
    * Substituted `data-default-class` for `data-default` [#1010](https://github.com/yesodweb/wai/pull/1010)

--- www/hs-wai-extra/buildlink3.mk
+++ www/hs-wai-extra/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 16:36:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-wai-extra
 
 .if !defined(HS_WAI_EXTRA_BUILDLINK3_MK)
 HS_WAI_EXTRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-wai-extra+=	hs-wai-extra>=3.1.14
-BUILDLINK_ABI_DEPENDS.hs-wai-extra+=	hs-wai-extra>=3.1.14nb1
+BUILDLINK_API_DEPENDS.hs-wai-extra+=	hs-wai-extra>=3.1.17
+BUILDLINK_ABI_DEPENDS.hs-wai-extra+=	hs-wai-extra>=3.1.17
 BUILDLINK_PKGSRCDIR.hs-wai-extra?=	../../www/hs-wai-extra
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -15,7 +15,7 @@ BUILDLINK_PKGSRCDIR.hs-wai-extra?=	../../www/hs-wai-extra
 .include "../../devel/hs-call-stack/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../www/hs-cookie/buildlink3.mk"
-.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../sysutils/hs-fast-logger/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../devel/hs-HUnit/buildlink3.mk"
