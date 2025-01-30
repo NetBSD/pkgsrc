c6ab92d28cf27be1475745c75ec463b3b1943596
pho <pho@pkgsrc.org>
2025-01-30 05:18:44 +0000

    devel/hs-data-default: update to 0.8.0.0
    No release notes have been provided by upstream.

--- devel/hs-data-default/buildlink3.mk
+++ devel/hs-data-default/buildlink3.mk
@@ -1,18 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:31:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/01/30 05:18:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-default
 
 .if !defined(HS_DATA_DEFAULT_BUILDLINK3_MK)
 HS_DATA_DEFAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-data-default+=	hs-data-default>=0.7.1.1nb8
+BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.8.0
+BUILDLINK_ABI_DEPENDS.hs-data-default+=	hs-data-default>=0.8.0.0
 BUILDLINK_PKGSRCDIR.hs-data-default?=	../../devel/hs-data-default
-
-.include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../devel/hs-data-default-instances-containers/buildlink3.mk"
-.include "../../devel/hs-data-default-instances-dlist/buildlink3.mk"
-.include "../../devel/hs-data-default-instances-old-locale/buildlink3.mk"
 .endif	# HS_DATA_DEFAULT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-data-default
