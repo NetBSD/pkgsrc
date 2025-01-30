0e4726b930beeacf2842debb2025deb190b2b8fd
pho <pho@pkgsrc.org>
2025-01-30 16:18:17 +0000

    net/hs-network-control: update to network-control-0.1.4
    ## 0.1.4
    
    * Using Integer instead of Int in LRUCache.
    
    ## 0.1.3
    
    * Simplify `maybeOpenRxWindow` and improve docs
      [#7](https://github.com/kazu-yamamoto/network-control/pull/7)
    
    ## 0.1.2
    
    * introducing a minimum size for window update
      [#5](https://github.com/kazu-yamamoto/network-control/pull/5)
    
    ## 0.1.1
    
    * Change defaultMaxData
      [#4](https://github.com/kazu-yamamoto/network-control/pull/4)

--- net/hs-network-control/buildlink3.mk
+++ net/hs-network-control/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/05/09 01:32:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/01/30 16:18:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-network-control
 
 .if !defined(HS_NETWORK_CONTROL_BUILDLINK3_MK)
 HS_NETWORK_CONTROL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network-control+=	hs-network-control>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-network-control+=	hs-network-control>=0.1.0nb1
+BUILDLINK_API_DEPENDS.hs-network-control+=	hs-network-control>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-network-control+=	hs-network-control>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-network-control?=	../../net/hs-network-control
 
 .include "../../devel/hs-psqueues/buildlink3.mk"
