51e80c4b64164d46bc3af4dacdf99b06607bfde7
pho <pho@pkgsrc.org>
2025-01-30 04:43:49 +0000

    time/hs-text-iso8601: update to text-iso8601-0.1.1
    # 0.1.1
    
    - Support GHC-8.6.5...9.10.1

--- time/hs-text-iso8601/buildlink3.mk
+++ time/hs-text-iso8601/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 04:43:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-iso8601
 
 .if !defined(HS_TEXT_ISO8601_BUILDLINK3_MK)
 HS_TEXT_ISO8601_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-iso8601+=	hs-text-iso8601>=0.1
-BUILDLINK_ABI_DEPENDS.hs-text-iso8601+=	hs-text-iso8601>=0.1nb2
+BUILDLINK_API_DEPENDS.hs-text-iso8601+=	hs-text-iso8601>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-text-iso8601+=	hs-text-iso8601>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-text-iso8601?=	../../time/hs-text-iso8601
 
 .include "../../math/hs-integer-conversion/buildlink3.mk"
