77f73975db0e7340353b61c31c4c717da360abf0
pho <pho@pkgsrc.org>
2025-01-30 12:48:54 +0000

    textproc/hs-typst-symbols: update to typst-symbols-0.1.7
    ## 0.1.7
    
    * Update to typst 0.12.

--- textproc/hs-typst-symbols/buildlink3.mk
+++ textproc/hs-typst-symbols/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:32:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/30 12:48:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-typst-symbols
 
 .if !defined(HS_TYPST_SYMBOLS_BUILDLINK3_MK)
 HS_TYPST_SYMBOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.6
-BUILDLINK_ABI_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.6nb1
+BUILDLINK_API_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.7
+BUILDLINK_ABI_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.7
 BUILDLINK_PKGSRCDIR.hs-typst-symbols?=		../../textproc/hs-typst-symbols
 .endif	# HS_TYPST_SYMBOLS_BUILDLINK3_MK
 
