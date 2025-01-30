932649f8736dba376b083238bb253642221281f8
pho <pho@pkgsrc.org>
2025-01-30 07:41:55 +0000

    devel/hs-reflection: update to reflection-2.1.9
    # 2.1.9 [2024.12.04]
    * Drop support for pre-8.0 versions of GHC.
    
    # 2.1.8 [2024.05.04]
    * Fix a memory leak in `reifyTypeable`.

--- devel/hs-reflection/buildlink3.mk
+++ devel/hs-reflection/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:32:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/30 07:41:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-reflection
 
 .if !defined(HS_REFLECTION_BUILDLINK3_MK)
 HS_REFLECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.7
-BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.7nb2
+BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.9
+BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.9
 BUILDLINK_PKGSRCDIR.hs-reflection?=	../../devel/hs-reflection
 .endif	# HS_REFLECTION_BUILDLINK3_MK
 
