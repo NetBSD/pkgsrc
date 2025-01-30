f2076ad377a7f935ef5470d009d6934aab5a910d
pho <pho@pkgsrc.org>
2025-01-30 11:37:08 +0000

    devel/hs-th-lift: update to th-lift-0.8.6
    ## [0.8.6] - 2024.12.05
    
    * Support building with `template-haskell-2.23.0`.
    
    ## [0.8.5] - 2024.10.22
    
    * Support building with `template-haskell-2.22.1`.
    * Drop support for pre-8.0 versions of GHC.

--- devel/hs-th-lift/buildlink3.mk
+++ devel/hs-th-lift/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:15 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/30 11:37:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-th-lift
 
 .if !defined(HS_TH_LIFT_BUILDLINK3_MK)
 HS_TH_LIFT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.4
-BUILDLINK_ABI_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.4nb2
+BUILDLINK_API_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.6
+BUILDLINK_ABI_DEPENDS.hs-th-lift+=	hs-th-lift>=0.8.6
 BUILDLINK_PKGSRCDIR.hs-th-lift?=	../../devel/hs-th-lift
 
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
