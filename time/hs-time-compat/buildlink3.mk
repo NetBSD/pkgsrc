d64be41585a46b61968f1e9869c31591c7e036ee
pho <pho@pkgsrc.org>
2025-01-30 04:41:09 +0000

    time/hs-time-compat: update to time-compat-1.9.8
    # 1.9.7
    
    - Drop support for GHCs prior 8.6.5

--- time/hs-time-compat/buildlink3.mk
+++ time/hs-time-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:32:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/30 04:41:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-time-compat
 
 .if !defined(HS_TIME_COMPAT_BUILDLINK3_MK)
 HS_TIME_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-time-compat+=	hs-time-compat>=1.9.6
-BUILDLINK_ABI_DEPENDS.hs-time-compat?=	hs-time-compat>=1.9.6.1nb6
+BUILDLINK_API_DEPENDS.hs-time-compat+=	hs-time-compat>=1.9.8
+BUILDLINK_ABI_DEPENDS.hs-time-compat+=	hs-time-compat>=1.9.8
 BUILDLINK_PKGSRCDIR.hs-time-compat?=	../../time/hs-time-compat
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
