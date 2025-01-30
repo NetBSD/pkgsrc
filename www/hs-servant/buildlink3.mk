631cbbae02a45d85a0bee31ef171acaed8752cb4
pho <pho@pkgsrc.org>
2025-01-30 15:54:10 +0000

    0.20.2 ---- - Full query string helpers [#1604](https://github.com/haskell-servant/servant/pull/1604)
      This PR introduces `DeepQuery`, a route combinator that implements a pattern commonly known as deep objects.
      It builds upon the convention of using `[]` for a list of parameters:
      `books?filter[search]=value&filter[author][name]=value`.
      The corresponding type would be `DeepQuery "filter" BookQuery :> Get '[JSON] [Book]`.
    - Add IsIn instance for NamedRoutes [#1707](https://github.com/haskell-servant/servant/pull/1707)
    - Renamed `AtLeastOneFragment` type class to `AtMostOneFragment` [#1727](https://github.com/haskell-servant/servant/pull/1727)
    
      The previously named `AtLeastOneFragment` type class defined in the
      `Servant.API.TypeLevel` module has been renamed to `AtMostOneFragment`,
      since the previous name was misleading.
    - Use `Header'` in response headers. [#1697](https://github.com/haskell-servant/servant/pull/1697)
    
      Use `Header'` instead of `Header` in response, so it's possible to provide
      `Description`, for example:
    
      ```
      type PaginationTotalCountHeader =
        Header'
          '[ Description "Indicates to the client total count of items in collection"
           , Optional
           , Strict
           ]
          "Total-Count"
          Int
      ```
    
      Note: if you want to add header with description you should use `addHeader'`
      or `noHeader'` which accepts `Header'` with all modifiers.

--- www/hs-servant/buildlink3.mk
+++ www/hs-servant/buildlink3.mk
@@ -1,30 +1,27 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 15:54:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-servant
 
 .if !defined(HS_SERVANT_BUILDLINK3_MK)
 HS_SERVANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-servant+=	hs-servant>=0.20.1
-BUILDLINK_ABI_DEPENDS.hs-servant+=	hs-servant>=0.20.1nb2
+BUILDLINK_API_DEPENDS.hs-servant+=	hs-servant>=0.20.2
+BUILDLINK_ABI_DEPENDS.hs-servant+=	hs-servant>=0.20.2
 BUILDLINK_PKGSRCDIR.hs-servant?=	../../www/hs-servant
 
+.include "../../devel/hs-constraints/buildlink3.mk"
+.include "../../devel/hs-sop-core/buildlink3.mk"
+.include "../../www/hs-http-api-data/buildlink3.mk"
+.include "../../devel/hs-singleton-bool/buildlink3.mk"
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
-.include "../../devel/hs-base-compat/buildlink3.mk"
 .include "../../math/hs-bifunctors/buildlink3.mk"
 .include "../../textproc/hs-case-insensitive/buildlink3.mk"
-.include "../../devel/hs-constraints/buildlink3.mk"
-.include "../../www/hs-http-api-data/buildlink3.mk"
 .include "../../www/hs-http-media/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../devel/hs-mmorph/buildlink3.mk"
 .include "../../net/hs-network-uri/buildlink3.mk"
 .include "../../devel/hs-QuickCheck/buildlink3.mk"
-.include "../../devel/hs-singleton-bool/buildlink3.mk"
-.include "../../devel/hs-sop-core/buildlink3.mk"
-.include "../../converters/hs-string-conversions/buildlink3.mk"
-.include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-vault/buildlink3.mk"
 .endif	# HS_SERVANT_BUILDLINK3_MK
 
