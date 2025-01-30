960db559a0d89c7d20738ef4edbb447351125061
pho <pho@pkgsrc.org>
2025-01-30 04:27:04 +0000

    devel/hs-uuid-types: update to uuid-types-1.0.6
    ## 1.0.6 (2023-04-16)
    
    - Support GHC-8.6.5..GHC-9.10.1

--- devel/hs-uuid-types/buildlink3.mk
+++ devel/hs-uuid-types/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:18 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/30 04:27:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-uuid-types
 
 .if !defined(HS_UUID_TYPES_BUILDLINK3_MK)
 HS_UUID_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.5.1
-BUILDLINK_ABI_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.5.1nb2
+BUILDLINK_API_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.6
+BUILDLINK_ABI_DEPENDS.hs-uuid-types+=	hs-uuid-types>=1.0.6
 BUILDLINK_PKGSRCDIR.hs-uuid-types?=	../../devel/hs-uuid-types
 
 .include "../../devel/hs-hashable/buildlink3.mk"
