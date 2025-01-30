f96308931e4816c1b087b6f0246a8e400d5c9543
pho <pho@pkgsrc.org>
2025-01-30 10:19:34 +0000

    time/hs-unix-time: update to unix-time-0.4.16
    # ChangeLog for unix-time
    
    ## v0.4.16
    
    * Make `tzset` optional.
      [#65](https://github.com/kazu-yamamoto/unix-time/pull/65)

--- time/hs-unix-time/buildlink3.mk
+++ time/hs-unix-time/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/30 10:19:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-unix-time
 
 .if !defined(HS_UNIX_TIME_BUILDLINK3_MK)
 HS_UNIX_TIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.12
-BUILDLINK_ABI_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.12nb1
+BUILDLINK_API_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.16
+BUILDLINK_ABI_DEPENDS.hs-unix-time+=	hs-unix-time>=0.4.16
 BUILDLINK_PKGSRCDIR.hs-unix-time?=	../../time/hs-unix-time
 
 .include "../../time/hs-old-time/buildlink3.mk"
