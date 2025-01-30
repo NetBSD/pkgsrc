7d41f2d41969b28691ca2e61425c8150d06e69e3
pho <pho@pkgsrc.org>
2025-01-30 12:02:07 +0000

    textproc/hs-unicode-data: update to unicode-data-0.6.0
    ## 0.6.0 (July 2024)
    
    - Updated to [Unicode 15.1.0](https://www.unicode.org/versions/Unicode15.1.0/).
    - Added `showCodePoint` to `Unicode.Char`.
    - Added `intToDigiT` to `Unicode.Char.Numeric`.
    
    ### Removed
    
    - Removed deprecated `isLetter` and `isSpace` from `Unicode.Char.General`.
      Use the corresponding functions from `Unicode.Char.General.Compat` instead.
    - Remove deprecated `isLower` and `isUpper` from `Unicode.Char.Case`.
      Use the corresponding functions from `Unicode.Char.Case.Compat` instead.
    - Removed deprecated `Unicode.Char.Numeric.isNumber`.
      Use `Unicode.Char.Numeric.Compat.isNumber` instead.
    
    ### Deprecations
    
    - `Unicode.Char.General.isAlphaNum`.
      Use `Unicode.Char.General.Compat.isAlphaNum` instead.
    
    ## 0.5.0 (July 2024)
    
    - Fix the inlining of `Addr#` literals and reduce their size. This results in
      a sensible decrease of the executable size.
    - Changed `integerValue` from `Char -> Maybe Int` to `(Integral a) => Char -> Maybe a`.

--- textproc/hs-unicode-data/buildlink3.mk
+++ textproc/hs-unicode-data/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/05/09 01:32:49 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/01/30 12:02:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-unicode-data
 
 .if !defined(HS_UNICODE_DATA_BUILDLINK3_MK)
 HS_UNICODE_DATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.4
-BUILDLINK_ABI_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.4.0.1nb4
+BUILDLINK_API_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.6.0
+BUILDLINK_ABI_DEPENDS.hs-unicode-data+=	hs-unicode-data>=0.6.0
 BUILDLINK_PKGSRCDIR.hs-unicode-data?=	../../textproc/hs-unicode-data
 .endif	# HS_UNICODE_DATA_BUILDLINK3_MK
 
