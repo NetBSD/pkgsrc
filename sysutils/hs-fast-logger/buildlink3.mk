fda5e9dade140a78b293f61a9faccec5e7820175
pho <pho@pkgsrc.org>
2025-01-30 16:03:45 +0000

    sysutils/hs-fast-logger: update to fast-logger-3.2.5
    ## 3.2.5
    
    * Giving names to threads.
    
    ## 3.2.4
    
    * Avoid unnecessary copy for Text values with text-2.0
      [#219](https://github.com/kazu-yamamoto/logger/pull/219)
    
    ## 3.2.3
    
    * Ensuring flush for single logger.
      [#214](https://github.com/kazu-yamamoto/logger/pull/214)

--- sysutils/hs-fast-logger/buildlink3.mk
+++ sysutils/hs-fast-logger/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:36 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/30 16:03:45 pho Exp $
 
 BUILDLINK_TREE+=	hs-fast-logger
 
 .if !defined(HS_FAST_LOGGER_BUILDLINK3_MK)
 HS_FAST_LOGGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.2.2
-BUILDLINK_ABI_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.2.2nb2
+BUILDLINK_API_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.2.5
+BUILDLINK_ABI_DEPENDS.hs-fast-logger+=	hs-fast-logger>=3.2.5
 BUILDLINK_PKGSRCDIR.hs-fast-logger?=	../../sysutils/hs-fast-logger
 
 .include "../../devel/hs-auto-update/buildlink3.mk"
