a924167cba97e23cda06f30ae506b0b6478ef010
pho <pho@pkgsrc.org>
2025-01-30 16:15:58 +0000

    devel/hs-psqueues: update to psqueues-0.2.8.1
    - 0.2.8.1 (2025-01-28)
        * Fix performance issue in OrdPSQ relating to balancing (#61).
        * Relax hashable upper bound to 1.5
        * Relax QuickCheck upper bound to 2.15
        * Relax tasty-quickcheck upper bound to 0.11

--- devel/hs-psqueues/buildlink3.mk
+++ devel/hs-psqueues/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/30 16:15:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-psqueues
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-psqueues
 HS_PSQUEUES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.8
-BUILDLINK_ABI_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.8.0nb1
+BUILDLINK_ABI_DEPENDS.hs-psqueues+=	hs-psqueues>=0.2.8.1
 BUILDLINK_PKGSRCDIR.hs-psqueues?=	../../devel/hs-psqueues
 
 .include "../../devel/hs-hashable/buildlink3.mk"
