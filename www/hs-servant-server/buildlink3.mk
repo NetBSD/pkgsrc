13387cd1b69b0328df63360b4482e335debbe9bc
pho <pho@pkgsrc.org>
2025-01-30 16:44:29 +0000

    www/hs-servant-server: update to servant-server-0.20.2
    0.20.2
    ----
    
    - Fix build of examples/greet.hs. Add "429 Too Many Requests" error. [#1591](https://github.com/haskell-servant/servant/pull/1591)
    - Full query string helpers [#1604](https://github.com/haskell-servant/servant/pull/1604)
      This involves a new instance `HasServer (QueryString :> api) context`.
    - Add `MkHandler` pattern synonym [#1732](https://github.com/haskell-servant/servant/issues/1732) [#1733](https://github.com/haskell-servant/servant/pull/1733)
    
      Add a bidirectional pattern synonym to construct `Handler a` values from `IO
      (Either ServerError a)` ones, and match in the other direction.
    - Add instance `HasServer (EmptyAPI :> api) context` [#1775](https://github.com/haskell-servant/servant/pull/1775)
    - Bugfix - CaptureAll produces [""] for empty paths due to trailing slash. [#1243](https://github.com/haskell-servant/servant/issues/1243) [#1516](https://github.com/haskell-servant/servant/pull/1516)
    
      CaptureAll resulted in `[""]` for empty paths due to trailing slash.  Similar
      oddities occurred around these edge cases like `"/"` resulted in `[]` correctly,
      but `"//"` resulted in `["", ""]`.  This patch simply eliminates the first `""`
      in the pathinfo list as taken from the wai response.  This might break user
      code that relies on personal shims to solve the problem, however simply removing their
      workarounds should fix their code as the behavior is now sane.

--- www/hs-servant-server/buildlink3.mk
+++ www/hs-servant-server/buildlink3.mk
@@ -1,27 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 16:44:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-servant-server
 
 .if !defined(HS_SERVANT_SERVER_BUILDLINK3_MK)
 HS_SERVANT_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-servant-server+=	hs-servant-server>=0.20
-BUILDLINK_ABI_DEPENDS.hs-servant-server+=	hs-servant-server>=0.20nb2
+BUILDLINK_API_DEPENDS.hs-servant-server+=	hs-servant-server>=0.20.2
+BUILDLINK_ABI_DEPENDS.hs-servant-server+=	hs-servant-server>=0.20.2
 BUILDLINK_PKGSRCDIR.hs-servant-server?=		../../www/hs-servant-server
 
-.include "../../devel/hs-base-compat/buildlink3.mk"
-.include "../../converters/hs-base64-bytestring/buildlink3.mk"
 .include "../../devel/hs-constraints/buildlink3.mk"
 .include "../../www/hs-http-api-data/buildlink3.mk"
+.include "../../www/hs-servant/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
 .include "../../www/hs-http-media/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
 .include "../../devel/hs-monad-control/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
-.include "../../net/hs-network-uri/buildlink3.mk"
 .include "../../devel/hs-resourcet/buildlink3.mk"
-.include "../../www/hs-servant/buildlink3.mk"
 .include "../../devel/hs-sop-core/buildlink3.mk"
-.include "../../converters/hs-string-conversions/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
 .include "../../www/hs-wai/buildlink3.mk"
