e2bf85be3d66f2d16fab47f49cee64d5a84da26f
pho <pho@pkgsrc.org>
2025-01-30 05:09:15 +0000

    devel/hs-binary-orphans: update to binary-orphans-1.0.5
    # 1.0.5
    
    - Support GHC-8.6.5..GHC-9.10.1

--- devel/hs-binary-orphans/buildlink3.mk
+++ devel/hs-binary-orphans/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:31:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 05:09:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-binary-orphans
 
 .if !defined(HS_BINARY_ORPHANS_BUILDLINK3_MK)
 HS_BINARY_ORPHANS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-binary-orphans+=	hs-binary-orphans>=1.0.4
-BUILDLINK_ABI_DEPENDS.hs-binary-orphans+=	hs-binary-orphans>=1.0.4.1nb2
+BUILDLINK_API_DEPENDS.hs-binary-orphans+=	hs-binary-orphans>=1.0.5
+BUILDLINK_ABI_DEPENDS.hs-binary-orphans+=	hs-binary-orphans>=1.0.5
 BUILDLINK_PKGSRCDIR.hs-binary-orphans?=		../../devel/hs-binary-orphans
 .endif	# HS_BINARY_ORPHANS_BUILDLINK3_MK
 
