c838ca09402a10ad3ed34e7eace2fb526c21da56
pho <pho@pkgsrc.org>
2025-01-30 23:58:33 +0000

    devel/hs-silently: update to silently-1.2.5.4
    # 1.2.5.4 November 2024
    
    * Bump `cabal-version` to 1.18.
    * README: alert of malfunction in multi-threaded settings.
    * Tested with GHC 8.0 - 9.12.0.

--- devel/hs-silently/buildlink3.mk
+++ devel/hs-silently/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 23:58:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-silently
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-silently
 HS_SILENTLY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-silently+=	hs-silently>=1.2.5
-BUILDLINK_ABI_DEPENDS.hs-silently+=	hs-silently>=1.2.5.3nb2
+BUILDLINK_ABI_DEPENDS.hs-silently+=	hs-silently>=1.2.5.4
 BUILDLINK_PKGSRCDIR.hs-silently?=	../../devel/hs-silently
 .endif	# HS_SILENTLY_BUILDLINK3_MK
 
