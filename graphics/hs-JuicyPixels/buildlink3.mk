b5e6c327f49b8d2c00ee85a881c008ef2441a3da
pho <pho@pkgsrc.org>
2025-01-30 09:50:50 +0000

    graphics/hs-JuicyPixels: update to JuicyPixels-3.3.9
    v3.3.9 June 2024
    ----------------
    
     * Something something compilation

--- graphics/hs-JuicyPixels/buildlink3.mk
+++ graphics/hs-JuicyPixels/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:32:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/30 09:50:50 pho Exp $
 
 BUILDLINK_TREE+=	hs-JuicyPixels
 
 .if !defined(HS_JUICYPIXELS_BUILDLINK3_MK)
 HS_JUICYPIXELS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.6
-BUILDLINK_ABI_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.8nb4
+BUILDLINK_API_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.9
+BUILDLINK_ABI_DEPENDS.hs-JuicyPixels+=	hs-JuicyPixels>=3.3.9
 BUILDLINK_PKGSRCDIR.hs-JuicyPixels?=	../../graphics/hs-JuicyPixels
 
-.include "../../devel/hs-primitive/buildlink3.mk"
-.include "../../devel/hs-vector/buildlink3.mk"
 .include "../../archivers/hs-zlib/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
 .endif	# HS_JUICYPIXELS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-JuicyPixels
