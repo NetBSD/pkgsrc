470cadf8417f94aaee366e0401e53d01915e08db
pho <pho@pkgsrc.org>
2025-01-30 04:23:20 +0000

    devel/hs-semialign: update to semialign-1.3.1
    # 1.3.1
    
    - Support GHC-8.6.5...GHC-9.10.1

--- devel/hs-semialign/buildlink3.mk
+++ devel/hs-semialign/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/05/09 01:32:11 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/30 04:23:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-semialign
 
 .if !defined(HS_SEMIALIGN_BUILDLINK3_MK)
 HS_SEMIALIGN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semialign+=	hs-semialign>=1.3
-BUILDLINK_ABI_DEPENDS.hs-semialign+=	hs-semialign>=1.3nb2
+BUILDLINK_API_DEPENDS.hs-semialign+=	hs-semialign>=1.3.1
+BUILDLINK_ABI_DEPENDS.hs-semialign+=	hs-semialign>=1.3.1
 BUILDLINK_PKGSRCDIR.hs-semialign?=	../../devel/hs-semialign
 
 .include "../../devel/hs-hashable/buildlink3.mk"
