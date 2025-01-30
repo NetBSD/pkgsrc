e86b20628195954421e9e22f628811c8877f2318
pho <pho@pkgsrc.org>
2025-01-30 04:20:41 +0000

    math/hs-semigroupoids: update to semigroupoids-6.0.1
    6.0.1 [2024.05.04]
    ------------------
    * Fix a build error when compiling with `-f-contravariant`.

--- math/hs-semigroupoids/buildlink3.mk
+++ math/hs-semigroupoids/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:32:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/30 04:20:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=6.0.0
-BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=6.0.0.1nb2
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=6.0.1
+BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=6.0.1
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=		../../math/hs-semigroupoids
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
