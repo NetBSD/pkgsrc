f0d6e67ca8b17814b966b6bdea10d730f05539b2
pho <pho@pkgsrc.org>
2025-01-30 05:04:55 +0000

    benchmarks/hs-criterion-measurement: update to criterion-measurement-0.2.3.0
    0.2.3.0
    
    * Drop support for pre-8.0 versions of GHC.
    
    0.2.2.0
    
    * Supporting building with all AArch64 platforms (not just Linux and macOS).

--- benchmarks/hs-criterion-measurement/buildlink3.mk
+++ benchmarks/hs-criterion-measurement/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:31:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 05:04:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-criterion-measurement
 
 .if !defined(HS_CRITERION_MEASUREMENT_BUILDLINK3_MK)
 HS_CRITERION_MEASUREMENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-criterion-measurement+=	hs-criterion-measurement>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-criterion-measurement+=	hs-criterion-measurement>=0.2.1.0nb2
+BUILDLINK_API_DEPENDS.hs-criterion-measurement+=	hs-criterion-measurement>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-criterion-measurement+=	hs-criterion-measurement>=0.2.3.0
 BUILDLINK_PKGSRCDIR.hs-criterion-measurement?=		../../benchmarks/hs-criterion-measurement
 
 .include "../../converters/hs-aeson/buildlink3.mk"
