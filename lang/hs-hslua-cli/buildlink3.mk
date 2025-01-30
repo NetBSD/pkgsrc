af5ec3f481157177ef3695bf99a5d5fc586f5699
pho <pho@pkgsrc.org>
2025-01-30 23:43:25 +0000

    lang/hs-hslua-cli: update to hslua-cli-1.4.3
    ## hslua-cli-1.4.3
    
    Release pending.
    
    -   Include copyright notice when printing version info; i.e., the
        behavior is now closer to that of the default `lua`
        interpreter.
    
    -   Avoid "redundant import" warning when compiling with GHC 9.10.

--- lang/hs-hslua-cli/buildlink3.mk
+++ lang/hs-hslua-cli/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:22 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/30 23:43:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-hslua-cli
 
 .if !defined(HS_HSLUA_CLI_BUILDLINK3_MK)
 HS_HSLUA_CLI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.4.2
-BUILDLINK_ABI_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.4.2nb1
+BUILDLINK_API_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.4.3
+BUILDLINK_ABI_DEPENDS.hs-hslua-cli+=	hs-hslua-cli>=1.4.3
 BUILDLINK_PKGSRCDIR.hs-hslua-cli?=	../../lang/hs-hslua-cli
 
 .include "../../lang/hs-hslua-core/buildlink3.mk"
