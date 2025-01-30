7292d3fbdf5fba12a538c86b3c8493119d66805a
pho <pho@pkgsrc.org>
2025-01-30 16:00:10 +0000

    devel/hs-auto-update: update to auto-update-0.2.6
    ## 0.2.6
    
    * Using the thread version of AutoUpdate for non-threaded RTS.
      [#1020](https://github.com/yesodweb/wai/pull/1020)
    
    ## 0.2.5
    
    * Thread less autoupdate
      [#1018](https://github.com/yesodweb/wai/pull/1018)
    
    ## 0.2.4
    
    * Simple refactoring.
    
    ## 0.2.3
    
    * [#996](https://github.com/yesodweb/wai/pull/996):
      Refactored the `Control.Debounce` logic to not leak threads.
    * [#996](https://github.com/yesodweb/wai/pull/996):
      Added extra `DebounceEdge` options for different types of debouncing.
      * `LeadingMute`: Action on first trigger, and ignore any triggers during cooldown
      * `TrailingDelay`: First trigger starts cooldown, and
        triggers during cooldown extend the cooldown. Action when cooldown expires.
    
    ## 0.2.2
    
    * NewAPI: updateThreadName, reaperThreadName, debounceThreadName:
      Names can be given via this field to threads
      for GHC.Conc.Sync.listThreads.
    
    ## 0.2.1
    
    * Labeling threads.

--- devel/hs-auto-update/buildlink3.mk
+++ devel/hs-auto-update/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/05/09 01:31:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/30 16:00:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-auto-update
 
 .if !defined(HS_AUTO_UPDATE_BUILDLINK3_MK)
 HS_AUTO_UPDATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-auto-update+=	hs-auto-update>=0.2.0
-BUILDLINK_ABI_DEPENDS.hs-auto-update+=	hs-auto-update>=0.2.0nb1
+BUILDLINK_API_DEPENDS.hs-auto-update+=	hs-auto-update>=0.2.6
+BUILDLINK_ABI_DEPENDS.hs-auto-update+=	hs-auto-update>=0.2.6
 BUILDLINK_PKGSRCDIR.hs-auto-update?=	../../devel/hs-auto-update
 .endif	# HS_AUTO_UPDATE_BUILDLINK3_MK
 
