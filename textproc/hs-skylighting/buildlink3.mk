40a76b94ea22c4a5433067df6293bf266ef04a9d
pho <pho@pkgsrc.org>
2025-01-30 12:44:44 +0000

    textproc/hs-skylighting: update to skylighting-0.14.6
    ## 0.14.6
    
      * Export Skylighting.Format.Typst from Skylighting [API change] (#201).
        Allow typst as an output format in the cli executable.
    
    ## 0.14.5
    
      * Update xml syntax definitions for bash, cmake, commonlisp, isocpp,
        javascript-react, julia, latex, lua, markdown, modelines, nix, orgmode,
        php, python, rhtml, ruby, swift, xml, yaml, zig, zsh. Add odin (required
        by orgmode).
    
      * Update JSON syntax definition from upstream. (#203)
    
    ## 0.14.4
    
      * Add `gdscript`, `typst`, `tlaplus` syntax (#184, #199).
    
    ## 0.14.3
    
      * Add crystal, racket, zip syntax (#194).
    
    ## 0.14.2
    
      * Add `loadValidSyntaxesFromDir` (Kevin Quick) [API change].
        The `loadSyntaxesFromDir` function is an all-or-nothing function:
        a single invalid file results in a error and *no* loaded syntaxes.
        This adds the `loadValidSyntaxesFromDir`, which is resilient
        against individual syntax file load failures.  It returns a map
        of the failure messages, and the SyntaxMap that is created from
        all the successful parsing.
    
    ## 0.14.1.2
    
      * Add terraform syntax (#190).

--- textproc/hs-skylighting/buildlink3.mk
+++ textproc/hs-skylighting/buildlink3.mk
@@ -1,19 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.17 2024/05/09 01:32:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2025/01/30 12:44:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-skylighting
 
 .if !defined(HS_SKYLIGHTING_BUILDLINK3_MK)
 HS_SKYLIGHTING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.14.1
-BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.14.1.1nb1
+BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.14.6
+BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.14.6
 BUILDLINK_PKGSRCDIR.hs-skylighting?=	../../textproc/hs-skylighting
 
 .include "../../textproc/hs-skylighting-core/buildlink3.mk"
 .include "../../textproc/hs-skylighting-format-ansi/buildlink3.mk"
-.include "../../textproc/hs-skylighting-format-blaze-html/buildlink3.mk"
 .include "../../textproc/hs-skylighting-format-context/buildlink3.mk"
 .include "../../textproc/hs-skylighting-format-latex/buildlink3.mk"
+.include "../../textproc/hs-skylighting-format-blaze-html/buildlink3.mk"
+.include "../../textproc/hs-skylighting-format-typst/buildlink3.mk"
 .endif	# HS_SKYLIGHTING_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-skylighting
