3bbdb829bdda685f0e3deea6e0caa4212d933590
pho <pho@pkgsrc.org>
2025-01-30 10:04:15 +0000

    security/hs-crypton: update to crypton-1.0.1
    ## 1.0.1
    
    * Update decaf library.
      [#38](https://github.com/kazu-yamamoto/crypton/pull/38)
    * Add TypeOperators language extension to EdDSA.hs.
      [#36](https://github.com/kazu-yamamoto/crypton/pull/36)

--- security/hs-crypton/buildlink3.mk
+++ security/hs-crypton/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:32:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/30 10:04:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypton
 
 .if !defined(HS_CRYPTON_BUILDLINK3_MK)
 HS_CRYPTON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypton+=	hs-crypton>=1.0.0
-BUILDLINK_ABI_DEPENDS.hs-crypton+=	hs-crypton>=1.0.0nb1
+BUILDLINK_API_DEPENDS.hs-crypton+=	hs-crypton>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-crypton+=	hs-crypton>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-crypton?=	../../security/hs-crypton
 
 .include "../../devel/hs-basement/buildlink3.mk"
