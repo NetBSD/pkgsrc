80760bda6f6ba198819324945a35b9e4e6828d40
pho <pho@pkgsrc.org>
2025-01-30 11:20:42 +0000

    net/hs-crypton-connection: update to crypton-connection-0.4.3
    # CHANGELOG
    
    ## Version 0.4.3
    
    * Creating the `Internal` module and export the `ConnectionContext` constructor.
      [#7](https://github.com/kazu-yamamoto/crypton-connection/pull/7)
    
    ## Version 0.4.2
    
    * Using data-default.
    
    ## Version 0.4.1
    
    * Preparing for tls v2.1
    
    ## Version 0.4.0
    
    * Add support for overriding the TLS clientSupported member in TLSSettingsSimple
      [#3](https://github.com/kazu-yamamoto/crypton-connection/pull/3)

--- net/hs-crypton-connection/buildlink3.mk
+++ net/hs-crypton-connection/buildlink3.mk
@@ -1,23 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:32:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/30 11:20:42 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypton-connection
 
 .if !defined(HS_CRYPTON_CONNECTION_BUILDLINK3_MK)
 HS_CRYPTON_CONNECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.2
-BUILDLINK_ABI_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.3.2nb1
+BUILDLINK_API_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.4.3
+BUILDLINK_ABI_DEPENDS.hs-crypton-connection+=	hs-crypton-connection>=0.4.3
 BUILDLINK_PKGSRCDIR.hs-crypton-connection?=	../../net/hs-crypton-connection
 
 .include "../../devel/hs-basement/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.include "../../net/hs-socks/buildlink3.mk"
 .include "../../security/hs-crypton-x509/buildlink3.mk"
 .include "../../security/hs-crypton-x509-store/buildlink3.mk"
 .include "../../security/hs-crypton-x509-system/buildlink3.mk"
 .include "../../security/hs-crypton-x509-validation/buildlink3.mk"
-.include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../net/hs-network/buildlink3.mk"
-.include "../../net/hs-socks/buildlink3.mk"
-.include "../../security/hs-tls/buildlink3.mk"
 .endif	# HS_CRYPTON_CONNECTION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-crypton-connection
