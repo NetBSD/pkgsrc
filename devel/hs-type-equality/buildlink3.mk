ec8de8fce184db5182b001c5e028f1a23c6e723b
pho <pho@pkgsrc.org>
2025-01-30 14:43:35 +0000

    devel/hs-type-equality: update to type-equality-1.0.1
    ## 1.0.1
    
      * Support GHC-8.6.5..GHC-9.10.1

--- devel/hs-type-equality/buildlink3.mk
+++ devel/hs-type-equality/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:17 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 14:43:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-type-equality
 
 .if !defined(HS_TYPE_EQUALITY_BUILDLINK3_MK)
 HS_TYPE_EQUALITY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-type-equality+=	hs-type-equality>=1
-BUILDLINK_ABI_DEPENDS.hs-type-equality+=	hs-type-equality>=1nb5
+BUILDLINK_API_DEPENDS.hs-type-equality+=	hs-type-equality>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-type-equality+=	hs-type-equality>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-type-equality?=		../../devel/hs-type-equality
 .endif	# HS_TYPE_EQUALITY_BUILDLINK3_MK
 
