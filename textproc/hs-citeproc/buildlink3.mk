2ecb6064481cfdfb604e4a0ad6d03962ddb40b40
pho <pho@pkgsrc.org>
2025-01-30 11:57:54 +0000

    textproc/hs-citeproc: update to citeproc-0.8.1.2
    ## 0.8.1.2
    
      * Allow containers 0.7 (#143)
    
      * Update tests to use Diff >= 1.0 (#146).
    
      * Fix `dropTextWhile` and `dropTextWhileEnd` in Citeproc.Pandoc.
        Ensure that they treat SoftBreak like Space (jgm/pandoc#10451).
    
    ## 0.8.1.1
    
      * Include `10/` prefix in short DOI links (#136).
    
      * Properly implement `demote-non-dropping-particle="sort-only"` (#141).
        We had previously gotten sorting behavior right for this, but
        not display behavior.

--- textproc/hs-citeproc/buildlink3.mk
+++ textproc/hs-citeproc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2024/05/09 01:32:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/01/30 11:57:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-citeproc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-citeproc
 HS_CITEPROC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-citeproc+=	hs-citeproc>=0.8.1
-BUILDLINK_ABI_DEPENDS.hs-citeproc+=	hs-citeproc>=0.8.1nb4
+BUILDLINK_ABI_DEPENDS.hs-citeproc+=	hs-citeproc>=0.8.1.2
 BUILDLINK_PKGSRCDIR.hs-citeproc?=	../../textproc/hs-citeproc
 
 .include "../../converters/hs-aeson/buildlink3.mk"
