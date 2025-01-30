abacae4473a657e94119ee849d01381614ed1219
pho <pho@pkgsrc.org>
2025-01-30 07:16:19 +0000

    devel/hs-half: update to half-0.3.2
    0.3.2 [2024.12.04]
    ------------------
    * Drop support for pre-8.0 versions of GHC.

--- devel/hs-half/buildlink3.mk
+++ devel/hs-half/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:31:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 07:16:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-half
 
 .if !defined(HS_HALF_BUILDLINK3_MK)
 HS_HALF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-half+=	hs-half>=0.3.1
-BUILDLINK_ABI_DEPENDS.hs-half+=	hs-half>=0.3.1nb5
+BUILDLINK_API_DEPENDS.hs-half+=	hs-half>=0.3.2
+BUILDLINK_ABI_DEPENDS.hs-half+=	hs-half>=0.3.2
 BUILDLINK_PKGSRCDIR.hs-half?=	../../devel/hs-half
 .endif	# HS_HALF_BUILDLINK3_MK
 
