bf4c5561e3de5bf650975841501962898ee319cc
pho <pho@pkgsrc.org>
2025-01-30 04:32:02 +0000

    math/hs-integer-conversion: update to integer-conversion-0.1.1
    ## 1.0.1
    
    - Support GHC-8.6.5..9.10.1

--- math/hs-integer-conversion/buildlink3.mk
+++ math/hs-integer-conversion/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:26 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 04:32:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-integer-conversion
 
 .if !defined(HS_INTEGER_CONVERSION_BUILDLINK3_MK)
 HS_INTEGER_CONVERSION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-integer-conversion+=	hs-integer-conversion>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-integer-conversion+=	hs-integer-conversion>=0.1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-integer-conversion+=	hs-integer-conversion>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-integer-conversion+=	hs-integer-conversion>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-integer-conversion?=	../../math/hs-integer-conversion
 
 .include "../../devel/hs-primitive/buildlink3.mk"
