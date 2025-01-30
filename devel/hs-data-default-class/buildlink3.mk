29aea6d2028e146c49035cd6d9957384965c5710
pho <pho@pkgsrc.org>
2025-01-30 05:20:55 +0000

    devel/hs-data-default-class: update to data-default-class-0.2.0.0
    No release notes have been provided by the upstream.

--- devel/hs-data-default-class/buildlink3.mk
+++ devel/hs-data-default-class/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:31:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/30 05:20:55 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-default-class
 
 .if !defined(HS_DATA_DEFAULT_CLASS_BUILDLINK3_MK)
 HS_DATA_DEFAULT_CLASS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default-class+=	hs-data-default-class>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-data-default-class+=	hs-data-default-class>=0.1.2.0nb8
+BUILDLINK_API_DEPENDS.hs-data-default-class+=	hs-data-default-class>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-data-default-class+=	hs-data-default-class>=0.2.0.0
 BUILDLINK_PKGSRCDIR.hs-data-default-class?=	../../devel/hs-data-default-class
+
+.include "../../devel/hs-data-default/buildlink3.mk"
 .endif	# HS_DATA_DEFAULT_CLASS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-data-default-class
