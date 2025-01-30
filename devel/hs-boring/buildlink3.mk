bebef6a4e8c343ebbebd8853dab6513eaeeeb8d2
pho <pho@pkgsrc.org>
2025-01-30 14:41:19 +0000

    devel/hs-boring: update to boring-0.2.2
    ## 0.2.2
    
    - Support GHC-8.6.5...9.10.1

--- devel/hs-boring/buildlink3.mk
+++ devel/hs-boring/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:31:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/30 14:41:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-boring
 
 .if !defined(HS_BORING_BUILDLINK3_MK)
 HS_BORING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-boring+=	hs-boring>=0.2.1
-BUILDLINK_ABI_DEPENDS.hs-boring+=	hs-boring>=0.2.1nb2
+BUILDLINK_API_DEPENDS.hs-boring+=	hs-boring>=0.2.2
+BUILDLINK_ABI_DEPENDS.hs-boring+=	hs-boring>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-boring?=		../../devel/hs-boring
 
 .include "../../devel/hs-tagged/buildlink3.mk"
