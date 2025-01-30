ffdfe6d686d0a5eec49b85178206d4864a3e9abd
pho <pho@pkgsrc.org>
2025-01-30 04:39:36 +0000

    textproc/hs-text-short: update to text-short-0.1.6
    ## 0.1.6
    
      * Drop support for GHC prior 8.6.5
      * Support GHC-9.10 (base-4.21)

--- textproc/hs-text-short/buildlink3.mk
+++ textproc/hs-text-short/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/30 04:39:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-short
 
 .if !defined(HS_TEXT_SHORT_BUILDLINK3_MK)
 HS_TEXT_SHORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-short+=	hs-text-short>=0.1.5
-BUILDLINK_ABI_DEPENDS.hs-text-short+=	hs-text-short>=0.1.5nb6
+BUILDLINK_API_DEPENDS.hs-text-short+=	hs-text-short>=0.1.6
+BUILDLINK_ABI_DEPENDS.hs-text-short+=	hs-text-short>=0.1.6
 BUILDLINK_PKGSRCDIR.hs-text-short?=	../../textproc/hs-text-short
 
 .include "../../devel/hs-hashable/buildlink3.mk"
