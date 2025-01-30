f02c551b33eb1b2a0cbf99e3060d0c0a9049704d
pho <pho@pkgsrc.org>
2025-01-30 10:12:49 +0000

    security/hs-crypton-x509-validation: update to crypton-x509-validation-1.6.13
    No release notes have been provided by the upstream.

--- security/hs-crypton-x509-validation/buildlink3.mk
+++ security/hs-crypton-x509-validation/buildlink3.mk
@@ -1,23 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/30 10:12:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypton-x509-validation
 
 .if !defined(HS_CRYPTON_X509_VALIDATION_BUILDLINK3_MK)
 HS_CRYPTON_X509_VALIDATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypton-x509-validation+=	hs-crypton-x509-validation>=1.6.12
-BUILDLINK_ABI_DEPENDS.hs-crypton-x509-validation+=	hs-crypton-x509-validation>=1.6.12nb2
+BUILDLINK_API_DEPENDS.hs-crypton-x509-validation+=	hs-crypton-x509-validation>=1.6.13
+BUILDLINK_ABI_DEPENDS.hs-crypton-x509-validation+=	hs-crypton-x509-validation>=1.6.13
 BUILDLINK_PKGSRCDIR.hs-crypton-x509-validation?=	../../security/hs-crypton-x509-validation
 
-.include "../../devel/hs-asn1-encoding/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../time/hs-hourglass/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../security/hs-pem/buildlink3.mk"
 .include "../../devel/hs-asn1-types/buildlink3.mk"
+.include "../../devel/hs-asn1-encoding/buildlink3.mk"
 .include "../../security/hs-crypton/buildlink3.mk"
 .include "../../security/hs-crypton-x509/buildlink3.mk"
 .include "../../security/hs-crypton-x509-store/buildlink3.mk"
-.include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../time/hs-hourglass/buildlink3.mk"
-.include "../../devel/hs-memory/buildlink3.mk"
-.include "../../security/hs-pem/buildlink3.mk"
 .endif	# HS_CRYPTON_X509_VALIDATION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-crypton-x509-validation
