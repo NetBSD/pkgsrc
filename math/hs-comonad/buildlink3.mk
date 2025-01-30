bcf0fb012a126ce4f2064cac027bdf353780bb73
pho <pho@pkgsrc.org>
2025-01-30 04:11:39 +0000

    math/hs-comonad: update to comonad-5.0.9
    5.0.9 [2024.12.04]
    ------------------
    * Drop support for pre-8.0 versions of GHC.

--- math/hs-comonad/buildlink3.mk
+++ math/hs-comonad/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/30 04:11:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-comonad
 
 .if !defined(HS_COMONAD_BUILDLINK3_MK)
 HS_COMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=5.0.8
-BUILDLINK_ABI_DEPENDS.hs-comonad+=	hs-comonad>=5.0.8nb8
+BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=5.0.9
+BUILDLINK_ABI_DEPENDS.hs-comonad+=	hs-comonad>=5.0.9
 BUILDLINK_PKGSRCDIR.hs-comonad?=	../../math/hs-comonad
 
-.include "../../math/hs-distributive/buildlink3.mk"
-.include "../../devel/hs-indexed-traversable/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../math/hs-distributive/buildlink3.mk"
+.include "../../devel/hs-indexed-traversable/buildlink3.mk"
 .endif	# HS_COMONAD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-comonad
