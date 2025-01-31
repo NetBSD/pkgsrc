230c3145e768772bba5a6ab45673b625bc4c2778
pho <pho@pkgsrc.org>
2025-01-31 10:49:17 +0000

    devel/hs-pretty-simple: update to pretty-simple-4.1.3.0
    ## 4.1.3.0
    
    *   Remove custom setup. This makes cross-compiling `pretty-simple` a lot more
        straightforward. No functionality has been lost from the library, since the
        custom setup was only used for generating tests.
        [#107](https://github.com/cdepillabout/pretty-simple/pull/107)

--- devel/hs-pretty-simple/buildlink3.mk
+++ devel/hs-pretty-simple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/31 10:49:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-pretty-simple
 
 .if !defined(HS_PRETTY_SIMPLE_BUILDLINK3_MK)
 HS_PRETTY_SIMPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.1.2
-BUILDLINK_ABI_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.1.2.0nb4
+BUILDLINK_API_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.1.3
+BUILDLINK_ABI_DEPENDS.hs-pretty-simple+=	hs-pretty-simple>=4.1.3.0
 BUILDLINK_PKGSRCDIR.hs-pretty-simple?=		../../devel/hs-pretty-simple
 
 .include "../../textproc/hs-prettyprinter/buildlink3.mk"
