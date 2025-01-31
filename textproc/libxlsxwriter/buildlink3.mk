1071fb90f8e2ea743386ca49c97c7b3969356ac4
sjmulder <sjmulder@pkgsrc.org>
2025-01-31 23:52:52 +0000

    textproc/libxlsxwriter: update to 1.1.9
    Changes in 1.1.9:
     - Fix minor pointer/value error in lxw_name_to_row() and lxw_name_to_col()
       utility functions.
    
    Changes in 1.1.8:
    - Added support for embedding images into worksheets with
      worksheet_embed_image().
    
      This can be useful if you are building up a spreadsheet of products with a
      column of images for each product. Embedded images move with the cell so they
      can be used in worksheet tables or data ranges that will be sorted or
      filtered.
    
      This functionality is the equivalent of Excel's menu option to insert an image
      using the option to "Place in Cell" which is available in Excel 365 versions
      from 2023 onwards.
    
    - Fixed various issues:
      - Failure on 32bit architectures #441
      - workbook_validate_sheet_name buffer-overflow #442
      - workbook_add_worksheet does not return a reasonable worksheet #443
      - workbook_define_name stack-buffer-underflow #444
      - workbook_close stack-buffer-overflow #445
      - _store_array_formula heap-buffer-overflow #446
      - worksheet_set_selection stack-buffer-overflow #447

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2024/07/15 19:39:43 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/01/31 23:52:52 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libxlsxwriter
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.7
-BUILDLINK_ABI_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.7
+BUILDLINK_ABI_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.9
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../archivers/minizip/buildlink3.mk"
