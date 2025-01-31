d28737910eee3539cee0763afdbd2607c71bf2fc
pho <pho@pkgsrc.org>
2025-01-31 14:42:22 +0000

    devel/hs-tasty-hunit: update to tasty-hunit-0.10.2
    Version 0.10.2
    --------------
    
    * Teach `testCaseSteps` to log progress
      ([#387](https://github.com/UnkindPartition/tasty/pull/387)).

--- devel/hs-tasty-hunit/buildlink3.mk
+++ devel/hs-tasty-hunit/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/31 14:42:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty-hunit
 
 .if !defined(HS_TASTY_HUNIT_BUILDLINK3_MK)
 HS_TASTY_HUNIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.1
-BUILDLINK_ABI_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.1nb2
+BUILDLINK_API_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.2
+BUILDLINK_ABI_DEPENDS.hs-tasty-hunit+=	hs-tasty-hunit>=0.10.2
 BUILDLINK_PKGSRCDIR.hs-tasty-hunit?=	../../devel/hs-tasty-hunit
 .endif	# HS_TASTY_HUNIT_BUILDLINK3_MK
 
