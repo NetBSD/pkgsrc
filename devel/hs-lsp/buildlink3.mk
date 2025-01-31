bb402ec8395aa52f7b6db26f507362c6b091834a
pho <pho@pkgsrc.org>
2025-01-31 09:56:22 +0000

    devel/hs-lsp: update to lsp-2.7.0.1
    ## 2.7.0.1 -- 2024-12-31
    
    - Relax dependency version bounds
    
    ## 2.7.0.0 -- 2024-06-06
    
    - Drop dependency on `uuid` and `random`
    - Fix handling of `rootPath` in `intializeParams`
    - Update to newer `lsp-types`
    
    ## 2.6.0.0
    
    - Progress reporting now has a configurable start delay and update delay. This allows
      servers to set up progress reporting for any operation and not worry about spamming
      the user with extremely short-lived progress sessions.

--- devel/hs-lsp/buildlink3.mk
+++ devel/hs-lsp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/31 09:56:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-lsp
 
 .if !defined(HS_LSP_BUILDLINK3_MK)
 HS_LSP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lsp+=	hs-lsp>=2.5.0
-BUILDLINK_ABI_DEPENDS.hs-lsp+=	hs-lsp>=2.5.0.0nb1
+BUILDLINK_API_DEPENDS.hs-lsp+=	hs-lsp>=2.7.0
+BUILDLINK_ABI_DEPENDS.hs-lsp+=	hs-lsp>=2.7.0.1
 BUILDLINK_PKGSRCDIR.hs-lsp?=	../../devel/hs-lsp
 
 .include "../../converters/hs-aeson/buildlink3.mk"
@@ -20,12 +20,11 @@ BUILDLINK_PKGSRCDIR.hs-lsp?=	../../devel/hs-lsp
 .include "../../converters/hs-lens-aeson/buildlink3.mk"
 .include "../../devel/hs-lsp-types/buildlink3.mk"
 .include "../../textproc/hs-prettyprinter/buildlink3.mk"
-.include "../../devel/hs-random/buildlink3.mk"
 .include "../../devel/hs-sorted-list/buildlink3.mk"
 .include "../../textproc/hs-text-rope/buildlink3.mk"
+.include "../../devel/hs-unliftio/buildlink3.mk"
 .include "../../devel/hs-unliftio-core/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
-.include "../../devel/hs-uuid/buildlink3.mk"
 .endif	# HS_LSP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-lsp
