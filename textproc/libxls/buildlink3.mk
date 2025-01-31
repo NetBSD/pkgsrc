be8a3ca51684932c4b9695cfd35f702785560456
sjmulder <sjmulder@pkgsrc.org>
2025-01-31 23:39:00 +0000

    textproc/libxls: update to 1.6.3
    Changes:
     - Fix style record size check on big-endian systems
     - Fix header include in C++ mode
     - Fix buffer overflows when parsing style records
     - Various build fixes
     - New LIBXLS_UNSUPPORTED_ENCRYPTION error code
     - Fix infinite loop with self-referencing sectors

--- textproc/libxls/buildlink3.mk
+++ textproc/libxls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/10/01 19:34:09 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/01/31 23:39:00 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxls
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libxls
 LIBXLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.6.1
+BUILDLINK_ABI_DEPENDS.libxls+=	libxls>=1.6.1
 BUILDLINK_PKGSRCDIR.libxls?=	../../textproc/libxls
 
 .include "../../converters/libiconv/buildlink3.mk"
