a1752cccf13cb0364976de370c9ca8b09154e300
pho <pho@pkgsrc.org>
2025-01-31 13:48:34 +0000

    textproc/hs-parsers: update to parsers-0.12.12
    0.12.12 [2024.10.26]
    --------------------
    * Support building with `text-2.1.2`.
    * Drop support for pre-8.0 versions of GHC.

--- textproc/hs-parsers/buildlink3.mk
+++ textproc/hs-parsers/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/31 13:48:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-parsers
 
 .if !defined(HS_PARSERS_BUILDLINK3_MK)
 HS_PARSERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsers+=	hs-parsers>=0.12.11
-BUILDLINK_ABI_DEPENDS.hs-parsers+=	hs-parsers>=0.12.11nb4
+BUILDLINK_API_DEPENDS.hs-parsers+=	hs-parsers>=0.12.12
+BUILDLINK_ABI_DEPENDS.hs-parsers+=	hs-parsers>=0.12.12
 BUILDLINK_PKGSRCDIR.hs-parsers?=	../../textproc/hs-parsers
 
-.include "../../devel/hs-base-orphans/buildlink3.mk"
 .include "../../textproc/hs-charset/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
