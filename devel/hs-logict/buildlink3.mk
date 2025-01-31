890ac0bed2f90e4839c2f1438e7b5058b7c4c8cc
pho <pho@pkgsrc.org>
2025-01-31 07:35:36 +0000

    devel/hs-logict: update to logict-0.8.2.0
    # 0.8.2.0
    
    * Add instances for `MonadThrow` and `MonadCatch`.
    * Add instances `Eq`, `Ord`, `Show`, `Read`, `IsList` for `Logic a`.
    * Speed up `instance MonadLogic Logic` with a trick to determine whether a callback is a constant failure.

--- devel/hs-logict/buildlink3.mk
+++ devel/hs-logict/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/05/09 01:32:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/01/31 07:35:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-logict
 
 .if !defined(HS_LOGICT_BUILDLINK3_MK)
 HS_LOGICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-logict+=	hs-logict>=0.8.1
-BUILDLINK_ABI_DEPENDS.hs-logict+=	hs-logict>=0.8.1.0nb2
+BUILDLINK_API_DEPENDS.hs-logict+=	hs-logict>=0.8.2
+BUILDLINK_ABI_DEPENDS.hs-logict+=	hs-logict>=0.8.2.0
 BUILDLINK_PKGSRCDIR.hs-logict?=		../../devel/hs-logict
 .endif	# HS_LOGICT_BUILDLINK3_MK
 
