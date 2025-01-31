1e965b26881b93c7b74f770cbd66fbd03e71f710
pho <pho@pkgsrc.org>
2025-01-31 14:39:28 +0000

    devel/hs-tasty-golden: import hs-tasty-golden-2.3.5
    This package provides support for "golden testing". A golden test is an IO
    action that writes its result to a file. To pass the test, this output file
    should be identical to the corresponding "golden" file, which contains the
    correct result for the test. To get started with golden testing and this
    library, see Introduction to golden testing
    (https://ro-che.info/articles/2017-12-04-golden-tests).

---
+++ devel/hs-tasty-golden/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/01/31 14:39:28 pho Exp $
+
+BUILDLINK_TREE+=	hs-tasty-golden
+
+.if !defined(HS_TASTY_GOLDEN_BUILDLINK3_MK)
+HS_TASTY_GOLDEN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tasty-golden+=	hs-tasty-golden>=2.3.5
+BUILDLINK_ABI_DEPENDS.hs-tasty-golden+=	hs-tasty-golden>=2.3.5
+BUILDLINK_PKGSRCDIR.hs-tasty-golden?=	../../devel/hs-tasty-golden
+
+.include "../../devel/hs-tasty/buildlink3.mk"
+.include "../../sysutils/hs-typed-process/buildlink3.mk"
+.include "../../devel/hs-optparse-applicative/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.endif	# HS_TASTY_GOLDEN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tasty-golden
