3c776fd83fc833ddae718f4b0fde0d193d70a47f
pho <pho@pkgsrc.org>
2025-01-31 06:44:33 +0000

    devel/hs-ghc-trace-events: update to ghc-trace-events-0.1.2.9
    ## v0.1.2.9 - 2024-05-18
    
    * Bump base bounds to support ghc 9.10 ([#15](https://github.com/maoe/ghc-trace-events/pull/15))

--- devel/hs-ghc-trace-events/buildlink3.mk
+++ devel/hs-ghc-trace-events/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/05/09 01:31:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/31 06:44:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-trace-events
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-ghc-trace-events
 HS_GHC_TRACE_EVENTS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-ghc-trace-events+=	hs-ghc-trace-events>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-ghc-trace-events+=	hs-ghc-trace-events>=0.1.2.8nb1
+BUILDLINK_ABI_DEPENDS.hs-ghc-trace-events+=	hs-ghc-trace-events>=0.1.2.9
 BUILDLINK_PKGSRCDIR.hs-ghc-trace-events?=	../../devel/hs-ghc-trace-events
 .endif	# HS_GHC_TRACE_EVENTS_BUILDLINK3_MK
 
