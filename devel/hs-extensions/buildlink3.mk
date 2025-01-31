b971cc176e59254ec0fcd92a5d71680ea2207af8
pho <pho@pkgsrc.org>
2025-01-31 10:46:15 +0000

    devel/hs-extensions: update to extensions-0.1.0.2
    ## 0.1.0.2 — Apr 29, 2024
    
    * Add support for GHC-9.8
    
    * Support `Cabal` `3.12` (only)

--- devel/hs-extensions/buildlink3.mk
+++ devel/hs-extensions/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/05/09 01:31:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/01/31 10:46:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-extensions
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-extensions
 HS_EXTENSIONS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-extensions+=	hs-extensions>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-extensions+=	hs-extensions>=0.1.0.1nb1
+BUILDLINK_ABI_DEPENDS.hs-extensions+=	hs-extensions>=0.1.0.2
 BUILDLINK_PKGSRCDIR.hs-extensions?=	../../devel/hs-extensions
 .endif	# HS_EXTENSIONS_BUILDLINK3_MK
 
