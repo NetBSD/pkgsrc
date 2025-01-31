16539c58c46d6facf77bfd73e6f2ba571da07e0a
wiz <wiz@pkgsrc.org>
2025-01-31 11:52:21 +0000

    kf6-kwindowsystem: pkglint fix

--- x11/kf6-kwindowsystem/buildlink3.mk
+++ x11/kf6-kwindowsystem/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/11/14 22:22:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/31 11:52:21 wiz Exp $
 
 BUILDLINK_TREE+=	kf6-kwindowsystem
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kf6-kwindowsystem
 KF6_KWINDOWSYSTEM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kf6-kwindowsystem+=	kf6-kwindowsystem>=6.0.0
-BUILDLINK_ABI_DEPENDS.kf6-kwindowsystem?=		kf6-kwindowsystem>=6.2.0nb4
+BUILDLINK_ABI_DEPENDS.kf6-kwindowsystem?=	kf6-kwindowsystem>=6.2.0nb4
 BUILDLINK_PKGSRCDIR.kf6-kwindowsystem?=		../../x11/kf6-kwindowsystem
 
 .include "../../devel/qt6-qtwayland/buildlink3.mk"
