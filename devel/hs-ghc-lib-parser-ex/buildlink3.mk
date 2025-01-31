11547a3c096e89f351550bc13c792da5818d8d1e
pho <pho@pkgsrc.org>
2025-01-31 04:29:08 +0000

    devel/hs-ghc-lib-parser-ex: update to ghc-lib-parser-ex-9.10.0.0
    ## unreleased
    - Deprecate `Dump`
    - New module `GHC.Hs.Dump`
    - Add support for ghc-9.10 series: `GHCLIB_API_910`

--- devel/hs-ghc-lib-parser-ex/buildlink3.mk
+++ devel/hs-ghc-lib-parser-ex/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:31:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/31 04:29:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-lib-parser-ex
 
 .if !defined(HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK)
 HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.8.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.8.0.2nb1
+BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.10.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.10.0.0
 BUILDLINK_PKGSRCDIR.hs-ghc-lib-parser-ex?=	../../devel/hs-ghc-lib-parser-ex
 
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
