a385b654e7e164bd0e70d45ab75317bf3cea34e0
pho <pho@pkgsrc.org>
2025-01-31 14:35:08 +0000

    devel/hs-tasty-expected-failure: import hs-tasty-expected-failure-0.12.3
    With the function Test.Tasty.ExpectedFailure.expectFail in the provided module
    Test.Tasty.ExpectedFailure, you can mark that you expect test cases to fail, and
    not to pass.
    
    This can for example be used for test-driven development: Create the tests, mark
    them with Test.Tasty.ExpectedFailure.expectFail, and you can still push to the
    main branch, without your continuous integration branch failing.
    
    Once someone implements the feature or fixes the bug (maybe unknowingly), the
    test suite will tell him so, due to the now unexpectedly passing test, and he
    can remove the Test.Tasty.ExpectedFailure.expectFail marker.
    
    The module also provides Test.Tasty.ExpectedFailure.ignoreTest to avoid running
    a test. Both funtions are implemented via the more general
    Test.Tasty.ExpectedFailure.wrapTest, which is also provided.

---
+++ devel/hs-tasty-expected-failure/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/01/31 14:35:08 pho Exp $
+
+BUILDLINK_TREE+=	hs-tasty-expected-failure
+
+.if !defined(HS_TASTY_EXPECTED_FAILURE_BUILDLINK3_MK)
+HS_TASTY_EXPECTED_FAILURE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tasty-expected-failure+=	hs-tasty-expected-failure>=0.12.3
+BUILDLINK_ABI_DEPENDS.hs-tasty-expected-failure+=	hs-tasty-expected-failure>=0.12.3
+BUILDLINK_PKGSRCDIR.hs-tasty-expected-failure?=		../../devel/hs-tasty-expected-failure
+
+.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../devel/hs-tasty/buildlink3.mk"
+.include "../../devel/hs-unbounded-delays/buildlink3.mk"
+.endif	# HS_TASTY_EXPECTED_FAILURE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tasty-expected-failure
