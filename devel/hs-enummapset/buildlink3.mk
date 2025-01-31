f2b9e56f99411982b14150d8f648416fdb1b5565
pho <pho@pkgsrc.org>
2025-01-31 06:40:37 +0000

    devel/hs-enummapset: update to enummapset-0.7.3.0
    No release notes have been provided by the upstream.

--- devel/hs-enummapset/buildlink3.mk
+++ devel/hs-enummapset/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:31:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/31 06:40:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-enummapset
 
 .if !defined(HS_ENUMMAPSET_BUILDLINK3_MK)
 HS_ENUMMAPSET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-enummapset+=	hs-enummapset>=0.7.2
-BUILDLINK_ABI_DEPENDS.hs-enummapset+=	hs-enummapset>=0.7.2.0nb1
+BUILDLINK_API_DEPENDS.hs-enummapset+=	hs-enummapset>=0.7.3
+BUILDLINK_ABI_DEPENDS.hs-enummapset+=	hs-enummapset>=0.7.3.0
 BUILDLINK_PKGSRCDIR.hs-enummapset?=	../../devel/hs-enummapset
 
 .include "../../converters/hs-aeson/buildlink3.mk"
