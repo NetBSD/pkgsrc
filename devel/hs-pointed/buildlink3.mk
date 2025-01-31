306ce00d98c59e8109f3edb8a2892474ee74ec80
pho <pho@pkgsrc.org>
2025-01-31 16:05:30 +0000

    devel/hs-pointed: update to pointed-5.0.5
    5.0.5 [2024.10.26]
    ------------------
    * Require building with GHC 8.0 or later.
    * Remove the `-fsemigroups` `cabal` flag, as `pointed` no longer needs to depend
      on the `semigroups` library for backwards compatibility.
    * Allow building with `data-default-class-0.2.*`.

--- devel/hs-pointed/buildlink3.mk
+++ devel/hs-pointed/buildlink3.mk
@@ -1,19 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/31 16:05:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-pointed
 
 .if !defined(HS_POINTED_BUILDLINK3_MK)
 HS_POINTED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pointed+=	hs-pointed>=5.0.4
-BUILDLINK_ABI_DEPENDS.hs-pointed+=	hs-pointed>=5.0.4nb4
+BUILDLINK_API_DEPENDS.hs-pointed+=	hs-pointed>=5.0.5
+BUILDLINK_ABI_DEPENDS.hs-pointed+=	hs-pointed>=5.0.5
 BUILDLINK_PKGSRCDIR.hs-pointed?=	../../devel/hs-pointed
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
 .include "../../math/hs-comonad/buildlink3.mk"
 .include "../../math/hs-kan-extensions/buildlink3.mk"
 .include "../../math/hs-semigroupoids/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
