d983caa4fd115ff9373fc4e72b729d0f9f49ee20
pho <pho@pkgsrc.org>
2025-01-31 14:22:52 +0000

    devel/hs-shake: import hs-shake-0.19.8
    Shake is a Haskell library for writing build systems - designed as a replacement
    for make. See Development.Shake for an introduction, including an example. The
    homepage contains links to a user manual, an academic paper and further
    information: https://shakebuild.com
    
    To use Shake the user writes a Haskell program that imports Development.Shake,
    defines some build rules, and calls the Development.Shake.shakeArgs function.
    Thanks to do notation and infix operators, a simple Shake build system is not
    too dissimilar from a simple Makefile. However, as build systems get more
    complex, Shake is able to take advantage of the excellent abstraction facilities
    offered by Haskell and easily support much larger projects. The Shake library
    provides all the standard features available in other build systems, including
    automatic parallelism and minimal rebuilds. Shake also provides more accurate
    dependency tracking, including seamless support for generated files, and
    dependencies on system information (e.g. compiler version).

---
+++ devel/hs-shake/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/01/31 14:22:52 pho Exp $
+
+BUILDLINK_TREE+=	hs-shake
+
+.if !defined(HS_SHAKE_BUILDLINK3_MK)
+HS_SHAKE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.19.8
+BUILDLINK_ABI_DEPENDS.hs-shake+=	hs-shake>=0.19.8
+BUILDLINK_PKGSRCDIR.hs-shake?=		../../devel/hs-shake
+
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../sysutils/hs-filepattern/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-heaps/buildlink3.mk"
+.include "../../www/hs-js-dgtable/buildlink3.mk"
+.include "../../www/hs-js-flot/buildlink3.mk"
+.include "../../www/hs-js-jquery/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.endif	# HS_SHAKE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-shake
