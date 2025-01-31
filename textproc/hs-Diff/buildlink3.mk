68a9461d2bbc31b7a788675e095203d63e2d96ec
pho <pho@pkgsrc.org>
2025-01-31 02:20:14 +0000

    textproc/hs-Diff: update to Diff-1.0.2
    # 1.0.2
    
      - Output correct format when an input file is empty, e.g. `@@ --0,0 +1,3 @@`.
    
    # 1.0.1.1
    
      - Require `base >= 4.11` (GHC 8.4).
    
    # 1.0
    
      - Add Unix diff style annotations to output of `prettyContextDiff`,
        e.g `@@ -1,5 +1,4 @@`.  This required three changes to the
        signature of `getContextDiff` due to the addition of a wrapper type
        `Numbered`, which enumerates the elements of the input list.
    
      - Signature change 1: The element pretty printer type changes from
        `(a -> Doc)` to `(Numbered a -> Doc)`.  An unnumber function is
        provided so that the old behavior can be obtained by changing that
        argument from `pretty` to `(pretty . unnumber)`
    
      - Signature change 2: The result type of getContextDiff changes from
        `ContextDiff a` to `ContextDiff (Numbered a)`.  A function
        `unNumberContextDiff` is provided to convert the result back to
        the old type.
    
      - Signature change 3: the context argument is now `Maybe Int` rather
        than `Int`, reflecting the change made to `getContextDiffNew` in 0.5.
    
      - A `prettyContextDiffOld` function is provided to get the old
        style output.
    
      - The old broken version of getContextDiffOld is removed.
    
      - Document the behavior of `groupBy'`.
    
    # 0.5
    
      - Bring space complexity down to D^2 (Bodigrim).
      - Add `Bifunctor` instance (Jonathan King).  Requires `base` >= 4.8.
      - Fix for the grouped context diff.  It was omitting all trailing contexts.
      - Allow unlimited number of context elements (`getContextDiffNew`).
    
    # 0.4
    
      - Generalize `Diff a` to `PolyDiff a b`.
        `Diff` has been replaced with a specialized synonym `type Diff a = PolyDiff a a`,
        but it's still not backward compatible if you imported `Diff(..)`.

--- textproc/hs-Diff/buildlink3.mk
+++ textproc/hs-Diff/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:32:38 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/31 02:20:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-Diff
 
 .if !defined(HS_DIFF_BUILDLINK3_MK)
 HS_DIFF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.5
-BUILDLINK_ABI_DEPENDS.hs-Diff+=	hs-Diff>=0.5nb2
+BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-Diff+=	hs-Diff>=1.0.2
 BUILDLINK_PKGSRCDIR.hs-Diff?=	../../textproc/hs-Diff
 .endif	# HS_DIFF_BUILDLINK3_MK
 
