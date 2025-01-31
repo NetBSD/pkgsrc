9f96ce9eb8da71beb92ef5b56695a2381d4476b5
pho <pho@pkgsrc.org>
2025-01-31 15:56:11 +0000

    devel/hs-atomic-write: update to atomic-write-0.2.1.0
    No release notes have been provided by the upstream.

--- devel/hs-atomic-write/buildlink3.mk
+++ devel/hs-atomic-write/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:31:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/31 15:56:11 pho Exp $
 
 BUILDLINK_TREE+=	hs-atomic-write
 
 .if !defined(HS_ATOMIC_WRITE_BUILDLINK3_MK)
 HS_ATOMIC_WRITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-atomic-write+=	hs-atomic-write>=0.2.0
-BUILDLINK_ABI_DEPENDS.hs-atomic-write+=	hs-atomic-write>=0.2.0.7nb5
+BUILDLINK_API_DEPENDS.hs-atomic-write+=	hs-atomic-write>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-atomic-write+=	hs-atomic-write>=0.2.1.0
 BUILDLINK_PKGSRCDIR.hs-atomic-write?=	../../devel/hs-atomic-write
 
 .include "../../sysutils/hs-temporary/buildlink3.mk"
