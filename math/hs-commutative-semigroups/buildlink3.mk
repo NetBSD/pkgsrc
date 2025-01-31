2d5b2f37c55559902c2e136806f747a92b66d6e9
pho <pho@pkgsrc.org>
2025-01-31 13:34:08 +0000

    math/hs-commutative-semigroups: update to commutative-semigroups-0.2.0.1
    ## 0.2.0.1 -- 2024-05-18
    
    - Loosen dependency bounds for GHC 9.10, thanks to yaitskov

--- math/hs-commutative-semigroups/buildlink3.mk
+++ math/hs-commutative-semigroups/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:25 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/31 13:34:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-commutative-semigroups
 
 .if !defined(HS_COMMUTATIVE_SEMIGROUPS_BUILDLINK3_MK)
 HS_COMMUTATIVE_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.2
-BUILDLINK_ABI_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.2nb1
+BUILDLINK_API_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-commutative-semigroups+=	hs-commutative-semigroups>=0.2.0.1
 BUILDLINK_PKGSRCDIR.hs-commutative-semigroups?=		../../math/hs-commutative-semigroups
 .endif	# HS_COMMUTATIVE_SEMIGROUPS_BUILDLINK3_MK
 
