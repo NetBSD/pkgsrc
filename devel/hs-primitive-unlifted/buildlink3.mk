c690fa7c6966178d6021f28090fe636fb8c013b4
pho <pho@pkgsrc.org>
2025-01-31 07:53:09 +0000

    devel/hs-primitive-unlifted: update to primitive-unlifted-2.2.0.0
    ## 2.2.0.0 -- 2024-10-05
    
    * Similarly to the change in 2.1.0.0, change the order of the type arguments to `SmallUnliftedArray_` and
      `SmallMutableUnliftedArray_`. This makes the library work better with
      the typeclasses in the `contiguous` library.

--- devel/hs-primitive-unlifted/buildlink3.mk
+++ devel/hs-primitive-unlifted/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/31 07:53:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive-unlifted
 
 .if !defined(HS_PRIMITIVE_UNLIFTED_BUILDLINK3_MK)
 HS_PRIMITIVE_UNLIFTED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=2.1.0.0nb2
+BUILDLINK_API_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=2.2.0
+BUILDLINK_ABI_DEPENDS.hs-primitive-unlifted+=	hs-primitive-unlifted>=2.2.0.0
 BUILDLINK_PKGSRCDIR.hs-primitive-unlifted?=	../../devel/hs-primitive-unlifted
 
 .include "../../devel/hs-primitive/buildlink3.mk"
