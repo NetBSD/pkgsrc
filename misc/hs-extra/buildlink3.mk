cf70a0ab2aca2aa89938e59f656e7a1bfe282230
pho <pho@pkgsrc.org>
2025-01-31 04:33:07 +0000

    misc/hs-extra: update to extra-1.8
    Changelog for Extra (* = breaking change)
    
    1.8, released 2024-10-19
        #109, add guarded to lift predicates into Alternatives
    *   #112, make Data.List.Extra.compareLength work on list/int only
        #112, add compareLength to List.NonEmpty and Foldable
    1.7.16, released 2024-05-11
        Fix to actually work with GHC 9.10
    1.7.15, released 2024-05-10
        #111, work with GHC 9.10
        Test with GHC 9.8

--- misc/hs-extra/buildlink3.mk
+++ misc/hs-extra/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/01/31 04:33:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-extra
 
 .if !defined(HS_EXTRA_BUILDLINK3_MK)
 HS_EXTRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.7.14
-BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.7.14nb2
+BUILDLINK_API_DEPENDS.hs-extra+=	hs-extra>=1.8
+BUILDLINK_ABI_DEPENDS.hs-extra+=	hs-extra>=1.8
 BUILDLINK_PKGSRCDIR.hs-extra?=		../../misc/hs-extra
 
 .include "../../time/hs-clock/buildlink3.mk"
