e80835255b63245468f461163aa2abfae7f9168a
pho <pho@pkgsrc.org>
2025-01-31 10:34:51 +0000

    devel/ormolu: update to ormolu-0.7.7.0
    ## Ormolu 0.7.7.0
    
    * Use single-line layout for parens around single-line content. [Issue
      1120](https://github.com/tweag/ormolu/issues/1120).
    
    * Allow function arguments to be on the same line even if the full type
      (with constraints and foralls) are on multiple lines. [PR
      1125](https://github.com/tweag/ormolu/pull/1125).
    
    ## Ormolu 0.7.6.0
    
    * Fix Haddock comments on infix constructors. [Issue
      758](https://github.com/tweag/ormolu/issues/758).
    
    * Don't require a trailing newline in `.ormolu` files. [Issue
      1122](https://github.com/tweag/ormolu/issues/1122).
    
    * Remove unnecessary indentation from list comprehensions. [Issue
      966](https://github.com/tweag/ormolu/issues/966).
    
    ## Ormolu 0.7.5.0
    
    * Switched to `ghc-lib-parser-9.10`, with the following new syntactic features/behaviors:
      * GHC proposal [#575](https://github.com/ghc-proposals/ghc-proposals/blob/10290a668608d608c3f6c6010be265cf7a02e1fc/proposals/0575-deprecated-instances.rst): deprecated instances.
      * GHC proposal [#281](https://github.com/ghc-proposals/ghc-proposals/blob/10290a668608d608c3f6c6010be265cf7a02e1fc/proposals/0281-visible-forall.rst): visible forall in types of terms.
        Enabled by `RequiredTypeArguments` (enabled by default).
      * `LinearTypes`: `let` and `where` bindings can now be linear, in particular have multiplicity annotations.
      * Using `forall` as an identifier is now a parse error.
      * GHC proposal [#65](https://github.com/ghc-proposals/ghc-proposals/blob/10290a668608d608c3f6c6010be265cf7a02e1fc/proposals/0065-type-infix.rst): namespacing fixity declarations for type names and WARNING/DEPRECATED pragmas.
      * `TypeAbstractions` now supports `@`-binders in lambdas and function equations.
      * Support for the `GHC2024` language.
    
    * Updated to `Cabal-syntax-3.12`.

--- devel/ormolu/buildlink3.mk
+++ devel/ormolu/buildlink3.mk
@@ -1,20 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/31 10:34:51 pho Exp $
 
 BUILDLINK_TREE+=	ormolu
 
 .if !defined(ORMOLU_BUILDLINK3_MK)
 ORMOLU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.7.4
-BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.7.4.0nb1
+BUILDLINK_API_DEPENDS.ormolu+=	ormolu>=0.7.7
+BUILDLINK_ABI_DEPENDS.ormolu+=	ormolu>=0.7.7.0
 BUILDLINK_PKGSRCDIR.ormolu?=	../../devel/ormolu
 
-.include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../textproc/hs-Diff/buildlink3.mk"
+.include "../../devel/hs-memotrie/buildlink3.mk"
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-choice/buildlink3.mk"
 .include "../../devel/hs-file-embed/buildlink3.mk"
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
 .include "../../textproc/hs-megaparsec/buildlink3.mk"
-.include "../../devel/hs-memotrie/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .endif	# ORMOLU_BUILDLINK3_MK
 
