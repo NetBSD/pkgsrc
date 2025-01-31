aba5fb04a0499493bff3a64e597b53fad9913359
pho <pho@pkgsrc.org>
2025-01-31 09:09:56 +0000

    textproc/hs-text-rope: update to text-rope-0.3
    # 0.3
    
    * Move `Data.Text.Utf16.Rope.Mixed` module to `Data.Text.Mixed.Rope`. `Data.Text.Utf16.Rope.Mixed` re-exports `Data.Text.Mixed.Rope` for legacy clients.
    * Add `Data.Text.Utf8.Lines` and `Data.Text.Utf8.Rope` modules for ropes indexed by UTF-8 code units.
    * Add UTF-8 indexing functionality to `Data.Text.Mixed.Rope`.
    * The metrics stored internally in the rope nodes has changed, which should improve performance by making some re-measuring redundant. As a consequence, the time complexity of `Data.Text.Rope.lengthAsPosition` is now linear in the length of the last line.
    * Add `getLine` functions to extract lines by 0-based index.

--- textproc/hs-text-rope/buildlink3.mk
+++ textproc/hs-text-rope/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2024/05/09 01:32:48 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/01/31 09:09:56 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-rope
 
 .if !defined(HS_TEXT_ROPE_BUILDLINK3_MK)
 HS_TEXT_ROPE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-rope+=	hs-text-rope>=0.2
-BUILDLINK_ABI_DEPENDS.hs-text-rope+=	hs-text-rope>=0.2nb4
+BUILDLINK_API_DEPENDS.hs-text-rope+=	hs-text-rope>=0.3
+BUILDLINK_ABI_DEPENDS.hs-text-rope+=	hs-text-rope>=0.3
 BUILDLINK_PKGSRCDIR.hs-text-rope?=	../../textproc/hs-text-rope
 
 .include "../../devel/hs-vector/buildlink3.mk"
