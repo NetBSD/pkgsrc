3a14d783c43755fc0e1ac33ab8809cb96b0803a5
pho <pho@pkgsrc.org>
2025-01-31 00:17:33 +0000

    devel/hs-fgl: update to fgl-5.8.3.0
    5.8.3.0
    -------
    
    * Data.Graph.Inductive.NodeMap now has functions mkLookupNode,
      insMapLookupNode, memberNode, and lookupNode for detecting whether a
      graph already contains a node (issue #72, PR #77).

--- devel/hs-fgl/buildlink3.mk
+++ devel/hs-fgl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2024/05/09 01:31:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2025/01/31 00:17:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-fgl
 
 .if !defined(HS_FGL_BUILDLINK3_MK)
 HS_FGL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.8.2
-BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.8.2.0nb2
+BUILDLINK_API_DEPENDS.hs-fgl+=	hs-fgl>=5.8.3
+BUILDLINK_ABI_DEPENDS.hs-fgl+=	hs-fgl>=5.8.3.0
 BUILDLINK_PKGSRCDIR.hs-fgl?=	../../devel/hs-fgl
 .endif	# HS_FGL_BUILDLINK3_MK
 
