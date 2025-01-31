ab96cce5e47e0e83e93c0c60dc3a368bf9a0a230
pho <pho@pkgsrc.org>
2025-01-31 15:53:05 +0000

    devel/hs-atomic-primops: update to atomic-primops-0.8.8
    ## 0.8.8 [2024.06.20]
    * Fix infinite loops in the implementations of `storeLoadBarrier`,
      `loadLoadBarrier`, and `writeBarrier` when building with GHC 9.10 or later.

--- devel/hs-atomic-primops/buildlink3.mk
+++ devel/hs-atomic-primops/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:31:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/01/31 15:53:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-atomic-primops
 
 .if !defined(HS_ATOMIC_PRIMOPS_BUILDLINK3_MK)
 HS_ATOMIC_PRIMOPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.7
-BUILDLINK_ABI_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.7nb1
+BUILDLINK_API_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.8
+BUILDLINK_ABI_DEPENDS.hs-atomic-primops+=	hs-atomic-primops>=0.8.8
 BUILDLINK_PKGSRCDIR.hs-atomic-primops?=		../../devel/hs-atomic-primops
 
 .include "../../devel/hs-primitive/buildlink3.mk"
