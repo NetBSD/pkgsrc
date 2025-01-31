5d822c5116fc6228b39f104180756106552a00df
pho <pho@pkgsrc.org>
2025-01-31 15:11:38 +0000

    devel/hs-ghc-exactprint: Forgot to modify buildlink3.mk

--- devel/hs-ghc-exactprint/buildlink3.mk
+++ devel/hs-ghc-exactprint/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2025/01/31 04:51:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/31 15:11:38 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-exactprint
 
 .if !defined(HS_GHC_EXACTPRINT_BUILDLINK3_MK)
 HS_GHC_EXACTPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.10.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.10.0.0
+BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.9.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.9.0.0
 BUILDLINK_PKGSRCDIR.hs-ghc-exactprint?=		../../devel/hs-ghc-exactprint
 
 .include "../../devel/hs-syb/buildlink3.mk"
