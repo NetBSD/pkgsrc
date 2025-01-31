7fc1f51b34eeb2ebe77aeab22ba10a4017c9ef57
pho <pho@pkgsrc.org>
2025-01-31 04:51:16 +0000

    devel/hs-ghc-exactprint: update to ghc-exactprint-1.10.0.0
    2024-05-21 v1.10.0.0
    	* Make the Language.Haskell.GHC.ExactPrint.Transform API pure
    	* Remove EPOoptions fields that had no effect
    2024-05-16 v1.9.0.0
    	* Support GHC 9.10.1

--- devel/hs-ghc-exactprint/buildlink3.mk
+++ devel/hs-ghc-exactprint/buildlink3.mk
@@ -1,17 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/05/09 01:31:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/01/31 04:51:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-exactprint
 
 .if !defined(HS_GHC_EXACTPRINT_BUILDLINK3_MK)
 HS_GHC_EXACTPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.8.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.8.0.0nb1
+BUILDLINK_API_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.10.0
+BUILDLINK_ABI_DEPENDS.hs-ghc-exactprint+=	hs-ghc-exactprint>=1.10.0.0
 BUILDLINK_PKGSRCDIR.hs-ghc-exactprint?=		../../devel/hs-ghc-exactprint
 
-.include "../../devel/hs-data-default/buildlink3.mk"
-.include "../../math/hs-free/buildlink3.mk"
-.include "../../devel/hs-ordered-containers/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .endif	# HS_GHC_EXACTPRINT_BUILDLINK3_MK
 
