49d7cf12ea539bed5361f0111d175f4890133dd4
pho <pho@pkgsrc.org>
2025-01-31 08:08:47 +0000

    math/hs-semirings: update to semirings-0.7
    0.7: [2024-05-21]
    -----------------
    * Add `Data.Semiring.Directed` for the semiring of directed sets.
    * Add `Data.Ring.Ordered` to represent ordered rings (as well as a simpler
      finitary case) and provide `signum` and `abs` via type class.
    * Modify code and CI to support GHC 8.0 and later only.
    * Support newer versions of dependencies
    * Move Generics-derived tuple instances from Data.Semiring.Generic to manually-written Data.Semiring

--- math/hs-semirings/buildlink3.mk
+++ math/hs-semirings/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/01/31 08:08:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-semirings
 
 .if !defined(HS_SEMIRINGS_BUILDLINK3_MK)
 HS_SEMIRINGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semirings+=	hs-semirings>=0.6
-BUILDLINK_ABI_DEPENDS.hs-semirings+=	hs-semirings>=0.6nb5
+BUILDLINK_API_DEPENDS.hs-semirings+=	hs-semirings>=0.7
+BUILDLINK_ABI_DEPENDS.hs-semirings+=	hs-semirings>=0.7
 BUILDLINK_PKGSRCDIR.hs-semirings?=	../../math/hs-semirings
 
-.include "../../devel/hs-base-compat-batteries/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_SEMIRINGS_BUILDLINK3_MK
