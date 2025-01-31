4dda10abf91d9d0109d5716abf7e517398ab0948
micha <micha@pkgsrc.org>
2025-01-31 14:33:17 +0000

    www/dillo: Switch to FLTK 1.4
    Part of FLTK 1.4 transition (Step 2).
    
    - Add patch to detect FLTK 1.4
    - Add patch for dillo-install-hyphenation
      POSIX::tmpnam() is replaced with File::Temp
    - Declare net/wget as dependency
      dillo uses wget at runtime
    - Declare usage of features "snprintf" and "vsnprintf"
    - Use option "tls" to enable/disable TLS support
      SSL is obsolete
    - Use option "libwebp" to enable/disable WEBP image support
      Option "webp" is not documented
    - Add more information to DESCR
    - Bump PKGREVISION

--- www/dillo/options.mk
+++ www/dillo/options.mk
@@ -1,31 +1,35 @@
-# $NetBSD: options.mk,v 1.8 2025/01/19 15:34:43 ktnb Exp $
+# $NetBSD: options.mk,v 1.9 2025/01/31 14:33:17 micha Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dillo
-PKG_SUPPORTED_OPTIONS=	inet6 ssl webp
-PKG_SUGGESTED_OPTIONS=	inet6 ssl webp
+PKG_SUPPORTED_OPTIONS=	inet6 tls libwebp
+PKG_SUGGESTED_OPTIONS=	inet6 tls libwebp
 
 .include "../../mk/bsd.options.mk"
 
+# Support for IPv6 protocol
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
+.else
+CONFIGURE_ARGS+=	--disable-ipv6
 .endif
 
-.if !empty(PKG_OPTIONS:Mssl)
+# Support for Transport Layer Security (TLS)
+.if !empty(PKG_OPTIONS:Mtls)
 CONFIGURE_ARGS+=	--enable-tls
+CONFIGURE_ARGS+=	--disable-mbedtls
+CONFIGURE_ARGS+=	--enable-openssl
+CONFIGURE_ARGS+=	--with-ca-certs-dir=${SSLCERTS}
 LIBS+=			-lssl
 .include "../../security/openssl/buildlink3.mk"
-
-SUBST_CLASSES+=		sslcerts
-SUBST_MESSAGE.sslcerts=	Fixing SSL certificate directory.
-SUBST_FILES.sslcerts=	src/IO/tls_openssl.c src/IO/tls_mbedtls.c
-SUBST_STAGE.sslcerts=	post-extract
-SUBST_SED.sslcerts=	-e 's,"/etc/ssl/certs,"${SSLCERTS},'
-
 .else
-CONFIGURE_ARGS+=	--disable-ssl
+CONFIGURE_ARGS+=	--disable-tls
+CONFIGURE_ARGS+=	--disable-mbedtls
+CONFIGURE_ARGS+=	--disable-openssl
 .endif
 
-.if !empty(PKG_OPTIONS:Mwebp)
+.if !empty(PKG_OPTIONS:Mlibwebp)
 CONFIGURE_ARGS+=	--enable-webp
 .include "../../graphics/libwebp/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-webp
 .endif
