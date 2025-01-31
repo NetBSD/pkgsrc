accc381fa98473cb83ca81e578619112509b54c1
pho <pho@pkgsrc.org>
2025-01-31 05:15:16 +0000

    devel/hs-foldl: update to foldl-1.4.18
    1.4.18
    
    - Add [`lifts`](https://github.com/Gabriella439/foldl/pull/214)
    - Add [`nest`](https://github.com/Gabriella439/foldl/pull/215) for `Fold1`
    - Add [`Choice`, `Closed`, `Cosieve`, `Extend`, `Semigroupoid`, `Category`, `Strong`, `Arrow` and `ArrowChoice`](https://github.com/Gabriella439/foldl/pull/215) instances for `Fold1`
    - Add [`Closed`](https://github.com/Gabriella439/foldl/pull/215) instance for `Fold`
    - [Define `stimes` from `EndoM`'s Semigroup instance for 0](https://github.com/Gabriella439/foldl/pull/217)
    
    1.4.17
    
    - Add [Fold1 utilities](https://github.com/Gabriella439/foldl/pull/212): `purely`, `purely_`, `premap`, `handles`, `foldOver`, `folded1`
    - Add pattern synonym [`Fold1_`](https://github.com/Gabriella439/foldl/pull/212) that makes the initial, step and extraction functions explicit.

--- devel/hs-foldl/buildlink3.mk
+++ devel/hs-foldl/buildlink3.mk
@@ -1,23 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:31:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/01/31 05:15:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-foldl
 
 .if !defined(HS_FOLDL_BUILDLINK3_MK)
 HS_FOLDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.16
-BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.16nb1
+BUILDLINK_API_DEPENDS.hs-foldl+=	hs-foldl>=1.4.18
+BUILDLINK_ABI_DEPENDS.hs-foldl+=	hs-foldl>=1.4.18
 BUILDLINK_PKGSRCDIR.hs-foldl?=		../../devel/hs-foldl
 
-.include "../../math/hs-comonad/buildlink3.mk"
-.include "../../math/hs-contravariant/buildlink3.mk"
-.include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../devel/hs-primitive/buildlink3.mk"
-.include "../../math/hs-profunctors/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
-.include "../../math/hs-semigroupoids/buildlink3.mk"
-.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-contravariant/buildlink3.mk"
+.include "../../math/hs-profunctors/buildlink3.mk"
+.include "../../math/hs-semigroupoids/buildlink3.mk"
+.include "../../math/hs-comonad/buildlink3.mk"
 .endif	# HS_FOLDL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-foldl
