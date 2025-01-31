30a11cee031e875e57a6176af166c89eac7bd691
pho <pho@pkgsrc.org>
2025-01-31 10:51:27 +0000

    devel/hs-relude: update to relude-1.2.2.0
    ## 1.2.2.0 – Oct 13, 2024
    
    - Support GHC-9.10 & GHC-9.8.
    - Allow hashable-1.5

--- devel/hs-relude/buildlink3.mk
+++ devel/hs-relude/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/05/09 01:32:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/01/31 10:51:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-relude
 
 .if !defined(HS_RELUDE_BUILDLINK3_MK)
 HS_RELUDE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-relude+=	hs-relude>=1.2.1
-BUILDLINK_ABI_DEPENDS.hs-relude+=	hs-relude>=1.2.1.0nb1
+BUILDLINK_API_DEPENDS.hs-relude+=	hs-relude>=1.2.2
+BUILDLINK_ABI_DEPENDS.hs-relude+=	hs-relude>=1.2.2.0
 BUILDLINK_PKGSRCDIR.hs-relude?=		../../devel/hs-relude
 
 .include "../../devel/hs-hashable/buildlink3.mk"
