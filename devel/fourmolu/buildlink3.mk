fbe0a6136ef7dcb4ee62e7b4c4a21ca19aa2d441
pho <pho@pkgsrc.org>
2025-01-31 03:55:34 +0000

    devel/fourmolu: update to fourmolu-0.17.0.0
    ## Fourmolu 0.17.0.0
    
    * Add new `import-grouping` option to group imports with grouping rules specified in configuration ([#403](https://github.com/fourmolu/fourmolu/pull/403))
    
    * Add new `sort-constraints` option to sort constraints alphabetically ([#433](https://github.com/fourmolu/fourmolu/pull/433))
    
    * Add new `sort-derived-classes` option to sort classes in deriving clauses ([#434](https://github.com/fourmolu/fourmolu/pull/434))
    
    * Add new `sort-derived-clauses` option to sort classes deriving clauses ([#434](https://github.com/fourmolu/fourmolu/pull/434))
    
    * Add new `trailing-section-operators` option to disable trailing "section" operators (those that are `infixr 0`, such as `$`) ([#444](https://github.com/fourmolu/fourmolu/pull/444))
    
    * Fix issue where `single-constraint-parens: never` would drop parentheses around implicit parameters ([#446](https://github.com/fourmolu/fourmolu/issues/446))
    
    * Fix indentation for parenthesized expressions that start off the indentation column ([#428](https://github.com/fourmolu/fourmolu/issues/428))
    
    * Allow multiline comments in indented contexts ([#65](https://github.com/fourmolu/fourmolu/issues/65))
    
    
    ## Fourmolu 0.16.2.0
    
    ### Upstream changes:
    
    #### Ormolu 0.7.7.0
    
    * Use single-line layout for parens around single-line content. [Issue
      1120](https://github.com/tweag/ormolu/issues/1120).
    
    * Allow function arguments to be on the same line even if the full type
      (with constraints and foralls) are on multiple lines. [PR
      1125](https://github.com/tweag/ormolu/pull/1125).
    
    ## Fourmolu 0.16.1.0
    
    ### Upstream changes:
    
    #### Ormolu 0.7.6.0
    
    * Fix Haddock comments on infix constructors
      [Issue 758](https://github.com/tweag/ormolu/issues/758).
    
    * Don't require a trailing newline in `.ormolu` files. [Issue
      1122](https://github.com/tweag/ormolu/issues/1122).
    
    * Remove unnecessary indentation from list comprehensions. [Issue
      966](https://github.com/tweag/ormolu/issues/966).
    
    ## Fourmolu 0.16.0.0
    
    * Allow specifying path to configuration file with `--config` ([#396](https://github.com/fourmolu/fourmolu/issues/396))
    
    ### Upstream changes:
    
    #### Ormolu 0.7.5.0
    
    * Switched to `ghc-lib-parser-9.10`, with the following new syntactic features/behaviors:
      * GHC proposal [#575](https://github.com/ghc-proposals/ghc-proposals/blob/10290a668608d608c3f6c6010be265cf7a02e1fc/proposals/0575-deprecated-instances.rst): deprecated instances.
      * GHC proposal [#281](https://github.com/ghc-proposals/ghc-proposals/blob/10290a668608d608c3f6c6010be265cf7a02e1fc/proposals/0281-visible-forall.rst): visible forall in types of terms.
        Enabled by `RequiredTypeArguments` (enabled by default).
      * `LinearTypes`: `let` and `where` bindings can now be linear, in particular have multiplicity annotations.
      * Using `forall` as an identifier is now a parse error.
      * GHC proposal [#65](https://github.com/ghc-proposals/ghc-proposals/blob/10290a668608d608c3f6c6010be265cf7a02e1fc/proposals/0065-type-infix.rst): namespacing fixity declarations for type names and WARNING/DEPRECATED pragmas.
      * `TypeAbstractions` now supports `@`-binders in lambdas and function equations.
      * Support for the `GHC2024` language.
    
    * Updated to `Cabal-syntax-3.12`.

--- devel/fourmolu/buildlink3.mk
+++ devel/fourmolu/buildlink3.mk
@@ -1,24 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:31:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/01/31 03:55:34 pho Exp $
 
 BUILDLINK_TREE+=	fourmolu
 
 .if !defined(FOURMOLU_BUILDLINK3_MK)
 FOURMOLU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fourmolu+=	fourmolu>=0.15.0
-BUILDLINK_ABI_DEPENDS.fourmolu+=	fourmolu>=0.15.0.0nb1
+BUILDLINK_API_DEPENDS.fourmolu+=	fourmolu>=0.17.0
+BUILDLINK_ABI_DEPENDS.fourmolu+=	fourmolu>=0.17.0.0
 BUILDLINK_PKGSRCDIR.fourmolu?=		../../devel/fourmolu
 
-.include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../textproc/hs-Diff/buildlink3.mk"
+.include "../../devel/hs-memotrie/buildlink3.mk"
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-choice/buildlink3.mk"
 .include "../../devel/hs-file-embed/buildlink3.mk"
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
 .include "../../textproc/hs-megaparsec/buildlink3.mk"
-.include "../../devel/hs-memotrie/buildlink3.mk"
-.include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
-.include "../../textproc/hs-yaml/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
 .endif	# FOURMOLU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fourmolu
