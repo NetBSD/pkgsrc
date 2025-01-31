bdd39c0444fe8d01802100a915e03de625192548
pho <pho@pkgsrc.org>
2025-01-31 13:45:47 +0000

    textproc/hs-charset: update to charset-0.3.11
    0.3.11 [2024.12.04]
    -------------------
    * Drop support for pre-8.0 versions of GHC.

--- textproc/hs-charset/buildlink3.mk
+++ textproc/hs-charset/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/01/31 13:45:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-charset
 
 .if !defined(HS_CHARSET_BUILDLINK3_MK)
 HS_CHARSET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-charset+=	hs-charset>=0.3.10
-BUILDLINK_ABI_DEPENDS.hs-charset+=	hs-charset>=0.3.10nb2
+BUILDLINK_API_DEPENDS.hs-charset+=	hs-charset>=0.3.11
+BUILDLINK_ABI_DEPENDS.hs-charset+=	hs-charset>=0.3.11
 BUILDLINK_PKGSRCDIR.hs-charset?=	../../textproc/hs-charset
 
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
