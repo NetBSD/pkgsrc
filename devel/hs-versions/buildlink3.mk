68586a320bb7b716c1f381d31f1062d07db826a7
pho <pho@pkgsrc.org>
2025-02-01 10:02:17 +0000

    devel/hs-versions: update to versions-6.0.7
    ## 6.0.7 (2024-06-03)
    
    #### Changed
    
    - Bumped upper bound on `base`.

--- devel/hs-versions/buildlink3.mk
+++ devel/hs-versions/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.9 2024/05/09 01:32:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/02/01 10:02:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-versions
 
 .if !defined(HS_VERSIONS_BUILDLINK3_MK)
 HS_VERSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-versions+=	hs-versions>=6.0.6
-BUILDLINK_ABI_DEPENDS.hs-versions+=	hs-versions>=6.0.6nb1
+BUILDLINK_API_DEPENDS.hs-versions+=	hs-versions>=6.0.7
+BUILDLINK_ABI_DEPENDS.hs-versions+=	hs-versions>=6.0.7
 BUILDLINK_PKGSRCDIR.hs-versions?=	../../devel/hs-versions
 
-.include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../textproc/hs-megaparsec/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-parser-combinators/buildlink3.mk"
 .endif	# HS_VERSIONS_BUILDLINK3_MK
 
