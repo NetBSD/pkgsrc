55d28b704355cb8852db973c7af43ef1ed96959c
pho <pho@pkgsrc.org>
2025-02-01 14:35:13 +0000

    www/hs-warp-tls: update to warp-tls-3.4.12
    ## 3.4.12
    
    * Rethrowing asynchronous exceptions
      [#1013](https://github.com/yesodweb/wai/pull/1013)
    
    ## 3.4.11
    
    * Removing `unliftio`.
    
    ## 3.4.10
    
    * Removed `data-default` dependency entirely. Does now require `>= tls-2.1.3`.
      [#1011](https://github.com/yesodweb/wai/pull/1011)
    
    ## 3.4.9
    
    * Using `timeout` for `handshake` to prevent thread leaks.
    
    ## 3.4.8
    
    * Substituted `data-default-class` for `data-default` [#1010](https://github.com/yesodweb/wai/pull/1010)
    
    ## 3.4.7
    
    * Expose `attachConn` to use post-handshake TLS connection.
      [#1007](https://github.com/yesodweb/wai/pull/1007)
    
    ## 3.4.6
    
    * Preparing for tls v2.1

--- www/hs-warp-tls/buildlink3.mk
+++ www/hs-warp-tls/buildlink3.mk
@@ -1,23 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.15 2024/05/09 01:32:56 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/02/01 14:35:13 pho Exp $
 
 BUILDLINK_TREE+=	hs-warp-tls
 
 .if !defined(HS_WARP_TLS_BUILDLINK3_MK)
 HS_WARP_TLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.4.5
-BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.4.5nb1
+BUILDLINK_API_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.4.12
+BUILDLINK_ABI_DEPENDS.hs-warp-tls+=	hs-warp-tls>=3.4.12
 BUILDLINK_PKGSRCDIR.hs-warp-tls?=	../../www/hs-warp-tls
 
-.include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../net/hs-network/buildlink3.mk"
-.include "../../net/hs-recv/buildlink3.mk"
-.include "../../devel/hs-streaming-commons/buildlink3.mk"
-.include "../../security/hs-tls/buildlink3.mk"
-.include "../../security/hs-tls-session-manager/buildlink3.mk"
-.include "../../devel/hs-unliftio/buildlink3.mk"
 .include "../../www/hs-wai/buildlink3.mk"
 .include "../../www/hs-warp/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../devel/hs-streaming-commons/buildlink3.mk"
+.include "../../security/hs-tls-session-manager/buildlink3.mk"
+.include "../../net/hs-recv/buildlink3.mk"
 .endif	# HS_WARP_TLS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-warp-tls
