c50d291f4d450558072372b6be47137155348270
pho <pho@pkgsrc.org>
2025-02-01 08:23:57 +0000

    devel/hs-foldable1-classes-compat: update to foldable1-classes-compat-0.1.1
    ## 0.1.1 -- 2024-12-05
    
    * Drop support for pre-8.0 versions of GHC.

--- devel/hs-foldable1-classes-compat/buildlink3.mk
+++ devel/hs-foldable1-classes-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:31:55 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/02/01 08:23:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-foldable1-classes-compat
 
 .if !defined(HS_FOLDABLE1_CLASSES_COMPAT_BUILDLINK3_MK)
 HS_FOLDABLE1_CLASSES_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-foldable1-classes-compat+=	hs-foldable1-classes-compat>=0.1
-BUILDLINK_ABI_DEPENDS.hs-foldable1-classes-compat+=	hs-foldable1-classes-compat>=0.1nb2
+BUILDLINK_API_DEPENDS.hs-foldable1-classes-compat+=	hs-foldable1-classes-compat>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-foldable1-classes-compat+=	hs-foldable1-classes-compat>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-foldable1-classes-compat?=	../../devel/hs-foldable1-classes-compat
 
 .include "../../devel/hs-tagged/buildlink3.mk"
