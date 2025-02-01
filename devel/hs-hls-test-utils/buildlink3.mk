a405b9f59681f1c2b25a0da91aaa10bb14df513a
pho <pho@pkgsrc.org>
2025-02-01 07:12:42 +0000

    devel/hs-hls-test-utils: import hs-hls-test-utils-2.9.0.0
    Please see the README on GitHub
    at https://github.com/haskell/haskell-language-server#readme

---
+++ devel/hs-hls-test-utils/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/02/01 07:12:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-hls-test-utils
+
+.if !defined(HS_HLS_TEST_UTILS_BUILDLINK3_MK)
+HS_HLS_TEST_UTILS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hls-test-utils+=	hs-hls-test-utils>=2.9.0
+BUILDLINK_ABI_DEPENDS.hs-hls-test-utils+=	hs-hls-test-utils>=2.9.0.0
+BUILDLINK_PKGSRCDIR.hs-hls-test-utils?=		../../devel/hs-hls-test-utils
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../misc/hs-extra/buildlink3.mk"
+.include "../../devel/hs-ghcide/buildlink3.mk"
+.include "../../devel/hs-hls-plugin-api/buildlink3.mk"
+.include "../../devel/hs-lens/buildlink3.mk"
+.include "../../devel/hs-lsp/buildlink3.mk"
+.include "../../devel/hs-lsp-test/buildlink3.mk"
+.include "../../devel/hs-lsp-types/buildlink3.mk"
+.include "../../textproc/hs-neat-interpolation/buildlink3.mk"
+.include "../../devel/hs-safe-exceptions/buildlink3.mk"
+.include "../../devel/hs-tasty/buildlink3.mk"
+.include "../../devel/hs-tasty-expected-failure/buildlink3.mk"
+.include "../../devel/hs-tasty-golden/buildlink3.mk"
+.include "../../devel/hs-tasty-hunit/buildlink3.mk"
+.include "../../devel/hs-tasty-rerun/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
+.include "../../textproc/hs-text-rope/buildlink3.mk"
+.endif	# HS_HLS_TEST_UTILS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hls-test-utils
