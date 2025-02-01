4558f20cf5d6aee2a3a1d6f51476aa1d5bde6757
pho <pho@pkgsrc.org>
2025-02-01 07:47:35 +0000

    devel/hs-data-default-instances-base: update to data-default-instances-base-0.1.0.3
    No release notes have been provided by the upstream.

--- devel/hs-data-default-instances-base/buildlink3.mk
+++ devel/hs-data-default-instances-base/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2024/05/09 01:31:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2025/02/01 07:47:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-default-instances-base
 
 .if !defined(HS_DATA_DEFAULT_INSTANCES_BASE_BUILDLINK3_MK)
 HS_DATA_DEFAULT_INSTANCES_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default-instances-base+=	hs-data-default-instances-base>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-data-default-instances-base+=	hs-data-default-instances-base>=0.1.0.1nb8
+BUILDLINK_API_DEPENDS.hs-data-default-instances-base+=	hs-data-default-instances-base>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-data-default-instances-base+=	hs-data-default-instances-base>=0.1.0.3
 BUILDLINK_PKGSRCDIR.hs-data-default-instances-base?=	../../devel/hs-data-default-instances-base
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
