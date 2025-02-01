40fb3654c27d163da5fb6c6ec98d1a798555afea
pho <pho@pkgsrc.org>
2025-02-01 12:47:22 +0000

    devel/hs-witherable: update to witherable-0.5
    0.5
    ---
    
    * `FilterableWithIndex` is a superclass of `WitherableWithIndex`.
    * Remove deprecated `Data.Witherable` module. Use `Witherable` module.
    * Relax `FilterableWithIndex` composition law so list-like instances are lawful.
    * Add `drain :: f a -> f b` method (with default implementation `drain = mapMaybe (Const Nothing)`) to the `Filterable`.

--- devel/hs-witherable/buildlink3.mk
+++ devel/hs-witherable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/05/09 01:32:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/02/01 12:47:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-witherable
 
 .if !defined(HS_WITHERABLE_BUILDLINK3_MK)
 HS_WITHERABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-witherable+=	hs-witherable>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-witherable+=	hs-witherable>=0.4.2nb7
+BUILDLINK_API_DEPENDS.hs-witherable+=	hs-witherable>=0.5
+BUILDLINK_ABI_DEPENDS.hs-witherable+=	hs-witherable>=0.5
 BUILDLINK_PKGSRCDIR.hs-witherable?=	../../devel/hs-witherable
 
 .include "../../devel/hs-base-orphans/buildlink3.mk"
