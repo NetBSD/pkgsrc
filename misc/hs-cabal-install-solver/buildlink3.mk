c99e63c4c4feed2585a7542a52dfac49ccf212c6
pho <pho@pkgsrc.org>
2025-02-01 13:48:35 +0000

    misc/hs-cabal-install-solver: update to cabal-install-solver-3.12.1.0
    Please see https://github.com/haskell/cabal/blob/master/release-notes/cabal-install-3.12.1.0.md

--- misc/hs-cabal-install-solver/buildlink3.mk
+++ misc/hs-cabal-install-solver/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/05/09 01:32:28 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/02/01 13:48:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-cabal-install-solver
 
 .if !defined(HS_CABAL_INSTALL_SOLVER_BUILDLINK3_MK)
 HS_CABAL_INSTALL_SOLVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.10.2
-BUILDLINK_ABI_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.10.2.1nb1
+BUILDLINK_API_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.12.1
+BUILDLINK_ABI_DEPENDS.hs-cabal-install-solver+=	hs-cabal-install-solver>=3.12.1.0
 BUILDLINK_PKGSRCDIR.hs-cabal-install-solver?=	../../misc/hs-cabal-install-solver
 
 .include "../../textproc/hs-edit-distance/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
 .endif	# HS_CABAL_INSTALL_SOLVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-cabal-install-solver
