39c54e7709247bbab05963fa7996ef4612176e4d
pho <pho@pkgsrc.org>
2025-02-01 09:05:29 +0000

    devel/hs-lukko: update to lukko-0.1.2
    # 0.1.2
    
    * Support GHC-8.6.5...9.10.1

--- devel/hs-lukko/buildlink3.mk
+++ devel/hs-lukko/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/05/09 01:32:02 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/02/01 09:05:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-lukko
 
 .if !defined(HS_LUKKO_BUILDLINK3_MK)
 HS_LUKKO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lukko+=	hs-lukko>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-lukko+=	hs-lukko>=0.1.1.3nb8
+BUILDLINK_API_DEPENDS.hs-lukko+=	hs-lukko>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-lukko+=	hs-lukko>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-lukko?=		../../devel/hs-lukko
 .endif	# HS_LUKKO_BUILDLINK3_MK
 
