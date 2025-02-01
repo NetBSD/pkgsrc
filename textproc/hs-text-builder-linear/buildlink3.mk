456e03ec79849cfb485a11296210e42cccf9189f
pho <pho@pkgsrc.org>
2025-02-01 10:15:24 +0000

    textproc/hs-text-builder-linear: update to text-builder-linear-0.1.3
    ## 0.1.3
    
    * Add decimal builders for unbounded inputs: `fromUnboundedDec`, `(|>$$)` and `($$<|)`.

--- textproc/hs-text-builder-linear/buildlink3.mk
+++ textproc/hs-text-builder-linear/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:32:47 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/02/01 10:15:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-text-builder-linear
 
 .if !defined(HS_TEXT_BUILDER_LINEAR_BUILDLINK3_MK)
 HS_TEXT_BUILDER_LINEAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.2nb1
+BUILDLINK_API_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-text-builder-linear+=	hs-text-builder-linear>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-text-builder-linear?=	../../textproc/hs-text-builder-linear
 
 .include "../../math/hs-quote-quot/buildlink3.mk"
