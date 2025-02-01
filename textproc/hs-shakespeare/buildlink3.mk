f05496a1265ece98ae2c6f539ac22d6828a3cf25
pho <pho@pkgsrc.org>
2025-02-01 10:33:13 +0000

    textproc/hs-shakespeare: update to shakespeare-2.1.1
    ### 2.1.1
    
    * Add support for `TypeApplications` inside Shakespeare quasiquotes

--- textproc/hs-shakespeare/buildlink3.mk
+++ textproc/hs-shakespeare/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/09 01:32:46 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/02/01 10:33:13 pho Exp $
 
 BUILDLINK_TREE+=	hs-shakespeare
 
 .if !defined(HS_SHAKESPEARE_BUILDLINK3_MK)
 HS_SHAKESPEARE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.1.1
+BUILDLINK_ABI_DEPENDS.hs-shakespeare+=	hs-shakespeare>=2.1.1
 BUILDLINK_PKGSRCDIR.hs-shakespeare?=	../../textproc/hs-shakespeare
 
 .include "../../converters/hs-aeson/buildlink3.mk"
