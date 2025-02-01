5a9065b09f9069f691745ae326119bd84def0c93
pho <pho@pkgsrc.org>
2025-02-01 08:42:15 +0000

    devel/hs-hashtables: update to hashtables-1.4.2
    ## 1.4.2
    
     - Fix C compile errors in cbits.
     - Bump lower bound for QuickCheck.
    
    ## 1.4.1
    
     - Fix broken compile when compiling with `-fportable` flag.
     - Make it build with GHC WASM (Bodigrim).
    
    ## 1.4.0
    
     - Replace deprecated Mutable Array function, which modifies the signature of the `length`
       function and hence the API.
     - Support more recent compilers.

--- devel/hs-hashtables/buildlink3.mk
+++ devel/hs-hashtables/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:31:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/02/01 08:42:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashtables
 
 .if !defined(HS_HASHTABLES_BUILDLINK3_MK)
 HS_HASHTABLES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashtables+=	hs-hashtables>=1.3.1
-BUILDLINK_ABI_DEPENDS.hs-hashtables+=	hs-hashtables>=1.3.1nb4
+BUILDLINK_API_DEPENDS.hs-hashtables+=	hs-hashtables>=1.4.2
+BUILDLINK_ABI_DEPENDS.hs-hashtables+=	hs-hashtables>=1.4.2
 BUILDLINK_PKGSRCDIR.hs-hashtables?=	../../devel/hs-hashtables
 
 .include "../../devel/hs-hashable/buildlink3.mk"
