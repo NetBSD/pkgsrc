b7f5d9d0943e60a5270a2f90016c403a8bce5361
pho <pho@pkgsrc.org>
2025-02-01 08:40:15 +0000

    devel/hs-git-lfs: update to git-lfs-1.2.3
    haskell-git-lfs (1.2.3) unstable; urgency=medium
    
      * Expand containers bounds to allow 0.7
    
     -- Joey Hess <id@joeyh.name>  Mon, 20 Jan 2025 16:08:14 -0400

--- devel/hs-git-lfs/buildlink3.mk
+++ devel/hs-git-lfs/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/05/09 01:31:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/02/01 08:40:15 pho Exp $
 
 BUILDLINK_TREE+=	hs-git-lfs
 
 .if !defined(HS_GIT_LFS_BUILDLINK3_MK)
 HS_GIT_LFS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-git-lfs+=	hs-git-lfs>=1.2.2
-BUILDLINK_ABI_DEPENDS.hs-git-lfs+=	hs-git-lfs>=1.2.2nb1
+BUILDLINK_API_DEPENDS.hs-git-lfs+=	hs-git-lfs>=1.2.3
+BUILDLINK_ABI_DEPENDS.hs-git-lfs+=	hs-git-lfs>=1.2.3
 BUILDLINK_PKGSRCDIR.hs-git-lfs?=	../../devel/hs-git-lfs
 
-.include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .include "../../www/hs-http-client/buildlink3.mk"
 .include "../../www/hs-http-types/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
 .endif	# HS_GIT_LFS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-git-lfs
