38f55d7b15f4f293c58e3811b2c5638b4d4d3756
pho <pho@pkgsrc.org>
2025-02-01 10:11:10 +0000

    textproc/hs-cassava-megaparsec: update to cassava-megaparsec-2.1.1
    No release notes have been provided by the upstream.

--- textproc/hs-cassava-megaparsec/buildlink3.mk
+++ textproc/hs-cassava-megaparsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2024/05/09 01:32:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2025/02/01 10:11:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-cassava-megaparsec
 
 .if !defined(HS_CASSAVA_MEGAPARSEC_BUILDLINK3_MK)
 HS_CASSAVA_MEGAPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.1.0
-BUILDLINK_ABI_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.1.0nb1
+BUILDLINK_API_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.1.1
+BUILDLINK_ABI_DEPENDS.hs-cassava-megaparsec+=	hs-cassava-megaparsec>=2.1.1
 BUILDLINK_PKGSRCDIR.hs-cassava-megaparsec?=	../../textproc/hs-cassava-megaparsec
 
 .include "../../textproc/hs-cassava/buildlink3.mk"
