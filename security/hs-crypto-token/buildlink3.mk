e4091c1a521a227b6a34db65c3e0cb815d6dbc4b
pho <pho@pkgsrc.org>
2025-02-01 14:29:40 +0000

    security/hs-crypto-token: update to crypto-token-0.1.2
    No release notes have been provided by the upstream.

--- security/hs-crypto-token/buildlink3.mk
+++ security/hs-crypto-token/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/05/09 01:32:32 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/02/01 14:29:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-crypto-token
 
 .if !defined(HS_CRYPTO_TOKEN_BUILDLINK3_MK)
 HS_CRYPTO_TOKEN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypto-token+=	hs-crypto-token>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-crypto-token+=	hs-crypto-token>=0.1.1nb1
+BUILDLINK_API_DEPENDS.hs-crypto-token+=	hs-crypto-token>=0.1.2
+BUILDLINK_ABI_DEPENDS.hs-crypto-token+=	hs-crypto-token>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-crypto-token?=	../../security/hs-crypto-token
 
 .include "../../security/hs-crypton/buildlink3.mk"
