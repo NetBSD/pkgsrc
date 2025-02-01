02103de92fd67d2aca6b57ab9e68775ab9668cf7
pho <pho@pkgsrc.org>
2025-02-01 10:28:58 +0000

    finance/hledger-lib: update to hledger-lib-1.41
    # 2024-12-09 1.41
    
    Breaking changes
    
    - New/refactored modules (Hledger.Write.*) and types (Spreadsheet) to help
      abstract rendering in various output formats, eg HTML, FODS and beancount.
      Spreadsheet is an abstraction for tabular reports, in addition to the
      tabular package we already use; there may be some overlap.
      (Henning Thielemann)
    
    - Rename displayDepth/prrDepth to displayIndent/prrIndent, and make them
      correspond to the number of indentation steps.
      (These are about indentation for rendering, not account depth.) [#2246]
    
    Improvements
    
    - Add Hledger.Data.Currency, currencySymbolToCode, currencyCodeToSymbol
    - AmountFormat: add displayQuotes flag to control enclosing quotes
    - InputOpts: add `posting_account_tags_` flag to control account tags on postings
    - Support ghc 9.10 and base 4.20.
      Note, when built with ghc 9.10.1, hledger error messages are displayed with two extra trailing newlines.
    
    Other API/doc changes
    
    - Hledger.Utils.IO: cleanup; rgb' now takes Float arguments instead of Word8
    - rename jinferredcommodities to jinferredcommoditystyles
    - rename jcommodities to jdeclaredcommodities
    - move/rename nullsourcepos
    - document isBlockActive, matcherMatches
    - posting*AsLines: fix some docs
    
    
    
    # 1.40 2024-09-09
    
    Breaking changes
    
    - Some constructors of the Interval type have been renamed for clarity.
    - Hledger.Read.CsvUtils has moved to Hledger.Write.Csv. (Henning Thielemann)
    - Tabular report rendering code has been added/reworked to allow new output formats and more reuse. (Henning Thielemann)
    
    Improvements
    
    - Added `journalDbg` debug output helper.
    
    - Allow doclayout 0.5.
    
    # 1.34 2024-06-01
    
    Improvements
    
    - InputOpts has a new `_defer` flag for internal use instead of overusing `strict_`
    - journalCheckBalanceAssertions has moved to JournalChecks

--- finance/hledger-lib/buildlink3.mk
+++ finance/hledger-lib/buildlink3.mk
@@ -1,18 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.18 2025/01/09 23:24:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2025/02/01 10:28:58 pho Exp $
 
 BUILDLINK_TREE+=	hledger-lib
 
 .if !defined(HLEDGER_LIB_BUILDLINK3_MK)
 HLEDGER_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hledger-lib+=	hledger-lib>=1.33.1
-BUILDLINK_ABI_DEPENDS.hledger-lib+=	hledger-lib>=1.33.1nb3
+BUILDLINK_API_DEPENDS.hledger-lib+=	hledger-lib>=1.41
+BUILDLINK_ABI_DEPENDS.hledger-lib+=	hledger-lib>=1.41
 BUILDLINK_PKGSRCDIR.hledger-lib?=	../../finance/hledger-lib
 
+.include "../../finance/hs-Decimal/buildlink3.mk"
+.include "../../textproc/hs-Glob/buildlink3.mk"
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../converters/hs-aeson-pretty/buildlink3.mk"
 .include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../textproc/hs-blaze-html/buildlink3.mk"
 .include "../../textproc/hs-blaze-markup/buildlink3.mk"
 .include "../../devel/hs-call-stack/buildlink3.mk"
 .include "../../textproc/hs-cassava/buildlink3.mk"
@@ -20,12 +23,11 @@ BUILDLINK_PKGSRCDIR.hledger-lib?=	../../finance/hledger-lib
 .include "../../devel/hs-cmdargs/buildlink3.mk"
 .include "../../devel/hs-colour/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
-.include "../../finance/hs-Decimal/buildlink3.mk"
 .include "../../textproc/hs-doclayout/buildlink3.mk"
 .include "../../misc/hs-extra/buildlink3.mk"
 .include "../../devel/hs-file-embed/buildlink3.mk"
-.include "../../textproc/hs-Glob/buildlink3.mk"
 .include "../../devel/hs-hashtables/buildlink3.mk"
+.include "../../textproc/hs-lucid/buildlink3.mk"
 .include "../../textproc/hs-megaparsec/buildlink3.mk"
 .include "../../devel/hs-microlens/buildlink3.mk"
 .include "../../devel/hs-microlens-th/buildlink3.mk"
@@ -37,12 +39,10 @@ BUILDLINK_PKGSRCDIR.hledger-lib?=	../../finance/hledger-lib
 .include "../../devel/hs-tasty/buildlink3.mk"
 .include "../../devel/hs-tasty-hunit/buildlink3.mk"
 .include "../../devel/hs-terminal-size/buildlink3.mk"
-.include "../../textproc/hs-text-ansi/buildlink3.mk"
 .include "../../devel/hs-timeit/buildlink3.mk"
 .include "../../devel/hs-uglymemo/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
-.include "../../misc/hs-pager/buildlink3.mk"
 .endif	# HLEDGER_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hledger-lib
