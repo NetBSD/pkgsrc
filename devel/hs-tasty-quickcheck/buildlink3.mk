28bf537394cebb584761df3cd8f65cc29736a60c
pho <pho@pkgsrc.org>
2025-02-01 09:38:07 +0000

    devel/hs-tasty-quickcheck: update to tasty-quickcheck-0.11.1
    Version 0.11.1
    --------------
    
    * Add timeouts for individual tests within a property
      ([#425](https://github.com/UnkindPartition/tasty/pull/425)).
    * Define `showDefaultValue` for `QuickCheckTests`,
      `QuickCheckMaxSize` and `QuickCheckMaxRatio`
      ([#428](https://github.com/UnkindPartition/tasty/pull/428)).
    * Print the number of QuickCheck shrinks in the progress message
      ([#431](https://github.com/UnkindPartition/tasty/pull/431)).
    * Drop support for GHC 7.10.
    
    Version 0.11
    --------------
    
    * Fix issues with QuickCheck progress reporting in the presence of `withMaxSuccess`
      ([#419](https://github.com/UnkindPartition/tasty/pull/419)).
    * Produce seeds that run a single failing tests instead of reproducing
      all the earlier successes ([#410](https://github.com/UnkindPartition/tasty/pull/410)).
    
      Seeds are now pairs instead of single integers, e.g.
      `--quickcheck-replay="(SMGen 2909028190965759779 12330386376379709109,0)"`
    
      Single integer seeds are still accepted as input, but they do run through
      earlier successes.
    
      The `QuickCheckReplay` type used as a tasty option has three data constructors
      now. `QuickCheckReplayNone` is the default value and provides no seed.
      `QuickCheckReplayLegacy` takes an integer as before. The `QuickCheckReplay`
      data constructor takes the new seed form.

--- devel/hs-tasty-quickcheck/buildlink3.mk
+++ devel/hs-tasty-quickcheck/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:14 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/02/01 09:38:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-tasty-quickcheck
 
 .if !defined(HS_TASTY_QUICKCHECK_BUILDLINK3_MK)
 HS_TASTY_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tasty-quickcheck+=	hs-tasty-quickcheck>=0.10.3
-BUILDLINK_ABI_DEPENDS.hs-tasty-quickcheck+=	hs-tasty-quickcheck>=0.10.3nb2
+BUILDLINK_API_DEPENDS.hs-tasty-quickcheck+=	hs-tasty-quickcheck>=0.11.1
+BUILDLINK_ABI_DEPENDS.hs-tasty-quickcheck+=	hs-tasty-quickcheck>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-tasty-quickcheck?=	../../devel/hs-tasty-quickcheck
 
 .include "../../devel/hs-optparse-applicative/buildlink3.mk"
