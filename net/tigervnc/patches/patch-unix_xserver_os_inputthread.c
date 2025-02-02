9e907e5d10fc62ee92c915e44da685f6a26e73ed
wiz <wiz@pkgsrc.org>
2025-02-02 16:23:06 +0000

    net/tigervnc: Update to 1.14.1
    From micha@ via wip.
    
    - Switch to FLTK 1.4
    - Remove pkgsrc patch that was merged upstream
    
    
    The bugfix release TigerVNC 1.14.1 is now available. This release fixes a
    number of regressions, the most prominent being:
    
    - Default installation of native viewer can once again handle VncAuth
    - Graphic acceleration now can now be disabled through the vncserver config
      file the same way as other features
    - Command vncpasswd can again correctly update passwords
    - Native viewer once again consider passwd file that contain more than one
      password valid
    - Native viewer can once again connect to RealVNC servers
    - Users of x0vncserver should no longer experience the mouse cursor moving
      to the upper left corner
    - H264 encoding no longer causes crashing

--- net/tigervnc/patches/patch-unix_xserver_os_inputthread.c
+++ net/tigervnc/patches/patch-unix_xserver_os_inputthread.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-unix_xserver_os_inputthread.c,v 1.1 2021/05/16 21:06:43 wiz Exp $
+$NetBSD: patch-unix_xserver_os_inputthread.c,v 1.2 2025/02/02 16:23:06 wiz Exp $
 
 --- unix/xserver/os/inputthread.c.orig	2021-04-28 14:41:01.046740215 +0000
 +++ unix/xserver/os/inputthread.c
-@@ -318,7 +317,9 @@ InputThreadDoWork(void *arg)
+@@ -318,7 +318,9 @@ InputThreadDoWork(void *arg)
      sigfillset(&set);
      pthread_sigmask(SIG_BLOCK, &set, NULL);
  
