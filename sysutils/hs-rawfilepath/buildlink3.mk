b4fec592944156bf25fdbaeffe95f5edefd47906
pho <pho@pkgsrc.org>
2025-02-02 05:31:41 +0000

    sysutils/hs-rawfilepath: update to rawfilepath-1.1.1
    No release notes have been provided by the upstream.

--- sysutils/hs-rawfilepath/buildlink3.mk
+++ sysutils/hs-rawfilepath/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/05/09 01:32:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/02/02 05:31:41 pho Exp $
 
 BUILDLINK_TREE+=	hs-rawfilepath
 
 .if !defined(HS_RAWFILEPATH_BUILDLINK3_MK)
 HS_RAWFILEPATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-rawfilepath+=	hs-rawfilepath>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-rawfilepath+=	hs-rawfilepath>=1.1.0nb2
+BUILDLINK_API_DEPENDS.hs-rawfilepath+=	hs-rawfilepath>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-rawfilepath+=	hs-rawfilepath>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-rawfilepath?=	../../sysutils/hs-rawfilepath
 .endif	# HS_RAWFILEPATH_BUILDLINK3_MK
 
