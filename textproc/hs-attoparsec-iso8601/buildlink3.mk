b60b2770f2eb54a3222218ed88f7847a9e68f71e
pho <pho@pkgsrc.org>
2025-02-02 05:34:23 +0000

    textproc/hs-attoparsec-iso8601: update to attoparsec-iso8601-1.1.1.0
    ### 1.1.1.0
    
    - Support GHC-8.6.5...9.10.1

--- textproc/hs-attoparsec-iso8601/buildlink3.mk
+++ textproc/hs-attoparsec-iso8601/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2024/05/09 01:32:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/02/02 05:34:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec-iso8601
 
 .if !defined(HS_ATTOPARSEC_ISO8601_BUILDLINK3_MK)
 HS_ATTOPARSEC_ISO8601_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.0.1nb2
+BUILDLINK_API_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.1
+BUILDLINK_ABI_DEPENDS.hs-attoparsec-iso8601+=	hs-attoparsec-iso8601>=1.1.1.0
 BUILDLINK_PKGSRCDIR.hs-attoparsec-iso8601?=	../../textproc/hs-attoparsec-iso8601
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
