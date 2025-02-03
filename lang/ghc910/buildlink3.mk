bd130b7d50b6168b12d2bdd0c2e3e27a91eae5b2
pho <pho@pkgsrc.org>
2025-02-03 16:09:09 +0000

    lang/ghc910: Prevent ${WRAPPER_DIR} from getting hardcoded in the compiler

--- lang/ghc910/buildlink3.mk
+++ lang/ghc910/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/01/29 15:28:29 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/02/03 16:09:09 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.10.1nb1
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.10.1nb1
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.10.1nb2
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.10.1nb2
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc910
 
 .include "../../converters/libiconv/buildlink3.mk"
