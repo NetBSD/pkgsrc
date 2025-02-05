ebb6468bfb296347760fe1451e9f5c979cad79f2
jschauma <jschauma@pkgsrc.org>
2025-02-05 02:46:30 +0000

    bump required version to ensure dependencies get support for MAYO

--- security/liboqs/buildlink3.mk
+++ security/liboqs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/09/13 18:02:45 jschauma Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/02/05 02:46:30 jschauma Exp $
 
 BUILDLINK_TREE+=	liboqs
 
 .if !defined(LIBOQS_BUILDLINK3_MK)
 LIBOQS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liboqs+=	liboqs>=0.10.1
+BUILDLINK_API_DEPENDS.liboqs+=	liboqs>=0.12.0
 BUILDLINK_PKGSRCDIR.liboqs?=	../../security/liboqs
 
 .endif	# LIBOQS_BUILDLINK3_MK
