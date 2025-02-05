ee06ee963a2733589ccaed98f5895f5d81270fe7
adam <adam@pkgsrc.org>
2025-02-05 09:11:59 +0000

    picard: updated to 2.13
    Version 2.13 - 2025-02-04
    
    Bugfixes
    - Endless recursion when reading settings after script editor dialog was opened
    - File naming options inaccessible on permission errors to target path
    - Children's Music is shown as "Children'S Music" in Picard
    - Picard requires charset-normalizer < 3.4.0
    - Poorly formatted `date` tags crash Picard with uncaught Overflow Exception
    - Wrong disc numbers when submitting release
    - Case only renaming broken on case-insensitive filesystems
    - Sorting on macOS does not sort empty values as expected
    
    Tasks
    - Update Windows code signing certificate
    - Drop support for Python 3.7
    - Support Python 3.13
    
    Improvements
    - Avoid manual copying of authentication token
    - Support PKCE for OAuth2 authorization
    - OAuth2: Implement token revocation on the server
    - Add context menu action to metadata box to merge original and new values

--- audio/picard/patches/patch-setup.py
+++ audio/picard/patches/patch-setup.py
@@ -1,23 +1,24 @@
-$NetBSD: patch-setup.py,v 1.1 2021/05/15 11:45:52 nia Exp $
+$NetBSD: patch-setup.py,v 1.2 2025/02/05 09:12:00 adam Exp $
 
 Don't selectively install app store data.
 
---- setup.py.orig	2020-05-06 09:36:12.000000000 +0000
+--- setup.py.orig	2025-02-04 16:09:45.000000000 +0000
 +++ setup.py
-@@ -289,8 +289,6 @@ class picard_build(build):
+@@ -313,7 +313,7 @@ class picard_build(build):
                  'publisher': os.environ.get('PICARD_APPX_PUBLISHER', default_publisher),
-                 'version': '.'.join([str(v) for v in store_version]),
+                 'version': '.'.join(str(v) for v in store_version),
              })
--        elif sys.platform == 'linux':
--            self.run_command('build_appdata')
-         build.run(self)
+-        elif sys.platform not in {'darwin', 'haiku1', 'win32'}:
++        elif False: #sys.platform not in {'darwin', 'haiku1', 'win32'}:
+             self.run_command('build_appdata')
+             self.run_command('build_desktop_file')
+         super().run()
+@@ -835,7 +835,7 @@ def find_file_in_path(filename):
+             return file_path
  
  
-@@ -815,7 +813,4 @@ args['data_files'] = [
- args['data_files'].append(('share/icons/hicolor/scalable/apps', ['resources/%s.svg' % PICARD_APP_ID]))
- args['data_files'].append(('share/applications', [PICARD_DESKTOP_NAME]))
- 
--if sys.platform == 'linux':
--    args['data_files'].append(('share/metainfo', [APPDATA_FILE]))
--
- setup(**args)
+-if sys.platform not in {'darwin', 'haiku1', 'win32'}:
++if False: #sys.platform not in {'darwin', 'haiku1', 'win32'}:
+     args['data_files'].append(('share/applications', [PICARD_DESKTOP_NAME]))
+     args['data_files'].append(('share/icons/hicolor/scalable/apps', ['resources/%s.svg' % PICARD_APP_ID]))
+     for size in (16, 24, 32, 48, 128, 256):
