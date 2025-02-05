26fed231a45f309e4b589daa19763dc9063e8995
riastradh <riastradh@pkgsrc.org>
2025-02-05 23:10:52 +0000

    devel/log4cplus/buildlink3.mk: Need to declare iconv here too.
    Otherwise downstream packages won't find -liconv as needed when
    PREFER_PKGSRC+=iconv.

--- devel/log4cplus/buildlink3.mk
+++ devel/log4cplus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2023/08/14 05:24:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/02/05 23:10:52 riastradh Exp $
 
 BUILDLINK_TREE+=	log4cplus
 
@@ -8,6 +8,8 @@ LOG4CPLUS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.log4cplus+=	log4cplus>=2.0.3
 BUILDLINK_ABI_DEPENDS.log4cplus?=		log4cplus>=2.0.3nb1
 BUILDLINK_PKGSRCDIR.log4cplus?=		../../devel/log4cplus
+
+.include "../../converters/libiconv/buildlink3.mk"
 .endif	# LOG4CPLUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-log4cplus
