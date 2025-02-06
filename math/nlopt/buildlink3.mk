f829ce5b8e3755ed4531f0b78aed1b50cc6a8b96
adam <adam@pkgsrc.org>
2025-02-06 08:48:59 +0000

    revbump for math/nlopt

--- math/nlopt/buildlink3.mk
+++ math/nlopt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/08/11 08:31:20 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/02/06 08:48:59 adam Exp $
 
 BUILDLINK_TREE+=	nlopt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nlopt
 NLOPT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.7.1
-BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.7.1
+BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.10.0
 BUILDLINK_PKGSRCDIR.nlopt?=	../../math/nlopt
 .endif # NLOPT_BUILDLINK3_MK
 
