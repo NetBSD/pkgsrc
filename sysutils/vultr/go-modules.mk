74c940429676e45cf9e0ac11bb88c0bc7dc5a8d3
schmonz <schmonz@pkgsrc.org>
2025-02-06 21:21:12 +0000

    vultr: update to 2.0.3. Changes:
    - update vendoring
    - Migrate to go modules build
    - read HTTP_PROXY from environment if set
    - Convert int to string using fmt.Sprint
    - fix: typo
    - Update plans.go

---
+++ sysutils/vultr/go-modules.mk
@@ -0,0 +1,25 @@
+# $NetBSD: go-modules.mk,v 1.1 2025/02/06 21:21:12 schmonz Exp $
+
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.zip
+GO_MODULE_FILES+=	github.com/jawher/mow.cli/@v/v0.0.0-20150204181009-33e2f99c7bed.mod
+GO_MODULE_FILES+=	github.com/jawher/mow.cli/@v/v0.0.0-20150204181009-33e2f99c7bed.zip
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.3.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191011191535-87dc89f01550.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191011191535-87dc89f01550.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20190308202827-9d24e82272b4.mod
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20190308202827-9d24e82272b4.zip
