5fd8e6dfc3f0d0311edb5cf02ad2bf5393574870
pho <pho@pkgsrc.org>
2025-02-07 12:36:23 +0000

    lang/quickjs: update to 2024-01-13
    2024-01-13:
    
    - top-level-await support in modules
    - allow 'await' in the REPL
    - added Array.prototype.{with,toReversed,toSpliced,toSorted} and
    TypedArray.prototype.{with,toReversed,toSorted}
    - added String.prototype.isWellFormed and String.prototype.toWellFormed
    - added Object.groupBy and Map.groupBy
    - added Promise.withResolvers
    - class static block
    - 'in' operator support for private fields
    - optional chaining fixes
    - added RegExp 'd' flag
    - fixed RegExp zero length match logic
    - fixed RegExp case insensitive flag
    - added os.getpid() and os.now()
    - added cosmopolitan build
    - misc bug fixes
    
    2023-12-09:
    
    - added Object.hasOwn, {String|Array|TypedArray}.prototype.at,
      {Array|TypedArray}.prototype.findLast{Index}
    - BigInt support is enabled even if CONFIG_BIGNUM disabled
    - updated to Unicode 15.0.0
    - misc bug fixes

--- lang/quickjs/patches/patch-qjs.c
+++ lang/quickjs/patches/patch-qjs.c
@@ -1,35 +1,15 @@
-$NetBSD: patch-qjs.c,v 1.2 2023/03/07 14:38:03 he Exp $
+$NetBSD: patch-qjs.c,v 1.3 2025/02/07 12:36:24 pho Exp $
 
 Portability patch for NetBSD.
 
---- qjs.c.orig	2019-10-27 10:55:35.000000000 +0000
+--- qjs.c.orig	2024-01-13 10:20:39.000000000 +0000
 +++ qjs.c
-@@ -146,7 +146,7 @@ static inline size_t js_trace_malloc_usa
+@@ -146,7 +146,7 @@ static size_t js_trace_malloc_usable_siz
      return malloc_size(ptr);
  #elif defined(_WIN32)
-     return _msize(ptr);
+     return _msize((void *)ptr);
 -#elif defined(EMSCRIPTEN)
 +#elif defined(EMSCRIPTEN) || defined(__NetBSD__)
      return 0;
  #elif defined(__linux__)
-     return malloc_usable_size(ptr);
-@@ -268,7 +268,7 @@ static const JSMallocFunctions trace_mf 
-     malloc_size,
- #elif defined(_WIN32)
-     (size_t (*)(const void *))_msize,
--#elif defined(EMSCRIPTEN)
-+#elif defined(EMSCRIPTEN) || defined(__NetBSD__)
-     NULL,
- #elif defined(__linux__)
-     (size_t (*)(const void *))malloc_usable_size,
-@@ -454,8 +454,10 @@ int main(int argc, char **argv)
-         }
-     }
- 
-+#ifdef CONFIG_BIGNUM
-     if (load_jscalc)
-         bignum_ext = 1;
-+#endif
- 
-     if (trace_memory) {
-         js_trace_malloc_init(&trace_data);
+     return malloc_usable_size((void *)ptr);
