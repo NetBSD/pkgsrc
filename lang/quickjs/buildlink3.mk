b3b9d6cf95f3ee8e6352b48b67795f01b1642d37
pho <pho@pkgsrc.org>
2025-02-07 12:36:23 +0000

    lang/quickjs: update to 2024-01-13
    2024-01-13:
    
    - top-level-await support in modules
    - allow 'await' in the REPL
    - added Array.prototype.{with,toReversed,toSpliced,toSorted} and
    TypedArray.prototype.{with,toReversed,toSorted}
    - added String.prototype.isWellFormed and String.prototype.toWellFormed
    - added Object.groupBy and Map.groupBy
    - added Promise.withResolvers
    - class static block
    - 'in' operator support for private fields
    - optional chaining fixes
    - added RegExp 'd' flag
    - fixed RegExp zero length match logic
    - fixed RegExp case insensitive flag
    - added os.getpid() and os.now()
    - added cosmopolitan build
    - misc bug fixes
    
    2023-12-09:
    
    - added Object.hasOwn, {String|Array|TypedArray}.prototype.at,
      {Array|TypedArray}.prototype.findLast{Index}
    - BigInt support is enabled even if CONFIG_BIGNUM disabled
    - updated to Unicode 15.0.0
    - misc bug fixes

--- lang/quickjs/buildlink3.mk
+++ lang/quickjs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/02/07 12:20:38 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/02/07 12:36:23 pho Exp $
 
 BUILDLINK_TREE+=	quickjs
 
 .if !defined(QUICKJS_BUILDLINK3_MK)
 QUICKJS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.quickjs+=	quickjs>=20210327nb3
+BUILDLINK_API_DEPENDS.quickjs+=	quickjs>=20240113
 BUILDLINK_PKGSRCDIR.quickjs?=	../../lang/quickjs
 .endif	# QUICKJS_BUILDLINK3_MK
 
--- lang/quickjs/patches/patch-quickjs.c
+++ lang/quickjs/patches/patch-quickjs.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-quickjs.c,v 1.3 2022/01/30 10:55:03 he Exp $
+$NetBSD: patch-quickjs.c,v 1.4 2025/02/07 12:36:24 pho Exp $
 
 Portability patch for NetBSD.
 
---- quickjs.c.orig	2021-03-27 10:00:32.000000000 +0000
+--- quickjs.c.orig	2024-01-13 10:20:39.000000000 +0000
 +++ quickjs.c
-@@ -70,7 +70,15 @@
+@@ -68,7 +68,15 @@
  /* define to include Atomics.* operations which depend on the OS
     threads */
  #if !defined(EMSCRIPTEN)
@@ -20,21 +20,12 @@ Portability patch for NetBSD.
  #endif
  
  #if !defined(EMSCRIPTEN)
-@@ -1680,7 +1688,7 @@ static inline size_t js_def_malloc_usabl
+@@ -1699,7 +1707,7 @@ static size_t js_def_malloc_usable_size(
      return malloc_size(ptr);
  #elif defined(_WIN32)
-     return _msize(ptr);
+     return _msize((void *)ptr);
 -#elif defined(EMSCRIPTEN)
 +#elif defined(EMSCRIPTEN) || defined(__NetBSD__)
      return 0;
  #elif defined(__linux__)
-     return malloc_usable_size(ptr);
-@@ -1754,7 +1762,7 @@ static const JSMallocFunctions def_mallo
-     malloc_size,
- #elif defined(_WIN32)
-     (size_t (*)(const void *))_msize,
--#elif defined(EMSCRIPTEN)
-+#elif defined(EMSCRIPTEN) || defined(__NetBSD__)
-     NULL,
- #elif defined(__linux__)
-     (size_t (*)(const void *))malloc_usable_size,
+     return malloc_usable_size((void *)ptr);
