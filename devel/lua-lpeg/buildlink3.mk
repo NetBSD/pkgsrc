5c8fb7d450027d57a2e75cbeee0f837776a7dcdf
adam <adam@pkgsrc.org>
2025-02-07 07:50:33 +0000

    lua-lpeg: also buildlink dylib for Darwin

--- devel/lua-lpeg/buildlink3.mk
+++ devel/lua-lpeg/buildlink3.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: buildlink3.mk,v 1.1 2025/01/24 12:45:54 riastradh Exp $
+#	$NetBSD: buildlink3.mk,v 1.2 2025/02/07 07:50:33 adam Exp $
 
 BUILDLINK_TREE+=	lua-lpeg
 
@@ -9,7 +9,8 @@ BUILDLINK_API_DEPENDS.lua-lpeg+=	${LUA_PKGPREFIX}-lpeg>=1.1.0nb1
 BUILDLINK_ABI_DEPENDS.lua-lpeg+=	${LUA_PKGPREFIX}-lpeg>=1.1.0nb1
 BUILDLINK_PKGSRCDIR.lua-lpeg=		../../devel/lua-lpeg
 
-BUILDLINK_FILES.lua-lpeg+=		lib/lua/*/lpeg.so
+BUILDLINK_FILES.lua-lpeg+=	lib/lua/*/lpeg.dylib
+BUILDLINK_FILES.lua-lpeg+=	lib/lua/*/lpeg.so
 .endif
 
 BUILDLINK_TREE+=	-lua-lpeg
