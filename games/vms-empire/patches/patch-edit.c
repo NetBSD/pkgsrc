c00bb588fc999c25005435c78099d7f2aacc1b23
triaxx <triaxx@pkgsrc.org>
2025-02-08 10:49:45 +0000

    vms-empire: Update to 1.18
    upstream changes:
    -----------------
    1.18: 2024-10-03::
    	Port changes for GCC 10.
    1.17: 2024-02-11::
    	Remove unnecessary delays.
    	Fix a defective sort in the fame AI.
    1.16: 2021-02-25 20:52:1Z
    	Minor documentation fixes and code cleanup.

--- games/vms-empire/patches/patch-edit.c
+++ games/vms-empire/patches/patch-edit.c
@@ -1,33 +1,33 @@
-$NetBSD: patch-edit.c,v 1.1 2016/01/11 19:37:43 leot Exp $
+$NetBSD: patch-edit.c,v 1.2 2025/02/08 10:49:45 triaxx Exp $
 
 Fix ctype(3) functions usage.
 
---- edit.c.orig	2014-12-15 00:46:31.000000000 +0000
+--- edit.c.orig	2024-02-11 16:53:28.000000000 +0000
 +++ edit.c
-@@ -316,7 +316,7 @@ static char dirs[] = "WEDCXZAQ";
- void
- e_stasis(loc_t loc)
- {
--    if (!isupper (user_map[loc].contents))
-+    if (!isupper ((unsigned char)user_map[loc].contents))
- 	huh (); /* no object here */
-     else if (user_map[loc].contents == 'X')
- 	huh ();
-@@ -437,7 +437,7 @@ Beginning of move to location.
- void
- e_move(loc_t *path_start, loc_t loc)
- {
--    if (!isupper(user_map[loc].contents)) huh (); /* nothing there? */
-+    if (!isupper((unsigned char)user_map[loc].contents)) huh (); /* nothing there? */
-     else if (user_map[loc].contents == 'X') huh (); /* enemy city? */
-     else *path_start = loc;
- }
-@@ -509,7 +509,7 @@ e_piece_info(loc_t edit_cursor, char ab)
-     int type;
-     char *p;
+@@ -305,7 +305,7 @@ Set object to move in a direction.
+ static char dirs[] = "WEDCXZAQ";
  
--    ab = toupper (ab);
-+    ab = toupper ((unsigned char)ab);
-     p = strchr (type_chars, ab);
-     type = p - type_chars;
+ void e_stasis(loc_t loc) {
+-	if (!isupper(game.user_map[loc].contents)) {
++	if (!isupper((unsigned char)game.user_map[loc].contents)) {
+ 		huh(); /* no object here */
+ 	} else if (game.user_map[loc].contents == 'X') {
+ 		huh();
+@@ -421,7 +421,7 @@ Beginning of move to location.
+ */
+ 
+ void e_move(loc_t *path_start, loc_t loc) {
+-	if (!isupper(game.user_map[loc].contents)) {
++	if (!isupper((unsigned char)game.user_map[loc].contents)) {
+ 		huh(); /* nothing there? */
+ 	} else if (game.user_map[loc].contents == 'X') {
+ 		huh(); /* enemy city? */
+@@ -493,7 +493,7 @@ void e_piece_info(loc_t edit_cursor, cha
+ 	int type;
+ 	char *p;
+ 
+-	ab = toupper(ab);
++	ab = toupper((unsigned char)ab);
+ 	p = strchr(type_chars, ab);
+ 	type = p - type_chars;
  
--- games/vms-empire/patches/patch-object.c
+++ games/vms-empire/patches/patch-object.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-object.c,v 1.1 2016/01/11 19:37:43 leot Exp $
+$NetBSD: patch-object.c,v 1.2 2025/02/08 10:49:45 triaxx Exp $
 
 Fix ctype(3) functions usage.
 
---- object.c.orig	2014-12-15 00:57:48.000000000 +0000
+--- object.c.orig	2024-02-11 15:57:48.000000000 +0000
 +++ object.c
-@@ -568,7 +568,7 @@ update(view_map_t vmap[], loc_t loc)
- 	else if (p->owner == USER)
- 	    vmap[loc].contents = piece_attr[p->type].sname;
- 	else
--	    vmap[loc].contents = tolower (piece_attr[p->type].sname);
-+	    vmap[loc].contents = tolower ((unsigned char)piece_attr[p->type].sname);
-     }
-     if (vmap == comp_map)
- 	display_locx (COMP, comp_map, loc);
+@@ -552,7 +552,7 @@ void update(view_map_t vmap[], loc_t loc
+ 		else if (p->owner == USER)
+ 			vmap[loc].contents = piece_attr[p->type].sname;
+ 		else
+-			vmap[loc].contents = tolower(piece_attr[p->type].sname);
++			vmap[loc].contents = tolower((unsigned char)piece_attr[p->type].sname);
+ 	}
+ 	if (vmap == game.comp_map)
+ 		display_locx(COMP, game.comp_map, loc);
