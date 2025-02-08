125806b5498d0ca66f65d9027235a5cc6d3330e7
wiz <wiz@pkgsrc.org>
2025-02-08 14:05:50 +0000

    lang/gcc13-gnat-libs: import gcc13-gnat-libs-13.3.0nb3
    Packaged for wip by Dmytro Kazankov.
    
    The GNU Compiler Collection (GCC) GNAT libraries
    
    This packages provides GNAT support libraries in a specific location and allows
    packages to depend on just the libraries rather than having to pull in the full
    GNAT package.

---
+++ lang/gcc13-gnat-libs/buildlink3.mk
@@ -0,0 +1,37 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/02/08 14:05:50 wiz Exp $
+
+BUILDLINK_TREE+=	gcc13-gnat-libs
+
+.if !defined(GCC13_GNAT_LIBS_BUILDLINK3_MK)
+GCC13_GNAT_LIBS_BUILDLINK3_MK:=
+
+.if !empty(USE_PKGSRC_GCC_RUNTIME:M[Yy][Ee][Ss])
+BUILDLINK_API_DEPENDS.gcc13-gnat-libs+=	gcc13-gnat-libs>=13.2.0
+.else
+BUILDLINK_API_DEPENDS.gcc13-gnat-libs+=	{gcc13-gnat,gcc13-gnat-libs}>=13.2.0
+.endif
+BUILDLINK_PKGSRCDIR.gcc13-gnat-libs=	../../lang/gcc13-gnat-libs
+BUILDLINK_DEPMETHOD.gcc13-gnat-libs?=	full
+
+GCC_TARGET_MACHINE?=	${MACHINE_GNU_PLATFORM}
+
+TARGET_LIBDIR.gcc13-gnat-libs=	${PREFIX}/gcc13-gnat/${GCC_TARGET_MACHINE}/lib
+
+BUILDLINK_PASSTHRU_DIRS+=	${TARGET_LIBDIR.gcc13-gnat-libs}
+
+BUILDLINK_FILES.gcc13-gnat-libs=	#empty
+BUILDLINK_AUTO_VARS.gcc13-gnat-libs=	no
+BUILDLINK_RPATHDIRS.gcc13-gnat-libs=	${TARGET_LIBDIR.gcc13-gnat-libs}
+
+.if defined(LD_RUN_PATH)
+LD_RUN_PATH:=	${TARGET_LIBDIR.gcc13-gnat-libs}:${LD_RUN_PATH}
+.else
+LD_RUN_PATH:=	${TARGET_LIBDIR.gcc13-gnat-libs}
+.endif
+
+.include "../../mk/dlopen.buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+
+.endif # GCC13_GNAT_LIBS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gcc13-gnat-libs
