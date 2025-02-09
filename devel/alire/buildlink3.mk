05f21aee3f942ad14e721895796192a453727622
wiz <wiz@pkgsrc.org>
2025-02-09 13:34:23 +0000

    devel/alire: Update to 2.0.1nb1
    From Dmytro Kazankov via wip.
    
    * Package updated to upstream 2.0.1 release
    * Build type changed to "release" to avoid runtime exceptions:
      some patches added/changed, Makefile modified
    * Build process moved to gprbuild.mk

--- devel/alire/buildlink3.mk
+++ devel/alire/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/03/12 17:53:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/02/09 13:34:24 wiz Exp $
 
 BUILDLINK_TREE+=	alire
 
@@ -8,6 +8,13 @@ ALIRE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.alire+=	alire>=2.0.0rc1
 BUILDLINK_PKGSRCDIR.alire=	../../devel/alire
 BUILDLINK_DEPMETHOD.alire?=	build
+
+BUILDLINK_AUTO_VARS.alire=	no
+BUILDLINK_AUTO_DIRS.alire=	no
+
+BUILDLINK_FILES.alire=			bin/alr share/gpr/manifests/alr*
+BUILDLINK_CONTENTS_FILTER.alire=	\
+	${EGREP} '(bin/alr|share/gpr/manifests/alr.*)'
 
 .endif
 
