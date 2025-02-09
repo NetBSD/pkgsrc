089ba64303735f95b5acc55c54c7ec6ba0fea5f2
wiz <wiz@pkgsrc.org>
2025-02-09 13:34:23 +0000

    devel/alire: Update to 2.0.1nb1
    From Dmytro Kazankov via wip.
    
    * Package updated to upstream 2.0.1 release
    * Build type changed to "release" to avoid runtime exceptions:
      some patches added/changed, Makefile modified
    * Build process moved to gprbuild.mk

--- devel/alire/patches/patch-src_alire_alire-platforms.ads
+++ devel/alire/patches/patch-src_alire_alire-platforms.ads
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_alire_alire-platforms.ads,v 1.1 2024/03/12 17:53:16 wiz Exp $
+$NetBSD: patch-src_alire_alire-platforms.ads,v 1.2 2025/02/09 13:34:24 wiz Exp $
 
 Add NetBSD support
 
---- src/alire/alire-platforms.ads.orig	2023-10-19 15:04:21.000000000 +0000
-+++ src/alire/alire-platforms.ads
-@@ -21,6 +21,7 @@ package Alire.Platforms with Preelaborat
-    --  See e.g. https://stackoverflow.com/a/45125525/761390
- 
-    type Operating_Systems is (FreeBSD,
-+                              NetBSD,
+--- src/alire/alire-platforms.ads.orig	2024-03-21 00:22:58.000000000 +0200
++++ src/alire/alire-platforms.ads	2024-04-09 16:21:29.009936835 +0300
+@@ -24,6 +24,7 @@
                                Linux,
                                MacOS,
                                Windows,
++                              NetBSD,
+                               OS_Unknown);
+    subtype Known_Operating_Systems is
+      Operating_Systems range
