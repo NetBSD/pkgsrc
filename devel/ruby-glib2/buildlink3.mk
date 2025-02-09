17c24c9484037aaf5539e56c6526106e5c7dc144
tsutsui <tsutsui@pkgsrc.org>
2025-02-09 09:19:08 +0000

    ruby-gnome: update to 4.2.7.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.2.7
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.2.6
    
    Ruby-GNOME 4.2.7: 2025-01-29
    
    This is a Ruby 2.5 on AlmaLinux 8 support release.
    
    Change
    
    Ruby/GLib2
    
      * Improvements
    
          + Added support for Ruby 2.5 on AlmaLinux 8 again.
    
    Ruby-GNOME 4.2.6: 2025-01-25
    
    This is a C23 support release. This includes ABI incompatible changes
    but API is still compatible. If your gem uses
    RG_DEF_METHOD()/RG_DEF_SMETHOD()/RG_DEF_PRIVATE_METHOD()
    directly instead of using GObjectIntrospection, you need to reinstall
    them.
    
    Changes
    
    Ruby/GLib2
    
      * Improvements
    
          + Added GLib::Value.try_convert for auto conversion.
    
          + Added support for instantiating GTypeInstance objects.
              o GH-1647
              o Reported by ppibburr
    
          + Added support for properties that uses GTypeInstance.
              o GH-1651
              o Reported by ppibburr
    
          + GLib::{Variant,Object,Param}: Changed parent class to
            GLib::Instantiatable.
    
          + Added support for GValue<GTypeInstance>
              o GH-1654
              o GH-1655
              o GH-1656
              o Patch by ppibburr
    
          + GLib::Instantiatable#inspect
              o GH-1652
              o GH-1653
              o Patch by ppibburr
    
          + Added support for C23.
              o rbg_define_singleton_method(): Removed because it's not C23
                compatible.
              o rbg_define_method(): Removed because it's not C23 compatible.
              o rbg_define_private_method(): Removed because it's not C23
                compatible.
              o GH-1660
              o Reported by Mamoru TASAKA
    
          + Removed Data_Get_Struct() fallback. We assume that all users
            migrated to TypedData from Data.
    
      * Fixes
    
          + GLib.setenv: Fixed a bug that the third argument can't be
            passed.
    
    Ruby/GObjectIntrospection
    
      * Improvements
    
          + Added support for (transfer full) of boolean/number return.
    
          + Added support for ref/unref for GTypeInstance
              o GH-1657
              o GH-1659
              o Patch by ppibburr
    
    Ruby/GTK4
    
      * Fixes
    
          + Fixed a bug that Gtk::TextBuffer#get_iter_at(:line) returns
            Array.
              o GH-1649
              o Reported by Eric Cunningham
    
    Ruby/GStreamer
    
      * Improvements
    
          + Gst::AudioLoader: Added.
              o GH-1634
              o GH-1641
              o Patch by KITAITI Makoto
    
          + Gst::Caps#set_int_value: Added. It's a convenient
            Gst::Caps#set_value for a GLib::Type::INT value.
    
          + Gst::Caps#[]=: Added. It's a convenient Gst::Caps#set_value
            wrapper.
    
          + Gst::Structure#[]: Added. It's a convenient
            Gst::Structure#get_value wrapper.
    
    Thanks
    
      * KITAITI Makoto
      * ppibburr
      * Eric Cunningham

--- devel/ruby-glib2/buildlink3.mk
+++ devel/ruby-glib2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/12/29 15:53:08 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/02/09 09:19:09 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-glib2
 
 .if !defined(RUBY_GLIB2_BUILDLINK3_MK)
 RUBY_GLIB2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.5
-BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.5
+BUILDLINK_API_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.7
+BUILDLINK_ABI_DEPENDS.ruby-glib2+=	${RUBY_PKGPREFIX}-glib2>=4.2.7
 BUILDLINK_PKGSRCDIR.ruby-glib2?=	../../devel/ruby-glib2
 
 .include "../../devel/glib2/buildlink3.mk"
--- graphics/ruby-gdk3/buildlink3.mk
+++ graphics/ruby-gdk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.27 2024/12/29 15:53:09 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2025/02/09 09:19:10 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gdk3
 
 .if !defined(RUBY_GDK3_BUILDLINK3_MK)
 RUBY_GDK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.2.5
-BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.2.5
+BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.2.7
+BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.2.7
 BUILDLINK_PKGSRCDIR.ruby-gdk3?=		../../graphics/ruby-gdk3
 
 .include "../../devel/ruby-pango/buildlink3.mk"
