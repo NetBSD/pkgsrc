186be2e1c9e9a20ae3b954edb5ae37e12382345d
pho <pho@pkgsrc.org>
2025-02-10 03:52:32 +0000

    net/libfetch: Install pkg-config fetch.pc
    It's particularly important for libfetch to have a pkg-config file since it
    can conditionally depend on libssl and libcrypto. Without it downstream
    software have a hard time figuring out how to link it correctly.
    
    While here fix several pkglint errors and warnings.

--- net/libfetch/buildlink3.mk
+++ net/libfetch/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2024/12/05 17:41:15 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2025/02/10 03:52:32 pho Exp $
 
 BUILDLINK_TREE+=	libfetch
 
@@ -7,8 +7,7 @@ LIBFETCH_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.libfetch?=	build
 
-BUILDLINK_API_DEPENDS.libfetch+=	libfetch>=2.28
-BUILDLINK_ABI_DEPENDS.libfetch?=	libfetch>=2.39nb2
+BUILDLINK_API_DEPENDS.libfetch+=	libfetch>=2.40nb1
 BUILDLINK_PKGSRCDIR.libfetch?=		../../net/libfetch
 
 pkgbase := libfetch
@@ -20,7 +19,7 @@ BUILDLINK_LIBS.libfetch+=	-lsocket
 
 .if ${PKG_BUILD_OPTIONS.libfetch:Mopenssl}
 .  include "../../security/openssl/buildlink3.mk"
-BUILDLINK_DEPMETHOD.openssl=	full
+BUILDLINK_DEPMETHOD.openssl+=	full
 BUILDLINK_LIBS.libfetch+=	-lssl -lcrypto
 .endif
 
