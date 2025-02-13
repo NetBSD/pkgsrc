db2cf747cb282544d9615d595e0307e876a64454
ryoon <ryoon@pkgsrc.org>
2025-02-13 03:26:47 +0000

    graphics/pstoedit: Add tested buildlink3.mk
    * I have forgotten to add this before updating graphics/autotrace.

--- graphics/pstoedit/buildlink3.mk
+++ graphics/pstoedit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/02/12 21:05:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/02/13 03:26:47 ryoon Exp $
 
 BUILDLINK_TREE+=	pstoedit
 
@@ -7,6 +7,10 @@ PSTOEDIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pstoedit+=	pstoedit>=3.78nb26
 BUILDLINK_PKGSRCDIR.pstoedit?=		../../graphics/pstoedit
+
+BUILDLINK_INCDIRS.pstoedit+=		include/pstoedit
+BUILDLINK_LIBDIRS.pstoedit+=		lib/pstoedit
+BUILDLINK_FNAME_TRANSFORM.pstoedit+=	-e 's|lib/pstoedit/pkgconfig/|lib/pkgconfig/|'
 
 .endif	# PSTOEDIT_BUILDLINK3_MK
 
