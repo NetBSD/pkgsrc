0a926929b71351900fce221693b9a49ac985f61c
vins <vins@pkgsrc.org>
2025-02-13 16:43:50 +0000

    math/sc-im: update to 0.8.4
    # pkgsrc changes
    * make threads and x11 support opt-in
    * do not require bison
    * fix Lua detection from pkg-config
    * package builds fine with lua54: use LUA_PACKAGE
    
    # upstream changes
    v0.8.4
    
    06/12/2023: :datefmt does not remove text content anymore
    	allowing future edition - work on item 3 of issue 769
    06/11/2023: avoid changing cell's color after :format - issue 819
    06/11/2023: avoid blanking cell's text content after :datefmt
    06/10/2023: fix number 2 of issue 769
    06/02/2023: fix configuration variable name in file.c:
    	import_delimited_to_text
    06/01/2023: restore transpose
    06/01/2023: added input_edit_mode setting PR 626
    06/01/2023: work on issue 708: xlsx file import
    06/01/2023: Fix issue 784: get row number and column or cell that calls
    	a LUA script with PR 814.
    06/01/2023: merged PR 783 that gets rid of issue 778 - changed load_rc
    	load_file order.06/01/2023: merged PR 776
    06/01/2023: merged PR 789
    06/01/2023: fix issue in csv import - 816
    02/03/2024: fix issue 868
    17/05/2024: fix missing include
    20/05/2024: PR 871
    02/07/2024: Modify Makefile to work with brew's ncurses
    10/07/2024: PR 880 / 861
    16/07/2024: work on issue 878
    23/07/2024: work on issue 875 / 879

--- math/sc-im/options.mk
+++ math/sc-im/options.mk
@@ -1,8 +1,16 @@
-# $NetBSD: options.mk,v 1.4 2021/07/26 01:03:28 sjmulder Exp $
+# $NetBSD: options.mk,v 1.5 2025/02/13 16:43:50 vins Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sc-im
-PKG_SUPPORTED_OPTIONS+=	color xls xlsx lua
+PKG_SUPPORTED_OPTIONS+=	color xls xlsx lua threads x11
 PKG_SUGGESTED_OPTIONS+=	color xls xlsx lua
+
+CHECK_BUILTIN.pthread:= yes
+.include "../../mk/pthread.builtin.mk"
+CHECK_BUILTIN.pthread:= no
+
+.if ${USE_BUILTIN.pthread:tl} == yes
+PKG_SUGGESTED_OPTIONS+=	threads
+.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -25,5 +33,22 @@ LDLIBS+=	-lxlsxwriter -lcrypto -lminizip
 .endif
 
 .if !empty(PKG_OPTIONS:Mlua)
-.include "../../lang/lua51/buildlink3.mk"
+MAKE_ENV+=   LUA_PKGNAME=${LUA_PACKAGE:S/lua/lua-/:S/5/5./}
+.include "../../lang/lua/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mthreads)
+.  include "../../mk/pthread.buildlink3.mk"
+PTHREAD_AUTO_VARS=      yes
+PTHREAD_OPTS+=          require
+CFLAGS+=		-DHAVE_PTHREAD
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+DEPENDS+=	xclip-[0-9]*:../../x11/xclip
+CFLAGS+=	-DDEFAULT_COPY_TO_CLIPBOARD_CMD=\""xclip -i -selection clipboard <"\"
+CFLAGS+=	-DDEFAULT_PASTE_FROM_CLIPBOARD_CMD=\""xclip -o -selection clipboard"\"
+.else
+CFLAGS+=	-DDEFAULT_COPY_TO_CLIPBOARD_CMD=\""tmux load-buffer"\"
+CFLAGS+=	-DDEFAULT_PASTE_FROM_CLIPBOARD_CMD=\""tmux show-buffer"\"
 .endif
