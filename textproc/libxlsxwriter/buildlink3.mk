4f4ad568e6dbcdb11a714bd208e62e93fe7d67e0
sjmulder <sjmulder@pkgsrc.org>
2025-02-13 10:45:34 +0000

    textproc/libxlsxwriter: Update to 1.2.0
    Changes:
     - Added format_set_font_family() and format_set_font_charset() format
       functions to support Middle Eastern and Asian fonts.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2025/01/31 23:52:52 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2025/02/13 10:45:34 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libxlsxwriter
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.7
-BUILDLINK_ABI_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.1.9
+BUILDLINK_ABI_DEPENDS.libxlsxwriter+=	libxlsxwriter>=1.2.0
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../archivers/minizip/buildlink3.mk"
