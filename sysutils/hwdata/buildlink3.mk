4534e16e9e94276838253638b0092ee6171a142d
vins <vins@pkgsrc.org>
2025-02-13 13:26:13 +0000

    sysutils/hwdata: fix usage of BUILDINK_FILES
    Bump revision.

--- sysutils/hwdata/buildlink3.mk
+++ sysutils/hwdata/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/01/29 17:25:01 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/02/13 13:26:13 vins Exp $
 
 BUILDLINK_TREE+=	hwdata
 
@@ -7,10 +7,10 @@ HWDATA_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hwdata?=	build
 
-BUILDLINK_API_DEPENDS.hwdata+=	hwdata>=0.390
-BUILDLINK_ABI_DEPENDS.hwdata+=	hwdata>=0.391
+BUILDLINK_API_DEPENDS.hwdata+=	hwdata>=0.391nb1
+BUILDLINK_ABI_DEPENDS.hwdata+=	hwdata>=0.391nb1
 BUILDLINK_PKGSRCDIR.hwdata?=	../../sysutils/hwdata
-BUILDLINK_FILES.hwdata+=	share/*
+BUILDLINK_FILES.hwdata+=	share/hwdata/*.ids
 .endif	# HWDATA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hwdata
