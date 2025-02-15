0969e2d6642263d77385cd2f8ab00c786022f099
he <he@pkgsrc.org>
2025-02-15 23:41:45 +0000

    graphics/librsvg: add patches so that this builds on aarch64_eb.
    This basically consists of adding conditional of target_endian = "little"
    in various places for conditional compilation.

---
+++ graphics/librsvg/patches/patch-.._vendor_memchr-2.7.4_src_vector.rs
@@ -0,0 +1,15 @@
+$NetBSD: patch-.._vendor_memchr-2.7.4_src_vector.rs,v 1.1 2025/02/15 23:41:46 he Exp $
+
+Don't try to do neon / SIMD on big-endian aarch64.
+
+--- ../vendor/memchr-2.7.4/src/vector.rs.orig	2025-02-15 20:56:52.793849071 +0000
++++ ../vendor/memchr-2.7.4/src/vector.rs
+@@ -289,7 +289,7 @@ mod x86avx2 {
+     }
+ }
+ 
+-#[cfg(target_arch = "aarch64")]
++#[cfg(all(target_arch = "aarch64", target_endian = "little"))]
+ mod aarch64neon {
+     use core::arch::aarch64::*;
+ 
