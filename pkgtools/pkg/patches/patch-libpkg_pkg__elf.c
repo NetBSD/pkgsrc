227280dfea7208ef8ef9dc7cc5b389a790285828
wiz <wiz@pkgsrc.org>
2025-02-15 15:49:45 +0000

    pkg: trust gelf.h to provide all the elf definitions we need

--- pkgtools/pkg/patches/patch-libpkg_pkg__elf.c
+++ pkgtools/pkg/patches/patch-libpkg_pkg__elf.c
@@ -1,15 +1,23 @@
-$NetBSD: patch-libpkg_pkg__elf.c,v 1.3 2025/02/10 13:13:27 wiz Exp $
+$NetBSD: patch-libpkg_pkg__elf.c,v 1.4 2025/02/15 15:49:45 wiz Exp $
 
 Avoid symbol conflicts on NetBSD due to libelf.h vs. exec_elf.h.
 
 --- libpkg/pkg_elf.c.orig	2018-01-06 22:53:49.000000000 +0000
 +++ libpkg/pkg_elf.c
-@@ -48,7 +48,7 @@
+@@ -48,16 +48,10 @@
  #include <fcntl.h>
  #include <gelf.h>
  #include <libgen.h>
 -#if defined(HAVE_LINK_H) && !defined(__DragonFly__) && defined(HAVE_LIBELF)
-+#if defined(HAVE_LINK_H) && !defined(__DragonFly__) && !defined(HAVE_LIBELF)
- #include <link.h>
- #endif
+-#include <link.h>
+-#endif
  #include <paths.h>
+ #include <stdbool.h>
+ #include <string.h>
+ #include <unistd.h>
+-#ifdef HAVE_LIBELF
+-#include <libelf.h>
+-#endif
+ 
+ #include <bsd_compat.h>
+ 
