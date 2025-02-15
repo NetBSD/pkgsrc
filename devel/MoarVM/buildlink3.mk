5b2c2fbb58000e1d35295f1cee21f4a4ec21451b
mef <mef@pkgsrc.org>
2025-02-15 11:39:20 +0000

    (devel/MoarVM) Updated 2024.12 to 2025.01
    (pkgsrc note)
     - timestamp inside $DISTFILES looks strange,
       but it is said known issue
       https://irclogs.raku.org/moarvm/2025-01-25.html#23:23
    
    (upstream)
    2025.01
    
    Core:
    
        Fix MVM_setjmp on 32bit Windows [caf97bf0]
        Micro-optimize P6opaque's get_attribute() (#1884) [df1715d6]
        Remove temporary define for migrating Rakudo extops (#1671) [7d15fcab]
        Fix OS-is-windows check that doesn't work in this file [4f183bc0]
        Use correct fallthrough annotation (#1885) [9a1aebb3]
        Port over "cache devshell dll location" from rakudo CI [fa5bfd92]
        Install gdb extension (#1848) [1c2ea87e]
    
    Documentation:
    
        beautify release_guide.md [8102c510]
    
    JIT:
    
        Don't try to enable the JIT unless it's possible (#1887) [dd98cb2e]
    
    Spesh:
    
        Guard against out-of-bounds jumps in spesh arg guard program. [52d3452e]
    
    CI:
    
        Maybe the order jobs are started is alphabetical? [777294c4]
        Fix format specifiers clang on OSX complains about [c1d1f60e]
        GH Actions: Remove the deprecated macos-12 runner [1f5bf409]
        Do not persist tokens in CI artifacts [afc8354b]
        Change comment style because gcc complains [6f3f53c4]

--- devel/MoarVM/buildlink3.mk
+++ devel/MoarVM/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2024/10/27 05:37:25 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2025/02/15 11:39:20 mef Exp $
 
 BUILDLINK_TREE+=	MoarVM
 
 .if !defined(MOARVM_BUILDLINK3_MK)
 MOARVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2024.10
-BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2024.10
+BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2025.01
+BUILDLINK_ABI_DEPENDS.MoarVM+=	MoarVM>=2025.01
 BUILDLINK_PKGSRCDIR.MoarVM?=	../../devel/MoarVM
 
 .include "../../devel/libatomic_ops/buildlink3.mk"
