dfac049a183ef2b50ade09e5373827cf5a917dc4
wiz <wiz@pkgsrc.org>
2025-02-15 23:06:05 +0000

    edt-o3one: fix help file handling
    From RVP in PR 59051.
    
    Bump PKGREVISION.

--- editors/edt-o3one/patches/patch-os.c
+++ editors/edt-o3one/patches/patch-os.c
@@ -1,8 +1,9 @@
-$NetBSD: patch-os.c,v 1.1 2025/02/11 08:01:29 wiz Exp $
+$NetBSD: patch-os.c,v 1.2 2025/02/15 23:06:05 wiz Exp $
 
 Add missing header for ioctl().
+Use path to help file.
 
---- os.c.orig	2016-12-23 00:35:48.000000000 +0000
+--- os.c.orig	2025-02-15 23:03:15.290131323 +0000
 +++ os.c
 @@ -25,6 +25,8 @@
  
@@ -13,3 +14,26 @@ Add missing header for ioctl().
  #define JOURNAL_FLUSH_INTERVAL (15)
  
  static int screenmode = 0;
+@@ -265,6 +267,14 @@ void os_initialization (void)
+     abort ();
+   }
+ 
++#ifdef EDT_HELPFILE
++  help_name = malloc(strlen(EDT_HELPFILE) + 1);
++  if (help_name == NULL) {
++    fprintf (stderr, "out of memory\n");
++    exit (1); 
++  }
++  strcpy(help_name, EDT_HELPFILE);
++#else
+   /* Try to find help file - same directory as executable */
+ 
+   pathstring = NULL;						/* haven't allocated a string yet */
+@@ -303,6 +313,7 @@ void os_initialization (void)
+   strcat (help_name, ".hlp");					/* append .hlp for the help file name */
+ 
+   if (pathstring != NULL) string_delete (pathstring);		/* free off string if we allocated one */
++#endif
+ 
+ #endif
+ 
