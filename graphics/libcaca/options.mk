b9d42dd1af5319bb2926c544d5972a5ecfd6c487
nia <nia@pkgsrc.org>
2025-02-17 22:00:48 +0000

    libcaca: Update to 0.99.20
    Special notes:
    - building without imlib2 is broken upstream, remove the option.
    - upstream switched to github.
    - verified to build on freebsd, netbsd, linux, macos by drecklypkg CI.
    
    Upstream changes:
    - IPv6 support in cacaserver
    - fixed a bug from 2004 that caused PDF documentation generation to
      fail
    - memory allocation functions are now more robust
    - numerous fixes for memory leaks and invalid memory accesses:
        CVE-2021-30498
        CVE-2021-30499
        CVE-2021-3410
        CVE-2018-20546
        CVE-2018-20547
        CVE-2018-20545
        CVE-2018-20548
        CVE-2018-20549

--- graphics/libcaca/options.mk
+++ graphics/libcaca/options.mk
@@ -1,19 +1,11 @@
-# $NetBSD: options.mk,v 1.7 2024/05/06 08:33:51 jperkin Exp $
+# $NetBSD: options.mk,v 1.8 2025/02/17 22:00:48 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libcaca
-PKG_SUPPORTED_OPTIONS=		imlib2
 PKG_OPTIONS_REQUIRED_GROUPS=	driver
 PKG_OPTIONS_GROUP.driver=	ncurses slang x11
 PKG_SUGGESTED_OPTIONS=		ncurses
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mimlib2)
-.  include "../../graphics/imlib2/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-imlib2
-.else
-CONFIGURE_ARGS+=	--disable-imlib2
-.endif
 
 .if !empty(PKG_OPTIONS:Mncurses)
 .  include "../../devel/ncurses/buildlink3.mk"
