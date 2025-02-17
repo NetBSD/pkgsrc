0d4d3c6916be23be7d12f9efd7a1822abbb61a74
taca <taca@pkgsrc.org>
2025-02-17 15:28:31 +0000

    lang/php83: update to 8.3.17
    PHP 8.3.17 (2025-02-13)
    
    - Core:
      . Fixed bug GH-16892 (ini_parse_quantity() fails to parse inputs starting
        with 0x0b). (nielsdos)
      . Fixed bug GH-16886 (ini_parse_quantity() fails to emit warning for 0x+0).
        (nielsdos)
      . Fixed bug GH-17214 (Relax final+private warning for trait methods with
        inherited final). (ilutov)
      . Fixed NULL arithmetic during system program execution on Windows. (cmb,
        nielsdos)
      . Fixed potential OOB when checking for trailing spaces on Windows. (cmb)
      . Fixed bug GH-17408 (Assertion failure Zend/zend_exceptions.c).
        (nielsdos, ilutov)
      . Fix may_have_extra_named_args flag for ZEND_AST_UNPACK. (nielsdos)
      . Fix NULL arithmetic in System V shared memory emulation for Windows. (cmb)
    
    - DOM:
      . Fixed bug GH-17500 (Segfault with requesting nodeName on nameless doctype).
        (nielsdos)
    
    - Enchant:
      . Fix crashes in enchant when passing null bytes. (nielsdos)
    
    - FTP:
      . Fixed bug GH-16800 (ftp functions can abort with EINTR). (nielsdos)
    
    - GD:
      . Fixed bug GH-17349 (Tiled truecolor filling looses single color
        transparency). (cmb)
      . Fixed bug GH-17373 (imagefttext() ignores clipping rect for palette
        images). (cmb)
      . Ported fix for libgd 223 (gdImageRotateGeneric() does not properly
        interpolate). (cmb)
    
    - Intl:
      . Fixed bug GH-11874 (intl causing segfault in docker images). (nielsdos)
      . Fixed bug GH-17469 (UConverter::transcode always emit E_WARNING on
        invalid encoding). (David Carlier)
    
    - Opcache:
      . Fixed bug GH-17307 (Internal closure causes JIT failure). (nielsdos)
      . Fixed bug GH-17564 (Potential UB when reading from / writing to struct
        padding). (ilutov)
    
    - PDO:
      . Fixed a memory leak when the GC is used to free a PDOStatment. (Girgias)
      . Fixed a crash in the PDO Firebird Statement destructor. (nielsdos)
      . Fixed UAFs when changing default fetch class ctor args. (Girgias, nielsdos)
    
    - Phar:
      . Fixed bug GH-17518 (offset overflow phar extractTo()). (nielsdos)
    
    - PHPDBG:
      . Fix crashes in function registration + test. (nielsdos, Girgias)
    
    - Session:
      . Fix type confusion with session SID constant. (nielsdos)
      . Fixed bug GH-17541 (ext/session NULL pointer dereferencement during
        ID reset). (Girgias)
    
    - SimpleXML:
      . Fixed bug GH-17409 (Assertion failure Zend/zend_hash.c:1730). (nielsdos)
    
    - SNMP:
      . Fixed bug GH-17330 (SNMP::setSecurity segfault on closed session).
        (David Carlier)
    
    - SPL:
      . Fixed bug GH-17463 (crash on SplTempFileObject::ftruncate with negative
        value). (David Carlier)
    
    - Zip:
      . Fixed bug GH-17139 (Fix zip_entry_name() crash on invalid entry).
        (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.453 2025/02/08 02:56:24 taca Exp $
+# $NetBSD: phpversion.mk,v 1.454 2025/02/17 15:28:31 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -112,7 +112,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.31
 PHP82_VERSION=	8.2.27
-PHP83_VERSION=	8.3.16
+PHP83_VERSION=	8.3.17
 
 _VARGROUPS+=	php
 _USER_VARS.php=	PHP_VERSION_DEFAULT
