2a83f6a5caef0d61a85f5815979fd5419ae62207
adam <adam@pkgsrc.org>
2025-02-18 09:03:18 +0000

    google-glog: updated to 0.7.1
    0.7.1
    
    This is a maintenance release that addresses several issues found in glog 0.7.0. In particular, we fixed Android detection and properly format unknown stack frames now.
    
    
    0.7.0
    
    In this release, glog underwent a major overhaul to take advantage of C++14 language and library features. Among the many changes, the public API has also been extended to use the more up-to-date standard library types (notably chrono).
    
    Highlights
    
    C++14 is now the primary language standard
    Emscripten support
    Many fixes and enhancements

--- devel/google-glog/patches/patch-src_glog_platform.h
+++ devel/google-glog/patches/patch-src_glog_platform.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_glog_platform.h,v 1.1 2023/04/12 10:39:21 jperkin Exp $
+$NetBSD: patch-src_glog_platform.h,v 1.2 2025/02/18 09:03:18 adam Exp $
 
 Support illumos.
 
---- src/glog/platform.h.orig	2022-04-04 22:03:27.000000000 +0000
+--- src/glog/platform.h.orig	2024-06-08 15:07:34.000000000 +0000
 +++ src/glog/platform.h
-@@ -50,6 +50,8 @@
- #define GLOG_OS_NETBSD
- #elif defined(__OpenBSD__)
- #define GLOG_OS_OPENBSD
+@@ -53,6 +53,8 @@
+ #  define GLOG_OS_OPENBSD
+ #elif defined(__EMSCRIPTEN__)
+ #  define GLOG_OS_EMSCRIPTEN
 +#elif defined(__illumos__)
-+#define GLOG_OS_ILLUMOS
++#  define GLOG_OS_ILLUMOS
  #else
  // TODO(hamaji): Add other platforms.
  #error Platform not supported by glog. Please consider to contribute platform information by submitting a pull request on Github.
