2468d0f306479055906f57d443175d48fd45a43e
adam <adam@pkgsrc.org>
2025-02-18 10:05:00 +0000

    librime: updated to 1.13.1
    librime 1.13.1
    
    Features
    
    simplifier: cache opencc
    punctuator: add option to commit digit separator
    
    
    librime 1.13.0
    
    Features
    
    punctuator: convert digit separators
    
    Bug Fixes
    
    navigator: wrong spans after selection
    api: export librime 0.9 API as extern C
    navigator: disconnect notifier
    rime_api_console: recreate session when necessary

--- inputmethod/librime/options.mk
+++ inputmethod/librime/options.mk
@@ -1,16 +1,17 @@
-# $NetBSD: options.mk,v 1.3 2024/08/25 06:18:55 wiz Exp $
-#
+# $NetBSD: options.mk,v 1.4 2025/02/18 10:05:00 adam Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.librime
-PKG_SUPPORTED_OPTIONS=		tests
+PKG_OPTIONS_VAR=	PKG_OPTIONS.librime
+PKG_SUPPORTED_OPTIONS=	tests
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mtests)
 .include "../../devel/googletest/buildlink3.mk"
 CMAKE_CONFIGURE_ARGS+=	-DBUILD_TEST=ON
-TEST_TARGET=	test
-TEST_ENV+=	LD_LIBRARY_PATH=${WRKSRC}/lib
+TEST_ENV+=		LD_LIBRARY_PATH=${WRKSRC}/lib
+# For Darwin
+TEST_ENV+=		DYLD_LIBRARY_PATH=${WRKSRC}/lib
+TEST_TARGET=		test
 .else
 CMAKE_CONFIGURE_ARGS+=	-DBUILD_TEST=OFF
 .endif
