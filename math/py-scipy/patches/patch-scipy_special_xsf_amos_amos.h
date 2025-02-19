f516bd81558256ec15fe7a28a4052f994f592d50
adam <adam@pkgsrc.org>
2025-02-19 09:23:15 +0000

    py-scipy: updated to 1.15.2
    Issues closed for 1.15.2
    
    BUG: _lib: crash in uarray on interpreter exit with a free-threaded...
    BUG: special: Unchecked calls of malloc() and calloc() in specfun.h
    BUG: Segmentation Fault when passing a special array to ``scipy.cluster.hierarch``...
    BUG: median_filter on 1D array crashes with memory corruption...
    BUG: special: Potential memory leak in the function ``besy()``...
    BUG: ndimage.median_filter: Heap corruption with scipy 1.15
    BUG: signal.medfilt and ndimage.median_filter not returning correct...
    BUG: special: Unchecked malloc in stirling2.h
    BUG: Matrix multiplication of a coo_matrix with an invalid type...
    BLD: libhighs.a static library is installed
    BUG: interpolate test_bary_rational.TestAAA.test_basic_functions...
    BUG: ``scipy.stats.zmap`` returns incorrect values for complex...
    BUG: spatial.transform: ``Rotation.from_matrix()`` uses inaccurate...
    BUG: io.loadmat thinks a file with a lot of zeros is valid
    BUG: Slicing sparse matrix with ``None`` gives result different...
    MAINT: scipy.stats: test_regressZEROX SIMD warning for certain...
    
    Pull requests for 1.15.2
    
    BUG: immortalize uarray global strings in order to prevent negative...
    BUG: special: Fix unchecked memory allocations in ``specfun.h``
    BUG: cluster: ``cophenet`` intercept invalid linkage matrix count
    REL, MAINT: prep for 1.15.2
    MAINT: stats.Mixture: make return type consistent when ``shape``...
    MAINT: stats.Mixture: fix inverse functions when mean is undefined
    BUG: special: Fix unchecked malloc in stirling2.h
    TST: turn off dtype check due to endianness
    BUG: sparse: fix selecting wrong dtype for coo coords
    TST: interpolate: small tolerance bump to TestAAA.test_basic_functions
    MAINT: Stop installing ``libhighs``
    BUG: sparse.linalg.norm: add test for and fix return type
    BUG: sparse: revert NotImplemented return values in dot and matmul
    DOC: sparse.linalg: add two recent functions to namespace and...
    BUG: wrap median_filter stability
    MAINT: stats.zmap: restore support for complex data
    MAINT: integrate.cumulative_simpson: bump test tolerance
    BUG: scipy.spatial: Fix inaccurate orthonormalization in ``Rotation.from_matrix(``...
    BUG: special: Fix a memory leak in the AMOS function besy().
    CI: migrate Linux aarch64 jobs to GitHub Actions, add cp313t...
    BUG: io.loadmat: throw error on file containing all zeros
    BUG: sparse: fix spmatrix indexing with None and implicit padding...
    MAINT: stats: pearsonr SIMD-related shim

--- math/py-scipy/patches/patch-scipy_special_xsf_amos_amos.h
+++ math/py-scipy/patches/patch-scipy_special_xsf_amos_amos.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-scipy_special_xsf_amos_amos.h,v 1.1 2025/01/05 08:58:47 adam Exp $
+$NetBSD: patch-scipy_special_xsf_amos_amos.h,v 1.2 2025/02/19 09:23:15 adam Exp $
 
 Prefer C++ headers.
 Fix error: 'isinf' was not declared in this scope; did you mean 'std::isinf'?
 
---- scipy/special/xsf/amos/amos.h.orig	2025-01-02 22:53:29.000000000 +0000
+--- scipy/special/xsf/amos/amos.h.orig	2025-02-16 22:01:46.000000000 +0000
 +++ scipy/special/xsf/amos/amos.h
-@@ -94,8 +94,11 @@
+@@ -94,10 +94,13 @@
  
  #include <stdlib.h>
  
@@ -13,9 +13,11 @@ Fix error: 'isinf' was not declared in this scope; did you mean 'std::isinf'?
 -#include <complex.h>
 +#include <cmath>
 +#include <complex>
-+
+ #include <memory>     // unique_ptr
+ 
 +using std::isinf;
 +using std::isnan;
- 
++
  namespace xsf {
  namespace amos {
+ 
