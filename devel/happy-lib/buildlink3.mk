053f23aa78660f8b7c60bc00585b712d1b375c4e
wiz <wiz@pkgsrc.org>
2025-02-19 13:26:19 +0000

    devel/happy-lib: update to happy-lib-2.1.5
    No release notes have been provided by the upstream.

--- devel/happy-lib/buildlink3.mk
+++ devel/happy-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/02/02 13:04:54 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/02/19 13:26:19 wiz Exp $
 
 BUILDLINK_TREE+=	happy-lib
 
 .if !defined(HAPPY_LIB_BUILDLINK3_MK)
 HAPPY_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.4
-BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.4nb1
+BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.5
+BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.5
 BUILDLINK_PKGSRCDIR.happy-lib?=		../../devel/happy-lib
 .endif	# HAPPY_LIB_BUILDLINK3_MK
 
