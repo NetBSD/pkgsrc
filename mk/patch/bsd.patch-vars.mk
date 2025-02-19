7c776c053595c024d0d8997a9433d18b6cb5f3ca
wiz <wiz@pkgsrc.org>
2025-02-19 09:44:57 +0000

    Revert previous
    mk: Optimise patch checks.
    
    on request by jperkin because it might cause problems in CVS checkouts.

--- mk/patch/bsd.patch-vars.mk
+++ mk/patch/bsd.patch-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.patch-vars.mk,v 1.9 2025/02/18 15:26:11 wiz Exp $
+# $NetBSD: bsd.patch-vars.mk,v 1.10 2025/02/19 09:44:57 wiz Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
@@ -27,11 +27,12 @@
 
 PATCHFILES?=	# none
 
+pkgsrc_patches=	${:!echo ${PATCHDIR}/patch-* ${PATCHDIR}/emul-*-patch-*!:N*\*}
 .if !empty(LOCALPATCHES)
 local_patches=	${:!echo ${LOCALPATCHES}/${PKGPATH}/*!:N*/CVS:N*/\*}
 .endif
 
-.if !empty(PATCHFILES) || exists(${PATCHDIR}) || !empty(local_patches)
+.if !empty(PATCHFILES) || !empty(pkgsrc_patches) || !empty(local_patches)
 USE_TOOLS+=	patch
 .endif
 .if (!empty(PATCHFILES) || !empty(pkgsrc_patches)) \
