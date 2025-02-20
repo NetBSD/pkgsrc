39f85449683094ffe7707a4bfc19ce1752a50f19
manu <manu@pkgsrc.org>
2025-02-20 15:28:28 +0000

    Do not mess with API/ABI version, as suggested by wiz@

--- devel/gnustep-base/buildlink3.mk
+++ devel/gnustep-base/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.46 2025/02/20 14:28:46 manu Exp $
+# $NetBSD: buildlink3.mk,v 1.47 2025/02/20 15:29:12 manu Exp $
 
 BUILDLINK_TREE+=	gnustep-base
 
 .if !defined(GNUSTEP_BASE_BUILDLINK3_MK)
 GNUSTEP_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-base+=	gnustep-base>=1.31.0
-BUILDLINK_ABI_DEPENDS.gnustep-base+=	gnustep-base>=1.31.0
+BUILDLINK_API_DEPENDS.gnustep-base+=	gnustep-base>=1.24.0
+BUILDLINK_ABI_DEPENDS.gnustep-base+=	gnustep-base>=1.29.0nb9
 BUILDLINK_PKGSRCDIR.gnustep-base?=	../../devel/gnustep-base
 
 .include "../../devel/gnustep-make/buildlink3.mk"
