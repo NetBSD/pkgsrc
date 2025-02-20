0b58a0c929135eeb33aba58d4d5ca0e3d6e4e8d0
manu <manu@pkgsrc.org>
2025-02-20 14:34:52 +0000

    Update www/gnustep-make to 2.9.3
    Change in version 2.9.3
     * Added support for ASAN/LSAN builds
     * Various minor bugfixes.
    
    Changes in version 2.9.2
     * Update for newer versions of Java
     * Various minor bugfixes.
    
    Changes in version 2.9.0
     * Better check for objc runtime on Windows.
    
     * Split linker flags to better support partial linking: 'ALL_LDFLAGS'
       is now a combination of 'FINAL_LDFLAGS' and 'ALL_LDFLAGS'.
    
     * Better support for newer gcc versions.
    
     * Add support for storyboard files.
    
     * Increase autoconf version to 2.65 and make autoconf handle
       Objective-C++ and OBJCXX variables directly.
    
     * Fix bug that prevented ARC from getting used.
    
     * Link subproject object files directly instead of first merging them
       into 'subproject.o'.
    
     * Support building on Windows with Clang MSVC target.
    
     * Improve mingw64 support: for instance, adopt the triplet used by
       the mingw-w64 project rather than using the one returned by
       autoconf.  Fixes building Gorm.

--- devel/gnustep-make/buildlink3.mk
+++ devel/gnustep-make/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/10/22 02:06:19 rh Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2025/02/20 14:34:52 manu Exp $
 
 BUILDLINK_TREE+=	gnustep-make
 
 .if !defined(GNUSTEP_MAKE_BUILDLINK3_MK)
 GNUSTEP_MAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-make+=	gnustep-make>=2.6.2
-BUILDLINK_ABI_DEPENDS.gnustep-make+=	gnustep-make>=2.6.2
+BUILDLINK_API_DEPENDS.gnustep-make+=	gnustep-make>=2.9.3
+BUILDLINK_ABI_DEPENDS.gnustep-make+=	gnustep-make>=2.9.3
 BUILDLINK_PKGSRCDIR.gnustep-make?=	../../devel/gnustep-make
 
 .include "../../devel/gnustep-make/gnustep.mk"
