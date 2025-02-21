5fd1817ecef662ca76cfc7d502681c75e2eac5da
schmonz <schmonz@pkgsrc.org>
2025-02-21 17:49:42 +0000

    mpop: update to 1.4.21. Changes:
    - Build with libgsasl by default again (if found).
    - Add IDN support to --configure (build with libidn2 to enable this).
    - Add support for SCRAM-SHA-256-PLUS and SCRAM-SHA-1-PLUS
      authentication, and prefer SCRAM methods over the PLAIN method because
      of their superior properties.
    
    Packaging adjusted to more closely resemble msmtp. Build-tested on
    NetBSD and macOS.

--- mail/mpop/options.mk
+++ mail/mpop/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2018/11/20 09:29:24 maya Exp $
-#
+# $NetBSD: options.mk,v 1.4 2025/02/21 17:49:42 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mpop
-PKG_SUPPORTED_OPTIONS=		gsasl idn
+PKG_SUPPORTED_OPTIONS=		gsasl idn scripts secret
 PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
 PKG_OPTIONS_GROUP.ssl=		gnutls ssl
 PKG_SUGGESTED_OPTIONS=		ssl
@@ -20,14 +19,37 @@ CONFIGURE_ARGS+=	--disable-tls
 .endif
 
 .if !empty(PKG_OPTIONS:Mgsasl)
-CONFIGURE_ARGS+=	--enable-gsasl
+CONFIGURE_ARGS+=	--with-libgsasl
+BUILDLINK_API_DEPENDS.gsasl+=	gsasl>=2.1
 .  include "../../security/gsasl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-libgsasl
 .endif
 
 .if !empty(PKG_OPTIONS:Midn)
-.  include "../../devel/libidn/buildlink3.mk"
+.  include "../../devel/libidn2/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-libidn
-CONFIGURE_ARGS+=	--with-libidn-prefix=${BUILDLINK_PREFIX.libidn}
 .else
 CONFIGURE_ARGS+=	--without-libidn
+.endif
+
+.if !empty(PKG_OPTIONS:Msecret)
+.  include "../../security/libsecret/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libsecret
+.else
+CONFIGURE_ARGS+=	--without-libsecret
+.endif
+
+.PHONY: install-mpop-scripts
+PLIST_VARS+=		scripts
+.if !empty(PKG_OPTIONS:Mscripts)
+PLIST.scripts=		yes
+USE_TOOLS+=		pax
+INSTALLATION_DIRS+=	share/mpop
+install-mpop-scripts:
+	cd ${WRKSRC}/scripts && \
+		pax -rw vim \
+			${DESTDIR}${PREFIX}/share/mpop
+.else
+install-mpop-scripts:
 .endif
