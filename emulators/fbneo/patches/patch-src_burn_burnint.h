7da059a6346dc5920169bee6cd986e689dce6055
rillig <rillig@pkgsrc.org>
2025-02-22 19:37:36 +0000

    emulators/fbneo: fix build on NetBSD-*-aarch64
    Patch provided by adr in
    https://mail-index.netbsd.org/pkgsrc-users/2025/02/11/msg041061.html

--- emulators/fbneo/patches/patch-src_burn_burnint.h
+++ emulators/fbneo/patches/patch-src_burn_burnint.h
@@ -1,9 +1,20 @@
-$NetBSD: patch-src_burn_burnint.h,v 1.1 2023/05/29 19:30:48 nia Exp $
+$NetBSD: patch-src_burn_burnint.h,v 1.2 2025/02/22 19:37:36 rillig Exp $
 
 Teach fbneo about endian on NetBSD.
 
 --- src/burn/burnint.h.orig	2021-05-01 20:32:23.000000000 +0000
 +++ src/burn/burnint.h
+@@ -30,8 +30,8 @@ typedef union
+ #define BURN_ENDIAN_SWAP_INT64(x)				x
+ #else
+ 
+-// Xbox 360 has byteswap 64-bit intrinsic
+-#ifndef _XBOX
++// Xbox 360 and NetBSD have byteswap 64-bit intrinsic
++#if !definded(_XBOX) && !defined(__NetBSD__)
+ #define NO_64BIT_BYTESWAP
+ #endif
+ 
 @@ -80,6 +80,13 @@ typedef union {
  #define BURN_ENDIAN_SWAP_INT8(x)				(x^1)
  #define BURN_ENDIAN_SWAP_INT16(x)				({uint16_t tt; __sthbrx(&tt, 0, x); tt;})
