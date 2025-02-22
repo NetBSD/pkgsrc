8038c407003b6b72b986971fb607cea80e804439
taca <taca@pkgsrc.org>
2025-02-22 15:27:55 +0000

    lang/php84: add package version 8.4.4
    PHP is a widely-used open source general-purpose scripting language
    that is especially suited for web development and can be embedded
    into HTML.  It is modular, and object-oriented.  Much of its syntax
    is borrowed from C, Java and Perl with a couple of unique PHP-specific
    features thrown in.  The language is designed to allow web developers
    to write dynamically generated pages quickly.
    
    PHP 8.4 comes with numerous improvements and new features such as
    
    * Property Hooks
    * Asymmetric Property Visibility
    * Lazy Objects
    * #[\Deprecated] attribute
    * Parsing RFC1867 (multipart) requests in non-POST HTTP requests
    * Chaining new expressions without parentheses
    * Improved Debugging Info for WeakReference
    * Improved Debugging Info for Closure
    * Defining Identical Symbols in Different Namespace Blocks
    * And much much more...

---
+++ lang/php84/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/02/22 15:27:55 taca Exp $
+
+BUILDLINK_TREE+=	php84
+
+.if !defined(PHP84_BUILDLINK3_MK)
+PHP84_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.php84+=	php${PHP_VER}>=8.4.0<8.5
+BUILDLINK_ABI_DEPENDS.php84+=	php${PHP_VER}>=8.4.3
+BUILDLINK_PKGSRCDIR.php84?=	../../lang/php84
+BUILDLINK_INCDIRS.php84+=	${PHP_INCDIR}
+
+pkgbase := php84
+.include "../../mk/pkg-build-options.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif # PHP84_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-php84
