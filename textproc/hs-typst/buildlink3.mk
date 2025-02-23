a2c898c3964ee84869cda868ebc4276901cb2812
wiz <wiz@pkgsrc.org>
2025-02-23 22:03:42 +0000

    textproc/hs-typst: update to typst-0.6.2
    ## 0.6.2
    
      * Allow types to act as constructor functions, as in typst (#61).
        Add new unexported module, Typst.Constructors, defining the
        constructors for each of the typst types.
        Fix name of string type: it is `str`, not `string` (which is only
        the `repr`).
    
      * Support `dict`, `datetime`, `symbol` constructors.
    
      * Improve path handling when loading files (#60).
        We now look in the "local path" (the path of the containing file)
        except when the path begins with `/` (in which case it is resolved
        relative to the package path).
    
      * Fix issue with expression parsing involving labels (#59).
    
      * Remove spurious trace in `getPath`.

--- textproc/hs-typst/buildlink3.mk
+++ textproc/hs-typst/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2025/02/02 13:05:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/02/23 22:03:42 wiz Exp $
 
 BUILDLINK_TREE+=	hs-typst
 
 .if !defined(HS_TYPST_BUILDLINK3_MK)
 HS_TYPST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typst+=	hs-typst>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-typst+=	hs-typst>=0.6.1nb1
+BUILDLINK_API_DEPENDS.hs-typst+=	hs-typst>=0.6.2
+BUILDLINK_ABI_DEPENDS.hs-typst+=	hs-typst>=0.6.2
 BUILDLINK_PKGSRCDIR.hs-typst?=		../../textproc/hs-typst
 
 .include "../../textproc/hs-typst-symbols/buildlink3.mk"
