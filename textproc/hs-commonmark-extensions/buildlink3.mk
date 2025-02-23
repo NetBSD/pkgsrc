a241c934f609b9d3a50d2f6567dc2f5df34b470f
wiz <wiz@pkgsrc.org>
2025-02-23 21:58:49 +0000

    textproc/hs-commonmark-extensions: update to commonmark-extensions-0.2.6
    ## 0.2.6
    
      * Track wikilinks with a class instead of the title (Evan
        Silberman). The use of title="wikilink" in HTML output traces
        back to Pandoc's hijacking of the title attribute for this purpose
        back when Pandoc links didn't have Attrs. A coordinated change in
        Pandoc moves this more appropriately into a class.

--- textproc/hs-commonmark-extensions/buildlink3.mk
+++ textproc/hs-commonmark-extensions/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2025/02/02 13:05:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2025/02/23 21:58:49 wiz Exp $
 
 BUILDLINK_TREE+=	hs-commonmark-extensions
 
 .if !defined(HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK)
 HS_COMMONMARK_EXTENSIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.5
-BUILDLINK_ABI_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.5.6nb1
+BUILDLINK_API_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.6
+BUILDLINK_ABI_DEPENDS.hs-commonmark-extensions+=	hs-commonmark-extensions>=0.2.6
 BUILDLINK_PKGSRCDIR.hs-commonmark-extensions?=		../../textproc/hs-commonmark-extensions
 
 .include "../../textproc/hs-commonmark/buildlink3.mk"
