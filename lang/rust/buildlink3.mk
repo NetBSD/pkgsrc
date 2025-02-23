73a6abd77684cc5df33998358ddd3f15619415ff
wiz <wiz@pkgsrc.org>
2025-02-23 09:11:30 +0000

    rust: fix path to itself in bl3.mk

--- lang/rust/buildlink3.mk
+++ lang/rust/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2025/02/23 08:53:54 he Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/02/23 09:11:30 wiz Exp $
 
 BUILDLINK_TREE+=	rust
 
@@ -10,7 +10,7 @@ BUILDLINK_TREE+=	rust
 RUST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rust+=	rust>=1.56.1
-BUILDLINK_PKGSRCDIR.rust?=	../../wip/rust
+BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
 .endif
 
