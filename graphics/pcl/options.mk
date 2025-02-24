a637afc3883fc47d8b06bd75648f3c8478e2afc3
adam <adam@pkgsrc.org>
2025-02-24 17:22:56 +0000

    pcl: fix openmp support with clang; fix PLIST for Darwin

--- graphics/pcl/options.mk
+++ graphics/pcl/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2024/08/25 06:18:53 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2025/02/24 17:22:56 adam Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pcl
 
 # Qt could be an option, but relies on vtk's qt support, too.
@@ -9,20 +9,26 @@ PKG_SUGGESTED_OPTIONS=	vtk
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	vtk qt
-.if !empty(PKG_OPTIONS:Mvtk)
+PLIST_VARS+=	qt vtk x11
 
+.if !empty(PKG_OPTIONS:Mvtk)
 CMAKE_CONFIGURE_ARGS+=	-DWITH_VTK=TRUE
 PLIST.vtk=	yes
 .include "../../graphics/glew/buildlink3.mk"
 .include "../../graphics/vtk/buildlink3.mk"
-.  if ${PKG_BUILD_OPTIONS.vtk:Mqt}
+.  if ${PKG_BUILD_OPTIONS.vtk:Mqt5}
 CMAKE_CONFIGURE_ARGS+=	-DWITH_QT=QT5
 CMAKE_CONFIGURE_ARGS+=	-DBUILD_apps=ON
 PLIST.qt=	yes
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.  elif ${PKG_BUILD_OPTIONS.vtk:Mqt6}
+CMAKE_CONFIGURE_ARGS+=	-DWITH_QT=QT6
+CMAKE_CONFIGURE_ARGS+=	-DBUILD_apps=ON
+PLIST.qt=	yes
+.  endif
+.  if ${OPSYS} != "Darwin"
+PLIST.x11=	yes
 .  endif
 .else
+CMAKE_CONFIGURE_ARGS+=	-DWITH_QT=NO
 CMAKE_CONFIGURE_ARGS+=	-DWITH_VTK=FALSE
-CMAKE_CONFIGURE_ARGS+=    -DWITH_QT=NO
 .endif
