59e3fbc8be66d98e7a85652a8e94ddb2d8a07b0a
adam <adam@pkgsrc.org>
2025-02-24 17:22:44 +0000

    vtk: updated to 9.4.1
    VTK v9.4.0
    https://www.kitware.com/vtk-v9-4-0/

--- graphics/vtk/options.mk
+++ graphics/vtk/options.mk
@@ -1,20 +1,29 @@
-# $NetBSD: options.mk,v 1.3 2024/08/25 06:18:53 wiz Exp $
-PKG_OPTIONS_VAR=	PKG_OPTIONS.vtk
+# $NetBSD: options.mk,v 1.4 2025/02/24 17:22:44 adam Exp $
 
-PKG_SUPPORTED_OPTIONS=	qt
-PKG_SUGGESTED_OPTIONS=	qt
+PKG_OPTIONS_VAR=		PKG_OPTIONS.vtk
+PKG_OPTIONS_OPTIONAL_GROUPS=	gui
+PKG_OPTIONS_GROUP.gui=		qt5 qt6
+PKG_SUGGESTED_OPTIONS=		qt6
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	qt
-.if !empty(PKG_OPTIONS:Mqt)
+.if !empty(PKG_OPTIONS:Mqt5) || !empty(PKG_OPTIONS:Mqt6)
+PLIST.qt=	yes
+CMAKE_CONFIGURE_ARGS+=	-DVTK_GROUP_ENABLE_Qt=YES
 CMAKE_CONFIGURE_ARGS+=	-DVTK_MODULE_ENABLE_VTK_GUISupportQt=YES
 CMAKE_CONFIGURE_ARGS+=	-DVTK_MODULE_ENABLE_VTK_GUISupportQtSQL=NO
 CMAKE_CONFIGURE_ARGS+=	-DVTK_MODULE_ENABLE_VTK_RenderingQt=YES
 CMAKE_CONFIGURE_ARGS+=	-DVTK_MODULE_ENABLE_VTK_ViewsQt=YES
+.  if !empty(PKG_OPTIONS:Mqt5)
 CMAKE_CONFIGURE_ARGS+=	-DVTK_QT_VERSION=5
-PLIST.qt=	yes
 .include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.  elif !empty(PKG_OPTIONS:Mqt6)
+CMAKE_CONFIGURE_ARGS+=	-DVTK_QT_VERSION=6
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.  endif
 .else
 CMAKE_CONFIGURE_ARGS+=	-DVTK_GROUP_ENABLE_Qt=NO
 .endif
