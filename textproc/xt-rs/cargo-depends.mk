356872d99941a8eedb52e5ae89a3869da89bbd1d
pin <pin@pkgsrc.org>
2025-02-24 08:39:47 +0000

    textproc/xt-rs: update to 0.19.4
    Fixed
    
        - Crashes on certain invalid YAML inputs. Previous versions of xt may have aborted with a Rust panic message
        when handling a YAML document consisting solely of an alias to an undefined anchor (e.g. *anchor).
        This bug was discovered through automated fuzzing; with the fix applied, xt now survives far longer fuzzing
        runs without further crashes.

--- textproc/xt-rs/cargo-depends.mk
+++ textproc/xt-rs/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.6 2025/02/22 14:43:29 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.7 2025/02/24 08:39:47 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.2
 CARGO_CRATE_DEPENDS+=	anes-0.1.6
