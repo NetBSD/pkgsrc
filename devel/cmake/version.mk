d137ae6a1115c89e3ad6cc43ff19683129b18dbd
adam <adam@pkgsrc.org>
2025-02-25 16:12:56 +0000

    cmake cmake-gui: updated to 3.31.6
    CMake 3.31.6
    * Help: Clarify when execute_process ENCODING UTF-8 became the default
    * Linker: Restore support for WHOLE_ARCHIVE with Swift libraries
    * Linker configuration: Add missing platform Haiku
    * ci: Update Windows builds to MSVC 14.43 toolset
    * cmGeneratorExpressionDAGChecker: Construct with initializer list syntax
    * cmGeneratorExpressionDAGChecker: De-duplicate constructor signature
    * cmGeneratorExpressionDAGChecker: Construct with initializer list syntax
    * Tests: Split CustomTransitiveProperties argument checks into helper
    * Tests: Extend CustomTransitiveProperties with non-transitive LINK_LIBRARIES
    * Tests: Extend CustomTransitiveProperties with custom transitive LINK_LIBRARIES
    * GenEx: Restore evaluation context for conditional transitive properties
    * GenEx: Fix evaluation of LINK_LIBRARIES as custom transitive property
    * GoogleTest: Restore support for spaces in explicit WORKING_DIRECTORY
    * TIClang: Make ARM architecture id uppercase

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.74 2025/01/29 14:31:30 adam Exp $
+# $NetBSD: version.mk,v 1.75 2025/02/25 16:12:56 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.5
+CMAKE_VERSION=	3.31.6
 CMAKE_API=	${CMAKE_VERSION:R}
