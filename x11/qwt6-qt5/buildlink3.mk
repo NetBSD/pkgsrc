f3832ad6c611c21e3d024fed5b9340ab803b304d
adam <adam@pkgsrc.org>
2025-02-26 21:29:54 +0000

    qwt6-qt5: updated to 6.3
    Qwt 6.3
    
    QwtPainter
    
    QwtPainter::polylineSplitting has been reenabled. Unfortunately the Qt raster paint engine still has serious performance issues with rendering long polylines.
    
    QwtPlotCurve
    
    - QwtPlotCurve::adjacentPoint
      for monotonic curves: using a binary search algo
    
    - QwtPlotCurve::interpolatedValueAt
      for monotonic curves: using a binary search algo
    QwtSeriesData
    
    - wrong inheritances fixed
    - firstSample, lastSample added
    QwtPlotIntervalCurve
    
    QwtPlotIntervalCurve::TubeAsLines mode added. A performance optimization
    for rendering huge datasets.
    Bugfixes/Adjustments
    
    Bugfixes/Adjustments - often needed because of new Qt developments

--- x11/qwt6-qt5/buildlink3.mk
+++ x11/qwt6-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2024/11/14 22:22:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2025/02/26 21:29:54 adam Exp $
 
 BUILDLINK_TREE+=	qwt6-qt5
 
@@ -9,11 +9,11 @@ BUILDLINK_API_DEPENDS.qwt6-qt5+=	qwt6-qt5>=6.1.5
 BUILDLINK_ABI_DEPENDS.qwt6-qt5+=	qwt6-qt5>=6.2.0nb4
 BUILDLINK_PKGSRCDIR.qwt6-qt5?=		../../x11/qwt6-qt5
 
-QWT_DIR=	${BUILDLINK_PREFIX.qwt6-qt5}/qwt-6.2.0
+QWT_DIR=	${BUILDLINK_PREFIX.qwt6-qt5}/qwt-6.3.0
 
-BUILDLINK_INCDIRS.qwt6-qt5+=	qwt-6.2.0/include
-BUILDLINK_LIBDIRS.qwt6-qt5+=	qwt-6.2.0/lib
-BUILDLINK_RPATHDIRS.qwt6-qt5+=	qwt-6.2.0/lib
+BUILDLINK_INCDIRS.qwt6-qt5+=	qwt-6.3.0/include
+BUILDLINK_LIBDIRS.qwt6-qt5+=	qwt-6.3.0/lib
+BUILDLINK_RPATHDIRS.qwt6-qt5+=	qwt-6.3.0/lib
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .include "../../x11/qt5-qtsvg/buildlink3.mk"
