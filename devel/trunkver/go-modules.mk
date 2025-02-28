de031fba2d0516dccdf1910d314be1e18bc231d1
schmonz <schmonz@pkgsrc.org>
2025-02-28 14:05:10 +0000

    Add trunkver: Versioning scheme for continuously delivered software
    TrunkVer is a versioning scheme for continuously-delivered, trunk-based
    applications and systems that don't follow a release scheme.
    
    It is a syntax-compatible drop-in replacement for SemVer and replaces
    the version with meaningful meta data, telling you at a glance what the
    artifact is, when it was built and where you may find the build log.

---
+++ devel/trunkver/go-modules.mk
@@ -0,0 +1,21 @@
+# $NetBSD: go-modules.mk,v 1.1 2025/02/28 14:05:11 schmonz Exp $
+
+GO_MODULE_FILES+=	github.com/!masterminds/semver/v3/@v/v3.3.1.mod
+GO_MODULE_FILES+=	github.com/!masterminds/semver/v3/@v/v3.3.1.zip
+GO_MODULE_FILES+=	github.com/cpuguy83/go-md2man/v2/@v/v2.0.4.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.zip
+GO_MODULE_FILES+=	github.com/inconshreveable/mousetrap/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/inconshreveable/mousetrap/@v/v1.1.0.zip
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/russross/blackfriday/v2/@v/v2.1.0.mod
+GO_MODULE_FILES+=	github.com/spf13/cobra/@v/v1.8.1.mod
+GO_MODULE_FILES+=	github.com/spf13/cobra/@v/v1.8.1.zip
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.mod
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.10.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.10.0.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.1.mod
+GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.1.zip
