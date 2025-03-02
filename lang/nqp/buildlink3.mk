bbed438afd76eb892560b126437a348aaae4b284
mef <mef@pkgsrc.org>
2025-03-02 02:15:46 +0000

    (lang/nqp) Updated 2025.01 to 2025.02, explicit ChangeLog unknown,
    (pkgsrc)
     - configure style with previous version ask for deleted files,
       as interim work around, old two files are now at files/ directory
       and copied before configure stage
    
    (upstream)
     -  diff from previous versinon, but just file names
    
    mef20@makoto 11:04:24/250302(/tmp/xx)% diff -urN nqp-2025.01 nqp-2025.02 |&  grep ^\++
    (deleted files)
    +++ nqp-2025.02/3rdparty/nqp-configure/.git     1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/LICENSE  1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/README.md        1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/bin/make.nqp     1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/doc/Macros.md    1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/doc/NQP-Config.md        1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/lib/NQP/Config/Test.pm   1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/lib/NQP/Config.pm        1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/lib/NQP/Macros.pm        1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/t/10-config.t    1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/t/20-macros.t    1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/3rdparty/nqp-configure/t/30-if-macro.t  1970-01-01 09:00:00.000000000 +0900
    +++ nqp-2025.02/MANIFEST        1970-01-01 09:00:00.000000000 +0900
    
    (changed files)
    +++ nqp-2025.02/VERSION 2025-02-23 05:06:01.000000000 +0900
    +++ nqp-2025.02/azure-pipelines.yml     2025-02-23 05:06:01.000000000 +0900
    +++ nqp-2025.02/src/QAST/Block.nqp      2025-02-23 05:06:01.000000000 +0900
    +++ nqp-2025.02/src/QAST/Node.nqp       2025-02-23 05:06:01.000000000 +0900
    +++ nqp-2025.02/src/vm/moar/QAST/QASTOperationsMAST.nqp 2025-02-23 05:06:01.000000000 +0900
    +++ nqp-2025.02/tools/templates/MOAR_REVISION   2025-02-23 05:06:01.000000000 +0900

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.24 2025/02/15 11:46:55 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2025/03/02 02:15:46 mef Exp $
 
 BUILDLINK_TREE+=	nqp
 
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2025.01
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2025.01
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2025.02
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2025.02
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
 .include "../../devel/MoarVM/buildlink3.mk"
