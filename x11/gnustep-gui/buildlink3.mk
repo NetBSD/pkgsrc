91c9968f75ec8566abf1efa8dee485725e7c0ae9
manu <manu@pkgsrc.org>
2025-03-03 01:26:20 +0000

    Updated x11/gnustep-gui to 0.32.0
    Changes since 0.31.1
    This version adds binding support for NSBrowser, NSOutlineView and
    NSTableView. Plus the usual bunch of bug fixes.
    
    - Add support for bindings in NSBrowser.
    - Return top level object when loading .gorm files.
    - Add bindings support to NSOutlineView,
    - Add support for 10.6 item-based delegate methods to NSBrowser.
    - Add NSMenuToolbarItem.
    - Add view based rendering to NSTableView and NSOutlineView.
    - Add code to NSTreeController.
    - Fix display of icons if the home is a symlink.
    - Make some ivars in NSView protected instead of package scoped.
    - Fix CI pipeline.
    - Revert _updateFieldEditor: on NSTextFieldCell.
    - GSTheme bug fix by Tom Sheffler tom.sheffler@gmail.com.
    - Fix memory leaks in test code.
    - Don't load nil filename in NSImageRep.
    - Prevent flickering in NSClipView.
    - Simplify ICU configuration.
    - Move GSColorSliderCell into gui itself.
    - Bugfix for NSMenuView.
    - Fix single colum handling in NSCollectionView.
    - Additional GSTheme method(s) for NSTabView.
    - Modify NSComboBox to handle attributed strings.
    - NSTextFieldCell: Decode placeholder string.
    - Make white default selection highlight color for NSTableView.
    - Implement showsresizeindicator.
    - NSApplication: toggle menu item "Hide" title and action on user click.
    - Add NSCellHitResult typedef, introduced in OSX 10.10.
    - Make menu visiblity themable.
    - Fix decoding alternateContents in NSButtonCell.
    - Font Panel autosizing fix.
    - Implement integerValue method in NSSliderCell.
    - NSBox: use macro to decode NSInteger into enum.
    - Fall back to pkgconfig for resolving libtiff.
    - GSMemoryPanel: Only call GSDebug* in debug mode.
    - Add APPKIT_EXPORT_CLASS, APPKIT_EXPORT annotations for some GS* classes.
    - Push modal window to top later in process.
    
    Changes since 0.31.0
    
    This is a bugfix release
    
    - Fix bug decoding menu items (breaking archive)
    - Remove use of deprecated lock from base library
    
    Changes since 0.30.0, previos version in pkgsrc
    
    This version adds view based cell support for NSTableView and
    NSOutlineView. Plus the usual bunch of bug fixes.
    
    - Add TGA detection for ImageMagick extension.
    - Correct endianess swapping for saving 16 and 32 bit TIFF images.
    - NSParagraphStyle restore old behaviour to have default tab stops.
    - Documentation updates.
    - A fix for autogsdoc documentation creation.
    - Improve theming for many classes.
    - Correct keyEquivalentModifierMask decoding in XIB files.
    - Add imageViewWithImage: to NSImageView.
    - Add implementation of NSUserInterfaceItemIdentifier to NSView.
    - Fix NSImageView intercepting mouse events when not editable
    - Move NSBox method isOpaque to GSTheme.
    - Many decoding improvements.
    - Fix compiler warnings.
    - Generate and install a gnustep-gui.pc file.
    - Add support for NSFilenamenPboardType in NSTextView.
    - Add support for NSPasteboardTypePNG in NSBitmapImageRep if the
      libpng is present.
    - Add support for ImageMagick >= 7.0
    - Increase pasteboard timeout to 30 seconds.
    - Add NSAppearance implementation.
    - Make PACKAGE_SCOPE public on MinGW.
    - Started implementing NSShadow.
    - Move awakeFromNib implementation to NSObject instead of NSView.
    - Changes for libGIF 5.2 and later.
    - Update NSViewController with lifeCycle methods.
    - Avoid accessing instance variables in inline functions when
      compiling with MSVC.
    - Add method removeAllItems to NSMenu.
    - Add badge handling to NSDockTile.
    - More improvements to layout constraints.
    - Add implementation of NSDictionaryController.
    - Add implementation of the NSCollectionView classes.
    - Improve NSDrawer opening.
    - Improver CI pipeline.

--- x11/gnustep-gui/buildlink3.mk
+++ x11/gnustep-gui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2024/11/14 22:22:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2025/03/03 01:26:21 manu Exp $
 
 BUILDLINK_TREE+=	gnustep-gui
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnustep-gui
 GNUSTEP_GUI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnustep-gui+=	gnustep-gui>=0.28
-BUILDLINK_ABI_DEPENDS.gnustep-gui+=	gnustep-gui>=0.30.0nb9
+BUILDLINK_ABI_DEPENDS.gnustep-gui+=	gnustep-gui>=0.30.0nb5
 BUILDLINK_PKGSRCDIR.gnustep-gui?=	../../x11/gnustep-gui
 
 .include "../../devel/gnustep-base/buildlink3.mk"
--- x11/gnustep-gui/patches/patch-Source_GSCharacterPanel.m
+++ x11/gnustep-gui/patches/patch-Source_GSCharacterPanel.m
@@ -1,10 +1,11 @@
-$NetBSD: patch-Source_GSCharacterPanel.m,v 1.1 2020/11/13 02:47:45 mef Exp $
+$NetBSD: patch-Source_GSCharacterPanel.m,v 1.2 2025/03/03 01:26:21 manu Exp $
 
 Adhoc patch to adapt icu-68 Fallout
 
---- Source/GSCharacterPanel.m.orig	2020-04-05 20:04:40.000000000 +0000
-+++ Source/GSCharacterPanel.m
-@@ -78,7 +78,7 @@
+--- ./Source/GSCharacterPanel.m.orig	2025-02-11 19:44:11.000000000 +0100
++++ ./Source/GSCharacterPanel.m	2025-03-03 01:45:00.162397109 +0100
+@@ -89,9 +89,9 @@
+ 
  static UBool enumCharNamesFn(void *context, UChar32 code, UCharNameChoice nameChoice, const char *name, int32_t length)
  {
    [(NSMutableIndexSet*)context addIndex: (NSUInteger)code];
@@ -13,7 +14,9 @@ Adhoc patch to adapt icu-68 Fallout
  }
  
  static NSIndexSet *AssignedCodepoints()
-@@ -103,7 +103,7 @@ static UBool searchCharNamesFn(void *con
+ {
+@@ -114,9 +114,9 @@
+   if (strstr(name, ctx->searchString) != NULL)
      {
        [ctx->set addIndex: (NSUInteger)code];
      }
@@ -22,3 +25,4 @@ Adhoc patch to adapt icu-68 Fallout
  }
  
  static NSIndexSet *CodepointsWithNameContainingSubstring(NSString *str)
+ {
