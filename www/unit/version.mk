e4f4cd36555679a3c88c33ed4d624fd6d46ce342
osa <osa@pkgsrc.org>
2025-03-03 19:58:25 +0000

    */*unit*: security update NGINX Unit from 1.34.1 to 1.34.2
    <ChangeLog>
    
    *) Security: fix missing websocket payload length validation in the Java
       language module which could lead to Java language module processes
       consuming excess CPU. (CVE-2025-1695).
    
    *) Bugfix: fix incorrect websocket payload length calculation in the
       Java language module.
    
    </ChangeLog>

--- www/unit/version.mk
+++ www/unit/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.15 2025/01/14 18:07:35 osa Exp $
+# $NetBSD: version.mk,v 1.16 2025/03/03 19:58:25 osa Exp $
 #
 # UNIT_EXTENSION_DIR
 #	Relative path to ${PREFIX} for NGINX Unit's modules.
@@ -12,7 +12,7 @@
 UNITVERSION_MK=	defined
 
 # Define NGINX Unit's version.
-UNIT_VERSION=	1.34.1
+UNIT_VERSION=	1.34.2
 
 # Define NGINX Unit's modules directory
 UNIT_EXTENSION_DIR=	libexec/unit/modules
