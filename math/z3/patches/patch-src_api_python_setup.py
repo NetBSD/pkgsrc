f0ad8f4a142869d872724d0b2a62b366e961c3d0
adam <adam@pkgsrc.org>
2025-03-04 05:09:51 +0000

    z3: allow building on all platforms

--- math/z3/patches/patch-src_api_python_setup.py
+++ math/z3/patches/patch-src_api_python_setup.py
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_api_python_setup.py,v 1.2 2025/02/20 10:27:01 adam Exp $
+$NetBSD: patch-src_api_python_setup.py,v 1.3 2025/03/04 05:09:51 adam Exp $
 
 Some platforms do not support LTO.
+
+Do not override finalize_options(). This allows to build on all platforms.
 
 --- src/api/python/setup.py.orig	2025-02-18 21:41:31.000000000 +0000
 +++ src/api/python/setup.py
@@ -13,3 +15,33 @@ Some platforms do not support LTO.
  
  IS_PYODIDE = 'PYODIDE_ROOT' in os.environ and os.environ.get('_PYTHON_HOST_PLATFORM', '').startswith('emscripten')
  
+@@ -295,29 +295,6 @@ class bdist_wheel(_bdist_wheel):
+         return os_version_tag
+             
+             
+-    def finalize_options(self):
+-        if BUILD_ARCH is not None and BUILD_PLATFORM is not None:
+-            os_version_tag = '_'.join(BUILD_OS_VERSION) if BUILD_OS_VERSION is not None else 'xxxxxx'
+-            os_version_tag = self.remove_build_machine_os_version(BUILD_PLATFORM, os_version_tag)
+-            TAGS = {
+-                # linux tags cannot be deployed - they must be auditwheel'd to pick the right compatibility tag based on imported libc symbol versions
+-                ("linux", "x86_64"): "linux_x86_64",
+-                ("linux", "aarch64"): "linux_aarch64",
+-                # windows arm64 is not supported by pypi yet
+-                ("win", "x64"): "win_amd64",
+-                ("win", "x86"): "win32",
+-                ("osx", "x64"): f"macosx_{os_version_tag}_x86_64",
+-                ("osx", "arm64"): f"macosx_{os_version_tag}_arm64",
+-                ("darwin", "x86_64"): f"macosx_{os_version_tag}_x86_64",
+-                ("darwin", "x64"): f"macosx_{os_version_tag}_x86_64",
+-                ("darwin", "arm64"): f"macosx_{os_version_tag}_arm64",
+-                ("sequoia", "x64"): f"macosx_{os_version_tag}_x86_64",
+-                ("sequoia", "x86_64"): f"macosx_{os_version_tag}_x86_64",
+-                ("sequoia", "arm64"): f"macosx_{os_version_tag}_arm64",
+-                ("emscripten", "wasm32"): f"emscripten_{os_version_tag}_wasm32",
+-            }  # type: dict[tuple[str, str], str]
+-            self.plat_name = TAGS[(BUILD_PLATFORM, BUILD_ARCH)]
+-        return super().finalize_options()
+ 
+ 
+ setup(
