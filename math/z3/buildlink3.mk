115beaede09a695ce3315d3673291155cc4ca45c
adam <adam@pkgsrc.org>
2025-03-04 05:09:51 +0000

    z3: allow building on all platforms

--- math/z3/buildlink3.mk
+++ math/z3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2024/02/21 10:50:35 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/03/04 05:09:51 adam Exp $
 
 BUILDLINK_TREE+=	z3
 
@@ -8,7 +8,7 @@ Z3_BUILDLINK3_MK:=
 USE_CXX_FEATURES+=	c++17
 
 BUILDLINK_API_DEPENDS.z3+=	z3>=4.8.3
-BUILDLINK_ABI_DEPENDS.z3?=	z3>=4.12.2nb1
+BUILDLINK_ABI_DEPENDS.z3+=	z3>=4.12.2nb1
 BUILDLINK_PKGSRCDIR.z3?=	../../math/z3
 
 .endif  # Z3_BUILDLINK3_MK
