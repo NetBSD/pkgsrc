beea654e8015faf21a97af8b15d286c544f275a6
vins <vins@pkgsrc.org>
2025-03-05 18:21:21 +0000

    comms/syncterm: update to v1.6
    # pkgsrc changes
    * Use the options framework to better handle build flags
    * NetBSD curses is now supported
    * Taking maintainership
    
    # upstream changes (since 1.1)
    v1.6:
    
        * Add Atari ST VT52 modes
        * Enable cursor for Mode 7
        * Adjust Prestel/Mode 7 mappings
        * Disable Prestel ENQ/Memory in BBC Micro Mode 7
        * Fix regression in Atari auto-login
        * Fix error adding a new Web List
    
    v1.5:
    
        * Support web-based dialing directories
        * Alt+Home now centres the window on the screen
        * Add setting to invert mouse wheel
        * Option to defer telnet negotiation
        * Use the SAA5050 font for Prestel mode
        * Add BBC Micro Mode 7 mode
        * Remove delays when sending login data
        * Fix Prestel regressions
        * Fix RIP rendering regressions
        * Fix 80x43 scrolling bottom line corruption
        * Fix overlinking with GNUmakefile
    
    v1.4:
    
        * Add support for JPEG XL graphics in addition to PPM
        * Massively improve output performance
        * Lower audio output volume by 12dB
        * Add option to configure audio output
        * Add support for ALT+XXX and ALT+0XXXXX key entry
        * Re-order BBS list entry fields
        * Support Shell connections on Windows 10+
        * Windows builds now include a .com (console) and a .exe (GUI) binary
        * Fix X11 mode scaling and fullscreen with minimal X servers
        * Fix crashes in text modes due to rip code being called
        * Better socket disconnect detection in SSH and telnets sessions
        * Reimplement status bar code
        * Fix various Prestel mode bugs
        * Fix error handling with telnets
    
    v1.3:
    
        * Fix a critical bug in editing BBS list entries
    
    v1.2:
    
        * Support PPM graphics
        * Initial support for Prestel mode
        * Start of RIP and SkyPix support (incomplete)
        * Support XTerm Bracketed Paste
        * Support LCF as defined in STD-070
        * New icons thanks to zuMi!
        * Added a comment field to listings
        * Support copy/paste in BBS list
        * Aspect ratio enforcement
        * Add ALT-O to toggle remote mouse support (to enable copy/paste locally)
        * Add -b and -n command-line options for BBS list and INI file respectively
        * Add a native (GDI) Windows mode (prefered over SDL mode)
        * Add support for data bits, stop bits, and parity
        * In telnet mode, always negotiate echo, binary, and suppress GA
        * Get Haiku support building again
        * Added "Build Options" menu item
        * Support internal smooth scaling
        * Support toggling scaling blocky vs smooth
        * Support for telnets
        * Support for SSH "none" auth method
        * Support for VGA mode
        * Allow hardware scaling in X11, GDI, and SDL modes
        * Support upploading SSH public key over SFTP channel in SSH mode
        * Add "Yellow is Yellow" option

---
+++ comms/syncterm/options.mk
@@ -0,0 +1,114 @@
+# $NetBSD: options.mk,v 1.1 2025/03/05 18:21:22 vins Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.syncterm
+PKG_OPTIONS_NONEMPTY_SETS=	display sound
+PKG_OPTIONS_SET.display=	curses sdl2 x11
+PKG_OPTIONS_SET.sound=		alsa oss portaudio pulseaudio sdl2
+
+PKG_SUPPORTED_OPTIONS=	alsa curses jxl oss portaudio pulseaudio sdl2 x11
+PKG_SUGGESTED_OPTIONS=	curses sdl2 x11
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS:M*BSD} || ${OPSYS} == "DragonFly"
+PKG_SUGGESTED_OPTIONS+=	oss
+.elif ${OPSYS} == "Linux"
+PKG_SUGGESTED_OPTIONS+=	alsa
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+##
+## ALSA sound backend.
+##
+.if !empty(PKG_OPTIONS:Malsa)
+.  include "../../audio/alsa-lib/buildlink3.mk"
+.else
+MAKE_FLAGS+=	WITHOUT_ALSA_SOUND=yes
+.endif
+
+##
+## Enable curses interface.
+## Choose the right curses library based on CURSES_TYPE.
+##
+.if !empty(PKG_OPTIONS:Mcurses)
+.  include "../../mk/curses.buildlink3.mk"
+USE_CURSES=	getmouse haldelay wide
+FAKE_NCURSES=	yes
+.  if !${CURSES_TYPE:M*ncurses*}
+CFLAGS+=	-DUSE_SYSTEM_CURSES
+.  else
+CFLAGS+=	-DN_CURSES_LIB
+.  endif
+.endif
+
+##
+## JXL
+## Support JPEG XL graphics.
+##
+.if !empty(PKG_OPTIONS:Mjxl)
+.  include "../../graphics/libjxl/buildlink3.mk"
+.else
+MAKE_FLAGS+=	WITHOUT_JPEG_XL=1
+.endif
+
+##
+## OSS sound backend.
+## Use native OSS or ossaudio(3) library.
+##
+.if !empty(PKG_OPTIONS:Moss)
+.  include "../../mk/oss.buildlink3.mk"
+SUBST_CLASSES+=         oss
+SUBST_STAGE.oss=        pre-configure
+SUBST_MESSAGE.oss=      Correcting the path to the OSS device.
+SUBST_FILES.oss+=	src/xpdev/xpbeep.c
+SUBST_SED.oss+=         -e "s,/dev/dsp,${DEVOSSAUDIO},g"
+.  if ${OPSYS} == "NetBSD" || ${OPSYS} == "OpenBSD"
+LDFLAGS+=               ${LIBOSSAUDIO}
+.  endif
+.endif
+
+##
+## PortAudio sound backend.
+##
+.if !empty(PKG_OPTIONS:Mportaudio)
+.  include "../../audio/portaudio/buildlink3.mk"
+CFLAGS+=	-DWITH_PORTAUDIO
+.else
+MAKE_FLAGS+=	WITHOUT_PORTAUDIO=1
+.endif
+
+##
+## Pulseaudio sound backend.
+##
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.  include "../../audio/pulseaudio/buildlink3.mk"
+.else
+MAKE_FLAGS+=	NO_PULSEAUDIO=1
+.endif
+
+##
+## SDL2
+## Enable SDL2 based display interface
+##
+.if !empty(PKG_OPTIONS:Msdl2)
+.  include "../../devel/SDL2/buildlink3.mk"
+MAKE_FLAGS+=	USE_SDL=1
+MAKE_FLAGS+=	USE_SDL_AUDIO=1
+MAKE_FLAGS+=	SDL_CONFIG=${PREFIX}/bin/sdl2-config
+.else
+MAKE_FLAGS+=	WITHOUT_SDL=1
+.endif
+
+##
+## X11
+## Enable X11 based display interface
+##
+.if !empty(PKG_OPTIONS:Mx11)
+.  include "../../x11/libXinerama/buildlink3.mk"
+.  include "../../x11/libXrandr/buildlink3.mk"
+.  include "../../x11/libX11/buildlink3.mk"
+MAKE_FLAGS+=	X_PATH=${X11BASE:Q}
+.else
+MAKE_FLAGS+=	NO_X=1
+.endif
