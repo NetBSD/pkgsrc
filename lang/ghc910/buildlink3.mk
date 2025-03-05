6ec95f84577344bc206920e21e80a42f4a6e92c7
pho <pho@pkgsrc.org>
2025-03-05 03:33:59 +0000

    lang/ghc910: Work around PR kern/59127
    This causes a behaviour change in the library "base". A recursive revbump
    will be needed.

--- lang/ghc910/buildlink3.mk
+++ lang/ghc910/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/02/03 16:09:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/03/05 03:33:59 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.10.1nb2
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.10.1nb2
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.10.1nb3
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.10.1nb3
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc910
 
 .include "../../converters/libiconv/buildlink3.mk"
