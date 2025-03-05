dfa78d39174a0de6e381ae94910662728b68b58e
vins <vins@pkgsrc.org>
2025-03-05 18:21:21 +0000

    comms/syncterm: update to v1.6
    # pkgsrc changes
    * Use the options framework to better handle build flags
    * NetBSD curses is now supported
    * Taking maintainership
    
    # upstream changes (since 1.1)
    v1.6:
    
        * Add Atari ST VT52 modes
        * Enable cursor for Mode 7
        * Adjust Prestel/Mode 7 mappings
        * Disable Prestel ENQ/Memory in BBC Micro Mode 7
        * Fix regression in Atari auto-login
        * Fix error adding a new Web List
    
    v1.5:
    
        * Support web-based dialing directories
        * Alt+Home now centres the window on the screen
        * Add setting to invert mouse wheel
        * Option to defer telnet negotiation
        * Use the SAA5050 font for Prestel mode
        * Add BBC Micro Mode 7 mode
        * Remove delays when sending login data
        * Fix Prestel regressions
        * Fix RIP rendering regressions
        * Fix 80x43 scrolling bottom line corruption
        * Fix overlinking with GNUmakefile
    
    v1.4:
    
        * Add support for JPEG XL graphics in addition to PPM
        * Massively improve output performance
        * Lower audio output volume by 12dB
        * Add option to configure audio output
        * Add support for ALT+XXX and ALT+0XXXXX key entry
        * Re-order BBS list entry fields
        * Support Shell connections on Windows 10+
        * Windows builds now include a .com (console) and a .exe (GUI) binary
        * Fix X11 mode scaling and fullscreen with minimal X servers
        * Fix crashes in text modes due to rip code being called
        * Better socket disconnect detection in SSH and telnets sessions
        * Reimplement status bar code
        * Fix various Prestel mode bugs
        * Fix error handling with telnets
    
    v1.3:
    
        * Fix a critical bug in editing BBS list entries
    
    v1.2:
    
        * Support PPM graphics
        * Initial support for Prestel mode
        * Start of RIP and SkyPix support (incomplete)
        * Support XTerm Bracketed Paste
        * Support LCF as defined in STD-070
        * New icons thanks to zuMi!
        * Added a comment field to listings
        * Support copy/paste in BBS list
        * Aspect ratio enforcement
        * Add ALT-O to toggle remote mouse support (to enable copy/paste locally)
        * Add -b and -n command-line options for BBS list and INI file respectively
        * Add a native (GDI) Windows mode (prefered over SDL mode)
        * Add support for data bits, stop bits, and parity
        * In telnet mode, always negotiate echo, binary, and suppress GA
        * Get Haiku support building again
        * Added "Build Options" menu item
        * Support internal smooth scaling
        * Support toggling scaling blocky vs smooth
        * Support for telnets
        * Support for SSH "none" auth method
        * Support for VGA mode
        * Allow hardware scaling in X11, GDI, and SDL modes
        * Support upploading SSH public key over SFTP channel in SSH mode
        * Add "Yellow is Yellow" option

--- comms/syncterm/patches/patch-src_xpdev_genwrap.h
+++ comms/syncterm/patches/patch-src_xpdev_genwrap.h
@@ -1,11 +1,12 @@
-$NetBSD: patch-src_xpdev_genwrap.h,v 1.1 2022/08/10 18:49:39 he Exp $
+$NetBSD: patch-src_xpdev_genwrap.h,v 1.2 2025/03/05 18:21:22 vins Exp $
 
-Recognize __powerpc__ as well.
+* Recognize __powerpc__ as well.
+* ARCHITECTURE_DESC is unused, no need to error out for that.
 
---- ./src/xpdev/genwrap.h.orig	2020-06-26 23:40:21.000000000 +0000
-+++ ./src/xpdev/genwrap.h
-@@ -203,7 +203,7 @@ extern "C" {
- 	#define ARCHITECTURE_DESC "mips"
+--- src/xpdev/genwrap.h.orig	2025-02-11 18:23:24.000000000 +0000
++++ src/xpdev/genwrap.h
+@@ -218,7 +218,7 @@ extern "C" {
+ 	#define ARCHITECTURE_DESC "arm64"
  #elif defined(__arm__)
  	#define ARCHITECTURE_DESC "arm"
 -#elif defined(_M_PPC) || defined(__ppc__)
@@ -13,3 +14,12 @@ Recognize __powerpc__ as well.
  	#define ARCHITECTURE_DESC "ppc"
  #elif defined(_M_IA64) || defined(__ia64__)
  	#define ARCHITECTURE_DESC "ia64"
+@@ -228,7 +228,7 @@ extern "C" {
+ 	#ifdef UNAME_ARCHITECTURE_DESC
+ 		#define ARCHITECTURE_DESC UNAME_ARCHITECTURE_DESC
+ 	#else
+-		#error "Need to describe target architecture"
++		#define ARCHITECTURE_DESC "unknown"
+ 	#endif
+ #endif
+ 
