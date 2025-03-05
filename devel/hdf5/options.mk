53441c3b2399320e8bee6ec4640aa0b060c673f3
adam <adam@pkgsrc.org>
2025-03-05 06:24:20 +0000

    hdf5: updated to 1.14.6
    1.14.6
    
    This is a maintenance release with some minor new changes:
    
    Reverted a change to the library’s handling of UTF-8 file names. A change was made in the HDF5 1.14.4 release to address some issues with the library’s handling of code pages and file paths on Windows. This change introduced other issues with handling UTF-8 file names that caused breakage for software using the 1.14.4 and 1.14.5 releases of HDF5.
    
    The change has been reverted for this release and will be addressed differently in a future release.
    
    If you use 1.14.4 or 1.14.5 and are happy with how UTF-8 file names are handled in those versions, do not update to 1.14.6.
    Added support for native, i.e., built-into-the HDF5 library, zlib-ng compression. CMake find package call also finds system zlib.
    Modified the CMake logic for finding the libaec compression library, a drop-in replacement for szip, for a system-installed library version.
    Removed the CMake module search find_package for the szip library.

--- devel/hdf5/options.mk
+++ devel/hdf5/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.12 2023/12/18 15:53:27 thor Exp $
+# $NetBSD: options.mk,v 1.13 2025/03/05 06:24:20 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.hdf5
 PKG_SUPPORTED_OPTIONS=	szip threads hdf5-unsafe-threads
@@ -14,7 +14,8 @@ CONFIGURE_ARGS+=	--with-szlib
 # Threadsafe API is incompatible with high-level and C++ APIs.
 PLIST_VARS+=	hl
 .if !empty(PKG_OPTIONS:Mthreads)
-CONFIGURE_ARGS+=	--enable-threadsafe --disable-hl
+CMAKE_CONFIGURE_ARGS+=	-DHDF5_BUILD_HL_LIB=OFF # why?
+CMAKE_CONFIGURE_ARGS+=	-DHDF5_ENABLE_THREADSAFE=ON
 .include "../../mk/pthread.buildlink3.mk"
 .else
 PLIST.hl=	yes
@@ -23,6 +24,6 @@ PLIST.hl=	yes
 # You can still enable threads if you are adventurous. There
 # are scientists like that. Not supported, but possible.
 .if !empty(PKG_OPTIONS:Mhdf5-unsafe-threads)
-CONFIGURE_ARGS+=       --enable-threadsafe --enable-unsupported
+CMAKE_CONFIGURE_ARGS+=	-DHDF5_ENABLE_THREADSAFE=ON #--enable-unsupported
 .include "../../mk/pthread.buildlink3.mk"
 .endif
