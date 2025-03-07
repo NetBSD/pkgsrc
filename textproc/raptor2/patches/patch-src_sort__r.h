f1ec2a2a6d7dac5c05b07058d56e4f64f554da32
mrg <mrg@pkgsrc.org>
2025-03-07 04:45:42 +0000

    update raptor2 for netbsd-current having POSIX qsort_r().
    with the new qsort_r() in -current, raptor2 is picking the wrong way
    to use it on netbsd (and likely, on modern freebsd.)
    
    as the _R_SORT_LINUX path is the POSIX version, use that on netbsd.
    
    the lack of qsort_r(), ie, netbsd before a few days ago, makes it
    provide it's own, differently named, backend for this functionality.
    
    test-built on -current and netbsd-10.
    
    no pkg bump because it doesn't affect the netbsd-10 or netbsd-9
    (or non-netbsd) pkgs, and it merely fixes -current build, not makes
    it better or different some how.

--- textproc/raptor2/patches/patch-src_sort__r.h
+++ textproc/raptor2/patches/patch-src_sort__r.h
@@ -1,20 +1,31 @@
-$NetBSD: patch-src_sort__r.h,v 1.3 2023/03/07 10:45:28 wiz Exp $
+$NetBSD: patch-src_sort__r.h,v 1.4 2025/03/07 04:45:42 mrg Exp $
 
 SunOS support
 https://github.com/dajobe/raptor/pull/57
 
---- src/sort_r.h.orig	2017-07-01 20:53:27.000000000 +0000
-+++ src/sort_r.h
-@@ -30,6 +30,8 @@ void sort_r(void *base, size_t nel, size
+Fix NetBSD support after NetBSD added sort_r().
+
+--- src/sort_r.h.orig	2017-07-01 13:53:27.000000000 -0700
++++ src/sort_r.h	2025-03-06 11:25:23.846999574 -0800
+@@ -22,14 +22,15 @@
+ #if (defined __APPLE__ || defined __MACH__ || defined __DARWIN__ || \
+      defined __FreeBSD__ || defined __BSD__ || defined __bsdi__ || \
+      defined OpenBSD3_1 || defined OpenBSD3_9 || defined __OpenBSD__ || \
+-     defined __NetBSD__ || \
+      defined __DragonFly__ || \
+      defined AMIGA)
+ #  define _SORT_R_BSD
+ #elif (defined _GNU_SOURCE || defined __gnu_hurd__ || defined __GNU__ || \
         defined __linux__ || defined __MINGW32__ || defined __GLIBC__ || \
-        defined __CYGWIN__)
+-       defined __CYGWIN__)
++       defined __CYGWIN__ || defined __NetBSD__)
  #  define _SORT_R_LINUX
 +#elif defined __sun
 +#  define _SORT_R_SUN
  #elif (defined _WIN32 || defined _WIN64 || defined __WINDOWS__)
  #  define _SORT_R_WINDOWS
  #else
-@@ -87,6 +89,11 @@ void sort_r(void *base, size_t nel, size
+@@ -87,6 +88,11 @@
                          __compar_d_fn_t __compar, void *arg)
        __attribute__((nonnull (1, 4)));
  
@@ -26,7 +37,7 @@ https://github.com/dajobe/raptor/pull/57
    #endif
  
    /* implementation */
-@@ -106,6 +113,10 @@ void sort_r(void *base, size_t nel, size
+@@ -106,6 +112,10 @@
        tmp.compar = compar;
        qsort_r(base, nel, width, &tmp, sort_r_arg_swap);
  
@@ -37,7 +48,7 @@ https://github.com/dajobe/raptor/pull/57
      #else /* defined _SORT_R_WINDOWS */
  
        struct sort_r_data tmp;
-@@ -121,5 +132,6 @@ void sort_r(void *base, size_t nel, size
+@@ -121,5 +131,6 @@
  #undef _SORT_R_WINDOWS
  #undef _SORT_R_LINUX
  #undef _SORT_R_BSD
