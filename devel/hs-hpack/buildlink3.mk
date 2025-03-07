2172a4a88f4cf600e200a471931d857af760a802
pho <pho@pkgsrc.org>
2025-03-07 11:08:19 +0000

    devel/hs-hpack: import hs-hpack-0.38.0
    Hpack is a format for Haskell packages. It is a modern alternative to the
    Cabal package format and follows different design principles.
    
    Hpack packages are described in a file named package.yaml. Both cabal2nix
    and stack support package.yaml natively. For other build tools the hpack
    executable can be used to generate a .cabal file from package.yaml.

---
+++ devel/hs-hpack/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/03/07 11:08:19 pho Exp $
+
+BUILDLINK_TREE+=	hs-hpack
+
+.if !defined(HS_HPACK_BUILDLINK3_MK)
+HS_HPACK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hpack+=	hs-hpack>=0.38.0
+BUILDLINK_ABI_DEPENDS.hs-hpack+=	hs-hpack>=0.38.0
+BUILDLINK_PKGSRCDIR.hs-hpack?=		../../devel/hs-hpack
+
+.include "../../textproc/hs-Glob/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../math/hs-bifunctors/buildlink3.mk"
+.include "../../security/hs-crypton/buildlink3.mk"
+.include "../../www/hs-http-client/buildlink3.mk"
+.include "../../www/hs-http-client-tls/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-infer-license/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-yaml/buildlink3.mk"
+.endif	# HS_HPACK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hpack
