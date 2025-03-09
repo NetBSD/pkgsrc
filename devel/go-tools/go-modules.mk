96f410ba9468afd41d6e85b6047eb3a11b3e10c4
bsiegert <bsiegert@pkgsrc.org>
2025-03-09 19:37:14 +0000

    go-tools: update to 0.31.0
    No changelog from upstream.

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,16 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.7 2024/12/22 20:12:32 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.8 2025/03/09 19:37:14 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.22.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.22.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.33.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.33.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.10.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.10.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.28.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.28.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.24.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.24.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.37.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.37.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.12.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.12.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.31.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.31.0.zip
 GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.mod
 GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.zip
