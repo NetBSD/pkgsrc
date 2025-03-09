e82709aaa8af3788e268e94411ddd14df984abc0
jakllsch <jakllsch@pkgsrc.org>
2025-03-09 14:04:48 +0000

    avoid bin/pagsh conflict with openafs in default case

--- security/heimdal/options.mk
+++ security/heimdal/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2021/10/22 07:31:54 wiz Exp $
+# $NetBSD: options.mk,v 1.7 2025/03/09 14:04:48 jakllsch Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.heimdal
 PKG_SUPPORTED_OPTIONS=	inet6 kerberos-prefix-cmds ldap
@@ -38,10 +38,11 @@ heimdal-ldap-schema:
 ###
 .if !empty(PKG_OPTIONS:Mkerberos-prefix-cmds)
 KRB5_PREFIX=		k
-HEIMDAL_TRANSFORM=	s/^su/${KRB5_PREFIX}&/;
+HEIMDAL_TRANSFORM=	s/^su/${KRB5_PREFIX}&/;s/^pagsh/${KRB5_PREFIX}&/;
 CONFIGURE_ARGS+=	--program-transform-name=${HEIMDAL_TRANSFORM:Q}
 .else
 KRB5_PREFIX=		# empty
+CONFLICTS+=		openafs-[0-9]*
 .endif
 
 PLIST_SUBST+=		KRB5_PREFIX=${KRB5_PREFIX:Q}
