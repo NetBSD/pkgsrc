c0f047634bf406960d1aa2e4c9cf9bde27be64cb
adam <adam@pkgsrc.org>
2025-03-11 14:00:04 +0000

    shfmt: updated to 3.11.0
    3.11.0
    
    This release drops support for Go 1.22 and includes many enhancements.
    
    cmd/shfmt
    Support -l=0 and -f=0 to split filenames with null bytes
    syntax
    New iterator API: Parser.WordsSeq
    Fix Parser.Incomplete and IsIncomplete to work well with Parser.Words
    Initial support for parsing incomplete shell via RecoverErrors
    Expand LangError to include which language was used when parsing
    interp
    Refactor setting variables to fix array declaration edge cases
    Fix test read/write/exec operators to work correctly on directories
    Replace the cancelreader dependency with os.File.SetReadDeadline
    Avoid waiting for process substitutions, matching Bash
    Skip OpenHandler when opening named pipes for process substitutions
    Use TMPDIR if set via Env to create temporary files such as named pipes
    expand
    New iterator API: FieldsSeq
    Correctly handle repeated backslashes in double quotes
    Don't expand backslashes inside here-documents

--- devel/shfmt/go-modules.mk
+++ devel/shfmt/go-modules.mk
@@ -1,31 +1,27 @@
-# $NetBSD: go-modules.mk,v 1.3 2025/01/09 13:27:57 adam Exp $
+# $NetBSD: go-modules.mk,v 1.4 2025/03/11 14:00:04 adam Exp $
 
-GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.23.mod
-GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.23.zip
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.24.mod
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.24.zip
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.9.mod
 GO_MODULE_FILES+=	github.com/go-quicktest/qt/@v/v1.101.0.mod
 GO_MODULE_FILES+=	github.com/go-quicktest/qt/@v/v1.101.0.zip
-GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
-GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.7.0.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.7.0.zip
 GO_MODULE_FILES+=	github.com/google/renameio/v2/@v/v2.0.0.mod
 GO_MODULE_FILES+=	github.com/google/renameio/v2/@v/v2.0.0.zip
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.3.1.mod
 GO_MODULE_FILES+=	github.com/kr/pretty/@v/v0.3.1.zip
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.2.0.mod
 GO_MODULE_FILES+=	github.com/kr/text/@v/v0.2.0.zip
-GO_MODULE_FILES+=	github.com/muesli/cancelreader/@v/v0.2.2.mod
-GO_MODULE_FILES+=	github.com/muesli/cancelreader/@v/v0.2.2.zip
 GO_MODULE_FILES+=	github.com/pkg/diff/@v/v0.0.0-20210226163009-20ebb0f2a09e.mod
-GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.13.1.mod
-GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.13.1.zip
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.14.1.mod
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.14.1.zip
 GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.9.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.zip
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.25.0.mod
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.25.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.29.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.29.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.27.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.27.0.zip
 GO_MODULE_FILES+=	mvdan.cc/editorconfig/@v/v0.3.0.mod
 GO_MODULE_FILES+=	mvdan.cc/editorconfig/@v/v0.3.0.zip
