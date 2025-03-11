97a6254a3511d46fa1c361d7c7cb341d6f1cc241
nia <nia@pkgsrc.org>
2025-03-11 13:04:01 +0000

    kf6-kwindowsystem: Build dependent packages with C++20 compiler.

--- x11/kf6-kwindowsystem/buildlink3.mk
+++ x11/kf6-kwindowsystem/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2025/01/31 11:52:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/03/11 13:04:01 nia Exp $
 
 BUILDLINK_TREE+=	kf6-kwindowsystem
 
@@ -8,6 +8,8 @@ KF6_KWINDOWSYSTEM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kf6-kwindowsystem+=	kf6-kwindowsystem>=6.0.0
 BUILDLINK_ABI_DEPENDS.kf6-kwindowsystem?=	kf6-kwindowsystem>=6.2.0nb4
 BUILDLINK_PKGSRCDIR.kf6-kwindowsystem?=		../../x11/kf6-kwindowsystem
+
+USE_CXX_FEATURES+=	c++20
 
 .include "../../devel/qt6-qtwayland/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
