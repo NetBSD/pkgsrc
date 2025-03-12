3a1276eafa8f8b4865ebc67483c798739bdcf0cd
adam <adam@pkgsrc.org>
2025-03-12 18:54:23 +0000

    slurm-wlm: updated to 20.11.9
    Changes in Slurm 20.11.9
    
    - burst_buffer - add missing common directory to the Makefile SUBDIRS.
    - sacct - fix truncation when printing jobidraw field.
    - GRES - Fix loading state of jobs using --gpus to request gpus.
    - Fix minor logic error in health check node state output
    - Fix GCC 11.1 compiler warnings.
    - Delay steps when memory already used instead of rejecting step request.
    - Fix memory leak in the slurmdbd when requesting wckeys from all clusters.
    - Fix determining if a reservation is used or not.
    - openapi/v0.0.35 - Honor kill_on_invalid_dependency as job parameter.
    - openapi/v0.0.36 - Honor kill_on_invalid_dependency as job parameter.
    - Fix various issues dealing with updates on magnetic reservations that could
      lead to abort slurmctld.
    - openapi/v0.0.36 - Avoid setting default values of min_cpus, job name, cwd,
      mail_type, and contiguous on job update.
    - openapi/v0.0.36 - Clear user hold on job update if hold=false.
    - Fix slurmctld segfault due to a bit_test() call with a MAINT+ANY_NODES
      reservation NULL node_bitmap.
    - Fix slurmctld segfault due to a bit_copy() call with a REPLACE+ANY_NODES
      reservation NULL node_bitmap.
    - Fix error in GPU frequency validation logic.
    - Fix error in pmix logic dealing with the incorrect size of buffer.
    - PMIx v1.1.4 and below are no longer supported.
    - Fix shutdown of slurmdbd plugin to correctly notice when the agent thread
      finishes.
    - Fix slurmctld segfault due to job array --batch features double free.
    - CVE-2022-29500 - Prevent credential abuse.
    - CVE-2022-29501 - Prevent abuse of REQUEST_FORWARD_DATA.

--- parallel/slurm-wlm/patches/patch-src_slurmd_common_task__plugin.h
+++ parallel/slurm-wlm/patches/patch-src_slurmd_common_task__plugin.h
@@ -1,15 +1,16 @@
-$NetBSD: patch-src_slurmd_common_task__plugin.h,v 1.1 2018/04/22 21:31:09 bacon Exp $
+$NetBSD: patch-src_slurmd_common_task__plugin.h,v 1.2 2025/03/12 18:54:24 adam Exp $
 
 # cpuset functions not standardized across platforms
 
---- src/slurmd/common/task_plugin.h.orig	2018-01-04 21:15:01.000000000 +0000
+--- src/slurmd/common/task_plugin.h.orig	2022-05-04 19:32:38.000000000 +0000
 +++ src/slurmd/common/task_plugin.h
-@@ -42,6 +42,16 @@
+@@ -48,6 +48,17 @@ typedef cpuset_t cpu_set_t;
  
  #include "src/slurmd/slurmstepd/slurmstepd_job.h"
  
 +// Keep in sync with slurmd/slurmd.h
 +#ifdef __NetBSD__    
++#define CPU_SETSIZE	MAXCPUS
 +#define	cpu_set_t	cpuset_t
 +#define CPU_ZERO(c)	cpuset_zero((c))   
 +#define CPU_SET(i,c)	cpuset_set((i),(c))
