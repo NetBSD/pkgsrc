664fd40338cf7d48f4213a993a99b31aef60c19e
taca <taca@pkgsrc.org>
2025-03-14 15:27:16 +0000

    lang/php82: update to 8.2.28
    This is security release.
    
    PHP 8.2.28 (2025-03-13)
    
    - Core:
      . Fixed bug GH-17211 (observer segfault on function loaded with dl()).
        (Arnaud)
    
    - LibXML:
      . Fixed GHSA-wg4p-4hqh-c3g9 (Reocurrence of #72714). (nielsdos)
      . Fixed GHSA-p3x9-6h7p-cgfc (libxml streams use wrong `content-type` header
        when requesting a redirected resource). (CVE-2025-1219) (timwolla)
    
    - Streams:
      . Fixed GHSA-hgf54-96fm-v528 (Stream HTTP wrapper header check might omit
        basic auth header). (CVE-2025-1736) (Jakub Zelenka)
      . Fixed GHSA-52jp-hrpf-2jff (Stream HTTP wrapper truncate redirect location
        to 1024 bytes). (CVE-2025-1861) (Jakub Zelenka)
      . Fixed GHSA-pcmh-g36c-qc44 (Streams HTTP wrapper does not fail for headers
        without colon). (CVE-2025-1734) (Jakub Zelenka)
      . Fixed GHSA-v8xr-gpvj-cx9g (Header parser of `http` stream wrapper does not
        handle folded headers). (CVE-2025-1217) (Jakub Zelenka)
    
    - Windows:
      . Fixed phpize for Windows 11 (24H2). (bwoebi)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.458 2025/03/14 15:25:30 taca Exp $
+# $NetBSD: phpversion.mk,v 1.459 2025/03/14 15:27:16 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -111,7 +111,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.32
-PHP82_VERSION=	8.2.27
+PHP82_VERSION=	8.2.28
 PHP83_VERSION=	8.3.19
 PHP84_VERSION=	8.4.5
 
