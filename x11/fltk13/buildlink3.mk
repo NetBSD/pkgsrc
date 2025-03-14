d50c1ccea28b014a1c54f2cef56148831e21b224
micha <micha@pkgsrc.org>
2025-03-14 16:00:40 +0000

    x11/fltk13: Do not use namespaces of x11/fltk
    - Declare "fltk13" package (this one) as successor for FLTK 1.3.x
    - Declare conflict with FLTK >1.4.0 (from x11/fltk)
    - Bump PKGREVISION
    
    This should fix problems with caches of bulk builds.

--- x11/fltk13/buildlink3.mk
+++ x11/fltk13/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.20 2025/01/27 22:21:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2025/03/14 16:00:40 micha Exp $
 
-BUILDLINK_TREE+=	fltk
+BUILDLINK_TREE+=	fltk13
 
-.if !defined(FLTK_BUILDLINK3_MK)
-FLTK_BUILDLINK3_MK:=
+.if !defined(FLTK13_BUILDLINK3_MK)
+FLTK13_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0<1.4.0
-BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.8nb3
-BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
-BUILDLINK_FILES.fltk+=		include/Fl/*
+BUILDLINK_API_DEPENDS.fltk13+=	fltk13>=1.3.0<1.4.0
+BUILDLINK_ABI_DEPENDS.fltk13?=	fltk13>=1.3.8nb3
+BUILDLINK_PKGSRCDIR.fltk13?=	../../x11/fltk13
+BUILDLINK_FILES.fltk13+=	include/Fl/*
 
-pkgbase := fltk
+pkgbase := fltk13
 .include "../../mk/pkg-build-options.mk"
 
 # For "opengl" option
-.if ${PKG_BUILD_OPTIONS.fltk:Mopengl}
+.if ${PKG_BUILD_OPTIONS.fltk13:Mopengl}
 .  if ${OPSYS} != "Darwin"
 .    include "../../graphics/MesaLib/buildlink3.mk"
 .    include "../../graphics/glu/buildlink3.mk"
@@ -34,6 +34,6 @@ pkgbase := fltk
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
-.endif # FLTK_BUILDLINK3_MK
+.endif # FLTK13_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-fltk
+BUILDLINK_TREE+=	-fltk13
