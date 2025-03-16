99352ae298ac41db60bead315e1e85f30a7924ac
riastradh <riastradh@pkgsrc.org>
2025-03-16 21:46:13 +0000

    emulators/qemu/version.mk: Note the new other user.
    No functional change intended -- comment only.

--- emulators/qemu/version.mk
+++ emulators/qemu/version.mk
@@ -1,4 +1,6 @@
-# $NetBSD: version.mk,v 1.1 2025/03/12 10:08:48 is Exp $
+# $NetBSD: version.mk,v 1.2 2025/03/16 21:46:13 riastradh Exp $
 #
 # used by emulators/qemu
+# used by sysutils/qemu-guest-agent
+
 QEMU_VERSION?=	9.2.2
