6281992ab13240ca192a360ba095166e4cc522f4
gdt <gdt@pkgsrc.org>
2025-03-21 16:16:19 +0000

    devel/py-maturin: tool.mk: Require recent version
    At least devel/py-rpds-py fails to build with somewhat older maturin,
    but this is hard to figure out from reading upstream NEWS.  And,
    py-rpds-py upstream simply says it needs maturin, wihtout a version
    requirement.
    
    While 1.8.3 is surely a more strict requirement than necessary, it's
    difficult to determine what the real requirements are.  When maturin
    is used as a tool, require >=1.8.3 (the current version).  This choice
    favors avoiding unknown problems with the downside that some
    people:(but only those that have pkgsrc installations with
    persistently outdated packages) will now have to update maturin.
    
    This commit does not intend to establish a rule that the minimum
    version should be increased on every update.

--- devel/py-maturin/tool.mk
+++ devel/py-maturin/tool.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tool.mk,v 1.1 2024/11/13 13:28:17 jperkin Exp $
+# $NetBSD: tool.mk,v 1.2 2025/03/21 16:16:19 gdt Exp $
 #
 # Maturin will by default spawn as many build threads as it finds online
 # CPUs, which is completely unsuitable for bulk builds.  This file should
@@ -8,7 +8,9 @@
 .if !defined(MATURIN_TOOL_MK)
 MATURIN_TOOL_MK:=
 
-TOOL_DEPENDS+=	${PYPKGPREFIX}-maturin-[0-9]*:../../devel/py-maturin
+# At least 1.7.8 fails to parse some now-considered-ok pyproject.toml
+# content.
+TOOL_DEPENDS+=	${PYPKGPREFIX}-maturin>=1.8.3:../../devel/py-maturin
 
 WHEEL_ARGS+=	-Cbuild-args=${_MAKE_JOBS}
 .endif
