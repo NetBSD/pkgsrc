fd655f13aff78b0507502bdcb16d7dc7d42a77d6
wiz <wiz@pkgsrc.org>
2025-03-21 16:43:53 +0000

    tinyxml2: switch to shared library
    Fix location of pkgconfig file in WRKSRC.
    
    From Anthony Mallet on pkgsrc-users.
    
    Bump PKGREVISION.

--- textproc/tinyxml2/buildlink3.mk
+++ textproc/tinyxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/04/27 08:19:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/03/21 16:43:53 wiz Exp $
 
 BUILDLINK_TREE+=	tinyxml2
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.tinyxml2+=	tinyxml2>=2.1.0
 BUILDLINK_ABI_DEPENDS.tinyxml2+=	tinyxml2>=9.0.0
 BUILDLINK_PKGSRCDIR.tinyxml2?=		../../textproc/tinyxml2
 
-BUILDLINK_DEPMETHOD.tinyxml2?=		build
 .endif	# TINYXML2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-tinyxml2
