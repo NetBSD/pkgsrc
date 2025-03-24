63a5707eb49a9030460a2955b0d7a5516083143d
nia <nia@pkgsrc.org>
2025-03-24 09:32:38 +0000

    openal-soft: Build dependent packages with the same compiler.
    XXX c++20 dependency is super premature for such a heavily
    depended-on package, good idea to probably split multiple older
    releases out from this package for e.g. netbsd-9, not much advantage
    to having the latest release

--- audio/openal-soft/buildlink3.mk
+++ audio/openal-soft/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/08 01:53:48 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/03/24 09:32:38 nia Exp $
 
 BUILDLINK_TREE+=	openal-soft
 
 .if !defined(OPENAL_SOFT_BUILDLINK3_MK)
 OPENAL_SOFT_BUILDLINK3_MK:=
+
+USE_CXX_FEATURES+=	c++20
 
 BUILDLINK_API_DEPENDS.openal-soft+=	openal-soft>=1.5.304
 BUILDLINK_PKGSRCDIR.openal-soft?=	../../audio/openal-soft
