b6dc8a310a3102815b9c1fd1caa20f1647e0dcc5
kim <kim@pkgsrc.org>
2025-03-25 09:01:20 +0000

    qemu-guest-agent: Daemonize properly
    Tested normal operation (no hang) on 10.1/amd64 (202412182240Z) and
    10.99.12/amd64 (202412281020Z), including sending a shutdown command
    from the hypervisor.
    
    Without the -d option to daemonize, the guest agent would keep its
    controlling tty open, which is problematic.

--- sysutils/qemu-guest-agent/files/qemu-guest-agent.sh
+++ sysutils/qemu-guest-agent/files/qemu-guest-agent.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qemu-guest-agent.sh,v 1.2 2025/03/18 17:43:44 is Exp $
+# $NetBSD: qemu-guest-agent.sh,v 1.3 2025/03/25 09:01:20 kim Exp $
 #
 # PROVIDE: qemu_guest_agent
 # REQUIRE: DAEMON
@@ -13,8 +13,7 @@ fi
 name="qemu_guest_agent"
 rcvar="${name}"
 command="@PREFIX@/bin/qemu-ga"
-command_args="-m isa-serial -p /dev/tty00 -r -t /var/run -f /var/run/${name}.pid"
-
+command_args="-d -m isa-serial -p /dev/tty00 -r -t /var/run -f /var/run/${name}.pid"
 
 load_rc_config $name
-(run_rc_command "$1" &)
+run_rc_command "$1"
