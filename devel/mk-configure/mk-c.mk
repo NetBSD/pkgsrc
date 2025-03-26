5f6706574bf853c425b95609f6c9e0d5e517a103
rillig <rillig@pkgsrc.org>
2025-03-26 20:26:26 +0000

    devel/mk-configure: fix NetBSD-9 build for packages using mk-configure
    See PR pkg/59184.

--- devel/mk-configure/mk-c.mk
+++ devel/mk-configure/mk-c.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mk-c.mk,v 1.11 2021/03/24 21:58:42 cheusov Exp $
+# $NetBSD: mk-c.mk,v 1.12 2025/03/26 20:26:26 rillig Exp $
 
 TOOL_DEPENDS+=	${BUILDLINK_API_DEPENDS.mk-c}:${BUILDLINK_PKGSRCDIR.mk-c}
 
@@ -7,6 +7,10 @@ MAKE_ENV+=		MANDIR=${PREFIX}/${PKGMANDIR}
 MAKE_ENV+=		SYSCONFDIR=${PKG_SYSCONFDIR}
 MAKE_ENV+=		MKCOMPILERSETTINGS=yes
 CONFIGURE_TARGET?=	configure
+
+# Fix the build on NetBSD 9 since it passes .MAKE.LEVEL.ENV via MAKEFLAGS,
+# and the pkgsrc make doesn't allow this variable.
+MAKE_ENV+=		MAKEFLAGS=
 
 .if !target(do-configure)
 do-configure:
