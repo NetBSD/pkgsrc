1278e7383f66854db7e8e76df910a218e73f1fc2
wiz <wiz@pkgsrc.org>
2025-03-27 13:49:25 +0000

    pound: add reference to upstream commit

--- www/pound/patches/patch-src_mem.h
+++ www/pound/patches/patch-src_mem.h
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_mem.h,v 1.3 2025/03/27 13:46:16 wiz Exp $
+$NetBSD: patch-src_mem.h,v 1.4 2025/03/27 13:49:25 wiz Exp $
 
 Add header for va_list.
+Already in upstream:
+https://github.com/graygnuorg/pound/commit/c320888be33d0c75d7ab9297ae9c7f95a8e6eaca
 
 --- src/mem.h.orig	2025-03-24 06:51:50.338105388 +0000
 +++ src/mem.h
