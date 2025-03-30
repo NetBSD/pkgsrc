657ca36a963bbf3278df980c2185185f6eec05ff
wiz <wiz@pkgsrc.org>
2025-03-30 15:43:52 +0000

    swi-prolog-lite: remove bitrig support

--- lang/swi-prolog-lite/patches/patch-packages_clib_sha1_brg_endian.h
+++ lang/swi-prolog-lite/patches/patch-packages_clib_sha1_brg_endian.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-packages_clib_sha1_brg_endian.h,v 1.3 2015/12/29 23:34:56 dholland Exp $
+$NetBSD: patch-packages_clib_sha1_brg_endian.h,v 1.4 2025/03/30 15:43:52 wiz Exp $
 
-Support Dragonfly and Bitrig.
+Support Dragonfly.
 
 --- packages/clib/sha1/brg_endian.h.orig	2014-04-13 08:40:33.000000000 +0000
 +++ packages/clib/sha1/brg_endian.h
@@ -9,7 +9,7 @@ Support Dragonfly and Bitrig.
  #if defined(__sun__)
  #  include <sys/isa_defs.h>
 -#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined( __NetBSD__ )
-+#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined( __NetBSD__ ) || defined( __DragonFly__ ) || defined( __Bitrig__ )
++#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined( __NetBSD__ ) || defined( __DragonFly__ )
  #  include <sys/endian.h>
  #elif defined( BSD ) && ( BSD >= 199103 ) || defined( __APPLE__ ) || \
        defined( __CYGWIN__ ) || defined( __DJGPP__ ) || defined( __osf__ )
