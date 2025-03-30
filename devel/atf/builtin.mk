aa43a191a9404cc2aa00a088312b4ee1f6d86f47
wiz <wiz@pkgsrc.org>
2025-03-30 07:51:54 +0000

    atf: update to 0.23.
    ## Changes in version 0.23
    
    Released on March, 29, 2025
    
    * Issue #101: fix release tarballs by using `make dist` instead of
      alternative ad hoc method.
    
    * Issue #76: `atf_check`: fix `std::length_error` thrown from `temp_file`.
    
    * #87: Update documentation/build files to point to FreeBSD repo/mailing lists.
    
    * #86: Improve diagnostics when paths cannot be opened
    
    * #85: various build-related changes. Highlights:
        * Remove leftover references of `auto_array` (bugfix).
        * Remove the `atf-*-api` manpages (enhancement).
        * Require -std=C++14 with ATF (enhancement).
        * Expose `WCOREDUMP(..)` on platforms that support it (bugfix).
        * Add `__attribute__((nonnull))` support (enhancement).
        * Restore `AC_PROG_CPP` (bugfix).
    
    ### General fixes
    
    * Fix various style issues which were blocking `make dist` from completing
      cleanly.
    
    * Fix `atf_utils_file_exists` declaration in atf-c(3).
    
    * Fix various memory leaks and logic errors.
    
    * `atf-test-case(4)`: update reference to the "descr" metadata property so it's
      marked Optional.
    
    * `atf-check.cpp`: remove unnecessary data copy in `temp_file`.
    
    ## Changes in version 0.22
    
    Released on November 25th, 2024.
    
    * Issue #23: Fix double-free triggered by atf_map_insert in low memory
      scenarios, caused by an overlook in the atf_list code.
    
    * Issue #31: Added require.progs metadata properties to the tests that
      need a compiler to run.
    
    * Added the atf_check_not_equal function to atf-sh to check for
      unequal values.
    
    * Add `-r timeout` flag to `atf-check`.
    
    * Open results files before executing tests to fix an issue ATF tests that
      adjust the processes' Capsicum rights as part of the testcase(s)
      executed.
    
    * Add Cirrus CI integration for FreeBSD CI/CD support.
    
    * Address compilation issues on OpenSolaris distributions.
    
    * Replace `auto_array` with `std::vector` (fixes modern C++ compliance).
    
    * Replace `auto_ptr` with `std::shared_ptr` (fixes modern C++ compliance).
    
    * Update autotools idioms and requirements. The minimum required version of
      autoconf is now 2.68.
    
    * Modify `ATF_CHECK*` and `ATF_REQUIRE*` macros to avoid double
      evaluation/allow the gcc toolchain to detect accidental single
      assignment typos.
    
    * Always define CPP to fix use of ATF_BUILD_CPP when the user did not
      define CPP when invoking the configure script.
    
    ### General fixes
    
    * Fix various typos and formatting errors in manual pages and markdown
      documents.

--- devel/atf/builtin.mk
+++ devel/atf/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.1 2017/06/08 13:30:16 jlam Exp $
+# $NetBSD: builtin.mk,v 1.2 2025/03/30 07:51:54 wiz Exp $
 
 BUILTIN_PKG:=		atf
 PKGCONFIG_BASE.atf=	/usr
@@ -7,8 +7,8 @@ PKGCONFIG_FILE.atf=	${PKGCONFIG_BASE.atf}/lib/pkgconfig/atf-sh.pc
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 
 CHECK_BUILTIN.atf?=	no
-.if !empty(CHECK_BUILTIN.atf:M[nN][oO])
-.  if !empty(USE_BUILTIN.atf:M[yY][eE][sS])
+.if ${CHECK_BUILTIN.atf:tl} == no
+.  if ${USE_BUILTIN.atf:tl} == yes
 BUILDLINK_PREFIX.atf=	${PKGCONFIG_BASE.atf}
 BUILDLINK_FILES.atf+=	lib/pkgconfig/atf-c++.pc
 BUILDLINK_FILES.atf+=	lib/pkgconfig/atf-c.pc
