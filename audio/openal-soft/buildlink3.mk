c528a7d2ad2178e4b0aaac97314d907b978e47e6
nia <nia@pkgsrc.org>
2025-03-30 09:33:26 +0000

    openal-soft: Select a suitable version depending on the compiler.

--- audio/openal-soft/buildlink3.mk
+++ audio/openal-soft/buildlink3.mk
@@ -1,6 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/03/24 09:32:38 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/03/30 09:33:26 nia Exp $
 
 BUILDLINK_TREE+=	openal-soft
+
+.include "../../mk/compiler.mk"
+
+.if !empty(CC_VERSION:Mgcc-4.*)
+# this defines OPENAL_SOFT_BUILDLINK3_MK so the next sections aren't used
+.  include "../../audio/openal-soft-c/buildlink3.mk"
+.endif
+
+.if !empty(CC_VERSION:Mgcc-[56789].*) || \
+    !empty(CC_VERSION:Mgcc-10.*) || !empty(CC_VERSION:Mgcc-11.*)
+.  include "../../audio/openal-soft-cxx14/buildlink3.mk"
+.endif
 
 .if !defined(OPENAL_SOFT_BUILDLINK3_MK)
 OPENAL_SOFT_BUILDLINK3_MK:=
