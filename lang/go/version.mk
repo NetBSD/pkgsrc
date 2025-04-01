98862459b6246b4ecb0093afb01bd3afd1ed0a0e
bsiegert <bsiegert@pkgsrc.org>
2025-04-01 17:47:18 +0000

    go119, go121: remove
    go124 is the current version. go123 is still supported.
    
    To build go124 from source, the chain is
    go14 - go118 - go120 - go122 - go124
    
    Thus, nothing needs go119 and go121.
    
    agreed by maya@ on tech-pkg

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.229 2025/04/01 17:44:25 bsiegert Exp $
+# $NetBSD: version.mk,v 1.230 2025/04/01 17:47:18 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -9,9 +9,7 @@
 GO124_VERSION=	1.24.2
 GO123_VERSION=	1.23.8
 GO122_VERSION=	1.22.12
-GO121_VERSION=	1.21.13
 GO120_VERSION=	1.20.14
-GO119_VERSION=	1.19.13
 GO118_VERSION=	1.18.10
 GO14_VERSION=	1.4.3
 
