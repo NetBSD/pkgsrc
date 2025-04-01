4621c9d1e17349af9b29cb2e822473452a09689d
adam <adam@pkgsrc.org>
2025-04-01 10:43:27 +0000

    py-rpds-py: updated to 0.24.0
    0.24.0
    replace quansight-labs/setup-python with actions/setup-python
    noxfile: mark pypy 3.11 as supported

--- devel/py-rpds-py/cargo-depends.mk
+++ devel/py-rpds-py/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.12 2025/02/26 09:49:29 adam Exp $
+# $NetBSD: cargo-depends.mk,v 1.13 2025/04/01 10:43:27 adam Exp $
 
 CARGO_CRATE_DEPENDS+=	archery-1.2.1
 CARGO_CRATE_DEPENDS+=	autocfg-1.3.0
@@ -11,16 +11,16 @@ CARGO_CRATE_DEPENDS+=	memoffset-0.9.1
 CARGO_CRATE_DEPENDS+=	once_cell-1.19.0
 CARGO_CRATE_DEPENDS+=	portable-atomic-1.6.0
 CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.86
-CARGO_CRATE_DEPENDS+=	pyo3-0.23.4
-CARGO_CRATE_DEPENDS+=	pyo3-build-config-0.23.4
-CARGO_CRATE_DEPENDS+=	pyo3-ffi-0.23.4
-CARGO_CRATE_DEPENDS+=	pyo3-macros-0.23.4
-CARGO_CRATE_DEPENDS+=	pyo3-macros-backend-0.23.4
+CARGO_CRATE_DEPENDS+=	pyo3-0.24.0
+CARGO_CRATE_DEPENDS+=	pyo3-build-config-0.24.0
+CARGO_CRATE_DEPENDS+=	pyo3-ffi-0.24.0
+CARGO_CRATE_DEPENDS+=	pyo3-macros-0.24.0
+CARGO_CRATE_DEPENDS+=	pyo3-macros-backend-0.24.0
 CARGO_CRATE_DEPENDS+=	python3-dll-a-0.2.12
 CARGO_CRATE_DEPENDS+=	quote-1.0.36
 CARGO_CRATE_DEPENDS+=	rpds-1.1.0
 CARGO_CRATE_DEPENDS+=	syn-2.0.69
-CARGO_CRATE_DEPENDS+=	target-lexicon-0.12.14
+CARGO_CRATE_DEPENDS+=	target-lexicon-0.13.2
 CARGO_CRATE_DEPENDS+=	triomphe-0.1.13
 CARGO_CRATE_DEPENDS+=	unicode-ident-1.0.12
 CARGO_CRATE_DEPENDS+=	unindent-0.2.3
