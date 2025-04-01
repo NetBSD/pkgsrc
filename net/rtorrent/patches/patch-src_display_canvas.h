cac4ca45ac3c1b7cd13718c076b1462248eec788
adam <adam@pkgsrc.org>
2025-04-01 09:37:54 +0000

    rtorrent: updated to 0.15.2
    0.15.2
    
    Support for colors.
    Add JSON-RPC capability.
    Lua support.
    Close low diskspace normal.
    Fix various macos issues.
    Added new tracker list commands.
    Fix possible crash with save_input_history.
    Added 'system.files.session.fdatasync' config option.

--- net/rtorrent/patches/patch-src_display_canvas.h
+++ net/rtorrent/patches/patch-src_display_canvas.h
@@ -1,24 +1,24 @@
-$NetBSD: patch-src_display_canvas.h,v 1.1 2018/06/24 16:59:52 adam Exp $
+$NetBSD: patch-src_display_canvas.h,v 1.2 2025/04/01 09:37:54 adam Exp $
 
 Make this work with "ncurses" 5.8. Patch taken from:
 http://www.mail-archive.com/arch-general@archlinux.org/msg18728.html
 
---- src/display/canvas.h.orig	2018-06-07 04:25:27.000000000 +0000
+--- src/display/canvas.h.orig	2025-03-28 18:55:13.000000000 +0000
 +++ src/display/canvas.h
-@@ -37,6 +37,7 @@
+@@ -1,6 +1,7 @@
  #ifndef RTORRENT_DISPLAY_CANVAS_H
  #define RTORRENT_DISPLAY_CANVAS_H
  
 +#include <cstdarg>
  #include <string>
+ #include <unordered_map>
  #include <vector>
- 
-@@ -48,7 +49,7 @@ class Canvas {
- public:
-   typedef std::vector<Attributes> attributes_list;
+@@ -15,7 +16,7 @@ public:
+   typedef std::vector<Attributes>      attributes_list;
+   typedef std::unordered_map<int, int> attributes_map;
  
 -  Canvas(int x = 0, int y = 0, int width = 0, int height = 0);
 +  Canvas(int x = 0, int y = 0, int width = 1, int height = 1);
-   ~Canvas() { if (!m_isDaemon) { delwin(m_window); } }
+   ~Canvas();
  
-   void                refresh()                                               { if (!m_isDaemon) { wnoutrefresh(m_window); } }
+   void         refresh();
