0b64a2de1aaf356dbcebeddecf1a2d5da25106f9
bsiegert <bsiegert@pkgsrc.org>
2025-04-01 20:30:09 +0000

    node_exporter: update to 1.9.1
    1.9.1
    
    [BUGFIX] pressure: Fix missing IRQ on older kernels
    [BUGFIX] Fix Darwin memory leak
    
    1.9.0
    
    [CHANGE] meminfo: Convert linux implementation to use procfs lib
    [CHANGE] Update logging to use Go log/slog
    [FEATURE] filesystem: Add node_filesystem_mount_info metric
    [FEATURE] btrfs: Add metrics for commit statistics
    [FEATURE] interrupts: Add collector include/exclude filtering
    [FEATURE] interrupts: Add "exclude zeros" filtering
    [FEATURE] slabinfo: Add filters for slab name.
    [FEATURE] pressure: add IRQ PSI metrics
    [FEATURE] hwmon: Add include and exclude filter for sensors
    [FEATURE] filesystem: Add NetBSD support
    [FEATURE] netdev: Add ifAlias label
    [FEATURE] hwmon: Add Support for GPU Clock Frequencies
    [FEATURE] Add exclude[] URL parameter
    [FEATURE] Add AIX support
    [FEATURE] filesystem: Add fs-types/mount-points include flags
    [FEATURE] netstat: Add collector for tcp packet counters for FreeBSD.
    [ENHANCEMENT] ethtool: Add logging for filtering flags
    [ENHANCEMENT] netstat: Add TCPRcvQDrop to default metrics
    [ENHANCEMENT] diskstats: Add block device rotational
    [ENHANCEMENT] cpu: Support CPU online status
    [ENHANCEMENT] arp: optimize interface name resolution
    [ENHANCEMENT] textfile: Allow specifiying multiple directory globs
    [ENHANCEMENT] filesystem: Add reporting of purgeable space on MacOS
    [ENHANCEMENT] ethtool: Skip full scan of NetClass directories
    [BUGFIX] zfs: Prevent procfs integer underflow
    [BUGFIX] pressure: Fix collection on systems that do not expose a full CPU stat
    [BUGFIX] cpu: Fix FreeBSD 32-bit host support and plug memory leak
    [BUGFIX] hwmon: Add safety check to hwmon read
    [BUGFIX] zfs: Allow space in dataset name

--- sysutils/node_exporter/patches/patch-collector_netdev__common.go
+++ sysutils/node_exporter/patches/patch-collector_netdev__common.go
@@ -1,18 +1,18 @@
-$NetBSD: patch-collector_netdev__common.go,v 1.1 2024/07/28 01:14:24 tnn Exp $
+$NetBSD: patch-collector_netdev__common.go,v 1.2 2025/04/01 20:30:09 bsiegert Exp $
 
 https://github.com/prometheus/node_exporter/pull/3078
 
---- collector/netdev_common.go.orig	2024-07-28 01:10:46.491634800 +0000
+--- collector/netdev_common.go.orig	2025-04-01 15:14:14.000000000 +0000
 +++ collector/netdev_common.go
 @@ -11,9 +11,9 @@
  // See the License for the specific language governing permissions and
  // limitations under the License.
  
--//go:build !nonetdev && (linux || freebsd || openbsd || dragonfly || darwin)
-+//go:build !nonetdev && (linux || freebsd || netbsd || openbsd || dragonfly || darwin)
+-//go:build !nonetdev && (linux || freebsd || openbsd || dragonfly || darwin || aix)
++//go:build !nonetdev && (linux || freebsd || netbsd || openbsd || dragonfly || darwin || aix)
  // +build !nonetdev
--// +build linux freebsd openbsd dragonfly darwin
-+// +build linux freebsd netbsd openbsd dragonfly darwin
+-// +build linux freebsd openbsd dragonfly darwin aix
++// +build linux freebsd netbsd openbsd dragonfly darwin aix
  
  package collector
  
