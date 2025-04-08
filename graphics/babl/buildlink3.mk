807ddd7cbca9628b40a01d03d9a97a668fb6463c
adam <adam@pkgsrc.org>
2025-04-08 11:32:18 +0000

    babl: updated to 0.1.112
    babl-0.1.112
    Fix crash for locales using neither '.' nor ',' as decimal seperator.
    Fix crash for locales using neither '.' nor ',' as decimal seperator.

--- graphics/babl/buildlink3.mk
+++ graphics/babl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/08/14 05:24:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/04/08 11:32:18 adam Exp $
 
 BUILDLINK_TREE+=	babl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	babl
 BABL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.babl+=	babl>=0.1.10
-BUILDLINK_ABI_DEPENDS.babl?=	babl>=0.1.106nb1
+BUILDLINK_ABI_DEPENDS.babl+=	babl>=0.1.106nb1
 BUILDLINK_PKGSRCDIR.babl?=	../../graphics/babl
 
 .include "../../mk/dlopen.buildlink3.mk"
