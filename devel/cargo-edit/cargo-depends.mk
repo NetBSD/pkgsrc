56f55bbd57535587d793da2a4abc06ade4b74c21
pin <pin@pkgsrc.org>
2025-04-10 11:45:13 +0000

    devel/cargo-edit: update to 0.13.2
    0.13.2 - 2025-03-17
    Fixes
    
     - (upgrade) Don't silence network errors

--- devel/cargo-edit/cargo-depends.mk
+++ devel/cargo-edit/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.1 2025/01/24 13:25:56 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.2 2025/04/10 11:45:13 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
