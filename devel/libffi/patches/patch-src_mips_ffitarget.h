2f2bd506de346d0a6b4161d84c75503ee43f3dc7
adam <adam@pkgsrc.org>
2025-04-10 09:47:49 +0000

    libffi: updated to 3.4.8
    3.4.8
    
    aarch64: add PAC to GNU Notes
    MIPS: Dont import asm/sgidefs.h on linux
    Update the Simple Example from the Docs to fix a compile error
    Fix bugs in the x86-64 and x32 target
    Add the "ABI_ATTR" attribute to called functions
    powerpc: Add static trampoline support
    testsuite: add two tests to Makefile.am

--- devel/libffi/patches/patch-src_mips_ffitarget.h
+++ devel/libffi/patches/patch-src_mips_ffitarget.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_mips_ffitarget.h,v 1.2 2021/12/08 08:20:17 adam Exp $
+$NetBSD: patch-src_mips_ffitarget.h,v 1.3 2025/04/10 09:47:49 adam Exp $
 
 Make a minimal adjustment to support the various NetBSD mips ports.
 
---- src/mips/ffitarget.h.orig	2021-06-27 15:17:08.000000000 +0000
+--- src/mips/ffitarget.h.orig	2025-03-27 00:35:35.000000000 +0000
 +++ src/mips/ffitarget.h
-@@ -41,8 +41,10 @@
+@@ -39,8 +39,10 @@
  #define _MIPS_SIM_ABI32		1
  #define _MIPS_SIM_NABI32	2
  #define _MIPS_SIM_ABI64		3
--#elif !defined(__OpenBSD__) && !defined(__FreeBSD__)
+-#elif !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(__linux__)
 +#elif defined(__sgi)
  # include <sgidefs.h>
 +#else
