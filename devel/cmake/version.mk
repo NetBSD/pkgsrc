d0a660d50443d342b8c8e9b74ed38127fbcb5288
adam <adam@pkgsrc.org>
2025-04-11 14:02:46 +0000

    cmake cmake-gui: updated to 3.31.7
    CMake 3.31.7
    * Tests: Teach RunCMake to ignore GNU ld --warn-rwx-segments warnings
    * cmCurl: Avoid using undocumented type for CURLOPT_NETRC values
    * MSVC: Split C++23 flag selection into dedicated block
    * file(MAKE_DIRECTORY): Do not make directories for command keywords
    * file(MAKE_DIRECTORY): Clarify formatting of unexpected arguments error
    * cmXCodeObject: Drop unused CoreFoundation header
    * cmake-gui: Fix regression that prints an incidental error on fresh build trees
    * MARMASM: Fix loading linker information
    * Linker: Honor CMAKE_<LANG>_LINK_LIBRARY_USING_FEATURE_SUPPORTED when FALSE
    * Linker: Save linker inspection results with compiler inspection results
    * Linker: Save GNU push-/pop-state detection with compiler inspection results
    * Linker: Detect GNU push-state/pop-state flags more robustly
    * CUDAToolkit: Restore ability to find cupti library
    * Help: Add 3.31 release note for CMAKE_CONFIG_DIR
    * macOS: Define _DARWIN_C_SOURCE in more sources
    * Source: Include specific CoreFoundation headers instead of umbrella header

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.75 2025/02/25 16:12:56 adam Exp $
+# $NetBSD: version.mk,v 1.76 2025/04/11 14:02:46 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.6
+CMAKE_VERSION=	3.31.7
 CMAKE_API=	${CMAKE_VERSION:R}
