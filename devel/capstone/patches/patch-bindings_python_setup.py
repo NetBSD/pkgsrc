c2df1ba02257fb680f724830bde11b8621d0fb9b
adam <adam@pkgsrc.org>
2025-04-12 09:44:00 +0000

    capstone py-capstone: updated to 5.0.6
    Version 5.0.6: March 23th, 2025
    
    What's Changed
    * V5 - Fix sdist build and add CI job for testing it.
    * Build Tarball before DEB/RPM package.
    * Fix cmake error when build with capstone as submodule
    * [v5] Tms32c64x little endian
    
    New Contributors
    * @WerWolv made their first contribution in https://github.com/capstone-engine/capstone/pull/2619
    
    Version 5.0.5: January 16th, 2025
    
    What's Changed
    * Fix code missing in v5.0.4
    
    Version 5.0.4: January 16th, 2025
    
    What's Changed
    * [v5][SuperH] Fix missing setting detail->sh
    * tricore: fixes 2386 in v5
    * Creating a functional Debian Package for Capstone v5
    * Duplicate of 2590 for v5 Branch
    * Update version to v5.0.4

--- devel/capstone/patches/patch-bindings_python_setup.py
+++ devel/capstone/patches/patch-bindings_python_setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-bindings_python_setup.py,v 1.4 2024/09/16 10:04:50 adam Exp $
+$NetBSD: patch-bindings_python_setup.py,v 1.5 2025/04/12 09:44:00 adam Exp $
 
---- bindings/python/setup.py.orig	2024-08-20 14:41:54.000000000 +0000
+--- bindings/python/setup.py.orig	2025-03-23 15:48:02.000000000 +0000
 +++ bindings/python/setup.py
-@@ -150,14 +150,6 @@ def build_libraries():
+@@ -147,10 +147,6 @@ def build_libraries():
          else:
              os.system('cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCAPSTONE_BUILD_TESTS=OFF -DCAPSTONE_BUILD_CSTOOL=OFF -G "Unix Makefiles" ..')
          os.system("cmake --build .")
@@ -10,10 +10,6 @@ $NetBSD: patch-bindings_python_setup.py,v 1.4 2024/09/16 10:04:50 adam Exp $
 -        os.system("CAPSTONE_BUILD_CORE_ONLY=yes bash ./make.sh")
 -
 -    shutil.copy(VERSIONED_LIBRARY_FILE, os.path.join(LIBS_DIR, LIBRARY_FILE))
--
--    # only copy static library if it exists (it's a build option)
--    if STATIC_LIBRARY_FILE and os.path.exists(STATIC_LIBRARY_FILE):
--        shutil.copy(STATIC_LIBRARY_FILE, LIBS_DIR)
      os.chdir(cwd)
  
  
