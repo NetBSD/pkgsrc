43eef0dad6a3bd8b1c49123fa44cd76888181359
pin <pin@pkgsrc.org>
2025-04-13 15:28:08 +0000

    audio/spotifyd: update to 0.4.1
    0.4.1
    Fixed
    
        revert default features to alsa_backend (#1337)
        do not require bindgen dependencies on supported systems (#1340)
        always return exitcode 1 on error (#1338)
        the released binaries for linux-x86_64 also work for older systems (#1342)
    
    0.4.0
    Breaking
    
        username and password authentaction have been removed and are replaced by OAuth (#1321) Look at the docs for setup instructions.
    
    Added
    
        retry enabling discovery several times before exiting (#1228)
        development artifacts are now available (#1332)
        prebuilt binaries are available for aarch64 (both Linux and macOS) (#1332)
        Windows and Android are now supported (#1219, #1314)
        expose pipe and subprocess backends (#1284)
        new device type options are available (#1316)
        OAuth login is now supported (#1321)
        a warning is emitted on unsupported configuration values (#1327)
    
    Changed
    
        the docs have been overhauled to reflect the current state of the project and simplify onboarding (#1327)
        librespot dependency has been updated to 0.6 (#1317)
        Credential caching has been re-enabled. (#1214)
    
    Fixed
    
        alsa mixer: volume calculation has been fixed (#1229)
    
    Removed
    
        armv6 binaries are no longer available by default (#1332) If you still need them, please join our matrix channel (https://matrix.to/#/#spotifyd:matrix.org) and tell us!

--- audio/spotifyd/patches/patch-src_utils.rs
+++ audio/spotifyd/patches/patch-src_utils.rs
@@ -1,15 +1,14 @@
-$NetBSD: patch-src_utils.rs,v 1.1 2023/07/13 20:38:53 pin Exp $
+$NetBSD: patch-src_utils.rs,v 1.2 2025/04/13 15:28:08 pin Exp $
 
 Allow building on NetBSD.
 
---- src/utils.rs.orig	2023-04-20 21:37:30.000000000 +0000
+--- src/utils.rs.orig	2025-04-13 15:09:04.578021815 +0000
 +++ src/utils.rs
-@@ -1,7 +1,7 @@
- use log::trace;
- use std::env;
- 
--#[cfg(any(target_os = "freebsd", target_os = "linux"))]
-+#[cfg(any(target_os = "netbsd", target_os = "freebsd", target_os = "linux"))]
- fn get_shell_ffi() -> Option<String> {
-     use libc::{geteuid, getpwuid_r};
-     use std::{ffi::CStr, mem, ptr};
+@@ -4,6 +4,7 @@ use std::{env, time::Duration};
+ #[cfg(any(
+     target_os = "freebsd",
+     target_os = "linux",
++    target_os = "netbsd",
+     target_os = "openbsd",
+     target_os = "android"
+ ))]
