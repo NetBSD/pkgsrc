93b46365f7243c7e88b7f0bff6e278f8e1fdf678
wiz <wiz@pkgsrc.org>
2025-04-13 18:36:18 +0000

    khal: update to 0.11.4.
    0.11.4
    ======
    2025-04-10
    
    * UPDATED REQUIREMENT urwid is now required >= 2.6.15
    * NEW REQUIREMENT for tests on python >= 3.12: pkg_resources
    * optimization in ikhal when editing events in the far future or past
    * FIX an issue in ikhal with updating the view of the event list after editing
      an event
    * NEW properties of ikhal themes (dark and light) can now be overriden from the
      config file (via the new [palette] section, check the documenation)
    * NEW timedelta strings can now have a leading ``+``, e.g. ``+1d``
    * NEW Add ``--json`` option to output event data as JSON objects
    * NEW Add default alarms configuration option
    * FIX defaults for ``default_event_duration`` and ``default_dayevent_duration``
      where mixed up, ``default_dayevent_duration`` is the default for all-day events
    * NEW event format option ``status-symbol`` which represents the status of an
      event with a symbol (e.g. ``✓`` for confirmed, ``✗`` for cancelled, ``?`` for
      tentative)
    * NEW event format option ``partstat-symbol`` which represents the participation
      status of an event with a symbol (e.g. ``✓`` for accepted, ``✗`` for declined,
      ``?`` for tentative); partication status is shown for the email addresses
      configured for the event's calendar
    * NEW support for color theme, command, and formatter plugins
    * FIX an issue where ikhal would forget changes to time or date fields if you
      left the field with page up/down or meta+enter
    * NEW support python 3.13
    * CHANGE various UI improvments to ikhal.
    * FIX Deleting multiple of instances of a recurring event in ikhal
    * NEW Add ``enable_mouse`` configuration option.
    * CHANGE the ``atomicwrites`` library is no longer required.

--- time/khal/patches/patch-khal_icalendar.py
+++ time/khal/patches/patch-khal_icalendar.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-khal_icalendar.py,v 1.1 2024/10/31 13:35:05 leot Exp $
+$NetBSD: patch-khal_icalendar.py,v 1.2 2025/04/13 18:36:18 wiz Exp $
 
 Make it compatible with icalendar>=6.0.0.
 
@@ -6,7 +6,7 @@ Shared upstream via:
 
  <https://github.com/pimutils/khal/pull/1364>
 
---- khal/icalendar.py.orig	2024-10-31 13:32:32.407885997 +0000
+--- khal/icalendar.py.orig	2025-04-10 21:43:33.000000000 +0000
 +++ khal/icalendar.py
 @@ -37,6 +37,10 @@ from .utils import generate_random_uid, 
  
@@ -17,9 +17,9 @@ Shared upstream via:
 +icalendar.use_pytz()
 +
  
- def split_ics(ics: str, random_uid: bool=False, default_timezone=None):
+ def split_ics(ics: str, random_uid: bool=False, default_timezone=None) -> List:
      """split an ics string into several according to VEVENT's UIDs
-@@ -56,8 +60,8 @@ def split_ics(ics: str, random_uid: bool
+@@ -55,8 +59,8 @@ def split_ics(ics: str, random_uid: bool
          # Standard Time' for 'Pacific/Auckland' in Olson format), we convert any
          # Windows format timezones to Olson.
          if item.name == 'VTIMEZONE':
