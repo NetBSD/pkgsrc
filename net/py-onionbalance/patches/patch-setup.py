f54b621cb5391736e554f35f90edd065a39a8b1e
wiz <wiz@pkgsrc.org>
2025-04-13 08:05:55 +0000

    py-onionbalance: update to 0.2.3.
    ## v0.2.3 - 2024-12-11
    
    * Maintenance:
        * Fix used-before-assignment ([tpo/onion-services/onionbalance!4][]).
        * Fix import on functional tests ([tpo/onion-services/onionbalance!5][]).
        * Drop dependency on future (thanks to Lumir Balhar) ([tpo/onion-services/onionbalance!6][]).
        * The v2 codebase was finally removed (thanks to Federico Ceratto)
          ([tpo/onion-services/onionbalance#8][]).
        * Fix coding style issues found by flake8 ([tpo/onion-services/onionbalance!9][]).
    * Workflow:
        * CI was fixed and updated, and now uses [GitLab CI][].
        * Added Git tags to all previous releases.
    * Repository:
        * The Onionbalance repository was moved to
          [gitlab.torproject.org/tpo/onion-services/onionbalance][onionbalance-repo].
        * The old repository at [github.com/torproject/onionbalance][old-repository] is
          configured as a mirror.
    * Documentation:
        * Onionbalance documentation was migrated to [Onion MkDocs][], then updated and
          included in the [Onion Services Ecosystem][ecosystem]
          ([tpo/onion-services/onionbalance#28][]).
        * New canonical documentation URL is
          [onionservices.torproject.org/apps/base/onionbalance][onionbalance-docs].
          The old documentation URLs are kept as redirects
          (https://onionbalance-v3.readthedocs.io and
          https://onionbalance.readthedocs.io).
        * Updated this ChangeLog to include dates for all releases.
    
    ## v0.2.2 - 2021-07-29
    
    * Add an OBv3 hacking guide.
    * Remove tox and simplify build procedure.
    * A single OnionBalance can now support multiple onion services.

--- net/py-onionbalance/patches/patch-setup.py
+++ net/py-onionbalance/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2023/07/13 07:30:18 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2025/04/13 08:05:55 wiz Exp $
 
 Replace pycryptodomex with pycryptodome.
 
---- setup.py.orig	2021-01-22 15:01:09.000000000 +0000
+--- setup.py.orig	2025-04-09 15:31:17.000000000 +0000
 +++ setup.py
-@@ -46,7 +46,7 @@ setup(
+@@ -47,7 +47,7 @@ setup(
          'setuptools',
          'stem>=1.8',
          'PyYAML>=4.2b1',
 -        'pycryptodomex',
 +        'pycryptodome',
-         'future>=0.14.0',
          'setproctitle',
          'cryptography>=2.5',
+     ],
