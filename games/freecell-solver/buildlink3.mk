6cd914124505786fc9de437df2c1ddeb170f8633
adam <adam@pkgsrc.org>
2025-04-16 20:43:41 +0000

    freecell-solver: updated to 6.12.0
    Version 6.12.0: (30-Jun-2024)
    
    1. Avoid packaging /.tidyall.d ( https://github.com/shlomif/fc-solve/issues/99 )
    2. Renames to "global_instance" and clean-ups.
    
    Version 6.10.0: (13-Jun-2024)
    
    1. Add experimental functions in FC_SOLVE_INSTANCE_PARAMS_QUERYING .
    2. Fewer compile-time / lint-time warnings .
    3. Convert some regular expressions from +\d+ to +[0-9]+ .
    
    Version 6.8.0: (20-Oct-2022)
    
    1. Workaround an ldd regression: https://github.com/shlomif/fc-solve/issues/87 .
    2. OpenCL deal finder cleanups.
    3. Made the tests suite pass on more CMake options' configurations.
    4. Cleanups.
    
    Version 6.6.0: (09-Oct-2021)
    
    1. Fixed the Windows binaries builds on GitHub Actions.
    2. Avoid the need of pyperl in the OpenCL code.
    3. Silence some compiler warnings.

--- games/freecell-solver/buildlink3.mk
+++ games/freecell-solver/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2023/08/14 05:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/04/16 20:43:41 adam Exp $
 
 BUILDLINK_TREE+=	freecell-solver
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	freecell-solver
 FREECELL_SOLVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.freecell-solver+=	freecell-solver>=5.14.0
-BUILDLINK_ABI_DEPENDS.freecell-solver?=	freecell-solver>=5.14.0nb6
+BUILDLINK_ABI_DEPENDS.freecell-solver+=	freecell-solver>=5.14.0nb6
 BUILDLINK_PKGSRCDIR.freecell-solver?=	../../games/freecell-solver
 .endif	# FREECELL_SOLVER_BUILDLINK3_MK
 
