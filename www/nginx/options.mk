41ae6e58f8786d8afb1a65890cf6d86f97602b96
osa <osa@pkgsrc.org>
2025-04-17 15:14:32 +0000

    */*: update NGINX Javascript from 0.8.9 to 0.8.10
    While I'm here, cleanup pcre1 dependency, since it's EoLed, so
    please use pcre2 instead.
    
    Bump PKGREVISIONs where it needed.
    
    <ChangeLog>
    
    nginx modules:
    
    *) Feature: reading r.requestText or r.requestBuffer from
       a temp file.
       Previously, an exception was thrown when accessing r.requestText
       or r.requestBuffer if a client request body size exceeded
       client_body_buffer_size.
    
    *) Improvement: improved reporting of unhandled promise rejections.
    
    *) Bugfix: fixed name corruption in variable and header processing.
    
    *) Bugfix: fixed SharedDict.incr() with empty init argument
       for QuickJS engine.
    
    *) Bugfix: accepting response headers with underscore characters
       in Fetch API.
    
    Core:
    
    *) Change: fixed serializeToString().
       Previously, serializeToString() was exclusiveC14n() which returned
       string instead of Buffer. According to the published documentation it
       should be c14n().
    
    *) Feature: added WebCrypto API for QuickJS engine.
    
    *) Feature: added TextEncoder/TextDecoder for QuickJS engine.
    
    *) Feature: added querystring module for QuickJS engine.
    
    *) Feature: added crypto module for QuickJS engine.
    
    *) Feature: added xml module for QuickJS engine.
    
    *) Feature: added support for QuickJS-NG library.
    
    *) Bugfix: fixed buffer.concat() with a single argument in quickjs.
    
    *) Bugfix: added missed syntax error for await in template literal.
    
    *) Bugfix: fixed non-NULL terminated strings formatting in
       exceptions for QuickJS engine.
    
    *) Bugfix: fixed compatibility with recent change in QuickJS
       and QuickJS-NG.
    
    </ChangeLog>

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.115 2025/02/07 09:00:23 adam Exp $
+# $NetBSD: options.mk,v 1.116 2025/04/17 15:14:32 osa Exp $
 
 CODELOAD_SITE_GITHUB=		https://codeload.github.com/
 
@@ -193,7 +193,7 @@ LUA_DISTNAME=		lua-nginx-module-${LUA_VERSION}
 LUA_DISTFILE=		${LUA_DISTNAME}.tar.gz
 SITES.${LUA_DISTFILE}=	-${MASTER_SITE_GITHUB:=openresty/lua-nginx-module/archive/}v${LUA_VERSION}.tar.gz
 DISTFILES+=		${LUA_DISTFILE}
-.include "../../devel/pcre/buildlink3.mk"
+.include "../../devel/pcre2/buildlink3.mk"
 BUILDLINK_API_DEPENDS.LuaJIT2+=	LuaJIT2>=2.1
 .include "../../lang/LuaJIT2/buildlink3.mk"
 DEPENDS+=		lua-resty-core>=0.1.27nb1:../../www/lua-resty-core
@@ -360,7 +360,7 @@ PLIST.rtmp=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mnginx-njs) || make(makesum) || make(mdi) || make(distclean)
-NJS_VERSION=		0.8.9
+NJS_VERSION=		0.8.10
 NJS_DISTNAME=		njs-${NJS_VERSION}
 NJS_DISTFILE=		${NJS_DISTNAME}.tar.gz
 SITES.${NJS_DISTFILE}=	-${MASTER_SITE_GITHUB:=nginx/njs/archive/}${NJS_VERSION}.tar.gz
