97cdfb1ffc1743cfd46fe34d191ce709356556f8
wiz <wiz@pkgsrc.org>
2025-04-18 22:23:08 +0000

    xhash: update to 3.6.0.
    Provided by Ricardo Branco in PR 58950.
    
    Changes:
    - Update to Go 1.24
    - Update dependencies
    - Support algorithms in lowercase with -c option
    - Prevent panic on invalid digest

--- security/xhash/go-modules.mk
+++ security/xhash/go-modules.mk
@@ -1,14 +1,14 @@
-# $NetBSD: go-modules.mk,v 1.3 2024/10/20 16:31:43 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.4 2025/04/18 22:23:08 wiz Exp $
 
 GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.mod
 GO_MODULE_FILES+=	github.com/klauspost/cpuid/v2/@v/v2.0.12.zip
-GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.mod
-GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.5.zip
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.6.mod
+GO_MODULE_FILES+=	github.com/spf13/pflag/@v/v1.0.6.zip
 GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.4.mod
 GO_MODULE_FILES+=	github.com/zeebo/blake3/@v/v0.2.4.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.28.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.28.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.8.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.26.0.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.37.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.37.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.13.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.13.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.32.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.32.0.zip
