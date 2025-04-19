b06327c418022cc685320e419d2c6e94c50cf9df
tsutsui <tsutsui@pkgsrc.org>
2025-04-19 23:14:24 +0000

    xv: apply several fixes from forked upstream.
    - https://gitlab.com/DavidGriffith/xv/-/commit/60a25db1
      > Updated xvbmp.c to support reading the three newer versions of BMP format.
    - https://gitlab.com/DavidGriffith/xv/-/commit/b91710b5
      > Fix off-by-one's resulting in dereferencing out-of-bound memory.
    - https://gitlab.com/DavidGriffith/xv/-/commit/a84406cb
      > Add XrandR support.
    - https://gitlab.com/DavidGriffith/xv/-/commit/72c80bff
      > Fix off-by-one in smoothing code and use better interpolation/rounding.
    - https://gitlab.com/DavidGriffith/xv/-/commit/f9d46bd9
      > Eliminate flicker
    
    Bump PKGREVISION.

--- graphics/xv/patches/patch-xv.c
+++ graphics/xv/patches/patch-xv.c
@@ -1,11 +1,36 @@
-$NetBSD: patch-xv.c,v 1.1 2023/07/30 07:55:45 tsutsui Exp $
+$NetBSD: patch-xv.c,v 1.2 2025/04/19 23:14:24 tsutsui Exp $
 
 - add webp support from forked upstream
   https://gitlab.com/DavidGriffith/xv/-/commit/5682a07e
+- pull XRandR support from forked upstream
+  https://gitlab.com/DavidGriffith/xv/-/commit/a84406cb
+- pull flicker fix from forked upstream
+  https://gitlab.com/DavidGriffith/xv/-/commit/f9d46bd9
 
---- xv.c.orig	2023-07-30 04:38:20.015663071 +0000
+--- xv.c.orig	2025-04-19 21:59:18.981323758 +0000
 +++ xv.c
-@@ -337,6 +337,10 @@ int main(argc, argv)
+@@ -140,6 +140,9 @@ static int  argcmp                   PAR
+                                            int, int, int *));
+ static void add_filelist_to_namelist PARM((char *, char **, int *, int));
+ 
++#ifdef HAVE_XRR
++extern int RRevent_number, RRerror_number;
++#endif
+ 
+ /* formerly local vars in main, made local to this module when
+    parseResources() and parseCmdLine() were split out of main() */
+@@ -169,6 +172,10 @@ int main(argc, argv)
+ #ifdef TV_L10N
+   int    j;
+ #endif
++#ifdef HAVE_XRR
++  int major = -1, minor = -1;
++  int nScreens;
++#endif
+   XColor ecdef;
+   Window rootReturn, parentReturn, *children;
+   unsigned int numChildren, rootDEEP;
+@@ -337,6 +344,10 @@ int main(argc, argv)
    pngW = (Window) NULL;  pngUp = 0;
  #endif
  
@@ -16,7 +41,7 @@ $NetBSD: patch-xv.c,v 1.1 2023/07/30 07:55:45 tsutsui Exp $
    pcdW = (Window) NULL;  pcdUp = 0;
  
  #ifdef HAVE_PIC2
-@@ -980,6 +984,11 @@ int main(argc, argv)
+@@ -980,6 +991,11 @@ int main(argc, argv)
    XSetTransientForHint(theDisp, pngW, dirW);
  #endif
  
@@ -28,7 +53,47 @@ $NetBSD: patch-xv.c,v 1.1 2023/07/30 07:55:45 tsutsui Exp $
  #ifdef HAVE_PCD
    CreatePCDW();
    XSetTransientForHint(theDisp, pcdW, dirW);
-@@ -1870,6 +1879,9 @@ static void cmdSyntax()
+@@ -1030,8 +1046,26 @@ int main(argc, argv)
+   /* make std colormap, maybe */
+   ChangeCmapMode(colorMapMode, 0, 0);
+ 
++  /* Xrandr */
++#ifdef HAVE_XRR
++  if (!XRRQueryExtension(theDisp, &RRevent_number, &RRerror_number)) {
++    major = -1;
++  } else {
++    if (DEBUG) fprintf(stderr, "XRRQueryExtension: %d, %d\n", RRevent_number, RRerror_number);
+ 
++    if (!XRRQueryVersion(theDisp, &major, &minor)) {
++      if (DEBUG) fprintf(stderr, "XRRQueryVersion failed!\n");
++    } else {
++      if (DEBUG) fprintf(stderr, "XRRQueryVersion: %d, %d\n", major, minor);
++    }
+ 
++    nScreens = ScreenCount(theDisp);
++
++    for (i = 0; i < nScreens; i++) {
++      XRRSelectInput(theDisp, RootWindow(theDisp, i), RRScreenChangeNotifyMask);
++    }
++  }
++#endif
+ 
+   /* Do The Thing... */
+   mainLoop();
+@@ -1182,10 +1216,10 @@ static void useOtherVisual(vinfo, best)
+     XFlush(theDisp);
+     XSync(theDisp, False);
+ 
+-    xswa.background_pixel = 0;
++    xswa.background_pixmap = None;
+     xswa.border_pixel     = 1;
+     xswa.colormap         = theCmap;
+-    xswamask = CWBackPixel | CWBorderPixel | CWColormap;
++    xswamask = CWBackPixmap | CWBorderPixel | CWColormap;
+ 
+     win = XCreateWindow(theDisp, rootW, 0, 0, 100, 100, 2, (int) dispDEEP,
+ 			InputOutput, theVisual, xswamask, &xswa);
+@@ -1870,6 +1904,9 @@ static void cmdSyntax()
  #ifdef HAVE_PNG
    VersionInfoPNG();
  #endif
@@ -38,7 +103,7 @@ $NetBSD: patch-xv.c,v 1.1 2023/07/30 07:55:45 tsutsui Exp $
    /* pbm/pgm/ppm support is native, not via pbmplus/netpbm libraries */
    fprintf(stderr, "\n");
  
-@@ -3121,6 +3133,15 @@ int ReadFileType(fname)
+@@ -3121,6 +3158,15 @@ int ReadFileType(fname)
             magicno[2]=='N'  && magicno[3]=='G')               rv = RFT_PNG;
  #endif
  
@@ -54,7 +119,7 @@ $NetBSD: patch-xv.c,v 1.1 2023/07/30 07:55:45 tsutsui Exp $
  #ifdef HAVE_PDS
    else if (strncmp((char *) magicno,  "NJPL1I00", (size_t) 8)==0 ||
  	   strncmp((char *) magicno+2,"NJPL1I",   (size_t) 6)==0 ||
-@@ -3258,6 +3279,10 @@ int ReadPicFile(fname, ftype, pinfo, qui
+@@ -3258,6 +3304,10 @@ int ReadPicFile(fname, ftype, pinfo, qui
    case RFT_PNG:     rv = LoadPNG   (fname, pinfo);         break;
  #endif
  
@@ -65,3 +130,22 @@ $NetBSD: patch-xv.c,v 1.1 2023/07/30 07:55:45 tsutsui Exp $
  #ifdef HAVE_PDS
    case RFT_PDSVICAR: rv = LoadPDS  (fname, pinfo);         break;
  #endif
+@@ -3890,7 +3940,7 @@ static void createMainWindow(geom, name)
+   hints.flags |= PSize | PMaxSize;
+ 
+   xswa.bit_gravity      = StaticGravity;
+-  xswa.background_pixel = bg;
++  xswa.background_pixmap  = None;
+   xswa.border_pixel     = fg;
+   xswa.colormap         = theCmap;
+ 
+@@ -3905,7 +3955,8 @@ static void createMainWindow(geom, name)
+      that windows, by default, have backing-store turned on, then the
+      image window will, too */
+ 
+-  xswamask = CWBackPixel | CWBorderPixel | CWColormap /* | CWBackingStore */;
++  /* CWBackPixel */
++  xswamask = CWBackPixmap | CWBorderPixel | CWColormap /* | CWBackingStore */;
+   if (!clearonload) xswamask |= CWBitGravity;
+ 
+   if (mainW) {
