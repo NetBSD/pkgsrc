b694c49df419393fddee71dd0dd810b6433f4ef4
wiz <wiz@pkgsrc.org>
2025-04-19 07:58:06 +0000

    *: recursive bump for default Kerberos implementation switch

--- net/gssdp/buildlink3.mk
+++ net/gssdp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.48 2025/04/17 21:51:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2025/04/19 07:58:10 wiz Exp $
 
 BUILDLINK_TREE+=	gssdp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gssdp
 GSSDP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=1.6
-BUILDLINK_ABI_DEPENDS.gssdp+=	gssdp>=1.6.2nb7
+BUILDLINK_ABI_DEPENDS.gssdp+=	gssdp>=1.6.2nb8
 BUILDLINK_PKGSRCDIR.gssdp?=	../../net/gssdp
 
 .include "../../devel/glib2/buildlink3.mk"
--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.49 2025/04/17 21:51:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.50 2025/04/19 07:58:12 wiz Exp $
 
 BUILDLINK_TREE+=	libsoup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libsoup
 LIBSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsoup+=	libsoup>=2.50.0
-BUILDLINK_ABI_DEPENDS.libsoup+=	libsoup>=2.74.3nb9
+BUILDLINK_ABI_DEPENDS.libsoup+=	libsoup>=2.74.3nb10
 
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 
