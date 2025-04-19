c71675e5ab1d792c3826a05d0eca7de6f88b4fe4
rhialto <rhialto@pkgsrc.org>
2025-04-19 12:54:00 +0000

    math/hercules4sdl-softfloat: update to 20250325
    This adds some extra functions needed for the new Hercules SDL 4.8
    (they were apparently left out previously).

--- math/hercules4sdl-softfloat/buildlink3.mk
+++ math/hercules4sdl-softfloat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/11/06 16:40:06 rhialto Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/04/19 12:54:00 rhialto Exp $
 
 BUILDLINK_TREE+=	hercules4sdl-softfloat
 
@@ -7,7 +7,7 @@ HERCULES4SDL_SOFTFLOAT_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hercules4sdl-softfloat?=	build
 
-BUILDLINK_API_DEPENDS.hercules4sdl-softfloat+=	hercules4sdl-softfloat>=3.5.0
+BUILDLINK_API_DEPENDS.hercules4sdl-softfloat+=	hercules4sdl-softfloat>=3.5.0nb20250325
 BUILDLINK_PKGSRCDIR.hercules4sdl-softfloat?=	../../math/hercules4sdl-softfloat
 .endif	# HERCULES4SDL_SOFTFLOAT_BUILDLINK3_MK
 
