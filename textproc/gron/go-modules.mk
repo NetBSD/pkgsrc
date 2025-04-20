44945dcea5180cb27c101a98e1aa72f7a20d3efb
kim <kim@pkgsrc.org>
2025-04-20 05:56:52 +0000

    gron: Add gron 0.7.1
    gron is a tool that transforms JSON (JavaScript Object Notation) into
    discrete assignments. This flattened format makes it easier to grep for
    specific values within the JSON structure and clearly shows the path to
    each value.

---
+++ textproc/gron/go-modules.mk
@@ -0,0 +1,14 @@
+# $NetBSD: go-modules.mk,v 1.1 2025/04/20 05:56:53 kim Exp $
+
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.7.0.mod
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.7.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.0.9.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.0.9.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.4.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.4.zip
+GO_MODULE_FILES+=	github.com/nwidger/jsoncolor/@v/v0.0.0-20170215171346-75a6de4340e5.mod
+GO_MODULE_FILES+=	github.com/nwidger/jsoncolor/@v/v0.0.0-20170215171346-75a6de4340e5.zip
+GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.8.0.mod
+GO_MODULE_FILES+=	github.com/pkg/errors/@v/v0.8.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220412211240-33da011f77ad.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220412211240-33da011f77ad.zip
