37bf833ab4b1a796d1765675061c83d0c44da768
wiz <wiz@pkgsrc.org>
2025-04-21 09:23:45 +0000

    meson: always use absolute paths in -rpath
    This was made conditional on old linkers in 1.7.2, and broke the build
    of gimp-devel.
    
    Bump PKGREVISION.

--- devel/meson/patches/patch-mesonbuild_linkers_linkers.py
+++ devel/meson/patches/patch-mesonbuild_linkers_linkers.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-mesonbuild_linkers_linkers.py,v 1.1 2022/02/22 17:56:53 jperkin Exp $
+$NetBSD: patch-mesonbuild_linkers_linkers.py,v 1.2 2025/04/21 09:23:45 wiz Exp $
 
-Avoid thin archives on SunOS.
+1. Avoid thin archives on SunOS.
+2. Always use absolute rpaths (per pkgsrc policy)
 
---- mesonbuild/linkers/linkers.py.orig	2022-01-02 20:12:32.000000000 +0000
+--- mesonbuild/linkers/linkers.py.orig	2025-04-03 17:13:56.000000000 +0000
 +++ mesonbuild/linkers/linkers.py
-@@ -197,7 +197,7 @@ class ArLinker(ArLikeLinker):
+@@ -372,7 +372,7 @@ class ArLinker(ArLikeLinker, StaticLinke
          thinargs = ''
          if '[D]' in stdo:
              stdargs += 'D'
@@ -13,3 +14,12 @@ Avoid thin archives on SunOS.
              thinargs = 'T'
          self.std_args = [stdargs]
          self.std_thin_args = [stdargs + thinargs]
+@@ -756,7 +756,7 @@ class GnuLikeDynamicLinkerMixin(DynamicL
+         # ...instead of just one single looooong option, like this:
+         #
+         #   -Wl,-rpath-link,/path/to/folder1:/path/to/folder2:...
+-        if self.id in {'ld.bfd', 'ld.gold'} and mesonlib.version_compare(self.version, '<2.28'):
++        if self.id in {'ld.bfd', 'ld.gold'} and mesonlib.version_compare(self.version, '<2.28') or True:
+             for p in rpath_paths:
+                 args.extend(self._apply_prefix('-rpath-link,' + os.path.join(build_dir, p)))
+ 
