3faa093c4f0ecc71582bfcef37e562ea082eb0f7
wiz <wiz@pkgsrc.org>
2025-04-21 16:04:30 +0000

    libares: remove Interix support

--- net/libares/buildlink3.mk
+++ net/libares/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:25:09 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/04/21 16:04:30 wiz Exp $
 
 BUILDLINK_TREE+=	libares
 
@@ -11,12 +11,6 @@ BUILDLINK_API_DEPENDS.libares+=	libares>=1.1.1
 BUILDLINK_PKGSRCDIR.libares?=	../../net/libares
 BUILDLINK_DEPMETHOD.libares?=	build
 
-.if ${OPSYS} == "Interix"
-BUILDLINK_PASSTHRU_DIRS+=	/usr/local/include/bind /usr/local/lib/bind
-BUILDLINK_CPPFLAGS.libares+=	-I/usr/local/include/bind
-BUILDLINK_LDFLAGS.libares+=	-L/usr/local/lib/bind
-BUILDLINK_LIBS.libares+=	-lbind -ldb
-.endif
 .endif # LIBARES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libares
