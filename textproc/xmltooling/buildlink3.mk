426d4b8f2e1db5875924c780ec65f64f13a8db53
perseant <perseant@pkgsrc.org>
2025-04-23 18:54:19 +0000

    Update to version 3.3.0, required for shibboleth-sp-3.5.0.

--- textproc/xmltooling/buildlink3.mk
+++ textproc/xmltooling/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/04/21 21:10:48 perseant Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/04/23 18:54:19 perseant Exp $
 
 BUILDLINK_TREE+=	xmltooling
 
 .if !defined(XMLTOOLING_BUILDLINK3_MK)
 XMLTOOLING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmltooling+=	xmltooling>=3.1.0
+BUILDLINK_API_DEPENDS.xmltooling+=	xmltooling>=3.3.0
 BUILDLINK_PKGSRCDIR.xmltooling?=	../../textproc/xmltooling
 
 .endif	# XMLTOOLING_BUILDLINK3_MK
