2f05dc59e5706c9f2dc89a84c16c6f951a5997c4
gutteridge <gutteridge@pkgsrc.org>
2025-04-24 02:14:48 +0000

    u-boot-version.mk: note the fragility of how PATCHDIR is set
    I've tripped over this twice now, so add a comment here explaining what
    can go wrong, if people simply sort buildlink3.mk inclusions
    alphabetically.

--- sysutils/u-boot/u-boot-version.mk
+++ sysutils/u-boot/u-boot-version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: u-boot-version.mk,v 1.5 2021/08/19 07:42:23 wiz Exp $
+# $NetBSD: u-boot-version.mk,v 1.6 2025/04/24 02:14:48 gutteridge Exp $
 
 UBOOT_DEFAULT_VERSION=	2018.11
 
@@ -6,6 +6,12 @@ UBOOT_VERSION?=	${UBOOT_DEFAULT_VERSION}
 
 .if ${UBOOT_VERSION} != ${UBOOT_DEFAULT_VERSION}
 DISTINFO_FILE?=	${.CURDIR}/../../sysutils/u-boot/distinfo-${UBOOT_VERSION}
+# If any consumer of this fragment happens to use buildlink3.mk inclusions
+# for other packages it depends upon, and those in turn happen to include
+# bsd.prefs.mk (as, for example, gnutls does), then packagers must ensure
+# whatever pulls this in is listed before those other dependencies, or
+# PATCHDIR will not be set as expected here (and the package will silently
+# not apply whatever patches may be intended).
 PATCHDIR?=	${.CURDIR}/../../sysutils/u-boot/patches-${UBOOT_VERSION}
 .else
 PATCHDIR?=	${.CURDIR}/../../sysutils/u-boot/patches
