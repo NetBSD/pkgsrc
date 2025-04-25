0b5f243fde8245b656e6a462e5d3d794ac33f8a4
pin <pin@pkgsrc.org>
2025-04-25 12:24:01 +0000

    sysutils/broot: update to 1.46.3
     - fix broot waiting for events on internals like :quit - Fix #1006

--- sysutils/broot/cargo-depends.mk
+++ sysutils/broot/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.94 2025/04/21 16:27:03 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.95 2025/04/25 12:24:01 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
 CARGO_CRATE_DEPENDS+=	ahash-0.8.11
