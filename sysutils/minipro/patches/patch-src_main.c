d74fe963543331580d83398fceb18a0d6b5e6dbc
thorpej <thorpej@pkgsrc.org>
2025-04-27 17:46:01 +0000

    Update to minipro-0.7.3.
    Summary of changes between 0.7 and 0.7.3:
    
    NEW FEATURES
    * Added a script to download an official Xgecu package and distill the
      individual algorithm files to a single "algorithm.xml" file for use
      with the T56 programmer.
    * Added firmware update capability for the T48 and T56 programmers.
    * Added OVC test and check for T48.
    * Much improved T48 and T56 support.
    
    BUG FIXES
    * Corrected wrong code_memory_size for some old PROMs.
    * Check for hardware fault in TL866A/CS about pin 20 GND driver.
    * Fixed AVR fuse parsing.
    * Fixed bad invocation of curl.
    * ...plus several other misc fixes.
    
    NOTES
    * Algorithms (FPGA bitstreams) for the T56 are not included with this
      package to avoid copyright complications.

--- sysutils/minipro/patches/patch-src_main.c
+++ sysutils/minipro/patches/patch-src_main.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-main.c,v 1.2 2024/04/21 20:37:52 thorpej Exp $
+$NetBSD: patch-src_main.c,v 1.1 2025/04/27 17:46:01 thorpej Exp $
 
 Use ctype(3) functions safely.
 
---- main.c.orig	2024-04-21 20:31:05.892824868 +0000
-+++ main.c	2024-04-21 20:31:14.668888370 +0000
-@@ -987,7 +987,7 @@ int get_config_value(const char *buffer,
+--- src/main.c.orig	2025-04-27 16:30:35
++++ src/main.c	2025-04-27 16:30:47
+@@ -1071,7 +1071,7 @@ int get_config_value(const char *buffer, const char *n
  		val = num;
  		cur += 2; /* Advances the pointer to the first numeric character */
  		while (cur < eol) {
