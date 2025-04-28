b9dd06684383bb1ea70008f775212de1863759b6
hauke <hauke@pkgsrc.org>
2025-04-28 11:38:37 +0000

    Update misc/mbuffer to v20241007
    Add NetBSD support
    
    Fix PKG_SYSCONFDIR
    
    From upstream's changelog:
    
    20241007:
    - build fix for -Wincompatible-pointer-types
    - configure script update to autoconf version 2.71
    
    20240929:
    - updated default buffer calculation for more sane defaults
    - minor updates for debug messages
    
    20240818:
    - fix port range check (Peter Pentchev)
    - fix ipv6 only mode
    - enhanced incoming host check to work with IP, hostname, FQHN
    - enhanced ipv6 hostname parsing
    
    20240707:
    - fix IPv4 address printing
    - listen on IPv6 and IPv4 if possible
    - add debug messages for defaults on address family
    - build fix for systems without getaddrinfo
    - gracefully handle empty address infos
    
    20240107:
    - corrections for documentation and help output
      (patch by Andreas Hartmann)
    - added environment variables for auto-loader command
    
    20231216:
    - changed from `which` to `command` for Debian forward compatibility
    
    20230301:
    - Fix breaking connections on WAN links: retry on EAGAIN
      (fix by Nico Schümann)
    
    20220418:
    - fix handling of filesystem full on stdout
    
    20211018:
    - fixes related to TCP timeout handling
    - enhancement: support setting config file via env var MBUFFERRC
    - documentation update
    
    20211004:
    - make TCPTimeout=0 disable the TCP timeout
    - changed default TCP timeout from 10s to 100s
    - TCP timeout now can be give with suffixes ms,s,min,h
    - documented option for TCP timeout
    
    20210829:
    - enhancement: accept IPv6 addresses in square bracket format
    - enhancement: more detailed warning message for connect failures
    - work around Solaris default old mercurial and incompatible sed
    
    20210328:
    - removed cancel after join for reader thread
    - updated install-sh
    - mbuffer.1 has outdated version tag
    
    20210209:
    - fix: hash algos should not suppress stdout
    
    20200929:
    - enhancement: added option --no-direct to disable use of O_DIRECT
    - defaults: raised default TCP timeout to 10ms for WAN connections
    - fix: leave TCP buffer size untouched if not set
    - enhancement: add option to set TCP timeout
    - performance optimization: use recv with MSG_WAITALL instead of read
    - configure fix: look for objdump also with target prefix
    - testing: make sure to use gtar for testing to avoid unexpected failures
    - portability: NetBSD compatibility fix
    - build enhancement: added dependency calculation for make
    - build enhancement: automatic version string generation
    
    20200505:
    - configure fix for some powerpc toolchains
    - update config.sub and config.guess
    - added option to perform direct I/Os on temporary file
    
    20191016:
    - autoadjust dependent parameters
    - human readable buffer size information instead of kiB
    
    20190725:
    - hashing infrastructure should also be enabled with libdl available
    - report input file name in message for open failure
    - fix: don't try to open network output as file after failure
    - fix: potential issue related to inet6 hosts handling
    - added complementary cmake based building and testing
    - some cleanup work
    
    20190127:
    - fix: erronous empty file generation on network connection failure
    
    20190119:
    - enhanced default settings for better performance
    - use O_DIRECT on input and output if possible
    - option --direct is now deprecated and has no effect
    - Output Size must be adjusted to Blocksize during initialization
    - man-page corrections
    - fixed some messages (typos and formatting)
    
    20190113:
    - fix for network initialization for systems without getaddrinfo
      (patch by Peter Pentchev)
    - support suppressing messages with Verbose=silent and Verbose=none
    - fix detection of invalid arguments to option -v
    - Copyright update to 2019
    
    20181119:
    - consistently use CLOCK_MONOTONIC if available
      (patch by Peter Pentchev)
    - handle time standing still or running backward
      (only relevant on systems without CLOCK_MONOTONIC)
    - updated INSTALL file to reflect available options
    - added explanation for lack of MacOS support

--- misc/mbuffer/patches/patch-mbuffer.c
+++ misc/mbuffer/patches/patch-mbuffer.c
@@ -1,10 +1,45 @@
-$NetBSD: patch-mbuffer.c,v 1.1 2018/09/18 09:49:29 fhajny Exp $
+$NetBSD: patch-mbuffer.c,v 1.2 2025/04/28 11:38:37 hauke Exp $
 
 Default sysconfdir location.
 
---- mbuffer.c.orig	2018-06-25 20:37:20.000000000 +0000
+Add NetBSD support for accessing the number of available VM pages.
+
+--- mbuffer.c.orig	2024-10-06 17:49:38.000000000 +0000
 +++ mbuffer.c
-@@ -1029,7 +1029,7 @@ static void initDefaults()
+@@ -46,6 +46,11 @@ typedef int caddr_t;
+ #include <sys/vmmeter.h>
+ #endif
+ 
++#ifdef __NetBSD__
++#include <sys/sysctl.h>
++#include <uvm/uvm_extern.h>
++#endif
++
+ #ifdef HAVE_SENDFILE
+ #ifdef HAVE_SENDFILE_H
+ #include <sys/sendfile.h>
+@@ -1032,6 +1037,20 @@ static void initDefaults()
+ 	} else {
+ 		AvP = vmt.t_free;
+ 	}
++#elif defined(__NetBSD__)
++	/* lifted from src/usr.bin/vmstat/vmstat.c, see also PR kern/49266 */
++	struct vmtotal total;
++	size_t size;
++	const int vmmeter_mib[] = { CTL_VM, VM_METER };
++
++	size = sizeof(total);
++	if (sysctl(vmmeter_mib, __arraycount(vmmeter_mib),
++		&total, &size, NULL, 0) == -1) {
++		warningmsg("unable to determine number of available pages: %s\n", strerror(errno));
++		AvP = 0;
++	} else {
++		AvP = total.t_free;
++	}
+ #else
+ 	warningmsg("no mechanism to determine number of available pages\n",strerror(errno));
+ #endif
+@@ -1060,7 +1079,7 @@ static void initDefaults()
  
  	const char *home = getenv("HOME");
  	readConfigFile("/etc/mbuffer.rc");
