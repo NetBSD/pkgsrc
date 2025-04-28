aa34e28dd905235c7ad6216629584c1e544db4ab
gdt <gdt@pkgsrc.org>
2025-04-28 12:38:17 +0000

    chat/ejabberd: Update to 25.04
    Packaging changes:
    
      - Rototill mix/rebar/rebar3 and add comments (still using rebar).
      - Update DEPENDS from rebar.config.
      - Drop lager dependency (only used for very old OTP).
      - Drop luerl because it goes with lua which we don't enable,
        demoting to a faux option that's always off.
      - Demote redis to a faux option that's always off, because
        erlang-redis is woefully out of date and not trivial to update.
        (There are no known users of redis with ejabberd from pkgsrc.)
      - Drop two patches that no longer apply because upstream at least
        more or less did the same thing.
      - Patch out CHOWN commands in Makefile.
      - Completely rototill patch-ae (about su); see patch comments and
        upstream pull request discussion.  Likely Solaris needs fixing,
        but in a way that doesn't break other systems, and this should be
        done upstream, or at least aligned with expected upstream changes.
    
    See https://docs.ejabberd.im/admin/upgrade/ for instructions on SQL schema changes.
    
    Upstream changes highlights (since 23.01) follow.  See
    https://github.com/processone/ejabberd/releases for more.
    
    25.04:
    
        security fixes
    
    25.03:
    
        Matrix Gateway Gets Room Support
        Multiple Simultaneous Password Types
        Execute API Commands Using XMPP Client
    
    
    24.12
    
        XEP-0484: Fast Authentication Streamlining Tokens: Reduce the time
        it takes for authentication. This helps with a faster start for
        clients on mobile.
    
        Commands API v3: We paved the way for API changes and improvements
        while allowing customers depending on older version to stay on a
        pinned version of the commands.
    
    24.10
    
        XEP-0288: Bidirectional Server-to-Server Connections
        XEP-0480: SASL Upgrade Tasks
        IQ permission in privileged entities
        PubSub varied fixes
        WebAdmin improvements
    
    24.07
    
        bugfixes
    
    24.06
    
        Webadmin rework
        Improved documentation
        Architecture and API improvements
    
    24.02
    
    ejabberd 24.02 has just been release and well, this is a huge release
    with 200 commits and more in the libraries. We've packed this update
    with a plethora of new features, significant improvements, and
    essential bug fixes, all designed to supercharge your messaging
    infrastructure.
    
        Matrix Federation Unleashed: Imagine seamlessly connecting with
        Matrix servers – it's now possible! ejabberd breaks new ground in
        cross-platform communication, fostering a more interconnected
        messaging universe. We have still some ground to cover and for
        that we are waiting for your feedback.
    
        Cutting-Edge Security with TLS 1.3 & SASL2: In an era where
        security is paramount, ejabberd steps up its game. With support
        for TLS 1.3 and advanced SASL2 protocols, we increase the overall
        security for all platform users.
    
        Performance Enhancements with Bind 2: Faster connection times,
        especially crucial for mobile network users, thanks to Bind 2 and
        other performance optimizations.
    
        User gains better control over on their messages: The new support
        for XEP-0424: Message Retraction allows users to manage their
        message history and remove something they posted by mistake.
    
        Optimized server pings by relying on an existing mechanism coming
        from XEP-0198
    
        Streamlined API Versioning: Our refined API versioning means
        smoother, more flexible integration for your applications.
    
        Enhanced Elixir, Mix and Rebar3 Support

--- chat/ejabberd/options.mk
+++ chat/ejabberd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2018/03/28 21:51:09 fhajny Exp $
+# $NetBSD: options.mk,v 1.7 2025/04/28 12:38:17 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ejabberd
 PKG_SUPPORTED_OPTIONS=	sqlite
@@ -6,9 +6,20 @@ PKG_SUGGESTED_OPTIONS=	#
 
 .include "../../mk/bsd.options.mk"
 
+# \todo Decide on a plan for options for databases, probably one of:
+#   - always include erlang module if it doesn't force a dependency,
+#     and never depend on the db package, and hence not having options
+#
+#   - for each, depend on db package and erlang module if option enabled
+#
+#   - for each, depend on db package if option enabled, and always
+#     depend on erlang module
+
+
 #
 # Add SQLite 3 support
 #
 .if !empty(PKG_OPTIONS:Msqlite)
-DEPENDS+=		erlang-sqlite3>=1.1.6:../../databases/erlang-sqlite3
+CONFIGURE_ARGS+=	--enable-sqlite
+DEPENDS+=		erlang-sqlite3>=1.1.13:../../databases/erlang-sqlite3
 .endif
