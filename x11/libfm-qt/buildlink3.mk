76d3be1648f11d2cac54715a73f9fabe9c49eade
gutteridge <gutteridge@pkgsrc.org>
2025-04-28 01:45:38 +0000

    libfm-qt: update to 2.2.0
    libfm-qt-2.2.0 / 2025-04-17
    ============================
     * Modernized string literals.
     * Enabled custom options for local terminals list.
     * List common apps in "Open With" menu with multiple MIME types.
     * Removed menu-cache version checks from `vfs-menu.c`.
     * Try better to change directory on mounting by clicking in side-pane.
     * Reorder XML ui files to correct tab orders.
     * Replaced qMin,qMax,qBound,qRound and qAbs with standard functions.
     * Support zstandard for Ark.
     * Capitalized "Move Files".

--- x11/libfm-qt/buildlink3.mk
+++ x11/libfm-qt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.36 2025/04/24 14:16:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2025/04/28 01:45:38 gutteridge Exp $
 
 BUILDLINK_TREE+=	libfm-qt
 
 .if !defined(LIBFM_QT_BUILDLINK3_MK)
 LIBFM_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=2.1.0
-BUILDLINK_ABI_DEPENDS.libfm-qt?=		libfm-qt>=2.1.0nb5
+BUILDLINK_API_DEPENDS.libfm-qt+=	libfm-qt>=2.2.0
 BUILDLINK_PKGSRCDIR.libfm-qt?=		../../x11/libfm-qt
 
 .include "../../x11/qt6-qtbase/buildlink3.mk"
