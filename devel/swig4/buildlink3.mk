dabfc20a189f608da9f4de8b826fc2ee0409ed22
adam <adam@pkgsrc.org>
2025-04-29 11:59:51 +0000

    swig4: updated to 4.3.1
    SWIG-4.3.1
    
    Small number of regression fixes for Python and Java.
    Corner case operator precedence bug fix affecting type deduction.
    Octave 10 compilation errors fix.
    
    SWIG-4.3.0
    
    Add experimental support for C as a target language.
    MzScheme/Racket is deprecated and planned for removal in SWIG-4.4.
    The distributed Windows binary is now a 64-bit executable.
    Add some missing use of move semantics for performance improvements.
    Enhanced handling of namespaces when using the nspace feature.
    STL wrapper enhancements for std::unique_ptr, std::string_view,
    std::filesystem.
    Various enum and enum class wrapping improvements.
    Other C++ handling improvements around templates, friends, C++11
    trailing return types and C++17 fold expressions.
    Many parser improvements for both C and C++, especially expressions.
    Improvements to handling of string and character literals.
    Minor preprocessor fixes.
    Python: Stricter stable ABI conformance, add support for python-3.13.
    C#: Add support for converting Doxygen comments into XML C# comments.
    Various other target language specific enhancements and updates for
    Java, Javascript, Lua, MzScheme, Ocaml, Octave, Perl, Python, R, Ruby.

--- devel/swig4/buildlink3.mk
+++ devel/swig4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/02/12 10:56:55 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/04/29 11:59:51 adam Exp $
 
 BUILDLINK_TREE+=	swig4
 
@@ -7,6 +7,9 @@ SWIG4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.swig4+=	swig4>=4.1.1
 BUILDLINK_PKGSRCDIR.swig4?=	../../devel/swig4
+
+BUILDLINK_FILES.swig4+=		bin/ccache-swig4.0
+BUILDLINK_FILES.swig4+=		bin/swig4.0
 
 .include "../../devel/pcre2/buildlink3.mk"
 .endif	# SWIG4_BUILDLINK3_MK
