8e622473d867cc7f9806f3fad68ffef5d5cc96bc
wiz <wiz@pkgsrc.org>
2025-04-29 10:46:11 +0000

    meson: update to 1.8.0.
    New argument android_exe_type for executables
    Changes to the b_sanitize option
    New C standard c2y (and gnu2y)
    i18n module xgettext
    version_compare now accept multiple compare strings
    Improvements to Objective-C and Objective-C++
    Per project subproject options rewrite
    Subproject specific values can be removed with -U
    objects added correctly to Rust executables
    rust.test now supports link_whole
    Meson can run "rustdoc" on Rust projects
    The Wayland module is stable
    New swift_std compiler option
    New option to execute a slice of tests
    Valgrind now fails tests if errors are found

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,16 +1,22 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.8 2024/10/22 09:42:05 wiz Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.9 2025/04/29 10:46:11 wiz Exp $
 
 Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
 a condition to match FreeBSD path norm for pkg-config files.
 
---- mesonbuild/modules/pkgconfig.py.orig	2024-10-20 18:20:39.000000000 +0000
+Also undo haiku-specific change.
+
+--- mesonbuild/modules/pkgconfig.py.orig	2025-04-28 15:36:57.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -701,7 +701,7 @@ class PkgConfigModule(NewExtensionModule
-         pcfile = filebase + '.pc'
+@@ -702,10 +702,10 @@ class PkgConfigModule(NewExtensionModule
          pkgroot = pkgroot_name = kwargs['install_dir'] or default_install_dir
          if pkgroot is None:
--            if mesonlib.is_freebsd():
+             m = state.environment.machines.host
+-            if m.is_freebsd():
 +            if False:
-                 pkgroot = os.path.join(_as_str(state.environment.coredata.get_option(OptionKey('prefix'))), 'libdata', 'pkgconfig')
+                 pkgroot = os.path.join(_as_str(state.environment.coredata.optstore.get_value_for(OptionKey('prefix'))), 'libdata', 'pkgconfig')
                  pkgroot_name = os.path.join('{prefix}', 'libdata', 'pkgconfig')
-             elif mesonlib.is_haiku():
+-            elif m.is_haiku():
++            elif False:
+                 pkgroot = os.path.join(_as_str(state.environment.coredata.optstore.get_value_for(OptionKey('prefix'))), 'develop', 'lib', 'pkgconfig')
+                 pkgroot_name = os.path.join('{prefix}', 'develop', 'lib', 'pkgconfig')
+             else:
