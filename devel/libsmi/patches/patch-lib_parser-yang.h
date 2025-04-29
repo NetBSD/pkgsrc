564a970f0139609ce3cedd10cbe5a685edeeb7d2
wiz <wiz@pkgsrc.org>
2025-04-29 07:46:55 +0000

    libsmi: fix previous
    add a forward declaration, struct Parser is not defined in that file or
    the headers it includes

--- devel/libsmi/patches/patch-lib_parser-yang.h
+++ devel/libsmi/patches/patch-lib_parser-yang.h
@@ -1,14 +1,15 @@
-$NetBSD: patch-lib_parser-yang.h,v 1.1 2025/04/28 19:34:48 tnn Exp $
+$NetBSD: patch-lib_parser-yang.h,v 1.2 2025/04/29 07:46:55 wiz Exp $
 
 Replace K&R prototype.
 
---- lib/parser-yang.h.orig	2025-04-28 19:31:08.092610491 +0000
+--- lib/parser-yang.h.orig	2013-06-04 12:34:00.000000000 +0000
 +++ lib/parser-yang.h
-@@ -13,7 +13,7 @@
+@@ -13,7 +13,8 @@
  
  
  
 -extern int yangparse();
++struct Parser;
 +extern int yangparse(struct Parser *);
  
  
