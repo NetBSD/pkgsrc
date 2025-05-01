97d87d2967f0b236e620d4d816fa56dd356b17b6
pin <pin@pkgsrc.org>
2025-05-01 13:06:09 +0000

    devel/cargo-edit: update to 0.13.3
    Fixes
    
     - (upgrade) Provide more context on registry errors

--- devel/cargo-edit/cargo-depends.mk
+++ devel/cargo-edit/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.2 2025/04/10 11:45:13 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.3 2025/05/01 13:06:09 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
