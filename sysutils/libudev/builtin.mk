8e0052b5a2e7e13c3520c0278dd3d45685c1eb73
vins <vins@pkgsrc.org>
2025-05-04 09:57:54 +0000

    sysutils/libudev: simplify builtin.mk
    Bump revision.

--- sysutils/libudev/builtin.mk
+++ sysutils/libudev/builtin.mk
@@ -1,41 +1,19 @@
-# $NetBSD: builtin.mk,v 1.2 2025/05/04 07:18:03 vins Exp $
+# $NetBSD: builtin.mk,v 1.3 2025/05/04 09:57:54 vins Exp $
 
-BUILTIN_PKG:=	libudev
+BUILTIN_PKG:=			libudev
+PKGCONFIG_BASE.libudev=		/usr
+PKGCONFIG_FILE.libudev=		${PKGCONFIG_BASE.libudev}/lib/pkgconfig/libudev.pc
+PKGCONFIG_FILE.libudev+=	${PKGCONFIG_BASE.libudev}/lib${LIBABISUFFIX}/pkgconfig/libudev.pc
 
-BUILTIN_FIND_PKGCONFIG_FILES_VAR:=		LIBUDEV_PC
-BUILTIN_FIND_PKGCONFIG_FILES.LIBUDEV_PC=	libudev.pc
+.include "../../mk/buildlink3/pkgconfig-builtin.mk"
 
-.include "../../mk/buildlink3/bsd.builtin.mk"
+CHECK_BUILTIN.libudev?=	no
+.if ${CHECK_BUILTIN.libudev:tl} == no
 
-.if !defined(IS_BUILTIN.libudev)
-IS_BUILTIN.libudev=	no
-.  if empty(LIBUDEV_PC:M__nonexistent__)
-IS_BUILTIN.libudev=	yes
+.  if ${USE_BUILTIN.libudev:tl} == yes
+BUILDLINK_PREFIX.libudev=	/usr
+BUILDLINK_FILES.libudev+=	lib/pkgconfig/libudev.pc
+BUILDLINK_FILES.libudev+=	lib${LIBABISUFFIX}/pkgconfig/libudev.pc
 .  endif
-.endif
-MAKEVARS+=		IS_BUILTIN.libudev
 
-.if !defined(USE_BUILTIN.libudev)
-USE_BUILTIN.libudev=	${IS_BUILTIN.libudev}
-.endif
-
-.PHONY: link-libudev-pkgconfig link-message-libudev-pkgconfig \
-	do-configure-pre-hook
-
-.if ${USE_BUILTIN.libudev:tl} == yes
-.  if !empty(USE_TOOLS:C/:.*//:Mpkg-config)
-do-configure-pre-hook: link-libudev-pkgconfig
-
-BLKDIR_PKGCFG=		${BUILDLINK_DIR}/lib/pkgconfig
-LIBUDEV_PKGCFGF=	${BUILTIN_FIND_PKGCONFIG_FILES.LIBUDEV_PC}
-
-link-libudev-pkgconfig: link-message-libudev-pkgconfig
-link-message-libudev-pkgconfig:
-	@${STEP_MSG} "Linking ${LIBUDEV_PC} file into ${BUILDLINK_DIR}."
-
-link-libudev-pkgconfig:
-	${RUN}						\
-	${MKDIR} ${BLKDIR_PKGCFG};			\
-	${LN} -sf ${LIBUDEV_PC} ${BLKDIR_PKGCFG}/${LIBUDEV_PKGCFGF}
-.  endif
-.endif
+.endif  # CHECK_BUILTIN.libudev
