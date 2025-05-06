0e87ebbb09bcce772f2dbed770e21786871aa450
wiz <wiz@pkgsrc.org>
2025-05-06 07:48:56 +0000

    chromaprint: pkglint cleanup

--- audio/chromaprint/options.mk
+++ audio/chromaprint/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2024/08/25 06:18:22 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2025/05/06 07:48:56 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.chromaprint
 
@@ -11,7 +11,7 @@ PKG_SUPPORTED_OPTIONS=	tests
 # googletest requires at least C++14. It bundles its own copy.
 USE_CXX_FEATURES+=	c++14
 TEST_TARGET=		check
-CMAKE_CONFIGURE_ARGS+=		-DBUILD_TESTS=ON
+CMAKE_CONFIGURE_ARGS+=	-DBUILD_TESTS=ON
 .else
-CMAKE_CONFIGURE_ARGS+=		-DBUILD_TESTS=OFF
+CMAKE_CONFIGURE_ARGS+=	-DBUILD_TESTS=OFF
 .endif
