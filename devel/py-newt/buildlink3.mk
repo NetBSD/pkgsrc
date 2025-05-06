8e16358ca53d8c4a09312f5bec26045c98fff4fc
vins <vins@pkgsrc.org>
2025-05-06 20:35:02 +0000

    devel/py-newt: update to 0.52.25
    0.52.25
    
    - improve Makefile (Ryan Carsten Schmidt)
    - fix "yes" in Spanish translation
    
    0.52.24
    
    - add support for python3.13
    - fix compiler warnings
    
    0.52.23
    
    - fix automatic height of menu/list in whiptail (broken in 0.52.22)
    - fix automatic width of whiptail --yesno box
    - fix automatic width in whiptail with unicode characters
    - fix automatic width with whiptail --noitem and --notags options
    - fix spacing with longer tags in whiptail
    - avoid overlapping backtitle in whiptail with automatic height
    
    0.52.22
    
    - fix crash in whiptail with new libpopt
    - switch from usleep to nanosleep (Rosen Penev)
    - fix libnewt.pc to enable static linking (Alexey Sheplyakov)
    - fix LDFLAGS order in snack linking (Sam James)
    - use CFLAGS when compiling snack
    - improve configure.ac (Thomas Kuehne)
    - install header and libnewt.pc with shared library (Michael Olbrich)
    
    0.52.21
    
    - define env NEWT_NOFLOWCTRL to disable flow control (Robert Gill)
    - don't leak memory on errors in dialogboxes
    - fix radio button selection check in snack
    - fix plural forms in Lithuanian translation (#1568999)
    - fix parallel build (Émeric Dupont)
    - allow python versions to be specified with --with-python option
    
    0.52.20
    
    - improve handling of long strings in whiptail menu (#1353792)
    - support screen resize as hotkey in form (#1432926)
    
    0.52.19
    
    - support --notags in whiptail checklist and radiolist (#1319794)
    - don't hard-code -I/usr/include/slang in CPPFLAGS (Alex Suykov)
    - use $(CC) instead of $(CPP) to generate .depend files (Samuel Martin)
    - update translations from Zanata
    
    0.52.18
    
    - fix widget key collision in snack on 64-bit archs (#1151455)
    - handle NEWT_EXIT_ERROR in snack
    - fix preprocessor warnings in snack
    - optimize textbox reflowing
    - remove newtListitem declarations (#1074092)
    
    0.52.17
    
    - add python3 support (Matthias Klose) (#963839)
    - implement newtComponentAddCallback() for forms as a focus-change
      callback (Dan Winship)
    - add newtEntryGet/SetCursorPosition and newtFormGet/SetScrollPosition
      (Dan Winship)
    - restore cursor position after changing help line (Dan Winship)
    - rename snackmodule to snack (#963839)
    - update CHANGES from spec changelog
    - add AUTHORS and README

--- devel/py-newt/buildlink3.mk
+++ devel/py-newt/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/01/01 06:55:23 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/05/06 20:35:02 vins Exp $
 
-BUILDLINK_TREE+=	${PYPKGPREFIX}-newt
+BUILDLINK_TREE+=	py-newt
 
 .if !defined(PY_NEWT_BUILDLINK3_MK)
 PY_NEWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-newt+=	${PYPKGPREFIX}-newt>=0.52.16
-BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-newt?=	../../devel/py-newt
+BUILDLINK_API_DEPENDS.py-newt+=	${PYPKGPREFIX}-newt>=0.52.16
+BUILDLINK_ABI_DEPENDS.py-newt+=	${PYPKGPREFIX}-newt>=0.52.25
+BUILDLINK_PKGSRCDIR.py-newt?=	../../devel/py-newt
 
 .endif	# PY_NEWT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-${PYPKGPREFIX}-newt
+BUILDLINK_TREE+=	-py-newt
