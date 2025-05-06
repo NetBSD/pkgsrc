b3419a9590418a28089af71552ad58e04fca7fe6
nia <nia@pkgsrc.org>
2025-05-06 21:22:41 +0000

    arcticfox: Fixes for NetBSD/mips.
    Right now the jit doesn't support N32 but should work with O32 and N64.

--- www/arcticfox/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h
+++ www/arcticfox/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h,v 1.1 2024/07/28 18:50:33 nia Exp $
+$NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h,v 1.2 2025/05/06 21:22:41 nia Exp $
 
 Fixes for NetBSD/arm, taken from www/firefox52
 
---- toolkit/components/protobuf/src/google/protobuf/stubs/atomicops.h.orig	2024-03-20 16:39:08.000000000 +0000
+--- toolkit/components/protobuf/src/google/protobuf/stubs/atomicops.h.orig	2025-03-13 13:33:35.000000000 +0000
 +++ toolkit/components/protobuf/src/google/protobuf/stubs/atomicops.h
 @@ -76,6 +76,11 @@ typedef intptr_t Atomic64;
  #endif
  #endif
  
-+#if defined(__NetBSD__) && defined(__arm__)
++#if defined(__NetBSD__) && (defined(__arm__) || defined(__mips__))
 +// NetBSD/arm uses long for intptr_t, which fails -fpermissive checks. So
 +// explicitly use int32 here instead.
-+typedef int32 AtomicWord;
++typedef Atomic32 AtomicWord;
 +#else
  // Use AtomicWord for a machine-sized pointer.  It will use the Atomic32 or
  // Atomic64 routines below, depending on your architecture.
