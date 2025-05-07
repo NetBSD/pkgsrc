efee5676ba40b4a67682c5b7d4550fb3ae1e37fa
vins <vins@pkgsrc.org>
2025-05-07 15:56:43 +0000

    xfile: switch to github to fetch the correct snapshot based on tag

--- sysutils/xfile/options.mk
+++ sysutils/xfile/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2025/05/01 07:34:20 vins Exp $
+# $NetBSD: options.mk,v 1.2 2025/05/07 15:56:43 vins Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.xfile
 
 PKG_SUPPORTED_OPTIONS=	dbus
+#PKG_SUGGESTED_OPTIONS=	dbus
 
 PLIST_VARS+=		dbus
 
@@ -17,6 +18,7 @@ PLIST_VARS+=		dbus
 .  include "../../sysutils/dbus/buildlink3.mk"
 
 DISTFILES+=	xfile-xdgsvc-src.tar.xz
+SITES.xfile-xdgsvc-src.tar.xz=	https://fastestcode.org/dl/
 
 SUBST_CLASSES+=		prefix
 SUBST_STAGE.prefix=	pre-configure
