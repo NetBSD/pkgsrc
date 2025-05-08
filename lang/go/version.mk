6221ad6cb6528d89548139e96c7eeb4a309ba7d8
bsiegert <bsiegert@pkgsrc.org>
2025-05-08 18:55:52 +0000

    go: update go123 to 1.23.9 and go124 to 1.24.3.
    The Go 1.24.3 minor release includes 1 security fix following the security
    policy:
    
    -   os: Root permits access to parent directory
    
        It was possible to improperly access the parent directory of an os.Root
        by opening a filename ending in "../". For example, Root.Open("../") would
        open the parent directory of the Root. This escape only permits opening
        the parent directory itself, not ancestors of the parent or files contained
        within the parent.
    
        Root now correctly returns an error in this case.
    
        This is CVE-2025-22873 and Go issue https://go.dev/issue/73555.
    
        Thanks to Dan Sebastian Thrane of SDU eScience Center for reporting this
        issue.
    
    This security fix only applies to Go 1.24.x releases. Go 1.23.x releases are
    not affected by this.
    
    go1.23.9 (released 2025-05-06) includes fixes to the runtime and the linker.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.230 2025/04/01 17:47:18 bsiegert Exp $
+# $NetBSD: version.mk,v 1.231 2025/05/08 18:55:52 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,8 +6,8 @@
 #
 .include "go-vars.mk"
 
-GO124_VERSION=	1.24.2
-GO123_VERSION=	1.23.8
+GO124_VERSION=	1.24.3
+GO123_VERSION=	1.23.9
 GO122_VERSION=	1.22.12
 GO120_VERSION=	1.20.14
 GO118_VERSION=	1.18.10
