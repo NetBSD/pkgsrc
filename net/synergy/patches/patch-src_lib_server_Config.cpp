135473b49ab7698a905e49b9380131e2766a6eed
tnn <tnn@pkgsrc.org>
2025-05-09 22:18:38 +0000

    net/synergy: put back removed package until after next branch or so
    requested by abs@

---
+++ net/synergy/patches/patch-src_lib_server_Config.cpp
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_lib_server_Config.cpp,v 1.4 2025/05/09 22:18:38 tnn Exp $
+
+Fix for some EOF problem. Not sure it's still required as the
+previous patch has been adapted to upstream changes.
+
+--- src/lib/server/Config.cpp.orig	2021-06-08 13:44:14.000000000 +0000
++++ src/lib/server/Config.cpp
+@@ -1974,7 +1974,7 @@ ConfigReadContext::getLineNumber() const
+ bool
+ ConfigReadContext::operator!() const
+ {
+-	return !m_stream;
++	return m_stream.bad();
+ }
+ 
+ OptionValue
