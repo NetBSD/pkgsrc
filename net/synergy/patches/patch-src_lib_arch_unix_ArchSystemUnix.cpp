30910afa6d19755e3d6329e24ef605dcc32c6ed1
tnn <tnn@pkgsrc.org>
2025-05-09 22:18:38 +0000

    net/synergy: put back removed package until after next branch or so
    requested by abs@

---
+++ net/synergy/patches/patch-src_lib_arch_unix_ArchSystemUnix.cpp
@@ -0,0 +1,24 @@
+$NetBSD: patch-src_lib_arch_unix_ArchSystemUnix.cpp,v 1.4 2025/05/09 22:18:38 tnn Exp $
+
+make dbus optional
+
+--- src/lib/arch/unix/ArchSystemUnix.cpp.orig	2022-02-21 11:08:47.000000000 +0000
++++ src/lib/arch/unix/ArchSystemUnix.cpp
+@@ -19,7 +19,7 @@
+ #include "arch/unix/ArchSystemUnix.h"
+ 
+ #include <sys/utsname.h>
+-#ifndef __APPLE__
++#ifdef WITH_DBUS
+ #include <QtDBus>
+ #endif
+ 
+@@ -82,7 +82,7 @@ ArchSystemUnix::getLibsUsed(void) const
+     return "not implemented.\nuse lsof on shell";
+ }
+ 
+-#ifndef __APPLE__
++#ifdef WITH_DBUS
+ bool
+ ArchSystemUnix::DBusInhibitScreenCall(InhibitScreenServices serviceID, bool state, std::string& error)
+ {
