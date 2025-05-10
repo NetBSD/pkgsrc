d8bcbfe381c49bd9a5bca47d50c7b52df13641eb
vins <vins@pkgsrc.org>
2025-05-10 18:59:47 +0000

    sysutils/xfce4-power-manager: fix build on SunOS

--- sysutils/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
+++ sysutils/xfce4-power-manager/patches/patch-src_xfpm-pm-helper.c
@@ -1,16 +1,21 @@
-$NetBSD: patch-src_xfpm-pm-helper.c,v 1.3 2025/01/05 13:50:37 bsiegert Exp $
+$NetBSD: patch-src_xfpm-pm-helper.c,v 1.4 2025/05/10 18:59:47 vins Exp $
 
-Add NetBSD support.
+Add NetBSD and SunOS support.
 
 --- src/xfpm-pm-helper.c.orig	2024-11-21 06:26:21.000000000 +0000
 +++ src/xfpm-pm-helper.c
-@@ -82,6 +82,11 @@
+@@ -82,6 +82,16 @@
  #define UP_BACKEND_HIBERNATE_COMMAND "/usr/sbin/ZZZ"
  #define UP_BACKEND_HYBRID_SLEEP_COMMAND "/usr/bin/false"
  #endif
 +#ifdef BACKEND_TYPE_NETBSD
 +#define UP_BACKEND_SUSPEND_COMMAND "sysctl -w hw.acpi.sleep.state=3"
 +#define UP_BACKEND_HIBERNATE_COMMAND "sysctl -w hw.acpi.sleep.state=4"
++#define UP_BACKEND_HYBRID_SLEEP_COMMAND "/usr/bin/false"
++#endif
++#ifdef BACKEND_TYPE_SOLARIS
++#define UP_BACKEND_SUSPEND_COMMAND "/usr/bin/sys-suspend -x"
++#define UP_BACKEND_HIBERNATE_COMMAND "/usr/bin/false"
 +#define UP_BACKEND_HYBRID_SLEEP_COMMAND "/usr/bin/false"
 +#endif
  
