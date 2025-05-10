7ebd3a72875392c155a9b8d907477d64ccf1fec2
vins <vins@pkgsrc.org>
2025-05-10 18:59:47 +0000

    sysutils/xfce4-power-manager: fix build on SunOS

--- sysutils/xfce4-power-manager/patches/patch-src_xfpm-suspend.c
+++ sysutils/xfce4-power-manager/patches/patch-src_xfpm-suspend.c
@@ -1,26 +1,34 @@
-$NetBSD: patch-src_xfpm-suspend.c,v 1.4 2025/05/10 17:21:40 vins Exp $
+$NetBSD: patch-src_xfpm-suspend.c,v 1.5 2025/05/10 18:59:47 vins Exp $
 
-Add NetBSD support.
+Add NetBSD and SunOS support.
 
---- src/xfpm-suspend.c.orig	2020-11-04 23:23:14.000000000 +0000
+--- src/xfpm-suspend.c.orig	2024-11-21 06:26:21.000000000 +0000
 +++ src/xfpm-suspend.c
-@@ -152,6 +151,9 @@ xfpm_suspend_can_suspend (void)
+@@ -141,7 +141,7 @@ xfpm_suspend_can_suspend (void)
+ #ifdef BACKEND_TYPE_LINUX
+   return linux_supports_sleep_state ("suspend");
+ #endif
+-#ifdef BACKEND_TYPE_OPENBSD
++#if defined (BACKEND_TYPE_OPENBSD) || defined (BACKEND_TYPE_NETBSD) || defined (BACKEND_TYPE_SOLARIS)
+   return TRUE;
+ #endif
+ 
+@@ -157,7 +157,7 @@ xfpm_suspend_can_hibernate (void)
+ #ifdef BACKEND_TYPE_LINUX
+   return linux_supports_sleep_state ("hibernate");
+ #endif
+-#ifdef BACKEND_TYPE_OPENBSD
++#if defined (BACKEND_TYPE_OPENBSD) || defined (BACKEND_TYPE_NETBSD)
+   return TRUE;
+ #endif
+ 
+@@ -176,6 +176,9 @@ xfpm_suspend_can_hybrid_sleep (void)
  #ifdef BACKEND_TYPE_OPENBSD
    return TRUE;
  #endif
-+#ifdef BACKEND_TYPE_NETBSD
++#if defined(BACKEND_TYPE_NETBSD) || defined(BACKEND_TYPE_SOLARIS)
 +  return TRUE;
 +#endif
-
-   return FALSE;
- }
-@@ -169,6 +171,9 @@ xfpm_suspend_can_hibernate (void)
- #ifdef BACKEND_TYPE_OPENBSD
-   return TRUE;
- #endif
-+#ifdef BACKEND_TYPE_NETBSD
-+  return TRUE;
-+#endif
-
+ 
    return FALSE;
  }
