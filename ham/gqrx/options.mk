cc002144cd3443244615de34d12adcda0f0df98b
gdt <gdt@pkgsrc.org>
2025-05-12 21:31:03 +0000

    ham/gqrx: Merge comments about dependencies from wip
    Dependencies are organized following upstream's README, so that one
    can verify that what upstream asks for is included, except for things
    not in pkgsrc, and some confusion about hardware drivers.
    
    NFCI.  Works on NetBSD 10 with remote rtl_rtp.

--- ham/gqrx/options.mk
+++ ham/gqrx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2025/03/01 17:22:13 adam Exp $
+# $NetBSD: options.mk,v 1.2 2025/05/12 21:31:03 gdt Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gqrx
 PKG_OPTIONS_OPTIONAL_GROUPS=	gui
@@ -13,8 +13,8 @@ CMAKE_CONFIGURE_ARGS+=	-DFORCE_QT5=ON
 .  include "../../x11/qt5-qtsvg/buildlink3.mk"
 .elif !empty(PKG_OPTIONS:Mqt6)
 CMAKE_CONFIGURE_ARGS+=	-DFORCE_QT6=ON
-.  include "../../graphics/qt6-qtsvg/buildlink3.mk"
 .  include "../../x11/qt6-qtbase/buildlink3.mk"
+.  include "../../graphics/qt6-qtsvg/buildlink3.mk"
 .else
 PKG_FAIL_REASON=	"One of qt5 or qt6 options must be selected."
 .endif
