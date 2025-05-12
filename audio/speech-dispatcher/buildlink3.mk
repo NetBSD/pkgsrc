64c0233567636283d6df6eef28ac38c9f27099cf
wiz <wiz@pkgsrc.org>
2025-05-12 10:38:30 +0000

    speech-dispatcher: update to 0.12.1.
    Test on Darwin by adam@, thanks!
    
    0.12.1
    
    Fix spd-say -k ' '
    Fix output getting stuck in some rare condition triggered in Orca preference dialog
    Fix taking character punctuation levels from NVDA files.
    espeak: Fix translated pronounciation.
    generic: Fix getting stuck on speaking nothing.
    generic: Add GenericDefaultCharset parameter
    generic: Fix random garbage on non-text utterances
    Fix Czech documentation encoding.
    
    Version 0.12
    * Add openjtalk module.
    * Add socket activation.
    * Add language and variant parameters to "list voice" command.
    * Add libspeechd-module library for making it simpler to create external spd modules.
    * libspeechd: Simplify buffer management.
    * Fix use of semaphores on MacOS.
    * Fix splitting long UTF-8 sequences.
    * Update CLDR to version 45, symbols from orca 45.2, and symbols from NVDA.
    * Add spd_fd function to C api.
    * Detect module failures from generic module.
    * Make the fallback espeak-ng and dummy modules hardcoded.
    * Better detect generic module failures to disable them.
    * pulse: Use asynchronous API to avoid buffer underruns.
    * generic: Make stripping punctuation use locale charset.
    * audio: Fix logging from audio modules in server-side audio.
    * Sort Baratinoo engine higher.
    * espeak-ng-mbrola: Fix mbrola voices with rate different from 22KHz
    * Add a run-spd-say script and make run-speechd and run-spd-say able to talk directly.
    * server: fix daemon mode on macOS with libao.
    * Add initial pipewire support.
    * Also support loading symbols from home directory

--- audio/speech-dispatcher/buildlink3.mk
+++ audio/speech-dispatcher/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2024/03/24 10:28:14 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/05/12 10:38:30 wiz Exp $
 
 BUILDLINK_TREE+=	speech-dispatcher
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.speech-dispatcher+=	speech-dispatcher>=0.10.2nb8
 BUILDLINK_PKGSRCDIR.speech-dispatcher?=		../../audio/speech-dispatcher
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libltdl/buildlink3.mk"
 .endif	# SPEECH_DISPATCHER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-speech-dispatcher
