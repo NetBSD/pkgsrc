b7a6b1c5025252fa19b243127f7406e8ca6ca11b
nia <nia@pkgsrc.org>
2025-05-15 11:40:32 +0000

    stund: Various portability improvements
    - Remove the assembler (and Solaris-specific) methods for seeding the
      RNG and just use /dev/urandom everywhere. This fixes the build on
      sparc64.
    
    - My (much earlier) attempt at fixing the build on SunOS wasn't quite
      right - SunOS needs -DBSD_COMP to expose all ioctl definitions.
    
    Confirmed to build on FreeBSD, OpenBSD, NetBSD, Linux (modern & retro),
    SunOS by drecklypkg ci.

--- net/stund/patches/patch-stun.cxx
+++ net/stund/patches/patch-stun.cxx
@@ -1,19 +1,17 @@
-$NetBSD: patch-stun.cxx,v 1.2 2024/10/10 20:18:02 nia Exp $
+$NetBSD: patch-stun.cxx,v 1.3 2025/05/15 11:40:32 nia Exp $
+
+Portable RNG seeding.
 
 --- stun.cxx.orig	2012-01-26 11:02:46.000000000 +0000
 +++ stun.cxx
-@@ -16,6 +16,7 @@
- #include <string.h>
- #include <sys/ioctl.h>
- #include <sys/socket.h>
-+#include <sys/sockio.h>
- #include <sys/time.h>
- #include <sys/types.h> 
- #include <arpa/inet.h>
-@@ -673,12 +674,10 @@ stunRand()
-       asm("rdtsc" : "=A" (tick));
- #elif defined (__SUNPRO_CC) || defined( __sparc__ )	
-       tick = gethrtime();
+@@ -669,16 +669,10 @@ stunRand()
+       tick = hightick;
+       tick <<= 32;
+       tick |= lowtick;
+-#elif defined(__GNUC__) && ( defined(__i686__) || defined(__i386__) )
+-      asm("rdtsc" : "=A" (tick));
+-#elif defined (__SUNPRO_CC) || defined( __sparc__ )	
+-      tick = gethrtime();
 -#elif defined(__MACH__)  || defined(__linux)
 -      int fd=open("/dev/random",O_RDONLY);
 +#else
