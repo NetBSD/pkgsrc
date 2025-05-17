b628dd6ab5f2bad00da80f0b313a4648a58443d8
gdt <gdt@pkgsrc.org>
2025-05-17 23:23:11 +0000

    *: revbump for gpsd update (shlib major change)

--- geography/gpsd/buildlink3.mk
+++ geography/gpsd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2023/08/14 05:24:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/05/17 23:23:11 gdt Exp $
 
 BUILDLINK_TREE+=	gpsd
 
@@ -7,7 +7,7 @@ GPSD_BUILDLINK3_MK:=
 
 # 3.19 changed structures and is thus an ABI break.
 BUILDLINK_API_DEPENDS.gpsd+=	gpsd>=3.19
-BUILDLINK_ABI_DEPENDS.gpsd+=	gpsd>=3.25nb2
+BUILDLINK_ABI_DEPENDS.gpsd+=	gpsd>=3.26.1
 BUILDLINK_PKGSRCDIR.gpsd?=	../../geography/gpsd
 .endif # GPSD_BUILDLINK3_MK
 
