8fc19bc2902b36b6e55633096cdc21e8f4863d51
gdt <gdt@pkgsrc.org>
2025-05-18 19:29:32 +0000

    ham/gnuradio-core: Fix C++ language standard issues
    gnuradio-core was inexplicably marked as needing C++20, apparently
    because it used to have GCC_REQD=10.  There is no evidence that it
    needs C++20 in upstream docs or CMakeLists, and it builds fine with
    C++17.
    
    Further, GNU Radio's build system does not test for and set --std, so
    it will fail if the compiler defaults to C++11, as can be shown by
    setting FORCE_CXX_STD to C++11.  Therefore, set FORCE_CXX_STD to
    C++17.
    
    Because the installed headers require C++17, also set these variables
    in buildlink3.mk.  FORCE is needed because code that uses GNU Radio
    headers but is otherwise ok with c++11 might set c++11 as the std.
    (Builds have only been working because the default C++ std is high
    enough with gcc10.)
    
    Fixes PR pkg/59348, by causing gcc10 to be used, instead of dragging
    in gcc12.  The osmosdr python is sensitive to gcc mismatch -- but we
    already know such mismatches are UB.

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,12 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.41 2025/02/27 13:49:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2025/05/18 19:29:32 gdt Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
 .if !defined(GNURADIO_CORE_BUILDLINK3_MK)
 GNURADIO_CORE_BUILDLINK3_MK:=
 
+# GNU Radio's header files do not compile in c++11 mode, but do in
+# c++17 mode.  See Makefile.common for status of reporting upstream.
+USE_LANGUAGES+=		c c++
+USE_CXX_FEATURES+=	c++17
+FORCE_CXX_STD=		c++17
+
 BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.10.2
-BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.10.12.0
+BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.10.12.0nb4
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
 
 .include "../../devel/boost-libs/buildlink3.mk"
