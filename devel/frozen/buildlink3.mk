f7356af8911ae108aa599675e8343e8938b5bbf5
ryoon <ryoon@pkgsrc.org>
2025-05-18 01:04:32 +0000

    devel/frozen: Update to 1.2.0
    Changelog:
    1.2.0:
    Fixing "'macro' is not defined" warning, when using clang-tidy

--- devel/frozen/buildlink3.mk
+++ devel/frozen/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2023/10/15 19:26:40 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/05/18 01:04:32 ryoon Exp $
 
 BUILDLINK_TREE+=	frozen
 
@@ -7,7 +7,7 @@ FROZEN_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.frozen?=	build
 
-BUILDLINK_API_DEPENDS.frozen+=	frozen>=1.1.1
+BUILDLINK_API_DEPENDS.frozen+=	frozen>=1.2.0
 BUILDLINK_PKGSRCDIR.frozen?=	../../devel/frozen
 .endif	# FROZEN_BUILDLINK3_MK
 
