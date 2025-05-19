27a985f9a4a601f789fcbc4547fdc4fcb09b7941
wiz <wiz@pkgsrc.org>
2025-05-19 11:20:01 +0000

    gimp: update to stable 3.0.4, from gimp-devel

--- graphics/gimp/options.mk
+++ graphics/gimp/options.mk
@@ -1,36 +1,27 @@
-# $NetBSD: options.mk,v 1.10 2021/05/12 06:30:56 wiz Exp $
+# $NetBSD: options.mk,v 1.11 2025/05/19 11:20:01 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gimp
-PKG_SUPPORTED_OPTIONS=	aalib mng python
-PKG_SUGGESTED_OPTIONS=	mng python
+PKG_SUPPORTED_OPTIONS=	aalib mng xpm
+PKG_SUGGESTED_OPTIONS=	mng xpm
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_SRC=		${PLIST_SRC_DFLT}
-PLIST_VARS+=		aalib mng
+PLIST_VARS+=		aalib mng xpm
 
 .if !empty(PKG_OPTIONS:Maalib)
 .include "../../graphics/aalib/buildlink3.mk"
+PRINT_PLIST_AWK+=       { if (/file-aa/) $$0 = "$${PLIST.aalib}" $$0 }
 PLIST.aalib=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mmng)
 .include "../../graphics/mng/buildlink3.mk"
+PRINT_PLIST_AWK+=       { if (/file-mng/) $$0 = "$${PLIST.mng}" $$0 }
 PLIST.mng=		yes
 .endif
 
-.if !empty(PKG_OPTIONS:Mpython)
-PLIST_SRC+=		PLIST.python
-PYTHON_VERSIONS_ACCEPTED=	27
-CONFIGURE_ARGS+=		--enable-python
-REPLACE_PYTHON+=		plug-ins/pygimp/*.py
-REPLACE_PYTHON+=		plug-ins/pygimp/plug-ins/*.py
-REPLACE_PYTHON+=		plug-ins/pygimp/plug-ins/*/*.py
-REPLACE_PYTHON+=		tools/*.py
-.include "../../x11/py-gtk2/buildlink3.mk"
-.include "../../graphics/py-cairo118/buildlink3.mk"
-.include "../../lang/python/pyversion.mk"
-.include "../../lang/python/application.mk"
-.else
-CONFIGURE_ARGS+=		--disable-python
+.if !empty(PKG_OPTIONS:Mxpm)
+.include "../../x11/libXpm/buildlink3.mk"
+PRINT_PLIST_AWK+=       { if (/file-xpm/) $$0 = "$${PLIST.xpm}" $$0 }
+PLIST.xpm=		yes
 .endif
