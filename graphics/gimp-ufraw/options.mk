438be790098a7ef64e168bc9600355b7377e2873
wiz <wiz@pkgsrc.org>
2025-05-19 11:11:14 +0000

    gimp-ufraw: remove gimp(2) option
    XXX: not sure about the name of the package...

--- graphics/gimp-ufraw/options.mk
+++ graphics/gimp-ufraw/options.mk
@@ -1,20 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2025/02/22 14:40:25 gdt Exp $
+# $NetBSD: options.mk,v 1.2 2025/05/19 11:11:14 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ufraw
-PKG_SUPPORTED_OPTIONS=	gimp lensfun
+PKG_SUPPORTED_OPTIONS=	lensfun
 PKG_SUGGESTED_OPTIONS=	lensfun
 
 .include "../../mk/bsd.options.mk"
 
-# gimp support only works with gimp2.  gimp3 is imminent (and 1 of 1
-# known users have switched to gimp3).
-PLIST_VARS+=	gimp
-.if !empty(PKG_OPTIONS:Mgimp)
-.include "../../graphics/gimp/buildlink3.mk"
-PLIST.gimp=	yes
-.endif
-
-.include "../../mk/bsd.options.mk"
 .if !empty(PKG_OPTIONS:Mlensfun)
 .include "../../graphics/lensfun/buildlink3.mk"
 .endif
