668a6fc1aaecd388cce24f42520ed234d11cabe8
wiz <wiz@pkgsrc.org>
2025-05-19 10:45:52 +0000

    babl: needs lcms2 in bl3.mk

--- graphics/babl/buildlink3.mk
+++ graphics/babl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2025/04/08 11:32:18 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/05/19 10:45:52 wiz Exp $
 
 BUILDLINK_TREE+=	babl
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.babl+=	babl>=0.1.10
 BUILDLINK_ABI_DEPENDS.babl+=	babl>=0.1.106nb1
 BUILDLINK_PKGSRCDIR.babl?=	../../graphics/babl
 
+.include "../../graphics/lcms2/buildlink3.mk"
 .include "../../mk/dlopen.buildlink3.mk"
 .endif # BABL_BUILDLINK3_MK
 
