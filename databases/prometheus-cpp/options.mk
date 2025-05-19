2c732e1b28a4cd96c10643e4d33128fce9dbfbcd
wiz <wiz@pkgsrc.org>
2025-05-19 12:46:50 +0000

    prometheus-cpp: fix pkglint

--- databases/prometheus-cpp/options.mk
+++ databases/prometheus-cpp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2024/08/25 06:18:31 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2025/05/19 12:46:50 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.prometheus-cpp
 
@@ -8,7 +8,7 @@ PKG_SUPPORTED_OPTIONS=	tests
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mtests)
-TEST_TARGET=	test
+TEST_TARGET=		test
 CMAKE_CONFIGURE_ARGS+=	-DENABLE_TESTING=ON
 # XXX: it's using an internal copy...
 #.  include "../../devel/googletest/buildlink3.mk"
