541f8815a534db825922945b19a5f515eb6b08e5
taca <taca@pkgsrc.org>
2025-05-19 15:00:56 +0000

    lang/php83: update to 8.3.21
    PHP 8.3.21 (2025-05-08)
    
    Core:
    
    * Fixed bug GH-18304 (Changing the properties of a DateInterval through
      dynamic properties triggers a SegFault).
    * Fix some leaks in php_scandir.
    
    Filter:
    
    * Fixed bug GH-18309 (ipv6 filter integer overflow).
    
    GD:
    
    * Fixed imagecrop() overflow with rect argument with x/width y/heigh usage
      in gdImageCrop().
    * Fixed GH-18243 imagettftext() overflow/underflow on font size value.
    
    Intl:
    
    * Fix reference support for intltz_get_offset().
    
    LDAP:
    
    * Fixed bug GH-17776 (LDAP_OPT_X_TLS_* options can't be overridden).
    * Fix NULL deref on high modification key.
    
    libxml:
    
    * Fixed custom external entity loader returning an invalid resource leading
      to a confusing TypeError message.
    
    OpenSSL:
    
    * Fix memory leak in openssl_sign() when passing invalid algorithm.
    * Fix potential leaks when writing to BIO fails.
    
    PDO Firebird:
    
    * Fixed GH-18276 - persistent connection - "zend_mm_heap corrupted" with
      setAttribute() (SakiTakamachi).
    
    SPL:
    
    * Fixed bug GH-18322 (SplObjectStorage debug handler mismanages memory).
    
    Standard:
    
    * Fixed bug GH-18145 (php8ts crashes in php_clear_stat_cache()).
    * Fixed bug GH-18209 (Use-after-free in extract() with EXTR_REFS).
    * Fixed bug GH-18212 (fseek with SEEK_CUR whence value and negative offset
      leads to negative stream position).
    * Fix resource leak in iptcembed() on error.
    
    Zip:
    
    * Fix uouv when handling empty options in ZipArchive::addGlob().
    * Fix memory leak when handling a too long path in ZipArchive::addGlob().

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.462 2025/04/13 16:00:00 taca Exp $
+# $NetBSD: phpversion.mk,v 1.463 2025/05/19 15:00:56 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -112,7 +112,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.32
 PHP82_VERSION=	8.2.28
-PHP83_VERSION=	8.3.20
+PHP83_VERSION=	8.3.21
 PHP84_VERSION=	8.4.6
 
 _VARGROUPS+=	php
