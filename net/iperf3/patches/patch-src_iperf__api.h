3eee2e77c4240cf626b2686a5be4ce61b270cbb1
wiz <wiz@pkgsrc.org>
2025-05-21 22:16:56 +0000

    iperf3: add link to upstream bug report

--- net/iperf3/patches/patch-src_iperf__api.h
+++ net/iperf3/patches/patch-src_iperf__api.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_iperf__api.h,v 1.3 2025/05/21 22:14:00 wiz Exp $
+$NetBSD: patch-src_iperf__api.h,v 1.4 2025/05/21 22:16:56 wiz Exp $
 
 Add missing header for va_list etc.
+https://github.com/esnet/iperf/issues/1890
 
 --- src/iperf_api.h.orig	2025-05-21 22:13:01.690766581 +0000
 +++ src/iperf_api.h
