babcf62c580db7815b4d387123b1687ef542e191
adam <adam@pkgsrc.org>
2025-05-23 20:01:11 +0000

    wireguard-go: updated to 0.0.20250522
    0.0.20250522
    Unknown changes

--- net/wireguard-go/go-modules.mk
+++ net/wireguard-go/go-modules.mk
@@ -1,16 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.1 2023/12/14 13:56:23 adam Exp $
+# $NetBSD: go-modules.mk,v 1.2 2025/05/23 20:01:11 adam Exp $
 
-GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.mod
-GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.2.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.2.0.zip
-GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.mod
-GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.zip
-GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.mod
-GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.zip
-GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20221203005347-703fd9b7fbc0.mod
-GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20221203005347-703fd9b7fbc0.zip
+GO_MODULE_FILES+=	github.com/google/btree/@v/v1.1.2.mod
+GO_MODULE_FILES+=	github.com/google/btree/@v/v1.1.2.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.37.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.37.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.39.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.39.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.32.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.32.0.zip
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.7.0.mod
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.7.0.zip
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20230126152724-0fa3db229ce2.mod
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20230126152724-0fa3db229ce2.zip
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20250503011706-39ed1f5ac29c.mod
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20250503011706-39ed1f5ac29c.zip
