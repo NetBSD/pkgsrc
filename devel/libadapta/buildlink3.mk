181be9af6767be406b52529ca0fe1b51766ee4d8
wiz <wiz@pkgsrc.org>
2025-05-23 14:36:48 +0000

    devel/libadapta: import libadapta-1.5.0
    libAdapta is libAdwaita with theme support and a few extra.
    
    It provides the same features and the same look as libAdwaita by
    default.
    
    In desktop environments which provide theme selection, libAdapta
    apps follow the theme and use the proper window controls.
    
    libAdwaita also provides a compatibility header which makes it easy
    for developers to switch between libAdwaita and libAdapta without
    requiring code changes.

---
+++ devel/libadapta/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/05/23 14:36:48 wiz Exp $
+
+BUILDLINK_TREE+=	libadapta
+
+.if !defined(LIBADAPTA_BUILDLINK3_MK)
+LIBADAPTA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libadapta+=	libadapta>=1.5.0
+BUILDLINK_PKGSRCDIR.libadapta?=		../../devel/libadapta
+
+.include "../../devel/appstream/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk4/buildlink3.mk"
+.endif	# LIBADAPTA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libadapta
