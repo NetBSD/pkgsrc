baa327e515b2cf11b763104108c5f7d12d8f8e85
wiz <wiz@pkgsrc.org>
2025-05-25 17:56:01 +0000

    py-qt5: fix case where dbus option is turned off
    from Rob Whitlock in PR 59187.

--- x11/py-qt5/options.mk
+++ x11/py-qt5/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2022/12/02 15:31:58 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2025/05/25 17:56:01 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-qt5
 PKG_SUPPORTED_OPTIONS+=	dbus
@@ -11,4 +11,6 @@ PLIST_VARS+=		dbus
 PLIST.dbus=		yes
 .include "../../sysutils/dbus-python-common/buildlink3.mk"
 .include "../../sysutils/py-dbus/buildlink3.mk"
+.else
+WHEEL_ARGS+=	-C=--disable=QtDBus
 .endif
