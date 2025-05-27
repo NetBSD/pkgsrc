f47e06f04ad46ad76341b6648c16d202ca83e495
bsiegert <bsiegert@pkgsrc.org>
2025-05-27 20:38:38 +0000

    maeparser: update to 1.3.2
    1.3.0
    
    The issues addressed in this release mostly relate to building and
    automated testing. The minor version was bumped because two new methods
    were added to Block.
    
    1.3.1
    
    Allow property names to include colons
    
    1.3.2
    
    Make the boost::iostreams dependency option

--- biology/maeparser/buildlink3.mk
+++ biology/maeparser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/12/29 15:09:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2025/05/27 20:38:38 bsiegert Exp $
 
 BUILDLINK_TREE+=	maeparser
 
@@ -7,7 +7,7 @@ MAEPARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.maeparser+=	maeparser>=1.2.4
 BUILDLINK_ABI_DEPENDS.maeparser?=	maeparser>=1.2.4nb9
-BUILDLINK_PKGSRCDIR.maeparser?=	../../biology/maeparser
+BUILDLINK_PKGSRCDIR.maeparser?=		../../biology/maeparser
 
 .include "../../devel/boost-libs/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
