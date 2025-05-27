2d7cc5ca2333519923ebc8badf1ae904461e6cff
bsiegert <bsiegert@pkgsrc.org>
2025-05-27 18:34:46 +0000

    esbuild: update to 0.25.5
    0.25.1
    
    Fix incorrect paths in inline source maps
    Fix invalid generated source maps
    Fix a regression with non-file source map paths
    Fix a crash with switch optimization
    
    0.25.2
    
    Support flags in regular expressions for the API
    Fix node-specific annotations for string literal export names
    Basic support for index source maps
    
    0.25.3
    
    Fix lowered async arrow functions before super()
    Fix memory leak with --watch=true
    More concise output for repeated legal comments
    Allow a custom host with the development server
    Add a limit to CSS nesting expansion
    Fix path resolution edge case
    
    0.25.4
    
    Add simple support for CORS to esbuild's development server
    Pass through invalid URLs in source maps unmodified
    Handle exports named __proto__ in ES modules

--- www/esbuild/go-modules.mk
+++ www/esbuild/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.3 2024/11/16 10:12:17 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.4 2025/05/27 18:34:46 bsiegert Exp $
 
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
