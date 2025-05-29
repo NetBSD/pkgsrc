baf3a24ed0174e00294c37c98257d7747f041407
adam <adam@pkgsrc.org>
2025-05-29 20:36:26 +0000

    haproxy: updated to 3.2.0
    3.2.0
    - MINOR: promex: Add agent check status/code/duration metrics
    - MINOR: ssl: support strict-sni in ssl-default-bind-options
    - MINOR: ssl: also provide the "tls-tickets" bind option
    - MINOR: server: define CLI I/O handler for "add server"
    - MINOR: server: implement "add server help"
    - MINOR: server: use stress mode for "add server help"
    - BUG/MEDIUM: server: fix crash after duplicate GUID insertion
    - BUG/MEDIUM: server: fix potential null-deref after previous fix
    - MINOR: config: list recently added sections with -dKcfg
    - BUG/MAJOR: cache: Crash because of wrong cache entry deleted
    - DOC: configuration: fix the example in crt-store
    - DOC: config: clarify the wording around single/double quotes
    - DOC: config: clarify the legacy cookie and header captures
    - DOC: config: fix alphabetical ordering of layer 7 sample fetch functions
    - DOC: config: fix alphabetical ordering of layer 6 sample fetch functions
    - DOC: config: fix alphabetical ordering of layer 5 sample fetch functions
    - DOC: config: fix alphabetical ordering of layer 4 sample fetch functions
    - DOC: config: fix alphabetical ordering of internal sample fetch functions
    - BUG/MINOR: h3: Set HTX flags corresponding to the scheme found in the request
    - BUG/MEDIUM: h3: Declare absolute URI as normalized when a :authority is found
    - DOC: config: mention in bytes_in and bytes_out that they're read on input
    - DOC: config: clarify the basics of ACLs (call point, multi-valued etc)
    - REGTESTS: Make the script testing conditional set-var compatible with Vtest2
    - REGTESTS: Explicitly allow failing shell commands in some scripts
    - MINOR: listeners: Add support for a label on bind line
    - BUG/MEDIUM: cli/ring: Properly handle shutdown in "show event" I/O handler
    - BUG/MEDIUM: hlua: Properly detect shudowns for TCP applets based on the new API
    - BUG/MEDIUM: hlua: Fix getline() for TCP applets to work with applet's buffers
    - BUG/MEDIUM: hlua: Fix receive API for TCP applets to properly handle shutdowns
    - CI: vtest: Rely on VTest2 to run regression tests
    - CI: vtest: Fix the build script to properly work on MaOS
    - CI: combine AWS-LC and AWS-LC-FIPS by template
    - BUG/MEDIUM: httpclient: Throw an error if an lua httpclient instance is reused
    - DOC: hlua: Add a note to warn user about httpclient object reuse
    - DOC: hlua: fix a few typos in HTTPMessage.set_body_len() documentation
    - DEV: patchbot: prepare for new version 3.3-dev
    - MINOR: version: mention that it's 3.2 LTS now.

--- net/haproxy/patches/patch-include_haproxy_proxy-t.h
+++ net/haproxy/patches/patch-include_haproxy_proxy-t.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-include_haproxy_proxy-t.h,v 1.2 2024/05/29 20:27:50 adam Exp $
+$NetBSD: patch-include_haproxy_proxy-t.h,v 1.3 2025/05/29 20:36:26 adam Exp $
 
 Avoid 'struct queue' conflict.
 
---- include/haproxy/proxy-t.h.orig	2024-05-29 12:43:38.000000000 +0000
+--- include/haproxy/proxy-t.h.orig	2025-05-28 14:35:14.000000000 +0000
 +++ include/haproxy/proxy-t.h
-@@ -350,7 +350,7 @@ struct proxy {
- 	__decl_thread(HA_RWLOCK_T lock);        /* may be taken under the server's lock */
+@@ -302,7 +302,7 @@ struct error_snapshot {
  
- 	char *id, *desc;			/* proxy id (name) and description */
--	struct queue queue;			/* queued requests (pendconns) */
-+	struct haqueue queue;			/* queued requests (pendconns) */
- 	int totpend;				/* total number of pending connections on this instance (for stats) */
- 	unsigned int feconn, beconn;		/* # of active frontend and backends streams */
- 	unsigned int fe_sps_lim;		/* limit on new sessions per second on the frontend */
+ /* Each proxy will have one occurrence of this structure per thread group */
+ struct proxy_per_tgroup {
+-	struct queue queue;
++	struct haqueue queue;
+ 	struct lbprm_per_tgrp lbprm;
+ } THREAD_ALIGNED(64);
+ 
