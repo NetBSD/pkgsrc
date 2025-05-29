e1d8680d3d1e1dd3ba0cfdc28c2798392a9357cb
adam <adam@pkgsrc.org>
2025-05-29 20:36:26 +0000

    haproxy: updated to 3.2.0
    3.2.0
    - MINOR: promex: Add agent check status/code/duration metrics
    - MINOR: ssl: support strict-sni in ssl-default-bind-options
    - MINOR: ssl: also provide the "tls-tickets" bind option
    - MINOR: server: define CLI I/O handler for "add server"
    - MINOR: server: implement "add server help"
    - MINOR: server: use stress mode for "add server help"
    - BUG/MEDIUM: server: fix crash after duplicate GUID insertion
    - BUG/MEDIUM: server: fix potential null-deref after previous fix
    - MINOR: config: list recently added sections with -dKcfg
    - BUG/MAJOR: cache: Crash because of wrong cache entry deleted
    - DOC: configuration: fix the example in crt-store
    - DOC: config: clarify the wording around single/double quotes
    - DOC: config: clarify the legacy cookie and header captures
    - DOC: config: fix alphabetical ordering of layer 7 sample fetch functions
    - DOC: config: fix alphabetical ordering of layer 6 sample fetch functions
    - DOC: config: fix alphabetical ordering of layer 5 sample fetch functions
    - DOC: config: fix alphabetical ordering of layer 4 sample fetch functions
    - DOC: config: fix alphabetical ordering of internal sample fetch functions
    - BUG/MINOR: h3: Set HTX flags corresponding to the scheme found in the request
    - BUG/MEDIUM: h3: Declare absolute URI as normalized when a :authority is found
    - DOC: config: mention in bytes_in and bytes_out that they're read on input
    - DOC: config: clarify the basics of ACLs (call point, multi-valued etc)
    - REGTESTS: Make the script testing conditional set-var compatible with Vtest2
    - REGTESTS: Explicitly allow failing shell commands in some scripts
    - MINOR: listeners: Add support for a label on bind line
    - BUG/MEDIUM: cli/ring: Properly handle shutdown in "show event" I/O handler
    - BUG/MEDIUM: hlua: Properly detect shudowns for TCP applets based on the new API
    - BUG/MEDIUM: hlua: Fix getline() for TCP applets to work with applet's buffers
    - BUG/MEDIUM: hlua: Fix receive API for TCP applets to properly handle shutdowns
    - CI: vtest: Rely on VTest2 to run regression tests
    - CI: vtest: Fix the build script to properly work on MaOS
    - CI: combine AWS-LC and AWS-LC-FIPS by template
    - BUG/MEDIUM: httpclient: Throw an error if an lua httpclient instance is reused
    - DOC: hlua: Add a note to warn user about httpclient object reuse
    - DOC: hlua: fix a few typos in HTTPMessage.set_body_len() documentation
    - DEV: patchbot: prepare for new version 3.3-dev
    - MINOR: version: mention that it's 3.2 LTS now.

--- net/haproxy/patches/patch-src_queue.c
+++ net/haproxy/patches/patch-src_queue.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_queue.c,v 1.2 2024/09/14 08:00:13 adam Exp $
+$NetBSD: patch-src_queue.c,v 1.3 2025/05/29 20:36:26 adam Exp $
 
 Avoid 'struct queue' conflict.
 
---- src/queue.c.orig	2024-09-03 13:37:09.000000000 +0000
+--- src/queue.c.orig	2025-05-28 14:35:14.000000000 +0000
 +++ src/queue.c
 @@ -177,7 +177,7 @@ static inline void pendconn_queue_unlock
   */
@@ -13,21 +13,30 @@ Avoid 'struct queue' conflict.
  	struct proxy  *px = q->px;
  	struct server *sv = q->sv;
  	uint oldidx;
-@@ -430,7 +430,7 @@ struct pendconn *pendconn_add(struct str
+@@ -555,7 +555,7 @@ struct pendconn *pendconn_add(struct str
  	struct pendconn *p;
  	struct proxy    *px;
  	struct server   *srv;
 -	struct queue    *q;
 +	struct haqueue    *q;
  	unsigned int *max_ptr;
+ 	unsigned int *queueslength;
  	unsigned int old_max, new_max;
+@@ -628,7 +628,7 @@ int pendconn_redistribute(struct server
+ 		goto skip_srv_queue;
  
-@@ -630,7 +630,7 @@ int pendconn_dequeue(struct stream *strm
-  */
- int pendconn_must_try_again(struct pendconn *p)
- {
--	struct queue  *q  = p->queue;
-+	struct haqueue *q  = p->queue;
- 	struct proxy  *px = q->px;
- 	struct server *sv = q->sv;
- 	int ret = 0;
+ 	for (i = 0; i < global.nbtgroups; i++) {
+-		struct queue *queue = &s->per_tgrp[i].queue;
++		struct haqueue *queue = &s->per_tgrp[i].queue;
+ 		int local_xferred = 0;
+ 
+ 		HA_SPIN_LOCK(QUEUE_LOCK, &queue->lock);
+@@ -663,7 +663,7 @@ int pendconn_redistribute(struct server
+ 		goto done;
+ 
+ 	for (i = 0; i < global.nbtgroups; i++) {
+-		struct queue *queue = &px->per_tgrp[i].queue;
++		struct haqueue *queue = &px->per_tgrp[i].queue;
+ 		int local_xferred = 0;
+ 
+ 		HA_SPIN_LOCK(QUEUE_LOCK, &queue->lock);
