342fc68c0a3c83b5a70d8f8a06111bdbe6f97980
wiz <wiz@pkgsrc.org>
2025-06-01 20:25:33 +0000

    mk: fix typo in comment

--- mk/openmp.mk
+++ mk/openmp.mk
@@ -1,8 +1,8 @@
-# $NetBSD: openmp.mk,v 1.1 2025/06/01 16:17:15 thor Exp $
+# $NetBSD: openmp.mk,v 1.2 2025/06/01 20:25:33 wiz Exp $
 #
 # This Makefile fragment is meant to be included by packages that use
 # OpenMP compiler extensions and may need an extra library package to
-# support that. GCC ships its own libgomp, clang pas it packaged separately.
+# support that. GCC ships its own libgomp, clang has it packaged separately.
 
 .if !empty(PKGSRC_COMPILER:M*clang*)
 .include "../../parallel/openmp/buildlink3.mk"
