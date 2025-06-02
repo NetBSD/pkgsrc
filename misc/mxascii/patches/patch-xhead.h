961c05bca1836a0bc591a2c84a7fdd6332e2c5a4
vins <vins@pkgsrc.org>
2025-06-02 07:45:16 +0000

    misc/mxascii: initial import of package
    MXascii is a X/Motif program to display ASCII character set (0-127)
    in decimal, hexadecimal, or octal.

---
+++ misc/mxascii/patches/patch-xhead.h
@@ -0,0 +1,14 @@
+$NetBSD: patch-xhead.h,v 1.1 2025/06/02 07:45:16 vins Exp $
+
+Do not require malloc.h
+
+--- xhead.h.orig	2025-06-02 07:30:16.310224142 +0000
++++ xhead.h
+@@ -26,7 +26,6 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include <memory.h>
+ #include <ctype.h>
+ #include <string.h>
