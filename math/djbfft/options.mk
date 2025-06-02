c1b56a32f67ec1729e6508c4d5d46c421a5460fd
schmonz <schmonz@pkgsrc.org>
2025-06-02 00:09:15 +0000

    djbfft: mollify pkglint.

--- math/djbfft/options.mk
+++ math/djbfft/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2007/02/22 19:26:47 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2025/06/02 00:09:15 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.djbfft
 PKG_OPTIONS_OPTIONAL_GROUPS=	optimization
@@ -11,13 +11,13 @@ PKG_SUGGESTED_OPTIONS+=	djbfft-sparc
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdjbfft-pentium)
-DJB_CONFIG_CMDS=	${ECHO} pentium > conf-opt
+DJB_CONFIG_CMDS=	${ECHO} pentium > conf-opt;
 .endif
 
 .if !empty(PKG_OPTIONS:Mdjbfft-ppro)
-DJB_CONFIG_CMDS=	${ECHO} ppro > conf-opt
+DJB_CONFIG_CMDS=	${ECHO} ppro > conf-opt;
 .endif
 
 .if !empty(PKG_OPTIONS:Mdjbfft-sparc)
-DJB_CONFIG_CMDS=	${ECHO} sparc > conf-opt
+DJB_CONFIG_CMDS=	${ECHO} sparc > conf-opt;
 .endif
