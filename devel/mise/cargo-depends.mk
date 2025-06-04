b079f4c42769e0692caad0a5df65736dc52aebc3
pin <pin@pkgsrc.org>
2025-06-04 07:48:24 +0000

    devel/mise: update to 2025.6.0
    2025.6.0 - 2025-06-02
    🐛 Bug Fixes
    
        race condition with uv_venv by @jdx in #5262
        disable victoria-metrics test by @jdx in 11bda4b
    
    2025.5.17 - 2025-05-31
    🚀 Features
    
        add railway cli by @jahands in #5083
    
    🐛 Bug Fixes
    (zig) exclude mach version from version list by @mangkoran in #5240
    refresh settings by @jdx in #5
    
        252
    
    ⚡ Performance
    
        re-enable parallelism for mise up by @jdx in #5249

--- devel/mise/cargo-depends.mk
+++ devel/mise/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.68 2025/05/30 07:36:37 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.69 2025/06/04 07:48:24 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
