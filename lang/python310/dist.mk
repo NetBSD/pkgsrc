d5f1c3d89cffa6e96161131bc9b6da682088f1aa
adam <adam@pkgsrc.org>
2025-06-04 14:13:37 +0000

    python310 py310-html-docs: updated to 3.10.18
    Python 3.10.18 final
    
    Security
    
    gh-135034: Fixes multiple issues that allowed tarfile extraction filters (filter="data" and filter="tar") to be bypassed using crafted symlinks and hard links.
    
    Addresses CVE 2024-12718, CVE 2025-4138, CVE 2025-4330, and CVE 2025-4517.
    
    gh-133767: Fix use-after-free in the “unicode-escape” decoder with a non-“strict” error handler.
    
    gh-128840: Short-circuit the processing of long IPv6 addresses early in ipaddress to prevent excessive memory consumption and a minor denial-of-service.
    
    Library
    
    gh-128840: Fix parsing long IPv6 addresses with embedded IPv4 address.
    gh-134062: ipaddress: fix collisions in __hash__() for IPv4Network and IPv6Network objects.
    gh-123409: Fix ipaddress.IPv6Address.reverse_pointer output according to RFC 3596, §2.5. Patch by Bénédikt Tran.
    bpo-43633: Improve the textual representation of IPv4-mapped IPv6 addresses (RFC 4291 Sections 2.2, 2.5.5.2) in ipaddress. Patch by Oleksandr Pavliuk.

--- lang/python310/dist.mk
+++ lang/python310/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.18 2025/04/09 14:46:45 adam Exp $
+# $NetBSD: dist.mk,v 1.19 2025/06/04 14:13:37 adam Exp $
 
-PY_DISTVERSION=	3.10.17
+PY_DISTVERSION=	3.10.18
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python310/distinfo
