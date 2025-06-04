033c6fabf60df34e427954f1dbf53f0f4bf19450
markd <markd@pkgsrc.org>
2025-06-04 11:17:44 +0000

    mailman3-web: fixes for py-django-allauth >= 65.5

--- www/mailman3-web/patches/patch-mailman__web_settings_mailman.py
+++ www/mailman3-web/patches/patch-mailman__web_settings_mailman.py
@@ -1,9 +1,24 @@
-$NetBSD: patch-mailman__web_settings_mailman.py,v 1.1 2023/06/10 06:00:53 markd Exp $
+$NetBSD: patch-mailman__web_settings_mailman.py,v 1.2 2025/06/04 11:17:44 markd Exp $
+
+https://gitlab.com/mailman/mailman-web/-/commit/3713b55989fbf0e5e79a6df64fd41cfdd3353262
+https://gitlab.com/mailman/mailman-web/-/commit/1af9f7c900c310b60ac17ae71e0cbb68ee97c0b9
+  for django-allauth >= 65.5
 
 configure paths
 
---- mailman_web/settings/mailman.py.orig	2022-09-10 14:41:36.000000000 +0000
+--- mailman_web/settings/mailman.py.orig	2024-03-07 08:49:37.048697200 +0000
 +++ mailman_web/settings/mailman.py
+@@ -28,8 +28,8 @@ DEFAULT_FROM_EMAIL = 'postorius@localhos
+ 
+ 
+ #: Django Allauth
+-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+-ACCOUNT_EMAIL_REQUIRED = True
++ACCOUNT_LOGIN_METHODS = {'email', 'username'}
++ACCOUNT_SIGNUP_FIELDS = ['username*', 'email*', 'password1*', 'password2*']
+ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ ACCOUNT_UNIQUE_EMAIL = True
+ 
 @@ -93,7 +93,7 @@ AUTHENTICATION_BACKENDS = (
  HAYSTACK_CONNECTIONS = {
      'default': {
