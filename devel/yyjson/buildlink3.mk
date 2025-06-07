c1918c08b624ef48d5f5caddf48c7bc550c34f15
vins <vins@pkgsrc.org>
2025-06-07 15:34:05 +0000

    devel/yyjson: update to 0.11.1
    Ths is a bug fix release.
    
    * Fix errors when unaligned access is disallowed

--- devel/yyjson/buildlink3.mk
+++ devel/yyjson/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/05/11 07:35:06 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/06/07 15:34:05 vins Exp $
 
 BUILDLINK_TREE+=	yyjson
 
@@ -8,7 +8,7 @@ YYJSON_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.yyjson?=	build
 
 BUILDLINK_API_DEPENDS.yyjson+=	yyjson>=0.10.0
-BUILDLINK_ABI_DEPENDS.yyjson+=	yyjson>=0.11.0
+BUILDLINK_ABI_DEPENDS.yyjson+=	yyjson>=0.11.1
 BUILDLINK_PKGSRCDIR.yyjson?=	../../devel/yyjson
 
 BUILDLINK_LIBNAME.yyjson=	yyjson
