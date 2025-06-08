69ffe381efdf3cd4f9d438bc750551ef35ec41a4
maya <maya@pkgsrc.org>
2025-06-08 19:00:23 +0000

    Pullup ticket #6971 - requested by bsiegert lang/go123: Security fix lang/go124: Security fix
    Revisions pulled up:
    - lang/go/version.mk                                            1.232
    - lang/go123/PLIST                                              1.10
    - lang/go123/distinfo                                           1.12
    - lang/go124/PLIST                                              1.5
    - lang/go124/distinfo                                           1.5
    
    ---
       Module Name:    pkgsrc
       Committed By:   bsiegert
       Date:           Fri Jun  6 13:45:15 UTC 2025
    
       Modified Files:
               pkgsrc/lang/go: version.mk
               pkgsrc/lang/go123: PLIST distinfo
               pkgsrc/lang/go124: PLIST distinfo
    
       Log Message:
       Update go123 to 1.23.10 and go124 to 1.24.4 (security)
    
       These minor releases include 3 security fixes following the security policy=
       :
    
       -   net/http: sensitive headers not cleared on cross-origin redirect
    
           Proxy-Authorization and Proxy-Authenticate headers persisted on
           cross-origin redirects potentially leaking sensitive information.
    
           Thanks to Takeshi Kaneko (GMO Cybersecurity by Ierae, Inc.) for reporti=
       ng
           this issue.
    
           This is CVE-2025-4673 and Go issue https://go.dev/issue/73816.
    
       -   os: inconsistent handling of O_CREATE|O_EXCL on Unix and Windows
    
           os.OpenFile(path, os.O_CREATE|O_EXCL) behaved differently on Unix and
           Windows systems when the target path was a dangling symlink. On Unix
           systems, OpenFile with O_CREATE and O_EXCL flags never follows symlinks=
       .
           On Windows, when the target path was a symlink to a nonexistent locatio=
       n,
           OpenFile would create a file in that location.
    
           OpenFile now always returns an error when the O_CREATE and O_EXCL flags
           are both set and the target path is a symlink.
    
           Thanks to Junyoung Park and Dong-uk Kim of KAIST Hacking Lab for
           discovering this issue.
    
           This is CVE-2025-0913 and Go issue https://go.dev/issue/73702.
    
       -   crypto/x509: usage of ExtKeyUsageAny disables policy validation
    
           Calling Verify with a VerifyOptions.KeyUsages that contains ExtKeyUsage=
       Any
           unintentionally disabledpolicy validation. This only affected certifica=
       te
           chains which contain policy graphs, which are rather uncommon.
    
           Thanks to Krzysztof Skrz=C4=99tnicki (@Tener) of Teleport for reporting=
        this
           issue.
    
           This is CVE-2025-22874 and Go issue https://go.dev/issue/73612.
    
       View the release notes for more information:
       https://go.dev/doc/devel/release#go1.24.4

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.228.2.2 2025/05/16 14:05:01 maya Exp $
+# $NetBSD: version.mk,v 1.228.2.3 2025/06/08 19:00:23 maya Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,8 +6,8 @@
 #
 .include "go-vars.mk"
 
-GO124_VERSION=	1.24.3
-GO123_VERSION=	1.23.9
+GO124_VERSION=	1.24.4
+GO123_VERSION=	1.23.10
 GO122_VERSION=	1.22.12
 GO121_VERSION=	1.21.13
 GO120_VERSION=	1.20.14
