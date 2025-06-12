3852d9de1f77271996515e52fc3a2542330654f2
adam <adam@pkgsrc.org>
2025-06-12 11:06:14 +0000

    python313 py313-html-docs: updated to 3.13.5
    Python 3.13.5
    
    Windows
    
    gh-135151: Avoid distributing modified pyconfig.h in the traditional installer. Extension module builds must always specify Py_GIL_DISABLED when targeting the free-threaded runtime.
    
    Tests
    
    gh-135120: Add test.support.subTests().
    
    Library
    
    gh-133967: Do not normalize locale name ‘C.UTF-8’ to ‘en_US.UTF-8’.
    gh-135326: Restore support of integer-like objects with __index__() in random.getrandbits().
    gh-135321: Raise a correct exception for values greater than 0x7fffffff for the BINSTRING opcode in the C implementation of pickle.
    gh-135276: Backported bugfixes in zipfile.Path from zipp 3.23. Fixed .name, .stem and other basename-based properties on Windows when working with a zipfile on disk.
    gh-134151: email: Fix TypeError in email.utils.decode_params() when sorting RFC 2231 continuations that contain an unnumbered section.
    gh-134152: email: Fix parsing of email message ID with invalid domain.
    gh-127081: Fix libc thread safety issues with os by replacing getlogin with getlogin_r re-entrant version.
    gh-131884: Fix formatting issues in json.dump() when both indent and skipkeys are used.
    
    Core and Builtins
    
    gh-135171: Roll back changes to generator and list comprehensions that went into 3.13.4 to fix gh-127682, but which involved semantic and bytecode changes not appropriate for a bugfix release.
    
    C API
    
    gh-134989: Fix Py_RETURN_NONE, Py_RETURN_TRUE and Py_RETURN_FALSE macros in the limited C API 3.11 and older: don’t treat Py_None, Py_True and Py_False as immortal. Patch by Victor Stinner.
    gh-134989: Implement PyObject_DelAttr() and PyObject_DelAttrString() as macros in the limited C API 3.12 and older. Patch by Victor Stinner.

--- lang/python313/dist.mk
+++ lang/python313/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.5 2025/06/04 14:15:56 adam Exp $
+# $NetBSD: dist.mk,v 1.6 2025/06/12 11:06:14 adam Exp $
 
-PY_DISTVERSION=	3.13.4
+PY_DISTVERSION=	3.13.5
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python313/distinfo
