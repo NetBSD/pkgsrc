885881f76127606f047453d1d41c0590249adbdb
wiz <wiz@pkgsrc.org>
2025-06-13 06:46:19 +0000

    gimp: directly use ghostscript-agpl
    since this doesn't compile with ghostscript-gpl, see PR 59464
    
    make the ghostscript-agpl dependency a default-on option
    
    Bump PKGREVISION.

--- graphics/gimp/options.mk
+++ graphics/gimp/options.mk
@@ -1,27 +1,35 @@
-# $NetBSD: options.mk,v 1.11 2025/05/19 11:20:01 wiz Exp $
+# $NetBSD: options.mk,v 1.12 2025/06/13 06:46:19 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gimp
-PKG_SUPPORTED_OPTIONS=	aalib mng xpm
-PKG_SUGGESTED_OPTIONS=	mng xpm
+PKG_SUPPORTED_OPTIONS=	aalib ghostscript mng xpm
+PKG_SUGGESTED_OPTIONS=	ghostscript mng xpm
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		aalib mng xpm
+PLIST_VARS+=		aalib ghostscript mng xpm
 
+PRINT_PLIST_AWK+=       { if (/file-aa/) $$0 = "$${PLIST.aalib}" $$0 }
 .if !empty(PKG_OPTIONS:Maalib)
 .include "../../graphics/aalib/buildlink3.mk"
-PRINT_PLIST_AWK+=       { if (/file-aa/) $$0 = "$${PLIST.aalib}" $$0 }
 PLIST.aalib=		yes
 .endif
 
+PRINT_PLIST_AWK+=       { if (/file-ps$$/) $$0 = "$${PLIST.ghostscript}" $$0 }
+.if !empty(PKG_OPTIONS:Mghostscript)
+.include "../../print/ghostscript-agpl/buildlink3.mk"
+PLIST.ghostscript=	yes
+.else
+MESON_ARGS+=	-Dghostscript=disabled
+.endif
+
+PRINT_PLIST_AWK+=       { if (/file-mng/) $$0 = "$${PLIST.mng}" $$0 }
 .if !empty(PKG_OPTIONS:Mmng)
 .include "../../graphics/mng/buildlink3.mk"
-PRINT_PLIST_AWK+=       { if (/file-mng/) $$0 = "$${PLIST.mng}" $$0 }
 PLIST.mng=		yes
 .endif
 
+PRINT_PLIST_AWK+=       { if (/file-xpm/) $$0 = "$${PLIST.xpm}" $$0 }
 .if !empty(PKG_OPTIONS:Mxpm)
 .include "../../x11/libXpm/buildlink3.mk"
-PRINT_PLIST_AWK+=       { if (/file-xpm/) $$0 = "$${PLIST.xpm}" $$0 }
 PLIST.xpm=		yes
 .endif
