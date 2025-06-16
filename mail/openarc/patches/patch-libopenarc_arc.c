cc75ae6c7bc18197d9decc5412f8f209760cadec
ryoon <ryoon@pkgsrc.org>
2025-06-16 16:17:05 +0000

    mail/openarc: Do not fail validation of emails from Microsoft email services
    * Do not fail when t= is not available.
    * Bump PKGREVISION.

--- mail/openarc/patches/patch-libopenarc_arc.c
+++ mail/openarc/patches/patch-libopenarc_arc.c
@@ -1,14 +1,23 @@
-$NetBSD: patch-libopenarc_arc.c,v 1.1 2023/04/21 00:49:56 manu Exp $
+$NetBSD: patch-libopenarc_arc.c,v 1.2 2025/06/16 16:17:05 ryoon Exp $
 
 Fix buffer overrun
 
 From upstream
 https://github.com/trusteddomainproject/OpenARC/pull/117
 
---- libopenarc/arc.c.orig	2021-03-30 15:33:39.683325974 +0200
-+++ libopenarc/arc.c	2021-03-30 15:34:50.693955186 +0200
-@@ -2329,8 +2329,12 @@
- 
+--- libopenarc/arc.c.orig	2018-09-21 15:51:23.000000000 +0000
++++ libopenarc/arc.c
+@@ -1664,8 +1664,7 @@ arc_process_set(ARC_MESSAGE *msg, arc_kv
+ 		    arc_param_get(set, (u_char *) "b") == NULL ||
+ 		    arc_param_get(set, (u_char *) "s") == NULL ||
+ 		    arc_param_get(set, (u_char *) "d") == NULL ||
+-		    arc_param_get(set, (u_char *) "a") == NULL ||
+-		    arc_param_get(set, (u_char *) "t") == NULL)
++		    arc_param_get(set, (u_char *) "a") == NULL)
+ 		{
+ 			arc_error(msg, "missing parameter(s) in %s data",
+ 			          settype);
+@@ -2330,6 +2329,10 @@ arc_parse_header_field(ARC_MESSAGE *msg,
  	while (end > hdr && isascii(*(end - 1)) && isspace(*(end - 1)))
  		end--;
  
@@ -19,9 +28,7 @@ https://github.com/trusteddomainproject/OpenARC/pull/117
  	/* don't allow a field name containing a semicolon */
  	semicolon = memchr(hdr, ';', hlen);
  	if (semicolon != NULL && colon != NULL && semicolon < colon)
- 		return ARC_STAT_SYNTAX;
-@@ -2676,8 +2680,9 @@
- 
+@@ -2677,6 +2680,7 @@ arc_eoh(ARC_MESSAGE *msg)
  	for (h = msg->arc_hhead; h != NULL; h = h->hdr_next)
  	{
  		char hnbuf[ARC_MAXHEADER + 1];
@@ -29,4 +36,3 @@ https://github.com/trusteddomainproject/OpenARC/pull/117
  
  		memset(hnbuf, '\0', sizeof hnbuf);
  		strncpy(hnbuf, h->hdr_text, h->hdr_namelen);
- 		if (strcasecmp(hnbuf, ARC_AR_HDRNAME) == 0 ||
