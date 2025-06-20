0018a1ad17f57c923c2f793a9bf023336814f6b2
nia <nia@pkgsrc.org>
2025-06-20 17:04:41 +0000

    mariadb1011-client: Fix server build on illumos
    via OmniOS CE

--- databases/mariadb1011-client/patches/patch-sql_mysqld.cc
+++ databases/mariadb1011-client/patches/patch-sql_mysqld.cc
@@ -1,11 +1,19 @@
-$NetBSD: patch-sql_mysqld.cc,v 1.3 2024/08/17 00:19:21 nia Exp $
+$NetBSD: patch-sql_mysqld.cc,v 1.4 2025/06/20 17:04:41 nia Exp $
 
-Add missing declaration of "concurrency".
-Fixes build on illumos. From OmniOS CE.
+Build fixes for illumos. From OmniOS CE.
 
---- sql/mysqld.cc.orig	2024-08-03 07:29:59.000000000 +0000
+--- sql/mysqld.cc.orig	2025-05-19 16:14:25.000000000 +0000
 +++ sql/mysqld.cc
-@@ -455,7 +455,7 @@ Atomic_counter<uint32_t> THD_count::coun
+@@ -421,7 +421,7 @@ char* opt_secure_file_priv;
+ my_bool lower_case_file_system= 0;
+ my_bool opt_large_pages= 0;
+ #ifdef HAVE_SOLARIS_LARGE_PAGES
+-my_bool opt_super_large_pages= 0;
++extern my_bool opt_super_large_pages;
+ #endif
+ my_bool opt_myisam_use_mmap= 0;
+ uint   opt_large_page_size= 0;
+@@ -457,7 +457,7 @@ Atomic_counter<uint32_t> THD_count::coun
  bool shutdown_wait_for_slaves;
  Atomic_counter<uint32_t> slave_open_temp_tables;
  ulong thread_created;
