8b364c1d368c180494d9f17249743068ba4800d5
wiz <wiz@pkgsrc.org>
2025-06-21 15:08:19 +0000

    libkate: fix PLIST for doxygen 1.14 and depend on it
    From Robert Whitlock in PR 59477.

--- multimedia/libkate/options.mk
+++ multimedia/libkate/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.13 2024/08/04 15:56:01 wiz Exp $
+# $NetBSD: options.mk,v 1.14 2025/06/21 15:08:19 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libkate
 PKG_SUPPORTED_OPTIONS=	debug doc oggz valgrind vorbis
@@ -13,7 +13,7 @@ CONFIGURE_ARGS+=	--enable-debug
 
 .if !empty(PKG_OPTIONS:Mdoc)
 PLIST.doc=		yes
-TOOL_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
+TOOL_DEPENDS+=		doxygen>=1.14:../../devel/doxygen
 .else
 CONFIGURE_ARGS+=	--disable-doc
 .endif
