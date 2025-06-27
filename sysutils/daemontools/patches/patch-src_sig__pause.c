a63d408d4deaed9954b600689311c92515e5a10a
schmonz <schmonz@pkgsrc.org>
2025-06-27 17:30:46 +0000

    daemontools: fix SmartOS build. Still good on NetBSD, macOS.

---
+++ sysutils/daemontools/patches/patch-src_sig__pause.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-src_sig__pause.c,v 1.1 2025/06/27 17:30:46 schmonz Exp $
+
+Find sigprocmask on Illumos when in C89 mode.
+
+--- src/sig_pause.c.orig	2001-07-12 16:49:49.000000000 +0000
++++ src/sig_pause.c
+@@ -1,5 +1,6 @@
+ /* Public domain. */
+ 
++#define _POSIX_C_SOURCE 1
+ #include <signal.h>
+ #include "sig.h"
+ #include "hassgprm.h"
