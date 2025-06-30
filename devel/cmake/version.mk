4bb5d9cb9b2c764df4a3bc530581b8210c5979ff
adam <adam@pkgsrc.org>
2025-06-30 13:14:28 +0000

    cmake cmake-gui: updated to 3.31.8
    Changes made since CMake 3.31.7:
    * CUDA/Clang: Fix list of architectures supported by Clang < 20.1
    * CUDA/Clang: Reorder the logic for setting supported archs
    * CUDA/Clang: Add archs supported by Clang 21
    * Tests/RunCMake: handle C++26 support where needed
    * gcc: support `import std`
    * Experimental: recycle the `import std` UUID
    * Clang/CXXImportStd: support `-stdlib=libstdc++`
    * experimental/CXXModules: recycle the UUID
    * ci: Tell CudaOnly.Architecture test what specific architecture to use
    * ci: add tags to tie CUDA jobs to runners with hardware supporting them
    * CUDA/Clang: Prefer NVCC's default architecture for each CUDA Toolkit version
    * AUTOMOC: Avoid compiler warnings while computing predefines
    * ci: Disable KWSys ConsoleBuf test that breaks on Windows Terminal 1.22+
    * ci: Tie Intel 2024.1 jobs on Windows to MSVC 14.43 toolset
    * ci: Update Windows builds to MSVC 14.44 toolset
    * macOS: Reliably apply workaround for system curl 8.{3,4,5} LibreSSL backend
    * FindPython: rely on ABIFLAGS on Windows for ABI profile
    * FindPython: Avoid implicit link library on Windows
    * ci: Tell CudaOnly.CompileFlags test what specific architecture to use
    * CUDA/Clang: Update architectures supported by CUDA 12.8
    * CUDA: Add CUDA 12.9 new architectures and family syntax
    * CUDA/Clang: Update nvcc default architectures with CUDA 13 deprecations
    * CUDA: Add include paths from nvcc SYSTEM_INCLUDES entry
    * KWSys: Backport "Fix GetNumberOfLogicalCPU on s390x" to CMake 3.31

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.76 2025/04/11 14:02:46 adam Exp $
+# $NetBSD: version.mk,v 1.77 2025/06/30 13:14:28 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.31.7
+CMAKE_VERSION=	3.31.8
 CMAKE_API=	${CMAKE_VERSION:R}
