34d8896a3eaeb2897d7ddd90cdd28ce5fc89ea5f
wiz <wiz@pkgsrc.org>
2025-07-01 21:18:51 +0000

    cargo-c: update to 0.10.14.
    No useful changelog found.

--- devel/cargo-c/patches/patch-.._vendor_memchr-2.7.5_src_memmem_searcher.rs
+++ devel/cargo-c/patches/patch-.._vendor_memchr-2.7.5_src_memmem_searcher.rs
@@ -1,9 +1,9 @@
-$NetBSD: patch-.._vendor_memchr-2.7.4_src_memmem_searcher.rs,v 1.1 2025/05/08 06:30:27 pin Exp $
+$NetBSD: patch-.._vendor_memchr-2.7.5_src_memmem_searcher.rs,v 1.1 2025/07/01 21:18:51 wiz Exp $
 
 Don't try to use neon / SIMD on big-endian aarch64.
 
---- ../vendor/memchr-2.7.4/src/memmem/searcher.rs.orig	2025-02-15 09:20:25.984377323 +0000
-+++ ../vendor/memchr-2.7.4/src/memmem/searcher.rs
+--- ../vendor/memchr-2.7.5/src/memmem/searcher.rs.orig	2025-02-15 09:20:25.984377323 +0000
++++ ../vendor/memchr-2.7.5/src/memmem/searcher.rs
 @@ -3,7 +3,7 @@ use crate::arch::all::{
      rabinkarp, twoway,
  };
