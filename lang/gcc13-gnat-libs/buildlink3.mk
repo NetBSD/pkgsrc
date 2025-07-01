05fb2ae7307ce82e63c5bd7d0c375de2b560356f
dkazankov <dkazankov@pkgsrc.org>
2025-07-01 20:34:51 +0000

    lang/gcc13-gnat-libs: fix var name cross usage with gcc

--- lang/gcc13-gnat-libs/buildlink3.mk
+++ lang/gcc13-gnat-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/02/16 10:10:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/07/01 20:34:51 dkazankov Exp $
 
 BUILDLINK_TREE+=	gcc13-gnat-libs
 
@@ -10,18 +10,19 @@ BUILDLINK_API_DEPENDS.gcc13-gnat-libs+=	gcc13-gnat-libs>=13.2.0
 .else
 BUILDLINK_API_DEPENDS.gcc13-gnat-libs+=	{gcc13-gnat,gcc13-gnat-libs}>=13.2.0
 .endif
-BUILDLINK_PKGSRCDIR.gcc13-gnat-libs=	../../lang/gcc13-gnat-libs
+BUILDLINK_PKGSRCDIR.gcc13-gnat-libs?=	../../lang/gcc13-gnat-libs
 BUILDLINK_DEPMETHOD.gcc13-gnat-libs?=	full
 
-GCC_TARGET_MACHINE?=	${MACHINE_GNU_PLATFORM}
+#GCC_TARGET_MACHINE?=	${MACHINE_GNU_PLATFORM}
 
-TARGET_LIBDIR.gcc13-gnat-libs=	${PREFIX}/gcc13-gnat/${GCC_TARGET_MACHINE}/lib
+#TARGET_LIBDIR.gcc13-gnat-libs=	${PREFIX}/gcc13-gnat/${GCC_TARGET_MACHINE}/lib
 
-BUILDLINK_PASSTHRU_DIRS+=	${TARGET_LIBDIR.gcc13-gnat-libs}
+#BUILDLINK_PASSTHRU_DIRS+=	${TARGET_LIBDIR.gcc13-gnat-libs}
 
-BUILDLINK_FILES.gcc13-gnat-libs=	#empty
-BUILDLINK_AUTO_VARS.gcc13-gnat-libs=	no
-BUILDLINK_AUTO_DIRS.gcc13-gnat-libs=	no
+#BUILDLINK_FILES.gcc13-gnat-libs=	#empty
+#BUILDLINK_AUTO_VARS.gcc13-gnat-libs=	no
+#BUILDLINK_AUTO_DIRS.gcc13-gnat-libs=	no
+BUILDLINK_CONTENTS_FILTER.gcc13-gnat-libs=	${FALSE}
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
