3d5286ab7209133758380f28165b65e93f876bc8
wiz <wiz@pkgsrc.org>
2025-07-01 18:20:51 +0000

    mame: update to 0.278.
    After two long months, MAME 0.278 is finally ready. Of course, the
    big news is that the new sound system has arrived! The benefits it
    brings include:
    
    - Native WASAPI support on Windows and PipeWire support on Linux.
    - Support for sound input for emulated systems that have microphones
      or other audio capture hardware.
    - Support for multi-channel input and output.
    - Built-in effects, including a parametric equaliser and dynamic
      range compressor.
    - Better quality sample rate conversion and mixing, and lower latency.
    
    As this is the first release of a major new component, it’s going
    to be rough around the edges in some ways. But we’re already thinking
    about some of the ways we can improve it further over the coming
    months.
    
    We’ve been busy with the sound system, but we haven’t stopped
    working on emulation. There are some nice fixes for graphical issues
    in 3D systems, including Sega Model 2 and Taito Type Zero. But it
    doesn’t stop with 3D – the 2D classics are still getting love.
    Quite a few Konami games are looking nicer, including often-overlooked
    cocktail mode support, and all the missing graphics in the iconic
    IGS mahjong game Long Hu Bang are finally fixed.
    
    There’s a long list of newly supported systems this month. We’ve
    got Exidy and Taito arcade games from the 1970s, LCD games from
    behind the Iron Curtain, and a whole pile of recently dumped TV
    games. On top of that, numerous additional versions of Gaelco arcade
    games have surfaced from the archives. In between, you’ll find rare
    Capcom arcade releases, music workstations and sequencers, game
    watches, casino games, and more. If you do play casino games, please
    be aware that some of the default input assignments for gambling
    games have changed in this release.
    
    There’s lots of fun to be had with the computer emulation updates
    in this release. The Victor 9000 has had its floppy support overhauled
    and SASI hard disk support added. There’s been a bit of a flurry
    of updates for the Sanyo PHC-25. The POKEY’s serial communication
    support used by the Atari 8-bit computers has finally been implemented
    properly.

--- emulators/mame/patches/patch-3rdparty_bx_src_thread.cpp
+++ emulators/mame/patches/patch-3rdparty_bx_src_thread.cpp
@@ -1,17 +1,17 @@
-$NetBSD: patch-3rdparty_bx_src_thread.cpp,v 1.5 2023/02/27 11:23:23 wiz Exp $
+$NetBSD: patch-3rdparty_bx_src_thread.cpp,v 1.6 2025/07/01 18:20:51 wiz Exp $
 
 NetBSD does not provide pthread_np.h.
 https://github.com/bkaradzic/bx/issues/295
 
---- 3rdparty/bx/src/thread.cpp.orig	2023-02-21 17:59:53.000000000 +0000
+--- 3rdparty/bx/src/thread.cpp.orig	2025-06-28 20:55:26.000000000 +0000
 +++ 3rdparty/bx/src/thread.cpp
-@@ -25,7 +25,9 @@
- 	|| BX_PLATFORM_NX
+@@ -17,7 +17,9 @@
+ #elif  BX_PLATFORM_POSIX
  #	include <pthread.h>
  #	if BX_PLATFORM_BSD
 +#ifndef __NetBSD__
  #		include <pthread_np.h>
 +#endif
  #	endif // BX_PLATFORM_BSD
- #	if BX_PLATFORM_LINUX && (BX_CRT_GLIBC < 21200)
- #		include <sys/prctl.h>
+ #elif  BX_PLATFORM_WINDOWS \
+ 	|| BX_PLATFORM_WINRT   \
