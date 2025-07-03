9d3607c649534910e09069f51dbdcfaa3e26706d
wiz <wiz@pkgsrc.org>
2025-07-03 06:28:29 +0000

    *: recursive bump for abseil shlib major change

--- devel/re2/buildlink3.mk
+++ devel/re2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2025/02/09 14:44:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/07/03 06:28:31 wiz Exp $
 
 BUILDLINK_TREE+=	re2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	re2
 RE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.re2+=	re2>=20160201
-BUILDLINK_ABI_DEPENDS.re2+=	re2>=20240702nb1
+BUILDLINK_ABI_DEPENDS.re2+=	re2>=20250626bnb1
 BUILDLINK_PKGSRCDIR.re2?=	../../devel/re2
 
 .include "../../devel/abseil/buildlink3.mk"
