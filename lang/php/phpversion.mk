6e8a4ab115e1ba20d47ec8e3623c0a747b7fb78f
maya <maya@pkgsrc.org>
2025-07-04 14:13:22 +0000

    Pullup ticket #6976 - requested by taca lang/php82: Security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.468
    - lang/php82/Makefile                                           1.21
    - lang/php82/distinfo                                           1.34
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Jul  4 01:23:50 UTC 2025
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php82: Makefile distinfo
    
       Log Message:
       lang/php82: update to 8.2.29
    
       PHP 8.2.29 (2025-07-03)
    
       - PGSQL:
         . Fixed GHSA-hrwm-9436-5mv3 (pgsql extension does not check for errors during
           escaping). (CVE-2025-1735) (Jakub Zelenka)
    
       - SOAP:
         . Fixed GHSA-453j-q27h-5p8x (NULL Pointer Dereference in PHP SOAP Extension
           via Large XML Namespace Prefix). (CVE-2025-6491) (Lekssays, nielsdos)
    
       - Standard:
         . Fixed GHSA-3cr5-j632-f35r (Null byte termination in hostnames).
           (CVE-2025-1220) (Jakub Zelenka)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.466 2025/06/08 16:27:30 taca Exp $
+# $NetBSD: phpversion.mk,v 1.466.2.1 2025/07/04 14:13:22 maya Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -111,7 +111,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.32
-PHP82_VERSION=	8.2.28
+PHP82_VERSION=	8.2.29
 PHP83_VERSION=	8.3.22
 PHP84_VERSION=	8.4.8
 
