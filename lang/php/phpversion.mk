2a63a21f01a71989a3a5bc4d0044fec704d0f338
taca <taca@pkgsrc.org>
2025-07-04 01:25:48 +0000

    lang/php83: update to 8.3.23
    PHP 8.3.23 (2025-07-03)
    
    - Core:
      . Fixed GH-18695 (zend_ast_export() - float number is not preserved).
        (Oleg Efimov)
      . Do not delete main chunk in zend_gc. (danog, Arnaud)
      . Fix compile issues with zend_alloc and some non-default options. (nielsdos)
    
    - Curl:
      . Fix memory leak when setting a list via curl_setopt fails. (nielsdos)
      . Fix incorrect OpenSSL version detection. (Peter Kokot)
    
    - Date:
      . Fix leaks with multiple calls to DatePeriod iterator current(). (nielsdos)
    
    - FPM:
      . Fixed GH-18662 (fpm_get_status segfault). (txuna)
    
    - Hash:
      . Fixed bug GH-14551 (PGO build fails with xxhash). (nielsdos)
    
    - Intl:
      . Fix memory leak in intl_datetime_decompose() on failure. (nielsdos)
      . Fix memory leak in locale lookup on failure. (nielsdos)
    
    - ODBC:
      . Fix memory leak on php_odbc_fetch_hash() failure. (nielsdos)
    
    - Opcache:
      . Fixed bug GH-18743 (Incompatibility in Inline TLS Assembly on Alpine 3.22).
        (nielsdos, Arnaud)
    
    - OpenSSL:
      . Fix memory leak of X509_STORE in php_openssl_setup_verify() on failure.
        (nielsdos)
      . Fixed bug #74796 (Requests through http proxy set peer name).
        (Jakub Zelenka)
    
    - PGSQL:
      . Fixed GHSA-hrwm-9436-5mv3 (pgsql extension does not check for errors during
        escaping). (CVE-2025-1735) (Jakub Zelenka)
    
    - Phar:
      . Add missing filter cleanups on phar failure. (nielsdos)
      . Fixed bug GH-18642 (Signed integer overflow in ext/phar fseek). (nielsdos)
    
    - PHPDBG:
      . Fix 'phpdbg --help' segfault on shutdown with USE_ZEND_ALLOC=0. (nielsdos)
    
    - PDO ODBC:
      . Fix memory leak if WideCharToMultiByte() fails. (nielsdos)
    
    - PGSQL:
      . Fix warning not being emitted when failure to cancel a query with
        pg_cancel_query(). (Girgias)
    
    - Random:
      . Fix reference type confusion and leak in user random engine.
        (nielsdos, timwolla)
    
    - Readline:
      . Fix memory leak when calloc() fails in php_readline_completion_cb().
        (nielsdos)
    
    - SOAP:
      . Fix memory leaks in php_http.c when call_user_function() fails. (nielsdos)
      . Fixed GHSA-453j-q27h-5p8x (NULL Pointer Dereference in PHP SOAP Extension
        via Large XML Namespace Prefix). (CVE-2025-6491) (Lekssays, nielsdos)
    
    - Standard:
      . Fixed GHSA-3cr5-j632-f35r (Null byte termination in hostnames).
        (CVE-2025-1220) (Jakub Zelenka)
    
    - Tidy:
      . Fix memory leak in tidy output handler on error. (nielsdos)
      . Fix tidyOptIsReadonly deprecation, using tidyOptGetCategory. (David Carlier)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.469 2025/07/04 01:24:52 taca Exp $
+# $NetBSD: phpversion.mk,v 1.470 2025/07/04 01:25:48 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -112,7 +112,7 @@ PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.33
 PHP82_VERSION=	8.2.29
-PHP83_VERSION=	8.3.22
+PHP83_VERSION=	8.3.23
 PHP84_VERSION=	8.4.10
 
 _VARGROUPS+=	php
