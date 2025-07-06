a56c65ed53008f87ae953ebac4ef3e3bfecc80dd
wiz <wiz@pkgsrc.org>
2025-07-06 19:00:52 +0000

    py-pillow_heif: fix build with libheif 1.20.1

---
+++ graphics/py-pillow_heif/patches/patch-pillow__heif___pillow__heif.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-pillow__heif___pillow__heif.c,v 1.1 2025/07/06 19:00:52 wiz Exp $
+
+Add header needed by libheif 1.20.1.
+
+--- pillow_heif/_pillow_heif.c.orig	2025-07-06 18:59:30.893276993 +0000
++++ pillow_heif/_pillow_heif.c
+@@ -2,6 +2,7 @@
+ 
+ #include "Python.h"
+ #include "libheif/heif.h"
++#include "libheif/heif_properties.h"
+ #include "_ph_postprocess.h"
+ 
+ /* =========== Common stuff ======== */
