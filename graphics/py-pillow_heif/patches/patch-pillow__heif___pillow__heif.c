33f8bf13f742c6250609787032f0a13d9fd0bd56
wiz <wiz@pkgsrc.org>
2025-07-06 19:01:44 +0000

    py-pillow_heif: add upstream bug report URL

--- graphics/py-pillow_heif/patches/patch-pillow__heif___pillow__heif.c
+++ graphics/py-pillow_heif/patches/patch-pillow__heif___pillow__heif.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-pillow__heif___pillow__heif.c,v 1.1 2025/07/06 19:00:52 wiz Exp $
+$NetBSD: patch-pillow__heif___pillow__heif.c,v 1.2 2025/07/06 19:01:44 wiz Exp $
 
 Add header needed by libheif 1.20.1.
+https://github.com/bigcat88/pillow_heif/issues/360
 
 --- pillow_heif/_pillow_heif.c.orig	2025-07-06 18:59:30.893276993 +0000
 +++ pillow_heif/_pillow_heif.c
