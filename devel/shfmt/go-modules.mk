a8e2cf0b249bf4a132ebbe736c5a45068b253eaa
adam <adam@pkgsrc.org>
2025-07-07 06:56:43 +0000

    shfmt: updated to 3.12.0
    3.12.0
    
    - The `mvdan-sh` JS package is discontinued in favor of `sh-syntax`
    **cmd/shfmt**
      - Support the "simplify" and "minify" flags via EditorConfig
      - Do not allow `--write` to replace non-regular files
    **interp**
      - Add `IsBuiltin` to check if a command name is a shell built-in
      - Add `HandlerContext.Builtin` to allow `ExecHandlerFunc` to call built-ins
      - Initial support for `$!` and `wait PID`
      - Return non-fatal `ExecHandlerFunc` errors via the `Runner.Run` API
      - Add `HandlerContext.Pos` to provide handlers with source positions
      - Deprecate `NewExitStatus` and `IsExitStatus` in favor of `ExitStatus`
      - Fix `wait` to always return the status of the last given job
      - Copy all env vars for background subshells to avoid data races
      - Support reading random numbers via `$RANDOM` and `$SRANDOM`
      - Set `$BASH_REMATCH` when matching regular expressions via `=~`
      - Support modifying local vars from the parent calling function
    **expand**
      - Adjust which backslash sequences are expanded in here-docs
      - Tweak tilde expansions to match Bash semantics
    **pattern**
      - Remove the flawed and broken `Braces` mode; use `syntax.SplitBraces` instead
      - Tweak `**` to only act as "globstar" when alone as a path element
      - Tweak `*` and `**` to not match leading dots in basenames
      - Add a `NoGlobStar` mode to match the POSIX semantics
    **fileutil**
      - Treat all non-regular files as definitely not shell scripts

--- devel/shfmt/go-modules.mk
+++ devel/shfmt/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.4 2025/03/11 14:00:04 adam Exp $
+# $NetBSD: go-modules.mk,v 1.5 2025/07/07 06:56:43 adam Exp $
 
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.24.mod
 GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.24.zip
@@ -17,11 +17,11 @@ GO_MODULE_FILES+=	github.com/pkg/diff/@v/v0.0.0-20210226163009-20ebb0f2a09e.mod
 GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.14.1.mod
 GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.14.1.zip
 GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.9.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.zip
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.29.0.mod
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.29.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.27.0.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.27.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.33.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.33.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.32.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.32.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.31.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.31.0.zip
 GO_MODULE_FILES+=	mvdan.cc/editorconfig/@v/v0.3.0.mod
 GO_MODULE_FILES+=	mvdan.cc/editorconfig/@v/v0.3.0.zip
