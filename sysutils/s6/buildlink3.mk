f90199a6ffd5d4c237a898cf9eaeb83ddb9eb7f2
schmonz <schmonz@pkgsrc.org>
2025-07-08 21:15:15 +0000

    s6: build from git to get macOS shlibs. Bump PKGREVISION.

--- sysutils/s6/buildlink3.mk
+++ sysutils/s6/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/11/14 14:12:09 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/07/08 21:15:15 schmonz Exp $
 
 BUILDLINK_TREE+=	s6
 
 .if !defined(S6_BUILDLINK3_MK)
 S6_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6+=	s6>=2.13.1.0
+BUILDLINK_API_DEPENDS.s6+=	s6>=2.13.2.0nb1
 BUILDLINK_PKGSRCDIR.s6?=	../../sysutils/s6
 BUILDLINK_INCDIRS.s6+=		include/s6
 BUILDLINK_LIBDIRS.s6+=		lib/s6
-BUILDLINK_DEPMETHOD.s6?=	build
 .endif	# S6_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-s6
