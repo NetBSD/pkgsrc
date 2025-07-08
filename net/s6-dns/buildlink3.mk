7ca5c89e620b5698b102b31db94aa03560936a5f
schmonz <schmonz@pkgsrc.org>
2025-07-08 21:16:00 +0000

    s6-dns: build from git to get macOS shlibs. Bump PKGREVISION.

--- net/s6-dns/buildlink3.mk
+++ net/s6-dns/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/11/14 14:12:27 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/07/08 21:16:00 schmonz Exp $
 
 BUILDLINK_TREE+=	s6-dns
 
 .if !defined(S6_DNS_BUILDLINK3_MK)
 S6_DNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.4.0.0
+BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.4.1.0nb1
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../net/s6-dns
 BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
 BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
-BUILDLINK_DEPMETHOD.s6-dns?=	build
 .endif	# S6_DNS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-s6-dns
