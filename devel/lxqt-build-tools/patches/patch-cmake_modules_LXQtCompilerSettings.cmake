097fdac2e5b78aa5b36e201c3d7eb506b0b139a0
gutteridge <gutteridge@pkgsrc.org>
2025-07-08 01:54:40 +0000

    lxqt-build-tools: update to 2.2.1
    We're not enforcing C++20 for pkgsrc yet, as there's no need to do so
    given our state of all dependencies. This will need to be changed once
    Plasma components are updated.
    
    lxqt-build-tools-2.2.1 / 2025-06-25
    ====================================
     * Bumped the minimum compatibility level to C++20.
     * Fixed bashisms in lxqt-transupdate for better portability with non-bash shells.
     * Use FindXCB module provided by Qt.

--- devel/lxqt-build-tools/patches/patch-cmake_modules_LXQtCompilerSettings.cmake
+++ devel/lxqt-build-tools/patches/patch-cmake_modules_LXQtCompilerSettings.cmake
@@ -1,8 +1,11 @@
-$NetBSD: patch-cmake_modules_LXQtCompilerSettings.cmake,v 1.5 2024/08/02 16:52:38 pin Exp $
+$NetBSD: patch-cmake_modules_LXQtCompilerSettings.cmake,v 1.6 2025/07/08 01:54:40 gutteridge Exp $
 
 Avoid -Bsymbolic* on SunOS.
 
---- cmake/modules/LXQtCompilerSettings.cmake.orig	2024-05-27 05:52:17.311703304 +0000
+Only require C++17; C++20 is not required for any dependent components
+in the present state of pkgsrc.
+
+--- cmake/modules/LXQtCompilerSettings.cmake.orig	2025-06-21 10:41:45.000000000 +0000
 +++ cmake/modules/LXQtCompilerSettings.cmake
 @@ -164,7 +164,11 @@ if (CMAKE_COMPILER_IS_GNUCXX OR LXQT_COM
          set(NO_UNDEFINED_FLAGS "-Wl,--no-undefined")
@@ -17,3 +20,17 @@ Avoid -Bsymbolic* on SunOS.
      endif()
  
      set(CMAKE_SHARED_LINKER_FLAGS
+@@ -181,11 +185,11 @@ endif()
+ 
+ 
+ #-----------------------------------------------------------------------------
+-# CXX20 requirements - no checks, we just set it
++# CXX17 requirements - no checks, we just set it
+ #-----------------------------------------------------------------------------
+ set(CMAKE_CXX_STANDARD_REQUIRED True)
+ set(CMAKE_CXX_EXTENSIONS OFF)
+-set(CMAKE_CXX_STANDARD 20 CACHE STRING "C++ ISO Standard")
++set(CMAKE_CXX_STANDARD 17 CACHE STRING "C++ ISO Standard")
+ 
+ 
+ #-----------------------------------------------------------------------------
