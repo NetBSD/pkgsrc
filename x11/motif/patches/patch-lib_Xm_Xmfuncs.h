b75613d625a241ca8088d4746de36cb578b846da
jperkin <jperkin@pkgsrc.org>
2025-07-09 10:51:32 +0000

    motif: Update SunOS patch for GCC 14.

--- x11/motif/patches/patch-lib_Xm_Xmfuncs.h
+++ x11/motif/patches/patch-lib_Xm_Xmfuncs.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_Xm_Xmfuncs.h,v 1.1 2012/11/10 17:10:10 ryoon Exp $
+$NetBSD: patch-lib_Xm_Xmfuncs.h,v 1.2 2025/07/09 10:51:32 jperkin Exp $
 
 Avoid all the outdated #ifdef mess on SunOS. SunOS had both bcopy/bzero/bcmp
 and memmove/memcmp/memset since at least 1996.
 
---- lib/Xm/Xmfuncs.h.orig	2012-10-22 14:50:38.000000000 +0000
+--- lib/Xm/Xmfuncs.h.orig	2016-03-16 02:10:08.000000000 +0000
 +++ lib/Xm/Xmfuncs.h
 @@ -34,6 +34,8 @@ in this Software without prior written a
  
@@ -14,10 +14,12 @@ and memmove/memcmp/memset since at least 1996.
  #ifdef X_USEBFUNCS
  void bcopy();
  void bzero();
-@@ -88,4 +90,6 @@ int bcmp();
+@@ -88,4 +90,8 @@ int bcmp();
  #endif /* SYSV else */
  #endif /* ! X_NOT_STDC_ENV else */
  
++#else
++#include <strings.h>
 +#endif /* __sun */
 +
  #endif /* _XFUNCS_H_ */
