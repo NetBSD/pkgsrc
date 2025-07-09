8c287c81a61a701b56a9b28db9cc40da91ba71b8
dkazankov <dkazankov@pkgsrc.org>
2025-07-09 17:10:43 +0000

    lang/gcc13-gnat: make it easier to use the package in child library packages
    * Eliminate use of GNAT_PREFIX variable - its definition
      will be moved to gcc.mk and will be used in child packages
    * Start unbinding of build process of gcc*-gnat packages from gcc.mk
    * Use BUILDLINK_FNAME_TRANSFORM instead of PREPEND_PATH

--- lang/gcc13-gnat/buildlink3.mk
+++ lang/gcc13-gnat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2025/07/02 18:09:02 dkazankov Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/07/09 17:10:43 dkazankov Exp $
 
 BUILDLINK_TREE+=	gcc13-gnat
 
@@ -7,30 +7,23 @@ GCC13_GNAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gcc13-gnat+=	gcc13-gnat>=13.1.0
 BUILDLINK_ABI_DEPENDS.gcc13-gnat+=	gcc13-gnat>=13.4.0
-BUILDLINK_PKGSRCDIR.gcc13-gnat=		../../lang/gcc13-gnat
+BUILDLINK_PKGSRCDIR.gcc13-gnat?=	../../lang/gcc13-gnat
 BUILDLINK_DEPMETHOD.gcc13-gnat?=	build
 
-PREPEND_PATH+=	${BUILDLINK_DIR}/gcc13-gnat/bin
+ADALIB_BASE=		lib/gcc/${MACHINE_GNU_PLATFORM}/13.4.0
 
-GNU_TARGET_MACHINE?=	${MACHINE_GNU_PLATFORM}
-
-ADALIB_PREFIX=		gcc13-gnat/lib/gcc/${GNU_TARGET_MACHINE}/13.4.0
-
-BUILDLINK_FILES.gcc13-gnat+=	gcc13-gnat/${GNU_TARGET_MACHINE}/lib/*
+BUILDLINK_FNAME_TRANSFORM.gcc13-gnat+=	-e "s|^${BUILDLINK_DIR}/gcc13-gnat/|${BUILDLINK_DIR}/|g"
 
 BUILDLINK_INCDIRS.gcc13-gnat+=	gcc13-gnat/include
-BUILDLINK_INCDIRS.gcc13-gnat+=	${ADALIB_PREFIX}/include
-BUILDLINK_INCDIRS.gcc13-gnat+=	${ADALIB_PREFIX}/include-fixed
-BUILDLINK_INCDIRS.gcc13-gnat+=	${ADALIB_PREFIX}/plugin/include
-BUILDLINK_INCDIRS.gcc13-gnat+=	${ADALIB_PREFIX}/adainclude
+BUILDLINK_INCDIRS.gcc13-gnat+=	gcc13-gnat/${ADALIB_BASE}/include
+BUILDLINK_INCDIRS.gcc13-gnat+=	gcc13-gnat/${ADALIB_BASE}/adainclude
 
 BUILDLINK_LIBDIRS.gcc13-gnat+=	gcc13-gnat/lib
-BUILDLINK_LIBDIRS.gcc13-gnat+=	${ADALIB_PREFIX}
-BUILDLINK_LIBDIRS.gcc13-gnat+=	${ADALIB_PREFIX}/adalib
-BUILDLINK_LIBDIRS.gcc13-gnat+=	gcc13-gnat/${GNU_TARGET_MACHINE}/lib
+BUILDLINK_LIBDIRS.gcc13-gnat+=	gcc13-gnat/${ADALIB_BASE}
+BUILDLINK_LIBDIRS.gcc13-gnat+=	gcc13-gnat/${ADALIB_BASE}/adalib
 
 BUILDLINK_CONTENTS_FILTER.gcc13-gnat=	${EGREP} \
-					'(bin/.*|include/.*|lib/.*|libexec/.*|${GNU_TARGET_MACHINE}/lib/.*)'
+					'(bin/.*|include/.*|lib/.*|libexec/.*)'
 
 pkgbase := gcc13-gnat
 .include "../../mk/pkg-build-options.mk"
