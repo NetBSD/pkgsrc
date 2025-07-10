85d91e883c2de7d4b5fda7d5ce58025718d55477
jperkin <jperkin@pkgsrc.org>
2025-07-10 14:23:11 +0000

    erlang: Remove broken SunOS patch.

--- lang/erlang/patches/patch-erts_emulator_nifs_common_prim__tty__nif.c
+++ lang/erlang/patches/patch-erts_emulator_nifs_common_prim__tty__nif.c
@@ -1,8 +1,9 @@
-$NetBSD: patch-erts_emulator_nifs_common_prim__tty__nif.c,v 1.1 2024/05/27 19:07:50 riastradh Exp $
+$NetBSD: patch-erts_emulator_nifs_common_prim__tty__nif.c,v 1.2 2025/07/10 14:23:11 jperkin Exp $
 
 Fix ctype(3) misuse.
+Remove broken SunOS patch.
 
---- erts/emulator/nifs/common/prim_tty_nif.c.orig	2024-03-06 15:30:18.000000000 +0000
+--- erts/emulator/nifs/common/prim_tty_nif.c.orig	2024-07-09 11:36:07.000000000 +0000
 +++ erts/emulator/nifs/common/prim_tty_nif.c
 @@ -266,7 +266,7 @@ static ERL_NIF_TERM isprint_nif(ErlNifEn
      int i;
@@ -12,4 +13,16 @@ Fix ctype(3) misuse.
 +        return isprint((unsigned char)i) ? atom_true : atom_false;
      }
      return enif_make_badarg(env);
+ }
+@@ -746,11 +746,7 @@ static ERL_NIF_TERM tty_tgetstr_nif(ErlN
+ static int tputs_buffer_index;
+ static unsigned char tputs_buffer[1024];
+ 
+-#if defined(__sun) && defined(__SVR4) /* Solaris */
+-static int tty_puts_putc(char c) {
+-#else
+ static int tty_puts_putc(int c) {
+-#endif
+     tputs_buffer[tputs_buffer_index++] = (unsigned char)c;
+     return 0;
  }
