e65afb12f38ee1970b75f10c112783b7a8f89bc8
dkazankov <dkazankov@pkgsrc.org>
2025-07-10 16:55:09 +0000

    textproc/ada-xmlada-25: Add new package version 25.0.0
    XML/Ada: An XML parser for Ada95, release 25

---
+++ textproc/ada-xmlada-25/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/07/10 16:55:09 dkazankov Exp $
+
+BUILDLINK_TREE+=	xmlada-25
+
+.if !defined(XMLADA25_BUILDLINK3_MK)
+XMLADA25_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xmlada-25+=	xmlada-${GNAT_NAME}>=25.0.0
+BUILDLINK_PKGSRCDIR.xmlada-25?=		../../textproc/ada-xmlada-25
+BUILDLINK_DEPMETHOD.xmlada-25?=		build
+
+BUILDLINK_CONTENTS_FILTER.xmlada-25=	\
+	${EGREP} "${GNAT_PKGBASE}/include/.*$$|${GNAT_PKGBASE}/lib/.*$$|${GNAT_PKGBASE}/share/gpr/.*$$"
+
+BUILDLINK_FNAME_TRANSFORM.xmlada-25+=	\
+	-e "s|${BUILDLINK_DIR}/${GNAT_PKGBASE}/|${BUILDLINK_DIR}/|g"
+
+.endif
+
+BUILDLINK_TREE+=	-xmlada-25
