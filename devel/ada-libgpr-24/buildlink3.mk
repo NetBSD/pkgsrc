54bc831acbad686e1667dc3db377850d4f0ce158
dkazankov <dkazankov@pkgsrc.org>
2025-07-10 12:27:48 +0000

    devel/ada-libgpr-24: Add new package version 24.0.0
    LIBGPR development libraries, release 24

---
+++ devel/ada-libgpr-24/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/07/10 12:27:48 dkazankov Exp $
+
+BUILDLINK_TREE+=	libgpr-24
+
+.if !defined(LIBGPR_24_BUILDLINK3_MK)
+LIBGPR_24_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgpr-24+=	libgpr-${GNAT_NAME}>=24.0.0
+BUILDLINK_PKGSRCDIR.libgpr-24?=		../../devel/ada-libgpr-24
+BUILDLINK_DEPMETHOD.libgpr-24?=		build
+
+BUILDLINK_CONTENTS_FILTER.libgpr-24=	\
+    ${EGREP} "${GNAT_PKGBASE}/include/.*$$|${GNAT_PKGBASE}/lib/.*$$|${GNAT_PKGBASE}/share/gpr/.*$$"
+
+BUILDLINK_FNAME_TRANSFORM.libgpr-24+=	\
+    -e "s|${BUILDLINK_DIR}/${GNAT_PKGBASE}/|${BUILDLINK_DIR}/|g"
+
+.endif
+
+BUILDLINK_TREE+=	-libgpr-24
