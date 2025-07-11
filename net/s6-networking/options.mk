39ebd1829166a57bdb7b5e19fb301892137305c4
schmonz <schmonz@pkgsrc.org>
2025-07-11 21:16:20 +0000

    s6-networking: change default options. Bump PKGREVISION.
    Instead of BearSSL, link with OpenSSL by default. The 'bearssl' option
    is still present and 'libressl' is now available too.
    
    Remove the transitional 'tls' option mapping.
    
    While here, remove s6-pkgsrc-cadir. pkgsrc doesn't have a clear
    library-independent notion of its "SSL cert directory", so we can't
    helpfully tell clients (or servers that want to validate client certs)
    where to find it.

--- net/s6-networking/options.mk
+++ net/s6-networking/options.mk
@@ -1,11 +1,10 @@
-# $NetBSD: options.mk,v 1.9 2025/06/04 13:29:33 schmonz Exp $
+# $NetBSD: options.mk,v 1.10 2025/07/11 21:16:20 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.s6-networking
 PKG_OPTIONS_OPTIONAL_GROUPS=	tls
-PKG_OPTIONS_GROUP.tls=		bearssl openssl
+PKG_OPTIONS_GROUP.tls=		bearssl libressl openssl
 PKG_SUPPORTED_OPTIONS+=		execline
-PKG_SUGGESTED_OPTIONS+=		execline bearssl
-PKG_OPTIONS_LEGACY_OPTS+=	tls:bearssl
+PKG_SUGGESTED_OPTIONS+=		execline openssl
 
 .include "../../mk/bsd.options.mk"
 
@@ -26,6 +25,17 @@ PLIST.bearssl=		yes
 CONFIGURE_ARGS+=	--enable-ssl=bearssl
 .endif
 
+# XXX libtls-bearssl
+
+.if !empty(PKG_OPTIONS:Mlibressl)
+PLIST.tls=		yes
+PLIST.libtls=		yes
+BUILDLINK_API_DEPENDS.libressl+=	libressl>=4.1.0
+.  include "../../security/libressl/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-ssl=libtls
+CONFIGURE_ARGS+=	--with-ssl-path=${PREFIX:Q}/libressl
+.endif
+
 .if !empty(PKG_OPTIONS:Mopenssl)
 PLIST.tls=		yes
 PLIST.libtls=		yes
@@ -39,21 +49,4 @@ PKG_USERS_VARS+=	UCSPI_SSL_USER
 PKG_GROUPS_VARS+=	UCSPI_SSL_GROUP
 PKG_GROUPS+=		${UCSPI_SSL_GROUP}
 PKG_USERS+=		${UCSPI_SSL_USER}:${UCSPI_SSL_GROUP}
-
-SUBST_CLASSES+=		paths
-SUBST_STAGE.paths=	pre-configure
-SUBST_FILES.paths=	s6-pkgsrc-cadir
-SUBST_VARS.paths=	SH SETENV SSLCERTS
-
-.PHONY: do-extract-cadir do-install-cadir
-post-extract: do-extract-cadir
-do-extract-cadir:
-	for f in s6-pkgsrc-cadir; do \
-		${CP} ${FILESDIR}/$$f.sh ${WRKSRC}/$$f; \
-	done
-post-install: do-install-cadir
-do-install-cadir:
-	for f in s6-pkgsrc-cadir; do \
-		${INSTALL_SCRIPT} ${WRKSRC}/$$f ${DESTDIR}${PREFIX}/bin/$$f; \
-	done
 .endif
