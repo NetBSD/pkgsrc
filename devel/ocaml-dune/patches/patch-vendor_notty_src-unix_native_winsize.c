06070aa1e0db51e72bb45a43f334ada9f3e45601
jperkin <jperkin@pkgsrc.org>
2025-07-11 11:55:36 +0000

    ocaml-dune: Extend SunOS fix.

--- devel/ocaml-dune/patches/patch-vendor_notty_src-unix_native_winsize.c
+++ devel/ocaml-dune/patches/patch-vendor_notty_src-unix_native_winsize.c
@@ -1,15 +1,17 @@
-$NetBSD: patch-vendor_notty_src-unix_native_winsize.c,v 1.1 2023/12/04 13:56:34 jperkin Exp $
+$NetBSD: patch-vendor_notty_src-unix_native_winsize.c,v 1.2 2025/07/11 11:55:37 jperkin Exp $
 
 SunOS build fix.
 
---- vendor/notty/src-unix/native/winsize.c.orig	2023-12-04 13:26:08.056576737 +0000
+--- vendor/notty/src-unix/native/winsize.c.orig	2023-10-09 11:08:29.000000000 +0000
 +++ vendor/notty/src-unix/native/winsize.c
-@@ -3,6 +3,9 @@
+@@ -3,6 +3,11 @@
  #ifdef _WIN32
  #include <caml/fail.h>
  #else
 +#ifdef __sun
 +#include <sys/termios.h>
++#include <unistd.h>
++#include <stropts.h>
 +#endif
  #include <sys/ioctl.h>
  #include <signal.h>
