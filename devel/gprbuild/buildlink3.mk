b38fc44bb425ed9d6938dc192586c0980b5f4959
dkazankov <dkazankov@pkgsrc.org>
2025-07-11 18:42:24 +0000

    fix lint warn, remove unnecessary var setting

--- devel/gprbuild/buildlink3.mk
+++ devel/gprbuild/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2025/02/16 10:11:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/07/11 18:42:24 dkazankov Exp $
 
 BUILDLINK_TREE+=	gprbuild
 
@@ -6,16 +6,12 @@ BUILDLINK_TREE+=	gprbuild
 GPRBUILD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gprbuild+=	gprbuild>=24.0.0
-BUILDLINK_PKGSRCDIR.gprbuild=		../../devel/gprbuild
+BUILDLINK_PKGSRCDIR.gprbuild?=		../../devel/gprbuild
 BUILDLINK_DEPMETHOD.gprbuild?=		build
 
 # GPRbuild does similar things in its own way
 BUILDLINK_AUTO_VARS.gprbuild=	no
 BUILDLINK_AUTO_DIRS.gprbuild=	no
-
-BUILDLINK_FILES.gprbuild=	bin/gpr* libexec/gprbuild/*
-BUILDLINK_FILES.gprbuild+=	share/gpr/_default.gpr share/gpr/manifests/gprbuild
-BUILDLINK_FILES.gprbuild+=	share/gprconfig/*
 
 BUILDLINK_CONTENTS_FILTER.gprbuild=	\
 	${EGREP} '(bin/gpr.*|libexec/gprbuild/gpr.*|share/gpr/.*\.gpr$$|share/gpr/manifests/.*|share/gprconfig/.*)'
