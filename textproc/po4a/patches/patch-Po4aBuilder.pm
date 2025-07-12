e30711c3afcb614d22b772b78b34d04efc36f443
adam <adam@pkgsrc.org>
2025-07-12 15:34:12 +0000

    po4a: updated to 0.74
    0.74
    
    General:
     * Fix a failure with Perl 5.40.
     * Add --target-lang option to po4a
     * Include bash_completion files
       Copy the share/bash-completion/completions/ files under /usr to use them.

--- textproc/po4a/patches/patch-Po4aBuilder.pm
+++ textproc/po4a/patches/patch-Po4aBuilder.pm
@@ -1,4 +1,4 @@
-$NetBSD: patch-Po4aBuilder.pm,v 1.12 2024/03/02 20:59:03 adam Exp $
+$NetBSD: patch-Po4aBuilder.pm,v 1.13 2025/07/12 15:34:12 adam Exp $
 
 (1) ryoon: remove gzip at initial import.
 (2) mef: Add PerlIO::F_UTF8 () macro.
@@ -6,50 +6,50 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.12 2024/03/02 20:59:03 adam Exp $
 (4) schmonz: Invoke pkgsrc's gettext-tools.
 (5) schmonz: Write to the full physical null device.
 
---- Po4aBuilder.pm.orig	2024-02-29 20:26:15.000000000 +0000
+--- Po4aBuilder.pm.orig	2025-07-12 13:28:07.000000000 +0000
 +++ Po4aBuilder.pm
-@@ -16,6 +16,8 @@ use File::stat;
+@@ -18,6 +18,8 @@ use File::stat qw(lstat stat);
  
- our @ISA = qw(Module::Build);
+ use IPC::Open3 qw(open3);
  
 +sub PerlIO::F_UTF8 () { 0x00008000 } # from perliol.h
 +
  sub ACTION_build {
      my $self = shift;
      $self->depends_on('code');
-@@ -67,7 +69,7 @@ sub ACTION_binpo {
+@@ -68,7 +70,7 @@ sub ACTION_binpo {
          chdir "../..";
  	
          if ( -e "po/bin/po4a.pot") {
 -            my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
 +            my $diff = qx(@DIFF@ -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
              if ( $diff eq "" ) {
-                 unlink "po/bin/po4a.pot.new" || die;
+                 unlink "po/bin/po4a.pot.new" or die;
                  # touch it
-@@ -89,11 +91,11 @@ sub ACTION_binpo {
+@@ -90,11 +92,11 @@ sub ACTION_binpo {
        if ($lang eq 'de') {
          unless ($self->up_to_date("po/bin/po4a.pot", $_)) {
              print "XX Sync $_: ";
--            system("msgmerge --previous $_ po/bin/po4a.pot -o $_.new") && die;
-+            system("@PREFIX@/bin/msgmerge --previous $_ po/bin/po4a.pot -o $_.new") && die;
+-            system("msgmerge", "--previous", $_, "po/bin/po4a.pot", "-o", "$_.new") && die;
++            system("@PREFIX@/bin/msgmerge", "--previous", $_, "po/bin/po4a.pot", "-o", "$_.new") && die;
              # Typically all that changes was a date. I'd
              # prefer not to commit such changes, so detect
              # and ignore them.
 -            my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
 +            my $diff = qx(@DIFF@ -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
              if ($diff eq "") {
-                 unlink "$_.new" || die;
+                 unlink "$_.new" or die;
                  # touch it
-@@ -108,7 +110,7 @@ sub ACTION_binpo {
+@@ -109,7 +111,7 @@ sub ACTION_binpo {
        }
          unless ($self->up_to_date($_,"blib/po/$lang/LC_MESSAGES/po4a.mo")) {
              mkpath( File::Spec->catdir( 'blib', 'po', $lang, "LC_MESSAGES" ), 0, oct(755) );
--            system("msgfmt -o blib/po/$lang/LC_MESSAGES/po4a.mo $_") && die;
-+            system("@PREFIX@/bin/msgfmt -o blib/po/$lang/LC_MESSAGES/po4a.mo $_") && die;
+-            system("msgfmt", "-o", "blib/po/$lang/LC_MESSAGES/po4a.mo", $_) && die;
++            system("@PREFIX@/bin/msgfmt", "-o", "blib/po/$lang/LC_MESSAGES/po4a.mo", $_) && die;
          }
      }
  }
-@@ -230,9 +232,9 @@ sub ACTION_man {
+@@ -231,9 +233,9 @@ sub ACTION_man {
              $parser->{release} = $parser->{center} = "Po4a Tools";
          } else {
              my $command;
@@ -62,26 +62,26 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.12 2024/03/02 20:59:03 adam Exp $
              $command .= "&& cat || cat > /dev/null'";
  
              my $title = `$command 2> /dev/null`;
-@@ -242,8 +244,6 @@ sub ACTION_man {
+@@ -243,8 +245,6 @@ sub ACTION_man {
          }
          $parser->parse_from_file( $file, $out );
  
--        system("gzip -9 -n -f $out") and die;
--        unlink "$file" || die;
+-        system("gzip", "-9", "-n", "-f", $out) and die;
+-        unlink "$file" or die;
      }
  
      if ( $^O ne 'MSWin32' ) {
-@@ -264,9 +264,7 @@ sub ACTION_man {
+@@ -275,9 +275,7 @@ sub ACTION_man {
                      print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
-                     system("xsltproc -o $outdir/$outfile.$section --nonet $docbook_xsl_url $file") and die;
+                     system("xsltproc", "-o", "$outdir/$outfile.$section", "--nonet", $docbook_xsl_url, $file) and die;
                  }
--                system("gzip -9 -n -f $outdir/$outfile.$section") and die;
+-                system("gzip", "-9", "-n", "-f", "$outdir/$outfile.$section") and die;
              }
--            unlink "$file" || die;
+-            unlink "$file" or die;
          }
      }
  }
-@@ -294,7 +292,7 @@ sub postats {
+@@ -305,7 +303,7 @@ sub postats {
      my (@t100,@t95,@t90,@t80,@t70,@t50,@t33,@t20,@starting);
      foreach my $file (sort @files) {
          my $lang = fileparse($file, qw{.po});
