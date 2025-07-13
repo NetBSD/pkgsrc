d08a91afa99caff6e497714f3d1e6bc82cf73b3f
wiz <wiz@pkgsrc.org>
2025-07-13 08:17:36 +0000

    *: recursive bump for poppler 25.07.0
    inkscape: fix build using upstream patches

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.108 2025/04/24 14:15:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.109 2025/07/13 08:17:36 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=24.10.0nb8
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=25.07.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 USE_CXX_FEATURES+=	c++20
