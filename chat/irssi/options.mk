e49ec464bd2179e2c5bbbc5a967b3a7012f424a4
wiz <wiz@pkgsrc.org>
2025-07-13 17:24:31 +0000

    irssi: switch to build with meson
    Build was broken with perl 5.42, and upstream recommends switching to meson.
    
    Bump PKGREVISION.

--- chat/irssi/options.mk
+++ chat/irssi/options.mk
@@ -1,40 +1,42 @@
-# $NetBSD: options.mk,v 1.24 2024/06/06 11:03:28 jperkin Exp $
+# $NetBSD: options.mk,v 1.25 2025/07/13 17:24:31 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.irssi
-PKG_SUPPORTED_OPTIONS=		otr perl truecolor
-PKG_SUGGESTED_OPTIONS=		otr perl truecolor
+PKG_SUPPORTED_OPTIONS=		otr perl irssi-truecolor
+PKG_SUGGESTED_OPTIONS=		otr perl irssi-truecolor
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		otr
 .if !empty(PKG_OPTIONS:Motr)
+PLIST.otr=		yes
 # Build it into the main irssi executable instead of a module.
-CONFIGURE_ARGS+=	--with-otr=static
-.  if ${USE_CROSS_COMPILE:tl} == "yes"
-CONFIGURE_ARGS+=	LIBGCRYPT_CONFIG=${CROSS_DESTDIR:Q}${BUILDLINK_PREFIX.libgcrypt}/bin/libgcrypt-config
-.  endif
+MESON_ARGS+=		-Dwith-otr=yes
+# TODO: statically linked previously, but this now gives:
+# ld: /usr/lib/libintl.a(sysdep.o): warning: relocation against `sysdep_pair_table' in read-only section `.text'
+# ld: /usr/lib/libintl.a(sysdep.o): relocation R_X86_64_PC32 against symbol `sysdep_pair_table' can not be used when making a shared object; recompile with -fPIC
+#MESON_ARGS+=		-Dstatic-dependency=yes
+# TODO: not sure how to replace this, if at all
+#.  if ${USE_CROSS_COMPILE:tl} == "yes"
+#CONFIGURE_ARGS+=	LIBGCRYPT_CONFIG=${CROSS_DESTDIR:Q}${BUILDLINK_PREFIX.libgcrypt}/bin/libgcrypt-config
+#.  endif
 .include "../../chat/libotr/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--with-otr=no
+MESON_ARGS+=		-Dwith-otr=no
 .endif
 
 PLIST_VARS+=		perl
 .if !empty(PKG_OPTIONS:Mperl)
 PLIST.perl=		yes
 USE_TOOLS+=		perl:run
-PERL5_PACKLIST=		auto/Irssi/.packlist
-PERL5_PACKLIST+=	auto/Irssi/Irc/.packlist
-PERL5_PACKLIST+=	auto/Irssi/UI/.packlist
-PERL5_PACKLIST+=	auto/Irssi/TextUI/.packlist
-.include "../../lang/perl5/packlist.mk"
 .include "../../lang/perl5/buildlink3.mk"
-# actually we don't need --with-perl here, irssi is compiled with it by default
+# this is actually the default
 # but who knows what may happen in future versions
-CONFIGURE_ARGS+=	--with-perl
-CONFIGURE_ARGS+=	--with-perl-lib=vendor
+MESON_ARGS+=		-Dwith-perl=yes
+MESON_ARGS+=		-Dwith-perl-lib=vendor
 .else
-CONFIGURE_ARGS+=	--with-perl=no
+MESON_ARGS+=		-Dwith-perl=no
 .endif
 
-.if !empty(PKG_OPTIONS:Mtruecolor)
-CONFIGURE_ARGS+=	--enable-true-color
+.if !empty(PKG_OPTIONS:Mirssi-truecolor)
+MESON_ARGS+=		-Denable-true-color=yes
 .endif
