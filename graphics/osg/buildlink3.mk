9ca70a362183c0990d89863ebfb6f8b471c71d63
wiz <wiz@pkgsrc.org>
2025-07-13 08:17:36 +0000

    *: recursive bump for poppler 25.07.0
    inkscape: fix build using upstream patches

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.150 2025/05/16 05:40:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.151 2025/07/13 08:17:39 wiz Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.6.5nb68
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.6.5nb69
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../mk/bsd.fast.prefs.mk"
--- print/poppler-qt5/buildlink3.mk
+++ print/poppler-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.60 2025/04/24 14:15:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.61 2025/07/13 08:17:40 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt5
 POPPLER_QT5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt5+=	poppler-qt5>=0.26.0
-BUILDLINK_ABI_DEPENDS.poppler-qt5+=	poppler-qt5>=24.10.0nb6
+BUILDLINK_ABI_DEPENDS.poppler-qt5+=	poppler-qt5>=25.07.0nb1
 BUILDLINK_PKGSRCDIR.poppler-qt5?=	../../print/poppler-qt5
 
 .include "../../print/poppler/buildlink3.mk"
