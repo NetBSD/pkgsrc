2e11f6b54e924c5a15ac6c807e2989f0121fc96d
adam <adam@pkgsrc.org>
2025-07-14 05:35:36 +0000

    defguard: added version 1.4.0
    Defguard provides Comprehensive Access Control (a complete security platform):
    * WireGuard VPN with 2FA/MFA - not 2FA to "access application" like most
      solutions
      - The only solution with automatic and real-time synchronization for users'
        desktop client settings (including all VPNs/locations).
      - Control users ability to manage devices and VPN options
    * ACLs/Firewall Management for Linux and FreeBSD/OPNSense
    * Integrated SSO based on OpenID Connect:
      - significant cost saving, simplifying deployment and maintenance
      - enabling features unavailable to VPN platforms relying upon 3rd party SSO
        integration
    * Already using Google/Microsoft or other OpenID Provider? - external OpenID
      provider support
    * Two way Active Directory/LDAP synchronization
    * Only solution with secure remote user Enrollment & Onboarding
    * Yubico YubiKey Hardware security key management and provisioning
    * Secure and robust architecture, featuring components and micro-services
      seamlessly deployable in diverse network setups (eg. utilizing network
      segments like Demilitarized Zones, Intranet with no external access, etc),
      ensuring a secure environment.
    * Enterprise ready (multiple Locations/Gateways/Kubernetes deployment, etc..)
    * Built on WireGuard protocol which is faster than IPSec, and significantly
      faster than OpenVPN
    * Built with Rust for speed and security

---
+++ security/defguard/version.mk
@@ -0,0 +1,23 @@
+# $NetBSD: version.mk,v 1.1 2025/07/14 05:35:36 adam Exp $
+DEFGUARD_VERSION=	1.4.0
+
+CATEGORIES=	security net
+MASTER_SITES=	${MASTER_SITE_GITHUB:=defguard/}
+GITHUB_TAG=	v${PKGVERSION_NOREV}
+
+MAINTAINER=	adam@NetBSD.org
+HOMEPAGE=	https://defguard.net/
+LICENSE=	gnu-agpl-v3 # AND defguard-enterprise-license
+
+# All components need protos.
+EXTRA_DIST+=	defguard-${DEFGUARD_VERSION}-proto${EXTRACT_SUFX}
+
+DISTFILES=	${DEFAULT_DISTFILES} ${EXTRA_DIST}
+
+TOOL_DEPENDS+=	protobuf>0:../../devel/protobuf
+
+MAKE_ENV+=	VERGEN_IDEMPOTENT=1
+MAKE_ENV+=	VERGEN_GIT_SHA=pkgsrc
+
+post-extract:
+	${RMDIR} ${WRKSRC}/proto && ${LN} -f -s ${WRKDIR}/proto ${WRKSRC}/proto
