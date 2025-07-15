c53a4c40cd084ce53f0ed4b5b9ff47e24af121af
ryoon <ryoon@pkgsrc.org>
2025-07-15 13:58:05 +0000

    www/firefox: Update to 139.0.4
    * Try to add some NetBSD-specific changes (incompleted).
    
    Changelog:
    139.0.4:
    Fixed
    
      * Fixed Firefox freezing when switching between apps or opening certain
        panels within the browser. (Bug 1969253 & Bug 1969346)
    
      * Fixed difficult selection of drop-down menu options in the Firefox
        preferences page when selected via the in-page search. (Bug 1968949)
    
      * Fixed various selection issues when triple-clicking text in some
        situations. (Bug 1969100 & Bug 1969432)
    
      * Fixed an incorrect filename being used when setting an image as the desktop
        wallpaper on Windows. (Bug 1969793)
    
      * Various security fixes.
    
    Security fixes:
    Mozilla Foundation Security Advisory 2025-47
    #CVE-2025-49709: Memory corruption in canvas surfaces
    #CVE-2025-49710: Integer overflow in OrderedHashTable
    
    
    139.0.1:
    Fixed
    
      * Fixed graphics corruption with certain NVIDIA graphics adapters and
        multiple monitors running at mixed refresh rates after updating to Firefox
        139. (Bug 1968876)
    
    
    139.0:
    New
    
      * By popular request, Full-Page Translations are now available within Firefox
        extension pages that start with the moz-extension:// URL scheme.
    
      * The New Tab custom wallpaper (and colors) option is now available! Your own
        image can be uploaded as your New Tab wallpaper or any custom color can be
        selected - from the brightest pink to dark gray. This feature will be
        rolling out gradually to new users and can also be enabled immediately via
        Firefox Labs. Additionally, new Wallpaper images and a new Celestial
        category have also been added.
    
      * Link Previews is currently available as an experimental feature which can
        be enabled via Firefox Labs in the Firefox settings. After enabling, use
        the Alt+Shift keyboard shortcut when hovering over a link to see the
        previews in action!
    
      * Address autofill has now been enabled for users in Australia and India.
    
    Fixed
    
      * PNG images with transparency now keep their transparency when pasted into
        Firefox.
    
      * The upload performance of HTTP/3 has been significantly improved,
        particularly on resumed connections (QUIC 0-RTT) and high-bandwidth and
        high-delay connections.
    
      * Various security fixes.
    
    Changed
    
      * Due to recent changes in how Chrome encrypts user data on Windows, the
        Firefox migration wizard can no longer directly import payment methods or
        passwords from Chrome. However, users can still export passwords from
        Chrome to a CSV file and then import them into Firefox using the migration
        wizard or the password manager.
    
      * The Review Checker feature is shutting down and will no longer be available
        after June 10, 2025.
    
    
    Mozilla Foundation Security Advisory 2025-42
    #CVE-2025-5283: Double-free in libvpx encoder
    #CVE-2025-5263: Error handling for script execution was incorrectly isolated
     from web content
    #CVE-2025-5264: Potential local code execution in “Copy as cURL” command
    #CVE-2025-5265: Potential local code execution in “Copy as cURL” command
    #CVE-2025-5266: Script element events leaked cross-origin resource status
    #CVE-2025-5271: Devtools' preview ignored CSP headers
    #CVE-2025-5267: Clickjacking vulnerability could have led to leaking saved
     payment card details
    #CVE-2025-5268: Memory safety bugs fixed in Firefox 139, Thunderbird 139,
     Firefox ESR 128.11, and Thunderbird 128.11
    #CVE-2025-5272: Memory safety bugs fixed in Firefox 139 and Thunderbird 139

--- www/firefox/patches/patch-third__party_libwebrtc_modules_video__capture_linux_device__info__v4l2.cc
+++ www/firefox/patches/patch-third__party_libwebrtc_modules_video__capture_linux_device__info__v4l2.cc
@@ -1,10 +1,50 @@
-$NetBSD: patch-third__party_libwebrtc_modules_video__capture_linux_device__info__v4l2.cc,v 1.3 2023/10/15 07:40:10 ryoon Exp $
+$NetBSD: patch-third__party_libwebrtc_modules_video__capture_linux_device__info__v4l2.cc,v 1.4 2025/07/15 13:58:05 ryoon Exp $
 
 * Fix build under NetBSD 9 and older.
   The older NetBSD's sys/videoio.h does not have v4l2_capability.device_caps.
 
---- third_party/libwebrtc/modules/video_capture/linux/device_info_v4l2.cc.orig	2023-09-14 21:15:08.000000000 +0000
+--- third_party/libwebrtc/modules/video_capture/linux/device_info_v4l2.cc.orig	2025-05-17 19:55:48.000000000 +0000
 +++ third_party/libwebrtc/modules/video_capture/linux/device_info_v4l2.cc
+@@ -52,14 +52,14 @@
+ #define V4L2_PIX_FMT_RGBA32 v4l2_fourcc('A', 'B', '2', '4')
+ #endif
+ 
+-#ifdef WEBRTC_LINUX
++#ifdef WEBRTC_LINUX || WEBRTC_BSD
+ #define EVENT_SIZE  ( sizeof (struct inotify_event) )
+ #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+ #endif
+ 
+ namespace webrtc {
+ namespace videocapturemodule {
+-#ifdef WEBRTC_LINUX
++#ifdef WEBRTC_LINUX || WEBRTC_BSD
+ void DeviceInfoV4l2::HandleEvent(inotify_event* event, int fd)
+ {
+     if (event->mask & IN_CREATE) {
+@@ -175,11 +175,11 @@ void DeviceInfoV4l2::InotifyProcess()
+ #endif
+ 
+ DeviceInfoV4l2::DeviceInfoV4l2() : DeviceInfoImpl()
+-#ifdef WEBRTC_LINUX
++#ifdef WEBRTC_LINUX || WEBRTC_BSD
+     , _isShutdown(false)
+ #endif
+ {
+-#ifdef WEBRTC_LINUX
++#ifdef WEBRTC_LINUX || WEBRTC_BSD
+   _inotifyEventThread = rtc::PlatformThread::SpawnJoinable(
+       [this] {
+         InotifyProcess();
+@@ -192,7 +192,7 @@ int32_t DeviceInfoV4l2::Init() {
+ }
+ 
+ DeviceInfoV4l2::~DeviceInfoV4l2() {
+-#ifdef WEBRTC_LINUX
++#ifdef WEBRTC_LINUX || WEBRTC_BSD
+     _isShutdown = true;
+ 
+     if (!_inotifyEventThread.empty()) {
 @@ -211,11 +211,13 @@ uint32_t DeviceInfoV4l2::NumberOfDevices
    for (int n = 0; n < 64; n++) {
      snprintf(device, sizeof(device), "/dev/video%d", n);
@@ -19,7 +59,7 @@ $NetBSD: patch-third__party_libwebrtc_modules_video__capture_linux_device__info_
  
        close(fd);
        count++;
-@@ -242,11 +244,13 @@ int32_t DeviceInfoV4l2::GetDeviceName(ui
+@@ -243,11 +245,13 @@ int32_t DeviceInfoV4l2::GetDeviceName(ui
    for (int n = 0; n < 64; n++) {
      snprintf(device, sizeof(device), "/dev/video%d", n);
      if ((fd = open(device, O_RDONLY)) != -1) {
@@ -33,7 +73,7 @@ $NetBSD: patch-third__party_libwebrtc_modules_video__capture_linux_device__info_
        if (count == deviceNumber) {
          // Found the device
          found = true;
-@@ -317,6 +321,7 @@ int32_t DeviceInfoV4l2::CreateCapability
+@@ -318,6 +322,7 @@ int32_t DeviceInfoV4l2::CreateCapability
      if (fd == -1)
        continue;
  
@@ -41,7 +81,7 @@ $NetBSD: patch-third__party_libwebrtc_modules_video__capture_linux_device__info_
      // query device capabilities
      struct v4l2_capability cap;
      if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == 0) {
-@@ -341,6 +346,7 @@ int32_t DeviceInfoV4l2::CreateCapability
+@@ -342,6 +347,7 @@ int32_t DeviceInfoV4l2::CreateCapability
          }
        }
      }
@@ -49,7 +89,7 @@ $NetBSD: patch-third__party_libwebrtc_modules_video__capture_linux_device__info_
      close(fd);  // close since this is not the matching device
    }
  
-@@ -386,11 +392,15 @@ bool DeviceInfoV4l2::IsDeviceNameMatches
+@@ -387,11 +393,15 @@ bool DeviceInfoV4l2::IsDeviceNameMatches
  
  bool DeviceInfoV4l2::IsVideoCaptureDevice(struct v4l2_capability* cap)
  {
