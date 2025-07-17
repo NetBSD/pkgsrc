3fa4c1e2530d4fae36ed105857ae3052e23fe31e
pin <pin@pkgsrc.org>
2025-07-17 17:13:41 +0000

    sysutils/parallel-disk-usage: update to 0.13.1
     No ChangeLog provided.

--- sysutils/parallel-disk-usage/cargo-depends.mk
+++ sysutils/parallel-disk-usage/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.11 2025/06/23 08:28:51 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.12 2025/07/17 17:13:41 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	anstream-0.6.19
 CARGO_CRATE_DEPENDS+=	anstyle-1.0.11
