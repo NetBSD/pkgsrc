2be029abc40a6924c7a345fc31706292bc38911f
schmonz <schmonz@pkgsrc.org>
2025-07-18 16:19:44 +0000

    cln: update to 1.3.7. Changes:
    * Fixed compilation on ARM platforms.
    * Add support for LoongArch.

--- math/cln/patches/patch-src_base_cl__low.h
+++ math/cln/patches/patch-src_base_cl__low.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_base_cl__low.h,v 1.1 2015/05/15 09:17:03 joerg Exp $
+$NetBSD: patch-src_base_cl__low.h,v 1.2 2025/07/18 16:19:44 schmonz Exp $
 
---- src/base/cl_low.h.orig	2015-04-30 23:51:42.000000000 +0000
+--- src/base/cl_low.h.orig	2022-04-26 22:51:17.000000000 +0000
 +++ src/base/cl_low.h
-@@ -202,13 +202,7 @@ inline uint32 mulu32_unchecked (uint32 a
- // > arg1, arg2 : zwei 32-Bit-Zahlen
- // < 2^32*hi+lo : eine 64-Bit-Zahl
+@@ -218,13 +218,7 @@ inline uint32 mulu32_unchecked (uint32 a
+ #else
    extern "C" uint32 mulu32_ (uint32 arg1, uint32 arg2); // -> Low-Teil
+ #endif
 -#ifdef _MSC_VER
 -  // Workaround MSVC compiler bug: extern "C" results in wrong symbols, when
 -  // declared inside a namespace!
@@ -16,7 +16,7 @@ $NetBSD: patch-src_base_cl__low.h,v 1.1 2015/05/15 09:17:03 joerg Exp $
  #if defined(__GNUC__) && defined(__m68k__) && !defined(NO_ASM)
    #define mulu32(x,y,hi_zuweisung,lo_zuweisung)  \
      ({ var uint32 _x = (x);       \
-@@ -530,12 +524,7 @@ inline uint32 mulu32_unchecked (uint32 a
+@@ -537,12 +531,7 @@ inline uint32 mulu32_unchecked (uint32 a
  // < uint16 r: x mod y
  // < x = q*y+r
    extern "C" uint32 divu_3216_3216_ (uint32 x, uint16 y); // -> Quotient q
@@ -29,7 +29,7 @@ $NetBSD: patch-src_base_cl__low.h,v 1.1 2015/05/15 09:17:03 joerg Exp $
  #if defined(__GNUC__) && defined(__sparc64__) && !defined(NO_ASM)
    #define divu_3216_3216(x,y,q_zuweisung,r_zuweisung)  \
      ({var uint32 __x = (x);        \
-@@ -585,12 +574,7 @@ inline uint32 mulu32_unchecked (uint32 a
+@@ -592,12 +581,7 @@ inline uint32 mulu32_unchecked (uint32 a
  // < uint32 r: x mod y
  // < x = q*y+r
    extern "C" uint32 divu_3232_3232_ (uint32 x, uint32 y); // -> Quotient q
