f2bff80819ed5f07bcb8c67a3aced061313817c1
wiz <wiz@pkgsrc.org>
2025-07-19 20:50:18 +0000

    converters/hs-pandoc-server: update to pandoc-server-0.1.0.11
    No release notes have been provided by the upstream.

--- converters/hs-pandoc-server/buildlink3.mk
+++ converters/hs-pandoc-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/03/08 05:13:50 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/07/19 20:50:18 wiz Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-server
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-pandoc-server
 HS_PANDOC_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-pandoc-server+=	hs-pandoc-server>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-pandoc-server+=	hs-pandoc-server>=0.1.0.10nb3
+BUILDLINK_ABI_DEPENDS.hs-pandoc-server+=	hs-pandoc-server>=0.1.0.11
 BUILDLINK_PKGSRCDIR.hs-pandoc-server?=		../../converters/hs-pandoc-server
 
 .include "../../converters/hs-pandoc/buildlink3.mk"
