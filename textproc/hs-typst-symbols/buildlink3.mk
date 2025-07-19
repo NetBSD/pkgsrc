03fa32a273e1394b990d9b82f6d616a83044cfd2
wiz <wiz@pkgsrc.org>
2025-07-19 20:47:38 +0000

    hs-typst-symbols: update to 0.1.8.1.
    ## 0.1.8.1
    
    * Revert sorting in scrape-symbols.hs, introduced mistakenly in 0.1.8.
    
    * Regenerate symbol tables, so that defaults for things like `dots` are fixed.
    
    ## 0.1.8
    
    * Update to typst 0.13.
    textproc/hs-typst-symbols: update to typst-symbols-0.1.8.1
    
    ## 0.1.8.1
    
    * Revert sorting in scrape-symbols.hs, introduced mistakenly in 0.1.8.
    
    * Regenerate symbol tables, so that defaults for things like `dots` are fixed.
    
    ## 0.1.8
    
    * Update to typst 0.13.

--- textproc/hs-typst-symbols/buildlink3.mk
+++ textproc/hs-typst-symbols/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2025/03/05 03:40:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/07/19 20:47:38 wiz Exp $
 
 BUILDLINK_TREE+=	hs-typst-symbols
 
 .if !defined(HS_TYPST_SYMBOLS_BUILDLINK3_MK)
 HS_TYPST_SYMBOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.7
-BUILDLINK_ABI_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.7nb2
+BUILDLINK_API_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.8
+BUILDLINK_ABI_DEPENDS.hs-typst-symbols+=	hs-typst-symbols>=0.1.8.1
 BUILDLINK_PKGSRCDIR.hs-typst-symbols?=		../../textproc/hs-typst-symbols
 .endif	# HS_TYPST_SYMBOLS_BUILDLINK3_MK
 
