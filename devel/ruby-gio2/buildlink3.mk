9eeafd36f0928ba744f34b59ffe8051d152210d5
wiz <wiz@pkgsrc.org>
2025-07-19 05:37:32 +0000

    *: bump gobject-introspection users for ABI bump
    XXX: I'm actually not sure why the ABI was bumped for 1.84, but now that
    it is we have to bump PKGREVISIONs, otherwise pkgin is quite unhappy

--- devel/ruby-gio2/buildlink3.mk
+++ devel/ruby-gio2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2025/05/31 20:18:16 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/07/19 05:37:36 wiz Exp $
 
 BUILDLINK_TREE+=	ruby-gio2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gio2
 RUBY_GIO2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.9
-BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.9
+BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=4.2.9nb1
 BUILDLINK_PKGSRCDIR.ruby-gio2?=		../../devel/ruby-gio2
 
 .include "../../devel/ruby-gobject-introspection/buildlink3.mk"
