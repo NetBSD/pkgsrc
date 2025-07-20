dec94193fd14272f87dc6542e7d4e3f7b7f56fbc
wiz <wiz@pkgsrc.org>
2025-07-20 10:14:38 +0000

    devel/happy-lib: update to happy-lib-2.1.6
    ## 2.1.6
    
    * No longer emit `{-# LANGUAGE PartialTypeSignatures #-}` twice
      ([#339](https://github.com/haskell/happy/issues/339)).
    
    * No longer emit `import Data.Function` twice
      ([#341](https://github.com/haskell/happy/issues/341)).
    
    * Tested with GHC 8.0 - 9.12.2.
      The Haskell code generated by Happy is for GHC 8.0 and up.

--- devel/happy-lib/buildlink3.mk
+++ devel/happy-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2025/03/05 03:39:00 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/07/20 10:14:38 wiz Exp $
 
 BUILDLINK_TREE+=	happy-lib
 
 .if !defined(HAPPY_LIB_BUILDLINK3_MK)
 HAPPY_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.5
-BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.5nb1
+BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.6
+BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.6
 BUILDLINK_PKGSRCDIR.happy-lib?=		../../devel/happy-lib
 .endif	# HAPPY_LIB_BUILDLINK3_MK
 
