7e79ffe18a97cdc75e9aa02cec8cb9abdffc458b
jperkin <jperkin@pkgsrc.org>
2025-07-24 10:33:38 +0000

    rust: Do not include libunwind on Darwin.
    It is unclear why this was added in the first place, and it is likely broken
    for the majority of packages anyway as rust defaults to DEPMETHOD=build so
    libunwind will fail indirect library checks.  That all said it is actively
    harmful on Darwin in other cases so just skip it for that platform for now.
    This is nothing more than a kludge, and those for whom libunwind is actually
    required should implement a proper fix.

--- lang/rust/buildlink3.mk
+++ lang/rust/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2025/03/31 16:38:43 jakllsch Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2025/07/24 10:33:38 jperkin Exp $
 
 BUILDLINK_TREE+=	rust
 
@@ -13,9 +13,13 @@ BUILDLINK_API_DEPENDS.rust+=	rust>=1.56.1
 BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
 
+.include "../../mk/bsd.fast.prefs.mk"
+
 pkgbase:= rust
 .include "../../mk/pkg-build-options.mk"
-.if empty(PKG_BUILD_OPTIONS.rust:Mrust-internal-llvm)
+# XXX: unclear why this is necessary, but is actively harmful on Darwin, and
+# likely broken for most uses anyway as rust defaults to DEPMETHOD=build.
+.if empty(PKG_BUILD_OPTIONS.rust:Mrust-internal-llvm) && ${OPSYS} != "Darwin"
 .  include "../../lang/libunwind/buildlink3.mk"
 .endif
 
