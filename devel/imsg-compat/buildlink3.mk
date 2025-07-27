beb2c04c3a493d359671d40889d9ce07b98f93e2
vins <vins@pkgsrc.org>
2025-07-27 14:37:37 +0000

    devel/imsg-compat: silence some compiler warnings

--- devel/imsg-compat/buildlink3.mk
+++ devel/imsg-compat/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/07/26 09:06:46 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/07/27 14:37:37 vins Exp $
 
 BUILDLINK_TREE+=	imsg-compat
 
 .if !defined(IMSG_COMPAT_BUILDLINK3_MK)
 IMSG_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.imsg-compat+=	imsg-compat>=8.0.0nb1
+BUILDLINK_API_DEPENDS.imsg-compat+=	imsg-compat>=8.0.0nb3
 BUILDLINK_PKGSRCDIR.imsg-compat?=	../../devel/imsg-compat
 
 BUILDLINK_LIBNAME.imsg-compat=	imsg
--- devel/imsg-compat/patches/patch-src___imsg__compat.h
+++ devel/imsg-compat/patches/patch-src___imsg__compat.h
@@ -1,18 +1,31 @@
-$NetBSD: patch-src___imsg__compat.h,v 1.2 2025/07/26 09:06:46 vins Exp $
+$NetBSD: patch-src___imsg__compat.h,v 1.3 2025/07/27 14:37:38 vins Exp $
 
-Define PATH_MAX on NetBSD.
+* Define PATH_MAX on NetBSD.
+* Do not warn about unused parameters.
 
 --- src/_imsg_compat.h.orig	2024-09-04 22:07:27.000000000 +0000
 +++ src/_imsg_compat.h
-@@ -27,8 +27,10 @@
+@@ -27,13 +27,21 @@
  #include <string.h>
  #include <unistd.h>
  
 -#ifdef __linux__
 +#if defined(__linux__)
  # include <linux/limits.h>
-+#elif defined(__NetBSD__) 
++#elif defined(__NetBSD__)
 +# include <sys/syslimits.h>
++#endif
++
++#ifdef __GNUC__
++#  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
++#else
++#  define UNUSED(x) UNUSED_ ## x
  #endif
  
  #ifndef HAVE_EXPLICIT_BZERO
+ static void
+-__explicit_bzero_hook(void *buf, size_t len)
++__explicit_bzero_hook(void *UNUSED(buf), size_t UNUSED(len))
+ {
+ }
+ 
