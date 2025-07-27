5aab2e41e5a02593a2cbf086e106d613d4f8d1c5
wiz <wiz@pkgsrc.org>
2025-07-27 09:18:49 +0000

    x11/tk90: import tk90-9.0.2
    Based on work by Sean Cole.
    
    This package contains the binary release of Tk, a GUI toolkit for Tcl.
    
    The best way to get started with Tcl is to read ``Practical Programming in
    TCL and Tk, 4th Edition'' by Brent Welch and Ken Jones, Prentice Hall PTR,
    ISBN 0-13-038560-3.  There's also the collectible ``Tcl and the Tk
    Toolkit'' by John K. Ousterhout, Addison-Wesley, ISBN 0-20-163337-X.
    
    This package contains the 9.0 branch of Tk.

---
+++ x11/tk90/version.mk
@@ -0,0 +1,10 @@
+# $NetBSD: version.mk,v 1.1 2025/07/27 09:18:49 wiz Exp $
+# used by cad/xcircuit/Makefile
+# used by devel/tcl-tclcl/Makefile
+# used by lang/tcl/Makefile
+# used by lang/tcl-otcl/Makefile
+# used by x11/tk/Makefile
+
+TK_SONUM=	90
+TK_BASEVER=	9.0
+TK_VERSION=	9.0.2
