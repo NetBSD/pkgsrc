a27b5733caa1e9d2dc46273dbbd5688528561ec3
wiz <wiz@pkgsrc.org>
2025-07-27 09:17:06 +0000

    lang/tcl90: import tcl90-9.0.2
    Based on work by Sean Cole.
    
    Tcl (Tool Command Language) is a very powerful but easy to learn dynamic
    programming language, suitable for a very wide range of uses, including
    web and desktop applications, networking, administration, testing and
    many more. Open source and business-friendly, Tcl is a mature yet
    evolving language that is truly cross platform, easily deployed and
    highly extensible.
    
    Tcl/Tk resources:
      Newsgroup: comp.lang.tcl
      Wiki: http://wiki.tcl.tk
      Books:
       * Practical Programming in Tcl and Tk, 4th Edition
         ISBN: 0-13-038560-3
       * Tcl and the Tk Toolkit, 2nd Edition
         ISBN: 978-0-321-33633-0
    
    This package contains the 9.0 branch of tcl.

---
+++ lang/tcl90/version.mk
@@ -0,0 +1,15 @@
+# $NetBSD: version.mk,v 1.1 2025/07/27 09:17:06 wiz Exp $
+# used by cad/xcircuit/Makefile
+# used by devel/tcl-tclcl/Makefile
+# used by lang/tcl/Makefile
+# used by lang/tcl-otcl/Makefile
+
+TCL_SONUM=	90
+TCL_BASEVER=	9.0
+TCL_VERSION=	9.0.2
+
+# Update bundled versions in lang/tcl/buildlink3.mk
+ITCL_VERSION=		4.3.3
+SQLITE3_VERSION=	3.49.1
+TDBC_VERSION=		1.1.11
+THREAD_VERSION=		3.0.2
