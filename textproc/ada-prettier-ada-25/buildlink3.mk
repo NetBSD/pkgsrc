871f5be70b962f5daa83b28ae00eec21a8b2a27b
dkazankov <dkazankov@pkgsrc.org>
2025-07-28 11:40:31 +0000

    textproc/ada-prettier-ada-25: add new package 25.0.0
    Prettier-Ada - port of the Prettier formatter to the Ada

---
+++ textproc/ada-prettier-ada-25/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/07/28 11:40:31 dkazankov Exp $
+
+BUILDLINK_TREE+=	prettier-ada-25
+
+.if !defined(PRETTIER_ADA_25_BUILDLINK3_MK)
+PRETTIER_ADA_25_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.prettier-ada-25+=		prettier-ada-${GNAT_NAME}>=25.0.0
+BUILDLINK_PKGSRCDIR.prettier-ada-25?=		../../textproc/ada-prettier-ada-25
+BUILDLINK_DEPMETHOD.prettier-ada-25?=		build
+
+BUILDLINK_CONTENTS_FILTER.prettier-ada-25=	\
+    ${EGREP} "${GNAT_PKGBASE}/include/.*$$|${GNAT_PKGBASE}/lib/.*$$|${GNAT_PKGBASE}/share/gpr/.*$$"
+
+BUILDLINK_FNAME_TRANSFORM.prettier-ada-25+=	\
+    -e "s|${BUILDLINK_DIR}/${GNAT_PKGBASE}/|${BUILDLINK_DIR}/|g"
+
+.endif
+
+BUILDLINK_TREE+=	-prettier-ada-25
