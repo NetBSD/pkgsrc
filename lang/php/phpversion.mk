11b92721045611e1d5a77fe06a309e2338ab5a30
taca <taca@pkgsrc.org>
2025-08-02 03:13:16 +0000

    lang/php84: update to 8.4.11
    PHP 8.4.11 (2025-07-31)
    
    - Calendar:
      . Fixed jewishtojd overflow on year argument. (David Carlier)
    
    - Core:
      . Fixed bug GH-18833 (Use after free with weakmaps dependent on destruction
        order). (Daniil Gentili)
      . Fixed bug GH-18907 (Leak when creating cycle in hook). (ilutov)
      . Fix OSS-Fuzz #427814456. (nielsdos)
      . Fix OSS-Fuzz #428983568 and #428760800. (nielsdos)
      . Fixed bug GH-17204 (-Wuseless-escape warnings emitted by re2c). (Peter Kokot)
      . Fixed bug GH-19064 (Undefined symbol 'execute_ex' on Windows ARM64).
        (Demon)
    
    - Curl:
      . Fix memory leaks when returning refcounted value from curl callback.
        (nielsdos)
      . Remove incorrect string release. (nielsdos)
    
    - DOM:
      . Fixed bug GH-18979 (Dom\XMLDocument::createComment() triggers undefined
        behavior with null byte). (nielsdos)
    
    - LDAP:
      . Fixed GH-18902 ldap_exop/ldap_exop_sync assert triggered on empty
        request OID. (David Carlier)
    
    - MbString:
      . Fixed bug GH-18901 (integer overflow mb_split). (nielsdos)
    
    - Opcache:
      . Fixed bug GH-18639 (Internal class aliases can break preloading + JIT).
        (nielsdos)
      . Fixed bug GH-18899 (JIT function crash when emitting undefined variable
        warning and opline is not set yet). (nielsdos)
      . Fixed bug GH-14082 (Segmentation fault on unknown address 0x600000000018
        in ext/opcache/jit/zend_jit.c). (nielsdos)
      . Fixed bug GH-18898 (SEGV zend_jit_op_array_hot with property hooks
        and preloading). (nielsdos)
    
    - OpenSSL:
      . Fixed bug #80770 (It is not possible to get client peer certificate with
        stream_socket_server). (Jakub Zelenka)
    
    - PCNTL:
      . Fixed bug GH-18958 (Fatal error during shutdown after pcntl_rfork() or
        pcntl_forkx() with zend-max-execution-timers). (Arnaud)
    
    - Phar:
      . Fix stream double free in phar. (nielsdos, dixyes)
      . Fix phar crash and file corruption with SplFileObject. (nielsdos)
    
    - SOAP:
      . Fixed bug GH-18990, bug #81029, bug #47314 (SOAP HTTP socket not closing
        on object destruction). (nielsdos)
      . Fix memory leak when URL parsing fails in redirect. (Girgias)
    
    - SPL:
      . Fixed bug GH-19094 (Attaching class with no Iterator implementation to
        MultipleIterator causes crash). (nielsdos)
    
    - Standard:
      . Fix misleading errors in printf(). (nielsdos)
      . Fix RCN violations in array functions. (nielsdos)
      . Fixed GH-18976 pack() overflow with h/H format and INT_MAX repeater value.
        (David Carlier)
    
    - Streams:
      . Fixed GH-13264 (fgets() and stream_get_line() do not return false on filter
        fatal error). (Jakub Zelenka)
    
    - Zip:
      . Fix leak when path is too long in ZipArchive::extractTo(). (nielsdos)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.471 2025/08/02 03:11:45 taca Exp $
+# $NetBSD: phpversion.mk,v 1.472 2025/08/02 03:13:16 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -113,7 +113,7 @@ PHP74_VERSION=	7.4.33
 PHP81_VERSION=	8.1.33
 PHP82_VERSION=	8.2.29
 PHP83_VERSION=	8.3.24
-PHP84_VERSION=	8.4.10
+PHP84_VERSION=	8.4.11
 
 _VARGROUPS+=	php
 _USER_VARS.php=	PHP_VERSION_DEFAULT
