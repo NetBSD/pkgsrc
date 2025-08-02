fe1577715a24c34e99910c2a62af6864db3286c4
pin <pin@pkgsrc.org>
2025-08-02 20:46:09 +0000

    devel/mise: update to 2025.8.2
    v2025.8.2
    🚀 Features
    
        (registry) add jjui by @TyceHerrman in #5877
        (registry) add trunk metalinter by @daveio in #5875
    
    🐛 Bug Fixes
    
        (python) Windows OS no longer suffixed with -shared by @malept in #5879
    
    v2025.8.1
    🐛 Bug Fixes
    
        node gpg keys by @jdx in #5866
    
    v2025.8.0
    🚀 Features
    
        (registry) use npm backend for yarn by @mrazauskas in #5745
        (registry) add codebuff tool by @zacheryph in #5856
    
    🐛 Bug Fixes
    
        (go) implement heuristic-based go module find logic by @risu729 in #5851
        (node) Add NodeJS maintainer Antoine du Hamel's new GPG key by @chadlwilson in #5862
        (pipx) align HTML backend with PEP 503 registry URL assignment by @acesyde in #5853
        (registry) fix balena ubi backend options by @risu729 in #5861
        (registry) add aqua backends to tools by @risu729 in #5863
    
    📚 Documentation
    
        fix uv_venv_create_args reference for python by @jasonraimondi in #5854
        expand on env directive examples and formats by @syhol in #5857
    
    ◀️ Revert
    
        Revert "docs: fix uv_venv_create_args reference for python" by @jdx in #5859

--- devel/mise/cargo-depends.mk
+++ devel/mise/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.79 2025/07/31 07:56:47 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.80 2025/08/02 20:46:09 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
