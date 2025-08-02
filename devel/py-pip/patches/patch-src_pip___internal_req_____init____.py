a5e0b87caf34e7892876e8a1ca14813fb3c6c22f
adam <adam@pkgsrc.org>
2025-08-02 17:24:33 +0000

    py-pip: updated to 25.2
    25.2 (2025-07-30)
    
    Features
    
    - Declare support for Python 3.14
    - Automatic download resumption and retrying is enabled by default.
    - Requires-Python error message displays version clauses in numerical order.
    - Minor performance improvement getting the order to install a very large number of interdependent packages.
    - Show time taken instead of ``eta 0:00:00`` at download completion.
    - Speed up small CLI tools by removing ``import re`` from the console
      script executable template.
    - Remove warning when cloning from a Git reference that does not look like a commit hash.
    
    Bug Fixes
    
    - ``pip config debug`` now correctly separates options as set by the different files
      at the same level.
    - Ensure truststore feature remains active even when a proxy is also in use.
    - Include sub-commands in tab completion.
    - ``pip list`` with the ``json`` or ``freeze`` format enabled will no longer
      crash when encountering a package with an invalid version.
    - Provide a hint if a system error is raised involving long filenames or path segments on Windows.
    - Resumed downloads are saved to the HTTP cache like any other normal download.
    - Configured verbosity is consistently forwarded while calling Git during
      VCS operations.
    - Suppress the progress bar, when running with ``--log`` and ``--quiet``.
    
      Consequently, a new ``auto`` mode for ``--progress-bar`` has been added.
      ``auto`` will enable progress bars unless suppressed by ``--quiet``,
      while ``on`` will always enable progress bars.
    - Fix normalization of local URLs with non-``file`` schemes.
    - Fix normalization of local file URLs on Windows in newer Python versions.
    - Fix remaining test failures in Python 3.14 by adjusting ``path_to_url`` and similar functions.
    - Fix missing ``network`` test markings, making the suite pass in offline
      environments again.
    
    Vendored Libraries
    
    - Upgrade CacheControl to 0.14.3
    - Upgrade certifi to 2025.7.14
    - Upgrade distlib to 0.4.0
    - Upgrade msgpack to 1.1.1
    - Upgrade platformdirs to 4.3.8
    - Upgrade pygments to 2.19.2
    - Upgrade requests to 2.32.4
    - Upgrade resolvelib to 1.2.0
    - Upgrade rich to 14.1.0
    - Remove vendored typing-extensions.
    
    Process
    
    - pip's own licensing metadata now follows :pep:`639`.
      In addition, the licenses of pip's vendored dependencies are now included
      in the ``License-File`` metadata field and in the wheel.

--- devel/py-pip/patches/patch-src_pip___internal_req_____init____.py
+++ devel/py-pip/patches/patch-src_pip___internal_req_____init____.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-src_pip___internal_req_____init____.py,v 1.1 2023/06/14 11:30:54 riastradh Exp $
+$NetBSD: patch-src_pip___internal_req_____init____.py,v 1.2 2025/08/02 17:24:33 adam Exp $
 
 Allow pip install --executable=/path to set path in script #! lines.
 https://github.com/pypa/pip/issues/12087
 https://github.com/pypa/pip/pull/12088
 
---- src/pip/_internal/req/__init__.py.orig	2023-04-26 09:23:01.000000000 +0000
+--- src/pip/_internal/req/__init__.py.orig	2025-07-30 21:31:52.000000000 +0000
 +++ src/pip/_internal/req/__init__.py
-@@ -40,6 +40,7 @@ def install_given_reqs(
-     root: Optional[str],
-     home: Optional[str],
-     prefix: Optional[str],
-+    executable: Optional[str],
+@@ -41,6 +41,7 @@ def install_given_reqs(
+     root: str | None,
+     home: str | None,
+     prefix: str | None,
++    executable: str | None,
      warn_script_location: bool,
      use_user_site: bool,
      pycompile: bool,
-@@ -74,6 +75,7 @@ def install_given_reqs(
+@@ -87,6 +88,7 @@ def install_given_reqs(
                      root=root,
                      home=home,
                      prefix=prefix,
