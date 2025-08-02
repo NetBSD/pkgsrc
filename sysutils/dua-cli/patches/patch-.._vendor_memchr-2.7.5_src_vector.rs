309267e065e672f037950c0809bca71f272e8185
pin <pin@pkgsrc.org>
2025-08-02 14:26:14 +0000

    sysutils/dua-cli: update to 2.31.0
    This release prominently adds a prompt that shows before quitting the app.
    When you pressed esc or q, the status bar will show the prompt first.
    To really quit, you need to press esc or q again.
    You can also cancel the quit operation by pressing any key else.
    Meanwhile, ctrl-c still quits the app directly since it's a combination key.
    
    That way, dua will not cause users to accidentally quit the app when they only
    want to dismiss some other panels.
    It's especially frustrating if the scan took a long time
    
    New Features
    
     - prompt before quitting

--- sysutils/dua-cli/patches/patch-.._vendor_memchr-2.7.5_src_vector.rs
+++ sysutils/dua-cli/patches/patch-.._vendor_memchr-2.7.5_src_vector.rs
@@ -1,9 +1,9 @@
-$NetBSD: patch-.._vendor_memchr-2.7.4_src_vector.rs,v 1.1 2024/10/02 17:49:02 he Exp $
+$NetBSD: patch-.._vendor_memchr-2.7.5_src_vector.rs,v 1.1 2025/08/02 14:26:15 pin Exp $
 
 Make this build on big-endian aarch64.
 
---- ../vendor/memchr-2.7.4/src/vector.rs.orig	2024-10-01 22:43:45.510956667 +0000
-+++ ../vendor/memchr-2.7.4/src/vector.rs
+--- ../vendor/memchr-2.7.5/src/vector.rs.orig	2025-08-02 13:25:30.811977578 +0000
++++ ../vendor/memchr-2.7.5/src/vector.rs
 @@ -289,7 +289,7 @@ mod x86avx2 {
      }
  }
