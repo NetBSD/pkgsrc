fc8b8f2006e1a737cb74da16620b4ae5c4a1f488
adam <adam@pkgsrc.org>
2025-08-02 17:24:33 +0000

    py-pip: updated to 25.2
    25.2 (2025-07-30)
    
    Features
    
    - Declare support for Python 3.14
    - Automatic download resumption and retrying is enabled by default.
    - Requires-Python error message displays version clauses in numerical order.
    - Minor performance improvement getting the order to install a very large number of interdependent packages.
    - Show time taken instead of ``eta 0:00:00`` at download completion.
    - Speed up small CLI tools by removing ``import re`` from the console
      script executable template.
    - Remove warning when cloning from a Git reference that does not look like a commit hash.
    
    Bug Fixes
    
    - ``pip config debug`` now correctly separates options as set by the different files
      at the same level.
    - Ensure truststore feature remains active even when a proxy is also in use.
    - Include sub-commands in tab completion.
    - ``pip list`` with the ``json`` or ``freeze`` format enabled will no longer
      crash when encountering a package with an invalid version.
    - Provide a hint if a system error is raised involving long filenames or path segments on Windows.
    - Resumed downloads are saved to the HTTP cache like any other normal download.
    - Configured verbosity is consistently forwarded while calling Git during
      VCS operations.
    - Suppress the progress bar, when running with ``--log`` and ``--quiet``.
    
      Consequently, a new ``auto`` mode for ``--progress-bar`` has been added.
      ``auto`` will enable progress bars unless suppressed by ``--quiet``,
      while ``on`` will always enable progress bars.
    - Fix normalization of local URLs with non-``file`` schemes.
    - Fix normalization of local file URLs on Windows in newer Python versions.
    - Fix remaining test failures in Python 3.14 by adjusting ``path_to_url`` and similar functions.
    - Fix missing ``network`` test markings, making the suite pass in offline
      environments again.
    
    Vendored Libraries
    
    - Upgrade CacheControl to 0.14.3
    - Upgrade certifi to 2025.7.14
    - Upgrade distlib to 0.4.0
    - Upgrade msgpack to 1.1.1
    - Upgrade platformdirs to 4.3.8
    - Upgrade pygments to 2.19.2
    - Upgrade requests to 2.32.4
    - Upgrade resolvelib to 1.2.0
    - Upgrade rich to 14.1.0
    - Remove vendored typing-extensions.
    
    Process
    
    - pip's own licensing metadata now follows :pep:`639`.
      In addition, the licenses of pip's vendored dependencies are now included
      in the ``License-File`` metadata field and in the wheel.

--- devel/py-pip/patches/patch-src_pip___internal_operations_install_wheel.py
+++ devel/py-pip/patches/patch-src_pip___internal_operations_install_wheel.py
@@ -1,30 +1,31 @@
-$NetBSD: patch-src_pip___internal_operations_install_wheel.py,v 1.1 2023/06/14 11:30:54 riastradh Exp $
+$NetBSD: patch-src_pip___internal_operations_install_wheel.py,v 1.2 2025/08/02 17:24:33 adam Exp $
 
 Allow pip install --executable=/path to set path in script #! lines.
 https://github.com/pypa/pip/issues/12087
 https://github.com/pypa/pip/pull/12088
 
---- src/pip/_internal/operations/install/wheel.py.orig	2023-04-26 09:23:01.000000000 +0000
+--- src/pip/_internal/operations/install/wheel.py.orig	2025-07-30 21:31:52.000000000 +0000
 +++ src/pip/_internal/operations/install/wheel.py
-@@ -422,6 +422,9 @@ def _raise_for_invalid_entrypoint(specif
+@@ -417,6 +417,10 @@ class PipScriptMaker(ScriptMaker):
+ """
+     )
  
- 
- class PipScriptMaker(ScriptMaker):
 +    def __init__(self, executable, *args, **kwargs):
 +        super().__init__(*args, **kwargs)
 +        self.executable = executable
++
      def make(
-         self, specification: str, options: Optional[Dict[str, Any]] = None
-     ) -> List[str]:
-@@ -434,6 +437,7 @@ def _install_wheel(
+         self, specification: str, options: dict[str, Any] | None = None
+     ) -> list[str]:
+@@ -429,6 +433,7 @@ def _install_wheel(  # noqa: C901, PLR09
      wheel_zip: ZipFile,
      wheel_path: str,
      scheme: Scheme,
-+    executable: Optional[str] = None,
++    executable: str | None = None,
      pycompile: bool = True,
      warn_script_location: bool = True,
-     direct_url: Optional[DirectUrl] = None,
-@@ -624,7 +628,7 @@ def _install_wheel(
+     direct_url: DirectUrl | None = None,
+@@ -630,7 +635,7 @@ def _install_wheel(  # noqa: C901, PLR09
                          record_installed(pyc_record_path, pyc_path)
          logger.debug(stdout.getvalue())
  
@@ -33,15 +34,15 @@ https://github.com/pypa/pip/pull/12088
  
      # Ensure old scripts are overwritten.
      # See https://github.com/pypa/pip/issues/1800
-@@ -721,6 +725,7 @@ def install_wheel(
+@@ -727,6 +732,7 @@ def install_wheel(
      wheel_path: str,
      scheme: Scheme,
      req_description: str,
-+    executable: Optional[str] = None,
++    executable: str | None = None,
      pycompile: bool = True,
      warn_script_location: bool = True,
-     direct_url: Optional[DirectUrl] = None,
-@@ -733,6 +738,7 @@ def install_wheel(
+     direct_url: DirectUrl | None = None,
+@@ -739,6 +745,7 @@ def install_wheel(
                  wheel_zip=z,
                  wheel_path=wheel_path,
                  scheme=scheme,
--- devel/py-pip/patches/patch-src_pip___internal_req_req__install.py
+++ devel/py-pip/patches/patch-src_pip___internal_req_req__install.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-src_pip___internal_req_req__install.py,v 1.2 2023/10/15 19:37:43 adam Exp $
+$NetBSD: patch-src_pip___internal_req_req__install.py,v 1.3 2025/08/02 17:24:33 adam Exp $
 
 Allow pip install --executable=/path to set path in script #! lines.
 https://github.com/pypa/pip/issues/12087
 https://github.com/pypa/pip/pull/12088
 
---- src/pip/_internal/req/req_install.py.orig	2023-10-15 16:17:57.000000000 +0000
+--- src/pip/_internal/req/req_install.py.orig	2025-07-30 21:31:52.000000000 +0000
 +++ src/pip/_internal/req/req_install.py
-@@ -812,6 +812,7 @@ class InstallRequirement:
-         root: Optional[str] = None,
-         home: Optional[str] = None,
-         prefix: Optional[str] = None,
-+        executable: Optional[str] = None,
+@@ -813,6 +813,7 @@ class InstallRequirement:
+         root: str | None = None,
+         home: str | None = None,
+         prefix: str | None = None,
++        executable: str | None = None,
          warn_script_location: bool = True,
          use_user_site: bool = False,
          pycompile: bool = True,
-@@ -848,6 +849,7 @@ class InstallRequirement:
+@@ -871,6 +872,7 @@ class InstallRequirement:
              self.req.name,
              self.local_file_path,
              scheme=scheme,
