6814259f9c55633576658f30f3f0419bd621f737
dkazankov <dkazankov@pkgsrc.org>
2025-08-05 05:08:18 +0000

    ada-gnatcoll-bindings-25: refactor previous patch to fix build error on NetBSD 9

--- devel/ada-gnatcoll-bindings-25/options.mk
+++ devel/ada-gnatcoll-bindings-25/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2025/07/11 12:46:50 dkazankov Exp $
+# $NetBSD: options.mk,v 1.2 2025/08/05 05:08:18 dkazankov Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gnatcoll-bindings-25
 PKG_SUPPORTED_OPTIONS=		gnatcoll-cpp gmp iconv lzma openmp \
@@ -24,6 +24,13 @@ BINDINGS+=	gmp
 USE_GNU_ICONV=	yes
 .  include "../../converters/libiconv/buildlink3.mk"
 BINDINGS+=	iconv
+.if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 099982
+SUBST_CLASSES+=		iconv
+SUBST_STAGE.iconv=	pre-configure
+SUBST_FILES.iconv=	iconv/iconv_support.c
+SUBST_MESSAGE.iconv=	Fix parameter type in ${SUBST_FILES.iconv}
+SUBST_SED.iconv=	-e 's/iconv(cd, inbuf,/iconv(cd, (const char **)inbuf,/'
+.endif
 .endif
 
 .if !empty(PKG_OPTIONS:Mlzma)
