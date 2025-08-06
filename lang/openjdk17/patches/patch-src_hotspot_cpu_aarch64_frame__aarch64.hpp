3761528a73b222226f9dc84144a1f7d8b1ab6e00
pho <pho@pkgsrc.org>
2025-08-06 08:47:15 +0000

    lang/openjdk17: Workaround for the VM dying on Apple Silicon chips
    See the patch for threadWXSetters.inline.hpp for details. I rebuilt the
    bootkit for aarch64 with new patches applied. This may cause some
    performance regression on Cortex series, but as I stated in the patch
    comment, relying on implementation details of a particular chip is
    fundamentally unsound, and that's why it didn't work on Apple Silicon in
    the first place.
    
    But since the upstream has stated they are knowingly doing this, the chance
    of these patches getting accepted is slim, if not zero.
    
    Also backported fixes regarding floating point arithmetics that appeared in
    OpenJDK 22.

---
+++ lang/openjdk17/patches/patch-src_hotspot_cpu_aarch64_frame__aarch64.hpp
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_hotspot_cpu_aarch64_frame__aarch64.hpp,v 1.1 2025/08/06 08:47:15 pho Exp $
+
+Backport a bugfix appeared in Java 22:
+https://github.com/openjdk/jdk21u-dev/commit/9e582fcbb8ab34b44ff01ac13de5cc4d6487396d
+
+--- src/hotspot/cpu/aarch64/frame_aarch64.hpp.orig	2025-08-01 18:15:42.300499547 +0000
++++ src/hotspot/cpu/aarch64/frame_aarch64.hpp
+@@ -95,7 +95,7 @@
+     // Entry frames
+     // n.b. these values are determined by the layout defined in
+     // stubGenerator for the Java call stub
+-    entry_frame_after_call_words                     = 27,
++    entry_frame_after_call_words                     = 29,
+     entry_frame_call_wrapper_offset                  = -8,
+ 
+     // we don't need a save area
---
+++ lang/openjdk17/patches/patch-src_hotspot_cpu_aarch64_macroAssembler__aarch64.cpp
@@ -0,0 +1,31 @@
+$NetBSD: patch-src_hotspot_cpu_aarch64_macroAssembler__aarch64.cpp,v 1.1 2025/08/06 08:47:15 pho Exp $
+
+Backport a bugfix appeared in Java 22:
+https://github.com/openjdk/jdk/commit/50f31240555888018f0f496ab29c8a5932dce459
+
+--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp.orig	2025-08-01 18:07:27.105868141 +0000
++++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+@@ -3670,6 +3670,23 @@ void MacroAssembler::load_klass(Register
+   }
+ }
+ 
++void MacroAssembler::restore_cpu_control_state_after_jni(Register tmp1, Register tmp2) {
++  if (RestoreMXCSROnJNICalls) {
++    Label OK;
++    get_fpcr(tmp1);
++    mov(tmp2, tmp1);
++    // Set FPCR to the state we need. We do want Round to Nearest. We
++    // don't want non-IEEE rounding modes or floating-point traps.
++    bfi(tmp1, zr, 22, 4); // Clear DN, FZ, and Rmode
++    bfi(tmp1, zr, 8, 5);  // Clear exception-control bits (8-12)
++    bfi(tmp1, zr, 0, 2);  // Clear AH:FIZ
++    eor(tmp2, tmp1, tmp2);
++    cbz(tmp2, OK);        // Only reset FPCR if it's wrong
++    set_fpcr(tmp1);
++    bind(OK);
++  }
++}
++
+ // ((OopHandle)result).resolve();
+ void MacroAssembler::resolve_oop_handle(Register result, Register tmp) {
+   // OopHandle::resolve is an indirection.
