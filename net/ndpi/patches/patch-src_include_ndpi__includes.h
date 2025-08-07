b84e716f9ac68477e976548ef13a6e3ae6646126
adam <adam@pkgsrc.org>
2025-08-07 05:03:08 +0000

    ndpi: updated to 4.14
    4.14
    
    Major Changes
    
    Introduce QoE (Quality of Experience) classification
    
    New Supported Protocols and Services
    
    Add DigitalOcean protocol
    Add GearUP Booster application protocol/dissector (heuristic based)
    Add LagoFast protocol dissector.
    Add RUTUBE
    Add Vivox support
    Add new protocol ID to handle Mozilla/Firefox generic traffic
    Add health category
    Unify "Skype" and "Teams" IDs
    Information about all protocols are available at https://github.com/ntop/nDPI/blob/dev/doc/protocols.rst
    
    New features
    
    Add ndpi_find_protocol_qoe() API call
    Add ndpi_network_ptree6_match() API call
    Add ndpi_data_jitter() API call
    
    New configuration knobs
    
    Add configuration parameter to enable/disable export of flow risk info
    Add a specific configuration for classification only
    Add the ability to enable/disable every specific flow risks
    Extend configuration to enable/disable export of flow risk info
    bittorrent: add configuration for "hash" metadata
    HTTP: add configuration for some metadata
    SSDP: add configuration for disabling metadata extraction

--- net/ndpi/patches/patch-src_include_ndpi__includes.h
+++ net/ndpi/patches/patch-src_include_ndpi__includes.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_include_ndpi__includes.h,v 1.2 2023/11/20 17:57:31 adam Exp $
+$NetBSD: patch-src_include_ndpi__includes.h,v 1.3 2025/08/07 05:03:08 adam Exp $
 
 Same case with SunOS.
 
---- src/include/ndpi_includes.h.orig	2023-10-23 09:45:30.000000000 +0000
+--- src/include/ndpi_includes.h.orig	2025-04-28 06:58:26.000000000 +0000
 +++ src/include/ndpi_includes.h
-@@ -50,7 +50,7 @@
- #include <netinet/udp.h>
- #endif
+@@ -45,7 +45,7 @@
+ #include <sys/socket.h>
+ #include <netinet/in.h>
  
 -#if !defined __APPLE__ && !defined __FreeBSD__ && !defined __NetBSD__ && !defined __OpenBSD__
 +#if !defined __APPLE__ && !defined __FreeBSD__ && !defined __NetBSD__ && !defined __OpenBSD__ && !defined __sun
