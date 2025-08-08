9d8ae182c9f24275f871bc9e69f0d23a48598356
wiz <wiz@pkgsrc.org>
2025-08-08 08:13:08 +0000

    rtmpdump: update to 2.6.0.0.6f6bb1353fc84f4cc37138baa99f586750028a01.
    1 March 2024, v2.6
    - miscellaneous cleanup of authentication and encryption support

--- net/rtmpdump/buildlink3.mk
+++ net/rtmpdump/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2025/04/17 21:52:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2025/08/08 08:13:08 wiz Exp $
 
 BUILDLINK_TREE+=	rtmpdump
 
@@ -9,9 +9,7 @@ BUILDLINK_API_DEPENDS.rtmpdump+=	rtmpdump>=2.3
 BUILDLINK_ABI_DEPENDS.rtmpdump+=	rtmpdump>=2.4.0.0.fa8646daeb19dfd12c181f7d19de708d623704c0nb16
 BUILDLINK_PKGSRCDIR.rtmpdump?=		../../net/rtmpdump
 
-.include "../../devel/gmp/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../security/nettle/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .endif	# RTMPDUMP_BUILDLINK3_MK
 
