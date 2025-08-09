2621a8474cc3e115d07afd81984fc75b11a9d477
leot <leot@pkgsrc.org>
2025-08-09 19:15:13 +0000

    direnv: Update to 2.37.1
    Changes:
    2.37.1 / 2025-07-20
    ===================
    * fix: regression in displaying export errors
    
    2.37.0 / 2025-07-02
    ===================
    * docs: add github-actions page
    * docs: document sub-commands
    * docs: fix link to guix manual
    * docs: re-generate manpages
    * feat(direnv export gha): strengthen export format
    * feat: add windows arm64 target
    * fix(powershell): "export pwsh" to resolve PowerShell special character issues
    * fix(python): do not include patch level in virtual environment names
    * fix(use_nix): always restore special variables
    * fix: accept true as valid DIRENV_DEBUG value
    * fix: add trailing newline to error messages
    * fix: delete duplicate ansi escape code
    
    2.36.0 / 2025-04-11
    ===================
    * direnv now requires go 1.24
    * doc: Correct duplicate usage of 'with' in the direnv(1)
    * doc: note direnv version for log_{format,filter}
    * feat: Add `use_flox` to stdlib.sh
    * feat: logging filter
    * fix use_nix: unset TMPDIR variables
    * fix: A more universal fix for the python 3.14 `find_spec` deprecation warning
    * fix: Don't give an error when the current directory doesn't exist
    * fix: add support to fully reproducible guix shells
    * fix: assert minimum powershell version
    * fix: escape newlines in generated vimscript
    * fix: fix empty array error in install.sh
    * fix: optionally authenticate against github api during install
    * fix: use_guix: Enable the watching of Guix related files.

--- sysutils/direnv/go-modules.mk
+++ sysutils/direnv/go-modules.mk
@@ -1,10 +1,11 @@
-# $NetBSD: go-modules.mk,v 1.7 2024/10/20 09:51:28 leot Exp $
+# $NetBSD: go-modules.mk,v 1.8 2025/08/09 19:15:13 leot Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.4.0.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.4.0.zip
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.5.0.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.5.0.zip
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.mod
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.19.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.19.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.25.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.25.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.zip
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.zip
