f02aba991562c440fd32d2fb5a53039d0305219f
gdt <gdt@pkgsrc.org>
2025-08-10 21:51:59 +0000

    sysutils/ups-nut: Update to 2.8.4
    Release notes for NUT 2.8.4 - what's new since 2.8.3
    -------------------------------------------------------------
    
    https://github.com/networkupstools/nut/milestone/9
    
     - New NUT drivers:
       * Introduced a `ve-direct` driver for Victron Energy UPS/solar panels
         monitoring. Most specific reported values are in an `experimental.*`
         namespace, as a community we need to come up with standard naming for
         those via `docs/nut-names.txt`. [#440]
       * Introduced a `nutdrv_hashx` driver for numerous devices from Ablerex,
         Atlantis Land, Epyc, Infosec, ION, PowerWalker, Right Power Technology,
         Salicru, UPS Solutions and other vendors (originally shipped with a
         "PowerMaster+", "PowerMaster" or "PowerGuide" software companion suite).
         This seems to be a protocol developed by Cyber Energy for serial-port
         devices, subsequently used by different vendors in their own products
         or re-branded Cyber Energy creations. [#2940]
       * Introduced a `failover` driver for monitoring multiple UPS driver sockets
         and seamless switching out of UPS data in a failover situation, includes
         support for end-to-end tracked instant commands and also variable updating.
         [#2962]
       * Introduced USB (`powervar_cx_usb`) and Serial (`powervar_cx_ser`) drivers
         for Powervar CUSPP protocol, tested with GTS (USB) and UPM (USB, Serial)
         models. [#2988]
    
     - NUT Monitor GUI:
       * Ported Python 3 version to Qt6, now shipped alongside Qt5 for systems
         with either or both, maximizing compatibility with old and new setups.
         [#2946]

--- sysutils/ups-nut/buildlink3.mk
+++ sysutils/ups-nut/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2022/06/28 11:36:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/08/10 21:51:59 gdt Exp $
 
 BUILDLINK_TREE+=	ups-nut
 
@@ -14,8 +14,8 @@ pkgbase:= ups-nut
 
 .if ${PKG_BUILD_OPTIONS.ups-nut:Mnss}
 .  include "../../devel/nss/buildlink3.mk"
-#.elif ${PKG_BUILD_OPTIONS.ups-nut:Mopenssl}
-#.  include "../../security/openssl/buildlink3.mk"
+.elif ${PKG_BUILD_OPTIONS.ups-nut:Mopenssl}
+.  include "../../security/openssl/buildlink3.mk"
 .endif
 
 .include "../../mk/pthread.buildlink3.mk"
