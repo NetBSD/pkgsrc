bd506da2cc675ae3d4cb983ad8ed181a24487194
gdt <gdt@pkgsrc.org>
2025-08-10 14:36:24 +0000

    chat/py-xmpppy: Update to 0.7.2
    pkgsrc:
      - Convert to wheel.mk (NFCI).
      - Drop ssl patch cherrypicked from upstream, now in this release.
      - Add patch with attempts to remove disabling of certificate
        validation (to match behavior of 0.7.1 in pkgsrc), and comments
        about why that doesn't work, as being on the path to working with
        upstream.
    
    2025-08-09 0.7.2
    ================
    - Fixed ``AttributeError: module 'ssl' has no attribute 'wrap_socket'``.
      Thanks, @juanvalino.
    - Ensured certificate checking is disabled to maintain backward compatibility.
      Thanks, @juanvalino.
    - Added example for sending data to a MUC room. Thanks, @sebix.

--- chat/py-xmpppy/patches/patch-xmpp_transports.py
+++ chat/py-xmpppy/patches/patch-xmpp_transports.py
@@ -1,31 +1,21 @@
-$NetBSD: patch-xmpp_transports.py,v 1.5 2024/11/24 00:52:59 gdt Exp $
+$NetBSD: patch-xmpp_transports.py,v 1.6 2025/08/10 14:36:24 gdt Exp $
 
-Adapt to ssl usage in python 3.12 and up.
+Upstream disables validating certificates.  In 2025, this is a bug.
+Record how to address the bug, and why that fails.
 
-Taken from:
-  https://github.com/xmpppy/xmpppy/pull/70/
+https://github.com/xmpppy/xmpppy/issues/71
 
-This patch asks python to load the system's configured trust anchors.
-Validation and hostname matching are in theory enabled by default.
-This is a behavior change from upstream, but it is a bugfix.
-See
-  https://github.com/xmpppy/xmpppy/issues/71
-
-
---- xmpp/transports.py.orig	2022-04-10 18:25:44.000000000 +0000
+--- xmpp/transports.py.orig	2025-08-09 12:24:46.000000000 +0000
 +++ xmpp/transports.py
-@@ -383,8 +383,13 @@ class TLS(PlugIn):
-     def _startSSL(self):
-         """ Immidiatedly switch socket to TLS mode. Used internally."""
+@@ -385,6 +385,11 @@ class TLS(PlugIn):
          """ Here we should switch pending_data to hint mode."""
-+        context=ssl.create_default_context()
-+        # Uncomment if you need to work around trust anchor configuration.
-+        #context.check_hostname = False
-+        #context.verify_mode = ssl.CERT_NONE
-+
          tcpsock=self._owner.Connection
--        tcpsock._sslObj    = ssl.wrap_socket(tcpsock._sock, None, None)
-+        tcpsock._sslObj    = context.wrap_socket(tcpsock._sock, server_hostname=self._owner.Server)
-         tcpsock._sslIssuer = tcpsock._sslObj.getpeercert().get('issuer')
-         tcpsock._sslServer = tcpsock._sslObj.getpeercert().get('server')
-         tcpsock._recv = tcpsock._sslObj.read
+         context=ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
++        #pkgsrc:
++        #  - Choose defaults for 2025.
++        #  - https://github.com/xmpppy/xmpppy/issues/71
++        #  - \todo hostname is not set, and thus cannot be checked
++        #  - \todo "ssl.SSLCertVerificationError: (SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1010)'),)"
+         context.check_hostname = False
+         context.verify_mode = ssl.CERT_NONE
+ 
