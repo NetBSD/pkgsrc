100672df1c24ce14f533e796a9b2e94986d3460a
wiz <wiz@pkgsrc.org>
2025-08-10 10:38:51 +0000

    *: recursive bump for imath 3.2.0

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.151 2025/07/13 08:17:39 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.152 2025/08/10 10:38:56 wiz Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.6.5nb69
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.6.5nb70
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../mk/bsd.fast.prefs.mk"
--- math/imath/buildlink3.mk
+++ math/imath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/06/06 08:02:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/08/10 10:38:51 wiz Exp $
 
 BUILDLINK_TREE+=	imath
 
@@ -9,7 +9,7 @@ IMATH_BUILDLINK3_MK:=
 GCC_REQD+=	6
 
 BUILDLINK_API_DEPENDS.imath+=	imath>=3.0.4
-BUILDLINK_ABI_DEPENDS.imath+=	imath>=3.1.9
+BUILDLINK_ABI_DEPENDS.imath+=	imath>=3.2.0
 BUILDLINK_PKGSRCDIR.imath?=	../../math/imath
 .endif	# IMATH_BUILDLINK3_MK
 
