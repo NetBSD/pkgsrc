28aba0170c00dfdaaf1e2fa7c6572b3cd077c774
ryoon <ryoon@pkgsrc.org>
2025-08-10 13:58:34 +0000

    fonts/fontconfig: Update to 2.17.1
    * Use meson as build tool to avoid build failure in fccase.h handling.
    * Disable doc generation completely. It requires docbook2man and docbook2html tool.
      They are not packaged yet.
    
    Changelog:
    2.17.1:
    added (1 change)
    
        meson: Add 'noinstall' to default-hinting, default-sub-pixel-rendering, bitmap-conf build options (merge request)
    
    fixed (1 change)
    
        Fix a heap buffer overflow (merge request)
    
    
    2.17.0:
    
    fixed (13 changes)
    
        Avoid conflict between dgettext macro and declaration in fcint.h (merge request)
        fix: Skip empty entries in XDG_DATA_DIRS parsing by @ComixHe (merge request)
        Fix padding with "und" in pattern elements (merge request)
        Fix a crash with broken cache (merge request)
        meson: Fix additional-fonts-dirs build option that not taking effect (merge request)
        Add default font paths for Android in configure script (merge request)
        Fix use-after-free in FcConfigGetPrgname (merge request)
        conf.d: Add a conf to guess a generic-family for substitution (merge request)
        Drop FcObjectFini() from FcFini() to fix memory leaks (merge request)
        conf.d/65-nonlatin.conf: Rename Lohit Oriya to Lohit Odia (merge request)
        Make sure that the debugging facilities are initialized at loading config phase (merge request)
        meson: don't force installation of a static library by @xry111 (merge request)
        meson: don't try to call run_command for gperf on --wrap-mode=forcefallback (merge request)
    
    changed (10 changes)
    
        Do not fallback decoding with UTF-16BE if no iconv support (merge request)
        Trim trailing newline in string in cache (merge request)
        Parse foundry from OS/2 for table version 0 (merge request)
        Allow dotfiles to scan for caching (merge request)
        Increase a reference count for default FcConfig instance with FcInit() (merge request)
        Free the mutex object only when there are no references to the default FcConfig instance (merge request)
        Free the mutex object only when all cache objects isn't referenced (merge request)
        Drop the configuration path migration code (merge request)
        Drop FcDefaultFini() from FcFini() to fix memory leaks (merge request)
        [Fontations] Process and append font capabilities to Pattern (merge request)
    
    added (6 changes)
    
        Add bitmap-conf build option to choose default bitmap conf (merge request)
        [Fontations] Enable fc-query indexing through Fontations (merge request)
        Add FcConfigPerferAppFont() to allow changing the order of application fonts (merge request)
        [Fontations] Pattern Bindings for CharSet and LangSet (merge request)
        Add FcPatternObjectGet* impl for CharSet and LangSet (merge request)
        [Fontations] Factor out fcpat.c - add Fontations dependencies (merge request)
    
    performance (1 change)
    
        Improve performance of FcPtrListIterInitAtLast (merge request)
    
    
    2.16.2:
    
    changed (1 change)
    
        meson: Use Requires.private instead of Requires (merge request)
    
    fixed (2 changes)
    
        meson: don't force build of a shared library by @kasper93 (merge request)
        meson: do not require libintl if nls feature is disabled (merge request)
    
    added (1 change)
    
        [Fontations] Add internal PatternBuilder abstraction (merge request)
    
    
    2.16.1:
    Not available.

--- fonts/fontconfig/patches/patch-src_fcint.h
+++ fonts/fontconfig/patches/patch-src_fcint.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_fcint.h,v 1.3 2018/03/11 17:54:03 wiz Exp $
+$NetBSD: patch-src_fcint.h,v 1.4 2025/08/10 13:58:35 ryoon Exp $
 
---- src/fcint.h.orig	2018-02-04 10:20:56.000000000 +0000
+--- src/fcint.h.orig	2025-07-02 04:46:34.000000000 +0000
 +++ src/fcint.h
-@@ -76,6 +76,17 @@ extern pfnSHGetFolderPathA pSHGetFolderP
- #define FC_PATH_MAX	128
+@@ -92,6 +92,17 @@ extern pfnSHGetFolderPathA          pSHG
+ #  define FC_PATH_MAX 128
  #endif
  
 +/*
@@ -18,20 +18,20 @@ $NetBSD: patch-src_fcint.h,v 1.3 2018/03/11 17:54:03 wiz Exp $
 +#endif
 +
  #if __GNUC__ >= 4
- #define FC_UNUSED	__attribute__((unused))
+ #  define FC_UNUSED __attribute__ ((unused))
  #else
-@@ -103,7 +114,13 @@ extern pfnSHGetFolderPathA pSHGetFolderP
- #define FC_MAX(a,b) ((a) > (b) ? (a) : (b))
+@@ -123,7 +134,13 @@ extern pfnSHGetFolderPathA          pSHG
+ #define FC_MAX(a, b)                     ((a) > (b) ? (a) : (b))
  
  /* slim_internal.h */
--#if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3)) && defined(__ELF__) && !defined(__sun)
+-#if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3)) && defined(__ELF__) && !defined(__sun) && !defined(BINDGEN_IGNORE_VISIBILITY)
 +/* Bug of GCC 4.2 (visibility and -fPIC)
 +   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=46861 */
 +#if (__GNUC__ == 4 && __GNUC_MINOR__ == 2) && defined(__ELF__) && !defined(__sun)
 +#define FcPrivate
 +#define HAVE_GNUC_ATTRIBUTE 1
 +#include "fcalias.h"
-+#elif (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3)) && defined(__ELF__) && !defined(__sun) && !(__GNUC__ == 4 && __GNUC_MINOR__ == 2)
- #define FcPrivate		__attribute__((__visibility__("hidden")))
- #define HAVE_GNUC_ATTRIBUTE 1
- #include "fcalias.h"
++#elif (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3)) && defined(__ELF__) && !defined(__sun) && !defined(BINDGEN_IGNORE_VISIBILITY)
+ #  define FcPrivate           __attribute__ ((__visibility__ ("hidden")))
+ #  define HAVE_GNUC_ATTRIBUTE 1
+ #  include "fcalias.h"
--- fonts/fontconfig/patches/patch-src_fcstat.c
+++ fonts/fontconfig/patches/patch-src_fcstat.c
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_fcstat.c,v 1.7 2025/03/30 21:10:42 wiz Exp $
+$NetBSD: patch-src_fcstat.c,v 1.8 2025/08/10 13:58:35 ryoon Exp $
 
 The argument types to scandir(3) differ between different OSes.
 I used the provided alphasort instead of rewriting the equivalent
 sorter function.
 
---- src/fcstat.c.orig	2016-04-06 03:22:55.000000000 +0000
+--- src/fcstat.c.orig	2025-07-02 04:46:34.000000000 +0000
 +++ src/fcstat.c
-@@ -52,6 +52,9 @@ typedef long long INT64;
- #define EPOCH_OFFSET 11644473600i64
- typedef __int64 INT64;
+@@ -337,6 +337,9 @@ FcStatChecksum (const FcChar8 *file, str
+ 	    return -1;
+     }
  #endif
 +#if defined(_SCO_DS)
 +#include "../scandir.c"
 +#endif
  
- /* Workaround for problems in the stat() in the Microsoft C library:
-  *
+     return 0;
+ }
