5756cec29826e61f0269b5bdd563b174693f2703
wiz <wiz@pkgsrc.org>
2025-08-11 08:37:06 +0000

    libclucene: fix pkglint

--- textproc/libclucene/buildlink3.mk
+++ textproc/libclucene/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2024/12/29 15:09:59 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2025/08/11 08:37:06 wiz Exp $
 
 BUILDLINK_TREE+=	libclucene
 
@@ -8,9 +8,10 @@ LIBCLUCENE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libclucene+=	libclucene>=2.2.0
 BUILDLINK_ABI_DEPENDS.libclucene+=	libclucene>=2.3.3.4nb27
 BUILDLINK_PKGSRCDIR.libclucene?=	../../textproc/libclucene
-.endif # LIBCLUCENE_BUILDLINK3_MK
 
 # boost-libs sets GCC_REQD, so we need to ensure we are in sync otherwise
 # we may end up with the wrong libstdc++ runtime.
 .include "../../devel/boost-libs/buildlink3.mk"
+.endif # LIBCLUCENE_BUILDLINK3_MK
+
 BUILDLINK_TREE+=	-libclucene
