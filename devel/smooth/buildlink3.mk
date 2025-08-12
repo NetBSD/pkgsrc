7aa4cfbf00790f866a584cabbdc8dda62cff67b2
ryoon <ryoon@pkgsrc.org>
2025-08-12 00:54:26 +0000

    devel/smooth: import smooth-0.9.10
    smooth is an object oriented C++ class library for Windows, macOS,
    Linux and most Unix-like operating systems. It provides basic
    functionality and platform support for applications and libraries.
    
    Features provided by smooth include:
    
        a user interface API with various widgets
        a simple to use multithreading API
        a file and network IO interface
        completely transparent Unicode and software internationalization support
        a libxml2 based XML parser

---
+++ devel/smooth/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/08/12 00:54:26 ryoon Exp $
+
+BUILDLINK_TREE+=	smooth
+
+.if !defined(SMOOTH_BUILDLINK3_MK)
+SMOOTH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.smooth+=	smooth>=0.9.10
+BUILDLINK_PKGSRCDIR.smooth?=	../../devel/smooth
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../converters/fribidi/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/libwebp/buildlink3.mk"
+.include "../../sysutils/libcpuid/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# SMOOTH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-smooth
