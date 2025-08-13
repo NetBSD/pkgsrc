00d2ca438fb92045f13cde743f64066693bcf61a
pho <pho@pkgsrc.org>
2025-08-13 10:56:52 +0000

    lang/ghc910: Fix a long-standing bug in Cabal regarding rpaths

--- lang/ghc910/buildlink3.mk
+++ lang/ghc910/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2025/03/05 03:33:59 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/08/13 10:56:52 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.10.1nb3
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.10.1nb3
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=9.10.1nb4
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=9.10.1nb4
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc910
 
 .include "../../converters/libiconv/buildlink3.mk"
