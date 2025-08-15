325cf506046d28e438b9816b33c9114d54099781
adam <adam@pkgsrc.org>
2025-08-15 07:07:11 +0000

    ldns drill: updated to 1.8.4
    1.8.4   2024-07-19
    * Fix building documentation in build directory.
      Thanks Michael Tokarev
    * Updated the configure script for split openssl directories.
    * Fix for windows compile; asctime_r compat, search for inet_ntop
      in ws2_32, avoid double decl of inet_ntop, check for symlink call,
      include config.h in edns.c, have socket compare use correct
      signedness, add -u repo option to makewin.sh, fix quoting in
      makewin.sh and check return value of bind and connect in net.c
      for failure instead of a socket type. And fix to install .exe files.
    * Fix to compile with OpenSSL 3.2. Update acx_nlnetlabs.m4 to 48.
    * Fix to compile DLLs, and pick out of the lib64 dir, and include
      libssp-0.dll and ldns-config.
    * bugfix: Resolver uses nameserver commented out in
      /etc/resolv.conf. Thanks grembo.
    * Added RESINFO rrtype.
    * Added WALLET rrtype.
    * Fix coverity cited problems. Thanks Enji Cooper.
    * Several Python SWIG issues. Thanks Florian Weimer.
    * const ldns_rdf * typemap fixes to support swig-4.2.0
      Thanks William S Fulton
    * keys.c: fix build with libressl >= 3.8.1
      Thanks Fabrice Fontaine
    * Portable executable progs don't underlink to libldns
      Thanks Ørjan Malde
    * Update declaration for function with no argument
      Thanks Ed Maste
    * fix the build in FreeBSD's base system
      Thanks Pierre Pronchery and Dag-Erling Smørgrav
    * improve 'next-label' algorithm in ldns-walk
      Thanks Jelte Jansen
    * Add functions to extract RFC 8914 extended errors
      Thanks Roland van Rijswijk-Deij
    * Expand CI: Cygwin, BSDs, musl libc, 32-bit, and big-endian
      Thanks Felipe Gasper
    * 16-unit-tests-edns: Fix leak & warnings
      Thanks Felipe Gasper
    * Added NXNAME rrtype.
    * Openssl no engine improved support

--- net/ldns/buildlink3.mk
+++ net/ldns/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2022/06/28 11:35:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2025/08/15 07:07:11 adam Exp $
 
 BUILDLINK_TREE+=	ldns
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ldns
 LDNS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ldns+=	ldns>=1.4
-BUILDLINK_ABI_DEPENDS.ldns?=	ldns>=1.8.0nb1
+BUILDLINK_ABI_DEPENDS.ldns+=	ldns>=1.8.4
 BUILDLINK_PKGSRCDIR.ldns?=	../../net/ldns
 .endif # LDNS_BUILDLINK3_MK
 
--- net/ldns/version.mk
+++ net/ldns/version.mk
@@ -1,7 +1,12 @@
-# $NetBSD: version.mk,v 1.1 2024/02/26 20:11:44 he Exp $
+# $NetBSD: version.mk,v 1.2 2025/08/15 07:07:11 adam Exp $
+# used by net/ldns/Makefile
+# used by net/drill/Makefile
 
-# used by
-#   ../../net/ldns/Makefile
-#   ../../net/drill/Makefile
+LDNS_VERSION=	1.8.4
 
-LDNS_VERSION=1.8.3
+DISTNAME=	ldns-${LDNS_VERSION}
+CATEGORIES=	net
+MASTER_SITES=	https://www.nlnetlabs.nl/downloads/ldns/
+
+DISTINFO_FILE=	${.CURDIR}/../../net/ldns/distinfo
+PATCHDIR=	${.CURDIR}/../../net/ldns/patches
