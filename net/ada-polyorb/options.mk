ce8b5e670186e454793af816861d0ca4829afd58
dkazankov <dkazankov@pkgsrc.org>
2025-08-15 09:06:10 +0000

    polyorb: fix specific option names

--- net/ada-polyorb/options.mk
+++ net/ada-polyorb/options.mk
@@ -1,15 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2025/08/15 08:10:03 dkazankov Exp $
+# $NetBSD: options.mk,v 1.2 2025/08/15 09:06:10 dkazankov Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.polyorb
-PKG_SUPPORTED_OPTIONS=		cs-event cs-ir cs-naming cs-notification cs-time \
-				gnatcoll openssl
-PKG_OPTIONS_NONEMPTY_SETS=	ap pp
-PKG_OPTIONS_SET.ap=		ap-corba ap-dsa ap-moma ap-aws
-PKG_OPTIONS_SET.pp=		pp-giop pp-soap pp-srp pp-dns
-PKG_SUGGESTED_OPTIONS=		ap-corba ap-dsa ap-moma \
-				pp-giop pp-soap pp-srp pp-dns \
-				cs-event cs-ir cs-naming cs-notification cs-time \
-				gnatcoll openssl
+PKG_SUPPORTED_OPTIONS=		polyorb-cs-event polyorb-cs-ir polyorb-cs-naming \
+				polyorb-cs-notification polyorb-cs-time \
+				polyorb-gnatcoll openssl
+PKG_OPTIONS_NONEMPTY_SETS=	polyorb-ap polyorb-pp
+PKG_OPTIONS_SET.polyorb-ap=	polyorb-ap-corba polyorb-ap-dsa polyorb-ap-moma polyorb-ap-aws
+PKG_OPTIONS_SET.polyorb-pp=	polyorb-pp-giop polyorb-pp-soap polyorb-pp-srp polyorb-pp-dns
+PKG_SUGGESTED_OPTIONS=		polyorb-ap-corba polyorb-ap-dsa polyorb-ap-moma \
+				polyorb-pp-giop polyorb-pp-soap polyorb-pp-srp polyorb-pp-dns \
+				polyorb-cs-event polyorb-cs-ir polyorb-cs-naming \
+				polyorb-cs-notification polyorb-cs-time \
+				polyorb-gnatcoll openssl
 
 .include "../../mk/bsd.fast.prefs.mk"
 .include "../../mk/bsd.options.mk"
@@ -19,54 +21,54 @@ PKG_SUGGESTED_OPTIONS=		ap-corba ap-dsa ap-moma \
 CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
 .endif
 
-.if !empty(PKG_OPTIONS:Mgnatcoll)
+.if !empty(PKG_OPTIONS:Mpolyorb-gnatcoll)
 .include "../../textproc/ada-xmlada-25/buildlink3.mk"
 .include "../../devel/ada-libgpr-25/buildlink3.mk"
 .include "../../devel/ada-gnatcoll-core-25/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gnatcoll
 .endif
 
-.if !empty(PKG_OPTIONS:Map-aws)
+.if !empty(PKG_OPTIONS:Mpolyorb-ap-aws)
 # AWS personality is not currently compiling
 #APP_PERSONALITIES+=	aws
 #.include "../../textproc/ada-xmlada-25/buildlink3.mk"
 .endif
-.if !empty(PKG_OPTIONS:Map-corba)
+.if !empty(PKG_OPTIONS:Mpolyorb-ap-corba)
 APP_PERSONALITIES+=	corba
 .endif
-.if !empty(PKG_OPTIONS:Map-dsa)
+.if !empty(PKG_OPTIONS:Mpolyorb-ap-dsa)
 APP_PERSONALITIES+=	dsa
 .endif
-.if !empty(PKG_OPTIONS:Map-moma)
+.if !empty(PKG_OPTIONS:Mpolyorb-ap-moma)
 APP_PERSONALITIES+=	moma
 .endif
 
-.if !empty(PKG_OPTIONS:Mpp-dns)
+.if !empty(PKG_OPTIONS:Mpolyorb-pp-dns)
 PROTOCOL_PERSONALITIES+=	dns
 .endif
-.if !empty(PKG_OPTIONS:Mpp-giop)
+.if !empty(PKG_OPTIONS:Mpolyorb-pp-giop)
 PROTOCOL_PERSONALITIES+=	giop
 .endif
-.if !empty(PKG_OPTIONS:Mpp-soap)
+.if !empty(PKG_OPTIONS:Mpolyorb-pp-soap)
 PROTOCOL_PERSONALITIES+=	soap
 .include "../../textproc/ada-xmlada-25/buildlink3.mk"
 .endif
-.if !empty(PKG_OPTIONS:Mpp-srp)
+.if !empty(PKG_OPTIONS:Mpolyorb-pp-srp)
 PROTOCOL_PERSONALITIES+=	srp
 .endif
 
-.if !empty(PKG_OPTIONS:Mcs-event)
+.if !empty(PKG_OPTIONS:Mpolyorb-cs-event)
 CORBA_SERVICES+=	event
 .endif
-.if !empty(PKG_OPTIONS:Mcs-ir)
+.if !empty(PKG_OPTIONS:Mpolyorb-cs-ir)
 CORBA_SERVICES+=	ir
 .endif
-.if !empty(PKG_OPTIONS:Mcs-naming)
+.if !empty(PKG_OPTIONS:Mpolyorb-cs-naming)
 CORBA_SERVICES+=	naming
 .endif
-.if !empty(PKG_OPTIONS:Mcs-notification)
+.if !empty(PKG_OPTIONS:Mpolyorb-cs-notification)
 CORBA_SERVICES+=	notification
 .endif
-.if !empty(PKG_OPTIONS:Mcs-time)
+.if !empty(PKG_OPTIONS:Mpolyorb-cs-time)
 CORBA_SERVICES+=	time
 .endif
