35291531932d574f58d9886e8874547396fb09b8
triaxx <triaxx@pkgsrc.org>
2025-08-16 20:55:51 +0000

    KoboDeluxe: Fix installation on Darwin
    pkgsrc changes:
    ---------------
      * Add missing patch comments as recommended by pkglint.
      * Add dedicated PLIST.bundle for Darwin that list files of macOS bundle.
      * Add patches to correctly install GFX and SFX files into the bundle.

--- games/KoboDeluxe/patches/patch-graphics_window.cpp
+++ games/KoboDeluxe/patches/patch-graphics_window.cpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-graphics_window.cpp,v 1.1 2020/03/18 17:54:54 joerg Exp $
+$NetBSD: patch-graphics_window.cpp,v 1.2 2025/08/16 20:55:51 triaxx Exp $
+
+Fix invalide pointer substraction since pointers was of different types.
 
 --- graphics/window.cpp.orig	2020-03-18 13:31:58.788125295 +0000
 +++ graphics/window.cpp
