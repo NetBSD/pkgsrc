3ee48ed9b47aa0d29219f3b06ca603dedb32e421
wiz <wiz@pkgsrc.org>
2025-08-17 21:05:42 +0000

    cups-filters: restore buildlink3.mk
    still used by print/cups and print/p5-Net-CUPS

---
+++ print/cups-filters/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.35 2025/08/17 21:05:42 wiz Exp $
+
+BUILDLINK_TREE+=	cups-filters
+
+.if !defined(CUPS_FILTERS_BUILDLINK3_MK)
+CUPS_FILTERS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cups-filters+=	cups-filters>=1.8.2
+BUILDLINK_ABI_DEPENDS.cups-filters+=	cups-filters>=1.28.16nb20
+BUILDLINK_PKGSRCDIR.cups-filters?=	../../print/cups-filters
+
+.include "../../print/cups-base/buildlink3.mk"
+.endif # CUPS_FILTERS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cups-filters
