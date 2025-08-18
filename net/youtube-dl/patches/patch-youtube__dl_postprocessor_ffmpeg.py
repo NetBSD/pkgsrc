a11adb928d5587a0ff42f7472460ab60420c0565
leot <leot@pkgsrc.org>
2025-08-18 09:15:34 +0000

    youtube-dl: switch to more recent ffmpeg-s
    Look up for ffmpeg[765] instead of older ffmpeg-s that are likely not
    installed (or in case of ffmpeg2 no longer in pkgsrc!).
    
    PKGREVISION++

--- net/youtube-dl/patches/patch-youtube__dl_postprocessor_ffmpeg.py
+++ net/youtube-dl/patches/patch-youtube__dl_postprocessor_ffmpeg.py
@@ -1,6 +1,6 @@
-$NetBSD: patch-youtube__dl_postprocessor_ffmpeg.py,v 1.6 2022/05/11 17:58:35 jklos Exp $
+$NetBSD: patch-youtube__dl_postprocessor_ffmpeg.py,v 1.7 2025/08/18 09:15:35 leot Exp $
 
-Also look and use ffmpeg[234]/ffprobe[234] if possible, preferring
+Also look and use ffmpeg[765]/ffprobe[765] if possible, preferring
 the unversioned one (i.e. selected via alternatives framework)
 or the newest one.
 
@@ -11,7 +11,7 @@ or the newest one.
  
      def _determine_executables(self):
 -        programs = ['avprobe', 'avconv', 'ffmpeg', 'ffprobe']
-+        programs = ['avprobe', 'avconv', 'ffmpeg', 'ffmpeg5', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2', 'ffprobe', 'ffprobe5', 'ffprobe4', 'ffprobe3', 'ffprobe2']
++        programs = ['avprobe', 'avconv', 'ffmpeg', 'ffmpeg7', 'ffmpeg6', 'ffmpeg5', 'ffprobe', 'ffprobe7', 'ffprobe6', 'ffprobe5']
          prefer_ffmpeg = True
  
          def get_ffmpeg_version(path):
@@ -20,7 +20,7 @@ or the newest one.
                          return None
                      location = os.path.dirname(os.path.abspath(location))
 -                    if basename in ('ffmpeg', 'ffprobe'):
-+                    if basename in ('ffmpeg', 'ffmpeg5', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2', 'ffprobe', 'ffprobe5', 'ffprobe4', 'ffprobe3', 'ffprobe2'):
++                    if basename in ('ffmpeg', 'ffmpeg7', 'ffmpeg6', 'ffmpeg5', 'ffprobe', 'ffprobe7', 'ffprobe6', 'ffprobe5'):
                          prefer_ffmpeg = True
  
                  self._paths = dict(
@@ -29,10 +29,10 @@ or the newest one.
  
          if prefer_ffmpeg is False:
 -            prefs = ('avconv', 'ffmpeg')
-+            prefs = ('avconv', 'ffmpeg', 'ffmpeg5', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2')
++            prefs = ('avconv', 'ffmpeg', 'ffmpeg7', 'ffmpeg6', 'ffmpeg5')
          else:
 -            prefs = ('ffmpeg', 'avconv')
-+            prefs = ('ffmpeg', 'ffmpeg4', 'ffmpeg5', 'ffmpeg3', 'ffmpeg2', 'avconv')
++            prefs = ('ffmpeg', 'ffmpeg7', 'ffmpeg6', 'ffmpeg5', 'avconv')
          for p in prefs:
              if self._versions[p]:
                  self.basename = p
@@ -40,10 +40,10 @@ or the newest one.
  
          if prefer_ffmpeg is False:
 -            prefs = ('avprobe', 'ffprobe')
-+            prefs = ('avprobe', 'ffprobe', 'ffprobe5', 'ffprobe4', 'ffprobe3', 'ffprobe2')
++            prefs = ('avprobe', 'ffprobe', 'ffprobe7', 'ffprobe6', 'ffprobe5')
          else:
 -            prefs = ('ffprobe', 'avprobe')
-+            prefs = ('ffprobe', 'ffprobe5', 'ffprobe4', 'ffprobe3', 'ffprobe2', 'avprobe')
++            prefs = ('ffprobe', 'ffprobe7', 'ffprobe6', 'ffprobe5', 'avprobe')
          for p in prefs:
              if self._versions[p]:
                  self.probe_basename = p
