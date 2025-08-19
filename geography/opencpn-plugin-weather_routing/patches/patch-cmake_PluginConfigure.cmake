5352781f62f9d5c80c80fbc66fcdea41eba37b10
wiz <wiz@pkgsrc.org>
2025-08-19 13:15:38 +0000

    opencpn-plugin-weather_routing: fix build with cmake 4
    Convert to cmake/build.mk

--- geography/opencpn-plugin-weather_routing/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-weather_routing/patches/patch-cmake_PluginConfigure.cmake
@@ -1,9 +1,26 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.2 2017/08/24 13:01:33 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.3 2025/08/19 13:15:38 wiz Exp $
+
+Fix build with cmake 4.
+
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
---- cmake/PluginConfigure.cmake.orig	2017-04-01 02:46:48.000000000 +0200
-+++ cmake/PluginConfigure.cmake	2017-08-24 11:54:54.830898473 +0200
-@@ -44,9 +44,9 @@
+--- cmake/PluginConfigure.cmake.orig	2018-06-21 21:28:17.000000000 +0000
++++ cmake/PluginConfigure.cmake
+@@ -6,13 +6,6 @@
+ 
+ SET(PLUGIN_SOURCE_DIR .)
+ 
+-# This should be 2.8.0 to have FindGTK2 module
+-IF (COMMAND cmake_policy)
+-  CMAKE_POLICY(SET CMP0003 OLD)
+-  CMAKE_POLICY(SET CMP0005 OLD)
+-  CMAKE_POLICY(SET CMP0011 OLD)
+-ENDIF (COMMAND cmake_policy)
+-
+ MESSAGE (STATUS "*** Staging to build ${PACKAGE_NAME} ***")
+ 
+ configure_file(cmake/version.h.in ${PROJECT_SOURCE_DIR}/src/version.h)
+@@ -40,9 +33,9 @@ IF(NOT MSVC)
   ENDIF(PROFILING)
  
   IF(NOT APPLE)
