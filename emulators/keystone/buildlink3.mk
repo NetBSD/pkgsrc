893a52cafa607bd6ea3c1abceed41b3efb6781e1
wiz <wiz@pkgsrc.org>
2025-08-19 10:15:19 +0000

    keystone: fix build with cmake 4

--- emulators/keystone/buildlink3.mk
+++ emulators/keystone/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/08/14 05:24:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/08/19 10:15:19 wiz Exp $
 
 BUILDLINK_TREE+=	keystone
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	keystone
 KEYSTONE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.keystone+=	keystone>=0.9
-BUILDLINK_ABI_DEPENDS.keystone?=		keystone>=0.9.2nb2
+BUILDLINK_ABI_DEPENDS.keystone?=	keystone>=0.9.2nb2
 BUILDLINK_PKGSRCDIR.keystone?=		../../emulators/keystone
 .endif	# KEYSTONE_BUILDLINK3_MK
 
