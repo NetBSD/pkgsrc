06bfc08aeacdef45ae0cba9c242a3d285ce2c037
wiz <wiz@pkgsrc.org>
2025-08-19 13:47:35 +0000

    neko: fix build with cmake
    Convert to cmake/build.mk
    
    While here, fix path to itself in buildlink3.mk file.

--- lang/neko/buildlink3.mk
+++ lang/neko/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/24 13:23:46 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/08/19 13:47:35 wiz Exp $
 
 BUILDLINK_TREE+=	neko
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	neko
 NEKO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.neko+=	neko>=2.3.0
-BUILDLINK_PKGSRCDIR.neko?=	../../mywip/neko
+BUILDLINK_PKGSRCDIR.neko?=	../../lang/neko
 .endif	# NEKO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-neko
