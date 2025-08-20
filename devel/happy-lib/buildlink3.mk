93ac49acc28d19a494e8008383a15735eac32070
wiz <wiz@pkgsrc.org>
2025-08-20 19:22:52 +0000

    devel/happy-lib: update to happy-lib-2.1.7
    ## 2.1.7
    
    * Add support for `{-# OPTIONS_HAPPY ... #-}` pragmas
      ([#342](https://github.com/haskell/happy/issues/342)).
    
    * Tested with GHC 8.0 - 9.12.2.
      The Haskell code generated by Happy is for GHC 8.0 and up.

--- devel/happy-lib/buildlink3.mk
+++ devel/happy-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2025/08/13 11:06:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2025/08/20 19:22:52 wiz Exp $
 
 BUILDLINK_TREE+=	happy-lib
 
 .if !defined(HAPPY_LIB_BUILDLINK3_MK)
 HAPPY_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.6
-BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.6nb2
+BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.7
+BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.7
 BUILDLINK_PKGSRCDIR.happy-lib?=		../../devel/happy-lib
 .endif	# HAPPY_LIB_BUILDLINK3_MK
 
