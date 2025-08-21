08b124339848350a5d5879401c945c6eb44040d8
pho <pho@pkgsrc.org>
2025-08-21 02:50:14 +0000

    lang/openjdk21: Fix distinfo that I broke in my previous commit
    I forgot to upload the new bootkit to LOCAL_PORTS, and also the previous
    distinfo had a wrong version of the intended bootkit. I fixed them both,
    and now the package should be buildable again. Sorry for the breakage.

--- lang/openjdk21/bootstrap.mk
+++ lang/openjdk21/bootstrap.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bootstrap.mk,v 1.3 2025/08/11 18:06:32 pho Exp $
+# $NetBSD: bootstrap.mk,v 1.4 2025/08/21 02:50:14 pho Exp $
 
 .if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 090000
 PKG_FAIL_REASON+=		"Only supports NetBSD >= 9"
@@ -25,11 +25,11 @@ EXTRACT_ONLY+=			${BOOT.nb9-amd64}
 .endif
 
 ONLY_FOR_PLATFORM+=		NetBSD-*-aarch64
-BOOT.nb10-aarch64=		bootstrap-jdk-1.21.0.7.6-netbsd-10-aarch64-20250811.tar.xz
-SITES.${BOOT.nb10-aarch64}=	${MASTER_SITE_LOCAL:=openjdk21/}
+BOOT.nb9-aarch64=		bootstrap-jdk-1.21.0.7.6-netbsd-9-aarch64-20250811.tar.xz
+SITES.${BOOT.nb9-aarch64}=	${MASTER_SITE_LOCAL:=openjdk21/}
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-aarch64) || make(distinfo)
-DISTFILES+=			${BOOT.nb10-aarch64}
-EXTRACT_ONLY+=			${BOOT.nb10-aarch64}
+DISTFILES+=			${BOOT.nb9-aarch64}
+EXTRACT_ONLY+=			${BOOT.nb9-aarch64}
 .endif
 
 #ONLY_FOR_PLATFORM+=		NetBSD-*-earmv[67]hf
