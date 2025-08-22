cbe098d2c0dc59fa41d1d18121794e45d49e3907
adam <adam@pkgsrc.org>
2025-08-22 08:15:51 +0000

    revbump for poppler

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.116 2025/08/10 10:38:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.117 2025/08/22 08:15:52 adam Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2nb7
-BUILDLINK_ABI_DEPENDS.simgear+=	simgear>=2020.3.11nb57
+BUILDLINK_ABI_DEPENDS.simgear+=	simgear>=2020.3.11nb58
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
