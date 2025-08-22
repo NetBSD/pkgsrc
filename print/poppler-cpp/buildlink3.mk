a22b9cb4031c190f041fc88dc1c9734517f3622e
adam <adam@pkgsrc.org>
2025-08-22 11:08:17 +0000

    poppler: bump ABI_DEPENDS

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.76 2025/07/13 08:17:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.77 2025/08/22 11:08:17 adam Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=25.07.0nb1
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=25.08.0
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
