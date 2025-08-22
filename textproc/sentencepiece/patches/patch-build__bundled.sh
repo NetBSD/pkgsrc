3e16c62bff703f0c85efe33596a1c304a838b8bc
wiz <wiz@pkgsrc.org>
2025-08-22 14:31:48 +0000

    py-sentencepiece: fix build with cmake 4

---
+++ textproc/sentencepiece/patches/patch-build__bundled.sh
@@ -0,0 +1,13 @@
+$NetBSD: patch-build__bundled.sh,v 1.1 2025/08/22 14:31:49 wiz Exp $
+
+Fix build with cmake 4.
+
+--- build_bundled.sh.orig	2024-02-19 08:06:52.000000000 +0000
++++ build_bundled.sh
+@@ -18,5 +18,5 @@ else
+   SRC_DIR=./sentencepiece
+ fi
+ 
+-cmake ${SRC_DIR} -B ${BUILD_DIR} -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
++cmake ${SRC_DIR} -B ${BUILD_DIR} -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+ cmake --build ${BUILD_DIR} --config Release --target install --parallel $(nproc)
