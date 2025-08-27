b6c5c201ad12e9b076883b1ac10812403848d1a8
adam <adam@pkgsrc.org>
2025-08-27 06:27:03 +0000

    qemu qemu-guest-agent: updated to 10.1.0
    10.1.0
    https://wiki.qemu.org/ChangeLog/10.1

--- emulators/qemu/patches/patch-target_i386_cpu.c
+++ emulators/qemu/patches/patch-target_i386_cpu.c
@@ -1,27 +1,10 @@
-$NetBSD: patch-target_i386_cpu.c,v 1.5 2025/04/25 11:24:29 adam Exp $
+$NetBSD: patch-target_i386_cpu.c,v 1.6 2025/08/27 06:27:03 adam Exp $
 
 Permit to use `-cpu host' with NVMM.
 
---- target/i386/cpu.c.orig	2025-04-22 20:26:11.000000000 +0000
+--- target/i386/cpu.c.orig	2025-08-26 18:32:38.000000000 +0000
 +++ target/i386/cpu.c
-@@ -26,6 +26,7 @@
- #include "tcg/helper-tcg.h"
- #include "exec/translation-block.h"
- #include "system/hvf.h"
-+#include "system/nvmm.h"
- #include "hvf/hvf-i386.h"
- #include "kvm/kvm_i386.h"
- #include "sev.h"
-@@ -1899,7 +1900,7 @@ uint32_t xsave_area_size(uint64_t mask,
- 
- static inline bool accel_uses_host_cpuid(void)
- {
--    return kvm_enabled() || hvf_enabled();
-+    return kvm_enabled() || hvf_enabled() || nvmm_enabled();
- }
- 
- static inline uint64_t x86_cpu_xsave_xcr0_components(X86CPU *cpu)
-@@ -8190,7 +8191,8 @@ static void x86_cpu_realizefn(DeviceStat
+@@ -9326,7 +9326,8 @@ static void x86_cpu_realizefn(DeviceStat
  
      if (xcc->host_cpuid_required && !accel_uses_host_cpuid()) {
          g_autofree char *name = x86_cpu_class_get_model_name(xcc);
