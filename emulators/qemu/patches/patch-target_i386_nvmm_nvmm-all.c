22c792c0d5f97ce21861de47a68bf09e597c867d
ryoon <ryoon@pkgsrc.org>
2025-08-27 10:38:31 +0000

    emulators/qemu: Fix build for NetBSD/nvmm
    * My Windows 10 VM still works.
    * And Remove meson.build from distinfo.

--- emulators/qemu/patches/patch-target_i386_nvmm_nvmm-all.c
+++ emulators/qemu/patches/patch-target_i386_nvmm_nvmm-all.c
@@ -1,13 +1,22 @@
-$NetBSD: patch-target_i386_nvmm_nvmm-all.c,v 1.10 2025/03/10 13:46:26 wiz Exp $
+$NetBSD: patch-target_i386_nvmm_nvmm-all.c,v 1.11 2025/08/27 10:38:31 ryoon Exp $
 
 emulators/qemu: Fix NVMM for UEFI (OVMF)
 
-* From DragonFly BSD's Redmine's issue 3110 to fix memory error
+* From DragonFly BSD's Redmine's issue 3310 to fix memory error
   when UEFI boot.
 
---- target/i386/nvmm/nvmm-all.c.orig	2024-11-20 22:48:05.000000000 +0000
+--- target/i386/nvmm/nvmm-all.c.orig	2025-08-26 18:32:38.000000000 +0000
 +++ target/i386/nvmm/nvmm-all.c
-@@ -1057,7 +1057,11 @@ nvmm_process_section(MemoryRegionSection
+@@ -984,7 +984,7 @@ nvmm_init_vcpu(CPUState *cpu)
+         }
+     }
+ 
+-    qcpu->vcpu_dirty = true;
++    cpu->vcpu_dirty = true;
+     cpu->accel = qcpu;
+ 
+     return 0;
+@@ -1059,7 +1059,11 @@ nvmm_process_section(MemoryRegionSection
      unsigned int delta;
      uintptr_t hva;
  
