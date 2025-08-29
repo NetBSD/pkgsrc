1496bae5f1e2c7b0a509d9359e7f27152594c624
bsiegert <bsiegert@pkgsrc.org>
2025-08-29 18:50:09 +0000

    esbuild: update to 0.25.9
    0.25.9
    - Better support building projects that use Yarn on Windows
    - Preserve parentheses around function expressions
    
    v0.25.8
    - Fix another TypeScript parsing edge case
    - Fix a regression with the parsing of source phase imports
    
    v0.25.7
    - Parse and print JavaScript imports with an explicit phase
    - Support optionally emitting absolute paths instead of relative paths
    - Fix a TypeScript parsing edge case
    - Inline small constant strings when minifying
    
    v0.25.6
    - Fix a memory leak when cancel() is used on a build context
    - Support empty :is() and :where() syntax in CSS
    - Improve tree-shaking of try statements in dead code
    - Consider negated bigints to have no side effects
    - Support a configurable delay in watch mode before rebuilding
    - Allow mixed array for entryPoints API option

--- www/esbuild/go-modules.mk
+++ www/esbuild/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.4 2025/05/27 18:34:46 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.5 2025/08/29 18:50:09 bsiegert Exp $
 
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
