b0c3e7c52f1e6535cc05e97dde2bc434ae6a3a4f
leot <leot@pkgsrc.org>
2025-08-29 12:17:31 +0000

    the_silver_searcher: address ctype(3) abuses
    Address all ctype(3) abuses.
    
    PKGREVISION++
    
    Reported by <abs> via current-users@ ML and correctly guessed by
    <martin>.
    Thanks David and Martin!

--- textproc/the_silver_searcher/patches/patch-src_main.c
+++ textproc/the_silver_searcher/patches/patch-src_main.c
@@ -1,10 +1,20 @@
-$NetBSD: patch-src_main.c,v 1.2 2024/04/02 01:28:00 charlotte Exp $
+$NetBSD: patch-src_main.c,v 1.3 2025/08/29 12:17:31 leot Exp $
 
-DragonFly also has a particular 'cpuset' type.
+- Fix ctype(3) abuses
+- DragonFly also has a particular 'cpuset' type
 
---- src/main.c.orig	2022-05-23 22:57:49.749747000 -0700
-+++ src/main.c	2022-05-23 22:58:26.439723000 -0700
-@@ -156,7 +156,7 @@
+--- src/main.c.orig	2018-08-07 06:20:25.000000000 +0000
++++ src/main.c
+@@ -117,7 +117,7 @@ int main(int argc, char **argv) {
+             /* Search routine needs the query to be lowercase */
+             char *c = opts.query;
+             for (; *c != '\0'; ++c) {
+-                *c = (char)tolower(*c);
++                *c = (char)tolower((unsigned char)*c);
+             }
+         }
+         generate_alpha_skip(opts.query, opts.query_len, alpha_skip_lookup, opts.casing == CASE_SENSITIVE);
+@@ -156,7 +156,7 @@ int main(int argc, char **argv) {
              if (opts.use_thread_affinity) {
  #ifdef __linux__
                  cpu_set_t cpu_set;
