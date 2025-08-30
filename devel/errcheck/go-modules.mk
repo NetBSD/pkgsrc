6a0c7d1e0269b63bf0b9f201f3a6da10538196c0
bsiegert <bsiegert@pkgsrc.org>
2025-08-30 09:54:27 +0000

    errcheck: update to 1.9.0
    This fixes the build with Go 1.25.
    
    1.9.0
    - Added supported for Go 1.24
    - Dropped support for Go 1.18 - 1.21 due to updated golang.org/x/tools
    - Bump GitHub action workflows
    - Remove no longer relevant fmtTags
    - Deprecate ErrNoGoFiles as it's never returned
    
    1.8.0
    - Add more check for type assert
    - Support GODEBUG=gotypesalias=1
    - Exclude maphash methods that "never fail"

--- devel/errcheck/go-modules.mk
+++ devel/errcheck/go-modules.mk
@@ -1,6 +1,8 @@
-# $NetBSD: go-modules.mk,v 1.4 2025/02/11 09:39:24 wiz Exp $
+# $NetBSD: go-modules.mk,v 1.5 2025/08/30 09:54:27 bsiegert Exp $
 
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.14.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.14.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.17.0.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.17.0.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.23.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.23.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.11.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.11.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.30.0.zip
