fc6e60ee6d0a4d1a42f1fc60fd93d02187105038
pho <pho@pkgsrc.org>
2025-08-30 08:48:46 +0000

    misc/gkrellsun: Fix a use-after-free bug

--- misc/gkrellsun/patches/patch-src20_gkrellsun.c
+++ misc/gkrellsun/patches/patch-src20_gkrellsun.c
@@ -1,9 +1,14 @@
-$NetBSD: patch-src20_gkrellsun.c,v 1.2 2023/08/23 06:29:53 pho Exp $
+$NetBSD: patch-src20_gkrellsun.c,v 1.3 2025/08/30 08:48:46 pho Exp $
 
 * Avoid "sun" conflict.
 
 * Fix a build breakage with glib2-2.76. No idea why it has a declaration
   here. It conflicts with a macro in <glib-2.0/glib/gstring.h>.
+
+* Uncomment a mysteriously commented line setting textOptions.fontDesc to
+  NULL in cb_plugin_disabled(). NULL-ifying it is very important, otherwise
+  gkrellsun will crash for use-after-free of PangoFontDescription* when the
+  plugin is enabled, disabled, then enabled again.
 
 --- src20/gkrellsun.c.orig	2006-03-17 13:29:51.000000000 +0000
 +++ src20/gkrellsun.c
@@ -20,3 +25,12 @@ $NetBSD: patch-src20_gkrellsun.c,v 1.2 2023/08/23 06:29:53 pho Exp $
  extern void pango_font_description_free (/*@owned@*/PangoFontDescription *);
  
  #define PLUGIN_HEIGHT 54
+@@ -1160,7 +1163,7 @@ cb_plugin_disabled()
+    }
+    colorsCreated = FALSE;
+    colormap = NULL;
+-/*   textOptions.fontDesc = NULL;*/
++   textOptions.fontDesc = NULL;
+ }
+ 
+ /* Calculate height and width of text given a Pango Font */
