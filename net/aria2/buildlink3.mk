b440db3f30738ca305ab99152c2d50e2df4b3ea4
wiz <wiz@pkgsrc.org>
2025-08-31 20:25:50 +0000

    aria2: buildlink3.mk cleanup

--- net/aria2/buildlink3.mk
+++ net/aria2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/08/31 20:25:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/08/31 20:25:50 wiz Exp $
 
 BUILDLINK_TREE+=	aria2
 
@@ -8,10 +8,6 @@ ARIA2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.aria2+=	aria2>=1.36.0nb13
 BUILDLINK_PKGSRCDIR.aria2?=	../../net/aria2
 
-#.include "../../converters/libiconv/buildlink3.mk"
-#.include "../../devel/zlib/buildlink3.mk"
-#.include "../../net/libcares/buildlink3.mk"
-#.include "../../textproc/expat/buildlink3.mk"
 .endif	# ARIA2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-aria2
