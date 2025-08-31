1c080976e6e130027b611cfa7147f3fbeec828d7
leot <leot@pkgsrc.org>
2025-08-31 19:40:17 +0000

    trufflehog: Update to 3.90.5
    pkgsrc changes:
    - Build with Go 1.25.
    - Switch to use "regexp". github.com/wasilibs/go-re2 needs
      NOT_PAX_MPROTECT_SAFE:
    
            panic: permission denied
    
            goroutine 1 [running]:
            github.com/tetratelabs/wazero/internal/engine/wazevo.mmapExecutable(...)
                    .../pkg/mod/github.com/tetratelabs/wazero@v1.9.0/internal/engine/wazevo/engine.go:776
            github.com/tetratelabs/wazero/internal/engine/wazevo.(*engine).compileSharedFunctions(0xc0005a22d0)
                    .../pkg/mod/github.com/tetratelabs/wazero@v1.9.0/internal/engine/wazevo/engine.go:605 +0xc85
            github.com/tetratelabs/wazero/internal/engine/wazevo.NewEngine({0x4f0cee8, 0xc0003a5260}, 0xc0cb48?, {0x0, 0x0})
                    .../pkg/mod/github.com/tetratelabs/wazero@v1.9.0/internal/engine/wazevo/engine.go:115 +0x145
            github.com/tetratelabs/wazero.NewRuntimeWithConfig({0x4f0cee8?, 0xc0003a5260?}, {0x4f265a0?, 0xc000469d00})
                    .../pkg/mod/github.com/tetratelabs/wazero@v1.9.0/runtime.go:178 +0x12c
            github.com/wasilibs/go-re2/internal.init.0()
                    .../pkg/mod/github.com/wasilibs/go-re2@v1.9.0/internal/re2_wazero.go:180 +0x137
    
    
    Changes:
    [Changelog basically list every single commit and is not very easy to
    read. I have omitted a lot of them, for full changes please see
    <https://github.com/trufflesecurity/trufflehog/releases>]
    
    v3.90.4
    * Disabled besnappy detector
    * Add option to skip binary files during filesystem source scanning
    * Bug fixes and improvements
    
    v3.90.2
    * Added Anypoint API OAuth2 Detector
    
    v3.90.1
    * Salesforce Refresh Token Detector
    
    v3.90.0
    * Added LangSmith API Key detector
    * Bannerbear Detector V2
    * Add support for docker daemon as a source
    
    v3.89.2
    * Analyzer/datadog
    
    v3.89.0
    * Stripe Payment Intent Detector
    
    v3.88.35
    * Added deepseek support
    
    v3.88.34
    * Enhanced smartsheets detector
    
    v3.88.31
    * Added DataBricks Analyzer
    
    v3.88.30
    * Detector implementation for Azure Configuration Connection String Key
    * Added Ngrok API Key Analyzer
    * Added New AccuWeather Detector Version
    * Added Mux API Analyzer
    * Implementation of Posthog Analyzer
    
    v3.88.29
    * Detector implementation for Azure API Management Direct Management Key
    * Monday App Analyzer
    
    v3.88.28
    * Add xAI detector
    * Netlify Analyzer
    
    v3.88.27
    * bing subscription key support
    
    v3.88.26
    * Support Dockerhub OATs
    * langfuse support
    
    v3.88.24
    * Plaid API Analyzer
    * Addition of Detector - Azure Subsciprtion Keys
    
    v3.88.21
    * Added Figma (PAT) Analyzer
    * Detector implementation for Azure API Management Repository key
    
    v3.88.19
    * added sentry org token detector
    * Detector implementation for Azure SAS Tokens
    
    v3.88.16
    * Groq Analyzer
    * LaunchDarkly Token Analyzer
    
    v3.88.15
    * Planetscale Analyzer
    * Airtable Analyzer for Personal Access Tokens
    
    v3.88.14
    * Airtable Personal Access Token Detector
    
    v3.88.13
    * elevenlabs analyzer
    
    v3.88.12
    * Airtable Analyzer for OAuth Tokens
    
    v3.88.8
    * anthropic api key analyzer
    
    v3.88.7
    * dockerhub analyzer
    
    v3.88.6
    * Airtable OAuth Detector
    
    v3.88.5
    * Create detector for Azure refresh tokens
    * PrivateKey Analyzer
    
    v3.88.0
    * Added new detector for Twilio APIKey
    
    v3.86.0
    * Add new detector for Twitch OAuth Access Tokens
    
    v3.85.0
    * Add Flexport detector
    
    v3.84.1
    * Support S3 Source Resumption
    
    v3.84.0
    * S3 Progress Tracker
    * Added ZohoCRM detector
    
    v3.83.7
    * Add support for scanning APK files
    
    v3.83.5
    * add config option for s3 resumption
    
    v3.82.9
    * Add SaladCloud API Key detector
    
    v3.82.7
    * Added Cisco Meraki API Key detector
    
    v3.82.5
    * RailwayApp Detector
    
    v3.82.2
    * Add Analyzer for MySQL
    * Add Analyzer for Mailgun
    * Add analyzer interface for Shopify
    
    v3.82.0
    * Add Robinhood Crypto detector
    
    v3.80.2
    * Added Twitter v2 Detector
    
    v3.80.0
    * Elevenlabs detector
    * Atlassian Token Detector
    
    v3.79.0
    * Adding Larksuite Detectors
    
    v3.78.2
    * adding twitter + Consumer key detector
    
    v3.78.0
    * Add Jenkins scanning
    
    v3.77.0
    * adding Groq detector

--- security/trufflehog/patches/patch-main.go
+++ security/trufflehog/patches/patch-main.go
@@ -1,37 +1,37 @@
-$NetBSD: patch-main.go,v 1.4 2024/01/07 13:10:55 leot Exp $
+$NetBSD: patch-main.go,v 1.5 2025/08/31 19:40:17 leot Exp $
 
 Disable update functionality.
 
 This avoid to patch overseer in order to add support for more operating
 systems and all the logic is not desiderable for a package system too.
 
---- main.go.orig	2023-12-23 06:45:27.000000000 +0000
+--- main.go.orig	2025-08-31 17:53:14.235574170 +0000
 +++ main.go
-@@ -15,7 +15,6 @@ import (
- 	"github.com/alecthomas/kingpin/v2"
+@@ -19,7 +19,6 @@ import (
+ 	"github.com/fatih/color"
  	"github.com/felixge/fgprof"
  	"github.com/go-logr/logr"
 -	"github.com/jpillora/overseer"
  	"github.com/mattn/go-isatty"
- 	"google.golang.org/protobuf/types/known/anypb"
+ 	"go.uber.org/automaxprocs/maxprocs"
  
-@@ -32,7 +31,6 @@ import (
- 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/sourcespb"
+@@ -38,7 +37,6 @@ import (
+ 	"github.com/trufflesecurity/trufflehog/v3/pkg/output"
  	"github.com/trufflesecurity/trufflehog/v3/pkg/sources"
  	"github.com/trufflesecurity/trufflehog/v3/pkg/tui"
 -	"github.com/trufflesecurity/trufflehog/v3/pkg/updater"
+ 	"github.com/trufflesecurity/trufflehog/v3/pkg/verificationcache"
  	"github.com/trufflesecurity/trufflehog/v3/pkg/version"
  )
- 
-@@ -42,7 +40,6 @@ var (
- 	debug               = cli.Flag("debug", "Run in debug mode.").Bool()
- 	trace               = cli.Flag("trace", "Run in trace mode.").Bool()
+@@ -51,7 +49,6 @@ var (
+ 	debug               = cli.Flag("debug", "Run in debug mode.").Hidden().Bool()
+ 	trace               = cli.Flag("trace", "Run in trace mode.").Hidden().Bool()
  	profile             = cli.Flag("profile", "Enables profiling and sets a pprof and fgprof server on :18066.").Bool()
 -	localDev            = cli.Flag("local-dev", "Hidden feature to disable overseer for local dev.").Hidden().Bool()
  	jsonOut             = cli.Flag("json", "Output in JSON format.").Short('j').Bool()
  	jsonLegacy          = cli.Flag("json-legacy", "Use the pre-v3.0 JSON format. Only works with git, gitlab, and github sources.").Bool()
  	gitHubActionsFormat = cli.Flag("github-actions", "Output in GitHub Actions format.").Bool()
-@@ -191,36 +188,12 @@ func main() {
+@@ -345,37 +342,11 @@ func main() {
  	// make it the default logger for contexts
  	context.SetDefaultLogger(logger)
  
@@ -50,9 +50,10 @@ systems and all the logic is not desiderable for a package system too.
 -		// TODO: Eventually add a PreUpgrade func for signature check w/ x509 PKCS1v15
 -		// PreUpgrade: checkUpdateSignature(binaryPath string),
 -	}
- 
+-
 -	if !*noUpdate {
--		updateCfg.Fetcher = updater.Fetcher(version.BuildVersion)
+-		topLevelCmd, _, _ := strings.Cut(cmd, " ")
+-		updateCfg.Fetcher = updater.Fetcher(topLevelCmd, usingTUI)
 -	}
 -	if version.BuildVersion == "dev" {
 -		updateCfg.Fetcher = nil
@@ -67,6 +68,6 @@ systems and all the logic is not desiderable for a package system too.
  
 -func run(state overseer.State) {
 +func run() {
+ 
  	ctx, cancel := context.WithCancelCause(context.Background())
  	defer cancel(nil)
- 
