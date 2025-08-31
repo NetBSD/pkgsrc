2a8e7247302629e6ef6fa577a726d139e87ed814
bsiegert <bsiegert@pkgsrc.org>
2025-08-31 09:39:25 +0000

    go-staticcheck: update to 2025.1.1
    Fixes build with Go 1.25.
    
    - The keyify utility has been removed. The recommended alternative is
      gopls.
    - staticcheck -merge now exits with a non-zero status if any problems
      have been found.

--- devel/go-staticcheck/go-modules.mk
+++ devel/go-staticcheck/go-modules.mk
@@ -1,12 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.4 2024/01/07 17:26:47 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.5 2025/08/31 09:39:25 bsiegert Exp $
 
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.mod
-GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.2.1.zip
-GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20221208152030-732eee02a75a.mod
-GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20221208152030-732eee02a75a.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.12.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.12.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.11.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.12.1-0.20230825192346-2191a27a6dc5.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.12.1-0.20230825192346-2191a27a6dc5.zip
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.4.1-0.20240526193622-a339e1f7089c.mod
+GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.4.1-0.20240526193622-a339e1f7089c.zip
+GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20231108232855-2478ac86f678.mod
+GO_MODULE_FILES+=	golang.org/x/exp/typeparams/@v/v0.0.0-20231108232855-2478ac86f678.zip
+GO_MODULE_FILES+=	golang.org/x/exp/@v/v0.0.0-20231110203233-9a3e6036ecaa.mod
+GO_MODULE_FILES+=	golang.org/x/exp/@v/v0.0.0-20231110203233-9a3e6036ecaa.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.23.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.23.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.11.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.11.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.30.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.30.0.zip
