d14f275d1bc7c3d666559c603f188e0ba6fbd2a1
adam <adam@pkgsrc.org>
2025-09-01 08:25:16 +0000

    cmake cmake-gui: updated to 4.1.1
    CMake 4.1.1
    * cmGlobalVisualStudio7Generator: fix initialization typo
    * StdIo: Fix Terminal abstraction to avoid unnecessary VT100 escape sequences
    * Makefile: Fix regression that prints unnecessary VT100 escape sequences
    * ctest: Restore default of no time limit for command-line `-T Test` step
    * ctest: Restore default test timeout for command-line `-T Test` step
    * Ninja: Fix regression with `.bat` wrapper around `ninja`
    * cmList: fix swap function definition
    * string(GENEX_STRIP): Fix regression on nested generator expressions
    * Clang/CUDA: Support CUDA Toolkit 13 new include layout
    * PkgC: Allow system paths during IMPORT, POPULATE
    * PkgC: Only remove system paths which are exact matches

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.78 2025/08/20 11:34:12 wiz Exp $
+# $NetBSD: version.mk,v 1.79 2025/09/01 08:25:16 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.1.0
+CMAKE_VERSION=	4.1.1
 CMAKE_API=	${CMAKE_VERSION:R}
