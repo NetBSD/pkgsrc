311d52ad8a05ad5de18f515bc733b8ce934abc50
adam <adam@pkgsrc.org>
2025-09-01 08:05:47 +0000

    nodejs22: updated to 22.19.0
    22.19.0 'Jod' (LTS)
    
    Notable Changes
    
    - (SEMVER-MINOR) cli: add NODE_USE_SYSTEM_CA=1 (Joyee Cheung)
    - (SEMVER-MINOR) cli: support ${pid} placeholder in --cpu-prof-name (Haram Jeong)
    - (SEMVER-MINOR) crypto: add tls.setDefaultCACertificates() (Joyee Cheung)
    - (SEMVER-MINOR) dns: support max timeout (theanarkh)
    - doc: update the instruction on how to verify releases (Antoine du Hamel)
    - (SEMVER-MINOR) esm: unflag --experimental-wasm-modules (Guy Bedford)
    - (SEMVER-MINOR) http: add server.keepAliveTimeoutBuffer option (Haram Jeong)
    - lib: docs deprecate _http_* (Sebastian Beltran)
    - (SEMVER-MINOR) net: update net.blocklist to allow file save and file management (alphaleadership)
    - (SEMVER-MINOR) process: add threadCpuUsage (Paolo Insogna)
    - (SEMVER-MINOR) zlib: add dictionary support to zstdCompress and zstdDecompress (lluisemper)

--- lang/nodejs22/patches/patch-deps_v8_src_base_platform_platform-posix.cc
+++ lang/nodejs22/patches/patch-deps_v8_src_base_platform_platform-posix.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.1 2024/11/13 08:45:17 adam Exp $
+$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.2 2025/09/01 08:05:47 adam Exp $
 
 Use sysconf(_SC_THREAD_STACK_MIN) instead of PTHREAD_STACK_MIN.
 Cast explicitly.
@@ -7,24 +7,9 @@ Remove legacy madvise(2) prototypes, prefer posix_madvise(2) if available.
 Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
 (PR port-arm/55533)
 
---- deps/v8/src/base/platform/platform-posix.cc.orig	2023-10-24 10:04:41.000000000 +0000
+--- deps/v8/src/base/platform/platform-posix.cc.orig	2025-08-28 12:10:12.000000000 +0000
 +++ deps/v8/src/base/platform/platform-posix.cc
-@@ -77,14 +77,6 @@
- #define MAP_ANONYMOUS MAP_ANON
- #endif
- 
--#if defined(V8_OS_SOLARIS)
--#if (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE > 2) || defined(__EXTENSIONS__)
--extern "C" int madvise(caddr_t, size_t, int);
--#else
--extern int madvise(caddr_t, size_t, int);
--#endif
--#endif
--
- #ifndef MADV_FREE
- #define MADV_FREE MADV_DONTNEED
- #endif
-@@ -399,6 +391,10 @@ void* OS::GetRandomMmapAddr() {
+@@ -409,6 +409,10 @@ void* OS::GetRandomMmapAddr() {
  #endif
  #endif
  #endif
@@ -35,7 +20,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
    return reinterpret_cast<void*>(raw_addr);
  }
  
-@@ -565,14 +561,11 @@ bool OS::DiscardSystemPages(void* addres
+@@ -575,14 +579,11 @@ bool OS::DiscardSystemPages(void* addres
      // MADV_FREE_REUSABLE sometimes fails, so fall back to MADV_DONTNEED.
      ret = madvise(address, size, MADV_DONTNEED);
    }
@@ -52,7 +37,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
  #else
    int ret = madvise(address, size, MADV_DONTNEED);
  #endif
-@@ -815,6 +808,8 @@ int OS::GetCurrentThreadId() {
+@@ -830,6 +831,8 @@ int OS::GetCurrentThreadId() {
    return static_cast<int>(syscall(__NR_gettid));
  #elif V8_OS_ANDROID
    return static_cast<int>(gettid());
@@ -61,7 +46,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
  #elif V8_OS_AIX
    return static_cast<int>(thread_self());
  #elif V8_OS_FUCHSIA
-@@ -1091,7 +1086,11 @@ Thread::Thread(const Options& options)
+@@ -1126,7 +1129,11 @@ Thread::Thread(const Options& options)
        stack_size_(options.stack_size()),
        priority_(options.priority()),
        start_semaphore_(nullptr) {
@@ -73,7 +58,7 @@ Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
    if (stack_size_ > 0) stack_size_ = std::max(stack_size_, min_stack_size);
    set_name(options.name());
  }
-@@ -1106,7 +1105,7 @@ static void SetThreadName(const char* na
+@@ -1141,7 +1148,7 @@ static void SetThreadName(const char* na
    pthread_set_name_np(pthread_self(), name);
  #elif V8_OS_NETBSD
    static_assert(Thread::kMaxThreadNameLength <= PTHREAD_MAX_NAMELEN_NP);
