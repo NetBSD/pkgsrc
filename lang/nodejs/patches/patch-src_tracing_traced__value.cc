4b20b177d48e95f351a8eec743ff551185f23df4
wiz <wiz@pkgsrc.org>
2025-09-02 07:37:58 +0000

    nodejs: update NetBSD patch after upstream feedback

--- lang/nodejs/patches/patch-src_tracing_traced__value.cc
+++ lang/nodejs/patches/patch-src_tracing_traced__value.cc
@@ -1,19 +1,15 @@
-$NetBSD: patch-src_tracing_traced__value.cc,v 1.1 2025/09/01 20:57:39 wiz Exp $
+$NetBSD: patch-src_tracing_traced__value.cc,v 1.2 2025/09/02 07:37:58 wiz Exp $
 
 Fix build on NetBSD.
 https://github.com/nodejs/node/issues/59714
 
---- src/tracing/traced_value.cc.orig	2025-09-01 20:45:44.230556337 +0000
+--- src/tracing/traced_value.cc.orig	2025-09-02 07:14:52.741019499 +0000
 +++ src/tracing/traced_value.cc
-@@ -87,7 +87,11 @@ std::string EscapeString(const char* val
- }
+@@ -9,6 +9,7 @@
+ #include <unicode/utypes.h>
+ #endif
  
- std::string DoubleToCString(double v) {
-+#if defined(__NetBSD__)
-+  switch (fpclassify(v)) {
-+#else
-   switch (FPCLASSIFY_NAMESPACE::fpclassify(v)) {
-+#endif
-     case FP_NAN: return "\"NaN\"";
-     case FP_INFINITE: return (v < 0.0 ? "\"-Infinity\"" : "\"Infinity\"");
-     case FP_ZERO: return "0";
++#include <cmath>
+ #include <sstream>
+ 
+ #include "node_metadata.h"
