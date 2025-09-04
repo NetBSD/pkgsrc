8060d8f4f57c3702f54dc4cc7efcf30f214f92c3
dkazankov <dkazankov@pkgsrc.org>
2025-09-04 08:20:02 +0000

    add inplace build support
    * add inplace.mk
    * move substitutions from pre-configure stage to post-extract stage
    * fix guard varname in buildlink3.mk

--- textproc/ada-xmlada-25/buildlink3.mk
+++ textproc/ada-xmlada-25/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/07/10 16:55:09 dkazankov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/09/04 08:20:02 dkazankov Exp $
 
 BUILDLINK_TREE+=	xmlada-25
 
-.if !defined(XMLADA25_BUILDLINK3_MK)
-XMLADA25_BUILDLINK3_MK:=
+.if !defined(XMLADA_25_BUILDLINK3_MK)
+XMLADA_25_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xmlada-25+=	xmlada-${GNAT_NAME}>=25.0.0
 BUILDLINK_PKGSRCDIR.xmlada-25?=		../../textproc/ada-xmlada-25
