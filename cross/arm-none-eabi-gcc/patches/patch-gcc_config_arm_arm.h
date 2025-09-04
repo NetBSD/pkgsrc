e47ae3f33729d56259b2f22a632cf6e0d9d1f127
lloyd <lloyd@pkgsrc.org>
2025-09-04 23:06:24 +0000

    Update the arm-none-eabi and riscv64-none-elf toolchains.
    These toolchains have been updated to
    
    	binutils-2.45
    	gcc-14.3.0
    	gdb-16.3
    	newlib-4.5.0.20241231
    
    from
    
    	binutils-2.32
    	gcc-8.3.0
    	gdb-7.10
    	newlib-3.1.0 (4.4.0.20231231 for riscv64)
    
    This affects
    
    	cross/arm-none-eabi-binutils
    	cross/arm-none-eabi-gcc
    	cross/arm-none-eabi-gdb
    	cross/riscv64-none-elf-binutils
    	cross/riscv64-none-elf-gcc
    
    
    Multilib is now enabled so that code for microcontrollers can be
    compiled. These cross compilers can now be used to compile code for
    microcontrollers such as the Raspberry Pi Pico 2.

--- cross/arm-none-eabi-gcc/patches/patch-gcc_config_arm_arm.h
+++ cross/arm-none-eabi-gcc/patches/patch-gcc_config_arm_arm.h
@@ -1,12 +1,9 @@
-$NetBSD: patch-gcc_config_arm_arm.h,v 1.1 2020/02/11 01:04:05 mrg Exp $
+$NetBSD: patch-gcc_config_arm_arm.h,v 1.2 2025/09/04 23:06:24 lloyd Exp $
 
-Disable -march=native for cross compilers.
-
-
---- gcc/config/arm/arm.h.orig	2018-03-22 10:24:41.000000000 -0700
-+++ gcc/config/arm/arm.h	2020-02-10 16:39:34.594580129 -0800
-@@ -2194,8 +2194,8 @@
-   { "target_mode_check", arm_target_thumb_only },
+--- gcc/config/arm/arm.h.orig	2025-05-23 11:02:04.304197710 +0000
++++ gcc/config/arm/arm.h
+@@ -2493,8 +2493,8 @@ extern const char *arm_target_mode (int 
+   { "target_mode_check", arm_target_mode },
  
  /* -mcpu=native handling only makes sense with compiler running on
 -   an ARM chip.  */
