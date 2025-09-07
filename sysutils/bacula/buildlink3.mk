ca5c721e9a4dabc5a67464e220be16e0d398102f
jnemeth <jnemeth@pkgsrc.org>
2025-09-07 03:37:07 +0000

    pkgsrc: fix chio-changer script
    Fix chio-changer script so that it will strip a trailing "ver. 0" from
    voltags which my changer adds.
    
    E-mail if you're changer does something else weird.  You can test with
    chio-changer <changer dev> list

--- sysutils/bacula/buildlink3.mk
+++ sysutils/bacula/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/10/12 20:56:41 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/09/07 03:37:07 jnemeth Exp $
 
 BUILDLINK_TREE+=	bacula
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	bacula
 BACULA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.bacula+=	{bacula,bacula-clientonly}>=3.0.1nb1
-BUILDLINK_ABI_DEPENDS.bacula?=	{bacula,bacula-clientonly}>=13.0.1
+BUILDLINK_ABI_DEPENDS.bacula?=	bacula>=15.0.3nb1
 BUILDLINK_PKGSRCDIR.bacula?=	../../sysutils/bacula
 .endif	# BACULA_BUILDLINK3_MK
 
