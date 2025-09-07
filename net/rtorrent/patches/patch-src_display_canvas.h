41d0ec316608dc55f80e0e2b74837c631781a12e
adam <adam@pkgsrc.org>
2025-09-07 07:49:15 +0000

    libtorrent rtorrent: updated to 0.16.0
    0.16.0
    
    This release moves libcurl dependency to libtorrent and makes http requests in a separate thread. The network.block.* commands now work properly for IPv4/6, although IPv6 support still has some issues.
    
    DHT has been fixed and magnet links without any embedded trackers should now work properly.

--- net/rtorrent/patches/patch-src_display_canvas.h
+++ net/rtorrent/patches/patch-src_display_canvas.h
@@ -1,18 +1,10 @@
-$NetBSD: patch-src_display_canvas.h,v 1.2 2025/04/01 09:37:54 adam Exp $
+$NetBSD: patch-src_display_canvas.h,v 1.3 2025/09/07 07:49:16 adam Exp $
 
 Make this work with "ncurses" 5.8. Patch taken from:
 http://www.mail-archive.com/arch-general@archlinux.org/msg18728.html
 
 --- src/display/canvas.h.orig	2025-03-28 18:55:13.000000000 +0000
 +++ src/display/canvas.h
-@@ -1,6 +1,7 @@
- #ifndef RTORRENT_DISPLAY_CANVAS_H
- #define RTORRENT_DISPLAY_CANVAS_H
- 
-+#include <cstdarg>
- #include <string>
- #include <unordered_map>
- #include <vector>
 @@ -15,7 +16,7 @@ public:
    typedef std::vector<Attributes>      attributes_list;
    typedef std::unordered_map<int, int> attributes_map;
