24515d9aaa93dc0ad9c15dfec7ee09e7d64e8f01
dkazankov <dkazankov@pkgsrc.org>
2025-09-07 08:02:41 +0000

    textproc/ada-libgnatdoc-25: add new package 25.0.0
    GNAT Documentation Generation Tool, library package.

---
+++ textproc/ada-libgnatdoc-25/patches/patch-source_gnatdoc_gnatdoc-version.ads
@@ -0,0 +1,14 @@
+$NetBSD: patch-source_gnatdoc_gnatdoc-version.ads,v 1.1 2025/09/07 08:02:41 dkazankov Exp $
+
+Set version information
+
+--- source/gnatdoc/gnatdoc-version.ads.orig	2024-10-04 16:57:51.000000000 +0300
++++ source/gnatdoc/gnatdoc-version.ads
+@@ -18,6 +18,6 @@
+ package GNATdoc.Version is
+ 
+    Version_String : VSS.Strings.Virtual_String :=
+-     "%VERSION% (%DATE%)";
++     "25.0 (20241024)";
+ 
+ end GNATdoc.Version;
