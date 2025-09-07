37373de0528396779b8311b5fb704fa8ff1eb467
dkazankov <dkazankov@pkgsrc.org>
2025-09-07 07:48:27 +0000

    textproc/ada-libgnatformat-25: add new package 25.0.0
    Opinionated code formatter for the Ada language, library package.

---
+++ textproc/ada-libgnatformat-25/patches/patch-src_gnatformat.ads
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_gnatformat.ads,v 1.1 2025/09/07 07:48:28 dkazankov Exp $
+
+Set version information
+
+--- src/gnatformat.ads.orig	2024-09-24 18:34:53.000000000 +0300
++++ src/gnatformat.ads
+@@ -11,8 +11,8 @@
+ 
+    Gnatformat_Trace : GNATCOLL.Traces.Trace_Handle :=
+      GNATCOLL.Traces.Create ("GNATFORMAT", GNATCOLL.Traces.Off);
+-   Version    : constant String := "debug";
+-   Build_Date : constant String := "debug";
++   Version    : constant String := "25.0";
++   Build_Date : constant String := "20241031";
+ 
+    type Supported_Languages is (Ada_Language);
+ 
