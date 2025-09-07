3cef6198747edf638a3615a304f0f286c01623ba
pin <pin@pkgsrc.org>
2025-09-07 13:10:19 +0000

    math/kalker: update to 2.2.2
     - Keep sign when rendering symbolic estimations of square roots
     - Print numbers within matrices properly in the CLI version

--- math/kalker/cargo-depends.mk
+++ math/kalker/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.10 2025/06/19 20:24:27 wiz Exp $
+# $NetBSD: cargo-depends.mk,v 1.11 2025/09/07 13:10:19 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.3
 CARGO_CRATE_DEPENDS+=	ansi_term-0.12.1
