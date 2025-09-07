c0acebc4ccb703a6ecbcd7499dacd1a328130cb5
dkazankov <dkazankov@pkgsrc.org>
2025-09-07 07:20:48 +0000

    devel/libadalang-tools: add new package 25.0.0
    Libadalang-based development tools for Ada.

---
+++ devel/libadalang-tools/patches/patch-src_utils-versions.ads
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_utils-versions.ads,v 1.1 2025/09/07 07:20:48 dkazankov Exp $
+
+Set version information
+
+--- src/utils-versions.ads.orig	2024-09-23 16:30:31.000000000 +0300
++++ src/utils-versions.ads
+@@ -23,12 +23,12 @@
+ 
+ package Utils.Versions is
+ 
+-   Version      : constant String := "dev";
+-   Current_Year : constant String := "unknown";
++   Version      : constant String := "25.0";
++   Current_Year : constant String := "2024";
+ 
+    type Gnat_Build_Type is (Gnatpro, GPL);
+ 
+-   Build_Type : constant Gnat_Build_Type := Gnatpro;
++   Build_Type : constant Gnat_Build_Type := GPL;
+    --  Kind of GNAT Build:
+    --
+    --    Gnatpro
