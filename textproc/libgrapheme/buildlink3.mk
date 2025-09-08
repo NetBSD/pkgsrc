6f92a3bf92b04367b7bd683cf45c17f2a426cd6f
vins <vins@pkgsrc.org>
2025-09-08 08:26:18 +0000

    libgrapheme: fix typo

--- textproc/libgrapheme/buildlink3.mk
+++ textproc/libgrapheme/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/09/08 08:22:16 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/09/08 08:26:18 vins Exp $
 
 BUILDLINK_TREE+=	libgrapheme
 
 .if !defined(LIBGRAPHEME_BUILDLINK3_MK)
 LIBGRAPHEME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgrapheme+=	libgrapheme>=2.0.2p1
+BUILDLINK_API_DEPENDS.libgrapheme+=	libgrapheme>=2.0.2nb1
 BUILDLINK_PKGSRCDIR.libgrapheme?=	../../textproc/libgrapheme
 .endif	# LIBGRAPHEME_BUILDLINK3_MK
 
