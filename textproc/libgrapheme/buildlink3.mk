b3b54dea732b54e9a45fdfab2bad01898a7828c3
vins <vins@pkgsrc.org>
2025-09-08 08:22:16 +0000

    libgrapheme: fix build.
    The code doesn't iactually compile in strict C99 dspite its claims.

--- textproc/libgrapheme/buildlink3.mk
+++ textproc/libgrapheme/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/07/26 06:36:33 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/09/08 08:22:16 vins Exp $
 
 BUILDLINK_TREE+=	libgrapheme
 
 .if !defined(LIBGRAPHEME_BUILDLINK3_MK)
 LIBGRAPHEME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgrapheme+=	libgrapheme>=2.0.2
+BUILDLINK_API_DEPENDS.libgrapheme+=	libgrapheme>=2.0.2p1
 BUILDLINK_PKGSRCDIR.libgrapheme?=	../../textproc/libgrapheme
 .endif	# LIBGRAPHEME_BUILDLINK3_MK
 
