f8d0725a464870b3b85b4ecb7d614d8514a5ca97
vins <vins@pkgsrc.org>
2025-09-08 07:05:27 +0000

    wm/fvwm3: fix build on SunOS when using pkgsrc iconv.
    Use a hacks.mk fragment to detect the iconv implementation in use and
    apply a fix for the iconv() prototype only if needed.
    Bump revision

---
+++ wm/fvwm3/hacks.mk
@@ -0,0 +1,32 @@
+# $NetBSD: hacks.mk,v 1.1 2025/09/08 07:05:27 vins Exp $
+
+.if !defined(FVWM3_HACKS_MK)
+FVWM3_HACKS_MK=	defined
+
+### [Mon Sept 8 06:52:31 UTC 2025 : vins]
+### The second argument of iconv() is of type `char **' in some
+### implementations (including GNU libiconv), while it expects a `const char **'
+### type in some others (at the very least on SunOS and NetBSD up to 9.*).
+### This is a knwon issue. See https://gnats.netbsd.org/57672.
+### Use the SUBST framework to fix the iconv prototype mismatch when needed.
+
+.if (${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 100000) || ${OPSYS} == "SunOS"
+
+CHECK_BUILTIN.iconv:=   yes
+.include "../../converters/libiconv/builtin.mk"
+CHECK_BUILTIN.iconv:=   no
+
+.  if ${USE_BUILTIN.iconv:tl} == yes
+
+PKG_HACKS+=		iconv_const
+
+SUBST_CLASSES+=		iconv
+SUBST_STAGE.iconv=	pre-configure
+SUBST_MESSAGE.iconv=	Applying portability fixes for SunOS iconv.
+SUBST_FILES.iconv=	meson.build
+SUBST_SED.iconv=	-e "s:conf.set('ICONV_ARG_CONST', ''):conf.set('ICONV_ARG_CONST', 'const'):g"
+
+.  endif
+.endif
+
+.endif  # FVWM3_HACKS_MK
