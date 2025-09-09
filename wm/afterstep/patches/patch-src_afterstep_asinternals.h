fbea01a23716b0088dbf80beabd956b6a8cbd08b
gutteridge <gutteridge@pkgsrc.org>
2025-09-09 22:07:36 +0000

    afterstep: add missing function prototypes

--- wm/afterstep/patches/patch-src_afterstep_asinternals.h
+++ wm/afterstep/patches/patch-src_afterstep_asinternals.h
@@ -1,10 +1,31 @@
-$NetBSD: patch-src_afterstep_asinternals.h,v 1.3 2021/07/11 12:13:17 bsiegert Exp $
+$NetBSD: patch-src_afterstep_asinternals.h,v 1.4 2025/09/09 22:07:36 gutteridge Exp $
 
 Patch up gcc inline mess.
 
+Add missing function prototypes.
+
 --- src/afterstep/asinternals.h.orig	2013-05-01 13:34:11.000000000 +0000
 +++ src/afterstep/asinternals.h
-@@ -689,8 +689,8 @@ void ParsePopupEntry (char *tline, FILE 
+@@ -575,6 +575,7 @@ void asdbus_shutdown();
+ void asdbus_process_messages ();
+ 
+ char *asdbus_RegisterSMClient(const char *sm_client_id);
++void asdbus_UnregisterSMClient(const char *sm_client_path);
+ void asdbus_Notify(const char *summary, const char *body, int timeout);
+ 
+ typedef enum {
+@@ -587,6 +588,10 @@ Bool asdbus_GetCanLogout ();
+ Bool asdbus_Logout (ASDbusLogoutMode mode, int timeout);
+ Bool asdbus_GetCanShutdown ();
+ Bool asdbus_Shutdown (int timeout);
++Bool asdbus_GetCanSuspend ();
++Bool asdbus_Suspend (int timeout);
++Bool asdbus_GetCanHibernate ();
++Bool asdbus_Hibernate (int timeout);
+ Bool get_gnome_autosave ();
+ 
+ /*************************** decorations.c ********************************/
+@@ -689,8 +694,8 @@ void ParsePopupEntry (char *tline, FILE 
  
  
  /*************************** misc.c *********************************/
