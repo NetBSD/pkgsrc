39c4a314093fc0d9dfb70ae18a39ee4eafa40afe
vins <vins@pkgsrc.org>
2025-09-10 10:34:02 +0000

    net/libasr: update to 20240904
    # pkgsrc changes
    
    As of 22-06-2023, libasr is no more maintained independently [0].
    The project encourages using the bundled version found in OpenSMTPD.
    However, a modular approach seems more reasonable, as somebody could
    make use of libasr without having to install the full smtpd suite.
    
    This is also in line with the fact that we're still relying on a
    patched libretls for OpenSMTPD instead of using the bundled libtls library.
    
    The present libasr version is based on OpenSMTPD's (which in turn is in
    sync with OpenBSD's src), with minor modifications and a couple of
    patches for NetBSD/SunOS applied. Hosted on ftp.netbsd.org.
    
    [0] https://github.com/OpenSMTPD/libasr/commit/05b8bf7d480c25195b365552f9ed6021cd0a68e3
    
    # upstream changes (since 1.0.4)
    
    * Readd knob to prefer ipv6 over ipv4.
    * Readd SOCK_DNS.
    * Do not ignore safety check when EAI_BADHINTS is not defined.
    * Avoid non portable memcpy(malloc(0), ..., 0).
    * Patch against asr errata OpenBSD.
    * Fix minor compiler warnings.

--- net/libasr/buildlink3.mk
+++ net/libasr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/01 05:03:23 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/09/10 10:34:02 vins Exp $
 
 BUILDLINK_TREE+=	libasr
 
 .if !defined(LIBASR_BUILDLINK3_MK)
 LIBASR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libasr+=	libasr>=1.0.2
+BUILDLINK_API_DEPENDS.libasr+=	libasr>=20240904
 BUILDLINK_PKGSRCDIR.libasr?=	../../net/libasr
 .endif	# LIBASR_BUILDLINK3_MK
 
