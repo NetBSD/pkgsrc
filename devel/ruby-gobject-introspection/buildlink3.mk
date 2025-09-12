2b83b402031b08f2117ad869f9ffc1a5284a5e9d
tsutsui <tsutsui@pkgsrc.org>
2025-09-12 14:04:47 +0000

    ruby-gnome: update to 4.3.3.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.3.3
    
    Ruby-GNOME 4.3.3: 2025-08-17
    
    This is a graphene1 require path fix release.
    
    Changes
    
     Ruby/Graphene1
    
      * Improvements
          + Improved Graphene::Size API.
    
      * Fixes
          + Renamed require path to graphene1 from graphene.
    
     Ruby/GSK4
    
      * Improvements
          + Improved Gsk::RoundedRect API.
    
     Ruby/GObjectIntrospection
    
      * Improvements
          + Added support for try_convert protocol for no GTyped
            structs.

--- devel/ruby-gobject-introspection/buildlink3.mk
+++ devel/ruby-gobject-introspection/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2025/08/15 15:06:51 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2025/09/12 14:04:48 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gobject-introspection
 
 .if !defined(RUBY_GOBJECT_INTROSPECTION_BUILDLINK3_MK)
 RUBY_GOBJECT_INTROSPECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gobject-introspection+=	${RUBY_PKGPREFIX}-gobject-introspection>=4.3.2
-BUILDLINK_ABI_DEPENDS.ruby-gobject-introspection+=	${RUBY_PKGPREFIX}-gobject-introspection>=4.3.2
+BUILDLINK_API_DEPENDS.ruby-gobject-introspection+=	${RUBY_PKGPREFIX}-gobject-introspection>=4.3.3
+BUILDLINK_ABI_DEPENDS.ruby-gobject-introspection+=	${RUBY_PKGPREFIX}-gobject-introspection>=4.3.3
 BUILDLINK_PKGSRCDIR.ruby-gobject-introspection?=	../../devel/ruby-gobject-introspection
 
 .include "../../devel/gobject-introspection/buildlink3.mk"
--- devel/ruby-pango/buildlink3.mk
+++ devel/ruby-pango/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.24 2025/08/15 15:06:51 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2025/09/12 14:04:48 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-pango
 
 .if !defined(RUBY_PANGO_BUILDLINK3_MK)
 RUBY_PANGO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.3.2
-BUILDLINK_ABI_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.3.2
+BUILDLINK_API_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.3.3
+BUILDLINK_ABI_DEPENDS.ruby-pango+=	${RUBY_PKGPREFIX}-pango>=4.3.3
 BUILDLINK_PKGSRCDIR.ruby-pango?=	../../devel/ruby-pango
 
 .include "../../devel/pango/buildlink3.mk"
