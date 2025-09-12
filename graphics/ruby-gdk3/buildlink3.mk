2c06d8f2ce6c507a163f2c38c1ef302ff43802af
tsutsui <tsutsui@pkgsrc.org>
2025-09-12 14:04:47 +0000

    ruby-gnome: update to 4.3.3.
    Upstream changes:
     https://github.com/ruby-gnome/ruby-gnome/releases/tag/4.3.3
    
    Ruby-GNOME 4.3.3: 2025-08-17
    
    This is a graphene1 require path fix release.
    
    Changes
    
     Ruby/Graphene1
    
      * Improvements
          + Improved Graphene::Size API.
    
      * Fixes
          + Renamed require path to graphene1 from graphene.
    
     Ruby/GSK4
    
      * Improvements
          + Improved Gsk::RoundedRect API.
    
     Ruby/GObjectIntrospection
    
      * Improvements
          + Added support for try_convert protocol for no GTyped
            structs.

--- graphics/ruby-gdk3/buildlink3.mk
+++ graphics/ruby-gdk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.33 2025/08/30 22:45:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2025/09/12 14:04:48 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gdk3
 
 .if !defined(RUBY_GDK3_BUILDLINK3_MK)
 RUBY_GDK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.3.2
-BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.3.2nb1
+BUILDLINK_API_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.3.3
+BUILDLINK_ABI_DEPENDS.ruby-gdk3+=	${RUBY_PKGPREFIX}-gdk3>=4.3.3
 BUILDLINK_PKGSRCDIR.ruby-gdk3?=		../../graphics/ruby-gdk3
 
 .include "../../devel/ruby-pango/buildlink3.mk"
--- x11/ruby-gtk3/buildlink3.mk
+++ x11/ruby-gtk3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.32 2025/08/30 22:46:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2025/09/12 14:04:49 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gtk3
 
 .if !defined(RUBY_GTK3_BUILDLINK3_MK)
 RUBY_GTK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.3.2
-BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.3.2nb1
+BUILDLINK_API_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.3.3
+BUILDLINK_ABI_DEPENDS.ruby-gtk3+=	${RUBY_PKGPREFIX}-gtk3>=4.3.3
 BUILDLINK_PKGSRCDIR.ruby-gtk3?=		../../x11/ruby-gtk3
 
 .include "../../graphics/ruby-gdk3/buildlink3.mk"
