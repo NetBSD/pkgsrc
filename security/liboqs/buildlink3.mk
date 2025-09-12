20bfe3e742cfea0a13b8416e4bb5b68316152983
jschauma <jschauma@pkgsrc.org>
2025-09-12 21:07:54 +0000

    bump required version to 0.14.0 in preparation for oqs-provider update

--- security/liboqs/buildlink3.mk
+++ security/liboqs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/02/05 02:46:30 jschauma Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/09/12 21:07:54 jschauma Exp $
 
 BUILDLINK_TREE+=	liboqs
 
 .if !defined(LIBOQS_BUILDLINK3_MK)
 LIBOQS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liboqs+=	liboqs>=0.12.0
+BUILDLINK_API_DEPENDS.liboqs+=	liboqs>=0.14.0
 BUILDLINK_PKGSRCDIR.liboqs?=	../../security/liboqs
 
 .endif	# LIBOQS_BUILDLINK3_MK
