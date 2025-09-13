e57b36904d04545041c5e5e5ead6fd6552941db2
vins <vins@pkgsrc.org>
2025-09-13 16:19:20 +0000

    opensmtpd-filter-dnsbl: update patch to reflect net/libasr update
    Remove event_asr_* compat code from main.c. Use libasr built-in
    implementation.

--- mail/opensmtpd-filter-dnsbl/patches/patch-main.c
+++ mail/opensmtpd-filter-dnsbl/patches/patch-main.c
@@ -1,10 +1,8 @@
-$NetBSD: patch-main.c,v 1.2 2023/09/08 21:05:37 vins Exp $
+$NetBSD: patch-main.c,v 1.3 2025/09/13 16:19:20 vins Exp $
 
 * Provide an implementation of recallocarray().
-* Provide compatibility functions for OpenBSD's libevent
-  (pulled from OpenSMTD/openbsd-compat).
 
---- main.c.orig	2021-10-27 09:33:56.000000000 +0000
+--- main.c.orig	2025-05-19 19:01:24.000000000 +0000
 +++ main.c
 @@ -1,5 +1,6 @@
  /*
@@ -13,7 +11,7 @@ $NetBSD: patch-main.c,v 1.2 2023/09/08 21:05:37 vins Exp $
   *
   * Permission to use, copy, modify, and distribute this software for any
   * purpose with or without fee is hereby granted, provided that the above
-@@ -30,6 +31,104 @@
+@@ -30,6 +31,42 @@
  #include <asr.h>
  
  #include "opensmtpd.h"
@@ -52,68 +50,6 @@ $NetBSD: patch-main.c,v 1.2 2023/09/08 21:05:37 vins Exp $
 +	free(ptr);
 +
 +	return newptr;
-+}
-+
-+struct event_asr {
-+	struct event	 ev;
-+	struct asr_query *async;
-+	void		(*cb)(struct asr_result *, void *);
-+	void		*arg;
-+};
-+
-+struct event_asr * event_asr_run(struct asr_query *,
-+    void (*)(struct asr_result *, void *), void *);
-+void event_asr_abort(struct event_asr *);
-+
-+static void
-+event_asr_dispatch(int fd __attribute__((__unused__)),
-+    short ev __attribute__((__unused__)), void *arg)
-+{
-+	struct event_asr	*eva = arg;
-+	struct asr_result	 ar;
-+	struct timeval		 tv;
-+
-+	event_del(&eva->ev);
-+
-+	if (asr_run(eva->async, &ar)) {
-+		eva->cb(&ar, eva->arg);
-+		free(eva);
-+	} else {
-+		event_set(&eva->ev, ar.ar_fd,
-+		    ar.ar_cond == ASR_WANT_READ ? EV_READ : EV_WRITE,
-+		    event_asr_dispatch, eva);
-+		tv.tv_sec = ar.ar_timeout / 1000;
-+		tv.tv_usec = (ar.ar_timeout % 1000) * 1000;
-+		event_add(&eva->ev, &tv);
-+	}
-+}
-+
-+struct event_asr *
-+event_asr_run(struct asr_query *async, void (*cb)(struct asr_result *, void *),
-+    void *arg)
-+{
-+	struct event_asr *eva;
-+	struct timeval tv;
-+
-+	eva = calloc(1, sizeof *eva);
-+	if (eva == NULL)
-+		return (NULL);
-+	eva->async = async;
-+	eva->cb = cb;
-+	eva->arg = arg;
-+	tv.tv_sec = 0;
-+	tv.tv_usec = 0;
-+	evtimer_set(&eva->ev, event_asr_dispatch, eva);
-+	evtimer_add(&eva->ev, &tv);
-+	return (eva);
-+}
-+
-+void
-+event_asr_abort(struct event_asr *eva)
-+{
-+	asr_abort(eva->async);
-+	event_del(&eva->ev);
-+	free(eva);
 +}
  
  struct dnsbl_session;
