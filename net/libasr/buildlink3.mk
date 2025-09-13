618b6e97f8b4e24e1c969230a1d7340dca6ac15e
vins <vins@pkgsrc.org>
2025-09-13 16:16:21 +0000

    net/libasr: bump version to 20250910
    The package now includes support for the event_asr_* functions found in
    OpenBSD's libevent. This makes the present standalone implementation of
    libasr more flexible when porting software from OpenBSD.

--- net/libasr/buildlink3.mk
+++ net/libasr/buildlink3.mk
@@ -1,12 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/09/10 10:34:02 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/09/13 16:16:21 vins Exp $
 
 BUILDLINK_TREE+=	libasr
 
 .if !defined(LIBASR_BUILDLINK3_MK)
 LIBASR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libasr+=	libasr>=20240904
+BUILDLINK_API_DEPENDS.libasr+=	libasr>=20250910
 BUILDLINK_PKGSRCDIR.libasr?=	../../net/libasr
+
+BUILDLINK_LIBNAME.libasr=	asr
+BUILDLINK_LDADD.libasr+=	${BUILDLINK_LIBNAME.libasr:S/^/-l/:S/^-l$//}
+
+.include "../../devel/libevent/buildlink3.mk"
+BUILDLINK_LDADD.libasr+=        ${BUILDLINK_LDADD.libevent}
+
 .endif	# LIBASR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libasr
