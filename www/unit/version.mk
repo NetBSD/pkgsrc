8172bda07f84172c5e996969e0827c7f4e176b96
osa <osa@pkgsrc.org>
2025-09-18 11:19:05 +0000

    */*unit*: update NGINX Unit: 1.34.2 -> 1.35.0
    Going forward, NGINX Unit will no longer receive new features or
    enhancements from the project maintainers.
    
    NGINX Unit is still open to community contributions.
    
    The team is focusing its limited capacity to keep NGINX Unit stable,
    secure and high-quality. Its focus will be on critical maintenance, such
    as for security issues, dependency upgrades, and resolving crashes or
    undefined behavior.
    
    <ChangeLog>
    
    *) Security: fix missing websocket payload length validation in the Java
       language module which could lead to Java language module processes
       consuming excess CPU. (CVE-2025-1695).
    
    *) Change: if building with njs, version 0.9.0 or later is now required.
    
    *) Feature: HTTP compression.
    
    *) Feature: PHP 8.5 compatibility.
    
    *) Feature: Ruby 3.4 compatibility.
    
    *) Feature: Django 5.x compatibility.
    
    *) Feature: Python Litestar WebSockets compatibility.
    
    *) Feature: GCC 15 compatibility.
    
    *) Bugfix: set SERVER_PORT to the actual value.
    
    *) Bugfix: fix issue in node.js with duplicate headers in response.
    
    *) Bugfix: fix WebSockets with Firefox.
    
    *) Bugfix: fix incorrect websocket payload length calculation in the
       Java language module.
    
    *) Bugfix: fix instability issues due to OpenTelemetry (OTEL) support.
    
    *) Bugfix: fix issues with building OpenTelemetry (OTEL) support on
       various platforms, including macOS.
    
    </ChangeLog>

--- www/unit/version.mk
+++ www/unit/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.16 2025/03/03 19:58:25 osa Exp $
+# $NetBSD: version.mk,v 1.17 2025/09/18 11:19:05 osa Exp $
 #
 # UNIT_EXTENSION_DIR
 #	Relative path to ${PREFIX} for NGINX Unit's modules.
@@ -12,7 +12,7 @@
 UNITVERSION_MK=	defined
 
 # Define NGINX Unit's version.
-UNIT_VERSION=	1.34.2
+UNIT_VERSION=	1.35.0
 
 # Define NGINX Unit's modules directory
 UNIT_EXTENSION_DIR=	libexec/unit/modules
