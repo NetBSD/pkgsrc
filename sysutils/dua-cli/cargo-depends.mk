1e8e9db8631cda4eb5487cf43fbaa894440ad330
pin <pin@pkgsrc.org>
2025-09-19 13:44:29 +0000

    sysutils/dua-cli: update to 2.32.0
    New Features
    
     - Ctrl+f in the glob prompt now toggles the mode from case-insensitive to sensitive.

--- sysutils/dua-cli/cargo-depends.mk
+++ sysutils/dua-cli/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.31 2025/08/02 14:26:14 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.32 2025/09/19 13:44:29 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
