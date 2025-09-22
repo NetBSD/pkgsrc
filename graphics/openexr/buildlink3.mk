55eb3dffe08721b77e8e7e710a3dd471f1267c4d
wiz <wiz@pkgsrc.org>
2025-09-22 07:49:50 +0000

    openexr: needs to provide OpenJPH headers

--- graphics/openexr/buildlink3.mk
+++ graphics/openexr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2025/09/21 15:11:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2025/09/22 07:49:50 wiz Exp $
 
 BUILDLINK_TREE+=	openexr
 
@@ -14,6 +14,7 @@ PTHREAD_OPTS+=	require
 USE_CXX_FEATURES+=	c++17 filesystem
 
 .include "../../devel/libdeflate/buildlink3.mk"
+.include "../../graphics/OpenJPH/buildlink3.mk"
 .include "../../math/imath/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # OPENEXR_BUILDLINK3_MK
