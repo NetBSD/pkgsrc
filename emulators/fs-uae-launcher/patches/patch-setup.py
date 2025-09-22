67fd0b8d9e97de66bedf3fbdb5945200cb6527d6
adam <adam@pkgsrc.org>
2025-09-22 10:36:14 +0000

    fs-uae-launcher: updated to 3.2.35
    Version 3.2.35:
    
    Dark mode fix for Windows (more reliable detection of dark mode).
    Bumping version to 3.2.35 to synchronize version with FS-UAE.
    
    Version 3.2.23:
    
    Minor workaround for macOS 12 running on too old hardware (?)

--- emulators/fs-uae-launcher/patches/patch-setup.py
+++ emulators/fs-uae-launcher/patches/patch-setup.py
@@ -1,16 +1,26 @@
-$NetBSD: patch-setup.py,v 1.7 2025/09/14 10:29:34 adam Exp $
+$NetBSD: patch-setup.py,v 1.8 2025/09/22 10:36:14 adam Exp $
 
+Fix version number.
 Fix installation on Darwin.
 
---- setup.py.orig	2025-09-06 22:20:15.000000000 +0000
+--- setup.py.orig	2025-09-18 15:34:18.000000000 +0000
 +++ setup.py
+@@ -20,7 +20,7 @@ title = "FS-UAE Launcher"
+ name = "fs-uae-launcher"
+ py_name = "fs_uae_launcher"
+ tar_name = "fs-uae-launcher"
+-version = "3.2.20"
++version = "3.2.35"
+ author = "Frode Solheim"
+ author_email = "frode@fs-uae.net"
+ package_map = {
 @@ -178,10 +178,6 @@ if sys.argv[1] == "build_exe":
  if sys.platform == "win32" and False:
      setup_kwargs["windows"] = scripts
  
 -if sys.platform == "darwin":
 -    setup_kwargs["name"] = title
--    setup_kwargs["version"] = "3.2.20"
+-    setup_kwargs["version"] = "3.2.35"
 -else:
 -    setup_kwargs["scripts"] = scripts
 +setup_kwargs["scripts"] = scripts
