9f9fa54b4314b24b3a02837ce6a7efd7b5764cbd
kikadf <kikadf@pkgsrc.org>
2025-09-23 11:39:46 +0000

    www/chromium: update to 140.0.7339.185
    * 140.0.7339.185
    This update includes 4 security fixes. Below, we highlight fixes
    that were contributed by external researchers. Please see the
    Chrome Security Page for more information.
    [NA][445380761] High CVE-2025-10585: Type Confusion in V8.
    Reported by Google Threat Analysis Group on 2025-09-16
    [$15000][435875050] High CVE-2025-10500: Use after free in Dawn.
    Reported by Giunash (Gyujeong Jin) on 2025-08-03
    [$10000][440737137] High CVE-2025-10501: Use after free in WebRTC.
    Reported by sherkito on 2025-08-23
    [TBD][438038775] High CVE-2025-10502: Heap buffer overflow in ANGLE.
    Reported by Google Big Sleep on 2025-08-12
    
    Google is aware that an exploit for CVE-2025-10585 exists in the wild.

--- www/chromium/patches/patch-net_tools_net__watcher_net__watcher.cc
+++ www/chromium/patches/patch-net_tools_net__watcher_net__watcher.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-net_tools_net__watcher_net__watcher.cc,v 1.7 2025/09/12 16:02:32 kikadf Exp $
+$NetBSD: patch-net_tools_net__watcher_net__watcher.cc,v 1.8 2025/09/23 11:39:46 kikadf Exp $
 
 * Part of patchset to build chromium on NetBSD
 * Based on OpenBSD's chromium patches, and
   pkgsrc's qt5-qtwebengine patches
 
---- net/tools/net_watcher/net_watcher.cc.orig	2025-09-08 23:21:33.000000000 +0000
+--- net/tools/net_watcher/net_watcher.cc.orig	2025-09-16 20:11:32.000000000 +0000
 +++ net/tools/net_watcher/net_watcher.cc
 @@ -31,7 +31,7 @@
  #include "net/proxy_resolution/proxy_config_service.h"
@@ -24,15 +24,6 @@ $NetBSD: patch-net_tools_net__watcher_net__watcher.cc,v 1.7 2025/09/12 16:02:32 
  // Flag to specifies which network interfaces to ignore. Interfaces should
  // follow as a comma seperated list.
  const char kIgnoreNetifFlag[] = "ignore-netif";
-@@ -143,7 +143,7 @@ class NetWatcher :
- }  // namespace
- 
- int main(int argc, char* argv[]) {
--#if BUILDFLAG(IS_APPLE)
-+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
-   base::apple::ScopedNSAutoreleasePool pool;
- #endif
-   base::AtExitManager exit_manager;
 @@ -160,7 +160,7 @@ int main(int argc, char* argv[]) {
  
    NetWatcher net_watcher;
