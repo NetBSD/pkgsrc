ea3c45850f3e718ba1bfa9329d90865d2c410883
taca <taca@pkgsrc.org>
2025-09-23 15:41:34 +0000

    devel/php-raphf2: update to 2.0.2
    pkgsrc change: fix buildlinke3.mk with pkglint.
    
    2.0.2 (2025-08-29)
    
    * Merged gh#9: fix format, used is unsigned long (Remi)

--- devel/php-raphf/buildlink3.mk
+++ devel/php-raphf/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/03/12 08:33:12 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/09/23 15:41:34 taca Exp $
 
-.include "../../lang/php/phpversion.mk"
+BUILDLINK_TREE+=	php-raphf
 
-BUILDLINK_TREE+=	${PHP_PKG_PREFIX}-raphf
+.if !defined(PHP_RAPHF_BUILDLINK3_MK)
+PHP_RAPHF_BUILDLINK3_MK:=
 
-.if !defined(${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK)
-${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK:=
+BUILDLINK_API_DEPENDS.php-raphf+=	${PHP_PKG_PREFIX}-raphf>=1.0.4nb1<2
+BUILDLINK_PKGSRCDIR.php-raphf?=		../../devel/php-raphf
 
-BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-raphf+=	${PHP_PKG_PREFIX}-raphf>=1.0.4nb1<2
-BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-raphf?=	../../devel/php-raphf
-.endif	# ${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK
+.endif	# PHP_RAPHF_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-${PHP_PKG_PREFIX}-raphf
+BUILDLINK_TREE+=	-php-raphf
