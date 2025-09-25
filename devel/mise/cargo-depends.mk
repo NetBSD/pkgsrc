92860165c3f019e6a1cbc2cdc2a54d038e2587d4
pin <pin@pkgsrc.org>
2025-09-25 06:56:46 +0000

    devel/mise: update to 2025.9.18
    ## [2025.9.18](https://github.com/jdx/mise/compare/v2025.9.17..v2025.9.18) - 2025-09-24
    
    ### 📦 Registry
    
    - replace amplify-cli github backend with ubi by @eggplants in [#6396](https://github.com/jdx/mise/pull/6396)
    
    ### 🚀 Features
    
    - **(template)** add read_file() function by @jdx in [#6400](https://github.com/jdx/mise/pull/6400)
    
    ### 🐛 Bug Fixes
    
    - **(aqua)** support github_artifact_attestations.enabled by @risu729 in [#6372](https://github.com/jdx/mise/pull/6372)
    - use /c instead of -c on windows in postinstall hook by @risu729 in [#6397](https://github.com/jdx/mise/pull/6397)
    
    ### 🧪 Testing
    
    - **(test-tool)** uninstall all versions and clear cache before installation by @jdx in [#6393](https://github.com/jdx/mise/pull/6393)
    
    ### New Contributors
    
    - @eggplants made their first contribution in [#6396](https://github.com/jdx/mise/pull/6396)
    
    ## [2025.9.17](https://github.com/jdx/mise/compare/v2025.9.16..v2025.9.17) - 2025-09-24
    
    ### 🚀 Features
    
    - **(java)** add support for Liberica NIK releases by @roele in [#6382](https://github.com/jdx/mise/pull/6382)
    
    ### 🐛 Bug Fixes
    
    - **(toolset)** handle underflow in version_sub function by @koh-sh in [#6389](https://github.com/jdx/mise/pull/6389)
    
    ### 📚 Documentation
    
    - document MISE_ENV behavior for global/system configs by @jdx in [#6385](https://github.com/jdx/mise/pull/6385)
    
    ### New Contributors
    
    - @jc00ke made their first contribution in [#6386](https://github.com/jdx/mise/pull/6386)
    - @koh-sh made their first contribution in [#6389](https://github.com/jdx/mise/pull/6389)

--- devel/mise/cargo-depends.mk
+++ devel/mise/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.92 2025/09/24 11:56:20 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.93 2025/09/25 06:56:46 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
