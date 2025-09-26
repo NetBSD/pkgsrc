1d9eb6cbca9a47ed0247df948786950deea797d3
ryoon <ryoon@pkgsrc.org>
2025-09-26 23:15:49 +0000

    www/firefox: Update to 143.0.1
    Changelog:
    143.0.1:
    Fixed
    
      * Fixed a tab crash experienced by some users caused by DLL injection. (Bug
        1872261)
    
    
    143.0:
    New
    
      * On Windows, Firefox now supports running websites as web apps pinned
        directly to the taskbar. These are sites that you can pin and run as
        simplified windows directly from the taskbar without losing access to your
        installed add-ons. This feature is not currently available for Firefox
        installs from the Microsoft Store.
    
      * Tabs can now be pinned by dragging them to the start of the tab strip,
        making it easier to keep important sites within reach.
    
      * Copilot from Microsoft can now be chosen as a chatbot to use in the sidebar
        for quick access without leaving your main view.
    
      * When a site asks for camera access, it can now be previewed inside the
        permission dialog. This is especially helpful when switching between
        multiple cameras.
    
      * The Firefox address bar can now show you important dates and events. This
        feature supports displaying events (e.g. ??Mother??s Day??) in the United
        States, United Kingdom, Germany, France, and Italy regions.
    
      * Firefox has expanded its Fingerprinting Protection by reporting constant
        values for several more attributes of user's computers.
    
      * When downloading a file in Private Browsing mode, Firefox now asks whether
        to keep or delete it after that session ends. You can adjust this behavior
        in Settings.
    
      * Firefox now supports Windows UI Automation, which improves support for
        accessibility tools such as Windows Voice Access, Text Cursor Indicator and
        Narrator.
    
    Fixed
    
      * Various security fixes.
    
    Security fixes:
    Mozilla Foundation Security Advisory 2025-73
    #CVE-2025-10527: Sandbox escape due to use-after-free in the Graphics: Canvas2D
     component
    #CVE-2025-10528: Sandbox escape due to undefined behavior, invalid pointer in
     the Graphics: Canvas2D component
    #CVE-2025-10529: Same-origin policy bypass in the Layout component
    #CVE-2025-10530: Spoofing issue in the WebAuthn component in Firefox for
     Android
    #CVE-2025-10531: Mitigation bypass in the Web Compatibility: Tooling component
    #CVE-2025-10532: Incorrect boundary conditions in the JavaScript: GC component
    #CVE-2025-10533: Integer overflow in the SVG component
    #CVE-2025-10534: Spoofing issue in the Site Permissions component
    #CVE-2025-10535: Information disclosure, mitigation bypass in the Privacy
     component in Firefox for Android
    #CVE-2025-10536: Information disclosure in the Networking: Cache component
    #CVE-2025-10537: Memory safety bugs fixed in Firefox ESR 140.3, Thunderbird ESR
     140.3, Firefox 143 and Thunderbird 143

--- www/firefox/patches/patch-third__party_libwebrtc_rtc__base_memory__usage.cc
+++ www/firefox/patches/patch-third__party_libwebrtc_rtc__base_memory__usage.cc
@@ -1,22 +1,23 @@
-$NetBSD: patch-third__party_libwebrtc_rtc__base_memory__usage.cc,v 1.3 2025/07/26 14:12:54 ryoon Exp $
+$NetBSD: patch-third__party_libwebrtc_rtc__base_memory__usage.cc,v 1.4 2025/09/26 23:15:49 ryoon Exp $
 
---- third_party/libwebrtc/rtc_base/memory_usage.cc.orig	2025-05-21 12:27:20.023110519 +0000
+--- third_party/libwebrtc/rtc_base/memory_usage.cc.orig	2025-09-10 12:13:46.353394466 +0000
 +++ third_party/libwebrtc/rtc_base/memory_usage.cc
-@@ -10,7 +10,7 @@
+@@ -12,7 +12,7 @@
  
- #include "rtc_base/memory_usage.h"
+ #include <cstdint>
  
 -#if defined(WEBRTC_LINUX)
 +#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
  #include <unistd.h>
  
  #include <cstdio>
-@@ -32,7 +32,7 @@
- namespace rtc {
+@@ -34,7 +34,8 @@
+ namespace webrtc {
  
  int64_t GetProcessResidentSizeBytes() {
 -#if defined(WEBRTC_LINUX)
 +#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
++ #include <unistd.h>
    FILE* file = fopen("/proc/self/statm", "r");
    if (file == nullptr) {
      RTC_LOG(LS_ERROR) << "Failed to open /proc/self/statm";
