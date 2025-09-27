643be36403241ebe44bb597a9fb3f4a654ac8b24
ryoon <ryoon@pkgsrc.org>
2025-09-27 00:55:03 +0000

    mail/thunderbird: Update to 143.0.1
    Changelog:
    143.0.1:
    fixed
    Thunderbird could crash on startup
    
    
    143.0:
    What's Fixed
    fixed
    Web pages with bad certificates displayed as blank
    
    fixed
    Users could no longer send using smtp-relay.gmail.com
    
    fixed
    Changing to new drafts folder and then back did not correctly restore the
    folder
    
    fixed
    Option did not exist to create new address book under File -> New
    
    fixed
    Some folders showed new mail count prior to receiving mail
    
    fixed
    Menu bar was hidden after updating from 128esr to 140esr
    
    fixed
    Menu Bar -> View contained duplicate accelerator keys
    
    fixed
    Warning was not logged if mail.openpgp.alias_rules_file file did not exist
    
    fixed
    Cmd+Shift+F no longer invoked "Search Messages" on macOS
    
    fixed
    Arrow key selection in global search bar skipped every other result
    
    fixed
    Drag-drop of unselected contact inserted wrong or no email address
    
    fixed
    Thunderbird could crash on startup
    
    fixed
    Adding an account could fail and cause UI hang
    
    fixed
    Thunderbird could crash when importing mail
    
    fixed
    Windows new message notification click did not bring Thunderbird to foreground
    
    fixed
    Delete or detach attachments did not work for some IMAP servers
    
    fixed
    Saving a new draft retained superceded version
    
    fixed
    Language field in settings was empty after restart in Troubleshoot Mode
    
    fixed
    Visual and UX improvements
    
    fixed
    Security fixes
    
    Security fixes:
    Mozilla Foundation Security Advisory 2025-77
    #CVE-2025-10527: Sandbox escape due to use-after-free in the Graphics: Canvas2D
     component
    #CVE-2025-10528: Sandbox escape due to undefined behavior, invalid pointer in
     the Graphics: Canvas2D component
    #CVE-2025-10529: Same-origin policy bypass in the Layout component
    #CVE-2025-10530: Spoofing issue in the WebAuthn component in Firefox for
     Android
    #CVE-2025-10531: Mitigation bypass in the Web Compatibility: Tooling component
    #CVE-2025-10532: Incorrect boundary conditions in the JavaScript: GC component
    #CVE-2025-10533: Integer overflow in the SVG component
    #CVE-2025-10534: Spoofing issue in the Site Permissions component
    #CVE-2025-10536: Information disclosure in the Networking: Cache component
    #CVE-2025-10537: Memory safety bugs fixed in Firefox ESR 140.3, Thunderbird ESR
     140.3, Firefox 143 and Thunderbird 143

---
+++ mail/thunderbird/patches/patch-third__party_libwebrtc_rtc__base_memory__usage.cc
@@ -0,0 +1,23 @@
+$NetBSD: patch-third__party_libwebrtc_rtc__base_memory__usage.cc,v 1.1 2025/09/27 00:55:04 ryoon Exp $
+
+--- third_party/libwebrtc/rtc_base/memory_usage.cc.orig	2025-09-10 12:13:46.353394466 +0000
++++ third_party/libwebrtc/rtc_base/memory_usage.cc
+@@ -12,7 +12,7 @@
+ 
+ #include <cstdint>
+ 
+-#if defined(WEBRTC_LINUX)
++#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
+ #include <unistd.h>
+ 
+ #include <cstdio>
+@@ -34,7 +34,8 @@
+ namespace webrtc {
+ 
+ int64_t GetProcessResidentSizeBytes() {
+-#if defined(WEBRTC_LINUX)
++#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
++ #include <unistd.h>
+   FILE* file = fopen("/proc/self/statm", "r");
+   if (file == nullptr) {
+     RTC_LOG(LS_ERROR) << "Failed to open /proc/self/statm";
