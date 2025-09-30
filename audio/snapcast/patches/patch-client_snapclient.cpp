d3452b85b007d6a81d0b535a3567167f2d336603
adam <adam@pkgsrc.org>
2025-09-30 06:49:37 +0000

    snapcast: updated to 0.33.0
    0.33.0
    
    Features
    
    Server: Add PipeWire stream reader
    Client: Add PipeWire player
    Server: process stream sources can be added via RPC
    Server: mDNS can be disabled
    Server: Audio streaming over plain TCP (port 1704) can be disabled
    Server: Add control script for go-librespot
    
    Bugfixes
    
    Client: Fix case of command line arguments
    Client: Fix deadlock during shutdown
    Fix building with boost v1.89.0
    Fix MacOS CI
    
    General
    
    Server: Log warning if default config is missing
    Server: Make SSL support optional (-DBUILD_WITH_SSL)
    Make ALSA optional
    Enable OpenSSL support for Android builds in CMakeLists.txt
    Update Readme
    Build Trixie packages for amd64
    tcp section in snapserver.conf renamed to tcp-control
    TCP streaming settings moved from stream to tcp-streaming

--- audio/snapcast/patches/patch-client_snapclient.cpp
+++ audio/snapcast/patches/patch-client_snapclient.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-client_snapclient.cpp,v 1.1 2022/07/03 16:09:15 nia Exp $
+$NetBSD: patch-client_snapclient.cpp,v 1.2 2025/09/30 06:49:37 adam Exp $
 
 Add Sun Audio support for NetBSD.
 
---- client/snapclient.cpp.orig	2021-12-22 17:40:36.000000000 +0000
+--- client/snapclient.cpp.orig	2025-09-23 19:02:05.000000000 +0000
 +++ client/snapclient.cpp
-@@ -32,6 +32,9 @@
+@@ -28,6 +28,9 @@
  #ifdef HAS_PULSE
  #include "player/pulse_player.hpp"
  #endif
@@ -14,39 +14,34 @@ Add Sun Audio support for NetBSD.
  #ifdef HAS_WASAPI
  #include "player/wasapi_player.hpp"
  #endif
-@@ -62,8 +65,12 @@ PcmDevice getPcmDevice(const std::string
+@@ -73,7 +76,7 @@ namespace
+ PcmDevice getPcmDevice(const std::string& player, const std::string& parameter, const std::string& soundcard)
  {
-     LOG(DEBUG, LOG_TAG) << "Trying to get PCM device for player: " << player << ", parameter: "
-                         << ", card: " << soundcard << "\n";
--#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI)
-+#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_SUN)
+     LOG(DEBUG, LOG_TAG) << "Trying to get PCM device for player: " << player << ", parameter: " << ", card: " << soundcard << "\n";
+-#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_PIPEWIRE)
++#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_PIPEWIRE) || defined(HAS_SUN)
      vector<PcmDevice> pcm_devices;
-+#if defined(HAS_SUN)
-+    if (player == player::SUN)
-+        pcm_devices = SunPlayer::pcm_list();
-+#endif
  #if defined(HAS_ALSA)
      if (player == player::ALSA)
-         pcm_devices = AlsaPlayer::pcm_list();
-@@ -142,7 +149,7 @@ int main(int argc, char** argv)
-         op.add<Value<string>>("", "hostID", "unique host id, default is MAC address", "", &settings.host_id);
+@@ -186,7 +189,7 @@ int main(int argc, char** argv)
+             op.add<Implicit<std::filesystem::path>>("", "server-cert", "Verify server with CA certificate (PEM format)", "default certificates");
  
  // PCM device specific
--#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI)
-+#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_SUN)
-         auto listSwitch = op.add<Switch>("l", "list", "list PCM devices");
-         /*auto soundcardValue =*/op.add<Value<string>>("s", "soundcard", "index or name of the pcm device", pcm_device, &pcm_device);
+-#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_PIPEWIRE)
++#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_PIPEWIRE) || defined(HAS_SUN)
+         auto listSwitch = op.add<Switch>("l", "list", "List PCM devices");
+         op.add<Value<string>>("s", "soundcard", "Index or name of the PCM device", pcm_device, &pcm_device);
  #endif
-@@ -210,7 +217,7 @@ int main(int argc, char** argv)
+@@ -258,7 +261,7 @@ int main(int argc, char** argv)
  
          settings.player.player_name = utils::string::split_left(settings.player.player_name, ':', settings.player.parameter);
  
--#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI)
-+#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_SUN)
+-#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_PIPEWIRE)
++#if defined(HAS_ALSA) || defined(HAS_PULSE) || defined(HAS_WASAPI) || defined(HAS_PIPEWIRE) || defined(HAS_SUN)
          if (listSwitch->is_set())
          {
              try
-@@ -224,6 +231,10 @@ int main(int argc, char** argv)
+@@ -272,6 +275,10 @@ int main(int argc, char** argv)
                  if (settings.player.player_name == player::PULSE)
                      pcm_devices = PulsePlayer::pcm_list(settings.player.parameter);
  #endif
