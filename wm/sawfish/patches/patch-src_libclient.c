12fcf062c25b2ca90e93ac5bdda20b38210f0105
gutteridge <gutteridge@pkgsrc.org>
2025-09-30 14:32:24 +0000

    sawfish: use ctype.h correctly

---
+++ wm/sawfish/patches/patch-src_libclient.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_libclient.c,v 1.1 2025/09/30 14:32:24 gutteridge Exp $
+
+Use ctype.h correctly.
+
+--- src/libclient.c.orig	2021-12-24 19:22:59.000000000 +0000
++++ src/libclient.c
+@@ -130,7 +130,7 @@ canonical_host (char *host)
+     ptr = buf;
+     while (*host != 0)
+     {
+-	*ptr++ = tolower (*host);
++	*ptr++ = tolower ((unsigned char)*host);
+ 	host++;
+     }
+     return buf;
