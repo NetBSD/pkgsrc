52b57e487c253309a30af080d9580b5c093fc4bc
pin <pin@pkgsrc.org>
2025-10-01 09:34:31 +0000

    shells/oh-my-posh: update to 26.26.1
    v26.26.1
    Bug Fixes
    
     - upgrade: always check for upgrade when enabled via configuration (80cc9a6), closes #6775
    
    v26.26.0
    Bug Fixes
    
     - windows: disable upgrade for MSIX (5a8a777)
    
    Features
    
     - toggle: allow toggling from config (1f78d5c), closes #6816
     - winget: add msix package (f47754a)
    
    Reverts
    
     - http: use Google DNS by default (c04d9d1), closes #6818

--- shells/oh-my-posh/go-modules.mk
+++ shells/oh-my-posh/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.106 2025/09/19 13:50:13 pin Exp $
+# $NetBSD: go-modules.mk,v 1.107 2025/10/01 09:34:31 pin Exp $
 
 GO_MODULE_FILES+=	dario.cat/mergo/@v/v1.0.2.mod
 GO_MODULE_FILES+=	dario.cat/mergo/@v/v1.0.2.zip
@@ -99,8 +99,8 @@ GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.mod
 GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.zip
 GO_MODULE_FILES+=	github.com/mattn/go-localereader/@v/v0.0.1.mod
 GO_MODULE_FILES+=	github.com/mattn/go-localereader/@v/v0.0.1.zip
-GO_MODULE_FILES+=	github.com/mattn/go-runewidth/@v/v0.0.16.mod
-GO_MODULE_FILES+=	github.com/mattn/go-runewidth/@v/v0.0.16.zip
+GO_MODULE_FILES+=	github.com/mattn/go-runewidth/@v/v0.0.17.mod
+GO_MODULE_FILES+=	github.com/mattn/go-runewidth/@v/v0.0.17.zip
 GO_MODULE_FILES+=	github.com/mitchellh/copystructure/@v/v1.2.0.mod
 GO_MODULE_FILES+=	github.com/mitchellh/copystructure/@v/v1.2.0.zip
 GO_MODULE_FILES+=	github.com/mitchellh/go-wordwrap/@v/v1.0.1.mod
