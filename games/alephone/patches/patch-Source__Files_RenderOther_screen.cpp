d4ce7991a07dace07b4fe2c75f4ee6687e750415
adam <adam@pkgsrc.org>
2025-10-02 11:19:51 +0000

    alephone: updated to 20250829
    Aleph One 1.11
    
    This release is not network compatible with the previous release, and films saved with this release cannot be played back with older releases.
    
    Highlights:
    
    Native Steam Linux support
    Audio improvements
    Additional sprite rendering option for 3D perspective
    Replay films for saved games

--- games/alephone/patches/patch-Source__Files_RenderOther_screen.cpp
+++ games/alephone/patches/patch-Source__Files_RenderOther_screen.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-Source__Files_RenderOther_screen.cpp,v 1.1 2023/04/27 13:33:47 yhardy Exp $
+$NetBSD: patch-Source__Files_RenderOther_screen.cpp,v 1.2 2025/10/02 11:19:51 adam Exp $
 
 Use GLEW for GL extensions, works around a linking problem on NetBSD.
 
---- Source_Files/RenderOther/screen.cpp.orig	2021-10-31 00:46:41.000000000 +0000
+--- Source_Files/RenderOther/screen.cpp.orig	2025-08-29 15:31:33.000000000 +0000
 +++ Source_Files/RenderOther/screen.cpp
-@@ -963,7 +963,7 @@ static void change_screen_mode(int width
+@@ -967,7 +967,7 @@ static void change_screen_mode(int width
  			SDL_GL_CreateContext(main_screen);
  			context_created = true;
  		}
--#ifdef __WIN32__
-+#if 1
+-#if defined (__WIN32__) && (HAVE_OPENGL)
++#if HAVE_OPENGL
  		glewInit();
  #endif
  		if (!OGL_CheckExtension("GL_ARB_vertex_shader") || !OGL_CheckExtension("GL_ARB_fragment_shader") || !OGL_CheckExtension("GL_ARB_shader_objects") || !OGL_CheckExtension("GL_ARB_shading_language_100"))
