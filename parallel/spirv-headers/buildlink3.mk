0857a67c73898ce0dd66803e784152ecc8d36f7a
tnn <tnn@pkgsrc.org>
2025-10-04 12:07:45 +0000

    spirv-headers: apply some upstream commits to fix the build of spirv-tools

--- parallel/spirv-headers/buildlink3.mk
+++ parallel/spirv-headers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/11/29 16:25:48 triaxx Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/10/04 12:07:45 tnn Exp $
 
 BUILDLINK_TREE+=	spirv-headers
 
 .if !defined(SPIRV_HEADERS_BUILDLINK3_MK)
 SPIRV_HEADERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spirv-headers+=	spirv-headers>=1.3.216.0
+BUILDLINK_API_DEPENDS.spirv-headers+=	spirv-headers>=1.4.321.0nb1
 BUILDLINK_PKGSRCDIR.spirv-headers?=	../../parallel/spirv-headers
 BUILDLINK_DEPMETHOD.spirv-headers?=	build
 .endif	# SPIRV_HEADERS_BUILDLINK3_MK
