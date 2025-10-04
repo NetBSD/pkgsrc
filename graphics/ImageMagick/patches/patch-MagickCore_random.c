36e51eb7cffc8a2da5effd5ca98dfc0800e79d70
tnn <tnn@pkgsrc.org>
2025-10-04 10:30:48 +0000

    ImageMagick: needs <sys/random.h> for getentropy(2) on macOS

--- graphics/ImageMagick/patches/patch-MagickCore_random.c
+++ graphics/ImageMagick/patches/patch-MagickCore_random.c
@@ -1,15 +1,18 @@
-$NetBSD: patch-MagickCore_random.c,v 1.1 2025/10/01 10:52:58 nia Exp $
+$NetBSD: patch-MagickCore_random.c,v 1.2 2025/10/04 10:30:48 tnn Exp $
 
 Include correct header for getentropy.
 
---- MagickCore/random.c.orig	2025-10-01 10:27:19.831792297 +0000
+--- MagickCore/random.c.orig	2025-08-30 13:35:00.000000000 +0000
 +++ MagickCore/random.c
-@@ -65,7 +65,7 @@
+@@ -65,8 +65,11 @@
  #include "MagickCore/utility.h"
  #include "MagickCore/utility-private.h"
  #if defined(MAGICKCORE_HAVE_GETENTROPY)
--#include <sys/random.h>
 +#include <unistd.h>
++#if defined(__APPLE__)
+ #include <sys/random.h>
  #endif
++#endif
  /*
    Define declarations.
+ */
