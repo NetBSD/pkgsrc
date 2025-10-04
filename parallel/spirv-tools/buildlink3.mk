31affdfd8f88f91f91716f2b4112b4cbb0446b59
tnn <tnn@pkgsrc.org>
2025-10-04 12:14:22 +0000

    spirv-tools: update to 2025.4
    No changelog provided.

--- parallel/spirv-tools/buildlink3.mk
+++ parallel/spirv-tools/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/09/23 13:05:31 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/10/04 12:14:22 tnn Exp $
 
 BUILDLINK_TREE+=	spirv-tools
 
 .if !defined(SPIRV_TOOLS_BUILDLINK3_MK)
 SPIRV_TOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spirv-tools+=	spirv-tools>=2025.1
+BUILDLINK_API_DEPENDS.spirv-tools+=	spirv-tools>=2025.4
 BUILDLINK_PKGSRCDIR.spirv-tools?=	../../parallel/spirv-tools
 BUILDLINK_FILES.spirv-tools?=					\
 					bin/spirv-as		\
 					bin/spirv-cfg		\
+					bin/spirv-diff		\
 					bin/spirv-dis		\
 					bin/spirv-link		\
 					bin/spirv-lint		\
