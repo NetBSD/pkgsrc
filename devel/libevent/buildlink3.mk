deb8c4acf360534f3b654e380b088bbd81064258
js <js@pkgsrc.org>
2025-10-05 02:53:06 +0000

    devel/libevent: Same change for buildlink3.mk

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2023/10/24 22:08:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2025/10/05 02:53:06 js Exp $
 
 BUILDLINK_TREE+=	libevent
 
@@ -9,7 +9,9 @@ BUILDLINK_API_DEPENDS.libevent+=	libevent>=0.6
 BUILDLINK_ABI_DEPENDS.libevent+=	libevent>=2.1.12nb1
 BUILDLINK_PKGSRCDIR.libevent?=		../../devel/libevent
 
+.if ${OPSYS} != "QNX"
 .include "../../security/openssl/buildlink3.mk"
+.endif
 .endif # LIBEVENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libevent
