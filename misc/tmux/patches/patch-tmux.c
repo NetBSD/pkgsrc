4091f97c3fbd1e1544c74a9aeef6fae61f738fd2
js <js@pkgsrc.org>
2025-10-05 10:53:23 +0000

    Make misc/tmux build on QNX
    It only somewhat works so far. It starts and can be used, however it seems to
    not play nice with QNX's terminal yet and there is corruption. It almost looks
    like it gets the terminal size wrong.

---
+++ misc/tmux/patches/patch-tmux.c
@@ -0,0 +1,27 @@
+$NetBSD: patch-tmux.c,v 1.1 2025/10/05 10:53:23 js Exp $
+
+--- tmux.c.orig	2024-10-05 05:57:07.000000000 +0000
++++ tmux.c
+@@ -22,7 +22,9 @@
+ 
+ #include <errno.h>
+ #include <fcntl.h>
+-#include <langinfo.h>
++#ifndef __QNX__
++# include <langinfo.h>
++#endif
+ #include <locale.h>
+ #include <pwd.h>
+ #include <signal.h>
+@@ -361,9 +363,11 @@ main(int argc, char **argv)
+ 	    setlocale(LC_CTYPE, "C.UTF-8") == NULL) {
+ 		if (setlocale(LC_CTYPE, "") == NULL)
+ 			errx(1, "invalid LC_ALL, LC_CTYPE or LANG");
++#ifndef __QNX__
+ 		s = nl_langinfo(CODESET);
+ 		if (strcasecmp(s, "UTF-8") != 0 && strcasecmp(s, "UTF8") != 0)
+ 			errx(1, "need UTF-8 locale (LC_CTYPE) but have %s", s);
++#endif
+ 	}
+ 
+ 	setlocale(LC_TIME, "");
