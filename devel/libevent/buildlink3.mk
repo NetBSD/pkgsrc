e3a499996e801262b49ad18c5e01d8d55c98207c
wiz <wiz@pkgsrc.org>
2025-10-05 07:14:36 +0000

    libevent: make sure OPSYS is defined before checking it

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2025/10/05 02:53:06 js Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2025/10/05 07:14:36 wiz Exp $
 
 BUILDLINK_TREE+=	libevent
 
@@ -8,6 +8,8 @@ LIBEVENT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libevent+=	libevent>=0.6
 BUILDLINK_ABI_DEPENDS.libevent+=	libevent>=2.1.12nb1
 BUILDLINK_PKGSRCDIR.libevent?=		../../devel/libevent
+
+.include "../../mk/bsd.fast.prefs.mk"
 
 .if ${OPSYS} != "QNX"
 .include "../../security/openssl/buildlink3.mk"
