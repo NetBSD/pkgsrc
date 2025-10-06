090aee8be97951846c6b1bd02b92f03558c3a3ab
adam <adam@pkgsrc.org>
2025-10-06 09:34:44 +0000

    cmake cmake-gui: updated to 4.1.2
    CMake 4.1.2
    * Ninja: escape special characters in custom command comments
    * cmCTestCurl: Avoid using undocumented type for CURLOPT_PROXYTYPE values
    * ASM: Restore CMAKE_ASM_COMPILER_ARCHITECTURE_ID persistence
    * Tests: Extend RunCMake.ParseImplicitLinkInfo for LFortran 0.55.0
    * Tests: Teach RunCMake to ignore Xcode an IDERunDestination warning
    * ci: update macOS jobs to use Xcode 26.0
    * cmFileSet: Constify local generator usage
    * cmGeneratorExpressionEvaluationFile: Constify some local generator usage
    * cmGeneratorExpressionNode: Simplify EvaluateDependentExpression signature
    * GenEx: Rename cmGeneratorExpressionContext to cm::GenEx::Evaluation
    * GenEx: Factor out a dedicated evaluation Context structure
    * GenEx: Collect evaluation arguments into local Context structures
    * GenEx: Construct cmGeneratorExpressionDAGChecker with full evaluation context
    * GenEx: Consolidate target property evaluation context arguments
    * Help: Document when policy CMP0189 takes effect
    * cmGeneratorTarget: Pass genex evaluation context to IsTransitiveProperty
    * file(GENERATE): Record CMP0189 at each call site
    * bootstrap: Fix compilation with gcc 16 devirtualization
    * cmGeneratorTarget: Factor out helper to detect LINK_LIBRARIES evaluation
    * CMP0189: Restore support for linking `$<TARGET_PROPERTY:tgt,LINK_LIBRARIES>`
    * LFortran: Update flags renamed by version 0.55
    * GNUInstallDirs: Fix regression on -DCMAKE_INSTALL_<dir>=<default> in /usr
    * cmLocalGenerator: check variables before usage
    * cmPropertySentinel: update definition
    * FindPython: Ensure correct handling of Python_FIND_ABI variable
    * project: Revert changes to VERSION handling

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.79 2025/09/01 08:25:16 adam Exp $
+# $NetBSD: version.mk,v 1.80 2025/10/06 09:34:44 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.1.1
+CMAKE_VERSION=	4.1.2
 CMAKE_API=	${CMAKE_VERSION:R}
