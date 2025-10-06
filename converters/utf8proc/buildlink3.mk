6e789551469f2bc345f602f8fe0d94e99e057fb9
micha <micha@pkgsrc.org>
2025-10-06 08:40:53 +0000

    converters/utf8proc: Update to 2.11.0
    - Remove pkgsrc patch (merged upstream)
    - Remove C99 declaration from bl3
    
    Upstream changelog:
    - Unicode 17 support (#292, #294)
    - Documentation improvements (#295, #291)
    - Build fix for C90 (#284), silence ASAN warning (#240)

--- converters/utf8proc/buildlink3.mk
+++ converters/utf8proc/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/10/23 14:04:18 micha Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/10/06 08:40:53 micha Exp $
 
 BUILDLINK_TREE+=	utf8proc
 
 .if !defined(UTF8PROC_BUILDLINK3_MK)
 UTF8PROC_BUILDLINK3_MK:=
-
-USE_CC_FEATURES+=	c99
 
 BUILDLINK_API_DEPENDS.utf8proc+=	utf8proc>=2.1.1
 BUILDLINK_ABI_DEPENDS.utf8proc+=	utf8proc>=2.9.0
