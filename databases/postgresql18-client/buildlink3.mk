f44ccacf96bda40d1839c799d17d7bfa44083af8
adam <adam@pkgsrc.org>
2025-10-06 13:30:21 +0000

    postgresql18: added version 18.0
    PostgreSQL 18 improves performance for workloads of all sizes through a new I/O
    subsystem that has demonstrated up to 3× performance improvements when reading
    from storage, and also increases the number of queries that can use indexes.
    This release makes major-version upgrades less disruptive, accelerating upgrade
    times and reducing the time required to reach expected performance after an
    upgrade completes. Developers also benefit from PostgreSQL 18 features,
    including virtual generated columns that compute values at query time, and the
    database-friendly uuidv7() function that provides better indexing and read
    performance for UUIDs. PostgreSQL 18 makes it easier to integrate with
    single-sign on (SSO) systems with support for OAuth 2.0 authentication.

---
+++ databases/postgresql18-client/buildlink3.mk
@@ -0,0 +1,38 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/10/06 13:30:22 adam Exp $
+
+BUILDLINK_TREE+=	postgresql18-client
+
+.if !defined(POSTGRESQL18_CLIENT_BUILDLINK3_MK)
+POSTGRESQL18_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql18-client+=	postgresql18-client>=18.0
+BUILDLINK_ABI_DEPENDS.postgresql18-client+=	postgresql18-client>=18.0
+BUILDLINK_PKGSRCDIR.postgresql18-client?=	../../databases/postgresql18-client
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql18-client+=	-lpq ${BUILDLINK_LDADD.gettext}
+BUILDLINK_FILES.postgresql18-client+=	bin/pg_config
+
+pkgbase := postgresql18-client
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql18-client:Mlz4}
+.include "../../archivers/lz4/buildlink3.mk"
+.endif
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "SunOS"
+.include "../../devel/ossp-uuid/buildlink3.mk"
+.endif
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql18-client:Mgssapi}
+BUILDLINK_API_DEPENDS.mit-krb5+=	mit-krb5>=1.11	# gss_store_cred_into
+.  include "../../security/mit-krb5/buildlink3.mk"
+.endif
+.endif # POSTGRESQL18_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql18-client
--- mk/pgsql.buildlink3.mk
+++ mk/pgsql.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: pgsql.buildlink3.mk,v 1.67 2024/12/27 13:16:33 adam Exp $
+# $NetBSD: pgsql.buildlink3.mk,v 1.68 2025/10/06 13:30:21 adam Exp $
 #
 # User-settable variables:
 #
@@ -26,7 +26,7 @@ _SYS_VARS.pgsql=	PG_LIB_EXT PGSQL_TYPE PGPKGSRCDIR
 .include "../../mk/bsd.prefs.mk"
 
 PGSQL_VERSION_DEFAULT?=		16
-PGSQL_VERSIONS_ACCEPTED?=	17 16 15 14 13
+PGSQL_VERSIONS_ACCEPTED?=	18 17 16 15 14 13
 
 # transform the list into individual variables
 .for pv in ${PGSQL_VERSIONS_ACCEPTED}
@@ -41,6 +41,9 @@ PG_LIB_EXT=so
 
 # check what is installed
 .if ${OPSYS} == "Darwin"
+.  if exists(${_CROSS_DESTDIR:U}${LOCALBASE}/lib/libecpg.6.18.dylib)
+_PGSQL_VERSION_18_INSTALLED=	yes
+.  endif
 .  if exists(${_CROSS_DESTDIR:U}${LOCALBASE}/lib/libecpg.6.17.dylib)
 _PGSQL_VERSION_17_INSTALLED=	yes
 .  endif
@@ -57,6 +60,9 @@ _PGSQL_VERSION_14_INSTALLED=	yes
 _PGSQL_VERSION_13_INSTALLED=	yes
 .  endif
 .else
+.  if exists(${_CROSS_DESTDIR:U}${LOCALBASE}/lib/libecpg.so.6.18)
+_PGSQL_VERSION_18_INSTALLED=	yes
+.  endif
 .  if exists(${_CROSS_DESTDIR:U}${LOCALBASE}/lib/libecpg.so.6.17)
 _PGSQL_VERSION_17_INSTALLED=	yes
 .  endif
@@ -109,7 +115,10 @@ _PGSQL_VERSION=	${_PGSQL_VERSION_FIRSTACCEPTED}
 .endif
 
 # set variables for the version we decided to use:
-.if ${_PGSQL_VERSION} == "17"
+.if ${_PGSQL_VERSION} == "18"
+PGSQL_TYPE=	postgresql18-client
+PGPKGSRCDIR=	../../databases/postgresql18-client
+.elif ${_PGSQL_VERSION} == "17"
 PGSQL_TYPE=	postgresql17-client
 PGPKGSRCDIR=	../../databases/postgresql17-client
 .elif ${_PGSQL_VERSION} == "16"
