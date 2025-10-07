62599dd8f88d078fce2a4c9d3f9ce71581e2d7e1
wiz <wiz@pkgsrc.org>
2025-10-07 11:54:05 +0000

    attr: found matching upstream commit for patch

--- sysutils/attr/patches/patch-tools_attr.c
+++ sysutils/attr/patches/patch-tools_attr.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-tools_attr.c,v 1.3 2025/10/07 05:29:46 wiz Exp $
+$NetBSD: patch-tools_attr.c,v 1.4 2025/10/07 11:54:05 wiz Exp $
 
 Add missing header for basename().
+https://cgit.git.savannah.gnu.org/cgit/attr.git/commit/?id=8a80d895dfd779373363c3a4b62ecce5a549efb2
 
 --- tools/attr.c.orig	2025-10-07 05:28:37.594126215 +0000
 +++ tools/attr.c
