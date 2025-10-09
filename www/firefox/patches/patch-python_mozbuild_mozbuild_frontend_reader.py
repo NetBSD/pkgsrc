61ae2d9026008c04e01b2f1a3781081c7d61d5b7
ryoon <ryoon@pkgsrc.org>
2025-10-09 14:48:03 +0000

    www/firefox: Update to 143.0.4
    * Fix build with Python 3.14 with upstream changes.
    
    Changelog:
    143.0.4:
    Fixed
    
      * Improved Firefox' connection fallback behavior to mitigate sporadic slow/
        failed loads on some Google sites on systems with certain third-party
        software installed. (Bug 1990699)

---
+++ www/firefox/patches/patch-python_mozbuild_mozbuild_frontend_reader.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-python_mozbuild_mozbuild_frontend_reader.py,v 1.1 2025/10/09 14:48:03 ryoon Exp $
+
+* For Python 3.14 too.
+
+--- python/mozbuild/mozbuild/frontend/reader.py.orig	2025-10-08 15:23:09.699830974 +0000
++++ python/mozbuild/mozbuild/frontend/reader.py
+@@ -470,7 +470,7 @@ class TemplateFunction:
+             return c(
+                 ast.Subscript(
+                     value=c(ast.Name(id=self._global_name, ctx=ast.Load())),
+-                    slice=c(ast.Index(value=c(ast.Str(s=node.id)))),
++                    slice=c(ast.Index(value=c(ast.Constant(value=node.id)))),
+                     ctx=node.ctx,
+                 )
+             )
