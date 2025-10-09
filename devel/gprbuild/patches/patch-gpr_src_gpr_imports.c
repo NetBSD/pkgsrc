be3bebcbfb245d6e7039a9dbb1bc701e011e326d
dkazankov <dkazankov@pkgsrc.org>
2025-10-09 15:46:43 +0000

    gprbuild: update to upstream version 25.2
    * Update to upstream bugfix version
    * Use xmlada as inplace package rather than github module
    * Unify with ada-libgpr-25
    * Use pkg_alternatives to comply with gpr2-tools
    * Some pkglinting

--- devel/gprbuild/patches/patch-gpr_src_gpr_imports.c
+++ devel/gprbuild/patches/patch-gpr_src_gpr_imports.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-gpr_src_gpr_imports.c,v 1.1 2024/03/12 17:50:13 wiz Exp $
+$NetBSD: patch-gpr_src_gpr_imports.c,v 1.2 2025/10/09 15:46:43 dkazankov Exp $
 
 Add support for NetBSD (same as for FreeBSD) and fix getrusage() warning at linking stage
 
 --- gpr/src/gpr_imports.c.orig	2023-10-09 21:31:11.000000000 +0300
-+++ gpr/src/gpr_imports.c	2023-12-21 19:52:58.220771312 +0200
++++ gpr/src/gpr_imports.c
 @@ -64,6 +64,15 @@
  char __gnat_shared_libgcc_default = STATIC;
  const char *__gnat_default_libgcc_subdir = "lib";
