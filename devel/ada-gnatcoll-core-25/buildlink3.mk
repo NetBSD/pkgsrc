d71d1bf4a755b97ad503c1b3037dab67dd76ead5
dkazankov <dkazankov@pkgsrc.org>
2025-10-09 17:52:44 +0000

    ada-gnatcoll-core-25: update to upstream version 25.2

--- devel/ada-gnatcoll-core-25/buildlink3.mk
+++ devel/ada-gnatcoll-core-25/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/07/10 18:52:41 dkazankov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/10/09 17:52:44 dkazankov Exp $
 
 BUILDLINK_TREE+=	gnatcoll-core-25
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	gnatcoll-core-25
 GNATCOLL_CORE_25_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnatcoll-core-25+=	gnatcoll-core-${GNAT_NAME}>=25.0.0
+BUILDLINK_ABI_DEPENDS.gnatcoll-core-25+=	gnatcoll-core-${GNAT_NAME}>=25.2.0
 BUILDLINK_PKGSRCDIR.gnatcoll-core-25?=		../../devel/ada-gnatcoll-core-25
 BUILDLINK_DEPMETHOD.gnatcoll-core-25?=		build
 
@@ -14,6 +15,8 @@ BUILDLINK_CONTENTS_FILTER.gnatcoll-core-25=	\
 
 BUILDLINK_FNAME_TRANSFORM.gnatcoll-core-25+=	\
 	-e "s|${BUILDLINK_DIR}/${GNAT_PKGBASE}/|${BUILDLINK_DIR}/|g"
+
+.include "../../devel/ada-libgpr-25/buildlink3.mk"
 
 .endif
 
