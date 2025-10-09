db72703d29f47805f06387b5cf16d68d134d7b2f
dkazankov <dkazankov@pkgsrc.org>
2025-10-09 21:12:16 +0000

    gprbuild: transform buildlinked gpr1 names

--- devel/gprbuild/buildlink3.mk
+++ devel/gprbuild/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2025/10/09 15:46:43 dkazankov Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2025/10/09 21:12:16 dkazankov Exp $
 
 BUILDLINK_TREE+=	gprbuild
 
@@ -11,6 +11,8 @@ BUILDLINK_DEPMETHOD.gprbuild?=		build
 
 BUILDLINK_CONTENTS_FILTER.gprbuild=	\
 	${EGREP} '(bin/gpr.*|libexec/gprbuild/gpr.*|share/gpr/.*\.gpr$$|share/gpr/manifests/.*|share/gprconfig/.*)'
+
+BUILDLINK_FNAME_TRANSFORM.gprbuild+=	-e "s|^${BUILDLINK_DIR}/bin/gpr1|${BUILDLINK_DIR}/bin/gpr|g"
 
 .include "../../mk/bsd.fast.prefs.mk"
 
