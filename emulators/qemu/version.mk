52234bd096df542b988f45abac92b520ae739927
adam <adam@pkgsrc.org>
2025-10-09 10:05:02 +0000

    qemu: updated to 10.1.1
    10.1.1
    Bug fixes

--- emulators/qemu/version.mk
+++ emulators/qemu/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.7 2025/08/27 06:27:03 adam Exp $
+# $NetBSD: version.mk,v 1.8 2025/10/09 10:05:02 adam Exp $
 #
 # used by emulators/qemu
 # used by sysutils/qemu-guest-agent
 
-QEMU_VERSION?=	10.1.0
+QEMU_VERSION?=	10.1.1
