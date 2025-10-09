b8c5b1e70ab4b7662eef0b47548ffc033f6c4925
dkazankov <dkazankov@pkgsrc.org>
2025-10-09 20:49:50 +0000

    ada-libgpr2-25: update to upstream version 25.2

--- devel/ada-libgpr2-25/buildlink3.mk
+++ devel/ada-libgpr2-25/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/07/28 05:46:26 dkazankov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/10/09 20:49:50 dkazankov Exp $
 
 BUILDLINK_TREE+=	libgpr2-25
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libgpr2-25
 LIBGPR2_25_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgpr2-25+=	libgpr2-${GNAT_NAME}>=25.0.0
+BUILDLINK_ABI_DEPENDS.libgpr2-25+=	libgpr2-${GNAT_NAME}>=25.2.0
 BUILDLINK_PKGSRCDIR.libgpr2-25?=	../../devel/ada-libgpr2-25
 BUILDLINK_DEPMETHOD.libgpr2-25?=	build
 
@@ -14,6 +15,13 @@ BUILDLINK_CONTENTS_FILTER.libgpr2-25=	\
 
 BUILDLINK_FNAME_TRANSFORM.libgpr2-25+=	\
 	-e "s|${BUILDLINK_DIR}/${GNAT_PKGBASE}/|${BUILDLINK_DIR}/|g"
+
+.include "../../textproc/ada-xmlada-25/buildlink3.mk"
+.include "../../devel/ada-gnatcoll-core-25/buildlink3.mk"
+.include "../../devel/ada-gnatcoll-bindings-25/buildlink3.mk"
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
 
 .endif
 
