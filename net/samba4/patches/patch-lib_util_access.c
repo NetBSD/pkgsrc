c834c105fdcc24bcc3de12e146c6373a7e116098
wiz <wiz@pkgsrc.org>
2025-10-09 07:45:50 +0000

    samba4: update patch after upstream feedback

--- net/samba4/patches/patch-lib_util_access.c
+++ net/samba4/patches/patch-lib_util_access.c
@@ -3,13 +3,15 @@ $NetBSD$
 Add missing header.
 https://bugzilla.samba.org/show_bug.cgi?id=15932
 
---- lib/util/access.c.orig	2025-10-07 19:00:15.593339882 +0000
+--- lib/util/access.c.orig	2024-07-29 09:03:15.058628300 +0000
 +++ lib/util/access.c
-@@ -23,6 +23,8 @@
+@@ -23,6 +23,10 @@
  #include "lib/util/unix_match.h"
  #include "lib/util/smb_strtox.h"
  
++#ifdef HAVE_NETGROUP_H
 +#include <netgroup.h>
++#endif
 +
  #define NAME_INDEX 0
  #define ADDR_INDEX 1
--- net/samba4/patches/patch-source3_auth_user__util.c
+++ net/samba4/patches/patch-source3_auth_user__util.c
@@ -3,13 +3,15 @@ $NetBSD$
 Add missing header.
 https://bugzilla.samba.org/show_bug.cgi?id=15932
 
---- source3/auth/user_util.c.orig	2025-10-07 19:00:41.861180195 +0000
+--- source3/auth/user_util.c.orig	2024-07-29 09:03:15.302630200 +0000
 +++ source3/auth/user_util.c
-@@ -25,6 +25,8 @@
+@@ -25,6 +25,10 @@
  #include "auth.h"
  #include "lib/gencache.h"
  
++#ifdef HAVE_NETGROUP_H
 +#include <netgroup.h>
++#endif
 +
  /*******************************************************************
   Map a username from a dos name to a unix name by looking in the username
