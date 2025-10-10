98fd5fc5bf3355dae58f85ea77b46e9f93a99ac5
js <js@pkgsrc.org>
2025-10-10 20:48:02 +0000

    Make shells/bash build and work on QNX

--- shells/bash/patches/patch-examples_loadables_finfo.c
+++ shells/bash/patches/patch-examples_loadables_finfo.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-examples_loadables_finfo.c,v 1.1 2025/07/10 14:36:20 jperkin Exp $
+$NetBSD: patch-examples_loadables_finfo.c,v 1.2 2025/10/10 20:48:02 js Exp $
 
 MAJOR_IN_MKDEV is not in config.h so will never be defined, for now just go
 with the simple workaround for illumos which will definitely need mkdev.h.
@@ -14,3 +14,12 @@ with the simple workaround for illumos which will definitely need mkdev.h.
  #  include <sys/mkdev.h>
  #endif
  #ifdef MAJOR_IN_SYSMACROS
+@@ -273,7 +273,7 @@ printst(struct stat *st)
+ 
+ 	ma = major (st->st_rdev);
+ 	mi = minor (st->st_rdev);
+-#if defined (makedev)
++#if defined (makedev) && !defined(__QNX__)
+ 	d = makedev (ma, mi);
+ #else
+ 	d = st->st_rdev & 0xFF;
