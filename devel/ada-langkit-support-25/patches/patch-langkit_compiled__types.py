e2df9a552b45059d451c2eda95590a9fcad5e793
dkazankov <dkazankov@pkgsrc.org>
2025-10-12 10:00:04 +0000

    ada-langkit-support-25: add missing patches

---
+++ devel/ada-langkit-support-25/patches/patch-langkit_compiled__types.py
@@ -0,0 +1,17 @@
+$NetBSD: patch-langkit_compiled__types.py,v 1.1 2025/10/12 10:00:04 dkazankov Exp $
+
+Transition uses of the Python pipes module to shlex
+"pipes" was removed from Python 3.13.
+https://github.com/AdaCore/langkit/commit/dbacb9b5545315e7045ef082dba99d2c96ed1356
+
+--- langkit/compiled_types.py.orig	2024-12-10 12:23:59.000000000 +0200
++++ langkit/compiled_types.py
+@@ -6,7 +6,7 @@
+ from dataclasses import dataclass
+ import difflib
+ from itertools import count, takewhile
+-import pipes
++import shlex
+ from typing import (
+     Any,
+     Callable,
