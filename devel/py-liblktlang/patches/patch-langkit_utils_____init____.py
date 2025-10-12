275e85d87d497298db9faf88f0230a891cf3d104
dkazankov <dkazankov@pkgsrc.org>
2025-10-12 13:18:28 +0000

    py-liblktlang: update to upstream version 25.2
    * Incorporate py-langkit patches for python >= 3.13 support
    * Lkt: introduce the distinction between and/or binops and %and/%or
    * Lkt: convey logic context/error location through builtin dynvars
    * Lkt: rework the syntax for the logic DSL
    * Lkt lowering: remove useless generic type
    * Lkt: rework the %all/%any equation constructors
    * [25.1] Lkt: rework Logic DSL
    * Lkt lowering: introduce the @Property annotation for methods
    * [25.1] Change the access syntax for builtin properties
    * Enable style checks for Lkt and fix existing issues
    * [25.1] Fine tune unparsers for Lkt

---
+++ devel/py-liblktlang/patches/patch-langkit_utils_____init____.py
@@ -0,0 +1,25 @@
+$NetBSD: patch-langkit_utils_____init____.py,v 1.1 2025/10/12 13:18:28 dkazankov Exp $
+
+Transition uses of the Python pipes module to shlex
+"pipes" was removed from Python 3.13.
+https://github.com/AdaCore/langkit/commit/dbacb9b5545315e7045ef082dba99d2c96ed1356
+
+--- langkit/utils/__init__.py.orig	2024-12-10 12:23:59.000000000 +0200
++++ langkit/utils/__init__.py
+@@ -11,7 +11,6 @@
+ from copy import copy
+ from enum import Enum
+ import os
+-import pipes
+ import shlex
+ import shutil
+ from typing import (
+@@ -273,7 +272,7 @@
+     environment variable.
+     """
+     return (
+-        f'{name}={pipes.quote(path)}"{os.pathsep}${name}";'
++        f'{name}={shlex.quote(path)}"{os.pathsep}${name}";'
+         f" export {name}"
+     )
+ 
