36ee952a2ae3ead99a00487e44e07a579d22d8ef
mrg <mrg@pkgsrc.org>
2025-10-12 00:57:38 +0000

    make build with GCC 14.  phew!
    it's a lot of standard things:
    - missing headers
    - missing types
    - conflicting types

--- mail/metamail/patches/patch-metamail_shared.c
+++ mail/metamail/patches/patch-metamail_shared.c
@@ -1,27 +1,36 @@
-$NetBSD: patch-metamail_shared.c,v 1.1 2012/12/30 05:38:55 dholland Exp $
+$NetBSD: patch-metamail_shared.c,v 1.2 2025/10/12 00:57:39 mrg Exp $
 
 - use standard headers
 - don't provide own declarations of standard functions
 - don't cast the return value of malloc
+- add types for GCC 14.
 
---- metamail/shared.c~	1993-05-16 18:19:32.000000000 +0000
-+++ metamail/shared.c
-@@ -1,4 +1,6 @@
+--- metamail/shared.c.orig	1993-05-16 11:19:32.000000000 -0700
++++ metamail/shared.c	2025-10-11 16:52:18.287181298 -0700
+@@ -1,16 +1,20 @@
  #include <stdio.h>
 +#include <stdlib.h>
 +#include <string.h>
  #include <ctype.h>
  #include <config.h>
- #ifdef SYSV
-@@ -6,7 +8,6 @@
+-#ifdef SYSV
+ #include <unistd.h>
++#ifdef SYSV
  #include <sys/utsname.h>
  #endif
  
 -extern char *malloc();
++#include "metamail.h"
++
  char **Exceptions;
  int *NeedsPortableNewlines;
  int ExceptionsAlloced = 0, ExceptionsUsed = 0;
-@@ -18,14 +19,14 @@ int needsportable;
+ 
++int
+ ExceptionalNewline(contenttype, needsportable)
+ char *contenttype;
+ int needsportable;
+@@ -18,14 +22,14 @@ int needsportable;
      char *s;
      if (ExceptionsAlloced == 0) {
          ExceptionsAlloced = 25;
@@ -40,3 +49,27 @@ $NetBSD: patch-metamail_shared.c,v 1.1 2012/12/30 05:38:55 dholland Exp $
          if (!Exceptions || !NeedsPortableNewlines) return(-1);
      }
      s = malloc(1+strlen(contenttype));
+@@ -40,6 +44,7 @@ int needsportable;
+     return(0);
+ }
+ 
++int
+ DoesNeedPortableNewlines(ctype)
+ char *ctype;
+ {
+@@ -65,6 +70,7 @@ char *ctype;
+     return(0);
+ }
+ 
++int
+ lc2strncmp(s1, s2, len)
+ char *s1, *s2;
+ int len;
+@@ -78,6 +84,7 @@ int len;
+     return((*s1 == *s2) ? 0 : -1);
+ }
+ 
++int
+ lc2strcmp(s1, s2)
+ char *s1, *s2;
+ {
