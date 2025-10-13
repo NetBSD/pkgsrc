4a1fd830d33ae9267a278436ab87d46112bdc6a9
dkazankov <dkazankov@pkgsrc.org>
2025-10-13 11:40:36 +0000

    ada-liblal-refactor-25: update to upstream version 25.2
    * Fix 'Add Parameter' on functions without parameters
    * Add --latest to CI
    * Use Source_Location_Range."<" from Langkit_Support.Slocs
    * Test: Update baseline

--- textproc/ada-liblal-refactor-25/buildlink3.mk
+++ textproc/ada-liblal-refactor-25/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/09/07 07:31:38 dkazankov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/10/13 11:40:36 dkazankov Exp $
 
 BUILDLINK_TREE+=	liblal-refactor-25
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	liblal-refactor-25
 LIBLAL_REFACTOR_25_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.liblal-refactor-25+=	liblal-refactor-${GNAT_NAME}>=25.0.0
+BUILDLINK_ABI_DEPENDS.liblal-refactor-25+=	liblal-refactor-${GNAT_NAME}>=25.2.0
 BUILDLINK_PKGSRCDIR.liblal-refactor-25?=	../../textproc/ada-liblal-refactor-25
 BUILDLINK_DEPMETHOD.liblal-refactor-25?=	build
 
