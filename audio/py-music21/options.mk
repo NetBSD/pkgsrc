e1e450a4c67f4ccf5d6622031bc462402df52a79
gutteridge <gutteridge@pkgsrc.org>
2025-10-14 03:21:50 +0000

    py-music21: revise some comments
    lilypond isn't really the build issue it used to be, AFAIK. Move detail
    about py-game to a consistent spot to find summary.

--- audio/py-music21/options.mk
+++ audio/py-music21/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.6 2022/12/06 01:45:17 gutteridge Exp $
+# $NetBSD: options.mk,v 1.7 2025/10/14 03:21:50 gutteridge Exp $
 
 # print/lilypond and devel/py-game are optional because they pull in
-# large and complex dependency chains. (lilypond also has a tendency
-# to fail to build.)
+# large and complex dependency chains. py-game is required for MIDI
+# realtime playback.
 
 # audio/musescore is also an application dependency that gets called
 # by music21's show() method, but it's a pretty heavy dependency for
@@ -18,7 +18,6 @@ PKG_SUGGESTED_OPTIONS=	py-game
 DEPENDS+=	lilypond-[0-9]*:../../print/lilypond
 .endif
 
-# py-game is required for MIDI realtime playback.
 .if !empty(PKG_OPTIONS:Mpy-game)
 DEPENDS+=	${PYPKGPREFIX}-game-[0-9]*:../../devel/py-game
 .endif
