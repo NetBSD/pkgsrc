6361e068c2a18420e0fc648696cead4216855122
adam <adam@pkgsrc.org>
2025-10-15 09:14:00 +0000

    python313 py313-html-docs: updated to 3.13.9
    Python 3.13.9
    
    Library
    gh-139783: Fix inspect.getsourcelines() for the case when a decorator is followed by a comment or an empty line.

--- lang/python313/dist.mk
+++ lang/python313/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.9 2025/10/08 05:55:24 adam Exp $
+# $NetBSD: dist.mk,v 1.10 2025/10/15 09:14:00 adam Exp $
 
-PY_DISTVERSION=	3.13.8
+PY_DISTVERSION=	3.13.9
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python313/distinfo
