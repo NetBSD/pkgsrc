138b070afe8b84078e46eb582be9292f3090cb30
maya <maya@pkgsrc.org>
2025-10-17 23:34:26 +0000

    Pullup ticket #7012 - requested by sborrill databases/mysql80-client: Build fix databases/mysql80-server: Build fix
    Revisions pulled up:
    - databases/mysql80-client/distinfo                             1.17
    - databases/mysql80-client/patches/patch-plugin_x_client_xprotocol__impl.cc 1.1
    - databases/mysql80-client/patches/patch-plugin_x_protocol_plugin_encoder__file__output.h 1.1
    - databases/mysql80-client/patches/patch-plugin_x_protocol_plugin_message__field__chain.cc 1.1
    - databases/mysql80-client/patches/patch-plugin_x_protocol_plugin_messages__used__by__server.cc 1.1
    - databases/mysql80-client/patches/patch-plugin_x_protocol_stream_decompression__input__stream.h 1.1
    - databases/mysql80-client/patches/patch-plugin_x_src_io_vio__input__stream.h 1.1
    - databases/mysql80-client/patches/patch-plugin_x_src_prepare__param__handler.cc 1.1
    - databases/mysql80-client/patches/patch-plugin_x_tests_driver_connector_session__holder.cc 1.1
    - databases/mysql80-client/patches/patch-plugin_x_tests_driver_formatters_message__formatter.cc 1.1
    - databases/mysql80-client/patches/patch-plugin_x_tests_driver_processor_send__message__block__processor.cc 1.1
    - databases/mysql80-server/Makefile                             1.39
    - databases/mysql80-server/PLIST                                1.6
    
    ---
       Module Name:	pkgsrc
       Committed By:	jnemeth
       Date:		Wed Oct 15 03:52:38 UTC 2025
    
       Modified Files:
       	pkgsrc/databases/mysql80-client: distinfo
       Added Files:
       	pkgsrc/databases/mysql80-client/patches:
       	    patch-plugin_x_client_xprotocol__impl.cc
       	    patch-plugin_x_protocol_plugin_encoder__file__output.h
       	    patch-plugin_x_protocol_plugin_message__field__chain.cc
       	    patch-plugin_x_protocol_plugin_messages__used__by__server.cc
       	    patch-plugin_x_protocol_stream_decompression__input__stream.h
       	    patch-plugin_x_src_io_vio__input__stream.h
       	    patch-plugin_x_src_prepare__param__handler.cc
       	    patch-plugin_x_tests_driver_connector_session__holder.cc
       	    patch-plugin_x_tests_driver_formatters_message__formatter.cc
    
       patch-plugin_x_tests_driver_processor_send__message__block__processor.cc
    
       Log Message:
       PR/59670 -- patches from joerg@ to work with modern external abseil/protobuf
    
    ---
       Module Name:	pkgsrc
       Committed By:	jnemeth
       Date:		Wed Oct 15 04:02:19 UTC 2025
    
       Modified Files:
       	pkgsrc/databases/mysql80-server: Makefile
    
       Log Message:
       PR/59670 -- revert switch to included protobuf which didn't work
    
    ---
       Module Name:	pkgsrc
       Committed By:	jnemeth
       Date:		Wed Oct 15 22:06:10 UTC 2025
    
       Modified Files:
       	pkgsrc/databases/mysql80-server: PLIST
    
       Log Message:
       PR/59670 -- missed reverting PLIST

---
+++ databases/mysql80-client/patches/patch-plugin_x_client_xprotocol__impl.cc
@@ -0,0 +1,31 @@
+$NetBSD: patch-plugin_x_client_xprotocol__impl.cc,v 1.1.2.2 2025/10/17 23:34:26 maya Exp $
+
+--- plugin/x/client/xprotocol_impl.cc.orig	2025-09-26 13:01:59.742201211 +0000
++++ plugin/x/client/xprotocol_impl.cc
+@@ -782,7 +782,7 @@ std::unique_ptr<XProtocol::Message> Prot
+   DBUG_LOG("debug", "Deserialize message: " << ret_val->GetTypeName());
+   if (!ret_val->ParseFromCodedStream(input_stream)) {
+     std::string error_message(ERR_MSG_MESSAGE_NOT_INITIALIZED);
+-    error_message += "Name:" + ret_val->GetTypeName() + ", ";
++    error_message += "Name:" + std::string(ret_val->GetTypeName()) + ", ";
+     error_message += ret_val->InitializationErrorString();
+     *out_error = XError(CR_MALFORMED_PACKET, error_message);
+ 
+@@ -816,7 +816,7 @@ std::unique_ptr<XProtocol::Message> Prot
+ 
+   if (!ret_val->IsInitialized()) {
+     std::string err(ERR_MSG_MESSAGE_NOT_INITIALIZED);
+-    err += "Name:" + ret_val->GetTypeName() + ", ";
++    err += "Name:" + std::string(ret_val->GetTypeName()) + ", ";
+     err += ret_val->InitializationErrorString();
+     *out_error = XError(CR_MALFORMED_PACKET, err);
+ 
+@@ -1122,7 +1122,7 @@ XProtocol::Message *Protocol_impl::recv_
+ 
+     if (!m_compressed.ParseFromCodedStream(&cis)) {
+       std::string error_message(ERR_MSG_MESSAGE_NOT_INITIALIZED);
+-      error_message += "Name:" + m_compressed.GetTypeName() + ", ";
++      error_message += "Name:" + std::string(m_compressed.GetTypeName()) + ", ";
+       error_message += m_compressed.InitializationErrorString();
+       *out_error = XError(CR_MALFORMED_PACKET, error_message);
+       return nullptr;
---
+++ databases/mysql80-client/patches/patch-plugin_x_src_prepare__param__handler.cc
@@ -0,0 +1,15 @@
+$NetBSD: patch-plugin_x_src_prepare__param__handler.cc,v 1.1.2.2 2025/10/17 23:34:26 maya Exp $
+
+--- plugin/x/src/prepare_param_handler.cc.orig	2025-09-26 12:59:25.775536085 +0000
++++ plugin/x/src/prepare_param_handler.cc
+@@ -147,8 +147,8 @@ ngs::Error_code Prepare_param_handler::p
+                         "' and of type '%s' is not supported for binding"
+                         " to prepared statement",
+                         ph.m_id,
+-                        arg.has_scalar() ? arg.scalar().GetTypeName().c_str()
+-                                         : arg.GetTypeName().c_str());
++                        arg.has_scalar() ? std::string(arg.scalar().GetTypeName()).c_str()
++                                         : std::string(arg.GetTypeName()).c_str());
+     }
+   }
+   return ngs::Success();
