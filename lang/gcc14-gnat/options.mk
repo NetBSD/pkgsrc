0bac3d1c8378b81200efb6b0391fe9ad3c5df3b2
dkazankov <dkazankov@pkgsrc.org>
2025-10-18 05:31:22 +0000

    gcc14-gnat: migrate to new bootstrap, some patch and option changes
    * Update common patches from gcc14.
    * Move conditional NetBSD specific Ada patches to SUBST framework.
    * Default math now use inplace option.
    * GCC's inplace graphite changed to distribution downloading variant.
    * NetBSD builds migrated to the new bootstrap compiler.
      Other platforms uses old build with gcc10-aux package which still
      doesn't work.

--- lang/gcc14-gnat/options.mk
+++ lang/gcc14-gnat/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2025/07/02 06:28:28 dkazankov Exp $
+# $NetBSD: options.mk,v 1.4 2025/10/18 05:31:22 dkazankov Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc14-gnat
 PKG_SUPPORTED_OPTIONS=	nls gcc-inplace-math gcc-graphite
-PKG_SUGGESTED_OPTIONS=	gcc-graphite
+PKG_SUGGESTED_OPTIONS=	gcc-inplace-math gcc-graphite
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -52,10 +52,10 @@ USE_TOOLS+=		msgfmt
 CONFIGURE_ARGS+=	--enable-nls
 CONFIGURE_ARGS+=	--with-libiconv-prefix=${BUILDLINK_PREFIX.iconv}
 MAKE_ENV+=		ICONVPREFIX=${BUILDLINK_PREFIX.iconv}
-.if ${OPSYS} == "NetBSD"
+.  if ${OPSYS} == "NetBSD"
 USE_GNU_ICONV=		yes
 USE_BUILTIN.gettext=	no
-.endif
+.  endif
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .else
@@ -93,11 +93,15 @@ LIBS.SunOS+=		-lgmp
 ### Graphite Support
 ###
 .if !empty(PKG_OPTIONS:Mgcc-graphite)
-BUILDLINK_API_DEPENDS.isl+=	isl>=0.24
 .  if !empty(PKG_OPTIONS:Mgcc-inplace-math)
-.    include "../../math/isl/inplace.mk"
-FORCE_CXX_STD=	c++17
+ISL24=			isl-0.24
+SITES.${ISL24}.tar.bz2=	${MASTER_SITE_GNU:=gcc/infrastructure/}
+DISTFILES+=		${ISL24}.tar.xz
+post-extract:
+	${MV} ${WRKDIR}/${ISL24} ${WRKSRC}/isl
+
 .  else
+BUILDLINK_API_DEPENDS.isl+=	isl>=0.24
 .    include "../../math/isl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-isl=${BUILDLINK_PREFIX.isl}
 .  endif
