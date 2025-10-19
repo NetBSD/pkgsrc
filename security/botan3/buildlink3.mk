2fd9019447f299752b23f4c174135c70e175645f
adam <adam@pkgsrc.org>
2025-10-19 13:45:30 +0000

    botan3 py-botan3: updated to 3.9.0
    3.9.0
    
    Add SHA-1 implementation using AVX2/BMI2
    
    Add Camellia implementation using GFNI/AVX2
    
    Add SHACAL2 implementation using AVX512
    
    The eFrodoKEM TLS 1.3 ciphersuites have changed the suite code to match changes in OQS.
    
    Add support for TLS 1.2 NULL cipher suites. These suites are disabled in the build by default, enable tls_null module to use.
    
    Add support for X.509 extensions from RFC 3779
    
    Elliptic curve improvements
    
    Add EC_Scalar::hash following RFC 9380’s hash_to_field
    
    Modify the OID lookup system to use a static switch for builtin OIDs.
    
    Optimizations for X448 and Ed448
    
    Modify BOTAN_CLEAR_CPUID so that clearing ssse3 also disables AVX2/AVX512
    
    Remove various internal references to “EMSA”, an obsolete term used for RSA signature padding that originates from IEEE 1363.
    
    Enable support for GCC’s “strub” stack clearing. This is disabled by default, use the --enable-stack-scrubbing option to turn on.
    
    Use std::span in the internal block cipher padding mode interfaces
    
    Properly check DNS label length restrictions when checking wildcards.
    
    Work around a GCC 13/14 miscompilation when LTO is used
    
    Fix a bug preventing building System_RNG with only getrandom enabled.
    
    Document the specific threat model the library uses
    
    Remove configure.py options to disable specific CPU instructions.
    
    Remove configure.py option --with-local-config
    
    Add a better interface for encoding optional ASN.1 elements using std::optional
    
    Internal cleanups relating to multiprecision integers
    
    Resolve many warnings from clang-tidy
    
    CMake improvements
    
    CI improvements

--- security/botan3/buildlink3.mk
+++ security/botan3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2025/09/27 09:57:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2025/10/19 13:45:30 adam Exp $
 
 BUILDLINK_TREE+=	botan3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	botan3
 BOTAN3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.botan3+=	botan3>=3.0.0
-BUILDLINK_ABI_DEPENDS.botan3?=	botan3>=3.6.1nb6
+BUILDLINK_ABI_DEPENDS.botan3+=	botan3>=3.6.1nb6
 BUILDLINK_PKGSRCDIR.botan3?=	../../security/botan3
 
 .include "../../devel/boost-libs/buildlink3.mk"
