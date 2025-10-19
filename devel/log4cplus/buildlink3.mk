f3895eb2f18afe78b5e1f5410927781efb61af1c
adam <adam@pkgsrc.org>
2025-10-19 13:43:08 +0000

    log4cplus: updated to 2.1.2
    2.1.2
    
    IMPORTANT: Dropping Visual Studio 2015 compatibility. It is no longer able to compile the thread pool source.
    
    Implement LOG4CPLUS_ASSERT_FMT() - formats assertion message using C-style format string.
    
    New configuration property: log4cplus.threadPoolBlockOnFull. When this property is true (default), threads will block when internal thread pool queue is full.
    
    Warn about full internal thread pool queue when dropping events due to log4cplus.threadPoolBlockOnFull being false.

--- devel/log4cplus/buildlink3.mk
+++ devel/log4cplus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2025/02/05 23:10:52 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2025/10/19 13:43:08 adam Exp $
 
 BUILDLINK_TREE+=	log4cplus
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	log4cplus
 LOG4CPLUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.log4cplus+=	log4cplus>=2.0.3
-BUILDLINK_ABI_DEPENDS.log4cplus?=		log4cplus>=2.0.3nb1
+BUILDLINK_ABI_DEPENDS.log4cplus+=	log4cplus>=2.0.3nb1
 BUILDLINK_PKGSRCDIR.log4cplus?=		../../devel/log4cplus
 
 .include "../../converters/libiconv/buildlink3.mk"
