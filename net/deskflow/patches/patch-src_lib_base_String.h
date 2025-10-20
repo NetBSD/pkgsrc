9b9c79a913a8d225e8723cc087889ba139431e9a
tnn <tnn@pkgsrc.org>
2025-10-20 19:49:12 +0000

    deskflow: update to 1.24.0
    - The client and server binaries have been merged:
      Use "deskflow-core client" instead of "deskflow-client" and
      ditto for the server.
    - The log levels values in the gui have changed.
      Users may want to adjust the Log Level setting after upgrading.
    - Bugfixes and cleanups

--- net/deskflow/patches/patch-src_lib_base_String.h
+++ net/deskflow/patches/patch-src_lib_base_String.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_lib_base_String.h,v 1.3 2025/08/07 13:59:30 abs Exp $
+$NetBSD: patch-src_lib_base_String.h,v 1.4 2025/10/20 19:49:12 tnn Exp $
 
 cstdarg for va_list
 
---- src/lib/base/String.h.orig	2025-07-23 13:27:06.000000000 +0000
+--- src/lib/base/String.h.orig	2025-09-11 14:59:52.000000000 +0000
 +++ src/lib/base/String.h
-@@ -10,6 +10,7 @@
+@@ -8,6 +8,7 @@
  
- #include "common/Common.h"
+ #pragma once
  
 +#include <cstdarg>
  #include <string>
