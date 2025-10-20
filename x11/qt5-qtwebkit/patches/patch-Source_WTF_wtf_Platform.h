4f842e711306c27c665829eb4814220596b0ab5d
hauke <hauke@pkgsrc.org>
2025-10-20 11:43:38 +0000

    Unbreak x11/qt5-qtwebkit build
    by applying a patch from
    <https://github.com/qtwebkit/qtwebkit/pull/1107> to disable ICU C++
    header-only APIs introduced in ICU 76.
    
    At this point, the c++17 requirement is not just unnecessary, but
    breaks the build unless removed.
    
    Relevant to PR pkg/59028

--- x11/qt5-qtwebkit/patches/patch-Source_WTF_wtf_Platform.h
+++ x11/qt5-qtwebkit/patches/patch-Source_WTF_wtf_Platform.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.7 2025/01/24 11:54:37 wiz Exp $
+$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.8 2025/10/20 11:43:38 hauke Exp $
 
 * Use system's malloc for NetBSD, do not use fastmalloc from qt5,
   fix segfault of qtwebkit consumers
@@ -8,8 +8,9 @@ $NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.7 2025/01/24 11:54:37 wiz Exp $
 
 * Fix build with ICU 75.
   From: https://github.com/qtwebkit/qtwebkit/commit/756e1c8f23dc2720471298281c421c0076d02df8.patch
+  with addition from: https://github.com/qtwebkit/qtwebkit/pull/1107
 
---- Source/WTF/wtf/Platform.h.orig	2024-06-02 11:06:38.219855486 +0000
+--- Source/WTF/wtf/Platform.h.orig	2020-03-04 17:16:37.000000000 +0000
 +++ Source/WTF/wtf/Platform.h
 @@ -686,6 +686,12 @@
  #define USE_SYSTEM_MALLOC 1
@@ -35,13 +36,15 @@ $NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.7 2025/01/24 11:54:37 wiz Exp $
  #if !defined(ENABLE_DISASSEMBLER) && USE(UDIS86)
  #define ENABLE_DISASSEMBLER 1
  #endif
-@@ -1125,6 +1135,13 @@
+@@ -1125,6 +1135,15 @@
  #define ENABLE_PLATFORM_FONT_LOOKUP 1
  #endif
  
 +/* FIXME: This does not belong in Platform.h and should instead be included in another mechanism (compiler option, prefix header, config.h, etc) */
 +/* ICU configuration. Some of these match ICU defaults on some platforms, but we would like them consistently set everywhere we build WebKit. */
 +#define U_SHOW_CPLUSPLUS_API 0
++/* From <https://github.com/raspbian-packages/qtwebkit-opensource-src/commit/36c4b66e86c3a93a095ec55e239bedead8a56b50> */
++#define U_SHOW_CPLUSPLUS_HEADER_API 0
 +#ifdef __cplusplus
 +#define UCHAR_TYPE char16_t
 +#endif
