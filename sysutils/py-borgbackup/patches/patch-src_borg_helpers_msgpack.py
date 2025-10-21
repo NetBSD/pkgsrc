4d51dafa4cb7122de539c7220c0233e8b0f364c0
maya <maya@pkgsrc.org>
2025-10-21 01:43:30 +0000

    Pullup ticket #7013 - requested by bsiegert sysutils/py-borgbackup: Functionality fix
    Revisions pulled up:
    - sysutils/py-borgbackup/Makefile                               1.59
    - sysutils/py-borgbackup/distinfo                               1.39
    - sysutils/py-borgbackup/patches/patch-pyproject.toml           1.1
    - sysutils/py-borgbackup/patches/patch-src_borg_helpers_msgpack.py 1.5
    
    ---
       Module Name:    pkgsrc
       Committed By:   he
       Date:           Thu Oct 16 21:30:21 UTC 2025
    
       Modified Files:
                pkgsrc/sysutils/py-borgbackup: Makefile distinfo
       Added Files:
                pkgsrc/sysutils/py-borgbackup/patches: patch-pyproject.toml
                    patch-src_borg_helpers_msgpack.py
    
       Log Message:
       sysutils/py-borgbackup: add patches to accept py-msgpack up to 1.1.2.
    
       This evidently got lost in the previous update...
       This replicates what upstream has already done in 1.4.2rc*.

---
+++ sysutils/py-borgbackup/patches/patch-src_borg_helpers_msgpack.py
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_borg_helpers_msgpack.py,v 1.5.2.2 2025/10/21 01:43:30 maya Exp $
+
+Permit newer versions of msgpack than what's originally
+declared by upstream.
+
+--- src/borg/helpers/msgpack.py.orig	2024-03-12 09:32:37.673913724 +0000
++++ src/borg/helpers/msgpack.py
+@@ -137,7 +137,7 @@ def is_slow_msgpack():
+ def is_supported_msgpack():
+     # DO NOT CHANGE OR REMOVE! See also requirements and comments in pyproject.toml.
+     import msgpack
+-    return (1, 0, 3) <= msgpack.version <= (1, 1, 0) and \
++    return (1, 0, 3) <= msgpack.version <= (1, 1, 2) and \
+            msgpack.version not in []  # < add bad releases here to deny list
+ 
+ 
