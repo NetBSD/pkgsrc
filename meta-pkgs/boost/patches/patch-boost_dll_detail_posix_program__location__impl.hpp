a80aba3e783a49a8c9fcf95eb9327e1ba5536432
wiz <wiz@pkgsrc.org>
2025-10-21 07:53:10 +0000

    boost-headers: fix a bug in the dll module
    Fixes the build of uhd.
    
    Bump PKGREVISION.

---
+++ meta-pkgs/boost/patches/patch-boost_dll_detail_posix_program__location__impl.hpp
@@ -0,0 +1,18 @@
+$NetBSD: patch-boost_dll_detail_posix_program__location__impl.hpp,v 1.1 2025/10/21 07:53:10 wiz Exp $
+
+Fix build error in this file.
+
+--- boost/dll/detail/posix/program_location_impl.hpp.orig	2025-10-21 07:45:26.595305483 +0000
++++ boost/dll/detail/posix/program_location_impl.hpp
+@@ -100,7 +100,10 @@ namespace boost { namespace dll { namesp
+ 
+ namespace boost { namespace dll { namespace detail {
+     inline boost::dll::fs::path program_location_impl(std::error_code &ec) {
+-        return boost::dll::fs::read_symlink("/proc/curproc/exe", ec);
++        boost::dll::fs::error_code fs_errc;
++        auto result = boost::dll::fs::read_symlink("/proc/curproc/exe", fs_errc);
++        ec = fs_errc;
++        return result;
+     }
+ }}} // namespace boost::dll::detail
+ 
