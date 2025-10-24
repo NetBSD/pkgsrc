220dd1388a1e237719a9b341d016f4f76a7c62d6
js <js@pkgsrc.org>
2025-10-24 20:05:04 +0000

    devel/objfw: Make dependency on Clang optional

--- devel/objfw/options.mk
+++ devel/objfw/options.mk
@@ -1,16 +1,41 @@
-# $NetBSD: options.mk,v 1.2 2024/04/14 17:05:03 js Exp $
+# $NetBSD: options.mk,v 1.3 2025/10/24 20:05:04 js Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.objfw
+.if ${MACHINE_ARCH} != "alpha" && \
+	${MACHINE_ARCH} != "coldfire" && \
+	${MACHINE_ARCH} != "hppa" && \
+	${MACHINE_ARCH} != "ia64" && \
+	${MACHINE_ARCH} != "m68k" && \
+	${MACHINE_ARCH} != "m68ksf" && \
+	${MACHINE_ARCH} != "or1k" && \
+	${MACHINE_ARCH} != "sh3eb" && \
+	${MACHINE_ARCH} != "sh3el" && \
+	${MACHINE_ARCH} != "vax"
+PKG_SUPPORTED_OPTIONS=		clang
+PKG_SUGGESTED_OPTIONS+=		clang
+.endif
 PKG_OPTIONS_OPTIONAL_GROUPS=	tls
 PKG_OPTIONS_GROUP.tls=		openssl gnutls mbedtls
 .if ${OPSYS} == "Darwin"
 PKG_OPTIONS_GROUP.tls+=		securetransport
-PKG_SUGGESTED_OPTIONS=		securetransport
+PKG_SUGGESTED_OPTIONS+=		securetransport
 .else
-PKG_SUGGESTED_OPTIONS=		openssl
+PKG_SUGGESTED_OPTIONS+=		openssl
 .endif
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mclang)
+CONFIGURE_ARGS+=	OBJC=clang
+.  if ${OPSYS} != "Darwin"
+# Darwin already has Clang as the default compiler.
+TOOL_DEPENDS+=		clang>=3.2:../../lang/clang
+PKGSRC_COMPILER=	clang
+.  endif
+.else
+# Need to explicitly set GCC, as configure prefers Clang.
+CONFIGURE_ARGS+=	OBJC=gcc
+.endif
 
 .if !empty(PKG_OPTIONS:Mopenssl)
 .  include "../../security/openssl/buildlink3.mk"
