eb929941df6ed8a94af4c11438d2bbab0d81d548
maya <maya@pkgsrc.org>
2025-10-24 23:53:01 +0000

    Pullup ticket #7027 - requested by bsiegert www/curl: Bug fix www/libcurl-gnutls: Bug fix
    Revisions pulled up:
    - www/curl/Makefile                                             1.301
    - www/curl/distinfo                                             1.220
    - www/curl/patches/patch-lib_curl__threads.c                    1.1
    - www/libcurl-gnutls/Makefile                                   1.19
    
    ---
       Module Name:	pkgsrc
       Committed By:	riastradh
       Date:		Fri Oct 24 04:01:11 UTC 2025
    
       Modified Files:
       	pkgsrc/www/curl: Makefile distinfo
       	pkgsrc/www/libcurl-gnutls: Makefile
       Added Files:
       	pkgsrc/www/curl/patches: patch-lib_curl__threads.c
    
       Log Message:
       www/curl, www/libcurl-gnutls: Disable pthread_cancel.
    
       This upstream change is broken on just about every platform because
       getaddrinfo is not really cancel-safe, even though it's kinda sorta
       supposed to be in POSIX
       (https://pubs.opengroup.org/onlinepubs/9799919799/functions/V2_chap02.html#tag_16_09_05_02).
    
       The mistake has been backed out upstream for the next release, but
       that won't come until November and we need to pull up the fix now
       because it's already biting users.
    
       https://eissing.org/icing/posts/pthread_cancel/
       https://eissing.org/icing/posts/rip_pthread_cancel/
       https://github.com/curl/curl/issues/18532
       https://github.com/curl/curl/pull/18540/files
    
       PR pkg/59720: curl hangs or crashes with pthread_cancel enabled

---
+++ www/curl/patches/patch-lib_curl__threads.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-lib_curl__threads.c,v 1.1.2.2 2025/10/24 23:53:01 maya Exp $
+
+Disable pthread_cancel.
+https://github.com/curl/curl/issues/18532
+https://eissing.org/icing/posts/rip_pthread_cancel/
+https://gnats.NetBSD.org/59720
+
+--- lib/curl_threads.c.orig	2025-09-10 05:43:43.000000000 +0000
++++ lib/curl_threads.c
+@@ -106,7 +106,7 @@ int Curl_thread_join(curl_thread_t *hnd)
+  * - this is a -fsanitize=thread build
+  *   (clang sanitizer reports false positive when functions to not return)
+  */
+-#if defined(PTHREAD_CANCEL_ENABLE) && !defined(__FreeBSD__)
++#if 0 && defined(PTHREAD_CANCEL_ENABLE) && !defined(__FreeBSD__)
+ #if defined(__has_feature)
+ #  if !__has_feature(thread_sanitizer)
+ #define USE_PTHREAD_CANCEL
