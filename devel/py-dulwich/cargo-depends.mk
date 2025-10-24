73b59ad65effcd062d24deb5f795ab6016392d2d
adam <adam@pkgsrc.org>
2025-10-24 07:34:20 +0000

    py-dulwich: updated to 0.24.7
    0.24.7  2025-10-23
    
     * Add sparse index support for improved performance with large repositories.
       Implements reading and writing of sparse directory entries, index expansion/
       collapse operations, and the 'sdir' extension.
    
     * Add support for core.fsyncObjectFiles configuration option.
    
     * Work around typing module bug in Python 3.9.0 and 3.9.1 by using string
       annotation for Callable type in reflog.py.
    
     * Fix passing ssh_command, password, and key_filename parameters to the SSH
       vendor. Regression from 0.24.2.
    
     * Fix LFS URL validation to prevent DNS resolution errors when ``lfs.url`` is
       configured with an invalid value. Implement full support for ``file://`` URLs
       to access local LFS repositories, matching git-lfs behavior.

--- devel/py-dulwich/cargo-depends.mk
+++ devel/py-dulwich/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.9 2025/10/20 07:18:41 adam Exp $
+# $NetBSD: cargo-depends.mk,v 1.10 2025/10/24 07:34:20 adam Exp $
 
 CARGO_CRATE_DEPENDS+=	autocfg-1.5.0
 CARGO_CRATE_DEPENDS+=	heck-0.5.0
@@ -9,11 +9,11 @@ CARGO_CRATE_DEPENDS+=	memoffset-0.9.1
 CARGO_CRATE_DEPENDS+=	once_cell-1.21.3
 CARGO_CRATE_DEPENDS+=	portable-atomic-1.11.1
 CARGO_CRATE_DEPENDS+=	proc-macro2-1.0.101
-CARGO_CRATE_DEPENDS+=	pyo3-0.26.0
-CARGO_CRATE_DEPENDS+=	pyo3-build-config-0.26.0
-CARGO_CRATE_DEPENDS+=	pyo3-ffi-0.26.0
-CARGO_CRATE_DEPENDS+=	pyo3-macros-0.26.0
-CARGO_CRATE_DEPENDS+=	pyo3-macros-backend-0.26.0
+CARGO_CRATE_DEPENDS+=	pyo3-0.27.0
+CARGO_CRATE_DEPENDS+=	pyo3-build-config-0.27.0
+CARGO_CRATE_DEPENDS+=	pyo3-ffi-0.27.0
+CARGO_CRATE_DEPENDS+=	pyo3-macros-0.27.0
+CARGO_CRATE_DEPENDS+=	pyo3-macros-backend-0.27.0
 CARGO_CRATE_DEPENDS+=	quote-1.0.41
 CARGO_CRATE_DEPENDS+=	syn-2.0.107
 CARGO_CRATE_DEPENDS+=	target-lexicon-0.13.3
