7164b74f04379d3b8e7f4a457e9d3511dfcb168d
tsutsui <tsutsui@pkgsrc.org>
2025-10-28 11:37:50 +0000

    firefox: 144.0 requires nss>=3.116.
    XXX1: no at-spi2-core>=2.58.1 even in HEAD? (2.54.0nb6 seems working)
    XXX2: at-spi2-core is already required by x11/gtk3/buildlink3.mk

--- www/firefox/mozilla-common.mk
+++ www/firefox/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.299 2025/10/27 14:24:32 ryoon Exp $
+# $NetBSD: mozilla-common.mk,v 1.300 2025/10/28 11:37:50 tsutsui Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -261,7 +261,7 @@ BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.34
 BUILDLINK_API_DEPENDS.icu+=	icu>=76.1
 .include "../../textproc/icu/buildlink3.mk"
 # See build/moz.configure/nss.configure
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.113
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.116
 .include "../../devel/nss/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 #.include "../../mk/jpeg.buildlink3.mk"
