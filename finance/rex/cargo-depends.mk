9df6f54796c1fc7af439ecadf31c6cfaeb8d998b
pin <pin@pkgsrc.org>
2025-10-28 08:50:24 +0000

    finance/rex: update to 0.2.2
    Updates
     - Fix incorrect balances on the homepage after tx position swap
     - The search page lists the date, month and year once again
    
    Changes
     - Fix home page balance after tx swap by @TheRustyPickle in #116
     - Search date by @TheRustyPickle in #117

--- finance/rex/cargo-depends.mk
+++ finance/rex/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.11 2025/10/12 12:28:21 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.12 2025/10/28 08:50:24 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
