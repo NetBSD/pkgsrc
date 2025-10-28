435afbf8c79091807c370a9f0fefe6ab99868973
taca <taca@pkgsrc.org>
2025-10-28 15:46:38 +0000

    www/squid7: Apply fix for Squid Bug 5520
    * Add patch for fix Squid bug 5520.
    * Reduce pkglint warnings.
    * There is no scripts/tcp-banger.pl any more.
    * Use PKG_OPTIONS.squid7 instead of PKG_OPTIONS.squid5.
    
    Bump PKGREVISION.

--- www/squid7/options.mk
+++ www/squid7/options.mk
@@ -1,6 +1,6 @@
-# $NetBSD: options.mk,v 1.1 2025/07/16 10:58:02 sborrill Exp $
+# $NetBSD: options.mk,v 1.2 2025/10/28 15:46:38 taca Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.squid5
+PKG_OPTIONS_VAR=		PKG_OPTIONS.squid7
 PKG_SUPPORTED_OPTIONS=		inet6 snmp squid-backend-aufs \
 				squid-backend-diskd squid-backend-rock \
 				squid-backend-ufs squid-ecap squid-esi \
