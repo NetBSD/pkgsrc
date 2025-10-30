c450c8e29ef3c69517b7e305c5386011efbe87eb
adam <adam@pkgsrc.org>
2025-10-30 14:58:40 +0000

    py-scipy: updated to 1.16.3
    Issues closed for 1.16.3
    
    BUG: linalg: wrong handling of args/kwargs in ``_apply_over_batch``
    BUG: huge speed and quality regression for stats.multivariate_normal.cdf
    BUG: ``lqmn(1,1,0)`` and ``lqmn(1,1,0j)`` return incompatible...
    CI: meson setup fails with "invalid int value: 'Sentinel.UNSET'"
    BUG: ``special.expi(inf)`` is ``nan``\ , should be ``inf``
    MAINT, TST: optimize test_input_validation torch failure
    TST, MAINT: boxcox_llf failing again
    
    Pull requests for 1.16.3
    
    REL, MAINT: prepare for SciPy 1.16.3
    TST, MAINT: backports, restore lfilter
    BUG: linalg:solve...are: fix bug when optional array is skipped
    MAINT: optimize: fix usage of ``nextafter(..., where=)`` without...
    CI: pin Python 3.11.13 in CircleCI job
    BUG:optimize:SLSQP: Use double negative in an if condition for...
    ENH: scipy.stats: speed up bivariate normal cdf
    BUG/TST: Bump xsf to v0.1.3 add tests for expi limits
    CI: optimize: allow new Pytorch message
    MAINT: patch vendored qhull source for security fix
    TST: stats.boxcox_llf: bump test tolerance

--- math/py-scipy/patches/patch-subprojects_xsf_include_xsf_amos_amos.h
+++ math/py-scipy/patches/patch-subprojects_xsf_include_xsf_amos_amos.h
@@ -1,16 +1,14 @@
-$NetBSD: patch-subprojects_xsf_include_xsf_amos_amos.h,v 1.1 2025/08/20 12:19:24 adam Exp $
+$NetBSD: patch-subprojects_xsf_include_xsf_amos_amos.h,v 1.2 2025/10/30 14:58:40 adam Exp $
 
 Prefer C++ headers.
 
---- subprojects/xsf/include/xsf/amos/amos.h.orig	2025-04-26 00:49:42.000000000 +0000
+--- subprojects/xsf/include/xsf/amos/amos.h.orig	2025-10-10 18:07:54.000000000 +0000
 +++ subprojects/xsf/include/xsf/amos/amos.h
-@@ -94,8 +94,8 @@
- 
+@@ -95,7 +95,7 @@
  #include <stdlib.h>
  
--#include <complex.h>
+ #include <complex>
 -#include <math.h>
-+#include <complex>
 +#include <cmath>
  #include <memory> // unique_ptr
  
