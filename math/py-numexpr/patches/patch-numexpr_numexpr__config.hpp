3a11d2948b3d692a538721698ef0e2e8d5165705
adam <adam@pkgsrc.org>
2025-10-30 15:19:43 +0000

    py-numexpr: updated to 2.14.1
    Changes from 2.14.0 to 2.14.1
    
    * Rolled back static typing support to ensure compatibiity with NumPy 1.26.
    * Added CI tests for NumPy 1.26

--- math/py-numexpr/patches/patch-numexpr_numexpr__config.hpp
+++ math/py-numexpr/patches/patch-numexpr_numexpr__config.hpp
@@ -1,34 +1,15 @@
-$NetBSD: patch-numexpr_numexpr__config.hpp,v 1.1 2025/09/14 14:03:44 wiz Exp $
+$NetBSD: patch-numexpr_numexpr__config.hpp,v 1.2 2025/10/30 15:19:43 adam Exp $
 
 Fix build on NetBSD.
-https://github.com/pydata/numexpr/issues/528
 
---- numexpr/numexpr_config.hpp.orig	2025-09-14 13:57:31.046041558 +0000
+--- numexpr/numexpr_config.hpp.orig	2025-10-13 15:50:29.000000000 +0000
 +++ numexpr/numexpr_config.hpp
-@@ -40,6 +40,7 @@
- #include "mkl_vml.h"
- #include "mkl_service.h"
+@@ -42,7 +42,7 @@
  #endif
-+#include <cmath>
+ #include <cmath>
+ //no single precision version of signbit in C++ standard
+-inline bool signbitf(float x) { return signbit((double)x); }
++inline bool signbitf(float x) { return std::signbit((double)x); }
  
  #ifdef _WIN32
    #ifndef __MINGW32__
-@@ -51,12 +52,12 @@
- msvc_function_stubs contains windows alternatives
- /* Due to casting problems (normally return ints not bools, easiest to define
- non-overloaded wrappers for these functions) */
--inline bool isfinitef_(float x) { return !!::isfinite(x); }
--inline bool isnanf_(float x)    { return !!::isnan(x); }
--inline bool isfinited(double x) { return !!::isfinite(x); }
--inline bool isnand(double x)    { return !!::isnan(x); }
--inline bool isinff_(float x) { return !!::isinf(x); }
--inline bool isinfd(double x)    { return !!::isinf(x); }
-+inline bool isfinitef_(float x) { return !!std::isfinite(x); }
-+inline bool isnanf_(float x)    { return !!std::isnan(x); }
-+inline bool isfinited(double x) { return !!std::isfinite(x); }
-+inline bool isnand(double x)    { return !!std::isnan(x); }
-+inline bool isinff_(float x) { return !!std::isinf(x); }
-+inline bool isinfd(double x)    { return !!std::isinf(x); }
- #endif
- 
- #endif // NUMEXPR_CONFIG_HPP
