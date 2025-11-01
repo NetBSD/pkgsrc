fcfd6380d1cf9f0ee33484bc44be7c99857b6f58
wiz <wiz@pkgsrc.org>
2025-11-01 22:30:38 +0000

    libkiwix: update to 14.1.0.
    libkiwix 14.1.0
    ===============
    
     * Server:
      - Viewer detects & tracks intrapage navigation anchors too (@veloman-yunkan #1213)
      - Add support for catalog only mode (@veloman-yunkan #1219)
      - Add API which returns server access url (@vighnesh-sawant #1234)
      - Fix chrome searchbar placeholder text overflow (@aditii2712 #1185)
      - Fix magnet link queryStyring (@rgaudin #1160)
      - Improve chrome printing stylesheet (@kelson42 #1202)
      - Default white background (@kelson42 #1205)
    
     * Other:
      - Switched to the new libzim illustrations API (@veloman-yunkan #1226)
      - Stop building Windows with DEBUG symbols in CI (@kelson42 #1165)
      - Update many things in the CI/CD (@kelson42 #1203 #1194 #1209 #1207 #1235)
      - Requires now libzim 9.4.0 (@kelson42 #1231)
      - Fix compilation for FreeBSD (@OICe2 #1173 #1174)
      - Wait up to 1s to let aria2c to start before complaining (@kelson42 #1169)

--- www/libkiwix/buildlink3.mk
+++ www/libkiwix/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/10/05 19:26:26 js Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/11/01 22:30:38 wiz Exp $
 
 BUILDLINK_TREE+=	libkiwix
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkiwix
 LIBKIWIX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkiwix+=	libkiwix>=14.0.0
-BUILDLINK_ABI_DEPENDS.libkiwix?=		libkiwix>=14.0.0nb1
+BUILDLINK_ABI_DEPENDS.libkiwix?=	libkiwix>=14.0.0nb1
 BUILDLINK_PKGSRCDIR.libkiwix?=		../../www/libkiwix
 
 .include "../../textproc/pugixml/buildlink3.mk"
