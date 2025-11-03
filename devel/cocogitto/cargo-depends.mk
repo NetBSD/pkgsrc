d281a30ab1aac5c529fd5e4620e5ee98b03d4f61
pin <pin@pkgsrc.org>
2025-11-03 07:01:41 +0000

    devel/cocogitto: update to 6.5.0
    Bug Fixes
    
        9d14c0b - set filetype for commit edit message - @oknozor
        5628b0c - correctly handle bumping from prerelease - @ba-lindner
    
    Continuous Integration
    
        9330f2b - add dispatch release to cocogitto-action - @oknozor
    
    Documentation
    
        3cf84ae - (bump) fix monorepo bump docs - @ba-lindner
        2ee784e - (changelog) update changelog docs to show current template output - @ba-lindner
    
    Features
    
        6f61a58 - (bump) add --include-packages option to allow manual bump for all monorepo versions - @ba-lindner
        55fb7f0 - (get-version) add options to print full tag and include prerelease versions - @ba-lindner
        12fe810 - add option to combine package and global changes into one changelog - @ba-lindner
        cba4101 - add builtin macros for tera templates - @oknozor
    
    Refactoring
    
        075d5a1 - (changelog) add tera extension to template files - @oknozor
        fa1dcce - unify getting next version for bump - @ba-lindner

--- devel/cocogitto/cargo-depends.mk
+++ devel/cocogitto/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.5 2025/10/18 14:45:39 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.6 2025/11/03 07:01:41 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.3
 CARGO_CRATE_DEPENDS+=	android_system_properties-0.1.5
