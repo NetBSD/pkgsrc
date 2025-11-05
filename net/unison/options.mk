0502ee074b0bf34035f4509a144685a90e00d629
gdt <gdt@pkgsrc.org>
2025-11-05 00:21:39 +0000

    net/unison: Update to 2.53.8
    Packaging changes:
    
      - drop gmake usage in pkgsrc makefile
      - enable lablgtk (because it builds now)
      - add fsmonitor option, enabled on NetBSD
    
    Upstream NEWS
    
    ## Changes in 2.53.8
    
    Released 2025-11-05
    
      * Improve and simplify makefiles.
        It is now possible to build with BSD make, Solaris/illumos make and
            NMAKE, in addition to GNU Make.  Building with MSVC is much simpler
            now.  The changes are larger than might be expected in a micro;
        anyone building from source (or packaging) should consult
        INSTALL.md for updated build instructions.
      * Stop installing fsmonitor.py.
      * Add ability to use reflinking instead of copying (if supported by
        filesystem).
      * Don't write fpcache if it is not necessary.
      * Experimental default-off support to detect moves/renames and
            rename on destination instead of copying (see -moves-experimental).
    
      Deprecation warning: support for external rsync will be removed,
      likely in 2.54.0.
    
      DEPRECATION MAY HAPPEN WITH LESS THAN TYPICAL NOTICE:
      lablgtk is difficult to use safely, and future maintenance is
      unclear.  Implementing the GUI without lablgtk requires more effort
      than seems likely to happen.  Thus, it is possible that support for
      the unison GUI may end suddenly, on a particular platform, or on all
      platforms -- even in a micro release.  See
        https://github.com/bcpierce00/unison/issues/1075
    
      MAINTENANCE WARNING: No one is contributing to verify that the Mac
      GUI continues to work -- so it might not.  (So far, there are no
      reports of serious failures.)

--- net/unison/options.mk
+++ net/unison/options.mk
@@ -1,17 +1,32 @@
-# $NetBSD: options.mk,v 1.9 2024/05/07 23:52:11 gdt Exp $
+# $NetBSD: options.mk,v 1.10 2025/11/05 00:21:39 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unison
-PKG_SUPPORTED_OPTIONS=	lablgtk
-# ocaml-lablgtk3 is broken
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS=	lablgtk inotify
+PKG_SUGGESTED_OPTIONS=	lablgtk
+
+# gtk3 is true if unison-gui is built.
+# monitor is true if unison-fsmonitor is built.
+PLIST_VARS+=	gtk3 monitor
+
+# Extend to systems as they are tested to build fsmonitor.
+# \todo FreeBSD, Linux, macOS, OpenBSD, SunOS.
+# \todo Perhaps, just enable unless libinotify says NOT_FOR_PLATFORM.
+
+.if ${MACHINE_PLATFORM:MNetBSD-*}
+PKG_SUGGESTED_OPTIONS+=	inotify
+.endif
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	gtk3
-
 .if !empty(PKG_OPTIONS:Mlablgtk)
 PLIST.gtk3=	yes
-DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
 
+DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
 .include "../../x11/ocaml-lablgtk3/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Minotify)
+PLIST.monitor=	yes
+
+.include "../../devel/libinotify/buildlink3.mk"
 .endif
