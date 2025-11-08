0818e7370e24e39235c77801916d4830c4af59df
bsiegert <bsiegert@pkgsrc.org>
2025-11-08 02:26:15 +0000

    go: update to 1.25.4 and 1.24.10
    go1.25.4 (released 2025-11-05) includes fixes to the compiler, the runtime, and
    the crypto/subtle, encoding/pem, net/url, and os packages.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.239 2025/10/16 17:58:17 bsiegert Exp $
+# $NetBSD: version.mk,v 1.240 2025/11/08 02:26:15 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,8 +6,8 @@
 #
 .include "go-vars.mk"
 
-GO125_VERSION=	1.25.3
-GO124_VERSION=	1.24.9
+GO125_VERSION=	1.25.4
+GO124_VERSION=	1.24.10
 GO123_VERSION=	1.23.12
 GO122_VERSION=	1.22.12
 GO120_VERSION=	1.20.14
