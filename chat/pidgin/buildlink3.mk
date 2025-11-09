6d85e32e72b6e74a306a458e7cb5a1963230d58b
wiz <wiz@pkgsrc.org>
2025-11-09 19:26:39 +0000

    libpurple, piding: pass thru include dirs
    Fixes pidgin-icb build

--- chat/pidgin/buildlink3.mk
+++ chat/pidgin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.54 2025/10/23 20:36:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.55 2025/11/09 19:26:39 wiz Exp $
 
 BUILDLINK_TREE+=	pidgin
 
@@ -8,6 +8,8 @@ PIDGIN_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.pidgin+=	pidgin>=2.5.1
 BUILDLINK_ABI_DEPENDS.pidgin+=	pidgin>=2.14.14nb4
 BUILDLINK_PKGSRCDIR.pidgin?=	../../chat/pidgin
+
+BUILDLINK_INCDIRS.pidgin+=	include/pidgin
 
 .include "../../chat/libpurple/buildlink3.mk"
 .endif # PIDGIN_BUILDLINK3_MK
