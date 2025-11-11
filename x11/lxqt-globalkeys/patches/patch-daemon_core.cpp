c31ab7a07da84c1cd1e03f13d5198179c2cf1ee6
gutteridge <gutteridge@pkgsrc.org>
2025-11-11 03:01:24 +0000

    lxqt-globalkeys: update to 2.3.0
    lxqt-globalkeys-2.3.0 / 2025-11-05
    ===================================
     * Capitalized "Id" as "ID", updated translations, and bumped the version.

--- x11/lxqt-globalkeys/patches/patch-daemon_core.cpp
+++ x11/lxqt-globalkeys/patches/patch-daemon_core.cpp
@@ -1,7 +1,7 @@
-$NetBSD: patch-daemon_core.cpp,v 1.4 2023/01/07 01:26:14 gutteridge Exp $
+$NetBSD: patch-daemon_core.cpp,v 1.5 2025/11/11 03:01:24 gutteridge Exp $
 
-Fix SunOS and OpenBSD builds, and work around a bug in NetBSD with the same
-macro (PR lib/57172).
+Fix SunOS and OpenBSD builds, and work around a bug in older NetBSD with
+the same macro (PR lib/57172).
 
 --- daemon/core.cpp.orig	2022-11-05 08:17:16.000000000 +0000
 +++ daemon/core.cpp
@@ -10,9 +10,9 @@ macro (PR lib/57172).
  #include "core.h"
 
 +/* 
-+ * SunOS and modern OpenBSD don't define this, and NetBSD's version has an
-+ * incorrect shift applied (log facilities are already defined with "<<3", it
-+ * shouldn't be done a second time).
++ * SunOS and modern OpenBSD don't define this, and older NetBSD versions have
++ * an incorrect shift applied (log facilities are already defined with "<<3",
++ * it shouldn't be done a second time).
 + */
 +#if !defined(LOG_MAKEPRI) || defined(__NetBSD__)
 +#define LOG_MAKEPRI(fac, pri)    ((fac) | (pri))
