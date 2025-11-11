7a45788f545655afd9434fe7baf7f40b96070f0b
vins <vins@pkgsrc.org>
2025-11-11 20:40:56 +0000

    mail/imap-uw: fix build with gcc14 on NetBSD
    Since we're here, apply some lint fixes.

--- mail/imap-uw/buildlink3.mk
+++ mail/imap-uw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2023/10/24 22:09:43 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2025/11/11 20:40:56 vins Exp $
 
 BUILDLINK_TREE+=	imap-uw
 
@@ -8,13 +8,19 @@ IMAP_UW_BUILDLINK3_MK:=
 .include "../../mk/bsd.fast.prefs.mk"
 
 BUILDLINK_API_DEPENDS.imap-uw+=	imap-uw>=2007dnb1
-BUILDLINK_ABI_DEPENDS.imap-uw+=	imap-uw>=2007fnb12
+BUILDLINK_ABI_DEPENDS.imap-uw+=	imap-uw>=2007fnb15
 BUILDLINK_PKGSRCDIR.imap-uw?=	../../mail/imap-uw
 .if ${OPSYS} == "Darwin"
 BUILDLINK_LDFLAGS.imap-uw+=	-flat_namespace
 .endif
 
-.include "../../security/openssl/buildlink3.mk"
+pkgbase :=      imap-uw
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.imap-uw:Mssl}
+
+.  include "../../security/openssl/buildlink3.mk"
+.endif # PKG_BUILD_OPTIONS
 .endif # IMAP_UW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-imap-uw
