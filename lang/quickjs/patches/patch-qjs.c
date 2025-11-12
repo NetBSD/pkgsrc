e9b2fadb621e78a87d7fb9db8e63c3d64520e310
leot <leot@pkgsrc.org>
2025-11-12 11:49:38 +0000

    quickjs: Update to 20250913
    pkgsrc changes:
    - Remove patches no longer needed because applied upstream
    
    Changes:
    2025-09-13:
    - added JSON modules and import attributes
    - added JS_PrintValue() API
    - qjs: pretty print objects in print() and console.log()
    - qjs: better promise rejection tracker heuristics
    - added RegExp v flag
    - added RegExp modifiers
    - added RegExp.escape
    - added Float16Array
    - added Promise.try
    - improved JSON parser spec conformance
    - qjs: improved compatibility of std.parseExtJSON() with JSON5 and
      accept JSON5 modules
    - added JS_FreePropertyEnum() and JS_AtomToCStringLen() API
    - added Error.isError()
    - misc bug fixes
    
    2025-04-26:
    - removed the bignum extensions and qjscalc
    - new BigInt implementation optimized for small numbers
    - added WeakRef, FinalizationRegistry and symbols as weakrefs
    - added builtin float64 printing and parsing functions for more correctness
    - faster repeated string concatenation
    - qjs: promise unhandled rejections are fatal errors by default
    - added column number in debug information
    - removed the "use strip" extension
    - qjs: added -s and --strip-source options
    - qjsc: added -s and --keep-source options
    - added JS_GetAnyOpaque()
    - added more callbacks for exotic objects in JSClassExoticMethods
    - misc bug fixes

--- lang/quickjs/patches/patch-qjs.c
+++ lang/quickjs/patches/patch-qjs.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-qjs.c,v 1.3 2025/02/07 12:36:24 pho Exp $
+$NetBSD: patch-qjs.c,v 1.4 2025/11/12 11:49:38 leot Exp $
 
 Portability patch for NetBSD.
 
---- qjs.c.orig	2024-01-13 10:20:39.000000000 +0000
+--- qjs.c.orig	2025-09-13 08:48:28.000000000 +0000
 +++ qjs.c
-@@ -146,7 +146,7 @@ static size_t js_trace_malloc_usable_siz
+@@ -136,7 +136,7 @@ static size_t js_trace_malloc_usable_siz
      return malloc_size(ptr);
  #elif defined(_WIN32)
      return _msize((void *)ptr);
 -#elif defined(EMSCRIPTEN)
 +#elif defined(EMSCRIPTEN) || defined(__NetBSD__)
      return 0;
- #elif defined(__linux__)
+ #elif defined(__linux__) || defined(__GLIBC__)
      return malloc_usable_size((void *)ptr);
