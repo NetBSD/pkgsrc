2f71f6ff42149bce65a335705c69c47ea20b3ab7
leot <leot@pkgsrc.org>
2025-11-12 11:49:38 +0000

    quickjs: Update to 20250913
    pkgsrc changes:
    - Remove patches no longer needed because applied upstream
    
    Changes:
    2025-09-13:
    - added JSON modules and import attributes
    - added JS_PrintValue() API
    - qjs: pretty print objects in print() and console.log()
    - qjs: better promise rejection tracker heuristics
    - added RegExp v flag
    - added RegExp modifiers
    - added RegExp.escape
    - added Float16Array
    - added Promise.try
    - improved JSON parser spec conformance
    - qjs: improved compatibility of std.parseExtJSON() with JSON5 and
      accept JSON5 modules
    - added JS_FreePropertyEnum() and JS_AtomToCStringLen() API
    - added Error.isError()
    - misc bug fixes
    
    2025-04-26:
    - removed the bignum extensions and qjscalc
    - new BigInt implementation optimized for small numbers
    - added WeakRef, FinalizationRegistry and symbols as weakrefs
    - added builtin float64 printing and parsing functions for more correctness
    - faster repeated string concatenation
    - qjs: promise unhandled rejections are fatal errors by default
    - added column number in debug information
    - removed the "use strip" extension
    - qjs: added -s and --strip-source options
    - qjsc: added -s and --keep-source options
    - added JS_GetAnyOpaque()
    - added more callbacks for exotic objects in JSClassExoticMethods
    - misc bug fixes

--- lang/quickjs/patches/patch-qjsc.c
+++ lang/quickjs/patches/patch-qjsc.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-qjsc.c,v 1.2 2022/01/30 10:55:03 he Exp $
+$NetBSD: patch-qjsc.c,v 1.3 2025/11/12 11:49:38 leot Exp $
 
 Portability patch for NetBSD.
 
---- qjsc.c.orig	2021-03-27 10:00:32.000000000 +0000
+--- qjsc.c.orig	2025-09-13 08:48:28.000000000 +0000
 +++ qjsc.c
-@@ -450,7 +450,9 @@ static int output_executable(const char 
+@@ -502,7 +502,9 @@ static int output_executable(const char 
               lib_dir, bn_suffix, lto_suffix);
      *arg++ = libjsname;
      *arg++ = "-lm";
@@ -13,4 +13,4 @@ Portability patch for NetBSD.
 +#endif
      *arg++ = "-lpthread";
      *arg = NULL;
-     
+ 
