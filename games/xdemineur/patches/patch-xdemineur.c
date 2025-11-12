e2ad6584de1571e150bdb208d7b062ceeae336ae
pgoyette <pgoyette@pkgsrc.org>
2025-11-12 05:28:19 +0000

    Include <string.h> as needed - gcc14

---
+++ games/xdemineur/patches/patch-xdemineur.c
@@ -0,0 +1,10 @@
+--- xdemineur.c.orig	2025-11-11 21:18:56.344085309 -0800
++++ xdemineur.c	2025-11-11 21:17:29.968673309 -0800
+@@ -19,6 +19,7 @@
+ #include <errno.h>
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <sysexits.h>
+ #include <unistd.h>
+ 
