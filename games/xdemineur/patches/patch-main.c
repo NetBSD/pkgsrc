d972f66f8fa95a0b5bf1000e8fa98d79fd6030d1
pgoyette <pgoyette@pkgsrc.org>
2025-11-12 05:28:19 +0000

    Include <string.h> as needed - gcc14

---
+++ games/xdemineur/patches/patch-main.c
@@ -0,0 +1,10 @@
+--- main.c.orig	1999-08-01 13:57:46.000000000 -0700
++++ main.c	2025-11-11 21:22:02.926660266 -0800
+@@ -19,6 +19,7 @@
+ #include <signal.h>
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <sysexits.h>
+ #include <time.h>
+ 
