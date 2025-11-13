04b7f1a1c45e6413edf2d24dc7ddb2769b209867
adam <adam@pkgsrc.org>
2025-11-13 19:00:12 +0000

    nodejs24: added version 24.11.1
    24.11.1 'Krypton' (LTS)
    
    Notable Changes
    
    The known issue relating to Buffer.allocUnsafe incorrectly zero-filling buffers
    has now been addressed and now returns uninitialized memory as documented in the
    Buffer.allocUnsafe
    documentation.

---
+++ lang/nodejs24/patches/patch-deps_v8_src_base_platform_memory.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-deps_v8_src_base_platform_memory.h,v 1.1 2025/11/13 19:00:13 adam Exp $
+
+Fix build on NetBSD.
+
+--- deps/v8/src/base/platform/memory.h.orig	2025-05-06 12:36:30.000000000 +0000
++++ deps/v8/src/base/platform/memory.h
+@@ -27,7 +27,7 @@
+ #include <malloc.h>
+ #endif
+ 
+-#if (V8_OS_POSIX && !V8_OS_AIX && !V8_OS_SOLARIS && !V8_OS_ZOS && !V8_OS_OPENBSD) || V8_OS_WIN
++#if (V8_OS_POSIX && !V8_OS_AIX && !V8_OS_SOLARIS && !V8_OS_ZOS && !V8_OS_OPENBSD && !V8_OS_NETBSD) || V8_OS_WIN
+ #define V8_HAS_MALLOC_USABLE_SIZE 1
+ #endif
+ 
