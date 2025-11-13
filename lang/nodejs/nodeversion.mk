973e55fd110c97fb3992e6dc0481f2502edb80d5
adam <adam@pkgsrc.org>
2025-11-13 19:01:12 +0000

    nodejs: updated to 25.2.0
    25.2.0 (Current)
    
    Notable Changes
    
    - (SEMVER-MINOR) lib: add options to util.deprecate (Rafael Gonzaga)
    - lib: throw from localStorage getter on missing storage path (René)
    - (SEMVER-MINOR) module: mark type stripping as stable (Marco Ippolito)
    - (SEMVER-MINOR) net: increase network family autoselection timeout to 500ms (Rod Vagg)
    - (SEMVER-MINOR) node-api: add napi_create_object_with_properties (Miguel Marcondes Filho)
    - (SEMVER-MINOR) v8: adding total_allocated_bytes to HeapStatistics (Caio Lima)

--- lang/nodejs/nodeversion.mk
+++ lang/nodejs/nodeversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: nodeversion.mk,v 1.21 2025/05/07 05:56:47 adam Exp $
+# $NetBSD: nodeversion.mk,v 1.22 2025/11/13 19:01:12 adam Exp $
 
 # This file determines which nodejs version is used as a dependency for
 # a package.
@@ -8,7 +8,7 @@
 # NODE_VERSION_DEFAULT
 #	The preferred node version to use.
 #
-#	Possible values: 20 22 24
+#	Possible values: 20 22 24 25
 #	Default: 20
 #
 # === Infrastructure variables ===
@@ -27,13 +27,13 @@
 #	is significant; those listed earlier are preferred over those
 #	listed later.
 #
-#	Possible values: 20 22 24
+#	Possible values: 20 22 24 25
 #	Default: 20 22 24
 #
 # NODE_VERSIONS_INCOMPATIBLE
 #	The node versions that the package *cannot* build against.
 #
-#	Possible values: 20 22 24
+#	Possible values: 20 22 24 25
 #	Default: <empty>
 #
 # Keywords: node
@@ -56,7 +56,7 @@ BUILD_DEFS+=		NODE_VERSION_DEFAULT
 BUILD_DEFS_EFFECTS+=	NODE_PACKAGE
 
 NODE_VERSION_DEFAULT?=		20
-NODE_VERSIONS_ACCEPTED?=	20 22 24
+NODE_VERSIONS_ACCEPTED?=	20 22 24 25
 NODE_VERSIONS_INCOMPATIBLE?=	# empty
 
 # Resolve NODE_VERSIONS_INCOMPATIBLE and generate the _OK vars.
@@ -91,6 +91,8 @@ _NODE_VERSION?=		none
 .elif ${_NODE_VERSION} == "22"
 .include "../../lang/nodejs22/buildlink3.mk"
 .elif ${_NODE_VERSION} == "24"
+.include "../../lang/nodejs24/buildlink3.mk"
+.elif ${_NODE_VERSION} == "25"
 .include "../../lang/nodejs/buildlink3.mk"
 .else
 PKG_FAIL_REASON+=	"No valid node version found"
---
+++ lang/nodejs/patches/patch-deps_v8_third__party_abseil-cpp_absl_debugging_internal_elf__mem__image.cc
@@ -0,0 +1,18 @@
+$NetBSD: patch-deps_v8_third__party_abseil-cpp_absl_debugging_internal_elf__mem__image.cc,v 1.1 2025/11/13 19:01:13 adam Exp $
+
+Fix build on NetBSD.
+
+--- deps/v8/third_party/abseil-cpp/absl/debugging/internal/elf_mem_image.cc.orig	2025-11-13 13:08:23.508427916 +0000
++++ deps/v8/third_party/abseil-cpp/absl/debugging/internal/elf_mem_image.cc
+@@ -377,11 +377,7 @@ void ElfMemImage::SymbolIterator::Update
+   const ElfW(Versym) *version_symbol = image->GetVersym(index_);
+   ABSL_RAW_CHECK(symbol && version_symbol, "");
+   const char *const symbol_name = image->GetDynstr(symbol->st_name);
+-#if defined(__NetBSD__)
+-  const int version_index = version_symbol->vs_vers & VERSYM_VERSION;
+-#else
+   const ElfW(Versym) version_index = version_symbol[0] & VERSYM_VERSION;
+-#endif
+   const ElfW(Verdef) *version_definition = nullptr;
+   const char *version_name = "";
+   if (symbol->st_shndx == SHN_UNDEF) {
