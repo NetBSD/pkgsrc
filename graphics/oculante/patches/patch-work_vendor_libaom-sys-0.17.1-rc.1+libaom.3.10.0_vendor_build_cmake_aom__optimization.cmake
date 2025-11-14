7da6e5d411bb9dc3d45d2dce3390185a9e041469
wiz <wiz@pkgsrc.org>
2025-11-14 08:28:04 +0000

    oculante: add link to upstream-upstream commit

--- graphics/oculante/patches/patch-work_vendor_libaom-sys-0.17.1-rc.1+libaom.3.10.0_vendor_build_cmake_aom__optimization.cmake
+++ graphics/oculante/patches/patch-work_vendor_libaom-sys-0.17.1-rc.1+libaom.3.10.0_vendor_build_cmake_aom__optimization.cmake
@@ -1,6 +1,7 @@
-$NetBSD: patch-work_vendor_libaom-sys-0.17.1-rc.1+libaom.3.10.0_vendor_build_cmake_aom__optimization.cmake,v 1.1 2025/11/14 08:22:20 wiz Exp $
+$NetBSD: patch-work_vendor_libaom-sys-0.17.1-rc.1+libaom.3.10.0_vendor_build_cmake_aom__optimization.cmake,v 1.2 2025/11/14 08:28:04 wiz Exp $
 
 Fix build with nasm 3.0.
+https://aomedia.googlesource.com/aom/+/6d2b7f71b98bfa28e372b1f2d85f137280bdb3de%5E%21/
 
 --- work/vendor/libaom-sys-0.17.1-rc.1+libaom.3.10.0/vendor/build/cmake/aom_optimization.cmake.orig	2006-07-24 01:21:28.000000000 +0000
 +++ work/vendor/libaom-sys-0.17.1-rc.1+libaom.3.10.0/vendor/build/cmake/aom_optimization.cmake
