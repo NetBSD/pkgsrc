7f36a24397e1391dc95866e843eae0c30ab5963d
wiz <wiz@pkgsrc.org>
2025-11-16 20:59:55 +0000

    py-maturin: update to 1.10.1.
    ## [1.10.1]
    
    * Fix wrong dependency on Homebrew liblzma on macOS by static linking liblama
    
    ## [1.10.0]
    
    * Add `tool.maturin.editable-profile` option to override profile for editable package installations.
    * Add support for Cygwin.
    * When building `abi3` wheels on non-Windows platforms that aren't cross-compiling, the `sysconfigdata` of the interpreter used to run maturin will now be used, rather than a dummy interpreter.
    * Allow iOS cross-platform virtual environments, such as those used by cibuildwheel, to imply an iOS target.
    * Fix iOS wheel naming to be compliant with PEP 730.
    * Fix generated WHEEL Tag metadata to be spec compliant.
    * Fix incorrect warning about missing `extension-module` feature on PyO3 0.26+.
    * Remove `add_directory()` from ModuleWriter and make it an implementation detail for the specific impl.
    * Clear out uid/gid and set deterministic mtime for files in sdist.
    * Always use "library" mode to build uniffi bindings.

--- devel/py-maturin/patches/patch-src_build__context.rs
+++ devel/py-maturin/patches/patch-src_build__context.rs
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_build__context.rs,v 1.4 2025/08/29 09:51:37 adam Exp $
+$NetBSD: patch-src_build__context.rs,v 1.5 2025/11/16 20:59:55 wiz Exp $
 
 Fix wrong python wheel tag on NetBSD/evbarm.
 
---- src/build_context.rs.orig	2025-08-27 11:23:56.000000000 +0000
+--- src/build_context.rs.orig	2006-07-24 01:21:28.000000000 +0000
 +++ src/build_context.rs
-@@ -623,6 +623,17 @@ impl BuildContext {
-                     format!("macosx_{x86_64_tag}_x86_64")
-                 }
+@@ -635,6 +635,17 @@ impl BuildContext {
+                 let (min_sdk_major, min_sdk_minor) = iphoneos_deployment_target(env::var("IPHONEOS_DEPLOYMENT_TARGET").ok().as_deref())?;
+                 format!("ios_{min_sdk_major}_{min_sdk_minor}_{arch}_{abi}")
              }
 +            // NetBSD evbarm
 +            (Os::NetBsd, Arch::Armv7L)
