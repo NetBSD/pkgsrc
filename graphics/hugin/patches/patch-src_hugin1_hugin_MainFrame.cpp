3ad18221c2fde354f178f93596eb9e60cadb728d
adam <adam@pkgsrc.org>
2025-11-19 16:00:48 +0000

    hugin: updated to 25.0.0
    Changes since 2024.0.1
    
    The most changes are under the hood and affect mainly the builders. Now a C++17 compiler is needed. Therefore the boost library is not needed anymore. Also libjpeg and libpng are not needed when building the shared version (which is the default). wxWidgets 3.2 or later is now required. The code has be refactored to follow the modern wxWidgets code style. With this change Hugin is now using vector SVG files instead of raster PNG files for the toolboor and buttons. This should improve the appearance on hdpi displays. Depending on the used compiler it may necessary to use a more up-to-date version of the needed libraries (e.g. libpano 2.9.23)
    A new GUI has be added: hugin_toolbox. It provides a GUI for enfuse and for simple perspective correction.
    Beside there are many small bugfixes.

--- graphics/hugin/patches/patch-src_hugin1_hugin_MainFrame.cpp
+++ graphics/hugin/patches/patch-src_hugin1_hugin_MainFrame.cpp
@@ -1,15 +1,16 @@
-$NetBSD: patch-src_hugin1_hugin_MainFrame.cpp,v 1.1 2025/03/05 06:27:43 adam Exp $
+$NetBSD: patch-src_hugin1_hugin_MainFrame.cpp,v 1.2 2025/11/19 16:00:48 adam Exp $
 
 Fix crash on Darwin during startup.
 
---- src/hugin1/hugin/MainFrame.cpp.orig	2025-03-04 10:02:46.088652871 +0000
+--- src/hugin1/hugin/MainFrame.cpp.orig	2025-09-28 16:07:49.000000000 +0000
 +++ src/hugin1/hugin/MainFrame.cpp
-@@ -379,7 +379,7 @@ MainFrame::MainFrame(wxWindow* parent, H
+@@ -297,7 +297,9 @@ MainFrame::MainFrame(wxWindow* parent, H
              dc.DrawText(version, bitmap.GetWidth() - tw - 3, bitmap.GetHeight() - th - 3);
              dc.SelectObject(wxNullBitmap);
          }
--#if wxCHECK_VERSION(3,1,6)
-+#if !defined(__WXMAC__) && wxCHECK_VERSION(3,1,6)
++#if !defined(__WXMAC__)
          bitmap.SetScaleFactor(GetDPIScaleFactor());
- #endif
++#endif
          splash = new HuginSplashScreen(NULL, bitmap);
+     } else {
+         wxLogFatalError(_("Fatal installation error\nThe file data/splash.png was not found at:") + huginApp::Get()->GetXRCPath());
