f4562d1089db0f90d7e11339f986d1c5e9e87729
nia <nia@pkgsrc.org>
2025-11-19 08:47:25 +0000

    pkcs11-helper: Update to 1.31.0
       2025-11-10 - Version 1.31.0
    
         * threading: fix mutex handling for cond_wait, thanks to Gleb Popov.
         * mbed: initialize certificate early using mbedtls_x509_crt_init.
         * util: fix deserialize buffer overflow. thanks to Aarnav Bos.
    
    Builds on at least macOS, Linux, FreeBSD, NetBSD, OpenBSD.

---
+++ security/pkcs11-helper/patches/patch-lib_pkcs11h-util.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_pkcs11h-util.c,v 1.1 2025/11/19 08:47:25 nia Exp $
+
+Fix ctype usage.
+
+--- lib/pkcs11h-util.c.orig	2025-11-19 08:30:09.777808327 +0000
++++ lib/pkcs11h-util.c
+@@ -179,7 +179,7 @@ _pkcs11h_util_escapeString (
+ 
+ 	while (*s != '\x0') {
+ 
+-		if (*s == '\\' || strchr (invalid_chars, (unsigned char)*s) || !isgraph (*s)) {
++		if (*s == '\\' || strchr (invalid_chars, (unsigned char)*s) || !isgraph ((unsigned char)*s)) {
+ 			if (t != NULL) {
+ 				if (n+4 > *max) {
+ 					rv = CKR_ATTRIBUTE_VALUE_INVALID;
