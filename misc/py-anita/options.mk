a31bbde1e7ffc8f922173daff93b4c4f4bf3a01d
gson <gson@pkgsrc.org>
2025-11-22 13:02:31 +0000

    misc/py-anita: update to 2.17.
    Changes from 2.15:
    
    When downloading a file results in an error, attempt to determine
    whether the error is due to the expected nonexistence of the file or
    some other error that should be treated differently.
    
    Fix a compatibility issue between setup.py and recent pkgsrc pip.
    
    Deal with Python 3.13 no longer having urllib.FancyURLopener

--- misc/py-anita/options.mk
+++ misc/py-anita/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.15 2023/12/07 17:27:17 gdt Exp $
+# $NetBSD: options.mk,v 1.16 2025/11/22 13:02:31 gson Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.py-anita
 PKG_SUPPORTED_OPTIONS=		gxemul qemu simh xen
@@ -30,8 +30,7 @@ DEPENDS+=	qemu>=3:../../emulators/qemu
 DEPENDS+=	open-simh>=3:../../emulators/open-simh
 .endif
 
-# Xen 415 is the standard approach, per the Xen HOWTO.
 .if !empty(PKG_OPTIONS:Mxen)
-DEPENDS+=	xenkernel415>=4.2:../../sysutils/xenkernel415
-DEPENDS+=	xentools415>=4.2:../../sysutils/xentools415
+DEPENDS+=	xenkernel418-[0-9]*:../../sysutils/xenkernel418
+DEPENDS+=	xentools418-[0-9]*:../../sysutils/xentools418
 .endif
