40f8943057a46e3ad3aacda8afd005050259836f
ryoon <ryoon@pkgsrc.org>
2025-11-23 13:38:20 +0000

    x11/gtk4: Remove references to nonexistent file

--- x11/gtk4/modules.mk
+++ x11/gtk4/modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: modules.mk,v 1.1 2025/11/15 11:50:12 ryoon Exp $
+# $NetBSD: modules.mk,v 1.2 2025/11/23 13:38:20 ryoon Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # GTK4 modules.  It takes care of rebuilding the corresponding databases at
@@ -11,8 +11,6 @@
 .if !defined(GTK4_MODULES_MK)
 GTK4_MODULES_MK=	# defined
 
-GTK4_QUERY_IMMODULES=	${BUILDLINK_PREFIX.gtk4}/bin/gtk-query-immodules-4.0
-
 .if defined(_BUILDING_GTK4)
 BUILDLINK_PREFIX.gtk4=	${PREFIX}
 GTK4_IMMODULES=		YES
@@ -20,7 +18,6 @@ GTK4_IMMODULES=		YES
 
 .if defined(GTK4_IMMODULES) && !empty(GTK4_IMMODULES:M[Yy][Ee][Ss])
 FILES_SUBST+=		GTK4_IMMODULES_DB=${GTK4_IMMODULES_DB:Q}
-FILES_SUBST+=		GTK4_QUERY_IMMODULES=${GTK4_QUERY_IMMODULES:Q}
 GTK4_IMMODULES_DB=	${BUILDLINK_PREFIX.gtk4}/lib/gtk-4.0/4.0.0/immodules.cache
 INSTALL_TEMPLATES+=	../../x11/gtk4/files/immodules.tmpl
 DEINSTALL_TEMPLATES+=	../../x11/gtk4/files/immodules.tmpl
