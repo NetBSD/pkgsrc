196a588a99f221cb8a2d2de24d51d42a52c601bc
wiz <wiz@pkgsrc.org>
2025-11-23 07:04:32 +0000

    vcdimager: fix to arch patch for fixing build with libxml2.
    Bump PKGREVISION.

--- sysutils/vcdimager/patches/patch-frontends_xml_vcd__xml__common.c
+++ sysutils/vcdimager/patches/patch-frontends_xml_vcd__xml__common.c
@@ -1,25 +1,41 @@
-$NetBSD: patch-frontends_xml_vcd__xml__common.c,v 1.1 2025/04/25 10:42:29 wiz Exp $
+$NetBSD: patch-frontends_xml_vcd__xml__common.c,v 1.2 2025/11/23 07:04:32 wiz Exp $
 
-Fix build with libxml2 2.14.
-https://savannah.gnu.org/support/index.php?111233
+Fix build with libxml 2.14.
+https://gitlab.archlinux.org/archlinux/packaging/packages/vcdimager/-/raw/88dc511b7f3dea8fb45e0c2bfa1345a75a088848/libxml214.diff
 
---- frontends/xml/vcd_xml_common.c.orig	2025-04-25 10:37:59.722970842 +0000
+--- frontends/xml/vcd_xml_common.c.orig	2018-01-03 20:17:37.000000000 +0000
 +++ frontends/xml/vcd_xml_common.c
-@@ -242,13 +242,13 @@ _convert (const char in[], const char en
+@@ -242,17 +242,27 @@ _convert (const char in[], const char en
  
    temp = size - 1;
    if (from) {
 -    if (NULL != handler->output)
 -      ret = handler->output (out, &out_size, (const unsigned char *) in, &temp);
-+    if (NULL != handler->output.legacyFunc)
+-    else
++    if (!(handler->flags & 2) && NULL != handler->output.func)
++      ret = handler->output.func (handler->outputCtxt, out, &out_size, (const unsigned char *) in, &temp, 1);
++    if ((handler->flags & 2) && NULL != handler->output.legacyFunc)
 +      ret = handler->output.legacyFunc (out, &out_size, (const unsigned char *) in, &temp);
-     else
++    else {
++      xmlCharEncCloseFunc(handler);
        return strdup(in);
++    }
    } else {
 -    if (NULL != handler->input)
 -      ret = handler->input (out, &out_size, (const unsigned char *) in, &temp);
-+    if (NULL != handler->input.legacyFunc)
+-    else
++    if (!(handler->flags & 2) && NULL != handler->input.func)
++      ret = handler->input.func (handler->inputCtxt, out, &out_size, (const unsigned char *) in, &temp, 1);
++    if ((handler->flags & 2) && NULL != handler->input.legacyFunc)
 +      ret = handler->input.legacyFunc (out, &out_size, (const unsigned char *) in, &temp);
-     else
++    else {
++      xmlCharEncCloseFunc(handler);
        return strdup(in);
++    }
    }
+ 
++  xmlCharEncCloseFunc(handler);
++
+   if (ret < 0 || (temp - size + 1))
+     {
+       free (out);
