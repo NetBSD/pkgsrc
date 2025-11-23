bd5a96d9c8f583eb498ea3a9a8b78532b19cc5c9
markd <markd@pkgsrc.org>
2025-11-23 19:52:14 +0000

    plasma6*: update plasma6 packages to 6.5.2
    17 months of development.

--- meta-pkgs/kde/plasma6.mk
+++ meta-pkgs/kde/plasma6.mk
@@ -1,9 +1,11 @@
-# $NetBSD: plasma6.mk,v 1.2 2024/06/09 00:42:14 markd Exp $
+# $NetBSD: plasma6.mk,v 1.3 2025/11/23 19:52:14 markd Exp $
 
-PLASMAVER?=	6.0.5
+PLASMAVER?=	6.5.2
 CATEGORIES+=	kde
 MASTER_SITES=	${MASTER_SITE_KDE:=plasma/${PLASMAVER}/}
 EXTRACT_SUFX=	.tar.xz
 PKGNAME?=	plasma6-${DISTNAME}
+
+USE_CXX_FEATURES+=	c++20
 
 .include "../../meta-pkgs/kde/Makefile.common"
