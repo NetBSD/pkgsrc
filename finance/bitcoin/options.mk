e02b3627e5ec7cbeaaba7421de49c59a5e6c9f21
adam <adam@pkgsrc.org>
2025-11-24 07:25:24 +0000

    bitcoin: updated to 30.0
    30.0
    https://github.com/bitcoin/bitcoin/blob/master/doc/release-notes/release-notes-30.0.md

--- finance/bitcoin/options.mk
+++ finance/bitcoin/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.4 2025/07/17 11:22:59 adam Exp $
+# $NetBSD: options.mk,v 1.5 2025/11/24 07:25:24 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bitcoin
-PKG_SUPPORTED_OPTIONS+=	qt5
-# qt5 is off because it doubles the footprint of the package.
+PKG_SUPPORTED_OPTIONS+=	qt6
+# qt6 is off because it doubles the footprint of the package.
 # Please do not enable it by default; instead, create a split
 # package.
 
@@ -10,10 +10,10 @@ PKG_SUPPORTED_OPTIONS+=	qt5
 
 PLIST_VARS+=	qt
 
-.if !empty(PKG_OPTIONS:Mqt5)
+.if !empty(PKG_OPTIONS:Mqt6)
 CMAKE_CONFIGURE_ARGS+=	-DBUILD_GUI=ON
 PLIST.qt=	yes
 .include "../../converters/qrencode/buildlink3.mk"
 .include "../../devel/protobuf/buildlink3.mk"
-.include "../../x11/qt5-qttools/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif
