9db90f8211b5eead00880bf4da3168e2f7106200
hauke <hauke@pkgsrc.org>
2025-11-28 18:01:43 +0000

    If upstream specified the version of the lua interpreter, make sure we replace that, too.

--- lang/lua/application.mk
+++ lang/lua/application.mk
@@ -1,4 +1,4 @@
-# $NetBSD: application.mk,v 1.4 2014/05/03 13:00:44 alnsn Exp $
+# $NetBSD: application.mk,v 1.5 2025/11/28 18:01:43 hauke Exp $
 #
 # This makefile fragment is intended to be included by packages that
 # install Lua scripts.
@@ -17,7 +17,7 @@
 
 .if defined(REPLACE_LUA)
 REPLACE_INTERPRETER+=	lua
-REPLACE.lua.old=	.*lua
+REPLACE.lua.old=	.*lua[^[:space:]]*
 REPLACE.lua.new=	${LUA_INTERPRETER}
 REPLACE_FILES.lua=	${REPLACE_LUA}
 .endif
