21ede4768dd970dc93d1a0f0a685b60b3b43bda9
pin <pin@pkgsrc.org>
2025-11-29 20:21:13 +0000

    textproc/xan: update to 0.54.1
    Fixes
     - Fixing xan freq --groupby incorrectly unescaping group cells.
     - Fixing help related to xan pivot & xan unpivot.
     - Upgrading simd-csv to get safety fixes.

--- textproc/xan/cargo-depends.mk
+++ textproc/xan/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.4 2025/11/18 13:41:20 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.5 2025/11/29 20:21:13 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	ahash-0.8.12
@@ -231,7 +231,7 @@ CARGO_CRATE_DEPENDS+=	servo_arc-0.4.1
 CARGO_CRATE_DEPENDS+=	sha2-0.10.9
 CARGO_CRATE_DEPENDS+=	shlex-1.3.0
 CARGO_CRATE_DEPENDS+=	simd-adler32-0.3.7
-CARGO_CRATE_DEPENDS+=	simd-csv-0.9.0
+CARGO_CRATE_DEPENDS+=	simd-csv-0.10.2
 CARGO_CRATE_DEPENDS+=	simd-json-0.14.3
 CARGO_CRATE_DEPENDS+=	simdutf8-0.1.5
 CARGO_CRATE_DEPENDS+=	siphasher-1.0.1
