34f73e0962e5177d72a0b777003cc93027cea896
vins <vins@pkgsrc.org>
2025-11-30 19:17:09 +0000

    libtirpc: allow detection of native Sun RPC library inside builtin.mk

--- devel/libtirpc/builtin.mk
+++ devel/libtirpc/builtin.mk
@@ -1,10 +1,14 @@
-# $NetBSD: builtin.mk,v 1.2 2025/04/11 22:19:49 maya Exp $
+# $NetBSD: builtin.mk,v 1.3 2025/11/30 19:17:09 vins Exp $
 
 BUILTIN_PKG:=	libtirpc
 
-BUILTIN_FIND_HEADERS_VAR:=		H_LIBTIRPC
-BUILTIN_FIND_HEADERS.H_LIBTIRPC=	rpc/rpc.h tirpc/rpc/rpc.h
-BUILTIN_FIND_GREP.H_LIBTIRPC=		\#define[ 	]*_TIRPC_RPC_H
+BUILTIN_FIND_LIBS:=		tirpc
+BUILTIN_FIND_HEADERS_VAR:=	H_TIRPC H_RPC
+
+BUILTIN_FIND_HEADERS.H_RPC=	rpc/rpc.h
+BUILTIN_FIND_HEADERS.H_TIRPC=	tirpc/rpc/rpc.h
+BUILTIN_FIND_GREP.H_RPC=	\#define[       ]*_RPC_RPC_H
+BUILTIN_FIND_GREP.H_TIRPC=	\#define[       ]*_TIRPC_RPC_H
 
 .include "../../mk/buildlink3/bsd.builtin.mk"
 
@@ -12,13 +16,18 @@ BUILTIN_FIND_GREP.H_LIBTIRPC=		\#define[ 	]*_TIRPC_RPC_H
 ### Determine if there is a built-in implementation of the package and
 ### set IS_BUILTIN.<pkg> appropriately ("yes" or "no").
 ###
-.if !defined(IS_BUILTIN.libtirpc)
+.if !defined(IS_BUILTIN.rpc)
 IS_BUILTIN.libtirpc=	no
-.  if empty(H_LIBTIRPC:M__nonexistent__) && empty(H_LIBTIRPC:M${LOCALBASE}/*)
+.  if empty(H_TIRPC:M__nonexistent__) && \
+	!empty(BUILTIN_LIB_FOUND.tirpc:M[yY][eE][sS])
 IS_BUILTIN.libtirpc=	yes
+.  elif empty(H_RPC:M__nonexistent__)
+IS_BUILTIN.libtirpc=	yes
+.  else
+IS_BUILTIN.libtirpc=	no
 .  endif
 .endif
-MAKEVARS+=		IS_BUILTIN.libtirpc
+MAKEVARS+=	IS_BUILTIN.libtirpc
 
 ###
 ### Determine whether we should use the built-in implementation.
@@ -34,7 +43,7 @@ USE_BUILTIN.libtirpc=	yes
 .      for _dep_ in ${BUILDLINK_API_DEPENDS.libtirpc}
 .        if ${USE_BUILTIN.libtirpc:tl} == yes
 USE_BUILTIN.libtirpc!=							\
-	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.libtirpc:Q}; then \
+	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.libtirpc}; then	\
 		${ECHO} yes;						\
 	else								\
 		${ECHO} no;						\
@@ -42,6 +51,6 @@ USE_BUILTIN.libtirpc!=							\
 .        endif
 .      endfor
 .    endif
-.  endif  # PREFER.libtirpc
+.  endif		# PREFER.libtirpc
 .endif
 MAKEVARS+=		USE_BUILTIN.libtirpc
