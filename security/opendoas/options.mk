b58452d967cbe9c01ad028bcc10a6671da7a02b5
vins <vins@pkgsrc.org>
2025-11-30 16:57:34 +0000

    opendoas: further Makefile revision

--- security/opendoas/options.mk
+++ security/opendoas/options.mk
@@ -1,9 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2025/02/02 10:59:47 vins Exp $
+# $NetBSD: options.mk,v 1.3 2025/11/30 16:57:34 vins Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opendoas
 
-PKG_SUPPORTED_OPTIONS+=	persist
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS+=	pam persist
+.if exists(/usr/include/security/pam_appl.h)
+PKG_SUGGESTED_OPTIONS+=	pam
+.endif
+
+PLIST_VARS+=	pam-conf
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,6 +15,18 @@ PKG_SUGGESTED_OPTIONS=
 ## Enable credential caching.
 ##
 .if !empty(PKG_OPTIONS:Mpersist)
-CONFIGURE_ENV+=		OS=linux
 CONFIGURE_ARGS+=	--with-timestamp
+.endif
+
+###
+### Enable PAM auth support.
+###
+.if !empty(PKG_OPTIONS:Mpam)
+.  include "../../mk/pam.buildlink3.mk"
+.  if ${OPSYS} == "Linux" || ${OPSYS} == "Darwin"
+PLIST.pam-conf= yes
+REQD_FILES=     ${EGDIR}/doas /etc/pam.d/doas
+.  endif
+.else
+CONFIGURE_ARGS+=        --without-pam
 .endif
