17583f846d2f37d7eb7ea35e4879a59e56de7472
adam <adam@pkgsrc.org>
2025-12-01 13:34:43 +0000

    xmp: updated to 4.2.0
    4.2.0 (20230615):
    	Changes by Özkan Sezer:
    	- Fix OS/2 and Amiga builds. OS-specific tweaks.
    	- DOS Sound Blaster output, adapted from mikmod.
    	- Build system improvements.
    	Changes by Emanuel Haupt:
    	- Make configure detect sndio on FreeBSD
    	Changes by Alice Rowan:
    	- Fix mod->xxs out-of-bounds read.
    	- Fix terminal output for MSYS2.
    	- Fix crash-on-exit in Windows audio output.
    	- Fixes to BSD audio output modules.
    	Changes by Cameron Cawley:
    	- Support compiling for Windows with OpenWatcom.
    	- DOS Sound Blaster support using OpenWatcom.

--- audio/xmp/patches/patch-src_options.c
+++ audio/xmp/patches/patch-src_options.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_options.c,v 1.1 2013/12/10 07:14:45 rxg Exp $
+$NetBSD: patch-src_options.c,v 1.2 2025/12/01 13:34:43 adam Exp $
 
---- src/options.c.orig	2013-05-19 14:18:19.000000000 +0000
+--- src/options.c.orig	2023-06-15 20:11:10.000000000 +0000
 +++ src/options.c
-@@ -317,6 +317,6 @@ void get_options(int argc, char **argv, 
+@@ -385,8 +385,8 @@ void get_options(int argc, char **argv,
  	/* Set limits */
  	if (options->rate < 1000)
  		options->rate = 1000;	/* Min. rate 1 kHz */
@@ -10,4 +10,6 @@ $NetBSD: patch-src_options.c,v 1.1 2013/12/10 07:14:45 rxg Exp $
 -		options->rate = 48000;	/* Max. rate 48 kHz */
 +	if (options->rate > 192000)
 +		options->rate = 192000;	/* Max. rate 192 kHz */
- }
+ 
+ 	/* apply guess if no driver selected */
+ 	if (!options->driver_id)
