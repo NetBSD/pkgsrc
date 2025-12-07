3da69b31b789826971bed45ce1111f580b363e4c
bsiegert <bsiegert@pkgsrc.org>
2025-12-07 17:03:58 +0000

    go-tools: update to 0.39.0
    This no longer contains the "godoc" binary, which makes it a lot less
    useful. To get an HTTP server with package docs, try "go doc -http".

--- devel/go-tools/go-modules.mk
+++ devel/go-tools/go-modules.mk
@@ -1,16 +1,16 @@
-# $NetBSD: go-modules.mk,v 1.8 2025/03/09 19:37:14 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.9 2025/12/07 17:03:58 bsiegert Exp $
 
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
 GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.mod
 GO_MODULE_FILES+=	github.com/yuin/goldmark/@v/v1.4.13.zip
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.24.0.mod
-GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.24.0.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.37.0.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.37.0.zip
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.12.0.mod
-GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.12.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.31.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.31.0.zip
-GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.mod
-GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20240521205824-bda55230c457.zip
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.30.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.30.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.47.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.47.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.18.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.18.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.38.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.38.0.zip
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20251111182119-bc8e575c7b54.mod
+GO_MODULE_FILES+=	golang.org/x/telemetry/@v/v0.0.0-20251111182119-bc8e575c7b54.zip
