9258edeb5873b6faeeb25b189152219a0dca6d0d
manu <manu@pkgsrc.org>
2025-12-11 00:44:17 +0000

    Replace crash workaround submitted upstream by an already committed fix from upstream develop branch, as suggested by maintainer in the pull request.

--- mail/openarc/patches/patch-libopenarc_arc-canon.c
+++ mail/openarc/patches/patch-libopenarc_arc-canon.c
@@ -1,31 +1,41 @@
-$NetBSD: patch-libopenarc_arc-canon.c,v 1.1 2025/11/18 14:02:20 manu Exp $
+From 5a93cd6620004a774ccc968894ed341c054f1be1 Mon Sep 17 00:00:00 2001
+From: Paul Arthur <paul.arthur@flowerysong.com>
+Date: Mon, 16 Apr 2018 14:30:35 +0000
+Subject: [PATCH] Fix crash when parsing "c" tags
 
-Cope with badly formed input
+["/" sig-c-tag-alg] is optional, and even if it weren't we shouldn't
+crash when it's not present.
+---
+ libopenarc/arc-canon.c | 17 +++++++++++++----
+ 1 file changed, 13 insertions(+), 4 deletions(-)
 
---- libopenarc/arc-canon.c.orig	2025-11-07 02:15:05.538163024 +0100
-+++ libopenarc/arc-canon.c	2025-11-07 02:16:20.165199776 +0100
-@@ -2134,18 +2134,22 @@
- 	if (tag[0] == '\0')
- 		return ARC_STAT_INVALID;
- 
- 	token = strtok_r(tag, "/", &last);
-+	if (token == NULL)
-+		return ARC_STAT_INVALID;
-+		
- 	code = arc_name_to_code(canonicalizations, token);
--
- 	if (code == -1)
- 		return ARC_STAT_INVALID;
- 
+diff --git a/libopenarc/arc-canon.c b/libopenarc/arc-canon.c
+index b2d66b0a..aa0f1453 100644
+--- ./libopenarc/arc-canon.c.orig
++++ ./libopenarc/arc-canon.c
+@@ -2143,12 +2143,21 @@ arc_parse_canon_t(unsigned char *tag, arc_canon_t *hdr_canon,
  	*hdr_canon = (arc_canon_t) code;
  
  	token = strtok_r(NULL, "/", &last);
 -	code = arc_name_to_code(canonicalizations, token);
+ 
+-	if (code == -1)
+-		return ARC_STAT_INVALID;
 +	if (token == NULL)
-+		return ARC_STAT_INVALID;
++	{
++		/* Per RFC 6376, if no body canonicalization is provided the
++		 * default is simple. */
++		*body_canon = ARC_CANON_SIMPLE;
++	}
++	else
++	{
++		code = arc_name_to_code(canonicalizations, token);
  
-+	code = arc_name_to_code(canonicalizations, token);
- 	if (code == -1)
- 		return ARC_STAT_INVALID;
+-	*body_canon = (arc_canon_t) code;
++		if (code == -1)
++			return ARC_STAT_INVALID;
++		*body_canon = (arc_canon_t) code;
++	}
  
- 	*body_canon = (arc_canon_t) code;
+ 	return ARC_STAT_OK;
+ }
