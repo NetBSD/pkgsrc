4ff9f9a82400ccebd7c62a17c0c75efccfababbb
adam <adam@pkgsrc.org>
2025-12-12 09:08:40 +0000

    nodejs24: updated to 24.12.0
    24.12.0 'Krypton' (LTS)
    
    Notable Changes
    
    - (SEMVER-MINOR) http: add optimizeEmptyRequests server option (Rafael Gonzaga)
    - (SEMVER-MINOR) lib: add options to util.deprecate (Rafael Gonzaga)
    - (SEMVER-MINOR) module: mark type stripping as stable (Marco Ippolito)
    - (SEMVER-MINOR) node-api: add napi_create_object_with_properties (Miguel Marcondes Filho)
    - (SEMVER-MINOR) sqlite: allow setting defensive flag (Bart Louwers)
    - (SEMVER-MINOR) src: add watch config namespace (Marco Ippolito)
    - (SEMVER-MINOR) src: add an option to make compile cache portable (Aditi)
    - (SEMVER-MINOR) src,permission: add --allow-inspector ability (Rafael Gonzaga)
    - (SEMVER-MINOR) v8: add cpu profile (theanarkh)

--- lang/nodejs24/patches/patch-deps_v8_third__party_abseil-cpp_absl_debugging_internal_elf__mem__image.cc
+++ lang/nodejs24/patches/patch-deps_v8_third__party_abseil-cpp_absl_debugging_internal_elf__mem__image.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-deps_v8_third__party_abseil-cpp_absl_debugging_internal_elf__mem__image.cc,v 1.3 2025/11/17 13:06:22 ryoon Exp $
+$NetBSD: patch-deps_v8_third__party_abseil-cpp_absl_debugging_internal_elf__mem__image.cc,v 1.4 2025/12/12 09:08:40 adam Exp $
 
 Fix build on NetBSD.
 
 --- deps/v8/third_party/abseil-cpp/absl/debugging/internal/elf_mem_image.cc.orig	2025-11-11 13:52:04.000000000 +0000
 +++ deps/v8/third_party/abseil-cpp/absl/debugging/internal/elf_mem_image.cc
-@@ -377,7 +381,7 @@ void ElfMemImage::SymbolIterator::Update
+@@ -377,7 +377,7 @@ void ElfMemImage::SymbolIterator::Update
    const ElfW(Versym) *version_symbol = image->GetVersym(index_);
    ABSL_RAW_CHECK(symbol && version_symbol, "");
    const char *const symbol_name = image->GetDynstr(symbol->st_name);
