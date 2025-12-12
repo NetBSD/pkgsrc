049ef0d957f533073b8e1a13f885641fa3952acf
adam <adam@pkgsrc.org>
2025-12-12 09:42:00 +0000

    qemu: updated to 10.1.3
    10.1.3
    There are 96 changes since the previous v10.1.2 release, including
    the fix for CVE-2025-11234 (use after free in websocket handshake code).

--- emulators/qemu/version.mk
+++ emulators/qemu/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.9 2025/10/22 06:35:00 adam Exp $
+# $NetBSD: version.mk,v 1.10 2025/12/12 09:42:00 adam Exp $
 #
 # used by emulators/qemu
 # used by sysutils/qemu-guest-agent
 
-QEMU_VERSION?=	10.1.2
+QEMU_VERSION?=	10.1.3
