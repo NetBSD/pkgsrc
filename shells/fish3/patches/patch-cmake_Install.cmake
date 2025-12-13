ef88c15591cc34b176b49f5b0e6d4bc199741905
vins <vins@pkgsrc.org>
2025-12-13 17:58:55 +0000

    shells/fish3: reimport fish 3.x branch as fish3
    fish is a user friendly command line shell for UNIX-like operating
    systems, written mainly with interactive use in mind.  It differs
    from other shells in that it only provides as few commands as
    built-ins as possible and has a daemon which allows it to have
    shared variables and command-line history between shell instances.
    It also features feature-rich tab-completion and has command-line
    syntax highlighting.
    
    This is the old 3.x branch of the fish shell, written in C++.
    Unmaintained and no longer supported upstream, but kept in pkgsrc
    for portability with platforms where a Rust compiler might not be
    available.

---
+++ shells/fish3/patches/patch-cmake_Install.cmake
@@ -0,0 +1,30 @@
+$NetBSD: patch-cmake_Install.cmake,v 1.1 2025/12/13 17:58:55 vins Exp $
+
+pkgsrc-ifying:
+Install config files to examples rather than etc.
+Install desktop files on macOS regardless to simplify PLIST.
+
+--- cmake/Install.cmake.orig	2022-03-25 16:22:53.000000000 +0000
++++ cmake/Install.cmake
+@@ -81,9 +81,9 @@ install(TARGETS ${PROGRAMS}
+                     GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+         DESTINATION ${bindir})
+ 
+-fish_create_dirs(${sysconfdir}/fish/conf.d ${sysconfdir}/fish/completions
+-    ${sysconfdir}/fish/functions)
+-install(FILES etc/config.fish DESTINATION ${sysconfdir}/fish/)
++fish_create_dirs(${rel_datadir}/examples/fish/conf.d ${rel_datadir}/examples/fish/completions
++    ${rel_datadir}/examples/fish/functions)
++install(FILES etc/config.fish DESTINATION ${rel_datadir}/examples/fish/)
+ 
+ fish_create_dirs(${rel_datadir}/fish ${rel_datadir}/fish/completions
+                  ${rel_datadir}/fish/functions ${rel_datadir}/fish/groff
+@@ -170,7 +170,7 @@ if(GETTEXT_FOUND)
+   endforeach()
+ endif()
+ 
+-if (NOT APPLE)
++if (1)
+     install(FILES fish.desktop DESTINATION ${rel_datadir}/applications)
+     install(FILES ${SPHINX_SRC_DIR}/python_docs_theme/static/fish.png DESTINATION ${rel_datadir}/pixmaps)
+ endif()
