ec1652e8061262bafc5b438c64a8a1db0c094f6f
bsiegert <bsiegert@pkgsrc.org>
2025-12-13 13:27:45 +0000

    New package, golds-0.8.2.
    Golds is a Go local docs server, a Go docs generator, and a Go code
    reader. It tries to extract as much information as possible from Go code
    to help gophers understand, study and use Go packages.
    
    JavaScript-off friendly, though the experience is a little richer when
    JavaScript is on.
    
    All functionalities are implemented locally, No tracing, no external
    websites visiting (except explicitly and manually required by you).

---
+++ devel/golds/go-modules.mk
@@ -0,0 +1,12 @@
+# $NetBSD: go-modules.mk,v 1.1 2025/12/13 13:27:46 bsiegert Exp $
+
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.26.0.mod
+GO_MODULE_FILES+=	golang.org/x/mod/@v/v0.26.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.42.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.42.0.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.16.0.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.16.0.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.27.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.27.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.35.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.35.0.zip
