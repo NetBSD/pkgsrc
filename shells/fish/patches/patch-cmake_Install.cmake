f48be8b9452af3ba52e3fb83b98292514a9cc405
vins <vins@pkgsrc.org>
2025-12-13 17:47:21 +0000

    shells/fish: update to 4.2.1 using shells/fish4

--- shells/fish/patches/patch-cmake_Install.cmake
+++ shells/fish/patches/patch-cmake_Install.cmake
@@ -1,30 +1,48 @@
-$NetBSD: patch-cmake_Install.cmake,v 1.2 2022/04/21 11:24:52 jperkin Exp $
+$NetBSD: patch-cmake_Install.cmake,v 1.3 2025/12/13 17:47:21 vins Exp $
 
-pkgsrc-ifying:
-Install config files to examples rather than etc.
-Install desktop files on macOS regardless to simplify PLIST.
+Use pkgsrc defaults. 
 
---- cmake/Install.cmake.orig	2022-03-25 16:22:53.000000000 +0000
+--- cmake/Install.cmake.orig	2025-11-01 23:44:37.984452608 +0000
 +++ cmake/Install.cmake
-@@ -81,9 +81,9 @@ install(TARGETS ${PROGRAMS}
-                     GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
-         DESTINATION ${bindir})
+@@ -6,11 +6,14 @@ set(prefix ${CMAKE_INSTALL_PREFIX})
+ set(bindir ${CMAKE_INSTALL_BINDIR})
+ set(sysconfdir ${CMAKE_INSTALL_SYSCONFDIR})
+ set(mandir ${CMAKE_INSTALL_MANDIR})
++set(libdir ${CMAKE_INSTALL_LIBDIR})
++
  
--fish_create_dirs(${sysconfdir}/fish/conf.d ${sysconfdir}/fish/completions
--    ${sysconfdir}/fish/functions)
+ set(datadir ${CMAKE_INSTALL_FULL_DATADIR})
+ file(RELATIVE_PATH rel_datadir ${CMAKE_INSTALL_PREFIX} ${datadir})
+ 
+ set(docdir ${CMAKE_INSTALL_DOCDIR})
++set(egdir "${datadir}/examples")
+ 
+ set(rel_completionsdir "fish/vendor_completions.d")
+ set(rel_functionsdir "fish/vendor_functions.d")
+@@ -82,7 +85,7 @@ install(PROGRAMS ${PROGRAMS}
+ 
+ fish_create_dirs(${sysconfdir}/fish/conf.d ${sysconfdir}/fish/completions
+     ${sysconfdir}/fish/functions)
 -install(FILES etc/config.fish DESTINATION ${sysconfdir}/fish/)
-+fish_create_dirs(${rel_datadir}/examples/fish/conf.d ${rel_datadir}/examples/fish/completions
-+    ${rel_datadir}/examples/fish/functions)
-+install(FILES etc/config.fish DESTINATION ${rel_datadir}/examples/fish/)
++install(FILES etc/config.fish DESTINATION ${egdir}/fish/)
  
  fish_create_dirs(${rel_datadir}/fish ${rel_datadir}/fish/completions
                   ${rel_datadir}/fish/functions ${rel_datadir}/fish/groff
-@@ -170,7 +170,7 @@ if(GETTEXT_FOUND)
-   endforeach()
- endif()
+@@ -102,7 +105,7 @@ install(FILES share/config.fish
+ fish_create_dirs(${rel_datadir}/fish/vendor_completions.d ${rel_datadir}/fish/vendor_functions.d
+     ${rel_datadir}/fish/vendor_conf.d)
  
--if (NOT APPLE)
-+if (1)
-     install(FILES fish.desktop DESTINATION ${rel_datadir}/applications)
-     install(FILES ${SPHINX_SRC_DIR}/python_docs_theme/static/fish.png DESTINATION ${rel_datadir}/pixmaps)
- endif()
+-fish_try_create_dirs(${rel_datadir}/pkgconfig)
++fish_try_create_dirs(${libdir}/pkgconfig)
+ configure_file(fish.pc.in fish.pc.noversion @ONLY)
+ 
+ add_custom_command(OUTPUT fish.pc
+@@ -115,7 +118,7 @@ add_custom_command(OUTPUT fish.pc
+ add_custom_target(build_fish_pc ALL DEPENDS fish.pc)
+ 
+ install(FILES ${CMAKE_CURRENT_BINARY_DIR}/fish.pc
+-        DESTINATION ${rel_datadir}/pkgconfig)
++        DESTINATION ${libdir}/pkgconfig)
+ 
+ install(DIRECTORY share/completions/
+         DESTINATION ${rel_datadir}/fish/completions
