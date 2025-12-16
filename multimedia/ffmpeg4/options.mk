f037ebc5d08ad013d0edfe4e95534101ee23b2ea
vins <vins@pkgsrc.org>
2025-12-16 14:57:45 +0000

    multimedia/ffmpeg*: add optional sndio support

--- multimedia/ffmpeg4/options.mk
+++ multimedia/ffmpeg4/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.19 2023/11/09 16:31:18 nia Exp $
+# $NetBSD: options.mk,v 1.20 2025/12/16 14:57:45 vins Exp $
 
 # Global and legacy options
 
@@ -8,7 +8,7 @@ PKG_OPTIONS_GROUP.ssl=		gnutls mbedtls openssl
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ffmpeg4
 PKG_SUPPORTED_OPTIONS=	ass av1 bluray doc fdk-aac fontconfig freetype jack \
 			lame libvpx libwebp opencore-amr opus pulseaudio rpi rtmp \
-			speex tesseract theora vorbis x11 x264 x265 xvid
+			sndio speex tesseract theora vorbis x11 x264 x265 xvid
 PKG_SUGGESTED_OPTIONS=	ass av1 bluray freetype fontconfig gnutls lame \
 			libvpx libwebp opus speex theora vorbis x264 x265 xvid
 
@@ -152,6 +152,13 @@ CONFIGURE_ARGS+=	--disable-libpulse
 .if !empty(PKG_OPTIONS:Mrtmp)
 CONFIGURE_ARGS+=	--enable-librtmp
 .include "../../net/rtmpdump/buildlink3.mk"
+.endif
+
+# sndio support
+.if !empty(PKG_OPTIONS:Msndio)
+.include "../../audio/sndio/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-sndio
 .endif
 
 # OCR filter using Tesseract
