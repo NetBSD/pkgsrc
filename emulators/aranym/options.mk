03ac1404c4afc09d4a5c5266463b8cf0dd02f989
nia <nia@pkgsrc.org>
2025-12-18 14:06:16 +0000

    aranym: Add support for SunOS.
    Required making the mmu optional, so I build tested this on NetBSD
    and Linux also.

--- emulators/aranym/options.mk
+++ emulators/aranym/options.mk
@@ -1,7 +1,10 @@
-# $NetBSD: options.mk,v 1.2 2019/08/30 17:48:07 nia Exp $
+# $NetBSD: options.mk,v 1.3 2025/12/18 14:06:16 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.aranym
-PKG_SUPPORTED_OPTIONS=	opengl
+PKG_SUPPORTED_OPTIONS=	opengl aranym-mmu
+
+PKG_SUGGESTED_OPTIONS.NetBSD+=	aranym-mmu
+PKG_SUGGESTED_OPTIONS.Linux+=	aranym-mmu
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +13,17 @@ CONFIGURE_ARGS+=	--enable-opengl
 .include "../../graphics/MesaLib/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-opengl
+.endif
+
+PLIST_VARS+=		mmu nommu
+
+.if !empty(PKG_OPTIONS:Maranym-mmu)
+PLIST.mmu=		yes
+CONFIGURE_ARGS+=	--enable-fullmmu
+CONFIGURE_ARGS+=	--enable-fpe=mpfr
+.include "../../math/mpfr/buildlink3.mk"
+.else
+PLIST.nommu=		yes
+CONFIGURE_ARGS+=	--disable-fullmmu
+CONFIGURE_ARGS+=	--enable-fpe=uae
 .endif
