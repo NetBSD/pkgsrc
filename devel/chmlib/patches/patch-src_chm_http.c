79a0e6fcafc57457a08544d00bb23a09340c8a75
nia <nia@pkgsrc.org>
2025-12-20 12:19:03 +0000

    chmlib: Fix more implicit function decls.

--- devel/chmlib/patches/patch-src_chm_http.c
+++ devel/chmlib/patches/patch-src_chm_http.c
@@ -1,10 +1,18 @@
-$NetBSD: patch-src_chm_http.c,v 1.1 2025/09/29 20:47:29 mrg Exp $
+$NetBSD: patch-src_chm_http.c,v 1.2 2025/12/20 12:19:03 nia Exp $
 
-Add missing header for inet_addr(3).
+Add missing headers for inet_addr(3), close(2), write(2).
 
---- src/chm_http.c.orig	2009-05-23 08:08:18.000000000 -0700
-+++ src/chm_http.c	2025-09-25 11:57:21.415075179 -0700
-@@ -43,6 +43,7 @@
+--- src/chm_http.c.orig	2009-05-23 15:08:18.000000000 +0000
++++ src/chm_http.c
+@@ -33,6 +33,7 @@
+ #define _REENTRANT
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <unistd.h>
+ #include <string.h>
+ #if __sun || __sgi
+ #include <strings.h>
+@@ -43,6 +44,7 @@
  #include <sys/socket.h>
  #include <sys/types.h>
  #include <netinet/in.h>
