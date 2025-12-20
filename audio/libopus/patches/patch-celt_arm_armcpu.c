c86db654963eb23bf442146a7a632e377d0625df
wiz <wiz@pkgsrc.org>
2025-12-20 22:21:28 +0000

    libopus: update to 1.6.
    Opus 1.6 builds on the new ML-based features introduced in Opus 1.5.
    
        A new wideband-to-fullband bandwidth extension (BWE) module
        Support for 96 kHz audio with Opus HD
        Significant improvement to Deep Redundancy (DRED)
        A new 24-bit encoder/decoder API
        Fixed-point improvements
    
    In addition to the improvements above, this release includes many minor bug fixes.

--- audio/libopus/patches/patch-celt_arm_armcpu.c
+++ audio/libopus/patches/patch-celt_arm_armcpu.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-celt_arm_armcpu.c,v 1.3 2024/03/07 17:26:28 adam Exp $
+$NetBSD: patch-celt_arm_armcpu.c,v 1.4 2025/12/20 22:21:28 wiz Exp $
 
 Don't error on ARM if we're not on Darwin, Linux, or MSVC.
 Currently the code attempts to open /proc/cpuinfo, and tests for
 failure - if that doesn't contain useful info or doesn't exist,
 it still behaves correctly.
 
---- celt/arm/armcpu.c.orig	2024-03-01 07:55:09.000000000 +0000
+--- celt/arm/armcpu.c.orig	2025-11-07 22:12:41.000000000 +0000
 +++ celt/arm/armcpu.c
 @@ -92,7 +92,7 @@ static OPUS_INLINE opus_uint32 opus_cpu_
    return flags;
@@ -16,9 +16,9 @@ it still behaves correctly.
  /* Linux based */
  #include <stdio.h>
  
-@@ -195,7 +195,7 @@ opus_uint32 opus_cpu_capabilities(void)
+@@ -275,7 +275,7 @@ static opus_uint32 opus_cpu_capabilities
  /* The feature registers which can tell us what the processor supports are
-  * accessible in priveleged modes only, so we can't have a general user-space
+  * accessible in privileged modes only, so we can't have a general user-space
   * detection method like on x86.*/
 -# error "Configured to use ARM asm but no CPU detection method available for " \
 +# warning "Configured to use ARM asm but no CPU detection method available for " \
