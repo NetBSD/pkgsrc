789c73e8001d23cb41529ce06c959ead1508557b
wiz <wiz@pkgsrc.org>
2025-12-20 22:49:17 +0000

    meson: update to 1.10.0.
    # New features
    ## Support for the `counted_by` attribute
    ## Added a `values()` method for dictionaries
    ## Add cmd_array method to ExternalProgram
    ## Microchip XC32 compiler support
    ## Added OS/2 support
    ## Android cross file generator
    ## Array `.slice()` method
    ## `-Db_vscrt` on clang
    ## Added `build_subdir` arg to various targets
    ## Support for Cargo workspaces
    ## Experimental Codegen module
    ## Methods from compiler object now accept strings for include_directories
    ## `meson format` has a new `--check-diff` option
    ## `-Db_thinlto_cache` now supported for GCC
    ## Using `meson.get_compiler()` to get a language from another project is marked broken
    ## Experimental C++ import std support
    ## Common `Cargo.lock` for all Cargo subprojects
    ## Add a configure log in meson-logs
    ## Added new `namingscheme` option
    ## Rewriter improvements
    ## Passing `-C default-linker-libraries` to rustc
    ## `rustc` will receive `-C embed-bitcode=no` and `-C lto` command line options
    ## New method to handle GNU and Windows symbol visibility for C/C++/ObjC/ObjC++
    ## Vala BuildTarget dependency enhancements
    ## `i18n.xgettext` now accepts CustomTarget and CustomTargetIndex as sources

--- devel/meson/patches/patch-mesonbuild_dependencies_dev.py
+++ devel/meson/patches/patch-mesonbuild_dependencies_dev.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-mesonbuild_dependencies_dev.py,v 1.6 2022/07/13 03:57:22 triaxx Exp $
+$NetBSD: patch-mesonbuild_dependencies_dev.py,v 1.7 2025/12/20 22:49:17 wiz Exp $
 
 Allow handling different versions of LLVM in pkgsrc using LLVM_CONFIG_PATH.
 
---- mesonbuild/dependencies/dev.py.orig	2021-08-18 11:22:33.000000000 +0000
+--- mesonbuild/dependencies/dev.py.orig	2025-12-08 16:48:27.000000000 +0000
 +++ mesonbuild/dependencies/dev.py
-@@ -208,7 +208,10 @@ class LLVMDependencyConfigTool(ConfigToo
+@@ -188,7 +188,10 @@ class LLVMDependencyConfigTool(ConfigToo
      __cpp_blacklist = {'-DNDEBUG'}
  
-     def __init__(self, name: str, environment: 'Environment', kwargs: T.Dict[str, T.Any]):
+     def __init__(self, name: str, environment: 'Environment', kwargs: DependencyObjectKWs):
 -        self.tools = get_llvm_tool_names('llvm-config')
 +        if 'LLVM_CONFIG_PATH' in os.environ:
 +            self.tools = [os.environ['LLVM_CONFIG_PATH']]
