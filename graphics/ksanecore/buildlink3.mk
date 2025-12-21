422f0dfb1b1a262c1241339b2a5ec2b69a129e67
markd <markd@pkgsrc.org>
2025-12-21 12:32:36 +0000

    ksanecore: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- graphics/ksanecore/buildlink3.mk
+++ graphics/ksanecore/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.15 2025/10/23 20:37:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/12/21 12:32:36 markd Exp $
 
 BUILDLINK_TREE+=	ksanecore
 
 .if !defined(KSANECORE_BUILDLINK3_MK)
 KSANECORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ksanecore+=	ksanecore>=22.08.0
-BUILDLINK_ABI_DEPENDS.ksanecore?=	ksanecore>=23.08.4nb9
-BUILDLINK_PKGSRCDIR.ksanecore?=	../../graphics/ksanecore
+BUILDLINK_API_DEPENDS.ksanecore+=	ksanecore>=25.08.2
+BUILDLINK_ABI_DEPENDS.ksanecore?=	ksanecore>=25.08.2
+BUILDLINK_PKGSRCDIR.ksanecore?=		../../graphics/ksanecore
 
-.include "../../devel/ki18n/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KSANECORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ksanecore
