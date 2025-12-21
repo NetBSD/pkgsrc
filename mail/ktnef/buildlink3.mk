8a055e23ba1d76f182af2b3b944b0aff5f658d76
markd <markd@pkgsrc.org>
2025-12-21 13:16:07 +0000

    ktnef: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/ktnef/buildlink3.mk
+++ mail/ktnef/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.19 2025/10/23 20:38:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2025/12/21 13:16:07 markd Exp $
 
 BUILDLINK_TREE+=	ktnef
 
 .if !defined(KTNEF_BUILDLINK3_MK)
 KTNEF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ktnef+=	ktnef>=20.04.1
-BUILDLINK_ABI_DEPENDS.ktnef?=	ktnef>=23.08.4nb10
+BUILDLINK_API_DEPENDS.ktnef+=	ktnef>=25.08.3
+BUILDLINK_ABI_DEPENDS.ktnef?=	ktnef>=25.08.3
 BUILDLINK_PKGSRCDIR.ktnef?=	../../mail/ktnef
 
-.include "../../misc/kcontacts/buildlink3.mk"
+.include "../../misc/kf6-kcontacts/buildlink3.mk"
 .include "../../time/kcalutils/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KTNEF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ktnef
