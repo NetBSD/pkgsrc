a272837374ac97bf79bc560ca3649623c08a20ff
markd <markd@pkgsrc.org>
2025-12-21 19:46:57 +0000

    kpimtextedit: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- textproc/kpimtextedit/buildlink3.mk
+++ textproc/kpimtextedit/buildlink3.mk
@@ -1,21 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.39 2025/10/23 20:39:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2025/12/21 19:46:57 markd Exp $
 
 BUILDLINK_TREE+=	kpimtextedit
 
 .if !defined(KPIMTEXTEDIT_BUILDLINK3_MK)
 KPIMTEXTEDIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kpimtextedit+=	kpimtextedit>=17.12.1
-BUILDLINK_ABI_DEPENDS.kpimtextedit?=	kpimtextedit>=23.08.4nb10
+BUILDLINK_API_DEPENDS.kpimtextedit+=	kpimtextedit>=25.08.2
+BUILDLINK_ABI_DEPENDS.kpimtextedit?=	kpimtextedit>=25.08.2
 BUILDLINK_PKGSRCDIR.kpimtextedit?=	../../textproc/kpimtextedit
 
-.include "../../devel/grantlee-qt5/buildlink3.mk"
-.include "../../devel/kio/buildlink3.mk"
-.include "../../misc/kemoticons/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
 .include "../../textproc/ktextaddons/buildlink3.mk"
-.include "../../textproc/syntax-highlighting/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../x11/qt5-qtspeech/buildlink3.mk"
+.include "../../textproc/kf6-syntax-highlighting/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KPIMTEXTEDIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kpimtextedit
