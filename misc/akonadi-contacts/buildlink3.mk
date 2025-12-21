e7678ccb2788e40568a56b48f9ef86906772df86
markd <markd@pkgsrc.org>
2025-12-21 13:40:09 +0000

    akonadi-contacts: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/akonadi-contacts/buildlink3.mk
+++ misc/akonadi-contacts/buildlink3.mk
@@ -1,21 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 13:40:09 markd Exp $
 
 BUILDLINK_TREE+=	akonadi-contacts
 
 .if !defined(AKONADI_CONTACTS_BUILDLINK3_MK)
 AKONADI_CONTACTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.akonadi-contacts+=	akonadi-contacts>=17.12.1
-BUILDLINK_ABI_DEPENDS.akonadi-contacts?=		akonadi-contacts>=23.08.4nb11
+BUILDLINK_API_DEPENDS.akonadi-contacts+=	akonadi-contacts>=25.08.3
+BUILDLINK_ABI_DEPENDS.akonadi-contacts?=	akonadi-contacts>=25.08.3
 BUILDLINK_PKGSRCDIR.akonadi-contacts?=		../../misc/akonadi-contacts
 
-.include "../../graphics/prison/buildlink3.mk"
-.include "../../mail/akonadi-mime/buildlink3.mk"
+.include "../../devel/kf6-kcmutils/buildlink3.mk"
+.include "../../graphics/kf6-prison/buildlink3.mk"
+.include "../../mail/akonadi/buildlink3.mk"
 .include "../../mail/kmime/buildlink3.mk"
 .include "../../misc/grantleetheme/buildlink3.mk"
-.include "../../misc/kcontacts/buildlink3.mk"
-.include "../../security/libkleo/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../misc/kf6-kcontacts/buildlink3.mk"
+.include "../../textproc/ktextaddons/buildlink3.mk"
+.include "../../x11/kf6-ktextwidgets/buildlink3.mk"
+.include "../../x11/kf6-kxmlgui/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# AKONADI_CONTACTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-akonadi-contacts
