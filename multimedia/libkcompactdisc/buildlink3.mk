b8dd2103bea2cffd035d2fdd45ddd0b0f30416eb
markd <markd@pkgsrc.org>
2025-12-21 15:00:42 +0000

    libkcompactdisc: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- multimedia/libkcompactdisc/buildlink3.mk
+++ multimedia/libkcompactdisc/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.18 2025/10/23 20:38:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2025/12/21 15:00:42 markd Exp $
 
 BUILDLINK_TREE+=	libkcompactdisc
 
 .if !defined(LIBKCOMPACTDISC_BUILDLINK3_MK)
 LIBKCOMPACTDISC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkcompactdisc+=	libkcompactdisc>=20.04.1
-BUILDLINK_ABI_DEPENDS.libkcompactdisc?=	libkcompactdisc>=23.08.4nb8
+BUILDLINK_API_DEPENDS.libkcompactdisc+=	libkcompactdisc>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkcompactdisc?=	libkcompactdisc>=25.08.2
 BUILDLINK_PKGSRCDIR.libkcompactdisc?=	../../multimedia/libkcompactdisc
 
-.include "../../devel/kcoreaddons/buildlink3.mk"
-.include "../../devel/ki18n/buildlink3.mk"
-.include "../../multimedia/phonon-qt5/buildlink3.mk"
-.include "../../sysutils/solid/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-kcoreaddons/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../multimedia/phonon-qt6/buildlink3.mk"
+.include "../../sysutils/kf6-solid/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKCOMPACTDISC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkcompactdisc
