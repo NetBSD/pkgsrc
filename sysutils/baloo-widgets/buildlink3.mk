85fd9b0796570a4763aa766bef0aee8dbca7af29
markd <markd@pkgsrc.org>
2025-12-21 19:27:53 +0000

    baloo-widgets: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- sysutils/baloo-widgets/buildlink3.mk
+++ sysutils/baloo-widgets/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.29 2025/10/23 20:39:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2025/12/21 19:27:53 markd Exp $
 
 BUILDLINK_TREE+=	baloo-widgets
 
 .if !defined(BALOO_WIDGETS_BUILDLINK3_MK)
 BALOO_WIDGETS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.baloo-widgets+=	baloo-widgets>=16.04.0
-BUILDLINK_ABI_DEPENDS.baloo-widgets?=	baloo-widgets>=23.08.4nb10
+BUILDLINK_API_DEPENDS.baloo-widgets+=	baloo-widgets>=25.08.2
+BUILDLINK_ABI_DEPENDS.baloo-widgets?=	baloo-widgets>=25.08.2
 BUILDLINK_PKGSRCDIR.baloo-widgets?=	../../sysutils/baloo-widgets
 
-.include "../../sysutils/baloo5/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../sysutils/kf6-baloo/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# BALOO_WIDGETS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-baloo-widgets
