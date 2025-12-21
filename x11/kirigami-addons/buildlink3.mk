3e782816a5f3b33acf9ff127eac343d645e041c2
markd <markd@pkgsrc.org>
2025-12-21 20:17:06 +0000

    kirigami-addons: update to 1.10.0
    Qt6/KF6 version - Two years of development.

--- x11/kirigami-addons/buildlink3.mk
+++ x11/kirigami-addons/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/02/06 03:28:25 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/12/21 20:17:06 markd Exp $
 
 BUILDLINK_TREE+=	kirigami-addons
 
 .if !defined(KIRIGAMI_ADDONS_BUILDLINK3_MK)
 KIRIGAMI_ADDONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kirigami-addons+=	kirigami-addons>=0.11.0
+BUILDLINK_API_DEPENDS.kirigami-addons+=	kirigami-addons>=1.10.0
 BUILDLINK_PKGSRCDIR.kirigami-addons?=	../../x11/kirigami-addons
 
 .endif	# KIRIGAMI_ADDONS_BUILDLINK3_MK
