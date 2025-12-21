a88e438e6b0a2c868e76229746d38a2c0939c651
markd <markd@pkgsrc.org>
2025-12-21 15:28:04 +0000

    libkleo: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- security/libkleo/buildlink3.mk
+++ security/libkleo/buildlink3.mk
@@ -1,18 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:39:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 15:28:04 markd Exp $
 
 BUILDLINK_TREE+=	libkleo
 
 .if !defined(LIBKLEO_BUILDLINK3_MK)
 LIBKLEO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkleo+=	libkleo>=20.04.1
-BUILDLINK_ABI_DEPENDS.libkleo?=	libkleo>=23.08.4nb11
+BUILDLINK_API_DEPENDS.libkleo+=	libkleo>=25.08.3
+BUILDLINK_ABI_DEPENDS.libkleo?=	libkleo>=25.08.3
 BUILDLINK_PKGSRCDIR.libkleo?=	../../security/libkleo
 
-.include "../../devel/kitemmodels/buildlink3.mk"
+BUILDLINK_FILES.libkleo+=	share/KPim6Libkleo/find-modules/*
+
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../devel/kf6-kitemmodels/buildlink3.mk"
 .include "../../security/qgpgme/buildlink3.mk"
 .include "../../textproc/kpimtextedit/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKLEO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkleo
