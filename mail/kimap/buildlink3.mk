c83a19c71218f11528fc74eff5486afbf123148c
markd <markd@pkgsrc.org>
2025-12-21 13:02:46 +0000

    kimap: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/kimap/buildlink3.mk
+++ mail/kimap/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.38 2025/10/23 20:38:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2025/12/21 13:02:46 markd Exp $
 
 BUILDLINK_TREE+=	kimap
 
 .if !defined(KIMAP_BUILDLINK3_MK)
 KIMAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kimap+=	kimap>=17.12.1
-BUILDLINK_ABI_DEPENDS.kimap?=	kimap>=23.08.4nb10
+BUILDLINK_API_DEPENDS.kimap+=	kimap>=25.08.2
+BUILDLINK_ABI_DEPENDS.kimap?=	kimap>=25.08.2
 BUILDLINK_PKGSRCDIR.kimap?=	../../mail/kimap
 
-.include "../../devel/kio/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
 .include "../../mail/kmime/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KIMAP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kimap
