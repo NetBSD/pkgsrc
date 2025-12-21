dc7de0c4353e22d26cf36f569dd6c6fbaf506187
markd <markd@pkgsrc.org>
2025-12-21 19:56:12 +0000

    calendarsupport: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- time/calendarsupport/buildlink3.mk
+++ time/calendarsupport/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:39:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 19:56:12 markd Exp $
 
 BUILDLINK_TREE+=	calendarsupport
 
 .if !defined(CALENDARSUPPORT_BUILDLINK3_MK)
 CALENDARSUPPORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.calendarsupport+=	calendarsupport>=20.04.1
-BUILDLINK_ABI_DEPENDS.calendarsupport?=	calendarsupport>=23.08.4nb11
+BUILDLINK_API_DEPENDS.calendarsupport+=	calendarsupport>=25.08.3
+BUILDLINK_ABI_DEPENDS.calendarsupport?=	calendarsupport>=25.08.3
 BUILDLINK_PKGSRCDIR.calendarsupport?=	../../time/calendarsupport
 
-.include "../../misc/pimcommon/buildlink3.mk"
 .include "../../time/akonadi-calendar/buildlink3.mk"
-.include "../../time/kholidays/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../time/kf6-kholidays/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# CALENDARSUPPORT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-calendarsupport
