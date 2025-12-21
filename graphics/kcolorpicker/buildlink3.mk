9d4d4058ff926f807fa6a9381268d858875b9735
markd <markd@pkgsrc.org>
2025-12-21 12:10:00 +0000

    kcolorpicker: update to 0.3.1nb7
    Qt6/KF6 version

--- graphics/kcolorpicker/buildlink3.mk
+++ graphics/kcolorpicker/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2024/08/24 04:19:00 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/12/21 12:10:01 markd Exp $
 
 BUILDLINK_TREE+=	kcolorpicker
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kcolorpicker
 KCOLORPICKER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kcolorpicker+=	kcolorpicker>=0.2.0
-BUILDLINK_ABI_DEPENDS.kcolorpicker?=	kcolorpicker>=0.3.1
+BUILDLINK_ABI_DEPENDS.kcolorpicker?=	kcolorpicker>=0.3.1nb7
 BUILDLINK_PKGSRCDIR.kcolorpicker?=	../../graphics/kcolorpicker
 
 .endif	# KCOLORPICKER_BUILDLINK3_MK
