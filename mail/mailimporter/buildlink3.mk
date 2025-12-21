3b5e37eb385a4fc78457828bc081a1344a24099f
markd <markd@pkgsrc.org>
2025-12-21 13:23:12 +0000

    mailimporter: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/mailimporter/buildlink3.mk
+++ mail/mailimporter/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 13:23:12 markd Exp $
 
 BUILDLINK_TREE+=	mailimporter
 
 .if !defined(MAILIMPORTER_BUILDLINK3_MK)
 MAILIMPORTER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mailimporter+=	mailimporter>=20.04.1
-BUILDLINK_ABI_DEPENDS.mailimporter?=	mailimporter>=23.08.4nb11
+BUILDLINK_API_DEPENDS.mailimporter+=	mailimporter>=25.08.3
+BUILDLINK_ABI_DEPENDS.mailimporter?=	mailimporter>=25.08.3
 BUILDLINK_PKGSRCDIR.mailimporter?=	../../mail/mailimporter
 
 .include "../../misc/pimcommon/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# MAILIMPORTER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mailimporter
