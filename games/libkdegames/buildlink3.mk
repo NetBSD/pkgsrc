f15e57d71d00d3551dde695498b88e086c144436
markd <markd@pkgsrc.org>
2025-12-21 11:52:08 +0000

    libkdegames: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- games/libkdegames/buildlink3.mk
+++ games/libkdegames/buildlink3.mk
@@ -1,18 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.39 2025/10/23 20:37:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2025/12/21 11:52:08 markd Exp $
 
 BUILDLINK_TREE+=	libkdegames
 
 .if !defined(LIBKDEGAMES_BUILDLINK3_MK)
 LIBKDEGAMES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdegames+=	libkdegames>=19.08.3
-BUILDLINK_ABI_DEPENDS.libkdegames?=	libkdegames>=23.08.4nb10
+BUILDLINK_API_DEPENDS.libkdegames+=	libkdegames>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkdegames?=	libkdegames>=25.08.2
 BUILDLINK_PKGSRCDIR.libkdegames?=	../../games/libkdegames
 
-.include "../../devel/kdeclarative/buildlink3.mk"
-.include "../../net/kdnssd/buildlink3.mk"
-.include "../../net/knewstuff/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-kdeclarative/buildlink3.mk"
+.include "../../net/kf6-kdnssd/buildlink3.mk"
+.include "../../net/kf6-knewstuff/buildlink3.mk"
+.include "../../textproc/kf6-kcompletion/buildlink3.mk"
+.include "../../x11/kf6-kxmlgui/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKDEGAMES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkdegames
