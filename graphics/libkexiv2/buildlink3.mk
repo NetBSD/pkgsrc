151d6a6cc2ff352d268cec36ea5d7f3a5391486d
markd <markd@pkgsrc.org>
2025-12-21 12:46:26 +0000

    libkexiv2: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- graphics/libkexiv2/buildlink3.mk
+++ graphics/libkexiv2/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.45 2025/10/23 20:37:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.46 2025/12/21 12:46:26 markd Exp $
 
 BUILDLINK_TREE+=	libkexiv2
 
 .if !defined(LIBKEXIV2_BUILDLINK3_MK)
 LIBKEXIV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=17.12.1
-BUILDLINK_ABI_DEPENDS.libkexiv2?=		libkexiv2>=23.08.4nb8
+BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkexiv2?=	libkexiv2>=25.08.2
 BUILDLINK_PKGSRCDIR.libkexiv2?=		../../graphics/libkexiv2
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKEXIV2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkexiv2
