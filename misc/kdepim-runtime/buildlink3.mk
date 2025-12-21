596ac53d254b0fe92b8b5c76d0c677fc4bd2c919
markd <markd@pkgsrc.org>
2025-12-21 14:02:15 +0000

    kdepim-runtime: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/kdepim-runtime/buildlink3.mk
+++ misc/kdepim-runtime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 14:02:15 markd Exp $
 
 BUILDLINK_TREE+=	kdepim-runtime
 
@@ -6,14 +6,18 @@ BUILDLINK_TREE+=	kdepim-runtime
 KDEPIM_RUNTIME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdepim-runtime+=	kdepim-runtime>=20.04.1
-BUILDLINK_ABI_DEPENDS.kdepim-runtime?=	kdepim-runtime>=23.08.4nb16
+BUILDLINK_ABI_DEPENDS.kdepim-runtime?=	kdepim-runtime>=25.08.3
 BUILDLINK_PKGSRCDIR.kdepim-runtime?=	../../misc/kdepim-runtime
 
-.include "../../devel/knotifyconfig/buildlink3.mk"
-.include "../../misc/akonadi-notes/buildlink3.mk"
+.include "../../devel/kf6-knotifyconfig/buildlink3.mk"
+.include "../../devel/libkgapi/buildlink3.mk"
+.include "../../mail/kmbox/buildlink3.mk"
+.include "../../misc/kf6-kdav/buildlink3.mk"
 .include "../../misc/pimcommon/buildlink3.mk"
+.include "../../textproc/libkolabxml/buildlink3.mk"
 .include "../../time/akonadi-calendar/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../time/kf6-kholidays/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KDEPIM_RUNTIME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kdepim-runtime
