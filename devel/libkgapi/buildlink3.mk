3feb8e5afc667a11cc225656352c56178f9dd6d9
markd <markd@pkgsrc.org>
2025-12-21 10:23:01 +0000

    libkgapi:  update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- devel/libkgapi/buildlink3.mk
+++ devel/libkgapi/buildlink3.mk
@@ -1,20 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.17 2025/10/23 20:36:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2025/12/21 10:23:01 markd Exp $
 
 BUILDLINK_TREE+=	libkgapi
 
 .if !defined(LIBKGAPI_BUILDLINK3_MK)
 LIBKGAPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkgapi+=	libkgapi>=20.04.1
-BUILDLINK_ABI_DEPENDS.libkgapi?=		libkgapi>=23.08.4nb10
+BUILDLINK_API_DEPENDS.libkgapi+=	libkgapi>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkgapi?=	libkgapi>=25.08.2
 BUILDLINK_PKGSRCDIR.libkgapi?=		../../devel/libkgapi
 
-.include "../../devel/kio/buildlink3.mk"
-.include "../../misc/kcontacts/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../misc/kf6-kcontacts/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
-.include "../../time/kcalendarcore/buildlink3.mk"
-.include "../../x11/qt5-qtwebengine/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../time/kf6-kcalendarcore/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKGAPI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkgapi
