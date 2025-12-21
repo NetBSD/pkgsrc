c932116ffe4a6b33c11a0f76f320633c0cb9e36e
markd <markd@pkgsrc.org>
2025-12-21 15:14:45 +0000

    okular: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- print/okular/buildlink3.mk
+++ print/okular/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2021/08/31 11:29:24 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2025/12/21 15:14:45 markd Exp $
 
 BUILDLINK_TREE+=	okular
 
 .if !defined(OKULAR_BUILDLINK3_MK)
 OKULAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.okular+=	okular>=20.12.3nb8
+BUILDLINK_API_DEPENDS.okular+=	okular>=25.08.2
 BUILDLINK_PKGSRCDIR.okular?=	../../print/okular
 
 .endif	# OKULAR_BUILDLINK3_MK
