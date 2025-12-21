7c272f98963f8ac0c55f160e18dd18ad1bf51f61
markd <markd@pkgsrc.org>
2025-12-21 12:35:52 +0000

    libkdcraw: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- graphics/libkdcraw/buildlink3.mk
+++ graphics/libkdcraw/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.77 2025/10/23 20:37:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.78 2025/12/21 12:35:52 markd Exp $
 
 BUILDLINK_TREE+=	libkdcraw
 
 .if !defined(LIBKDCRAW_BUILDLINK3_MK)
 LIBKDCRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=17.12.1
-BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=23.08.4nb8
+BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=25.08.2
 BUILDLINK_PKGSRCDIR.libkdcraw?=		../../graphics/libkdcraw
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKDCRAW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkdcraw
