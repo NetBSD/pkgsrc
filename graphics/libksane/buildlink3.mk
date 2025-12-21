46d9c7b7e119caf66d610ac38eeeb10e3c02462c
markd <markd@pkgsrc.org>
2025-12-21 12:48:34 +0000

    libksane: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- graphics/libksane/buildlink3.mk
+++ graphics/libksane/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.74 2025/10/23 20:37:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.75 2025/12/21 12:48:34 markd Exp $
 
 BUILDLINK_TREE+=	libksane
 
 .if !defined(LIBKSANE_BUILDLINK3_MK)
 LIBKSANE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libksane+=	libksane>=17.12.1
-BUILDLINK_ABI_DEPENDS.libksane?=		libksane>=23.08.4nb10
+BUILDLINK_API_DEPENDS.libksane+=	libksane>=25.08.2
+BUILDLINK_ABI_DEPENDS.libksane?=	libksane>=25.08.2
 BUILDLINK_PKGSRCDIR.libksane?=		../../graphics/libksane
 
 .include "../../graphics/ksanecore/buildlink3.mk"
-.include "../../security/kwallet/buildlink3.mk"
-.include "../../x11/ktextwidgets/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../security/kf6-kwallet/buildlink3.mk"
+.include "../../x11/kf6-ktextwidgets/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKSANE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libksane
