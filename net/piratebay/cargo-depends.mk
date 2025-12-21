2b07ba3cdc96e6b2de682a3e2391172f9042bd6e
pin <pin@pkgsrc.org>
2025-12-21 17:39:16 +0000

    net/piratebay: update to 0.2.0
    What's Changed
    
        readme: add instructions for archlinux (AUR) by @tsirysndr in #12
        Torrent::num_files is optional, tracker refresh by @schell in #13

--- net/piratebay/cargo-depends.mk
+++ net/piratebay/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.5 2024/04/20 20:32:46 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.6 2025/12/21 17:39:16 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.21.0
 CARGO_CRATE_DEPENDS+=	adler-1.0.2
