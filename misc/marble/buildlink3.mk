d9f01a73e970d035a5a09375b2b18f9593cea972
markd <markd@pkgsrc.org>
2025-12-21 14:38:38 +0000

    marble: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/marble/buildlink3.mk
+++ misc/marble/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.60 2025/10/23 20:38:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.61 2025/12/21 14:38:38 markd Exp $
 
 BUILDLINK_TREE+=	marble
 
 .if !defined(MARBLE_BUILDLINK3_MK)
 MARBLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.marble+=	marble>=19.08.3
-BUILDLINK_ABI_DEPENDS.marble?=	marble>=23.08.4nb17
+BUILDLINK_API_DEPENDS.marble+=	marble>=25.08.2
+BUILDLINK_ABI_DEPENDS.marble?=	marble>=25.08.2
 BUILDLINK_PKGSRCDIR.marble?=	../../misc/marble
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# MARBLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-marble
