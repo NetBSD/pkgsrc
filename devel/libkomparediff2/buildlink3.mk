abb7136f562fab9965e79dccd03479b67dbae50a
markd <markd@pkgsrc.org>
2025-12-21 10:25:40 +0000

    libkomparediff2: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- devel/libkomparediff2/buildlink3.mk
+++ devel/libkomparediff2/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.39 2025/10/23 20:36:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2025/12/21 10:25:40 markd Exp $
 
 BUILDLINK_TREE+=	libkomparediff2
 
 .if !defined(LIBKOMPAREDIFF2_BUILDLINK3_MK)
 LIBKOMPAREDIFF2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkomparediff2+=	libkomparediff2>=19.08.3
-BUILDLINK_ABI_DEPENDS.libkomparediff2?=	libkomparediff2>=23.08.4nb10
+BUILDLINK_API_DEPENDS.libkomparediff2+=	libkomparediff2>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkomparediff2?=	libkomparediff2>=25.08.2
 BUILDLINK_PKGSRCDIR.libkomparediff2?=	../../devel/libkomparediff2
 
-.include "../../devel/kio/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../x11/kf6-kxmlgui/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKOMPAREDIFF2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkomparediff2
