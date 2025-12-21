b6b1ff0c2a19bb6920b90fbb350dbddd2347bfdf
markd <markd@pkgsrc.org>
2025-12-21 15:09:54 +0000

    libktorrent: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- net/libktorrent/buildlink3.mk
+++ net/libktorrent/buildlink3.mk
@@ -1,19 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.73 2025/10/23 20:38:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.74 2025/12/21 15:09:54 markd Exp $
 
 BUILDLINK_TREE+=	libktorrent
 
 .if !defined(LIBKTORRENT_BUILDLINK3_MK)
 LIBKTORRENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libktorrent+=	libktorrent>=23.04.3
-BUILDLINK_ABI_DEPENDS.libktorrent?=	libktorrent>=23.08.4nb11
+BUILDLINK_API_DEPENDS.libktorrent+=	libktorrent>=25.08.2
+BUILDLINK_ABI_DEPENDS.libktorrent?=	libktorrent>=25.08.2
 BUILDLINK_PKGSRCDIR.libktorrent?=	../../net/libktorrent
 
 .include "../../devel/boost-headers/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
-.include "../../devel/kio/buildlink3.mk"
-.include "../../security/qca2-qt5/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKTORRENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libktorrent
