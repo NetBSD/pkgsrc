5787a9018f9ee77b5fc62e766863f01b0d47a86c
markd <markd@pkgsrc.org>
2025-12-21 13:13:55 +0000

    ksmtp: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/ksmtp/buildlink3.mk
+++ mail/ksmtp/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.38 2025/10/23 20:38:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2025/12/21 13:13:55 markd Exp $
 
 BUILDLINK_TREE+=	ksmtp
 
 .if !defined(KSMTP_BUILDLINK3_MK)
 KSMTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ksmtp+=	ksmtp>=17.12.1
-BUILDLINK_ABI_DEPENDS.ksmtp?=	ksmtp>=23.08.4nb10
+BUILDLINK_API_DEPENDS.ksmtp+=	ksmtp>=25.08.3
+BUILDLINK_ABI_DEPENDS.ksmtp?=	ksmtp>=25.08.3
 BUILDLINK_PKGSRCDIR.ksmtp?=	../../mail/ksmtp
 
-.include "../../devel/kio/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
 .include "../../mail/kmime/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KSMTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ksmtp
