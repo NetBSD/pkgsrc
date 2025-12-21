8e68c3606182f517db08ac19ed7bf45bab966625
markd <markd@pkgsrc.org>
2025-12-21 12:56:08 +0000

    akonadi: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/akonadi/buildlink3.mk
+++ mail/akonadi/buildlink3.mk
@@ -1,20 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.51 2025/10/23 20:38:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.52 2025/12/21 12:56:08 markd Exp $
 
 BUILDLINK_TREE+=	akonadi
 
 .if !defined(AKONADI_BUILDLINK3_MK)
 AKONADI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.akonadi+=	akonadi>=17.12.1
-BUILDLINK_ABI_DEPENDS.akonadi?=	akonadi>=23.08.4nb11
+BUILDLINK_API_DEPENDS.akonadi+=	akonadi>=25.08.2
+BUILDLINK_ABI_DEPENDS.akonadi?=	akonadi>=25.08.2
 BUILDLINK_PKGSRCDIR.akonadi?=	../../mail/akonadi
 
 BUILDLINK_FILES.akonadi+=	share/dbus-1/interfaces/org.freedesktop.Akonadi.*.xml
-BUILDLINK_FILES.akonadi+=	share/kf5/akonadi/kcfg2dbus.xsl
+BUILDLINK_FILES.akonadi+=	share/kf6/akonadi/kcfg2dbus.xsl
 
-.include "../../devel/kio/buildlink3.mk"
-.include "../../devel/kitemmodels/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../devel/kf6-kitemmodels/buildlink3.mk"
+.include "../../devel/qt6-qttools/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/kf6-kxmlgui/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# AKONADI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-akonadi
