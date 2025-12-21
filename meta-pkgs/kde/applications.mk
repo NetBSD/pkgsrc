95e12cda85531ec0f1d45993614f2ef581f92498
markd <markd@pkgsrc.org>
2025-12-21 09:45:08 +0000

    kde: bump default KAPPSVER to 25.08.3

--- meta-pkgs/kde/applications.mk
+++ meta-pkgs/kde/applications.mk
@@ -1,8 +1,12 @@
-# $NetBSD: applications.mk,v 1.10 2024/02/06 10:30:14 markd Exp $
+# $NetBSD: applications.mk,v 1.11 2025/12/21 09:45:08 markd Exp $
 
-KAPPSVER?=	23.08.4
+KAPPSVER?=	25.08.3
 CATEGORIES+=	kde
 MASTER_SITES?=	${MASTER_SITE_KDE:=release-service/${KAPPSVER}/src/}
 EXTRACT_SUFX?=	.tar.xz
+
+.if !defined(STILL_KF5)
+USE_CXX_FEATURES+=	c++20
+.endif
 
 .include "../../meta-pkgs/kde/Makefile.common"
