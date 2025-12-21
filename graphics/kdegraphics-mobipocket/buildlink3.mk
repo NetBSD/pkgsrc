6256bd89303fe7d3dc3233d676b4e6fdeab15bf1
markd <markd@pkgsrc.org>
2025-12-21 12:12:12 +0000

    kdegraphics-mobipocket: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- graphics/kdegraphics-mobipocket/buildlink3.mk
+++ graphics/kdegraphics-mobipocket/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.54 2025/10/23 20:37:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.55 2025/12/21 12:12:12 markd Exp $
 
 BUILDLINK_TREE+=	kdegraphics-mobipocket
 
 .if !defined(KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK)
 KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kdegraphics-mobipocket+=	kdegraphics-mobipocket>=17.12.1
-BUILDLINK_ABI_DEPENDS.kdegraphics-mobipocket?=	kdegraphics-mobipocket>=23.08.4nb10
+BUILDLINK_API_DEPENDS.kdegraphics-mobipocket+=	kdegraphics-mobipocket>=25.08.2
+BUILDLINK_ABI_DEPENDS.kdegraphics-mobipocket?=	kdegraphics-mobipocket>=25.08.2
 BUILDLINK_PKGSRCDIR.kdegraphics-mobipocket?=	../../graphics/kdegraphics-mobipocket
 
-.include "../../devel/kio/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kdegraphics-mobipocket
