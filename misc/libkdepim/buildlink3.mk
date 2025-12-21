27b151b5655d4e7acd41c8d4b54bf2354bca81a7
markd <markd@pkgsrc.org>
2025-12-21 14:32:11 +0000

    libkdepim: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/libkdepim/buildlink3.mk
+++ misc/libkdepim/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 14:32:11 markd Exp $
 
 BUILDLINK_TREE+=	libkdepim
 
 .if !defined(LIBKDEPIM_BUILDLINK3_MK)
 LIBKDEPIM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdepim+=	libkdepim>=17.12.1
-BUILDLINK_ABI_DEPENDS.libkdepim?=		libkdepim>=23.08.4nb11
+BUILDLINK_API_DEPENDS.libkdepim+=	libkdepim>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkdepim?=	libkdepim>=25.08.2
 BUILDLINK_PKGSRCDIR.libkdepim?=		../../misc/libkdepim
 
 BUILDLINK_FILES.libkdepim+=	share/dbus-1/interfaces/org.kde.*.xml
 
-.include "../../databases/kldap/buildlink3.mk"
-.include "../../misc/akonadi-contacts/buildlink3.mk"
-.include "../../misc/akonadi-search/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../textproc/kf6-kcompletion/buildlink3.mk"
+.include "../../x11/kf6-kwidgetsaddons/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKDEPIM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkdepim
