54adf3d128a1b53c68f91d16e384a3fc516b5c8f
markd <markd@pkgsrc.org>
2025-12-21 14:44:08 +0000

    pimcommon: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/pimcommon/buildlink3.mk
+++ misc/pimcommon/buildlink3.mk
@@ -1,20 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 14:44:08 markd Exp $
 
 BUILDLINK_TREE+=	pimcommon
 
 .if !defined(PIMCOMMON_BUILDLINK3_MK)
 PIMCOMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pimcommon+=	pimcommon>=17.12.1
-BUILDLINK_ABI_DEPENDS.pimcommon?=		pimcommon>=23.08.4nb11
+BUILDLINK_API_DEPENDS.pimcommon+=	pimcommon>=25.08.3
+BUILDLINK_ABI_DEPENDS.pimcommon?=	pimcommon>=25.08.3
 BUILDLINK_PKGSRCDIR.pimcommon?=		../../misc/pimcommon
 
+.include "../../databases/kldap/buildlink3.mk"
+.include "../../devel/kf6-purpose/buildlink3.mk"
+.include "../../devel/qt6-qttools/buildlink3.mk"
 .include "../../mail/kimap/buildlink3.mk"
+.include "../../misc/akonadi-contacts/buildlink3.mk"
+.include "../../misc/akonadi-search/buildlink3.mk"
 .include "../../misc/libkdepim/buildlink3.mk"
-.include "../../net/knewstuff/buildlink3.mk"
-.include "../../textproc/kpimtextedit/buildlink3.mk"
+.include "../../net/kf6-knewstuff/buildlink3.mk"
 .include "../../textproc/ktextaddons/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/plasma6-plasma-activities/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# PIMCOMMON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pimcommon
