d7a8e19081a46ed65fb32d367d3bc63817424bf7
markd <markd@pkgsrc.org>
2025-12-21 19:51:45 +0000

    akonadi-calendar: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- time/akonadi-calendar/buildlink3.mk
+++ time/akonadi-calendar/buildlink3.mk
@@ -1,18 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:39:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 19:51:45 markd Exp $
 
 BUILDLINK_TREE+=	akonadi-calendar
 
 .if !defined(AKONADI_CALENDAR_BUILDLINK3_MK)
 AKONADI_CALENDAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.akonadi-calendar+=	akonadi-calendar>=17.12.1
-BUILDLINK_ABI_DEPENDS.akonadi-calendar?=		akonadi-calendar>=23.08.4nb11
+BUILDLINK_API_DEPENDS.akonadi-calendar+=	akonadi-calendar>=25.08.3
+BUILDLINK_ABI_DEPENDS.akonadi-calendar?=	akonadi-calendar>=25.08.3
 BUILDLINK_PKGSRCDIR.akonadi-calendar?=		../../time/akonadi-calendar
 
 .include "../../mail/kmailtransport/buildlink3.mk"
 .include "../../misc/akonadi-contacts/buildlink3.mk"
+.include "../../net/messagelib/buildlink3.mk"
 .include "../../time/kcalutils/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# AKONADI_CALENDAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-akonadi-calendar
