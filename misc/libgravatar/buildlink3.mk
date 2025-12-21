e1510c357c1716032eb46dc13294d54f5657ac13
markd <markd@pkgsrc.org>
2025-12-21 14:30:08 +0000

    libgravatar: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/libgravatar/buildlink3.mk
+++ misc/libgravatar/buildlink3.mk
@@ -1,16 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 14:30:08 markd Exp $
 
 BUILDLINK_TREE+=	libgravatar
 
 .if !defined(LIBGRAVATAR_BUILDLINK3_MK)
 LIBGRAVATAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgravatar+=	libgravatar>=20.04.1
-BUILDLINK_ABI_DEPENDS.libgravatar?=	libgravatar>=23.08.4nb10
+BUILDLINK_API_DEPENDS.libgravatar+=	libgravatar>=25.08.3
+BUILDLINK_ABI_DEPENDS.libgravatar?=	libgravatar>=25.08.3
 BUILDLINK_PKGSRCDIR.libgravatar?=	../../misc/libgravatar
 
-.include "../../misc/pimcommon/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../x11/kf6-kconfigwidgets/buildlink3.mk"
+.include "../../x11/kf6-kwidgetsaddons/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBGRAVATAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgravatar
