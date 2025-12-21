b602c577a8422d9d6a850502037206ea52445f26
markd <markd@pkgsrc.org>
2025-12-21 13:51:08 +0000

    incidenceeditor: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/incidenceeditor/buildlink3.mk
+++ misc/incidenceeditor/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 13:51:08 markd Exp $
 
 BUILDLINK_TREE+=	incidenceeditor
 
 .if !defined(INCIDENCEEDITOR_BUILDLINK3_MK)
 INCIDENCEEDITOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.incidenceeditor+=	incidenceeditor>=20.04.1
-BUILDLINK_ABI_DEPENDS.incidenceeditor?=	incidenceeditor>=23.08.4nb11
+BUILDLINK_API_DEPENDS.incidenceeditor+=	incidenceeditor>=25.08.3
+BUILDLINK_ABI_DEPENDS.incidenceeditor?=	incidenceeditor>=25.08.3
 BUILDLINK_PKGSRCDIR.incidenceeditor?=	../../misc/incidenceeditor
 
 .include "../../misc/eventviews/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# INCIDENCEEDITOR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-incidenceeditor
