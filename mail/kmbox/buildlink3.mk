7151fc23248113dcba15a14937863bd6025ee951
markd <markd@pkgsrc.org>
2025-12-21 13:10:00 +0000

    kmbox: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/kmbox/buildlink3.mk
+++ mail/kmbox/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.24 2025/10/23 20:38:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2025/12/21 13:10:00 markd Exp $
 
 BUILDLINK_TREE+=	kmbox
 
 .if !defined(KMBOX_BUILDLINK3_MK)
 KMBOX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kmbox+=	kmbox>=17.12.1
-BUILDLINK_ABI_DEPENDS.kmbox?=	kmbox>=23.08.4nb8
+BUILDLINK_API_DEPENDS.kmbox+=	kmbox>=25.08.2
+BUILDLINK_ABI_DEPENDS.kmbox?=	kmbox>=25.08.2
 BUILDLINK_PKGSRCDIR.kmbox?=	../../mail/kmbox
 
 .include "../../mail/kmime/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KMBOX_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kmbox
