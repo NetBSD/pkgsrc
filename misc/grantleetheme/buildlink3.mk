9e7aef8e8da93d59332667eea7e9203029b6e8af
markd <markd@pkgsrc.org>
2025-12-21 13:49:08 +0000

    grantleetheme: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/grantleetheme/buildlink3.mk
+++ misc/grantleetheme/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 13:49:08 markd Exp $
 
 BUILDLINK_TREE+=	grantleetheme
 
 .if !defined(GRANTLEETHEME_BUILDLINK3_MK)
 GRANTLEETHEME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.grantleetheme+=	grantleetheme>=20.04.1
-BUILDLINK_ABI_DEPENDS.grantleetheme?=	grantleetheme>=23.08.4nb10
+BUILDLINK_API_DEPENDS.grantleetheme+=	grantleetheme>=25.08.2
+BUILDLINK_ABI_DEPENDS.grantleetheme?=	grantleetheme>=25.08.2
 BUILDLINK_PKGSRCDIR.grantleetheme?=	../../misc/grantleetheme
 
-.include "../../devel/grantlee-qt5/buildlink3.mk"
-.include "../../net/knewstuff/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../net/kf6-knewstuff/buildlink3.mk"
+.include "../../textproc/kf6-ktexttemplate/buildlink3.mk"
+.include "../../x11/kf6-kxmlgui/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# GRANTLEETHEME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-grantleetheme
