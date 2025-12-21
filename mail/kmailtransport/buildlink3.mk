862d1922d3eb09323d0731ed4ab0184b623badc5
markd <markd@pkgsrc.org>
2025-12-21 13:08:31 +0000

    kmailtransport: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/kmailtransport/buildlink3.mk
+++ mail/kmailtransport/buildlink3.mk
@@ -1,19 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 13:08:31 markd Exp $
 
 BUILDLINK_TREE+=	kmailtransport
 
 .if !defined(KMAILTRANSPORT_BUILDLINK3_MK)
 KMAILTRANSPORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kmailtransport+=	kmailtransport>=17.12.1
-BUILDLINK_ABI_DEPENDS.kmailtransport?=	kmailtransport>=23.08.4nb11
+BUILDLINK_API_DEPENDS.kmailtransport+=	kmailtransport>=25.08.2
+BUILDLINK_ABI_DEPENDS.kmailtransport?=	kmailtransport>=25.08.2
 BUILDLINK_PKGSRCDIR.kmailtransport?=	../../mail/kmailtransport
 
-.include "../../devel/kcmutils/buildlink3.mk"
+.include "../../devel/libkgapi/buildlink3.mk"
+.include "../../devel/kf6-kcmutils/buildlink3.mk"
 .include "../../mail/akonadi-mime/buildlink3.mk"
 .include "../../mail/ksmtp/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../security/qt6-qtkeychain/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KMAILTRANSPORT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kmailtransport
