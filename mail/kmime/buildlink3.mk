02fed878ac4f85a675133384b150e990a60c301f
markd <markd@pkgsrc.org>
2025-12-21 13:11:23 +0000

    kmime: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/kmime/buildlink3.mk
+++ mail/kmime/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.24 2025/10/23 20:38:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2025/12/21 13:11:23 markd Exp $
 
 BUILDLINK_TREE+=	kmime
 
 .if !defined(KMIME_BUILDLINK3_MK)
 KMIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kmime+=	kmime>=17.12.1
-BUILDLINK_ABI_DEPENDS.kmime?=	kmime>=23.08.4nb8
+BUILDLINK_API_DEPENDS.kmime+=	kmime>=25.08.2
+BUILDLINK_ABI_DEPENDS.kmime?=	kmime>=25.08.2
 BUILDLINK_PKGSRCDIR.kmime?=	../../mail/kmime
 
-.include "../../devel/ki18n/buildlink3.mk"
-.include "../../textproc/kcodecs/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../devel/qt6-qttools/buildlink3.mk"
+.include "../../textproc/kf6-kcodecs/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KMIME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kmime
