fbb25896e94de98bd83375acf40363ef9e6fa30f
markd <markd@pkgsrc.org>
2025-12-21 13:20:56 +0000

    mailcommon: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- mail/mailcommon/buildlink3.mk
+++ mail/mailcommon/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.14 2025/10/23 20:38:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2025/12/21 13:20:56 markd Exp $
 
 BUILDLINK_TREE+=	mailcommon
 
 .if !defined(MAILCOMMON_BUILDLINK3_MK)
 MAILCOMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mailcommon+=	mailcommon>=20.04.1
-BUILDLINK_ABI_DEPENDS.mailcommon?=	mailcommon>=23.08.4nb11
+BUILDLINK_API_DEPENDS.mailcommon+=	mailcommon>=25.08.3
+BUILDLINK_ABI_DEPENDS.mailcommon?=	mailcommon>=25.08.3
 BUILDLINK_PKGSRCDIR.mailcommon?=	../../mail/mailcommon
 
 .include "../../mail/mailimporter/buildlink3.mk"
 .include "../../net/messagelib/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# MAILCOMMON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mailcommon
