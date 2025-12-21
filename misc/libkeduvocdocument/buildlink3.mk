22054d8913d74e8d2389b51a72db1eae5ea75950
markd <markd@pkgsrc.org>
2025-12-21 14:33:41 +0000

    libkeduvocdocument: update to 25.08.3
    Qt6/KF6 version - Two years of development.

--- misc/libkeduvocdocument/buildlink3.mk
+++ misc/libkeduvocdocument/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.38 2025/10/23 20:38:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2025/12/21 14:33:41 markd Exp $
 
 BUILDLINK_TREE+=	libkeduvocdocument
 
 .if !defined(LIBKEDUVOCDOCUMENT_BUILDLINK3_MK)
 LIBKEDUVOCDOCUMENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkeduvocdocument+=	libkeduvocdocument>=19.08.3
-BUILDLINK_ABI_DEPENDS.libkeduvocdocument?=	libkeduvocdocument>=23.08.4nb10
+BUILDLINK_API_DEPENDS.libkeduvocdocument+=	libkeduvocdocument>=25.08.2
+BUILDLINK_ABI_DEPENDS.libkeduvocdocument?=	libkeduvocdocument>=25.08.2
 BUILDLINK_PKGSRCDIR.libkeduvocdocument?=	../../misc/libkeduvocdocument
 
-.include "../../devel/kio/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# LIBKEDUVOCDOCUMENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkeduvocdocument
