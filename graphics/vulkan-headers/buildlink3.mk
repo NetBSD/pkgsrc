45d0dfb2e0dd5e860a48fc68a4279200dc06d96b
adam <adam@pkgsrc.org>
2025-12-22 14:43:12 +0000

    vulkan-headers: updated to 1.4.335
    1.4.335
    Unknown changes

--- graphics/vulkan-headers/buildlink3.mk
+++ graphics/vulkan-headers/buildlink3.mk
@@ -1,17 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.4 2024/04/14 14:24:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2025/12/22 14:43:12 adam Exp $
 
 BUILDLINK_TREE+=	vulkan-headers
 
 .if !defined(VULKAN_HEADERS_BUILDLINK3_MK)
 VULKAN_HEADERS_BUILDLINK3_MK:=
 
+BUILDLINK_DEPMETHOD.vulkan-headers?=	build
+
 BUILDLINK_API_DEPENDS.vulkan-headers+=	vulkan-headers>=1.1.126.0
 BUILDLINK_ABI_DEPENDS.vulkan-headers+=	vulkan-headers>=1.3.224nb1
 BUILDLINK_PKGSRCDIR.vulkan-headers?=	../../graphics/vulkan-headers
 
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} != "Darwin"
 .include "../../x11/libXrandr/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libxcb/buildlink3.mk"
+.endif
 .endif	# VULKAN_HEADERS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-vulkan-headers
