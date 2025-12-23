830fb49090c4640bdcb316d1d066882f9bf2dfb1
markd <markd@pkgsrc.org>
2025-12-23 06:14:01 +0000

    plasma6-libplasma: add version 6.5.2
    Foundational libraries, components, and tools of the Plasma workspaces
    
    libplasma provides the following:
    - QML components that can be used by any Plasma shell
    - A C++ library: `libplasma` itself
    - Script engines

---
+++ x11/plasma6-libplasma/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/23 06:14:02 markd Exp $
+
+BUILDLINK_TREE+=	plasma6-libplasma
+
+.if !defined(PLASMA6_LIBPLASMA_BUILDLINK3_MK)
+PLASMA6_LIBPLASMA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma6-libplasma+=	plasma6-libplasma>=6.5.2
+BUILDLINK_PKGSRCDIR.plasma6-libplasma?=		../../x11/plasma6-libplasma
+
+.include "../../devel/kf6-kconfig/buildlink3.mk"
+.include "../../devel/kf6-kcoreaddons/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../devel/kf6-knotifications/buildlink3.mk"
+.include "../../devel/kf6-kpackage/buildlink3.mk"
+.include "../../graphics/kf6-kiconthemes/buildlink3.mk"
+.include "../../graphics/kf6-ksvg/buildlink3.mk"
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../x11/kf6-kglobalaccel/buildlink3.mk"
+.include "../../x11/kf6-kguiaddons/buildlink3.mk"
+.include "../../x11/kf6-kirigami/buildlink3.mk"
+.include "../../x11/kf6-kwindowsystem/buildlink3.mk"
+.include "../../x11/plasma6-plasma-activities/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# PLASMA6_LIBPLASMA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma6-libplasma
