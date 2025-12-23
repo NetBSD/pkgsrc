23370f79b494432b77d4b267bcc6960118c10db8
markd <markd@pkgsrc.org>
2025-12-23 06:22:34 +0000

    plasma6-kdecoration: add version 6.5.2
    KDecoration3 is a library to create window decorations. These window decorations
    can be used by for example an X11 based window manager which re-parents a Client
    window to a window decoration frame.
    
    The library consists of two parts:
    * Decoration API for implementing a Decoration theme
    * Private API to implement the backend part (e.g. from Window Manager side)

---
+++ x11/plasma6-kdecoration/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/23 06:22:34 markd Exp $
+
+BUILDLINK_TREE+=	plasma6-kdecoration
+
+.if !defined(PLASMA6_KDECORATION_BUILDLINK3_MK)
+PLASMA6_KDECORATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma6-kdecoration+=	plasma6-kdecoration>=6.5.2
+BUILDLINK_PKGSRCDIR.plasma6-kdecoration?=	../../x11/plasma6-kdecoration
+
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# PLASMA6_KDECORATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma6-kdecoration
