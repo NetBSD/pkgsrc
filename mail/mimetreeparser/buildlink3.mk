35b62fd2d3e95504bb4bf6a4b7d3640e0fbaf3bb
markd <markd@pkgsrc.org>
2025-12-23 03:30:30 +0000

    mimetreeparser: add version 25.08.3
    This repository contains a parser for a MIME tree and is based on KMime. The
    goal is given a MIME tree to extract a list of parts (e.g. text, html) and a
    list of attachments, check the validity of the signatures and decrypt any
    encrypted part.

---
+++ mail/mimetreeparser/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/23 03:30:31 markd Exp $
+
+BUILDLINK_TREE+=	mimetreeparser
+
+.if !defined(MIMETREEPARSER_BUILDLINK3_MK)
+MIMETREEPARSER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mimetreeparser+=	mimetreeparser>=25.08.3
+BUILDLINK_PKGSRCDIR.mimetreeparser?=	../../mail/mimetreeparser
+
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../mail/kmbox/buildlink3.mk"
+.include "../../mail/kmime/buildlink3.mk"
+.include "../../security/libkleo/buildlink3.mk"
+.include "../../textproc/kf6-kcodecs/buildlink3.mk"
+.include "../../time/kf6-kcalendarcore/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# MIMETREEPARSER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mimetreeparser
