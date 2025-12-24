72b311410335084bed5c595c0eae0ec327332546
ryoon <ryoon@pkgsrc.org>
2025-12-24 16:47:38 +0000

    mail/thunderbird: Update to 146.0.1
    Changelog:
    146.0.1:
    What's Fixed
    
    fixed
    Reverted fix that localized consistent special folder names in server's language
    
    
    146.0:
    What's New
    
    new
    Enabled configuration of preferred OpenPGP keyserver via the UI
    
    What's Changed
    
    changed
    Existing logins are migrated to make use of more modern AES cryptography
    
    What's Fixed
    
    fixed
    Memory was not cleaned up after opening New Window from folder pane context
    menu
    
    fixed
    Thunderbird could crash in server subscription logic
    
    fixed
    Importing a vCard only displayed VCF files in the file picker
    
    fixed
    Could not distinguish folders with same name in 'Recent Destinations' and
    'Favorites'
    
    fixed
    Could not compose new message if the folder pane was empty
    
    fixed
    'Delete' was missing from context menu when multiple IMAP folders were selected
    
    fixed
    'Delete' button in unified toolbar could delete attachments instead of message
    
    fixed
    Servers lacking localization showed special folder names in server??s language
    
    fixed
    'Recent destinations' submenu was not sorted by time of modification
    
    fixed
    Account column could display incorrect account name
    
    fixed
    'Tag' submenu of mail context menu could be populated incorrectly
    
    fixed
    Extending the expiration date of OpenPGP key set incorrect date
    
    fixed
    Could not sign/encrypt (PGP) when using -compose and Thunderbird was not
    running
    
    fixed
    Account Hub email success messages were not always accurate about config source
    
    fixed
    Account Hub advanced config setup did not include default outgoing config
    
    fixed
    Account Hub local address book creation could continue with blank name
    
    fixed
    Detailed security exception dialog was not shown when adding certificate
    exception
    
    fixed
    The default account could be reset after restart if uninitialized
    
    fixed
    Opening a vCard (.vcf) from file manager or CLI did not work in some cases
    
    fixed
    Dragging attachments on macOS to folder or desktop no longer worked
    
    fixed
    EWS folder properties "Select this folder for offline use" did not work
    
    fixed
    Thunderbird could crash when mail folder was renamed or moved
    
    fixed
    Account Hub could fail when manually creating EWS account
    
    fixed
    'Offline' folder flag for EWS folders was reset on restart
    
    fixed
    'Open with' of Microsoft Office attachment did not mark file with MOTW label
    
    fixed
    Visual and UX improvements
    
    fixed
    Security fixes
    
    Security fixes:
    Mozilla Foundation Security Advisory 2025-95
    #CVE-2025-14321: Use-after-free in the WebRTC: Signaling component
    #CVE-2025-14322: Sandbox escape due to incorrect boundary conditions in the
     Graphics: CanvasWebGL component
    #CVE-2025-14323: Privilege escalation in the DOM: Notifications component
    #CVE-2025-14324: JIT miscompilation in the JavaScript Engine: JIT component
    #CVE-2025-14325: JIT miscompilation in the JavaScript Engine: JIT component
    #CVE-2025-14326: Use-after-free in the Audio/Video: GMP component
    #CVE-2025-14327: Spoofing issue in the Downloads Panel component
    #CVE-2025-14328: Privilege escalation in the Netmonitor component
    #CVE-2025-14329: Privilege escalation in the Netmonitor component
    #CVE-2025-14330: JIT miscompilation in the JavaScript Engine: JIT component
    #CVE-2025-14331: Same-origin policy bypass in the Request Handling component
    #CVE-2025-14332: Memory safety bugs fixed in Firefox 146 and Thunderbird 146
    #CVE-2025-14333: Memory safety bugs fixed in Firefox ESR 140.6, Thunderbird ESR
     140.6, Firefox 146 and Thunderbird 146

---
+++ mail/thunderbird/patches/patch-third__party_libwebrtc_modules_video__capture_video__capture__options.cc
@@ -0,0 +1,13 @@
+$NetBSD: patch-third__party_libwebrtc_modules_video__capture_video__capture__options.cc,v 1.1 2025/12/24 16:47:38 ryoon Exp $
+
+--- third_party/libwebrtc/modules/video_capture/video_capture_options.cc.orig	2025-05-21 12:41:53.782245708 +0000
++++ third_party/libwebrtc/modules/video_capture/video_capture_options.cc
+@@ -37,7 +37,7 @@ void VideoCaptureOptions::Init(Callback*
+     return;
+   }
+ #endif
+-#if defined(WEBRTC_LINUX)
++#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
+   if (!allow_v4l2_)
+     callback->OnInitialized(Status::UNAVAILABLE);
+   else
---
+++ mail/thunderbird/patches/patch-third__party_libwebrtc_rtc__base_platform__thread__types.cc
@@ -0,0 +1,29 @@
+$NetBSD: patch-third__party_libwebrtc_rtc__base_platform__thread__types.cc,v 1.1 2025/12/24 16:47:38 ryoon Exp $
+
+--- third_party/libwebrtc/rtc_base/platform_thread_types.cc.orig	2025-05-21 12:30:11.564823191 +0000
++++ third_party/libwebrtc/rtc_base/platform_thread_types.cc
+@@ -71,7 +71,11 @@ bool IsThreadRefEqual(const PlatformThre
+ #if defined(WEBRTC_WIN) || defined(WEBRTC_FUCHSIA)
+   return a == b;
+ #elif defined(WEBRTC_POSIX)
++#  if defined(__NetBSD__)
++  return a == b;
++#  else
+   return pthread_equal(a, b);
++#  endif
+ #endif
+ }
+ 
+@@ -118,6 +122,12 @@ void SetCurrentThreadName(const char* na
+   prctl(PR_SET_NAME, reinterpret_cast<unsigned long>(name));  // NOLINT
+ #elif defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
+   pthread_setname_np(name);
++#elif defined(WEBRTC_BSD)
++#  if defined(__NetBSD__)
++  pthread_setname_np(pthread_self(), "%s", const_cast<char *>(name));
++#  else
++  pthread_setname_np(pthread_self(), name);
++#  endif
+ #elif defined(WEBRTC_FUCHSIA)
+   zx_status_t status = zx_object_set_property(zx_thread_self(), ZX_PROP_NAME,
+                                               name, strlen(name));
