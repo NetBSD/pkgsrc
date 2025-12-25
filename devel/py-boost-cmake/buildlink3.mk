9f2cd4e9f1b9bf275c2967813963e7fc5bc1657b
adam <adam@pkgsrc.org>
2025-12-25 22:45:27 +0000

    boost: updated to 1.90.0
    1.90.0
    
    New Libraries
      OpenMethod:
      * Open-(multi-)methods in C++17 and above, from Jean-Louis Leroy.
    
    https://www.boost.org/releases/latest/

--- devel/py-boost-cmake/buildlink3.mk
+++ devel/py-boost-cmake/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/09/27 06:54:05 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/12/25 22:45:28 adam Exp $
 
 BUILDLINK_TREE+=	py-boost-cmake
 
 .if !defined(PY_BOOST_CMAKE_BUILDLINK3_MK)
 PY_BOOST_CMAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-boost-cmake+=	py-boost-cmake-1.89.*
-BUILDLINK_ABI_DEPENDS.py-boost-cmake+=	py-boost-cmake-1.89.*
+BUILDLINK_API_DEPENDS.py-boost-cmake+=	py-boost-cmake-1.90.*
+BUILDLINK_ABI_DEPENDS.py-boost-cmake+=	py-boost-cmake-1.90.*
 BUILDLINK_PKGSRCDIR.py-boost-cmake?=	../../devel/py-boost-cmake
 .endif # PY_BOOST_CMAKE_BUILDLINK3_MK
 
