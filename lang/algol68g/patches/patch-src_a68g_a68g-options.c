325eebc7112db39f84542a0715b706770d9b487c
rhialto <rhialto@pkgsrc.org>
2025-12-25 15:06:11 +0000

    lang/algol68g: update to 3.10.9.
    No changelog provided.

--- lang/algol68g/patches/patch-src_a68g_a68g-options.c
+++ lang/algol68g/patches/patch-src_a68g_a68g-options.c
@@ -1,15 +1,20 @@
-$NetBSD: patch-src_a68g_a68g-options.c,v 1.2 2023/12/01 18:35:12 rhialto Exp $
+$NetBSD: patch-src_a68g_a68g-options.c,v 1.3 2025/12/25 15:06:12 rhialto Exp $
 
 * Enable compiler for *BSD.
 
---- src/a68g/a68g-options.c.orig	2023-10-27 20:14:34.000000000 +0000
+--- src/a68g/a68g-options.c.orig	2025-12-02 17:58:48.000000000 +0000
 +++ src/a68g/a68g-options.c
-@@ -478,7 +478,7 @@ BOOL_T need_library (OPTION_LIST_T *i)
- #endif
+@@ -547,11 +547,11 @@ BOOL_T need_library (OPTION_LIST_T *i)
+     #endif
    }
    if (eq (q, "linux")) {
--#if defined (BUILD_LINUX)
-+#if defined (BUILD_LINUX) || defined(BUILD_BSD)
-     return (A68_TRUE);
- #else
-     io_close_tty_line ();
+-    #if defined (BUILD_LINUX)
++    #if defined (BUILD_LINUX) || defined(BUILD_BSD)
+       return (A68G_TRUE);
+     #else
+       io_close_tty_line ();
+-      WRITE (A68G_STDERR, "linux required - exiting graciously");
++      WRITE (A68G_STDERR, "BSD or linux required - exiting graciously");
+       a68g_exit (EXIT_SUCCESS);
+     #endif
+   }
