35ac01e58fc5d0b46ccbc8b8278280ca996ec349
adam <adam@pkgsrc.org>
2025-12-26 11:10:46 +0000

    libcdio-paranoia: updated to 2.0.2
    10.2+2.0.2
    
    A 6-year-old bug, seen primarily by Whipper users, concerning
    adjusting of CD-drives that have large sample offsets, or negative
    offsets has been fixed. Fix is from and thanks are due to
    buddyabaddon.
    Code has moved from savannah.gnu.org to github. CircleCI
    testing is now incorporated into builds.
    Add test on LSN on span check errors, and give a better
    message when there is a problem.
    Require libcdio at least 2.0.0
    Remove dead code after exit.
    Fix manpage for Japanese.
    Add notes for using gmake for building on FreeBSD.

--- misc/libcdio-paranoia/buildlink3.mk
+++ misc/libcdio-paranoia/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/06/28 11:34:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2025/12/26 11:10:46 adam Exp $
 
 BUILDLINK_TREE+=	libcdio-paranoia
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libcdio-paranoia
 LIBCDIO_PARANOIA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcdio-paranoia+=	libcdio-paranoia>=0.93
-BUILDLINK_ABI_DEPENDS.libcdio-paranoia?=		libcdio-paranoia>=2.0.1nb3
+BUILDLINK_ABI_DEPENDS.libcdio-paranoia+=	libcdio-paranoia>=2.0.1nb4
 BUILDLINK_PKGSRCDIR.libcdio-paranoia?=		../../misc/libcdio-paranoia
 
 .include "../../misc/libcdio/buildlink3.mk"
