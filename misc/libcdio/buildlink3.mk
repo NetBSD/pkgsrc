ddb84a7954f6588207fd0a1063d5e0760100b23e
adam <adam@pkgsrc.org>
2025-12-26 11:12:06 +0000

    revbump for libcdio

--- misc/libcdio/buildlink3.mk
+++ misc/libcdio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2022/06/28 11:34:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2025/12/26 11:12:06 adam Exp $
 
 BUILDLINK_TREE+=	libcdio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libcdio
 LIBCDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcdio+=	libcdio>=0.93
-BUILDLINK_ABI_DEPENDS.libcdio+=	libcdio>=2.1.0nb5
+BUILDLINK_ABI_DEPENDS.libcdio+=	libcdio>=2.3.0
 BUILDLINK_PKGSRCDIR.libcdio?=	../../misc/libcdio
 .endif # LIBCDIO_BUILDLINK3_MK
 
