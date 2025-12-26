8484657ba9c3efbae90c4b00726b84127b46b40a
ktnb <ktnb@pkgsrc.org>
2025-12-26 02:57:44 +0000

    gamb: update to 4.9.7
    bug fixes and minor build system changes

--- lang/gambc/version.mk
+++ lang/gambc/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.6 2024/04/11 15:19:11 ktnb Exp $
+# $NetBSD: version.mk,v 1.7 2025/12/26 02:57:44 ktnb Exp $
 ###
 .if !defined(GAMBC_VERSION_MK)
 GAMBC_VERSION_MK=	# empty
 ###
 ### The package version sets the distribution version.
 ###
-GAMBC_VERSION=		4.9.5
+GAMBC_VERSION=		4.9.7
 GAMBC_DIST_VERSION=	${GAMBC_VERSION:C|^.*|v&|:S|.|_|g}
 .endif
