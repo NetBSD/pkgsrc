222f218fc93508134082a625ec42f912951cb47b
bsiegert <bsiegert@pkgsrc.org>
2025-12-26 10:41:36 +0000

    Pullup ticket #7038 - requested by gutteridge www/firefox140: build fix
    Revisions pulled up:
    - www/firefox140/distinfo                                       1.5
    - www/firefox140/patches/patch-third__party_python_jsonschema_jsonschema_validators.py 1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	gutteridge
       Date:		Wed Dec 24 02:11:49 UTC 2025
    
       Modified Files:
       	pkgsrc/www/firefox140: distinfo
       Added Files:
       	pkgsrc/www/firefox140/patches:
       	    patch-third__party_python_jsonschema_jsonschema_validators.py
    
       Log Message:
       firefox140: fix builds with Python >= 3.14
    
       Build failure reported by Hisashi Todd Fujinaka in PR pkg/59854. A fix
       was applied upstream by Mozilla directly against their "vendored"
       version of jsonschema, but not backported to the ESR branch.

---
+++ www/firefox140/patches/patch-third__party_python_jsonschema_jsonschema_validators.py
@@ -0,0 +1,20 @@
+$NetBSD: patch-third__party_python_jsonschema_jsonschema_validators.py,v 1.1.2.2 2025/12/26 10:41:36 bsiegert Exp $
+
+Fix builds with Python 3.14.
+https://github.com/mozilla-firefox/firefox/commit/b09b27487e5d649612e0baae309608954ff9d542
+
+--- third_party/python/jsonschema/jsonschema/validators.py.orig	2025-12-01 18:15:47.000000000 +0000
++++ third_party/python/jsonschema/jsonschema/validators.py
+@@ -875,8 +875,11 @@ class RefResolver:
+             return None
+         uri, fragment = urldefrag(url)
+         for subschema in subschemas:
++            id = subschema["$id"]
++            if not isinstance(id, str):
++                continue
+             target_uri = self._urljoin_cache(
+-                self.resolution_scope, subschema["$id"],
++                self.resolution_scope, id,
+             )
+             if target_uri.rstrip("/") == uri.rstrip("/"):
+                 if fragment:
