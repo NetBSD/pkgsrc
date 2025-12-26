8bba800947f5153a5ca4f1eb22cfa34a507ea0e5
adam <adam@pkgsrc.org>
2025-12-26 09:23:43 +0000

    timescaledb-tune: updated to 0.18.2
    0.18.2
    
    Restart message
    chore: Bump go version
    Bump go version mentioned in README
    test: add pg 10-18 integration tests and build infra (WIP)

--- databases/timescaledb-tune/go-modules.mk
+++ databases/timescaledb-tune/go-modules.mk
@@ -1,14 +1,15 @@
-# $NetBSD: go-modules.mk,v 1.3 2021/12/10 11:59:34 tnn Exp $
+# $NetBSD: go-modules.mk,v 1.4 2025/12/26 09:23:43 adam Exp $
 
-GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.9.0.mod
-GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.9.0.zip
-GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.mod
-GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.zip
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.11.mod
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.11.zip
-GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.8.mod
-GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20190104145345-974d429e7ae4.mod
-GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20190104145345-974d429e7ae4.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190222072716-a9d3bda3a223.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.zip
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.17.0.mod
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.17.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.13.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.13.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.16.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.20.zip
+GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20210728143218-7b4eea64cf58.mod
+GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20210728143218-7b4eea64cf58.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220811171246-fbc7d0a398ab.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.25.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.25.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.6.0.mod
