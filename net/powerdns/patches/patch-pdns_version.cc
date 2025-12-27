e62d1b71af6ccc241211d3c0795cc26a60058e5f
wiz <wiz@pkgsrc.org>
2025-12-27 19:04:17 +0000

    net/powerdns: Update to version 5.0.2
    Provided by Marcin Gondek in wip.
    
    5.0.2
    Released: 11th of December 2025
    This is release 5.0.2 of the Authoritative Server. It contains bug fixes and a minor feature.
    Please review the Upgrade Notes before upgrading from versions < 5.0.x.
    
    New Features
    allow finer-grained rrset changes through the API
    References: #16589, pull request 16599
    
    Bug Fixes
    m4/pdns_check_libcrypto: fix ecdsa/eddsa includedir (famfo)
    References: #16538, pull request 16603
    add missing catch block for STL exceptions in createForward
    References: #16564, pull request 16566
    REST API: bring back 404 errors
    References: #16059, pull request 16532
    Give backends the ability to perform extra actions during zone rectify
    References: #15894, #16499, pull request 16512
    meson build: restore original binary names
    References: #16401, pull request 16486
    sdist: copy files as files, not as new dirs¶
    References: #16398, pull request 16466

--- net/powerdns/patches/patch-pdns_version.cc
+++ net/powerdns/patches/patch-pdns_version.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-pdns_version.cc,v 1.3 2025/06/30 11:00:50 jperkin Exp $
+$NetBSD: patch-pdns_version.cc,v 1.4 2025/12/27 19:04:17 wiz Exp $
 
 Do not store configure args, as they contain workdir references.
 
---- pdns/version.cc.orig	2025-05-07 09:29:28.000000000 +0000
+--- pdns/version.cc.orig	2025-12-11 09:59:35.000000000 +0100
 +++ pdns/version.cc
-@@ -160,7 +160,7 @@ void showBuildConfiguration()
-   const auto& modules = BackendMakers().getModules();
-   g_log << Logger::Warning << "Loaded modules: " << boost::join(modules, " ") << endl;
+@@ -176,7 +176,7 @@
+   ret << "Loaded modules: " << boost::join(modules, " ") << endl;
  #endif
+ // NOLINTBEGIN(cppcoreguidelines-macro-usage)
 -#ifdef PDNS_CONFIG_ARGS
 +#if 0
  #define double_escape(s) #s
  #define escape_quotes(s) double_escape(s)
-   g_log<<Logger::Warning<<"Configured with: "<<escape_quotes(PDNS_CONFIG_ARGS)<<endl;
+   // NOLINTEND(cppcoreguidelines-macro-usage)
