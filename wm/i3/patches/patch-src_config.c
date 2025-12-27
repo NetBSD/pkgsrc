09b67db4f2defa8193b930fc8feac8a4ad6e6afa
wiz <wiz@pkgsrc.org>
2025-12-27 15:37:49 +0000

    i3: update to 4.25.
    Most changes in this release cycle have been “behind the scenes”,
    without an observable effect for users. For example, we switched
    to clang-format-20, fixed a number of (benign) memory issues,
    fixed a few flaky tests and maintained our GitHub Actions setup,
    e.g. for the shutdown of baltocdn (our old host for nightly builds).

--- wm/i3/patches/patch-src_config.c
+++ wm/i3/patches/patch-src_config.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_config.c,v 1.1 2023/11/06 11:08:52 nia Exp $
+$NetBSD: patch-src_config.c,v 1.2 2025/12/27 15:37:50 wiz Exp $
 
 Include the correct headers for realpath(3), PATH_MAX.
 
 Should help SunOS.
 
---- src/config.c.orig	2023-01-02 08:39:00.000000000 +0000
+--- src/config.c.orig	2025-12-19 07:19:53.000000000 +0000
 +++ src/config.c
 @@ -12,6 +12,8 @@
  
  #include <libgen.h>
- #include <unistd.h>
+ #include <xkbcommon/xkbcommon.h>
 +#include <stdlib.h>
 +#include <limits.h>
  
- #include <xkbcommon/xkbcommon.h>
- 
+ char *current_configpath = NULL;
+ Config config;
