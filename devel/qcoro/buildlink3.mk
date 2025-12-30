29203dde2aa255abd38ab0fdc92e6b461b6f7295
markd <markd@pkgsrc.org>
2025-12-30 02:07:53 +0000

    qcoro: add version 0.12.0
    QCoro provides the tools necessary to make easy use of C++20 coroutines with
    Qt. The cornerstone of the library is `QCoro::Task<T>`, which represents an
    executed coroutine and allows the result of the coroutine to be asynchronously
    awaited by its caller. Additionally, QCoro provides a set of wrappers for
    common Qt types, such as `QTimer`, `QNetworkReply`, `QDBusPendingCall`,
    `QFuture` and others, that allow to `co_await` their asynchronous operations
    directly.

---
+++ devel/qcoro/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/30 02:07:53 markd Exp $
+
+BUILDLINK_TREE+=	qcoro
+
+.if !defined(QCORO_BUILDLINK3_MK)
+QCORO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qcoro+=	qcoro>=0.12.0
+BUILDLINK_PKGSRCDIR.qcoro?=	../../devel/qcoro
+
+.include "../../lang/qt6-qtdeclarative/buildlink3.mk"
+.include "../../net/qt6-qtwebsockets/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# QCORO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qcoro
