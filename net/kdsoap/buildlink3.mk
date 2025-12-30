f113585cb024f28f5c9bc8aba197f10d166da9e1
markd <markd@pkgsrc.org>
2025-12-30 02:18:47 +0000

    kdsoap: update to 2.2.0
    Qt6/KF6 version.

--- net/kdsoap/buildlink3.mk
+++ net/kdsoap/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.17 2025/10/23 20:38:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2025/12/30 02:18:47 markd Exp $
 
 BUILDLINK_TREE+=	kdsoap
 
 .if !defined(KDSOAP_BUILDLINK3_MK)
 KDSOAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kdsoap+=	kdsoap>=1.9.1
-BUILDLINK_ABI_DEPENDS.kdsoap?=	kdsoap>=1.9.1nb19
+BUILDLINK_API_DEPENDS.kdsoap+=	kdsoap>=2.2.0
+BUILDLINK_ABI_DEPENDS.kdsoap?=	kdsoap>=2.2.0
 BUILDLINK_PKGSRCDIR.kdsoap?=	../../net/kdsoap
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+BUILDLINK_FILES.kdsoap+=	bin/kdwsdl2cpp-qt6
+
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# KDSOAP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kdsoap
