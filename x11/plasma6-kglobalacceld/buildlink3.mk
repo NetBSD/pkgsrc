0a9e93dd4273393f5887d8ed8bab8a9971b1fe17
markd <markd@pkgsrc.org>
2025-12-31 03:13:00 +0000

    plasma6-kglobalacceld: add version 6.5.2
    Daemon providing Global Keyboard Shortcut (Accelerator) functionality.

---
+++ x11/plasma6-kglobalacceld/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/31 03:13:00 markd Exp $
+
+BUILDLINK_TREE+=	plasma6-kglobalacceld
+
+.if !defined(PLASMA6_KGLOBALACCELD_BUILDLINK3_MK)
+PLASMA6_KGLOBALACCELD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma6-kglobalacceld+=	plasma6-kglobalacceld>=6.5.2
+BUILDLINK_PKGSRCDIR.plasma6-kglobalacceld?=	../../x11/plasma6-kglobalacceld
+
+.include "../../devel/kf6-kconfig/buildlink3.mk"
+.include "../../devel/kf6-kcoreaddons/buildlink3.mk"
+.include "../../devel/kf6-kcrash/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../devel/kf6-kservice/buildlink3.mk"
+.include "../../sysutils/kf6-kdbusaddons/buildlink3.mk"
+.include "../../x11/kf6-kglobalaccel/buildlink3.mk"
+.include "../../x11/kf6-kjobwidgets/buildlink3.mk"
+.include "../../x11/kf6-kwindowsystem/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.include "../../x11/xcb-util-cursor/buildlink3.mk"
+.endif	# PLASMA6_KGLOBALACCELD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma6-kglobalacceld
