eea77dd2e96a6128be3e66d673f153b778dddb89
markd <markd@pkgsrc.org>
2025-12-31 03:17:11 +0000

    plasma6-knighttime: add version 6.5.2
    The `KNightTime` provides helpers for scheduling the dark-light cycle.
    It can be used to implement features such as adjusting the screen color
    temperature based on time of day, etc.

---
+++ time/plasma6-knighttime/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/31 03:17:11 markd Exp $
+
+BUILDLINK_TREE+=	plasma6-knighttime
+
+.if !defined(PLASMA6_KNIGHTTIME_BUILDLINK3_MK)
+PLASMA6_KNIGHTTIME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma6-knighttime+=	plasma6-knighttime>=6.5.2
+BUILDLINK_PKGSRCDIR.plasma6-knighttime?=	../../time/plasma6-knighttime
+
+.include "../../devel/kf6-kconfig/buildlink3.mk"
+.include "../../devel/kf6-kcoreaddons/buildlink3.mk"
+.include "../../devel/qt6-qttools/buildlink3.mk"
+.include "../../geography/qt6-qtpositioning/buildlink3.mk"
+.include "../../sysutils/kf6-kdbusaddons/buildlink3.mk"
+.include "../../time/kf6-kholidays/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# PLASMA6_KNIGHTTIME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma6-knighttime
