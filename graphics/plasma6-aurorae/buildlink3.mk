2f5a6c0965a549a138bd92fe3d62ecdc7146127f
markd <markd@pkgsrc.org>
2025-12-31 04:01:40 +0000

    plasma6-aurorae: add version 6.5.2
    Aurorae is a themeable window decoration for KWin.
    
    It supports theme files consisting of several SVG files for decoration and
    buttons. Themes can be installed and selected directly in the configuration
    module of KWin decorations.

---
+++ graphics/plasma6-aurorae/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2025/12/31 04:01:40 markd Exp $
+
+BUILDLINK_TREE+=	plasma6-aurorae
+
+.if !defined(PLASMA6_AURORAE_BUILDLINK3_MK)
+PLASMA6_AURORAE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma6-aurorae+=	plasma6-aurorae>=6.5.2
+BUILDLINK_PKGSRCDIR.plasma6-aurorae?=	../../wip/plasma6-aurorae
+
+.endif	# PLASMA6_AURORAE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma6-aurorae
