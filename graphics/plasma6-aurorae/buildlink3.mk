e3eb861c9e93c322402c35c7c6124baa75a0fc24
markd <markd@pkgsrc.org>
2025-12-31 04:08:47 +0000

    plasma6-aurorae: s/wip/graphics/

--- graphics/plasma6-aurorae/buildlink3.mk
+++ graphics/plasma6-aurorae/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/12/31 04:01:40 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2025/12/31 04:08:47 markd Exp $
 
 BUILDLINK_TREE+=	plasma6-aurorae
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	plasma6-aurorae
 PLASMA6_AURORAE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.plasma6-aurorae+=	plasma6-aurorae>=6.5.2
-BUILDLINK_PKGSRCDIR.plasma6-aurorae?=	../../wip/plasma6-aurorae
+BUILDLINK_PKGSRCDIR.plasma6-aurorae?=	../../graphics/plasma6-aurorae
 
 .endif	# PLASMA6_AURORAE_BUILDLINK3_MK
 
