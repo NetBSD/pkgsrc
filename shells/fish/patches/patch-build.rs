eb1e18b16e5eea795c45a37892d8e81bbda6c9bc
ktnb <ktnb@pkgsrc.org>
2025-12-31 00:47:58 +0000

    fish: update to 4.3.2
    Packaging update:
    - requires py-sphinx to make man pages now
    - upstream fixed two patches!
    
    4.3.2:
    Some other bugs
    
    4.3.1:
    Fixes possible crash after expanding an abbreviation
    
    4.3.0:
    Deprecations and removed features
      - fish no longer sets universal variables by default, which
        simplifies configuration. Specifically, the fish_color_*,
        fish_pager_color_* and fish_key_bindings variables are now set in
        the global scope by default.
      - fish_config theme choose now clears only color variables that
        were set by earlier invocations of a fish_config theme choose
        command (which is how fish’s default theme is set).
    
    Scripting improvements
      - New status language command allows showing and modifying
        language settings for fish messages without having to modify
        environment variables.
    
    Interactive improvements
      - When typing immediately after starting fish, the first prompt is
        now rendered correctly.
      - Completion accuracy was improved for file paths containing = or :
      - Prefix-matching completions are now shown even if they don’t
        match the case typed by the user
      - Autosuggestions now also show soft-wrapped portions
    
    New or improved bindings
      - ctrl-w (backward-kill-path-component) also deletes escaped spaces
      - New special input functions backward-path-component,
        forward-path-component and kill-path-component
    
    Improved terminal support
      - Themes can now be made color-theme-aware by including both [light]
        and [dark] sections in the theme file
      - The working directory is now reported on every fresh prompt (via
        OSC 7), fixing scenarios where a child process (like ssh) left
        behind a stale working directory
      - OSC 133 prompt markers now also mark the prompt end, which
        improves shell integration with terminals like iTerm2
      - Operating-system-specific key bindings are now decided based on
        the terminal’s host OS.
      - Focus reporting is enabled unconditionally, not just inside tmux

--- shells/fish/patches/patch-build.rs
+++ shells/fish/patches/patch-build.rs
@@ -1,14 +1,14 @@
-$NetBSD: patch-build.rs,v 1.1 2025/12/13 17:47:21 vins Exp $
+$NetBSD: patch-build.rs,v 1.2 2025/12/31 00:47:58 ktnb Exp $
 
 Introduce illumos support. 
 
---- build.rs.orig	2025-11-13 12:09:28.000000000 +0000
+--- build.rs.orig	2025-12-28 15:54:44.000000000 +0000
 +++ build.rs
-@@ -84,6 +84,7 @@ fn detect_cfgs(target: &mut Target) {
-         ("using_cmake", &|_| option_env!("FISH_CMAKE_BINARY_DIR").is_some()),
-         ("use_prebuilt_docs", &|_| env_var("FISH_USE_PREBUILT_DOCS").is_some_and(|v| v == "TRUE") ),
-         ("cygwin", &detect_cygwin),
+@@ -76,6 +76,7 @@ fn detect_cfgs(target: &mut Target) {
+         ("apple", &(|_| target_os_is_apple())),
+         ("bsd", &(|_| target_os_is_bsd())),
+         ("cygwin", &(|_| target_os_is_cygwin())),
 +        ("illumos", &|_| env::var("CARGO_CFG_TARGET_OS").unwrap() == "illumos"),
-         ("small_main_stack", &has_small_stack),
-         // See if libc supports the thread-safe localeconv_l(3) alternative to localeconv(3).
-         ("localeconv_l", &|target| {
+         ("have_eventfd", &|target| {
+             // FIXME: NetBSD 10 has eventfd, but the libc crate does not expose it.
+             if target_os() == "netbsd" {
