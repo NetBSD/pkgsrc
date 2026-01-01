af26fbbda245ab58b7ee50d3e3d055408712b1e2
rillig <rillig@pkgsrc.org>
2026-01-01 10:02:49 +0000

    textproc/libxml2: remove unknown configure options, update homepage

--- textproc/libxml2/options.mk
+++ textproc/libxml2/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2020/04/12 07:21:04 adam Exp $
+# $NetBSD: options.mk,v 1.4 2026/01/01 10:02:49 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libxml2
-PKG_SUPPORTED_OPTIONS+=	icu inet6
-PKG_SUGGESTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS+=	icu
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,10 +10,4 @@ PKG_SUGGESTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--with-icu
 .else
 CONFIGURE_ARGS+=	--without-icu
-.endif
-
-.if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-ipv6
-.else
-CONFIGURE_ARGS+=	--disable-ipv6
 .endif
