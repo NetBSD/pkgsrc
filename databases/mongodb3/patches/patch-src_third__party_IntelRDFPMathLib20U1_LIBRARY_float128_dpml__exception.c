84f59cd97125aa9ec5fc93414c1375d55111b8d8
ryoon <ryoon@pkgsrc.org>
2026-01-02 13:09:26 +0000

    databases/mongodb3: Fix build under NetBSD 11.99.4
    * raise(3) requires signal.h insatead of sys/signal.h.

--- databases/mongodb3/patches/patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_float128_dpml__exception.c
+++ databases/mongodb3/patches/patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_float128_dpml__exception.c
@@ -1,12 +1,14 @@
-$NetBSD: patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_float128_dpml__exception.c,v 1.1 2019/06/28 16:30:56 gdt Exp $
+$NetBSD: patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_float128_dpml__exception.c,v 1.2 2026/01/02 13:09:26 ryoon Exp $
 
---- src/third_party/IntelRDFPMathLib20U1/LIBRARY/float128/dpml_exception.c.orig	2017-04-20 21:43:42.000000000 +0000
+--- src/third_party/IntelRDFPMathLib20U1/LIBRARY/float128/dpml_exception.c.orig	2020-01-08 16:30:41.000000000 +0000
 +++ src/third_party/IntelRDFPMathLib20U1/LIBRARY/float128/dpml_exception.c
-@@ -131,6 +131,7 @@
+@@ -131,7 +131,8 @@
  #if !defined(DPML_SIGNAL) && !defined(MINIMAL_SILENT_MODE_EXCEPTION_HANDLER) && \
      !defined(wnt)
  
+-#   include <sys/signal.h>
 +#   include <sys/types.h>
- #   include <sys/signal.h>
++#   include <signal.h>
  #   define DPML_SIGNAL(p)	 raise(SIGFPE)
  
+ #else
