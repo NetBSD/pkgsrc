f0ce7719788dd9b458bda6d049e3d430e0c58228
markd <markd@pkgsrc.org>
2026-01-02 03:56:20 +0000

    plasma6-plasma5support: add version 6.5.2
    Support components for porting from KF5/Qt5 to KF6/Qt6

---
+++ devel/plasma6-plasma5support/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1 2026/01/02 03:56:20 markd Exp $
+
+BUILDLINK_TREE+=	plasma6-plasma5support
+
+.if !defined(PLASMA6_PLASMA5SUPPORT_BUILDLINK3_MK)
+PLASMA6_PLASMA5SUPPORT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma6-plasma5support+=	plasma6-plasma5support>=6.5.2
+BUILDLINK_PKGSRCDIR.plasma6-plasma5support?=	../../devel/plasma6-plasma5support
+
+.include "../../devel/kf6-kconfig/buildlink3.mk"
+.include "../../devel/kf6-kcoreaddons/buildlink3.mk"
+.include "../../devel/kf6-ki18n/buildlink3.mk"
+.include "../../devel/kf6-kidletime/buildlink3.mk"
+.include "../../devel/kf6-kio/buildlink3.mk"
+.include "../../devel/kf6-knotifications/buildlink3.mk"
+.include "../../devel/kf6-kservice/buildlink3.mk"
+.include "../../misc/kf6-kunitconversion/buildlink3.mk"
+.include "../../time/kf6-kholidays/buildlink3.mk"
+.include "../../sysutils/kf6-solid/buildlink3.mk"
+.include "../../sysutils/plasma6-libksysguard/buildlink3.mk"
+.include "../../x11/kf6-kguiaddons/buildlink3.mk"
+.include "../../x11/plasma6-plasma-activities/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
+.endif	# PLASMA6_PLASMA5SUPPORT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma6-plasma5support
