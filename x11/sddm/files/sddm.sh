214b6b36b46f7a055607191edb476d1a34b2b8dd
gutteridge <gutteridge@pkgsrc.org>
2026-01-02 22:22:23 +0000

    sddm: apply various functional fixes
    From Stepan Ipatov via pkgsrc-wip:
    
    Fix keyboard responsiveness on startup. "It happens due to the way sddm
    selects the virtual TTY for starting the X server. It calls
    ioctl(fd, VT_OPENQRY, &vt) and gets the first free TTY. The problem is
    that at the stage of system boot when the rc scripts are executed, the
    ioctl() call returns /dev/ttyE1. Apparently, sddm and getty do not
    coexist well on the same TTY. The only workaround I've found so far is
    to delay starting sddm until init has already spawned getty. Once that
    happens, ioctl() returns the expected /dev/ttyE4."
    
    Require dbus during rc.d startup.
    
    Further changes from me:
    
    Fixed typo in the rc.d/sddm script that prevented directories from
    being created.
    
    Fixed typo in PKG_GROUPS_VARS assignment.
    
    Increased parameterization of the rc.d/sddm script. (Some of these
    changes are different from what was last committed to wip.)

--- x11/sddm/files/sddm.sh
+++ x11/sddm/files/sddm.sh
@@ -1,9 +1,9 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: sddm.sh,v 1.1 2025/12/30 04:00:09 gutteridge Exp $
+# $NetBSD: sddm.sh,v 1.2 2026/01/02 22:22:23 gutteridge Exp $
 #
 # PROVIDE: sddm
-# REQUIRE: DAEMON LOGIN wscons
+# REQUIRE: DAEMON LOGIN wscons dbus
 # KEYWORD: shutdown
 
 $_rc_subr_loaded . /etc/rc.subr
@@ -15,16 +15,30 @@ pidfile="@VARBASE@/run/$name.pid"
 command="@PREFIX@/sbin/daemonize"
 command_args="$command_args -p $pidfile"
 command_args="$command_args $procname"
-
 start_precmd=sddm_prestart
+start_cmd=sddm_start
 
 sddm_prestart() {
-	dirs="@VARBASE@/lib/$name"
-	if test ! -d $dir; then
-		/bin/mkdir -p $dir
-		/bin/chmod 0755 $dir
-		/usr/sbin/chown sddm:sddm $dir
+	dir="@VARBASE@/lib/$name"
+	if @TEST@ ! -d $dir; then
+		@MKDIR@ $dir
+		@CHMOD@ 0755 $dir
+		@CHOWN@ @SDDM_USER@:@SDDM_GROUP@ $dir
 	fi
+}
+
+sddm_start()
+{
+	@ECHO@ "Starting ${name}."
+
+	(
+		# Delay starting sddm until init has already spawned getty.
+		for i in $(seq 1 60); do
+			pgrep -f '^/usr/libexec/getty ' >/dev/null && break
+			sleep 1
+		done
+		$command $command_args
+	) &
 }
 
 load_rc_config $name
