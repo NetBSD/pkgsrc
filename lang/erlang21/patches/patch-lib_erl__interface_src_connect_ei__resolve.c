3351714fd62063032e4aa2f8c0a1523d7df6be25
ryoon <ryoon@pkgsrc.org>
2026-01-02 13:23:41 +0000

    lang/erlang21: Fix previous patch

--- lang/erlang21/patches/patch-lib_erl__interface_src_connect_ei__resolve.c
+++ lang/erlang21/patches/patch-lib_erl__interface_src_connect_ei__resolve.c
@@ -1,15 +1,14 @@
-$NetBSD: patch-lib_erl__interface_src_connect_ei__resolve.c,v 1.1 2026/01/02 13:11:02 ryoon Exp $
+$NetBSD: patch-lib_erl__interface_src_connect_ei__resolve.c,v 1.2 2026/01/02 13:23:41 ryoon Exp $
 
 * Port undef HAVE_GETHOSTBYNAME_R from erlang-27 to fix build under NetBSD.
 
---- lib/erl_interface/src/connect/ei_resolve.c.orig	2026-01-02 13:04:03.328523871 +0000
+--- lib/erl_interface/src/connect/ei_resolve.c.orig	2019-04-17 14:14:50.000000000 +0000
 +++ lib/erl_interface/src/connect/ei_resolve.c
-@@ -55,6 +55,11 @@
+@@ -55,6 +55,10 @@
  #include "ei_resolve.h"
  #include "ei_locking.h"
  
-+#if defined(_AIX) || defined(__NetBSD__) || (defined(__ANDROID__) && (__ANDROID_
-+API__ < 23))
++#if defined(_AIX) || defined(__NetBSD__) || (defined(__ANDROID__) && (__ANDROID_API__ < 23))
 +#undef HAVE_GETHOSTBYNAME_R
 +#endif
 +
