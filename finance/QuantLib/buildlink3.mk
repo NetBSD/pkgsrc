52fc562c434e5aca581f30823b4b3cd2d9b8cda6
wiz <wiz@pkgsrc.org>
2026-01-03 11:57:44 +0000

    QuantLib: update to 1.40.
    7 years of changes, fixes build on NetBSD-current.

--- finance/QuantLib/buildlink3.mk
+++ finance/QuantLib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2025/09/27 09:57:16 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/01/03 11:57:44 wiz Exp $
 
 BUILDLINK_TREE+=	QuantLib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	QuantLib
 QUANTLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.QuantLib+=	QuantLib>=1.12.1
-BUILDLINK_ABI_DEPENDS.QuantLib?=		QuantLib>=1.12.1nb16
+BUILDLINK_ABI_DEPENDS.QuantLib?=	QuantLib>=1.12.1nb16
 BUILDLINK_PKGSRCDIR.QuantLib?=		../../finance/QuantLib
 
 .include "../../devel/boost-headers/buildlink3.mk"
