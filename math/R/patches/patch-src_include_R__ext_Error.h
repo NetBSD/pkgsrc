479c9bdb56dd5a7c41e35fe571102806945f88cc
mef <mef@pkgsrc.org>
2026-01-03 09:17:26 +0000

    (math/R) Updated 4.4.2 to 4.4.3, based on wip/R
    The usual commit log was once made on 2025/03/02 as follows:
    ----------------------------
    revision 1.266
    date: 2025-03-02 19:26:38 +0900;  author: mef;  state: Exp;  lines: +2 -3;  commitid: 2z43hv4v7E22OtLF;
    (math/R) Updated 4.4.2 to 4.4.3
    
    CHANGES IN R 4.4.3:
       .....
    ----------------------------
    The problem on above commit may be turned out as one char typo:
    
    -+AC_CONFIG_MACRO_DIR([m4])
    ++AC_CONFIG_MACRO_DIRS([m4])
    
    really sorry

--- math/R/patches/patch-src_include_R__ext_Error.h
+++ math/R/patches/patch-src_include_R__ext_Error.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_include_R__ext_Error.h,v 1.3 2025/03/02 11:38:51 mef Exp $
+$NetBSD: patch-src_include_R__ext_Error.h,v 1.4 2026/01/03 09:17:26 mef Exp $
 
 Fix building with C++.
 
---- src/include/R_ext/Error.h.orig	2024-03-05 16:09:06.557665563 +0000
-+++ src/include/R_ext/Error.h
-@@ -35,7 +35,8 @@ extern "C" {
+--- src/include/R_ext/Error.h.orig	2025-01-19 08:15:01.000000000 +0900
++++ src/include/R_ext/Error.h	2026-01-03 08:54:10.539893482 +0900
+@@ -45,7 +45,8 @@
   * In C11 there is _Noreturn * (or noreturn in header <stdnoreturn.h>).
   */
  #if defined NORET
--#elif (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202301L)
-+#elif (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202301L) || \
+-#elif (defined(__clang__) && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202301L)
++#elif (defined(__clang__) && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202301L) || \
 +      (defined(__cplusplus) && __cplusplus >= 201103L)
  # define NORET [[noreturn]]
  #elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201102L
