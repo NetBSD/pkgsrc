069284963b1928e59049d14748c4c84f0352c3c7
wiz <wiz@pkgsrc.org>
2026-01-04 21:21:00 +0000

    discount: update to 3.0.1.2.
    Changes not found.

--- textproc/discount/patches/patch-main.c
+++ textproc/discount/patches/patch-main.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-main.c,v 1.1 2025/09/29 20:38:04 mrg Exp $
+$NetBSD: patch-main.c,v 1.2 2026/01/04 21:21:00 wiz Exp $
 
 Make function prototypes match for GCC 14.
 
---- main.c.orig	2023-08-23 01:09:27.000000000 -0700
-+++ main.c	2025-09-25 12:07:41.131761165 -0700
-@@ -100,8 +100,9 @@
- }
+--- main.c.orig	2025-09-08 16:51:18.000000000 +0000
++++ main.c
+@@ -107,8 +107,9 @@ char *external_formatter = 0;
+ #define SENDER 1
  
  char *
 -external_codefmt(char *src, int len, char *lang)
 +external_codefmt(const char *src, const int len, void *arg3)
  {
 +    char *lang = arg3;
-     int extra = 0;
-     int i, x;
+     int child_status;
+     int size, bufsize, curr;
      char *res;
