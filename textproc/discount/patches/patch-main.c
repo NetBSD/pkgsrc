8033e3c994355e2d616e74cb4da6e1cbaf5cac9c
wiz <wiz@pkgsrc.org>
2026-01-04 21:56:31 +0000

    discount: revert update, breaks p5-Text-Markdown-Discount

--- textproc/discount/patches/patch-main.c
+++ textproc/discount/patches/patch-main.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-main.c,v 1.2 2026/01/04 21:21:00 wiz Exp $
+$NetBSD: patch-main.c,v 1.3 2026/01/04 21:56:31 wiz Exp $
 
 Make function prototypes match for GCC 14.
 
---- main.c.orig	2025-09-08 16:51:18.000000000 +0000
-+++ main.c
-@@ -107,8 +107,9 @@ char *external_formatter = 0;
- #define SENDER 1
+--- main.c.orig	2023-08-23 01:09:27.000000000 -0700
++++ main.c	2025-09-25 12:07:41.131761165 -0700
+@@ -100,8 +100,9 @@
+ }
  
  char *
 -external_codefmt(char *src, int len, char *lang)
 +external_codefmt(const char *src, const int len, void *arg3)
  {
 +    char *lang = arg3;
-     int child_status;
-     int size, bufsize, curr;
+     int extra = 0;
+     int i, x;
      char *res;
