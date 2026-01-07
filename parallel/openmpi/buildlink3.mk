b186f8aeee82c6888f15675269da72e28eff7e5d
adam <adam@pkgsrc.org>
2026-01-07 18:27:38 +0000

    openmpi: updated to 4.1.8
    Open MPI version 4.1.6
    
    Fix configure issue with XCode 15.
    Update embedded PMIx to 3.2.5. PMIx 3.2.5 addresses CVE-2023-41915.
    
    Note
    
    Prior versions of Open MPI (and their associated PMIx implementations) are not impacted by this CVE, because Open MPI never uses escalated privileges on behalf of an unprivileged user. We are backporting this change both because it is low risk and to avoid alarms from CVE scanners.
    Fix issue with buffered sends and MTL-based interfaces (Libfabric, PSM, Portals).
    Add missing MPI_F_STATUS_SIZE to mpi.h. Thanks to @jprotze for reporting the issue.
    Update Fortran mpi module configure check to be more correct. Thanks to Sergey Kosukhin for identifying the issue and supplying the fix.
    Update to properly handle PMIx v>=4.2.3. Thanks to Bruno Chareyre, Github user @sukanka, and Christof Koehler for raising the compatibility issues and helping test the fixes.
    Fix minor issues and add some minor performance optimizations with OFI support.
    Support the striping_factor and striping_unit MPI_Info names recomended by the MPI standard for parallel IO.
    Fixed some minor issues with UCX support.
    Minor optimization for 0-byte MPI_Alltoallw (i.e., make it a no-op).

--- parallel/openmpi/buildlink3.mk
+++ parallel/openmpi/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.12 2022/09/22 16:47:27 thor Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2026/01/07 18:27:38 adam Exp $
 
 BUILDLINK_TREE+=	openmpi
 
 .if !defined(OPENMPI_BUILDLINK3_MK)
 OPENMPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=1.5.1
+BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=4.1.0
 BUILDLINK_PKGSRCDIR.openmpi?=	../../parallel/openmpi
 
 BUILDLINK_FILES.openmpi+=	lib/*.mod
 
+.include "../../parallel/hwloc/buildlink3.mk"
 .endif # OPENMPI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openmpi
