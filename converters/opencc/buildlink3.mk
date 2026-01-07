3d2407dea7357eb8ef5d65c6614a2963ce5481fe
adam <adam@pkgsrc.org>
2026-01-07 19:32:04 +0000

    opencc: updated to 1.1.9
    1.1.9
    
    Add Bazel Python library and test.
    Fix the build issue of rapidjson.
    Fix the ABI issue.

--- converters/opencc/buildlink3.mk
+++ converters/opencc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/08/14 05:23:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/01/07 19:32:04 adam Exp $
 #
 
 BUILDLINK_TREE+=	opencc
@@ -9,8 +9,10 @@ OPENCC_BUILDLINK3_MK:=
 USE_CXX_FEATURES+=	c++14
 
 BUILDLINK_API_DEPENDS.opencc+=	opencc>=1.1.1
-BUILDLINK_ABI_DEPENDS.opencc?=	opencc>=1.1.3nb1
+BUILDLINK_ABI_DEPENDS.opencc+=	opencc>=1.1.3nb1
 BUILDLINK_PKGSRCDIR.opencc?=	../../converters/opencc
+
+.include "../../textproc/marisa/buildlink3.mk"
 .endif	# OPENCC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-opencc
