7400b6131a438a8c48fa5902199c3405f074439a
wiz <wiz@pkgsrc.org>
2026-01-08 09:27:05 +0000

    py-numpy: update to 2.4.0.
    NumPy 2.4.0 released
    
    20 Dec, 2025 – The NumPy 2.4.0 release continues the work to improve
    free threaded Python support, user dtypes implementation, and
    annotations. There are many expired deprecations and bug fixes as
    well. Highlights are:
    
        Many annotation improvements. In particular, runtime signature introspection.
        New casting kwarg 'same_value' for casting by value.
        New PyUFunc_AddLoopsFromSpec function that can be used to add user sort loops using the ArrayMethod API.
        New __numpy_dtype__ protocol.
    
    This release supports Python versions 3.11-3.14

--- math/py-numpy/patches/patch-numpy_distutils_log.py
+++ math/py-numpy/patches/patch-numpy_distutils_log.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-numpy_distutils_log.py,v 1.1 2022/11/20 19:03:47 adam Exp $
+$NetBSD: patch-numpy_distutils_log.py,v 1.2 2026/01/08 09:27:06 wiz Exp $
 
 Backport distutils.log.Log from setuptools < 65.6.0.
 
---- numpy/distutils/log.py.orig	2022-11-20 17:58:45.000000000 +0000
+--- numpy/distutils/log.py.orig	2025-12-20 07:38:18.000000000 +0000
 +++ numpy/distutils/log.py
-@@ -1,8 +1,8 @@
+@@ -1,8 +1,8 @@ from distutils.log import *  # noqa: F403
  # Colored log
  import sys
  from distutils.log import *  # noqa: F403
@@ -14,7 +14,7 @@ Backport distutils.log.Log from setuptools < 65.6.0.
  
  from numpy.distutils.misc_util import (red_text, default_text, cyan_text,
          green_text, is_sequence, is_string)
-@@ -16,7 +16,48 @@ def _fix_args(args,flag=1):
+@@ -16,10 +16,51 @@ def _fix_args(args,flag=1):
      return args
  
  
@@ -23,12 +23,12 @@ Backport distutils.log.Log from setuptools < 65.6.0.
 +    def __init__(self, threshold=WARN):
 +        self.threshold = threshold
 +
-+    def _log(self, level, msg, args):
+     def _log(self, level, msg, args):
 +        if level not in (DEBUG, INFO, WARN, ERROR, FATAL):
 +            raise ValueError('%s wrong log level' % str(level))
 +
-+        if level >= self.threshold:
-+            if args:
+         if level >= self.threshold:
+             if args:
 +                msg = msg % args
 +            if level in (WARN, ERROR, FATAL):
 +                stream = sys.stderr
@@ -61,10 +61,13 @@ Backport distutils.log.Log from setuptools < 65.6.0.
 +    def fatal(self, msg, *args):
 +        self._log(FATAL, msg, args)
 +
-     def _log(self, level, msg, args):
-         if level >= self.threshold:
-             if args:
-@@ -44,6 +85,7 @@ class Log(old_Log):
++    def _log(self, level, msg, args):
++        if level >= self.threshold:
++            if args:
+                 msg = msg % _fix_args(args)
+             if 0:
+                 if msg.startswith('copying ') and msg.find(' -> ') != -1:
+@@ -44,6 +85,7 @@ _global_log.__class__ = Log
  
  
  _global_log.__class__ = Log
