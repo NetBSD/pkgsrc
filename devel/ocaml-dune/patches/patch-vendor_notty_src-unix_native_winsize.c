9ab497dfd8e1cd63ab9d8d90f6c9a1ed258ef01a
alnsn <alnsn@pkgsrc.org>
2026-01-11 18:09:04 +0000

    Update devel/ocaml-dune to 3.20.2.
    All changes between 3.11.1 and 3.20.2 are listed here:
    https://github.com/ocaml/dune/blob/main/CHANGES.md

--- devel/ocaml-dune/patches/patch-vendor_notty_src-unix_native_winsize.c
+++ devel/ocaml-dune/patches/patch-vendor_notty_src-unix_native_winsize.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-vendor_notty_src-unix_native_winsize.c,v 1.2 2025/07/11 11:55:37 jperkin Exp $
+$NetBSD: patch-vendor_notty_src-unix_native_winsize.c,v 1.3 2026/01/11 18:09:04 alnsn Exp $
 
 SunOS build fix.
 
---- vendor/notty/src-unix/native/winsize.c.orig	2023-10-09 11:08:29.000000000 +0000
+--- vendor/notty/src-unix/native/winsize.c.orig	2026-01-11 01:23:58.628536099 +0000
 +++ vendor/notty/src-unix/native/winsize.c
 @@ -3,6 +3,11 @@
  #ifdef _WIN32
- #include <caml/fail.h>
+ #include <windows.h>
  #else
 +#ifdef __sun
 +#include <sys/termios.h>
