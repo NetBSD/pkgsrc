b87ef7a7015a0f65d398805a1e038ab21e80be69
adam <adam@pkgsrc.org>
2026-01-11 10:28:54 +0000

    py-scikit-learn: updated to 1.8.0
    Version 1.8.0
    
    Changes impacting many modules
    
    - |Efficiency| Improved CPU and memory usage in estimators and metric functions that rely on
      weighted percentiles and better match NumPy and Scipy (un-weighted) implementations
      of percentiles.
    
    Support for Array API
    
    Additional estimators and functions have been updated to include support for all
    `Array API <https://data-apis.org/array-api/latest/>`_ compliant inputs.

--- math/py-scikit-learn/patches/patch-sklearn_preprocessing___target__encoder__fast.pyx
+++ math/py-scikit-learn/patches/patch-sklearn_preprocessing___target__encoder__fast.pyx
@@ -1,12 +1,12 @@
-$NetBSD: patch-sklearn_preprocessing___target__encoder__fast.pyx,v 1.2 2024/08/03 07:21:06 adam Exp $
+$NetBSD: patch-sklearn_preprocessing___target__encoder__fast.pyx,v 1.3 2026/01/11 10:28:55 adam Exp $
 
 Fix build on NetBSD.
 
---- sklearn/preprocessing/_target_encoder_fast.pyx.orig	2024-07-02 17:14:07.000000000 +0000
+--- sklearn/preprocessing/_target_encoder_fast.pyx.orig	2025-12-09 16:13:35.000000000 +0000
 +++ sklearn/preprocessing/_target_encoder_fast.pyx
 @@ -1,4 +1,4 @@
 -from libc.math cimport isnan
 +from libcpp.cmath cimport isnan
  from libcpp.vector cimport vector
  
- from ..utils._typedefs cimport float32_t, float64_t, int32_t, int64_t
+ from sklearn.utils._typedefs cimport float32_t, float64_t, int32_t, int64_t
