dbaabb237e5f5c13a62fe6354216278933ee5045
adam <adam@pkgsrc.org>
2026-01-11 08:15:05 +0000

    py-scipy: updated to 1.17.0
    SciPy 1.17.0 is the culmination of 6 months of hard work. It contains
    many new features, numerous bug-fixes, improved test coverage and better
    documentation. There have been a number of deprecations and API changes
    in this release, which are documented below. All users are encouraged to
    upgrade to this release, as there are a large number of bug-fixes and
    optimizations. Before upgrading, we recommend that users check that
    their own code does not use deprecated SciPy functionality (to do so,
    run your code with ``python -Wd`` and check for ``DeprecationWarning`` s).
    Our development attention will now shift to bug-fix releases on the
    1.17.x branch, and on adding new features on the main branch.

--- math/py-scipy/patches/patch-scipy_special_boost__special__functions.h
+++ math/py-scipy/patches/patch-scipy_special_boost__special__functions.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-scipy_special_boost__special__functions.h,v 1.2 2025/01/05 08:58:47 adam Exp $
+$NetBSD: patch-scipy_special_boost__special__functions.h,v 1.3 2026/01/11 08:15:05 adam Exp $
 
 Fix build.
 
---- scipy/special/boost_special_functions.h.orig	2025-01-02 22:53:29.000000000 +0000
+--- scipy/special/boost_special_functions.h.orig	2026-01-10 17:57:13.000000000 +0000
 +++ scipy/special/boost_special_functions.h
-@@ -5,6 +5,7 @@
- #include <stdexcept>
- #include "sf_error.h"
+@@ -47,6 +47,7 @@ boost::math::policies::user_evaluation_e
+     return val;
+ }
  
 +using std::isnan;
  
- #include "boost/math/special_functions/beta.hpp"
- #include "boost/math/special_functions/erf.hpp"
+ template <class RealType>
+ RealType
