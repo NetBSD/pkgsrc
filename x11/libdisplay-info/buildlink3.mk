dbecfd626660e616b119679c28690d6ab7ce0895
gutteridge <gutteridge@pkgsrc.org>
2026-01-12 22:20:29 +0000

    Bump PKGREVISIONs for libdisplay-info SO major change

--- x11/libdisplay-info/buildlink3.mk
+++ x11/libdisplay-info/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2024/12/26 18:37:07 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/01/12 22:20:29 gutteridge Exp $
 
 BUILDLINK_TREE+=	libdisplay-info
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libdisplay-info
 LIBDISPLAY_INFO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdisplay-info+=	libdisplay-info>=0.2.0
+BUILDLINK_ABI_DEPENDS.libdisplay-info?=	libdisplay-info>=0.3.0
 BUILDLINK_PKGSRCDIR.libdisplay-info?=	../../x11/libdisplay-info
 
 .endif	# LIBDISPLAY_INFO_BUILDLINK3_MK
