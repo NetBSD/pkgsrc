677d9cb13fcada5a9a5058092eb95305ad70f66b
adam <adam@pkgsrc.org>
2026-01-13 11:57:54 +0000

    opencolorio: updated to 2.5.1
    2.5.1
    
    This is an ABI-compatible update for the 2.5.x series that includes all fixes since the 2.5.0 release.
    
    The two most notable changes are:
    
    The output of pure-gamma TRC curves in ICC profiles is no longer clamped.
    The texture binding indices for Vulkan have been reworked

--- graphics/opencolorio/patches/patch-share_cmake_modules_Findyaml-cpp.cmake
+++ graphics/opencolorio/patches/patch-share_cmake_modules_Findyaml-cpp.cmake
@@ -1,15 +1,15 @@
-$NetBSD: patch-share_cmake_modules_Findyaml-cpp.cmake,v 1.2 2024/06/26 16:20:55 adam Exp $
+$NetBSD: patch-share_cmake_modules_Findyaml-cpp.cmake,v 1.3 2026/01/13 11:57:54 adam Exp $
 
-Fix yaml-cpp-0.8.8 detection.
+Fix yaml-cpp-0.8.0 detection.
 
---- share/cmake/modules/Findyaml-cpp.cmake.orig	2024-01-30 07:11:35.000000000 +0000
+--- share/cmake/modules/Findyaml-cpp.cmake.orig	2026-01-13 03:55:53.000000000 +0000
 +++ share/cmake/modules/Findyaml-cpp.cmake
-@@ -49,7 +49,7 @@ if(NOT OCIO_INSTALL_EXT_PACKAGES STREQUA
+@@ -44,7 +44,7 @@ if(NOT OCIO_INSTALL_EXT_PACKAGES STREQUA
+     set(_yaml-cpp_REQUIRED_VARS yaml-cpp_LIBRARY)
  
      # Search for yaml-cpp-config.cmake
-     if(NOT DEFINED yaml-cpp_ROOT)
--        find_package(yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG ${quiet})
-+        find_package(yaml-cpp::yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG ${quiet})
-     endif()
+-    find_package(yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG ${quiet})
++    find_package(yaml-cpp::yaml-cpp ${yaml-cpp_FIND_VERSION} CONFIG ${quiet})
  
      if(yaml-cpp_FOUND)
+         # Alias target for yaml-cpp < 0.8 compatibility
