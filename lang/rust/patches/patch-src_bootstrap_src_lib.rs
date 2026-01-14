0e97142d90b1ce91f7fa16e771098f41d8c3b717
wiz <wiz@pkgsrc.org>
2026-01-14 06:44:31 +0000

    irust: restore v1.8 of patch-src_bootstrap_src_lib.rs
    Requested by tnn.

--- lang/rust/patches/patch-src_bootstrap_src_lib.rs
+++ lang/rust/patches/patch-src_bootstrap_src_lib.rs
@@ -1,11 +1,12 @@
-$NetBSD: patch-src_bootstrap_src_lib.rs,v 1.9 2026/01/13 20:48:23 wiz Exp $
+$NetBSD: patch-src_bootstrap_src_lib.rs,v 1.10 2026/01/14 06:44:31 wiz Exp $
 
 Don't filter out optimization flags.
-FreeBSD has a particular C++ runtime library name
+FreeBSD has a particular C++ runtime library name.
+Don't assume files in ${PREFIX}/lib are readable by the building user.
 
---- src/bootstrap/src/lib.rs.orig	2024-01-06 20:52:50.892941220 +0000
+--- src/bootstrap/src/lib.rs.orig	2025-09-14 15:05:11.000000000 +0000
 +++ src/bootstrap/src/lib.rs
-@@ -1209,7 +1209,6 @@ Executed at: {executed_at}"#,
+@@ -1256,7 +1256,6 @@ impl Build {
          base.args()
              .iter()
              .map(|s| s.to_string_lossy().into_owned())
@@ -13,7 +14,7 @@ FreeBSD has a particular C++ runtime library name
              .collect::<Vec<String>>()
      }
  
-@@ -1225,7 +1224,8 @@ Executed at: {executed_at}"#,
+@@ -1272,7 +1271,8 @@ impl Build {
          // If we're compiling C++ on macOS then we add a flag indicating that
          // we want libc++ (more filled out than libstdc++), ensuring that
          // LLVM/etc are all properly compiled.
@@ -22,4 +23,18 @@ FreeBSD has a particular C++ runtime library name
 +            (target.contains("apple-darwin") || target.contains("freebsd")) {
              base.push("-stdlib=libc++".into());
          }
+ 
+@@ -1813,7 +1813,12 @@ impl Build {
+             // but if that fails just fall back to a slow `copy` operation.
+         } else {
+             if let Err(e) = fs::copy(&src, dst) {
+-                panic!("failed to copy `{}` to `{}`: {}", src.display(), dst.display(), e)
++                if e.kind() == io::ErrorKind::PermissionDenied {
++                    eprintln!("Skipping copy of `{}` to `{}`: {}", src.display(), dst.display(), e);
++                    return;
++                } else {
++                    panic!("failed to copy `{}` to `{}`: {}", src.display(), dst.display(), e)
++                }
+             }
+             t!(fs::set_permissions(dst, metadata.permissions()));
  
