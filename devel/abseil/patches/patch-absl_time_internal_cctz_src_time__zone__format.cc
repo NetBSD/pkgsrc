4ed11dbb4e8b479c21ba4ddb7ec29ececb1fc0ce
wiz <wiz@pkgsrc.org>
2026-01-14 12:56:47 +0000

    abseil: update to 20260107.0.
    What's New
    
        absl::StringResizeAndOverwrite(): A new function in
        absl/strings/resize_and_overwrite.h that acts as a polyfill
        for C++23's std::basic_string::resize_and_overwrite. This allows
        for efficient resizing and in-place initialization of strings,
        avoiding the overhead of default initialization, which is
        particularly useful when working with C-style APIs that write
        directly to a buffer. See #1136 (comment) for performance notes.
    
        absl::chunked_queue: A new container in absl/container/chunked_queue.h
        optimized for use as a FIFO (First-In, First-Out) queue.
    
        absl::linked_hash_map and absl::linked_hash_set: New hash
        containers that maintain iteration order matching the insertion
        order. These are available in absl/container/linked_hash_map.h
        and absl/container/linked_hash_set.h.
    
        absl::down_cast: Added to absl/base/casts.h. This function
        provides a safer way to perform downcasts in a polymorphic type
        hierarchy. In debug builds (or when hardened asserts are
        enabled), it uses dynamic_cast to verify the validity of the
        cast and terminates the program if the cast is invalid. In
        optimized builds, it uses static_cast for performance.
    
    Breaking Changes
    
        Mutex Deprecations: Legacy Mutex methods and MutexLock pointer
        constructors have been marked as deprecated. Users should
        migrate to the recommended APIs.
    
        The polyfill type absl::string_view is now an alias for
        std::string_view in all builds. (9ebd93a)

--- devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
+++ devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
@@ -1,13 +1,13 @@
-$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.4 2024/01/30 18:22:53 adam Exp $
+$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.5 2026/01/14 12:56:47 wiz Exp $
 
 Avoid _XOPEN_SOURCE.
 
---- absl/time/internal/cctz/src/time_zone_format.cc.orig	2024-01-22 17:59:25.000000000 +0000
+--- absl/time/internal/cctz/src/time_zone_format.cc.orig	2026-01-07 19:26:27.000000000 +0000
 +++ absl/time/internal/cctz/src/time_zone_format.cc
-@@ -19,7 +19,7 @@
+@@ -21,7 +21,7 @@
  #endif
  
- #if defined(HAS_STRPTIME) && HAS_STRPTIME
+ #if HAS_STRPTIME
 -#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
 +#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
  #define _XOPEN_SOURCE 500  // Exposes definitions for SUSv2 (UNIX 98).
