41259e0d0312a2d85dcda452d70f1e964c34883b
adam <adam@pkgsrc.org>
2026-01-19 08:32:03 +0000

    py-fido2: a better patch

--- security/py-fido2/patches/patch-fido2_hid_____init____.py
+++ security/py-fido2/patches/patch-fido2_hid_____init____.py
@@ -1,15 +1,22 @@
-$NetBSD: patch-fido2_hid_____init____.py,v 1.3 2026/01/19 08:17:33 adam Exp $
+$NetBSD: patch-fido2_hid_____init____.py,v 1.4 2026/01/19 08:32:04 adam Exp $
 
-Properly detect NetBSD. See docs for sys.platform.
+Properly detect platforms.
+https://github.com/Yubico/python-fido2/pull/276
 
 --- fido2/hid/__init__.py.orig	1970-01-01 00:00:00.000000000 +0000
 +++ fido2/hid/__init__.py
-@@ -50,7 +50,7 @@ elif sys.platform == "darwin":
+@@ -48,11 +48,11 @@ elif sys.platform == "win32":
+     from . import windows as backend
+ elif sys.platform == "darwin":
      from . import macos as backend
- elif sys.platform == "freebsd":
+-elif sys.platform == "freebsd":
++elif sys.platform.startswith("freebsd"):
      from . import freebsd as backend
 -elif sys.platform == "netbsd":
 +elif sys.platform.startswith("netbsd"):
      from . import netbsd as backend
- elif sys.platform == "openbsd":
+-elif sys.platform == "openbsd":
++elif sys.platformstartswith("openbsd"):
      from . import openbsd as backend
+ else:
+     raise Exception("Unsupported platform")
