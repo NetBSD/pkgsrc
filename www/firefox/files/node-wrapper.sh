154558afe97ddaf8f52a90d27e4621c2a38ff9ed
ryoon <ryoon@pkgsrc.org>
2026-01-19 12:06:03 +0000

    www/firefox: Update to 147.0.1
    Changelog:
    147.0.1:
    Fixed
    
      * Fixed compatibility problems with websites that use the new Compression
        Dictionaries technology, such as ChatGPT, by temporarily disabling the
        feature. (Bug 2010712)
    
      * Fixed an issue where an unnecessary empty directory was created on Linux
        systems. (Bug 2001887)
    
      * Fixed an issue where time formats could cause certain websites to display
        incorrectly. (Bug 2010411)
    
    
    147.0:
    New
    
      * WebGPU support is now enabled for devices with Apple Silicon processors on
        all supported macOS versions.
    
      * Improved video playback performance on systems with AMD GPUs by enabling
        zero-copy playback for hardware-decoded video where supported, bringing
        them to parity with Intel and NVIDIA GPUs.
    
      * Firefox now supports the Safe Browsing V5 protocol and is migrating from
        Safe Browsing V4 to the local list mode of Safe Browsing V5 protocol.
    
      * Users with Enhanced Tracking Protection (ETP) set to Strict will have local
        network access restrictions enabled by default. Firefox will now require
        users to explicitly allow public websites to access local network
        resources.
    
      * Firefox now supports the Freedesktop.org XDG Base Directory Specification.
    
      * A Picture-in-Picture player window can now optionally be opened
        automatically for a video playing in a tab when that tab goes into the
        background. A special thanks to volunteer Daniele Ferla for contributing
        this feature!
    
    Fixed
    
      * Fixed an issue that prevented some Windows users from selecting a tab when
        the cursor was at the top of the screen and the Firefox window was
        maximized.
    
      * Fixed a bug that made HTTP/3 requests containing non-UTF-8 header values
        time out or fall back to HTTP/2 after a while.
    
      * A draggable button can now be dragged if initiated from the button itself.
    
      * For Linux GNOME Mutter users, window and rendering surface sizes were
        updated to match the actual pixel grid so Firefox delivers sharp rendering
        on fractionally scaled displays regardless of the actual window size.
    
      * Various security fixes.
    
    
    Mozilla Foundation Security Advisory 2026-01
    #CVE-2026-0877: Mitigation bypass in the DOM: Security component
    #CVE-2026-0878: Sandbox escape due to incorrect boundary conditions in the
     Graphics: CanvasWebGL component
    #CVE-2026-0879: Sandbox escape due to incorrect boundary conditions in the
     Graphics component
    #CVE-2026-0880: Sandbox escape due to integer overflow in the Graphics
     component
    #CVE-2026-0881: Sandbox escape in the Messaging System component
    #CVE-2026-0882: Use-after-free in the IPC component
    #CVE-2026-0883: Information disclosure in the Networking component
    #CVE-2026-0884: Use-after-free in the JavaScript Engine component
    #CVE-2026-0885: Use-after-free in the JavaScript: GC component
    #CVE-2026-0886: Incorrect boundary conditions in the Graphics component
    #CVE-2026-0887: Clickjacking issue, information disclosure in the PDF Viewer
     component
    #CVE-2026-0888: Information disclosure in the XML component
    #CVE-2026-0889: Denial-of-service in the DOM: Service Workers component
    #CVE-2026-0890: Spoofing issue in the DOM: Copy & Paste and Drag & Drop
     component
    #CVE-2026-0891: Memory safety bugs fixed in Firefox ESR 140.7, Thunderbird ESR
     140.7, Firefox 147 and Thunderbird 147
    #CVE-2026-0892: Memory safety bugs fixed in Firefox 147 and Thunderbird 147

--- www/firefox/files/node-wrapper.sh
+++ www/firefox/files/node-wrapper.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-VERS=v25.2.1
+VERS=v25.3.0
 
 if [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
 	printf "${VERS}\n"
--- www/firefox/patches/patch-media_ffvpx_libavutil_arm_bswap.h
+++ www/firefox/patches/patch-media_ffvpx_libavutil_arm_bswap.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-media_ffvpx_libavutil_arm_bswap.h,v 1.3 2026/01/01 16:14:38 tnn Exp $
+$NetBSD: patch-media_ffvpx_libavutil_arm_bswap.h,v 1.4 2026/01/19 12:06:04 ryoon Exp $
 
 avutil/arm/bswap.h is preferred over the system's <arm/bswap.h> due to
 Firefox's include path order. This breaks <stdlib.h> and others, so
 make sure to pull in the system header when this happens.
 
---- media/ffvpx/libavutil/arm/bswap.h.orig	2025-12-17 16:38:49.000000000 +0000
+--- media/ffvpx/libavutil/arm/bswap.h.orig	2025-12-31 13:17:28.000000000 +0000
 +++ media/ffvpx/libavutil/arm/bswap.h
 @@ -16,6 +16,10 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -26,10 +26,11 @@ make sure to pull in the system header when this happens.
  #ifdef __ARMCC_VERSION
  
  #if HAVE_ARMV6
-@@ -47,4 +53,6 @@ static av_always_inline av_const unsigne
+@@ -46,5 +52,7 @@ static av_always_inline av_const unsigned av_bswap16(u
+ }
  #endif
  #endif /* __ARMCC_VERSION */
- 
-+#endif /* __aarch64__ */
 +
++#endif /* __aarch64__ */
+ 
  #endif /* AVUTIL_ARM_BSWAP_H */
