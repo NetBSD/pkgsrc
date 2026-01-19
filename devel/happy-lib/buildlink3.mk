4c73a622f107806e381e2f02f8894ce8715c33cb
wiz <wiz@pkgsrc.org>
2026-01-19 11:56:21 +0000

    devel/happy-lib: update to happy-lib-2.2
    ## 2.2
    
    * Error out when `<$>` appears in semantic action code
      ([#335](https://github.com/haskell/happy/issues/335)).

--- devel/happy-lib/buildlink3.mk
+++ devel/happy-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2025/08/20 19:22:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2026/01/19 11:56:21 wiz Exp $
 
 BUILDLINK_TREE+=	happy-lib
 
 .if !defined(HAPPY_LIB_BUILDLINK3_MK)
 HAPPY_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.1.7
-BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.1.7
+BUILDLINK_API_DEPENDS.happy-lib+=	happy-lib>=2.2
+BUILDLINK_ABI_DEPENDS.happy-lib+=	happy-lib>=2.2
 BUILDLINK_PKGSRCDIR.happy-lib?=		../../devel/happy-lib
 .endif	# HAPPY_LIB_BUILDLINK3_MK
 
