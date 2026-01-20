dcb7b431c0a190b0a865058c045921822c388250
wiz <wiz@pkgsrc.org>
2026-01-20 08:27:51 +0000

    macro: fix build on NetBSD-current

--- wm/marco/buildlink3.mk
+++ wm/marco/buildlink3.mk
@@ -1,29 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.30 2025/10/23 20:39:41 wiz Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.31 2026/01/20 08:27:51 wiz Exp $
 
 BUILDLINK_TREE+=	marco
 
 .if !defined(MARCO_BUILDLINK3_MK)
 MARCO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.marco+=	marco>=1.8.2
-BUILDLINK_ABI_DEPENDS.marco+=	marco>=1.26.2nb12
+BUILDLINK_API_DEPENDS.marco+=	marco>=1.26.2nb12
 BUILDLINK_PKGSRCDIR.marco?=	../../wm/marco
 
 .include "../../audio/libcanberra/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/libgtop/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/startup-notification/buildlink3.mk"
-.include "../../x11/libXcomposite/buildlink3.mk"
-.include "../../x11/libXrender/buildlink3.mk"
-.include "../../x11/libXcursor/buildlink3.mk"
-.include "../../x11/libSM/buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/libXinerama/buildlink3.mk"
-.include "../../x11/libXfixes/buildlink3.mk"
-.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif	# MARCO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-marco
