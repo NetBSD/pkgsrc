a6c09335e7a398a0032232c685a0b40f9c8f1989
markd <markd@pkgsrc.org>
2026-01-20 09:49:27 +0000

    dolphin: fix buildlink3.mk for new version

--- sysutils/dolphin/buildlink3.mk
+++ sysutils/dolphin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2026/01/07 08:48:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2026/01/20 09:49:27 markd Exp $
 
 BUILDLINK_TREE+=	dolphin
 
@@ -6,10 +6,10 @@ BUILDLINK_TREE+=	dolphin
 DOLPHIN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.dolphin+=	dolphin>=16.04.0
-BUILDLINK_ABI_DEPENDS.dolphin?=	dolphin>=25.08.3nb12
+BUILDLINK_ABI_DEPENDS.dolphin?=	dolphin>=25.08.3
 BUILDLINK_PKGSRCDIR.dolphin?=	../../sysutils/dolphin
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# DOLPHIN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-dolphin
