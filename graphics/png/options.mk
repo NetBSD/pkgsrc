2555bc686d522ca5015e1757870884e229dce812
tnn <tnn@pkgsrc.org>
2026-01-21 09:14:43 +0000

    png: update apng patch for current release

--- graphics/png/options.mk
+++ graphics/png/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2025/05/08 10:39:45 tnn Exp $
+# $NetBSD: options.mk,v 1.3 2026/01/21 09:14:43 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.png
 PKG_SUPPORTED_OPTIONS=	apng
@@ -7,9 +7,9 @@ PKG_SUGGESTED_OPTIONS=	# do not suggest apng without consulting tech-pkg@
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mapng) || make(distinfo) || make(mdi)
-APNGPATCHVERSION=		20250220
+APNGPATCHVERSION=		20260120
 APNGPATCH=			apng-${APNGPATCHVERSION}.patch
 PATCHFILES+=			${APNGPATCH}
-SITES.${APNGPATCH}=		-https://hg-edge.mozilla.org/mozilla-central/raw-file/1a06aa3c2aab3bdd600869ffa8831c56093c699f/media/libpng/apng.patch
+SITES.${APNGPATCH}=		-https://hg-edge.mozilla.org/mozilla-central/raw-file/f0b42fcd1bcab924abe3b603e759d7bd9fb8ee97/media/libpng/apng.patch
 PATCH_DIST_STRIP.${APNGPATCH}=	-p1
 .endif
