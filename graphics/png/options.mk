24e3184586ffdf9d5ba8aefb666ed8658506f119
tnn <tnn@pkgsrc.org>
2026-01-21 09:18:19 +0000

    png: reference apng commit id instead of the tip. NFC

--- graphics/png/options.mk
+++ graphics/png/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2026/01/21 09:14:43 tnn Exp $
+# $NetBSD: options.mk,v 1.4 2026/01/21 09:18:19 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.png
 PKG_SUPPORTED_OPTIONS=	apng
@@ -7,9 +7,9 @@ PKG_SUGGESTED_OPTIONS=	# do not suggest apng without consulting tech-pkg@
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mapng) || make(distinfo) || make(mdi)
-APNGPATCHVERSION=		20260120
+APNGPATCHVERSION=		20260116
 APNGPATCH=			apng-${APNGPATCHVERSION}.patch
 PATCHFILES+=			${APNGPATCH}
-SITES.${APNGPATCH}=		-https://hg-edge.mozilla.org/mozilla-central/raw-file/f0b42fcd1bcab924abe3b603e759d7bd9fb8ee97/media/libpng/apng.patch
+SITES.${APNGPATCH}=		-https://hg-edge.mozilla.org/mozilla-central/raw-file/d19a323e9b878d874ee71f46f4277c68e69b7822/media/libpng/apng.patch
 PATCH_DIST_STRIP.${APNGPATCH}=	-p1
 .endif
