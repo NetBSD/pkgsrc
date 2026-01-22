8e0ba00cfbe566c0950fb620556d1cc112e2e173
wiz <wiz@pkgsrc.org>
2026-01-22 22:46:35 +0000

    mbedtls4: fix bl3.mk define

--- security/mbedtls4/buildlink3.mk
+++ security/mbedtls4/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2026/01/22 19:29:11 js Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/01/22 22:46:35 wiz Exp $
 
 BUILDLINK_TREE+=	mbedtls4
 
-.if !defined(MBEDTLS3_BUILDLINK3_MK)
-MBEDTLS3_BUILDLINK3_MK:=
+.if !defined(MBEDTLS4_BUILDLINK3_MK)
+MBEDTLS4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mbedtls4+=	mbedtls4>=4.0.0
 BUILDLINK_ABI_DEPENDS.mbedtls4?=	mbedtls4>=4.0.0
 BUILDLINK_PKGSRCDIR.mbedtls4?=		../../security/mbedtls4
 
-.endif	# MBEDTLS3_BUILDLINK3_MK
+.endif	# MBEDTLS4_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mbedtls4
