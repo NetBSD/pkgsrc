c42d6c9141234fc6d7d25b9d2c4906aae80bd703
adam <adam@pkgsrc.org>
2026-01-23 16:04:03 +0000

    cmake cmake-gui: updated to 4.2.2
    CMake 4.2.2
    * cmProcess: explicitly track the StopTimeout
    * gitlab-ci: get the sccache server port from the runner
    * cmProcess: compute the timeout when needed
    * Tests/CTestTimeoutAfterMatch: add case for stop time bug
    * ci: Restore support for Windows 7 in official precompiled binaries
    * gitattributes: Do not export ast-grep infrastructure in source archives
    * StdIo: Restore compilation on 32-bit MinGW
    * Utilities: Select bundled or external dependencies very early
    * Linux: Do not force 64-bit `time_t` on 32-bit archs with system libarchive
    * Help: Add 4.1 release note about VS link flag suppression
    * VS: Restore subsystem link flag for DLLs
    * cmake: Fix SARIF diagnostics output path encoding on Windows
    * ci: update macOS jobs to use Xcode 26.2
    * VS: Restore unicode character set for custom targrets on WindowsStore
    * ci: Fix CUDA tests on GPUs newer than the toolkit supports
    * ci: Clamp CUDA 'native' architecture in NVHPC job
    * Tests: Fix RunCMake.include_external_msproject .sln check failure
    * VS: Add include_external_msproject detection of `.njsproj` type
    * VS: Add include_external_msproject GUIDs for more project types
    * VS: Fix SLNX generation so .NET Core projects build in the IDE
    * file(GET_RUNTIME_DEPENDENCIES): Support VS 2026 without VS 2022
    * Help: Fix minor grammar problems
    * Help: Restore correct sort order for property and variable manuals
    * Help: Correct and expand AppImage CPack generator docs
    * fileapi: Handle unused imported libraries with missing IMPORTED_IMPLIB
    * FASTBuild: set PlatformToolset in the generated .bff file
    * FASTBuild: fix configure for non-English MSVC
    * CMake: Restore support for compiling as C++11 on Windows
    * fileapi: Fix crash on cmake_file_api() call in initial cache
    * instrumentation: Fix crash on cmake_instrumentation() call in initial cache
    * CPack/AppImage: Add support for a custom AppRun file
    * GenEx: Partially restore pre-CMP0199 behavior of $<CONFIG>
    * Tutorial: Fix path to file in exercise solution
    * Tutorial: Provide MSVC example to build Vendor library
    * FindBLAS: Make find_package(nvpl) quiet

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.81 2025/12/21 19:24:08 adam Exp $
+# $NetBSD: version.mk,v 1.82 2026/01/23 16:04:03 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.2.1
+CMAKE_VERSION=	4.2.2
 CMAKE_API=	${CMAKE_VERSION:R}
