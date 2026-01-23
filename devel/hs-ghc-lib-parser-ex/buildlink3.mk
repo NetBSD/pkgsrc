f15ea1eebbc6d487457d110df13893514b1ee0f4
wiz <wiz@pkgsrc.org>
2026-01-23 21:42:01 +0000

    devel/hs-ghc-lib-parser-ex: update to ghc-lib-parser-ex-9.14.2.0
    # Unreleased
    - Add support for ghc-9.14 series: `GHCLIB_API_914`
    
    # 9.12.0.0 released
    - Add support for ghc-9.12 series: `GHCLIB_API_912`

--- devel/hs-ghc-lib-parser-ex/buildlink3.mk
+++ devel/hs-ghc-lib-parser-ex/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2025/08/13 11:06:41 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2026/01/23 21:42:01 wiz Exp $
 
 BUILDLINK_TREE+=	hs-ghc-lib-parser-ex
 
 .if !defined(HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK)
 HS_GHC_LIB_PARSER_EX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.10.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.10.0.0nb3
+BUILDLINK_API_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.14.2
+BUILDLINK_ABI_DEPENDS.hs-ghc-lib-parser-ex+=	hs-ghc-lib-parser-ex>=9.14.2.0
 BUILDLINK_PKGSRCDIR.hs-ghc-lib-parser-ex?=	../../devel/hs-ghc-lib-parser-ex
 
 .include "../../devel/hs-ghc-lib-parser/buildlink3.mk"
