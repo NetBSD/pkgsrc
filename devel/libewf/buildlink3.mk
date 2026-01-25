63555db39aa5614710e6b0e28c006f3d786378d9
gutteridge <gutteridge@pkgsrc.org>
2026-01-25 00:59:15 +0000

    libewf & sleuthkit: propagate openssl >= 3.0 dependency

--- devel/libewf/buildlink3.mk
+++ devel/libewf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/06/10 20:37:57 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/01/25 00:59:15 gutteridge Exp $
 
 BUILDLINK_TREE+=	libewf
 
@@ -7,6 +7,9 @@ LIBEWF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libewf+=	libewf>=20150126
 BUILDLINK_PKGSRCDIR.libewf?=	../../devel/libewf
+
+BUILDLINK_API_DEPENDS.openssl+= openssl>=3.0
+.include "../../security/openssl/buildlink3.mk"
 
 .endif	# LIBEWF_BUILDLINK3_MK
 
