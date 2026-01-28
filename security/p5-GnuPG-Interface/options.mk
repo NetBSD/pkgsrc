11101bfbac2124a66a9f7e1099b48f2f2d50f320
wiz <wiz@pkgsrc.org>
2026-01-28 10:52:27 +0000

    p5-GnuPG-Interface: adapt for gpg binary name change
    Bump PKGREVISION.

--- security/p5-GnuPG-Interface/options.mk
+++ security/p5-GnuPG-Interface/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2021/04/27 12:13:11 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2026/01/28 10:52:27 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.p5_GnuPG_Interface
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -8,21 +8,21 @@ PKG_SUGGESTED_OPTIONS=		gnupg2
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnupg2)
-DEPENDS+=		gnupg2>=2.2:../../security/gnupg2
+DEPENDS+=		gnupg2>=2.5:../../security/gnupg2
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnupg1)
+DEPENDS+=		gnupg>=1.4.23nb28:../../security/gnupg
 
 SUBST_CLASSES+=		gpg
-SUBST_SED.gpg+=		-e "s,'gpg','gpg2',g"
+SUBST_SED.gpg+=		-e "s,'gpg','gpg1',g"
 SUBST_FILES.gpg+=	lib/GnuPG/Interface.pm
 SUBST_STAGE.gpg=	pre-configure
 SUBST_MESSAGE.gpg=	Fixing path to gnupg binary.
 
 SUBST_CLASSES+=		gpg2
-SUBST_SED.gpg2+=	-e "s,gpg,gpg2,g"
+SUBST_SED.gpg2+=	-e "s,gpg,gpg1,g"
 SUBST_FILES.gpg2+=	Makefile.PL
 SUBST_STAGE.gpg2=	pre-configure
 SUBST_MESSAGE.gpg2=	Fixing path to gnupg binary.
-.endif
-
-.if !empty(PKG_OPTIONS:Mgnupg1)
-DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
 .endif
