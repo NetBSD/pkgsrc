8590d85feb31c2ac19553e62da8709230604f250
mef <mef@pkgsrc.org>
2026-01-29 13:10:57 +0000

    (converters/orcus) Update buildlink3.mk for API update, tks Marc Baudoin

--- converters/orcus/buildlink3.mk
+++ converters/orcus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2025/08/30 05:16:00 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2026/01/29 13:10:57 mef Exp $
 
 BUILDLINK_TREE+=	orcus
 
 .if !defined(ORCUS_BUILDLINK3_MK)
 ORCUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.20.2
+BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.21.0
 BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
 
 .endif	# ORCUS_BUILDLINK3_MK
