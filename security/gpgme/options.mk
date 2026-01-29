bdbfaaadf465789e8eacd86adcc5f56de61b8bbe
wiz <wiz@pkgsrc.org>
2026-01-29 10:03:11 +0000

    *: allow gnupg2 version 2.4.9nb2 for the renamed binaries

--- security/gpgme/options.mk
+++ security/gpgme/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.13 2026/01/28 10:50:49 wiz Exp $
+# $NetBSD: options.mk,v 1.14 2026/01/29 10:03:12 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gpgme
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -8,8 +8,9 @@ PKG_SUGGESTED_OPTIONS=		gnupg2
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnupg2)
-DEPENDS+=		gnupg2>=2.5:../../security/gnupg2
+DEPENDS+=		gnupg2>=2.4.9nb2:../../security/gnupg2
 REPLACE_SH+=		tests/gpg/pinentry
+GPG_DEFAULT=		${PREFIX}/bin/gpg
 MAKE_FLAGS+=		GPG=${GPG_DEFAULT}
 .else
 DEPENDS+=		gnupg>=1.4.23nb28:../../security/gnupg
