0f6840e6febd3f6401555904d458e33be59a35b3
adam <adam@pkgsrc.org>
2026-01-30 10:05:16 +0000

    cmake cmake-gui: updated to 4.2.3
    CMake 4.2.3
    * libarchive: Restore tolerance of archives with non-OEM paths on Windows
    * Tests: Remove invalid CUDA code from tests

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.82 2026/01/23 16:04:03 adam Exp $
+# $NetBSD: version.mk,v 1.83 2026/01/30 10:05:16 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.2.2
+CMAKE_VERSION=	4.2.3
 CMAKE_API=	${CMAKE_VERSION:R}
