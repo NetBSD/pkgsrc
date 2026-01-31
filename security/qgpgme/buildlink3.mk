32f37b1c37e5bb84e612f8295d2c846c9c62c64e
markd <markd@pkgsrc.org>
2026-01-31 08:04:30 +0000

    qgpgme: don't build qt5 version. add gpgmepp to buildlink.

--- security/qgpgme/buildlink3.mk
+++ security/qgpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2026/01/07 08:48:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2026/01/31 08:04:30 markd Exp $
 
 BUILDLINK_TREE+=	qgpgme
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.qgpgme?=	qgpgme>=1.24.3nb1
 BUILDLINK_PKGSRCDIR.qgpgme?=	../../security/qgpgme
 
 .include "../../security/gpgme/buildlink3.mk"
+.include "../../security/gpgmepp/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# QGPGME_BUILDLINK3_MK
 
