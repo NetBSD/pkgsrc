53f6252c8ae1cc349aadfa8ee002ec500322f39f
ryoon <ryoon@pkgsrc.org>
2026-02-01 06:50:58 +0000

    lang/openjdk8: Update to 1.8.482
    Changelog:
    Updated BSD port of JDK 8
    
    Additional features include:
    
        Updated to 8u482 GA

--- lang/openjdk8/patches/patch-jdk_src_share_native_sun_awt_libpng_pngpriv.h
+++ lang/openjdk8/patches/patch-jdk_src_share_native_sun_awt_libpng_pngpriv.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-jdk_src_share_native_sun_awt_libpng_pngpriv.h,v 1.3 2018/12/15 20:27:45 jperkin Exp $
+$NetBSD: patch-jdk_src_share_native_sun_awt_libpng_pngpriv.h,v 1.4 2026/02/01 06:50:58 ryoon Exp $
 
 Set _POSIX_SOURCE correctly.
 
---- jdk/src/share/native/sun/awt/libpng/pngpriv.h.orig	2018-12-09 09:51:37.000000000 +0000
+--- jdk/src/share/native/sun/awt/libpng/pngpriv.h.orig	2026-01-23 00:36:25.000000000 +0000
 +++ jdk/src/share/native/sun/awt/libpng/pngpriv.h
-@@ -63,7 +63,7 @@
+@@ -75,7 +75,7 @@
   * Windows/Visual Studio) there is no effect; the OS specific tests below are
   * still required (as of 2011-05-02.)
   */
 -#ifndef _POSIX_SOURCE
 +#if (!defined(__sun) || (__STDC_VERSION__-0 < 199901L)) && !defined(_POSIX_SOURCE)
- # define _POSIX_SOURCE 1 /* Just the POSIX 1003.1 and C89 APIs */
+ #  define _POSIX_SOURCE 1 /* Just the POSIX 1003.1 and C89 APIs */
  #endif
  
