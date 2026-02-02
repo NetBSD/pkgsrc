c8a9d7861832e5867c141d750906a619edf07d18
wiz <wiz@pkgsrc.org>
2026-02-02 22:52:49 +0000

    ghc98: remove AVX workaround for NetBSD 11+
    Problem has been fixed as part of PR 57661.
    
    Bump PKGREVISION.

--- lang/ghc98/patches/patch-libraries_text_cbits_measure__off.c
+++ lang/ghc98/patches/patch-libraries_text_cbits_measure__off.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-libraries_text_cbits_measure__off.c,v 1.1 2025/01/08 14:51:06 pho Exp $
+$NetBSD: patch-libraries_text_cbits_measure__off.c,v 1.2 2026/02/02 22:52:49 wiz Exp $
 
 Workaround for http://gnats.netbsd.org/58379
 
@@ -11,16 +11,23 @@ because AVX-512 being disabled is only a side effect of the macro being
 defined. When the upstream code changes we can be promptly notified by the
 patch not applying cleanly.
 
---- libraries/text/cbits/measure_off.c.orig	2025-01-08 14:36:12.967927622 +0000
+This was fixed for NetBSD 11, but no 10.x or 9.x releases yet (though it was
+pulled up), see PR 57661.
+
+--- libraries/text/cbits/measure_off.c.orig	2024-05-10 05:07:47.000000000 +0000
 +++ libraries/text/cbits/measure_off.c
-@@ -13,10 +13,7 @@
+@@ -13,9 +13,13 @@
  #endif
  #include <stdbool.h>
  
--// stdatomic.h has been introduces in gcc 4.9
++#ifdef __NetBSD__
++#include <sys/param.h>
++#endif
++
+ // stdatomic.h has been introduces in gcc 4.9
 -#if !(__GNUC__ >= 5 || __GNUC__ == 4 && __GNUC_MINOR__ >= 9 || defined(__clang_major__))
++#if !(__GNUC__ >= 5 || __GNUC__ == 4 && __GNUC_MINOR__ >= 9 || defined(__clang_major__)) || (defined(__NetBSD__) && (__NetBSD_Version__ < 1100000000))
  #define __STDC_NO_ATOMICS__
--#endif
+ #endif
  
  #ifndef __STDC_NO_ATOMICS__
- #include <stdatomic.h>
