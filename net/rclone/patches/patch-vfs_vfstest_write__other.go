9e5984ce82964518d0a605f7519fe657f3cd6564
adam <adam@pkgsrc.org>
2026-02-02 08:49:57 +0000

    rclone: updated to 1.73.0
    1.73.0
    https://rclone.org/changelog/#v1-73-0-2026-01-30

--- net/rclone/patches/patch-vfs_vfstest_write__other.go
+++ net/rclone/patches/patch-vfs_vfstest_write__other.go
@@ -1,11 +1,11 @@
-$NetBSD: patch-vfs_vfstest_write__other.go,v 1.2 2025/11/29 17:15:36 adam Exp $
+$NetBSD: patch-vfs_vfstest_write__other.go,v 1.3 2026/02/02 08:49:57 adam Exp $
 
 NetBSD and OpenBSD are Unix-like operating systems.
 
---- vfs/vfstest/write_other.go.orig	2025-11-21 17:10:17.000000000 +0000
+--- vfs/vfstest/write_other.go.orig	2026-01-30 20:53:56.000000000 +0000
 +++ vfs/vfstest/write_other.go
 @@ -1,4 +1,4 @@
--//go:build !linux && !darwin && !freebsd && !windows
+-//go:build !linux && !darwin && !freebsd && !openbsd && !windows
 +//go:build !linux && !darwin && !freebsd && !windows && !netbsd && !openbsd
  
  package vfstest
