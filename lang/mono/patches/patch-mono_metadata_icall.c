f0bd30c1a9c1c63a7af221d62e8daa81c5387d52
ryoon <ryoon@pkgsrc.org>
2026-02-03 15:17:53 +0000

    lang/mono: Update to 6.14.1
    Changelog:
    The 6.14.1 release of Framework Mono is now available.
    
    This is a bugfix release and contains no new features.
    The main changes since 6.14.0 are build fixes for cmake 4.0
    and (separately) macOS 15.4.1.

--- lang/mono/patches/patch-mono_metadata_icall.c
+++ lang/mono/patches/patch-mono_metadata_icall.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-mono_metadata_icall.c,v 1.2 2021/03/03 12:05:41 nia Exp $
+$NetBSD: patch-mono_metadata_icall.c,v 1.3 2026/02/03 15:17:53 ryoon Exp $
 
 NetBSD will side-load <stdbool.h> which must have #define bool _Bool.
 This causes some problems with the C preprocessor usage here. undef it.
 
---- mono/metadata/icall.c.orig	2020-04-30 07:46:10.000000000 +0000
+--- mono/metadata/icall.c.orig	2025-04-29 17:18:29.000000000 +0000
 +++ mono/metadata/icall.c
-@@ -120,6 +120,8 @@
- #include "mono/metadata/icall-signatures.h"
- #include "mono/utils/mono-signal-handler.h"
+@@ -127,6 +127,8 @@
+ #undef bool
+ #endif
  
 +#undef bool
 +
  //#define MONO_DEBUG_ICALLARRAY
  
- #ifdef MONO_DEBUG_ICALLARRAY
+ // Inline with CoreCLR heuristics, https://github.com/dotnet/runtime/blob/69e114c1abf91241a0eeecf1ecceab4711b8aa62/src/coreclr/vm/threads.cpp#L6408.
--- lang/mono/patches/patch-mono_utils_mono-sigcontext.h
+++ lang/mono/patches/patch-mono_utils_mono-sigcontext.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-mono_utils_mono-sigcontext.h,v 1.3 2022/09/20 15:59:33 he Exp $
+$NetBSD: patch-mono_utils_mono-sigcontext.h,v 1.4 2026/02/03 15:17:53 ryoon Exp $
 
 NetBSD/aarch64 support.
 Fix NetBSD/powerpc support.
 
---- mono/utils/mono-sigcontext.h.orig	2019-09-19 07:46:07.000000000 +0000
+--- mono/utils/mono-sigcontext.h.orig	2025-01-13 19:10:41.000000000 +0000
 +++ mono/utils/mono-sigcontext.h
-@@ -356,8 +356,8 @@ typedef struct ucontext {
+@@ -359,8 +359,8 @@ typedef struct ucontext {
  
  	#define UCONTEXT_REG_Rn(ctx, n)   (((os_ucontext*)(ctx))->uc_mcontext.__gregs [(n)])
  	#define UCONTEXT_REG_FPRn(ctx, n) (((os_ucontext*)(ctx))->uc_mcontext.__fpregs.__fpu_regs [(n)])
@@ -16,16 +16,16 @@ Fix NetBSD/powerpc support.
  #elif defined(__FreeBSD__)
  	typedef ucontext_t os_ucontext;
  
-@@ -472,6 +472,12 @@ typedef struct ucontext {
- 	#define UCONTEXT_REG_SP(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_sp)
- 	#define UCONTEXT_REG_R0(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x [ARMREG_R0])
- 	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x))
+@@ -486,6 +486,12 @@ typedef struct ucontext {
+ 	#define UCONTEXT_REG_SP(ctx) (((ucontext_t*)(ctx))->sc_sp)
+ 	#define UCONTEXT_REG_R0(ctx) (((ucontext_t*)(ctx))->sc_x [ARMREG_R0])
+ 	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->sc_x))
 +#elif defined(__NetBSD__)
 +#include <ucontext.h>
 +	#define UCONTEXT_REG_PC(ctx) _UC_MACHINE_PC((ucontext_t*)(ctx))
 +	#define UCONTEXT_REG_SP(ctx) _UC_MACHINE_SP((ucontext_t*)(ctx))
 +	#define UCONTEXT_REG_R0(ctx) _UC_MACHINE_INTRV((ucontext_t*)(ctx))
 +	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->uc_mcontext.__gregs))
- #else
+ #elif !defined(HOST_WIN32)
  #include <ucontext.h>
  	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.pc)
