7211937535fca5f634f442cfbf90d179a45b34c2
wiz <wiz@pkgsrc.org>
2026-02-04 00:06:54 +0000

    py-gyp: update to 0.1pre20240206.
    Switch to wheel.mk, remove git hash from version number.
    
    Three years of changes, but it's only around 10 commits.

--- devel/gyp/patches/patch-pylib_gyp_common.py
+++ devel/gyp/patches/patch-pylib_gyp_common.py
@@ -1,19 +1,9 @@
-$NetBSD: patch-pylib_gyp_common.py,v 1.5 2022/01/27 18:42:15 wiz Exp $
+$NetBSD: patch-pylib_gyp_common.py,v 1.6 2026/02/04 00:06:54 wiz Exp $
 
-* Python 3.10 support
 * Add NetBSD support
 
 --- pylib/gyp/common.py.orig	2022-01-27 18:36:11.000000000 +0000
 +++ pylib/gyp/common.py
-@@ -4,7 +4,7 @@
- 
- from __future__ import with_statement
- 
--import collections
-+import collections.abc as collections
- import errno
- import filecmp
- import os.path
 @@ -445,6 +445,7 @@ def CopyTool(flavor, out_path, generator
    prefix = {
        'aix': 'flock',
