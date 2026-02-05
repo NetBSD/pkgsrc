82947735ed68e7c7b0e28b96406ec254d023c7a3
adam <adam@pkgsrc.org>
2026-02-05 07:57:36 +0000

    raqm: updated to 0.10.4
    Overview of changes leading to 0.10.4
    
    Add build option to skip tests.
    Add dependency override for use as a subproject.
    Fix tests when b_ndebug=true.
    Build, CI and documentation updates.

--- graphics/raqm/buildlink3.mk
+++ graphics/raqm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/10/23 20:37:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2026/02/05 07:57:36 adam Exp $
 
 BUILDLINK_TREE+=	raqm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	raqm
 RAQM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.raqm+=	raqm>=0.10.1
-BUILDLINK_ABI_DEPENDS.raqm?=	raqm>=0.10.3nb1
+BUILDLINK_ABI_DEPENDS.raqm+=	raqm>=0.10.3nb1
 BUILDLINK_PKGSRCDIR.raqm?=	../../graphics/raqm
 
 .include "../../converters/fribidi/buildlink3.mk"
