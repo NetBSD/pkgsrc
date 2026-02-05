b4ed2da335549b2a9658c56a1b41fb896c8124e7
adam <adam@pkgsrc.org>
2026-02-05 12:42:40 +0000

    gperftools: updated to 2.18
    gperftools 2.18 is out!
    
    This release contains a number of improvements for the bazel build
    system, in particular for Windows. Plus a number of correctness fixes.
    
    Notable changes:
    
    * Correctness of C23 sized deallocation (free_sized) vs realloc has
      been fixed. Previously, free_sized could crash the process when used
      on reallocated objects. This also significantly changes behavior of
      realloc. Previously, we had heuristics to avoid shrinking if the new
      size was at least 50% of the old size, and to pad small growths to
      at least 1.25x the original size. These heuristics have been removed
      to ensure compatibility with free_sized.
    
    * Bazel build support for Windows has been significantly improved, now
      working with MinGW and MSVC.
    
    * We now have experimental :tcmalloc_minimal_nopatch Bazel target for
      Windows.
    
    * Fast TLS is now enabled for clang-mingw builds on Windows.
    
    * Building with 256k logical pages on 32-bit targets is fixed.
    
    * Several fixes to Valgrind integration were made. Thanks to
      GitHub user stefvanvlierberghe for reporting and advocating for these
      changes.
    
    * CPU profiler now uses a different mechanism to force child processes
      to use pid in cpu profile file name. Previously, we used a kludge that
      crashed certain software that insisted on valid UTF-8 in environment
      variables. See GitHub issues
      https://github.com/gperftools/gperftools/issues/1044 and
      https://github.com/gperftools/gperftools/issues/1603.
    
    * We now properly recognize SPARCv9 cache line length. Thanks to Nia
      Alarie for the patch.
    
    * Basic support for compile_commands.json generation was added,
      enabling nicer integration with modern IDEs.
    
    * Matthieu MOREL contributed a Bazel 9 compatibility fix.

--- devel/gperftools/patches/patch-src_base_basictypes.h
+++ devel/gperftools/patches/patch-src_base_basictypes.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_base_basictypes.h,v 1.3 2025/11/16 14:56:45 nia Exp $
+$NetBSD: patch-src_base_basictypes.h,v 1.4 2026/02/05 12:42:41 adam Exp $
 
 Add support for NetBSD/powerpc, recognized via __powerpc__.
 
---- src/base/basictypes.h.orig	2025-02-17 16:34:39.000000000 +0000
+--- src/base/basictypes.h.orig	2026-01-24 04:13:42.000000000 +0000
 +++ src/base/basictypes.h
-@@ -150,7 +150,7 @@ inline void bit_store(Dest *dest, const 
+@@ -150,7 +150,7 @@ inline void bit_store(Dest *dest, const
  #if defined(HAVE___ATTRIBUTE__)
  # if (defined(__i386__) || defined(__x86_64__))
  #   define CACHELINE_ALIGNED __attribute__((aligned(64)))
@@ -13,9 +13,9 @@ Add support for NetBSD/powerpc, recognized via __powerpc__.
  #   define CACHELINE_ALIGNED __attribute__((aligned(16)))
  # elif (defined(__arm__))
  #   define CACHELINE_ALIGNED __attribute__((aligned(64)))
-@@ -166,6 +166,8 @@ inline void bit_store(Dest *dest, const 
+@@ -168,6 +168,8 @@ inline void bit_store(Dest *dest, const
  #   define CACHELINE_ALIGNED __attribute__((aligned(64)))
- # elif defined(__loongarch64)
+ # elif defined(__sparcv9) || defined(__sparcv9__)
  #   define CACHELINE_ALIGNED __attribute__((aligned(64)))
 +# elif defined(__sparcv9) || defined(__sparcv9__)
 +#   define CACHELINE_ALIGNED __attribute__((aligned(64)))
