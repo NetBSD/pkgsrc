765110004cc1d8be6039a5fbd4ad9ce769288b2d
adam <adam@pkgsrc.org>
2026-02-07 18:22:48 +0000

    freetds: updated to 1.5.10
    1.5
    
    Fix some compatibility with OpenVMS
    Fix dbreadtext crash on NULL values
    Allows to pass NULL to dbsetifile

--- databases/freetds/patches/patch-include_freetds_thread.h
+++ databases/freetds/patches/patch-include_freetds_thread.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-include_freetds_thread.h,v 1.1 2017/09/17 14:57:59 taca Exp $
+$NetBSD: patch-include_freetds_thread.h,v 1.2 2026/02/07 18:22:48 adam Exp $
 
-* Check _REENTRANT, too.
+Check _REENTRANT, too.
 
---- include/freetds/thread.h.orig	2016-05-18 20:02:15.000000000 +0000
+--- include/freetds/thread.h.orig	2025-03-06 20:38:39.000000000 +0000
 +++ include/freetds/thread.h
 @@ -24,7 +24,7 @@
  
@@ -11,5 +11,5 @@ $NetBSD: patch-include_freetds_thread.h,v 1.1 2017/09/17 14:57:59 taca Exp $
 -#if defined(_THREAD_SAFE) && defined(TDS_HAVE_PTHREAD_MUTEX)
 +#if (defined(_REENTRANT) || defined(_THREAD_SAFE)) && defined(TDS_HAVE_PTHREAD_MUTEX)
  
- #include <pthread.h>
- 
+ #include <tds_sysdep_public.h>
+ #include <freetds/sysdep_private.h>
