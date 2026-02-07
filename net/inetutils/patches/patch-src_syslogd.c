c3050462c2ec2e744d482548f6bfa5289e48a3c5
vins <vins@pkgsrc.org>
2026-02-07 09:53:47 +0000

    net/inetutils: update to 2.7
    # Noteworthy changes in release 2.7 (2025-12-14) [stable]
    
    * Systems without asprintf are now supported through the use of gnulib.
    
    * Fix link errors on Solaris 11 OmniOS.
    
    * ftpd: Expanded documentation of user authentication rules.
      Thanks to Benjamin Cathelineau, see
      <https://lists.gnu.org/archive/html/bug-inetutils/2025-10/msg00000.html>.
    
    * syslogd: Fix a stack-based buffer overflow (CWE-121).
    
    * syslogd: Log 'DEL' control characters as '^?'.
    
    * Fix codespell typos.
    
    * syslogd, talkd: Better compatibility with utmp-less GNU/Linux systems
      when configuring with --enable-systemd.
      Thanks to Valentin Haudiquet for reporting the issue and testing possible
      solutions.  For more info, see the thread starting at
      <https://lists.gnu.org/archive/html/bug-inetutils/2025-11/msg00000.html>.

--- net/inetutils/patches/patch-src_syslogd.c
+++ net/inetutils/patches/patch-src_syslogd.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_syslogd.c,v 1.1 2023/12/31 19:06:15 adam Exp $
+$NetBSD: patch-src_syslogd.c,v 1.2 2026/02/07 09:53:47 vins Exp $
 
-Fix conflicting types error.
+This is only relevant to glibc.
 
---- src/syslogd.c.orig	2023-12-31 19:00:12.282997217 +0000
+--- src/syslogd.c.orig	2026-02-03 17:15:08.433185757 +0000
 +++ src/syslogd.c
-@@ -278,7 +278,7 @@ void logerror (const char *);
- void logmsg (int, const char *, const char *, int);
- void printline (const char *, const char *);
- void printsys (const char *);
--char *ttymsg (struct iovec *, int, char *, int);
-+char *ttymsg (struct iovec *, int, const char *, int);
- void wallmsg (struct filed *, struct iovec *);
- char **crunch_list (char **oldlist, char *list);
- char *textpri (int pri);
+@@ -120,7 +120,7 @@
+  * the definition LOG_USER = (1 << 3).  Avoid this
+  * harmful mistake.
+  */
+-#ifdef LOG_MAKEPRI
++#if defined (LOG_MAKEPRI) && defined(__GLIBC__)
+ # if LOG_MAKEPRI (1, 0) > LOG_PRIMASK
+ #  warning Discarding faulty LOG_MAKEPRI defined in system header file.
+ #  undef LOG_MAKEPRI
