cf61ed3ef7dd26d53d35200e2363777a130e3c2a
leot <leot@pkgsrc.org>
2026-02-08 12:00:01 +0000

    age: Update to 1.3.1
    pkgsrc changes:
    - Install man pages
    - Take maintainership
    
    
    Changes:
    v1.3.1
    v1.3.1 is a minor release to restore version injection from downstream
    package build processes.
    
    
    v1.3.0: post-quantum (and more)!
    Exactly six years after the first age beta release, v1.3.0 brings
    post-quantum resistance to age, along with a couple long-requested
    features, built-in support for recipients compatible with hardware
    plugins, I/O API improvements, and many usability enhancements.
    
    - Post-quantum recipients
    - New I/O APIs
    - age-inspect: presents the metadata of an age file without decrypting it
    - Built-in recipients compatible with hardware plugins
    - Non-interactive passphrase input
    - Armor fixes
    - Detached headers
    - Usability improvements

--- security/age/go-modules.mk
+++ security/age/go-modules.mk
@@ -1,16 +1,20 @@
-# $NetBSD: go-modules.mk,v 1.5 2024/10/17 18:47:27 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.6 2026/02/08 12:00:01 leot Exp $
 
-GO_MODULE_FILES+=	c2sp.org/!c!c!t!v/age/@v/v0.0.0-20240306222714-3ec4d716e805.mod
-GO_MODULE_FILES+=	c2sp.org/!c!c!t!v/age/@v/v0.0.0-20240306222714-3ec4d716e805.zip
+GO_MODULE_FILES+=	c2sp.org/!c!c!t!v/age/@v/v0.0.0-20251208015420-e9274a7bdbfd.mod
+GO_MODULE_FILES+=	c2sp.org/!c!c!t!v/age/@v/v0.0.0-20251208015420-e9274a7bdbfd.zip
 GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.1.0.mod
 GO_MODULE_FILES+=	filippo.io/edwards25519/@v/v1.1.0.zip
-GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.12.0.mod
-GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.12.0.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.24.0.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.21.0.zip
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.21.0.mod
-GO_MODULE_FILES+=	golang.org/x/term/@v/v0.21.0.zip
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.mod
-GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.22.0.zip
+GO_MODULE_FILES+=	filippo.io/hpke/@v/v0.4.0.mod
+GO_MODULE_FILES+=	filippo.io/hpke/@v/v0.4.0.zip
+GO_MODULE_FILES+=	filippo.io/nistec/@v/v0.0.4.mod
+GO_MODULE_FILES+=	filippo.io/nistec/@v/v0.0.4.zip
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.14.1.mod
+GO_MODULE_FILES+=	github.com/rogpeppe/go-internal/@v/v1.14.1.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.45.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.45.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.38.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.38.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.37.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.37.0.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.39.0.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.39.0.zip
