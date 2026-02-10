148d81d03f0ae3e6dd5ec08ea0a32e8312573365
jakllsch <jakllsch@pkgsrc.org>
2026-02-10 20:17:59 +0000

    Patch to make (ft)jam build with gcc-14.
    Also, fix all pkglint (except lack of LICENSE=).

--- devel/jam/patches/patch-jambase.c
+++ devel/jam/patches/patch-jambase.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-jambase.c,v 1.1 2012/12/26 18:51:47 ryoon Exp $
+$NetBSD: patch-jambase.c,v 1.2 2026/02/10 20:17:59 jakllsch Exp $
+
+No need to handle ar specially on Solaris in pkgsrc.
 
 --- jambase.c.orig	2012-12-26 18:50:42.000000000 +0000
 +++ jambase.c
---
+++ devel/jam/patches/patch-mkjambase.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-mkjambase.c,v 1.1 2026/02/10 20:17:59 jakllsch Exp $
+
+Explicit int return type for main().
+
+--- mkjambase.c.orig	2026-02-10 18:07:54.048821831 +0000
++++ mkjambase.c
+@@ -24,6 +24,7 @@
+ # include <stdio.h>
+ # include <string.h>
+ 
++int
+ main( int argc, char **argv, char **envp )
+ {
+ 	char buf[ 1024 ];
