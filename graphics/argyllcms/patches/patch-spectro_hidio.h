2c39ba5e27f9624b032a18b7d209ed962869fcfc
jakllsch <jakllsch@pkgsrc.org>
2026-02-11 02:41:15 +0000

    Update argyllcms to 3.5.0
    Change log since 3.4.1:
    
    Version 3.5.0  4th February 2026
    -------------
    
     * Added MSWindows ARM release.
    
     * Added Mac ARM release.
    
     * Removed Mac PowerPC build from release.
    
     * Added test profile forward and backward lookup error checks to
       release sanity checking script.
    
     * Changed colprof so that rather than just warning about an ink limit
       being set for an additive device space, it ignores it.
    
     * Fixed bug in targen introduced in V3.3.0 where it adds a default
       ink limit for all device types. If not overidden later in the
       profiling process, this can introduce an ink limit 10% less
       than the unconstrained limit. This is a problem for additive devices.
    
     * Fixed reported problem with some models of Spyder2024.
    
     * Upgraded built-in libpng to V1.6.51
    
     * Upgraded built-in zlib to V1.3.1
    
     * Took a stab at adding libusb-win32 ARM64 system driver support.
       Reported to work, but installing libusb-win32 system driver
       is more difficult.
    
     * Added Munsell Linear Grayscale scanin reference files.
       (Thanks to Tarek A. Haila and Frank Schaumann)
    
     * Added -g option to xicc/fakeCMY to emit RGB values rather
       than CMY values.
    
     * Tweaked i1d3 frequency measurement mode to return exactly
       zero on measuring perfect black rather than the correctly
       rounded value, so as to make it more consistent with the
       period measurement mode. This inconsistency shows up with a
       Rev. B when  I1D3_DISABLE_AIO is set and the 0x83 bug
       is triggered, and frequency measurement is used as a fallback
       to period measurement mode.
       Rev. A instruments shouldn't see any change since for them
       the period measurement mode doesn't suffer from the 0x83 bug.
    
     * Added -o observer option to xicc/specplot
    
     * Improved robustness of command name filename parsing in a number of utilities.
    
     * Expand spectro/average to handle input test charts.
    
     * Improve JETI Specbos 2501 operation.

--- graphics/argyllcms/patches/patch-spectro_hidio.h
+++ graphics/argyllcms/patches/patch-spectro_hidio.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-spectro_hidio.h,v 1.1 2023/11/17 17:37:48 jakllsch Exp $
+$NetBSD: patch-spectro_hidio.h,v 1.2 2026/02/11 02:41:15 jakllsch Exp $
 
 Attempt to add NetBSD support to spectro/hidio
 
---- spectro/hidio.h.orig	2023-10-23 00:56:17.000000000 +0000
+--- spectro/hidio.h.orig	2026-02-09 22:25:25.000000000 +0000
 +++ spectro/hidio.h
 @@ -80,6 +80,15 @@ struct hid_idevice {
      int bread;            				/* Bytes read by callback */
- #endif	/* __MAC_OS_X_VERSION_MAX_ALLOWED < 1060 */
+ #endif	/* MAC_OS_X_VERSION_MIN_REQUIRED < 1060 */
  #endif
 +#if defined(UNIX_X11)
 +#if defined(__NetBSD__)
@@ -17,6 +17,6 @@ Attempt to add NetBSD support to spectro/hidio
 +	int wchunk;
 +#endif
 +#endif
+ 	char *SerialNumber;		/* If not-NULL, USB serial number string */
  };
  
- /* Cleanup and then free an hidd entry */
