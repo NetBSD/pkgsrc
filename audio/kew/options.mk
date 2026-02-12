6070912d24f74419a305b67dd4ea9033b46a6780
ktnb <ktnb@pkgsrc.org>
2026-02-12 12:55:00 +0000

    kew: fixed bug with non-dbus option

--- audio/kew/options.mk
+++ audio/kew/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2024/12/13 15:12:57 ktnb Exp $
+# $NetBSD: options.mk,v 1.3 2026/02/12 12:55:00 ktnb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kew
 PKG_SUPPORTED_OPTIONS=	dbus
@@ -10,5 +10,8 @@ PKG_SUGGESTED_OPTIONS=
 ###  Include dbus
 ###
 .if !empty(PKG_OPTIONS:Mdbus)
+MAKE_FLAGS+=	USE_DBUS=1
 .include "../../sysutils/dbus/buildlink3.mk"
+.else
+MAKE_FLAGS+=	USE_DBUS=0
 .endif
