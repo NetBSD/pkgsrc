507f99dcb0b677b9437be776cc0898c69b35642f
adam <adam@pkgsrc.org>
2026-02-14 18:31:50 +0000

    wcslib: updated to 8.5
    WCSLIB version 8.5 (2025/12/06)
    
    WCSLIB's RCS version files are now included in the distribution thereby
    providing a documented history of all changes made since v1.0.  They
    will be found in RCS subdirectories in each code directory.  A small
    number of discontinued files are preserved with their RCS version files
    having a ',x' suffix instead of ',v'.
    
    * C library
    
      - In linp2x(), quarantine NaN elements of the pixel coordinate vector
        from infecting non-NaN elements in the case where the PCij matrix is
        diagonal (noting that zero * NaN = NaN).  Likewise in linx2p().
        Reported by Thomas Robitaille with patch.
    
      - Quelled nuisance compiler warnings in wcspih() and wcsbth().
    
    * User manual
    
      - Documentation generation moved to doxygen 1.15.0 (was 1.12.0).
        The makefile patches doxygen.sty as per my Doxygen bug report 11881.

--- graphics/wcslib/buildlink3.mk
+++ graphics/wcslib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/03/20 11:05:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/02/14 18:31:50 adam Exp $
 
 BUILDLINK_TREE+=	wcslib
 
@@ -7,6 +7,13 @@ WCSLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.wcslib+=	wcslib>=7.2
 BUILDLINK_PKGSRCDIR.wcslib?=	../../graphics/wcslib
+
+BUILDLINK_TARGETS+=	buildlink-include-wcslib
+
+.PHONY: buildlink-include-wcslib
+buildlink-include-wcslib:
+	${LN} -s ${BUILDLINK_DIR}/include/wcslib-8.5 ${BUILDLINK_DIR}/include/wcslib
+
 .endif # WCSLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wcslib
