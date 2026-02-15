945813280aa21e777e1a56cd0928b55e35358a08
gdt <gdt@pkgsrc.org>
2026-02-15 18:14:31 +0000

    lang/erlang: Update to 27.3.4.7
    The update to 27.3.4.7 is more or less bugfixes and minor improvements
    relative to 27.0.1.  A number of erlang packages now assume that bugs
    fixed in later 27.x are fixed.  This update is intentionally not to
    28.x, which is deferred until post branch.
    
    Rebuilding ejabberd and many depending erlang-foo with 27.3.4.7 on
    NetBSD 10 amd64 results in a working server, with no build fallout at
    least among those packages.
    
    * OTP 27.3.[1234] and 27.3.4.[1234567]
    
    These are micro releases, per a scan of the README files (which are
    changelog not NEWS).
    
    * OTP 27.3
    
    Erlang/OTP 27.3 is the third maintenance patch package for OTP 27,
    with mostly bug fixes as well as improvements.
    
    * OTP 27.2
    
    Erlang/OTP 27.2 is the second maintenance patch package for OTP 27,
    with mostly bug fixes as well as improvements.  Potential
    incompatibilities:
    
      The HTTP client now correctly takes into account the full_result
      request option when returning an asynchronous request.
    
    * OTP 27.1
    
    Erlang/OTP 27.1 is the first maintenance patch package for OTP 27,
    with mostly bug fixes as well as improvements.

--- lang/erlang/patches/patch-erts_emulator_drivers_common_inet__drv.c
+++ lang/erlang/patches/patch-erts_emulator_drivers_common_inet__drv.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-erts_emulator_drivers_common_inet__drv.c,v 1.6 2024/05/27 19:07:50 riastradh Exp $
+$NetBSD: patch-erts_emulator_drivers_common_inet__drv.c,v 1.7 2026/02/15 18:14:31 gdt Exp $
 
 Fix ctype(3) misuse.
 
 Unbreak build due to differences in the NetBSD SCTP implementation.
 
---- erts/emulator/drivers/common/inet_drv.c.orig	2024-03-06 15:30:18.000000000 +0000
+--- erts/emulator/drivers/common/inet_drv.c.orig	2026-02-04 14:57:36.000000000 +0000
 +++ erts/emulator/drivers/common/inet_drv.c
-@@ -591,9 +591,9 @@ static int my_strncasecmp(const char *s1
+@@ -606,9 +606,9 @@ static int my_strncasecmp(const char *s1
  {
      int i;
  
@@ -18,11 +18,10 @@ Unbreak build due to differences in the NetBSD SCTP implementation.
  }
  	
  
-@@ -1331,7 +1331,11 @@ static struct erl_drv_entry tcp_inet_dri
-     inet_emergency_close
+@@ -1362,6 +1362,11 @@ static struct erl_drv_entry tcp_inet_dri
  };
  
--
+ 
 +#ifdef __NetBSD__
 +#undef SCTP_DELAYED_ACK_TIME
 +#undef HAVE_DECL_SCTP_DELAYED_ACK_TIME
@@ -30,4 +29,4 @@ Unbreak build due to differences in the NetBSD SCTP implementation.
 +#endif
  
  #ifdef HAVE_UDP
- static int        packet_inet_init(void);
+ 
