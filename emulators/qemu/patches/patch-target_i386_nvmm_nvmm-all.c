1add28c9c5a9e0d166114e6617f2e19038d956f3
adam <adam@pkgsrc.org>
2026-02-15 07:37:37 +0000

    qemu: updated to 10.2.1
    10.2.1
    Bug fixes

--- emulators/qemu/patches/patch-target_i386_nvmm_nvmm-all.c
+++ emulators/qemu/patches/patch-target_i386_nvmm_nvmm-all.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-target_i386_nvmm_nvmm-all.c,v 1.11 2025/08/27 10:38:31 ryoon Exp $
+$NetBSD: patch-target_i386_nvmm_nvmm-all.c,v 1.12 2026/02/15 07:37:37 adam Exp $
 
 emulators/qemu: Fix NVMM for UEFI (OVMF)
 
@@ -7,15 +7,6 @@ emulators/qemu: Fix NVMM for UEFI (OVMF)
 
 --- target/i386/nvmm/nvmm-all.c.orig	2025-08-26 18:32:38.000000000 +0000
 +++ target/i386/nvmm/nvmm-all.c
-@@ -984,7 +984,7 @@ nvmm_init_vcpu(CPUState *cpu)
-         }
-     }
- 
--    qcpu->vcpu_dirty = true;
-+    cpu->vcpu_dirty = true;
-     cpu->accel = qcpu;
- 
-     return 0;
 @@ -1059,7 +1059,11 @@ nvmm_process_section(MemoryRegionSection
      unsigned int delta;
      uintptr_t hva;
