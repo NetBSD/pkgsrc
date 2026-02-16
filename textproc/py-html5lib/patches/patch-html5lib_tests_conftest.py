9a56f0d71cc8fa882af59123ee1f8a8ae7629a26
wiz <wiz@pkgsrc.org>
2026-02-16 19:01:02 +0000

    py-html5lib: Fix build with latest setuptools.
    Bump PKGREVISION.

--- textproc/py-html5lib/patches/patch-html5lib_tests_conftest.py
+++ textproc/py-html5lib/patches/patch-html5lib_tests_conftest.py
@@ -1,10 +1,36 @@
-$NetBSD: patch-html5lib_tests_conftest.py,v 1.1 2024/04/30 09:01:14 wiz Exp $
+$NetBSD: patch-html5lib_tests_conftest.py,v 1.2 2026/02/16 19:01:02 wiz Exp $
 
 https://github.com/html5lib/html5lib-python/pull/506
 
 --- html5lib/tests/conftest.py.orig	2020-06-22 23:23:02.000000000 +0000
 +++ html5lib/tests/conftest.py
-@@ -99,10 +99,19 @@ def pytest_collect_file(path, parent):
+@@ -2,7 +2,6 @@ import sys
+ import os.path
+ import sys
+ 
+-import pkg_resources
+ import pytest
+ 
+ from .tree_construction import TreeConstructionFile
+@@ -63,17 +62,6 @@ def pytest_configure(config):
+                             spec, marker = line.strip().split(";", 1)
+                         else:
+                             spec, marker = line.strip(), None
+-                        req = pkg_resources.Requirement.parse(spec)
+-                        if marker and not pkg_resources.evaluate_marker(marker):
+-                            msgs.append("%s not available in this environment" % spec)
+-                        else:
+-                            try:
+-                                installed = pkg_resources.working_set.find(req)
+-                            except pkg_resources.VersionConflict:
+-                                msgs.append("Outdated version of %s installed, need %s" % (req.name, spec))
+-                            else:
+-                                if not installed:
+-                                    msgs.append("Need %s" % spec)
+ 
+         # Check cElementTree
+         import xml.etree.ElementTree as ElementTree
+@@ -99,10 +87,19 @@ def pytest_collect_file(path, parent):
  
      if _tree_construction in dir_and_parents:
          if path.ext == ".dat":
--- textproc/py-html5lib/patches/patch-setup.py
+++ textproc/py-html5lib/patches/patch-setup.py
@@ -1,12 +1,40 @@
-$NetBSD: patch-setup.py,v 1.1 2025/10/09 11:59:29 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2026/02/16 19:01:02 wiz Exp $
 
 Fix build with Python 3.14
 Based on a comment in
 https://github.com/html5lib/html5lib-python/pull/589
 
---- setup.py.orig	2025-10-09 11:57:09.810246679 +0000
+--- setup.py.orig	2020-06-22 23:23:02.000000000 +0000
 +++ setup.py
-@@ -89,9 +89,14 @@ with open(join(here, "html5lib", "__init
+@@ -6,9 +6,7 @@ from setuptools import setup, find_packages, __version
+ 
+ from os.path import join, dirname
+ from setuptools import setup, find_packages, __version__ as setuptools_version
+-from pkg_resources import parse_version
+ 
+-import pkg_resources
+ 
+ try:
+     import _markerlib.markers
+@@ -46,17 +44,6 @@ if _markerlib and sys.version_info[0] == 3:
+ 
+     _markerlib.default_environment = default_environment
+ 
+-# Avoid the very buggy pkg_resources.parser, which doesn't consistently
+-# recognise the markers needed by this setup.py
+-# Change this to setuptools 20.10.0 to support all markers.
+-if pkg_resources:
+-    if parse_version(setuptools_version) < parse_version('18.5'):
+-        MarkerEvaluation = pkg_resources.MarkerEvaluation
+-
+-        del pkg_resources.parser
+-        pkg_resources.evaluate_marker = MarkerEvaluation._markerlib_evaluate
+-        MarkerEvaluation.evaluate_marker = MarkerEvaluation._markerlib_evaluate
+-
+ classifiers = [
+     'Development Status :: 5 - Production/Stable',
+     'Intended Audience :: Developers',
+@@ -89,9 +76,14 @@ with open(join(here, "html5lib", "__init__.py"), "rb")
      for a in assignments:
          if (len(a.targets) == 1 and
                  isinstance(a.targets[0], ast.Name) and
