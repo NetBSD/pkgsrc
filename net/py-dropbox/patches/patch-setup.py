d3594b84d1b3595176d9375bc70291b27b151a73
wiz <wiz@pkgsrc.org>
2026-02-17 15:27:36 +0000

    py-dropbox: fix build with setuptools 82

--- net/py-dropbox/patches/patch-setup.py
+++ net/py-dropbox/patches/patch-setup.py
@@ -1,11 +1,31 @@
-$NetBSD: patch-setup.py,v 1.3 2024/08/10 07:14:15 wiz Exp $
+$NetBSD: patch-setup.py,v 1.4 2026/02/17 15:27:36 wiz Exp $
+
+Remove ez_setup.py use, pkgsrc will provide setuptools:
+https://github.com/dropbox/dropbox-sdk-python/issues/525
 
 Unpin stone.
+
 Remove unused pytest-runner dependency.
 
 --- setup.py.orig	2024-06-03 16:45:16.000000000 +0000
 +++ setup.py
-@@ -27,13 +27,10 @@ version = eval(line.split('=', 1)[1].str
+@@ -6,15 +6,6 @@ import sys
+ import os
+ import sys
+ 
+-# Ensure setuptools is available
+-try:
+-    from ez_setup import use_setuptools
+-    use_setuptools()
+-except ImportError:
+-    # Try to use ez_setup, but if not, continue anyway. The import is known
+-    # to fail when installing from a tar.gz.
+-    print('Could not import ez_setup', file=sys.stderr)
+-
+ from setuptools import setup
+ 
+ dbx_mod_path = os.path.join(os.path.dirname(__file__), 'dropbox/dropbox_client.py')
+@@ -27,13 +18,10 @@ install_reqs = [
  install_reqs = [
      'requests>=2.16.2',
      'six >= 1.12.0',
