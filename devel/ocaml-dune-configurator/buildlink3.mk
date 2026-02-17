0e67a764d2f87294d4c905f4e07e397ec4333a60
wiz <wiz@pkgsrc.org>
2026-02-17 13:38:07 +0000

    ocaml-dune*: these are build dependencies, only build-depend on them by default

--- devel/ocaml-dune-configurator/buildlink3.mk
+++ devel/ocaml-dune-configurator/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/11/05 21:28:13 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/02/17 13:38:07 wiz Exp $
 
 BUILDLINK_TREE+=	ocaml-dune-configurator
 
 .if !defined(OCAML_DUNE_CONFIGURATOR_BUILDLINK3_MK)
 OCAML_DUNE_CONFIGURATOR_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.ocaml-dune-configurator?=	build
 
 BUILDLINK_API_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.0.0
 BUILDLINK_ABI_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.11.1
--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2023/11/05 21:26:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2026/02/17 13:38:07 wiz Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
 .if !defined(OCAML_DUNE_BUILDLINK3_MK)
 OCAML_DUNE_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.ocaml-dune?=	build
 
 BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=3.0.0
 BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=3.11.1
