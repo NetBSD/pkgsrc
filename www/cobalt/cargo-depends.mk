5151082a8fdf3c2c5f3b9177bc5e8c0b6f69fd60
pin <pin@pkgsrc.org>
2026-02-17 13:10:46 +0000

    www/cobalt: update to 0.20.3
     - No ChangeLog provided.

--- www/cobalt/cargo-depends.mk
+++ www/cobalt/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.9 2025/10/03 15:51:45 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.10 2026/02/17 13:10:46 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.24.2
 CARGO_CRATE_DEPENDS+=	adler2-2.0.0
