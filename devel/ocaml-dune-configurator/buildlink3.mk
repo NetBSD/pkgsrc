2733772f8ed39e8c1ff6226d5aec2b3be8c7ec01
wiz <wiz@pkgsrc.org>
2026-02-17 13:38:53 +0000

    *: recursive bump for ocaml-dune becoming a build dependency

--- devel/ocaml-dune-configurator/buildlink3.mk
+++ devel/ocaml-dune-configurator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2026/02/17 13:38:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2026/02/17 13:38:58 wiz Exp $
 
 BUILDLINK_TREE+=	ocaml-dune-configurator
 
@@ -8,7 +8,7 @@ OCAML_DUNE_CONFIGURATOR_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.ocaml-dune-configurator?=	build
 
 BUILDLINK_API_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.11.1
+BUILDLINK_ABI_DEPENDS.ocaml-dune-configurator+=	ocaml-dune-configurator>=3.21.1nb1
 BUILDLINK_PKGSRCDIR.ocaml-dune-configurator?=	../../devel/ocaml-dune-configurator
 
 .include "../../devel/ocaml-dune/buildlink3.mk"
--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2026/02/17 13:38:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2026/02/17 13:38:53 wiz Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
@@ -8,7 +8,7 @@ OCAML_DUNE_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.ocaml-dune?=	build
 
 BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=3.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=3.11.1
+BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=3.21.1
 BUILDLINK_PKGSRCDIR.ocaml-dune?=	../../devel/ocaml-dune
 .endif	# OCAML_DUNE_BUILDLINK3_MK
 
