cf803c6568064fc0ecab373b9b946419c2eefe39
wiz <wiz@pkgsrc.org>
2026-02-19 15:09:47 +0000

    libxml2: update to 2.15.1.
    docs disabled (which is the default) because after changes in 2.15
    (see below) they need doxygen + libxslt, introducing a dependency
    cycle.
    
    v2.15.1: Oct 16 2025
    
    ### Regressions
    
    - parser: Fix attribute normalization and standalone check
    
    ### Build
    
    - io: Handle clashing error codes on AIX
    - Fixed compilation error on GCC versions prior to 4.5 (Lars Erik Wik)
    - meson: Fix install dir of man pages
    
    ### Deprecations
    
    - tree: Undeprecate several struct members
    - io: Undeprecate xmlOutputBuffer members
    - valid: Undeprecate xmlValidGetPotentialChildren
    
    
    v2.15.0: Sep 15 2025
    
    ### Major changes
    
    The API documentation is now generated with Doxygen. Building the
    documentation requires the new --with-docs configuration option as well
    as Doxygen, xsltproc and the DocBook 4 XSLT stylesheets.
    
    The Python bindings are disabled by default now. Building the bindings
    also requires Doxygen.
    
    Support for Schematron is now disabled by default.
    
    The parser option XML_PARSE_UNZIP is now required to read compressed data.
    
    HTML serialization and handling of character encodings is more in line
    with the HTML5 spec now.
    
    More accessors for xmlParserCtxt were added.
    
    ### Deprecations
    
    More internal functions and struct members were deprecated.
    
    ### Removals
    
    The built-in HTTP client and support for LZMA compression were removed.
    
    The custom Windows build system in `win32` was removed in favor of CMake.
    
    ### Planned removals
    
    The Python bindings and support for Schematron validation are planned to
    be removed in the 2.16 release.
    
    The following features are considered for removal:
    
    - Modules API (xmlmodule.h)
    - Support for zlib compressed file I/O
    
    RELAX NG support is still in a bad state and a long-term removal
    candidate.

--- textproc/libxml2/buildlink3.mk
+++ textproc/libxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2026/01/07 08:49:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2026/02/19 15:09:47 wiz Exp $
 
 BUILDLINK_TREE+=	libxml2
 
@@ -20,7 +20,6 @@ pkgbase := libxml2
 .include "../../textproc/icu/buildlink3.mk"
 .endif
 
-.include "../../archivers/xz/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif # LIBXML2_BUILDLINK3_MK
