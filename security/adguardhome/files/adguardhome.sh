c29ccb59b486ed03f03db1dc9e3675db2a539021
adam <adam@pkgsrc.org>
2026-02-23 13:22:14 +0000

    adguardhome: fix for NetBSD

--- security/adguardhome/files/adguardhome.sh
+++ security/adguardhome/files/adguardhome.sh
@@ -4,13 +4,13 @@
 # REQUIRE: DAEMON
 # BEFORE:  LOGIN
 
- . /etc/rc.subr
+. /etc/rc.subr
 
- name="AdGuardHome"
- rcvar="adguardhome"
- command="@PREFIX@/bin/${name}"
- command_args="--no-check-update -c @PKG_SYSCONFDIR@/AdGuardHome.yaml -w @PREFIX@/libdata/adguardhome --pidfile @VARBASE@/run/${name}.pid -l syslog &"
- pidfile="/var/run/${name}.pid"
+name="AdGuardHome"
+rcvar="adguardhome"
+command="@PREFIX@/bin/${name}"
+command_args="--no-check-update -c @PKG_SYSCONFDIR@/AdGuardHome.yaml -w @PREFIX@/libdata/adguardhome --pidfile @VARBASE@/run/${name}.pid -l syslog &"
+pidfile="/var/run/${name}.pid"
 
- load_rc_config $name
- run_rc_command "$1"
+load_rc_config $name
+run_rc_command "$1"
