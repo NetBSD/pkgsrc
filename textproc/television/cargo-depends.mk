6c9bbae84b61c497ab25054e8e15af125e1f59c6
pin <pin@pkgsrc.org>
2026-02-26 12:56:12 +0000

    textproc/television: update to 0.15.2
    0.15.2 - 2026-02-05
    ⛰️ Features
        d997d66 (fish) Add channel name completion by @simono in #891
    
    ⚡ Performance
        2fde413 (entries) Remove unnecessary clone by @alexpasmantier
        2ba6887 (frecency) Remove per-frame frecency cache refresh by @alexpasmantier
        5eaa2c3 (matcher) Optimize Utf32 to String conversion for ASCII content by @alexpasmantier
        b8154d2 (matcher) Skip results pipeline for actions that don't affect results by @alexpasmantier
        83ee4b9 (update) Add update_cycle benchmarks for Television::update() by @alexpasmantier
        5019651 (uncategorized) Avoid unnecessary clones in matcher and entry pipeline by @alexpasmantier
    
    ⚙️ Miscellaneous Tasks
        9436217 (uncategorized) Release version 0.15.2 by @alexpasmantier
        057c10e (uncategorized) Release version 0.15.1 by @alexpasmantier
    
    0.15.1 - 2026-02-04
    ⛰️ Features
        644c818 (cable) 20 new channels by @alexpasmantier in #896
        f372439 (cable) New channel actions by @alexpasmantier
        c7a5164 (config) Enable a global shell override configuration option by @alexpasmantier in #892
    
    📚 Documentation
        cc3cb54 (uncategorized) Add frecency fields to channel config docs by @alexpasmantier
        7bef281 (uncategorized) Proofreading by @alexpasmantier in #887
        03d5f51 (uncategorized) Documentation overhaul by @alexpasmantier in #886
    
    ⚙️ Miscellaneous Tasks
        9a3a00f (cable_docs) Fix generate_cable_docs workflow by @alexpasmantier
        3110878 (uncategorized) Release version 0.15.1 by @alexpasmantier
    
    Build
        5e4a9bb (dependabot) Add cargo and rust-toolchain ecosystems by @simono in #889
    
    0.15.0 - 2026-01-27
    ⛰️ Features
        f2fa3f8 (actions) Add a new picker for channel actions by @alexpasmantier in #880
        6be1cfb (cable) 21 new channels by @alexpasmantier
        c564ec0 (cable) Existing channel improvements, more default actions by @alexpasmantier
        ab6bbfc (cli) Sort list-channels output alphabetically by @simono in #866
        26ba031 (prototypes) Enable using a custom shell for source, preview and action commands by @alexpasmantier
        aa6fe62 (remote) Add remote control missing requirements popup by @alexpasmantier
        cf3261e (ui) Add cycle_x keybinding hint next to tab indicator when cycling is enabled by @alexpasmantier in #876
        a8ca74a (ui) Add cycle_sources and cycle_previews tab indicators by @alexpasmantier in #870
        b4b61c7 (update-channels) Skip channels with unmet requirements by default by @louis-thevenet in #859
        6cfca4d (uncategorized) Frecency sorting by @alexpasmantier in #869
        ccec17b (uncategorized) Generalize support for custom sorting by @alexpasmantier
        1facb56 (uncategorized) Allow disabling automatic sorting of matcher results by @alexpasmantier in #856
    
    🐛 Bug Fixes
        a16c886 (sesh) Allow paths containing whitespaces by @alexpasmantier
    
    🚜 Refactor
        86846ec (cable) Simpler systemd units channel by @alexpasmantier
        49c8370 (ui) Replace stateful spinner by static symbol by @alexpasmantier
    
    📚 Documentation
        767db54 (uncategorized) Fix: fix typos and grammar by @simono in #860
    
    ⚡ Performance
        549eed2 (frecency) Avoid string allocations in sort comparisons by @lalvarezt in #877
    
    🧪 Testing
        e259535 (uncategorized) Faster local testing by @alexpasmantier
    
    ⚙️ Miscellaneous Tasks
        ded7ef6 (uncategorized) Release version 0.15.0 by @alexpasmantier
        f4465f0 (uncategorized) Cleaning up mode color dead code by @alexpasmantier

--- textproc/television/cargo-depends.mk
+++ textproc/television/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.11 2026/01/12 12:05:10 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.12 2026/02/26 12:56:12 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.25.1
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
