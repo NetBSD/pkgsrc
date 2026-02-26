1d1e9dc4c9ee993a840af8dc7312c78168e00fdf
adam <adam@pkgsrc.org>
2026-02-26 15:45:32 +0000

    octave: updated to 11.1.0
    11.1.0
    https://octave.org/NEWS-11.html

--- math/octave/patches/patch-libgnu_stdio.in.h
+++ math/octave/patches/patch-libgnu_stdio.in.h
@@ -1,15 +1,14 @@
-$NetBSD: patch-libgnu_stdio.in.h,v 1.5 2022/10/24 18:02:19 adam Exp $
+$NetBSD: patch-libgnu_stdio.in.h,v 1.6 2026/02/26 15:45:32 adam Exp $
 
 Do not re-define gets() on SunOS, incompatible prototype.
 
---- libgnu/stdio.in.h.orig	2022-07-28 13:08:26.000000000 +0000
+--- libgnu/stdio.in.h.orig	2026-02-18 17:54:40.000000000 +0000
 +++ libgnu/stdio.in.h
-@@ -934,10 +934,12 @@ _GL_WARN_ON_USE (getline, "getline is un
+@@ -1132,9 +1132,11 @@ _GL_WARN_ON_USE (getline, "getline is un
  /* It is very rare that the developer ever has full control of stdin,
     so any use of gets warrants an unconditional warning; besides, C11
     removed it.  */
 +#ifndef __sun
- #undef gets
  #if HAVE_RAW_DECL_GETS && !defined __cplusplus
  _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
  #endif
