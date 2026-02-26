890c8540e5b68a88ca14eadc285027a9fb0d693a
kikadf <kikadf@pkgsrc.org>
2026-02-26 11:35:58 +0000

    www/esbuild: update to 0.27.3
    * 0.27.3
    Preserve URL fragments in data URLs
    Parse and print CSS @scope rules
    Fix a minification bug with lowering of for await
    Update the Go compiler from v1.25.5 to v1.25.7
    
    * 0.27.2
    Allow import path specifiers starting with #/
    Automatically add the -webkit-mask prefix
    Additional minification of switch statements
    Forbid using declarations inside switch clauses
    
    * 0.27.1
    Fix bundler bug with var nested inside if
    Fix minifier bug with for inside try inside label
    Inline IIFEs containing a single expression
    The minifier now strips empty finally clauses
    Allow tree-shaking of the Symbol constructor
    
    * 0.27.0
    This release deliberately contains backwards-incompatible changes.
    Use Uint8Array.fromBase64 if available
    Update the Go compiler from v1.23.12 to v1.25.4
    
    * 0.26.0
    Enable trusted publishing
    
    * 0.25.12
    Fix a minification regression with CSS media queries
    Update the list of known JavaScript globals
    Add support for the new @view-transition CSS rule
    Trim CSS rules that will never match
    
    * 0.25.11
    Add support for with { type: 'bytes' } imports
    Lower CSS media query range syntax
    
    * 0.25.10
    Fix a panic in a minification edge case
    Fix @supports nested inside pseudo-element

--- www/esbuild/go-modules.mk
+++ www/esbuild/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.5 2025/08/29 18:50:09 bsiegert Exp $
+# $NetBSD: go-modules.mk,v 1.6 2026/02/26 11:35:58 kikadf Exp $
 
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
