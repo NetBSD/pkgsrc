7fbd911842d1b4870e47b59d06216b51317fdc1d
alnsn <alnsn@pkgsrc.org>
2026-03-01 22:34:45 +0000

    Add BUILDLINK_INCDIRS.

--- devel/zycore-c/buildlink3.mk
+++ devel/zycore-c/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2026/02/26 22:51:44 alnsn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/03/01 22:34:45 alnsn Exp $
 
 BUILDLINK_TREE+=	zycore-c
 
@@ -8,6 +8,9 @@ ZYCORE_C_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.zycore-c+=	zycore-c>=1.5.1
 BUILDLINK_ABI_DEPENDS.zycore-c+=	zycore-c>=1.5.1
 BUILDLINK_PKGSRCDIR.zycore-c?=		../../devel/zycore-c
+
+BUILDLINK_INCDIRS.zycore-c+=		include/Zycore
+
 .endif	# ZYCORE_C_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zycore-c
--- devel/zydis/buildlink3.mk
+++ devel/zydis/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2026/02/26 22:55:54 alnsn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/03/01 22:35:20 alnsn Exp $
 
 BUILDLINK_TREE+=	zydis
 
@@ -8,6 +8,8 @@ ZYDIS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.zydis+=	zydis>=4.1.1
 BUILDLINK_ABI_DEPENDS.zydis+=	zydis>=4.1.1
 BUILDLINK_PKGSRCDIR.zydis?=	../../devel/zydis
+
+BUILDLINK_INCDIRS.zydis+=		include/Zydis
 
 .include "../../devel/zycore-c/buildlink3.mk"
 .endif	# ZYDIS_BUILDLINK3_MK
