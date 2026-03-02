7259918a753f172744ce177f4a3c5286fe7056b6
yhardy <yhardy@pkgsrc.org>
2026-03-02 13:31:38 +0000

    print/zathura-pdf-mupdf: update to zathura-pdf-mupdf-2026.02.03
    "Looks good to me!" from leot@
    
    Version 2026.02.03
    
        Update for changes in girara
    
    Version 2026.01.30
    
        Various bug fixes and improvements
    
    Version 0.4.6
    
        Upgrade to mupdf 1.26
        Add support for user-provided CSS styles
        Add support for attachment export
        Various fixes and improvements

--- print/zathura-pdf-mupdf/patches/patch-document.c
+++ print/zathura-pdf-mupdf/patches/patch-document.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-document.c,v 1.5 2018/10/04 19:13:28 leot Exp $
+$NetBSD: patch-document.c,v 1.6 2026/03/02 13:31:38 yhardy Exp $
 
 Correct the glib-2.0 include header file path.
 
---- zathura-pdf-mupdf/document.c.orig	2018-03-17 19:47:01.000000000 +0000
+--- zathura-pdf-mupdf/document.c.orig	2026-03-02 08:48:22.436471952 +0000
 +++ zathura-pdf-mupdf/document.c
 @@ -3,7 +3,7 @@
  #include <mupdf/fitz.h>
@@ -12,4 +12,4 @@ Correct the glib-2.0 include header file path.
 +#include <glib.h>
  
  #include "plugin.h"
- 
+ #include <girara/utils.h>
