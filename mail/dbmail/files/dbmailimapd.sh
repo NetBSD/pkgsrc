b5b7d318d5a2da2645e216c527dc99cc28b1e7d1
tnn <tnn@pkgsrc.org>
2026-03-03 20:07:00 +0000

    dbmail: ensure pidfile directory exists. Bump

--- mail/dbmail/files/dbmailimapd.sh
+++ mail/dbmail/files/dbmailimapd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dbmailimapd.sh,v 1.3 2026/01/02 17:57:31 tnn Exp $
+# $NetBSD: dbmailimapd.sh,v 1.4 2026/03/03 20:07:00 tnn Exp $
 #
 
 # PROVIDE: dbmailimap mail
@@ -13,6 +13,16 @@ rcvar=${name}
 required_files="@PKG_SYSCONFDIR@/dbmail.conf"
 command="@PREFIX@/sbin/dbmail-imapd"
 pidfile="@VARBASE@/run/dbmail/dbmail-imapd.pid"
+start_precmd="dbmailimapd_precmd"
+
+dbmailimapd_precmd()
+{
+	if [ ! -d @VARBASE@/run/dbmail ]; then
+		@MKDIR@ @VARBASE@/run/dbmail
+		@CHMOD@ 0750 @VARBASE@/run/dbmail
+		@CHOWN@ @DBMAIL_USER@:@DBMAIL_GROUP@ @VARBASE@/run/dbmail
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
--- mail/dbmail/files/dbmailsieved.sh
+++ mail/dbmail/files/dbmailsieved.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dbmailsieved.sh,v 1.1 2026/01/02 17:57:31 tnn Exp $
+# $NetBSD: dbmailsieved.sh,v 1.2 2026/03/03 20:07:00 tnn Exp $
 #
 
 # PROVIDE: dbmailieved mail
@@ -13,6 +13,16 @@ rcvar=${name}
 required_files="@PKG_SYSCONFDIR@/dbmail.conf"
 command="@PREFIX@/sbin/dbmail-sieved"
 pidfile="@VARBASE@/run/dbmail/dbmail-sieved.pid"
+start_precmd="dbmailsieved_precmd"
+
+dbmailsieved_precmd()
+{
+	if [ ! -d @VARBASE@/run/dbmail ]; then
+		@MKDIR@ @VARBASE@/run/dbmail
+		@CHMOD@ 0750 @VARBASE@/run/dbmail
+		@CHOWN@ @DBMAIL_USER@:@DBMAIL_GROUP@ @VARBASE@/run/dbmail
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
