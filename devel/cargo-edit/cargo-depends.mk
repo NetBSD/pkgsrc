c9b48741e015982e54ef7f26268e7ac63d477226
pin <pin@pkgsrc.org>
2026-03-03 19:09:47 +0000

    devel/cargo-edit: update to 0.13.9
    0.13.9 - 2026-03-02
    Fixes
    
     - Report error when failing to update lockfile

--- devel/cargo-edit/cargo-depends.mk
+++ devel/cargo-edit/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.6 2025/11/12 09:36:02 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.7 2026/03/03 19:09:47 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.4
