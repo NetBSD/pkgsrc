d623a5db59c10883f0c8745e2b297902a6e62242
bsiegert <bsiegert@pkgsrc.org>
2026-03-07 11:00:59 +0000

    goffice0.10: update to 0.10.60
    - Fix fraction format problem. [Bug 803].
    - Doc fixes.
    - Remove workaround for ancient gtk+ bug. [Bug 81].
    - I18N fix for format selector.
    
    Maybe this helps with PR pkg/60059.

--- misc/goffice0.10/patches/patch-goffice_math_go-complex.c
+++ misc/goffice0.10/patches/patch-goffice_math_go-complex.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-goffice_math_go-complex.c,v 1.2 2017/10/03 12:44:05 wiz Exp $
+$NetBSD: patch-goffice_math_go-complex.c,v 1.3 2026/03/07 11:00:59 bsiegert Exp $
 
 Format string is passed in as argument.
 
---- goffice/math/go-complex.c.orig	2016-03-24 21:19:04.000000000 +0000
+--- goffice/math/go-complex.c.orig	2026-02-11 02:06:20.000000000 +0000
 +++ goffice/math/go-complex.c
-@@ -87,6 +87,8 @@ MAKE_BOXED_TYPE(go_complex_get_type, "GO
- #endif
+@@ -36,6 +36,8 @@
+ #define COMPLEX SUFFIX(GOComplex)
  
  /* ------------------------------------------------------------------------- */
 +#pragma GCC diagnostic push
 +#pragma GCC diagnostic ignored "-Wformat-nonliteral"
  
- char *
- SUFFIX(go_complex_to_string) (COMPLEX const *src, char const *reformat,
-@@ -132,6 +134,7 @@ SUFFIX(go_complex_to_string) (COMPLEX co
+ static COMPLEX *
+ SUFFIX(go_complex_dup) (COMPLEX *src)
+@@ -131,6 +133,7 @@ SUFFIX(go_complex_to_string) (COMPLEX co
  
  	return res;
  }
