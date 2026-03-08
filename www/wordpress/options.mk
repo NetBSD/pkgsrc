5c72c66f9d0d639febda497b209f92d05087ff16
taca <taca@pkgsrc.org>
2026-03-08 13:45:27 +0000

    www/wordpress: update to 6.9.1
    pkgsrc change
    * Allow php84 and php85.
    * Remove a pkglint warning.
    
    WordPress 6.8.3 (2025-09-30)
    WordPress 6.9 (2025-12-02)
    WordPress 6.9.1 (2026-02-03)
    
    * Changes from WordPress 6.8.2 are too many to write here, please refer
      <https://wordpress.org/news/category/releases/> in detail.

--- www/wordpress/options.mk
+++ www/wordpress/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2025/07/26 07:20:28 taca Exp $
+# $NetBSD: options.mk,v 1.7 2026/03/08 13:45:27 taca Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.wordpress
 PKG_SUPPORTED_OPTIONS=		ap-php php-fpm
@@ -14,4 +14,3 @@ DEPENDS+=	${APACHE_PKG_PREFIX}-${PHP_PKG_PREFIX}>=${PHP_BASE_VERS}:../../www/ap-
 .if !empty(PKG_OPTIONS:Mphp-fpm)
 DEPENDS+=	${PHP_PKG_PREFIX}-fpm>=${PHP_BASE_VERS}:../../www/php-fpm
 .endif
-
