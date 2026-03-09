f917c761e0a13a9dae83471beeaab274683f3474
adam <adam@pkgsrc.org>
2026-03-09 10:19:53 +0000

    gstreamer1: updated to 1.28.1
    1.28.1
    
    Highlighted bugfixes:
    
    Various security fixes and playback fixes
    Add new whisper-based speech-to-text transcription element
    Add new debugseimetainserter plugin for testing SEI meta insertion
    Fix scaling and resizing with UIView on EAGL and Vulkan
    Reverse playback and gap handling fixes in various components
    avviddec: Handle field/order changes in mixed interlace mode
    awstranscriber2: workaround for suspected Rust SDK regression
    cudaupload, cudadownload: Fix CUDA/GL interop copy path
    decodebin3: Fix switch to smaller collections and improve collection change on existing pad
    devenv: Add a subproject for providing the LunarG MoltenVK SDK
    livesync: fixes and reverse playback handling; ignore upstream latency when upstream is not live
    objectdetectionoverlay: add support for rotated bounding boxes
    qml6glsrc: Fix rendering of scene with clipped items
    speechmatics: allow configuring audio events such as detecting applause, laughter and music
    livekit webrtc: emit session-requested only for Producer role
    tsdemux: Fix Continuity Counter handling and handle clock change/resets without skew correction
    v4l2: Add support for AV1 stateful V4l2 decoder
    vpxdec: Support downstream pools with alignment requirements
    vtdec, vtenc: Lots of Apple VideoToolbox decoder and encoder fixes
    applemedia build improvements, patches for tvOS support, tvos cross file
    wavpack: Fix handling of format changes, extend parser with new features, handle non-S32 samples
    webrtcsink: allow specifying custom headers to signalling server
    webrtcsink: negotiate profile and level for input encoded in H.264
    webrtcsrc: add request type pads and allow sending encoded data downstream
    cerbero: wheel: Add a new `gstreamer_meta` package with fewer deps
    Various bug fixes, build fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.28 2026/02/13 18:50:23 wiz Exp $
+# $NetBSD: version.mk,v 1.29 2026/03/09 10:19:53 adam Exp $
 
-GST_VERSION=	1.28.0
+GST_VERSION=	1.28.1
 .if ${GST_VERSION:E} < 10
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 .else
