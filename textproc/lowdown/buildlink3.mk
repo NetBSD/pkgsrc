1b9b49406bd6d8c9a9ed28f6e69e377d13c09df1
wiz <wiz@pkgsrc.org>
2026-03-09 17:32:36 +0000

    lowdown: update to 3.0.0.
    Added initial support for outputting modern Unix manpages with
    -tmdoc. This is intended as a replacement for the legacy -tman.
    Supplement this with --roff-manpage, which accepts a markdown file
    that looks like a Unix manpage and attempts to convert it into a
    well-formed mdoc or man. Most of this is purely syntactic, but
    several sections in Unix manpages (e.g., the NAME and SYNOPSIS)
    have a rigorous syntax that this mode attempts to output.
    
    Several minor (but breaking) changes to the API to take up fewer
    bits in the oflags field of struct lowdown_opts: consolidate all
    XXX_NOLINK, XXX_RELLINK, and XXX_SHORTLINK as LOWDOWN_NOLINK,
    LOWDOWN_RELLINK, and LOWDOWN_SHORTLINK. This only applies to the
    LOWDOWN_MAN, LOWDOWN_MS, and LOWDOWN_TERM output modes. The earlier
    LOWDOWN_NROFF output mode is deprecated and is now an alias for
    LOWDOWN_MS, which should be used moving forward.
    
    Do the same with the various XXX_SKIP_HTML bits.
    
    Deprecate the lowdown_nroff_xxx in favour of lowdown_roff_xxx. The
    functions remain as deprecated and passing through to the new
    equivalents.
    
    Deprecate the macro variables LOWDOWN_NROFF_GROFF LOWDOWN_NROFF_NUMBERED
    LOWDOWN_NROFF_MANPAGE LOWDOWN_NROFF_ENDNOTES in favour of their
    ROFF names. The old macros are aliased for compatibility.
    
    Library bumped from version 2 to 3 for those reasons.
    
    Tweak the behaviour of -tterm and --term-no-links (or --term-no-rellinks)
    with respect to embedded images and no text so that there's always
    a clickable link and image component, if possible.
    
    Add --roff-no-rellinks, similar to --term-no-rellinks. Respect
    --roff-short-links in more cases as well.
    
    Change the behaviour of -tms and -tman where --roff-no-links (or
    the new --roff-no-rellinks) will have consistent behaviour even if
    --roff-traditional is not specified.
    
    Fix that, when using lowdown-diff for the terminal, resetting the
    colour after an insertion or delete would trigger the black colour,
    which obviously doesn't work with a black background. Instead, use
    the default colour.
    
    When generating header identifiers in -thtml, incorporate codespan
    text as well. This is the same as pandoc's default behaviour as
    well.

--- textproc/lowdown/buildlink3.mk
+++ textproc/lowdown/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/05/26 20:38:47 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/03/09 17:32:36 wiz Exp $
 
 BUILDLINK_TREE+=	lowdown
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	lowdown
 LOWDOWN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.lowdown+=	lowdown>=1.0.2
+BUILDLINK_ABI_DEPENDS.lowdown+=	lowdown>=3.0.0
 BUILDLINK_PKGSRCDIR.lowdown?=	../../textproc/lowdown
 .endif	# LOWDOWN_BUILDLINK3_MK
 
