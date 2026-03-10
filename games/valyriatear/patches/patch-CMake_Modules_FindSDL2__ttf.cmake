3d9402145ef206f1fded47f6f0676b92c98f0c2f
adam <adam@pkgsrc.org>
2026-03-10 09:09:53 +0000

    valyriatear: fix build with newer SDL2_ttf and on Darwin

---
+++ games/valyriatear/patches/patch-CMake_Modules_FindSDL2__ttf.cmake
@@ -0,0 +1,15 @@
+$NetBSD: patch-CMake_Modules_FindSDL2__ttf.cmake,v 1.1 2026/03/10 09:09:53 adam Exp $
+
+Fix for newer CMake.
+
+--- CMake/Modules/FindSDL2_ttf.cmake.orig	2026-03-10 08:35:02.820680595 +0000
++++ CMake/Modules/FindSDL2_ttf.cmake
+@@ -74,7 +74,7 @@ endif()
+ set(SDL2_TTF_LIBRARIES ${SDL2_TTF_LIBRARY})
+ set(SDL2_TTF_INCLUDE_DIRS ${SDL2_TTF_INCLUDE_DIR})
+ 
+-# include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
++include(FindPackageHandleStandardArgs)
+ 
+ FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2_ttf
+                                   REQUIRED_VARS SDL2_TTF_LIBRARIES SDL2_TTF_INCLUDE_DIRS
---
+++ games/valyriatear/patches/patch-src_utils_src_utils_gl__include.h
@@ -0,0 +1,19 @@
+$NetBSD: patch-src_utils_src_utils_gl__include.h,v 1.1 2026/03/10 09:09:54 adam Exp $
+
+Fix build on Darwin.
+
+--- src/utils/src/utils/gl_include.h.orig	2026-03-10 08:40:27.314681845 +0000
++++ src/utils/src/utils/gl_include.h
+@@ -11,11 +11,10 @@
+ #ifndef __GL_INCLUDE_HEADER__
+ #define __GL_INCLUDE_HEADER__
+ 
++#include <GL/glew.h>
+ #ifdef __APPLE__
+-#include <OpenGL/glew.h>
+ #include <OpenGL/gl.h>
+ #else
+-#include <GL/glew.h>
+ #include <GL/gl.h>
+ #endif
+ 
