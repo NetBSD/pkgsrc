a39c9beb3196da91fcb112ef51cd88fbf7831eee
osa <osa@pkgsrc.org>
2026-03-10 17:24:10 +0000

    lang/quickjs: update to the recent snapshot
    While I'm here enable building/installing a shared library, it may
    require to build nginx javascript module.
    
    Discussed:	https://mail-index.netbsd.org/pkgsrc-users/2026/03/05/msg042691.html

--- lang/quickjs/buildlink3.mk
+++ lang/quickjs/buildlink3.mk
@@ -1,11 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/02/07 12:36:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2026/03/10 17:24:10 osa Exp $
 
 BUILDLINK_TREE+=	quickjs
 
 .if !defined(QUICKJS_BUILDLINK3_MK)
 QUICKJS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.quickjs+=	quickjs>=20240113
+BUILDLINK_INCDIRS.quickjs?=	include/quickjs
+
+BUILDLINK_API_DEPENDS.quickjs+=	quickjs>=20250913
 BUILDLINK_PKGSRCDIR.quickjs?=	../../lang/quickjs
 .endif	# QUICKJS_BUILDLINK3_MK
 
