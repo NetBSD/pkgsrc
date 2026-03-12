fbeee71f04e2dcd8767366df0e00043aff56d8a3
adam <adam@pkgsrc.org>
2026-03-12 12:19:38 +0000

    defguard: updated to 1.6.4
    1.6.4
    
    This is a security patch for the major 1.6 release.
    It includes dependency updates to resolve the following CVEs:
    
    CVE-2026-25537
    GHSA-7587-4wv6-m68m
    GHSA-8h58-w33p-wq3g
    GHSA-c7ph-f7jm-xv4w
    CVE-2026-25727
    CVE-2026-25639
    CVE-2026-2391

--- security/defguard/version.mk
+++ security/defguard/version.mk
@@ -1,5 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2025/10/21 18:31:28 adam Exp $
-DEFGUARD_VERSION=	1.5.1
+# $NetBSD: version.mk,v 1.3 2026/03/12 12:19:38 adam Exp $
+
+DEFGUARD_VERSION=	1.6.4
 
 CATEGORIES=	security net
 MASTER_SITES=	${MASTER_SITE_GITHUB:=defguard/}
@@ -15,9 +16,14 @@ EXTRA_DIST+=	defguard-${DEFGUARD_VERSION}-proto${EXTRACT_SUFX}
 DISTFILES=	${DEFAULT_DISTFILES} ${EXTRA_DIST}
 
 TOOL_DEPENDS+=	protobuf>0:../../devel/protobuf
+USE_TOOLS+=	pkg-config
 
+MAKE_ENV+=	LIBGIT2_NO_VENDOR=1
+MAKE_ENV+=	OPENSSL_DIR=${BUILDLINK_PREFIX.openssl}
 MAKE_ENV+=	VERGEN_IDEMPOTENT=1
 MAKE_ENV+=	VERGEN_GIT_SHA=pkgsrc
+RUSTFLAGS+=	-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.libgit2}/lib
+RUSTFLAGS+=	-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.openssl}/lib
 
 post-extract:
 	${RMDIR} ${WRKSRC}/proto && ${LN} -f -s ${WRKDIR}/proto ${WRKSRC}/proto
