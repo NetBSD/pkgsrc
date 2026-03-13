479cbc31b6d72e1601b68b932edbdb47fda84b6b
hauke <hauke@pkgsrc.org>
2026-03-13 21:09:18 +0000

    Adopt NetBSD build fix from security/mbedtls4.

--- www/hiawatha/patches/patch-mbedtls_tf-psa-crypto_drivers_builtin_src_platform__util.c
+++ www/hiawatha/patches/patch-mbedtls_tf-psa-crypto_drivers_builtin_src_platform__util.c
@@ -1,17 +1,15 @@
-$NetBSD: patch-mbedtls_tf-psa-crypto_drivers_builtin_src_platform__util.c,v 1.1 2026/03/13 21:02:50 hauke Exp $
+$NetBSD: patch-mbedtls_tf-psa-crypto_drivers_builtin_src_platform__util.c,v 1.2 2026/03/13 21:09:18 hauke Exp $
 
-Make devmajor_t and friends available on NetBSD for KERN_ARND support
+    Fix build on NetBSD (from security/mbedtls4)
 
 --- mbedtls/tf-psa-crypto/drivers/builtin/src/platform_util.c.orig	2026-03-12 19:02:22.000000000 +0000
 +++ mbedtls/tf-psa-crypto/drivers/builtin/src/platform_util.c
-@@ -20,6 +20,10 @@
- #define _GNU_SOURCE
+@@ -11,7 +11,7 @@
+  * mbedtls_config.h, which pulls in glibc's features.h. Harmless on other platforms
+  * except OpenBSD, where it stops us accessing explicit_bzero.
+  */
+-#if !defined(_POSIX_C_SOURCE) && !defined(__OpenBSD__)
++#if !defined(_POSIX_C_SOURCE) && !defined(__OpenBSD__) && !defined(__NetBSD__)
+ #define _POSIX_C_SOURCE 200112L
  #endif
  
-+#if defined(__NetBSD__)
-+#define _NETBSD_SOURCE
-+#endif
-+
- #include "tf_psa_crypto_common.h"
- 
- #include "mbedtls/platform_util.h"
