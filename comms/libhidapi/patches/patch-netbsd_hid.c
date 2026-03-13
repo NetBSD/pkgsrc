6e5cf1d955d0aaf11339cdb5d1ae788418762d51
tsutsui <tsutsui@pkgsrc.org>
2026-03-13 10:37:25 +0000

    libhidapi: update to 0.15.0
    pkgsrc changes:
    
     - add PLIST_VARS for libhidapi-netbsd (NetBSD HID native backend)
     - pull iconv(3) const check from libusb version
     - check both addr 0 and 1 on enumerating devices (see NetBSD PR/60073)
    
    Upstream changes:
     https://github.com/libusb/hidapi/releases/tag/hidapi-0.15.0
    
    general: Add hid_send_output_report() (#677)
    general: Add hid_read_error() (#721)
    
    winapi: add hid_winapi_set_write_timeout (#700)
    winapi: improvements for hid_get_report_descriptor/hid_winapi_descriptor_reconstruct_pp_data (#707)
    
    hidraw: report only Top-Level Usage_page/Usage pairs as 'unique' devices (#601)
    
    macOS: add option to mimic 1.0.0 compatibility version (#709)
    
    NetBSD: UHID native backend implementation (#612)
    
    libusb: add abstraction for thread model, allow building using non-POSIX runtime
    libusb: enable support for Xbox 360 and Xbox One controllers (#572)
    
    general: various other fixes and improvements

---
+++ comms/libhidapi/patches/patch-netbsd_hid.c
@@ -0,0 +1,59 @@
+$NetBSD: patch-netbsd_hid.c,v 1.1 2026/03/13 10:37:25 tsutsui Exp $
+
+- don't assume roothub addr is always zero (see NetBSD PR/60073)
+- check iconv(3) arg type properly
+
+--- netbsd/hid.c.orig	2025-05-18 18:25:13.000000000 +0000
++++ netbsd/hid.c
+@@ -30,6 +30,10 @@
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include <iconv.h>
++#ifndef ICONV_CONST
++#define ICONV_CONST
++#endif
++
+ #include <poll.h>
+ 
+ /* NetBSD */
+@@ -724,6 +728,7 @@ struct hid_device_info HID_API_EXPORT * 
+ 	for (size_t i = 0; i < len; i++) {
+ 		char devpath[USB_MAX_DEVNAMELEN];
+ 		int bus;
++		struct hid_device_info *prev_end;
+ 
+ 		strlcpy(devpath, "/dev/", sizeof(devpath));
+ 		strlcat(devpath, arr[i], sizeof(devpath));
+@@ -732,7 +737,13 @@ struct hid_device_info HID_API_EXPORT * 
+ 		if (bus == -1)
+ 			continue;
+ 
+-		enumerate_usb_devices(bus, 0, hid_enumerate_callback, &hed);
++		/* ehci/ohci/uhci/dwctwo etc. use 'addr 1' for root hubs */
++		prev_end = hed.end;
++		enumerate_usb_devices(bus, 1, hid_enumerate_callback, &hed);
++		/* xhci uses 'addr 0' so check it if no device at addr 1 */
++		if (hed.end == prev_end)
++			enumerate_usb_devices(bus, 0,
++			    hid_enumerate_callback, &hed);
+ 
+ 		close(bus);
+ 	}
+@@ -1087,7 +1098,7 @@ int HID_API_EXPORT_CALL hid_get_indexed_
+ 	struct usb_string_desc usd;
+ 	usb_string_descriptor_t *str;
+ 	iconv_t ic;
+-	const char *src;
++	ICONV_CONST char *src;
+ 	size_t srcleft;
+ 	char *dst;
+ 	size_t dstleft;
+@@ -1131,7 +1142,7 @@ int HID_API_EXPORT_CALL hid_get_indexed_
+ 		return -1;
+ 	}
+ 
+-	src = (const char *) str->bString;
++	src = (ICONV_CONST char *)str->bString;
+ 	srcleft = str->bLength - 2;
+ 	dst = (char *) string;
+ 	dstleft = sizeof(wchar_t[maxlen]);
