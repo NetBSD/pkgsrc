aa36ce238d52c7f84d3e181f83b7afcc1adaedea
nia <nia@pkgsrc.org>
2026-03-16 11:24:47 +0000

    sunclock: Fix implicit declaration of alloca

--- time/sunclock/patches/patch-readvmf.c
+++ time/sunclock/patches/patch-readvmf.c
@@ -1,11 +1,26 @@
-$NetBSD: patch-readvmf.c,v 1.1 2022/12/09 14:13:00 vins Exp $
+$NetBSD: patch-readvmf.c,v 1.2 2026/03/16 11:24:48 nia Exp $
+
+Fix implicit declaration of alloca.
 
 Fix incorrect type usage (gzFile is already a pointer type).
 Don't use data read from file as a format string.
 
 --- readvmf.c.orig	2008-08-22 13:58:52.000000000 +0000
 +++ readvmf.c
-@@ -340,7 +340,7 @@ pixmap_image()
+@@ -25,8 +25,12 @@
+  */
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <math.h>
++#if defined(__sun) || defined(__linux__)
++#include <alloca.h>
++#endif
+ #ifdef ZLIB
+ #include <zlib.h>
+ #include <errno.h>
+@@ -340,7 +344,7 @@ pixmap_image()
  char *
  getdata(fd)
  #ifdef ZLIB
@@ -14,7 +29,7 @@ Don't use data read from file as a format string.
  #else
  FILE *fd;
  #endif
-@@ -433,7 +433,7 @@ struct Sundata * Context;
+@@ -433,7 +437,7 @@ struct Sundata * Context;
    double theta, phi;
    char *str, *ptr;
  #ifdef ZLIB
@@ -23,7 +38,7 @@ Don't use data read from file as a format string.
    int plen;
    char *zpath;
  #else
-@@ -505,7 +505,7 @@ struct Sundata * Context;
+@@ -505,7 +509,7 @@ struct Sundata * Context;
       ret_value = 5;
       goto abort;
    }
