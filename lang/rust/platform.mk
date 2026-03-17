f7a8fbc64a89e47d1c7a534cd7eba70ca9396401
nia <nia@pkgsrc.org>
2026-03-17 07:02:08 +0000

    rust: pretend not to work on armv6
    right now cargo-c doesn't build on armv6 and the pbulk fallout is
    quite large.

--- lang/rust/platform.mk
+++ lang/rust/platform.mk
@@ -1,4 +1,4 @@
-# $NetBSD: platform.mk,v 1.34 2025/10/02 16:59:50 nia Exp $
+# $NetBSD: platform.mk,v 1.35 2026/03/17 07:02:08 nia Exp $
 
 # This file encodes whether a given platform has support for rust.
 
@@ -8,7 +8,7 @@
 .if !defined(PLATFORM_SUPPORTS_RUST)
 
 # Rust needs NetBSD>8
-.for rust_arch in aarch64 earmv6hf earmv7hf i386 powerpc riscv64 sparc64 x86_64
+.for rust_arch in aarch64 earmv7hf i386 powerpc riscv64 sparc64 x86_64
 .  for rust_os in Darwin FreeBSD Linux NetBSD SunOS
 .    if ${OPSYS} != "NetBSD" || empty(OS_VERSION:M[0-8].*)
 RUST_PLATFORMS+=	${rust_os}-*-${rust_arch}
