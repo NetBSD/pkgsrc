afce5180763be10423ae372e96535b9fe47c773a
nia <nia@pkgsrc.org>
2026-03-18 16:19:54 +0000

    xv: Fix build on SunOS with recent gcc

--- graphics/xv/patches/patch-xv.h
+++ graphics/xv/patches/patch-xv.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
+$NetBSD: patch-xv.h,v 1.5 2026/03/18 16:19:54 nia Exp $
 
 - don't declare errno and sys_errlist
 - use strerror
@@ -8,7 +8,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
 - pull XRandR support from forked upstream
   https://gitlab.com/DavidGriffith/xv/-/commit/a84406cb
 
---- xv.h.orig	2025-04-19 21:39:44.640689781 +0000
+--- xv.h.orig	2026-03-18 16:17:56.151309998 +0000
 +++ xv.h
 @@ -139,9 +139,7 @@
  #endif
@@ -39,7 +39,17 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  #    define ERRSTR(x) strerror(x)
  #  else
  #    define ERRSTR(x) sys_errlist[x]
-@@ -208,7 +206,9 @@
+@@ -196,6 +194,9 @@
+ #  include <perror.h>
+ #endif
+ 
++#if defined(__sun) || defined(__linux__)
++#  include <alloca.h>
++#endif
+ 
+ /* GRR 20070512:  Very few modern systems even have a malloc.h anymore;
+  *                stdlib.h is, well, the standard.  (Former explicitly listed
+@@ -208,7 +209,9 @@
  #  if defined(hp300) || defined(hp800) || defined(NeXT)
  #    include <sys/malloc.h>    /* it's in "sys" on HPs and NeXT */
  #  else
@@ -50,7 +60,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  #  endif
  #endif
  
-@@ -222,6 +222,11 @@
+@@ -222,6 +225,11 @@
  #include <X11/Xatom.h>
  #include <X11/Xmd.h>
  
@@ -62,7 +72,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  #ifdef TV_L10N
  #  include <X11/Xlocale.h>
  #endif
-@@ -392,7 +397,7 @@
+@@ -392,7 +400,7 @@
  #  endif
  #endif
  
@@ -71,7 +81,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  #  define USE_GETCWD
  #endif
  
-@@ -411,6 +416,9 @@
+@@ -411,6 +419,9 @@
  /* END OF CONFIGURATION INFO */
  /*****************************/
  
@@ -81,7 +91,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  
  #ifdef DOJPEG
  #  define HAVE_JPEG
-@@ -568,6 +576,12 @@
+@@ -568,6 +579,12 @@
  #  define F_TIFINC  0
  #endif
  
@@ -94,7 +104,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  #ifdef HAVE_PNG
  #  define F_PNGINC  1
  #else
-@@ -621,7 +635,8 @@
+@@ -621,7 +638,8 @@
  #define F_JPC       ( 0 + F_PNGINC + F_JPGINC)
  #define F_JP2       ( 0 + F_PNGINC + F_JPGINC + F_JP2INC)
  #define F_GIF       ( 0 + F_PNGINC + F_JPGINC + F_JP2INC + F_JP2INC)  /* always avail; index varies */
@@ -104,7 +114,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  #define F_PS        ( 1 + F_TIFF)
  #define F_PBMRAW    ( 2 + F_TIFF)
  #define F_PBMASCII  ( 3 + F_TIFF)
-@@ -691,6 +706,7 @@
+@@ -691,6 +709,7 @@
  #define RFT_PI       (JP_EXT_RFT + 4)
  #define RFT_PIC2     (JP_EXT_RFT + 5)
  #define RFT_MGCSFX   (JP_EXT_RFT + 6)
@@ -112,7 +122,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  
  /* definitions for page up/down, arrow up/down list control */
  #define LS_PAGEUP   0
-@@ -1404,6 +1420,11 @@ WHERE Window        pngW;
+@@ -1404,6 +1423,11 @@ WHERE Window        pngW;
  WHERE int           pngUp;        /* is pngW mapped, or what? */
  #endif
  
@@ -124,7 +134,7 @@ $NetBSD: patch-xv.h,v 1.4 2025/04/21 20:23:27 wiz Exp $
  
  #ifdef ENABLE_FIXPIX_SMOOTH
  WHERE int           do_fixpix_smooth;  /* GRR 19980607: runtime FS dithering */
-@@ -2094,6 +2115,14 @@ int LoadSunRas             PARM((char *,
+@@ -2094,6 +2118,14 @@ int LoadSunRas             PARM((char *,
  int WriteSunRas            PARM((FILE *, byte *, int, int, int, byte *,
  				 byte *, byte*, int, int, int));
  
