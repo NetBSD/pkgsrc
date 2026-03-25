720864b3900fcd686d98fb72c8493e5a96a9d67e
wiz <wiz@pkgsrc.org>
2026-03-25 23:06:03 +0000

    openmp: update to 20.1.8.
    Part of the llvm update.

--- parallel/openmp/buildlink3.mk
+++ parallel/openmp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2025/05/09 05:51:26 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2026/03/25 23:06:03 wiz Exp $
 
 BUILDLINK_TREE+=	openmp
 
 .if !defined(OPENMP_BUILDLINK3_MK)
 OPENMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openmp+=	openmp>=19.1.0
-BUILDLINK_ABI_DEPENDS.openmp+=	openmp>=19.1.0
+BUILDLINK_API_DEPENDS.openmp+=	openmp>=20.1.0
+BUILDLINK_ABI_DEPENDS.openmp+=	openmp>=20.1.0
 BUILDLINK_PKGSRCDIR.openmp?=	../../parallel/openmp
 .endif	# OPENMP_BUILDLINK3_MK
 
