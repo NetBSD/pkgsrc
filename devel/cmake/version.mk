c50b945016c17422caa86e90c5bea753a544c116
adam <adam@pkgsrc.org>
2026-03-26 09:00:32 +0000

    cmake: updated to 4.3.0
    Some of the most significant changes in CMake 4.3 are:
    
    Support for importing and exporting packages described using the
    Common Package Specification (CPS) was added.
    
    “cmake-instrumentation(7)” was added to enable collection of timing
    data, target information, and system diagnostic information during
    the configure, generate, build, test, and install steps of a CMake
    project
    
    “cmake –build” now supports specifying a build directory and preset
    together. The build preset will be used with the explicit build
    directory substituted.
    
    The “cmake -E bin2c” command-line tool was added.
    
    CMake archive operations gained support for multithreading, compression
    levels, and new compression methods.
    
    The “get_property()” and “set_property()” commands now support a
    “FILE_SET” scope for file set properties of a target.
    
    The “CMAKE_<LANG>_LINK_FLAGS” and “CMAKE_<LANG>_LINK_FLAGS_<CONFIG>”
    variables were added to support per-language link flags for all
    target types.
    
    “$<STRING:…>” generator expressions were added for query,
    generation, and transformation operations on strings.

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.83 2026/01/30 10:05:16 adam Exp $
+# $NetBSD: version.mk,v 1.84 2026/03/26 09:00:32 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.2.3
+CMAKE_VERSION=	4.3.0
 CMAKE_API=	${CMAKE_VERSION:R}
