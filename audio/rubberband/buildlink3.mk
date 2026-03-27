026fe69211de9a40e1a927a4f237a649b795be30
pin <pin@pkgsrc.org>
2026-03-27 09:37:22 +0000

    audio/rubberband: update to 4.0.0
    Version 4.0.0 is a major release which adds a new API, RubberBandLiveShifter, which is simpler to use than the general RubberBandStretcher interface in cases where only pitch-shifting is required. For more general purposes the original interface is still the proper one.
    
    The rest of the API is otherwise unchanged, so the library continues to be binary compatible with the 2.x and 1.x releases for existing applications. Code written to use earlier versions of the library can link and run against this version without alteration.

--- audio/rubberband/buildlink3.mk
+++ audio/rubberband/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2025/02/12 06:44:28 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2026/03/27 09:37:22 pin Exp $
 
 BUILDLINK_TREE+=	rubberband
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	rubberband
 RUBBERBAND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rubberband+=	rubberband>=1.8.1
-BUILDLINK_ABI_DEPENDS.rubberband?=	rubberband>=3.1.2nb3
+BUILDLINK_ABI_DEPENDS.rubberband+=	rubberband>=4.0.0
 BUILDLINK_PKGSRCDIR.rubberband?=	../../audio/rubberband
 
 .include "../../audio/ladspa/buildlink3.mk"
