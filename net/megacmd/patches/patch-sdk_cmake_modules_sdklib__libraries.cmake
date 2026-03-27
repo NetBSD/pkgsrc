ee1e512abaea7c084a3e1a595c23cd8725ac9d3e
kikadf <kikadf@pkgsrc.org>
2026-03-27 11:06:04 +0000

    net/megacmd: update to 2.5.0
    Wed Mar 11 10:36:46 AM UTC 2026 - linux@mega.co.nz
    - Update to version 2.5.0:
      * Shell UX improvements: empty Enter shows a new prompt; no duplicate prompt while command is executing
      * Sync engine improvements: prevent re-uploading data when local drive fingerprint has changed
      * put: Fixed -c (autocreate) not working when destination is an absolute path
      * mediainfo: Fixed audio file duration not being displayed
      * Transfers: prevent failures on resumption
      * thumbnail/preview: Print error when the requested file does not exist
      * share: Return success (exit code 0) when no shares exist below the current folder
      * passwd: fixes issues when 2FA is enabled
      * Path and string handling fixes (trailing separators, trimming)
      * Expanded unit test coverage
      * Fix: Improved restart-after-update reliability on POSIX (server PID changes after update)
      * Stability, memory, typos and other improvements
    
    - Use builtin megasdk-10.8.0

--- net/megacmd/patches/patch-sdk_cmake_modules_sdklib__libraries.cmake
+++ net/megacmd/patches/patch-sdk_cmake_modules_sdklib__libraries.cmake
@@ -1,10 +1,11 @@
-$NetBSD: patch-sdk_cmake_modules_sdklib__libraries.cmake,v 1.2 2025/05/16 19:43:58 wiz Exp $
+$NetBSD: patch-sdk_cmake_modules_sdklib__libraries.cmake,v 1.3 2026/03/27 11:06:04 kikadf Exp $
 
 * Fix to find libcrypto++
+* Fix missing icu link library
 
---- sdk/cmake/modules/sdklib_libraries.cmake.orig	2025-04-02 09:16:59.000000000 +0200
+--- sdk/cmake/modules/sdklib_libraries.cmake.orig	2026-02-18 08:41:33.000000000 +0000
 +++ sdk/cmake/modules/sdklib_libraries.cmake
-@@ -91,7 +91,7 @@ macro(load_sdklib_libraries)
+@@ -98,7 +98,7 @@ macro(load_sdklib_libraries)
  
          find_package(PkgConfig REQUIRED) # For libraries loaded using pkg-config
  
@@ -13,3 +14,14 @@ $NetBSD: patch-sdk_cmake_modules_sdklib__libraries.cmake,v 1.2 2025/05/16 19:43:
          target_link_libraries(SDKlib PUBLIC PkgConfig::cryptopp) # TODO: Private for SDK core
  
          pkg_check_modules(sodium REQUIRED IMPORTED_TARGET libsodium)
+@@ -110,8 +110,8 @@ macro(load_sdklib_libraries)
+         pkg_check_modules(curl REQUIRED IMPORTED_TARGET libcurl)
+         target_link_libraries(SDKlib PRIVATE PkgConfig::curl)
+ 
+-        find_package(ICU COMPONENTS uc data REQUIRED)
+-        target_link_libraries(SDKlib PRIVATE ICU::uc ICU::data)
++        find_package(ICU COMPONENTS i18n uc data REQUIRED)
++        target_link_libraries(SDKlib PRIVATE ICU::i18n ICU::uc ICU::data)
+ 
+         if(USE_OPENSSL)
+             find_package(OpenSSL REQUIRED)
