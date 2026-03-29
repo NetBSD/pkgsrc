19168869e1310c7d7f3a90dc19fdb03e44ac02f2
taca <taca@pkgsrc.org>
2026-03-29 07:29:05 +0000

    textproc/ruby-rdtool: update to 0.6.39
    pkgsrc change: remove one pkglint warning.
    
    
    0.6.39 (2026-03-17)
    
    * Modernize packaging metadata, CI, and release workflow for current Ruby.
    * Refactor rd2 internals without breaking external formatter compatibility.
    * Add configurable man section support and improve rd2man output quality.
    * Add a simple HTML5 formatter, and document bundled format libraries.
    * Modernize rdswap and document its purpose.

--- textproc/ruby-rdtool/rdtool.mk
+++ textproc/ruby-rdtool/rdtool.mk
@@ -1,8 +1,8 @@
-# $NetBSD: rdtool.mk,v 1.3 2023/06/06 12:42:34 riastradh Exp $
+# $NetBSD: rdtool.mk,v 1.4 2026/03/29 07:29:05 taca Exp $
 
 .if !defined(RUBY_RDTOOL_MK)
 
-RUBY_RDTOOL_MK	:=
+RUBY_RDTOOL_MK:=
 
 RUBY_RD=	${PREFIX}/bin/rd2${RUBY_SUFFIX}
 TOOL_DEPENDS+=	${RUBY_PKGPREFIX}-rdtool>=0.6.14:../../textproc/ruby-rdtool
