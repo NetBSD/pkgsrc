d82ebc1df3bb1341045b33e0c3ec55c82a268b27
wiz <wiz@pkgsrc.org>
2026-04-02 19:07:00 +0000

    rust-bin: update to 1.94.1.
    Based on wip/rust194-bin by he@

--- lang/rust-bin/buildlink3.mk
+++ lang/rust-bin/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/07/01 07:00:28 pin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/04/02 19:07:00 wiz Exp $
 
 BUILDLINK_TREE+=	rust-bin
 
 .if !defined(RUST_BIN_BUILDLINK3_MK)
 RUST_BIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rust-bin+=	rust-bin>=1.56.1
+BUILDLINK_API_DEPENDS.rust-bin+=	rust-bin>=1.85.0
 BUILDLINK_PKGSRCDIR.rust-bin?=		../../lang/rust-bin
 BUILDLINK_PASSTHRU_DIRS+=		${PREFIX}/lib/rustlib
 .endif
