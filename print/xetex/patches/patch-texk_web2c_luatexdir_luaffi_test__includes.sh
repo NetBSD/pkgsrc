ed162b23fe47e4abf0e4e6e691d32c2e56c2a040
markd <markd@pkgsrc.org>
2026-04-03 03:45:13 +0000

    xetex to 0.999998
    part of TeXlive 2026
    
    * When the “Infinite glue shrinkage” error is ignored (see the 2025 release
      notes), the word “error” is no longer included in the output, and the message
      is written only to the log ﬁle. This was a source of minor confusion and false
      positives.

---
+++ print/xetex/patches/patch-texk_web2c_luatexdir_luaffi_test__includes.sh
@@ -0,0 +1,15 @@
+$NetBSD: patch-texk_web2c_luatexdir_luaffi_test__includes.sh,v 1.1 2026/04/03 03:45:13 markd Exp $
+
+test fix
+
+--- texk/web2c/luatexdir/luaffi/test_includes.sh.orig	2026-02-02 21:53:30.000000000 +0000
++++ texk/web2c/luatexdir/luaffi/test_includes.sh
+@@ -5,7 +5,7 @@ mkdir -p test_includes
+ for f in /usr/include/*.h
+ do
+   gcc -E -c "${f}" > tmp 2>/dev/null
+-  if [ $? == 0 ]
++  if [ $? = 0 ]
+   then
+     echo "${f}";
+     lua -e '
