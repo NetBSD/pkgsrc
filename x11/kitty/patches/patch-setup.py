28c1baa5862acdbb61cb1afedd768829710e36b3
maya <maya@pkgsrc.org>
2026-04-05 16:12:32 +0000

    Pullup ticket #7069 - requested by ktnb x11/kitty: Build fix
    Revisions pulled up:
    - x11/kitty/Makefile                                            1.85
    - x11/kitty/PLIST                                               1.37
    - x11/kitty/distinfo                                            1.40
    - x11/kitty/patches/patch-setup.py                              1.4
    
    ---
       Module Name:    pkgsrc
       Committed By:   ktnb
       Date:           Mon Mar 30 16:55:13 UTC 2026
    
       Modified Files:
               pkgsrc/x11/kitty: Makefile PLIST distinfo
       Added Files:
               pkgsrc/x11/kitty/patches: patch-setup.py
    
       Log Message:
       kitty: fixed build issues on Darwin

---
+++ x11/kitty/patches/patch-setup.py
@@ -0,0 +1,16 @@
+$NetBSD: patch-setup.py,v 1.4.2.2 2026/04/05 16:12:32 maya Exp $
+
+Only point to the pkgsrc fonts for darwin
+
+--- setup.py.orig	2026-03-27 19:19:48.618708465 +0000
++++ setup.py
+@@ -924,7 +924,8 @@ def add_builtin_fonts(args: Options) ->
+         font_file = ''
+         if is_macos:
+             candidates = (
+-                os.path.expanduser('~/Library/Fonts'), '/Library/Fonts', '/System/Library/Fonts', '/Network/Library/Fonts')
++                '@PREFIX@/share/fonts/'
++            )
+             for candidate in candidates:
+                 q = os.path.join(candidate, filename)
+                 if os.path.exists(q):
