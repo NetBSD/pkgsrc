62fd6f2ace3a98aba50607626d1512aef70da1d0
wiz <wiz@pkgsrc.org>
2026-04-07 12:23:36 +0000

    m4: update to 1.4.21.
    * Noteworthy changes in release 1.4.21 (2026-02-06) [stable]
    
    ** Fix the `eval' builtin to reject input like `0x' that C does not
       accept as an integer literal, rather than silently treating it as zero
       (present since "the beginning").
    
    ** Fix the `define' and `pushdef' builtins to always warn when attempting
       to concatenate the result of using the `defn' macro on a builtin macro
       with anything else.  This is a counterpart to the change in m4 1.4.11
       that issues a warning when `defn' with multiple arguments attempts to
       perform concatenation.  Attempting to use the output of `defn' anywhere
       other than a lone argument during macro definition has never been
       portable, but now the outcome is always the remaining text, rather
       than being dependent on whether `defn' was used before or after the
       other text (present since "the beginning").
    
    ** Port to glibc 2.43, which implements functions like strchr as macros
       when compiled with a C23 compiler.
    
    
    * Noteworthy changes in release 1.4.20 (2025-05-10) [stable]
    
    ** Fix a bug in the `eval' builtin where it does not suppress warnings
       about division by zero that occurs within a more complex expression on
       the right hand side of || or && (present since short-circuiting was
       introduced in 1.4.8b).
    
    ** The `syscmd' and `esyscmd' builtins no longer mishandle a command line
       starting with `-' or `+' (present since "the beginning").
    
    ** Fix regression introduced in 1.4.19 where trace output (such as with
       `debugmode(t)') could read invalid memory when tracing a series of
       pushed macros that are popped during argument collection.
    
    ** Fix regression introduced in 1.4.19 where the `format' builtin
       inadvertently took on locale-dependent parsing and output of floating
       point numbers as a side-effect of introducing message translations.
       While it would be nice for m4 to be fully locale-aware, such a behavior
       change belongs in a major version release such as 1.6, and not a minor
       release.
    
    ** Fix regression introduced in 1.4.11 where the experimental `changeword'
       builtin could cause a crash if given a regex that does not match all
       one-byte prefixes of valid longer matches.  As a reminder, `changeword'
       is not recommended for production use, and will likely not be present
       in the next major version release.
    
    ** On non-Unix platforms where binary files differ from text, loading a
       frozen file (which should be cross-platform compatible) now correctly
       uses binary mode.
    
    ** Several documentation improvements to the manual.
    
    ** Update to comply with newer C standards, and inherit portability
       improvements from gnulib.

--- devel/m4/patches/patch-lib_sigsegv.c
+++ devel/m4/patches/patch-lib_sigsegv.c
@@ -1,11 +1,13 @@
-$NetBSD: patch-lib_sigsegv.c,v 1.2 2025/02/18 15:07:14 wiz Exp $
+$NetBSD: patch-lib_sigsegv.c,v 1.3 2026/04/07 12:23:36 wiz Exp $
 
 Use older regnames on MacOS X 10.4 "Tiger", taken from
 <https://github.com/macports/macports-ports/blob/f9641f5f5cb7287e6787b566945fff460a80bc39/devel/m4/files/patch-m4-use-older-regnames-on-tiger.diff>
 
---- lib/sigsegv.c.orig	2021-05-19 14:36:49.000000000 +0000
+Sent to bug-m4 mailing list on 2026-04-07.
+
+--- lib/sigsegv.c.orig	2026-02-03 19:04:18.000000000 +0000
 +++ lib/sigsegv.c
-@@ -33,6 +33,9 @@
+@@ -35,6 +35,9 @@
  # include <sys/param.h> /* defines macro OpenBSD */
  #endif
  
@@ -15,7 +17,7 @@ Use older regnames on MacOS X 10.4 "Tiger", taken from
  
  /* Version number.  */
  int libsigsegv_version = LIBSIGSEGV_VERSION;
-@@ -562,7 +565,11 @@ int libsigsegv_version = LIBSIGSEGV_VERS
+@@ -660,7 +663,11 @@ int libsigsegv_version = LIBSIGSEGV_VERSION;
       - 'ucontext_t' and 'struct __darwin_ucontext' in <sys/_types/_ucontext.h>,
       - 'struct __darwin_mcontext64' in <i386/_mcontext.h>, and
       - 'struct __darwin_x86_thread_state64' in <mach/i386/_structs.h>.  */
@@ -27,7 +29,7 @@ Use older regnames on MacOS X 10.4 "Tiger", taken from
  
  # elif defined __i386__
  
-@@ -570,7 +577,11 @@ int libsigsegv_version = LIBSIGSEGV_VERS
+@@ -668,7 +675,11 @@ int libsigsegv_version = LIBSIGSEGV_VERSION;
       - 'ucontext_t' and 'struct __darwin_ucontext' in <sys/_types/_ucontext.h>,
       - 'struct __darwin_mcontext32' in <i386/_mcontext.h>, and
       - 'struct __darwin_i386_thread_state' in <mach/i386/_structs.h>.  */
@@ -38,16 +40,4 @@ Use older regnames on MacOS X 10.4 "Tiger", taken from
 +# endif
  
  # elif defined __arm64__
- 
-@@ -586,7 +597,11 @@ int libsigsegv_version = LIBSIGSEGV_VERS
-      - 'ucontext_t' and 'struct __darwin_ucontext' in <sys/_structs.h>,
-      - 'struct __darwin_mcontext' in <ppc/_structs.h>, and
-      - 'struct __darwin_ppc_thread_state' in <mach/ppc/_structs.h>.  */
-+# if __MAC_OS_X_VERSION_MAX_ALLOWED < 1050
-+#  define SIGSEGV_FAULT_STACKPOINTER  ((ucontext_t *) ucp)->uc_mcontext->ss.r1
-+# else
- #  define SIGSEGV_FAULT_STACKPOINTER  ((ucontext_t *) ucp)->uc_mcontext->__ss.__r1
-+#endif
- 
- # endif
  
