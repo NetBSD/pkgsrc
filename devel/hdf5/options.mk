81ba6fdcfd716b296583631ab97d771f1c392179
wiz <wiz@pkgsrc.org>
2026-04-09 07:11:41 +0000

    hdf5: add mpi option
    While here:
    - ignore a file for shell checking because it's a bash script
    - comment out unwrap SUBST block because it fails to do anything here

--- devel/hdf5/options.mk
+++ devel/hdf5/options.mk
@@ -1,13 +1,22 @@
-# $NetBSD: options.mk,v 1.14 2025/03/19 09:18:14 adam Exp $
+# $NetBSD: options.mk,v 1.15 2026/04/09 07:11:41 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.hdf5
-PKG_SUPPORTED_OPTIONS=	szip threads hdf5-unsafe-threads
+PKG_SUPPORTED_OPTIONS=	mpi szip threads hdf5-unsafe-threads
 PKG_SUGGESTED_OPTIONS=	szip
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	mpi
+.if !empty(PKG_OPTIONS:Mmpi)
+PLIST.mpi=	yes
+CMAKE_CONFIGURE_ARGS+=	-DHDF5_ENABLE_PARALLEL=ON
+.include "../../parallel/openmpi/buildlink3.mk"
+.else
+CMAKE_CONFIGURE_ARGS+=	-DHDF5_ENABLE_PARALLEL=OFF
+.endif
+
 .if !empty(PKG_OPTIONS:Mszip)
-CONFIGURE_ARGS+=	--with-szlib
+CMAKE_CONFIGURE_ARGS+=	-DHDF5_ENABLE_SZIP_SUPPORT=ON
 .include "../../archivers/libaec/buildlink3.mk"
 .endif
 
@@ -16,6 +25,7 @@ PLIST_VARS+=	hl
 .if !empty(PKG_OPTIONS:Mthreads)
 CMAKE_CONFIGURE_ARGS+=	-DHDF5_BUILD_HL_LIB=OFF # not compatible with threadsafe
 CMAKE_CONFIGURE_ARGS+=	-DHDF5_ENABLE_THREADSAFE=ON
+#SUBST_FILES.unwrap+=	${DESTDIR}${PREFIX}/bin/h5hlcc
 .include "../../mk/pthread.buildlink3.mk"
 .else
 PLIST.hl=	yes
