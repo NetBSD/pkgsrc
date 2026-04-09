604a0a6d9b906cdf8ad369e353edde346a55d532
wiz <wiz@pkgsrc.org>
2026-04-09 05:59:30 +0000

    libunistring: add upstream bug report URL

--- textproc/libunistring/patches/patch-tests_pselect.c
+++ textproc/libunistring/patches/patch-tests_pselect.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-tests_pselect.c,v 1.1 2025/10/05 14:51:09 js Exp $
+$NetBSD: patch-tests_pselect.c,v 1.2 2026/04/09 05:59:30 wiz Exp $
 
 Fix compiling on QNX.
+https://savannah.gnu.org/bugs/index.php?68222
 
 --- tests/pselect.c.orig	2024-01-29 00:58:12.000000000 +0000
 +++ tests/pselect.c
