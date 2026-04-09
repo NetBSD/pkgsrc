bc1c9ef36a5ddd51f93f67aed9be5d446933f073
pin <pin@pkgsrc.org>
2026-04-09 17:15:03 +0000

    editors/redox: update to 0.3.2
    What's Changed
    
        Revamp motion parsing system and add some new motions by @JackDerksen in #17
        Extend motion tree and add more motions by @JackDerksen in #18
        Search primitives by @JackDerksen in #19

--- editors/redox/cargo-depends.mk
+++ editors/redox/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.3 2026/03/27 11:19:19 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.4 2026/04/09 17:15:03 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.4
