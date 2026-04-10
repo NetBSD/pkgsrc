b571317767b413a949fa5b41edc8eb0ed5a4dab9
pin <pin@pkgsrc.org>
2026-04-10 18:29:41 +0000

    editors/reovim: fix patchsum
    While here, allow building on SmartOS.

--- editors/reovim/patches/patch-server_lib_drivers_module-loader_src_discovery.rs
+++ editors/reovim/patches/patch-server_lib_drivers_module-loader_src_discovery.rs
@@ -1,34 +1,22 @@
-$NetBSD: patch-server_lib_drivers_module-loader_src_discovery.rs,v 1.1 2026/04/09 17:16:54 pin Exp $
+$NetBSD: patch-server_lib_drivers_module-loader_src_discovery.rs,v 1.2 2026/04/10 18:29:41 pin Exp $
 
-Allow compilation on *BSD.
+Allow building on other *nix.
 
---- server/lib/drivers/module-loader/src/discovery.rs.orig	2026-04-09 12:34:36.950570004 +0000
-+++ server/lib/drivers/module-loader/src/discovery.rs
-@@ -65,17 +65,18 @@ pub const fn library_extension() -> &'static str {
+--- ../reovim-0.14.4/server/lib/drivers/module-loader/src/discovery.rs.orig	2026-04-10 18:15:39.711016518 +0000
++++ ../reovim-0.14.4/server/lib/drivers/module-loader/src/discovery.rs
+@@ -64,7 +64,14 @@ pub const fn library_extension() -> &'static str {
+ /// Get shared library extension for the current platform.
  #[must_use]
  pub const fn library_extension() -> &'static str {
-     #[cfg(target_os = "linux")]
--    {
--        "so"
--    }
-+    { "so" }
-     #[cfg(target_os = "macos")]
--    {
--        "dylib"
--    }
-+    { "dylib" }
-     #[cfg(target_os = "windows")]
--    {
--        "dll"
--    }
-+    { "dll" }
+-    #[cfg(target_os = "linux")]
 +    #[cfg(any(
++        target_os = "linux",
 +        target_os = "netbsd",
 +        target_os = "freebsd",
 +        target_os = "openbsd",
 +        target_os = "dragonfly",
++        target_os = "solaris",
 +    ))]
-+    { "so" }
- }
- 
- /// Get library filename for a module name.
+     {
+         "so"
+     }
