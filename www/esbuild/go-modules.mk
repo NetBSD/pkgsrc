064fc78171d2fb40bac644879eaede01ccbf1ae4
kikadf <kikadf@pkgsrc.org>
2026-04-10 17:24:31 +0000

    www/esbuild: update to 0.28.0
    * 0.28.0
    - Add support for with { type: 'text' } imports (#4435)
    - Add integrity checks to fallback download path (#4343)
    - Update the Go compiler from 1.25.7 to 1.26.1
    
    * 0.27.7
    - Fix lowering of define semantics for TypeScript parameter properties (#4421)

--- www/esbuild/go-modules.mk
+++ www/esbuild/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.6 2026/02/26 11:35:58 kikadf Exp $
+# $NetBSD: go-modules.mk,v 1.7 2026/04/10 17:24:31 kikadf Exp $
 
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
