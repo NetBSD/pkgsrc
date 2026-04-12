8ab19bfbc340530e82c38b0ecc6758a9584d0a0d
tsutsui <tsutsui@pkgsrc.org>
2026-04-12 15:10:28 +0000

    mikutter: update to 5.1.2
    Upstream changes:
     https://mikutter.hatenablog.com/entry/2026/04/12/183234
    
    * belated Happy New Year
    * enable YJIT only when available in the system Ruby
     * thanks Izumi Tsutsui
    * allow Addressable 2.9
    * fix emoji rendering shifted downward
     * thanks Izumi Tsutsui

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,11 +1,9 @@
-$NetBSD: patch-mikutter.rb,v 1.13 2026/04/07 14:32:54 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.14 2026/04/12 15:10:28 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
-- fix crash on startup when YJIT is not enabled in Ruby
-  https://dev.mikutter.hachune.net/issues/1606
 
---- mikutter.rb.orig	2025-02-08 12:21:47.000000000 +0000
+--- mikutter.rb.orig	2026-04-12 08:53:44.000000000 +0000
 +++ mikutter.rb
 @@ -1,7 +1,5 @@
 -#!/bin/sh
@@ -15,13 +13,4 @@ $NetBSD: patch-mikutter.rb,v 1.13 2026/04/07 14:32:54 tsutsui Exp $
 -#!ruby
  =begin rdoc
  = mikutter - simple, powerful and moeful Mastodon client
- Copyright (C) 2009-2024 Toshiaki Asai
-@@ -61,7 +59,7 @@ Plugin.call(:boot, nil)
- # _profile_ がtrueなら、プロファイリングした結果を一時ディレクトリに保存する
- def boot!(profile)
-   begin
--    RubyVM::YJIT.enable if RUBY_VERSION.split('.') >= %w[3 3]
-+    RubyVM::YJIT.enable if RUBY_VERSION.split('.') >= %w[3 3] && defined?(RubyVM::YJIT)
-     if profile
-       require 'ruby-prof'
-       begin
+ Copyright (C) 2009-2026 Toshiaki Asai
