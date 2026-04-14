b983297a3ed5d18ae30257b94123a1f05e684fec
adam <adam@pkgsrc.org>
2026-04-14 12:49:12 +0000

    jemalloc: updated to 5.3.1
    5.3.1 (Apr 13, 2026)
    
    This release includes over 390 commits spanning bug fixes, new features,
    performance optimizations, and portability improvements.  Multiple percent
    of system-level metric improvements were measured in tested production
    workloads.  The release has gone through large-scale production testing
    at Meta.

--- devel/jemalloc/buildlink3.mk
+++ devel/jemalloc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2022/09/26 18:02:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/04/14 12:49:12 adam Exp $
 
 BUILDLINK_TREE+=	jemalloc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	jemalloc
 JEMALLOC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jemalloc+=	jemalloc>=4.0.0
-BUILDLINK_ABI_DEPENDS.jemalloc?=	jemalloc>=5.2.1nb4
+BUILDLINK_ABI_DEPENDS.jemalloc+=	jemalloc>=5.2.1nb4
 BUILDLINK_PKGSRCDIR.jemalloc?=		../../devel/jemalloc
 .endif	# JEMALLOC_BUILDLINK3_MK
 
