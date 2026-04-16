ffdf16f16004f35757d64f22fadc3231bc059fa0
morr <morr@pkgsrc.org>
2026-04-16 21:11:01 +0000

    Update to version 9.2.0357.
    Changes:
    - patch 9.2.0357: [security]: command injection via backticks in tag files
    - patch 9.2.0356: Cannot apply 'scrolloff' context lines at end of file
    - patch 9.2.0355: runtime(tar): missing path traversal checks in tar#Extract()
    - patch 9.2.0354: filetype: not all Bitbake include files are recognized
    - runtime(bitbake): support forward-slashes in bitbake varflags
    - patch 9.2.0353: Missing out-of-memory check in register.c
    - runtime(doc): Tweak documentation style in channel.txt
    - patch 9.2.0352: 'winhighlight' of left window blends into right window
    - patch 9.2.0351: repeat_string() can be improved
    - runtime(zip): also block single leading slash and absolute paths in Extract
    - patch 9.2.0350: Enabling modelines poses a risk
    - patch 9.2.0349: cannot style non-current window separator
    - patch 9.2.0348: potential buffer underrun when setting statusline like option
    - CI: Separate out ASan tests
    - patch 9.2.0347: Vim9: script-local variable not found
    - patch 9.2.0346: Wrong cursor position when entering command line window
    - patch 9.2.0345: Wrong autoformatting with 'autocomplete'
    - patch 9.2.0344: channel: ch_listen() can bind to network interface
    - runtime(doc): Tweak documentation style in options.txt
    - patch 9.2.0343: tests: test_clientserver may fail on slower systems
    - patch 9.2.0342: tests: test_excmd.vim leaves swapfiles behind
    - patch 9.2.0341: some functions can be run from the sandbox
    - runtime(zip): Detect path traversal issues on Windows
    - Revert "runtime(jjdescription): allow to configure summary width"
    - patch 9.2.0340: pum_redraw() may cause flicker
    - Fix a few typos
    - patch 9.2.0339: regexp: nfa_regmatch() allocates and frees too often
    - runtime(jjdescription): allow to configure summary width
    - patch 9.2.0338: Cannot handle mouseclicks in the tabline
    - patch 9.2.0337: list indexing broken on big-endian 32-bit platforms
    - runtime(hlyank): verify winid in lambda before matchdelete()
    - patch 9.2.0336: libvterm: no terminal reflow support
    - patch 9.2.0335: json_encode() uses recursive algorithm
    - patch 9.2.0334: GTK: window geometry shrinks with with client-side decorations
    - patch 9.2.0333: filetype: PklProject files are not recognized
    - runtime(doc): Update documentation on statusline click handler
    - patch 9.2.0332: popup: still opacity rendering issues
    - patch 9.2.0331: spellfile: stack buffer overflows in spell file generation
    - patch 9.2.0330: tests: some patterns in tar and zip plugin tests not strict enough
    - patch 9.2.0329: tests: test_indent.vim leaves swapfiles behind
    - patch 9.2.0328: Cannot handle mouseclicks in the statusline
    - patch 9.2.0327: filetype: uv scripts are not detected
    - runtime(jjdescription): Add highlighting for 'Renamed' status lines
    - runtime(jjdescription): Anchor status matches to start of line
    - patch 9.2.0326: runtime(tar): but with dotted path
    - patch 9.2.0325: runtime(tar): bug in zstd handling
    - patch 9.2.0324: 0x9b byte not unescaped in <Cmd> mapping
    - runtime(doc): Tweak documentation style a bit
    - runtime(org): Link to generic bold/Italic highlighting groups
    - runtime(help): support highlighting groups in translated syntax doc
    - runtime(vimball): detect more path traversal attacks
    - patch 9.2.0323: filetype: buf.lock files are not recognized
    - patch 9.2.0322: tests: test_popupwin fails
    - patch 9.2.0321: MS-Windows: No OpenType font support
    - patch 9.2.0320: several bugs with text properties

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.165 2026/04/07 20:56:26 morr Exp $
+# $NetBSD: version.mk,v 1.166 2026/04/16 21:11:01 morr Exp $
 
 VIM_VERSION=		9.2
-VIM_PATCHLEVEL=		0319
+VIM_PATCHLEVEL=		0357
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s"
 VIM_SUBDIR=		vim92
