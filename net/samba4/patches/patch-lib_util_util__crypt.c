d96c0f51d0f1557ea3520bc23ea30edf48c33974
adam <adam@pkgsrc.org>
2026-04-17 08:35:42 +0000

    samba4: updated to 4.24.1
    Changes since 4.24.0
    
    * BUG 16057: autobuild fails if /proc/version contains trailing space
    * BUG 16035: use after free in streams_xattr_connect()
    * BUG 16042: rpc workers with long living clients grow server  memory keytab
    * BUG 16058: vfs_snapper failing to access or enumerate files in subfolders
    * BUG 16040: Samba is not build with FORTIFY_SOURCE
    * BUG 16055: Fix tests with MIT Kerberos 1.22.x
    
    4.24.0
    
    NEW FEATURES/CHANGES
    * Authentication information audit support
    * vfs_streams_xattr can hold larger streams
    * Support for remote password management (Entra ID SSPR, Keycloak)
    * Kerberos PKINIT KeyTrust logon support
    * msDS-KeyCredentialLink validation
    * Kerberos PKINIT strong/flexible key mappings
    * Kerberos PKINIT SID extension
    * KDC includes PAC by default
    * KDC can insist clients request canonicalization
    * KDC can avoid potentially confusing canonicalization
    * KDC provides Kerberos acceptors with canonical client names

--- net/samba4/patches/patch-lib_util_util__crypt.c
+++ net/samba4/patches/patch-lib_util_util__crypt.c
@@ -1,22 +1,10 @@
-$NetBSD: patch-lib_util_util__crypt.c,v 1.2 2025/06/19 02:08:47 gutteridge Exp $
-
-Do not include <crypt.h> unconditionally.
+$NetBSD: patch-lib_util_util__crypt.c,v 1.3 2026/04/17 08:35:42 adam Exp $
 
 Account for platforms that don't define ENOTRECOVERABLE, like NetBSD
 before 10.0.
 
 --- lib/util/util_crypt.c.orig	2025-02-17 15:48:20.606155400 +0000
 +++ lib/util/util_crypt.c
-@@ -2,7 +2,9 @@
- #include "data_blob.h"
- #include "discard.h"
- #include <talloc.h>
-+#ifdef HAVE_CRYPT_H
- #include <crypt.h>
-+#endif
- #include "util_crypt.h"
- 
- 
 @@ -62,7 +64,11 @@ static int crypt_as_best_we_can(TALLOC_C
  	if (hash == NULL || hash[0] == '*') {
  		if (ret == 0) {
