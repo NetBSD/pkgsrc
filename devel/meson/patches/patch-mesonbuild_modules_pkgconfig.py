042157bd006b9fe582f1ff39757040ac0b2a4c3c
wiz <wiz@pkgsrc.org>
2026-04-19 17:14:21 +0000

    meson: update to 1.11.0.
    # New features
    ## BuildTarget(install_dir) length > 1 replaced with keywords
    ## Cargo workspace object
    ## Cython no longer requires explicitly enabling C or C++
    ## Deduplication of OpenMP linker arguments
    ## `meson dist` now accepts `-j`/`--num-processes`
    ## Deprecate `should_fail` and rename it to `expected_fail`, also introduce `expected_exitcode`
    ## The external_project module uses the cygpath command to convert paths
    ## install_man and install_headers: add support for install_tag kwarg
    ## Added `link_early_args` to targets performing linking
    ## Machine files now expand `~` as the user's home directory
    ## `meson format` file sorting is now disabled by default and uses natural sorting
    ## `-Db_lto` and `-Db_pgo` now supported for MSVC
    ## Last major version supporting Python 3.7, 3.8, and 3.9
    ## Python extension modules default to C ABI for Rust
    ## Meson now defines `QT_DEBUG` or `QT_NO_DEBUG` depending on build type
    ## `compiler_target()` method in the Rust module
    ## Change to handling of linker arguments for Rust
    ## XC32 support now aware of v5.00 features
    ## windows.compile_resources now detects header changes with rc.exe
    ## Added `implicit_include_directories` argument to `windows.compile_resources`

--- devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
+++ devel/meson/patches/patch-mesonbuild_modules_pkgconfig.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.9 2025/04/29 10:46:11 wiz Exp $
+$NetBSD: patch-mesonbuild_modules_pkgconfig.py,v 1.10 2026/04/19 17:14:21 wiz Exp $
 
 Revert https://github.com/mesonbuild/meson/commit/aba8792 that introduced
 a condition to match FreeBSD path norm for pkg-config files.
 
 Also undo haiku-specific change.
 
---- mesonbuild/modules/pkgconfig.py.orig	2025-04-28 15:36:57.000000000 +0000
+--- mesonbuild/modules/pkgconfig.py.orig	2026-04-13 14:03:15.000000000 +0000
 +++ mesonbuild/modules/pkgconfig.py
-@@ -702,10 +702,10 @@ class PkgConfigModule(NewExtensionModule
-         pkgroot = pkgroot_name = kwargs['install_dir'] or default_install_dir
+@@ -778,10 +778,10 @@ class PkgConfigModule(NewExtensionModule):
+             pkgroot_name = pkgroot.optname
          if pkgroot is None:
              m = state.environment.machines.host
 -            if m.is_freebsd():
