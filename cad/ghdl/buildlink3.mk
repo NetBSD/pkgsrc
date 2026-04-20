0c26f25cbd79d1a4e7aff6f0f6c4466afd0e1ee6
wiz <wiz@pkgsrc.org>
2026-04-20 08:21:29 +0000

    *: recursive bump for so name change in ghdl

--- cad/ghdl/buildlink3.mk
+++ cad/ghdl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/12/25 16:39:59 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/04/20 08:21:29 wiz Exp $
 
 BUILDLINK_TREE+=	ghdl
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ghdl
 GHDL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ghdl+=	ghdl>=5.1.1
+BUILDLINK_ABI_DEPENDS.ghdl?=	ghdl>=6.0.0
 BUILDLINK_PKGSRCDIR.ghdl?=	../../cad/ghdl
 
 .include "../../lang/gcc14-gnat/buildlink3.mk"
