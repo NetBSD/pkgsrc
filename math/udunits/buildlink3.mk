ddf6bad434a502398cda11020f87ed5d285412c5
wiz <wiz@pkgsrc.org>
2026-04-20 05:57:14 +0000

    udunits: fix pkglint

--- math/udunits/buildlink3.mk
+++ math/udunits/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2024/01/11 11:29:47 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2026/04/20 05:57:14 wiz Exp $
 
 BUILDLINK_TREE+=	udunits
 
@@ -7,8 +7,7 @@ UDUNITS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.udunits+=	udunits>=2.2.26
 BUILDLINK_PKGSRCDIR.udunits?=	../../math/udunits
-.endif	# UDUNITS_BUILDLINK3_MK
-
 .include "../../textproc/expat/buildlink3.mk"
+.endif	# UDUNITS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-udunits
