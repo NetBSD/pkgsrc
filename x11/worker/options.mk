76f3e5e8c15d5b793f5cfb78823f2ae13520492d
vins <vins@pkgsrc.org>
2026-04-22 18:00:46 +0000

    x11/worker: update to 5.4.0
    # pkgsrc changes
    * Add optional AVFS support (default enabled).
    * Take maintainership.
    
    # noteworthy upstream changes
    * Add internal image viewer in the show image mode.
    * Add search result preview in the search window.
    * Add new command to copy file content to clipboard.
    * The clockbar can now be customized.
    * The copy command now supports more overwrite modes.
    * Introduce history for search expressions.

--- x11/worker/options.mk
+++ x11/worker/options.mk
@@ -1,11 +1,20 @@
-# $NetBSD: options.mk,v 1.1 2025/05/25 18:18:07 vins Exp $
+# $NetBSD: options.mk,v 1.2 2026/04/22 18:00:46 vins Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.worker
-PKG_SUPPORTED_OPTIONS=	dbus lua inotify ssl xft2
-PKG_SUGGESTED_OPTIONS=	dbus ssl xft2
+PKG_SUPPORTED_OPTIONS=	avfs dbus lua inotify ssl xft2
+PKG_SUGGESTED_OPTIONS=	avfs dbus ssl xft2
 
 .include "../../mk/bsd.options.mk"
 
+# AVFS backend
+.if !empty(PKG_OPTIONS:Mavfs)
+CONFIGURE_ARGS+=        --with-avfs
+.  include "../../filesystems/avfs/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --without-avfs
+.endif
+
+# Xft support
 .if !empty(PKG_OPTIONS:Mxft2)
 CONFIGURE_ARGS+=	--enable-xft
 .include	"../../x11/libXft/buildlink3.mk"
@@ -13,6 +22,7 @@ CONFIGURE_ARGS+=	--enable-xft
 CONFIGURE_ARGS+=	--disable-xft
 .endif
 
+# Lua scripting
 .if !empty(PKG_OPTIONS:Mlua)
 CONFIGURE_ARGS+=	--enable-lua
 .include	"../../lang/lua/buildlink3.mk"
@@ -20,6 +30,7 @@ CONFIGURE_ARGS+=	--enable-lua
 CONFIGURE_ARGS+=	--disable-lua
 .endif
 
+# DBUS device handling
 .if !empty(PKG_OPTIONS:Mdbus)
 CONFIGURE_ARGS+=        --with-dbus
 .  include "../../sysutils/dbus/buildlink3.mk"
@@ -27,10 +38,12 @@ CONFIGURE_ARGS+=        --with-dbus
 CONFIGURE_ARGS+=        --without-dbus
 .endif
 
+# Checksumming support
 .if !empty(PKG_OPTIONS:Mssl)
 .include	"../../security/openssl/buildlink3.mk"
 .endif
 
+# Inotify support
 .if !empty(PKG_OPTIONS:Minotify)
 CONFIGURE_ARGS+=	--enable-inotify
 .  include	"../../devel/libinotify/buildlink3.mk"
