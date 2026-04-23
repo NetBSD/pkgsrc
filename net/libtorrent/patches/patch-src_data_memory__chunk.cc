316d814620dfeb139edc68fdfb663dd51a233b26
adam <adam@pkgsrc.org>
2026-04-23 13:51:33 +0000

    libtorrent rtorrent: updated to 0.16.10
    0.16.10
    
    Adds support for gzip accepted_encoding in scgi headers and special syntax to
    config files to enable logging before libtorrent initialization.

--- net/libtorrent/patches/patch-src_data_memory__chunk.cc
+++ net/libtorrent/patches/patch-src_data_memory__chunk.cc
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_data_memory__chunk.cc,v 1.1 2023/11/06 11:26:45 nia Exp $
+$NetBSD: patch-src_data_memory__chunk.cc,v 1.2 2026/04/23 13:51:33 adam Exp $
 
 Fix Sun-specific definition of madvise.
 
---- src/data/memory_chunk.cc.orig	2019-07-19 11:44:08.000000000 +0000
+--- src/data/memory_chunk.cc.orig	2026-04-23 08:31:38.000000000 +0000
 +++ src/data/memory_chunk.cc
-@@ -45,7 +45,7 @@
- #include "memory_chunk.h"
+@@ -8,7 +8,7 @@
  
  #ifdef __sun__
+ 
 -extern "C" int madvise(void *, size_t, int);
 +extern "C" int madvise(caddr_t, size_t, int);
  //#include <sys/mman.h>
