343c45ba99397ea1e4f372f58bb11fe4003140df
pin <pin@pkgsrc.org>
2026-04-25 19:07:00 +0000

    textproc/rumdl: update to 0.1.81
    Added
    
        md057: add relative_to_roots option for absolute link validation (e911919)
        md024: treat headings with different {#custom-id} anchors as distinct (0b4dadb)
    
    Fixed
    
        md013: don't warn on under-limit list items in normalize mode (3e08094)
        fmt: honor
        opt-in across all flavors (4c9c8dc)
        docs: restore grid-cards formatting on landing page (5acba85)
        md005,md032: preserve nesting under tight continuation followed by sub-list (993c0f5)

--- textproc/rumdl/cargo-depends.mk
+++ textproc/rumdl/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.10 2026/04/23 13:15:24 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.11 2026/04/25 19:07:00 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	ahash-0.8.12
