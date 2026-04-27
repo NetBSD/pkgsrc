2e3d480fbad026debf557fa3d6cf9f0d6b04fa06
adam <adam@pkgsrc.org>
2026-04-27 10:25:01 +0000

    qemu: updated to 11.0.0
    11.0.0
    https://wiki.qemu.org/ChangeLog/11.0

--- emulators/qemu/patches/patch-target_arm_tcg_translate-sve.c
+++ emulators/qemu/patches/patch-target_arm_tcg_translate-sve.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-target_arm_tcg_translate-sve.c,v 1.3 2025/08/27 06:27:03 adam Exp $
+$NetBSD: patch-target_arm_tcg_translate-sve.c,v 1.4 2026/04/27 10:25:01 adam Exp $
 
 Avoid FSCALE define, notably on SunOS.
 
---- target/arm/tcg/translate-sve.c.orig	2025-08-26 18:32:38.000000000 +0000
+--- target/arm/tcg/translate-sve.c.orig	2026-04-21 19:36:26.000000000 +0000
 +++ target/arm/tcg/translate-sve.c
-@@ -4238,6 +4238,9 @@ TRANS_FEAT_NONSTREAMING(FTSMUL, aa64_sve
+@@ -4241,6 +4241,9 @@ TRANS_FEAT_NONSTREAMING(FTSMUL, aa64_sve
                 s->fpcr_ah ? name##_ah_zpzz_fns[a->esz] :                \
                 name##_zpzz_fns[a->esz], a)
  
 +#ifdef FSCALE
 +#undef FSCALE
 +#endif
- DO_ZPZZ_FP_B16(FADD_zpzz, aa64_sve, sve_fadd)
- DO_ZPZZ_FP_B16(FSUB_zpzz, aa64_sve, sve_fsub)
- DO_ZPZZ_FP_B16(FMUL_zpzz, aa64_sve, sve_fmul)
+ DO_ZPZZ_FP_B16(FADD_zpzz, aa64_sme_or_sve, sve_fadd)
+ DO_ZPZZ_FP_B16(FSUB_zpzz, aa64_sme_or_sve, sve_fsub)
+ DO_ZPZZ_FP_B16(FMUL_zpzz, aa64_sme_or_sve, sve_fmul)
