faa87b05029aafdf35fb92ee37465481e49ba7e5
pin <pin@pkgsrc.org>
2026-04-28 07:54:39 +0000

    sysutils/lxqt-menu-data: update to 2.4.0
    lxqt-menu-data-2.4.0 / 2026-04-20
    =================================
    * Updated translations and bumped the version.

--- sysutils/lxqt-menu-data/buildlink3.mk
+++ sysutils/lxqt-menu-data/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/01/22 11:08:06 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/04/28 07:54:39 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-menu-data
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	lxqt-menu-data
 LXQT_MENU_DATA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.lxqt-menu-data+=	lxqt-menu-data>=2.1.0
-BUILDLINK_ABI_DEPENDS.lxqt-menu-data+=	lxqt-menu-data>=2.1.0
+BUILDLINK_ABI_DEPENDS.lxqt-menu-data?=	lxqt-menu-data>=2.4.0
 BUILDLINK_PKGSRCDIR.lxqt-menu-data?=	../../sysutils/lxqt-menu-data
 
 BUILDLINK_FILES.lxqt-menu-data+=	share/cmake/*
