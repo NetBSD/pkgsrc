44a7d2e6df521b6328f608d5b9e1a6f2e3c3013e
jperkin <jperkin@pkgsrc.org>
2026-04-28 15:48:45 +0000

    opendmarc: Default to mariadb instead of mysql.
    The pkgsrc default is mariadb so it makes sense to avoid conflicts.  While
    here fix pkglint.  Bump PKGREVISION.

--- mail/opendmarc/options.mk
+++ mail/opendmarc/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2026/01/18 17:00:44 ryoon Exp $
+# $NetBSD: options.mk,v 1.2 2026/04/28 15:48:45 jperkin Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.opendmarc
+PKG_OPTIONS_VAR=		PKG_OPTIONS.opendmarc
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=	mysql mariadb
-PKG_SUGGESTED_OPTIONS=	mysql
+PKG_OPTIONS_GROUP.db=		mysql mariadb
+PKG_SUGGESTED_OPTIONS=		mariadb
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,9 +11,9 @@ PKG_SUGGESTED_OPTIONS=	mysql
 ### Backend database support
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
-DEPENDS+=	p5-DBD-mysql-[0-9]*:../../databases/p5-DBD-mysql
+DEPENDS+=		p5-DBD-mysql-[0-9]*:../../databases/p5-DBD-mysql
 CONFIGURE_ARGS+=	--with-sql-backend=mysql
 .elif !empty(PKG_OPTIONS:Mmariadb)
-DEPENDS+=	p5-DBD-MariaDB-[0-9]*:../../databases/p5-DBD-MariaDB
+DEPENDS+=		p5-DBD-MariaDB-[0-9]*:../../databases/p5-DBD-MariaDB
 CONFIGURE_ARGS+=	--with-sql-backend=MariaDB
 .endif
