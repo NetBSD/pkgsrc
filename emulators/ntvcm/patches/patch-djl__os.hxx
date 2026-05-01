29a7489cc242fe10f2c0ec4805717c65194dba9f
hauke <hauke@pkgsrc.org>
2026-05-01 19:52:47 +0000

    emulators/ntvcm: Update to 20260329 HEAD
    Minor changes
    Make package version number regular

--- emulators/ntvcm/patches/patch-djl__os.hxx
+++ emulators/ntvcm/patches/patch-djl__os.hxx
@@ -1,15 +1,15 @@
-$NetBSD: patch-djl__os.hxx,v 1.1 2025/07/04 16:30:12 hauke Exp $
+$NetBSD: patch-djl__os.hxx,v 1.2 2026/05/01 19:52:47 hauke Exp $
 
 NetBSD does not have sched_setaffinity()
 
---- djl_os.hxx.orig	2025-07-02 22:41:46.000000000 +0000
+--- djl_os.hxx.orig	2026-03-29 16:33:15.000000000 +0000
 +++ djl_os.hxx
-@@ -87,7 +87,7 @@ extern "C" int nanosleep( const struct t
+@@ -86,7 +86,7 @@
  
      inline void set_process_affinity( uint64_t processAffinityMask )
      {
--#if !defined(__APPLE__) && !defined( OLDGCC ) && !defined( M68K )
-+#if !defined(__APPLE__) && !defined( OLDGCC ) && !defined( M68K ) && !defined( __NetBSD__ )
+-#if !defined(__APPLE__) && !defined( __mc68000__ )
++#if !defined(__APPLE__) && !defined( __mc68000__ ) && !defined( __NetBSD__ )
          cpu_set_t mask;
          CPU_ZERO( &mask );
  
