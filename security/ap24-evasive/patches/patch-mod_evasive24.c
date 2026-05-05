dadc9effec3a1c9b9fa8ba1e4dd9510034bbcbfc
tm <tm@pkgsrc.org>
2026-05-05 10:19:42 +0000

    ap24-evasive: fix SunOS build

--- security/ap24-evasive/patches/patch-mod_evasive24.c
+++ security/ap24-evasive/patches/patch-mod_evasive24.c
@@ -1,23 +1,25 @@
-$NetBSD: patch-mod_evasive24.c,v 1.1 2025/05/30 17:31:48 jschauma Exp $
+$NetBSD: patch-mod_evasive24.c,v 1.2 2026/05/05 10:19:43 tm Exp $
 
 Patch in an s6_addr32 definition for NetBSD, and specify
 sendmail(1) as the mailer, since the module attempts to
 write headers to stdin.
 
---- mod_evasive24.c.orig	2025-02-16 11:36:05.000000000 -0500
-+++ mod_evasive24.c	2025-05-29 22:23:46.266203700 -0400
-@@ -31,6 +31,10 @@
+--- mod_evasive24.c.orig	Sun Feb 16 16:36:05 2025
++++ mod_evasive24.c	Tue May  5 08:16:45 2026
+@@ -31,6 +31,12 @@
  #include <errno.h>
  #include <unistd.h>  // getpid(2)
  
 +#ifdef __NetBSD__
 +#define s6_addr32 __u6_addr.__u6_addr32 /* XXX nonstandard */
++#elif defined(__sun)
++#define s6_addr32 _S6_un._S6_u32
 +#endif
 +
  #define PCRE2_CODE_UNIT_WIDTH 8
  #include <pcre2.h>
  
-@@ -45,7 +49,13 @@
+@@ -45,7 +51,13 @@
  
  AP_DECLARE_MODULE(evasive);
  
