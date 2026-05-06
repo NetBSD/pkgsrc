b8bc5c044c5ad7b0ce4427fcf595369cb66f577e
vins <vins@pkgsrc.org>
2026-05-06 17:08:48 +0000

    filesystems/fuse: update to 3.18.2
    Changelog too long. Refer to
    https://github.com/libfuse/libfuse/blob/master/ChangeLog.rst

--- filesystems/fuse/buildlink3.mk
+++ filesystems/fuse/buildlink3.mk
@@ -1,19 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.7 2023/09/08 06:01:04 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2026/05/06 17:08:48 vins Exp $
 
 BUILDLINK_TREE+=	fuse
 
 .if !defined(FUSE_BUILDLINK3_MK)
 FUSE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fuse+=	fuse>=2.5.2
-BUILDLINK_ABI_DEPENDS.fuse+=	fuse>=2.9.9nb1
+BUILDLINK_API_DEPENDS.fuse+=	fuse>=3.10
+BUILDLINK_API_DEPENDS.fuse+=	fuse>=3.18.2
 BUILDLINK_PKGSRCDIR.fuse?=	../../filesystems/fuse
 
 .include "../../mk/bsd.fast.prefs.mk"
 
 .if ${OPSYS} == "NetBSD"
-.include "../../filesystems/perfuse/buildlink3.mk"
+.  include "../../filesystems/perfuse/buildlink3.mk"
+.elif ${OPSYS} == "Linux"
+.  include "../../devel/liburing/buildlink3.mk"
 .endif
+
 .endif # FUSE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fuse
