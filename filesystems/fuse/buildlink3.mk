8408e1e4d0fe9799adfe18e7bcefe1a4d24d86be
vins <vins@pkgsrc.org>
2026-05-09 19:21:36 +0000

    filesystems/fuse: revert update
    Some software still depends on fuse 2.x and hasn't migrated to fuse3.
    Any software asking for fuse3 should pull filesystems/fuse3 (to be
    reimported) instead, at least until fuse 2.x isn't fully deprecated.
    
    The update was commited without adequate testing. Apologizing for the
    fuss it caused. ReFUSE on NetBSD is at 3.x already and this led me
    to wrong assumptions.

--- filesystems/fuse/buildlink3.mk
+++ filesystems/fuse/buildlink3.mk
@@ -1,22 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.8 2026/05/06 17:08:48 vins Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2026/05/09 19:21:37 vins Exp $
 
 BUILDLINK_TREE+=	fuse
 
 .if !defined(FUSE_BUILDLINK3_MK)
 FUSE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fuse+=	fuse>=3.10
-BUILDLINK_API_DEPENDS.fuse+=	fuse>=3.18.2
+BUILDLINK_API_DEPENDS.fuse+=	fuse>=2.6
+BUILDLINK_ABI_DEPENDS.fuse+=	fuse>=2.9.9
 BUILDLINK_PKGSRCDIR.fuse?=	../../filesystems/fuse
 
 .include "../../mk/bsd.fast.prefs.mk"
 
 .if ${OPSYS} == "NetBSD"
-.  include "../../filesystems/perfuse/buildlink3.mk"
-.elif ${OPSYS} == "Linux"
-.  include "../../devel/liburing/buildlink3.mk"
+.include "../../filesystems/perfuse/buildlink3.mk"
 .endif
-
 .endif # FUSE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fuse
--- filesystems/fuse/builtin.mk
+++ filesystems/fuse/builtin.mk
@@ -1,13 +1,13 @@
-# $NetBSD: builtin.mk,v 1.7 2026/05/06 17:08:48 vins Exp $
+# $NetBSD: builtin.mk,v 1.8 2026/05/09 19:21:37 vins Exp $
 
 BUILTIN_PKG:=		fuse
 PKGCONFIG_BASE.fuse=	/usr
-PKGCONFIG_FILE.fuse=	/usr/lib/pkgconfig/fuse3.pc
-PKGCONFIG_FILE.fuse+=	/usr/lib${LIBABISUFFIX}/pkgconfig/fuse3.pc
+PKGCONFIG_FILE.fuse=	/usr/lib/pkgconfig/fuse.pc
+PKGCONFIG_FILE.fuse+=	/usr/lib${LIBABISUFFIX}/pkgconfig/fuse.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 
 .if ${USE_BUILTIN.fuse:tl} == yes
-BUILDLINK_FILES.fuse+=	lib/pkgconfig/fuse3.pc
-BUILDLINK_FILES.fuse+=	lib${LIBABISUFFIX}/pkgconfig/fuse3.pc
+BUILDLINK_FILES.fuse+=	lib/pkgconfig/fuse.pc
+BUILDLINK_FILES.fuse+=	lib${LIBABISUFFIX}/pkgconfig/fuse.pc
 .endif
