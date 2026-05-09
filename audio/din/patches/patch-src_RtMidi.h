cb94dfb0c6107bbaf9984429bf8a3e8c71a8e8dd
fox <fox@pkgsrc.org>
2026-05-09 14:24:05 +0000

    audio/din: Update to 64.1
    Changes since 60:
    
    DIN Is Noise 64.1:
    
      /*
    
        Beat Divider is now called Beat Handler because in addition
        to divisions of the beat pattern, you can now:
    
        mute: flatten the pattern between start and end vertices
              and remove the inbetween vertices/tangents.
    
        copy: copy the pattern between start and end vertices
    
        cut:  cut the pattern between start and end vertices. a copy
              of the cut pattern is now available.
    
        replace: replace the pattern between start and end vertices
                 with the cut or copied pattern. DIN will squash or
                 stretch the copied or cut pattern to fit the space
                 between start and end vertices.
    
        append: append the cut or copied pattern after the end
                vertex.
    
        when Beater @ BPM button is green, DIN envelops the lead
        voice or noise, all the drones, all the triggered notes of
        keyboard-keyboard, mondrian and all the binaural drones with
        the beat pattern starting at the first beat and ending at the
        last beat. you can change this first and last beat for
        interesting effects.
    
        default first beat is 0. default last beat is 8. default
        beat pattern has 8 beats.
    
        g: set first beat of beat pattern
        h: set last beat of beat pattern
        j: shift first and last beat of beat pattern 1 beat to left
        k: shift first and last beat of beat pattern 1 beat to right
    
      */
    
    DIN Is Noise 63:
    
      * reinstated auto accent
    
          auto accent
    
              on or off
    
                off sets accents from the accent curve
    
                    to edit accent curve click
    
                      Edit swing and accent curves
    
                  default is all divisions have same volume
    
              with auto accent on,
    
                for N divisions
    
                volume scaled down by N every division.
    
      * fixed bug in swing on Beat Divider
    
    DIN Is Noise 62.1:
    
      * removed auto accent, accent on Beat Divider
    
      /*
        edit Swing and Accent curves instead
    
          more flexible
          default no swing, no accent ie full volume
    
      */
    
      * aligning codebase with binary versions for Linux, Windows
        and macOS
    
    DIN Is Noise 61:
    
      /*
    
        flags for rest in the Beat Divider
    
          default
    
            all the flags are on
              so all the divisions are on
                so no rest
    
            if a flag is off
              corresponding division is off
                a rest!
    
          turn flags on and off to insert rests
    
            syncopation!
    
          (special) case study
    
            Divisions = 1
            (only) flag is off
              deletes original beat pattern
    
        accent is just a value multiplied to the
        volume of every division.
    
        auto accent
    
          on or off
    
            off sets accents of all divisions to 1.0
    
              so all divisions have same volume
    
          with auto accent on,
    
            for N divisions
    
            volume scaled down by N every division.
    
        suggestion
    
          combine rest flags with auto accent!
    
      */
    
    DIN Is Noise 60.1:
    
      * improved last beat feature on the Beater editor
    
        * press SPACE to set last beat of the beat pattern
          to x position of the nearest vertex to mouse cursor
    
        * press SHIFT + SPACE to set last beat to the x
          position of the mouse cursor
    
      * fixed view alteration on undo/redo on curve editor
      * removed a debug message from DIN codebase

--- audio/din/patches/patch-src_RtMidi.h
+++ audio/din/patches/patch-src_RtMidi.h
@@ -1,17 +1,18 @@
-$NetBSD: patch-src_RtMidi.h,v 1.1 2025/01/19 21:32:47 fox Exp $
+$NetBSD: patch-src_RtMidi.h,v 1.2 2026/05/09 14:24:05 fox Exp $
 
 pthread_yield() is not in NetBSD's pthread. Provide sched_yield() instead.
 
---- src/RtMidi.h.orig	2024-08-05 22:04:30.000000000 +0000
+--- src/RtMidi.h.orig	2026-03-02 18:05:40.000000000 +0000
 +++ src/RtMidi.h
-@@ -65,6 +65,10 @@
+@@ -49,6 +49,11 @@
  #include <string>
  #include <vector>
  
-+#if defined __NetBSD__ 
++#if defined __NetBSD__
 +#include <sched.h>
 +#define pthread_yield()	sched_yield()
 +#endif
- 
- /************************************************************************/
- /*! \class RtMidiError
++
+ class RtMidi
+ {
+  public:
