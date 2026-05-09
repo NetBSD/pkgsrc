4995c92ff13f726282a999ef3bd716a05e385e18
morr <morr@pkgsrc.org>
2026-05-09 21:56:07 +0000

    Update to version 9.2.0461.
    Changes:
    - patch 9.2.0461: Corrupted undofile causes use-after-free
    - patch 9.2.0460: did_set_shellpipe_redir() in wrong file
    - patch 9.2.0459: tests: test_termcodes fails (after v9.2.0456)
    - CI: Bump the github-actions group across 1 directory with 2 updates
    - patch 9.2.0458: Crash with invalid shellredir/shellpipe value
    - patch 9.2.0457: Compile warning about unused variable
    - patch 9.2.0456: stray p character displayed on some terms
    - patch 9.2.0455: 'findfunc' only allows extra info for cmdline completion
    - patch 9.2.0454: tests: no test that "abbr" in customlist completion is shown
    - patch 9.2.0453: vertical separator of statusline blend into active statusline
    - patch 9.2.0452: screen.c popup opacity blend logic is duplicated
    - patch 9.2.0451: 'findfunc' can't return extra info for cmdline completion
    - patch 9.2.0450: [security]: heap buffer overflow in spellfile.c read_compound()
    - runtime(vim9): Check cmd.exe on WSL is executable

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.168 2026/05/06 20:26:50 morr Exp $
+# $NetBSD: version.mk,v 1.169 2026/05/09 21:56:07 morr Exp $
 
 VIM_VERSION=		9.2
-VIM_PATCHLEVEL=		0449
+VIM_PATCHLEVEL=		0461
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s"
 VIM_SUBDIR=		vim92
