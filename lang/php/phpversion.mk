241ab164b2bd4b934d96df03327335333963cb38
bsiegert <bsiegert@pkgsrc.org>
2026-05-09 19:01:50 +0000

    Pullup ticket #7108 - requested by taca lang/php83: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.497
    - lang/php83/distinfo                                           1.34
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri May  8 02:04:25 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php83: distinfo
    
       Log Message:
       lang/php83: update to 8.3.31
    
       PHP 8.3.31 (2026-05-07)
    
       - Curl:
         . Add support for brotli and zstd on Windows. (Shivam Mathur)
    
       - FPM:
         . Fixed GHSA-7qg2-v9fj-4mwv (XSS within status endpoint). (CVE-2026-6735)
           (Jakub Zelenka)
    
       - MBString:
         . Fixed GHSA-wm6j-2649-pv75 (Null pointer dereference in
           php_mb_check_encoding() via mb_ereg_search_init()). (CVE-2026-7259)
           (vi3tL0u1s)
    
       - OpenSSL:
         . Fix compatibility issues with OpenSSL 4.0. (jordikroon, Remi)
    
       - PDO_Firebird:
         . Fixed GHSA-w476-322c-wpvm (SQL injection via NUL bytes in quoted strings).
           (CVE-2025-14179) (SakiTakamachi)
    
       - SOAP:
         . Fixed GHSA-85c2-q967-79q5 (Stale SOAP_GLOBAL(ref_map) pointer with Apache
           Map). (CVE-2026-6722) (ilutov)
         . Fixed GHSA-m33r-qmcv-p97q (Use-after-free after header parsing failure with
           SOAP_PERSISTENCE_SESSION). (CVE-2026-7261) (ilutov)
         . Fixed GHSA-hmxp-6pc4-f3vv (Broken Apache map value NULL check).
           (CVE-2026-7262) (ilutov)
    
       - Standard:
         . Fixed GHSA-96wq-48vp-hh57 (Signed integer overflow of char array offset).
           (CVE-2026-7568) (TimWolla)
         . Fixed GHSA-m8rr-4c36-8gq4 (Consistently pass unsigned char to ctype.h
           functions). (CVE-2026-7258) (ilutov)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.494.2.2 2026/05/09 17:36:14 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.494.2.3 2026/05/09 19:01:50 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -111,7 +111,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP82_VERSION=	8.2.30
-PHP83_VERSION=	8.3.30
+PHP83_VERSION=	8.3.31
 PHP84_VERSION=	8.4.20
 PHP85_VERSION=	8.5.6
 
