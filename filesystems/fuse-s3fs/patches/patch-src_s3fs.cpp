c03e0318e437d1ad1699cab06286e41b72bda0af
vins <vins@pkgsrc.org>
2026-05-10 14:40:43 +0000

    filesystems/fuse-s3fs: update to 1.97
    # changes
    
    Version 1.97 -- 7 Dec, 2025 (major changes only)
    #2765 Do not honor -o nonempty when compiled with FUSE 3
    
    Version 1.96 -- 24 Nov, 2025 (major changes only)
    #1867 - Require FUSE 3 on Linux
    #2596 - Require C++14
    #2623 - Improve IO concurrency
    #2669 - Rename `-o endpoint` to `-o region` for clarity
    #2728 - #2731 - #2736 - Avoid HeadObject calls with improved stats cache usage
    
    Version 1.95 -- 25 Oct, 2024 (major changes only)
    #2424 - Add ipresolve option to select IPv4- or IPv6-only
    #2443 - Retry request on HTTP 429 error
    #2448 - Changed s3fs logo
    #2455 - Fix deadlock in FdManager::ChangeEntityToTempPath
    #2487 - #2492 - #2493 - Enable static lock checking and fix locking errors
    #2506 - #2517 - Fix Windows compilation
    #2515 - Fix FreeBSD support
    #2532 - Fix use-after-free in FdManager::ChangeEntityToTempPath

--- filesystems/fuse-s3fs/patches/patch-src_s3fs.cpp
+++ filesystems/fuse-s3fs/patches/patch-src_s3fs.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_s3fs.cpp,v 1.1 2024/04/18 04:02:26 pho Exp $
+$NetBSD: patch-src_s3fs.cpp,v 1.2 2026/05/10 14:40:43 vins Exp $
 
 Fix build on NetBSD: <sys/wait.h> is required for WIFEXITED()
 TODO: Upstream this
 
---- src/s3fs.cpp.orig	2024-04-17 13:55:35.890317856 +0000
+--- src/s3fs.cpp.orig	2025-12-07 20:44:50.000000000 +0000
 +++ src/s3fs.cpp
 @@ -18,6 +18,8 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -14,13 +14,13 @@ TODO: Upstream this
  #include <algorithm>
  #include <atomic>
  #include <cerrno>
-@@ -28,6 +30,9 @@
- #include <unistd.h>
- #include <dirent.h>
+@@ -31,6 +33,9 @@
+ #include <utility>
+ #include <dirent.h>  // NOLINT(misc-include-cleaner)
  #include <sys/types.h>
 +#if defined(HAVE_SYS_WAIT_H)
 +#  include <sys/wait.h>
 +#endif
- #include <getopt.h>
+ #include <getopt.h>  // NOLINT(misc-include-cleaner)
  
  #include "common.h"
--- filesystems/fuse-s3fs/patches/patch-test_mknod__test.cc
+++ filesystems/fuse-s3fs/patches/patch-test_mknod__test.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-test_mknod__test.cc,v 1.1 2024/04/18 04:02:26 pho Exp $
+$NetBSD: patch-test_mknod__test.cc,v 1.2 2026/05/10 14:40:43 vins Exp $
 
 Fix build on NetBSD: <sys/sysmacros.h> doesn't exist on this OS.
 TODO: Upstream this.
 
---- test/mknod_test.cc.orig	2024-04-17 14:07:06.018370712 +0000
+--- test/mknod_test.cc.orig	2025-12-07 20:44:50.000000000 +0000
 +++ test/mknod_test.cc
 @@ -18,6 +18,8 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -14,14 +14,12 @@ TODO: Upstream this.
  #include <cerrno>
  #include <cstdio>
  #include <cstdlib>
-@@ -26,8 +28,8 @@
+@@ -26,7 +28,7 @@
  #include <sys/stat.h>
  #include <fcntl.h>
  #include <unistd.h>
--#ifndef __APPLE__
--#include <sys/sysmacros.h>
+-#if !defined(__APPLE__) && !defined(__FreeBSD__)
 +#if defined(HAVE_SYS_SYSMACROS_H)
-+#  include <sys/sysmacros.h>
+ #include <sys/sysmacros.h>
  #endif
  
- //---------------------------------------------------------
