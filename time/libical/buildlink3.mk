d320e7198b6c9aead112934ebefe4ada5f908ad0
wiz <wiz@pkgsrc.org>
2026-05-10 17:56:04 +0000

    libical: remove non-working upper bound from bl3.mk

--- time/libical/buildlink3.mk
+++ time/libical/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2026/05/04 13:30:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2026/05/10 17:56:04 wiz Exp $
 
 BUILDLINK_TREE+=	libical
 
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	libical
 LIBICAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libical+=	libical>=0.42
-BUILDLINK_API_DEPENDS.libical+=	libical<4
 BUILDLINK_ABI_DEPENDS.libical+=	libical>=3.0.20nb4
 BUILDLINK_PKGSRCDIR.libical?=	../../time/libical
 
