ddba1787d0a4399c48f61910bb49810bae312bd5
adam <adam@pkgsrc.org>
2026-05-11 11:53:45 +0000

    python314 py314-html-docs: updated to 3.14.5
    Python 3.14.5
    
    Security
    
    gh-148178: Hardened _remote_debugging by validating remote debug offset tables before using them to size memory reads or interpret remote layouts.
    
    Core and Builtins
    
    gh-146270: Fix a sequential consistency bug in structmember.c.
    gh-137293: Fix SystemError when searching ELF Files in sys.remote_exec().
    
    Library
    
    gh-149388: Make asyncio.windows_utils.PipeHandle closing idempotent.
    gh-149377: Update bundled pip to 26.1.1
    gh-138907: Support RFC 9309 in urllib.robotparser.
    gh-148615: Fix pdb to accept standard – end of options separator. Reported by haampie. Patched by Shrey Naithani.
    gh-130750: Restore quoting of choices in argparse error messages for improved clarity and consistency with documentation.
    gh-141449: Improve tests and documentation for non-function callables as annotate functions.
    
    Tests
    
    gh-149425: Increase time delta in test.test_zipfile.test_core.OtherTests.test_write_without_source_date_epoch
    gh-145736: Fix test_tkinter test_configure_values test case backport miss for Tk 9.
    
    macOS
    
    gh-142295: For Python macOS framework builds, update Info.plist files to be more compliant with current Apple guidelines. Original patch contributed by Martinus Verburg.
    gh-124111: Update macOS installer to use Tcl/Tk 9.0.3.

--- lang/python314/dist.mk
+++ lang/python314/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.5 2026/04/09 07:17:29 adam Exp $
+# $NetBSD: dist.mk,v 1.6 2026/05/11 11:53:45 adam Exp $
 
-PY_DISTVERSION=	3.14.4
+PY_DISTVERSION=	3.14.5
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python314/distinfo
