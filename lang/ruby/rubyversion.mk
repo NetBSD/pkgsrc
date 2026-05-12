251fea1e2219f9fa11bc3805c612c756f384a0f9
bsiegert <bsiegert@pkgsrc.org>
2026-05-12 18:38:42 +0000

    Pullup ticket #7104 - requested by taca lang/ruby34: security fix
    Revisions pulled up:
    - lang/ruby/rubyversion.mk                                      1.321
    - lang/ruby34/Makefile                                          1.8
    - lang/ruby34/distinfo                                          1.14
    - lang/ruby34/patches/patch-lib_erb.rb                          1.1
    - lang/ruby34/patches/patch-lib_erb_version.rb                  1.1
    - lang/ruby34/patches/patch-test_erb_test__erb.rb               1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Wed May  6 05:15:35 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/ruby: rubyversion.mk
       	pkgsrc/lang/ruby34: Makefile distinfo
       Added Files:
       	pkgsrc/lang/ruby34/patches: patch-lib_erb.rb patch-lib_erb_version.rb
       	    patch-test_erb_test__erb.rb
    
       Log Message:
       lang/ruby34: update default gem erb to 4.0.4.1
    
       Update default gem erb to 4.0.4.1 to fix security problem of CVE-2026-41316.
    
       Bump PKGREVISION.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.314.2.6 2026/05/09 16:52:07 bsiegert Exp $
+# $NetBSD: rubyversion.mk,v 1.314.2.7 2026/05/12 18:38:42 bsiegert Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -474,7 +474,7 @@ RUBY_CGI_VER=			0.4.2
 RUBY_DELEGATE_VER=		0.4.0
 RUBY_DID_YOU_MEAN_VER=		2.0.0
 RUBY_ENGLISH_VER=		0.8.0
-RUBY_ERB_VER=			4.0.4
+RUBY_ERB_VER=			4.0.4.1
 RUBY_ERROR_HIGHLIGHT_VER=	0.7.0
 RUBY_FILEUTILS_VER=		1.7.3
 RUBY_FIND_VER=			0.2.0
