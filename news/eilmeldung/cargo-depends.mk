2d50292522d5f07a4beedf11e7af5570c70ed0e6
pin <pin@pkgsrc.org>
2026-05-14 20:28:22 +0000

    news/eilmeldung: update to 1.5.2
    1.5.2 - 2026-05-14
    
        restored default border theme (connected and rounded)
        fixed bug which resulted in rapid swapping of article content
    
    1.5.1 - 2026-05-13
    
        open borders for everyone!
            you can now freely customize the border style of the panels!
            want to highlight the focused panel by a double border and use a plain border for inactive panels? just append this to your config.toml:
    
        [border_theme]
        focused = "double"
        unfocused = "plain"
        framing = "connected"
    
            these settings also feature to new "connected" style which elegantly connected the borders of each panel; you can still go back to the classic style by using framing = "open" or even use completely closed border with framing = "closed"
            read more in docs/configuration.md in the section Border Theme
        fixed double inputs in command input and help popup under Windows
        show_top_bar is deprecated now and the top bar is removed entirely
        the settings scrollbar_begin_symbol``, scrollbar_end_symbol, scrollbar_track_symbol, scrollbar_thumb_symbol` are deprecated now; the scrollbar characters are determined by the chosen border type

--- news/eilmeldung/cargo-depends.mk
+++ news/eilmeldung/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.3 2026/05/05 10:06:57 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.4 2026/05/14 20:28:22 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.25.1
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
