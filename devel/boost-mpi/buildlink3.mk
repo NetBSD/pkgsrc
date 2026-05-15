f72cc03c1ff547020df266f7ef1662c1ec701af2
adam <adam@pkgsrc.org>
2026-05-15 09:13:48 +0000

    boost: updated to 1.91.0
    1.91.0
    
    General Notes
    StaticAssert has been merged into Config. This includes code, tests and
    documentation. For backward compatibility git submodule, CMake and b2 targets
    of StaticAssert are still available; the targets simply introduce a dependency
    on Config. Eventually, the submodule and targets will be removed. Users are
    recommended to update their dependencies on StaticAssert to replace it with
    Config. No C++ code modifications are necessary. Most Boost libraries have been
    updated accordingly.
    
    Read more https://www.boost.org/releases/latest/

--- devel/boost-mpi/buildlink3.mk
+++ devel/boost-mpi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2025/12/25 22:45:28 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2026/05/15 09:13:49 adam Exp $
 
 BUILDLINK_TREE+=	boost-mpi
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	boost-mpi
 BOOST_MPI_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.boost-mpi+=	boost-mpi-1.90.*
-BUILDLINK_ABI_DEPENDS.boost-mpi+=	boost-mpi>=1.90.0
+BUILDLINK_API_DEPENDS.boost-mpi+=	boost-mpi-1.91.*
+BUILDLINK_ABI_DEPENDS.boost-mpi+=	boost-mpi>=1.91.0
 BUILDLINK_PKGSRCDIR.boost-mpi?=		../../devel/boost-mpi
 
 .include "../../devel/boost-libs/buildlink3.mk"
--- meta-pkgs/boost/patches/patch-boost_asio_detail_config.hpp
+++ meta-pkgs/boost/patches/patch-boost_asio_detail_config.hpp
@@ -1,12 +1,10 @@
-$NetBSD: patch-boost_asio_detail_config.hpp,v 1.9 2026/02/26 10:21:00 wiz Exp $
+$NetBSD: patch-boost_asio_detail_config.hpp,v 1.10 2026/05/15 09:13:49 adam Exp $
 
 1. chunk: ?
-2. chunk: NetBSD has MSG_NOSIGNAL, even though _POSIX_VERSION is only 200112L
-https://github.com/chriskohlhoff/asio/pull/1717
 
---- boost/asio/detail/config.hpp.orig	2025-12-03 13:46:38.000000000 +0000
+--- boost/asio/detail/config.hpp.orig	2026-04-15 14:38:52.000000000 +0000
 +++ boost/asio/detail/config.hpp
-@@ -521,7 +521,9 @@
+@@ -528,7 +528,9 @@
  #  elif defined(__GNUC__)
  #   if ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 9)) || (__GNUC__ > 4)
  #    if (__cplusplus >= 201402)
@@ -16,12 +14,3 @@ https://github.com/chriskohlhoff/asio/pull/1717
  #    endif // (__cplusplus >= 201402)
  #   endif // ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 9)) || (__GNUC__ > 4)
  #  endif // defined(__GNUC__)
-@@ -1392,7 +1394,7 @@
- 
- // Kernel support for MSG_NOSIGNAL.
- #if !defined(BOOST_ASIO_HAS_MSG_NOSIGNAL)
--# if defined(__linux__)
-+# if defined(__linux__) || defined(__NetBSD__)
- #  define BOOST_ASIO_HAS_MSG_NOSIGNAL 1
- # elif defined(_POSIX_VERSION)
- #  if (_POSIX_VERSION >= 200809L)
