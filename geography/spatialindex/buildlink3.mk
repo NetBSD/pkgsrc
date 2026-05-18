824fc047a4b68f756348dc5520d5b0f2128fa10a
gdt <gdt@pkgsrc.org>
2026-05-18 15:36:44 +0000

    geography/spatialindex: Update to 2.0.0
    packaging NEWS:
    
      Note that 2.0.0 is not the latest release.  As described on qgis
      lists, 2.1.0 changed how nearest neighbor queries with custom
      functions work and it does not seem possible to use them correctly.
      Thus pkgsrc is avoiding 2.1.0, as recommended by qgis.
    
    Not mentioned in upstream NEWS:
    
      shlib version changed from 6.1.1 to 7.0.0
    
    upstream NEWS content;
    
      2.0.0 has been released on 07 JUNE 2024. There are no significant
      major enhancements or refactorings that should change any
      behaviors. It had been a long time (almost 5 years!) since a release
      and 2.0.0 was the next version number...
    
      Changes of Note
    
          Added Ball Shape for point neighborhood queries by @Plaba in #232
          libspatialindex no longer emits debugging lint to stderr 0e26d16
    
      Build configuration
    
          Fix MinGW build by @SpaceIm in #185
          Allow building static libs by @SpaceIm in #183
          cmake targets by @hobu in #186
          CMakeLists.txt: fix CMAKE_BUILD_TYPE by @ffontaine in #188
          .gitignore: remove mostly autconf/automake components, add build by @mwtoews in #192
          CMake: modern formatting and features by @mwtoews in #196
          CMake: deprecate SIDX_BUILD_TESTS with CTest's BUILD_TESTING option by @mwtoews in #197
          CMake: fix find_package(libspatialindex) by @mwtoews in #199
          pkg-config: restore functionality (via CMake), change Cflags by @mwtoews in #201
          Export target namespace as 'libspatialindex' in CMake Targets #205 by @hobu in #207
          Fix building static libs by @m-kuhn in #223
          Modernize CMake configuration by @hobu in #240
          Release package building by @hobu in #242

--- geography/spatialindex/buildlink3.mk
+++ geography/spatialindex/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2023/02/15 23:17:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/05/18 15:36:44 gdt Exp $
 
 BUILDLINK_TREE+=	spatialindex
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	spatialindex
 SPATIALINDEX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.spatialindex+=	spatialindex>=1.8.5
-BUILDLINK_ABI_DEPENDS.spatialindex+=	spatialindex>=1.9.3
+BUILDLINK_ABI_DEPENDS.spatialindex+=	spatialindex>=2.0.0
 BUILDLINK_PKGSRCDIR.spatialindex?=	../../geography/spatialindex
 .endif	# SPATIALINDEX_BUILDLINK3_MK
 
