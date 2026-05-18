5361e156b919fda149804d5e05b71227a235e126
morr <morr@pkgsrc.org>
2026-05-18 17:50:09 +0000

    Update to version 9.2.0496.
    Changes:
    - patch 9.2.0496: [security]: Code Injection in cucumber filetype plugin
    - patch 9.2.0495: [security]: runtime(netrw): code injection via NetrwBookHistSave()
    - runtime(just): add 'suffixesadd' to ftplugin
    - runtime(sh): Do not conflate empty array and function declarations in Bash
    - patch 9.2.0494: User commands cannot handle single args with spaces
    - patch 9.2.0493: popup: missing  Popup, PopupBorder and PopupTitle hi groups
    - patch 9.2.0492: popup: decoration wrongly drawn with clipping on border
    - CI: Update clang to v22
    - runtime(htmldjango): Add syntax highlighting of comparison operators
    - runtime(django): Resolve FIXME of comparrison operators + localization tags
    - ccfilter: uses unbounded strcat()/strcpy()
    - NSIS: Don't install 32-bit dll on ARM64

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,9 +1,9 @@
-# $NetBSD: version.mk,v 1.170 2026/05/17 17:23:38 morr Exp $
+# $NetBSD: version.mk,v 1.171 2026/05/18 17:50:09 morr Exp $
 
 VIM_VERSION=		9.2
-VIM_PATCHLEVEL=		0491
+VIM_PATCHLEVEL=		0496
 # Changelog: see https://github.com/vim/vim/commits/master/ or
-# with git clone: $ git log --pretty=format:"- %s"
+# with git clone: $ git log --pretty=format:"- %s" v<old_version>...HEAD
 VIM_SUBDIR=		vim92
 
 PRINT_PLIST_AWK+=	{ gsub(/${VIM_SUBDIR}/, "$${VIM_SUBDIR}"); print; next; }
