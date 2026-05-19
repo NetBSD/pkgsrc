4085353701f962254b6381acf127bc8924874f3f
taca <taca@pkgsrc.org>
2026-05-19 14:45:52 +0000

    lang/ruby33: improve rdoc
    * Fix rdoc problem on C locale, backported from rdoc-7.0.4.
    * Remove pkglint warning.
    
    Bump PKGREVISION.

---
+++ lang/ruby33/patches/patch-lib_rdoc_parser_c.rb
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_rdoc_parser_c.rb,v 1.1 2026/05/19 14:45:52 taca Exp $
+
+Backport from rdoc-7.0.4.
+
+--- lib/rdoc/parser/c.rb.orig	2026-03-26 00:05:04.000000000 +0000
++++ lib/rdoc/parser/c.rb
+@@ -1011,7 +1011,7 @@ class RDoc::Parser::C < RDoc::Parser
+         file_name = File.join @file_dir, source_file
+ 
+         if File.exist? file_name then
+-          file_content = File.read file_name
++          file_content = RDoc::Encoding.read_file file_name, @options.encoding
+         else
+           @options.warn "unknown source #{source_file} for #{meth_name} in #{@file_name}"
+         end
