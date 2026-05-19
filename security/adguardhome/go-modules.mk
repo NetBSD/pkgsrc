3411a3ad83b61173f56bc09985e8e5bb493f1ab0
adam <adam@pkgsrc.org>
2026-05-19 18:16:38 +0000

    adguardhome: updated to 0.107.75
    0.107.75
    
    We’ve fixed a critical vulnerability affecting DNS-over-QUIC and DNS-over-HTTPS
    that could put your DNS privacy at risk. The gap is now closed, and your
    encrypted traffic is back to traveling through a properly armored tunnel.

--- security/adguardhome/go-modules.mk
+++ security/adguardhome/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.5 2026/04/16 12:40:07 adam Exp $
+# $NetBSD: go-modules.mk,v 1.6 2026/05/19 18:16:38 adam Exp $
 
 GO_MODULE_FILES+=	cloud.google.com/go/@v/v0.123.0.mod
 GO_MODULE_FILES+=	cloud.google.com/go/@v/v0.123.0.zip
@@ -6,10 +6,10 @@ GO_MODULE_FILES+=	cloud.google.com/go/auth/@v/v0.20.0.mod
 GO_MODULE_FILES+=	cloud.google.com/go/auth/@v/v0.20.0.zip
 GO_MODULE_FILES+=	cloud.google.com/go/compute/metadata/@v/v0.9.0.mod
 GO_MODULE_FILES+=	cloud.google.com/go/compute/metadata/@v/v0.9.0.zip
-GO_MODULE_FILES+=	github.com/!adguard!team/dnsproxy/@v/v0.81.1.mod
-GO_MODULE_FILES+=	github.com/!adguard!team/dnsproxy/@v/v0.81.1.zip
-GO_MODULE_FILES+=	github.com/!adguard!team/golibs/@v/v0.35.11.mod
-GO_MODULE_FILES+=	github.com/!adguard!team/golibs/@v/v0.35.11.zip
+GO_MODULE_FILES+=	github.com/!adguard!team/dnsproxy/@v/v0.81.3.mod
+GO_MODULE_FILES+=	github.com/!adguard!team/dnsproxy/@v/v0.81.3.zip
+GO_MODULE_FILES+=	github.com/!adguard!team/golibs/@v/v0.35.12.mod
+GO_MODULE_FILES+=	github.com/!adguard!team/golibs/@v/v0.35.12.zip
 GO_MODULE_FILES+=	github.com/!adguard!team/urlfilter/@v/v0.23.2.mod
 GO_MODULE_FILES+=	github.com/!adguard!team/urlfilter/@v/v0.23.2.zip
 GO_MODULE_FILES+=	github.com/!burnt!sushi/toml/@v/v1.6.0.mod
