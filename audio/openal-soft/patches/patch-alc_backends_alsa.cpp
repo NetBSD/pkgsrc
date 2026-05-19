42e495ea30f186ad21e03816b419a246b3b95c10
adam <adam@pkgsrc.org>
2026-05-19 05:18:45 +0000

    openal-soft: updated to 1.25.2
    openal-soft-1.25.2:
    
    Fixed the library version string for builds made without Git repo info.
    
    Fixed an STL hardening assertion in the reverb effect.
    
    Fixed a potential crash for builds made with older PipeWire headers.
    
    Fixed capturing mono from a stereo or greater WASAPI input device.
    
    Fixed building on macOS without std::format.
    
    Fixed height encoding for TSME output.
    
    Fixed the Super Stereo decoder's stereo separation with TSME output.
    
    Implemented 3D processing for the Distortion, Chorus, Flanger, Pitch
    Shifter, and Frequency Shifter effects.
    
    Improved handling duplicate device names from PipeWire.
    
    Added support for 7.1.4 output autodetection with CoreAudio.
    
    Added capture support to the SDL3 backend.
    
    Added support for device added and removed events to the SDL3 backend.
    
    Removed use of "protected" visibility for exported symbols, now using the
    normal "default" visibility.
    
    Actually changed the default Super Stereo width to 0.46.

--- audio/openal-soft/patches/patch-alc_backends_alsa.cpp
+++ audio/openal-soft/patches/patch-alc_backends_alsa.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-alc_backends_alsa.cpp,v 1.1 2019/11/30 13:40:29 nia Exp $
+$NetBSD: patch-alc_backends_alsa.cpp,v 1.2 2026/05/19 05:18:45 adam Exp $
 
 Support building on non-Linux.
 
---- alc/backends/alsa.cpp.orig	2019-11-28 08:45:08.000000000 +0000
+--- alc/backends/alsa.cpp.orig	2026-05-12 02:21:07.000000000 +0000
 +++ alc/backends/alsa.cpp
-@@ -53,6 +53,9 @@
- 
- #include <alsa/asoundlib.h>
+@@ -54,6 +54,9 @@ import logging;
+ #include "core/logging.h"
+ #endif
  
 +#ifndef ESTRPIPE
 +#define ESTRPIPE EPIPE
