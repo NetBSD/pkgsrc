5b4d1b117504037b3e84b9645ed1244a84a920bc
osa <osa@pkgsrc.org>
2026-05-21 10:17:06 +0000

    www/freenginx-devel: update njs 0.9.8 -> 0.9.9
    Bump PKGREVISION.
    
    Sponsored by:	tipi.work
    
    <ChangeLog>
    
    nginx modules:
    
    *) Security: a heap buffer overflow might occur in a worker process
       when the "js_fetch_proxy" directive value contains nginx
       variables derived from the client request ($http_*, $arg_*,
       $cookie_*, etc.) and the location's JS handler invokes
       ngx.fetch(). The issue was introduced in dea83189 (0.9.4).
    
    *) Feature: added js_access directive.
    
    *) Feature: added r.readRequestText(), r.readRequestArrayBuffer(),
       and r.readRequestJSON() - async methods that read the request
       body, available in js_access and js_content directives.
    
    *) Feature: added r.readRequestForm() - async method that parses
       the request body submitted from an HTML form
       (application/x-www-form-urlencoded and multipart/form-data) and
       returns a structured accessor object.  The method is available
       in js_access and js_content directives.
    
    *) Feature: added jsVarNames() method.  The method returns the
       names of variables declared with js_var.
    
    Core:
    
    *) Bugfix: fixed evaluation order of call arguments with side
       effects.  Previously, an earlier argument could observe a later
       argument's mutation, e.g. f(a, a = 2) passed 2 as both
       arguments.
       The issue was introduced in fd5e523f (0.9.7).
    
    </ChangeLog>

--- www/freenginx-devel/options.mk
+++ www/freenginx-devel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2026/05/10 11:59:32 osa Exp $
+# $NetBSD: options.mk,v 1.6 2026/05/21 10:17:06 osa Exp $
 
 CODELOAD_SITE_GITHUB=		https://codeload.github.com/
 
@@ -387,7 +387,7 @@ PLIST.rtmp=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mnginx-njs) || make(makesum) || make(mdi) || make(distclean)
-NJS_VERSION=		0.9.8
+NJS_VERSION=		0.9.9
 NJS_DISTNAME=		njs-${NJS_VERSION}
 NJS_DISTFILE=		${NJS_DISTNAME}.tar.gz
 SITES.${NJS_DISTFILE}=	-${MASTER_SITE_GITHUB:=nginx/njs/archive/}${NJS_VERSION}.tar.gz
