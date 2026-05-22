c037632050b40f4075aa58b8dce58c42e4a44661
adam <adam@pkgsrc.org>
2026-05-22 08:06:31 +0000

    nodejs22: updated to 22.22.3
    22.22.3 'Jod' (LTS)
    Bug fixes

--- lang/nodejs22/buildlink3.mk
+++ lang/nodejs22/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2026/01/07 08:47:39 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/05/22 08:06:31 adam Exp $
 
 BUILDLINK_TREE+=	nodejs
 
@@ -11,6 +11,7 @@ BUILDLINK_PKGSRCDIR.nodejs?=	../../lang/nodejs22
 
 .include "../../mk/bsd.fast.prefs.mk"
 
+.include "../../databases/sqlite3/buildlink3.mk"
 .if ${OPSYS} != "Darwin"
 .include "../../devel/libexecinfo/buildlink3.mk"
 .endif
