9e5d2fd333ae6ccc52364d6a546c5a78dfc5af3d
adam <adam@pkgsrc.org>
2026-05-26 13:38:14 +0000

    vaultwarden: updated to 1.36.0
    1.36.0
    
    Security Fixes
    
    This release contains security fixes for the following advisories. We strongly advice to update as soon as possible.
    
    SSO Login CSRF
    GHSA-pfp2-jhgq-6hg5
    GHSA-w6h6-8r66-hcv7
    User/Organization Enumeration
    GHSA-hxqh-ff5p-wfr3
    SSO existing-user binding
    GHSA-j4j8-gpvj-7fqr
    GHSA-6x5c-84vm-5j56
    SSRF via Icon Endpoint
    GHSA-72vh-x5jq-m82g
    Some crate's updated and other minor security enhancements
    These are private for now, pending CVE assignment.
    
    Notes
    
    Archiving of items is available
    https://bitwarden.com/blog/keep-your-vault-tidy-with-item-archiving/
    https://bitwarden.com/nl-nl/help/managing-items/#archive
    Web Vault updated to v2026.4.1
    
    What's Changed
    
    SSO fallback to UserInfo preferred_username
    Dummy identifier need to pass for a guid
    add new /identity/accounts/prelogin/password
    Add DuckDuckGo browser device type
    Apply duration_suboptimal_units lint findings
    Apply ref_option lint findings
    Fix hardcoded sso identifier
    Update crates and fix a nightly lint
    Fix Host/IP resolving
    Several SSO Fixes
    Add support for archiving items
    Fix favicon fetching to check all icon links instead of just the first one
    Fix merge conflict
    Replace organization_uuid unwrap with proper error handling
    fix: return Err instead of panic on unknown cipher atype in to_json()
    Allow SQLite to be linked against dynamically
    Update crates and web-vault
    Update hickory

--- security/vaultwarden/options.mk
+++ security/vaultwarden/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2026/04/28 08:36:15 adam Exp $
+# $NetBSD: options.mk,v 1.11 2026/05/26 13:38:14 adam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.vaultwarden
 PKG_SUPPORTED_OPTIONS=		web-vault
@@ -30,7 +30,7 @@ RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/
 
 .if !empty(PKG_OPTIONS:Mweb-vault)
 WEB_VAULT_FILE=		bw_web_v${WEB_VAULT_VERS}.tar.gz
-WEB_VAULT_VERS=		2026.3.1
+WEB_VAULT_VERS=		2026.4.1
 DISTFILES+=		${WEB_VAULT_FILE}
 SITES.${WEB_VAULT_FILE}= ${MASTER_SITE_GITHUB:=dani-garcia/bw_web_builds/releases/download/v${WEB_VAULT_VERS}/}
 
