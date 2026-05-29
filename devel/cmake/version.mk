9e82f74e868aa660372d020d18760f694a108c2a
adam <adam@pkgsrc.org>
2026-05-29 09:13:07 +0000

    cmake cmake-gui: updated to 4.3.3
    4.3.3
    The export(PACKAGE_INFO) command now writes to the cps/<package-name>
    subdirectory of the current build directory, rather than directly to the
    current build directory.

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.85 2026/04/23 09:08:28 adam Exp $
+# $NetBSD: version.mk,v 1.86 2026/05/29 09:13:07 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.3.2
+CMAKE_VERSION=	4.3.3
 CMAKE_API=	${CMAKE_VERSION:R}
