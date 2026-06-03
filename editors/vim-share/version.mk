93b2604922ae039abf6437a4e62ff9fb8b064852
morr <morr@pkgsrc.org>
2026-06-03 22:33:11 +0000

    Update to version 9.2.0593.
    Changes:
    - patch 9.2.0593: :wqall ignores term_setkill() on running terminal buffers
    - patch 9.2.0592: Error when restoring session with terminal window
    - patch 9.2.0591: 'scrolljump' ignored when scrolling up
    - patch 9.2.0590: GTK4: drawing area loses focus shape on popup menu open
    - patch 9.2.0589: filetype: xinitrc files are not recognized
    - runtime(doc): Update mapping descriptions
    - runtime(kitty): Fix regex for kittyMapSeq region
    - patch 9.2.0588: GTK4: drawing area loses focus after closing a menubar popover
    - patch 9.2.0587: GTK4: left scrollbar overlaps drawarea
    - runtime(doc): fix a typo in :write-plugin
    - runtime(doc): Tweak documentation style
    - runtime(cpp): recognize C++23 stdfloat types
    - patch 9.2.0586: Crash with TextPut autocmd when pasting in terminal buffer
    - runtime(c): classify type qualifiers, function specifiers and C23 attributes
    - patch 9.2.0585: line number wrong after undoing a deletion in quickfix buffer
    - runtime(sgf): Include sgf syntax script
    - patch 9.2.0584: GTK4: missing UI features
    - patch 9.2.0583: completion: indent not ignored for fuzzy line completion
    - patch 9.2.0582: GTK4: compile error when XFONTSET is defined
    - runtime(vim): Update ftplugin, add heredocs to b:match_words
    - patch 9.2.0581: After maximizing and deleting the quickfix buffer, window height is wrong
    - runtime(matchit): Update matchit plugin
    - translation(it): Update Italian xxd man page
    - patch 9.2.0580: xxd: binary output is not colored with -R
    - patch 9.2.0579: :mksession, :mkview and :mkvimrc emit legacy Vim script
    - patch 9.2.0578: GTK4: :unmenu does not remove entries from the menubar
    - patch 9.2.0577: GTK4: window resizing issues
    - patch 9.2.0576: popup_create() not blocked in secure/sandbox
    - patch 9.2.0575: tests: filetype test for v9.2.0557 can be improved
    - patch 9.2.0574: tests: missing test for v9.2.0572
    - patch 9.2.0573: Vim9: missing EX_WHOLE on some block keywords
    - NSIS: Fix 32-bit context menu
    - patch 9.2.0572: lines disappear with wrapping virtual text after a double-width char
    - patch 9.2.0571: Vim9: memory leak in compile_nested_function() on failure
    - patch 9.2.0570: GTK4: mouse wheel scrolling does not work correctly
    - patch 9.2.0569: out-of-bounds access in libvterm CSI 8 t resize
    - patch 9.2.0568: pythoncomplete: g:pythoncomplete_allow_import had no effect
    - patch 9.2.0567: dict function name allocation failure not handled
    - runtime(vim9): remove unnecessary temp variable
    - CI: Bump github/codeql-action
    - runtime(autoload): consistently align with TABs in README.txt
    - runtime(pilrc): fix typo country names in pilrcCountry syntax list
    - patch 9.2.0566: <C-w>f duplicates window if do_ecmd() is aborted
    - patch 9.2.0565: [security]: out-of-bounds read in update_snapshot()

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.173 2026/05/30 16:32:21 morr Exp $
+# $NetBSD: version.mk,v 1.174 2026/06/03 22:33:11 morr Exp $
 
 VIM_VERSION=		9.2
-VIM_PATCHLEVEL=		0564
+VIM_PATCHLEVEL=		0593
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s" v<old_version>...HEAD
 VIM_SUBDIR=		vim92
