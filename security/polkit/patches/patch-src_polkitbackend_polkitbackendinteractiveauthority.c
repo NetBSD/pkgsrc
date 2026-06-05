e23fdf8f6e83d2c34c3e0332f10d5cedf021b401
wiz <wiz@pkgsrc.org>
2026-06-05 22:16:43 +0000

    polkit: remove unnecessary define
    (upstream feedback)
    
    NFCI

--- security/polkit/patches/patch-src_polkitbackend_polkitbackendinteractiveauthority.c
+++ security/polkit/patches/patch-src_polkitbackend_polkitbackendinteractiveauthority.c
@@ -1,17 +1,16 @@
-$NetBSD: patch-src_polkitbackend_polkitbackendinteractiveauthority.c,v 1.5 2025/12/21 15:44:05 wiz Exp $
+$NetBSD: patch-src_polkitbackend_polkitbackendinteractiveauthority.c,v 1.6 2026/06/05 22:16:44 wiz Exp $
 
 * for *BSD netgroup functions
 https://github.com/polkit-org/polkit/pull/624
 
 --- src/polkitbackend/polkitbackendinteractiveauthority.c.orig	2025-12-17 16:14:53.000000000 +0000
 +++ src/polkitbackend/polkitbackendinteractiveauthority.c
-@@ -25,8 +25,13 @@
+@@ -25,8 +25,12 @@
  #ifdef HAVE_NETGROUP_H
  #include <netgroup.h>
  #else
 +#if defined(__NetBSD__)
 +#include <netgroup.h>
-+#define BSD_NETGROUP
 +#else
  #include <netdb.h>
  #endif
@@ -19,7 +18,7 @@ https://github.com/polkit-org/polkit/pull/624
  #include <string.h>
  #include <glib/gstdio.h>
  #include <locale.h>
-@@ -2383,7 +2388,7 @@ get_users_in_net_group (PolkitIdentity  
+@@ -2383,7 +2387,7 @@ get_users_in_net_group (PolkitIdentity                
  
    name = polkit_unix_netgroup_get_name (POLKIT_UNIX_NETGROUP (group));
  
