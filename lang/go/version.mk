a411a7ff311e991c6e98eab3c6e6f3f13d8354ba
bsiegert <bsiegert@pkgsrc.org>
2026-06-05 10:18:24 +0000

    go: update to 1.26.4 and 1.25.11 (security).
    These releases include 3 security fixes following the security policy:
    
    -	mime: quadratic complexity in WordDecoder.DecodeHeader
    
    	Decoding a maliciously-crafted MIME header containing many invalid
    	encoded-words could consume excessive CPU.
    	The MIME decoder now better handles this case.
    
    	Thanks to p4p3r (https://hackerone.com/p4p3r_hak) for reporting this issue.
    
    	This is CVE-2026-42504 and Go issue https://go.dev/issue/79217.
    
    -	net/textproto: arbitrary input are included in errors without any escaping
    
    	When returning errors, functions in the net/textproto package would
    	include its input as part of the error, without any escaping. Note that
    	said input is often controlled by external parties when using this
    	package naturally. For example, a net/http client uses ReadMIMEHeader
    	when parsing the headers it receive from a server.
    
    	As a result, an attacker could inject arbitrary content into the error.
    	Practically, this can result in an attacker injecting misleading
    	content, terminal control bytes, etc. into a victim's output or logs.
    
    	This is CVE-2026-42507 and Go issue https://go.dev/issue/79346
    
    -	crypto/x509: split candidate hostname only once
    
    	(*x509.Certificate).VerifyHostname previously called matchHostnames in a loop
    	over all DNS Subject Alternative Name (SAN) entries. This caused
    	strings.Split(host, ".") to execute repeatedly on the same input hostname.
    
    	With a large DNS SAN list, verification costs scaled quadratically based on the
    	number of SAN entries multiplied by the hostname's label count. Because
    	x509.Verify validates hostnames before building the certificate chain, this
    	overhead occurred even for untrusted certificates.
    
    	Thanks to Jakub Ciolek (https://ciolek.dev) for reporting this issue.
    
    	This is CVE-2026-27145 and https://go.dev/issue/79694.
    
    View the release notes for more information:
    https://go.dev/doc/devel/release#go1.26.4

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.249 2026/05/07 18:40:36 bsiegert Exp $
+# $NetBSD: version.mk,v 1.250 2026/06/05 10:18:24 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,8 +6,8 @@
 #
 .include "go-vars.mk"
 
-GO126_VERSION=	1.26.3
-GO125_VERSION=	1.25.10
+GO126_VERSION=	1.26.4
+GO125_VERSION=	1.25.11
 GO124_VERSION=	1.24.13
 GO123_VERSION=	1.23.12
 GO122_VERSION=	1.22.12
