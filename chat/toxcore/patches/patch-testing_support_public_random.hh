3d393f477baa8df7cff321bf74cafd4dfdb4dd4c
ktnb <ktnb@pkgsrc.org>
2026-06-05 12:15:14 +0000

    toxcore: update to 0.2.23
    Bug Fixes
    - limit number of saved group peers when loading from disk And add
      `unpack_gc_saved_peers` bounds test
    - DHT:
      - refactor memory handling of loaded dht state Previous code could
        double-free if more than one dht states per tox-file and a following
        allocation fails
      - Suppress the warning about IPv6 nodes when IPv6 is turned off
    - av: Fix Use-After-Free when peer goes offline.
    - gca: announce max sizes where 32bytes too large
    - onion: Size GCA announce response buffer to actual content length.
    - test: fix mypy failure
    
    Features
    - Add `Tox_Iterate_Options` for granular control over `tox_iterate`.
    - Add a function to get the list of valid group chat numbers.

--- chat/toxcore/patches/patch-testing_support_public_random.hh
+++ chat/toxcore/patches/patch-testing_support_public_random.hh
@@ -1,11 +1,11 @@
-$NetBSD: patch-testing_support_public_random.hh,v 1.1 2026/02/13 02:55:05 ktnb Exp $
+$NetBSD: patch-testing_support_public_random.hh,v 1.2 2026/06/05 12:15:14 ktnb Exp $
 
 File uses size_t, include it.
 
---- testing/support/public/random.hh.orig	2026-02-07 20:14:53.000000000 +0000
+--- testing/support/public/random.hh.orig	2026-06-03 18:11:07.000000000 +0000
 +++ testing/support/public/random.hh
-@@ -3,6 +3,7 @@
- 
+@@ -4,6 +4,7 @@
+ #include <cstddef>
  #include <cstdint>
  #include <vector>
 +#include <stdlib.h>
