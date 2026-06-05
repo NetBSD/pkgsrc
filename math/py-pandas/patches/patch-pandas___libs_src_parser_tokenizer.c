16ec4cf0f430e66cda96399f8820620bdf6ffaa4
adam <adam@pkgsrc.org>
2026-06-05 10:41:53 +0000

    py-pandas: updated to 3.0.3
    3.0.3
    
    Enhancements
    
    Starting with pandas 3.0.0, time zones are represented by default using the standard library’s zoneinfo module (or datetime.timezone for fixed offsets) instead of using pytz (release note).
    The IO methods using pyarrow under the hood such as read_parquet(), read_feather() and read_orc() (or read_csv() when specifying the engine) were still returning timezone using pytz. Those have now been updated to consistently use default zoneinfo time zones as well (GH 65134).
    
    Fixed regressions
    
    Fixed a regression in pandas.bdate_range() where specifying end on a weekend with periods returned fewer dates than expected (GH 64834)
    Fixed a regression in to_timedelta() ignoring the unit argument for round float values when mixed with non-round floats in a list (GH 65150)
    Fixed a regression in Series.rank() with custom extension dtypes (GH 64976)
    Fixed a regression in Timedelta.round(), Timedelta.floor(), and Timedelta.ceil() raising ZeroDivisionError for sub-second freq (GH 64828)
    Fixed reading of Parquet files with timezone-aware timestamps or localizing of a timeseries with a pytz timezone when an older version of pytz was installed (GH 64978)
    Fixed a regression in read_csv() with engine="c" where thousands="," could incorrectly parse non-numeric values like "1 ," as integers instead of strings (GH 64655)
    Fixed a performance regression when working with timezone-aware timeseries data using a default zoneinfo time zone, i.e. slowdown when accessing components or localizing a naive timeseries (GH 64363)
    
    Bug fixes
    
    Fixed ArrowDtype string arithmetic for addition between string and large_string typed arrays (GH 65220)
    Fixed a bug in adding missing values (e.g. NA) to a pyarrow-backed string array or Series raising an error instead of propagating the missing value (GH 64968)
    Fixed a bug in Series.rank() with period dtype and missing values, always sorting missing values at the top, regardless of the na_option value (GH 64976)
    Fixed a bug in reading files with a chained URL scheme (e.g. tar://) with the latest version of fsspec (GH 65462).
    Fixed a hang in Series.str.replace() for PyArrow-backed string data when replacing an empty pattern; behavior now matches Python’s str.replace (GH 64941).

--- math/py-pandas/patches/patch-pandas___libs_src_parser_tokenizer.c
+++ math/py-pandas/patches/patch-pandas___libs_src_parser_tokenizer.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-pandas___libs_src_parser_tokenizer.c,v 1.1 2025/10/31 00:22:22 ryoon Exp $
+$NetBSD: patch-pandas___libs_src_parser_tokenizer.c,v 1.2 2026/06/05 10:41:53 adam Exp $
 
-* Fix segfault in some read_csv() cases under NetBSD.
+Fix segfault in some read_csv() cases under NetBSD.
 
---- pandas/_libs/src/parser/tokenizer.c.orig	2025-10-31 00:17:11.113268604 +0000
+--- pandas/_libs/src/parser/tokenizer.c.orig	2026-05-11 16:18:35.000000000 +0000
 +++ pandas/_libs/src/parser/tokenizer.c
-@@ -630,7 +630,7 @@ static int parser_buffer_bytes(parser_t 
+@@ -634,7 +634,7 @@ static int parser_buffer_bytes(parser_t
  
  // applied when in a field
  #define IS_DELIMITER(c)                                                        \
@@ -13,7 +13,7 @@ $NetBSD: patch-pandas___libs_src_parser_tokenizer.c,v 1.1 2025/10/31 00:22:22 ry
  
  #define _TOKEN_CLEANUP()                                                       \
    self->stream_len = slen;                                                     \
-@@ -772,7 +772,7 @@ static int tokenize_bytes(parser_t *self
+@@ -776,7 +776,7 @@ static int tokenize_bytes(parser_t *self
          self->state = EAT_CRNL_NOP;
          break;
        } else if (!self->delim_whitespace) {
@@ -22,7 +22,7 @@ $NetBSD: patch-pandas___libs_src_parser_tokenizer.c,v 1.1 2025/10/31 00:22:22 ry
          } else { // backtrack
            // use i + 1 because buf has been incremented but not i
            do {
-@@ -802,7 +802,7 @@ static int tokenize_bytes(parser_t *self
+@@ -806,7 +806,7 @@ static int tokenize_bytes(parser_t *self
        } else if (IS_COMMENT_CHAR(c)) {
          self->state = EAT_COMMENT;
          break;
@@ -31,7 +31,7 @@ $NetBSD: patch-pandas___libs_src_parser_tokenizer.c,v 1.1 2025/10/31 00:22:22 ry
          self->state = START_FIELD;
          PD_FALLTHROUGH; // fall through to subsequent state
        } else {
-@@ -846,7 +846,7 @@ static int tokenize_bytes(parser_t *self
+@@ -850,7 +850,7 @@ static int tokenize_bytes(parser_t *self
        } else if (IS_COMMENT_CHAR(c)) {
          self->state = EAT_LINE_COMMENT;
          break;
--- math/py-pandas/patches/patch-pandas___libs_window_aggregations.pyx
+++ math/py-pandas/patches/patch-pandas___libs_window_aggregations.pyx
@@ -1,36 +1,44 @@
-$NetBSD: patch-pandas___libs_window_aggregations.pyx,v 1.3 2023/08/28 10:34:02 adam Exp $
+$NetBSD: patch-pandas___libs_window_aggregations.pyx,v 1.4 2026/06/05 10:41:53 adam Exp $
 
-On NetBSD, signbit and sqrt cannot be imported from "libc", because
-even though <math.h> is included, Python.h later includes
-<cmath> which in turn #undef's all the relevant definitions
-from <math.h>, forcing the use of the std:: variants of those
-functions.  So ... re-do how we import signbit() and sqrt().
+On NetBSD, isnan, signbit and sqrt cannot be imported from "libc", because even
+though <math.h> is included, Python.h later includes <cmath> which in turn
+#undef's all the relevant definitions from <math.h>, forcing the use of the
+std:: variants of those functions.
 
 Submitted upstream:
 https://github.com/pandas-dev/pandas/pull/51049
 
---- pandas/_libs/window/aggregations.pyx.orig	2023-03-16 14:52:19.000000000 +0000
+--- pandas/_libs/window/aggregations.pyx.orig	2026-05-11 16:18:35.000000000 +0000
 +++ pandas/_libs/window/aggregations.pyx
 @@ -1,10 +1,6 @@
  # cython: boundscheck=False, wraparound=False, cdivision=True
  
 -from libc.math cimport (
--    round,
+-    fabs,
 -    signbit,
 -    sqrt,
 -)
-+from libc.math cimport round
++from libc.math cimport fabs;
  from libcpp.deque cimport deque
- 
- from pandas._libs.algos cimport TiebreakEnumType
-@@ -19,6 +15,10 @@ from numpy cimport (
+ from libcpp.stack cimport stack
+ from libcpp.unordered_map cimport unordered_map
+@@ -21,6 +17,11 @@ from numpy cimport (
      ndarray,
  )
  
 +cdef extern from "<cmath>" namespace "std":
++    int isnan(float64_t) nogil
 +    int signbit(float64_t) nogil
 +    float64_t sqrt(float64_t x) nogil
 +
  cnp.import_array()
  
  import cython
+@@ -995,7 +996,6 @@ cdef int64_t bisect_left(
+             hi = mid
+     return lo
+ 
+-from libc.math cimport isnan
+ 
+ # Prior version of moving maximum / minimum code taken from Bottleneck
+ # Licence at LICENSES/BOTTLENECK_LICENCE
