0c0a332bd0bb8ccb6399f146d3bd7218f03696db
adam <adam@pkgsrc.org>
2026-06-07 17:30:05 +0000

    rtmpdump: switch to openssl (which is the default) to fix problem building with nettle 4.0

--- net/rtmpdump/buildlink3.mk
+++ net/rtmpdump/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2026/05/14 16:41:50 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2026/06/07 17:30:05 adam Exp $
 
 BUILDLINK_TREE+=	rtmpdump
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.rtmpdump+=	rtmpdump>=2.6.0.0.20240301nb2
 BUILDLINK_PKGSRCDIR.rtmpdump?=		../../net/rtmpdump
 
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../security/gnutls/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif	# RTMPDUMP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rtmpdump
