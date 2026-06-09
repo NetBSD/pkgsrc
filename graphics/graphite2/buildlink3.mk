b4e950eefc671942c7b7c00088286f5f491b7160
adam <adam@pkgsrc.org>
2026-06-09 07:52:22 +0000

    graphite2: updated to 1.3.15
    1.3.15
    . Bug fixes.
    . Update graphite website documentation.
    . Use SPDX lines, and improve license declarations.
    . Fix incorrectly generated graphite2.pc pkgconf file.

--- graphics/graphite2/buildlink3.mk
+++ graphics/graphite2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2025/08/11 08:26:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2026/06/09 07:52:22 adam Exp $
 
 BUILDLINK_TREE+=	graphite2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	graphite2
 GRAPHITE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.graphite2+=	graphite2>=1.2
-BUILDLINK_ABI_DEPENDS.graphite2?=	graphite2>=1.3.14nb2
+BUILDLINK_ABI_DEPENDS.graphite2+=	graphite2>=1.3.14nb2
 BUILDLINK_PKGSRCDIR.graphite2?=		../../graphics/graphite2
 .endif # GRAPHITE2_BUILDLINK3_MK
 
