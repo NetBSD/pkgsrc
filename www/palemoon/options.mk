d988cb237672dc3e0cc5c3cbba94c46defa4d10b
maya <maya@pkgsrc.org>
2026-06-10 01:32:44 +0000

    Pullup ticket #7129 - requested by nia www/palemoon: Security fix
    Revisions pulled up:
    - www/palemoon/Makefile.common                                  1.7
    - www/palemoon/distinfo                                         1.44
    - www/palemoon/options.mk                                       1.4
    - www/palemoon/patches/patch-platform__media__ffvpx__config_unix_ppc.h deleted
    - www/palemoon/patches/patch-platform_layout_base_FrameProperties.h deleted
    - www/palemoon/patches/patch-platform_media_libvpx_config_linux_arm64_vpx__config.h deleted
    - www/palemoon/patches/patch-platform_security_generate__mapfile.py deleted
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Sat Jun  6 12:50:07 UTC 2026
    
       Modified Files:
       	pkgsrc/www/palemoon: Makefile.common distinfo options.mk
       Removed Files:
       	pkgsrc/www/palemoon/patches:
       	    patch-platform__media__ffvpx__config_unix_ppc.h
       	    patch-platform_layout_base_FrameProperties.h
       	    patch-platform_media_libvpx_config_linux_arm64_vpx__config.h
       	    patch-platform_security_generate__mapfile.py
    
       Log Message:
       palemoon: Update to 34.3.0
    
       - Enhanced support for ES2024, and newer CSS features.
       - Builds with Python 3.
       - Upstreamed NetBSD/powerpc support.
       - A large security audit was performed.
    
       Build tested on CentOS 7 and NetBSD/sparc64.

--- www/palemoon/options.mk
+++ www/palemoon/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2026/02/22 11:05:44 nia Exp $
+# $NetBSD: options.mk,v 1.2.2.1 2026/06/10 01:32:44 maya Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.palemoon
 PKG_SUPPORTED_OPTIONS=		dbus pulseaudio
@@ -8,7 +8,6 @@ PKG_SUPPORTED_OPTIONS=		dbus pulseaudio
 .if !empty(PKG_OPTIONS:Mdbus)
 CONFIGURE_ARGS+=	--enable-dbus
 .  include "../../sysutils/dbus/buildlink3.mk"
-.  include "../../sysutils/dbus-glib/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-dbus
 .endif
