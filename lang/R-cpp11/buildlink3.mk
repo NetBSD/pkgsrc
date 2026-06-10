aee75aa6be835325e84daa8b33a66189b32c978b
mef <mef@pkgsrc.org>
2026-06-10 12:25:54 +0000

    (lang/R-cpp11) API/ABI requirement updated

--- lang/R-cpp11/buildlink3.mk
+++ lang/R-cpp11/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/04/30 14:33:39 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/06/10 12:25:54 mef Exp $
 #
 
 BUILDLINK_TREE+=	r_cpp11
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	r_cpp11
 .if !defined(R_CPP11_BUILDLINK3_MK)
 R_CPP11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.r_cpp11+=	R-cpp11>=0.4.2
-BUILDLINK_ABI_DEPENDS.r_cpp11+=	R-cpp11>=0.4.2
+BUILDLINK_API_DEPENDS.r_cpp11+=	R-cpp11>=0.5.2
+BUILDLINK_ABI_DEPENDS.r_cpp11+=	R-cpp11>=0.5.2
 BUILDLINK_PKGSRCDIR.r_cpp11?=	../../lang/R-cpp11
 
 BUILDLINK_FILES.r_cpp11=	lib/R/library/cpp11/include/cpp11/*
