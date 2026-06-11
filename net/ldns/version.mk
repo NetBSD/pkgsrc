00e7d97831210525b344bc2f91953f90cfd4fed9
adam <adam@pkgsrc.org>
2026-06-11 12:42:25 +0000

    ldns: updated to 1.9.2
    1.9.2   2026-06-10
    * Fix to set VERSION_INFO to create .so.3 instead of .so.11 which will
      be reserved for a future 1.10.0 release
    
    1.9.1   2026-06-10
    * Bugfix: Insufficient verification that responses belong to a
      query (CVE-2026-10846). Thanks Pablo Ruiz from 'codecome.ai'

--- net/ldns/version.mk
+++ net/ldns/version.mk
@@ -1,8 +1,8 @@
-# $NetBSD: version.mk,v 1.3 2026/04/21 09:19:56 adam Exp $
+# $NetBSD: version.mk,v 1.4 2026/06/11 12:42:25 adam Exp $
 # used by net/ldns/Makefile
 # used by net/drill/Makefile
 
-LDNS_VERSION=	1.9.0
+LDNS_VERSION=	1.9.2
 
 DISTNAME=	ldns-${LDNS_VERSION}
 CATEGORIES=	net
