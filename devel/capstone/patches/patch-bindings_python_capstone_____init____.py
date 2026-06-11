8698fe2100f7d123930a6600f93f12d8fc2ab3f6
wiz <wiz@pkgsrc.org>
2026-06-11 07:23:26 +0000

    capstone: update to 5.0.9.
    Version 5.0.9: May 28th, 2026
    
    ## What's Changed
    
    * Set the correct CS_VERSION_EXTRA version number in capstone.h
    * Fix reachable OOB read in M68KInstPrinter.c (GHSA-jrw4-wj52-2vw8).
    * Fix invalid decoding for malformed br_table instruction in WASM (GHSA-5m9f-vqcm-g5pr).
    
    --------------------------------
    Version 5.0.8: May 23th, 2026
    
    ## What's Changed
    * Python binding: Removing xprint utility used in the tests in v5 branch by @Antelox in https://github.com/capstone-engine/capstone/pull/2873
    * v5: Speedup x86 decoding with the help of LUT tables by @trufae in https://github.com/capstone-engine/capstone/pull/2907
    * Backport #2886 - cs_disasm_iter: return early on zero-size input by @Rot127 in https://github.com/capstone-engine/capstone/pull/2920
    * Backport #2913 - Implement load libcapstone.so with RTLD_DEEPBIND by @Rot127 in https://github.com/capstone-engine/capstone/pull/2919
    * Backport #2915 - Use setattr to rewire Cs.disasm to avoid type checking errors by @Rot127 in https://github.com/capstone-engine/capstone/pull/2918
    * Backport of CVE-2026-47143 fix by @Rot127 in https://github.com/capstone-engine/capstone/pull/2924
    * Backport #2930 by @Rot127 in https://github.com/capstone-engine/capstone/pull/2931

--- devel/capstone/patches/patch-bindings_python_capstone_____init____.py
+++ devel/capstone/patches/patch-bindings_python_capstone_____init____.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-bindings_python_capstone_____init____.py,v 1.2 2020/08/18 20:20:46 riastradh Exp $
+$NetBSD: patch-bindings_python_capstone_____init____.py,v 1.3 2026/06/11 07:23:26 wiz Exp $
 
---- bindings/python/capstone/__init__.py.orig	2020-05-08 10:03:30.000000000 +0000
+--- bindings/python/capstone/__init__.py.orig	2026-05-28 15:32:54.000000000 +0000
 +++ bindings/python/capstone/__init__.py
-@@ -280,7 +280,7 @@ else:
+@@ -373,7 +373,7 @@ def _load_lib(path):
  _found = False
  
  def _load_lib(path):
 -    lib_file = join(path, _lib)
 +    lib_file = join(sys.prefix, 'lib', _lib)
      if os.path.exists(lib_file):
-         return ctypes.cdll.LoadLibrary(lib_file)
+         return ctypes.CDLL(lib_file, mode=mode)
      else:
