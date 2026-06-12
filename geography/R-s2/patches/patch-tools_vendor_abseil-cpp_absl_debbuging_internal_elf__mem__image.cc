7353f353140e6cb5d04db242d651ec6182e4e554
mef <mef@pkgsrc.org>
2026-06-12 13:07:51 +0000

    (geography/R-s2) Updated 1.1.7 to 1.1.9
    # s2 (development version)
    
    * Fix code to help gcc-ubsan understand the region coverer (#275)
    * Inspect `S2_FORCE_BUNDLED_ABSEIL` in `conifigure`: if non-empty, any system
      install of Abseil is ignored (e.g., if using a non-standard compiler on a
      system where system Abseil is available via pkg-config) (#275)
    * Disable optimization in compact_array.h that confused compilers when compiling
      with `-Wpedantic` (#275).
    * Add `cmake` to SystemReqirements. Even though this is technically optional
      (system Abseil can be used), adding to requirements helps some installers
      automatically install the dependency (#277).
    
    # s2 1.1.8
    
    * `s2_buffer_cells()` recycles `max_dist` and `min_level` arguments, allowing
       to specify these by feature (#264 and
       https://github.com/r-spatial/sf/issues/2488).
    * The internal version of s2geometry is now 0.11.1 (#257, #263).
    * The Abseil dependency is resolved using pkg-config where possible.
      Where this is not possible, a vendored version of Abseil will be built using
      CMake (#258).

--- geography/R-s2/patches/patch-tools_vendor_abseil-cpp_absl_debbuging_internal_elf__mem__image.cc
+++ geography/R-s2/patches/patch-tools_vendor_abseil-cpp_absl_debbuging_internal_elf__mem__image.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_absl_debugging_internal_elf__mem__image.cc,v 1.1 2026/04/20 06:06:38 wiz Exp $
+$NetBSD: patch-tools_vendor_abseil-cpp_absl_debbuging_internal_elf__mem__image.cc,v 1.1 2026/06/12 13:07:51 mef Exp $
 
 Fix for ELF header updates in NetBSD 11.
 
---- src/absl/debugging/internal/elf_mem_image.cc.orig	2023-11-25 15:31:07.000000000 +0000
-+++ src/absl/debugging/internal/elf_mem_image.cc
-@@ -351,7 +351,7 @@ void ElfMemImage::SymbolIterator::Update(int increment
+--- tools/vendor/abseil-cpp/absl/debugging/internal/elf_mem_image.cc.orig	2026-06-12 21:57:03.074022311 +0900
++++ tools/vendor/abseil-cpp/absl/debugging/internal/elf_mem_image.cc	2026-06-12 21:58:19.429281523 +0900
+@@ -350,7 +350,7 @@ void ElfMemImage::SymbolIterator::Update
    const ElfW(Versym) *version_symbol = image->GetVersym(index_);
    ABSL_RAW_CHECK(symbol && version_symbol, "");
    const char *const symbol_name = image->GetDynstr(symbol->st_name);
