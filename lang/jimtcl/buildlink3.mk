b0201101879ec60d5e4050bc2688e1146e7291bb
thorpej <thorpej@pkgsrc.org>
2026-06-18 13:12:35 +0000

    jimtcl: bump BUILDLINK_API_DEPENDS to 0.83nb1.
    The current devel/openocd (0.12.0) fails to compile with jimtcl < 0.83,
    but since the BUILDLINK_API_DEPENDS was 0.80nb1, you would not notice
    the failure unless you had an older version of jimtcl already installed.
    
    So, jimtcl is now 0.83nb1, BUILDLINK_API_DEPENDS set to match, and openocd
    bumped to 0.12.0nb1 to force the correct dependency to be recorded.

--- lang/jimtcl/buildlink3.mk
+++ lang/jimtcl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2024/01/10 08:20:10 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2026/06/18 13:12:35 thorpej Exp $
 
 BUILDLINK_TREE+=	jimtcl
 
 .if !defined(JIMTCL_BUILDLINK3_MK)
 JIMTCL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jimtcl+=	jimtcl>=0.80nb1
+BUILDLINK_API_DEPENDS.jimtcl+=	jimtcl>=0.83nb1
 BUILDLINK_PKGSRCDIR.jimtcl?=	../../lang/jimtcl
 
 .include "../../devel/zlib/buildlink3.mk"
