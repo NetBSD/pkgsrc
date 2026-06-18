2e68eb44ab80c7987b6f8c4390f5235848af2948
nia <nia@pkgsrc.org>
2026-06-18 14:56:55 +0000

    ted: Handle the case where GNU libiconv gets pulled in.

--- editors/ted/patches/patch-textEncoding_textConverter.c
+++ editors/ted/patches/patch-textEncoding_textConverter.c
@@ -1,10 +1,13 @@
-$NetBSD: patch-textEncoding_textConverter.c,v 1.1 2025/12/18 09:14:40 nia Exp $
+$NetBSD: patch-textEncoding_textConverter.c,v 1.2 2026/06/18 14:56:55 nia Exp $
 
 Support non-POSIX iconv in SunOS and old NetBSD.
 
---- textEncoding/textConverter.c.orig	2025-12-18 08:30:05.458713346 +0000
+Making sure to carefully handle the case of an external iconv being
+pulled in from GNU libiconv.
+
+--- textEncoding/textConverter.c.orig	2013-02-01 12:11:08.000000000 +0000
 +++ textEncoding/textConverter.c
-@@ -20,6 +20,15 @@
+@@ -20,6 +20,13 @@
  #   include	"uniLegacyEncoding.h"
  #   include	"uniLegacyMapping.h"
  
@@ -12,19 +15,17 @@ Support non-POSIX iconv in SunOS and old NetBSD.
 +#include <sys/param.h>
 +#if __NetBSD_Prereq__(9,99,17)
 +#define NETBSD_POSIX_ICONV 1
-+#else
-+#define NETBSD_POSIX_ICONV 0
 +#endif
 +#endif
 +
  void textConverterSetNativeEncodingName(	TextConverter *	tc,
  						const char *	encodingName )
      {
-@@ -265,7 +274,11 @@ static int textConverterConvertIconv(	TextConverter *	
+@@ -265,7 +272,11 @@ static int textConverterConvertIconv(	TextConverter *	
  	    /*  return value is irrelevant: Just tells that the	*/
  	    /*  conversion was incomplete. So does ileft > 0.	*/
  
-+#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
++#if (defined(__NetBSD__) || defined(__sun)) && !defined(NETBSD_POSIX_ICONV) && !defined(_LIBICONV_VERSION)
 +	    (void)iconv( ico, (const char **)&ibuf, &ileft, &obuf, &oleft );
 +#else
  	    (void)iconv( ico, &ibuf, &ileft, &obuf, &oleft );
