e5357201fc6363c570a29ee6a60f094f6cce3450
nia <nia@pkgsrc.org>
2026-06-20 00:33:12 +0000

    libmtp: Handle the case where the iconv(3) is overridden by GNU libiconv.

--- devel/libmtp/patches/patch-src_unicode.c
+++ devel/libmtp/patches/patch-src_unicode.c
@@ -1,10 +1,11 @@
-$NetBSD: patch-src_unicode.c,v 1.1 2025/12/19 11:00:46 nia Exp $
+$NetBSD: patch-src_unicode.c,v 1.2 2026/06/20 00:33:12 nia Exp $
 
 Support non-POSIX iconv in SunOS and old NetBSD.
+Make sure that GNU libiconv isn't being used.
 
---- src/unicode.c.orig	2025-12-19 10:50:53.932872746 +0000
+--- src/unicode.c.orig	2022-11-15 09:28:49.000000000 +0000
 +++ src/unicode.c
-@@ -41,6 +41,15 @@
+@@ -41,6 +41,13 @@
  #include "util.h"
  #include "ptp.h"
  
@@ -12,15 +13,13 @@ Support non-POSIX iconv in SunOS and old NetBSD.
 +#include <sys/param.h>
 +#if __NetBSD_Prereq__(9,99,17)
 +#define NETBSD_POSIX_ICONV 1
-+#else
-+#define NETBSD_POSIX_ICONV 0
 +#endif
 +#endif
 +
  /**
   * The size of the buffer (in characters) used for creating string copies.
   */
-@@ -81,7 +90,11 @@ char *utf16_to_utf8(LIBMTP_mtpdevice_t *device, const 
+@@ -81,7 +88,11 @@ char *utf16_to_utf8(LIBMTP_mtpdevice_t *device, const 
    loclstr[0]='\0';
    #if defined(HAVE_ICONV) && defined(HAVE_LANGINFO_H)
    PTPParams *params = (PTPParams *) device->params;
@@ -32,11 +31,11 @@ Support non-POSIX iconv in SunOS and old NetBSD.
    char *locp = loclstr;
    size_t nconv;
    size_t convlen = (ucs2_strlen(unicstr)+1) * sizeof(uint16_t); // UCS-2 is 16 bit wide, include terminator
-@@ -120,7 +133,11 @@ uint16_t *utf8_to_utf16(LIBMTP_mtpdevice_t *device, co
+@@ -120,7 +131,11 @@ uint16_t *utf8_to_utf16(LIBMTP_mtpdevice_t *device, co
  
    #if defined(HAVE_ICONV) && defined(HAVE_LANGINFO_H)
    PTPParams *params = (PTPParams *) device->params;
-+#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
++#if (defined(__NetBSD__) || defined(__sun)) && !defined(NETBSD_POSIX_ICONV) && !defined(_LIBICONV_VERSION)
 +  const char *stringp = (const char *) localstr;
 +#else
    char *stringp = (char *) localstr; // cast away "const"
