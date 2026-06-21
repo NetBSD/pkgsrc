9c219d61e50a5908b2700104ac570a8b36ccc5be
tron <tron@pkgsrc.org>
2026-06-21 07:31:00 +0000

    mail/mutt: Update to version 2.4.0
    Changes since version 2.3.3:
      ! Maildir folders are now only recognized if they contain cur, new, and tmp
        subfolders.  Previously mutt only checked for the cur subfolder when
        opening.
      ! --without-wc-funcs configure option is deprecated.
      + New functions <open-thread>, <open-all-threads>, <close-thread>,
        <close-all-threads> were added to explicitly open/close a thread, in
        addition to the thread toggle functions.
      + $tmpdraftdir, defaulting /var/tmp, sets the directory where message
        composition drafts are saved.
      ! --textmode is no longer required for pgp classic mode signing, although
        mutt still keeps the flags in the sample config files.
      ! ~C and ~L patterns match Bcc recipient lists too.
      ! When querying for addresses, via <query> or <complete-query>,
        the query menu can be exiting via <quit> after tagging entries.
        Previously, <select-entry> had to be used for tagged entries to be
        processed.
      ! Classic S/MIME now generates application/pkcs7-mime and
        application/pkcs7-signature types instead of the deprecated
        x-pkcs7-mime and x-pkcs7-signature types.
      ! Saving attachments will sanitize filenames by substituting '_' for '/'.
      ! Regular expression handling when searching in the pager has been
        improved slightly.  (REG_NEWLINE mode has been turned off, and
        newlines are excluded from searches).  This means searches such
        as /^$ now work properly.
      ! When compiled with OpenSSL, Mutt follows RFC6125, and does not match
        against CN entries in the certificate if there are any DNS entries.

--- mail/mutt/patches/patch-color.c
+++ mail/mutt/patches/patch-color.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-color.c,v 1.2 2020/12/05 13:02:03 tonio Exp $
+$NetBSD: patch-color.c,v 1.3 2026/06/21 07:31:00 tron Exp $
 
 Fix build under NetBSD
 
---- color.c.orig	2020-11-03 17:50:37.000000000 +0000
-+++ color.c	2020-11-08 16:39:21.085043268 +0000
-@@ -119,7 +119,7 @@
+--- color.c.orig	2026-06-12 06:03:22.000000000 +0100
++++ color.c	2026-06-21 08:25:36.282919578 +0100
+@@ -133,7 +133,7 @@
  
- #define COLOR_QUOTE_INIT	8
+ #define COLOR_QUOTE_INIT        8
  
 -#ifdef NCURSES_VERSION
 +#if defined(NCURSES_VERSION) || (defined(__NetBSD__) && !defined(USE_SLANG_CURSES))
  #define ATTR_MASK (A_ATTRIBUTES ^ A_COLOR)
- #elif defined (USE_SLANG_CURSES)
+ #elif defined(USE_SLANG_CURSES)
  #define ATTR_MASK (~(unsigned int)A_NORMAL ^ (A_CHARTEXT | A_UNUSED | A_COLOR))
