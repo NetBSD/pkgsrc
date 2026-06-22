c2ba5f179258abb3e7e7a0dfbff7e2bc08fc9576
kikadf <kikadf@pkgsrc.org>
2026-06-22 08:17:58 +0000

    vulkan-tools: update to vulkan-sdk-1.4.350.1
    No changelog

--- graphics/vulkan-tools/patches/patch-cube_cube.c
+++ graphics/vulkan-tools/patches/patch-cube_cube.c
@@ -1,53 +1,26 @@
-$NetBSD: patch-cube_cube.c,v 1.1 2025/10/04 12:39:56 tnn Exp $
+$NetBSD: patch-cube_cube.c,v 1.2 2026/06/22 08:17:58 kikadf Exp $
 
 Allow building on NetBSD.
+Fix ctype usage
 
---- cube/cube.c.orig	2025-10-04 12:29:59.680457395 +0000
+--- cube/cube.c.orig	2026-05-04 19:54:00.000000000 +0200
 +++ cube/cube.c
-@@ -44,7 +44,9 @@
- #include "xcb_loader.h"
- #endif
- #if defined(VK_USE_PLATFORM_WAYLAND_KHR)
-+#if defined(__linux__)
- #include <linux/input.h>
-+#endif
- #include "wayland_loader.h"
- #endif
- 
-@@ -3581,9 +3583,11 @@ static void pointer_handle_motion(void *
- static void pointer_handle_button(void *data, struct wl_pointer *wl_pointer, uint32_t serial, uint32_t time, uint32_t button,
-                                   uint32_t state) {
-     struct demo *demo = data;
-+#if defined(__linux__)
-     if (button == BTN_LEFT && state == WL_POINTER_BUTTON_STATE_PRESSED) {
-         xdg_toplevel_move(demo->xdg_toplevel, demo->seat, serial);
-     }
-+#endif
- }
- 
- static void pointer_handle_axis(void *data, struct wl_pointer *wl_pointer, uint32_t time, uint32_t axis, wl_fixed_t value) {}
-@@ -3603,6 +3607,7 @@ static void keyboard_handle_key(void *da
-                                 uint32_t state) {
-     if (state != WL_KEYBOARD_KEY_STATE_RELEASED) return;
-     struct demo *demo = data;
-+#if defined(__linux__)
-     switch (key) {
-         case KEY_ESC:  // Escape
-             demo->quit = true;
-@@ -3617,6 +3622,7 @@ static void keyboard_handle_key(void *da
-             demo->pause = !demo->pause;
-             break;
-     }
-+#endif
- }
- 
- static void keyboard_handle_modifiers(void *data, struct wl_keyboard *keyboard, uint32_t serial, uint32_t mods_depressed,
-@@ -5079,7 +5085,7 @@ void android_main(struct android_app *ap
+@@ -4780,7 +4780,7 @@ static void demo_init(struct demo *demo,
+         if ((strcmp(argv[i], "--wsi") == 0) && (i < argc - 1)) {
+             size_t argc_len = strlen(argv[i + 1]);
+             for (size_t argc_i = 0; argc_i < argc_len; argc_i++) {
+-                argv[i + 1][argc_i] = tolower(argv[i + 1][argc_i]);
++                argv[i + 1][argc_i] = tolower((unsigned char)argv[i + 1][argc_i]);
+             }
+             WSI_PLATFORM selection = wsi_from_string(argv[i + 1]);
+             if (selection == WSI_PLATFORM_INVALID) {
+@@ -5098,7 +5098,8 @@ void android_main(struct android_app *ap
          }
      }
  }
 -#elif defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__QNX__) || defined(__GNU__)
-+#elif defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__QNX__) || defined(__GNU__) || defined(__NetBSD__)
++#elif defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__QNX__) || defined(__GNU__) || \
++      defined(__NetBSD__)
  int main(int argc, char **argv) {
      struct demo demo;
  
