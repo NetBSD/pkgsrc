273b8ecb4e3d2ead42cbcf8ed2f99332e68e163e
wiz <wiz@pkgsrc.org>
2026-06-23 10:30:58 +0000

    gexiv2: do not accept gexiv2 0.16

--- graphics/gexiv2/buildlink3.mk
+++ graphics/gexiv2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2025/10/23 20:37:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2026/06/23 10:30:58 wiz Exp $
 
 BUILDLINK_TREE+=	gexiv2
 
 .if !defined(GEXIV2_BUILDLINK3_MK)
 GEXIV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gexiv2+=	gexiv2>=0.12.0
-BUILDLINK_ABI_DEPENDS.gexiv2+=	gexiv2>=0.14.3nb2
+BUILDLINK_API_DEPENDS.gexiv2+=	gexiv2>=0.12.0<0.16
+BUILDLINK_ABI_DEPENDS.gexiv2+=	gexiv2>=0.14.3nb2<0.16
 BUILDLINK_PKGSRCDIR.gexiv2?=	../../graphics/gexiv2
 
 .include "../../devel/glib2/buildlink3.mk"
