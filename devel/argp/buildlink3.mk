32e5b51658bade187dd652e03f5a1ee79548d50f
wiz <wiz@pkgsrc.org>
2026-06-23 09:52:56 +0000

    *: recursive bump for argp 1.5

--- devel/argp/buildlink3.mk
+++ devel/argp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2026/06/23 09:52:25 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/06/23 09:52:56 wiz Exp $
 
 BUILDLINK_TREE+=	argp
 
@@ -8,6 +8,7 @@ ARGP_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.argp?=	build
 
 BUILDLINK_API_DEPENDS.argp+=	argp>=1.5.0
+BUILDLINK_ABI_DEPENDS.argp?=	argp>=1.5.0
 BUILDLINK_PKGSRCDIR.argp?=	../../devel/argp
 .endif	# ARGP_BUILDLINK3_MK
 
