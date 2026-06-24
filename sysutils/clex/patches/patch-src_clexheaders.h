d17cd909c9f7ccbafe0c7cef0abf3e3cb0fb3013
drixter <drixter@pkgsrc.org>
2026-06-24 11:53:10 +0000

    clex: PR pkg/60361 (sysutils/clex: update to 4.7)

--- sysutils/clex/patches/patch-src_clexheaders.h
+++ sysutils/clex/patches/patch-src_clexheaders.h
@@ -1,12 +1,15 @@
-$NetBSD: patch-src_clexheaders.h,v 1.1 2012/02/17 13:23:28 obache Exp $
+$NetBSD: patch-src_clexheaders.h,v 1.2 2026/06/24 11:53:10 drixter Exp $
 
-* Need to treat *BSD same as FreeBSD.
+ 
+ * Need to treat *BSD same as FreeBSD.
+ 
 
---- src/clexheaders.h.orig	2009-01-11 19:44:35.000000000 +0000
+--- src/clexheaders.h.orig	2026-06-24 08:43:18.387468661 +0000
 +++ src/clexheaders.h
-@@ -1,4 +1,4 @@
--#if !defined(__APPLE__) && !defined(__FreeBSD__)
-+#if !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__)
+@@ -1,6 +1,6 @@
+ #if defined(__APPLE__)
+ # define _XOPEN_SOURCE_EXTENDED
+-#elif !defined(__FreeBSD__)
++#elif !defined(__FreeBSD__) && !defined(__NetBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__)
  # define _XOPEN_SOURCE 600
  #endif
- 
