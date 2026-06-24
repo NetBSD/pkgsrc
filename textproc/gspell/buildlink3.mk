c42fb8919dad8d37413bf155054437a8571146a4
wiz <wiz@pkgsrc.org>
2026-06-24 22:12:32 +0000

    *: recursive bump for gspell 1.14

--- textproc/gspell/buildlink3.mk
+++ textproc/gspell/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2026/06/24 22:12:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2026/06/24 22:12:32 wiz Exp $
 
 BUILDLINK_TREE+=	gspell
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gspell
 GSPELL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gspell+=	gspell>=1.0.1
-BUILDLINK_ABI_DEPENDS.gspell+=	gspell>=1.8.4nb14
+BUILDLINK_ABI_DEPENDS.gspell+=	gspell>=1.14.3
 BUILDLINK_PKGSRCDIR.gspell?=	../../textproc/gspell
 
 .include "../../devel/glib2/buildlink3.mk"
