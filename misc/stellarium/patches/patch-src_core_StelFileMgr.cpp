fae68a95618511ff7f0b0e833b44b619ad8cb5a1
adam <adam@pkgsrc.org>
2026-06-25 08:32:03 +0000

    stellarium: updated to 26.2
    26.2
    
    The major changes of this version:
    - Added new sky culture
    - Added new plugin: Planes
    - Many improvements in plugins
    - Many improvements in Core and GUI
    - Many updates in sky cultures

--- misc/stellarium/patches/patch-src_core_StelFileMgr.cpp
+++ misc/stellarium/patches/patch-src_core_StelFileMgr.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_core_StelFileMgr.cpp,v 1.3 2022/10/03 09:07:03 adam Exp $
+$NetBSD: patch-src_core_StelFileMgr.cpp,v 1.4 2026/06/25 08:32:03 adam Exp $
 
 On Darwin, do not use bundles.
 
---- src/core/StelFileMgr.cpp.orig	2022-09-21 12:07:19.000000000 +0000
+--- src/core/StelFileMgr.cpp.orig	2026-06-24 16:39:30.000000000 +0000
 +++ src/core/StelFileMgr.cpp
-@@ -103,7 +103,7 @@ void StelFileMgr::init()
- 	}	
- 	else
- 	{
--	#if defined(Q_OS_MACOS)
-+	#if 0
- 		QString relativePath = "/../Resources";
- 		if (QCoreApplication::applicationDirPath().contains("src")) {
- 			relativePath = "/../..";
+@@ -116,7 +116,7 @@ void StelFileMgr::init()
+ 		searchPaths += info.absoluteFilePath() + "/0/stellarium";
+ 	}
+ 	searchPaths += "/sdcard/stellarium";
+-#elif defined(Q_OS_MACOS)
++#elif 0
+ 	QString relativePath = "/../Resources";
+ 	if (QCoreApplication::applicationDirPath().contains("src")) {
+ 		relativePath = "/../..";
