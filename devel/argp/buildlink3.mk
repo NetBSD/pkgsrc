427b97515ce5bf4b9b69e21fbbae23bf054c9641
wiz <wiz@pkgsrc.org>
2026-06-26 06:15:09 +0000

    argp: install shared library
    Bump PKGREVISION.
    
    Fixes xentools420 build, tested by gdt@ - thanks!

--- devel/argp/buildlink3.mk
+++ devel/argp/buildlink3.mk
@@ -1,14 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2026/06/23 09:52:56 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2026/06/26 06:15:09 wiz Exp $
 
 BUILDLINK_TREE+=	argp
 
 .if !defined(ARGP_BUILDLINK3_MK)
 ARGP_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.argp?=	build
-
-BUILDLINK_API_DEPENDS.argp+=	argp>=1.5.0
-BUILDLINK_ABI_DEPENDS.argp?=	argp>=1.5.0
+BUILDLINK_API_DEPENDS.argp+=	argp>=1.5.0nb1
+BUILDLINK_ABI_DEPENDS.argp?=	argp>=1.5.0nb1
 BUILDLINK_PKGSRCDIR.argp?=	../../devel/argp
 .endif	# ARGP_BUILDLINK3_MK
 
