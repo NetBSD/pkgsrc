ffcf35819813678a2755fb71dd3d912b05b68432
morr <morr@pkgsrc.org>
2026-06-29 19:14:13 +0000

    Update to version 9.2.0747.
    Changes:
    - patch 9.2.0747: cscope: connection leak when growing the array fails
    - patch 9.2.0746: NULL pointer dereference in gui_photon
    - patch 9.2.0745: Crash with truncated spellfile
    - patch 9.2.0744: popup_atcursor() closes immediately on white space
    - runtime(odin): Update indent script, add indent tests
    - CI: MS-Windows: Run gvim/vim tests in parallel
    - patch 9.2.0743: string macros silently accept a size of the wrong type
    - runtime(vim): Fix heredoc triggering misidentifcation of Vim9 script
    - patch 9.2.0742: filetype: SSH keys and related filetypes not recognized
    - runtime(css): add more missing CSS properties
    - patch 9.2.0741: complete_check() does not return TRUE for mapped input
    - patch 9.2.0740: GTK4: scrollbar wrongly displayed
    - patch 9.2.0739: completion: 'autocompletedelay' blocks the main loop and drops autocommands
    - runtime: guard recommended style settings consistently
    - patch 9.2.0738: ml_recover() may write beyond block buffer
    - CI: Bump actions/checkout in the github-actions group across 1 directory
    - patch 9.2.0737: tests: comment test can be improved
    - patch 9.2.0736: potential command execution in PHP omni-completion
    - patch 9.2.0735: [security]: arbitrary Ex command execution during C omni-completion
    - runtime(doc): Tweak documentation style and typo
    - patch 9.2.0734: function pointer passed to STRNCMP() instead of a length
    - runtime(cabal): Update compiler, ftplugin, syntax, add indent script
    - patch 9.2.0733: GTK3: GUI slow on X11 since dropping the alpha channel
    - patch 9.2.0732: session: terminal restored using absolute columns/rows
    - patch 9.2.0731: GTK4 GUI scrollbar size not updated when restoring a session
    - patch 9.2.0730: GTK4 GUI tabline is not updated
    - patch 9.2.0729: % skips parens on continued quoted lines
    - runtime(doc): Add installer updates to version9.txt
    - patch 9.2.0728: filetype: supertux info pattern is relative to current dir
    - patch 9.2.0727: popup images not rendered correctly when unfocused
    - patch 9.2.0726: filetype detect missing from completion
    - patch 9.2.0725: [security]: Stack out-of-bounds write in spell_soundfold_sal()
    - patch 9.2.0724: Use-after-free when freeing exit_cb job on exit
    - patch 9.2.0723: term_start() does not support "noclose"
    - patch 9.2.0722: GTK4: find/replace dialog can be improved
    - patch 9.2.0721: serverlist() returns strings separated by \n
    - patch 9.2.0720: GTK4: no support for browsefilter
    - patch 9.2.0719: GTK4: default menu is lacking
    - patch 9.2.0718: :syn sync without an argument also lists syntax cluster
    - patch 9.2.0717: tests: strange indent in Test_autocmd_dup_arg()
    - patch 9.2.0716: filetype: not all supertux files are recognized
    - patch 9.2.0715: Coverity warns about copy/paste error in hl_blend_attr()
    - patch 9.2.0714: Coverity warns for NULL deref
    - patch 9.2.0713: completion: ruler not updated correctly when the popup menu is visible
    - patch 9.2.0712: GTK4: dialogs not handling mnemonics correctly
    - patch 9.2.0711: leak in ins_compl_infercase_gettext() in error case
    - patch 9.2.0710: GTK4 GUI resize handling can be improved
    - patch 9.2.0709: GTK4: a few minor issues
    - patch 9.2.0708: Leaks in do_autocmd in error case
    - runtime(dnsmasq): add new keywords and order existing keywords alphabetically
    - CI: Restore daily Coverity Scan

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.176 2026/06/22 22:54:13 morr Exp $
+# $NetBSD: version.mk,v 1.177 2026/06/29 19:14:13 morr Exp $
 
 VIM_VERSION=		9.2
-VIM_PATCHLEVEL=		0707
+VIM_PATCHLEVEL=		0747
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s" v<old_version>...HEAD
 VIM_SUBDIR=		vim92
