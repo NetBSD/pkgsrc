c8e18a94350a1d32a0b5f7ff269c2c5f1ea94617
adam <adam@pkgsrc.org>
2026-06-30 20:27:08 +0000

    py-curl: updated to 7.47.0
    PycURL 7.47.0 - 2026-06-29
    
    This release adds AsyncCurlMulti (initial async pycurl support), implements a
    curl multi notify API, and adds initial free-threaded CPython support.  This
    release also fixes numerous minor issues and makes tests more reliable.

--- www/py-curl/patches/patch-setup.py
+++ www/py-curl/patches/patch-setup.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-setup.py,v 1.5 2022/12/17 06:22:02 adam Exp $
+$NetBSD: patch-setup.py,v 1.6 2026/06/30 20:27:08 adam Exp $
 
 Add multi Python variant support.
 
---- setup.py.orig	2022-12-16 19:31:55.000000000 +0000
+--- setup.py.orig	2026-06-29 18:09:02.000000000 +0000
 +++ setup.py
-@@ -662,10 +662,11 @@ def get_extension(argv, split_extension_
+@@ -673,10 +673,11 @@ def get_extension(argv, split_extension_
  def get_data_files():
      # a list of tuples with (path to install to, a list of local files)
      data_files = []
 +    version = '{}.{}'.format(sys.version_info.major, sys.version_info.minor)
      if sys.platform == "win32":
--        datadir = os.path.join("doc", PACKAGE)
-+        datadir = os.path.join("doc", PACKAGE + version)
+-        datadir = os.path.join("doc", PROJECT_NAME)
++        datadir = os.path.join("doc", PROJECT_NAME + version)
      else:
--        datadir = os.path.join("share", "doc", PACKAGE)
-+        datadir = os.path.join("share", "doc", PACKAGE  + version)
+-        datadir = os.path.join("share", "doc", PROJECT_NAME)
++        datadir = os.path.join("share", "doc", PROJECT_NAME + version)
      #
      files = ["AUTHORS", "ChangeLog", "COPYING-LGPL", "COPYING-MIT",
          "INSTALL.rst", "README.rst", "RELEASE-NOTES.rst"]
