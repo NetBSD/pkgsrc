72772afb28da6d47445579c3dfee70c0313585db
adam <adam@pkgsrc.org>
2026-06-30 09:57:45 +0000

    py-scipy: updated to 1.18.0
    SciPy 1.18.0 is the culmination of 6 months of hard work. It contains
    many new features, numerous bug-fixes, improved test coverage and better
    documentation. There have been a number of deprecations and API changes
    in this release, which are documented below. All users are encouraged to
    upgrade to this release, as there are a large number of bug-fixes and
    optimizations. Before upgrading, we recommend that users check that
    their own code does not use deprecated SciPy functionality (to do so,
    run your code with ``python -Wd`` and check for ``DeprecationWarning`` s).
    Our development attention will now shift to bug-fix releases on the
    1.18.x branch, and on adding new features on the main branch.

--- math/py-scipy/patches/patch-scipy_spatial___ckdtree.pyx
+++ math/py-scipy/patches/patch-scipy_spatial___ckdtree.pyx
@@ -1,4 +1,4 @@
-$NetBSD: patch-scipy_spatial___ckdtree.pyx,v 1.4 2023/07/17 19:39:56 tnn Exp $
+$NetBSD: patch-scipy_spatial___ckdtree.pyx,v 1.5 2026/06/30 09:57:45 adam Exp $
 
 Fix missing isnan/isinf.
 Build breaks on at least NetBSD-10.99.4-aarch64 without this:
@@ -6,10 +6,10 @@ Build breaks on at least NetBSD-10.99.4-aarch64 without this:
 scipy/spatial/_ckdtree.cxx: In function 'PyObject* __pyx_pf_5scipy_7spatial_8_ckdtree_7cKDTree_16count_neighbors(__pyx_obj_5scipy_7spatial_8_ckdtree_cKDTree*, __pyx_obj_5scipy_7spatial_8_ckdtree_cKDTree*, PyObject*, __pyx_t_5numpy_float64_t, PyObject*, int)':
 scipy/spatial/_ckdtree.cxx:13392:19: error: 'isinf' was not declared in this scope; did you mean 'std::isinf'?
 
---- scipy/spatial/_ckdtree.pyx.orig	1970-01-01 00:00:00.000000000 +0000
+--- scipy/spatial/_ckdtree.pyx.orig	2026-06-19 03:00:59.000000000 +0000
 +++ scipy/spatial/_ckdtree.pyx
-@@ -16,7 +16,9 @@ cimport numpy as np
- from cpython.mem cimport PyMem_Malloc, PyMem_Free
+@@ -17,7 +17,9 @@ from cpython.mem cimport PyMem_Malloc, P
+ from libcpp.mutex cimport py_safe_call_once, py_safe_once_flag
  from libcpp.vector cimport vector
  from libcpp cimport bool
 -from libc.math cimport isinf, INFINITY
