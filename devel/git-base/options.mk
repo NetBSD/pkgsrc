08a9f3fe29a9915c1f702568832291832c29bd51
adam <adam@pkgsrc.org>
2026-06-30 20:19:42 +0000

    git-base: disable Rust; rewrite options

--- devel/git-base/options.mk
+++ devel/git-base/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.7 2025/10/05 18:09:33 js Exp $
+# $NetBSD: options.mk,v 1.8 2026/06/30 20:19:42 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.git
-PKG_SUPPORTED_OPTIONS=	apple-common-crypto openssl
-PKG_SUGGESTED_OPTIONS=	
+PKG_SUPPORTED_OPTIONS=	openssl
+.if ${OPSYS} == "Darwin"
+PKG_SUPPORTED_OPTIONS+=	apple-common-crypto
+.endif
 .if ${OPSYS} != "QNX"
 PKG_SUGGESTED_OPTIONS+=	openssl
 .endif
@@ -10,15 +12,13 @@ PKG_SUGGESTED_OPTIONS+=	openssl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mapple-common-crypto)
-.  if ${MACHINE_PLATFORM:MDarwin-[0-8].*-*}
-PKG_FAIL_REASON+=	"apple-common-crypto not available on this system"
-.  endif
 CONFIGURE_ARGS+=	--without-openssl
-.elif !empty(PKG_OPTIONS:Mopenssl)
-.include "../../security/openssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
-MAKE_FLAGS+=		NO_APPLE_COMMON_CRYPTO=1
 .else
-CONFIGURE_ARGS+=	--without-openssl
 MAKE_FLAGS+=		NO_APPLE_COMMON_CRYPTO=1
+.  if !empty(PKG_OPTIONS:Mopenssl)
+.    include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
+.  else
+CONFIGURE_ARGS+=	--without-openssl
+.  endif
 .endif
