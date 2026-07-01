5e81ced6fab0cf7d4c2b9381d3f7f3513535e255
wiz <wiz@pkgsrc.org>
2026-07-01 08:07:23 +0000

    fontconfig: switch to upstream version of NetBSD build fix
    Bump PKGREVISION.

--- fonts/fontconfig/patches/patch-src_fccompat.c
+++ fonts/fontconfig/patches/patch-src_fccompat.c
@@ -1,19 +1,20 @@
-$NetBSD: patch-src_fccompat.c,v 1.1 2026/06/22 19:26:14 wiz Exp $
+$NetBSD: patch-src_fccompat.c,v 1.2 2026/07/01 08:07:23 wiz Exp $
 
-NetBSD doesn't have uselocale.
+NetBSD does not provide uselocale().
 https://gitlab.freedesktop.org/fontconfig/fontconfig/-/work_items/543
 
---- src/fccompat.c.orig	2026-06-22 19:21:13.540675219 +0000
+--- src/fccompat.c.orig	2026-07-01 08:04:28.911964970 +0000
 +++ src/fccompat.c
-@@ -375,7 +375,11 @@ FcLocaleSetCurrent (FcLocale loc)
+@@ -375,7 +375,12 @@ FcLocaleSetCurrent (FcLocale loc)
  FcLocale
  FcLocaleSetCurrent (FcLocale loc)
  {
-+#ifdef __NetBSD__
++#  if HAVE_VASPRINTF_L || HAVE__VSNPRINTF_L
++    /* This function won't be used */
 +    return NULL;
-+#else
++#  else
      return uselocale (loc);
-+#endif
++#  endif
  }
  #endif
  
