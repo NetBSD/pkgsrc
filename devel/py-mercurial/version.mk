d2b74f9d544a97426e20dc7246f18794d8b672dd
wiz <wiz@pkgsrc.org>
2026-07-01 22:05:35 +0000

    py-mercurial: mention rhg (for updates) in version.mk

--- devel/py-mercurial/version.mk
+++ devel/py-mercurial/version.mk
@@ -1,3 +1,6 @@
-# $NetBSD: version.mk,v 1.69 2026/06/09 15:41:38 wiz Exp $
+# $NetBSD: version.mk,v 1.70 2026/07/01 22:05:35 wiz Exp $
+#
+# please also update devel/rhg when changing the version number
+#
 
 VERSION=	7.2.2
