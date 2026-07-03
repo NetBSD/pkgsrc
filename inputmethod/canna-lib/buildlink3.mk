7d517f3ee66549c2b4dff490b45dd9c082cefaf0
tsutsui <tsutsui@pkgsrc.org>
2026-07-03 20:00:01 +0000

    canna-lib: also bump API_DEPENDS to 3.8
    It looks there is no explicit API changes between 3.7pl3 and 3.8,
    but it could be problematic to mix >10 years old versions.

--- inputmethod/canna-lib/buildlink3.mk
+++ inputmethod/canna-lib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/01/07 13:04:17 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2026/07/03 20:00:01 tsutsui Exp $
 
 BUILDLINK_TREE+=	Canna-lib
 
 .if !defined(CANNA_LIB_BUILDLINK3_MK)
 CANNA_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Canna-lib+=	Canna-lib>=3.7pl3
+BUILDLINK_API_DEPENDS.Canna-lib+=	Canna-lib>=3.8
 BUILDLINK_PKGSRCDIR.Canna-lib?=		../../inputmethod/canna-lib
 .endif # CANNA_LIB_BUILDLINK3_MK
 
