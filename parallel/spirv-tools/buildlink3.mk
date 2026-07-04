cfa7b0b5b641feb88493799ee433192c8df47c16
wiz <wiz@pkgsrc.org>
2026-07-04 06:08:37 +0000

    spirv-tools: don't accept old binary packages
    before the package was "downgraded" from 2025.x to 1.x

--- parallel/spirv-tools/buildlink3.mk
+++ parallel/spirv-tools/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2026/04/13 13:22:21 kikadf Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/07/04 06:08:37 wiz Exp $
 
 BUILDLINK_TREE+=	spirv-tools
 
 .if !defined(SPIRV_TOOLS_BUILDLINK3_MK)
 SPIRV_TOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spirv-tools+=	spirv-tools>=1.4.341.0
+BUILDLINK_API_DEPENDS.spirv-tools+=	spirv-tools>=1.4.341.0<2000
 BUILDLINK_PKGSRCDIR.spirv-tools?=	../../parallel/spirv-tools
 BUILDLINK_FILES.spirv-tools+=					\
 					bin/spirv-as		\
