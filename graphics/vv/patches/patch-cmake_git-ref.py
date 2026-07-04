3fbf9d1c8698238c5a331605268c3abd49ca1f7b
wiz <wiz@pkgsrc.org>
2026-07-04 21:44:30 +0000

    vv: update to 3.2.20260704.
    Fixes build with latest libjxl.
    
    No upstream summary of changes found; repository
    moved and new tool is now installed.

---
+++ graphics/vv/patches/patch-cmake_git-ref.py
@@ -0,0 +1,33 @@
+$NetBSD: patch-cmake_git-ref.py,v 1.1 2026/07/04 21:44:31 wiz Exp $
+
+Pass in commit id.
+https://github.com/wolfpld/moderncore/issues/5
+
+--- cmake/git-ref.py.orig	2026-07-04 20:57:10.000000000 +0000
++++ cmake/git-ref.py
+@@ -3,14 +3,21 @@ import os
+ import filecmp
+ import subprocess
+ import os
++import sys
+ 
+ out = "GitRef.hpp"
+ tmp = f"{out}.tmp"
+ 
+-try:
+-    ref = subprocess.run(["git", "rev-parse", "--short", "HEAD"], check=True, capture_output=True).stdout.decode().strip()
+-except subprocess.CalledProcessError:
+-    ref = "unknown"
++filename = sys.argv[1] if len(sys.argv) > 1 else None
++if filename and os.path.exists(filename):
++    with open(filename, "r") as fh:
++        for line in fh:
++            ref = line.rstrip()
++else:
++    try:
++        ref = subprocess.run(["git", "rev-parse", "--short", "HEAD"], check=True, capture_output=True).stdout.decode().strip()
++    except subprocess.CalledProcessError:
++        ref = "unknown"
+ 
+ if not os.path.exists(out):
+     with open(out, "w") as f:
