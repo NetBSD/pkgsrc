c7275a2c619710c9f8abd8339e80fb87fd629ae0
bsiegert <bsiegert@pkgsrc.org>
2026-07-06 18:26:41 +0000

    foot: update to 1.27.0
    1.27.0
    
    * url.style=none|single|double|curly|dotted|dashed option added,
      allowing you to configure how URL underlines are drawn. The default
      is dotted.
    * URL underlines are now dotted by default, instead of plain
      underlines. This can be changed with the new url.style option.
    * If the cursor foreground and background colors are identical, use
      the current cell's foreground and background colors (inverted),
      instead of the default foreground and background colors.
    
    Fixed:
    * Other output (key presses, query replies etc) being mixed with paste
      data, both interactive pastes and OSC-52.
    * Scrollback search not working correctly when the terminal
      application has enabled the kitty keyboard protocol with release
      event reporting.
    * Keypad escapes in the legacy keyboard protocol ignoring the shift
      modifier.
    * Crash when querying the kitty notification protocol (OSC-99, with
      p=?).

--- wayland/foot/patches/patch-char32.c
+++ wayland/foot/patches/patch-char32.c
@@ -1,16 +1,15 @@
-$NetBSD: patch-char32.c,v 1.1 2026/04/14 12:11:48 kikadf Exp $
+$NetBSD: patch-char32.c,v 1.2 2026/07/06 18:26:41 bsiegert Exp $
 
 * Add NetBSD support
 
---- char32.c.orig	2026-02-05 16:00:31.082232514 +0000
+--- char32.c.orig	2026-05-15 06:26:04.000000000 +0000
 +++ char32.c
-@@ -34,7 +34,8 @@ _Static_assert(
+@@ -34,7 +34,7 @@ _Static_assert(
  #if !defined(__STDC_UTF_32__) || !__STDC_UTF_32__
   #error "char32_t does not use UTF-32"
  #endif
--#if (!defined(__STDC_ISO_10646__) || !__STDC_ISO_10646__) && !defined(__FreeBSD__)
-+#if (!defined(__STDC_ISO_10646__) || !__STDC_ISO_10646__) && !defined(__FreeBSD__) && \
-+    !defined(__NetBSD__)
+-#if (!defined(__STDC_ISO_10646__) || !__STDC_ISO_10646__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
++#if (!defined(__STDC_ISO_10646__) || !__STDC_ISO_10646__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
   #error "wchar_t does not use UTF-32"
  #endif
  
