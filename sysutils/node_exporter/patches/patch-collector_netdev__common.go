d2c92299d4e8f04b29752b960849d6001ac81635
wiz <wiz@pkgsrc.org>
2026-07-06 09:46:07 +0000

    node_exporter: update to 1.11.1.
    ## 1.11.1 / 2026-04-07
    
    * [BUGFIX] Fix kernel_hung for no data #3613
    
    ## 1.11.0 / 2026-04-04
    
    * [CHANGE] Adds distroless container image. #3592
    * [FEATURE] infiniband: Add `hw_counters` metrics #2827
    * [FEATURE] kernel_hung: Add collector for kernel hung tasks #3470
    * [FEATURE] mixin: Add "All" option to cluster variable in dashboards #3509
    * [FEATURE] netlink: Add node_network_altnames_info metric #3541
    * [FEATURE] thermal: Add support for CPU temperature metrics on Apple Silicon #3547
    * [FEATURE] bcachefs: Add new collector for bcachefs #3523
    * [ENHANCEMENT] mixin: Make devices in network graphs configurable #3525
    * [ENHANCEMENT] nvme: Add namespace parameters #3360
    * [BUGFIX] netdev: Respect deviceFilter for `node_network_address_info` metric #3340
    * [BUGFIX] mountstats: Fix multiple transports statistics labeling #3358
    * [BUGFIX] diskstats: Fix panic when using device-include flag only #3603

--- sysutils/node_exporter/patches/patch-collector_netdev__common.go
+++ sysutils/node_exporter/patches/patch-collector_netdev__common.go
@@ -1,18 +1,15 @@
-$NetBSD: patch-collector_netdev__common.go,v 1.2 2025/04/01 20:30:09 bsiegert Exp $
+$NetBSD: patch-collector_netdev__common.go,v 1.3 2026/07/06 09:46:07 wiz Exp $
 
 https://github.com/prometheus/node_exporter/pull/3078
 
---- collector/netdev_common.go.orig	2025-04-01 15:14:14.000000000 +0000
+--- collector/netdev_common.go.orig	2026-04-07 15:44:04.000000000 +0000
 +++ collector/netdev_common.go
-@@ -11,9 +11,9 @@
+@@ -11,7 +11,7 @@
  // See the License for the specific language governing permissions and
  // limitations under the License.
  
 -//go:build !nonetdev && (linux || freebsd || openbsd || dragonfly || darwin || aix)
 +//go:build !nonetdev && (linux || freebsd || netbsd || openbsd || dragonfly || darwin || aix)
- // +build !nonetdev
--// +build linux freebsd openbsd dragonfly darwin aix
-+// +build linux freebsd netbsd openbsd dragonfly darwin aix
  
  package collector
  
