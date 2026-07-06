71b6ab937e6414761f0220913041d75cf0a64935
he <he@pkgsrc.org>
2026-07-06 21:46:26 +0000

    multimedia/libaom: restore buildability on NetBSD/macppc.
    Re-apply fix for "So far no dynamic CPU feature detection on NetBSD".

--- multimedia/libaom/patches/patch-aom__ports_ppc__cpudetect.c
+++ multimedia/libaom/patches/patch-aom__ports_ppc__cpudetect.c
@@ -1,14 +1,16 @@
-$NetBSD: patch-aom__ports_ppc__cpudetect.c,v 1.4 2026/05/14 17:34:02 ryoon Exp $
+$NetBSD: patch-aom__ports_ppc__cpudetect.c,v 1.5 2026/07/06 21:46:26 he Exp $
 
 Make this at least build on NetBSD.
 So far no dynamic CPU feature detection on NetBSD.
 
 --- aom_ports/ppc_cpudetect.c.orig	2026-04-01 20:41:46.000000000 +0000
 +++ aom_ports/ppc_cpudetect.c
-@@ -18,8 +18,10 @@
+@@ -17,9 +17,11 @@
+ 
  #include "aom_ports/ppc.h"
  
- #if CONFIG_RUNTIME_CPU_DETECT
+-#if CONFIG_RUNTIME_CPU_DETECT
++#if CONFIG_RUNTIME_CPU_DETECT && !defined(__NetBSD__)
 +#ifdef __linux
  #include <asm/cputable.h>
  #include <linux/auxvec.h>
