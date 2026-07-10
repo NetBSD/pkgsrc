3e23c48c17846038b410188d25904c8fb70e35f1
adam <adam@pkgsrc.org>
2026-07-10 09:42:35 +0000

    py-cffi: updated to 2.1.0
    2.1.0
    
    Added the cffi-gen-src command-line tool (also invocable as python -m cffi.gen_src), which generates CFFI C extension source code, so that a build backend such as meson-python can build the extension. See Generating C Sources for CFFI Extensions for details. Integrated from the cffi-buildtool project by Rose Davidson.
    Added support for arm64 iOS wheels.
    Dropped support for Python 3.9.
    Added support for Python 3.15 and support for C extensions generated by CFFI to target the new abi3t free-threaded ABI.
    Avoid crashes inside of __delitem__.
    Avoid “string too big” error under MSVC.
    Fix mingw builds.

--- devel/py-cffi/patches/patch-setup.py
+++ devel/py-cffi/patches/patch-setup.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-setup.py,v 1.1 2024/03/19 10:49:46 nia Exp $
+$NetBSD: patch-setup.py,v 1.2 2026/07/10 09:42:35 adam Exp $
 
 Nope, we want the one from pkgsrc. Honour pkg-config.
 
 Also, -iwithsysroot is not supported by all compilers.
 
---- setup.py.orig	2024-03-19 10:46:00.000000000 +0000
+--- setup.py.orig	2026-07-06 20:52:02.000000000 +0000
 +++ setup.py
-@@ -148,14 +148,6 @@ else:
+@@ -154,14 +154,6 @@ else:
      ask_supports_thread()
      ask_supports_sync_synchronize()
  
--if 'darwin' in sys.platform:
+-if sysconfig.get_platform().startswith('macosx'):
 -    # priority is given to `pkg_config`, but always fall back on SDK's libffi.
 -    extra_compile_args += ['-iwithsysroot/usr/include/ffi']
 -
