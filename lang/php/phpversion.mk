cfb45bd2fa774cd31ca2ec95bd170bac15ba86af
bsiegert <bsiegert@pkgsrc.org>
2026-07-13 18:24:06 +0000

    Pullup ticket #7166 - requested by taca lang/php85: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.506
    - lang/php85/distinfo                                           1.8
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jul  4 06:24:46 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php85: distinfo
    
       Log Message:
       lang/php85: update to 8.4.23
    
       PHP 8.5.8 (2026-07-02)
    
       - Core:
         . Fixed bug GH-22280 (Incorrect compile error for goto to label preceding
           try/finally block). (Pratik Bhujel)
         . Fixed bug GH-22112 (Assertion when error handler throws during NaN to
           bool/string coercion). (iliaal)
    
       - BCMath:
         . Fixed issues with oversized allocations and signed overflow in bcround()
           and BcMath\Number::round(). (edorian)
    
       - Date:
         . Fix incorrect recurrence check of DatePeriod::createFromISO8601String().
           (ndossche)
    
       - Exif:
         . Read correct value for single and double tags. (ndossche)
    
       - GD:
         . Fixed bug GH-22121 (Double free in gdImageSetStyle() after
           overflow-triggered early return). (iliaal)
    
       - Intl:
         . Fix incorrect argument positions for invalid start/end arguments in
           transliterator_transliterate().
           (Weilin Du)
         . Fixed IntlTimeZone::getDisplayName() to synchronize object error state
           for invalid display types. (Weilin Du)
    
       - Lexbor:
         . Merge patch c3a6847. (ilutov, timwolla)
    
       - Opcache:
         . Fixed bug GH-22265 (Another tailcall vm_interrupt bug). (Levi Morrison)
         . Fixed bug GH-20469 (Unsafe inheritance cache replay with reentrant
           autoloading). (Levi Morrison)
         . Fixed bug GH-21972 (Corrupted variable type when a typed by-value return
           contains a reference wrapper). (Weilin Du)
    
       - Phar:
         . Fixed a bypass of the magic ".phar" directory protection in
           Phar::addEmptyDir() for paths starting with "/.phar", while allowing
           non-magic directory names that merely share the ".phar" prefix. (Weilin Du)
    
       - Reflection:
         . Preserve class-name case in ReflectionClass::getProperty() error messages
           and autoloading. (jorgsowa)
    
       - SOAP:
         . Fixed bug GH-22218 (SoapServer::handle() crash on $_SERVER not being
           an array). (David Carlier / Rex-Reynolds)
         . Fixed bug GH-22285 (Soap server requires the raw input to be passed
           to $server->handle). (David Carlier / ndossche)
    
       - Sqlite:
         . Fix error checks for column retrieval. (ndossche)
    
       - URI:
         . Add LEXBOR_STATIC to CFLAGS_URI on Windows so ext/uri does not see
           LXB_API as __declspec(dllimport) when linked statically into PHP.
           (Luther Monson)
         . Clean error logs before each Uri\WhatWg\Url wither call so that errors from
           previous wither calls are not returned the next time a UrlValidationError
           is thrown. (kocsismate)
    
       - Zip:
         . Fixed error-related memory leaks. (ndossche)
    
       - Zlib:
         . Fixed memory leak if deflate initialization fails and there is a dict.
           (ndossche)
         . Fixed memory leak in inflate_add(). (ndossche)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.502.2.3 2026/07/13 17:30:47 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.502.2.4 2026/07/13 18:24:06 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -113,7 +113,7 @@ PHP74_VERSION=	7.4.33
 PHP82_VERSION=	8.2.32
 PHP83_VERSION=	8.3.32
 PHP84_VERSION=	8.4.23
-PHP85_VERSION=	8.5.7
+PHP85_VERSION=	8.5.8
 
 _VARGROUPS+=	php
 _USER_VARS.php=	PHP_VERSION_DEFAULT
