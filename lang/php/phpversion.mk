c072ad7f699466d3ed65a006b5f9d5ff30338024
bsiegert <bsiegert@pkgsrc.org>
2026-07-13 16:46:08 +0000

    Pullup ticket #7163 - requested by taca lang/php82: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.503
    - lang/php82/distinfo                                           1.37
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jul  4 06:19:04 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php82: distinfo
    
       Log Message:
       lang/php82: update to 8.2.32
    
       PHP 8.2.32 (2026-07-02)
    
       - OpenSSL:
         . Fixed bug GH-22187 (Memory corruption (zend_mm_heap corrupted) in
           openssl_encrypt with AES-WRAP-PAD). (David Carlier)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.502 2026/06/06 02:43:55 taca Exp $
+# $NetBSD: phpversion.mk,v 1.502.2.1 2026/07/13 16:46:08 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -110,7 +110,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
-PHP82_VERSION=	8.2.31
+PHP82_VERSION=	8.2.32
 PHP83_VERSION=	8.3.31
 PHP84_VERSION=	8.4.22
 PHP85_VERSION=	8.5.7
