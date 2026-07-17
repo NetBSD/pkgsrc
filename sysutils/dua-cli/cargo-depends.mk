10347694e47a5192490fdc82552de5835f471ce4
pin <pin@pkgsrc.org>
2026-07-17 05:25:07 +0000

    sysutils/dua-cli: update to 2.38.0
    New Features
    
        notify after interactive work when unfocused
        Interactive scans, refreshes, deletion, and trash operations can take long
        enough that users leave the terminal, but dua previously had no way to signal
        completion without exiting the TUI.
    
        Track terminal focus events and emit sanitized OSC 777 notifications only when
        the terminal is unfocused. Include concise entry, byte, duration, and error
        statistics, and add a notifications config section whose scan_finished and
        delete_finished switches default to true.

--- sysutils/dua-cli/cargo-depends.mk
+++ sysutils/dua-cli/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.37 2026/06/22 09:07:45 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.38 2026/07/17 05:25:07 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.25.1
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
