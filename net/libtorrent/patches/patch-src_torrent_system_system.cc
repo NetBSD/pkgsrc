82ca73b673bd81fb52eecda2f602876f9bb6cab2
gdt <gdt@pkgsrc.org>
2026-07-18 18:39:57 +0000

    net/libtorrent: Add upstream URL for sys/socket.h bug

--- net/libtorrent/patches/patch-src_torrent_system_system.cc
+++ net/libtorrent/patches/patch-src_torrent_system_system.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_torrent_system_system.cc,v 1.1 2026/07/18 18:28:15 gdt Exp $
+$NetBSD: patch-src_torrent_system_system.cc,v 1.2 2026/07/18 18:39:57 gdt Exp $
 
 system.cc uses AF_INET.  POSIX says that constants are defined in sys/socket:
 
   https://pubs.opengroup.org/onlinepubs/9799919799/functions/socket.html
 
-Not yet reported upstream.
+https://github.com/rakshasa/rtorrent/issues/1892
 
 --- src/torrent/system/system.cc.orig	2026-07-18 18:18:35.886055891 +0000
 +++ src/torrent/system/system.cc
