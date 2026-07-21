258e3de21f95d7bab9252f5fce2ada2f82f523d9
wiz <wiz@pkgsrc.org>
2026-07-21 15:47:06 +0000

    geeqie: pass linker flags the meson way
    Fixes relro build.
    
    Mark this as using c++17 while here.

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.11 2024/01/26 14:13:01 wiz Exp $
+# $NetBSD: options.mk,v 1.12 2026/07/21 15:47:06 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.geeqie
-PKG_SUPPORTED_OPTIONS=		gspell libarchive libchamplain libraw libwebp pdf
+PKG_SUPPORTED_OPTIONS=		gspell libarchive libchamplain libraw libwebp pdf # gtk4
 PKG_SUGGESTED_OPTIONS=		# none
 
 .include "../../mk/bsd.options.mk"
@@ -9,6 +9,14 @@ PKG_SUGGESTED_OPTIONS=		# none
 .if !empty(PKG_OPTIONS:Mgspell)
 .include "../../textproc/gspell/buildlink3.mk"
 .endif
+
+#.if !empty(PKG_OPTIONS:Mgtk4)
+## doesn't build as of 2.9
+#MESON_ARGS+=	-Dgtk4=enabled
+#.include "../../x11/gtk4/buildlink3.mk"
+#.else
+#.include "../../x11/gtk3/buildlink3.mk"
+#.endif
 
 .if !empty(PKG_OPTIONS:Mlibarchive)
 .include "../../archivers/libarchive/buildlink3.mk"
