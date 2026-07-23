e6a1b57ac00dc9e13aa299e098f53a04188340be
prlw1 <prlw1@pkgsrc.org>
2026-07-23 12:28:40 +0000

    Update threadingbuildingblocks (oneTBB) to 2023.1.0
    - XXX cargo culted flag removal
    - colloidal suspension simulation runs a tad faster
    
    
    Highlights:
    
    - Significantly improved scalability for concurrent ordered containers
      on systems with many threads. This change results in greater than
      3x performance for some use cases.
    
    - Fixed ``concurrent_queue`` and ``concurrent_bounded_queue``
      capacity preserving on copying, moving, and swapping
      (https://github.com/uxlfoundation/oneTBB/issues/1598).
    
    - Fixed issues with Flow Graph priorities when using limited
      concurrency nodes (https://github.com/uxlfoundation/oneTBB/issues/1595).
    
    - Added support for Python 3.14.
    
    - ``blocked_nd_range`` is now a fully supported feature.
    
    - Fixed a CMake configuration error on systems with non-English
      locales. Contributed by moritz-h
      (https://github.com/uxlfoundation/oneTBB/pull/1606).
    
    - Improved performance scalability of spin_mutex::lock,
      spin_mutex::try_lock and queuing_mutex::scoped_lock::try_acquire.
    
    - Fixed potential oversubscription issue by respecting CPU quota
      and period limitations provided via cgroups settings on Linux*
      (https://github.com/uxlfoundation/oneTBB/issues/190, https://github.c
      om/uxlfoundation/oneTBB/issues/1760).

--- parallel/threadingbuildingblocks/patches/patch-cmake_compilers_Clang.cmake
+++ parallel/threadingbuildingblocks/patches/patch-cmake_compilers_Clang.cmake
@@ -1,17 +1,17 @@
-$NetBSD: patch-cmake_compilers_Clang.cmake,v 1.4 2025/06/20 14:20:32 nia Exp $
+$NetBSD: patch-cmake_compilers_Clang.cmake,v 1.5 2026/07/23 12:28:40 prlw1 Exp $
 
 Remove security hardening flags so that the mk/ infrastructure can pass
 them depending on whether they are supported by the target platform.
 
---- cmake/compilers/Clang.cmake.orig	2025-06-20 14:04:56.076853522 +0000
+--- cmake/compilers/Clang.cmake.orig	2026-07-19 16:53:28.896273751 +0000
 +++ cmake/compilers/Clang.cmake
-@@ -65,11 +65,6 @@ endif()
- # Clang flags to prevent compiler from optimizing out security checks
- set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-security -Werror=format-security -fPIC $<$<NOT:$<BOOL:${EMSCRIPTEN}>>:-fstack-protector-strong>)
+@@ -97,11 +97,6 @@ endif()
+     endif()
+ endif()
  
--# -z switch is not supported on MacOS
--if (NOT APPLE)
--    set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now)
+-# -z switch is not supported on MacOS and Windows
+-if (NOT APPLE AND NOT WIN32)
+-    set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now,-z,noexecstack)
 -endif()
 -
  set(TBB_COMMON_LINK_LIBS ${CMAKE_DL_LIBS})
--- parallel/threadingbuildingblocks/patches/patch-cmake_compilers_GNU.cmake
+++ parallel/threadingbuildingblocks/patches/patch-cmake_compilers_GNU.cmake
@@ -1,16 +1,21 @@
-$NetBSD: patch-cmake_compilers_GNU.cmake,v 1.6 2025/06/20 14:20:32 nia Exp $
+$NetBSD: patch-cmake_compilers_GNU.cmake,v 1.7 2026/07/23 12:28:40 prlw1 Exp $
 
 Remove security hardening flags so that the mk/ infrastructure can pass
 them depending on whether they are supported by the target platform.
 
---- cmake/compilers/GNU.cmake.orig	2025-06-20 14:05:05.891314120 +0000
+--- cmake/compilers/GNU.cmake.orig	2026-07-19 16:53:28.901108647 +0000
 +++ cmake/compilers/GNU.cmake
-@@ -103,15 +103,6 @@ endif ()
+@@ -106,20 +106,6 @@ set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAG
  
  # Gnu flags to prevent compiler from optimizing out security checks
  set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -fno-strict-overflow -fno-delete-null-pointer-checks -fwrapv)
 -set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-security -Werror=format-security
 -    -fstack-protector-strong )
+-if (CMAKE_SYSTEM_PROCESSOR MATCHES "(AMD64|amd64|i.86|x86)" AND NOT EMSCRIPTEN)
+-    set(TBB_LIB_COMPILE_FLAGS ${TBB_LIB_COMPILE_FLAGS} $<$<NOT:$<VERSION_LESS:${CMAKE_CXX_COMPILER_VERSION},8.0>>:-fcf-protection=full>)
+-endif ()
+-set(TBB_LIB_COMPILE_FLAGS ${TBB_LIB_COMPILE_FLAGS} $<$<NOT:$<VERSION_LESS:${CMAKE_CXX_COMPILER_VERSION},8.0>>:-fstack-clash-protection>)
+-
 -# -z switch is not supported on MacOS and MinGW
 -if (NOT APPLE AND NOT MINGW)
 -    set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now,-z,noexecstack)
@@ -19,5 +24,5 @@ them depending on whether they are supported by the target platform.
 -  set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} $<$<NOT:$<CONFIG:Debug>>:-D_FORTIFY_SOURCE=2> )
 -endif ()
  
- # TBB malloc settings
- set(TBBMALLOC_LIB_COMPILE_FLAGS -fno-rtti -fno-exceptions)
+ if (TBB_FILE_TRIM AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8 AND NOT CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+     set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -ffile-prefix-map=${NATIVE_TBB_PROJECT_ROOT_DIR}/= -ffile-prefix-map=${NATIVE_TBB_RELATIVE_BIN_PATH}/=)
