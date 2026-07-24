30f9b829f67086c9b8a929baa1709485d86188e2
wiz <wiz@pkgsrc.org>
2026-07-24 18:36:40 +0000

    cadical: bl3.mk: do not accept cadical 3

--- math/cadical/buildlink3.mk
+++ math/cadical/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2026/07/02 19:15:03 alnsn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/07/24 18:36:40 wiz Exp $
 
 BUILDLINK_TREE+=	cadical
 
 .if !defined(CADICAL_BUILDLINK3_MK)
 CADICAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cadical+=	cadical>=2.1.3
+BUILDLINK_API_DEPENDS.cadical+=	cadical>=2.1.3<3
 BUILDLINK_ABI_DEPENDS.cadical+=	cadical>=2.1.3
 BUILDLINK_PKGSRCDIR.cadical?=	../../math/cadical
 .endif # CADICAL_BUILDLINK3_MK
