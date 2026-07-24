2541c7818334493a646ddaa18e7eea23cb120f45
adam <adam@pkgsrc.org>
2026-07-24 09:03:52 +0000

    defguard-gateway: updated to 2.0.3
    2.0.3
    Fix memory access problem on BSD.

--- security/defguard/version.mk
+++ security/defguard/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.6 2026/07/13 07:29:18 adam Exp $
+# $NetBSD: version.mk,v 1.7 2026/07/24 09:03:52 adam Exp $
 
 DEFGUARD_VERSION=	2.0.2
 
@@ -9,9 +9,6 @@ GITHUB_TAG=	v${PKGVERSION_NOREV}
 MAINTAINER=	adam@NetBSD.org
 HOMEPAGE=	https://defguard.net/
 LICENSE=	gnu-agpl-v3 # AND defguard-enterprise-license
-
-# All components need protos.
-EXTRA_DIST+=	defguard-${DEFGUARD_VERSION}-proto${EXTRACT_SUFX}
 
 DISTFILES=	${DEFAULT_DISTFILES} ${EXTRA_DIST}
 
@@ -24,6 +21,3 @@ MAKE_ENV+=	VERGEN_IDEMPOTENT=1
 MAKE_ENV+=	VERGEN_GIT_SHA=pkgsrc
 RUSTFLAGS+=	-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.libgit2}/lib
 RUSTFLAGS+=	-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.openssl}/lib
-
-post-extract:
-	${RMDIR} ${WRKSRC}/proto && ${LN} -f -s ${WRKDIR}/proto ${WRKSRC}/proto
