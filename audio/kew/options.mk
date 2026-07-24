d2ba9828d103c69bc2669beda78b3223bc134a6e
ktnb <ktnb@pkgsrc.org>
2026-07-24 15:13:03 +0000

    kew: update to 4.2.7
    Packaging updates:
    - added support for artists.db
      - click on the artist name and it'll open that artist's website in
        whatever browser xdg-open(1) opens.
    
    Upstream updates:
    
    Enhancements:
    - Added mini-controls. The idea here is that you will be able to do
      all the basic operations and will be able to sit a whole session
      of listening to music, without ever using a keyboard.
    - Added a new visualizer mode with a two color gradient.
    - The visualizer and footer are now under the cover in track view and
      don't move around depending on metadata position.
    - Removed mp4 support, since it's not fully supported. Fragmented
      mp4s that are used in streaming are not supported.
    
    Bug Fixes:
    - Fix freeze if the first played song was in an unsupported format
    - Fix set path leading to errors if the music library path's last
      character is a '/'
    - Re-enable desktop notifications.
    - Fixed an issue on Nix and with the flake.nix
    - Don't search for covers in sub-folders if the song is in the root
      library path.
    - Fixed bug where the root library path was used as artist and album
      fallback if it didn't contain a '/' at the end.
    - Fixed crash on loading Enhanced AC3 files and added proper handling
      of unsupported files in general.
    - Implemented mpris/dbus get_loop_status

--- audio/kew/options.mk
+++ audio/kew/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2026/02/12 12:55:00 ktnb Exp $
+# $NetBSD: options.mk,v 1.4 2026/07/24 15:13:03 ktnb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kew
-PKG_SUPPORTED_OPTIONS=	dbus
+PKG_SUPPORTED_OPTIONS=	dbus artist-db
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
@@ -14,4 +14,27 @@ MAKE_FLAGS+=	USE_DBUS=1
 .include "../../sysutils/dbus/buildlink3.mk"
 .else
 MAKE_FLAGS+=	USE_DBUS=0
+.endif
+
+###
+###  Include artist database for clickable artist names
+###
+PLIST_VARS+=	artists
+.if !empty(PKG_OPTIONS:Martist-db)
+VERSION_HASH=	8f0c30b1b4ee593117b41bbc6074a895d6a61639
+
+ARTIST_DB=	artists-${VERSION_HASH:C%^(.......).*%\1%}.db
+DISTFILES=	${DEFAULT_DISTFILES}
+DISTFILES+=	${ARTIST_DB}
+
+PLIST.artists=		yes
+
+SITES.${ARTIST_DB}= \
+	-https://github.com/ravachol/kew-data/raw/${VERSION_HASH}/artists.db
+
+DEPENDS+=	xdg-utils-[0-9]*:../../misc/xdg-utils
+
+post-install:
+	${INSTALL_DATA} ${WRKDIR}/${ARTIST_DB} ${DESTDIR}${PREFIX}/share/akew/artists.db
+
 .endif
