504b6632cbef1240fa60e61392eb1d0d6037d332
wiz <wiz@pkgsrc.org>
2026-07-25 09:40:53 +0000

    lean4: fix alloca warning (during package build, and 'lake build')
    The -std=gnu99 didn't help because alloca() was used in a C++ file.
    Replace calls to alloca with __builtin_alloca instead, and get rid of
    the forced gnu99.
    
    Bump PKGREVISION.

---
+++ math/lean4/patches/patch-src_include_lean_lean.h
@@ -0,0 +1,19 @@
+$NetBSD: patch-src_include_lean_lean.h,v 1.1 2026/07/25 09:40:53 wiz Exp $
+
+Call compiler-provided alloca(). Fixes
+Warning: reference to the libc supplied alloca(3); this most likely will not work. Please use the compiler provided version of alloca(3), by supplying the appropriate compiler flags (e.g. -std=gnu99).
+
+--- src/include/lean/lean.h.orig	2026-07-25 09:24:11.724804692 +0000
++++ src/include/lean/lean.h
+@@ -37,7 +37,11 @@ extern "C" {
+ #include <stdnoreturn.h>
+ #define LEAN_NORETURN _Noreturn
+ #else
++#if defined(__GNUC__) || defined(__clang__)
++#define LEAN_ALLOCA(s) __builtin_alloca(s)
++#else
+ #define LEAN_ALLOCA(s) alloca(s)
++#endif
+ #define LEAN_NORETURN __attribute__((noreturn))
+ #endif
+ 
