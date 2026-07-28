51134c0ab8fc20ee491e082c26d65ff31ec572d9
ryoon <ryoon@pkgsrc.org>
2026-07-28 03:10:59 +0000

    print/libcupsfilters: Buildlink dbus conditionally to fix build of cups-filters

--- print/libcupsfilters/buildlink3.mk
+++ print/libcupsfilters/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2026/07/27 19:02:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2026/07/28 03:10:59 ryoon Exp $
 
 BUILDLINK_TREE+=	libcupsfilters
 
@@ -8,6 +8,13 @@ LIBCUPSFILTERS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libcupsfilters+=	libcupsfilters>=2.1.1
 BUILDLINK_ABI_DEPENDS.libcupsfilters?=	libcupsfilters>=2.1.1nb7
 BUILDLINK_PKGSRCDIR.libcupsfilters?=	../../print/libcupsfilters
+
+pkgbase := libcupsfilters
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.libcupsfilters:Mdbus}
+.include "../../sysutils/dbus/buildlink3.mk"
+.endif
 
 .include "../../graphics/libexif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
