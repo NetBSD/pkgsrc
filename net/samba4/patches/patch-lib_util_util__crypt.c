813d53df74bb9860c98707baea6ad044446c0c60
taca <taca@pkgsrc.org>
2026-07-30 15:37:00 +0000

    net/samba4: update to 4.24.5
    pkgsrc change:
    
    * remove pkglint warnings of patches/patch-lib_util_util__crypt.c
    
    
                       ==============================
                       Release Notes for Samba 4.24.5
                               July 28, 2026
                       ==============================
    
    
    This is a security release in order to address the following defects:
    
    o CVE-2026-6949:   TSIG packet with name compression can crash DNS
    
                       Incorrect size calculations when a TSIG record contains
                       compressed names can lead to a large out-of-bounds write
                       causing the server to crash.
    
                       https://www.samba.org/samba/security/CVE-2026-6949.html
    
    
    o CVE-2026-58216:  An authenticated user could possibly crash a KDC process
    
                       A kpasswd packet that contains malformed ASN.1 might cause
                       the server to access 6 bytes of unallocated memory. This
                       memory is not exposed to the user, but in some
                       circumstances the server could crash.
    
                       https://www.samba.org/samba/security/CVE-2026-58216.html
    
    
    o CVE-2026-58218:  DNS signing DoS via TKEY name cache exhaustion
    
                       An unauthenticated user can repeatedly register names TKEY
                       names, which floods a cache causing legitimate TKEYs to be
                       expunged. This can practically block the use DNS TSIG
                       signing.
    
                       https://www.samba.org/samba/security/CVE-2026-58218.html
    
    
    o CVE-2026-58221:  Samba AD authenticated LDAP access domain takeover
    
                       Samba AD low-privilege authenticated LDAP access allows
                       modifications to internal LDB special DNs, which permits a
                       domain takeover.
    
                       https://www.samba.org/samba/security/CVE-2026-58221.html
    
    
    o CVE-2026-58222:  Samba AD LDAP Compare filter injection and trusted-request
                       confusion disclose protected attributes
    
                       An ordinary authenticated domain user can bypass access
                       checks and query confidential Active Directory attributes
                       (such as KDS root keys) via LDAP Compare requests. Due to a
                       filter injection flaw and trusted execution context, the
                       LDAP Compare operation can be turned into a
                       protected-attribute disclosure oracle.
    
                       https://www.samba.org/samba/security/CVE-2026-58222.html
    
    
    o CVE-2026-58224   The CTDB protocol has bounds checking issues
    
                       CTDB fails to do integrity checking of received packets.
                       This includes failure to check field lengths against packet
                       lengths when unmarshalling packets.
    
                       https://www.samba.org/samba/security/CVE-2026-58224.html
    
    
    Changes since 4.24.4
    --------------------
    
    o  Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
       * BUG 16087: CVE-2026-58216
    
    o  Volker Lendecke <vl@samba.org>
       * BUG 16115: CVE-2026-58218
    
    o  Stefan Metzmacher <metze@samba.org>
       * BUG 16083: CVE-2026-6949
       * BUG 16147: CVE-2026-58221
       * BUG 16148: CVE-2026-58222
    
    o  Martin Schwenke <mschwenke@ddn.com>
       * BUG 16085: CVE-2026-58224

--- net/samba4/patches/patch-lib_util_util__crypt.c
+++ net/samba4/patches/patch-lib_util_util__crypt.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-lib_util_util__crypt.c,v 1.3 2026/04/17 08:35:42 adam Exp $
+$NetBSD: patch-lib_util_util__crypt.c,v 1.4 2026/07/30 15:37:00 taca Exp $
 
 Account for platforms that don't define ENOTRECOVERABLE, like NetBSD
 before 10.0.
 
 --- lib/util/util_crypt.c.orig	2025-02-17 15:48:20.606155400 +0000
 +++ lib/util/util_crypt.c
-@@ -62,7 +64,11 @@ static int crypt_as_best_we_can(TALLOC_C
+@@ -64,7 +64,11 @@ static int crypt_as_best_we_can(TALLOC_C
  	if (hash == NULL || hash[0] == '*') {
  		if (ret == 0) {
  			/* this is annoying */
@@ -17,7 +17,7 @@ before 10.0.
  		}
  	}
  	if (ret != 0) {
-@@ -107,7 +113,11 @@ char *talloc_crypt_errstring(TALLOC_CTX 
+@@ -109,7 +113,11 @@ char *talloc_crypt_errstring(TALLOC_CTX 
  			mem_ctx,
  			"Password exceeds maximum length allowed for crypt() hashing");
  	}
