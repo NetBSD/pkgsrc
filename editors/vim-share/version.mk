1a461ea718de96a1efc3d9d5e1c30fbe3c010121
morr <morr@pkgsrc.org>
2026-07-31 13:26:11 +0000

    Update vim to version 9.2.0859
    Changes:
    - patch 9.2.0859: GTK2: Link error
    - runtime(python): highlight the 'lazy' soft keyword
    - patch 9.2.0858: MS-Windows GUI: white flash when VimEnter is slow
    - patch 9.2.0857: popup: opacity popup over a terminal is not cleared when closed
    - runtime(python): Fix indenting for brackets within python byte strings
    - runtime(sh): fix Bash/Ksh array highlighting after an escaped paren
    - patch 9.2.0856: GTK4: undercurl rendering is inefficient
    - CI: Bump the github-actions group across 1 directory with 3 updates
    - patch 9.2.0855: 'showcmd' not redrawn with empty mapping triggered on timeout
    - runtime(osc52): don't use osc52 provider if gui is running
    - runtime(doc): clarify expandcmd() and "~" behaviour
    - runtime(doc): Tweak documentation style a bit
    - runtime(doc): remove incorrect note about spellcapcheck and the first word
    - runtime(doc): document &t_8u as &t_8f and &t_8b

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.178 2026/07/25 18:15:50 morr Exp $
+# $NetBSD: version.mk,v 1.179 2026/07/31 13:26:11 morr Exp $
 
 VIM_VERSION=		9.2
-VIM_PATCHLEVEL=		0854
+VIM_PATCHLEVEL=		0859
 # Changelog: see https://github.com/vim/vim/commits/master/ or
 # with git clone: $ git log --pretty=format:"- %s" v<old_version>...HEAD
 VIM_SUBDIR=		vim92
