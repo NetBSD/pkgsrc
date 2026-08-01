1f35f8263241d2ab8c695bf893b4c0e00699e930
adam <adam@pkgsrc.org>
2026-08-01 16:26:44 +0000

    cmake[-gui]: updated to 4.4.2
    4.4.2
    This version made no changes to documented features or interfaces. Some
    implementation updates were made to support ecosystem changes and/or fix
    regressions.

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.89 2026/08/01 14:28:07 adam Exp $
+# $NetBSD: version.mk,v 1.90 2026/08/01 16:26:44 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.4.1
+CMAKE_VERSION=	4.4.2
 CMAKE_API=	${CMAKE_VERSION:R}
