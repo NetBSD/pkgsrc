9d33bbc0ca6729eb2a4ae5f8a9e6218bebb858c3
adam <adam@pkgsrc.org>
2026-08-01 14:28:07 +0000

    cmake cmake-gui: updated to 4.4.1
    CMake 4.4.1
    * gitlab-ci: make `needs` explicitly empty where possible
    * gitlab-ci: move macOS arm64 jobs to `tart` runners
    * gitlab-ci: extend Xcode CI job timeout to 3 hours
    * ci: do not limit testing based on load in a `tart` runner
    * Tests: Update RunCMake.Framework expected output for macOS 26.4
    * try_compile: Restore toleration of toolchain file setting internal variable
    * GNUInstallDirs: Document cross-compiling behavior w.r.t. lib directories
    * Style: Replace some UTF-8 characters with ASCII equivalents
    * CPackIFW: Add recent release versions to search list
    * FASTBuild: Prefer first compiler in PATH on Windows
    * CTest: Restore support for dashboard client build targets
    * Help: target_sources: Align descriptions with signatures
    * Presets: Forward presets-file to workflow steps
    * FindPython: fix SABIModule detection failure
    * FindPython: Add support for pydebug ABI flag on Windows
    * CMAKE_TEST_BUILD_DEPENDS: Use config when evaluating test dependency targets
    * Tests: Fix wrong check in CPS import test
    * Check*: Refactor variable handling
    * Check*: Restore more toleration of `;-W...` in CMAKE_REQUIRED_FLAGS
    * Diagnostics: Fix interaction with cached state
    * Tests: Fix symbolic component tests
    * Ninja: Restore cmcldeps command-line ordering
    * ctest: Exit nonzero when write to output JUnit or XML fails
    * GoogleTest: Fix gtest_discover_tests race on POST_BUILD of multiple targets
    * GoogleTest: Avoid generation error on duplicate target test discovery
    * VS: Avoid unsupported ScanSourceForModuleDependencies under ClangCl
    * c++modules: Fix transitive synthetic BMI discovery
    * c++modules: discover synth targets for all configs
    * c++modules: Account for preprocessor flags in compile options
    * CPS: Add package requirements for per-config link dependencies
    * CPS/c++modules: Use file system root as base directory for module manifests
    * ctest: Avoid unused-cli warning when propagating SITE/BUILDNAME
    * Help/CMAKE_LANG_COMPILER: Document Visual Studio, Xcode, GHS generators quirks

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.88 2026/07/16 11:14:00 adam Exp $
+# $NetBSD: version.mk,v 1.89 2026/08/01 14:28:07 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.4.0
+CMAKE_VERSION=	4.4.1
 CMAKE_API=	${CMAKE_VERSION:R}
