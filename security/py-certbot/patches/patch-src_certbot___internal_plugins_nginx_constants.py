be5482df672f4c9257e9b09217d34e4ef030d435
adam <adam@pkgsrc.org>
2026-08-03 09:36:28 +0000

    py-certbot: fixed finding nginx.conf

--- security/py-certbot/patches/patch-src_certbot___internal_plugins_nginx_constants.py
+++ security/py-certbot/patches/patch-src_certbot___internal_plugins_nginx_constants.py
@@ -1,11 +1,12 @@
-$NetBSD: patch-src_certbot___internal_plugins_nginx_constants.py,v 1.1 2026/04/13 12:44:45 adam Exp $
+$NetBSD: patch-src_certbot___internal_plugins_nginx_constants.py,v 1.2 2026/08/03 09:36:28 adam Exp $
 
 Look for nginx.conf in PKG_SYSCONFDIR
 
---- src/certbot/_internal/plugins/nginx/constants.py.orig	2025-09-02 15:46:13.000000000 +0000
+--- src/certbot/_internal/plugins/nginx/constants.py.orig	2026-07-07 18:28:26.000000000 +0000
 +++ src/certbot/_internal/plugins/nginx/constants.py
-@@ -2,19 +2,8 @@
- import platform
+@@ -1,20 +1,8 @@
+ """nginx plugin constants."""
+-import platform
  from typing import Any
  
 -FREEBSD_DARWIN_SERVER_ROOT = "/usr/local/etc/nginx"
@@ -21,7 +22,7 @@ Look for nginx.conf in PKG_SYSCONFDIR
 -
  CLI_DEFAULTS: dict[str, Any] = {
 -    "server_root": server_root_tmp,
-+    "server_root": "@PKG_SYSCONFDIR@/nginx",
++    "server_root": "@PKG_SYSCONFBASE@/nginx",
      "ctl": "nginx",
      "sleep_seconds": 1
  }
