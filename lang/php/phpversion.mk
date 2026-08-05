298e1856b0acedd56fb57b8542b67616f93c5def
maya <maya@pkgsrc.org>
2026-08-05 15:24:23 +0000

    Pullup ticket #7218 - requested by taca lang/php83: Security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.509
    - lang/php83/distinfo                                           1.36
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Jul 31 04:49:00 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php83: distinfo
    
       Log Message:
       lang/php83: update to 8.3.33
    
       PHP 8.3.33 (2026-07-30)
    
       - Date:
         . Fixed leak on double DatePeriod::__construct() call. (ilutov)
    
       - GD:
         . Upgrade libgd. (CVE-2026-9672) (Pierre Joye)
    
       - PGSQL:
         . Fixed GHSA-7qpv-r5mr-78m4 (SQL injection via E'...' backslash breakout).
           (CVE-2026-17543) (ilutov)
    
       - Phar:
         . Fixed GHSA-vc5h-9ppw-p5f3 (Crash via recursive symlinks). (CVE-2026-7260)
           (Jakub Zelenka)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.502.2.5 2026/08/05 15:21:43 maya Exp $
+# $NetBSD: phpversion.mk,v 1.502.2.6 2026/08/05 15:24:23 maya Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -111,7 +111,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP82_VERSION=	8.2.33
-PHP83_VERSION=	8.3.32
+PHP83_VERSION=	8.3.33
 PHP84_VERSION=	8.4.23
 PHP85_VERSION=	8.5.8
 
