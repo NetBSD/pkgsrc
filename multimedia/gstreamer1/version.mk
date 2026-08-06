83077efb374719e5c17f20d4a3eea83bfe623817
adam <adam@pkgsrc.org>
2026-08-06 19:11:10 +0000

    gstreamer1: updated to 1.28.6
    1.28.6
    
    Highlighted bugfixes:
    
    Various security fixes and playback fixes
    playbin3, playbin: fix stalls after re-enabling previously disabled subtitles
    Fix regression in core if a pad is re-linked while changed sticky events are being pushed
    dtls/webrtc: fix some issues with OpenSSL 4.0.0
    RTP retransmission bitrate estimation fixes
    Fix RTP depayloading of SMPTE ST291 frames with multiple ANC packets
    Add H.266 muxing support to the Rust (f)mp4 muxers
    Better handling of input buffers without timestamps in Rust (f)mp4 muxers
    webrtcsink H.264 level/profile negotiation fixes and support for nvv4l2h265enc encoder
    SMPTE ST2038 ancillary metadata and closed caption combiner improvements
    Fix SEI insertion into H.265/HEVC streams with alpha
    Windows D3D11 WinRT screen capture element fixes
    Improved coded buffer size handling for VA encoders
    Textaccumulate: various tweaks how the element outputs text, plus better handling of French punctuation
    hlssink3: improved handling of input buffers without timestamps
    Fix build against FFmpeg 9.0
    cerbero: fix Windows packages binary size increase regression; upgrade libsrt recipe to 1.5.6
    Various bug fixes, build fixes, memory leak fixes, and other stability and reliability improvements

--- multimedia/gstreamer1/version.mk
+++ multimedia/gstreamer1/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.33 2026/07/17 13:42:16 adam Exp $
+# $NetBSD: version.mk,v 1.34 2026/08/06 19:11:10 adam Exp $
 
-GST_VERSION=	1.28.5
+GST_VERSION=	1.28.6
 .if ${GST_VERSION:E} < 10
 SHLIBVER=	${GST_VERSION:S/1.//:S/./0/}
 .else
