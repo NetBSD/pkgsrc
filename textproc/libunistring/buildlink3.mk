1aee01428ece9662e5768ea77ffa7001fb9a1744
adam <adam@pkgsrc.org>
2026-08-07 09:03:53 +0000

    libunistring: fix for iconv detection on Darwin

--- textproc/libunistring/buildlink3.mk
+++ textproc/libunistring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2022/10/26 10:31:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2026/08/07 09:03:53 adam Exp $
 
 BUILDLINK_TREE+=	libunistring
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libunistring
 LIBUNISTRING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libunistring+=	libunistring>=0.9.3
-BUILDLINK_ABI_DEPENDS.libunistring?=	libunistring>=1.1
+BUILDLINK_ABI_DEPENDS.libunistring+=	libunistring>=1.1
 BUILDLINK_PKGSRCDIR.libunistring?=	../../textproc/libunistring
 
 .endif	# LIBUNISTRING_BUILDLINK3_MK
