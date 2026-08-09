c4ef69dc13b14be445900ed91855104e9204c54e
wiz <wiz@pkgsrc.org>
2026-08-09 17:28:28 +0000

    geeqie: update to 3.1.
    Migrate to GTK4.

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,35 +1,23 @@
-# $NetBSD: options.mk,v 1.12 2026/07/21 15:47:06 wiz Exp $
+# $NetBSD: options.mk,v 1.13 2026/08/09 17:28:28 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.geeqie
-PKG_SUPPORTED_OPTIONS=		gspell libarchive libchamplain libraw libwebp pdf # gtk4
+PKG_SUPPORTED_OPTIONS=		libarchive libraw libshumate libwebp pdf
 PKG_SUGGESTED_OPTIONS=		# none
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mgspell)
-.include "../../textproc/gspell/buildlink3.mk"
-.endif
-
-#.if !empty(PKG_OPTIONS:Mgtk4)
-## doesn't build as of 2.9
-#MESON_ARGS+=	-Dgtk4=enabled
-#.include "../../x11/gtk4/buildlink3.mk"
-#.else
-#.include "../../x11/gtk3/buildlink3.mk"
-#.endif
+# TODO libspelling option, when package exists
 
 .if !empty(PKG_OPTIONS:Mlibarchive)
 .include "../../archivers/libarchive/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mlibchamplain)
-# While clutter is optional, the map requires it.
-.include "../../graphics/clutter/buildlink3.mk"
-.include "../../geography/libchamplain012/buildlink3.mk"
-.endif
-
 .if !empty(PKG_OPTIONS:Mlibraw)
 .include "../../graphics/libraw/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibshumate)
+.include "../../geography/libshumate/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibwebp)
---
+++ graphics/geeqie/patches/patch-src_cellrenderericon.cc
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_cellrenderericon.cc,v 1.1 2026/08/09 17:28:28 wiz Exp $
+
+../src/cellrenderericon.cc: In function 'gint gqv_cell_renderer_icon_mark_at(GtkCellRenderer*, GtkWidget*, const GdkRectangle*, gdouble, gdouble)':
+../src/cellrenderericon.cc:101:50: error: 'floor' is not a member of 'std'; did you mean 'floor'?
+  101 |         return std::clamp(static_cast<gint>(std::floor(((x - first_center) / TOGGLE_SPACING) + 0.5)),
+      |                                                  ^~~~~
+
+--- src/cellrenderericon.cc.orig	2026-08-09 17:20:27.910468088 +0000
++++ src/cellrenderericon.cc
+@@ -20,6 +20,7 @@
+ #include "cellrenderericon.h"
+ 
+ #include <algorithm>
++#include <cmath>
+ #include <cstddef>
+ 
+ #include <cairo.h>
