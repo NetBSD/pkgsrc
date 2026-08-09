f963de0042103b4ea5d215f8657d722ff093f022
wiz <wiz@pkgsrc.org>
2026-08-09 17:42:34 +0000

    geeqie: add upstream bug report URL

--- graphics/geeqie/patches/patch-src_cellrenderericon.cc
+++ graphics/geeqie/patches/patch-src_cellrenderericon.cc
@@ -1,9 +1,10 @@
-$NetBSD: patch-src_cellrenderericon.cc,v 1.1 2026/08/09 17:28:28 wiz Exp $
+$NetBSD: patch-src_cellrenderericon.cc,v 1.2 2026/08/09 17:42:34 wiz Exp $
 
 ../src/cellrenderericon.cc: In function 'gint gqv_cell_renderer_icon_mark_at(GtkCellRenderer*, GtkWidget*, const GdkRectangle*, gdouble, gdouble)':
 ../src/cellrenderericon.cc:101:50: error: 'floor' is not a member of 'std'; did you mean 'floor'?
   101 |         return std::clamp(static_cast<gint>(std::floor(((x - first_center) / TOGGLE_SPACING) + 0.5)),
       |                                                  ^~~~~
+https://github.com/BestImageViewer/geeqie/pull/2533
 
 --- src/cellrenderericon.cc.orig	2026-08-09 17:20:27.910468088 +0000
 +++ src/cellrenderericon.cc
