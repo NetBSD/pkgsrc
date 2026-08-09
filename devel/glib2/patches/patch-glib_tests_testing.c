f96c7030ea02e0c1b4d44f511c99402a3475d702
mef <mef@pkgsrc.org>
2026-08-09 02:32:02 +0000

    (devel/glib2) move the line '#define _POSIX_C_SOURCE 200809L' a little bit eariler

--- devel/glib2/patches/patch-glib_tests_testing.c
+++ devel/glib2/patches/patch-glib_tests_testing.c
@@ -1,11 +1,10 @@
-$NetBSD: patch-glib_tests_testing.c,v 1.1 2026/08/08 22:59:54 mef Exp $
+$NetBSD: patch-glib_tests_testing.c,v 1.2 2026/08/09 02:32:02 mef Exp $
 
 revert the changes from 2.88.1 to 2.88.3
-(with minor mod.)
 to fix F_DUPFD_CLOEXEC undefined issue
 
---- /tmp/devel/glib2/work/glib-2.88.3/glib/tests/testing.c.orig	2026-07-29 22:17:36.000000000 +0900
-+++ glib/tests/testing.c	2026-08-09 07:50:57.685554036 +0900
+--- glib/tests/testing.c.orig	2026-07-29 22:17:36.000000000 +0900
++++ glib/tests/testing.c	2026-08-09 11:25:57.279948186 +0900
 @@ -22,7 +22,6 @@
   * if advised of the possibility of such damage.
   */
@@ -14,12 +13,11 @@ to fix F_DUPFD_CLOEXEC undefined issue
  
  #include "config.h"
  
-@@ -37,6 +36,8 @@
+@@ -33,6 +32,7 @@
+ #define G_LOG_DOMAIN "testing"
+ 
+ #include <glib.h>
++#define _POSIX_C_SOURCE 200809L  /* for F_DUPFD_CLOEXEC */
+ #include <locale.h>
  #include <stdlib.h>
  #include <string.h>
- 
-+#define _POSIX_C_SOURCE 200809L  /* for F_DUPFD_CLOEXEC */
-+
- #ifdef G_OS_UNIX
- #include <fcntl.h>
- #include <glib-unix.h>
