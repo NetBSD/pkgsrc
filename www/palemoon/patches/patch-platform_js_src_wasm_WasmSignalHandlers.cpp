8c3821e57c33203af07e5fc7c2642b95e77d6e10
bsiegert <bsiegert@pkgsrc.org>
2026-08-10 11:41:38 +0000

    Pullup ticket #7225 - requested by nia www/palemoon: security fix
    Revisions pulled up:
    - www/palemoon/Makefile.common                                  1.10
    - www/palemoon/distinfo                                         1.47
    - www/palemoon/patches/patch-platform_js_src_wasm_WasmSignalHandlers.cpp 1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Sat Aug  8 11:59:01 UTC 2026
    
       Modified Files:
       	pkgsrc/www/palemoon: Makefile.common distinfo
       Added Files:
       	pkgsrc/www/palemoon/patches:
       	    patch-platform_js_src_wasm_WasmSignalHandlers.cpp
    
       Log Message:
       palemoon: Update to 34.3.2
    
       - A large security audit of over 200 sec bugs in Mozilla land has been
         performed, with the vast majority not being applicable to our code base
         (primarily e10s/IPC bugs).
       - Implemented the URL.Parse() JavaScript convenience function.

---
+++ www/palemoon/patches/patch-platform_js_src_wasm_WasmSignalHandlers.cpp
@@ -0,0 +1,17 @@
+$NetBSD: patch-platform_js_src_wasm_WasmSignalHandlers.cpp,v 1.1.2.2 2026/08/10 11:41:38 bsiegert Exp $
+
+Fix build on newer NetBSD versions.
+
+PR kern/60539 XSAVE changes break ucontext userspace API
+
+--- platform/js/src/wasm/WasmSignalHandlers.cpp.orig	2026-07-28 13:51:54.000000000 +0000
++++ platform/js/src/wasm/WasmSignalHandlers.cpp
+@@ -174,7 +174,7 @@ class AutoSetHandlingSegFault
+ #  define RFP_sig(p) ((p)->uc_mcontext.__gregs[22])
+ # endif
+ #elif defined(__NetBSD__)
+-# define XMM_sig(p,i) (((struct fxsave64*)(p)->uc_mcontext.__fpregs)->fx_xmm[i])
++# define XMM_sig(p,i) (((struct fxsave64*)&(p)->uc_mcontext.__fpregs)->fx_xmm[i])
+ # define EIP_sig(p) ((p)->uc_mcontext.__gregs[_REG_EIP])
+ # define RIP_sig(p) ((p)->uc_mcontext.__gregs[_REG_RIP])
+ # define RAX_sig(p) ((p)->uc_mcontext.__gregs[_REG_RAX])
