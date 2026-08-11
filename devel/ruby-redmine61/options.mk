82ff482bf88909117c266a710abefcfb9a7428a3
taca <taca@pkgsrc.org>
2026-08-11 15:54:57 +0000

    devel/ruby-redmine61: update dependency
    Depends on databases/ruby-with_advisory_lock since there was no reason to
    restrict to 5.x.
    
    Bump PKGREVISION.

--- devel/ruby-redmine61/options.mk
+++ devel/ruby-redmine61/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2026/02/11 07:53:37 taca Exp $
+# $NetBSD: options.mk,v 1.2 2026/08/11 15:54:57 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.redmine60
 
@@ -16,7 +16,7 @@ PLIST_VARS+=			unicorn
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
 DEPENDS+=	${RUBY_PKGPREFIX}-mysql2>=0.5.0<0.6:../../databases/ruby-mysql2
-DEPENDS+=	${RUBY_PKGPREFIX}-with_advisory_lock53>=5.3.0<6:../../databases/ruby-with_advisory_lock53
+DEPENDS+=	${RUBY_PKGPREFIX}-with_advisory_lock>=5.3.0:../../databases/ruby-with_advisory_lock
 .elif !empty(PKG_OPTIONS:Mpgsql)
 DEPENDS+=	${RUBY_PKGPREFIX}-pg>=1.5.3<2.0:../../databases/ruby-pg
 .elif !empty(PKG_OPTIONS:Msqlite3)
