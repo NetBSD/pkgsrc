428932cc0d85c35ce6107bcebac06eaaa44a9d08
ktnb <ktnb@pkgsrc.org>
2026-08-11 14:40:33 +0000

    mu: update to 1.14.3
    Many internal cleanups/fixups/C++20-modernization, but also a number
    of new features:
    mu:
     - better handling of html parts
     - fall back to most recent 'Received' for 'Date:'-less messages
     - record the time-zone offset
     - command-line completion for bash & zsh
    mu4e:
     - improved mime-handling
     - improved HTML view (generate HTML view for pure text as well)
     - hide "clickables" until requested (but see
     - mu4e-view-always-show-url-indicators)
    scm:
     - performance improvements
     - new methods for querying system configuration" and querying
       database
     - fields, and getting timezone information (for 'date' and
       'changed')

--- mail/mu/patches/patch-lib_message_mu-contact.hh
+++ mail/mu/patches/patch-lib_message_mu-contact.hh
@@ -1,15 +1,15 @@
-$NetBSD: patch-lib_message_mu-contact.hh,v 1.1 2026/01/29 16:41:56 ktnb Exp $
+$NetBSD: patch-lib_message_mu-contact.hh,v 1.2 2026/08/11 14:40:34 ktnb Exp $
 
 Fix error -30 on NetBSD 11
 
---- lib/message/mu-contact.hh.orig	2026-01-15 19:59:15.000000000 +0000
+--- lib/message/mu-contact.hh.orig	2026-08-09 13:09:55.896030812 +0000
 +++ lib/message/mu-contact.hh
-@@ -184,7 +184,7 @@ struct Contact {
+@@ -166,7 +166,7 @@ struct Contact { (private)
  private:
  	void cleanup_name() { // replace control characters by spaces.
  		for (auto& c: name)
--			if (iscntrl(c))
-+			if (iscntrl(static_cast<unsigned char>(c)))
+-			if (is_ascii_cntrl(c))
++			if (is_ascii_cntrl(static_cast<unsigned char>(c)))
  				c = ' ';
  	}
  
