459c0ceba966ba02188655667c482d62ee5e32f0
wiz <wiz@pkgsrc.org>
2026-08-16 12:11:59 +0000

    googletest: update to 1.18.0.
    C++ Language Support
    
        GoogleTest now requires at least C++17 and follows Google's
        Foundational C++ Support Policy. See this table for a list of
        currently supported versions compilers, platforms, and build
        tools.
    
    Notable Changes
    
        Set GTEST_INTERNAL_HAS_STRING_VIEW = 1 in googletest as C++17
        always supports it (4f47274)
    
        Many bug fixes

--- devel/googletest/buildlink3.mk
+++ devel/googletest/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2023/07/20 09:46:33 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/08/16 12:11:59 wiz Exp $
 
 BUILDLINK_TREE+=	googletest
 
 .if !defined(GOOGLETEST_BUILDLINK3_MK)
 GOOGLETEST_BUILDLINK3_MK:=
 
-USE_CXX_FEATURES+=		c++14
+USE_CXX_FEATURES+=		c++17
 
 BUILDLINK_API_DEPENDS.googletest+=	googletest>=1.4.0
 BUILDLINK_DEPMETHOD.googletest?=	build
