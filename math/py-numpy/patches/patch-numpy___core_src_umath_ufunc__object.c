bc24136a0a51fd91e2e27776646268f64e981000
adam <adam@pkgsrc.org>
2026-08-17 09:27:49 +0000

    py-numpy: updated to 2.5.2
    2.5.2
    
    MAINT: Prepare 2.5.x for further development
    TYP: Backport multiple static typing fixes 1.
    TST: add tests for stable ABI numpy extensions
    BUG: fix ``StringDType`` coerce flag in binary ufunc promotion...
    BLD: fix meson deprecation warnings
    TYP: Backport multiple typing fixes 2.
    MAINT: Update x86-simd-sort subproject (5adb334 → fa944ef)
    BUG: fix crash on 32 bit systems using abi3t
    MNT: remove some obsolete string to bool workarounds
    BUG: centralized helper for output coerce and na_object in stringdtype...
    BUG: fix CPU feature env diagnostic buffer overruns
    BUG: restore ndarray.conjugate() for legacy user-defined dtypes...
    TYP: Avoid shadowed dtype annotations
    MAINT: Update verdored-meson/meson to match main.
    BUG: fix refcount leak on overlapping copyto with where=False
    BUG: fix swallowed cast error in fancy indexing assignment
    BUG: Fix buffered iterator stride after removing multi-index
    BUG: fix ``np.fromiter`` corruption when reusing a ``StringDType``...
    BUG: add a special case for StringDType in np.isdtype
    BUG: reference leak in ``simd_sequence_from_iterable``
    BUG: ensure lock is held when accessing or writing to RNG state...
    BUG: fully reset cached RNG state for non-MT19937 RNGs
    TYP: type capabilities max dimensions
    BUG: avoid possible stack overflow in arraydescr_dealloc
    MAINT: Update cibuildwheel to v4.2.0
    MAINT: Skip limited_api tests on some platforms.
    TYP: ``isclose`` shape-typing fix for 2d array-likes
    BUG: avoid segfaults when legacy copyswap slot is not defined...

---
+++ math/py-numpy/patches/patch-numpy___core_src_umath_ufunc__object.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-numpy___core_src_umath_ufunc__object.c,v 1.1 2026/08/17 09:27:49 adam Exp $
+
+Avoid static_assert in C.
+
+--- numpy/_core/src/umath/ufunc_object.c.orig	2026-08-14 12:42:43.705330610 +0000
++++ numpy/_core/src/umath/ufunc_object.c
+@@ -5489,8 +5489,6 @@ PyUFunc_RegisterLoopForType(PyUFuncObjec
+ #undef _SETCPTR
+ 
+ #undef _PyUFuncObject_GET_ITEM_DATA
+-static_assert(NPY_ALIGNOF(PyUFuncObject_fields) <= 8,
+-              "PyUFuncObject must not require more than 8-byte alignment");
+ /*UFUNC_API*/
+ NPY_NO_EXPORT PyUFuncObject_fields *
+ _PyUFuncObject_GET_ITEM_DATA(const PyUFuncObject *obj)
