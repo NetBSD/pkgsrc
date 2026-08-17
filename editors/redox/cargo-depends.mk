f76b891161cbf7fbc8cc51dc99452558f58281a3
pin <pin@pkgsrc.org>
2026-08-17 07:15:22 +0000

    editors/redox: update to 0.8.1
    This is the last tiny set of fixes before a thorough editor rewrite, which will mostly be to clean up the scope of the project and the implementation details of some features.
    
        Tweaked the yellow shades in the base colour scheme
        Removed the perf corners widget because it's fundamentally not useful for assessing editor performance, which I somehow only just realized :)

--- editors/redox/cargo-depends.mk
+++ editors/redox/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.11 2026/07/22 06:28:28 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.12 2026/08/17 07:15:22 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.4
