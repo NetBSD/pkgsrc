619d194c9c430e3c966c65f6a2a56de1c1e94488
drixter <drixter@pkgsrc.org>
2026-08-20 21:30:44 +0000

    resterm: Update to 1.2.1
    Fixed
    Recover from a broken environment file
    Environment edits now take effect

--- www/resterm/go-modules.mk
+++ www/resterm/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.20 2026/08/18 18:48:17 drixter Exp $
+# $NetBSD: go-modules.mk,v 1.21 2026/08/20 21:30:44 drixter Exp $
 
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.zip
