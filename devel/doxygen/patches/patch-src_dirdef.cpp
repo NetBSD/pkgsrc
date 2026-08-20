27d76b65aa55b7089c77711ec0521ee2c22a8535
adam <adam@pkgsrc.org>
2026-08-20 07:48:05 +0000

    doxygen: updated to 1.18.0
    1.18.0
    
    Features
    
    Doxywizard's GUI and config option documentation are now translated to 8 addition languages (next to English): German, French, Spanish, Russian, Korean, Chinese, Taiwanese, and Japanese.
    From the menu one can switch between languages (doing so will relaunch the application).
    Via the wizard the Doxyfile can also be generated in 8 different languages next to English.
    The expert panel of Doxywizard now has a search field to quickly search for and filter configuration options. [view], [view], [view], [view], [view], [view], [view], [view], [view], [view], [view], [view], and [view]
    Changed doxygen.ico to higher resolution version [view]
    When using the external search engine, icons are used instead of numbers and words for search result types [view], and [view]
    
    Minor incompatibilities
    
    Made SOURCE_TOOLTIPS independent of HTML_DYNAMIC_SECTIONS [view]
    
    Bug fixes

--- devel/doxygen/patches/patch-src_dirdef.cpp
+++ devel/doxygen/patches/patch-src_dirdef.cpp
@@ -1,17 +1,19 @@
-$NetBSD: patch-src_dirdef.cpp,v 1.5 2023/12/31 09:48:43 wiz Exp $
+$NetBSD: patch-src_dirdef.cpp,v 1.6 2026/08/20 07:48:05 adam Exp $
 
 Doxygen encodes the source directory path into some of the output filenames.
 This is problematic in a pkgsrc context as it means WRKDIR pathnames can appear
 in generated filenames.
 
---- src/dirdef.cpp.orig	2023-12-31 08:43:03.452447140 +0000
+--- src/dirdef.cpp.orig	2026-08-13 11:44:36.000000000 +0000
 +++ src/dirdef.cpp
-@@ -220,7 +220,7 @@ static QCString encodeDirName(const QCSt
+@@ -224,8 +224,8 @@ static DString encodeDirName(const DStri
  
- QCString DirDefImpl::getOutputFileBase() const
+ DString DirDefImpl::getOutputFileBase() const
  {
--  QCString dir = "dir_"+encodeDirName(m_diskName);
-+  QCString dir = "dir_"+encodeDirName(m_shortName);
-   AUTO_TRACE("diskName={} result={}",m_diskName,dir);
+-  DString dir = "dir_"+encodeDirName(m_diskName);
+-  AUTO_TRACE("diskName={} result={}",m_diskName,dir);
++  DString dir = "dir_"+encodeDirName(m_shortName);
++  AUTO_TRACE("diskName={} result={}",m_shortName,dir);
    return dir;
  }
+ 
