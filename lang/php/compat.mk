10709f359cc862960da2b7e7e2466f43775bfd84
maya <maya@pkgsrc.org>
2026-08-20 01:56:28 +0000

    Pullup ticket #7239 - requested by taca lang/php: New mk file to ease pull-ups to PHP packages
    Revisions pulled up:
    - lang/php/compat.mk                                            1.1-1.3
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu Jul 30 16:15:31 UTC 2026
    
       Added Files:
       	pkgsrc/lang/php: compat.mk
    
       Log Message:
       lang/php: introduce compat.mk
    
       php/compat.mk is more generic version of php/json.mk.
    
       PHP_REQUIRE_MODULE is list of key words which speicifies conditionally
       depending php packages.  Currently, it supports imap, json and mcrypt.
    
       * imap and mcrypt
    
       	specifies bundled package or pecl version.
    
       * json
    
       	specifies bundled package for prior to PHP 8.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu Jul 30 16:25:54 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/php: compat.mk
    
       Log Message:
       lang/php/compat.mk: remove pkglint warnings
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Aug  7 01:40:27 UTC 2026
    
       Modified Files:
       	pkgsrc/lang/php: compat.mk
    
       Log Message:
       php/compat.mk: Add support for apcu and opcache

---
+++ lang/php/compat.mk
@@ -0,0 +1,54 @@
+# $NetBSD$
+#
+# Support for dependency several php packages which was contained
+# in base PHP distributions.
+#
+# PHP_REQUIRE_MODULE
+#	list of key words which speicifies conditionally depending
+#	php packages.
+#
+#	Possible:	apcu imap json mcrypt opcache
+#	Default:	(empty)
+#
+.if !defined(PHP_COMPAT_MK)
+PHP_COMPAT_MK:=	# empty
+
+.include "../../lang/php/phpversion.mk"
+
+.if ${PHP_REQUIRE_MODULE:Mapcu}
+. if ${PHP_VER} < 70
+DEPENDS+=	${PHP_PKG_PREFIX}-apcu<5:../../www/php-apcu4
+. else
+DEPENDS+=	${PHP_PKG_PREFIX}-apcu>=5:../../www/php-apcu
+. endif
+.endif # apcu
+
+.if ${PHP_REQUIRE_MODULE:Mimap}
+. if ${PHP_VER} < 84
+DEPENDS+=	${PHP_PKG_PREFIX}-imap>=${PHP_BASE_VERS}:../../mail/php-imap
+. else
+DEPENDS+=	${PHP_PKG_PREFIX}-imap>=1.0.3:../../mail/php-pecl-imap
+. endif
+.endif # imap
+
+.if ${PHP_REQUIRE_MODULE:Mjson}
+. if ${PHP_VER} < 80
+DEPENDS+=	${PHP_PKG_PREFIX}-json>=${PHP_BASE_VERS}:../../textproc/php-json
+. endif
+.endif # json
+
+.if ${PHP_REQUIRE_MODULE:Mmcrypt}
+. if ${PHP_VER} < 71
+DEPENDS+=	${PHP_PKG_PREFIX}-mcrypt>=${PHP_BASE_VERS}:../../security/php-mcrypt
+.  else
+DEPENDS+=	${PHP_PKG_PREFIX}-pecl-mcrypt>=1.0.1:../../security/php-pecl-mcrypt
+.  endif
+.endif # mcrypt
+
+.if ${PHP_REQUIRE_MODULE:Mopcache}
+. if ${PHP_VER} < 85
+DEPENDS+=	${PHP_PKG_PREFIX}-opcache>=${PHP_BASE_VERS}:../../devel/php-opcache
+. endif
+.endif # opcache
+
+.endif	# PHP_COMPAT_MK
