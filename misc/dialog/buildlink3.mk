33bef0497d12ca4bf0f039fb8624db0691541947
adam <adam@pkgsrc.org>
2026-08-21 10:27:56 +0000

    dialog: updated to 1.3.20260721
    2026/07/21
    + correct order of sprintf parameters for token-count error messages
      (patch by Xinting Zhou).
    + updated cs.po, zh_CN.po from
            https://translationproject.org/latest/dialog/

--- misc/dialog/buildlink3.mk
+++ misc/dialog/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/08/18 07:31:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/08/21 10:27:56 adam Exp $
 
 BUILDLINK_TREE+=	dialog
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	dialog
 DIALOG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.dialog+=	dialog>=1.1.20080819nb1
-BUILDLINK_ABI_DEPENDS.dialog?=	dialog>=1.2.20150528nb2
+BUILDLINK_ABI_DEPENDS.dialog+=	dialog>=1.2.20150528nb2
 BUILDLINK_PKGSRCDIR.dialog?=	../../misc/dialog
 
 .include "../../mk/curses.buildlink3.mk"
