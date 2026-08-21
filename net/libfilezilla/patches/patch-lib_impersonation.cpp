4141ec86d28d1cde8dbf578715861d4371c50228
wiz <wiz@pkgsrc.org>
2026-08-21 12:29:04 +0000

    libfilezilla: update to 0.57.0.
    0.57.0 (2026-08-18)
    
    + Added support for creating impersonation_token from either user names or user ids
    + Added APIs to check whether impersonation is available before trying to create a token
    + Added fz::get_user_id() and fz::get_user_name()
    + MSW: Added passwordless impersonation tokens
    + MSW: Added desktop selection for processes spawned under impersonation
    + MSW: Process spawning under impersonation can now report setup failures through a logger
    + Improved the impersonation demo with user id, passwordless, desktop and custom command support
    + Added fz::secure_buffer that wipes memory on deallocation
    + Added fz::pbkd2 function taking hmac algorithm as argument
    - MSW: Loading impersonated user profiles no longer changes caller process privileges
    - MSW: Fixed EOF handling when reading redirected process output after the child process exits

--- net/libfilezilla/patches/patch-lib_impersonation.cpp
+++ net/libfilezilla/patches/patch-lib_impersonation.cpp
@@ -1,20 +1,20 @@
-$NetBSD: patch-lib_impersonation.cpp,v 1.4 2022/07/21 11:31:23 wiz Exp $
+$NetBSD: patch-lib_impersonation.cpp,v 1.5 2026/08/21 12:29:04 wiz Exp $
 
 Hide Linux-specific stuff in ifdef __Linux__
 https://trac.filezilla-project.org/ticket/12658
 
---- lib/impersonation.cpp.orig	2022-07-08 13:18:44.000000000 +0000
+--- lib/impersonation.cpp.orig	2026-08-18 13:40:55.000000000 +0000
 +++ lib/impersonation.cpp
-@@ -7,7 +7,7 @@
- #include <optional>
- #include <tuple>
+@@ -81,7 +81,7 @@ impersonation_token::impersonation_token(fz::user cons
+ 
+ #include "libfilezilla/buffer.hpp"
  
 -#if FZ_UNIX
 +#if FZ_UNIX && defined(__Linux__)
  #include <crypt.h>
  #include <shadow.h>
  #endif
-@@ -98,7 +98,7 @@ std::optional<gid_t> get_group(native_st
+@@ -177,7 +177,7 @@ std::optional<gid_t> get_group(native_string const& gn
  	return {};
  }
  
@@ -23,7 +23,7 @@ https://trac.filezilla-project.org/ticket/12658
  struct shadow_holder {
  	shadow_holder() = default;
  	shadow_holder(shadow_holder const&) = delete;
-@@ -190,7 +190,7 @@ std::vector<gid_t> get_supplementary(std
+@@ -265,7 +265,7 @@ bool check_auth(native_string const& username, native_
  
  bool check_auth(native_string const& username, native_string const& password)
  {
