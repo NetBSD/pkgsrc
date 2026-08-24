9e9b74cb4b612319dc71b20a9b35a782178108f2
adam <adam@pkgsrc.org>
2026-08-24 08:07:02 +0000

    librevenge: updated to 0.0.6
    librevenge 0.0.6
    
    - Improve robustness when reading damaged or malformed OLE2 and Zip
      streams.
    - Improve performance.
    - Replace the deprecated std::shared_ptr::unique() with use_count() == 1.

--- converters/librevenge/buildlink3.mk
+++ converters/librevenge/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2026/05/15 09:58:43 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2026/08/24 08:07:02 adam Exp $
 
 BUILDLINK_TREE+=	librevenge
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	librevenge
 LIBREVENGE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.librevenge+=	librevenge>=0.0.1
-BUILDLINK_ABI_DEPENDS.librevenge+=	librevenge>=0.0.5nb4
+BUILDLINK_ABI_DEPENDS.librevenge+=	librevenge>=0.0.6
 BUILDLINK_PKGSRCDIR.librevenge?=	../../converters/librevenge
 
 .include "../../devel/boost-libs/buildlink3.mk"
