9a0c65baf8bfc62719ce2157a4c2d7ff22ef38d8
adam <adam@pkgsrc.org>
2026-08-24 08:10:39 +0000

    libvisio: updated to 0.1.9
    libvisio 0.1.9
    - Implement displaying Angle TextField
    - Correctly display Text Background for VSD 6 and 11 format
    - Better handling of theme colors

--- converters/libvisio/buildlink3.mk
+++ converters/libvisio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.51 2026/05/15 09:58:43 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.52 2026/08/24 08:10:39 adam Exp $
 
 BUILDLINK_TREE+=	libvisio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libvisio
 LIBVISIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libvisio+=	libvisio>=0.0.15
-BUILDLINK_ABI_DEPENDS.libvisio+=	libvisio>=0.1.7nb32
+BUILDLINK_ABI_DEPENDS.libvisio+=	libvisio>=0.1.9
 BUILDLINK_PKGSRCDIR.libvisio?=		../../converters/libvisio
 
 .include "../../converters/librevenge/buildlink3.mk"
