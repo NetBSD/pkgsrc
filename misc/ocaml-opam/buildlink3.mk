e2efb0708cd369731d04dc7ec544ad463cc80025
wiz <wiz@pkgsrc.org>
2026-08-25 18:07:44 +0000

    ocaml-opam: update to 2.5.2.
    Provided by upstream, Kate, in wip.
    
    - update from 2.1.3 to 2.5.2 (fixing two CVEs in the process)
    - the -full tarball is now used instead of the one autogenerated by
      github
    - rsync is now a runtime dependency (it is technically optional but it
      helps users to have it for the moment)
    - ocaml-dune is now explicitly a build dependency but no longer used
      as-is. It is now used through gmake
    - ocaml-cmdliner, ocaml-re, ocamlgraph, dose3, ocaml-cudf and
      ocaml-opam-file-format are no longer dependencies
    - the vendored versions of all the dependencies contained by the -full
      archive is now used. This allows to simplify the build process
      greatly and avoids having to import all the new dependencies on new opam
      releases. This is also what a majority of BSD, Linux and macOS distributions
      out-there do.
    - The opam-solver library is no longer installed. This could not be used
      on its own anyway because the OCaml build process would require other
      modules that are not installed whose checksum wouldn't match if they
      were built separately.
    - Dummy and unused META, dune-package and opam files generated by dune
      are no longer installed
    - CHANGES, LICENSE and README.md are no longer installed

--- misc/ocaml-opam/buildlink3.mk
+++ misc/ocaml-opam/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2021/03/04 09:27:35 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2026/08/25 18:07:44 wiz Exp $
 
 BUILDLINK_TREE+=	ocaml-opam
 
 .if !defined(OCAML_OPAM_BUILDLINK3_MK)
 OCAML_OPAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.8
+BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=2.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.5.2
 BUILDLINK_PKGSRCDIR.ocaml-opam?=	../../misc/ocaml-opam
 
 .endif	# OCAML_OPAM_BUILDLINK3_MK
