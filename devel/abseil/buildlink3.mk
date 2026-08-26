6d0e3a86072a399d2766a78170a86a7e5cbbd931
adam <adam@pkgsrc.org>
2026-08-26 10:09:07 +0000

    abseil: updated to 20260817.0
    20260817.0
    
    What's New
    
    Added URL escaping utilities
    Many bug fixes and optimizations
    
    Breaking Changes
    
    absl::void_t is now deprecated; users should use C++17 std::void_t directly.
    Hashtable iterators returned from insert/emplace are now non-iterable (any prior use of iteration was likely a bug).
    
    Known Issues
    
    None at this time

--- devel/abseil/buildlink3.mk
+++ devel/abseil/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2026/06/22 19:38:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2026/08/26 10:09:07 adam Exp $
 
 BUILDLINK_TREE+=	abseil
 
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	abseil
 ABSEIL_BUILDLINK3_MK:=
 
 # .buildlink/include/absl/base/policy_checks.h:57:2:
-# error: #error "This package requires GCC 7 or higher."
-GCC_REQD+=	7
+# error: #error "This package requires GCC 10 or higher."
+GCC_REQD+=	10
 
 USE_CXX_FEATURES+=	c++17 filesystem
 
 BUILDLINK_API_DEPENDS.abseil+=	abseil>=20220623.0
-BUILDLINK_ABI_DEPENDS.abseil+=	abseil>=20260526.0
+BUILDLINK_ABI_DEPENDS.abseil+=	abseil>=20260817.0
 BUILDLINK_PKGSRCDIR.abseil?=	../../devel/abseil
 .endif	# ABSEIL_BUILDLINK3_MK
 
--- devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
+++ devel/abseil/patches/patch-absl_time_internal_cctz_src_time__zone__format.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.5 2026/01/14 12:56:47 wiz Exp $
+$NetBSD: patch-absl_time_internal_cctz_src_time__zone__format.cc,v 1.6 2026/08/26 10:09:07 adam Exp $
 
 Avoid _XOPEN_SOURCE.
 
---- absl/time/internal/cctz/src/time_zone_format.cc.orig	2026-01-07 19:26:27.000000000 +0000
+--- absl/time/internal/cctz/src/time_zone_format.cc.orig	2026-08-18 12:53:30.000000000 +0000
 +++ absl/time/internal/cctz/src/time_zone_format.cc
-@@ -21,7 +21,7 @@
- #endif
+@@ -22,7 +22,7 @@
  
  #if HAS_STRPTIME
--#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
-+#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
+ #if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && \
+-    !defined(__OpenBSD__) && !defined(__APPLE__)
++    !defined(__OpenBSD__) && !defined(__APPLE__) && !defined(__NetBSD__)
  #define _XOPEN_SOURCE 500  // Exposes definitions for SUSv2 (UNIX 98).
  #endif
  #endif
