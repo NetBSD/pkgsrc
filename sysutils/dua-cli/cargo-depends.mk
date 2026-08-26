09a800ffb500a766a708a8ce92fef351a9b86f7c
pin <pin@pkgsrc.org>
2026-08-26 10:58:24 +0000

    sysutils/dua-cli: update to 2.43.0
    Thanks to our contributors, there are not one, but two (a joke, a human is writing this with fleshy fingers :D) headline features:
    
        1. dua aggregate --depth N shows an indented tree-like view, instead of a flat list
        2. dua aggregate --stack | inferno-flamegraph > fire.svg outputs a flame-graph compatible stack listing, for pretty visualisations of the directory. It also works with --depth N.
    
    On the side, on macOS, the bytes output of dua now matches du byte-perfectly, and for the first time it's possible to pipe dua
    output into a file without terminal escape sequences (why did this take me so long again?).
    New Features
    
        Add dua aggregate --depth N for indented tree output
    
    Bug Fixes
    
        don't output colors when stdout isn't a terminal.
        This facilitates piping into a file.
        --stats printed the u128 sentinel as the smallest file size
        The sentinel reset was guarded by entries_traversed == 0, but an entry
        whose metadata cannot be read still counts as traversed while never
        contributing a size. Reset when the sentinel is still in place instead.
        Apply --ignore-dirs to prepared aggregate roots
        Apply --ignore-dirs to paths dua expanded into roots itself

--- sysutils/dua-cli/cargo-depends.mk
+++ sysutils/dua-cli/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.42 2026/08/15 11:21:13 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.43 2026/08/26 10:58:24 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	addr2line-0.25.1
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
