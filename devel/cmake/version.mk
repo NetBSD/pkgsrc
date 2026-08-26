3b774b35a2936566ecb5ce755d2a3509eafd3958
adam <adam@pkgsrc.org>
2026-08-26 06:12:05 +0000

    cmake cmake-gui: updated to 4.4.3
    4.4.3
    Swift policy CMP0215's NEW behavior has been updated to require policies CMP0157 and CMP0195 to also be set to NEW.

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.90 2026/08/01 16:26:44 adam Exp $
+# $NetBSD: version.mk,v 1.91 2026/08/26 06:12:05 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	4.4.2
+CMAKE_VERSION=	4.4.3
 CMAKE_API=	${CMAKE_VERSION:R}
