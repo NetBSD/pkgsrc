ecf0a1bdba9a82a225b07219baebc157df6f4db6
leot <leot@pkgsrc.org>
2026-08-27 20:46:35 +0000

    net/opentofu16: import opentofu16-1.6.3
    OpenTofu is an OSS tool for building, changing, and versioning
    infrastructure safely and efficiently. OpenTofu can manage existing and
    popular service providers as well as custom in-house solutions.
    
    The key features of OpenTofu are:
    
    * Infrastructure as Code: Infrastructure is described using a
      high-level configuration syntax. This allows a blueprint of your
      datacenter to be versioned and treated as you would any other code.
      Additionally, infrastructure can be shared and re-used.
    * Execution Plans: OpenTofu has a "planning" step where it generates
      an execution plan. The execution plan shows what OpenTofu will do
      when you call apply. This lets you avoid any surprises when
      OpenTofu manipulates infrastructure.
    * Resource Graph: OpenTofu builds a graph of all your resources, and
      parallelizes the creation and modification of any non-dependent
      resources. Because of this, OpenTofu builds infrastructure as
      efficiently as possible, and operators get insight into dependencies in
      their infrastructure.
    * Change Automation: Complex changesets can be applied to your
      infrastructure with minimal human interaction. With the previously
      mentioned execution plan and resource graph, you know exactly what
      OpenTofu will change and in what order, avoiding many possible human
      errors.
    
    This package tracks OpenTofu 1.6.x version.
    
    Discussed on tech-pkg@ and no negative feedbacks
    <https://mail-index.NetBSD.org/tech-pkg/2026/03/04/msg032042.html>
    (it took a bit more than a week to import them though, sorry!)

---
+++ net/opentofu16/patches/patch-provider__source.go
@@ -0,0 +1,16 @@
+$NetBSD: patch-provider__source.go,v 1.1 2026/08/27 20:46:35 leot Exp $
+
+Honors providers installed via pkgsrc.
+
+--- cmd/tofu/provider_source.go.orig	2021-03-24 18:39:36.000000000 +0000
++++ cmd/tofu/provider_source.go
+@@ -160,6 +160,9 @@ func implicitProviderSource(services *di
+ 		addLocalDir(dir)
+ 	}
+ 
++	// Providers installed via pkgsrc
++	addLocalDir(filepath.Join("@PREFIX@", "share/terraform/plugins"))
++
+ 	// Anything we found in local directories above is excluded from being
+ 	// looked up via the registry source we're about to construct.
+ 	var directExcluded getproviders.MultiSourceMatchingPatterns
