c9087d0e17785378e88ac3106b1779f858487fa4
adam <adam@pkgsrc.org>
2026-08-28 07:54:20 +0000

    qemu: updated to 11.1.1
    11.1.1
    Bug fixes

--- emulators/qemu/version.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: version.mk,v 1.18 2026/08/27 07:27:38 adam Exp $
-#
-# used by emulators/qemu
-# used by sysutils/qemu-guest-agent
-
-QEMU_VERSION?=	11.1.0
-
-CHECK_PORTABILITY_SKIP+=	scripts/gitlab-map-update
