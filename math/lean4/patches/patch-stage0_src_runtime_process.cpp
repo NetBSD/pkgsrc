9aeb5d44bed1883a06af2b6120273cca93865d8d
wiz <wiz@pkgsrc.org>
2026-08-28 18:35:12 +0000

    lean4: prepare environment before fork
    This was done between fork + exec. This reduces lake hangs due to
    malloc state inherited by the child.
    
    Bump PKGREVISION.

--- math/lean4/patches/patch-stage0_src_runtime_process.cpp
+++ math/lean4/patches/patch-stage0_src_runtime_process.cpp
@@ -1,12 +1,25 @@
-$NetBSD: patch-stage0_src_runtime_process.cpp,v 1.2 2026/07/24 21:26:20 wiz Exp $
+$NetBSD: patch-stage0_src_runtime_process.cpp,v 1.3 2026/08/28 18:35:12 wiz Exp $
 
 Set proper thread id on NetBSD.
 clearenv() does not exist on NetBSD.
 https://github.com/leanprover/lean4/pull/14543
 
---- stage0/src/runtime/process.cpp.orig	2025-07-31 11:25:32.645020915 +0000
+Create environment before forking
+https://github.com/leanprover/lean4/issues/14587
+
+--- stage0/src/runtime/process.cpp.orig	2026-08-15 19:18:21.000000000 +0000
 +++ stage0/src/runtime/process.cpp
-@@ -340,6 +340,8 @@ extern "C" LEAN_EXPORT obj_res lean_io_g
+@@ -8,7 +8,9 @@ Author: Jared Roesch
+ #include <fstream>
+ #include <iostream>
+ #include <iomanip>
++#include <map>
+ #include <utility>
++#include <vector>
+ #include <system_error>
+ 
+ #if defined(LEAN_WINDOWS)
+@@ -341,6 +343,8 @@ extern "C" LEAN_EXPORT uint64_t lean_io_get_tid() {
      uint64_t tid;
  #ifdef __APPLE__
      lean_always_assert(pthread_threadid_np(NULL, &tid) == 0);
@@ -15,7 +28,7 @@ https://github.com/leanprover/lean4/pull/14543
  #elif defined(LEAN_EMSCRIPTEN)
      tid = 0;
  #else
-@@ -430,7 +432,7 @@ static optional<pipe> setup_stdio(stdio 
+@@ -431,10 +435,52 @@ static optional<pipe> setup_stdio(stdio cfg) {
      lean_unreachable();
  }
  
@@ -24,12 +37,96 @@ https://github.com/leanprover/lean4/pull/14543
  extern "C" char **environ;
  #endif
  
-@@ -446,7 +448,7 @@ static obj_res spawn(string_ref const & 
++static std::vector<char*> build_environ(bool inherit_env, array_ref<pair_ref<string_ref, option_ref<string_ref>>> const & env) {
++    std::map<std::string, std::string> current;
++    if (inherit_env) {
++	/* Build a lookup map from current environ */
++	for (char** env = environ; *env; ++env) {
++	    auto sep = std::strstr(*env, "=");
++	    if (sep) {
++		std::string key(*env, sep - *env);
++		current[std::move(key)] = sep + 1;
++	    }
++	}
++    }
++
++    /* Apply overrides */
++    for (auto & entry : env) {
++	if (entry.snd()) {
++	    current[entry.fst().data()] = entry.snd().get()->data();
++	} else {
++	    current.erase(entry.fst().data());
++	}
++    }
++
++    /* Create the new environ array */
++    std::vector<char*> result;
++    result.reserve(current.size() + 1);
++
++    for (auto & [key, val] : current) {
++        size_t key_len = key.size();
++        size_t val_len = val.size();
++        size_t total = key_len + 1 + val_len + 1;
++        char* entry = new char[total];
++        std::memcpy(entry, key.data(), key_len);
++        entry[key_len] = '=';
++        std::memcpy(entry + key_len + 1, val.data(), val_len);
++        entry[key_len + 1 + val_len] = '\0';
++        result.push_back(entry);
++    }
++
++    result.push_back(nullptr);
++    return result;
++}
++
+ static obj_res spawn(string_ref const & proc_name, array_ref<string_ref> const & args, stdio stdin_mode, stdio stdout_mode,
+   stdio stderr_mode, option_ref<string_ref> const & cwd, array_ref<pair_ref<string_ref, option_ref<string_ref>>> const & env,
+   bool inherit_env, bool do_setsid) {
+@@ -450,24 +496,11 @@ static obj_res spawn(string_ref const & proc_name, arr
+         pargs.push_back(strdup(arg.data()));
+     pargs.push_back(NULL);
+ 
++    std::vector<char*> new_env =  build_environ(inherit_env, env);
++
+     int pid = fork();
  
      if (pid == 0) {
-         if (!inherit_env) {
+-        if (!inherit_env) {
 -#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__NetBSD__)
-             environ = NULL;
- #else
-             clearenv();
+-            environ = NULL;
+-#else
+-            clearenv();
+-#endif
+-        }
+-        for (auto & entry : env) {
+-            if (entry.snd()) {
+-                setenv(entry.fst().data(), entry.snd().get()->data(), true);
+-            } else {
+-                unsetenv(entry.fst().data());
+-            }
+-        }
+-
+         if (stdin_pipe) {
+             dup2(stdin_pipe->m_read_fd, STDIN_FILENO);
+             close(stdin_pipe->m_write_fd);
+@@ -507,7 +540,7 @@ static obj_res spawn(string_ref const & proc_name, arr
+             lean_always_assert(setsid() >= 0);
+         }
+ 
+-        if (execvp(pargs[0], pargs.data()) < 0) {
++        if (execvpe(pargs[0], pargs.data(), new_env.data()) < 0) {
+             std::cerr << "could not execute external process '" << pargs[0] << "'" << std::endl;
+             // See the comment above about `_exit` vs `exit`.
+             _exit(-1);
+@@ -520,6 +553,11 @@ static obj_res spawn(string_ref const & proc_name, arr
+         if (parg != NULL) {
+             free(parg);
+         }
++    }
++    for (char* entry : new_env) {
++        if (entry != NULL) {
++	    free(entry);
++	}
+     }
+ 
+     object * parent_stdin  = box(0);
