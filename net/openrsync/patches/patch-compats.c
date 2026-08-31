994851eda1da819ea9c0ad554457b1258d431d54
vins <vins@pkgsrc.org>
2026-08-31 14:02:04 +0000

    net/openrsync: update to openrsync-0.20260730
    Sync with upstream, pulling another year of commits.
    Noteworthy changes:
    * Many portability fixes (with specific regard to NetBSD)
    * Various bug fixes.
    * Several improvents merged from Apple.
    Refer to the github history for more details.

--- net/openrsync/patches/patch-compats.c
+++ net/openrsync/patches/patch-compats.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-compats.c,v 1.1 2025/11/30 11:46:38 vins Exp $
+$NetBSD: patch-compats.c,v 1.2 2026/08/31 14:02:05 vins Exp $
 
 Prevent incompatble pointer types for memset. 
 
---- compats.c.orig	2025-01-27 04:36:41.000000000 +0000
+--- compats.c.orig	2026-07-30 04:55:04.000000000 +0000
 +++ compats.c
 @@ -2145,7 +2145,7 @@ explicit_bzero(void *p, size_t n)
   * Indirect memset through a volatile pointer to hopefully avoid
   * dead-store optimisation eliminating the call.
   */
--static void (* volatile ssh_memset)(void *, int, size_t) = memset;
+-static void * (* volatile ssh_memset)(void *, int, size_t) = memset;
 +static void * (* ssh_memset)(void *, int, size_t) = memset;
  
  void
--- net/openrsync/patches/patch-extern.h
+++ net/openrsync/patches/patch-extern.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-extern.h,v 1.3 2025/11/30 11:46:38 vins Exp $
+$NetBSD: patch-extern.h,v 1.4 2026/08/31 14:02:05 vins Exp $
 
-Add missing include.
+Add missing header for uint*_t types.
 
---- extern.h.orig	2025-11-30 12:37:24.789764856 +0000
+--- extern.h.orig	2026-08-31 04:45:33.623155744 +0000
 +++ extern.h
-@@ -17,6 +17,7 @@
- #ifndef EXTERN_H
- #define EXTERN_H
- 
-+#include <stdint.h>
- #include "md4.h"
+@@ -21,6 +21,7 @@
+ #include <fts.h> /* FTSENT */
+ #include <stdbool.h>
+ #include <stdio.h> /* FILE */
++#include <stdint.h> /* uint8_t */
+ #include <time.h> /* struct timespec, gettimeofday(), etc. */
  
  #if !HAVE_PLEDGE
