5cefeaca53f58b68faabb484f31e1bdd6dfb0a98
ktnb <ktnb@pkgsrc.org>
2026-09-01 19:29:51 +0000

    gumbo-parser: update to 0.14.0
    * Add gumbo_tag_is_void() public API function
    * Add support for <selectedcontent> tag
    * Add support for processing instructions
    * Comply with spec changes to <select> parsing logic
    * Fix compliance to adoption agency algorithm, step 4.3
    * Fix foster parented text not being merged into preceding text node
    * Fix memory leak of ignored doctype token in "in head noscript" insertion mode
    * Delete dependency on Ragel
    * Adapt to migration of tree construction tests suite from
      html5lib-tests to WPT

--- www/gumbo-parser/buildlink3.mk
+++ www/gumbo-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2025/01/05 22:37:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2026/09/01 19:29:51 ktnb Exp $
 
 BUILDLINK_TREE+=	gumbo-parser
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gumbo-parser
 GUMBO_PARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gumbo-parser+=	gumbo-parser>=0.11.0
-BUILDLINK_ABI_DEPENDS.gumbo-parser?=	gumbo-parser>=0.13.0
+BUILDLINK_ABI_DEPENDS.gumbo-parser?=	gumbo-parser>=0.14.0
 BUILDLINK_PKGSRCDIR.gumbo-parser?=	../../www/gumbo-parser
 .endif	# GUMBO_PARSER_BUILDLINK3_MK
 
