1d45056d4b7c33f53954d24b5d2c259f04b63f6f
fox <fox@pkgsrc.org>
2026-09-01 19:16:33 +0000

    lang/gjs: Update to 1.82.3
    Switch to mozjs128. Mark binaries not PaX mprotect safe (SpiderMonkey
    JIT). Disable PCH. Remove obsolete patch.
    
    Changes since 1.68.4:
    
    Too many changes to list; see the upstream NEWS file for the full
    release notes covering all versions between 1.68.4 and 1.82.3:
    
    https://gitlab.gnome.org/GNOME/gjs/-/blob/1.82.3/NEWS

--- lang/gjs/buildlink3.mk
+++ lang/gjs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2026/01/27 08:40:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2026/09/01 19:16:33 fox Exp $
 
 BUILDLINK_TREE+=	gjs
 
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	gjs
 GJS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gjs+=	gjs>=1.53.4
-BUILDLINK_ABI_DEPENDS.gjs?=	gjs>=1.68.4nb27
+BUILDLINK_ABI_DEPENDS.gjs?=	gjs>=1.82.3
 BUILDLINK_PKGSRCDIR.gjs?=	../../lang/gjs
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
-.include "../../lang/mozjs78/buildlink3.mk"
+.include "../../lang/mozjs128/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .endif	# GJS_BUILDLINK3_MK
 
