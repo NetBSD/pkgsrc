c210499dc030212a2c274659942efdff2397a3af
adam <adam@pkgsrc.org>
2026-09-02 14:11:41 +0000

    spirv-llvm-translator: updated to 21.1.11
    21.1.11
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/compare/v21.1.10...v21.1.11

--- parallel/spirv-llvm-translator/buildlink3.mk
+++ parallel/spirv-llvm-translator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2025/05/15 11:52:15 rjs Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2026/09/02 14:11:41 adam Exp $
 
 BUILDLINK_TREE+=	spirv-llvm-translator
 
@@ -8,7 +8,7 @@ SPIRV_LLVM_TRANSLATOR_BUILDLINK3_MK:=
 GCC_REQD+=	7 # sync with llvm
 
 BUILDLINK_API_DEPENDS.spirv-llvm-translator+=	spirv-llvm-translator>=14.0.0
-BUILDLINK_ABI_DEPENDS.spirv-llvm-translator?=	spirv-llvm-translator>=19.1.7nb1
+BUILDLINK_ABI_DEPENDS.spirv-llvm-translator+=	spirv-llvm-translator>=19.1.7nb1
 BUILDLINK_PKGSRCDIR.spirv-llvm-translator?=	../../parallel/spirv-llvm-translator
 
 .include "../../lang/llvm/buildlink3.mk"
