dfd3dc7399698624703902bfd7cbbd9c1ed309e7
drixter <drixter@pkgsrc.org>
2026-09-04 13:19:13 +0000

    resterm: Update to 1.5.6
    Argument order
    Init directory
    Mock scope in the header

--- www/resterm/go-modules.mk
+++ www/resterm/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.30 2026/09/02 13:49:47 drixter Exp $
+# $NetBSD: go-modules.mk,v 1.31 2026/09/04 13:19:13 drixter Exp $
 
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.zip
