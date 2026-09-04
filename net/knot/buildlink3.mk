68cfc7755535493038a83be3ae76d0ed4ddae49f
drixter <drixter@pkgsrc.org>
2026-09-04 18:25:08 +0000

    knot: Update to 3.5.8
    Knot DNS 3.5.8 (2026-09-04)
    
    Improvements:
    knotc: better compatibility with SmartOS #979
    knotd: added a check for possible errors while reading a backup label file
    knotd: more robust parsing of data stored in LMDB (Thanks to Jim Alves-Foss)
    
    Bugfixes:
    knotd: unauthenticated TSIG chosen-prefix signing that enables DDNS forgery (Thanks to Gia Bui)
    knotd: server crash due to a missing check for malformed TCP packet in XDP mode (Thanks to Kushal Das)
    kzonecheck: defective zone origin detection if the file name is too short (Thanks to Kushal Das)

--- net/knot/buildlink3.mk
+++ net/knot/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2026/08/18 19:01:10 drixter Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/09/04 18:25:08 drixter Exp $
 
 BUILDLINK_TREE+=	knot
 
 .if !defined(KNOT_BUILDLINK3_MK)
 KNOT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.knot+=	knot>=3.5.7
+BUILDLINK_API_DEPENDS.knot+=	knot>=3.5.8
 BUILDLINK_PKGSRCDIR.knot?=	../../net/knot
 
 .endif
