5419188d3e64b4245b4213a27284389081bca375
drixter <drixter@pkgsrc.org>
2026-09-08 14:31:07 +0000

    resterm: Update to 1.7.0
    Editor diagnostics
    Diagnostic settings
    Variable and expression errors
    Unicode text
    Response split shortcuts
    Other improvements

--- www/resterm/go-modules.mk
+++ www/resterm/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.32 2026/09/06 19:23:26 drixter Exp $
+# $NetBSD: go-modules.mk,v 1.33 2026/09/08 14:31:07 drixter Exp $
 
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.zip
