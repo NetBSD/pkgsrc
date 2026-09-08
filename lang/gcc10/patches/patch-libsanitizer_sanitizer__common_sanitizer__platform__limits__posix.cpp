fa04079763bd16fb01ff1232bfebc8e6bc0d9753
sjmulder <sjmulder@pkgsrc.org>
2026-09-08 13:20:35 +0000

    lang/gcc10: account for linux/scc.h removal
    Header removed in Linux 7.1:
    
    https://github.com/torvalds/linux/commit/64edfa65062dc4509ba75978116b2f6d392346f5

---
+++ lang/gcc10/patches/patch-libsanitizer_sanitizer__common_sanitizer__platform__limits__posix.cpp
@@ -0,0 +1,23 @@
+$NetBSD: patch-libsanitizer_sanitizer__common_sanitizer__platform__limits__posix.cpp,v 1.1 2026/09/08 13:20:35 sjmulder Exp $
+
+Account for linux/scc.h removal (Linux 7.1)
+
+--- libsanitizer/sanitizer_common/sanitizer_platform_limits_posix.cpp.orig	2026-09-07 16:20:54.733454515 +0000
++++ libsanitizer/sanitizer_common/sanitizer_platform_limits_posix.cpp
+@@ -137,7 +137,6 @@ typedef struct user_fpregs elf_fpregset_
+ #include <linux/lp.h>
+ #include <linux/mroute.h>
+ #include <linux/mroute6.h>
+-#include <linux/scc.h>
+ #include <linux/serial.h>
+ #include <sys/msg.h>
+ #include <sys/ipc.h>
+@@ -456,8 +455,6 @@ unsigned struct_ElfW_Phdr_sz = sizeof(El
+   unsigned struct_kbsentry_sz = sizeof(struct kbsentry);
+   unsigned struct_mtconfiginfo_sz = sizeof(struct mtconfiginfo);
+   unsigned struct_nr_parms_struct_sz = sizeof(struct nr_parms_struct);
+-  unsigned struct_scc_modem_sz = sizeof(struct scc_modem);
+-  unsigned struct_scc_stat_sz = sizeof(struct scc_stat);
+   unsigned struct_serial_multiport_struct_sz
+       = sizeof(struct serial_multiport_struct);
+   unsigned struct_serial_struct_sz = sizeof(struct serial_struct);
