a1d08c56e1391fc7b96c66ee620365cf21bdf240
drixter <drixter@pkgsrc.org>
2026-09-09 06:10:29 +0000

    resterm: Update to 1.7.2
    Smaller binary
    This release makes the Resterm binary smaller by removing unused code.

--- www/resterm/go-modules.mk
+++ www/resterm/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.34 2026/09/08 20:49:42 drixter Exp $
+# $NetBSD: go-modules.mk,v 1.35 2026/09/09 06:10:29 drixter Exp $
 
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.zip
