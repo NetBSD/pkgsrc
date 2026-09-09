335eefadc55d680d7e642b3deb8dd882f9f0dd65
jperkin <jperkin@pkgsrc.org>
2026-09-09 11:58:12 +0000

    libiscsi: Extend patch to support illumos too.

--- net/libiscsi/patches/patch-lib_multithreading.c
+++ net/libiscsi/patches/patch-lib_multithreading.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-lib_multithreading.c,v 1.1 2026/08/07 08:14:45 adam Exp $
+$NetBSD: patch-lib_multithreading.c,v 1.2 2026/09/09 11:58:13 jperkin Exp $
 
 Add thread id retrieval for FreeBSD/DragonFly, OpenBSD and NetBSD.
 https://github.com/sahlberg/libiscsi/pull/466
 
 --- lib/multithreading.c.orig	2025-07-15 09:27:12.000000000 +0000
 +++ lib/multithreading.c
-@@ -144,6 +144,12 @@ int iscsi_mt_sem_wait(libiscsi_sem_t* se
+@@ -144,6 +144,15 @@ int iscsi_mt_sem_wait(libiscsi_sem_t* se
  #include <signal.h>
  #include <unistd.h>
  #include <sys/syscall.h>
@@ -15,10 +15,13 @@ https://github.com/sahlberg/libiscsi/pull/466
 +#if defined(__NetBSD__)
 +#include <lwp.h>
 +#endif
++#if defined(__illumos__)
++#include <thread.h>
++#endif
  
  iscsi_tid_t iscsi_mt_get_tid(void)
  {
-@@ -151,6 +157,15 @@ iscsi_tid_t iscsi_mt_get_tid(void)
+@@ -151,6 +160,18 @@ iscsi_tid_t iscsi_mt_get_tid(void)
          iscsi_tid_t tid;
          pthread_threadid_np(NULL, &tid);
          return tid;
@@ -31,6 +34,9 @@ https://github.com/sahlberg/libiscsi/pull/466
 +#elif defined(__NetBSD__)
 +        lwpid_t tid = _lwp_self();
 +        return tid;
++#elif defined(__illumos__)
++	thread_t tid = thr_self();
++	return tid;
  #elif defined(SYS_gettid)
          pid_t tid = syscall(SYS_gettid);
          return tid;
