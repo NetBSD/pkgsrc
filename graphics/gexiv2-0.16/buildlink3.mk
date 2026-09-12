ebe2a6e59e95134e7d2f2fedba4510ecb0d863ad
wiz <wiz@pkgsrc.org>
2026-09-12 12:47:27 +0000

    gexiv2-0.16: change PKGNAME to avoid conflict with gexiv2 0.14
    They have no common names in the PLISTs

--- graphics/gexiv2-0.16/buildlink3.mk
+++ graphics/gexiv2-0.16/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2026/09/02 19:02:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2026/09/12 12:47:27 wiz Exp $
 
-BUILDLINK_TREE+=	gexiv2
+BUILDLINK_TREE+=	gexiv2_0.16
 
-.if !defined(GEXIV2_BUILDLINK3_MK)
-GEXIV2_BUILDLINK3_MK:=
+.if !defined(GEXIV2_0.16_BUILDLINK3_MK)
+GEXIV2_0.16_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gexiv2+=	gexiv2>=0.16.0
-BUILDLINK_ABI_DEPENDS.gexiv2?=	gexiv2>=0.16.0nb1
-BUILDLINK_PKGSRCDIR.gexiv2?=	../../graphics/gexiv2-0.16
+BUILDLINK_API_DEPENDS.gexiv2_0.16+=	gexiv2_0.16>=0.16.0nb1
+BUILDLINK_PKGSRCDIR.gexiv2_0.16?=	../../graphics/gexiv2-0.16
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/exiv2/buildlink3.mk"
-.endif	# GEXIV2_BUILDLINK3_MK
+.endif	# GEXIV2_0.16_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-gexiv2
+BUILDLINK_TREE+=	-gexiv2_0.16
