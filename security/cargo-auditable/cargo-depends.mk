89fab08239323df1f66be84d3dcf9a0b3a8f1489
pin <pin@pkgsrc.org>
2026-09-14 14:15:36 +0000

    security/cargo-auditable: update to 0.7.6
    (leaf package)
    No changelog provided.

--- security/cargo-auditable/cargo-depends.mk
+++ security/cargo-auditable/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.2 2026/06/03 20:09:26 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.3 2026/09/14 14:15:36 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.4
@@ -35,7 +35,7 @@ CARGO_CRATE_DEPENDS+=	leb128-0.2.5
 CARGO_CRATE_DEPENDS+=	libc-0.2.177
 CARGO_CRATE_DEPENDS+=	linux-raw-sys-0.11.0
 CARGO_CRATE_DEPENDS+=	memchr-2.7.6
-CARGO_CRATE_DEPENDS+=	miniz_oxide-0.8.9
+CARGO_CRATE_DEPENDS+=	miniz_oxide-0.9.1
 CARGO_CRATE_DEPENDS+=	num-conv-0.2.0
 CARGO_CRATE_DEPENDS+=	num-traits-0.2.19
 CARGO_CRATE_DEPENDS+=	object-0.37.3
