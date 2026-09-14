ef9df015ad7c47c653b21f50f3100b629129863e
wiz <wiz@pkgsrc.org>
2026-09-14 22:11:00 +0000

    socket_wrapper: bl3.mk: fix multiple-inclusion protection

--- devel/socket_wrapper/buildlink3.mk
+++ devel/socket_wrapper/buildlink3.mk
@@ -1,8 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.1 2025/09/06 18:29:02 riastradh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/09/14 22:11:00 wiz Exp $
 
 BUILDLINK_TREE+=	socket_wrapper
 
 .if !defined(SOCKET_WRAPPER_BUILDLINK3_MK)
+SOCKET_WRAPPER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.socket_wrapper+=	socket_wrapper>=1.5.0
 BUILDLINK_ABI_DEPENDS.socket_wrapper+=	socket_wrapper>=1.5.0
