ca897dc9e8d8069bc9c3779efbcde671c75dae5c
wiz <wiz@pkgsrc.org>
2026-09-15 06:29:15 +0000

    libopus: fix PLIST for doc option
    depend on the appropriate doxygen and bump PKGREVISION
    
    From Robert Whitlock in PR 60730.

--- audio/libopus/options.mk
+++ audio/libopus/options.mk
@@ -1,14 +1,15 @@
-# $NetBSD: options.mk,v 1.5 2025/06/19 06:08:35 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2026/09/15 06:29:15 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libopus
 PKG_SUPPORTED_OPTIONS=	doc
+PKG_SUGGESTED_OPTIONS=	#
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	doc
 
 .if !empty(PKG_OPTIONS:Mdoc)
-TOOL_DEPENDS+=		doxygen>=1.14:../../devel/doxygen
+TOOL_DEPENDS+=		doxygen>=1.18:../../devel/doxygen
 PLIST.doc=		yes
 PRINT_PLIST_AWK+=	{ if (/^share\/doc/) $$0 = "$${PLIST.doc}" $$0 }
 PRINT_PLIST_AWK+=	{ if (/^man/) $$0 = "$${PLIST.doc}" $$0 }
