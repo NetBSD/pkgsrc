1671dc6184858fefba0d0d61c431914353b36f87
pin <pin@pkgsrc.org>
2026-09-22 14:50:31 +0000

    misc/rozi: update to 0.0.23
    Added
    
        Refreshes the system theme from the host palette.
    
    Changed
    
        Retires CHANGELOG.md and migrates historical notes to GitHub Releases.
    
    Fixed
    
        Removes the delay when opening extension pickers.

--- misc/rozi/cargo-depends.mk
+++ misc/rozi/cargo-depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cargo-depends.mk,v 1.3 2026/09/19 15:39:07 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.4 2026/09/22 14:50:31 pin Exp $
 
 CARGO_CRATE_DEPENDS+=	adler2-2.0.1
 CARGO_CRATE_DEPENDS+=	aho-corasick-1.1.5
@@ -375,8 +375,8 @@ CARGO_CRATE_DEPENDS+=	toml_parser-1.1.3+spec-1.1.0
 CARGO_CRATE_DEPENDS+=	toml_writer-1.1.2+spec-1.1.0
 CARGO_CRATE_DEPENDS+=	tree_magic_mini-3.2.2
 CARGO_CRATE_DEPENDS+=	ttf-parser-0.25.1
-CARGO_CRATE_DEPENDS+=	tui-lipan-0.11.6
-CARGO_CRATE_DEPENDS+=	tui-lipan-macro-0.11.6
+CARGO_CRATE_DEPENDS+=	tui-lipan-0.11.7
+CARGO_CRATE_DEPENDS+=	tui-lipan-macro-0.11.7
 CARGO_CRATE_DEPENDS+=	typed-path-0.12.3
 CARGO_CRATE_DEPENDS+=	typenum-1.20.1
 CARGO_CRATE_DEPENDS+=	ucd-trie-0.1.7
