e0b9f504129120b3a6dc579bfa2612b36a74f409
kikadf <kikadf@pkgsrc.org>
2026-09-22 14:28:56 +0000

    megacmd: update to 2.6.0
    Wed Aug 19 10:27:09 AM UTC 2026 - linux@mega.co.nz
    - Update to version 2.6.0:
      * webdav: streaming now keeps what it already fetched on disk, so seeking back or replaying a part you have watched is instant and is not downloaded again
      * webdav: that cache is cleaned up on its own, and the file_service_reclaim_* settings of "configure" let you bound it. See "help --streaming"
      * The http_proxy and https_proxy environment variables are now used on Linux and macOS when no proxy is set
      * File transfers now always use HTTPS: "https on" does nothing and "https off" is rejected
      * Faster path lookups in folders holding many thousands of files
      * webdav: less memory used while serving a file
      * transfers: downloads keep their order across restarts
      * transfers: fixed a crash listing completed transfers while the buffer was being trimmed
      * Hardened handling of malformed keys and share keys
      * sync: a rename that only changes capitalisation is now applied on Windows and macOS
      * webdav: better handling of names with special characters in folder listings
      * mv: fixed a file in a folder shared with you being deleted when moved into the folder it was already in
      * Stability, memory, typos and other improvements, including in the Windows updater
    
    - Use builtin megasdk-10.19.0

--- net/megacmd/patches/patch-sdk_cmake_modules_sdklib__libraries.cmake
+++ net/megacmd/patches/patch-sdk_cmake_modules_sdklib__libraries.cmake
@@ -1,7 +1,6 @@
-$NetBSD: patch-sdk_cmake_modules_sdklib__libraries.cmake,v 1.3 2026/03/27 11:06:04 kikadf Exp $
+$NetBSD: patch-sdk_cmake_modules_sdklib__libraries.cmake,v 1.4 2026/09/22 14:28:56 kikadf Exp $
 
 * Fix to find libcrypto++
-* Fix missing icu link library
 
 --- sdk/cmake/modules/sdklib_libraries.cmake.orig	2026-02-18 08:41:33.000000000 +0000
 +++ sdk/cmake/modules/sdklib_libraries.cmake
@@ -14,14 +13,3 @@ $NetBSD: patch-sdk_cmake_modules_sdklib__libraries.cmake,v 1.3 2026/03/27 11:06:
          target_link_libraries(SDKlib PUBLIC PkgConfig::cryptopp) # TODO: Private for SDK core
  
          pkg_check_modules(sodium REQUIRED IMPORTED_TARGET libsodium)
-@@ -110,8 +110,8 @@ macro(load_sdklib_libraries)
-         pkg_check_modules(curl REQUIRED IMPORTED_TARGET libcurl)
-         target_link_libraries(SDKlib PRIVATE PkgConfig::curl)
- 
--        find_package(ICU COMPONENTS uc data REQUIRED)
--        target_link_libraries(SDKlib PRIVATE ICU::uc ICU::data)
-+        find_package(ICU COMPONENTS i18n uc data REQUIRED)
-+        target_link_libraries(SDKlib PRIVATE ICU::i18n ICU::uc ICU::data)
- 
-         if(USE_OPENSSL)
-             find_package(OpenSSL REQUIRED)
--- net/megacmd/patches/patch-sdk_include_mega_localpath.h
+++ net/megacmd/patches/patch-sdk_include_mega_localpath.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-sdk_include_mega_localpath.h,v 1.1 2026/02/11 17:04:10 kikadf Exp $
+$NetBSD: patch-sdk_include_mega_localpath.h,v 1.2 2026/09/22 14:28:56 kikadf Exp $
 
 * Add filesystem support to BSDs
 
 --- sdk/include/mega/localpath.h.orig	2026-02-08 19:23:32.037218658 +0100
 +++ sdk/include/mega/localpath.h
 @@ -33,6 +33,10 @@ enum FileSystemType
-     FS_SMB = 12,
      FS_SMB2 = 13,
      FS_LIFS = 14,
+     FS_LAST = FS_LIFS,
 +    FS_FFS = 15,
 +    FS_HAMMER = 16,
 +    FS_UFS = 17,
