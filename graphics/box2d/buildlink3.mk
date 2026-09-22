253c3902c2e4bc1bfd44deb02509954e40561135
ryoon <ryoon@pkgsrc.org>
2026-09-22 07:55:31 +0000

    graphics/box2d: Update to 3.1.1
    Changelog:
    3.1.1:
    What's Changed
    
        Warning as error off by default by @erincatto in #914
        functions to get and set surface material by @erincatto in #921
        Fixed another shape distance assert by @erincatto in #926
        Revolute and prismatic targets by @erincatto in #929
        Cast initial overlap by @erincatto in #933
        Tunable joint stiffness by @erincatto in #934
        _aligned_free for mingw by @erincatto in #939
        Removed MSVC static library setting by @erincatto in #940
    
    
    # v3.1 Release Notes
    
    ## API Changes
    - 64-bit filter categories and masks
    - 64-bit dynamic tree user data
    - Renamed `b2SmoothSegment` to `b2ChainSegment`
    - Cast and overlap functions modified for argument consistency
    - Contact begin events now provide the manifold
    - More consistent functions to make polygons
    - Contact events are now disabled by default
    - Replaced `b2Timer` with `uint64_t`
    - Shape material properties now use `b2SurfaceMaterial`
    
    ## New Features
    - New character mover features and sample
    - Revised sensor system is now independent of body type and sleep
    - Rolling resistance and tangent speed
    - Friction and restitution mixing callbacks
    - World explosions
    - World access to the maximum linear speed
    - More control over body mass updates
    - Filter joint to disable collision between specific bodies
    - Bodies can now have names for debugging
    - Added `b2Body_SetTargetTransform` for kinematic bodies
    
    ## Improvements
    - Cross-platform determinism
    - Custom SSE2 and Neon for significantly improved performance
    - SSE2 is the default instead of AVX2
    - Removed SIMDE library dependency
    - Faster ray and shape casts
    - Faster continuous collision
    - Each segment of a chain shape may have a different surface material
    - Reduced overhead of restitution when not used
    - Implemented atomic platform wrappers eliminating the `experimental:c11atomics` flag
    
    ## Bugs Fixes
    - Many bug fixes based on user testing
    - Fixed missing hit events
    - Capsule and polygon manifold fixes
    - Fixed missing contact end events
    - PreSolve is now called in continuous collision
    - Reduced clipping into chain shapes by fast bodies
    - Friction and restitution are now remixed in the contact solver every time step
    - Body move events are now correctly adjusted for time of impact
    
    ## Infrastructure
    - Unit test for API coverage
    - macOS and Windows samples built in GitHub actions
    - CMake install
    - imgui and glfw versions are now pinned in FetchContent
    - Initial Emscripten support

--- graphics/box2d/buildlink3.mk
+++ graphics/box2d/buildlink3.mk
@@ -1,12 +1,27 @@
-# $NetBSD: buildlink3.mk,v 1.1 2021/02/06 06:41:33 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2026/09/22 07:55:31 ryoon Exp $
 
 BUILDLINK_TREE+=	box2d
 
 .if !defined(BOX2D_BUILDLINK3_MK)
 BOX2D_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.box2d+=	box2d>=2.4.1
+BUILDLINK_API_DEPENDS.box2d+=	box2d>=3.1.1
 BUILDLINK_PKGSRCDIR.box2d?=	../../graphics/box2d
+
+BUILDLINK_TARGETS+=	box2d-fake-pc
+
+.PHONY: box2d-fake-pc
+box2d-fake-pc:
+	${RUN} \
+	${MKDIR} ${BUILDLINK_DIR}/lib/pkgconfig; \
+	{ ${ECHO} "Name: box2d"; \
+	${ECHO} "Description: 2D physics engine for games"; \
+	${ECHO} "Version: 3.1.1"; \
+	${ECHO} "Cflags: -I${BUILDLINK_PREFIX.box2d}/include"; \
+	${ECHO} "Libs: -L${BUILDLINK_PREFIX.box2d}/lib" \
+	"${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.box2d}/lib" \
+	"-lbox2d"; \
+	} > ${BUILDLINK_DIR}/lib/pkgconfig/box2d.pc; \
 
 .endif	# BOX2D_BUILDLINK3_MK
 
