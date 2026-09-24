b8c446f60506c95b66a212bf2150c5ee689160ed
taca <taca@pkgsrc.org>
2026-09-24 15:32:45 +0000

    lang/php83: update to 8.3.35
    PHP 8.3.34 (seems to be skipped)
    
    PHP 8.3.35 (2026-09-24)
    
    - Filter:
      . Fixed GHSA-ch8v-r6jh-4vvr (FILTER_SANITIZE_ENCODED does not encode 0xFF).
        (Ilia Alshanetsky)
    
    - FPM:
      . Fixed GHSA-62xp-839h-2637 (IPv6 ACL bypass in FastCGI listen.allowed_clients
        due to partial address comparison). (CVE-2026-91768) (Alexandre Daubois)
    
    - MySQLnd:
      . Fixed GHSA-r6x9-5r99-36j7 (Various packet overreads in mysqlnd wire
        protocol). (CVE-2025-1218) (Jakub Zelenka, Nora Dossche)
    
    - OpenSSL:
      . Fixed GHSA-vvx9-73fr-5jjx (TLS hostname verification falls back to CN after
        SAN mismatch). (CVE-2026-91769) (Jakub Zelenka)
      . Fixed GHSA-xr7j-rvgx-xq5p (Heap buffer overflow in
        php_openssl_matches_wildcard_name() on crafted server certificate wildcard
        CN). (CVE-2026-91767) (Jakub Zelenka)
    
    - Phar:
      . Fixed GHSA-j3wh-g957-2m85 (Integer overflow in phar_tar_number() allowing
        TAR archive entry injection). (CVE-2026-6103) (Jakub Zelenka)
    
    - SOAP:
      . Fixed GHSA-rgrp-mwpx-f6rm (Unbounded recursion in server-side
        cleanup_xml_node()). (CVE-2026-91765) (Alexandre Daubois)
      . Fixed GHSA-cj93-vc83-wgqv (Integer overflow to buffer overflow in SOAP HTTP
        parsing). (CVE-2025-14181) (Nora Dossche, Jakub Zelenka)
    
    - Standard:
      . Fixed GHSA-88hq-2827-7pg6 (Out-of-bounds read in convert.* stream filters
        when line-break-chars contains NUL). (CVE-2026-92842) (geeknik)
      . Fixed GHSA-fpwc-w8rq-cr92 (Cross-origin credential leak in HTTP stream
        wrapper redirects). (CVE-2026-91766) (Alexandre Daubois, Jakub Zelenka)
      . Fixed GHSA-7875-c8px-7q5f (Out-of-bounds read in the HTTP stream wrapper
        when following a redirect with an empty Location header). (CVE-2026-93682)
        (Ilia Alshanetsky, Jordi Kroon)
    
    - Windows:
      . Fixed GHSA-9f67-6fw4-hpfp (Reserved device names are not rejected before
        file and stream I/O). (CVE-2026-17545) (Shivam Mathur, Jakub Zelenka)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.513 2026/08/28 15:19:43 taca Exp $
+# $NetBSD: phpversion.mk,v 1.514 2026/09/24 15:32:45 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -111,7 +111,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP74_VERSION=	7.4.33
 PHP82_VERSION=	8.2.33
-PHP83_VERSION=	8.3.33
+PHP83_VERSION=	8.3.35
 PHP84_VERSION=	8.4.25
 PHP85_VERSION=	8.5.10
 
