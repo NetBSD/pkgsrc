61d4862a9a25b9ff4ab85c310b1f73c3013c07f2
drixter <drixter@pkgsrc.org>
2026-09-25 07:20:52 +0000

    resterm: Update to 1.9.0
    Run variables
    Live Profile tab
    Compare with the previous run
    Profile history

--- www/resterm/go-modules.mk
+++ www/resterm/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.37 2026/09/18 20:02:32 drixter Exp $
+# $NetBSD: go-modules.mk,v 1.38 2026/09/25 07:20:52 drixter Exp $
 
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.zip
