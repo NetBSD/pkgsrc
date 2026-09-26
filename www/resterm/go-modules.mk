e53fcf891349764a1ea187432df9f6233c9cd586
drixter <drixter@pkgsrc.org>
2026-09-26 12:47:39 +0000

    resterm: Update to 1.9.1
    This patch fixes two bugs from 1.9.0.
    The Profile tab is now visible while a profile runs,
    and vars.get no longer returns template text when a
    @const has the same name.

--- www/resterm/go-modules.mk
+++ www/resterm/go-modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: go-modules.mk,v 1.38 2026/09/25 07:20:52 drixter Exp $
+# $NetBSD: go-modules.mk,v 1.39 2026/09/26 12:47:39 drixter Exp $
 
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/!make!now!just/heredoc/@v/v1.0.0.zip
